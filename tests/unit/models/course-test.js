fe7745e26cb991b9f2299fc3cb70dfc261aef586
Jérémy Buget <jbuget@octo.com>
2016-07-15 12:39:36 +0200

    Remove useless tests and add some model tests

--- tests/unit/models/course-test.js
+++ tests/unit/models/course-test.js
@@ -1,12 +1,23 @@
 import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('course', 'Unit | Model | course', {
-  // Specify the other units that are required for this test.
   needs: []
 });
 
-test('it exists', function(assert) {
+test('it exists', function (assert) {
   let model = this.subject();
   // let store = this.store();
   assert.ok(!!model);
+});
+
+test('should have a name', function (assert) {
+  let model = this.subject({ name: 'My super test' });
+
+  assert.equal(model.get('name'), 'My super test', '');
+});
+
+test('may have a description', function (assert) {
+  let model = this.subject({ description: '<p>Coucou les tests</p>' });
+
+  assert.equal(model.get('description'), '<p>Coucou les tests</p>', '');
 });
