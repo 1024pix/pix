4bf4c2bd6e7419a03e2d556b9a31521fb66d4658
Jérémy Buget <jbuget@octo.com>
2016-07-18 18:17:33 +0200

    Sketch some challenges

--- app/router.js
+++ app/router.js
@@ -7,11 +7,16 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('index', { path: '/' });
-  this.route('home');
   this.route('courses', { path: '/parcours' });
   this.route('assessment-create', { path: '/course/:id_course/create_assessment' });
   this.route('assessment-show', { path: '/assessment/:id_assessment' });
+
+  /* sketeched routes */
+  this.route('home');
   this.route('preferences');
+  this.route('challenge-123');
+  this.route('challenge-456');
+  this.route('challenge-789');
 });
 
 export default Router;
