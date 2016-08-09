a53e95bde8c276dc5a7efca16de11c96bfddafcd
Jérémy Buget <jbuget@octo.com>
2016-08-09 14:58:31 +0200

    Replace all instances of term 'test' by 'course' in home page

--- tests/acceptance/2-voir-liste-tests-test.js
+++ tests/acceptance/2-voir-liste-tests-test.js
@@ -32,22 +32,22 @@ describe('Acceptance | 2 - voir la liste des tests', function() {
 
   it('2.1 la liste des tests apparaît', function () {
     expect(findWithAssert('.title').text()).to.contains('Liste des tests');
-    expect(findWithAssert('.enveloppe-test')).to.have.length.above(1);
+    expect(findWithAssert('.course')).to.have.length.above(1);
   });
 
   it('2.2 chaque test possède un intitulé', function () {
-    expect(findWithAssert('.enveloppe-test:nth(0) .intitule-test').text()).to.contains(courses[0].attrs.name);
+    expect(findWithAssert('.course:nth(0) .course-name').text()).to.contains(courses[0].attrs.name);
   });
 
   it('2.3 chaque test possède une description courte', function () {
-    expect(findWithAssert('.enveloppe-test:nth(0) .description-test').text()).to.contains(courses[0].attrs.description);
+    expect(findWithAssert('.course:nth(0) .course-description').text()).to.contains(courses[0].attrs.description);
   });
 
   it('2.4 chaque test possède une image', function () {
-    expect(findWithAssert('.enveloppe-test:nth(0) img')).to.be.ok;
+    expect(findWithAssert('.course:nth(0) img')).to.be.ok;
   });
 
   it('2.5 chaque test possède un bouton démarrer le test', function () {
-    expect(findWithAssert('.enveloppe-test:nth(0) a.btn').text()).to.contains('Démarrer le Test');
+    expect(findWithAssert('.course:nth(0) a.btn').text()).to.contains('Démarrer le Test');
   });
 });
