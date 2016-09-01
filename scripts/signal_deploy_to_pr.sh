87aa2af3c4145ed2e04719e0782e7e283bcef39c
Thomas Wickham <twickham@octo.com>
2016-09-01 22:02:31 +0200

    no PR is NOT a fatal error for the script

--- scripts/signal_deploy_to_pr.sh
+++ scripts/signal_deploy_to_pr.sh
@@ -4,13 +4,13 @@
 	echo 'FATAL: $GITHUB_TOKEN is absent'
 	exit 1
 }
-[ -z $CI_PULL_REQUEST ] && {
-	echo 'FATAL: $CI_PULL_REQUEST is absent'
-	exit 1
-}
 [ -z $CIRCLE_BRANCH ] && {
 	echo 'FATAL: $CIRCLE_BRANCH is absent'
 	exit 1
+}
+[ -z $CI_PULL_REQUEST ] && {
+	echo 'INFO: $CI_PULL_REQUEST is absent. I will not post a message to github. Bye !'
+	exit 0
 }
 
 PR_NUMBER=`echo $CI_PULL_REQUEST | grep -Po '(?<=pix-live/pull/)(\d+)'`
