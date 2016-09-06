c7519728b634a9136638126df3d04f2e380c3072
Jérémy Buget <jbuget@octo.com>
2016-09-06 16:08:35 +0200

    Fix for US-40 : having user's data during assessment creation

--- app/routes/courses/create-assessment.js
+++ app/routes/courses/create-assessment.js
@@ -3,10 +3,18 @@ import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
 
+  session: Ember.inject.service(),
+
   model(params) {
+
     const store = this.get('store');
     return store.findRecord('course', params.course_id).then((course) => {
-      const assessment = store.createRecord('assessment', { course });
+
+      // FIXME : add (route?) tests
+      const userName = `${this.get('session.firstname')} ${this.get('session.lastname')}`;
+      const userEmail = this.get('session.email');
+
+      const assessment = store.createRecord('assessment', { course, userName, userEmail });
       return RSVP.hash({
         assessment: assessment.save(),
         challenge: course.get('challenges.firstObject')
