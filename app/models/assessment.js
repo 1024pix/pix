635db76c4fafe5788d213fcb1c6b26be482a4f46
Jérémy Buget <jbuget@octo.com>
2016-09-07 18:44:59 +0200

    Take into account skipped answers in assessement's answers count on results page

--- app/models/assessment.js
+++ app/models/assessment.js
@@ -1,13 +1,19 @@
 import DS from 'ember-data';
 
 const { attr, Model, belongsTo, hasMany } = DS;
+const { computed, isEqual } = Ember;
 
 export default Model.extend({
 
   course: belongsTo('course', { inverse: null }),
   answers: hasMany('answer'),
-
   userName: attr('string'),
-  userEmail: attr('string')
+  userEmail: attr('string'),
+
+  numberOfValidatedAnswers: computed('answers', function () {
+    return this.get('answers').filter((answer) => {
+      return !isEqual(answer.get('value'), '#ABAND#');
+    }).get('length');
+  })
 
 });
