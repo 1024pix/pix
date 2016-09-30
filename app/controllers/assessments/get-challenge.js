e776ed1376223cde9d4c7369882ab16ecb1e97a6
David Boureau <bdavidxyz@users.noreply.github.com>
2016-09-30 19:20:46 +0200

    BSR : Clean controllers (#68)
    * cleaning one controller, transferring responsibilities to route
    
    * preparing refactoring

--- app/controllers/assessments/get-challenge.js
+++ app/controllers/assessments/get-challenge.js
@@ -1,34 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-
-  assessmentService: Ember.inject.service('assessment'),
-
-  saveAnswerAndNavigate(currentChallenge, assessment, answerValue) {
-
-    const answer = this._createAnswer(answerValue, currentChallenge, assessment);
-    answer.save().then(() => {
-      this._navigateToNextView(currentChallenge, assessment);
-    });
-  },
-
-  _createAnswer: function (answerValue, currentChallenge, assessment) {
-
-    return this.get('store').createRecord('answer', {
-      value: answerValue,
-      challenge: currentChallenge,
-      assessment
-    });
-  },
-
-  _navigateToNextView(currentChallenge, assessment) {
-
-    this.get('assessmentService').getNextChallenge(currentChallenge, assessment).then((challenge) => {
-      if (challenge) {
-        return this.transitionToRoute('assessments.get-challenge', { challenge, assessment });
-      }
-      return this.transitionToRoute('assessments.get-results', { assessment });
-    });
-  }
-
 });
--- app/routes/assessments/get-challenge.js
+++ app/routes/assessments/get-challenge.js
@@ -4,11 +4,45 @@ import DS from 'ember-data';
 
 export default Ember.Route.extend({
 
+  assessmentService: Ember.inject.service('assessment'),
+
   model(params) {
     const store = this.get('store');
+    const assessmentPromise = store.findRecord('assessment', params.assessment_id);
+    const challengePromise = store.findRecord('challenge', params.challenge_id);
+
     return RSVP.hash({
-      assessment: store.findRecord('assessment', params.assessment_id),
-      challenge: store.findRecord('challenge', params.challenge_id)
+      assessment: assessmentPromise,
+      challenge: challengePromise
+    });
+  },
+
+  actions : {
+
+    saveAnswerAndNavigate: function (currentChallenge, assessment, answerValue) {
+      const answer = this._createAnswer(answerValue, currentChallenge, assessment);
+      answer.save().then(() => {
+        this._navigateToNextView(currentChallenge, assessment);
+      });
+    }
+  },
+
+  _createAnswer: function (answerValue, currentChallenge, assessment) {
+
+    return this.get('store').createRecord('answer', {
+      value: answerValue,
+      challenge: currentChallenge,
+      assessment
+    });
+  },
+
+  _navigateToNextView: function (currentChallenge, assessment) {
+
+    this.get('assessmentService').getNextChallenge(currentChallenge, assessment).then((challenge) => {
+      if (challenge) {
+        return this.transitionTo('assessments.get-challenge', { challenge, assessment });
+      }
+      return this.transitionTo('assessments.get-results', { assessment });
     });
   },
 
