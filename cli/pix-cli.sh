c4ccaf2e326e4277f7c5c5dbfaa7cc8f8cd5f4a4
Jérémy Buget <jbuget@users.noreply.github.com>
2017-01-24 12:02:30 +0100

    Fix db:backup command ; rename logs:db into db:logs and logs:app into app:logs (#250)
    [#250] [BUGFIX] Correction de la commande `db:backup` du PIX-CLI

--- cli/pix-cli.sh
+++ cli/pix-cli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PIX_CLI_VERSION="1.1.0"
+PIX_CLI_VERSION="1.2.0"
 
 COMMAND=$1
 
@@ -18,8 +18,8 @@ if [ "$COMMAND" == "help" ]; then
   echo "  help                                          # Display PIX-CLI usage"
   echo "  db:backup <environment>                       # Export a dump of the postgres service database"
   echo "  db:restore <environment> <file>               # Restore a database by importing a PostgreSQL dump file"
-  echo "  logs:app <environment>                        # Show the last log(s) of the given environment"
-  echo "  logs:db <environment>                         # Print the most recent log(s) for the database of the given environment"
+  echo "  db:logs <environment>                         # Print the most recent log(s) for the database of the given environment"
+  echo "  app:logs <environment>                        # Show the last log(s) of the given environment"
   echo "  ssh-keys:add <name> </path/to/key>            # Add a new public key by pipe or path (alias for Dokku command 'ssh-keys:add')"
   echo "  ssh-keys:list                                 # List of all authorized dokku public ssh keys (alias for Dokku command 'ssh-keys:list')"
   echo "  ssh-keys:remove <name>                        # Remove SSH public key by name (alias for Dokku command ssh-keys:remove'')"
@@ -42,9 +42,13 @@ if [ "$COMMAND" == "db:backup" ]; then
     exit 1
   fi
 
-  BACKUPS_DB_FOLDER="~/backups/db"
+  BACKUPS_DB_FOLDER=~/backups/db
   DATE=`date +%Y%m%d`
-  DUMP_FILE="$BACKUPS_DB_FOLDER/$DATE/$ENVIRONMENT.dump"
+  DUMP_FOLDER=$BACKUPS_DB_FOLDER/$DATE
+  DUMP_FILE=$DUMP_FOLDER/$ENVIRONMENT.dump
+
+  mkdir $DUMP_FOLDER
+  dokku postgres:export pg-$ENVIRONMENT > $DUMP_FILE
 
   echo "A dump file for $ENVIRONMENT database was created at $DUMP_FILE."
   exit 0
@@ -71,34 +75,15 @@ if [ "$COMMAND" == "db:restore" ]; then
     exit 1
   fi
 
-  dokku postgres:import pg-$ENVIRONMENT $DUMP_FILE
+  dokku postgres:import pg-$ENVIRONMENT < $DUMP_FILE
 
   echo "The $ENVIRONMENT database was restored."
   exit 0
 fi
 
-# pix logs:app <environment>
-# ex: pix logs:app production
-if [ "$COMMAND" == "logs:app" ]; then
-  if [ $# -ne 2 ]; then
-    echo "Bad number of arguments. See usage for command $COMMAND."
-    exit 1
-  fi
-
-  ENVIRONMENT=$2
-  if [ "$ENVIRONMENT" != "production" ] && [ "$ENVIRONMENT" != "staging" ]; then
-    echo "Bad environment supplied. Expected environment 'production' or 'staging'."
-    exit 1
-  fi
-
-  dokku logs api-$ENVIRONMENT
-
-  exit 0
-fi
-
-# pix logs:db <environment>
-# ex: pix logs:db production
-if [ "$COMMAND" == "logs:db" ]; then
+# pix db:logs <environment>
+# ex: pix db:logs production
+if [ "$COMMAND" == "db:logs" ]; then
   if [ $# -ne 2 ]; then
     echo "Bad number of arguments. See usage for command $COMMAND."
     exit 1
@@ -111,6 +96,25 @@ if [ "$COMMAND" == "logs:db" ]; then
   fi
 
   dokku postgres:logs pg-$ENVIRONMENT
+
+  exit 0
+fi
+
+# pix app:logs <environment>
+# ex: pix app:logs production
+if [ "$COMMAND" == "app:logs" ]; then
+  if [ $# -ne 2 ]; then
+    echo "Bad number of arguments. See usage for command $COMMAND."
+    exit 1
+  fi
+
+  ENVIRONMENT=$2
+  if [ "$ENVIRONMENT" != "production" ] && [ "$ENVIRONMENT" != "staging" ]; then
+    echo "Bad environment supplied. Expected environment 'production' or 'staging'."
+    exit 1
+  fi
+
+  dokku logs api-$ENVIRONMENT
 
   exit 0
 fi
