9fdf97e976580b571c136eeed52ebbfa74bbad55
Jérémy Buget <jbuget@users.noreply.github.com>
2017-02-23 18:57:19 +0100

    Should close issue #274 (#307)

--- live/app/adapters/solution.js
+++ live/app/adapters/solution.js
@@ -1,14 +1,12 @@
 import ApplicationAdapter from './application';
 import Ember from 'ember';
+import RSVP from 'rsvp';
 
 export default ApplicationAdapter.extend({
-  // XXX : can't find in the docs why query params are in 3rd position
-  // XXX : need the small 'if' for production. Hacky, icky, ugly.
+
   queryRecord(modelName, clazz, query) {
-    let prefix = '/';
-    if (this.host !== '/') {
-      prefix = this.host + ('/');
-    }
-    return Ember.$.getJSON( prefix + this.namespace + '/assessments/' + query.assessmentId +  '/solutions/' + query.answerId);
+    return Ember.$.getJSON( `${this.host}/${this.namespace}/assessments/${query.assessmentId}/solutions/${query.answerId}`, (data) => {
+      return RSVP.resolve(data);
+    });
   }
 });
