beea183636b98f0a8119625365e8af37384edc4d
Adrien SAUNIER <asaunier@octo.com>
2017-04-28 16:35:52 +0200

    Revert "FIX - Removing unique constraint on user.email from migration script"
    This reverts commit c6cedc7fa613c9f376b5d59938acdaad1e4eae7f.

--- api/db/migrations/20170418114929_remove_login_from_users.js
+++ api/db/migrations/20170418114929_remove_login_from_users.js
@@ -6,6 +6,7 @@ exports.up = function(knex, Promise) {
     knex.schema.table(TABLE_NAME, function (table) {
       table.dropColumn('login');
       table.boolean('cgu');
+      table.unique('email');
     })
   ]);
 
