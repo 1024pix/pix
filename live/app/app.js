779788759c4b3e3332db896e78dfc87c2de89e4d
Pierre de La Morinerie <kemenaran@gmail.com>
2017-05-22 17:09:04 +0200

    [#424] [CLEANUP] Corrige des dépréciations dans les tests (#424)
    * Supprime Ember.MODEL_FACTORY_INJECTIONS
    
    Ce comportement est devenu le comportement par défaut. Cela supprime
    un warning de dépréciation pendant les tests.
    
    * Corrige une dépréciation sur le routeur
    
    Corrige le message :
    
    ```
    DEPRECATION: Usage of "router" is deprecated, use "_routerMicrolib" instead.
    ```
    
    Cf. https://github.com/dbbk/ember-routable-modal/pull/4

--- live/app/app.js
+++ live/app/app.js
@@ -3,8 +3,6 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-Ember.MODEL_FACTORY_INJECTIONS = true;
-
 const App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
