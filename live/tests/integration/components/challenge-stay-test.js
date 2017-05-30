7cf1fc89015a21f0d232c57d21c0a0fb9d0ca788
Jérémy Buget <jbuget@users.noreply.github.com>
2017-05-30 12:11:03 +0200

    [#431] [CLEANUP] Extraction des images SVG dans le code (part 2). (#431)
    * Extract challenge-stay SVG image
    
    * Extract qroc-solution-panel SVG arrow image
    
    * Remove useless component template components/get-result
    
    * Extract qrocm-ind-solution-panel SVG arrow file
    
    * Make test helper config be like a new project's one

--- live/tests/integration/components/challenge-stay-test.js
+++ live/tests/integration/components/challenge-stay-test.js
@@ -14,4 +14,14 @@ describe('Integration | Component | challenge stay', function() {
     expect(this.$()).to.have.length(1);
   });
 
+  it('should display a warning icon with an accessible description', function() {
+    // when
+    this.render(hbs`{{challenge-stay}}`);
+
+    // then
+    const $img = this.$('.challenge-stay__icon-img');
+    expect($img).to.have.lengthOf(1);
+    expect($img.attr('src')).to.equal('/images/icon-warning.svg');
+    expect($img.attr('alt')).to.not.be.empty;
+  });
 });
