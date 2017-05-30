23976f452fc266ac09d4ec7b5b2c2b7101b93021
Adrien SAUNIER <asaunier@octo.com>
2017-05-30 15:43:40 +0200

    [US-213] [FEATURE] Signin-form transmit informations on submit

--- live/app/components/signin-form.js
+++ live/app/components/signin-form.js
@@ -2,6 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  classNames: ['signin-form-container']
+  classNames: [ 'signin-form-container' ],
+
+  signin: null,
+  email: '',
+  password: '',
+
+  actions: {
+    submit() {
+      this.get('onSubmit')(this.get('email'), this.get('password'));
+    }
+  }
 
 });
--- live/tests/integration/components/signin-form-test.js
+++ live/tests/integration/components/signin-form-test.js
@@ -1,16 +1,54 @@
-import { expect } from 'chai';
-import { describe, it } from 'mocha';
-import { setupComponentTest } from 'ember-mocha';
+import {expect} from 'chai';
+import {describe, it} from 'mocha';
+import {setupComponentTest} from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
 
 describe('Integration | Component | signin form', function() {
+
   setupComponentTest('signin-form', {
     integration: true
   });
 
-  it('renders', function() {
-    this.render(hbs`{{signin-form}}`);
+  const expectedEmail = 'email@example.fr';
+  const expectedPassword = 'azerty';
 
-    expect(this.$()).to.have.length(1);
+  it('should give email and password to action given in parameter', function(done) {
+    // Expect
+    this.on('onSubmitAction', (email, password) => {
+      expect(email).to.equal(expectedEmail);
+      expect(password).to.equal(expectedPassword);
+      done();
+    });
+
+    // Given
+    this.render(hbs`{{signin-form onSubmit=(action 'onSubmitAction')}}`);
+
+    _fillSigninForm(this, expectedEmail, expectedPassword);
+
+    // When
+    this.$('button[type=submit]').click();
   });
+
+  it('should also use action on submit', function(done) {
+    // Expect
+    this.on('onSubmitAction', (email, password) => {
+      expect(email).to.equal(expectedEmail);
+      expect(password).to.equal(expectedPassword);
+      done();
+    });
+
+    this.render(hbs`{{signin-form onSubmit=(action 'onSubmitAction')}}`);
+    _fillSigninForm(this, expectedEmail, expectedPassword);
+
+    // When
+    this.$('.signin-form__form form').submit();
+  });
+
+  function _fillSigninForm(context, email, password) {
+    context.$('#pix-email').val(email);
+    context.$('#pix-email').change();
+
+    context.$('#pix-password').val(password);
+    context.$('#pix-password').change();
+  }
 });
