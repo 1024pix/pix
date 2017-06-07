9f0958934a02a85dfaeb14a7c103b2d1949513ec
Brandone Martins <brandone.martins@gmail.com>
2017-06-07 12:06:49 +0200

    Add the model in front

---
+++ live/app/models/serie.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+const { Model, attr, hasMany } = DS;
+
+export default Model.extend({
+
+  name: attr('string'),
+  courses: hasMany('course', { inverse: null })
+
+});
