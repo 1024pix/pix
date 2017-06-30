0ee24458090b3aaadadeec1bd004dcbc75456e67
Brandone Martins <brandone.martins@gmail.com>
2017-06-30 11:07:58 +0200

    delete comment in scenario mirage

--- live/mirage/scenarios/default.js
+++ live/mirage/scenarios/default.js
@@ -1,7 +1,5 @@
 export default function(server) {
 
-  //console.log('je suis passé dans le scenario');
-
   const courses = server.createList('course', 2, {name: 'course name'});
   server.createList('courseGroup', 3, {courses});
 
