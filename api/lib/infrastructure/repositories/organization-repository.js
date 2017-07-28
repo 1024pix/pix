ca914c65435694237c1ed9fb84adb0ca5641b7f3
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2017-07-28 17:47:49 +0200

    [FEATURE] isCodeAvailable() in organization repository

--- api/lib/infrastructure/repositories/organization-repository.js
+++ api/lib/infrastructure/repositories/organization-repository.js
@@ -1,8 +1,23 @@
+const Organization = require('../../domain/models/data/organization');
 
 module.exports = {
 
   saveFromModel(organizationModel) {
     return organizationModel.save();
+  },
+
+  isCodeAvailable(code) {
+    return Organization
+      .where({ code })
+      .fetch()
+      .then(codes => {
+
+        if(codes) {
+          return Promise.reject();
+        }
+
+        return code;
+      });
   }
 
 };
