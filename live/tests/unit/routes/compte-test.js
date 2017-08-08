cfeb0dccd49da109bec90c5571fb9155ec2a10ba
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2017-08-08 15:53:51 +0200

    [FEATURE] creating searchForOrganization action

--- live/tests/unit/routes/compte-test.js
+++ live/tests/unit/routes/compte-test.js
@@ -1,6 +1,8 @@
+import Ember from 'ember';
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupTest } from 'ember-mocha';
+import sinon  from 'sinon';
 
 describe('Unit | Route | compte', function() {
   setupTest('route:compte', {
@@ -18,5 +20,54 @@ describe('Unit | Route | compte', function() {
 
     // Then
     expect(route.authenticationRoute).to.equal('/');
+  });
+
+  describe('searchForOrganization', function() {
+
+    let storeQueryStub;
+    let storyStub;
+    let apiResult;
+
+    beforeEach(() => {
+      apiResult = {};
+
+      storeQueryStub = sinon.stub().resolves(apiResult);
+      storyStub = Ember.Service.extend({
+        query: storeQueryStub
+      });
+    });
+
+    it('should search for an organization', function() {
+      // Given
+      this.register('service:store', storyStub);
+      this.inject.service('store', { as: 'store' });
+
+      const route = this.subject();
+
+      // When
+      route.actions.searchForOrganization.call(route, 'RVSG44');
+
+      // Then
+      sinon.assert.calledOnce(storeQueryStub);
+      sinon.assert.calledWith(storeQueryStub, 'organization', {
+        filter: {
+          code: 'RVSG44'
+        }
+      });
+    });
+
+    it('should return the results as promise', function() {
+      // Given
+      this.register('service:store', storyStub);
+      this.inject.service('store', { as: 'store' });
+      const route = this.subject();
+
+      // When
+      const routeActionResult = route.actions.searchForOrganization.call(route, 'RVSG44');
+
+      return routeActionResult.then(function(organizations) {
+        expect(organizations).to.deep.equal(apiResult);
+      });
+    });
   });
 });
