95b10d1dbf19973c00253468ee175561347a1cd0
hypernikao <fenoh@octo.com>
2017-08-18 15:38:08 +0200

    Remove unused test in snapshot model

--- live/tests/unit/models/snapshot-test.js
+++ live/tests/unit/models/snapshot-test.js
@@ -4,14 +4,11 @@ import {setupModelTest} from 'ember-mocha';
 
 describe('Snapshot', function() {
   setupModelTest('snapshot', {
-    // Specify the other units that are required for this test.
     needs: []
   });
 
-  // Replace this with your real tests.
   it('exists', function() {
     const model = this.subject();
-    // var store = this.store();
     expect(model).to.be.ok;
   });
 });
