621725a397ad4d64586da354726e24e82c46e57d
hypernikao <fenoh@octo.com>
2017-08-18 11:58:02 +0200

    Add unit test to snapshot index route

---
+++ api/tests/unit/application/snapshots/index_test.js
@@ -0,0 +1,34 @@
+const { describe, it, before, after, beforeEach, expect, sinon } = require('../../../test-helper');
+const Hapi = require('hapi');
+
+const snapshotController = require('../../../../lib/application/snapshots/snapshot-controller');
+
+describe('Unit | Router | snapshot-router', () => {
+
+  let server;
+
+  beforeEach(() => {
+    server = new Hapi.Server();
+    server.connection({ port: null });
+    server.register({ register: require('../../../../lib/application/snapshots') });
+  });
+
+  describe('POST /api/snapshots', _ => {
+
+    before(() => {
+      sinon.stub(snapshotController, 'create').callsFake((request, reply) => reply('ok'));
+    });
+
+    after(() => {
+      snapshotController.create.restore();
+    });
+
+    it('should exist', (done) => {
+      return server.inject({ method: 'POST', url: '/api/snapshots' }, res => {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
+
+});
