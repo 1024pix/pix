06eec24ee7fb383aeb4fb9ca4bfe46521b5ffab0
hypernikao <fenoh@octo.com>
2017-08-21 18:42:17 +0200

    Update snapshot model with constraint on organisationId

--- api/lib/domain/models/data/snapshot.js
+++ api/lib/domain/models/data/snapshot.js
@@ -1,5 +1,10 @@
 const Bookshelf = require('../../../infrastructure/bookshelf');
+const Organization = require('./organization');
 
 module.exports = Bookshelf.Model.extend({
-  tableName: 'snapshots'
+  tableName: 'snapshots',
+  
+  organizations() {
+    return this.belongsTo(Organization);
+  }
 });
