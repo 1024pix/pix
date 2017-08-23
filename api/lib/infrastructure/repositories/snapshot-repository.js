7a6b940e34fb61af2febae5435fb85d3c25cfe0c
hypernikao <fenoh@octo.com>
2017-08-23 11:38:42 +0200

    Refacto snapshot repository with unit test

--- api/lib/infrastructure/repositories/snapshot-repository.js
+++ api/lib/infrastructure/repositories/snapshot-repository.js
@@ -2,6 +2,13 @@ const Snapshot = require('../../domain/models/data/snapshot');
 
 module.exports = {
   save(snapshotRawData) {
-    return new Snapshot(snapshotRawData).save();
+    return new Snapshot(snapshotRawData)
+      .save()
+      .then((snapshot) => {
+        return snapshot.get('id');
+      })
+      .catch((err) => {
+        throw err;
+      });
   }
 };
