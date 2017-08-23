ea1ce51f27cc1c45700a2ead1e91ea748ee95048
hypernikao <fenoh@octo.com>
2017-08-23 12:24:28 +0200

    Refacto snapshot repository wirh logicless

--- api/lib/infrastructure/repositories/snapshot-repository.js
+++ api/lib/infrastructure/repositories/snapshot-repository.js
@@ -3,12 +3,6 @@ const Snapshot = require('../../domain/models/data/snapshot');
 module.exports = {
   save(snapshotRawData) {
     return new Snapshot(snapshotRawData)
-      .save()
-      .then((snapshot) => {
-        return snapshot.get('id');
-      })
-      .catch((err) => {
-        throw err;
-      });
+      .save();
   }
 };
