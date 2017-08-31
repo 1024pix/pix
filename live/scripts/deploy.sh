def34ff7a2374a8677d839d99eea32fbb5fe191b
Jérémy Buget <jbuget@gmail.com>
2017-09-01 01:02:16 +0200

    Modify live/scripts/deploy.sh script

--- live/scripts/deploy.sh
+++ live/scripts/deploy.sh
@@ -58,7 +58,7 @@ GIT_HASH=`git rev-parse HEAD`
 (ember build --environment $BUILD_ENV --output-path $tmpdir \
     && git checkout gh-pages                                \
     && git pull origin gh-pages                             \
-    && ( [ -d ./$BUILD_OUTPUT ] && rm -rf ./$BUILD_OUTPUT)  \
+    && ( [ -d ./$BUILD_OUTPUT ] && rm -rf ./$BUILD_OUTPUT ) \
     && mv $tmpdir ./$BUILD_OUTPUT                           \
     && git add -A ./$BUILD_OUTPUT                           \
     && git commit -m "Release of $BUILD_OUTPUT with env $BUILD_ENV (via commit hash: $GIT_CURRENT_HASH)" \
