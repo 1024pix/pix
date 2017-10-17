894cd03b2f14e19ab0afbdce9f6881c337d60864
Brandone MARTINS <brandone.martins@canal-plus.com>
2017-10-17 18:32:00 +0200

    [#561] [CLEANUP] Remaniement de la route courses/create-assessment (US-866). (#561)
    * delete useless redirection to get-results and if condition on challenge
    
    * remove useless routes courses/get-course-preview
    
    delete acceptance test pour courses/get-course-preview
    
    * delete create-assessment-old
    
    * flatten promise afterModel of createAssessment route
    
    * Useless change to fix build

--- live/tests/acceptance/a4-demarrer-un-test-test.js
+++ live/tests/acceptance/a4-demarrer-un-test-test.js
@@ -26,11 +26,6 @@ describe('Acceptance | a4 - Démarrer un test |', function() {
     expect(_.endsWith(currentURL(), 'assessments/ref_assessment_id/challenges/ref_qcm_challenge_id')).to.be.true;
   });
 
-  it('a4.2 Je peux démarrer un test directement depuis l\'ancienne url "courses/:course_id/assessment"', async function() {
-    await visit('/courses/ref_course_id/assessment');
-    expect(_.endsWith(currentURL(), 'assessments/ref_assessment_id/challenges/ref_qcm_challenge_id')).to.be.true;
-  });
-
   it('a4.4 Quand je démarre un test, je suis redirigé vers la première épreuve du test', function() {
     const $startLink = findWithAssert(START_BUTTON);
     return click($startLink).then(function() {
@@ -40,7 +35,6 @@ describe('Acceptance | a4 - Démarrer un test |', function() {
   });
 
   it('a4.5 Quand je démarre un test sur mobile, une modale m\'averti que l\'expérience ne sera pas optimale, mais je peux quand même continuer', function(done) {
-
     const $startLink = findWithAssert(START_BUTTON);
 
     expect(find(MODAL_SELECTOR)).to.have.lengthOf(0);
