cd8063fff1cc2859049ae612356b10215b936bf5
mlaurent <34855127+mlaurent@users.noreply.github.com>
2018-01-22 10:32:14 +0100

    [US-998] Acces page resultats certification redirige vers homepage (#660)
    [#660] [FEATURE] Page de résultats de certification est rendue inaccessible (renvoi vers homepage) (US-998)

--- live/tests/unit/routes/assessments/results-test.js
+++ live/tests/unit/routes/assessments/results-test.js
@@ -1,6 +1,8 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupTest } from 'ember-mocha';
+import sinon from 'sinon';
+import Ember from 'ember';
 
 describe('Unit | Route | Assessments.ResultsRoute', function() {
 
@@ -11,6 +13,38 @@ describe('Unit | Route | Assessments.ResultsRoute', function() {
   it('exists', function() {
     const route = this.subject();
     expect(route).to.be.ok;
+  });
+
+  describe('#afterModel', function() {
+
+    it('should redirect to homepage if assessment is a certification', function() {
+      // given
+      const route = this.subject();
+      route.transitionTo = sinon.spy();
+
+      const certification = Ember.Object.create({ id: 123, isCertification: true });
+
+      // when
+      route.afterModel(certification);
+
+      // then
+      sinon.assert.calledWith(route.transitionTo, 'index');
+    });
+
+    it('should not redirect to homepage if assessment is not a certification', function() {
+      // given
+      const route = this.subject();
+      route.transitionTo = sinon.spy();
+
+      const certification = Ember.Object.create({ id: 123, isCertification: false });
+
+      // when
+      route.afterModel(certification);
+
+      // then
+      sinon.assert.notCalled(route.transitionTo);
+    });
+
   });
 
 });
