b61f8103aa26a9f279e149c220e6fc0f0b4d739f
qcattez <quentin.cattez@beta.gouv.fr>
2018-01-29 13:37:21 +0100

    [#650] [TECH] Ajout d'un script de déploiement pour l'appli Maths (US-1010). (#650)
    * adding script to deploy math app
    
    * merging deploy preview & maths scripts

--- scripts/release/deploy-side-apps.sh
+++ scripts/release/deploy-side-apps.sh
@@ -22,22 +22,25 @@ PACKAGE_VERSION=$(cat package.json \
   | sed 's/[",]//g' \
   | tr -d '[[:space:]]')
 
-echo -e "Updating ${GREEN}preview${RESET_COLOR} environment.\n"
-
 echo -e "Updating local version of ${YELLOW}master${RESET_COLOR} branch.\n"
 git checkout master
 git pull
 
-echo -e "Rebasing ${YELLOW}preview${RESET_COLOR} on ${YELLOW}master${RESET_COLOR}.\n"
-git checkout preview
-git pull
-git rebase master
+for env in preview maths
+do 
+  echo -e "Updating ${GREEN}${env}${RESET_COLOR} environment.\n"
 
-git commit -m "[RELEASE] Updating the preview environment to the new version ${PACKAGE_VERSION}." --allow-empty
+  echo -e "Rebasing ${YELLOW}${env}${RESET_COLOR} on ${YELLOW}master${RESET_COLOR}.\n"
+  git checkout ${env}
+  git pull
+  git rebase master
 
-git push origin preview --force
+  git commit -m "[RELEASE] Updating the ${env} environment to the new version ${PACKAGE_VERSION}." --allow-empty
+
+  git push origin ${env} --force
+
+  echo -e "${YELLOW}${env}${RESET_COLOR} environement is updated to ${YELLOW}master${RESET_COLOR}.\n"
+  echo -e "Circle CI will deploy the new version within few minutes.\n"
+done
 
 git checkout dev
-
-echo -e "${YELLOW}Preview${RESET_COLOR} environement is updated to ${YELLOW}master${RESET_COLOR}.\n"
-echo -e "Circle CI will deploy the new version within few minutes"
