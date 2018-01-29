71a4c174c0f757b7c53d015dae9d0eaa7f0525ec
Thomas Wickham <mackwic@gmail.com>
2018-01-29 17:14:38 +0100

    Refresh the file in the dev branch

--- api/db/migrations/20180116161332_modify_column_completedPercentage_to_tests_finished_in_snapshots.js
+++ api/db/migrations/20180116161332_modify_column_completedPercentage_to_tests_finished_in_snapshots.js
@@ -9,7 +9,7 @@ exports.up = function(knex, Promise) {
   }).then(() => {
     return knex(TABLE_NAME)
       .update({
-        // XXX : the '??' bind the row value
+        // XXX: the '??' bind the row value
         testsFinished: knex.raw('ROUND(?? * ' + MULTIPLICATOR_PERCENTAGE_TO_COMPETENCES + ', 0)', ['completionPercentage'])
       });
   }).then(() => {
