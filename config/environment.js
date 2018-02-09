acde431e01ada38ba17116ebf5426f62bb9e398b
Jérémy Buget <jbuget@gmail.com>
2018-02-09 22:15:44 +0100

    Add Google fonts

--- config/environment.js
+++ config/environment.js
@@ -20,6 +20,17 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    googleFonts: [
+      'Open+Sans',
+      'Raleway'
+    ],
+
+    // Set or update content security policies
+    contentSecurityPolicy: {
+      'font-src': "'self' fonts.gstatic.com",
+      'style-src': "'self' fonts.googleapis.com"
     }
   };
 
