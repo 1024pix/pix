7f30ffe8d9859c7d77a68846488e9b44f3c556e3
Melanie B <31532858+MelanieMEB@users.noreply.github.com>
2018-02-18 10:19:45 +0100

    [#684] [FEATURE] Changement du texte dans le partage de profil pour les PRO (US-1086). (#684)
    * Change text in shared profile page
    
    * Change text for shared profile + add one text

--- live/tests/unit/components/share-profile-test.js
+++ live/tests/unit/components/share-profile-test.js
@@ -127,11 +127,12 @@ describe('Unit | Component | share-profile', function() {
 
       // then
       expect(organizationLabel.text1).to.equal('Vous vous apprêtez à transmettre une copie de votre profil Pix à l\'organisation :');
-      expect(organizationLabel.text2).to.equal('En cliquant sur le bouton « Envoyer », elle recevra les informations suivantes :');
-      expect(organizationLabel.text3).to.equal('Elle ne recevra les évolutions futures de votre profil que si vous le partagez à nouveau.');
+      expect(organizationLabel.text2).to.equal('En cliquant sur le bouton « Envoyer », vous transmettrez à l\'organisation :');
+      expect(organizationLabel.text3).to.equal('votre ID-Pix et le code campagne');
+      expect(organizationLabel.text4).to.equal('L\'organisation ne recevra les évolutions futures de votre profil que si vous le partagez à nouveau.');
     });
 
-    it('should return adapted ("établissement"-based) labels when organization type is SUP or SCO', function() {
+    it('should return adapted ("établissement"-based) labels when organization type is SUP', function() {
       // given
       component.set('_organization', { type: 'SUP' });
 
@@ -140,8 +141,23 @@ describe('Unit | Component | share-profile', function() {
 
       // then
       expect(organizationLabel.text1).to.equal('Vous vous apprêtez à transmettre une copie de votre profil Pix à l\'établissement :');
-      expect(organizationLabel.text2).to.equal('En cliquant sur le bouton « Envoyer », il recevra les informations suivantes :');
-      expect(organizationLabel.text3).to.equal('Il ne recevra les évolutions futures de votre profil que si vous le partagez à nouveau.');
+      expect(organizationLabel.text2).to.equal('En cliquant sur le bouton « Envoyer », vous transmettrez à l\'établissement :');
+      expect(organizationLabel.text3).to.equal('votre numéro d\'étudiant et le code campagne');
+      expect(organizationLabel.text4).to.equal('L\'établissement ne recevra les évolutions futures de votre profil que si vous le partagez à nouveau.');
+    });
+
+    it('should return adapted ("établissement"-based) labels when organization type is SCO', function() {
+      // given
+      component.set('_organization', { type: 'SCO' });
+
+      // when
+      const organizationLabel = component.get('organizationLabels');
+
+      // then
+      expect(organizationLabel.text1).to.equal('Vous vous apprêtez à transmettre une copie de votre profil Pix à l\'établissement :');
+      expect(organizationLabel.text2).to.equal('En cliquant sur le bouton « Envoyer », vous transmettrez à l\'établissement :');
+      expect(organizationLabel.text3).to.equal('le code campagne');
+      expect(organizationLabel.text4).to.equal('L\'établissement ne recevra les évolutions futures de votre profil que si vous le partagez à nouveau.');
     });
 
   });
