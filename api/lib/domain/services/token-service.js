18a58bf16a45eb274b86993bafc7e5e810d253f9
Jérémy Buget <jbuget@users.noreply.github.com>
2018-02-21 17:35:02 +0100

    Remove email from JSON Web token (because it was useless) (#690)

--- api/lib/domain/services/token-service.js
+++ api/lib/domain/services/token-service.js
@@ -5,7 +5,6 @@ const settings = require('../../settings');
 function createTokenFromUser(user) {
   return jsonwebtoken.sign({
     user_id: user.get('id'),
-    email: user.get('email')
   }, settings.authentication.secret, { expiresIn: settings.authentication.tokenLifespan });
 }
 
