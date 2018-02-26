17534e24c6f3cbaacb54f1cedae9f7ca1bad7f44
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2018-02-26 16:13:28 +0100

    Migration step by step 2 - migration to corrections

---
+++ api/db/migrations/20180221162245_create_corrections_table.js
@@ -0,0 +1,25 @@
+const TABLE_NAME = 'corrections';
+
+exports.up = (knex) => {
+  return knex.schema
+    .createTable(TABLE_NAME, (t) => {
+      t.increments().primary();
+      t.dateTime('createdAt').notNullable().defaultTo(knex.fn.now());
+      t.integer('level');
+      t.integer('pixScore').unsigned();
+      t.text('emitter').notNull();
+      t.text('comment').notNull();
+      t.integer('assessmentId').unsigned().references('assessments.id');
+    })
+    .then(() => {
+      console.log(`${TABLE_NAME} table is created!`);
+    });
+};
+
+exports.down = (knex) => {
+  return knex.schema
+    .dropTable(TABLE_NAME)
+    .then(() => {
+      console.log(`${TABLE_NAME} table was dropped!`);
+    });
+};
