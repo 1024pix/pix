7ffc6ec6c0aa26b21f50ccd643eba7624fa289d1
Jérémy Buget <jbuget@gmail.com>
2018-03-07 01:50:04 +0100

    Take into account login errors

--- app/authenticators/oauth2.js
+++ app/authenticators/oauth2.js
@@ -1,6 +1,6 @@
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
 
 export default OAuth2PasswordGrant.extend({
-  serverTokenEndpoint: 'http://localhost:9000/token',
-  serverTokenRevocationEndpoint: 'http://localhost:9000/revoke'
+  serverTokenEndpoint: 'http://localhost:3000/token',
+  serverTokenRevocationEndpoint: 'http://localhost:3000/revoke'
 });
--- app/controllers/login.js
+++ app/controllers/login.js
@@ -7,8 +7,21 @@ export default Controller.extend({
   actions: {
     authenticate() {
       let { identification, password } = this.getProperties('identification', 'password');
-      this.get('session').authenticate('authenticator:oauth2', identification, password).catch((reason) => {
-        this.set('errorMessage', reason.error || reason);
+      this.get('session').authenticate('authenticator:oauth2', identification, password).catch((response) => {
+        if (response && response.errors && response.errors.length > 0) {
+          const firstError = response.errors[0];
+          switch (firstError.code) {
+            case '403':
+              this.set('errorMessage', 'Identifiants de connexion invalides');
+              break;
+            case '400':
+              this.set('errorMessage', 'Syntaxe (de requête) invalide');
+              break;
+            default:
+              this.set('errorMessage', 'Un problème est survenu');
+              break;
+          }
+        }
       });
     }
   }
