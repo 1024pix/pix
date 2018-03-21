f05f2c309b7650faefd138c885081f885bbd7e37
Melanie B <31532858+MelanieMEB@users.noreply.github.com>
2018-03-21 16:09:59 +0100

    [#716] [BUGFIX] Change name of tables session for sessions (US-1117).

--- api/db/migrations/20180315164647_add-column-session-id-on-certifications.js
+++ api/db/migrations/20180315164647_add-column-session-id-on-certifications.js
@@ -2,12 +2,12 @@ const TABLE_NAME = 'certification-courses';
 
 exports.up = function(knex, Promise) {
   return knex.schema.table(TABLE_NAME, function(table){
-    table.integer('sessionId').references('session.id').index();
+    table.integer('sessionId').references('sessions.id').index();
   });
 };
 
 exports.down = function(knex, Promise) {
   return knex.schema.table(TABLE_NAME, function(table){
-    table.integer('sessionId').references('session.id').index();
+    table.integer('sessionId').references('sessions.id').index();
   });
 };
