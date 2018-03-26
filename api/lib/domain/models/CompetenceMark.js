6b757ea29ec6da5a6175bbab160241f0d0116ab6
MelanieMEB <mboudard@octo.com>
2018-03-26 17:44:19 +0200

    Add a restriction on level/score

--- api/lib/domain/models/CompetenceMark.js
+++ api/lib/domain/models/CompetenceMark.js
@@ -3,8 +3,8 @@ const { ObjectValidationError } = require('../errors');
 
 const schemaValidateCompetenceMark = Joi.object().keys({
   id: Joi.number().optional(),
-  level: Joi.number().min(-1).max(5).required(),
-  score: Joi.number().min(0).max(64).required(),
+  level: Joi.number().integer().min(-1).max(5).required(),
+  score: Joi.number().integer().min(0).max(64).required(),
   area_code: Joi.required(),
   competence_code: Joi.required(),
   assessmentResultId:  Joi.number().optional()
