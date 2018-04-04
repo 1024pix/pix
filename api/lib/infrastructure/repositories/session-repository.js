fe88c26a6e65c168c2f33856cc35b376dd746c6f
Brandone Martins <brandone.martins@gmail.com>
2018-04-04 15:11:25 +0200

    implement session repository get

--- api/lib/infrastructure/repositories/session-repository.js
+++ api/lib/infrastructure/repositories/session-repository.js
@@ -1,9 +1,14 @@
+const _ = require('lodash');
+
 const BookshelfSession = require('../data/session');
 const Session = require('../../domain/models/Session');
+const { NotFoundError } = require('../../domain/errors');
 
 function _toDomain(bookshelfSession) {
-  if(bookshelfSession) {
+  if (bookshelfSession) {
     const sessionReturned = bookshelfSession.toJSON();
+    const certificationCourses = bookshelfSession.related('certificationCourses').toJSON();
+    sessionReturned.certificationCourses = certificationCourses;
     return new Session(sessionReturned);
   }
   return null;
@@ -11,6 +16,8 @@ function _toDomain(bookshelfSession) {
 
 module.exports = {
   save: (sessionToBeSaved) => {
+    sessionToBeSaved = _.omit(sessionToBeSaved, ['certificationCourses'])
+
     return new BookshelfSession(sessionToBeSaved)
       .save()
       .then(_toDomain);
@@ -28,5 +35,15 @@ module.exports = {
       .where({ accessCode })
       .fetch({})
       .then(_toDomain);
+  },
+
+  get(idSession) {
+    return BookshelfSession
+      .where({ id: idSession })
+      .fetch({ require: true, withRelated: ['certificationCourses'] })
+      .then(_toDomain)
+      .catch(() => {
+        return Promise.reject(new NotFoundError());
+      });
   }
 };
--- api/tests/integration/infrastructure/repositories/session-repository_test.js
+++ api/tests/integration/infrastructure/repositories/session-repository_test.js
@@ -2,6 +2,8 @@ const { expect, knex } = require('../../../test-helper');
 
 const Session = require('../../../../lib/domain/models/Session');
 const sessionRepository = require('../../../../lib/infrastructure/repositories/session-repository');
+const { NotFoundError } = require('../../../../lib/domain/errors');
+
 
 describe('Integration | Repository | Session', function() {
 
@@ -143,5 +145,89 @@ describe('Integration | Repository | Session', function() {
     });
   });
 
+  describe('#get', function() {
+
+    const certifications = [{
+      id: 1,
+      userId: 1,
+      sessionId: 1
+    }, {
+      id: 2,
+      userId: 2,
+      sessionId: 1
+    }, {
+      id: 3,
+      userId: 3,
+      sessionId: 2
+    }];
+
+    beforeEach(() => {
+      return knex('sessions').insert({
+        id: 1,
+        certificationCenter: 'Parc des Princes',
+        address: 'Boulbi',
+        room: 'Hall Pauleta',
+        examiner: 'Neymar Jr',
+        date: '23/02/2018',
+        time: '12:00',
+        description: 'La Champions League est pour nous cette fois',
+        accessCode: 'NJR10'
+      }).then(() => {
+        return knex('certification-courses').insert(certifications);
+      });
+    });
+
+    afterEach(() => {
+      return knex('sessions').delete()
+        .then(() => {
+          return knex('certification-courses').delete();
+        });
+    });
+
+    it('should return session informations in a session Object', function() {
+      // given
+
+      // when
+      const promise = sessionRepository.get(1);
+
+      // then
+      return promise.then((session) => {
+        expect(session).to.be.instanceOf(Session);
+        expect(session.id).to.be.equal(1);
+        expect(session.certificationCenter).to.be.equal('Parc des Princes');
+        expect(session.address).to.be.equal('Boulbi');
+        expect(session.room).to.be.equal('Hall Pauleta');
+        expect(session.examiner).to.be.equal('Neymar Jr');
+        expect(session.date).to.be.equal('23/02/2018');
+        expect(session.time).to.be.equal('12:00');
+        expect(session.description).to.be.equal('La Champions League est pour nous cette fois');
+        expect(session.accessCode).to.be.equal('NJR10');
+      });
+    });
+
+
+    it('should return associated certifications', function() {
+      // when
+      const promise = sessionRepository.get(1);
+
+      // then
+      return promise.then((session) => {
+        expect(session.certificationCourses).to.be.instanceOf(Array);
+        expect(session.certificationCourses.length).to.be.equal(2);
+        expect(session.certificationCourses[0].id).to.be.equal(1);
+        expect(session.certificationCourses[0].userId).to.be.equal(1);
+        expect(session.certificationCourses[1].id).to.be.equal(2);
+        expect(session.certificationCourses[1].userId).to.be.equal(2);
+      });
+    });
+
+    it('should return a Not found error when no session was found', function() {
+      // when
+      const promise = sessionRepository.get(2);
+
+      // then
+      return expect(promise).to.be.rejectedWith(NotFoundError);
+    });
+  });
 });
 
