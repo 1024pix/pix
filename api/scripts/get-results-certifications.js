9d519fe063a034467292dfc9c2ee43c6c829fb0a
Melanie B <31532858+MelanieMEB@users.noreply.github.com>
2018-04-10 17:15:37 +0200

    [#16] [FEATURE] Ajout de toutes les informations dans le CSV d'export (US-1181).
    * Add column in csv export + add save in csv file
    
    * Remove test of deleted function
    
    * Small corrections after review
    
    * Remove Column in name of column and change date in test
    
    * Add simple at false to authorized 404 answers and return an empty line when there is no data
    
    * Remove old test about transform

--- api/scripts/get-results-certifications.js
+++ api/scripts/get-results-certifications.js
@@ -1,23 +1,22 @@
 #! /usr/bin/env node
 
+const fileSystem = require('fs');
+
 const request = require('request-promise-native');
 const json2csv = require('json2csv');
 const moment = require('moment-timezone');
 
-// request.debug = true;
 const HEADERS = [
-  'Numero certification', 'Date de début', 'Date de fin', 'Note Pix',
+  'Numero certification', 'Numero de session', 'Date de début', 'Date de fin',
+  'Status de la session', 'Note Pix',
+  'Prénom', 'Nom', 'Date de naissance', 'Lieu de naissance',
+  'Commentaire pour le candidat', 'Commentaire pour l\'organisation', 'Commentaire du jury',
   '1.1', '1.2', '1.3',
   '2.1', '2.2', '2.3', '2.4',
   '3.1', '3.2', '3.3', '3.4',
   '4.1', '4.2', '4.3',
-  '5.1', '5.2',
+  '5.1', '5.2'
 ];
-
-function parseArgs(argv) {
-  const [_a, _b, _c, ...args] = argv;
-  return args;
-}
 
 function buildRequestObject(baseUrl, authToken, certificationId) {
   return {
@@ -27,15 +26,8 @@ function buildRequestObject(baseUrl, authToken, certificationId) {
     baseUrl: baseUrl,
     url: `/api/admin/certifications/${certificationId}`,
     json: true,
-    transform: (body) => {
-      body.data.attributes.certificationId = certificationId;
-      return body;
-    }
+    simple: false
   };
-}
-
-function makeRequest(config) {
-  return request(config);
 }
 
 function findCompetence(profile, competenceName) {
@@ -44,31 +36,70 @@ function findCompetence(profile, competenceName) {
 }
 
 function toCSVRow(rowJSON) {
+  if(!rowJSON.data) {
+    return {};
+  }
   const certificationData = rowJSON.data.attributes;
   const res = {};
-  const [idColumn, dateStartColumn, dateEndColumn, noteColumn, ...competencesColumns] = HEADERS;
-  res[idColumn] = certificationData.certificationId;
-  res[dateStartColumn] = moment.utc(certificationData['created-at']).tz('Europe/Paris').format('DD/MM/YYYY HH:mm:ss');
+
+  const [id,
+    sessionNumber,
+    dateStart,
+    dateEnd,
+    status,
+    note,
+    firstname,
+    lastname,
+    birthdate,
+    birthplace,
+    commentCandidate,
+    commentOrganization,
+    commentJury,
+    ...competencess] = HEADERS;
+
+  res[id] = certificationData['certification-id'];
+  res[sessionNumber] = certificationData['session-id'];
+
+  res[dateStart] = moment.utc(certificationData['created-at']).tz('Europe/Paris').format('DD/MM/YYYY HH:mm:ss');
   if (certificationData['completed-at']) {
-    res[dateEndColumn] = moment(certificationData['completed-at']).tz('Europe/Paris').format('DD/MM/YYYY HH:mm:ss');
+    res[dateEnd] = moment(certificationData['completed-at']).tz('Europe/Paris').format('DD/MM/YYYY HH:mm:ss');
   } else {
-    res[dateEndColumn] = '';
+    res[dateEnd] = '';
   }
 
-  res[noteColumn] = certificationData['pix-score'];
-  competencesColumns.forEach(column => {
+  res[status] = certificationData['status'];
+  res[note] = certificationData['pix-score'];
+  res[firstname] = certificationData['first-name'];
+  res[lastname] = certificationData['last-name'];
+  res[birthdate] = certificationData['birthdate'];
+  res[birthplace] = certificationData['birthplace'];
+  res[commentCandidate] = certificationData['comment-for-candidate'];
+  res[commentOrganization] = certificationData['comment-for-organization'];
+  res[commentJury] = certificationData['comment-for-jury'];
+
+  competencess.forEach(column => {
     res[column] = findCompetence(certificationData['competences-with-mark'], column);
   });
+
   return res;
+}
+
+function saveInFile(csv) {
+  const filepath = moment().format('DD-MM-YYYY_HH-mm') + '.csv';
+  const csvData = '\uFEFF' + csv;
+  fileSystem.writeFile(filepath, csvData, (err) => {
+    if (err) throw err;
+    console.log('Les données de certifications sont dans le fichier :' + filepath);
+  });
 }
 
 function main() {
   const baseUrl = process.argv[2];
   const authToken = process.argv[3];
-  const ids = parseArgs(process.argv.slice(4));
+  const ids = process.argv.slice(4);
   const requests = Promise.all(
     ids.map(id => buildRequestObject(baseUrl, authToken, id))
-      .map(requestObject => makeRequest(requestObject))
+      .map(requestObject => request(requestObject))
   );
 
   return requests.then(certificationResults => certificationResults.map(toCSVRow))
@@ -78,6 +109,7 @@ function main() {
       del: ';',
     }))
     .then(csv => {
+      saveInFile(csv);
       console.log(`\n\n${csv}\n\n`);
       return csv;
     });
@@ -88,7 +120,6 @@ if (process.env.NODE_ENV !== 'test') {
 }
 
 module.exports = {
-  parseArgs,
   buildRequestObject,
   toCSVRow,
   findCompetence
