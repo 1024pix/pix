a997bc5d0002ced72e6f91a1c207681ceca5a30a
Antoine Boileau <apem.boileau@gmail.com>
2018-04-11 16:41:47 +0200

    Add possibility of hint presence in tutorial-panel component

--- live/tests/integration/components/tutorial-panel-test.js
+++ live/tests/integration/components/tutorial-panel-test.js
@@ -1,3 +1,4 @@
+import EmberObject from '@ember/object';
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
@@ -8,31 +9,53 @@ describe('Integration | Component | tutorial panel', function() {
     integration: true
   });
 
-  describe('component rendering', function() {
+  it('should not render default message nor hint when answer is correct', function() {
+    // given
+    this.set('hint', null);
+    this.set('resultItemStatus', 'ok');
 
-    it('should render component', function() {
-      // when
-      this.render(hbs`{{tutorial-panel}}`);
+    // when
+    this.render(hbs`{{tutorial-panel hint=hint resultItemStatus=resultItemStatus}}`);
 
-      // then
-      expect(this.$()).to.have.length(1);
-    });
+    // then
+    expect(this.$('.tutorial-panel')).to.have.lengthOf(1);
+    expect(this.$('.tutorial-panel__hint-container')).to.have.lengthOf(0);
+    expect(this.$('.tutorial-panel__default-message-container')).to.have.lengthOf(0);
+  });
 
-    [
-      { itemClassName: '.tutorial-panel' },
-      { itemClassName: '.tutorial-panel__box-container' },
-      { itemClassName: '.tutorial-panel__box-title' },
-      { itemClassName: '.tutorial-panel__box-picto-container' },
-      { itemClassName: '.tutorial-panel__box-picto' },
-      { itemClassName: '.tutorial-panel__separator' }
-    ].forEach(({ itemClassName }) => {
-      it(`should render a div with class ${itemClassName}`, function() {
-        // when
-        this.render(hbs`{{tutorial-panel}}`);
+  it('should render the hint when answer is not correct and hint is present', function() {
+    // given
+    this.set('hint', 'Ceçi est une astuce.');
+    this.set('resultItemStatus', 'ko');
 
-        // then
-        expect(this.$(itemClassName)).to.have.lengthOf(1);
-      });
-    });
+    // when
+    this.render(hbs`{{tutorial-panel hint=hint resultItemStatus=resultItemStatus}}`);
+
+    // then
+    expect(this.$('.tutorial-panel')).to.have.lengthOf(1);
+    expect(this.$('.tutorial-panel__hint-container')).to.have.lengthOf(1);
+    expect(this.$('.tutorial-panel__hint-title')).to.have.lengthOf(1);
+    expect(this.$('.tutorial-panel__hint-picto-container')).to.have.lengthOf(1);
+    expect(this.$('.tutorial-panel__hint-picto')).to.have.lengthOf(1);
+    expect(this.$('.tutorial-panel__hint-content')).to.have.lengthOf(1);
+
+    const $contentElement = this.$('.tutorial-panel__hint-content');
+    expect($contentElement.text().trim()).to.equal('Ceçi est une astuce.');
+  });
+
+  it('should render the default message when answer is not correct and hint is not defined', function() {
+    // given
+    this.set('hint', null);
+    this.set('resultItemStatus', 'ko');
+
+    // when
+    this.render(hbs`{{tutorial-panel hint=hint resultItemStatus=resultItemStatus}}`);
+
+    // then
+    expect(this.$('.tutorial-panel')).to.have.lengthOf(1);
+    expect(this.$('.tutorial-panel__default-message-container')).to.have.lengthOf(1);
+    expect(this.$('.tutorial-panel__default-message-title')).to.have.lengthOf(1);
+    expect(this.$('.tutorial-panel__default-message-picto-container')).to.have.lengthOf(1);
+    expect(this.$('.tutorial-panel__default-message-picto')).to.have.lengthOf(1);
   });
 });
