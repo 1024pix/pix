f60d21fd16e69e668ea147c14fadf0986e526f80
Jérémy Buget <jbuget@gmail.com>
2018-04-13 09:53:49 +0200

    Reset organisation form fields when creations was successfull

--- app/components/organization-form.js
+++ app/components/organization-form.js
@@ -1,36 +1,22 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 const organizationTypes = [
-  {
-    label: 'Organisation professionnelle',
-    value: 'PRO'
-  }, {
-    label: 'Établissement scolaire',
-    value: 'SCO'
-
-  }, {
-    label: 'Établissement supérieur',
-    value: 'SUP'
-  }
+  { value: 'PRO', label: 'Organisation professionnelle' },
+  { value: 'SCO', label: 'Établissement scolaire' },
+  { value: 'SUP', label: 'Établissement supérieur' },
 ];
 
 export default Component.extend({
 
+  notifications: service('notification-messages'),
+
   organizationTypes,
-  selectedOrganizationType: null,
 
   init() {
     this._super(...arguments);
-    this.organization = {
-      name: null,
-      type: null
-    };
-    this.contact = {
-      firstName: null,
-      lastName: null,
-      email: null,
-      password: null,
-    };
+    this.organization = this._newEmptyOrganization();
+    this.contact = this._newEmptyOrganization();
   },
 
   actions: {
@@ -38,12 +24,34 @@ export default Component.extend({
       this.set('organization.type', this.get('selectedOrganizationType.value'));
       const organization = this.get('organization');
       const contact = this.get('contact');
-      return this.get('onSubmitOrganization')(organization, contact);
+      return this.get('onSubmitOrganization')(organization, contact)
+        .then(() => {
+          this._resetFields();
+          this.get('notifications').success('L’organisation a été créée avec succès.');
+        })
+        .catch(() => {
+          this.get('notifications').error('Une erreur est survenue.')
+        });
     },
 
     selectOrganizationType(organizationType) {
       this.set('selectedOrganizationType', organizationType);
       this.set('organization.type', organizationType.value);
     }
+  },
+
+  _newEmptyOrganization() {
+    return { name: null, type: null };
+  },
+
+  _newEmptyContact() {
+    return { firstName: null, lastName: null, email: null, password: null };
+  },
+
+  _resetFields() {
+    this.set('organization', this._newEmptyOrganization());
+    this.set('contact', this._newEmptyContact());
+    this.set('selectedOrganizationType', null);
   }
+
 });
