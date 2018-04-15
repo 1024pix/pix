19ca684344d72d925016a2be9c50f6a4a02cbb82
Jérémy Buget <jbuget@gmail.com>
2018-04-16 00:29:13 +0200

    Redesign application

--- app/components/menu-bar.js
+++ app/components/menu-bar.js
@@ -4,8 +4,6 @@ import { inject as service } from '@ember/service';
 export default Component.extend({
   session: service(),
 
-  classNames: 'menu-bar',
-
   actions: {
     logout() {
       return this.get('session').invalidate();
--- tests/integration/components/organization-form-test.js
+++ tests/integration/components/organization-form-test.js
@@ -1,4 +1,4 @@
-import { module, test } from 'qunit';
+import { module, test, only } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
@@ -12,5 +12,17 @@ module('Integration | Component | organization-form', function(hooks) {
 
     // then
     assert.dom('.organization-form').exists();
+  });
+
+  only('should display error messages panel when one or multiple errors occurred', async function(assert) {
+    // given
+    const errorMessages = ['Erreur A', 'Erreur B', 'Erreur C'];
+    this.set('errorMessages', errorMessages);
+
+    // when
+    await render(hbs`{{organization-form errorMessages=errorMessages}}`);
+
+    // then
+    assert.dom('.error-messages-panel').exists();
   });
 });
