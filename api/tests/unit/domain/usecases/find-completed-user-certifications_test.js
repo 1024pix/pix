316acf775bc49fa55f8b797ba87bf74ea2515ff3
Brandone Martins <brandone.martins@gmail.com>
2018-04-19 18:25:49 +0200

    directory usecases

---
+++ api/tests/unit/domain/usecases/find-completed-user-certifications_test.js
@@ -0,0 +1,35 @@
+const { expect, sinon } = require('../../../test-helper');
+const Certification = require('../../../../lib/domain/models/Certification');
+const usecases = require('../../../../lib/domain/usecases');
+
+describe('Unit | UseCase | find-completed-user-certifications', () => {
+
+  let sandbox;
+  let certificationRepository = {};
+
+  beforeEach(() => {
+    certificationRepository.findByUserId = sinon.stub();
+  });
+
+  afterEach(() => {
+
+  });
+
+  it('should return all the needed informations about certifications', function() {
+    // given
+    const userId = 1;
+    const certification = new Certification({ date: '14/08/2018', certificationCenter: 'Université du Pix'});
+    certificationRepository.findByUserId.resolves([certification]);
+
+    // when
+    const promise = usecases.findCompletedUserCertifications({ userId, certificationRepository });
+
+    // then
+    return promise.then((certifications) => {
+      expect(certificationRepository.findByUserId).to.have.been.calledWith(userId);
+      expect(certifications[0].date).to.equal('14/08/2018');
+      expect(certifications[0].certificationCenter).to.equal('Université du Pix');
+    });
+  });
+
+});
