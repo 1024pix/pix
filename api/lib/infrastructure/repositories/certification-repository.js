111a7083450cc91d408aa00c367c832fa572e37b
Brandone Martins <brandone.martins@gmail.com>
2018-04-19 17:19:32 +0200

    Creating certifications repository

---
+++ api/lib/infrastructure/repositories/certification-repository.js
@@ -0,0 +1,17 @@
+const Certification = require('../../../lib/domain/models/Certification');
+const CertificationCourseBookshelf = require('../../../lib/infrastructure/data/certification-course');
+
+module.exports = {
+
+  findByUserId(userId) {
+    return CertificationCourseBookshelf
+      .where({ userId })
+      .fetch({ withRelated: ['session'] })
+      .then((certificationCourseBookshelf) => {
+        return Promise.resolve([new Certification({
+          date : certificationCourseBookshelf.get('completedAt'),
+          certificationCenter : certificationCourseBookshelf.related('session').toJSON().certificationCenter
+        })]);
+      });
+  }
+};
