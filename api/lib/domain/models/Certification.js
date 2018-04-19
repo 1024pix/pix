0f129451adde9efd052ea090d7261200b44adde7
Brandone Martins <brandone.martins@gmail.com>
2018-04-19 18:16:24 +0200

    Define Certification model

---
+++ api/lib/domain/models/Certification.js
@@ -0,0 +1,9 @@
+class Certification {
+
+  constructor({ date, certificationCenter} = {}) {
+    this.date = date;
+    this.certificationCenter = certificationCenter;
+  }
+}
+
+module.exports = Certification;
