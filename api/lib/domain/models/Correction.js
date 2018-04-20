e42cf4aabe8382c4656fb58e7c0578367fd86888
Antoine Boileau <apem.boileau@gmail.com>
2018-04-20 15:43:45 +0200

    add the selection of the relevant hint to display in the frontend

--- api/lib/domain/models/Correction.js
+++ api/lib/domain/models/Correction.js
@@ -1,9 +1,18 @@
+function _byLowestSkillName(hintA, hintB) {
+  return hintA.skillName > hintB.skillName;
+}
+
 class Correction {
 
   constructor({ id, solution, hints = [] } = {}) {
     this.id = id;
     this.solution = solution;
     this.hints = hints;
+  }
+
+  get relevantHint() {
+    const hintsToSort = this.hints.slice();
+    return hintsToSort.sort(_byLowestSkillName)[0];
   }
 }
 
