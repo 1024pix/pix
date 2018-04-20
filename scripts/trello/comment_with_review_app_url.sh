310cdffc926f2fb8721c25d9e3e7bb30c863315f
Quentin Cattez <qcattez@octo.com>
2018-04-20 17:28:00 +0200

    fixing comment on inexisting trello cards

--- scripts/trello/comment_with_review_app_url.sh
+++ scripts/trello/comment_with_review_app_url.sh
@@ -14,8 +14,15 @@ API_URL="https://api.trello.com/$API_VERSION"
 BOARD_ID="577a53d10c4ba2372878ed14"
 CARD_SHORT_ID=$(extract_card_short_id_from_branch_name $CIRCLE_BRANCH)
 CREDENTIALS="key=$TRELLO_API_KEY&token=$TRELLO_TOKEN"
-CARD_ID=$(curl "$API_URL/boards/$BOARD_ID/cards/$CARD_SHORT_ID?$CREDENTIALS" | jq .id | tr -d '"')
 
+RESPONSE=$(curl "$API_URL/boards/$BOARD_ID/cards/$CARD_SHORT_ID?$CREDENTIALS")
+if [[ $RESPONSE == "invalid id" ]]
+then
+    echo "The id of the Trello card could not be found. Stopping here."
+    exit 0
+fi
+
+CARD_ID=$(echo $RESPONSE | jq .id | tr -d '"')
 CARD_COMMENTS=$(curl "$API_URL/cards/$CARD_ID/actions?$CREDENTIALS" | jq '.[].data.text')
 REVIEW_APP_URL="http://$CIRCLE_BRANCH.pix-dev.ovh"
 if [[ $CARD_COMMENTS =~ .*$REVIEW_APP_URL.* ]]
