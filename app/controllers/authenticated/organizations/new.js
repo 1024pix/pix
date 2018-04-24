4b1d4504df6398365bebb2019bc0d571988de25c
Jérémy Buget <jbuget@gmail.com>
2018-04-25 01:38:20 +0200

    Fix: when an error occurs during organization creation, it does not add the model entity into the Ember Data store anymore

--- app/controllers/authenticated/organizations/new.js
+++ app/controllers/authenticated/organizations/new.js
@@ -4,14 +4,21 @@ export default Controller.extend({
 
   actions: {
     addOrganization(organization, contact) {
-      return this.get('store').createRecord('organization', {
+      const store = this.get('store');
+
+      const model = store.createRecord('organization', {
         name: organization.name,
         type: organization.type,
         firstName: contact.firstName,
         lastName: contact.lastName,
         email: contact.email,
         password: contact.password,
-      }).save();
+      });
+
+      return model.save().catch(error => {
+        model.unloadRecord();
+        throw error;
+      });
     }
   }
 
