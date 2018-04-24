36374d6d85071639a2387f4a83b1376431fb3ba0
Jérémy Buget <jbuget@gmail.com>
2018-04-25 01:29:24 +0200

    Modify route GET /organizations in order to allow Pix Masters to fetch all organizations (for Pix Admin)

--- api/tests/unit/infrastructure/repositories/user-repository_test.js
+++ api/tests/unit/infrastructure/repositories/user-repository_test.js
@@ -3,19 +3,19 @@ const { expect, sinon } = require('../../../test-helper');
 const BookshelfUser = require('../../../../lib/infrastructure/data/user');
 const userRepository = require('../../../../lib/infrastructure/repositories/user-repository');
 const DomainUser = require('../../../../lib/domain/models/User');
-const { NotFoundError } = require('../../../../lib/domain/errors');
+const { NotFoundError, UserNotFoundError } = require('../../../../lib/domain/errors');
 
 describe('Unit | Infrastructure | Repository | UserRepository', () => {
 
+  beforeEach(() => {
+    sinon.stub(BookshelfUser.prototype, 'where');
+  });
+
+  afterEach(() => {
+    BookshelfUser.prototype.where.restore();
+  });
+
   describe('#get', () => {
-
-    beforeEach(() => {
-      sinon.stub(BookshelfUser.prototype, 'where');
-    });
-
-    afterEach(() => {
-      BookshelfUser.prototype.where.restore();
-    });
 
     it('should resolve a domain User matching its ID', () => {
       // given
@@ -51,6 +51,55 @@ describe('Unit | Infrastructure | Repository | UserRepository', () => {
         .catch(err => expect(err).to.be.an.instanceof(NotFoundError));
     });
 
+  });
+
+  describe('#hasRolePixMaster', () => {
+
+    it('should resolve true when user has role PIX_MASTER', () => {
+      // given
+      const userId = 1234;
+      const domainUser = new DomainUser({
+        id: userId,
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'jdoe@example.net',
+        pixRoles: [{ name: 'PIX_MASTER' }],
+      });
+      const bookshelfUser = { toDomainEntity: () => domainUser };
+      const fetchStub = sinon.stub().resolves(bookshelfUser);
+      BookshelfUser.prototype.where.returns({ fetch: fetchStub });
+
+      // when
+      const promise = userRepository.hasRolePixMaster(userId);
+
+      // then
+      return promise.then(result => {
+        expect(result).to.be.true;
+      });
+    });
+
+    it('should resolve false when user does not have role PIX_MASTER', () => {
+      // given
+      const userId = 1234;
+      const domainUser = new DomainUser({
+        id: userId,
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'jdoe@example.net',
+        pixRoles: [],
+      });
+      const bookshelfUser = { toDomainEntity: () => domainUser };
+      const fetchStub = sinon.stub().resolves(bookshelfUser);
+      BookshelfUser.prototype.where.returns({ fetch: fetchStub });
+
+      // when
+      const promise = userRepository.hasRolePixMaster(userId);
+
+      // then
+      return promise.then(result => {
+        expect(result).to.be.false;
+      });
+    });
   });
 
 });
