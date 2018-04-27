db8b21408bff90653bf1845db0ca64838a0c3e2f
Mathieu Laurent <mlaurent@octo.com>
2018-04-27 15:07:39 +0200

    Create Organization validator

---
+++ api/lib/domain/validators/organization-validator.js
@@ -0,0 +1,63 @@
+const _ = require('lodash');
+const Joi = require('joi');
+const { OrganizationValidationErrors } = require('../../domain/errors');
+
+const JOI_VALIDATION_ERROR = 'ValidationError';
+const validationConfiguration = { abortEarly: false, allowUnknown: true };
+
+const organizationValidationJoiSchema = Joi.object().keys({
+
+  code: Joi.string().regex(/[A-Z]{4}\d{2}/).required().error((errors) => {
+    const error = errors[0];
+    if (error.type === 'any.empty') {
+      return { message: 'Le code n’est pas renseigné.' };
+    }
+    return { message: 'Le code doit respecter le format AAAA99.' };
+  }),
+
+  name: Joi.string().required().error(() => {
+    return { message: 'Le nom n’est pas renseigné.' };
+  }),
+
+  type: Joi.string().required().valid('SCO', 'SUP', 'PRO').error((errors) => {
+    const error = errors[0];
+    if (error.type === 'any.empty') {
+      return { message: 'Le type n’est pas renseigné.' };
+    }
+    return { message: 'Le type doit avoir l’une des valeurs suivantes: SCO, SUP, PRO.' };
+  }),
+
+  email: Joi.string().email().required().error((errors) => {
+    const error = errors[0];
+    if (error.type === 'any.empty') {
+      return { message: 'L’adresse électronique n’est pas renseignée.' };
+    }
+    return { message: 'L’adresse électronique n’est pas correcte.' };
+  })
+});
+
+function _formatJoiValidationError(joiError) {
+  return {
+    source: {
+      pointer: `/data/attributes/${_.kebabCase(joiError.context.key)}`
+    },
+    title: `Invalid organization data attribute "${joiError.context.key}"`,
+    detail: joiError.message,
+    meta: {
+      field: joiError.context.key
+    }
+  };
+}
+
+module.exports = {
+
+  validate(organizationData) {
+    return Joi.validate(organizationData, organizationValidationJoiSchema, validationConfiguration).catch(error => {
+      if (error.name === JOI_VALIDATION_ERROR) {
+        return Promise.reject(new OrganizationValidationErrors(error.details.map(_formatJoiValidationError)));
+      }
+      throw error;
+    });
+  }
+
+};
