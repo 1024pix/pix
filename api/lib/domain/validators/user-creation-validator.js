f2681f92d26ac3e0f8d4c0b1b395703aa8c167a7
Mathieu Laurent <mlaurent@octo.com>
2018-04-27 12:20:12 +0200

    Use of UserValidator in UserCreationValidator

--- api/lib/domain/validators/user-creation-validator.js
+++ api/lib/domain/validators/user-creation-validator.js
@@ -1,59 +1,7 @@
-const _ = require('lodash');
-const Joi = require('joi');
 const googleReCaptcha = require('../../infrastructure/validators/grecaptcha-validator');
+const userValidator = require('./user-validator');
 const { UserValidationErrors } = require('../../domain/errors');
 const { InvalidRecaptchaTokenError } = require('../../infrastructure/validators/errors');
-const validatePassword = require('../../infrastructure/validators/password-validator');
-
-const JOI_VALIDATION_ERROR = 'ValidationError';
-
-const customJoi = Joi.extend((joi) => ({
-  name: 'string',
-  base: joi.string(),
-  rules: [
-    {
-      name: 'password',
-      validate(params, value, state, options) {
-        const isValid = validatePassword(value);
-        if (!isValid) {
-          return this.createError('string.password', { v: value }, state, options);
-        }
-        return value;
-      }
-    },
-  ]
-}));
-
-const schemaValidateUser = customJoi.object().keys({
-
-  firstName: Joi.string().required().error(() => {
-    return { message: 'Votre prénom n’est pas renseigné.' };
-  }),
-
-  lastName: Joi.string().required().error(() => {
-    return { message: 'Votre nom n’est pas renseigné.' };
-  }),
-
-  email: Joi.string().email().required().error((errors) => {
-    const error = errors[0];
-    if (error.type === 'any.empty') {
-      return { message: 'Votre adresse électronique n’est pas renseignée.' };
-    }
-    return { message: 'Votre adresse électronique n’est pas correcte.' };
-  }),
-
-  password: customJoi.string().password().required().min(8).error((errors) => {
-    const error = errors[0];
-    if (error.type === 'any.empty') {
-      return { message: 'Votre mot de passe n’est pas renseigné.' };
-    }
-    return { message: 'Votre mot de passe doit comporter au moins une lettre, un chiffre et 8 caractères.' };
-  }),
-
-  cgu: Joi.boolean().required().valid(true).error(() => {
-    return { message: 'Vous devez accepter les conditions d’utilisation de Pix pour créer un compte.' };
-  }),
-});
 
 function _verifyReCaptcha(reCaptchaToken) {
   return googleReCaptcha.verify(reCaptchaToken).catch(error => {
@@ -74,28 +22,6 @@ function _verifyReCaptcha(reCaptchaToken) {
   });
 }
 
-function _validateUserData(userData) {
-  const validationConfiguration = { abortEarly: false, allowUnknown: true };
-  return Joi.validate(userData, schemaValidateUser, validationConfiguration).catch(error => {
-    if (error.name === JOI_VALIDATION_ERROR) {
-      const userDataErrors = error.details.map(joiError => {
-        return {
-          source: {
-            pointer: `/data/attributes/${_.kebabCase(joiError.context.key)}`
-          },
-          title: `Invalid user data attribute "${joiError.context.key}"`,
-          detail: joiError.message,
-          meta: {
-            field: joiError.context.key
-          }
-        };
-      });
-      return userDataErrors;
-    }
-    throw error;
-  });
-}
-
 function _concatErrors(recaptchaError, userDataErrors) {
   const validationErrors = [];
   if (recaptchaError) {
@@ -110,16 +36,16 @@ function _concatErrors(recaptchaError, userDataErrors) {
 // FIXME move it in the "future" Use Case that creates a User
 module.exports = {
 
-  validate(userData, recaptchaToken, recaptchaTokenRequired) {
+  validate(userData, recaptchaToken) {
     return Promise.all([
       _verifyReCaptcha(recaptchaToken),
-      _validateUserData(userData),
+      userValidator.validate(userData).catch((error) => error),
     ])
       .then(values => {
         const recaptchaError = values[0];
-        const userDataErrors = values[1];
+        const userDataErrors = values[1] || {};
 
-        const validationErrors = _concatErrors(recaptchaError, userDataErrors);
+        const validationErrors = _concatErrors(recaptchaError, userDataErrors.errors);
 
         if (validationErrors.length > 0) {
           return Promise.reject(new UserValidationErrors(validationErrors));
