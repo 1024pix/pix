87e87f1b5440bcf4cc7dcbf0f9a5112a5b7084f3
Mathieu Laurent <mlaurent@octo.com>
2018-04-27 11:20:08 +0200

    Create a User Validator

---
+++ api/lib/domain/validators/user-validator.js
@@ -0,0 +1,81 @@
+const _ = require('lodash');
+const Joi = require('joi');
+const validatePassword = require('../../infrastructure/validators/password-validator');
+const { UserValidationErrors } = require('../../domain/errors');
+
+const JOI_VALIDATION_ERROR = 'ValidationError';
+const validationConfiguration = { abortEarly: false, allowUnknown: true };
+
+const joiWithPasswordValidation = Joi.extend((joi) => ({
+  name: 'string',
+  base: joi.string(),
+  rules: [
+    {
+      name: 'password',
+      validate(params, value, state, options) {
+        const isValid = validatePassword(value);
+        if (!isValid) {
+          return this.createError('string.password', { v: value }, state, options);
+        }
+        return value;
+      }
+    },
+  ]
+}));
+
+const userValidationJoiSchema = Joi.object().keys({
+
+  firstName: Joi.string().required().error(() => {
+    return { message: 'Votre prénom n’est pas renseigné.' };
+  }),
+
+  lastName: Joi.string().required().error(() => {
+    return { message: 'Votre nom n’est pas renseigné.' };
+  }),
+
+  email: Joi.string().email().required().error((errors) => {
+    const error = errors[0];
+    if (error.type === 'any.empty') {
+      return { message: 'Votre adresse électronique n’est pas renseignée.' };
+    }
+    return { message: 'Votre adresse électronique n’est pas correcte.' };
+  }),
+
+  password: joiWithPasswordValidation.string().password().required().min(8).error((errors) => {
+    const error = errors[0];
+    if (error.type === 'any.empty') {
+      return { message: 'Votre mot de passe n’est pas renseigné.' };
+    }
+    return { message: 'Votre mot de passe doit comporter au moins une lettre, un chiffre et 8 caractères.' };
+  }),
+
+  cgu: Joi.boolean().required().valid(true).error(() => {
+    return { message: 'Vous devez accepter les conditions d’utilisation de Pix pour créer un compte.' };
+  }),
+});
+
+function _formatJoiValidationError(joiError) {
+  return {
+    source: {
+      pointer: `/data/attributes/${_.kebabCase(joiError.context.key)}`
+    },
+    title: `Invalid user data attribute "${joiError.context.key}"`,
+    detail: joiError.message,
+    meta: {
+      field: joiError.context.key
+    }
+  };
+}
+
+module.exports = {
+
+  validate(userData) {
+    return Joi.validate(userData, userValidationJoiSchema, validationConfiguration).catch(error => {
+      if (error.name === JOI_VALIDATION_ERROR) {
+        return Promise.reject(new UserValidationErrors(error.details.map(_formatJoiValidationError)));
+      }
+      throw error;
+    });
+  }
+
+};
---
+++ api/tests/unit/domain/validations/user-validator_test.js
@@ -0,0 +1,211 @@
+const { expect } = require('../../../test-helper');
+const userValidator = require('../../../../lib/domain/validators/user-validator');
+const { UserValidationErrors } = require('../../../../lib/domain/errors');
+
+const MISSING_VALUE = '';
+
+function _assertErrorMatchesWithExpectedOne(err, expectedError) {
+  expect(err).to.be.an.instanceof(UserValidationErrors);
+  expect(err.errors).to.have.lengthOf(1);
+  expect(err.errors[0]).to.deep.equal(expectedError);
+}
+
+describe('Unit | Domain | Validators | user-validator', function() {
+
+  let userData;
+
+  beforeEach(() => {
+    userData = {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john.doe@example.net',
+      password: 'password1234',
+      cgu: true,
+    };
+  });
+
+  describe('#validate', () => {
+
+    context('when validation is successful', () => {
+
+      it('should resolve (with no value) when validation is successful', () => {
+        // when
+        const promise = userValidator.validate(userData);
+
+        // then
+        return expect(promise).to.be.fulfilled;
+      });
+    });
+
+    context('when user data validation fails', () => {
+
+      it('should reject with error on field "first name" when first name is missing', () => {
+        // given
+        const expectedError = {
+          source: { pointer: '/data/attributes/first-name' },
+          title: 'Invalid user data attribute "firstName"',
+          detail: 'Votre prénom n’est pas renseigné.',
+          meta: {
+            field: 'firstName'
+          }
+        };
+        userData.firstName = MISSING_VALUE;
+
+        // when
+        const promise = userValidator.validate(userData);
+
+        // then
+        return promise
+          .then(() => expect.fail('Expected rejection with UserValidationErrors'))
+          .catch((err) => _assertErrorMatchesWithExpectedOne(err, expectedError));
+      });
+
+      it('should reject with error on field "last name" when last name is missing', () => {
+        // given
+        const expectedError = {
+          source: { pointer: '/data/attributes/last-name' },
+          title: 'Invalid user data attribute "lastName"',
+          detail: 'Votre nom n’est pas renseigné.',
+          meta: {
+            field: 'lastName'
+          }
+        };
+        userData.lastName = MISSING_VALUE;
+
+        // when
+        const promise = userValidator.validate(userData);
+
+        // then
+        return promise
+          .then(() => expect.fail('Expected rejection with UserValidationErrors'))
+          .catch((err) => _assertErrorMatchesWithExpectedOne(err, expectedError));
+      });
+
+      it('should reject with error on field "email" when email is missing', () => {
+        // given
+        const expectedError = {
+          source: { pointer: '/data/attributes/email' },
+          title: 'Invalid user data attribute "email"',
+          detail: 'Votre adresse électronique n’est pas renseignée.',
+          meta: {
+            field: 'email'
+          }
+        };
+        userData.email = MISSING_VALUE;
+
+        // when
+        const promise = userValidator.validate(userData);
+
+        // then
+        return promise
+          .then(() => expect.fail('Expected rejection with UserValidationErrors'))
+          .catch((err) => _assertErrorMatchesWithExpectedOne(err, expectedError));
+      });
+
+      it('should reject with error on field "email" when email is invalid', () => {
+        // given
+        const expectedError = {
+          source: { pointer: '/data/attributes/email' },
+          title: 'Invalid user data attribute "email"',
+          detail: 'Votre adresse électronique n’est pas correcte.',
+          meta: {
+            field: 'email'
+          }
+        };
+        userData.email = 'invalid_email';
+
+        // when
+        const promise = userValidator.validate(userData);
+
+        // then
+        return promise
+          .then(() => expect.fail('Expected rejection with UserValidationErrors'))
+          .catch((err) => _assertErrorMatchesWithExpectedOne(err, expectedError));
+      });
+
+      it('should reject with error on field "password" when password is missing', () => {
+        // given
+        const expectedError = {
+          source: { pointer: '/data/attributes/password' },
+          title: 'Invalid user data attribute "password"',
+          detail: 'Votre mot de passe n’est pas renseigné.',
+          meta: {
+            field: 'password'
+          }
+        };
+        userData.password = MISSING_VALUE;
+
+        // when
+        const promise = userValidator.validate(userData);
+
+        // then
+        return promise
+          .then(() => expect.fail('Expected rejection with UserValidationErrors'))
+          .catch((err) => _assertErrorMatchesWithExpectedOne(err, expectedError));
+      });
+
+      it('should reject with error on field "password" when password is invalid', () => {
+        // given
+        const expectedError = {
+          source: { pointer: '/data/attributes/password' },
+          title: 'Invalid user data attribute "password"',
+          detail: 'Votre mot de passe doit comporter au moins une lettre, un chiffre et 8 caractères.',
+          meta: {
+            field: 'password'
+          }
+        };
+        userData.password = 'invalid';
+
+        // when
+        const promise = userValidator.validate(userData);
+
+        // then
+        return promise
+          .then(() => expect.fail('Expected rejection with UserValidationErrors'))
+          .catch((err) => _assertErrorMatchesWithExpectedOne(err, expectedError));
+      });
+
+      it('should reject with error on field "cgu" when cgu is false', () => {
+        // given
+        const expectedError = {
+          source: { pointer: '/data/attributes/cgu' },
+          title: 'Invalid user data attribute "cgu"',
+          detail: 'Vous devez accepter les conditions d’utilisation de Pix pour créer un compte.',
+          meta: {
+            field: 'cgu'
+          }
+        };
+        userData.cgu = 'false';
+
+        // when
+        const promise = userValidator.validate(userData);
+
+        // then
+        return promise
+          .then(() => expect.fail('Expected rejection with UserValidationErrors'))
+          .catch((err) => _assertErrorMatchesWithExpectedOne(err, expectedError));
+      });
+
+      it('should reject with errors on all fields (but only once by field) when all fields are missing', () => {
+        // given
+        userData = {
+          firstName: '',
+          lastName: '',
+          email: '',
+          password: '',
+        };
+
+        // when
+        const promise = userValidator.validate(userData);
+
+        // then
+        return promise
+          .then(() => expect.fail('Expected rejection with UserValidationErrors'))
+          .catch((err) => {
+            expect(err).to.be.an.instanceof(UserValidationErrors);
+            expect(err.errors).to.have.lengthOf(5);
+          });
+      });
+    });
+  });
+});
