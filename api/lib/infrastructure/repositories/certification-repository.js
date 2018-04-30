4ab4f48f6d43b3e1459cc86d202369bdbee41b07
Brandone MARTINS <5923431+MBrandone@users.noreply.github.com>
2018-04-30 17:02:45 +0200

    [#53] [HOTFIX] Resolution du bug sur staging pour récupérer les certifications (US-1134) (#53)
    * do not use innerJoin in bookshelf to retrieve certifications
    
    * refacto for tech review

--- api/lib/infrastructure/repositories/certification-repository.js
+++ api/lib/infrastructure/repositories/certification-repository.js
@@ -1,27 +1,31 @@
-const Assessment = require('../../../lib/domain/models/Assessment');
 const Certification = require('../../../lib/domain/models/Certification');
 const CertificationCourseBookshelf = require('../../../lib/infrastructure/data/certification-course');
-
-function _toDomain(certificationCourseBookshelf) {
-  return new Certification({
-    id: certificationCourseBookshelf.get('id'),
-    date: certificationCourseBookshelf.get('completedAt'),
-    certificationCenter: certificationCourseBookshelf.related('session').get('certificationCenter')
-  });
-}
 
 module.exports = {
 
   findCompletedCertificationsByUserId(userId) {
     return CertificationCourseBookshelf
-      .query((qb) => {
-        qb.innerJoin('assessments', 'certification-courses.id', 'assessments.courseId');
-        qb.where('assessments.state', '=', Assessment.states.COMPLETED);
-        qb.where('certification-courses.userId', '=', userId);
-      })
+      .where({ userId })
       .fetchAll({ withRelated: ['session', 'assessment'] })
       .then((certificationCoursesBookshelf) => {
-        return certificationCoursesBookshelf.map(_toDomain);
+
+        const certifications = [];
+
+        certificationCoursesBookshelf.forEach((certificationCourseBookshelf) => {
+          if (certificationCourseBookshelf.related('assessment').get('state') !== 'completed') {
+            return;
+          }
+
+          const certification = new Certification({
+            id: certificationCourseBookshelf.get('id'),
+            date: certificationCourseBookshelf.get('completedAt'),
+            certificationCenter: certificationCourseBookshelf.related('session').get('certificationCenter')
+          });
+
+          certifications.push(certification);
+        });
+
+        return Promise.resolve(certifications);
       });
   }
 };
