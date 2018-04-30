e119f777ab49f79a8e4126fd12695bef922ca504
Antoine Boileau <apem.boileau@gmail.com>
2018-04-30 17:34:37 +0200

    Finish certification repository

--- api/lib/infrastructure/repositories/certification-repository.js
+++ api/lib/infrastructure/repositories/certification-repository.js
@@ -4,89 +4,49 @@ const Assessment = require('../../../lib/domain/models/Assessment');
 const Certification = require('../../../lib/domain/models/Certification');
 const { NotFoundError } = require('../../../lib/domain/errors');
 const CertificationCourseBookshelf = require('../../../lib/infrastructure/data/certification-course');
-const AssessmentResultBookshelf = require('../../../lib/infrastructure/data/assessment-result');
-
-function _getDomainModels(certificationCoursesBookshelf) {
-
-  return Promise.all(
-    certificationCoursesBookshelf.map(_getDomainModel)
-  );
-}
-
-function _getDomainModel(certificationCourseBookshelf) {
-
-  return _getAssessmentResults(certificationCourseBookshelf)
-    .then((assessmentResults) => {
-
-      return new Certification({
-        id: certificationCourseBookshelf.get('id'),
-        date: certificationCourseBookshelf.get('completedAt'),
-        certificationCenter: certificationCourseBookshelf.related('session').get('certificationCenter'),
-        isPublished: Boolean(certificationCourseBookshelf.get('isPublished')),
-        assessmentState: certificationCourseBookshelf.related('assessment').get('state'),
-        assessmentResults: assessmentResults
-      });
-    });
-}
-
-function _getAssessmentResults(certificationCourseBookshelf) {
-
-  return AssessmentResultBookshelf
-    .where({
-      assessmentId: certificationCourseBookshelf.related('assessment').get('id')
-    })
-    .fetchAll()
-    .then((assessmentResultsBookshelf) => assessmentResultsBookshelf.map(_assessmentResultToDomain));
-}
 
 function _assessmentResultToDomain(assessmentResultBookshelf) {
-
   return new AssessmentResult({
     level: assessmentResultBookshelf.get('level'),
     pixScore: assessmentResultBookshelf.get('pixScore'),
-    emitter: assessmentResultBookshelf.get('emitter'),
     status: assessmentResultBookshelf.get('status')
   });
 }
 
+function _createCertificationDomainModel({ certificationCourseBookshelf, assessmentResults }) {
+  return new Certification({
+    id: certificationCourseBookshelf.get('id'),
+    date: certificationCourseBookshelf.get('completedAt'),
+    certificationCenter: certificationCourseBookshelf.related('session').get('certificationCenter'),
+    isPublished: Boolean(certificationCourseBookshelf.get('isPublished')),
+    assessmentState: certificationCourseBookshelf.related('assessment').get('state'),
+    assessmentResults: assessmentResults
+  });
+}
+
 module.exports = {
-
-  findCompletedCertificationsByUserId(userId) {
-    return CertificationCourseBookshelf
-      .where({ userId })
-      .fetchAll({ withRelated: ['session', 'assessment'] })
-      .then((certificationCoursesBookshelf) => {
-
-        const certifications = [];
-
-        certificationCoursesBookshelf.forEach((certificationCourseBookshelf) => {
-          if (certificationCourseBookshelf.related('assessment').get('state') !== 'completed') {
-            return;
-          }
-
-          const certification = new Certification({
-            id: certificationCourseBookshelf.get('id'),
-            date: certificationCourseBookshelf.get('completedAt'),
-            certificationCenter: certificationCourseBookshelf.related('session').get('certificationCenter')
-          });
-
-          certifications.push(certification);
-        });
-
-        return Promise.resolve(certifications);
-      });
-  },
-
   findCertificationsByUserId(userId) {
+
     return CertificationCourseBookshelf
       .where({ userId })
       .fetchAll({
         withRelated: [
           'session',
-          'assessment'
+          'assessment',
+          'assessment.assessmentResults'
         ]
       })
-      .then(_getDomainModels);
+      .then((certificationCoursesBookshelf) => {
+        return certificationCoursesBookshelf.map((certificationCourseBookshelf) => {
+          const assessmentResultsBookshelf = certificationCourseBookshelf
+            .related('assessment')
+            .related('assessmentResults');
+
+          const assessmentResults = assessmentResultsBookshelf.map(_assessmentResultToDomain);
+
+          return _createCertificationDomainModel({ certificationCourseBookshelf, assessmentResults });
+        });
+      });
   },
 
   updateCertification({ id, attributes }) {
--- api/tests/integration/infrastructure/repositories/certification-repository_test.js
+++ api/tests/integration/infrastructure/repositories/certification-repository_test.js
@@ -6,127 +6,6 @@ const { NotFoundError } = require('../../../../lib/domain/errors');
 
 describe('Integration | Repository | Certification ', () => {
 
-  describe('#findCompletedCertificationsByUserId', () => {
-
-    const JOHN_USERID = 1;
-    const JANE_USERID = 2;
-    const JOHN_CERTIFICATION_ID = 1;
-    const JANE_COMPLETED_CERTIFICATION_ID = 2;
-    const JANE_NOT_COMPLETED_CERTIFICATION_ID = 3;
-
-    context('when there are no certification to return', () => {
-
-      it('should return an empty list', () => {
-        // when
-        const promise = certificationRepository.findCompletedCertificationsByUserId(JANE_USERID);
-
-        // then
-        return promise.then((certifications) => {
-          expect(certifications).to.be.an('array').and.to.have.lengthOf(0);
-        });
-      });
-    });
-
-    context('when there are certifications in different states and for multiple users', () => {
-
-      const john_certificationCourse = {
-        id: JOHN_CERTIFICATION_ID,
-        userId: JOHN_USERID,
-        firstName: 'John',
-        lastName: 'Doe',
-        birthplace: 'Earth',
-        birthdate: '24/10/1989',
-        completedAt: '01/02/2003',
-        sessionId: 1
-      };
-
-      const jane_certificationCourseCompleted = {
-        id: JANE_COMPLETED_CERTIFICATION_ID,
-        userId: JANE_USERID,
-        firstName: 'Jane',
-        lastName: 'Kalamity',
-        birthplace: 'Earth',
-        birthdate: '24/10/1989',
-        completedAt: '01/02/2004',
-        sessionId: 1
-      };
-
-      const jane_certificationCourseNotCompleted = {
-        id: JANE_NOT_COMPLETED_CERTIFICATION_ID,
-        userId: JANE_USERID,
-        firstName: 'Jane',
-        lastName: 'Kalamity',
-        birthplace: 'Earth',
-        birthdate: '24/10/1989',
-        completedAt: '01/02/2004',
-        sessionId: 2
-      };
-
-      const john_completedAssessment = {
-        courseId: JOHN_CERTIFICATION_ID,
-        userId: JOHN_USERID,
-        type: 'CERTIFICATION',
-        state: 'completed'
-      };
-
-      const jane_completedAssessment = {
-        courseId: JANE_COMPLETED_CERTIFICATION_ID,
-        userId: JANE_USERID,
-        type: 'CERTIFICATION',
-        state: 'completed'
-      };
-
-      const jane_notCompletedAssessment = {
-        courseId: JANE_NOT_COMPLETED_CERTIFICATION_ID,
-        userId: JANE_USERID,
-        type: 'CERTIFICATION',
-        state: 'started'
-      };
-
-      const session = {
-        id: 1,
-        certificationCenter: 'Université du Pix',
-        address: '137 avenue de Bercy',
-        room: 'La grande',
-        examiner: 'Serge le Mala',
-        date: '24/10/1989',
-        time: '21:30',
-        accessCode: 'ABCD12'
-      };
-
-      beforeEach(() => {
-        return knex('sessions').insert(session)
-          .then(() => knex('certification-courses').insert([john_certificationCourse, jane_certificationCourseCompleted, jane_certificationCourseNotCompleted]))
-          .then(() => knex('assessments').insert([john_completedAssessment, jane_completedAssessment, jane_notCompletedAssessment]));
-      });
-
-      afterEach(() => {
-        return knex('assessments').delete()
-          .then(() => {
-            return knex('certification-courses').delete();
-          })
-          .then(() => {
-            return knex('sessions').delete();
-          });
-      });
-
-      it('should return a list of the completed Certifications for the specified user', () => {
-        // when
-        const promise = certificationRepository.findCompletedCertificationsByUserId(JANE_USERID);
-
-        // then
-        return promise.then((certifications) => {
-          expect(certifications).to.be.an('array');
-          expect(certifications.length).to.equal(1);
-          expect(certifications[0]).to.be.an.instanceOf(Certification);
-          expect(certifications[0].id).not.to.be.undefined;
-          expect(certifications[0].certificationCenter).to.equal('Université du Pix');
-          expect(certifications[0].date).to.equal('01/02/2004');
-        });
-      });
-    });
-  });
-
   describe('#findCertificationbyUserId', () => {
 
     const USER_ID = 1;
@@ -193,9 +72,7 @@ describe('Integration | Repository | Certification ', () => {
     it('should return an array of Certification with needed informations', () => {
       // given
       const assessmentResult = new AssessmentResult({
-        level: 1,
         pixScore: 23,
-        emitter: 'PIX-ALGO',
         status: 'rejected'
       });
       const expectedCertifications = [
@@ -205,8 +82,6 @@ describe('Integration | Repository | Certification ', () => {
           date: '12/02/2000',
           isPublished: true,
           assessmentState: 'completed',
-          status: 'rejected_',
-          pixScore: 23,
           assessmentResults: [assessmentResult]
         })
       ];
