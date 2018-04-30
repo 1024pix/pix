5992833178bb117e419bcead46b249a378f48511
Brandone Martins <brandone.martins@gmail.com>
2018-04-30 15:58:00 +0200

    do not use innerJoin in bookshelf to retrieve certifications

--- api/lib/infrastructure/repositories/certification-repository.js
+++ api/lib/infrastructure/repositories/certification-repository.js
@@ -1,27 +1,31 @@
-const Assessment = require('../../../lib/domain/models/Assessment');
 const Certification = require('../../../lib/domain/models/Certification');
 const CertificationCourseBookshelf = require('../../../lib/infrastructure/data/certification-course');
-
-function _toDomain(certificationCourseBookshelf) {
-  return new Certification({
-    id: certificationCourseBookshelf.get('id'),
-    date: certificationCourseBookshelf.get('completedAt'),
-    certificationCenter: certificationCourseBookshelf.related('session').get('certificationCenter')
-  });
-}
 
 module.exports = {
 
   findCompletedCertificationsByUserId(userId) {
     return CertificationCourseBookshelf
-      .query((qb) => {
-        qb.innerJoin('assessments', 'certification-courses.id', 'assessments.courseId');
-        qb.where('assessments.state', '=', Assessment.states.COMPLETED);
-        qb.where('certification-courses.userId', '=', userId);
-      })
+      .where({ userId })
       .fetchAll({ withRelated: ['session', 'assessment'] })
       .then((certificationCoursesBookshelf) => {
-        return certificationCoursesBookshelf.map(_toDomain);
+
+        if (!certificationCoursesBookshelf) {
+          return [];
+        }
+
+        const certifications = [];
+
+        certificationCoursesBookshelf.map((certificationCourseBookshelf) => {
+          if (certificationCourseBookshelf.related('assessment').get('state') === 'completed') {
+            certifications.push(new Certification({
+              id: certificationCourseBookshelf.get('id'),
+              date: certificationCourseBookshelf.get('completedAt'),
+              certificationCenter: certificationCourseBookshelf.related('session').get('certificationCenter')
+            }));
+          }
+        });
+
+        return Promise.resolve(certifications);
       });
   }
 };
