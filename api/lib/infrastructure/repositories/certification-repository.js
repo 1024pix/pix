34481d512d6e9b9e7302307db88817348e3403cc
Antoine Boileau <apem.boileau@gmail.com>
2018-04-30 16:08:14 +0200

    Add a first version of certification repository

--- api/lib/infrastructure/repositories/certification-repository.js
+++ api/lib/infrastructure/repositories/certification-repository.js
@@ -1,6 +1,53 @@
+const AssessmentResult = require('../../../lib/domain/models/AssessmentResult');
+const Assessment = require('../../../lib/domain/models/Assessment');
+
 const Certification = require('../../../lib/domain/models/Certification');
 const { NotFoundError } = require('../../../lib/domain/errors');
 const CertificationCourseBookshelf = require('../../../lib/infrastructure/data/certification-course');
+const AssessmentResultBookshelf = require('../../../lib/infrastructure/data/assessment-result');
+
+function _getDomainModels(certificationCoursesBookshelf) {
+
+  return Promise.all(
+    certificationCoursesBookshelf.map(_getDomainModel)
+  );
+}
+
+function _getDomainModel(certificationCourseBookshelf) {
+
+  return _getAssessmentResults(certificationCourseBookshelf)
+    .then((assessmentResults) => {
+
+      return new Certification({
+        id: certificationCourseBookshelf.get('id'),
+        date: certificationCourseBookshelf.get('completedAt'),
+        certificationCenter: certificationCourseBookshelf.related('session').get('certificationCenter'),
+        isPublished: Boolean(certificationCourseBookshelf.get('isPublished')),
+        assessmentState: certificationCourseBookshelf.related('assessment').get('state'),
+        assessmentResults: assessmentResults
+      });
+    });
+}
+
+function _getAssessmentResults(certificationCourseBookshelf) {
+
+  return AssessmentResultBookshelf
+    .where({
+      assessmentId: certificationCourseBookshelf.related('assessment').get('id')
+    })
+    .fetchAll()
+    .then((assessmentResultsBookshelf) => assessmentResultsBookshelf.map(_assessmentResultToDomain));
+}
+
+function _assessmentResultToDomain(assessmentResultBookshelf) {
+
+  return new AssessmentResult({
+    level: assessmentResultBookshelf.get('level'),
+    pixScore: assessmentResultBookshelf.get('pixScore'),
+    emitter: assessmentResultBookshelf.get('emitter'),
+    status: assessmentResultBookshelf.get('status')
+  });
+}
 
 module.exports = {
 
@@ -31,7 +78,15 @@ module.exports = {
   },
 
   findCertificationsByUserId(userId) {
-    return Promise.resolve([]);
+    return CertificationCourseBookshelf
+      .where({ userId })
+      .fetchAll({
+        withRelated: [
+          'session',
+          'assessment'
+        ]
+      })
+      .then(_getDomainModels);
   },
 
   updateCertification({ id, attributes }) {
