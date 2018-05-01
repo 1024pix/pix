4f8c81712b46ac1e05e10b330eac8839b22a5751
Jérémy Buget <jbuget@gmail.com>
2018-05-01 10:44:39 +0200

    Bump dependencies version (and improve notifications configuration)

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,9 +5,4 @@ import { inject as service } from '@ember/service';
 export default Route.extend(ApplicationRouteMixin, {
   notifications: service('notification-messages'),
   routeAfterAuthentication: 'authenticated.organizations.list',
-
-  init() {
-    this._super(...arguments);
-    this.get('notifications').setDefaultAutoClear(true);
-  }
 });
