285e19f6d1c3eb2afbb6b2ccee69b6c396c8b824
Jérémy Buget <jbuget@gmail.com>
2018-05-01 15:35:52 +0200

    Setup Ember Exam (tests with randomization, splitting, and parallelization)

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,7 +2,12 @@ import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import loadEmberExam from 'ember-exam/test-support/load';
+
+loadEmberExam();
 
 setApplication(Application.create(config.APP));
 
 start();
+
+
