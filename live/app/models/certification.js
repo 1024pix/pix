8f5a19fa965542628fa6f53986aecac88b211daa
Antoine Boileau <apem.boileau@gmail.com>
2018-05-02 11:32:33 +0200

    Update certification ember model object

--- live/app/models/certification.js
+++ live/app/models/certification.js
@@ -4,9 +4,9 @@ const { Model, attr, belongsTo } = DS;
 
 export default Model.extend({
   date: attr('string'),
-  status: attr('string'),
-  score: attr('string'),
   certificationCenter: attr('string'),
+  isPublished: attr('boolean'),
+  pixScore: attr('number'),
+  status: attr('string'),
   user: belongsTo('user'),
-
 });
