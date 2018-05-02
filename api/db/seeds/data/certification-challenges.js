5bbfe1acd340fa8e00aa3eadf817731c7d1b3fa2
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2018-05-02 18:04:55 +0200

    Revert "[#50] [TECH] Ajout d'un jeu d'exemple pour une certification rejeté par l'algo (PF-98)."
    This reverts commit 4a15ad473d7e6d45edb229cc3f10e6c9930988e0.

--- api/db/seeds/data/certification-challenges.js
+++ api/db/seeds/data/certification-challenges.js
@@ -13,30 +13,5 @@ module.exports = [
   { challengeId: 'rec5o3fE3scuqxKnx', competenceId: 'recOdC9UDVJbAXHAm', associatedSkill: '@distinctionContenuForme5',courseId: 1 },
   { challengeId: 'rec3CXFJOTMy9dUHu', competenceId: 'recsvLz0W2ShyfD63', associatedSkill: '@eval6',courseId: 1 },
   { challengeId: 'rec39bDMnaVw3MyMR', competenceId: 'recsvLz0W2ShyfD63', associatedSkill: '@utiliserserv6',courseId: 1 },
-  { challengeId: 'recjtrBI01FvZHYfz', competenceId: 'recsvLz0W2ShyfD63', associatedSkill: '@url6', courseId: 1 },
-
-  // Mener une recherche et une veille d’information
-  { challengeId: 'rec39bDMnaVw3MyMR', competenceId: 'recsvLz0W2ShyfD63', associatedSkill: '@utiliserserv6',courseId: 2 },
-  { challengeId: 'rec3CXFJOTMy9dUHu', competenceId: 'recsvLz0W2ShyfD63', associatedSkill: '@eval6',courseId: 2 },
-  { challengeId: 'recjtrBI01FvZHYfz', competenceId: 'recsvLz0W2ShyfD63', associatedSkill: '@url6',courseId: 2 },
-
-  // Traiter des données
-  { challengeId: 'rec5WBXcLWAlMZlUI', competenceId: 'recNv8qhaY887jQb2', associatedSkill: '@form_intero6',courseId: 2 },
-  { challengeId: 'recBYXbGG8m7AYIPs', competenceId: 'recNv8qhaY887jQb2', associatedSkill: '@recopie6',courseId: 2 },
-  { challengeId: 'rec1GYIaMxDz1eS51', competenceId: 'recNv8qhaY887jQb2', associatedSkill: '@openData6',courseId: 2 },
-
-  // Interagir
-  { challengeId: 'recUgaNAIuu2ng0wR', competenceId: 'recDH19F7kKrfL3Ii', associatedSkill: '@communicationAsynchrone6',courseId: 2 },
-  { challengeId: 'recDEsKGRnH3yt5rG', competenceId: 'recDH19F7kKrfL3Ii', associatedSkill: '@risquescourriel5',courseId: 2 },
-  { challengeId: 'recAprCTY1ixJ9ISA', competenceId: 'recDH19F7kKrfL3Ii', associatedSkill: '@champsCourriel5',courseId: 2 },
-
-  // Développer des documents textuels
-  { challengeId: 'recRVQCpdir9b0x1H', competenceId: 'recOdC9UDVJbAXHAm', associatedSkill: '@miseEnFormeDiaporama6',courseId: 2 },
-  { challengeId: 'recn838PC4uEeQLcW', competenceId: 'recOdC9UDVJbAXHAm', associatedSkill: '@miseEnFormeDiaporama5',courseId: 2 },
-  { challengeId: 'rec5o3fE3scuqxKnx', competenceId: 'recOdC9UDVJbAXHAm', associatedSkill: '@distinctionContenuForme5',courseId: 2 },
-
-  // Résoudre des problèmes techniques
-  { challengeId: 'rect9c6ahXsCjNn3e', competenceId: 'recIhdrmCuEmCDAzj', associatedSkill: '@ouvertureFichiers6',courseId: 2 },
-  { challengeId: 'recUuLdYdKOoOh2W2', competenceId: 'recIhdrmCuEmCDAzj', associatedSkill: '@probl\xe8melavier6',courseId: 2 },
-  { challengeId: 'rec6qcVEOmD3JNE8v', competenceId: 'recIhdrmCuEmCDAzj', associatedSkill: '@connexionSmart5',courseId: 2 },
+  { challengeId: 'recjtrBI01FvZHYfz', competenceId: 'recsvLz0W2ShyfD63', associatedSkill: '@url6',courseId: 1 }
 ];
--- api/db/seeds/data/sessions.js
+++ api/db/seeds/data/sessions.js
@@ -9,16 +9,5 @@ module.exports = [
     description: 'Sesssion 1',
     room: 'Salle 2',
     accessCode: 'ABC12'
-  },
-  {
-    id: 2,
-    certificationCenter: 'Tour Gamma',
-    address: 'Rue de bercy',
-    examiner: 'Sophie',
-    date: '2028-04-27',
-    time: '10:00',
-    description: 'Session de rattrapage',
-    room: 'Salle 2',
-    accessCode: 'DEF34'
   }
 ];
