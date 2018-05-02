fe5dae71ac543a7917802bdc876788b358deb8c9
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2018-05-02 18:04:55 +0200

    Revert "[#50] [TECH] Ajout d'un jeu d'exemple pour une certification rejeté par l'algo (PF-98)."
    This reverts commit f7d78556ef0b2ca57b1bf12ea7d9e270bab94d3b.

--- api/db/seeds/data/assessment-results.js
+++ api/db/seeds/data/assessment-results.js
@@ -1,31 +1,9 @@
 module.exports = [
-  { id:1, createdAt: '2018-02-15 14:59:35', emitter: 'PIX-ALGO', commentForJury: 'Computed', status: 'validated', assessmentId: 1, level: 5, pixScore: 44 },
-  { id:2, createdAt: '2018-02-15 15:00:34', emitter: 'PIX-ALGO', commentForJury: 'Computed', status: 'validated', assessmentId: 2, level: 2, pixScore: 23 },
-  { id:3, createdAt: '2018-02-15 15:03:18', emitter: 'PIX-ALGO', commentForJury: 'Computed', status: 'validated', assessmentId: 3, level: 5, pixScore: 47 },
-  { id:4, createdAt: '2018-02-15 15:04:26', emitter: 'PIX-ALGO', commentForJury: 'Computed', status: 'validated', assessmentId: 4, level: 4, pixScore: 34 },
-  { id:5, createdAt: '2018-02-15 15:07:02', emitter: 'PIX-ALGO', commentForJury: 'Computed', status: 'validated', assessmentId: 5, level: 5, pixScore: 48 },
-  { id:6, createdAt: '2018-02-15 15:14:46', emitter: 'PIX-ALGO', commentForJury: 'Computed', status: 'validated', assessmentId: 6, level: 0, pixScore: 157 },
-  { id:7, createdAt: '2018-04-27 10:11:02', emitter: 'PIX-ALGO', commentForJury: 'Computed', status: 'rejected', assessmentId: 7, level: -1, pixScore: 0 },
-  {
-    id: 8,
-    createdAt: '2018-04-28 09:17:41',
-    emitter: 'Benoit',
-    commentForJury: 'Ceci est un commentaire jury à destination du Jury. Le Lorem Ipsum est simplement du faux texte ' +
-    'employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de ' +
-    'l\'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour ' +
-    'réaliser un livre spécimen de polices de texte.',
-    commentForOrganization: 'Ceci est un commentaire jury à destination de l\'organisation. Le Lorem Ipsum est ' +
-    'simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le' +
-    ' faux texte standard de l\'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des' +
-    ' morceaux de texte pour réaliser un livre spécimen de polices de texte.',
-    commentForCandidate: 'Ceci est un commentaire jury à destination du candidat. Le Lorem Ipsum est simplement du faux ' +
-    'texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de ' +
-    'l\'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour ' +
-    'réaliser un livre spécimen de polices de texte.x',
-    status: 'rejected',
-    assessmentId: 7,
-    level: -1,
-    pixScore: 0
-  }
+  { id:1, createdAt: '2018-02-15 14:59:35', emitter: 'PIX-ALGO', commentForJury: 'Computed', status: 'validated', assessmentId: 1, level: 5, pixScore: 44},
+  { id:2, createdAt: '2018-02-15 15:00:34', emitter: 'PIX-ALGO', commentForJury: 'Computed', status: 'validated', assessmentId: 2, level: 2, pixScore: 23},
+  { id:3, createdAt: '2018-02-15 15:03:18', emitter: 'PIX-ALGO', commentForJury: 'Computed', status: 'validated', assessmentId: 3, level: 5, pixScore: 47},
+  { id:4, createdAt: '2018-02-15 15:04:26', emitter: 'PIX-ALGO', commentForJury: 'Computed', status: 'validated', assessmentId: 4, level: 4, pixScore: 34},
+  { id:5, createdAt: '2018-02-15 15:07:02', emitter: 'PIX-ALGO', commentForJury: 'Computed', status: 'validated', assessmentId: 5, level: 5, pixScore: 48},
+  { id:6, createdAt: '2018-02-15 15:14:46', emitter: 'PIX-ALGO', commentForJury: 'Computed', status: 'validated', assessmentId: 6, level: 0, pixScore: 157},
 ];
 
