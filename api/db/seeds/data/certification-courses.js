f8f3e3564513f0341ccf2d8099272682e4b5a196
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2018-05-03 18:22:27 +0200

    [#54] [TECH] Ajout d'un jeu d'exemple pour une certification rejeté par l'algo (PF-98).
    * [#50] [TECH] Ajout d'un jeu d'exemple pour une certification rejeté par l'algo (PF-98).
    
    * Added a test new case for certifications
    
    * Fix seeds

--- api/db/seeds/data/certification-courses.js
+++ api/db/seeds/data/certification-courses.js
@@ -1,6 +1,6 @@
 module.exports = [
   {
-    id:1,
+    id: 1,
     userId: 1,
     completedAt: '2018-02-15T15:15:52.504Z',
     createdAt: '2018-02-15 15:14:46',
@@ -10,5 +10,29 @@ module.exports = [
     birthplace: 'Paris',
     sessionId: 1,
     externalId: 'Hubert'
+  },
+  {
+    id: 2,
+    userId: 1,
+    completedAt: '2018-04-27T10:11:02.504Z',
+    createdAt: '2018-04-27T10:10:52.504Z',
+    firstName: 'Kevin',
+    lastName: 'Ducobu',
+    birthdate: '01/04/1994',
+    birthplace: 'Bruxelles',
+    sessionId: 2,
+    externalId: 'L\'élève'
+  },
+  {
+    id: 3,
+    userId: 1,
+    completedAt: '2018-05-12T15:15:52.504Z',
+    createdAt: '2018-05-12 15:14:46',
+    firstName: 'Pix',
+    lastName: 'Aile',
+    birthdate: '12/12/1964',
+    birthplace: 'Paris <- Et oui !',
+    sessionId: 2,
+    externalId: 'NumeroEtudiant'
   }
 ];
