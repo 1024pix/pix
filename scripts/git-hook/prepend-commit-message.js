bc7932c684cb6e885b161bea677c992c8d9dac29
Antoine Boileau <apem.boileau@gmail.com>
2018-05-14 15:37:42 +0200

    Finalize git hook script

--- scripts/git-hook/prepend-commit-message.js
+++ scripts/git-hook/prepend-commit-message.js
@@ -10,7 +10,7 @@ const { execSync } = require('child_process');
  * > Doesn't start with #
  *
  * AND
- * > branch name starts with AA-XXX_something (e.g. pi-123_branch-description)
+ * > branch name starts with AA-XXX-something (e.g. pi-123-branch-description)
  *
  * THEN
  * > prepend the issue tag to the commit message
@@ -18,14 +18,14 @@ const { execSync } = require('child_process');
  * My awesome commit -> [pi-123] My awesome commit
  */
 
-console.log('Checking commit message...');
+console.log('[prepend-commit-message] Checking commit message...');
 
 const startsWithBraces = (str) => str.match(/^\[[^\]]/);
 const startsWithMergeBranch = (str) => str.indexOf('Merge branch') === 0;
 const startsWithMergePR = (str) => str.indexOf('Merge pull request') === 0;
 const startsWithHash = (str) => str.indexOf('#') === 0;
 
-const isMessageToBePrepended = (str) =>
+const isCommitMessageToBePrepended = (str) =>
   !startsWithBraces(str) &&
   !startsWithMergeBranch(str) &&
   !startsWithMergePR(str) &&
@@ -35,7 +35,9 @@ const tagMatcher = new RegExp('^(..-\\d+)', 'i');
 
 const getIssueTagFromBranchName = (str) => {
   const matched = str.match(tagMatcher);
-  return matched[0];
+  if (matched && matched[0]) {
+    return matched[0];
+  }
 };
 
 const messageFile = process.env.GIT_PARAMS;
@@ -43,20 +45,19 @@ const message = fs.readFileSync(messageFile, { encoding: 'utf-8' });
 
 const messageTitle = message.split('\n')[0];
 
-const branchName = execSync('git rev-parse --abbrev-ref HEAD', { encoding: 'utf-8' })
-  .split('\n')[0];
-
+const branchName = execSync('git rev-parse --abbrev-ref HEAD', { encoding: 'utf-8' }).split('\n')[0];
 const issueTag = getIssueTagFromBranchName(branchName);
 
-if (issueTag && isMessageToBePrepended(messageTitle)) {
-  console.log('Changing commit message...');
+if (issueTag && isCommitMessageToBePrepended(messageTitle)) {
 
   // Apply the issue tag to message title
   const messageLines = message.split('\n');
   messageLines[0] = `[${issueTag}] ${messageTitle}`;
   fs.writeFileSync(messageFile, messageLines.join('\n'), { encoding: 'utf-8' });
 
-  console.log(`New message title: ${messageLines[0]}`);
+  console.log(`[prepend-commit-message] New message title: ${messageLines[0]}`);
+
+} else {
+  console.log('[prepend-commit-message] Commit message not to be modified');
 }
 
-console.log('Commit message ok');
