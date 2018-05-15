f782bb6c4e562b6d2a2dd10fb20f9101240720a3
Antoine Boileau <apem.boileau@gmail.com>
2018-05-15 15:28:17 +0200

    [pf-13] Add detail column and expandable panel

--- live/tests/integration/components/certifications-list-item-test.js
+++ live/tests/integration/components/certifications-list-item-test.js
@@ -4,9 +4,9 @@ import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
 import EmberObject from '@ember/object';
 
-describe('Integration | Component | certifications list item', function() {
+describe.only('Integration | Component | certifications list item', function() {
   setupComponentTest('certifications-list-item', {
-    integration: true
+    integration: true,
   });
 
   let certification;
@@ -26,7 +26,7 @@ describe('Integration | Component | certifications list item', function() {
         status: 'validated',
         certificationCenter: 'Université de Paris',
         isPublished: false,
-        pixScore: 231
+        pixScore: 231,
       });
       this.set('certification', certification);
 
@@ -47,30 +47,71 @@ describe('Integration | Component | certifications list item', function() {
 
   context('when the certification is published and rejected', function() {
 
-    beforeEach(function() {
-      // given
-      certification = EmberObject.create({
-        id: 1,
-        date: '2018-02-15T15:15:52.504Z',
-        status: 'rejected',
-        certificationCenter: 'Université de Paris',
-        isPublished: true,
-        pixScore: 231
+    context('without commentForCandidate', function() {
+      beforeEach(function() {
+        // given
+        certification = EmberObject.create({
+          id: 1,
+          date: '2018-02-15T15:15:52.504Z',
+          status: 'rejected',
+          certificationCenter: 'Université de Paris',
+          isPublished: true,
+          pixScore: 231,
+          commentForCandidate: null,
+        });
+        this.set('certification', certification);
+
+        // when
+        this.render(hbs`{{certifications-list-item certification=certification}}`);
       });
-      this.set('certification', certification);
 
-      // when
-      this.render(hbs`{{certifications-list-item certification=certification}}`);
+      // then
+      it('should render a certifications-list-item__published-item div', function() {
+        expect(this.$('.certifications-list-item__published-item')).to.have.lengthOf(1);
+      });
+
+      it('should show Certification non obtenue', function() {
+        expect(this.$('img.certifications-list-item__cross-img')).to.have.lengthOf(1);
+        expect(this.$('.certifications-list-item').text()).to.include('Certification non obtenue');
+      });
+
+      it('should show not show Détail in last column', function() {
+        expect(this.$('.certifications-list-item__cell-detail-button')).to.have.lengthOf(0);
+      });
     });
 
-    // then
-    it('should render a certifications-list-item__published-item div', function() {
-      expect(this.$('.certifications-list-item__published-item')).to.have.lengthOf(1);
-    });
+    context('with a commentForCandidate', function() {
+      beforeEach(function() {
+        // given
+        certification = EmberObject.create({
+          id: 1,
+          date: '2018-02-15T15:15:52.504Z',
+          status: 'rejected',
+          certificationCenter: 'Université de Paris',
+          isPublished: true,
+          pixScore: 231,
+          commentForCandidate: 'Commentaire pour le candidat',
+        });
+        this.set('certification', certification);
 
-    it('should show Certification non obtenue', function() {
-      expect(this.$('img.certifications-list-item__cross-img')).to.have.lengthOf(1);
-      expect(this.$('.certifications-list-item').text()).to.include('Certification non obtenue');
+        // when
+        this.render(hbs`{{certifications-list-item certification=certification}}`);
+      });
+
+      // then
+      it('should render a certifications-list-item__published-item div', function() {
+        expect(this.$('.certifications-list-item__published-item')).to.have.lengthOf(1);
+      });
+
+      it('should show Certification non obtenue', function() {
+        expect(this.$('img.certifications-list-item__cross-img')).to.have.lengthOf(1);
+        expect(this.$('.certifications-list-item').text()).to.include('Certification non obtenue');
+      });
+
+      it('should show Détail in last column', function() {
+        expect(this.$('.certifications-list-item__cell-detail-button')).to.have.lengthOf(1);
+        expect(this.$('.certifications-list-item__cell-detail-button').text()).to.include('DÉTAIL');
+      });
     });
   });
 
@@ -84,7 +125,7 @@ describe('Integration | Component | certifications list item', function() {
         status: 'validated',
         certificationCenter: 'Université de Paris',
         isPublished: true,
-        pixScore: 231
+        pixScore: 231,
       });
       this.set('certification', certification);
 
