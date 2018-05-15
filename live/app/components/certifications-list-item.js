8fedda6579b65a937b64c35f6b779c302b05b356
Antoine Boileau <apem.boileau@gmail.com>
2018-05-15 15:28:17 +0200

    [pf-13] Add detail column and expandable panel

--- live/app/components/certifications-list-item.js
+++ live/app/components/certifications-list-item.js
@@ -1,10 +1,14 @@
 import { computed } from '@ember/object';
 import Component from '@ember/component';
 
+let { and, not, equal } = computed;
+
 export default Component.extend({
   certification: null,
   classNames: ['certifications-list-item'],
   classNameBindings: ['certification.isPublished:certifications-list-item__published-item:certifications-list-item__unpublished-item'],
 
-  isValidated: computed.equal('certification.status', 'validated'),
+  isValidated: equal('certification.status', 'validated'),
+  isNotValidated: not('isValidated'),
+  hasComment: and('isNotValidated', 'certification.{isPublished,commentForCandidate}'),
 });
