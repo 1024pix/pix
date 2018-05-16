aff8a2809ec7adf315328ad46e696eee66b47a92
Benoit <benoit.prady@pix.fr>
2018-05-16 19:25:57 +0200

    added certification-details-answer component

---
+++ app/components/certification-details-answer.js
@@ -0,0 +1,19 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  classNames: ['card', 'text-center', 'certification-details-answer', 'border-secondary'],
+  answer: null,
+  resultClass: computed('answer.jury', function() {
+    const answerJury = this.get("answer.jury");
+    return (answerJury)? "answer-result jury" : "answer-result";
+  }),
+  actions: {
+    setResult(value){
+      const answer = this.get("answer");
+      let jury = (value !== answer.result) ? value : false;
+      this.set("answer.jury", jury);
+      this.get("updateCompetence")();
+    }
+  }
+});
