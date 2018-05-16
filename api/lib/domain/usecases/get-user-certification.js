8eb66bbbb06fce5a48c5f956655fa94f5c5e43aa
Antoine Boileau <apem.boileau@gmail.com>
2018-05-16 17:33:15 +0200

    [pf-5] Create getUserCertification usecase

--- api/lib/domain/usecases/get-user-certification.js
+++ api/lib/domain/usecases/get-user-certification.js
@@ -1,3 +1,12 @@
-module.exports = function({}) {
-  return Promise.resolve();
+const { UserNotAuthorizedToAccessEntity } = require('../errors');
+
+module.exports = function({ certificationId, certificationRepository, userId }) {
+  return certificationRepository.getCertification({ id: certificationId })
+    .then((certification) => {
+      if (certification.userId !== parseInt(userId, 10)) {
+        throw new UserNotAuthorizedToAccessEntity();
+      }
+
+      return certification;
+    });
 };
