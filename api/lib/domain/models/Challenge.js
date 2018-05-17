b771f439145be4c99a9e6ef5206431aec66ade82
MelanieMEB <mboudard@octo.com>
2018-05-17 14:29:16 +0200

    [PF-91] Add function of algo in Challenge Model

--- api/lib/domain/models/Challenge.js
+++ api/lib/domain/models/Challenge.js
@@ -12,8 +12,13 @@ class Challenge {
     return this.skills.filter((skill) => skill.name === searchedSkill.name).length > 0;
   }
 
+  // Same than isActive for algo
   isPublished() {
     return ['validé', 'validé sans test', 'pré-validé'].includes(this.status);
+  }
+
+  get hardestSkill() {
+    return this.skills.reduce((s1, s2) => (s1.difficulty > s2.difficulty) ? s1 : s2);
   }
 
 }
