c2ea2c47d6ab4e27404c11e8bfe5bfdc8b1bf57f
Jérémy Buget <jbuget@gmail.com>
2018-05-19 16:12:16 +0200

    [PF-38] Add button to launch simulator on component challenge-statement-embed-panel

--- live/tests/integration/components/challenge-statement-embed-panel-test.js
+++ live/tests/integration/components/challenge-statement-embed-panel-test.js
@@ -6,25 +6,76 @@ import sinon from 'sinon';
 import { run } from '@ember/runloop';
 
 describe('Integration | Component | challenge statement embed panel', function() {
+
   setupComponentTest('challenge-statement-embed-panel', {
     integration: true
   });
 
-  it('renders', function() {
-    this.render(hbs`{{challenge-statement-embed-panel}}`);
-    expect(this.$()).to.have.length(1);
+  describe('Aknowledgment overlay', function() {
+
+    it('should be displayed when component has just been rendered', function() {
+      // when
+      this.render(hbs`{{challenge-statement-embed-panel}}`);
+
+      // then
+      expect(this.$('.challenge-statement-embed-panel__aknowledgment-overlay')).to.have.lengthOf(1);
+    });
+
+    it('should contain a button to launch the simulator', function() {
+      // when
+      this.render(hbs`{{challenge-statement-embed-panel}}`);
+
+      // then
+      const $launchSimulatorButton = this.$('.challenge-statement-embed-panel__aknowledgment-overlay .challenge-statement-embed-panel__lunch-simulator-button');
+      expect($launchSimulatorButton).to.have.lengthOf(1);
+    });
   });
 
-  it('should reload iframe when user clicked on button reload', async function() {
-    // given
-    const stubReloadIframe = sinon.stub();
-    this.set('stubReloadIframe', stubReloadIframe);
-    this.render(hbs`{{challenge-statement-embed-panel _reloadIframe=stubReloadIframe}}`);
+  describe('Launch simulator button', () => {
 
-    // when
-    run(() => document.querySelector('.challenge-statement-embed-panel__reload-button').click());
+    it('should have text "Lancer le simulateur"', function() {
+      // when
+      this.render(hbs`{{challenge-statement-embed-panel}}`);
 
-    // then
-    sinon.assert.calledOnce(stubReloadIframe);
+      // then
+      const $launchSimulatorButton = this.$('.challenge-statement-embed-panel__aknowledgment-overlay .challenge-statement-embed-panel__lunch-simulator-button');
+      expect($launchSimulatorButton.text().trim()).to.equal('Lancer le simulateur');
+    });
+
+    it('should close the aknowledgment overlay when clicked', async function() {
+      // given
+      this.render(hbs`{{challenge-statement-embed-panel}}`);
+
+      // when
+      run(() => document.querySelector('.challenge-statement-embed-panel__lunch-simulator-button').click());
+
+      // then
+      expect(this.$('.challenge-statement-embed-panel__aknowledgment-overlay')).to.have.lengthOf(0);
+    });
+  });
+
+  describe('Reload simulator button', () => {
+
+    it('should have text "Recharger le simulateur"', function() {
+      // when
+      this.render(hbs`{{challenge-statement-embed-panel}}`);
+
+      // then
+      const $reloadSimulatorButton = this.$('.challenge-statement-embed-panel__reload-button');
+      expect($reloadSimulatorButton.text().trim()).to.equal('Recharger le simulateur');
+    });
+
+    it('should reload simulator when user clicked on button reload', async function() {
+      // given
+      const stubReloadSimulator = sinon.stub();
+      this.set('stubReloadSimulator', stubReloadSimulator);
+      this.render(hbs`{{challenge-statement-embed-panel _reloadSimulator=stubReloadSimulator}}`);
+
+      // when
+      run(() => document.querySelector('.challenge-statement-embed-panel__reload-button').click());
+
+      // then
+      sinon.assert.calledOnce(stubReloadSimulator);
+    });
   });
 });
