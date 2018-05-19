36dc111a916fca32a794496784f7f77ae78385ac
Jérémy Buget <jbuget@gmail.com>
2018-05-19 15:24:22 +0200

    [PF-38] Add button/action to reload embed document iframe in challenge-statement-embed-panel

--- live/tests/integration/components/challenge-statement-embed-panel-test.js
+++ live/tests/integration/components/challenge-statement-embed-panel-test.js
@@ -2,6 +2,8 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
+import sinon from 'sinon';
+import { run } from '@ember/runloop';
 
 describe('Integration | Component | challenge statement embed panel', function() {
   setupComponentTest('challenge-statement-embed-panel', {
@@ -9,16 +11,20 @@ describe('Integration | Component | challenge statement embed panel', function()
   });
 
   it('renders', function() {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.on('myAction', function(val) { ... });
-    // Template block usage:
-    // this.render(hbs`
-    //   {{#challenge-statement-embed-panel}}
-    //     template content
-    //   {{/challenge-statement-embed-panel}}
-    // `);
-
     this.render(hbs`{{challenge-statement-embed-panel}}`);
     expect(this.$()).to.have.length(1);
+  });
+
+  it('should reload iframe when user clicked on button reload', async function() {
+    // given
+    const stubReloadIframe = sinon.stub();
+    this.set('stubReloadIframe', stubReloadIframe);
+    this.render(hbs`{{challenge-statement-embed-panel _reloadIframe=stubReloadIframe}}`);
+
+    // when
+    run(() => document.querySelector('.challenge-statement-embed-panel__reload-button').click());
+
+    // then
+    sinon.assert.calledOnce(stubReloadIframe);
   });
 });
