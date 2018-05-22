a7a8e985cd0c2a58fc14a68067ec1da843b984f7
MelanieMEB <mboudard@octo.com>
2018-05-22 14:02:22 +0200

    [PF-57] Add authentication in campaigns routes

--- live/app/routes/campaigns/create-assessment.js
+++ live/app/routes/campaigns/create-assessment.js
@@ -1,6 +1,10 @@
+import { inject as service } from '@ember/service';
 import BaseRoute from 'pix-live/routes/base-route';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default BaseRoute.extend({
+export default BaseRoute.extend(AuthenticatedRouteMixin, {
+  authenticationRoute: '/connexion',
+  session: service(),
 
   redirect() {
     const store = this.get('store');
