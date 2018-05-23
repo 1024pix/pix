77eafe25a3203edd662f9eb058698c55afaf4098
Antoine Boileau <apem.boileau@gmail.com>
2018-05-23 16:26:59 +0200

    [pf-5] Add entire row as clickable zone, and modify a bit the appearance of detail page

--- live/tests/integration/components/certifications-list-item-test.js
+++ live/tests/integration/components/certifications-list-item-test.js
@@ -153,8 +153,8 @@ describe('Integration | Component | certifications list item', function() {
     });
 
     // then
-    it('should render a certifications-list-item__published-item div', function() {
-      expect(this.$('.certifications-list-item__published-item')).to.have.lengthOf(1);
+    it('should render certifications-list-item__published-item with a link inside', function() {
+      expect(this.$('.certifications-list-item__published-item a')).to.have.lengthOf(1);
     });
 
     it('should show Certification obtenue', function() {
