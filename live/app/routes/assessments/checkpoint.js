c2ab2c9fd1096511a1eb69be3001827edbc505ae
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2018-05-23 11:40:55 +0200

    [PF-59] Displaying the checkpoint page after 5 questions on a SMART_PLACEMENT test

--- live/app/routes/assessments/checkpoint.js
+++ live/app/routes/assessments/checkpoint.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+  actions: {
+    resumeAssessment(assessment) {
+      this.transitionTo('assessments.resume', assessment.get('id'));
+    }
+  }
 });
