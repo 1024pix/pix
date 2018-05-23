fc72a1465958c83edde54a657a2fd19948ce40f8
Quentin Cattez <qcattez@octo.com>
2018-05-23 17:55:36 +0200

    [pi-26] Refactor challenge-repository methods to use new Airtable client methods

--- api/lib/infrastructure/repositories/challenge-repository.js
+++ api/lib/infrastructure/repositories/challenge-repository.js
@@ -1,4 +1,3 @@
-const cache = require('../caches/cache');
 const airtable = require('../airtable');
 const serializer = require('../serializers/airtable/challenge-serializer');
 const Challenge = require('../../domain/models/Challenge');
@@ -11,27 +10,6 @@ const { NotFoundError } = require('../../domain/errors');
 
 const AIRTABLE_TABLE_NAME = 'Epreuves';
 
-function _fetchChallenge(id, cacheKey, resolve, reject) {
-  // TODO: use the ChallengeDataSource and not directly airtable
-  airtable.getRecord(AIRTABLE_TABLE_NAME, id, serializer)
-    .then(challenge => {
-      cache.set(cacheKey, challenge);
-      return resolve(challenge);
-    })
-    .catch(reject);
-}
-
-function _fetchChallenges(cacheKey, resolve, reject, filterFunction) {
-  // TODO: use the ChallengeDataSource and not directly airtable
-  airtable.getRecords(AIRTABLE_TABLE_NAME, {}, serializer)
-    .then(challenges => {
-      const filteredChallenges = challenges.filter(filterFunction);
-      cache.set(cacheKey, filteredChallenges);
-      return resolve(filteredChallenges);
-    })
-    .catch(reject);
-}
-
 function _getSkillDataObjects(challengeDataObject) {
   const skillDataObjectPromises = challengeDataObject.skillIds.map(skillDatasource.get);
   return Promise.all(skillDataObjectPromises);
@@ -40,30 +18,13 @@ function _getSkillDataObjects(challengeDataObject) {
 module.exports = {
 
   list() {
-    return new Promise((resolve, reject) => {
-      const cacheKey = 'challenge-repository_list';
-      cache.get(cacheKey, (err, cachedValue) => {
-        if (err) return reject(err);
-        if (cachedValue) return resolve(cachedValue);
-        return _fetchChallenges(cacheKey, resolve, reject, () => true);
-      });
-    });
+    return airtable.findRecords(AIRTABLE_TABLE_NAME, {})
+      .then(challenges => challenges.map(serializer.deserialize));
   },
 
   findByCompetence(competence) {
-    const cacheKey = `challenge-repository_find_by_competence_${competence.id}`;
-    const cachedChallenges = cache.get(cacheKey);
-
-    if (cachedChallenges) {
-      return Promise.resolve(cachedChallenges);
-    }
-
-    // TODO: use the ChallengeDataSource and not directly airtable
-    return airtable.getRecords(AIRTABLE_TABLE_NAME, { view: competence.reference }, serializer)
-      .then(fetchedChallenges => {
-        cache.set(cacheKey, fetchedChallenges);
-        return Promise.resolve(fetchedChallenges);
-      });
+    return airtable.findRecords(AIRTABLE_TABLE_NAME, { view: competence.reference })
+      .then(challenges => challenges.map(serializer.deserialize));
   },
 
   findBySkills(skills) {
@@ -87,16 +48,13 @@ module.exports = {
       });
   },
 
-  // TODO: Bascule vers le nouveau getRecord
   get(id) {
-
     let challengeDataObject;
 
     return challengeDatasource.get(id)
       .then((result) => challengeDataObject = result)
       .then(_getSkillDataObjects)
       .then((skillDataObjects) => {
-
         const skills = skillDataObjects.map((skillDataObject) => new Skill(skillDataObject));
 
         return new Challenge({
@@ -121,23 +79,5 @@ module.exports = {
 
         throw error;
       });
-  },
-
-  // TODO: delete
-  /**
-   * @deprecated use the generic refresh endpoint
-   */
-  refresh(id) {
-    return new Promise((resolve, reject) => {
-      const cacheKey = `challenge-repository_get_${id}`;
-      cache.del(cacheKey, (err) => {
-        if (err) return reject(err);
-        const cacheSolutionKey = `solution_${id}`;
-        cache.del(cacheSolutionKey, (err) => {
-          if (err) return reject(err);
-          return _fetchChallenge(id, cacheKey, resolve, reject);
-        });
-      });
-    });
   }
 };
