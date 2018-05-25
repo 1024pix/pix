6bff32f5a4b76d94898d7bcf2ae05ffaaa012047
Antoine Boileau <apem.boileau@gmail.com>
2018-05-25 16:24:19 +0200

    [pf-170] BSR on CertificationMark

--- api/lib/domain/models/CompetenceMark.js
+++ api/lib/domain/models/CompetenceMark.js
@@ -7,17 +7,29 @@ const schemaValidateCompetenceMark = Joi.object().keys({
   score: Joi.number().integer().min(0).max(64).required(),
   area_code: Joi.required(),
   competence_code: Joi.required(),
-  assessmentResultId:  Joi.number().optional()
+  assessmentResultId: Joi.number().optional(),
 });
 
 class CompetenceMark {
-  constructor(model) {
-    Object.assign(this, model);
+  constructor({
+    id,
+    level,
+    score,
+    area_code,
+    competence_code,
+    assessmentResultId,
+  } = {}) {
+    this.id = id;
+    this.level = level;
+    this.score = score;
+    this.area_code = area_code;
+    this.competence_code = competence_code;
+    this.assessmentResultId = assessmentResultId;
   }
 
   validate() {
     const result = Joi.validate(this, schemaValidateCompetenceMark);
-    if(result.error === null) {
+    if (result.error === null) {
       return Promise.resolve();
     } else {
       return Promise.reject(new ObjectValidationError(result.error));
