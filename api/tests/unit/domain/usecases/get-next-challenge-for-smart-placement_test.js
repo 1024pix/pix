a576be303f8f9ec534ca004311f8bb5976c16116
MelanieMEB <mboudard@octo.com>
2018-05-29 16:05:28 +0200

    [PF-91] Add tests on usercase get-next-challenge-for-smart-placement

---
+++ api/tests/unit/domain/usecases/get-next-challenge-for-smart-placement_test.js
@@ -0,0 +1,45 @@
+const { expect, sinon } = require('../../../test-helper');
+
+const getNextChallengeForSmartRandom = require('../../../../lib/domain/usecases/get-next-challenge-for-smart-placement');
+const Assessment = require('../../../../lib/domain/models/Assessment');
+const Challenge = require('../../../../lib/domain/models/Challenge');
+const Skill = require('../../../../lib/domain/models/Skill');
+
+describe('Unit | Domain | Use Cases | get-next-challenge-for-smart-placement', () => {
+
+  describe('#getNextChallengeForSmartRandom', () => {
+
+    let answerRepository;
+    let challengeRepository;
+
+    const skill = new Skill({ name: '@unite2' });
+    const challenge = new Challenge({ status: 'validé', id: 'challenge_ID', skills: [skill] });
+    const assessment = new Assessment({ id: 'assessment_ID' });
+
+    beforeEach(() => {
+      answerRepository = { findByAssessment: sinon.stub().resolves([]) };
+      challengeRepository = { findBySkills: sinon.stub().resolves([challenge]) };
+    });
+
+    it('should find answers of the smart placement assessment', () => {
+      // when
+      const promise = getNextChallengeForSmartRandom({ assessment, answerRepository, challengeRepository });
+
+      // then
+      return promise.then(() => {
+        expect(answerRepository.findByAssessment).to.have.been.calledWith('assessment_ID');
+      });
+    });
+
+    it('should return the next Challenge', () => {
+      // when
+      const promise = getNextChallengeForSmartRandom({ assessment, answerRepository, challengeRepository });
+
+      // then
+      return promise.then((challenge) => {
+        expect(challenge).to.equal('challenge_ID');
+      });
+    });
+  });
+
+});
