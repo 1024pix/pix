47756c2aa410c3ee61d350c35a0131203003a127
Antoine Boileau <apem.boileau@gmail.com>
2018-05-30 16:26:31 +0200

    [pf-189] BSR on Session

--- api/lib/domain/models/Session.js
+++ api/lib/domain/models/Session.js
@@ -1,15 +1,26 @@
 class Session {
 
-  constructor({ id, certificationCenter, address, room, examiner, date, time, description, accessCode, certifications } = {}) {
+  constructor({
+    id,
+    accessCode,
+    address,
+    certificationCenter,
+    certifications,
+    date,
+    description,
+    examiner,
+    room,
+    time,
+  } = {}) {
     this.id = id;
-    this.certificationCenter = certificationCenter;
-    this.address = address;
-    this.room = room;
-    this.examiner = examiner;
-    this.date = date;
-    this.time = time;
-    this.description = description;
     this.accessCode = accessCode;
+    this.address = address;
+    this.certificationCenter = certificationCenter;
+    this.date = date;
+    this.description = description;
+    this.examiner = examiner;
+    this.room = room;
+    this.time = time;
 
     this.certifications = certifications;
   }
