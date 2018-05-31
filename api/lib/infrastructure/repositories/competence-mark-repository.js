2b05c1b7c3968fa0581994477b7a369e49ff09cb
David Riviere <driviere@octo.com>
2018-05-31 15:32:10 +0200

    [PF-11] Add findByAssessmentResultId in competenceMarks Repo

--- api/lib/infrastructure/repositories/competence-mark-repository.js
+++ api/lib/infrastructure/repositories/competence-mark-repository.js
@@ -1,9 +1,22 @@
 const BookshelfCompetenceMark = require('../data/competence-mark');
+const CompetenceMark = require('../../domain/models/CompetenceMark');
+
+function _toDomain(bookshelfCompetenceMark) {
+  return new CompetenceMark(bookshelfCompetenceMark.attributes);
+}
 
 module.exports = {
   save: (competenceMark) => {
     return competenceMark.validate()
       .then(() => new BookshelfCompetenceMark(competenceMark).save())
       .then(savedCompetenceMark => savedCompetenceMark.toDomainEntity());
-  }
+  },
+
+  findByAssessmentResultId(assessmentResultId) {
+    return BookshelfCompetenceMark
+      .where({ assessmentResultId })
+      .fetchAll()
+      .then(challenges => challenges.models.map(_toDomain));
+  },
+
 };
