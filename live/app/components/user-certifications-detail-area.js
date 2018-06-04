82f703c27c86a69e62059816fe6fe4ff81771b4d
MelanieMEB <mboudard@octo.com>
2018-06-04 16:40:46 +0200

    [PF-11] Rename file of components + display simple info from certifiedProfile

--- live/app/components/user-certifications-detail-area.js
+++ live/app/components/user-certifications-detail-area.js
@@ -1,6 +1,6 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['user-certifications-detail-list-competences'],
+  classNames: ['user-certifications-detail-area'],
   certification: null,
 });
