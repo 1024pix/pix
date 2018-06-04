236eecf253ce779ca198fcdc5474b2ad7d1fdbfc
MelanieMEB <mboudard@octo.com>
2018-06-04 16:29:12 +0200

    [PF-91] Add method organizeCompetences to propose the list of competencesMarks organized by area and competence

--- api/lib/domain/models/CertifiedProfile.js
+++ api/lib/domain/models/CertifiedProfile.js
@@ -1,3 +1,15 @@
+const _ = require('lodash');
+
+function _insertAreaAndCompetence(table, area, competence) {
+  const areaFind = _.find(table, area);
+  if(areaFind) {
+    areaFind.competences.push(competence);
+  } else {
+    area.competences.push(competence);
+    table.push(area);
+  }
+  return table;
+}
 
 function _addMarksOnCompetences(allCompetences, competencesEvaluated) {
   return allCompetences.map(competence => {
@@ -18,8 +30,30 @@ class CertifiedProfile {
   constructor({
     allCompetences = [],
     competencesEvaluated = [],
+    id = 0,
   } = {}) {
+    this.id = id;
     this.competencesWithMark = _addMarksOnCompetences(allCompetences, competencesEvaluated);
+  }
+
+  organizeCompetences() {
+    let organizedCompetences = [];
+    _(this.competencesWithMark).sortBy((competence) => competence.competenceIndex)
+      .forEach((competence) => {
+        const area = {
+          areaName: competence.areaName,
+          areaIndex: competence.areaIndex,
+          competences: [],
+        };
+        const competenceInArea = {
+          competenceName: competence.competenceName,
+          competenceIndex: competence.competenceIndex,
+          level: competence.level,
+        };
+        organizedCompetences = _insertAreaAndCompetence(organizedCompetences, area, competenceInArea);
+      });
+
+    return organizedCompetences;
   }
 }
 
