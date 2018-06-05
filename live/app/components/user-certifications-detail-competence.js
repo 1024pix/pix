326e75ba777d00fec73a58ff7f9945890b29767f
MelanieMEB <mboudard@octo.com>
2018-06-05 11:22:13 +0200

    [PF-11] Add css files + add display of bar level + add ember-truth-helpers in package.json

--- live/app/components/user-certifications-detail-competence.js
+++ live/app/components/user-certifications-detail-competence.js
@@ -1,6 +1,8 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['user-certifications-detail-component'],
-  certification: null,
+  classNames: ['user-certifications-detail-competence'],
+  competence: null,
+  levels: [1,2,3,4,5,6,7,8],
+
 });
