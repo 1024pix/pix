826725aaf2e6fb04360f43a82cfdc5562a0dc06c
Thomas Wickham <mackwic@gmail.com>
2018-06-05 10:56:32 +0200

    [pf-222]  Add review apps to Jira issues

---
+++ scripts/jira/comment_with_review_app_url.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -x
+
+function extract_issue_id_from_branch_name() {
+    USER_STORY_BRANCH_NAME=$1
+    echo $USER_STORY_BRANCH_NAME | grep --perl-regexp --only-matching '^\w+-\d+' | tr '[:lower:]' '[:upper:]'
+}
+
+API_VERSION="2"
+API_URL="https://1024pix.atlassian.net/rest/api/$API_VERSION"
+
+CREDENTIALS="$JIRA_API_KEY:$JIRA_API_SECRET"
+ISSUE_ID=$(extract_issue_id_from_branch_name $CIRCLE_BRANCH)
+
+RESPONSE=$(curl "$API_URL/issue/$ISSUE_ID/comment" --user $CREDENTIALS --fail)
+
+if [[ $? != 0 ]]
+then
+    echo "The id of the Jira issue could not be found. Stopping here."
+    exit 0
+fi
+
+ISSUE_COMMENTS=$(echo $RESPONSE | jq .comments[].body)
+REVIEW_APP_URL="http://$CIRCLE_BRANCH.integration.pix.fr"
+if [[ $ISSUE_COMMENTS =~ .*$REVIEW_APP_URL.* ]]
+then
+    echo "Review app url already found in issue comments. No need to add it again"
+else
+    TEXT="J'ai déployé la Review App. Elle est consultable sur $REVIEW_APP_URL"
+    curl -X POST "$API_URL/issue/$ISSUE_ID/comment" --header 'Content-Type: application/json' --fail --user $CREDENTIALS --data '{"body": "'"$TEXT"'"}'
+fi
