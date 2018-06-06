d5d24ef924be93ab601daca9144cf6187f94ff1e
Thomas Wickham <mackwic@gmail.com>
2018-06-06 16:58:16 +0200

    [pf-72] Fix issue with assessment with checkpoints
    We should reset the current step after a checkpoint, so that the current
    step is the number of answers modulo 5.

--- live/app/models/assessment.js
+++ live/app/models/assessment.js
@@ -5,52 +5,62 @@ import ENV from 'pix-live/config/environment';
 
 const { attr, Model, belongsTo, hasMany } = DS;
 
+const CHECKPOINTS_MAX_STEP = ENV.APP.NUMBER_OF_CHALLENGE_BETWEEN_TWO_CHECKPOINTS_IN_SMART_PLACEMENT;
+
 export default Model.extend({
 
-  course: belongsTo('course', { inverse: null }),
   answers: hasMany('answer'),
+  course: belongsTo('course', { inverse: null }),
+  certificationNumber: attr('string'),
+  estimatedLevel: attr('number'),
+  firstChallenge: alias('course.challenges.firstObject'),
+  hasCheckpoints: equal('type', 'SMART_PLACEMENT'),
+  isCertification: equal('type', 'CERTIFICATION'),
+  pixScore: attr('number'),
+  result: belongsTo('assessment-result'),
+  type: attr('string'),
   userName: attr('string'),
   userEmail: attr('string'),
-  firstChallenge: alias('course.challenges.firstObject'),
-  estimatedLevel: attr('number'),
-  pixScore: attr('number'),
-  type: attr('string'),
-  certificationNumber: attr('string'),
-  isCertification: equal('type', 'CERTIFICATION'),
 
-  hasCheckpoints: equal('type', 'SMART_PLACEMENT'),
   answersSinceLastCheckpoints: computed('answers.[]', function() {
 
-    const howManyAnswersSinceTheLastCheckpoint = this.get('answers.length') % ENV.APP.NUMBER_OF_CHALLENGE_BETWEEN_TWO_CHECKPOINTS_IN_SMART_PLACEMENT;
+    const howManyAnswersSinceTheLastCheckpoint = this.get('howManyAnswersSinceTheLastCheckpoint');
     const sliceAnswersFrom = (howManyAnswersSinceTheLastCheckpoint === 0)
-      ? -ENV.APP.NUMBER_OF_CHALLENGE_BETWEEN_TWO_CHECKPOINTS_IN_SMART_PLACEMENT
+      ? -CHECKPOINTS_MAX_STEP
       : -howManyAnswersSinceTheLastCheckpoint;
 
     return this.get('answers').slice(sliceAnswersFrom);
   }),
 
-  result: belongsTo('assessment-result'),
+  howManyAnswersSinceTheLastCheckpoint: computed('answers.[]', function() {
+    return this.get('answers.length') % CHECKPOINTS_MAX_STEP;
+  }),
 
   progress: computed('answers', 'course', function() {
 
     let maxStep;
-    if (this.get('type') === 'SMART_PLACEMENT') {
-      maxStep = 5;
+    if (this.get('hasCheckpoints')) {
+      maxStep = CHECKPOINTS_MAX_STEP;
     } else {
       maxStep = this.get('course.nbChallenges');
     }
 
     const answersCount = this.get('answers.length');
-    const currentStep = _getCurrentStep(answersCount, maxStep);
+    const currentStep = this._getCurrentStep(answersCount, maxStep);
     const stepPercentage = currentStep / maxStep * 100;
 
     return { currentStep, maxStep, stepPercentage };
   }),
 
+  _getCurrentStep(answersCount = 0, maxStep) {
+    const step = answersCount + 1;
+
+    if (this.get('hasCheckpoints')) {
+      return 1 + this.get('howManyAnswersSinceTheLastCheckpoint');
+    } else if (maxStep == null) {
+      return step;
+    } else {
+      return Math.min(step, maxStep);
+    }
+  }
 });
-
-function _getCurrentStep(answersCount, maxStep) {
-  const step = answersCount + 1;
-
-  return (step > maxStep) ? maxStep : step;
-}
