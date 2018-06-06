bec3611c3d8651546504b98a849661185f8ab42c
Jérémy Buget <jbuget@gmail.com>
2018-06-06 23:29:16 +0200

    [pf-226] Cache endpoints return 204 (to follow HTTP specs) or a JSON API error (to make the API consistent)

--- api/lib/application/cache/cache-controller.js
+++ api/lib/application/cache/cache-controller.js
@@ -1,24 +1,25 @@
 const cache = require('../../infrastructure/caches/cache');
 const usecases = require('../../domain/usecases');
+const errorSerializer = require('../../infrastructure/serializers/jsonapi/error-serializer');
+const { InfrastructureError } = require('../../infrastructure/errors');
+
+function _buildJsonApiInternalServerError(error) {
+  const internalError = new InfrastructureError(error.message);
+  return errorSerializer.serialize(internalError);
+}
 
 module.exports = {
 
   removeCacheEntry(request, reply) {
     const cacheKey = request.params.cachekey;
-
     return usecases.removeCacheEntry({ cacheKey, cache })
-      .then((numberOfDeletedKeys) => {
-        if(numberOfDeletedKeys === 0) {
-          return reply('Entry not found').code(404);
-        }
-        return reply('Entry successfully deleted').code(200);
-      })
-      .catch((error) => reply(error).code(500));
+      .then(() => reply().code(204))
+      .catch((error) => reply(_buildJsonApiInternalServerError(error)).code(500));
   },
 
   removeAllCacheEntries(request, reply) {
     return usecases.removeAllCacheEntries({ cache })
-      .then(() => reply('Entries successfully deleted').code(200))
-      .catch((error) => reply(error).code(500));
+      .then(() => reply().code(204))
+      .catch((error) => reply(_buildJsonApiInternalServerError(error)).code(500));
   }
 };
--- api/lib/application/cache/index.js
+++ api/lib/application/cache/index.js
@@ -13,9 +13,9 @@ exports.register = function(server, options, next) {
           assign: 'hasRolePixMaster'
         }],
         handler: CacheController.removeCacheEntry,
-        tags: ['api'],
+        tags: ['api', 'cache'],
         notes: [
-          'Cette route est restreinte aux utilisateurs authentifiés',
+          'Cette route est restreinte aux utilisateurs authentifiés avec le rôle Pix Master',
           'Elle permet de supprimer une entrée du cache de l’application\n' +
           'Attention : pour un état cohérent des objets stockés en cache, utiliser DELETE /api/cache'
         ]
@@ -29,10 +29,10 @@ exports.register = function(server, options, next) {
           assign: 'hasRolePixMaster'
         }],
         handler: CacheController.removeAllCacheEntries,
-        tags: ['api'],
+        tags: ['api', 'cache'],
         notes: [
-          'Cette route est restreinte aux utilisateurs authentifiés',
-          'Elle permet de supprimer toutes les entrées du cache de l’application\n',
+          'Cette route est restreinte aux utilisateurs authentifiés avec le rôle Pix Master',
+          'Elle permet de supprimer toutes les entrées du cache de l’application',
         ]
       }
     }
