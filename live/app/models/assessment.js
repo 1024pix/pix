843f8ce9e0e9d3b5055d7c021fefffc008ee9c22
Thomas Wickham <mackwic@gmail.com>
2018-06-08 14:29:40 +0200

    [pf-72] Refacto: specialize front assessment with a behavior

--- live/app/models/assessment.js
+++ live/app/models/assessment.js
@@ -1,11 +1,10 @@
 import { alias, equal } from '@ember/object/computed';
 import { computed } from '@ember/object';
 import DS from 'ember-data';
-import ENV from 'pix-live/config/environment';
 
 const { attr, Model, belongsTo, hasMany } = DS;
 
-const CHECKPOINTS_MAX_STEP = ENV.APP.NUMBER_OF_CHALLENGE_BETWEEN_TWO_CHECKPOINTS_IN_SMART_PLACEMENT;
+import { getProgressionBehaviorFromAssessmentType } from 'pix-live/models/behaviors/assessment-progression';
 
 export default Model.extend({
 
@@ -22,45 +21,19 @@ export default Model.extend({
   userName: attr('string'),
   userEmail: attr('string'),
 
+  ready() {
+    this.progressionBehavior = getProgressionBehaviorFromAssessmentType(this.get('type'));
+  },
+
   answersSinceLastCheckpoints: computed('answers.[]', function() {
-
-    const howManyAnswersSinceTheLastCheckpoint = this.get('howManyAnswersSinceTheLastCheckpoint');
-    const sliceAnswersFrom = (howManyAnswersSinceTheLastCheckpoint === 0)
-      ? -CHECKPOINTS_MAX_STEP
-      : -howManyAnswersSinceTheLastCheckpoint;
-
-    return this.get('answers').slice(sliceAnswersFrom);
-  }),
-
-  howManyAnswersSinceTheLastCheckpoint: computed('answers.[]', function() {
-    return this.get('answers.length') % CHECKPOINTS_MAX_STEP;
+    const answers = this.get('answers').toArray();
+    return this.progressionBehavior.answersSinceLastCheckpoints(answers);
   }),
 
   progress: computed('answers', 'course', function() {
-
-    let maxStep;
-    if (this.get('hasCheckpoints')) {
-      maxStep = CHECKPOINTS_MAX_STEP;
-    } else {
-      maxStep = this.get('course.nbChallenges');
-    }
-
-    const answersCount = this.get('answers.length');
-    const currentStep = this._getCurrentStep(answersCount, maxStep);
-    const stepPercentage = currentStep / maxStep * 100;
-
-    return { currentStep, maxStep, stepPercentage };
+    return this.progressionBehavior.progress(
+      this.get('answers.length'),
+      this.get('course.nbChallenges'),
+    );
   }),
-
-  _getCurrentStep(answersCount = 0, maxStep) {
-    const step = answersCount + 1;
-
-    if (this.get('hasCheckpoints')) {
-      return 1 + this.get('howManyAnswersSinceTheLastCheckpoint');
-    } else if (maxStep == null) {
-      return step;
-    } else {
-      return Math.min(step, maxStep);
-    }
-  }
 });
