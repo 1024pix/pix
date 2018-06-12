ce60f5976c55aa9b8f9bc13ebce40e34edf3cb3a
Thomas Wickham <mackwic@gmail.com>
2018-06-12 17:59:51 +0200

    [pf-242] Use attribute data-value to store the number of QCU proposal

--- live/app/components/challenge-item-qroc.js
+++ live/app/components/challenge-item-qroc.js
@@ -6,9 +6,7 @@ const ChallengeItemQroc = ChallengeItemGeneric.extend({
     return this._getAnswerValue().length < 1;
   },
 
-  // XXX : data is extracted from DOM of child component, breaking child encapsulation.
-  // This is not "the Ember way", however it makes code easier to read,
-  // and moreover, it is a much more robust solution when you need to test it properly.
+  // FIXME refactor that
   _getAnswerValue() {
     return this.$('input[data-uid="qroc-proposal-uid"]').val();
   },
