6f0ed91a5f97b3536707b0e44aaa367531758d11
Jérémy Buget <jbuget@gmail.com>
2018-06-13 16:18:06 +0200

    Remove FastBoot

--- app/session-stores/application.js
+++
@@ -1,8 +0,0 @@
-import CookieStore from 'ember-simple-auth/session-stores/cookie';
-import config from '../config/environment';
-
-export default CookieStore.extend({
-
-  cookieDomain: config.authentication.cookieDomain
-
-});
