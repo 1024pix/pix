9eb42e48d89bae084448e645cf7b32fb47148849
Antoine Boileau <apem.boileau@gmail.com>
2018-06-18 19:08:48 +0200

    [pf-178] Fix tests after rebase

--- api/tests/factory/build-area.js
+++ api/tests/factory/build-area.js
@@ -3,7 +3,7 @@ const Area = require('../../lib/domain/models/Area');
 
 module.exports = function buildArea({
   id = faker.random.uuid(),
-  name = '1 Information et données',
+  name = '1. Information et données',
   code = '1',
   title = 'Information et données',
   competences = [],
