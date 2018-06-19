33a0af4ecfcf24f9d4caa17997415921fe370534
Quentin Cattez <qcattez@octo.com>
2018-06-19 14:13:14 +0200

    [pf-94] Add skill-review-controller

--- api/lib/application/skillReviews/skill-review-controller.js
+++ api/lib/application/skillReviews/skill-review-controller.js
@@ -1,5 +1,34 @@
-function get(request, reply) {
-  return reply();
+const assessmentRepository = require('../../infrastructure/repositories/assessment-repository');
+const answerRepository = require('../../infrastructure/repositories/answer-repository');
+const challengeRepository = require('../../infrastructure/repositories/challenge-repository');
+const logger = require('../../infrastructure/logger');
+const skillReviewSerializer = require('../../infrastructure/serializers/jsonapi/skill-review-serializer');
+const usecases = require('../../domain/usecases');
+
+const errorSerializer = require('../../infrastructure/serializers/jsonapi/error-serializer');
+const { InfrastructureError } = require('../../infrastructure/errors');
+const { NotFoundError } = require('../../domain/errors');
+
+function _buildJsonApiInternalServerError(error) {
+  const internalError = new InfrastructureError(error.message);
+  return errorSerializer.serialize(internalError);
 }
 
-module.exports = { get };
+module.exports = {
+
+  get(request, reply) {
+    const skillReviewId = request.params.id;
+    const assessmentId = skillReviewId; // TODO change when several skillReview will come
+
+    return usecases.getSkillReviewFromAssessmentId({ assessmentId, assessmentRepository, answerRepository, challengeRepository })
+      .then(skillReviewSerializer.serialize)
+      .then(serializedSkillReview => reply(serializedSkillReview).code(200))
+      .catch(error => {
+        logger.error(error);
+        if (error instanceof NotFoundError) return reply().code(404);
+
+        reply(_buildJsonApiInternalServerError(error)).code(500);
+      });
+  }
+
+};
--- api/tests/unit/domain/usecases/get-skill-review-from-assessment-id_test.js
+++ api/tests/unit/domain/usecases/get-skill-review-from-assessment-id_test.js
@@ -12,6 +12,7 @@ const Challenge = require('../../../../lib/domain/models/Challenge');
 const Course = require('../../../../lib/domain/models/Course');
 const Skill= require('../../../../lib/domain/models/Skill');
 const SkillReview = require('../../../../lib/domain/models/SkillReview');
+const { NotFoundError } = require('../../../../lib/domain/errors');
 
 describe('Unit | Domain | Use Cases | get-skill-review-from-assessment-id', () => {
 
@@ -55,7 +56,7 @@ describe('Unit | Domain | Use Cases | get-skill-review-from-assessment-id', () 
 
     beforeEach(() => {
       sandbox = sinon.createSandbox();
-      sandbox.stub(assessmentRepository, 'get').resolves(assessment);
+      sandbox.stub(assessmentRepository, 'get');
       sandbox.stub(challengeRepository, 'findBySkills').resolves(challenges);
       sandbox.stub(answerRepository, 'findByAssessment').resolves(answers);
     });
@@ -64,74 +65,97 @@ describe('Unit | Domain | Use Cases | get-skill-review-from-assessment-id', () 
       sandbox.restore();
     });
 
-    it('should use repositories to retrieve relevant informations', () => {
-      // given
-      const expectedSkillReviewSkills = [
-        new Skill({ name: '@accesDonnées2' }),
-        new Skill({ name: '@accesDonnées1' }),
-        new Skill({ name: '@collecteDonnées2' }),
-        new Skill({ name: '@collecteDonnées1' }),
-        new Skill({ name: '@infosPerso4' }),
-        new Skill({ name: '@infosPerso3' }),
-        new Skill({ name: '@infosPerso2' }),
-        new Skill({ name: '@infosPerso1' }),
-        new Skill({ name: '@tracesLocales3' }),
-        new Skill({ name: '@tracesLocales2' }),
-        new Skill({ name: '@tracesLocales1' }),
-        new Skill({ name: '@tracesPratiques6' }),
-        new Skill({ name: '@tracesPratiques5' }),
-        new Skill({ name: '@tracesPratiques4' }),
-        new Skill({ name: '@tracesPratiques3' }),
-        new Skill({ name: '@tracesPratiques2' }),
-        new Skill({ name: '@tracesPratiques1' }),
-        new Skill({ name: '@archive4' }),
-        new Skill({ name: '@archive3' }),
-        new Skill({ name: '@archive2' }),
-        new Skill({ name: '@archive1' }),
-        new Skill({ name: '@fichier1' }),
-        new Skill({ name: '@propFichier3' }),
-        new Skill({ name: '@propFichier2' }),
-        new Skill({ name: '@propFichier1' }),
-        new Skill({ name: '@sauvegarde6' }),
-        new Skill({ name: '@sauvegarde5' }),
-        new Skill({ name: '@sauvegarde4' }),
-        new Skill({ name: '@sauvegarde3' }),
-        new Skill({ name: '@sauvegarde2' }),
-        new Skill({ name: '@sauvegarde1' }),
-        new Skill({ name: '@unite2' }),
-        new Skill({ name: '@unite1' }),
-      ];
+    context('when assessment if found', () => {
 
-      // when
-      const promise = useCase.getSkillReviewFromAssessmentId({
-        assessmentId: assessment.id,
-        assessmentRepository,
-        challengeRepository,
-        answerRepository
+      beforeEach(() => assessmentRepository.get.resolves(assessment));
+
+      it('should use repositories to retrieve relevant informations', () => {
+        // given
+        const expectedSkillReviewSkills = [
+          new Skill({ name: '@accesDonnées2' }),
+          new Skill({ name: '@accesDonnées1' }),
+          new Skill({ name: '@collecteDonnées2' }),
+          new Skill({ name: '@collecteDonnées1' }),
+          new Skill({ name: '@infosPerso4' }),
+          new Skill({ name: '@infosPerso3' }),
+          new Skill({ name: '@infosPerso2' }),
+          new Skill({ name: '@infosPerso1' }),
+          new Skill({ name: '@tracesLocales3' }),
+          new Skill({ name: '@tracesLocales2' }),
+          new Skill({ name: '@tracesLocales1' }),
+          new Skill({ name: '@tracesPratiques6' }),
+          new Skill({ name: '@tracesPratiques5' }),
+          new Skill({ name: '@tracesPratiques4' }),
+          new Skill({ name: '@tracesPratiques3' }),
+          new Skill({ name: '@tracesPratiques2' }),
+          new Skill({ name: '@tracesPratiques1' }),
+          new Skill({ name: '@archive4' }),
+          new Skill({ name: '@archive3' }),
+          new Skill({ name: '@archive2' }),
+          new Skill({ name: '@archive1' }),
+          new Skill({ name: '@fichier1' }),
+          new Skill({ name: '@propFichier3' }),
+          new Skill({ name: '@propFichier2' }),
+          new Skill({ name: '@propFichier1' }),
+          new Skill({ name: '@sauvegarde6' }),
+          new Skill({ name: '@sauvegarde5' }),
+          new Skill({ name: '@sauvegarde4' }),
+          new Skill({ name: '@sauvegarde3' }),
+          new Skill({ name: '@sauvegarde2' }),
+          new Skill({ name: '@sauvegarde1' }),
+          new Skill({ name: '@unite2' }),
+          new Skill({ name: '@unite1' }),
+        ];
+
+        // when
+        const promise = useCase.getSkillReviewFromAssessmentId({
+          assessmentId: assessment.id,
+          assessmentRepository,
+          challengeRepository,
+          answerRepository
+        });
+
+        // then
+        return promise.then(() => {
+          expect(assessmentRepository.get).to.have.been.calledWith(assessment.id);
+          expect(challengeRepository.findBySkills).to.have.been.calledWith(expectedSkillReviewSkills);
+        });
       });
 
-      // then
-      return promise.then(() => {
-        expect(assessmentRepository.get).to.have.been.calledWith(assessment.id);
-        expect(challengeRepository.findBySkills).to.have.been.calledWith(expectedSkillReviewSkills);
+      it('should return a skill review associated to the assessment', () => {
+        // when
+        const promise = useCase.getSkillReviewFromAssessmentId({
+          assessmentId: assessment.id,
+          assessmentRepository,
+          challengeRepository,
+          answerRepository
+        });
+
+        // then
+        return promise.then((skillReview) => {
+          expect(skillReview).to.be.an.instanceOf(SkillReview);
+          expect(skillReview.assessment).to.equal(assessment);
+          expect(skillReview.assessment.answers).to.equal(answers);
+          expect(skillReview.assessment.answers[0].challenge).to.equal(challengeAnswered);
+        });
       });
     });
 
-    it('should return a skill review associated to the assessment', () => {
-      // when
-      const promise = useCase.getSkillReviewFromAssessmentId({
-        assessmentId: assessment.id,
-        assessmentRepository,
-        challengeRepository,
-        answerRepository
-      });
+    context('when assessment does not exist', () => {
 
-      // then
-      return promise.then((skillReview) => {
-        expect(skillReview).to.be.an.instanceOf(SkillReview);
-        expect(skillReview.assessment).to.equal(assessment);
-        expect(skillReview.assessment.answers).to.equal(answers);
-        expect(skillReview.assessment.answers[0].challenge).to.equal(challengeAnswered);
+      beforeEach(() => assessmentRepository.get.resolves());
+
+      it('should throw a Not Found error', () => {
+        // when
+        const promise = useCase.getSkillReviewFromAssessmentId({
+          assessmentId: 'NonExistentId',
+          assessmentRepository,
+          challengeRepository,
+          answerRepository
+        });
+
+        // then
+        return expect(promise).to.be.rejectedWith(NotFoundError);
       });
     });
   });
