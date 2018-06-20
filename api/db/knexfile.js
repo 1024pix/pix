6d294d8b30c67efef8c6ce38dd0796e0db306076
Quentin Cattez <qcattez@octo.com>
2018-06-20 14:46:51 +0200

    add a postgresql database on circleci test job

--- api/db/knexfile.js
+++ api/db/knexfile.js
@@ -62,17 +62,19 @@ module.exports = {
   },
 
   test: {
-    client: 'sqlite3',
-    connection: {
-      filename: `${__dirname}/test.sqlite3`
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 10,
+      max: 100
     },
     migrations: {
-      directory: `${__dirname}/migrations`
+      tableName: 'knex_migrations',
+      directory: './migrations'
     },
     seeds: {
-      directory: `${__dirname}/seeds`
-    },
-    useNullAsDefault: true
-  }
+      directory: './seeds'
+    }
+  },
 
 };
