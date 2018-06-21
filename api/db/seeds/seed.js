2dd88194e8f56ffb10fc7baff095d7fa3be2927c
Quentin Cattez <qcattez@octo.com>
2018-06-21 12:02:40 +0200

    [pf-93] Fix seeds for postgres usage

--- api/db/seeds/seed.js
+++ api/db/seeds/seed.js
@@ -1,42 +1,58 @@
 'use strict';
-const listSeeds = [
-  'assessments',
-  'assessment-results',
-  'answers',
-  'certification-courses',
-  'certification-challenges',
-  'competence-marks',
-  'organizations',
-  'users',
-  'sessions',
-  'snapshots',
-  'users_pix_roles',
-];
+
+const listSeeds = {
+  '1st-to-create': [
+    'sessions',
+    'users',
+  ],
+  '2nd-to-create': [
+    'assessments',
+    'certification-courses',
+    'organizations',
+    'users_pix_roles',
+  ],
+  '3rd-to-create': [
+    'answers',
+    'assessment-results',
+    'certification-challenges',
+    'snapshots',
+  ],
+  '4th-to-create': [
+    'competence-marks',
+  ],
+};
 
 function addData(knex, table, data) {
   return Promise.all(data)
     .then(data => {
       return knex(table)
         .insert(data)
-        .catch((err) => {
-          console.log(err);
-        });
+        .catch(console.log);
     });
 }
 
-function createDataByTables() {
-  return listSeeds.map(tableName => {
+function createDataByTables(groupName) {
+  const directory = groupName;
+  const files = listSeeds[groupName];
+  return files.map(tableName => {
     return {
       table: tableName,
-      data: require('./data/'+tableName+'.js')
+      data: require('./data/'+directory+'/'+tableName+'.js')
     };
   });
 }
 
-exports.seed = (knex) => {
-  const dataByTables = createDataByTables();
+function insertSeedsByGroup(knex, groupName) {
+  const dataByTables = createDataByTables(groupName);
   const dataToAdd = dataByTables.map(dataByTable => {
     return addData(knex, dataByTable.table, dataByTable.data);
   });
   return Promise.all(dataToAdd);
+}
+
+exports.seed = (knex) => {
+  return insertSeedsByGroup(knex, '1st-to-create')
+    .then(() => insertSeedsByGroup(knex, '2nd-to-create'))
+    .then(() => insertSeedsByGroup(knex, '3rd-to-create'))
+    .then(() => insertSeedsByGroup(knex, '4th-to-create'));
 };
