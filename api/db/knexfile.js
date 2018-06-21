91b7584d1aea8f0bf7559498bf6d6720b6e9cfe0
Quentin Cattez <qcattez@octo.com>
2018-06-21 11:58:52 +0200

    [pf-93] Change Dev environment to use PG as database

--- api/db/knexfile.js
+++ api/db/knexfile.js
@@ -1,17 +1,19 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: `${__dirname}/dev.sqlite3`
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 10,
+      max: 100
     },
     migrations: {
+      tableName: 'knex_migrations',
       directory: './migrations'
     },
     seeds: {
       directory: './seeds'
-    },
-    useNullAsDefault: true
+    }
   },
 
   integration: {
