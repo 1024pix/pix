6a27a5d6f8d6c1b53dd21159b4b33800b55547d2
Antoine Boileau <apem.boileau@gmail.com>
2018-06-22 11:07:16 +0200

    [pf-94] Add json api errors to 403 and 404 errors on skill-review controller

--- api/lib/application/skillReviews/skill-review-controller.js
+++ api/lib/application/skillReviews/skill-review-controller.js
@@ -1,14 +1,15 @@
+const JSONAPIError = require('jsonapi-serializer').Error;
+
 const assessmentRepository = require('../../infrastructure/repositories/assessment-repository');
 const answerRepository = require('../../infrastructure/repositories/answer-repository');
 const challengeRepository = require('../../infrastructure/repositories/challenge-repository');
 const logger = require('../../infrastructure/logger');
 const skillReviewSerializer = require('../../infrastructure/serializers/jsonapi/skill-review-serializer');
-const tokenService = require('../../domain/services/token-service');
 const usecases = require('../../domain/usecases');
 
 const errorSerializer = require('../../infrastructure/serializers/jsonapi/error-serializer');
 const { InfrastructureError } = require('../../infrastructure/errors');
-const { NotFoundError, ForbiddenAccess } = require('../../domain/errors');
+const { NotFoundError, UserNotAuthorizedToAccessEntity } = require('../../domain/errors');
 
 function _buildJsonApiInternalServerError(error) {
   const internalError = new InfrastructureError(error.message);
@@ -18,21 +19,41 @@ function _buildJsonApiInternalServerError(error) {
 module.exports = {
 
   get(request, reply) {
-    const token = tokenService.extractTokenFromAuthChain(request.headers.authorization);
-    const userId = tokenService.extractUserId(token);
+    const userId = request.auth.credentials.userId;
 
     const skillReviewId = request.params.id;
 
-    return usecases.getSkillReview({ skillReviewId, userId, assessmentRepository, answerRepository, challengeRepository })
+    return usecases.getSkillReview({
+      skillReviewId,
+      userId,
+      assessmentRepository,
+      answerRepository,
+      challengeRepository,
+    })
       .then(skillReviewSerializer.serialize)
       .then(serializedSkillReview => reply(serializedSkillReview).code(200))
       .catch(error => {
-        if (error instanceof ForbiddenAccess) return reply().code(403);
-        if (error instanceof NotFoundError) return reply().code(404);
+
+        if (error instanceof UserNotAuthorizedToAccessEntity) {
+          const jsonAPIError = new JSONAPIError({
+            code: '403',
+            title: 'Unauthorized Access',
+            detail: 'Vous n’avez pas accès à ce profil d’avancement',
+          });
+          return reply(jsonAPIError).code(403);
+        }
+
+        if (error instanceof NotFoundError) {
+          const jsonApiError = new JSONAPIError({
+            title: 'Not Found',
+            code: '404',
+            detail: `Profil d’avancement introuvable pour l’id ${skillReviewId}`,
+          });
+          return reply(jsonApiError).code(404);
+        }
 
         logger.error(error);
         reply(_buildJsonApiInternalServerError(error)).code(500);
       });
-  }
-
+  },
 };
