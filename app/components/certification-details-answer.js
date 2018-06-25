efc15221d925616d45b7535d616c5a59db6ea56e
Benoit <benoit.prady@pix.fr>
2018-06-25 16:05:45 +0200

    Fixed jury value

--- app/components/certification-details-answer.js
+++ app/components/certification-details-answer.js
@@ -4,15 +4,17 @@ import { computed } from '@ember/object';
 export default Component.extend({
   classNames: ['card', 'text-center', 'certification-details-answer', 'border-secondary'],
   answer: null,
-  resultClass: computed('answer.jury', function() {
-    const answerJury = this.get("answer").jury;
-    return (answerJury)? "answer-result jury" : "answer-result";
+  jury:false,
+  resultClass: computed('jury', function() {
+    const jury = this.get("jury");
+    return (jury)? "answer-result jury" : "answer-result";
   }),
   actions: {
     setResult(value){
-      const answer = this.get("answer");
+      let answer = this.get("answer");
       let jury = (value !== answer.result) ? value : false;
       answer.jury = jury;
+      this.set("jury", jury);
       this.get("updateRate")();
     }
   }
