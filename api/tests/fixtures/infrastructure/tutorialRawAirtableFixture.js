c0858c37372c4ca3784ed2d9b606e244db887951
David Riviere <driviere@octo.com>
2018-06-27 17:52:01 +0200

    [PF-62] Add tutorial datasource, Tutorial object and Airtable tutorial Raw & Fixture

---
+++ api/tests/fixtures/infrastructure/tutorialRawAirtableFixture.js
@@ -0,0 +1,13 @@
+module.exports = function() {
+  return {
+    'id': 'receomyzL0AmpMFGw',
+    'fields': {
+      'Durée': '00:01:30',
+      'Format': 'video',
+      'Lien': 'https://youtube.fr',
+      'Source': 'Youtube',
+      'Titre':'Comment dresser un panda',
+    },
+    'createdTime': '2018-01-31T12:41:07.000Z'
+  };
+};
---
+++ api/tests/unit/infrastructure/datasources/airtable/models/Tutorial_test.js
@@ -0,0 +1,22 @@
+const dataModels = require('../../../../../../lib/infrastructure/datasources/airtable/objects/index');
+const tutorialRawAirTableFixture = require('../../../../../fixtures/infrastructure/tutorialRawAirtableFixture');
+const TutorialAirtableDataModelFixture = require('../../../../../fixtures/infrastructure/tutorialAirtableDataObjectFixture');
+const { expect } = require('../../../../../test-helper');
+
+describe('Unit | Infrastructure | Datasource | Airtable | Model | Tutorial', () => {
+
+  context('#fromAirTableObject', () => {
+
+    it('should create a Tutorial from the AirtableRecord', () => {
+      // given
+      const expectedTuto = TutorialAirtableDataModelFixture();
+
+      // when
+      const tuto = dataModels.Tutorial.fromAirTableObject(tutorialRawAirTableFixture());
+
+      // then
+      expect(tuto).to.be.an.instanceof(dataModels.Tutorial);
+      expect(tuto).to.deep.equal(expectedTuto);
+    });
+  });
+});
