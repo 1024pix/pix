0732f430deb665dcb2e3d0af0472c5c5874dd473
Benoit <benoit.prady@pix.fr>
2018-06-27 18:43:49 +0200

    Enhanced certification display

--- app/controllers/authenticated/certifications/single/info.js
+++ app/controllers/authenticated/certifications/single/info.js
@@ -3,4 +3,14 @@ import { alias } from '@ember/object/computed';
 
 export default Controller.extend({
   certification:alias('model'),
+  edition:false,
+  actions: {
+    edit() {
+      this.set("edition", true);
+    },
+    cancel() {
+      this.set("edition", false);
+      this.get("certification").rollbackAttributes();
+    }
+  }
 });
