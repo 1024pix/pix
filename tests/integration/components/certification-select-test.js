cdf2e2becc99510219314548988a5f9e3f75d20d
Benoit <benoit.prady@pix.fr>
2018-06-27 10:39:24 +0200

    Updated tests for component certification-select

--- tests/integration/components/certification-select-test.js
+++ tests/integration/components/certification-select-test.js
@@ -1,7 +1,8 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { click, fillIn, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { resolve } from 'rsvp';
 
 module('Integration | Component | certification-select', function(hooks) {
   setupRenderingTest(hooks);
@@ -10,4 +11,22 @@ module('Integration | Component | certification-select', function(hooks) {
     await render(hbs`{{certification-select}}`);
     assert.dom('form.certifications-search').exists();
   });
+
+  test('provides correct id', async function(assert) {
+    // given
+    this.set("externalAction", (value) => {
+      this.set('certificationId', value);
+      return resolve();
+    });
+
+    // when
+    await render(hbs`{{certification-select id=null select=(action externalAction)}}`);
+    await fillIn('.certifications-search__control', '1234');
+    await click('.certifications-search__button');
+
+    // then
+    assert.equal(this.get('certificationId'), '1234');
+  });
+
+
 });
