6da0d15053300341e5c9f1b3c1dd7d0f0237c5f6
Benoit <benoit.prady@pix.fr>
2018-06-27 18:43:49 +0200

    Enhanced certification display

---
+++ app/components/certification-info-field.js
@@ -0,0 +1,22 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  large:false,
+  leftWidth:computed('large', function() {
+    let large = this.get('large');
+    if (large) {
+      return "col-sm-3";
+    } else {
+      return "col-sm-5";
+    }
+  }),
+  rightWidth:computed('large', function() {
+    let large = this.get('large');
+    if (large) {
+      return "col-sm-9";
+    } else {
+      return "col-sm-7";
+    }
+  })
+});
