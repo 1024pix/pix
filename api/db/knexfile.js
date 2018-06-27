9bddf5fefde85a14ad00320a2191cee0c979f406
Thomas Wickham <mackwic@gmail.com>
2018-06-27 17:03:10 +0200

    [pf-259] Reset knexfile from dev

--- api/db/knexfile.js
+++ api/db/knexfile.js
@@ -1,55 +1,79 @@
+module.exports = {
 
-const commonPgProperties = {
-  client: 'postgresql',
-  connection: process.env.DATABASE_URL,
-  pool: {
-    min: 1,
-    max: (parseInt(process.env.DATABASE_CONNECTION_POOL_MAX_SIZE, '10') || 4),
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: `${__dirname}/dev.sqlite3`
+    },
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    },
+    useNullAsDefault: true
   },
-  migrations: {
-    tableName: 'knex_migrations',
-    directory: './migrations'
+
+  integration: {
+    client: 'sqlite3',
+    connection: {
+      filename: `${__dirname}/integration.sqlite3`
+    },
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    },
+    useNullAsDefault: true
   },
-  seeds: {
-    directory: './seeds'
+
+  staging: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 10,
+      max: 100
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
   },
-  ssl: ('true' === process.env.DATABASE_SSL_ENABLED)
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 1,
+      max: (parseInt(process.env.DATABASE_CONNECTION_POOL_MAX_SIZE, '10') || 4),
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    },
+    ssl: ('true' === process.env.DATABASE_SSL_ENABLED)
+  },
+
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: `${__dirname}/test.sqlite3`
+    },
+    migrations: {
+      directory: `${__dirname}/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/seeds`
+    },
+    useNullAsDefault: true
+  }
+
 };
 
-const commonSqliteProperties = {
-  client: 'sqlite3',
-  migrations: {
-    directory: './migrations'
-  },
-  seeds: {
-    directory: './seeds'
-  },
-  useNullAsDefault: true
-};
-
-const config = {
-
-  development: Object.assign({}, commonPgProperties),
-
-  integration: Object.assign({}, commonSqliteProperties, {
-    connection: { filename: `${__dirname}/integration.sqlite3` },
-  }),
-
-  staging: Object.assign({}, commonPgProperties),
-
-  production: Object.assign({}, commonPgProperties),
-
-  test_with_sqlite: Object.assign({}, commonSqliteProperties, {
-    connection: { filename: `${__dirname}/test.sqlite3` }
-  }),
-
-  test_with_pg: Object.assign({}, commonPgProperties),
-};
-
-if (process.env.USE_PG) {
-  config.test = config.test_with_pg;
-} else {
-  config.test = config.test_with_sqlite;
-}
-
-module.exports = config;
