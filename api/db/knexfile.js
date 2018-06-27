a12f5806e05d6534f340f45ea0a1f71680e7f73c
Thomas Wickham <mackwic@gmail.com>
2018-06-27 15:02:06 +0200

    [pf-259] Factorize knexfile config

--- api/db/knexfile.js
+++ api/db/knexfile.js
@@ -1,82 +1,47 @@
+
+const commonPgProperties = {
+  client: 'postgresql',
+  connection: process.env.DATABASE_URL,
+  pool: {
+    min: 1,
+    max: (parseInt(process.env.DATABASE_CONNECTION_POOL_MAX_SIZE, '10') || 4),
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: './migrations'
+  },
+  seeds: {
+    directory: './seeds'
+  },
+  ssl: ('true' === process.env.DATABASE_SSL_ENABLED)
+};
+
+const commonSqliteProperties = {
+  client: 'sqlite3',
+  migrations: {
+    directory: './migrations'
+  },
+  seeds: {
+    directory: './seeds'
+  },
+  useNullAsDefault: true
+};
+
 module.exports = {
 
-  development: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 10,
-      max: 100
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: './migrations'
-    },
-    seeds: {
-      directory: './seeds'
-    }
-  },
+  development: Object.assign({}, commonPgProperties),
 
-  integration: {
-    client: 'sqlite3',
-    connection: {
-      filename: `${__dirname}/integration.sqlite3`
-    },
-    migrations: {
-      directory: './migrations'
-    },
-    seeds: {
-      directory: './seeds'
-    },
-    useNullAsDefault: true
-  },
+  integration: Object.assign({}, commonSqliteProperties, {
+    connection: { filename: `${__dirname}/integration.sqlite3` },
+  }),
 
-  staging: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 10,
-      max: 100
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: './migrations'
-    },
-    seeds: {
-      directory: './seeds'
-    }
-  },
+  staging: Object.assign({}, commonPgProperties),
 
-  production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 1,
-      max: (parseInt(process.env.DATABASE_CONNECTION_POOL_MAX_SIZE, '10') || 4),
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: './migrations'
-    },
-    seeds: {
-      directory: './seeds'
-    },
-    ssl: ('true' === process.env.DATABASE_SSL_ENABLED)
-  },
+  production: Object.assign({}, commonPgProperties),
 
-  test: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 10,
-      max: 100
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: './migrations'
-    },
-    seeds: {
-      directory: './seeds'
-    }
-  },
+  test: Object.assign({}, commonSqliteProperties, {
+    connection: { filename: `${__dirname}/test.sqlite3` }
+  }),
 
+  test_with_pg: Object.assign({}, commonPgProperties),
 };
