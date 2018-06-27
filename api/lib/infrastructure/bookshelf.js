22249ba56eabda4aa06621573299f3c4fef2c74b
Thomas Wickham <mackwic@gmail.com>
2018-06-27 15:02:42 +0200

    [pf-259] Override Bookshelf Model.count method to always return an int

--- api/lib/infrastructure/bookshelf.js
+++ api/lib/infrastructure/bookshelf.js
@@ -1,9 +1,11 @@
 const knexConfig = require('../../db/knexfile');
 const settings = require('./../settings');
-const knex = require('knex')(knexConfig[settings.environment]);
+const logger = require('./logger');
 const validator = require('validator');
-
 const _ = require('lodash');
+
+const knex = require('knex')(knexConfig[settings.environment]);
+const bookshelf = require('bookshelf')(knex);
 
 validator.isRequired = function(value) {
   return !_.isNil(value);
@@ -13,7 +15,6 @@ validator.isTrue = function(value) {
   return _.isBoolean(value) && value === true;
 };
 
-const bookshelf = require('bookshelf')(knex);
 
 bookshelf.plugin('bookshelf-validate', {
   validateOnSave: true,
@@ -21,5 +22,25 @@ bookshelf.plugin('bookshelf-validate', {
 });
 
 bookshelf.plugin('registry');
+
+if (bookshelf.VERSION !== '0.12.1') {
+  logger.error('WARNING: nous avons patché la version 0.12.1 de Bookshelf. Risque de bug dans les versions à venir.');
+  logger.error('WARNING: supprimer le patch si `Model.count()` retourne un Integer sous PostgreSQL. Réréfence: https://github.com/bookshelf/bookshelf/issues/1275');
+}
+
+// XXX a supprimer si Model.count() retourne un Integer
+// ref: https://github.com/bookshelf/bookshelf/issues/1275
+bookshelf.plugin(bookshelf => {
+  const Model = bookshelf.Model;
+
+  const PatchedModel = Model.extend({
+    count(...options) {
+      const promise = Model.prototype.count.apply(this, options);
+      return promise.then(count => parseInt(count, 10));
+    }
+  });
+
+  bookshelf.Model = PatchedModel;
+});
 
 module.exports = bookshelf;
