73fcf294866f7e8e7b5ed8abfe7334beadb8b063
Antoine Boileau <apem.boileau@gmail.com>
2018-06-27 12:05:09 +0200

    [pf-94] Change Model API, to create a skill review from Assessment

--- api/lib/domain/models/SkillReview.js
+++ api/lib/domain/models/SkillReview.js
@@ -1,21 +1,31 @@
+const SKILL_REVIEW_ID_PREFIX = 'skill-review-';
+
 /*
  * Traduction : Profil d'avancement
  */
 class SkillReview {
 
-  constructor({ id, tragetedSkills, validatedSkills, failedSkills } = {}) {
+  constructor({ id, targetedSkills, validatedSkills, failedSkills } = {}) {
     this.id = id;
-    this.tragetedSkills = tragetedSkills;
+    this.targetedSkills = targetedSkills;
     this.validatedSkills = validatedSkills;
     this.failedSkills = failedSkills;
   }
 
   get profileMasteryRate() {
-    const numberOfTargetedSkills = this.tragetedSkills.length;
+    const numberOfTargetedSkills = this.targetedSkills.length;
     const numberOfValidatedSkills = this.validatedSkills.length;
     const targetProfileHasSkills = numberOfTargetedSkills !== 0;
 
     return targetProfileHasSkills ? (numberOfValidatedSkills / numberOfTargetedSkills) : 0;
+  }
+
+  static generateIdFromAssessmentId(assessmentId) {
+    return `${SKILL_REVIEW_ID_PREFIX}${assessmentId}`;
+  }
+
+  static getAssessmentIdFromId(skillReviewId) {
+    return parseInt(skillReviewId.replace(SKILL_REVIEW_ID_PREFIX, ''), 10);
   }
 }
 
