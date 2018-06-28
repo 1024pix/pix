ff88cffcc731b2040b8a34e203434b6f6f9e7d31
David Riviere <driviere@octo.com>
2018-06-28 12:03:02 +0200

    [PF-62] Correcting test after adding tutorials object in correction in serializer

--- api/lib/infrastructure/serializers/jsonapi/correction-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/correction-serializer.js
@@ -4,7 +4,19 @@ module.exports = {
 
   serialize(correction) {
     return new Serializer('corrections', {
-      attributes: ['solution', 'hint'],
+      attributes: ['solution', 'hint', 'tutorials'],
+      tutorials: {
+        ref: 'id',
+        includes: true,
+        attributes: [
+          'id',
+          'duration',
+          'format',
+          'link',
+          'source',
+          'title',
+        ]
+      },
       transform: (record) => {
         const correction = Object.assign({}, record);
         correction.hint = typeof(record.relevantHint) !== 'undefined' ? record.relevantHint.value : null;
