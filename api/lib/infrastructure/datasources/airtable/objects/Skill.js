2f6e0695f1c999684cb31392a66060d34e595402
MelanieMEB <mboudard@octo.com>
2018-06-28 11:11:58 +0200

    [PF-62] Add tutorialId to Skill object

--- api/lib/infrastructure/datasources/airtable/objects/Skill.js
+++ api/lib/infrastructure/datasources/airtable/objects/Skill.js
@@ -1,9 +1,10 @@
 class Skill {
-  constructor({ id, name, hint, hintStatus } = {}) {
+  constructor({ id, name, hint, hintStatus, tutorialId } = {}) {
     this.id = id;
     this.name = name;
     this.hint = hint;
     this.hintStatus = hintStatus;
+    this.tutorialId = tutorialId;
   }
 
   static fromAirTableObject(airtableEpreuveObject) {
@@ -12,6 +13,7 @@ class Skill {
       name: airtableEpreuveObject['fields']['Nom'],
       hint: airtableEpreuveObject['fields']['Indice'],
       hintStatus: airtableEpreuveObject['fields']['Statut de l\'indice'],
+      tutorialId: airtableEpreuveObject['fields']['Comprendre'],
     });
   }
 }
