68ed38206febe7c0605390a960c379c4b7348602
Antoine Boileau <apem.boileau@gmail.com>
2018-06-29 15:35:19 +0200

    [pf-94] Resolve first bug (no challenges on screen)

--- live/app/routes/assessments/rating.js
+++ live/app/routes/assessments/rating.js
@@ -3,20 +3,21 @@ import Route from '@ember/routing/route';
 export default Route.extend({
 
   afterModel(assessment) {
-    this.get('store').createRecord('assessment-result', { assessment }).save();
 
-    switch (assessment.get('type')) {
-      case 'CERTIFICATION':
-        this.transitionTo('certifications.results', assessment.get('certificationNumber'));
-        break;
+    return this.get('store')
+      .createRecord('assessment-result', { assessment })
+      .save()
+      .finally(() => {
+        switch (assessment.get('type')) {
+          case 'CERTIFICATION':
+            return this.transitionTo('certifications.results', assessment.get('certificationNumber'));
 
-      case 'SMART_PLACEMENT':
-        this.transitionTo('campaigns.skill-review', assessment.get('id'));
-        break;
+          case 'SMART_PLACEMENT':
+            return this.transitionTo('campaigns.skill-review', assessment.get('id'));
 
-      default:
-        this.transitionTo('assessments.results', assessment.get('id'));
-    }
-  }
-
+          default:
+            return this.transitionTo('assessments.results', assessment.get('id'));
+        }
+      });
+  },
 });
