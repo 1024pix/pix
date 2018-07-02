23f5a8d5df0a14e86d50fd657873e1213ddecfb0
David Riviere <driviere@octo.com>
2018-07-02 16:05:26 +0200

    [PF-62] Correcting hasardus hint & tutos displays

--- live/app/components/tutorial-panel.js
+++ live/app/components/tutorial-panel.js
@@ -12,19 +12,19 @@ export default Component.extend({
     return this.get('resultItemStatus') !== 'ok';
   }),
 
-  shouldDisplayHintOrTuto: computed('resultItemStatus', function() {
+  shouldDisplayHintOrTuto: computed('tutorials', 'hint', function() {
     const tutorials = this.get('tutorials') || [];
     const hint = this.get('hint') || [];
 
     return (hint.length > 0) || (tutorials.length > 0);
   }),
 
-  shouldDisplayHint: computed('resultItemStatus', 'hint', function() {
+  shouldDisplayHint: computed('hint', function() {
     const hint = this.get('hint') || [];
     return hint.length > 0;
   }),
 
-  shouldDisplayTutorial: computed('resultItemStatus', 'tutorials', function() {
+  shouldDisplayTutorial: computed('tutorials', function() {
     const tutorials = this.get('tutorials') || [];
     return tutorials.length > 0;
   }),
