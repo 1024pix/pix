86b05d176c0053526a059dd1d549f4d0968bd89f
Jérémy Buget <jbuget@gmail.com>
2018-07-03 17:20:47 +0200

    [pf-93] [Front] [BSR] Improve course name rendering into component course-banner

--- live/app/components/course-banner.js
+++ live/app/components/course-banner.js
@@ -1,10 +1,18 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
 
+  // Element
   classNames: ['course-banner'],
 
+  // Data props
   course: null,
-  withHomeLink: false
+  withHomeLink: false,
 
+  // CPs
+  courseName: computed('course.name', function() {
+    const courseName = this.get('course.name');
+    return courseName ? courseName : 'Parcours e-pro';
+  })
 });
