38058ee3a66437b5853fb3f98534080bbaaf744b
Benoit <benoit.prady@pix.fr>
2018-07-03 13:54:01 +0200

    Improved tests

--- tests/integration/components/certification-details-competence-test.js
+++ tests/integration/components/certification-details-competence-test.js
@@ -9,17 +9,17 @@ module('Integration | Component | certification-details-competence', function(ho
 
   let answer = (result) => {
     return {
-      skill:"@skill1",
-      challengeId:"rec12345",
-      order:"1",
+      skill:'@skill1',
+      challengeId:'rec12345',
+      order:'1',
       result:result
     }
   };
 
   let competence = (...result) => {
     return {
-      name: "Une compétence",
-      index: "1.1",
+      name: 'Une compétence',
+      index: '1.1',
       positionedLevel: 3,
       positionedScore: 26,
       obtainedLevel: -1,
@@ -30,324 +30,48 @@ module('Integration | Component | certification-details-competence', function(ho
 
   test('it renders', async function(assert) {
     // given
-    this.set("competenceData", competence("ok", "ko", "partially"));
-    this.set("externalAction", () => {
+    this.set('competenceData', competence('ok', 'ko', 'partially'));
+    this.set('externalAction', () => {
       return resolve();
     });
 
     // when
-    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=false updateRate=(action externalAction)}}`);
+    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=false onUpdateRate=(action externalAction)}}`);
 
     // then
     assert.dom('.certification-details-competence').exists();
   });
 
-  test('it should not render jury values when no jury rate is set', async function(assert) {
+  test('it should not render jury values when no jury values are set', async function(assert) {
     // given
-    this.set("competenceData", competence("ok", "ko", "partially"));
-    this.set("externalAction", () => {
+    this.set('competenceData', competence('ok', 'ko', 'partially'));
+    this.set('externalAction', () => {
       return resolve();
     });
 
     // when
-    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=false updateRate=(action externalAction)}}`);
+    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=false onUpdateRate=(action externalAction)}}`);
 
     // then
     assert.dom('.jury').doesNotExist();
   });
 
-  test('it should render jury values when a jury rate is set and score differs', async function(assert) {
+  test('it should render jury values when these values are set', async function(assert) {
     // given
-    this.set("competenceData", competence("ok", "ok", "ko"));
-    this.set("externalAction", () => {
+    this.set('competenceData', competence('ok', 'ok', 'ko'));
+    this.set('externalAction', () => {
       return resolve();
     });
+    assert.expect(4);
 
     // when
-    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=70 updateRate=(action externalAction)}}`);
+    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=70 onUpdateRate=(action externalAction)}}`);
 
     // then
-    assert.expect(2);
     assert.dom('.jury.competence-level').exists();
     assert.dom('.jury.competence-score').exists();
+    assert.dom('.jury.competence-level').hasText('2');
+    assert.dom('.jury.competence-score').hasText('18 Pix');
   });
-
-  test('it should not render jury values when a jury rate is set and score does not differ', async function(assert) {
-    // given
-    this.set("competenceData", competence("ko", "ko", "ko"));
-    this.set("externalAction", () => {
-      return resolve();
-    });
-
-    // when
-    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=70 updateRate=(action externalAction)}}`);
-
-    // then
-    assert.expect(2);
-    assert.dom('.jury.competence-level').doesNotExist();
-    assert.dom('.jury.competence-score').doesNotExist();
-  });
-
-  test('it should give level n when jury rate is set and 3 ok', async function(assert) {
-    // given
-    this.set("competenceData", competence("ok", "ok", "ok"));
-    this.set("externalAction", () => {
-      return resolve();
-    });
-
-    // when
-    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=70 updateRate=(action externalAction)}}`);
-
-    // then
-    assert.expect(2);
-    assert.dom('.jury.competence-level').hasText("3");
-    assert.dom('.jury.competence-score').hasText("26 Pix");
-  });
-
-  test('it should give level n when jury rate is set to 81 and 2 ok and 1 ko', async function(assert) {
-    // given
-    this.set("competenceData", competence("ok", "ok", "ko"));
-    this.set("externalAction", () => {
-      return resolve();
-    });
-
-    // when
-    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=81 updateRate=(action externalAction)}}`);
-
-    // then
-    assert.expect(2);
-    assert.dom('.jury.competence-level').hasText("3");
-    assert.dom('.jury.competence-score').hasText("26 Pix");
-  });
-
-  test('it should give level n when jury rate is set to 81 and 2 ok and 1 aband', async function(assert) {
-    // given
-    this.set("competenceData", competence("ok", "ok", "aband"));
-    this.set("externalAction", () => {
-      return resolve();
-    });
-
-    // when
-    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=81 updateRate=(action externalAction)}}`);
-
-    // then
-    assert.expect(2);
-    assert.dom('.jury.competence-level').hasText("3");
-    assert.dom('.jury.competence-score').hasText("26 Pix");
-  });
-
-  test('it should give level n when jury rate is set to 81 and 2 ok and 1 partially', async function(assert) {
-    // given
-    this.set("competenceData", competence("ok", "ok", "partially"));
-    this.set("externalAction", () => {
-      return resolve();
-    });
-
-    // when
-    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=81 updateRate=(action externalAction)}}`);
-
-    // then
-    assert.expect(2);
-    assert.dom('.jury.competence-level').hasText("3");
-    assert.dom('.jury.competence-score').hasText("26 Pix");
-  });
-
-  test('it should give level n when jury rate is set to 81 and 2 ok and 1 timedout', async function(assert) {
-    // given
-    this.set("competenceData", competence("ok", "ok", "timedout"));
-    this.set("externalAction", () => {
-      return resolve();
-    });
-
-    // when
-    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=81 updateRate=(action externalAction)}}`);
-
-    // then
-    assert.expect(2);
-    assert.dom('.jury.competence-level').hasText("3");
-    assert.dom('.jury.competence-score').hasText("26 Pix");
-  });
-
-  test('it should give level n-1 when jury rate is set to 65 and 2 ok and 1 ko', async function(assert) {
-    // given
-    this.set("competenceData", competence("ok", "ok", "ko"));
-    this.set("externalAction", () => {
-      return resolve();
-    });
-
-    // when
-    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=65 updateRate=(action externalAction)}}`);
-
-    // then
-    assert.expect(2);
-    assert.dom('.jury.competence-level').hasText("2");
-    assert.dom('.jury.competence-score').hasText("18 Pix");
-  });
-
-  test('it should give level n-1 when jury rate is set to 65 and 2 ok and 1 aband', async function(assert) {
-    // given
-    this.set("competenceData", competence("ok", "ok", "aband"));
-    this.set("externalAction", () => {
-      return resolve();
-    });
-
-    // when
-    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=65 updateRate=(action externalAction)}}`);
-
-    // then
-    assert.expect(2);
-    assert.dom('.jury.competence-level').hasText("2");
-    assert.dom('.jury.competence-score').hasText("18 Pix");
-  });
-
-  test('it should give level n-1 when jury rate is set to 65 and 2 ok and 1 partially', async function(assert) {
-    // given
-    this.set("competenceData", competence("ok", "ok", "partially"));
-    this.set("externalAction", () => {
-      return resolve();
-    });
-
-    // when
-    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=65 updateRate=(action externalAction)}}`);
-
-    // then
-    assert.expect(2);
-    assert.dom('.jury.competence-level').hasText("2");
-    assert.dom('.jury.competence-score').hasText("18 Pix");
-  });
-
-  test('it should give level n-1 when jury rate is set to 65 and 2 ok and 1 timedout', async function(assert) {
-    // given
-    this.set("competenceData", competence("ok", "ok", "timedout"));
-    this.set("externalAction", () => {
-      return resolve();
-    });
-
-    // when
-    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=65 updateRate=(action externalAction)}}`);
-
-    // then
-    assert.expect(2);
-    assert.dom('.jury.competence-level').hasText("2");
-    assert.dom('.jury.competence-score').hasText("18 Pix");
-  });
-
-  test('it should give level -1 when jury rate is set and 1 ok', async function(assert) {
-    // given
-    this.set("competenceData", competence("ok", "ko", "aband"));
-    this.set("externalAction", () => {
-      return resolve();
-    });
-
-    // when
-    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=90 updateRate=(action externalAction)}}`);
-
-    // then
-    assert.expect(2);
-    // Elements not displayed since score is unchanged (-1)
-    assert.dom('.jury.competence-level').doesNotExist();
-    assert.dom('.jury.competence-score').doesNotExist();
-  });
-
-  test('it should give level -1 when jury rate is set to 49', async function(assert) {
-    // given
-    this.set("competenceData", competence("ok", "ok", "ok"));
-    this.set("externalAction", () => {
-      return resolve();
-    });
-
-    // when
-    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=49 updateRate=(action externalAction)}}`);
-
-    // then
-    assert.expect(2);
-    // Elements not displayed since score is unchanged (-1)
-    assert.dom('.jury.competence-level').doesNotExist();
-    assert.dom('.jury.competence-score').doesNotExist();
-  });
-
-
-  // SKIP
-
-  test('it should give level n when jury rate is set to 81 and 2 ok and 1 skip', async function(assert) {
-    // given
-    this.set("competenceData", competence("ok", "ok", "skip"));
-    this.set("externalAction", () => {
-      return resolve();
-    });
-
-    // when
-    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=81 updateRate=(action externalAction)}}`);
-
-    // then
-    assert.expect(2);
-    assert.dom('.jury.competence-level').hasText("3");
-    assert.dom('.jury.competence-score').hasText("26 Pix");
-  });
-
-  test('it should give level n when jury rate is set to 65 and 2 ok and 1 skip', async function(assert) {
-    // given
-    this.set("competenceData", competence("ok", "ok", "skip"));
-    this.set("externalAction", () => {
-      return resolve();
-    });
-
-    // when
-    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=65 updateRate=(action externalAction)}}`);
-
-    // then
-    assert.expect(2);
-    assert.dom('.jury.competence-level').hasText("3");
-    assert.dom('.jury.competence-score').hasText("26 Pix");
-  });
-
-  test('it should give level n when jury rate is set to 90, 1 ok, 1 partially, 1 skip', async function(assert) {
-    // given
-    this.set("competenceData", competence("ok", "partially", "skip"));
-    this.set("externalAction", () => {
-      return resolve();
-    });
-
-    // when
-    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=90 updateRate=(action externalAction)}}`);
-
-    // then
-    assert.expect(2);
-    assert.dom('.jury.competence-level').hasText("3");
-    assert.dom('.jury.competence-score').hasText("26 Pix");
-  });
-
-  test('it should give level n when jury rate is set to 65, 1 ok, 1 partially, 1 skip', async function(assert) {
-    // given
-    this.set("competenceData", competence("ok", "partially", "skip"));
-    this.set("externalAction", () => {
-      return resolve();
-    });
-
-    // when
-    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=65 updateRate=(action externalAction)}}`);
-
-    // then
-    assert.expect(2);
-    assert.dom('.jury.competence-level').hasText("2");
-    assert.dom('.jury.competence-score').hasText("18 Pix");
-  });
-
-  test('it should give level -1 when jury rate is set, 1 ok, 1 ko, 1 skip', async function(assert) {
-    // given
-    this.set("competenceData", competence("ok", "ko", "skip"));
-    this.set("externalAction", () => {
-      return resolve();
-    });
-
-    // when
-    await render(hbs`{{certification-details-competence competence=competenceData rate=60 juryRate=90 updateRate=(action externalAction)}}`);
-
-    // then
-    assert.expect(2);
-    // Elements not displayed since score is unchanged (-1)
-    assert.dom('.jury.competence-level').doesNotExist();
-    assert.dom('.jury.competence-score').doesNotExist();
-  })
 
 });
--- tests/unit/controllers/authenticated/certifications/single/details-test.js
+++ tests/unit/controllers/authenticated/certifications/single/details-test.js
@@ -8,18 +8,18 @@ module('Unit | Controller | authenticated/certifications/single/details', functi
 
   let answer = (result) => {
     return {
-      skill:"@skill1",
-      challengeId:"rec12345",
-      order:"1",
-      result:"ok",
+      skill:'@skill1',
+      challengeId:'rec12345',
+      order:'1',
+      result:'ok',
       jury:result
     }
   };
 
   let competence = (juryScore, ...result) => {
     return {
-      name: "Une compétence",
-      index: "1.1",
+      name: 'Une compétence',
+      index: '1.1',
       positionedLevel: 3,
       positionedScore: 26,
       obtainedLevel: 3,
@@ -32,25 +32,25 @@ module('Unit | Controller | authenticated/certifications/single/details', functi
   test('it computes jury rate correctly', function(assert) {
     // given
     let controller = this.owner.lookup('controller:authenticated/certifications/single/details');
-    controller.set("model", EmberObject.create( {
-      competences:[competence(false,"ok","ok","skip"), competence(false,"ok", "ko", "ok"), competence(false,"ok", "aband", "ok"), competence(false,"ok", "timedout", "ok"), competence(false,"ok", "ok", "ok")]
+    controller.set('model', EmberObject.create( {
+      competences:[competence(false,'ok','ok','skip'), competence(false,'ok', 'ko', 'ok'), competence(false,'ok', 'aband', 'ok'), competence(false,'ok', 'timedout', 'ok'), competence(false,'ok', 'ok', 'ok')]
     }));
     run(function() {
-      controller.send("updateRate");
+      controller.send('onUpdateRate');
     });
-    assert.equal(controller.get("juryRate"), 78.57);
+    assert.equal(controller.get('juryRate'), 78.57);
   });
 
   test('it computes jury score correctly', function(assert) {
     // given
     let controller = this.owner.lookup('controller:authenticated/certifications/single/details');
-    controller.set("model", EmberObject.create( {
-      competences:[competence(true,"ok","ok","skip"), competence(false,"ok", "ko", "ok"), competence(true,"ok", "aband", "ok"), competence(false,"ok", "timedout", "ok"), competence(true,"ok", "ok", "ok")]
+    controller.set('model', EmberObject.create( {
+      competences:[competence(true,'ok','ok','skip'), competence(false,'ok', 'ko', 'ok'), competence(true,'ok', 'aband', 'ok'), competence(false,'ok', 'timedout', 'ok'), competence(true,'ok', 'ok', 'ok')]
     }));
     run(function() {
-      controller.send("updateRate");
+      controller.send('onUpdateRate');
     });
     // 3 jury scores + 2 obtained scores
-    assert.equal(controller.get("juryScore"), 12*3+26*2);
+    assert.equal(controller.get('juryScore'), 12*3+26*2);
   });
 });
