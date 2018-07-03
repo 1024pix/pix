80ee9471aaea7f543ed8c08f16dcedadf62f3a81
Jérémy Buget <jbuget@gmail.com>
2018-07-03 17:23:07 +0200

    [pf-93] [Front] Refactor model assessment in order to remove "progress" CP used only for rendering in component progress-bar

--- live/app/components/progress-bar.js
+++ live/app/components/progress-bar.js
@@ -1,11 +1,27 @@
 import { htmlSafe } from '@ember/string';
 import { computed } from '@ember/object';
 import Component from '@ember/component';
+import AssessmentProgression from '../models/assessment-progression';
 
 export default Component.extend({
+
+  // Element
   classNames: ['progress', 'pix-progress-bar'],
 
-  barStyle: computed('progress.stepPercentage', function() {
-    return htmlSafe(`width: ${this.get('progress.stepPercentage')}%`);
+  // Data props
+  assessment: null,
+
+  // CPs
+  progression: computed('assessment.{type,answers,course.nbChallenges}', function() {
+    return new AssessmentProgression({
+      assessmentType: this.get('assessment.type'),
+      nbAnswers: this.get('assessment.answers.length'),
+      nbChallenges: this.get('assessment.course.nbChallenges')
+    });
+  }),
+
+  barStyle: computed('progression', function() {
+    return htmlSafe(`width: ${this.get('progression.valueNow')}%`);
   })
+
 });
