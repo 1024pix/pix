c833934b2eeb7526c55f975711b9296ef5479c03
Benoit <benoit.prady@pix.fr>
2018-07-03 15:57:42 +0200

    Added tests for certification-info-field

--- tests/integration/components/certification-info-field-test.js
+++ tests/integration/components/certification-info-field-test.js
@@ -11,4 +11,32 @@ module('Integration | Component | certification-info-field', function(hooks) {
 
     assert.dom('.certification-info-field').exists();
   });
+
+  test('it renders label and field value', async function(assert) {
+    // Given
+    this.set('fieldValue', 'a field value');
+    this.set('fieldLabel', 'a field label');
+    assert.expect(2);
+
+    // When
+    await render(hbs`{{certification-info-field value=fieldValue label=fieldLabel edition=false}}`);
+
+    // Then
+    assert.dom('.certification-info-field .certification-info-label').hasText('a field label');
+    assert.dom('.certification-info-field .certification-info-value').hasText('a field value');
+  });
+
+  test('it renders field value with suffix if provided', async function(assert) {
+    // Given
+    this.set('fieldValue', 'a field value');
+    this.set('fieldLabel', 'a field label');
+    this.set('fieldSuffix', ' SUFFIX');
+
+    // When
+    await render(hbs`{{certification-info-field value=fieldValue label=fieldLabel suffix=fieldSuffix edition=false}}`);
+
+    // Then
+    assert.dom('.certification-info-field .certification-info-value').hasText('a field value SUFFIX');
+  });
+
 });
