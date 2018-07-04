c2ad30d3d7f0e2325b4eb278669676f340028f67
Jérémy Buget <jbuget@gmail.com>
2018-07-04 14:53:08 +0200

    [pf-93] [Front] [BSR] Little refactor of the computed property models/assessment@answersSinceLastCheckpoints

--- live/app/models/assessment.js
+++ live/app/models/assessment.js
@@ -2,6 +2,7 @@ import { equal } from '@ember/object/computed';
 import { computed } from '@ember/object';
 import DS from 'ember-data';
 import ENV from 'pix-live/config/environment';
+
 const { attr, Model, belongsTo, hasMany } = DS;
 
 export default Model.extend({
@@ -17,17 +18,13 @@ export default Model.extend({
   result: belongsTo('assessment-result'),
   type: attr('string'),
 
-  // FIXME move into routes/assessments/checkpoint
   answersSinceLastCheckpoints: computed('answers.[]', function() {
     const answers = this.get('answers').toArray();
-    if (this.get('type') === 'SMART_PLACEMENT') {
-      const howManyAnswersSinceTheLastCheckpoint = answers.length % ENV.APP.NUMBER_OF_CHALLENGE_BETWEEN_TWO_CHECKPOINTS_IN_SMART_PLACEMENT;
-      const sliceAnswersFrom = (howManyAnswersSinceTheLastCheckpoint === 0)
-        ? -ENV.APP.NUMBER_OF_CHALLENGE_BETWEEN_TWO_CHECKPOINTS_IN_SMART_PLACEMENT
-        : -howManyAnswersSinceTheLastCheckpoint;
-      return answers.slice(sliceAnswersFrom);
-    }
-    throw new Error('This is an Assessment without checkpoints !');
+    const howManyAnswersSinceTheLastCheckpoint = answers.length % ENV.APP.NUMBER_OF_CHALLENGE_BETWEEN_TWO_CHECKPOINTS_IN_SMART_PLACEMENT;
+    const sliceAnswersFrom = (howManyAnswersSinceTheLastCheckpoint === 0)
+      ? -ENV.APP.NUMBER_OF_CHALLENGE_BETWEEN_TWO_CHECKPOINTS_IN_SMART_PLACEMENT
+      : -howManyAnswersSinceTheLastCheckpoint;
+    return answers.slice(sliceAnswersFrom);
   })
 
 });
