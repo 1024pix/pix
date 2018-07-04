97a32c107fcd5958d2c07f78d8bd3dac64789e69
Jonathan Perret <jonathan@ut7.fr>
2018-07-04 17:41:10 +0200

    [pf-284] Respect DATABASE_CONNECTION_POOL_MAX_SIZE env var for PG pool size in staging

--- api/db/knexfile.js
+++ api/db/knexfile.js
@@ -32,8 +32,8 @@ module.exports = {
     client: 'postgresql',
     connection: process.env.DATABASE_URL,
     pool: {
-      min: 10,
-      max: 100
+      min: 1,
+      max: (parseInt(process.env.DATABASE_CONNECTION_POOL_MAX_SIZE, 10) || 4),
     },
     migrations: {
       tableName: 'knex_migrations',
@@ -49,7 +49,7 @@ module.exports = {
     connection: process.env.DATABASE_URL,
     pool: {
       min: 1,
-      max: (parseInt(process.env.DATABASE_CONNECTION_POOL_MAX_SIZE, '10') || 4),
+      max: (parseInt(process.env.DATABASE_CONNECTION_POOL_MAX_SIZE, 10) || 4),
     },
     migrations: {
       tableName: 'knex_migrations',
