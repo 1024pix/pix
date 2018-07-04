4e5941eb8c51ddaf73c7dde57b2c509a7a94fc94
Antoine Boileau <apem.boileau@gmail.com>
2018-07-04 15:18:22 +0200

    [pf-243] Add build solution factory and BSR in factory/index

---
+++ api/tests/factory/build-solution.js
@@ -0,0 +1,23 @@
+const faker = require('faker');
+const Solution = require('../../lib/domain/models/Solution');
+
+module.exports = function buildSolution({
+  id = `rec${faker.random.uuid()}`,
+  type = 'qcm',
+  value = '1',
+  isT1Enabled = false,
+  isT2Enabled = false,
+  isT3Enabled = false,
+  scoring = '',
+} = {}) {
+
+  return new Solution({
+    id,
+    type,
+    value,
+    isT1Enabled,
+    isT2Enabled,
+    isT3Enabled,
+    scoring,
+  });
+};
