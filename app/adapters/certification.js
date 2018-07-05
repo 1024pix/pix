32aa35c42f5213e55afd762d80515cf5e8fe80b5
Benoit <benoit.prady@pix.fr>
2018-07-05 16:50:37 +0200

    updated certification model, adapter and serializer to handle certification edition

--- app/adapters/certification.js
+++ app/adapters/certification.js
@@ -3,5 +3,28 @@ import ApplicationAdapter from './application';
 export default ApplicationAdapter.extend({
   urlForFindRecord (id) {
     return this.host + '/admin/certifications/'+id;
+  },
+
+  urlForUpdateMarks() {
+    return this.host + '/admin/assessment-results/';
+  },
+
+  urlForUpdateRecord(id) {
+    return this.host + '/certification-courses/'+id;
+  },
+
+  updateRecord(store, type, snapshot) {
+    let data = {}, marksData = {};
+    let requests = [];
+    let serializer = store.serializerFor(type.modelName);
+    serializer.serializeIntoHash(data, type, snapshot, { includeId: true, onlyInformation:true });
+    requests.push(this.ajax(this.buildURL(type.modelName, snapshot.id, snapshot, 'updateRecord'), 'PATCH', { data: data }));
+    serializer.serializeIntoHash(marksData, type, snapshot, { includeId: true });
+    marksData.data.type = 'results';
+    marksData.data.attributes['jury-id'] = null;
+    marksData.data.attributes['emitter'] = 'Jury Pix';
+    requests.push(this.ajax(this.urlForUpdateMarks(), 'POST', { data: marksData }));
+    return Promise.all(requests);
   }
+
 });
--- app/serializers/certification.js
+++ app/serializers/certification.js
@@ -5,6 +5,33 @@ export default DS.JSONAPISerializer.extend({
     payload.data.id = id;
     return this.normalizeSingleResponse(...arguments);
   },
+
+  extractAttributes() {
+    let attributes = this._super(...arguments);
+    if (attributes['birthdate']) {
+      attributes['birthdate'] = (new Date(attributes['birthdate'])).toLocaleDateString('fr-FR');
+    }
+    return attributes;
+  },
+
+  serialize(snapshot, options) {
+    if (options.onlyInformation) {
+      let data = {};
+      this.serializeAttribute(snapshot, data, 'firstName', 'first-name');
+      this.serializeAttribute(snapshot, data, 'lastName', 'last-name');
+      this.serializeAttribute(snapshot, data, 'birthplace', 'birthplace');
+      this.serializeAttribute(snapshot, data, 'birthdate', 'birthdate');
+      this.serializeAttribute(snapshot, data, 'externalId', 'external-id');
+      data.type = 'certifications';
+      if (options.includeId) {
+        data.id = parseInt(snapshot.id);
+      }
+      return {data:data};
+    } else {
+      return this._super(...arguments);
+    }
+  },
+
   modelNameFromPayloadKey() {
     return 'certification';
   }
