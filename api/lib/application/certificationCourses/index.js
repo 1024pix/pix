72d8e3e47a1e6ae0045fd5f000b6c882fae314d6
MelanieMEB <mboudard@octo.com>
2018-07-08 19:30:56 +0200

    [PF-253] Add comment on route

--- api/lib/application/certificationCourses/index.js
+++ api/lib/application/certificationCourses/index.js
@@ -17,6 +17,7 @@ exports.register = function(server, options, next) {
         notes: [
           'Cette route est utilisé par Pix Admin',
           'Elle sert au cas où une certification a eu une erreur de calcul',
+          'Cette route ne renvoie pas le bon format.',
         ]
       }
     },
