1d2e883dcb928edcca0fc41bbeca5dcdf3e4154b
Antoine Boileau <apem.boileau@gmail.com>
2018-07-11 12:28:36 +0200

    [pf-243] Add creation of KnowledgeElements from answers and skills

--- api/lib/domain/models/SmartPlacementKnowledgeElement.js
+++ api/lib/domain/models/SmartPlacementKnowledgeElement.js
@@ -1,3 +1,6 @@
+const Skill = require('../models/Skill');
+const _ = require('lodash');
+
 const SmartPlacementKnowledgeElementSourceType = Object.freeze({
   DIRECT: 'direct',
   INFERRED: 'inferred',
@@ -7,6 +10,9 @@ const SmartPlacementKnowledgeElementStatusType = Object.freeze({
   VALIDATED: 'validated',
   INVALIDATED: 'invalidated',
 });
+
+const validatedStatus = SmartPlacementKnowledgeElementStatusType.VALIDATED;
+const invalidatedStatus = SmartPlacementKnowledgeElementStatusType.INVALIDATED;
 
 /*
  * Traduction : élément de connaissance d'un profil exploré dans le cadre d'un smart placement
@@ -43,9 +49,131 @@ class SmartPlacementKnowledgeElement {
   get isInvalidated() {
     return this.status === SmartPlacementKnowledgeElementStatusType.INVALIDATED;
   }
+
+  static createKnowledgeElementsForAnswer({
+    answer,
+    associatedChallenge,
+    previouslyValidatedSkills,
+    previouslyFailedSkills,
+    targetSkills,
+  }) {
+
+    const directKnowledgeElements = createDirectKnowledgeElements({
+      answer, associatedChallenge, previouslyFailedSkills, previouslyValidatedSkills, targetSkills,
+    });
+
+    return enrichDirectKnowledgeElementsWithInferredKnowledgeElements({
+      answer,
+      directKnowledgeElements,
+      previouslyFailedSkills,
+      previouslyValidatedSkills,
+      targetSkills,
+    });
+  }
 }
 
 SmartPlacementKnowledgeElement.SourceType = SmartPlacementKnowledgeElementSourceType;
 SmartPlacementKnowledgeElement.StatusType = SmartPlacementKnowledgeElementStatusType;
 
+function createDirectKnowledgeElements({ answer, associatedChallenge, targetSkills, previouslyFailedSkills, previouslyValidatedSkills }) {
+
+  const status = answer.isOk() ? validatedStatus : invalidatedStatus;
+
+  return associatedChallenge.skills
+    .filter(skillIsInTargetedSkills({ targetSkills }))
+    .filter(skillIsNotAlreadyAssessed({ previouslyFailedSkills, previouslyValidatedSkills }))
+    .map((skill) => {
+      const source = SmartPlacementKnowledgeElement.SourceType.DIRECT;
+      return createKnowledgeElementsForSkill({ skill, source, status, answerId: answer.id });
+    });
+}
+
+function skillIsInTargetedSkills({ targetSkills }) {
+  return (skill) => _.intersectionWith(targetSkills, skill, Skill.areEqual).length > 0;
+}
+
+function skillIsNotAlreadyAssessed({ previouslyFailedSkills, previouslyValidatedSkills }) {
+  const alreadyAssessedSkills = previouslyValidatedSkills.concat(previouslyFailedSkills);
+  return (skill) => _.intersectionWith(alreadyAssessedSkills, skill, Skill.areEqual).length === 0;
+}
+
+function enrichDirectKnowledgeElementsWithInferredKnowledgeElements({
+  answer,
+  directKnowledgeElements,
+  previouslyFailedSkills,
+  previouslyValidatedSkills,
+  targetSkills,
+}) {
+  const targetSkillsGroupedByTubeName = _.groupBy(targetSkills, (skill) => skill.tubeName);
+  const status = answer.isOk() ? validatedStatus : invalidatedStatus;
+
+  return directKnowledgeElements.reduce((totalKnowledgeElements, directKnowledgeElement) => {
+
+    const directSkill = new Skill({ name: directKnowledgeElement.skillId });
+
+    targetSkillsGroupedByTubeName[directSkill.tubeName]
+      .filter(skillIsNotAlreadyAssessed({ previouslyFailedSkills, previouslyValidatedSkills }))
+      .forEach((skillToInfer) => {
+
+        const knowledgeElementThatExistForThatSkill = totalKnowledgeElements
+          .find((knowledgeElement) => {
+            const skillOfKnowledgeElement = new Skill({ name: knowledgeElement.skillId });
+            return Skill.areEqual(skillToInfer, skillOfKnowledgeElement);
+          });
+        const knowledgeElementDoesNotExistForThatSkill = knowledgeElementThatExistForThatSkill === undefined;
+
+        if (status === validatedStatus
+          && knowledgeElementDoesNotExistForThatSkill
+          && skillToInfer.difficulty < directSkill.difficulty) {
+
+          const newKnowledgeElement = createInferredValidatedKnowledgeElement({ answer, skillToInfer });
+          totalKnowledgeElements = totalKnowledgeElements.concat(newKnowledgeElement);
+        }
+        if (status === invalidatedStatus
+          && knowledgeElementDoesNotExistForThatSkill
+          && skillToInfer.difficulty > directSkill.difficulty) {
+
+          const newKnowledgeElement = createInferredInvalidatedKnowledgeElement({ answer, skillToInfer });
+          totalKnowledgeElements = totalKnowledgeElements.concat(newKnowledgeElement);
+        }
+      });
+
+    return totalKnowledgeElements;
+  }, directKnowledgeElements);
+
+}
+
+function createInferredValidatedKnowledgeElement({ answer, skillToInfer }) {
+  const source = SmartPlacementKnowledgeElement.SourceType.INFERRED;
+
+  return createKnowledgeElementsForSkill({
+    skill: skillToInfer,
+    source,
+    status: validatedStatus,
+    answerId: answer.id,
+  });
+}
+
+function createInferredInvalidatedKnowledgeElement({ answer, skillToInfer }) {
+  const source = SmartPlacementKnowledgeElement.SourceType.INFERRED;
+
+  return createKnowledgeElementsForSkill({
+    skill: skillToInfer,
+    source,
+    status: invalidatedStatus,
+    answerId: answer.id,
+  });
+}
+
+function createKnowledgeElementsForSkill({ skill, source, status, answerId }) {
+  return new SmartPlacementKnowledgeElement({
+    source,
+    status,
+    pixScore: 0,
+    answerId,
+    skillId: skill.name,
+  });
+}
+
 module.exports = SmartPlacementKnowledgeElement;
+
--- api/tests/unit/domain/models/SmartPlacementKnowledgeElement_test.js
+++ api/tests/unit/domain/models/SmartPlacementKnowledgeElement_test.js
@@ -1,4 +1,5 @@
 const { expect, factory } = require('../../../test-helper');
+const AnswerStatus = require('../../../../lib/domain/models/AnswerStatus');
 const SmartPlacementKnowledgeElement = require('../../../../lib/domain/models/SmartPlacementKnowledgeElement');
 
 describe('Unit | Domain | Models | SmartPlacementKnowledgeElement', () => {
@@ -58,6 +59,210 @@ describe('Unit | Domain | Models | SmartPlacementKnowledgeElement', () => {
 
       // then
       expect(isInvalidated).to.be.false;
+    });
+  });
+
+  describe('#createKnowledgeElementsForAnswer', () => {
+
+    context('when challenge skill is not in target profile', () => {
+
+      it('should return an empty array', () => {
+        // given
+        const skill = factory.buildSkill();
+        const otherSkill = factory.buildSkill();
+        const challenge = factory.buildChallenge({ skills: [skill] });
+        const answer = factory.buildAnswer({ challengeId: challenge.id, result: AnswerStatus.OK });
+
+        // when
+        const createdKnowledgeElements = SmartPlacementKnowledgeElement.createKnowledgeElementsForAnswer({
+          answer,
+          associatedChallenge: challenge,
+          previouslyValidatedSkills: [],
+          previouslyFailedSkills: [],
+          targetSkills: [otherSkill],
+        });
+
+        // then
+        expect(createdKnowledgeElements).to.deep.equal([]);
+      });
+    });
+
+    context('when some of the challenge skills are already assessed', () => {
+
+      it('should return knowledge elements only for the unknown skills', () => {
+        // given
+        const skillCollection = factory.buildSkillCollection();
+        const [skill1, skill2, skill3] = skillCollection;
+        const challenge = factory.buildChallenge({ skills: [skill1, skill2, skill3] });
+        const answer = factory.buildAnswer({ challengeId: challenge.id, result: AnswerStatus.OK });
+
+        // when
+        const createdKnowledgeElements = SmartPlacementKnowledgeElement.createKnowledgeElementsForAnswer({
+          answer,
+          associatedChallenge: challenge,
+          previouslyValidatedSkills: [skill1],
+          previouslyFailedSkills: [skill3],
+          targetSkills: skillCollection,
+        });
+
+        // then
+        const directKnowledgeElement = factory.buildSmartPlacementKnowledgeElement({
+          source: SmartPlacementKnowledgeElement.SourceType.DIRECT,
+          status: SmartPlacementKnowledgeElement.StatusType.VALIDATED,
+          pixScore: 0,
+          answerId: answer.id,
+          skillId: skill2.name,
+        });
+        directKnowledgeElement.id = undefined;
+
+        expect(createdKnowledgeElements).to.deep.equal([directKnowledgeElement]);
+      });
+    });
+
+    context('with right answer and no previous answers', () => {
+
+      it('should return one validated direct knowledge element and the inferred ones', () => {
+        // given
+        const skillCollection = factory.buildSkillCollection();
+        const [skill1, skill2] = skillCollection;
+        const challenge = factory.buildChallenge({ skills: [skill2] });
+        const answer = factory.buildAnswer({ challengeId: challenge.id, result: AnswerStatus.OK });
+
+        // when
+        const createdKnowledgeElements = SmartPlacementKnowledgeElement.createKnowledgeElementsForAnswer({
+          answer,
+          associatedChallenge: challenge,
+          previouslyValidatedSkills: [],
+          previouslyFailedSkills: [],
+          targetSkills: skillCollection,
+        });
+
+        // then
+        const directKnowledgeElement = factory.buildSmartPlacementKnowledgeElement({
+          source: SmartPlacementKnowledgeElement.SourceType.DIRECT,
+          status: SmartPlacementKnowledgeElement.StatusType.VALIDATED,
+          pixScore: 0,
+          answerId: answer.id,
+          skillId: skill2.name,
+        });
+        directKnowledgeElement.id = undefined;
+        const inferredKnowledgeElement = factory.buildSmartPlacementKnowledgeElement({
+          source: SmartPlacementKnowledgeElement.SourceType.INFERRED,
+          status: SmartPlacementKnowledgeElement.StatusType.VALIDATED,
+          pixScore: 0,
+          answerId: answer.id,
+          skillId: skill1.name,
+        });
+        inferredKnowledgeElement.id = undefined;
+        const expectedKnowledgeElements = [directKnowledgeElement, inferredKnowledgeElement];
+
+        expect(createdKnowledgeElements).to.deep.equal(expectedKnowledgeElements);
+      });
+    });
+
+    context('with wrong answer and no previous answers', () => {
+
+      it('should return one invalidated direct knowledge element and the inferred one', () => {
+        // given
+        const skillCollection = factory.buildSkillCollection();
+        const [, skill2, skill3] = skillCollection;
+        const challenge = factory.buildChallenge({ skills: [skill2] });
+        const answer = factory.buildAnswer({ challengeId: challenge.id, result: AnswerStatus.KO });
+
+        // when
+        const createdKnowledgeElements = SmartPlacementKnowledgeElement.createKnowledgeElementsForAnswer({
+          answer,
+          associatedChallenge: challenge,
+          previouslyValidatedSkills: [],
+          previouslyFailedSkills: [],
+          targetSkills: skillCollection,
+        });
+
+        // then
+        const directKnowledgeElement = factory.buildSmartPlacementKnowledgeElement({
+          source: SmartPlacementKnowledgeElement.SourceType.DIRECT,
+          status: SmartPlacementKnowledgeElement.StatusType.INVALIDATED,
+          pixScore: 0,
+          answerId: answer.id,
+          skillId: skill2.name,
+        });
+        directKnowledgeElement.id = undefined;
+        const inferredKnowledgeElement = factory.buildSmartPlacementKnowledgeElement({
+          source: SmartPlacementKnowledgeElement.SourceType.INFERRED,
+          status: SmartPlacementKnowledgeElement.StatusType.INVALIDATED,
+          pixScore: 0,
+          answerId: answer.id,
+          skillId: skill3.name,
+        });
+        inferredKnowledgeElement.id = undefined;
+        const expectedKnowledgeElements = [directKnowledgeElement, inferredKnowledgeElement];
+
+        expect(createdKnowledgeElements).to.deep.equal(expectedKnowledgeElements);
+      });
+    });
+
+    context('with multi skills challenge', () => {
+
+      it('should return multiple validated direct knowledge elements', () => {
+        // given
+        const skill = factory.buildSkill();
+        const challenge = factory.buildChallenge({ skills: [skill] });
+        const answer = factory.buildAnswer({ challengeId: challenge.id, result: AnswerStatus.KO });
+
+        // when
+        const createdKnowledgeElements = SmartPlacementKnowledgeElement.createKnowledgeElementsForAnswer({
+          answer,
+          associatedChallenge: challenge,
+          previouslyValidatedSkills: [],
+          previouslyFailedSkills: [],
+          targetSkills: [skill],
+        });
+
+        // then
+        const expectedKnowledgeElement = factory.buildSmartPlacementKnowledgeElement({
+          source: SmartPlacementKnowledgeElement.SourceType.DIRECT,
+          status: SmartPlacementKnowledgeElement.StatusType.INVALIDATED,
+          pixScore: 0,
+          answerId: answer.id,
+          skillId: skill.name,
+        });
+        expectedKnowledgeElement.id = undefined;
+
+        expect(createdKnowledgeElements).to.deep.equal([expectedKnowledgeElement]);
+      });
+    });
+
+    context('with previous answers', () => {
+
+      it('should return not return inferred knowledge elements on already assessed skills', () => {
+        // given
+        const skillCollection = factory.buildSkillCollection();
+        const [skill1, skill2, skill3] = skillCollection;
+        const challenge = factory.buildChallenge({ skills: [skill1] });
+        const answer = factory.buildAnswer({ challengeId: challenge.id, result: AnswerStatus.KO });
+
+        // when
+        const createdKnowledgeElements = SmartPlacementKnowledgeElement.createKnowledgeElementsForAnswer({
+          answer,
+          associatedChallenge: challenge,
+          previouslyValidatedSkills: [],
+          previouslyFailedSkills: [skill2, skill3],
+          targetSkills: skillCollection,
+        });
+
+        // then
+        const directKnowledgeElement = factory.buildSmartPlacementKnowledgeElement({
+          source: SmartPlacementKnowledgeElement.SourceType.DIRECT,
+          status: SmartPlacementKnowledgeElement.StatusType.INVALIDATED,
+          pixScore: 0,
+          answerId: answer.id,
+          skillId: skill1.name,
+        });
+        directKnowledgeElement.id = undefined;
+        const expectedKnowledgeElements = [directKnowledgeElement];
+
+        expect(createdKnowledgeElements).to.deep.equal(expectedKnowledgeElements);
+      });
     });
   });
 });
