95596aa6f30e4acc3047ee586244eb90defa2e2a
Benoit <benoit.prady@pix.fr>
2018-07-13 15:37:12 +0200

    Added session routes

---
+++ app/models/session.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  certificationCenter: DS.attr(),
+  address: DS.attr(),
+  room: DS.attr(),
+  examiner: DS.attr(),
+  date: DS.attr(),
+  time: DS.attr(),
+  description: DS.attr(),
+  accessCode: DS.attr(),
+  certifications: DS.hasMany('certification')
+});
