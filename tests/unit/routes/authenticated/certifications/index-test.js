76e5cd415bf04eb17167490f57f9172be625b263
Benoit <benoit.prady@pix.fr>
2018-07-13 15:42:08 +0200

    Replaced route menu with route index

--- tests/unit/routes/authenticated/certifications/index-test.js
+++ tests/unit/routes/authenticated/certifications/index-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | authenticated/certifications/menu', function(hooks) {
+module('Unit | Route | authenticated/certifications/index', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:authenticated/certifications/menu');
+    let route = this.owner.lookup('route:authenticated/certifications/index');
     assert.ok(route);
   });
 });
