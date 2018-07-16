8c61987faad7ab37daf2b1c2fe5af03446a1dc3e
Antoine Boileau <apem.boileau@gmail.com>
2018-07-16 15:46:30 +0200

    [pf-243] Add controller replies and use them on answer controller

---
+++ api/lib/infrastructure/controller-replies.js
@@ -0,0 +1,29 @@
+const errorSerializer = require('./serializers/jsonapi/error-serializer');
+const infraError = require('./errors');
+const logger = require('./logger');
+
+function controllerReplies(reply) {
+  return {
+
+    ok(payload) {
+      return reply(payload).code(200);
+    },
+
+    created(payload) {
+      return reply(payload).code(201);
+    },
+
+    error(error) {
+
+      if (error instanceof infraError.InfrastructureError) {
+        return reply(errorSerializer.serialize(error)).code(error.code);
+      }
+
+      logger.error(error);
+      const mappedError = new infraError.InfrastructureError(error.message);
+      return reply(errorSerializer.serialize(mappedError)).code(mappedError.code);
+    },
+  };
+}
+
+module.exports = controllerReplies;
