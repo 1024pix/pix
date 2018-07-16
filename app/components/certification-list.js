e37065a72bb2aba981ed93e778017caa1242874e
Benoit <benoit.prady@pix.fr>
2018-07-16 12:01:06 +0200

    added certification fields and link to session in certification list

--- app/components/certification-list.js
+++ app/components/certification-list.js
@@ -3,7 +3,32 @@ import Component from '@ember/component';
 const columns = [
   {
     propertyName: 'id',
-    title: 'Identifiant'
+    title: 'Id',
+    routeName: "authenticated.certifications.single.info"
+  },
+  {
+    propertyName: 'firstName',
+    title: 'Prénom'
+  },
+  {
+    propertyName: 'lastName',
+    title: 'Nom'
+  },
+  {
+    propertyName: 'status',
+    title: 'Statut'
+  },
+  {
+    propertyName: 'pixScore',
+    title: 'Score'
+  },
+  {
+    propertyName: 'creationDate',
+    title: 'Début'
+  },
+  {
+    propertyName: 'completionDate',
+    title: 'Fin'
   }
 ];
 
