cc065f8ec75a4f6f5474e4e08666f14b6c85f290
Antoine Boileau <apem.boileau@gmail.com>
2018-07-18 17:54:23 +0200

    [pf-243] Add validators for all types of challenges

---
+++ api/lib/domain/models/ValidatorQROCMDep.js
@@ -0,0 +1,33 @@
+const solutionServiceQROCMInd = require('../services/solution-service-qrocm-dep');
+const Validation = require('./Validation');
+const Validator = require('./Validator');
+
+/**
+ * Traduction: Vérificateur de réponse pour un QROCM Dep
+ */
+class ValidatorQROCMDep extends Validator {
+
+  constructor({
+    // attributes
+    // includes
+    solution,
+    // references
+  } = {}) {
+    super();
+    // attributes
+    // includes
+    this.solution = solution;
+    // references
+  }
+
+  assess(answer) {
+    const result = solutionServiceQROCMInd.match(answer.value, this.solution.value, this.solution.deactivations);
+
+    return new Validation({
+      result,
+      resultDetails: null,
+    });
+  }
+}
+
+module.exports = ValidatorQROCMDep;
---
+++ api/lib/domain/models/ValidatorQROCMInd.js
@@ -0,0 +1,33 @@
+const solutionServiceQROCMInd = require('../services/solution-service-qrocm-ind');
+const Validation = require('./Validation');
+const Validator = require('./Validator');
+
+/**
+ * Traduction: Vérificateur de réponse pour un QROCM Ind
+ */
+class ValidatorQROCMInd extends Validator {
+
+  constructor({
+    // attributes
+    // includes
+    solution,
+    // references
+  } = {}) {
+    super();
+    // attributes
+    // includes
+    this.solution = solution;
+    // references
+  }
+
+  assess(answer) {
+    const resultObject = solutionServiceQROCMInd.match(answer.value, this.solution.value, this.solution.enabledTreatments);
+
+    return new Validation({
+      result: resultObject.result,
+      resultDetails: resultObject.resultDetails,
+    });
+  }
+}
+
+module.exports = ValidatorQROCMInd;
