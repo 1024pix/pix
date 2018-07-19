81d07452f49d43e5f218fa5f267405ad118d2ded
Antoine Boileau <apem.boileau@gmail.com>
2018-07-19 11:57:36 +0200

    [pf-243] Convert the solution repository to use the solution adapter

--- api/lib/infrastructure/repositories/solution-repository.js
+++ api/lib/infrastructure/repositories/solution-repository.js
@@ -1,24 +1,11 @@
-const Solution = require('../../domain/models/Solution');
 const challengeDatasource = require('../datasources/airtable/challenge-datasource');
+const solutionAdapter = require('../adapters/solution-adapter');
 const _ = require('../../../lib/infrastructure/utils/lodash-utils');
 
 module.exports = {
 
   getByChallengeId(challengeId) {
     return challengeDatasource.get(challengeId)
-      .then((challengeDataObject) => {
-        const scoring = _.ensureString(challengeDataObject.scoring).replace(/@/g, ''); // XXX YAML ne supporte pas @
-
-        return new Solution({
-          id: challengeDataObject.id,
-          isT1Enabled: challengeDataObject.t1Status !== 'Désactivé',
-          isT2Enabled: challengeDataObject.t2Status !== 'Désactivé',
-          isT3Enabled: challengeDataObject.t3Status !== 'Désactivé',
-          type: challengeDataObject.type,
-          value: challengeDataObject.solution,
-          scoring
-        });
-      });
-  }
+      .then(solutionAdapter.fromChallengeAirtableDataObject);
+  },
 };
-
