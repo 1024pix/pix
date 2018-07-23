db86b931f78b24f80757a075d8781ac93df50219
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2018-07-23 17:11:42 +0200

    [pf-296] Create a script that allow a huge computation of results on PRODUCTION

---
+++ api/scripts/recompute-assessment-results.js
@@ -0,0 +1,56 @@
+#! /usr/bin/env node
+const request = require('request-promise-native');
+const { batch } = require('../db/batchTreatment');
+
+function compute(listOfAssessmentsToRecompute, request) {
+
+  return batch(null, listOfAssessmentsToRecompute, (assessmentId) => {
+    return request({
+      method: 'POST',
+      uri: 'https://pix.fr/api/assessment-results?recompute=true',
+      body: {
+        'data': {
+          'relationships': {
+            'assessment': {
+              'data': {
+                'type': 'assessments',
+                'id': assessmentId
+              }
+            }
+          }, 'type': 'assessment-results'
+        }
+      },
+      json: true
+    });
+  });
+}
+
+/**
+ * Le script déclenche une demande de calcule pour chaque assessmentId listé dans un fichier.
+ * Le fichier doit contenir un seul assessmentId par ligne.
+ *
+ * Ex: ./recompute-assessment-results.js ./path/to/list/of/ids.txt
+ */
+function main() {
+  const pathToListOfIds = process.argv[2];
+
+  const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream(pathToListOfIds)
+  });
+
+  const listOfAssessmentIdsToRecompute = [];
+  lineReader.on('line', function(line) {
+    listOfAssessmentIdsToRecompute.push(line);
+  });
+
+  lineReader.on('close', () => {
+    compute(listOfAssessmentIdsToRecompute, request);
+  });
+
+}
+
+if (process.env.NODE_ENV !== 'test') {
+  main();
+}
+
+module.exports = { compute };
