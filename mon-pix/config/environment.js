d8df34e69a39f72e280cfb7cdbd3472968763cfd
Thomas Wickham <mackwic@gmail.com>
2018-07-27 16:26:13 +0200

    [pf-305]  let environment override API_HOST config

--- mon-pix/config/environment.js
+++ mon-pix/config/environment.js
@@ -21,7 +21,7 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-      API_HOST: '',
+      API_HOST: process.env.API_HOST,
       isChallengeTimerEnable: true,
       MESSAGE_DISPLAY_DURATION: 1500,
       isMobileSimulationEnabled: false,
