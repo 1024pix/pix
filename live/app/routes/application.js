8b00354d94db64d5e6a8c28b4806d90604b02139
Brandone Martins <brandone.martins@gmail.com>
2018-07-27 11:38:50 +0200

    [pf-302] move code error 401 from api management to error route

--- live/app/routes/application.js
+++ live/app/routes/application.js
@@ -1,27 +1,12 @@
 import { inject as service } from '@ember/service';
-import _ from 'lodash';
 
 import BaseRoute from 'pix-live/routes/base-route';
 
 export default BaseRoute.extend({
-  session: service(),
   splash: service(),
 
   activate() {
     this.get('splash').hide();
   },
 
-  hasUnauthorizedError(errorEvent) {
-    const statusCode = _.get(errorEvent, 'errors[0].code');
-    return statusCode === 401;
-  },
-
-  actions: {
-    error: function(error) {
-      if (this.hasUnauthorizedError(error)) {
-        return this.get('session').invalidate()
-          .then(() => this.transitionTo('login'));
-      }
-    }
-  }
 });
