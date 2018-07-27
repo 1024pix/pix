62cc33bc18cf7c19190aff6eb17c10078337bc0a
Sébastien Roccaserra <sroccaserra@yahoo.com>
2018-07-27 16:51:21 +0200

    [pf-243] [MOB] Revue de code : méthode factory pour la création de validateurs
    Note : je n'ai pas pu mettre cette méthode dans Validator car elle
    dépend des classes ValidatorQ*, qui à leur tour dépendent de la classe
    Validator. Vu avec Antoine : on a mis la méthode dans la classe
    Challenge.
    
    Note : inclue aussi un changement de gestion de catch vu en revue
    collective.

--- api/lib/infrastructure/repositories/challenge-repository.js
+++ api/lib/infrastructure/repositories/challenge-repository.js
@@ -2,12 +2,6 @@ const airtable = require('../airtable');
 const serializer = require('../serializers/airtable/challenge-serializer');
 const Challenge = require('../../domain/models/Challenge');
 const Skill = require('../../domain/models/Skill');
-const Validator = require('../../domain/models/Validator');
-const ValidatorQCM = require('../../domain/models/ValidatorQCM');
-const ValidatorQCU = require('../../domain/models/ValidatorQCU');
-const ValidatorQROC = require('../../domain/models/ValidatorQROC');
-const ValidatorQROCMDep = require('../../domain/models/ValidatorQROCMDep');
-const ValidatorQROCMInd = require('../../domain/models/ValidatorQROCMInd');
 
 const challengeDatasource = require('../datasources/airtable/challenge-datasource');
 const skillDatasource = require('../datasources/airtable/skill-datasource');
@@ -20,28 +14,6 @@ const AIRTABLE_TABLE_NAME = 'Epreuves';
 function _getSkillDataObjects(challengeDataObject) {
   const skillDataObjectPromises = challengeDataObject.skillIds.map(skillDatasource.get);
   return Promise.all(skillDataObjectPromises);
-}
-
-function getValidatorForType({ challengeType, solution }) {
-  switch (challengeType) {
-    case 'QCU':
-      return new ValidatorQCU({ solution });
-
-    case 'QCM':
-      return new ValidatorQCM({ solution });
-
-    case 'QROC':
-      return new ValidatorQROC({ solution });
-
-    case 'QROCM-ind':
-      return new ValidatorQROCMInd({ solution });
-
-    case 'QROCM-dep':
-      return new ValidatorQROCMDep({ solution });
-
-    default:
-      return new Validator({ solution });
-  }
 }
 
 module.exports = {
@@ -86,7 +58,10 @@ module.exports = {
       .then((skillDataObjects) => {
         const skills = skillDataObjects.map((skillDataObject) => new Skill(skillDataObject));
         const solution = solutionAdapter.fromChallengeAirtableDataObject(challengeDataObject);
-        const validator = getValidatorForType({ challengeType: challengeDataObject.type, solution });
+        const validator = Challenge.createValidatorForChallengeType({
+          challengeType: challengeDataObject.type,
+          solution
+        });
 
         return new Challenge({
           id: challengeDataObject.id,
