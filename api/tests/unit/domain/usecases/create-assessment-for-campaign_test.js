47746bd8085a8a4066bc2f8cdc69c54df2ed7708
MelanieMEB <mboudard@octo.com>
2018-07-30 17:02:43 +0200

    [PF-294] Save a campaignParticipation when we create a SmartPlacement

--- api/tests/unit/domain/usecases/create-assessment-for-campaign_test.js
+++ api/tests/unit/domain/usecases/create-assessment-for-campaign_test.js
@@ -1,5 +1,6 @@
 const { expect, sinon, factory } = require('../../../test-helper');
 const usecases = require('../../../../lib/domain/usecases');
+const CampaignParticipation = require('../../../../lib/domain/models/CampaignParticipation');
 const { CampaignCodeError } = require('../../../../lib/domain/errors');
 
 describe('Unit | UseCase | create-assessment-for-campaign', () => {
@@ -7,64 +8,114 @@ describe('Unit | UseCase | create-assessment-for-campaign', () => {
   let sandbox;
   const availableCampaignCode = 'ABCDEF123';
   const campaignRepository = { getByCode: () => undefined };
+  const campaignParticipationRepository = { save: () => undefined };
   const assessmentRepository = { save: () => undefined };
 
   beforeEach(() => {
     sandbox = sinon.sandbox.create();
     sandbox.stub(campaignRepository, 'getByCode');
     sandbox.stub(assessmentRepository, 'save');
+    sandbox.stub(campaignParticipationRepository, 'save');
   });
 
   afterEach(() => {
     sandbox.restore();
   });
 
-  it('should throw an CampaignCodeError if code Campaign not exist', () => {
-    // given
-    campaignRepository.getByCode.resolves(null);
+  context('when campaignCode not exist', () => {
+    it('should throw an CampaignCodeError', () => {
+      // given
+      campaignRepository.getByCode.resolves(null);
 
-    // when
-    const promise = usecases.createAssessmentForCampaign({ assessment: {}, codeCampaign:availableCampaignCode,  campaignRepository, assessmentRepository });
+      // when
+      const promise = usecases.createAssessmentForCampaign({
+        assessment: {},
+        codeCampaign:availableCampaignCode,
+        campaignRepository,
+        assessmentRepository,
+        campaignParticipationRepository
+      });
 
-    // then
-    return expect(promise).to.be.rejectedWith(CampaignCodeError);
+      // then
+      return expect(promise).to.be.rejectedWith(CampaignCodeError);
+    });
+
   });
 
-  it('should save the campaign with name, userId, organizationId and generated code', () => {
-    // given
-    const campaign = factory.buildCampaign();
-    campaignRepository.getByCode.resolves(campaign);
-    const assessment = factory.buildAssessment({
-      type: 'SMART_PLACEMENT',
+  context('when campaignCode exist', () => {
+    let campaign;
+    let assessment;
+    let campaignParticipation;
+
+    beforeEach(() => {
+      // given
+      campaign = factory.buildCampaign({
+        id: 'campaignId',
+      });
+      assessment = factory.buildAssessment({
+        id: 'assessmentId',
+        type: 'SMART_PLACEMENT',
+      });
+
+      campaignParticipation = new CampaignParticipation({
+        assessmentId: assessment.id,
+        campaignId: campaign.id,
+      });
+
+      campaignRepository.getByCode.resolves(campaign);
+      assessmentRepository.save.resolves(assessment);
+      campaignParticipationRepository.save.resolves({});
     });
 
-    // when
-    const promise = usecases.createAssessmentForCampaign({ assessment, codeCampaign:availableCampaignCode,  campaignRepository, assessmentRepository });
+    it('should save the assessment', () => {
+      // when
+      const promise = usecases.createAssessmentForCampaign({
+        assessment,
+        codeCampaign:availableCampaignCode,
+        campaignRepository,
+        assessmentRepository,
+        campaignParticipationRepository
+      });
 
-    // then
-    return promise.then(() => {
-      expect(assessmentRepository.save).to.have.been.called;
-      expect(assessmentRepository.save).to.have.been.calledWith(assessment);
+      // then
+      return promise.then(() => {
+        expect(assessmentRepository.save).to.have.been.called;
+        expect(assessmentRepository.save).to.have.been.calledWith(assessment);
+      });
     });
+
+    it('should save a campaign-participation object', () => {
+      // when
+      const promise = usecases.createAssessmentForCampaign({
+        assessment,
+        codeCampaign:availableCampaignCode,
+        campaignRepository,
+        assessmentRepository,
+        campaignParticipationRepository
+      });
+
+      // then
+      return promise.then(() => {
+        expect(campaignParticipationRepository.save).to.have.been.called;
+        expect(campaignParticipationRepository.save).to.have.been.calledWith(campaignParticipation);
+      });
+    });
+
+    it('should return the newly created assessment', () => {
+      // when
+      const promise = usecases.createAssessmentForCampaign({
+        assessment,
+        codeCampaign:availableCampaignCode,
+        campaignRepository,
+        assessmentRepository,
+        campaignParticipationRepository
+      });
+
+      // then
+      return promise.then((assessmentCreated) => {
+        expect(assessmentCreated).to.deep.equal(assessment);
+      });
+    });
+
   });
-
-  it('should return the newly created assessment', () => {
-    // given
-    const campaign = factory.buildCampaign();
-    const assessment = factory.buildAssessment({
-      type: 'SMART_PLACEMENT',
-    });
-
-    campaignRepository.getByCode.resolves(campaign);
-    assessmentRepository.save.resolves(assessment);
-
-    // when
-    const promise = usecases.createAssessmentForCampaign({ assessment, codeCampaign:availableCampaignCode,  campaignRepository, assessmentRepository });
-
-    // then
-    return promise.then((assessmentCreated) => {
-      expect(assessmentCreated).to.deep.equal(assessment);
-    });
-  });
-
 });
