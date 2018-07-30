fde7754f05f439dbc70695d7814931774679c3ad
MelanieMEB <mboudard@octo.com>
2018-07-30 09:45:55 +0200

    [PF-300] Add missing comma

--- api/lib/infrastructure/repositories/certification-course-repository.js
+++ api/lib/infrastructure/repositories/certification-course-repository.js
@@ -18,7 +18,7 @@ function _toDomain(model) {
     birthdate: model.get('birthdate'),
     sessionId: model.get('sessionId'),
     externalId: model.get('externalId'),
-    isPublished: model.get('isPublished')
+    isPublished: model.get('isPublished'),
   });
 }
 
