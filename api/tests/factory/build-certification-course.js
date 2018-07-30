4a2f628741c4fe0d8fdea7bd516fe905c5c0cc75
MelanieMEB <mboudard@octo.com>
2018-07-30 10:27:03 +0200

    [PF-300] Add CertificationCourse Builder in repository tests

---
+++ api/tests/factory/build-certification-course.js
@@ -0,0 +1,32 @@
+const CertificationCourse = require('../../lib/domain/models/CertificationCourse');
+
+module.exports = function buildCertificationCourse({
+  id= 1,
+  userId= 1,
+  completedAt= new Date('2018-12-01'),
+  createdAt= new Date('2018-12-01'),
+  firstName= 'Prenom',
+  lastName= 'Nom',
+  birthplace= 'Paris',
+  birthdate= '21/03/1991',
+  sessionId= 1,
+  externalId= 'nomDeLaPersonne',
+  isPublished= false,
+} = {}) {
+
+  const certificationCourse = CertificationCourse.fromAttributes({
+    id,
+    userId,
+    completedAt,
+    createdAt,
+    firstName,
+    lastName,
+    birthplace,
+    birthdate,
+    sessionId,
+    externalId,
+    isPublished,
+  });
+
+  return certificationCourse;
+};
