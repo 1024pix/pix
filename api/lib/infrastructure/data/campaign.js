bbcb3c77c4bbdc93834fefeb14fc3f046aab5fb9
MelanieMEB <mboudard@octo.com>
2018-07-31 15:10:47 +0200

    [PF-294] Add link between bookshelf objects

--- api/lib/infrastructure/data/campaign.js
+++ api/lib/infrastructure/data/campaign.js
@@ -2,9 +2,20 @@ const Bookshelf = require('../bookshelf');
 
 require('./user');
 require('./organization');
+require('./campaign-participation');
+require('./assessment');
 
 module.exports = Bookshelf.model('Campaign', {
 
   tableName: 'campaigns',
+
+  campaignParticipations() {
+    return this.hasMany('CampaignParticipation', 'campaignId');
+  },
+
+  assessments: function() {
+    return this.belongsToMany('Assessment', 'campaingId').through('CampaignParticipation');
+  }
+
 
 });
