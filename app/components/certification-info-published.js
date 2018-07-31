41ec989d62963533ceb61c1506d4f3d5607f9002
Benoit <benoit.prady@pix.fr>
2018-07-31 19:42:42 +0200

    Update certification-info-published component in order to reuse it in certif info

--- app/components/certification-info-published.js
+++ app/components/certification-info-published.js
@@ -2,7 +2,9 @@ import Component from '@ember/component';
 import {computed} from '@ember/object';
 
 export default Component.extend({
-  classNames: ['certification-list-published'],
+  classNames: ['certification-info-published'],
+  classNameBindings: ['float:certification-info-published-float'],
+
   color:computed('record.isPublished', function() {
     let value = this.get('record.isPublished');
     return value?'#7F7':'#777';
--- tests/integration/components/certification-info-published-test.js
+++ tests/integration/components/certification-info-published-test.js
@@ -3,14 +3,14 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | certification-list-published', function(hooks) {
+module('Integration | Component | certification-info-published', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{certification-list-published}}`);
+    await render(hbs`{{certification-info-published}}`);
 
     assert.dom('svg').exists();
   });
