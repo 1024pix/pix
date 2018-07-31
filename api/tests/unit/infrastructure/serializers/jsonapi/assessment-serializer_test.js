cdade862e49ed9f02ea5a8d592e3f198a4200ad0
MelanieMEB <mboudard@octo.com>
2018-07-31 17:06:31 +0200

    [PF-294] Add and clean tests

--- api/tests/unit/infrastructure/serializers/jsonapi/assessment-serializer_test.js
+++ api/tests/unit/infrastructure/serializers/jsonapi/assessment-serializer_test.js
@@ -1,7 +1,7 @@
 const { expect, factory } = require('../../../../test-helper');
 const serializer = require('../../../../../lib/infrastructure/serializers/jsonapi/assessment-serializer');
-const BookshelfAssessment = require('../../../../../lib/infrastructure/data/assessment');
 const Assessment = require('../../../../../lib/domain/models/Assessment');
+const Campaign = require('../../../../../lib/domain/models/Campaign');
 
 describe('Unit | Serializer | JSONAPI | assessment-serializer', function() {
 
@@ -17,10 +17,9 @@ describe('Unit | Serializer | JSONAPI | assessment-serializer', function() {
       name: 'PIX EST FORMIDABLE',
     };
 
-    modelObject = new BookshelfAssessment({
+    modelObject = new Assessment({
       id: 'assessment_id',
       courseId: 'course_id',
-      successRate: 24,
       type: 'charade',
       course: associatedCourse,
     });
@@ -32,7 +31,7 @@ describe('Unit | Serializer | JSONAPI | assessment-serializer', function() {
         attributes: {
           'estimated-level': undefined,
           'pix-score': undefined,
-          'success-rate': 24,
+          'success-rate': undefined,
           'type': 'charade',
           'certification-number': null,
         },
@@ -43,6 +42,9 @@ describe('Unit | Serializer | JSONAPI | assessment-serializer', function() {
               id: 'course_id',
             },
           },
+          answers: {
+            data: []
+          }
         },
       },
       included: [{
@@ -101,6 +103,19 @@ describe('Unit | Serializer | JSONAPI | assessment-serializer', function() {
         .and.to.contain.key('skill-review');
 
       expect(json.data.relationships['skill-review']).to.deep.equal(expectedSkillReviewRelationship);
+    });
+
+    it('should add campaign-code when the model has a campaign', function() {
+      // given
+      const codeCampaign = 'CODECAMP';
+      modelObject.campaign = new Campaign({ code: codeCampaign });
+      jsonAssessment.data.attributes['code-campaign'] = codeCampaign;
+
+      // when
+      const json = serializer.serialize(modelObject);
+
+      // then
+      expect(json).to.deep.equal(jsonAssessment);
     });
 
   });
