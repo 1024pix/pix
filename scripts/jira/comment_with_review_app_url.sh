67d6ed99851313a68b882ce1f5715b448e4d7238
Thomas Wickham <mackwic@gmail.com>
2018-08-01 16:18:48 +0200

    [pf-323]  better comments for JIRA and github

--- scripts/jira/comment_with_review_app_url.sh
+++ scripts/jira/comment_with_review_app_url.sh
@@ -25,12 +25,14 @@ fi
 
 ISSUE_COMMENTS=$(echo $RESPONSE | jq .comments[].body)
 PR_NUMBER=`echo $CI_PULL_REQUEST | grep -Po '(?<=pix/pull/)(\d+)'`
-REVIEW_APP_URL="https://pix-mon-pix-integration-pr$PR_NUMBER.scalingo.io"
+RA_MON_PIX_URL="https://pix-mon-pix-integration-pr$PR_NUMBER.scalingo.io"
+RA_ORGA_URL="https://pix-orga-integration-pr$PR_NUMBER.scalingo.io"
+RA_API_URL="https://pix-api-integration-pr$PR_NUMBER.scalingo.io"
 
-if [[ $ISSUE_COMMENTS =~ .*$REVIEW_APP_URL.* ]]
+if [[ $ISSUE_COMMENTS =~ .*$RA_MON_PIX_URL.* ]]
 then
     echo "Review app url already found in issue comments. No need to add it again"
 else
-    TEXT="Je m'apprête à déployer la Review App. Elle sera consultable sur $REVIEW_APP_URL"
+    TEXT="Je m'apprête à déployer la Review App. Elle sera consultable sur les URL suivantes:\n- Mon Pix: $RA_MON_PIX_URL\n- Orga: $RA_ORGA_URL\n- API: $RA_API_URL"
     curl -X POST "$API_URL/issue/$ISSUE_ID/comment" --header 'Content-Type: application/json' --fail --user $CREDENTIALS --data '{"body": "'"$TEXT"'"}'
 fi
