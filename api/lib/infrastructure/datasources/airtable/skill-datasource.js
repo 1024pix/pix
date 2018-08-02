373fe916d329d959ebf12f36b62c2bc84ef92490
Brandone Martins <brandone.martins@gmail.com>
2018-08-02 11:20:15 +0200

    [po-101] implemenet datasource to retrieve skills by an array of ids

--- api/lib/infrastructure/datasources/airtable/skill-datasource.js
+++ api/lib/infrastructure/datasources/airtable/skill-datasource.js
@@ -7,6 +7,23 @@ module.exports = {
   get(id) {
     return airtable.getRecord(AIRTABLE_TABLE_NAME, id)
       .then((airtableRawObject) => airTableDataObjects.Skill.fromAirTableObject(airtableRawObject));
+  },
+
+  findByRecordIds(skillRecordIds) {
+
+    const listOfAirtableFilters = skillRecordIds.map((recordId) => {
+      return `FIND("${recordId}", {Record Id})`;
+    });
+
+    const query = { filterByFormula: `OR(${listOfAirtableFilters.join(', ')})` };
+
+    return airtable.findRecords(AIRTABLE_TABLE_NAME, query)
+      .then((airtableSkillsRawObjectsArray) => {
+        return airtableSkillsRawObjectsArray
+          .map((airTableSkill) => {
+            return airTableDataObjects.Skill.fromAirTableObject(airTableSkill);
+          });
+      });
   }
 };
 
