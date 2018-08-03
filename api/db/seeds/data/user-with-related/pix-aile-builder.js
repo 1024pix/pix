d1c3980174aa455ccc15531467e9f40a3cd35767
Antoine Boileau <apem.boileau@gmail.com>
2018-08-03 18:38:05 +0200

    [pf-324] Add new way to add seeds

---
+++ api/db/seeds/data/user-with-related/pix-aile-builder.js
@@ -0,0 +1,21 @@
+module.exports = function addPixAileUserAndRelations({ databaseBuilder }) {
+
+  const pixAile = databaseBuilder.factory.buildUser({
+    id: 1,
+    firstName: 'Pix',
+    lastName: 'Aile',
+    email: 'userpix1@example.net',
+    password: 'pix123',
+    cgu: true,
+  });
+
+  databaseBuilder.factory.buildAssessment({
+    id: 1,
+    courseId: 'recyochcrrSOALQPS',
+    createdAt: new Date('2018-02-15T15:00:34'),
+    updatedAt: new Date('2018-02-15T15:00:34'),
+    userId: pixAile.id,
+    type: 'PLACEMENT',
+    state: 'completed',
+  });
+};
