577e3f99609e4db50979ece437953d6b2c1363a7
Brandone Martins <brandone.martins@gmail.com>
2018-08-07 16:49:27 +0200

    [po-101] --amend

--- api/tests/factory/build-target-profile.js
+++ api/tests/factory/build-target-profile.js
@@ -1,5 +1,5 @@
 const TargetProfile = require('../../lib/domain/models/TargetProfile');
 
-module.exports = function buildTargetProfile({ name, isPublic, skills = [] } = {}) {
-  return new TargetProfile({ name, isPublic, skills });
+module.exports = function buildTargetProfile({ name, isPublic, skills = [], organizationId } = {}) {
+  return new TargetProfile({ name, isPublic, skills, organizationId });
 };
