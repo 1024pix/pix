bd5efa16a6a4516fc5e34ba0fb6a6ec86eaf6916
Mathieu Laurent <mlaurent@octo.com>
2018-08-07 12:06:57 +0200

    [po-22] load current organization after authentication

--- orga/app/routes/application.js
+++ orga/app/routes/application.js
@@ -5,21 +5,37 @@ import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mi
 export default Route.extend(ApplicationRouteMixin, {
 
   routeAfterAuthentication: 'authenticated',
-  currentUser: service('current-user'),
+  currentUser: service(),
+  currentOrganization: service(),
 
   beforeModel() {
-    return this._loadCurrentUser();
+    return this._loadInitialData();
   },
 
   sessionAuthenticated() {
-    this._super(...arguments);
-    this._loadCurrentUser();
+    this._loadInitialData()
+      .then(() => {
+        // Because ember-simple-auth does not support calling this._super() asynchronously,
+        // we have to do this hack to call the original "sessionAuthenticated"
+        let mixin = ApplicationRouteMixin.mixins[0];
+        mixin.properties.sessionAuthenticated.call(this);
+      });
+  },
+
+  _loadInitialData() {
+    return this._loadCurrentUser()
+      .then((user) => this._loadCurrentOrganization(user));
   },
 
   _loadCurrentUser() {
-    return this.get('currentUser').load().catch((error) => {
-      this.get('session').invalidate();
-      throw error;
-    });
+    return this.get('currentUser').load()
+      .catch((error) => {
+        this.get('session').invalidate();
+        throw error;
+      });
+  },
+
+  _loadCurrentOrganization(user) {
+    return this.get('currentOrganization').load(user);
   }
 });
