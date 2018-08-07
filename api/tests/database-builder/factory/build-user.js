c639d50157e0ca5bb79d698476ad95f0f3828bdb
Antoine Boileau <apem.boileau@gmail.com>
2018-08-07 16:59:19 +0200

    [pf-324] Correct PR comments

--- api/tests/database-builder/factory/build-user.js
+++ api/tests/database-builder/factory/build-user.js
@@ -2,7 +2,7 @@ const databaseBuffer = require('../database-buffer');
 const faker = require('faker');
 const encrypt = require('../../../lib/domain/services/encryption-service');
 
-module.exports = function buildUser({
+const buildUser = function buildUser({
   id = faker.random.number(),
   firstName = faker.name.firstName(),
   lastName = faker.name.lastName(),
@@ -23,3 +23,27 @@ module.exports = function buildUser({
   return values;
 };
 
+buildUser.withUnencryptedPassword = function buildUserWithUnencryptedPassword({
+  id = faker.random.number(),
+  firstName = faker.name.firstName(),
+  lastName = faker.name.lastName(),
+  email = faker.internet.email(),
+  rawPassword = faker.internet.password(),
+  cgu = true,
+}) {
+
+  const password = encrypt.hashPasswordSync(rawPassword);
+
+  const values = {
+    id, firstName, lastName, email, password, cgu,
+  };
+
+  databaseBuffer.pushInsertable({
+    tableName: 'users',
+    values,
+  });
+
+  return values;
+};
+
+module.exports = buildUser;
