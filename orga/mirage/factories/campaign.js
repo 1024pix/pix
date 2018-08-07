35e87e23997e16b8305c2f7f69ceca3e1d236bed
Mathieu Laurent <mlaurent@octo.com>
2018-08-07 14:40:54 +0200

    [po-22] add campaigns to mirage

---
+++ orga/mirage/factories/campaign.js
@@ -0,0 +1,13 @@
+import { Factory, faker } from 'ember-cli-mirage';
+
+export default Factory.extend({
+
+  name() {
+    return faker.company.companyName();
+  },
+
+  code() {
+    return faker.address.zipCode();
+  },
+
+});
