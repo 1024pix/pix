0c3813f8fe10d789cee25c7a40d70d93afae86e6
Mathieu Laurent <mlaurent@octo.com>
2018-08-07 14:20:03 +0200

    [po-22] display the campaigns of the current organization

--- orga/app/routes/authenticated/campaigns/list.js
+++ orga/app/routes/authenticated/campaigns/list.js
@@ -1,4 +1,15 @@
 import Route from '@ember/routing/route';
 
+import { inject as service } from '@ember/service';
+
 export default Route.extend({
+
+  currentOrganization: service(),
+
+  model() {
+    return this.currentOrganization.organization
+      .then((organization) => {
+        return organization.get('campaigns');
+      })
+  }
 });
