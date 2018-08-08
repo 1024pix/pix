bec2881565aef5f9c48a60a6110ccc87a1e788bc
MelanieMEB <mboudard@octo.com>
2018-08-08 11:15:34 +0200

    [PO-124] Correct test and bugs

--- api/tests/tooling/database-builder/factory/index.js
+++ api/tests/tooling/database-builder/factory/index.js
@@ -1,6 +1,8 @@
 module.exports = {
   buildAnswer: require('./build-answer'),
   buildAssessment: require('./build-assessment'),
+  buildCampaign: require('./build-campaign'),
+  buildCampaignParticipation: require('./build-campaign-participation'),
   buildOrganization: require('./build-organization'),
   buildSmartPlacementKnowledgeElement: require('./build-smart-placement-knowledge-element'),
   buildTargetProfile: require('./build-target-profile'),
