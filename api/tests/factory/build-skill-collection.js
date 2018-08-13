5b675fa4f068a9cd9a007681ba52320ba522d66e
Antoine Boileau <apem.boileau@gmail.com>
2018-08-13 15:03:44 +0200

    [pf-330] Fix collision on skill names for tests

--- api/tests/factory/build-skill-collection.js
+++ api/tests/factory/build-skill-collection.js
@@ -1,15 +1,14 @@
-const faker = require('faker');
 const buildSkill = require('./build-skill');
 
 module.exports = function BuildSkillCollection({
-  name = faker.lorem.word(),
+  name = buildSkill.buildRandomTubeName(),
   minLevel = 3,
   maxLevel = 5,
 } = {}) {
   const collection = [];
 
   for (let i = minLevel; i <= maxLevel; i += 1) {
-    collection.push(buildSkill({ name: `@${name}${i}` }));
+    collection.push(buildSkill({ name: `${name}${i}` }));
   }
 
   return collection;
