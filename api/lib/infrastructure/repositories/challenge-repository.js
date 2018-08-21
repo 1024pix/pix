5b5f62849b961b32034cf325455c1d6e0a2f1ecc
Antoine Boileau <apem.boileau@gmail.com>
2018-08-21 11:33:25 +0200

    [pf-311] Change findByCompetence function in challengeRepository to better toDomain function

--- api/lib/infrastructure/repositories/challenge-repository.js
+++ api/lib/infrastructure/repositories/challenge-repository.js
@@ -11,11 +11,6 @@ const { NotFoundError } = require('../../domain/errors');
 
 const AIRTABLE_TABLE_NAME = 'Epreuves';
 
-function _getSkillDataObjects(challengeDataObject) {
-  const skillDataObjectPromises = challengeDataObject.skillIds.map(skillDatasource.get);
-  return Promise.all(skillDataObjectPromises);
-}
-
 module.exports = {
 
   list() {
@@ -23,9 +18,22 @@ module.exports = {
       .then((airtableChallenges) => airtableChallenges.map(serializer.deserialize));
   },
 
+  get(id) {
+    return challengeDatasource.get(id)
+      .then(_convertChallengeDatasourceToDomain)
+      .catch((error) => {
+        if (error instanceof airtableDatasourceObjects.AirtableResourceNotFound) {
+          throw new NotFoundError();
+        }
+
+        throw error;
+      });
+  },
+
   findByCompetence(competence) {
-    return airtable.findRecords(AIRTABLE_TABLE_NAME, { view: competence.reference })
-      .then((airtableChallenges) => airtableChallenges.map(serializer.deserialize));
+
+    return challengeDatasource.findByCompetence(competence)
+      .then((challengeDataObjects) => Promise.all(challengeDataObjects.map(_convertChallengeDatasourceToDomain)));
   },
 
   findBySkills(skills) {
@@ -49,41 +57,42 @@ module.exports = {
       });
   },
 
-  get(id) {
-    let challengeDataObject;
-
-    return challengeDatasource.get(id)
-      .then((result) => challengeDataObject = result)
-      .then(_getSkillDataObjects)
-      .then((skillDataObjects) => {
-        const skills = skillDataObjects.map((skillDataObject) => new Skill(skillDataObject));
-        const solution = solutionAdapter.fromChallengeAirtableDataObject(challengeDataObject);
-        const validator = Challenge.createValidatorForChallengeType({
-          challengeType: challengeDataObject.type,
-          solution
-        });
-
-        return new Challenge({
-          id: challengeDataObject.id,
-          type: challengeDataObject.type,
-          instruction: challengeDataObject.instruction,
-          proposals: challengeDataObject.proposals,
-          timer: challengeDataObject.timer,
-          illustrationUrl: challengeDataObject.illustrationUrl,
-          attachments: challengeDataObject.attachments,
-          skills,
-          embedUrl: challengeDataObject.embedUrl,
-          embedTitle: challengeDataObject.embedTitle,
-          embedHeight: challengeDataObject.embedHeight,
-          validator,
-        });
-      })
-      .catch((error) => {
-        if (error instanceof airtableDatasourceObjects.AirtableResourceNotFound) {
-          throw new NotFoundError();
-        }
-
-        throw error;
-      });
-  },
 };
+
+function _convertChallengeDatasourceToDomain(challengeDataObject) {
+
+  return Promise.resolve(challengeDataObject)
+    .then(_getSkillDataObjects)
+    .then((skillDataObjects) => {
+      const skills = skillDataObjects.map((skillDataObject) => new Skill({
+        id: skillDataObject.id,
+        name: skillDataObject.name,
+      }));
+      const solution = solutionAdapter.fromChallengeAirtableDataObject(challengeDataObject);
+      const validator = Challenge.createValidatorForChallengeType({
+        challengeType: challengeDataObject.type,
+        solution,
+      });
+
+      return new Challenge({
+        id: challengeDataObject.id,
+        type: challengeDataObject.type,
+        status: challengeDataObject.status,
+        instruction: challengeDataObject.instruction,
+        proposals: challengeDataObject.proposals,
+        timer: challengeDataObject.timer,
+        illustrationUrl: challengeDataObject.illustrationUrl,
+        attachments: challengeDataObject.attachments,
+        skills,
+        embedUrl: challengeDataObject.embedUrl,
+        embedTitle: challengeDataObject.embedTitle,
+        embedHeight: challengeDataObject.embedHeight,
+        validator,
+      });
+    });
+}
+
+function _getSkillDataObjects(challengeDataObject) {
+  const skillDataObjectPromises = challengeDataObject.skillIds.map(skillDatasource.get);
+  return Promise.all(skillDataObjectPromises);
+}
