2ef67efa9770130d9d64206cb8aea57fce1fc579
Antoine Boileau <apem.boileau@gmail.com>
2018-08-21 16:09:50 +0200

    [pf-311] BSR on target profile

--- api/lib/infrastructure/repositories/target-profile-repository.js
+++ api/lib/infrastructure/repositories/target-profile-repository.js
@@ -1,7 +1,7 @@
-const TargetProfile = require('../../domain/models/TargetProfile');
 const BookshelfTargetProfile = require('../../infrastructure/data/target-profile');
 const skillDatasource = require('../../infrastructure/datasources/airtable/skill-datasource');
 const Skill = require('../../domain/models/Skill');
+const TargetProfile = require('../../domain/models/TargetProfile');
 
 module.exports = {
 
@@ -13,7 +13,8 @@ module.exports = {
       .then((bookshelfTargetProfile) => {
         return _fetchTargetProfileSkillDataObjects(bookshelfTargetProfile)
           .then((skillDataObjects) => {
-            return _convertDataObjectsIntoDomain(bookshelfTargetProfile, skillDataObjects);
+
+            return _toDomain(bookshelfTargetProfile, skillDataObjects);
           });
       });
   },
@@ -28,7 +29,8 @@ module.exports = {
 
           return _fetchTargetProfileSkillDataObjects(bookshelfTargetProfile)
             .then((skillDataObjects) => {
-              return _convertDataObjectsIntoDomain(bookshelfTargetProfile, skillDataObjects);
+
+              return _toDomain(bookshelfTargetProfile, skillDataObjects);
             });
         });
 
@@ -46,44 +48,40 @@ module.exports = {
 
           return _fetchTargetProfileSkillDataObjects(bookshelfTargetProfile)
             .then((skillDataObjects) => {
-              return _convertDataObjectsIntoDomain(bookshelfTargetProfile, skillDataObjects);
+
+              return _toDomain(bookshelfTargetProfile, skillDataObjects);
             });
         });
 
         return Promise.all(promises);
       });
   },
-
 };
 
 function _fetchTargetProfileSkillDataObjects(bookshelfTargetProfile) {
   const skillRecordIds = bookshelfTargetProfile
     .related('skillIds')
     .map((BookshelfSkillId) => BookshelfSkillId.get('skillId'));
+
   return skillDatasource.findByRecordIds(skillRecordIds);
 }
 
-function _convertDataObjectsIntoDomain(bookshelfTargetProfile, skillAssociatedToTargetProfileWIthName) {
-  const targetProfile = _toDomain(bookshelfTargetProfile);
-  targetProfile.skills = _toDomainSkills(skillAssociatedToTargetProfileWIthName);
-  return targetProfile;
-}
+function _toDomain(targetProfileBookshelf, associatedSkillDataObjects) {
 
-function _toDomain(targetProfileBookshelf) {
+  const associatedSkills = associatedSkillDataObjects.map(_toDomainSkill);
+
   return new TargetProfile({
     id: targetProfileBookshelf.get('id'),
     name: targetProfileBookshelf.get('name'),
     isPublic: Boolean(targetProfileBookshelf.get('isPublic')),
     organizationId: targetProfileBookshelf.get('organizationId'),
-    skills: []
+    skills: associatedSkills,
   });
 }
 
-function _toDomainSkills(skillsDataObjects) {
-  return skillsDataObjects.map((skillDataObject) => {
-    return new Skill({
-      id: skillDataObject.id,
-      name: skillDataObject.name
-    });
+function _toDomainSkill(skillDataObject) {
+  return new Skill({
+    id: skillDataObject.id,
+    name: skillDataObject.name,
   });
 }
