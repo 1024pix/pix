3232d3131f40f2bacaf969f32eb95fa9809f664c
Antoine Boileau <apem.boileau@gmail.com>
2018-08-22 12:06:34 +0200

    [pf-311] Fix database builders for target profile

--- api/tests/tooling/database-builder/factory/build-target-profiles-skills.js
+++ api/tests/tooling/database-builder/factory/build-target-profiles-skills.js
@@ -1,21 +1,23 @@
+const buildTargetProfile = require('./build-target-profile');
 const databaseBuffer = require('../database-buffer');
 const faker = require('faker');
 
 module.exports = function buildTargetProfilesSkills({
   id = faker.random.number(),
-  targetProfileId = faker.random.number(),
-  skillId = 'rec' + faker.random.word()
+  targetProfileId = buildTargetProfile().id,
+  skillId = `rec${faker.random.uuid()}`,
 } = {}) {
 
   const values = {
-    id, targetProfileId, skillId
+    id,
+    targetProfileId,
+    skillId,
   };
 
   databaseBuffer.pushInsertable({
     tableName: 'target-profiles_skills',
     values,
   });
-
+  
   return values;
 };
-
