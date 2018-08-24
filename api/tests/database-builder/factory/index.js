d46fa594e08470150d94e94b873cf6ddce0ea316
Brandone Martins <brandone.martins@gmail.com>
2018-08-24 14:42:22 +0200

    [po-123] Add campaign factory database builder

--- api/tests/database-builder/factory/index.js
+++ api/tests/database-builder/factory/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   buildAnswer: require('./build-answer'),
   buildAssessment: require('./build-assessment'),
+  buildCampaign: require('./build-campaign'),
   buildOrganization: require('./build-organization'),
   buildOrganizationAccess: require('./build-organization-access'),
   buildSmartPlacementKnowledgeElement: require('./build-smart-placement-knowledge-element'),
