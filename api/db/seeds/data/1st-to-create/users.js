b53e76689c5a5c4b610122b7f5f97c6cc0401088
MelanieMEB <mboudard@octo.com>
2018-08-28 14:57:20 +0200

    [PO-121] Change seed

--- api/db/seeds/data/1st-to-create/users.js
+++ api/db/seeds/data/1st-to-create/users.js
@@ -22,7 +22,6 @@ function createUser(id, firstName, lastName, email, password) {
 }
 
 module.exports = [
-  createUser(2,'Daenerys','Targaryen', 'pro@example.net','pix123'),
   createUser(3,'Tyrion','Lannister', 'sup@example.net','pix123'),
   createUser(4,'John','Snow', 'sco@example.net','pix123'),
   createUser(5,'Pix','Master', 'pixmaster@example.net','pix123'),
