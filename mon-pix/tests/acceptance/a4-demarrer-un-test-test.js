78ab7de3699f50f108b5e0d5a14f3de294d15056
MelanieMEB <mboudard@octo.com>
2018-09-06 16:33:57 +0200

    [PF-336] Remove old test

--- mon-pix/tests/acceptance/a4-demarrer-un-test-test.js
+++ mon-pix/tests/acceptance/a4-demarrer-un-test-test.js
@@ -1,4 +1,3 @@
-import { later } from '@ember/runloop';
 import {
   describe,
   it,
@@ -12,7 +11,6 @@ import { authenticateAsSimpleUser } from '../helpers/testing';
 import defaultScenario from '../../mirage/scenarios/default';
 
 const URL_OF_FIRST_TEST = '/assessments/ref_assessment_id/challenges/ref_qcm_challenge_id';
-const MODAL_SELECTOR = '.modal.fade.js-modal-mobile.in';
 const START_BUTTON = '.competence-level-progress-bar__link-start';
 
 describe('Acceptance | a4 - Démarrer un test |', function() {
@@ -42,57 +40,6 @@ describe('Acceptance | a4 - Démarrer un test |', function() {
     return click($startLink).then(function() {
       findWithAssert('.assessment-challenge');
       expect(currentURL()).to.contain(URL_OF_FIRST_TEST);
-    });
-  });
-
-  it('a4.3 Quand je démarre un test sur mobile, une modale m\'averti que l\'expérience ne sera pas optimale, mais je peux quand même continuer', function(done) {
-    const $startLink = findWithAssert(START_BUTTON);
-
-    expect(find(MODAL_SELECTOR)).to.have.lengthOf(0);
-
-    // test on mobile
-    triggerEvent('.competence-list', 'simulateMobileScreen');
-
-    // clear local storage
-    andThen(() => {
-      window.localStorage.clear();
-      expect(currentURL()).to.equals('/');
-      expect(find(MODAL_SELECTOR)).to.have.lengthOf(0);
-    });
-
-    // start a test
-    click($startLink);
-
-    // blocked by modal
-    andThen(() => {
-      // XXX : ickiest hack : wait 500ms for bootstrap transition to complete
-      later(function() {
-        expect(find(MODAL_SELECTOR)).to.have.lengthOf(1);
-        expect(currentURL()).to.equals('/');
-        find('a[data-dismiss]').click();
-
-        return click($startLink).then(() => {
-          expect(currentURL()).to.contain(URL_OF_FIRST_TEST);
-          done();
-        });
-      }, 500);
-    });
-  });
-
-  it('a4.4 Quand je RE-démarre un test sur mobile, la modale NE s\'affiche PAS', function(done) {
-    const $startLink = findWithAssert(START_BUTTON);
-    triggerEvent('.competence-list', 'simulateMobileScreen');
-
-    andThen(() => {
-      later(function() {
-        expect(currentURL()).to.equals('/compte');
-        expect(find(MODAL_SELECTOR)).to.have.lengthOf(0);
-      }, 500);
-    });
-    click($startLink);
-    andThen(() => {
-      expect(currentURL()).to.contain('/assessments/ref_assessment_id/challenges/ref_qcm_challenge_id');
-      done();
     });
   });
 
