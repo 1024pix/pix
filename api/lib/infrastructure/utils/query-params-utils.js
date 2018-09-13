71aa116886f894561ae6a8539b0d9d51d584b284
Jérémy Buget <jbuget@gmail.com>
2018-09-14 00:15:49 +0200

    [pa-46] Improve #extractFilters method in 'query-params-utils' module in order to support case when the request query params does not contain "filter[xxx]" param

--- api/lib/infrastructure/utils/query-params-utils.js
+++ api/lib/infrastructure/utils/query-params-utils.js
@@ -2,9 +2,12 @@ const _ = require('lodash');
 
 function extractFilters(request) {
   return _.reduce(request.query, (result, queryFilterValue, queryFilterKey) => {
-    const field = queryFilterKey.match(/filter\[([a-zA-Z]*)]/)[1];
-    if (field) {
-      result[field] = queryFilterValue;
+    const filter = queryFilterKey.match(/filter\[([a-zA-Z]*)]/);
+    if (filter) {
+      const field = filter[1];
+      if (field) {
+        result[field] = queryFilterValue;
+      }
     }
     return result;
   }, {});
