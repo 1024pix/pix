a5aed9d883bd36a8cd24b8d62934c1d80a83c093
Mathieu Laurent <mlaurent@octo.com>
2018-09-21 16:26:26 +0200

    [pf-390] manage campaign not found in get campaign by code use case

--- api/lib/domain/usecases/get-campaign-by-code.js
+++ api/lib/domain/usecases/get-campaign-by-code.js
@@ -1,3 +1,11 @@
+const { NotFoundError } = require('../../domain/errors');
+
 module.exports = function getCampaignByCode({ code, campaignRepository }) {
-  return campaignRepository.getByCode(code);
+  return campaignRepository.getByCode(code)
+    .then((campaign) => {
+      if(campaign === null) {
+        return Promise.reject(new NotFoundError(`Not found campaign with code ${code}`));
+      }
+      return campaign;
+    });
 };
