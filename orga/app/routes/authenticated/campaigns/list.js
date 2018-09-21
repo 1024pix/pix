c55a9250881e71ac39c234df8ddba5c10f9e0699
Jérémy Buget <jbuget@gmail.com>
2018-09-21 12:05:09 +0200

    [po-119] Add a FIXME comment to improve the way organization's campaign are fetched in Pix Orga

--- orga/app/routes/authenticated/campaigns/list.js
+++ orga/app/routes/authenticated/campaigns/list.js
@@ -7,6 +7,8 @@ export default Route.extend({
   currentOrganization: service(),
 
   model() {
+    // FIXME Maybe we should fetch organizations directly from API rather than using Ember Data relationships mechanisms and even make API requests
+    // FIXME And then the model force reload in the `#afterModel` hook would be useless
     return this.currentOrganization.organization
       .then((organization) => organization.get('campaigns'));
   },
