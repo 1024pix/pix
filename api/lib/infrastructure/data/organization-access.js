47fead3b909686ceeb3c0ae12865ef4c7c36d1f3
Jérémy Buget <jbuget@gmail.com>
2018-09-25 18:48:16 +0200

    [pa-39] Add user relationship to OrganizationAccess

--- api/lib/infrastructure/data/organization-access.js
+++ api/lib/infrastructure/data/organization-access.js
@@ -1,11 +1,16 @@
 const Bookshelf = require('../bookshelf');
 
+require('./user');
 require('./organization');
 require('./organization-role');
 
 module.exports = Bookshelf.model('OrganizationAccess', {
 
   tableName: 'organizations-accesses',
+
+  user() {
+    return this.belongsTo('User', 'userId');
+  },
 
   organization() {
     return this.belongsTo('Organization', 'organizationId');
