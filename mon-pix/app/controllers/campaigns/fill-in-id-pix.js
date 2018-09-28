999005f5019ed86c079d56c666a51904dd978e4c
Melanie B <mboudard@octo.com>
2018-09-28 13:54:17 +0200

    [PF-271] Add loader on starter Button

--- mon-pix/app/controllers/campaigns/fill-in-id-pix.js
+++ mon-pix/app/controllers/campaigns/fill-in-id-pix.js
@@ -6,8 +6,10 @@ export default Controller.extend({
 
     submit() {
       this.set('model.errorMessage', null);
+
       const participantExternalId = this.get('model.participantExternalId');
       if (participantExternalId) {
+        this.set('model.loading', true);
         return this.get('start')(this.get('model.campaignCode'), participantExternalId);
       } else {
         return this.set('model.errorMessage', `Merci de renseigner votre ${this.get('model.idPixLabel')}.`);
