6aea76931fa67d307d4f9164fa3de471e901afb8
Mathieu Laurent <mlaurent@octo.com>
2018-10-02 15:45:08 +0200

    [PF-407] Update campaign participation serializer

--- api/tests/unit/infrastructure/serializers/jsonapi/campaign-participation-serializer_test.js
+++ api/tests/unit/infrastructure/serializers/jsonapi/campaign-participation-serializer_test.js
@@ -1,8 +1,54 @@
-const { expect } = require('../../../../test-helper');
+const { expect, factory } = require('../../../../test-helper');
 const serializer = require('../../../../../lib/infrastructure/serializers/jsonapi/campaign-participation-serializer');
 const CampaignParticipation  = require('../../../../../lib/domain/models/CampaignParticipation');
 
 describe('Unit | Serializer | JSONAPI | campaign-participation-serializer', function() {
+
+  describe('#serialize', function() {
+
+    it('should convert a CampaignParticipation model object into JSON API data', function() {
+      // given
+      const campaignParticipation = factory.buildCampaignParticipation({
+        id: 5,
+        isShared: true,
+        sharedAt: '2018-02-06 14:12:44',
+        campaignId: 12345,
+        assessmentId: 67890,
+      });
+
+      const expectedSerializedCampaignParticipation = {
+        data: {
+          type: 'campaign-participations',
+          id: 5,
+          attributes: {
+            'is-shared': true,
+            'shared-at': '2018-02-06 14:12:44',
+          },
+          relationships: {
+            assessment: {
+              data: {
+                id: '67890',
+                type: 'assessments'
+              }
+            },
+            campaign: {
+              data: {
+                id: '12345',
+                type: 'campaigns'
+              }
+            }
+          }
+        }
+      };
+
+      // when
+      const json = serializer.serialize(campaignParticipation);
+
+      // then
+      expect(json).to.deep.equal(expectedSerializedCampaignParticipation);
+    });
+
+  });
 
   describe('#deserialize', function() {
 
