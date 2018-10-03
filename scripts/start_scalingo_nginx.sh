3fe425e900ed43aae12a4a6af5c4da1bff8d75fc
Jonathan Perret <jonathan@ut7.fr>
2018-10-03 18:08:39 +0200

    Switch to a subdir buildpack, that moves everything up one directory
    The following commands need to be run (adjusting the app names) to adjust
    the Scalingo environment variables to deploy this correctly:
    
        scalingo env-set -a pix-app-integration BUILDPACK_URL=https://github.com/1024pix/subdir-buildpack
        scalingo env-set -a pix-app-integration BUILDPACK_SUBDIR=mon-pix
    
        scalingo env-set -a pix-orga-integration BUILDPACK_URL=https://github.com/1024pix/subdir-buildpack
        scalingo env-set -a pix-orga-integration BUILDPACK_SUBDIR=orga
    
        scalingo env-set -a pix-api-integration BUILDPACK_URL=https://github.com/1024pix/subdir-buildpack
        scalingo env-set -a pix-api-integration BUILDPACK_SUBDIR=api

--- scripts/start_scalingo_nginx.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-# XXX We absolutely need to be in the app directory so that the nginx script works
-cd /app
-# We do the normal nginx launch command of the nginx-buildpack
-/app/bin/run
