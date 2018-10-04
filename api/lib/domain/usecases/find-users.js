80a88634ef8873ec1b92c12c229e5d1d047145eb
Jérémy Buget <jbuget@gmail.com>
2018-10-04 09:33:15 +0200

    [pa-46] Review: improve variable naming

--- api/lib/domain/usecases/find-users.js
+++ api/lib/domain/usecases/find-users.js
@@ -1,15 +1,17 @@
 const SearchResultList = require('../models/SearchResultList');
 
 module.exports = function findUsers({ filters, pagination, userRepository }) {
+
   return Promise.all([
     userRepository.find(filters, pagination),
     userRepository.count(filters)
-  ]).then((values) => {
+  ]).then(([paginatedResults, totalResults]) => {
+
     return new SearchResultList({
       page: pagination.page,
       pageSize: pagination.pageSize,
-      totalResults: values[1],
-      paginatedResults: values[0]
+      totalResults,
+      paginatedResults
     });
   });
 };
