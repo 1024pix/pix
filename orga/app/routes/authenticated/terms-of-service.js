5a3c4ace143073ddcbb789996bdbedde109ce49d
lisequesnel <quesnel.lise@gmail.com>
2018-10-08 18:44:07 +0200

    [po-15] redirect to campaign list if terms of service have already been accepted

--- orga/app/routes/authenticated/terms-of-service.js
+++ orga/app/routes/authenticated/terms-of-service.js
@@ -1,9 +1,25 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+
+  currentUser: service(),
+
   renderTemplate() {
     this.render('authenticated.terms-of-service', {
     into: 'application'
     })
+  },
+
+  beforeModel() {
+    this.get('currentUser').load()
+      .then((user) => {
+        if (user.pixOrgaTermsOfServiceAccepted) {
+          return this.transitionTo('authenticated.campaigns.list');
+        }
+      })
+      .catch(() => {
+        return this.transitionTo('authenticated');
+      });
   }
 });
