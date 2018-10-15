f93948d0f18e17ce0d8edc84d34c339a8f79b271
Jonathan Perret <jonathan@ut7.fr>
2018-10-15 17:34:12 +0200

    [pf-415] Rewrite competence-tree-repository to reuse area-repository
    As the CompetenceTree is barely more than the list of areas, it
    seems to make more sense to not duplicate the area construction code.

--- api/tests/acceptance/application/certification-controller_test.js
+++ api/tests/acceptance/application/certification-controller_test.js
@@ -180,184 +180,176 @@ describe('Acceptance | API | Certifications', () => {
       nock.cleanAll();
 
       nock('https://api.airtable.com')
-        .get('/v0/test-base/Competences/recsvLz0W2ShyfD63')
+        .get('/v0/test-base/Competences')
         .query(true)
         .times(2)
         .reply(200, {
-          'id': 'recsvLz0W2ShyfD63',
-          'fields': {
-            'Domaine': [
-              'recvoGdo7z2z7pXWa',
-            ],
-            'Epreuves': [
-              'rec02tVrimXNkgaLD',
-              'rec0gm0GFue3PQB3k',
-              'rec0hoSlSwCeNNLkq',
-              'rec2FcZ4jsPuY1QYt',
-              'rec39bDMnaVw3MyMR',
-              'rec3FMoD8h9USTktb',
-              'rec3P7fvPSpFkIFLV',
-            ],
-            'Sous-domaine': '1.1',
-            'Titre': 'Mener une recherche et une veille d’information',
-            'Tests': [
-              'recNPB7dTNt5krlMA',
-            ],
-            'Acquis (identifiants)': [
-              'recV11ibSCXvaUzZd',
-              'recD01ptfJy7c4Sex',
-              'recfO8994EvSQV9Ip',
-              'recDMMeHSZRCjqo5x',
-              'reci0phtJi0lvqW9j',
-              'recUQSpjuDvwqKMst',
-              'recxqogrKZ9p8b1u8',
-              'recRV35kIeqUQj8cI',
-              'rec50NXHkatsRkjVQ',
-            ],
-            'Référence': '1.1 Mener une recherche et une veille d’information',
-            'Tests Record ID': [
-              'recNPB7dTNt5krlMA',
-            ],
-            'Acquis': [
-              '@url2',
-              '@url5',
-              '@utiliserserv6',
-              '@rechinfo1',
-              '@eval2',
-              '@publi4',
-              '@modèleEco2',
-              '@utiliserserv1',
-            ],
-            'Record ID': 'recsvLz0W2ShyfD63',
-            'Domaine Titre': [
-              'Information et données',
-            ],
-            'Domaine Code': [
-              '1',
-            ],
-          },
-          'createdTime': '2017-06-13T13:53:17.000Z',
-        });
-
-      nock('https://api.airtable.com')
-        .get('/v0/test-base/Competences/recNv8qhaY887jQb2')
-        .query(true)
-        .times(2)
-        .reply(200, {
-          'id': 'recNv8qhaY887jQb2',
-          'fields': {
-            'Domaine': [
-              'recvoGdo7z2z7pXWa',
-            ],
-            'Epreuves': [
-              'rec02tVrimXNkgaLD',
-              'rec0gm0GFue3PQB3k',
-              'rec0hoSlSwCeNNLkq',
-              'rec2FcZ4jsPuY1QYt',
-              'rec39bDMnaVw3MyMR',
-              'rec3FMoD8h9USTktb',
-              'rec3P7fvPSpFkIFLV',
-            ],
-            'Sous-domaine': '1.2',
-            'Titre': 'Gérer des données',
-            'Tests': [
-              'recNPB7dTNt5krlMA',
-            ],
-            'Acquis (identifiants)': [
-              'recV11ibSCXvaUzZd',
-              'recD01ptfJy7c4Sex',
-              'recfO8994EvSQV9Ip',
-              'recDMMeHSZRCjqo5x',
-              'reci0phtJi0lvqW9j',
-              'recUQSpjuDvwqKMst',
-              'recxqogrKZ9p8b1u8',
-              'recRV35kIeqUQj8cI',
-              'rec50NXHkatsRkjVQ',
-            ],
-            'Référence': '1.2 Gérer des données',
-            'Tests Record ID': [
-              'recNPB7dTNt5krlMA',
-            ],
-            'Acquis': [
-              '@url2',
-              '@url5',
-              '@utiliserserv6',
-              '@rechinfo1',
-              '@eval2',
-              '@publi4',
-              '@modèleEco2',
-              '@utiliserserv1',
-            ],
-            'Record ID': 'recNv8qhaY887jQb2',
-            'Domaine Titre': [
-              'Information et données',
-            ],
-            'Domaine Code': [
-              '1',
-            ],
-          },
-          'createdTime': '2017-06-13T13:53:17.000Z',
-        });
-
-      nock('https://api.airtable.com')
-        .get('/v0/test-base/Competences/recIkYm646lrGvLNT')
-        .query(true)
-        .times(2)
-        .reply(200, {
-          'id': 'recIkYm646lrGvLNT',
-          'fields': {
-            'Domaine': [
-              'recvoGdo7z2z7pXWa',
-            ],
-            'Epreuves': [
-              'rec02tVrimXNkgaLD',
-              'rec0gm0GFue3PQB3k',
-              'rec0hoSlSwCeNNLkq',
-              'rec2FcZ4jsPuY1QYt',
-              'rec39bDMnaVw3MyMR',
-              'rec3FMoD8h9USTktb',
-              'rec3P7fvPSpFkIFLV',
-            ],
-            'Sous-domaine': '1.3',
-            'Titre': 'Traiter des données',
-            'Tests': [
-              'recNPB7dTNt5krlMA',
-            ],
-            'Acquis (identifiants)': [
-              'recV11ibSCXvaUzZd',
-              'recD01ptfJy7c4Sex',
-              'recfO8994EvSQV9Ip',
-              'recDMMeHSZRCjqo5x',
-              'reci0phtJi0lvqW9j',
-              'recUQSpjuDvwqKMst',
-              'recxqogrKZ9p8b1u8',
-              'recRV35kIeqUQj8cI',
-              'rec50NXHkatsRkjVQ',
-            ],
-            'Référence': '1.3 Traiter des données',
-            'Tests Record ID': [
-              'recNPB7dTNt5krlMA',
-            ],
-            'Acquis': [
-              '@url2',
-              '@url5',
-              '@utiliserserv6',
-              '@rechinfo1',
-              '@eval2',
-              '@publi4',
-              '@modèleEco2',
-              '@utiliserserv1',
-            ],
-            'Record ID': 'recIkYm646lrGvLNT',
-            'Domaine Titre': [
-              'Information et données',
-            ],
-            'Domaine Code': [
-              '1',
-            ],
-          },
-          'createdTime': '2017-06-13T13:53:17.000Z',
-        });
+          records: [
+            {
+              'id': 'recsvLz0W2ShyfD63',
+              'fields': {
+                'Domaine': [
+                  'recvoGdo7z2z7pXWa',
+                ],
+                'Epreuves': [
+                  'rec02tVrimXNkgaLD',
+                  'rec0gm0GFue3PQB3k',
+                  'rec0hoSlSwCeNNLkq',
+                  'rec2FcZ4jsPuY1QYt',
+                  'rec39bDMnaVw3MyMR',
+                  'rec3FMoD8h9USTktb',
+                  'rec3P7fvPSpFkIFLV',
+                ],
+                'Sous-domaine': '1.1',
+                'Titre': 'Mener une recherche et une veille d’information',
+                'Tests': [
+                  'recNPB7dTNt5krlMA',
+                ],
+                'Acquis (identifiants)': [
+                  'recV11ibSCXvaUzZd',
+                  'recD01ptfJy7c4Sex',
+                  'recfO8994EvSQV9Ip',
+                  'recDMMeHSZRCjqo5x',
+                  'reci0phtJi0lvqW9j',
+                  'recUQSpjuDvwqKMst',
+                  'recxqogrKZ9p8b1u8',
+                  'recRV35kIeqUQj8cI',
+                  'rec50NXHkatsRkjVQ',
+                ],
+                'Référence': '1.1 Mener une recherche et une veille d’information',
+                'Tests Record ID': [
+                  'recNPB7dTNt5krlMA',
+                ],
+                'Acquis': [
+                  '@url2',
+                  '@url5',
+                  '@utiliserserv6',
+                  '@rechinfo1',
+                  '@eval2',
+                  '@publi4',
+                  '@modèleEco2',
+                  '@utiliserserv1',
+                ],
+                'Record ID': 'recsvLz0W2ShyfD63',
+                'Domaine Titre': [
+                  'Information et données',
+                ],
+                'Domaine Code': [
+                  '1',
+                ],
+              },
+              'createdTime': '2017-06-13T13:53:17.000Z',
+            },
+            {
+              'id': 'recNv8qhaY887jQb2',
+              'fields': {
+                'Domaine': [
+                  'recvoGdo7z2z7pXWa',
+                ],
+                'Epreuves': [
+                  'rec02tVrimXNkgaLD',
+                  'rec0gm0GFue3PQB3k',
+                  'rec0hoSlSwCeNNLkq',
+                  'rec2FcZ4jsPuY1QYt',
+                  'rec39bDMnaVw3MyMR',
+                  'rec3FMoD8h9USTktb',
+                  'rec3P7fvPSpFkIFLV',
+                ],
+                'Sous-domaine': '1.2',
+                'Titre': 'Gérer des données',
+                'Tests': [
+                  'recNPB7dTNt5krlMA',
+                ],
+                'Acquis (identifiants)': [
+                  'recV11ibSCXvaUzZd',
+                  'recD01ptfJy7c4Sex',
+                  'recfO8994EvSQV9Ip',
+                  'recDMMeHSZRCjqo5x',
+                  'reci0phtJi0lvqW9j',
+                  'recUQSpjuDvwqKMst',
+                  'recxqogrKZ9p8b1u8',
+                  'recRV35kIeqUQj8cI',
+                  'rec50NXHkatsRkjVQ',
+                ],
+                'Référence': '1.2 Gérer des données',
+                'Tests Record ID': [
+                  'recNPB7dTNt5krlMA',
+                ],
+                'Acquis': [
+                  '@url2',
+                  '@url5',
+                  '@utiliserserv6',
+                  '@rechinfo1',
+                  '@eval2',
+                  '@publi4',
+                  '@modèleEco2',
+                  '@utiliserserv1',
+                ],
+                'Record ID': 'recNv8qhaY887jQb2',
+                'Domaine Titre': [
+                  'Information et données',
+                ],
+                'Domaine Code': [
+                  '1',
+                ],
+              },
+              'createdTime': '2017-06-13T13:53:17.000Z',
+            },
+            {
+              'id': 'recIkYm646lrGvLNT',
+              'fields': {
+                'Domaine': [
+                  'recvoGdo7z2z7pXWa',
+                ],
+                'Epreuves': [
+                  'rec02tVrimXNkgaLD',
+                  'rec0gm0GFue3PQB3k',
+                  'rec0hoSlSwCeNNLkq',
+                  'rec2FcZ4jsPuY1QYt',
+                  'rec39bDMnaVw3MyMR',
+                  'rec3FMoD8h9USTktb',
+                  'rec3P7fvPSpFkIFLV',
+                ],
+                'Sous-domaine': '1.3',
+                'Titre': 'Traiter des données',
+                'Tests': [
+                  'recNPB7dTNt5krlMA',
+                ],
+                'Acquis (identifiants)': [
+                  'recV11ibSCXvaUzZd',
+                  'recD01ptfJy7c4Sex',
+                  'recfO8994EvSQV9Ip',
+                  'recDMMeHSZRCjqo5x',
+                  'reci0phtJi0lvqW9j',
+                  'recUQSpjuDvwqKMst',
+                  'recxqogrKZ9p8b1u8',
+                  'recRV35kIeqUQj8cI',
+                  'rec50NXHkatsRkjVQ',
+                ],
+                'Référence': '1.3 Traiter des données',
+                'Tests Record ID': [
+                  'recNPB7dTNt5krlMA',
+                ],
+                'Acquis': [
+                  '@url2',
+                  '@url5',
+                  '@utiliserserv6',
+                  '@rechinfo1',
+                  '@eval2',
+                  '@publi4',
+                  '@modèleEco2',
+                  '@utiliserserv1',
+                ],
+                'Record ID': 'recIkYm646lrGvLNT',
+                'Domaine Titre': [
+                  'Information et données',
+                ],
+                'Domaine Code': [
+                  '1',
+                ],
+              },
+              'createdTime': '2017-06-13T13:53:17.000Z',
+            }] });
 
       nock('https://api.airtable.com')
         .get('/v0/test-base/Domaines')
--- api/tests/unit/infrastructure/repositories/area-repository_test.js
+++ api/tests/unit/infrastructure/repositories/area-repository_test.js
@@ -1,6 +1,7 @@
 const { expect, sinon, factory } = require('../../../test-helper');
 const Area = require('../../../../lib/domain/models/Area');
 const areaDatasource = require('../../../../lib/infrastructure/datasources/airtable/area-datasource');
+const competenceRepository = require('../../../../lib/infrastructure/repositories/competence-repository');
 
 const areaRepository = require('../../../../lib/infrastructure/repositories/area-repository');
 
@@ -11,6 +12,21 @@ describe('Unit | Repository | area-repository', function() {
   beforeEach(() => {
     sandbox = sinon.sandbox.create();
     sandbox.stub(areaDatasource, 'list');
+
+    areaDatasource.list.resolves([
+      factory.buildAreaAirtableDataObject({
+        id: 'recDomaine1',
+        code: '1',
+        title: 'Domaine 1',
+        name: '1. Domaine 1',
+      }),
+      factory.buildAreaAirtableDataObject({
+        id: 'recDomaine2',
+        code: '2',
+        title: 'Domaine 2',
+        name: '2. Domaine 2',
+      }),
+    ]);
   });
 
   afterEach(() => {
@@ -18,23 +34,6 @@ describe('Unit | Repository | area-repository', function() {
   });
 
   describe('#list', () => {
-
-    beforeEach(() => {
-      areaDatasource.list.resolves([
-        factory.buildAreaAirtableDataObject({
-          id: 'recDomaine1',
-          code: '1',
-          title: 'Domaine 1',
-          name: '1. Domaine 1',
-        }),
-        factory.buildAreaAirtableDataObject({
-          id: 'recDomaine2',
-          code: '2',
-          title: 'Domaine 2',
-          name: '2. Domaine 2',
-        }),
-      ]);
-    });
 
     it('should fetch all area records from Airtable "Domaines" table', () => {
       // when
@@ -70,6 +69,54 @@ describe('Unit | Repository | area-repository', function() {
       return fetchedAreas.then((areas) => {
         expect(areas).to.deep.equal(expectedAreas);
         expect(areas[0]).to.be.an.instanceOf(Area);
+      });
+    });
+  });
+
+  describe('#listWithCompetences', () => {
+    const competence1 = factory.buildCompetence({
+        area: { id: 'recDomaine1' }
+      }), competence2 = factory.buildCompetence({
+        area: { id: 'recDomaine2' }
+      });
+
+    beforeEach(() => {
+      sandbox.stub(competenceRepository, 'list');
+      competenceRepository.list.resolves([
+        competence1,
+        competence2,
+      ]);
+    });
+
+    it('should add competences to Area objects', () => {
+      // given
+      const expectedAreas = [
+        new Area({
+          id: 'recDomaine1',
+          name: '1. Domaine 1',
+          code: '1',
+          title: 'Domaine 1',
+          competences: [
+            competence1
+          ],
+        }),
+        new Area({
+          id: 'recDomaine2',
+          name: '2. Domaine 2',
+          code: '2',
+          title: 'Domaine 2',
+          competences: [
+            competence2
+          ],
+        }),
+      ];
+
+      // when
+      const fetchedAreas = areaRepository.listWithCompetences();
+
+      // then
+      return fetchedAreas.then((areas) => {
+        expect(areas).to.deep.equal(expectedAreas);
       });
     });
   });
