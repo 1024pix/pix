d8245b95c878329d8af26fa94395537d55cca807
Jérémy Buget <jbuget@gmail.com>
2018-10-18 23:55:44 +0200

    Display organization members on organization details page

--- app/routes/authenticated/organizations/get.js
+++ app/routes/authenticated/organizations/get.js
@@ -3,7 +3,14 @@ import Route from '@ember/routing/route';
 export default Route.extend({
 
   model(params) {
-    return this.get('store').findRecord('organization', params.organization_id);
+    return this.get('store').findRecord('organization', params.organization_id)
+      .then((organization) => {
+        organization.set('members', [
+          {firstName: 'Bruce', lastName: 'Wayne', email: 'bruce.wayne@gotham.city', role: 'OWNER'},
+          {firstName: 'Selina', lastName: 'Kyle', email: 'selina.kyle@gotham.city', role: 'PRESCRIBER'},
+          ]);
+        return organization;
+      });
   }
 
 });
