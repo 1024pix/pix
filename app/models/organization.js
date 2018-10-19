77396042a540f8307f837a0689378f8a91a2153e
Jérémy Buget <jbuget@gmail.com>
2018-10-19 09:41:27 +0200

    Add organization-access data model and relationship with organization & user

--- app/models/organization.js
+++ app/models/organization.js
@@ -3,10 +3,13 @@ import DS from 'ember-data';
 const { attr, hasMany } = DS;
 
 export default DS.Model.extend({
+
   // Props
   name: attr(),
   type: attr(),
   code: attr(),
 
   // Relationships
+  memberships: hasMany('organization-access')
+
 });
--- app/routes/authenticated/organizations/get.js
+++ app/routes/authenticated/organizations/get.js
@@ -3,14 +3,7 @@ import Route from '@ember/routing/route';
 export default Route.extend({
 
   model(params) {
-    return this.get('store').findRecord('organization', params.organization_id)
-      .then((organization) => {
-        organization.set('members', [
-          {firstName: 'Bruce', lastName: 'Wayne', email: 'bruce.wayne@gotham.city', role: 'OWNER'},
-          {firstName: 'Selina', lastName: 'Kyle', email: 'selina.kyle@gotham.city', role: 'PRESCRIBER'},
-          ]);
-        return organization;
-      });
+    return this.get('store').findRecord('organization', params.organization_id);
   }
 
 });
