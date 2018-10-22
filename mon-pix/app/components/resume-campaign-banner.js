8c5b287c72ec7f145c97ad95984035d40504f2e5
Sébastien Roccaserra <sroccaserra@yahoo.com>
2018-10-22 11:02:33 +0200

    [pf-269] Create resume-campaign-banner component

--- mon-pix/app/components/resume-campaign-banner.js
+++ mon-pix/app/components/resume-campaign-banner.js
@@ -1,7 +1,22 @@
+import _orderBy from 'lodash/orderBy';
+import _filter from 'lodash/filter';
+import { computed } from '@ember/object';
 import Component from '@ember/component';
 
 export default Component.extend({
 
   classNames: ['resume-campaign-banner'],
+  campaignParticipations: [],
+
+  campaignToResume: computed('campaignParticipations', function() {
+    const campaignParticipationsNotShared = _filter((this.get('campaignParticipations')),
+      (campaignParticipation) => campaignParticipation.isShared === false);
+    const campaignParticipationOrdered = _orderBy(campaignParticipationsNotShared, 'createdAt', 'desc');
+    return campaignParticipationOrdered[0];
+  }),
+
+  canResumeCampaign: computed('campaignParticipations', function() {
+    return this.get('campaignToResume');
+  }),
 
 });
---
+++ mon-pix/tests/unit/components/resume-campaign-banner-test.js
@@ -0,0 +1,65 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { setupTest } from 'ember-mocha';
+import EmberObject from '@ember/object';
+
+describe('Unit | Component | resume-campaign-banner-component ', function() {
+
+  setupTest('component:resume-campaign-banner', {});
+
+  let component;
+
+  const campaignWanted = EmberObject.create({
+    isShared: false,
+    createdAt: '2018-01-01',
+    campaign: EmberObject.create({
+      code: 'AZERTY',
+    })
+  });
+  const oldCampaignNotFinished = EmberObject.create({
+    isShared: false,
+    createdAt: '2017-01-01',
+    campaign: EmberObject.create({
+      code: 'AZERTY',
+    })
+  });
+  const campaignFinish = EmberObject.create({
+    isShared: true,
+    campaign: EmberObject.create({
+      code: 'AZERTY',
+    })
+  });
+
+  beforeEach(function() {
+    component = this.subject();
+  });
+
+  describe('#campaignToResume', function() {
+
+    it('should return the most recent not shared campaign', function() {
+      // given
+      const listCampaignParticipations = [campaignFinish, oldCampaignNotFinished, campaignWanted];
+      component.set('campaignParticipations', listCampaignParticipations);
+
+      // when
+      const campaignToResume = component.get('campaignToResume');
+
+      // then
+      expect(campaignToResume).to.equal(campaignWanted);
+    });
+
+    it('should return undefined when all campaign are finished', function() {
+      // given
+      const listCampaignParticipations = [campaignFinish];
+      component.set('campaignParticipations', listCampaignParticipations);
+
+      // when
+      const campaignToResume = component.get('campaignToResume');
+
+      // then
+      expect(campaignToResume).to.equal(undefined);
+    });
+
+  });
+
+});
