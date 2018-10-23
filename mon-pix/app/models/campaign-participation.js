400603676f765bfce274a09fc80e3b510daef306
Melanie B <mboudard@octo.com>
2018-10-23 17:19:25 +0200

    [PF-269] Add attributes to User and CampaignParticipation, and find CampaignParticipation in Compte route

--- mon-pix/app/models/campaign-participation.js
+++ mon-pix/app/models/campaign-participation.js
@@ -5,6 +5,7 @@ const { Model, attr, belongsTo } = DS;
 export default Model.extend({
   isShared: attr('boolean'),
   participantExternalId: attr('string'),
+  createdAt: attr('date'),
   assessment: belongsTo('assessment'),
   campaign: belongsTo('campaign'),
 });
