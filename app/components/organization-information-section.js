f97ed938d0b861fb701b44c16657311cd0cfff21
Jérémy Buget <jbuget@gmail.com>
2018-10-25 01:24:47 +0200

    BSR

--- app/components/organization-information-section.js
+++ app/components/organization-information-section.js
@@ -8,7 +8,7 @@ export default Component.extend({
   actions: {
     updateLogo(file) {
       return file.readAsDataURL().then((b64) => {
-        this.get('organization').set('logoUrl', b64);
+        this.set('organization.logoUrl', b64);
         return this.get('onLogoUpdated')();
       });
     }
