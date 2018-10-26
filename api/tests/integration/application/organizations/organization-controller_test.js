d52903363eeb3808457d851b4f18fabf93634bb4
Jérémy Buget <jbuget@gmail.com>
2018-10-26 16:40:07 +0200

    [PA-53] [API] [Tests] Centralize all the tests into integration/application/organization-controller

--- api/tests/integration/application/organizations/organization-controller_test.js
+++ api/tests/integration/application/organizations/organization-controller_test.js
@@ -2,7 +2,6 @@ const { expect, sinon, factory, HttpTestServer } = require('../../../test-helper
 const usecases = require('../../../../lib/domain/usecases');
 const securityController = require('../../../../lib/interfaces/controllers/security-controller');
 const { NotFoundError } = require('../../../../lib/domain/errors');
-
 const moduleUnderTest = require('../../../../lib/application/organizations');
 
 describe('Integration | Application | Organizations | organization-controller', () => {
@@ -13,7 +12,7 @@ describe('Integration | Application | Organizations | organization-controller', 
 
   beforeEach(() => {
     sinon.stub(usecases, 'updateOrganizationInformation');
-    sinon.stub(securityController, 'checkUserHasRolePixMaster').callsFake((request, reply) => reply(true));
+    sinon.stub(securityController, 'checkUserHasRolePixMaster');
     httpTestServer = new HttpTestServer(moduleUnderTest);
   });
 
@@ -38,6 +37,10 @@ describe('Integration | Application | Organizations | organization-controller', 
     };
 
     context('Success cases', () => {
+
+      beforeEach(() => {
+        securityController.checkUserHasRolePixMaster.callsFake((request, reply) => reply(true));
+      });
 
       it('should resolve a 200 HTTP response', () => {
         // given
@@ -68,34 +71,59 @@ describe('Integration | Application | Organizations | organization-controller', 
 
     context('Error cases', () => {
 
-      it('should resolve a 404 HTTP response when organization does not exist', () => {
-        // given
-        const error = new NotFoundError('Organization not found');
-        usecases.updateOrganizationInformation.rejects(error);
+      context('when user is allowed to access resource', () => {
 
-        // when
-        const promise = httpTestServer.request('PATCH', '/api/organizations/1234', payload);
-
-        // then
-        return promise.then((response) => {
-          expect(response.statusCode).to.equal(404);
+        beforeEach(() => {
+          securityController.checkUserHasRolePixMaster.callsFake((request, reply) => {
+            return Promise.resolve(reply().code(403).takeover());
+          });
         });
-      });
 
-      it('should resolve a 500 HTTP response when an unexpected exception occurred', () => {
-        // given
-        usecases.updateOrganizationInformation.rejects(new Error());
+        it('should resolve a 403 HTTP response', () => {
+          // when
+          const promise = httpTestServer.request('PATCH', '/api/organizations/1234', payload);
 
-        // when
-        const promise = httpTestServer.request('PATCH', '/api/organizations/1234', payload);
+          // then
+          return promise.then((response) => {
+            expect(response.statusCode).to.equal(403);
+          });
+        });
 
-        // then
-        return promise.then((response) => {
-          expect(response.statusCode).to.equal(500);
-          expect(response.result.errors[0].code).to.equal('500');
+        context('when user is allowed to access resource', () => {
+
+          beforeEach(() => {
+            securityController.checkUserHasRolePixMaster.callsFake((request, reply) => reply(true));
+          });
+
+          it('should resolve a 404 HTTP response when organization does not exist', () => {
+            // given
+            const error = new NotFoundError('Organization not found');
+            usecases.updateOrganizationInformation.rejects(error);
+
+            // when
+            const promise = httpTestServer.request('PATCH', '/api/organizations/1234', payload);
+
+            // then
+            return promise.then((response) => {
+              expect(response.statusCode).to.equal(404);
+            });
+          });
+
+          it('should resolve a 500 HTTP response when an unexpected exception occurred', () => {
+            // given
+            usecases.updateOrganizationInformation.rejects(new Error());
+
+            // when
+            const promise = httpTestServer.request('PATCH', '/api/organizations/1234', payload);
+
+            // then
+            return promise.then((response) => {
+              expect(response.statusCode).to.equal(500);
+              expect(response.result.errors[0].code).to.equal('500');
+            });
+          });
         });
       });
     });
   });
-
 });
