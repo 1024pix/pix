2eb7774f678486ea6e37397e44d50d834000a6ae
Melanie B <mboudard@octo.com>
2018-10-29 17:24:03 +0100

    [PF-71] Add campaign tutorial text in model

---
+++ mon-pix/app/models/campaign-tutorial.js
@@ -0,0 +1,28 @@
+export default {
+  tutorial: [
+    {
+      id: 0,
+      title: 'Vous pouvez rechercher sur internet',
+      icon: 'icn-recherche.svg',
+      explanation: 'Si vous ignorez une réponse, \nelle se trouve sûrement sur internet.'
+    },
+    {
+      id: 1,
+      title: 'Pas de limite de temps !',
+      icon: 'icn-temps.svg',
+      explanation: 'Prenez le temps nécessaire pour terminer votre parcours. Si une question est chronométrée, cela vous sera indiqué.'
+    },
+    {
+      id: 2,
+      title: 'Des tutos pour apprendre',
+      icon: 'icn-tutos.svg',
+      explanation: 'Accédez à des tutos pour apprendre davantage sur chaque question et progresser.'
+    },
+    {
+      id: 3,
+      title: 'Un niveau de difficulté adapté',
+      icon: 'icn-algo.svg',
+      explanation: 'En fonction de vos réponses,\n Pix adapte la difficulté des questions. '
+    }
+  ],
+};
