7054af85214f7e4f3c5fd777110b31322d9e3b3f
Jérémy Buget <jbuget@gmail.com>
2018-10-31 22:00:09 +0100

    [BSR] [Pix Orga] Rename 'Organization Access' objects, properties and relationships into 'Membership'

--- orga/tests/acceptance/campaign-list-test.js
+++ orga/tests/acceptance/campaign-list-test.js
@@ -2,7 +2,7 @@ import { module, test } from 'qunit';
 import { currentURL, visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { authenticateSession } from 'ember-simple-auth/test-support';
-import { createUserWithOrganizationAccess } from '../helpers/test-init';
+import { createUserWithMembership } from '../helpers/test-init';
 
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
@@ -21,7 +21,7 @@ module('Acceptance | Campaign List', function(hooks) {
 
   test('it should be accessible for an authenticated user', async function(assert) {
     // given
-    const user = createUserWithOrganizationAccess();
+    const user = createUserWithMembership();
 
     await authenticateSession({
       user_id: user.id,
@@ -36,7 +36,7 @@ module('Acceptance | Campaign List', function(hooks) {
 
   test('it should show title indicate than user can create a campaign', async function(assert) {
     // given
-    const user = createUserWithOrganizationAccess();
+    const user = createUserWithMembership();
 
     await authenticateSession({
       user_id: user.id,
@@ -51,7 +51,7 @@ module('Acceptance | Campaign List', function(hooks) {
 
   test('it should list the campaigns of the current organization', async function(assert) {
     // given
-    const user = createUserWithOrganizationAccess();
+    const user = createUserWithMembership();
     server.createList('campaign', 12);
 
     await authenticateSession({
