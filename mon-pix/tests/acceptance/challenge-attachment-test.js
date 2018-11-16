e73f51cf1c66186fb82259d229db7e40596bb209
LJA <ljacquemin@octo.com>
2018-11-16 11:50:26 +0100

    Translating epreuve-pj-test to english

--- mon-pix/tests/acceptance/challenge-attachment-test.js
+++ mon-pix/tests/acceptance/challenge-attachment-test.js
@@ -14,7 +14,8 @@ function visitTimedChallenge() {
   click('.challenge-item-warning button');
 }
 
-describe('Acceptance | Télécharger une pièce jointe depuis la consigne d\'une épreuve', function() {
+
+describe('Acceptance | Download an attachment from a challenge', function() {
 
   let application;
 
@@ -28,36 +29,36 @@ describe('Acceptance | Télécharger une pièce jointe depuis la consigne d\'une
     destroyApp(application);
   });
 
-  describe('Quand l\'épreuve contient une pièce jointe en consigne', function() {
+  describe('When the challenge has an attachment', function() {
 
     beforeEach(function() {
       visitTimedChallenge();
     });
 
-    it('Il existe un moyen pour télécharger la pièce jointe d\'une épreuve dans la zone de consigne', function() {
+    it('should have a way to download the attachment', function() {
       const $ATTACHMENT_LINK = findWithAssert('.challenge-statement__action-link');
       expect($ATTACHMENT_LINK.length).to.equal(1);
     });
 
-    it('Le lien de la pièce jointe pointe vers le bon lien', function() {
+    it('should expose the correct attachment link', function() {
       const $ATTACHMENT_LINK = $('.challenge-statement__action-link');
       expect($ATTACHMENT_LINK.text()).to.contain('Télécharger');
       expect($ATTACHMENT_LINK.attr('href')).to.equal('http://example_of_url');
     });
 
-    it('Il n\'y a qu\'un seul fichier téléchargeable', function() {
+    it('should only have one file downloadable', function() {
       const $attachment = findWithAssert('.challenge-statement__action-link');
       expect($attachment.length).to.equal(1);
     });
   });
 
-  describe('Quand l\'épreuve ne contient pas de pièce jointe en consigne', function() {
+  describe('When the challenge does not contain an attachment', function() {
 
     beforeEach(function() {
       visit('/assessments/ref_assessment_id/challenges/ref_qroc_challenge_id');
     });
 
-    it('La section de téléchargement des pièces jointes est cachée', function() {
+    it('should hide the download section for the attachment', function() {
       // We are in a challenge...
       findWithAssert('.challenge-item');
 
