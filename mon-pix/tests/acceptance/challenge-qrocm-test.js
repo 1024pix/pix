9b701f25430b09dca9f22e57dc4bcd575197fded
LJA <ljacquemin@octo.com>
2018-11-16 12:14:35 +0100

    Translating epreuve-qrocm-test to english

--- mon-pix/tests/acceptance/challenge-qrocm-test.js
+++ mon-pix/tests/acceptance/challenge-qrocm-test.js
@@ -9,7 +9,7 @@ import startApp from '../helpers/start-app';
 import destroyApp from '../helpers/destroy-app';
 import $ from 'jquery';
 
-describe('Acceptance | Afficher un QROCM', function() {
+describe('Acceptance | Displaying un QROCM', function() {
 
   let application;
 
@@ -22,20 +22,18 @@ describe('Acceptance | Afficher un QROCM', function() {
     destroyApp(application);
   });
 
-  it('It should render challenge instruction', function() {
+  it('should render the challenge instruction', function() {
     const $challengeInstruction = $('.challenge-statement__instruction');
     const instructiontext = 'Un QROCM est une question ouverte avec plusieurs champs texte libre pour repondre';
     expect($challengeInstruction.text().trim()).to.equal(instructiontext);
   });
 
-  it('It should display only one input text as proposal to user', function() {
+  it('should display only one input text as proposal to user', function() {
     expect($('.challenge-response__proposal-input')).to.have.lengthOf(3);
   });
 
-  it('Error alert box should be displayed if user validate without checking a checkbox', async function() {
-    // 1st make sure all inputs are cleared
+  it('should display an error alert if the user tried to validate without checking anything first', async function() {
     $(':input').val('');
-    // Then try to validate sth
     await click($('.challenge-actions__action-validate'));
 
     expect($('.alert')).to.have.lengthOf(1);
