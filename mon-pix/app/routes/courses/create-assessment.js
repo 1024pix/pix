30e593a7a9e2c36edb4cf85768f9c851dfae54df
lisequesnel <quesnel.lise@gmail.com>
2018-11-19 11:52:20 +0100

    [pf-430] Create only one assessment when user begin 2 times the same competence

--- mon-pix/app/routes/courses/create-assessment.js
+++ mon-pix/app/routes/courses/create-assessment.js
@@ -1,4 +1,5 @@
 import BaseRoute from 'mon-pix/routes/base-route';
+import { isEmpty } from '@ember/utils';
 
 export default BaseRoute.extend({
 
@@ -7,9 +8,22 @@ export default BaseRoute.extend({
 
     let assessment;
 
-    return store.createRecord('assessment', { course, type: course.get('type') }).save()
-      .then((createdAssessment) => assessment = createdAssessment)
-      .then(() => store.queryRecord('challenge', { assessmentId: assessment.get('id') }))
-      .then((challenge) => this.replaceWith('assessments.challenge', { assessment, challenge }));
+    return store.query('assessment', { filter: { type: course.get('type'), courseId: course.id, state: 'started' } })
+      .then(async (assessments) => {
+        if (this._thereIsNoStartedAssessment(assessments)) {
+          await store.createRecord('assessment', { course, type: course.get('type') }).save()
+            .then((createdAssessment) => assessment = createdAssessment);
+        } else {
+          assessment = assessments.get('firstObject');
+        }
+        return store.queryRecord('challenge', { assessmentId: assessment.get('id') })
+          .then((challenge) => {
+            this.replaceWith('assessments.challenge', { assessment, challenge });
+          });
+      });
+  },
+
+  _thereIsNoStartedAssessment(assessments) {
+    return isEmpty(assessments);
   }
 });
