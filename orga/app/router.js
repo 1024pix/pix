12254c4429e26d737e120705c5f50cc6bfaeda1c
Jérémy Buget <jbuget@gmail.com>
2018-11-20 19:29:32 +0100

    [pf-1234] Add Ember Metrics to Pix Orga
    	modified:   api/package-lock.json

--- orga/app/router.js
+++ orga/app/router.js
@@ -1,9 +1,26 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
+import { scheduleOnce } from '@ember/runloop';
+import { inject as service } from '@ember/service';
 
 const Router = EmberRouter.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
+
+  metrics: service(),
+
+  didTransition() {
+    this._super(...arguments);
+    this._trackPage();
+  },
+
+  _trackPage() {
+    scheduleOnce('afterRender', this, () => {
+      const page = this.get('url');
+      const title = this.getWithDefault('currentRouteName', 'unknown');
+      this.get('metrics').trackPage({ page, title });
+    });
+  }
 });
 
 Router.map(function() {
