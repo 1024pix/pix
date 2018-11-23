10b3fb5a17de2e0437733e97beb5d6d3f81ea874
Jérémy Buget <jbuget@gmail.com>
2018-11-23 14:37:27 +0100

    [pf-1234] Update Matomo URL

--- orga/config/environment.js
+++ orga/config/environment.js
@@ -32,7 +32,7 @@ module.exports = function(environment) {
         name: 'Piwik',
         environments: ['production'],
         config: {
-          piwikUrl: '//stats.pix.fr',
+          piwikUrl: 'https://pix.matomo.cloud',
           siteId: 5
         }
       }
