8f8ebb9d4c44f4e1df465544f451199fec9e5d05
Forster <groux.forster@gmail.com>
2018-11-27 18:01:58 +0100

    Hide certification details for certification without assessment

--- app/controllers/authenticated/organizations/new.js
+++ app/controllers/authenticated/organizations/new.js
@@ -1,6 +1,8 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  notifications: service('notification-messages'),
 
   actions: {
 
@@ -11,15 +13,13 @@ export default Controller.extend({
     addOrganization() {
       return this.get('model').save()
         .then(() => {
-          // Take care to not return the result of the transition because then
-          // it will display a notification error even if all succeeded.
-          this.doGoBackToOrganizationListPage();
+          this.transitionToRoute('authenticated.organizations');
+          this.get('notifications').success('L’organisation a été créée avec succès.');
+        })
+        .catch(() => {
+          this.get('notifications').error('Une erreur est survenue.')
         });
     }
   },
-
-  doGoBackToOrganizationListPage() {
-    this.transitionToRoute('authenticated.organizations');
-  }
 
 });
--- tests/integration/components/organization-form-test.js
+++ tests/integration/components/organization-form-test.js
@@ -1,9 +1,9 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { click, fillIn, render } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import EmberObject from '@ember/object';
-import { resolve, reject } from 'rsvp';
+import { resolve } from 'rsvp';
 import { selectChoose } from 'ember-power-select/test-support/helpers';
 import Service from '@ember/service';
 
@@ -53,58 +53,6 @@ module('Integration | Component | organization-form', function(hooks) {
       // then
       assert.equal(this.get('selectedOrganizationType.value'), 'SCO');
       assert.equal(this.get('organization.type'), 'SCO');
-    });
-  });
-
-  async function fillInAllFormFields() {
-    await fillIn('#organizationName', 'ACME');
-    await selectChoose('#organizationTypeSelector', 'Établissement scolaire');
-  }
-
-  module('#submitOrganization', function() {
-
-    test('should display successful notification when organization creation succeeded', async function(assert) {
-      assert.expect(2);
-
-      // test double for the external action
-      this.set('organization', EmberObject.create());
-      this.set('externalAction', () => {
-        return resolve();
-      });
-
-      await render(hbs`{{organization-form organization=organization onSubmitOrganization=(action externalAction)}}`);
-
-      // fill out the form and force an onchange
-      await fillInAllFormFields();
-
-      // click the button to submit the form
-      await click('.form-action--submit');
-
-      let notificationMessages = this.owner.lookup('service:notification-messages');
-      assert.equal(notificationMessages.get('calledMethod'), 'success');
-      assert.equal(notificationMessages.get('message'), 'L’organisation a été créée avec succès.');
-    });
-
-    test('should display failed notification when organization creation failed', async function(assert) {
-      assert.expect(2);
-
-      // test double for the external action
-      this.set('organization', EmberObject.create());
-      this.set('externalAction', () => {
-        return reject();
-      });
-
-      await render(hbs`{{organization-form organization=organization onSubmitOrganization=(action externalAction)}}`);
-
-      // fill out the form and force an onchange
-      await fillInAllFormFields();
-
-      // click the button to submit the form
-      await click('.form-action--submit');
-
-      let notificationMessages = this.owner.lookup('service:notification-messages');
-      assert.equal(notificationMessages.get('calledMethod'), 'error');
-      assert.equal(notificationMessages.get('message'), 'Une erreur est survenue.');
     });
   });
 
