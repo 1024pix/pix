1024pix/pix

commit 8ff104ae46cb94ed3404a6b2640f3aa1931db798

[pf-362] mon-pix: accept token in URL
The Ember app now accepts a pre-generated token on the '/connexion'
route, as an alternative to submitting email and password.

mon-pix/app/routes/login.js
@@ -1,6 +1,7 @@
 import { inject as service } from '@ember/service';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 import BaseRoute from 'mon-pix/routes/base-route';
+import RSVP from 'rsvp';
 
 export default BaseRoute.extend(UnauthenticatedRouteMixin, {
 
@@ -10,10 +11,22 @@ export default BaseRoute.extend(UnauthenticatedRouteMixin, {
   routeIfAlreadyAuthenticated: 'compte',
   routeForLoggedUserLinkedToOrganization: 'board',
 
+  beforeModel({ queryParams }) {
+    if (queryParams && queryParams.token && queryParams['user-id']) {
+      return this.get('session')
+        .authenticate('authenticator:simple', { token: queryParams.token, userId: parseInt(queryParams['user-id']) })
+        .then((_) => {
+          this.transitionTo('compte');
+        });
+    } else {
+      return RSVP.resolve();
+    }
+  },
+
   actions: {
     signin(email, password) {
       return this.get('session')
-        .authenticate('authenticator:simple', email, password)
+        .authenticate('authenticator:simple', { email, password })
         .then((_) => {
           return this.get('store').queryRecord('user', {});
         });

mon-pix/tests/unit/routes/login-test.js
@@ -26,7 +26,7 @@ describe('Unit | Route | login page', function() {
     this.inject.service('store', { as: 'store' });
   });
 
-  it('should authenticate the user', function() {
+  it('should authenticate the user given email and password', function() {
     // Given
     authenticatedStub.resolves();
 
@@ -37,11 +37,29 @@ describe('Unit | Route | login page', function() {
     };
 
     // When
-    const promise = route.actions.signin.call(route, expectedEmail, expectedPassword);
+    const promise = route.beforeModel({})
+      .then((_) => route.actions.signin.call(route, expectedEmail, expectedPassword));
 
     // Then
     return promise.then(() => {
-      sinon.assert.calledWith(authenticatedStub, 'authenticator:simple', expectedEmail, expectedPassword);
+      sinon.assert.calledWith(authenticatedStub, 'authenticator:simple', { email: expectedEmail, password: expectedPassword });
+    });
+  });
+
+  it('should authenticate the user given token in URL', function() {
+    // Given
+    authenticatedStub.resolves();
+
+    const route = this.subject();
+    sinon.stub(route, 'transitionTo');
+
+    // When
+    const promise = route.beforeModel({ queryParams: { token: 'dummy-token', 'user-id': '123' } });
+
+    // Then
+    return promise.then(() => {
+      sinon.assert.calledWith(authenticatedStub, 'authenticator:simple', { token: 'dummy-token', userId: 123 });
+      sinon.assert.calledWith(route.transitionTo, 'compte');
     });
   });
 

mon-pix/app/authenticators/simple.js
@@ -10,7 +10,11 @@ export default Base.extend({
     return RSVP.resolve(data);
   },
 
-  authenticate(email, password) {
+  authenticate({ email, password, token, userId }) {
+    if (token) {
+      return RSVP.resolve({ token, userId });
+    }
+
     return this.get('ajax').request('/api/authentications', {
       method: 'POST',
       data: JSON.stringify({

mon-pix/app/routes/inscription.js
@@ -24,7 +24,7 @@ export default BaseRoute.extend(UnauthenticatedRouteMixin, {
 
     authenticateUser({ email, password }) {
       return this.get('session')
-        .authenticate('authenticator:simple', email, password)
+        .authenticate('authenticator:simple', { email, password })
         .then(() => {
           return this.get('store').queryRecord('user', {});
         });

mon-pix/tests/unit/authenticators/simple-test.js
@@ -39,7 +39,7 @@ describe('Unit | Authenticator | simple', function() {
     const authenticator = this.subject();
 
     // When
-    const promise = authenticator.authenticate(email, password);
+    const promise = authenticator.authenticate({ email, password });
 
     // Then
     return promise.then((_) => {
@@ -59,7 +59,23 @@ describe('Unit | Authenticator | simple', function() {
     const authenticator = this.subject();
 
     // When
-    const promise = authenticator.authenticate(email, password);
+    const promise = authenticator.authenticate({ email, password });
+
+    // Then
+    return promise.then((data) => {
+      expect(data.userId).to.equal(expectedUserId);
+      expect(data.token).to.equal(expectedToken);
+    });
+  });
+
+  it('should accept pre-generated token and userId', function() {
+    // Given
+    const token = expectedToken;
+    const userId = expectedUserId;
+    const authenticator = this.subject();
+
+    // When
+    const promise = authenticator.authenticate({ token, userId });
 
     // Then
     return promise.then((data) => {

mon-pix/tests/unit/routes/inscription-test.js
@@ -34,7 +34,7 @@ describe('Unit | Route | inscription', function() {
 
     return promise.then(() => {
       // Then
-      sinon.assert.calledWith(authenticateStub, 'authenticator:simple', expectedEmail, expectedPassword);
+      sinon.assert.calledWith(authenticateStub, 'authenticator:simple', { email: expectedEmail, password: expectedPassword });
       sinon.assert.calledWith(queryRecordStub, 'user', {});
     });
   });

commit de6eee982cbcf9bdd08bd01a72eec05d6fdb5192

[PF-456] [BUGFIX] Restore redirection from /connexion to /compte for logged-in users
This was broken in 8ff104ae as part of the SAML login work. We forgot to
call `super(...)` when overriding `beforeModel`.

This commit adds a test for the redirection, and fixes the behavior.

mon-pix/app/routes/login.js
@@ -1,7 +1,6 @@
 import { inject as service } from '@ember/service';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 import BaseRoute from 'mon-pix/routes/base-route';
-import RSVP from 'rsvp';
 
 export default BaseRoute.extend(UnauthenticatedRouteMixin, {
 
@@ -9,7 +8,6 @@ export default BaseRoute.extend(UnauthenticatedRouteMixin, {
 
   routeIfNotAuthenticated: 'connexion',
   routeIfAlreadyAuthenticated: 'compte',
-  routeForLoggedUserLinkedToOrganization: 'board',
 
   beforeModel({ queryParams }) {
     if (queryParams && queryParams.token && queryParams['user-id']) {
@@ -19,7 +17,7 @@ export default BaseRoute.extend(UnauthenticatedRouteMixin, {
           this.transitionTo('compte');
         });
     } else {
-      return RSVP.resolve();
+      return this._super(...arguments);
     }
   },
 

mon-pix/tests/unit/routes/login-test.js
@@ -9,58 +9,77 @@ describe('Unit | Route | login page', function() {
     needs: ['service:session', 'service:current-routed-modal', 'service:metrics']
   });
 
-  const authenticatedStub = sinon.stub();
-  const queryRecordStub = sinon.stub();
-  const expectedEmail = 'email@example.net';
-  const expectedPassword = 'azerty';
+  context('when user is not authenticated', function() {
+    let authenticateStub;
+    let queryRecordStub;
+    const expectedEmail = 'email@example.net';
+    const expectedPassword = 'azerty';
 
-  beforeEach(function() {
-    this.register('service:session', Service.extend({
-      authenticate: authenticatedStub
-    }));
-    this.inject.service('session', { as: 'session' });
+    beforeEach(function() {
+      queryRecordStub = sinon.stub();
+      authenticateStub = sinon.stub();
+      this.register('service:session', Service.extend({
+        authenticate: authenticateStub,
+      }));
+      this.inject.service('session', { as: 'session' });
 
-    this.register('service:store', Service.extend({
-      queryRecord: queryRecordStub
-    }));
-    this.inject.service('store', { as: 'store' });
-  });
-
-  it('should authenticate the user given email and password', function() {
-    // Given
-    authenticatedStub.resolves();
-
-    const foundUser = EmberObject.create({ id: 12 });
-    queryRecordStub.resolves(foundUser);
-    const route = this.subject();
-    route.transitionTo = () => {
-    };
-
-    // When
-    const promise = route.beforeModel({})
-      .then((_) => route.actions.signin.call(route, expectedEmail, expectedPassword));
-
-    // Then
-    return promise.then(() => {
-      sinon.assert.calledWith(authenticatedStub, 'authenticator:simple', { email: expectedEmail, password: expectedPassword });
+      this.register('service:store', Service.extend({
+        queryRecord: queryRecordStub
+      }));
+      this.inject.service('store', { as: 'store' });
     });
-  });
 
-  it('should authenticate the user given token in URL', function() {
-    // Given
-    authenticatedStub.resolves();
+    it('should authenticate the user given email and password', async function() {
+      // Given
+      authenticateStub.resolves();
 
-    const route = this.subject();
-    sinon.stub(route, 'transitionTo');
+      const foundUser = EmberObject.create({ id: 12 });
+      queryRecordStub.resolves(foundUser);
+      const route = this.subject();
+      sinon.stub(route, 'transitionTo').throws('Must not be called');
 
-    // When
-    const promise = route.beforeModel({ queryParams: { token: 'dummy-token', 'user-id': '123' } });
+      // When
+      await route.beforeModel({});
+      await route.actions.signin.call(route, expectedEmail, expectedPassword);
 
-    // Then
-    return promise.then(() => {
-      sinon.assert.calledWith(authenticatedStub, 'authenticator:simple', { token: 'dummy-token', userId: 123 });
+      // Then
+      sinon.assert.calledWith(authenticateStub, 'authenticator:simple', { email: expectedEmail, password: expectedPassword });
+    });
+
+    it('should authenticate the user given token in URL', async function() {
+      // Given
+      authenticateStub.resolves();
+
+      const route = this.subject();
+      sinon.stub(route, 'transitionTo');
+
+      // When
+      await route.beforeModel({ queryParams: { token: 'dummy-token', 'user-id': '123' } });
+
+      // Then
+      sinon.assert.calledWith(authenticateStub, 'authenticator:simple', { token: 'dummy-token', userId: 123 });
       sinon.assert.calledWith(route.transitionTo, 'compte');
     });
   });
 
+  context('when user is authenticated', function() {
+    beforeEach(function() {
+      this.register('service:session', Service.extend({
+        isAuthenticated: true
+      }));
+      this.inject.service('session', { as: 'session' });
+    });
+
+    it('should redirect authenticated users to /compte', async function() {
+      // Given
+      const route = this.subject();
+      sinon.stub(route, 'transitionTo');
+
+      // When
+      await route.beforeModel({});
+
+      // Then
+      sinon.assert.calledWith(route.transitionTo, 'compte');
+    });
+  });
 });