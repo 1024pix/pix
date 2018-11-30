df24dd38e941b91ec0f07b9edea1bcf5cc8512d3
Mathieu Laurent <mlaurent@octo.com>
2018-11-30 09:32:25 +0100

    [pf-458] Do no start placement if there is a completed placement, and retry not possible yet

--- api/lib/domain/models/Assessment.js
+++ api/lib/domain/models/Assessment.js
@@ -1,4 +1,6 @@
 const _ = require('lodash');
+const moment = require('moment');
+
 const AnswerStatus = require('./AnswerStatus');
 const { ObjectValidationError } = require('../errors');
 
@@ -187,6 +189,22 @@ class Assessment {
   isCertifiable() {
     return this.getLastAssessmentResult().level >= 1;
   }
+
+  canStartNewAttemptOnCourse() {
+    if(!this.isPlacementAssessment()) throw new Error('Only available for a placement assessment');
+
+    return this.isCompleted() && this.getRemainingDaysBeforeNewAttempt() <= 0;
+  }
+
+  getRemainingDaysBeforeNewAttempt() {
+    const lastResult = this.getLastAssessmentResult();
+    const daysSinceLastCompletedAssessment = moment().diff(lastResult.createdAt, 'days', true);
+
+    const remainingDaysToWait = Math.ceil(MINIMUM_DELAY_IN_DAYS_BETWEEN_TWO_PLACEMENTS - daysSinceLastCompletedAssessment);
+
+    return remainingDaysToWait > 0 ? remainingDaysToWait : 0;
+  }
+
 }
 
 Assessment.states = states;
--- api/tests/unit/domain/usecases/start-placement-assessment_test.js
+++ api/tests/unit/domain/usecases/start-placement-assessment_test.js
@@ -1,5 +1,7 @@
 const { expect, sinon, domainBuilder } = require('../../../test-helper');
 
+const moment = require('moment');
+
 const startPlacementAssessment = require('../../../../lib/domain/usecases/start-placement-assessment');
 
 const Assessment = require('../../../../lib/domain/models/Assessment');
@@ -8,49 +10,54 @@ const { AssessmentStartError } = require('../../../../lib/domain/errors');
 describe('Unit | UseCase | start-placement-assessment', () => {
 
   let sandbox;
+  let clock;
+  let testCurrentDate;
+
   const assessmentRepository = {
     save: () => undefined,
     getLastPlacementAssessmentByUserIdAndCourseId: () => undefined,
   };
 
   beforeEach(() => {
+    testCurrentDate = new Date('2018-01-10 05:00:00');
+    clock = sinon.useFakeTimers(testCurrentDate.getTime());
+
     sandbox = sinon.sandbox.create();
     sandbox.stub(assessmentRepository, 'save');
     sandbox.stub(assessmentRepository, 'getLastPlacementAssessmentByUserIdAndCourseId');
   });
 
   afterEach(() => {
+    clock.restore();
     sandbox.restore();
   });
 
   context('when there is no assessment completed and no assessment started concerning the user and course', () => {
 
-    it('should start the assessment', () => {
+    it('should start the assessment', async () => {
       // given
       const givenAssessment = domainBuilder.buildAssessment();
       const assessmentSpy = sinon.spy(givenAssessment, 'start');
       assessmentRepository.save.resolves();
 
       // when
-      const promise = startPlacementAssessment({ assessment: givenAssessment, assessmentRepository });
+      await startPlacementAssessment({ assessment: givenAssessment, assessmentRepository });
 
       // then
-      return promise.then(() => {
-        expect(assessmentSpy).to.have.been.calledOnce;
-      });
+      return expect(assessmentSpy).to.have.been.calledOnce;
     });
 
-    it('should return the started and saved assessment', () => {
+    it('should return the started and saved assessment', async () => {
       // given
       const givenAssessment = domainBuilder.buildAssessment();
       const savedAssessment = domainBuilder.buildAssessment();
       assessmentRepository.save.withArgs(givenAssessment).resolves(savedAssessment);
 
       // when
-      const promise = startPlacementAssessment({ assessment: givenAssessment, assessmentRepository });
+      const assessment = await startPlacementAssessment({ assessment: givenAssessment, assessmentRepository });
 
       // then
-      return expect(promise).to.be.fulfilled.then((assessment) => expect(assessment).to.deep.equal(savedAssessment));
+      return expect(assessment).to.deep.equal(savedAssessment);
     });
 
   });
@@ -81,26 +88,98 @@ describe('Unit | UseCase | start-placement-assessment', () => {
       const promise = startPlacementAssessment({ assessment: placementAssessmentToStart, assessmentRepository });
 
       // then
-      return expect(promise).to.be.rejectedWith(AssessmentStartError, 'Une évaluation en cours existe déjà');
+      return expect(promise).to.be.rejectedWith(AssessmentStartError, 'Impossible de démarrer un nouveau positionnement');
     });
 
   });
 
   context('when there is completed assessments and no started assessment concerning the user and the course', () => {
 
-    context('and the user has to wait before being able to replay the course', () => {
+    context('and the user has to wait before being able start a new placement assessment on the course', () => {
 
       it('should throw an error with correct message', () => {
+        // given
+        const courseId = 'rec43mpMIR5dUzdjh';
+        const userId = '42';
+        const placementAssessmentToStart = domainBuilder.buildAssessment({
+          type: Assessment.types.PLACEMENT,
+          state: null,
+          userId,
+          courseId,
+        });
+
+        const assessmentResultCreationDate = moment(testCurrentDate).subtract(3, 'day').toDate();
+        const assessmentResult = domainBuilder.buildAssessmentResult({ createdAt: assessmentResultCreationDate });
+
+        const completedAssessment = domainBuilder.buildAssessment({
+          type: Assessment.types.PLACEMENT,
+          state: Assessment.states.COMPLETED,
+          assessmentResults: [assessmentResult],
+        });
+
+        assessmentRepository.getLastPlacementAssessmentByUserIdAndCourseId
+          .withArgs(userId, courseId)
+          .resolves(completedAssessment);
+
+        // when
+        const promise = startPlacementAssessment({ assessment: placementAssessmentToStart, assessmentRepository });
+
+        // then
+        return expect(promise).to.be.rejectedWith(AssessmentStartError, 'Impossible de démarrer un nouveau positionnement');
       });
 
     });
 
-    context('and the user can replay the course', () => {
+    context('and the user can start a new placement assessment on the course', () => {
 
-      it('should start the new assessment', () => {
+      let placementAssessmentToStart;
+
+      beforeEach(() => {
+        const courseId = 'rec43mpMIR5dUzdjh';
+        const userId = '42';
+        placementAssessmentToStart = domainBuilder.buildAssessment({
+          type: Assessment.types.PLACEMENT,
+          state: null,
+          userId,
+          courseId,
+        });
+
+        const assessmentResultCreationDate = moment(testCurrentDate).subtract(8, 'day').toDate();
+        const assessmentResult = domainBuilder.buildAssessmentResult({ createdAt: assessmentResultCreationDate });
+
+        const completedAssessment = domainBuilder.buildAssessment({
+          type: Assessment.types.PLACEMENT,
+          state: Assessment.states.COMPLETED,
+          assessmentResults: [assessmentResult],
+        });
+
+        assessmentRepository.getLastPlacementAssessmentByUserIdAndCourseId
+          .withArgs(userId, courseId)
+          .resolves(completedAssessment);
       });
 
-      it('should return the started and saved assessment', () => {
+      it('should start the assessment', async () => {
+        // given
+        const assessmentSpy = sinon.spy(placementAssessmentToStart, 'start');
+
+        // when
+        await startPlacementAssessment({ assessment: placementAssessmentToStart, assessmentRepository });
+
+        // then
+        return expect(assessmentSpy).to.have.been.calledOnce;
+
+      });
+
+      it('should return the started and saved assessment', async () => {
+        // given
+        const savedAssessment = domainBuilder.buildAssessment();
+        assessmentRepository.save.withArgs(placementAssessmentToStart).resolves(savedAssessment);
+
+        // when
+        const assessment = await startPlacementAssessment({ assessment: placementAssessmentToStart, assessmentRepository });
+
+        // then
+        return expect(assessment).to.deep.equal(savedAssessment);
       });
 
     });
