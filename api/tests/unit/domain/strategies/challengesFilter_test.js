87cd1cb9f077015a7e2e61af5c6b0b61c16da6c9
Melanie B <mboudard@octo.com>
2018-11-30 17:38:41 +0100

    [pf-437] Changes order of rules for filter + change filter for first question + add tests

--- api/tests/unit/domain/strategies/challengesFilter_test.js
+++ api/tests/unit/domain/strategies/challengesFilter_test.js
@@ -9,7 +9,94 @@ const KNOWLEDGE_ELEMENT_STATUS = {
 
 describe('Unit | Domain | Models | filterChallenge', () => {
 
-  describe('filteredChallenges()', function() {
+  describe('#filteredChallengeForFirstChallenge', () => {
+    it('should return a first challenge possible', () => {
+      // given
+      const skill1 = domainBuilder.buildSkill({ name: '@web3' });
+      const targetProfile = new TargetProfile({ skills: [skill1] });
+      const challengeAssessingSkill1 = domainBuilder.buildChallenge({ skills: [skill1] });
+      const knowledgeElements = [];
+      const challenges = [challengeAssessingSkill1];
+
+      // when
+      const result = filterChallenge.filteredChallengeForFirstChallenge({
+        challenges: challenges,
+        knowledgeElements,
+        targetProfile: targetProfile
+      });
+
+      // then
+      expect(result).to.deep.equal([challengeAssessingSkill1]);
+    });
+
+    it('should return empty array if challenge are not valid', () => {
+      // given
+      const skill1 = domainBuilder.buildSkill({ name: '@web3' });
+      const targetProfile = new TargetProfile({ skills: [skill1] });
+      const challengeAssessingSkill1 = domainBuilder.buildChallenge({ skills: [skill1], status: 'PAS VALIDE' });
+      const knowledgeElements = [];
+      const challenges = [challengeAssessingSkill1];
+
+      // when
+      const result = filterChallenge.filteredChallengeForFirstChallenge({
+        challenges: challenges,
+        knowledgeElements,
+        targetProfile: targetProfile
+      });
+
+      // then
+      expect(result).to.deep.equal([]);
+    });
+
+    it('should return a challenge even if the only tube has a skill with difficulty > 3', () => {
+      // given
+      const skill1 = domainBuilder.buildSkill({ name: '@web4' });
+      const targetProfile = new TargetProfile({ skills: [skill1] });
+      const challengeAssessingSkill1 = domainBuilder.buildChallenge({ skills: [skill1]});
+      const knowledgeElements = [];
+      const challenges = [challengeAssessingSkill1];
+
+      // when
+      const result = filterChallenge.filteredChallengeForFirstChallenge({
+        challenges: challenges,
+        knowledgeElements,
+        targetProfile: targetProfile
+      });
+
+      // then
+      expect(result).to.deep.equal([challengeAssessingSkill1]);
+    });
+  });
+
+  describe('#filteredChallenges', function() {
+    describe('Verify rules 1 : published and valid challenges', () => {
+      it('should not ask a question that targets a skill already assessed', function() {
+        // given
+        const [skill1] = domainBuilder.buildSkillCollection();
+
+        const targetProfile = new TargetProfile({ skills: [skill1] });
+
+        const challengeAssessingSkill1 = domainBuilder.buildChallenge({ skills: [skill1], status: 'invalidated' });
+
+        const knowledgeElements = [];
+
+        const challenges = [
+          challengeAssessingSkill1,
+        ];
+
+        // when
+        const result = filterChallenge.filteredChallenges({
+          challenges: challenges,
+          knowledgeElements,
+          predictedLevel: 3,
+          targetProfile: targetProfile
+        });
+
+        // then
+        expect(result).to.deep.equal([]);
+      });
+    });
+
     it('should not ask a question that targets a skill already assessed', function() {
       // given
       const [skill1, skill2, skill3] = domainBuilder.buildSkillCollection();
@@ -71,6 +158,72 @@ describe('Unit | Domain | Models | filterChallenge', () => {
 
         // then
         expect(result).to.deep.equal([]);
+      });
+    });
+
+    describe('Verify rules : Not timed challenge after timed challenge', () => {
+      it('should return the not timed challenge if last one was timed', () => {
+        // given
+        const [skill1, skill2] = domainBuilder.buildSkillCollection();
+
+        const targetProfile = domainBuilder.buildTargetProfile({ skills: [skill1, skill2] });
+
+        const lastChallenge = domainBuilder.buildChallenge({ skills: [skill1], timer: 34 });
+        const challengeNotTimed = domainBuilder.buildChallenge({ skills: [skill2], timer: null });
+        const challengeTimed = domainBuilder.buildChallenge({ skills: [skill2], timer: 23 });
+
+        const knowledgeElements = [
+          domainBuilder.buildSmartPlacementKnowledgeElement({ skillId: skill1.id, status: KNOWLEDGE_ELEMENT_STATUS.VALIDATED, source: 'direct' }),
+        ];
+        const challenges = [
+          lastChallenge,
+          challengeNotTimed,
+          challengeTimed
+        ];
+
+        // when
+        const result = filterChallenge.filteredChallenges({
+          challenges: challenges,
+          knowledgeElements,
+          predictedLevel: 2,
+          targetProfile: targetProfile,
+          lastChallenge
+        });
+
+        // then
+        expect(result).to.deep.equal([challengeNotTimed]);
+      });
+
+      it('should return timed challenges if last one was timed but we dont have not timed challenge', () => {
+        // given
+        const [skill1, skill2] = domainBuilder.buildSkillCollection();
+
+        const targetProfile = domainBuilder.buildTargetProfile({ skills: [skill1, skill2] });
+
+        const lastChallenge = domainBuilder.buildChallenge({ skills: [skill1], timer: 34 });
+        const challengeNotTimed = domainBuilder.buildChallenge({ skills: [skill2], timer: 45 });
+        const challengeTimed = domainBuilder.buildChallenge({ skills: [skill2], timer: 23 });
+
+        const knowledgeElements = [
+          domainBuilder.buildSmartPlacementKnowledgeElement({ skillId: skill1.id, status: KNOWLEDGE_ELEMENT_STATUS.VALIDATED, source: 'direct' }),
+        ];
+        const challenges = [
+          lastChallenge,
+          challengeNotTimed,
+          challengeTimed
+        ];
+
+        // when
+        const result = filterChallenge.filteredChallenges({
+          challenges: challenges,
+          knowledgeElements,
+          predictedLevel: 2,
+          targetProfile: targetProfile,
+          lastChallenge
+        });
+
+        // then
+        expect(result).to.have.members([challengeNotTimed, challengeTimed]);
       });
     });
 
