5f7226ce5f9fefbb002598abf9a6c1cb51c2f826
lisequesnel <quesnel.lise@gmail.com>
2018-12-03 16:24:25 +0100

    [pc-5] Update sort on sessions + add limit of 10 of retrieved sessions

--- certif/app/controllers/authenticated/sessions/list.js
+++ certif/app/controllers/authenticated/sessions/list.js
@@ -1,7 +1,7 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 
-const SORTING_ORDER = ['date:desc', 'createdAt:desc'];
+const SORTING_ORDER = ['date:desc', 'time:desc'];
 
 export default Controller.extend({
   sortingOrder: SORTING_ORDER,
--- certif/tests/unit/controllers/authenticated/sessions/list-test.js
+++ certif/tests/unit/controllers/authenticated/sessions/list-test.js
@@ -13,9 +13,9 @@ module('Unit | Controller | authenticated/sessions/list', function(hooks) {
   test('it should sort the sessions from more recent to older', function(assert) {
     // given
     let controller = this.owner.lookup('controller:authenticated/sessions/list');
-    const session1 = { id: 1, date: new Date('2018-08-07 14:00:44') };
-    const session2 = { id: 2, date: new Date('2018-08-07 14:00:45') };
-    const session3 = { id: 3, date: new Date('2018-08-07 14:00:40') };
+    const session1 = { id: 1, date: new Date('2018-08-06'), time: '08:00' };
+    const session2 = { id: 2, date: new Date('2018-08-07'), time: '08:00' };
+    const session3 = { id: 3, date: new Date('2018-08-07'), time: '18:00' };
     const sessions = ArrayProxy.create({
       content: [session1, session2, session3]
     });
@@ -25,9 +25,9 @@ module('Unit | Controller | authenticated/sessions/list', function(hooks) {
     const sortedSessions = controller.get('sortedSessions');
 
     // then
-    assert.equal(sortedSessions[0].id, 2);
-    assert.equal(sortedSessions[1].id, 1);
-    assert.equal(sortedSessions[2].id, 3);
+    assert.equal(sortedSessions[0].id, 3);
+    assert.equal(sortedSessions[1].id, 2);
+    assert.equal(sortedSessions[2].id, 1);
   });
 
   test('it should know when there is no sessions', function(assert) {
