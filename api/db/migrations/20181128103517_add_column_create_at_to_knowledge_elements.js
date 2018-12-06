b5c69fc8ec41f5e814edafaf48766a8e6dfec796
Melanie B <mboudard@octo.com>
2018-12-06 15:22:53 +0100

    [pf-437] Change migration to set a correct createdAt column

--- api/db/migrations/20181128103517_add_column_create_at_to_knowledge_elements.js
+++ api/db/migrations/20181128103517_add_column_create_at_to_knowledge_elements.js
@@ -1,12 +1,13 @@
 const TABLE_NAME = 'knowledge-elements';
-const moment = require('moment');
 
-exports.up = (knex ) => {
+exports.up = async (knex) =>  {
 
-  const currentTime = moment().format('YYYY-MM-DD HH:mm:ss');
+  const info = await knex(TABLE_NAME).columnInfo();
+  if(!info.createdAt){
+    return knex.schema.table(TABLE_NAME, (table) => table.dateTime('createdAt').notNullable().defaultTo(knex.fn.now()))
+      .then(() => knex.raw('update "knowledge-elements" set "createdAt" = (select "createdAt" from answers where "knowledge-elements"."answerId" = answers.id);'));
+  }
 
-  return knex.schema.table(TABLE_NAME, (table) => table.dateTime('createdAt').notNullable().defaultTo(currentTime))
-    .then(() => knex.raw('update "knowledge-elements" set "createdAt" = (select "createdAt" from answers where "knowledge-elements"."answerId" = answers.id);'));
 };
 
 exports.down = (knex) => {
