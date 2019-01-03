139113f619a8882d0aa7d3b8542bc72e17a78c0a
Melanie B <mboudard@octo.com>
2019-01-03 14:42:59 +0100

    [PF-478] Use method to create token in mirage tests

--- mon-pix/mirage/routes/get-user-me.js
+++ mon-pix/mirage/routes/get-user-me.js
@@ -1,10 +1,7 @@
 export default function getAuthenticatedUser(schema, request) {
 
   const userToken = request.requestHeaders.Authorization.replace('Bearer ', '');
+  const userId = JSON.parse(atob(userToken.split('.')[1])).user_id;
 
-  if (userToken === 'aaa.eyJ1c2VyX2lkIjoxLCJzb3VyY2UiOiJwaXgiLCJpYXQiOjE1NDUyMTg5MDh9.bbbb') return schema.users.find(1);
-
-  if (userToken === 'aaa.eyJ1c2VyX2lkIjoyLCJzb3VyY2UiOiJwaXgiLCJpYXQiOjE1NDUyMTg4Nzl9.bbbb') return schema.users.find(2);
-
-  return schema.users.find(3);
+  return schema.users.find(userId);
 }
