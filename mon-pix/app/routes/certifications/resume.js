c5591f8a77486c4ff83d6716deb8abdc7adceaa8
lisequesnel <quesnel.lise@gmail.com>
2019-01-07 14:16:37 +0100

    [pf-143] Redirect to assessments.resume when creating or resuming a course assessment
    This fixes resuming a certification when it is completed (PF-455), and
    removes a bit of duplication of the "next challenge" logic.
    
    The legacy certifications.resume route (/certifications/1234) now goes
    directly to courses.create-assessment
    
    Since assessments.resume is now used for new assessments as well, the
    loading message was changed to be more generic.
    
    Also, if an error occurred while retrieving an existing assessment, it
    would not be shown because the redirect hook is expected to be
    synchronous. Using the afterModel hook instead for the same purpose
    makes Ember wait for promise resolution before showing the page.

--- mon-pix/app/routes/certifications/resume.js
+++ mon-pix/app/routes/certifications/resume.js
@@ -5,21 +5,7 @@ export default BaseRoute.extend({
   model(params) {
     const certificationCourseId = params.certification_course_id;
 
-    return this.get('store').query('assessment', {
-      filter: {
-        courseId: certificationCourseId,
-        type: 'CERTIFICATION'
-      }
-    }).then((assessments)=> {
-      return assessments.get('firstObject');
-    });
-  },
-
-  afterModel(assessment) {
-    return this.get('store')
-      .queryRecord('challenge', { assessmentId: assessment.get('id') })
-      .then((nextChallenge) => this.transitionTo('assessments.challenge', assessment.get('id'), nextChallenge.get('id')))
-      .catch(() => this.transitionTo('certifications.results', assessment.get('course.id')));
+    return this.transitionTo('courses.create-assessment', certificationCourseId);
   }
 
 });
