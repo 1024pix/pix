4cd26eaa69f5dde1df175b842691c42149dd08eb
LJA <ljacquemin@octo.com>
2019-01-15 16:29:42 +0100

    [pc-15] Added repository and domain to certification-center object

---
+++ api/lib/domain/models/CertificationCenter.js
@@ -0,0 +1,20 @@
+class CertificationCenter {
+
+  constructor({
+    id,
+    // attributes
+    name,
+    createdAt,
+    // includes
+    // references
+  } = {}) {
+    this.id = id;
+    // attributes
+    this.name = name;
+    this.createdAt = createdAt;
+    // includes
+    // references
+  }
+}
+
+module.exports = CertificationCenter;
