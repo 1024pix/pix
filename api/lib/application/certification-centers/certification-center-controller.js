adc8c99e3fe3659b8659c1324789304d9e084b95
Melanie B <mboudard@octo.com>
2019-01-16 12:01:56 +0100

    [pc-15] Add controller and usecase to get/find/save certificationCenter

--- api/lib/application/certification-centers/certification-center-controller.js
+++ api/lib/application/certification-centers/certification-center-controller.js
@@ -1,16 +1,45 @@
+const usecases = require('../../domain/usecases');
+const certificationCenterSerializer = require('../../infrastructure/serializers/jsonapi/certification-center-serializer');
+const controllerReplies = require('../../infrastructure/controller-replies');
+const {
+  NotFoundError: InfrastructureNotFoundError,
+} = require('../../infrastructure/errors');
+
+const {
+  NotFoundError,
+} = require('../../domain/errors');
+
 module.exports = {
 
   save(request, h) {
-    return true;
+    const certificationCenter = certificationCenterSerializer.deserialize(request.payload);
+    return usecases.saveCertificationCenter({ certificationCenter })
+      .then((certificationCenter) => certificationCenterSerializer.serialize(certificationCenter))
+      .catch((error) => {
+        return controllerReplies(h).error(error);
+      });
   },
 
   getById(request, h) {
     const certificationCenterId = request.params.id;
-    return certificationCenterId;
+    return usecases.getCertificationCenter({ id: certificationCenterId })
+      .then(certificationCenterSerializer.serialize)
+      .then(controllerReplies(h).ok)
+      .catch((error) => {
+        if (error instanceof NotFoundError) {
+          const err = new InfrastructureNotFoundError(error.message);
+          return controllerReplies(h).error(err);
+        }
+        return controllerReplies(h).error(error);
+      });
   },
-
 
   find(request, h) {
-    return [];
+    return usecases.findCertificationCenters()
+      .then(certificationCenterSerializer.serialize)
+      .then(controllerReplies(h).ok)
+      .catch((error) => {
+        return controllerReplies(h).error(error);
+      });
   },
-}
+};
