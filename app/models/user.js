d309dccb4bb13c932ef9cea53b460452a683d3d7
Jérémy Buget <jbuget@octo.com>
2019-01-18 00:39:10 +0100

    Update models Membership, Organization and User

--- app/models/user.js
+++ app/models/user.js
@@ -3,8 +3,13 @@ import DS from 'ember-data';
 const { attr, hasMany } = DS;
 
 export default DS.Model.extend({
+
+  // Props
   firstName: attr(),
   lastName: attr(),
   email: attr(),
+
+  // Relationships
   memberships: hasMany('membership'),
+
 });
