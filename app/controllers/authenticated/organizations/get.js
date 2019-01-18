1e78d89ce6455513837ae56d2d90461e64592136
Jérémy Buget <jbuget@octo.com>
2019-01-18 01:33:34 +0100

    Manage memberships in organization detail page

--- app/controllers/authenticated/organizations/get.js
+++ app/controllers/authenticated/organizations/get.js
@@ -1,22 +1,39 @@
 import Controller from '@ember/controller';
-/*import { inject as service } from '@ember/service';*/
-
+import { inject as service } from '@ember/service';
+import { reject } from 'rsvp';
 
 export default Controller.extend({
 
-  /*store: service(),*/
+  notifications: service('notification-messages'),
 
   actions: {
+
     updateOrganizationInformation() {
       return this.get('model.organization').save();
     },
 
-    async addMembership() {
-
+    async addMembership(email) {
       const organization = this.get('model.organization');
-      const user = (await this.store.query('user', { email: 'pixmaster@example.net' })).firstObject;
-      const membership = this.store.createRecord('membership', { organization, user });
-      return membership.save();
+      const matchingUsers = (await this.store.query('user', { email }));
+      const user = matchingUsers.findBy('email', email);
+
+      if (!user) {
+        this.get('notifications').error(`Il n'existe pas de compte utilisateur associé à l'adresse "${email}".`);
+        return reject();
+      }
+
+      return this.store
+        .createRecord('membership', { organization, user })
+        .save()
+        .then(async () => {
+          const users = await this.store.query('user', { organizationId: organization.id });
+          this.set('model.users', users);
+          this.get('notifications').success('Accès attribué avec succès.');
+        })
+        .catch((err) => {
+          this.get('notifications').error('Une erreur est survenue.');
+          return reject(err);
+        });
     }
   }
 });
--- app/routes/authenticated/organizations/get.js
+++ app/routes/authenticated/organizations/get.js
@@ -3,12 +3,10 @@ import Route from '@ember/routing/route';
 export default Route.extend({
 
   async model(params) {
-
     const store = this.get('store');
     const organization = await store.findRecord('organization', params.organization_id);
-    const users = await store.query('user', {
-      membershipOrganizationId: organization.id,
-    });
+    const users = await store.query('user', { organizationId: organization.id });
     return { organization, users };
-  },
+  }
+
 });
