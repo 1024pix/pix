348581d7df7b8ea1f83b3212b4f3ae71f0c1f579
David Riviere <driviere@octo.com>
2019-01-21 18:43:51 +0100

    Add test on membership organization

--- mirage/config.js
+++ mirage/config.js
@@ -23,9 +23,33 @@ export default function() {
 
     http://www.ember-cli-mirage.com/docs/v0.4.x/shorthands/
   */
- this.namespace = '/api';
+  this.namespace = '/api';
 
- this.get('/organizations/:id');
- this.get('/users');
- this.post('/memberships');
+  this.post('/memberships');
+
+  this.get('/organizations/:id');
+
+  this.get('/users', (schema, request) => {
+    let users = schema.users;
+
+    if (!request.queryParams) {
+      return users;
+    }
+
+    if (request.queryParams.organizationId) {
+      const organizationId = request.queryParams.organizationId;
+      const memberships = schema.memberships.where({ organizationId });
+      const userIds = memberships.models.map(membership => membership.userId);
+      users = users.find(userIds);
+    }
+
+    if (request.queryParams.email) {
+      const email = request.queryParams.email;
+      users = users.where({ email });
+    }
+
+    return users;
+
+  });
+
 }
