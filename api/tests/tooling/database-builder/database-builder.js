a7093ed333ebb539ecbcd97c90ceed42b688976c
Sébastien Roccaserra <sroccaserra@yahoo.com>
2019-01-23 16:55:38 +0100

    Always use postgreSQL when TEST_DATABASE_URL is set

--- api/tests/tooling/database-builder/database-builder.js
+++ api/tests/tooling/database-builder/database-builder.js
@@ -1,5 +1,3 @@
-const _ = require('lodash');
-
 const databaseBuffer = require('./database-buffer');
 const factory = require('./factory/index');
 
@@ -8,14 +6,6 @@ module.exports = class DatabaseBuilder {
     this.knex = knex;
     this.databaseBuffer = databaseBuffer;
     this.factory = factory;
-
-    this.databaseName = knex.client.database();
-    const clientName = knex.schema.client.config.client;
-    this.isSQLite = clientName === 'sqlite3';
-
-    const queries = _dbSpecificQueries[clientName];
-    this.listTablesQuery = queries.listTablesQuery;
-    this.truncateTableQuery = queries.truncateTableQuery;
   }
 
   commit() {
@@ -41,39 +31,5 @@ module.exports = class DatabaseBuilder {
       .then(() => {
         this.databaseBuffer.purge();
       });
-  }
-
-  async listAllTableNames() {
-    const bindings = this.isSQLite ? undefined : [this.databaseName];
-    const resultSet = await this.knex.raw(this.listTablesQuery, bindings);
-
-    const rows = this.isSQLite ? resultSet : resultSet.rows;
-    return _.map(rows, 'table_name');
-  }
-
-  async clearAllTables() {
-    const tableNames = await this.listAllTableNames();
-    const tablesToDelete = _.without(tableNames,
-      'knex_migrations',
-      'knex_migrations_lock',
-      'sqlite_sequence'
-    );
-
-    for(const tableName of tablesToDelete) {
-      await this.knex.raw(this.truncateTableQuery, [tableName]);
-    }
-  }
-
-};
-
-const _dbSpecificQueries = {
-  postgresql: {
-    listTablesQuery: 'SELECT table_name FROM information_schema.tables WHERE table_schema = current_schema() AND table_catalog = ?',
-    truncateTableQuery: 'TRUNCATE TABLE ?? CASCADE;'
-  },
-
-  sqlite3: {
-    listTablesQuery: 'SELECT name AS table_name FROM sqlite_master WHERE type=\'table\'',
-    truncateTableQuery: 'DELETE FROM ??;'
   }
 };
