65e545211a15c516566bf02a87d5c9fd68629b5d
lisequesnel <quesnel.lise@gmail.com>
2019-01-29 12:18:33 +0100

    [pf-485] Move contentReference to init function

--- mon-pix/app/components/result-item-campaign.js
+++ mon-pix/app/components/result-item-campaign.js
@@ -1,41 +1,14 @@
 import { computed } from '@ember/object';
 import Component from '@ember/component';
 
-const contentReference = {
-  ok: {
-    icon: 'check-circle',
-    color: 'green',
-    tooltip: 'Réponse correcte'
-  },
-  ko: {
-    icon: 'times-circle',
-    color: 'red',
-    tooltip: 'Réponse incorrecte'
-  },
-  aband: {
-    icon: 'times-circle',
-    color: 'grey',
-    tooltip: 'Sans réponse'
-  },
-  partially: {
-    icon: 'check-circle',
-    color: 'orange',
-    tooltip: 'Réponse partielle'
-  },
-  timedout: {
-    icon: 'times-circle',
-    color: 'red',
-    tooltip: 'Temps dépassé'
-  }
-};
-
 export default Component.extend({
 
   openComparison: null,
+  contentReference: null,
 
   resultItem: computed('answer.result', function() {
     if (!this.get('answer.result')) return;
-    return contentReference[this.get('answer.result')];
+    return this.contentReference[this.get('answer.result')];
   }),
 
   resultTooltip: computed('resultItem', function() {
@@ -55,6 +28,37 @@ export default Component.extend({
   textLength: computed('', function() {
     return window.innerWidth <= 767 ? 60 : 110;
   }),
+
+  init() {
+    this._super(...arguments);
+    this.contentReference = {
+      ok: {
+        icon: 'check-circle',
+        color: 'green',
+        tooltip: 'Réponse correcte'
+      },
+      ko: {
+        icon: 'times-circle',
+        color: 'red',
+        tooltip: 'Réponse incorrecte'
+      },
+      aband: {
+        icon: 'times-circle',
+        color: 'grey',
+        tooltip: 'Sans réponse'
+      },
+      partially: {
+        icon: 'check-circle',
+        color: 'orange',
+        tooltip: 'Réponse partielle'
+      },
+      timedout: {
+        icon: 'times-circle',
+        color: 'red',
+        tooltip: 'Temps dépassé'
+      }
+    };
+  },
 
   didRender() {
     this._super(...arguments);
