1225628b7ae6df1330391f350d1788000fc918d0
Melanie B <mboudard@octo.com>
2019-01-29 17:15:44 +0100

    [pf-335] Remove old menu and old page

--- mon-pix/app/components/pix-content-backdrop.js
+++
@@ -1,8 +0,0 @@
-import ContentBackdrop from 'ember-side-menu/components/content-backdrop';
-
-export default ContentBackdrop.extend({
-
-  touchStart() {
-    this.get('sideMenu').close();
-  }
-});
