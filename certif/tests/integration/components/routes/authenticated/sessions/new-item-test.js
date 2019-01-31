835447971c08e4a481b0b41ac25896e9a2788e4d
Forster <groux.forster@gmail.com>
2019-01-31 17:02:33 +0100

    [pc-6] add validations

--- certif/tests/integration/components/routes/authenticated/sessions/new-item-test.js
+++ certif/tests/integration/components/routes/authenticated/sessions/new-item-test.js
@@ -28,7 +28,7 @@ module('Integration | Component | routes/authenticated/session | new-item', func
     assert.dom('#session-examiner').exists();
     assert.dom('#session-description').exists();
     assert.dom('button[type="submit"]').exists();
-    assert.dom('#session-date').hasAttribute('maxLength', "10");
+    assert.dom('#session-date').hasAttribute('maxLength', "8");
     assert.dom('#session-time').hasAttribute('maxLength', "5");
     assert.dom('#session-description').hasAttribute('maxLength', "350");
   });
