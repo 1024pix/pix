90c47c33e5b7ca1d26c146e6b87c4a0e20fa4f60
Mathieu Laurent <mlaurent@octo.com>
2019-01-31 13:56:03 +0100

    [pc-6] add create session use case

---
+++ api/lib/domain/usecases/create-session.js
@@ -0,0 +1,36 @@
+const { ForbiddenAccess } = require('../errors');
+
+function _createSessionAsPixMaster(certificationCenterId, session, certificationCenterRepository, sessionRepository) {
+  if(certificationCenterId) {
+    return _setCertifCenterNameInSessionAndSave(session, certificationCenterId, certificationCenterRepository, sessionRepository);
+  }
+  return sessionRepository.save(session);
+}
+
+async function _createSessionAsNormalUser(userId, certificationCenterId, session, certificationCenterRepository, sessionRepository, userRepository) {
+  const userWithCertifCenters = await userRepository.getWithCertificationCenterMemberships(userId);
+
+  if(userWithCertifCenters.hasAccessToCertificationCenter(certificationCenterId)) {
+    return _setCertifCenterNameInSessionAndSave(session, certificationCenterId, certificationCenterRepository, sessionRepository);
+  }
+  throw new ForbiddenAccess('User is not a member of the certification center');
+}
+
+function _setCertifCenterNameInSessionAndSave(session, certificationCenterId, certificationCenterRepository, sessionRepository) {
+  return certificationCenterRepository.get(certificationCenterId)
+    .then((certificationCenter) => session.certificationCenter = certificationCenter.name)
+    .then(() => sessionRepository.save(session));
+}
+
+module.exports = async function createSession({ userId, session, certificationCenterRepository, sessionRepository, userRepository }) {
+  const certificationCenterId = session.certificationCenterId;
+  const user = await userRepository.get(userId);
+
+  // We keep this code here so that Anne-Cécile can still create the sessions the old way through Postman, for now :)
+  // To remove when we will not create sessions with no certifCenterId through Postman anymore
+  if(user.hasRolePixMaster) {
+    return _createSessionAsPixMaster(certificationCenterId, session, certificationCenterRepository, sessionRepository);
+  }
+
+  return _createSessionAsNormalUser(userId, certificationCenterId, session, certificationCenterRepository, sessionRepository, userRepository);
+};
