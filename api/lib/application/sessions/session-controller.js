dafcc3942ab28457c6a196f3f83098d524f34e5c
Thierry Loesch <thierry.loesch@amac02xh5b6jg5m.home>
2019-02-04 11:00:33 +0100

    [pc-10] Add route PATCH /api/sessions/{id}

--- api/lib/application/sessions/session-controller.js
+++ api/lib/application/sessions/session-controller.js
@@ -63,5 +63,9 @@ module.exports = {
       const serializedError = errorSerializer.serialize(error.getErrorMessage());
       return h.response(serializedError).code(400);
     }
+  },
+
+  update(request, h) {
+
   }
 };
