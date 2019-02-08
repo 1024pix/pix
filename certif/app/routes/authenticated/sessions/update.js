84fff439b3cb471520e79b9a45d5c67d50e75aa8
Thierry Loesch <tloesch@octo.com>
2019-02-08 18:01:22 +0100

    [pc-10] Fix date and time format in input field

--- certif/app/routes/authenticated/sessions/update.js
+++ certif/app/routes/authenticated/sessions/update.js
@@ -1,12 +1,18 @@
 import Route from '@ember/routing/route';
+import moment from 'moment';
 
 export default Route.extend({
 
   model(params) {
-    return this.store.findRecord('session', params.session_id);
+    return this.store.findRecord('session', params.session_id)
+      .then((session) => {
+        session.set('date', moment(session.get('date')).format('DD/MM/YYYY'));
+        session.set('time', moment(session.get('time'), 'HH:mm:ss').format('HH:mm'));
+        return session;
+      });
   },
 
-  deactivate: function() {
+  deactivate: function () {
     this.controller.model.rollbackAttributes();
   },
-})
+});
