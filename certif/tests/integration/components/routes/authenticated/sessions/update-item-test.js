0cc435168b2dda58f0b0b870d75acf783f8f8875
Thierry Loesch <tloesch@octo.com>
2019-02-15 14:58:13 +0100

    [pc-10] Add acceptance test : in update session inputs should be filled with correct values

--- certif/tests/integration/components/routes/authenticated/sessions/update-item-test.js
+++ certif/tests/integration/components/routes/authenticated/sessions/update-item-test.js
@@ -1,8 +1,9 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { click, fillIn, render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
 import EmberObject from '@ember/object';
+import hbs from 'htmlbars-inline-precompile';
+import moment from 'moment';
 
 module('Integration | Component | routes/authenticated/session | update-item', function(hooks) {
   setupRenderingTest(hooks);
@@ -10,8 +11,11 @@ module('Integration | Component | routes/authenticated/session | update-item', f
 
   hooks.beforeEach(function() {
     session = new EmberObject({ });
+    session.set('address', '20 rue du gros chat');
+    session.set('room', '2B');
     session.set('date', '2028-05-26T22:00:00.000Z');
-    session.set('time', '20:00:00');
+    session.set('time', '20:00');
+    session.set('examiner', 'Monsieur Rougeaud');
     this.set('updateSessionSpy', (updatedSession) => {
       session = updatedSession;
     });
@@ -34,6 +38,23 @@ module('Integration | Component | routes/authenticated/session | update-item', f
     assert.dom('#session-time').exists();
     assert.dom('#session-examiner').exists();
     assert.dom('#session-description').exists();
+  });
+
+  test('it should fill inputs with session attributes values', async function(assert) {
+    // given
+    this.set('model', session);
+
+    // when
+    await render(hbs`{{routes/authenticated/sessions/update-item session=model updateSession=(action updateSessionSpy) cancel=(action cancelSpy)}}`);
+
+    // then
+    const expectedDate = moment(session.get('date')).format('DD/MM/YYYY');
+
+    assert.dom('#session-address').hasValue(session.get('address'));
+    assert.dom('#session-room').hasValue(session.get('room'));
+    assert.dom('#session-date').hasValue(expectedDate);
+    assert.dom('#session-time').hasValue(session.get('time'));
+    assert.dom('#session-examiner').hasValue(session.get('examiner'));
   });
 
   test('it should send session update action when submitted', async function(assert) {
