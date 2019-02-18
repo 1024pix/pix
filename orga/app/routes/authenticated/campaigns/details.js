7cd7907a032832e3b57eeee8c0416ea196458d77
lisequesnel <quesnel.lise@gmail.com>
2019-02-18 14:13:33 +0100

    [po-177] Add navbar + Split participants and parameters in different routes

--- orga/app/routes/authenticated/campaigns/details.js
+++ orga/app/routes/authenticated/campaigns/details.js
@@ -1,9 +1,19 @@
-
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+  navbarItem: 'parameters',
+
+  beforeModel(transition) {
+    this.set('navbarItem', transition.targetName.split('.')[3]);
+    this._super(...arguments);
+  },
+
   model(params) {
-    return this.store.findRecord('campaign', params.campaign_id, { include: 'targetProfile' });
+    return this.store.findRecord('campaign', params.campaign_id, { include: 'targetProfile' })
+      .then((campaign) => {
+        return { campaign, navbarItem: this.get('navbarItem') };
+      });
   },
 
   afterModel(model) {
