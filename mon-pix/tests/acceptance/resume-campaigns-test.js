a9f650080ddaccc7e48282560fb716b4ae1379b6
LJA <ljacquemin@octo.com>
2019-02-19 17:14:59 +0100

    Fixed test caused by usage of ember controller
    Ember controller is a singleton. As such, it should not be used
    to store state variables, because they will be shared among different
    transitions and cause unexpected results.

--- mon-pix/tests/acceptance/resume-campaigns-test.js
+++ mon-pix/tests/acceptance/resume-campaigns-test.js
@@ -155,6 +155,7 @@ describe('Acceptance | Campaigns | Resume Campaigns', function() {
 
   describe('Resume 2 campaigns', function() {
     beforeEach(async function() {
+
       server.create('assessment', {
         id: 1,
         type: 'SMART_PLACEMENT',
@@ -184,6 +185,7 @@ describe('Acceptance | Campaigns | Resume Campaigns', function() {
       });
 
       await authenticateAsSimpleUser();
+
     });
 
     context('When user has finished but not shared 2 campaigns', function() {
@@ -212,17 +214,11 @@ describe('Acceptance | Campaigns | Resume Campaigns', function() {
     context('When user has finished both campaigns but shared only 1 campaign', function() {
 
       beforeEach(async function() {
-        server.create('campaignParticipation', {
-          id: 1,
-          isShared: true,
-          campaignId: 1,
-          assessmentId: 1,
-        });
+        await visit('/campagnes/AZERTY1');
+        await click('.skill-review__share__button');
       });
 
       it('should show thanks message for the first campaign', async function() {
-        // when
-        await visit('/campagnes/AZERTY1');
 
         // then
         return andThen(() => {
