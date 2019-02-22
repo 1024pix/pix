1d4cf77a2f3f36eff3fad24ebc0da4261d74e2bd
Jérémy Buget <jbuget@gmail.com>
2019-02-22 17:55:23 +0100

    [API] Improve error management for endpoint /api/challanges/:id

--- api/lib/application/challenges/challenge-controller.js
+++ api/lib/application/challenges/challenge-controller.js
@@ -1,25 +1,29 @@
-const Boom = require('boom');
-
+const { NotFoundError: DomainNotFoundError } = require('../../domain/errors');
+const controllerReplies = require('../../infrastructure/controller-replies');
+const { NotFoundError: InfrastructureNotFoundError } = require('../../infrastructure/errors');
+const logger = require('../../infrastructure/logger');
 const challengeRepository = require('../../infrastructure/repositories/challenge-repository');
 const challengeSerializer = require('../../infrastructure/serializers/jsonapi/challenge-serializer');
 
-const logger = require('../../infrastructure/logger');
-
 module.exports = {
 
-  get(request) {
+  async get(request, h) {
+    try {
+      const challengeId = request.params.id;
 
-    return challengeRepository
-      .get(request.params.id)
-      .then((challenge) => challengeSerializer.serialize(challenge))
-      .catch((err) => {
-        logger.error(err);
-        if (err.error && 'MODEL_ID_NOT_FOUND' === err.error.type) {
-          throw Boom.notFound(err);
-        }
-        throw Boom.badImplementation(err);
-      });
+      const challenge = await challengeRepository.get(challengeId);
 
+      return challengeSerializer.serialize(challenge);
+    } catch (err) {
+
+      if (err instanceof DomainNotFoundError) {
+        const error = new InfrastructureNotFoundError(err.message);
+        return controllerReplies(h).error(error);
+      }
+
+      logger.error(err);
+      return controllerReplies(h).error(err);
+    }
   },
 
 };
