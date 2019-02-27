fe4532cdee2154367a74e3501d80b6cfffb11e56
LJA <ljacquemin@octo.com>
2019-02-27 10:53:25 +0100

    [po-177] Added method to paginate on relationship for campaign participations
    When requesting the database for a paginated results including relationships,
    the relationship must be included *before* the pagination is computed. The db
    must first aggregate all rows  with an inner-join, and then it can compute
    the pages and gives back the desired result.

--- api/tests/test-helper.js
+++ api/tests/test-helper.js
@@ -137,12 +137,12 @@ function catchErr(promiseFn, ctx) {
   return async (...args) => {
     try {
       await promiseFn.call(ctx, ...args);
-      return "should have thrown an error";
+      return 'should have thrown an error';
     } catch (err) {
       return err;
     }
-  }
- }
+  };
+}
 
 module.exports = {
   airtableBuilder,
--- api/tests/unit/domain/usecases/find-campaign-participations_test.js
+++ api/tests/unit/domain/usecases/find-campaign-participations_test.js
@@ -7,32 +7,32 @@ describe('Unit | UseCase | find-campaign-participations', () => {
   let campaignParticipationsResult;
   let requestErr;
 
-  let options
+  let options;
   let userId;
   let assessmentId;
   let campaignParticipations;
 
   const smartPlacementAssessmentRepository = { checkIfAssessmentBelongToUser: sinon.stub() };
-  const campaignParticipationRepository = { find: sinon.stub() };
+  const campaignParticipationRepository = { find: sinon.stub(), findWithUsersPaginated: sinon.stub() };
 
   beforeEach(() => {
     options = null;
     requestErr = null;
-    userId = "dummy userId";
-    assessmentId = "dummy assessmentId";
-    campaignParticipations = ["some campaigns participations"];
+    userId = 'dummy userId';
+    assessmentId = 'dummy assessmentId';
+    campaignParticipations = ['some campaigns participations'];
   });
 
   context('the campaign participation is searched by assessment id', () => {
     beforeEach(() => {
-      options = { filter: { assessmentId }}
+      options = { filter: { assessmentId } };
     });
     context('the assessment belongs to the user', () => {
       beforeEach(async () => {
         smartPlacementAssessmentRepository.checkIfAssessmentBelongToUser.resolves(true);
         campaignParticipationRepository.find.resolves(campaignParticipations);
 
-        campaignParticipationsResult = await findCampaignParticipations({ userId, options, campaignParticipationRepository, smartPlacementAssessmentRepository })
+        campaignParticipationsResult = await findCampaignParticipations({ userId, options, campaignParticipationRepository, smartPlacementAssessmentRepository });
       });
       it('should check if the assessment belongs to the user', () => {
         expect(smartPlacementAssessmentRepository.checkIfAssessmentBelongToUser).to.have.been.calledWithExactly(assessmentId, userId);
@@ -43,27 +43,27 @@ describe('Unit | UseCase | find-campaign-participations', () => {
       it('should return the campaign participations', () => {
         expect(campaignParticipationsResult).to.deep.equal(campaignParticipations);
       });
-    })
+    });
     context('the assessment does not belong to the user', () => {
       beforeEach(async () => {
         smartPlacementAssessmentRepository.checkIfAssessmentBelongToUser.resolves(false);
 
-        requestErr = await catchErr(findCampaignParticipations)({ userId, options, campaignParticipationRepository, smartPlacementAssessmentRepository })
+        requestErr = await catchErr(findCampaignParticipations)({ userId, options, campaignParticipationRepository, smartPlacementAssessmentRepository });
       });
       it('should throw a UserNotAuthorizedToAccessEntity error', () => {
         expect(requestErr).to.be.instanceOf(UserNotAuthorizedToAccessEntity);
       });
-    })
+    });
   });
   context('the campaign participation is not searched by assessment id', () => {
     beforeEach(async () => {
       options = { filter: {} };
-      campaignParticipationRepository.find.resolves(campaignParticipations);
+      campaignParticipationRepository.findWithUsersPaginated.resolves(campaignParticipations);
 
-      campaignParticipationsResult = await findCampaignParticipations({ userId, options, campaignParticipationRepository, smartPlacementAssessmentRepository })
+      campaignParticipationsResult = await findCampaignParticipations({ userId, options, campaignParticipationRepository, smartPlacementAssessmentRepository });
     });
     it('should find the campaign participations', () => {
-      expect(campaignParticipationRepository.find).to.have.been.calledWithExactly(options);
+      expect(campaignParticipationRepository.findWithUsersPaginated).to.have.been.calledWithExactly(options);
     });
     it('should return the campaign participations', () => {
       expect(campaignParticipationsResult).to.deep.equal(campaignParticipations);
