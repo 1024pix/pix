78f0484a227a7e0871ed2cffbce3c6cda5c6b477
Melanie B <mboudard@octo.com>
2019-03-04 12:12:10 +0100

    Remove useless "unsigned" on userId, not used in PG

--- api/db/migrations/20190301111215_add_column_user_id_to_knowledge_elements.js
+++ api/db/migrations/20190301111215_add_column_user_id_to_knowledge_elements.js
@@ -2,7 +2,7 @@ const TABLE_NAME = 'knowledge-elements';
 
 exports.up = function(knex) {
   return knex.schema.table(TABLE_NAME, (table) => {
-    table.integer('userId').unsigned().references('users.id').index();
+    table.integer('userId').references('users.id').index();
   });
 };
 
