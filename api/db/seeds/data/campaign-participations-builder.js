69cf7126384e814c1db860b1a4f9ff467ee6616a
LJA <ljacquemin@octo.com>
2019-03-04 18:34:14 +0100

    [po-177] Rgpd

--- api/db/seeds/data/campaign-participations-builder.js
+++ api/db/seeds/data/campaign-participations-builder.js
@@ -3,18 +3,19 @@ const Assessment = require('../../../lib/domain/models/Assessment');
 module.exports = function addCampaignWithParticipations({ databaseBuilder }) {
 
   const pixMembers = [
-    { firstName: 'Mélanie', lastName: 'Boudard' },
-    { firstName: 'Mathieu', lastName: 'Laurent' },
-    { firstName: 'Jérémy', lastName: 'Buget' },
-    { firstName: 'Léo', lastName: 'Jacquemin' },
-    { firstName: 'Forster', lastName: 'Groux' },
-    { firstName: 'Thierry', lastName: 'Loesch' },
-    { firstName: 'Jonathan', lastName: 'Perret' },
-    { firstName: 'Stéphane', lastName: 'Bedeau' },
-    { firstName: 'Lise', lastName: 'Quesnel' },
-    { firstName: 'Sébastien', lastName: 'Roccaserra' },
-    { firstName: 'Thomas', lastName: 'Wickham' },
-    { firstName: 'Antoine', lastName: 'Boileau' },
+    { firstName: 'Mélanie', lastName: 'Darboo' },
+    { firstName: 'Matteo', lastName: 'Lorenzio' },
+    { firstName: 'Jérémy', lastName: 'Bugietta' },
+    { firstName: 'Léo', lastName: 'Subzéro' },
+    { firstName: 'Forster', lastName: 'Gillay Djones' },
+    { firstName: 'Thierry', lastName: 'Donckele' },
+    { firstName: 'Jaune', lastName: 'Attend' },
+    { firstName: 'Stéphan', lastName: 'Deumonaco' },
+    { firstName: 'Lise', lastName: 'Nelkay' },
+    { firstName: 'Sébastien', lastName: 'Serra Oupas' },
+    { firstName: 'Thomas', lastName: 'Whiskas' },
+    { firstName: 'Antoine', lastName: 'Boiduvin' },
+    { firstName: 'Brandone', lastName: 'Bro' },
   ];
 
   const startCampaign = (member) => {
