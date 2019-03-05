eab4982d49282ce177bd5fdc01a957cc406b27d7
lisequesnel <quesnel.lise@gmail.com>
2019-03-05 11:52:37 +0100

    [po-177] Change lodash upperFirst by capitalize

--- orga/app/components/routes/authenticated/campaigns/details/participants-tab.js
+++ orga/app/components/routes/authenticated/campaigns/details/participants-tab.js
@@ -1,11 +1,11 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
-import _upperFirst from 'lodash/upperFirst';
+import _capitalize from 'lodash/capitalize';
 
 export default Component.extend({
 
   idPixLabelDisplay: computed('campaign.idPixLabel', function () {
     const idPixLabel = this.get('campaign.idPixLabel');
-    return _upperFirst(idPixLabel);
+    return _capitalize(idPixLabel);
   }),
 });
