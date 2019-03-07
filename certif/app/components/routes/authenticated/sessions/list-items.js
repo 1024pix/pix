985e94e009b9577eda78981cd5c22ad9434d581b
lisequesnel <quesnel.lise@gmail.com>
2019-03-07 17:16:27 +0100

    [pf-534] Do not use anymore ember-table for displaying tables in Pix Certif

--- certif/app/components/routes/authenticated/sessions/list-items.js
+++ certif/app/components/routes/authenticated/sessions/list-items.js
@@ -6,43 +6,4 @@ const SORTING_ORDER = ['date:desc', 'time:desc'];
 export default Component.extend({
   sortingOrder: SORTING_ORDER,
   sortedSessions: computed.sort('sessions', 'sortingOrder'),
-  columns: null,
-
-  init() {
-    this._super(...arguments);
-    this.set('columns', [
-      {
-        name: 'ID session',
-        valuePath: 'id',
-        width: 8
-      },
-      {
-        name: 'Nom du site',
-        valuePath: 'address',
-        width: 40
-      },
-      {
-        name: 'Salle',
-        valuePath: 'room',
-        width: 30
-      },
-      {
-        name: 'Date',
-        valuePath: 'date',
-        isDate: true,
-        width: 10
-      },
-      {
-        name: 'Heure',
-        valuePath: 'time',
-        isHour: true,
-        width: 5
-      },
-      {
-        name: 'Surveillant',
-        valuePath: 'examiner',
-        width: 30
-      }
-    ]);
-  },
 });
