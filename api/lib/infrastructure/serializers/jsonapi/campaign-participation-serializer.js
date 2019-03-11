68501d177227a13601bbfb48df92eb08e19c75d7
Forster <groux.forster@gmail.com>
2019-03-11 15:46:25 +0100

    [po-190] add non included relationship to serializer

--- api/lib/infrastructure/serializers/jsonapi/campaign-participation-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/campaign-participation-serializer.js
@@ -7,13 +7,15 @@ module.exports = {
 
   serialize(campaignParticipation, meta) {
     return new Serializer('campaign-participation', {
-      attributes: ['isShared', 'sharedAt', 'createdAt', 'participantExternalId', 'campaign', 'user', 'campaignParticipationResult'],
+      attributes: ['isShared', 'sharedAt', 'createdAt', 'participantExternalId',  'campaign', 'user', 'campaignParticipationResult'],
       campaign: {
         ref: 'id',
+        included: _includeRelationship(campaignParticipation, 'campaign'),
         attributes: ['code', 'title']
       },
       user: {
         ref: 'id',
+        included: _includeRelationship(campaignParticipation, 'user'),
         attributes: ['firstName', 'lastName'],
       },
       campaignParticipationResult: {
@@ -36,5 +38,18 @@ module.exports = {
         campaignParticipation.campaignId = _.get(json.data, ['relationships', 'campaign', 'data', 'id']);
         return new CampaignParticipation(campaignParticipation);
       });
-  }
+  },
 };
+
+function _includeRelationship(campaignParticipation, relationshipName) {
+  if (_.isEmpty(campaignParticipation)) {
+    // default value for included
+    return true;
+  }
+
+  const item = Array.isArray(campaignParticipation)
+    ? campaignParticipation[0]
+    : campaignParticipation;
+
+  return Object.keys(item[relationshipName]).length > 1;
+}
