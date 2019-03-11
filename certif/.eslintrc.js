c8396482974907d00ce7f5e1a337a3d2bb76d94f
Jérémy Buget <jbuget@gmail.com>
2019-03-11 17:37:41 +0100

    [Certif] Bump Ember from 3.7 to 3.8

--- certif/.eslintrc.js
+++ certif/.eslintrc.js
@@ -29,7 +29,8 @@ module.exports = {
         'testem.js',
         'blueprints/*/index.js',
         'config/**/*.js',
-        'lib/*/index.js'
+        'lib/*/index.js',
+        'server/**/*.js'
       ],
       parserOptions: {
         sourceType: 'script',
