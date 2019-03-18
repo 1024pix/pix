6d1e1feed32515e9c9611a9920b247e3cce6ded5
Melanie B <mboudard@octo.com>
2019-03-18 16:19:07 +0100

    [pf-549] Return pixScore and Level through assessmentResult

--- api/lib/domain/usecases/get-assessment.js
+++ api/lib/domain/usecases/get-assessment.js
@@ -1,5 +1,3 @@
-const { MAX_REACHABLE_LEVEL } = require('../../../lib/domain/models/Profile');
-const scoringService = require('../services/scoring/scoring-service');
 const { NotFoundError } = require('../errors');
 
 module.exports = async function getAssessment(
@@ -7,32 +5,21 @@ module.exports = async function getAssessment(
     // arguments
     assessmentId,
     // dependencies
-    answerRepository,
     assessmentRepository,
-    challengeRepository,
-    competenceRepository,
-    courseRepository,
-    skillRepository,
   }) {
-
-  const dependencies = { answerRepository, challengeRepository, competenceRepository, courseRepository, skillRepository };
-
   const assessment = await assessmentRepository.get(assessmentId);
-
   if (!assessment) {
     throw new NotFoundError(`Assessment not found for ID ${assessmentId}`);
   }
+  const assessmentResult = assessment.getLastAssessmentResult();
 
-  /*
-   * FIXME
-   * Le score et le niveau ne sont utilisés que dans l'affichage de la page de résultat d'un parcours de positionnement
-   * par compétence. Il faudrait sortir ces informations dans une sous-ressource de Assessment et faire un appel de plus
-   * dans le front plutôt qu'engager un calcul lourd à chaque affichage d'épreuve ou chaque récupération d'assessment.
-   */
-  const assessmentScore = await scoringService.calculateAssessmentScore(dependencies, assessment);
-  assessment.estimatedLevel = Math.min(assessmentScore.level, MAX_REACHABLE_LEVEL);
-  assessment.pixScore = assessmentScore.nbPix;
+  if(assessmentResult) {
+    assessment.estimatedLevel = assessmentResult.level;
+    assessment.pixScore = assessmentResult.pixScore;
+  } else {
+    assessment.estimatedLevel = null;
+    assessment.pixScore = null;
+  }
 
   return assessment;
-
 };
