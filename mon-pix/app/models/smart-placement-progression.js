59cfd68b062bed5571ab916f8c06504da4a0e655
lisequesnel <quesnel.lise@gmail.com>
2019-03-18 18:45:29 +0100

    [pf-543] initiate page

--- mon-pix/app/models/smart-placement-progression.js
+++ mon-pix/app/models/smart-placement-progression.js
@@ -5,6 +5,10 @@ export default DS.Model.extend({
   validationRate: DS.attr('number'),
   completionRate: DS.attr('number'),
 
+  validationPercentage: computed('validationRate', function() {
+    return Number((this.get('validationRate') * 100).toFixed(0));
+  }),
+
   masteryPercentage: computed('validationRate', function() {
     return Number((this.get('validationRate') * 100).toFixed(0)) + '%';
   }),
---
+++ mon-pix/app/static-data/domain-colors.js
@@ -0,0 +1,18 @@
+export default {
+  colors: [{
+    domain: '1',
+    color: '#F1A141',
+  }, {
+    domain: '2',
+    color: '#57C884',
+  }, {
+    domain: '3',
+    color: '#12A3FF',
+  }, {
+    domain: '4',
+    color: '#FF3F94',
+  }, {
+    domain: '5',
+    color: '#574DA6',
+  }],
+};
