37e8b3c96b667170be88e60a143bd178369b6659
Jonas Grillères <grilleres.j@gmail.com>
2019-03-25 12:18:33 +0100

    [po-193] add empty table when no result or results not shared

---
+++ orga/tests/integration/components/routes/authenticated/campaigns/details/participants/results-item-test.js
@@ -0,0 +1,101 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { run } from '@ember/runloop';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | routes/authenticated/campaign/details/participants | results-item', function (hooks) {
+  setupRenderingTest(hooks);
+
+  let store;
+
+  hooks.beforeEach(function () {
+    run(() => {
+      store = this.owner.lookup('service:store');
+    });
+  });
+
+  test('it should display a sentence when there is no result yet', async function (assert) {
+    // given
+    const campaignParticipation = run(() => store.createRecord('campaignParticipation', {
+      isShared: false,
+      campaignParticipationResult: null
+    }));
+
+    this.set('campaignParticipation', campaignParticipation);
+
+    // when
+    await render(hbs`{{routes/authenticated/campaigns/details/participants/results-item campaignParticipation=campaignParticipation}}`);
+
+    // then
+    assert.dom('.participant-results-content--multiple .content-text--big').hasText('-');
+    assert.dom('.table__empty').hasText('En attente de résultats');
+  });
+
+  test('it should display a sentence when the results have not been shared yet', async function (assert) {
+    // given
+    const campaignParticipation = run(() => store.createRecord('campaignParticipation', {
+      isShared: false,
+      campaignParticipationResult: {
+        totalSkillsCount: 30,
+        testedSkillsCount: 29,
+        validatedSkillsCount: 15,
+        competenceResults: [{
+          name: 'Compétence 1',
+          index: '1.1',
+          totalSkillsCount: 10,
+          testedSkillsCount: 9,
+          validatedSkillsCount: 5,
+        }]
+      }
+    }));
+
+    this.set('campaignParticipation', campaignParticipation);
+
+    // when
+    await render(hbs`{{routes/authenticated/campaigns/details/participants/results-item campaignParticipation=campaignParticipation}}`);
+
+    // then
+    assert.dom('.participant-results-content--multiple .content-text--big').hasText('-');
+    assert.dom('.table__empty').hasText('En attente de résultats');
+  });
+
+  test('it should display the results', async function (assert) {
+    // given
+    const competenceResult = run(() => store.createRecord('competenceResult', {
+      name: 'Compétence 1',
+      index: '1.1',
+      totalSkillsCount: 10,
+      testedSkillsCount: 9,
+      validatedSkillsCount: 5,
+    }));
+
+    const campaignParticipationResult = run(() => store.createRecord('campaignParticipationResult', {
+      totalSkillsCount: 30,
+      testedSkillsCount: 29,
+      validatedSkillsCount: 15,
+      competenceResults: [competenceResult]
+    }));
+
+    const campaignParticipation = run(() => store.createRecord('campaignParticipation', {
+      isShared: true,
+      campaignParticipationResult
+    }));
+
+    this.set('campaignParticipation', campaignParticipation);
+
+    // when
+    await render(hbs`{{routes/authenticated/campaigns/details/participants/results-item campaignParticipation=campaignParticipation}}`);
+
+    // then
+    assert.dom('.participant-results-content--multiple .content-text--big').hasText('1');
+    assert.dom('table tbody tr td span:nth-child(2)').hasText('Compétence 1');
+    assert.dom('.participant-results-content__circle-chart-value').hasText('50%');
+  });
+});
+
+
+
+
+
+
