73c0d030a33f5c78e191dca68ab9fa79099f21e6
lisequesnel <quesnel.lise@gmail.com>
2019-03-25 09:57:53 +0100

    [po-193] Initiate table + add components

---
+++ orga/app/models/competence-result.js
@@ -0,0 +1,28 @@
+import DS from 'ember-data';
+import { computed } from '@ember/object';
+import domainColors from 'pix-orga/static-data/domain-colors';
+
+const { Model, attr, belongsTo } = DS;
+
+export default Model.extend({
+  name: attr('string'),
+  index: attr('string'),
+  totalSkillsCount: attr('number'),
+  testedSkillsCount: attr('number'),
+  validatedSkillsCount: attr('number'),
+  campaignParticipationResult: belongsTo('campaignParticipationResult'),
+
+  totalSkillsCountPercentage: computed('totalSkillsCount', 'campaignParticipationResult', function() {
+    return Math.round(this.totalSkillsCount * 100 / this.campaignParticipationResult.get('maxTotalSkillsCountInCompetences'));
+  }),
+
+  validatedSkillsPercentage: computed('validatedSkillsCount', 'totalSkillsCount', function() {
+    return Math.round(this.validatedSkillsCount * 100 / this.totalSkillsCount);
+  }),
+
+  domainColor: computed('index', function() {
+    const domainIndex = this.index.charAt(0);
+    const foundDomain = domainColors.find((colors) => colors.domain === domainIndex);
+    return foundDomain.color;
+  }),
+});
