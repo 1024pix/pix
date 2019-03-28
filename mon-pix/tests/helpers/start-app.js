e0bc7b5efc52039ab6c7a953abb6ed780a38085a
Jérémy Buget <jbuget@gmail.com>
2019-03-28 19:34:40 +0100

    Fix deprecation "ember-polyfills.deprecate-merge"
    Message:
    DEPRECATION: Use of `merge` has been deprecated. Please use `assign` instead. [deprecation id: ember-polyfills.deprecate-merge] See https://emberjs.com/deprecations/v3.x/#toc_ember-polyfills-deprecate-merge for more details.

--- mon-pix/tests/helpers/start-app.js
+++ mon-pix/tests/helpers/start-app.js
@@ -1,13 +1,14 @@
 import Application from '../../app';
 import config from '../../config/environment';
 import { run } from '@ember/runloop';
-import { merge } from '@ember/polyfills';
+import { assign } from '@ember/polyfills';
+
 import './responsive';
 
 export default function startApp(attrs) {
-  let attributes = merge({}, config.APP);
+  let attributes = assign({}, config.APP);
   attributes.autoboot = true;
-  attributes = merge(attributes, attrs); // use defaults, but you can override;
+  attributes = assign(attributes, attrs); // use defaults, but you can override;
 
   return run(() => {
     const application = Application.create(attributes);
