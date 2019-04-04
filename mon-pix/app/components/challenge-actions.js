a6542e35740b8f5d46ac7981ca816f6d2dbfb587
David Hérault <david.herault@pix.fr>
2019-04-04 14:24:17 +0200

    as a user, i want to press enter to validate a question

--- mon-pix/app/components/challenge-actions.js
+++ mon-pix/app/components/challenge-actions.js
@@ -1,8 +1,9 @@
 import { equal } from '@ember/object/computed';
 import Component from '@ember/component';
+import ChallengeItemGeneric from './challenge-item-generic';
 
-const pendingValue = 'pending';
-const enableValue = 'enable';
+const { enabled, pending, offline } = ChallengeItemGeneric.buttonStatuses;
+
 export default Component.extend({
 
   classNames: ['challenge-actions'],
@@ -10,35 +11,20 @@ export default Component.extend({
   challengeSkipped: null, // action
   answerValidated: null, // action
 
-  _validateButtonStatus: enableValue, // enable, pending, offline
-  _skipButtonStatus: enableValue,
-  isValidateButtonEnable: equal('_validateButtonStatus', enableValue),
-  isValidateButtonPending: equal('_validateButtonStatus', pendingValue),
-  isValidateButtonOffline: equal('_validateButtonStatus', 'offline'),
+  isValidateButtonEnable: equal('validateButtonStatus', enabled),
+  isValidateButtonPending: equal('validateButtonStatus', pending),
+  isValidateButtonOffline: equal('validateButtonStatus', offline),
 
-  isSkipButtonEnable: equal('_skipButtonStatus', enableValue),
-  isSkipButtonPending: equal('_skipButtonStatus', pendingValue),
-
-  didUpdateAttrs() {
-    this._super(...arguments);
-    this.set('_validateButtonStatus', enableValue);
-    this.set('_skipButtonStatus', enableValue);
-  },
+  isSkipButtonEnable: equal('skipButtonStatus', enabled),
+  isSkipButtonPending: equal('skipButtonStatus', pending),
 
   actions: {
     skipChallenge() {
-      if (this._validateButtonStatus === enableValue) {
-        this.set('_skipButtonStatus', pendingValue);
-        this.challengeSkipped();
-      }
+      this.challengeSkipped();
     },
 
     validateAnswer() {
-      if (this._skipButtonStatus === enableValue) {
-        this.set('_validateButtonStatus', pendingValue);
-        this.answerValidated()
-          .catch(() => this.set('_validateButtonStatus', enableValue));
-      }
-    }
-  }
+      this.answerValidated();
+    },
+  },
 });
