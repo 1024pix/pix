3ef4e154bc0c9b12b85b0daaf498d6de348c5a76
LJA <ljacquemin@octo.com>
2019-04-05 16:53:16 +0200

    [po-196] Improved method that ensure user has access to campaign participations by adding owner access

--- api/lib/infrastructure/repositories/campaign-repository.js
+++ api/lib/infrastructure/repositories/campaign-repository.js
@@ -76,9 +76,11 @@ module.exports = {
 
   checkIfUserOrganizationHasAccessToCampaign(campaignId, userId) {
     return BookshelfCampaign
-      .where({ 'campaigns.id': campaignId, userId })
       .query((qb) => {
-        qb.innerJoin('memberships', 'campaigns.organizationId', 'memberships.organizationId');
+        qb.where({ 'campaigns.id': campaignId, 'memberships.userId': userId })
+          .orWhere({ 'campaigns.id': campaignId, 'organizations.userId': userId });
+        qb.innerJoin('memberships', 'memberships.organizationId', 'campaigns.organizationId');
+        qb.innerJoin('organizations', 'organizations.id', 'campaigns.organizationId');
       })
       .fetch({
         require: true,
