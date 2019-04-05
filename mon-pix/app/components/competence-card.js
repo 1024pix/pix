d30bad289ff1f06a10f13991ab3cce656e86e8bb
lisequesnel <quesnel.lise@gmail.com>
2019-04-05 12:02:22 +0200

    [pf-522] display 95 as max percentage + add tests on it

--- mon-pix/app/components/competence-card.js
+++ mon-pix/app/components/competence-card.js
@@ -4,6 +4,7 @@ import { htmlSafe } from '@ember/string';
 import areaColors from 'mon-pix/static-data/area-colors';
 
 const NUMBER_OF_PIX_BY_LEVEL = 8;
+const MAX_DISPLAYED_PERCENTAGE = 95;
 
 export default Component.extend({
 
@@ -13,7 +14,8 @@ export default Component.extend({
   }),
 
   percentageAheadOfNextLevel: computed('pixScoreAheadOfNextLevel', function() {
-    return this.pixScoreAheadOfNextLevel / NUMBER_OF_PIX_BY_LEVEL * 100;
+    const percentage = this.pixScoreAheadOfNextLevel / NUMBER_OF_PIX_BY_LEVEL * 100;
+    return percentage >= MAX_DISPLAYED_PERCENTAGE ? MAX_DISPLAYED_PERCENTAGE : percentage;
   }),
 
   displayedLevel: computed('level,percentageAheadOfNextLevel', function() {
