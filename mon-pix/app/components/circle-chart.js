c1ea74b88a5dc881f906d86f45e02078c0853558
lisequesnel <quesnel.lise@gmail.com>
2019-04-05 14:45:17 +0200

    [pf-522] pass modifier classes to competence-card instead of values for css

--- mon-pix/app/components/circle-chart.js
+++ mon-pix/app/components/circle-chart.js
@@ -1,17 +1,7 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
-import { htmlSafe } from '@ember/string';
 
 export default Component.extend({
 
   classNames: ['circle-chart'],
-
-  strokeWidthStyle: computed('strokeWidth', function() {
-    return htmlSafe(`stroke-width: ${this.strokeWidth}`);
-  }),
-
-  chartWidthStyle: computed('chartWidth', function() {
-    return htmlSafe(`width: ${this.chartWidth}px; height: ${this.chartWidth}px`);
-  }),
 
 });
