f978e13c011d3afbeadd27077f5d549029c0fe7a
LJA <ljacquemin@octo.com>
2019-04-08 15:19:34 +0200

    [po-196] Adapted the test to reflect the fact that code use a getter instead of prop

--- api/tests/unit/domain/models/CampaignParticipation_test.js
+++ api/tests/unit/domain/models/CampaignParticipation_test.js
@@ -1,4 +1,6 @@
 const CampaignParticipation = require('../../../../lib/domain/models/CampaignParticipation');
+const SmartPlacementKnowledgeElement = require('../../../../lib/domain/models/SmartPlacementKnowledgeElement');
+const CampaignParticipationResult = require('../../../../lib/domain/models/CampaignParticipationResult');
 const { expect, domainBuilder } = require('../../../test-helper');
 
 describe('Unit | Domain | Models | CampaignParticipation', () => {
@@ -36,5 +38,69 @@ describe('Unit | Domain | Models | CampaignParticipation', () => {
       expect(targetProfileId).to.equal(null);
     });
 
+  });
+
+  describe('#addCampaignParticipationResult', () => {
+    const campaignParticipationId = 'campaignParticipationId';
+    const userId = 'userId';
+    const assessmentId = 'assessmentId';
+
+    const skills = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }];
+    const knowledgeElements = [
+      new SmartPlacementKnowledgeElement({ skillId: 1, status: 'validated' }),
+      new SmartPlacementKnowledgeElement({ skillId: 2, status: 'invalidated' }),
+      new SmartPlacementKnowledgeElement({ skillId: 7, status: 'validated' }),
+    ];
+
+    const competences = [
+      { id: 1, name: 'Economie symbiotique', index: '5.1', skills: [1] },
+      { id: 2, name: 'Désobéissance civile', index: '6.9', skills: [2, 3, 4] },
+      { id: 3, name: 'Démocratie liquide', index: '8.6', skills: [5, 6] },
+    ];
+
+    const targetProfile = {
+      skills,
+    };
+
+    const assessment = {
+      id: assessmentId,
+      userId,
+      isCompleted() {
+        return false;
+      },
+    };
+
+    const campaignParticipation = new CampaignParticipation({
+      id: campaignParticipationId,
+    });
+
+    it('should add the campaign participation results', () => {
+
+      campaignParticipation.addCampaignParticipationResult({ assessment, competences, targetProfile, knowledgeElements });
+
+      expect(campaignParticipation.campaignParticipationResult).to.be.an.instanceOf(CampaignParticipationResult);
+      expect(campaignParticipation.campaignParticipationResult).to.deep.equal({
+        id: campaignParticipationId,
+        isCompleted: false,
+        totalSkillsCount: 4,
+        testedSkillsCount: 2,
+        validatedSkillsCount: 1,
+        competenceResults: [{
+          id: 1,
+          name: 'Economie symbiotique',
+          index: '5.1',
+          totalSkillsCount: 1,
+          testedSkillsCount: 1,
+          validatedSkillsCount: 1,
+        }, {
+          id: 2,
+          name: 'Désobéissance civile',
+          index: '6.9',
+          totalSkillsCount: 3,
+          testedSkillsCount: 1,
+          validatedSkillsCount: 0,
+        }],
+      });
+    });
   });
 });
