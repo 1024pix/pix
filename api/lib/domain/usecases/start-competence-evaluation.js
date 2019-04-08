5b7f5201d5e3402bf843f61bdad455ccc768e06d
Stephane Bedeau <sbedeau@octo.com>
2019-04-08 11:27:11 +0200

    [pf-372] Add usecase to start a competence evaluation with tests

--- api/lib/domain/usecases/start-competence-evaluation.js
+++ api/lib/domain/usecases/start-competence-evaluation.js
@@ -1,4 +1,38 @@
-module.exports = function startCompetenceEvaluation({ userId, competenceId }) {
-  return { userId, competenceId };
+const Assessment = require('../models/Assessment');
+const CompetenceEvaluation = require('../models/CompetenceEvaluation');
+const { NotFoundError } = require('../../domain/errors');
+
+module.exports = function startCompetenceEvaluation({ competenceId, userId, competenceEvaluationRepository, assessmentRepository, competenceRepository }) {
+  return _checkCompetenceExists(competenceId, competenceRepository)
+    .then(() => _createAssessmentForCompetenceEvaluation(userId, assessmentRepository))
+    .then((assessment) => _saveCompetenceEvaluation(competenceId, assessment, userId, competenceEvaluationRepository));
 };
 
+function _checkCompetenceExists(competenceId, competenceRepository) {
+  return competenceRepository.get(competenceId)
+    .then((competence) => {
+      if (competence === null) {
+        throw new NotFoundError('La compétence demandée n\'existe pas');
+      }
+      return Promise.resolve();
+    });
+}
+
+function _createAssessmentForCompetenceEvaluation(userId, assessmentRepository) {
+  const assessment = new Assessment({
+    userId,
+    state: Assessment.states.STARTED,
+    type: Assessment.types.COMPETENCE_EVALUATION,
+    courseId: Assessment.courseIdMessage.COMPETENCE_EVALUATION
+  });
+  return assessmentRepository.save(assessment);
+}
+
+function _saveCompetenceEvaluation(competenceId, assessment, userId, competenceEvaluationRepository) {
+  const competenceEvaluation = new  CompetenceEvaluation({
+    userId,
+    assessmentId: assessment.id,
+    competenceId,
+  });
+  return competenceEvaluationRepository.save(competenceEvaluation);
+}
