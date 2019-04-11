eda7ac376d13bbb2fabc27443c8536c22c18dd2b
Stephane Bedeau <sbedeau@octo.com>
2019-04-11 10:49:19 +0200

    [pf-372] Update serializer to fit JSON API format expected in front-end

--- api/lib/infrastructure/serializers/jsonapi/competence-evaluation-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/competence-evaluation-serializer.js
@@ -2,9 +2,18 @@ const { Serializer } = require('jsonapi-serializer');
 
 module.exports = {
 
-  serialize(competenceEvaluation) {
+  serialize(competenceEvaluations) {
     return new Serializer('competence-evaluations', {
-      attributes: ['createdAt', 'updatedAt', 'userId', 'competenceId', 'assessmentId'],
-    }).serialize(competenceEvaluation);
+      transform(current) {
+        const competenceEvaluation = Object.assign({}, current);
+        competenceEvaluation.assessment = { id: current.assessmentId };
+        return competenceEvaluation;
+      },
+      attributes: ['createdAt', 'updatedAt', 'userId', 'competenceId', 'assessment'],
+      assessment: {
+        ref: 'id',
+        included: false,
+      }
+    }).serialize(competenceEvaluations);
   },
 };
