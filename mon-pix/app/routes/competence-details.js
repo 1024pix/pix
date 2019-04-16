e60d4bb7f60ace0eaaf80eb6c91f354c08f10d7e
David Hérault <david.herault@pix.fr>
2019-04-16 15:55:15 +0200

    [pf-553] edit scorecard.id to be equal to comptence.id

--- mon-pix/app/routes/competence-details.js
+++ mon-pix/app/routes/competence-details.js
@@ -5,7 +5,7 @@ import Route from '@ember/routing/route';
 export default Route.extend(AuthenticatedRouteMixin, {
 
   session: service(),
-  
+
   async model(params) {
     const user = await this.store.findRecord('user', this.get('session.data.authenticated.userId'));
 
@@ -14,13 +14,10 @@ export default Route.extend(AuthenticatedRouteMixin, {
     }
 
     await user.competences;
-    
-    const competenceId = params.scorecard_id.split('_')[1];
-    const competence = user.competences.find((competence) => competence.id === competenceId);
-    
+
     return {
-      competence,
-      scorecard: this.store.findRecord('scorecard', params.scorecard_id),
+      scorecard: this.store.findRecord('scorecard', params.competence_id),
+      competence: user.competences.find((competence) => competence.id === params.competence_id),
     };
   },
 
