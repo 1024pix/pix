3769ca5324676aa4d95e1e52b381869900e3065b
Jérémy Buget <jbuget@gmail.com>
2019-04-24 13:34:21 +0200

    [API] Add resource GET /api/gc/heap-profile restricted to Pix Masters.

--- api/lib/application/gc/index.js
+++ api/lib/application/gc/index.js
@@ -20,6 +20,22 @@ exports.register = async function(server) {
         tags: ['api', 'gc']
       }
     },
+    {
+      method: 'GET',
+      path: '/api/gc/heap-profile',
+      config: {
+        pre: [{
+          method: securityController.checkUserHasRolePixMaster,
+          assign: 'hasRolePixMaster'
+        }],
+        handler: gcController.generateAndDownloadHeapProfile,
+        notes: [
+          '- **Route nécessitant une authentification en tant que Pix Master**\n' +
+          '- Génère et retourne un fichier heap profile'
+        ],
+        tags: ['api', 'gc']
+      }
+    },
   ]);
 };
 
