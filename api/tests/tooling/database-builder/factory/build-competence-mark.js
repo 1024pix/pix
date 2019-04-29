e6e2749832e69704cf1e23976d3b37d3abd177c8
lisequesnel <quesnel.lise@gmail.com>
2019-04-29 14:26:39 +0200

    convert certifications data raw seeds to builder

---
+++ api/tests/tooling/database-builder/factory/build-competence-mark.js
@@ -0,0 +1,24 @@
+const faker = require('faker');
+const buildAssessmentResult = require('./build-assessment-result');
+const databaseBuffer = require('../database-buffer');
+
+module.exports = function buildCompetenceMark({
+  id = faker.random.number(),
+  level = faker.random.number(),
+  score = faker.random.number(),
+  area_code = faker.random.number(),
+  competence_code = `${faker.random.number()}_${faker.random.number()}`,
+  assessmentResultId = buildAssessmentResult().id,
+} = {}) {
+
+  const values = {
+    id, level, score, area_code, competence_code, assessmentResultId
+  };
+
+  databaseBuffer.pushInsertable({
+    tableName: 'competence-marks',
+    values,
+  });
+
+  return values;
+};
