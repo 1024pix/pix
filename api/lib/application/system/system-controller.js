f38b922b6ceb1c849a97017283519e122ad819e3
Jérémy Buget <jbuget@gmail.com>
2019-05-06 16:46:17 +0200

    [API] Add a suffix {hostname} in /system/heap-xxx
    The routes heap-dump and heap-profile now require a hostname (or
    container name for Scalingo apps)

--- api/lib/application/system/system-controller.js
+++ api/lib/application/system/system-controller.js
@@ -1,3 +1,4 @@
+const os = require('os');
 const util = require('util');
 const heapdump = require('heapdump');
 const heapProfile = require('heap-profile');
@@ -5,12 +6,22 @@ const heapProfile = require('heap-profile');
 module.exports = {
 
   async generateAndDownloadHeapDump(request, h) {
+
+    if (!os.hostname().endsWith(request.params.hostname)) {
+      return h.redirect(request.url.path);
+    }
+
     const writeHeapDump = util.promisify(heapdump.writeSnapshot);
     const filename = await writeHeapDump();
     return h.file(filename, { mode: 'attachment' });
   },
 
   async generateAndDownloadHeapProfile(request, h) {
+
+    if (!os.hostname().endsWith(request.params.hostname)) {
+      return h.redirect(request.url.path);
+    }
+
     const writeHeapProfile = util.promisify(heapProfile.write);
     const filename = await writeHeapProfile();
     return h.file(filename, { mode: 'attachment' });
--- api/tests/integration/application/system/system-controller_test.js
+++ api/tests/integration/application/system/system-controller_test.js
@@ -1,3 +1,4 @@
+const os = require('os');
 const { expect, sinon, HttpTestServer } = require('../../../test-helper');
 const securityController = require('../../../../lib/interfaces/controllers/security-controller');
 const systemController = require('../../../../lib/application/system/system-controller');
@@ -28,7 +29,7 @@ describe('Integration | Application | System | system-controller', () => {
 
       it('should resolve a 200 HTTP response', async () => {
         // when
-        const response = await httpTestServer.request('GET', '/api/system/heap-dump');
+        const response = await httpTestServer.request('GET', `/api/system/heap-dump/${os.hostname()}`);
 
         // then
         expect(response.statusCode).to.equal(200);
@@ -47,7 +48,7 @@ describe('Integration | Application | System | system-controller', () => {
 
         it('should resolve a 403 HTTP response', () => {
           // when
-          const promise = httpTestServer.request('GET', '/api/system/heap-dump');
+          const promise = httpTestServer.request('GET', `/api/system/heap-dump/${os.hostname()}`);
 
           // then
           return promise.then((response) => {
@@ -68,7 +69,7 @@ describe('Integration | Application | System | system-controller', () => {
 
       it('should resolve a 200 HTTP response', async () => {
         // when
-        const response = await httpTestServer.request('GET', '/api/system/heap-profile');
+        const response = await httpTestServer.request('GET', `/api/system/heap-profile/${os.hostname()}`);
 
         // then
         expect(response.statusCode).to.equal(200);
@@ -87,7 +88,7 @@ describe('Integration | Application | System | system-controller', () => {
 
         it('should resolve a 403 HTTP response', () => {
           // when
-          const promise = httpTestServer.request('GET', '/api/system/heap-profile');
+          const promise = httpTestServer.request('GET', `/api/system/heap-profile/${os.hostname()}`);
 
           // then
           return promise.then((response) => {
