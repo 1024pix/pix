131f7a4fd4f12a589b6a0072356ac203c6bd36bf
Forster <groux.forster@gmail.com>
2019-05-09 10:37:38 +0200

    [pf-569] explicit component attribute

--- mon-pix/app/components/competence-card.js
+++ mon-pix/app/components/competence-card.js
@@ -2,6 +2,8 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 
 export default Component.extend({
+  scorecard: null,
+
   displayedLevel: computed('scorecard.{level,isNotStarted}', function() {
     if (this.scorecard.isNotStarted) {
       return null;
