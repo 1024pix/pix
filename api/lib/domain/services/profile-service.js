c11a61f918c1806f2ba509a2f1c52b32eb236ae5
LJA <ljacquemin@octo.com>
2019-05-10 15:16:18 +0200

    [pf-579] Remove unnecessary variables from profile-service
    The file was loaded with temporary variables to hurt
    readability.
    
    Also, added a few spaces before if statements to enhance
    readability too.

--- api/lib/domain/services/profile-service.js
+++ api/lib/domain/services/profile-service.js
@@ -7,32 +7,38 @@ const organizationRepository = require('../../infrastructure/repositories/organi
 
 const Profile = require('../models/Profile');
 
-function getByUserId(user_id) {
-  const user = userRepository.findUserById(user_id);
-  const competences = competenceRepository.list();
-  const areas = areaRepository.list();
+async function getByUserId(user_id) {
+  return Promise.all([
+    userRepository.findUserById(user_id),
+    competenceRepository.list(),
+    areaRepository.list(),
+    assessmentRepository.findLastAssessmentsForEachCoursesByUser(user_id),
+    assessmentRepository.findCompletedAssessmentsByUserId(user_id),
+    courseRepository.getAdaptiveCourses(),
+    organizationRepository.getByUserId(user_id),
+    assessmentRepository.hasCampaignOrCompetenceEvaluation(user_id)
+  ])
+    .then(([
+      user,
+      competences,
+      areas,
+      lastAssessments,
+      assessmentsCompletedWithResults,
+      courses,
+      organizations,
+      usesProfileV2
 
-  const adaptiveCourses = courseRepository.getAdaptiveCourses();
-  const lastAssessments = assessmentRepository.findLastAssessmentsForEachCoursesByUser(user_id);
-  const assessmentsCompletedWithResults = assessmentRepository.findCompletedAssessmentsByUserId(user_id);
-  const organizations = organizationRepository.getByUserId(user_id);
-  const usesProfileV2 = assessmentRepository.hasCampaignOrCompetenceEvaluation(user_id);
-
-  return Promise.all([user, competences, areas, lastAssessments, assessmentsCompletedWithResults, adaptiveCourses, organizations, usesProfileV2])
-    .then(([user, competences, areas, lastAssessments, assessmentsCompletedWithResults, adaptiveCourses, organizations, usesProfileV2]) => {
-
-      return new Profile({
-        user,
-        competences,
-        areas,
-        lastAssessments,
-        assessmentsCompletedWithResults,
-        courses: adaptiveCourses,
-        organizations,
-        usesProfileV2
-      });
-    });
-};
+    ]) => new Profile({
+      user,
+      competences,
+      areas,
+      lastAssessments,
+      assessmentsCompletedWithResults,
+      courses,
+      organizations,
+      usesProfileV2
+    }));
+}
 
 module.exports = {
   getByUserId
