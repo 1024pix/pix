a190389df43c8c6d47dc1cbf2c6a393eba6901ef
LJA <ljacquemin@octo.com>
2019-05-10 16:22:13 +0200

    [pf-579] Reorder methods in catAlgorithm.js
    The methods were in a chaotic order, one was not prefixed with _
    even though private. Something had to be done.

--- api/lib/domain/services/smart-random/catAlgorithm.js
+++ api/lib/domain/services/smart-random/catAlgorithm.js
@@ -6,44 +6,9 @@ const { pipe } = require('lodash/fp');
 // https://en.wikipedia.org/wiki/Item_response_theory
 
 module.exports = {
-  findMaxRewardingChallenges: findMaxRewardingChallenges,
-  getPredictedLevel: getPredictedLevel
+  findMaxRewardingChallenges,
+  getPredictedLevel
 };
-
-function findMaxRewardingChallenges(...args) {
-  return pipe(
-    _getMaxRewardingChallenges,
-    _clearChallengesIfNotRewarding
-  )(...args);
-}
-
-function _getMaxRewardingChallenges({ availableChallenges, predictedLevel, courseTubes, knowledgeElements }) {
-  return _.reduce(availableChallenges, (acc, challenge) => {
-    const challengeReward = computeReward({ challenge, predictedLevel, courseTubes, knowledgeElements });
-    if (challengeReward > acc.maxReward) {
-      acc.maxReward = challengeReward;
-      acc.maxRewardingChallenges = [challenge];
-    }
-    else if (challengeReward === acc.maxReward) {
-      acc.maxRewardingChallenges.push(challenge);
-    }
-    return acc;
-  }, { maxRewardingChallenges: [], maxReward: -Infinity })
-    .maxRewardingChallenges;
-}
-
-// Challenges that won't bring anymore information on the user is a termination condition of the CAT algorithm
-function _clearChallengesIfNotRewarding(challenges) {
-  return _.filter(challenges, (challenge) => challenge.reward !== 0);
-}
-
-function computeReward({ challenge, predictedLevel, courseTubes, knowledgeElements }) {
-  const proba = _probaOfCorrectAnswer(predictedLevel, challenge.hardestSkill.difficulty);
-  const nbExtraSkillsIfSolved = _getNewSkillsInfoIfChallengeSolved(challenge, courseTubes, knowledgeElements).length;
-  const nbFailedSkillsIfUnsolved = _getNewSkillsInfoIfChallengeUnsolved(challenge, courseTubes, knowledgeElements).length;
-
-  return proba * nbExtraSkillsIfSolved + (1 - proba) * nbFailedSkillsIfUnsolved;
-}
 
 function getPredictedLevel(knowledgeElements, skills) {
   let maxLikelihood = -Infinity;
@@ -62,8 +27,8 @@ function getPredictedLevel(knowledgeElements, skills) {
 }
 
 function _computeProbabilityOfCorrectLevelPredicted(level, knowledgeElements, skills) {
-  const directKnowledgeElements = _.filter(knowledgeElements, (ke)=> ke.source === 'direct');
-  const extraAnswers = directKnowledgeElements.map((ke)=> {
+  const directKnowledgeElements = _.filter(knowledgeElements, (ke) => ke.source === 'direct');
+  const extraAnswers = directKnowledgeElements.map((ke) => {
     const skill = skills.find((skill) => skill.id === ke.skillId);
     const maxDifficulty = skill.difficulty || 2;
     const binaryOutcome = (ke.status === 'validated') ? 1 : 0;
@@ -78,6 +43,47 @@ function _computeProbabilityOfCorrectLevelPredicted(level, knowledgeElements, sk
     answer.binaryOutcome - _probaOfCorrectAnswer(level, answer.maxDifficulty));
 
   return -Math.abs(diffBetweenResultAndProbaToResolve.reduce((a, b) => a + b));
+}
+
+function findMaxRewardingChallenges(...args) {
+  return pipe(
+    _getMaxRewardingChallenges,
+    _clearChallengesIfNotRewarding
+  )(...args);
+}
+
+function _getMaxRewardingChallenges({ availableChallenges, predictedLevel, courseTubes, knowledgeElements }) {
+  return _.reduce(availableChallenges, (acc, challenge) => {
+    const challengeReward = _computeReward({ challenge, predictedLevel, courseTubes, knowledgeElements });
+    if (challengeReward > acc.maxReward) {
+      acc.maxReward = challengeReward;
+      acc.maxRewardingChallenges = [challenge];
+    }
+    else if (challengeReward === acc.maxReward) {
+      acc.maxRewardingChallenges.push(challenge);
+    }
+    return acc;
+  }, { maxRewardingChallenges: [], maxReward: -Infinity })
+    .maxRewardingChallenges;
+}
+
+// Challenges that won't bring anymore information on the user is a termination condition of the CAT algorithm
+function _clearChallengesIfNotRewarding(challenges) {
+  return _.filter(challenges, (challenge) => challenge.reward !== 0);
+}
+
+function _computeReward({ challenge, predictedLevel, courseTubes, knowledgeElements }) {
+  const proba = _probaOfCorrectAnswer(predictedLevel, challenge.hardestSkill.difficulty);
+  const nbExtraSkillsIfSolved = _getNewSkillsInfoIfChallengeSolved(challenge, courseTubes, knowledgeElements).length;
+  const nbFailedSkillsIfUnsolved = _getNewSkillsInfoIfChallengeUnsolved(challenge, courseTubes, knowledgeElements).length;
+
+  return proba * nbExtraSkillsIfSolved + (1 - proba) * nbFailedSkillsIfUnsolved;
+}
+
+// The probability P(gap) of giving the correct answer is given by the "logistic function"
+// https://en.wikipedia.org/wiki/Logistic_function
+function _probaOfCorrectAnswer(userEstimatedLevel, challengeDifficulty) {
+  return 1 / (1 + Math.exp(-(userEstimatedLevel - challengeDifficulty)));
 }
 
 function _getNewSkillsInfoIfChallengeSolved(challenge, courseTubes, knowledgeElements) {
@@ -108,10 +114,4 @@ function _findTubeByName(courseTubes, tubeName) {
 function _skillNotTestedYet(skill, knowledgesElements) {
   const skillsAlreadyTested = _.map(knowledgesElements, 'skillId');
   return !skillsAlreadyTested.includes(skill.id);
-}
-
-// The probability P(gap) of giving the correct answer is given by the "logistic function"
-// https://en.wikipedia.org/wiki/Logistic_function
-function _probaOfCorrectAnswer(userEstimatedLevel, challengeDifficulty) {
-  return 1 / (1 + Math.exp(-(userEstimatedLevel - challengeDifficulty)));
 }
