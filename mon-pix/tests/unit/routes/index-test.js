1caebff4097c08ea1b81f4d5e8df4c2db785e9ac
LJA <ljacquemin@octo.com>
2019-05-13 11:55:50 +0200

    mon-pix: use currentUser in index
    Also, we no longer check for "board" users in index, and rely instead on
    /compte and /profilv2 to do that redirection if necessary.
    
    Likewise, we count on /compte to redirect to /login if necessary.

--- mon-pix/tests/unit/routes/index-test.js
+++ mon-pix/tests/unit/routes/index-test.js
@@ -1,120 +1,59 @@
-import EmberObject from '@ember/object';
-import Service from '@ember/service';
-import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupTest } from 'ember-mocha';
 import sinon from 'sinon';
+import Service from '@ember/service';
 
 describe('Unit | Route | index', function() {
 
   setupTest('route:index', {
-    needs: ['service:session', 'service:metrics']
+    needs: ['service:metrics', 'service:currentUser', 'service:session']
   });
 
-  it('exists', function() {
-    const route = this.subject();
-    expect(route).to.be.ok;
-  });
-
-  describe('when the user is not logged id', () => {
-
-    it('should redirect the user to login', function() {
-      this.register('service:session', Service.extend({ isAuthenticated: false }));
-      this.inject.service('session', { as: 'session' });
-
-      const route = this.subject();
-      route.transitionTo = sinon.stub();
-
-      // When
-      route.beforeModel();
-
-      // Then
-      sinon.assert.calledWith(route.transitionTo, 'login');
-    });
-  });
-
-  describe('when the user is authenticated', () => {
-
-    let storeServiceStub;
+  describe('model', function() {
 
     beforeEach(function() {
-
-      storeServiceStub = {
-        findRecord: sinon.stub().resolves(EmberObject.create({ organizations: [] }))
-      };
-      this.register('service:store', Service.extend(storeServiceStub));
-      this.inject.service('store', { as: 'store' });
-
       this.register('service:session', Service.extend({
         isAuthenticated: true,
-        data: {
-          authenticated: {
-            userId: 1435
-          }
-        }
       }));
-      this.inject.service('session', { as: 'session' });
+      this.inject.service('session');
     });
 
-    it('should redirect the user somewhere else', function() {
-      // Given
-      const route = this.subject();
-      route.transitionTo = sinon.stub();
+    context('when user uses ProfileV2', function() {
 
-      // When
-      const promise = route.beforeModel();
+      beforeEach(function() {
+        this.register('service:currentUser', Service.extend({
+          user: { usesProfileV2: true }
+        }));
+        this.inject.service('currentUser');
+      });
 
-      // Then
-      return promise.then(() => {
-        sinon.assert.calledOnce(route.transitionTo);
+      it('should redirect to /profilv2', async function() {
+        // Given
+        const route = this.subject();
+        route.transitionTo = sinon.spy();
+
+        // When
+        await route.model();
+
+        // Then
+        sinon.assert.calledWith(route.transitionTo, 'profilv2');
       });
     });
 
-    it('should redirect the user to /compte by default', function() {
-      // Given
-      const route = this.subject();
-      route.transitionTo = sinon.stub();
+    context('when user does not use ProfileV2', function() {
 
-      // When
-      const promise = route.beforeModel();
+      it('should redirect to /compte', async function() {
+        // Given
+        const route = this.subject();
+        route.transitionTo = sinon.spy();
 
-      // Then
-      return promise.then(() => {
+        // When
+        await route.model();
+
+        // Then
         sinon.assert.calledWith(route.transitionTo, 'compte');
       });
     });
-
-    it('should load user details from the store', function() {
-      const route = this.subject();
-      route.transitionTo = sinon.stub();
-
-      // When
-      const promise = route.beforeModel();
-
-      // Then
-      return promise.then(() => {
-        sinon.assert.calledOnce(storeServiceStub.findRecord);
-        sinon.assert.calledWith(storeServiceStub.findRecord, 'user', 1435);
-      });
-    });
-
-    it('should redirect to board when the user is linked to an organization', function() {
-      // Given
-      storeServiceStub.findRecord.resolves(EmberObject.create({
-        organizations: [EmberObject.create()]
-      }));
-
-      const route = this.subject();
-      route.transitionTo = sinon.stub();
-
-      // When
-      const promise = route.beforeModel();
-
-      // Then
-      return promise.then(() => {
-        sinon.assert.calledWith(route.transitionTo, 'board');
-      });
-    });
   });
-});
 
+});
