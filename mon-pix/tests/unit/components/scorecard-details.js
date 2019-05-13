06d7aa56dd370b367e035731fcb360383d6049d5
David Hérault <dherault@gmail.com>
2019-05-13 15:33:44 +0200

    Add controller and tests for scorecard-details
    Make level-info nowrap
    Add scorecard-details__button on scorecard-details
    Display "n pix avant le niveau x" unless scorecard.isNotStarted

---
+++ mon-pix/tests/unit/components/scorecard-details.js
@@ -0,0 +1,66 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { setupTest } from 'ember-mocha';
+
+describe('Unit | Component | scorecard-details ', function() {
+  setupTest();
+
+  describe('#level', function() {
+    it('returns null if the scorecard isNotStarted', function() {
+      // given
+      const controller = this.owner.lookup('controller:scorecard-details');
+
+      // when
+      controller.set('scorecard', { isNotStarted: true });
+
+      // then
+      expect(controller.get('level')).to.be.equal(null);
+    });
+
+    it('returns the level if the scorecard level is over zero', function() {
+      // given
+      const controller = this.owner.lookup('controller:scorecard-details');
+
+      // when
+      controller.set('scorecard', { level: 1 });
+
+      // then
+      expect(controller.get('level')).to.be.equal(1);
+    });
+
+    it('returns a dash if the scorecard level is over zero', function() {
+      // given
+      const controller = this.owner.lookup('controller:scorecard-details');
+
+      // when
+      controller.set('scorecard', { level: 0 });
+
+      // then
+      expect(controller.get('level')).to.be.equal('–');
+    });
+  });
+
+  describe('#earnedPix', function() {
+    it('returns the earnedPix if the scorecard earnedPix is over zero', function() {
+      // given
+      const controller = this.owner.lookup('controller:scorecard-details');
+
+      // when
+      controller.set('scorecard', { earnedPix: 1 });
+
+      // then
+      expect(controller.get('earnedPix')).to.be.equal(1);
+    });
+
+    it('returns a dash if the scorecard earnedPix is over zero', function() {
+      // given
+      const controller = this.owner.lookup('controller:scorecard-details');
+
+      // when
+      controller.set('scorecard', { earnedPix: 0 });
+
+      // then
+      expect(controller.get('earnedPix')).to.be.equal('–');
+    });
+  });
+});
