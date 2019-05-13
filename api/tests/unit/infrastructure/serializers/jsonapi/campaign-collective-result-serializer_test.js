af6c24726d79dd4b70140a7388530d6461ab9772
Jérémy Buget <jbuget@gmail.com>
2019-05-14 01:07:01 +0200

    [po-199] Add CampaignCollectiveResult JSON:API serializer.
    We force the inclusion of CampaignCompetenceCollectiveResult objects because otherwise the first one would be useless.

---
+++ api/tests/unit/infrastructure/serializers/jsonapi/campaign-collective-result-serializer_test.js
@@ -0,0 +1,81 @@
+const { expect, domainBuilder } = require('../../../../test-helper');
+const serializer = require('../../../../../lib/infrastructure/serializers/jsonapi/campaign-collective-result-serializer');
+
+describe('Unit | Serializer | JSONAPI | campaign-collective-results-serializer', () => {
+
+  describe('#serialize', () => {
+
+    it('should return a serialized JSON data object', () => {
+      // given
+      const campaignId = 123;
+
+      const campaignCollectiveResult = domainBuilder.buildCampaignCollectiveResult({
+        id: campaignId,
+        campaignCompetenceCollectiveResults: [
+          domainBuilder.buildCampaignCompetenceCollectiveResult({
+            averageValidatedSkills: 2,
+            campaignId: campaignId,
+            competenceId: 'rec1',
+            competenceIndex: '1.2',
+            competenceName: 'Cuisson des legumes d’automne',
+            totalSkillsCount: 3,
+          }),
+          domainBuilder.buildCampaignCompetenceCollectiveResult({
+            averageValidatedSkills: 1,
+            campaignId: campaignId,
+            competenceId: 'rec2',
+            competenceIndex: '3.4',
+            competenceName: 'Tourner un champignon',
+            totalSkillsCount: 4,
+          }),
+        ]
+      });
+
+      const expectedSerializedResult = {
+        data: {
+          id: campaignId.toString(),
+          type: 'campaign-collective-results',
+          attributes: {},
+          relationships: {
+            'campaign-competence-collective-results': {
+              data: [{
+                id: `${campaignId.toString()}_rec1`,
+                type: 'campaignCompetenceCollectiveResults',
+              }, {
+                id: `${campaignId.toString()}_rec2`,
+                type: 'campaignCompetenceCollectiveResults',
+              }]
+            },
+          },
+        },
+        included: [{
+          id: '123_rec1',
+          type: 'campaignCompetenceCollectiveResults',
+          attributes: {
+            'average-validated-skills': 2,
+            'competence-id': 'rec1',
+            'competence-name': 'Cuisson des legumes d’automne',
+            'domain-code': '1',
+            'total-skills-count': 3,
+          }
+        }, {
+          id: '123_rec2',
+          type: 'campaignCompetenceCollectiveResults',
+          attributes: {
+            'average-validated-skills': 1,
+            'competence-id': 'rec2',
+            'competence-name': 'Tourner un champignon',
+            'domain-code': '3',
+            'total-skills-count': 4,
+          }
+        }]
+      };
+
+      // when
+      const result = serializer.serialize(campaignCollectiveResult);
+
+      // then
+      expect(result).to.deep.equal(expectedSerializedResult);
+    });
+  });
+});
