1d18abb90cafe31b763c3027ff74e376cce2ecd9
Jérémy Buget <jbuget@gmail.com>
2019-05-14 01:10:55 +0200

    [po-199] Link entity CampaignCollectiveResult to Campaign.
    There is also a link in JSON:API campaign serializer (that reduces the code in Ember).

--- api/lib/infrastructure/serializers/jsonapi/campaign-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/campaign-serializer.js
@@ -8,7 +8,7 @@ module.exports = {
   serialize(campaigns, { tokenForCampaignResults, ignoreCampaignReportRelationshipData = true } = {}) {
 
     return new Serializer('campaign', {
-      attributes: ['name', 'code', 'title', 'createdAt', 'customLandingPageText', 'tokenForCampaignResults', 'idPixLabel', 'organizationLogoUrl', 'targetProfile', 'campaignReport'],
+      attributes: ['name', 'code', 'title', 'createdAt', 'customLandingPageText', 'tokenForCampaignResults', 'idPixLabel', 'organizationLogoUrl', 'targetProfile', 'campaignReport', 'campaignCollectiveResult'],
       transform: (record) => {
         const campaign = Object.assign({}, record);
         campaign.tokenForCampaignResults = tokenForCampaignResults;
@@ -26,6 +26,15 @@ module.exports = {
         relationshipLinks: {
           related(record, current, parent) {
             return `/api/campaigns/${parent.id}/campaign-report`;
+          }
+        }
+      },
+      campaignCollectiveResult: {
+        ref: 'id',
+        ignoreRelationshipData: true,
+        relationshipLinks: {
+          related(record, current, parent) {
+            return `/api/campaigns/${parent.id}/collective-results`;
           }
         }
       },
