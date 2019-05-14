debdd67a9e256e7b0da751ee81b19dbeab72fee4
David Hérault <dherault@gmail.com>
2019-05-14 16:23:40 +0200

    Fix bug on competence-result page
    On competences result page, the scorecard is not the model (a POJO)
    but an Ember Proxy object, requiring a getter to access.

--- mon-pix/app/components/scorecard-details.js
+++ mon-pix/app/components/scorecard-details.js
@@ -10,11 +10,11 @@ export default Component.extend({
   scorecard: null,
 
   level: computed('scorecard.{level,isNotStarted}', function() {
-    return this.scorecard.isNotStarted ? null : replaceZeroByDash(this.scorecard.level);
+    return this.get('scorecard.isNotStarted') ? null : replaceZeroByDash(this.get('scorecard.level'));
   }),
 
   earnedPix: computed('scorecard.earnedPix', function() {
-    return replaceZeroByDash(this.scorecard.earnedPix);
+    return replaceZeroByDash(this.get('scorecard.earnedPix'));
   }),
 
 });
