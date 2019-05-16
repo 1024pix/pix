0759767d591bd3cba64bec89f3010853684a35cb
LJA <ljacquemin@octo.com>
2019-05-16 17:14:28 +0200

    [pf-579] Refactor data-fetcher to use spread
    The spread operator has interesting use cases when some code
    moves a lot of dependencies around. It enhances readability
    by reducing the amount of noise. However, the benefit does not
    always outweight the cost which is loosing sight of which objects
    are being passed around.
    
    By documenting the signature with destructured
    parameters and using the spread operator on the argument keywords, we
    arguably get the best of both worlds.

--- api/lib/domain/usecases/get-next-challenge-for-competence-evaluation.js
+++ api/lib/domain/usecases/get-next-challenge-for-competence-evaluation.js
@@ -1,28 +1,41 @@
+/* eslint-disable no-unused-vars */
 const { AssessmentEndedError, UserNotAuthorizedToAccessEntity } = require('../errors');
 const smartRandom = require('../services/smart-random/smart-random');
 const dataFetcher = require('../services/smart-random/data-fetcher');
 
-async function getNextChallengeForCompetenceEvaluation({ assessment, userId, answerRepository, competenceEvaluationRepository, challengeRepository, knowledgeElementRepository, skillRepository }) {
+async function getNextChallengeForCompetenceEvaluation({
+  knowledgeElementRepository,
+  competenceEvaluationRepository,
+  challengeRepository,
+  answerRepository,
+  skillRepository,
+  assessment,
+  userId,
+}) {
+
   _checkIfAssessmentBelongsToUser(assessment, userId);
-  const competenceEvaluation = await competenceEvaluationRepository.getByAssessmentId(assessment.id);
-  const [answers, targetSkills, challenges, knowledgeElements] = await dataFetcher.fetchForCompetenceEvaluations({
-    assessment,
-    competenceEvaluation,
-    answerRepository,
-    challengeRepository,
-    knowledgeElementRepository,
-    skillRepository
-  });
+
+  const [
+    answers,
+    targetSkills,
+    challenges,
+    knowledgeElements
+  ] = await dataFetcher.fetchForCompetenceEvaluations(...arguments);
 
   const {
     nextChallenge,
     assessmentEnded,
-  } = smartRandom.getNextChallenge({ answers, challenges, targetSkills, knowledgeElements });
+
+  } = smartRandom.getNextChallenge({
+    answers,
+    challenges,
+    targetSkills,
+    knowledgeElements
+  });
 
   if (assessmentEnded) {
     throw new AssessmentEndedError();
   }
-
   return nextChallenge;
 }
 
--- api/lib/domain/usecases/get-next-challenge-for-smart-placement.js
+++ api/lib/domain/usecases/get-next-challenge-for-smart-placement.js
@@ -1,25 +1,37 @@
+/* eslint-disable no-unused-vars */
 const { AssessmentEndedError } = require('../errors');
 const smartRandom = require('../services/smart-random/smart-random');
 const dataFetcher = require('../services/smart-random/data-fetcher');
 
-async function getNextChallengeForSmartPlacement({ assessment, answerRepository, challengeRepository, knowledgeElementRepository, targetProfileRepository }) {
-  const [answers, [targetProfile, challenges], knowledgeElements] = await dataFetcher.fetchForCampaigns({
-    assessment,
-    answerRepository,
-    challengeRepository,
-    knowledgeElementRepository,
-    targetProfileRepository,
-  });
+async function getNextChallengeForSmartPlacement(
+  knowledgeElementRepository,
+  targetProfileRepository,
+  challengeRepository,
+  answerRepository,
+  assessment,
+) {
+
+  const [
+    answers,
+    targetProfile,
+    challenges,
+    knowledgeElements,
+  ] = await dataFetcher.fetchForCampaigns(...arguments);
 
   const {
     nextChallenge,
     assessmentEnded,
-  } = smartRandom.getNextChallenge({ answers, challenges, targetSkills: targetProfile.skills, knowledgeElements });
+
+  } = smartRandom.getNextChallenge({
+    answers,
+    challenges,
+    targetSkills: targetProfile.skills,
+    knowledgeElements,
+  });
 
   if (assessmentEnded) {
     throw new AssessmentEndedError();
   }
-
   return nextChallenge;
 }
 
