2675d1cba177f4985248fbf417aba2a49063b95a
LJA <ljacquemin@octo.com>
2019-05-16 16:03:45 +0200

    [pf-579] Rename challengesFilter.js to dash-case

--- api/tests/unit/domain/services/smart-random/challengesFilter_test.js
+++ api/tests/unit/domain/services/smart-random/challengesFilter_test.js
@@ -1,6 +1,6 @@
 const { expect, domainBuilder } = require('../../../../test-helper');
 const TargetProfile = require('../../../../../lib/domain/models/TargetProfile');
-const filterChallenge = require('../../../../../lib/domain/services/smart-random/challengesFilter');
+const filterChallenge = require('../../../../../lib/domain/services/smart-random/challenges-filter');
 const Tube = require('../../../../../lib/domain/models/Tube');
 
 const KNOWLEDGE_ELEMENT_STATUS = {
