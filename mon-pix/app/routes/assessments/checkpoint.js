e568d9f60d6662b397c686b41c5e36d88644c011
Forster <groux.forster@gmail.com>
2019-05-16 11:04:41 +0200

    [pf-578] rewrite checkpoint route

--- mon-pix/app/routes/assessments/checkpoint.js
+++ mon-pix/app/routes/assessments/checkpoint.js
@@ -1,22 +1,17 @@
 import Route from '@ember/routing/route';
-import RSVP from 'rsvp';
 
 export default Route.extend({
 
-  afterModel(assessment) {
+  async afterModel(assessment) {
 
-    if (assessment.isCompetenceEvaluation) {
-      return assessment.belongsTo('progression').reload();
+    if (assessment.isCompetenceEvaluation || assessment.isSmartPlacement) {
+      await assessment.belongsTo('progression').reload();
     }
 
     if (assessment.isSmartPlacement) {
-      return RSVP.hash({
-        campaigns: this.store.query('campaign', { filter: { code: assessment.codeCampaign } }),
-        progression: assessment.belongsTo('progression').reload()
-      }).then((hash) => {
-        assessment.set('campaign', hash.campaigns.get('firstObject'));
-        return assessment;
-      });
+      const campaigns = await this.store.query('campaign', { filter: { code: assessment.codeCampaign } });
+
+      assessment.set('campaign', campaigns.get('firstObject'));
     }
   },
 
