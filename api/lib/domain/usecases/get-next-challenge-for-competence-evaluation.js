1e50d4cc28d1b5a6ec1bfc390719af3b725afb33
LJA <ljacquemin@octo.com>
2019-05-16 16:33:30 +0200

    [pf-579] Add a data-fetcher.js file to smart random
    In order to ease the usage of the next challenge in various
    use cases, it is necessary to extract generic data fetching
    code into its own service.

--- api/lib/domain/usecases/get-next-challenge-for-competence-evaluation.js
+++ api/lib/domain/usecases/get-next-challenge-for-competence-evaluation.js
@@ -1,10 +1,11 @@
 const { AssessmentEndedError, UserNotAuthorizedToAccessEntity } = require('../errors');
 const smartRandom = require('../services/smart-random/smart-random');
+const dataFetcher = require('../services/smart-random/data-fetcher');
 
 async function getNextChallengeForCompetenceEvaluation({ assessment, userId, answerRepository, competenceEvaluationRepository, challengeRepository, knowledgeElementRepository, skillRepository }) {
   _checkIfAssessmentBelongsToUser(assessment, userId);
   const competenceEvaluation = await competenceEvaluationRepository.getByAssessmentId(assessment.id);
-  const [answers, targetSkills, challenges, knowledgeElements] = await getSmartRandomInputValues({
+  const [answers, targetSkills, challenges, knowledgeElements] = await dataFetcher.fetchForCompetenceEvaluations({
     assessment,
     competenceEvaluation,
     answerRepository,
@@ -29,15 +30,6 @@ function _checkIfAssessmentBelongsToUser(assessment, userId) {
   if (assessment.userId != userId) {
     throw new UserNotAuthorizedToAccessEntity();
   }
-}
-
-function getSmartRandomInputValues({ assessment, competenceEvaluation, answerRepository, challengeRepository, knowledgeElementRepository, skillRepository }) {
-  return Promise.all([
-    answerRepository.findByAssessment(assessment.id),
-    skillRepository.findByCompetenceId(competenceEvaluation.competenceId),
-    challengeRepository.findByCompetenceId(competenceEvaluation.competenceId),
-    knowledgeElementRepository.findUniqByUserId({ userId: assessment.userId })]
-  );
 }
 
 module.exports = getNextChallengeForCompetenceEvaluation;
