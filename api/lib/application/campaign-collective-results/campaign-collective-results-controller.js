c313f99da303d2bb54c496ee844527fe192caadf
Christophe Charles <ccharles@octo.com>
2019-05-16 11:56:10 +0200

    [po-199] Use destructuring assignment and remove try catch

--- api/lib/application/campaign-collective-results/campaign-collective-results-controller.js
+++ api/lib/application/campaign-collective-results/campaign-collective-results-controller.js
@@ -1,23 +1,13 @@
 const usecases = require('../../domain/usecases');
-const logger = require('../../infrastructure/logger');
 const serializer = require('../../infrastructure/serializers/jsonapi/campaign-collective-result-serializer');
-const { UserNotAuthorizedToAccessEntity } = require('../../../lib/domain/errors');
 
 module.exports = {
 
-  async get(request, h) {
-    const userId = request.auth.credentials.userId;
+  async get(request) {
+    const { userId } = request.auth.credentials;
     const campaignId = request.params.id;
 
-    try {
-      const campaignCollectiveResult = await usecases.computeCampaignCollectiveResult({ userId, campaignId });
-      return serializer.serialize(campaignCollectiveResult);
-    } catch (err) {
-      logger.error(err);
-      if (err instanceof UserNotAuthorizedToAccessEntity) {
-        return h.response(err.toString()).code(403);
-      }
-      return h.response(err.toString()).code(400);
-    }
+    const campaignCollectiveResult = await usecases.computeCampaignCollectiveResult({ userId, campaignId });
+    return serializer.serialize(campaignCollectiveResult);
   }
 };
