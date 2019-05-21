2c05735ffee7ccd2e9eed81bc3cc82de08a02054
LJA <ljacquemin@octo.com>
2019-05-21 16:23:36 +0200

    [pf-579] Refactor Scorecards to use a different repo method
    This commit refactor scorecard usecases to use the new
    knowledge-elements repository method introduced in #5face34e5.

--- api/lib/domain/usecases/get-scorecard.js
+++ api/lib/domain/usecases/get-scorecard.js
@@ -1,6 +1,5 @@
 const { UserNotAuthorizedToAccessEntity } = require('../errors');
 const Scorecard = require('../models/Scorecard');
-const _ = require('lodash');
 
 module.exports = async ({ authenticatedUserId, scorecardId, knowledgeElementRepository, competenceRepository, competenceEvaluationRepository }) => {
 
@@ -9,13 +8,13 @@ module.exports = async ({ authenticatedUserId, scorecardId, knowledgeElementRepo
     throw new UserNotAuthorizedToAccessEntity();
   }
 
-  const [knowledgeElements, competence, competenceEvaluations] = await Promise.all([
-    knowledgeElementRepository.findUniqByUserId({ userId: authenticatedUserId }),
+  const [knowledgeElementsGroupedByCompetenceId, competence, competenceEvaluations] = await Promise.all([
+    knowledgeElementRepository.findUniqByUserIdGroupedByCompetenceId({ userId: authenticatedUserId }),
     competenceRepository.get(competenceId),
     competenceEvaluationRepository.findByUserId(authenticatedUserId),
   ]);
 
-  const knowledgeElementsForCompetence = _.groupBy(knowledgeElements, 'competenceId')[competence.id];
-  return Scorecard.buildFrom({ userId: authenticatedUserId, knowledgeElements: knowledgeElementsForCompetence, competence, competenceEvaluations });
+  const knowledgeElements = knowledgeElementsGroupedByCompetenceId[competence.id];
+  return Scorecard.buildFrom({ userId: authenticatedUserId, knowledgeElements, competence, competenceEvaluations });
 };
 
