89b7b77d9941decc4ab5218aaabc0547266c62c9
lisequesnel <quesnel.lise@gmail.com>
2019-05-24 18:28:18 +0200

    [pf-579] Handle when competenceEvaluation does not exists

--- api/lib/domain/usecases/reset-competence-evaluation.js
+++ api/lib/domain/usecases/reset-competence-evaluation.js
@@ -1,5 +1,5 @@
 const CompetenceEvaluation = require('../models/CompetenceEvaluation');
-const { UserNotAuthorizedToAccessEntity } = require('../errors');
+const { UserNotAuthorizedToAccessEntity, NotFoundError } = require('../errors');
 
 module.exports = async function resetCompetenceEvaluation({
   authenticatedUserId,
@@ -9,6 +9,17 @@ module.exports = async function resetCompetenceEvaluation({
 }) {
   if (authenticatedUserId !== requestedUserId) {
     throw new UserNotAuthorizedToAccessEntity();
+  }
+
+  try {
+    await competenceEvaluationRepository.getByCompetenceIdAndUserId(competenceId, authenticatedUserId);
+  } catch (err) {
+    // If user wants to reset its knowledge elements on a competence, but has only validated or invalidated them on campaigns
+    if (err instanceof NotFoundError) {
+      return null;
+    } else {
+      throw err;
+    }
   }
 
   return competenceEvaluationRepository.updateStatusByUserIdAndCompetenceId(authenticatedUserId, competenceId, CompetenceEvaluation.statuses.RESET);
