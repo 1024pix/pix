5720c01cddfd389e6c7581370290c60dbae22495
lisequesnel <quesnel.lise@gmail.com>
2019-05-24 18:28:18 +0200

    [pf-579] Handle when competenceEvaluation does not exists

--- api/tests/unit/domain/usecases/reset-competence-evaluation_test.js
+++ api/tests/unit/domain/usecases/reset-competence-evaluation_test.js
@@ -1,7 +1,7 @@
 const { expect, sinon, catchErr } = require('../../../test-helper');
 const CompetenceEvaluation = require('../../../../lib/domain/models/CompetenceEvaluation');
 const resetCompetenceEvaluation = require('../../../../lib/domain/usecases/reset-competence-evaluation');
-const { UserNotAuthorizedToAccessEntity } = require('../../../../lib/domain/errors');
+const { UserNotAuthorizedToAccessEntity, NotFoundError } = require('../../../../lib/domain/errors');
 
 describe('Unit | UseCase | reset-competence-evaluation', () => {
 
@@ -9,12 +9,13 @@ describe('Unit | UseCase | reset-competence-evaluation', () => {
   const competenceEvaluationId = 111;
   const competenceId = 123;
   const authenticatedUserId = 456;
-  let requestedUserId = 456;
+  let requestedUserId;
 
   const competenceEvaluationRepository = {};
 
   beforeEach(() => {
     competenceEvaluationRepository.updateStatusByUserIdAndCompetenceId = sinon.stub();
+    competenceEvaluationRepository.getByCompetenceIdAndUserId = sinon.stub();
 
     competenceEvaluation = {
       id: competenceEvaluationId,
@@ -27,11 +28,15 @@ describe('Unit | UseCase | reset-competence-evaluation', () => {
   context('when the user owns the competenceEvaluation', () => {
     it('should update the competenceEvaluation', async () => {
       // given
+      requestedUserId = 456;
       const expectedCompetenceEvaluation = {
         ...competenceEvaluation,
         status: CompetenceEvaluation.statuses.RESET,
       };
 
+      competenceEvaluationRepository.getByCompetenceIdAndUserId
+        .withArgs(competenceId, authenticatedUserId)
+        .resolves(competenceEvaluation);
       competenceEvaluationRepository.updateStatusByUserIdAndCompetenceId
         .withArgs(authenticatedUserId, competenceId, CompetenceEvaluation.statuses.RESET)
         .resolves(expectedCompetenceEvaluation);
@@ -54,6 +59,24 @@ describe('Unit | UseCase | reset-competence-evaluation', () => {
 
       // then
       expect(requestErr).to.be.instanceOf(UserNotAuthorizedToAccessEntity);
+    });
+  });
+
+  context('when there is no competenceEvaluation', () => {
+    it('should do nothing', async () => {
+      // given
+      requestedUserId = 456;
+
+      competenceEvaluationRepository.getByCompetenceIdAndUserId
+        .withArgs(competenceId, authenticatedUserId)
+        .rejects(new NotFoundError());
+
+      // when
+      const response = await resetCompetenceEvaluation({ authenticatedUserId, requestedUserId, competenceId, competenceEvaluationRepository });
+
+      // then
+      sinon.assert.neverCalledWith(competenceEvaluationRepository.updateStatusByUserIdAndCompetenceId, competenceId, authenticatedUserId);
+      expect(response).to.equal(null);
     });
   });
 });
