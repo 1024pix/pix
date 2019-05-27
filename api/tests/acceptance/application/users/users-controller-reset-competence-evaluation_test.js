5e4e2b3b7066caa58f785573b7e1cd5a10e91f2a
LJA <ljacquemin@octo.com>
2019-05-27 17:46:26 +0200

    [pf-579] Add an acceptance test for comp. eval reset
    This commit completes the acceptance test to ensure that all cases are
    covered :
    - the reset of only the requested competence eval matching competenceId
    - the reset of only knowledge elements matching competenceId
    - the handling of the case where knowledge elements coming from
    campaigns do not match any competence evaluation

--- api/tests/acceptance/application/users/users-controller-reset-competence-evaluation_test.js
+++ api/tests/acceptance/application/users/users-controller-reset-competence-evaluation_test.js
@@ -1,4 +1,5 @@
-const { databaseBuilder, expect, generateValidRequestAuhorizationHeader } = require('../../../test-helper');
+const { knex, databaseBuilder, expect, generateValidRequestAuhorizationHeader } = require('../../../test-helper');
+const _ = require('lodash');
 
 const createServer = require('../../../../server');
 
@@ -6,8 +7,21 @@ describe('Acceptance | Controller | users-controller-reset-competence-evaluation
 
   let options;
   let server;
-  const userId = 5678;
-  const competenceId = 1234;
+
+  const userId = 1234;
+  const competenceId = 'recAbe382T0e1337';
+
+  function inspectCompetenceEvaluationInDb({ userId, competenceId }) {
+    return knex.select('*')
+      .from('competence-evaluations')
+      .where({ userId, competenceId });
+  }
+
+  function inspectKnowledgeElementsInDb({ userId, competenceId }) {
+    return knex.select('*')
+      .from('knowledge-elements')
+      .where({ userId, competenceId });
+  }
 
   beforeEach(async () => {
 
@@ -20,7 +34,7 @@ describe('Acceptance | Controller | users-controller-reset-competence-evaluation
     server = await createServer();
   });
 
-  describe('GET /users/:id/competences/:id/reset', () => {
+  describe('PATCH /users/{id}/competences/{id}/reset', () => {
 
     describe('Resource access management', () => {
 
@@ -77,15 +91,44 @@ describe('Acceptance | Controller | users-controller-reset-competence-evaluation
 
     describe('Success case', () => {
 
-      const competenceEvaluationId = 111;
+      let response;
+      const otherStartedCompetenceId = 'recBejNZgJke422G';
 
       beforeEach(async () => {
         options.headers.authorization = generateValidRequestAuhorizationHeader(userId);
 
-        databaseBuilder.factory.buildCompetenceEvaluation({
-          id: competenceEvaluationId,
-          userId,
-          competenceId,
+        databaseBuilder.factory.buildUser({ id: userId });
+
+        _.each([
+          {
+            assessment: { id: 1, userId, },
+            competenceEvaluation: { id: 111, competenceId, userId, status: 'started' },
+            knowledgeElements: [
+              { id: 1, skillId: 'web1', status: 'validated', source: 'direct', competenceId, earnedPix: 1, },
+              { id: 2, skillId: 'web2', status: 'invalidated', source: 'direct', competenceId, earnedPix: 2, },
+              { id: 3, skillId: 'web4', status: 'invalidated', source: 'inferred', competenceId, earnedPix: 4, },
+              { id: 4, skillId: 'url2', status: 'validated', source: 'direct', competenceId, earnedPix: 4, },
+            ]
+          },
+          {
+            assessment: { id: 2, userId, },
+            competenceEvaluation: { id: 222, competenceId: otherStartedCompetenceId, userId, status: 'started' },
+            knowledgeElements: [
+              { id: 5, skillId: 'rechInfo3', status: 'validated', source: 'direct', competenceId: otherStartedCompetenceId, earnedPix: 3, },
+            ]
+          },
+          {
+            assessment: { id: 3, userId, },
+            campaignParticipation: { id: 111 },
+            knowledgeElements: [
+              { id: 6, skillId: 'url1', status: 'validated', source: 'direct', competenceId, earnedPix: 2, },
+            ]
+          }
+        ], ({ assessment, competenceEvaluation, knowledgeElements, campaignParticipation }) => {
+          const assessmentId = databaseBuilder.factory.buildAssessment(assessment).id;
+          databaseBuilder.factory.buildCompetenceEvaluation({ assessmentId, ...competenceEvaluation });
+          databaseBuilder.factory.buildCampaignParticipation({ assessmentId, ...campaignParticipation });
+          _.each(knowledgeElements, (ke) => databaseBuilder.factory.buildKnowledgeElement({ userId, assessmentId, ...ke }));
         });
 
         databaseBuilder.factory.buildKnowledgeElement({
@@ -104,11 +147,37 @@ describe('Acceptance | Controller | users-controller-reset-competence-evaluation
 
       it('should return 204', async () => {
         // when
-        const response = await server.inject(options);
+        response = await server.inject(options);
 
         // then
         expect(response.statusCode).to.equal(204);
         expect(response.result).to.be.null;
+      });
+
+      it('should have reset the competence evaluation', async () => {
+        // when
+        response = await server.inject(options);
+
+        // then
+        const competenceEvaluation = await inspectCompetenceEvaluationInDb({ userId, competenceId });
+        const otherCompetenceEvaluation = await inspectCompetenceEvaluationInDb({ userId, competenceId: otherStartedCompetenceId });
+        expect(competenceEvaluation[0].status).to.equal('reset');
+        expect(otherCompetenceEvaluation[0].status).to.equal('started');
+      });
+
+      it('should have reset the knowledge elements created from both competence evaluations and campaign', async () => {
+        // when
+        response = await server.inject(options);
+
+        // then
+        const knowledgeElement = await inspectKnowledgeElementsInDb({ userId, competenceId });
+        const knowledgeElementsOtherCompetence = await inspectKnowledgeElementsInDb({ userId, competenceId: otherStartedCompetenceId });
+
+        expect(knowledgeElement).to.have.length(5);
+        expect(knowledgeElement[0].earnedPix).to.equal(0);
+        expect(knowledgeElement[0].status).to.equal('reset');
+        expect(knowledgeElementsOtherCompetence[0].earnedPix).to.equal(3);
+        expect(knowledgeElementsOtherCompetence[0].status).to.equal('validated');
       });
     });
   });
