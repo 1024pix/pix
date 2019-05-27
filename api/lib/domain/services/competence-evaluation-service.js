25de5b75352c06c81c5d2e6faf93f69f72c49498
LJA <ljacquemin@octo.com>
2019-05-27 14:26:32 +0200

    [pf-579] Add reset competence evaluation in the service
    This commit adds the reset of the competence evaluation into the service
    so that the whole reset logic is encapsulated in the
    resetCompetenceEvaluation method, and not at scattered at different
    places.

--- api/lib/domain/services/competence-evaluation-service.js
+++ api/lib/domain/services/competence-evaluation-service.js
@@ -1,3 +1,4 @@
+const CompetenceEvaluation = require('../models/CompetenceEvaluation');
 const KnowledgeElement = require('../models/KnowledgeElement');
 const _ = require('lodash');
 
@@ -5,15 +6,22 @@ async function resetCompetenceEvaluation({
   userId,
   competenceId,
   knowledgeElementRepository,
+  competenceEvaluationRepository,
 }) {
+
+  return Promise.all([
+    _resetCompetenceEvaluation({ userId, competenceId, competenceEvaluationRepository }),
+    _resetKnowledgeElements({ userId, competenceId, knowledgeElementRepository }),
+  ]);
+}
+
+async function _resetKnowledgeElements({ userId, competenceId, knowledgeElementRepository }) {
   const knowledgeElements = await knowledgeElementRepository.findUniqByUserIdAndCompetenceId({
     userId, competenceId
   });
-
   const resetKnowledgeElementsPromises = _.map(knowledgeElements,
     (knowledgeElement) => _resetKnowledgeElement({ knowledgeElement, knowledgeElementRepository })
   );
-
   return Promise.all(resetKnowledgeElementsPromises);
 }
 
@@ -24,6 +32,12 @@ function _resetKnowledgeElement({ knowledgeElement, knowledgeElementRepository }
     ...knowledgeElement
   };
   return knowledgeElementRepository.save(newKnowledgeElement);
+}
+
+function _resetCompetenceEvaluation({ userId, competenceId, competenceEvaluationRepository }) {
+  return competenceEvaluationRepository.updateStatusByUserIdAndCompetenceId({
+    competenceId, userId, status: CompetenceEvaluation.statuses.RESET
+  });
 }
 
 module.exports = {
