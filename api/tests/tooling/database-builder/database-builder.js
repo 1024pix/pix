d1dc721951e770e733ebf8c8b1fd97b73c14b824
LJA <ljacquemin@octo.com>
2019-05-29 16:38:45 +0200

    Asyncinfy database-builder.js
    This commit asyncify the database builder which enhances readability.

--- api/tests/tooling/database-builder/database-builder.js
+++ api/tests/tooling/database-builder/database-builder.js
@@ -8,29 +8,17 @@ module.exports = class DatabaseBuilder {
     this.factory = factory;
   }
 
-  commit() {
-    const initialPromise = Promise.resolve();
-
-    return this.databaseBuffer.objectsToInsert.reduce((promise, objectToInsert) => {
-
-      return promise
-        .then(() => this.knex(objectToInsert.tableName).insert(objectToInsert.values));
-
-    }, initialPromise);
+  async commit() {
+    for (const objectToInsert of this.databaseBuffer.objectsToInsert) {
+      await this.knex(objectToInsert.tableName).insert(objectToInsert.values);
+    }
   }
 
-  clean() {
-    const initialPromise = Promise.resolve();
+  async clean() {
     const objectsToDelete = this.databaseBuffer.objectsToInsert.slice().reverse();
-
-    return objectsToDelete.reduce((promise, objectToDelete) => {
-
-      return promise
-        .then(() => this.knex(objectToDelete.tableName).where({ id: objectToDelete.values.id }).delete());
-
-    }, initialPromise)
-      .then(() => {
-        this.databaseBuffer.purge();
-      });
+    for (const objectToDelete of objectsToDelete) {
+      await this.knex(objectToDelete.tableName).where({ id: objectToDelete.values.id }).delete();
+    }
+    this.databaseBuffer.purge();
   }
 };
