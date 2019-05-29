0dfe9376133c00acfcf9d79744030cf8d366633d
Thierry Loesch <tloesch@octo.com>
2019-05-29 17:25:44 +0200

    Refacto notFromSession in component
    The component is certification-session-report

--- admin/tests/unit/components/certification-session-report-test.js
+++ admin/tests/unit/components/certification-session-report-test.js
@@ -66,7 +66,7 @@ module('Unit | Components | certification-session-report', function(hooks) {
   });
 
   module('#withoutCandidate', function() {
-    test('should return an empty array when every candidates certification IDs match an existing certification', function(assert) {
+    test('should return an empty array when every certification has an associated candidate', function(assert) {
       // given
       const candidates = [
         { row: 1, certificationId: 123 },
@@ -115,6 +115,58 @@ module('Unit | Components | certification-session-report', function(hooks) {
         { id: 200000 }
       ];
       assert.deepEqual(certificationsWithoutCandidate, expectedResult);
+    });
+  });
+
+  module('#notFromSession', function() {
+    test('should return an empty array when every candidates certification IDs match an existing certification', function(assert) {
+      // given
+      const candidates = [
+        { firstName: 'Toto', certificationId: 123 },
+        { firstName: 'Jean', certificationId: 456 },
+        { firstName: 'Michel', certificationId: 789 },
+      ];
+      component.set('candidates', candidates);
+
+      const certifications = [
+        { id: 123 },
+        { id: 456 },
+        { id: 789 },
+      ];
+      component.set('certifications', certifications);
+
+      // when
+      const certificationsWithoutCandidate = component.notFromSession;
+
+      // then
+      const expectedResult = [];
+      assert.deepEqual(certificationsWithoutCandidate, expectedResult);
+    });
+
+    test('should return an array with candidates missing associated certification', function(assert) {
+      // given
+      const candidateWithCertification =
+        { firstName: 'Candidate with certification', certificationId: 123 };
+      component.set('candidates', candidateWithCertification);
+
+      const candidatesWithoutCertification = [
+        { firstName: 'Candidate without certification 1', certificationId: 456 },
+        { firstName: 'Candidate without certification 2', certificationId: 789 },
+      ];
+      component.set('candidates', candidatesWithoutCertification);
+
+      const certifications = [
+        { id: 123 },
+        { id: 100000 },
+        { id: 200000 },
+      ];
+      component.set('certifications', certifications);
+
+      // when
+      const notFromSession = component.notFromSession;
+
+      // then
+      assert.deepEqual(notFromSession, candidatesWithoutCertification);
     });
   });
 
