205d49253faaac9209487c332600566317d672b2
Stephane Bedeau <sbedeau@octo.com>
2019-06-01 20:37:35 +0200

    [pf-578] Apply the feature to smart placement assessments

--- api/tests/unit/domain/usecases/get-assessment_test.js
+++ api/tests/unit/domain/usecases/get-assessment_test.js
@@ -1,6 +1,7 @@
 const { expect, sinon, domainBuilder } = require('../../../test-helper');
 const getAssessment = require('../../../../lib/domain/usecases/get-assessment');
 const assessmentRepository = require('../../../../lib/infrastructure/repositories/assessment-repository');
+const campaignRepository = require('../../../../lib/infrastructure/repositories/campaign-repository');
 const competenceRepository = require('../../../../lib/infrastructure/repositories/competence-repository');
 const competenceEvaluationRepository = require('../../../../lib/infrastructure/repositories/competence-evaluation-repository');
 const Assessment = require('../../../../lib/domain/models/Assessment');
@@ -10,18 +11,25 @@ describe('Unit | UseCase | get-assessment', () => {
 
   let assessment;
   let assessmentResult;
+  let campaign;
+  let campaignParticipation;
   let competence;
+  const expectedCampaignName = 'Campagne Il';
   const expectedAssessmentTitle = 'Traiter des données';
 
   beforeEach(() => {
     assessmentResult = domainBuilder.buildAssessmentResult();
-    assessment = domainBuilder.buildAssessment({
-      assessmentResults:[assessmentResult]
-    });
-
+    campaign = domainBuilder.buildCampaign({ title: expectedCampaignName });
+    campaignParticipation = domainBuilder.buildCampaignParticipation({ campaign });
     competence = domainBuilder.buildCompetence({ id: 'recsvLz0W2ShyfD63', name: expectedAssessmentTitle });
 
+    assessment = domainBuilder.buildAssessment({
+      assessmentResults:[assessmentResult],
+      campaignParticipation,
+    });
+
     sinon.stub(assessmentRepository, 'get');
+    sinon.stub(campaignRepository, 'get');
     sinon.stub(competenceEvaluationRepository, 'getByAssessmentId');
     sinon.stub(competenceRepository, 'get');
   });
@@ -40,7 +48,7 @@ describe('Unit | UseCase | get-assessment', () => {
     expect(result.estimatedLevel).to.equal(assessmentResult.level);
   });
 
-  it('should resolve the Assessment domain object with competence title matching the given assessment ID', async () => {
+  it('should resolve the Assessment domain object with COMPETENCE_EVALUATION title matching the given assessment ID', async () => {
     // given
     assessment.type = Assessment.types.COMPETENCE_EVALUATION;
     competenceEvaluationRepository.getByAssessmentId.resolves(competence);
@@ -63,9 +71,30 @@ describe('Unit | UseCase | get-assessment', () => {
     expect(result.title).to.equal(expectedAssessmentTitle);
   });
 
-  it('should resolve the Assessment domain object without competence title matching the given assessment ID', async () => {
+  it('should resolve the Assessment domain object with SMARTPLACEMENT title matching the given assessment ID', async () => {
     // given
     assessment.type = Assessment.types.SMARTPLACEMENT;
+    campaignRepository.get.resolves(campaign);
+    assessmentRepository.get.resolves(assessment);
+
+    // when
+    const result = await getAssessment({
+      assessmentId: assessment.id,
+      assessmentRepository,
+      campaignRepository,
+    });
+
+    // then
+    expect(result).to.be.an.instanceOf(Assessment);
+    expect(result.id).to.equal(assessment.id);
+    expect(result.pixScore).to.equal(assessmentResult.pixScore);
+    expect(result.estimatedLevel).to.equal(assessmentResult.level);
+    expect(result.title).to.equal(expectedCampaignName);
+  });
+
+  it('should resolve the Assessment domain object without title matching the given assessment ID', async () => {
+    // given
+    assessment.type = Assessment.types.DEMO;
     competenceEvaluationRepository.getByAssessmentId.resolves(competence);
     competenceRepository.get.resolves(competence);
     assessmentRepository.get.resolves(assessment);
