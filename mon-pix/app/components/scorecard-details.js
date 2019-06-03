18da0d2b412468b62a34c6da4f8b8b0cc6501b5b
lisequesnel <quesnel.lise@gmail.com>
2019-06-03 17:49:03 +0200

    [pf-579] Add a warning modal to reset

--- mon-pix/app/components/scorecard-details.js
+++ mon-pix/app/components/scorecard-details.js
@@ -1,9 +1,14 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
 
+  currentUser: service(),
+  store: service(),
+
   scorecard: null,
+  showResetModal: false,
 
   level: computed('scorecard.{level,isNotStarted}', function() {
     return this.get('scorecard.isNotStarted') ? null : this.get('scorecard.level');
@@ -25,5 +30,24 @@ export default Component.extend({
     const daysBeforeReset = this.get('scorecard.remainingDaysBeforeReset');
     return `Remise à zéro disponible dans ${daysBeforeReset} ${daysBeforeReset <= 1 ? 'jour' : 'jours'}`;
   }),
+
+  actions: {
+    openModal() {
+      this.set('showResetModal', true);
+    },
+
+    closeModal() {
+      this.set('showResetModal', false);
+    },
+
+    reset() {
+      this.currentUser.user.save({ adapterOptions: { resetCompetence: true, competenceId: this.get('scorecard.competenceId') } })
+        .then(() => {
+          this.get('scorecard').reload();
+        });
+
+      this.set('showResetModal', false);
+    }
+  },
 
 });
