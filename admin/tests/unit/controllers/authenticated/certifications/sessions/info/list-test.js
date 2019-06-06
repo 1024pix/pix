2a2381cf7295546e9e5334fabf5e1d6b4cb8101e
Jérémy Buget <jbuget@gmail.com>
2019-06-06 15:31:58 +0200

    Fix tests

--- admin/tests/unit/controllers/authenticated/certifications/sessions/info/list-test.js
+++ admin/tests/unit/controllers/authenticated/certifications/sessions/info/list-test.js
@@ -13,33 +13,35 @@ module('Unit | Controller | authenticated/certifications/sessions/info/list', fu
     assert.ok(controller);
   });
 
-  test('it generates well formatted result file', function(assert) {
+  test('it generates well formatted result file', async function(assert) {
     // given
     const fileSaverStub = Service.extend({
-      content:'',
+      content: '',
       saveAs(content) {
         this.set('content', content);
       },
       getContent() {
-        return this.get('content');
+        return this.content;
       }
     });
     this.owner.register('service:file-saver', fileSaverStub);
     const controller = this.owner.lookup('controller:authenticated/certifications/sessions/info/list');
-    controller.set('model', {
-      session:EmberObject.create({
-      }),
-      certifications:EmberObject.create({
-      }),
-      certificationIds:A([1,2,3,4,5])
-    });
+    controller.set('model', EmberObject.create({
+      certifications: A([
+        EmberObject.create({ id: 1 }),
+        EmberObject.create({ id: 2 }),
+        EmberObject.create({ id: 3 }),
+        EmberObject.create({ id: 4 }),
+        EmberObject.create({ id: 5 }),
+      ])
+    }));
     this.set('counter', 0);
     const that = this;
     controller.set('store', {
       findRecord(type, id) {
         that.set('counter', that.get('counter') + 1);
         return Promise.resolve(EmberObject.create({
-          id:id,
+          id: id,
           sessionId: 5,
           certificationId: id,
           assessmentId: 5,
@@ -49,18 +51,18 @@ module('Unit | Controller | authenticated/certifications/sessions/info/list', fu
           birthplace: 'une ville',
           externalId: '1234',
           creationDate: '20/07/2018 14:23:56',
-          completionDate:'20/07/2018 14:23:56',
-          resultsCreationDate:'20/07/2018 14:23:56',
+          completionDate: '20/07/2018 14:23:56',
+          resultsCreationDate: '20/07/2018 14:23:56',
           status: 'completed',
           juryId: '',
           commentForCandidate: 'candidate',
           commentForOrganization: 'organization',
           commentForJury: 'jury',
           pixScore: 100,
-          indexedCompetences:{
-            '1.1':{ level:1, score:2 },
-            '5.4':{ level:5, score:4 },
-            '4.3':{ level:-1, score:0 }
+          indexedCompetences: {
+            '1.1': { level: 1, score: 2 },
+            '5.4': { level: 5, score: 4 },
+            '4.3': { level: -1, score: 0 }
           }
         }));
       }
@@ -68,12 +70,12 @@ module('Unit | Controller | authenticated/certifications/sessions/info/list', fu
     assert.expect(2);
 
     // when
-    controller.send('onExportResults');
+    await controller.send('downloadResultsAfterJurysDeliberation');
 
     // then
     return settled()
       .then(() => {
-        assert.equal(this.get('counter'),5);
+        assert.equal(this.get('counter'), 5);
         const service = this.owner.lookup('service:file-saver');
         assert.equal(service.getContent(), '"Numéro de certification";"Prénom";"Nom";"Date de naissance";"Lieu de naissance";"Identifiant Externe";"Nombre de Pix";"1.1";"1.2";"1.3";"2.1";"2.2";"2.3";"2.4";"3.1";"3.2";"3.3";"3.4";"4.1";"4.2";"4.3";"5.1";"5.2";"Session";"Centre de certification";"Date de passage de la certification"\n1;"Toto";"Le héros";"20/03/1986";"une ville";"1234";100;1;"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";5;;"20/07/2018"\n2;"Toto";"Le héros";"20/03/1986";"une ville";"1234";100;1;"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";5;;"20/07/2018"\n3;"Toto";"Le héros";"20/03/1986";"une ville";"1234";100;1;"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";5;;"20/07/2018"\n4;"Toto";"Le héros";"20/03/1986";"une ville";"1234";100;1;"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";5;;"20/07/2018"\n5;"Toto";"Le héros";"20/03/1986";"une ville";"1234";100;1;"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";5;;"20/07/2018"\n');
       });
@@ -83,31 +85,33 @@ module('Unit | Controller | authenticated/certifications/sessions/info/list', fu
   test('it generates well formatted jury file', function(assert) {
     // given
     const fileSaverStub = Service.extend({
-      content:'',
+      content: '',
       saveAs(content) {
         this.set('content', content);
       },
       getContent() {
-        return this.get('content');
+        return this.content;
       }
     });
     this.owner.register('service:file-saver', fileSaverStub);
     const controller = this.owner.lookup('controller:authenticated/certifications/sessions/info/list');
-    controller.set('model', {
-      session:EmberObject.create({
-      }),
-      certifications:EmberObject.create({
-      }),
-      certificationIds:A([1,2,3,4,5])
-    });
+    controller.set('model', EmberObject.create({
+      certifications: A([
+        EmberObject.create({ id: 1 }),
+        EmberObject.create({ id: 2 }),
+        EmberObject.create({ id: 3 }),
+        EmberObject.create({ id: 4 }),
+        EmberObject.create({ id: 5 }),
+      ])
+    }));
     this.set('counter', 0);
     const that = this;
     controller.set('store', {
       findRecord(type, id) {
         that.set('counter', that.get('counter') + 1);
-        const status = (id == 2) ? 'error' : 'validated';
+        const status = (id === 2) ? 'error' : 'validated';
         return Promise.resolve(EmberObject.create({
-          id:id,
+          id: id,
           sessionId: 5,
           certificationId: id,
           assessmentId: 5,
@@ -117,32 +121,32 @@ module('Unit | Controller | authenticated/certifications/sessions/info/list', fu
           birthplace: 'une ville',
           externalId: '1234',
           creationDate: '20/07/2018 14:23:56',
-          completionDate:'20/07/2018 14:23:56',
-          resultsCreationDate:'20/07/2018 14:23:56',
+          completionDate: '20/07/2018 14:23:56',
+          resultsCreationDate: '20/07/2018 14:23:56',
           status: status,
           juryId: '',
           commentForCandidate: 'candidate',
           commentForOrganization: 'organization',
           commentForJury: 'jury',
           pixScore: 100,
-          indexedCompetences:{
-            '1.1':{ level:1, score:2 },
-            '5.4':{ level:5, score:4 },
-            '4.3':{ level:-1, score:0 }
+          indexedCompetences: {
+            '1.1': { level: 1, score: 2 },
+            '5.4': { level: 5, score: 4 },
+            '4.3': { level: -1, score: 0 }
           }
         }));
       }
     });
     assert.expect(2);
     const certificationWithComment = {
-      id:5,
+      id: 5,
       certificationId: 5,
       firstName: 'Toto',
       lastName: 'Le héros',
       birthdate: '20/03/1986',
       birthplace: 'une ville',
       externalId: '1234',
-      comments:'manager comments'
+      comments: 'manager comments'
     };
 
     // when
@@ -151,7 +155,7 @@ module('Unit | Controller | authenticated/certifications/sessions/info/list', fu
     // then
     return settled()
       .then(() => {
-        assert.equal(this.get('counter'),5);
+        assert.equal(this.get('counter'), 5);
         const service = this.owner.lookup('service:file-saver');
         assert.equal(service.getContent(), '"ID de session";"ID de certification";"Statut de la certification";"Date de debut";"Date de fin";"Commentaire surveillant";"Commentaire pour le jury";"Note Pix";"1.1";"1.2";"1.3";"2.1";"2.2";"2.3";"2.4";"3.1";"3.2";"3.3";"3.4";"4.1";"4.2";"4.3";"5.1";"5.2"\n5;2;"error";"20/07/2018 14:23:56";"20/07/2018 14:23:56";;"jury";100;1;"";"";"";"";"";"";"";"";"";"";"";"";-1;"";""\n5;5;"validated";"20/07/2018 14:23:56";"20/07/2018 14:23:56";"manager comments";"jury";100;1;"";"";"";"";"";"";"";"";"";"";"";"";-1;"";""\n');
       });
