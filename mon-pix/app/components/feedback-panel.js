2e6d220e98f0b24d3ea5374bd4249b0249d85f4d
Forster <groux.forster@gmail.com>
2019-06-14 16:26:12 +0200

    [pf-612] remove cancel button

--- mon-pix/app/components/feedback-panel.js
+++ mon-pix/app/components/feedback-panel.js
@@ -1,51 +1,23 @@
 import { isEmpty } from '@ember/utils';
 import $ from 'jquery';
-import { equal } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 import config from 'mon-pix/config/environment';
 
-const FORM_CLOSED = 'FORM_CLOSED';
-const FORM_OPENED = 'FORM_OPENED';
-const FORM_SUBMITTED = 'FORM_SUBMITTED';
-
 export default Component.extend({
-
-  store: service(),
 
   classNames: ['feedback-panel'],
 
+  store: service(),
+
   assessment: null,
   challenge: null,
-  collapsible: true,
 
-  _status: FORM_CLOSED,
   _content: null,
   _error: null,
 
-  isFormClosed: equal('_status', FORM_CLOSED),
-  isFormOpened: equal('_status', FORM_OPENED),
-  isFormSubmitted: equal('_status', FORM_SUBMITTED),
-
-  didReceiveAttrs() {
-    this._super(...arguments);
-    this._reset();
-  },
-
-  _reset() {
-    this.set('_content', null);
-    this.set('_error', null);
-    this.set('_status', this._getDefaultStatus());
-  },
-
-  _closeForm() {
-    this.set('_status', FORM_CLOSED);
-    this.set('_error', null);
-  },
-
-  _getDefaultStatus() {
-    return this.collapsible ? FORM_CLOSED : FORM_OPENED;
-  },
+  isFormOpened: false,
+  isSubmitted: false,
 
   _scrollToPanel: function() {
     $('html,body').animate({
@@ -55,33 +27,34 @@ export default Component.extend({
 
   actions: {
 
-    openFeedbackForm() {
-      this.set('_status', FORM_OPENED);
-      this._scrollToPanel();
+    toggleFeedbackForm() {
+      if (this.isFormOpened) {
+        this.set('isFormOpened', false);
+        this.set('isSubmitted', false);
+        this.set('_error', null);
+      } else {
+        this.set('isFormOpened', true);
+        this._scrollToPanel();
+      }
     },
 
-    cancelFeedback() {
-      this._closeForm();
-    },
-
-    sendFeedback() {
+    async sendFeedback() {
       const content = this._content;
+
       if (isEmpty(content) || isEmpty(content.trim())) {
         this.set('_error', 'Vous devez saisir un message.');
         return;
       }
 
-      const store = this.store;
-      const assessment = this.assessment;
-      const challenge = this.challenge;
-
-      const feedback = store.createRecord('feedback', {
-        content: this._content,
-        assessment,
-        challenge
+      const feedback = this.store.createRecord('feedback', {
+        content,
+        assessment: this.assessment,
+        challenge: this.challenge,
       });
-      feedback.save()
-        .then(() => this.set('_status', FORM_SUBMITTED));
+
+      await feedback.save();
+
+      this.set('isSubmitted', true);
     }
   }
 });
