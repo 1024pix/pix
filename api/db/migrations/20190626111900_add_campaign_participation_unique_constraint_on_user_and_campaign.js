fbf175153cda934799c2145ca0c2c03400029348
lisequesnel <quesnel.lise@gmail.com>
2019-06-26 16:05:38 +0200

    [PF-702] Migrate database
    -create unique constraint on campaignId and userId in campaign-participation table
    -delete rows which does not respect this constraint (delete is done based on first createdAt)

---
+++ api/db/migrations/20190626111900_add_campaign_participation_unique_constraint_on_user_and_campaign.js
@@ -0,0 +1,27 @@
+const TABLE_NAME = 'campaign-participations';
+
+exports.up = async function(knex) {
+  await knex.raw(`
+    DELETE FROM "campaign-participations"
+    WHERE id IN (
+      SELECT cp.id
+      FROM "campaign-participations" AS cp
+      INNER JOIN "campaign-participations" AS cpbis
+      ON cp."campaignId" = cpbis."campaignId" AND cp."userId" = cpbis."userId"
+      WHERE cp.id != cpbis.id
+      AND cp."createdAt" < cpbis."createdAt"
+    );
+  `);
+
+  await knex.schema.table(TABLE_NAME, (table) => {
+    table.unique(['campaignId', 'userId']);
+  });
+};
+
+exports.down = function(knex) {
+
+  return knex.schema.table(TABLE_NAME, (table) => {
+    table.dropUnique(['campaignId', 'userId']);
+  });
+};
+
