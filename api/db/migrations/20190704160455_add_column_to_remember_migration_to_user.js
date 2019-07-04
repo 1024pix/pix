7e30a52ea51723cf6d09721578f379edc41688ae
Melanie B <mboudard@octo.com>
2019-07-04 11:27:23 +0200

    Add migration to add column hasMigratedToV2 in users table

---
+++ api/db/migrations/20190704160455_add_column_to_remember_migration_to_user.js
@@ -0,0 +1,14 @@
+const TABLE_NAME = 'users';
+const COLUMN_NAME = 'isProfileV2';
+
+exports.up = function(knex) {
+  return knex.schema.table(TABLE_NAME, (table) => {
+    table.boolean(COLUMN_NAME).notNullable().defaultTo(false);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.table(TABLE_NAME, (table) => {
+    table.dropColumn(COLUMN_NAME);
+  });
+};
