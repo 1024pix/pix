27191e347661d4ed0cd251d3a9dffa880e68d3fd
Elodie Pelcat <elodiepelcat@pix.fr>
2019-07-08 11:20:30 +0200

    [po-99] Forbid access to team page for members

--- orga/app/models/membership.js
+++ orga/app/models/membership.js
@@ -1,6 +1,10 @@
 import DS from 'ember-data';
+import { equal } from '@ember/object/computed';
 
 export default DS.Model.extend({
   user: DS.belongsTo('user'),
-  organization: DS.belongsTo('organization')
+  organization: DS.belongsTo('organization'),
+  organizationRole: DS.attr('string'),
+
+  isOwner: equal('organizationRole', 'OWNER'),
 });
