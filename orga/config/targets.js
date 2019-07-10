be5a059e4fd617aa95b53af93dfc631c04944eb6
David Hérault <dherault@gmail.com>
2019-07-10 14:19:53 +0200

    Target Internet Explorer 9

--- orga/config/targets.js
+++ orga/config/targets.js
@@ -1,21 +1,10 @@
 'use strict';
 
-const browsers = [
-  'ie 11',
-  'last 1 Chrome versions',
-  'last 1 Firefox versions',
-  'last 1 Safari versions'
-];
-
-/* We override the Ember 3.4 config in order to support IE 11 even in development or integration environment */
-//
-// const isCI = !!process.env.CI;
-// const isProduction = process.env.EMBER_ENV === 'production';
-//
-// if (isCI || isProduction) {
-//   browsers.push('ie 11');
-// }
-
 module.exports = {
-  browsers
+  browsers: [
+    'ie 9',
+    'last 1 Chrome versions',
+    'last 1 Firefox versions',
+    'last 1 Safari versions',
+  ],
 };
