e4870fe3438e94d758f8d0e27fd66fad456ccd18
David Riviere <driviere@octo.com>
2019-07-22 16:45:13 +0200

    [pa-83] Add externalId to Organization API representation.

--- api/tests/tooling/database-builder/factory/build-organization.js
+++ api/tests/tooling/database-builder/factory/build-organization.js
@@ -9,11 +9,12 @@ const buildOrganization = function buildOrganization({
   name = faker.company.companyName(),
   code = 'ABCD12',
   logoUrl = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
+  externalId = faker.lorem.word(),
   userId = null,
   createdAt = faker.date.recent()
 } = {}) {
 
-  const values = { id, type, name, code, logoUrl, createdAt, userId };
+  const values = { id, type, name, code, logoUrl, externalId, createdAt, userId };
   return databaseBuffer.pushInsertable({
     tableName: 'organizations',
     values,
@@ -26,13 +27,14 @@ buildOrganization.withUser = function buildOrganizationWithUser({
   name = faker.company.companyName(),
   code = 'ABCD12',
   logoUrl = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
+  externalId = faker.lorem.word(),
   userId,
   createdAt = faker.date.recent()
 } = {}) {
 
   userId = _.isNil(userId) ? buildUser().id : userId;
 
-  const values = { id, type, name, code, logoUrl, createdAt, userId };
+  const values = { id, type, name, code, logoUrl, externalId, createdAt, userId };
   return databaseBuffer.pushInsertable({
     tableName: 'organizations',
     values,
