4469780832a8a633a6bb439e8ba778a0cde9544a
Melanie B <mboudard@octo.com>
2019-08-06 17:56:43 +0200

    [pf-750] Add constant "DAYS_BEFORE_IMPROVING"
    - Add a constant to know how many days we have to wait before retry a knowledge element

--- api/lib/settings.js
+++ api/lib/settings.js
@@ -81,6 +81,7 @@ module.exports = (function() {
     },
 
     features: {
+      dayBeforeImproving: process.env.DAYS_BEFORE_IMPROVING,
       dayBeforeCompetenceResetV2: process.env.DAY_BEFORE_COMPETENCE_RESET_V2
     },
   };
