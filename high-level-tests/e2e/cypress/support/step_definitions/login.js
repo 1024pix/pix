0281d084d02cf6dc3d31dce6a18d1dd9c41a3465
Elodie Pelcat <elodiepelcat@pix.fr>
2019-08-06 15:25:19 +0200

    [pf-774] add e2e test

--- high-level-tests/e2e/cypress/support/step_definitions/login.js
+++ high-level-tests/e2e/cypress/support/step_definitions/login.js
@@ -4,6 +4,10 @@ given(`je me connecte avec le compte {string}`, (email) => {
   cy.get('button[type=submit]').click();
 });
 
+when('je vais sur Pix via un organisme externe', () => {
+  cy.loginToken('daenerys.targaryen@pix.fr', 'pix123');
+});
+
 then(`je suis redirigé vers le compte de {string}`, (fullName) => {
   cy.url().should('include', '/compte');
   cy.get('.logged-user-name').should((userName) => {
