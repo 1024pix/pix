9d40dd7859320c8fc3363eb8b302a589266f616d
Thierry Loesch <tloesch@octo.com>
2019-08-07 15:59:07 +0200

    [pf-788] Show RA links when the PR is created
    The script scalingo-post-ra-creation from root package.json now post the links in the PR

--- scripts/signal_deploy_to_pr.sh
+++ scripts/signal_deploy_to_pr.sh
@@ -9,16 +9,13 @@
 	echo 'FATAL: $GITHUB_USER_TOKEN is absent'
 	exit 1
 }
-[ -z $CIRCLE_BRANCH ] && {
-	echo 'FATAL: $CIRCLE_BRANCH is absent'
-	exit 1
-}
-[ -z $CI_PULL_REQUEST ] && {
-	echo 'INFO: $CI_PULL_REQUEST is absent. I will not post a message to github. Bye !'
+
+[ -z $APP ] && {
+	echo 'INFO: $APP is absent. I will not post a message to github. Bye !'
 	exit 0
 }
 
-PR_NUMBER=`echo $CI_PULL_REQUEST | grep -Po '(?<=pix/pull/)(\d+)'`
+PR_NUMBER=$(echo $APP | grep -Po '(?<=integration-pr)\d+')
 RA_APP_URL="https://app-pr$PR_NUMBER.review.pix.fr"
 RA_ORGA_URL="https://orga-pr$PR_NUMBER.review.pix.fr"
 RA_CERTIF_URL="https://certif-pr$PR_NUMBER.review.pix.fr"
