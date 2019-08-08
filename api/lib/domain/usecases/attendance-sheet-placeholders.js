06bc925311f341581626958cdb2dba9bf03fa60f
laura-bergoens <laura.bergoens@gmail.com>
2019-08-08 16:23:07 +0200

    [pc-61] Extract constants from usecase

---
+++ api/lib/domain/usecases/attendance-sheet-placeholders.js
@@ -0,0 +1,38 @@
+const ATTENDANCE_SHEET_SESSION_TEMPLATE_VALUES = [
+  {
+    placeholder: 'SESSION_ID',
+    propertyName: 'id',
+  },
+  {
+    placeholder: 'SESSION_START_DATE',
+    propertyName: 'date',
+  },
+  {
+    placeholder: 'SESSION_START_TIME',
+    propertyName: 'startTime',
+  },
+  {
+    placeholder: 'SESSION_END_TIME',
+    propertyName: 'endTime',
+  },
+  {
+    placeholder: 'SESSION_ADDRESS',
+    propertyName: 'address',
+  },
+  {
+    placeholder: 'SESSION_ROOM',
+    propertyName: 'room',
+  },
+  {
+    placeholder: 'SESSION_EXAMINER',
+    propertyName: 'examiner',
+  },
+  {
+    placeholder: 'CERTIFICATION_CENTER_NAME',
+    propertyName: 'certificationCenterName',
+  },
+];
+
+module.exports = {
+  ATTENDANCE_SHEET_SESSION_TEMPLATE_VALUES,
+};
