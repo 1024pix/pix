a55e9781a1c2585311b6c69ee01a5c74214c4bb2
laura-bergoens <laura.bergoens@gmail.com>
2019-08-08 16:23:07 +0200

    [pc-61] Extract constants from usecase

--- api/lib/domain/usecases/get-attendance-sheet.js
+++ api/lib/domain/usecases/get-attendance-sheet.js
@@ -1,43 +1,9 @@
 const odsService = require('../services/ods-service');
 const sessionXmlService = require('../services/session-xml-service');
+const { ATTENDANCE_SHEET_SESSION_TEMPLATE_VALUES } = require('./attendance-sheet-placeholders');
 const { UserNotAuthorizedToAccessEntity } = require('../errors');
 const moment = require('moment');
 const _ = require('lodash');
-
-const ATTENDANCE_SHEET_TEMPLATE_VALUES = [
-  {
-    placeholder: 'SESSION_ID',
-    propertyName: 'id',
-  },
-  {
-    placeholder: 'SESSION_START_DATE',
-    propertyName: 'date',
-  },
-  {
-    placeholder: 'SESSION_START_TIME',
-    propertyName: 'startTime',
-  },
-  {
-    placeholder: 'SESSION_END_TIME',
-    propertyName: 'endTime',
-  },
-  {
-    placeholder: 'SESSION_ADDRESS',
-    propertyName: 'address',
-  },
-  {
-    placeholder: 'SESSION_ROOM',
-    propertyName: 'room',
-  },
-  {
-    placeholder: 'SESSION_EXAMINER',
-    propertyName: 'examiner',
-  },
-  {
-    placeholder: 'CERTIFICATION_CENTER_NAME',
-    propertyName: 'certificationCenterName',
-  },
-];
 
 module.exports = getAttendanceSheet;
 
@@ -52,7 +18,7 @@ async function getAttendanceSheet({ userId, sessionId, sessionRepository }) {
   const stringifiedXml = await odsService.getContentXml({ odsFilePath: _getAttendanceTemplatePath() });
   const session = await sessionRepository.get(sessionId);
   const attendanceSheetData = _.transform(session, transformSessionIntoAttendanceSheetData);
-  const stringifiedUpdatedXml = sessionXmlService.getUpdatedXmlWithSessionData({ stringifiedXml, dataToInject: attendanceSheetData, templateValues: ATTENDANCE_SHEET_TEMPLATE_VALUES });
+  const stringifiedUpdatedXml = sessionXmlService.getUpdatedXmlWithSessionData({ stringifiedXml, dataToInject: attendanceSheetData, templateValues: ATTENDANCE_SHEET_SESSION_TEMPLATE_VALUES });
 
   return await odsService.makeUpdatedOdsByContentXml({ stringifiedXml: stringifiedUpdatedXml, odsFilePath: _getAttendanceTemplatePath() });
 }
--- api/tests/unit/domain/usecases/get-attendance-sheet_test.js
+++ api/tests/unit/domain/usecases/get-attendance-sheet_test.js
@@ -1,5 +1,6 @@
 const { expect, sinon } = require('../../../test-helper');
 const getAttendanceSheet = require('../../../../lib/domain/usecases/get-attendance-sheet');
+const { ATTENDANCE_SHEET_SESSION_TEMPLATE_VALUES } = require('../../../../lib/domain/usecases/attendance-sheet-placeholders');
 const odsService = require('../../../../lib/domain/services/ods-service');
 const sessionXmlService = require('../../../../lib/domain/services/session-xml-service');
 const _ = require('lodash');
@@ -11,42 +12,6 @@ describe('Unit | UseCase | get-attendance-sheet-in-ods-format', () => {
   const userId = 'dummyUserId';
   const sessionId = 'dummySessionId';
   const sessionRepository = { get: _.noop, ensureUserHasAccessToSession: _.noop };
-
-  const ATTENDANCE_SHEET_TEMPLATE_VALUES = [
-    {
-      placeholder: 'SESSION_ID',
-      propertyName: 'id',
-    },
-    {
-      placeholder: 'SESSION_START_DATE',
-      propertyName: 'date',
-    },
-    {
-      placeholder: 'SESSION_START_TIME',
-      propertyName: 'startTime',
-    },
-    {
-      placeholder: 'SESSION_END_TIME',
-      propertyName: 'endTime',
-    },
-    {
-      placeholder: 'SESSION_ADDRESS',
-
-      propertyName: 'address',
-    },
-    {
-      placeholder: 'SESSION_ROOM',
-      propertyName: 'room',
-    },
-    {
-      placeholder: 'SESSION_EXAMINER',
-      propertyName: 'examiner',
-    },
-    {
-      placeholder: 'CERTIFICATION_CENTER_NAME',
-      propertyName: 'certificationCenterName',
-    },
-  ];
 
   const session = {
     id: 1,
@@ -79,7 +44,7 @@ describe('Unit | UseCase | get-attendance-sheet-in-ods-format', () => {
       sinon.stub(sessionRepository, 'get').resolves(session);
       sinon.stub(odsService, 'getContentXml').resolves(stringifiedXml);
       sinon.stub(odsService, 'makeUpdatedOdsByContentXml').resolves(odsBuffer);
-      sinon.stub(sessionXmlService, 'getUpdatedXmlWithSessionData').withArgs({ stringifiedXml, sessionData: attendanceSheetSessionData, sessionTemplateValues: ATTENDANCE_SHEET_TEMPLATE_VALUES }).returns(stringifiedSessionUpdatedXml);
+      sinon.stub(sessionXmlService, 'getUpdatedXmlWithSessionData').withArgs({ stringifiedXml, sessionData: attendanceSheetSessionData, sessionTemplateValues: ATTENDANCE_SHEET_SESSION_TEMPLATE_VALUES }).returns(stringifiedSessionUpdatedXml);
     });
 
     context('user has access to the session', () => {
@@ -95,7 +60,7 @@ describe('Unit | UseCase | get-attendance-sheet-in-ods-format', () => {
         expect(sessionRepository.getWithCertificationCandidates).to.have.been.calledWithExactly(sessionId);
       });
       it('should have build an updated content.xml file from attendance sheet data', () => {
-        expect(sessionXmlService.getUpdatedXmlWithSessionData).to.have.been.calledWithExactly({ stringifiedXml, sessionData: attendanceSheetSessionData, sessionTemplateValues: ATTENDANCE_SHEET_TEMPLATE_VALUES });
+        expect(sessionXmlService.getUpdatedXmlWithSessionData).to.have.been.calledWithExactly({ stringifiedXml, sessionData: attendanceSheetSessionData, sessionTemplateValues: ATTENDANCE_SHEET_SESSION_TEMPLATE_VALUES });
       });
       it('should have rebuild the ods zip with new content.xml file', () => {
         expect(odsService.makeUpdatedOdsByContentXml).to.have.been.calledWithExactly({ stringifiedXml: stringifiedSessionUpdatedXml, odsFilePath: sinon.match('attendance_sheet_template.ods') });
