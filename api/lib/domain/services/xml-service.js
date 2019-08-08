dd1861fa07b37f3afbd07f982a419a3ef966047e
laura-bergoens <laura.bergoens@gmail.com>
2019-08-08 10:45:17 +0200

    [pc-61] XML Service can now add generic rows
    XML service has been modified so it is domain agnostic. It now presents two distinct features when updating a XML DOM:
    1) Can replace sparse values throughout the XML DOM using placeholders.
    2) Can add generic rows based on a model row defined by its placeholders.

--- api/lib/domain/services/xml-service.js
+++ api/lib/domain/services/xml-service.js
@@ -1,28 +1,115 @@
 const { DOMParser, XMLSerializer } = require('xmldom');
+const _ = require('lodash');
 
 module.exports = {
-  getUpdatedXml
+  updateXmlSparseValues,
+  updateXmlRows,
 };
 
-function getUpdatedXml({ stringifiedXml, templateValues, dataToInject }) {
+function updateXmlSparseValues({ stringifiedXml, templateValues, dataToInject }) {
   const parsedXmlDom = _buildXmlDomFromXmlString(stringifiedXml);
-  const updatedXmlDom = _updatedXmlDomWithSessionData(parsedXmlDom, dataToInject, templateValues);
-  return _buildStringifiedXmlFromXmlDom(updatedXmlDom);
+  const parsedXmlDomUpdated = _updateXmlDomWithData(parsedXmlDom, dataToInject, templateValues);
+  return _buildStringifiedXmlFromXmlDom(parsedXmlDomUpdated);
+}
+
+function updateXmlRows({ stringifiedXml, rowMarkerPlaceholder, rowTemplateValues, dataToInject }) {
+  const parsedXmlDom = _buildXmlDomFromXmlString(stringifiedXml);
+
+  const {
+    referenceRowElement,
+    rowsContainerElement,
+  } = _getRefRowAndContainerDomElements(parsedXmlDom, rowMarkerPlaceholder);
+
+  const cloneRowElement = _deepCloneDomElement(referenceRowElement);
+  rowsContainerElement.removeChild(referenceRowElement);
+
+  _.each(dataToInject, (rowDataToInject) => {
+    const currentCloneRowElement = _deepCloneDomElement(cloneRowElement);
+    _updateXmlElementWithData(currentCloneRowElement, rowDataToInject, rowTemplateValues);
+    rowsContainerElement.appendChild(currentCloneRowElement);
+  });
+
+  return _buildStringifiedXmlFromXmlDom(parsedXmlDom);
+}
+
+function _getRefRowAndContainerDomElements(parsedXmlDom, rowMarkerPlaceholder) {
+  const referenceRowElement = _getXmlDomElementByText(parsedXmlDom, rowMarkerPlaceholder).parentNode.parentNode;
+  return {
+    referenceRowElement,
+    rowsContainerElement: referenceRowElement.parentNode,
+  };
+}
+
+function _deepCloneDomElement(domElement) {
+  const isDeep = true;
+  return domElement.cloneNode(isDeep);
 }
 
 function _buildXmlDomFromXmlString(stringifiedXml) {
   return new DOMParser().parseFromString(stringifiedXml);
 }
 
-function _updatedXmlDomWithSessionData(parsedXmlDom, dataToInject, templateValues) {
-  for (const templateValue of templateValues) {
-    const targetXmlDomElement = _getXmlDomElementByText(parsedXmlDom, templateValue.placeholder);
+function _updateXmlDomWithData(parsedXmlDom, dataToInject, templateValues) {
+  const parsedXmlDomUpdated = _.cloneDeep(parsedXmlDom);
+  for (const { placeholder, propertyName } of templateValues) {
+    const targetXmlDomElement = _getXmlDomElementByText(parsedXmlDomUpdated, placeholder);
     if (targetXmlDomElement) {
-      const newXmlValue = dataToInject[templateValue.propertyName];
+      const newXmlValue = dataToInject[propertyName];
       targetXmlDomElement.textContent = newXmlValue;
     }
   }
-  return parsedXmlDom;
+  return parsedXmlDomUpdated;
+}
+
+function _updateXmlElementWithData(xmlElement, dataToInject, templateValues) {
+  for (const { placeholder, propertyName } of templateValues) {
+    const targetXmlDomElement = _getXmlDomElementByText(xmlElement, placeholder);
+    if (targetXmlDomElement) {
+      _updateXmlElementByType(targetXmlDomElement.parentNode, targetXmlDomElement, dataToInject[propertyName]);
+    }
+  }
+}
+
+function _updateXmlElementByType(xmlElement, xmlElementTextChild, data) {
+  if (data === '') {
+    _clearXmlElement(xmlElement, xmlElementTextChild);
+    return;
+  }
+  const valueType = xmlElement.getAttribute('office:value-type');
+  switch (valueType) {
+    case 'date':
+      _updateDateXmlElement(xmlElement, xmlElementTextChild, data);
+      break;
+    case 'percentage':
+      _updatePercentageXmlElement(xmlElement, xmlElementTextChild, data);
+      break;
+    case 'string':
+    default:
+      _updateStringXmlElement(xmlElementTextChild, data);
+      break;
+  }
+}
+
+function _clearXmlElement(xmlElement, xmlElementTextChild) {
+  xmlElement.removeChild(xmlElementTextChild);
+  xmlElement.removeAttribute('office:value-type');
+  xmlElement.removeAttribute('office:date-value');
+  xmlElement.removeAttribute('calcext:value-type');
+  xmlElement.removeAttribute('office:value');
+}
+
+function _updateStringXmlElement(xmlElementTextChild, data) {
+  xmlElementTextChild.textContent = data;
+}
+
+function _updateDateXmlElement(xmlElement, xmlElementTextChild, data) {
+  xmlElement.setAttribute('office:date-value', data);
+  xmlElement.removeChild(xmlElementTextChild);
+}
+
+function _updatePercentageXmlElement(xmlElement, xmlElementTextChild, data) {
+  xmlElement.setAttribute('office:value', data);
+  xmlElement.removeChild(xmlElementTextChild);
 }
 
 function _getXmlDomElementByText(parsedXmlDom, text) {
--- api/tests/unit/domain/usecases/get-attendance-sheet_test.js
+++ api/tests/unit/domain/usecases/get-attendance-sheet_test.js
@@ -1,7 +1,7 @@
 const { expect, sinon } = require('../../../test-helper');
 const getAttendanceSheet = require('../../../../lib/domain/usecases/get-attendance-sheet');
 const odsService = require('../../../../lib/domain/services/ods-service');
-const xmlService = require('../../../../lib/domain/services/xml-service');
+const sessionXmlService = require('../../../../lib/domain/services/session-xml-service');
 const _ = require('lodash');
 const { UserNotAuthorizedToAccessEntity } = require('../../../../lib/domain/errors');
 
@@ -53,12 +53,12 @@ describe('Unit | UseCase | get-attendance-sheet-in-ods-format', () => {
     address: 'Rue de bercy',
     room: 'Salle 2',
     examiner: 'Benoit',
-    date: '2018-01-15',
+    date: '2018-01-16',
     time: '14:00:00',
     certificationCenter: 'Tour Gamma',
   };
 
-  const attendanceSheetData = {
+  const attendanceSheetSessionData = {
     id: 1,
     address: 'Rue de bercy',
     room: 'Salle 2',
@@ -66,11 +66,11 @@ describe('Unit | UseCase | get-attendance-sheet-in-ods-format', () => {
     certificationCenterName: 'Tour Gamma',
     startTime: '14:00',
     endTime: '16:00',
-    date: '15/01/2018'
+    date: '16/01/2018',
   };
 
   const stringifiedXml = '<xml>Some xml</xml>';
-  const stringifiedUpdatedXml = '<xml>Some updated xml</xml>';
+  const stringifiedSessionUpdatedXml = '<xml>Some updated session xml</xml>';
   const odsBuffer = Buffer.from('some ods file');
 
   describe('getAttendanceSheet', () => {
@@ -79,8 +79,7 @@ describe('Unit | UseCase | get-attendance-sheet-in-ods-format', () => {
       sinon.stub(sessionRepository, 'get').resolves(session);
       sinon.stub(odsService, 'getContentXml').resolves(stringifiedXml);
       sinon.stub(odsService, 'makeUpdatedOdsByContentXml').resolves(odsBuffer);
-      sinon.stub(xmlService, 'getUpdatedXml').returns(stringifiedUpdatedXml);
-      result = await getAttendanceSheet({ userId, sessionId, sessionRepository });
+      sinon.stub(sessionXmlService, 'getUpdatedXmlWithSessionData').withArgs({ stringifiedXml, sessionData: attendanceSheetSessionData, sessionTemplateValues: ATTENDANCE_SHEET_TEMPLATE_VALUES }).returns(stringifiedSessionUpdatedXml);
     });
 
     context('user has access to the session', () => {
@@ -92,14 +91,14 @@ describe('Unit | UseCase | get-attendance-sheet-in-ods-format', () => {
       it('should return the attendance sheet', () => {
         expect(result).to.deep.equal(odsBuffer);
       });
-      it('should have fetched the session', () => {
-        expect(sessionRepository.get).to.have.been.calledWithExactly(sessionId);
+      it('should have fetched the session with certification candidates', () => {
+        expect(sessionRepository.getWithCertificationCandidates).to.have.been.calledWithExactly(sessionId);
       });
       it('should have build an updated content.xml file from attendance sheet data', () => {
-        expect(xmlService.getUpdatedXml).to.have.been.calledWithExactly({ stringifiedXml, dataToInject: attendanceSheetData, templateValues: ATTENDANCE_SHEET_TEMPLATE_VALUES });
+        expect(sessionXmlService.getUpdatedXmlWithSessionData).to.have.been.calledWithExactly({ stringifiedXml, sessionData: attendanceSheetSessionData, sessionTemplateValues: ATTENDANCE_SHEET_TEMPLATE_VALUES });
       });
       it('should have rebuild the ods zip with new content.xml file', () => {
-        expect(odsService.makeUpdatedOdsByContentXml).to.have.been.calledWithExactly({ stringifiedXml: stringifiedUpdatedXml, odsFilePath: sinon.match('attendance_sheet_template.ods') });
+        expect(odsService.makeUpdatedOdsByContentXml).to.have.been.calledWithExactly({ stringifiedXml: stringifiedSessionUpdatedXml, odsFilePath: sinon.match('attendance_sheet_template.ods') });
       });
       it('should return something when user has access', async () => {
         expect(result).to.deep.equal(odsBuffer);
