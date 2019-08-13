8a0656eef9595dca3ad382819d7f38ce39e56f2f
Forster <groux.forster@gmail.com>
2019-08-13 11:47:00 +0200

    [pf-754] fix test after rebase

--- mon-pix/tests/acceptance/navigation-test.js
+++ mon-pix/tests/acceptance/navigation-test.js
@@ -1,21 +1,18 @@
-import { afterEach, beforeEach, describe, it } from 'mocha';
+import { beforeEach, describe, it } from 'mocha';
 import { expect } from 'chai';
-import startApp from '../helpers/start-app';
-import destroyApp from '../helpers/destroy-app';
 import defaultScenario from '../../mirage/scenarios/default';
 import { authenticateAsSimpleUser } from '../helpers/testing';
+import { setupApplicationTest } from 'ember-mocha';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+import visitWithAbortedTransition from '../helpers/visit';
+import { click, currentURL, find } from '@ember/test-helpers';
 
 describe('Acceptance | Navbar', function() {
-
-  let application;
+  setupApplicationTest();
+  setupMirage();
 
   beforeEach(function() {
-    application = startApp();
-    defaultScenario(server);
-  });
-
-  afterEach(function() {
-    destroyApp(application);
+    defaultScenario(this.server);
   });
 
   describe('Authenticated cases as simple user', function() {
@@ -39,27 +36,24 @@ describe('Acceptance | Navbar', function() {
     ].forEach((usecase) => {
       it(`should redirect from "${usecase.initialRoute}" to "${usecase.expectedRoute}"`, async function() {
         // given
-        await visit(usecase.initialRoute);
-        expect(find(usecase.initialNavigationItem).attr('class')).to.contain('active');
+        await visitWithAbortedTransition(usecase.initialRoute);
+        expect(find(usecase.initialNavigationItem).getAttribute('class')).to.contain('active');
 
         // when
         await click(usecase.targetedNavigationItem);
 
         // then
         expect(currentURL()).to.equal(usecase.expectedRoute);
-        expect(find(usecase.targetedNavigationItem).attr('class')).to.contain('active');
+        expect(find(usecase.targetedNavigationItem).getAttribute('class')).to.contain('active');
       });
     });
 
     it('should contain link to pix.fr/aide', async function() {
-      // given
-      await visit('/profil');
-
       // when
-      const faqLink = findWithAssert('.navbar-desktop-header-menu__item:nth-child(4)');
+      await visitWithAbortedTransition('/profil');
 
       // then
-      expect(faqLink.attr('href').trim()).to.equal('https://pix.fr/aide');
+      expect(find('.navbar-desktop-header-menu__item:nth-child(4)').getAttribute('href')).to.equal('https://pix.fr/aide');
     });
   });
 });
