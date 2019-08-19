51ff9b368f29b8b44cf46499cfcf753ad1ff571a
LJA <ljacquemin@octo.com>
2019-08-19 16:48:58 +0200

    [pc-67] Unplug the possibility to certify V1 profiles
    Because of the V1->V2 migration, it should not be possible for users to
    certify their V1 profiles anymore. However, if the V1 profile score was
    superior to the V2 profile score, the code would still allow V1 profiles
    to be certified. This commit will prevent this situation from happening
    in the future.

--- api/lib/domain/usecases/retrieve-last-or-create-certification-course.js
+++ api/lib/domain/usecases/retrieve-last-or-create-certification-course.js
@@ -10,32 +10,6 @@ function _canStartACertification(userCompetences) {
   return _.size(competencesWithEstimatedLevelHigherThan0) >= 5;
 }
 
-function _selectProfileToCertify(userCompetencesProfileV1, userCompetencesProfileV2) {
-  const canStartACertificationOnProfileV2 = _canStartACertification(userCompetencesProfileV2);
-  const canStartACertificationOnProfileV1 = _canStartACertification(userCompetencesProfileV1);
-
-  if (!canStartACertificationOnProfileV1 && !canStartACertificationOnProfileV2) {
-    return null;
-  }
-
-  else if (canStartACertificationOnProfileV1 && !canStartACertificationOnProfileV2) {
-    return userCompetencesProfileV1;
-  }
-
-  else if (!canStartACertificationOnProfileV1 && canStartACertificationOnProfileV2) {
-    return userCompetencesProfileV2;
-  }
-
-  else {
-    const pixScoreProfileV1 = _.sumBy(userCompetencesProfileV1, 'pixScore');
-    const pixScoreProfileV2 = _.sumBy(userCompetencesProfileV2, 'pixScore');
-
-    if (pixScoreProfileV1 >= pixScoreProfileV2) return userCompetencesProfileV1;
-
-    return userCompetencesProfileV2;
-  }
-}
-
 async function _startNewCertification({
   userId,
   sessionId,
@@ -45,21 +19,15 @@ async function _startNewCertification({
 }) {
 
   const now = new Date();
-  const [userCompetencesProfileV1, userCompetencesProfileV2] = await Promise.all([
-    userService.getProfileToCertifyV1({ userId, limitDate: now }),
-    userService.getProfileToCertifyV2({ userId, limitDate: now }),
-  ]);
+  const userCompetencesProfile = await userService.getProfileToCertifyV2({ userId, limitDate: now });
 
-  const userCompetencesToCertify = _selectProfileToCertify(userCompetencesProfileV1, userCompetencesProfileV2);
-  if (!userCompetencesToCertify) {
+  if (!_canStartACertification(userCompetencesProfile)) {
     throw new UserNotAuthorizedToCertifyError();
   }
 
-  const isV2Certification = userCompetencesToCertify === userCompetencesProfileV2;
-
-  const newCertificationCourse = new CertificationCourse({ userId, sessionId, isV2Certification });
+  const newCertificationCourse = new CertificationCourse({ userId, sessionId, isV2Certification: true });
   const savedCertificationCourse = await certificationCourseRepository.save(newCertificationCourse);
-  return certificationChallengesService.saveChallenges(userCompetencesToCertify, savedCertificationCourse);
+  return certificationChallengesService.saveChallenges(userCompetencesProfile, savedCertificationCourse);
 }
 
 module.exports = async function retrieveLastOrCreateCertificationCourse({
