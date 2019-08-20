63ac49180ed1384aa225342c90ca16734154c861
lisequesnel <quesnel.lise@gmail.com>
2019-08-20 19:13:59 +0200

    [po-217] Add several attributes to student

---
+++ api/db/migrations/20190820092603_add_several_attributes_to_student.js
@@ -0,0 +1,33 @@
+const TABLE_NAME = 'students';
+
+exports.up = function(knex) {
+  return knex.schema.table(TABLE_NAME, (table) => {
+    table.string('preferredLastName');
+    table.string('middleName');
+    table.string('thirdName');
+    table.string('birthCity');
+    table.string('birthCityCode');
+    table.string('birthProvinceCode');
+    table.string('birthCountryCode');
+    table.string('MEFCode');
+    table.string('status');
+    table.string('nationalStudentId').unique().index();
+    table.string('division');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.table(TABLE_NAME, (table) => {
+    table.dropColumn('preferredLastName');
+    table.dropColumn('middleName');
+    table.dropColumn('thirdName');
+    table.dropColumn('birthCity');
+    table.dropColumn('birthCityCode');
+    table.dropColumn('birthProvinceCode');
+    table.dropColumn('birthCountryCode');
+    table.dropColumn('MEFCode');
+    table.dropColumn('status');
+    table.dropColumn('nationalStudentId');
+    table.dropColumn('division');
+  });
+};
