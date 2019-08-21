04ea8e16bedb6495bcba516181477380550647eb
Jonas Grillères <grilleres.j@gmail.com>
2019-08-21 12:07:58 +0200

    [pf-782] Add dropdowns to label issues on feedback-panel

--- mon-pix/app/components/feedback-panel.js
+++ mon-pix/app/components/feedback-panel.js
@@ -1,8 +1,11 @@
 import { isEmpty } from '@ember/utils';
 import $ from 'jquery';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 import Component from '@ember/component';
 import config from 'mon-pix/config/environment';
+
+import { topLevelLabels, questions } from 'mon-pix/static-data/feedback-panel-issue-labels';
 
 export default Component.extend({
 
@@ -16,8 +19,23 @@ export default Component.extend({
   isFormOpened: false,
 
   _content: null,
+  _category: null,
   _error: null,
   _isSubmitted: false,
+
+  nextCategory: null,
+  tutorialContent: null,
+  displayTextBox: null,
+  displaySecondDropdown: false,
+
+  _questions: questions,
+
+  categories: computed('context', function() {
+    if (this.context === 'comparison-window') {
+      return topLevelLabels;
+    }
+    return topLevelLabels.filter((label) => !label.displayOnlyOnComparisonWindow);
+  }),
 
   _scrollToPanel: function() {
     $('html,body').animate({
@@ -36,6 +54,17 @@ export default Component.extend({
     this.set('_content', null);
   },
 
+  _showFeedbackActionBasedOnCategoryType(category) {
+    this.set('displayTextBox', false);
+    this.set('tutorialContent', null);
+
+    if (category.type === 'tutorial') {
+      this.set('tutorialContent', category.content);
+    } else if (category.type === 'textbox') {
+      this.set('displayTextBox', true);
+    }
+  },
+
   actions: {
 
     toggleFeedbackForm() {
@@ -50,6 +79,7 @@ export default Component.extend({
 
     async sendFeedback() {
       const content = this._content;
+      const category = this._category;
 
       if (isEmpty(content) || isEmpty(content.trim())) {
         this.set('_error', 'Vous devez saisir un message.');
@@ -58,14 +88,38 @@ export default Component.extend({
 
       const feedback = this.store.createRecord('feedback', {
         content,
+        category,
         assessment: this.assessment,
         challenge: this.challenge,
       });
 
       await feedback.save();
 
-      this.set('_content', null);
       this.set('_isSubmitted', true);
+      this.set('_content', null);
+      this.set('_category', null);
+      this.set('nextCategory', null);
+    },
+
+    firstLevelCategory() {
+      this.set('displayTextBox', false);
+      this.set('tutorialContent', null);
+      this.set('_error', null);
+      this.set('displaySecondDropdown', false);
+
+      this.set('nextCategory', this._questions[event.target.value]);
+      this.set('_category', event.target.value);
+
+      if (this.nextCategory.length > 1) {
+        this.set('displaySecondDropdown', true);
+      } else {
+        this._showFeedbackActionBasedOnCategoryType(this.nextCategory[0]);
+      }
+    },
+
+    showFeedback() {
+      this.set('_error', null);
+      this._showFeedbackActionBasedOnCategoryType(this.nextCategory[event.target.value]);
     }
   }
 });
