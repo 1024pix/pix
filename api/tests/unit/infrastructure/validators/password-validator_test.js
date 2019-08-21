7314399ab495b84707505a40acef5103241d683c
Christophe Charles <ccharles@octo.com>
2019-08-21 14:59:33 +0200

    [pf-748] Update api password format With uppercase, lowercase, digit and 8 characters

--- api/tests/unit/infrastructure/validators/password-validator_test.js
+++ api/tests/unit/infrastructure/validators/password-validator_test.js
@@ -1,23 +1,28 @@
 const { expect } = require('../../../test-helper');
-const isPasswordvalid = require('../../../../lib/infrastructure/validators/password-validator');
+const isPasswordValid = require('../../../../lib/infrastructure/validators/password-validator');
 
 describe('Unit | Validator | password-validator', function() {
 
   describe('Validation rules', function() {
 
     it('should contain at least 8 characters:', function() {
-      expect(isPasswordvalid('ABCD1234')).to.be.true;
-      expect(isPasswordvalid('A1')).to.be.false;
-    });
-
-    it('should contain at least one letter ', function() {
-      expect(isPasswordvalid('ABCD1234')).to.be.true;
-      expect(isPasswordvalid('12345678')).to.be.false;
+      expect(isPasswordValid('Ab123456')).to.be.true;
+      expect(isPasswordValid('Ab1')).to.be.false;
     });
 
     it('should contain at least one digit', function() {
-      expect(isPasswordvalid('ABCD1234')).to.be.true;
-      expect(isPasswordvalid('ABCDEFGH')).to.be.false;
+      expect(isPasswordValid('Ab123456')).to.be.true;
+      expect(isPasswordValid('AbCDEFGH')).to.be.false;
+    });
+
+    it('should contain at least one uppercase letter', function() {
+      expect(isPasswordValid('Ab123456')).to.be.true;
+      expect(isPasswordValid('ab123456')).to.be.false;
+    });
+
+    it('should contain at least one lowercase letter', function() {
+      expect(isPasswordValid('Ab123456')).to.be.true;
+      expect(isPasswordValid('AB123456')).to.be.false;
     });
 
   });
@@ -33,33 +38,35 @@ describe('Unit | Validator | password-validator', function() {
       'password',
       '12345678&',
       '+!@)-=`"#&',
-      '+!@)-=`"#&1'
+      '+!@)-=`"#&1',
+      '+!@)-=`"#&1A',
+      '+!@)-=`"#&1a'
     ].forEach(function(badPassword) {
       it(`should return false when password is invalid: ${badPassword}`, function() {
-        expect(isPasswordvalid(badPassword)).to.be.false;
+        expect(isPasswordValid(badPassword)).to.be.false;
       });
     });
   });
 
   describe('Valid password', function() {
     [
-      'PIXBETA1',
-      'PIXBETA12',
-      'NULLNULL1',
-      '12345678a',
-      '12345678ab',
-      '12345678ab+',
-      '12345678ab+!',
-      '12345678ab+!@',
-      '12345678ab+!@)-=`',
-      '12345678ab+!@)-=`"',
-      '12345678ab+!@)-=`"#&',
+      'PIXBETa1',
+      'PIXBETa12',
+      'NULLNuLL1',
+      '1234567Aa',
+      '12345678Ab',
+      '12345678Ab+',
+      '12345678Ab+!',
+      '12345678Ab+!@',
+      '12345678Ab+!@)-=`',
+      '12345678Ab+!@)-=`"',
+      '12345678Ab+!@)-=`"#&',
       '1234Password avec espace',
-      '1A      A1',
-      'à1      '
+      '1A      a1',
+      'Aà1      '
     ].forEach(function(validPassword) {
       it(`should return true if provided password is valid: ${validPassword}`, function() {
-        expect(isPasswordvalid(validPassword)).to.be.true;
+        expect(isPasswordValid(validPassword)).to.be.true;
       });
     });
   });
