a886d1d3e56af2245bb1bdbd3de3e47a6baa5ff2
LJA <ljacquemin@octo.com>
2019-08-21 15:53:39 +0200

    Replace promises by async/await in pw reset
    Async/await proves to be really a good technique to avoid having many
    short-lived variables just to store the intermediate states.

--- api/lib/application/passwords/password-controller.js
+++ api/lib/application/passwords/password-controller.js
@@ -1,6 +1,5 @@
 const settings = require('../../settings');
 
-const userService = require('../../domain/services/user-service');
 const mailService = require('../../domain/services/mail-service');
 const resetPasswordService = require('../../domain/services/reset-password-service');
 const tokenService = require('../../domain/services/token-service');
@@ -10,40 +9,25 @@ const userSerializer = require('../../infrastructure/serializers/jsonapi/user-se
 const resetPasswordDemandRepository = require('../../infrastructure/repositories/reset-password-demands-repository');
 const userRepository = require('../../infrastructure/repositories/user-repository');
 
-function _sendPasswordResetDemandUrlEmail(email, temporaryKey) {
-
-  const passwordResetDemandUrl = `http://${settings.app.domain}`;
-  return mailService
-    .sendResetPasswordDemandEmail(email, passwordResetDemandUrl, temporaryKey);
-}
-
 module.exports = {
-  createResetDemand(request, h) {
 
+  async createResetDemand(request, h) {
     const user = userSerializer.deserialize(request.payload);
+    await userRepository.isUserExistingByEmail(user.email);
+    const temporaryKey = resetPasswordService.generateTemporaryKey();
+    const passwordResetDemand = await resetPasswordDemandRepository.create({ email: user.email, temporaryKey });
+    await mailService.sendResetPasswordDemandEmail(user.email, `http://${settings.app.domain}`, temporaryKey);
+    const serializedPayload = passwordResetSerializer.serialize(passwordResetDemand.attributes);
 
-    let temporaryKey, passwordResetDemand;
-
-    return userRepository.isUserExistingByEmail(user.email)
-      .then(resetPasswordService.generateTemporaryKey)
-      .then((key) => {
-        temporaryKey = key;
-        return resetPasswordDemandRepository.create({ email: user.email, temporaryKey });
-      })
-      .then((demand) => {
-        passwordResetDemand = demand;
-        _sendPasswordResetDemandUrlEmail(user.email, temporaryKey);
-      })
-      .then(() => passwordResetSerializer.serialize(passwordResetDemand.attributes))
-      .then((serializedPayload) => h.response(serializedPayload).created());
+    return h.response(serializedPayload).created();
   },
 
-  checkResetDemand(request) {
+  async checkResetDemand(request) {
     const temporaryKey = request.params.temporaryKey;
-
-    return tokenService.verifyValidity(temporaryKey)
-      .then(() => resetPasswordService.verifyDemand(temporaryKey))
-      .then((passwordResetDemand) => userRepository.findByEmail(passwordResetDemand.email))
-      .then((user) => userSerializer.serialize(user));
+    await tokenService.verifyValidity(temporaryKey);
+    const passwordResetDemand = await resetPasswordService.verifyDemand(temporaryKey);
+    const user = await userRepository.findByEmail(passwordResetDemand.email);
+      
+    return userSerializer.serialize(user);
   }
 };
