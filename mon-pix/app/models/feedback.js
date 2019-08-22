3c578ad945bd84317b5ef0a58045022de5d38bea
Jonas Grillères <grilleres.j@gmail.com>
2019-08-22 15:36:47 +0200

    [pf-782] Add category to the ember model

--- mon-pix/app/models/feedback.js
+++ mon-pix/app/models/feedback.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   content: DS.attr('string'),
+  category: DS.attr('string'),
   assessment: DS.belongsTo('assessment'),
   challenge: DS.belongsTo('challenge')
 });
