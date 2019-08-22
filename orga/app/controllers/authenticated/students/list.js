034a997dbf98bd5e851d94da7200d309940fd9e1
lisequesnel <quesnel.lise@gmail.com>
2019-08-22 13:00:17 +0200

    [po-217] Add notifications system for students upload success or error

--- orga/app/controllers/authenticated/students/list.js
+++ orga/app/controllers/authenticated/students/list.js
@@ -6,17 +6,37 @@ export default Controller.extend({
 
   session: service(),
   currentUser: service(),
+  message: null,
 
   actions: {
     async importStudents(file) {
+      this.set('message', null);
       const { access_token } = this.get('session.data.authenticated');
 
-      await file.uploadBinary(`${ENV.APP.API_HOST}/api/organizations/${this.get('currentUser.organization.id')}/import-students`, {
-        headers: {
-          Authorization: `Bearer ${access_token}`,
+      try {
+        await file.uploadBinary(`${ENV.APP.API_HOST}/api/organizations/${this.get('currentUser.organization.id')}/import-students`, {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          }
+        });
+        await this.model.reload();
+        this.set('message', { type: 'success' });
+
+      } catch (errorResponse) {
+
+        if (!errorResponse.body.errors) {
+          return this.set('message', { type: 'error' });
         }
-      });
-      await this.model.reload();
+
+        errorResponse.body.errors.forEach((error) => {
+          if (error.status === '409') {
+            return this.set('message', { type: 'warning', detail: error.detail });
+          } else if (error.status === '422') {
+            return this.set('message', { type: 'error', detail: error.detail });
+          }
+          return this.set('message', { type: 'error' });
+        });
+      }
     }
   },
 });
