cb47760cc918f0e540141474c8fa5ee93bb67ee4
lisequesnel <quesnel.lise@gmail.com>
2019-08-23 12:59:42 +0200

    [pf-719] Use prehandler in PATCH /users/id/remember...instructions

--- api/tests/unit/domain/usecases/remember-user-has-seen-assessment-instructions_test.js
+++ api/tests/unit/domain/usecases/remember-user-has-seen-assessment-instructions_test.js
@@ -1,7 +1,6 @@
-const { expect, sinon, catchErr } = require('../../../test-helper');
+const { expect, sinon } = require('../../../test-helper');
 const rememberUserHasSeenAssessmentInstructions = require('../../../../lib/domain/usecases/remember-user-has-seen-assessment-instructions');
 const userRepository = require('../../../../lib/infrastructure/repositories/user-repository');
-const { UserNotAuthorizedToAccessEntity } = require('../../../../lib/domain/errors');
 
 describe('Unit | UseCase | remember-user-has-seen-assessment-instructions', () => {
 
@@ -12,28 +11,14 @@ describe('Unit | UseCase | remember-user-has-seen-assessment-instructions', () =
 
   it('should update user hasSeenAssessmentInstructions', async () => {
     // given
-    const authenticatedUserId = 1;
-    const requestedUserId = 1;
-    userRepository.get.withArgs(requestedUserId).resolves({ id: 1 });
+    const userId = 1;
+    userRepository.get.withArgs(userId).resolves({ id: 1 });
     userRepository.updateUser.withArgs({ id: 1, hasSeenAssessmentInstructions: true }).resolves('ok');
 
     // when
-    const result = await rememberUserHasSeenAssessmentInstructions({ authenticatedUserId, requestedUserId, userRepository });
+    const result = await rememberUserHasSeenAssessmentInstructions({ userId, userRepository });
 
     // then
     expect(result).to.be.equal('ok');
   });
-
-  it('should throw an error if authenticated user is different from requested user', async () => {
-    // given
-    const authenticatedUserId = 1;
-    const requestedUserId = 2;
-
-    // when
-    const result = await catchErr(rememberUserHasSeenAssessmentInstructions)({ authenticatedUserId, requestedUserId, userRepository });
-
-    // then
-    expect(result).to.be.instanceOf(UserNotAuthorizedToAccessEntity);
-  });
-
 });
