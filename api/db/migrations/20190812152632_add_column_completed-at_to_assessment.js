80b4c4825a1e8d114283ba170ebec6324931c55d
Melanie B <mboudard@octo.com>
2019-08-23 14:21:32 +0200

    [pf-750] Modify the migration to update previous assessment

--- api/db/migrations/20190812152632_add_column_completed-at_to_assessment.js
+++ api/db/migrations/20190812152632_add_column_completed-at_to_assessment.js
@@ -1,14 +1,41 @@
-const TABLE_NAME = 'assessments';
+const { batch } = require('../batchTreatment');
+
+const TABLE_ASSESSMENTS_NAME = 'assessments';
 const COLUMN_NAME = 'completedAt';
 
-exports.up = function(knex) {
-  return knex.schema.table(TABLE_NAME, (table) => {
+exports.up = async function(knex) {
+
+  await knex.schema.table(TABLE_ASSESSMENTS_NAME, (table) => {
     table.dateTime(COLUMN_NAME).defaultTo(null);
+  });
+
+  const assessmentsCompleted = await knex(TABLE_ASSESSMENTS_NAME)
+    .select('id', 'state', 'completedAt', 'updatedAt')
+    .where('state', '=', 'completed')
+    .andWhere('completedAt', null);
+
+  return batch(knex, assessmentsCompleted, async (assessment) => {
+    const lastAnswers = await knex('answers')
+      .select('createdAt')
+      .where('assessmentId', '=', assessment.id)
+      .orderBy('createdAt', 'desc')
+      .limit(1);
+    console.log(lastAnswers[0]);
+    console.log(assessment);
+    if(lastAnswers[0]) {
+      return knex(TABLE_ASSESSMENTS_NAME)
+        .where('id', '=', assessment.id)
+        .update('completedAt', lastAnswers[0].createdAt);
+    } else {
+      return knex(TABLE_ASSESSMENTS_NAME)
+        .where('id', '=', assessment.id)
+        .update('completedAt',assessment.updatedAt);
+    }
   });
 };
 
 exports.down = function(knex) {
-  return knex.schema.table(TABLE_NAME, (table) => {
+  return knex.schema.table(TABLE_ASSESSMENTS_NAME, (table) => {
     table.dropColumn(COLUMN_NAME);
   });
 };
