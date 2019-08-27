377cf9d53e25ceac0bf9ef09c7dcee29ea8fc442
LJA <ljacquemin@octo.com>
2019-08-27 13:18:38 +0200

    [pf-881] Use async/await in certification-service
    This is one of the many commits that progressively replace promise-based
    code in favor of async/await when it is more appropriate.

--- api/lib/domain/services/certification-service.js
+++ api/lib/domain/services/certification-service.js
@@ -31,54 +31,42 @@ module.exports = {
       .then((assessment) => certificationResultService.getCertificationResult(assessment, continueOnError));
   },
 
-  getCertificationResult(certificationCourseId) {
-    let assessment = {};
-    let certification = {};
-    return assessmentRepository
-      .getByCertificationCourseId(certificationCourseId)
-      .then((foundAssessment) => {
-        assessment = foundAssessment;
-        return certificationCourseRepository.get(certificationCourseId);
-      })
-      .then((foundCertification) => {
-        certification = foundCertification;
+  async getCertificationResult(certificationCourseId) {
+    const assessment = await assessmentRepository.getByCertificationCourseId(certificationCourseId);
+    const certification = await certificationCourseRepository.get(certificationCourseId);
 
-        if (assessment) {
-          const lastAssessmentResult = assessment.getLastAssessmentResult();
+    let lastAssessmentResultFull = { competenceMarks: [], status: assessment ? assessment.state : 'missing-assessment' };
 
-          if (lastAssessmentResult) {
-            return assessmentResultRepository.get(lastAssessmentResult.id);
-          }
-        }
+    const lastAssessmentResult = assessment && assessment.getLastAssessmentResult();
+    if (lastAssessmentResult) {
+      lastAssessmentResultFull = await assessmentResultRepository.get(lastAssessmentResult.id);
+    }
 
-        return { competenceMarks: [], status: assessment ? assessment.state : 'missing-assessment' };
-      })
-      .then((lastAssessmentResultFull) => {
-        return {
-          level: lastAssessmentResultFull.level,
-          certificationId: certification.id,
-          assessmentId: assessment ? assessment.id : null,
-          emitter: lastAssessmentResultFull.emitter,
-          commentForJury: lastAssessmentResultFull.commentForJury,
-          commentForCandidate: lastAssessmentResultFull.commentForCandidate,
-          commentForOrganization: lastAssessmentResultFull.commentForOrganization,
-          status: lastAssessmentResultFull.status,
-          pixScore: lastAssessmentResultFull.pixScore,
-          createdAt: certification.createdAt,
-          juryId: lastAssessmentResultFull.juryId,
-          resultCreatedAt: lastAssessmentResultFull.createdAt,
-          completedAt: certification.completedAt,
-          competencesWithMark: lastAssessmentResultFull.competenceMarks,
-          firstName: certification.firstName,
-          lastName: certification.lastName,
-          birthdate: certification.birthdate,
-          birthplace: certification.birthplace,
-          sessionId: certification.sessionId,
-          externalId: certification.externalId,
-          isPublished: certification.isPublished,
-          isV2Certification: certification.isV2Certification,
-        };
-      });
+    return {
+      level: lastAssessmentResultFull.level,
+      certificationId: certification.id,
+      assessmentId: assessment ? assessment.id : null,
+      emitter: lastAssessmentResultFull.emitter,
+      commentForJury: lastAssessmentResultFull.commentForJury,
+      commentForCandidate: lastAssessmentResultFull.commentForCandidate,
+      commentForOrganization: lastAssessmentResultFull.commentForOrganization,
+      status: lastAssessmentResultFull.status,
+      pixScore: lastAssessmentResultFull.pixScore,
+      createdAt: certification.createdAt,
+      juryId: lastAssessmentResultFull.juryId,
+      resultCreatedAt: lastAssessmentResultFull.createdAt,
+      completedAt: certification.completedAt,
+      competencesWithMark: lastAssessmentResultFull.competenceMarks,
+      firstName: certification.firstName,
+      lastName: certification.lastName,
+      birthdate: certification.birthdate,
+      birthplace: certification.birthplace,
+      sessionId: certification.sessionId,
+      externalId: certification.externalId,
+      isPublished: certification.isPublished,
+      isV2Certification: certification.isV2Certification,
+    };
+
   },
 
   _computeAnswersSuccessRate,
