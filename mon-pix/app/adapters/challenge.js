5fe94549c765ead5d20f545dd9b25887a1438884
Melanie B <mboudard@octo.com>
2019-08-30 11:55:49 +0200

    [pf-750] Modify challenge Adapater to user urlForQueryRecord and improving
    - To simplify the call of getNextChallenge, and to simplify test, the adapter is refacto to user urlForQueryRecord instead of queryRecord
    - If the query has the option TryIfCanImprove, we add it to the url

--- mon-pix/app/adapters/challenge.js
+++ mon-pix/app/adapters/challenge.js
@@ -2,9 +2,14 @@ import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
 
-  queryRecord(store, type, query) {
-    const url = `${this.host}/${this.namespace}/assessments/${query.assessmentId}/next`;
-    return this.ajax(url, 'GET');
+  urlForQueryRecord(query) {
+    let url;
+    if (query.tryIfCanImprove) {
+      url = `${this.host}/${this.namespace}/assessments/${query.assessmentId}/next?tryImproving=true`;
+    } else {
+      url = `${this.host}/${this.namespace}/assessments/${query.assessmentId}/next`;
+    }
+    return url;
   }
 
 });
