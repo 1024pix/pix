43e88132bb7acf394a1ab468110da5e726af5fae
Melanie B <mboudard@octo.com>
2019-08-30 11:57:03 +0200

    [pf-750] Verify in page skill-review if the user can try to improve
    - In afterModel, we try to get a next challenge with the improving option
    - We do not show the button if there is no challenge

--- mon-pix/tests/acceptance/skill-review-page-test.js
+++ mon-pix/tests/acceptance/skill-review-page-test.js
@@ -62,6 +62,16 @@ describe('Acceptance | Campaigns | Campaigns Result', function() {
         expect(find('table tbody tr td:nth-child(2) .progression-gauge__tooltip').textContent).to.include('30%');
       });
 
+      it('should display the button to retry', async function() {
+        // when
+        await resumeCampaignByCode('AZERTY2');
+        await completeCampaignAndSeeResultsByCode('AZERTY2');
+        await click('.skill-review__share-button');
+
+        // then
+        expect(find('.skill-review__improvement-button')).to.exists;
+      });
+
       it('should share the results', async function() {
         // when
         await resumeCampaignByCode('AZERTY2');
@@ -72,6 +82,7 @@ describe('Acceptance | Campaigns | Campaigns Result', function() {
         expect(find('.skill-review-share__thanks')).to.exists;
         expect(find('.skill-review-share__back-to-home')).to.exists;
         expect(find('.skill-review-share__legal')).to.not.exists;
+        expect(find('.skill-review__improvement-button')).to.not.exists;
       });
 
       it('should redirect to home/profil page on click', async function() {
