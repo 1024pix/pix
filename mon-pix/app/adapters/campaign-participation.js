7505b24caca7935b442e46b8485e0f05c1226fc7
Melanie B <mboudard@octo.com>
2019-09-02 16:21:23 +0200

    [pf-750] Rename start-improvement to begin-improvement

--- mon-pix/app/adapters/campaign-participation.js
+++ mon-pix/app/adapters/campaign-participation.js
@@ -5,9 +5,9 @@ export default ApplicationAdapter.extend({
   urlForUpdateRecord(id, modelName, { adapterOptions }) {
     const url = this._super(...arguments);
 
-    if (adapterOptions && adapterOptions.startImprovement) {
-      delete adapterOptions.startImprovement;
-      return url + '/start-improvement';
+    if (adapterOptions && adapterOptions.beginImprovement) {
+      delete adapterOptions.beginImprovement;
+      return url + '/begin-improvement';
     }
     return url;
   },
