3067f80e3d6710313f3c7eaa607ba73151a6eff5
Christophe Charles <ccharles@octo.com>
2019-09-11 10:33:26 +0200

    [po-238] Create organization-invitation model

---
+++ api/lib/domain/models/OrganizationInvitation.js
@@ -0,0 +1,37 @@
+const statuses = {
+  PENDING: 'pending',
+  ACCEPTED: 'accepted',
+};
+
+class OrganizationInvitation {
+
+  constructor({
+    id,
+    // attributes
+    organizationId,
+    email,
+    status,
+    createdAt,
+    updatedAt,
+  } = {}) {
+    this.id = id;
+    // attributes
+    this.organizationId = organizationId;
+    this.email = email;
+    this.status = status;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+
+  get isPending() {
+    return this.status === statuses.PENDING;
+  }
+
+  get isAccepted() {
+    return this.status === statuses.ACCEPTED;
+  }
+}
+
+OrganizationInvitation.StatusType = statuses;
+
+module.exports = OrganizationInvitation;
