92853a12e98352340c95972e8a0defe4f2aeba56
laura-bergoens <laura.bergoens@gmail.com>
2019-09-17 11:20:03 +0200

    [pc-61] Setup ember notifications
    Set nicer notifications to user after uploading the attendance
    sheet to import candidates.

--- certif/app/controllers/authenticated/sessions/details.js
+++ certif/app/controllers/authenticated/sessions/details.js
@@ -1,23 +1,35 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 
+import config from '../../../config/environment';
+
 export default Controller.extend({
   session: service(),
-  message: null,
+  notifications: service('notification-messages'),
 
   actions: {
     async uploadCertificationCandidates(file) {
-      this.set('message', null);
       const { access_token } = this.get('session.data.authenticated');
+      this.get('notifications').clearAll();
+
+      const autoClear = config.notifications.autoClear;
+      const clearDuration = config.notifications.clearDuration;
+
       try {
         await file.upload(this.model.urlToUpload, {
           headers: { Authorization: `Bearer ${access_token}` },
         });
         this.get('model').reload();
-        return this.set('message', { type: 'success' });
+        this.get('notifications').success('La liste des candidats a été importée avec succès', {
+          autoClear,
+          clearDuration,
+        });
       }
-      catch (error) {
-        return this.set('message', { type: 'error', detail: error.detail });
+      catch (err) {
+        this.get('notifications').error('Une erreur s\'est produite lors de l\'import des candidats', {
+          autoClear,
+          clearDuration,
+        });
       }
     },
   }
--- certif/app/routes/authenticated/sessions/details.js
+++ certif/app/routes/authenticated/sessions/details.js
@@ -2,7 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(params) {
-    this.set('message', null);
     return this.store.findRecord('session', params.session_id);
   },
 });
