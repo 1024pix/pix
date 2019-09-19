b96328ce9150fcca7281237a5278196f5c88d592
Christophe Charles <ccharles@octo.com>
2019-09-19 10:52:37 +0200

    [po-238] Add controller to create organization-invitation

--- api/tests/unit/application/organizations/index_test.js
+++ api/tests/unit/application/organizations/index_test.js
@@ -1,5 +1,6 @@
 const { expect, sinon } = require('../../../test-helper');
 const Hapi = require('hapi');
+const securityController = require('../../../../lib/interfaces/controllers/security-controller');
 const organizationController = require('../../../../lib/application/organizations/organization-controller');
 
 let server;
@@ -10,12 +11,16 @@ function startServer() {
 }
 
 describe('Unit | Router | organization-router', () => {
-  describe('GET /api/organizations', () => {
 
-    beforeEach(() => {
-      sinon.stub(organizationController, 'find').returns('ok');
-      startServer();
-    });
+  beforeEach(() => {
+    sinon.stub(securityController, 'checkUserIsOwnerInOrganization').returns(true);
+    sinon.stub(organizationController, 'find').returns('ok');
+    sinon.stub(organizationController, 'sendInvitation').callsFake((request, h) => h.response().created());
+
+    startServer();
+  });
+
+  describe('GET /api/organizations', () => {
 
     it('should exist', () => {
       // given
@@ -32,7 +37,23 @@ describe('Unit | Router | organization-router', () => {
         expect(response.statusCode).to.equal(200);
       });
     });
+  });
 
+  describe('POST /api/organizations/{id}/invitations', () => {
+
+    it('should exist', async () => {
+      // given
+      const options = {
+        method: 'POST',
+        url: '/api/organizations/1/invitations'
+      };
+
+      // when
+      const response = await server.inject(options);
+
+      // then
+      expect(response.statusCode).to.equal(201);
+    });
   });
 
 });
