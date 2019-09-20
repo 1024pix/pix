0b5847431d234c3dbfde05aae281302a38d82934
Jonas Grillères <grilleres.j@gmail.com>
2019-09-20 14:04:28 +0200

    Adjust tests for common cases

--- mon-pix/tests/acceptance/challenge-commons-test.js
+++ mon-pix/tests/acceptance/challenge-commons-test.js
@@ -1,4 +1,4 @@
-import { click, currentURL, find } from '@ember/test-helpers';
+import { click, currentURL, find, findAll } from '@ember/test-helpers';
 import { beforeEach, describe, it } from 'mocha';
 import { expect } from 'chai';
 import { authenticateAsSimpleUser } from '../helpers/testing';
@@ -11,51 +11,80 @@ describe('Acceptance | Common behavior to all challenges', function() {
   setupApplicationTest();
   setupMirage();
 
-  beforeEach(async function() {
-    defaultScenario(this.server);
-    await authenticateAsSimpleUser();
-    await visitWithAbortedTransition('/assessments/ref_assessment_id/challenges/ref_qrocm_challenge_id');
+  context('Challenge asked for the first time', function() {
+    beforeEach(async function() {
+      defaultScenario(this.server);
+      await authenticateAsSimpleUser();
+      await visitWithAbortedTransition('/assessments/ref_assessment_id/challenges/ref_qcm_challenge_id_not_yet_answered');
+    });
+
+    it('should display the name of the test', function() {
+      expect(find('.course-banner__name').textContent).to.contain('First Course');
+    });
+
+    it('should display the challenge instruction', function() {
+      const instructionText = 'Un QCM propose plusieurs choix, l\'utilisateur peut en choisir plusieurs';
+      expect(find('.challenge-statement__instruction').textContent.trim()).to.equal(instructionText);
+    });
+
+    it('should format content written as [foo](bar) as clickable link', function() {
+      expect(find('.challenge-statement__instruction a')).to.exist;
+      expect(find('.challenge-statement__instruction a').textContent).to.equal('plusieurs');
+      expect(find('.challenge-statement__instruction a').getAttribute('href')).to.equal('http://link.plusieurs.url');
+    });
+
+    it('should open links in a new tab', function() {
+      expect(find('.challenge-statement__instruction a').getAttribute('target')).to.equal('_blank');
+    });
+
+    it('should display the skip button', function() {
+      expect(find('.challenge-actions__action-skip')).to.exist;
+    });
+
+    it('should display the validate button', function() {
+      expect(find('.challenge-actions__action-skip')).to.exist;
+    });
+
+    it('should display a button to come back to the courses list', function() {
+      expect(find('.course-banner__home-link').textContent).to.equal('Revenir à l\'accueil');
+    });
+
+    it('should come back to the home route when the back button is clicked', async function() {
+      // when
+      await click('.course-banner__home-link');
+
+      expect(currentURL()).to.equal('/profil');
+    });
+
+    it('should be able to send a feedback about the current challenge', () => {
+      expect(find('.feedback-panel')).to.exist;
+    });
+
+    it('should hide the error message after the user has clicked a checkbox', async function() {
+      await click('.challenge-actions__action-validate');
+
+      // when
+      await click(findAll('.proposal-text')[1]);
+
+      // then
+      expect(find('.alert')).to.not.exist;
+    });
   });
 
-  it('should display the name of the test', function() {
-    expect(find('.course-banner__name').textContent).to.contain('First Course');
-  });
+  context('Challenge already asked (hitting back button)', function() {
+    beforeEach(async function() {
+      defaultScenario(this.server);
+      await authenticateAsSimpleUser();
+      await visitWithAbortedTransition('/assessments/ref_assessment_id/challenges/ref_qcm_challenge_id');
+      await click('.challenge-item-warning button');
+    });
 
-  it('should display the challenge instruction', function() {
-    const instructionText = 'Un QROCM est une question ouverte avec plusieurs champs texte libre pour repondre';
-    expect(find('.challenge-statement__instruction').textContent.trim()).to.equal(instructionText);
-  });
+    it('should show the continue button', function() {
+      expect(expect(find('.challenge-actions__action-continue')).to.exist);
+    });
 
-  it('should format content written as [foo](bar) as clickable link', function() {
-    expect(find('.challenge-statement__instruction a')).to.exist;
-    expect(find('.challenge-statement__instruction a').textContent).to.equal('ouverte');
-    expect(find('.challenge-statement__instruction a').getAttribute('href')).to.equal('http://link.ouverte.url');
-  });
-
-  it('should open links in a new tab', function() {
-    expect(find('.challenge-statement__instruction a').getAttribute('target')).to.equal('_blank');
-  });
-
-  it('should display the skip button', function() {
-    expect(find('.challenge-actions__action-skip')).to.exist;
-  });
-
-  it('should display the validate button', function() {
-    expect(find('.challenge-actions__action-skip')).to.exist;
-  });
-
-  it('should display a button to come back to the courses list', function() {
-    expect(find('.course-banner__home-link').textContent).to.equal('Revenir à l\'accueil');
-  });
-
-  it('should come back to the home route when the back button is clicked', async function() {
-    // when
-    await click('.course-banner__home-link');
-
-    expect(currentURL()).to.equal('/profil');
-  });
-
-  it('should be able to send a feedback about the current challenge', () => {
-    expect(find('.feedback-panel')).to.exist;
+    it('should show the already answered warning text', function() {
+      expect(expect(find('.challenge-actions__already-answered')).to.exist);
+    });
   });
 });
