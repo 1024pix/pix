86080711e905f77f74935802a7b04f92e7dc9aa7
Stephane Bedeau <sbedeau@octo.com>
2019-09-23 19:13:09 +0200

    Fix next assessment call

--- mon-pix/app/adapters/challenge.js
+++ mon-pix/app/adapters/challenge.js
@@ -4,10 +4,17 @@ export default ApplicationAdapter.extend({
 
   urlForQueryRecord(query) {
     let url = `${this.host}/${this.namespace}/assessments/${query.assessmentId}/next`;
+
+    if (query.assessmentId) {
+      delete query.assessmentId;
+    }
+
     if (query.tryIfCanImprove) {
+      delete query.tryIfCanImprove;
       url = url + '?tryImproving=true';
     }
+
     return url;
-  }
+  },
 
 });
