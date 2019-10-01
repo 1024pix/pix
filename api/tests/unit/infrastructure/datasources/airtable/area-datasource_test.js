d0f17f086c08cdbd72d092067190a76f4a31059e
Jonathan Perret <j-github@jonathanperret.net>
2019-10-01 09:49:58 +0200

    Reduce Area, Challenge, Tutorial datasource to necessary fields

--- api/tests/unit/infrastructure/datasources/airtable/area-datasource_test.js
+++ api/tests/unit/infrastructure/datasources/airtable/area-datasource_test.js
@@ -2,7 +2,7 @@ const { expect, sinon } = require('../../../../test-helper');
 const airtable = require('../../../../../lib/infrastructure/airtable');
 const areaDatasource = require('../../../../../lib/infrastructure/datasources/airtable/area-datasource');
 const areaRawAirTableFixture = require('../../../../tooling/fixtures/infrastructure/areaRawAirTableFixture');
-const airTableDataObjects = require('../../../../../lib/infrastructure/datasources/airtable/objects');
+const { Area } = require('../../../../../lib/infrastructure/datasources/airtable/objects');
 
 describe('Unit | Infrastructure | Datasource | Airtable | AreaDatasource', () => {
 
@@ -12,15 +12,17 @@ describe('Unit | Infrastructure | Datasource | Airtable | AreaDatasource', () =>
       // given
       sinon.stub(airtable, 'findRecords').resolves([areaRawAirTableFixture()]);
 
+      sinon.stub(Area, 'getUsedAirtableFields').returns(['field1', 'field2']);
+
       // when
       const promise = areaDatasource.list();
 
       // then
       return promise.then((areas) => {
-        expect(airtable.findRecords).to.have.been.calledWith('Domaines');
+        expect(airtable.findRecords).to.have.been.calledWith('Domaines', ['field1', 'field2']);
 
         expect(areas).to.have.lengthOf(1);
-        expect(areas[0]).to.be.an.instanceof(airTableDataObjects.Area);
+        expect(areas[0]).to.be.an.instanceof(Area);
         expect(areas[0].id).to.equal('recvoGdo7z2z7pXWa');
       });
     });
