4ce4e9017d763aef00dbd8b233796da92346e5b3
Jonathan Perret <j-github@jonathanperret.net>
2019-10-01 18:23:43 +0200

    Move AirtableResourceNotFound outside of aurtable/objects
    This lets us iterate over all classes in airtable/objects.

--- api/lib/infrastructure/repositories/challenge-repository.js
+++ api/lib/infrastructure/repositories/challenge-repository.js
@@ -6,7 +6,7 @@ const Skill = require('../../domain/models/Skill');
 const challengeDatasource = require('../datasources/airtable/challenge-datasource');
 const skillDatasource = require('../datasources/airtable/skill-datasource');
 const solutionAdapter = require('../adapters/solution-adapter');
-const airtableDatasourceObjects = require('../datasources/airtable/objects');
+const AirtableResourceNotFound = require('../datasources/airtable/AirtableResourceNotFound');
 const { NotFoundError } = require('../../domain/errors');
 
 module.exports = {
@@ -16,7 +16,7 @@ module.exports = {
     return challengeDatasource.get(id)
       .then(_generateChallengeDomainModel)
       .catch((error) => {
-        if (error instanceof airtableDatasourceObjects.AirtableResourceNotFound) {
+        if (error instanceof AirtableResourceNotFound) {
           throw new NotFoundError();
         }
         throw error;
