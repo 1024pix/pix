1da2461cfbde6d590d192a28da99d48a35f7d685
laura-bergoens <laura.bergoens@gmail.com>
2019-10-02 15:58:49 +0200

    Make sure tables are empty when launching tests
    This commit moves a target profile and its skills from a migration
    file (where they don't belong) to a seed file.
    It also uncomment an emptying database test that used to be
    commented.
    To finish, a before hook has been added to make sure all tables
    are empty when launching tests.

--- api/db/migrations/20180731102046_create_target_profiles_table.js
+++ api/db/migrations/20180731102046_create_target_profiles_table.js
@@ -2,40 +2,6 @@ const TABLE_NAME_TARGET_PROFILES = 'target-profiles';
 const TABLE_NAME_TARGET_PROFILES_SKILLS = 'target-profiles_skills';
 const TABLE_NAME_CAMPAIGNS = 'campaigns';
 
-const targetProfile = {
-  name: 'PIC - Diagnostic Initial',
-  isPublic: true,
-  organizationId: null
-};
-
-const associatedSkills = [
-  { skillId: 'rectL2ZZeWPc7yezp' },
-  { skillId: 'recndXqXiv4pv2Ukp' },
-  { skillId: 'recMOy4S8XnaWblYI' },
-  { skillId: 'recagUd44RPEWti0X' },
-  { skillId: 'recrvTvLTUXEcUIV1' },
-  { skillId: 'recX7RyCsdNV2p168' },
-  { skillId: 'recxtb5aLs6OAAKIg' },
-  { skillId: 'receRbbt9Lb661wFB' },
-  { skillId: 'rec71e3PSct2zLEMj' },
-  { skillId: 'recFwJlpllhWzuLom' },
-  { skillId: 'rec0J9OXaAj5v7w3r' },
-  { skillId: 'reclY3njuk6EySJuU' },
-  { skillId: 'rec5V9gp65a58nnco' },
-  { skillId: 'recPrXhP0X07OdHXe' },
-  { skillId: 'recPG9ftlGZLiF0O6' },
-  { skillId: 'rectLj7NPg5JcSIqN' },
-  { skillId: 'rec9qal2FLjWysrfu' },
-  { skillId: 'rechRPFlSryfY3UnG' },
-  { skillId: 'recL0AotZshb9quhR' },
-  { skillId: 'recrOwaV2PTt1N0i5' },
-  { skillId: 'recpdpemRXuzV9r10' },
-  { skillId: 'recWXtN5cNP1JQUVx' },
-  { skillId: 'recTIddrkopID28Ep' },
-  { skillId: 'recBrDIfDDW2IPpZV' },
-  { skillId: 'recgOc2OreHCosoRp' },
-];
-
 exports.up = function(knex) {
   return knex.schema
     .createTable(TABLE_NAME_TARGET_PROFILES, (t) => {
@@ -58,22 +24,6 @@ exports.up = function(knex) {
     })
     .then(() => {
       console.log(`${TABLE_NAME_TARGET_PROFILES_SKILLS} table is created!`);
-    })
-    .then(() => {
-      return knex(TABLE_NAME_TARGET_PROFILES)
-        .insert(targetProfile)
-        .returning('id');
-    })
-    .then((insertedTargetProfileId) => {
-      const associatedSkillsWithTargetProfileId = associatedSkills.map((associatedSkill) => {
-        associatedSkill.targetProfileId = insertedTargetProfileId[0];
-        return associatedSkill;
-      });
-      return associatedSkillsWithTargetProfileId;
-    })
-    .then((associatedSkillsWithTargetProfileId) => {
-      return knex(TABLE_NAME_TARGET_PROFILES_SKILLS)
-        .insert(associatedSkillsWithTargetProfileId);
     })
     .then(() => {
       return knex.schema.table(TABLE_NAME_CAMPAIGNS, function(table) {
