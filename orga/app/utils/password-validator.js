0fae27a8cd6eb036b4a6faef27678b00114cd246
lisequesnel <quesnel.lise@gmail.com>
2019-10-04 18:55:12 +0200

    [po-19] Add validators for email and password (same as pix app)

---
+++ orga/app/utils/password-validator.js
@@ -0,0 +1,9 @@
+import XRegExp from 'xregexp';
+
+export default function isPasswordValid(password) {
+  if (!password) {
+    return false;
+  }
+  const pattern = XRegExp('^(?=.*\\p{Lu})(?=.*\\p{Ll})(?=.*\\d).{8,}$');
+  return pattern.test(password);
+}
