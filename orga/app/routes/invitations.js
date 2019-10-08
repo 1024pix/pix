478f34d74924a7cbf9fef2e24427dfff6ec0b0ae
Christophe Charles <ccharles@octo.com>
2019-10-08 10:10:17 +0200

    [po-244] Create route /invitations

---
+++ orga/app/routes/invitations.js
@@ -0,0 +1,25 @@
+import { inject as service } from '@ember/service';
+import Route from '@ember/routing/route';
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
+
+export default Route.extend(UnauthenticatedRouteMixin, {
+
+  session: service(),
+
+  queryParams: {
+    temporaryKey: {
+      refreshModel: true
+    }
+  },
+
+  model(params) {
+    const organizationInvitationId = params.organization_invitation_id;
+    const status = 'accepted';
+
+    return this.store.createRecord('organization-invitation-response', {
+      temporaryKey: params.temporaryKey,
+      status
+    }).save({ adapterOptions: { organizationInvitationId } });
+  },
+
+});
