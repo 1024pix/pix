06c818d601954ef08e17bb8e5307fa2a1e849a99
lisequesnel <quesnel.lise@gmail.com>
2019-10-10 18:16:09 +0200

    [po-19] Display only a form at a time (login or register form)

--- orga/app/components/routes/login-or-register.js
+++ orga/app/components/routes/login-or-register.js
@@ -1,4 +1,12 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+
+  displayRegisterForm: true,
+
+  actions: {
+    toggleFormsVisibility() {
+      this.toggleProperty('displayRegisterForm');
+    },
+  }
 });
--- orga/tests/integration/components/routes/login-or-register-test.js
+++ orga/tests/integration/components/routes/login-or-register-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | routes/login-or-register', function(hooks) {
@@ -10,7 +10,38 @@ module('Integration | Component | routes/login-or-register', function(hooks) {
     // when
     await render(hbs`{{routes/login-or-register}}`);
 
-    //then
+    // then
     assert.dom('.login-or-register').exists();
+  });
+
+  test('it toggle the register form by default', async function(assert) {
+    // when
+    await render(hbs`{{routes/login-or-register}}`);
+
+    // then
+    assert.dom('.register-form').exists();
+  });
+
+  test('it toggle the login form on click on login button', async function(assert) {
+    // given
+    await render(hbs`{{routes/login-or-register}}`);
+
+    // when
+    await click('#login');
+
+    // then
+    assert.dom('.login-form').exists();
+  });
+
+  test('it toggle the register form on click on register button', async function(assert) {
+    // given
+    await render(hbs`{{routes/login-or-register}}`);
+
+    // when
+    await click('#login');
+    await click('#register');
+
+    // then
+    assert.dom('.register-form').exists();
   });
 });
