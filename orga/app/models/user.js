18297b14afa1f0bd0e8d1466791d7c8e58ebd907
lisequesnel <quesnel.lise@gmail.com>
2019-10-10 13:42:58 +0200

    [po-19] Create register-form component

--- orga/app/models/user.js
+++ orga/app/models/user.js
@@ -5,6 +5,8 @@ export default DS.Model.extend({
   email: DS.attr('string'),
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
+  password: DS.attr('string'),
+  cgu: DS.attr('boolean'),
   pixOrgaTermsOfServiceAccepted: DS.attr('boolean'),
   memberships: DS.hasMany('membership')
 });
