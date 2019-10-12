9d69948a54321611bcc4aa9ffbb9cf8953fe53eb
laura-bergoens <laura.bergoens@gmail.com>
2019-10-12 13:52:42 +0200

    Refacto around certifiability computing
    UserCompetence has method isCertifiable
    CertificationProfile has method isCertifiable, that runs
    on all its userCompetences their method isCertifiable
    while ensuring business rule

--- api/lib/domain/models/CertificationProfile.js
+++ api/lib/domain/models/CertificationProfile.js
@@ -1,3 +1,8 @@
+const {
+  MINIMUM_CERTIFIABLE_COMPETENCES_FOR_CERTIFIABILITY,
+} = require('../constants');
+const _ = require('lodash');
+
 class CertificationProfile {
   constructor(
     {
@@ -12,6 +17,14 @@ class CertificationProfile {
     this.userId = userId;
     this.userCompetences = userCompetences;
     this.challengeIdsCorrectlyAnswered = challengeIdsCorrectlyAnswered;
+  }
+
+  isCertifiable() {
+    const certifiableCompetences = _(this.userCompetences)
+      .filter((userCompetence) => userCompetence.isCertifiable())
+      .size();
+
+    return certifiableCompetences >= MINIMUM_CERTIFIABLE_COMPETENCES_FOR_CERTIFIABILITY;
   }
 }
 
