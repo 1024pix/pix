4bc1b40a2e0a673bd29f68081259f7e3c0c27fe0
Jonathan Perret <j-github@jonathanperret.net>
2019-10-15 11:31:22 +0200

    Certification: compute answer number from answer collection

--- mon-pix/app/components/certification-progress.js
+++ mon-pix/app/components/certification-progress.js
@@ -1,8 +1,26 @@
 import { computed } from '@ember/object';
+import { mapBy } from '@ember/object/computed';
 import Component from '@ember/component';
 
 export default Component.extend({
-  currentAnswerNumber: computed('assessment.answers.length', function() {
-    return this.get('assessment.answers.length') + 1;
+  challenges: mapBy('assessment.answers', 'challenge'),
+
+  challengeIds: mapBy('challenges', 'id'),
+
+  currentAnswerNumber: computed('challengeId', 'challengeIds', function() {
+    const challengeIds = this.get('challengeIds');
+    const currentChallengeId = this.get('challengeId');
+
+    const foundIndex = challengeIds.indexOf(currentChallengeId);
+
+    if (foundIndex >= 0) {
+      return foundIndex + 1;
+    }
+
+    if (challengeIds.indexOf(undefined) >= 0) {
+      return '';
+    }
+
+    return challengeIds.length + 1;
   })
 });
--- mon-pix/tests/unit/components/certification-progress-test.js
+++ mon-pix/tests/unit/components/certification-progress-test.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupTest } from 'ember-mocha';
+import { A } from '@ember/array';
 import EmberObject from '@ember/object';
 
 describe('Unit | Component | certification-progress', function() {
@@ -8,20 +9,54 @@ describe('Unit | Component | certification-progress', function() {
   setupTest();
 
   describe('@currentAnswerNumber', () => {
+    let component, firstAnswer, secondAnswer, assessment;
 
-    it('should return the current answer number', function() {
+    beforeEach(function() {
       // given
-      const component = this.owner.lookup('component:certification-progress');
-      const assessment = EmberObject.create({
-        answers: [{}, {}],
+      component = this.owner.lookup('component:certification-progress');
+      firstAnswer = EmberObject.create({ id: 1, challenge: EmberObject.create({ id: 'recChallenge1' }) }),
+      secondAnswer = EmberObject.create({ id: 2, challenge: EmberObject.create({ id: undefined }) }),
+      assessment = EmberObject.create({
+        answers: A([firstAnswer, secondAnswer])
       });
       component.set('assessment', assessment);
 
+      // pre-compute property to check that dependencies cause recomputation
+      component.get('currentAnswerNumber');
+    });
+
+    it('when challenge was answered, should return the matching answer number', function() {
       // when
-      const currentAnswerNumber = component.get('currentAnswerNumber');
+      component.set('challengeId', 'recChallenge1');
 
       // then
-      expect(currentAnswerNumber).to.equal(3);
+      expect(component.get('currentAnswerNumber')).to.equal(1);
+    });
+
+    it('when challenge is new but not all answers are loaded, should return an empty string', function() {
+      // when
+      component.set('challengeId', 'recChallengeNew');
+
+      // then
+      expect(component.get('currentAnswerNumber')).to.equal('');
+    });
+
+    it('when challenge is new and all answers are loaded, should return the next answer number', function() {
+      // when
+      component.set('challengeId', 'recChallengeNew');
+      secondAnswer.set('challenge.id', 'recChallenge2');
+
+      // then
+      expect(component.get('currentAnswerNumber')).to.equal(3);
+    });
+
+    it('when challenge is known and then answer is loaded, should return the matching answer number', function() {
+      // when
+      component.set('challengeId', 'recChallenge2');
+      secondAnswer.set('challenge.id', 'recChallenge2');
+
+      // then
+      expect(component.get('currentAnswerNumber')).to.equal(2);
     });
   });
 });
