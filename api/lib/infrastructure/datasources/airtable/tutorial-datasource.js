234998467dc8f17a7269e86302a6ad164c4f4486
Jonas Grillères <grilleres.j@gmail.com>
2019-10-15 10:51:06 +0200

    Add findByRecordIds in Tutorial Datasource

--- api/lib/infrastructure/datasources/airtable/tutorial-datasource.js
+++ api/lib/infrastructure/datasources/airtable/tutorial-datasource.js
@@ -1,10 +1,26 @@
 const airtable = require('../../airtable');
 const { Tutorial } = require('./objects');
 
+const _ = require('lodash');
+
+function _doQuery(filter) {
+  return airtable.findRecords(Tutorial.getAirtableName(), Tutorial.getUsedAirtableFields())
+    .then((rawTutorials) => {
+      return _(rawTutorials)
+        .filter(filter)
+        .map(Tutorial.fromAirTableObject)
+        .value();
+    });
+}
+
 module.exports = {
+  findByRecordIds(tutorialRecordIds) {
+    return _doQuery((rawTutorial) => _.includes(tutorialRecordIds, rawTutorial.id));
+  },
+  
   get(id) {
     return airtable.getRecord(Tutorial.getAirtableName(), id)
       .then((airtableRawObject) => Tutorial.fromAirTableObject(airtableRawObject));
-  }
+  },
 };
 
