400b46e8eab63bf4007a239def46c1e95dee5554
David Hérault <dherault@gmail.com>
2019-10-15 11:19:33 +0200

    Add createOrUpdateOrganizations script

---
+++ api/scripts/create-or-update-sco-organizations.js
@@ -0,0 +1,182 @@
+/* eslint-disable no-console */
+// Usage: BASE_URL=... PIXMASTER_EMAIL=... PIXMASTER_PASSWORD=... node create-or-update-sco-organizations.js path/file.csv
+
+'use strict';
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const papa = require('papaparse');
+const request = require('request-promise-native');
+
+function assertFileValidity(filePath) {
+  if (!fs.existsSync(filePath)) {
+    throw new Error(`File not found ${filePath}`);
+  }
+
+  const fileExtension = path.extname(filePath);
+
+  if (fileExtension !== '.csv') {
+    throw new Error(`File extension not supported ${fileExtension}`);
+  }
+}
+
+function organizeOrganizationsByExternalId(data) {
+  const organizationsByExternalId = {};
+
+  data.forEach((organization) => {
+    if (organization.attributes['external-id']) {
+      organizationsByExternalId[organization.attributes['external-id']] = { id: organization.id, ...organization.attributes };
+    }
+  });
+
+  return organizationsByExternalId;
+}
+
+async function createOrUpdateOrganizations(accessToken, organizationsByExternalId, data) {
+  for (let i = 0; i < data.length; i++) {
+    process.stdout.write(`${i + 1}/${data.length}`);
+
+    const [externalId, name] = data[i];
+
+    if (!(externalId && name)) return;
+
+    const existingOrganization = organizationsByExternalId[externalId];
+
+    if (existingOrganization && name !== existingOrganization.name) {
+      await request(_buildPatchOrganizationRequestObject(accessToken, { id: existingOrganization.id, name }));
+    }
+    else if (!existingOrganization) {
+      await request(_buildPostOrganizationRequestObject(accessToken, {
+        name,
+        externalId,
+        provinceCode: externalId.substring(0, 3),
+        type: 'SCO',
+        isManagingStudents: true,
+      }));
+    }
+
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+  }
+}
+
+function _buildAccessTokenRequestObject() {
+  return {
+    method: 'POST',
+    baseUrl: process.env.BASE_URL,
+    url: '/api/token',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    form: {
+      grant_type: 'password',
+      username: process.env.PIXMASTER_EMAIL,
+      password: process.env.PIXMASTER_PASSWORD,
+    },
+    json: true,
+  };
+}
+
+function _buildGetOrganizationsRequestObject(accessToken) {
+  return {
+    method: 'GET',
+    headers: {
+      authorization: `Bearer ${accessToken}`,
+    },
+    baseUrl: process.env.BASE_URL,
+    url: '/api/organizations',
+    json: true,
+  };
+}
+
+function _buildPatchOrganizationRequestObject(accessToken, organization) {
+  return {
+    method: 'PATCH',
+    headers: {
+      authorization: `Bearer ${accessToken}`,
+    },
+    baseUrl: process.env.BASE_URL,
+    url: `/api/organizations/${organization.id}`,
+    json: true,
+    body: {
+      data: {
+        type: 'organizations',
+        id: organization.id,
+        attributes: {
+          name: organization.name,
+        },
+      },
+    },
+  };
+}
+
+function _buildPostOrganizationRequestObject(accessToken, organization) {
+  return {
+    method: 'POST',
+    headers: {
+      authorization: `Bearer ${accessToken}`,
+    },
+    baseUrl: process.env.BASE_URL,
+    url: '/api/organizations',
+    json: true,
+    body: {
+      data: {
+        type: 'organizations',
+        attributes: {
+          name: organization.name,
+          type: organization.type,
+          'external-id': organization.externalId,
+          'province-code': organization.provinceCode,
+          'is-managing-students': organization.isManagingStudents,
+        },
+      },
+    },
+  };
+}
+
+async function main() {
+  console.log('Starting creating or updating SCO organizations.');
+
+  try {
+    const filePath = process.argv[2];
+
+    process.stdout.write('Testing file validity... ');
+    assertFileValidity(filePath);
+    console.log('ok');
+
+    process.stdout.write('Reading and parsing data... ');
+    const rawData = fs.readFileSync(filePath, 'utf8');
+    const { data } = papa.parse(rawData);
+    console.log('ok');
+
+    process.stdout.write('Requesting API access token... ');
+    const response1 = await request(_buildAccessTokenRequestObject());
+    const accessToken = response1.access_token;
+    console.log('ok');
+
+    process.stdout.write('Fetching existing organizations... ');
+    const response2 = await request(_buildGetOrganizationsRequestObject(accessToken));
+    const organizationsByExternalId = organizeOrganizationsByExternalId(response2.data);
+    console.log('ok');
+
+    console.log('Creating or updating organizations...');
+    await createOrUpdateOrganizations(accessToken, organizationsByExternalId, data);
+    console.log('\nDone.');
+
+  } catch (error) {
+    console.log();
+    console.error(error);
+
+    process.exit(1);
+  }
+}
+
+if (require.main === module) {
+  main();
+}
+
+module.exports = {
+  assertFileValidity,
+  organizeOrganizationsByExternalId,
+  createOrUpdateOrganizations,
+};
---
+++ api/tests/acceptance/scripts/create-or-update-sco-organizations_test.js
@@ -0,0 +1,162 @@
+const { expect, nock } = require('../../test-helper');
+const script = require('../../../scripts/create-or-update-sco-organizations');
+
+describe('Acceptance | Scripts | create-or-update-sco-organizations.js', () => {
+
+  describe('#assertFileValidity', () => {
+
+    it('should throw an error when file does not exist', () => {
+      // given
+      const filePath = 'inexistant.file';
+
+      try {
+        // when
+        script.assertFileValidity(filePath);
+
+        // then
+        expect.fail('Expected error to have been thrown');
+      } catch (err) {
+        expect(err.message).to.equal('File not found inexistant.file');
+      }
+    });
+
+    it('should throw an error when file extension is not ".csv"', () => {
+      // given
+      const filePath = `${__dirname}/file_with_bad_extension.html`;
+
+      try {
+        // when
+        script.assertFileValidity(filePath);
+
+        // then
+        expect.fail('Expected error to have been thrown');
+      } catch (err) {
+        expect(err.message).to.equal('File extension not supported .html');
+      }
+    });
+
+  });
+
+  describe('#organizeOrganizationsByExternalId', () => {
+
+    it('should return organaized organizations', () => {
+      // given
+      const data = [
+        {
+          id: 1,
+          attributes: {
+            name: 'Lycée Jean Moulin',
+            'external-id': 'A100',
+          },
+        },
+        {
+          id: 2,
+          attributes: {
+            name: 'Lycée Jean Guedin',
+            'external-id': 'B200',
+          },
+        },
+      ];
+
+      const expectedResult = {
+        A100: {
+          id: 1,
+          name: 'Lycée Jean Moulin',
+          'external-id': 'A100',
+        },
+        B200: {
+          id: 2,
+          name: 'Lycée Jean Guedin',
+          'external-id': 'B200',
+        },
+      };
+
+      // when
+      const result = script.organizeOrganizationsByExternalId(data);
+
+      // then
+      expect(result).to.deep.equal(expectedResult);
+    });
+  });
+
+  describe('#createOrUpdateOrganizations', () => {
+
+    it('should create or update organizations', async () => {
+      // given
+      const organizationsByExternalId = {
+        A100: {
+          id: 1,
+          name: 'Lycée Jean Moulin',
+          'external-id': 'A10',
+        },
+        B200: {
+          id: 2,
+          name: 'Lycée Jean Guedin',
+          'external-id': 'B20',
+        },
+      };
+
+      const data = [
+        ['A100', 'Lycée Jean Moulin'], // untouched
+        ['B200', 'Lycée Technique Jean Guedin'], // updated
+        ['C300', 'Lycée Professionnel Jean Rémy'], // created
+      ];
+
+      const expectedPatchBody = {
+        data: {
+          type: 'organizations',
+          id: 2,
+          attributes: {
+            name: 'Lycée Technique Jean Guedin',
+          },
+        },
+      };
+
+      const expectedPostBody = {
+        data: {
+          type: 'organizations',
+          attributes: {
+            name: 'Lycée Professionnel Jean Rémy',
+            type: 'SCO',
+            'external-id': 'C300',
+            'province-code': 'C30',
+            'is-managing-students': true,
+          },
+        },
+      };
+
+      let postCallCount = 0;
+      let patchCallCount = 0;
+
+      const networkStub = nock(
+        'http://localhost:3000',
+        {
+          reqheaders: {
+            authorization: 'Bearer token',
+          },
+        }
+      )
+        .patch('/api/organizations/2', (body) => JSON.stringify(body) === JSON.stringify(expectedPatchBody))
+        .reply(204, () => {
+          patchCallCount++;
+
+          return {};
+        })
+        .post('/api/organizations', (body) => JSON.stringify(body) === JSON.stringify(expectedPostBody))
+        .reply(201, () => {
+          postCallCount++;
+
+          return {};
+        });
+
+      // when
+      await script.createOrUpdateOrganizations('token', organizationsByExternalId, data);
+
+      // then
+      expect(networkStub.isDone()).to.be.true;
+      expect(postCallCount).to.be.equal(1);
+      expect(patchCallCount).to.be.equal(1);
+    });
+  });
+
+});
