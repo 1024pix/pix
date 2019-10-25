290568c76873050151c3228d34cd8acae21516f4
Krysthalia <ang.schall@gmail.com>
2019-10-25 17:09:39 +0200

    Add column userId to certification candidate table

--- api/lib/infrastructure/data/certification-candidate.js
+++ api/lib/infrastructure/data/certification-candidate.js
@@ -1,6 +1,7 @@
 const Bookshelf = require('../bookshelf');
 
 require('./session');
+require('./user');
 
 module.exports = Bookshelf.model('CertificationCandidate', {
   tableName: 'certification-candidates',
@@ -8,5 +9,9 @@ module.exports = Bookshelf.model('CertificationCandidate', {
 
   session() {
     return this.belongsTo('Session', 'sessionId');
+  },
+
+  user() {
+    return this.belongsTo('User', 'userId');
   }
 });
