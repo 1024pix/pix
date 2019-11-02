bbe226e9458cd5414658c91a75f762d1372b5810
David Riviere <driviere@octo.com>
2019-11-02 11:51:57 +0100

    Add getStudent in user-contoller

--- api/lib/application/users/user-controller.js
+++ api/lib/application/users/user-controller.js
@@ -4,6 +4,7 @@ const certificationProfileSerializer = require('../../infrastructure/serializers
 const membershipSerializer = require('../../infrastructure/serializers/jsonapi/membership-serializer');
 const pixScoreSerializer = require('../../infrastructure/serializers/jsonapi/pix-score-serializer');
 const scorecardSerializer = require('../../infrastructure/serializers/jsonapi/scorecard-serializer');
+const studentSerializer = require('../../infrastructure/serializers/jsonapi/student-serializer');
 const userSerializer = require('../../infrastructure/serializers/jsonapi/user-serializer');
 
 const usecases = require('../../domain/usecases');
@@ -144,5 +145,10 @@ module.exports = {
       .then(scorecardSerializer.serialize);
   },
 
-  getStudent() {}
+  getStudent(request) {
+    const authenticatedUserId = request.auth.credentials.userId;
+
+    return usecases.getUserStudent({ userId: authenticatedUserId })
+      .then(studentSerializer.serialize);
+  }
 };
---
+++ api/lib/domain/usecases/get-user-student.js
@@ -0,0 +1,6 @@
+module.exports = async function linkUserToOrganizationStudentData({
+  userId,
+  studentRepository,
+}) {
+  return 0;
+};
