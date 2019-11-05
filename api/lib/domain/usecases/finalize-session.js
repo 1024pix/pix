172c6dac7ed82e537a331b4bba6a9e04163138c0
LJA <ljacquemin@octo.com>
2019-11-05 15:03:03 +0100

    Add a finalizeSession usecase

---
+++ api/lib/domain/usecases/finalize-session.js
@@ -0,0 +1,20 @@
+const {
+  ForbiddenAccess
+} = require('../errors');
+
+const {
+  statuses
+} = require('../../domain/models/Session');
+
+module.exports = async function finalizeSession({
+  userId,
+  sessionId,
+  sessionRepository,
+} = {}) {
+  try {
+    await sessionRepository.ensureUserHasAccessToSession(userId, sessionId);
+  } catch (err) {
+    throw new ForbiddenAccess('User does not have the rights to finalize this session');
+  }
+  return sessionRepository.updateStatus({ sessionId, status: statuses.COMPLETED });
+};
