0525c5fd08e9907e0168d94a46773e1b7cf0afcb
Stephane Bedeau <sbedeau@octo.com>
2019-11-05 16:30:51 +0100

    [pf-839] Rename qroc-size helper

---
+++ mon-pix/tests/unit/helpers/get-qroc-input-size-test.js
@@ -0,0 +1,17 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { getQrocInputSize } from 'mon-pix/helpers/get-qroc-input-size';
+
+describe('Unit | Helper | get qroc input size', function() {
+  [
+    { format: 'petit', size: '5' },
+    { format: 'mots', size: '15' },
+    { format: 'phrase', size: '50' },
+    { format: 'paragraphe', size: '100' },
+    { format: 'another_unreferenced_word', size: '15' }
+  ].forEach((expected) => {
+    it(`should return correct size ${expected.size} for a given format ${expected.format}`, function() {
+      expect(getQrocInputSize(expected.format)).to.equal(expected.size);
+    });
+  });
+});
