718a7798cf1d7b0043d427a659aefac5fd223724
LJA <ljacquemin@octo.com>
2019-11-05 14:04:02 +0100

    Add session finalization to the frontend
    This commit adds the finalization logic to the frontend in the most
    basic yet functionnal form. Subsequent commits can then improve the
    experience and build on top of it.

---
+++ certif/app/components/routes/authenticated/sessions/session-finalizer.js
@@ -0,0 +1,17 @@
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+
+export default Component.extend({
+  tagName: '',
+  isLoading: false,
+  store: service(),
+
+  actions: {
+    finalizeSession(session) {
+      this.set('isLoading', true);
+      return session.finalize()
+        .then(console.log)
+        .then(() => this.set('isLoading', false))
+    }
+  }
+});
