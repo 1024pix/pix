51cc4a270b65019f4843f1f7cefb852bcdfd2979
Sébastien Roccaserra <sroccaserra@yahoo.com>
2019-11-05 11:03:38 +0100

    Add front toggle for new certification start

--- mon-pix/app/controllers/certifications/start.js
+++ mon-pix/app/controllers/certifications/start.js
@@ -1,6 +1,9 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 
+import config from 'mon-pix/config/environment';
+
 export default Controller.extend({
   currentUser: service(),
+  isNewCertificationStartActive: config.APP.isNewCertificationStartActive,
 });
