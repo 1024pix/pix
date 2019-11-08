698fe8abf93a3725b4063f89920318f57a1cc81e
Stephane Bedeau <sbedeau@octo.com>
2019-11-08 18:14:06 +0100

    [pf-839] Refactor QROC tests

--- mon-pix/tests/integration/components/qroc-proposal-test.js
+++ mon-pix/tests/integration/components/qroc-proposal-test.js
@@ -27,65 +27,6 @@ describe('Integration | Component | QROC proposal', function() {
       expect(find('.challenge-response__proposal-input')).to.not.exist;
       expect(find('.challenge-response__proposal-textarea').tagName).to.equal('TEXTAREA');
     });
-
-    describe('Component behavior when the user clicks on the input:', function() {
-      it('should not display autocompletion answers', async function() {
-        // given
-        const proposals = '${myInput}';
-        this.set('proposals', proposals);
-        this.set('answerValue', '');
-        this.set('format', 'paragraphe');
-
-        // when
-        await render(hbs`{{qroc-proposal proposals=proposals format=format answerValue=answerValue}}`);
-        // then
-        expect(find('.challenge-response__proposal-textarea').getAttribute('autocomplete')).to.equal('off');
-      });
-    });
-
-    describe('Component behavior when user fill input of challenge:', function() {
-
-      it('should display a value when a non-empty value is providing by user', async function() {
-        // given
-        const proposals = '${myInput}';
-        this.set('proposals', proposals);
-        this.set('answerValue', 'myValue');
-        this.set('format', 'paragraphe');
-
-        // when
-        await render(hbs`{{qroc-proposal proposals=proposals format=format answerValue=answerValue}}`);
-        // then
-        expect(find('.challenge-response__proposal-textarea').value).to.equal('myValue');
-      });
-    });
-
-    describe('Component behavior when user skip challenge:', function() {
-
-      [
-        { input: 'aband', output: 'aband' },
-        { input: '#aband#', output: '#aband#' },
-        { input: 'aband#', output: 'aband#' },
-        { input: 'ABAND', output: 'ABAND' },
-        { input: '#ABAND', output: '#ABAND' },
-        { input: 'ABAND#', output: 'ABAND#' },
-        { input: '#ABAND#', output: '' },
-        { input: '', output: '' }
-      ].forEach(({ input, output }) => {
-
-        it(`should display '' value ${input} is providing to component`, async function() {
-          // given
-          this.set('proposals', '${myLabel}');
-          this.set('answerValue', input);
-          this.set('format', 'paragraphe');
-
-          // when
-          await render(hbs`{{qroc-proposal proposals=proposals format=format answerValue=answerValue}}`);
-          // then
-          expect(find('.challenge-response__proposal-textarea').value).to.be.equal(output);
-        });
-
-      });
-    });
   });
 
   describe('When format is not a paragraph', function() {
@@ -111,58 +52,72 @@ describe('Integration | Component | QROC proposal', function() {
       });
     });
 
-    describe('Component behavior when the user clicks on the input:', function() {
-      it('should not display autocompletion answers', async function() {
-        // given
-        const proposals = '${myInput}';
-        this.set('proposals', proposals);
-        this.set('answerValue', '');
-        // when
-        await render(hbs`{{qroc-proposal proposals=proposals answerValue=answerValue}}`);
-        // then
-        expect(find('.challenge-response__proposal-input').getAttribute('autocomplete')).to.equal('off');
-      });
-    });
+  });
 
-    describe('Component behavior when user fill input of challenge:', function() {
-
-      it('should display a value when a non-empty value is providing by user', async function() {
-        // given
-        const proposals = '${myInput}';
-        this.set('proposals', proposals);
-        this.set('answerValue', 'myValue');
-        // when
-        await render(hbs`{{qroc-proposal proposals=proposals answerValue=answerValue}}`);
-        // then
-        expect(find('.challenge-response__proposal-input').value).to.equal('myValue');
-      });
-    });
-
-    describe('Component behavior when user skip challenge:', function() {
-
-      [
-        { input: 'aband', output: 'aband' },
-        { input: '#aband#', output: '#aband#' },
-        { input: 'aband#', output: 'aband#' },
-        { input: 'ABAND', output: 'ABAND' },
-        { input: '#ABAND', output: '#ABAND' },
-        { input: 'ABAND#', output: 'ABAND#' },
-        { input: '#ABAND#', output: '' },
-        { input: '', output: '' }
-      ].forEach(({ input, output }) => {
-
-        it(`should display '' value ${input} is providing to component`, async function() {
+  describe('Whatever the format', function() {
+    [
+      { format: 'mots', cssClass: '.challenge-response__proposal-input', inputType: 'input' },
+      { format: 'unreferenced_format', cssClass: '.challenge-response__proposal-input', inputType: 'input' },
+      { format: 'paragraphe', cssClass: '.challenge-response__proposal-textarea', inputType: 'textarea' },
+    ].forEach((data) => {
+      describe(`Component behavior when the user clicks on the ${data.inputType}:`, function() {
+        it('should not display autocompletion answers', async function() {
           // given
-          this.set('proposals', '${myLabel}');
-          this.set('answerValue', input);
-          // when
-          await render(hbs`{{qroc-proposal proposals=proposals answerValue=answerValue}}`);
-          // then
-          expect(find('.challenge-response__proposal-input').value).to.be.equal(output);
-        });
+          const proposals = '${myInput}';
+          this.set('proposals', proposals);
+          this.set('answerValue', '');
+          this.set('format', `${data.format}`);
 
+          // when
+          await render(hbs`{{qroc-proposal proposals=proposals format=format answerValue=answerValue}}`);
+          // then
+          expect(find(`${data.cssClass}`).getAttribute('autocomplete')).to.equal('off');
+        });
+      });
+
+      describe('Component behavior when user fill input of challenge:', function() {
+
+        it('should display a value when a non-empty value is providing by user', async function() {
+          // given
+          const proposals = '${myInput}';
+          this.set('proposals', proposals);
+          this.set('answerValue', 'myValue');
+          this.set('format', `${data.format}`);
+
+          // when
+          await render(hbs`{{qroc-proposal proposals=proposals format=format answerValue=answerValue}}`);
+          // then
+          expect(find(`${data.cssClass}`).value).to.equal('myValue');
+        });
+      });
+
+      describe('Component behavior when user skip challenge:', function() {
+
+        [
+          { input: 'aband', output: 'aband' },
+          { input: '#aband#', output: '#aband#' },
+          { input: 'aband#', output: 'aband#' },
+          { input: 'ABAND', output: 'ABAND' },
+          { input: '#ABAND', output: '#ABAND' },
+          { input: 'ABAND#', output: 'ABAND#' },
+          { input: '#ABAND#', output: '' },
+          { input: '', output: '' }
+        ].forEach(({ input, output }) => {
+
+          it(`should display '' value ${input} is providing to component`, async function() {
+            // given
+            this.set('proposals', '${myLabel}');
+            this.set('answerValue', input);
+            this.set('format', `${data.format}`);
+
+            // when
+            await render(hbs`{{qroc-proposal proposals=proposals format=format answerValue=answerValue}}`);
+            // then
+            expect(find(`${data.cssClass}`).value).to.be.equal(output);
+          });
+
+        });
       });
     });
   });
-
 });
