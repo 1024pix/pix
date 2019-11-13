0ce6a767ca5f3b1b4e63391a5974e7a63ae953a4
laura-bergoens <laura.bergoens@gmail.com>
2019-11-13 11:03:59 +0100

    Add date-utils method for formatting date
    This method takes a value, an input format and an output format
    to parse the value.

--- api/lib/infrastructure/utils/date-utils.js
+++ api/lib/infrastructure/utils/date-utils.js
@@ -4,6 +4,11 @@ function isValidDate(dateValue, format) {
   return moment.utc(dateValue, format, true).isValid();
 }
 
+function convertDateValue(dateString, inputFormat, outputFormat) {
+  return isValidDate(dateString, inputFormat) ? moment(dateString, inputFormat, true).format(outputFormat) : null;
+}
+
 module.exports = {
   isValidDate,
+  convertDateValue,
 };
