6747cda92f9139a749597ec759c678a4fa20b0d8
David Hérault <dherault@gmail.com>
2019-11-14 15:41:53 +0100

    Display role in team list
    Sort members by role and lastName

--- orga/tests/integration/components/routes/authenticated/team/list-items-test.js
+++ orga/tests/integration/components/routes/authenticated/team/list-items-test.js
@@ -1,17 +1,36 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
+import { run } from '@ember/runloop';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | routes/authenticated/team | list-items', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it should display a list of members, their firstName and lastName', async function(assert) {
+  test('it should display a list of members, their firstName, lastName and role', async function(assert) {
     // given
-    this.set('memberships', [
-      { user: { firstName: 'Gigi', lastName: 'La Terreur' } },
-      { user: { firstName: 'Gogo', lastName: 'L\'asticot' } },
-    ]);
+    const store = this.owner.lookup('service:store');
+    const memberships = [
+      run(() => store.createRecord('membership', {
+        id: 1,
+        organizationRole: 'ADMIN',
+        user: run(() => store.createRecord('user', {
+          id: 111,
+          firstName: 'Gigi',
+          lastName: 'La Terreur',
+        })),
+      })),
+      run(() => store.createRecord('membership', {
+        id: 2,
+        organizationRole: 'MEMBER',
+        user: run(() => store.createRecord('user', {
+          id: 112,
+          firstName: 'Gogo',
+          lastName: 'L\'asticot',
+        })),
+      })),
+    ];
+    this.set('memberships', memberships);
 
     // when
     await render(hbs`{{routes/authenticated/team/list-items memberships=memberships}}`);
@@ -19,7 +38,8 @@ module('Integration | Component | routes/authenticated/team | list-items', funct
     // then
     assert.dom('#table-members tbody tr').exists({ count: 2 });
     assert.dom('#table-members tbody tr:first-child td:first-child').hasText('La Terreur');
-    assert.dom('#table-members tbody tr:first-child td:last-child').hasText('Gigi');
+    assert.dom('#table-members tbody tr:first-child td:nth-child(2)').hasText('Gigi');
+    assert.dom('#table-members tbody tr:first-child td:nth-child(3)').hasText('Administrateur');
   });
 
   test('it should display a list of invitations, their email and creation date', async function(assert) {
