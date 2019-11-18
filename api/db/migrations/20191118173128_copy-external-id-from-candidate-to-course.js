9f301fbfb4b93935a3d5c47f9c504aa83c97e6c4
yahya-youness <you@octo.com>
2019-11-18 18:12:13 +0100

    script copy externalId from candidate to course

---
+++ api/db/migrations/20191118173128_copy-external-id-from-candidate-to-course.js
@@ -0,0 +1,24 @@
+exports.up = async function(knex) {
+
+  await knex.raw(`
+      UPDATE "public"."certification-courses" as cc
+      SET "externalId" = joinCcAndCca."externalIdCertificationCandidate"
+      FROM
+      ( SELECT 
+             cc."id" AS "idCertificationCourse",
+             cca."externalId" AS "externalIdCertificationCandidate"
+      FROM "public"."certification-courses" AS cc
+      JOIN "public"."certification-candidates" AS cca ON cc."firstName" = cca."firstName"
+      AND cc."lastName" = cca."lastName" AND cc."birthdate" = cca."birthdate"
+      AND cc."sessionId" = cca."sessionId"
+      WHERE cca."externalId" IS NOT NULL
+        AND cc."externalId" IS  NULL ) as joinCcAndCca
+        
+      WHERE cc."id" = joinCcAndCca."idCertificationCourse";
+    `);
+};
+
+exports.down = function() {
+  // no rollback for this case
+};
+
