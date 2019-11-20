63bb856570ccf84d7f2497a0ed7da56f1a935834
laura-bergoens <laura.bergoens@gmail.com>
2019-11-20 15:25:25 +0100

    Add step with formbuilder link
    Add a template to add easily a new finalization step +
    specific formbuilder step

---
+++ certif/tests/integration/components/session-finalization-formbuilder-link-step-test.js
@@ -0,0 +1,14 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | session-finalization-formbuilder-link-step', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    await render(hbs`<SessionFinalizationFormbuilderLinkStep />`);
+
+    assert.equal(this.element.textContent.trim().replace(/\s+/g, ' '), 'Pour transmettre le PV de session sur 123formbuilder, suivez ce lien Formulaire 123formbuilder');
+  });
+});
