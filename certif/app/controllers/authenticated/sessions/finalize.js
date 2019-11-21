e461d1ea6def5648c7721a61a23d37fb67385ed0
Krysthalia <ang.schall@gmail.com>
2019-11-21 16:10:30 +0100

    Adding message notification on finalization

--- certif/app/controllers/authenticated/sessions/finalize.js
+++ certif/app/controllers/authenticated/sessions/finalize.js
@@ -1,7 +1,11 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+
+import config from '../../../config/environment';
 
 export default Controller.extend({
   isLoading: false,
+  notifications: service('notification-messages'),
 
   actions: {
     finalizeSession() {
@@ -10,7 +14,15 @@ export default Controller.extend({
         .then(() => {
           this.set('isLoading', false);
           this.transitionToRoute('authenticated.sessions.details', this.model.id);
+
+          const autoClear = config.notifications.autoClear;
+          const clearDuration = config.notifications.clearDuration;
+
+          this.get('notifications').success(
+            'Les informations de la session ont été transmises avec succès.',
+            { autoClear, clearDuration },
+          );
         });
-    }
+    },
   },
 });
