a30497346bbaab0e2b6f575f8481cf4e07eb7515
Krysthalia <ang.schall@gmail.com>
2019-11-25 16:44:32 +0100

    Disable finalized button when session finalized
    In finalize.js we blocked the redirection (if the session is finalized) in afterModel()

--- certif/app/routes/authenticated/sessions/finalize.js
+++ certif/app/routes/authenticated/sessions/finalize.js
@@ -3,5 +3,11 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model({ session_id }) {
     return this.store.findRecord('session', session_id);
-  }
+  },
+
+  afterModel(model, transition) {
+    if (model.isFinalized) {
+      transition.abort();
+    }
+  },
 });
