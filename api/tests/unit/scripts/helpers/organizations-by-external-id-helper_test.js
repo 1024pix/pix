8c46a3b946705740026769925584c3c820037411
lisequesnel <quesnel.lise@gmail.com>
2019-11-25 10:01:05 +0100

    create file organizations-by-external-id-helper

---
+++ api/tests/unit/scripts/helpers/organizations-by-external-id-helper_test.js
@@ -0,0 +1,60 @@
+const { expect, sinon } = require('../../../test-helper');
+
+const { findOrganizationsByExternalIds, organizeOrganizationsByExternalId } = require('../../../../scripts/organizations-by-external-id-helper');
+const organizationRepository = require('../../../../lib/infrastructure/repositories/organization-repository');
+
+describe('Acceptance | Scripts | organizations-by-external-id-helper.js', () => {
+
+  describe('#organizeOrganizationsByExternalId', () => {
+
+    it('should return organizations data by externalId', () => {
+      // given
+      const organizations = [
+        { id: 1, externalId: 'A100' },
+        { id: 2, externalId: 'B200' },
+      ];
+
+      const expectedResult = {
+        A100: {
+          id: 1,
+          externalId: 'A100',
+        },
+        B200: {
+          id: 2,
+          externalId: 'B200',
+        },
+      };
+
+      // when
+      const result = organizeOrganizationsByExternalId(organizations);
+
+      // then
+      expect(result).to.deep.equal(expectedResult);
+    });
+  });
+
+  describe('#findOrganizationsByExternalIds', () => {
+
+    let organizationRepositoryStub;
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('should find organizations with given externalIds', async () => {
+      // given
+      const checkedData = [
+        { externalId: 'a100', targetProfileIdList: ['1', '2', '999'] },
+        { externalId: 'b200', targetProfileIdList: ['1', '3', '6'] }
+      ];
+      organizationRepositoryStub = sinon.stub(organizationRepository, 'findByExternalIds').withArgs(['A100', 'B200']).resolves([]);
+
+      // when
+      await findOrganizationsByExternalIds({ checkedData });
+
+      // then
+      expect(organizationRepositoryStub).to.have.been.calledOnce;
+    });
+  });
+
+});
