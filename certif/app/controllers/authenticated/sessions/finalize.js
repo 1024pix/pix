72577005ce76e788eb331cc2dac5015ec1da4b10
Krysthalia <ang.schall@gmail.com>
2019-11-26 15:03:11 +0100

    Map domain error to infra error
    SessionAlreadyFinalizedError (domain error) is now
    link to a BadRequest (infra error).
    Front side we add notification error,
    and make sure that we can't finalize a session
    when it's already finalized.

--- certif/app/controllers/authenticated/sessions/finalize.js
+++ certif/app/controllers/authenticated/sessions/finalize.js
@@ -9,22 +9,32 @@ export default Controller.extend({
 
   showConfirmModal: false,
 
+  showErrorNotification(message) {
+    const { autoClear, clearDuration } = config.notifications;
+    this.get('notifications').error(message, { autoClear, clearDuration });
+  },
+
+  showSuccessNotification(message) {
+    const { autoClear, clearDuration } = config.notifications;
+    this.get('notifications').success(message, { autoClear, clearDuration });
+  },
+
   actions: {
     finalizeSession() {
       this.set('isLoading', true);
       return this.model.finalize()
         .then(() => {
-          this.set('showConfirmModal', false);
+          this.showSuccessNotification('Les informations de la session ont été transmises avec succès.');
+        })
+        .catch((err) => {
+          (err.errors && err.errors[0] && err.errors[0].status === '400')
+            ? this.showErrorNotification('Cette session a déjà été finalisée.')
+            : this.showErrorNotification('Erreur lors de la finalisation de session.');
+        })
+        .finally(() => {
           this.set('isLoading', false);
+          this.set('showConfirmModal', false);
           this.transitionToRoute('authenticated.sessions.details', this.model.id);
-
-          const autoClear = config.notifications.autoClear;
-          const clearDuration = config.notifications.clearDuration;
-
-          this.get('notifications').success(
-            'Les informations de la session ont été transmises avec succès.',
-            { autoClear, clearDuration },
-          );
         });
     },
 
@@ -35,6 +45,6 @@ export default Controller.extend({
     closeModal() {
       this.set('showConfirmModal', false);
     },
-
   },
+
 });
