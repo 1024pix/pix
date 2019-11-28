d64c3efb936ae95bca9af8df5743cddad8ffbe4f
lisequesnel <quesnel.lise@gmail.com>
2019-11-28 16:32:12 +0100

    Update users find in findPaginatedFiltered

---
+++ api/tests/unit/domain/usecases/find-paginated-filtered-users_test.js
@@ -0,0 +1,33 @@
+const { expect, sinon } = require('../../../test-helper');
+const usecases = require('../../../../lib/domain/usecases');
+const User = require('../../../../lib/domain/models/User');
+
+describe('Unit | UseCase | find-paginated-filtered-users', () => {
+
+  it('should search users with filtering and pagination', async () => {
+    // given
+    const filter = { email: 'gigi@example.net' };
+    const page = { number: 1, size: 2 };
+
+    const resolvedPagination = { page: 1, pageSize: 2, itemsCount: 3, pagesCount: 2 };
+    const matchingUsers = [
+      new User({ id: 1 }),
+      new User({ id: 2 }),
+      new User({ id: 3 }),
+    ];
+    const userRepository = {
+      findPaginatedFiltered: sinon.stub()
+    };
+    userRepository.findPaginatedFiltered.withArgs({ filter, page }).resolves({ models: matchingUsers, pagination: resolvedPagination });
+
+    // when
+    const response = await usecases.findPaginatedFilteredUsers({ filter, page, userRepository });
+
+    // then
+    expect(response.models).to.equal(matchingUsers);
+    expect(response.pagination.page).to.equal(resolvedPagination.page);
+    expect(response.pagination.pageSize).to.equal(resolvedPagination.pageSize);
+    expect(response.pagination.itemsCount).to.equal(resolvedPagination.itemsCount);
+    expect(response.pagination.pagesCount).to.equal(resolvedPagination.pagesCount);
+  });
+});
