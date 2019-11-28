15f147018e473306d75593d1c0816fa364818de7
lisequesnel <quesnel.lise@gmail.com>
2019-11-28 17:07:22 +0100

    Add a list-items component to list users with pagination & filtering

--- admin/app/routes/authenticated/users/list.js
+++ admin/app/routes/authenticated/users/list.js
@@ -2,10 +2,45 @@ import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Route.extend(AuthenticatedRouteMixin, {
+  queryParams: {
+    pageNumber: {
+      refreshModel: true
+    },
+    pageSize: {
+      refreshModel: true
+    },
+    firstName: {
+      refreshModel: true
+    },
+    lastName: {
+      refreshModel: true
+    },
+    email: {
+      refreshModel: true
+    }
+  },
 
-  model() {
-    // ℹ️ We use `#query` instead of `#findAll` because only the first one handle metadata
-    return this.store.query('user', {});
+  model(params) {
+    return this.store.query('user', {
+      filter: {
+        firstName: params.firstName ? params.firstName.trim() : '',
+        lastName: params.lastName ? params.lastName.trim() : '',
+        email: params.email ? params.email.trim() : '',
+      },
+      page: {
+        number: params.pageNumber,
+        size: params.pageSize,
+      },
+    });
+  },
+
+  resetController(controller, isExiting) {
+    if (isExiting) {
+      controller.set('pageNumber', 1);
+      controller.set('pageSize', 10);
+      controller.set('firstName', null);
+      controller.set('lastName', null);
+      controller.set('email', null);
+    }
   }
-
 });
