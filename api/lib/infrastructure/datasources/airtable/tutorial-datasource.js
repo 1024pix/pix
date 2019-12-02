64909db8fa2e1d92d86641e425543e514bb52762
jbuget@gmail.com <jbuget@gmail.com>
2019-12-02 14:39:08 +0100

    Simplify a little the way to write a Datasource.

--- api/lib/infrastructure/datasources/airtable/tutorial-datasource.js
+++ api/lib/infrastructure/datasources/airtable/tutorial-datasource.js
@@ -1,34 +1,28 @@
 const _ = require('lodash');
 const datasource = require('./datasource');
 
-const tableName = 'Tutoriels';
-
-const usedFields = [
-  'Durée',
-  'Format',
-  'Lien',
-  'Source',
-  'Titre',
-];
-
-function fromAirTableObject(airtableTutorialObject) {
-  return {
-    id: airtableTutorialObject.getId(),
-    duration: airtableTutorialObject.get('Durée'),
-    format: airtableTutorialObject.get('Format'),
-    link: airtableTutorialObject.get('Lien'),
-    source: airtableTutorialObject.get('Source'),
-    title: airtableTutorialObject.get('Titre'),
-  };
-}
-
 module.exports = datasource.extend({
 
-  tableName,
+  tableName: 'Tutoriels',
 
-  usedFields,
+  usedFields: [
+    'Durée',
+    'Format',
+    'Lien',
+    'Source',
+    'Titre',
+  ],
 
-  fromAirTableObject,
+  fromAirTableObject(airtableTutorialObject) {
+    return {
+      id: airtableTutorialObject.getId(),
+      duration: airtableTutorialObject.get('Durée'),
+      format: airtableTutorialObject.get('Format'),
+      link: airtableTutorialObject.get('Lien'),
+      source: airtableTutorialObject.get('Source'),
+      title: airtableTutorialObject.get('Titre'),
+    };
+  },
 
   findByRecordIds(tutorialRecordIds) {
     return this.list({ filter: (rawTutorial) => _.includes(tutorialRecordIds, rawTutorial.id) });
