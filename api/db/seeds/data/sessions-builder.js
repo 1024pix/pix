114e0d008290aa62246893f70c5312fdbc41e406
laura-bergoens <laura.bergoens@gmail.com>
2019-12-05 14:49:13 +0100

    Add session seeds with examinerComment

--- api/db/seeds/data/sessions-builder.js
+++ api/db/seeds/data/sessions-builder.js
@@ -49,6 +49,8 @@ module.exports = function sessionsBuilder({ databaseBuilder }) {
     description: 'Session',
     room: 'Salle Opette',
     accessCode: 'JKL78',
+    status: 'finalized',
     certificationCenterId: 1,
+    examinerComment: 'Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l\'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n\'a pas fait que survivre cinq siècles, mais s\'est aussi adapté à la bureautique informatique, sans que son contenu n\'en soit modifié.',
   });
 };
