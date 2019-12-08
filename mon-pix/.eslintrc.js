e4d2294058a36d10dea2124cc3a0a6901a55a005
laura-bergoens <laura.bergoens@gmail.com>
2019-12-08 14:34:15 +0100

    Upgrade eslint related modules
    eslint: 5.16.0 -> 6.7.2
    eslint-plugin-ember : 6.4.1 -> 7.7.1
    eslint-plugin-mocha : 5.3.0 -> 6.2.2
    
    Adding a rule to ignore :
      ember/no-new-mixins (unless someone wants to handle it ;) )
    
    Applying changes to respect new rules :
      ember/require-return-from-computed
      ember/no-incorrect-calls-with-inline-anonymous-functions
      ember/no-unnecessary-route-path-option

--- mon-pix/.eslintrc.js
+++ mon-pix/.eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   rules: {
     'ember/avoid-leaking-state-in-ember-objects': 0,
     'ember/no-restricted-resolver-tests': 0,
+    'ember/no-new-mixins': 0,
   },
   overrides: [
     // node files
