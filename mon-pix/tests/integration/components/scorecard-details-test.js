121c7749a5011ae10651af907724622907bf9386
Stephane Bedeau <sbedeau@octo.com>
2019-12-09 19:14:11 +0100

    BSR - Add some contexts in scorecard-details-test
    Contexts have been added to tests in order to :
    - enhance readability;
    - simplify the insertion of new tests by reusing
    given and when conditions in a beforeEach.

--- mon-pix/tests/integration/components/scorecard-details-test.js
+++ mon-pix/tests/integration/components/scorecard-details-test.js
@@ -80,55 +80,6 @@ describe('Integration | Component | scorecard-details', function() {
       expect(this.element.querySelector('.scorecard-details-content-right__level-info').textContent).to.contain(`${scorecard.remainingPixToNextLevel} pix avant le niveau ${scorecard.level + 1}`);
     });
 
-    it('should not display remainingPixToNextLevel if scorecard.isMaxLevel is true', async function() {
-      // given
-      const scorecard = {
-        remainingPixToNextLevel: 1,
-        isMaxLevel: true,
-      };
-
-      this.set('scorecard', scorecard);
-
-      // when
-      await render(hbs`{{scorecard-details scorecard=scorecard}}`);
-
-      // then
-      expect(this.element.querySelector('.scorecard-details-content-right__level-info')).to.not.exist;
-    });
-
-    it('should not display remainingPixToNextLevel if scorecard.isFinished is true', async function() {
-      // given
-      const scorecard = {
-        remainingPixToNextLevel: 1,
-        isFinished: true,
-      };
-
-      this.set('scorecard', scorecard);
-
-      // when
-      await render(hbs`{{scorecard-details scorecard=scorecard}}`);
-
-      // then
-      expect(this.element.querySelector('.scorecard-details-content-right__level-info')).to.not.exist;
-    });
-
-    it('should not display the level and remainingPixToNextLevel if scorecard.isNotStarted is true', async function() {
-      // given
-      const scorecard = {
-        remainingPixToNextLevel: 1,
-        isNotStarted: true,
-      };
-
-      this.set('scorecard', scorecard);
-
-      // when
-      await render(hbs`{{scorecard-details scorecard=scorecard}}`);
-
-      // then
-      expect(this.element.querySelector('.scorecard-details-content-right__score-container')).to.not.exist;
-      expect(this.element.querySelector('.scorecard-details-content-right__level-info')).to.not.exist;
-    });
-
     it('should display a dash instead of the scorecard level and earnedPix if they are set to zero', async function() {
       // given
       const scorecard = {
@@ -146,158 +97,209 @@ describe('Integration | Component | scorecard-details', function() {
       expect(this.element.querySelector('.score-value').textContent).to.contain('–');
     });
 
-    it('should not display a button if scorecard.isFinished', async function() {
-      // given
-      const scorecard = {
-        isFinished: true,
-      };
+    context('When the user has reached the max level, i.e. scorecard.isMaxLevel is true', async function() {
+      it('should not display remainingPixToNextLevel', async function() {
+        // given
+        const scorecard = {
+          remainingPixToNextLevel: 1,
+          isMaxLevel: true,
+        };
 
-      this.set('scorecard', scorecard);
+        this.set('scorecard', scorecard);
 
-      // when
-      await render(hbs`{{scorecard-details scorecard=scorecard}}`);
+        // when
+        await render(hbs`{{scorecard-details scorecard=scorecard}}`);
 
-      // then
-      expect(this.element.querySelector('.scorecard-details__resume-or-start-button')).to.not.exist;
+        // then
+        expect(this.element.querySelector('.scorecard-details-content-right__level-info')).to.not.exist;
+      });
     });
 
-    it('should display a button stating "Commencer" if scorecard.isStarted is false', async function() {
-      // given
-      const scorecard = {
-        competenceId: 1,
-        isStarted: false,
-      };
+    context('When the user has finished a competence, i.e. scorecard.isFinished is true', async function() {
+      beforeEach(async function() {
+        // given
+        const scorecard = {
+          remainingPixToNextLevel: 1,
+          isFinished: true,
+        };
 
-      this.set('scorecard', scorecard);
+        this.set('scorecard', scorecard);
 
-      // when
-      await render(hbs`{{scorecard-details scorecard=scorecard}}`);
+        // when
+        await render(hbs`{{scorecard-details scorecard=scorecard}}`);
+      });
 
-      // then
-      const element = this.element.querySelector('.scorecard-details__resume-or-start-button');
-      expect(element).to.exist;
-      expect(element.textContent).to.contain('Commencer');
+      it('should not display remainingPixToNextLevel', async function() {
+        // then
+        expect(this.element.querySelector('.scorecard-details-content-right__level-info')).to.not.exist;
+      });
+
+      it('should not display a button', async function() {
+        // then
+        expect(this.element.querySelector('.scorecard-details__resume-or-start-button')).to.not.exist;
+      });
     });
 
-    it('should display a button stating "Reprendre" if scorecard.isStarted is true', async function() {
-      // given
-      const scorecard = {
-        competenceId: 1,
-        isStarted: true,
-      };
+    context('When the user did not started a competence', function() {
+      it('should not display the level and remainingPixToNextLevel if scorecard.isNotStarted is true', async function() {
+        // given
+        const scorecard = {
+          remainingPixToNextLevel: 1,
+          isNotStarted: true,
+        };
 
-      this.set('scorecard', scorecard);
+        this.set('scorecard', scorecard);
 
-      // when
-      await render(hbs`{{scorecard-details scorecard=scorecard}}`);
+        // when
+        await render(hbs`{{scorecard-details scorecard=scorecard}}`);
 
-      // then
-      const element = this.element.querySelector('.scorecard-details__resume-or-start-button');
-      expect(element).to.exist;
-      expect(element.textContent).to.contain('Reprendre');
+        // then
+        expect(this.element.querySelector('.scorecard-details-content-right__score-container')).to.not.exist;
+        expect(this.element.querySelector('.scorecard-details-content-right__level-info')).to.not.exist;
+      });
+
+      it('should display a button stating "Commencer" if scorecard.isStarted is false', async function() {
+        // given
+        const scorecard = {
+          competenceId: 1,
+          isStarted: false,
+        };
+
+        this.set('scorecard', scorecard);
+
+        // when
+        await render(hbs`{{scorecard-details scorecard=scorecard}}`);
+
+        // then
+        const element = this.element.querySelector('.scorecard-details__resume-or-start-button');
+        expect(element).to.exist;
+        expect(element.textContent).to.contain('Commencer');
+      });
     });
 
-    it('should not display the tutorial section when there is no tutorial to show', async function() {
-      // given
-      const scorecard = {
-        competenceId: 1,
-        isStarted: true,
-      };
+    context('When the user has started a competence, i.e. scorecard.isStarted is true', async function() {
 
-      this.set('scorecard', scorecard);
+      it('should display a button stating "Reprendre"', async function() {
+        // given
+        const scorecard = {
+          competenceId: 1,
+          isStarted: true,
+        };
 
-      // when
-      await render(hbs`{{scorecard-details scorecard=scorecard}}`);
+        this.set('scorecard', scorecard);
 
-      // then
-      expect(find('.tutorials')).to.not.exist;
+        // when
+        await render(hbs`{{scorecard-details scorecard=scorecard}}`);
+
+        // then
+        const element = this.element.querySelector('.scorecard-details__resume-or-start-button');
+        expect(element).to.exist;
+        expect(element.textContent).to.contain('Reprendre');
+      });
+
+      it('should not display the tutorial section when there is no tutorial to show', async function() {
+        // given
+        const scorecard = {
+          competenceId: 1,
+          isStarted: true,
+        };
+
+        this.set('scorecard', scorecard);
+
+        // when
+        await render(hbs`{{scorecard-details scorecard=scorecard}}`);
+
+        // then
+        expect(find('.tutorials')).to.not.exist;
+      });
+
+      context('and the user has some tutorials', async function() {
+        it('should display the tutorial section and the related tutorials', async function() {
+          // given
+          const tuto1 = EmberObject.create({
+            title: 'Tuto 1.1',
+            tubeName: '@first_tube',
+            tubePracticalTitle: 'Practical Title',
+            duration: '00:15:10',
+          });
+          const tuto2 = EmberObject.create({
+            title: 'Tuto 2.1',
+            tubeName: '@second_tube',
+            tubePracticalTitle: 'Practical Title',
+            duration: '00:04:00',
+          });
+          const tuto3 = EmberObject.create({
+            title: 'Tuto 2.2',
+            tubeName: '@second_tube',
+            tubePracticalTitle: 'Practical Title',
+            duration: '00:04:00',
+          });
+
+          const tutorials = A([tuto1, tuto2, tuto3]);
+
+          const scorecard = EmberObject.create({
+            competenceId: 1,
+            isStarted: true,
+            tutorials,
+          });
+
+          this.set('scorecard', scorecard);
+
+          // when
+          await render(hbs`{{scorecard-details scorecard=scorecard}}`);
+
+          // then
+          expect(find('.tutorials')).to.exist;
+          expect(findAll('.tube')).to.have.lengthOf(2);
+          expect(findAll('.tutorial')).to.have.lengthOf(3);
+        });
+
+        it('should display the 4th tutorial of a tube on a new line', async function() {
+          // given
+          const tuto1 = EmberObject.create({
+            title: 'Tuto 1.1',
+            tubeName: '@first_tube',
+            tubePracticalTitle: 'Practical Title',
+            duration: '00:15:10',
+          });
+          const tuto2 = EmberObject.create({
+            title: 'Tuto 1.2',
+            tubeName: '@first_tube',
+            tubePracticalTitle: 'Practical Title',
+            duration: '00:04:00',
+          });
+          const tuto3 = EmberObject.create({
+            title: 'Tuto 1.3',
+            tubeName: '@first_tube',
+            tubePracticalTitle: 'Practical Title',
+            duration: '00:04:00',
+          });
+          const tuto4 = EmberObject.create({
+            title: 'Tuto 1.4',
+            tubeName: '@first_tube',
+            tubePracticalTitle: 'Practical Title',
+            duration: '00:04:00',
+          });
+
+          const tutorials = A([tuto1, tuto2, tuto3, tuto4]);
+
+          const scorecard = EmberObject.create({
+            competenceId: 1,
+            isStarted: true,
+            tutorials,
+          });
+
+          this.set('scorecard', scorecard);
+
+          // when
+          await render(hbs`{{scorecard-details scorecard=scorecard}}`);
+
+          // then
+          expect(find('.tutorials')).to.exist;
+          expect(findAll('.tube')).to.have.lengthOf(1);
+          expect(findAll('.tutorial')).to.have.lengthOf(4);
+        });
+      });
     });
-
-    it('should display the tutorial section and the related tutorials', async function() {
-      // given
-      const tuto1 = EmberObject.create({
-        title: 'Tuto 1.1',
-        tubeName: '@first_tube',
-        tubePracticalTitle: 'Practical Title',
-        duration: '00:15:10',
-      });
-      const tuto2 = EmberObject.create({
-        title: 'Tuto 2.1',
-        tubeName: '@second_tube',
-        tubePracticalTitle: 'Practical Title',
-        duration: '00:04:00',
-      });
-      const tuto3 = EmberObject.create({
-        title: 'Tuto 2.2',
-        tubeName: '@second_tube',
-        tubePracticalTitle: 'Practical Title',
-        duration: '00:04:00',
-      });
-
-      const tutorials = A([tuto1, tuto2, tuto3]);
-
-      const scorecard = EmberObject.create({
-        competenceId: 1,
-        isStarted: true,
-        tutorials,
-      });
-
-      this.set('scorecard', scorecard);
-
-      // when
-      await render(hbs`{{scorecard-details scorecard=scorecard}}`);
-
-      // then
-      expect(find('.tutorials')).to.exist;
-      expect(findAll('.tube')).to.have.lengthOf(2);
-      expect(findAll('.tutorial')).to.have.lengthOf(3);
-    });
-
-    it('should display the 4th tutorial of a tube on a new line', async function() {
-      // given
-      const tuto1 = EmberObject.create({
-        title: 'Tuto 1.1',
-        tubeName: '@first_tube',
-        tubePracticalTitle: 'Practical Title',
-        duration: '00:15:10',
-      });
-      const tuto2 = EmberObject.create({
-        title: 'Tuto 1.2',
-        tubeName: '@first_tube',
-        tubePracticalTitle: 'Practical Title',
-        duration: '00:04:00',
-      });
-      const tuto3 = EmberObject.create({
-        title: 'Tuto 1.3',
-        tubeName: '@first_tube',
-        tubePracticalTitle: 'Practical Title',
-        duration: '00:04:00',
-      });
-      const tuto4 = EmberObject.create({
-        title: 'Tuto 1.4',
-        tubeName: '@first_tube',
-        tubePracticalTitle: 'Practical Title',
-        duration: '00:04:00',
-      });
-
-      const tutorials = A([tuto1, tuto2, tuto3, tuto4]);
-
-      const scorecard = EmberObject.create({
-        competenceId: 1,
-        isStarted: true,
-        tutorials,
-      });
-
-      this.set('scorecard', scorecard);
-
-      // when
-      await render(hbs`{{scorecard-details scorecard=scorecard}}`);
-
-      // then
-      expect(find('.tutorials')).to.exist;
-      expect(findAll('.tube')).to.have.lengthOf(1);
-      expect(findAll('.tutorial')).to.have.lengthOf(4);
-    });
-
   });
 });
