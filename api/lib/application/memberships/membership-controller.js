936a31eb9904108e4e0dd359d1b8486b0983c7a5
Christophe Charles <ccharles@octo.com>
2019-12-10 15:26:35 +0100

    Add PATCH /api/memberships/{id}

--- api/lib/application/memberships/membership-controller.js
+++ api/lib/application/memberships/membership-controller.js
@@ -1,10 +1,10 @@
-const usecases = require('../../domain/usecases');
+const logger = require('../../infrastructure/logger');
 const membershipSerializer = require('../../infrastructure/serializers/jsonapi/membership-serializer');
+const usecases = require('../../domain/usecases');
 
 module.exports = {
 
   create(request, h) {
-
     const userId = request.payload.data.relationships.user.data.id;
     const organizationId = request.payload.data.relationships.organization.data.id;
 
@@ -12,5 +12,18 @@ module.exports = {
       .then((membership) => {
         return h.response(membershipSerializer.serialize(membership)).created();
       });
+  },
+
+  update(request, h) {
+    const membershipId = request.params.id;
+    const { 'organization-role': organizationRole } = request.payload.data.attributes;
+
+    logger.debug('update membership for id %s with role %s', membershipId, organizationRole);
+
+    return usecases.updateMembershipRole({ membershipId, organizationRole })
+      .then((membership) => {
+        return h.response(membershipSerializer.serialize(membership));
+      });
   }
+
 };
