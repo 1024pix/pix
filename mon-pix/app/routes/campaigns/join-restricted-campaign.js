2af619113e7fcc69dc8b06c892b878cb8eeb5ffa
celineung <celine.ung@zenika.com>
2019-12-10 16:00:20 +0100

    Add title to profile page

--- mon-pix/app/routes/campaigns/join-restricted-campaign.js
+++ mon-pix/app/routes/campaigns/join-restricted-campaign.js
@@ -7,8 +7,10 @@ export default Route.extend(AuthenticatedRouteMixin, {
 
   currentUser: service(),
   session: service(),
+  _isReady: false,
 
   async beforeModel(transition) {
+    this.set('_isReady', false);
     const campaignCode = transition.to.params.campaign_code;
     const student = await this.store.queryRecord('student-user-association', { userId: this.currentUser.user.id, campaignCode });
 
@@ -23,11 +25,21 @@ export default Route.extend(AuthenticatedRouteMixin, {
     return params.campaign_code;
   },
 
+  afterModel() {
+    this.set('_isReady', true);
+  },
+
   setupController(controller) {
     this._super(...arguments);
     if (this.session.data.authenticated.source === 'external') {
       controller.set('firstName', this.currentUser.user.firstName);
       controller.set('lastName', this.currentUser.user.lastName);
+    }
+  },
+
+  actions: {
+    loading() {
+      return this.get('_isReady');
     }
   },
 });
