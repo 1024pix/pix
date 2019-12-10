8ee34dfed202be796a88dba1331292e054d0bbe8
lisequesnel <quesnel.lise@gmail.com>
2019-12-10 16:41:12 +0100

    Add Joi validation for route POST /student-user-associations

--- api/lib/application/student-user-associations/index.js
+++ api/lib/application/student-user-associations/index.js
@@ -1,4 +1,6 @@
 const studentUserAssociationController = require('./student-user-association-controller');
+const Joi = require('@hapi/joi');
+const JSONAPIError = require('jsonapi-serializer').Error;
 
 exports.register = async function(server) {
   server.route([
@@ -7,6 +9,30 @@ exports.register = async function(server) {
       path: '/api/student-user-associations',
       config: {
         handler: studentUserAssociationController.associate,
+        validate: {
+          options: {
+            allowUnknown: true
+          },
+          payload: Joi.object({
+            data: {
+              attributes: {
+                'first-name': Joi.string().empty(Joi.string().regex(/^\s*$/)).required(),
+                'last-name': Joi.string().empty(Joi.string().regex(/^\s*$/)).required(),
+                'birthdate':Joi.date().iso().required(),
+                'campaign-code': Joi.string().empty(Joi.string().regex(/^\s*$/)).required(),
+              },
+            },
+          }),
+          failAction: (request, h) => {
+            const errorHttpStatusCode = 422;
+            const jsonApiError = new JSONAPIError({
+              status: errorHttpStatusCode.toString(),
+              title: 'Unprocessable entity',
+              detail: 'Un des champs saisis n’est pas valide.',
+            });
+            return h.response(jsonApiError).code(errorHttpStatusCode).takeover();
+          }
+        },
         notes: [
           '- **Cette route est restreinte aux utilisateurs authentifiés**\n' +
           '- Elle associe des données de l’utilisateur qui fait la requete, au student de l’organisation'
