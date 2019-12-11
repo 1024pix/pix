c3e59e7133c6789d4e7cb1d606a3d6073959a696
Stephane Bedeau <sbedeau@octo.com>
2019-12-11 18:22:54 +0100

    WIP - Upgrade Ember from 3.10 to 3.12 for Pix App

--- mon-pix/.eslintrc.js
+++ mon-pix/.eslintrc.js
@@ -5,10 +5,7 @@ module.exports = {
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
-    ecmaFeatures: {
-      legacyDecorators: true
-    }
+    sourceType: 'module'
   },
   plugins: [
     'ember',
