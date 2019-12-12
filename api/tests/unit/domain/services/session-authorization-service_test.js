d7e5883684ad19a54d75284f59537a65adc16221
laura-bergoens <laura.bergoens@gmail.com>
2019-12-12 09:17:54 +0100

    Add service to check for session authorization

---
+++ api/tests/unit/domain/services/session-authorization-service_test.js
@@ -0,0 +1,70 @@
+const { expect, sinon } = require('../../../test-helper');
+const sessionAuthorizationService = require('../../../../lib/domain/services/session-authorization-service');
+const sessionRepository = require('../../../../lib/infrastructure/repositories/session-repository');
+const userRepository = require('../../../../lib/infrastructure/repositories/user-repository');
+
+describe('Unit | Service | SessionAuthorizationService', () => {
+
+  describe('#isAuthorizedToAccessSession', () => {
+    const userId = 'userId';
+    const sessionId = 'sessionId';
+
+    it('should exist', () => {
+      expect(sessionAuthorizationService.isAuthorizedToAccessSession).to.exist.and.to.be.a('function');
+    });
+
+    beforeEach(() => {
+      sinon.stub(userRepository, 'hasRolePixMaster');
+      sinon.stub(sessionRepository, 'doesUserHaveCertificationCenterMembershipForSession');
+    });
+
+    context('when user has membership for session', () => {
+
+      it('should return', async () => {
+        // given
+        sessionRepository.doesUserHaveCertificationCenterMembershipForSession.withArgs(userId, sessionId).returns(true);
+
+        // when
+        const isAuthorized = await sessionAuthorizationService.isAuthorizedToAccessSession({ userId, sessionId });
+
+        // then
+        expect(isAuthorized).to.be.true;
+      });
+    });
+
+    context('when user has no membership for session', () => {
+
+      beforeEach(() => {
+        sessionRepository.doesUserHaveCertificationCenterMembershipForSession.withArgs(userId, sessionId).returns(false);
+      });
+
+      context('when user is PixMaster', () => {
+
+        it('should return true', async () => {
+          // given
+          userRepository.hasRolePixMaster.withArgs(userId).returns(true);
+
+          // when
+          const isAuthorized = await sessionAuthorizationService.isAuthorizedToAccessSession({ userId, sessionId });
+
+          // then
+          expect(isAuthorized).to.be.true;
+        });
+      });
+
+      context('when user is not PixMaster', () => {
+
+        it('should return false', async () => {
+          // given
+          userRepository.hasRolePixMaster.withArgs(userId).returns(false);
+
+          // when
+          const isAuthorized = await sessionAuthorizationService.isAuthorizedToAccessSession({ userId, sessionId });
+
+          // then
+          expect(isAuthorized).to.be.false;
+        });
+      });
+    });
+  });
+});
