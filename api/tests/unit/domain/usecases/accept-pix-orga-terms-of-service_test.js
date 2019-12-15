d3427c918e7ad81d393950fb8ccb6f8cb5e706e7
laura-bergoens <laura.bergoens@gmail.com>
2019-12-15 17:11:36 +0100

    Use dedicated methods in toggle usecases
    usecases to update hasSeenAssessmentInstruction,
    pixCertifTermsOfServiceAccepted and pixOrgaTermsOfServiceAccepted
    where all three of them first calling userRepo.get(),
    which triggers 3 DB queries and then proceed in updating.
    We replace this with their dedicated update repo method.

--- api/tests/unit/domain/usecases/accept-pix-orga-terms-of-service_test.js
+++ api/tests/unit/domain/usecases/accept-pix-orga-terms-of-service_test.js
@@ -1,51 +1,25 @@
-const { expect, sinon, domainBuilder } = require('../../../test-helper');
+const { expect, sinon } = require('../../../test-helper');
 const acceptPixOrgaTermsOfService = require('../../../../lib/domain/usecases/accept-pix-orga-terms-of-service');
 const userRepository = require('../../../../lib/infrastructure/repositories/user-repository');
-const User = require('../../../../lib/domain/models/User');
 
 describe('Unit | UseCase | accept-pix-orga-terms-of-service', () => {
 
   beforeEach(() => {
-    sinon.stub(userRepository, 'get');
-    sinon.stub(userRepository, 'updateUser');
+    sinon.stub(userRepository, 'updatePixOrgaTermsOfServiceAcceptedToTrue');
   });
 
-  context('when user has already accepted pix-orga terms of service', () => {
+  it('should accept terms of service of pix-orga', async () => {
+    // given
+    const userId = Symbol('userId');
+    const updatedUser = Symbol('updateduser');
+    userRepository.updatePixOrgaTermsOfServiceAcceptedToTrue.resolves(updatedUser);
 
-    it('should not update terms of service validation', async () => {
-      // given
-      const userId = 1;
-      const user = domainBuilder.buildUser({
-        pixOrgaTermsOfServiceAccepted: true
-      });
-      userRepository.get.withArgs(userId).resolves(user);
+    // when
+    const actualUpdatedUser = await acceptPixOrgaTermsOfService({ userId, userRepository });
 
-      // when
-      await acceptPixOrgaTermsOfService({ userId, userRepository });
-
-      // then
-      expect(userRepository.updateUser).to.not.have.been.called;
-    });
-  });
-
-  context('when user has not accepted pix orga terms of service yet', () => {
-
-    it('should accept terms of service of pix-orga', async () => {
-      // given
-      const userId = 1;
-      const user = domainBuilder.buildUser({
-        pixOrgaTermsOfServiceAccepted: false
-      });
-      userRepository.get.withArgs(userId).resolves(user);
-      const expectedUser = new User({ ...user, pixOrgaTermsOfServiceAccepted: true });
-
-      // when
-      await acceptPixOrgaTermsOfService({ userId, userRepository });
-
-      // then
-      expect(userRepository.updateUser).to.have.been.calledWith(expectedUser);
-    });
-
+    // then
+    expect(userRepository.updatePixOrgaTermsOfServiceAcceptedToTrue).to.have.been.calledWith(userId);
+    expect(actualUpdatedUser).to.equal(updatedUser);
   });
 
 });
