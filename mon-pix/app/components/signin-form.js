f5616059462abcd6dfa5c388eed375155f25b047
Vincent Hardouin <vincent.hardouin@pix.fr>
2019-12-23 17:27:49 +0100

    Allow to signin with username

--- mon-pix/app/components/signin-form.js
+++ mon-pix/app/components/signin-form.js
@@ -3,14 +3,14 @@ import ENV from 'mon-pix/config/environment';
 
 export default Component.extend({
   displayErrorMessage: false,
-  email: '',
+  username: '',
   password: '',
   urlHome: ENV.APP.HOME_HOST,
 
   actions: {
     signin() {
       this.set('displayErrorMessage', false);
-      this.authenticateUser(this.email, this.password)
+      this.authenticateUser(this.username, this.password)
         .catch(() => {
           this.set('displayErrorMessage', true);
         });
