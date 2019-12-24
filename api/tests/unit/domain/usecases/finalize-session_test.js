1029317f597b9743b446b4c820a9f271613fd381
laura-bergoens <laura.bergoens@gmail.com>
2019-12-24 17:17:20 +0100

    Finalize candidates when finalizing session

--- api/tests/unit/domain/usecases/finalize-session_test.js
+++ api/tests/unit/domain/usecases/finalize-session_test.js
@@ -9,17 +9,25 @@ const { SessionAlreadyFinalizedError } = require('../../../../lib/domain/errors'
 
 describe('Unit | UseCase | finalize-session', () => {
 
-  let sessionId, sessionRepository, updatedSession, examinerComment;
+  let sessionId;
+  let updatedSession;
+  let examinerComment;
+  let certificationCandidates;
+  let sessionRepository;
+  let certificationCandidateRepository;
 
   beforeEach(async () => {
     sessionId = 'dummy session id';
     updatedSession = Symbol('updated session');
     examinerComment = 'It was a fine session my dear.';
+    certificationCandidates = Symbol('certificationCandidates');
     sessionRepository = {
       updateStatusAndExaminerComment: sinon.stub(),
       isFinalized: sinon.stub(),
     };
-
+    certificationCandidateRepository = {
+      finalizeAll: sinon.stub(),
+    };
   });
 
   context('When the session status is already finalized', () => {
@@ -30,26 +38,46 @@ describe('Unit | UseCase | finalize-session', () => {
 
     it('should throw a SessionAlreadyFinalizedError error', async () => {
       // when
-      const err = await catchErr(finalizeSession)({ sessionId, examinerComment, sessionRepository });
+      const err = await catchErr(finalizeSession)({
+        sessionId,
+        examinerComment,
+        sessionRepository,
+        certificationCandidates,
+        certificationCandidateRepository
+      });
 
       // then
       expect(err).to.be.instanceOf(SessionAlreadyFinalizedError);
     });
+
   });
 
   context('When the session status is not finalized yet ', () => {
 
     beforeEach(() => {
       sessionRepository.isFinalized.withArgs(sessionId).resolves(false);
-      sessionRepository.updateStatusAndExaminerComment.withArgs({ id: sessionId, status: 'finalized', examinerComment }).resolves(updatedSession);
+      certificationCandidateRepository.finalizeAll.withArgs(certificationCandidates).resolves();
+      sessionRepository.updateStatusAndExaminerComment.withArgs({
+        id: sessionId,
+        status: 'finalized',
+        examinerComment,
+      }).resolves(updatedSession);
     });
 
     it('should return the updated session', async () => {
       // when
-      const res = await finalizeSession({ sessionId, examinerComment, sessionRepository });
+      const res = await finalizeSession({
+        sessionId,
+        examinerComment,
+        sessionRepository,
+        certificationCandidates,
+        certificationCandidateRepository,
+      });
 
       // then
       expect(res).to.deep.equal(updatedSession);
     });
+
   });
+
 });
