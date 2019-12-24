001388db6c4602788ef58340a946f8f013a2c967
laura-bergoens <laura.bergoens@gmail.com>
2019-12-24 16:20:14 +0100

    Setup a model/serializer for certification result

---
+++ api/lib/infrastructure/serializers/jsonapi/certification-result-serializer.js
@@ -0,0 +1,32 @@
+const { Serializer } = require('jsonapi-serializer');
+
+module.exports = {
+
+  serialize(certificationResult) {
+    return new Serializer('results', {
+      attributes: [
+        'assessmentId',
+        'level',
+        'pixScore',
+        'createdAt',
+        'resultCreatedAt',
+        'status',
+        'completedAt',
+        'emitter',
+        'juryId',
+        'commentForCandidate',
+        'commentForOrganization',
+        'commentForJury',
+        'competencesWithMark',
+        'firstName',
+        'lastName',
+        'birthdate',
+        'birthplace',
+        'sessionId',
+        'externalId',
+        'isPublished',
+        'isV2Certification',
+      ],
+    }).serialize(certificationResult);
+  },
+};
