7a4f24244a05706044d8878fdab9d683ab229f7d
laura-bergoens <laura.bergoens@gmail.com>
2019-12-24 09:11:21 +0100

    Add seeds for session and candidates

--- api/db/seeds/data/sessions-builder.js
+++ api/db/seeds/data/sessions-builder.js
@@ -53,4 +53,19 @@ module.exports = function sessionsBuilder({ databaseBuilder }) {
     certificationCenterId: 1,
     examinerComment: 'Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l\'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n\'a pas fait que survivre cinq siècles, mais s\'est aussi adapté à la bureautique informatique, sans que son contenu n\'en soit modifié.',
   });
+
+  databaseBuilder.factory.buildSession({
+    id: 5,
+    certificationCenter: 'Tour Gamma',
+    address: 'Rue Bikub',
+    examiner: 'Mathias',
+    date: '2019-06-10',
+    time: '15:00',
+    description: 'Session',
+    room: 'Salle Tainbanque',
+    accessCode: 'JKL78',
+    status: 'started',
+    certificationCenterId: 1,
+    examinerComment: '',
+  });
 };
