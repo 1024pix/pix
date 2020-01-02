15264ddb5a68a6bf5630bd70cbbd97b1da2095e0
David Riviere <driviere@octo.com>
2020-01-02 12:38:29 +0100

    Use both of mailler service

--- api/lib/infrastructure/sendinblue.js
+++ api/lib/infrastructure/sendinblue.js
@@ -2,40 +2,33 @@ const SibApiV3Sdk = require('sib-api-v3-sdk');
 const defaultClient = SibApiV3Sdk.ApiClient.instance;
 const mailingConfig = require('../config').mailing_sendinblue;
 const API_KEY = mailingConfig.sendinblueApiKey;
+const mailCheck = require('./mail-check');
 
 // Configure API key authorization: api-key
 const apiKey = defaultClient.authentications['api-key'];
 apiKey.apiKey = API_KEY;
 
-// Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
-//apiKey.apiKeyPrefix['api-key'] = "Token"
-function getAccount() {
-  const api = new SibApiV3Sdk.AccountApi();
-  api.getAccount().then((data) => {
-    console.log('API called successfully. Returned data: ' + data);
-  }, function(error) {
-    console.error(error);
-  });
-
-}
-
-function sendEmail() {
-  const apiInstance = new SibApiV3Sdk.SMTPApi();
-  let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
-
-  sendSmtpEmail = {
-    to: [{
-      email: 'david.riviere@pix.fr',
-      name: 'David Riv'
-    }],
+function _formatPayload(options) {
+  return {
+    to: 'david.riviere@pix.fr', //TODO change in options.email
     templateId: 1,
     headers: {
       'content-type': 'application/json',
       'accept': 'application/json',
     }
   };
+}
 
-  apiInstance.sendTransacEmail(sendSmtpEmail).then(function(data) {
+async function sendEmail(options) {
+  if (!mailingConfig) {
+    return Promise.resolve();
+  }
+  const apiInstance = new SibApiV3Sdk.SMTPApi();
+
+  const payload = _formatPayload(options);
+  await mailCheck.checkMail(options.to);
+
+  apiInstance.sendTransacEmail(payload).then(function(data) {
     console.log('API called successfully. Returned data: ' + data);
   }, function(error) {
     console.error(error);
@@ -43,6 +36,5 @@ function sendEmail() {
 }
 
 module.exports = {
-  getAccount,
   sendEmail,
 };
