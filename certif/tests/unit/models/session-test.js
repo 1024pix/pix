d582d28691ef99d7e9293808d69bff2c39b81675
David Hérault <dherault@gmail.com>
2020-01-02 13:12:53 +0100

    Add session status column to session list

--- certif/tests/unit/models/session-test.js
+++ certif/tests/unit/models/session-test.js
@@ -18,4 +18,19 @@ module('Unit | Model | session', function(hooks) {
     }));
     assert.equal(model.id, 123);
   });
+
+  test('it should return the correct displayName', function(assert) {
+    const store = this.owner.lookup('service:store');
+    const model1 = run(() => store.createRecord('session', {
+      id: 123,
+      status: 'started',
+    }));
+    const model2 = run(() => store.createRecord('session', {
+      id: 1234,
+      status: 'finalized',
+    }));
+
+    assert.equal(model1.displayStatus, 'Prête');
+    assert.equal(model2.displayStatus, 'Finalisée');
+  });
 });
