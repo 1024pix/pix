151ca34aefa27ca3781ce5c41d47c1c54ab6a005
lisequesnel <quesnel.lise@gmail.com>
2020-01-03 13:38:26 +0100

    Create usecase findAssociationPossibilities

--- api/lib/domain/usecases/index.js
+++ api/lib/domain/usecases/index.js
@@ -79,6 +79,7 @@ module.exports = injectDependencies({
   finalizeSession: require('./finalize-session'),
   findAnswerByChallengeAndAssessment: require('./find-answer-by-challenge-and-assessment'),
   findAssociationBetweenUserAndOrganizationStudent: require('./find-association-between-user-and-organization-student'),
+  findAssociationPossibilities: require('./find-association-possibilities'),
   findCampaignParticipationsRelatedToAssessment: require('./find-campaign-participations-related-to-assessment'),
   findCampaignParticipationsRelatedToUser: require('./find-campaign-participations-related-to-user'),
   findCampaignParticipationsWithResults: require('./find-campaign-participations-with-results'),
