d56b7c255847c539d9e7d5bcf604b7d036fe8af5
lisequesnel <quesnel.lise@gmail.com>
2020-01-03 17:24:09 +0100

    Add createAndAssociateUserToStudent controller

--- api/lib/application/student-dependent-users/student-dependent-user-controller.js
+++ api/lib/application/student-dependent-users/student-dependent-user-controller.js
@@ -1,6 +1,20 @@
+const usecases = require('../../domain/usecases');
+const userSerializer = require('../../infrastructure/serializers/jsonapi/user-serializer');
+
 module.exports = {
 
-  createAndAssociateUserToStudent() {
-    return null;
+  async createAndAssociateUserToStudent(request, h) {
+    const payload = request.payload.data.attributes;
+    const userAttributes = {
+      firstName: payload['first-name'],
+      lastName: payload['last-name'],
+      birthdate: payload['birthdate'],
+      email: payload.email,
+      password: payload.password,
+    };
+
+    const createdUser = await usecases.createAndAssociateUserToStudent({ userAttributes, campaignCode: payload['campaign-code'] });
+
+    return h.response(userSerializer.serialize(createdUser)).created();
   }
 };
