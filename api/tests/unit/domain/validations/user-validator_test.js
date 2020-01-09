88e2071d4ccfb5d21d0864fc752d9f28b2cff6d5
lisequesnel <quesnel.lise@gmail.com>
2020-01-09 12:37:08 +0100

    Change user-validator implementation by adding username and optional cgu

--- api/tests/unit/domain/validations/user-validator_test.js
+++ api/tests/unit/domain/validations/user-validator_test.js
@@ -1,4 +1,4 @@
-const { expect } = require('../../../test-helper');
+const { expect, catchErr } = require('../../../test-helper');
 const userValidator = require('../../../../lib/domain/validators/user-validator');
 const { EntityValidationError } = require('../../../../lib/domain/errors');
 const User = require('../../../../lib/domain/models/User');
@@ -15,174 +15,300 @@ describe('Unit | Domain | Validators | user-validator', function() {
 
   let user;
 
-  beforeEach(() => {
-    user = new User({
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'john.doe@example.net',
-      password: 'Password1234',
-      cgu: true,
-    });
-  });
-
   describe('#validate', () => {
 
-    context('when validation is successful', () => {
+    context('when validation is for normal user', () => {
 
-      it('should resolve (with no value) when validation is successful', () => {
-        // when
-        const promise = userValidator.validate(user);
+      beforeEach(() => {
+        user = new User({
+          firstName: 'John',
+          lastName: 'Doe',
+          email: 'john.doe@example.net',
+          password: 'Password1234',
+          cgu: true,
+        });
+      });
 
-        // then
-        return expect(promise).to.be.fulfilled;
+      context('when validation is successful', () => {
+
+        it('should resolve (with no value) when validation is successful', () => {
+          // when
+          const promise = userValidator.validate({ user });
+
+          // then
+          return expect(promise).to.be.fulfilled;
+        });
+      });
+
+      context('when user data validation fails', () => {
+
+        it('should reject with error when user is undefined', async () => {
+          // given
+          const expectedError = {
+            attribute: undefined,
+            message: 'Aucun champ n\'est renseigné.'
+          };
+
+          // when
+          const entityValidationErrors = await catchErr(userValidator.validate)({ user: undefined });
+
+          // then
+          _assertErrorMatchesWithExpectedOne(entityValidationErrors, expectedError);
+        });
+
+        it('should reject with error on field "first name" when first name is missing', async () => {
+          // given
+          const expectedError = {
+            attribute: 'firstName',
+            message: 'Votre prénom n’est pas renseigné.'
+          };
+          user.firstName = MISSING_VALUE;
+
+          // when
+          const entityValidationErrors = await catchErr(userValidator.validate)({ user });
+
+          // then
+          _assertErrorMatchesWithExpectedOne(entityValidationErrors, expectedError);
+        });
+
+        it('should reject with error on field "last name" when last name is missing', async () => {
+          // given
+          const expectedError = {
+            attribute: 'lastName',
+            message: 'Votre nom n’est pas renseigné.'
+          };
+          user.lastName = MISSING_VALUE;
+
+          // when
+          const entityValidationErrors = await catchErr(userValidator.validate)({ user });
+
+          // then
+          _assertErrorMatchesWithExpectedOne(entityValidationErrors, expectedError);
+        });
+
+        it('should reject with error on field "password" when password is missing', async () => {
+          // given
+          const expectedError = {
+            attribute: 'password',
+            message: 'Votre mot de passe n’est pas renseigné.'
+          };
+          user.password = MISSING_VALUE;
+
+          // when
+          const entityValidationErrors = await catchErr(userValidator.validate)({ user });
+
+          // then
+          _assertErrorMatchesWithExpectedOne(entityValidationErrors, expectedError);
+        });
+
+        it('should reject with error on field "password" when password is invalid', async () => {
+          // given
+          const expectedError = {
+            attribute: 'password',
+            message: 'Votre mot de passe doit contenir 8 caractères au minimum et comporter au moins une majuscule, une minuscule et un chiffre.'
+          };
+          user.password = 'invalid';
+
+          // when
+          const entityValidationErrors = await catchErr(userValidator.validate)({ user });
+
+          // then
+          _assertErrorMatchesWithExpectedOne(entityValidationErrors, expectedError);
+        });
+
+        it('should reject with error on field "cgu" when cgu is false', async () => {
+          // given
+          const expectedError = {
+            attribute: 'cgu',
+            message: 'Vous devez accepter les conditions d’utilisation de Pix pour créer un compte.'
+          };
+          user.cgu = 'false';
+
+          // when
+          const entityValidationErrors = await catchErr(userValidator.validate)({ user });
+
+          // then
+          _assertErrorMatchesWithExpectedOne(entityValidationErrors, expectedError);
+        });
+
+        it('should reject with error on field "email" when email is missing', async () => {
+          // given
+          const expectedError = {
+            attribute: 'email',
+            message: 'Votre adresse e-mail n’est pas renseignée.'
+          };
+          user.email = MISSING_VALUE;
+
+          // when
+          const entityValidationErrors = await catchErr(userValidator.validate)({ user });
+
+          // then
+          _assertErrorMatchesWithExpectedOne(entityValidationErrors, expectedError);
+        });
+
+        it('should reject with error on field "email" when email is invalid', async () => {
+          // given
+          const expectedError = {
+            attribute: 'email',
+            message: 'Votre adresse e-mail n’est pas correcte.'
+          };
+          user.email = 'invalid_email';
+
+          // when
+          const entityValidationErrors = await catchErr(userValidator.validate)({ user });
+
+          // then
+          _assertErrorMatchesWithExpectedOne(entityValidationErrors, expectedError);
+        });
+
+        it('should reject with errors on all fields (but only once by field) when all fields are missing', async () => {
+          // given
+          user = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+          };
+
+          // when
+          const entityValidationErrors = await catchErr(userValidator.validate)({ user });
+
+          // then
+          expect(entityValidationErrors.invalidAttributes).to.have.lengthOf(5);
+        });
       });
     });
 
-    context('when user data validation fails', () => {
+    context('when validation is for student dependent user', () => {
 
-      it('should reject with error on field "first name" when first name is missing', async () => {
-        // given
-        const expectedError = {
-          attribute: 'firstName',
-          message: 'Votre prénom n’est pas renseigné.'
-        };
-        user.firstName = MISSING_VALUE;
+      const cguRequired = false;
 
-        try {
-          // when
-          await userValidator.validate(user);
-
-        } catch (entityValidationErrors) {
-          // then
-          _assertErrorMatchesWithExpectedOne(entityValidationErrors, expectedError);
-        }
+      beforeEach(() => {
+        user = new User({
+          firstName: 'John',
+          lastName: 'Doe',
+          username: 'john.doe1212',
+          password: 'Password1234',
+        });
       });
 
-      it('should reject with error on field "last name" when last name is missing', async () => {
-        // given
-        const expectedError = {
-          attribute: 'lastName',
-          message: 'Votre nom n’est pas renseigné.'
-        };
-        user.lastName = MISSING_VALUE;
+      context('when validation is successful', () => {
 
-        try {
+        it('should resolve (with no value) when validation is successful', () => {
           // when
-          await userValidator.validate(user);
+          const promise = userValidator.validate({ user, cguRequired });
 
-        } catch (entityValidationErrors) {
           // then
-          _assertErrorMatchesWithExpectedOne(entityValidationErrors, expectedError);
-        }
+          return expect(promise).to.be.fulfilled;
+        });
       });
 
-      it('should reject with error on field "password" when password is missing', async () => {
-        // given
-        const expectedError = {
-          attribute: 'password',
-          message: 'Votre mot de passe n’est pas renseigné.'
-        };
-        user.password = MISSING_VALUE;
+      context('when user data validation fails', () => {
 
-        try {
+        it('should reject with error when user is undefined', async () => {
+          // given
+          const expectedError = {
+            attribute: undefined,
+            message: 'Aucun champ n\'est renseigné.'
+          };
+
           // when
-          await userValidator.validate(user);
+          const entityValidationErrors = await catchErr(userValidator.validate)({ user: undefined, cguRequired });
 
-        } catch (entityValidationErrors) {
           // then
           _assertErrorMatchesWithExpectedOne(entityValidationErrors, expectedError);
-        }
-      });
+        });
 
-      it('should reject with error on field "password" when password is invalid', async () => {
-        // given
-        const expectedError = {
-          attribute: 'password',
-          message: 'Votre mot de passe doit contenir 8 caractères au minimum et comporter au moins une majuscule, une minuscule et un chiffre.'
-        };
-        user.password = 'invalid';
+        it('should reject with error on field "first name" when first name is missing', async () => {
+          // given
+          const expectedError = {
+            attribute: 'firstName',
+            message: 'Votre prénom n’est pas renseigné.'
+          };
+          user.firstName = MISSING_VALUE;
 
-        try {
           // when
-          await userValidator.validate(user);
+          const entityValidationErrors = await catchErr(userValidator.validate)({ user, cguRequired });
 
-        } catch (entityValidationErrors) {
           // then
           _assertErrorMatchesWithExpectedOne(entityValidationErrors, expectedError);
-        }
-      });
+        });
 
-      it('should reject with error on field "cgu" when cgu is false', async () => {
-        // given
-        const expectedError = {
-          attribute: 'cgu',
-          message: 'Vous devez accepter les conditions d’utilisation de Pix pour créer un compte.'
-        };
-        user.cgu = 'false';
+        it('should reject with error on field "last name" when last name is missing', async () => {
+          // given
+          const expectedError = {
+            attribute: 'lastName',
+            message: 'Votre nom n’est pas renseigné.'
+          };
+          user.lastName = MISSING_VALUE;
 
-        try {
           // when
-          await userValidator.validate(user);
+          const entityValidationErrors = await catchErr(userValidator.validate)({ user, cguRequired });
 
-        } catch (entityValidationErrors) {
           // then
           _assertErrorMatchesWithExpectedOne(entityValidationErrors, expectedError);
-        }
-      });
+        });
 
-      it('should reject with error on field "email" when email is missing', async () => {
-        // given
-        const expectedError = {
-          attribute: 'email',
-          message: 'Votre adresse e-mail n’est pas renseignée.'
-        };
-        user.email = MISSING_VALUE;
+        it('should reject with error on field "password" when password is missing', async () => {
+          // given
+          const expectedError = {
+            attribute: 'password',
+            message: 'Votre mot de passe n’est pas renseigné.'
+          };
+          user.password = MISSING_VALUE;
 
-        try {
           // when
-          await userValidator.validate(user);
+          const entityValidationErrors = await catchErr(userValidator.validate)({ user, cguRequired });
 
-        } catch (entityValidationErrors) {
           // then
           _assertErrorMatchesWithExpectedOne(entityValidationErrors, expectedError);
-        }
-      });
+        });
 
-      it('should reject with error on field "email" when email is invalid', async () => {
-        // given
-        const expectedError = {
-          attribute: 'email',
-          message: 'Votre adresse e-mail n’est pas correcte.'
-        };
-        user.email = 'invalid_email';
+        it('should reject with error on field "password" when password is invalid', async () => {
+          // given
+          const expectedError = {
+            attribute: 'password',
+            message: 'Votre mot de passe doit contenir 8 caractères au minimum et comporter au moins une majuscule, une minuscule et un chiffre.'
+          };
+          user.password = 'invalid';
 
-        try {
           // when
-          await userValidator.validate(user);
+          const entityValidationErrors = await catchErr(userValidator.validate)({ user, cguRequired });
 
-        } catch (entityValidationErrors) {
           // then
           _assertErrorMatchesWithExpectedOne(entityValidationErrors, expectedError);
-        }
-      });
+        });
 
-      it('should reject with errors on all fields (but only once by field) when all fields are missing', async () => {
-        // given
-        user = {
-          firstName: '',
-          lastName: '',
-          email: '',
-          password: '',
-        };
+        it('should reject with error on field "username" when username is missing', async () => {
+          // given
+          const expectedError = {
+            attribute: 'username',
+            message: 'Votre identifiant n’est pas renseigné.'
+          };
+          user.username = MISSING_VALUE;
 
-        try {
           // when
-          await userValidator.validate(user);
+          const entityValidationErrors = await catchErr(userValidator.validate)({ user, cguRequired });
 
-        } catch (entityValidationErrors) {
           // then
-          expect(entityValidationErrors.invalidAttributes).to.have.lengthOf(5);
-        }
+          _assertErrorMatchesWithExpectedOne(entityValidationErrors, expectedError);
+        });
+
+        it('should reject with errors on all fields (but only once by field) when all fields are missing', async () => {
+          // given
+          user = {
+            firstName: '',
+            lastName: '',
+            username: '',
+            password: '',
+          };
+
+          // when
+          const entityValidationErrors = await catchErr(userValidator.validate)({ user, cguRequired });
+
+          // then
+          expect(entityValidationErrors.invalidAttributes).to.have.lengthOf(4);
+        });
       });
     });
   });
