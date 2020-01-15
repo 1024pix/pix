6cd9036ee35aea584382097dba7b650ace8b480e
laura-bergoens <laura.bergoens@gmail.com>
2020-01-15 14:00:10 +0100

    Change routes to native JS Classes
    Except for the routes with mixin from ember-simple-auth

--- certif/app/routes/authenticated/sessions/finalize.js
+++ certif/app/routes/authenticated/sessions/finalize.js
@@ -3,24 +3,21 @@ import { inject as service } from '@ember/service';
 
 import config from '../../../config/environment';
 
-export default Route.extend({
-  notifications: service('notifications'),
+export default class AuthenticatedSessionsFinalizeRoute extends Route {
+  @service notifications;
 
   async model({ session_id }) {
-    return this.store.findRecord('session', session_id, { reload: true })
-      .then(async (session) => {
-        await session.certificationCandidates;
-        return session;
-      });
-  },
+    const session = await this.store.findRecord('session', session_id, { reload: true });
+    await session.certificationCandidates;
+    return session;
+  }
 
   async afterModel(model, transition) {
     if (model.isFinalized) {
       const { autoClear, clearDuration } = config.notifications;
-      this.notifications
-        .error('Cette session a déjà été finalisée.', { autoClear, clearDuration });
+      this.notifications.error('Cette session a déjà été finalisée.', { autoClear, clearDuration });
 
       transition.abort();
     }
-  },
-});
+  }
+}
