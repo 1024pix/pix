17db9bcc6da1cc8578c4cd10116ea5530b42bd37
laura-bergoens <laura.bergoens@gmail.com>
2020-01-15 13:26:13 +0100

    Change DateOnly Transform into JS Native class

--- certif/app/transforms/date-only.js
+++ certif/app/transforms/date-only.js
@@ -1,14 +1,15 @@
 import Transform from '@ember-data/serializer/transform';
 
-export default Transform.extend({
-  serialize: function(date) {
+export default class DateOnlyTransform extends Transform {
+  serialize(date) {
     return date;
-  },
-  deserialize: function(date) {
+  }
+
+  deserialize(date) {
     const dateRegex = '^[0-9]{4}-[0-9]{2}-[0-9]{2}$';
     if (date && date.search(dateRegex) === 0) {
       return date;
     }
     return null;
   }
-});
+}
