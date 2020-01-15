24c3e66d9e0f8652996a1dcc73ea6f97f79633ed
laura-bergoens <laura.bergoens@gmail.com>
2020-01-15 11:06:21 +0100

    Change authenticator OAuth2 to native JS class

--- certif/app/authenticators/oauth2.js
+++ certif/app/authenticators/oauth2.js
@@ -1,7 +1,7 @@
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
 import ENV from 'pix-certif/config/environment';
 
-export default OAuth2PasswordGrant.extend({
-  serverTokenEndpoint: `${ENV.APP.API_HOST}/api/token`,
-  serverTokenRevocationEndpoint: `${ENV.APP.API_HOST}/api/revoke`,
-});
+export default class OAuth2Authenticator extends OAuth2PasswordGrant {
+  serverTokenEndpoint = `${ENV.APP.API_HOST}/api/token`;
+  serverTokenRevocationEndpoint = `${ENV.APP.API_HOST}/api/revoke`;
+}
