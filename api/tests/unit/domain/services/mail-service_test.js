61593fafa9752f9b6ae8e24718192dd7d427718a
David Riviere <driviere@octo.com>
2020-01-16 09:25:08 +0100

    Upgrade mail-service test to saync/await

--- api/tests/unit/domain/services/mail-service_test.js
+++ api/tests/unit/domain/services/mail-service_test.js
@@ -17,22 +17,20 @@ describe('Unit | Service | MailService', () => {
         mailJetSendEmailStub = sinon.stub(mailJet, 'sendEmail').resolves();
       });
 
-      it('should use mailJet to send an email', () => {
+      it('should use mailJet to send an email', async () => {
         // given
         const email = 'text@example.net';
 
         // when
-        const promise = mailService.sendAccountCreationEmail(email);
+        await mailService.sendAccountCreationEmail(email);
 
         // then
-        return promise.then(() => {
-          sinon.assert.calledWith(mailJetSendEmailStub, {
-            to: email,
-            template: 'test-account-creation-template-id',
-            from: 'ne-pas-repondre@pix.fr',
-            fromName: 'PIX - Ne pas répondre',
-            subject: 'Création de votre compte PIX'
-          });
+        expect(mailJetSendEmailStub).to.have.been.calledWith({
+          to: email,
+          template: 'test-account-creation-template-id',
+          from: 'ne-pas-repondre@pix.fr',
+          fromName: 'PIX - Ne pas répondre',
+          subject: 'Création de votre compte PIX'
         });
       });
     });
@@ -46,22 +44,20 @@ describe('Unit | Service | MailService', () => {
         sendinblueSendEmailStub = sinon.stub(sendinblue, 'sendEmail').resolves();
       });
 
-      it('should use mailJet to send an email', () => {
+      it('should use mailJet to send an email', async () => {
         // given
         const email = 'text@example.net';
 
         // when
-        const promise = mailService.sendAccountCreationEmail(email);
+        await mailService.sendAccountCreationEmail(email);
 
         // then
-        return promise.then(() => {
-          sinon.assert.calledWith(sendinblueSendEmailStub, {
-            to: email,
-            template: 'test-account-creation-template-id',
-            from: 'ne-pas-repondre@pix.fr',
-            fromName: 'PIX - Ne pas répondre',
-            subject: 'Création de votre compte PIX'
-          });
+        expect(sendinblueSendEmailStub).to.have.been.calledWith({
+          to: email,
+          template: 'test-account-creation-template-id',
+          from: 'ne-pas-repondre@pix.fr',
+          fromName: 'PIX - Ne pas répondre',
+          subject: 'Création de votre compte PIX'
         });
       });
     });
@@ -79,27 +75,26 @@ describe('Unit | Service | MailService', () => {
       });
 
       describe('when provided passwordResetDemandBaseUrl is not production', () => {
-        it('should call Mailjet with a sub-domain prefix', () => {
+
+        it('should call Mailjet with a sub-domain prefix', async () => {
           // given
           const email = 'text@example.net';
           const fakeTemporaryKey = 'token';
           const passwordResetDemandBaseUrl = 'http://dev.pix.fr';
 
           // when
-          const promise = mailService.sendResetPasswordDemandEmail(email, passwordResetDemandBaseUrl, fakeTemporaryKey);
+          await mailService.sendResetPasswordDemandEmail(email, passwordResetDemandBaseUrl, fakeTemporaryKey);
 
           // then
-          return promise.then(() => {
-            sinon.assert.calledWith(mailJetSendEmailStub, {
-              to: email,
-              template: 'test-password-reset-template-id',
-              from: 'ne-pas-repondre@pix.fr',
-              fromName: 'PIX - Ne pas répondre',
-              subject: 'Demande de réinitialisation de mot de passe PIX',
-              variables: {
-                resetUrl: `${passwordResetDemandBaseUrl}/changer-mot-de-passe/${fakeTemporaryKey}`
-              }
-            });
+          expect(mailJetSendEmailStub).to.have.been.calledWith({
+            to: email,
+            template: 'test-password-reset-template-id',
+            from: 'ne-pas-repondre@pix.fr',
+            fromName: 'PIX - Ne pas répondre',
+            subject: 'Demande de réinitialisation de mot de passe PIX',
+            variables: {
+              resetUrl: `${passwordResetDemandBaseUrl}/changer-mot-de-passe/${fakeTemporaryKey}`
+            }
           });
         });
       });
@@ -115,27 +110,26 @@ describe('Unit | Service | MailService', () => {
       });
 
       describe('when provided passwordResetDemandBaseUrl is not production', () => {
-        it('should call Sendinblue with a sub-domain prefix', () => {
+
+        it('should call Sendinblue with a sub-domain prefix', async () => {
           // given
           const email = 'text@example.net';
           const fakeTemporaryKey = 'token';
           const passwordResetDemandBaseUrl = 'http://dev.pix.fr';
 
           // when
-          const promise = mailService.sendResetPasswordDemandEmail(email, passwordResetDemandBaseUrl, fakeTemporaryKey);
+          await mailService.sendResetPasswordDemandEmail(email, passwordResetDemandBaseUrl, fakeTemporaryKey);
 
           // then
-          return promise.then(() => {
-            sinon.assert.calledWith(sendinblueSendEmailStub, {
-              to: email,
-              template: 'test-password-reset-template-id',
-              from: 'ne-pas-repondre@pix.fr',
-              fromName: 'PIX - Ne pas répondre',
-              subject: 'Demande de réinitialisation de mot de passe PIX',
-              variables: {
-                resetUrl: `${passwordResetDemandBaseUrl}/changer-mot-de-passe/${fakeTemporaryKey}`
-              }
-            });
+          expect(sendinblueSendEmailStub).to.have.been.calledWith({
+            to: email,
+            template: 'test-password-reset-template-id',
+            from: 'ne-pas-repondre@pix.fr',
+            fromName: 'PIX - Ne pas répondre',
+            subject: 'Demande de réinitialisation de mot de passe PIX',
+            variables: {
+              resetUrl: `${passwordResetDemandBaseUrl}/changer-mot-de-passe/${fakeTemporaryKey}`
+            }
           });
         });
       });
@@ -204,7 +198,7 @@ describe('Unit | Service | MailService', () => {
         });
 
         // then
-        sinon.assert.calledWith(sendinblueSendEmailStub, {
+        expect(sendinblueSendEmailStub).to.have.been.calledWith({
           to: email,
           template: 'test-organization-invitation-demand-template-id',
           from: 'ne-pas-repondre@pix.fr',
