1b3582510d9632500a1222279a17400e0fd7266b
Mathieu Gilet <mathieu.gilet@zenika.com>
2020-01-16 13:50:33 +0100

    student-dependent-user-controller_test improved

--- api/tests/integration/application/student-dependent-users/student-dependent-user-controller_test.js
+++ api/tests/integration/application/student-dependent-users/student-dependent-user-controller_test.js
@@ -23,16 +23,14 @@ describe('Integration | Application | Student-dependent-users | student-dependen
   describe('#createAndAssociateUserToStudent', () => {
 
     const payload = { data: { attributes: {} } };
-    const campaignCode = 'CODE';
 
     beforeEach(() => {
       payload.data.attributes = {
         'first-name': 'Robert',
         'last-name': 'Smith',
         'birthdate': '2012-12-12',
-        'campaign-code': campaignCode,
+        'campaign-code': 'RESTRICTD',
         'password': 'P@ssw0rd',
-        email: undefined,
       };
     });
 
@@ -42,10 +40,27 @@ describe('Integration | Application | Student-dependent-users | student-dependen
 
       context('When email is used', () => {
 
-        payload.data.attributes.email = 'toto@example.net';
+        it('should return an HTTP response with status code 201', async () => {
+          // given
+          payload.data.attributes.email = 'toto@example.net';
+          payload.data.attributes['with-username'] = false;
+          usecases.createAndAssociateUserToStudent.resolves(createdUser);
+
+          // when
+          const response = await httpTestServer.request('POST', '/api/student-dependent-users', payload);
+
+          // then
+          expect(response.statusCode).to.equal(201);
+          expect(response.result.data.id).to.equal(createdUser.id.toString());
+        });
+      });
+
+      context('When username is used', () => {
 
         it('should return an HTTP response with status code 201', async () => {
           // given
+          payload.data.attributes.username = 'robert.smith1212';
+          payload.data.attributes['with-username'] = true;
           usecases.createAndAssociateUserToStudent.resolves(createdUser);
 
           // when
