817d290cf374efdcd9a689822dcd0aac900e5225
Mathieu Gilet <mathieu.gilet@zenika.com>
2020-01-17 16:48:19 +0100

    Error management corrected in user-existence-verification

--- api/lib/application/preHandlers/user-existence-verification.js
+++ api/lib/application/preHandlers/user-existence-verification.js
@@ -1,5 +1,4 @@
 const userRepository = require('../../../lib/infrastructure/repositories/user-repository');
-const User = require('../../infrastructure/data/user');
 const errorSerializer = require('../../../lib/infrastructure/serializers/jsonapi/validation-error-serializer');
 const { UserNotFoundError } = require('../../domain/errors');
 
@@ -8,7 +7,7 @@ module.exports = {
     return userRepository
       .get(parseInt(request.params.id))
       .catch((err) => {
-        if (err instanceof User.NotFoundError) {
+        if (err instanceof UserNotFoundError) {
           const serializedError = errorSerializer.serialize(new UserNotFoundError().getErrorMessage());
           return h.response(serializedError).code(404).takeover();
         }
