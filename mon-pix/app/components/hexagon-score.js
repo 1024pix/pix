4e09f8fd848b90692b42b6cbfee5eb85703d5f90
Melanie B <mboudard@octo.com>
2020-01-22 10:59:30 +0100

    Add cross to close information for mobile

--- mon-pix/app/components/hexagon-score.js
+++ mon-pix/app/components/hexagon-score.js
@@ -3,9 +3,18 @@ import { computed } from '@ember/object';
 import Component from '@ember/component';
 
 export default Component.extend({
-  pixScore: null,
+  displayHelp: 'hexagon-score__information--hide',
 
   score: computed('pixScore', function() {
     return (isNone(this.pixScore) || this.pixScore === 0) ? '–' : Math.floor(this.pixScore);
-  })
+  }),
+
+  actions: {
+    hideHelp: function() {
+      this.set('displayHelp', 'hexagon-score__information--hide');
+    },
+    showHelp: function() {
+      this.set('displayHelp', 'hexagon-score__information--show');
+    }
+  }
 });
