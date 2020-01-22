676c63ef4f362602af907de53373de8333e68f5c
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-01-22 17:02:38 +0100

    Compute lead times of all commits

--- scripts/compute-lead-times.js
+++ scripts/compute-lead-times.js
@@ -1,12 +1,32 @@
 const moment = require('moment');
 
-function computeLeadTimes(tagDate, commitDates) {
-  latestCommitDate = commitDates[0];
-  const leadTime = moment(tagDate).diff(moment(latestCommitDate), 'days');
+///
+// TODO
+//
+// Inputs:
+// $ git show --pretty=format:"%ad" --summary v2.105.0 | tail -1 # tag date
+// $ git log  --pretty=format:"%ad" v2.104.0...v2.105.0          # commit dates
+//
+// Outputs:
+// - convert to csv
 
-  return [{leadTime}];
+function computeLeadTimes(tagDate, commitDateStrings) {
+  const tagMoment = moment(new Date(tagDate));
+
+  return commitDateStrings
+    .map((dateString) => new Date(dateString))
+    .map((commitDate) => moment(commitDate))
+    .map((commitMoment) => {
+      return {
+        leadTime: _computeLeadTime(tagMoment, commitMoment)
+      };
+    });
+}
+
+function _computeLeadTime(tagMoment, commitMoment) {
+  return tagMoment.diff(commitMoment, 'days');
 }
 
 module.exports = {
   computeLeadTimes
-}
+};
