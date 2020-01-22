222d610b977f38108588f49d506c68be5132f562
Jonas Grillères <grilleres.j@gmail.com>
2020-01-22 16:40:57 +0100

    Remove bootstrap.js from ember-cli-build

--- mon-pix/ember-cli-build.js
+++ mon-pix/ember-cli-build.js
@@ -23,7 +23,6 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  app.import('node_modules/bootstrap/dist/js/bootstrap.js');
   app.import('node_modules/bootstrap/dist/css/bootstrap.css');
 
   return app.toTree();
