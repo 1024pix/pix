d451ae708a87760e0f3434561242c78d1c2c377e
Guillaume Lagorce <guillaume@hey-gul.com>
2020-01-22 13:27:20 +0100

    Add a function to list only Pix competences

--- api/tests/unit/infrastructure/repositories/competence-repository_test.js
+++ api/tests/unit/infrastructure/repositories/competence-repository_test.js
@@ -23,6 +23,7 @@ describe('Unit | Repository | competence-repository', () => {
     description: 'Competence description 1',
     areaId: 'recArea',
     courseId: 'recCourse1',
+    origin: 'Pix',
     skillIds: ['recSkill1', 'recSkill2'],
   };
 
@@ -32,13 +33,25 @@ describe('Unit | Repository | competence-repository', () => {
     index: '1.2',
     description: 'Competence description 2',
     areaId: 'recArea',
+    origin: 'Pix',
+    courseId: 'recCourse2',
+    skillIds: [],
+  };
+
+  const nonPixCompetenceData = {
+    id: 'recNonPixCompetence',
+    name: 'NonPixCompetenceName',
+    index: '4.1',
+    description: 'Non Pix Competence description',
+    areaId: 'recArea',
+    origin: 'Autre que Pix',
     courseId: 'recCourse2',
     skillIds: [],
   };
 
   beforeEach(() => {
     sinon.stub(areaDatasource, 'list').resolves([areaData]);
-    sinon.stub(competenceDatasource, 'list').resolves([competenceData2, competenceData1]);
+    sinon.stub(competenceDatasource, 'list').resolves([competenceData2, competenceData1, nonPixCompetenceData]);
     sinon.stub(competenceDatasource, 'get').withArgs('recCompetence1').resolves(competenceData1);
   });
 
@@ -47,6 +60,23 @@ describe('Unit | Repository | competence-repository', () => {
     it('should return domain Competence objects sorted by index', () => {
       // when
       const fetchedCompetences = competenceRepository.list();
+
+      // then
+      return fetchedCompetences.then((competences) => {
+        expect(competences).to.have.lengthOf(3);
+        expect(competences[0]).to.be.an.instanceOf(Competence);
+        expect(competences[0].index).to.equal('1.1');
+        expect(competences[1].index).to.equal('1.2');
+        expect(competences[2].index).to.equal('4.1');
+      });
+    });
+  });
+
+  describe('#listPixCompetencesOnly', () => {
+
+    it('should return Pix only domain Competence objects sorted by index', () => {
+      // when
+      const fetchedCompetences = competenceRepository.listPixCompetencesOnly();
 
       // then
       return fetchedCompetences.then((competences) => {
@@ -71,6 +101,7 @@ describe('Unit | Repository | competence-repository', () => {
         name: 'Mener une recherche d’information',
         description: 'Competence description 1',
         courseId: 'recCourse1',
+        origin: 'Pix',
         skills: ['recSkill1', 'recSkill2'],
         area: new Area({
           id: 'recArea',
@@ -98,6 +129,7 @@ describe('Unit | Repository | competence-repository', () => {
         index: '1.1',
         name: 'Mener une recherche d’information',
         courseId: 'recCourse',
+        origin: 'Pix',
         skills: ['recSkill1', 'recSkill2'],
         area: new Area({
           id: 'recArea',
