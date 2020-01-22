318e48ca0f1e61951be0dfac27952b43a17d8d4b
laura-bergoens <laura.bergoens@gmail.com>
2020-01-22 09:45:32 +0100

    Replace homemade date input for existing component

--- certif/tests/integration/components/certification-candidate-in-staging-item-test.js
+++ certif/tests/integration/components/certification-candidate-in-staging-item-test.js
@@ -61,9 +61,7 @@ module('Integration | Component | certification-candidate-in-staging-item', func
       await fillIn('[data-test-id="panel-candidate__birthCity__add-staging"] > div > input', 'MaVille');
       await fillIn('[data-test-id="panel-candidate__birthProvinceCode__add-staging"] > div > input', 'MonDép');
       await fillIn('[data-test-id="panel-candidate__birthCountry__add-staging"] > div > input', 'MonPays');
-      await fillIn('[data-test-id="date-input__day"]', '04');
-      await fillIn('[data-test-id="date-input__month"]', '04');
-      await fillIn('[data-test-id="date-input__year"]', '1994');
+      await fillIn('[data-test-id="panel-candidate__birthdate__add-staging"] > div > input', '01021990');
     });
 
     test('it render a clickable save button', async function(assert) {
