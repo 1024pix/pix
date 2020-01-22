be7c25f8f8a3cf1b44641901ea6fd12619e301f2
laura-bergoens <laura.bergoens@gmail.com>
2020-01-22 09:31:32 +0100

    Setup isCandidateValid computed property

--- certif/app/components/certification-candidate-in-staging-item.js
+++ certif/app/components/certification-candidate-in-staging-item.js
@@ -1,5 +1,17 @@
+import moment from 'moment';
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   tagName: 'tr',
+
+  isCandidateDataValid: computed('candidateData.{firstName,lastName,birthCountry,birthProvinceCode,birthCity,birthdate}', function() {
+    return this.candidateData.firstName &&
+      this.candidateData.lastName &&
+      this.candidateData.birthCity &&
+      this.candidateData.birthProvinceCode &&
+      this.candidateData.birthCountry &&
+      moment.utc(this.candidateData.birthdate, 'YYYY-MM-DD', true).isValid();
+  }),
+
 });
