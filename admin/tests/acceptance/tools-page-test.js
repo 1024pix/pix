6d7848af1db40eb2658094443622f92e112a2502
jbuget@gmail.com <jbuget@gmail.com>
2020-01-29 23:56:04 +0100

    Add button to refresh cache

---
+++ admin/tests/acceptance/tools-page-test.js
@@ -0,0 +1,38 @@
+import { module, test } from 'qunit';
+import { currentURL, visit } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import { authenticateSession } from 'ember-simple-auth/test-support';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+
+module('Acceptance | tools page', function(hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  hooks.beforeEach(async function() {
+    await authenticateSession({ userId: 1 });
+  });
+
+  module('Access', function() {
+
+    test('Tools page should be accessible from /tools', async function(assert) {
+      // when
+      await visit('/tools');
+
+      // then
+      assert.equal(currentURL(), '/tools');
+    });
+  });
+
+  module('Rendering', function(hooks) {
+
+    hooks.beforeEach(async function() {
+      await visit('/tools');
+    });
+
+    test('Should content "Learning content" section', async function(assert) {
+      assert.dom('section.learning-content').exists();
+      assert.dom('button.btn-refresh-cache').exists();
+    });
+  });
+
+});
