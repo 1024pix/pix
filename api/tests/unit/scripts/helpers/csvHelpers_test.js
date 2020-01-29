f69248745dc7a07aeb2639e9847cb9217461bb2c
Christophe Charles <ccharles@octo.com>
2020-01-29 09:58:15 +0100

    Add parseCsvWithHeader method

--- api/tests/unit/scripts/helpers/csvHelpers_test.js
+++ api/tests/unit/scripts/helpers/csvHelpers_test.js
@@ -1,41 +1,38 @@
 const { expect, catchErr } = require('../../../test-helper');
 const { NotFoundError, FileValidationError } = require('../../../../lib/domain/errors');
-const { checkCsvExtensionFile, parseCsv } = require('../../../../scripts/helpers/csvHelpers');
+const { checkCsvExtensionFile, parseCsv, parseCsvWithHeader } = require('../../../../scripts/helpers/csvHelpers');
 
 describe('Unit | Scripts | Helpers | csvHelpers.js', () => {
+
+  const notExistFilePath = 'notExist.file';
+  const badExtensionFilePath = `${__dirname}/files/bad_extension.html`;
+  const validFilePath = `${__dirname}/files/valid-organizations-test.csv`;
+  const utf8FilePath = `${__dirname}/files/utf8_excel-test.csv`;
+  const withHeaderFilePath = `${__dirname}/files/withHeader-test.csv`;
 
   describe('#checkCsvExtensionFile', () => {
 
     it('should throw a NotFoundError when file does not exist', async () => {
-      // given
-      const filePath = 'inexistant.file';
-
       // when
-      const error = await catchErr(checkCsvExtensionFile)(filePath);
+      const error = await catchErr(checkCsvExtensionFile)(notExistFilePath);
 
       // then
       expect(error).to.be.instanceOf(NotFoundError);
-      expect(error.message).to.equal('File not found inexistant.file');
+      expect(error.message).to.equal(`File ${notExistFilePath} not found!`);
     });
 
     it('should throw a FileValidationError when file extension is not ".csv"', async () => {
-      // given
-      const filePath = `${__dirname}/file_with_bad_extension.html`;
-
       // when
-      const error = await catchErr(checkCsvExtensionFile)(filePath);
+      const error = await catchErr(checkCsvExtensionFile)(badExtensionFilePath);
 
       // then
       expect(error).to.be.instanceOf(FileValidationError);
-      expect(error.message).to.equal('File extension not supported .html');
+      expect(error.message).to.equal('File with extension .html not supported!');
     });
 
     it('should return true if file is valid', () => {
-      // given
-      const filePath = `${__dirname}/valid-organizations-test-file.csv`;
-
       // when
-      const result = checkCsvExtensionFile(filePath);
+      const result = checkCsvExtensionFile(validFilePath);
 
       // then
       expect(result).to.be.true;
@@ -46,24 +43,20 @@ describe('Unit | Scripts | Helpers | csvHelpers.js', () => {
   describe('#parseCsv', () => {
 
     it('should throw a NotFoundError when file does not exist', async () => {
-      // given
-      const filePath = 'inexistant.file';
-
       // when
-      const error = await catchErr(parseCsv)(filePath);
+      const error = await catchErr(parseCsv)(notExistFilePath);
 
       // then
       expect(error).to.be.instanceOf(NotFoundError);
-      expect(error.message).to.equal('File not found inexistant.file');
+      expect(error.message).to.equal(`File ${notExistFilePath} not found!`);
     });
 
     it('should parse csv file with 3 lines', () => {
       // given
-      const filePath = `${__dirname}/valid-organizations-test-file.csv`;
       const options = { skipEmptyLines: true };
 
       // when
-      const data = parseCsv(filePath, options);
+      const data = parseCsv(validFilePath, options);
 
       // then
       expect(data.length).to.equal(3);
@@ -71,14 +64,30 @@ describe('Unit | Scripts | Helpers | csvHelpers.js', () => {
     });
 
     it('should cast the unexpected utf8 char add by Excel', async () => {
-      // given
-      const filePath = `${__dirname}/csvHelpers-parseCsv_excel-test-file.csv`;
-
       // when
-      const data = parseCsv(filePath);
+      const data = parseCsv(utf8FilePath);
 
       // then
       expect(data.length).to.equal(4);
+    });
+  });
+
+  describe('#parseCsvWithHeader', () => {
+
+    it('should parse csv file with header', () => {
+      // given
+      const expectedItems = [
+        { uai: '0080017A', name: 'Collège Les Pixous' },
+        { uai: '0080018B', name: 'Lycée Pix' },
+        { uai: '0080040A', name: 'Lycée Tant Pix' }
+      ];
+
+      // when
+      const items = parseCsvWithHeader(withHeaderFilePath);
+
+      // then
+      expect(items.length).to.equal(3);
+      expect(items).to.have.deep.members(expectedItems);
     });
   });
 
