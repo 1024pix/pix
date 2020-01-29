7cc36925a16a014c35b339a80c80d643fbb6fd65
jbuget@gmail.com <jbuget@gmail.com>
2020-01-29 08:42:56 +0100

    Add menu-bar entry to page "tools"

---
+++ admin/app/controllers/authenticated/tools.js
@@ -0,0 +1,10 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+
+  actions: {
+    refreshLearningContent() {
+      console.log('TODO refreshLearningContent');
+    }
+  }
+});
