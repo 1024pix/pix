b8153b90156ad7823b46710c09fe338812b39fa0
lisequesnel <quesnel.lise@gmail.com>
2020-01-31 07:59:02 +0100

    Debounce triggerFiltering action
    Add a debounce method to avoid calling the API each time a key is pressed in the input.

--- orga/app/controllers/authenticated/campaigns/list.js
+++ orga/app/controllers/authenticated/campaigns/list.js
@@ -2,21 +2,29 @@ import { isEmpty } from '@ember/utils';
 import { empty } from '@ember/object/computed';
 import { computed } from '@ember/object';
 import Controller from '@ember/controller';
+import { debounce } from '@ember/runloop';
 
 export default Controller.extend({
   queryParams: ['pageNumber', 'pageSize', 'name'],
   pageNumber: 1,
   pageSize: 25,
   name: null,
+  searchFilter: null,
+  campaignName: null,
 
   hasNoCampaign: empty('model'),
   displayNoCampaignPanel: computed('name,hasNoCampaign', function() {
     return this.hasNoCampaign && isEmpty(this.name);
   }),
 
+  setFieldName() {
+    this.set(this.searchFilter.fieldName, this.searchFilter.value);
+  },
+
   actions: {
     triggerFiltering(fieldName, value) {
-      this.set(fieldName, value);
+      this.set('searchFilter', { fieldName, value });
+      debounce(this, this.setFieldName, 500);
     },
 
     goToCampaignPage(campaignId) {
