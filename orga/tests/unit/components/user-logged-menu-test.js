9b6b0486e62b79f7ce9f29b8c541307b75403852
Mathieu Gilet <mathieu.gilet@zenika.com>
2020-01-31 11:14:18 +0100

    Update user-logged-menu :
    - Organizations displayed and switchable
    - authenticated controller added to send action to route
    - authenticated route updated with function to reload campaign on main organiation switch
    - main organization display on sidebar menu removed

---
+++ orga/tests/unit/components/user-logged-menu-test.js
@@ -0,0 +1,35 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Component | user-logged-menu', (hooks) => {
+
+  setupTest(hooks);
+
+  let component;
+
+  hooks.beforeEach(function() {
+    component = this.owner.factoryFor('component:user-logged-menu').create();
+  });
+
+  module('action#toggleUserMenu', () => {
+    test('should return false as default value', function(assert) {
+      // then
+      assert.equal(component.get('isMenuOpen'), false);
+    });
+    
+    test('should return true, when user details is clicked', function(assert) {
+      // when
+      component.send('toggleUserMenu');
+      // then
+      assert.equal(component.get('isMenuOpen'), true);
+    });
+
+    test('should return false, when isMenuOpen was previously true', function(assert) {
+      // when
+      component.send('toggleUserMenu');
+      component.send('toggleUserMenu');
+      // then
+      assert.equal(component.get('isMenuOpen'), false);
+    });
+  });
+});
