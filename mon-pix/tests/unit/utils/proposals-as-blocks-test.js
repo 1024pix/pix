635a0815e7f18ec0c7d2f9bdf1f12666db4d666b
celineung <celine.ung@zenika.com>
2020-01-31 18:10:42 +0100

    Rename part to block

--- mon-pix/tests/unit/utils/proposals-as-blocks-test.js
+++ mon-pix/tests/unit/utils/proposals-as-blocks-test.js
@@ -6,35 +6,35 @@ describe('Unit | Utility | proposals as blocks', function() {
 
   const testData = [
     { data: '', expected: [] },
-    { data: 'Text', expected: [{ text: 'Text', input: undefined, placeholder: undefined, ariaLabel: '' }] },
+    { data: 'Text', expected: [{ text: 'Text', input: undefined, placeholder: undefined, ariaLabel: null }] },
     { data: '${qroc}', expected: [{ input: 'qroc', text: undefined, placeholder: undefined, ariaLabel: 'Réponse 1' }] },
     { data: 'Test: ${test}', expected: [
-      { text: 'Test:', input: 'test', placeholder: undefined, ariaLabel: '' }] },
+      { text: 'Test:', input: 'test', placeholder: undefined, ariaLabel: null }] },
     { data: 'Test: ${test} (kilometres)', expected: [
-      { text: 'Test:', input: 'test', placeholder: undefined, ariaLabel: '' },
-      { text: '(kilometres)', input: undefined, placeholder: undefined, ariaLabel: '' }] },
+      { text: 'Test:', input: 'test', placeholder: undefined, ariaLabel: null },
+      { text: '(kilometres)', input: undefined, placeholder: undefined, ariaLabel: null }] },
     {
       data: '${plop}, ${plop} ${plop}',
       expected: [
         { input: 'plop', text: undefined, placeholder: undefined, ariaLabel: 'Réponse 1' },
-        { input: undefined, text: ',' , placeholder: undefined, ariaLabel: '' },
+        { input: undefined, text: ',' , placeholder: undefined, ariaLabel: null },
         { input: 'plop', text: undefined, placeholder: undefined, ariaLabel: 'Réponse 2' },
         { input: 'plop', text: undefined, placeholder: undefined, ariaLabel: 'Réponse 3' }]
     },
     { data: '${plop#var}', expected: [{ input: 'plop', placeholder: 'var', text: undefined, ariaLabel: 'Réponse 1' }] },
     { data: 'line1\nline2', expected: [
-      { text: 'line1', input: undefined, placeholder: undefined, ariaLabel: '' },
+      { text: 'line1', input: undefined, placeholder: undefined, ariaLabel: null },
       { breakline: true },
-      { text: 'line2', input: undefined, placeholder: undefined, ariaLabel: '' }] },
+      { text: 'line2', input: undefined, placeholder: undefined, ariaLabel: null }] },
     { data: 'line1\r\nline2', expected: [
-      { text: 'line1', input: undefined, placeholder: undefined, ariaLabel: '' },
+      { text: 'line1', input: undefined, placeholder: undefined, ariaLabel: null },
       { breakline: true },
-      { text: 'line2', input: undefined, placeholder: undefined, ariaLabel: '' }] },
+      { text: 'line2', input: undefined, placeholder: undefined, ariaLabel: null }] },
     { data: '- ${plop}', expected: [
-      { text: '-', input: undefined, placeholder: undefined, ariaLabel: '' },
+      { text: '-', input: undefined, placeholder: undefined, ariaLabel: null },
       { text: undefined, input: 'plop', placeholder: undefined, ariaLabel: 'Réponse 1' }] },
     { data: '- line ${plop}', expected: [
-      { text: '- line', input: 'plop', placeholder: undefined, ariaLabel: '' }] },
+      { text: '- line', input: 'plop', placeholder: undefined, ariaLabel: null }] },
   ];
 
   testData.forEach(({ data, expected }) => {
