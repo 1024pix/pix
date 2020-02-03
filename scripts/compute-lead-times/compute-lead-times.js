6e11c8450d3c91da23cd45aecf44d958cc992170
Guillaume Lagorce <guillaume@hey-gul.com>
2020-02-03 17:35:14 +0100

    Remove useless TODO in comment

--- scripts/compute-lead-times/compute-lead-times.js
+++ scripts/compute-lead-times/compute-lead-times.js
@@ -1,15 +1,5 @@
 const moment = require('moment');
 
-///
-// TODO
-//
-// Inputs:
-// $ git show --pretty=format:"%ad" --summary v2.105.0 | tail -1 # tag date
-// $ git log  --pretty=format:"%ad" v2.104.0...v2.105.0          # commit dates
-//
-// Outputs:
-// - convert to csv
-
 function computeLeadTimes(tagDate, commitDateStrings) {
   const tagMoment = moment(new Date(tagDate));
 
