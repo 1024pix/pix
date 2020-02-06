056c54a4f2b1e8b02902aa4e07fdc0978123abd6
jeremie_jade <jeremie.jade@pix.fr>
2020-02-06 17:08:48 +0100

    Get answer word from iframe when embed is solved

--- mon-pix/app/components/qroc-proposal.js
+++ mon-pix/app/components/qroc-proposal.js
@@ -11,6 +11,7 @@ export default class QrocProposal extends Component {
   proposals = null;
   answerValue = null;
   answerChanged = null; // action
+  postMessageHandler = null;
 
   @computed('proposals')
   get _blocks() {
@@ -22,12 +23,25 @@ export default class QrocProposal extends Component {
     const answer = this.answerValue || '';
     return answer.indexOf('#ABAND#') > -1 ? '' : answer;
   }
+  _setAnswerValue(event) {
+    this.set('answerValue', event.data) ;
+  }
+
+  didUpdateAttrs() {
+    this.set('userAnswer', '');
+  }
 
   didInsertElement() {
+    this.postMessageHandler = this._setAnswerValue.bind(this);
+    window.addEventListener('message', this.postMessageHandler);
 
     this.$('input').keydown(() => {
       this.answerChanged();
     });
+  }
+
+  didDestroyElement() {
+    window.removeEventListener('message',this.postMessageHandler);
   }
 
   willRender() {
