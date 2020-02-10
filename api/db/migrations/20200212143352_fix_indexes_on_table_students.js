d7e98fc4aa4f3bf9fef618e1df4549516f488312
laura-bergoens <laura.bergoens@gmail.com>
2020-02-10 02:35:08 +0100

    Fix indexes - table students
    Remove unused indexes on nationalStudentId and userId
    Remove redundant index on organizationId
    Swap composite unique index from nationalStudentId/organizationId
    to organizationId/nationalStudentId

---
+++ api/db/migrations/20200212143352_fix_indexes_on_table_students.js
@@ -0,0 +1,24 @@
+const TABLE_NAME = 'students';
+const USERID_COLUMN = 'userId';
+const ORGANIZATIONID_COLUMN = 'organizationId';
+const NATIONALSTUDENTID_COLUMN = 'nationalStudentId';
+
+exports.up = function(knex) {
+  return knex.schema.table(TABLE_NAME, function(table) {
+    table.dropIndex(NATIONALSTUDENTID_COLUMN);
+    table.dropIndex(ORGANIZATIONID_COLUMN);
+    table.dropIndex(USERID_COLUMN);
+    table.dropUnique([NATIONALSTUDENTID_COLUMN, ORGANIZATIONID_COLUMN]);
+    table.unique([ORGANIZATIONID_COLUMN, NATIONALSTUDENTID_COLUMN]);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.table(TABLE_NAME, function(table) {
+    table.index(NATIONALSTUDENTID_COLUMN);
+    table.index(ORGANIZATIONID_COLUMN);
+    table.index(USERID_COLUMN);
+    table.unique([NATIONALSTUDENTID_COLUMN, ORGANIZATIONID_COLUMN]);
+    table.dropUnique([ORGANIZATIONID_COLUMN, NATIONALSTUDENTID_COLUMN]);
+  });
+};
