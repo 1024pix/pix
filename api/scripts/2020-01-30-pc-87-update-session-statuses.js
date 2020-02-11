43bb21fe5297232c44522f22f07c8f02a8cc6b60
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-02-11 12:42:37 +0100

    Use stdin as script input and remove json file

--- api/scripts/2020-01-30-pc-87-update-session-statuses.js
+++ api/scripts/2020-01-30-pc-87-update-session-statuses.js
@@ -1,6 +1,18 @@
 const { knex } = require('../db/knex-database-connection');
 
-const sessionIdsWithReport = require('./2020-01-30-pc-87-session-ids-with-reports.json');
+/**
+ * Utilisation
+ * ===========
+ *
+ * Créer un fichier json contenant la liste des ids des sessions dont on a
+ * déjà reçu le pv de session. Par exemple :
+ * `[ 123, 456 ]`
+ *
+ * Puis invoquer le script en lui passant ce fichier par stdin :
+ * ```
+ * node scripts/2020-01-30-pc-87-update-session-statuses < sessionIds.json
+ * ```
+ */
 
 function updateSessionStatuses(sessionIds, finalizationDatetime = new Date()) {
   const updateRequest = `
@@ -20,11 +32,29 @@ function updateSessionStatuses(sessionIds, finalizationDatetime = new Date()) {
   return knex.raw(updateRequest, [finalizationDatetime, sessionIds]);
 }
 
+function readStream(stream) {
+  stream.setEncoding('utf8');
+
+  return new Promise((resolve, reject) => {
+    let data = '';
+
+    stream.on('data', (chunk) => data += chunk);
+    stream.on('end', () => resolve(data));
+    stream.on('error', (error) => reject(error));
+  });
+}
+
 if (require.main === module) {
-  console.log('Updating session statuses to finalized...');
-  updateSessionStatuses(sessionIdsWithReport)
+  readStream(process.stdin)
+    .then(JSON.parse)
+    .then((sessionIdsWithReport) => {
+      console.log('Parsed IDs of session with reports:', sessionIdsWithReport);
+      console.log('Updating session statuses to finalized...');
+      return sessionIdsWithReport;
+    })
+    .then(updateSessionStatuses)
     .then(() => console.log('Session statuses updated.'))
-    .catch((error) => console.error(error))
+    .catch(console.error)
     .finally(() => process.exit(0));
 }
 
