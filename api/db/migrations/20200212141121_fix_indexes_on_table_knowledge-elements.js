5760050229ea8dfe3c998b809ccb2391f5b646bc
laura-bergoens <laura.bergoens@gmail.com>
2020-02-11 17:20:58 +0100

    Fix indexes - table knowledge-elements
    Remove 4 unused indexes on columns :
    skillId, answerId, assessmentId and competenceId

---
+++ api/db/migrations/20200212141121_fix_indexes_on_table_knowledge-elements.js
@@ -0,0 +1,23 @@
+const TABLE_NAME = 'knowledge-elements';
+const SKILLID_COLUMN = 'skillId';
+const ANSWERID_COLUMN = 'answerId';
+const ASSESSMENTID_COLUMN = 'assessmentId';
+const COMPETENCEID_COLUMN = 'competenceId';
+
+exports.up = function(knex) {
+  return knex.schema.table(TABLE_NAME, function(table) {
+    table.dropIndex(SKILLID_COLUMN);
+    table.dropIndex(ANSWERID_COLUMN);
+    table.dropIndex(ASSESSMENTID_COLUMN);
+    table.dropIndex(COMPETENCEID_COLUMN);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.table(TABLE_NAME, function(table) {
+    table.index(SKILLID_COLUMN);
+    table.index(ANSWERID_COLUMN);
+    table.index(ASSESSMENTID_COLUMN);
+    table.index(COMPETENCEID_COLUMN);
+  });
+};
