8947c181c742dec86c46e40a32ad9bdf274c3905
Christophe Charles <ccharles@octo.com>
2020-02-11 14:17:00 +0100

    Add seeds to certification centers

--- api/tests/tooling/database-builder/factory/build-certification-center.js
+++ api/tests/tooling/database-builder/factory/build-certification-center.js
@@ -4,12 +4,14 @@ const databaseBuffer = require('../database-buffer');
 module.exports = function buildCertificationCenter({
   id,
   name = faker.company.companyName(),
+  externalId = faker.random.alphaNumeric(8).toUpperCase(),
   createdAt = faker.date.recent(),
 } = {}) {
 
   const values = {
     id,
     name,
+    externalId,
     createdAt,
   };
   return databaseBuffer.pushInsertable({
