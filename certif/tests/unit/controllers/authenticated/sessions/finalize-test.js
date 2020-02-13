db1fe70ce04c7b5bf41005fdee0f9a69d2c1a968
Antoine Ceol <antoine.ceol@gmail.com>
2020-02-13 17:35:10 +0100

    hide contextual message if no unchecked box

--- certif/tests/unit/controllers/authenticated/sessions/finalize-test.js
+++ certif/tests/unit/controllers/authenticated/sessions/finalize-test.js
@@ -67,4 +67,42 @@ module('Unit | Controller | ' + FINALIZE_PATH, function(hooks) {
     // then
     assert.strictEqual(uncheckedHasSeenEndTestScreenCount, 1);
   });
+
+  test('it should be false if no unchecked certification reports', function(assert) {
+
+    // given
+    const controller = this.owner.lookup('controller:' + FINALIZE_PATH);
+    const sessions = ArrayProxy.create({
+      certificationReports: [
+        { hasSeenEndTestScreen: true },
+        { hasSeenEndTestScreen: true },
+      ]
+    });
+    controller.set('model', sessions);
+
+    // when
+    const hasUncheckedHasSeenEndTestScreen = controller.get('hasUncheckedHasSeenEndTestScreen');
+
+    // then
+    assert.equal(hasUncheckedHasSeenEndTestScreen, false);
+  });
+
+  test('it should be true if at least one unchecked certification reports', function(assert) {
+
+    // given
+    const controller = this.owner.lookup('controller:' + FINALIZE_PATH);
+    const sessions = ArrayProxy.create({
+      certificationReports: [
+        { hasSeenEndTestScreen: false },
+        { hasSeenEndTestScreen: true },
+      ]
+    });
+    controller.set('model', sessions);
+
+    // when
+    const hasUncheckedHasSeenEndTestScreen = controller.get('hasUncheckedHasSeenEndTestScreen');
+
+    // then
+    assert.equal(hasUncheckedHasSeenEndTestScreen, true);
+  });
 });
