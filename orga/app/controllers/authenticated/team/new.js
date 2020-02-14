4421a6620056e0c4ca7f0d14a3fb742ed6e1c424
Christophe Charles <ccharles@octo.com>
2020-02-14 18:20:58 +0100

    Add isLoading parameter to desactivate the button

--- orga/app/controllers/authenticated/team/new.js
+++ orga/app/controllers/authenticated/team/new.js
@@ -5,9 +5,11 @@ export default Controller.extend({
 
   store: service(),
   notifications: service('notifications'),
+  isLoading: false,
 
   actions: {
     async createOrganizationInvitation(organizationInvitation) {
+      this.set('isLoading', true);
       this.get('notifications').clearAll();
 
       return organizationInvitation.save({ adapterOptions: { organizationId: organizationInvitation.organizationId } })
@@ -15,18 +17,23 @@ export default Controller.extend({
           this.transitionToRoute('authenticated.team');
         })
         .catch((errorResponse) => {
-          errorResponse.errors.forEach((error) => {
-            if (error.status === '421') {
-              return this.get('notifications').sendError('Ce membre a déjà été ajouté.');
-            }
-            if (error.status === '404') {
-              return this.get('notifications').sendError('Cet email n\'appartient à aucun utilisateur.');
-            }
-            if (error.status === '500') {
-              return this.get('notifications').sendError(error.title);
-            }
-          });
-        });
+          if (errorResponse.errors && errorResponse.errors.length > 0) {
+            errorResponse.errors.forEach((error) => {
+              if (error.status === '421') {
+                return this.get('notifications').sendError('Ce membre a déjà été ajouté.');
+              }
+              if (error.status === '404') {
+                return this.get('notifications').sendError('Cet email n\'appartient à aucun utilisateur.');
+              }
+              if (error.status === '500') {
+                return this.get('notifications').sendError(error.title);
+              }
+            });
+          } else {
+            return this.get('notifications').sendError('Le service est momentanément indisponible. Veuillez réessayer ultérieurement.');
+          }
+        })
+        .finally(() => this.set('isLoading', false));
     },
 
     cancel() {
