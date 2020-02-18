bad97b8a98f1d4a53e225ccd9803ce092a632e28
Krysthalia <ang.schall@gmail.com>
2020-02-18 19:04:44 +0100

    Add route /api/sessions/{id}/results-sent-to-prescriber

--- api/tests/acceptance/application/session/session-controller-put-results-sent-to-prescriber_test.js
+++ api/tests/acceptance/application/session/session-controller-put-results-sent-to-prescriber_test.js
@@ -3,33 +3,27 @@ const {
 } = require('../../../test-helper');
 const createServer = require('../../../../server');
 
-describe('PUT /api/sessions/:id/sent-to-prescriber', () => {
-
+describe('PUT /api/sessions/:id/results-sent-to-prescriber', () => {
   let server;
-  let options;
+  const options = { method: 'PUT' };
+  let userId;
 
   beforeEach(async () => {
     server = await createServer();
   });
 
-  context('User does not have the role PIX MASTER', () => {
+  context('when user does not have the role PIX MASTER', () => {
 
-    beforeEach(async () => {
-      // given
-      const user = databaseBuilder.factory.buildUser();
-      const authHeader = generateValidRequestAuthorizationHeader(user.id);
-      const token = authHeader.replace('Bearer ', '');
-      const headers = { 'authorization': `Bearer ${token}` };
-      options = {
-        method: 'PUT',
-        url: '/api/sessions/1/sent-to-prescriber',
-        headers,
-      };
-
+    beforeEach(() => {
+      userId = databaseBuilder.factory.buildUser().id;
       return databaseBuilder.commit();
     });
 
-    it('will shut me down ', async () => {
+    it('should return a 403 error code', async () => {
+      // given
+      options.url = '/api/sessions/any/results-sent-to-prescriber';
+      options.headers = { authorization: generateValidRequestAuthorizationHeader(userId) };
+
       // when
       const response = await server.inject(options);
 
@@ -39,59 +33,113 @@ describe('PUT /api/sessions/:id/sent-to-prescriber', () => {
 
   });
 
-  context('User has role PixMaster', () => {
-    let sessionWithAlreadyADateId;
-    let sessionWithoutADateId;
+  context('when user has role PixMaster', () => {
 
-    beforeEach(async () => {
+    beforeEach(() => {
       // given
-      const user = databaseBuilder.factory.buildUser.withPixRolePixMaster();
-      sessionWithAlreadyADateId = databaseBuilder.factory.buildSession({ sentToPrescriberAt: new Date() }).id;
-      sessionWithoutADateId = databaseBuilder.factory.buildSession().id;
-
-      const authHeader = generateValidRequestAuthorizationHeader(user.id);
-      const token = authHeader.replace('Bearer ', '');
-      const headers = Object.assign({}, { 'authorization': `Bearer ${token}` });
-      options = {
-        method: 'PUT',
-        headers,
-      };
-
+      userId = databaseBuilder.factory.buildUser.withPixRolePixMaster().id;
+      options.headers = { authorization: generateValidRequestAuthorizationHeader(userId) };
       return databaseBuilder.commit();
     });
 
-    it('should return an 404 status when the session doesn\'t exist', async () => {
-      // given
-      options.url = `/api/sessions/${sessionWithAlreadyADateId + sessionWithoutADateId}/sent-to-prescriber`;
+    context('when the session id has an invalid format', () => {
 
-      // when
-      const response = await server.inject(options);
+      it('should return a 404 error code', async () => {
+        // given
+        options.url = '/api/sessions/any/results-sent-to-prescriber';
 
-      // then
-      expect(response.statusCode).to.equal(404);
+        // when
+        const response = await server.inject(options);
+
+        // then
+        expect(response.statusCode).to.equal(404);
+      });
     });
 
-    it('should return an 201 status when the session sentToPrescriberAt already exists', async () => {
-      // given 
-      options.url = `/api/sessions/${sessionWithAlreadyADateId}/sent-to-prescriber`;
+    context('when the session id is a number', () => {
 
-      // when
-      const response = await server.inject(options);
+      context('when the session does not exist', () => {
 
-      // then
-      expect(response.statusCode).to.equal(201);
-    });
+        it('should return a 404 error code', async () => {
+          // given
+          options.url = '/api/sessions/1/results-sent-to-prescriber';
 
-    it('should return an 200 status when the session sentToPrescriberAt does not exist', async () => {
-      // given 
-      options.url = `/api/sessions/${sessionWithoutADateId}/sent-to-prescriber`;
+          // when
+          const response = await server.inject(options);
 
-      // when
-      const response = await server.inject(options);
+          // then
+          expect(response.statusCode).to.equal(404);
+        });
+      });
 
-      // then
-      expect(response.statusCode).to.equal(200);
+      context('when the session exists', () => {
+        let sessionId;
+
+        context('when the session results were already flagged as sent to prescriber', () => {
+          const date = new Date();
+
+          beforeEach(() => {
+            // given
+            sessionId = databaseBuilder.factory.buildSession({ resultsSentToPrescriberAt: date }).id;
+            options.headers = { authorization: generateValidRequestAuthorizationHeader(userId) };
+            return databaseBuilder.commit();
+          });
+
+          it('should return a 200 status code', async () => {
+            // given
+            options.url = `/api/sessions/${sessionId}/results-sent-to-prescriber`;
+
+            // when
+            const response = await server.inject(options);
+
+            // then
+            expect(response.statusCode).to.equal(200);
+          });
+
+          it('should return the serialized session with an untouched resultsSentToPrescriberAt date', async () => {
+            // given
+            options.url = `/api/sessions/${sessionId}/results-sent-to-prescriber`;
+
+            // when
+            const response = await server.inject(options);
+
+            // then
+            expect(response.result.data.attributes['results-sent-to-prescriber-at']).to.deep.equal(date);
+          });
+        });
+
+        context('when the session results were not flagged as sent to prescriber', () => {
+
+          beforeEach(() => {
+            // given
+            sessionId = databaseBuilder.factory.buildSession({ resultsSentToPrescriberAt: null }).id;
+            options.headers = { authorization: generateValidRequestAuthorizationHeader(userId) };
+            return databaseBuilder.commit();
+          });
+
+          it('should return a 201 status code', async () => {
+            // given
+            options.url = `/api/sessions/${sessionId}/results-sent-to-prescriber`;
+
+            // when
+            const response = await server.inject(options);
+
+            // then
+            expect(response.statusCode).to.equal(201);
+          });
+
+          it('should return the serialized session with a defined resultsSentToPrescriberAt date', async () => {
+            // given
+            options.url = `/api/sessions/${sessionId}/results-sent-to-prescriber`;
+
+            // when
+            const response = await server.inject(options);
+
+            // then
+            expect(response.result.data.attributes['results-sent-to-prescriber-at']).to.be.an.instanceOf(Date);
+          });
+        });
+      });
     });
   });
-
 });
