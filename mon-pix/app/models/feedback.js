b65da97f41a911f25028890fabb7359ef879ee65
Stephane Bedeau <sbedeau@octo.com>
2020-02-19 17:28:13 +0100

    Convert models/feedback.js

--- mon-pix/app/models/feedback.js
+++ mon-pix/app/models/feedback.js
@@ -1,9 +1,13 @@
 import Model, { attr, belongsTo } from '@ember-data/model';
 
-export default Model.extend({
-  content: attr('string'),
-  category: attr('string'),
-  answer: attr('string'),
-  assessment: belongsTo('assessment'),
-  challenge: belongsTo('challenge')
-});
+export default class Feedback extends Model {
+
+  // attributes
+  @attr('string') answer;
+  @attr('string') category;
+  @attr('string') content;
+
+  // includes
+  @belongsTo('assessment') assessment;
+  @belongsTo('challenge') challenge;
+}
