e7c68fb2467b2c2428a64d2a269e5cf6369d003b
Vincent Hardouin <vincent.hardouin@pix.fr>
2020-02-19 14:59:22 +0100

    Remove empty get-user usecases

--- api/lib/domain/usecases/index.js
+++ api/lib/domain/usecases/index.js
@@ -128,7 +128,6 @@ module.exports = injectDependencies({
   getSessionCertificationCandidates: require('./get-session-certification-candidates'),
   getSessionCertifications: require('./get-session-certifications'),
   getSessionCertificationReports: require('./get-session-certification-reports'),
-  getUser: require('./get-user'),
   getUserCertificationCenterMemberships: require('./get-user-certification-center-memberships'),
   getUserCertificationWithResultTree: require('./get-user-certification-with-result-tree'),
   getUserCurrentCertificationProfile: require('./get-user-current-certification-profile'),
