c5ec17ed9a9b2f799969c0e2e5dcc743a5e57bd4
Elodie Pelcat <elodiepelcat@pix.fr>
2020-02-19 16:01:57 +0100

    Remove Snapshot repository

--- api/lib/domain/usecases/index.js
+++ api/lib/domain/usecases/index.js
@@ -43,7 +43,6 @@ const dependencies = {
   smartPlacementAssessmentRepository: require('../../infrastructure/repositories/smart-placement-assessment-repository'),
   sessionAuthorizationService: require('../../domain/services/session-authorization-service'),
   sessionRepository: require('../../infrastructure/repositories/session-repository'),
-  snapshotRepository: require('../../infrastructure/repositories/snapshot-repository'),
   studentRepository: require('../../infrastructure/repositories/student-repository'),
   studentsXmlService: require('../../domain/services/students-xml-service'),
   targetProfileRepository: require('../../infrastructure/repositories/target-profile-repository'),
