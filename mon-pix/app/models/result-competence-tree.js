d6f8effc5a76cc34ff25ae16eeee3bfa31671682
Stephane Bedeau <sbedeau@octo.com>
2020-02-19 17:29:23 +0100

    Convert models/result-competence-tree.js

--- mon-pix/app/models/result-competence-tree.js
+++ mon-pix/app/models/result-competence-tree.js
@@ -1,5 +1,7 @@
 import Model, { hasMany } from '@ember-data/model';
 
-export default Model.extend({
-  areas: hasMany('area', { inverse: null }),
-});
+export default class ResultCompetenceTree extends Model {
+
+  // includes
+  @hasMany('area', { inverse: null }) areas;
+}
