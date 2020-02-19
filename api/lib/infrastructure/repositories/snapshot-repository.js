d189313acb679f550c9f9888266780ce31c00ad2
Elodie Pelcat <elodiepelcat@pix.fr>
2020-02-19 16:01:57 +0100

    Remove Snapshot repository

--- api/lib/infrastructure/repositories/snapshot-repository.js
+++
@@ -1,8 +0,0 @@
-const BookshelfSnapshot = require('../data/snapshot');
-const queryBuilder = require('../utils/query-builder');
-
-module.exports = {
-  find(options) {
-    return queryBuilder.find(BookshelfSnapshot, options);
-  },
-};
