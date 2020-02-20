eff457d8449f6ac2a380a493f33ef94505a1d7bd
lisequesnel <quesnel.lise@gmail.com>
2020-02-20 11:30:52 +0100

    Clean mirage with organizations useless routes and scenarios

--- mon-pix/mirage/routes/get-organizations.js
+++
@@ -1,9 +0,0 @@
-export default function(schema, request) {
-  const code = request.queryParams['code'];
-
-  if (code) {
-    return schema.organizations.where({ code });
-  }
-
-  return schema.organizations.all();
-}
