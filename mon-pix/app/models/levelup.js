c33dce7f5441d95dabae18276dc105727b36e432
Stephane Bedeau <sbedeau@octo.com>
2020-02-20 12:03:36 +0100

    Add no-empty-attrs eslint rule
    This rule is not yet adapted to Octane.
    We added it for the future and according to our new practices
    which consist to strenghen our eslint rules
    https://github.com/ember-cli/eslint-plugin-ember/blob/master/docs/rules/no-empty-attrs.md

--- mon-pix/app/models/levelup.js
+++ mon-pix/app/models/levelup.js
@@ -3,6 +3,6 @@ import Model, { attr } from '@ember-data/model';
 export default class Levelup extends Model {
 
   // attributes
-  @attr competenceName;
-  @attr level;
+  @attr('string') competenceName;
+  @attr('number') level;
 }
