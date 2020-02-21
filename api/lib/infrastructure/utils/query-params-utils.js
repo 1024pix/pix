94ae4b874e35927ddf9d4538e6f383953d52f2ed
ARJ <arj@octo.com>
2020-02-21 17:50:06 +0100

    Convert page params for pagination to int
    To handle bad page number Bookshelf has ensureIntWithDefault
    method. When page is O default page is applied (page 1)
    but if page is "0", it throws an error.

--- api/lib/infrastructure/utils/query-params-utils.js
+++ api/lib/infrastructure/utils/query-params-utils.js
@@ -19,7 +19,9 @@ function _extractFilter(query) {
 
 function _extractPage(query) {
   const regex = /page\[([a-zA-Z]*)]/;
-  return _extractObjectParameter(query, regex);
+  const params = _extractObjectParameter(query, regex);
+
+  return _convertToInt(params);
 }
 
 function _extractObjectParameter(query, regex) {
@@ -34,4 +36,8 @@ function _extractObjectParameter(query, regex) {
 
 function _extractArrayParameter(query, parameterName) {
   return query[parameterName] ? query[parameterName].split(',') : [];
+}
+
+function _convertToInt(params) {
+  return _.mapValues(params, (value) => parseInt(value));
 }
