f551321bc45845071b52afd7f7117e74f23bde69
laura-bergoens <laura.bergoens@gmail.com>
2020-02-24 12:41:33 +0100

    Add cleaner adapter to finalize session

--- certif/app/adapters/session.js
+++ certif/app/adapters/session.js
@@ -24,4 +24,37 @@ export default class SessionAdapter extends ApplicationAdapter {
       },
     });
   }
+
+  urlForUpdateRecord(id, modelName, { adapterOptions }) {
+    const url = super.urlForUpdateRecord(...arguments);
+    if (adapterOptions && adapterOptions.finalization)  {
+      delete adapterOptions.finalization;
+      return url + '/finalization';
+    }
+
+    return url;
+  }
+
+  updateRecord(store, type, snapshot) {
+    if (snapshot.adapterOptions && snapshot.adapterOptions.finalization) {
+      const model = snapshot.record;
+      const data = {
+        data: {
+          attributes: {
+            'examiner-global-comment': model.get('examinerGlobalComment'),
+          },
+          included: model.get('certificationReports')
+            .filter((certificationReport) => certificationReport.hasDirtyAttributes)
+            .map((certificationReport) => ({
+              type: 'certification-reports',
+              id: certificationReport.get('id'),
+              attributes: certificationReport.toJSON(),
+            })),
+        },
+      };
+      return this.ajax(this.urlForUpdateRecord(snapshot.id, type.modelName, snapshot), 'PUT', { data });
+    }
+
+    return super.updateRecord(...arguments);
+  }
 }
