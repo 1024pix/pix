6caae9624948c425605a19ff4c12a1bb4aed62cc
Mathieu Gilet <mathieu.gilet@zenika.com>
2020-02-24 10:42:56 +0100

    Create a user-orga-settings when validating CGU

--- orga/app/controllers/terms-of-service.js
+++ orga/app/controllers/terms-of-service.js
@@ -4,11 +4,20 @@ import { inject as service } from '@ember/service';
 export default Controller.extend({
 
   currentUser: service(),
+  store: service(),
 
   actions: {
     async submit() {
-      await this.currentUser.user.save({ adapterOptions: { acceptPixOrgaTermsOfService: true } });
-      this.replaceRoute('');
+      const user = this.currentUser.user;
+      await user.save({ adapterOptions: { acceptPixOrgaTermsOfService: true } });
+
+      const userOrgaSettings = await user.userOrgaSettings;
+      if (!userOrgaSettings) {
+        const organization = this.currentUser.organization;
+        this.store.createRecord('user-orga-setting', { user, organization }).save();
+      }
+
+      return this.replaceRoute('');
     }
   }
 });
