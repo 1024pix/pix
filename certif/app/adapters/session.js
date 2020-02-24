6748ed78b5938ae5a2096d997e9dc9f35eff55a3
laura-bergoens <laura.bergoens@gmail.com>
2020-02-24 12:42:04 +0100

    Replace existing finalization method with new one

--- certif/app/adapters/session.js
+++ certif/app/adapters/session.js
@@ -2,29 +2,6 @@ import ApplicationAdapter from './application';
 
 export default class SessionAdapter extends ApplicationAdapter {
 
-  finalize(model) {
-    const url = `${this.buildURL('session', model.id)}/finalization`;
-
-    // Here we try to respect the JSON-API format
-    // so the API knows how to handle this payload
-    return this.ajax(url, 'PUT', {
-      data: {
-        data: {
-          attributes: {
-            'examiner-global-comment': model.get('examinerGlobalComment'),
-          },
-          included: model.get('certificationReports')
-            .filter((certificationReport) => certificationReport.hasDirtyAttributes)
-            .map((certificationReport) => ({
-              type: 'certification-reports',
-              id: certificationReport.get('id'),
-              attributes: certificationReport.toJSON(),
-            })),
-        },
-      },
-    });
-  }
-
   urlForUpdateRecord(id, modelName, { adapterOptions }) {
     const url = super.urlForUpdateRecord(...arguments);
     if (adapterOptions && adapterOptions.finalization)  {
