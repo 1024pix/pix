ee25ce26f96332c36fb5d63e9d4ff433b45807d5
Mathieu Gilet <mathieu.gilet@zenika.com>
2020-02-25 11:41:29 +0100

    Create update-user-orga-settings usecase

--- api/tests/integration/infrastructure/repositories/user-orga-settings-repository_test.js
+++ api/tests/integration/infrastructure/repositories/user-orga-settings-repository_test.js
@@ -3,15 +3,22 @@ const UserOrgaSettings = require('../../../../lib/domain/models/UserOrgaSettings
 const BookshelfUserOrgaSettings = require('../../../../lib/infrastructure/data/user-orga-settings');
 const userOrgaSettingsRepository = require('../../../../lib/infrastructure/repositories/user-orga-settings-repository');
 const { UserOrgaSettingsCreationError } = require('../../../../lib/domain/errors');
+const _ = require('lodash');
 
 describe('Integration | Repository | UserOrgaSettings', function() {
 
-  let userId;
-  let organizationId;
+  const USER_OMITTED_PROPERTIES = ['campaignParticipations', 'certificationCenterMemberships', 'knowledgeElements',
+    'memberships', 'pixRoles', 'pixScore', 'samlId', 'scorecards', 'userOrgaSettings'];
+
+  const ORGANIZATION_OMITTED_PROPERTIES = ['memberships', 'organizationInvitations', 'students', 'targetProfileShares',
+    'createdAt', 'updatedAt'];
+
+  let user;
+  let organization;
 
   beforeEach(async () => {
-    userId = databaseBuilder.factory.buildUser().id;
-    organizationId = databaseBuilder.factory.buildOrganization().id;
+    user = databaseBuilder.factory.buildUser();
+    organization = databaseBuilder.factory.buildOrganization();
     await databaseBuilder.commit();
   });
 
@@ -23,7 +30,7 @@ describe('Integration | Repository | UserOrgaSettings', function() {
 
     it('should return an UserOrgaSettings domain object', async () => {
       // when
-      const userOrgaSettingsSaved = await userOrgaSettingsRepository.create(userId, organizationId);
+      const userOrgaSettingsSaved = await userOrgaSettingsRepository.create(user.id, organization.id);
 
       // then
       expect(userOrgaSettingsSaved).to.be.an.instanceof(UserOrgaSettings);
@@ -34,7 +41,7 @@ describe('Integration | Repository | UserOrgaSettings', function() {
       const nbBeforeCreation = await BookshelfUserOrgaSettings.count();
 
       // when
-      await userOrgaSettingsRepository.create(userId, organizationId);
+      await userOrgaSettingsRepository.create(user.id, organization.id);
 
       // then
       const nbAfterCreation = await BookshelfUserOrgaSettings.count();
@@ -43,25 +50,46 @@ describe('Integration | Repository | UserOrgaSettings', function() {
 
     it('should save model properties', async () => {
       // when
-      const userOrgaSettingsSaved = await userOrgaSettingsRepository.create(userId, organizationId);
+      const userOrgaSettingsSaved = await userOrgaSettingsRepository.create(user.id, organization.id);
 
       // then
       expect(userOrgaSettingsSaved.id).to.not.be.undefined;
-      expect(userOrgaSettingsSaved.user.id).to.equal(userId);
-      expect(userOrgaSettingsSaved.currentOrganization.id).to.equal(organizationId);
+      expect(_.omit(userOrgaSettingsSaved.user, USER_OMITTED_PROPERTIES)).to.deep.equal(_.omit(user, USER_OMITTED_PROPERTIES));
+      expect(_.omit(userOrgaSettingsSaved.currentOrganization, ORGANIZATION_OMITTED_PROPERTIES)).to.deep.equal(_.omit(organization, ORGANIZATION_OMITTED_PROPERTIES));
     });
 
     it('should throw a UserOrgaSettingsCreationError when userOrgaSettings already exist', async () => {
       // given
-      databaseBuilder.factory.buildUserOrgaSettings({ userId, currentOrganizationId: organizationId });
+      databaseBuilder.factory.buildUserOrgaSettings({ userId: user.id, currentOrganizationId: organization.id });
       await databaseBuilder.commit();
 
       // when
-      const error = await catchErr(userOrgaSettingsRepository.create)(userId, organizationId);
+      const error = await catchErr(userOrgaSettingsRepository.create)(user.id, organization.id);
 
       // then
       expect(error).to.be.instanceOf(UserOrgaSettingsCreationError);
     });
   });
 
+  describe('#update', () => {
+
+    let userOrgaSettingsId;
+    let expectedOrganization;
+
+    beforeEach(async () => {
+      userOrgaSettingsId = databaseBuilder.factory.buildUserOrgaSettings({ userId: user.id, currentOrganizationId: organization.id }).id;
+      expectedOrganization = databaseBuilder.factory.buildOrganization();
+      await databaseBuilder.commit();
+    });
+
+    it('should return the updated userOrgaSettings', async () => {
+      // when
+      const updatedUserOrgaSettings = await userOrgaSettingsRepository.update(user.id, expectedOrganization.id);
+
+      // then
+      expect(updatedUserOrgaSettings.id).to.deep.equal(userOrgaSettingsId);
+      expect(_.omit(updatedUserOrgaSettings.user, USER_OMITTED_PROPERTIES)).to.deep.equal(_.omit(user, USER_OMITTED_PROPERTIES));
+      expect(_.omit(updatedUserOrgaSettings.currentOrganization, ORGANIZATION_OMITTED_PROPERTIES)).to.deep.equal(_.omit(expectedOrganization, ORGANIZATION_OMITTED_PROPERTIES));
+    });
+  });
 });
