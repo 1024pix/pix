361fd6142fd46903f9168fd2ec24e2f0722e1dc4
laura-bergoens <laura.bergoens@gmail.com>
2020-02-25 16:05:42 +0100

    Rename certifiable profile data to specify that it's a strong one!

--- api/db/seeds/data/certification/user-profiles-builder.js
+++ api/db/seeds/data/certification/user-profiles-builder.js
@@ -1,6 +1,6 @@
 const _ = require('lodash');
 const { CERTIF_SUCCESS_USER_ID, CERTIF_FAILURE_USER_ID } = require('./users');
-const { CERTIFIABLE_PROFILE_DATA_OBJECTS_FOR_BUILDING_ANSWERS_AND_KNOWLEDGE_ELEMENTS } = require('./certification-data');
+const { STRONG_CERTIFIABLE_PROFILE_DATA_OBJECTS_FOR_BUILDING_ANSWERS_AND_KNOWLEDGE_ELEMENTS } = require('./certification-data');
 
 function certificationUserProfilesBuilder({ databaseBuilder }) {
   // Both have a certifiable profile
@@ -8,7 +8,7 @@ function certificationUserProfilesBuilder({ databaseBuilder }) {
   const updatedAt = createdAt;
   const assessmentIdForSuccess = databaseBuilder.factory.buildAssessment({ userId: CERTIF_SUCCESS_USER_ID, type: 'COMPETENCE_EVALUATION', state: 'completed' }).id;
   const assessmentIdForFailure = databaseBuilder.factory.buildAssessment({ userId: CERTIF_FAILURE_USER_ID, type: 'COMPETENCE_EVALUATION', state: 'completed' }).id;
-  _.each(CERTIFIABLE_PROFILE_DATA_OBJECTS_FOR_BUILDING_ANSWERS_AND_KNOWLEDGE_ELEMENTS, (data) => {
+  _.each(STRONG_CERTIFIABLE_PROFILE_DATA_OBJECTS_FOR_BUILDING_ANSWERS_AND_KNOWLEDGE_ELEMENTS, (data) => {
     let answerId = databaseBuilder.factory.buildAnswer({ ...data, createdAt, updatedAt, assessmentId: assessmentIdForSuccess, value: 'Dummy value' }).id;
     databaseBuilder.factory.buildKnowledgeElement({ ...data, createdAt, answerId, userId: CERTIF_SUCCESS_USER_ID ,assessmentId: assessmentIdForSuccess });
     answerId = databaseBuilder.factory.buildAnswer({ ...data, createdAt, updatedAt, assessmentId: assessmentIdForFailure, value: 'Dummy value' }).id;
