9a70accd9ec50d0ffde8794a31dddec04d2099fc
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-02-25 14:59:39 +0100

    Remove unused CertificationCourse deserializer

--- api/lib/infrastructure/serializers/jsonapi/certification-course-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/certification-course-serializer.js
@@ -1,8 +1,4 @@
 const { Serializer } = require('jsonapi-serializer');
-const CertificationCourse = require('../../../domain/models/CertificationCourse');
-
-const { WrongDateFormatError } = require('../../../domain/errors');
-const { isValidDate } = require('../../utils/date-utils');
 
 module.exports = {
 
@@ -31,26 +27,5 @@ module.exports = {
         }
       },
     }).serialize(certificationCourse);
-  },
-
-  deserialize(json) {
-    if (!isValidDate(json.data.attributes.birthdate, 'YYYY-MM-DD')) {
-      throw new WrongDateFormatError();
-    }
-
-    return new CertificationCourse({
-      id: json.data.id,
-      createdAt: json.data.attributes.createdAt,
-      updatedAt: json.data.attributes.updatedAt,
-      userId: json.data.attributes.userId,
-      completedAt: json.data.attributes.completedAt,
-      firstName: json.data.attributes.firstName,
-      lastName: json.data.attributes.lastName,
-      birthdate: json.data.attributes.birthdate,
-      birthplace: json.data.attributes.birthplace,
-      isV2Certification: json.data.attributes.isV2Certification,
-      examinerComment: json.data.attributes.examinerComment,
-      hasSeenEndTestScreen: json.data.attributes.hasSeenEndTestScreen,
-    });
   },
 };
