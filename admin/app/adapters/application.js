7a856d39333ef9ce19047ec6ffcbb5af5dabaf1e
jbuget@gmail.com <jbuget@gmail.com>
2020-02-25 15:39:28 +0100

    Bump Ember-Simpl Auth

--- admin/app/adapters/application.js
+++ admin/app/adapters/application.js
@@ -1,14 +1,17 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+import { computed } from '@ember/object';
 import ENV from 'pix-admin/config/environment';
 
 export default JSONAPIAdapter.extend(DataAdapterMixin, {
   host: ENV.APP.API_HOST,
   namespace: 'api',
 
-  authorize(xhr) {
-    const { access_token } = this.get('session.data.authenticated');
-    xhr.setRequestHeader('Authorization', `Bearer ${access_token}`);
-  },
-
+  headers: computed('session.data.authenticated.token', function() {
+    const headers = {};
+    if (this.session.isAuthenticated) {
+      headers['Authorization'] = `Bearer ${this.session.data.authenticated.access_token}`;
+    }
+    return headers;
+  })
 });
