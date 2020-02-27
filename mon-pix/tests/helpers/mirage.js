5c19d7e6d3a89590d212eac2cdaa0ef9cad41f54
yahya-youness <you@octo.com>
2020-02-27 16:23:25 +0100

    fix linter space

--- mon-pix/tests/helpers/mirage.js
+++ mon-pix/tests/helpers/mirage.js
@@ -1,5 +1,5 @@
 import { Response } from 'ember-cli-mirage';
 
 export default function overrideApiResponse(status, responseBody) {
-  return new Response(status,{ 'content-type': 'application/json; charset=utf-8' }, responseBody);
+  return new Response(status, { 'content-type': 'application/json; charset=utf-8' }, responseBody);
 }
