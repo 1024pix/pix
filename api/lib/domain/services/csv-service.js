967792a4b7d62f94a7db69e02f47d1e18ca82d12
Christophe Charles <ccharles@octo.com>
2020-02-27 15:10:00 +0100

    Create csv service to sanitize data

---
+++ api/lib/domain/services/csv-service.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+  sanitize(data) {
+    const specialCharacters = ['@', '-', '+', '='];
+    let result = data;
+
+    if (typeof result === 'string' && specialCharacters.includes(result[0])) {
+      result = '\'' + result;
+    }
+
+    return result;
+  }
+
+};
