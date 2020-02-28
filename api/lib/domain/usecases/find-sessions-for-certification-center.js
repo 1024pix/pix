d1279cdb4592e75eb6f1ea2920500c3f09be02b3
LJA <ljacquemin@octo.com>
2020-02-28 14:04:16 +0100

    Refactor usecase to match standards
    The current standards prefers a vertical alignement over large lines,
    async/await over promises and error first.

--- api/lib/domain/usecases/find-sessions-for-certification-center.js
+++ api/lib/domain/usecases/find-sessions-for-certification-center.js
@@ -1,11 +1,16 @@
-const { ForbiddenAccess } = require('../../domain/errors');
+const {
+  ForbiddenAccess
+} = require('../../domain/errors');
 
-module.exports = function findSessionsForCertificationCenter({ userId, certificationCenterId, sessionRepository, userRepository }) {
-  return userRepository.getWithCertificationCenterMemberships(userId)
-    .then((user) => {
-      if (user.hasAccessToCertificationCenter(certificationCenterId)) {
-        return sessionRepository.findByCertificationCenterId(certificationCenterId);
-      }
-      throw new ForbiddenAccess(`User ${userId} is not a member of certification center ${certificationCenterId}`);
-    });
+module.exports = async function findSessionsForCertificationCenter({
+  userId,
+  userRepository,
+  sessionRepository,
+  certificationCenterId,
+}) {
+  const user = await userRepository.getWithCertificationCenterMemberships(userId);
+  if (!user.hasAccessToCertificationCenter(certificationCenterId)) {
+    throw new ForbiddenAccess(`User ${userId} is not a member of certification center ${certificationCenterId}`);
+  }
+  return sessionRepository.findByCertificationCenterId(certificationCenterId);
 };
