12abad77e7c847c3ab3818d1742187ae4f0be2bd
laura-bergoens <laura.bergoens@gmail.com>
2020-03-01 10:18:06 +0100

    Add service to compute tubes from skills
    Computing tubes from skills is a feature present
    in the model Course, which does not make sense anymore.
    Courses are now dedicated to demo, it's legacy to left
    such a computing feature in here.
    So we first create a service that provides that feature.

---
+++ api/lib/domain/services/tube-service.js
@@ -0,0 +1,25 @@
+const _ = require('lodash');
+const Tube = require('../models/Tube');
+
+function computeTubesFromSkills(skills) {
+  const tubes = [];
+
+  skills.forEach((skill) => {
+    const tubeNameOfSkill = skill.tubeName;
+    const existingTube = tubes.find((tube) => tube.name === tubeNameOfSkill);
+    if (existingTube) {
+      existingTube.addSkill(skill);
+    } else {
+      tubes.push(new Tube({ skills: [skill], name: tubeNameOfSkill }));
+    }
+  });
+  tubes.forEach((tube) =>  {
+    tube.skills = _.sortBy(tube.skills, ['difficulty']);
+  });
+
+  return tubes;
+}
+
+module.exports = {
+  computeTubesFromSkills,
+};
