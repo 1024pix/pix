a971eb9e93916cbccdff86cb2476cef96add0677
ARJ <arj@octo.com>
2020-03-02 16:22:05 +0100

    Keep orignal campaign name until campaign update

--- orga/app/routes/authenticated/campaigns/update.js
+++ orga/app/routes/authenticated/campaigns/update.js
@@ -5,6 +5,11 @@ export default Route.extend({
     return this.store.findRecord('campaign', params.campaign_id);
   },
 
+  setupController: function(controller, model) {
+    this._super(controller, model);
+    controller.set('campaignName', model.name);
+  },
+
   deactivate: function() {
     this.controller.model.rollbackAttributes();
   },
