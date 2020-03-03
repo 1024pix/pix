18f995de40d5d83e9b5c4abbfaed45d373b87241
Mathieu Gilet <mathieu.gilet@zenika.com>
2020-03-03 14:20:38 +0100

    Add updateAd and createdAt columns to userOrgaSettings

--- api/lib/infrastructure/data/user-orga-settings.js
+++ api/lib/infrastructure/data/user-orga-settings.js
@@ -6,6 +6,7 @@ require('./organization');
 module.exports = Bookshelf.model('UserOrgaSettings', {
 
   tableName: 'user-orga-settings',
+  hasTimestamps: ['createdAt', 'updatedAt'],
 
   user() {
     return this.belongsTo('User', 'userId');
