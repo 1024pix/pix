c9ebd70ce876c8c220bdc61afe3f8aaddb6d4ca3
jbuget@gmail.com <jbuget@gmail.com>
2020-03-05 09:56:14 +0100

    Use Glimmer component for certification-list

--- admin/app/components/certification-list.js
+++ admin/app/components/certification-list.js
@@ -1,53 +1,49 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 
-const columns = [
-  {
-    component: 'certification-list-checkbox',
-    useFilter: false,
-    mayBeHidden: false,
-    componentForSortCell: 'certification-list-select-all'
-  },
-  {
-    propertyName: 'id',
-    title: 'Id',
-    routeName: 'authenticated.certifications.single.info',
-    sortPrecedence: 1
-  },
-  {
-    propertyName: 'firstName',
-    title: 'Prénom'
-  },
-  {
-    propertyName: 'lastName',
-    title: 'Nom'
-  },
-  {
-    propertyName: 'status',
-    title: 'Statut'
-  },
-  {
-    propertyName: 'pixScore',
-    title: 'Score'
-  },
-  {
-    propertyName: 'creationDate',
-    title: 'Début'
-  },
-  {
-    propertyName: 'completionDate',
-    title: 'Fin'
-  },
-  {
-    component: 'certification-info-published',
-    useFilter: false,
-    mayBeHidden: false,
-    title: 'Publiée'
-  },
-];
+export default class CertificationList extends Component {
 
-export default Component.extend({
-  init() {
-    this._super(...arguments);
-    this.columns = columns;
-  }
-});
+  columns = [
+    {
+      component: 'certification-list-checkbox',
+      useFilter: false,
+      mayBeHidden: false,
+      componentForSortCell: 'certification-list-select-all'
+    },
+    {
+      propertyName: 'id',
+      title: 'Id',
+      routeName: 'authenticated.certifications.single.info',
+      sortPrecedence: 1
+    },
+    {
+      propertyName: 'firstName',
+      title: 'Prénom'
+    },
+    {
+      propertyName: 'lastName',
+      title: 'Nom'
+    },
+    {
+      propertyName: 'status',
+      title: 'Statut'
+    },
+    {
+      propertyName: 'pixScore',
+      title: 'Score'
+    },
+    {
+      propertyName: 'creationDate',
+      title: 'Début'
+    },
+    {
+      propertyName: 'completionDate',
+      title: 'Fin'
+    },
+    {
+      component: 'certification-info-published',
+      useFilter: false,
+      mayBeHidden: false,
+      title: 'Publiée'
+    },
+  ];
+}
