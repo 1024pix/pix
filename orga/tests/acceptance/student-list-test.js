3343c4c78f4ab2d987d66770ee4a8f8b7ef0e034
yahya-youness <you@octo.com>
2020-03-09 10:47:48 +0100

    Refactor tests -show manage button for username,email auth student and -fix conflicts for integration test

--- orga/tests/acceptance/student-list-test.js
+++ orga/tests/acceptance/student-list-test.js
@@ -17,6 +17,7 @@ module('Acceptance | Student List', function(hooks) {
   let user;
   let organizationId;
   let username;
+  let email;
 
   module('When user is not logged in', function() {
 
@@ -64,13 +65,14 @@ module('Acceptance | Student List', function(hooks) {
         user = createUserManagingStudents();
         organizationId = user.memberships.models.firstObject.organizationId;
         username = 'firstname.lastname0112';
-
+        email = 'firstname.lastname0112@example.net';
         server.createList('student', 5, { organizationId });
         server.create('student', {
           organizationId,
           firstName: 'FirstName',
           lastName: 'LastName',
-          username
+          username,
+          email,
         });
 
         await authenticateSession({
@@ -118,26 +120,14 @@ module('Acceptance | Student List', function(hooks) {
         assert.dom('.table thead th:nth-child(5)').hasText('');
       });
 
-      module('when student have a username', async function() {
+      module('when student authenticated by username and email', async function() {
 
-        test('it should display password update button for student with username', async function(assert) {
+        test('it should display password update button for student', async function(assert) {
           // when
           await visit('/eleves');
 
           // then
           assert.dom('.table tbody tr:nth-child(6) td:last-child button .fa-cog').exists();
-        });
-
-        test('it should open password modal window', async function(assert) {
-          // given
-          await visit('/eleves');
-
-          // when
-          await click('.table tbody tr:nth-child(6) td:last-child button');
-
-          // then
-          assert.dom('.pix-modal-overlay').exists();
-          assert.dom('#username').hasValue(username);
         });
 
         test('it should update password and close modal window', async function(assert) {
@@ -169,111 +159,160 @@ module('Acceptance | Student List', function(hooks) {
           assert.dom('.pix-modal-overlay').doesNotExist();
           assert.dom('[data-test-notification-message="error"]').exists();
         });
-      });
 
-      module('When user is admin in organization', function(hooks) {
+        test('it should open password modal window with email and username value', async function(assert) {
 
-        hooks.beforeEach(async () => {
-          user = createUserManagingStudents('ADMIN');
-          await authenticateSession({
-            user_id: user.id,
-            access_token: 'aaa.' + btoa(`{"user_id":${user.id},"source":"pix","iat":1545321469,"exp":4702193958}`) + '.bbb',
-            expires_in: 3600,
-            token_type: 'Bearer token type',
-          });
-        });
-
-        test('it should display import button', async function(assert) {
-          // when
-          await visit('/eleves');
-
-          // then
-          assert.dom('.button').hasText('Importer (.xml)');
-        });
-
-        test('it should display success message and reload students', async function(assert) {
           // given
           await visit('/eleves');
 
-          const file = new Blob(['foo'], { type: 'valid-file' });
-
           // when
-          const input = find('#students-file-upload');
-          await triggerEvent(input, 'change', { files: [file] });
+          await click('.table tbody tr:nth-child(6) td:last-child button');
 
           // then
-          assert.dom('[data-test-notification-message="success"]').exists();
-          assert.dom('[data-test-notification-message="success"]').hasText('La liste a été importée avec succès.');
-          assert.dom('.table tbody tr').exists({ count: 1 });
-          assert.dom('.table tbody tr td:first-child').hasText('Cover');
-          assert.dom('.table tbody tr td:nth-child(2)').hasText('Harry');
-        });
-
-        test('it should display an error message when uploading an invalid file', async function(assert) {
-          // given
-          await visit('/eleves');
-
-          const file = new Blob(['foo'], { type: 'invalid-file' });
-
-          // when
-          const input = find('#students-file-upload');
-          await triggerEvent(input, 'change', { files: [file] });
-
-          // then
-          assert.dom('[data-test-notification-message="error"]').exists();
-          assert.dom('[data-test-notification-message="error"]').hasText('422 - Le détail affiché est envoyé par le back');
-        });
-
-        test('it should display an error message when uploading a file with students informations problems', async function(assert) {
-          // given
-          await visit('/eleves');
-
-          const file = new Blob(['foo'], { type: 'file-with-students-info-problems' });
-
-          // when
-          const input = find('#students-file-upload');
-          await triggerEvent(input, 'change', { files: [file] });
-
-          // then
-          assert.dom('[data-test-notification-message="error"]').exists();
-          assert.dom('[data-test-notification-message="error"]').hasText('409 - Le détail affiché est envoyé par le back');
-        });
-
-        test('it should display an error message when something unexpected went wrong on the server', async function(assert) {
-          // given
-          await visit('/eleves');
-
-          const file = new Blob(['foo'], { type: '' });
-
-          // when
-          const input = find('#students-file-upload');
-          await triggerEvent(input, 'change', { files: [file] });
-
-          // then
-          assert.dom('[data-test-notification-message="error"]').exists();
-          assert.dom('[data-test-notification-message="error"]').hasText('Quelque chose s\'est mal passé. Veuillez réessayer.');
+          assert.dom('.pix-modal-overlay').exists();
+          assert.dom('#username').hasValue(username);
+          assert.dom('#email').hasValue(email);
         });
       });
 
-      module('When user is not admin in organization', function() {
+      module('when student authenticated by username )', async function() {
 
-        test('it should not display import button', async function(assert) {
+        test('it should open password modal window with username value', async function(assert) {
           // given
-          user = createUserManagingStudents('MEMBER');
-          await authenticateSession({
-            user_id: user.id,
-            access_token: 'aaa.' + btoa(`{"user_id":${user.id},"source":"pix","iat":1545321469,"exp":4702193958}`) + '.bbb',
-            expires_in: 3600,
-            token_type: 'Bearer token type',
-          });
-
-          // when
           await visit('/eleves');
 
+          // when
+          await click('.table tbody tr:nth-child(6) td:last-child button');
+
           // then
-          assert.dom('.button').doesNotExist();
+          assert.dom('.pix-modal-overlay').exists();
+          assert.dom('#username').hasValue(username);
+        });
+
+      });
+
+      module('when student authenticated by email )', async function() {
+
+        test('it should open password modal window with email value', async function(assert) {
+
+          // given
+          await visit('/eleves');
+
+          // when
+          await click('.table tbody tr:nth-child(6) td:last-child button');
+
+          // then
+          assert.dom('.pix-modal-overlay').exists();
+          assert.dom('#email').hasValue(email);
+        });
+
+      });
+
+    });
+
+    module('When user is admin in organization', function(hooks) {
+
+      hooks.beforeEach(async () => {
+        user = createUserManagingStudents('ADMIN');
+        await authenticateSession({
+          user_id: user.id,
+          access_token: 'aaa.' + btoa(`{"user_id":${user.id},"source":"pix","iat":1545321469,"exp":4702193958}`) + '.bbb',
+          expires_in: 3600,
+          token_type: 'Bearer token type',
         });
       });
+
+      test('it should display import button', async function(assert) {
+        // when
+        await visit('/eleves');
+
+        // then
+        assert.dom('.button').hasText('Importer (.xml)');
+      });
+
+      test('it should display success message and reload students', async function(assert) {
+        // given
+        await visit('/eleves');
+
+        const file = new Blob(['foo'], { type: 'valid-file' });
+
+        // when
+        const input = find('#students-file-upload');
+        await triggerEvent(input, 'change', { files: [file] });
+
+        // then
+        assert.dom('[data-test-notification-message="success"]').exists();
+        assert.dom('[data-test-notification-message="success"]').hasText('La liste a été importée avec succès.');
+        assert.dom('.table tbody tr').exists({ count: 1 });
+        assert.dom('.table tbody tr td:first-child').hasText('Cover');
+        assert.dom('.table tbody tr td:nth-child(2)').hasText('Harry');
+      });
+
+      test('it should display an error message when uploading an invalid file', async function(assert) {
+        // given
+        await visit('/eleves');
+
+        const file = new Blob(['foo'], { type: 'invalid-file' });
+
+        // when
+        const input = find('#students-file-upload');
+        await triggerEvent(input, 'change', { files: [file] });
+
+        // then
+        assert.dom('[data-test-notification-message="error"]').exists();
+        assert.dom('[data-test-notification-message="error"]').hasText('422 - Le détail affiché est envoyé par le back');
+      });
+
+      test('it should display an error message when uploading a file with students informations problems', async function(assert) {
+        // given
+        await visit('/eleves');
+
+        const file = new Blob(['foo'], { type: 'file-with-students-info-problems' });
+
+        // when
+        const input = find('#students-file-upload');
+        await triggerEvent(input, 'change', { files: [file] });
+
+        // then
+        assert.dom('[data-test-notification-message="error"]').exists();
+        assert.dom('[data-test-notification-message="error"]').hasText('409 - Le détail affiché est envoyé par le back');
+      });
+
+      test('it should display an error message when something unexpected went wrong on the server', async function(assert) {
+        // given
+        await visit('/eleves');
+
+        const file = new Blob(['foo'], { type: '' });
+
+        // when
+        const input = find('#students-file-upload');
+        await triggerEvent(input, 'change', { files: [file] });
+
+        // then
+        assert.dom('[data-test-notification-message="error"]').exists();
+        assert.dom('[data-test-notification-message="error"]').hasText('Quelque chose s\'est mal passé. Veuillez réessayer.');
+      });
+    });
+
+    module('When user is not admin in organization', function() {
+
+      test('it should not display import button', async function(assert) {
+        // given
+        user = createUserManagingStudents('MEMBER');
+        await authenticateSession({
+          user_id: user.id,
+          access_token: 'aaa.' + btoa(`{"user_id":${user.id},"source":"pix","iat":1545321469,"exp":4702193958}`) + '.bbb',
+          expires_in: 3600,
+          token_type: 'Bearer token type',
+        });
+
+        // when
+        await visit('/eleves');
+
+        // then
+        assert.dom('.button').doesNotExist();
+      });
+
     });
 
   });
