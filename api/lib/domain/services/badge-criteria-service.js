d8e8bc14e89cddb9f2d2b7204210b5e1b0ff2fa6
Stephane Bedeau <sbedeau@octo.com>
2020-03-10 11:39:36 +0100

    Add criterion for competence mastery percentage

--- api/lib/domain/services/badge-criteria-service.js
+++ api/lib/domain/services/badge-criteria-service.js
@@ -1,5 +1,19 @@
+const _ = require('lodash');
+
+const CLEA_CAMPAIGN_PARTICIPATION_RESULT_THRESHOLD = 85;
+const CLEA_COMPETENCE_RESULT_THRESHOLD = 75;
+
 function areBadgeCriteriaFulfilled({ campaignParticipationResult }) {
-  return (campaignParticipationResult.masteryPercentage >= 85);
+  return _verifyCampaignParticipationResultMasteryPercentageCriterion(campaignParticipationResult)
+    && _verifyEveryCompetenceResultMasteryPercentageCriterion(campaignParticipationResult.competenceResults);
+}
+
+function _verifyCampaignParticipationResultMasteryPercentageCriterion(campaignParticipationResult) {
+  return campaignParticipationResult.masteryPercentage >= CLEA_CAMPAIGN_PARTICIPATION_RESULT_THRESHOLD;
+}
+
+function _verifyEveryCompetenceResultMasteryPercentageCriterion(competenceResults) {
+  return _.every(competenceResults, (competenceResult) => competenceResult.masteryPercentage >= CLEA_COMPETENCE_RESULT_THRESHOLD);
 }
 
 module.exports = {
