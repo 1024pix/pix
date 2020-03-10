c4151a174eb9680e025c558c3499420abf18fb24
Pierre TOP (TOPI) <ptop@octo.com>
2020-03-10 18:09:53 +0100

    Add npm task to deliver in production

---
+++ scripts/release/deploy-production.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+source $(dirname $0)/common.sh
+
+VERSION_NUMBER=$1
+
+function ensure_version_exists {
+    COMMIT_REFERENCE="$(git rev-parse --verify --quiet ${VERSION_NUMBER})"
+
+    if [ -z "${COMMIT_REFERENCE}" ];
+    then
+        echo -e "${RED}Version ${VERSION_NUMBER} does not exists !${RESET_COLOR}.\n"
+        exit 1
+    fi
+}
+
+git fetch --all
+ensure_version_exists
+
+git push origin COMMIT_REFERENCE:prod
