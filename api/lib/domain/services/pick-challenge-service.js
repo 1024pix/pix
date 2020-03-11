63ef692ebc1fadd7b21cb8f12d14c815a933f70a
Guillaume Lagorce <guillaume@hey-gul.com>
2020-03-11 16:59:11 +0100

    Add acceptance test related to ensure retrieving challenge in correct locale

--- api/lib/domain/services/pick-challenge-service.js
+++ api/lib/domain/services/pick-challenge-service.js
@@ -2,13 +2,14 @@ const hashInt = require('hash-int');
 const UNEXISTING_ITEM = null;
 
 module.exports = {
+  // TODO: add locale argument (with default to 'fr-fr'?)
   pickChallenge(skills, randomSeed) {
     if (skills.length === 0) {
       return UNEXISTING_ITEM;
     }
     const key = Math.abs(hashInt(randomSeed));
     const chosenSkill = skills[key % skills.length];
-    // TODO: Filter on selected language only
+    // TODO: filter challenges to get only locale compliant ones
     return chosenSkill.challenges[key % chosenSkill.challenges.length];
   }
 };
