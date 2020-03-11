7698582770a35a8d402f7aba0b6b8819948fae62
laura-bergoens <laura.bergoens@gmail.com>
2020-03-11 16:33:30 +0100

    Add mirage config + acceptance test on session list

--- admin/mirage/config.js
+++ admin/mirage/config.js
@@ -1,39 +1,17 @@
 import { createMembership } from './handlers/memberships';
 import { getOrganizationMemberships } from './handlers/organizations';
 import { getCertificationsBySessionId } from './handlers/certifications-by-session-id';
+import { findPaginatedAndFilteredSessions } from './handlers/find-paginated-and-filtered-sessions';
 import { upload } from 'ember-file-upload/mirage';
 
 export default function() {
-
-  // These comments are here to help you get started. Feel free to delete them.
-
-  /*
-    Config (with defaults).
-
-    Note: these only affect routes defined *after* them!
-  */
-
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    http://www.ember-cli-mirage.com/docs/v0.4.x/shorthands/
-  */
   this.logging = true;
   this.urlPrefix = 'http://localhost:3000';
   this.namespace = 'api';
 
   this.get('/users');
   this.get('/certification-centers');
+  this.get('/sessions', findPaginatedAndFilteredSessions);
 
   this.post('/memberships', createMembership);
   this.get('/organizations');
