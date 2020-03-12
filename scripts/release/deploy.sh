6ecb1a72ee654ef36ba980a985fd0c7b8a19d11a
Pierre TOP (TOPI) <ptop@octo.com>
2020-03-12 12:06:17 +0100

    Add logging to release script

--- scripts/release/deploy.sh
+++ scripts/release/deploy.sh
@@ -5,7 +5,7 @@ source $(dirname "$0")/common.sh
 
 VERSION_NUMBER=${1:?Please specify release to deploy}
 
-function ensure_version_exists {
+function get_release_commit_hash {
     COMMIT_HASH="$(git rev-parse --verify --quiet "${VERSION_NUMBER}")"
 
     if [ -z "${COMMIT_HASH}" ];
@@ -14,18 +14,25 @@ function ensure_version_exists {
         exit 1
     fi
 
-    echo "$COMMIT_HASH"
+    echo "Fetched release commit hash"
 }
 
 function update_production_branch {
     git push origin "$COMMIT_HASH":prod
+
+    echo "Pushed changes on branch origin/prod"
 }
 
 function finalize_release_on_sentry {
     npx sentry-cli releases -o pix finalize "v${VERSION_NUMBER}"
+    echo "Finalized release on Sentry"
 }
 
+echo -e "Publishing a new release to ${RED}production${RESET_COLOR}.\n"
+
 git fetch
-ensure_version_exists
+get_release_commit_hash
 update_production_branch
 finalize_release_on_sentry
+
+echo -e "Release deployment ${GREEN}succeeded${RESET_COLOR}."
