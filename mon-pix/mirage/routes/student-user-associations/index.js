17380efa619d873ea9c193b763272d78b8b4f05c
laura-bergoens <laura.bergoens@gmail.com>
2020-03-12 13:36:06 +0100

    Move some routes in specific route folder

---
+++ mon-pix/mirage/routes/student-user-associations/index.js
@@ -0,0 +1,17 @@
+import { Response } from 'ember-cli-mirage';
+
+export default function index(config) {
+  config.post('/student-user-associations', () => {
+    return new Response(204);
+  });
+
+  config.put('/student-user-associations/possibilities', () => {
+    return new Response(204);
+  });
+
+  config.get('/student-user-associations', (schema, request) => {
+    const userId =  request.queryParams.userId;
+    const student = schema.students.findBy({ userId });
+    return student ? student : { data: null };
+  });
+}
