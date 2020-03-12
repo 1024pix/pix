234b2aca7bc03caad278e552323b2cc7f7cd1d35
laura-bergoens <laura.bergoens@gmail.com>
2020-03-12 10:15:58 +0100

    Reorganize certification course routes

--- mon-pix/mirage/routes/post-certification-course.js
+++
@@ -1,8 +0,0 @@
-export default function(schema) {
-  const newAssessment = {
-    type: 'CERTIFICATION',
-    certificationNumber: 'certification-course-id',
-  };
-  const assessment = schema.assessments.create(newAssessment);
-  return schema.certificationCourses.create({ id: 'certification-course-id', nbChallenges: 10, assessment });
-}
