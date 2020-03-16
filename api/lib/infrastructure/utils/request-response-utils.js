495cea227d0bbe1b6c2a95565b5eca57b186b61d
Guillaume Lagorce <guillaume@hey-gul.com>
2020-03-16 09:43:14 +0100

    Handle accept-language header in a dedicated function

--- api/lib/infrastructure/utils/request-response-utils.js
+++ api/lib/infrastructure/utils/request-response-utils.js
@@ -1,6 +1,7 @@
 const tokenService = require('../../domain/services/token-service');
+const accept = require('@hapi/accept');
 
-module.exports = { escapeFileName, extractUserIdFromRequest };
+module.exports = { escapeFileName, extractUserIdFromRequest, extractLocaleFromRequest };
 
 function escapeFileName(fileName) {
   return fileName.replace(/[^_. A-Za-z0-9-]/g, '_');
@@ -12,4 +13,13 @@ function extractUserIdFromRequest(request) {
     return tokenService.extractUserId(token);
   }
   return null;
+}
+
+function extractLocaleFromRequest(request) {
+  const defaultLocale = 'fr-fr';
+  const languageHeader = request.headers && request.headers['accept-language'];
+  if (!languageHeader) {
+    return defaultLocale;
+  }
+  return accept.language(languageHeader, ['fr', 'fr-fr']) || defaultLocale;
 }
