2d650417aa1c3efd0a446aa736e4f15f2d947651
melanieb <melanie.boudard@gmail.com>
2020-03-18 15:47:13 +0100

    Add service with all method use in appi

---
+++ api/lib/domain/services/scoring/scoring-service.js
@@ -0,0 +1,69 @@
+const {
+  PIX_COUNT_BY_LEVEL,
+  MAX_REACHABLE_LEVEL,
+  MAX_REACHABLE_PIX_BY_COMPETENCE,
+} = require('../../constants');
+
+const _ = require('lodash');
+
+function calculateScoringInformationForCompetence(knowledgeElements, blockReachablePixAndLevel = true) {
+
+  const realTotalPixScoreForCompetence = _(knowledgeElements).sumBy('earnedPix');
+  const pixScoreForCompetence = _getPixScoreForOneCompetence(realTotalPixScoreForCompetence, blockReachablePixAndLevel);
+  const currentLevel = _getCompetenceLevel(pixScoreForCompetence, blockReachablePixAndLevel);
+  const pixAheadForNextLevel = _getPixScoreAheadOfNextLevel(pixScoreForCompetence);
+  return {
+    realTotalPixScoreForCompetence,
+    pixScoreForCompetence,
+    currentLevel,
+    pixAheadForNextLevel
+  };
+}
+
+function getblockedLevel(level) {
+  return _getBlockedLevel(level);
+}
+
+function getCompetenceLevelByPix(pix) {
+  return _getCompetenceLevel(pix, true);
+}
+
+function totalUserPixScore(pixEarnedByCompetence) {
+  const pixByCompetenceLimited = _.map(pixEarnedByCompetence, (pixEarnedForOneCompetence) => _getBlockedPixScoreByCompetence(pixEarnedForOneCompetence));
+  return _.sum(pixByCompetenceLimited);
+}
+
+module.exports = {
+  calculateScoringInformationForCompetence,
+  getblockedLevel,
+  getCompetenceLevelByPix,
+  totalUserPixScore
+};
+
+function _getPixScoreForOneCompetence(exactlyEarnedPix, blockReachablePixAndLevel = true) {
+  const userEarnedPix = _.floor(exactlyEarnedPix);
+  if (blockReachablePixAndLevel) {
+    return _getBlockedPixScoreByCompetence(userEarnedPix);
+  }
+  return userEarnedPix;
+}
+
+function _getCompetenceLevel(pixScoreForCompetence, blockReachablePixAndLevel = true) {
+  const level = _.floor(pixScoreForCompetence / PIX_COUNT_BY_LEVEL);
+  if (blockReachablePixAndLevel) {
+    return _getBlockedLevel(level);
+  }
+  return level;
+}
+
+function _getPixScoreAheadOfNextLevel(earnedPix) {
+  return earnedPix % PIX_COUNT_BY_LEVEL;
+}
+
+function _getBlockedLevel(level) {
+  return Math.min(level, MAX_REACHABLE_LEVEL);
+}
+
+function _getBlockedPixScoreByCompetence(pixScore) {
+  return Math.min(pixScore, MAX_REACHABLE_PIX_BY_COMPETENCE);
+}
