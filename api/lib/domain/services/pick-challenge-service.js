614c05dfa5779f424c26fd28a671c11caddd3d05
Guillaume Lagorce <guillaume@hey-gul.com>
2020-03-18 17:04:12 +0100

    Add unit tests on pick-challenge-service

--- api/lib/domain/services/pick-challenge-service.js
+++ api/lib/domain/services/pick-challenge-service.js
@@ -3,10 +3,11 @@ const hashInt = require('hash-int');
 const UNEXISTING_ITEM = null;
 
 module.exports = {
-  pickChallenge({ skills, randomSeed, locale, key = Math.abs(hashInt(randomSeed)) }) {
+  pickChallenge({ skills, randomSeed, locale }) {
     if (skills.length === 0) {
       return UNEXISTING_ITEM;
     }
+    const key = Math.abs(hashInt(randomSeed));
     const chosenSkill = skills[key % skills.length];
     const localeChallenges = _.filter(chosenSkill.challenges, ((challenge) => challenge.locale === locale));
     if (_.isEmpty(localeChallenges)) {
