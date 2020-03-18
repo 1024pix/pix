8d1b50ca0a6af436f584df84152531cdde962eab
Etienne GIROT <egirot@octo.com>
2020-03-18 17:06:15 +0100

    Add comment on cleaBadgeCreation behavior

--- api/lib/domain/event-handlers/index.js
+++ api/lib/domain/event-handlers/index.js
@@ -1,5 +1,12 @@
 const cleaBadgeCreationHandler = {
-  handle: function() {}
+  handle: function(/*event*/) {
+    /*
+        - Decide whether the completed assessment should create a badge
+        - Create it
+        If the reaction to the event if the same as an existing command use the command
+        Otherwise perform tasks with dependencies (repositories, ...)
+     */
+  }
 };
 
 module.exports = {
