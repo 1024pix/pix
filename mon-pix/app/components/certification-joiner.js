fc1586351c7819bf5ffa64a1efb45e920649749a
Stephane Bedeau <sbedeau@octo.com>
2020-03-19 14:53:16 +0100

    Start convert of certification joiner to glimmer
    In order to finish the convertion and remove the classic decorator,
    we need to convert the didInsertElement as a render modifier or
    move it's content into an existing function.

--- mon-pix/app/components/certification-joiner.js
+++ mon-pix/app/components/certification-joiner.js
@@ -1,6 +1,8 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import classic from 'ember-classic-decorator';
 
 function _pad(num, size) {
   let s = num + '';
@@ -8,98 +10,98 @@ function _pad(num, size) {
   return s;
 }
 
-export default Component.extend({
-  store: service(),
-  peeker: service(),
-  currentUser: service(),
+@classic
+export default class CertificationJoiner extends Component {
+  @service store;
+  @service peeker;
+  @service currentUser;
 
-  isLoading: false,
-  errorMessage: null,
-  showCongratulationsBanner: true,
-  sessionId: null,
-  firstName: null,
-  lastName: null,
-  dayOfBirth: null,
-  monthOfBirth: null,
-  yearOfBirth: null,
+  @tracked isLoading = false;
+  @tracked errorMessage = null;
+  @tracked showCongratulationsBanner = true;
+  @tracked sessionId = null;
+  @tracked firstName = null;
+  @tracked lastName = null;
+  @tracked dayOfBirth = null;
+  @tracked monthOfBirth = null;
+  @tracked yearOfBirth = null;
 
-  birthdate: computed('yearOfBirth', 'monthOfBirth', 'dayOfBirth', function() {
+  get birthdate() {
     const monthOfBirth = _pad(this.monthOfBirth, 2);
     const dayOfBirth = _pad(this.dayOfBirth, 2);
     return [this.yearOfBirth, monthOfBirth, dayOfBirth].join('-');
-  }),
+  }
 
-  success() {},
+  success() {}
 
   didInsertElement() {
-    this._super(...arguments);
+    super.didInsertElement(...arguments);
     if (this.getCurrentCandidate()) {
       this.success();
     }
-  },
+  }
 
   joinCertificationSession() {
     const { firstName, lastName, birthdate, sessionId } = this;
 
-    return this.store
-      .createRecord('certification-candidate', {
-        sessionId,
-        birthdate,
-        firstName: firstName
-          ? firstName.trim()
-          : null,
-        lastName:
-        lastName
-          ? lastName.trim()
-          : null,
-      })
-      .save({ adapterOptions: { joinSession: true, sessionId } });
-  },
+    return this.store.createRecord('certification-candidate', {
+      sessionId,
+      birthdate,
+      firstName: firstName
+        ? firstName.trim()
+        : null,
+      lastName: lastName
+        ? lastName.trim()
+        : null,
+    }).save({ adapterOptions: { joinSession: true, sessionId } });
+  }
 
   getCurrentCandidate() {
     return this.peeker.findOne('certification-candidate');
-  },
+  }
 
-  actions: {
-    closeBanner() {
-      this.set('showCongratulationsBanner', false);
-    },
+  @action
+  closeBanner() {
+    this.showCongratulationsBanner = false;
+  }
 
-    async attemptNext() {
-      this.set('stepsData.joiner', { sessionId: this.sessionId });
-      this.set('isLoading', true);
-      try {
-        await this.joinCertificationSession();
-        this.success();
-      } catch (err) {
-        const currentCandidate = this.getCurrentCandidate();
-        if (currentCandidate) {
-          currentCandidate.deleteRecord();
-        }
-        this.set('isLoading', false);
-        this.set('errorMessage',
-          'Oups ! Nous ne parvenons pas à vous trouver.\nVérifiez vos informations afin de continuer ou prévenez le surveillant.');
+  @action
+  async attemptNext() {
+    this.stepsData.joiner = { sessionId: this.sessionId };
+    this.isLoading = true;
+    try {
+      await this.joinCertificationSession();
+      this.success();
+    } catch (err) {
+      const currentCandidate = this.getCurrentCandidate();
+      if (currentCandidate) {
+        currentCandidate.deleteRecord();
       }
-    },
-
-    handleDayInputChange(event) {
-      const { value } = event.target;
-
-      if (value.length === 2) {
-        document.getElementById('certificationJoinerMonthOfBirth').focus();
-      }
-    },
-
-    handleMonthInputChange(event) {
-      const { value } = event.target;
-
-      if (value.length === 2) {
-        document.getElementById('certificationJoinerYearOfBirth').focus();
-      }
-    },
-
-    handleInputFocus(value, event) {
-      event.target.select();
+      this.isLoading = false;
+      this.errorMessage = 'Oups ! Nous ne parvenons pas à vous trouver.\nVérifiez vos informations afin de continuer ou prévenez le surveillant.';
     }
-  },
-});
+  }
+
+  @action
+  handleDayInputChange(event) {
+    const { value } = event.target;
+
+    if (value.length === 2) {
+      document.getElementById('certificationJoinerMonthOfBirth').focus();
+    }
+  }
+
+  @action
+  handleMonthInputChange(event) {
+    const { value } = event.target;
+
+    if (value.length === 2) {
+      document.getElementById('certificationJoinerYearOfBirth').focus();
+    }
+  }
+
+  @action
+  handleInputFocus(value, event) {
+    event.target.select();
+  }
+}
