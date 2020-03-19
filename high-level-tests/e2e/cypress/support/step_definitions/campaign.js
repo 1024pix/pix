0d7b7cf53c5d53095efc46857cefce8f7dc13204
lisequesnel <quesnel.lise@gmail.com>
2020-03-19 19:11:26 +0100

    Refactor old steps

--- high-level-tests/e2e/cypress/support/step_definitions/campaign.js
+++ high-level-tests/e2e/cypress/support/step_definitions/campaign.js
@@ -14,15 +14,7 @@ then(`je vois le bandeau de reprise de parcours`, () => {
   cy.get('.resume-campaign-banner__container').should('exist');
 });
 
-when(`je saisis mon prénom {string}`, (firstName) => {
-  cy.get('input#firstName').type(firstName);
-});
-
-when(`je saisis mon nom {string}`, (lastName) => {
-  cy.get('input#lastName').type(lastName);
-});
-
-when(`je saisis ma date de naissance {int}-{int}-{int}`, (dayOfBirth, monthOfBirth, yearOfBirth) => {
+when(`je saisis la date de naissance {int}-{int}-{int}`, (dayOfBirth, monthOfBirth, yearOfBirth) => {
   cy.get('input#dayOfBirth').type(dayOfBirth);
   cy.get('input#monthOfBirth').type(monthOfBirth);
   cy.get('input#yearOfBirth').type(yearOfBirth);
