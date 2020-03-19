477982873d09eb19411e95722d08c69496c8ae67
lisequesnel <quesnel.lise@gmail.com>
2020-03-19 18:43:28 +0100

    Add some steps to Cypress scenarios

--- high-level-tests/e2e/cypress/support/step_definitions/campaign.js
+++ high-level-tests/e2e/cypress/support/step_definitions/campaign.js
@@ -28,10 +28,8 @@ when(`je saisis ma date de naissance {int}-{int}-{int}`, (dayOfBirth, monthOfBir
   cy.get('input#yearOfBirth').type(yearOfBirth);
 });
 
-then(`je vois la liste des campagnes`, () => {
-  cy.get('.campaign-list table tbody tr').should('have.lengthOf', 2);
-  cy.get('.campaign-list table thead').contains('Créé par');
-  cy.get('.campaign-list table tbody tr td:nth-child(2)').contains(/(.+)/);
+then(`je vois {int} campagnes`, (numberOfCampaigns) => {
+  cy.get('[aria-label="Campagne"]').should('have.lengthOf', numberOfCampaigns);
 });
 
 when(`je recherche une campagne avec le nom {string}`, (campaignSearchName) => {
@@ -42,10 +40,22 @@ then(`la liste est filtrée`, () => {
   cy.get('.campaign-list table tbody tr').should('have.lengthOf', 1);
 });
 
-when(`je clique sur la campagne {string}`, (campaignName) => {
-  cy.contains(campaignName).click();
-});
-
 then(`je vois le détail de la campagne {string}`, (campaignName) => {
   cy.get('.page__title').should('contain', campaignName);
+});
+
+then(`je vois {int} participants`, (numberOfParticipants) => {
+  cy.get('[aria-label="Participant"]').should('have.lengthOf', numberOfParticipants);
+});
+
+then(`je vois un avancement de {int}%`, (progression) => {
+  cy.get('[aria-label="Avancement"]').contains(`${progression}%`);
+});
+
+when(`je vois {int} résultats par compétence`, (numberOfResultsByCompetence) => {
+  cy.get('[aria-label="Résultats par compétence"]').should('have.lengthOf', numberOfResultsByCompetence);
+});
+
+then(`je vois la moyenne des résultats à {int}%`, (averageResult) => {
+  cy.get('[aria-label="Moyenne des résultats"]').contains(`${averageResult}%`);
 });
