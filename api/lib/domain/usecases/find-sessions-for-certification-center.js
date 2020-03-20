4f4f39b88025da11cf916f74062fd291dd77c857
laura-bergoens <laura.bergoens@gmail.com>
2020-03-20 15:19:24 +0100

    Refacto usecase to find all sessions in certifcenter

--- api/lib/domain/usecases/find-sessions-for-certification-center.js
+++ api/lib/domain/usecases/find-sessions-for-certification-center.js
@@ -1,11 +1,14 @@
 const { ForbiddenAccess } = require('../../domain/errors');
 
-module.exports = function findSessionsForCertificationCenter({ userId, certificationCenterId, sessionRepository, userRepository }) {
-  return userRepository.getWithCertificationCenterMemberships(userId)
-    .then((user) => {
-      if (user.hasAccessToCertificationCenter(certificationCenterId)) {
-        return sessionRepository.findByCertificationCenterId(certificationCenterId);
-      }
-      throw new ForbiddenAccess(`User ${userId} is not a member of certification center ${certificationCenterId}`);
-    });
+module.exports = async function findSessionsForCertificationCenter({ 
+  userId, 
+  certificationCenterId, 
+  certificationCenterMembershipRepository,
+  sessionRepository, 
+}) {
+  const hasAccess = await certificationCenterMembershipRepository.doesUserHaveMembershipToCertificationCenter(userId, certificationCenterId);
+  if (hasAccess) {
+    return sessionRepository.findByCertificationCenterId(certificationCenterId);
+  }
+  throw new ForbiddenAccess(`User ${userId} is not a member of certification center ${certificationCenterId}`);
 };
