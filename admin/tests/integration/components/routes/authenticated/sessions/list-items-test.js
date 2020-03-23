9f73f2abd65ef2e029f077dc77d8086bb97f125c
laura-bergoens <laura.bergoens@gmail.com>
2020-03-23 19:06:26 +0100

    Set the correct use of certificationCenter and certificationCenterName in PixAdmin

--- admin/tests/integration/components/routes/authenticated/sessions/list-items-test.js
+++ admin/tests/integration/components/routes/authenticated/sessions/list-items-test.js
@@ -12,9 +12,9 @@ module('Integration | Component | routes/authenticated/sessions | list-items', f
     const now = new Date();
     const displayDate = now.toLocaleString('fr-FR', { day: 'numeric', month: 'numeric', year: 'numeric' });
     const sessions = [
-      { id: 1, certificationCenter: 'Centre A', date: now, time: '14:00:00', displayDate },
-      { id: 2, certificationCenter: 'Centre B', date: now, time: '14:00:00', displayDate },
-      { id: 3, certificationCenter: 'Centre C', date: now, time: '14:00:00', displayDate },
+      { id: 1, certificationCenterName: 'Centre A', date: now, time: '14:00:00', displayDate },
+      { id: 2, certificationCenterName: 'Centre B', date: now, time: '14:00:00', displayDate },
+      { id: 3, certificationCenterName: 'Centre C', date: now, time: '14:00:00', displayDate },
     ];
 
     sessions.meta = { rowCount: 3 };
@@ -28,7 +28,7 @@ module('Integration | Component | routes/authenticated/sessions | list-items', f
 
     // then
     assert.dom('table tbody tr:first-child td:first-child').hasText(sessions[0].id.toString());
-    assert.dom('table tbody tr:first-child td:nth-child(2)').hasText(sessions[0].certificationCenter);
+    assert.dom('table tbody tr:first-child td:nth-child(2)').hasText(sessions[0].certificationCenterName);
     assert.dom('table tbody tr:first-child td:nth-child(4)').hasText(displayDate + ' à ' + sessions[0].time);
     assert.dom('table tbody tr').exists({ count: 3 });
   });
