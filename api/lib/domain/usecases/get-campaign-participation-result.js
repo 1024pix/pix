841d0723a21d0080d02d53dbda4a1921187a78d3
Etienne GIROT <egirot@octo.com>
2020-03-24 17:09:39 +0100

    Remove badge acquisition from get-campaign-results

--- api/lib/domain/usecases/get-campaign-participation-result.js
+++ api/lib/domain/usecases/get-campaign-participation-result.js
@@ -1,4 +1,3 @@
-const _ = require('lodash');
 const { UserNotAuthorizedToAccessEntity } = require('../errors');
 const CampaignParticipationResultFactory = require('../models/CampaignParticipationResultFactory');
 
