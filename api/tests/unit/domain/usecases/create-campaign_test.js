0b93bfb8bec0ff30164a20756064417c02be58df
FRIN Yvonnick <frin.yvonnick@gmail.com>
2020-03-24 16:48:46 +0100

    Update usecase create-campaign test

--- api/tests/unit/domain/usecases/create-campaign_test.js
+++ api/tests/unit/domain/usecases/create-campaign_test.js
@@ -1,8 +1,9 @@
-const { expect, sinon, domainBuilder } = require('../../../test-helper');
+const { expect, sinon, domainBuilder, catchErr } = require('../../../test-helper');
 const createCampaign = require('../../../../lib/domain/usecases/create-campaign');
 const campaignCodeGenerator = require('../../../../lib/domain/services/campaigns/campaign-code-generator');
 const campaignValidator = require('../../../../lib/domain/validators/campaign-validator');
 const { EntityValidationError, UserNotAuthorizedToCreateCampaignError } = require('../../../../lib/domain/errors');
+const _ = require('lodash');
 
 describe('Unit | UseCase | create-campaign', () => {
 
@@ -30,31 +31,31 @@ describe('Unit | UseCase | create-campaign', () => {
     sinon.stub(organizationService, 'findAllTargetProfilesAvailableForOrganization');
   });
 
-  it('should throw an EntityValidationError if campaign is not valid', () => {
+  it('should throw an EntityValidationError if campaign is not valid', async () => {
     // given
     campaignValidator.validate.throws(new EntityValidationError({ invalidAttributes: [] }));
 
     // when
-    const promise = createCampaign({ campaign: campaignToCreate, campaignRepository, userRepository, organizationService });
+    const error = await catchErr(createCampaign)({ campaign: campaignToCreate, campaignRepository, userRepository, organizationService });
 
     // then
-    return expect(promise).to.be.rejectedWith(EntityValidationError);
+    expect(error).to.be.instanceOf(EntityValidationError);
   });
 
-  it('should throw an error if user do not have an access to the campaign organization', () => {
+  it('should throw an error if user do not have an access to the campaign organization', async () => {
     // given
     const organizationIdDifferentFromCampaign = 98437;
     campaignValidator.validate.returns();
     _stubGetUserWithOrganizationsAccesses(organizationIdDifferentFromCampaign);
 
     // when
-    const promise = createCampaign({ campaign: campaignToCreate, campaignRepository, userRepository, organizationService  });
+    const error = await catchErr(createCampaign)({ campaign: campaignToCreate, campaignRepository, userRepository, organizationService });
 
     // then
-    return expect(promise).to.be.rejectedWith(UserNotAuthorizedToCreateCampaignError);
+    expect(error).to.be.instanceOf(UserNotAuthorizedToCreateCampaignError);
   });
 
-  it('should generate a new code to the campaign', () => {
+  it('should generate a new code to the campaign', async () => {
     // given
     campaignValidator.validate.returns();
     _stubGetUserWithOrganizationsAccesses(campaignToCreate.organizationId);
@@ -62,16 +63,13 @@ describe('Unit | UseCase | create-campaign', () => {
     campaignRepository.save.resolves(savedCampaign);
 
     // when
-    const promise = createCampaign({ campaign: campaignToCreate, campaignRepository, userRepository, organizationService });
+    await createCampaign({ campaign: campaignToCreate, campaignRepository, userRepository, organizationService });
 
     // then
-    return promise.then(() => {
-      expect(campaignCodeGenerator.generate).to.have.been.called;
-    });
-
+    expect(campaignCodeGenerator.generate).to.have.been.called;
   });
 
-  it('should save the campaign with name, userId, organizationId and generated code', () => {
+  it('should save the campaign with name, type, userId, organizationId and generated code', async () => {
     // given
     campaignValidator.validate.returns();
     _stubGetUserWithOrganizationsAccesses(campaignToCreate.organizationId);
@@ -79,21 +77,18 @@ describe('Unit | UseCase | create-campaign', () => {
     campaignRepository.save.resolves(savedCampaign);
 
     // when
-    const promise = createCampaign({ campaign: campaignToCreate, campaignRepository, userRepository, organizationService });
+    await createCampaign({ campaign: campaignToCreate, campaignRepository, userRepository, organizationService });
 
     // then
-    return promise.then(() => {
-      expect(campaignRepository.save).to.have.been.called;
+    const [campaignToCreateWithCode] = campaignRepository.save.firstCall.args;
 
-      const campaignToCreateWithCode = campaignRepository.save.firstCall.args[0];
-      expect(campaignToCreateWithCode.name).to.equal(campaignToCreate.name);
-      expect(campaignToCreateWithCode.code).to.equal(availableCampaignCode);
-      expect(campaignToCreateWithCode.userId).to.equal(campaignToCreate.userId);
-      expect(campaignToCreateWithCode.organizationId).to.equal(campaignToCreate.organizationId);
+    expect(campaignToCreateWithCode).to.deep.include({
+      ..._.pick(campaignToCreate, ['name', 'userId', 'type', 'organizationId']),
+      code: availableCampaignCode
     });
   });
 
-  it('should return the newly created campaign', () => {
+  it('should return the newly created campaign', async () => {
     // given
     campaignValidator.validate.returns();
     _stubGetUserWithOrganizationsAccesses(campaignToCreate.organizationId);
@@ -101,12 +96,10 @@ describe('Unit | UseCase | create-campaign', () => {
     campaignRepository.save.resolves(savedCampaign);
 
     // when
-    const promise = createCampaign({ campaign: campaignToCreate, campaignRepository, userRepository, organizationService });
+    const campaign = await createCampaign({ campaign: campaignToCreate, campaignRepository, userRepository, organizationService });
 
     // then
-    return promise.then((campaign) => {
-      expect(campaign).to.deep.equal(savedCampaign);
-    });
+    expect(campaign).to.deep.equal(savedCampaign);
   });
 
 });
