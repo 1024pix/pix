7f2bd1221c3d5755975b73b2402709fdfaa6396b
Elodie Pelcat <elodiepelcat@pix.fr>
2020-03-25 18:09:32 +0100

    Add tests details and participants in campaign navigation

--- orga/tests/integration/components/routes/authenticated/campaigns/details-item-test.js
+++ orga/tests/integration/components/routes/authenticated/campaigns/details-item-test.js
@@ -30,8 +30,9 @@ module('Integration | Component | routes/authenticated/campaign | details-item',
     // then
     assert.dom('.campaign-details-header__title').hasText('campagne 1');
   });
+
   module('Campaign details header', function() {
-    test('it should display the campaign report', async function (assert) {
+    test('it should display the campaign report', async function(assert) {
       // given
       const campaignReport = run(() => store.createRecord('campaignReport', {
         id: 1,
@@ -77,6 +78,52 @@ module('Integration | Component | routes/authenticated/campaign | details-item',
       // then
       assert.dom('.campaign-details-header-report__info:nth-child(2) .campaign-details-content__text').hasText('-');
       assert.dom('.campaign-details-header-report__shared .campaign-details-content__text').hasText('-');
+    });
+  });
+
+  module('Navigation', function() {
+    test('it should display campaign details item', async function(assert) {
+      // given
+      const campaign = store.createRecord('campaign', {});
+
+      this.set('campaign', campaign);
+
+      // when
+      await render(hbs`<Routes::Authenticated::Campaigns::DetailsItem @campaign={{campaign}}/>`);
+      // then
+      assert.dom('[aria-label="Détails de la campagne"]').hasText('Détails');
+    });
+    module('When campaign type is TEST_GIVEN', function() {
+      test('it should display participation item', async function(assert) {
+        // given
+        const campaignReport = store.createRecord('campaignReport', {
+          participationsCount: 1,
+        });
+        const campaign = store.createRecord('campaign', {
+          campaignReport,
+          type: 'TEST-GIVEN',
+        });
+
+        this.set('campaign', campaign);
+
+        // when
+        await render(hbs`<Routes::Authenticated::Campaigns::DetailsItem @campaign={{campaign}}/>`);
+        // then
+        assert.dom('[aria-label="Participants"]').hasText('Participants (1)');
+      });
+      test('it should display collective results item', async function(assert) {
+        // given
+        const campaign = store.createRecord('campaign', {
+          type: 'TEST-GIVEN',
+        });
+
+        this.set('campaign', campaign);
+
+        // when
+        await render(hbs`<Routes::Authenticated::Campaigns::DetailsItem @campaign={{campaign}}/>`);
+        // then
+        assert.dom('[aria-label="Résultats collectifs"]').hasText('Résultats collectifs');
+      });
     });
   });
 });
