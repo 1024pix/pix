6ab74c399632c38c00e5ee92ac5dbf8817ae3487
lisequesnel <quesnel.lise@gmail.com>
2020-03-25 10:14:50 +0100

    Rename addTargetProfilesToOrganization and add test for it

--- api/lib/infrastructure/repositories/target-profile-share-repository.js
+++ api/lib/infrastructure/repositories/target-profile-share-repository.js
@@ -2,10 +2,11 @@ const Bookshelf = require('../bookshelf');
 
 module.exports = {
 
-  addToOrganization({ organizationId, targetProfileIdList }) {
+  addTargetProfilesToOrganization({ organizationId, targetProfileIdList }) {
     const targetProfileShareToAdd = targetProfileIdList.map((targetProfileId) => {
       return { organizationId, targetProfileId };
     });
-    return Bookshelf.knex.batchInsert('target-profile-shares', targetProfileShareToAdd).then(() => null);
+    return Bookshelf.knex.batchInsert('target-profile-shares', targetProfileShareToAdd)
+      .then(() => null);
   },
 };
