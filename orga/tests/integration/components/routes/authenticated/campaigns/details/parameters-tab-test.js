3a1337114e3f3c241aea276eab1c7bf72405e03e
ARJ <arj@octo.com>
2020-03-25 14:36:03 +0100

    Hide campaign target profile for profiles collection campaigns

--- orga/tests/integration/components/routes/authenticated/campaigns/details/parameters-tab-test.js
+++ orga/tests/integration/components/routes/authenticated/campaigns/details/parameters-tab-test.js
@@ -6,7 +6,7 @@ import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | routes/authenticated/campaign/details | parameters-tab', function(hooks) {
   setupRenderingTest(hooks);
-  
+
   let store;
 
   hooks.beforeEach(function() {
@@ -15,24 +15,42 @@ module('Integration | Component | routes/authenticated/campaign/details | parame
     });
   });
 
-  test('it should display target profile related to campaign', async function(assert) {
-    // given
-    const targetProfile = run(() => store.createRecord('targetProfile', {
-      id: 1,
-      name: 'profil cible de la campagne 1',
-    }));
-    const campaign = run(() => store.createRecord('campaign', {
-      id: 1,
-      name: 'campagne 1',
-      targetProfile
-    }));
+  module('on TargetProfile display', function() {
+    module('when type is TEST_GIVEN', function() {
+      test('it should display target profile related to campaign', async function(assert) {
+        // given
+        const targetProfile = store.createRecord('targetProfile', {
+          name: 'profil cible de la campagne 1',
+        });
+        const campaign = store.createRecord('campaign', {
+          type: 'TEST_GIVEN',
+          targetProfile
+        });
 
-    this.set('campaign', campaign);
+        this.set('campaign', campaign);
 
-    // when
-    await render(hbs`{{routes/authenticated/campaigns/details/parameters-tab campaign=campaign}}`);
+        // when
+        await render(hbs`<Routes::Authenticated::Campaigns::Details::ParametersTab @campaign={{campaign}}/>`);
 
-    // then
-    assert.dom('.campaign-details-content:first-child .campaign-details-content__text').hasText('profil cible de la campagne 1');
+        // then
+        assert.dom('[aria-label="Détails de la campagne"]').includesText('profil cible de la campagne 1');
+      });
+    });
+
+    module('when type is PROFILES_COLLECTION', function() {
+      test('it should not display target profile', async function(assert) {
+        const campaign = store.createRecord('campaign', {
+          type: 'PROFILES_COLLECTION'
+        });
+
+        this.set('campaign', campaign);
+
+        // when
+        await render(hbs`<Routes::Authenticated::Campaigns::Details::ParametersTab @campaign={{campaign}}/>`);
+
+        // then
+        assert.dom('[aria-label="Détails de la campagne"]').doesNotHaveTextContaining('Profil cible');
+      });
+    });
   });
 });
