dee1f7b0d7f402ac07dbcc7a826213cfe09deb69
laura-bergoens <laura.bergoens@gmail.com>
2020-03-26 17:52:43 +0100

    Update publishedAt timestamp in publication usecase

--- api/lib/domain/usecases/update-publication-session.js
+++ api/lib/domain/usecases/update-publication-session.js
@@ -2,7 +2,14 @@ module.exports = async function updatePublicationSession({
   sessionId,
   toPublish,
   certificationRepository,
+  sessionRepository,
+  publishedAt = new Date(),
 }) {
   await certificationRepository.updatePublicationStatusesBySessionId(sessionId, toPublish);
-  return;
+  if (toPublish) {
+    const session = await sessionRepository.updatePublishedAt({ id: sessionId, publishedAt });
+    return { publishedAtUpdated: true, session };
+  }
+  const session = await sessionRepository.get(sessionId);
+  return { publishedAtUpdated: false, session };
 };
