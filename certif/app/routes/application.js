58187e00bfa5596d505ac36ba8ecd38da8dd6b08
laura-bergoens <laura.bergoens@gmail.com>
2020-03-26 10:19:24 +0100

    Transform mixin into native class mixin

--- certif/app/routes/application.js
+++ certif/app/routes/application.js
@@ -1,26 +1,25 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-import { inject as service } from '@ember/service';
-
-export default Route.extend(ApplicationRouteMixin, {
-  routeAfterAuthentication: 'authenticated',
-  currentUser: service(),
+export default class ApplicationRoute extends Route.extend(ApplicationRouteMixin) {
+  @service currentUser;
+  routeAfterAuthentication = 'authenticated';
 
   beforeModel() {
     return this._loadCurrentUser();
-  },
+  }
 
   async sessionAuthenticated() {
     await this._loadCurrentUser();
     this.transitionTo(this.routeAfterAuthentication);
-  },
+  }
 
   sessionInvalidated() {
     this.transitionTo('login');
-  },
+  }
 
   _loadCurrentUser() {
     return this.currentUser.load();
-  },
-});
+  }
+}
