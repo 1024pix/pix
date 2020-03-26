b6ec2fce44c3fb36da18e1b84bbc5996fe59c464
Krysthalia <ang.schall@gmail.com>
2020-03-26 16:45:45 +0100

    admin: change certification search default page

---
+++ admin/tests/unit/controllers/authenticated/certifications-test.js
@@ -0,0 +1,24 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import sinon from 'sinon';
+
+module('Unit | Controller | authenticated/certifications', function(hooks) {
+  setupTest(hooks);
+
+  test('#loadCertification', function(assert) {
+    // given
+    const controller = this.owner.lookup('controller:authenticated.certifications');
+    const routerStub = {
+      transitionTo: sinon.stub().resolves(),
+    };
+    controller.router = routerStub;
+    controller.inputId = 5;
+
+    // when
+    controller.send('loadCertification');
+
+    // then
+    sinon.assert.calledWith(routerStub.transitionTo, 'authenticated.certifications.single.info', 5);
+    assert.ok(controller);
+  });
+});
