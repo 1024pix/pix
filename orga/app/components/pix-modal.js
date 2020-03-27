d11342cc321c40e7575c728e2b5cfc1119d94b8f
Mathieu Gilet <mathieu.gilet@zenika.com>
2020-03-27 17:03:19 +0100

    Update password-reset-window design

--- orga/app/components/pix-modal.js
+++ orga/app/components/pix-modal.js
@@ -6,26 +6,22 @@ export default ModalDialog.extend(EmberKeyboardMixin, {
 
   translucentOverlay: true,
   targetAttachment: 'none',
-  wrapperClass: 'centered-scrolling-wrapper',
-  overlayClass: 'centered-scrolling-overlay',
-  containerClass: 'centered-scrolling-container',
+  wrapperClass: 'pix-modal-wrapper',
+  overlayClass: 'pix-modal-overlay',
 
   init() {
     this._super(...arguments);
+    this.set('containerClassNames', ['pix-modal-dialog']);
     this.set('keyboardActivated', true);
-    this.set('wrapperClassNames', ['pix-modal-wrapper']);
-    this.set('overlayClassNames', ['pix-modal-overlay']);
   },
 
   didRender() {
     this._super(...arguments);
     document.querySelector('#modal-overlays').classList.add('active');
-    document.body.classList.add('centered-modal-showing');
   },
 
   willDestroyElement() {
     document.querySelector('#modal-overlays').classList.remove('active');
-    document.body.classList.remove('centered-modal-showing');
     this._super(...arguments);
   },
 
