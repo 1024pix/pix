126e28388ee1a21795631983ec46c0747978cb4b
laura-bergoens <laura.bergoens@gmail.com>
2020-03-28 09:35:00 +0100

    Add user serializer method to get minimal info

--- api/tests/unit/infrastructure/serializers/jsonapi/user-serializer_test.js
+++ api/tests/unit/infrastructure/serializers/jsonapi/user-serializer_test.js
@@ -174,6 +174,36 @@ describe('Unit | Serializer | JSONAPI | user-serializer', () => {
     });
   });
 
+  describe('#serializeMinimal', () => {
+    const minimalJsonApi = {
+      data: {
+        id: '1',
+        type: 'users',
+        attributes: {
+          'first-name': 'Luke',
+          'last-name': 'Skywalker',
+          email: 'lskywalker@deathstar.empire',
+        },
+      }
+    };
+
+    it('should serialize minimal info', () => {
+      // given
+      const modelObject = new User({
+        id: 1,
+        firstName: 'Luke',
+        lastName: 'Skywalker',
+        email: 'lskywalker@deathstar.empire',
+      });
+
+      // when
+      const json = serializer.serializeMinimal(modelObject);
+
+      // then
+      expect(json).to.be.deep.equal(minimalJsonApi);
+    });
+  });
+
   describe('#deserialize()', () => {
 
     it('should convert JSON API data into an User model object', () => {
