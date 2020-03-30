c806a994bf96a53f7c25a5af8a27a023c340ee64
Stephane Bedeau <sbedeau@octo.com>
2020-03-30 12:26:27 +0200

    Convert transforms to native syntax

--- mon-pix/app/transforms/date-only.js
+++ mon-pix/app/transforms/date-only.js
@@ -1,14 +1,17 @@
+import classic from 'ember-classic-decorator';
 import Transform from '@ember-data/serializer/transform';
 
-export default Transform.extend({
-  serialize: function(date) {
+@classic
+export default class DateOnly extends Transform {
+  serialize(date) {
     return date;
-  },
-  deserialize: function(date) {
+  }
+
+  deserialize(date) {
     const dateRegex = '^[0-9]{4}-[0-9]{2}-[0-9]{2}$';
     if (date && date.search(dateRegex) === 0) {
       return date;
     }
     return null;
   }
-});
+}
