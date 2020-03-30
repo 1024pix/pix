ce8b455c720b7a660d1ae9aa41ffdc9d05edec88
lisequesnel <quesnel.lise@gmail.com>
2020-03-30 18:20:23 +0200

    Add a field to select campaign goal

--- orga/app/components/routes/authenticated/campaigns/new-item.js
+++ orga/app/components/routes/authenticated/campaigns/new-item.js
@@ -1,11 +1,13 @@
 import { action, computed } from '@ember/object';
 import Component from '@ember/component';
+import { tracked } from '@glimmer/tracking';
 
 export default class NewItem extends Component {
 
   campaign = null;
   targetProfiles = null;
   wantIdPix = false;
+  @tracked isCampaignGoalAssessment = null;
 
   @computed('wantIdPix')
   get notWantIdPix() {
@@ -29,5 +31,15 @@ export default class NewItem extends Component {
     const selectedTargetProfile = this.targetProfiles
       .find((targetProfile) => targetProfile.get('id') === event.target.value);
     this.campaign.set('targetProfile', selectedTargetProfile);
+  }
+
+  @action
+  setCampaignGoal(event) {
+    if (event.target.value === 'collect-participants-profile') {
+      this.isCampaignGoalAssessment = false;
+      return this.campaign.type = 'PROFILES_COLLECTION';
+    }
+    this.isCampaignGoalAssessment = true;
+    return this.campaign.type = 'ASSESSMENT';
   }
 }
