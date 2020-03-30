7f7ab332445e8bd245ced6ed93b36abd9f661fd7
Stephane Bedeau <sbedeau@octo.com>
2020-03-30 10:36:31 +0200

    Convert splash service

--- mon-pix/app/services/splash.js
+++ mon-pix/app/services/splash.js
@@ -1,10 +1,12 @@
+import classic from 'ember-classic-decorator';
 import Service from '@ember/service';
 
-export default Service.extend({
+@classic
+export default class SplashService extends Service {
   hide() {
     const splash = document.getElementById('app-splash');
     if (splash) {
       splash.parentNode.removeChild(splash);
     }
   }
-});
+}
