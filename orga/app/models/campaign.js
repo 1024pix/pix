578ecef9f93a4d4fb055ce94a40626bae933f889
Elodie Pelcat <elodiepelcat@pix.fr>
2020-03-31 11:50:36 +0200

    Octanify campaign model

--- orga/app/models/campaign.js
+++ orga/app/models/campaign.js
@@ -1,60 +1,102 @@
 import DS from 'ember-data';
 import { computed } from '@ember/object';
+import { equal } from '@ember/object/computed';
 import ENV from 'pix-orga/config/environment';
+const { Model, attr, belongsTo } = DS;
 
 const PROFILES_COLLECTION_TEXT = 'Collecte de profils';
 const ASSESSMENT_TEXT = 'Évaluation';
 
-export default DS.Model.extend({
-  name: DS.attr('string'),
-  code: DS.attr('string'),
-  type: DS.attr('string'),
-  title: DS.attr('string'),
-  createdAt: DS.attr('date'),
-  creator: DS.belongsTo('user'),
-  archivedAt: DS.attr('date'),
-  idPixLabel: DS.attr('string'),
-  customLandingPageText: DS.attr('string'),
+export default class Campaign extends Model {
+  @attr('string')
+  name;
+
+  @attr('string')
+  code;
+
+  @attr('string')
+  type;
+
+  @attr('string')
+  title;
+
+  @attr('date')
+  createdAt;
+
+  @belongsTo('user')
+  creator;
+
+  @attr('date')
+  archivedAt;
+
+  @attr('string')
+  idPixLabel;
+
+  @attr('string')
+  customLandingPageText;
+
   // TODO remove organizationId and work only with the relationship
-  organizationId: DS.attr('number'),
-  organization: DS.belongsTo('organization'),
-  tokenForCampaignResults: DS.attr('string'),
-  targetProfile: DS.belongsTo('target-profile'),
-  campaignReport: DS.belongsTo('campaign-report'),
-  campaignCollectiveResult: DS.belongsTo('campaign-collective-result'),
-  campaignAnalysis: DS.belongsTo('campaign-analysis'),
 
-  isTypeProfilesCollection: computed.equal('type', 'PROFILES_COLLECTION'),
-  isTypeAssessment: computed.equal('type', 'ASSESSMENT'),
+  @attr('number')
+  organizationId;
 
-  readableType: computed('isTypeProfilesCollection', function() {
+  @belongsTo('organization')
+  organization;
+
+  @attr('string')
+  tokenForCampaignResults;
+
+  @belongsTo('target-profile')
+  targetProfile;
+
+  @belongsTo('campaign-report')
+  campaignReport;
+
+  @belongsTo('campaign-collective-result')
+  campaignCollectiveResult;
+
+  @belongsTo('campaign-analysis')
+  campaignAnalysis;
+
+  @equal('type', 'PROFILES_COLLECTION')
+  isTypeProfilesCollection;
+
+  @equal('type', 'ASSESSMENT')
+  isTypeAssessment;
+
+  @computed('isTypeProfilesCollection')
+  get readableType() {
     return this.isTypeProfilesCollection ? PROFILES_COLLECTION_TEXT : ASSESSMENT_TEXT;
-  }),
+  }
 
-  url: computed('code', function() {
+  @computed('code')
+  get url() {
     const code = this.code;
     return `${ENV.APP.CAMPAIGNS_ROOT_URL}${code}`;
-  }),
+  }
 
-  urlToResult: computed('id', 'tokenForCampaignResults', function() {
+  @computed('id', 'tokenForCampaignResults')
+  get urlToResult() {
     return `${ENV.APP.API_HOST}/api/campaigns/${this.id}/csvResults?accessToken=${this.tokenForCampaignResults}`;
-  }),
+  }
 
-  isArchived: computed('archivedAt', function() {
+  @computed('archivedAt')
+  get isArchived() {
     return Boolean(this.archivedAt);
-  }),
+  }
 
-  canBeArchived: computed('isTypeAssessment', 'isArchived', function() {
+  @computed('isTypeAssessment', 'isArchived')
+  get canBeArchived() {
     return this.isTypeAssessment && ! this.isArchived;
-  }),
+  }
 
   async archive() {
     await this.store.adapterFor('campaign').archive(this);
     return this.store.findRecord('campaign', this.id, { include: 'targetProfile' });
-  },
+  }
 
   async unarchive() {
     await this.store.adapterFor('campaign').unarchive(this);
     return this.store.findRecord('campaign', this.id, { include: 'targetProfile' });
   }
-});
+}
