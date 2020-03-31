d33099aadc4a5e86bc1e4b0d944edae80b80e03a
Etienne GIROT <egirot@octo.com>
2020-03-31 11:29:02 +0200

    Move injectDependencies to dependency-injection

---
+++ api/lib/infrastructure/utils/dependency-injection.js
@@ -0,0 +1,11 @@
+const _ = require('lodash');
+
+function injectDefaults(defaults, targetFn) {
+  return (args) => targetFn(Object.assign(Object.create(defaults), args));
+}
+
+function injectDependencies(toBeInjected, dependencies) {
+  return _.mapValues(toBeInjected, _.partial(injectDefaults, dependencies));
+}
+
+module.exports = { injectDependencies };
