9589d234dda0a01ef9d0e3f06b36c78b94af3b95
laura-bergoens <laura.bergoens@gmail.com>
2020-03-31 13:29:05 +0200

    orga: Refacto application adapter into JS class

--- orga/app/adapters/application.js
+++ orga/app/adapters/application.js
@@ -1,29 +1,19 @@
-import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
-import DS from 'ember-data';
+import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import ENV from 'pix-orga/config/environment';
 
-export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
-  host: ENV.APP.API_HOST,
-  namespace: 'api',
+export default class ApplicationAdapter extends JSONAPIAdapter.extend(DataAdapterMixin) {
+  host = ENV.APP.API_HOST;
+  namespace = 'api';
 
-  session: service(),
-
-  /*
-  * Appelé uniquement lorsqu'on est connecté
-  * ref: http://ember-simple-auth.com/api/classes/BaseAuthorizer.html#method_authorize
-  */
-  authorize(xhr) {
-    const { access_token } = this.get('session.data.authenticated');
-    xhr.setRequestHeader('Authorization', `Bearer ${access_token}`);
-  },
-
-  headers: computed('session.data.authenticated.access_token', function() {
+  @computed('session.data.authenticated.access_token')
+  get headers() {
     const headers = {};
     if (this.session.isAuthenticated) {
       headers['Authorization'] = `Bearer ${this.session.data.authenticated.access_token}`;
     }
+
     return headers;
-  })
-});
+  }
+}
