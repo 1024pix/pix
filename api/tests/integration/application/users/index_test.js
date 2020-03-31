6c0b9c9a2104226d944736b4ba324e2e410c419d
jbuget <jbuget@gmail.com>
2020-03-31 18:26:21 +0200

    [api] Add route GET /api/users/{id}
    This route is restricted to Pix Masters.

---
+++ api/tests/integration/application/users/index_test.js
@@ -0,0 +1,31 @@
+const { expect, sinon, HttpTestServer } = require('../../../test-helper');
+
+const securityController = require('../../../../lib/interfaces/controllers/security-controller');
+const userController = require('../../../../lib/application/users/user-controller');
+const moduleUnderTest = require('../../../../lib/application/users');
+
+describe('Integration | Application | Users | Routes', () => {
+
+  let httpTestServer;
+
+  beforeEach(() => {
+    sinon.stub(securityController, 'checkUserHasRolePixMaster').callsFake((request, h) => h.response(true));
+    sinon.stub(userController, 'getUserDetail').returns('ok');
+    httpTestServer = new HttpTestServer(moduleUnderTest);
+  });
+
+  describe('GET /api/users/{id}', () => {
+
+    it('should exist', async () => {
+      // given
+      const method = 'GET';
+      const url = '/api/users/123';
+
+      // when
+      const response = await httpTestServer.request(method, url);
+
+      // then
+      expect(response.statusCode).to.equal(200);
+    });
+  });
+});
