b8aac4d7db2f7feea73f0581a87a56328e3dbaad
laura-bergoens <laura.bergoens@gmail.com>
2020-04-03 10:24:42 +0200

    admin: Rename ApplicationAdapter class correctly

--- admin/app/adapters/application.js
+++ admin/app/adapters/application.js
@@ -2,7 +2,7 @@ import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import ENV from 'pix-admin/config/environment';
 
-export default class Application extends JSONAPIAdapter.extend(DataAdapterMixin) {
+export default class ApplicationAdapter extends JSONAPIAdapter.extend(DataAdapterMixin) {
 
   host = ENV.APP.API_HOST;
   namespace = 'api';
