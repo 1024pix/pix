d81f8983dba038b93897347bfe40d44e443fb135
Guillaume Lagorce <guillaume@hey-gul.com>
2020-04-06 12:19:17 +0200

    Fix updating tutorial when model changes

--- mon-pix/app/routes/user-tutorials.js
+++ mon-pix/app/routes/user-tutorials.js
@@ -3,8 +3,8 @@ import Route from '@ember/routing/route';
 export default class UserTutorialsRoute extends Route {
 
   async model() {
-    const tutorials = await this.store.findAll('tutorial');
-    return tutorials.filter((tutorial) => tutorial.isSaved);
+    const tutorials = await this.store.findAll('tutorial', { reload: true });
+    return tutorials.filterBy('isSaved');
   }
 
 }
