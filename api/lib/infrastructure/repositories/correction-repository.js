bdfe8a7434455a4a9d9f77b726dfc00334bd2f46
Guillaume Lagorce <guillaume@hey-gul.com>
2020-04-07 00:03:13 +0200

    BSR: Refactor correction-repository with async/await

--- api/lib/infrastructure/repositories/correction-repository.js
+++ api/lib/infrastructure/repositories/correction-repository.js
@@ -9,71 +9,54 @@ const VALIDATED_HINT_STATUSES = ['Validé', 'pré-validé'];
 
 module.exports = {
 
-  getByChallengeId({ challengeId, userId }) {
-    let challengeDataObject;
-    let hintsForChallenge;
-    let skillsForChallenge;
+  async getByChallengeId({ challengeId, userId }) {
+    const challenge = await challengeDatasource.get(challengeId);
+    const skills = await _getSkills(challenge);
+    const hints = await _getHints(skills);
 
-    return challengeDatasource.get(challengeId)
-      .then((retrievedChallengeDataObject) => {
-        challengeDataObject = retrievedChallengeDataObject;
-        return retrievedChallengeDataObject;
-      })
-      .then(_getSkillDataObjects)
-      .then((skills) => {
-        skillsForChallenge = skills;
-        return _filterSkillDataObjectsWithValidatedHint(skillsForChallenge);
-      })
-      .then(_convertSkillDataObjectsToHints)
-      .then((hints) => {
-        hintsForChallenge = hints;
-        const tutorialsIds = _(skillsForChallenge)
-          .map((skill) => skill.tutorialIds)
-          .filter((tutorialId) => !_.isEmpty(tutorialId))
-          .flatten()
-          .uniq()
-          .value();
+    const tutorials = await _getTutorials(userId, skills, 'tutorialIds');
+    const learningMoreTutorials = await _getTutorials(userId, skills, 'learningMoreTutorialIds');
 
-        const learningMoreTutorialIds = _(skillsForChallenge)
-          .map((skill) => skill.learningMoreTutorialIds)
-          .filter((tutorialId) => !_.isEmpty(tutorialId))
-          .flatten()
-          .uniq()
-          .value();
-
-        return Promise.all([
-          tutorialRepository.findByRecordIdsForCurrentUser({ ids: tutorialsIds, userId }),
-          tutorialRepository.findByRecordIdsForCurrentUser({ ids: learningMoreTutorialIds, userId }),
-        ]);
-      })
-      .then(([tutorials, learningMoreTutorials]) => {
-
-        return new Correction({
-          id: challengeDataObject.id,
-          solution: challengeDataObject.solution,
-          hints: hintsForChallenge,
-          tutorials: tutorials,
-          learningMoreTutorials: learningMoreTutorials,
-        });
-      });
+    return new Correction({
+      id: challenge.id,
+      solution: challenge.solution,
+      hints,
+      tutorials,
+      learningMoreTutorials: learningMoreTutorials,
+    });
   }
 };
 
-function _getSkillDataObjects(challengeDataObject) {
+async function _getHints(skills) {
+  const skillsWithHints = await _filterSkillsWithValidatedHint(skills);
+  return _convertSkillsToHints(skillsWithHints);
+}
+
+function _getSkills(challengeDataObject) {
   const skillDataObjectPromises = challengeDataObject.skillIds.map(skillDatasource.get);
   return Promise.all(skillDataObjectPromises);
 }
 
-function _filterSkillDataObjectsWithValidatedHint(skillDataObjects) {
+function _filterSkillsWithValidatedHint(skillDataObjects) {
   return skillDataObjects.filter((skillDataObject) => VALIDATED_HINT_STATUSES.includes(skillDataObject.hintStatus));
 }
 
-function _convertSkillDataObjectsToHints(skillDataObjects) {
+function _convertSkillsToHints(skillDataObjects) {
   return skillDataObjects.map((skillDataObject) => {
     return new Hint({
       skillName: skillDataObject.name,
       value: skillDataObject.hint
     });
   });
+}
+
+async function _getTutorials(userId, skills, tutorialIdsProperty) {
+  const tutorialsIds = _(skills)
+    .map((skill) => skill[tutorialIdsProperty])
+    .filter((tutorialId) => !_.isEmpty(tutorialId))
+    .flatten()
+    .uniq()
+    .value();
+  return tutorialRepository.findByRecordIdsForCurrentUser({ ids: tutorialsIds, userId });
 }
 
