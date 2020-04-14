860ff0ad74e8681e8b45b713472758e4105ae15c
ARJ <arj@octo.com>
2020-04-14 17:46:11 +0200

    [API] create model for tube recommendation

--- api/lib/domain/models/CampaignTubeRecommendation.js
+++ api/lib/domain/models/CampaignTubeRecommendation.js
@@ -1,25 +1,59 @@
+const recommendationService = require('../services/recommendation-service');
+
+const _ = require('lodash');
+
 class CampaignTubeRecommendation {
 
   constructor({
     // attributes
     campaignId,
-    tubeId,
-    competenceId,
-    competenceName,
-    tubePracticalTitle,
-    areaColor,
+    tube,
+    competence,
+    skills,
+    validatedKnowledgeElements,
+    participantsCount,
+    maxSkillLevelInTargetProfile
   } = {}) {
     // attributes
     this.campaignId = campaignId;
-    this.tubeId = tubeId;
-    this.competenceId = competenceId;
-    this.competenceName = competenceName;
-    this.tubePracticalTitle = tubePracticalTitle;
-    this.areaColor = areaColor;
+    this.tubeId = tube.id;
+    this.competenceId = competence.id;
+    this.competenceName = competence.name;
+    this.tubePracticalTitle = tube.practicalTitle;
+    this.areaColor = competence.area.color;
+    this.averageScore = null;
+    if (participantsCount) {
+      this.averageScore = this._computeAverageScore(validatedKnowledgeElements, skills, participantsCount, maxSkillLevelInTargetProfile);
+    }
   }
 
   get id() {
     return `${this.campaignId}_${this.tubeId}`;
+  }
+
+  _computeAverageScore(validatedKnowledgeElements, skills, participantsCount, maxSkillLevelInTargetProfile) {
+
+    const knowledgeElementsByParticipants = this._knowledgeElementsByParticipant(validatedKnowledgeElements, participantsCount);
+
+    const sumOfScores = _(knowledgeElementsByParticipants)
+      .values()
+      .sumBy((knowledgeElements) =>
+        recommendationService.computeRecommendationScore(
+          skills,
+          maxSkillLevelInTargetProfile,
+          knowledgeElements
+        ));
+
+    return sumOfScores / participantsCount;
+
+  }
+
+  _knowledgeElementsByParticipant(validatedKnowledgeElements, participantsCount) {
+    const knowledgeElementsByParticipant = _.toArray(_.groupBy(validatedKnowledgeElements, 'userId'));
+
+    const participantsCountWithoutKnowledgeElements = participantsCount - knowledgeElementsByParticipant.length;
+    _.times(participantsCountWithoutKnowledgeElements, () => knowledgeElementsByParticipant.push([]));
+    return knowledgeElementsByParticipant;
   }
 }
 
