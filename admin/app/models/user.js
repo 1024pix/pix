3f1172249aa80a645af445741d1bea43cf74f424
yahya-youness <you@octo.com>
2020-04-15 15:11:56 +0200

    Replace computed by new angle bracket syntax

--- admin/app/models/user.js
+++ admin/app/models/user.js
@@ -11,23 +11,5 @@ export default class User extends Model {
   @attr('boolean') pixOrgaTermsOfServiceAccepted;
   @attr('boolean') pixCertifTermsOfServiceAccepted;
 
-  @computed('cgu')
-  get isPixTermsOfServiceAccepted() {
-    const value = this.cgu;
-    return value ? 'Oui' : 'Non';
-  }
-
-  @computed('pixOrgaTermsOfServiceAccepted')
-  get isPixOrgaTermsOfServiceAccepted() {
-    const value = this.pixOrgaTermsOfServiceAccepted;
-    return value ? 'Oui' : 'Non';
-  }
-
-  @computed('pixCertifTermsOfServiceAccepted')
-  get isPixCertifTermsOfServiceAccepted() {
-    const value = this.pixCertifTermsOfServiceAccepted;
-    return value ? 'Oui' : 'Non';
-  }
-
   @hasMany('membership') memberships;
 }
