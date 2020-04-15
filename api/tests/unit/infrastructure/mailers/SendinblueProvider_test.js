6c403e574a6dccbff2580a577ab9665315fbedd3
Christophe Charles <ccharles@octo.com>
2020-04-15 15:08:16 +0200

    [API] Add tags property in sendinblue provider

--- api/tests/unit/infrastructure/mailers/SendinblueProvider_test.js
+++ api/tests/unit/infrastructure/mailers/SendinblueProvider_test.js
@@ -1,7 +1,9 @@
 const { sinon, expect, nock } = require('../../../test-helper');
-const SendinblueProvider = require('../../../../lib/infrastructure/mailers/SendinblueProvider');
+
 const mailCheck = require('../../../../lib/infrastructure/mail-check');
 const { mailing } = require('../../../../lib/config');
+
+const SendinblueProvider = require('../../../../lib/infrastructure/mailers/SendinblueProvider');
 
 describe('Unit | Class | SendinblueProvider', () => {
 
@@ -13,40 +15,48 @@ describe('Unit | Class | SendinblueProvider', () => {
 
   describe('#sendEmail', () => {
 
-    const recipient = 'test@example.net';
+    const senderEmailAddress = 'no-reply@example.net';
+    const userEmailAddress = 'user@example.net';
+    const templateId = 129291;
+
+    let stubbedSendinblueSMTPApi;
+    let mailingProvider;
 
     context('when mail sending is enabled', () => {
 
       beforeEach(() => {
         sinon.stub(mailing, 'enabled').value(true);
         sinon.stub(mailing, 'provider').value('sendinblue');
+
+        sinon.stub(SendinblueProvider, 'createSendinblueSMTPApi');
+        sinon.stub(mailCheck, 'checkMail').withArgs(userEmailAddress).resolves();
+
+        stubbedSendinblueSMTPApi = { sendTransacEmail: sinon.stub() };
+        SendinblueProvider.createSendinblueSMTPApi.returns(stubbedSendinblueSMTPApi);
+
+        mailingProvider = new SendinblueProvider();
       });
 
       context('when email check succeeds', () => {
 
-        beforeEach(() => {
-          sinon.stub(SendinblueProvider, 'createSendinblueSMTPApi');
-          sinon.stub(mailCheck, 'checkMail').withArgs(recipient).resolves();
-        });
-
         it('should call the given sendinblue api instance', async () => {
           // given
-          const stubbedSibSMTPApi = { sendTransacEmail: sinon.stub() };
-          SendinblueProvider.createSendinblueSMTPApi.returns(stubbedSibSMTPApi);
-          const mailingProvider = new SendinblueProvider();
+          const options = {
+            from: senderEmailAddress, to: userEmailAddress,
+            fromName: 'Ne pas repondre', subject: 'Creation de compte',
+            template: templateId
+          };
 
-          const from = 'no-reply@example.net';
-          const email = recipient;
           const expectedPayload = {
             to: [{
-              email,
+              email: userEmailAddress,
             }],
             sender: {
               name: 'Ne pas repondre',
-              email: from,
+              email: senderEmailAddress,
             },
             subject: 'Creation de compte',
-            templateId: 129291,
+            templateId,
             headers: {
               'content-type': 'application/json',
               'accept': 'application/json',
@@ -54,16 +64,80 @@ describe('Unit | Class | SendinblueProvider', () => {
           };
 
           // when
-          await mailingProvider.sendEmail({
-            from,
-            to: email,
-            fromName: 'Ne pas repondre',
-            subject: 'Creation de compte',
-            template: '129291'
-          });
+          await mailingProvider.sendEmail(options);
 
           // then
-          expect(stubbedSibSMTPApi.sendTransacEmail).to.have.been.calledWithExactly(expectedPayload);
+          expect(stubbedSendinblueSMTPApi.sendTransacEmail).to.have.been.calledWithExactly(expectedPayload);
+        });
+
+        context('when tags property is given', () => {
+
+          it('should add tags when it is not a empty array', async () => {
+            // given
+            const tags = ['TEST'];
+
+            const options = {
+              from: senderEmailAddress, to: userEmailAddress,
+              fromName: 'Ne pas repondre', subject: 'Creation de compte',
+              template: templateId, tags
+            };
+
+            const expectedPayload = {
+              to: [{
+                email: userEmailAddress,
+              }],
+              sender: {
+                name: 'Ne pas repondre',
+                email: senderEmailAddress,
+              },
+              subject: 'Creation de compte',
+              templateId,
+              headers: {
+                'content-type': 'application/json',
+                'accept': 'application/json',
+              },
+              tags
+            };
+
+            // when
+            await mailingProvider.sendEmail(options);
+
+            // then
+            expect(stubbedSendinblueSMTPApi.sendTransacEmail).to.have.been.calledWithExactly(expectedPayload);
+          });
+
+          it('should not add tags when it is empty', async () => {
+            // given
+            const tags = null;
+
+            const options = {
+              from: senderEmailAddress, to: userEmailAddress,
+              fromName: 'Ne pas repondre', subject: 'Creation de compte',
+              template: templateId, tags
+            };
+
+            const expectedPayload = {
+              to: [{
+                email: userEmailAddress,
+              }],
+              sender: {
+                name: 'Ne pas repondre',
+                email: senderEmailAddress,
+              },
+              subject: 'Creation de compte',
+              templateId,
+              headers: {
+                'content-type': 'application/json',
+                'accept': 'application/json',
+              }
+            };
+
+            // when
+            await mailingProvider.sendEmail(options);
+
+            // then
+            expect(stubbedSendinblueSMTPApi.sendTransacEmail).to.have.been.calledWithExactly(expectedPayload);
+          });
         });
       });
     });
