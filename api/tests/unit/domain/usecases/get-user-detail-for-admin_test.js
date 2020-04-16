c2c4d33fe8b0310128d2bb4ea3ad1344262a56f1
yahya-youness <you@octo.com>
2020-04-16 19:06:45 +0200

    [Admin:api] Rename usecase getUserDetail to getUserDetailForAdmin

---
+++ api/tests/unit/domain/usecases/get-user-detail-for-admin_test.js
@@ -0,0 +1,23 @@
+const { expect, sinon } = require('../../../test-helper');
+const getUserDetailForAdmin = require('../../../../lib/domain/usecases/get-user-detail-for-admin');
+
+describe('Unit | UseCase | get-user-detail-for-admin', () => {
+
+  let userRepository;
+
+  beforeEach(() => {
+    userRepository = { getUserDetailForAdmin: sinon.stub() };
+  });
+
+  it('should get the user detail in adminstration contexte', async () => {
+    // given
+    const userId = 1;
+    userRepository.get.withArgs(userId).resolves({ id: userId });
+
+    // when
+    const result = await getUserDetailForAdmin({ userId, userRepository });
+
+    // then
+    expect(result).to.deep.equal({ id: userId });
+  });
+});
