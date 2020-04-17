48eeb71dee9882fd29a224f112b55b045defa630
Krysthalia <ang.schall@gmail.com>
2020-04-17 13:00:12 +0200

    API: add route to assign user PixMaster to session

---
+++ api/tests/acceptance/application/session/session-controller-patch-user-assignment_test.js
@@ -0,0 +1,109 @@
+const {
+  expect, generateValidRequestAuthorizationHeader, databaseBuilder,
+} = require('../../../test-helper');
+const createServer = require('../../../../server');
+
+describe('PATCH /api/sessions/:id/certification-officer-assignment', () => {
+  let server;
+  const options = { method: 'PATCH' };
+  let certificationOfficerId;
+
+  beforeEach(async () => {
+    server = await createServer();
+  });
+
+  context('when user does not have the role PIX MASTER', () => {
+
+    beforeEach(() => {
+      certificationOfficerId = databaseBuilder.factory.buildUser().id;
+      return databaseBuilder.commit();
+    });
+
+    it('should return a 403 error code', async () => {
+      // given
+      options.url = '/api/sessions/any/certification-officer-assignment';
+      options.headers = { authorization: generateValidRequestAuthorizationHeader(certificationOfficerId) };
+
+      // when
+      const response = await server.inject(options);
+
+      // then
+      expect(response.statusCode).to.equal(403);
+    });
+
+  });
+
+  context('when user has role PixMaster', () => {
+
+    beforeEach(() => {
+      // given
+      certificationOfficerId = databaseBuilder.factory.buildUser.withPixRolePixMaster().id;
+      options.headers = { authorization: generateValidRequestAuthorizationHeader(certificationOfficerId) };
+      return databaseBuilder.commit();
+    });
+
+    context('when the session id has an invalid format', () => {
+
+      it('should return a 422 error code', async () => {
+        // given
+        options.url = '/api/sessions/any/certification-officer-assignment';
+
+        // when
+        const response = await server.inject(options);
+
+        // then
+        expect(response.statusCode).to.equal(422);
+      });
+    });
+
+    context('when the session id is a number', () => {
+
+      context('when the session does not exist', () => {
+
+        it('should return a 404 error code', async () => {
+          // given
+          options.url = '/api/sessions/1/certification-officer-assignment';
+
+          // when
+          const response = await server.inject(options);
+
+          // then
+          expect(response.statusCode).to.equal(404);
+        });
+      });
+
+      context('when the session exists', () => {
+        let sessionId;
+
+        beforeEach(() => {
+          // given
+          sessionId = databaseBuilder.factory.buildSession().id;
+          return databaseBuilder.commit();
+        });
+
+        it('should return a 200 status code', async () => {
+          // given
+          options.url = `/api/sessions/${sessionId}/certification-officer-assignment`;
+
+          // when
+          const response = await server.inject(options);
+
+          // then
+          expect(response.statusCode).to.equal(200);
+        });
+
+        it('should return the serialized session with a link to the assigned user', async () => {
+          // given
+          options.url = `/api/sessions/${sessionId}/certification-officer-assignment`;
+
+          // when
+          const response = await server.inject(options);
+
+          // then
+          const linkToAssignedUser = response.result.data.relationships['assigned-certification-officer'].links.related;
+          expect(linkToAssignedUser).to.equal(`/api/users/${certificationOfficerId}`);
+        });
+      });
+    });
+  });
+});
