1d322aca4e3477fd7456c4641b7e1f5eee389fb6
laura-bergoens <laura.bergoens@gmail.com>
2020-04-17 09:38:42 +0200

    admin: Add status filter on sessions list

--- admin/app/controllers/authenticated/sessions/list.js
+++ admin/app/controllers/authenticated/sessions/list.js
@@ -2,6 +2,8 @@ import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { debounce } from '@ember/runloop';
+import _ from 'lodash';
+import { statusToDisplayName } from 'pix-admin/models/session';
 
 const DEFAULT_PAGE_NUMBER = 1;
 
@@ -12,11 +14,22 @@ export default class SessionListController extends Controller {
   @tracked pageNumber = DEFAULT_PAGE_NUMBER;
   @tracked pageSize = 10;
   @tracked id = null;
+  @tracked status = null;
 
+  sessionStatusAndLabels = [
+    { status: '', label: 'Tous' },
+    ..._.map(statusToDisplayName, (label, status) => ({ status, label })),
+  ];
   searchFilter = null;
 
   setFieldName() {
     this.searchFilter.fieldName = this.searchFilter.value;
+    this.pageNumber = DEFAULT_PAGE_NUMBER;
+  }
+
+  @action
+  setStatusFilter(status) {
+    this.status = status;
     this.pageNumber = DEFAULT_PAGE_NUMBER;
   }
 
