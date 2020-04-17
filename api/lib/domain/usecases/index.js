81e513f2465ffd961d28e21a68fdf6e8a7aad0b0
laura-bergoens <laura.bergoens@gmail.com>
2020-04-17 12:37:15 +0200

    API: add usecase to assign user to session

--- api/lib/domain/usecases/index.js
+++ api/lib/domain/usecases/index.js
@@ -70,6 +70,7 @@ module.exports = injectDependencies({
   answerToOrganizationInvitation: require('./answer-to-organization-invitation'),
   attachTargetProfilesToOrganization: require('./attach-target-profiles-to-organization'),
   archiveCampaign: require('./archive-campaign'),
+  assignCertificationOfficerToSession: require('./assign-certification-officer-to-session'),
   authenticateUser: require('./authenticate-user'),
   beginCampaignParticipationImprovement: require('./begin-campaign-participation-improvement'),
   completeAssessment: require('./complete-assessment'),
