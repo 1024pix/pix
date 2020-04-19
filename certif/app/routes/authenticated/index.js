c01906c89c7d064a15d5aa67823c6f5649758b11
laura-bergoens <laura.bergoens@gmail.com>
2020-04-19 21:59:07 +0200

    [BSR] Remove unused currentUser service

--- certif/app/routes/authenticated/index.js
+++ certif/app/routes/authenticated/index.js
@@ -1,9 +1,6 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
-
-  @service currentUser;
 
   beforeModel() {
     return this.replaceWith('authenticated.sessions.list');
