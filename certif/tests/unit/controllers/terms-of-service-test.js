0b044f355abd6ad15a5b7df2a4835e053a82a0d8
laura-bergoens <laura.bergoens@gmail.com>
2020-04-19 21:29:22 +0200

    Replace empty tests with useful ones

--- certif/tests/unit/controllers/terms-of-service-test.js
+++ certif/tests/unit/controllers/terms-of-service-test.js
@@ -1,11 +1,35 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import sinon from 'sinon';
 
 module('Unit | Controller | terms-of-service', function(hooks) {
   setupTest(hooks);
+  let controller;
 
-  test('it exists', function(assert) {
-    const controller = this.owner.lookup('controller:terms-of-service');
-    assert.ok(controller);
+  module('#action submit', function(hooks) {
+
+    hooks.beforeEach(function() {
+      controller = this.owner.lookup('controller:terms-of-service');
+      controller.transitionToRoute = sinon.stub().resolves();
+      controller.currentUser = { user: { save: sinon.stub().resolves() } };
+    });
+
+    test('it should save acceptance of terms of service', async function(assert) {
+      // when
+      await controller.send('submit');
+
+      // then
+      sinon.assert.calledWith(controller.currentUser.user.save, { adapterOptions: { acceptPixCertifTermsOfService: true } });
+      assert.ok(controller);
+    });
+
+    test('it should transition to authenticated.sessions.list', async function(assert) {
+      // when
+      await controller.send('submit');
+
+      // then
+      sinon.assert.calledWith(controller.transitionToRoute, 'authenticated.sessions.list');
+      assert.ok(controller);
+    });
   });
 });
