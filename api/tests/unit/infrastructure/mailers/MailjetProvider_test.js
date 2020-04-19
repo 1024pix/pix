fc8b47ae63007f8baff036e522c47e058bdabe6f
Guillaume Lagorce <guillaume@hey-gul.com>
2020-04-19 22:44:54 +0200

    BSR: remove useless contexts in MailjetProvider test file

--- api/tests/unit/infrastructure/mailers/MailjetProvider_test.js
+++ api/tests/unit/infrastructure/mailers/MailjetProvider_test.js
@@ -1,8 +1,6 @@
 const nodeMailjet = require('node-mailjet');
 const { sinon } = require('../../../test-helper');
 const MailjetProvider = require('../../../../lib/infrastructure/mailers/MailjetProvider');
-const mailCheck = require('../../../../lib/infrastructure/mail-check');
-const { mailing } = require('../../../../lib/config');
 
 describe('Unit | Class | MailjetProvider', function() {
 
@@ -14,89 +12,74 @@ describe('Unit | Class | MailjetProvider', function() {
 
     const recipient = 'test@example.net';
 
-    context('when mail sending is enabled', () => {
+    it('should request with a payload', async () => {
+      // given
+      const from = 'no-reply@example.net';
+      const requestStub = sinon.stub().returns(Promise.resolve());
+      const postStub = sinon.stub().returns({ request: requestStub });
+      nodeMailjet.connect.returns({ post: postStub });
+      const mailingProvider = new MailjetProvider();
 
-      beforeEach(() => {
-        sinon.stub(mailing, 'enabled').value(true);
-        sinon.stub(mailing, 'provider').value('mailjet');
+      // when
+      await mailingProvider.sendEmail({
+        from,
+        to: recipient,
+        fromName: 'Ne Pas Repondre',
+        subject: 'Creation de compte',
+        template: '129291'
       });
 
-      context('when email check succeeds', () => {
+      // then
+      sinon.assert.calledWith(requestStub, {
+        'FromEmail': 'no-reply@example.net',
+        'FromName': 'Ne Pas Repondre',
+        'Subject': 'Creation de compte',
+        'MJ-TemplateID': '129291',
+        'MJ-TemplateLanguage': 'true',
+        'Recipients': [{ 'Email': recipient, 'Vars': {} }]
+      });
+    });
 
-        beforeEach(() => {
-          sinon.stub(mailCheck, 'checkMail').withArgs(recipient).resolves();
-        });
+    it('should have default values', async () => {
+      // given
+      const requestStub = sinon.stub().returns(Promise.resolve());
+      const postStub = sinon.stub().returns({ request: requestStub });
+      nodeMailjet.connect.returns({ post: postStub });
+      const mailingProvider = new MailjetProvider();
 
-        it('should request with a payload', async () => {
-          // given
-          const from = 'no-reply@example.net';
-          const requestStub = sinon.stub().returns(Promise.resolve());
-          const postStub = sinon.stub().returns({ request: requestStub });
-          nodeMailjet.connect.returns({ post: postStub });
-          const mailingProvider = new MailjetProvider();
+      // when
+      await mailingProvider.sendEmail({ template: '129291', to: recipient });
 
-          // when
-          await mailingProvider.sendEmail({
-            from,
-            to: recipient,
-            fromName: 'Ne Pas Repondre',
-            subject: 'Creation de compte',
-            template: '129291'
-          });
+      // then
+      sinon.assert.calledWith(requestStub, {
+        'FromEmail': 'communaute@pix.fr',
+        'FromName': 'Communauté PIX',
+        'Subject': 'Bienvenue dans la communauté PIX',
+        'MJ-TemplateID': '129291',
+        'MJ-TemplateLanguage': 'true',
+        'Recipients': [{ 'Email': recipient, 'Vars': {} }]
+      });
+    });
 
-          // then
-          sinon.assert.calledWith(requestStub, {
-            'FromEmail': 'no-reply@example.net',
-            'FromName': 'Ne Pas Repondre',
-            'Subject': 'Creation de compte',
-            'MJ-TemplateID': '129291',
-            'MJ-TemplateLanguage': 'true',
-            'Recipients': [{ 'Email': recipient, 'Vars': {} }]
-          });
-        });
+    it('should set variables in values', async () => {
+      // given
+      const requestStub = sinon.stub().returns(Promise.resolve());
+      const postStub = sinon.stub().returns({ request: requestStub });
+      const variables = { resetUrl: 'token' };
+      nodeMailjet.connect.returns({ post: postStub });
+      const mailingProvider = new MailjetProvider();
 
-        it('should have default values', async () => {
-          // given
-          const requestStub = sinon.stub().returns(Promise.resolve());
-          const postStub = sinon.stub().returns({ request: requestStub });
-          nodeMailjet.connect.returns({ post: postStub });
-          const mailingProvider = new MailjetProvider();
+      // when
+      await mailingProvider.sendEmail({ template: '129291', to: recipient, variables });
 
-          // when
-          await mailingProvider.sendEmail({ template: '129291', to: recipient });
-
-          // then
-          sinon.assert.calledWith(requestStub, {
-            'FromEmail': 'communaute@pix.fr',
-            'FromName': 'Communauté PIX',
-            'Subject': 'Bienvenue dans la communauté PIX',
-            'MJ-TemplateID': '129291',
-            'MJ-TemplateLanguage': 'true',
-            'Recipients': [{ 'Email': recipient, 'Vars': {} }]
-          });
-        });
-
-        it('should set variables in values', async () => {
-          // given
-          const requestStub = sinon.stub().returns(Promise.resolve());
-          const postStub = sinon.stub().returns({ request: requestStub });
-          const variables = { resetUrl: 'token' };
-          nodeMailjet.connect.returns({ post: postStub });
-          const mailingProvider = new MailjetProvider();
-
-          // when
-          await mailingProvider.sendEmail({ template: '129291', to: recipient, variables });
-
-          // then
-          sinon.assert.calledWith(requestStub, {
-            'FromEmail': 'communaute@pix.fr',
-            'FromName': 'Communauté PIX',
-            'Subject': 'Bienvenue dans la communauté PIX',
-            'MJ-TemplateID': '129291',
-            'MJ-TemplateLanguage': 'true',
-            'Recipients': [{ 'Email': recipient, 'Vars': variables }]
-          });
-        });
+      // then
+      sinon.assert.calledWith(requestStub, {
+        'FromEmail': 'communaute@pix.fr',
+        'FromName': 'Communauté PIX',
+        'Subject': 'Bienvenue dans la communauté PIX',
+        'MJ-TemplateID': '129291',
+        'MJ-TemplateLanguage': 'true',
+        'Recipients': [{ 'Email': recipient, 'Vars': variables }]
       });
     });
   });
