2bf43f3dcdaded6f98acf38749a3d07c54bd64c3
Antoine Ceol <antoine.ceol@gmail.com>
2020-04-20 18:13:02 +0200

    Check for grey zone

--- api/lib/domain/models/CertificationPartnerAcquisition.js
+++ api/lib/domain/models/CertificationPartnerAcquisition.js
@@ -1,5 +1,6 @@
 const CERTIF_GREEN_ZONE = 'green_zone';
 const CERTIF_RED_ZONE = 'red_zone';
+const CERTIF_GREY_ZONE = 'grey_zone';
 
 const { MINIMUM_REPRODUCIBILITY_RATE_TO_BE_CERTIFIED, MINIMUM_REPRODUCIBILITY_RATE_TO_BE_TRUSTED } = require('../constants');
 
@@ -16,27 +17,32 @@ class CertificationPartnerAcquisition {
 
   hasAcquiredCertification({
     hasAcquiredBadge = false,
-    percentageCorrectAnswers = 0 }) {
+    reproducibilityRate = 0
+  }) {
     if (!hasAcquiredBadge) return false;
 
-    switch (this._getPartnerCertificationObtentionZone(percentageCorrectAnswers)) {
+    switch (this._getPartnerCertificationObtentionZone(reproducibilityRate)) {
       case CERTIF_GREEN_ZONE:
         return true;
       case CERTIF_RED_ZONE:
         return false;
-        // case ZONE_GRISE
-        // zone grise
+      case CERTIF_GREY_ZONE:
+        return this._checkCleaPixRate();
     }
   }
 
-  _getPartnerCertificationObtentionZone(percentageCorrectAnswers) {
-    if (percentageCorrectAnswers >= MINIMUM_REPRODUCIBILITY_RATE_TO_BE_TRUSTED) {
+  _getPartnerCertificationObtentionZone(reproducibilityRate) {
+    if (reproducibilityRate >= MINIMUM_REPRODUCIBILITY_RATE_TO_BE_TRUSTED) {
       return CERTIF_GREEN_ZONE;
-    } else if (percentageCorrectAnswers <= MINIMUM_REPRODUCIBILITY_RATE_TO_BE_CERTIFIED) {
+    } else if (reproducibilityRate <= MINIMUM_REPRODUCIBILITY_RATE_TO_BE_CERTIFIED) {
       return CERTIF_RED_ZONE;
     }
+    return CERTIF_GREY_ZONE;
+  }
 
-    return null;
+  _checkCleaPixRate() {
+    // implémentation de la “zone grise” => mise en place d’un seuil, un nombre de Pix minimum à obtenir / compétence couvertes pas le référentiel Cléa numérique pour répondre au critère d’obtention du certif Cléa “avoir 75% des acquis validés par compétence”
+    return false;
   }
 }
 
--- api/tests/unit/domain/models/CertificationPartnerAcquisition_test.js
+++ api/tests/unit/domain/models/CertificationPartnerAcquisition_test.js
@@ -1,6 +1,9 @@
 const { expect } = require('../../../test-helper');
 const CertificationPartnerAcquisition = require('../../../../lib/domain/models/CertificationPartnerAcquisition');
 
+const GREEN_ZONE_REPRO = [80, 90, 100];
+const RED_ZONE_REPRO = [1, 50];
+const GREY_ZONE_REPRO = [75];
 describe('Unit | Domain | Models | CertificationPartnerAcquisition', () => {
   let certificationPartnerAcquisition;
   describe('#hasAcquiredCertification', () => {
@@ -13,15 +16,13 @@ describe('Unit | Domain | Models | CertificationPartnerAcquisition', () => {
         );
       });
 
-      [1, 50, 80, 90, 100].forEach((reproducabilityRate) =>
-        it(`for ${reproducabilityRate} reproducability rate, it should not obtain certification`, async () => {
-          // when
-          const hasAcquiredCertif = certificationPartnerAcquisition.hasAcquiredCertification({ percentageCorrectAnswers: reproducabilityRate, hasAcquiredBadge: false });
+      it('for any reproducibility rate, it should not obtain certification', async () => {
+        // when
+        const hasAcquiredCertif = certificationPartnerAcquisition.hasAcquiredCertification({ hasAcquiredBadge: false });
 
-          // then
-          expect(hasAcquiredCertif).to.be.false;
-        })
-      );
+        // then
+        expect(hasAcquiredCertif).to.be.false;
+      });
     });
 
     context('when user has badge', () => {
@@ -32,27 +33,49 @@ describe('Unit | Domain | Models | CertificationPartnerAcquisition', () => {
         });
       });
 
-      [80, 90, 100].forEach((reproducabilityRate) =>
-        it(`for ${reproducabilityRate} reproducability rate, it should obtain certification`, async () => {
+      context('reproducibility rate in green zone', () => {
+        GREEN_ZONE_REPRO.forEach((reproducibilityRate) =>
+          it(`for ${reproducibilityRate} reproducibility rate, it should obtain certification`, async () => {
+            // when
+            const hasAcquiredCertif = certificationPartnerAcquisition.hasAcquiredCertification({
+              hasAcquiredBadge: true,
+              reproducibilityRate
+            });
+
+            // then
+            expect(hasAcquiredCertif).to.be.true;
+          })
+        );
+      });
+
+      context('reproducibility rate in grey zone', () => {
+        const reproducibilityRate = GREY_ZONE_REPRO[0];
+        it(`for ${reproducibilityRate} reproducibility rate, it should obtain certification`, async () => {
           // when
-          const hasAcquiredCertif = certificationPartnerAcquisition.hasAcquiredCertification({ hasAcquiredBadge: true, percentageCorrectAnswers:reproducabilityRate });
+          const hasAcquiredCertif = certificationPartnerAcquisition.hasAcquiredCertification({
+            hasAcquiredBadge: true,
+            reproducibilityRate
+          });
 
           // then
           expect(hasAcquiredCertif).to.be.true;
-        })
-      );
+        });
+      });
 
-      [1, 50].forEach((reproducabilityRate) =>
-        it(`for ${reproducabilityRate} reproducability rate, it should not obtain certification`, async () => {
-          // when
-          const hasAcquiredCertif = certificationPartnerAcquisition.hasAcquiredCertification({ hasAcquiredBadge: true, percentageCorrectAnswers:reproducabilityRate });
+      context('reproducibility rate in red zone', () => {
+        RED_ZONE_REPRO.forEach((reproducibilityRate) =>
+          it(`for ${reproducibilityRate} reproducibility rate, it should not obtain certification`, async () => {
+            // when
+            const hasAcquiredCertif = certificationPartnerAcquisition.hasAcquiredCertification({
+              hasAcquiredBadge: true,
+              reproducibilityRate
+            });
 
-          // then
-          expect(hasAcquiredCertif).to.be.false;
-
-        })
-      );
+            // then
+            expect(hasAcquiredCertif).to.be.false;
+          })
+        );
+      });
     });
-
   });
 });
