f99fbefaa8a3aedb621524b4ad4c231baddc4177
Antoine Ceol <antoine.ceol@gmail.com>
2020-04-22 09:34:49 +0200

    Get Badge by key

--- api/lib/infrastructure/repositories/badge-repository.js
+++ api/lib/infrastructure/repositories/badge-repository.js
@@ -4,8 +4,16 @@ const BookshelfBadge = require('../../infrastructure/data/badge');
 module.exports = {
 
   findOneByTargetProfileId(targetProfileId) {
-    return BookshelfBadge
-      .where({ targetProfileId })
+    return new BookshelfBadge({ targetProfileId })
+      .fetch({
+        require: false,
+        withRelated: ['badgePartnerCompetences']
+      })
+      .then((results) => bookshelfToDomainConverter.buildDomainObject(BookshelfBadge, results));
+  },
+
+  findOneByKey(key) {
+    return new BookshelfBadge({ key })
       .fetch({
         require: false,
         withRelated: ['badgePartnerCompetences']
