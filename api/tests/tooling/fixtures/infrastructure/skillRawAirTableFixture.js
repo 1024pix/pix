79096273f688b4f90034eb2bcb85fab2801eb23b
Antoine Ceol <antoine.ceol@gmail.com>
2020-04-23 15:35:06 +0200

    [BSR] use async await and simplifying tooling

--- api/tests/tooling/fixtures/infrastructure/skillRawAirTableFixture.js
+++ api/tests/tooling/fixtures/infrastructure/skillRawAirTableFixture.js
@@ -1,37 +1,57 @@
 const AirtableRecord = require('airtable').Record;
 
-module.exports = function skillRawAirTableFixture({ id } = { id: 'recTIddrkopID28Ep' }) {
-  return new AirtableRecord('Skill', id,{
-    'id': id,
-    'fields': {
-      'id persistant': id,
-      'Nom': '@accesDonnées1',
-      'Indice': 'Peut-on géo-localiser un téléphone lorsqu’il est éteint ?',
-      'Statut de l\'indice': 'Validé',
-      'Status': 'actif',
-      'Epreuves': [
-        'recF2iWmZKIuOsKO1',
-        'recYu7YmDXXt5Owo8',
-        'recbH4xMDsDZnRzzN'
-      ],
-      'Compétence (via Tube) (id persistant)': [
-        'recofJCxg0NqTqTdP'
-      ],
-      'Comprendre (id persistant)': [
-        'receomyzL0AmpMFGw'
-      ],
-      'PixValue': 2.4,
-      'En savoir plus (id persistant)': [
-        'recQbjXNAPsVJthXh',
-        'rec3DkUX0a6RNi2Hz'
-      ],
-      'Tags': [
-        'recdUq3RwhedQoRwS'
-      ],
-      'Tube (id persistant)': [
-        'recofJCazertqTdP'
-      ]
-    },
-    'createdTime': '2018-01-31T12:41:07.000Z'
-  });
-};
+const ACTIVE_STATUS = 'actif';
+
+class SkillRawAirTableFixture extends AirtableRecord {
+  constructor(id) {
+    super('Skill', id,{
+      'id': id,
+      'fields': {
+        'id persistant': id,
+        'Nom': '@accesDonnées1',
+        'Indice': 'Peut-on géo-localiser un téléphone lorsqu’il est éteint ?',
+        'Statut de l\'indice': 'Validé',
+        'Status': ACTIVE_STATUS,
+        'Epreuves': [
+          'recF2iWmZKIuOsKO1',
+          'recYu7YmDXXt5Owo8',
+          'recbH4xMDsDZnRzzN'
+        ],
+        'Compétence (via Tube) (id persistant)': [
+          'recofJCxg0NqTqTdP'
+        ],
+        'Comprendre (id persistant)': [
+          'receomyzL0AmpMFGw'
+        ],
+        'PixValue': 2.4,
+        'En savoir plus (id persistant)': [
+          'recQbjXNAPsVJthXh',
+          'rec3DkUX0a6RNi2Hz'
+        ],
+        'Tags': [
+          'recdUq3RwhedQoRwS'
+        ],
+        'Tube (id persistant)': [
+          'recofJCazertqTdP'
+        ]
+      },
+      'createdTime': '2018-01-31T12:41:07.000Z'
+    });
+  }
+
+  withActiveStatus() {
+    this.fields['Status'] = ACTIVE_STATUS;
+    return this;
+  }
+
+  withInactiveStatus() {
+    this.fields['Status'] = 'périmé';
+    return this;
+  }
+}
+
+function skillRawAirTableFixture({ id } = { id: 'recTIddrkopID28Ep' }) {
+  return new SkillRawAirTableFixture(id);
+}
+
+module.exports = skillRawAirTableFixture;
