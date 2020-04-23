c2f91eb44e369bc6b3696ccd5226c4e069dbef1d
laura-bergoens <laura.bergoens@gmail.com>
2020-04-23 09:12:00 +0200

    api: apply filter validation on usecase

--- api/lib/domain/usecases/find-paginated-filtered-sessions.js
+++ api/lib/domain/usecases/find-paginated-filtered-sessions.js
@@ -1,3 +1,18 @@
+const sessionValidator = require('../validators/session-validator');
+
 module.exports = async function findPaginatedFilteredSessions({ filters, page, sessionRepository }) {
+  try {
+    sessionValidator.validateFilters(filters);
+  } catch (err) {
+    return {
+      sessions: [],
+      pagination: {
+        page: page.number,
+        pageSize: page.size,
+        rowCount: 0, 
+        pageCount: 0,
+      },
+    };
+  }
   return sessionRepository.findPaginatedFiltered({ filters, page });
 };
