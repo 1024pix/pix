0a82bd4af7034530d712fd3f7695fe55f8958135
laura-bergoens <laura.bergoens@gmail.com>
2020-04-23 07:44:53 +0200

    Fix debounce on sessions list filters
    Debounce was not working anymore since PixAdmin
    upgraded to Octane. This is roughly due do two reasons :
    - <Input> component is two-way-binded
    - Updating a value of a native JS class through this.a = b
      do trigger all related events
    So, when typing something in the input field, it was directly updating
    the controller field of the related queryParam. Thus, the controller
    immediately triggers the router to send a new XHR Request, without
    waiting for the debounce timer.
    Basically, to fix this, we just need to change the <Input> built-in
    Ember component for the classic <input> (which is one-way binded).
    Though, in addition of that, this commit also setup the usage
    of ember-concurrency to deal with the debounced class.
    To me, it's clean and precise, and only require one function instead
    of two with the previous way of doing it.

--- admin/app/controllers/authenticated/sessions/list.js
+++ admin/app/controllers/authenticated/sessions/list.js
@@ -1,54 +1,54 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
-import { debounce } from '@ember/runloop';
+import { task, timeout } from 'ember-concurrency';
 import _ from 'lodash';
+import config from 'pix-admin/config/environment';
 import { statusToDisplayName, FINALIZED } from 'pix-admin/models/session';
 
 const DEFAULT_PAGE_NUMBER = 1;
 
 export default class SessionListController extends Controller {
 
-  queryParams = ['pageNumber', 'pageSize', 'id'];
+  queryParams = ['pageNumber', 'pageSize', 'id', 'status', 'resultsSentToPrescriberAt'];
+  DEBOUNCE_MS = config.pagination.debounce;
 
   @tracked pageNumber = DEFAULT_PAGE_NUMBER;
   @tracked pageSize = 10;
   @tracked id = null;
   @tracked status = FINALIZED;
   @tracked resultsSentToPrescriberAt = null;
-  searchFilter = null;
 
   sessionStatusAndLabels = [
-    { status: '', label: 'Tous' },
+    { status: null, label: 'Tous' },
     ..._.map(statusToDisplayName, (label, status) => ({ status, label })),
   ];
 
   sessionResultsSentToPrescriberAtAndLabels = [
-    { value: '', label: 'Tous' },
+    { value: null, label: 'Tous' },
     { value: 'true', label: 'Résultats diffusés' },
     { value: 'false', label: 'Résultats non diffusés' },
   ];
 
-  setFieldName() {
-    this.searchFilter.fieldName = this.searchFilter.value;
+  @(task(function * (fieldName, param) {
+    let value;
+    let debounceDuration = this.DEBOUNCE_MS;
+    switch (fieldName) {
+      case 'id':
+        value = param.target.value; // param is an InputEvent
+        break;
+      case 'status':
+        debounceDuration = 0;
+        value = param;
+        break;
+      case 'resultsSentToPrescriberAt':
+        debounceDuration = 0;
+        value = param;
+        break;
+      default:
+        return;
+    }
+    yield timeout(debounceDuration);
+    this[fieldName] = value;
     this.pageNumber = DEFAULT_PAGE_NUMBER;
-  }
-
-  @action
-  setStatusFilter(status) {
-    this.status = status;
-    this.pageNumber = DEFAULT_PAGE_NUMBER;
-  }
-
-  @action
-  setResultsSentToPrescriberAtFilter(value) {
-    this.resultsSentToPrescriberAt = value;
-    this.pageNumber = DEFAULT_PAGE_NUMBER;
-  }
-
-  @action
-  triggerFiltering(fieldName, value) {
-    this.searchFilter = { fieldName, value };
-    debounce(this, this.setFieldName, 500);
-  }
+  }).restartable()) triggerFiltering;
 }
---
+++ admin/tests/unit/routes/authenticated/sessions/list-test.js
@@ -0,0 +1,151 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import sinon from 'sinon';
+
+module('Unit | Route | authenticated/sessions/list', function(hooks) {
+  setupTest(hooks);
+  let route;
+
+  hooks.beforeEach(function() {
+    route = this.owner.lookup('route:authenticated/sessions/list');
+  });
+
+  module('#model', function(hooks) {
+    let params;
+    const expectedQueryArgs = {};
+
+    hooks.beforeEach(function() {
+      route.store.query = sinon.stub().resolves();
+      params = {};
+      params.pageNumber = 'somePageNumber';
+      params.pageSize = 'somePageSize';
+      expectedQueryArgs.page = {
+        number: 'somePageNumber',
+        size: 'somePageSize',
+      };
+    });
+
+    module('when queryParams are undefined', function() {
+
+      test('it should call store.query with no filters', async function(assert) {
+        // when
+        await route.model(params);
+        expectedQueryArgs.filter = {
+          id: undefined,
+          status: undefined,
+          resultsSentToPrescriberAt: undefined,
+        };
+
+        // then
+        sinon.assert.calledWith(route.store.query, 'session', expectedQueryArgs);
+        assert.ok(true);
+      });
+    });
+
+    module('when queryParams id is truthy', function() {
+
+      test('it should call store.query with a filter with trimmed id', async function(assert) {
+        // given
+        params.id = ' someId';
+        expectedQueryArgs.filter = {
+          id: 'someId',
+          status: undefined,
+          resultsSentToPrescriberAt: undefined,
+        };
+
+        // when
+        await route.model(params);
+
+        // then
+        sinon.assert.calledWith(route.store.query, 'session', expectedQueryArgs);
+        assert.ok(true);
+      });
+    });
+
+    module('when queryParams status is truthy', function() {
+
+      test('it should call store.query with a filter with trimmed status', async function(assert) {
+        // given
+        params.status = ' someStatus';
+        expectedQueryArgs.filter = {
+          id: undefined,
+          status: 'someStatus',
+          resultsSentToPrescriberAt: undefined,
+        };
+
+        // when
+        await route.model(params);
+
+        // then
+        sinon.assert.calledWith(route.store.query, 'session', expectedQueryArgs);
+        assert.ok(true);
+      });
+    });
+
+    module('when queryParams resultsSentToPrescriberAt is truthy', function() {
+
+      test('it should call store.query with a filter with trimmed resultsSentToPrescriberAt', async function(assert) {
+        // given
+        params.resultsSentToPrescriberAt = ' someValue';
+        expectedQueryArgs.filter = {
+          id: undefined,
+          status: undefined,
+          resultsSentToPrescriberAt: 'someValue',
+        };
+
+        // when
+        await route.model(params);
+
+        // then
+        sinon.assert.calledWith(route.store.query, 'session', expectedQueryArgs);
+        assert.ok(true);
+      });
+    });
+
+  });
+
+  module('#resetController', function(hooks) {
+    let controller;
+
+    hooks.beforeEach(function() {
+      controller = {
+        pageNumber: 'somePageNumber',
+        pageSize: 'somePageSize',
+        id: 'someId',
+        status: 'someStatus',
+        resultsSentToPrescriberAt: 'someValue',
+      };
+    });
+
+    module('when route is exiting', function() {
+
+      test('it should reset controller', function(assert) {
+        // when
+        route.resetController(controller, true);
+
+        // then
+        assert.equal(controller.pageNumber, 1);
+        assert.equal(controller.pageSize, 10);
+        assert.equal(controller.id, null);
+        assert.equal(controller.status, 'finalized');
+        assert.equal(controller.resultsSentToPrescriberAt, null);
+      });
+    });
+
+    module('when route is not exiting', function() {
+
+      test('it should not reset controller', function(assert) {
+        // when
+        route.resetController(controller, false);
+
+        // then
+        assert.equal(controller.pageNumber, 'somePageNumber');
+        assert.equal(controller.pageSize, 'somePageSize');
+        assert.equal(controller.id, 'someId');
+        assert.equal(controller.status, 'someStatus');
+        assert.equal(controller.resultsSentToPrescriberAt, 'someValue');
+      });
+    });
+
+  });
+});
