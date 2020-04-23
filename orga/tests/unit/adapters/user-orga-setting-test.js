4096f61466a50662783be1007dce561e9ef7f337
Vincent Hardouin <vincent.hardouin@pix.fr>
2020-04-23 16:55:57 +0200

    [ORGA] Enhance adapter to use PUT for create

--- orga/tests/unit/adapters/user-orga-setting-test.js
+++ orga/tests/unit/adapters/user-orga-setting-test.js
@@ -58,4 +58,46 @@ module('Unit | Adapters | user-orga-setting', function(hooks) {
     });
   });
 
+  module('#urlForCreateRecord', function() {
+
+    test('it should build create url from user id', async function(assert) {
+      // when
+      const snapshot = { adapterOptions: { userId: 1 } };
+      const url = await adapter.urlForCreateRecord('user-orga-setting', snapshot);
+
+      // then
+      assert.equal(url.endsWith('/user-orga-settings/1'), true);
+    });
+  });
+
+  module('#createRecord', function() {
+    module('when userId adapterOption passed', function(hooks) {
+
+      hooks.beforeEach(function() {
+        sinon.stub(adapter, 'ajax');
+      });
+
+      hooks.afterEach(function() {
+        adapter.ajax.restore();
+      });
+
+      test('should send a put request to /user-orga-settings to create', async function(assert) {
+        // given
+        adapter.ajax.resolves();
+        const snapshot = {
+          id: 1,
+          adapterOptions: { userId },
+          serialize: function() {},
+
+        };
+
+        // when
+        await adapter.createRecord(null, { modelName: 'user-orga-setting' }, snapshot);
+
+        // then
+        sinon.assert.calledWith(adapter.ajax, `http://localhost:3000/api/user-orga-settings/${userId}`, 'PUT');
+        assert.ok(adapter); /* required because QUnit wants at least one expect (and does not accept Sinon's one) */
+      });
+    });
+  });
 });
