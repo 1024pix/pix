aff4757686cad539321b2a171450234bb0061b13
laura-bergoens <laura.bergoens@gmail.com>
2020-04-23 08:04:07 +0200

    Fix debounce on user list filters
    Debounce was not working anymore since PixAdmin
    upgraded to Octane. This is roughly due do two reasons :
    - <Input> component is two-way-binded
    - Updating a value of a native JS class through this.a = b
      do trigger all related events
    So, when typing something in the input field, it was directly updating
    the controller field of the related queryParam. Thus, the controller
    immediately triggers the router to send a new XHR Request, without
    waiting for the debounce timer.
    Basically, to fix this, we just need to change the <Input> built-in
    Ember component for the classic <input> (which is one-way binded).
    Though, in addition of that, this commit also setup the usage
    of ember-concurrency to deal with the debounced class.
    To me, it's clean and precise, and only require one function instead
    of two with the previous way of doing it.

--- admin/tests/unit/routes/authenticated/users/list-test.js
+++ admin/tests/unit/routes/authenticated/users/list-test.js
@@ -1,11 +1,112 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import sinon from 'sinon';
 
 module('Unit | Route | authenticated/users/list', function(hooks) {
   setupTest(hooks);
+  let route;
 
-  test('it exists', function(assert) {
-    const route = this.owner.lookup('route:authenticated/users/list');
-    assert.ok(route);
+  hooks.beforeEach(function() {
+    route = this.owner.lookup('route:authenticated/users/list');
+  });
+
+  module('#model', function(hooks) {
+    const params = {};
+    const expectedQueryArgs = {};
+
+    hooks.beforeEach(function() {
+      route.store.query = sinon.stub().resolves();
+      params.pageNumber = 'somePageNumber';
+      params.pageSize = 'somePageSize';
+      expectedQueryArgs.page = {
+        number: 'somePageNumber',
+        size: 'somePageSize',
+      };
+    });
+
+    module('when queryParams filters are falsy', function() {
+
+      test('it should call store.query with no filters on name, type and externalId', async function(assert) {
+        // when
+        await route.model(params);
+        expectedQueryArgs.filter = {
+          firstName: '',
+          lastName: '',
+          email: '',
+        };
+
+        // then
+        sinon.assert.calledWith(route.store.query, 'user', expectedQueryArgs);
+        assert.ok(true);
+      });
+    });
+
+    module('when queryParams filters are  truthy', function() {
+
+      test('it should call store.query with filters containing trimmed values', async function(assert) {
+        // given
+        params.firstName = ' someFirstName';
+        params.lastName = 'someLastName ';
+        params.email = 'someEmail';
+        expectedQueryArgs.filter = {
+          firstName: 'someFirstName',
+          lastName: 'someLastName',
+          email: 'someEmail',
+        };
+
+        // when
+        await route.model(params);
+
+        // then
+        sinon.assert.calledWith(route.store.query, 'user', expectedQueryArgs);
+        assert.ok(true);
+      });
+    });
+
+  });
+
+  module('#resetController', function(hooks) {
+    let controller;
+
+    hooks.beforeEach(function() {
+      controller = {
+        pageNumber: 'somePageNumber',
+        pageSize: 'somePageSize',
+        firstName: 'someFirstName',
+        lastName: 'someLastName',
+        email: 'someEmail',
+      };
+    });
+
+    module('when route is exiting', function() {
+
+      test('it should reset controller', function(assert) {
+        // when
+        route.resetController(controller, true);
+
+        // then
+        assert.equal(controller.pageNumber, 1);
+        assert.equal(controller.pageSize, 10);
+        assert.equal(controller.firstName, null);
+        assert.equal(controller.lastName, null);
+        assert.equal(controller.email, null);
+      });
+    });
+
+    module('when route is not exiting', function() {
+
+      test('it should not reset controller', function(assert) {
+        // when
+        route.resetController(controller, false);
+
+        // then
+        assert.equal(controller.pageNumber, 'somePageNumber');
+        assert.equal(controller.pageSize, 'somePageSize');
+        assert.equal(controller.firstName, 'someFirstName');
+        assert.equal(controller.lastName, 'someLastName');
+        assert.equal(controller.email, 'someEmail');
+      });
+    });
+
   });
 });
