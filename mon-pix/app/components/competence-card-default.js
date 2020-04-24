8795ba06ed16eccff971d482735773c5c8a2b87c
ARJ <arj@octo.com>
2020-04-24 17:29:57 +0200

    Refacto: Use CompetenceCard to choose card to display

--- mon-pix/app/components/competence-card-default.js
+++ mon-pix/app/components/competence-card-default.js
@@ -3,7 +3,7 @@ import Component from '@ember/component';
 import classic from 'ember-classic-decorator';
 
 @classic
-export default class CompetenceCard extends Component {
+export default class CompetenceCardDefault extends Component {
   scorecard = null;
 
   @computed('scorecard.{level,isNotStarted}')
