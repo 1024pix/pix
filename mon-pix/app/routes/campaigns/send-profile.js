b56d378e5282bc7b3a15ada018680daf93fee79a
ARJ <arj@octo.com>
2020-04-24 17:34:29 +0200

    Add profile to sharing profile template

--- mon-pix/app/routes/campaigns/send-profile.js
+++ mon-pix/app/routes/campaigns/send-profile.js
@@ -7,19 +7,22 @@ export default class CampaignsSendProfileRoute extends Route {
   @service store;
 
   async model(params) {
-    const userId = this.currentUser.user.id;
+    const user = this.currentUser.user;
 
     const campaigns = await this.store.query('campaign', { filter: { code: params.campaign_code } });
     const campaign = campaigns.get('firstObject');
     return RSVP.hash({
       campaign,
-      campaignParticipation: this.store.queryRecord('campaignParticipation', { campaignId: campaign.id, userId }),
+      campaignParticipation: this.store.queryRecord('campaignParticipation', { campaignId: campaign.id, userId: user.id }),
+      user
     });
   }
 
-  async afterModel({ campaign, campaignParticipation }) {
+  async afterModel({ campaign, campaignParticipation, user }) {
     if (!campaignParticipation) {
       return this.replaceWith('campaigns.start-or-resume', campaign.code);
     }
+    await user.hasMany('scorecards').reload();
+    await user.belongsTo('pixScore').reload();
   }
 }
