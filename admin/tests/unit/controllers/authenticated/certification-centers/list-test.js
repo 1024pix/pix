2bf06442feb4826446f92c9ffdbbbdeeffed8e6d
laura-bergoens <laura.bergoens@gmail.com>
2020-04-24 11:29:22 +0200

    Fix debounce for certification-centers list filters

---
+++ admin/tests/unit/controllers/authenticated/certification-centers/list-test.js
@@ -0,0 +1,74 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Controller | authenticated/certification-centers/list', function(hooks) {
+  setupTest(hooks);
+  let controller;
+
+  hooks.beforeEach(function() {
+    controller = this.owner.lookup('controller:authenticated.certification-centers.list');
+  });
+
+  module('#triggerFiltering task', function() {
+
+    module('updating id', function() {
+
+      test('it should update controller id field', async function(assert) {
+        // given
+        controller.id = 'someId';
+        const expectedValue = 'someOtherId';
+
+        // when
+        await controller.triggerFiltering.perform('id', { target: { value: expectedValue } });
+
+        // then
+        assert.equal(controller.id, expectedValue);
+      });
+    });
+
+    module('updating name', function() {
+
+      test('it should update controller name field', async function(assert) {
+        // given
+        controller.name = 'someName';
+        const expectedValue = 'someOtherName';
+
+        // when
+        await controller.triggerFiltering.perform('name', { target: { value: expectedValue } });
+
+        // then
+        assert.equal(controller.name, expectedValue);
+      });
+    });
+
+    module('updating type', function() {
+
+      test('it should update controller type field', async function(assert) {
+        // given
+        controller.type = 'someType';
+        const expectedValue = 'someOtherType';
+
+        // when
+        await controller.triggerFiltering.perform('type', { target: { value: expectedValue } });
+
+        // then
+        assert.equal(controller.type, expectedValue);
+      });
+    });
+
+    module('updating externalId', function() {
+
+      test('it should update controller externalId field', async function(assert) {
+        // given
+        controller.externalId = 'someExternalId';
+        const expectedValue = 'someOtherExternalId';
+
+        // when
+        await controller.triggerFiltering.perform('externalId', { target: { value: expectedValue } });
+
+        // then
+        assert.equal(controller.externalId, expectedValue);
+      });
+    });
+  });
+});
---
+++ admin/tests/unit/routes/authenticated/certification-centers/list-test.js
@@ -0,0 +1,118 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import sinon from 'sinon';
+
+module('Unit | Route | authenticated/certification-centers/list', function(hooks) {
+  setupTest(hooks);
+  let route;
+
+  hooks.beforeEach(function() {
+    route = this.owner.lookup('route:authenticated/certification-centers/list');
+  });
+
+  module('#model', function(hooks) {
+    const params = {};
+    const expectedQueryArgs = {};
+
+    hooks.beforeEach(function() {
+      route.store.query = sinon.stub().resolves();
+      params.pageNumber = 'somePageNumber';
+      params.pageSize = 'somePageSize';
+      expectedQueryArgs.page = {
+        number: 'somePageNumber',
+        size: 'somePageSize',
+      };
+    });
+
+    module('when queryParams filters are falsy', function() {
+
+      test('it should call store.query with no filters on name, type and externalId', async function(assert) {
+        // when
+        await route.model(params);
+        expectedQueryArgs.filter = {
+          id: '',
+          name: '',
+          type: '',
+          externalId: '',
+        };
+
+        // then
+        sinon.assert.calledWith(route.store.query, 'certification-center', expectedQueryArgs);
+        assert.ok(true);
+      });
+    });
+
+    module('when queryParams filters are  truthy', function() {
+
+      test('it should call store.query with filters containing trimmed values', async function(assert) {
+        // given
+        params.id = 'someId';
+        params.name = ' someName';
+        params.type = 'someType ';
+        params.externalId = 'someExternalId';
+        expectedQueryArgs.filter = {
+          id: 'someId',
+          name: 'someName',
+          type: 'someType',
+          externalId: 'someExternalId',
+        };
+
+        // when
+        await route.model(params);
+
+        // then
+        sinon.assert.calledWith(route.store.query, 'certification-center', expectedQueryArgs);
+        assert.ok(true);
+      });
+    });
+
+  });
+
+  module('#resetController', function(hooks) {
+    let controller;
+
+    hooks.beforeEach(function() {
+      controller = {
+        pageNumber: 'somePageNumber',
+        pageSize: 'somePageSize',
+        id: 'someId',
+        name: 'someName',
+        type: 'someType',
+        externalId: 'someExternalId',
+      };
+    });
+
+    module('when route is exiting', function() {
+
+      test('it should reset controller', function(assert) {
+        // when
+        route.resetController(controller, true);
+
+        // then
+        assert.equal(controller.pageNumber, 1);
+        assert.equal(controller.pageSize, 10);
+        assert.equal(controller.id, null);
+        assert.equal(controller.name, null);
+        assert.equal(controller.type, null);
+        assert.equal(controller.externalId, null);
+      });
+    });
+
+    module('when route is not exiting', function() {
+
+      test('it should not reset controller', function(assert) {
+        // when
+        route.resetController(controller, false);
+
+        // then
+        assert.equal(controller.pageNumber, 'somePageNumber');
+        assert.equal(controller.pageSize, 'somePageSize');
+        assert.equal(controller.id, 'someId');
+        assert.equal(controller.name, 'someName');
+        assert.equal(controller.type, 'someType');
+        assert.equal(controller.externalId, 'someExternalId');
+      });
+    });
+
+  });
+});
