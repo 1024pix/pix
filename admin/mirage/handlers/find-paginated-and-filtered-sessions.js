82b2aead39feb6b14f33401dc962eaa425d187c8
laura-bergoens <laura.bergoens@gmail.com>
2020-04-24 14:34:05 +0200

    Add acceptance tests on paginated session list

--- admin/mirage/handlers/find-paginated-and-filtered-sessions.js
+++ admin/mirage/handlers/find-paginated-and-filtered-sessions.js
@@ -1,31 +1,57 @@
 import _ from 'lodash';
 
 export function findPaginatedAndFilteredSessions(schema, request) {
-  const pageSize = request.queryParams.page ? parseInt(request.queryParams.page.size) : 10;
-  const filters = request.queryParams.filter;
+  const queryParams = request.queryParams;
+  const pageSize = queryParams ? parseInt(queryParams['page[size]']) : 10;
+  const page = queryParams ? parseInt(queryParams['page[number]']) : 1;
+  const start = (page - 1) * pageSize;
+  const end = start + pageSize;
+  const idFilter = queryParams ? queryParams['filter[id]'] : null;
+  const certificationCenterNameFilter = queryParams ? queryParams['filter[certificationCenterName]'] : null;
+  const statusFilter = queryParams ? queryParams['filter[status]'] : null;
+  const resultsSentToPrescriberAtFilter = queryParams ? queryParams['filter[resultsSentToPrescriberAt]'] : null;
   const sessions = schema.sessions.all().models;
-  const allSessionsCount = sessions.length;
+  const rowCount = sessions.length;
+
   // Apply filters
   let filteredSessions = sessions;
-  if (filters && filters.id) {
-    const filterId = filters.id;
-    filteredSessions = _.map(sessions, (session) => {
-      const currentSessionId = session.id;
-      const match = currentSessionId.search(filterId);
-      return match !== -1;
+  if (idFilter) {
+    filteredSessions = _.filter(filteredSessions, (session) => {
+      return session.id === idFilter;
     });
   }
-  // Apply sorting
-  const sortedSessions = _.sortBy(filteredSessions, ['id']);
+  if (certificationCenterNameFilter) {
+    const filterName = certificationCenterNameFilter.toLowerCase();
+    filteredSessions = _.filter(filteredSessions, (session) => {
+      const currentName = session.certificationCenterName.toLowerCase();
+      return currentName.search(filterName) !== -1;
+    });
+  }
+  if (statusFilter) {
+    filteredSessions = _.filter(filteredSessions, { status: statusFilter });
+  }
+  if (resultsSentToPrescriberAtFilter) {
+    if (resultsSentToPrescriberAtFilter === 'true') {
+      filteredSessions = _.filter(filteredSessions, (session) => {
+        return Boolean(session.resultsSentToPrescriberAt);
+      });
+    }
+    if (resultsSentToPrescriberAtFilter === 'false') {
+      filteredSessions = _.filter(filteredSessions, (session) => {
+        return !(session.resultsSentToPrescriberAt);
+      });
+    }
+  }
+
   // Apply page size
-  const sessionsToSend = _.slice(sortedSessions, 0, pageSize);
+  const sessionsToSend = _.slice(filteredSessions, start, end);
 
   const json = this.serialize({ modelName: 'session', models: sessionsToSend }, 'session');
   json.meta = {
-    page: 1,
+    page,
     pageSize,
-    rowCount: allSessionsCount,
-    pageCount: Math.ceil(allSessionsCount / pageSize),
+    rowCount,
+    pageCount: Math.ceil(rowCount / pageSize),
   };
   return json;
 }
