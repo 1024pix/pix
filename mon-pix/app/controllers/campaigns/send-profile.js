7fa31537a7a0b1a62d063815c2c74637c98877ee
ARJ <arj@octo.com>
2020-04-24 17:43:47 +0200

    Use new syntax for send-profile controller

--- mon-pix/app/controllers/campaigns/send-profile.js
+++ mon-pix/app/controllers/campaigns/send-profile.js
@@ -1,29 +1,27 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
-export default Controller.extend({
+export default class SendProfileController extends Controller {
 
-  isLoading: false,
-  errorMessage: null,
-  pageTitle: 'Envoyer mon profil',
+  isLoading = false;
+  errorMessage = null;
 
-  actions: {
+  @action
+  sendProfile() {
+    this.set('errorMessage', null);
+    this.set('isLoading', true);
 
-    sendProfile() {
-      this.set('errorMessage', null);
-      this.set('isLoading', true);
+    const campaignParticipation = this.get('model.campaignParticipation');
+    campaignParticipation.set('isShared', true);
 
-      const campaignParticipation = this.get('model.campaignParticipation');
-      campaignParticipation.set('isShared', true);
-
-      return campaignParticipation.save()
-        .then(() => {
-          this.set('isLoading', false);
-        })
-        .catch(() => {
-          campaignParticipation.rollbackAttributes();
-          this.set('isLoading', false);
-          this.set('errorMessage', true);
-        });
-    },
+    return campaignParticipation.save()
+      .then(() => {
+        this.set('isLoading', false);
+      })
+      .catch(() => {
+        campaignParticipation.rollbackAttributes();
+        this.set('isLoading', false);
+        this.set('errorMessage', true);
+      });
   }
-});
+}
