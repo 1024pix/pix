488ad192095af3fbc3687da24c8be9223ab217e5
Benjamin Petetot <bpetetot@gmail.com>
2020-04-27 18:25:24 +0200

    Add banner to send collect profile

--- mon-pix/tests/integration/components/resume-campaign-banner-test.js
+++ mon-pix/tests/integration/components/resume-campaign-banner-test.js
@@ -15,7 +15,8 @@ describe('Integration | Component | resume-campaign-banner', function() {
       createdAt: '2019-09-30T12:30:00Z',
       campaign: EmberObject.create({
         code: 'AZERTY',
-        title: 'Parcours Pix'
+        title: 'Parcours Pix',
+        isTypeAssessment: true,
       })
     });
     const oldCampaignNotFinished = EmberObject.create({
@@ -23,6 +24,7 @@ describe('Integration | Component | resume-campaign-banner', function() {
       createdAt: '2019-09-30T10:30:00Z',
       campaign: EmberObject.create({
         code: 'AZERTY',
+        isTypeAssessment: true,
       })
     });
     const campaignFinished = EmberObject.create({
@@ -30,146 +32,172 @@ describe('Integration | Component | resume-campaign-banner', function() {
       createdAt: '2019-09-30T14:30:00Z',
       campaign: EmberObject.create({
         code: 'AZERTY',
+        isTypeAssessment: true,
       }),
       assessment: EmberObject.create({
         isCompleted: true,
       }),
     });
 
-    context('when campaign is not finished and not shared', function() {
+    context('when campaign has type assessment', function() {
 
-      it('should display the resume campaign banner', async function() {
-        // given
-        this.set('campaignParticipations', [campaignToResume, oldCampaignNotFinished, campaignFinished]);
+      context('when campaign is not finished and not shared', function() {
 
-        // when
-        await render(hbs`{{resume-campaign-banner campaignParticipations=campaignParticipations}}`);
-
-        // then
-        expect(find('.resume-campaign-banner__container')).to.exist;
+        it('should display the resume campaign banner', async function() {
+          // given
+          this.set('campaignParticipations', [campaignToResume, oldCampaignNotFinished, campaignFinished]);
+  
+          // when
+          await render(hbs`<ResumeCampaignBanner @campaignParticipations={{campaignParticipations}} />`);
+  
+          // then
+          expect(find('.resume-campaign-banner__container')).to.exist;
+        });
+  
+        it('should display a link to resume the campaign', async function() {
+          // given
+          this.set('campaignParticipations', [campaignToResume]);
+  
+          // when
+          await render(hbs`<ResumeCampaignBanner @campaignParticipations={{campaignParticipations}} />`);
+  
+          // then
+          expect(find('.resume-campaign-banner__button').textContent).to.equal('Reprendre');
+        });
+  
+        it('should display a sentence to ask user to resume with the title of campaign', async function() {
+          // given
+          this.set('campaignParticipations', [campaignToResume]);
+  
+          // when
+          await render(hbs`<ResumeCampaignBanner @campaignParticipations={{campaignParticipations}} />`);
+  
+          // then
+          expect(find('.resume-campaign-banner__title').textContent).to.equal(`Vous n'avez pas terminé le parcours "${campaignToResume.campaign.title}"`);
+        });
+  
+        it('should display a simple sentence to ask user to resume when campaign has no title', async function() {
+          // given
+          campaignToResume.campaign.set('title', null);
+          this.set('campaignParticipations', [campaignToResume]);
+  
+          // when
+          await render(hbs`<ResumeCampaignBanner @campaignParticipations={{campaignParticipations}} />`);
+  
+          // then
+          expect(find('.resume-campaign-banner__title').textContent).to.equal('Vous n\'avez pas terminé votre parcours');
+        });
+  
+      });
+  
+      context('when campaign is finished but not shared', function() {
+  
+        const campaignFinishedButNotShared = EmberObject.create({
+          isShared: false,
+          createdAt: '2019-09-30T10:30:00Z',
+          campaign: EmberObject.create({
+            code: 'AZERTY',
+            title: 'Parcours Pix',
+            isTypeAssessment: true,
+          }),
+          assessment: EmberObject.create({
+            isCompleted: true,
+          }),
+        });
+  
+        it('should display the resume campaign banner', async function() {
+          // given
+          this.set('campaignParticipations', [oldCampaignNotFinished, campaignFinished, campaignFinishedButNotShared]);
+          // when
+          await render(hbs`<ResumeCampaignBanner @campaignParticipations={{campaignParticipations}} />`);
+          // then
+          expect(find('.resume-campaign-banner__container')).to.exist;
+        });
+  
+        it('should display a link to shared the results', async function() {
+          // given
+          this.set('campaignParticipations', [campaignFinishedButNotShared]);
+  
+          // when
+          await render(hbs`<ResumeCampaignBanner @campaignParticipations={{campaignParticipations}} />`);
+  
+          // then
+          expect(find('.resume-campaign-banner__button').textContent).to.equal('Continuer');
+        });
+  
+        it('should display a sentence to ask user to share his results with the title of campaign', async function() {
+          // given
+          this.set('campaignParticipations', [campaignFinishedButNotShared]);
+  
+          // when
+          await render(hbs`<ResumeCampaignBanner @campaignParticipations={{campaignParticipations}} />`);
+  
+          // then
+          expect(find('.resume-campaign-banner__title').textContent).to.equal(`Parcours "${campaignFinishedButNotShared.campaign.title}" terminé. N'oubliez pas de finaliser votre envoi !`);
+        });
+  
+        it('should display a simple sentence to ask user to share his results when campaign has no title', async function() {
+          // given
+          campaignFinishedButNotShared.campaign.set('title', null);
+          this.set('campaignParticipations', [campaignFinishedButNotShared]);
+  
+          // when
+          await render(hbs`<ResumeCampaignBanner @campaignParticipations={{campaignParticipations}} />`);
+  
+          // then
+          expect(find('.resume-campaign-banner__title').textContent).to.equal('N\'oubliez pas de finaliser votre envoi !');
+        });
+      });
+  
+      context('when campaign is finished and shared', function() {
+  
+        it('should not display the resume campaign banner when the list of campaigns contains only finished campaign', async function() {
+          // given
+          this.set('campaignParticipations', [campaignFinished]);
+          // when
+          await render(hbs`<ResumeCampaignBanner @campaignParticipations={{campaignParticipations}} />`);
+          // then
+          expect(find('.resume-campaign-banner__container')).to.not.exist;
+        });
       });
 
-      it('should display a link to resume the campaign', async function() {
-        // given
-        this.set('campaignParticipations', [campaignToResume]);
-
-        // when
-        await render(hbs`{{resume-campaign-banner campaignParticipations=campaignParticipations}}`);
-
-        // then
-        expect(find('.resume-campaign-banner__button')).to.exist;
-        expect(find('.resume-campaign-banner__button').textContent).to.equal('Reprendre');
-      });
-
-      it('should display a sentence to ask user to resume with the title of campaign', async function() {
-        // given
-        this.set('campaignParticipations', [campaignToResume]);
-
-        // when
-        await render(hbs`{{resume-campaign-banner campaignParticipations=campaignParticipations}}`);
-
-        // then
-        expect(find('.resume-campaign-banner__title')).to.exist;
-        expect(find('.resume-campaign-banner__title').textContent).to.equal(`Vous n'avez pas terminé le parcours "${campaignToResume.campaign.title}"`);
-      });
-
-      it('should display a simple sentence to ask user to resume when campaign has no title', async function() {
-        // given
-        campaignToResume.campaign.set('title', null);
-        this.set('campaignParticipations', [campaignToResume]);
-
-        // when
-        await render(hbs`{{resume-campaign-banner campaignParticipations=campaignParticipations}}`);
-
-        // then
-        expect(find('.resume-campaign-banner__title')).to.exist;
-        expect(find('.resume-campaign-banner__title').textContent).to.equal('Vous n\'avez pas terminé votre parcours');
-      });
-
-    });
-
-    context('when campaign is finished but not shared', function() {
-
-      const campaignFinishedButNotShared = EmberObject.create({
-        isShared: false,
-        createdAt: '2019-09-30T10:30:00Z',
-        campaign: EmberObject.create({
-          code: 'AZERTY',
-          title: 'Parcours Pix'
-        }),
-        assessment: EmberObject.create({
-          isCompleted: true,
-        }),
-      });
-
-      it('should display the resume campaign banner', async function() {
-        // given
-        this.set('campaignParticipations', [oldCampaignNotFinished, campaignFinished, campaignFinishedButNotShared]);
-        // when
-        await render(hbs`{{resume-campaign-banner campaignParticipations=campaignParticipations}}`);
-        // then
-        expect(find('.resume-campaign-banner__container')).to.exist;
-      });
-
-      it('should display a link to shared the results', async function() {
-        // given
-        this.set('campaignParticipations', [campaignFinishedButNotShared]);
-
-        // when
-        await render(hbs`{{resume-campaign-banner campaignParticipations=campaignParticipations}}`);
-
-        // then
-        expect(find('.resume-campaign-banner__button')).to.exist;
-        expect(find('.resume-campaign-banner__button').textContent).to.equal('Continuer');
-      });
-
-      it('should display a sentence to ask user to share his results with the title of campaign', async function() {
-        // given
-        this.set('campaignParticipations', [campaignFinishedButNotShared]);
-
-        // when
-        await render(hbs`{{resume-campaign-banner campaignParticipations=campaignParticipations}}`);
-
-        // then
-        expect(find('.resume-campaign-banner__title')).to.exist;
-        expect(find('.resume-campaign-banner__title').textContent).to.equal(`Parcours "${campaignFinishedButNotShared.campaign.title}" terminé ! Envoyez vos résultats.`);
-      });
-
-      it('should display a simple sentence to ask user to share his results when campaign has no title', async function() {
-        // given
-        campaignFinishedButNotShared.campaign.set('title', null);
-        this.set('campaignParticipations', [campaignFinishedButNotShared]);
-
-        // when
-        await render(hbs`{{resume-campaign-banner campaignParticipations=campaignParticipations}}`);
-
-        // then
-        expect(find('.resume-campaign-banner__title')).to.exist;
-        expect(find('.resume-campaign-banner__title').textContent).to.equal('Parcours terminé ! Envoyez vos résultats.');
-      });
-    });
-
-    context('when campaign is finished and shared', function() {
-
-      it('should not display the resume campaign banner when the list of campaigns contains only finished campaign', async function() {
-        // given
-        this.set('campaignParticipations', [campaignFinished]);
-        // when
-        await render(hbs`{{resume-campaign-banner campaignParticipations=campaignParticipations}}`);
-        // then
-        expect(find('.resume-campaign-banner__container')).to.not.exist;
-      });
     });
 
     context('when campaign is not started yet', function() {
-
+  
       it('should not display the resume campaign banner when the list of campaigns is empty', async function() {
         // given
         this.set('campaignParticipations', []);
         // when
-        await render(hbs`{{resume-campaign-banner campaignParticipations=campaignParticipations}}`);
+        await render(hbs`<ResumeCampaignBanner @campaignParticipations={{campaignParticipations}} />`);
         // then
         expect(find('.resume-campaign-banner__container')).to.not.exist;
+      });
+    });
+
+    context('when campaign has type collect profiles', function() {
+      context('when campaign is not shared', function() {
+  
+        const campaignCollectNotShared = EmberObject.create({
+          isShared: false,
+          createdAt: '2019-09-30T14:30:00Z',
+          campaign: EmberObject.create({
+            code: 'AZERTY',
+            isTypeAssessment: false,
+          }),
+        });
+  
+        it('should display the resume campaign banner', async function() {
+          // given
+          this.set('campaignParticipations', [campaignCollectNotShared]);
+  
+          // when
+          await render(hbs`<ResumeCampaignBanner @campaignParticipations={{campaignParticipations}} />`);
+  
+          // then
+          expect(find('.resume-campaign-banner__title').textContent).to.equal('N\'oubliez pas de finaliser votre envoi !');
+        });
+  
       });
     });
 
