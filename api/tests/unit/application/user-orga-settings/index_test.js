73b9635c6156fdd33acaeaf832859261458f24b9
Vincent Hardouin <vincent.hardouin@pix.fr>
2020-04-27 10:59:58 +0200

    [API] Remove depreciate POST /user-orga-settings

--- api/tests/unit/application/user-orga-settings/index_test.js
+++ api/tests/unit/application/user-orga-settings/index_test.js
@@ -8,85 +8,9 @@ describe('Unit | Router | user-orga-settings-router', () => {
   let httpTestServer;
 
   beforeEach(() => {
-    sinon.stub(userOrgaSettingsController, 'create').returns('ok');
     sinon.stub(userOrgaSettingsController, 'update').returns('ok');
     sinon.stub(userOrgaSettingsController, 'createOrUpdate').returns('ok');
     httpTestServer = new HttpTestServer(moduleUnderTest);
-  });
-
-  describe('POST /api/user-orga-settings', () => {
-
-    let method;
-    let url;
-    let payload;
-
-    beforeEach(() => {
-      method = 'POST';
-      url = '/api/user-orga-settings';
-      payload = {
-        data: {
-          relationships: {
-            organization: {
-              data: {
-                id: 1,
-                type: 'organizations'
-              }
-            },
-            user: {
-              data: {
-                id: 1,
-                type: 'users'
-              }
-            }
-          }
-        }
-      };
-    });
-
-    it('should exist', async () => {
-      // when
-      const response = await httpTestServer.request(method, url, payload);
-
-      // then
-      expect(response.statusCode).to.equal(200);
-    });
-
-    describe('Payload schema validation', () => {
-
-      it('should have a payload', async () => {
-        // given
-        payload = undefined;
-
-        // when
-        const result = await httpTestServer.request(method, url, payload);
-
-        // then
-        expect(result.statusCode).to.equal(400);
-      });
-
-      it('should have an organization id in relationship in payload', async () => {
-        // given
-        payload.data.relationships.organization.data.id = undefined;
-
-        // when
-        const response = await httpTestServer.request(method, url, payload);
-
-        // then
-        expect(response.statusCode).to.equal(400);
-      });
-
-      it('should have a user id in relationship in payload', async () => {
-        // given
-        payload.data.relationships.user.data.id = undefined;
-
-        // when
-        const response = await httpTestServer.request(method, url, payload);
-
-        // then
-        expect(response.statusCode).to.equal(400);
-      });
-    });
-
   });
 
   describe('PATCH /api/user-orga-settings/{id}', () => {
--- api/tests/unit/application/user-orga-settings/user-orga-settings-controller_test.js
+++ api/tests/unit/application/user-orga-settings/user-orga-settings-controller_test.js
@@ -1,10 +1,4 @@
-const { sinon, expect, hFake, catchErr } = require('../../../test-helper');
-
-const UserOrgaSettings = require('../../../../lib/domain/models/UserOrgaSettings');
-const User = require('../../../../lib/domain/models/User');
-const Organization = require('../../../../lib/domain/models/Organization');
-
-const { UserNotAuthorizedToCreateResourceError } = require('../../../../lib/domain/errors');
+const { sinon, expect } = require('../../../test-helper');
 
 const userOrgaSettingsController = require('../../../../lib/application/user-orga-settings/user-orga-settings-controller');
 
@@ -13,85 +7,6 @@ const usecases = require('../../../../lib/domain/usecases');
 const userOrgaSettingsSerializer = require('../../../../lib/infrastructure/serializers/jsonapi/user-orga-settings-serializer');
 
 describe('Unit | Controller | user-orga-settings-controller', () => {
-
-  describe('#create', () => {
-    const userId = 123;
-    const organizationId = 234;
-
-    const userOrgaSettings = new UserOrgaSettings({ user: new User({ id: userId }), organization: new Organization({ id: organizationId }) });
-
-    let request;
-
-    beforeEach(() => {
-      request = {
-        auth: { credentials: { userId } },
-        payload: {
-          data: {
-            relationships: {
-              organization: {
-                data: {
-                  id: organizationId,
-                  type: 'organizations'
-                }
-              },
-              user: {
-                data: {
-                  id: userId,
-                  type: 'users'
-                }
-              }
-            },
-          },
-        },
-      };
-
-      sinon.stub(userOrgaSettingsSerializer, 'serialize');
-      sinon.stub(usecases, 'createUserOrgaSettings');
-    });
-
-    describe('when request is valid', () => {
-
-      beforeEach(() => {
-        usecases.createUserOrgaSettings.resolves(userOrgaSettings);
-      });
-
-      it('should return a serialized user and a 201 status code', async () => {
-        // given
-        const expectedSerializedUserOrgaSettings = { message: 'serialized user' };
-        userOrgaSettingsSerializer.serialize.returns(expectedSerializedUserOrgaSettings);
-
-        // when
-        const response = await userOrgaSettingsController.create(request, hFake);
-
-        // then
-        expect(userOrgaSettingsSerializer.serialize).to.have.been.calledWith(userOrgaSettings);
-        expect(response.source).to.deep.equal(expectedSerializedUserOrgaSettings);
-        expect(response.statusCode).to.equal(201);
-      });
-
-      it('should call the user orga settings creation usecase', async () => {
-        // when
-        await userOrgaSettingsController.create(request, hFake);
-
-        // then
-        expect(usecases.createUserOrgaSettings).to.have.been.calledWith({ userId, organizationId });
-      });
-    });
-
-    describe('when request in not valid', function() {
-
-      it('should throw a UserNotAuthorizedToCreateResourceError when payload user and connected user are not the same', async () => {
-        // given
-        request.auth.credentials.userId = 321;
-
-        // when
-        const error = await catchErr(userOrgaSettingsController.create)(request, hFake);
-
-        // then
-        expect(error).to.be.instanceof(UserNotAuthorizedToCreateResourceError);
-      });
-    });
-  });
 
   describe('#update', () => {
 
