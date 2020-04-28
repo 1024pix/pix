229a4e89fb9b1acb2ba98a08b7b7b48ee1c5f864
FRIN Yvonnick <frin.yvonnick@gmail.com>
2020-04-28 16:33:27 +0200

    Add documentation link in sidebar menu for users of pro organizations

--- orga/app/components/sidebar-menu.js
+++ orga/app/components/sidebar-menu.js
@@ -1,6 +1,20 @@
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default class SidebarMenu extends Component {
   @service currentUser;
+
+  @computed('currentUser.organization')
+  get documentationUrl() {
+    if (this.currentUser.isSCOManagingStudents) {
+      return 'https://bit.ly/ressourcespixorga';
+    }
+
+    if (this.currentUser.organization.isPro) {
+      return 'https://cloud.pix.fr/s/cwZN2GAbqSPGnw4';
+    }
+
+    return null;
+  }
 }
