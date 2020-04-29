40d15df506d47cd21c2a6b0655885ae749780790
Benjamin Petetot <bpetetot@gmail.com>
2020-04-29 11:20:38 +0200

    Change error messages on campaign code

--- mon-pix/app/controllers/campaigns/fill-in-campaign-code.js
+++ mon-pix/app/controllers/campaigns/fill-in-campaign-code.js
@@ -12,32 +12,31 @@ export default class FillInCampaignCodeController extends Controller {
 
   @action
   async startCampaign() {
-    this.set('errorMessage', null);
+    this.clearErrorMessage();
 
     if (!this.campaignCode) {
-      this.set('errorMessage', 'Merci de renseigner le code du parcours.');
-
-    } else {
-      const campaignCode = this.campaignCode.toUpperCase();
-
-      const campaignCodeExists = await this.store.query('campaign', { filter: { code: campaignCode } })
-        .then(() => true,
-          (error) => {
-            if (error.errors[0].status === '403') {
-              this.set('errorMessage', 'Oups ! nous ne parvenons pas à vous trouver. Verifiez vos informations afin de continuer ou prévenez l’organisateur de votre parcours.');
-              return false;
-            }
-            if (error.errors[0].status === '404') {
-              this.set('errorMessage', 'Votre code de parcours est erroné, veuillez vérifier ou contacter la personne organisant le parcours de test.');
-              return false;
-            }
-            throw (error);
-          });
-
-      if (campaignCodeExists) {
-        return this.transitionToRoute('campaigns.start-or-resume', campaignCode);
-      }
+      this.set('errorMessage', 'Veuillez saisir un code.');
+      return;
     }
+  
+    const campaignCode = this.campaignCode.toUpperCase();
+    try {
+      await this.store.query('campaign', { filter: { code: campaignCode } });
+      return this.transitionToRoute('campaigns.start-or-resume', campaignCode);
+    } catch (error) {
+      this.onStartCampaignError(error);
+    }
+  }
+
+  onStartCampaignError(error) {
+    const { status } = error.errors[0];
+    if (status === '403') {
+      this.set('errorMessage', 'Oups ! nous ne parvenons pas à vous trouver. Verifiez vos informations afin de continuer ou prévenez l’organisateur.');
+    } else if (status === '404') {
+      this.set('errorMessage', 'Votre code est erroné, veuillez vérifier ou contacter l\'organisateur.');
+    } else {
+      throw (error);
+    } 
   }
 
   @action
