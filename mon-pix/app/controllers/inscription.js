3f67bf7855d99c24cbb3b474d89577b4ab27e21b
celineung <celine.ung@zenika.com>
2020-04-30 12:04:52 +0200

    Integrate translations on signup page title

--- mon-pix/app/controllers/inscription.js
+++ mon-pix/app/controllers/inscription.js
@@ -1,7 +1,8 @@
-import classic from 'ember-classic-decorator';
+import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
 
-@classic
 export default class InscriptionController extends Controller {
-  pageTitle = 'Inscription';
+  @service intl;
+
+  pageTitle = this.intl.t('page-title.signup');
 }
