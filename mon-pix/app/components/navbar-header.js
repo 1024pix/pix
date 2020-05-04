445b650cedf81a2e618fca6287dea39bd3b1130b
celineung <celine.ung@zenika.com>
2020-05-04 11:33:15 +0200

    Glimmerize navbar-header component

--- mon-pix/app/components/navbar-header.js
+++ mon-pix/app/components/navbar-header.js
@@ -1,9 +1,5 @@
-import { classNames } from '@ember-decorators/component';
-import Component from '@ember/component';
-import classic from 'ember-classic-decorator';
+import Component from '@glimmer/component';
 
-@classic
-@classNames('navbar-header')
 export default class NavbarHeader extends Component {
-  burger = null;
+
 }
