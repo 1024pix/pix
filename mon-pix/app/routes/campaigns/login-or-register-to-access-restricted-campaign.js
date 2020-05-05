7da4045b538cfd7db3ea6acf3c72376528dd9ce0
lisequesnel <quesnel.lise@gmail.com>
2020-05-05 18:30:09 +0200

    Handle 2 different logic in 2 start-or-resume

--- mon-pix/app/routes/campaigns/login-or-register-to-access-restricted-campaign.js
+++ mon-pix/app/routes/campaigns/login-or-register-to-access-restricted-campaign.js
@@ -4,9 +4,16 @@ import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-
 
 @classic
 export default class LoginOrRegisterToAccessRestrictedCampaignRoute extends Route.extend(UnauthenticatedRouteMixin) {
+
+  campaignCode = null;
+
+  async beforeModel(transition) {
+    this.campaignCode = transition.to.parent.params.campaign_code;
+    return super.beforeModel(...arguments);
+  }
+
   async model() {
-    const campaignCode = this.paramsFor('campaigns').campaign_code;
-    const campaigns = await this.store.query('campaign', { filter: { code: campaignCode } });
+    const campaigns = await this.store.query('campaign', { filter: { code: this.campaignCode } });
     return campaigns.firstObject;
   }
 }
--- mon-pix/app/routes/campaigns/start-or-resume.js
+++ mon-pix/app/routes/campaigns/start-or-resume.js
@@ -1,41 +1,27 @@
-import classic from 'ember-classic-decorator';
-import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 import SecuredRouteMixin from 'mon-pix/mixins/secured-route-mixin';
 
-import { isEmpty } from '@ember/utils';
-
-@classic
 export default class StartOrResumeRoute extends Route.extend(SecuredRouteMixin) {
   @service currentUser;
   @service session;
 
   campaignCode = null;
-  campaign = null;
-  associationDone = false;
+  participantExternalId = null;
   campaignIsRestricted = false;
-  givenParticipantExternalId = null;
   userHasSeenLanding = false;
-  userHasJustConsultedTutorial = false;
-  campaignParticipationIsStarted = false;
   authenticationRoute = 'inscription';
-  _isReady = false;
 
   beforeModel(transition) {
-    this.set('_isReady', false);
-    this.set('campaignCode', transition.to.parent.params.campaign_code);
-    this.set('associationDone', transition.to.queryParams.associationDone);
-    this.set('campaignIsRestricted', transition.to.queryParams.campaignIsRestricted);
-    this.set('givenParticipantExternalId', this.givenParticipantExternalId || transition.to.queryParams.participantExternalId);
-    this.set('userHasSeenLanding', transition.to.queryParams.hasSeenLanding);
-    this.set('userHasJustConsultedTutorial', transition.to.queryParams.hasJustConsultedTutorial);
-    this.set('campaignParticipationIsStarted', transition.to.queryParams.campaignParticipationIsStarted);
+    this.campaignCode = transition.to.parent.params.campaign_code;
+    this.participantExternalId = transition.to.queryParams.participantExternalId;
+    this.campaignIsRestricted = transition.to.queryParams.campaignIsRestricted;
+    this.userHasSeenLanding = transition.to.queryParams.hasSeenLanding;
 
     if (this._userIsUnauthenticated() && !this.userHasSeenLanding && this.campaignIsRestricted) {
-      this.set('authenticationRoute', 'campaigns.login-or-register-to-access-restricted-campaign');
-      this.transitionTo('campaigns.login-or-register-to-access-restricted-campaign',this.campaignCode);
+      this.authenticationRoute = 'campaigns.login-or-register-to-access-restricted-campaign';
     }
+
     if (this._userIsUnauthenticated() && !this.userHasSeenLanding && !this.campaignIsRestricted) {
       return this.replaceWith('campaigns.campaign-landing-page', this.campaignCode, { queryParams: transition.to.queryParams });
     }
@@ -43,7 +29,7 @@ export default class StartOrResumeRoute extends Route.extend(SecuredRouteMixin) 
   }
 
   async model() {
-    this.set('isLoading', true);
+    this.isLoading = true;
     const campaigns = await this.store.query('campaign', { filter: { code: this.campaignCode } });
 
     return campaigns.get('firstObject');
@@ -52,58 +38,26 @@ export default class StartOrResumeRoute extends Route.extend(SecuredRouteMixin) 
   async redirect(campaign) {
 
     if (campaign.isArchived) {
-      this.set('isLoading', false);
-      return this.set('_isReady', true);
+      this.isLoading = false;
+      return;
     }
 
-    const smartPlacementAssessments = await this.store.query(
-      'assessment',
-      { filter: { type: 'SMART_PLACEMENT', codeCampaign: this.campaignCode } },
-    );
-    this.set('_isReady', true);
-
-    if (this._thereIsNoAssessment(smartPlacementAssessments)) {
-
-      if (campaign.isTypeProfilesCollection && this.campaignParticipationIsStarted) {
-        return this.replaceWith('profiles-collection-campaigns.send-profile', this.campaignCode);
-      }
-      if (this.userHasSeenLanding) {
-        return this.replaceWith('campaigns.fill-in-id-pix', this.campaignCode, { queryParams: { givenParticipantExternalId: this.givenParticipantExternalId } });
-      }
-      if (!campaign.isRestricted || this.associationDone) {
-        return this.replaceWith('campaigns.campaign-landing-page', this.campaignCode, { queryParams: { givenParticipantExternalId: this.givenParticipantExternalId } });
-      }
-      return this.replaceWith('campaigns.join-restricted-campaign', this.campaignCode, { queryParams: { givenParticipantExternalId: this.givenParticipantExternalId } });
+    if (campaign.isTypeProfilesCollection) {
+      return this.replaceWith('profiles-collection-campaigns.start-or-resume', this.campaignCode, {
+        queryParams: {
+          participantExternalId: this.participantExternalId,
+        }
+      });
     }
 
-    const assessment = await smartPlacementAssessments.get('firstObject').reload();
-
-    if (this._showTutorial(assessment)) {
-      return this.replaceWith('assessment-campaigns.tutorial', this.campaignCode);
-    }
-
-    return this.replaceWith('assessments.resume', assessment.get('id'));
+    return this.replaceWith('assessment-campaigns.start-or-resume', this.campaignCode, {
+      queryParams: {
+        participantExternalId: this.participantExternalId,
+      }
+    });
   }
 
   _userIsUnauthenticated() {
     return this.session.isAuthenticated === false;
-  }
-
-  _thereIsNoAssessment(assessments) {
-    return isEmpty(assessments);
-  }
-
-  _showTutorial(assessment) {
-    return (
-      !this.userHasJustConsultedTutorial
-      && assessment.answers.length === 0
-      && !assessment.isCompleted
-      && !this.currentUser.user.hasSeenAssessmentInstructions
-    );
-  }
-
-  @action
-  loading() {
-    return this._isReady;
   }
 }
