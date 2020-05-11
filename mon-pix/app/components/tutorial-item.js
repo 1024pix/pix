77a5eb63c9fe494b3c9e4d960ed1f77acf46fd68
Guillaume Lagorce <guillaume@hey-gul.com>
2020-05-11 17:12:50 +0200

    BSR: Rename and clearly split names and status between save and evaluation tutorial process.

--- mon-pix/app/components/tutorial-item.js
+++ mon-pix/app/components/tutorial-item.js
@@ -3,10 +3,10 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
-const statusTypes = {
-  unsaved: 'unsaved',
-  saving: 'saving',
-  saved: 'saved',
+const buttonStatusTypes = {
+  unrecorded: 'unrecorded',
+  pending: 'pending',
+  recorded: 'recorded',
 };
 
 export default class TutorialItemComponent extends Component {
@@ -18,13 +18,13 @@ export default class TutorialItemComponent extends Component {
     'son': 'son',
     'page': 'page'
   };
-  @tracked status = statusTypes.unsaved;
-  @tracked evaluationStatus = statusTypes.unsaved;
+  @tracked savingStatus = buttonStatusTypes.unrecorded;
+  @tracked evaluationStatus = buttonStatusTypes.unrecorded;
 
   constructor(owner, args) {
     super(owner, args);
-    this.status = this.tutorial.isSaved ? statusTypes.saved : statusTypes.unsaved;
-    this.evaluationStatus = this.tutorial.isEvaluated ? statusTypes.saved : statusTypes.unsaved;
+    this.savingStatus = this.tutorial.isSaved ? buttonStatusTypes.recorded : buttonStatusTypes.unrecorded;
+    this.evaluationStatus = this.tutorial.isEvaluated ? buttonStatusTypes.recorded : buttonStatusTypes.unrecorded;
   }
 
   get tutorial() {
@@ -40,63 +40,63 @@ export default class TutorialItemComponent extends Component {
   }
 
   get isSaved() {
-    return this.status === 'saved';
+    return this.savingStatus === buttonStatusTypes.recorded;
   }
 
   get isEvaluated() {
-    return this.evaluationStatus === 'saved';
+    return this.evaluationStatus === buttonStatusTypes.recorded;
   }
 
   get buttonText() {
-    return this.status === statusTypes.saved ? 'Retirer' : 'Enregistrer';
+    return this.savingStatus === buttonStatusTypes.recorded ? 'Retirer' : 'Enregistrer';
   }
 
   get buttonTitle() {
-    return this.status === statusTypes.saved ? 'Retirer' : 'Enregistrer dans ma liste de tutos';
+    return this.savingStatus === buttonStatusTypes.recorded ? 'Retirer' : 'Enregistrer dans ma liste de tutos';
   }
 
-  get isButtonDisabled() {
-    return this.status === statusTypes.saving;
+  get isSaveButtonDisabled() {
+    return this.savingStatus === buttonStatusTypes.pending;
   }
 
   get isEvaluateButtonDisabled() {
-    return this.evaluationStatus !== statusTypes.unsaved;
+    return this.evaluationStatus !== buttonStatusTypes.unrecorded;
   }
 
   @action
   async saveTutorial() {
-    this.status = statusTypes.saving;
+    this.savingStatus = buttonStatusTypes.pending;
     const userTutorial = this.store.createRecord('userTutorial', { tutorial: this.tutorial });
     try {
       await userTutorial.save({ adapterOptions: { tutorialId: this.tutorial.id } });
       userTutorial.tutorial = this.tutorial;
-      this.status = statusTypes.saved;
+      this.savingStatus = buttonStatusTypes.recorded;
     } catch (e) {
-      this.status = statusTypes.unsaved;
+      this.savingStatus = buttonStatusTypes.unrecorded;
     }
   }
 
   @action
   async removeTutorial() {
-    this.status = statusTypes.saving;
+    this.savingStatus = buttonStatusTypes.pending;
     try {
       await this.tutorial.userTutorial.destroyRecord({ adapterOptions: { tutorialId: this.tutorial.id } });
-      this.status = statusTypes.unsaved;
+      this.savingStatus = buttonStatusTypes.unrecorded;
     } catch (e) {
-      this.status = statusTypes.saved;
+      this.savingStatus = buttonStatusTypes.recorded;
     }
   }
 
   @action
   async evaluateTutorial() {
-    this.evaluationStatus = statusTypes.saving;
+    this.evaluationStatus = buttonStatusTypes.pending;
     const tutorialEvaluation = this.store.createRecord('tutorialEvaluation', { tutorial: this.tutorial });
     try {
       await tutorialEvaluation.save({ adapterOptions: { tutorialId: this.tutorial.id } });
       tutorialEvaluation.tutorial = this.tutorial;
-      this.evaluationStatus = statusTypes.saved;
+      this.evaluationStatus = buttonStatusTypes.recorded;
     } catch (e) {
-      this.evaluationStatus = statusTypes.unsaved;
+      this.evaluationStatus = buttonStatusTypes.unrecorded;
     }
   }
 }
