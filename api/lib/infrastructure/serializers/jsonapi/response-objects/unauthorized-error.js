1393ba0493acbc767693df1709abe1b46cbd5965
jbuget <jbuget@gmail.com>
2020-05-13 12:54:22 +0200

    Remove useless JSONAPI module `unauthorized-error`

--- api/lib/infrastructure/serializers/jsonapi/response-objects/unauthorized-error.js
+++
@@ -1,9 +0,0 @@
-const JSONAPIError = require('jsonapi-serializer').Error;
-
-module.exports = (errorMessage) => {
-  return new JSONAPIError({
-    status: '401',
-    title: 'Unauthorized',
-    detail: errorMessage
-  });
-};
