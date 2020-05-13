6a23927b802b510b1e86f27d4941c21db2545b75
jbuget <jbuget@gmail.com>
2020-05-13 23:54:05 +0200

    Remove useless JSONAPI module `bad-request-error`

--- api/lib/infrastructure/serializers/jsonapi/response-objects/bad-request-error.js
+++
@@ -1,9 +0,0 @@
-const JSONAPIError = require('jsonapi-serializer').Error;
-
-module.exports = (errorMessage) => {
-  return new JSONAPIError({
-    status: '400',
-    title: 'Bad Request',
-    detail: errorMessage
-  });
-};
