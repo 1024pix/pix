a7d2086d27c5a2dafbca28138d22c0fe9e2bbf6f
jbuget <jbuget@gmail.com>
2020-05-13 23:42:34 +0200

    Remove useless JSONAPI module `internal-error`

--- api/lib/infrastructure/serializers/jsonapi/response-objects/index.js
+++ api/lib/infrastructure/serializers/jsonapi/response-objects/index.js
@@ -1,7 +1,6 @@
 const emptyDataResponse = require('./empty-data-response');
 const badRequest = require('./bad-request-error');
 const forbiddenError = require('./forbidden-error');
-const internalError = require('./internal-error');
 const notFoundError = require('./not-found-error');
 const unprocessableEntityError = require('./unprocessable-entity-error');
 
@@ -9,7 +8,6 @@ module.exports = {
   emptyDataResponse,
   badRequest,
   forbiddenError,
-  internalError,
   notFoundError,
   unprocessableEntityError,
 };
