fc3580ce4f3d1e3b46605bde996bc38410ebe521
ARJ <arj@octo.com>
2020-05-13 10:35:51 +0200

    update call to getCertificationProfile function

--- api/tests/unit/domain/usecases/get-user-current-certification-profile_test.js
+++ api/tests/unit/domain/usecases/get-user-current-certification-profile_test.js
@@ -4,12 +4,19 @@ const getUserCurrentCertificationProfile = require('../../../../lib/domain/useca
 describe('Unit | UseCase | get-user-current-certification-profile', () => {
 
   let userService;
+  let competenceRepository;
+  let clock;
+  const now = new Date(2020, 1, 1);
+  const competences = [{ id: 'rec1' }, { id: 'rec2' }];
 
   beforeEach(() => {
+    clock = sinon.useFakeTimers(now);
     userService = { getCertificationProfile: sinon.stub() };
+    competenceRepository = { listPixCompetencesOnly: sinon.stub().resolves(competences) };
   });
 
   afterEach(() => {
+    clock.restore();
     sinon.restore();
   });
 
@@ -17,12 +24,13 @@ describe('Unit | UseCase | get-user-current-certification-profile', () => {
     // given
     const userId = 2;
 
-    userService.getCertificationProfile.resolves('certificationProfile');
+    userService.getCertificationProfile.withArgs({ userId, limitDate: now, competences }).resolves('certificationProfile');
 
     // when
     const actualCertificationProfile = await getUserCurrentCertificationProfile({
       userId,
       userService,
+      competenceRepository
     });
 
     //then
