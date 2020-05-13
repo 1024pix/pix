1932e0472ea8e5378d2297d50a5473008d6e9375
jbuget <jbuget@gmail.com>
2020-05-13 23:56:34 +0200

    Remove useless JSONAPI module `not-found-error`

--- api/lib/infrastructure/serializers/jsonapi/response-objects/index.js
+++ api/lib/infrastructure/serializers/jsonapi/response-objects/index.js
@@ -1,9 +1,7 @@
 const emptyDataResponse = require('./empty-data-response');
-const notFoundError = require('./not-found-error');
 const unprocessableEntityError = require('./unprocessable-entity-error');
 
 module.exports = {
   emptyDataResponse,
-  notFoundError,
   unprocessableEntityError,
 };
