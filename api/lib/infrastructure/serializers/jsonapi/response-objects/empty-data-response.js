bc3fdcd11b2247ea4ddca7bdc74ad4cb8f4bc83f
jbuget <jbuget@gmail.com>
2020-05-14 00:02:20 +0200

    Remove useless JSONAPI module `empty-data-response`

--- api/lib/infrastructure/serializers/jsonapi/response-objects/empty-data-response.js
+++
@@ -1,6 +0,0 @@
-const JSONAPISerializer = require('jsonapi-serializer').Serializer;
-
-module.exports = () => {
-  const object = new JSONAPISerializer('', {});
-  return object.serialize(null);
-};
