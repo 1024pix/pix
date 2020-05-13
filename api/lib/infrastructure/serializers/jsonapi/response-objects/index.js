c57b117d80b416fbd6dfdbdf6d3d85786996a5c8
jbuget <jbuget@gmail.com>
2020-05-14 00:02:20 +0200

    Remove useless JSONAPI module `empty-data-response`

--- api/lib/infrastructure/serializers/jsonapi/response-objects/index.js
+++ api/lib/infrastructure/serializers/jsonapi/response-objects/index.js
@@ -1,7 +1,5 @@
-const emptyDataResponse = require('./empty-data-response');
 const unprocessableEntityError = require('./unprocessable-entity-error');
 
 module.exports = {
-  emptyDataResponse,
   unprocessableEntityError,
 };
