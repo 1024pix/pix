4e4c811125a89f2d2cdbba0f3a60ee4220bf094b
jbuget <jbuget@gmail.com>
2020-05-13 23:54:05 +0200

    Remove useless JSONAPI module `bad-request-error`

--- api/lib/infrastructure/serializers/jsonapi/response-objects/index.js
+++ api/lib/infrastructure/serializers/jsonapi/response-objects/index.js
@@ -1,11 +1,9 @@
 const emptyDataResponse = require('./empty-data-response');
-const badRequest = require('./bad-request-error');
 const notFoundError = require('./not-found-error');
 const unprocessableEntityError = require('./unprocessable-entity-error');
 
 module.exports = {
   emptyDataResponse,
-  badRequest,
   notFoundError,
   unprocessableEntityError,
 };
