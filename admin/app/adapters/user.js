672069c1e1b91fbc51913a6626ffbd12c65cc73f
laura-bergoens <laura.bergoens@gmail.com>
2020-05-14 16:02:49 +0200

    Admin: add condition to self assign button

--- admin/app/adapters/user.js
+++ admin/app/adapters/user.js
@@ -2,6 +2,15 @@ import ApplicationAdapter from './application';
 
 export default class UserAdapter extends ApplicationAdapter {
 
+  urlForQueryRecord(query) {
+    if (query.me) {
+      delete query.me;
+      return `${super.urlForQueryRecord(...arguments)}/me`;
+    }
+
+    return super.urlForQueryRecord(...arguments);
+  }
+
   urlForFindRecord(id) {
     return `${this.host}/${this.namespace}/admin/users/${id}`;
   }
