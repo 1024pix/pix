02a1cdb257240163cd78c2aabcbc5a1ff57d0eec
Krysthalia <ang.schall@gmail.com>
2020-05-14 16:01:10 +0200

    Admin: add test helper file with 2 methods
    createUser() to create a random user
    createAuthenticateSession() to create a session authenticated
    (with a token in order to succeed when fetching /user/me)

---
+++ admin/tests/helpers/test-init.js
@@ -0,0 +1,10 @@
+import { authenticateSession } from 'ember-simple-auth/test-support';
+
+export async function createAuthenticateSession({ userId }) {
+  return authenticateSession({
+    user_id: userId,
+    access_token: 'aaa.' + btoa(`{"user_id":${userId},"source":"pix","iat":1545321469,"exp":4702193958}`) + '.bbb',
+    expires_in: 3600,
+    token_type: 'Bearer token type',
+  });
+}
