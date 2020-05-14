caafe8440cd3c80bce7fd8f949b3ab44324cb835
Benjamin Petetot <bpetetot@gmail.com>
2020-05-14 16:35:59 +0200

    Add read model for collect profile participant

---
+++ api/lib/domain/models/CampaignProfilesCollectionParticipantSummary.js
@@ -0,0 +1,17 @@
+class CampaignProfilesCollectionParticipantSummary {
+  constructor({
+    campaignParticipationId,
+    firstName,
+    lastName,
+    participantExternalId,
+    sharedAt,
+  }) {
+    this.id = campaignParticipationId;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.participantExternalId = participantExternalId;
+    this.sharedAt = sharedAt;
+  }
+}
+
+module.exports = CampaignProfilesCollectionParticipantSummary;
---
+++ api/lib/infrastructure/repositories/campaign-profiles-collection-participant-summary-repository.js
@@ -0,0 +1,18 @@
+const { knex } = require('../bookshelf');
+const CampaignProfilesCollectionParticipantSummary = require('../../domain/models/CampaignProfilesCollectionParticipantSummary');
+
+const CampaignProfilesCollectionParticipantSummaryRepository = {
+  async listForCampaign(campaignId) {
+    const results = await knex
+      .select('"campaign-participations"."id" AS campaignParticipationId', 'users.firstName', 'users.lastName', 'campaign-participations.participantExternalId', 'campaign-participations.sharedAt')
+      .from('campaign-participations')
+      .join('users', 'users.id', 'campaign-participations.userId')
+      .join('campaigns', 'campaigns.id', 'campaign-participations.campaignId')
+      .where('campaign-participations.campaignId', '=', campaignId)
+      .orderByRaw('LOWER("lastName") ASC, LOWER("firstName") ASC');
+
+    return results.map((result) => new CampaignProfilesCollectionParticipantSummary(result));
+  }
+};
+
+module.exports = CampaignProfilesCollectionParticipantSummaryRepository;
