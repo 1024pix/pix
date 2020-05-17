d1ee8cc2f3de47a3635d07c1872c9b545e785a8c
jbuget <jbuget@gmail.com>
2020-05-17 17:51:40 +0200

    Remove import from re-integrated modules (pagination & registry)

--- api/lib/infrastructure/bookshelf.js
+++ api/lib/infrastructure/bookshelf.js
@@ -17,12 +17,15 @@ bookshelf.plugin('bookshelf-validate', {
   validator: validator
 });
 
-bookshelf.plugin('registry');
-
 // The registry plugin who adds the .model() method on bookshelf does not
 // add the table name on the instance, which is inconvenient. This decorator
 // ensures that it is accessible on the `bookshelfName` property
 bookshelf.model = ((f) => (...args) => {
+  // Keep old Bookshelf version (0.15.0) behaviour
+  // XXX see https://github.com/bookshelf/bookshelf/wiki/Migrating-from-0.15.1-to-1.0.0#default-to-require-true-on-modelfetch-and-collectionfetchone
+  if (args[1]) {
+    args[1].requireFetch = false;
+  }
   // This is like doing bookshelf.model() with any arguments it is passed
   const Ctor = f.call(bookshelf, ...args);
   // The decoration part
@@ -30,7 +33,5 @@ bookshelf.model = ((f) => (...args) => {
   // The original method returns Bookshelf constructor so we must return it
   return Ctor;
 })(bookshelf.model);
-
-bookshelf.plugin('pagination');
 
 module.exports = bookshelf;
