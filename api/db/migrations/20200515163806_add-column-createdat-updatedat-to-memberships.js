45c057f3f36311645c949a37b198a024d6c6fc81
yahya-youness <you@octo.com>
2020-05-18 16:35:11 +0200

    [API] Add audit date on memberships
    Co-authored-by: Pierre TOP - TOPI <ptop@octo.com>
    
    Content
    - add migration script for new data with current timestamp
    - add createdAt,updatedAt to Bookshelf memberships

---
+++ api/db/migrations/20200515163806_add-column-createdat-updatedat-to-memberships.js
@@ -0,0 +1,24 @@
+const TABLE_NAME = 'memberships';
+const CREATED_AT_COLUMN = 'createdAt';
+const UPDATED_AT_COLUMN = 'updatedAt';
+
+exports.up = async function(knex) {
+
+  await knex.schema.table(TABLE_NAME, (table) => {
+    table.dateTime(CREATED_AT_COLUMN);
+    table.dateTime(UPDATED_AT_COLUMN);
+  });
+  await knex.schema.alterTable(TABLE_NAME, (table) => {
+    table.dateTime(CREATED_AT_COLUMN).defaultTo(knex.fn.now()).alter();
+    table.dateTime(UPDATED_AT_COLUMN).defaultTo(knex.fn.now()).alter();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.table(TABLE_NAME, (table) => {
+    table.dropColumn(UPDATED_AT_COLUMN);
+    table.dropColumn(CREATED_AT_COLUMN);
+
+  });
+};
+
