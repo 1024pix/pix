2a89d275ecfececb22deda99e2149c9f608e42ae
Mathieu Gilet <mathieu.gilet@zenika.com>
2020-05-20 14:49:43 +0200

    Add edit button to update membership's role

--- admin/app/components/organization-members-section.js
+++ admin/app/components/organization-members-section.js
@@ -1,4 +1,5 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class OrganizationMembersSection extends Component {
 
@@ -7,22 +8,43 @@ export default class OrganizationMembersSection extends Component {
       propertyName: 'id',
       title: 'Numéro du membre',
       disableFiltering: true,
+      editable: false,
     },
     {
       propertyName: 'user.firstName',
       title: 'Prénom',
+      editable: false,
     },
     {
       propertyName: 'user.lastName',
       title: 'Nom',
+      editable: false,
     },
     {
       propertyName: 'user.email',
       title: 'Courriel',
+      editable: false,
     },
     {
       propertyName: 'displayedOrganizationRole',
       title: 'Rôle',
+      componentForEdit: 'role-edit',
+    },
+    {
+      title: 'Action',
+      component: 'editRow',
+      editable: false,
     },
   ];
+
+  @action
+  onSaveRow({ record }) {
+    return record.save();
+  }
+
+  @action
+  onCancelRow({ record }) {
+    record.rollbackAttributes();
+    return true;
+  }
 }
