0833be9120756e4ac046f0bc98aade1c9e87cce9
lisequesnel <quesnel.lise@gmail.com>
2020-05-25 16:34:47 +0200

    Fix problem with campaign participation list update

--- orga/app/adapters/campaign-profiles-collection-participation-summary.js
+++ orga/app/adapters/campaign-profiles-collection-participation-summary.js
@@ -2,9 +2,14 @@ import ApplicationAdapter from './application';
 
 export default class CampaignProfilesCollectionParticipationSummary extends ApplicationAdapter {
 
-  urlForFindAll(modelName, { adapterOptions }) {
-    const { campaignId } = adapterOptions;
-    return `${this.host}/${this.namespace}/campaigns/${campaignId}/profiles-collection/participations`;
+  urlForQuery(query) {
+    if (query.filter.campaignId) {
+      const { campaignId } = query.filter;
+      delete query.filter.campaignId;
+
+      return `${this.host}/${this.namespace}/campaigns/${campaignId}/profiles-collection/participations`;
+    }
+    return super.urlForQuery(...arguments);
   }
 
 }
