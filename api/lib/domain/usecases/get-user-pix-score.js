4ee972d9122c36dba2fdb89e2497c986d4431b63
Jonathan Perret <j-github@jonathanperret.net>
2020-05-25 12:18:57 +0200

    get-user-pix-score: compute score in JavaScript
    For consistency with all of the other places that compute a user's
    Pix score, get-user-pix-score now calls competence-repository's
    getPixScoreByCompetence which computes the sum in JavaScript.

--- api/lib/domain/usecases/get-user-pix-score.js
+++ api/lib/domain/usecases/get-user-pix-score.js
@@ -1,4 +1,7 @@
-module.exports = async function getUserPixScore({ userId, knowledgeElementRepository }) {
-  const userPixScore = await knowledgeElementRepository.getSumOfPixFromUserKnowledgeElements(userId);
+const _ = require('lodash');
+
+module.exports = async function getUserPixScore({ userId, competenceRepository }) {
+  const competenceScores = await competenceRepository.getPixScoreByCompetence({ userId });
+  const userPixScore = _.sum(_.values(competenceScores));
   return { id: userId, value: userPixScore };
 };
