380442492933ea1cec7d015b587295f0baa95ef1
Etienne GIROT <egirot@octo.com>
2020-05-26 17:57:16 +0200

    Add method to retrieve competence marks by certificationCourseId

--- api/lib/infrastructure/repositories/competence-mark-repository.js
+++ api/lib/infrastructure/repositories/competence-mark-repository.js
@@ -1,5 +1,8 @@
+const DomainTransaction = require('../DomainTransaction');
 const BookshelfCompetenceMark = require('../data/competence-mark');
 const CompetenceMark = require('../../domain/models/CompetenceMark');
+const BookshelfAssessmentResult = require('../data/assessment-result');
+const bookshelfToDomainConverter = require('../utils/bookshelf-to-domain-converter');
 
 function _toDomain(bookshelfCompetenceMark) {
   return new CompetenceMark(bookshelfCompetenceMark.attributes);
@@ -18,5 +21,21 @@ module.exports = {
       .fetchAll()
       .then((competenceMarks) => competenceMarks.models.map(_toDomain));
   },
+
+  async getLatestByCertificationCourseId({ certificationCourseId, domainTransaction = DomainTransaction.emptyTransaction() }) {
+    const mostRecentAssessmentResultId = await BookshelfAssessmentResult
+      .where({ 'assessments.certificationCourseId': certificationCourseId })
+      .query((qb) => {
+        qb.innerJoin('assessments', 'assessments.id', 'assessment-results.assessmentId');
+      })
+      .orderBy('assessment-results.createdAt', 'desc')
+      .fetch({ require: true, columns: ['assessment-results.id'], transacting: domainTransaction.knexTransaction });
+
+    const mostRecentCompetenceMarksBookshelf = await BookshelfCompetenceMark
+      .where({ assessmentResultId: mostRecentAssessmentResultId.id })
+      .fetchAll({ transacting: domainTransaction.knexTransaction });
+
+    return bookshelfToDomainConverter.buildDomainObjects(BookshelfCompetenceMark, mostRecentCompetenceMarksBookshelf);
+  }
 
 };
