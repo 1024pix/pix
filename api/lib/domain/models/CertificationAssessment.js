bb19fcee8dc789160a10588f54b717816b4d7b4d
laura-bergoens <laura.bergoens@gmail.com>
2020-05-26 12:05:03 +0200

    Validate CertificationAssessment upon construction

--- api/lib/domain/models/CertificationAssessment.js
+++ api/lib/domain/models/CertificationAssessment.js
@@ -1,6 +1,26 @@
-const _ = require('lodash');
+const Joi = require('@hapi/joi')
+  .extend(require('@hapi/joi-date'));
 const { states } = require('./Assessment');
 const { ObjectValidationError } = require('../errors');
+
+const certificationAssessmentSchema = Joi.object({
+  id: Joi.number().integer().required(),
+  userId: Joi.number().integer().required(),
+  certificationCourseId: Joi.number().integer().required(),
+  createdAt: Joi.date().required(),
+  completedAt: Joi.date().allow(null),
+  state: Joi.string().valid(states.COMPLETED, states.STARTED, states.ABORTED).required(),
+  isV2Certification: Joi.boolean().required(),
+  certificationChallenges: Joi.array().min(1).required(),
+  certificationAnswers: Joi.array().min(1).required(),
+});
+
+function validateEntity(schema, entity) {
+  const { error } = schema.validate(entity);
+  if (error) {
+    throw new ObjectValidationError(error);
+  }
+}
 
 class CertificationAssessment {
   constructor({
@@ -11,6 +31,8 @@ class CertificationAssessment {
     completedAt,
     state,
     isV2Certification,
+    certificationChallenges,
+    certificationAnswers,
   } = {}) {
     this.id = id;
     this.userId = userId;
@@ -18,12 +40,11 @@ class CertificationAssessment {
     this.createdAt = createdAt;
     this.completedAt = completedAt;
     this.state = state;
-    if (!_.keys(states).includes(this.state)) {
-      new ObjectValidationError(`CertificationAssessment : wrong state ${this.state}`);
-    }
     this.isV2Certification = isV2Certification;
-    this.certificationChallenges = [];
-    this.certificationAnswers = [];
+    this.certificationChallenges = certificationChallenges;
+    this.certificationAnswers = certificationAnswers;
+
+    validateEntity(certificationAssessmentSchema, this);
   }
 }
 
