a74e15e168232df9afd2457c6b0d94f95b9b93b9
Benjamin Petetot <bpetetot@gmail.com>
2020-05-26 17:55:28 +0200

    Add pagination in collections profile participations in pix-orga

---
+++ orga/tests/acceptance/campaign-profiles-test.js
@@ -0,0 +1,107 @@
+import { module, test } from 'qunit';
+import { visit, click, fillIn } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import { authenticateSession } from 'ember-simple-auth/test-support';
+import { createUserWithMembershipAndTermsOfServiceAccepted } from '../helpers/test-init';
+
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+
+module.only('Acceptance | Campaign Profiles', function(hooks) {
+
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  let user;
+
+  const defaultPage = '1';
+  const pageSize = 10;
+  const rowCount = 50;
+  hooks.beforeEach(async () => {
+    user = createUserWithMembershipAndTermsOfServiceAccepted();
+    await authenticateSession({
+      user_id: user.id,
+      access_token: 'aaa.' + btoa(`{"user_id":${user.id},"source":"pix","iat":1545321469,"exp":4702193958}`) + '.bbb',
+      expires_in: 3600,
+      token_type: 'Bearer token type',
+    });
+    server.create('campaign', { id: 1, type: 'PROFILES_COLLECTION' });
+    server.createList('campaign-participation', rowCount, { campaignId: 1 });
+  });
+
+  module('When user arrives on profiles page', function() {
+
+    test('it should display profile list with default settings for pagination', async function(assert) {
+      // when
+      await visit('/campagnes/1/profils');
+
+      // then
+      assert.dom('table tbody tr').exists({ count: pageSize });
+      assert.dom('.page-navigation__current-page').hasText(defaultPage);
+      assert.dom('.page-navigation__last-page').containsText('5');
+      assert.dom('.page-size option:checked').hasText(pageSize.toString());
+    });
+
+    test('it should display profile list with settings in url for pagination', async function(assert) {
+      // given
+      const changedPageSize = 25;
+      const changedPageNumber = 2;
+
+      // when
+      await visit(`/campagnes/1/profils?pageNumber=${changedPageNumber}&pageSize=${changedPageSize}`);
+
+      // then
+      assert.dom('table tbody tr').exists({ count: changedPageSize });
+      assert.dom('.page-navigation__current-page').hasText(changedPageNumber.toString());
+      assert.dom('.page-navigation__last-page').containsText('2');
+      assert.dom('.page-size option:checked').hasText(changedPageSize.toString());
+    });
+  });
+
+  module('When user is already on profiles page and changes pagination', function() {
+
+    test('it should display profile list with updated page size', async function(assert) {
+      // given
+      const changedPageSize = 25;
+
+      // when
+      await visit('/campagnes/1/profils');
+      await fillIn('.page-size .select', changedPageSize);
+
+      // then
+      assert.dom('table tbody tr').exists({ count: changedPageSize });
+      assert.dom('.page-navigation__current-page').hasText(defaultPage);
+      assert.dom('.page-navigation__last-page').containsText('2');
+      assert.dom('.page-size option:checked').hasText(changedPageSize.toString());
+    });
+
+    test('it should change profile list page when user clicks on next page', async function(assert) {
+      // given
+      await visit('/campagnes/1/profils');
+      const someElementFromPage1 = this.element.querySelector('table tbody tr:nth-child(5)').textContent;
+
+      // when
+      await click('.page-navigation__arrow--next .icon-button');
+
+      // then
+      assert.dom('.page-navigation__current-page').hasText('2');
+      assert.dom('table tbody').doesNotContainText(someElementFromPage1);
+    });
+
+    test('it should go back to first page when user changes page size', async function(assert) {
+      // given
+      const changedPageSize = 25;
+      const startPage = 3;
+
+      // when
+      await visit(`/campagnes/1/profils?pageNumber=${startPage}`);
+      await fillIn('.page-size .select', changedPageSize);
+
+      // then
+      assert.dom('table tbody tr').exists({ count: changedPageSize });
+      assert.dom('.page-navigation__current-page').hasText(defaultPage);
+      assert.dom('.page-navigation__last-page').containsText('2');
+      assert.dom('.page-size option:checked').hasText(changedPageSize.toString());
+    });
+  });
+});
+
