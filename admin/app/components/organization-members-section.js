b1cdb6ef26314431b9e151bed2e17008face8481
Mathieu Gilet <mathieu.gilet@zenika.com>
2020-05-29 14:54:46 +0200

    Reduce members table column width
    - Columns id, role and action

--- admin/app/components/organization-members-section.js
+++ admin/app/components/organization-members-section.js
@@ -6,34 +6,40 @@ export default class OrganizationMembersSection extends Component {
   columns = [
     {
       propertyName: 'id',
-      title: 'Numéro du membre',
+      title: 'ID Membre',
       disableFiltering: true,
       editable: false,
+      className: 'models-table-column--small',
     },
     {
       propertyName: 'user.firstName',
       title: 'Prénom',
       editable: false,
+      className: 'models-table-column',
     },
     {
       propertyName: 'user.lastName',
       title: 'Nom',
       editable: false,
+      className: 'models-table-column',
     },
     {
       propertyName: 'user.email',
       title: 'Courriel',
       editable: false,
+      className: 'models-table-column',
     },
     {
       propertyName: 'displayedOrganizationRole',
       title: 'Rôle',
       componentForEdit: 'role-edit',
+      className: 'models-table-column--small',
     },
     {
       title: 'Action',
       component: 'editRow',
       editable: false,
+      className: 'models-table-column--small',
     },
   ];
 
