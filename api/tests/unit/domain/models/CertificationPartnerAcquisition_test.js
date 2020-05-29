ee0fa90c5d40e8dadcd5284f0d001596eac898b8
laura-bergoens <laura.bergoens@gmail.com>
2020-05-29 06:59:46 +0200

    Consider only certifiable competences when scoring Cléa certification

--- api/tests/unit/domain/models/CertificationPartnerAcquisition_test.js
+++ api/tests/unit/domain/models/CertificationPartnerAcquisition_test.js
@@ -54,11 +54,12 @@ describe('Unit | Domain | Models | CertificationPartnerAcquisition', () => {
         const reproducibilityRate = GREY_ZONE_REPRO[0];
         const competenceId1 = 'competenceId1', competenceId2 = 'competenceId2';
 
-        it('for 70 reproducibility rate, it should obtain certification with all pixValue above 75% of Clea skills pixValue for each competence', async () => {
+        it('for 70 reproducibility rate, it should obtain certification when the pixScore for each certifiable competences is above 75% of Clea corresponding competence\'s pixScore', async () => {
           // given
           const totalPixCleaByCompetence = {
             [competenceId1]: 20,
             [competenceId2]: 10,
+            ['competenceId4']: 30,
           };
 
           const competenceMarks = [
@@ -94,11 +95,12 @@ describe('Unit | Domain | Models | CertificationPartnerAcquisition', () => {
           expect(hasAcquiredCertif).to.be.true;
         });
 
-        it('for 70 reproducibility rate, it should not obtain certification without all pixValue above 75% of Clea skills pixValue for each competence', async () => {
+        it('for 70 reproducibility rate, it should not obtain certification when the pixScore for each certifiable competences is above 75% of Clea corresponding competence\'s pixScore', async () => {
           // given
           const totalPixCleaByCompetence = {
             [competenceId1]: 20,
             [competenceId2]: 10,
+            ['competenceId4']: 30,
           };
           const competenceMarks = [
             new CompetenceMark(
