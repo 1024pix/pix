7a650d51b474c6790bf6bab9831ce8c3897dd704
lisequesnel <quesnel.lise@gmail.com>
2020-06-02 19:47:27 +0200

    Refactor tests
    Move some acceptance tests into integration
    Add an aria-label="Élève"

--- orga/tests/integration/components/routes/authenticated/students/list-items-test.js
+++ orga/tests/integration/components/routes/authenticated/students/list-items-test.js
@@ -8,20 +8,27 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | routes/authenticated/students | list-items', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it should display the header names', async function(assert) {
+  test('it should show title of team page', async function(assert) {
+    // when
+    await render(hbs`<Routes::Authenticated::Students::ListItems/>`);
+
+    // then
+    assert.contains('Élèves');
+  });
+
+  test('it should display the header labels', async function(assert) {
     // given
     this.set('students', []);
 
     // when
-    await render(hbs`{{routes/authenticated/students/list-items students=students}}`);
+    await render(hbs`<Routes::Authenticated::Students::ListItems @students={{students}}/>`);
 
     // then
-    assert.dom('.table thead tr th').exists({ count: 5 });
-    assert.dom('.table thead tr th:first-child').hasText('Nom');
-    assert.dom('.table thead tr th:nth-child(2)').hasText('Prénom');
-    assert.dom('.table thead tr th:nth-child(3)').hasText('Date de naissance');
-    assert.dom('.table thead tr th:nth-child(4)').hasText('Connecté avec');
-    assert.dom('.table thead tr th:last-child').hasText('');
+    assert.dom('th').exists({ count: 5 });
+    assert.contains('Nom');
+    assert.contains('Prénom');
+    assert.contains('Date de naissance');
+    assert.contains('Connecté avec');
   });
 
   test('it should display a list of students', async function(assert) {
@@ -34,180 +41,138 @@ module('Integration | Component | routes/authenticated/students | list-items', f
     this.set('students', students);
 
     // when
-    await render(hbs`{{routes/authenticated/students/list-items students=students}}`);
+    await render(hbs`<Routes::Authenticated::Students::ListItems @students={{students}}/>`);
 
     // then
-    assert.dom('.table tbody tr').exists();
-    assert.dom('.table tbody tr').exists({ count: 2 });
+    assert.dom('[aria-label="Élève"]').exists({ count: 2 });
   });
 
   test('it should display the firstName, lastName and birthdate of student', async function(assert) {
     // given
-    const students = [
-      { lastName: 'La Terreur', firstName: 'Gigi', birthdate: new Date('2010-02-01') },
-      { lastName: 'L\'asticot', firstName: 'Gogo', birthdate: new Date('2010-05-10') },
-    ];
+    const students = [{ lastName: 'La Terreur', firstName: 'Gigi', birthdate: new Date('2010-02-01') },];
 
     this.set('students', students);
 
     // when
-    await render(hbs`{{routes/authenticated/students/list-items students=students}}`);
+    await render(hbs`<Routes::Authenticated::Students::ListItems @students={{students}}/>`);
 
     // then
-    assert.dom('.table tbody tr:first-child td:first-child').hasText('La Terreur');
-    assert.dom('.table tbody tr:first-child td:nth-child(2)').hasText('Gigi');
-    assert.dom('.table tbody tr:first-child td:nth-child(3)').hasText('01/02/2010');
+    assert.contains('La Terreur');
+    assert.contains('Gigi');
+    assert.contains('01/02/2010');
   });
 
-  test('it should not display password update button when student have not username', async function(assert) {
-    const store = this.owner.lookup('service:store');
-
+  module('when user is not reconciled', function({ beforeEach }) {
     const storedStudents = [];
-    [
-      {
-        id: 1,
-        firstName: 'Paul',
-        lastName: 'Durant',
-        birthdate: new Date('2008-10-01'),
-      },
-      {
-        id: 2,
-        firstName: 'Jackie',
-        lastName: 'Coogan',
-        birthdate: new Date('2004-10-26'),
-        username: 'jackie.coogan2610',
-        isAuthenticatedFromGar: false,
-      },
-    ].forEach((student) => {
-      storedStudents.push(run(() => store.createRecord('student', student)));
+
+    beforeEach(function() {
+      const store = this.owner.lookup('service:store');
+      [{
+        lastName: 'La Terreur',
+        firstName: 'Gigi',
+        birthdate: '2010-01-01',
+      }].forEach((student) => {
+        storedStudents.push(run(() => store.createRecord('student', student)));
+      });
     });
 
-    this.set('students', storedStudents);
-
-    // when
-    await render(hbs`{{routes/authenticated/students/list-items students=students}}`);
-
-    // then
-    assert.dom('.table tbody tr:first-child td:last-child button .fa-cog').doesNotExist();
-    assert.dom('.table tbody tr:nth-child(2) td:last-child button .fa-cog').exists();
-  });
-
-  module('it should display the authentication method', ()=> {
-
-    test('it should display dash when not reconcilied', async function(assert) {
+    test('it should display dash for authentication method', async function(assert) {
       // given
-      const store = this.owner.lookup('service:store');
       const dash = '\u2013';
-
-      const storedStudents = [];
-      [
-        {
-          lastName: 'La Terreur',
-          firstName: 'Gigi',
-          birthdate: '2010-01-01',
-        },
-      ].forEach((student) => {
-        storedStudents.push(run(() => store.createRecord('student', student)));
-      });
-
       this.set('students', storedStudents);
 
       // when
-      await render(hbs`{{routes/authenticated/students/list-items students=students}}`);
+      await render(hbs`<Routes::Authenticated::Students::ListItems @students={{students}}/>`);
 
       // then
-      assert.dom('.table tbody tr:first-child td:first-child').hasText('La Terreur');
-      assert.dom('.table tbody tr:first-child td:nth-child(2)').hasText('Gigi');
-      assert.dom('.table tbody tr:first-child td:nth-child(3)').hasText('01/01/2010');
-      assert.dom('.table tbody tr:first-child td:nth-child(4)').hasText(dash);
+      assert.dom('[aria-label="Élève"]').containsText(dash);
     });
-    test('it should display Identifiant when identified by username', async function(assert) {
-      // given
-      const store = this.owner.lookup('service:store');
 
-      const storedStudents = [];
-      [
-        {
-          lastName: 'La Terreur',
-          firstName: 'Gigi',
-          birthdate: '2010-01-01',
-          username: 'blueivy.carter0701',
-          isAuthenticatedFromGar: false,
-        },
-      ].forEach((student) => {
-        storedStudents.push(run(() => store.createRecord('student', student)));
-      });
-
+    test('it should not display actions menu for username', async function(assert) {
       this.set('students', storedStudents);
 
       // when
-      await render(hbs`{{routes/authenticated/students/list-items students=students}}`);
+      await render(hbs`<Routes::Authenticated::Students::ListItems @students={{students}}/>`);
 
       // then
-      assert.dom('.table tbody tr:first-child td:first-child').hasText('La Terreur');
-      assert.dom('.table tbody tr:first-child td:nth-child(2)').hasText('Gigi');
-      assert.dom('.table tbody tr:first-child td:nth-child(3)').hasText('01/01/2010');
-      assert.dom('.table tbody tr:first-child td:nth-child(4)').hasText('Identifiant');
+      assert.dom('[aria-label="Afficher les actions"]').doesNotExist();
     });
-
   });
 
-  module('when student have a username', () => {
+  module('when user is reconciled with username', function({ beforeEach }) {
+    const storedStudents = [];
 
-    test('it should display the student\'s firstName, lastName and birthdate, and password update button', async function(assert) {
-      // given
+    beforeEach(function() {
       const store = this.owner.lookup('service:store');
-      const student =
-        run(() => store.createRecord('student', {
-          id: 1,
-          lastName: 'lastName',
-          firstName: 'firstName',
-          birthdate: new Date('2008-10-01'),
-          username: 'firstname.lastname0110',
-          isAuthenticatedFromGar: false,
-        }));
-
-      this.set('students', [student]);
-
-      // when
-      await render(hbs`{{routes/authenticated/students/list-items students=students}}`);
-
-      // then
-      assert.dom('.table tbody tr:first-child td:first-child').hasText('lastName');
-      assert.dom('.table tbody tr:first-child td:nth-child(2)').hasText('firstName');
-      assert.dom('.table tbody tr:first-child td:nth-child(3)').hasText('01/10/2008');
-      assert.dom('.table tbody tr:first-child td:last-child button .fa-cog').exists();
+      [{
+        lastName: 'La Terreur',
+        firstName: 'Gigi',
+        birthdate: '2010-01-01',
+        username: 'blueivy.carter0701',
+        isAuthenticatedFromGar: false,
+      }].forEach((student) => {
+        storedStudents.push(run(() => store.createRecord('student', student)));
+      });
     });
 
+    test('it should display "Identifiant" as authentication method', async function(assert) {
+      // given
+      this.set('students', storedStudents);
+
+      // when
+      await render(hbs`<Routes::Authenticated::Students::ListItems @students={{students}}/>`);
+
+      // then
+      assert.dom('[aria-label="Élève"]').containsText('Identifiant');
+    });
+
+    test('it should display actions menu', async function(assert) {
+      this.set('students', storedStudents);
+
+      // when
+      await render(hbs`<Routes::Authenticated::Students::ListItems @students={{students}}/>`);
+
+      // then
+      assert.dom('[aria-label="Afficher les actions"]').exists();
+    });
   });
 
-  module('when student have an email', () => {
+  module('when user is reconciled with email', function({ beforeEach }) {
+    const storedStudents = [];
 
-    test('it should display the student\'s firstName, lastName and birthdate, and password manage button', async function(assert) {
-      // given
+    beforeEach(function() {
       const store = this.owner.lookup('service:store');
-      const student =
-        run(() => store.createRecord('student', {
-          id: 1,
-          lastName: 'lastName',
-          firstName: 'firstName',
-          birthdate: new Date('2008-10-01'),
-          email: 'firstname.lastname@example.net',
-          isAuthenticatedFromGar: false,
-        }));
-
-      this.set('students', [student]);
-
-      // when
-      await render(hbs`{{routes/authenticated/students/list-items students=students}}`);
-
-      // then
-      assert.dom('.table tbody tr:first-child td:first-child').hasText('lastName');
-      assert.dom('.table tbody tr:first-child td:nth-child(2)').hasText('firstName');
-      assert.dom('.table tbody tr:first-child td:nth-child(3)').hasText('01/10/2008');
-      assert.dom('.table tbody tr:first-child td:last-child button .fa-cog').exists();
+      [{
+        lastName: 'La Terreur',
+        firstName: 'Gigi',
+        birthdate: '2010-01-01',
+        email: 'firstname.lastname@example.net',
+        isAuthenticatedFromGar: false,
+      }].forEach((student) => {
+        storedStudents.push(run(() => store.createRecord('student', student)));
+      });
     });
 
+    test('it should display "Adresse email" as authentication method', async function(assert) {
+      // given
+      this.set('students', storedStudents);
+
+      // when
+      await render(hbs`<Routes::Authenticated::Students::ListItems @students={{students}}/>`);
+
+      // then
+      assert.dom('[aria-label="Élève"]').containsText('Adresse e-mail');
+    });
+
+    test('it should display actions menu for email', async function(assert) {
+      this.set('students', storedStudents);
+
+      // when
+      await render(hbs`<Routes::Authenticated::Students::ListItems @students={{students}}/>`);
+
+      // then
+      assert.dom('[aria-label="Afficher les actions"]').exists();
+    });
   });
 
   module('when user is admin in organization', (hooks) => {
@@ -220,10 +185,10 @@ module('Integration | Component | routes/authenticated/students | list-items', f
 
     test('it should display import button', async function(assert) {
       // when
-      await render(hbs`{{routes/authenticated/students/list-items students=students importStudents=(action importStudentsSpy)}}`);
+      await render(hbs`<Routes::Authenticated::Students::ListItems @students={{students}}/>`);
 
       // then
-      assert.dom('.button').hasText('Importer (.xml)');
+      assert.contains('Importer (.xml)');
     });
   });
 
@@ -236,10 +201,10 @@ module('Integration | Component | routes/authenticated/students | list-items', f
       this.set('students', []);
 
       // when
-      await render(hbs`{{routes/authenticated/students/list-items students=students}}`);
+      await render(hbs`<Routes::Authenticated::Students::ListItems @students={{students}}/>`);
 
       // then
-      assert.dom('.button').doesNotExist();
+      assert.notContains('Importer (.xml)');
     });
   });
 
