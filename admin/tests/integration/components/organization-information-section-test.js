e0af42a28ea56c1bbbda695af1c5f371632679f7
Mathieu Gilet <mathieu.gilet@zenika.com>
2020-06-05 17:02:44 +0200

    [ADMIN] Add update organization action

--- admin/tests/integration/components/organization-information-section-test.js
+++ admin/tests/integration/components/organization-information-section-test.js
@@ -84,6 +84,54 @@ module('Integration | Component | organization-information-section', function(ho
       assert.dom('input#provinceCode').hasValue(organization.provinceCode);
     });
 
+    test('it should show error message if organization\'s name is empty', async function(assert) {
+      // given
+      await render(hbs`<OrganizationInformationSection @organization={{this.organization}} />`);
+
+      // when
+      await click('button[aria-label=\'Editer\'');
+      await fillIn('#name', '');
+
+      // then
+      assert.dom('div[aria-label="Message d\'erreur du champ nom"]').hasText('Le nom ne peut pas être vide');
+    });
+
+    test('it should show error message if organization\'s name is longer than 255 characters', async function(assert) {
+      // given
+      await render(hbs`<OrganizationInformationSection @organization={{this.organization}} />`);
+
+      // when
+      await click('button[aria-label=\'Editer\'');
+      await fillIn('#name', 'a'.repeat(256));
+
+      // then
+      assert.dom('div[aria-label="Message d\'erreur du champ nom"]').hasText('La longueur du nom ne doit pas excéder 255 caractères');
+    });
+
+    test('it should show error message if organization\'s external id is longer than 255 characters', async function(assert) {
+      // given
+      await render(hbs`<OrganizationInformationSection @organization={{this.organization}} />`);
+
+      // when
+      await click('button[aria-label=\'Editer\'');
+      await fillIn('#externalId', 'a'.repeat(256));
+
+      // then
+      assert.dom('div[aria-label="Message d\'erreur du champ identifiant externe"]').hasText('La longueur de l\'identifiant externe ne doit pas excéder 255 caractères');
+    });
+
+    test('it should show error message if organization\'s province code is longer than 255 characters', async function(assert) {
+      // given
+      await render(hbs`<OrganizationInformationSection @organization={{this.organization}} />`);
+
+      // when
+      await click('button[aria-label=\'Editer\'');
+      await fillIn('#provinceCode', 'a'.repeat(256));
+
+      // then
+      assert.dom('div[aria-label="Message d\'erreur du champ département"]').hasText('La longueur du département ne doit pas excéder 255 caractères');
+    });
+
     test('it should toggle display mode on click to cancel button', async function(assert) {
       // given
       await render(hbs`<OrganizationInformationSection @organization={{this.organization}} />`);
@@ -96,7 +144,7 @@ module('Integration | Component | organization-information-section', function(ho
       assert.dom('.organization__data').exists();
     });
 
-    test('it should revert form changes on click to cancel button', async function(assert) {
+    test('it should revert changes on click to cancel button', async function(assert) {
       // given
       await render(hbs`<OrganizationInformationSection @organization={{this.organization}} />`);
       await click('button[aria-label=\'Editer\'');
@@ -106,12 +154,29 @@ module('Integration | Component | organization-information-section', function(ho
 
       // when
       await click('button[aria-label=\'Annuler\'');
-      await click('button[aria-label=\'Editer\'');
 
       // then
-      assert.dom('input#name').hasValue(organization.name);
-      assert.dom('input#externalId').hasValue(organization.externalId);
-      assert.dom('input#provinceCode').hasValue(organization.provinceCode);
+      assert.dom('.organization__name').hasText(organization.name);
+      assert.dom('.organization__externalId').hasText(organization.externalId);
+      assert.dom('.organization__provinceCode').hasText(organization.provinceCode);
+    });
+
+    test('it should submit the form if there is no error', async function(assert) {
+      // given
+      this.set('onSubmit', () => {});
+      await render(hbs`<OrganizationInformationSection @organization={{this.organization}} @onSubmit={{this.onSubmit}} />`);
+      await click('button[aria-label=\'Editer\'');
+      await fillIn('input#name', 'new name');
+      await fillIn('input#externalId', 'new externalId');
+      await fillIn('input#provinceCode', '  ');
+
+      // when
+      await click('button[aria-label=\'Enregistrer\'');
+
+      // then
+      assert.dom('.organization__name').hasText('new name');
+      assert.dom('.organization__externalId').hasText('new externalId');
+      assert.dom('.organization__provinceCode').doesNotExist();
     });
   });
 
