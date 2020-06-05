27ed537de2c7bf39b88c4b555ad1e6f75baaf128
ARJ <arj@octo.com>
2020-06-05 18:39:16 +0200

    [ORGA] add mirage config for campaign-profile

---
+++ orga/mirage/factories/campaign-profile.js
@@ -0,0 +1,46 @@
+import { Factory } from 'ember-cli-mirage';
+import faker from 'faker';
+
+export default Factory.extend({
+
+  firstName() {
+    return faker.name.firstName();
+  },
+
+  lastName() {
+    return faker.name.lastName();
+  },
+
+  externalId() {
+    return faker.name.title();
+  },
+
+  createdAt() {
+    return faker.date.past();
+  },
+
+  sharedAt() {
+    return faker.date.recent();
+  },
+
+  pixScore() {
+    return 8;
+  },
+
+  certifiableCompetencesCount() {
+    return 1;
+  },
+
+  competencesCount() {
+    return 12;
+  },
+
+  isCertifiable() {
+    return true;
+  },
+
+  isShared() {
+    return true;
+  },
+});
+
