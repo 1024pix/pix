7cf8b641e85d67886983daeef3a30c305efb8c86
melanieb <melanie.boudard@gmail.com>
2020-06-15 16:43:58 +0200

    Rename findSmartPlacementAssessments usecase

--- api/lib/domain/usecases/find-campaign-assessments.js
+++ api/lib/domain/usecases/find-campaign-assessments.js
@@ -1,4 +1,4 @@
-module.exports = function findSmartPlacementAssessments({ userId, filters, assessmentRepository }) {
+module.exports = function findCampaignAssessments({ userId, filters, assessmentRepository }) {
 
   const campaignCode = filters.codeCampaign;
 
