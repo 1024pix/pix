9dda41aa496f820f6861d394a34e639ded2cd2aa
lisequesnel <quesnel.lise@gmail.com>
2020-06-16 18:01:45 +0200

    [ADMIN] Replace ModelsTable by our own table for organization memberships

--- admin/tests/integration/components/organization-members-section-test.js
+++ admin/tests/integration/components/organization-members-section-test.js
@@ -2,17 +2,27 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import EmberObject from '@ember/object';
 
 module('Integration | Component | organization-members-section', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  hooks.beforeEach(function() {
+    this.set('noop', () => {});
+    const user1 = EmberObject.create({ firstName: 'Jojo', lastName: 'La Gringue', email: 'jojo@lagringue.fr' });
+    const user2 = EmberObject.create({ firstName: 'Froufrou', lastName: 'Le froussard', email: 'froufrou@lefroussard.fr' });
+    const membership1 = EmberObject.create({ id: 1, user: user1, displayedOrganizationRole: 'Administrateur', save: () => {} });
+    const membership2 = EmberObject.create({ id: 1, user: user2, displayedOrganizationRole: 'Membre', save: () => {} });
+    const memberships = [membership1, membership2];
+    this.set('memberships', memberships);
+    memberships.meta = { rowCount: 2 };
+  });
 
-    this.set('addMembership', () => true);
-    this.set('createOrganizationInvitation', () => true);
+  test('it should display a list of members', async function(assert) {
+    // when
+    await render(hbs`<OrganizationMembersSection @memberships={{memberships}} @addMembership={{noop}} @createOrganizationInvitation={{noop}} @triggerFiltering={{noop}} @selectRoleForSearch={{noop}}/>`);
 
-    await render(hbs`{{organization-members-section addMembership=addMembership createOrganizationInvitation=createOrganizationInvitation}}`);
-
-    assert.dom('.member-list').exists();
+    // then
+    assert.dom('[aria-label="Membre"]').exists({ count: 2 });
   });
 });
