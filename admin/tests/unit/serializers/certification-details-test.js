7eb1a9b8595572ed975212d2470258987e306cca
Pierre TOP (TOPI) <ptop@octo.com>
2020-06-17 14:29:03 +0200

    Remove redundant it exists ember tests

--- admin/tests/unit/serializers/certification-details-test.js
+++ admin/tests/unit/serializers/certification-details-test.js
@@ -5,14 +5,6 @@ import { run } from '@ember/runloop';
 module('Unit | Serializer | certification details', function(hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function(assert) {
-    const store = this.owner.lookup('service:store');
-    const serializer = store.serializerFor('certification-details');
-
-    assert.ok(serializer);
-  });
-
   test('it serializes records', function(assert) {
     const store = this.owner.lookup('service:store');
     const record = run(() => store.createRecord('certification-details', {}));
