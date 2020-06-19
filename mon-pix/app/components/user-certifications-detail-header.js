69fd5ae268fb655d984bf7c312ad1c800e49ed9f
Krysthalia <ang.schall@gmail.com>
2020-06-19 11:42:12 +0200

    BSR remove useless controller

--- mon-pix/app/components/user-certifications-detail-header.js
+++
@@ -1,9 +0,0 @@
-import { classNames } from '@ember-decorators/component';
-import Component from '@ember/component';
-import classic from 'ember-classic-decorator';
-
-@classic
-@classNames('user-certifications-detail-header')
-export default class UserCertificationsDetailHeader extends Component {
-  certification = null;
-}
