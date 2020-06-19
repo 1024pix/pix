56176396fb5d74414259b2c82cf9d4e566526ad7
Vincent Hardouin <vincent.hardouin@pix.fr>
2020-06-19 14:46:11 +0200

    Allow connected user on reset-password page

--- mon-pix/tests/acceptance/reset-password-test.js
+++ mon-pix/tests/acceptance/reset-password-test.js
@@ -1,6 +1,7 @@
 import { click, currentURL, fillIn, find } from '@ember/test-helpers';
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
+import { authenticateByEmail } from '../helpers/authentication';
 import visit from '../helpers/visit';
 import { setupApplicationTest } from 'ember-mocha';
 import { setupMirage } from 'ember-cli-mirage/test-support';
@@ -41,6 +42,29 @@ describe('Acceptance | Reset Password Form', function() {
     // then
     expect(currentURL()).to.equal('/changer-mot-de-passe/brandone-reset-key');
     expect(find('.sign-form__body').textContent).to.contain('Votre mot de passe a été modifié avec succès');
+  });
 
+  it('should allow connected user to visit reset-password page', async () => {
+    // given
+    const user = server.create('user', {
+      id: 1000,
+      firstName: 'Brandone',
+      lastName: 'Martins',
+      email: 'brandone.martins@pix.com',
+      password: '1024pix!'
+    });
+
+    server.create('password-reset-demand', {
+      temporaryKey: 'brandone-reset-key',
+      email: 'brandone.martins@pix.com',
+    });
+
+    await authenticateByEmail(user);
+
+    // when
+    await visit('/changer-mot-de-passe/brandone-reset-key');
+
+    // then
+    expect(currentURL()).to.equal('/changer-mot-de-passe/brandone-reset-key');
   });
 });
