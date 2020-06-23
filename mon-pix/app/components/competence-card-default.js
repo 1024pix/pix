330c5af0b599a8b4a93a86828f4de87454125b70
melanieb <melanie.boudard@gmail.com>
2020-06-23 17:01:28 +0200

    Add action and style in improving button

--- mon-pix/app/components/competence-card-default.js
+++ mon-pix/app/components/competence-card-default.js
@@ -1,7 +1,13 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 import config from 'mon-pix/config/environment';
 
 export default class CompetenceCardDefault extends Component {
+  @service currentUser;
+  @service store;
+  @service router;
+
   get displayImproveButton() {
     return config.APP.FT_IMPROVE_COMPETENCE_EVALUATION;
   }
@@ -12,4 +18,16 @@ export default class CompetenceCardDefault extends Component {
     }
     return this.args.scorecard.level;
   }
+
+  @action
+  async improveCompetenceEvaluation() {
+    await this.store.queryRecord('competence-evaluation', {
+      improve: true,
+      userId: this.currentUser.user.id,
+      competenceId: this.args.scorecard.competenceId
+    });
+
+    this.router.transitionTo('competences.resume', this.args.scorecard.competenceId);
+  }
+
 }
