123d2e5febbd35a3d1a9f13c83f9641e751fa4da
melanieb <melanie.boudard@gmail.com>
2020-06-23 16:13:39 +0200

    Add text Retenter when there will be the button

--- mon-pix/app/components/competence-card-default.js
+++ mon-pix/app/components/competence-card-default.js
@@ -1,6 +1,11 @@
 import Component from '@glimmer/component';
+import config from 'mon-pix/config/environment';
 
 export default class CompetenceCardDefault extends Component {
+  get displayImproveButton() {
+    return config.APP.FT_IMPROVE_COMPETENCE_EVALUATION;
+  }
+
   get displayedLevel() {
     if (this.args.scorecard.isNotStarted) {
       return null;
