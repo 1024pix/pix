450e2a808465a423eef9d0f8e41dffbfcfb920ad
Mathieu Gilet <mathieu.gilet@zenika.com>
2020-06-23 14:38:29 +0200

    Move membership role update to controller

--- admin/tests/integration/components/member-item-test.js
+++ admin/tests/integration/components/member-item-test.js
@@ -4,19 +4,19 @@ import { click, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { selectChoose } from 'ember-power-select/test-support/helpers';
 import EmberObject from '@ember/object';
+import sinon from 'sinon';
 
 module('Integration | Component | member-item', function(hooks) {
   setupRenderingTest(hooks);
 
   hooks.beforeEach(function() {
     const user = EmberObject.create({ firstName: 'Jojo', lastName: 'La Gringue', email: 'jojo@lagringue.fr' });
-    const membership = EmberObject.create({ id: 1, user, displayedOrganizationRole: 'Administrateur', save: () => {} });
-    this.set('membership', membership);
+    this.membership = EmberObject.create({ id: 1, user, displayedOrganizationRole: 'Administrateur', save: () => {} });
   });
 
   test('it should display a member', async function(assert) {
     // when
-    await render(hbs`<MemberItem @membership={{membership}} />`);
+    await render(hbs`<MemberItem @membership={{this.membership}} />`);
 
     // then
     assert.contains('1');
@@ -29,18 +29,18 @@ module('Integration | Component | member-item', function(hooks) {
 
   test('it should display save and cancel button on click', async function(assert) {
     // when
-    await render(hbs`<MemberItem @membership={{membership}} />`);
-    await click('#edit-organization-role');
+    await render(hbs`<MemberItem @membership={{this.membership}} />`);
+    await click('button[aria-label="Modifier le rôle"]');
 
     // then
     assert.contains('Enregistrer');
-    assert.dom('[aria-label="Annuler"]');
+    assert.dom('button[aria-label="Annuler"]');
   });
 
   test('it should display the options when select is open', async function(assert) {
     // when
-    await render(hbs`<MemberItem @membership={{membership}} />`);
-    await click('#edit-organization-role');
+    await render(hbs`<MemberItem @membership={{this.membership}} />`);
+    await click('button[aria-label="Modifier le rôle"]');
     await click('.ember-power-select-trigger');
 
     // then
@@ -48,36 +48,35 @@ module('Integration | Component | member-item', function(hooks) {
     assert.contains('Administrateur');
   });
 
-  test('it should set selected value', async function(assert) {
-    // when
-    await render(hbs`<MemberItem @membership={{membership}} />`);
-    await click('#edit-organization-role');
-    await selectChoose('.editable-cell', 'Membre');
-
-    // then
-    assert.contains('Membre');
-  });
-
   test('it should update role on save', async function(assert) {
+    // given
+    this.updateMembership = sinon.spy();
+
     // when
-    await render(hbs`<MemberItem @membership={{membership}} />`);
-    await click('#edit-organization-role');
+    await render(hbs`<MemberItem @membership={{this.membership}} @updateMembership={{this.updateMembership}} />`);
+    await click('button[aria-label="Modifier le rôle"]');
     await selectChoose('.editable-cell', 'Membre');
-    await click('#save-organization-role');
+    await click('button[aria-label="Enregistrer"]');
 
     // then
     assert.notContains('Enregistrer');
+    assert.equal(this.membership.organizationRole, 'MEMBER');
+    assert.ok(this.updateMembership.called);
   });
 
   test('it should not update role on cancel', async function(assert) {
+    // given
+    this.updateMembership = sinon.spy();
+
     // when
-    await render(hbs`<MemberItem @membership={{membership}} />`);
-    await click('#edit-organization-role');
+    await render(hbs`<MemberItem @membership={{this.membership}} @updateMembership={{this.updateMembership}} />`);
+    await click('button[aria-label="Modifier le rôle"]');
     await selectChoose('.editable-cell', 'Membre');
-    await click('#cancel-update-organization-role');
+    await click('button[aria-label="Annuler"]');
 
     // then
     assert.contains('Administrateur');
     assert.notContains('Enregistrer');
+    assert.notOk(this.updateMembership.called);
   });
 });
