428fb25903caffba937c195c39c6133a11ac35ed
yahya-youness <you@octo.com>
2020-06-24 03:02:00 +0200

    [Api] Change update role to update membership attrs in usecase
    -Rename file to update membership attributes
    -Store the user who updated the role in updatedByUserID

--- api/lib/domain/usecases/update-membership-role.js
+++
@@ -1,5 +0,0 @@
-
-module.exports = async function updateMembershipRole({ membershipRepository, membershipId, organizationRole }) {
-  const membershipUpdated = await membershipRepository.updateRoleById({ id: membershipId, organizationRole });
-  return membershipUpdated;
-};
