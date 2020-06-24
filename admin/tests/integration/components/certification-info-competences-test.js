6b4745d69f2c7c25a278a8d66d31d71d818fe5a9
Christophe Charles <ccharles@octo.com>
2020-06-24 21:50:53 +0200

    [ADMIN] Fix hbs lint errors

--- admin/tests/integration/components/certification-info-competences-test.js
+++ admin/tests/integration/components/certification-info-competences-test.js
@@ -4,6 +4,7 @@ import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | certification-info-competences', function(hooks) {
+
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
@@ -13,53 +14,50 @@ module('Integration | Component | certification-info-competences', function(hook
   });
 
   test('it should display an entry per competence', async function(assert) {
-    // Given
+    // given
     this.set('competences', [{ index:'1.1', score:'30', level:'3' }, { index:'2.1', score:'30', level:'3' }, { index:'5.2', score:'30', level:'3' }]);
-    assert.expect(3);
 
-    // When
-    await render(hbs`{{certification-info-competences competences=competences edition=false}}`);
+    // when
+    await render(hbs`<CertificationInfoCompetences @competences={{this.competences}} @edition={{false}} />`);
 
-    // Then
+    // then
     assert.dom('.certification-info-competence-index').exists({ count:3 });
     assert.dom('.certification-info-competence-level').exists({ count:3 });
     assert.dom('.certification-info-competence-score').exists({ count:3 });
   });
 
   test('it should display competence index, score and level', async function(assert) {
-    // Given
+    // given
     this.set('competences', [{ index:'1.1', score:'30', level:'3' }]);
-    assert.expect(3);
 
-    // When
-    await render(hbs`{{certification-info-competences competences=competences edition=false}}`);
+    // when
+    await render(hbs`<CertificationInfoCompetences @competences={{this.competences}} @edition={{false}} />`);
 
-    // Then
+    // then
     assert.dom('.certification-info-competence-index').hasText('1.1');
     assert.dom('.certification-info-competence-level').hasText('Niveau : 3');
     assert.dom('.certification-info-competence-score').hasText('30 Pix');
   });
 
   test('it should display 16 entries in edition mode', async function(assert) {
-    // Given
+    // given
     this.set('competences', [{ index:'1.1', score:'30', level:'3' }, { index:'2.1', score:'30', level:'3' }, { index:'5.2', score:'30', level:'3' }]);
 
-    // When
-    await render(hbs`{{certification-info-competences competences=competences edition=true}}`);
+    // when
+    await render(hbs`<CertificationInfoCompetences @competences={{this.competences}} @edition={{true}} />`);
 
-    // Then
+    // then
     assert.dom('.certification-info-field').exists({ count:16 });
   });
 
   test('it should display competence levels and scores at the right places in edition mode', async function(assert) {
-    // Given
+    // given
     this.set('competences', [{ index:'1.1', score:'30', level:'3' }, { index:'2.1', score:'16', level:'2' }, { index:'5.2', score:'42', level:'5' }]);
-    assert.expect(6);
 
-    // When
-    await render(hbs`{{certification-info-competences competences=competences edition=true}}`);
+    // when
+    await render(hbs`<CertificationInfoCompetences @competences={{this.competences}} @edition={{true}} />`);
 
-    // Then
+    // then
     assert.dom('#certification-info-score_0').hasValue('30');
     assert.dom('#certification-info-level_0').hasValue('3');
     assert.dom('#certification-info-score_3').hasValue('16');
