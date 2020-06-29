f5bd142548e4da3aceddb66516c10c5cdffd37b9
Stephane Bedeau <sbedeau@octo.com>
2020-06-29 15:58:43 +0200

    Add acceptance tests for competence details

--- mon-pix/mirage/serializers/scorecard.js
+++ mon-pix/mirage/serializers/scorecard.js
@@ -11,6 +11,7 @@ export default ApplicationSerializer.extend({
     'pixScoreAheadOfNextLevel',
     'status',
     'remainingDaysBeforeReset',
+    'remainingDaysBeforeImproving',
   ],
   include: ['area'],
   links(record) {
