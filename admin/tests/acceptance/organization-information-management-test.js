b51287cebe55abe7483bcde1c7db5b8fd9083999
yahya-youness <you@octo.com>
2020-06-30 22:31:47 +0200

    [Ihm] add email to update organization form

--- admin/tests/acceptance/organization-information-management-test.js
+++ admin/tests/acceptance/organization-information-management-test.js
@@ -14,7 +14,7 @@ module('Acceptance | organization information management', function(hooks) {
 
   module('editing organization', function() {
 
-    test('should edit organization\'s name, externalId and provinceCode', async function(assert) {
+    test('should edit organization\'s name, externalId, provinceCode and email', async function(assert) {
       // given
       const organization = this.server.create('organization', { name: 'oldOrganizationName', externalId: 'oldOrganizationExternalId', provinceCode: 'oldProvinceCode' });
       await visit(`/organizations/${organization.id}`);
@@ -24,12 +24,16 @@ module('Acceptance | organization information management', function(hooks) {
       await fillIn('#name', 'newOrganizationName');
       await fillIn('#externalId', 'newOrganizationExternalId');
       await fillIn('#provinceCode', 'newOrganizationProvinceCode');
+      await fillIn('#email', 'sco.generic.newaccount@example.net');
+
       await click('button[aria-label="Enregistrer"]');
 
       // then
       assert.contains('newOrganizationName');
       assert.contains('newOrganizationExternalId');
       assert.contains('newOrganizationProvinceCode');
+      assert.contains('sco.generic.newaccount@example.net');
+
     });
   });
 });
