da0e555bfcb991f5316028c1dce1b0b23d94efd3
Krysthalia <ang.schall@gmail.com>
2020-07-02 12:25:16 +0200

    Fix sessions search by id

--- admin/app/controllers/authenticated/sessions/session.js
+++ admin/app/controllers/authenticated/sessions/session.js
@@ -2,6 +2,7 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import _ from 'lodash';
 
 export default class SessionController extends Controller {
   @service router;
@@ -9,8 +10,9 @@ export default class SessionController extends Controller {
   @tracked inputId;
 
   @action
-  loadSession() {
-    const sessionId = this.inputId;
+  loadSession(event) {
+    event.preventDefault();
+    const sessionId = _.trim(this.inputId);
     const routeName = this.router.currentRouteName;
     this.router.transitionTo(routeName, sessionId);
   }
