e56651ab9b763cf686d08a4d116b111c5831792b
lisequesnel <quesnel.lise@gmail.com>
2020-07-02 13:24:00 +0200

    Use either operative or validated challenges in smart-random
    choice is made uppon context :
    - competenceEvaluation : only active challenges
    - campaign : operative = active + archived

--- api/tests/unit/domain/usecases/get-next-challenge-for-campaign-assessment_test.js
+++ api/tests/unit/domain/usecases/get-next-challenge-for-campaign-assessment_test.js
@@ -101,6 +101,7 @@ describe('Unit | Domain | Use Cases | get-next-challenge-for-campaign-assessmen
         challenges,
         targetSkills: targetProfile.skills,
         knowledgeElements: recentKnowledgeElements,
+        validatedOnly: false,
       });
     });
 
