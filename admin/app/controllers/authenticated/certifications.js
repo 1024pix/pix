743d7c281689f3310b2a0a7ce2f851fe0310badc
Krysthalia <ang.schall@gmail.com>
2020-07-02 11:49:46 +0200

    Fix certifications search by id

--- admin/app/controllers/authenticated/certifications.js
+++ admin/app/controllers/authenticated/certifications.js
@@ -1,13 +1,17 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import _ from 'lodash';
 
 export default class CertificationsController extends Controller {
   @service router;
 
   @action
-  loadCertification() {
-    const certifId = this.inputId;
-    this.router.transitionTo('authenticated.certifications.certification', certifId);
+  loadCertification(event) {
+    event.preventDefault();
+    const certifId = _.trim(this.inputId);
+    const routeName = 'authenticated.certifications.certification';
+    this.router.transitionTo(routeName, certifId);
   }
 }
