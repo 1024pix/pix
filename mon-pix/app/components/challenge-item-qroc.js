d692309ba08d88fa32ba5d595b842ac912e2ed10
Guillaume Lagorce <guillaume@hey-gul.com>
2020-07-08 12:32:00 +0200

    Add event listener to handle auto reply

--- mon-pix/app/components/challenge-item-qroc.js
+++ mon-pix/app/components/challenge-item-qroc.js
@@ -11,13 +11,23 @@ class ChallengeItemQroc extends ChallengeItemGeneric {
     return this._getAnswerValue().length < 1;
   }
 
-  // FIXME refactor that
   _getAnswerValue() {
     return this.showProposal ? (document.querySelector('[data-uid="qroc-proposal-uid"]')).value : this.autoReplyAnswer;
   }
 
   _getErrorMessage() {
     return 'Jouer l\'épreuve pour valider. Sinon, passer.';
+  }
+
+  _addEventListener() {
+    this.postMessageHandler = this._receiveEmbedMessage.bind(this);
+    window.addEventListener('message', this.postMessageHandler);
+  }
+
+  _receiveEmbedMessage(event) {
+    if (typeof event.data === 'string') {
+      this.autoReplyAnswer = event.data;
+    }
   }
 
   get showProposal() {
@@ -28,6 +38,19 @@ class ChallengeItemQroc extends ChallengeItemGeneric {
   answerChanged() {
     this.set('errorMessage', null);
   }
+
+  didInsertElement() {
+    if (this.challenge.autoReply) {
+      this._addEventListener();
+    }
+  }
+
+  didDestroyElement() {
+    if (this.challenge.autoReply) {
+      window.removeEventListener('message', this.postMessageHandler);
+    }
+  }
+
 }
 
 export default ChallengeItemQroc;
