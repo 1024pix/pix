ea30d44a88eeed0d8d2ec0e809ecf6cfa8cc5057
ARJ <ajoanny@octo.com>
2020-07-13 11:21:37 +0200

    [API] wrap csv-parser dependency

---
+++ api/lib/infrastructure/serializers/csv/csv-parser.js
@@ -0,0 +1,28 @@
+const parse = require('csv-parse/lib/sync');
+
+module.exports = {
+  parse(input) {
+    const lines = parse(input, {
+      columns: true,
+      relax_column_count: true,
+      delimiter: ';',
+      trim: true,
+      bom: true,
+    });
+
+    removeEmptyColumns(lines);
+    return lines;
+  }
+};
+
+const EMPTY_COLUMN = '';
+
+function removeEmptyColumns(lines) {
+  lines.forEach((csvLine) => {
+    const columnList = Object.keys(csvLine);
+
+    columnList
+      .filter((column) => csvLine[column] === EMPTY_COLUMN)
+      .forEach((column) => delete csvLine[column]);
+  });
+}
