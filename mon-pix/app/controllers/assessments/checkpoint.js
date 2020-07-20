7fff3c512b67909e6920620ff638f473b9c54cb2
Stephane Bedeau <sbedeau@octo.com>
2020-07-20 22:24:15 +0200

    Glimmerize CheckpointController

--- mon-pix/app/controllers/assessments/checkpoint.js
+++ mon-pix/app/controllers/assessments/checkpoint.js
@@ -1,52 +1,45 @@
-/* eslint ember/require-computed-property-dependencies: 0 */
-
-import classic from 'ember-classic-decorator';
-import { action, computed } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 import Controller from '@ember/controller';
 
-const { and } = computed;
-
-@classic
 export default class CheckpointController extends Controller {
   queryParams = ['finalCheckpoint', 'newLevel', 'competenceLeveled'];
-  finalCheckpoint = false;
-  newLevel = null;
-  competenceLeveled = null;
-  isShowingModal = false;
-  answer = null;
-  challenge = null;
 
-  @and('model.showLevelup', 'newLevel')
-  showLevelup;
+  @tracked answer = null;
+  @tracked challenge = null;
+  @tracked competenceLeveled = null;
+  @tracked finalCheckpoint = false;
+  @tracked isShowingModal = false;
+  @tracked newLevel = null;
 
-  @computed('finalCheckpoint')
+  get showLevelup() {
+    return this.model.showLevelup && this.newLevel;
+  }
+
   get nextPageButtonText() {
     return this.finalCheckpoint ? 'Voir mes résultats' : 'Continuer mon parcours';
   }
 
-  @computed('finalCheckpoint', 'model.progression.completionPercentage')
   get completionPercentage() {
     return this.finalCheckpoint ? 100 : this.model.get('progression.completionPercentage');
   }
 
-  @computed('model.answersSinceLastCheckpoints')
   get shouldDisplayAnswers() {
     return !!this.model.answersSinceLastCheckpoints.length;
   }
 
-  @computed('finalCheckpoint')
   get pageTitle() {
     return this.finalCheckpoint ? 'Fin de votre évaluation' : 'Avancement de l\'évaluation';
   }
 
   @action
   openComparisonWindow(answer) {
-    this.set('answer', answer);
-    this.set('isShowingModal', true);
+    this.answer = answer;
+    this.isShowingModal = true;
   }
 
   @action
   closeComparisonWindow() {
-    this.set('isShowingModal', false);
+    this.isShowingModal = false;
   }
 }
