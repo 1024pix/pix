ea808fcb9d3e6808b5a9301bf7e7fcaa252555c9
Stephane Bedeau <sbedeau@octo.com>
2020-07-21 19:08:02 +0200

    Add translation to checkpoint page

--- mon-pix/app/controllers/assessments/checkpoint.js
+++ mon-pix/app/controllers/assessments/checkpoint.js
@@ -1,9 +1,12 @@
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
 
 export default class CheckpointController extends Controller {
   queryParams = ['finalCheckpoint', 'newLevel', 'competenceLeveled'];
+
+  @service intl;
 
   @tracked answer = null;
   @tracked challenge = null;
@@ -17,7 +20,7 @@ export default class CheckpointController extends Controller {
   }
 
   get nextPageButtonText() {
-    return this.finalCheckpoint ? 'Voir mes résultats' : 'Continuer mon parcours';
+    return this.finalCheckpoint ? this.intl.t('pages.checkpoint.actions.next-page.results') : this.intl.t('pages.checkpoint.actions.next-page.continue');
   }
 
   get completionPercentage() {
@@ -29,7 +32,7 @@ export default class CheckpointController extends Controller {
   }
 
   get pageTitle() {
-    return this.finalCheckpoint ? 'Fin de votre évaluation' : 'Avancement de l\'évaluation';
+    return this.finalCheckpoint ? this.intl.t('pages.checkpoint.title.end-of-the-assessment') : this.intl.t('pages.checkpoint.title.assessment-progress');
   }
 
   @action
