47eb039150ee12aeaff78b80ab134df1ae879844
yahya-youness <you@octo.com>
2020-07-22 17:08:18 +0200

    [Api] Add feature flipping for username generation feature

--- orga/config/environment.js
+++ orga/config/environment.js
@@ -31,7 +31,8 @@ module.exports = function(environment) {
       CAMPAIGNS_ROOT_URL: process.env.CAMPAIGNS_ROOT_URL,
       HOME_URL: process.env.HOME_URL,
       MAX_CONCURRENT_AJAX_CALLS: _getEnvironmentVariableAsNumber({ environmentVariableName: 'MAX_CONCURRENT_AJAX_CALLS', defaultValue: 8, minValue: 1 }),
-      PIX_APP_URL_WITHOUT_EXTENSION: process.env.PIX_APP_URL_WITHOUT_EXTENSION || 'https://app.pix.'
+      PIX_APP_URL_WITHOUT_EXTENSION: process.env.PIX_APP_URL_WITHOUT_EXTENSION || 'https://app.pix.',
+      IS_GENERATE_USERNAME_FEATURE_ENABLED: process.env.IS_GENERATE_USERNAME_FEATURE_ENABLED === 'true',
     },
 
     googleFonts: [
@@ -88,6 +89,7 @@ module.exports = function(environment) {
   if (environment === 'test') {
     ENV.APP.API_HOST = 'http://localhost:3000';
     ENV.APP.CAMPAIGNS_ROOT_URL = 'http://localhost:4200/campagnes/';
+    ENV.APP.IS_GENERATE_USERNAME_FEATURE_ENABLED = true;
 
     // Testem prefers this...
     ENV.locationType = 'none';
