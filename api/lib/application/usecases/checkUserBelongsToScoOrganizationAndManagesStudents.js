c731d1c4310cc9ec704c462a2b760cd18265af64
yahya-youness <you@octo.com>
2020-07-22 08:57:39 +0200

    [Api] BSR remove magic string SCO

--- api/lib/application/usecases/checkUserBelongsToScoOrganizationAndManagesStudents.js
+++ api/lib/application/usecases/checkUserBelongsToScoOrganizationAndManagesStudents.js
@@ -1,11 +1,12 @@
 const membershipRepository = require('../../infrastructure/repositories/membership-repository');
+const Organisation = require('../../domain/models/Organization')
 
 module.exports = {
 
   execute(userId, organizationId) {
     return membershipRepository.findByUserIdAndOrganizationId({ userId, organizationId, includeOrganization: true })
       .then((memberships) => memberships.reduce((belongsToScoOrganization, membership) => {
-        return belongsToScoOrganization || (membership.organization.isManagingStudents && membership.organization.type === 'SCO');
+        return belongsToScoOrganization || (membership.organization.isManagingStudents && Organisation.types.SCO === membership.organization.type);
       }, false));
   }
 };
