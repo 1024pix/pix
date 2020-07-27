61b44ff2a86e8ae22fe9803636d3530db63abb29
Jonathan Perret <j-github@jonathanperret.net>
2020-07-27 20:16:39 +0200

    Revert "Refactor read title from variable"
    This reverts commit cf78e4a8ed192d74d7b3a5df0d1be3673e221fc5.
    
    It turns out localizing page titles is more easily done in the
    template rather than in a controller.
    
    Controllers that existed only to provide a pageTitle property
    are removed. `controllers/certifications/start.js` and
    `controllers/profile.js` also provided `currentUser` but
    it was no longer used in their templates so the controllers
    were removed as well.

--- mon-pix/app/controllers/campaigns/assessment/tutorial.js
+++
@@ -1,7 +0,0 @@
-import classic from 'ember-classic-decorator';
-import Controller from '@ember/controller';
-
-@classic
-export default class TutorialController extends Controller {
-  pageTitle = 'Didacticiel';
-}
--- mon-pix/app/controllers/competences/details.js
+++
@@ -1,8 +0,0 @@
-import Controller from '@ember/controller';
-import { inject as service } from '@ember/service';
-
-export default class DetailsController extends Controller {
-  @service intl;
-
-  pageTitle = this.intl.t('pages.competence-details.title');
-}
