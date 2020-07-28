eb4e29feabd5292d9382bf7006a6932c73866197
laura-bergoens <laura.bergoens@gmail.com>
2020-07-28 12:00:50 +0200

    Start SQL script that will insert massive data
    Within a transaction, we UNLOGGED then LOGGED all the tables.
    Data written in an unlogged table will not be written in the WAL.
    That way, we will gain a lot of time writing massive data into the
    tables.

---
+++ high-level-tests/load-testing/data/generate_mass_data.sql
@@ -0,0 +1,83 @@
+BEGIN;
+
+-----------------------------------------------------------------------------------------------------
+--				Empêcher la journalisation des tables   ---------------------------------------------
+-----------------------------------------------------------------------------------------------------
+ALTER TABLE "knowledge-elements" SET UNLOGGED;
+ALTER TABLE "competence-marks" SET UNLOGGED;
+ALTER TABLE "feedbacks" SET UNLOGGED;
+ALTER TABLE "answers" SET UNLOGGED;
+ALTER TABLE "assessment-results" SET UNLOGGED;
+ALTER TABLE "competence-evaluations" SET UNLOGGED;
+ALTER TABLE "assessments" SET UNLOGGED;
+ALTER TABLE "certification-challenges" SET UNLOGGED;
+ALTER TABLE "badge-partner-competences" SET UNLOGGED;
+ALTER TABLE "partner-certifications" SET UNLOGGED;
+ALTER TABLE "campaign-participations" SET UNLOGGED;
+ALTER TABLE "badge-acquisitions" SET UNLOGGED;
+ALTER TABLE "badge-criteria" SET UNLOGGED;
+ALTER TABLE "target-profile-shares" SET UNLOGGED;
+ALTER TABLE "badges" SET UNLOGGED;
+ALTER TABLE "campaigns" SET UNLOGGED;
+ALTER TABLE "certification-candidates" SET UNLOGGED;
+ALTER TABLE "certification-courses" SET UNLOGGED;
+ALTER TABLE "target-profiles_skills" SET UNLOGGED;
+ALTER TABLE "stages" SET UNLOGGED;
+ALTER TABLE "users_pix_roles" SET UNLOGGED;
+ALTER TABLE "certification-center-memberships" SET UNLOGGED;
+ALTER TABLE "memberships" SET UNLOGGED;
+ALTER TABLE "organization-invitations" SET UNLOGGED;
+ALTER TABLE "schooling-registrations" SET UNLOGGED;
+ALTER TABLE "sessions" SET UNLOGGED;
+ALTER TABLE "target-profiles" SET UNLOGGED;
+ALTER TABLE "user-orga-settings" SET UNLOGGED;
+ALTER TABLE "certification-centers" SET UNLOGGED;
+ALTER TABLE "user_tutorials" SET UNLOGGED;
+ALTER TABLE "users" SET UNLOGGED;
+ALTER TABLE "tutorial-evaluations" SET UNLOGGED;
+ALTER TABLE "organizations" SET UNLOGGED;
+
+
+-----------------------------------------------------------------------------------------------------
+--				Rétablir la journalisation des tables   ---------------------------------------------
+-----------------------------------------------------------------------------------------------------
+ALTER TABLE "organizations" SET LOGGED;
+ALTER TABLE "tutorial-evaluations" SET LOGGED;
+ALTER TABLE "users" SET LOGGED;
+ALTER TABLE "user_tutorials" SET LOGGED;
+ALTER TABLE "certification-centers" SET LOGGED;
+ALTER TABLE "user-orga-settings" SET LOGGED;
+ALTER TABLE "target-profiles" SET LOGGED;
+ALTER TABLE "sessions" SET LOGGED;
+ALTER TABLE "schooling-registrations" SET LOGGED;
+ALTER TABLE "organization-invitations" SET LOGGED;
+ALTER TABLE "memberships" SET LOGGED;
+ALTER TABLE "certification-center-memberships" SET LOGGED;
+ALTER TABLE "users_pix_roles" SET LOGGED;
+ALTER TABLE "stages" SET LOGGED;
+ALTER TABLE "target-profiles_skills" SET LOGGED;
+ALTER TABLE "certification-courses" SET LOGGED;
+ALTER TABLE "certification-candidates" SET LOGGED;
+ALTER TABLE "campaigns" SET LOGGED;
+ALTER TABLE "badges" SET LOGGED;
+ALTER TABLE "target-profile-shares" SET LOGGED;
+ALTER TABLE "badge-criteria" SET LOGGED;
+ALTER TABLE "badge-acquisitions" SET LOGGED;
+ALTER TABLE "campaign-participations" SET LOGGED;
+ALTER TABLE "partner-certifications" SET LOGGED;
+ALTER TABLE "badge-partner-competences" SET LOGGED;
+ALTER TABLE "certification-challenges" SET LOGGED;
+ALTER TABLE "assessments" SET LOGGED;
+ALTER TABLE "competence-evaluations" SET LOGGED;
+ALTER TABLE "assessment-results" SET LOGGED;
+ALTER TABLE "answers" SET LOGGED;
+ALTER TABLE "feedbacks" SET LOGGED;
+ALTER TABLE "competence-marks" SET LOGGED;
+ALTER TABLE "knowledge-elements" SET LOGGED;
+
+
+
+ROLLBACK;
+
+
+
