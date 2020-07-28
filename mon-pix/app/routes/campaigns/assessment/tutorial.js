d1de3dc95dc2712bf9c2e62e36c6a269b0200296
Alexandre COIN <alexandre.pc.coin@gmail.com>
2020-07-28 10:17:58 +0200

    Removed unnecessary file and improved trad

--- mon-pix/app/routes/campaigns/assessment/tutorial.js
+++ mon-pix/app/routes/campaigns/assessment/tutorial.js
@@ -3,7 +3,6 @@
 
 import Route from '@ember/routing/route';
 import SecuredRouteMixin from 'mon-pix/mixins/secured-route-mixin';
-import campaignTutorial from 'mon-pix/static-data/campaign-tutorial';
 import { inject as service } from '@ember/service';
 
 export default Route.extend(SecuredRouteMixin, {
@@ -13,7 +12,7 @@ export default Route.extend(SecuredRouteMixin, {
 
   campaignCode: null,
   tutorialPageId: 0,
-  tutorial: campaignTutorial.tutorial,
+  tutorialPageCount: 4,
 
   _setupPaging(numberOfPages, currentTutorialPageId) {
     const classOfTutorialPages = new Array(numberOfPages);
@@ -23,13 +22,13 @@ export default Route.extend(SecuredRouteMixin, {
 
   model() {
     this.campaignCode = this.paramsFor('campaigns').code;
-    const maxTutorialPageId = this.tutorial.length - 1;
+    const maxTutorialPageId = this.tutorialPageCount - 1;
     return {
       title: this.intl.t(`pages.tutorial.pages.page${this.tutorialPageId}.title`),
       icon: this.intl.t(`pages.tutorial.pages.page${this.tutorialPageId}.icon`),
       explanation: this.intl.t(`pages.tutorial.pages.page${this.tutorialPageId}.explanation`),
       showNextButton: this.tutorialPageId < maxTutorialPageId,
-      paging: this._setupPaging(this.tutorial.length, this.tutorialPageId)
+      paging: this._setupPaging(this.tutorialPageCount, this.tutorialPageId)
     };
   },
 
@@ -47,7 +46,7 @@ export default Route.extend(SecuredRouteMixin, {
 
     next() {
       const nextTutorialPageId = this.tutorialPageId + 1;
-      if (nextTutorialPageId < this.tutorial.length) {
+      if (nextTutorialPageId < this.tutorialPageCount) {
         this.tutorialPageId = nextTutorialPageId;
         this.refresh();
       }
