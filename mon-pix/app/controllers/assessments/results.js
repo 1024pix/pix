b9be7fbee6db622c0963fba76d9752262798ed44
melanieb <melanie.boudard@gmail.com>
2020-07-28 15:41:11 +0200

    Put title with translation in hbs instead of controller

--- mon-pix/app/controllers/assessments/results.js
+++ mon-pix/app/controllers/assessments/results.js
@@ -1,13 +1,8 @@
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
 
 export default class ResultsController extends Controller {
-  @service intl;
-
-  pageTitle = this.intl.t('pages.assessment-results.title');
-
   @tracked isShowingModal = false;
   @tracked answer = null;
 
