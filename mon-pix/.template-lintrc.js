a306ad035133dd8ad79a2f72305273c995266c3e
Stephane Bedeau <sbedeau@octo.com>
2020-07-29 18:58:17 +0200

    Add exceptions to currently broken rules

--- mon-pix/.template-lintrc.js
+++ mon-pix/.template-lintrc.js
@@ -1,5 +1,21 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended'
+  extends: 'recommended',
+
+  rules: {
+    'link-href-attributes': false,
+    'link-rel-noopener': false,
+    'no-html-comments': false,
+    'no-inline-styles': false,
+    'no-invalid-interactive': false,
+    'no-negated-condition': false,
+    'no-nested-interactive': false,
+    'no-quoteless-attributes': false,
+    'no-triple-curlies': false,
+    'no-unused-block-params': false,
+    'require-button-type': false,
+    'require-valid-alt-text': false,
+    'style-concatenation': false,
+  }
 };
