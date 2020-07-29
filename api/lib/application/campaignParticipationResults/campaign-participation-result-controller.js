d3ffbf396f5c49e173fc114cc76a7ecc580df291
laura-bergoens <laura.bergoens@gmail.com>
2020-07-29 11:11:38 +0200

    Translate competences name when fetching participation result

--- api/lib/application/campaignParticipationResults/campaign-participation-result-controller.js
+++ api/lib/application/campaignParticipationResults/campaign-participation-result-controller.js
@@ -1,12 +1,14 @@
 const usecases = require('../../domain/usecases');
 const serializer = require('../../infrastructure/serializers/jsonapi/campaign-participation-result-serializer');
+const { extractLocaleFromRequest } = require('../../infrastructure/utils/request-response-utils');
 
 module.exports = {
   async get(request) {
+    const locale = extractLocaleFromRequest(request);
     const campaignParticipationId = parseInt(request.params.id);
     const userId = request.auth.credentials.userId;
 
-    const report = await usecases.getCampaignParticipationResult({ campaignParticipationId, userId });
+    const report = await usecases.getCampaignParticipationResult({ campaignParticipationId, userId, locale });
 
     return serializer.serialize(report);
   },
