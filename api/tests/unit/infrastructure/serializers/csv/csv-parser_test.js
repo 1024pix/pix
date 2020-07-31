a9e19868a2997e40a5c4f2cb26867ce7cdc6faf2
laura-bergoens <laura.bergoens@gmail.com>
2020-07-31 11:46:56 +0200

    Allow , as acceptable separator for csv import

--- api/tests/unit/infrastructure/serializers/csv/csv-parser_test.js
+++ api/tests/unit/infrastructure/serializers/csv/csv-parser_test.js
@@ -6,16 +6,16 @@ describe('csv-parser', () => {
     it('returns an array with csv lines parsed', () => {
       const input = `
       column1;column2;column3
-      1;2;3
+      1;2,3
       ;;4
-      "5";;"6"
+      "5";;"6,7"
       7`;
       const lines = parse(input);
 
       expect(lines).to.deep.equal([
         { 'column1': '1', 'column2': '2', 'column3': '3' },
         { 'column3': '4' },
-        { 'column1': '5', 'column3': '6' },
+        { 'column1': '5', 'column3': '6,7' },
         { 'column1': '7' },
       ]);
     });
