111690a694cd08a0ceed4fd55afad204c36d7668
Mathieu Gilet <mathieu.gilet@zenika.com>
2020-08-05 15:21:50 +0200

    [ORGA] Remove student-dependent-users model

--- orga/app/models/student-dependent-user.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-const { Model, attr } = DS;
-
-export default class StudentDependantUser extends Model {
-  @attr('number') organizationId;
-  @attr('number') studentId;
-  @attr('string') password;
-}
