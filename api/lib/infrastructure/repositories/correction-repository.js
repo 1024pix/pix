eb8b69584ebd85ec2f41d14cf26cb45f21e8330c
Alexandre COIN <alexandre.pc.coin@gmail.com>
2020-08-06 11:56:24 +0200

    Add locale to hint retrieval for corrections

--- api/lib/infrastructure/repositories/correction-repository.js
+++ api/lib/infrastructure/repositories/correction-repository.js
@@ -6,13 +6,14 @@ const challengeDatasource = require('../datasources/airtable/challenge-datasourc
 const skillDatasource = require('../datasources/airtable/skill-datasource');
 const tutorialRepository = require('./tutorial-repository');
 const VALIDATED_HINT_STATUSES = ['Validé', 'pré-validé'];
+const { ENGLISH_SPOKEN } = require('../../domain/constants').LOCALE;
 
 module.exports = {
 
   async getByChallengeId({ challengeId, userId, locale }) {
     const challenge = await challengeDatasource.get(challengeId);
     const skills = await _getSkills(challenge);
-    const hints = await _getHints(skills);
+    const hints = await _getHints(skills, locale);
 
     const tutorials = await _getTutorials({ userId, skills, tutorialIdsProperty: 'tutorialIds', locale });
     const learningMoreTutorials = await _getTutorials({ userId, skills, tutorialIdsProperty: 'learningMoreTutorialIds', locale });
@@ -27,9 +28,9 @@ module.exports = {
   }
 };
 
-async function _getHints(skills) {
+async function _getHints(skills, locale) {
   const skillsWithHints = await _filterSkillsWithValidatedHint(skills);
-  return _convertSkillsToHints(skillsWithHints);
+  return _convertSkillsToHints(skillsWithHints, locale);
 }
 
 function _getSkills(challengeDataObject) {
@@ -41,11 +42,19 @@ function _filterSkillsWithValidatedHint(skillDataObjects) {
   return skillDataObjects.filter((skillDataObject) => VALIDATED_HINT_STATUSES.includes(skillDataObject.hintStatus));
 }
 
-function _convertSkillsToHints(skillDataObjects) {
+function _getTranslatedText(locale, translations = { frenchText: '', englishText: '' }) {
+  if (locale === ENGLISH_SPOKEN) {
+    return translations.englishText;
+  }
+
+  return translations.frenchText;
+}
+
+function _convertSkillsToHints(skillDataObjects, locale) {
   return skillDataObjects.map((skillDataObject) => {
     return new Hint({
       skillName: skillDataObject.name,
-      value: skillDataObject.hint
+      value: _getTranslatedText(locale, { frenchText: skillDataObject.hintFrFr, englishText: skillDataObject.hintEnUs }),
     });
   });
 }
