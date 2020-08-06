7dbd23584ed59ce42c59753842f68a7b50bb6c6c
lisequesnel <quesnel.lise@gmail.com>
2020-08-06 17:32:16 +0200

    Display schooling-registrations infos in profile collection details
    firstName & lastName

--- api/lib/infrastructure/repositories/campaign-profile-repository.js
+++ api/lib/infrastructure/repositories/campaign-profile-repository.js
@@ -1,7 +1,7 @@
 const CampaignProfile = require('../../../lib/domain/read-models/CampaignProfile');
 const placementProfileService = require('../../domain/services/placement-profile-service');
 const { NotFoundError } = require('../../../lib/domain/errors');
-const Bookshelf = require('../bookshelf');
+const { knex } = require('../bookshelf');
 
 module.exports = {
   async findProfile(campaignId, campaignParticipationId) {
@@ -17,12 +17,12 @@ module.exports = {
 
 async function _fetchCampaignProfileAttributesFromCampaignParticipation(campaignId, campaignParticipationId) {
 
-  const [profile] = await Bookshelf.knex.with('campaignProfile',
+  const [profile] = await knex.with('campaignProfile',
     (qb) => {
       qb.select([
         'users.id AS userId',
-        'users.firstName',
-        'users.lastName',
+        knex.raw('COALESCE ("schooling-registrations"."firstName", "users"."firstName") AS "firstName"'),
+        knex.raw('COALESCE ("schooling-registrations"."lastName", "users"."lastName") AS "lastName"'),
         'campaign-participations.id AS campaignParticipationId',
         'campaign-participations.campaignId',
         'campaign-participations.createdAt',
@@ -32,6 +32,11 @@ async function _fetchCampaignProfileAttributesFromCampaignParticipation(campaign
       ])
         .from('campaign-participations')
         .join('users', 'campaign-participations.userId', 'users.id')
+        .leftJoin('schooling-registrations', 'campaign-participations.userId', 'schooling-registrations.userId')
+        .leftJoin('campaigns', function() {
+          this.on({ 'campaign-participations.campaignId': 'campaigns.id' })
+            .andOn({ 'campaigns.organizationId': 'schooling-registrations.organizationId' });
+        })
         .where({
           campaignId,
           'campaign-participations.id': campaignParticipationId
