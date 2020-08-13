6e6061e91f2d2198029e6f1151b73ac13f2b35f2
Guillaume Lagorce <guillaume@hey-gul.com>
2020-08-13 11:42:46 +0200

    Add unit tests for Reached-Stage component

--- mon-pix/app/components/reached-stage.js
+++ mon-pix/app/components/reached-stage.js
@@ -4,12 +4,10 @@ import range from 'lodash/range';
 export default class ReachedStage extends Component {
 
   get acquiredStars() {
-    // returns an array of [1...N] where N is the number of acquired stars
     return range(1, this.args.starCount + 1);
   }
 
   get unacquiredStars() {
-    // returns an array of [N+1...M] where N is the number of acquired stars and M the total number of stages (stars)
     return range(this.args.starCount + 1, this.args.stageCount + 1);
   }
 }
