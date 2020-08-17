9590a5b36bb5535e99ba69743d45f584fd3228e1
melanieb <melanie.boudard@gmail.com>
2020-08-17 16:40:14 +0200

    Add loader in Improving button in Competence card

--- mon-pix/app/components/competence-card-default.js
+++ mon-pix/app/components/competence-card-default.js
@@ -1,13 +1,16 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import config from 'mon-pix/config/environment';
+import buttonStatusTypes from 'mon-pix/utils/button-status-types';
 
 export default class CompetenceCardDefault extends Component {
   @service currentUser;
   @service store;
   @service router;
   @service competenceEvaluation;
+  @tracked improveButtonStatus = buttonStatusTypes.unrecorded;
 
   get displayImproveButton() {
     return config.APP.FT_IMPROVE_COMPETENCE_EVALUATION;
@@ -24,13 +27,22 @@ export default class CompetenceCardDefault extends Component {
     return this.args.scorecard.remainingDaysBeforeImproving > 0;
   }
 
+  get isImprovingButtonDisabled() {
+    return this.improveButtonStatus === buttonStatusTypes.pending;
+  }
+
   @action
   async improveCompetenceEvaluation() {
+    this.improveButtonStatus = buttonStatusTypes.pending;
+
     const userId = this.currentUser.user.id;
     const competenceId = this.args.scorecard.competenceId;
     const scorecardId = this.args.scorecard.id;
-
-    this.competenceEvaluation.improve({ userId, competenceId, scorecardId });
+    try {
+      this.competenceEvaluation.improve({ userId, competenceId, scorecardId });
+    } catch {
+      this.improveButtonStatus = buttonStatusTypes.unrecorded;
+    }
   }
 
 }
