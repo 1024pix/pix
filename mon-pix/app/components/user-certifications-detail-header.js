3b2dd1307b2ea58b9d61dcdf55f251761df43941
Krysthalia <ang.schall@gmail.com>
2020-08-24 11:24:50 +0200

    [Mon-Pix] Show verificationCode on certificate

--- mon-pix/app/components/user-certifications-detail-header.js
+++ mon-pix/app/components/user-certifications-detail-header.js
@@ -1,9 +1,19 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class UserCertificationsDetailHeader extends Component {
   @service intl;
+
+  @tracked tooltipText = 'Copier le code';
+
   get birthdate() {
     return this.intl.formatDate(this.args.certification.birthdate, { format: 'LL' });
+  }
+
+  @action
+  clipboardSuccess() {
+    this.tooltipText = 'Code copié !';
   }
 }
