1452c0c8ee1f1db989599509a76feafd4c387db5
laura-bergoens <laura.bergoens@gmail.com>
2020-08-25 11:59:01 +0200

    Add repository method to fetch CampaignParticipationInfo

---
+++ api/lib/infrastructure/repositories/campaign-participation-info-repository.js
@@ -0,0 +1,42 @@
+const { knex } = require('../bookshelf');
+const Assessment = require('../../domain/models/Assessment');
+const CampaignParticipationInfo = require('../../domain/read-models/CampaignParticipationInfo');
+
+module.exports = {
+  async findByCampaignId(campaignId) {
+    const results = await knex.with('campaignParticipationWithUserAndRankedAssessment',
+      (qb) => {
+        qb.select([
+          'campaign-participations.*',
+          'assessments.state',
+          _assessmentRankByCreationDate(),
+          'users.firstName',
+          'users.lastName',
+        ])
+          .from('campaign-participations')
+          .leftJoin('users', 'campaign-participations.userId', 'users.id')
+          .leftJoin('assessments', 'campaign-participations.id', 'assessments.campaignParticipationId')
+          .where({ campaignId: campaignId });
+      })
+      .from('campaignParticipationWithUserAndRankedAssessment')
+      .where({ rank: 1 });
+
+    return results.map(_rowToCampaignParticipationInfo);
+  },
+};
+
+function _assessmentRankByCreationDate() {
+  return knex.raw('ROW_NUMBER() OVER (PARTITION BY ?? ORDER BY ?? DESC) AS rank', ['assessments.campaignParticipationId', 'assessments.createdAt']);
+}
+
+function _rowToCampaignParticipationInfo(row) {
+  return new CampaignParticipationInfo({
+    participantFirstName: row.firstName,
+    participantLastName: row.lastName,
+    participantExternalId: row.participantExternalId,
+    userId: row.userId,
+    isCompleted: row.state === Assessment.states.COMPLETED,
+    createdAt: new Date(row.createdAt),
+    sharedAt: row.sharedAt ? new Date(row.sharedAt) : null,
+  });
+}
