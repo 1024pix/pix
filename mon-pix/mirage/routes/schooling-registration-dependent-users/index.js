2a872d968bcb0dea901e133de15fd7a632598327
Mathieu Gilet <mathieu.gilet@zenika.com>
2020-08-25 10:22:13 +0200

    Update campaign acceptance test

--- mon-pix/mirage/routes/schooling-registration-dependent-users/index.js
+++ mon-pix/mirage/routes/schooling-registration-dependent-users/index.js
@@ -1,3 +1,5 @@
+import { decodeToken } from 'mon-pix/helpers/jwt';
+
 export default function index(config) {
   config.post('/schooling-registration-dependent-users', (schema, request) => {
     const params = JSON.parse(request.requestBody);
@@ -19,5 +21,33 @@ export default function index(config) {
     student.update({ userId: user.id, organizationId });
     schema.schoolingRegistrationUserAssociations.create({ campaignCode });
     return user;
+  });
+
+  config.post('/schooling-registration-dependent-users/external-user-token', (schema, request) => {
+    const params = JSON.parse(request.requestBody);
+
+    const campaignCode = params.data.attributes['campaign-code'];
+    const organizationId = schema.campaigns.findBy({ code: campaignCode }).organizationId;
+
+    const externalUserToken = params.data.attributes['external-user-token'];
+    const decodedExternalUserToken = decodeToken(externalUserToken);
+
+    const firstName = decodedExternalUserToken['first_name'];
+    const lastName = decodedExternalUserToken['last_name'];
+    const samlId = decodedExternalUserToken['saml_id'];
+
+    const newUser = {
+      firstName,
+      lastName,
+      samlId,
+      password: '',
+      cgu: false,
+    };
+
+    const student = schema.students.findBy({ firstName, lastName });
+    const user = schema.users.create(newUser);
+    student.update({ userId: user.id, organizationId });
+
+    return schema.externalUsers.create({ accessToken: 'aaa.' + btoa(`{"user_id":${user.id},"source":"external","iat":1545321469,"exp":4702193958}`) + '.bbb' });
   });
 }
