b5fa3a635c1a421fc0e856a14ddbaaead19661d0
ajoanny <arj@octo.com>
2020-08-26 14:09:42 +0200

    refacto CampaignParticipationInfoRepository test

--- api/tests/integration/infrastructure/repositories/campaign-participation-info-repository_test.js
+++ api/tests/integration/infrastructure/repositories/campaign-participation-info-repository_test.js
@@ -6,126 +6,49 @@ describe('Integration | Repository | Campaign Participation Info', () => {
 
   describe('#findByCampaignId', () => {
 
-    let campaign1;
-    let campaign2;
-    let campaignParticipation1;
-    let userId;
-
-    beforeEach(async () => {
-      userId = databaseBuilder.factory.buildUser({
-        firstName: 'First',
-        lastName: 'Last',
-      }).id;
-      campaign1 = databaseBuilder.factory.buildCampaign({ type: Campaign.types.ASSESSMENT });
-      campaign2 = databaseBuilder.factory.buildCampaign({ type: Campaign.types.ASSESSMENT });
-
-      campaignParticipation1 = databaseBuilder.factory.buildCampaignParticipation({
-        campaignId: campaign1.id,
-        userId,
-        isShared: true,
-      });
-      databaseBuilder.factory.buildAssessment({ campaignParticipationId: campaignParticipation1.id, userId, state: 'started', createdAt: new Date('2019-12-31'),
-      });
-      databaseBuilder.factory.buildCampaignParticipation({
-        campaignId: campaign2.id,
-        isShared: true,
-      });
-      await databaseBuilder.commit();
-    });
-
-    it('should return all the campaign-participation links to the given campaign', async () => {
-      databaseBuilder.factory.buildAssessment({
-        campaignParticipationId: campaignParticipation1.id,
-        createdAt: new Date('2020-01-02'),
-        state: 'started',
-      });
-
-      await databaseBuilder.commit();
-
-      // given
-      const campaignId = campaign1.id;
-
-      // when
-      const campaignParticipationInfos = await campaignParticipationInfoRepository.findByCampaignId(campaignId);
-
-      // then
-      expect(campaignParticipationInfos).to.deep.equal([
-        {
-          sharedAt: campaignParticipation1.sharedAt,
-          createdAt: campaignParticipation1.createdAt,
-          participantExternalId: campaignParticipation1.participantExternalId,
-          userId: campaignParticipation1.userId,
-          isCompleted: false,
-          participantFirstName: 'First',
-          participantLastName: 'Last',
-          studentNumber: null,
-        }
-      ]);
-      expect(campaignParticipationInfos[0].isShared).to.be.true;
-    });
-
-    context('When last assessment is completed', () => {
+    context('when there are several campaign', () => {
+      let campaignParticipation1;
+      let campaign1;
       beforeEach(async () => {
-        databaseBuilder.factory.buildAssessment({
-          campaignParticipationId: campaignParticipation1.id,
-          createdAt: new Date('2020-01-02'),
-          state: 'completed',
-          userId
+        const userId = databaseBuilder.factory.buildUser({
+          firstName: 'First',
+          lastName: 'Last',
+        }).id;
+
+        campaign1 = databaseBuilder.factory.buildCampaign({ type: Campaign.types.ASSESSMENT });
+
+        campaignParticipation1 = databaseBuilder.factory.buildCampaignParticipation({
+          campaignId: campaign1.id,
+          userId,
+          isShared: true,
         });
+
         databaseBuilder.factory.buildAssessment({
           campaignParticipationId: campaignParticipation1.id,
+          userId, state: 'started',
           createdAt: new Date('2020-01-01'),
-          state: 'started',
-          userId
         });
+
+        const campaign2 = databaseBuilder.factory.buildCampaign({ type: Campaign.types.ASSESSMENT });
+
+        const campaignParticipation2 = databaseBuilder.factory.buildCampaignParticipation({
+          campaignId: campaign2.id,
+          userId,
+          isShared: true,
+        });
+
+        databaseBuilder.factory.buildAssessment({
+          campaignParticipationId: campaignParticipation2.id,
+          userId, state: 'completed',
+          createdAt: new Date('2020-01-01'),
+        });
+
         await databaseBuilder.commit();
       });
 
-      it('Should return True for isCompleted', async () => {
-        // given
-        const campaignId = campaign1.id;
-
+      it('should return the campaign-participation for the given campaign', async () => {
         // when
-        const campaignParticipationInfos = await campaignParticipationInfoRepository.findByCampaignId(campaignId);
-
-        // then
-        expect(campaignParticipationInfos).to.deep.equal([
-          {
-            sharedAt: campaignParticipation1.sharedAt,
-            createdAt: campaignParticipation1.createdAt,
-            participantExternalId: campaignParticipation1.participantExternalId,
-            userId: campaignParticipation1.userId,
-            isCompleted: true,
-            participantFirstName: 'First',
-            participantLastName: 'Last',
-            studentNumber: null,
-          }
-        ]);
-        expect(campaignParticipationInfos[0].isShared).to.be.true;
-      });
-    });
-
-    context('When the last assessment is not completed', () => {
-      beforeEach(async () => {
-        databaseBuilder.factory.buildAssessment({
-          campaignParticipationId: campaignParticipation1.id,
-          createdAt: new Date('2020-01-01'),
-          state: 'completed',
-        });
-        databaseBuilder.factory.buildAssessment({
-          campaignParticipationId: campaignParticipation1.id,
-          createdAt: new Date('2020-01-02'),
-          state: 'started',
-        });
-        await databaseBuilder.commit();
-      });
-
-      it('Should return false for isCompleted', async () => {
-        // given
-        const campaignId = campaign1.id;
-
-        // when
-        const campaignParticipationInfos = await campaignParticipationInfoRepository.findByCampaignId(campaignId);
+        const campaignParticipationInfos = await campaignParticipationInfoRepository.findByCampaignId(campaign1.id);
 
         // then
         expect(campaignParticipationInfos).to.deep.equal([
@@ -144,47 +67,156 @@ describe('Integration | Repository | Campaign Participation Info', () => {
       });
     });
 
-    context('When the share at is null', () => {
-      it('Should return null as shared date', async () => {
-        // given
-        const campaign = databaseBuilder.factory.buildCampaign({ sharedAt: null });
-        const campaignParticipationId = databaseBuilder.factory.buildCampaignParticipation({
+    context('when there are several participant', () => {
+      let campaign;
+      let campaignParticipation1;
+      let campaignParticipation2;
+
+      beforeEach(async () => {
+        campaign = databaseBuilder.factory.buildCampaign({ type: Campaign.types.ASSESSMENT });
+
+        const user1Id = databaseBuilder.factory.buildUser({ firstName: 'The', lastName: 'Narrator', }).id;
+        const user2Id = databaseBuilder.factory.buildUser({ firstName: 'Tyler', lastName: 'Durden', }).id;
+
+        campaignParticipation1 = databaseBuilder.factory.buildCampaignParticipation({
           campaignId: campaign.id,
-          userId,
+          userId: user1Id,
+          isShared: true,
+          sharedAt: new Date(),
+        });
+
+        databaseBuilder.factory.buildAssessment({
+          campaignParticipationId: campaignParticipation1.id,
+          userId:  user1Id,
+          state: 'started'
+        });
+
+        campaignParticipation2 = databaseBuilder.factory.buildCampaignParticipation({
+          campaignId: campaign.id,
+          userId: user2Id,
           isShared: false,
-          sharedAt: null
-        }).id;
-        databaseBuilder.factory.buildAssessment({ campaignParticipationId, userId, state: 'started' });
+          sharedAt: null,
+        });
+
+        databaseBuilder.factory.buildAssessment({
+          campaignParticipationId: campaignParticipation2.id,
+          userId: user2Id,
+          state: 'completed',
+        });
 
         await databaseBuilder.commit();
+      });
 
+      it('should return all the campaign-participation', async () => {
         // when
         const campaignParticipationInfos = await campaignParticipationInfoRepository.findByCampaignId(campaign.id);
-
+        const campaignParticipationInfosOrdered =  campaignParticipationInfos.sort((a, b) =>  a.lastName < b.lastName);
         // then
-        expect(campaignParticipationInfos[0].isShared).to.be.false;
-        expect(campaignParticipationInfos[0].sharedAt).to.equal(null);
+        expect(campaignParticipationInfosOrdered.length).to.equal(2);
+        expect(campaignParticipationInfosOrdered[0]).to.deep.equal({
+          sharedAt: campaignParticipation1.sharedAt,
+          createdAt: campaignParticipation1.createdAt,
+          participantExternalId: campaignParticipation1.participantExternalId,
+          userId: campaignParticipation1.userId,
+          isCompleted: false,
+          participantFirstName: 'The',
+          participantLastName: 'Narrator',
+          studentNumber: null,
+        });
+        expect(campaignParticipationInfosOrdered[0].isShared).to.equal(true);
+
+        expect(campaignParticipationInfosOrdered[1]).to.deep.equal({
+          sharedAt: campaignParticipation2.sharedAt,
+          createdAt: campaignParticipation2.createdAt,
+          participantExternalId: campaignParticipation2.participantExternalId,
+          userId: campaignParticipation2.userId,
+          isCompleted: true,
+          participantFirstName: 'Tyler',
+          participantLastName: 'Durden',
+          studentNumber: null,
+        });
+        expect(campaignParticipationInfosOrdered[1].isShared).to.equal(false);
       });
     });
 
-    context('When the participation has a schooling registration with a student number', () => {
+    context('when a participant has several assessment', () => {
+      let campaign;
+      let campaignParticipation;
 
-      it('should return the student number', async () => {
-        // given
-        const campaign = databaseBuilder.factory.buildCampaign({ sharedAt: null });
+      beforeEach(async () => {
+        campaign = databaseBuilder.factory.buildCampaign({ type: Campaign.types.ASSESSMENT });
+
+        const userId = databaseBuilder.factory.buildUser({ firstName: 'The', lastName: 'Narrator', }).id;
+
+        campaignParticipation = databaseBuilder.factory.buildCampaignParticipation({
+          campaignId: campaign.id,
+          userId,
+          isShared: true,
+          sharedAt: new Date(),
+        });
+
+        databaseBuilder.factory.buildAssessment({
+          campaignParticipationId: campaignParticipation.id,
+          userId,
+          state: 'completed',
+          createdAt: '2020-01-02'
+        });
+
+        databaseBuilder.factory.buildAssessment({
+          campaignParticipationId: campaignParticipation.id,
+          userId,
+          state: 'started',
+          createdAt: '2020-01-01'
+        });
+
+        await databaseBuilder.commit();
+      });
+
+      it('should information about the newest assessment', async () => {
+        // when
+        const campaignParticipationInfos = await campaignParticipationInfoRepository.findByCampaignId(campaign.id);
+        // then
+        expect(campaignParticipationInfos).to.deep.equal([{
+          sharedAt: campaignParticipation.sharedAt,
+          createdAt: campaignParticipation.createdAt,
+          participantExternalId: campaignParticipation.participantExternalId,
+          userId: campaignParticipation.userId,
+          isCompleted: true,
+          participantFirstName: 'The',
+          participantLastName: 'Narrator',
+          studentNumber: null,
+        }]);
+      });
+    });
+
+    context('when the participant has a schooling registration for the campaign\'s organization', () => {
+      let studentNumber;
+      let campaign;
+      beforeEach(async () => {
+        const userId = databaseBuilder.factory.buildUser({
+          firstName: 'First',
+          lastName: 'Last',
+        }).id;
+        campaign = databaseBuilder.factory.buildCampaign({ sharedAt: null });
         const campaignParticipationId = databaseBuilder.factory.buildCampaignParticipation({
           campaignId: campaign.id,
           userId,
-          isShared: false,
-          sharedAt: null
         }).id;
         databaseBuilder.factory.buildAssessment({ campaignParticipationId, userId, state: 'started' });
-        const studentNumber = databaseBuilder.factory.buildSchoolingRegistration({
+        studentNumber = databaseBuilder.factory.buildSchoolingRegistration({
           organizationId: campaign.organizationId,
           userId,
           studentNumber: 'Pipon et Jambon',
         }).studentNumber;
+        databaseBuilder.factory.buildSchoolingRegistration({
+          userId,
+          studentNumber: 'Yippee Ki Yay',
+        });
+
         await databaseBuilder.commit();
+      });
+
+      it('should return the student number of the schooling registration associated to the given organization', async () => {
 
         // when
         const campaignParticipationInfos = await campaignParticipationInfoRepository.findByCampaignId(campaign.id);
