3a71403b64bf64819a40651edc8cb35f22886295
yahya-youness <you@octo.com>
2020-08-26 11:57:09 +0200

    [IHM] replace extractDataFromToken by decode token helper

--- mon-pix/app/authenticators/oauth2.js
+++ mon-pix/app/authenticators/oauth2.js
@@ -1,6 +1,7 @@
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
 import RSVP from 'rsvp';
 import ENV from 'mon-pix/config/environment';
+import { decodeToken } from 'mon-pix/helpers/jwt';
 
 export default OAuth2PasswordGrant.extend({
   serverTokenEndpoint: `${ENV.APP.API_HOST}/api/token`,
@@ -9,8 +10,9 @@ export default OAuth2PasswordGrant.extend({
   authenticate({ login, password, scope, token }) {
     if (token) {
       const token_type = 'bearer';
-      const user_id = this.extractDataFromToken(token).user_id;
-      const source = this.extractDataFromToken(token).source;
+      const decodedAccessToken = decodeToken(token);
+      const user_id = decodedAccessToken.user_id;
+      const source = decodedAccessToken.source;
       return RSVP.resolve({
         token_type,
         access_token: token,
@@ -21,8 +23,4 @@ export default OAuth2PasswordGrant.extend({
     return this._super(login, password, scope);
   },
 
-  extractDataFromToken(token) {
-    const payloadOfToken = token.split('.')[1];
-    return JSON.parse(atob(payloadOfToken));
-  }
 });
