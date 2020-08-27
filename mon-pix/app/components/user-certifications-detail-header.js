554644ab3ac6363fab53e462a611b62803948578
Krysthalia <ang.schall@gmail.com>
2020-08-27 12:36:12 +0200

    [Mon-Pix] Add feature toggle to show verificationCode

--- mon-pix/app/components/user-certifications-detail-header.js
+++ mon-pix/app/components/user-certifications-detail-header.js
@@ -2,11 +2,13 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import config from 'mon-pix/config/environment';
 
 export default class UserCertificationsDetailHeader extends Component {
   @service intl;
 
   @tracked tooltipText = this.intl.t('pages.certificate.verification-code.copy');
+  isSharedCertificateActive = config.APP.FT_IS_SHARED_CERTIFICATE_ACTIVE;
 
   get birthdate() {
     return this.intl.formatDate(this.args.certification.birthdate, { format: 'LL' });
