2178222d0c74209b8c1d52169ec5b1db8da7ee4b
Antoine Ceol <antoine.ceol@gmail.com>
2020-08-31 15:16:06 +0200

    Handle direct access to verification page

---
+++ mon-pix/tests/unit/routes/shared-certification-test.js
@@ -0,0 +1,24 @@
+// import { expect } from 'chai';
+import sinon from 'sinon';
+import { describe, it } from 'mocha';
+import { setupTest } from 'ember-mocha';
+
+describe('Unit | Route | shared-certification', function() {
+  setupTest();
+
+  it('should redirect if there a no data (direct access)', function() {
+    const route = this.owner.lookup('route:shared-certification');
+    sinon.stub(route, 'replaceWith');
+
+    route.redirect({}, {});
+    sinon.assert.calledWithExactly(route.replaceWith,'/verification-certificat');
+  });
+
+  it('should not redirect with certification', function() {
+    const route = this.owner.lookup('route:shared-certification');
+    sinon.stub(route, 'replaceWith');
+
+    route.redirect({ data: {} }, {});
+    sinon.assert.notCalled(route.replaceWith);
+  });
+});
