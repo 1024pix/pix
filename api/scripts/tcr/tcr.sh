a0d495ed6f95f60a67f13e022214f4ecfb5ab26c
ajoanny <arj@octo.com>
2020-09-02 16:58:32 +0200

    add npx to run linter

--- api/scripts/tcr/tcr.sh
+++ api/scripts/tcr/tcr.sh
@@ -3,7 +3,7 @@
 alteredFilePaths=$(git ls-files -m '*.js');
 
 if [ ! -z "$alteredFilePaths" ] ; then
-  if eslint $alteredFilePaths --fix ; then
+  if npx eslint $alteredFilePaths --fix ; then
     if npm run test:api:path $@ -- --bail; then
       echo "TCR => COMMIT"
       git commit -am "TCR:WIP"
