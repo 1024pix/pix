b9472fd16c1826dfb690515e2a4b5c23a31bf45f
Mathieu Gilet <mathieu.gilet@zenika.com>
2020-09-02 10:45:46 +0200

    Prevent redirection after GAR user authentication

--- mon-pix/app/controllers/campaigns/restricted/join.js
+++ mon-pix/app/controllers/campaigns/restricted/join.js
@@ -25,6 +25,11 @@ export default class JoinRestrictedCampaignController extends Controller {
     const response = await externalUser.save();
 
     this.session.set('data.externalUser', null);
+
+    // Needed to avoid ember-simple-auth from redirecting to routeAfterAuthentication (ie /profil) after authentication
+    // https://github.com/simplabs/ember-simple-auth/blob/e88f1c163e5e9c7a8524bdfca115843d30128a86/addon/mixins/application-route-mixin.js#L104-L112
+    this.session.set('attemptedTransition', { retry: () => {} });
+
     await this.session.authenticate('authenticator:oauth2', { token: response.accessToken });
     await this.currentUser.load();
 
