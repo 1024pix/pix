1a167206b30c541501cba60adbfaa713fd6db972
laura-bergoens <laura.bergoens@gmail.com>
2020-09-02 08:53:44 +0200

    Setup parser to handle import errors based on validation errors

--- api/tests/unit/infrastructure/serializers/csv/higher-education-registration-parser_test.js
+++ api/tests/unit/infrastructure/serializers/csv/higher-education-registration-parser_test.js
@@ -1,4 +1,4 @@
-const { expect } = require('../../../../test-helper');
+const { expect, catchErr } = require('../../../../test-helper');
 const HigherEducationRegistrationParser = require('../../../../../lib/infrastructure/serializers/csv/higher-education-registration-parser');
 const _ = require('lodash');
 
@@ -72,6 +72,96 @@ describe('Unit | Infrastructure | HigherEducationRegistrationParser', () => {
           isSupernumerary: false
         });
       });
+    });
+  });
+
+  context('When file does not match requirements', () => {
+    const organizationId = 123;
+    it('should throw an error if the file is not csv', async () => {
+      const input = `Premier prénom\\Deuxième prénom\\Troisième prénom\\Nom de famille\\Nom d’usage\\Date de naissance (jj/mm/aaaa)\\Email\\Numéro étudiant\\Composante\\Équipe pédagogique\\Groupe\\Diplôme\\Régime
+      Beatrix\\The\\Bride\\Kiddo\\Black Mamba\\01/01/1970\\thebride@example.net\\12346\\Assassination Squad\\Hattori Hanzo\\Deadly Viper Assassination Squad\\Master\\hello darkness my old friend\\`;
+      const parser = new HigherEducationRegistrationParser(input, organizationId);
+
+      const error = await catchErr(parser.parse, parser)();
+
+      expect(error.message).to.equal('Le fichier doit être au format csv avec séparateur virgule ou point-virgule.');
+    });
+
+    it('should throw an error if a column is not recognized', async () => {
+      const input = `Premier prénom;Deuxième prénom2;Troisième prénom;Nom de famille;Nom d’usage;Date de naissance (jj/mm/aaaa);Email;Numéro étudiant;Composante;Équipe pédagogique;Groupe;Diplôme;Régime
+      Beatrix;The;Bride;Kiddo;Black Mamba;01/01/1970;thebride@example.net;12346;Assassination Squad;Hattori Hanzo;Deadly Viper Assassination Squad;Master;hello darkness my old friend;`;
+      const parser = new HigherEducationRegistrationParser(input, organizationId);
+
+      const error = await catchErr(parser.parse, parser)();
+
+      expect(error.message).to.equal('Les entêtes de colonnes doivent être identiques à celle du modèle.');
+    });
+
+    it('should throw an error if a column is missing', async () => {
+      const input = `Deuxième prénom;Troisième prénom;Nom de famille;Nom d’usage;Date de naissance (jj/mm/aaaa);Email;Numéro étudiant;Composante;Équipe pédagogique;Groupe;Diplôme;Régime
+      The;Bride;Kiddo;Black Mamba;01/01/1970;thebride@example.net;12346;Assassination Squad;Hattori Hanzo;Deadly Viper Assassination Squad;Master;hello darkness my old friend;`;
+      const parser = new HigherEducationRegistrationParser(input, organizationId);
+
+      const error = await catchErr(parser.parse, parser)();
+
+      expect(error.message).to.equal('Les entêtes de colonnes doivent être identiques à celle du modèle.');
+    });
+
+  });
+
+  context('When a column value does not match requirements', () => {
+    const organizationId = 123;
+
+    it('should throw an error if column value exceed 255 characters', async () => {
+      const input = `Premier prénom;Deuxième prénom;Troisième prénom;Nom de famille;Nom d’usage;Date de naissance (jj/mm/aaaa);Email;Numéro étudiant;Composante;Équipe pédagogique;Groupe;Diplôme;Régime
+      Beatrix;Thhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhe;Bride;Kiddo;Black Mamba;01/01/1970;thebride@example.net;12346;Assassination Squad;Hattori Hanzo;Deadly Viper Assassination Squad;Master;hello darkness my old friend;`;
+      const parser = new HigherEducationRegistrationParser(input, organizationId);
+
+      const error = await catchErr(parser.parse, parser)();
+
+      expect(error.message).to.contain('Le champ “Deuxième prénom” doit être inférieur à 255 caractères.');
+    });
+
+    it('should throw an error if the birthdate does not have the right format', async () => {
+      const input = `Premier prénom;Deuxième prénom;Troisième prénom;Nom de famille;Nom d’usage;Date de naissance (jj/mm/aaaa);Email;Numéro étudiant;Composante;Équipe pédagogique;Groupe;Diplôme;Régime
+      Beatrix;The;Bride;Kiddo;Black Mamba;1970-01-01;thebride@example.net;12346;Assassination Squad;Hattori Hanzo;Deadly Viper Assassination Squad;Master;hello darkness my old friend;`;
+      const parser = new HigherEducationRegistrationParser(input, organizationId);
+
+      const error = await catchErr(parser.parse, parser)();
+
+      expect(error.message).to.contain('Le champ “Date de naissance” doit être au format jj/mm/aaaa.');
+    });
+
+    it('should throw an error if a required field is missing', async () => {
+      const input = `Premier prénom;Deuxième prénom;Troisième prénom;Nom de famille;Nom d’usage;Date de naissance (jj/mm/aaaa);Email;Numéro étudiant;Composante;Équipe pédagogique;Groupe;Diplôme;Régime
+      Beatrix;The;Bride;Kiddo;Black Mamba;01/01/1970;thebride@example.net;;Assassination Squad;Hattori Hanzo;Deadly Viper Assassination Squad;Master;hello darkness my old friend;`;
+      const parser = new HigherEducationRegistrationParser(input, organizationId);
+
+      const error = await catchErr(parser.parse, parser)();
+
+      expect(error.message).to.contain('Le champ “Numéro étudiant” est obligatoire.');
+    });
+
+    it('should throw an error if the student number is not unique', async () => {
+      const input = `Premier prénom;Deuxième prénom;Troisième prénom;Nom de famille;Nom d’usage;Date de naissance (jj/mm/aaaa);Email;Numéro étudiant;Composante;Équipe pédagogique;Groupe;Diplôme;Régime
+      Beatrix;The;Bride;Kiddo;Black Mamba;01/01/1970;thebride@example.net;123;Assassination Squad;Hattori Hanzo;Deadly Viper Assassination Squad;Master;hello darkness my old friend;
+      Beatrix;The;Bride;Kiddo;Black Mamba;01/01/1970;thebride@example.net;123;Assassination Squad;Hattori Hanzo;Deadly Viper Assassination Squad;Master;hello darkness my old friend;`;
+      const parser = new HigherEducationRegistrationParser(input, organizationId);
+
+      const error = await catchErr(parser.parse, parser)();
+
+      expect(error.message).to.contain('Le champ “Numéro étudiant” doit être unique au sein du fichier.');
+    });
+
+    it('should throw an error including line number', async () => {
+      const input = `Premier prénom;Deuxième prénom;Troisième prénom;Nom de famille;Nom d’usage;Date de naissance (jj/mm/aaaa);Email;Numéro étudiant;Composante;Équipe pédagogique;Groupe;Diplôme;Régime
+      Beatrix;The;Bride;Kiddo;Black Mamba;01/01/1970;thebride@example.net;123;Assassination Squad;Hattori Hanzo;Deadly Viper Assassination Squad;Master;hello darkness my old friend;
+      Beatrix;The;Bride;Kiddo;Black Mamba;01/01/1970;thebride@example.net;123;Assassination Squad;Hattori Hanzo;Deadly Viper Assassination Squad;Master;hello darkness my old friend;`;
+      const parser = new HigherEducationRegistrationParser(input, organizationId);
+
+      const error = await catchErr(parser.parse, parser)();
+
+      expect(error.message).to.contain('A partir de la ligne 2 :');
     });
   });
 });
