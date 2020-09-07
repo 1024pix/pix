f617cb9f381f802a37ab8bbe5c00c985a658215b
alexandrecoin <alexandre.pc.coin@gmail.com>
2020-09-07 14:10:31 +0200

    Glimmerize resume-campaign-banner

--- mon-pix/app/components/resume-campaign-banner.js
+++ mon-pix/app/components/resume-campaign-banner.js
@@ -1,30 +1,16 @@
-/* eslint ember/no-classic-components: 0 */
-/* eslint ember/require-computed-property-dependencies: 0 */
-/* eslint ember/require-tagless-components: 0 */
-
-import { classNames } from '@ember-decorators/component';
-import { computed } from '@ember/object';
-import { filterBy } from '@ember/object/computed';
 import _maxBy from 'lodash/maxBy';
-import Component from '@ember/component';
-import classic from 'ember-classic-decorator';
+import Component from '@glimmer/component';
 
-@classic
-@classNames('resume-campaign-banner')
 export default class ResumeCampaignBanner extends Component {
-  campaignParticipations = [];
 
-  @filterBy('campaignParticipations', 'isShared', false)
-  unsharedCampaignParticipations;
+  get unsharedCampaignParticipations() {
+    return this.args.campaignParticipations.filter((campaignParticipation) => campaignParticipation.isShared === false);
+  }
 
-  @computed('unsharedCampaignParticipations.@each.createdAt')
   get lastUnsharedCampaignParticipation() {
     return _maxBy(this.unsharedCampaignParticipations, 'createdAt');
   }
 
-  @computed(
-    'lastUnsharedCampaignParticipation.campaign.{title,code,isTypeAssessment},lastUnsharedCampaignParticipation.assessment.isCompleted'
-  )
   get campaignParticipationState() {
     if (this.lastUnsharedCampaignParticipation) {
       return {
