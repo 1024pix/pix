3bc7594f54a6411e042d070cfa3910323df07679
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-09-07 16:18:09 +0200

    Add prefix to tcr funs (there is a "test" builtin)

--- api/scripts/tcr/tcr.sh
+++ api/scripts/tcr/tcr.sh
@@ -3,27 +3,27 @@
 alteredFilePaths=$(git ls-files -m '*.js');
 files=( "$@" )
 
-function lint () {
+function _lint () {
   npx eslint $alteredFilePaths --fix
 }
 
-function test () {
+function _test () {
   npm run test:api:path "${files[0]}" -- --bail
 }
 
-function commit () {
+function _commit () {
   echo "TCR => COMMIT"
   git commit -am "TCR:WIP"
 }
 
-function revert () {
+function _revert () {
   echo "TCR => REVERT"
   git checkout HEAD lib/
 }
 
 if [ -n "$alteredFilePaths" ] ; then
-  if lint ; then
-    if test ; then commit ; else revert ; fi
+  if _lint ; then
+    if _test ; then _commit ; else _revert ; fi
   else
     echo "TCR => ABORT"
   fi
