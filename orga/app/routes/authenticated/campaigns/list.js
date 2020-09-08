f8ec449c444df1d43fa1c09bdabcd53cc26153c7
yahya-youness <you@octo.com>
2020-09-08 12:25:52 +0200

    [IHM] remove after model for memberhips

--- orga/app/routes/authenticated/campaigns/list.js
+++ orga/app/routes/authenticated/campaigns/list.js
@@ -38,10 +38,6 @@ export default class ListRoute extends Route {
     }, { reload: true });
   }
 
-  afterModel() {
-    return this.currentUser.organization.memberships;
-  }
-
   resetController(controller, isExiting) {
     if (isExiting) {
       controller.pageNumber = 1;
