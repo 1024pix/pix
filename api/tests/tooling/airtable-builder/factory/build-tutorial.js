5e2fcda23d6f9d01d1d05ad265eac256a61ce103
laura-bergoens <laura.bergoens@gmail.com>
2020-09-11 09:05:23 +0200

    Add buildFromDomaiin to airtable builder tutorial

--- api/tests/tooling/airtable-builder/factory/build-tutorial.js
+++ api/tests/tooling/airtable-builder/factory/build-tutorial.js
@@ -1,4 +1,4 @@
-module.exports = function buildTutorial({
+const buildTutorial = function buildTutorial({
   id = 'recTIddrgjhID28Ep',
   titre = 'Communiquer',
   format = 'vidéo',
@@ -23,3 +23,25 @@ module.exports = function buildTutorial({
     'createdTime': createdTime,
   };
 };
+
+buildTutorial.fromDomain = function buildTutorialFromDomain({
+  domainTutorial,
+  language = 'fr-fr',
+  createdAt = '2018-03-15T14:38:03.000Z',
+}) {
+  return {
+    id: domainTutorial.id,
+    fields: {
+      'id persistant': domainTutorial.id,
+      'Titre': domainTutorial.title,
+      'Format': domainTutorial.format,
+      'Durée': domainTutorial.duration,
+      'Source': domainTutorial.source,
+      'Lien': domainTutorial.link,
+      'Langue': language,
+    },
+    'createdTime': createdAt,
+  };
+};
+
+module.exports = buildTutorial;
