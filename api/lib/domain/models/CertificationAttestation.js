2e86f45b3baa49ac1b03ed3a3ba1c00914ffbd77
Krysthalia <ang.schall@gmail.com>
2020-09-11 14:37:40 +0200

    [API] Add attestation model & repo method

---
+++ api/lib/domain/models/CertificationAttestation.js
@@ -0,0 +1,37 @@
+class CertificationAttestation {
+  constructor({
+    id,
+    firstName,
+    lastName,
+    birthdate,
+    birthplace,
+    isPublished,
+    userId,
+    date,
+    deliveredAt,
+    certificationCenter,
+    pixScore,
+    status,
+    cleaCertificationStatus,
+    resultCompetenceTree = null,
+    verificationCode,
+  } = {}) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthdate = birthdate;
+    this.birthplace = birthplace;
+    this.isPublished = isPublished;
+    this.userId = userId;
+    this.date = date;
+    this.deliveredAt = deliveredAt;
+    this.certificationCenter = certificationCenter;
+    this.pixScore = pixScore;
+    this.status = status;
+    this.cleaCertificationStatus = cleaCertificationStatus;
+    this.resultCompetenceTree = resultCompetenceTree;
+    this.verificationCode = verificationCode;
+  }
+}
+
+module.exports = CertificationAttestation;
