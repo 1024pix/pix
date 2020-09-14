66776ad7edea26cedba2ba50900aa05aa9c6e2eb
Stephane Bedeau <sbedeau@octo.com>
2020-09-14 20:18:30 +0200

    Design the footer for profile

---
+++ mon-pix/app/components/footer.js
@@ -0,0 +1,10 @@
+import { inject as service } from '@ember/service';
+import Component from '@glimmer/component';
+
+export default class Footer extends Component {
+  @service url;
+
+  get shouldShowTheMarianneLogo() {
+    return this.url.isFrenchDomainExtension;
+  }
+}
