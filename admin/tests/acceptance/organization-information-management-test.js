fec3bc30c1d0a483b37de1945bc7346b3d4236d4
lisequesnel <quesnel.lise@gmail.com>
2020-09-16 16:56:54 +0200

    [ADMIN] Update canCollectProfiles

--- admin/tests/acceptance/organization-information-management-test.js
+++ admin/tests/acceptance/organization-information-management-test.js
@@ -16,7 +16,7 @@ module('Acceptance | organization information management', function(hooks) {
 
     test('should edit organization\'s name, externalId, provinceCode and email', async function(assert) {
       // given
-      const organization = this.server.create('organization', { name: 'oldOrganizationName', externalId: 'oldOrganizationExternalId', provinceCode: 'oldProvinceCode' });
+      const organization = this.server.create('organization', { name: 'oldOrganizationName', externalId: 'oldOrganizationExternalId', provinceCode: 'oldProvinceCode', canCollectProfiles: true });
       await visit(`/organizations/${organization.id}`);
       await click('button[aria-label="Editer"]');
 
