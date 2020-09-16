dc8679504c84ac2721948b56eb004d03d9d4305f
laura-bergoens <laura.bergoens@gmail.com>
2020-09-16 21:29:39 +0200

    Delete unused bookshelf tricks in KE bookshelf model

--- api/lib/infrastructure/data/knowledge-element.js
+++ api/lib/infrastructure/data/knowledge-element.js
@@ -1,4 +1,3 @@
-const moment = require('moment');
 const Bookshelf = require('../bookshelf');
 
 require('./assessment');
@@ -18,19 +17,6 @@ module.exports = Bookshelf.model(modelName, {
 
   user() {
     return this.belongsTo('User');
-  },
-
-  isCoveredByTargetProfile(targetProfileSkillIds) {
-    return targetProfileSkillIds.includes(this.get('skillId'));
-  },
-
-  wasCreatedBefore(comparingDate) {
-    const keCreatedAt = moment(this.get('createdAt'));
-    return keCreatedAt.isBefore(comparingDate);
-  },
-
-  isValidated() {
-    return this.get('status') === 'validated';
   },
 
 }, {
