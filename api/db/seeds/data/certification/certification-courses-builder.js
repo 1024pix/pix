4e6ad32ad2c978b2621e7d1e0b49f69c480c6e04
D0C702WH0 <jemyplu@gmail.com>
2020-09-18 14:29:00 +0200

    [API] Adding partner certification builder && clea badge SUCCESS/FAILURE

--- api/db/seeds/data/certification/certification-courses-builder.js
+++ api/db/seeds/data/certification/certification-courses-builder.js
@@ -26,6 +26,8 @@ const ASSESSMENT_FAILURE_IN_PROBLEMS_FINALIZED_SESSION_ID = 105;
 const ASSESSMENT_STARTED_IN_PROBLEMS_FINALIZED_SESSION_ID = 106;
 const ASSESSMENT_SUCCESS_PUBLISHED_SESSION_ID = 107;
 const ASSESSMENT_FAILURE_PUBLISHED_SESSION_ID = 108;
+const CERTIFICATION_COURSE_SUCCESS_ID = 200;
+const CERTIFICATION_COURSE_FAILURE_ID = 403;
 
 function certificationCoursesBuilder({ databaseBuilder }) {
   // Each certification tests present the same questions
@@ -37,17 +39,17 @@ function certificationCoursesBuilder({ databaseBuilder }) {
     { userId: CERTIF_SUCCESS_USER_ID, sessionId: PROBLEMS_FINALIZED_SESSION_ID, assessmentId: ASSESSMENT_SUCCESS_IN_PROBLEMS_FINALIZED_SESSION_ID, candidateData: CANDIDATE_DATA_SUCCESS, examinerComment: 'A regardé son téléphone pendant le test', hasSeenEndTestScreen: true, isPublished: false, verificationCode: null },
     { userId: CERTIF_FAILURE_USER_ID, sessionId: PROBLEMS_FINALIZED_SESSION_ID, assessmentId: ASSESSMENT_FAILURE_IN_PROBLEMS_FINALIZED_SESSION_ID, candidateData: CANDIDATE_DATA_FAILURE, examinerComment: 'Son ordinateur a explosé', hasSeenEndTestScreen: false, isPublished: false, verificationCode: null },
     { userId: CERTIF_REGULAR_USER5_ID, sessionId: PROBLEMS_FINALIZED_SESSION_ID, assessmentId: ASSESSMENT_STARTED_IN_PROBLEMS_FINALIZED_SESSION_ID, candidateData: CANDIDATE_DATA_STARTED, examinerComment: 'Elle a pas finis sa certif', hasSeenEndTestScreen: false, isPublished: false, verificationCode: null  },
-    { userId: CERTIF_SUCCESS_USER_ID, sessionId: PUBLISHED_SESSION_ID, assessmentId: ASSESSMENT_SUCCESS_PUBLISHED_SESSION_ID, candidateData: CANDIDATE_DATA_SUCCESS, hasSeenEndTestScreen: true, isPublished: true, verificationCode: null },
-    { userId: CERTIF_FAILURE_USER_ID, sessionId: PUBLISHED_SESSION_ID, assessmentId: ASSESSMENT_FAILURE_PUBLISHED_SESSION_ID, candidateData: CANDIDATE_DATA_FAILURE, hasSeenEndTestScreen: true, isPublished: true, verificationCode: null },
+    { id: CERTIFICATION_COURSE_SUCCESS_ID, userId: CERTIF_SUCCESS_USER_ID, sessionId: PUBLISHED_SESSION_ID, assessmentId: ASSESSMENT_SUCCESS_PUBLISHED_SESSION_ID, candidateData: CANDIDATE_DATA_SUCCESS, hasSeenEndTestScreen: true, isPublished: true, verificationCode: null },
+    { id: CERTIFICATION_COURSE_FAILURE_ID, userId: CERTIF_FAILURE_USER_ID, sessionId: PUBLISHED_SESSION_ID, assessmentId: ASSESSMENT_FAILURE_PUBLISHED_SESSION_ID, candidateData: CANDIDATE_DATA_FAILURE, hasSeenEndTestScreen: true, isPublished: true, verificationCode: null },
   ], (certificationCourseData) => {
     _buildCertificationCourse(databaseBuilder, certificationCourseData);
   });
 }
 
-function _buildCertificationCourse(databaseBuilder, { assessmentId, userId, sessionId, candidateData, examinerComment, hasSeenEndTestScreen, isPublished, verificationCode }) {
+function _buildCertificationCourse(databaseBuilder, { id, assessmentId, userId, sessionId, candidateData, examinerComment, hasSeenEndTestScreen, isPublished, verificationCode }) {
   const createdAt = new Date('2020-01-31T00:00:00Z');
   const certificationCourseId = databaseBuilder.factory.buildCertificationCourse({
-    ...candidateData, createdAt, isPublished, isV2Certification: true, examinerComment, hasSeenEndTestScreen, sessionId, userId, verificationCode,
+    ...candidateData, id,  createdAt, isPublished, isV2Certification: true, examinerComment, hasSeenEndTestScreen, sessionId, userId, verificationCode,
   }).id;
   databaseBuilder.factory.buildAssessment({
     id: assessmentId, certificationCourseId, type: 'CERTIFICATION', state: 'completed', userId, competenceId: null,
@@ -69,4 +71,6 @@ module.exports = {
   ASSESSMENT_STARTED_IN_PROBLEMS_FINALIZED_SESSION_ID,
   ASSESSMENT_SUCCESS_PUBLISHED_SESSION_ID,
   ASSESSMENT_FAILURE_PUBLISHED_SESSION_ID,
+  CERTIFICATION_COURSE_FAILURE_ID,
+  CERTIFICATION_COURSE_SUCCESS_ID,
 } ;
