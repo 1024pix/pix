a5450f4359394e8adf16c15e47487edc2a1a55ff
alexandrecoin <alexandre.pc.coin@gmail.com>
2020-09-18 17:27:07 +0200

    Upgrade eslint dependencies

--- admin/app/routes/authenticated/certifications/certification/informations.js
+++ admin/app/routes/authenticated/certifications/certification/informations.js
@@ -5,18 +5,19 @@ export default class CertificationInformationsRoute extends Route {
 
   setupController(controller, model) {
     super.setupController(...arguments);
-    const singleCertificationController = this.controllerFor('authenticated.certifications.certification.informations');
-    singleCertificationController.certificationId = model.id;
-    singleCertificationController.certificationStatus = model.status;
+    controller.certificationId = model.id;
+    controller.certificationStatus = model.status;
     controller.send('onCheckMarks');
   }
 
   @action
   willTransition(transition) {
+    // eslint-disable-next-line ember/no-controller-access-in-routes
     if (this.controller.edition &&
       !confirm('Êtes-vous sûr de vouloir abandonner la modification en cours ?')) {
       transition.abort();
     } else {
+      // eslint-disable-next-line ember/no-controller-access-in-routes
       this.controller.edition = false;
       return true;
     }
