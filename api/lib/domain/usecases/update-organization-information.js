4f8736e567491d98d72f16242d5b250139fb0c20
lisequesnel <quesnel.lise@gmail.com>
2020-09-18 11:25:32 +0200

    [SR] unit test correctly update-organization-information usecase

--- api/lib/domain/usecases/update-organization-information.js
+++ api/lib/domain/usecases/update-organization-information.js
@@ -18,7 +18,7 @@ module.exports = async function updateOrganizationInformation({
   organization.email = email;
   organization.externalId = externalId;
   organization.provinceCode = provinceCode;
-  if (isManagingStudents) organization.isManagingStudents = isManagingStudents;
+  organization.isManagingStudents = isManagingStudents;
   organization.canCollectProfiles = canCollectProfiles;
 
   await organizationRepository.update(organization);
