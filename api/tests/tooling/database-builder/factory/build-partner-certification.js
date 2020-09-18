4ffd7796228f87933e802803b4036c5fd73a7b7d
D0C702WH0 <jemyplu@gmail.com>
2020-09-18 16:47:26 +0200

    [API] BSR fix buildPartnerCertification factory

--- api/tests/tooling/database-builder/factory/build-partner-certification.js
+++ api/tests/tooling/database-builder/factory/build-partner-certification.js
@@ -3,7 +3,7 @@ const databaseBuffer = require('../database-buffer');
 
 module.exports = function buildPartnerCertification({
   certificationCourseId,
-  partnerKey = faker.lorem.word,
+  partnerKey = faker.lorem.word(),
   acquired = true,
 }) {
   return databaseBuffer.objectsToInsert.push({
