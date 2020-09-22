152218d062d438ac0171ef1fb8b91a9bb3434550
Krysthalia <ang.schall@gmail.com>
2020-09-22 15:33:05 +0200

    [API] Add attestation generator usecase

---
+++ api/tests/unit/domain/usecases/get-certification-attestation_test.js
@@ -0,0 +1,119 @@
+const { expect, sinon, domainBuilder, catchErr } = require('../../../test-helper');
+const { NotFoundError } = require('../../../../lib/domain/errors');
+const getCertificationAttestation = require('../../../../lib/domain/usecases/certificate/get-certification-attestation');
+
+describe('Unit | UseCase | getCertificationAttestation', async () => {
+
+  const userId = 2;
+  const certificationId = '23';
+  let dependencies;
+  let certificate;
+  const deliveredAt = new Date('2020-09-17T01:02:03Z');
+  const cleaCertificationStatus = 'someStatus';
+
+  beforeEach(() => {
+    certificate = domainBuilder.buildPrivateCertificate({
+      userId,
+      id: certificationId,
+      deliveredAt,
+    });
+    const assessmentResult = domainBuilder.buildAssessmentResult();
+    assessmentResult.competenceMarks = [domainBuilder.buildCompetenceMark({ assessmentResultId: assessmentResult.id })];
+    const competenceTree = domainBuilder.buildCompetenceTree();
+    const certificationRepository = {
+      getCertificationAttestation: sinon.stub().withArgs(certificationId).resolves(certificate),
+    };
+    const cleaCertificationStatusRepository = {
+      getCleaCertificationStatus: sinon.stub().withArgs({ id:  certificationId }).resolves(cleaCertificationStatus),
+    };
+    const competenceTreeRepository = { get: sinon.stub().resolves(competenceTree) };
+    const assessmentResultRepository = {
+      findLatestByCertificationCourseIdWithCompetenceMarks: sinon.stub().withArgs(certificationId).resolves(assessmentResult),
+    };
+
+    dependencies = {
+      certificationRepository,
+      cleaCertificationStatusRepository,
+      assessmentResultRepository,
+      competenceTreeRepository,
+    };
+  });
+
+  context('when the user is not owner of the certification attestation', async () => {
+
+    it('should throw an error if user is not the owner of the certificate', async () => {
+      // given
+      const randomOtherUserId = 666;
+
+      // when
+      const error = await catchErr(getCertificationAttestation)({ certificationId, userId: randomOtherUserId, ...dependencies });
+
+      // then
+      expect(error).to.be.instanceOf(NotFoundError);
+    });
+  });
+
+  context('when the user is owner of the certification attestation', async () => {
+
+    it('should return the attestationPDF', async () => {
+      // given
+      const expectedData = {
+        'birthdate': '1992-06-12',
+        'birthplace': 'Paris',
+        'certificationCenter': 'L’univeristé du Pix',
+        cleaCertificationStatus,
+        'commentForCandidate': 'Comment for Candidate',
+        'date': certificate.date,
+        'deliveredAt': deliveredAt,
+        'firstName': 'Jean',
+        'id': '23',
+        'isPublished': true,
+        'lastName': 'Bon',
+        'pixScore': 31,
+        'resultCompetenceTree': {
+          'areas': [
+            {
+              'code': '1',
+              'color': 'jaffa',
+              'id': 'recvoGdo7z2z7pXWa',
+              'name': '1. Information et données',
+              'resultCompetences': [
+                {
+                  'id': 'recsvLz0W2ShyfD63',
+                  'index': '1.1',
+                  'level': 2,
+                  'name': 'Mener une recherche et une veille d’information',
+                  'score': 13,
+                },
+                {
+                  'id': 'recNv8qhaY887jQb2',
+                  'index': '1.2',
+                  'level': -1,
+                  'name': 'Mener une recherche et une veille d’information',
+                  'score': 0,
+                },
+                {
+                  'id': 'recIkYm646lrGvLNT',
+                  'index': '1.3',
+                  'level': -1,
+                  'name': 'Mener une recherche et une veille d’information',
+                  'score': 0,
+                },
+              ],
+              'title': 'Information et données',
+            },
+          ],
+          'id': '23-1',
+        },
+        'status': 'validated',
+        'userId': 2,
+        'verificationCode': 'P-BBBCCCDD',
+      };
+      // when
+      const result = await getCertificationAttestation({ certificationId, userId, ...dependencies });
+
+      // then
+      expect(result).to.deep.equal(expectedData);
+    });
+  });
+});
