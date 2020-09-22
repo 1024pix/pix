0f82718ac89db988fa87c5ad45835ca06d8c575a
Krysthalia <ang.schall@gmail.com>
2020-09-22 15:15:06 +0200

    [API] Add PDF generation utilities

---
+++ api/tests/integration/infrastructure/utils/pdf/write-pdf-utils_test.js
@@ -0,0 +1,26 @@
+const { expect } = require('../../../../test-helper');
+
+const { getPdfBuffer } = require('../../../../../lib/infrastructure/utils/pdf/write-pdf-utils');
+
+describe('Integration | Infrastructure | Utils | Pdf | write-Pdf-utils', () => {
+
+  describe('getPdfBuffer', () => {
+
+    it('should return the pdf file as a buffer', async () => {
+      // given
+      const templateFileName = 'attestation-pix-vierge-clea.pdf';
+
+      //For now we can't compare to a target file's buffer due to pdf-lib wich is non deterministic
+      //And should be fixed by this PR (https://github.com/Hopding/pdf-lib/pull/574)
+
+      // when
+      const result = await getPdfBuffer({
+        templatePath: `${__dirname}/files`,
+        templateFileName,
+      });
+
+      // then
+      expect(result).to.be.instanceOf(Buffer);
+    });
+  });
+});
