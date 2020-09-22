c3dd59cb696cb613ad615ff32692c0d482d03c8c
Krysthalia <ang.schall@gmail.com>
2020-09-22 15:18:23 +0200

    [API] Add getPDFAttestation() in controller

--- api/lib/infrastructure/utils/pdf/certification-attestation-pdf.js
+++ api/lib/infrastructure/utils/pdf/certification-attestation-pdf.js
@@ -1,16 +1,25 @@
 const { getPdfBuffer } = require('./write-pdf-utils');
+const moment = require('moment');
 
 module.exports = {
   async getCertificationAttestationPdfBuffer({
     certificate,
   }) {
+    const formatedDeliveryDate = moment(certificate.deliveredAt).format('YYYYMMDD');
+
+    const fileName = `attestation-pix-${formatedDeliveryDate}.pdf`;
+
     const templateFileName = certificate.hasAcquiredCleaCertification
       ? 'attestation-pix-vierge-clea.pdf'
       : 'attestation-pix-vierge-sans-clea.pdf';
-    return getPdfBuffer({
-      templatePath: `${__dirname}/files`,
-      templateFileName,
-      templateData: { certificate },
-    });
+
+    return {
+      file: await  getPdfBuffer({
+        templatePath: `${__dirname}/files`,
+        templateFileName,
+        templateData: { certificate },
+      }),
+      fileName,
+    };
   },
 };
