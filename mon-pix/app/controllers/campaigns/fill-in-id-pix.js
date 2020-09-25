e772a854d4a03abca1fa59d07451e88a6b43a8ab
lisequesnel <quesnel.lise@gmail.com>
2020-09-25 16:31:46 +0200

    [APP] Move logic into a component

--- mon-pix/app/controllers/campaigns/fill-in-id-pix.js
+++ mon-pix/app/controllers/campaigns/fill-in-id-pix.js
@@ -1,33 +1,14 @@
-import { inject as service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import Controller from '@ember/controller';
 
 export default class FillInIdPix extends Controller {
-  @service intl;
-
-  @tracked participantExternalId = null;
-  @tracked isLoading = false;
-  @tracked errorMessage = null;
-
   @action
-  submit() {
-    this.model.errorMessage = null;
-
-    const participantExternalId = this.participantExternalId;
-
-    if (participantExternalId) {
-      this.isLoading = true;
-      return this.transitionToRoute('campaigns.start-or-resume', this.model, { queryParams: { participantExternalId } });
-    } else {
-      return this.errorMessage = this.intl.t('pages.fill-in-id-pix.errors.missing-id-pix-label',  { idPixLabel: this.model.idPixLabel });
-    }
+  onSubmitParticipantExternalId(participantExternalId) {
+    return this.transitionToRoute('campaigns.start-or-resume', this.model, { queryParams: { participantExternalId } });
   }
 
   @action
-  cancel() {
-    this.errorMessage = null;
-
+  onCancel() {
     return this.transitionToRoute('campaigns.start-or-resume', this.model.code, {
       queryParams: { hasUserSeenLandingPage: false },
     });
