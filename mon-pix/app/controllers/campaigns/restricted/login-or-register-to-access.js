a124f5c9f1c6eeba9dff2f722906c59ea93f8eaf
Pierre TOP (TOPI) <ptop@octo.com>
2020-09-25 17:08:39 +0200

    [APP] Reconcile external user on first connexion

--- mon-pix/app/controllers/campaigns/restricted/login-or-register-to-access.js
+++ mon-pix/app/controllers/campaigns/restricted/login-or-register-to-access.js
@@ -7,6 +7,7 @@ export default class LoginOrRegisterToAccessRoute extends Controller {
 
   @service currentUser;
   @service session;
+  @service store;
 
   @tracked displayRegisterForm = true;
 
@@ -19,6 +20,7 @@ export default class LoginOrRegisterToAccessRoute extends Controller {
 
   @action
   async addGarAuthenticationMethodToUser(externalUserToken, expectedUserId) {
+
     await this.currentUser.load();
     await this.currentUser.user.save({
       adapterOptions: {
@@ -28,12 +30,25 @@ export default class LoginOrRegisterToAccessRoute extends Controller {
       },
     });
 
-    this.session.set('data.externalUser', null);
-    this.session.set('data.expectedUserId', null);
+    await this._clearExternalUserContext();
+
+    await this._reconcileUser();
 
     this.transitionToRoute('campaigns.start-or-resume', this.model.code, {
       queryParams: { associationDone: true, participantExternalId: this.participantExternalId },
     });
+  }
+  _reconcileUser() {
+    return this.store.createRecord(
+      'schooling-registration-user-association',
+      { userId: this.currentUser.user.id, campaignCode: this.model.code })
+      .save(
+        { adapterOptions: { tryReconciliation: true } });
+  }
+
+  _clearExternalUserContext() {
+    this.session.set('data.externalUser', null);
+    this.session.set('data.expectedUserId', null);
   }
 
 }
