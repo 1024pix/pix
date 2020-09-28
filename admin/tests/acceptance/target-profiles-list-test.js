f2074e716beca6c5a22467ed89d353eb4dee70c6
lisequesnel <quesnel.lise@gmail.com>
2020-09-28 11:46:10 +0200

    [ADMIN] Display target profiles list from menu
    Including pagination & filtering

---
+++ admin/tests/acceptance/target-profiles-list-test.js
@@ -0,0 +1,72 @@
+import { module, test } from 'qunit';
+import { currentURL, visit } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import { createAuthenticateSession } from 'pix-admin/tests/helpers/test-init';
+
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+
+module('Acceptance | Target Profiles List', function(hooks) {
+
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  module('When user is not logged in', function() {
+
+    test('it should not be accessible by an unauthenticated user', async function(assert) {
+      // when
+      await visit('/target-profiles/list');
+
+      // then
+      assert.equal(currentURL(), '/login');
+    });
+  });
+
+  module('When user is logged in', function(hooks) {
+
+    hooks.beforeEach(async () => {
+      await createAuthenticateSession({ userId: 1 });
+    });
+
+    test('it should be accessible for an authenticated user', async function(assert) {
+      // when
+      await visit('/target-profiles/list');
+
+      // then
+      assert.equal(currentURL(), '/target-profiles/list');
+    });
+
+    test('it should list target profiles', async function(assert) {
+      // given
+      server.createList('target-profile', 12);
+
+      // when
+      await visit('/target-profiles/list');
+
+      // then
+      assert.dom('[aria-label="Profil cible"]').exists({ count: 12 });
+    });
+
+    module('when filters are used', function(hooks) {
+
+      hooks.beforeEach(async () => {
+        server.createList('target-profile', 12);
+      });
+
+      test('it should display the current filter when target profiles are filtered by name', async function(assert) {
+        // when
+        await visit('/target-profiles/list?name=sav');
+
+        // then
+        assert.dom('input#name').hasValue('sav');
+      });
+
+      test('it should display the current filter when target profiles are filtered by id', async function(assert) {
+        // when
+        await visit('/target-profiles/list?id=123');
+
+        // then
+        assert.dom('input#id').hasValue('123');
+      });
+    });
+  });
+});
