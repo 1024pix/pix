0ff3bf315738da59e060d0cd5aeb9b1b6c7f709d
Krysthalia <ang.schall@gmail.com>
2020-09-30 11:06:34 +0200

    [Certif] BSR add context in test

--- certif/tests/unit/models/session-test.js
+++ certif/tests/unit/models/session-test.js
@@ -7,33 +7,39 @@ import { CREATED, FINALIZED } from 'pix-certif/models/session';
 module('Unit | Model | session', function(hooks) {
   setupTest(hooks);
 
-  test('it should return the correct displayName', function(assert) {
-    const store = this.owner.lookup('service:store');
-    const model1 = run(() => store.createRecord('session', {
-      id: 123,
-      status: CREATED,
-    }));
-    const model2 = run(() => store.createRecord('session', {
-      id: 1234,
-      status: FINALIZED,
-    }));
+  module('#displayStatus', function() {
 
-    assert.equal(model1.displayStatus, 'Créée');
-    assert.equal(model2.displayStatus, 'Finalisée');
+    test('it should return the correct displayName', function(assert) {
+      const store = this.owner.lookup('service:store');
+      const model1 = run(() => store.createRecord('session', {
+        id: 123,
+        status: CREATED,
+      }));
+      const model2 = run(() => store.createRecord('session', {
+        id: 1234,
+        status: FINALIZED,
+      }));
+
+      assert.equal(model1.displayStatus, 'Créée');
+      assert.equal(model2.displayStatus, 'Finalisée');
+    });
   });
 
-  test('it should return the correct urlToUpload', function(assert) {
-    const store = this.owner.lookup('service:store');
-    const model = run(() => store.createRecord('session', { id: 1 }));
+  module('#urlToDownload', function() {
 
-    assert.equal(model.urlToUpload, `${config.APP.API_HOST}/api/sessions/1/certification-candidates/import`);
-  });
+    test('it should return the correct urlToUpload', function(assert) {
+      const store = this.owner.lookup('service:store');
+      const model = run(() => store.createRecord('session', { id: 1 }));
 
-  test('it should return the correct urlToDownload', function(assert) {
-    const store = this.owner.lookup('service:store');
-    const model = run(() => store.createRecord('session', { id: 1 }));
-    model.session = { data: { authenticated: { access_token: '123' } } };
+      assert.equal(model.urlToUpload, `${config.APP.API_HOST}/api/sessions/1/certification-candidates/import`);
+    });
 
-    assert.equal(model.urlToDownload, `${config.APP.API_HOST}/api/sessions/1/attendance-sheet?accessToken=123`);
+    test('it should return the correct urlToDownload', function(assert) {
+      const store = this.owner.lookup('service:store');
+      const model = run(() => store.createRecord('session', { id: 1 }));
+      model.session = { data: { authenticated: { access_token: '123' } } };
+
+      assert.equal(model.urlToDownload, `${config.APP.API_HOST}/api/sessions/1/attendance-sheet?accessToken=123`);
+    });
   });
 });
