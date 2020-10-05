02c827d9731cf77fff2624625a213f2e37be4d5e
Benjamin Petetot <bpetetot@gmail.com>
2020-10-05 16:57:22 +0200

    [API] Add PerfTimer utility test tool

---
+++ api/lib/infrastructure/perftimer.js
@@ -0,0 +1,32 @@
+const { performance } = require('perf_hooks');
+
+class PerfTimer  {
+
+  constructor() {
+    this._start  = {};
+    this._time = {};
+  }
+
+  start(key) {
+    this._start[key] = performance.now();
+  }
+
+  stop(key) {
+    const time = performance.now() - this._start[key];
+    if (this._time[key] === undefined) {
+      this._time[key] = 0;
+    }
+    this._time[key] += time;
+  }
+
+  log() {
+    console.log(this._time);
+  }
+
+  reset() {
+    this._time = {};
+  }
+}
+
+module.exports = PerfTimer;
+
