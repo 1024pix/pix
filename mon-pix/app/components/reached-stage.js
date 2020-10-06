8f5e3e32ca8619d54c132652d4238ff29c852206
celineung <celine.ung@zenika.com>
2020-10-06 22:59:09 +0200

    Make stage accessible

--- mon-pix/app/components/reached-stage.js
+++ mon-pix/app/components/reached-stage.js
@@ -3,12 +3,41 @@ import range from 'lodash/range';
 
 export default class ReachedStage extends Component {
 
-  get acquiredStars() {
-    return range(1, this.args.starCount);
+  get firstStar() {
+    return this._stars[0];
   }
 
-  get unacquiredStars() {
-    return range(this.args.starCount, this.args.stageCount);
+  get otherStars() {
+    return this._stars.slice(1);
+  }
+
+  get acquiredStarsCount() {
+    return this._acquiredStars.length;
+  }
+
+  get totalStarsCount() {
+    return this._stars.length;
+  }
+
+  get _stars() {
+    return this._acquiredStars.concat(this._unacquiredStars);
+  }
+
+  get _acquiredStars() {
+    if (this.args.starCount < 1) {
+      return [];
+    }
+    return range(1, this.args.starCount)
+      .map(() => {
+        return { status: 'acquired', imageSrc: 'stage-plain-star.svg' };
+      });
+  }
+
+  get _unacquiredStars() {
+    return range(this.args.starCount, this.args.stageCount)
+      .map(() => {
+        return { status: 'unacquired', imageSrc: 'stage-clear-star.svg' };
+      });
   }
 }
 
