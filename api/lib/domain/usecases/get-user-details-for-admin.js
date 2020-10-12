48aa30302522d2a8d4dd0ef98d1765dbbadb4ed3
Christophe Charles <ccharles@octo.com>
2020-10-12 15:33:40 +0200

    [API] Update get UserDetailsForAdmin usecase

--- api/lib/domain/usecases/get-user-details-for-admin.js
+++ api/lib/domain/usecases/get-user-details-for-admin.js
@@ -1,3 +1,10 @@
-module.exports = function getUserDetailsForAdmin({ userId, userRepository }) {
-  return userRepository.getUserDetailsForAdmin(userId);
+module.exports = async function getUserDetailsForAdmin({
+  userId, userRepository, schoolingRegistrationRepository,
+}) {
+  const userDetailsForAdmin = await userRepository.getUserDetailsForAdmin(userId);
+
+  const foundSchoolingRegistrations = await schoolingRegistrationRepository.findByUserId({ userId });
+  userDetailsForAdmin.isAssociatedWithSchoolingRegistration = foundSchoolingRegistrations.length > 0;
+
+  return userDetailsForAdmin;
 };
