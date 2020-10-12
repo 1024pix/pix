8368a5c2f2ab437b0cc270533271e8e0662e582e
Christophe Charles <ccharles@octo.com>
2020-10-12 21:31:10 +0200

    [API] Add attribute to the update usecase
    Add this boolean attribute into UserDetailsForAdmin model,
    to indicate that the user is associated with one or more schooling registration

--- api/tests/integration/domain/usecases/update-user-details-for-administration_test.js
+++ api/tests/integration/domain/usecases/update-user-details-for-administration_test.js
@@ -1,7 +1,9 @@
 const { expect,catchErr, databaseBuilder } = require('../../../test-helper');
+
 const { AlreadyRegisteredEmailError } = require('../../../../lib/domain/errors');
 
 const userRepository = require('../../../../lib/infrastructure/repositories/user-repository');
+const schoolingRegistrationRepository = require('../../../../lib/infrastructure/repositories/schooling-registration-repository');
 const UserDetailsForAdmin = require('../../../../lib/domain/models/UserDetailsForAdmin');
 
 const updateUserDetailsForAdministration = require('../../../../lib/domain/usecases/update-user-details-for-administration');
@@ -12,64 +14,85 @@ describe('Integration | UseCases | updateUserDetailsForAdministration', () => {
 
   beforeEach(async () => {
     userId = databaseBuilder.factory.buildUser({ email: 'email@example.net' }).id;
-    databaseBuilder.factory.buildUser({ email: 'alreadyexist@example.net'   });
+    databaseBuilder.factory.buildUser({ email: 'alreadyexist@example.net' });
     await databaseBuilder.commit();
   });
 
-  it('should update user email,firstname,lastname', async () => {
+  it('should update user email, firstname and lastname', async () => {
     // given
     const userToUpdate = {
-      id: userId,
       email: 'partial@example.net',
       firstName: 'firstName',
       lastName: 'lastName',
     };
 
     // when
-    const result = await updateUserDetailsForAdministration({ userRepository, userId, userDetailsForAdministration: userToUpdate });
+    const result = await updateUserDetailsForAdministration({
+      userId,
+      userDetailsForAdministration: userToUpdate,
+      userRepository,
+      schoolingRegistrationRepository,
+    });
 
     // then
     expect(result).to.be.an.instanceOf(UserDetailsForAdmin);
     expect(result.email).equal(userToUpdate.email);
     expect(result.firstName).equal(userToUpdate.firstName);
     expect(result.lastName).equal(userToUpdate.lastName);
-
   });
 
   it('should update user email only', async () => {
     // given
     const userToUpdate = {
-      id: userId,
       email: 'partial@example.net',
     };
 
     // when
-    const result = await updateUserDetailsForAdministration({ userRepository, userId: userId, userDetailsForAdministration: userToUpdate });
+    const result = await updateUserDetailsForAdministration({
+      userId,
+      userDetailsForAdministration: userToUpdate,
+      userRepository,
+      schoolingRegistrationRepository,
+    });
 
     // then
-    expect(result).to.be.an.instanceOf(UserDetailsForAdmin);
     expect(result.email).equal(userToUpdate.email);
-
   });
 
-  it('should throw AlreadyRegisteredEmailError when email il already used by another user', async () => {
+  it('should update user and change attribute isAssociatedWithSchoolingRegistration when association exist', async () => {
+    // given
+    userId = databaseBuilder.factory.buildSchoolingRegistrationWithUser().userId;
+    await databaseBuilder.commit();
+
+    // when
+    const result = await updateUserDetailsForAdministration({
+      userId,
+      userDetailsForAdministration: { email: 'partial@example.net' },
+      userRepository,
+      schoolingRegistrationRepository,
+    });
+
+    // then
+    expect(result.isAssociatedWithSchoolingRegistration).to.be.true;
+  });
+
+  it('should throw AlreadyRegisteredEmailError when email is already used by another user', async () => {
     // given
     const userToUpdate = {
-      id: userId,
       email: 'alreadyEXIST@example.net',
     };
 
     // when
     const error = await catchErr(updateUserDetailsForAdministration)({
-      userRepository,
       userId,
       userDetailsForAdministration: userToUpdate,
+      userRepository,
+      schoolingRegistrationRepository,
     });
 
     // then
     expect(error).to.be.instanceOf(AlreadyRegisteredEmailError);
     expect(error.message).to.equal('Cet email est déjà utilisé.');
-
   });
 
 });
