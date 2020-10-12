346ae68bc541c4c7774aa64ccf904c46ec728b8d
Anne-G <ang.schall@gmail.com>
2020-10-12 18:30:11 +0200

    [Certif] Add adapter to get all certifCandidates

--- certif/app/adapters/certification-candidate.js
+++ certif/app/adapters/certification-candidate.js
@@ -2,6 +2,15 @@ import ApplicationAdapter from './application';
 
 export default class CertificationCandidateAdapter extends ApplicationAdapter {
 
+  urlForQuery(query) {
+    const sessionId = query.sessionId;
+    if (sessionId) {
+      delete query.sessionId;
+      return `${this.host}/${this.namespace}/sessions/${sessionId}/certification-candidates`;
+    }
+    return super.urlForQuery(...arguments);
+  }
+
   urlForCreateRecord(modelName, { adapterOptions }) {
     const url = super.urlForCreateRecord(...arguments);
 
