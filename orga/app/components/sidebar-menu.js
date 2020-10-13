5e41ad05f1d432ba6f616055666fff4f4666a203
Laurent <laurent.lucas@pix.fr>
2020-10-13 15:33:11 +0200

    Add documentation link for orga SCO-AGRI

--- orga/app/components/sidebar-menu.js
+++ orga/app/components/sidebar-menu.js
@@ -5,6 +5,10 @@ export default class SidebarMenu extends Component {
   @service currentUser;
 
   get documentationUrl() {
+    if (this.currentUser.isSCOManagingStudents && this.currentUser.isAgriculture) {
+      return 'https://view.genial.ly/5f85a0b87812e90d12b7b593';
+    }
+
     if (this.currentUser.isSCOManagingStudents) {
       return 'https://view.genial.ly/5f3e7a5ba8ffb90d11ac034f';
     }
