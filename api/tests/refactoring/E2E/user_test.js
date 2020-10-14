bba9fbb187d4ea06bfa79fa9ecd9fc9c6973e820
ajoanny <ajoanny@octo.com>
2020-10-14 15:03:42 +0200

    Add E2E test when user authenticated

--- api/tests/refactoring/E2E/user_test.js
+++ api/tests/refactoring/E2E/user_test.js
@@ -3,6 +3,7 @@ const expect = chai.expect;
 const axios = require('axios');
 
 describe('API /api/users/me', () => {
+
   it('returns 401  when user is not authenticated', async () => {
     const  requestUrl = 'http://localhost:3000/api/users/me';
     let response = null;
@@ -13,4 +14,27 @@ describe('API /api/users/me', () => {
     }
     expect(response.status).to.equal(401);
   });
+
+  it('returns 200 when user is authenticated', async () => {
+
+    const email = 'sco@example.net';
+    const authenticateUrl = 'http://localhost:3000/api/token';
+    const payload = 'grant_type=password&username=' + email + '&password=pix123&scope=mon-pix';
+
+    const authenticateResponse = await axios({ method: 'post', url: authenticateUrl, data: payload });
+    console.log(authenticateResponse);
+
+    const config = {
+      headers: { Authorization: `Bearer ${authenticateResponse.data.access_token}` }
+    };
+
+    const requestUrl = 'http://localhost:3000/api/users/me';
+    const response = await axios.get(requestUrl, config);
+
+    console.log(response);
+    expect(response.status).to.equal(200);
+    expect(response.data.data.attributes.email).to.equal(email);
+    
+  });
+
 });
