fdf76830a073df45954fc36e7709ae04eb9d6ad5
ajoanny <ajoanny@octo.com>
2020-10-14 15:51:01 +0200

    [API] create user in test

--- api/tests/refactoring/E2E/user_test.js
+++ api/tests/refactoring/E2E/user_test.js
@@ -1,8 +1,15 @@
 const chai = require('chai');
 const expect = chai.expect;
 const axios = require('axios');
+const { knex } = require('../../../db/knex-database-connection');
+
+const DatabaseBuilder = require('../../tooling/database-builder/database-builder');
+const databaseBuilder = new DatabaseBuilder({ knex });
 
 describe('API /api/users/me', () => {
+  afterEach(function() {
+    return databaseBuilder.clean();
+  });
 
   it('returns 401  when user is not authenticated', async () => {
     const  requestUrl = 'http://localhost:3000/api/users/me';
@@ -16,25 +23,26 @@ describe('API /api/users/me', () => {
   });
 
   it('returns 200 when user is authenticated', async () => {
-
     const email = 'sco@example.net';
+    const password = 'pix123';
+
+    databaseBuilder.factory.buildUser({ email, password });
+    await databaseBuilder.commit();
+
     const authenticateUrl = 'http://localhost:3000/api/token';
-    const payload = 'grant_type=password&username=' + email + '&password=pix123&scope=mon-pix';
+    const payload = 'grant_type=password&username=' + email + '&password=' + password + '&scope=mon-pix';
 
     const authenticateResponse = await axios({ method: 'post', url: authenticateUrl, data: payload });
-    console.log(authenticateResponse);
 
     const config = {
-      headers: { Authorization: `Bearer ${authenticateResponse.data.access_token}` }
+      headers: { Authorization: `Bearer ${authenticateResponse.data.access_token}` },
     };
 
     const requestUrl = 'http://localhost:3000/api/users/me';
     const response = await axios.get(requestUrl, config);
 
-    console.log(response);
     expect(response.status).to.equal(200);
     expect(response.data.data.attributes.email).to.equal(email);
-    
   });
 
 });
