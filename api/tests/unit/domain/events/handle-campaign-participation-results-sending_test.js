3de42730a274a01ff924587ad84f7de919ebcc5c
lisequesnel <quesnel.lise@gmail.com>
2020-10-15 14:17:54 +0200

    [API] Console.log mocked data for Pole Emploi

--- api/tests/unit/domain/events/handle-campaign-participation-results-sending_test.js
+++ api/tests/unit/domain/events/handle-campaign-participation-results-sending_test.js
@@ -10,6 +10,49 @@ describe('Unit | Domain | Events | handle-campaign-participation-results-sending
     organizationRepository,
   };
 
+  const expectedResults = '{' +
+    '"resultatIndividu":{' +
+      '"campagne":{' +
+        '"nom":"Campagne Pôle Emploi",' +
+        '"dateDebut":"2020-01-01T00:00:00.000Z",' +
+        '"dateFin":"2020-02-01T00:00:00.000Z",' +
+        '"typeCampagne":"EVALUATION",' +
+        '"idCampagne":11223344,' +
+        '"codeCampagne":"CODEPE123",' +
+        '"URLCampagne":"https://app.pix.fr/campagnes/CODEPE123",' +
+        '"nomOrganisme":"Pix",' +
+        '"typeOrganisme":"externe"' +
+      '},' +
+      '"individu":{' +
+        '"nom":"Bonneau",' +
+        '"prenom":"Jean"' +
+      '},' +
+      '"test":{' +
+        '"etat":4,' +
+        '"progression":100,' +
+        '"typeTest":"DI",' +
+        '"referenceExterne":55667788,' +
+        '"dateDebut":"2020-01-02T00:00:00.000Z",' +
+        '"dateModification":"2020-01-03T00:00:00.000Z",' +
+        '"dateValidation":"2020-01-03T00:00:00.000Z",' +
+        '"evaluationCible":42.5,' +
+        '"uniteValidation":"A",' +
+        '"elementsEvalues":[{' +
+          '"libelle":"Gérer des données",' +
+          '"categorie":"compétence",' +
+          '"type":"compétence Pix",' +
+          '"domaineRattachement":"Information et données",' +
+          '"nbSousElements":3,' +
+          '"evaluation":{' +
+            '"scoreObtenu":14.2,' +
+            '"uniteScore":"A",' +
+            '"nbSousElementValide":2' +
+          '}' +
+        '}]' +
+      '}' +
+    '}' +
+  '}';
+
   it('fails when event is not of correct type', async () => {
     // given
     const event = 'not an event of the correct type';
@@ -49,7 +92,9 @@ describe('Unit | Domain | Events | handle-campaign-participation-results-sending
         });
 
         // then
-        expect(console.log).to.have.been.calledWithMatch('resultats mockés');
+        expect(console.log).to.have.been.calledOnce;
+        const results = console.log.firstCall.args[0];
+        expect(results).to.deep.equal(expectedResults);
       });
     });
 
@@ -67,7 +112,7 @@ describe('Unit | Domain | Events | handle-campaign-participation-results-sending
         sinon.stub(console, 'log').resolves();
       });
 
-      it('it should console.log results', async () => {
+      it('it should not console.log results', async () => {
         // when
         await handleCampaignParticipationResultsSending({
           event, ...dependencies,
@@ -92,7 +137,7 @@ describe('Unit | Domain | Events | handle-campaign-participation-results-sending
         sinon.stub(console, 'log').resolves();
       });
 
-      it('it should console.log results', async () => {
+      it('it should not console.log results', async () => {
         // when
         await handleCampaignParticipationResultsSending({
           event, ...dependencies,
