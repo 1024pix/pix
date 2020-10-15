a0f05154b0bfd3aae730c9deacb2137b498b1134
Mathieu Gilet <mathieu.gilet@zenika.com>
2020-10-15 14:13:11 +0200

    [API] Update admin/users/{id} and admin/users/{id}/dissociate

--- api/lib/domain/usecases/dissociate-schooling-registrations.js
+++ api/lib/domain/usecases/dissociate-schooling-registrations.js
@@ -5,10 +5,5 @@ module.exports = async function dissociateSchoolingRegistrations({
 }) {
   await schoolingRegistrationRepository.dissociateByUser(userId);
 
-  const userDetailsForAdmin = await userRepository.getUserDetailsForAdmin(userId);
-
-  const foundSchoolingRegistrations = await schoolingRegistrationRepository.findByUserId({ userId });
-  userDetailsForAdmin.isAssociatedWithSchoolingRegistration = foundSchoolingRegistrations.length > 0;
-
-  return userDetailsForAdmin;
+  return await userRepository.getUserDetailsForAdmin(userId);
 };
--- api/tests/unit/domain/usecases/dissociate-schooling-registrations_test.js
+++ api/tests/unit/domain/usecases/dissociate-schooling-registrations_test.js
@@ -10,7 +10,6 @@ describe('Unit | UseCase | dissociate-schooling-registrations', () => {
   beforeEach(() => {
     schoolingRegistrationRepository = {
       dissociateByUser: sinon.stub().resolves(),
-      findByUserId: sinon.stub(),
     };
     userRepository = {
       getUserDetailsForAdmin: sinon.stub(),
@@ -22,34 +21,10 @@ describe('Unit | UseCase | dissociate-schooling-registrations', () => {
     const userId = 1;
     const expectedUserDetailsForAdmin = {
       id: userId,
-      isAssociatedWithSchoolingRegistration: false,
+      schoolingRegistrations: [],
     };
 
-    schoolingRegistrationRepository.findByUserId.resolves([]);
-    userRepository.getUserDetailsForAdmin.resolves({ id: userId });
-
-    // when
-    const updatedUserDetailsForAdmin = await dissociateSchoolingRegistrations({
-      userId,
-      schoolingRegistrationRepository,
-      userRepository,
-    });
-
-    // then
-    expect(updatedUserDetailsForAdmin).to.deep.equal(expectedUserDetailsForAdmin);
-  });
-
-  it('should return updated user when it is not associated with schooling registration', async () => {
-    // given
-    const userId = 999;
-    const expectedUserDetailsForAdmin = {
-      id: userId,
-      isAssociatedWithSchoolingRegistration: false,
-    };
-
-    schoolingRegistrationRepository.dissociateByUser.returns(null);
-    schoolingRegistrationRepository.findByUserId.resolves([]);
-    userRepository.getUserDetailsForAdmin.resolves({ id: userId });
+    userRepository.getUserDetailsForAdmin.resolves({ id: userId, schoolingRegistrations: [] });
 
     // when
     const updatedUserDetailsForAdmin = await dissociateSchoolingRegistrations({
