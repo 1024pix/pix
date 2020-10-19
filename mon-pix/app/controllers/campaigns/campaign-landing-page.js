a02a3f5def20cccc3024f08677be4e3683155b5e
melanieb <melanie.boudard@gmail.com>
2020-10-19 17:22:02 +0200

    Remove useless isLoading in campaign-landing-page

--- mon-pix/app/controllers/campaigns/campaign-landing-page.js
+++ mon-pix/app/controllers/campaigns/campaign-landing-page.js
@@ -1,14 +1,10 @@
 import { action } from '@ember/object';
 import Controller from '@ember/controller';
-import { tracked } from '@glimmer/tracking';
 
 export default class CampaignLandingPageController extends Controller {
 
-  @tracked isLoading = false;
-
   @action
   startCampaignParticipation() {
-    this.isLoading = true;
     return this.transitionToRoute('campaigns.start-or-resume', this.model.code, {
       queryParams: { hasUserSeenLandingPage: true },
     });
