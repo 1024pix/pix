f074f0a0d389e252656e6bf4b9090bff944252ed
jbuget <jbuget@gmail.com>
2020-10-20 23:01:14 +0200

    Fix ESLint error `no-unbound` from eslint-plugin-ember

--- admin/.eslintrc.js
+++ admin/.eslintrc.js
@@ -50,8 +50,8 @@ module.exports = {
 
         // this can be removed once the following is fixed
         // https://github.com/mysticatea/eslint-plugin-node/issues/77
-        'node/no-unpublished-require': 'off'
-      })
-    }
-  ]
+        'node/no-unpublished-require': 'off',
+      }),
+    },
+  ],
 };
