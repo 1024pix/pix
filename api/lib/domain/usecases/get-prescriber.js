aec1ba5b5c2566e8fa8b4a18802b1da7d6adba5c
lisequesnel <quesnel.lise@gmail.com>
2020-10-21 11:55:18 +0200

    [API] Create if needed UserOrgaSettings when get prescriber

--- api/lib/domain/usecases/get-prescriber.js
+++ api/lib/domain/usecases/get-prescriber.js
@@ -1,3 +1,19 @@
-module.exports = function getPrescriber({ userId, prescriberRepository }) {
+const { UserNotMemberOfOrganizationError } = require('../errors');
+const _ = require('lodash');
+
+module.exports = async function getPrescriber({ userId, prescriberRepository, membershipRepository, userOrgaSettingsRepository }) {
+
+  const memberships = await membershipRepository.findByUserId({ userId });
+  if (_.isEmpty(memberships)) {
+    throw new UserNotMemberOfOrganizationError(`L’utilisateur ${userId} n’est membre d’aucune organisation.`);
+  }
+
+  const userOrgaSettings = await userOrgaSettingsRepository.findOneByUserId(userId);
+
+  if (_.isEmpty(userOrgaSettings)) {
+    const currentOrganization = memberships[0].organization;
+    await userOrgaSettingsRepository.create(userId, currentOrganization.id);
+  }
+
   return prescriberRepository.getPrescriber(userId);
 };
