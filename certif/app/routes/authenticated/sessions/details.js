ac90243622309133da001e8664f44a28eb915315
Anne-G <ang.schall@gmail.com>
2020-10-21 14:46:31 +0200

    [Certif] Introduce isUserFromSco notion

--- certif/app/routes/authenticated/sessions/details.js
+++ certif/app/routes/authenticated/sessions/details.js
@@ -1,16 +1,19 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class SessionsDetailsRoute extends Route {
+  @service currentUser;
+
   async model(params) {
     const session = await this.store.findRecord('session', params.session_id);
     await session.certificationCandidates;
-    const certificationCenter = this.modelFor('authenticated');
+    const isUserFromSco = this.currentUser.isFromSco;
     const featureToggles = this.store.peekRecord('feature-toggle', 0);
     const isCertifPrescriptionScoEnabled = featureToggles.certifPrescriptionSco;
 
     return {
       session,
-      isCertificationCenterSco: certificationCenter.isSco,
+      isUserFromSco,
       isCertifPrescriptionScoEnabled,
     };
   }
