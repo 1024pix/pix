3b3aa6291beae99fe76ee10b4ea3d61c3d6d1990
Benjamin Petetot <bpetetot@gmail.com>
2020-10-22 16:15:41 +0200

    [API] Update CampaignReport serializer

--- api/lib/infrastructure/serializers/jsonapi/campaign-report-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/campaign-report-serializer.js
@@ -3,7 +3,12 @@ const { Serializer } = require('jsonapi-serializer');
 module.exports = {
   serialize(reports) {
     return new Serializer('campaign-report', {
-      attributes: ['participationsCount', 'sharedParticipationsCount'],
+      attributes: ['participationsCount', 'sharedParticipationsCount', 'stages'],
+      stages: {
+        ref: 'id',
+        included: true,
+        attributes: ['title', 'message', 'threshold'],
+      },
     }).serialize(reports);
   },
 };
