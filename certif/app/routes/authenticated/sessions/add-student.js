4477ef46fbfe045f04ab6b901c3b0c3de463e83d
Anne-G <ang.schall@gmail.com>
2020-10-22 14:43:22 +0200

    [Certif] Give all session to add-student-list compo

--- certif/app/routes/authenticated/sessions/add-student.js
+++ certif/app/routes/authenticated/sessions/add-student.js
@@ -2,10 +2,11 @@ import Route from '@ember/routing/route';
 
 export default class AuthenticatedSessionsDetailsAddStudentRoute extends Route {
   async model(params) {
+    const session = await this.store.findRecord('session', params.session_id);
     const { id: certificationCenterId } = this.modelFor('authenticated');
     const students = await this.store.findAll('student',
       { adapterOptions : { certificationCenterId } },
     );
-    return { sessionId: params.session_id, students };
+    return { session, students };
   }
 }
