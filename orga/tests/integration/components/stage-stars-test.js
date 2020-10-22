0c01b7dc38d98330c132d9365d88eb7d1aae166f
Laurent <laurent.lucas@pix.fr>
2020-10-22 15:47:13 +0200

    [ORGA] Add stages in participation list

---
+++ orga/tests/integration/components/stage-stars-test.js
@@ -0,0 +1,54 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | stage-stars', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('should render stars for given stages', async function(assert) {
+    // given
+    this.set('result', 50);
+    this.set('stages', [{ threshold: 20 }, { threshold: 70 }]);
+
+    // when
+    await render(hbs`<StageStars @result={{this.result}} @stages={{this.stages}} />`);
+
+    const acquiredStars = this.element.querySelectorAll('.pix-stars__acquired');
+    const unacquiredStars = this.element.querySelectorAll('.pix-stars__unacquired');
+
+    // then
+    assert.equal(acquiredStars.length, 1);
+    assert.equal(unacquiredStars.length, 1);
+  });
+
+  test('should render a star only when threshold > 0 and reached', async function(assert) {
+    // given
+    this.set('result', 50);
+    this.set('stages', [{ threshold: 0 },{ threshold: 20 }, { threshold: 70 }]);
+
+    // when
+    await render(hbs`<StageStars @result={{this.result}} @stages={{this.stages}} />`);
+
+    const acquiredStars = this.element.querySelectorAll('.pix-stars__acquired');
+    const unacquiredStars = this.element.querySelectorAll('.pix-stars__unacquired');
+
+    // then
+    assert.equal(acquiredStars.length, 1);
+    assert.equal(unacquiredStars.length, 1);
+  });
+
+  test('should render a screen reader message', async function(assert) {
+    // given
+    this.set('result', 50);
+    this.set('stages', [{ threshold: 20 }, { threshold: 70 }]);
+
+    // when
+    await render(hbs`<StageStars @result={{this.result}} @stages={{this.stages}} />`);
+
+    const srOnly = this.element.querySelector('.sr-only');
+
+    // then
+    assert.equal(srOnly.textContent.trim(), '1 étoiles sur 2');
+  });
+});
