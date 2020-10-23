909da5f7173a076c3eb853b2707a1ec20f8531f6
alexandrecoin <alexandre.pc.coin@gmail.com>
2020-10-23 17:11:27 +0200

    Add @formatjs/intl-pluralrules

--- mon-pix/app/app.js
+++ mon-pix/app/app.js
@@ -4,6 +4,10 @@ import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 import { InitSentryForEmber } from '@sentry/ember';
 
+import '@formatjs/intl-pluralrules/polyfill';
+import '@formatjs/intl-pluralrules/locale-data/en';
+import '@formatjs/intl-pluralrules/locale-data/fr';
+
 if (config.sentry.enabled) {
   InitSentryForEmber();
 }
