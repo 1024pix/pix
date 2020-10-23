8d7e2131a708a3c506adeef7594af0b06317ed42
lisequesnel <quesnel.lise@gmail.com>
2020-10-23 17:26:55 +0200

    [API] Give URL to find target profile organizations

--- api/lib/infrastructure/serializers/jsonapi/target-profile-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/target-profile-serializer.js
@@ -3,7 +3,17 @@ const { Serializer } = require('jsonapi-serializer');
 module.exports = {
   serialize(targetProfiles, meta) {
     return new Serializer('target-profile', {
-      attributes: ['name', 'outdated', 'isPublic', 'organizationId'],
+      attributes: ['name', 'outdated', 'isPublic', 'organizationId', 'organizations'],
+      organizations: {
+        ref: 'id',
+        ignoreRelationshipData: true,
+        nullIfMissing: true,
+        relationshipLinks: {
+          related(record, current, parent) {
+            return `/api/target-profiles/${parent.id}/organizations`;
+          },
+        },
+      },
       meta,
     }).serialize(targetProfiles);
   },
