cadc86d8e231761ce2e0f15c8b996451eef115b3
Antoine Ceol <antoine.ceol@gmail.com>
2020-10-26 23:17:55 +0100

    Add enrolled candidates nominal test

--- certif/tests/integration/components/enrolled-candidates-test.js
+++ certif/tests/integration/components/enrolled-candidates-test.js
@@ -5,16 +5,25 @@ import { hbs } from 'ember-cli-htmlbars';
 
 import config from 'pix-certif/config/environment';
 
-module.only('Integration | Component | enrolled-candidates', function(hooks) {
+module('Integration | Component | enrolled-candidates', function(hooks) {
   setupRenderingTest(hooks);
 
-  const CERTIFICATION_CANDIDATES_TABLE = 'certification-candidates-table tbody';
-  const CERTIFICATION_CANDIDATES_ACTION_DELETE = 'certification-candidates-actions';
-  const DELETE_BUTTON_DOM = 'certification-candidates-actions__delete-button';
-  const DELETE_BUTTON_DOM_DISABLED = `${DELETE_BUTTON_DOM}--disabled`;
-  const ADD_BUTTON = 'certification-candidates-add-button__text';
+  const CERTIFICATION_CANDIDATES_TABLE_SELECTOR = 'certification-candidates-table tbody';
+  const CERTIFICATION_CANDIDATES_ACTION_DELETE_SELECTOR = 'certification-candidates-actions';
+  const DELETE_BUTTON_SELECTOR = 'certification-candidates-actions__delete-button';
+  const DELETE_BUTTON_DISABLED_SELECTOR = `${DELETE_BUTTON_SELECTOR}--disabled`;
+  const ADD_BUTTON_SELECTOR = 'certification-candidates-add-button__text';
   const EXTERNAL_ID_COLUMN_SELECTOR = 'panel-candidate__externalId__';
   const RESULT_RECIPIENT_EMAIL_COLUMN_SELECTOR = 'panel-candidate__result-recipient-email__';
+  const BIRTHDATE_COLUMN_SELECTOR = 'panel-candidate__birthdate__';
+  const LAST_NAME_COLUMN_SELECTOR = 'panel-candidate__lastName__';
+  const FIRST_NAME_COLUMN_SELECTOR = 'panel-candidate__firstName__';
+  const BIRTH_CITY_COLUMN_SELECTOR = 'panel-candidate__birthCity__';
+  const BIRTH_PROVINCE_CODE_COLUMN_SELECTOR = 'panel-candidate__birthProvinceCode__';
+  const BIRTH_COUNTRY_SELECTOR = 'panel-candidate__birthCountry__';
+  const EMAIL_SELECTOR = 'panel-candidate__email__';
+  const EXTRA_TIME_SELECTOR = 'panel-candidate__extraTimePercentage__';
+
   const ft = config.APP.FT_IS_RESULT_RECIPIENT_EMAIL_VISIBLE;
 
   hooks.before(() => {
@@ -23,6 +32,34 @@ module.only('Integration | Component | enrolled-candidates', function(hooks) {
 
   hooks.after(() => {
     config.APP.FT_IS_RESULT_RECIPIENT_EMAIL_VISIBLE = ft;
+  });
+
+  test('it display candidates information', async function(assert) {
+
+    const candidate = _buildCertificationCandidate({
+      externalId: 'externalId',
+      birthdate: new Date('1980-07-01'),
+      isLinked: false,
+      extraTimePercentage: 0.1,
+    });
+    const certificationCandidates = [candidate];
+
+    this.set('certificationCandidates', certificationCandidates);
+
+    await render(hbs`
+      <EnrolledCandidates @sessionId="1" @certificationCandidates={{certificationCandidates}}>
+      </EnrolledCandidates>
+    `);
+
+    assert.dom(`[data-test-id=${EXTERNAL_ID_COLUMN_SELECTOR}${candidate.id}]`).hasText(candidate.externalId);
+    assert.dom(`[data-test-id=${BIRTHDATE_COLUMN_SELECTOR}${candidate.id}]`).hasText('01/07/1980');
+    assert.dom(`[data-test-id=${LAST_NAME_COLUMN_SELECTOR}${candidate.id}]`).hasText(candidate.lastName);
+    assert.dom(`[data-test-id=${FIRST_NAME_COLUMN_SELECTOR}${candidate.id}]`).hasText(candidate.firstName);
+    assert.dom(`[data-test-id=${BIRTH_CITY_COLUMN_SELECTOR}${candidate.id}]`).hasText(candidate.birthCity);
+    assert.dom(`[data-test-id=${BIRTH_PROVINCE_CODE_COLUMN_SELECTOR}${candidate.id}]`).hasText(candidate.birthProvinceCode);
+    assert.dom(`[data-test-id=${BIRTH_COUNTRY_SELECTOR}${candidate.id}]`).hasText(candidate.birthCountry);
+    assert.dom(`[data-test-id=${EMAIL_SELECTOR}${candidate.id}]`).hasText(candidate.email);
+    assert.dom(`[data-test-id=${EXTRA_TIME_SELECTOR}${candidate.id}]`).hasText('10 %');
   });
 
   test('it display candidates with delete disabled button if linked', async function(assert) {
@@ -34,18 +71,17 @@ module.only('Integration | Component | enrolled-candidates', function(hooks) {
 
     this.set('certificationCandidates', certificationCandidates);
 
-    // Template block usage:
     await render(hbs`
       <EnrolledCandidates @sessionId="1" @certificationCandidates={{certificationCandidates}}>
       </EnrolledCandidates>
     `);
 
-    assert.dom(`.${CERTIFICATION_CANDIDATES_TABLE} tr`).isVisible({ count: 3 });
-    assert.dom(`.${CERTIFICATION_CANDIDATES_TABLE} tr .${CERTIFICATION_CANDIDATES_ACTION_DELETE} button`).isVisible({ count: 3 });
+    assert.dom(`.${CERTIFICATION_CANDIDATES_TABLE_SELECTOR} tr`).isVisible({ count: 3 });
+    assert.dom(`.${CERTIFICATION_CANDIDATES_TABLE_SELECTOR} tr .${CERTIFICATION_CANDIDATES_ACTION_DELETE_SELECTOR} button`).isVisible({ count: 3 });
 
-    assert.dom(`.${CERTIFICATION_CANDIDATES_TABLE} tr:nth-child(1) .${CERTIFICATION_CANDIDATES_ACTION_DELETE} button`).hasClass(DELETE_BUTTON_DOM);
-    assert.dom(`.${CERTIFICATION_CANDIDATES_TABLE} tr:nth-child(2) .${CERTIFICATION_CANDIDATES_ACTION_DELETE} button`).hasClass(DELETE_BUTTON_DOM_DISABLED);
-    assert.dom(`.${CERTIFICATION_CANDIDATES_TABLE} tr:nth-child(3) .${CERTIFICATION_CANDIDATES_ACTION_DELETE} button`).hasClass(DELETE_BUTTON_DOM);
+    assert.dom(`.${CERTIFICATION_CANDIDATES_TABLE_SELECTOR} tr:nth-child(1) .${CERTIFICATION_CANDIDATES_ACTION_DELETE_SELECTOR} button`).hasClass(DELETE_BUTTON_SELECTOR);
+    assert.dom(`.${CERTIFICATION_CANDIDATES_TABLE_SELECTOR} tr:nth-child(2) .${CERTIFICATION_CANDIDATES_ACTION_DELETE_SELECTOR} button`).hasClass(DELETE_BUTTON_DISABLED_SELECTOR);
+    assert.dom(`.${CERTIFICATION_CANDIDATES_TABLE_SELECTOR} tr:nth-child(3) .${CERTIFICATION_CANDIDATES_ACTION_DELETE_SELECTOR} button`).hasClass(DELETE_BUTTON_SELECTOR);
   });
 
   [
@@ -73,9 +109,9 @@ module.only('Integration | Component | enrolled-candidates', function(hooks) {
     `);
 
       if (buttonVisible) {
-        assert.dom(`.${ADD_BUTTON}`).isVisible();
+        assert.dom(`.${ADD_BUTTON_SELECTOR}`).isVisible();
       } else {
-        assert.dom(`.${ADD_BUTTON}`).isNotVisible();
+        assert.dom(`.${ADD_BUTTON_SELECTOR}`).isNotVisible();
       }
     }),
   );
