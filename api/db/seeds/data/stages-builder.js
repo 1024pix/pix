7aab5cff078dd4762c9825b3cca168a093dd1f1a
Benjamin Petetot <bpetetot@gmail.com>
2020-10-26 16:09:27 +0100

    [API] Change stage seed level

--- api/db/seeds/data/stages-builder.js
+++ api/db/seeds/data/stages-builder.js
@@ -8,7 +8,7 @@ module.exports = function stagesBuilder({ databaseBuilder }) {
 function _buildStagesForTargetProfileId(databaseBuilder, targetProfileId) {
   const stages = [
     { title: 'palier 1', message: 'Tu as le palier 1', threshold: 0, targetProfileId },
-    { title: 'palier 2', message: 'Tu as le palier 2', threshold: 20, targetProfileId },
+    { title: 'palier 2', message: 'Tu as le palier 2', threshold: 5, targetProfileId },
     { title: 'palier 3', message: 'Tu as le palier 3', threshold: 40, targetProfileId },
     { title: 'palier 4', message: 'Tu as le palier 4', threshold: 60, targetProfileId },
     { title: 'palier 5', message: 'Tu as le palier 5', threshold: 80, targetProfileId },
