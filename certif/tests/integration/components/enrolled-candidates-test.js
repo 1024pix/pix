ad368abd63a852dd0eaf43d2dbbe17bbcb431262
D0C702WH0 <jemyplu@gmail.com>
2020-10-26 12:40:38 +0100

    Hide add candidate button for sco session

--- certif/tests/integration/components/enrolled-candidates-test.js
+++ certif/tests/integration/components/enrolled-candidates-test.js
@@ -10,6 +10,7 @@ module('Integration | Component | enrolled-candidates', function(hooks) {
   const CERTIFICATION_CANDIDATES_ACTION_DELETE = 'certification-candidates-actions';
   const DELETE_BUTTON_DOM = 'certification-candidates-actions__delete-button';
   const DELETE_BUTTON_DOM_DISABLED = `${DELETE_BUTTON_DOM}--disabled`;
+  const ADD_BUTTON = 'certification-candidates-add-button__text';
 
   test('it display candidates with delete disabled button if linked', async function(assert) {
     const certificationCandidates = [
@@ -19,10 +20,13 @@ module('Integration | Component | enrolled-candidates', function(hooks) {
     ];
 
     this.set('certificationCandidates', certificationCandidates);
+    this.set('isUserFromSco', true);
 
-    // Template block usage:
     await render(hbs`
-      <EnrolledCandidates @sessionId="1" @certificationCandidates={{certificationCandidates}}>
+      <EnrolledCandidates
+        @sessionId="1"
+        @certificationCandidates={{this.certificationCandidates}}
+        @isUserFromSco={{this.isUserFromSco}}>
       </EnrolledCandidates>
     `);
 
@@ -33,4 +37,37 @@ module('Integration | Component | enrolled-candidates', function(hooks) {
     assert.dom(`.${CERTIFICATION_CANDIDATES_TABLE} tr:nth-child(2) .${CERTIFICATION_CANDIDATES_ACTION_DELETE} button`).hasClass(DELETE_BUTTON_DOM_DISABLED);
     assert.dom(`.${CERTIFICATION_CANDIDATES_TABLE} tr:nth-child(3) .${CERTIFICATION_CANDIDATES_ACTION_DELETE} button`).hasClass(DELETE_BUTTON_DOM);
   });
+
+  [
+    { isSco: true, toggle: true, buttonVisible: false },
+    { isSco: true, toggle: false, buttonVisible: true },
+    { isSco: false, toggle: false, buttonVisible: true },
+    { isSco: false, toggle: true, buttonVisible: true },
+  ].forEach(({ isSco, toggle, buttonVisible }) =>
+    test(`it does ${buttonVisible ? '' : 'not '}display candidates add button if user ${isSco ? '' : 'not '}sco and if feature toggle is ${toggle}`, async function(assert) {
+      const certificationCandidates = [];
+
+      this.set('certificationCandidates', certificationCandidates);
+      this.set('isUserFromSco', isSco);
+      this.set('isCertifPrescriptionScoEnabled', toggle);
+
+      // Template block usage:
+      await render(hbs`
+      <EnrolledCandidates
+        @sessionId="1"
+        @certificationCandidates={{this.certificationCandidates}}
+        @isUserFromSco={{this.isUserFromSco}}
+        @isCertifPrescriptionScoEnabled={{this.isCertifPrescriptionScoEnabled}}
+      >
+      </EnrolledCandidates>
+    `);
+
+      if (buttonVisible) {
+        assert.dom(`.${ADD_BUTTON}`).isVisible();
+      } else {
+        assert.dom(`.${ADD_BUTTON}`).isNotVisible();
+      }
+    }),
+  );
+
 });
