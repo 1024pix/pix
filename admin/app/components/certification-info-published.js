117c453a8c3e743721d30c6c1535533e73b81250
jbuget <jbuget@gmail.com>
2020-10-27 14:20:08 +0100

    [Admin] Clean up component <CertificationInfoPublished/>
    - move up template into /components folder
    - glimmerize component
    - rename argument "record" into "certification"

--- admin/app/components/certification-info-published.js
+++ admin/app/components/certification-info-published.js
@@ -1,14 +1,11 @@
-import Component from '@ember/component';
-import { computed } from '@ember/object';
+import Component from '@glimmer/component';
 
 export default class CertificationInfoPublished extends Component {
 
-  classNames = ['certification-informations__published'];
   classNameBindings = ['float:certification-informations__published-float'];
 
-  @computed('record.isPublished')
   get color() {
-    const value = this.record.isPublished;
+    const value = this.args.certification.isPublished;
     return value ? '#39B97A' : '#8090A5';
   }
 }
