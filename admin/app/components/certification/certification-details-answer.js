a8b318a2ac238d857ad05453dc303da003de878d
jbuget <jbuget@gmail.com>
2020-10-27 01:11:52 +0100

    [Admin] Clean up component <CertificationDetailsAnswer/>
    - move up template into /components folder
    - glimmerize component
    - replace emberx-select by ember-power-select component
    
    The logic of this component and its whole bunch is still a big ball of mud.

---
+++ admin/app/components/certification/certification-details-answer.js
@@ -0,0 +1,43 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import find from 'lodash/find';
+
+const options = [
+  { value: 'ok', label: 'Succès' },
+  { value: 'ko', label: 'Échec' },
+  { value: 'partially', label: 'Succès partiel' },
+  { value: 'timedout', label: 'Temps écoulé' },
+  { value: 'aband', label: 'Abandon' },
+  { value: 'skip', label: 'Neutralisée' },
+];
+
+export default class CertificationDetailsAnswer extends Component {
+
+  @tracked selectedOption = null;
+  @tracked hasJuryResult = false;
+
+  constructor() {
+    super(...arguments);
+    this.resultOptions = options;
+    this.selectedOption = this.getOption(this.args.answer.result);
+  }
+
+  getOption(resultValue) {
+    return find(options, { value: resultValue });
+  }
+
+  get resultClass() {
+    return this.hasJuryResult ? 'answer-result jury' : 'answer-result';
+  }
+
+  @action
+  selectOption(selected) {
+    const answer = this.args.answer;
+    const newResult = (selected.value !== answer.result) ? selected.value : null;
+    answer.jury = newResult;
+    this.selectedOption = newResult ? this.getOption(newResult) : this.getOption(this.args.answer.result);
+    this.hasJuryResult = !!newResult;
+    this.args.onUpdateRate();
+  }
+}
