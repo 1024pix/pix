f40d107dbe55233af401b7293d0b99dcefad51bf
jbuget <jbuget@gmail.com>
2020-10-28 01:02:29 +0100

    Fixup: certification-info-competences

--- admin/tests/unit/components/certification-info-competences-test.js
+++ admin/tests/unit/components/certification-info-competences-test.js
@@ -1,18 +1,27 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import createGlimmerComponent from '../../helpers/create-glimmer-component';
 
 module('Unit | Component | certification-info-competences', function(hooks) {
   setupTest(hooks);
 
+  let component;
+
+  hooks.beforeEach(function() {
+    component = createGlimmerComponent('component:certification-info-competences');
+  });
+
   test('it computes indexed values correctly', function(assert) {
-    // given
-    const component = this.owner.factoryFor('component:certification-info-competences').create();
+    component.args = {
+      competences: [{ 'index':'1.1', value:'a competence', score:16, level:2 }, { 'index':'3.3', value:'another competence', score:42, level:5 }, { 'index':'5.2', value:'and another competence', score:37, level:4 }],
+    };
+    const expected = { 'scores':[16, null, null, null, null, null, null, null, null, 42, null, null, null, null, null, 37], 'levels':[2, null, null, null, null, null, null, null, null, 5, null, null, null, null, null, 4] };
 
     // when
-    component.set('competences', [{ 'index':'1.1', value:'a competence', score:16, level:2 }, { 'index':'3.3', value:'another competence', score:42, level:5 }, { 'index':'5.2', value:'and another competence', score:37, level:4 }]);
+    const actual = component.indexedValues;
 
     // then
-    assert.deepEqual(component.get('indexedValues'), { 'scores':[16, null, null, null, null, null, null, null, null, 42, null, null, null, null, null, 37], 'levels':[2, null, null, null, null, null, null, null, null, 5, null, null, null, null, null, 4] });
+    assert.deepEqual(actual, expected);
   });
 
 });
