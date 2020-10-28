8b11802ac1fa23d4b93294b3d0934746ac54e592
Mathieu Gilet <mathieu.gilet@zenika.com>
2020-10-28 13:49:00 +0100

    [API] Create table authentication-methods

---
+++ api/db/migrations/20201028134814_create_table_authentication_methods.js
@@ -0,0 +1,22 @@
+const TABLE_NAME = 'authentication-methods';
+
+exports.up = async (knex) => {
+  await knex.schema.createTable(TABLE_NAME, (t) => {
+    t.increments('id').primary();
+    t.integer('userId').references('users.id').index();
+    t.string('identityProvider').notNullable();
+    t.jsonb('authenticationComplement');
+    t.string('externalIdentifier');
+    t.dateTime('createdAt').notNullable().defaultTo(knex.fn.now());
+    t.dateTime('updatedAt').notNullable().defaultTo(knex.fn.now());
+
+    t.unique(['identityProvider', 'externalIdentifier']);
+    t.unique(['identityProvider', 'userId']);
+  });
+
+  return knex.raw('ALTER TABLE "authentication-methods" ADD CONSTRAINT "authentication_methods_identityProvider_check" CHECK ( "identityProvider" IN (\'PIX\', \'GAR\', \'POLE_EMPLOI\') )');
+};
+
+exports.down = (knex) => {
+  return knex.schema.dropTable(TABLE_NAME);
+};
