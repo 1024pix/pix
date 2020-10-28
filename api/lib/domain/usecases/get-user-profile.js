21b92ce452b253ccf7b25a3979469614aafa9ddc
melanieb <melanie.boudard@gmail.com>
2020-10-28 16:17:04 +0100

    [API] Add usecase to get the user profile
    It is the same usecase for get all scorecards but return pixScore and Scorecards

---
+++ api/lib/domain/usecases/get-user-profile.js
@@ -0,0 +1,38 @@
+const Scorecard = require('../models/Scorecard');
+const _ = require('lodash');
+
+module.exports = async function getUserProfile({
+  userId,
+  competenceRepository,
+  competenceEvaluationRepository,
+  knowledgeElementRepository,
+  locale,
+}) {
+  const [knowledgeElementsGroupedByCompetenceId, competencesWithArea, competenceEvaluations] = await Promise.all([
+    knowledgeElementRepository.findUniqByUserIdGroupedByCompetenceId({ userId }),
+    competenceRepository.listPixCompetencesOnly({ locale }),
+    competenceEvaluationRepository.findByUserId(userId),
+  ]);
+
+  const scorecards =  _.map(competencesWithArea, (competence) => {
+    const competenceId = competence.id;
+    const knowledgeElementsForCompetence = knowledgeElementsGroupedByCompetenceId[competenceId];
+    const competenceEvaluation = _.find(competenceEvaluations, { competenceId });
+
+    return Scorecard.buildFrom({
+      userId,
+      knowledgeElements: knowledgeElementsForCompetence,
+      competence,
+      competenceEvaluation,
+    });
+  });
+
+  const pixScore = _.sumBy(scorecards, 'earnedPix');
+
+  return {
+    id: userId,
+    pixScore,
+    scorecards,
+  };
+};
+
