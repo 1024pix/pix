3ea73a3cb6745fba499c5c1f357c1426a132fbd0
Xavier Carron <xavier.carron@pix.fr>
2020-10-29 10:28:36 +0100

    [FEATURE][PE] mise à jour des données mocker pour le partage de résultat de campagne (PIX-1537)

--- api/lib/domain/events/handle-campaign-participation-results-sending.js
+++ api/lib/domain/events/handle-campaign-participation-results-sending.js
@@ -13,45 +13,55 @@ async function handleCampaignParticipationResultsSending({
   if (event.isAssessment && organization.isPoleEmploi) {
 
     const resultsToSend = {
-      resultatIndividu: {
-        campagne: {
-          nom: 'Campagne Pôle Emploi',
-          dateDebut: new Date('2020-01-01'),
-          dateFin: new Date('2020-02-01'),
-          typeCampagne: 'EVALUATION',
-          idCampagne: 11223344,
-          codeCampagne: 'CODEPE123',
-          URLCampagne: 'https://app.pix.fr/campagnes/CODEPE123',
-          nomOrganisme: 'Pix',
-          typeOrganisme: 'externe',
+      campagne: {
+        nom: 'Campagne Pôle Emploi',
+        dateDebut: new Date('2020-01-01'),
+        dateFin: new Date('2020-02-01'),
+        type: 'EVALUATION',
+        idCampagne: 11223344,
+        codeCampagne: 'CODEPE123',
+        urlCampagne: 'https://app.pix.fr/campagnes/CODEPE123',
+        nomOrganisme: 'Pix',
+        typeOrganisme: 'externe',
+      },
+      individu: {
+        nom: 'Bonneau',
+        prenom: 'Jean',
+      },
+      test: {
+        etat: 4,
+        progression: 100,
+        typeTest: 'DI',
+        referenceExterne: 55667788,
+        dateDebut: new Date('2020-01-02'),
+        dateProgression: new Date('2020-01-03'),
+        dateValidation: new Date('2020-01-03'),
+        evaluationCible: 62.47,
+        uniteEvaluation: 'A',
+        elementsEvalues: [{
+          libelle: 'Gérer des données',
+          categorie: 'competence',
+          type: 'competence Pix',
+          domaineRattachement: 'Information et données',
+          nbSousElements: 3,
+          evaluation: {
+            scoreObtenu: 66.6,
+            uniteScore: 'A',
+            nbSousElementValide: 2,
+          },
         },
-        individu: {
-          nom: 'Bonneau',
-          prenom: 'Jean',
-        },
-        test: {
-          etat: 4,
-          progression: 100,
-          typeTest: 'DI',
-          referenceExterne: 55667788,
-          dateDebut: new Date('2020-01-02'),
-          dateModification: new Date('2020-01-03'),
-          dateValidation: new Date('2020-01-03'),
-          evaluationCible: 42.5,
-          uniteValidation: 'A',
-          elementsEvalues: [{
-            libelle: 'Gérer des données',
-            categorie: 'compétence',
-            type: 'compétence Pix',
-            domaineRattachement: 'Information et données',
-            nbSousElements: 3,
-            evaluation: {
-              scoreObtenu: 14.2,
-              uniteScore: 'A',
-              nbSousElementValide: 2,
-            },
-          }],
-        },
+        {
+          libelle: 'Gérer des données 2',
+          categorie: 'competence 2',
+          type: 'competence Pix 2',
+          domaineRattachement: 'Information et données',
+          nbSousElements: 5,
+          evaluation: {
+            scoreObtenu: 60.0,
+            uniteScore: 'B',
+            nbSousElementValide: 3,
+          },
+        }],
       },
     };
     console.log(JSON.stringify(resultsToSend));
