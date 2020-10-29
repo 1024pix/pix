480b297370cfcde2f89c881b4b12426e1c9b30c7
Laura Bergoens <laura.bergoens@pix.fr>
2020-10-29 17:08:15 +0100

    fix: setup unique constraint to avoid dups

---
+++ api/db/migrations/20201029134518_add-unique-constraint-organizationid-targetprofileid-on-target-profile-shares.js
@@ -0,0 +1,23 @@
+exports.up = async function(knex) {
+  await knex.with('all_target_profile_shares_ranked',
+    (qb) => {
+      qb.select([
+        'target-profile-shares.id',
+        knex.raw('ROW_NUMBER() OVER (PARTITION BY ??, ?? ORDER BY ??) AS rank', [
+          'target-profile-shares.organizationId',
+          'target-profile-shares.targetProfileId',
+          'target-profile-shares.createdAt',
+        ]),
+      ])
+        .from('target-profile-shares')
+        .join('organizations', 'organizations.id', 'target-profile-shares.organizationId');
+    })
+    .from('target-profile-shares')
+    .whereRaw('id = ANY((SELECT id FROM all_target_profile_shares_ranked WHERE rank > 1))')
+    .del();
+  return knex.schema.table('target-profile-shares', (table) => table.unique(['organizationId', 'targetProfileId']));
+};
+
+exports.down = async function(knex) {
+  return knex.schema.table('target-profile-shares', (table) => table.dropUnique(['organizationId', 'targetProfileId']));
+};
