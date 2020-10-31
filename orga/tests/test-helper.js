8b9a991211b51ac72e97385d70c787faf504e3a8
Laura Bergoens <laura.bergoens@pix.fr>
2020-10-31 09:58:41 +0100

    Separate jobs within orga and split files for linting

--- orga/tests/test-helper.js
+++ orga/tests/test-helper.js
@@ -1,7 +1,7 @@
 import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
+import start from 'ember-exam/test-support/start';
 import NotificationMessageService from 'ember-cli-notifications/services/notifications';
 
 NotificationMessageService.reopen({
