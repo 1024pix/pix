f2301a7391044da19e400dfa9276f34762447e15
Laura Bergoens <laura.bergoens@pix.fr>
2020-10-31 10:31:13 +0100

    Separate jobs within admin and split files for linting

--- admin/tests/test-helper.js
+++ admin/tests/test-helper.js
@@ -1,10 +1,7 @@
 import Application from 'pix-admin/app';
 import config from 'pix-admin/config/environment';
 import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
-import loadEmberExam from 'ember-exam/test-support/load';
-
-loadEmberExam();
+import start from 'ember-exam/test-support/start';
 
 setApplication(Application.create(config.APP));
 
