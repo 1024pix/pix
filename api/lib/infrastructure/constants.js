480d6ef53f26427d154cbbdb9e6428705e2fd53c
yahya-youness <you@octo.com>
2020-11-03 00:19:04 +0100

    [API] chunk insertion schooling registration db
    In order to serve a large file and do not use a lot of memory, we chunk the insertion in database by 1000 by default

--- api/lib/infrastructure/constants.js
+++ api/lib/infrastructure/constants.js
@@ -3,4 +3,5 @@ const settings = require('../config');
 module.exports = {
   CONCURRENCY_HEAVY_OPERATIONS: settings.infra.concurrencyForHeavyOperations,
   CHUNK_SIZE_CAMPAIGN_RESULT_PROCESSING: settings.infra.chunkSizeForCampaignResultProcessing,
+  CHUNK_SIZE_SCHOOLING_REGISTRATION_DATA_PROCESSING: settings.infra.chunkSizeForSchoolingRegistrationDataProcessing,
 };
