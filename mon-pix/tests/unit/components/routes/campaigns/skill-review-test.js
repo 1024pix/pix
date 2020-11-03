8a4237fb1c36874fd0d19fd8c8e7644cee18480f
alexandrecoin <alexandre.pc.coin@gmail.com>
2020-11-03 09:32:02 +0100

    Refactor tests
    Add skill-review integration test
    Refactor controller to component unit test
    Update skill-review unit test to match async campaignParticipation property update

--- mon-pix/tests/unit/components/routes/campaigns/skill-review-test.js
+++ mon-pix/tests/unit/components/routes/campaigns/skill-review-test.js
@@ -3,22 +3,20 @@ import { describe, it, beforeEach } from 'mocha';
 import { setupTest } from 'ember-mocha';
 import sinon from 'sinon';
 import EmberObject from '@ember/object';
+import createGlimmerComponent from '../../../../helpers/create-glimmer-component';
 
-describe('Unit | Controller | Campaigns | Evaluation | Skill Review', function() {
+describe('Unit | component | Campaigns | Evaluation | Skill Review', function() {
 
   setupTest();
 
-  let controller;
+  let component;
 
   beforeEach(function() {
-    controller = this.owner.lookup('controller:campaigns.assessment.skill-review');
 
-    const setStub = sinon.stub();
-
-    controller.set('model', {
+    const model = {
       campaignParticipation: EmberObject.create({
         isShared: false,
-        set: setStub,
+        set: sinon.stub().resolves({}),
         save: sinon.stub().resolves({}),
         campaignParticipationResult: EmberObject.create({
           masteryPercentage: 50,
@@ -36,36 +34,40 @@ describe('Unit | Controller | Campaigns | Evaluation | Skill Review', function()
         save: sinon.stub().resolves({}),
         codeCampaign: 'codeCampaign',
       },
+    };
+
+    component = createGlimmerComponent('component:routes/campaigns/assessment/skill-review', {
+      model,
     });
-    controller.set('showButtonToShareResult', true);
-    controller.transitionToRoute = sinon.stub();
+
+    component.transitionToRoute = sinon.stub();
   });
 
   describe('#shareCampaignParticipation', function() {
-    it('should set isShared to true', function() {
+    it('should set isShared to true', async function() {
       // when
-      controller.actions.shareCampaignParticipation.call(controller);
+      await component.actions.shareCampaignParticipation.call(component);
 
       // then
-      expect(controller.model.campaignParticipation.isShared).to.equal(true);
+      expect(component.args.model.campaignParticipation.isShared).to.equal(true);
     });
   });
 
   describe('#improvementCampaignParticipation', function() {
     it('should save the campaignParticipation to start the improvement', async function() {
       // when
-      await controller.actions.improvementCampaignParticipation.call(controller);
+      await component.actions.improvementCampaignParticipation.call(component);
 
       // then
-      sinon.assert.calledWith(controller.get('model.campaignParticipation.save'), { adapterOptions: { beginImprovement: true } });
+      sinon.assert.calledWith(component.args.model.campaignParticipation.save, { adapterOptions: { beginImprovement: true } });
     });
 
     it('should redirect to campaigns.start-or-resume', async function() {
       // when
-      await controller.actions.improvementCampaignParticipation.call(controller);
+      await component.actions.improvementCampaignParticipation.call(component);
 
       // then
-      sinon.assert.calledWith(controller.transitionToRoute, 'campaigns.start-or-resume');
+      sinon.assert.calledWith(component.transitionToRoute, 'campaigns.start-or-resume');
     });
   });
 
@@ -74,10 +76,10 @@ describe('Unit | Controller | Campaigns | Evaluation | Skill Review', function()
     it('should showCleaCompetences when campaignParticipationResult has a clea badge', function() {
       // given
       const cleaBadge = { id: 111 };
-      controller.model.campaignParticipation.campaignParticipationResult.cleaBadge = cleaBadge;
+      component.args.model.campaignParticipation.campaignParticipationResult.cleaBadge = cleaBadge;
 
       // when
-      const shouldShowCleaCompetences = controller.showCleaCompetences;
+      const shouldShowCleaCompetences = component.showCleaCompetences;
 
       // then
       expect(shouldShowCleaCompetences).to.equal(true);
@@ -85,10 +87,10 @@ describe('Unit | Controller | Campaigns | Evaluation | Skill Review', function()
 
     it('should not show clea competence when there is no cleaBadge', function() {
       // given
-      controller.model.campaignParticipation.campaignParticipationResult.cleaBadge = undefined;
+      component.args.model.campaignParticipation.campaignParticipationResult.cleaBadge = undefined;
 
       // when
-      const shouldShowCleaCompetences = controller.showCleaCompetences;
+      const shouldShowCleaCompetences = component.showCleaCompetences;
 
       // then
       expect(shouldShowCleaCompetences).to.equal(false);
@@ -100,10 +102,10 @@ describe('Unit | Controller | Campaigns | Evaluation | Skill Review', function()
     it('should show badges when acquired', function() {
       // given
       const badges = [{ id: 33, isAcquired: true }];
-      controller.model.campaignParticipation.campaignParticipationResult.campaignParticipationBadges = badges;
+      component.args.model.campaignParticipation.campaignParticipationResult.campaignParticipationBadges = badges;
 
       // when
-      const shouldShowBadges = controller.showBadges;
+      const shouldShowBadges = component.showBadges;
 
       // then
       expect(shouldShowBadges).to.equal(true);
@@ -112,10 +114,10 @@ describe('Unit | Controller | Campaigns | Evaluation | Skill Review', function()
     it('should not show badges when not acquired', function() {
       // given
       const badges = [{ id: 33, isAcquired: false }];
-      controller.model.campaignParticipation.campaignParticipationResult.campaignParticipationBadges = badges;
+      component.args.model.campaignParticipation.campaignParticipationResult.campaignParticipationBadges = badges;
 
       // when
-      const shouldShowBadges = controller.showBadges;
+      const shouldShowBadges = component.showBadges;
 
       // then
       expect(shouldShowBadges).to.equal(false);
@@ -124,10 +126,10 @@ describe('Unit | Controller | Campaigns | Evaluation | Skill Review', function()
     it('should not show badges when none', function() {
       // given
       const badges = [];
-      controller.model.campaignParticipation.campaignParticipationResult.campaignParticipationBadges = badges;
+      component.args.model.campaignParticipation.campaignParticipationResult.campaignParticipationBadges = badges;
 
       // when
-      const shouldShowBadges = controller.showBadges;
+      const shouldShowBadges = component.showBadges;
 
       // then
       expect(shouldShowBadges).to.equal(false);
@@ -139,10 +141,10 @@ describe('Unit | Controller | Campaigns | Evaluation | Skill Review', function()
     it('should only display acquired badges', function() {
       // given
       const badges = [{ id: 33, isAcquired: true }, { id: 34, isAcquired: false }];
-      controller.model.campaignParticipation.campaignParticipationResult.campaignParticipationBadges = badges;
+      component.args.model.campaignParticipation.campaignParticipationResult.campaignParticipationBadges = badges;
 
       // when
-      const acquiredBadges = controller.acquiredBadges;
+      const acquiredBadges = component.acquiredBadges;
 
       // then
       expect(acquiredBadges).to.deep.equal([{ id: 33, isAcquired: true }]);
