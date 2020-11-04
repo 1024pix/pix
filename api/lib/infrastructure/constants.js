bb066dff923d0144e3c69385ce57791c67ca4e2a
Jonathan Perret <j-github@jonathanperret.net>
2020-11-04 20:04:06 +0100

    [API] schooling-registrations-xml-service refactoring

--- api/lib/infrastructure/constants.js
+++ api/lib/infrastructure/constants.js
@@ -3,5 +3,5 @@ const settings = require('../config');
 module.exports = {
   CONCURRENCY_HEAVY_OPERATIONS: settings.infra.concurrencyForHeavyOperations,
   CHUNK_SIZE_CAMPAIGN_RESULT_PROCESSING: settings.infra.chunkSizeForCampaignResultProcessing,
-  CHUNK_SIZE_SCHOOLING_REGISTRATION_DATA_PROCESSING: settings.infra.chunkSizeForSchoolingRegistrationDataProcessing,
+  SCHOOLING_REGISTRATION_CHUNK_SIZE: settings.infra.chunkSizeForSchoolingRegistrationDataProcessing,
 };
