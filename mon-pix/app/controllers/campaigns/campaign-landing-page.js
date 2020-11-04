9e662f9a249f3139b931807e557022ef23feebe7
Jonas Grillères <grilleres.j@gmail.com>
2020-11-04 11:08:43 +0100

    Hide bottom presentation page

--- mon-pix/app/controllers/campaigns/campaign-landing-page.js
+++ mon-pix/app/controllers/campaigns/campaign-landing-page.js
@@ -1,7 +1,12 @@
 import { action } from '@ember/object';
 import Controller from '@ember/controller';
+import ENV from 'mon-pix/config/environment';
 
 export default class CampaignLandingPageController extends Controller {
+
+  get isNotPixConcours() {
+    return this.model.isTypeAssessment && ENV.APP.IS_PIX_CONCOURS === 'false';
+  }
 
   @action
   startCampaignParticipation() {
