18dce5817d6d9779dea112d17eef945864468070
ajoanny <ajoanny@octo.com>
2020-11-05 10:39:58 +0100

    [API] extract the contruction on CSV line in another class

--- api/lib/infrastructure/serializers/csv/campaign-profile-collection-export.js
+++ api/lib/infrastructure/serializers/csv/campaign-profile-collection-export.js
@@ -1,10 +1,8 @@
 const _ = require('lodash');
-const moment = require('moment');
 const bluebird = require('bluebird');
 const csvSerializer = require('./csv-serializer');
 const constants = require('../../constants');
-const EMPTY_ARRAY = [];
-const NOT_SHARED = 'NA';
+const CampaignProfileCollectionResultLine = require('../../exports/campaigns/campaign-profile-collection-result-line');
 
 class CampaignProfileCollectionExport {
 
@@ -73,88 +71,11 @@ class CampaignProfileCollectionExport {
     let csvLines = '';
     for (const placementProfile of placementProfiles) {
       const campaignParticipationResultData = campaignParticipationResultDatas.find(({ userId }) => userId === placementProfile.userId);
-      const csvLine = this._buildLine({ campaignParticipationResultData, placementProfile });
-      csvLines = csvLines.concat(csvLine);
+
+      const line = new CampaignProfileCollectionResultLine(this.campaign, this.organization, campaignParticipationResultData, this.competences, placementProfile);
+      csvLines = csvLines.concat(line.toCsvLine());
     }
     return csvLines;
-  }
-
-  _buildLine({ campaignParticipationResultData, placementProfile }) {
-    const line =  [
-      this.organization.name,
-      this.campaign.id,
-      this.campaign.name,
-      campaignParticipationResultData.participantLastName,
-      campaignParticipationResultData.participantFirstName,
-      ...(this._getDivisionColumn(campaignParticipationResultData)),
-      ...(this._getStudentNumberColumn(campaignParticipationResultData)),
-      ...(this._getIdPixLabelColumn(campaignParticipationResultData)),
-      this._yesOrNo(campaignParticipationResultData.isShared),
-      this._getSharedAtColumn(campaignParticipationResultData),
-      this._getTotalEarnedPixColumn(placementProfile.userCompetences, campaignParticipationResultData.isShared),
-      this._getIsCertifiableColumn(placementProfile, campaignParticipationResultData.isShared),
-      this._getCompetencesCountColumn(placementProfile, campaignParticipationResultData.isShared),
-      ...this._competenceColumns(this.competences, placementProfile.userCompetences, campaignParticipationResultData.isShared),
-    ];
-
-    return csvSerializer.serializeLine(line);
-  }
-
-  _getDivisionColumn(campaignParticipationResultData) {
-    return this.organization.isSco ? [campaignParticipationResultData.division] : EMPTY_ARRAY;
-  }
-
-  _getCompetencesCountColumn(placementProfile, isShared) {
-    return isShared ? placementProfile.getCertifiableCompetencesCount() : NOT_SHARED;
-  }
-
-  _getIsCertifiableColumn(placementProfile, isShared) {
-    return isShared ? this._yesOrNo(placementProfile.isCertifiable()) : NOT_SHARED;
-  }
-
-  _getIdPixLabelColumn(campaignParticipationResultData) {
-    return this.idPixLabel ? [campaignParticipationResultData.participantExternalId] : EMPTY_ARRAY;
-  }
-
-  _getStudentNumberColumn(campaignParticipationResultData) {
-    const displayStudentNumber = this.organization.isSup && this.organization.isManagingStudents;
-
-    return displayStudentNumber ? [campaignParticipationResultData.studentNumber] : EMPTY_ARRAY;
-  }
-
-  _getSharedAtColumn(campaignParticipationResultData) {
-    return campaignParticipationResultData.isShared ? moment.utc(campaignParticipationResultData.sharedAt).format('YYYY-MM-DD') : NOT_SHARED;
-  }
-
-  _getTotalEarnedPixColumn(userCompetences, isShared) {
-    let totalEarnedPix = NOT_SHARED;
-    if (isShared)  {
-      totalEarnedPix = 0;
-      userCompetences.forEach(({ pixScore }) => {
-        totalEarnedPix += pixScore;
-      });
-    }
-
-    return totalEarnedPix;
-  }
-
-  _yesOrNo(value) {
-    return value ? 'Oui' : 'Non';
-  }
-
-  _competenceColumns(competencesList, userCompetences, isShared) {
-    const columns = [];
-    competencesList.forEach((competence) => {
-      const { estimatedLevel, pixScore } = userCompetences.find(({ id }) => id === competence.id);
-
-      if (isShared) {
-        columns.push(estimatedLevel, pixScore);
-      } else {
-        columns.push(NOT_SHARED, NOT_SHARED);
-      }
-    });
-
-    return columns;
   }
 
   _competenceColumnHeaders(competencesList) {
