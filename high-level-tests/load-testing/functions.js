91bc89ee85d778f7d7d1a1a5bdc6c98c707eda8e
GradedJestRisk <graded.jest.risk@gmail.com>
2020-11-08 14:25:34 +0100

    Load XML from external file

--- high-level-tests/load-testing/functions.js
+++ high-level-tests/load-testing/functions.js
@@ -1,9 +1,5 @@
 const faker = require('faker');
-
-module.exports = {
-  setupSignupFormData,
-  foundNextChallenge,
-};
+const fs = require('fs');
 
 function setupSignupFormData(context, events, done) {
   context.vars['firstName'] = faker.name.firstName();
@@ -17,3 +13,20 @@ function foundNextChallenge(context, next) {
   const continueLooping = !!context.vars.challengeId;
   return next(continueLooping);
 }
+
+const loadXMLFile = function(context, events, done) {
+
+  fs.readFile('./data/schooling-registration/1-user.xml', 'utf8', function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    context.vars['XML'] = data;
+    return done();
+  });
+};
+
+module.exports = {
+  setupSignupFormData,
+  foundNextChallenge,
+  loadXMLFile,
+};
