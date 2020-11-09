8e0e83d76b559cd53892ba64f02a207c86c87e7a
Benjamin Petetot <bpetetot@gmail.com>
2020-11-09 14:02:09 +0100

    [ORGA] Add tooltip for SCO Agriculture imports

--- orga/app/components/routes/authenticated/sco-students/list-items.js
+++ orga/app/components/routes/authenticated/sco-students/list-items.js
@@ -3,6 +3,7 @@ import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import ENV from 'pix-orga/config/environment';
+import { htmlSafe } from '@ember/string';
 
 export default class ListItems extends Component {
 
@@ -26,6 +27,10 @@ export default class ListItems extends Component {
 
   get urlToDownloadCsvTemplate() {
     return `${ENV.APP.API_HOST}/api/organizations/${this.currentUser.organization.id}/schooling-registrations/csv-template?accessToken=${this.session.data.authenticated.access_token}`;
+  }
+
+  get textTooltipForAgri() {
+    return htmlSafe('Quelques informations concernant l’import :<ul><li>Pour vos élèves : utiliser l’export FREGATA vers Pix et cliquer sur le bouton importer.</li><li>Pour vos apprentis : Pix importe tous les apprentis fin novembre au sein de votre espace Pix Orga. Les ajouts ponctuels d’apprentis se feront à partir de décembre, pour cela consulter la documentation Pix Orga et utiliser le modèle fourni.</li></ul>');
   }
 
   @action
