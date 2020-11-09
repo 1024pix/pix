7e05b151c55bef64ab76a40cb8623586c498418e
Xavier Carron <xavier.carron@pix.fr>
2020-11-09 10:13:05 +0100

    add migration unicity constraint organizationId nationalApprenticeId

---
+++ api/db/migrations/20201106093514_add_unicity_contraint_schooling_registration_on_apprenticeId_and_organizationId.js
@@ -0,0 +1,13 @@
+const TABLE_NAME = 'schooling-registrations';
+
+exports.up = function(knex) {
+  return knex.schema.table(TABLE_NAME, (table) => {
+    table.unique(['organizationId','nationalApprenticeId']);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.table(TABLE_NAME, (table) => {
+    table.dropUnique(['organizationId','nationalApprenticeId']);
+  });
+};
