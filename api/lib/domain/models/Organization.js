78ab71fa3f1480a63abb882604b91250b16bb0e6
Xavier Carron <xavier.carron@pix.fr>
2020-11-09 10:15:17 +0100

    add tags to Organization models

--- api/lib/domain/models/Organization.js
+++ api/lib/domain/models/Organization.js
@@ -68,6 +68,10 @@ class Organization {
   get isPoleEmploi() {
     return process.env['POLE_EMPLOI_ORGANIZATION_ID'] === this.id.toString();
   }
+
+  get hasApprentice() {
+    return this.isAgriculture;
+  }
 }
 
 Organization.types = types;
