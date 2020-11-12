4d2631329b94f0f81648b2f8d8921d09fb630c12
alexandrecoin <alexandre.pc.coin@gmail.com>
2020-11-12 17:46:37 +0100

    Update navbar content for Pix-Contest

--- mon-pix/app/components/navbar-header.js
+++ mon-pix/app/components/navbar-header.js
@@ -1,10 +1,15 @@
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
+import ENV from 'mon-pix/config/environment';
 
 export default class NavbarHeader extends Component {
   @service url;
 
   get isFrenchDomainExtension() {
     return this.url.isFrenchDomainExtension;
+  }
+
+  get isPixContest() {
+    return ENV.APP.IS_PIX_CONTEST === 'true';
   }
 }
