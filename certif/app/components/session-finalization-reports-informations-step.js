ca12e109edc853d97c98a32f8161568a5163d74f
Anne-G <ang.schall@gmail.com>
2020-11-12 15:06:19 +0100

    [Certif] Add examinerReportModal

--- certif/app/components/session-finalization-reports-informations-step.js
+++ certif/app/components/session-finalization-reports-informations-step.js
@@ -1,7 +1,17 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class SessionFinalizationReportsInformationsStep extends Component {
   textareaMaxLength = 500;
+  reportToEdit = null;
+
+  @tracked
+  showExaminerReportModal = false;
+
+  get myFeatureToggle() {
+    return true;
+  }
 
   get certifReportsAreNotEmpty() {
     return this.args.certificationReports.length !== 0;
@@ -19,6 +29,17 @@ export default class SessionFinalizationReportsInformationsStep extends Componen
 
   get headerCheckboxStatus() {
     return this.hasCheckedEverything ? 'checked' : this.hasCheckedSomething ? 'partial' : 'unchecked';
+  }
+
+  @action
+  async openExaminerReportModal(report) {
+    this.showExaminerReportModal = true;
+    this.reportToEdit = report;
+  }
+
+  @action
+  async closeExaminerReportModal() {
+    this.showExaminerReportModal = false;
   }
 
 }
