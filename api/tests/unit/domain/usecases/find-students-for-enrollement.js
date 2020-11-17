f0b975fdb61c36065edaa7173bd71cc3275ac5fa
Antoine Ceol <antoine.ceol@gmail.com>
2020-11-17 13:47:42 +0100

    [API] Remove check for organisation in students enrollment

--- api/tests/unit/domain/usecases/find-students-for-enrollement.js
+++ api/tests/unit/domain/usecases/find-students-for-enrollement.js
@@ -1,7 +1,6 @@
 const _ = require('lodash');
-const { expect, sinon, catchErr, domainBuilder } = require('../../../test-helper');
-const usecases = require('../../../../lib/domain/usecases');
-const { ForbiddenAccess, NotFoundError } = require('../../../../lib/domain/errors');
+const { expect, sinon, domainBuilder } = require('../../../test-helper');
+const { NotFoundError } = require('../../../../lib/domain/errors');
 const StudentForEnrollement = require('../../../../lib/domain/read-models/StudentForEnrollement');
 
 const findStudentsForEnrollement = require('../../../../lib/domain/usecases/find-students-for-enrollement');
@@ -19,10 +18,6 @@ describe('Unit | UseCase | find-students-for-enrollement', () => {
     findByOrganizationIdOrderByDivision: sinon.stub(),
   };
 
-  const certificationCenterMembershipRepository = {
-    doesUserHaveMembershipToCertificationCenter: sinon.stub(),
-  };
-
   const certificationCandidateRepository = {
     findBySessionId: sinon.stub(),
   };
@@ -34,9 +29,6 @@ describe('Unit | UseCase | find-students-for-enrollement', () => {
 
     organizationRepository.getIdByCertificationCenterId
       .withArgs(certificationCenter.id).resolves(organization.id);
-
-    certificationCenterMembershipRepository.doesUserHaveMembershipToCertificationCenter
-      .withArgs(userId, certificationCenter.id).resolves(true);
   });
 
   describe('when user has access to certification center', () => {
@@ -51,7 +43,6 @@ describe('Unit | UseCase | find-students-for-enrollement', () => {
           certificationCenterId,
           organizationRepository,
           schoolingRegistrationRepository,
-          certificationCenterMembershipRepository,
           certificationCandidateRepository,
         });
 
@@ -74,7 +65,6 @@ describe('Unit | UseCase | find-students-for-enrollement', () => {
         userId,
         certificationCenterId,
         sessionId,
-        certificationCenterMembershipRepository,
         organizationRepository,
         schoolingRegistrationRepository,
         certificationCandidateRepository,
@@ -82,8 +72,8 @@ describe('Unit | UseCase | find-students-for-enrollement', () => {
 
       // then
       const expectedEnrolledStudent = new StudentForEnrollement({ ...enrolledStudent, isEnrolled: true });
-      const exepectedEnrollableStudents = enrollableStudents.map((student) => new StudentForEnrollement({ ...student, isEnrolled: false }));
-      const expectedStudents = [ expectedEnrolledStudent, ...exepectedEnrollableStudents ];
+      const expectedEnrollableStudents = enrollableStudents.map((student) => new StudentForEnrollement({ ...student, isEnrolled: false }));
+      const expectedStudents = [ expectedEnrolledStudent, ...expectedEnrollableStudents ];
       expect(studentsFounds).to.be.deep.equal(expectedStudents);
     });
 
@@ -100,34 +90,12 @@ describe('Unit | UseCase | find-students-for-enrollement', () => {
           certificationCenterId,
           organizationRepository,
           schoolingRegistrationRepository,
-          certificationCenterMembershipRepository,
           certificationCandidateRepository,
         });
 
         // then
         expect(studentsFounds).to.be.deep.equal([]);
       });
-    });
-  });
-
-  describe('when user does not have access to certification center', () => {
-    it('should throw an access error', async () => {
-      // given
-      const wrongCertificationCenterId = 666;
-      certificationCenterMembershipRepository.doesUserHaveMembershipToCertificationCenter
-        .withArgs(userId, wrongCertificationCenterId).resolves(false);
-
-      // when
-      const error = await catchErr(usecases.findStudentsForEnrollement)({
-        userId,
-        certificationCenterId: wrongCertificationCenterId,
-        organizationRepository,
-        schoolingRegistrationRepository,
-        certificationCenterMembershipRepository,
-      });
-
-      // then
-      expect(error).to.be.instanceOf(ForbiddenAccess);
     });
   });
 
