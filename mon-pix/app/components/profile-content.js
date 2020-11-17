1306457a488aae302440c9ec50e26d4c1132cd01
alexandrecoin <alexandre.pc.coin@gmail.com>
2020-11-17 15:56:45 +0100

    Add send Pix Contest results button on homepage

--- mon-pix/app/components/profile-content.js
+++ mon-pix/app/components/profile-content.js
@@ -1,9 +1,18 @@
 import Component from '@glimmer/component';
 import ENV from 'mon-pix/config/environment';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class ProfileContent extends Component {
 
+  @service currentUser;
+
   get isPixContest() {
     return ENV.APP.IS_PIX_CONTEST === 'true';
+  }
+
+  @action
+  sendPixContestResults() {
+    this.currentUser.user.save({ adapterOptions: { finishPixContest: true } });
   }
 }
