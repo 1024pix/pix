9b99b656b8bc1922a0a94358a562264c52a865f4
lisequesnel <quesnel.lise@gmail.com>
2020-11-18 17:17:56 +0100

    [API] Create a tag POLE EMPLOI in seeds

--- api/db/seeds/data/tags-builder.js
+++ api/db/seeds/data/tags-builder.js
@@ -2,4 +2,5 @@ module.exports = function tagsBuilder({ databaseBuilder }) {
   databaseBuilder.factory.buildTag({ id: 1, name: 'AGRICULTURE' });
   databaseBuilder.factory.buildTag({ id: 2, name: 'PUBLIC' });
   databaseBuilder.factory.buildTag({ id: 3, name: 'PRIVE' });
+  databaseBuilder.factory.buildTag({ id: 4, name: 'POLE EMPLOI' });
 };
