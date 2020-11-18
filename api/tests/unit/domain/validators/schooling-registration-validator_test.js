df85735e7546689ab98aefaa721d241c55e4138d
Xavier Carron <xavier.carron@pix.fr>
2020-11-18 12:01:38 +0100

    [API] Unit tests to verify birthCountryCode and french birthCityCode

--- api/tests/unit/domain/validators/schooling-registration-validator_test.js
+++ api/tests/unit/domain/validators/schooling-registration-validator_test.js
@@ -1,5 +1,5 @@
 const { expect, catchErr } = require('../../../test-helper');
-const { checkValidation } = require('../../../../lib/domain/validators/schooling-registration-validator');
+const { checkValidation, FRANCE_COUNTRY_CODE } = require('../../../../lib/domain/validators/schooling-registration-validator');
 
 describe('Unit | Domain | Schooling Registration validator', () => {
   context('#checkValidation', () => {
@@ -13,7 +13,7 @@ describe('Unit | Domain | Schooling Registration validator', () => {
       birthCity: 'city1',
       birthCityCode: 'city1',
       birthProvinceCode: '10',
-      birthCountryCode: '12345',
+      birthCountryCode: '99125',
       status: 'ST',
       MEFCode: 'ABCDE',
       division: 'EDCBA',
@@ -70,12 +70,36 @@ describe('Unit | Domain | Schooling Registration validator', () => {
     });
 
     context('birthCountryCode', () => {
+      context('is valid', () => {
+        it('respects INSEE Code, only number', async () => {
+          try {
+            checkValidation({ ...validAttributes, birthCountryCode: '99123' });
+          } catch (e) {
+            expect.fail('SchoolingRegistration is valid birthCountryCode respect INSEE code');
+          }  
+        });
+      });
+
       it('throw an error when birthCountryCode has not 5 characters', async () => {
         const error = await catchErr(checkValidation)({ ...validAttributes, birthCountryCode: '123456' });
 
         expect(error.key).to.equal('birthCountryCode');
         expect(error.why).to.equal('length');
         expect(error.limit).to.equal(5);
+      });
+
+      it('throw an error when birthCountryCode not start with 99', async () => {
+        const error = await catchErr(checkValidation)({ ...validAttributes, birthCountryCode: '88123' });
+
+        expect(error.key).to.equal('birthCountryCode');
+        expect(error.why).to.equal('not_valid_insee_code');
+      });
+
+      it('throw an error when birthCountryCode contains letter', async () => {
+        const error = await catchErr(checkValidation)({ ...validAttributes, birthCountryCode: '2B122' });
+
+        expect(error.key).to.equal('birthCountryCode');
+        expect(error.why).to.equal('not_valid_insee_code');
       });
     });
 
@@ -142,10 +166,8 @@ describe('Unit | Domain | Schooling Registration validator', () => {
       });
 
       it('is valid when birthCity is undefined and birthCountry is France', async () => {
-        const franceCountryCode = '99100';
-
         try {
-          checkValidation({ ...validAttributes, birthCountryCode: franceCountryCode, birthCity: undefined });
+          checkValidation({ ...validAttributes, birthCountryCode: FRANCE_COUNTRY_CODE, birthCityCode: '51430', birthCity: undefined });
         } catch (e) {
           expect.fail('SchoolingRegistration is valid when birthCity is undefined and birthCountry is France');
         }
@@ -153,30 +175,65 @@ describe('Unit | Domain | Schooling Registration validator', () => {
     });
 
     context('birthCityCode', () => {
-      it('throw an error when birthCityCode is not France and birthCity is undefined', async () => {
-        const franceCountryCode = '99100';
+      context('is valid', () => {
+        context('when birthCountryCode equal to France', () => {
 
-        const error = await catchErr(checkValidation)({ ...validAttributes, birthCountryCode: franceCountryCode, birthCityCode: undefined });
+          it('respects INSEE Code, with one letter', async () => {
+            try {
+              checkValidation({ ...validAttributes, birthCountryCode: FRANCE_COUNTRY_CODE, birthCityCode: '2B125' });
+            } catch (e) {
+              expect.fail('SchoolingRegistration is valid birthCityCode respect INSEE code, like Corsica');
+            }  
+          });
+    
+          it('respects INSEE Code, only number', async () => {
+            try {
+              checkValidation({ ...validAttributes, birthCountryCode: FRANCE_COUNTRY_CODE, birthCityCode: '13125' });
+            } catch (e) {
+              expect.fail('SchoolingRegistration is valid birthCityCode respect INSEE code, like Corsica');
+            }  
+          });    
+        });
 
-        expect(error.key).to.equal('birthCityCode');
-        expect(error.why).to.equal('required');
+        context('when birthCountryCode not equal to France', () => {
+          it('is valid with birthCityCode undefined', async () => {
+            try {
+              checkValidation({ ...validAttributes, birthCountryCode: '99125', birthCityCode: undefined });
+            } catch (e) {
+              expect.fail('SchoolingRegistration is valid when birthCity is undefined and birthCountry is not France');
+            }
+          });
+        });
       });
 
-      it('is valid when birthCityCode is undefined and birthCountry is not France', async () => {
-
-        try {
-          checkValidation({ ...validAttributes, birthCountryCode: '12345', birthCityCode: undefined });
-        } catch (e) {
-          expect.fail('SchoolingRegistration is valid when birthCity is undefined and birthCountry is not France');
-        }
-      });
-
-      it('throw an error when birthCityCode has not 5 characters', async () => {
-        const error = await catchErr(checkValidation)({ ...validAttributes, birthCityCode: '1' });
-
-        expect(error.key).to.equal('birthCityCode');
-        expect(error.why).to.equal('length');
-        expect(error.limit).to.equal(5);
+      context('is invalid', () => {
+        context('when birthCountryCode is equal to France', () => {
+          it('throw an error with a birthCityCode of 5 characters', async () => {
+            const error = await catchErr(checkValidation)({ ...validAttributes, birthCountryCode: FRANCE_COUNTRY_CODE, birthCityCode: '123456' });
+    
+            expect(error.key).to.equal('birthCityCode');
+            expect(error.why).to.equal('length');
+            expect(error.limit).to.equal(5);
+          });
+    
+          it('throws an error with a birthCityCode which has a letter not in second character', async () => {
+            const error = await catchErr(checkValidation)({ ...validAttributes, birthCountryCode: FRANCE_COUNTRY_CODE, birthCityCode: '21B22' });
+    
+            expect(error.key).to.equal('birthCityCode');
+            expect(error.why).to.equal('not_valid_insee_code');
+          });
+        });
+        
+        context('when birthCountryCode is not equal to France', () => {
+          it('throws an error with a birthCityCode of 256 characters', async () => {
+            const stringOf256Char = 'hZSJIp6WBhnZFxsnTxEQo1oSoWkRDSB8nQsbScrK9IfAmVGb1PFNdX333k6Tsn6YKHfebdRg2VryzQcY06GTm1sYIN9Y3B0uy1ZsZIFpZ3cQNLxnawaUfVQFylq1GFba9LNDowH7lISfn7HJbdf3hNawofdCbVNgRdw7ZAN8XdggDJUgyAs91GpQ6vCkrxa08AMYTI8QClkhUVazVGgwndtwN4EBG23K2AfayHKWVi6jSlPOgUrx4tgSAcxELxW2';
+            const error = await catchErr(checkValidation)({ ...validAttributes, birthCityCode: stringOf256Char });
+    
+            expect(error.key).to.equal('birthCityCode');
+            expect(error.why).to.equal('max_length');
+            expect(error.limit).to.equal(255);
+          });
+        });
       });
     });
   });
