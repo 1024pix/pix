4898a103c4c39dda1294a88f9688b9daa3be860e
lisequesnel <quesnel.lise@gmail.com>
2020-11-20 16:10:46 +0100

    [API] Get learning content with /admin/target-profiles/id

---
+++ api/lib/infrastructure/serializers/jsonapi/target-profile-with-learning-content-serializer.js
@@ -0,0 +1,20 @@
+const { Serializer } = require('jsonapi-serializer');
+
+module.exports = {
+  serialize(targetProfiles, meta) {
+    return new Serializer('target-profile', {
+      attributes: ['name', 'outdated', 'isPublic', 'organizationId', 'organizations'],
+      organizations: {
+        ref: 'id',
+        ignoreRelationshipData: true,
+        nullIfMissing: true,
+        relationshipLinks: {
+          related(record, current, parent) {
+            return `/api/target-profiles/${parent.id}/organizations`;
+          },
+        },
+      },
+      meta,
+    }).serialize(targetProfiles);
+  },
+};
