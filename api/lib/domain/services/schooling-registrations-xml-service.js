108eadde600e6e95142188109cc804f2329d80fb
ajoanny <ajoanny@octo.com>
2020-11-20 12:11:02 +0100

    [API] refacto extract stream creation in a parser class

--- api/lib/domain/services/schooling-registrations-xml-service.js
+++ api/lib/domain/services/schooling-registrations-xml-service.js
@@ -1,16 +1,9 @@
 const { FileValidationError, SameNationalStudentIdInFileError } = require('../errors');
-const fs = require('fs');
-const StreamZip = require('node-stream-zip');
-const FileType = require('file-type');
-const iconv = require('iconv-lite');
 const moment = require('moment');
 const xml2js = require('xml2js');
-const sax = require('sax');
 const saxPath = require('saxpath');
-const xmlEncoding = require('xml-buffer-tostring').xmlEncoding;
 const { isEmpty, isNil, each, isUndefined, noop } = require('lodash');
 
-const DEFAULT_FILE_ENCODING = 'iso-8859-15';
 const DIVISION = 'D';
 const NODE_ORGANIZATION_UAI = '/BEE_ELEVES/PARAMETRES';
 const NODES_SCHOOLING_REGISTRATIONS = '/BEE_ELEVES/DONNEES/*/*';
@@ -19,71 +12,56 @@ const STRUCTURE_ELEVE_ELEMENT = '<STRUCTURES_ELEVE';
 const NO_STUDENTS_IMPORTED_FROM_INVALID_FILE = 'Aucun élève n’a pu être importé depuis ce fichier. Vérifiez que le fichier est conforme.';
 const UAI_SIECLE_FILE_NOT_MATCH_ORGANIZATION_UAI = 'Aucun étudiant n’a été importé. L’import n’est pas possible car l’UAI du fichier SIECLE ne correspond pas à celui de votre établissement. En cas de difficulté, contactez support.pix.fr.';
 
-const Stream = require('stream');
+const XMLStreamer = require('../../infrastructure/utils/xml/xml-streamer');
 
-class StreamPipe extends Stream.Transform {
-
-  constructor() {
-    super();
-  }
-
-  _transform(chunk, enc, cb) {
-    this.push(chunk);
-    cb();
-  }
-}
+let XmlStreamer;
 
 module.exports = {
   extractSchoolingRegistrationsInformationFromSIECLE,
 };
 
 async function extractSchoolingRegistrationsInformationFromSIECLE(path, organization) {
-  const encoding = await _detectEncodingFromFirstLineOfSiecleFile(path);
+  XmlStreamer = await XMLStreamer.create(path)
 
-  const UAIFromSIECLE = await _extractUAI(path, encoding);
+  const UAIFromSIECLE = await _extractUAI(path);
   const UAIFromUserOrganization = organization.externalId;
 
   if (UAIFromSIECLE !== UAIFromUserOrganization) {
     throw new FileValidationError(UAI_SIECLE_FILE_NOT_MATCH_ORGANIZATION_UAI);
   }
 
-  const schoolingRegistrations = await _processSiecleFile(path, encoding);
+  const schoolingRegistrations = await _processSiecleFile(path);
 
   return schoolingRegistrations.filter((schoolingRegistration) => !isUndefined(schoolingRegistration.division));
 }
 
-function _extractUAI(path, encoding) {
-  return _withSiecleStream(path, encoding, _UAIextractor);
+function _extractUAI() {
+  return _withSiecleStream(_UAIextractor);
 }
 
-async function _processSiecleFile(path, encoding) {
-  return _withSiecleStream(path, encoding, _registrationExtractor);
+async function _processSiecleFile() {
+  return _withSiecleStream(_registrationExtractor);
 }
 
-async function _withSiecleStream(path, encoding, extractor) {
-  const rawStream = await createStream(path);
-  const siecleFileStream = rawStream.pipe(iconv.decodeStream(encoding));
+async function _withSiecleStream(extractor) {
+  const siecleFileStream = await XmlStreamer.getStream();
 
   try {
     return await new Promise((resolve, reject_) => {
-      const saxParser = sax.createStream(true);
-
       const reject = (e) => {
-        saxParser.removeAllListeners();
-        saxParser.on('error', noop);
+        siecleFileStream.removeAllListeners();
+        siecleFileStream.on('error', noop);
         return reject_(e);
       };
 
-      saxParser.on('error', () => {
+      siecleFileStream.on('error', () => {
         reject(new FileValidationError(NO_STUDENTS_IMPORTED_FROM_INVALID_FILE));
       });
 
-      extractor(saxParser, resolve, reject);
-
-      siecleFileStream.pipe(saxParser);
+      extractor(siecleFileStream, resolve, reject);
     });
   } finally {
-    rawStream.destroy();
+    XmlStreamer.destroyStream();
   }
 }
 
@@ -148,35 +126,6 @@ function _mapStudentInformationToSchoolingRegistration(nodeData) {
   };
 }
 
-async function _readFirstLineFromFile(path) {
-  const readStream = await createStream(path);
-  return new Promise((resolve, reject) => {
-    const lineEndingCharacter = '\n';
-    const BOM = 0xFEFF;
-    let value = '';
-    let position = 0;
-    let index;
-    readStream.on('data', (chunk) => {
-      index = chunk.indexOf(lineEndingCharacter);
-      value += chunk;
-      if (index === -1) {
-        position += chunk.length;
-      } else {
-        position += index;
-        readStream.destroy();
-      }
-    })
-      .on('close', () => {
-        const rawFirstLine = value;
-        const lineStartsAt = rawFirstLine.charCodeAt(0) === BOM ? 1 : 0;
-        const lineEndsAt = position;
-        const firstLine = rawFirstLine.slice(lineStartsAt, lineEndsAt);
-        resolve(firstLine);
-      })
-      .on('error', reject);
-  });
-}
-
 function _isSchoolingRegistrationNode(xmlNode) {
   return xmlNode.startsWith(ELEVE_ELEMENT) || xmlNode.startsWith(STRUCTURE_ELEVE_ELEMENT);
 }
@@ -191,11 +140,6 @@ function _isStudentEligible(studentData, mapSchoolingRegistrationsByStudentId) {
 function _getValueFromParsedElement(obj) {
   if (isNil(obj)) return null;
   return (Array.isArray(obj) && !isEmpty(obj)) ? obj[0] : obj;
-}
-
-async function _detectEncodingFromFirstLineOfSiecleFile(path) {
-  const firstLine = await _readFirstLineFromFile(path);
-  return xmlEncoding(Buffer.from(firstLine)) || DEFAULT_FILE_ENCODING;
 }
 
 function _processStudentsNodes(mapSchoolingRegistrationsByStudentId, nodeData, nationalStudentIds) {
@@ -225,34 +169,4 @@ function _throwIfNationalStudentIdIsDuplicatedInFile(nationalStudentId, national
   if (nationalStudentId && nationalStudentIds.indexOf(nationalStudentId) !== -1) {
     throw new SameNationalStudentIdInFileError(nationalStudentId);
   }
-}
-
-function _unzippedStream(path) {
-  const zip = new StreamZip({ file: path });
-  const stream = new StreamPipe();
-
-  zip.on('entry', (entry) => {
-    zip.stream(entry, (err, stm) => {
-
-      if (!entry.name.includes('/')) {
-        stm.pipe(stream);
-      }
-    });
-  });
-
-  return stream;
-}
-
-async function createStream(path) {
-  const { ext } = await FileType.fromFile(path);
-  let stream = null;
-  if (ext === 'zip') {
-    stream = _unzippedStream(path);
-  }
-  else {
-    stream = fs.createReadStream(path);
-  }
-
-  return stream;
-
 }
