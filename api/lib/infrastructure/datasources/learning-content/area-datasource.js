9f47744914231bea20928b6dee1ad3c0be84afba
Thomasevano <thomas-evano@hotmail.fr>
2020-11-23 17:39:31 +0100

    Remove airtable references in area-datasource

--- api/lib/infrastructure/datasources/learning-content/area-datasource.js
+++ api/lib/infrastructure/datasources/learning-content/area-datasource.js
@@ -2,32 +2,7 @@ const datasource = require('./datasource');
 
 module.exports = datasource.extend({
 
-  modelName: 'Area',
-
-  tableName: 'Domaines',
-
-  usedFields: [
-    'id persistant',
-    'Code',
-    'Nom',
-    'Titre',
-    'Titre fr-fr',
-    'Titre en-us',
-    'Competences (identifiants) (id persistant)',
-    'Couleur',
-  ],
-
-  fromAirTableObject(airtableRecord) {
-    return {
-      id: airtableRecord.get('id persistant'),
-      code: airtableRecord.get('Code'),
-      name: airtableRecord.get('Nom'),
-      titleFrFr: airtableRecord.get('Titre fr-fr') || airtableRecord.get('Titre'),
-      titleEnUs: airtableRecord.get('Titre en-us') || airtableRecord.get('Titre'),
-      competenceIds: airtableRecord.get('Competences (identifiants) (id persistant)'),
-      color: airtableRecord.get('Couleur'),
-    };
-  },
+  modelName: 'areas',
 
   async findByRecordIds(areaIds) {
     const areas = await this.list();
