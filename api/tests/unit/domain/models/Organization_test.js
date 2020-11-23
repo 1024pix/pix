a5143d5eb54a037625822c00987c5b5414648c4f
Xavier Carron <xavier.carron@pix.fr>
2020-11-23 17:45:26 +0100

    [API] unit test organization getter

--- api/tests/unit/domain/models/Organization_test.js
+++ api/tests/unit/domain/models/Organization_test.js
@@ -160,4 +160,39 @@ describe('Unit | Domain | Models | Organization', () => {
       expect(organization.isPoleEmploi).is.true;
     });
   });
+
+  describe('get#isCFA', () => {
+    context('when organization is not SCO', ()  =>  {
+      it('should return false when the organization has the "CFA" tag', () => {
+        // given
+        const tag = domainBuilder.buildTag({ name: Tag.CFA });
+        const organization = domainBuilder.buildOrganization({ type: 'SUP', tags: [tag] });
+
+        // when / then
+        expect(organization.isCFA).is.false;
+      });
+    });
+
+    context('when organization is SCO', ()  =>  {
+      it('should return true when organization is of type SCO and has the "CFA" tag', () => {
+        // given
+        const tag1 = domainBuilder.buildTag({ name: Tag.CFA });
+        const tag2 = domainBuilder.buildTag({ name: 'OTHER' });
+        const organization = domainBuilder.buildOrganization({ type: 'SCO', tags: [tag1, tag2] });
+
+        // when / then
+        expect(organization.isCFA).is.true;
+      });
+
+      it('should return false when when organization is of type SCO and has not the "CFA" tag', () => {
+        // given
+        const tag1 = domainBuilder.buildTag({ name: 'To infinity…and beyond!' });
+        const tag2 = domainBuilder.buildTag({ name: 'OTHER' });
+        const organization = domainBuilder.buildOrganization({ type: 'SCO', tags: [tag1, tag2] });
+
+        // when / then
+        expect(organization.isCFA).is.false;
+      });
+    });
+  });
 });
