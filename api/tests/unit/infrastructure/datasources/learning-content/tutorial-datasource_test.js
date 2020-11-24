97ac5b03b63a3f3eb57ba20c1a73733336baa7c3
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2020-11-24 09:47:59 +0100

    Remove airtable references in tutorial-datasource

--- api/tests/unit/infrastructure/datasources/learning-content/tutorial-datasource_test.js
+++ api/tests/unit/infrastructure/datasources/learning-content/tutorial-datasource_test.js
@@ -1,41 +1,24 @@
 const _ = require('lodash');
 const { expect, sinon } = require('../../../../test-helper');
-const airtable = require('../../../../../lib/infrastructure/airtable');
+const lcms = require('../../../../../lib/infrastructure/lcms');
 const tutorialDatasource = require('../../../../../lib/infrastructure/datasources/learning-content/tutorial-datasource');
-const tutorialAirtableDataObjectFixture = require('../../../../tooling/fixtures/infrastructure/tutorialAirtableDataObjectFixture');
-const tutorialRawAirTableFixture = require('../../../../tooling/fixtures/infrastructure/tutorialRawAirtableFixture');
 const cache = require('../../../../../lib/infrastructure/caches/learning-content-cache');
-const makeAirtableFake = require('../../../../tooling/airtable-builder/make-airtable-fake');
 
-describe('Unit | Infrastructure | Datasource | Airtable | TutorialDatasource', () => {
+describe('Unit | Infrastructure | Datasource | Learning Content | TutorialDatasource', () => {
 
   beforeEach(() => {
     sinon.stub(cache, 'get').callsFake((key, generator) => generator());
   });
 
-  describe('#fromAirTableObject', () => {
-
-    it('should create a Tutorial from the AirtableRecord', () => {
-      // given
-      const expectedTuto = tutorialAirtableDataObjectFixture();
-
-      // when
-      const tuto = tutorialDatasource.fromAirTableObject(tutorialRawAirTableFixture());
-
-      // then
-      expect(tuto).to.deep.equal(expectedTuto);
-    });
-  });
-
   describe('#findByRecordIds', () => {
 
-    it('should return an array of airtable tutorial data objects', function() {
+    it('should return an array of tutorial data objects', function() {
       // given
-      const rawTutorial1 = tutorialRawAirTableFixture({ id: 'FAKE_REC_ID_RAW_TUTORIAL_1' });
-      const rawTutorial2 = tutorialRawAirTableFixture({ id: 'FAKE_REC_ID_RAW_TUTORIAL_2' });
-      const rawTutorial3 = tutorialRawAirTableFixture({ id: 'FAKE_REC_ID_RAW_TUTORIAL_3' });
+      const rawTutorial1 = { id: 'FAKE_REC_ID_RAW_TUTORIAL_1' };
+      const rawTutorial2 = { id: 'FAKE_REC_ID_RAW_TUTORIAL_2' };
+      const rawTutorial3 = { id: 'FAKE_REC_ID_RAW_TUTORIAL_3' };
       const records = [rawTutorial1, rawTutorial2, rawTutorial3];
-      sinon.stub(airtable, 'findRecords').callsFake(makeAirtableFake(records));
+      sinon.stub(lcms, 'getLatestRelease').resolves({ tutorials: records });
 
       // when
       const promise = tutorialDatasource.findByRecordIds([rawTutorial1.id, rawTutorial3.id]);
@@ -44,7 +27,7 @@ describe('Unit | Infrastructure | Datasource | Airtable | TutorialDatasource', (
       return promise.then((foundTutorials) => {
         expect(foundTutorials).to.be.an('array');
         expect(_.map(foundTutorials, 'id')).to.deep.equal([rawTutorial1.id, rawTutorial3.id]);
-        expect(airtable.findRecords).to.have.been.calledWith('Tutoriels');
+        expect(lcms.getLatestRelease).to.have.been.called;
       });
     });
   });
