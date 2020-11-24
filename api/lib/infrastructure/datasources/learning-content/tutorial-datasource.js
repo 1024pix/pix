5ad01ce0cc1403a944b0bf6f635162cf1eb40fc4
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2020-11-24 09:47:59 +0100

    Remove airtable references in tutorial-datasource

--- api/lib/infrastructure/datasources/learning-content/tutorial-datasource.js
+++ api/lib/infrastructure/datasources/learning-content/tutorial-datasource.js
@@ -3,31 +3,7 @@ const datasource = require('./datasource');
 
 module.exports = datasource.extend({
 
-  modelName: 'Tutorial',
-
-  tableName: 'Tutoriels',
-
-  usedFields: [
-    'id persistant',
-    'Durée',
-    'Format',
-    'Lien',
-    'Source',
-    'Titre',
-    'Langue',
-  ],
-
-  fromAirTableObject(airtableRecord) {
-    return {
-      id: airtableRecord.get('id persistant'),
-      duration: airtableRecord.get('Durée'),
-      format: airtableRecord.get('Format'),
-      link: airtableRecord.get('Lien'),
-      source: airtableRecord.get('Source'),
-      title: airtableRecord.get('Titre'),
-      locale: airtableRecord.get('Langue'),
-    };
-  },
+  modelName: 'tutorials',
 
   async findByRecordIds(tutorialRecordIds) {
     const tutorials = await this.list();
