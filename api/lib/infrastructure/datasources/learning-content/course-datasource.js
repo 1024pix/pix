685b5d875d655699bc4eda075b2e237a9b4c444d
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2020-11-24 08:51:55 +0100

    Remove airtable references in course-datasource

--- api/lib/infrastructure/datasources/learning-content/course-datasource.js
+++ api/lib/infrastructure/datasources/learning-content/course-datasource.js
@@ -1,35 +1,7 @@
-const _ = require('lodash');
 const datasource = require('./datasource');
 
 module.exports = datasource.extend({
 
-  modelName: 'Course',
-
-  tableName: 'Tests',
-
-  usedFields: [
-    'id persistant',
-    'Nom',
-    'Description',
-    'Competence (id persistant)',
-    'Épreuves (id persistant)',
-    'Image',
-  ],
-
-  fromAirTableObject(airtableRecord) {
-    let imageUrl;
-    if (airtableRecord.get('Image')) {
-      imageUrl = airtableRecord.get('Image')[0].url;
-    }
-
-    return {
-      id: airtableRecord.get('id persistant'),
-      name: airtableRecord.get('Nom'),
-      description: airtableRecord.get('Description'),
-      competences: airtableRecord.get('Competence (id persistant)'),
-      challenges: _.reverse(airtableRecord.get('Épreuves (id persistant)')),
-      imageUrl,
-    };
-  },
+  modelName: 'courses',
 
 });
