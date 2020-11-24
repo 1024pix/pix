d6bac6b2ce522b5d891f355c5f9223454b7d0921
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2020-11-24 09:32:03 +0100

    Remove airtable references in tube-datasource

--- api/lib/infrastructure/datasources/learning-content/tube-datasource.js
+++ api/lib/infrastructure/datasources/learning-content/tube-datasource.js
@@ -3,36 +3,7 @@ const datasource = require('./datasource');
 
 module.exports = datasource.extend({
 
-  modelName: 'Tube',
-
-  tableName: 'Tubes',
-
-  usedFields: [
-    'id persistant',
-    'Nom',
-    'Titre',
-    'Description',
-    'Titre pratique fr-fr',
-    'Titre pratique en-us',
-    'Description pratique fr-fr',
-    'Description pratique en-us',
-    'Competences (id persistant)',
-  ],
-
-  fromAirTableObject(airtableRecord) {
-
-    return {
-      id: airtableRecord.get('id persistant'),
-      name: airtableRecord.get('Nom'),
-      title: airtableRecord.get('Titre'),
-      description: airtableRecord.get('Description'),
-      practicalTitleFrFr: airtableRecord.get('Titre pratique fr-fr'),
-      practicalTitleEnUs: airtableRecord.get('Titre pratique en-us'),
-      practicalDescriptionFrFr: airtableRecord.get('Description pratique fr-fr'),
-      practicalDescriptionEnUs: airtableRecord.get('Description pratique en-us'),
-      competenceId: _.head(airtableRecord.get('Competences (id persistant)')),
-    };
-  },
+  modelName: 'tubes',
 
   async findByNames(tubeNames) {
     const tubes = await this.list();
