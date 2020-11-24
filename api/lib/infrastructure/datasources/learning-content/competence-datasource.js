d8d462d80cebfd08bc0fa46c0cb094d22f713a1d
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2020-11-24 08:50:28 +0100

    Remove airtable references in competence-datasource

--- api/lib/infrastructure/datasources/learning-content/competence-datasource.js
+++ api/lib/infrastructure/datasources/learning-content/competence-datasource.js
@@ -2,39 +2,7 @@ const datasource = require('./datasource');
 
 module.exports = datasource.extend({
 
-  modelName: 'Competence',
-
-  tableName: 'Competences',
-
-  usedFields: [
-    'id persistant',
-    'Titre',
-    'Titre fr-fr',
-    'Titre en-us',
-    'Sous-domaine',
-    'Description',
-    'Description fr-fr',
-    'Description en-us',
-    'Domaine (id persistant)',
-    'Acquis (via Tubes) (id persistant)',
-    'Origine',
-  ],
-
-  fromAirTableObject(airtableRecord) {
-    return {
-      id: airtableRecord.get('id persistant'),
-      name: airtableRecord.get('Titre'),
-      nameFrFr: airtableRecord.get('Titre fr-fr') || airtableRecord.get('Titre'),
-      nameEnUs: airtableRecord.get('Titre en-us') || airtableRecord.get('Titre'),
-      index: airtableRecord.get('Sous-domaine'),
-      description: airtableRecord.get('Description'),
-      descriptionFrFr: airtableRecord.get('Description fr-fr') || airtableRecord.get('Description'),
-      descriptionEnUs: airtableRecord.get('Description en-us') || airtableRecord.get('Description'),
-      areaId: airtableRecord.get('Domaine (id persistant)') ? airtableRecord.get('Domaine (id persistant)')[0] : '',
-      skillIds: airtableRecord.get('Acquis (via Tubes) (id persistant)') || [],
-      origin: airtableRecord.get('Origine'),
-    };
-  },
+  modelName: 'competences',
 
   async findByRecordIds(competenceIds) {
     const competences = await this.list();
--- api/tests/unit/infrastructure/datasources/learning-content/competence-datasource_test.js
+++ api/tests/unit/infrastructure/datasources/learning-content/competence-datasource_test.js
@@ -1,40 +1,29 @@
-const { expect, sinon, domainBuilder } = require('../../../../test-helper');
+const { expect, sinon } = require('../../../../test-helper');
 const competenceDatasource = require('../../../../../lib/infrastructure/datasources/learning-content/competence-datasource');
-const airtable = require('../../../../../lib/infrastructure/airtable');
-const competenceRawAirTableFixture = require('../../../../tooling/fixtures/infrastructure/competenceRawAirTableFixture');
-const makeAirtableFake = require('../../../../tooling/airtable-builder/make-airtable-fake');
+const lcms = require('../../../../../lib/infrastructure/lcms');
+const cache = require('../../../../../lib/infrastructure/caches/learning-content-cache');
 
-describe('Unit | Infrastructure | Datasource | Airtable | CompetenceDatasource', () => {
+describe('Unit | Infrastructure | Datasource | Learning Content | CompetenceDatasource', () => {
 
-  describe('#fromAirTableObject', () => {
-
-    it('should create a Competence from the AirtableRecord', () => {
-      // given
-      const expectedCompetence = domainBuilder.buildCompetenceAirtableDataObject();
-
-      // when
-      const area = competenceDatasource.fromAirTableObject(competenceRawAirTableFixture());
-
-      // then
-      expect(area).to.deep.equal(expectedCompetence);
-    });
+  beforeEach(() => {
+    sinon.stub(cache, 'get').callsFake((key, generator) => generator());
   });
 
   describe('#findByRecordIds', () => {
 
-    it('should return an array of matching airtable competence data objects', async function() {
+    it('should return an array of matching competence data objects', async function() {
       // given
-      const rawCompetence1 = competenceRawAirTableFixture('RECORD_ID_RAW_COMPETENCE_1');
-      const rawCompetence2 = competenceRawAirTableFixture('RECORD_ID_RAW_COMPETENCE_2');
-      const rawCompetence3 = competenceRawAirTableFixture('RECORD_ID_RAW_COMPETENCE_3');
-      const rawCompetence4 = competenceRawAirTableFixture('RECORD_ID_RAW_COMPETENCE_4');
+      const rawCompetence1 = { id: 'RECORD_ID_RAW_COMPETENCE_1' };
+      const rawCompetence2 = { id: 'RECORD_ID_RAW_COMPETENCE_2' };
+      const rawCompetence3 = { id: 'RECORD_ID_RAW_COMPETENCE_3' };
+      const rawCompetence4 = { id: 'RECORD_ID_RAW_COMPETENCE_4' };
 
       const records = [rawCompetence1, rawCompetence2, rawCompetence3, rawCompetence4];
-      sinon.stub(airtable, 'findRecords').callsFake(makeAirtableFake(records));
+      sinon.stub(lcms, 'getLatestRelease').resolves({ competences: records });
       const expectedCompetenceIds = [
-        rawCompetence1.fields['id persistant'],
-        rawCompetence2.fields['id persistant'],
-        rawCompetence4.fields['id persistant'],
+        rawCompetence1.id,
+        rawCompetence2.id,
+        rawCompetence4.id,
       ];
 
       // when
@@ -45,10 +34,10 @@ describe('Unit | Infrastructure | Datasource | Airtable | CompetenceDatasource',
 
     it('should return an empty array when there are no objects matching the ids', async function() {
       // given
-      const rawCompetence1 = competenceRawAirTableFixture('RECORD_ID_RAW_COMPETENCE_1');
+      const rawCompetence1 = { id: 'RECORD_ID_RAW_COMPETENCE_1' };
 
       const records = [rawCompetence1];
-      sinon.stub(airtable, 'findRecords').callsFake(makeAirtableFake(records));
+      sinon.stub(lcms, 'getLatestRelease').resolves({ competences: records });
 
       // when
       const foundCompetences = await competenceDatasource.findByRecordIds(['some_other_id']);
