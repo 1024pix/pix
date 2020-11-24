b5ececd5d45bd1510dd3029098e58c61fc7af651
Thomasevano <thomas-evano@hotmail.fr>
2020-11-24 17:16:17 +0100

    Use lcms instead of airtable in tube-repository integration tests

--- api/tests/integration/infrastructure/repositories/tube-repository_test.js
+++ api/tests/integration/infrastructure/repositories/tube-repository_test.js
@@ -1,100 +1,179 @@
-const _ = require('lodash');
-const { expect, airtableBuilder, domainBuilder } = require('../../../test-helper');
-const cache = require('../../../../lib/infrastructure/caches/learning-content-cache');
+const { expect, mockLearningContent } = require('../../../test-helper');
 const Tube = require('../../../../lib/domain/models/Tube');
 const tubeRepository = require('../../../../lib/infrastructure/repositories/tube-repository');
 
 describe('Integration | Repository | tube-repository', () => {
 
-  afterEach(() => {
-    airtableBuilder.cleanAll();
-    return cache.flushAll();
-  });
-
   describe('#get', () => {
-    it('should return the tube without setting skills', async () => {
+    it('should return the tube', async () => {
       // given
-      const expectedTube = domainBuilder.buildTube();
-      delete expectedTube.skills;
-      const airtableTube = airtableBuilder.factory.buildTube.fromDomain({ domainTube: expectedTube });
-      airtableBuilder.mockLists({ tubes: [airtableTube] });
-
+      const expectedTube = new Tube({ 
+        id: 'recTube0', 
+        name: 'tubeName',
+        title: 'tubeTitle',
+        description: 'tubeDescription',
+        practicalTitle: 'translatedPracticalTitle',
+        practicalDescription: 'translatedPracticalDescription',
+        competenceId: 'recCompetence0',
+      });
+      const learningContent = {
+        tubes: [{ 
+          id: 'recTube0', 
+          name: 'tubeName',
+          title: 'tubeTitle',
+          description: 'tubeDescription',
+          practicalTitleFrFr: 'translatedPracticalTitle',
+          practicalDescriptionFrFr: 'translatedPracticalDescription',
+          competenceId: 'recCompetence0',
+        }],
+      };
+      mockLearningContent(learningContent);
+      
       // when
       const tube = await tubeRepository.get(expectedTube.id);
 
       // then
       expect(tube).to.be.instanceof(Tube);
-      expect(_.omit(tube, 'skills')).to.deep.equal(expectedTube);
-      expect(tube.skills).to.be.empty;
+      expect(tube).to.deep.equal(expectedTube);
     });
   });
 
   describe('#list', () => {
     it('should return the tubes', async () => {
       // given
-      const tube0 = domainBuilder.buildTube();
-      delete tube0.skills;
-      const tube1 = domainBuilder.buildTube();
-      delete tube1.skills;
-      const airtableTube0 = airtableBuilder.factory.buildTube.fromDomain({ domainTube: tube0 });
-      const airtableTube1 = airtableBuilder.factory.buildTube.fromDomain({ domainTube: tube1 });
-      airtableBuilder.mockLists({ tubes: [airtableTube0, airtableTube1] });
+      const tube0 = new Tube({ 
+        id: 'recTube0', 
+        name: 'tubeName0',
+        title: 'tubeTitle0',
+        description: 'tubeDescription0',
+        practicalTitle: 'translatedPracticalTitle0',
+        practicalDescription: 'translatedPracticalDescription0',
+        competenceId: 'recCompetence0',
+      });
+      const tube1 = new Tube({ 
+        id: 'recTube1', 
+        name: 'tubeName1',
+        title: 'tubeTitle1',
+        description: 'tubeDescription1',
+        practicalTitle: 'translatedPracticalTitle1',
+        practicalDescription: 'translatedPracticalDescription1',
+        competenceId: 'recCompetence1',
+      });
+      const learningContentTube0 = {
+        id: 'recTube0', 
+        name: 'tubeName0',
+        title: 'tubeTitle0',
+        description: 'tubeDescription0',
+        practicalTitleFrFr: 'translatedPracticalTitle0',
+        practicalDescriptionFrFr: 'translatedPracticalDescription0',
+        competenceId: 'recCompetence0',
+      };
+      
+      const learningContentTube1 = {
+        id: 'recTube1', 
+        name: 'tubeName1',
+        title: 'tubeTitle1',
+        description: 'tubeDescription1',
+        practicalTitleFrFr: 'translatedPracticalTitle1',
+        practicalDescriptionFrFr: 'translatedPracticalDescription1',
+        competenceId: 'recCompetence1',
+      };
+      mockLearningContent({ tubes: [learningContentTube0, learningContentTube1] });
 
       // when
       const tubes = await tubeRepository.list();
 
       // then
       expect(tubes).to.have.length(2);
-      expect(tubes[0]).to.be.instanceof(Tube);
-    });
-
-    it('should return tubes without setting skills', async () => {
-      // given
-      const expectedTube = domainBuilder.buildTube();
-      delete expectedTube.skills;
-      const airtableTube = airtableBuilder.factory.buildTube.fromDomain({ domainTube: expectedTube });
-      airtableBuilder.mockLists({ tubes: [airtableTube] });
-
-      // when
-      const tubes = await tubeRepository.list();
-
-      // then
-      expect(_.omit(tubes[0], 'skills')).to.deep.equal(expectedTube);
-      expect(tubes[0].skills).to.be.empty;
+      expect(tubes[0]).to.deep.equal(tube0);
+      expect(tubes[1]).to.deep.equal(tube1);
     });
   });
 
   describe('#findByNames', () => {
-    it('should return the tubes without setting skills ordered by name', async () => {
+    it('should return the tubes ordered by name', async () => {
       // given
-      const expectedTube = domainBuilder.buildTube();
-      delete expectedTube.skills;
-      const airtableTube = airtableBuilder.factory.buildTube.fromDomain({ domainTube: expectedTube });
-      airtableBuilder.mockLists({ tubes: [airtableTube] });
+      const tube0 = new Tube({ 
+        id: 'recTube0', 
+        name: 'tubeName0',
+        title: 'tubeTitle0',
+        description: 'tubeDescription0',
+        practicalTitle: 'translatedPracticalTitle0',
+        practicalDescription: 'translatedPracticalDescription0',
+        competenceId: 'recCompetence0',
+      });
+      
+      const tube1 = new Tube({ 
+        id: 'recTube1', 
+        name: 'tubeName1',
+        title: 'tubeTitle1',
+        description: 'tubeDescription1',
+        practicalTitle: 'translatedPracticalTitle1',
+        practicalDescription: 'translatedPracticalDescription1',
+        competenceId: 'recCompetence1',
+      });
+      
+      const learningContentTube0 = {
+        id: 'recTube0', 
+        name: 'tubeName0',
+        title: 'tubeTitle0',
+        description: 'tubeDescription0',
+        practicalTitleFrFr: 'translatedPracticalTitle0',
+        practicalDescriptionFrFr: 'translatedPracticalDescription0',
+        competenceId: 'recCompetence0',
+      };
+      
+      const learningContentTube1 = {
+        id: 'recTube1', 
+        name: 'tubeName1',
+        title: 'tubeTitle1',
+        description: 'tubeDescription1',
+        practicalTitleFrFr: 'translatedPracticalTitle1',
+        practicalDescriptionFrFr: 'translatedPracticalDescription1',
+        competenceId: 'recCompetence1',
+      };
+      mockLearningContent({ tubes: [learningContentTube1, learningContentTube0] });
 
       // when
-      const tubes = await tubeRepository.findByNames({ tubeNames: [expectedTube.name] });
+      const tubes = await tubeRepository.findByNames({ tubeNames: ['tubeName1', 'tubeName0'] });
 
       // then
-      expect(_.omit(tubes[0], 'skills')).to.deep.equal(expectedTube);
-      expect(tubes[0].skills).to.be.empty;
+      expect(tubes[0]).to.deep.equal(tube0);
+      expect(tubes[1]).to.deep.equal(tube1);
     });
 
     context('when no locale is provided (using default locale)', () => {
 
       it('should return the tubes with default locale translation', async () => {
         // given
-        const expectedTube = domainBuilder.buildTube();
-        delete expectedTube.skills;
-        const airtableTube = airtableBuilder.factory.buildTube.fromDomain({ domainTube: expectedTube });
-        airtableBuilder.mockLists({ tubes: [airtableTube] });
+        const expectedTube = new Tube({ 
+          id: 'recTube0', 
+          name: 'tubeName',
+          title: 'tubeTitle',
+          description: 'tubeDescription',
+          practicalTitle: 'translatedPracticalTitle',
+          practicalDescription: 'translatedPracticalDescription',
+          competenceId: 'recCompetence0',
+        });
+        const learningContent = {
+          tubes: [{ 
+            id: 'recTube0', 
+            name: 'tubeName',
+            title: 'tubeTitle',
+            description: 'tubeDescription',
+            practicalTitleFrFr: 'translatedPracticalTitle',
+            practicalDescriptionFrFr: 'translatedPracticalDescription',
+            competenceId: 'recCompetence0',
+          }],
+        };
+        mockLearningContent(learningContent);
 
         // when
-        const tubes = await tubeRepository.findByNames({ tubeNames: [expectedTube.name] });
+        const tubes = await tubeRepository.findByNames({ tubeNames: ['tubeName'] });
 
         // then
-        expect(tubes[0].practicalTitle).to.equal(airtableTube.fields['Titre pratique fr-fr']);
-        expect(tubes[0].practicalDescription).to.equal(airtableTube.fields['Description pratique fr-fr']);
+        expect(tubes[0].practicalTitle).to.equal(expectedTube.practicalTitle);
+        expect(tubes[0].practicalDescription).to.equal(expectedTube.practicalDescription);
       });
     });
 
@@ -102,18 +181,37 @@ describe('Integration | Repository | tube-repository', () => {
 
       it('should return the tubes with appropriate translation', async () => {
         // given
+        const expectedTube = new Tube({ 
+          id: 'recTube0', 
+          name: 'tubeName',
+          title: 'tubeTitle',
+          description: 'tubeDescription',
+          practicalTitle: 'translatedPracticalTitleEnUs',
+          practicalDescription: 'translatedPracticalDescriptionEnUs',
+          competenceId: 'recCompetence0',
+        });
+        const learningContent = {
+          tubes: [{ 
+            id: 'recTube0', 
+            name: 'tubeName',
+            title: 'tubeTitle',
+            description: 'tubeDescription',
+            practicalTitleFrFr: 'translatedPracticalTitle',
+            practicalTitleEnUs: 'translatedPracticalTitleEnUs',
+            practicalDescriptionFrFr: 'translatedPracticalDescription',
+            practicalDescriptionEnUs: 'translatedPracticalDescriptionEnUs',
+            competenceId: 'recCompetence0',
+          }],
+        };
+        mockLearningContent(learningContent);
         const locale = 'en';
-        const expectedTube = domainBuilder.buildTube();
-        delete expectedTube.skills;
-        const airtableTube = airtableBuilder.factory.buildTube.fromDomain({ domainTube: expectedTube, locale });
-        airtableBuilder.mockLists({ tubes: [airtableTube] });
 
         // when
-        const tubes = await tubeRepository.findByNames({ tubeNames: [expectedTube.name], locale });
+        const tubes = await tubeRepository.findByNames({ tubeNames: 'tubeName', locale });
 
         // then
-        expect(tubes[0].practicalTitle).to.equal(airtableTube.fields['Titre pratique en-us']);
-        expect(tubes[0].practicalDescription).to.equal(airtableTube.fields['Description pratique en-us']);
+        expect(tubes[0].practicalTitle).to.equal(expectedTube.practicalTitle);
+        expect(tubes[0].practicalDescription).to.equal(expectedTube.practicalDescription);
       });
     });
   });
