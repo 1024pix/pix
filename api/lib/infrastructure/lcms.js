583a9b13256c8336b78d6268bba111f30ec40fc6
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2020-11-24 10:48:14 +0100

    Implement LCMS API call

--- api/lib/infrastructure/lcms.js
+++ api/lib/infrastructure/lcms.js
@@ -1,5 +1,12 @@
-module.exports = {
-  getLatestRelease() {
+const axios = require('axios');
+const { lcms } = require('../config');
 
+module.exports = {
+  async getLatestRelease() {
+    const response = await axios.get('/releases/latest', {
+      baseURL: lcms.url,
+      headers: { Authorization: `Bearer ${lcms.apiKey}` },
+    });
+    return response.data;
   },
 };
---
+++ api/tests/unit/infrastructure/lcms_test.js
@@ -0,0 +1,52 @@
+const { expect, nock, catchErr } = require('../../test-helper');
+
+const lcms = require('../../../lib/infrastructure/lcms');
+
+describe('Unit | Infrastructure | LCMS', () => {
+  describe('#getLatestRelease', () => {
+
+    it('calls LCMS API to get learning content release', async () => {
+      // given
+      const lcmsCall = nock('https://lcms-test.pix.fr/api')
+        .get('/releases/latest')
+        .matchHeader('Authorization', 'Bearer test-api-key')
+        .reply(200);
+
+      // when
+      await lcms.getLatestRelease();
+
+      // then
+      expect(lcmsCall.isDone()).to.equal(true);
+    });
+
+    it('returns learning content release', async () => {
+      // given
+      const learningContent = { models: [{ id: 'recId' }] };
+      nock('https://lcms-test.pix.fr/api')
+        .get('/releases/latest')
+        .matchHeader('Authorization', 'Bearer test-api-key')
+        .reply(200, learningContent);
+
+      // when
+      const response = await lcms.getLatestRelease();
+
+      // then
+      expect(response).to.deep.equal(learningContent);
+    });
+
+    it('rejects when learning content release failed to get', async () => {
+      // given
+      nock('https://lcms-test.pix.fr/api')
+        .get('/releases/latest')
+        .matchHeader('Authorization', 'Bearer test-api-key')
+        .reply(500);
+
+      // when
+      const error = await catchErr(lcms.getLatestRelease)();
+
+      // then
+      expect(error).to.be.not.null;
+    });
+
+  });
+});
