a3eb7e48ad274cd47739afd21fad6685e83044df
Etienne GIROT <egirot@octo.com>
2020-11-26 11:06:42 +0100

    [API] Modèle Category & migration examinerComment

---
+++ api/db/migrations/20201126103437_migrate-examinerComment-to-certification-issue-reports.js
@@ -0,0 +1,31 @@
+const { CertificationIssueReportCategories } = require('../../lib/domain/models/CertificationIssueReportCategory');
+const CERTIFICATION_COURSES = 'certification-courses';
+const CERTIFICATION_ISSUE_REPORTS = 'certification-issue-reports';
+
+exports.up = async (knex) => {
+  const certificationCoursesWithExaminerComment = await knex(CERTIFICATION_COURSES)
+    .select('id', 'examinerComment')
+    .whereNotNull('examinerComment');
+
+  const reportsToInsert = certificationCoursesWithExaminerComment.map(({ id, examinerComment }) => {
+    return {
+      certificationCourseId: id,
+      description: examinerComment,
+      categoryId: CertificationIssueReportCategories.OTHER,
+    };
+  });
+
+  return knex.batchInsert(CERTIFICATION_ISSUE_REPORTS, reportsToInsert);
+};
+
+exports.down = async (knex) => {
+  const certificationCoursesWithExaminerComment = await knex(CERTIFICATION_COURSES)
+    .select('id')
+    .whereNotNull('examinerComment');
+
+  const idsToDelete = certificationCoursesWithExaminerComment.map((c) => c.id);
+
+  return knex(CERTIFICATION_ISSUE_REPORTS)
+    .whereIn('certificationCourseId', idsToDelete)
+    .delete();
+};
---
+++ api/lib/domain/models/CertificationIssueReportCategory.js
@@ -0,0 +1,5 @@
+module.exports = {
+  CertificationIssueReportCategories: {
+    OTHER: 1,
+  },
+};
