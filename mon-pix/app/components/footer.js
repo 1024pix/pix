f1291d4567ecf2b1d0697a602f8450b73c46be4c
Stephane Bedeau <sbedeau@octo.com>
2020-12-02 16:49:54 +0100

    chore: apply rule no-side-effects

--- mon-pix/app/components/footer.js
+++ mon-pix/app/components/footer.js
@@ -1,18 +1,15 @@
 import { inject as service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
 import Component from '@glimmer/component';
 
 export default class Footer extends Component {
   @service url;
-
-  @tracked date;
 
   get shouldShowTheMarianneLogo() {
     return this.url.isFrenchDomainExtension;
   }
 
   get currentYear() {
-    this.date = new Date();
-    return this.date.getFullYear().toString();
+    const date = new Date();
+    return date.getFullYear().toString();
   }
 }
