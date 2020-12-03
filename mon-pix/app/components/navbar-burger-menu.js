7eae70ad783d8e52095fbdd6ea50d3376e8c444c
alexandrecoin <alexandre.pc.coin@gmail.com>
2020-12-03 18:02:23 +0100

    Add link to dashboard on mobile

--- mon-pix/app/components/navbar-burger-menu.js
+++ mon-pix/app/components/navbar-burger-menu.js
@@ -4,8 +4,13 @@
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 import classic from 'ember-classic-decorator';
+import ENV from 'mon-pix/config/environment';
 
 @classic
 export default class NavbarBurgerMenu extends Component {
   @service currentUser;
+
+  get showDashboard() {
+    return ENV.APP.FT_DASHBOARD;
+  }
 }
