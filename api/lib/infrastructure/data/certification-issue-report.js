26a9c18a3eb02fd8118cbfffbd4e4a75abe376b5
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-04 18:19:17 +0100

    [API] Supprime parse() inutilisé dans un model db

--- api/lib/infrastructure/data/certification-issue-report.js
+++ api/lib/infrastructure/data/certification-issue-report.js
@@ -3,19 +3,9 @@ const Bookshelf = require('../bookshelf');
 const modelName = 'CertificationIssueReport';
 
 module.exports = Bookshelf.model(modelName, {
-
   tableName: 'certification-issue-reports',
   hasTimestamps: ['createdAt', 'updatedAt'],
   requireFetch: false,
-
-  parse(rawAttributes) {
-    if (rawAttributes.completedAt) {
-      rawAttributes.completedAt = new Date(rawAttributes.completedAt);
-    }
-
-    return rawAttributes;
-  },
-
 }, {
   modelName,
 });
