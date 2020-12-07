57661dce0b496643370e2b35b58c61735aef194e
alexandrecoin <alexandre.pc.coin@gmail.com>
2020-12-07 14:44:44 +0100

    Fix tests with tutorials information

--- mon-pix/tests/integration/components/navbar-desktop-header-test.js
+++ mon-pix/tests/integration/components/navbar-desktop-header-test.js
@@ -138,9 +138,10 @@ describe('Integration | Component | navbar-desktop-header', function() {
 
       // then
       expect(find('.navbar-desktop-header-container__menu')).to.exist;
-      expect(findAll('.navbar-desktop-header-menu__item')).to.have.lengthOf(4);
+      expect(findAll('.navbar-desktop-header-menu__item')).to.have.lengthOf(5);
       expect(contains('Accueil')).to.exist;
       expect(contains('Profil')).to.exist;
+      expect(contains('Mes tutos')).to.exist;
       expect(contains('Certification')).to.exist;
       expect(contains('Aide')).to.exist;
     });
