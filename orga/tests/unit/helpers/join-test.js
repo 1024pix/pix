bf8c21623356703a1e9e03a33dcdf812c4400a8f
ajoanny <ajoanny@octo.com>
2020-12-07 15:38:07 +0100

    [ORGA] add missing test

--- orga/tests/unit/helpers/join-test.js
+++ orga/tests/unit/helpers/join-test.js
@@ -6,8 +6,15 @@ module('Unit | Helper | join', function(hooks) {
   setupTest(hooks);
 
   module('join', () => {
-    test('it joins all values using the seperator', function(assert) {
-      assert.equal(join([['Un',  'Deux'], ', ']), 'Un, Deux');
+    module('when there are several values', () => {
+      test('it joins all values using the seperator', function(assert) {
+        assert.equal(join([['Un',  'Deux'], ', ']), 'Un, Deux');
+      });
+    });
+    module('when there is only one value', () => {
+      test('it returns the value', function(assert) {
+        assert.equal(join([['Un'], ', ']), 'Un');
+      });
     });
   });
 });
