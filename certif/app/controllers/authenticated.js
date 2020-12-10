021183142f2fe7595f930f64b7e2c5f48e34e12b
Anne-G <ang.schall@gmail.com>
2020-12-10 12:10:58 +0100

    [Certif] Add banner for sco user

--- certif/app/controllers/authenticated.js
+++ certif/app/controllers/authenticated.js
@@ -1,13 +1,29 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 const LINK_SCO = 'http://cloud.pix.fr/s/GqwW6dFDDrHezfS';
 const LINK_OTHER = 'http://cloud.pix.fr/s/fLSG4mYCcX7GDRF';
 
 export default class AuthenticatedController extends Controller {
+
+  @tracked isBannerVisible = true;
+  @service currentUser;
+
+  get showBanner() {
+    return this.currentUser.isFromSco && this.isBannerVisible;
+  }
+
   get documentationLink() {
     if (this.model.isSco) {
       return LINK_SCO;
     }
     return LINK_OTHER;
+  }
+
+  @action
+  async closeBanner() {
+    this.isBannerVisible = false;
   }
 }
