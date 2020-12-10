8e6528be522f183769743f38fa8f4fb74d0a64b9
alexandrecoin <alexandre.pc.coin@gmail.com>
2020-12-10 15:43:49 +0100

    Add card

---
+++ mon-pix/tests/unit/components/campaign-participation/card-test.js
@@ -0,0 +1,57 @@
+import EmberObject from '@ember/object';
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { setupTest } from 'ember-mocha';
+import createGlimmerComponent from 'mon-pix/tests/helpers/create-glimmer-component';
+
+describe('Unit | Component | CampaignParticipation | Card', function() {
+  let component;
+
+  setupTest();
+
+  beforeEach(function() {
+    // given
+    component = createGlimmerComponent('component:campaign-participation/card');
+  });
+
+  describe('#status', function() {
+
+    it('should return the status when the campaign is completed', function() {
+      // given
+      const assessment = EmberObject.create({
+        state: 'completed',
+      });
+      component.args.model = EmberObject.create({ assessment });
+
+      // when
+      const result = component.status;
+
+      // then
+      expect(result).to.eql({
+        tagText: 'pages.campaign-participation.card.tag.completed',
+        tagColor: 'yellow',
+        actionText: 'pages.campaign-participation.card.send',
+        actionClass: 'button--yellow',
+      });
+    });
+
+    it('should return the status when the campaign is not completed', function() {
+      // given
+      const assessment = EmberObject.create({
+        state: 'started',
+      });
+      component.args.model = EmberObject.create({ assessment });
+
+      // when
+      const result = component.status;
+
+      // then
+      expect(result).to.eql({
+        tagText: 'pages.campaign-participation.card.tag.started',
+        tagColor: 'green',
+        actionText: 'pages.campaign-participation.card.resume',
+        actionClass: '',
+      });
+    });
+  });
+});
