44f8bd00a557301807429574b9625f78d024751a
yahya youness <yahyayounes@hotmail.fr>
2020-12-14 00:52:05 +0100

    Add read models for LSU certifications results
    Co-authored-by: Antoine ceol <antoine.ceol@pix.fr>

---
+++ api/lib/domain/read-models/livret-scolaire/Certificate.js
@@ -0,0 +1,36 @@
+class Certificate {
+
+  constructor({
+    id,
+    firstName,
+    middleName,
+    thirdName,
+    lastName,
+    birthdate,
+    nationalStudentId,
+    status,
+    pixScore,
+    verificationCode,
+    date,
+    deliveredAt,
+    certificationCenter,
+    competenceResults = [],
+  } = {}) {
+    this.id = id;
+    this.firstName = firstName;
+    this.middleName = middleName;
+    this.thirdName = thirdName;
+    this.lastName = lastName;
+    this.birthdate = birthdate;
+    this.nationalStudentId = nationalStudentId;
+    this.date = date;
+    this.deliveredAt = deliveredAt;
+    this.pixScore = pixScore;
+    this.status = status;
+    this.competenceResults = competenceResults;
+    this.certificationCenter = certificationCenter;
+    this.verificationCode = verificationCode;
+  }
+}
+
+module.exports = Certificate;
