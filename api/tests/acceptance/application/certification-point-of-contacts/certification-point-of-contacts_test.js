bf1c9fbc5a30a7099a61090b7cdcd1695677a136
Laura Bergoens <laura.bergoens@pix.fr>
2020-12-14 09:26:59 +0100

    feat: add route to get certification referent

---
+++ api/tests/acceptance/application/certification-point-of-contacts/certification-point-of-contacts_test.js
@@ -0,0 +1,38 @@
+const { expect, databaseBuilder, generateValidRequestAuthorizationHeader } = require('../../../test-helper');
+const createServer = require('../../../../server');
+
+describe('Acceptance | Route | CertificationPointOfContact', () => {
+
+  let server;
+
+  beforeEach(async () => {
+    server = await createServer();
+  });
+
+  describe('GET /api/certification-point-of-contacts/:userId', () => {
+
+    it('should 200 HTTP status code', async () => {
+      // given
+      const userId = databaseBuilder.factory.buildUser().id;
+      const certificationCenterId = databaseBuilder.factory.buildCertificationCenter().id;
+      databaseBuilder.factory.buildCertificationCenterMembership({
+        userId,
+        certificationCenterId,
+      });
+      await databaseBuilder.commit();
+      const options = {
+        method: 'GET',
+        url: `/api/certification-point-of-contacts/${userId}`,
+        headers: { authorization: generateValidRequestAuthorizationHeader(userId) },
+      };
+
+      // when
+      const response = await server.inject(options);
+
+      // then
+      expect(response.statusCode).to.equal(200);
+      expect(response.result.data.id).to.equal(userId.toString());
+    });
+  });
+
+});
