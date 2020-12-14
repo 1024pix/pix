1141ec2d6b0d048a4b0246704c639056fbb306b5
Laura Bergoens <laura.bergoens@pix.fr>
2020-12-14 09:07:26 +0100

    feat: add certification referent jsonapi serializer

---
+++ api/tests/tooling/domain-builder/factory/build-certification-point-of-contact.js
@@ -0,0 +1,28 @@
+const CertificationPointOfContact = require('../../../../lib/domain/read-models/CertificationPointOfContact');
+const CertificationCenter = require('../../../../lib/domain/models/CertificationCenter');
+
+module.exports = function buildCertificationPointOfContact(
+  {
+    id = 123,
+    firstName = 'Chèvre',
+    lastName = 'Brebis',
+    email = 'chevre.brebis@example.net',
+    pixCertifTermsOfServiceAccepted = true,
+    certificationCenterId = 456,
+    certificationCenterName = 'Centre de la prairie verdoyante',
+    certificationCenterType = CertificationCenter.types.PRO,
+    certificationCenterExternalId = 'CHEVRE456',
+  } = {}) {
+
+  return new CertificationPointOfContact({
+    id,
+    firstName,
+    lastName,
+    email,
+    pixCertifTermsOfServiceAccepted,
+    certificationCenterId,
+    certificationCenterName,
+    certificationCenterType,
+    certificationCenterExternalId,
+  });
+};
