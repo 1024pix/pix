53062fca6ca0ee7aa2d9859b363c5630c8eb23b7
Laura Bergoens <laura.bergoens@pix.fr>
2020-12-15 00:06:02 +0100

    feat: add new usecase to get competence evaluations by assessment

---
+++ api/tests/unit/domain/usecases/find-competence-evaluations-by-assessment_test.js
@@ -0,0 +1,52 @@
+const _ = require('lodash');
+const { expect, sinon, catchErr } = require('../../../test-helper');
+const findCompetenceEvaluationsByAssessment = require('../../../../lib/domain/usecases/find-competence-evaluations-by-assessment');
+const { UserNotAuthorizedToAccessEntity } = require('../../../../lib/domain/errors');
+
+describe('Unit | UseCase | find-competence-evaluations-by-assessment', () => {
+
+  const userId = 1;
+  const assessmentId = 2;
+  const assessmentRepository = { ownedByUser: _.noop() };
+  const competenceEvaluationRepository = { findByAssessmentId: _.noop() };
+
+  beforeEach(() => {
+    assessmentRepository.ownedByUser = sinon.stub();
+    competenceEvaluationRepository.findByAssessmentId = sinon.stub();
+  });
+
+  it('should throw an UserNotAuthorizedToAccessEntity error when user is not the owner of the assessment', async () => {
+    // given
+    assessmentRepository.ownedByUser.withArgs({ id: assessmentId, userId }).resolves(false);
+
+    // when
+    const error = await catchErr(findCompetenceEvaluationsByAssessment)({
+      userId,
+      assessmentId,
+      competenceEvaluationRepository,
+      assessmentRepository,
+    });
+
+    // then
+    expect(error).to.be.instanceOf(UserNotAuthorizedToAccessEntity);
+  });
+
+  it('should return the assessment competence-evaluations', async () => {
+    // given
+    const expectedCompetenceEvaluations = Symbol('competenceEvaluations');
+    assessmentRepository.ownedByUser.withArgs({ id: assessmentId, userId }).resolves(true);
+    competenceEvaluationRepository.findByAssessmentId.withArgs(assessmentId).resolves(expectedCompetenceEvaluations);
+
+    // when
+    const actualCompetenceEvaluations = await findCompetenceEvaluationsByAssessment({
+      userId,
+      assessmentId,
+      competenceEvaluationRepository,
+      assessmentRepository,
+    });
+
+    // then
+    expect(actualCompetenceEvaluations).to.deep.equal(expectedCompetenceEvaluations);
+  });
+
+});
