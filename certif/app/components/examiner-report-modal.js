23945333e9063b5a4c46e0d90aa5259fa1850cf4
Anne-G <ang.schall@gmail.com>
2020-12-16 11:50:40 +0100

    [Certif] Refacto category management

--- certif/app/components/examiner-report-modal.js
+++ certif/app/components/examiner-report-modal.js
@@ -8,93 +8,54 @@ class RadioButtonCategory {
   @tracked isChecked;
   @tracked name;
 
-  constructor() {
+  constructor({ name }) {
+    this.name = name;
     this.isChecked = false;
+  }
+
+  toggle(categoryNameToBeingCheck) {
+    this.isChecked = this.name === categoryNameToBeingCheck;
+  }
+
+  issueReport(certificationReport) {
+    return {
+      category: this.name,
+      description: this.description,
+      certificationReport,
+    };
+  }
+}
+
+class RadioButtonCategoryWithDescription extends RadioButtonCategory {
+  @tracked description;
+
+  toggle(categoryNameToBeingCheck) {
+    super.toggle(categoryNameToBeingCheck);
+    this.description = '';
   }
 }
 
 export default class ExaminerReportModal extends Component {
   @service store
 
-  @tracked otherCategory = new RadioButtonCategory();
-  @tracked lateOrLeavingCategory = new RadioButtonCategory();
-  @tracked candidateInformationsChangesCategory = new RadioButtonCategory();
-  @tracked connexionOrEndScreenCategory = new RadioButtonCategory();
-  @tracked currentIssueReport = {
-    category: null,
-    description: null,
-    certificationReport: null,
-  };
+  @tracked otherCategory = new RadioButtonCategoryWithDescription({ name: certificationIssueReportCategories.OTHER });
+  @tracked lateOrLeavingCategory = new RadioButtonCategoryWithDescription({ name: certificationIssueReportCategories.LATE_OR_LEAVING });
+  @tracked candidateInformationsChangesCategory = new RadioButtonCategoryWithDescription({ name: certificationIssueReportCategories.CANDIDATE_INFORMATIONS_CHANGES });
+  @tracked connexionOrEndScreenCategory = new RadioButtonCategoryWithDescription({ name: certificationIssueReportCategories.CONNEXION_OR_END_SCREEN });
+  categories = [ this.otherCategory, this.lateOrLeavingCategory, this.candidateInformationsChangesCategory, this.connexionOrEndScreenCategory ];
+
   @tracked reportLength = 0;
 
-  constructor() {
-    super(...arguments);
-    this.otherCategory.name = certificationIssueReportCategories.OTHER;
-    this.lateOrLeavingCategory.name = certificationIssueReportCategories.LATE_OR_LEAVING;
-    this.candidateInformationsChangesCategory.name = certificationIssueReportCategories.CANDIDATE_INFORMATIONS_CHANGES;
-    this.connexionOrEndScreenCategory.name = certificationIssueReportCategories.CONNEXION_OR_END_SCREEN;
-
-    const certificationReport = this.args.report;
-    const certificationIssueReports = certificationReport.certificationIssueReports;
-    const existingIssueReport = certificationIssueReports && certificationIssueReports[0];
-
-    if (existingIssueReport) {
-      this.currentIssueReport = existingIssueReport;
-      this.reportLength = existingIssueReport.description.length
-        ? existingIssueReport.description.length
-        : 0;
-
-      switch (existingIssueReport.category) {
-        case certificationIssueReportCategories.OTHER:
-          this.otherCategory.isChecked = true;
-          break;
-
-        case certificationIssueReportCategories.LATE_OR_LEAVING:
-          this.lateOrLeavingCategory.isChecked = true;
-          break;
-
-        case certificationIssueReportCategories.CANDIDATE_INFORMATIONS_CHANGES:
-          this.candidateInformationsChangesCategory.isChecked = true;
-          break;
-
-        case certificationIssueReportCategories.CONNEXION_OR_END_SCREEN:
-          this.connexionOrEndScreenCategory.isChecked = true;
-          break;
-
-        default:
-          break;
-      }
-    } else {
-      this.currentIssueReport = { certificationReport: this.args.report };
-    }
-  }
-
   @action
-  toggleOnCategory(category) {
-    category.isChecked = !category.isChecked;
-    this._resetAllCurrentIssueReportData();
-    if (category.isChecked) {
-      this._toggleOffAllCategoryExceptOne(category.name);
-      this.currentIssueReport.category = category.name;
-    }
-  }
-
-  _resetAllCurrentIssueReportData() {
-    delete this.currentIssueReport.description;
-    this.reportLength = 0;
-  }
-
-  _toggleOffAllCategoryExceptOne(categoryToExcludeName) {
-    this.otherCategory.isChecked = this.otherCategory.name === categoryToExcludeName;
-    this.lateOrLeavingCategory.isChecked = this.lateOrLeavingCategory.name === categoryToExcludeName;
-    this.candidateInformationsChangesCategory.isChecked = this.candidateInformationsChangesCategory.name === categoryToExcludeName;
-    this.connexionOrEndScreenCategory.isChecked = this.connexionOrEndScreenCategory.name === categoryToExcludeName;
+  toggleOnCategory(selectedCategory) {
+    this.categories.forEach((category) => category.toggle(selectedCategory.name));
   }
 
   @action
   async submitReport(event) {
     event.preventDefault();
-    const issueReportToSave = this.store.createRecord('certification-issue-report', this.currentIssueReport);
+    const categoryToAdd = this.categories.find((category) => category.isChecked);
+    const issueReportToSave = this.store.createRecord('certification-issue-report', categoryToAdd.issueReport(this.args.report));
     await issueReportToSave.save();
     this.args.closeModal();
   }
