261bea00722c21e59ec258477fa751ef1450e1ad
Anne-G <ang.schall@gmail.com>
2020-12-17 15:54:54 +0100

    [Certif-API] Add subcategory for connexionOrEndScreen

--- certif/tests/integration/components/connexion-or-end-screen-certification-issue-report-fields-test.js
+++ certif/tests/integration/components/connexion-or-end-screen-certification-issue-report-fields-test.js
@@ -1,7 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, click, fillIn } from '@ember/test-helpers';
-import { RadioButtonCategoryWithDescription } from 'pix-certif/components/examiner-report-modal';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import sinon from 'sinon';
 
@@ -9,8 +8,7 @@ module('Integration | Component | connexion-or-end-screen-certification-issue-re
   setupRenderingTest(hooks);
 
   const INPUT_RADIO_SELECTOR = '#input-radio-for-category-connexion-or-end-screen';
-  const TEXTAREA_SELECTOR = '#text-area-for-category-connexion-or-end-screen';
-  const CHAR_COUNT_SELECTOR = '.connexion-or-end-screen-certification-issue-report-fields-details__char-count';
+  const SUBCATEGORY_SELECTOR = '#subcategory-for-category-connexion-or-end-screen';
 
   test('it should call toggle function on click radio button', async function(assert) {
     // given
@@ -32,7 +30,7 @@ module('Integration | Component | connexion-or-end-screen-certification-issue-re
     assert.ok(toggleOnCategory.calledOnceWith(connexionOrEndScreenCategory));
   });
 
-  test('it should show textarea if category is checked', async function(assert) {
+  test('it should show subcategory selector if category is checked', async function(assert) {
     // given
     const toggleOnCategory = sinon.stub();
     const connexionOrEndScreenCategory = { isChecked: true };
@@ -49,10 +47,10 @@ module('Integration | Component | connexion-or-end-screen-certification-issue-re
     await click(INPUT_RADIO_SELECTOR);
 
     // then
-    assert.dom('.connexion-or-end-screen-certification-issue-report-fields__details').exists();
+    assert.dom(SUBCATEGORY_SELECTOR).exists();
   });
 
-  test('it should not show textarea if category is unchecked', async function(assert) {
+  test('it should not show subcategory selector if category is unchecked', async function(assert) {
     // given
     const toggleOnCategory = sinon.stub();
     const connexionOrEndScreenCategory = { isChecked: false };
@@ -69,27 +67,7 @@ module('Integration | Component | connexion-or-end-screen-certification-issue-re
     await click(INPUT_RADIO_SELECTOR);
 
     // then
-    assert.dom('.connexion-or-end-screen-certification-issue-report-fields__details').doesNotExist();
-  });
-
-  test('it should count textarea characters length', async function(assert) {
-    // given
-    const toggleOnCategory = sinon.stub();
-    const connexionOrEndScreenCategory = new RadioButtonCategoryWithDescription({ name: 'CONNEXION_OR_END_SCREEN', isChecked: true });
-    this.set('toggleOnCategory', toggleOnCategory);
-    this.set('connexionOrEndScreenCategory', connexionOrEndScreenCategory);
-
-    // when
-    await render(hbs`
-      <ConnexionOrEndScreenCertificationIssueReportFields
-        @connexionOrEndScreenCategory={{this.connexionOrEndScreenCategory}}
-        @toggleOnCategory={{this.toggleOnCategory}}
-        @maxlength={{500}}
-      />`);
-    await fillIn(TEXTAREA_SELECTOR, 'Coucou');
-
-    // then
-    assert.dom(CHAR_COUNT_SELECTOR).hasText('6   / 500');
+    assert.dom(SUBCATEGORY_SELECTOR).doesNotExist();
   });
 
 });
