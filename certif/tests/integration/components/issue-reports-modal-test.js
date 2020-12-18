730b95b8273b225b7f36e6ec7326acd946d9f6d3
Jonas Grillères <grilleres.j@gmail.com>
2020-12-18 16:09:18 +0100

    [CERTIF] Display issue reports count on modal

--- certif/tests/integration/components/issue-reports-modal-test.js
+++ certif/tests/integration/components/issue-reports-modal-test.js
@@ -4,8 +4,9 @@ import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import sinon from 'sinon';
 import EmberObject from '@ember/object';
+import { A } from '@ember/array';
 
-module.only('Integration | Component | issue-reports-modal', function(hooks) {
+module('Integration | Component | issue-reports-modal', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it show candidate informations in title', async function(assert) {
@@ -62,5 +63,45 @@ module.only('Integration | Component | issue-reports-modal', function(hooks) {
 
     // then
     assert.ok(onClickIssueReportStub.calledOnceWith(report));
+  });
+
+  test('it should show Mes signalements (2)', async function(assert) {
+    // given
+    const issue1 = EmberObject.create({
+      category: 'category',
+      subcategory: 'subcategory',
+      description: 'description',
+    });
+
+    const issue2 = EmberObject.create({
+      category: 'category2',
+      subcategory: 'subcategory2',
+      description: 'description2',
+    });
+
+    const report = EmberObject.create({
+      certificationCourseId: 1,
+      certificationIssueReports: A([issue1, issue2]),
+      firstName: 'Lisa',
+      lastName: 'Monpud',
+      hasSeenEndTestScreen: false,
+    });
+    const onClickIssueReportStub = sinon.stub();
+    const closeIssueReportsModalStub = sinon.stub();
+    this.set('report', report);
+    this.set('onClickIssueReport', onClickIssueReportStub);
+    this.set('closeIssueReportsModal', closeIssueReportsModalStub);
+
+    // when
+    await render(hbs`
+      <IssueReportsModal
+        @report={{this.report}}
+        @closeModal={{this.closeIssueReportsModal}}
+        @onClickIssueReport={{this.onClickIssueReport}}
+      />
+    `);
+
+    // then
+    assert.contains('Mes signalements (2)');
   });
 });
