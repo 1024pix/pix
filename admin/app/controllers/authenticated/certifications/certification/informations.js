8efd640874896d402278b9645df0c33d14130597
melanieb <melanie.boudard@gmail.com>
2020-12-18 16:34:03 +0100

    Rename variable for pix by level

--- admin/app/controllers/authenticated/certifications/certification/informations.js
+++ admin/app/controllers/authenticated/certifications/certification/informations.js
@@ -9,12 +9,13 @@ import find from 'lodash/find';
 import ENV from 'pix-admin/config/environment';
 
 import { tracked } from '@glimmer/tracking';
+const PIX_COUNT_BY_LEVEL = 8;
 
 export default class CertificationInformationsController extends Controller {
 
   // Domain constants
   MAX_REACHABLE_LEVEL = ENV.APP.MAX_REACHABLE_LEVEL;
-  MAX_REACHABLE_PIX_BY_COMPETENCE = this.MAX_REACHABLE_LEVEL * 8;
+  MAX_REACHABLE_PIX_BY_COMPETENCE = this.MAX_REACHABLE_LEVEL * PIX_COUNT_BY_LEVEL;
 
   // Properties
   @alias('model') certification;
