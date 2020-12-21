d18465936dedbd8ad882f26815f1892c8f986ca1
P-Jeremy <jemyplu@gmail.com>
2020-12-21 15:24:33 +0100

    [API] add repo method to delete certification issue report

--- api/lib/infrastructure/repositories/certification-issue-report-repository.js
+++ api/lib/infrastructure/repositories/certification-issue-report-repository.js
@@ -1,4 +1,3 @@
-
 const CertificationIssueReportBookshelf = require('../data/certification-issue-report');
 const bookshelfToDomainConverter = require('../utils/bookshelf-to-domain-converter');
 
@@ -6,5 +5,18 @@ module.exports = {
   async save(certificationIssueReport) {
     const newCertificationIssueReport = await new CertificationIssueReportBookshelf(certificationIssueReport).save();
     return bookshelfToDomainConverter.buildDomainObject(CertificationIssueReportBookshelf, newCertificationIssueReport);
+  },
+
+  async delete(id) {
+    try {
+      await CertificationIssueReportBookshelf
+        .where({ id })
+        .destroy({ require: true });
+      return true;
+    } catch (err) {
+      if (err instanceof CertificationIssueReportBookshelf.NoRowsDeletedError) {
+        return false;
+      }
+    }
   },
 };
