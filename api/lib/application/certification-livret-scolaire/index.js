d638f8485036a0dd526cab92e334372fe7f8de8a
Antoine Ceol <antoine.ceol@gmail.com>
2020-12-21 16:27:52 +0100

    Add open-api-doc setup for LSU/LSL API

--- api/lib/application/certification-livret-scolaire/index.js
+++ api/lib/application/certification-livret-scolaire/index.js
@@ -1,7 +1,12 @@
 const certificationController = require('./certification-controller');
 const { featureToggles } = require('../../config');
+const Joi = require('@hapi/joi');
+
+const responseErrorObjectDoc = require('../../infrastructure/open-api-doc/livret-scolaire/response-object-error-doc');
+const certificationsResultsResponseDoc = require('../../infrastructure/open-api-doc/livret-scolaire/certifications-results-doc');
 
 exports.register = async function(server) {
+
   server.route([
     {
       method: 'GET',
@@ -10,10 +15,26 @@ exports.register = async function(server) {
         auth: (featureToggles.isLivretScolaireSandboxApiEnabled) ? false : undefined,
         handler: certificationController.getCertificationsByOrganizationUAI,
         notes: [
-          '- **Route en version v0 sans authentification**\n' +
+          '- **API for LSU/LSL qui nécessite une authentification de type client credentiel grant**\n' +
           '- Récupération des résultats de certifications pour une organisation accompagnée du référentiel des compétences',
         ],
-        tags: ['api', 'organisation', 'certifications'],
+        tags: ['api', 'organisation', 'livret-scolaire'],
+        response: {
+          status: {
+            200: certificationsResultsResponseDoc,
+            401: responseErrorObjectDoc,
+            403: responseErrorObjectDoc,
+          },
+          schema: certificationsResultsResponseDoc,
+        },
+        validate: {
+          params: Joi.object({
+            uai: Joi.string().required().description('RNE/UAI (Unité Administrative Immatriculée du Répertoire National des Établissements) '),
+          }),
+          headers: Joi.object({
+            'authorization': Joi.string().required().description('Bearer Access token to access to API '),
+          }).unknown(),
+        },
       },
     },
   ]);
