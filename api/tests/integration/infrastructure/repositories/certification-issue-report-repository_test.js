8bcc818f1eb7162b662a39a2725001b7f1b08742
P-Jeremy <jemyplu@gmail.com>
2020-12-21 15:24:33 +0100

    [API] add repo method to delete certification issue report

--- api/tests/integration/infrastructure/repositories/certification-issue-report-repository_test.js
+++ api/tests/integration/infrastructure/repositories/certification-issue-report-repository_test.js
@@ -1,7 +1,6 @@
+const _ = require('lodash');
 const { expect, databaseBuilder, knex } = require('../../../test-helper');
-const omit = require('lodash/omit');
 const certificationIssueReportRepository = require('../../../../lib/infrastructure/repositories/certification-issue-report-repository');
-
 const CertificationIssueReport = require('../../../../lib/domain/models/CertificationIssueReport');
 const { CertificationIssueReportCategories } = require('../../../../lib/domain/models/CertificationIssueReportCategory');
 
@@ -35,8 +34,48 @@ describe('Integration | Repository | Certification Issue Report', function() {
         subcategory: null,
       };
 
-      expect(omit(savedCertificationIssueReport, 'id')).to.deep.equal(expectedSavedCertificationIssueReport);
+      expect(_.omit(savedCertificationIssueReport, 'id')).to.deep.equal(expectedSavedCertificationIssueReport);
       expect(savedCertificationIssueReport).to.be.an.instanceOf(CertificationIssueReport);
+    });
+  });
+
+  describe('#delete', () => {
+
+    it('should delete the issue report when it exists in certification course id', async () => {
+      // given
+      const certificationIssueReportToDeleteId = databaseBuilder.factory.buildCertificationIssueReport().id;
+      databaseBuilder.factory.buildCertificationIssueReport();
+      await databaseBuilder.commit();
+
+      // when
+      await certificationIssueReportRepository.delete(certificationIssueReportToDeleteId);
+
+      // then
+      const exists = await knex('certification-issue-reports').where({ id: certificationIssueReportToDeleteId }).first();
+      expect(Boolean(exists)).to.be.false;
+    });
+
+    it('should return true when deletion happened', async () => {
+      // given
+      const certificationIssueReportToDeleteId = databaseBuilder.factory.buildCertificationIssueReport().id;
+      await databaseBuilder.commit();
+
+      // when
+      const deleted = await certificationIssueReportRepository.delete(certificationIssueReportToDeleteId);
+
+      // then
+      expect(deleted).to.be.true;
+    });
+
+    it('should return false when there was nothing to delete', async () => {
+      // given
+      const certificationIssueReportToDeleteId = databaseBuilder.factory.buildCertificationIssueReport().id;
+
+      // when
+      const deleted = await certificationIssueReportRepository.delete(certificationIssueReportToDeleteId);
+
+      // then
+      expect(deleted).to.be.false;
     });
   });
 });
