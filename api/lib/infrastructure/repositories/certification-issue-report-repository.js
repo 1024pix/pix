f985598a73c789a3ec1598afdac1da445d4a6e97
P-Jeremy <jemyplu@gmail.com>
2020-12-21 15:40:43 +0100

    [API] add repo method to get certification issue report

--- api/lib/infrastructure/repositories/certification-issue-report-repository.js
+++ api/lib/infrastructure/repositories/certification-issue-report-repository.js
@@ -1,3 +1,4 @@
+const { NotFoundError } = require('../../domain/errors');
 const CertificationIssueReportBookshelf = require('../data/certification-issue-report');
 const bookshelfToDomainConverter = require('../utils/bookshelf-to-domain-converter');
 
@@ -5,6 +6,20 @@ module.exports = {
   async save(certificationIssueReport) {
     const newCertificationIssueReport = await new CertificationIssueReportBookshelf(certificationIssueReport).save();
     return bookshelfToDomainConverter.buildDomainObject(CertificationIssueReportBookshelf, newCertificationIssueReport);
+  },
+
+  async get(id) {
+    try {
+      const certificationIssueReport = await CertificationIssueReportBookshelf
+        .where({ id })
+        .fetch({ require: true });
+      return bookshelfToDomainConverter.buildDomainObject(CertificationIssueReportBookshelf, certificationIssueReport);
+    } catch (err) {
+      if (err instanceof CertificationIssueReportBookshelf.NotFoundError) {
+        throw new NotFoundError('Le signalement n\'existe pas');
+      }
+      throw err;
+    }
   },
 
   async delete(id) {
