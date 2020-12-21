7e151444f2f9e36762d5c17c2bed72ace4608972
Laura Bergoens <laura.bergoens@pix.fr>
2020-12-21 19:13:26 +0100

    feat: create new model campaignToJoin and repository

---
+++ api/tests/unit/domain/models/CampaignToJoin_test.js
@@ -0,0 +1,59 @@
+const { expect, domainBuilder } = require('../../../test-helper');
+const { types } = require('../../../../lib/domain/models/CampaignToJoin');
+
+describe('Unit | Domain | Models | CampaignToJoin', () => {
+
+  describe('#isAssessment', () => {
+    it('should return true if the campaign is of type ASSESSMENT', () => {
+      // given
+      const campaignToJoin = domainBuilder.buildCampaignToJoin({ type: types.ASSESSMENT });
+
+      // when / then
+      expect(campaignToJoin.isAssessment()).to.be.true;
+    });
+
+    it('should return false if the campaign is not of type ASSESSMENT', () => {
+      // given
+      const campaignToJoin = domainBuilder.buildCampaignToJoin({ type: types.PROFILES_COLLECTION });
+
+      // when / then
+      expect(campaignToJoin.isAssessment()).to.be.false;
+    });
+  });
+
+  describe('#isProfilesCollection', () => {
+    it('should return true if the campaign is of type PROFILES_COLLECTION', () => {
+      // given
+      const campaignToJoin = domainBuilder.buildCampaignToJoin({ type: types.PROFILES_COLLECTION });
+
+      // when / then
+      expect(campaignToJoin.isProfilesCollection()).to.be.true;
+    });
+
+    it('should return false if the campaign is not of type PROFILES_COLLECTION', () => {
+      // given
+      const campaignToJoin = domainBuilder.buildCampaignToJoin({ type: types.ASSESSMENT });
+
+      // when / then
+      expect(campaignToJoin.isProfilesCollection()).to.be.false;
+    });
+  });
+
+  describe('#isArchived', () => {
+    it('should return true if the campaign is archived', () => {
+      // given
+      const campaignToJoin = domainBuilder.buildCampaignToJoin({ archivedAt: new Date('2020-02-02') });
+
+      // when / then
+      expect(campaignToJoin.isArchived()).to.be.true;
+    });
+
+    it('should return false if the campaign is not archived', () => {
+      // given
+      const campaignToJoin = domainBuilder.buildCampaignToJoin({ archivedAt: null });
+
+      // when / then
+      expect(campaignToJoin.isArchived()).to.be.false;
+    });
+  });
+});
