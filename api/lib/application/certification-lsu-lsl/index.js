2da87bd399032e1564f9f8c1b3bbd27d7b1955c7
yahya youness <yahyayounes@hotmail.fr>
2020-12-22 16:49:26 +0100

    Add open-api documentation for LSU/LSL api

--- api/lib/application/certification-lsu-lsl/index.js
+++ api/lib/application/certification-lsu-lsl/index.js
@@ -1,7 +1,12 @@
 const certificationController = require('./certification-controller');
 const { featureToggles } = require('../../config');
+const Joi = require('@hapi/joi');
+
+const responseErrorObjectDoc = require('../../infrastructure/open-api-doc/livret-scolaire/response-object-error-doc');
+const certificationsResultsResponseDoc = require('../../infrastructure/open-api-doc/livret-scolaire/certifications-results-doc');
 
 exports.register = async function(server) {
+
   server.route([
     {
       method: 'GET',
@@ -10,10 +15,26 @@ exports.register = async function(server) {
         auth: (!featureToggles.isApiForLSULSLEnabled) ? false : true,
         handler: certificationController.getCertificationsByOrganizationUAI,
         notes: [
-          '- **Route en version v0 sans authentification**\n' +
+          '- **API for LSU/LSL qui nécessite une authentification de type client credentiel grant**\n' +
           '- Récupération des résultats de certifications pour une organisation accompagnée du référentiel des compétences',
         ],
         tags: ['api', 'organisation', 'certifications', 'livret-scolaire'],
+        response: {
+          status: {
+            200: certificationsResultsResponseDoc,
+            401: responseErrorObjectDoc,
+            403: responseErrorObjectDoc,
+          },
+          schema: certificationsResultsResponseDoc,
+        },
+        validate: {
+          params: Joi.object({
+            uai: Joi.string().required().description('RNE/UAI (unité administrative immatriculée) '),
+          }),
+          headers: Joi.object({
+            'authorization': Joi.string().required().description('Bearer Access token to access to API '),
+          }).unknown(),
+        },
       },
     },
   ]);
