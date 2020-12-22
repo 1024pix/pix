4073170d9e0983b1225922956bfd924dfb7bea89
Laura Bergoens <laura.bergoens@pix.fr>
2020-12-22 17:00:44 +0100

    [API] Add new column questionNumber everywhere back side

--- api/lib/infrastructure/serializers/jsonapi/certification-report-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/certification-report-serializer.js
@@ -19,6 +19,7 @@ module.exports = {
           'category',
           'description',
           'subcategory',
+          'questionNumber',
         ],
       },
     }).serialize(certificationReports);
