26849e694d51998523c0c093600d6bc4ef362e0d
Pierre TOP (TOPI) <ptop@octo.com>
2020-12-22 15:05:38 +0100

    [API] Throw error if password check not called with arguments [BSR]

--- api/tests/unit/domain/services/encryption-service_test.js
+++ api/tests/unit/domain/services/encryption-service_test.js
@@ -8,38 +8,68 @@ describe('Unit | Service | Encryption', () => {
 
   describe('#checkPassword', () => {
 
-    describe('#when password and hash are matching', async() => {
-      it('should resolve', async () => {
+    describe('when password and hash are matching', async() => {
+
+      it('should resolve to undefined', async () => {
+
         // given
         const password = 'my-real-password';
         /* eslint-disable no-sync */
-        const passwordHashed = bcrypt.hashSync(password, 1);
+        const passwordHash = bcrypt.hashSync(password, 1);
 
         // when
-        const error = await catchErr(encryptionService.checkPassword)({
-          password,
-          passwordHashed,
+        const result = await encryptionService.checkPassword({
+          rawPassword: password,
+          hashedPassword: passwordHash,
         });
 
         // then
-        expect(error).to.be.an.instanceof(PasswordNotMatching);
+        expect(result).to.be.undefined;
+
       });
     });
 
-    describe('#when password and hash are not matching', async () => {
+    describe('when password and hash are not matching', async () => {
+
       it('should reject a PasswordNotMatching error ', async () => {
+
         // given
         const password = 'my-expected-password';
-        const passwordHashed = 'ABCDEF1234';
+        const passwordHash = 'ABCDEF1234';
 
         // when
         const error = await catchErr(encryptionService.checkPassword)({
-          password,
-          passwordHashed,
+          rawPassword: password,
+          hashedPassword: passwordHash,
         });
+
         // then
         expect(error).to.be.an.instanceof(PasswordNotMatching);
+
       });
+
+    });
+
+    describe('when password is not supplied', async () => {
+
+      it('should reject, but not a PasswordNotMatching error ', async () => {
+
+        // given
+        const password = undefined;
+        /* eslint-disable no-sync */
+        const passwordHash = bcrypt.hashSync('my-real-password', 1);
+
+        try {
+          await encryptionService.checkPassword({
+            rawPassword: password,
+            hashedPassword: passwordHash,
+          });
+        } catch (error) {
+          expect(error).not.to.be.an.instanceof(PasswordNotMatching);
+        }
+
+      });
+
     });
 
   });
