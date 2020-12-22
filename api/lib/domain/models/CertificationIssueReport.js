78f7068997f5937d75012ccf2d3838c6ec627547
Laura Bergoens <laura.bergoens@pix.fr>
2020-12-22 16:06:41 +0100

    [API] reinforce validation when creating new issue

--- api/lib/domain/models/CertificationIssueReport.js
+++ api/lib/domain/models/CertificationIssueReport.js
@@ -2,12 +2,40 @@ const Joi = require('@hapi/joi');
 const { InvalidCertificationIssueReportForSaving } = require('../errors');
 const { CertificationIssueReportCategories, CertificationIssueReportSubcategories } = require('./CertificationIssueReportCategory');
 
-const certificationIssueReportValidationJoiSchema = Joi.object({
+const categoryOtherJoiSchema = Joi.object({
   certificationCourseId: Joi.number().required().empty(null),
-  category: Joi.string().required().valid(...Object.values(CertificationIssueReportCategories)),
-  description: Joi.string().allow(null).optional(),
-  subcategory: Joi.string().allow(null).optional().valid(...Object.values(CertificationIssueReportSubcategories)),
+  category: Joi.string().required().valid(CertificationIssueReportCategories.OTHER),
+  description: Joi.string().trim().required(),
+  subcategory: Joi.string().min(0).max(0).allow('').allow(null).optional(),
 });
+
+const categoryLateOrLeavingJoiSchema = Joi.object({
+  certificationCourseId: Joi.number().required().empty(null),
+  category: Joi.string().required().valid(CertificationIssueReportCategories.LATE_OR_LEAVING),
+  description: Joi.string().trim().required(),
+  subcategory: Joi.string().required().valid(CertificationIssueReportSubcategories.LEFT_EXAM_ROOM, CertificationIssueReportSubcategories.SIGNATURE_ISSUE),
+});
+
+const categoryCandidateInformationChangesJoiSchema = Joi.object({
+  certificationCourseId: Joi.number().required().empty(null),
+  category: Joi.string().required().valid(CertificationIssueReportCategories.CANDIDATE_INFORMATIONS_CHANGES),
+  description: Joi.string().trim().required(),
+  subcategory: Joi.string().required().valid(CertificationIssueReportSubcategories.NAME_OR_BIRTHDATE, CertificationIssueReportSubcategories.EXTRA_TIME_PERCENTAGE),
+});
+
+const categoryConnectionOrEndScreenJoiSchema = Joi.object({
+  certificationCourseId: Joi.number().required().empty(null),
+  category: Joi.string().required().valid(CertificationIssueReportCategories.CONNECTION_OR_END_SCREEN),
+  description: Joi.string().trim().min(0).max(0).allow('').allow(null).optional(),
+  subcategory: Joi.string().min(0).max(0).allow('').allow(null).optional(),
+});
+
+const categorySchemas = {
+  [CertificationIssueReportCategories.OTHER]: categoryOtherJoiSchema,
+  [CertificationIssueReportCategories.LATE_OR_LEAVING]: categoryLateOrLeavingJoiSchema,
+  [CertificationIssueReportCategories.CANDIDATE_INFORMATIONS_CHANGES]: categoryCandidateInformationChangesJoiSchema,
+  [CertificationIssueReportCategories.CONNECTION_OR_END_SCREEN]: categoryConnectionOrEndScreenJoiSchema,
+};
 
 class CertificationIssueReport {
   constructor(
@@ -44,7 +72,11 @@ class CertificationIssueReport {
   }
 
   validate() {
-    const { error } = certificationIssueReportValidationJoiSchema.validate(this, { allowUnknown: true });
+    const schemaToUse = categorySchemas[this.category];
+    if (!schemaToUse) {
+      throw new InvalidCertificationIssueReportForSaving(`Unknown category : ${this.category}`);
+    }
+    const { error } = schemaToUse.validate(this, { allowUnknown: true });
     if (error) {
       throw new InvalidCertificationIssueReportForSaving(error);
     }
