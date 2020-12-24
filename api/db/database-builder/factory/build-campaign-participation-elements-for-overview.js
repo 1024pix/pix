3e2c47129d2fa16f79c28642584a1216e6132b9a
Stephane Bedeau <sbedeau@octo.com>
2020-12-24 12:14:51 +0100

    Add buildCampaignParticipationForOverview db builder

---
+++ api/db/database-builder/factory/build-campaign-participation-elements-for-overview.js
@@ -0,0 +1,42 @@
+const Assessment = require('../../../lib/domain/models/Assessment');
+const buildAssessment = require('./build-assessment');
+const buildOrganization = require('./build-organization');
+const buildCampaignParticipation = require('./build-campaign-participation');
+const buildCampaign = require('./build-campaign');
+
+module.exports = ({ userId, index, lastAssessmentState, campaignParticipationCreatedAt, isShared }) => {
+  const organization = buildOrganization({
+    name: `${index} - My organization`,
+  });
+
+  const campaign = buildCampaign({
+    organizationId: organization.id,
+    title: `${index} - My campaign`,
+    createdAt: new Date('2000-01-01T10:00:00Z'),
+    archivedAt: null,
+  });
+
+  const campaignParticipation = buildCampaignParticipation({
+    userId,
+    createdAt: campaignParticipationCreatedAt,
+    campaignId: campaign.id,
+    isShared,
+  });
+
+  buildAssessment({
+    userId,
+    campaignParticipationId: campaignParticipation.id,
+    state: Assessment.states.COMPLETED,
+    createdAt: new Date('2000-07-01T10:00:00Z'),
+  });
+
+  buildAssessment({
+    userId,
+    campaignParticipationId: campaignParticipation.id,
+    state: lastAssessmentState,
+    createdAt: new Date('2000-07-02T10:00:00Z'),
+  });
+
+  return { campaign, campaignParticipation, organization };
+};
+
