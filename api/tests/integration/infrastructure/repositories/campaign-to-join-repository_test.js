79196ade51d3904762edf3c7c8a61948c0b2447e
Laura Bergoens <laura.bergoens@pix.fr>
2020-12-27 11:30:02 +0100

    feat: add repo method to get campaign by code

--- api/tests/integration/infrastructure/repositories/campaign-to-join-repository_test.js
+++ api/tests/integration/infrastructure/repositories/campaign-to-join-repository_test.js
@@ -11,6 +11,7 @@ describe('Integration | Repository | CampaignToJoin', () => {
       // given
       const organization = databaseBuilder.factory.buildOrganization();
       const expectedCampaign = databaseBuilder.factory.buildCampaign({ organizationId: organization.id });
+      databaseBuilder.factory.buildCampaign();
       await databaseBuilder.commit();
 
       // when
@@ -42,6 +43,52 @@ describe('Integration | Repository | CampaignToJoin', () => {
 
       // when
       const error = await catchErr(campaignToJoinRepository.get)(existingId + 1000);
+
+      // then
+      expect(error).to.be.instanceOf(NotFoundError);
+    });
+  });
+
+  describe('#getByCode', () => {
+
+    it('should return the CampaignToJoin by its code', async () => {
+      // given
+      const code = 'LAURA123';
+      const organization = databaseBuilder.factory.buildOrganization();
+      const expectedCampaign = databaseBuilder.factory.buildCampaign({ code, organizationId: organization.id });
+      databaseBuilder.factory.buildCampaign();
+      await databaseBuilder.commit();
+
+      // when
+      const actualCampaign = await campaignToJoinRepository.getByCode(code);
+
+      // then
+      expect(actualCampaign).to.be.instanceOf(CampaignToJoin);
+      expect(actualCampaign.id).to.equal(expectedCampaign.id);
+      expect(actualCampaign.code).to.deep.equal(expectedCampaign.code);
+      expect(actualCampaign.title).to.deep.equal(expectedCampaign.title);
+      expect(actualCampaign.idPixLabel).to.deep.equal(expectedCampaign.idPixLabel);
+      expect(actualCampaign.customLandingPageText).to.deep.equal(expectedCampaign.customLandingPageText);
+      expect(actualCampaign.externalIdHelpImageUrl).to.deep.equal(expectedCampaign.externalIdHelpImageUrl);
+      expect(actualCampaign.alternativeTextToExternalIdHelpImage).to.deep.equal(expectedCampaign.alternativeTextToExternalIdHelpImage);
+      expect(actualCampaign.archivedAt).to.deep.equal(expectedCampaign.archivedAt);
+      expect(actualCampaign.type).to.deep.equal(expectedCampaign.type);
+      expect(actualCampaign.organizationId).to.deep.equal(organization.id);
+      expect(actualCampaign.organizationName).to.deep.equal(organization.name);
+      expect(actualCampaign.organizationType).to.deep.equal(organization.type);
+      expect(actualCampaign.organizationLogoUrl).to.deep.equal(organization.logoUrl);
+      expect(actualCampaign.isRestricted).to.deep.equal(organization.isManagingStudents);
+    });
+
+    it('should throw a NotFoundError when no campaign exists with given code', async () => {
+      // given
+      const code = 'LAURA123';
+      const organizationId = databaseBuilder.factory.buildOrganization().id;
+      databaseBuilder.factory.buildCampaign({ code, organizationId });
+      await databaseBuilder.commit();
+
+      // when
+      const error = await catchErr(campaignToJoinRepository.getByCode)('LAURA456');
 
       // then
       expect(error).to.be.instanceOf(NotFoundError);
