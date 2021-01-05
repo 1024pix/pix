0af6696f28b87dd131726e454f379bec8040619b
ajoanny <ajoanny@octo.com>
2021-01-05 15:04:20 +0100

    [ORGA] create division filter in participations view

---
+++ orga/app/components/routes/authenticated/campaign/assessment/list.js
@@ -0,0 +1,20 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class List extends Component {
+  @service currentUser;
+
+  get displayDivisionFilter() {
+    return this.currentUser.isSCOManagingStudents;
+  }
+
+  get divisionOptions() {
+    return this.args.campaign.divisions.map(({ name }) => ({ value: name }));
+  }
+
+  @action
+  onSelectDivision(divisions) {
+    this.args.triggerFiltering({ divisions });
+  }
+}
--- orga/app/routes/authenticated/campaigns/campaign/assessments.js
+++ orga/app/routes/authenticated/campaigns/campaign/assessments.js
@@ -1,8 +1,8 @@
 import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
+import { action } from '@ember/object';
 
 export default class AssessmentsRoute extends Route {
-
   queryParams = {
     pageNumber: {
       refreshModel: true,
@@ -10,19 +10,34 @@ export default class AssessmentsRoute extends Route {
     pageSize: {
       refreshModel: true,
     },
+    divisions: {
+      refreshModel: true,
+    },
   };
+
+  @action
+  loading(transition) {
+    if (transition.from && transition.from.name === 'authenticated.campaigns.campaign.assessments') {
+      return false;
+    }
+  }
 
   model(params) {
     const campaign = this.modelFor('authenticated.campaigns.campaign');
     return RSVP.hash({
       campaign,
-      campaignAssessmentParticipationSummaries: this.store.query('campaignAssessmentParticipationSummary', {
-        page: {
-          number: params.pageNumber,
-          size: params.pageSize,
-        },
-        campaignId: campaign.id,
-      }),
+      campaignAssessmentParticipationSummaries: this.fetchSummaries({ campaignId: campaign.id, ...params }),
+    });
+  }
+
+  fetchSummaries(params) {
+    return this.store.query('campaignAssessmentParticipationSummary', {
+      page: {
+        number: params.pageNumber,
+        size: params.pageSize,
+      },
+      filter: { divisions: params.divisions },
+      campaignId: params.campaignId,
     });
   }
 
@@ -30,6 +45,7 @@ export default class AssessmentsRoute extends Route {
     if (isExiting) {
       controller.pageNumber = 1;
       controller.pageSize = 10;
+      controller.divisions = [];
     }
   }
 }
