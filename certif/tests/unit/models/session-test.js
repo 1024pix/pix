ac5c17bb97ee9bd9087278a9ca888eafc607184a
P-Jeremy <jemyplu@gmail.com>
2021-01-05 11:21:28 +0100

    [CERTIF] Add issue report dl button

--- certif/tests/unit/models/session-test.js
+++ certif/tests/unit/models/session-test.js
@@ -10,6 +10,7 @@ module('Unit | Model | session', function(hooks) {
   module('#displayStatus', function() {
 
     test('it should return the correct displayName', function(assert) {
+      // given
       const store = this.owner.lookup('service:store');
       const model1 = run(() => store.createRecord('session', {
         id: 123,
@@ -20,26 +21,44 @@ module('Unit | Model | session', function(hooks) {
         status: FINALIZED,
       }));
 
+      // when/then
       assert.equal(model1.displayStatus, 'Créée');
       assert.equal(model2.displayStatus, 'Finalisée');
     });
   });
 
-  module('#urlToDownloadAttendanceSheet', function() {
-
+  module('#urlToUpload', function() {
     test('it should return the correct urlToUpload', function(assert) {
+      // given
       const store = this.owner.lookup('service:store');
       const model = run(() => store.createRecord('session', { id: 1 }));
 
+      // when/then
       assert.equal(model.urlToUpload, `${config.APP.API_HOST}/api/sessions/1/certification-candidates/import`);
     });
+  });
 
+  module('#urlToDownloadAttendanceSheet', function() {
     test('it should return the correct urlToDownloadAttendanceSheet', function(assert) {
+      // given
       const store = this.owner.lookup('service:store');
       const model = run(() => store.createRecord('session', { id: 1 }));
       model.session = { data: { authenticated: { access_token: '123' } } };
 
+      // when/then
       assert.equal(model.urlToDownloadAttendanceSheet, `${config.APP.API_HOST}/api/sessions/1/attendance-sheet?accessToken=123`);
+    });
+  });
+
+  module('#urlToDownloadSessionIssueReportSheet', function() {
+    test('it should return the correct urlToDownloadSessionIssueReportSheet', function(assert) {
+      // given
+      const store = this.owner.lookup('service:store');
+      const model = run(() => store.createRecord('session', { id: 1 }));
+      model.session = { data: { authenticated: { access_token: '123' } } };
+
+      // when/then
+      assert.equal(model.urlToDownloadSessionIssueReportSheet, config.urlToDownloadSessionIssueReportSheet);
     });
   });
 });
