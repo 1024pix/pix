5d7d4ee6bdf60e80d0ce68f6455783299f0af86c
Thierry Loesch <tloesch@octo.com>
2021-01-05 14:48:14 +0100

    [API] call refresh token when access token is expired

--- api/lib/infrastructure/externals/pole-emploi/pole-emploi-notifier.js
+++ api/lib/infrastructure/externals/pole-emploi/pole-emploi-notifier.js
@@ -3,6 +3,7 @@ const authenticationMethodRepository = require('../../repositories/authenticatio
 const AuthenticationMethod = require('../../../domain/models/AuthenticationMethod');
 const httpAgent = require('../../http/http-agent');
 const settings = require('../../../config');
+const querystring = require('querystring');
 const { UnexpectedUserAccount } = require('../../../domain/errors');
 
 module.exports = {
@@ -12,6 +13,27 @@ module.exports = {
     if (!accessToken) {
       throw new UnexpectedUserAccount({ message: 'Le compte utilisateur n\'est pas rattaché à l\'organisation Pôle Emploi' });
     }
+
+    const expiredDate = _.get(authenticationMethod, 'authenticationComplement.expiredDate');
+    const refreshToken = _.get(authenticationMethod, 'authenticationComplement.refreshToken');
+    if (!refreshToken || expiredDate <= new Date()) {
+      const data = {
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken,
+        client_secret: settings.poleEmploi.clientSecret,
+        client_id: settings.poleEmploi.clientId,
+      };
+
+      await httpAgent.post(
+        settings.poleEmploi.tokenUrl,
+        querystring.stringify(data),
+        {
+          headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        },
+      );
+
+    }
+
     const url = settings.poleEmploi.sendingUrl;
     const headers = {
       'Authorization': `Bearer ${accessToken}`,
--- api/tests/unit/infrastructure/externals/pole-emploi/pole-emploi-notifier_test.js
+++ api/tests/unit/infrastructure/externals/pole-emploi/pole-emploi-notifier_test.js
@@ -4,6 +4,7 @@ const httpAgent = require('../../../../../lib/infrastructure/http/http-agent');
 const authenticationMethodRepository = require('../../../../../lib/infrastructure/repositories/authentication-method-repository');
 const { notify } = require('../../../../../lib/infrastructure/externals/pole-emploi/pole-emploi-notifier');
 const settings = require('../../../../../lib/config');
+const querystring = require('querystring');
 const { UnexpectedUserAccount } = require('../../../../../lib/domain/errors');
 
 describe('Unit | Infrastructure | Externals/Pole-Emploi | pole-emploi-notifier', () => {
@@ -38,31 +39,70 @@ describe('Unit | Infrastructure | Externals/Pole-Emploi | pole-emploi-notifier',
       expect(error.message).to.equal('Le compte utilisateur n\'est pas rattaché à l\'organisation Pôle Emploi');
     });
 
-    it('should send the notification to Pole Emploi', async () => {
-      // given
-      const poleEmploiSendingUrl = 'someUrlToPoleEmploi';
-      settings.poleEmploi.sendingUrl = poleEmploiSendingUrl;
-      const userId = 123;
-      const payload = 'somePayload';
-      const authenticationMethod = { authenticationComplement: { accessToken: 'someAccessToken' } };
-      const code = 'someCode';
-      const successState = 'someState';
-      const poleEmploiSending = domainBuilder.buildPoleEmploiSending();
-      authenticationMethodRepository.findOneByUserIdAndIdentityProvider
-        .withArgs({ userId, identityProvider: AuthenticationMethod.identityProviders.POLE_EMPLOI })
-        .resolves(authenticationMethod);
-      httpAgent.post.resolves({ isSuccessful: successState, code });
+    context('when access token is valid', () => {
 
-      // when
-      await notify(userId, payload, poleEmploiSending);
+      it('should send the notification to Pole Emploi', async () => {
+        // given
+        const poleEmploiSendingUrl = 'someUrlToPoleEmploi';
+        settings.poleEmploi.sendingUrl = poleEmploiSendingUrl;
+        const userId = 123;
+        const payload = 'somePayload';
+        const authenticationMethod = { authenticationComplement: { accessToken: 'someAccessToken' } };
+        const code = 'someCode';
+        const successState = 'someState';
+        const poleEmploiSending = domainBuilder.buildPoleEmploiSending();
+        authenticationMethodRepository.findOneByUserIdAndIdentityProvider
+          .withArgs({ userId, identityProvider: AuthenticationMethod.identityProviders.POLE_EMPLOI })
+          .resolves(authenticationMethod);
+        httpAgent.post.resolves({ isSuccessful: successState, code });
 
-      // then
-      expect(httpAgent.post).to.have.been.calledWithExactly(poleEmploiSendingUrl, payload, {
-        'Authorization': `Bearer ${authenticationMethod.authenticationComplement.accessToken}`,
-        'Content-type': 'application/json',
-        'Accept': 'application/json',
-        'Service-source': 'Pix',
+        // when
+        await notify(userId, payload, poleEmploiSending);
+
+        // then
+        expect(httpAgent.post).to.have.been.calledWithExactly(poleEmploiSendingUrl, payload, {
+          'Authorization': `Bearer ${authenticationMethod.authenticationComplement.accessToken}`,
+          'Content-type': 'application/json',
+          'Accept': 'application/json',
+          'Service-source': 'Pix',
+        });
       });
     });
+
+    context('when access token is invalid', () => {
+
+      it('should try to refresh the access token', async () => {
+        // given
+        const poleEmploiTokenUrl = 'someUrlToPoleEmploi';
+        settings.poleEmploi.tokenUrl = poleEmploiTokenUrl;
+        const userId = 123;
+        const payload = 'somePayload';
+        const expiredDate = new Date('2021-01-01');
+        const refreshToken = 'someRefreshToken';
+        const authenticationMethod = { authenticationComplement: { accessToken: 'someAccessToken', expiredDate, refreshToken } };
+        const data = {
+          grant_type: 'refresh_token',
+          refresh_token: refreshToken,
+          client_secret: settings.poleEmploi.clientSecret,
+          client_id: settings.poleEmploi.clientId,
+        };
+        const code = 'someCode';
+        const successState = 'someState';
+        const poleEmploiSending = domainBuilder.buildPoleEmploiSending();
+        authenticationMethodRepository.findOneByUserIdAndIdentityProvider
+          .withArgs({ userId, identityProvider: AuthenticationMethod.identityProviders.POLE_EMPLOI })
+          .resolves(authenticationMethod);
+        httpAgent.post.resolves({ isSuccessful: successState, code });
+
+        // when
+        await notify(userId, payload, poleEmploiSending);
+
+        // then
+        expect(httpAgent.post).to.have.been.calledWithExactly(poleEmploiTokenUrl, querystring.stringify(data), {
+          headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        });
+      });
+    });
+
   });
 });
