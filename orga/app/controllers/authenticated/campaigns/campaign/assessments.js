8f1d121216f796976bb44def732f1be0b4b43d7c
ajoanny <ajoanny@octo.com>
2021-01-05 15:04:20 +0100

    [ORGA] create division filter in participations view

--- orga/app/controllers/authenticated/campaigns/campaign/assessments.js
+++ orga/app/controllers/authenticated/campaigns/campaign/assessments.js
@@ -1,14 +1,30 @@
 import { action } from '@ember/object';
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
+import debounce from 'lodash/debounce';
+import config from 'pix-orga/config/environment';
 
 export default class AssessmentsController extends Controller {
-  queryParams = ['pageNumber', 'pageSize'];
+  queryParams = ['pageNumber', 'pageSize', 'divisions'];
+
   @tracked pageNumber = 1;
   @tracked pageSize = 10;
+  @tracked divisions = [];
 
   @action
   goToAssessmentPage(campaignId, participantId) {
     this.transitionToRoute('authenticated.campaigns.assessment', campaignId, participantId);
+  }
+
+  @action
+  triggerFiltering(filters) {
+    this.debouncedUpdateFilters(filters);
+  }
+
+  debouncedUpdateFilters = debounce(this._updateFilters, config.pagination.debounce);
+
+  _updateFilters(filters) {
+    this.pageNumber = null;
+    this.divisions = filters.divisions;
   }
 }
