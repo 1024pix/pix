6c652096e5c8309cecb096bbaeeda33163e3b53c
Stephane Bedeau <sbedeau@octo.com>
2021-01-06 14:37:12 +0100

    chore!: remove overriden a11y rules

--- mon-pix/.template-lintrc.js
+++ mon-pix/.template-lintrc.js
@@ -4,13 +4,10 @@ module.exports = {
   extends: ['octane', 'a11y'],
 
   rules: {
-    'link-href-attributes': false,
     'link-rel-noopener': false,
     'no-html-comments': false,
     'no-inline-styles': false,
-    'no-invalid-interactive': false,
     'no-negated-condition': false,
-    'no-nested-interactive': false,
     'no-quoteless-attributes': false,
     'no-triple-curlies': false,
     'no-unused-block-params': false,
