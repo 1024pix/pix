dc25790c61ba2d02d62faf74ba56a215ba335098
melanieb <melanie.boudard@gmail.com>
2021-01-06 14:38:11 +0100

    [CLEAN] Glimmerize QCM-solution-panel

--- mon-pix/app/components/qcm-solution-panel.js
+++ mon-pix/app/components/qcm-solution-panel.js
@@ -1,35 +1,21 @@
-/* eslint ember/no-classic-components: 0 */
-/* eslint ember/require-computed-property-dependencies: 0 */
-/* eslint ember/require-tagless-components: 0 */
-
-import { classNames } from '@ember-decorators/component';
-import { computed } from '@ember/object';
-import Component from '@ember/component';
-import classic from 'ember-classic-decorator';
+import Component from '@glimmer/component';
 import labeledCheckboxes from 'mon-pix/utils/labeled-checkboxes';
 import valueAsArrayOfBoolean from 'mon-pix/utils/value-as-array-of-boolean';
 import proposalsAsArray from 'mon-pix/utils/proposals-as-array';
 import isEmpty from 'lodash/isEmpty';
 
-@classic
-@classNames('qcm-solution-panel')
 export default class QcmSolutionPanel extends Component {
-  answer = null;
-  solution = null;
-  challenge = null;
 
-  @computed('solution')
   get solutionArray() {
-    const solution = this.solution;
+    const solution = this.args.solution;
     return !isEmpty(solution) ? valueAsArrayOfBoolean(solution) : [];
   }
 
-  @computed('answer')
   get labeledCheckboxes() {
-    const answer = this.answer.value;
+    const answer = this.args.answer.value;
     let checkboxes = [];
     if (!isEmpty(answer)) {
-      const proposals = this.challenge.get('proposals');
+      const proposals = this.args.challenge.get('proposals');
       const proposalsArray = proposalsAsArray(proposals);
       const answerArray = valueAsArrayOfBoolean(answer);
       checkboxes = labeledCheckboxes(proposalsArray, answerArray);
