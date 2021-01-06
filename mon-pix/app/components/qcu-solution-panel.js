713584225279040d4111ca742b756d8857c6b2d5
melanieb <melanie.boudard@gmail.com>
2021-01-06 14:38:47 +0100

    [CLEAN] Glimmerize QCU-Solution-panel

--- mon-pix/app/components/qcu-solution-panel.js
+++ mon-pix/app/components/qcu-solution-panel.js
@@ -1,28 +1,15 @@
-/* eslint ember/no-classic-components: 0 */
-/* eslint ember/require-computed-property-dependencies: 0 */
-/* eslint ember/require-tagless-components: 0 */
-
-import { classNames } from '@ember-decorators/component';
-import { computed } from '@ember/object';
-import Component from '@ember/component';
-import classic from 'ember-classic-decorator';
+import Component from '@glimmer/component';
 import labeledCheckboxes from 'mon-pix/utils/labeled-checkboxes';
 import valueAsArrayOfBoolean from 'mon-pix/utils/value-as-array-of-boolean';
 import proposalsAsArray from 'mon-pix/utils/proposals-as-array';
 import isEmpty from 'lodash/isEmpty';
 import ENV from 'mon-pix/config/environment';
 
-@classic
-@classNames('qcu-solution-panel')
 export default class QcuSolutionPanel extends Component {
-  answer = null;
-  solution = null;
-  challenge = null;
   featureFlagDisplayForWrongAnswers = ENV.APP.FT_IMPROVE_DISPLAY_FOR_WRONG_ANSWERS_FOR_QCU;
 
-  @computed('solution')
   get solutionArray() {
-    const solution = this.solution;
+    const solution = this.args.solution;
     return !isEmpty(solution) ? valueAsArrayOfBoolean(solution) : [];
   }
 
@@ -34,12 +21,11 @@ export default class QcuSolutionPanel extends Component {
     return solutionAndStatus[0];
   }
 
-  @computed('answer')
   get labeledRadios() {
-    const answer = this.answer.value;
+    const answer = this.args.answer.value;
     let radiosArray = [];
     if (!isEmpty(answer)) {
-      const proposals = this.challenge.get('proposals');
+      const proposals = this.args.challenge.get('proposals');
       const proposalsArray = proposalsAsArray(proposals);
       const answerArray = valueAsArrayOfBoolean(answer);
       radiosArray = labeledCheckboxes(proposalsArray, answerArray);
@@ -49,9 +35,9 @@ export default class QcuSolutionPanel extends Component {
   }
 
   get isAnswerValid() {
-    if (!this.answer) {
+    if (!this.args.answer) {
       return false;
     }
-    return this.solution === this.answer.value;
+    return this.args.solution === this.args.answer.value;
   }
 }
