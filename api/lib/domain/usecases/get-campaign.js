98b5cdf88ea51a76f17d6ac8aee4b21822be30ac
lisequesnel <quesnel.lise@gmail.com>
2021-01-06 18:23:10 +0100

    [API] Add badges to CampaignReport read model

--- api/lib/domain/usecases/get-campaign.js
+++ api/lib/domain/usecases/get-campaign.js
@@ -1,16 +1,18 @@
 const { NotFoundError } = require('../../domain/errors');
 
-module.exports = async function getCampaign({ campaignId, campaignReportRepository, stageRepository }) {
+module.exports = async function getCampaign({ campaignId, badgeRepository, campaignReportRepository, stageRepository }) {
   const integerCampaignId = parseInt(campaignId);
   if (!Number.isFinite(integerCampaignId)) {
     throw new NotFoundError(`Campaign not found for ID ${campaignId}`);
   }
 
-  const [campaignReport, stages] = await Promise.all([
+  const [campaignReport, badges, stages] = await Promise.all([
     campaignReportRepository.get(integerCampaignId),
+    badgeRepository.findByCampaignId(integerCampaignId),
     stageRepository.findByCampaignId(integerCampaignId),
   ]);
 
+  campaignReport.badges = badges;
   campaignReport.stages = stages;
   return campaignReport;
 };
