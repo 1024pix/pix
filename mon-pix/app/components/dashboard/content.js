31c3b35c96cedddb698887982e9c605f028952fc
celineung <celine.ung@zenika.com>
2021-01-11 17:33:25 +0100

    Display recommended competences zone in dashboard

--- mon-pix/app/components/dashboard/content.js
+++ mon-pix/app/components/dashboard/content.js
@@ -2,6 +2,11 @@ import Component from '@glimmer/component';
 
 export default class Content extends Component {
   get hasCampaignParticipationOverviews() {
-    return this.args.model.length > 0;
+    return this.args.model.campaignParticipationOverviews
+      && this.args.model.campaignParticipationOverviews.length > 0;
+  }
+
+  get hasRecommendedCompetences() {
+    return this.args.model.scorecards && this.args.model.scorecards.length > 0;
   }
 }
--- mon-pix/tests/integration/components/dashboard/content-test.js
+++ mon-pix/tests/integration/components/dashboard/content-test.js
@@ -5,40 +5,77 @@ import { find, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import setupIntlRenderingTest from '../../../helpers/setup-intl-rendering';
 
-describe('Integration | Component | Dashboard | Content', function() {
+describe.only('Integration | Component | Dashboard | Content', function() {
   setupIntlRenderingTest();
 
   it('should render component', async function() {
-    // when
-    await render(hbs`<Dashboard::Content />}`);
-
-    // then
-    expect(find('.dashboard-content')).to.exist;
-  });
-
-  it('should render campaign participation when there is at least one campaign participation overviews', async function() {
     // given
-    const campaignParticipationOverview = EmberObject.create({
-      isShared: false,
-      createdAt: '2020-12-10T15:16:20.109Z',
-      assessmentState: 'started',
-      campaignTitle: 'My campaign',
-      organizationName: 'My organization',
-    });
-    this.set('model', [campaignParticipationOverview]);
+    this.set('model', {});
 
     // when
     await render(hbs`<Dashboard::Content @model={{this.model}} />}`);
 
     // then
-    expect(find('.dashboard-content__campaign-participation-overviews')).to.exist;
+    expect(find('.dashboard-content')).to.exist;
   });
 
-  it('should not render campaign participations when there is no campaign participation overviews', async function() {
-    // when
-    await render(hbs`<Dashboard::Content />}`);
+  describe('campaign-participation-overview rendering', function() {
+    it('should render campaign participation when there is at least one campaign participation overviews', async function() {
+      // given
+      const campaignParticipationOverview = EmberObject.create({
+        isShared: false,
+        createdAt: '2020-12-10T15:16:20.109Z',
+        assessmentState: 'started',
+        campaignTitle: 'My campaign',
+        organizationName: 'My organization',
+      });
+      this.set('model', {
+        campaignParticipationOverviews: [campaignParticipationOverview],
+      });
 
-    // then
-    expect(find('.dashboard-content__campaign-participation-overviews')).to.not.exist;
+      // when
+      await render(hbs`<Dashboard::Content @model={{this.model}} />}`);
+
+      // then
+      expect(find('section[data-test-campaign-participation-overviews]')).to.exist;
+    });
+
+    it('should not render campaign participations when there is no campaign participation overviews', async function() {
+      // given
+      this.set('model', {});
+
+      // when
+      await render(hbs`<Dashboard::Content @model={{this.model}} />}`);
+
+      // then
+      expect(find('section[data-test-campaign-participation-overviews]')).to.not.exist;
+    });
+  });
+
+  describe('recommended competence-card rendering', function() {
+    it('should render competence-card when there is at least one competence-card', async function() {
+      // given
+      const scorecard = EmberObject.create();
+      this.set('model', {
+        scorecards: [scorecard],
+      });
+
+      // when
+      await render(hbs`<Dashboard::Content @model={{this.model}} />}`);
+
+      // then
+      expect(find('section[data-test-recommended-competences]')).to.exist;
+    });
+
+    it('should not render competence-card when there is no competence-card', async function() {
+      // given
+      this.set('model', {});
+
+      // when
+      await render(hbs`<Dashboard::Content @model={{this.model}} />}`);
+
+      // then
+      expect(find('section[data-test-recommended-competences]')).to.not.exist;
+    });
   });
 });
