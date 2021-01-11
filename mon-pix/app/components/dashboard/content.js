a90212e5200fdb428303fa61f15f558c12379a17
celineung <celine.ung@zenika.com>
2021-01-11 17:57:02 +0100

    Fetch scorecard datas in route

--- mon-pix/app/components/dashboard/content.js
+++ mon-pix/app/components/dashboard/content.js
@@ -2,11 +2,12 @@ import Component from '@glimmer/component';
 
 export default class Content extends Component {
   get hasCampaignParticipationOverviews() {
-    return this.args.model.campaignParticipationOverviews
-      && this.args.model.campaignParticipationOverviews.length > 0;
+    const campaignParticipationOverviews = this.args.model.campaignParticipationOverviews;
+    return campaignParticipationOverviews && campaignParticipationOverviews.length > 0;
   }
 
   get hasRecommendedCompetences() {
-    return this.args.model.scorecards && this.args.model.scorecards.length > 0;
+    const scorecards = this.args.model.scorecards;
+    return scorecards && scorecards.length > 0;
   }
 }
