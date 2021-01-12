1bf1d5c7da7b73ad8b281219828709f9dedeef0a
Stephane Bedeau <sbedeau@octo.com>
2021-01-12 16:49:14 +0100

    chore: remove classic from route not-connected

--- mon-pix/app/routes/not-connected.js
+++ mon-pix/app/routes/not-connected.js
@@ -1,6 +1,4 @@
-import classic from 'ember-classic-decorator';
 import Route from '@ember/routing/route';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-@classic
 export default class NotConnectedRoute extends Route.extend(UnauthenticatedRouteMixin) {}
