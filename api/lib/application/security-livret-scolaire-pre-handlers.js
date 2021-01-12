15ed36c2a97dacb14417bab5a7d16f177a8c3b66
yahya youness <yahyayounes@hotmail.fr>
2021-01-12 17:03:00 +0100

    Add new schema to authenticate application
    Co-authored-by: ceol <antoine.ceol@gmail.com>

---
+++ api/lib/application/security-livret-scolaire-pre-handlers.js
@@ -0,0 +1,68 @@
+const tokenService = require('../domain/services/token-service');
+const boom = require('boom');
+const app = require('../config').credentialLivretScolaireGraviteeApplication;
+const auth = require('../config').livretScolaireAuthentication;
+
+const JSONAPIError = require('jsonapi-serializer').Error;
+
+function _replyWithAuthenticationError(h) {
+  return Promise.resolve().then(() => {
+    const errorHttpStatusCode = 401;
+
+    const jsonApiError = new JSONAPIError({
+      code: errorHttpStatusCode,
+      title: 'Unauthorized access',
+      detail: 'Missing or invalid access token in request auhorization headers.',
+    });
+
+    return h.response(jsonApiError).code(errorHttpStatusCode).takeover();
+  });
+}
+
+function _replyWithAuthorizationError(h) {
+  return Promise.resolve().then(() => {
+    const errorHttpStatusCode = 403;
+
+    const jsonApiError = new JSONAPIError({
+      code: errorHttpStatusCode,
+      title: 'Forbidden access',
+      detail: 'Missing or insufficient permissions.',
+    });
+
+    return h.response(jsonApiError).code(errorHttpStatusCode).takeover();
+  });
+}
+
+const SCHEMA_JWT = 'jwt';
+
+async function checkApplicationIsAuthenticated(request, h) {
+
+  if (!request.headers.authorization) {
+    return boom.unauthorized(null, SCHEMA_JWT);
+  }
+
+  const authorizationHeader = request.headers.authorization;
+  const accessToken = tokenService.extractTokenFromAuthChain(authorizationHeader);
+
+  if (!accessToken) {
+    return _replyWithAuthenticationError(h);
+  }
+
+  const decodedAccessToken = tokenService.getDecodedToken(accessToken, auth.secret);
+
+  if (decodedAccessToken && decodedAccessToken.client_id !== app.clientId) {
+    return _replyWithAuthenticationError(h);
+  }
+
+  if (decodedAccessToken && decodedAccessToken.scope !== app.scope) {
+    return _replyWithAuthorizationError(h);
+  }
+  if (decodedAccessToken && decodedAccessToken.scope === app.scope) {
+    return h.authenticated({ credentials: { accessToken, client_id: decodedAccessToken.clientId, source: decodedAccessToken.source } });
+  }
+  return _replyWithAuthenticationError(h);
+}
+
+module.exports = {
+  checkApplicationIsAuthenticated,
+};
