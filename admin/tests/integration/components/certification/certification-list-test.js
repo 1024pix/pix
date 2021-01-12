fc97b12b047b02a05ba3d75b1b765804f4c07722
P-Jeremy <jemyplu@gmail.com>
2021-01-12 16:56:22 +0100

    [ADMIN] Display nb of issue reports w/ required action

--- admin/tests/integration/components/certification/certification-list-test.js
+++ admin/tests/integration/components/certification/certification-list-test.js
@@ -8,7 +8,7 @@ module('Integration | Component | <Certification::CertificationList/>', function
 
   setupRenderingTest(hooks);
 
-  test('sould display many certifications', async function(assert) {
+  test('should display many certifications', async function(assert) {
     // given
     this.certifications = [
       EmberObject.create({ id: 1 }),
@@ -21,5 +21,18 @@ module('Integration | Component | <Certification::CertificationList/>', function
 
     const $tableRows = this.element.querySelectorAll('tbody > tr');
     assert.equal($tableRows.length, 3);
+  });
+
+  test('should display number of certification issue reports with required action', async function(assert) {
+    // given
+    this.certifications = [
+      EmberObject.create({ id: 1, numberOfCertificationIssueReportsWithActionRequired: 2 }),
+    ];
+
+    // when
+    await render(hbs`<Certification::CertificationList @certifications={{certifications}} />`);
+
+    const numberOfCertificationIssueReportsWithRequiredAction = this.element.querySelector('tbody > tr td:nth-child(5)');
+    assert.dom(numberOfCertificationIssueReportsWithRequiredAction).hasText('2');
   });
 });
