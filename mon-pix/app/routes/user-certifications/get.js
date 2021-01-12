f8336bb8feed61bfe153df9070e4963ccbce5fa8
Stephane Bedeau <sbedeau@octo.com>
2021-01-12 17:43:57 +0100

    chore: remove classic from routes user-certifications

--- mon-pix/app/routes/user-certifications/get.js
+++ mon-pix/app/routes/user-certifications/get.js
@@ -1,16 +1,12 @@
-import classic from 'ember-classic-decorator';
 import Route from '@ember/routing/route';
 import SecuredRouteMixin from 'mon-pix/mixins/secured-route-mixin';
 
-@classic
 export default class GetRoute extends Route.extend(SecuredRouteMixin) {
-  model(params) {
-    return this.store.findRecord('certification', params.id, { reload: true })
-      .then((certification) => {
-        if (!certification.get('isPublished') || certification.get('status') !== 'validated') {
-          return this.replaceWith('/mes-certifications');
-        }
-        return certification;
-      });
+  async model(params) {
+    const certification = await this.store.findRecord('certification', params.id, { reload: true });
+    if (!certification.isPublished || certification.status !== 'validated') {
+      return this.replaceWith('/mes-certifications');
+    }
+    return certification;
   }
 }
