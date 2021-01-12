c2b04a4b282eaf1ae7d161a65990e4b0d50a9f15
celineung <celine.ung@zenika.com>
2021-01-12 17:58:13 +0100

    Display scorecard

--- mon-pix/app/components/dashboard/content.js
+++ mon-pix/app/components/dashboard/content.js
@@ -1,6 +1,9 @@
 import Component from '@glimmer/component';
+import orderBy from 'lodash/orderBy';
 
 export default class Content extends Component {
+  MAX_SCORECARDS_TO_DISPLAY = 4;
+
   get hasCampaignParticipationOverviews() {
     const campaignParticipationOverviews = this.args.model.campaignParticipationOverviews;
     return campaignParticipationOverviews && campaignParticipationOverviews.length > 0;
@@ -9,5 +12,12 @@ export default class Content extends Component {
   get hasRecommendedCompetences() {
     const scorecards = this.args.model.scorecards;
     return scorecards && scorecards.length > 0;
+  }
+
+  get recommendedScorecards() {
+    const scorecards = this.args.model.scorecards;
+    const filteredScorecards = scorecards.filter((scorecard) => scorecard.isNotStarted);
+    const orderedAndFilteredScorecards = orderBy(filteredScorecards, ['index']);
+    return orderedAndFilteredScorecards.slice(0, this.MAX_SCORECARDS_TO_DISPLAY);
   }
 }
