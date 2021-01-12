1fac9ccff690e61e0ebd435858ea2dc30a42f2d6
Stephane Bedeau <sbedeau@octo.com>
2021-01-12 14:22:15 +0100

    Glimmerize profile model

--- mon-pix/app/models/profile.js
+++ mon-pix/app/models/profile.js
@@ -1,17 +1,10 @@
-/* eslint ember/no-computed-properties-in-native-classes: 0 */
-
 import Model, { attr, hasMany } from '@ember-data/model';
-import { computed } from '@ember/object';
 
-export default class User extends Model {
-
-  // attributes
-  // includes
+export default class Profile extends Model {
   @attr('number') pixScore;
+
   @hasMany('scorecard') scorecards;
 
-  // methods
-  @computed('scorecards.@each.area')
   get areasCode() {
     return this.scorecards.mapBy('area.code').uniq();
   }
