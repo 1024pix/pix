5eebd23cc0059a04f6697c1afe01c49d0a65fb6e
Stephane Bedeau <sbedeau@octo.com>
2021-01-13 11:49:50 +0100

    chore: remove classic from route fill-in-campaign-code

--- mon-pix/app/routes/fill-in-campaign-code.js
+++ mon-pix/app/routes/fill-in-campaign-code.js
@@ -1,19 +1,17 @@
-import classic from 'ember-classic-decorator';
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
-@classic
 export default class FillInCampaignCodeRoute extends Route {
 
   @service session;
 
   beforeModel(transition) {
     if (transition.to.queryParams.externalUser) {
-      this.session.set('data.externalUser', transition.to.queryParams.externalUser);
+      this.session.data.externalUser = transition.to.queryParams.externalUser;
     }
   }
 
   resetController(controller) {
-    controller.set('campaignCode', null);
+    controller.campaignCode = null;
   }
 }
