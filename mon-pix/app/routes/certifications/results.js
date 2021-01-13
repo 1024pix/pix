0a62b03df1d61a828fc5d6f37a8f7b3fd59232d0
Stephane Bedeau <sbedeau@octo.com>
2021-01-13 11:50:37 +0100

    chore: remove classic from routes certifications

--- mon-pix/app/routes/certifications/results.js
+++ mon-pix/app/routes/certifications/results.js
@@ -1,8 +1,6 @@
-import classic from 'ember-classic-decorator';
 import SecuredRouteMixin from 'mon-pix/mixins/secured-route-mixin';
 import Route from '@ember/routing/route';
 
-@classic
 export default class ResultsRoute extends Route.extend(SecuredRouteMixin) {
   model(params) {
     // FIXME certification number is a domain attribute and should not be queried as a technical id
--- mon-pix/app/routes/certifications/start.js
+++ mon-pix/app/routes/certifications/start.js
@@ -1,9 +1,7 @@
-import classic from 'ember-classic-decorator';
 import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 import SecuredRouteMixin from 'mon-pix/mixins/secured-route-mixin';
 
-@classic
 export default class StartRoute extends Route.extend(SecuredRouteMixin) {
   @service currentUser;
 
