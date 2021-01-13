f690caf1b8edacc77ab45050cfd07c3201b87f3a
Stephane Bedeau <sbedeau@octo.com>
2021-01-13 11:53:13 +0100

    chore: remove classic from route not-found

--- mon-pix/app/routes/not-found.js
+++ mon-pix/app/routes/not-found.js
@@ -1,7 +1,5 @@
-import classic from 'ember-classic-decorator';
 import Route from '@ember/routing/route';
 
-@classic
 export default class NotFoundRoute extends Route {
   afterModel(model, transition) {
     transition.abort();
