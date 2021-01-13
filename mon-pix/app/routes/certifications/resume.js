213cc7a08128803fd6d0a848d07a5352942e4841
Stephane Bedeau <sbedeau@octo.com>
2021-01-13 11:50:37 +0100

    chore: remove classic from routes certifications

--- mon-pix/app/routes/certifications/resume.js
+++ mon-pix/app/routes/certifications/resume.js
@@ -1,11 +1,9 @@
-import classic from 'ember-classic-decorator';
 import Route from '@ember/routing/route';
 
-@classic
 export default class ResumeRoute extends Route {
   model(params) {
     const store = this.store;
     const certificationCourse = store.peekRecord('certification-course', params.certification_course_id);
-    this.replaceWith('assessments.resume', certificationCourse.get('assessment'));
+    this.replaceWith('assessments.resume', certificationCourse.assessment);
   }
 }
