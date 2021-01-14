d90b154b947df8b446376ecdd00e8601c61441d7
P-Jeremy <jemyplu@gmail.com>
2021-01-14 16:48:23 +0100

    [API] Retrieve candidates info in template

--- api/lib/infrastructure/files/candidates-import/candidates-import-placeholders.js
+++ api/lib/infrastructure/files/candidates-import/candidates-import-placeholders.js
@@ -29,9 +29,57 @@ const IMPORT_CANDIDATES_SESSION_TEMPLATE_VALUES = [
   },
 ];
 
+const IMPORT_CANDIDATES_TEMPLATE_VALUES = [
+  {
+    placeholder: 'COUNT',
+    propertyName: 'count',
+  },
+  {
+    placeholder: 'LAST_NAME',
+    propertyName: 'lastName',
+  },
+  {
+    placeholder: 'FIRST_NAME',
+    propertyName: 'firstName',
+  },
+  {
+    placeholder: '01/01/1900',
+    propertyName: 'birthdate',
+  },
+  {
+    placeholder: 'BIRTH_CITY',
+    propertyName: 'birthCity',
+  },
+  {
+    placeholder: 'BIRTH_PROVINCE_CODE',
+    propertyName: 'birthProvinceCode',
+  },
+  {
+    placeholder: 'BIRTH_COUNTRY',
+    propertyName: 'birthCountry',
+  },
+  {
+    placeholder: 'RESULT_RECIPIENT@EXAMPLE.FR',
+    propertyName: 'resultRecipientEmail',
+  },
+  {
+    placeholder: 'CONVOCATION@EXAMPLE.FR',
+    propertyName: 'email',
+  },
+  {
+    placeholder: 'EXTERNAL_ID',
+    propertyName: 'externalId',
+  },
+  {
+    placeholder: '999 %',
+    propertyName: 'extraTimePercentage',
+  },
+];
+
 const EXTRA_EMPTY_CANDIDATE_ROWS = 20;
 
 module.exports = {
   IMPORT_CANDIDATES_SESSION_TEMPLATE_VALUES,
+  IMPORT_CANDIDATES_TEMPLATE_VALUES,
   EXTRA_EMPTY_CANDIDATE_ROWS,
 };
