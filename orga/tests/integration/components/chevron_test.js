9b87237252f8b87c23ac36cec6dae9c39a9b8767
Karam Haytham <karam.haytham.mohammed@octo.com>
2021-01-15 15:00:39 +0100

    Removed accordion when no tutorials available and moved it to its own component

---
+++ orga/tests/integration/components/chevron_test.js
@@ -0,0 +1,51 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import sinon from 'sinon';
+import { render, click } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | chevron', function(hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach(function() {
+    const toggleParent = sinon.stub();
+    this.set('isOpen', false);
+    this.set('click', toggleParent);
+  });
+
+  test('it renders', async function(assert) {
+    // when
+    await render(hbs`<Chevron @isOpen={{isOpen}}  @toggleParent={{click}}/>`);
+
+    // then
+    assert.dom('button[type=button]').exists();
+    assert.dom('[aria-expanded="false"]').exists();
+  });
+
+  test('it should open the accordion when it is closed', async function(assert) {
+    // given
+    await render(hbs`<Chevron @isOpen={{isOpen}}  @toggleParent={{click}}/>`);
+
+    // when
+    await click('[data-icon="chevron-down"]');
+    this.set('isOpen', true);
+
+    // then
+    assert.dom('[aria-expanded="true"]').exists();
+
+  });
+
+  test('it should close the accordion when it already open', async function(assert) {
+    // given
+    await render(hbs`<Chevron @isOpen={{isOpen}}  @toggleParent={{click}}/>`);
+    this.set('isOpen', true);
+
+    // when
+    await click('[data-icon="chevron-up"]');
+    this.set('isOpen', false);
+
+    // then
+    assert.dom('[aria-expanded="false"]').exists();
+
+  });
+});
