c21cd52489865add4fd9ca51b81ae01055aaa318
Antoine Ceol <antoine.ceol@gmail.com>
2021-01-15 17:53:07 +0100

    [SR] Optimize queries to single query

--- api/lib/infrastructure/repositories/certification-livret-scolaire-repository.js
+++ api/lib/infrastructure/repositories/certification-livret-scolaire-repository.js
@@ -1,16 +1,13 @@
-const bluebird = require('bluebird');
-const CompetenceMarksBookshelf = require('../data/competence-mark');
 const Certificate = require('../../domain/read-models/livret-scolaire/Certificate');
-const CompetenceResults = require('../../domain/read-models/livret-scolaire/CompetenceResults');
 const PENDING_STATUS = require('../../domain/read-models/livret-scolaire/CertificateStatus').PENDING;
 const { knex } = require('../bookshelf');
 
 module.exports = {
 
   async getCertificatesByOrganizationUAI(uai) {
-    const results = await knex.with('organization_certifications_every_assess_results', (qb) => {
-      qb.select({
-        id: 'certification-courses.id',
+    const result = await knex
+      .distinct('certification-courses.id')
+      .select({
         firstName: 'schooling-registrations.firstName',
         middleName: 'schooling-registrations.middleName',
         thirdName: 'schooling-registrations.thirdName',
@@ -20,50 +17,40 @@ module.exports = {
         date: 'certification-courses.createdAt',
         verificationCode: 'certification-courses.verificationCode',
         deliveredAt: 'sessions.publishedAt',
-        isPublished: 'certification-courses.isPublished',
         certificationCenter: 'sessions.certificationCenter',
-        assessmentResultId: 'assessment-results.id',
-        status: 'assessment-results.status',
+        // eslint-disable-next-line no-restricted-syntax
+        status: knex.raw(`CASE WHEN "isPublished" THEN "assessment-results".status ELSE '${PENDING_STATUS}' END`),
         pixScore: 'assessment-results.pixScore',
       })
-        .select(knex.raw('ROW_NUMBER() OVER (PARTITION BY ?? ORDER BY ?? DESC) AS asr_row_number',
-          ['certification-courses.id', 'assessment-results.createdAt']))
-        .from('certification-courses')
-        .join('schooling-registrations', 'schooling-registrations.userId', 'certification-courses.userId')
-        .join('organizations', 'schooling-registrations.organizationId', 'organizations.id')
-        .join('assessments', 'assessments.certificationCourseId', 'certification-courses.id')
-        .join('assessment-results', 'assessment-results.assessmentId', 'assessments.id')
-        .join('sessions', 'sessions.id', 'certification-courses.sessionId')
-        .where('organizations.externalId', '=', uai);
-    })
-      .select('*')
-      .from('organization_certifications_every_assess_results')
-      .where('asr_row_number', '1')
+      .select(knex.raw('\'[\' || (string_agg(\'{ "level":\' || "competence-marks".level::VARCHAR || \', "competenceId":"\' || "competence-marks"."competence_code" || \'"}\', \',\') over (partition by "assessment-results".id)) || \']\' as "competenceResults"'))
+      .from('certification-courses')
+      .innerJoin('schooling-registrations', 'schooling-registrations.userId', 'certification-courses.userId')
+      .innerJoin('organizations', 'schooling-registrations.organizationId', 'organizations.id')
+      .innerJoin('assessments', 'assessments.certificationCourseId', 'certification-courses.id')
+      .innerJoin('assessment-results', 'assessment-results.assessmentId', 'assessments.id')
+      .innerJoin('sessions', 'sessions.id', 'certification-courses.sessionId')
+      .innerJoin('competence-marks', 'competence-marks.assessmentResultId', 'assessment-results.id')
+      .modify(_filterMostRecentAssessments)
+      .whereRaw(`LOWER("organizations"."externalId") = LOWER('${uai}')`)
+      .andWhereRaw('"last-assessment-results".id is null')
       .orderBy('lastName', 'ASC')
       .orderBy('firstName', 'ASC');
 
-    return bluebird.mapSeries(results, async (result) => {
-      //FIXME transform to get competences results by chunk of X certifications
-      const competenceResults = await _getCompetenceResults(result.assessmentResultId);
-
+    return result.map((certificate) => {
+      const competenceResults = JSON.parse(certificate.competenceResults);
       return new Certificate({
-        ...result,
-        status: result.isPublished ? result.status : PENDING_STATUS,
-        competenceResults,
+        ...certificate, competenceResults,
       });
     });
+
   },
 };
 
-async function _getCompetenceResults(assessmentResultId) {
-  const competenceMarks = await CompetenceMarksBookshelf
-    .query((qb) => {
-      qb.where('assessmentResultId', '=', assessmentResultId);
-    })
-    .orderBy('competence_code', 'asc')
-    .fetchAll({ columns: ['competence_code', 'level'], required: true });
+function _filterMostRecentAssessments(queryBuilder) {
+  queryBuilder
+    .leftJoin({ 'last-assessment-results': 'assessment-results' }, function() {
+      this.on('last-assessment-results.assessmentId', 'assessments.id')
+        .andOn('assessment-results.createdAt', '<', knex.ref('last-assessment-results.createdAt'));
+    });
 
-  return competenceMarks.models.map(({ attributes: cm }) =>
-    new CompetenceResults({ level: cm.level, competenceId: cm.competence_code }));
 }
-
