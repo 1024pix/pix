e08bb3a318eb580d064437a2d2d227a0c56a7e77
Stephane Bedeau <sbedeau@octo.com>
2021-01-15 15:38:21 +0100

    chore: glimmerize scorecard model

--- mon-pix/app/models/scorecard.js
+++ mon-pix/app/models/scorecard.js
@@ -1,8 +1,4 @@
-/* eslint ember/no-computed-properties-in-native-classes: 0 */
-
 import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
-import { equal, and } from '@ember/object/computed';
-import { computed } from '@ember/object';
 import ENV from 'mon-pix/config/environment';
 
 const NUMBER_OF_PIX_BY_LEVEL = 8;
@@ -27,40 +23,44 @@ export default class Scorecard extends Model {
   @hasMany('tutorial') tutorials;
 
   // methods
-  @and('isFinished', 'isMaxLevel') isFinishedWithMaxLevel;
+  get isFinishedWithMaxLevel() {
+    return this.isFinished && this.isMaxLevel;
+  }
 
-  @equal('status', 'COMPLETED') isFinished;
-  @equal('status', 'NOT_STARTED') isNotStarted;
-  @equal('status', 'STARTED') isStarted;
+  get isFinished() {
+    return this.status === 'COMPLETED';
+  }
 
-  @computed('level')
+  get isNotStarted() {
+    return this.status === 'NOT_STARTED';
+  }
+
+  get isStarted() {
+    return this.status === 'STARTED';
+  }
+
   get isMaxLevel() {
     return this.level >= ENV.APP.MAX_REACHABLE_LEVEL;
   }
 
-  @computed('earnedPix')
   get hasNotEarnAnything() {
     return this.earnedPix === 0;
   }
 
-  @computed('level')
   get hasNotReachLevelOne() {
     return this.level < 1;
   }
 
-  @computed('level')
   get hasReachAtLeastLevelOne() {
     return this.level >= 1;
   }
 
-  @computed('pixScoreAheadOfNextLevel')
   get percentageAheadOfNextLevel() {
     const percentage = this.pixScoreAheadOfNextLevel / NUMBER_OF_PIX_BY_LEVEL * 100;
 
     return percentage >= MAX_DISPLAYED_PERCENTAGE ? MAX_DISPLAYED_PERCENTAGE : percentage;
   }
 
-  @computed('pixScoreAheadOfNextLevel')
   get remainingPixToNextLevel() {
     return NUMBER_OF_PIX_BY_LEVEL - this.pixScoreAheadOfNextLevel;
   }
