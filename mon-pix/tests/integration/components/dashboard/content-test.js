924d86e8052b5ae2a1803da9507ab146b2884602
Stephane Bedeau <sbedeau@octo.com>
2021-01-15 17:17:43 +0100

    feat: add started competences to dashboard

--- mon-pix/tests/integration/components/dashboard/content-test.js
+++ mon-pix/tests/integration/components/dashboard/content-test.js
@@ -97,4 +97,50 @@ describe('Integration | Component | Dashboard | Content', function() {
       expect(findAll('.competence-card')).to.have.length(4);
     });
   });
+
+  describe('started competence-card rendering', function() {
+    it('should render competence-card when there is at least one competence-card started', async function() {
+      // given
+      const scorecard = { isStarted: true };
+      this.set('model', {
+        scorecards: [scorecard],
+      });
+
+      // when
+      await render(hbs`<Dashboard::Content @model={{this.model}} />}`);
+
+      // then
+      expect(find('section[data-test-started-competences]')).to.exist;
+    });
+
+    it('should not render competence-card when there is no competence-card', async function() {
+      // given
+      this.set('model', {});
+
+      // when
+      await render(hbs`<Dashboard::Content @model={{this.model}} />}`);
+
+      // then
+      expect(find('section[data-test-started-competences]')).to.not.exist;
+    });
+
+    it('should render the four first started competence cards from the received arguments', async function() {
+      // given
+      const scorecards = [
+        { id: 1, index: '1.1', isStarted: true },
+        { id: 2, index: '1.2', isStarted: true },
+        { id: 3, index: '3.1', isStarted: true },
+        { id: 5, index: '1.3', isStarted: false },
+        { id: 4, index: '2.4', isStarted: true },
+        { id: 4, index: '1.4', isStarted: true },
+      ];
+      this.set('model', { scorecards });
+
+      // when
+      await render(hbs`<Dashboard::Content @model={{this.model}} />}`);
+
+      // then
+      expect(findAll('.competence-card')).to.have.length(4);
+    });
+  });
 });
