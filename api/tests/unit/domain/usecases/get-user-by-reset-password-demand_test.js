f9a3744d5b8e7abddf850002a134ad8b0a019f4a
Christophe Charles <christophe.charles@octo.com>
2021-01-18 21:57:22 +0100

    [API] Add usecase get User By ResetPasswordDemand

---
+++ api/tests/unit/domain/usecases/get-user-by-reset-password-demand_test.js
@@ -0,0 +1,103 @@
+const { catchErr, domainBuilder, expect, sinon } = require('../../../test-helper');
+
+const User = require('../../../../lib/domain/models/User');
+const {
+  InvalidTemporaryKeyError,
+  PasswordResetDemandNotFoundError,
+  UserNotFoundError,
+} = require('../../../../lib/domain/errors');
+
+const getUserByResetPasswordDemand = require('../../../../lib/domain/usecases/get-user-by-reset-password-demand');
+
+describe('Unit | UseCase | get-user-by-reset-password-demand', () => {
+
+  const temporaryKey = 'ABCDEF123';
+  const email = 'user@example.net';
+
+  let resetPasswordService;
+  let tokenService;
+  let userRepository;
+
+  beforeEach(() => {
+    resetPasswordService = {
+      verifyDemand: sinon.stub(),
+    };
+    tokenService = {
+      decodeIfValid: sinon.stub(),
+    };
+    userRepository = {
+      getByEmail: sinon.stub(),
+    };
+
+    resetPasswordService.verifyDemand.resolves({ email });
+  });
+
+  it('should return a User with email', async () => {
+    // given
+    const user = domainBuilder.buildUser({ email });
+    userRepository.getByEmail.resolves(user);
+
+    // when
+    const result = await getUserByResetPasswordDemand({
+      temporaryKey,
+      resetPasswordService,
+      tokenService,
+      userRepository,
+    });
+
+    // then
+    expect(result).to.be.an.instanceOf(User);
+    expect(resetPasswordService.verifyDemand).to.have.been.calledWith(temporaryKey);
+    expect(tokenService.decodeIfValid).to.have.been.calledWith(temporaryKey);
+    expect(userRepository.getByEmail).to.have.been.calledWith(email);
+  });
+
+  it('should throw InvalidTemporaryKeyError if TemporaryKey is invalid', async () => {
+    // given
+    tokenService.decodeIfValid.rejects(new InvalidTemporaryKeyError());
+
+    // when
+    const error = await catchErr(getUserByResetPasswordDemand)({
+      temporaryKey,
+      resetPasswordService,
+      tokenService,
+      userRepository,
+    });
+
+    // then
+    expect(error).to.be.an.instanceOf(InvalidTemporaryKeyError);
+  });
+
+  it('should throw PasswordResetDemandNotFoundError if ResetPasswordDemand does not exist', async () => {
+    // given
+    resetPasswordService.verifyDemand.throws(new PasswordResetDemandNotFoundError());
+
+    // when
+    const error = await catchErr(getUserByResetPasswordDemand)({
+      temporaryKey,
+      resetPasswordService,
+      tokenService,
+      userRepository,
+    });
+
+    // then
+    expect(error).to.be.an.instanceOf(PasswordResetDemandNotFoundError);
+  });
+
+  it('should throw UserNotFoundError if user with the email does not exist', async () => {
+    // given
+    userRepository.getByEmail.throws(new UserNotFoundError());
+
+    // when
+    const error = await catchErr(getUserByResetPasswordDemand)({
+      temporaryKey,
+      resetPasswordService,
+      tokenService,
+      userRepository,
+    });
+
+    // then
+    expect(error).to.be.an.instanceOf(UserNotFoundError);
+  });
+
+});
