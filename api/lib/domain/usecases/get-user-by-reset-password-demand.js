a4e6b297d191ab011d25dc8620332624bc56d1df
Christophe Charles <christophe.charles@octo.com>
2021-01-18 21:57:22 +0100

    [API] Add usecase get User By ResetPasswordDemand

---
+++ api/lib/domain/usecases/get-user-by-reset-password-demand.js
@@ -0,0 +1,10 @@
+module.exports = async function getUserByResetPasswordDemand({
+  temporaryKey,
+  resetPasswordService,
+  tokenService,
+  userRepository,
+}) {
+  await tokenService.decodeIfValid(temporaryKey);
+  const { email } = await resetPasswordService.verifyDemand(temporaryKey);
+  return userRepository.getByEmail(email);
+};
