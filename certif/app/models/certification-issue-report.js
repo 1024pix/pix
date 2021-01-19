22f360c8a9bbf1ec7f354561252654c93d7ceb11
Clément Bourgoin <c@iwzr.fr>
2021-01-19 10:54:41 +0100

    [CERTIF] Update certification issue category codes and wording

--- certif/app/models/certification-issue-report.js
+++ certif/app/models/certification-issue-report.js
@@ -25,37 +25,37 @@ export const certificationIssueReportSubcategories = {
 };
 
 export const categoryToLabel = {
-  [certificationIssueReportCategories.OTHER]: 'Autre incident',
+  [certificationIssueReportCategories.OTHER]: 'Autre (si aucune des catégories ci-dessus ne correspond au signalement)',
   [certificationIssueReportCategories.CANDIDATE_INFORMATIONS_CHANGES]: 'Modification infos candidat',
   [certificationIssueReportCategories.LATE_OR_LEAVING]: 'Retard, absence ou départ',
   [certificationIssueReportCategories.FRAUD]: 'Suspicion de fraude',
   [certificationIssueReportCategories.TECHNICAL_PROBLEM]: 'Problème technique',
-  [certificationIssueReportCategories.CONNECTION_OR_END_SCREEN]: 'Connexion et fin de test : le candidat n’a pas pu terminer, faute de temps',
+  [certificationIssueReportCategories.CONNECTION_OR_END_SCREEN]: 'Le candidat n’a pas pu terminer, faute de temps',
   [certificationIssueReportCategories.IN_CHALLENGE]: 'Problème sur une épreuve',
 };
 
 export const subcategoryToLabel = {
-  [certificationIssueReportSubcategories.NAME_OR_BIRTHDATE]: 'Prénom/Nom/Date de naissance',
+  [certificationIssueReportSubcategories.NAME_OR_BIRTHDATE]: 'Modification des prénom/nom/date de naissance',
   [certificationIssueReportSubcategories.EXTRA_TIME_PERCENTAGE]: 'Ajout/modification du temps majoré',
-  [certificationIssueReportSubcategories.LEFT_EXAM_ROOM]: 'Ecran de fin de test non vu',
+  [certificationIssueReportSubcategories.LEFT_EXAM_ROOM]: 'Écran de fin de test non vu',
   [certificationIssueReportSubcategories.SIGNATURE_ISSUE]: 'Était présent(e) mais a oublié de signer, ou a signé sur la mauvaise ligne',
   [certificationIssueReportSubcategories.IMAGE_NOT_DISPLAYING]: 'L\'image ne s\'affiche pas',
-  [certificationIssueReportSubcategories.LINK_NOT_WORKING]: 'Le lien ne fonctionne pas',
   [certificationIssueReportSubcategories.EMBED_NOT_WORKING]: 'Le simulateur/l\'application ne s\'affiche pas',
   [certificationIssueReportSubcategories.FILE_NOT_OPENING]: 'Le fichier à télécharger ne s\'ouvre pas',
-  [certificationIssueReportSubcategories.WEBSITE_UNAVAILABLE]: 'Le site à visiter est mort/en maintenance/inaccessible',
-  [certificationIssueReportSubcategories.WEBSITE_BLOCKED]: 'Le site à visiter est bloqué par les restrictions réseau de l\'établissement (réseaux sociaux par ex.)',
-  [certificationIssueReportSubcategories.OTHER]: 'Autre',
+  [certificationIssueReportSubcategories.WEBSITE_UNAVAILABLE]: 'Le site à visiter est indisponible/en maintenance/inaccessible',
+  [certificationIssueReportSubcategories.WEBSITE_BLOCKED]: 'Le site est bloqué par les restrictions réseau de l\'établissement (réseaux sociaux par ex.)',
+  [certificationIssueReportSubcategories.LINK_NOT_WORKING]: 'Le lien ne fonctionne pas',
+  [certificationIssueReportSubcategories.OTHER]: 'Autre incident lié à une question',
 };
 
 export const categoryToCode = {
-  [certificationIssueReportCategories.OTHER]: 'A2',
   [certificationIssueReportCategories.CANDIDATE_INFORMATIONS_CHANGES]: 'C1-C2',
   [certificationIssueReportCategories.LATE_OR_LEAVING]: 'C3-C4',
   [certificationIssueReportCategories.CONNECTION_OR_END_SCREEN]: 'C5',
   [certificationIssueReportCategories.IN_CHALLENGE]: 'E1-E7',
   [certificationIssueReportCategories.FRAUD]: 'C6',
   [certificationIssueReportCategories.TECHNICAL_PROBLEM]: 'A1',
+  [certificationIssueReportCategories.OTHER]: 'A2',
 };
 
 export const subcategoryToCode = {
@@ -64,11 +64,11 @@ export const subcategoryToCode = {
   [certificationIssueReportSubcategories.LEFT_EXAM_ROOM]: 'C3',
   [certificationIssueReportSubcategories.SIGNATURE_ISSUE]: 'C4',
   [certificationIssueReportSubcategories.IMAGE_NOT_DISPLAYING]: 'E1',
-  [certificationIssueReportSubcategories.LINK_NOT_WORKING]: 'E2',
-  [certificationIssueReportSubcategories.EMBED_NOT_WORKING]: 'E3',
-  [certificationIssueReportSubcategories.FILE_NOT_OPENING]: 'E4',
-  [certificationIssueReportSubcategories.WEBSITE_UNAVAILABLE]: 'E5',
-  [certificationIssueReportSubcategories.WEBSITE_BLOCKED]: 'E6',
+  [certificationIssueReportSubcategories.EMBED_NOT_WORKING]: 'E2',
+  [certificationIssueReportSubcategories.FILE_NOT_OPENING]: 'E3',
+  [certificationIssueReportSubcategories.WEBSITE_UNAVAILABLE]: 'E4',
+  [certificationIssueReportSubcategories.WEBSITE_BLOCKED]: 'E5',
+  [certificationIssueReportSubcategories.LINK_NOT_WORKING]: 'E6',
   [certificationIssueReportSubcategories.OTHER]: 'E7',
 };
 
