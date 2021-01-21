c08f589a78d58e8ef12be062798c36b4ca37a7d8
yaelle <ymarsiac@octo.com>
2021-01-21 14:42:23 +0100

    to drop

--- orga/app/authenticators/oauth2.js
+++ orga/app/authenticators/oauth2.js
@@ -1,8 +1,9 @@
-import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
-import ENV from 'pix-orga/config/environment';
+import OAuth2PasswordGrant from "ember-simple-auth/authenticators/oauth2-password-grant";
+import ENV from "pix-orga/config/environment";
 
 export default class Oauth2 extends OAuth2PasswordGrant {
   serverTokenEndpoint = `${ENV.APP.API_HOST}/api/token`;
   serverTokenRevocationEndpoint = `${ENV.APP.API_HOST}/api/revoke`;
   sendClientIdAsQueryParam = true;
 }
+//coucou
