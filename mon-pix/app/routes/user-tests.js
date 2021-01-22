9bc1e3552ef83d98fff52d597b45330a3c1b93a6
Benjamin Petetot <bpetetot@gmail.com>
2021-01-22 14:24:22 +0100

    [APP] Fetch campaign participation overviews in user tests route

--- mon-pix/app/routes/user-tests.js
+++ mon-pix/app/routes/user-tests.js
@@ -1,7 +1,20 @@
+import { inject as service } from '@ember/service';
 import SecuredRouteMixin from 'mon-pix/mixins/secured-route-mixin';
 import Route from '@ember/routing/route';
 
 export default class UserTestsController extends Route.extend(SecuredRouteMixin) {
+  @service store;
+
   model() {
+    const user = this.currentUser.user;
+    const maximumDisplayed = 100;
+    const queryParams = {
+      'userId': user.id,
+      'page[number]': 1,
+      'page[size]': maximumDisplayed,
+      'filter[states]': ['ONGOING', 'TO_SHARE'],
+    };
+
+    return this.store.query('campaign-participation-overview', queryParams);
   }
 }
