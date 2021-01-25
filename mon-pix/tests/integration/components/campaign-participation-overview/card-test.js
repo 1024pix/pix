2914f43217be4e2b6bddefbf37aa3cc7b7493cf1
yaelle <ymarsiac@octo.com>
2021-01-25 17:57:08 +0100

    [APP] Add sharing date to participation card

--- mon-pix/tests/integration/components/campaign-participation-overview/card-test.js
+++ mon-pix/tests/integration/components/campaign-participation-overview/card-test.js
@@ -45,6 +45,7 @@ describe('Integration | Component | CampaignParticipationOverview | Card', funct
     expect(find('.campaign-participation-overview-card-header__tag')).to.exist;
     expect(find('.campaign-participation-overview-card-header__tag').textContent.trim()).to.equal(this.intl.t('pages.campaign-participation-overview.card.tag.started'));
     expect(find('.campaign-participation-overview-card__action').textContent.trim()).to.equal(this.intl.t('pages.campaign-participation-overview.card.resume'));
+    expect(find('.campaign-participation-overview-card-header__date').textContent.trim()).to.equal(this.intl.t('pages.campaign-participation-overview.card.started-at', { date: '10/12/2020' }));
   });
 
   it('should render component with a completed state but not shared', async function() {
@@ -63,5 +64,26 @@ describe('Integration | Component | CampaignParticipationOverview | Card', funct
     // then
     expect(find('.campaign-participation-overview-card-header__tag').textContent.trim()).to.equal(this.intl.t('pages.campaign-participation-overview.card.tag.completed'));
     expect(find('.campaign-participation-overview-card__action').textContent.trim()).to.equal(this.intl.t('pages.campaign-participation-overview.card.send'));
+    expect(find('.campaign-participation-overview-card-header__date').textContent.trim()).to.equal(this.intl.t('pages.campaign-participation-overview.card.started-at', { date: '10/12/2020' }));
+  });
+
+  it('should render component with a completed state and shared', async function() {
+    // given
+    const campaignParticipationOverview = EmberObject.create({
+      isShared: true,
+      createdAt: '2020-12-10T15:16:20.109Z',
+      sharedAt: '2020-12-18T15:16:20.109Z',
+      assessmentState: 'completed',
+      campaignTitle: 'My campaign',
+    });
+    this.set('campaignParticipationOverview', campaignParticipationOverview);
+
+    // when
+    await render(hbs`<CampaignParticipationOverview::Card @model={{this.campaignParticipationOverview}} />}`);
+
+    // then
+    expect(find('.campaign-participation-overview-card-header__tag').textContent.trim()).to.equal(this.intl.t('pages.campaign-participation-overview.card.tag.finished'));
+    expect(find('.campaign-participation-overview-card__action').textContent.trim()).to.equal(this.intl.t('pages.campaign-participation-overview.card.see-more'));
+    expect(find('.campaign-participation-overview-card-header__date').textContent.trim()).to.equal(this.intl.t('pages.campaign-participation-overview.card.finished-at', { date: '18/12/2020' }));
   });
 });
