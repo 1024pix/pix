3357de711a3b5b0879efa479b31eca291cc6fd68
Laura Bergoens <laura.bergoens@pix.fr>
2021-01-25 09:25:59 +0100

    fix(orga): service injection in user-logged-menu-test

--- orga/tests/unit/components/user-logged-menu-test.js
+++ orga/tests/unit/components/user-logged-menu-test.js
@@ -1,18 +1,23 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import Service from '@ember/service';
 import createGlimmerComponent from '../../helpers/create-glimmer-component';
 
-module('Unit | Component | user-logged-menu', (hooks) => {
-
+module('Unit | Component | user-logged-menu', function(hooks) {
   setupTest(hooks);
+
+  class currentUserStub extends Service {
+    organization = null;
+  }
 
   let component;
 
   hooks.beforeEach(function() {
     component = createGlimmerComponent('component:user-logged-menu');
+    this.owner.register('service:currentUser', currentUserStub);
   });
 
-  module('action#toggleUserMenu', () => {
+  module('action#toggleUserMenu', function() {
     test('should return false as default value', function(assert) {
       // then
       assert.equal(component.isMenuOpen, false);
@@ -34,29 +39,29 @@ module('Unit | Component | user-logged-menu', (hooks) => {
     });
   });
 
-  module('organizationNameAndExternalId', () => {
+  module('organizationNameAndExternalId', function() {
 
-    test('should return the organization name if the externalId is not defined', (assert) => {
+    test('should return the organization name if the externalId is not defined', function(assert) {
       // given
       const expectedOrganizationName = 'expectedOrganizationName';
-      const currentUser = { organization: { name: expectedOrganizationName } };
-      component.currentUser = currentUser;
+      this.owner.lookup('service:currentUser').set('organization', { name: expectedOrganizationName, externalId: null });
 
       // when
       const computedOrganizationName = component.organizationNameAndExternalId;
+
       // then
       assert.equal(computedOrganizationName, expectedOrganizationName);
     });
 
-    test('should return the organization name and externalId if the externalId is defined', (assert) => {
+    test('should return the organization name and externalId if the externalId is defined', function(assert) {
       // given
-      const expectedOrganizationName = 'expectedOrganizationName';
       const expectedExternalId = 'expectedExternalId';
-      const currentUser = { organization: { name: expectedOrganizationName, externalId: expectedExternalId } };
-      component.currentUser = currentUser;
+      const expectedOrganizationName = 'expectedOrganizationName';
+      this.owner.lookup('service:currentUser').set('organization', { name: expectedOrganizationName, externalId: expectedExternalId });
 
       // when
       const computedOrganizationName = component.organizationNameAndExternalId;
+
       // then
       assert.equal(computedOrganizationName, `${expectedOrganizationName} (${expectedExternalId})`);
     });
