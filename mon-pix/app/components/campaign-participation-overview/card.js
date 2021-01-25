cb73bc4e652e30e5872c0b2254372d3a0218b819
yaelle <ymarsiac@octo.com>
2021-01-25 18:04:39 +0100

    [APP] Add see more link in Finished participation card

--- mon-pix/app/components/campaign-participation-overview/card.js
+++ mon-pix/app/components/campaign-participation-overview/card.js
@@ -5,21 +5,21 @@ const STATUSES = {
     tagText: 'pages.campaign-participation-overview.card.tag.completed',
     tagColor: 'yellow-light',
     actionText: 'pages.campaign-participation-overview.card.send',
-    actionClass: 'button--yellow',
+    actionClass: 'button button--link button--yellow',
     dateText: 'pages.campaign-participation-overview.card.started-at',
   },
   started: {
     tagText: 'pages.campaign-participation-overview.card.tag.started',
     tagColor: 'green-light',
     actionText: 'pages.campaign-participation-overview.card.resume',
-    actionClass: '',
+    actionClass: 'button button--link',
     dateText: 'pages.campaign-participation-overview.card.started-at',
   },
   finished: {
     tagText: 'pages.campaign-participation-overview.card.tag.finished',
     tagColor: 'grey-light',
     actionText: 'pages.campaign-participation-overview.card.see-more',
-    actionClass: '',
+    actionClass: 'link campaign-participation-overview-card__see-more',
     dateText: 'pages.campaign-participation-overview.card.finished-at',
   },
 };
@@ -36,10 +36,9 @@ export default class Card extends Component {
 
   get date() {
     const isShared = this.args.model.get('isShared');
+    const sharedAt = this.args.model.get('sharedAt');
+    const createdAt = this.args.model.get('createdAt');
 
-    if (isShared) return this.args.model.get('sharedAt');
-
-    return this.args.model.get('createdAt');
-
+    return isShared ? sharedAt : createdAt;
   }
 }
