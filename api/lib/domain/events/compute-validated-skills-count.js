d36dad6694824871899c0c98b122084f6f6e36fe
ajoanny <ajoanny@octo.com>
2021-01-25 15:04:54 +0100

    [API] add handler to compute ValidatedSkillsCount

---
+++ api/lib/domain/events/compute-validated-skills-count.js
@@ -0,0 +1,21 @@
+const _ = require('lodash');
+const CampaignParticipationResultShared = require('./CampaignParticipationResultsShared');
+
+module.exports = async function computeValidatedSkillsCount({ event, campaignParticipationRepository, knowledgeElementRepository, targetProfileWithLearningContentRepository }) {
+  const campaignParticipation = await campaignParticipationRepository.get(event.campaignParticipationId, { include: ['campaign'] });
+  if (campaignParticipation.canComputeValidatedSkillsCount()) {
+
+    campaignParticipation.validatedSkillsCount = await _countValidatedSkills(campaignParticipation, knowledgeElementRepository, targetProfileWithLearningContentRepository);
+
+    await campaignParticipationRepository.update(campaignParticipation);
+  }
+};
+
+async function _countValidatedSkills(campaignParticipation, knowledgeElementRepository, targetProfileWithLearningContentRepository) {
+  const targetProfile = await targetProfileWithLearningContentRepository.getByCampaignId({ campaignId: campaignParticipation.campaignId });
+
+  const validatedSkillsCountByCompetence = await knowledgeElementRepository.countValidatedTargetedByCompetencesForOneUser(campaignParticipation.userId, campaignParticipation.sharedAt, targetProfile);
+  return _(validatedSkillsCountByCompetence).values().sum();
+}
+
+module.exports.eventType = CampaignParticipationResultShared;
---
+++ api/tests/unit/domain/events/compute-validated-skills-count_test.js
@@ -0,0 +1,77 @@
+const { expect, domainBuilder, sinon } = require('../../../test-helper');
+const computeValidatedSkillsCount = require('../../../../lib/domain/events/compute-validated-skills-count');
+const CampaignParticipationResultsShared = require('./../../../../lib/domain/events/CampaignParticipationResultsShared');
+
+describe('Unit | Domain | Events | compute-validated-skills-count', function() {
+
+  describe('eventType', () => {
+    it('returns the CampaignParticipationResultsShared', () => {
+      const eventType = computeValidatedSkillsCount.eventType;
+
+      expect(eventType).to.equals(CampaignParticipationResultsShared);
+    });
+  });
+
+  describe('.computeValidatedSkillsCount', () => {
+    let knowledgeElementRepository;
+    let campaignParticipationRepository;
+    let targetProfileWithLearningContentRepository;
+
+    beforeEach(() => {
+      campaignParticipationRepository = { get: sinon.stub(), update: sinon.stub() };
+      targetProfileWithLearningContentRepository = { getByCampaignId: sinon.stub() };
+      knowledgeElementRepository = { countValidatedTargetedByCompetencesForOneUser: sinon.stub() };
+    });
+
+    context('when the campaign is of type assessment', () => {
+      it('computes the validated skills count', async () => {
+        const targetProfile = domainBuilder.buildTargetProfile();
+        const campaign = domainBuilder.buildCampaign.ofTypeAssessment();
+        const campaignParticipation = domainBuilder.buildCampaignParticipation({ id: 1, campaignId: 2, userId: 3, campaign });
+        const event = new CampaignParticipationResultsShared({ campaignParticipationId: campaignParticipation.id });
+        const validatedSkillsCountByCompetence = {
+          'competence1': 2,
+          'competence2': 3,
+        };
+        const validatedSkillsCount = 5;
+
+        campaignParticipationRepository.get
+          .withArgs(event.campaignParticipationId, { include: ['campaign'] })
+          .resolves(campaignParticipation);
+
+        targetProfileWithLearningContentRepository.getByCampaignId
+          .withArgs({ campaignId: campaignParticipation.campaignId })
+          .resolves(targetProfile);
+
+        knowledgeElementRepository.countValidatedTargetedByCompetencesForOneUser
+          .withArgs(campaignParticipation.userId, campaignParticipation.sharedAt, targetProfile)
+          .resolves(validatedSkillsCountByCompetence);
+
+        await computeValidatedSkillsCount({ event, campaignParticipationRepository, knowledgeElementRepository, targetProfileWithLearningContentRepository });
+
+        expect(campaignParticipation.validatedSkillsCount).to.equals(validatedSkillsCount);
+        expect(campaignParticipationRepository.update).to.have.been.calledWith(campaignParticipation);
+      });
+    });
+
+    context('when the campaign is of type profile collection', () => {
+      it('does not compute the validated skills count', async () => {
+        const targetProfile = domainBuilder.buildTargetProfile();
+        const campaign = domainBuilder.buildCampaign.ofTypeProfilesCollection();
+        const campaignParticipation = domainBuilder.buildCampaignParticipation({ id: 1, campaignId: 2, userId: 3, campaign });
+        const event = new CampaignParticipationResultsShared({ campaignParticipationId: campaignParticipation.id });
+
+        campaignParticipationRepository.get.resolves(campaignParticipation);
+        targetProfileWithLearningContentRepository.getByCampaignId.resolves(targetProfile);
+        knowledgeElementRepository.countValidatedTargetedByCompetencesForOneUser.resolves({});
+
+        await computeValidatedSkillsCount({ event, campaignParticipationRepository, knowledgeElementRepository, targetProfileWithLearningContentRepository });
+
+        expect(campaignParticipationRepository.update).not.to.have.been.called;
+      });
+    });
+
+  });
+
+});
+
