ba9fb050ba31b1c23a2e8dff977aae83a700f1bc
Karam Haytham <karam.haytham.mohammed@octo.com>
2021-01-25 21:11:45 +0100

    added description to targetedTube model

--- api/lib/domain/models/TargetedTube.js
+++ api/lib/domain/models/TargetedTube.js
@@ -2,11 +2,13 @@ class TargetedTube {
   constructor({
     id,
     practicalTitle,
+    description,
     competenceId,
     skills = [],
   } = {}) {
     this.id = id;
     this.practicalTitle = practicalTitle;
+    this.description = description;
     this.competenceId = competenceId;
     this.skills = skills;
   }
