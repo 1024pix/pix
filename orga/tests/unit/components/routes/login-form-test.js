81f19038c481f3c6e034c3a878c66c7c761d3f8d
Laura Bergoens <laura.bergoens@pix.fr>
2021-01-25 09:37:17 +0100

    fix(orga): services injection in login-form-test

--- orga/tests/unit/components/routes/login-form-test.js
+++ orga/tests/unit/components/routes/login-form-test.js
@@ -1,36 +1,38 @@
 import sinon from 'sinon';
-
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import createGlimmerComponent from '../../../helpers/create-glimmer-component';
+import Service from '@ember/service';
 
-module('Unit | Component | Routes | login-form', (hooks) => {
-
+module('Unit | Component | Routes | login-form', function(hooks) {
   setupTest(hooks);
+
+  class SessionStub extends Service {
+    authenticate = null;
+  }
 
   let component;
 
   hooks.beforeEach(function() {
     component = createGlimmerComponent('component:routes/login-form');
+    this.owner.register('service:session', SessionStub);
   });
 
-  module('#authenticate', () => {
+  module('#authenticate', function() {
 
-    test('should save email without spaces', (assert) => {
+    test('should save email without spaces', function(assert) {
       // given
       const emailWithSpaces = '    user@example.net  ';
       component.email = emailWithSpaces;
-
-      const _authenticateStub = sinon.stub().resolves();
-      component._authenticate = _authenticateStub;
-
+      const authenticateStub = sinon.stub().resolves();
+      this.owner.lookup('service:session').set('authenticate', authenticateStub);
       const expectedEmail = emailWithSpaces.trim();
 
       // when
       component.authenticate(new Event('stub'));
 
       // then
-      assert.ok(_authenticateStub.calledWith(sinon.match.any, expectedEmail));
+      assert.ok(authenticateStub.calledWith(sinon.match.any, expectedEmail));
     });
   });
 
