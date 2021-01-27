f472874224c481fb4617c4d2bd64f25786a48655
Pierre TOP (TOPI) <ptop@octo.com>
2021-01-27 14:45:18 +0100

    [API] Fix id specification start boundary

---
+++ api/tests/unit/domain/validators/id-specification_test.js
@@ -0,0 +1,48 @@
+const { expect } = require('../../../test-helper');
+const { idSpecification } = require('../../../../lib/domain/validators/id-specification');
+
+describe('Unit | Domain | Validators | id-specification', () => {
+
+  describe('#idSpecification', () => {
+
+    context('when id is valid', () => {
+
+      it('should not reject', () => {
+        // given
+        const validId = 1;
+
+        // when
+        const { error } = idSpecification.validate(validId);
+
+        // then
+        expect(error).to.be.undefined;
+      });
+    });
+
+    context('when id is invalid', () => {
+
+      it('should reject outside of lower bound', async () => {
+        // given
+        const lowerBoundOutOfRangeId = 0;
+
+        // when
+        const { error } = idSpecification.validate(lowerBoundOutOfRangeId);
+
+        // then
+        expect(error.message).to.equal('"value" must be greater than or equal to 1');
+      });
+
+      it('should reject outside of upper bound', async () => {
+        // given
+        const upperBoundOutOfRangeId = 2147483648;
+
+        // when
+        const { error } = idSpecification.validate(upperBoundOutOfRangeId);
+
+        // then
+        expect(error.message).to.equal('"value" must be less than or equal to 2147483647');
+      });
+
+    });
+  });
+});
