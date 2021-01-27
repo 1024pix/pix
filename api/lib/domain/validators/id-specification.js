1c5bdc8b9572aaf81c7dc7ae3a9c782f0a8d48bf
Pierre TOP (TOPI) <ptop@octo.com>
2021-01-27 15:29:24 +0100

    [API] Introduce implementation-specific type

--- api/lib/domain/validators/id-specification.js
+++ api/lib/domain/validators/id-specification.js
@@ -4,7 +4,9 @@ const sequenceStartAt = 1;
 // Max 32 bits signed integer, our most common id type in Postgres
 const sequencesEndsAt = 2 ** 31 - 1;
 const idSpecification = Joi.number().integer().min(sequenceStartAt).max(sequencesEndsAt).required();
-const userId = idSpecification;
+
+const positive32bitIntegerType = idSpecification;
+const userId = positive32bitIntegerType;
 
 module.exports = {
   idSpecification,
