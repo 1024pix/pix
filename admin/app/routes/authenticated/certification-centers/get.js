76e16cdf8a1f19ac92818ab05bad834e7a235637
Christophe Charles <christophe.charles@octo.com>
2021-01-29 08:58:12 +0100

    [ADMIN] Update routes certification-centers get

--- admin/app/routes/authenticated/certification-centers/get.js
+++ admin/app/routes/authenticated/certification-centers/get.js
@@ -1,9 +1,20 @@
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import RSVP from 'rsvp';
 
 export default class CertificationCentersGetRoute extends Route.extend(AuthenticatedRouteMixin) {
 
-  model(params) {
-    return this.store.findRecord('certification-center', params.certification_center_id);
+  async model(params) {
+    const certificationCenter = await this.store.findRecord('certification-center', params.certification_center_id);
+    const certificationCenterMemberships = await this.store.query('certification-center-membership', {
+      filter: {
+        certificationCenterId: certificationCenter.id,
+      },
+    });
+
+    return RSVP.hash({
+      certificationCenterMemberships,
+      certificationCenter,
+    });
   }
 }
--- admin/tests/acceptance/authenticated/certification-centers/get-test.js
+++ admin/tests/acceptance/authenticated/certification-centers/get-test.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 import { module, test } from 'qunit';
 import { currentURL, visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
@@ -10,37 +12,45 @@ module('Acceptance | authenticated/certification-centers/get', function(hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
+  const certificationCenterData = {
+    name: 'Center 1',
+    externalId: 'ABCDEF',
+    type: 'SCO',
+  };
+
+  let certificationCenter;
+  let certificationCenterMembership1;
   let currentUser;
 
   hooks.beforeEach(async function() {
     currentUser = server.create('user');
     await createAuthenticateSession({ userId: currentUser.id });
+
+    certificationCenter = server.create('certification-center', certificationCenterData);
+
+    certificationCenterMembership1 = server.create('certification-center-membership', {
+      createdAt: new Date('2018-02-15T05:06:07Z'),
+      certificationCenter,
+      user: server.create('user'),
+    });
+    server.create('certification-center-membership', {
+      createdAt: new Date('2019-02-15T05:06:07Z'),
+      certificationCenter,
+      user: server.create('user'),
+    });
   });
 
   test('should access Certification center page by URL /certification-centers/:id', async function(assert) {
-    // given
-    server.create('certification-center');
-
     // when
-    await visit('/certification-centers/1');
+    await visit(`/certification-centers/${certificationCenter.id}`);
 
     // then
     assert.equal(currentURL(), '/certification-centers/1');
   });
 
   test('should display Certification center detail', async function(assert) {
-    // given
-    const certificationCenter = {
-      name: 'Center 1',
-      externalId: 'ABCDEF',
-      type: 'SCO',
-    };
-    server.create('certification-center', 1, {
-      ...certificationCenter,
-    });
-
     // when
-    await visit('/certification-centers/1');
+    await visit(`/certification-centers/${certificationCenter.id}`);
 
     // then
     assert.contains(certificationCenter.name);
@@ -48,4 +58,22 @@ module('Acceptance | authenticated/certification-centers/get', function(hooks) {
     assert.contains(certificationCenter.type);
   });
 
+  test('should display Certification center memberships', async function(assert) {
+    // given
+    const expectedDate1 = moment(certificationCenterMembership1.createdAt).format('DD-MM-YYYY - HH:mm:ss');
+
+    // when
+    await visit(`/certification-centers/${certificationCenter.id}`);
+
+    // then
+    assert.dom('[aria-label="Membre"]').exists({ count: 2 });
+
+    assert.dom('[aria-label="Membre"]:first-child td:nth-child(2)')
+      .hasText(certificationCenterMembership1.user.id);
+
+    assert.contains(certificationCenterMembership1.user.firstName);
+    assert.contains(certificationCenterMembership1.user.lastName);
+    assert.contains(certificationCenterMembership1.user.email);
+    assert.contains(expectedDate1);
+  });
 });
