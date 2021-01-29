1376f6eea985a9dda8f7911518079187a73db15c
melanieb <melanie.boudard@gmail.com>
2021-01-29 11:20:45 +0100

    Create `test-algo` application

---
+++ high-level-tests/test-algo/index.js
@@ -0,0 +1,46 @@
+'use strict';
+require('dotenv').config();
+
+const smartRandom = require('../../api/lib/domain/services/smart-random/smart-random');
+const dataFetcher = require('../../api/lib/domain/services/smart-random/data-fetcher');
+const challengeRepository = require('../../api/lib/infrastructure/repositories/challenge-repository');
+const skillRepository = require('../../api/lib/infrastructure/repositories/skill-repository');
+const improvementService = require('../../api/lib/domain/services/improvement-service');
+
+async function launch_test() {
+  const locale = 'fr';
+  const lastAnswer = null;
+  const allAnswers = [];
+  const knowledgeElements = [];
+  const assessment = {
+    id: null,
+    competenceId: 'recsvLz0W2ShyfD63',
+  };
+
+  const knowledgeElementRepository = {
+    findUniqByUserId: () => [],
+  };
+  const answerRepository = {
+    findByAssessment: () => [],
+  };
+  const { targetSkills, challenges } = await dataFetcher.fetchForCompetenceEvaluations({
+    assessment,
+    answerRepository,
+    challengeRepository,
+    knowledgeElementRepository,
+    skillRepository,
+    improvementService,
+  });
+
+  const result = smartRandom.getPossibleSkillsForNextChallenge({
+    knowledgeElements,
+    challenges,
+    targetSkills,
+    lastAnswer,
+    allAnswers,
+    locale,
+  });
+  console.log(result);
+}
+
+launch_test();
