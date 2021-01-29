4cac3a9791c264fe01f45c64a45bc39a66aa3a00
Andreia Pena <andreia.pena@pix.fr>
2021-01-29 16:05:02 +0100

    [ORGA][BSR] Add missing tests

--- orga/tests/integration/components/routes/authenticated/team/items-test.js
+++ orga/tests/integration/components/routes/authenticated/team/items-test.js
@@ -1,27 +1,44 @@
 import { module, test } from 'qunit';
+import sinon from 'sinon';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import { run } from '@ember/runloop';
+import { click, fillIn, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | routes/authenticated/team | list-items | items', function(hooks) {
 
   setupRenderingTest(hooks);
+  let adminMembership;
+  let memberMembership;
 
-  test('For an admin, it should display an administrator firstName, lastName, role and edit button', async function(assert) {
-    // given
-    const store = this.owner.lookup('service:store');
-    const membership = store.createRecord('membership', {
+  hooks.beforeEach(function() {
+    adminMembership = {
       id: 1,
+      displayRole: 'Administrateur',
       organizationRole: 'ADMIN',
-      user: store.createRecord('user', {
+      user: {
         id: 111,
         firstName: 'Gigi',
         lastName: 'La Terreur',
-      }),
-    });
+      },
+      save: sinon.stub(),
+    };
 
-    this.set('membership', membership);
+    memberMembership = {
+      id: 1,
+      displayRole: 'Membre',
+      organizationRole: 'MEMBER',
+      user: {
+        id: 111,
+        firstName: 'Jojo',
+        lastName: 'La Panique',
+      },
+      save: sinon.stub(),
+    };
+  });
+
+  test('For an admin, it should display an administrator firstName, lastName, role and edit button', async function(assert) {
+    // given
+    this.set('membership', adminMembership);
 
     // when
     await render(hbs`<Routes::Authenticated::Team::Items @membership={{membership}}/>`);
@@ -31,33 +48,85 @@ module('Integration | Component | routes/authenticated/team | list-items | items
     assert.contains('Gigi');
     assert.contains('Administrateur');
     assert.contains('Modifier le rôle');
-
   });
 
   test('For a member, it should display a firstName, lastName, role and edit button', async function(assert) {
     // given
-    const store = this.owner.lookup('service:store');
-    const membership =
-      run(() => store.createRecord('membership', {
-        id: 1,
-        organizationRole: 'MEMBER',
-        user: run(() => store.createRecord('user', {
-          id: 111,
-          firstName: 'Gigi',
-          lastName: 'La Terreur',
-        })),
-      }));
-
-    this.set('membership', membership);
+    this.set('membership', memberMembership);
 
     // when
     await render(hbs`<Routes::Authenticated::Team::Items @membership={{membership}}/>`);
 
     // then
-    assert.contains('La Terreur');
-    assert.contains('Gigi');
+    assert.contains('La Panique');
+    assert.contains('Jojo');
     assert.contains('Membre');
     assert.contains('Modifier le rôle');
   });
 
+  module('When edit organization role button is clicked', function() {
+
+    test('it should show update and save button, and show the drop down to select role to update', async function(assert) {
+      // given
+      this.set('membership', memberMembership);
+
+      await render(hbs`<Routes::Authenticated::Team::Items @membership={{membership}}/>`);
+
+      // when
+      await click('#edit-organization-role');
+
+      // then
+      assert.dom('.zone-save-cancel-role').exists({ count: 1 });
+      assert.dom('#save-organization-role').exists({ count: 1 });
+      assert.dom('#cancel-update-organization-role').exists({ count: 1 });
+    });
+
+    test('it should cancel the update if using the cancel button', async function(assert) {
+      // given
+      this.set('membership', memberMembership);
+
+      await render(hbs`<Routes::Authenticated::Team::Items @membership={{membership}}/>`);
+
+      await click('#edit-organization-role');
+
+      // when
+      await click('#cancel-update-organization-role');
+
+      // then
+      assert.equal(memberMembership.organizationRole, 'MEMBER');
+      sinon.assert.notCalled(memberMembership.save);
+    });
+
+    test('it should change the value of the drop down to Administrateur and display the modified role', async function(assert) {
+      // given
+      this.set('membership', memberMembership);
+
+      await render(hbs`<Routes::Authenticated::Team::Items @membership={{membership}}/>`);
+      await click('#edit-organization-role');
+
+      // when
+      await fillIn('select', 'ADMIN');
+      await click('#save-organization-role');
+
+      // then
+      assert.equal(memberMembership.organizationRole, 'ADMIN');
+      sinon.assert.called(memberMembership.save);
+    });
+
+    test('it should change the value of the drop down to Membre and display the modified role', async function(assert) {
+      // given
+      this.set('membership', adminMembership);
+
+      await render(hbs`<Routes::Authenticated::Team::Items @membership={{membership}}/>`);
+      await click('#edit-organization-role');
+
+      // when
+      await fillIn('select', 'MEMBER');
+      await click('#save-organization-role');
+
+      // then
+      assert.equal(adminMembership.organizationRole, 'MEMBER');
+      sinon.assert.called(adminMembership.save);
+    });
+  });
 });
