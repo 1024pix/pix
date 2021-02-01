88d244046e02101c867c3c53eac824909ba3fe90
melanieb <melanie.boudard@gmail.com>
2021-02-01 15:25:23 +0100

    Use pickChallenge service to retrieve challenge from competenceId

--- high-level-tests/test-algo/index.js
+++ high-level-tests/test-algo/index.js
@@ -6,8 +6,10 @@ const dataFetcher = require('../../api/lib/domain/services/smart-random/data-fet
 const challengeRepository = require('../../api/lib/infrastructure/repositories/challenge-repository');
 const skillRepository = require('../../api/lib/infrastructure/repositories/skill-repository');
 const improvementService = require('../../api/lib/domain/services/improvement-service');
+const pickChallengeService = require('../../api/lib/domain/services/pick-challenge-service');
 
 async function launch_test() {
+
   const locale = 'fr';
   const lastAnswer = null;
   const allAnswers = [];
@@ -40,7 +42,15 @@ async function launch_test() {
     allAnswers,
     locale,
   });
-  console.log(result);
+
+  const challenge = pickChallengeService.pickChallenge({
+    skills: result.possibleSkillsForNextChallenge,
+    randomSeed: assessment.id,
+    locale: locale,
+  });
+  console.log(challenge);
+
+  process.exit(0);
 }
 
 launch_test();
