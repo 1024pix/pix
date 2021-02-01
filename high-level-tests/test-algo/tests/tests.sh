a7ff20ae51173e11e5ec9bb741f6471aa892a3cd
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2021-02-01 17:25:05 +0100

    Add locale as parameter

--- high-level-tests/test-algo/tests/tests.sh
+++ high-level-tests/test-algo/tests/tests.sh
@@ -20,3 +20,5 @@ testExit() {
 
 testExit 'should fetch challenge when competenceId is specified' 'npm start -- --competenceId example_competence_id' 0
 testExit 'should return error when no competenceId is specified' "npm start" 1
+testExit 'should return error when specified locale does not exist' "npm start --competenceId example_competence_id --locale non-existing-locale" 1
+testExit 'should fetch challenge when competenceId and locale are specified' "npm start --competenceId example_competence_id --locale fr" 1
