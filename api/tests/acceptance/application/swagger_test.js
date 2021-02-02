edd98aabc82413604e215578b39dc8bfbbdf4784
Antoine Ceol <antoine.ceol@gmail.com>
2021-02-02 11:52:39 +0100

    Update Hapi dependencies after Joi upgrade

---
+++ api/tests/acceptance/application/swagger_test.js
@@ -0,0 +1,49 @@
+const { expect } = require('../../test-helper');
+const createServer = require('../../../server');
+
+describe('Acceptance | lib | swagger', () => {
+
+  let server;
+
+  beforeEach(async () => {
+    server = await createServer();
+  });
+
+  describe('GET swagger.json', () => {
+
+    describe('Resource access management', () => {
+      it('should respond with a 200', async () => {
+        // given
+        const options = {
+          method: 'GET',
+          url: '/api/swagger.json',
+          headers: {},
+        };
+        // when
+        const response = await server.inject(options);
+
+        // then
+        expect(response.statusCode).to.equal(200);
+      });
+    });
+  });
+
+  describe('GET /livret-scolaire/swagger.json', () => {
+
+    describe('Resource access management', () => {
+      it('should respond with a 200', async () => {
+        // given
+        const options = {
+          method: 'GET',
+          url: '/livret-scolaire/swagger.json',
+          headers: {},
+        };
+        // when
+        const response = await server.inject(options);
+
+        // then
+        expect(response.statusCode).to.equal(200);
+      });
+    });
+  });
+});
