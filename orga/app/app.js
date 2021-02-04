10fe8e4dd651413da2b8a56c7f813762b311929d
lisequesnel <quesnel.lise@gmail.com>
2021-02-04 17:36:16 +0100

    [ORGA] Add polyfills for intl

--- orga/app/app.js
+++ orga/app/app.js
@@ -3,6 +3,12 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
+import '@formatjs/intl-locale/polyfill';
+import '@formatjs/intl-pluralrules/polyfill';
+import '@formatjs/intl-pluralrules/locale-data/en';
+import '@formatjs/intl-pluralrules/locale-data/fr';
+import '@formatjs/intl-getcanonicallocales/polyfill';
+
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
