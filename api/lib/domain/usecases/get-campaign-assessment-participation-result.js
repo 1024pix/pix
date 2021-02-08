23c9a1be65e9e9021636badbfeac2ed7479e7ba8
Lise Quesnel <quesnel.lise@gmail.com>
2021-02-08 16:43:06 +0100

    [API] Transmit locale from usecase to repository

--- api/lib/domain/usecases/get-campaign-assessment-participation-result.js
+++ api/lib/domain/usecases/get-campaign-assessment-participation-result.js
@@ -1,9 +1,17 @@
 const { UserNotAuthorizedToAccessEntity } = require('../errors');
 
-module.exports = async function getCampaignAssessmentParticipationResult({ userId, campaignId, campaignParticipationId, campaignRepository, campaignAssessmentParticipationResultRepository }) {
+module.exports = async function getCampaignAssessmentParticipationResult(
+  {
+    userId,
+    campaignId,
+    campaignParticipationId,
+    campaignRepository,
+    campaignAssessmentParticipationResultRepository,
+    locale,
+  } = {}) {
   if (!(await campaignRepository.checkIfUserOrganizationHasAccessToCampaign(campaignId, userId))) {
     throw new UserNotAuthorizedToAccessEntity('User does not belong to the organization that owns the campaign');
   }
 
-  return campaignAssessmentParticipationResultRepository.getByCampaignIdAndCampaignParticipationId({ campaignId, campaignParticipationId });
+  return campaignAssessmentParticipationResultRepository.getByCampaignIdAndCampaignParticipationId({ campaignId, campaignParticipationId, locale });
 };
