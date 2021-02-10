b99dbbb7134ff3b357ad028f2cdc9863eb60e8b2
Christophe Charles <christophe.charles@octo.com>
2021-02-10 13:10:47 +0100

    [ADMIN] Add createRecord method in adapter

--- admin/tests/unit/adapters/certification-center-membership-test.js
+++ admin/tests/unit/adapters/certification-center-membership-test.js
@@ -1,16 +1,19 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import sinon from 'sinon';
 
 module('Unit | Adapter | certificationCenterMembership', function(hooks) {
+
   setupTest(hooks);
 
   let adapter;
 
   hooks.beforeEach(function() {
     adapter = this.owner.lookup('adapter:certification-center-membership');
+    adapter.ajax = sinon.stub().resolves();
   });
 
-  module('#urlForQuery', () => {
+  module('#urlForQuery', function() {
 
     test('should build query url from certificationCenter id', async function(assert) {
       const query = { filter: { certificationCenterId: '1' } };
@@ -18,6 +21,36 @@ module('Unit | Adapter | certificationCenterMembership', function(hooks) {
 
       assert.ok(url.endsWith('/api/certification-centers/1/certification-center-memberships'));
       assert.equal(query.filter.certificationCenterId, undefined);
+    });
+  });
+
+  module('#createRecord', function() {
+
+    module('when createByEmail is true', function() {
+
+      test('should call /api/certification-centers/id/certification-center-memberships', async function(assert) {
+        // given
+        const certificationCenterId = 1;
+        const email = 'user@example.net';
+
+        const snapshot = {
+          adapterOptions: {
+            createByEmail: true,
+            certificationCenterId,
+            email,
+          },
+        };
+        const expectedUrl = `http://localhost:3000/api/certification-centers/${certificationCenterId}/certification-center-memberships`;
+        const expectedMethod = 'POST';
+        const expectedPayload = { data: { email } };
+
+        // when
+        await adapter.createRecord(null, null, snapshot);
+
+        // then
+        sinon.assert.calledWith(adapter.ajax, expectedUrl, expectedMethod, expectedPayload);
+        assert.ok(true);
+      });
     });
   });
 });
