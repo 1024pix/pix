c511c390d824c6103fbcde1007bb7f9ac4a714c7
Laura Bergoens <laura.bergoens@pix.fr>
2021-02-11 22:49:01 +0100

    fix(certif): service injection in controller/terms-of-service

--- certif/tests/unit/controllers/terms-of-service-test.js
+++ certif/tests/unit/controllers/terms-of-service-test.js
@@ -1,6 +1,7 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import sinon from 'sinon';
+import Service from '@ember/service';
 
 module('Unit | Controller | terms-of-service', function(hooks) {
   setupTest(hooks);
@@ -9,17 +10,23 @@ module('Unit | Controller | terms-of-service', function(hooks) {
   module('#action submit', function(hooks) {
 
     hooks.beforeEach(function() {
+      class CurrentUserStub extends Service {
+        certificationPointOfContact = { save: sinon.stub().resolves() };
+      }
+      this.owner.register('service:current-user', CurrentUserStub);
       controller = this.owner.lookup('controller:terms-of-service');
       controller.transitionToRoute = sinon.stub().resolves();
-      controller.currentUser = { certificationPointOfContact: { save: sinon.stub().resolves() } };
     });
 
     test('it should save acceptance of terms of service', async function(assert) {
+      // given
+      const saveStub = this.owner.lookup('service:current-user').certificationPointOfContact.save;
+
       // when
       await controller.send('submit');
 
       // then
-      sinon.assert.calledWith(controller.currentUser.certificationPointOfContact.save, { adapterOptions: { acceptPixCertifTermsOfService: true } });
+      sinon.assert.calledWith(saveStub, { adapterOptions: { acceptPixCertifTermsOfService: true } });
       assert.ok(controller);
     });
 
