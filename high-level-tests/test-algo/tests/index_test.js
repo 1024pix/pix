c049fa62e1be0f290597cb07973c65ab9fdd171f
melanieb <melanie.boudard@gmail.com>
2021-02-11 12:02:51 +0100

    Remember KE created

--- high-level-tests/test-algo/tests/index_test.js
+++ high-level-tests/test-algo/tests/index_test.js
@@ -2,17 +2,57 @@ const sinon = require('sinon');
 const chai = require('chai');
 const expect = chai.expect;
 const { answerTheChallenge } = require('../algo');
+const KnowledgeElement = require('../../../api/lib/domain/models/KnowledgeElement');
 
 describe('#answerTheChallenge', () => {
-  it('should return the list of answers with previous answer with the new one', () => {
-    // given
-    const previousAnswers = [ { id: 1, result: 'ko' } ];
-    const challenge = { id: 'recId' };
 
+  let previousAnswers;
+  let previousKE;
+  let newKe;
+  let challenge;
+
+  beforeEach(() => {
+    previousAnswers = [ { id: 1, result: 'ko' } ];
+    previousKE = [{ id: 1 }];
+    challenge = { id: 'recId' };
+    newKe = { id: 'KE-id' };
+    sinon.stub(KnowledgeElement, "createKnowledgeElementsForAnswer").returns([newKe]);
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  })
+
+  it('should return the list of answers with previous answer with the new one', () => {
     // when
-    const allAnswers = answerTheChallenge({ challenge, allAnswers: previousAnswers });
+    const result = answerTheChallenge({
+        challenge,
+        allAnswers: previousAnswers,
+        allKnowledgeElements: previousKE,
+        targetSkills: [],
+        userId: 1
+      });
 
     // then
-    expect(allAnswers).lengthOf(previousAnswers.length+1);
+    expect(result.updatedAnswers).lengthOf(previousAnswers.length + 1);
+    expect(result.updatedAnswers[0]).to.be.deep.equal(previousAnswers[0]);
+    expect(result.updatedAnswers[1].challengeId).to.be.equal(challenge.id);
+  });
+
+  it('should return the list of previous KE with the new one', () => {
+
+    // when
+    const result = answerTheChallenge({
+      challenge,
+      allAnswers: previousAnswers,
+      allKnowledgeElements: previousKE,
+      targetSkills: [],
+      userId: 1
+    });
+
+    // then
+    expect(result.updatedKnowledgeElements).lengthOf(previousKE.length + 1);
+    expect(result.updatedKnowledgeElements[0]).to.deep.equal(previousKE[0]);
+    expect(result.updatedKnowledgeElements[1]).to.deep.equal(newKe);
   });
 });
