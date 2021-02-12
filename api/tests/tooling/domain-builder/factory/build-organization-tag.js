46461226fa8665798735719ce74a1bee100c5420
Laura Bergoens <laura.bergoens@pix.fr>
2021-02-13 00:04:03 +0100

    refactor: get rid of faker in Domain-buildOrganizationTag

--- api/tests/tooling/domain-builder/factory/build-organization-tag.js
+++ api/tests/tooling/domain-builder/factory/build-organization-tag.js
@@ -1,11 +1,10 @@
-const faker = require('faker');
+
 const OrganizationTag = require('../../../../lib/domain/models/OrganizationTag');
 
-module.exports = function buildOrganizationTag(
-  {
-    id = faker.random.number(),
-    organizationId = faker.random.number(),
-    tagId = faker.random.number(),
-  } = {}) {
+module.exports = function buildOrganizationTag({
+  id = 123,
+  organizationId = 456,
+  tagId = 789,
+} = {}) {
   return new OrganizationTag({ id, organizationId, tagId });
 };
