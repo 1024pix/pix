226a8f667bbb98944836ada0a80c51bb00cbac14
Laura Bergoens <laura.bergoens@pix.fr>
2021-02-12 23:37:26 +0100

    refactor: get rid of faker in Domain-buildCampaignCollectiveResults

--- api/tests/tooling/domain-builder/factory/build-campaign-collective-result.js
+++ api/tests/tooling/domain-builder/factory/build-campaign-collective-result.js
@@ -1,14 +1,12 @@
-const faker = require('faker');
 const { buildTargetProfileWithLearningContent } = require('./build-target-profile-with-learning-content');
 const CampaignCollectiveResult = require('../../../../lib/domain/read-models/CampaignCollectiveResult');
 
-module.exports = function buildCampaignCollectiveResult(
-  {
-    id = faker.random.number(),
-    targetProfile = buildTargetProfileWithLearningContent(),
-    participantsValidatedKECountByCompetenceId = [],
-    participantCount = 0,
-  } = {}) {
+module.exports = function buildCampaignCollectiveResult({
+  id = 123,
+  targetProfile = buildTargetProfileWithLearningContent(),
+  participantsValidatedKECountByCompetenceId = [],
+  participantCount = 0,
+} = {}) {
   const campaignCollectiveResult = new CampaignCollectiveResult({ id, targetProfile });
 
   if (participantCount) {
