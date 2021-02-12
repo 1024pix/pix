e5df7eafdd5f6ab5545b754b6b5fff21ab240ffe
Laura Bergoens <laura.bergoens@pix.fr>
2021-02-13 00:28:04 +0100

    refactor: get rid of faker in Domain-buildUserDetailsForAdmin

--- api/tests/tooling/domain-builder/factory/build-user-details-for-admin.js
+++ api/tests/tooling/domain-builder/factory/build-user-details-for-admin.js
@@ -1,23 +1,17 @@
-const faker = require('faker');
-const { isUndefined } = require('lodash');
-
 const UserDetailsForAdmin = require('../../../../lib/domain/models/UserDetailsForAdmin');
 
-module.exports = function buildUserDetailsForAdmin(
-  {
-    id = faker.random.number(),
-    firstName = faker.name.firstName(),
-    lastName = faker.name.lastName(),
-    email,
-    username = 'jean.bono1234',
-    cgu = true,
-    pixCertifTermsOfServiceAccepted = false,
-    pixOrgaTermsOfServiceAccepted = false,
-    isAuthenticatedFromGAR = false,
-    schoolingRegistrations = [],
-  } = {}) {
-
-  email = isUndefined(email) ? faker.internet.exampleEmail(firstName, lastName).toLowerCase() : email || null;
+module.exports = function buildUserDetailsForAdmin({
+  id = 123,
+  firstName = 'Louis',
+  lastName = 'Philippe',
+  email = 'louis.philippe@example.net',
+  username = 'jean.bono1234',
+  cgu = true,
+  pixCertifTermsOfServiceAccepted = false,
+  pixOrgaTermsOfServiceAccepted = false,
+  isAuthenticatedFromGAR = false,
+  schoolingRegistrations = [],
+} = {}) {
 
   return new UserDetailsForAdmin({
     id, firstName, lastName, email, username,
