68bb87f8b7ce8c840c4d36b2877eac811ad3fdce
Laura Bergoens <laura.bergoens@pix.fr>
2021-02-12 21:50:25 +0100

    refactor: get rid of faker in DB-buildPartnerCertification

--- api/db/database-builder/factory/build-partner-certification.js
+++ api/db/database-builder/factory/build-partner-certification.js
@@ -1,12 +1,14 @@
-const faker = require('faker');
 const databaseBuffer = require('../database-buffer');
 const buildBadge = require('./build-badge');
+const buildCertificationCourse = require('./build-certification-course');
+const _ = require('lodash');
 
 module.exports = function buildPartnerCertification({
   certificationCourseId,
   partnerKey,
-  acquired = faker.random.boolean(),
+  acquired = true,
 }) {
+  certificationCourseId = _.isUndefined(certificationCourseId) ? buildCertificationCourse().id : certificationCourseId;
   partnerKey = partnerKey ? partnerKey : buildBadge().key;
   return databaseBuffer.objectsToInsert.push({
     tableName: 'partner-certifications', values: { certificationCourseId, partnerKey, acquired },
