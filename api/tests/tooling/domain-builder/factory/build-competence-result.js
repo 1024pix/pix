b1727f4277cdcdef6ab5194e095f844482d93339
Laura Bergoens <laura.bergoens@pix.fr>
2021-02-12 23:54:30 +0100

    refactor: get rid of faker in Domain-buildCompetenceResult

--- api/tests/tooling/domain-builder/factory/build-competence-result.js
+++ api/tests/tooling/domain-builder/factory/build-competence-result.js
@@ -1,18 +1,15 @@
 const CompetenceResult = require('../../../../lib/domain/models/CompetenceResult');
 
-const faker = require('faker');
-
-module.exports = function buildCompetenceResult(
-  {
-    id = 1,
-    name = faker.random.words(),
-    index = '1.1',
-    areaColor = 'jaffa',
-    areaName,
-    totalSkillsCount = 10,
-    testedSkillsCount = 8,
-    validatedSkillsCount = 5,
-  } = {}) {
+module.exports = function buildCompetenceResult({
+  id = 1,
+  name = 'name',
+  index = '1.1',
+  areaColor = 'jaffa',
+  areaName,
+  totalSkillsCount = 10,
+  testedSkillsCount = 8,
+  validatedSkillsCount = 5,
+} = {}) {
 
   return new CompetenceResult({
     id,
