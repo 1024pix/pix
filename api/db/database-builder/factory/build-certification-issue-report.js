72f1a5860658b6c84948f9f0215ad67c50c311a1
Laura Bergoens <laura.bergoens@pix.fr>
2021-02-12 21:29:55 +0100

    refactor: get rid of faker in DB-buildCertificationIssueReport

--- api/db/database-builder/factory/build-certification-issue-report.js
+++ api/db/database-builder/factory/build-certification-issue-report.js
@@ -1,14 +1,13 @@
-const faker = require('faker');
 const buildCertificationCourse = require('./build-certification-course');
 const _ = require('lodash');
 const databaseBuffer = require('../database-buffer');
 const { CertificationIssueReportCategories } = require('../../../lib/domain/models/CertificationIssueReportCategory');
 
 module.exports = function buildCertificationIssueReport({
-  id,
+  id = databaseBuffer.getNextId(),
   certificationCourseId,
   category = CertificationIssueReportCategories.OTHER,
-  description = faker.lorem.sentence(),
+  description = 'Une super description',
   subcategory = null,
   questionNumber = null,
 } = {}) {
