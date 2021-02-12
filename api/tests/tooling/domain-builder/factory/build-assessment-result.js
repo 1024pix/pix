b910e92710d45c7c447dfcd1629428635a101821
Laura Bergoens <laura.bergoens@pix.fr>
2021-02-12 23:22:30 +0100

    refactor: get rid of faker in Domain-buildAssessmentResult

--- api/tests/tooling/domain-builder/factory/build-assessment-result.js
+++ api/tests/tooling/domain-builder/factory/build-assessment-result.js
@@ -1,18 +1,17 @@
-const faker = require('faker');
-const { status: assessmentStatuses } = require('../../../../lib/domain/models/AssessmentResult');
+const { status: assessmentResultStatuses } = require('../../../../lib/domain/models/AssessmentResult');
 const AssessmentResult = require('../../../../lib/domain/models/AssessmentResult');
 
 module.exports = function buildAssessmentResult({
+  id = 123,
   pixScore = 31,
-  status = faker.random.objectElement(assessmentStatuses),
+  status = assessmentResultStatuses.VALIDATED,
   emitter = 'PIX-ALGO',
   commentForJury = 'Comment for Jury',
   commentForCandidate = 'Comment for Candidate',
   commentForOrganization = 'Comment for Organization',
-  id = faker.random.number(),
   createdAt = new Date('2018-01-12T01:02:03Z'),
   juryId = 1,
-  assessmentId = faker.random.number(),
+  assessmentId = 456,
   competenceMarks = [],
 } = {}) {
 
