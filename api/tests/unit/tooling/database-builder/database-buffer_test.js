841748015b597756ba465829f177db23918ef7a5
Laura Bergoens <laura.bergoens@pix.fr>
2021-02-12 23:16:24 +0100

    cleanup: remove increment id logic in databaseBuffer.pushInsertable method

--- api/tests/unit/tooling/database-builder/database-buffer_test.js
+++ api/tests/unit/tooling/database-builder/database-buffer_test.js
@@ -35,20 +35,6 @@ describe('Unit | Tooling | DatabaseBuilder | database-buffer', () => {
       ]);
     });
 
-    it('should add an object to insert with a custom id property', () => {
-      // given
-      const tableName = 'someTableName';
-      const values = { sessionId: 123, a: 'aVal', b: 'bVal' };
-
-      // when
-      databaseBuffer.pushInsertable({ tableName, values, customIdKey: 'sessionId' });
-
-      // then
-      expect(databaseBuffer.objectsToInsert).to.deep.equal([
-        { tableName, values },
-      ]);
-    });
-
     it('should return inserted values', () => {
       // given
       const tableName = 'someTableName';
@@ -59,68 +45,6 @@ describe('Unit | Tooling | DatabaseBuilder | database-buffer', () => {
 
       // then
       expect(expectedValues).to.deep.equal(values);
-    });
-
-    context('when no id is provided but there is a custom id key', () => {
-
-      it('should add an object to insert with a custom id property', () => {
-        // given
-        const tableName = 'someTableName';
-        const values = { a: 'aVal', b: 'bVal' };
-        const expectedId = databaseBuffer.nextId;
-
-        // when
-        databaseBuffer.pushInsertable({ tableName, values, customIdKey: 'sessionId' });
-
-        // then
-        expect(databaseBuffer.objectsToInsert).to.deep.equal([
-          { tableName, values: { ...values, sessionId: expectedId } },
-        ]);
-      });
-    });
-
-    context('when no id is provided along with the object', () => {
-
-      it('should add an id property along with original properties in the values', () => {
-        // given
-        const tableName = 'someTableName';
-        const values = { a: 'aVal', b: 'bVal' };
-        const expectedId = databaseBuffer.nextId;
-
-        // when
-        databaseBuffer.pushInsertable({ tableName, values });
-
-        // then
-        expect(databaseBuffer.objectsToInsert).to.deep.equal([
-          { tableName, values: { ...values, id: expectedId } },
-        ]);
-      });
-
-      it('should increment internal id counter', () => {
-        // given
-        const tableName = 'someTableName';
-        const values = { a: 'aVal', b: 'bVal' };
-
-        // when
-        const { id: initialId } = databaseBuffer.pushInsertable({ tableName, values });
-        const { id: incrementedId } = databaseBuffer.pushInsertable({ tableName, values });
-
-        // then
-        expect(incrementedId).to.equal(initialId + 1);
-      });
-
-      it('should return inserted values with the id in it', () => {
-        // given
-        const tableName = 'someTableName';
-        const values = { a: 'aVal', b: 'bVal' };
-
-        // when
-        const { id: initialId } = databaseBuffer.pushInsertable({ tableName, values });
-        const expectedValues = databaseBuffer.pushInsertable({ tableName, values });
-
-        // then
-        expect(expectedValues).to.deep.equal({ ...expectedValues, id: initialId + 1 });
-      });
     });
   });
 
@@ -135,37 +59,6 @@ describe('Unit | Tooling | DatabaseBuilder | database-buffer', () => {
 
       // then
       expect(databaseBuffer.objectsToInsert).to.be.empty;
-    });
-
-    context('when no id is provided along with the object', () => {
-
-      it('should add an id property along with original properties in the values', () => {
-        // given
-        const tableName = 'someTableName';
-        const values = { a: 'aVal', b: 'bVal' };
-        const expectedId = databaseBuffer.nextId;
-
-        // when
-        databaseBuffer.pushInsertable({ tableName, values });
-
-        // then
-        expect(databaseBuffer.objectsToInsert).to.deep.equal([
-          { tableName, values: { ...values, id: expectedId } },
-        ]);
-      });
-
-      it('should increment internal id counter', () => {
-        // given
-        const tableName = 'someTableName';
-        const values = { a: 'aVal', b: 'bVal' };
-        const expectedNextIdIncremented = databaseBuffer.nextId + 1;
-
-        // when
-        databaseBuffer.pushInsertable({ tableName, values });
-
-        // then
-        expect(databaseBuffer.nextId).to.equal(expectedNextIdIncremented);
-      });
     });
   });
 });
