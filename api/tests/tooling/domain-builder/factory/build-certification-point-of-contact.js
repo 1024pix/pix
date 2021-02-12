598c5c7440297153f7476ecea0773251e53b4dbd
Laura Bergoens <laura.bergoens@pix.fr>
2021-02-12 23:45:14 +0100

    refactor: get rid of faker in Domain-buildCertificationReport

--- api/tests/tooling/domain-builder/factory/build-certification-point-of-contact.js
+++ api/tests/tooling/domain-builder/factory/build-certification-point-of-contact.js
@@ -1,15 +1,14 @@
 const CertificationPointOfContact = require('../../../../lib/domain/read-models/CertificationPointOfContact');
 
-module.exports = function buildCertificationPointOfContact(
-  {
-    id = 123,
-    firstName = 'Chèvre',
-    lastName = 'Brebis',
-    email = 'chevre.brebis@example.net',
-    pixCertifTermsOfServiceAccepted = true,
-    currentCertificationCenterId = 456,
-    certificationCenters,
-  } = {}) {
+module.exports = function buildCertificationPointOfContact({
+  id = 123,
+  firstName = 'Chèvre',
+  lastName = 'Brebis',
+  email = 'chevre.brebis@example.net',
+  pixCertifTermsOfServiceAccepted = true,
+  currentCertificationCenterId = 456,
+  certificationCenters,
+} = {}) {
   return new CertificationPointOfContact({
     id,
     firstName,
