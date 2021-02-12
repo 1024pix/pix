fd6ecc7c1cef17aeac79a580de2394022f3d9b49
Laura Bergoens <laura.bergoens@pix.fr>
2021-02-12 23:30:17 +0100

    refactor: get rid of faker in Domain-buildBadgeCriterion

--- api/tests/tooling/domain-builder/factory/build-badge-criterion.js
+++ api/tests/tooling/domain-builder/factory/build-badge-criterion.js
@@ -1,12 +1,10 @@
 const BadgeCriterion = require('../../../../lib/domain/models/BadgeCriterion');
-const faker = require('faker');
 
-module.exports = function buildBadgeCriterion(
-  {
-    id = 1,
-    scope = BadgeCriterion.SCOPES.CAMPAIGN_PARTICIPATION,
-    threshold = faker.random.number(),
-  } = {}) {
+module.exports = function buildBadgeCriterion({
+  id = 1,
+  scope = BadgeCriterion.SCOPES.CAMPAIGN_PARTICIPATION,
+  threshold = 40,
+} = {}) {
   return new BadgeCriterion({
     id,
     scope,
