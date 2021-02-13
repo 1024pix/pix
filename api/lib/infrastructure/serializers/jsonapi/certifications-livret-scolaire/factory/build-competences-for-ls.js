fe2f59466e188e4cd9c2258a0d65ffd6c28c09ff
Laura Bergoens <laura.bergoens@pix.fr>
2021-02-13 01:07:51 +0100

    refactor: get rid of faker in build-competence-for-ls

--- api/lib/infrastructure/serializers/jsonapi/certifications-livret-scolaire/factory/build-competences-for-ls.js
+++ api/lib/infrastructure/serializers/jsonapi/certifications-livret-scolaire/factory/build-competences-for-ls.js
@@ -1,21 +1,15 @@
-const faker = require('faker');
-
 const Competence = require('../../../../../../lib/infrastructure/serializers/jsonapi/certifications-livret-scolaire/response-objects/Competence');
 const { buildAreaForLS } = require('../../../../../../lib/infrastructure/serializers/jsonapi/certifications-livret-scolaire/factory/build-area-for-ls');
 
 module.exports = function buildCompetenceForLS({
-  id = faker.random.uuid(),
-  // attributes
-  name = faker.random.word(),
-  // relationships
+  id,
+  name,
   area = buildAreaForLS(),
 } = {}) {
 
   return new Competence({
     id,
-    // attributes
     name,
-    // relationships
     area,
   });
 };
