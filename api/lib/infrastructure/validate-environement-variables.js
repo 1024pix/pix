00f9ec3fcb716d2f7913e8da6544f5eaf3ce5df3
Pierre TOP (TOPI) <ptop@octo.com>
2021-02-15 09:04:44 +0100

    Prevent server start on invalid configuration

---
+++ api/lib/infrastructure/validate-environement-variables.js
@@ -0,0 +1,39 @@
+const Joi = require('joi');
+
+const schema = Joi.object({
+  REDIS_URL: Joi.string().uri().optional(),
+  DATABASE_URL: Joi.string().uri().optional(),
+  TEST_DATABASE_URL: Joi.string().optional(),
+  MAILING_ENABLED: Joi.string().optional().valid('true', 'false'),
+  MAILING_PROVIDER: Joi.string().optional().valid('sendinblue', 'mailjet'),
+  MAILJET_API_KEY: Joi.string().optional(),
+  MAILJET_API_SECRET: Joi.string().optional(),
+  MAILJET_ACCOUNT_CREATION_TEMPLATE_ID: Joi.number().optional(),
+  MAILJET_ORGANIZATION_INVITATION_TEMPLATE_ID: Joi.number().optional(),
+  MAILJET_PASSWORD_RESET_TEMPLATE_ID: Joi.number().optional(),
+  SENDINBLUE_API_KEY: Joi.string().optional(),
+  SENDINBLUE_ACCOUNT_CREATION_TEMPLATE_ID: Joi.number().optional(),
+  SENDINBLUE_ORGANIZATION_INVITATION_TEMPLATE_ID: Joi.number().optional(),
+  SENDINBLUE_ORGANIZATION_INVITATION_SCO_TEMPLATE_ID: Joi.number().optional(),
+  SENDINBLUE_PASSWORD_RESET_TEMPLATE_ID: Joi.number().optional(),
+  TLD_FR: Joi.string().optional(),
+  TLD_ORG: Joi.string().optional(),
+  DOMAIN_PIX: Joi.string().optional(),
+  DOMAIN_PIX_APP: Joi.string().optional(),
+  DOMAIN_PIX_ORGA: Joi.string().optional(),
+  LCMS_API_KEY: Joi.string().required(),
+  LCMS_API_URL: Joi.string().uri().required(),
+  LOG_ENABLED: Joi.string().optional().valid('true', 'false'),
+  LOG_LEVEL: Joi.string().optional().valid('fatal', 'error', 'warn', 'info', 'debug', 'trace'),
+  AUTH_SECRET: Joi.string().required(),
+}).options({ allowUnknown: true });
+
+const validateEnvironmentVariables = function() {
+  const { error } = schema.validate(process.env);
+  if (error) {
+    throw new Error('Configuration is invalid: ' + error.message + ', but was: ' + error.details[0].context.value);
+  }
+
+};
+
+module.exports = validateEnvironmentVariables;
