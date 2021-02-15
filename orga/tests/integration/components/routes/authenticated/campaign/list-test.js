4b6f5feb0d008b01b6fd021a8788b082b0b06011
Benjamin Petetot <bpetetot@gmail.com>
2021-02-15 16:30:33 +0100

    [ORGA] Set translation in campaign list table

--- orga/tests/integration/components/routes/authenticated/campaign/list-test.js
+++ orga/tests/integration/components/routes/authenticated/campaign/list-test.js
@@ -1,10 +1,10 @@
 import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import setupIntlRenderingTest from '../../../../../helpers/setup-intl-rendering';
 
 module('Integration | Component | routes/authenticated/campaign/list', function(hooks) {
-  setupRenderingTest(hooks);
+  setupIntlRenderingTest(hooks);
 
   hooks.beforeEach(function() {
     this.set('goToCampaignPageSpy', () => {});
