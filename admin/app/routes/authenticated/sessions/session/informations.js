a6c3c4e5d8ced72885acb33367539df185887375
Laura Bergoens <laura.bergoens@pix.fr>
2021-02-16 16:50:35 +0100

    fix: certifications publication state not updated correctly

--- admin/app/routes/authenticated/sessions/session/informations.js
+++ admin/app/routes/authenticated/sessions/session/informations.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 export default class InformationsRoute extends Route {
   async model() {
     const session = this.modelFor('authenticated.sessions.session');
-    await session.juryCertificationSummaries;
+    await session.juryCertificationSummaries.reload();
     return session;
   }
 }
