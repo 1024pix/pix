889c495d0465a6da40e258328be538ddc70117eb
Laura Bergoens <laura.bergoens@pix.fr>
2021-02-16 12:09:03 +0100

    fix(admin): correctly update session list when one is published away

--- admin/app/routes/authenticated/sessions/list/to-be-published.js
+++ admin/app/routes/authenticated/sessions/list/to-be-published.js
@@ -1,11 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default class AuthenticatedSessionsListToBePublishedRoute extends Route {
-  async model() {
-    try {
-      return await this.store.findAll('publishable-session');
-    } catch (error) {
-      return [];
-    }
+  model() {
+    return this.store.query('publishable-session', {});
   }
 }
