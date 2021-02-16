626154dd37fc9bdb06cfc2747bbeac45aa4fd04e
Laura Bergoens <laura.bergoens@pix.fr>
2021-02-16 16:00:19 +0100

    fix: publishedAt still displayed when session is unpublished

--- api/tests/unit/domain/usecases/unpublish-session_test.js
+++ api/tests/unit/domain/usecases/unpublish-session_test.js
@@ -3,21 +3,14 @@ const {
   sinon,
   expect,
 } = require('../../../test-helper');
-
 const unpublishSession = require('../../../../lib/domain/usecases/unpublish-session');
-const mailService = require('../../../../lib/domain/services/mail-service');
 
 describe('Unit | UseCase | unpublish-session', () => {
-
-  const sessionId = 123;
   let certificationRepository;
   let sessionRepository;
-  const now = new Date('2019-01-01T05:06:07Z');
-  const sessionDate = '2020-05-08';
-  let clock;
+  let finalizedSessionRepository;
 
   beforeEach(() => {
-    clock = sinon.useFakeTimers(now);
     certificationRepository = {
       unpublishCertificationCoursesBySessionId: sinon.stub(),
     };
@@ -25,64 +18,36 @@ describe('Unit | UseCase | unpublish-session', () => {
       updatePublishedAt: sinon.stub(),
       getWithCertificationCandidates: sinon.stub(),
     };
+    finalizedSessionRepository = {
+      updatePublishedAt: sinon.stub(),
+    };
     sessionRepository.flagResultsAsSentToPrescriber = sinon.stub();
-    mailService.sendCertificationResultEmail = sinon.stub();
   });
 
-  afterEach(() => {
-    clock.restore();
-  });
-
-  context('when the session exists', () => {
-    const recipient1 = 'email1@example.net';
-    const recipient2 = 'email2@example.net';
-    const certificationCenter = 'certificationCenter';
-    const candidateWithRecipient1 = domainBuilder.buildCertificationCandidate({
-      resultRecipientEmail: recipient1,
-    });
-    const candidateWithRecipient2 = domainBuilder.buildCertificationCandidate({
-      resultRecipientEmail: recipient2,
-    });
-    const candidate2WithRecipient2 = domainBuilder.buildCertificationCandidate({
-      resultRecipientEmail: recipient2,
-    });
-    const candidateWithNoRecipient = domainBuilder.buildCertificationCandidate({
-      resultRecipientEmail: null,
-    });
-    const originalSession = domainBuilder.buildSession({
+  it('should return the session', async () => {
+    // given
+    const sessionId = 123;
+    const expectedSession = domainBuilder.buildSession({
       id: sessionId,
-      certificationCenter,
-      date: sessionDate,
-      certificationCandidates: [
-        candidateWithRecipient1, candidateWithRecipient2, candidate2WithRecipient2, candidateWithNoRecipient,
-      ],
+      publishedAt: new Date('2020-01-01'),
+    });
+    sessionRepository.getWithCertificationCandidates.withArgs(sessionId).resolves(expectedSession);
+
+    // when
+    const actualSession = await unpublishSession({
+      sessionId,
+      certificationRepository,
+      sessionRepository,
+      finalizedSessionRepository,
     });
 
-    beforeEach(() => {
-      sessionRepository.getWithCertificationCandidates.withArgs(sessionId).resolves(originalSession);
+    // then
+    expect(certificationRepository.unpublishCertificationCoursesBySessionId).to.have.been.calledWithExactly(sessionId);
+    expect(sessionRepository.updatePublishedAt).to.have.been.calledWithExactly({ id: sessionId, publishedAt: null });
+    expect(finalizedSessionRepository.updatePublishedAt).to.have.been.calledWithExactly({ sessionId, publishedAt: null });
+    expect(actualSession).to.deep.equal({
+      ...expectedSession,
+      publishedAt: null,
     });
-
-    context('When we unpublish the session', () => {
-
-      beforeEach(() => {
-        certificationRepository.unpublishCertificationCoursesBySessionId.withArgs(sessionId).resolves();
-      });
-
-      it('should return the session', async () => {
-        // when
-        const session = await unpublishSession({
-          sessionId,
-          certificationRepository,
-          sessionRepository,
-        });
-
-        // then
-        expect(sessionRepository.updatePublishedAt).to.not.have.been.called;
-        expect(mailService.sendCertificationResultEmail).to.not.have.been.called;
-        expect(session).to.deep.equal(originalSession);
-      });
-    });
-
   });
-
 });
