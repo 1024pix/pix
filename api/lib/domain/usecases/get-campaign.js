1bf3c830e9dbeecc6b15b2a6d7283b6d87710e05
Christophe Charles <christophe.charles@octo.com>
2021-02-17 21:15:33 +0100

    [API] Add check on user into usecase
    Check if user is authorize to access the campaign

--- api/lib/domain/usecases/get-campaign.js
+++ api/lib/domain/usecases/get-campaign.js
@@ -1,14 +1,24 @@
-const { NotFoundError } = require('../../domain/errors');
+const {
+  NotFoundError,
+  UserNotAuthorizedToAccessEntityError,
+} = require('../../domain/errors');
 
 module.exports = async function getCampaign({
   campaignId,
+  userId,
   badgeRepository,
+  campaignRepository,
   campaignReportRepository,
   stageRepository,
 }) {
   const integerCampaignId = parseInt(campaignId);
   if (!Number.isFinite(integerCampaignId)) {
     throw new NotFoundError(`Campaign not found for ID ${campaignId}`);
+  }
+
+  const userHasAccessToCampaign = await campaignRepository.checkIfUserOrganizationHasAccessToCampaign(campaignId, userId);
+  if (!userHasAccessToCampaign) {
+    throw new UserNotAuthorizedToAccessEntityError('User does not belong to the organization that owns the campaign');
   }
 
   const [campaignReport, badges, stages] = await Promise.all([
