d65cf9145980cdefe696ff117c02a1c2be54fb7f
Laura Bergoens <laura.bergoens@pix.fr>
2021-02-17 23:02:47 +0100

    feat(api): add read-model CertifiedProfile and domainBuilder

---
+++ api/lib/domain/read-models/CertifiedProfile.js
@@ -0,0 +1,75 @@
+class CertifiedSkill {
+  constructor({
+    id,
+    name,
+    hasBeenAskedInCertif,
+    tubeId,
+  }) {
+    this.id = id;
+    this.name = name;
+    this.hasBeenAskedInCertif = hasBeenAskedInCertif;
+    this.tubeId = tubeId;
+  }
+}
+
+class CertifiedTube {
+  constructor({
+    id,
+    name,
+    competenceId,
+  }) {
+    this.id = id;
+    this.name = name;
+    this.competenceId = competenceId;
+  }
+}
+
+class CertifiedCompetence {
+  constructor({
+    id,
+    name,
+    areaId,
+  }) {
+    this.id = id;
+    this.name = name;
+    this.areaId = areaId;
+  }
+}
+
+class CertifiedArea {
+  constructor({
+    id,
+    name,
+    color,
+  }) {
+    this.id = id;
+    this.name = name;
+    this.color = color;
+  }
+}
+
+class CertifiedProfile {
+  constructor({
+    id,
+    userId,
+    certifiedSkills,
+    certifiedTubes,
+    certifiedCompetences,
+    certifiedAreas,
+  }) {
+    this.id = id;
+    this.userId = userId;
+    this.certifiedSkills = certifiedSkills;
+    this.certifiedTubes = certifiedTubes;
+    this.certifiedCompetences = certifiedCompetences;
+    this.certifiedAreas = certifiedAreas;
+  }
+}
+
+module.exports = {
+  CertifiedProfile,
+  CertifiedArea,
+  CertifiedCompetence,
+  CertifiedTube,
+  CertifiedSkill,
+};
