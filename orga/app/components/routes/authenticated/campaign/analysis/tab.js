75c0d1592c3bbac3b0dcc848bccdb716f512b5bf
Xavier Carron <xavier.carron@pix.fr>
2021-02-18 20:21:36 +0100

    [FEATURE] Add translation on campaign individual analysis

--- orga/app/components/routes/authenticated/campaign/analysis/tab.js
+++ orga/app/components/routes/authenticated/campaign/analysis/tab.js
@@ -1,8 +1,11 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { htmlSafe } from '@ember/string';
 
 export default class Tab extends Component {
+  @service intl;
   @tracked
   sortedRecommendations;
 
@@ -11,6 +14,12 @@ export default class Tab extends Component {
     this.sortedRecommendations = this.args.campaignTubeRecommendations
       ? this.args.campaignTubeRecommendations.sortBy('averageScore')
       : [];
+  }
+
+  get description() {
+    return htmlSafe(this.intl.t('pages.campaign-review.description',
+      { bubble: '<svg height="10" width="10"><circle cx="5" cy="5" r="5" class="campaign-details-analysis recommendation-indicator__bubble" /></svg>' },
+    ));
   }
 
   @action
