d3cc69e202ed2ab0ffe9532d66ecda98111dd01b
ajoanny <ajoanny@octo.com>
2021-02-19 15:08:20 +0100

    [API] add targetProfile in CampaignParticipationOverview

--- api/lib/domain/read-models/CampaignParticipationOverview.js
+++ api/lib/domain/read-models/CampaignParticipationOverview.js
@@ -7,35 +7,46 @@ class CampaignParticipationOverview {
     createdAt,
     isShared,
     sharedAt,
-    targetProfileId,
     validatedSkillsCount,
-    totalSkillsCount,
     organizationName,
     assessmentState,
     campaignCode,
     campaignTitle,
     campaignArchivedAt,
+    targetProfile,
   } = {}) {
     this.id = id;
     this.createdAt = createdAt;
     this.isShared = isShared;
     this.sharedAt = sharedAt;
-    this.targetProfileId = targetProfileId;
+    this.targetProfileId = targetProfile.id;
     this.validatedSkillsCount = validatedSkillsCount;
-    this.totalSkillsCount = totalSkillsCount;
+    this.totalSkillsCount = targetProfile.skills.length;
     this.organizationName = organizationName;
     this.assessmentState = assessmentState;
     this.campaignCode = campaignCode;
     this.campaignTitle = campaignTitle;
     this.campaignArchivedAt = campaignArchivedAt;
+    this.targetProfile = targetProfile;
   }
 
   get masteryPercentage() {
-    if (_.isNil(this.totalSkillsCount) || !this.isShared) return null;
+    if (!this.isShared) return null;
 
     if (this.totalSkillsCount === 0) return 0;
 
     return Math.round(this.validatedSkillsCount * 100 / this.totalSkillsCount);
+  }
+
+  get validatedStagesCount() {
+    if (_.isEmpty(this.targetProfile.stages) || !this.isShared) return null;
+
+    const validatedStages = this.targetProfile.stages.filter((stage) => stage.threshold <= this.masteryPercentage);
+    return validatedStages.length;
+  }
+
+  get totalStagesCount() {
+    return this.targetProfile.stages.length;
   }
 }
 
