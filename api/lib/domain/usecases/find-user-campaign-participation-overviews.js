2808baa0aca9bc6fe8040c97b0b4ee24938d78c7
ajoanny <ajoanny@octo.com>
2021-02-19 15:10:01 +0100

    [API] remove targetProfileRepositoryWithLearningContent in use case find-user-campaign-participation-overviews

--- api/lib/domain/usecases/find-user-campaign-participation-overviews.js
+++ api/lib/domain/usecases/find-user-campaign-participation-overviews.js
@@ -1,44 +1,17 @@
-const bluebird = require('bluebird');
-
-const CampaignParticipationOverview = require('../read-models/CampaignParticipationOverview');
-
 module.exports = async function findUserCampaignParticipationOverviews({
   userId,
   states,
   page,
   campaignParticipationOverviewRepository,
-  targetProfileWithLearningContentRepository,
 }) {
   const concatenatedStates = states ? [].concat(states) : undefined;
 
-  const result = await campaignParticipationOverviewRepository.findByUserIdWithFilters({
+  const campaignParticipationOverviews = await campaignParticipationOverviewRepository.findByUserIdWithFilters({
     userId,
     states: concatenatedStates,
     page,
   });
 
-  const campaignParticipationOverviews = await _buildCampaignParticipationOverviews(
-    result.campaignParticipationOverviews,
-    targetProfileWithLearningContentRepository,
-  );
-
-  return {
-    campaignParticipationOverviews,
-    pagination: result.pagination,
-  };
+  return campaignParticipationOverviews;
 };
 
-async function _buildCampaignParticipationOverviews(overviews, targetProfileWithLearningContentRepository) {
-  return bluebird.mapSeries(
-    overviews,
-    async (overview) => {
-      if (!overview.isShared) return overview;
-
-      const targetProfile = await targetProfileWithLearningContentRepository.get({ id: overview.targetProfileId });
-
-      return new CampaignParticipationOverview({
-        ...overview,
-        totalSkillsCount: targetProfile.skills.length,
-      });
-    });
-}
