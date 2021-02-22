ee56d7b195b96df27be4259f26c9c8a68b177f3e
Clément Bourgoin <clement.bourgoin@pix.fr>
2021-02-22 17:34:16 +0100

    [ORGA] Rename "Certification" page to "Certifications" (plural)

--- orga/tests/unit/routes/authenticated/certification-test.js
+++ orga/tests/unit/routes/authenticated/certification-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | authenticated/certification', function(hooks) {
+module('Unit | Route | authenticated/certifications', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    const route = this.owner.lookup('route:authenticated/certification');
+    const route = this.owner.lookup('route:authenticated/certifications');
     assert.ok(route);
   });
 });
