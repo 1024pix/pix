07afe4312f90e22d611635981c3c1aff7c2533e1
Quentin Lebouc <quentin.lebouc@pix.fr>
2021-02-23 15:47:23 +0100

    [ORGA] Redirect when not allowed to access /certifications URL

--- orga/tests/unit/routes/authenticated/certification-test.js
+++ orga/tests/unit/routes/authenticated/certification-test.js
@@ -1,11 +1,106 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import Service from '@ember/service';
+import sinon from 'sinon';
 
 module('Unit | Route | authenticated/certifications', function(hooks) {
   setupTest(hooks);
 
-  test('it exists', function(assert) {
-    const route = this.owner.lookup('route:authenticated/certifications');
-    assert.ok(route);
+  module('beforeModel', function(){
+    test('should redirect to application when featureToggles.isCertificationResultsInOrgaEnabled is false', function(assert) {
+      // given
+      class CurrentUserStub extends Service {
+        isSCOManagingStudents = true;
+      }
+
+      class FeatureToggleStub extends Service {
+        isCertificationResultsInOrgaEnabled = false;
+      }
+
+      this.owner.register('service:current-user', CurrentUserStub);
+      this.owner.register('service:feature-toggles', FeatureToggleStub);
+      const route = this.owner.lookup('route:authenticated.certifications');
+      const replaceWithStub = sinon.stub();
+      route.replaceWith = replaceWithStub;
+
+      // when
+      route.beforeModel();
+
+      // then
+      sinon.assert.calledOnceWithExactly(replaceWithStub, 'application');
+      assert.ok(true);
+    });
+
+    test('should redirect to application when currentUser.isSCOManagingStudents is false', function(assert) {
+      // given
+      class CurrentUserStub extends Service {
+        isSCOManagingStudents = false;
+      }
+
+      class FeatureToggleStub extends Service {
+        isCertificationResultsInOrgaEnabled = true;
+      }
+
+      this.owner.register('service:current-user', CurrentUserStub);
+      this.owner.register('service:feature-toggles', FeatureToggleStub);
+      const route = this.owner.lookup('route:authenticated.certifications');
+      const replaceWithStub = sinon.stub();
+      route.replaceWith = replaceWithStub;
+
+      // when
+      route.beforeModel();
+
+      // then
+      sinon.assert.calledOnceWithExactly(replaceWithStub, 'application');
+      assert.ok(true);
+    });
+
+    test('should redirect to application when currentUser.isSCOManagingStudents and featureToggles.isCertificationResultsInOrgaEnabled are false', function(assert) {
+      // given
+      class CurrentUserStub extends Service {
+        isSCOManagingStudents = false;
+      }
+
+      class FeatureToggleStub extends Service {
+        isCertificationResultsInOrgaEnabled = false;
+      }
+
+      this.owner.register('service:current-user', CurrentUserStub);
+      this.owner.register('service:feature-toggles', FeatureToggleStub);
+      const route = this.owner.lookup('route:authenticated.certifications');
+      const replaceWithStub = sinon.stub();
+      route.replaceWith = replaceWithStub;
+
+      // when
+      route.beforeModel();
+
+      // then
+      sinon.assert.calledOnceWithExactly(replaceWithStub, 'application');
+      assert.ok(true);
+    });
+
+    test('should redirect to application when currentUser.isSCOManagingStudents and featureToggles.isCertificationResultsInOrgaEnabled are true', function(assert) {
+      // given
+      class CurrentUserStub extends Service {
+        isSCOManagingStudents = true;
+      }
+
+      class FeatureToggleStub extends Service {
+        isCertificationResultsInOrgaEnabled = true;
+      }
+
+      this.owner.register('service:current-user', CurrentUserStub);
+      this.owner.register('service:feature-toggles', FeatureToggleStub);
+      const route = this.owner.lookup('route:authenticated.certifications');
+      const replaceWithStub = sinon.stub();
+      route.replaceWith = replaceWithStub;
+
+      // when
+      route.beforeModel();
+
+      // then
+      sinon.assert.notCalled(replaceWithStub);
+      assert.ok(true);
+    });
   });
 });
