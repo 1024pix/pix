15b459537d23eaa3277a98521b92298685889530
Antoine Ceol <antoine.ceol@gmail.com>
2021-02-23 16:50:26 +0100

    Order referential for certifications

--- api/lib/domain/usecases/certificate/get-certifications-results-for-ls.js
+++ api/lib/domain/usecases/certificate/get-certifications-results-for-ls.js
@@ -1,5 +1,6 @@
 const CertificationsResults = require('../../read-models/livret-scolaire/CertificationsResults');
 const Competence = require('../../read-models/livret-scolaire/Competence');
+const sortBy = require('lodash/sortBy');
 
 module.exports = async function getCertificationsResultsForLS({
   uai,
@@ -17,7 +18,8 @@ module.exports = async function getCertificationsResultsForLS({
     const { code: id, title: name } = competence.area;
     const area = { id, name };
     return new Competence({ area, id: competence.index, name: competence.name });
-  }).sort((competenceA, competenceB) => competenceA.id - competenceB.id);
+  });
+  const sortedCompetences = sortBy(competences, 'id');
 
-  return new CertificationsResults({ certifications, competences });
+  return new CertificationsResults({ certifications, competences: sortedCompetences });
 };
