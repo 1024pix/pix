b13e2cc687b4ee964c4ff3647378806fcaac6f76
Christophe Charles <christophe.charles@octo.com>
2021-02-24 09:23:49 +0100

    [APP] Update route to find user with id null

--- mon-pix/tests/unit/routes/update-expired-password-test.js
+++ mon-pix/tests/unit/routes/update-expired-password-test.js
@@ -1,17 +1,42 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
+import sinon from 'sinon';
+
 import { setupTest } from 'ember-mocha';
 
+import EmberObject from '@ember/object';
+import Service from '@ember/service';
+
 describe('Unit | Route | update-expired-password', function() {
+
   setupTest();
 
   let route;
+  let peekAllStub;
 
   beforeEach(function() {
     route = this.owner.lookup('route:update-expired-password');
+
+    peekAllStub = sinon.stub();
+    const storeStub = Service.create({
+      peekAll: peekAllStub,
+    });
+    route.set('store', storeStub);
   });
 
-  it('exists', function() {
-    expect(route).to.be.ok;
+  it('should retrieve user model with unset id', async function() {
+    // given
+    const userThatWasPreviouslyConnectedWithWrongAccount = EmberObject.create({ id: 1, username: 'user.name0112' });
+    const userWithExpiredPassword = EmberObject.create({ id: null, username: 'user.toupdate1501' });
+    peekAllStub.returns([
+      userThatWasPreviouslyConnectedWithWrongAccount,
+      userWithExpiredPassword,
+    ]);
+
+    // when
+    const user = await route.model();
+
+    // then
+    expect(user).to.equal(userWithExpiredPassword);
   });
 });
