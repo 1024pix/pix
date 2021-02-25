72d422a2fa5d2ed3152a1e80360e5a34a705c115
P-Jeremy <jemyplu@gmail.com>
2021-02-25 18:00:35 +0100

    [ORGA] Download certification results

---
+++ orga/app/services/file-saver.js
@@ -0,0 +1,57 @@
+import Service from '@ember/service';
+import fetch from 'fetch';
+
+export default class FileSaverService extends Service {
+
+  async save({
+    url,
+    token,
+    fetcher = _fetchData,
+    downloadFileForIEBrowser = _downloadFileForIEBrowser,
+    downloadFileForModernBrowsers = _downloadFileForModernBrowsers,
+  }) {
+    const response = await fetcher({ url, token });
+
+    if (response.status !== 200) {
+      const jsonResponse = await response.json();
+      throw jsonResponse.errors;
+    }
+
+    const fileName = _getFileNameFromHeader(response.headers);
+    const fileContent = await response.blob();
+
+    const browserIsInternetExplorer = window.document.documentMode;
+
+    browserIsInternetExplorer
+      ? downloadFileForIEBrowser({ fileContent, fileName })
+      : downloadFileForModernBrowsers({ fileContent, fileName });
+  }
+}
+
+function _fetchData({ url, token }) {
+  return fetch(url, {
+    headers: { 'Authorization': `Bearer ${token}` },
+  });
+}
+
+function _getFileNameFromHeader(headers) {
+  if (headers && headers.get('Content-Disposition')) {
+    const contentDispositionHeader = headers.get('Content-Disposition');
+    const [, fileName] = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/.exec(contentDispositionHeader);
+    return fileName;
+  }
+}
+
+function _downloadFileForIEBrowser({ fileContent, fileName }) {
+  window.navigator.msSaveOrOpenBlob(fileContent, fileName);
+}
+
+function _downloadFileForModernBrowsers({ fileContent, fileName }) {
+  const link = document.createElement('a');
+  link.style.display = 'none';
+  link.href = URL.createObjectURL(fileContent);
+  link.download = fileName;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
