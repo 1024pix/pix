fc9d251691640aa5d8e01bd1e4bf86840586acdd
Stephane Bedeau <sbedeau@octo.com>
2021-02-26 11:11:36 +0100

    chore(cypress)!: bump preprocessor@3.0.0
    Due to a breaking change, we now have to use Given/When/Then instead of given/when/then
    https://github.com/TheBrainFamily/cypress-cucumber-preprocessor/releases/tag/v3.0.0

--- high-level-tests/e2e/cypress/support/step_definitions/campaign.js
+++ high-level-tests/e2e/cypress/support/step_definitions/campaign.js
@@ -1,56 +1,56 @@
-when(`je vais sur la page d'accès à une campagne`, () => {
+When(`je vais sur la page d'accès à une campagne`, () => {
   cy.visitMonPix(`/campagnes`);
 });
 
-when(`je vais sur la campagne {string} avec l'identifiant {string}`, (campaignCode, participantExternalId) => {
+When(`je vais sur la campagne {string} avec l'identifiant {string}`, (campaignCode, participantExternalId) => {
   cy.visitMonPix(`/campagnes/${campaignCode}?participantExternalId=${participantExternalId}`);
 });
 
-when(`j'ouvre le sujet {string}`, (tubeName) => {
+When(`j'ouvre le sujet {string}`, (tubeName) => {
   cy.contains(tubeName).closest('[aria-label="Sujet"]').within(() => {
     cy.get('button').click();
   });
 });
 
-then(`je vois la page \(d'\)/\(de \){string} de la campagne`, (page) => {
+Then(`je vois la page \(d'\)/\(de \){string} de la campagne`, (page) => {
   cy.url().should('include', page);
 });
 
-then(`je vois le bandeau de reprise de parcours`, () => {
+Then(`je vois le bandeau de reprise de parcours`, () => {
   cy.get('.resume-campaign-banner__container').should('exist');
 });
 
-when(`je saisis la date de naissance {int}-{int}-{int}`, (dayOfBirth, monthOfBirth, yearOfBirth) => {
+When(`je saisis la date de naissance {int}-{int}-{int}`, (dayOfBirth, monthOfBirth, yearOfBirth) => {
   cy.get('input#dayOfBirth').type(dayOfBirth);
   cy.get('input#monthOfBirth').type(monthOfBirth);
   cy.get('input#yearOfBirth').type(yearOfBirth);
 });
 
-then(`je vois {int} campagne\(s\)`, (campaignsCount) => {
+Then(`je vois {int} campagne\(s\)`, (campaignsCount) => {
   cy.get('[aria-label="Campagne"]').should('have.lengthOf', campaignsCount);
 });
 
-then(`je vois {int} tutoriel\(s\)`, (tutorialsCount) => {
+Then(`je vois {int} tutoriel\(s\)`, (tutorialsCount) => {
   cy.get('[aria-label="Tutoriel"]').should('have.lengthOf', tutorialsCount);
 });
 
-when(`je recherche une campagne avec le nom {string}`, (campaignSearchName) => {
+When(`je recherche une campagne avec le nom {string}`, (campaignSearchName) => {
   cy.get('input#name').type(campaignSearchName);
 });
 
-then(`je vois le détail de la campagne {string}`, (campaignName) => {
+Then(`je vois le détail de la campagne {string}`, (campaignName) => {
   cy.get('[aria-label="Nom de la campagne"]').contains(campaignName);
 });
 
-then(`je vois {int} participants`, (numberOfParticipants) => {
+Then(`je vois {int} participants`, (numberOfParticipants) => {
   cy.get('[aria-label="Participant"]').should('have.lengthOf', numberOfParticipants);
 });
 
-then(`je vois {int} profils`, (numberOfProfiles) => {
+Then(`je vois {int} profils`, (numberOfProfiles) => {
   cy.get('[aria-label="Profil"]').should('have.lengthOf', numberOfProfiles);
 });
 
-when(`je vois {int} résultats par compétence`, (numberOfResultsByCompetence) => {
+When(`je vois {int} résultats par compétence`, (numberOfResultsByCompetence) => {
   if(numberOfResultsByCompetence === 0) {
     cy.get('.table__empty').should('contain', 'En attente de résultat');
   } else {
@@ -58,38 +58,38 @@ when(`je vois {int} résultats par compétence`, (numberOfResultsByCompetence) =
   }
 });
 
-when(`je vois {int} résultats pour la compétence`, (numberOfResultsByCompetence) => {
+When(`je vois {int} résultats pour la compétence`, (numberOfResultsByCompetence) => {
   cy.get('[aria-label="Votre résultat pour la compétence"]').should('have.lengthOf', numberOfResultsByCompetence);
 });
 
-then(`je vois la moyenne des résultats à {int}%`, (averageResult) => {
+Then(`je vois la moyenne des résultats à {int}%`, (averageResult) => {
   cy.get('[aria-label="Moyenne des résultats"]').contains(`${averageResult}%`);
 });
 
-then(`je vois un résultat global à {int}%`, (globalResult) => {
+Then(`je vois un résultat global à {int}%`, (globalResult) => {
   cy.get('[aria-label="Résultat global"]').contains(`${globalResult}%`);
 });
 
-then(`je vois que j'ai partagé mon profil`, () => {
+Then(`je vois que j'ai partagé mon profil`, () => {
   cy.contains('Merci, votre profil a bien été envoyé !');
 });
 
-then(`je vois que j'ai envoyé les résultats`, () => {
+Then(`je vois que j'ai envoyé les résultats`, () => {
   cy.contains('Merci, vos résultats ont bien été envoyés !');
 });
 
-then(`je vois {int} sujets`, (tubeCount) => {
+Then(`je vois {int} sujets`, (tubeCount) => {
   cy.get('[aria-label="Sujet"]').should('have.lengthOf', tubeCount);
 });
 
-then(`je vois que le sujet {string} est {string}`, (tubeName, recommendationLevel) => {
+Then(`je vois que le sujet {string} est {string}`, (tubeName, recommendationLevel) => {
   cy.contains(tubeName).closest('[aria-label="Sujet"]').get(`[aria-label="${recommendationLevel}"]`);
 });
 
-when(`je retourne au détail de la campagne`, () => {
+When(`je retourne au détail de la campagne`, () => {
   cy.get('[aria-label="Retour"]').click();
 });
 
-when('je clique sur le bouton "Associer"', () => {
+When('je clique sur le bouton "Associer"', () => {
   cy.get('[aria-label="Associer"]').click();
 });
--- high-level-tests/e2e/cypress/support/step_definitions/login-logout.js
+++ high-level-tests/e2e/cypress/support/step_definitions/login-logout.js
@@ -1,17 +1,17 @@
 // Given
-given(`je me connecte avec le compte {string}`, (email) => {
+Given(`je me connecte avec le compte {string}`, (email) => {
   cy.get('input[name="login"]').type(email);
   cy.get('input[name="password"]').type('pix123');
   cy.get('button[type=submit]').click();
 });
 
-given(`je me connecte avec un mot de passe temporaire`, () => {
+Given(`je me connecte avec un mot de passe temporaire`, () => {
   cy.get('input[name="login"]').type('user.shouldChangePassword1234');
   cy.get('input[name="password"]').type('Pix12345');
   cy.get('button[type=submit]').click();
 });
 
-given(`je m'inscris avec le prénom {string}, le nom {string}, le mail {string} et le mot de passe {string}`, (firstname, lastname, email, password) => {
+Given(`je m'inscris avec le prénom {string}, le nom {string}, le mail {string} et le mot de passe {string}`, (firstname, lastname, email, password) => {
   cy.get('input[id="firstName"]').type(firstname);
   cy.get('input[id="lastName"]').type(lastname);
   cy.get('input[id=email]').type(email);
@@ -21,35 +21,35 @@ given(`je m'inscris avec le prénom {string}, le nom {string}, le mail {string} 
 });
 
 // When
-when('je me connecte à Pix via le GAR', () => {
+When('je me connecte à Pix via le GAR', () => {
   cy.loginExternalPlatformForTheFirstTime();
 });
 
-when('je me connecte sur Pix pour la seconde fois via le GAR', () => {
+When('je me connecte sur Pix pour la seconde fois via le GAR', () => {
   cy.loginExternalPlatformForTheSecondTime();
 });
 
-when(`je vais sur l'inscription de Pix`, () => {
+When(`je vais sur l'inscription de Pix`, () => {
   cy.visitMonPix(`/inscription`);
 });
 
-when('je suis connecté avec un compte dont le token expire bientôt', () => {
+When('je suis connecté avec un compte dont le token expire bientôt', () => {
   cy.loginWithAlmostExpiredToken();
 });
 
-when(`j'attends {int} ms`, (duration) => {
+When(`j'attends {int} ms`, (duration) => {
   cy.wait(duration);
 });
 
 // Then
-then(`je suis redirigé vers la page d'accueil de {string}`, (firstName) => {
+Then(`je suis redirigé vers la page d'accueil de {string}`, (firstName) => {
   cy.url().should('include', '/accueil');
   cy.get('.logged-user-name').should((userName) => {
     expect(userName.text()).to.contains(firstName);
   });
 });
 
-then(`je suis redirigé vers le profil de {string}`, (firstName) => {
+Then(`je suis redirigé vers le profil de {string}`, (firstName) => {
   cy.url().should('include', '/competences');
   cy.get('.logged-user-name').should((userName) => {
     expect(userName.text()).to.contains(firstName);
@@ -59,7 +59,7 @@ then(`je suis redirigé vers le profil de {string}`, (firstName) => {
   });
 });
 
-then(`je suis redirigé vers le compte Orga de {string}`, (fullName) => {
+Then(`je suis redirigé vers le compte Orga de {string}`, (fullName) => {
   cy.url().should('include', '/campagnes');
   cy.get('.logged-user-summary__name').should((userName) => {
     expect(userName.text()).to.contains(fullName);
@@ -69,36 +69,36 @@ then(`je suis redirigé vers le compte Orga de {string}`, (fullName) => {
   });
 });
 
-then(`je suis redirigé vers le compte Certif de {string}`, (fullName) => {
+Then(`je suis redirigé vers le compte Certif de {string}`, (fullName) => {
   cy.url().should('include', '/sessions/list');
   cy.get('.logged-user-summary__name').should((userName) => {
     expect(userName.text()).to.contains(fullName);
   });
 });
 
-when(`je me déconnecte`, () => {
+When(`je me déconnecte`, () => {
   cy.get('.logged-user-name__link').click();
   cy.get('ul.logged-user-menu__actions').children().children("[href*='deconnexion']").click();
 });
 
-when(`je me déconnecte de Pix Orga`, () => {
+When(`je me déconnecte de Pix Orga`, () => {
   cy.get('.topbar__user-logged-menu').click();
   cy.get('.logged-user-menu-item:last-of-type').click();
 });
 
-when(`je me déconnecte de Pix Certif`, () => {
+When(`je me déconnecte de Pix Certif`, () => {
   cy.get('.topbar__user-logged-menu').click();
   cy.get('.logged-user-menu-item__last').click();
 });
 
-then(`je suis redirigé vers la page {string}`, (pathname) => {
+Then(`je suis redirigé vers la page {string}`, (pathname) => {
   cy.url().should('include', pathname);
 });
 
-when(`j'accepte les CGU de Pix`, () => {
+When(`j'accepte les CGU de Pix`, () => {
   cy.get('input[type=checkbox]').click();
 });
 
-when(`j'accepte les CGU de Pix Orga`, () => {
+When(`j'accepte les CGU de Pix Orga`, () => {
   cy.get('button').contains('J’accepte les conditions d’utilisation').click();
 });
