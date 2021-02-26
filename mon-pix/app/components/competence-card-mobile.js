5a1d65bb25aab3d409a9b4b0805fd032fd9a5102
celineung <celine.ung@zenika.com>
2021-02-26 16:30:54 +0100

    Make scorecard img accessible

--- mon-pix/app/components/competence-card-mobile.js
+++ mon-pix/app/components/competence-card-mobile.js
@@ -1,6 +1,16 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
 
 export default class CompetenceCardMobile extends Component {
+  @service ariaLabels;
+
+  get scoreAriaLabel() {
+    return this.ariaLabels.computeScoreAriaLabel({
+      isNotStarted: this.args.scorecard.isNotStarted,
+      currentLevel: this.displayedLevel,
+      percentageAheadOfNextLevel: this.args.scorecard.percentageAheadOfNextLevel,
+    });
+  }
 
   get displayedLevel() {
     if (this.args.scorecard.isNotStarted) {
