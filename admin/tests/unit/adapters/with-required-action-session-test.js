f8cef5f2fa66909f291ab26e5d3fdd9b29a86ad4
Etienne GIROT <egirot@octo.com>
2021-03-01 14:40:50 +0100

    [ADMIN] Add adapter for session with req action

---
+++ admin/tests/unit/adapters/with-required-action-session-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Adapter | with required action session', function(hooks) {
+  setupTest(hooks);
+
+  module('#urlForQuery', function() {
+    test('should return /admin/sessions/with-required-action', function(assert) {
+      // when
+      const adapter = this.owner.lookup('adapter:with-required-action-session');
+      const url = adapter.urlForQuery();
+
+      // then
+      assert.ok(url.endsWith('api/admin/sessions/with-required-action'));
+    });
+  });
+});
