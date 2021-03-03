6b35fb3071254fa0352f09c035824d72a3e13f71
Stephane Bedeau <sbedeau@octo.com>
2021-03-03 17:55:35 +0100

    lint: remove no-quoteless-attributes

--- mon-pix/.template-lintrc.js
+++ mon-pix/.template-lintrc.js
@@ -8,7 +8,6 @@ module.exports = {
     'no-html-comments': false,
     'no-inline-styles': false,
     'no-negated-condition': false,
-    'no-quoteless-attributes': false,
     'no-triple-curlies': false,
     'no-bare-strings': ['Pix', '&nbsp;', '&#8226;', '.', '*', '1024', '/', '•', '-', '%'],
   },
