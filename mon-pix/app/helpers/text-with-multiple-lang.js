0b6d5d11143337e176c007bf1a35c969a18a3bb7
melanieb <melanie.boudard@gmail.com>
2021-03-08 16:21:22 +0100

    Transform helper to use the intl service

--- mon-pix/app/helpers/text-with-multiple-lang.js
+++ mon-pix/app/helpers/text-with-multiple-lang.js
@@ -1,18 +1,24 @@
-import { helper } from '@ember/component/helper';
+import Helper from '@ember/component/helper';
+import { inject as service } from '@ember/service';
 
-function _clean(text) {
-  return text.replace(/\[\/?(fr|en)\]/g, '');
-}
-export function textWithMultipleLang(params) {
-  const text = params[0];
-  const lang = params[1];
-  if (text && lang) {
-    const regex = new RegExp(`(\\[${lang}\\]){1}(.|\n)*?(\\[\\/${lang}\\]){1}`);
-    const textForLang = text.match(regex);
-    return textForLang ? _clean(textForLang[0]) : _clean(text);
-  } else {
-    return text;
+export default class textWithMultipleLang extends Helper {
+  @service intl;
+
+  compute(params) {
+    const text = params[0];
+    const lang = params[1];
+    const listOfLocales = this.intl.locales;
+    if (text && lang && listOfLocales.includes(lang)) {
+      const regex = new RegExp(`(\\[${lang}\\]){1}(.|\n)*?(\\[\\/${lang}\\]){1}`);
+      const textForLang = text.match(regex);
+      return textForLang ? _clean(textForLang[0], listOfLocales) : _clean(text, listOfLocales);
+    } else {
+      return _clean(text, listOfLocales);
+    }
   }
 }
 
-export default helper(textWithMultipleLang);
+function _clean(text, listOfLocales) {
+  const regex = new RegExp(`\\[(\\/)?(${listOfLocales.join('|')})\\]`, 'g');
+  return text ? text.replace(regex, '') : text;
+}
--- mon-pix/tests/unit/helpers/text-with-multiple-lang-test.js
+++ mon-pix/tests/unit/helpers/text-with-multiple-lang-test.js
@@ -1,18 +1,25 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
-import { textWithMultipleLang } from 'mon-pix/helpers/text-with-multiple-lang';
+import textWithMultipleLang from 'mon-pix/helpers/text-with-multiple-lang';
 
 describe('Unit | Helper | text with multiple lang', function() {
+  let textWithMultipleLangHelper;
+
+  beforeEach(function() {
+    textWithMultipleLangHelper = new textWithMultipleLang();
+    textWithMultipleLangHelper.intl = { locales: ['fr', 'en'] };
+  });
   [
     { text: 'des mots', lang: 'fr', outputText: 'des mots' },
     { text: 'des mots', lang: null, outputText: 'des mots' },
     { text: null, lang: 'fr', outputText: null },
     { text: '[fr]des mots', lang: 'fr', outputText: 'des mots' },
     { text: '[fr]des mots[/fr][en]some words[/en]', lang: 'fr', outputText: 'des mots' },
-    { text: '[fr]des mots[/fr][en]some words[/en]', lang: 'en', outputText: 'some words' },
+    { text: '[fr]des mots[/fr][en]some words[/en]', lang: 'fr', outputText: 'des mots' },
+    { text: '[fr]des mots[/fr][en]some words[/en]', lang: 'notexist', outputText: 'des motssome words' },
   ].forEach((expected) => {
     it(`should return the text "${expected.outputText}" if the text is "${expected.text}" in lang ${expected.lang}`, function() {
-      expect(textWithMultipleLang([expected.text, expected.lang])).to.equal(expected.outputText);
+      expect(textWithMultipleLangHelper.compute([expected.text, expected.lang])).to.equal(expected.outputText);
     });
   });
 });
