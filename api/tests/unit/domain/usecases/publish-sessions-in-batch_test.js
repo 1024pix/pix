7e0d68e10985b7b858cae36654061b636e6970ba
Etienne GIROT <egirot@octo.com>
2021-03-09 14:49:58 +0100

    [API] Fix some tests

--- api/tests/unit/domain/usecases/publish-sessions-in-batch_test.js
+++ api/tests/unit/domain/usecases/publish-sessions-in-batch_test.js
@@ -1,6 +1,7 @@
 const { domainBuilder, sinon, expect } = require('../../../test-helper');
 
 const publishSessionSessionsInBatch = require('../../../../lib/domain/usecases/publish-sessions-in-batch');
+const EmailingAttempt = require('../../../../lib/domain/models/EmailingAttempt');
 const mailService = require('../../../../lib/domain/services/mail-service');
 
 describe('Unit | UseCase | publish-sessions-in-batch', () => {
@@ -84,6 +85,8 @@ describe('Unit | UseCase | publish-sessions-in-batch', () => {
         certificationRepository.publishCertificationCoursesBySessionId.withArgs(sessionId1).resolves();
         sessionRepository.updatePublishedAt.withArgs({ id: sessionId1, publishedAt: now }).resolves(updatedSessionWithPublishedAt);
         sessionRepository.flagResultsAsSentToPrescriber.withArgs({ id: sessionId1, resultsSentToPrescriberAt: now }).resolves(updatedSessionWithResultSent);
+        mailService.sendCertificationResultEmail.onCall(0).resolves(EmailingAttempt.success(recipientSession1));
+        mailService.sendCertificationResultEmail.onCall(1).resolves(EmailingAttempt.success(recipientSession2));
 
         // when
         await publishSessionSessionsInBatch({
@@ -99,12 +102,95 @@ describe('Unit | UseCase | publish-sessions-in-batch', () => {
         expect(finalizedSessionRepository.updatePublishedAt).to.have.been.calledWith({ sessionId: sessionId2, publishedAt: now });
       });
 
-      it.skip('should send result emails', async () => {
-        // pas besoin de retester ?
-      });
+      it('should send result emails', async () => {
+        // given
+        const recipientSession1 = 'email1@example.net';
+        const certificationCenter1 = 'certificationCenter1';
+        const candidate1 = domainBuilder.buildCertificationCandidate({
+          resultRecipientEmail: recipientSession1,
+        });
+        const candidate2 = domainBuilder.buildCertificationCandidate({
+          resultRecipientEmail: recipientSession1,
+        });
 
-      it.skip('should generate links for certification results for each unique recipient', async () => {
-        // pas besoin de retester ?
+        const sessionId1 = 123001;
+        const sessionDate1 = '2020-01-01';
+        const originalSession1 = domainBuilder.buildSession({
+          id: sessionId1,
+          certificationCenter1,
+          date: sessionDate1,
+          certificationCandidates: [
+            candidate1, candidate2,
+          ],
+        });
+
+        const recipientSession2 = 'email2@example.net';
+        const candidate1Session2 = domainBuilder.buildCertificationCandidate({
+          resultRecipientEmail: recipientSession2,
+        });
+        const candidate2Session2 = domainBuilder.buildCertificationCandidate({
+          resultRecipientEmail: recipientSession2,
+        });
+
+        const sessionId2 = 123002;
+        const sessionDate2 = '2020-01-02';
+        const certificationCenter2 = 'certificationCenter2';
+        const originalSession2 = domainBuilder.buildSession({
+          id: sessionId2,
+          certificationCenter2,
+          date: sessionDate2,
+          certificationCandidates: [
+            candidate1Session2, candidate2Session2,
+          ],
+        });
+
+        const sessionRepository = {
+          updatePublishedAt: sinon.stub(),
+          getWithCertificationCandidates: sinon.stub(),
+        };
+        sessionRepository.getWithCertificationCandidates.withArgs(sessionId1).resolves(originalSession1);
+        sessionRepository.getWithCertificationCandidates.withArgs(sessionId2).resolves(originalSession2);
+        sessionRepository.flagResultsAsSentToPrescriber = sinon.stub();
+
+        const certificationRepository = {
+          publishCertificationCoursesBySessionId: sinon.stub(),
+        };
+        const finalizedSessionRepository = {
+          updatePublishedAt: sinon.stub(),
+        };
+        mailService.sendCertificationResultEmail = sinon.stub();
+
+        const updatedSessionWithPublishedAt = { ...originalSession1, publishedAt: now };
+        const updatedSessionWithResultSent = { ...updatedSessionWithPublishedAt, resultsSentToPrescriberAt: now };
+
+        certificationRepository.publishCertificationCoursesBySessionId.withArgs(sessionId1).resolves();
+        sessionRepository.updatePublishedAt.withArgs({ id: sessionId1, publishedAt: now }).resolves(updatedSessionWithPublishedAt);
+        sessionRepository.flagResultsAsSentToPrescriber.withArgs({ id: sessionId1, resultsSentToPrescriberAt: now }).resolves(updatedSessionWithResultSent);
+        mailService.sendCertificationResultEmail.onCall(0).resolves(EmailingAttempt.success(recipientSession1));
+        mailService.sendCertificationResultEmail.onCall(1).resolves(EmailingAttempt.success(recipientSession2));
+
+        // when
+        await publishSessionSessionsInBatch({
+          sessionIds: [sessionId1, sessionId2],
+          certificationRepository,
+          finalizedSessionRepository,
+          sessionRepository,
+          publishedAt: now,
+        });
+
+        // then
+        expect(mailService.sendCertificationResultEmail).to.have.been.calledWithMatch({
+          email: recipientSession1,
+          sessionId: sessionId1,
+          resultRecipientEmail: 'email1@example.net',
+          daysBeforeExpiration: 30,
+        });
+        expect(mailService.sendCertificationResultEmail).to.have.been.calledWithMatch({
+          email: recipientSession2,
+          sessionId: sessionId2,
+          resultRecipientEmail: 'email2@example.net',
+          daysBeforeExpiration: 30,
+        });
       });
 
       context('when there is at least one results recipient', () => {
