44348be35673c26a4236f0b50529fba6b0f4bc90
Etienne GIROT <egirot@octo.com>
2021-03-09 16:35:07 +0100

    [Admin] Add batch publish sessions in front

--- admin/app/controllers/authenticated/sessions/list/to-be-published.js
+++ admin/app/controllers/authenticated/sessions/list/to-be-published.js
@@ -1,10 +1,13 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+
 import get from 'lodash/get';
 
 export default class SessionToBePublishedController extends Controller {
   @service notifications;
+  @tracked shouldShowModal = false;
 
   @action
   async publishSession(toBePublishedSession) {
@@ -14,5 +17,50 @@ export default class SessionToBePublishedController extends Controller {
       const finalErr = get(err, 'errors[0].detail', err);
       this.notifications.error(finalErr);
     }
+  }
+
+  @action
+  async batchPublishSessions() {
+    const sessionIdsToBePublished = this.model.map((session) => session.id);
+    const adapter = this.store.adapterFor('to-be-published-session');
+    try {
+      await adapter.publishSessionInBatch(sessionIdsToBePublished);
+      this.hideConfirmModal();
+      this._removePublishedSessionsFromStore(this.model);
+      this.notifications.success('Les sessions ont été publiées avec succès');
+    } catch (err) {
+      if (this._batchPublicationFailed(err)) {
+        this._notifyPublicationFailure(err);
+      } else {
+        this.notifications.error(err);
+      }
+      this.hideConfirmModal();
+      this.send('refreshModel');
+    }
+  }
+
+  @action
+  showConfirmModal() {
+    this.shouldShowModal = true;
+  }
+
+  @action
+  hideConfirmModal() {
+    this.shouldShowModal = false;
+  }
+
+  _notifyPublicationFailure(error) {
+    this.notifications.error(
+      'Une ou plusieurs erreurs se sont produites, veuillez conserver la référence suivante pour investigation auprès de l\'équipe technique : ' + get(error, 'errors[0].detail'),
+      { autoClear: false },
+    );
+  }
+
+  _batchPublicationFailed(error) {
+    return get(error, 'errors[0].code') === 'SESSION_BATCH_PUBLICATION_FAILED';
+  }
+
+  _removePublishedSessionsFromStore(sessions) {
+    sessions.map((session) => session.unloadRecord());
   }
 }
--- admin/tests/unit/controllers/authenticated/sessions/list/to-be-published-test.js
+++ admin/tests/unit/controllers/authenticated/sessions/list/to-be-published-test.js
@@ -2,6 +2,7 @@ import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import Service from '@ember/service';
 import sinon from 'sinon';
+import EmberObject from '@ember/object';
 
 module('Unit | Controller | authenticated/sessions/list/to-be-published', function(hooks) {
   setupTest(hooks);
@@ -45,6 +46,104 @@ module('Unit | Controller | authenticated/sessions/list/to-be-published', functi
 
       // then
       sinon.assert.calledWith(errorMock, publishError);
+      assert.ok(true);
+    });
+  });
+
+  module('#showConfirmModal', function() {
+    test('should change shouldShowModal to true', async function(assert) {
+      // given
+      const controller = this.owner.lookup('controller:authenticated.sessions.list.to-be-published');
+
+      // when
+      await controller.showConfirmModal();
+
+      // then
+      assert.ok(controller.shouldShowModal);
+    });
+  });
+
+  module('#hideConfirmModal', function() {
+    test('should change shouldShowModal to false', async function(assert) {
+      // given
+      const controller = this.owner.lookup('controller:authenticated.sessions.list.to-be-published');
+
+      // when
+      await controller.hideConfirmModal();
+
+      // then
+      assert.notOk(controller.shouldShowModal);
+    });
+  });
+
+  module('#batchPublishSessions', function() {
+    test('should publish several sessions', async function(assert) {
+      // given
+      const successMock = sinon.stub();
+      class NotificationsStub extends Service {
+        success = successMock;
+      }
+      this.owner.register('service:notifications', NotificationsStub);
+      const controller = this.owner.lookup('controller:authenticated.sessions.list.to-be-published');
+      const unloadRecord = sinon.stub().resolves();
+      const sessions = [EmberObject.create({ id: 1, unloadRecord }), EmberObject.create({ id: 2, unloadRecord }) ];
+      const store = {
+        adapterFor: sinon.stub().returns({
+          publishSessionInBatch: sinon.stub().resolves(),
+        }),
+      };
+      controller.set('model', sessions);
+      controller.set('store', store);
+
+      // when
+      await controller.batchPublishSessions();
+
+      // then
+      sinon.assert.calledTwice(unloadRecord);
+      sinon.assert.calledOnce(successMock);
+      assert.ok(true);
+    });
+
+    test('should notify error on notifications service when publication fail', async function(assert) {
+      // given
+      const errorMock = sinon.stub();
+      class NotificationsStub extends Service {
+        error = errorMock;
+      }
+      this.owner.register('service:notifications', NotificationsStub);
+      const controller = this.owner.lookup('controller:authenticated.sessions.list.to-be-published');
+
+      const unloadRecord = sinon.stub().resolves();
+      const sessions = [EmberObject.create({ id: 1, unloadRecord }), EmberObject.create({ id: 2, unloadRecord }) ];
+      const publishError = { error: {
+        errors: [
+          { code: 'SESSION_BATCH_PUBLICATION_FAILED', details: 'Erreur dans la publication' },
+        ],
+      } };
+      const store = {
+        adapterFor: sinon.stub().returns({
+          publishSessionInBatch: sinon.stub().rejects(publishError),
+        }),
+      };
+
+      const send = sinon.stub().resolves();
+
+      controller.set('send', send);
+      controller.set('model', sessions);
+      controller.set('store', store);
+
+      // when
+      await controller.batchPublishSessions();
+
+      // then
+      sinon.assert.calledWith(send, 'refreshModel');
+      sinon.assert.calledWith(errorMock,
+        { error: {
+          errors: [
+            { code: 'SESSION_BATCH_PUBLICATION_FAILED', details: 'Erreur dans la publication' },
+          ],
+        } },
+      );
       assert.ok(true);
     });
   });
