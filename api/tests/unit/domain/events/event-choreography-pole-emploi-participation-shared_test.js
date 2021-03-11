fe0f56e0390c5305a17c0da84118f02ad3651ba0
Jonas Grillères <grilleres.j@gmail.com>
2021-03-11 11:07:37 +0100

    [SR] Fix typo

--- api/tests/unit/domain/events/event-choreography-pole-emploi-participation-shared_test.js
+++ api/tests/unit/domain/events/event-choreography-pole-emploi-participation-shared_test.js
@@ -2,7 +2,7 @@ const { expect } = require('../../../test-helper');
 const buildEventDispatcherAndHandlersForTest = require('../../../tooling/events/event-dispatcher-builder');
 const CampaignParticipationResultsShared = require('../../../../lib/domain/events/CampaignParticipationResultsShared');
 
-describe('Event Choregraphy | Pole Emploi Participation Shared', function() {
+describe('Event Choreography | Pole Emploi Participation Shared', function() {
   it('Should trigger Pole Emploi participation shared handler on CampaignParticipationResultsShared event', async () => {
     // given
     const { handlerStubs, eventDispatcher } = buildEventDispatcherAndHandlersForTest();
