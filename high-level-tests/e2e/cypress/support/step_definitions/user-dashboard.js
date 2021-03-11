5ed288c470379f0d2af34f42811f95f5bc704c1b
alexandrecoin <alexandre.pc.coin@gmail.com>
2021-03-11 10:51:40 +0100

    refacto: move banner from skills to dashboard in e2e tests

--- high-level-tests/e2e/cypress/support/step_definitions/user-dashboard.js
+++ high-level-tests/e2e/cypress/support/step_definitions/user-dashboard.js
@@ -9,3 +9,7 @@ Then(`je vois le résumé de mes participations aux campagnes qui ne sont pas pa
 Then(`je vois {int} participation\(s\) aux campagnes`, (campaignParticipationOverviewCount) => {
   cy.get('.campaign-participation-overview-grid__item').should('have.lengthOf', campaignParticipationOverviewCount);
 });
+
+Then(`je vois le bandeau de reprise de la dernière campagne de collecte de profil non partagée`, () => {
+  cy.get('.new-information--yellow-gradient-background').should('exist');
+});
