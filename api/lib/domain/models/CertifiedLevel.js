b7bd791cd7c179e5cd9a06433b9892cc91dfa6ca
Etienne GIROT <egirot@octo.com>
2021-03-12 16:41:31 +0100

    Introduce CertifiedLevel model

---
+++ api/lib/domain/models/CertifiedLevel.js
@@ -0,0 +1,53 @@
+const {
+  MINIMUM_REPRODUCIBILITY_RATE_TO_BE_TRUSTED,
+  UNCERTIFIED_LEVEL,
+} = require('../constants');
+
+class CertifiedLevel {
+  constructor({ value, status }) {
+    this.value = value;
+    this.status = status;
+  }
+
+  static from({
+    numberOfCorrectAnswers,
+    estimatedLevel,
+    reproducibilityRate,
+  }) {
+    if (numberOfCorrectAnswers < 2) {
+      return this._uncertified();
+    }
+    if (reproducibilityRate < MINIMUM_REPRODUCIBILITY_RATE_TO_BE_TRUSTED && numberOfCorrectAnswers === 2) {
+      return this._downgraded(estimatedLevel);
+    }
+    return this._validated(estimatedLevel);
+  }
+
+  static _uncertified() {
+    return new CertifiedLevel({ value: UNCERTIFIED_LEVEL, status: statuses.UNCERTIFIED });
+  }
+  static _downgraded(estimatedLevel) {
+    return new CertifiedLevel({ value: estimatedLevel - 1, status: statuses.DOWNGRADED });
+  }
+
+  static _validated(estimatedLevel) {
+    return new CertifiedLevel({ value: estimatedLevel, status: statuses.VALIDATED });
+  }
+
+  isDowngraded() {
+    return this.status === statuses.DOWNGRADED;
+  }
+  isUncertified() {
+    return this.status === statuses.UNCERTIFIED;
+  }
+}
+
+const statuses = {
+  DOWNGRADED: 'DOWNGRADED',
+  UNCERTIFIED: 'UNCERTIFIED',
+  VALIDATED: 'VALIDATED',
+};
+
+module.exports = {
+  CertifiedLevel,
+};
--- api/tests/unit/domain/models/CertifiedLevel_test.js
+++ api/tests/unit/domain/models/CertifiedLevel_test.js
@@ -1,61 +1,64 @@
-const _ = require('lodash');
-const { expect } = require('../../../../test-helper');
-const { _getCertifiedLevel } = require('../../../../../lib/domain/services/certification-result-service');
+const { expect } = require('../../../test-helper');
+const { CertifiedLevel } = require('../../../../lib/domain/models/CertifiedLevel');
 
 const {
   MINIMUM_REPRODUCIBILITY_RATE_TO_BE_TRUSTED,
   UNCERTIFIED_LEVEL,
-} = require('../../../../../lib/domain/constants');
+} = require('../../../../lib/domain/constants');
 
-describe('Unit | Service | Certification Result Service :: getCertifiedLevel', function() {
+describe('Unit | Domain | Models | CertifiedLevel', function() {
   context('3 answers are correct', () => {
     it('certifies the estimated level', () => {
       // when
-      const result = _getCertifiedLevel({
+      const certifiedLevel = CertifiedLevel.from({
         numberOfCorrectAnswers: 3,
         estimatedLevel: 3,
         reproducibilityRate: 0, // unimportant
       });
 
       // then
-      expect(result).to.equal(3);
+      expect(certifiedLevel.value).to.equal(3);
+      expect(certifiedLevel.isUncertified()).to.be.false;
+      expect(certifiedLevel.isDowngraded()).to.be.false;
     });
   });
   context('only 2 answers are correct', () => {
     it(`certifies the estimated level when reproducibility rate >= ${MINIMUM_REPRODUCIBILITY_RATE_TO_BE_TRUSTED}`, () => {
       // when
-      const result = _getCertifiedLevel({
+      const certifiedLevel = CertifiedLevel.from({
         numberOfCorrectAnswers: 2,
         estimatedLevel: 3,
         reproducibilityRate: MINIMUM_REPRODUCIBILITY_RATE_TO_BE_TRUSTED,
       });
 
       // then
-      expect(result).to.equal(3);
+      expect(certifiedLevel.value).to.equal(3);
     });
     it(`certifies a level below the estimated level when reproducibility rate < ${MINIMUM_REPRODUCIBILITY_RATE_TO_BE_TRUSTED}`, () => {
       // when
-      const result = _getCertifiedLevel({
+      const certifiedLevel = CertifiedLevel.from({
         numberOfCorrectAnswers: 2,
         estimatedLevel: 3,
         reproducibilityRate: MINIMUM_REPRODUCIBILITY_RATE_TO_BE_TRUSTED - 1,
       });
 
       // then
-      expect(result).to.equal(2);
+      expect(certifiedLevel.value).to.equal(2);
+      expect(certifiedLevel.isDowngraded()).to.be.true;
     });
   });
   context('less than 2 answers are correct', () => {
     it('does not certify a level', () => {
       // when
-      const result = _getCertifiedLevel({
+      const certifiedLevel = CertifiedLevel.from({
         numberOfCorrectAnswers: 1,
         estimatedLevel: 3,
         reproducibilityRate: 100, // unimportant
       });
 
       // then
-      expect(result).to.equal(UNCERTIFIED_LEVEL);
+      expect(certifiedLevel.value).to.equal(UNCERTIFIED_LEVEL);
+      expect(certifiedLevel.isUncertified()).to.be.true;
     });
   });
 });
