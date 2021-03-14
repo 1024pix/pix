5f9abfd1db7450d85a64dfd8d99e727547567bbe
Etienne GIROT <egirot@octo.com>
2021-03-14 21:44:50 +0100

    Introduce local CompetenceAnswerCollection model

--- api/lib/domain/models/CertificationContract.js
+++ api/lib/domain/models/CertificationContract.js
@@ -1,4 +1,5 @@
 const { CertificationComputeError } = require('../../../lib/domain/errors');
+const _ = require('lodash');
 
 class CertificationContract {
 
@@ -25,6 +26,15 @@ class CertificationContract {
     if (scoreAfterRating < 1 && reproducibilityRate > 50) {
       throw new CertificationComputeError('Rejeté avec un taux de reproductibilité supérieur à 50');
     }
+  }
+
+  static assertThatEveryAnswerHasMatchingChallenge(answersForCompetence, challengesForCompetence) {
+    answersForCompetence.forEach((answer) => {
+      const challenge = _.find(challengesForCompetence, { challengeId: answer.challengeId });
+      if (!challenge) {
+        throw new CertificationComputeError('Problème de chargement du challenge ' + answer.challengeId);
+      }
+    });
   }
 }
 
