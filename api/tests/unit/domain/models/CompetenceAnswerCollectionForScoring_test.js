6d45a6b0024a14c21ad6979aef0ab55db0767f71
Etienne GIROT <egirot@octo.com>
2021-03-16 23:06:57 +0100

    Count number of neutralized challenges when no QROCMDep

--- api/tests/unit/domain/models/CompetenceAnswerCollectionForScoring_test.js
+++ api/tests/unit/domain/models/CompetenceAnswerCollectionForScoring_test.js
@@ -148,7 +148,7 @@ describe('Unit | Domain | Models | CompetenceAnswerCollectionForScoring', functi
     });
   });
   context('#numberOfNeutralizedChallenges', () => {
-    it('equals 0 when no answers', () => {
+    it('equals 0 when there are no answers', () => {
       // given
       const answerCollection = CompetenceAnswerCollectionForScoring.from({
         answersForCompetence: [],
@@ -161,11 +161,30 @@ describe('Unit | Domain | Models | CompetenceAnswerCollectionForScoring', functi
       // then
       expect(numberOfNeutralizeChallenges).to.equal(0);
     });
+    it('equals the number of challenges when there are all neutralized and none of them are QROCMDep', () => {
+      // given
+      const challenge1 = _buildDecoratedCertificationChallenge({ type: 'QCM', isNeutralized: true });
+      const challenge2 = _buildDecoratedCertificationChallenge({ type: 'QCM', isNeutralized: true });
+      const challenge3 = _buildDecoratedCertificationChallenge({ type: 'QCM', isNeutralized: true });
+      const answer1 = domainBuilder.buildAnswer({ challengeId: challenge1.challengeId });
+      const answer2 = domainBuilder.buildAnswer({ challengeId: challenge2.challengeId });
+      const answer3 = domainBuilder.buildAnswer({ challengeId: challenge3.challengeId });
+      const answerCollection = CompetenceAnswerCollectionForScoring.from({
+        answersForCompetence: [answer1, answer2, answer3],
+        challengesForCompetence: [challenge1, challenge2, challenge3],
+      });
+
+      // when
+      const numberOfNeutralizeChallenges = answerCollection.numberOfNeutralizedChallenges();
+
+      // then
+      expect(numberOfNeutralizeChallenges).to.equal(3);
+    });
   });
 });
 
-function _buildDecoratedCertificationChallenge({ challengeId, type }) {
-  const challenge = domainBuilder.buildCertificationChallenge({ challengeId });
+function _buildDecoratedCertificationChallenge({ challengeId, type, isNeutralized }) {
+  const challenge = domainBuilder.buildCertificationChallenge({ challengeId, isNeutralized });
   challenge.type = type; // TODO : CertificationChallenge are decorated with type in certification-result-service, find a better way.
   return challenge;
 }
