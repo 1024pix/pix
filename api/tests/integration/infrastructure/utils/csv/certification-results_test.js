36a19012754c7b341ec7e0ae2a12dd460955dfa0
P-Jeremy <jemyplu@gmail.com>
2021-03-16 14:15:01 +0100

    [API] Add getCertifResultCsvWithCleaStatus csv utils

--- api/tests/integration/infrastructure/utils/csv/certification-results_test.js
+++ api/tests/integration/infrastructure/utils/csv/certification-results_test.js
@@ -1,5 +1,6 @@
 const { expect, domainBuilder, databaseBuilder } = require('../../../../test-helper');
-const { getSessionCertificationResultsCsv, getDivisionCertificationResultsCsv } = require('../../../../../lib/infrastructure/utils/csv/certification-results');
+const { getSessionCertificationResultsCsv, getSessionCertificationResultsCsvWithCleaStatus, getDivisionCertificationResultsCsv } = require('../../../../../lib/infrastructure/utils/csv/certification-results');
+const { statuses: cleaStatuses } = require('../../../../../lib/infrastructure/repositories/clea-certification-status-repository');
 
 describe('Integration | Infrastructure | Utils | csv | certification-results', () => {
 
@@ -56,6 +57,65 @@ describe('Integration | Infrastructure | Utils | csv | certification-results', (
       expect(result).to.deep.equal(expectedResult);
     });
   });
+
+  describe('#getSessionCertificationResultsCsvWithCleaStatus', () => {
+
+    describe('when at least one certification has passed CleA certification', () => {
+
+      it('should return correct csvContent with the CleA information', async () => {
+        // given
+        const certificationCenterId = databaseBuilder.factory.buildCertificationCenter({}).id;
+        const session = databaseBuilder.factory.buildSession({ certificationCenterId });
+
+        const birthdate = new Date('1990-01-01');
+        const createdAt = new Date('2020-01-01');
+
+        const competencesWithMark1 = [
+          { competence_code: '1.1', level: 0 }, { competence_code: '1.2', level: 1 }, { competence_code: '1.3', level: 5 },
+          { competence_code: '5.1', level: 0 }, { competence_code: '5.2', level: -1 },
+        ];
+        const competencesWithMark2 = [ { competence_code: '5.1', level: 3 }, { competence_code: '5.2', level: -1 } ];
+
+        const lastAssessmentResult1 = domainBuilder.buildAssessmentResult({
+          status: 'validated',
+          competenceMarks: competencesWithMark1,
+        });
+        const lastAssessmentResult2 = domainBuilder.buildAssessmentResult({
+          status: 'rejected',
+          competenceMarks: competencesWithMark2,
+        });
+
+        const certifResult1 = domainBuilder.buildCertificationResult({
+          lastAssessmentResult: lastAssessmentResult1,
+          firstName: 'Lili',
+          birthdate,
+          createdAt,
+          cleaCertificationStatus: cleaStatuses.NOT_PASSED,
+        });
+        const certifResult2 = domainBuilder.buildCertificationResult({
+          lastAssessmentResult: lastAssessmentResult2,
+          firstName: 'Tom',
+          birthdate,
+          createdAt,
+          cleaCertificationStatus: cleaStatuses.REJECTED,
+        });
+
+        const certificationResults = [ certifResult1, certifResult2 ];
+
+        // when
+        const result = await getSessionCertificationResultsCsvWithCleaStatus({ session, certificationResults });
+
+        // then
+        const expectedBirthDate = '01/01/1990';
+        const expectedCreatedAt = '01/01/2020';
+        const expectedResult = '\uFEFF' +
+          '"Numéro de certification";"Prénom";"Nom";"Date de naissance";"Lieu de naissance";"Identifiant Externe";"Certification CléA numérique";"Nombre de Pix";"1.1";"1.2";"1.3";"2.1";"2.2";"2.3";"2.4";"3.1";"3.2";"3.3";"3.4";"4.1";"4.2";"4.3";"5.1";"5.2";"Session";"Centre de certification";"Date de passage de la certification"\n' +
+          `"${certifResult1.id}";"Lili";"${certifResult1.lastName}";"${expectedBirthDate}";"${certifResult1.birthplace}";${certifResult1.externalId};"not_passed";${certifResult1.pixScore};0;1;5;"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";0;0;${session.id};"${session.certificationCenter}";"${expectedCreatedAt}"\n` +
+          `"${certifResult2.id}";"Tom";"${certifResult2.lastName}";"${expectedBirthDate}";"${certifResult2.birthplace}";${certifResult2.externalId};"rejected";"0";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";0;0;${session.id};"${session.certificationCenter}";"${expectedCreatedAt}"`;
+        expect(result).to.deep.equal(expectedResult);
+      });
+    });
+  })
 
   describe('#getDivisionCertificationResultsCsv', () => {
 
