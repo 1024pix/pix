633d59a9ca73e6dd81f1cccc7e453a72eb147a4c
melanieb <melanie.boudard@gmail.com>
2021-03-18 15:53:05 +0100

    [API] Create service to get targetProfile from certifiable badges

---
+++ api/lib/domain/services/certifiable-badges.js
@@ -0,0 +1,19 @@
+const _ = require('lodash');
+const badgeAcquisitionRepository = require('../../infrastructure/repositories/badge-acquisition-repository');
+
+async function hasCertifiableBadges(userId) {
+  const certifiableBadgesAcquisitions = await badgeAcquisitionRepository.findCertifiable({ userId });
+  return certifiableBadgesAcquisitions.length > 0;
+}
+
+async function getTargetProfileIdFromAcquiredCertifiableBadges(userId) {
+  const certifiableBadgesAcquisitions = await badgeAcquisitionRepository.findCertifiable({ userId });
+  return _.uniq(certifiableBadgesAcquisitions.map((certifiableBadgesAcquisition) => {
+    return certifiableBadgesAcquisition.badge.targetProfileId;
+  }));
+}
+
+module.exports = {
+  hasCertifiableBadges,
+  getTargetProfileIdFromAcquiredCertifiableBadges,
+};
