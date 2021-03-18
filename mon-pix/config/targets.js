9da398ec584f70040d7936113b0983344844a926
Laura Bergoens <laura.bergoens@pix.fr>
2021-03-18 07:20:23 +0100

    config(mon-pix): replace ie9 by ie11 as older browser to support

--- mon-pix/config/targets.js
+++ mon-pix/config/targets.js
@@ -10,7 +10,7 @@ const isCI = Boolean(process.env.CI);
 const isProduction = process.env.EMBER_ENV === 'production';
 
 if (isCI || isProduction) {
-  browsers.push('ie 9');
+  browsers.push('ie 11');
 }
 
 module.exports = {
