c22db7452bb18755737e626f1fe9960072e00da9
Christophe Charles <christophe.charles@octo.com>
2021-03-19 10:11:28 +0100

    [ORGA] Manage lang during disconnection

--- orga/app/services/url.js
+++ orga/app/services/url.js
@@ -5,6 +5,7 @@ import ENV from 'pix-orga/config/environment';
 export default class Url extends Service {
 
   @service currentDomain;
+  @service intl;
 
   definedCampaignsRootUrl = ENV.APP.CAMPAIGNS_ROOT_URL;
   pixAppUrlWithoutExtension = ENV.APP.PIX_APP_URL_WITHOUT_EXTENSION;
@@ -18,7 +19,8 @@ export default class Url extends Service {
   }
 
   get homeUrl() {
-    return this.definedHomeUrl;
+    const currentLanguage = this.intl.t('current-lang');
+    return `${this.definedHomeUrl}?lang=${currentLanguage}`;
   }
 
 }
