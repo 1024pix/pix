c8e60ade1fb8f1f4dedb3a45fc8beed360ebf37f
Andreia Pena <andreia.pena@pix.fr>
2021-03-19 16:12:12 +0100

    [ORGA] Add translations for login-or-register page

--- orga/tests/integration/components/routes/login-or-register-test.js
+++ orga/tests/integration/components/routes/login-or-register-test.js
@@ -1,11 +1,18 @@
 import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import clickByLabel from '../../../helpers/extended-ember-test-helpers/click-by-label';
 import hbs from 'htmlbars-inline-precompile';
+import setupIntlRenderingTest from '../../../helpers/setup-intl-rendering';
 
 module('Integration | Component | routes/login-or-register', function(hooks) {
-  setupRenderingTest(hooks);
+
+  setupIntlRenderingTest(hooks);
+
+  let loginButton;
+
+  hooks.beforeEach(function() {
+    loginButton = this.intl.t('pages.login-or-register.login-form.button');
+  });
 
   test('it renders', async function(assert) {
     // when
@@ -17,10 +24,12 @@ module('Integration | Component | routes/login-or-register', function(hooks) {
 
   test('it display the organization name the user is invited to', async function(assert) {
     // when
+    const invitationMessage = this.intl.t('pages.login-or-register.invitation');
+
     await render(hbs`<Routes::LoginOrRegister @organizationName='Organization Aztec'/>`);
 
     // then
-    assert.dom('.login-or-register-panel__invitation').hasText('Vous êtes invité(e) à rejoindre l\'organisation Organization Aztec');
+    assert.dom('.login-or-register-panel__invitation').hasText(`${invitationMessage} Organization Aztec`);
   });
 
   test('it toggle the register form by default', async function(assert) {
@@ -36,7 +45,7 @@ module('Integration | Component | routes/login-or-register', function(hooks) {
     await render(hbs`<Routes::LoginOrRegister/>`);
 
     // when
-    await clickByLabel('Se connecter');
+    await clickByLabel(loginButton);
 
     // then
     assert.dom('.login-form').exists();
@@ -44,11 +53,13 @@ module('Integration | Component | routes/login-or-register', function(hooks) {
 
   test('it toggle the register form on click on register button', async function(assert) {
     // given
+    const registerButtonLabel = this.intl.t('pages.login-or-register.register-form.button');
+
     await render(hbs`<Routes::LoginOrRegister/>`);
 
     // when
-    await clickByLabel('Se connecter');
-    await clickByLabel('S\'inscrire');
+    await clickByLabel(loginButton);
+    await clickByLabel(registerButtonLabel);
 
     // then
     assert.dom('.register-form').exists();
