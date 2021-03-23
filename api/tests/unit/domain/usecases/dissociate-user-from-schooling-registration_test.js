c99bb04f6dfb9cf4916eb147857f2300f0dd77df
yahya youness <yahyayounes@hotmail.fr>
2021-03-23 14:37:35 +0100

    [API] Allow pix master to dissociate a user

--- api/tests/unit/domain/usecases/dissociate-user-from-schooling-registration_test.js
+++ api/tests/unit/domain/usecases/dissociate-user-from-schooling-registration_test.js
@@ -6,22 +6,34 @@ describe('Unit | UseCase | dissociate-user-from-schooling-registration', () => {
 
   let schoolingRegistrationRepositoryStub;
   let membershipRepositoryStub;
+  let userRepositoryStub;
   let schoolingRegistration;
   const organizationId = 1;
   const schoolingRegistrationId = 2;
 
-  context('when the user is an admin of the organization which manage the student', () => {
+  context('when the authenticated user has role pix master', () => {
     const userId = 12;
 
     beforeEach(() => {
 
-      schoolingRegistration = domainBuilder.buildSchoolingRegistration({ organization: { id: organizationId }, id: schoolingRegistrationId });
+      schoolingRegistration = domainBuilder.buildSchoolingRegistration({
+        organization: { id: organizationId },
+        id: schoolingRegistrationId,
+      });
 
       schoolingRegistrationRepositoryStub = {
         dissociateUserFromSchoolingRegistration: sinon.stub(),
         get: sinon.stub().resolves(schoolingRegistration),
       };
-      membershipRepositoryStub = { findByUserIdAndOrganizationId: sinon.stub().withArgs({ userId, organizationId: 9, includeOrganization: true }).resolves([{ isAdmin: true }]) };
+
+      userRepositoryStub = {
+        isPixMaster: sinon.stub().resolves(true),
+      };
+
+      membershipRepositoryStub = {
+        findByUserIdAndOrganizationId: sinon.stub().resolves([]),
+      };
+
     });
 
     it('should dissociate user from the schooling registration', async () => {
@@ -31,6 +43,7 @@ describe('Unit | UseCase | dissociate-user-from-schooling-registration', () => {
         schoolingRegistrationId,
         membershipRepository: membershipRepositoryStub,
         schoolingRegistrationRepository: schoolingRegistrationRepositoryStub,
+        userRepository: userRepositoryStub,
       });
 
       // then
@@ -40,18 +53,67 @@ describe('Unit | UseCase | dissociate-user-from-schooling-registration', () => {
     });
   });
 
-  context('when the user is not a member of the organization which manages the student', () => {
+  context('when the authenticated user is an admin of the organization which manage the student', () => {
     const userId = 12;
 
     beforeEach(() => {
 
-      schoolingRegistration = domainBuilder.buildSchoolingRegistration({ organization: { id: organizationId }, id: schoolingRegistrationId });
+      schoolingRegistration = domainBuilder.buildSchoolingRegistration({
+        organization: { id: organizationId },
+        id: schoolingRegistrationId,
+      });
+
+      schoolingRegistrationRepositoryStub = {
+        dissociateUserFromSchoolingRegistration: sinon.stub(),
+        get: sinon.stub().resolves(schoolingRegistration),
+      };
+      membershipRepositoryStub = {
+        findByUserIdAndOrganizationId: sinon.stub().withArgs({
+          userId,
+          organizationId: 9,
+          includeOrganization: true,
+        }).resolves([{ isAdmin: true }]),
+      };
+      userRepositoryStub = {
+        isPixMaster: sinon.stub().resolves(false),
+      };
+    });
+
+    it('should dissociate user from the schooling registration', async () => {
+
+      await usecases.dissociateUserFromSchoolingRegistration({
+        userId,
+        schoolingRegistrationId,
+        membershipRepository: membershipRepositoryStub,
+        schoolingRegistrationRepository: schoolingRegistrationRepositoryStub,
+        userRepository: userRepositoryStub,
+      });
+
+      // then
+      expect(schoolingRegistrationRepositoryStub.get).to.be.have.been.calledWith(schoolingRegistrationId);
+      expect(schoolingRegistrationRepositoryStub.dissociateUserFromSchoolingRegistration).to.be.have.been.calledWith(schoolingRegistrationId);
+      expect(membershipRepositoryStub.findByUserIdAndOrganizationId).to.be.have.been.called;
+    });
+  });
+
+  context('when the authenticated user is neither a member of the organization which manages the student nor has role pix master', () => {
+    const userId = 12;
+
+    beforeEach(() => {
+
+      schoolingRegistration = domainBuilder.buildSchoolingRegistration({
+        organization: { id: organizationId },
+        id: schoolingRegistrationId,
+      });
 
       schoolingRegistrationRepositoryStub = {
         dissociateUserFromSchoolingRegistration: sinon.stub(),
         get: sinon.stub().resolves(schoolingRegistration),
       };
       membershipRepositoryStub = { findByUserIdAndOrganizationId: sinon.stub().resolves([]) };
+      userRepositoryStub = {
+        isPixMaster: sinon.stub().resolves(false),
+      };
     });
 
     it('throws a ForbiddenAccess error', async () => {
@@ -61,23 +123,30 @@ describe('Unit | UseCase | dissociate-user-from-schooling-registration', () => {
         schoolingRegistrationId,
         membershipRepository: membershipRepositoryStub,
         schoolingRegistrationRepository: schoolingRegistrationRepositoryStub,
+        userRepository: userRepositoryStub,
       });
 
       expect(error).to.be.instanceof(ForbiddenAccess);
     });
   });
 
-  context('when the user is not a admin of the organization which manages the student', () => {
+  context('when the authenticated user is neither a admin of the organization which manages the student nor has role pix master', () => {
     const userId = 1;
 
     beforeEach(() => {
-      schoolingRegistration = domainBuilder.buildSchoolingRegistration({ organization: { id: organizationId }, id: schoolingRegistrationId });
+      schoolingRegistration = domainBuilder.buildSchoolingRegistration({
+        organization: { id: organizationId },
+        id: schoolingRegistrationId,
+      });
 
       schoolingRegistrationRepositoryStub = {
         dissociateUserFromSchoolingRegistration: sinon.stub(),
         get: sinon.stub().resolves(schoolingRegistration),
       };
       membershipRepositoryStub = { findByUserIdAndOrganizationId: sinon.stub().resolves([{ idAdmin: false }]) };
+      userRepositoryStub = {
+        isPixMaster: sinon.stub().resolves(false),
+      };
     });
 
     it('throws a ForbiddenAccess error', async () => {
@@ -87,6 +156,7 @@ describe('Unit | UseCase | dissociate-user-from-schooling-registration', () => {
         schoolingRegistrationId,
         membershipRepository: membershipRepositoryStub,
         schoolingRegistrationRepository: schoolingRegistrationRepositoryStub,
+        userRepository: userRepositoryStub,
       });
 
       expect(error).to.be.instanceof(ForbiddenAccess);
