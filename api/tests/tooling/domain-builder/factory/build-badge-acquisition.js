88ee21d2139b03138f8062edc1049d24263d7b75
Stephane Bedeau <sbedeau@octo.com>
2021-03-24 16:06:27 +0100

    feat: add campaignParticipationId to model

--- api/tests/tooling/domain-builder/factory/build-badge-acquisition.js
+++ api/tests/tooling/domain-builder/factory/build-badge-acquisition.js
@@ -6,11 +6,13 @@ module.exports = function buildBadgeAcquisition(
     id = faker.random.number(),
     userId = faker.random.number(),
     badgeId = faker.lorem.number(),
+    campaignParticipationId = faker.random.number(),
   } = {}) {
 
   return new BadgeAcquisition({
     id,
     userId,
     badgeId,
+    campaignParticipationId,
   });
 };
