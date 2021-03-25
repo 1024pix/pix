96cc9049cfc32904ab7065a521d7149b2f20bb76
Laura Bergoens <laura.bergoens@pix.fr>
2021-03-25 08:49:38 +0100

    test: write unit test for new service

--- api/tests/tooling/domain-builder/factory/build-knowledge-element.js
+++ api/tests/tooling/domain-builder/factory/build-knowledge-element.js
@@ -1,6 +1,6 @@
 const KnowledgeElement = require('../../../../lib/domain/models/KnowledgeElement');
 
-const buildKnowledgeElement = function buildKnowledgeElement({
+const buildKnowledgeElement = function({
   id = 123,
   source = KnowledgeElement.SourceType.DIRECT,
   status = KnowledgeElement.StatusType.VALIDATED,
@@ -64,6 +64,30 @@ buildKnowledgeElement.directlyInvalidated = function({
     id,
     source: KnowledgeElement.SourceType.DIRECT,
     status: KnowledgeElement.StatusType.INVALIDATED,
+    earnedPix,
+    createdAt,
+    answerId,
+    assessmentId,
+    skillId,
+    userId,
+    competenceId,
+  });
+};
+
+buildKnowledgeElement.inferredValidated = function({
+  id = 123,
+  earnedPix = 4,
+  createdAt,
+  answerId = 456,
+  assessmentId = 789,
+  skillId = 'recSKIL123',
+  userId = 159,
+  competenceId = 'recCOMP456',
+} = {}) {
+  return new KnowledgeElement({
+    id,
+    source: KnowledgeElement.SourceType.INFERRED,
+    status: KnowledgeElement.StatusType.VALIDATED,
     earnedPix,
     createdAt,
     answerId,
