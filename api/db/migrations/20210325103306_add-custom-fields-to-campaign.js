a4194e048492a790bc79a5ca320554b768647663
yaelle <ymarsiac@octo.com>
2021-03-25 10:52:05 +0100

    [API] add custom fields for the result page

---
+++ api/db/migrations/20210325103306_add-custom-fields-to-campaign.js
@@ -0,0 +1,20 @@
+const TABLE_NAME = 'campaigns';
+const CUSTOM_RESULT_PAGE_TEXT_COLUMN = 'customResultPageText';
+const CUSTOM_RESULT_PAGE_BUTTON_TEXT_COLUMN = 'customResultPageButtonText';
+const CUSTOM_RESULT_PAGE_BUTTON_URL_COLUMN = 'customResultPageButtonUrl';
+
+exports.up = function(knex) {
+  return knex.schema.table(TABLE_NAME, function(table) {
+    table.text(CUSTOM_RESULT_PAGE_TEXT_COLUMN);
+    table.string(CUSTOM_RESULT_PAGE_BUTTON_TEXT_COLUMN);
+    table.text(CUSTOM_RESULT_PAGE_BUTTON_URL_COLUMN);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.table(TABLE_NAME, (table) => {
+    table.dropColumn(CUSTOM_RESULT_PAGE_TEXT_COLUMN);
+    table.dropColumn(CUSTOM_RESULT_PAGE_BUTTON_TEXT_COLUMN);
+    table.dropColumn(CUSTOM_RESULT_PAGE_BUTTON_URL_COLUMN);
+  });
+};
