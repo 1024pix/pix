1426785f3984a781281e1f41662f16b3d8f25890
Laura Bergoens <laura.bergoens@pix.fr>
2021-03-26 12:32:45 +0100

    chore(api): remove requireFetch: false from BookshelfPartnerCertifications config

--- api/lib/infrastructure/data/partner-certification.js
+++ api/lib/infrastructure/data/partner-certification.js
@@ -5,7 +5,6 @@ const modelName = 'PartnerCertification';
 module.exports = Bookshelf.model('PartnerCertification', {
 
   tableName: 'partner-certifications',
-  requireFetch: false,
 
   get idAttribute() {
     return null;
