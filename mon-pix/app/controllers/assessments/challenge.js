9b5b67e7b015e4259da9e2885e2251b7c3e2ded2
melanieb <melanie.boudard@gmail.com>
2021-03-26 16:09:19 +0100

    Modify page title when the challenge is timed out

--- mon-pix/app/controllers/assessments/challenge.js
+++ mon-pix/app/controllers/assessments/challenge.js
@@ -3,6 +3,7 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import progressInAssessment from 'mon-pix/utils/progress-in-assessment';
+import { action } from '@ember/object';
 
 export default class ChallengeController extends Controller {
   queryParams = ['newLevel', 'competenceLeveled'];
@@ -10,6 +11,7 @@ export default class ChallengeController extends Controller {
   @service currentUser;
   @tracked newLevel = null;
   @tracked competenceLeveled = null;
+  @tracked challengeTitle = 'pages.challenge.title';
 
   get showLevelup() {
     return this.model.assessment.showLevelup && this.newLevel;
@@ -19,7 +21,12 @@ export default class ChallengeController extends Controller {
     const stepNumber = progressInAssessment.getCurrentStepNumber(this.model.assessment, get(this.model, 'answer.id'));
     const totalChallengeNumber = progressInAssessment.getMaxStepsNumber(this.model.assessment);
 
-    return this.intl.t('pages.challenge.title', { stepNumber, totalChallengeNumber });
+    return this.intl.t(this.challengeTitle, { stepNumber, totalChallengeNumber });
+  }
+
+  @action
+  timedoutChallenge() {
+    this.challengeTitle = 'pages.challenge.timed-out-title';
   }
 
   get displayHomeLink() {
