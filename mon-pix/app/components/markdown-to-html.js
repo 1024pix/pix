6108ec73ce6fbd8ea2f55ec7c7b9443d108a801b
Vincent Hardouin <vincent.hardouin@pix.fr>
2021-03-29 15:06:27 +0200

    Use extensions passed in arguments in MarkdownToHtml component

--- mon-pix/app/components/markdown-to-html.js
+++ mon-pix/app/components/markdown-to-html.js
@@ -12,8 +12,15 @@ function modifyWhiteList() {
 }
 
 export default class MarkdownToHtml extends Component {
+  get options() {
+    return {
+      ...ENV.showdown,
+      extensions: this.args.extensions ? this.args.extensions.split(' ') : [],
+    };
+  }
+
   get html() {
-    const converter = new showdown.Converter(ENV.showdown);
+    const converter = new showdown.Converter(this.options);
     const unsafeHtml = converter.makeHtml(this.args.markdown);
     const html = xss(unsafeHtml, {
       whiteList: modifyWhiteList(),
