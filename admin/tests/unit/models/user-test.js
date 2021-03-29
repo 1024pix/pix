44328482d7d345535a4b587935cdace1ad9c618e
Andreia Pena <andreia.pena@pix.fr>
2021-03-29 18:36:33 +0200

    [ADMIN] list the user's connection methods

--- admin/tests/unit/models/user-test.js
+++ admin/tests/unit/models/user-test.js
@@ -26,4 +26,55 @@ module('Unit | Model | user', function(hooks) {
       assert.equal(fullName, 'Jean-Baptiste Poquelin');
     });
   });
+
+  module('#hasEmailAuthenticationMethod', function() {
+
+    test('it should return true when email is defined and user has PIX authentication method', function(assert) {
+      // given
+      const authenticationMethod = store.createRecord('authentication-method', { identityProvider: 'PIX' });
+      const user = store.createRecord('user', { email: 'john.doe@example.net', authenticationMethods: [authenticationMethod] });
+
+      // then
+      assert.equal(user.hasEmailAuthenticationMethod, true);
+    });
+  });
+
+  module('#hasUsernameAuthenticationMethod', function() {
+
+    test('it should return true when username is defined and user has PIX authentication method', function(assert) {
+      // given
+      const authenticationMethod = store.createRecord('authentication-method', { identityProvider: 'PIX' });
+      const user = store.createRecord('user', {
+        username: 'john.doe0101',
+        authenticationMethods: [authenticationMethod],
+      });
+
+      // then
+      assert.equal(user.hasUsernameAuthenticationMethod, true);
+    });
+  });
+
+  module('#hasPoleEmploiAuthenticationMethod', function() {
+
+    test('it should return true when user has Pole Emploi authentication method', function(assert) {
+      // given
+      const authenticationMethod = store.createRecord('authentication-method', { identityProvider: 'POLE_EMPLOI' });
+      const user = store.createRecord('user', { authenticationMethods: [authenticationMethod] });
+
+      // then
+      assert.equal(user.hasPoleEmploiAuthenticationMethod, true);
+    });
+  });
+
+  module('#hasGARAuthenticationMethod', function() {
+
+    test('it should return true when user has GAR authentication method', function(assert) {
+      // given
+      const authenticationMethod = store.createRecord('authentication-method', { identityProvider: 'GAR' });
+      const user = store.createRecord('user', { authenticationMethods: [authenticationMethod] });
+
+      // then
+      assert.equal(user.hasGARAuthenticationMethod, true);
+    });
+  });
 });
