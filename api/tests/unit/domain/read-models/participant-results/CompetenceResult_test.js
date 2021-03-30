5c2716ebcc6a42ccee071d968021ee37883eab3c
ajoanny <ajoanny@octo.com>
2021-03-30 16:51:17 +0200

    [API] create CompetenceResult read model

---
+++ api/tests/unit/domain/read-models/participant-results/CompetenceResult_test.js
@@ -0,0 +1,37 @@
+const { domainBuilder, expect } = require('../../../../test-helper');
+const CompetenceResult = require('../../../../../lib/domain/read-models/participant-results/CompetenceResult');
+const KnowledgeElement = require('../../../../../lib/domain/models/KnowledgeElement');
+
+describe('Unit | Domain | Read-Models | ParticipantResult | CompetenceResult', () => {
+
+  it('computes the result for the given competence', () => {
+
+    const competence = {
+      id: 'rec1',
+      name: 'C1',
+      index: '1.1',
+      areaName: 'Domaine1',
+      areaColor: 'Couleur1',
+      skillIds: ['skill1', 'skill2', 'skill3'],
+    };
+
+    const knowledgeElements = [
+      domainBuilder.buildKnowledgeElement({ status: KnowledgeElement.StatusType.VALIDATED }),
+      domainBuilder.buildKnowledgeElement({ status: KnowledgeElement.StatusType.INVALIDATED }),
+    ];
+
+    const competenceResult = new CompetenceResult(competence, knowledgeElements);
+
+    expect(competenceResult).to.deep.equal({
+      id: 'rec1',
+      name: 'C1',
+      index: '1.1',
+      areaName: 'Domaine1',
+      areaColor: 'Couleur1',
+      testedSkillsCount: 2,
+      totalSkillsCount: 3,
+      validatedSkillsCount: 1,
+      masteryPercentage: 33,
+    });
+  });
+});
