e4c3eef01497e486a509aceeb498ee490a1b3e87
melanieb <melanie.boudard@gmail.com>
2021-03-31 17:31:15 +0200

    Create another component for markdowntohtml of challenge and add some tag in safe markdowntohtml

--- mon-pix/app/components/markdown-to-html-unsafe.js
+++ mon-pix/app/components/markdown-to-html-unsafe.js
@@ -1,23 +1,9 @@
 import Component from '@glimmer/component';
 import showdown from 'showdown';
-import xss from 'xss';
 import { htmlSafe } from '@ember/string';
 import ENV from 'mon-pix/config/environment';
 
-function modifyWhiteList() {
-  return {
-    ...xss.whiteList,
-    style: [],
-    span: ['style'],
-    th: ['style'],
-    td: ['style'],
-    tr: ['style'],
-    table: ['style'],
-    a: ['href', 'rel', 'target', 'title'],
-  };
-}
-
-export default class MarkdownToHtml extends Component {
+export default class MarkdownToHtmlUnsafe extends Component {
   get options() {
     return {
       ...ENV.showdown,
@@ -28,9 +14,6 @@ export default class MarkdownToHtml extends Component {
   get html() {
     const converter = new showdown.Converter(this.options);
     const unsafeHtml = converter.makeHtml(this.args.markdown);
-    const html = xss(unsafeHtml, {
-      whiteList: modifyWhiteList(),
-    });
-    return htmlSafe(html);
+    return htmlSafe(unsafeHtml);
   }
 }
