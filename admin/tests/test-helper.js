ee2c4b295c42d7bb78c847932196abaa07aedb31
celineung <celine.ung@zenika.com>
2021-03-31 15:37:40 +0200

    Update ember 3.23.1 to 3.24.0

--- admin/tests/test-helper.js
+++ admin/tests/test-helper.js
@@ -1,12 +1,15 @@
 import Application from 'pix-admin/app';
 import config from 'pix-admin/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 import loadEmberExam from 'ember-exam/test-support/load';
 
 loadEmberExam();
 
+setup(QUnit.assert);
+
 setApplication(Application.create(config.APP));
 
 start();
-
