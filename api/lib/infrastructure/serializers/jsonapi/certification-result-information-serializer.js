78555d6fdb65ee4830e1406ed4725626dfa8cfcf
Anne-G <ang.schall@gmail.com>
2021-04-01 14:52:55 +0200

    [API] Add certifResultInfo serializer

---
+++ api/lib/infrastructure/serializers/jsonapi/certification-result-information-serializer.js
@@ -0,0 +1,46 @@
+const { Serializer } = require('jsonapi-serializer');
+
+module.exports = {
+
+  serialize(certificationResultInformation) {
+    return new Serializer('certifications', {
+      transform(certificationResultInformation) {
+        return {
+          id: certificationResultInformation.certificationCourseId,
+          ...certificationResultInformation,
+        };
+      },
+      attributes: [
+        'sessionId',
+        'status',
+        'createdAt',
+        'completedAt',
+        'isPublished',
+        'isV2Certification',
+        'cleaCertificationStatus',
+        'firstName',
+        'lastName',
+        'birthdate',
+        'birthplace',
+        'certificationIssueReports',
+        'assessmentId',
+        'commentForCandidate',
+        'commentForOrganization',
+        'commentForJury',
+        'juryId',
+        'pixScore',
+        'competencesWithMark',
+      ],
+      certificationIssueReports: {
+        ref: 'id',
+        attributes: [
+          'category',
+          'description',
+          'subcategory',
+          'questionNumber',
+          'isActionRequired',
+        ],
+      },
+    }).serialize(certificationResultInformation);
+  },
+};
