0c55ec8253cf4b1064ae6995b92ed64a7cffbf2d
Anne-G <ang.schall@gmail.com>
2021-04-01 14:40:18 +0200

    [API] Add CertificationResultInformation readmodel

---
+++ api/tests/unit/domain/read-models/CertificationResultInforamtion_test.js
@@ -0,0 +1,54 @@
+const { expect } = require('../../../test-helper');
+const CertificationResultInformation = require('../../../../lib/domain/read-models/CertificationResultInformation');
+const GeneralCertificationInformation = require('../../../../lib/domain/read-models/GeneralCertificationInformation');
+const AssessmentResult = require('../../../../lib/domain/read-models/AssessmentResult');
+const cleaCertificationStatusRepository = require('../../../../lib/infrastructure/repositories/clea-certification-status-repository');
+
+describe('Unit | Domain | Read-Models | CertificationResultInformation', () => {
+
+  describe('#from', () => {
+    it('should return true if the campaign is of type ASSESSMENT', () => {
+      // given
+      const generalCertificationInformation = new GeneralCertificationInformation({
+        certificationCourseId: 123,
+        sessionId: 1000,
+        createdAt: new Date('2020-01-30T12:00:00Z'),
+        completedAt: new Date('2020-01-30T13:30:00Z'),
+        isPublished: false,
+        isV2Certification: true,
+        firstName: 'Lili',
+        lastName: 'Delamarche',
+        birthdate: new Date('1999-01-01T10:00:00Z'),
+        birthplace: 'Paris',
+        certificationIssueReports: [],
+      });
+      const assessmentResult = new AssessmentResult({
+        assessmentId: 124,
+        status: 'validated',
+        commentForCandidate: null,
+        commentForOrganization: null,
+        commentForJury: null,
+        juryId: 1,
+        pixScore: 555,
+        competencesWithMark: [],
+      });
+      const cleaCertificationStatus = cleaCertificationStatusRepository.statuses.NOT_PASSED;
+
+      // when
+      const certificationResultInformation = CertificationResultInformation.from({
+        generalCertificationInformation,
+        assessmentResult,
+        cleaCertificationStatus,
+      });
+
+      // then
+      const expectedCertificationResultInformation = {
+        ...generalCertificationInformation,
+        ...assessmentResult,
+        cleaCertificationStatus,
+      };
+      expect(certificationResultInformation).to.be.instanceOf(CertificationResultInformation);
+      expect(certificationResultInformation).to.deep.equal(expectedCertificationResultInformation);
+    });
+  });
+});
