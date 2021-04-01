18db6805691c15c4b6b0d02d28de2896cba1439c
Anne-G <ang.schall@gmail.com>
2021-04-01 14:45:54 +0200

    [API] Add generalCertifInfo.get() repo method

---
+++ api/lib/infrastructure/repositories/general-certification-information-repository.js
@@ -0,0 +1,50 @@
+const { knex } = require('../../../db/knex-database-connection');
+
+const { NotFoundError } = require('../../domain/errors');
+const GeneralCertificationInformation = require('../../domain/read-models/GeneralCertificationInformation');
+const CertificationIssueReport = require('../../domain/models/CertificationIssueReport');
+
+module.exports = {
+
+  async get({ certificationCourseId }) {
+    const certificationCourseDTO = await knex('certification-courses')
+      .where({ id: certificationCourseId })
+      .first();
+
+    const certificationIssueReportsDTO = await knex('certification-issue-reports')
+      .where({ certificationCourseId });
+
+    if (certificationCourseDTO) {
+      return _toDomain({ certificationCourseDTO, certificationIssueReportsDTO });
+    }
+    throw new NotFoundError(`Certification course of id ${certificationCourseId} does not exist.`);
+  },
+};
+
+function _toDomain({ certificationCourseDTO, certificationIssueReportsDTO }) {
+  const certificationIssueReports = certificationIssueReportsDTO
+    .map((certificationIssueReport) =>
+      new CertificationIssueReport({
+        id: certificationIssueReport.id,
+        certificationCourseId: certificationIssueReport.certificationCourseId,
+        category: certificationIssueReport.category,
+        description: certificationIssueReport.description,
+        subcategory: certificationIssueReport.subcategory,
+        questionNumber: certificationIssueReport.questionNumber,
+      }),
+    );
+
+  return new GeneralCertificationInformation({
+    certificationCourseId: certificationCourseDTO.id,
+    sessionId: certificationCourseDTO.sessionId,
+    createdAt: certificationCourseDTO.createdAt,
+    completedAt: certificationCourseDTO.completedAt,
+    isPublished: certificationCourseDTO.isPublished,
+    isV2Certification: certificationCourseDTO.isV2Certification,
+    firstName: certificationCourseDTO.firstName,
+    lastName: certificationCourseDTO.lastName,
+    birthdate: certificationCourseDTO.birthdate,
+    birthplace: certificationCourseDTO.birthplace,
+    certificationIssueReports,
+  });
+}
---
+++ api/tests/integration/infrastructure/repositories/general-certification-information_test.js
@@ -0,0 +1,81 @@
+const { catchErr, expect, databaseBuilder } = require('../../../test-helper');
+const generalCertificationInformationRepository = require('../../../../lib/infrastructure/repositories/general-certification-information-repository');
+const { NotFoundError } = require('../../../../lib/domain/errors');
+
+const GeneralCertificationInformation = require('../../../../lib/domain/read-models/GeneralCertificationInformation');
+
+describe('Integration | Repository | Certification Course', function() {
+
+  describe('#get', function() {
+
+    context('when the certification course exists for given id', () => {
+
+      it('should retrieve general certification information', async () => {
+        // given
+        const sessionId = databaseBuilder.factory.buildSession().id;
+        const certificationCourseDTO = {
+          sessionId,
+          firstName: 'Timon',
+          lastName: 'De La Havane',
+          birthdate: '1993-08-14',
+          birthplace: 'Cuba',
+          isPublished: false,
+          completedAt: new Date('2020-02-20T00:00:00Z'),
+          createdAt: new Date('2020-01-20T00:00:00Z'),
+          hasSeenEndTestScreen: false,
+          isV2Certification: true,
+        };
+        const certificationCourseId = databaseBuilder.factory.buildCertificationCourse(certificationCourseDTO).id;
+
+        const firstCertificationReport = databaseBuilder.factory.buildCertificationIssueReport({
+          certificationCourseId,
+          description: 'Houston nous avons un problème',
+        });
+        const secondCertificationReport = databaseBuilder.factory.buildCertificationIssueReport({
+          certificationCourseId,
+          description: 'Un autre problème',
+        });
+        await databaseBuilder.commit();
+
+        // when
+        const result = await generalCertificationInformationRepository.get({ certificationCourseId });
+
+        // then
+        const expectedGeneralCertificationInformation = {
+          certificationCourseId,
+          sessionId: certificationCourseDTO.sessionId,
+          createdAt: certificationCourseDTO.createdAt,
+          completedAt: certificationCourseDTO.completedAt,
+          isPublished: certificationCourseDTO.isPublished,
+          isV2Certification: certificationCourseDTO.isV2Certification,
+          firstName: certificationCourseDTO.firstName,
+          lastName: certificationCourseDTO.lastName,
+          birthdate: certificationCourseDTO.birthdate,
+          birthplace: certificationCourseDTO.birthplace,
+          certificationIssueReports: [
+            { ...firstCertificationReport, isActionRequired: true },
+            { ...secondCertificationReport, isActionRequired: true },
+          ],
+        };
+        expect(result).to.be.instanceOf(GeneralCertificationInformation);
+        expect(result).to.deep.equal(expectedGeneralCertificationInformation);
+      });
+
+    });
+
+    context('when the certification course does not exist', () => {
+      it('should retrieve a NotFoundError Error', async () => {
+        // given
+        const existingCertificationCourseId = databaseBuilder.factory.buildCertificationCourse().id;
+        const nonExistingId = existingCertificationCourseId + 1;
+        await databaseBuilder.commit();
+
+        // when
+        const error = await catchErr(generalCertificationInformationRepository.get)({ certificationCourseId: nonExistingId });
+
+        // then
+        expect(error).to.be.instanceOf(NotFoundError);
+      });
+    });
+  });
+});
