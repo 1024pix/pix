8325f06336c0475f71a2d6e6d2ad2bc9e84e353f
Mathieu Gilet <mathieu.gilet@zenika.com>
2021-04-02 16:31:39 +0200

    [ADMIN] Add remove authentication methods

--- admin/app/routes/authenticated/users/get.js
+++ admin/app/routes/authenticated/users/get.js
@@ -1,8 +1,14 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
 
 export default class AuthenticatedUsersGetRoute extends Route {
 
   model(params) {
-    return this.store.findRecord('user', params.user_id, { include: 'schoolingRegistrations' });
+    return this.store.findRecord('user', params.user_id, { include: 'schoolingRegistrations,authenticationMethods' });
+  }
+
+  @action
+  refreshModel() {
+    this.refresh();
   }
 }
