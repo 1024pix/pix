be3461c0f0e6bbfa8993a7fe1db59ffbf1debc10
Mathieu Gilet <mathieu.gilet@zenika.com>
2021-04-12 15:44:50 +0200

    [APP][SR] Update user-account-test.js

--- mon-pix/tests/acceptance/user-account-test.js
+++ mon-pix/tests/acceptance/user-account-test.js
@@ -9,23 +9,27 @@ import { setupMirage } from 'ember-cli-mirage/test-support';
 describe('Acceptance | User account page', function() {
   setupApplicationTest();
   setupMirage();
-  let user;
 
-  beforeEach(function() {
-    user = server.create('user', 'withEmail');
-  });
+  context('When user is not connected', function() {
 
-  describe('Access to the user account page', function() {
-
-    it('should not be accessible when user is not connected', async function() {
+    it('should be redirected to connection page', async function() {
       // when
       await visit('/mon-compte');
 
       // then
       expect(currentURL()).to.equal('/connexion');
     });
+  });
 
-    it('should be accessible when user is connected', async function() {
+  context('When user is connected', function() {
+
+    let user;
+
+    beforeEach(function() {
+      user = server.create('user', 'withEmail');
+    });
+
+    it('should display my account page', async function() {
       // given
       await authenticateByEmail(user);
 
@@ -36,7 +40,7 @@ describe('Acceptance | User account page', function() {
       expect(currentURL()).to.equal('/mon-compte');
     });
 
-    it('should edit e-mail', async function() {
+    it('should be able to edit the email', async function() {
       // given
       const newEmail = 'new-email@example.net';
       await authenticateByEmail(user);
