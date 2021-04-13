da0b4b89b526c7b43d855ea125fcd7b0b3814bad
Pierre TOP (TOPI) <ptop@octo.com>
2021-04-13 17:48:32 +0200

    [API] Rename file to make authentication explicit

--- api/tests/tooling/server/http-test-server.js
+++ api/tests/tooling/server/http-test-server.js
@@ -1,7 +1,7 @@
 const Hapi = require('@hapi/hapi');
 const Inert = require('@hapi/inert');
 
-const security = require('../../../lib/infrastructure/security');
+const security = require('../../../lib/infrastructure/authentication');
 
 const preResponseUtils = require('../../../lib/application/pre-response-utils');
 const { handleFailAction } = require('../../../lib/validate');
