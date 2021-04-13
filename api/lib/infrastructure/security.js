13ef565025385feb6bb0e74499d3d039814a369e
Pierre TOP (TOPI) <ptop@octo.com>
2021-04-13 17:47:05 +0200

    [API] Gather authentication to dedicated file

--- api/lib/infrastructure/security.js
+++ api/lib/infrastructure/security.js
@@ -1,5 +1,8 @@
-const tokenService = require('../domain/services/token-service');
+const { find } = require('lodash');
 const boom = require('boom');
+
+const tokenService = require('../domain/services/token-service');
+const config = require('../../lib/config');
 
 async function _checkIsAuthenticated(request, h, { key, validate }) {
 
@@ -29,10 +32,47 @@ async function _checkIsAuthenticated(request, h, { key, validate }) {
   return boom.unauthorized();
 }
 
+function validateUser(decoded) {
+  return { isValid: true, credentials: { userId: decoded.user_id } };
+}
+
+function validateClientApplication(decoded) {
+  const application = find(config.graviteeRegisterApplicationsCredentials, { clientId: decoded.client_id });
+
+  if (!application) {
+    return { isValid: false, errorCode: 401 };
+  }
+
+  if (decoded.scope !== application.scope) {
+    return { isValid: false, errorCode: 403 };
+  }
+
+  return { isValid: true, credentials: { client_id: decoded.clientId, scope: decoded.scope, source: decoded.source } };
+}
+
 module.exports = {
+
+  schemeName: 'jwt-scheme',
 
   scheme(_, { key, validate }) {
     return { authenticate: (request, h) => _checkIsAuthenticated(request, h, { key, validate }) };
   },
+
+  strategies:
+     [{
+       name: 'jwt-user', configuration: {
+         key: config.authentication.secret,
+         validate: validateUser,
+       },
+     }, {
+       name: 'jwt-livret-scolaire', configuration: {
+         //TODO rename var env to clientApplicationAuthentication
+         key: config.livretScolaireAuthentication.secret,
+         validate: validateClientApplication,
+       },
+     },
+     ],
+
+  defaultStrategy: 'jwt-user',
 
 };
