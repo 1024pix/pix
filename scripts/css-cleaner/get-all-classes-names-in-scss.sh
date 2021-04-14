c516bde3bbce4fef493d8422e21ab0842001783d
Anne-G <ang.schall@gmail.com>
2021-04-14 15:46:34 +0200

    Factorize classes names list generation

--- scripts/css-cleaner/get-all-classes-names-in-scss.sh
+++ scripts/css-cleaner/get-all-classes-names-in-scss.sh
@@ -2,21 +2,6 @@
 
 set -euo pipefail;
 
-if [ $# -eq 0 ]; then
-  echo "Merci de fournir le nom d'une app front en argument (admin, certif, mon-pix ou orga)."
-  exit 1
-fi
-
-APP=$1
-SCOPE="../../$APP/app"
-
-function ensure_correct_given_app() {
-  if [ $# -eq 0 -a $APP != "admin" -a $APP != "certif" -a $APP != "mon-pix" -a $APP != "orga" ]; then
-    echo -e "Merci de fournir le nom de l'application front sur laquelle vous voulez lancer le script.\n"
-    exit 1
-  fi
-}
-
 function create_temp_file_without_ember_dependancies() {
   sed '/['\''"]ember/s/.*//' $SCOPE/styles/app.scss > $SCOPE/styles/app-temp.scss
 }
@@ -37,8 +22,8 @@ function remove_temp_file() {
   rm $SCOPE/styles/app-temp.scss
 }
 
-
-ensure_correct_given_app
-create_temp_file_without_ember_dependancies
-search_classes_in_scss_files
-remove_temp_file
+function get_all_classes_names_in_scss() {
+  create_temp_file_without_ember_dependancies
+  search_classes_in_scss_files
+  remove_temp_file
+}
