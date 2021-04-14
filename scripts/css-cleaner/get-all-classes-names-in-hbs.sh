234033b1f438ebb298749c7cf0babe503d02abec
Anne-G <ang.schall@gmail.com>
2021-04-14 15:46:34 +0200

    Factorize classes names list generation

--- scripts/css-cleaner/get-all-classes-names-in-hbs.sh
+++ scripts/css-cleaner/get-all-classes-names-in-hbs.sh
@@ -2,21 +2,7 @@
 
 set -euo pipefail;
 
-if [ $# -eq 0 ]; then
-  echo "Merci de fournir le nom d'une app front en argument (admin, certif, mon-pix ou orga)."
-  exit 1
-fi
-
-APP=$1
-SCOPE="../../$APP/app"
 ALL_HBS_FILES=$(find $SCOPE -type f -name '*.hbs')
-
-function ensure_correct_given_app() {
-  if [ $# -eq 0 -a $APP != "admin" -a $APP != "certif" -a $APP != "mon-pix" -a $APP != "orga" ]; then
-    echo -e "Merci de fournir le nom de l'application front sur laquelle vous voulez lancer le script.\n"
-    exit 1
-  fi
-}
 
 function get_all_classes_names_in_hbs() {
   for fileName in "$ALL_HBS_FILES"
@@ -30,6 +16,3 @@ function get_all_classes_names_in_hbs() {
     echo "Voici la liste des classes HBS de l'app $APP:  $APP-classes-names-in-hbs.txt"
   done
 }
-
-ensure_correct_given_app
-get_all_classes_names_in_hbs
