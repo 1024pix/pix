3f2d58ea92b55a451639ab787054a1a5e2821436
Pierre TOP (TOPI) <ptop@octo.com>
2021-04-14 12:08:38 +0200

    [API] Add invalid authentication test on http-test-server

--- api/tests/unit/tooling/http-test-server_test.js
+++ api/tests/unit/tooling/http-test-server_test.js
@@ -20,5 +20,44 @@ describe('Unit | Tooling | Http-test-server', () => {
       // then
       expect(httpTestServer.hapiServer._core.extensions.route.onPreResponse.nodes[0].func.name).to.equal('handleDomainAndHttpErrors');
     });
+
+    describe('when authentication is enabled', async () => {
+
+      let httpTestServerWithAuthentication;
+
+      before(() => {
+        const moduleUnderTest = {
+          name: 'foo-route',
+          register: async function(server) {
+            server.route([{
+              method: 'GET',
+              path: '/foo',
+              config: {
+                handler: () => {
+                  return 'bar';
+                },
+              },
+            }]);
+          },
+        };
+        httpTestServerWithAuthentication = new HttpTestServer(moduleUnderTest, true);
+      });
+
+      it('should reject if no authentication is provided', async () => {
+
+        // given
+        const request = {
+          method: 'GET',
+          url: '/foo',
+        };
+
+        // when
+        const response = await httpTestServerWithAuthentication.request(request.method, request.url, null, null);
+
+        // then
+        expect(response.statusCode).to.equal(401);
+      });
+
+    });
   });
 });
