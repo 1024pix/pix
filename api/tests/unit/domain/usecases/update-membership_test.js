0448f24d68cf02de3fc663855b5bdb9b1fdbba88
Pierre TOP (TOPI) <ptop@octo.com>
2021-04-19 11:14:03 +0200

    [API] Create certif-center membership when role is ADMIN

--- api/tests/unit/domain/usecases/update-membership_test.js
+++ api/tests/unit/domain/usecases/update-membership_test.js
@@ -1,4 +1,4 @@
-const { expect, sinon, catchErr } = require('../../../test-helper');
+const { expect, sinon, catchErr, domainBuilder } = require('../../../test-helper');
 const { updateMembership } = require('../../../../lib/domain/usecases');
 const Membership = require('../../../../lib/domain/models/Membership');
 const { InvalidMembershipRoleError } = require('../../../../lib/domain/errors');
@@ -6,39 +6,145 @@ const { InvalidMembershipRoleError } = require('../../../../lib/domain/errors');
 describe('Unit | UseCase | update-membership', () => {
 
   let membershipRepository;
+  let certificationCenterRepository;
+  let certificationCenterMembershipRepository;
 
   beforeEach(() => {
     membershipRepository = {
-      updateById: sinon.stub().resolves(),
+      updateById: sinon.stub(),
+      get: sinon.stub(),
+    };
+    certificationCenterRepository = {
+      getByExternalId: sinon.stub(),
+    };
+    certificationCenterMembershipRepository = {
+      save: sinon.stub(),
     };
   });
 
-  context('when membership exists', () => {
+  it('should update the membership', async () => {
+    // given
+    const membershipId = 100;
+    const organizationRole = Membership.roles.ADMIN;
+    const membership = new Membership({ id: membershipId, organizationRole, updatedByUserId: 12345 });
+    membershipRepository.get.resolves(domainBuilder.buildMembership());
+    certificationCenterRepository.getByExternalId.resolves(null);
 
-    it('should update membership attributes', async () => {
-      // given
-      const membershipId = 100;
-      const organizationRole = Membership.roles.ADMIN;
-      const membership = new Membership({ id: membershipId, organizationRole, updatedByUserId: 12345 });
-
-      // when
-      await updateMembership({ membershipId, membership, membershipRepository });
-
-      // then
-      expect(membershipRepository.updateById).to.has.been.calledWith({ id: membershipId, membership });
+    // when
+    await updateMembership({
+      membershipId,
+      membership,
+      membershipRepository,
+      certificationCenterRepository,
+      certificationCenterMembershipRepository,
     });
 
-    it('should throw a InvalidMembershipRoleError if role is not valid', async () => {
-      // given
-      const membershipId = 100;
-      const organizationRole = 'NOT_VALID_ROLE';
-      const membership = new Membership({ id: membershipId, organizationRole, updatedByUserId: 12345 });
+    // then
+    expect(membershipRepository.updateById).to.has.been.calledWith({ id: membershipId, membership });
+  });
 
-      // when
-      const error = await catchErr(updateMembership)({ membershipId, membership, membershipRepository });
+  it('should throw a InvalidMembershipRoleError if role is not valid', async () => {
+    // given
+    const membershipId = 100;
+    const organizationRole = 'NOT_VALID_ROLE';
+    const membership = new Membership({ id: membershipId, organizationRole, updatedByUserId: 12345 });
 
-      // then
-      expect(error).to.an.instanceOf(InvalidMembershipRoleError);
+    // when
+    const error = await catchErr(updateMembership)({
+      membershipId,
+      membership,
+      membershipRepository,
+      certificationCenterRepository,
+      certificationCenterMembershipRepository,
+    });
+
+    // then
+    expect(error).to.an.instanceOf(InvalidMembershipRoleError);
+  });
+
+  context('when the role to update is set to administrator', () => {
+
+    context('when the membership\'s organization has a certification center', () => {
+      it('should create a certification center membership', async () => {
+        // given
+        const membershipId = 1;
+        const externalId = 'externalId';
+        const membership = new Membership({ organizationRole: Membership.roles.ADMIN });
+        const existingOrganization = domainBuilder.buildOrganization({ externalId });
+        const existingUser = domainBuilder.buildUser();
+        const existingCertificationCenter = domainBuilder.buildCertificationCenter({ externalId });
+        const existingMembership = domainBuilder.buildMembership({
+          organization: existingOrganization,
+          user: existingUser,
+        });
+
+        membershipRepository.get.withArgs(membershipId).resolves(existingMembership);
+        certificationCenterRepository.getByExternalId.withArgs({ externalId }).resolves(existingCertificationCenter);
+
+        // when
+        await updateMembership({
+          membershipId,
+          membership,
+          membershipRepository,
+          certificationCenterRepository,
+          certificationCenterMembershipRepository,
+        });
+
+        // then
+        expect(certificationCenterMembershipRepository.save).to.have.been.calledWith(existingUser.id, existingCertificationCenter.id);
+      });
+    });
+
+    context('when the membership\'s organization has no certification center', () => {
+      it('should not create a certification center membership', async () => {
+        // given
+        const membershipId = 1;
+        const externalId = 'externalId';
+        const membership = new Membership({ organizationRole: Membership.roles.ADMIN });
+        const existingOrganization = domainBuilder.buildOrganization({ externalId });
+        const existingUser = domainBuilder.buildUser();
+        const existingMembership = domainBuilder.buildMembership({
+          organization: existingOrganization,
+          user: existingUser,
+        });
+
+        membershipRepository.get.withArgs(membershipId).resolves(existingMembership);
+        certificationCenterRepository.getByExternalId.withArgs({ externalId }).resolves(null);
+
+        // when
+        await updateMembership({
+          membershipId,
+          membership,
+          membershipRepository,
+          certificationCenterRepository,
+          certificationCenterMembershipRepository,
+        });
+
+        // then
+        expect(certificationCenterMembershipRepository.save).to.not.have.been.called;
+      });
     });
   });
+
+  context('when the role to update is set to member', () => {
+    it('should not create a certification center membership', async () => {
+      // given
+      const membershipId = 1;
+      const membership = new Membership({ organizationRole: Membership.roles.MEMBER });
+
+      // when
+      await updateMembership({
+        membershipId,
+        membership,
+        membershipRepository,
+        certificationCenterRepository,
+        certificationCenterMembershipRepository,
+      });
+
+      // then
+      expect(certificationCenterMembershipRepository.save).to.not.have.been.called;
+    });
+  });
+
 });
+
