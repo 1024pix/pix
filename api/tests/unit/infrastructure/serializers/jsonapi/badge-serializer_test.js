c1a25b72df576e2c90db8700403a8d7b5879767e
François de Metz <francois.de-metz@pix.fr>
2021-04-19 16:42:20 +0200

    Serialize all badge partner competences of the criterion when the scope is EveryPartnerCompetence

--- api/tests/unit/infrastructure/serializers/jsonapi/badge-serializer_test.js
+++ api/tests/unit/infrastructure/serializers/jsonapi/badge-serializer_test.js
@@ -1,5 +1,6 @@
 const { expect, domainBuilder } = require('../../../../test-helper');
 const serializer = require('../../../../../lib/infrastructure/serializers/jsonapi/badge-serializer');
+const BadgeCriterion = require('../../../../../lib/domain/models/BadgeCriterion');
 
 describe('Unit | Serializer | JSONAPI | badge-serializer', function() {
 
@@ -142,6 +143,99 @@ describe('Unit | Serializer | JSONAPI | badge-serializer', function() {
               },
             },
             id: '2',
+            type: 'badge-criterion',
+          },
+          {
+            attributes: {
+              name: 'name',
+            },
+            id: '1',
+            type: 'badge-partner-competence',
+          },
+          {
+            attributes: {
+              name: 'name',
+            },
+            id: '2',
+            type: 'badge-partner-competence',
+          },
+        ],
+      };
+
+      // when
+      const json = serializer.serialize(badge);
+
+      // then
+      expect(json).to.deep.equal(expectedSerializedBadge);
+    });
+
+    it('should convert a Badge model and scope with every partner competence badge criteria scope into JSON API data', function() {
+      // given
+      const badge = domainBuilder.buildBadge({
+        id: '1',
+        altMessage: 'You won a banana badge',
+        imageUrl: '/img/banana.svg',
+        message: 'Congrats, you won a banana badge',
+        key: 'BANANA',
+        title: 'Banana',
+        targetProfileId: '1',
+        isCertifiable: false,
+        badgeCriteria: [
+          domainBuilder.buildBadgeCriterion({
+            scope: BadgeCriterion.SCOPES.EVERY_PARTNER_COMPETENCE,
+            partnerCompetenceIds: null,
+          }),
+        ],
+      });
+
+      const expectedSerializedBadge = {
+        data: {
+          attributes: {
+            'alt-message': 'You won a banana badge',
+            'image-url': '/img/banana.svg',
+            'is-certifiable': false,
+            message: 'Congrats, you won a banana badge',
+            title: 'Banana',
+            key: 'BANANA',
+          },
+          id: '1',
+          type: 'badges',
+          relationships: {
+            'badge-criteria': {
+              'data': [{
+                id: '1',
+                type: 'badge-criterion',
+              }],
+            },
+            'badge-partner-competences': {
+              'data': [{
+                id: '1',
+                type: 'badge-partner-competence',
+              }, {
+                id: '2',
+                type: 'badge-partner-competence',
+              }],
+            },
+          },
+        },
+        included: [
+          {
+            attributes: {
+              scope: 'EveryPartnerCompetence',
+              threshold: 40,
+            },
+            relationships: {
+              'partner-competences': {
+                data: [{
+                  id: '1',
+                  type: 'badge-partner-competence',
+                }, {
+                  id: '2',
+                  type: 'badge-partner-competence',
+                }],
+              },
+            },
+            id: '1',
             type: 'badge-criterion',
           },
           {
