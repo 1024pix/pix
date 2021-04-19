a21eb79b8517c9e7124d386c040c6ccfb3868346
Etienne GIROT <egirot@octo.com>
2021-04-19 15:12:46 +0200

    [Certif] Notifies success/failure on neutralization

--- admin/app/controllers/authenticated/certifications/certification/neutralization.js
+++ admin/app/controllers/authenticated/certifications/certification/neutralization.js
@@ -3,15 +3,22 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { alias } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
 
 export default class NeutralizationController extends Controller {
   @alias('model') certificationDetails;
+  @service notifications;
 
   @action
-  neutralize(challengeRecId) {
-    return this.certificationDetails.neutralizeChallenge({
-      certificationCourseId: this.certificationDetails.id,
-      challengeRecId,
-    });
+  async neutralize(challengeRecId) {
+    try {
+      await this.certificationDetails.neutralizeChallenge({
+        certificationCourseId: this.certificationDetails.id,
+        challengeRecId,
+      });
+      return this.notifications.success('Épreuve neutralisée avec succès.');
+    } catch (e) {
+      return this.notifications.error('Une erreur est survenue lors de la neutralisation de l\'épreuve.');
+    }
   }
 }
