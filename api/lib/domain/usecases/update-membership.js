483e7a48a804528fc60edf4f7dd219906a0201b7
Pierre TOP (TOPI) <ptop@octo.com>
2021-04-19 11:14:03 +0200

    [API] Create certif-center membership when role is ADMIN

--- api/lib/domain/usecases/update-membership.js
+++ api/lib/domain/usecases/update-membership.js
@@ -1,6 +1,20 @@
-module.exports = async function updateMembership({ membershipRepository, membershipId, membership }) {
-
+module.exports = async function updateMembership({
+  membershipId,
+  membership,
+  membershipRepository,
+  certificationCenterRepository,
+  certificationCenterMembershipRepository,
+}) {
   membership.validateRole();
+
+  if (membership.isAdmin) {
+    const existingMembership = await membershipRepository.get(membershipId);
+    const existingCertificationCenter = await certificationCenterRepository.getByExternalId({ externalId: existingMembership.organization.externalId });
+
+    if (existingCertificationCenter) {
+      certificationCenterMembershipRepository.save(existingMembership.user.id, existingCertificationCenter.id);
+    }
+  }
 
   return membershipRepository.updateById({ id: membershipId, membership });
 };
--- api/tests/integration/domain/usecases/update-membership_test.js
+++ api/tests/integration/domain/usecases/update-membership_test.js
@@ -1,25 +1,55 @@
-const { expect, databaseBuilder } = require('../../../test-helper');
+const { expect, databaseBuilder, knex } = require('../../../test-helper');
 
 const membershipRepository = require('../../../../lib/infrastructure/repositories/membership-repository');
+const certificationCenterRepository = require('../../../../lib/infrastructure/repositories/certification-center-repository');
+const certificationCenterMembershipRepository = require('../../../../lib/infrastructure/repositories/certification-center-membership-repository');
 const Membership = require('../../../../lib/domain/models/Membership');
 
 const updateMembership = require('../../../../lib/domain/usecases/update-membership');
 
 describe('Integration | UseCases | update-membership', () => {
 
-  let userId;
-  let updatedByUserId;
-  let organizationId;
-
-  beforeEach(async () => {
-    organizationId = databaseBuilder.factory.buildOrganization().id;
-    userId = databaseBuilder.factory.buildUser().id;
-    updatedByUserId = databaseBuilder.factory.buildUser().id;
-    await databaseBuilder.commit();
+  afterEach(() => {
+    return knex('certification-center-memberships').delete();
   });
 
   it('should update membership', async () => {
     // given
+    const organizationId = databaseBuilder.factory.buildOrganization().id;
+    const userId = databaseBuilder.factory.buildUser().id;
+    const updatedByUserId = databaseBuilder.factory.buildUser().id;
+
+    const membershipId = databaseBuilder.factory.buildMembership({
+      organizationId, userId,
+      organizationRole: Membership.roles.MEMBER,
+    }).id;
+
+    await databaseBuilder.commit();
+
+    const newOrganizationRole = Membership.roles.ADMIN;
+    const membership = new Membership({ id: membershipId, organizationRole: newOrganizationRole, updatedByUserId });
+
+    // when
+    const result = await updateMembership({
+      membershipId,
+      membership,
+      membershipRepository,
+      certificationCenterRepository,
+      certificationCenterMembershipRepository,
+    });
+
+    // then
+    expect(result).to.be.an.instanceOf(Membership);
+    expect(result.organizationRole).equal(newOrganizationRole);
+    expect(result.updatedByUserId).equal(updatedByUserId);
+  });
+
+  it('if the organization has a certification center and he role to update is set to administrator, it should create a certification center membership ', async () => {
+    // given
+    const externalId = 'foo';
+    const organizationId = databaseBuilder.factory.buildOrganization({ externalId }).id;
+    const userId = databaseBuilder.factory.buildUser().id;
+    const updatedByUserId = databaseBuilder.factory.buildUser().id;
     const membershipId = databaseBuilder.factory.buildMembership({
       organizationId, userId,
       organizationRole: Membership.roles.MEMBER,
@@ -27,15 +57,27 @@ describe('Integration | UseCases | update-membership', () => {
     const newOrganizationRole = Membership.roles.ADMIN;
     const membership = new Membership({ id: membershipId, organizationRole: newOrganizationRole, updatedByUserId });
 
+    const certificationCenterId = databaseBuilder.factory.buildCertificationCenter({ externalId }).id;
+
     await databaseBuilder.commit();
 
     // when
-    const result = await updateMembership({ membershipRepository, membershipId, membership });
+    await updateMembership({
+      membershipId,
+      membership,
+      membershipRepository,
+      certificationCenterRepository,
+      certificationCenterMembershipRepository,
+    });
 
     // then
-    expect(result).to.be.an.instanceOf(Membership);
-    expect(result.organizationRole).equal(newOrganizationRole);
-    expect(result.updatedByUserId).equal(updatedByUserId);
+    const certificationCenterMembership = await knex('certification-center-memberships').where({
+      userId,
+      certificationCenterId,
+    }).first();
+
+    expect(certificationCenterMembership).not.to.be.undefined;
+
   });
 
 });
