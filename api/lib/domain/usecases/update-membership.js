e2129211bab9350696dd7d78c2a6052f5909a0fb
Mathieu Gilet <mathieu.gilet@zenika.com>
2021-04-20 17:50:27 +0200

    [API] Add domain transaction

--- api/lib/domain/usecases/update-membership.js
+++ api/lib/domain/usecases/update-membership.js
@@ -1,10 +1,4 @@
-async function createCertificationCenterMembership(certificationCenterMembershipRepository, existingMembership, existingCertificationCenter) {
-  const isAlreadyMemberOfCertificationCenter = await certificationCenterMembershipRepository.isMemberOfCertificationCenter(existingMembership.user.id, existingCertificationCenter.id);
-
-  if (!isAlreadyMemberOfCertificationCenter) {
-    certificationCenterMembershipRepository.save(existingMembership.user.id, existingCertificationCenter.id);
-  }
-}
+const DomainTransaction = require('../../infrastructure/DomainTransaction');
 
 module.exports = async function updateMembership({
   membershipId,
@@ -20,7 +14,14 @@ module.exports = async function updateMembership({
     const existingCertificationCenter = await certificationCenterRepository.findByExternalId({ externalId: existingMembership.organization.externalId });
 
     if (existingCertificationCenter) {
-      await createCertificationCenterMembership(certificationCenterMembershipRepository, existingMembership, existingCertificationCenter);
+      const isAlreadyMemberOfCertificationCenter = await certificationCenterMembershipRepository.isMemberOfCertificationCenter(existingMembership.user.id, existingCertificationCenter.id);
+
+      if (!isAlreadyMemberOfCertificationCenter) {
+        return DomainTransaction.execute(async (domainTransaction) => {
+          await certificationCenterMembershipRepository.save(existingMembership.user.id, existingCertificationCenter.id, domainTransaction);
+          return membershipRepository.updateById({ id: membershipId, membership }, domainTransaction);
+        });
+      }
     }
   }
 
