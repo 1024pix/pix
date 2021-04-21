2033fd04878f107a31fb3a155327d561be3d99eb
Laura Bergoens <laura.bergoens@pix.fr>
2021-04-21 08:25:23 +0200

    feat(api): add repo with get method for CleaCertificationResult model

---
+++ api/tests/integration/infrastructure/repositories/clea-certification-result-repository_test.js
@@ -0,0 +1,57 @@
+const { expect, databaseBuilder } = require('../../../test-helper');
+const cleaCertificationResultRepository = require('../../../../lib/infrastructure/repositories/clea-certification-result-repository');
+const CleaCertificationResult = require('../../../../lib/domain/models/CleaCertificationResult');
+
+describe('Integration | Infrastructure | Repositories | clea-certification-result-repository', () => {
+
+  describe('#get', () => {
+
+    context('when there is no clea certification result for a given certification id', () => {
+
+      it('should return a not_passed result', async () => {
+        // when
+        const cleaCertificationResult = await cleaCertificationResultRepository.get({ certificationCourseId: 123 });
+
+        // then
+        expect(cleaCertificationResult).to.be.instanceOf(CleaCertificationResult);
+        expect(cleaCertificationResult.status).to.equal(CleaCertificationResult.cleaStatuses.NOT_PASSED);
+      });
+    });
+
+    context('when there is a acquired clea certification result for a given certification id', () => {
+
+      it('should return a acquired result', async () => {
+        // given
+        databaseBuilder.factory.buildBadge({ key: CleaCertificationResult.badgeKey });
+        const certificationCourseId = databaseBuilder.factory.buildCertificationCourse().id;
+        databaseBuilder.factory.buildPartnerCertification({ certificationCourseId, partnerKey: CleaCertificationResult.badgeKey, acquired: true });
+        await databaseBuilder.commit();
+
+        // when
+        const cleaCertificationResult = await cleaCertificationResultRepository.get({ certificationCourseId });
+
+        // then
+        expect(cleaCertificationResult).to.be.instanceOf(CleaCertificationResult);
+        expect(cleaCertificationResult.status).to.equal(CleaCertificationResult.cleaStatuses.ACQUIRED);
+      });
+    });
+
+    context('when there is a rejected clea certification result for a given certification id', () => {
+
+      it('should return a rejected result', async () => {
+        // given
+        databaseBuilder.factory.buildBadge({ key: CleaCertificationResult.badgeKey });
+        const certificationCourseId = databaseBuilder.factory.buildCertificationCourse().id;
+        databaseBuilder.factory.buildPartnerCertification({ certificationCourseId, partnerKey: CleaCertificationResult.badgeKey, acquired: false });
+        await databaseBuilder.commit();
+
+        // when
+        const cleaCertificationResult = await cleaCertificationResultRepository.get({ certificationCourseId });
+
+        // then
+        expect(cleaCertificationResult).to.be.instanceOf(CleaCertificationResult);
+        expect(cleaCertificationResult.status).to.equal(CleaCertificationResult.cleaStatuses.REJECTED);
+      });
+    });
+  });
+});
