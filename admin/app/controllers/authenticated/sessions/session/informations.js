36f2b1c5190890370e945ffd075195e462500a22
Clément Bourgoin <clement.bourgoin@pix.fr>
2021-04-21 16:53:17 +0200

    feat(admin): remove "Récupérer le fichier avant jury" button

--- admin/app/controllers/authenticated/sessions/session/informations.js
+++ admin/app/controllers/authenticated/sessions/session/informations.js
@@ -8,7 +8,6 @@ import { tracked } from '@glimmer/tracking';
 import { statusToDisplayName } from '../../../../models/session';
 
 export default class IndexController extends Controller {
-  @service sessionInfoService;
   @service notifications;
   @service currentUser;
   @service fileSaver;
@@ -47,16 +46,6 @@ export default class IndexController extends Controller {
         token = this.session.data.authenticated.access_token;
       }
       this.fileSaver.save({ url, fileName, token });
-    } catch (error) {
-      this.notifications.error(error);
-    }
-  }
-
-  @action
-  async downloadBeforeJuryFile() {
-    try {
-      const certifications = await this._loadAllCertifications(this.sessionModel);
-      this.sessionInfoService.downloadJuryFile({ sessionId: this.sessionModel.id, certifications });
     } catch (error) {
       this.notifications.error(error);
     }
@@ -120,19 +109,5 @@ export default class IndexController extends Controller {
       this.notifications.error('Erreur lors de l\'assignation à la session');
     }
     this.isShowingAssignmentModal = false;
-  }
-
-  async _loadAllCertifications(session) {
-    const certifications = [];
-    for (let i = 0; i < session.juryCertificationSummaries.length; ++i) {
-      const juryCertificationSummary = session.juryCertificationSummaries.objectAt(i);
-      let certification = this.store.peekRecord('certification', juryCertificationSummary.id);
-      if (!certification) {
-        certification = await this.store.findRecord('certification', juryCertificationSummary.id);
-      }
-      certifications.push(certification);
-    }
-
-    return certifications;
   }
 }
