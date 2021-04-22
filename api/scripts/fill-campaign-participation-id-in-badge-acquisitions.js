a70b467a5ced712cb340461bb546da600af21911
celineung <celine.ung@zenika.com>
2021-04-22 15:20:01 +0200

    Find campaignParticipation from a badge-participation

--- api/scripts/fill-campaign-participation-id-in-badge-acquisitions.js
+++ api/scripts/fill-campaign-participation-id-in-badge-acquisitions.js
@@ -3,12 +3,20 @@ const { knex } = require('../db/knex-database-connection');
 const BadgeAcquisitonsRepository = require('../lib/infrastructure/repositories/badge-acquisition-repository');
 
 async function fillCampaignParticipationIdInBadgeAcquisitions() {
-  return knex('badge-acquisitions').select().where({ campaignParticipationId : null });
+  return knex('badge-acquisitions').select().where({ campaignParticipationId: null });
+}
+
+async function getCampaignParticipationFromBadgeAcquisition(badgeAcquisition) {
+  const badge = await knex('badges').select('targetProfileId').where({ id: badgeAcquisition.badgeId }).first();
+  return knex('campaign-participations')
+    .select('campaign-participations.id')
+    .innerJoin('campaigns', 'campaigns.id', 'campaign-participations.campaignId')
+    .where({ 'campaign-participations.userId': badgeAcquisition.userId, 'campaigns.targetProfileId': badge.targetProfileId });
 }
 
 async function main() {
   try {
-    console.log("Coucou")
+    console.log('Coucou');
 
     // Récupérer les badges acquisitions sans campaignParticipationid
     const badgeAcquisitionsWithoutCPI = await fillCampaignParticipationIdInBadgeAcquisitions();
@@ -33,5 +41,6 @@ if (require.main === module) {
 }
 
 module.exports = {
-  fillCampaignParticipationIdInBadgeAcquisitions
-}
+  fillCampaignParticipationIdInBadgeAcquisitions,
+  getCampaignParticipationFromBadgeAcquisition,
+};
--- api/tests/integration/scripts/fill-campaign-participation-id-in-badge-acquisitions_test.js
+++ api/tests/integration/scripts/fill-campaign-participation-id-in-badge-acquisitions_test.js
@@ -2,9 +2,10 @@ const { expect, databaseBuilder } = require('../../test-helper');
 
 const {
   fillCampaignParticipationIdInBadgeAcquisitions,
+  getCampaignParticipationFromBadgeAcquisition,
 } = require('../../../scripts/fill-campaign-participation-id-in-badge-acquisitions');
 
-describe('Integration | Scripts | fillCampaignParticipationIdInBadgeAcquisitions', () => {
+describe.only('Integration | Scripts | fillCampaignParticipationIdInBadgeAcquisitions', () => {
 
   describe('#fillCampaignParticipationIdInBadgeAcquisitions', () => {
 
@@ -12,16 +13,16 @@ describe('Integration | Scripts | fillCampaignParticipationIdInBadgeAcquisitions
       // given
       const user = databaseBuilder.factory.buildUser();
       const badge = databaseBuilder.factory.buildBadge();
-      const badgeAcquisitionWithoutCampaignId = databaseBuilder.factory.buildBadgeAcquisition({
+      databaseBuilder.factory.buildBadgeAcquisition({
         userId: user.id,
         badgeId: badge.id,
-        campaignParticipationId: null
+        campaignParticipationId: null,
       });
       const campaignParticipation = databaseBuilder.factory.buildCampaignParticipation();
-      const badgeAcquisitionWithCampaignId = databaseBuilder.factory.buildBadgeAcquisition({
+      databaseBuilder.factory.buildBadgeAcquisition({
         userId: user.id,
         badgeId: badge.id,
-        campaignParticipationId: campaignParticipation.id
+        campaignParticipationId: campaignParticipation.id,
       });
       await databaseBuilder.commit();
 
@@ -31,6 +32,30 @@ describe('Integration | Scripts | fillCampaignParticipationIdInBadgeAcquisitions
       // then
       expect(result).to.have.lengthOf(1);
       expect(result[0].campaignParticipationId).to.equal(null);
+    });
+
+    it('should return possible campaignParticipations for one badge', async () => {
+      // given
+      const user = databaseBuilder.factory.buildUser();
+      const targetProfile = databaseBuilder.factory.buildTargetProfile();
+      const badge = databaseBuilder.factory.buildBadge({ targetProfileId: targetProfile.id });
+      const badgeAcquisitionWithoutCampaignId = databaseBuilder.factory.buildBadgeAcquisition({
+        userId: user.id,
+        badgeId: badge.id,
+        campaignParticipationId: null,
+      });
+      const campaign = databaseBuilder.factory.buildCampaign({ targetProfileId: targetProfile.id });
+      const campaignParticipation = databaseBuilder.factory.buildCampaignParticipation({
+        campaignId: campaign.id,
+        userId: user.id,
+      });
+      await databaseBuilder.commit();
+
+      // when
+      const result = await getCampaignParticipationFromBadgeAcquisition(badgeAcquisitionWithoutCampaignId);
+
+      // then
+      expect(result).to.deep.equal([{ id: campaignParticipation.id }]);
     });
   });
 });
