33698427aea45b2ef95a888451b98b4eca607d01
Pierre TOP (TOPI) <ptop@octo.com>
2021-04-23 08:05:00 +0200

    [API] Add get verb to http-agent

--- api/lib/infrastructure/http/http-agent.js
+++ api/lib/infrastructure/http/http-agent.js
@@ -46,4 +46,34 @@ module.exports = {
       });
     }
   },
+  async get({ url, payload, headers }) {
+    try {
+      const config = { data: payload, headers };
+      const httpResponse = await axios.get(url, config);
+      return new HttpResponse({
+        code: httpResponse.status,
+        data: httpResponse.data,
+        isSuccessful: true,
+      });
+    } catch (httpErr) {
+      const isSuccessful = false;
+
+      let code;
+      let data;
+
+      if (httpErr.response) {
+        code = httpErr.response.status;
+        data = httpErr.response.data;
+      } else {
+        code = '500';
+        data = null;
+      }
+
+      return new HttpResponse({
+        code,
+        data,
+        isSuccessful,
+      });
+    }
+  },
 };
