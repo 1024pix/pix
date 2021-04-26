20ae751661c59f0b5ca1d568d8005c806fb4ca2a
Mathieu Gilet <mathieu.gilet@zenika.com>
2021-04-26 16:29:33 +0200

    [API] Remove link not working in challenge issue (E6)

--- api/tests/unit/domain/models/CertificationIssueReport_test.js
+++ api/tests/unit/domain/models/CertificationIssueReport_test.js
@@ -191,7 +191,6 @@ describe('Unit | Domain | Models | CertificationIssueReport', () => {
       [...Object.values(CertificationIssueReportSubcategories)].forEach((subcategory) => {
         if ([
           CertificationIssueReportSubcategories.IMAGE_NOT_DISPLAYING,
-          CertificationIssueReportSubcategories.LINK_NOT_WORKING,
           CertificationIssueReportSubcategories.EMBED_NOT_WORKING,
           CertificationIssueReportSubcategories.FILE_NOT_OPENING,
           CertificationIssueReportSubcategories.WEBSITE_UNAVAILABLE,
@@ -214,7 +213,6 @@ describe('Unit | Domain | Models | CertificationIssueReport', () => {
 
       [
         CertificationIssueReportSubcategories.IMAGE_NOT_DISPLAYING,
-        CertificationIssueReportSubcategories.LINK_NOT_WORKING,
         CertificationIssueReportSubcategories.EMBED_NOT_WORKING,
         CertificationIssueReportSubcategories.FILE_NOT_OPENING,
         CertificationIssueReportSubcategories.WEBSITE_UNAVAILABLE,
@@ -261,6 +259,7 @@ describe('Unit | Domain | Models | CertificationIssueReport', () => {
           .to.throw(InvalidCertificationIssueReportForSaving);
       });
     });
+
     context('CATEGORY: FRAUD', () => {
       const certificationIssueReportDTO = {
         certificationCourseId: 123,
@@ -319,7 +318,6 @@ describe('Unit | Domain | Models | CertificationIssueReport', () => {
         { certificationCourseId: 42, category: 'IN_CHALLENGE', subcategory: 'WEBSITE_UNAVAILABLE', questionNumber: 42 },
         { certificationCourseId: 42, category: 'IN_CHALLENGE', subcategory: 'FILE_NOT_OPENING', questionNumber: 42 },
         { certificationCourseId: 42, category: 'IN_CHALLENGE', subcategory: 'OTHER', description: 'toto', questionNumber: 42 },
-        { certificationCourseId: 42, category: 'IN_CHALLENGE', subcategory: 'LINK_NOT_WORKING', questionNumber: 42 },
         { certificationCourseId: 42, category: 'IN_CHALLENGE', subcategory: 'IMAGE_NOT_DISPLAYING', questionNumber: 42 },
         { certificationCourseId: 42, category: 'IN_CHALLENGE', subcategory: 'EMBED_NOT_WORKING', questionNumber: 42 },
         { certificationCourseId: 42, category: 'TECHNICAL_PROBLEM', description: 'toto' },
