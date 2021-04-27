bdaba0ba9b671cfe4663418437775d656a563864
melanieb <melanie.boudard@gmail.com>
2021-04-27 17:19:26 +0200

    Update the eligibility of the clea badge during the certification

--- api/tests/unit/domain/events/handle-clea-certification-scoring_test.js
+++ api/tests/unit/domain/events/handle-clea-certification-scoring_test.js
@@ -12,8 +12,25 @@ describe('Unit | Domain | Events | handle-clea-certification-scoring', () => {
     save: _.noop(),
   };
 
+  const badgeRepository = {
+    getByKey: _.noop(),
+  };
+  const knowledgeElementRepository = {
+    findUniqByUserId: _.noop(),
+  };
+  const targetProfileRepository = {
+    get: _.noop(),
+  };
+  const badgeCriteriaService = {
+    areBadgeCriteriaFulfilled: _.noop(),
+  };
+
   const dependencies = {
     partnerCertificationScoringRepository,
+    badgeRepository,
+    knowledgeElementRepository,
+    targetProfileRepository,
+    badgeCriteriaService,
   };
 
   it('fails when event is not of correct type', async () => {
@@ -32,7 +49,10 @@ describe('Unit | Domain | Events | handle-clea-certification-scoring', () => {
   context('#handleCleaCertificationScoring', () => {
     const certificationCourseId = Symbol('certificationCourseId');
     const userId = Symbol('userId');
-    const cleaCertificationScoring = {};
+    const cleaCertificationScoring = { hasAcquiredBadge: true };
+    const targetProfile = { id: 'targetProfileId' };
+    const badge = { targetProfileId: targetProfile.id };
+    const knowledgeElements = Symbol('KnowledgeElements@& ');
 
     beforeEach(() => {
       event = new CertificationScoringCompleted({
@@ -42,6 +62,12 @@ describe('Unit | Domain | Events | handle-clea-certification-scoring', () => {
         reproducibilityRate,
         limitDate: new Date('2018-02-03'),
       });
+
+      badgeRepository.getByKey = sinon.stub().withArgs({ }).resolves(badge);
+      targetProfileRepository.get = sinon.stub().withArgs(targetProfile.id).resolves(targetProfile);
+      knowledgeElementRepository.findUniqByUserId = sinon.stub().withArgs({ userId: event.userId }).resolves(knowledgeElements);
+      badgeCriteriaService.areBadgeCriteriaFulfilled = sinon.stub().withArgs({ knowledgeElements, targetProfile, badge }).returns(true);
+      cleaCertificationScoring.setBadgeStillAcquired = sinon.stub().returns(true);
 
       partnerCertificationScoringRepository.save = sinon.stub();
       partnerCertificationScoringRepository.buildCleaCertificationScoring = sinon.stub();
@@ -55,6 +81,20 @@ describe('Unit | Domain | Events | handle-clea-certification-scoring', () => {
     });
 
     context('when certification is eligible', () => {
+
+      it('it should verify if the badge is still acquired', async () => {
+        // given
+        cleaCertificationScoring.isEligible = () => true;
+
+        // when
+        await handleCleaCertificationScoring({
+          event, ...dependencies,
+        });
+
+        // then
+        expect(badgeCriteriaService.areBadgeCriteriaFulfilled).to.have.been.calledWith({ knowledgeElements, targetProfile, badge });
+        expect(cleaCertificationScoring.setBadgeStillAcquired).to.have.been.calledWith(true);
+      });
 
       it('it should save a certif partner', async () => {
         // given
@@ -73,6 +113,21 @@ describe('Unit | Domain | Events | handle-clea-certification-scoring', () => {
     });
 
     context('when certification is not eligible', () => {
+
+      it('it not should verify if the badge is still acquired if the badge is not acquired', async () => {
+        // given
+        cleaCertificationScoring.hasAcquiredBadge = false;
+        cleaCertificationScoring.isEligible = () => true;
+
+        // when
+        await handleCleaCertificationScoring({
+          event, ...dependencies,
+        });
+
+        // then
+        expect(badgeCriteriaService.areBadgeCriteriaFulfilled).to.not.to.have.been.called;
+        expect(cleaCertificationScoring.setBadgeStillAcquired).to.not.to.have.been.called;
+      });
 
       it('it should not save a certif partner', async () => {
         // given
