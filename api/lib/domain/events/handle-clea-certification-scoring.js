bb5d6c33bc30f81b5479e33da0c48e1ea835fbde
melanieb <melanie.boudard@gmail.com>
2021-04-27 17:19:26 +0200

    Update the eligibility of the clea badge during the certification

--- api/lib/domain/events/handle-clea-certification-scoring.js
+++ api/lib/domain/events/handle-clea-certification-scoring.js
@@ -6,6 +6,10 @@ const eventTypes = [ CertificationScoringCompleted ];
 async function handleCleaCertificationScoring({
   event,
   partnerCertificationScoringRepository,
+  badgeRepository,
+  knowledgeElementRepository,
+  targetProfileRepository,
+  badgeCriteriaService,
 }) {
   checkEventTypes(event, eventTypes);
   const cleaCertificationScoring = await partnerCertificationScoringRepository.buildCleaCertificationScoring({
@@ -14,6 +18,12 @@ async function handleCleaCertificationScoring({
     reproducibilityRate: event.reproducibilityRate,
   });
 
+  if (cleaCertificationScoring.hasAcquiredBadge) {
+    const badge = await badgeRepository.getByKey(cleaCertificationScoring.partnerKey);
+    const targetProfile = await targetProfileRepository.get(badge.targetProfileId);
+    const knowledgeElements = await knowledgeElementRepository.findUniqByUserId({ userId: event.userId });
+    cleaCertificationScoring.setBadgeStillAcquired(badgeCriteriaService.areBadgeCriteriaFulfilled({ knowledgeElements, targetProfile, badge }));
+  }
   if (cleaCertificationScoring.isEligible()) {
     await partnerCertificationScoringRepository.save({ partnerCertificationScoring: cleaCertificationScoring });
   }
