23167fd0e5d3765aadca3340e535dded88ec9699
François de Metz <francois.de-metz@pix.fr>
2021-04-28 12:04:39 +0200

    Use PixButton to reload the cache

--- admin/app/controllers/authenticated/tools.js
+++ admin/app/controllers/authenticated/tools.js
@@ -6,20 +6,15 @@ import Controller from '@ember/controller';
 
 export default class ToolsController extends Controller {
 
-  isLoading = false;
-
   @service notifications;
 
   @action
   async refreshLearningContent() {
-    this.set('isLoading', true);
     try {
       await this.store.adapterFor('learning-content-cache').refreshCacheEntries();
       this.notifications.success('La demande de rechargement du cache a bien été prise en compte.');
     } catch (err) {
       this.notifications.error('Une erreur est survenue.');
-    } finally {
-      this.set('isLoading', false);
     }
   }
 }
