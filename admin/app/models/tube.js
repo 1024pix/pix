94e66fb955bee3566cc2155988ddee16724add22
François de Metz <francois.de-metz@pix.fr>
2021-04-28 10:53:02 +0200

    Add relations between skills, tubes and badge partner competences
    Co-authored-by: Guillaume Lagorce <guillaume.lagorce@pix.fr>
    Co-authored-by: Jonas Grilleres <jonas.grilleres@pix.fr>

--- admin/app/models/tube.js
+++ admin/app/models/tube.js
@@ -1,6 +1,8 @@
-import Model, { attr } from '@ember-data/model';
+import Model, { attr, hasMany } from '@ember-data/model';
 
 export default class Tube extends Model {
   @attr('string') practicalTitle;
   @attr('string') competenceId;
+
+  @hasMany('skill') skills;
 }
