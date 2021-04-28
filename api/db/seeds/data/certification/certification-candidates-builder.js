4fa767b6e7b22367919ec23e25af604a1a98da52
Antoine Ceol <antoine.ceol@gmail.com>
2021-04-28 16:30:47 +0200

    [SEEDS] Add sco students with published certif in certification seeds

--- api/db/seeds/data/certification/certification-candidates-builder.js
+++ api/db/seeds/data/certification/certification-candidates-builder.js
@@ -6,12 +6,14 @@ const {
   NO_PROBLEM_FINALIZED_SESSION_ID,
   PROBLEMS_FINALIZED_SESSION_ID,
   PUBLISHED_SESSION_ID,
+  PUBLISHED_SCO_SESSION_ID,
   PIX_DROIT_SESSION_ID,
 } = require('./certification-sessions-builder');
 const {
   CERTIF_SUCCESS_USER_ID,
   CERTIF_FAILED_USER_ID,
   CERTIF_REGULAR_USER5_ID,
+  CERTIF_SCO_STUDENT_ID,
 } = require('./users');
 
 const A_LOT_OF_CANDIDATES_COUNT = 150;
@@ -24,10 +26,12 @@ const FAILURE_CANDIDATE_IN_PROBLEMS_FINALIZED_SESSION_ID = 6;
 const STARTED_CANDIDATE_IN_PROBLEMS_FINALIZED_SESSION_ID = 7;
 const SUCCESS_CANDIDATE_IN_PUBLISHED_SESSION_ID = 8;
 const FAILURE_CANDIDATE_IN_PUBLISHED_SESSION_ID = 9;
+const SUCCESS_SCO_CANDIDATE_IN_PUBLISHED_SESSION_ID = 10;
 const CANDIDATE_DATA_SUCCESS = { firstName: 'anne', lastName: 'success', birthdate: '2000-01-01', birthCity: 'Ici', resultRecipientEmail: 'destinaire-des-resulats@example.net' };
 const CANDIDATE_DATA_FAILURE = { firstName: 'anne', lastName: 'failure', birthdate: '2000-01-01', birthCity: 'Ici', resultRecipientEmail: 'destinaire-des-resulats@example.net' };
 const CANDIDATE_DATA_MISSING = { firstName: 'anne', lastName: 'missing', birthdate: '2000-01-01', birthCity: 'Ici', resultRecipientEmail: null };
 const CANDIDATE_DATA_STARTED = { firstName: 'AnneNormale5', lastName: 'Certif5', birthdate: '2000-01-01', birthCity: 'Ici', resultRecipientEmail: null };
+const CANDIDATE_SCO_DATA_SUCCESS = { firstName: 'Student', lastName: 'Certif', birthdate: '2000-01-01', birthCity: 'Ici', resultRecipientEmail: null };
 const CANDIDATE_DROIT_1 = { firstName: 'Nicky', lastName: 'Larson', birthdate: '2000-01-01', birthCity: 'Ici', resultRecipientEmail: null };
 const CANDIDATE_DROIT_2 = { firstName: 'Saul', lastName: 'Goodman', birthdate: '2000-01-01', birthCity: 'Ici', resultRecipientEmail: null };
 
@@ -45,6 +49,7 @@ function certificationCandidatesBuilder({ databaseBuilder }) {
 
   let sessionId;
   const candidateDataSuccessWithUser = { ...CANDIDATE_DATA_SUCCESS, userId: CERTIF_SUCCESS_USER_ID };
+  const candidateDataSuccessWithUserSco = { ...CANDIDATE_SCO_DATA_SUCCESS, userId: CERTIF_SCO_STUDENT_ID };
   const candidateDataFailureWithUser = { ...CANDIDATE_DATA_FAILURE, userId: CERTIF_FAILED_USER_ID };
   const candidateDataMissingWithUser = { ...CANDIDATE_DATA_MISSING, userId: null };
   const candidateDataStartedWithUser = { ...CANDIDATE_DATA_STARTED, userId: CERTIF_REGULAR_USER5_ID };
@@ -73,6 +78,10 @@ function certificationCandidatesBuilder({ databaseBuilder }) {
   databaseBuilder.factory.buildCertificationCandidate({ id: SUCCESS_CANDIDATE_IN_PUBLISHED_SESSION_ID, ...candidateDataSuccessWithUser, sessionId });
   databaseBuilder.factory.buildCertificationCandidate({ id: FAILURE_CANDIDATE_IN_PUBLISHED_SESSION_ID, ...candidateDataFailureWithUser, sessionId });
 
+  // One candidate for published sco session
+  sessionId = PUBLISHED_SCO_SESSION_ID;
+  databaseBuilder.factory.buildCertificationCandidate({ id: SUCCESS_SCO_CANDIDATE_IN_PUBLISHED_SESSION_ID, ...candidateDataSuccessWithUserSco, sessionId });
+
   // Candidates for Pix+Droit certification
   sessionId = PIX_DROIT_SESSION_ID;
   databaseBuilder.factory.buildCertificationCandidate({ ...CANDIDATE_DROIT_1, sessionId, userId: null });
@@ -90,8 +99,10 @@ module.exports = {
   FAILURE_CANDIDATE_IN_PROBLEMS_FINALIZED_SESSION_ID,
   SUCCESS_CANDIDATE_IN_PUBLISHED_SESSION_ID,
   FAILURE_CANDIDATE_IN_PUBLISHED_SESSION_ID,
+  SUCCESS_SCO_CANDIDATE_IN_PUBLISHED_SESSION_ID,
   CANDIDATE_DATA_SUCCESS,
   CANDIDATE_DATA_FAILURE,
   CANDIDATE_DATA_MISSING,
   CANDIDATE_DATA_STARTED,
+  CANDIDATE_SCO_DATA_SUCCESS,
 };
