efcde15425ed988465c161fa20b5102f88028040
François de Metz <francois.de-metz@pix.fr>
2021-04-28 10:46:22 +0200

    Add model BadgeWithLearningContent

---
+++ api/lib/domain/models/BadgeWithLearningContent.js
@@ -0,0 +1,9 @@
+class BadgeWithLearningContent {
+  constructor({ badge, skills, tubes } = {}) {
+    this.badge = badge;
+    this.skills = skills;
+    this.tubes = tubes;
+  }
+}
+
+module.exports = BadgeWithLearningContent;
