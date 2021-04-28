b91d95098236475392c7aa2c6c2248af616a69d7
François de Metz <francois.de-metz@pix.fr>
2021-04-28 12:28:06 +0200

    Add UI to create the release and refresh the cache

--- admin/app/controllers/authenticated/tools.js
+++ admin/app/controllers/authenticated/tools.js
@@ -17,4 +17,14 @@ export default class ToolsController extends Controller {
       this.notifications.error('Une erreur est survenue.');
     }
   }
+
+  @action
+  async createLearningContentReleaseAndRefreshCache() {
+    try {
+      await this.store.adapterFor('learning-content-cache').createLearningContentReleaseAndRefreshCache();
+      this.notifications.success('La création de la version du référentiel et le rechargement du cache a bien été prise en compte.');
+    } catch (err) {
+      this.notifications.error('Une erreur est survenue.');
+    }
+  }
 }
