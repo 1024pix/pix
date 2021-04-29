e95e168508288971a49367606401f07b1a2ef2a5
Benjamin Petetot <bpetetot@gmail.com>
2021-04-29 11:17:40 +0200

    [API] Remove supernumerary in import csv usecase

--- api/lib/domain/usecases/import-higher-schooling-registrations.js
+++ api/lib/domain/usecases/import-higher-schooling-registrations.js
@@ -1,15 +1,5 @@
 const _ = require('lodash');
-const { findMatchingCandidateIdForGivenUser } = require('../../domain/services/user-reconciliation-service');
 const DomainTransaction = require('../../infrastructure/DomainTransaction');
-
-function getMatchingSchoolingRegistration(registration, existingRegistrations) {
-  const matchingCandidateId = findMatchingCandidateIdForGivenUser(existingRegistrations, registration);
-  const matchingCandidate = existingRegistrations.find((existing) => existing.id === matchingCandidateId);
-  if (!matchingCandidate || matchingCandidate.birthdate !== registration.birthdate) {
-    return null;
-  }
-  return matchingCandidate;
-}
 
 module.exports = async function importHigherSchoolingRegistration({
   organizationId,
@@ -19,25 +9,15 @@ module.exports = async function importHigherSchoolingRegistration({
   const higherSchoolingRegistrationSet = higherSchoolingRegistrationParser.parse();
 
   await DomainTransaction.execute(async (domainTransaction) => {
-    const studentNumbersInOrga = await higherSchoolingRegistrationRepository.findStudentNumbersNonSupernumerary(organizationId, domainTransaction);
-    const [registrationsToUpdate, registrationsToCheck] = _.partition(higherSchoolingRegistrationSet.registrations, (registration) => {
+    const studentNumbersInOrga = await higherSchoolingRegistrationRepository.findStudentNumbersByOrganization(organizationId, domainTransaction);
+
+    const [registrationsToUpdate, registrationsToCreate] = _.partition(higherSchoolingRegistrationSet.registrations, (registration) => {
       return studentNumbersInOrga.includes(registration.studentNumber);
     });
 
-    for (const registration of registrationsToUpdate) {
-      await higherSchoolingRegistrationRepository.saveNonSupernumerary(registration, domainTransaction);
-    }
-
-    const supernumeraryRegistrations = await higherSchoolingRegistrationRepository.findSupernumerary(organizationId, domainTransaction);
-
-    const registrationsToCreate = [];
-    for (const registration of registrationsToCheck) {
-      const existingRegistrations = _.filter(supernumeraryRegistrations, ({ studentNumber }) => registration.studentNumber === studentNumber);
-      const matchingRegistration = getMatchingSchoolingRegistration(registration, existingRegistrations);
-      if (matchingRegistration) {
-        await higherSchoolingRegistrationRepository.save({ id: matchingRegistration.id, ...registration }, domainTransaction);
-      } else {
-        registrationsToCreate.push(registration);
+    if (!_.isEmpty(registrationsToUpdate)) {
+      for (const registration of registrationsToUpdate) {
+        await higherSchoolingRegistrationRepository.saveByStudentNumber(registration, domainTransaction);
       }
     }
 
