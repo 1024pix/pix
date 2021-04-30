bc437981741790787a717b4207aebf2afa2eaa8e
Laura Bergoens <laura.bergoens@pix.fr>
2021-04-30 16:15:46 +0200

    chore(api): add seed to have one user with several partner certifications

--- api/db/seeds/data/certification/partner-certification-builder.js
+++ api/db/seeds/data/certification/partner-certification-builder.js
@@ -3,6 +3,7 @@ const { CERTIFICATION_COURSE_SUCCESS_ID, CERTIFICATION_COURSE_FAILURE_ID } = req
 
 function partnerCertificationBuilder({ databaseBuilder }) {
   databaseBuilder.factory.buildPartnerCertification({ certificationCourseId: CERTIFICATION_COURSE_SUCCESS_ID, acquired: true, partnerKey: Badge.keys.PIX_EMPLOI_CLEA });
+  databaseBuilder.factory.buildPartnerCertification({ certificationCourseId: CERTIFICATION_COURSE_SUCCESS_ID, acquired: true, partnerKey: 'PIX_DROIT_MAITRE_CERTIF' });
   databaseBuilder.factory.buildPartnerCertification({ certificationCourseId: CERTIFICATION_COURSE_FAILURE_ID, acquired: false, partnerKey: Badge.keys.PIX_EMPLOI_CLEA });
 }
 
