02a8712d4899bca8c42bb4f7bd09be2fd27e41b8
Benjamin Petetot <bpetetot@gmail.com>
2021-05-03 14:29:13 +0200

    [API] Set as unknown diploma and study scheme if not recognized

--- api/lib/domain/models/HigherSchoolingRegistrationSet.js
+++ api/lib/domain/models/HigherSchoolingRegistrationSet.js
@@ -1,15 +1,80 @@
 const HigherSchoolingRegistration = require('./HigherSchoolingRegistration');
 const { checkValidation } = require('../validators/higher-schooling-registration-set-validator');
+const { areTwoStringsCloseEnough } = require('../services/string-comparison-service');
+
+const RATIO = 0.25;
+
+const STUDY_SCHEMES = [
+  'csv-import-values.higher-schooling-registrations.study-schemes.initial-training',
+  'csv-import-values.higher-schooling-registrations.study-schemes.training',
+  'csv-import-values.higher-schooling-registrations.study-schemes.continuous-training',
+  'csv-import-values.higher-schooling-registrations.study-schemes.other',
+];
+
+const DIPLOMAS = [
+  'csv-import-values.higher-schooling-registrations.diplomas.license',
+  'csv-import-values.higher-schooling-registrations.diplomas.master',
+  'csv-import-values.higher-schooling-registrations.diplomas.doctorat',
+  'csv-import-values.higher-schooling-registrations.diplomas.dnmade',
+  'csv-import-values.higher-schooling-registrations.diplomas.dma ',
+  'csv-import-values.higher-schooling-registrations.diplomas.bts',
+  'csv-import-values.higher-schooling-registrations.diplomas.dut',
+  'csv-import-values.higher-schooling-registrations.diplomas.dts',
+  'csv-import-values.higher-schooling-registrations.diplomas.dcg ',
+  'csv-import-values.higher-schooling-registrations.diplomas.deust',
+  'csv-import-values.higher-schooling-registrations.diplomas.etat-controle',
+  'csv-import-values.higher-schooling-registrations.diplomas.ingenieur',
+  'csv-import-values.higher-schooling-registrations.diplomas.license-grade',
+  'csv-import-values.higher-schooling-registrations.diplomas.master-grade',
+  'csv-import-values.higher-schooling-registrations.diplomas.vise',
+  'csv-import-values.higher-schooling-registrations.diplomas.classe-preparatoire',
+  'csv-import-values.higher-schooling-registrations.diplomas.other',
+];
+
+const UNKNOWN = 'csv-import-values.higher-schooling-registrations.unknown';
+
 class HigherSchoolingRegistrationSet {
 
-  constructor() {
+  constructor(i18n) {
+    this.i18n = i18n;
     this.registrations = [];
+    this.warnings = [];
   }
 
   addRegistration(registrationAttributes) {
     const registration = new HigherSchoolingRegistration(registrationAttributes);
+    this._checkStudyScheme(registration);
+    this._checkDiploma(registration);
     this.registrations.push(registration);
+
     checkValidation(this);
+  }
+
+  addWarning(studentNumber, field, value, code) {
+    this.warnings.push({ studentNumber, field, value, code });
+  }
+
+  _checkStudyScheme(registration) {
+    const { studentNumber, studyScheme } = registration;
+    if (this._isValidI18nValue(STUDY_SCHEMES, studyScheme)) return;
+    this.addWarning(studentNumber, 'study-scheme', registration.studyScheme, 'unknown');
+    registration.studyScheme = this.i18n.__(UNKNOWN);
+  }
+
+  _checkDiploma(registration) {
+    const { studentNumber, diploma } = registration;
+    if (this._isValidI18nValue(DIPLOMAS, diploma)) return;
+    this.addWarning(studentNumber, 'diploma', registration.diploma, 'unknown');
+    registration.diploma = this.i18n.__(UNKNOWN);
+  }
+
+  _isValidI18nValue(keys, valueToCheck) {
+    if (!valueToCheck) return false;
+    return keys.some((key) => {
+      const reference = this.i18n.__(key).toLowerCase();
+      const input = valueToCheck.toLowerCase();
+      return areTwoStringsCloseEnough(input, reference, RATIO);
+    });
   }
 }
 
--- api/tests/unit/domain/models/HigherSchoolingRegistrationSet_test.js
+++ api/tests/unit/domain/models/HigherSchoolingRegistrationSet_test.js
@@ -1,14 +1,15 @@
 const HigherSchoolingRegistrationSet = require('../../../../lib/domain/models/HigherSchoolingRegistrationSet');
 const { expect, catchErr } = require('../../../test-helper');
+const { getI18n } = require('../../../tooling/i18n/i18n');
 const { EntityValidationError } = require('../../../../lib/domain/errors');
 
 describe('Unit | Domain | Models | HigherSchoolingRegistrationSet', () => {
+  const i18n = getI18n();
 
   context('#addRegistration', () => {
     context('when set has no registration', () => {
       it('creates the first registration of the set', () => {
-
-        const higherSchoolingRegistrationSet = new HigherSchoolingRegistrationSet();
+        const higherSchoolingRegistrationSet = new HigherSchoolingRegistrationSet(i18n);
         const registrationAttributes = {
           firstName: 'Beatrix',
           middleName: 'The',
@@ -18,11 +19,11 @@ describe('Unit | Domain | Models | HigherSchoolingRegistrationSet', () => {
           studentNumber: '1',
           email: 'thebride@example.net',
           birthdate: new Date('1980-07-01'),
-          diploma: 'Master',
+          diploma: 'Autre',
           department: 'Assassination Squad',
           educationalTeam: 'Pai Mei',
           group: 'Deadly Viper Assassination Squad',
-          studyScheme: 'I have no idea what it\'s like.',
+          studyScheme: 'Autre',
           organizationId: 1,
         };
 
@@ -36,8 +37,7 @@ describe('Unit | Domain | Models | HigherSchoolingRegistrationSet', () => {
 
     context('when set has registrations', () => {
       it('creates the a new registration for the set', () => {
-
-        const higherSchoolingRegistrationSet = new HigherSchoolingRegistrationSet();
+        const higherSchoolingRegistrationSet = new HigherSchoolingRegistrationSet(i18n);
         const registration1 = {
           firstName: 'Beatrix',
           middleName: 'The',
@@ -47,11 +47,11 @@ describe('Unit | Domain | Models | HigherSchoolingRegistrationSet', () => {
           studentNumber: '1',
           email: 'thebride@example.net',
           birthdate: new Date('1980-07-01'),
-          diploma: 'Master',
+          diploma: 'Autre',
           department: 'Assassination Squad',
           educationalTeam: 'Pai Mei',
           group: 'Deadly Viper Assassination Squad',
-          studyScheme: 'I have no idea what it\'s like.',
+          studyScheme: 'Autre',
           userId: 12345,
           organizationId: 1,
         };
@@ -64,11 +64,11 @@ describe('Unit | Domain | Models | HigherSchoolingRegistrationSet', () => {
           studentNumber: '2',
           email: 'bill@example.net',
           birthdate: new Date('1960-07-01'),
-          diploma: 'Doctorat',
+          diploma: 'Autre',
           department: 'Assassination Squad Management',
           educationalTeam: 'Pai Mei',
           group: 'Deadly Viper Assassination Squad',
-          studyScheme: 'I have always no idea what it\'s like.',
+          studyScheme: 'Autre',
           organizationId: 2,
         };
 
@@ -83,8 +83,7 @@ describe('Unit | Domain | Models | HigherSchoolingRegistrationSet', () => {
 
     context('when a registration is not valid', () => {
       it('throws an error', async () => {
-
-        const higherSchoolingRegistrationSet = new HigherSchoolingRegistrationSet();
+        const higherSchoolingRegistrationSet = new HigherSchoolingRegistrationSet(i18n);
         const registration = {
           firstName: null,
           lastName: 'Kiddo',
@@ -100,8 +99,7 @@ describe('Unit | Domain | Models | HigherSchoolingRegistrationSet', () => {
 
     context('when there is a registration with the same student number', () => {
       it('throws an error', async () => {
-
-        const higherSchoolingRegistrationSet = new HigherSchoolingRegistrationSet();
+        const higherSchoolingRegistrationSet = new HigherSchoolingRegistrationSet(i18n);
         const registration1 = {
           firstName: 'Beatrix',
           lastName: 'Kiddo',
@@ -118,11 +116,96 @@ describe('Unit | Domain | Models | HigherSchoolingRegistrationSet', () => {
         };
         await higherSchoolingRegistrationSet.addRegistration(registration1);
 
-        const error = await catchErr(higherSchoolingRegistrationSet.addRegistration, higherSchoolingRegistrationSet)(registration2);
+        const error = await catchErr(
+          higherSchoolingRegistrationSet.addRegistration,
+          higherSchoolingRegistrationSet,
+        )(registration2);
 
         expect(error).to.be.instanceOf(EntityValidationError);
         expect(error.key).to.equal('studentNumber');
         expect(error.why).to.equal('uniqueness');
+      });
+    });
+
+    context('When there are warnings', () => {
+      it('should add a diploma warning', async () => {
+        const higherSchoolingRegistrationSet = new HigherSchoolingRegistrationSet(i18n);
+        const registration = {
+          firstName: 'Beatrix',
+          lastName: 'Kiddo',
+          birthdate: '1990-04-01',
+          studentNumber: '123ABC',
+          organizationId: 123,
+          diploma: 'BAD',
+          studyScheme: 'Autre',
+        };
+
+        higherSchoolingRegistrationSet.addRegistration(registration);
+        const { registrations, warnings } = higherSchoolingRegistrationSet;
+
+        expect(registrations).to.have.lengthOf(1);
+        expect(registrations[0].diploma).to.equal('Non reconnu');
+        expect(warnings).to.have.lengthOf(1);
+        expect(warnings[0]).to.deep.equal({ studentNumber: '123ABC', field: 'diploma', value: 'BAD', code: 'unknown' });
+      });
+
+      it('should add a study scheme warning', async () => {
+        const higherSchoolingRegistrationSet = new HigherSchoolingRegistrationSet(i18n);
+        const registration = {
+          firstName: 'Beatrix',
+          lastName: 'Kiddo',
+          birthdate: '1990-04-01',
+          studentNumber: '123ABC',
+          organizationId: 123,
+          diploma: 'Autre',
+          studyScheme: 'BAD',
+        };
+
+        higherSchoolingRegistrationSet.addRegistration(registration);
+        const { registrations, warnings } = higherSchoolingRegistrationSet;
+
+        expect(registrations).to.have.lengthOf(1);
+        expect(registrations[0].studyScheme).to.equal('Non reconnu');
+        expect(warnings).to.have.lengthOf(1);
+        expect(warnings[0]).to.deep.equal({ studentNumber: '123ABC', field: 'study-scheme', value: 'BAD', code: 'unknown' });
+      });
+
+      it('should check diplomas and study schemes with lower case', async () => {
+        const higherSchoolingRegistrationSet = new HigherSchoolingRegistrationSet(i18n);
+        const registration = {
+          firstName: 'Beatrix',
+          lastName: 'Kiddo',
+          birthdate: '1990-04-01',
+          studentNumber: '123ABC',
+          organizationId: 123,
+          diploma: 'aUTRe',
+          studyScheme: 'aUTRe',
+        };
+
+        higherSchoolingRegistrationSet.addRegistration(registration);
+        const { registrations, warnings } = higherSchoolingRegistrationSet;
+
+        expect(registrations).to.have.lengthOf(1);
+        expect(warnings).to.have.lengthOf(0);
+      });
+
+      it('should check diplomas and study schemes with Levenshtein distance', async () => {
+        const higherSchoolingRegistrationSet = new HigherSchoolingRegistrationSet(i18n);
+        const registration = {
+          firstName: 'Beatrix',
+          lastName: 'Kiddo',
+          birthdate: '1990-04-01',
+          studentNumber: '123ABC',
+          organizationId: 123,
+          diploma: 'Autra',
+          studyScheme: 'Autra',
+        };
+
+        higherSchoolingRegistrationSet.addRegistration(registration);
+        const { registrations, warnings } = higherSchoolingRegistrationSet;
+
+        expect(registrations).to.have.lengthOf(1);
+        expect(warnings).to.have.lengthOf(0);
       });
     });
   });
