4e7893a8ca0d13cd3627ea803ef109416b55cb58
Laura Bergoens <laura.bergoens@pix.fr>
2021-05-03 13:56:12 +0200

    feat(api): add static builder to handle status for PrivateCertificate model

---
+++ api/tests/unit/domain/models/PrivateCertificate_test.js
@@ -0,0 +1,78 @@
+const { expect, domainBuilder } = require('../../../test-helper');
+const PrivateCertificate = require('../../../../lib/domain/models/PrivateCertificate');
+const { status: assessmentResultStatuses } = require('../../../../lib/domain/models/AssessmentResult');
+
+describe('Unit | Domain | Models | PrivateCertificate', () => {
+
+  context('#static buildFrom', () => {
+
+    const commonData = {
+      id: 1,
+      firstName: 'Jean',
+      lastName: 'Bon',
+      birthdate: '2000-03-20',
+      birthplace: 'Sarajevo',
+      isPublished: true,
+      userId: 123,
+      date: '2019-01-01',
+      deliveredAt: new Date('2019-05-05'),
+      certificationCenter: 'Centre des fruits et légumes',
+      pixScore: 250,
+      commentForCandidate: 'Bravo !',
+      cleaCertificationStatus: 'someCleaStatus',
+      resultCompetenceTree: null,
+      verificationCode: 'someVerifCode',
+      maxReachableLevelOnCertificationDate: 5,
+    };
+
+    it('builds a cancelled PrivateCertificate', async () => {
+      // when
+      const privateCertificate = PrivateCertificate.buildFrom({ ...commonData, isCancelled: true });
+
+      // then
+      const expectedPrivateCertificate = domainBuilder.buildPrivateCertificate.cancelled(commonData);
+      expect(privateCertificate).to.be.instanceOf(PrivateCertificate);
+      expect(privateCertificate).to.deep.equal(expectedPrivateCertificate);
+    });
+
+    it('builds a validated PrivateCertificate', async () => {
+      // when
+      const privateCertificate = PrivateCertificate.buildFrom({ ...commonData, isCancelled: false, assessmentResultStatus: assessmentResultStatuses.VALIDATED });
+
+      // then
+      const expectedPrivateCertificate = domainBuilder.buildPrivateCertificate.validated(commonData);
+      expect(privateCertificate).to.be.instanceOf(PrivateCertificate);
+      expect(privateCertificate).to.deep.equal(expectedPrivateCertificate);
+    });
+
+    it('builds a rejected PrivateCertificate', async () => {
+      // when
+      const privateCertificate = PrivateCertificate.buildFrom({ ...commonData, isCancelled: false, assessmentResultStatus: assessmentResultStatuses.REJECTED });
+
+      // then
+      const expectedPrivateCertificate = domainBuilder.buildPrivateCertificate.rejected(commonData);
+      expect(privateCertificate).to.be.instanceOf(PrivateCertificate);
+      expect(privateCertificate).to.deep.equal(expectedPrivateCertificate);
+    });
+
+    it('builds an error PrivateCertificate', async () => {
+      // when
+      const privateCertificate = PrivateCertificate.buildFrom({ ...commonData, isCancelled: false, assessmentResultStatus: assessmentResultStatuses.ERROR });
+
+      // then
+      const expectedPrivateCertificate = domainBuilder.buildPrivateCertificate.error(commonData);
+      expect(privateCertificate).to.be.instanceOf(PrivateCertificate);
+      expect(privateCertificate).to.deep.equal(expectedPrivateCertificate);
+    });
+
+    it('builds a started PrivateCertificate', async () => {
+      // when
+      const privateCertificate = PrivateCertificate.buildFrom({ ...commonData, isCancelled: false, assessmentResultStatus: null });
+
+      // then
+      const expectedPrivateCertificate = domainBuilder.buildPrivateCertificate.started(commonData);
+      expect(privateCertificate).to.be.instanceOf(PrivateCertificate);
+      expect(privateCertificate).to.deep.equal(expectedPrivateCertificate);
+    });
+  });
+});
