eb9e62cdfebc359470d9052d8c254a9e18eda851
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2021-05-04 11:10:36 +0200

    Add call to LCMS API to create a release
    Co-authored-by: François de Metz <francois.de-metz@pix.fr>
    Co-authored-by: Thomas Evano <thomas.evano@pix.fr>
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>

--- api/tests/unit/domain/usecases/create-lcms-release_test.js
+++ api/tests/unit/domain/usecases/create-lcms-release_test.js
@@ -1,5 +1,6 @@
 const { expect, sinon } = require('../../../test-helper');
 const lcms = require('../../../../lib/infrastructure/lcms');
+const cache = require('../../../../lib/infrastructure/caches/learning-content-cache');
 const createLcmsRelease = require('../../../../lib/domain/usecases/create-lcms-release');
 
 describe('Unit | UseCase | create-lcms-release', () => {
@@ -12,5 +13,18 @@ describe('Unit | UseCase | create-lcms-release', () => {
 
     // then
     expect(lcms.createRelease).to.have.been.called;
+  });
+
+  it('should update cache', async () => {
+    // given
+    const learningContent = Symbol('learning-content');
+    sinon.stub(lcms, 'createRelease').resolves(learningContent);
+    sinon.stub(cache, 'set').resolves();
+
+    // when
+    await createLcmsRelease();
+
+    // then
+    expect(cache.set).to.have.been.calledWith('LearningContent', learningContent);
   });
 });
