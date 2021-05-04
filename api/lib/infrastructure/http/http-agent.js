9323bb6c03cbe5c49199513caf2a18d4f890842c
François de Metz <francois.de-metz@pix.fr>
2021-05-04 12:41:00 +0200

    Add debug logs to trace requests on http agent

--- api/lib/infrastructure/http/http-agent.js
+++ api/lib/infrastructure/http/http-agent.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-restricted-modules
 const axios = require('axios');
+const logger = require('../logger');
 
 class HttpResponse {
   constructor({
@@ -15,10 +16,15 @@ class HttpResponse {
 
 module.exports = {
   async post({ url, payload, headers }) {
+    logger.debug(`Starting POST request to ${url}`);
+
     try {
       const httpResponse = await axios.post(url, payload, {
         headers,
       });
+
+      logger.debug(`End POST request to ${url} success: ${httpResponse.status}`);
+
       return new HttpResponse({
         code: httpResponse.status,
         data: httpResponse.data,
@@ -35,6 +41,8 @@ module.exports = {
         data = httpErr.message;
       }
 
+      logger.debug(`End POST request to ${url} error: ${code} ${data.toString()}`);
+
       return new HttpResponse({
         code,
         data,
@@ -43,9 +51,14 @@ module.exports = {
     }
   },
   async get({ url, payload, headers }) {
+    logger.debug(`Starting GET request to ${url}`);
+
     try {
       const config = { data: payload, headers };
       const httpResponse = await axios.get(url, config);
+
+      logger.debug(`End GET request to ${url} success: ${httpResponse.status}`);
+
       return new HttpResponse({
         code: httpResponse.status,
         data: httpResponse.data,
@@ -64,6 +77,8 @@ module.exports = {
         code = '500';
         data = null;
       }
+
+      logger.debug(`End GET request to ${url} error: ${code}`);
 
       return new HttpResponse({
         code,
