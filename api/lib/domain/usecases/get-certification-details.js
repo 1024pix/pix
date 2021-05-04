68e153a31c78a820bbf8d6f45ab16eca3cf55043
Etienne GIROT <egirot@octo.com>
2021-05-04 17:08:13 +0200

    Compute certif details on the fly when not completed

--- api/lib/domain/usecases/get-certification-details.js
+++ api/lib/domain/usecases/get-certification-details.js
@@ -1,13 +1,39 @@
 const CertificationDetails = require('../read-models/CertificationDetails');
+const CertificationAssessmentStates = require('../models/CertificationAssessment').states;
 
 module.exports = async function getCertificationDetails({
   certificationCourseId,
   competenceMarkRepository,
   certificationAssessmentRepository,
   placementProfileService,
+  certificationService,
 }) {
   const certificationAssessment = await certificationAssessmentRepository.getByCertificationCourseId({ certificationCourseId });
+  if (certificationAssessment.state === CertificationAssessmentStates.STARTED) {
+    return _computeCertificationDetailsOnTheFly(
+      certificationCourseId,
+      certificationService,
+    );
+  } else {
+    return _retrievePersistedCertificationDetails(
+      certificationCourseId,
+      certificationAssessment,
+      competenceMarkRepository,
+      placementProfileService,
+    );
+  }
+};
 
+async function _computeCertificationDetailsOnTheFly(certificationCourseId, certificationService) {
+  const certificationResult = await certificationService.calculateCertificationResultByCertificationCourseId(certificationCourseId);
+  return new CertificationDetails(
+    {
+      id: certificationCourseId,
+      ...certificationResult,
+    });
+}
+
+async function _retrievePersistedCertificationDetails(certificationCourseId, certificationAssessment, competenceMarkRepository, placementProfileService) {
   const competenceMarks = await competenceMarkRepository.findByCertificationCourseId(certificationCourseId);
 
   const placementProfile = await placementProfileService.getPlacementProfile({
@@ -21,4 +47,4 @@ module.exports = async function getCertificationDetails({
     certificationAssessment,
     placementProfile,
   });
-};
+}
