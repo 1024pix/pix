28ed3d256a9079a5c381687dd08a85b7f0c9e7b1
Clément Bourgoin <clement.bourgoin@pix.fr>
2021-05-04 16:56:54 +0200

    [API] Add isCancelled to GeneralCertificationInformation

--- api/lib/infrastructure/repositories/general-certification-information-repository.js
+++ api/lib/infrastructure/repositories/general-certification-information-repository.js
@@ -41,6 +41,7 @@ function _toDomain({ certificationCourseDTO, certificationIssueReportsDTO }) {
     createdAt: certificationCourseDTO.createdAt,
     completedAt: certificationCourseDTO.completedAt,
     isPublished: certificationCourseDTO.isPublished,
+    isCancelled: certificationCourseDTO.isCancelled,
     isV2Certification: certificationCourseDTO.isV2Certification,
     firstName: certificationCourseDTO.firstName,
     lastName: certificationCourseDTO.lastName,
