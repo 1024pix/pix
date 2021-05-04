ddee17c85980aa3ff244ea016a810f367205d5f8
Stephane Bedeau <sbedeau@octo.com>
2021-05-04 16:02:19 +0200

    test: add integration testing

--- mon-pix/tests/integration/components/feedback-panel_test.js
+++ mon-pix/tests/integration/components/feedback-panel_test.js
@@ -18,14 +18,16 @@ const BUTTON_SEND = '.feedback-panel__button--send';
 
 const TEXTAREA = 'textarea.feedback-panel__field--content';
 const DROPDOWN = '.feedback-panel__dropdown';
-const TUTORIAL_AREA = '.feedback-panel__tutorial-content';
+const TUTORIAL_AREA = '.feedback-panel__quick-help';
 
-const PICK_CATEGORY_WITH_NESTED_LEVEL = 'question';
-const PICK_CATEGORY_WITH_TEXTAREA = 'accessibility';
-const PICK_CATEGORY_WITH_TUTORIAL = 'picture';
+const PICK_SELECT_OPTION_WITH_NESTED_LEVEL = 'question';
+const PICK_ANOTHER_SELECT_OPTION_WITH_NESTED_LEVEL = 'embed';
+const PICK_SELECT_OPTION_WITH_TEXTAREA = 'accessibility';
+const PICK_SELECT_OPTION_WITH_TUTORIAL = 'picture';
+const PICK_SELECT_OPTION_WITH_TEXTAREA_AND_TUTORIAL = '3';
 
 async function setContent(content) {
-  await fillIn(DROPDOWN, PICK_CATEGORY_WITH_TEXTAREA);
+  await fillIn(DROPDOWN, PICK_SELECT_OPTION_WITH_TEXTAREA);
   await fillIn(TEXTAREA, content);
   await blur(TEXTAREA);
 }
@@ -77,7 +79,7 @@ describe('Integration | Component | feedback-panel', function() {
     context('when selecting a category', function() {
       it('should display a second dropdown with the list of questions when category have a nested level', async function() {
         // when
-        await fillIn('.feedback-panel__dropdown', PICK_CATEGORY_WITH_NESTED_LEVEL);
+        await fillIn('.feedback-panel__dropdown', PICK_SELECT_OPTION_WITH_NESTED_LEVEL);
 
         // then
         expect(findAll(DROPDOWN).length).to.equal(2);
@@ -87,7 +89,7 @@ describe('Integration | Component | feedback-panel', function() {
 
       it('should directly display the message box and the submit button when category has a textarea', async function() {
         // when
-        await fillIn('.feedback-panel__dropdown', PICK_CATEGORY_WITH_TEXTAREA);
+        await fillIn('.feedback-panel__dropdown', PICK_SELECT_OPTION_WITH_TEXTAREA);
 
         // then
         expect(findAll(DROPDOWN).length).to.equal(1);
@@ -96,7 +98,7 @@ describe('Integration | Component | feedback-panel', function() {
 
       it('should directly display the tuto without the textbox nor the send button when category has a tutorial', async function() {
         // when
-        await fillIn('.feedback-panel__dropdown', PICK_CATEGORY_WITH_TUTORIAL);
+        await fillIn('.feedback-panel__dropdown', PICK_SELECT_OPTION_WITH_TUTORIAL);
 
         // then
         expect(findAll(DROPDOWN).length).to.equal(2);
@@ -106,8 +108,8 @@ describe('Integration | Component | feedback-panel', function() {
 
       it('should show the correct feedback action when selecting two different categories', async function() {
         // when
-        await fillIn('.feedback-panel__dropdown', PICK_CATEGORY_WITH_TUTORIAL);
-        await fillIn('.feedback-panel__dropdown', PICK_CATEGORY_WITH_TEXTAREA);
+        await fillIn('.feedback-panel__dropdown', PICK_SELECT_OPTION_WITH_TUTORIAL);
+        await fillIn('.feedback-panel__dropdown', PICK_SELECT_OPTION_WITH_TEXTAREA);
 
         // then
         expect(findAll(DROPDOWN).length).to.equal(1);
@@ -118,14 +120,29 @@ describe('Integration | Component | feedback-panel', function() {
 
       it('should hide the second dropdown when category has fewer levels after a deeper category', async function() {
         // when
-        await fillIn('.feedback-panel__dropdown', PICK_CATEGORY_WITH_NESTED_LEVEL);
-        await fillIn('.feedback-panel__dropdown', PICK_CATEGORY_WITH_TEXTAREA);
+        await fillIn('.feedback-panel__dropdown', PICK_SELECT_OPTION_WITH_NESTED_LEVEL);
+        await fillIn('.feedback-panel__dropdown', PICK_SELECT_OPTION_WITH_TEXTAREA);
 
         // then
         expect(findAll(DROPDOWN).length).to.equal(1);
         expect(find(TUTORIAL_AREA)).to.not.exist;
         expect(find(BUTTON_SEND)).to.exist;
         expect(find(TEXTAREA)).to.exist;
+      });
+
+      it('should display tutorial with textarea with selecting related category and subcategory', async function() {
+        // when
+        const categoryDropdown = find('select[data-test-feedback-category-dropdown]');
+        await fillIn(categoryDropdown, PICK_ANOTHER_SELECT_OPTION_WITH_NESTED_LEVEL);
+
+        const subcategoryDropdown = find('select[data-test-feedback-subcategory-dropdown]');
+        await fillIn(subcategoryDropdown, PICK_SELECT_OPTION_WITH_TEXTAREA_AND_TUTORIAL);
+
+        // then
+        expect(findAll(DROPDOWN).length).to.equal(2);
+        expect(find(TUTORIAL_AREA)).to.exist;
+        expect(find(TEXTAREA)).to.exist;
+        expect(find(BUTTON_SEND)).to.exist;
       });
     });
   });
@@ -237,7 +254,7 @@ describe('Integration | Component | feedback-panel', function() {
 
     it('should display error if "content" is empty', async function() {
       // given
-      await fillIn('.feedback-panel__dropdown', PICK_CATEGORY_WITH_TEXTAREA);
+      await fillIn('.feedback-panel__dropdown', PICK_SELECT_OPTION_WITH_TEXTAREA);
 
       // when
       await click(BUTTON_SEND);
