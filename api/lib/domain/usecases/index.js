b30009f96fc1af1de9bb5516097d6e639c8d7b69
yahya youness <yahyayounes@hotmail.fr>
2021-05-06 13:49:44 +0200

    [API] Add use case to find all organizations tags

--- api/lib/domain/usecases/index.js
+++ api/lib/domain/usecases/index.js
@@ -160,6 +160,7 @@ module.exports = injectDependencies({
   dissociateSchoolingRegistrations: require('./dissociate-schooling-registrations'),
   enrollStudentsToSession: require('./enroll-students-to-session'),
   finalizeSession: require('./finalize-session'),
+  findAllOrganizationsTags: require('./find-all-organizations-tags'),
   findAnswerByAssessment: require('./find-answer-by-assessment'),
   findAnswerByChallengeAndAssessment: require('./find-answer-by-challenge-and-assessment'),
   findAssociationBetweenUserAndSchoolingRegistration: require('./find-association-between-user-and-schooling-registration'),
