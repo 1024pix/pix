faa3b41a70270ac6d0d3502ddb215c0fe75a4bc0
P-Jeremy <jemyplu@gmail.com>
2021-05-10 16:49:20 +0200

    [API] Remove sessionId and certificationOfficerId check in usecase

--- api/lib/domain/usecases/assign-certification-officer-to-jury-session.js
+++ api/lib/domain/usecases/assign-certification-officer-to-jury-session.js
@@ -1,5 +1,3 @@
-const { ObjectValidationError } = require('../errors');
-
 module.exports = async function assignCertificationOfficerToJurySession({
   sessionId,
   certificationOfficerId,
@@ -7,21 +5,9 @@ module.exports = async function assignCertificationOfficerToJurySession({
   finalizedSessionRepository,
   certificationOfficerRepository,
 } = {}) {
-  const integerSessionId = parseInt(sessionId);
-  if (!Number.isFinite(integerSessionId)) {
-    throw new ObjectValidationError(
-      `L'id ${sessionId} n'est pas un id de session valide.`,
-    );
-  }
-  const integerCertificationOfficerId = parseInt(certificationOfficerId);
-  if (!Number.isFinite(integerCertificationOfficerId)) {
-    throw new ObjectValidationError(
-      `L'id ${certificationOfficerId} n'est pas un id de chargé de certification valide.`,
-    );
-  }
 
   const certificationOfficer = await certificationOfficerRepository.get(certificationOfficerId);
-  const finalizedSession = await finalizedSessionRepository.get({ sessionId: integerSessionId });
+  const finalizedSession = await finalizedSessionRepository.get({ sessionId });
 
   finalizedSession.assignCertificationOfficer({ certificationOfficerName: certificationOfficer.getFullName() });
 
