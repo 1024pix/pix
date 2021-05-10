b0212d73a8b4187db06db7afaaa4cd15ebae729d
Andreia Pena <andreia.pena@pix.fr>
2021-05-10 15:47:58 +0200

    [API] Add Serializer into tag controller

--- api/lib/application/tags/tag-controller.js
+++ api/lib/application/tags/tag-controller.js
@@ -1,10 +1,11 @@
-const _ = require('lodash');
+const tagSerializer = require('../../infrastructure/serializers/jsonapi/tag-serializer');
 const usecases = require('../../domain/usecases');
 
 module.exports = {
 
   async findAllOrganizationsTags() {
     const organizationsTags = await usecases.findAllOrganizationsTags();
-    return null;
+    return tagSerializer.serialize(organizationsTags);
   },
+
 };
