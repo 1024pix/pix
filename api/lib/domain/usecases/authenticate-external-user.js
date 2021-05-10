57f44240fa856eb33462cee8738ab0f78cbe1e75
Anne-G <ang.schall@gmail.com>
2021-05-10 17:50:58 +0200

    [API] Add authenticate external user usecase PART-2

--- api/lib/domain/usecases/authenticate-external-user.js
+++ api/lib/domain/usecases/authenticate-external-user.js
@@ -5,15 +5,24 @@ const {
   UserNotFoundError,
   PasswordNotMatching,
   UserShouldChangePasswordError,
+  UnexpectedUserAccount,
+  InvalidExternalUserTokenError,
+  UserAlreadyExistsWithAuthenticationMethodError,
 } = require('../errors');
+
+const AuthenticationMethod = require('../models/AuthenticationMethod');
 
 const TOKEN_SOURCE_FOR_GAR = 'external';
 
 async function authenticateExternalUser({
   username,
   password,
+  externalUserToken,
+  expectedUserId,
   tokenService,
   authenticationService,
+  obfuscationService,
+  authenticationMethodRepository,
   userRepository,
 }) {
 
@@ -24,8 +33,25 @@ async function authenticateExternalUser({
       userRepository,
     });
 
+    if (foundUser.id !== expectedUserId) {
+      const authenticationMethod = await obfuscationService
+        .getUserAuthenticationMethodWithObfuscation(foundUser);
+
+      throw new UnexpectedUserAccount({
+        code: 'UNEXPECTED_USER_ACCOUNT',
+        meta: { value: authenticationMethod.value },
+      });
+    }
+
+    await _addGarAuthenticationMethod({
+      userId: foundUser.id,
+      externalUserToken,
+      tokenService,
+      authenticationMethodRepository,
+      userRepository,
+    });
+
     if (foundUser.shouldChangePassword) {
-      // todo : add gar authentication method
       throw new UserShouldChangePasswordError();
     }
 
@@ -38,6 +64,31 @@ async function authenticateExternalUser({
       throw (error);
     }
   }
+}
+
+async function _addGarAuthenticationMethod({
+  userId,
+  externalUserToken,
+  tokenService,
+  authenticationMethodRepository,
+  userRepository,
+}) {
+  const samlId = tokenService.extractSamlId(externalUserToken);
+  if (!samlId) {
+    throw new InvalidExternalUserTokenError('Une erreur est survenue. Veuillez réessayer de vous connecter depuis le médiacentre.');
+  }
+
+  const foundUserBySamlId = await userRepository.getBySamlId(samlId);
+  if (foundUserBySamlId && foundUserBySamlId.id !== userId) {
+    throw new UserAlreadyExistsWithAuthenticationMethodError();
+  }
+
+  const garAuthenticationMethod = new AuthenticationMethod({
+    identityProvider: AuthenticationMethod.identityProviders.GAR,
+    externalIdentifier: samlId,
+    userId,
+  });
+  await authenticationMethodRepository.create({ authenticationMethod: garAuthenticationMethod });
 }
 
 module.exports = authenticateExternalUser;
