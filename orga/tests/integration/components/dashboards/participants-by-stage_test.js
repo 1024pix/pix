09eae37898bb4c496b35c81a13357ad0b3a82dae
Benjamin Petetot <bpetetot@gmail.com>
2021-05-11 15:58:42 +0200

    [ORGA] Fetch particpations by stage data in component

--- orga/tests/integration/components/dashboards/participants-by-stage_test.js
+++ orga/tests/integration/components/dashboards/participants-by-stage_test.js
@@ -6,70 +6,64 @@ import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | Charts::ParticipantsByStage', function(hooks) {
   setupIntlRenderingTest(hooks);
+  const campaignId = 1;
   let onSelectStage;
 
-  module('For one stage', (hooks) => {
-    hooks.beforeEach(async function() {
-      // given
-      onSelectStage = sinon.stub();
-      this.set('onSelectStage', onSelectStage);
-      this.set('maxStage', 3);
-      this.set('data', [{ id: 100498, stage: 1, value: 5, title: 'title', description: 'description' }]);
+  hooks.beforeEach(async function() {
+    // given
+    onSelectStage = sinon.stub();
+    this.set('onSelectStage', onSelectStage);
+    this.set('campaignId', campaignId);
 
-      // when
-      await render(hbs`<Charts::ParticipantsByStage @data={{data}} @maxStage={{maxStage}} @onSelectStage={{onSelectStage}} />`);
+    const store = this.owner.lookup('service:store');
+    const adapter = store.adapterFor('campaign-stats');
+    const dataFetcher = sinon.stub(adapter, 'getParticipationsByStage');
+
+    dataFetcher.withArgs(campaignId).resolves({
+      data: {
+        attributes: {
+          data: [
+            { id: 100498, value: 0, title: 'title1', description: 'description1' },
+            { id: 100499, value: 5, title: 'title2', description: 'description2' },
+          ],
+        },
+      },
     });
 
-    test('it should display stage stars', async function(assert) {
-      // then
-      assert.dom('[data-test-status=unacquired]').isVisible({ count: 2 });
-      assert.dom('[data-test-status=acquired]').isVisible({ count: 1 });
-    });
-
-    test('it should display participants number', async function(assert) {
-      // then
-      assert.contains('5 participants');
-    });
-
-    test('it should display participants percentage by stages', async function(assert) {
-      // then
-      assert.contains('100 %');
-    });
-
-    test('it should contains tooltip info in dom', async function(assert) {
-      // then
-      assert.contains('title');
-      assert.contains('description');
-    });
-
-    test('it should call onSelectStage when user click on a bar', async function(assert) {
-      // when
-      await click('[role=button]');
-      // then
-      assert.dom('[role=button]').exists();
-      sinon.assert.calledWith(onSelectStage, 100498);
-    });
+    // when
+    await render(hbs`<Charts::ParticipantsByStage @campaignId={{campaignId}} @onSelectStage={{onSelectStage}} />`);
   });
 
-  module('For several stages', (hooks) => {
-    hooks.beforeEach(async function() {
-      // given
-      this.set('maxStage', 2);
-      this.set('data', [
-        { id: 100498, stage: 0, value: 5, title: 'title', description: 'description' },
-        { id: 100499, stage: 1, value: 10, title: 'title', description: 'description' },
-      ]);
+  test('it should display stage stars', async function(assert) {
+    assert.dom('[data-test-status=acquired]').isVisible({ count: 1 });
+    assert.dom('[data-test-status=unacquired]').isVisible({ count: 1 });
+  });
 
-      // when
-      await render(hbs`<Charts::ParticipantsByStage @data={{data}} @maxStage={{maxStage}} />`);
-    });
+  test('it should display participants number', async function(assert) {
+    // then
+    assert.contains('0 participant');
+    assert.contains('5 participants');
+  });
 
-    test('it should display data for each stage', async function(assert) {
-      // then
-      assert.contains('5 participants');
-      assert.contains('10 participants');
-      assert.contains('33 %');
-      assert.contains('67 %');
-    });
+  test('it should display participants percentage by stages', async function(assert) {
+    // then
+    assert.contains('0 %');
+    assert.contains('100 %');
+  });
+
+  test('it should contains tooltip info', async function(assert) {
+    // then
+    assert.contains('title1');
+    assert.contains('description1');
+    assert.contains('title2');
+    assert.contains('description2');
+  });
+
+  test('it should call onSelectStage when user click on a bar', async function(assert) {
+    // when
+    await click('[role=button]');
+    // then
+    assert.dom('[role=button]').exists();
+    sinon.assert.calledWith(onSelectStage, 100498);
   });
 });
