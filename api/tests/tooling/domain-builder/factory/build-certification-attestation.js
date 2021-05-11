3932f32fb3afd2ec279c4cc918d2364df6c73721
Laura Bergoens <laura.bergoens@pix.fr>
2021-05-11 10:12:57 +0200

    feat(api): create CertificationAttestation repo with get method

---
+++ api/tests/tooling/domain-builder/factory/build-certification-attestation.js
@@ -0,0 +1,39 @@
+const CertificationAttestation = require('../../../../lib/domain/models/CertificationAttestation');
+
+module.exports = function buildCertificationAttestation({
+  id = 1,
+  firstName = 'Jean',
+  lastName = 'Bon',
+  birthdate = '1992-06-12',
+  birthplace = 'Paris',
+  isPublished = true,
+  userId = 1,
+  certificationCenter = 'L’univeristé du Pix',
+  date = new Date('2018-12-01T01:02:03Z'),
+  deliveredAt = new Date('2018-10-03T01:02:03Z'),
+  pixScore = 123,
+  maxReachableLevelOnCertificationDate = 5,
+  verificationCode = 'P-SOMECODE',
+  status = 'validated',
+  cleaCertificationStatus = null,
+  resultCompetenceTree = null,
+} = {}) {
+  return new CertificationAttestation({
+    id,
+    firstName,
+    lastName,
+    birthdate,
+    birthplace,
+    isPublished,
+    userId,
+    certificationCenter,
+    date,
+    deliveredAt,
+    pixScore,
+    status,
+    maxReachableLevelOnCertificationDate,
+    verificationCode,
+    resultCompetenceTree,
+    cleaCertificationStatus,
+  });
+};
