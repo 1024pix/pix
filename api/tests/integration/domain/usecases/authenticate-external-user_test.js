d8caeca15bc2a098fe7b9b10d6a62d02dda63146
Pierre TOP (TOPI) <ptop@octo.com>
2021-05-11 15:08:51 +0200

    [API] Fix authentication method already exists

--- api/tests/integration/domain/usecases/authenticate-external-user_test.js
+++
@@ -1,7 +0,0 @@
-const { expect } = require('../../../test-helper');
-
-describe('Integration | UseCases | authenticate-external-user', () => {
-  it('should be true', () => {
-    expect(true).to.be.false;
-  });
-});
