458b9c842cb0511d47961b242db3879100bb7972
Andreia Pena <andreia.pena@pix.fr>
2021-05-19 17:51:00 +0200

    [ADMIN] feedback

--- admin/app/components/organization-all-tags.js
+++ admin/app/components/organization-all-tags.js
@@ -1,27 +1,21 @@
 import Component from '@glimmer/component';
+import map from 'lodash/map';
+import sortBy from 'lodash/sortBy';
 
 export default class OrganizationAllTags extends Component {
 
   get allTags() {
 
-    const organizationTags = this.args.model.organizationTags;
-    const allTags = this.args.model.allTags;
+    const organizationTagsNames = map(this.args.model.organizationTags.toArray(), 'name');
+    const allTags = sortBy(this.args.model.allTags.toArray(), 'name');
 
-    const organizationTagsName = [];
-    organizationTags.forEach((tag) => {
-      organizationTagsName.push(tag.name);
-    });
-
-    const allTagsAssociatedToCurrentOrganization = [];
-    allTags.forEach((tag) => {
-      if (organizationTagsName.includes(tag.name)) {
-        allTagsAssociatedToCurrentOrganization.push({ name: tag.name, isTagAssociateToOrganization: true });
+    return map(allTags, (tag) => {
+      if (organizationTagsNames.includes(tag.name)) {
+        return { name: tag.name, isTagAssignedToOrganization: true };
       }
       else {
-        allTagsAssociatedToCurrentOrganization.push({ name: tag.name, isTagAssociateToOrganization: false });
+        return { name: tag.name, isTagAssignedToOrganization: false };
       }
     });
-
-    return allTagsAssociatedToCurrentOrganization;
   }
 }
