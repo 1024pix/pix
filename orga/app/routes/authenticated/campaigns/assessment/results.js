24b2722edd7877959c2418af573c8cc1b8753c8f
Lise Quesnel <quesnel.lise@gmail.com>
2021-05-20 16:27:11 +0200

    [ORGA] Add titles for assessments details
    Co-authored-by: KaramHaitham <karamflyy@gmail.com>

--- orga/app/routes/authenticated/campaigns/assessment/results.js
+++ orga/app/routes/authenticated/campaigns/assessment/results.js
@@ -4,6 +4,6 @@ export default class ResultsRoute extends Route {
 
   model() {
     const { campaignAssessmentParticipation } = this.modelFor('authenticated.campaigns.assessment');
-    return campaignAssessmentParticipation.campaignAssessmentParticipationResult;
+    return campaignAssessmentParticipation;
   }
 }
