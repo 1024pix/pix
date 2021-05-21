2a6e9d293b0627ec52151649117b9a9e7a3da940
Laura Bergoens <laura.bergoens@pix.fr>
2021-05-21 09:20:26 +0200

    feat(api): add isAccessible method to Session model

--- api/tests/tooling/domain-builder/factory/build-session.js
+++ api/tests/tooling/domain-builder/factory/build-session.js
@@ -1,6 +1,6 @@
 const Session = require('../../../../lib/domain/models/Session');
 
-module.exports = function buildSession({
+const buildSession = function({
   id = 123,
   accessCode = 'ABCD123',
   address = '4 avenue du général perlimpimpim',
@@ -37,3 +37,137 @@ module.exports = function buildSession({
     certificationCandidates,
   });
 };
+
+buildSession.created = function({
+  id,
+  accessCode,
+  address,
+  certificationCenter,
+  certificationCenterId,
+  date,
+  description,
+  examiner,
+  room,
+  time,
+  certificationCandidates,
+} = {}) {
+  return buildSession({
+    id,
+    accessCode,
+    address,
+    certificationCenter,
+    certificationCenterId,
+    date,
+    description,
+    examiner,
+    room,
+    time,
+    certificationCandidates,
+    examinerGlobalComment: null,
+    finalizedAt: null,
+    resultsSentToPrescriberAt: null,
+    publishedAt: null,
+    assignedCertificationOfficerId: null,
+  });
+};
+
+buildSession.finalized = function({
+  id,
+  accessCode,
+  address,
+  certificationCenter,
+  certificationCenterId,
+  date,
+  description,
+  examiner,
+  room,
+  time,
+  certificationCandidates,
+} = {}) {
+  return buildSession({
+    id,
+    accessCode,
+    address,
+    certificationCenter,
+    certificationCenterId,
+    date,
+    description,
+    examiner,
+    room,
+    time,
+    certificationCandidates,
+    examinerGlobalComment: null,
+    finalizedAt: new Date('2020-01-01'),
+    resultsSentToPrescriberAt: null,
+    publishedAt: null,
+    assignedCertificationOfficerId: null,
+  });
+};
+
+buildSession.inProcess = function({
+  id,
+  accessCode,
+  address,
+  certificationCenter,
+  certificationCenterId,
+  date,
+  description,
+  examiner,
+  room,
+  time,
+  certificationCandidates,
+} = {}) {
+  return buildSession({
+    id,
+    accessCode,
+    address,
+    certificationCenter,
+    certificationCenterId,
+    date,
+    description,
+    examiner,
+    room,
+    time,
+    certificationCandidates,
+    examinerGlobalComment: null,
+    finalizedAt: new Date('2020-01-01'),
+    resultsSentToPrescriberAt: null,
+    publishedAt: null,
+    assignedCertificationOfficerId: 123,
+  });
+};
+
+buildSession.processed = function({
+  id,
+  accessCode,
+  address,
+  certificationCenter,
+  certificationCenterId,
+  date,
+  description,
+  examiner,
+  room,
+  time,
+  certificationCandidates,
+} = {}) {
+  return buildSession({
+    id,
+    accessCode,
+    address,
+    certificationCenter,
+    certificationCenterId,
+    date,
+    description,
+    examiner,
+    room,
+    time,
+    certificationCandidates,
+    examinerGlobalComment: null,
+    finalizedAt: new Date('2020-01-01'),
+    resultsSentToPrescriberAt: new Date('2020-01-02'),
+    publishedAt: new Date('2020-01-02'),
+    assignedCertificationOfficerId: 123,
+  });
+};
+
+module.exports = buildSession;
