24235af060fa650d9411d30807da09ad7f0f32bb
Laura Bergoens <laura.bergoens@pix.fr>
2021-05-26 15:38:00 +0200

    feat(certif): replace old list page by new paginated list page

--- certif/app/routes/authenticated/sessions/list.js
+++ certif/app/routes/authenticated/sessions/list.js
@@ -1,10 +1,32 @@
-import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import Route from '@ember/routing/route';
 
-export default class SessionsListRoute extends Route {
+export default class ListRoute extends Route {
+
+  queryParams = {
+    pageNumber: {
+      refreshModel: true,
+    },
+    pageSize: {
+      refreshModel: true,
+    },
+  };
+
   @service currentUser;
 
-  model() {
-    return this.currentUser.currentCertificationCenter.hasMany('sessions').reload();
+  model(params) {
+    return this.store.query('session-summary', {
+      page: {
+        number: params.pageNumber,
+        size: params.pageSize,
+      },
+    }, { reload: true });
+  }
+
+  resetController(controller, isExiting) {
+    if (isExiting) {
+      controller.pageNumber = 1;
+      controller.pageSize = 25;
+    }
   }
 }
