d941a7caa840f34d142c7c94a7f0d1121e0bed5b
Christophe Charles <christophe.charles@octo.com>
2021-05-27 10:38:31 +0200

    [ADMIN] Add organization id in pages titles

--- admin/app/routes/authenticated/organizations/get/all-tags.js
+++ admin/app/routes/authenticated/organizations/get/all-tags.js
@@ -3,9 +3,15 @@ import Route from '@ember/routing/route';
 export default class AllTags extends Route {
 
   async model() {
+    const organizationId = this.paramsFor('authenticated.organizations.get').organization_id;
+
     const organizationTags = this.modelFor('authenticated.organizations.get').tags;
     const allTags = await this.store.findAll('tag');
 
-    return { organizationTags, allTags };
+    return {
+      allTags,
+      organizationId,
+      organizationTags,
+    };
   }
 }
