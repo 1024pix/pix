87d2902df56b8c5ad10fee95f00bd9e143101b94
Andreia Pena <andreia.pena@pix.fr>
2021-05-28 16:08:51 +0200

    [API][ST] Use httpTestServer instead of hapi.server() and delete beforeEach in tests

--- api/tests/unit/application/user-tutorials/index_test.js
+++ api/tests/unit/application/user-tutorials/index_test.js
@@ -1,33 +1,26 @@
-const { expect, sinon } = require('../../../test-helper');
-const Hapi = require('@hapi/hapi');
+const {
+  expect,
+  HttpTestServer,
+  sinon,
+} = require('../../../test-helper');
 const userTutorialsController = require('../../../../lib/application/user-tutorials/user-tutorials-controller');
-
-let server;
-
-function startServer() {
-  server = Hapi.server();
-  return server.register(require('../../../../lib/application/user-tutorials'));
-}
+const moduleUnderTest = require('../../../../lib/application/user-tutorials');
 
 describe('Unit | Router | user-tutorials-router', () => {
 
   describe('PUT /api/users/tutorials/{tutorialId}', () => {
 
-    beforeEach(() => {
-      sinon.stub(userTutorialsController, 'add').
-        callsFake((request, h) => h.response().code(204));
-      startServer();
-    });
-
     it('should exist', async () => {
       // given
-      const options = {
-        method: 'PUT',
-        url: '/api/users/tutorials/{tutorialId}',
-      };
+      sinon.stub(userTutorialsController, 'add').callsFake((request, h) => h.response('ok').code(204));
+      const httpTestServer = new HttpTestServer();
+      await httpTestServer.register(moduleUnderTest);
+
+      const method = 'PUT';
+      const url = '/api/users/tutorials/{tutorialId}';
 
       // when
-      const response = await server.inject(options);
+      const response = await httpTestServer.request(method, url);
 
       // then
       expect(userTutorialsController.add).have.been.called;
@@ -37,21 +30,17 @@ describe('Unit | Router | user-tutorials-router', () => {
 
   describe('GET /api/users/tutorials', () => {
 
-    beforeEach(() => {
-      sinon.stub(userTutorialsController, 'find').
-        callsFake((request, h) => h.response().code(200));
-      startServer();
-    });
-
     it('should exist', async () => {
       // given
-      const options = {
-        method: 'GET',
-        url: '/api/users/tutorials',
-      };
+      sinon.stub(userTutorialsController, 'find').callsFake((request, h) => h.response('ok').code(200));
+      const httpTestServer = new HttpTestServer();
+      await httpTestServer.register(moduleUnderTest);
+
+      const method = 'GET';
+      const url = '/api/users/tutorials';
 
       // when
-      const response = await server.inject(options);
+      const response = await httpTestServer.request(method, url);
 
       // then
       expect(userTutorialsController.find).have.been.called;
@@ -61,21 +50,17 @@ describe('Unit | Router | user-tutorials-router', () => {
 
   describe('DELETE /api/users/tutorials/{tutorialId}', () => {
 
-    beforeEach(() => {
-      sinon.stub(userTutorialsController, 'removeFromUser').
-        callsFake((request, h) => h.response().code(204));
-      startServer();
-    });
-
     it('should exist', async () => {
       // given
-      const options = {
-        method: 'DELETE',
-        url: '/api/users/tutorials/tutorialId',
-      };
+      sinon.stub(userTutorialsController, 'removeFromUser').callsFake((request, h) => h.response().code(204));
+      const httpTestServer = new HttpTestServer();
+      await httpTestServer.register(moduleUnderTest);
+
+      const method = 'DELETE';
+      const url = '/api/users/tutorials/tutorialId';
 
       // when
-      const response = await server.inject(options);
+      const response = await httpTestServer.request(method, url);
 
       // then
       expect(userTutorialsController.removeFromUser).have.been.called;
