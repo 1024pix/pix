f3cab1e5113c98d02b8da96e5b821bb7543b6a5d
Andreia Pena <andreia.pena@pix.fr>
2021-06-01 16:13:58 +0200

    [API] Add admin/memberships/id route

--- api/lib/application/memberships/index.js
+++ api/lib/application/memberships/index.js
@@ -57,6 +57,34 @@ exports.register = async function(server) {
       },
     },
     {
+      method: 'PATCH',
+      path: '/api/admin/memberships/{id}',
+      config: {
+        pre: [{
+          method: securityPreHandlers.checkUserHasRolePixMaster,
+          assign: 'hasRolePixMaster',
+        }],
+        validate: {
+          params: Joi.object({
+            id: identifiersType.membershipId,
+          }),
+        },
+        handler: membershipController.update,
+        description: 'Update organization role by admin for a organization members',
+        notes: [
+          '- **Cette route est restreinte aux utilisateurs authentifiés en tant que Pix Master**\n' +
+          '- Elle permet de modifier le rôle d\'un membre de l\'organisation',
+        ],
+        plugins: {
+          'hapi-swagger': {
+            payloadType: 'form',
+            order: 2,
+          },
+        },
+        tags: ['api', 'memberships'],
+      },
+    },
+    {
       method: 'POST',
       path: '/api/memberships/{id}/disable',
       config: {
---
+++ api/tests/unit/application/memberships/index_test.js
@@ -0,0 +1,51 @@
+const {
+  expect,
+  HttpTestServer,
+  sinon,
+} = require('../../../test-helper');
+
+const securityPreHandlers = require('../../../../lib/application/security-pre-handlers');
+
+const moduleUnderTest = require('../../../../lib/application/memberships');
+
+const membershipController = require('../../../../lib/application/memberships/membership-controller');
+
+describe('Unit | Router | membership-router', () => {
+
+  describe('PATCH /api/admin/memberships/{id}', () => {
+
+    it('should return 200 if user is Pix Admin', async () => {
+      // given
+      sinon.stub(securityPreHandlers, 'checkUserHasRolePixMaster').callsFake((request, h) => h.response(true));
+      sinon.stub(membershipController, 'update').callsFake((request, h) => h.response().code(200));
+
+      const httpTestServer = new HttpTestServer();
+      httpTestServer.register(moduleUnderTest);
+      const id = 123;
+
+      // when
+      const response = await httpTestServer.request('PATCH', `/api/admin/memberships/${id}`);
+
+      // then
+      expect(response.statusCode).to.equal(200);
+      expect(membershipController.update).to.have.been.called;
+    });
+
+    it('should return 403 if user is not Pix Admin', async () => {
+      // given
+      sinon.stub(securityPreHandlers, 'checkUserHasRolePixMaster').callsFake((request, h) => h.response().code(403).takeover());
+      sinon.stub(membershipController, 'update');
+
+      const httpTestServer = new HttpTestServer();
+      httpTestServer.register(moduleUnderTest);
+      const id = 123;
+
+      // when
+      const response = await httpTestServer.request('PATCH', `/api/admin/memberships/${id}`);
+
+      // then
+      expect(response.statusCode).to.equal(403);
+      expect(membershipController.update).to.have.not.been.called;
+    });
+  });
+});
