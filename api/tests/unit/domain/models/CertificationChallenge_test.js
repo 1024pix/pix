2fce8e6465e99dd58981af85cba8811a84d086f4
Clément Bourgoin <clement.bourgoin@pix.fr>
2021-06-01 17:32:36 +0200

    clean(api): remove unused methods in CertificationChallenge model

--- api/tests/unit/domain/models/CertificationChallenge_test.js
+++ api/tests/unit/domain/models/CertificationChallenge_test.js
@@ -1,57 +1,7 @@
-const { expect, domainBuilder } = require('../../../test-helper');
+const { expect } = require('../../../test-helper');
 const CertificationChallenge = require('../../../../lib/domain/models/CertificationChallenge');
 
 describe('Unit | Domain | Models | CertificationChallenge', () => {
-
-  describe('#neutralize', () => {
-
-    it('should neutralize a non neutralized certification challenge', () => {
-      // given
-      const certificationChallenge = domainBuilder.buildCertificationChallenge({ isNeutralized: false });
-
-      // when
-      certificationChallenge.neutralize();
-
-      // then
-      expect(certificationChallenge.isNeutralized).to.be.true;
-    });
-
-    it('should leave a neutralized certification challenge if it was neutralized already', () => {
-      // given
-      const certificationChallenge = domainBuilder.buildCertificationChallenge({ isNeutralized: true });
-
-      // when
-      certificationChallenge.neutralize();
-
-      // then
-      expect(certificationChallenge.isNeutralized).to.be.true;
-    });
-  });
-
-  describe('#deneutralize', () => {
-
-    it('should deneutralize a neutralized certification challenge', () => {
-      // given
-      const certificationChallenge = domainBuilder.buildCertificationChallenge({ isNeutralized: true });
-
-      // when
-      certificationChallenge.deneutralize();
-
-      // then
-      expect(certificationChallenge.isNeutralized).to.be.false;
-    });
-
-    it('should leave a deneutralized certification challenge if it was deneutralized already', () => {
-      // given
-      const certificationChallenge = domainBuilder.buildCertificationChallenge({ isNeutralized: false });
-
-      // when
-      certificationChallenge.deneutralize();
-
-      // then
-      expect(certificationChallenge.isNeutralized).to.be.false;
-    });
-  });
 
   describe('#static createForPixCertification', () => {
 
@@ -116,31 +66,6 @@ describe('Unit | Domain | Models | CertificationChallenge', () => {
         certifiableBadgeKey,
       });
       expect(certificationChallenge).to.deep.equal(expectedCertificationChallenge);
-    });
-  });
-
-  describe('#isPixPlus', () => {
-
-    it('return true when challenge was picked for a pix plus certification', () => {
-      // given
-      const certificationChallenge = domainBuilder.buildCertificationChallenge({ certifiableBadgeKey: 'someValue' });
-
-      // when
-      const isPixPlus = certificationChallenge.isPixPlus();
-
-      // then
-      expect(isPixPlus).to.be.true;
-    });
-
-    it('return false when challenge was picked for a regular pix certification', () => {
-      // given
-      const certificationChallenge = domainBuilder.buildCertificationChallenge({ certifiableBadgeKey: null });
-
-      // when
-      const isPixPlus = certificationChallenge.isPixPlus();
-
-      // then
-      expect(isPixPlus).to.be.false;
     });
   });
 });
