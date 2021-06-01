6fc4b095403c9a5c42b741be6815a9cc1630821e
Laura Bergoens <laura.bergoens@pix.fr>
2021-06-01 08:28:12 +0200

    feat(api): create model CertificationEligibility

---
+++ api/lib/domain/read-models/CertificationEligibility.js
@@ -0,0 +1,11 @@
+class CertificationEligibility {
+  constructor({
+    id,
+    pixCertificationEligible,
+  }) {
+    this.id = id;
+    this.pixCertificationEligible = pixCertificationEligible;
+  }
+}
+
+module.exports = CertificationEligibility;
