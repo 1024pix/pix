de753c33a47df26243dfe893237426d4bd9a60b6
Jonas Grillères <grilleres.j@gmail.com>
2021-06-02 17:58:02 +0200

    feat: create usecase getChallengeAnswerForPixButton
    Co-authored-by: Céline Ung <celine.ung@pix.fr>
    Co-authored-by: Alexandre Coin <alexandre.coin@pix.fr>

---
+++ api/lib/domain/usecases/get-challenge-answer-for-pix-button.js
@@ -0,0 +1,15 @@
+const { NotFoundError } = require('../errors');
+
+module.exports = async function getChallengeAnswerForPixButton({
+  assessmentId,
+  assessmentRepository,
+  challengeRepository,
+}) {
+  const assessment = await assessmentRepository.get(assessmentId);
+  if (!assessment) {
+    throw new NotFoundError(`Assessment not found for ID ${assessmentId}`);
+  }
+
+  const lastChallengeId = assessment.lastChallengeId;
+  return challengeRepository.getSolution(lastChallengeId);
+};
---
+++ api/tests/unit/domain/usecases/get-challenge-answer-for-pix-button_test.js
@@ -0,0 +1,31 @@
+const { expect, sinon, domainBuilder } = require('../../../test-helper');
+const getChallengeAnswerForPixButton = require('../../../../lib/domain/usecases/get-challenge-answer-for-pix-button');
+const assessmentRepository = require('../../../../lib/infrastructure/repositories/assessment-repository');
+const challengeRepository = require('../../../../lib/infrastructure/repositories/challenge-repository');
+
+describe('Unit | UseCase | get-challenge-answer-for-pix-button', () => {
+
+  let assessment;
+  const challengeId = 1;
+  const solution = 'answer \n answer';
+  const lastChallengeId = 'last challenge id';
+
+  beforeEach(() => {
+    assessment = domainBuilder.buildAssessment({
+      lastChallengeId,
+    });
+
+    sinon.stub(assessmentRepository, 'get');
+    sinon.stub(challengeRepository, 'getSolution');
+  });
+
+  it('should return the solution of the last challenge from the given assessment', async () => {
+    assessmentRepository.get.resolves(assessment);
+    challengeRepository.getSolution.resolves(solution);
+
+    const result = await getChallengeAnswerForPixButton({ challengeId, assessmentRepository, challengeRepository });
+
+    expect(result).to.equal(solution);
+  });
+
+});
