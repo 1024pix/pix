623ac31c23c1c78350ef9779f92bbabae210f6ea
Benjamin Petetot <bpetetot@gmail.com>
2021-06-03 10:50:11 +0200

    [ORGA] Add and setup Chart.js lib

--- orga/ember-cli-build.js
+++ orga/ember-cli-build.js
@@ -29,6 +29,11 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('node_modules/chart.js/dist/chart.js', {
+    using: [
+      { transformation: 'amd', as: 'chart.js' },
+    ],
+  });
 
   return app.toTree();
 };
