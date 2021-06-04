278da27cff74a9ee12c326a57bcc8605c8976b89
Benjamin Petetot <bpetetot@gmail.com>
2021-06-04 16:34:43 +0200

    [ORGA] Refactor participation by status chart to manage profile collection

--- orga/app/components/charts/participants-by-status.js
+++ orga/app/components/charts/participants-by-status.js
@@ -1,40 +1,46 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
+import sumBy from 'lodash/sumBy';
 import { TOOLTIP_CONFIG } from './chart';
 
 export default class ParticipantsByStatus extends Component {
   @service store;
   @service intl;
 
-  @tracked data = {};
+  @tracked datasets = [];
   @tracked total = 0;
   @tracked loading = true;
 
   constructor(...args) {
     super(...args);
-    const { campaignId } = this.args;
+    const { campaignId, isTypeAssessment } = this.args;
 
     const adapter = this.store.adapterFor('campaign-stats');
-    adapter.getParticipationsByStatus(campaignId).then((response) => {
-      const { started, completed, shared } = response.data.attributes;
 
-      this.total = started + completed + shared;
-      this.data = {
-        labels: [
-          this.intl.t('charts.participants-by-status.labels.started'),
-          this.intl.t('charts.participants-by-status.labels.completed'),
-          this.intl.t('charts.participants-by-status.labels.shared'),
-        ],
-        datasets: [
-          {
-            data: [started, completed, shared],
-            backgroundColor: ['#0090d7', '#502ec4', '#67bb0b'],
-          },
-        ],
-      };
+    adapter.getParticipationsByStatus(campaignId).then((response) => {
+      const entries = Object.entries(response.data.attributes);
+      this.total = sumBy(entries, ([_, count]) => count);
+
+      this.datasets = entries.map(([key, count]) => {
+        const datasetLabels = this.getDatasetLabels(key, count, isTypeAssessment);
+        return { key, count, ...datasetLabels };
+      });
+
       this.loading = false;
     });
+  }
+
+  get data() {
+    return {
+      labels: this.datasets.map((data) => data.tooltip),
+      datasets: [
+        {
+          data: this.datasets.map((data) => data.count),
+          backgroundColor: this.datasets.map((data) => data.color),
+        },
+      ],
+    };
   }
 
   get options() {
@@ -45,32 +51,64 @@ export default class ParticipantsByStatus extends Component {
         legend: false,
         tooltip: {
           ...TOOLTIP_CONFIG,
-          callbacks: {
-            label: (data) => this.getTooltipLabel(data),
-          },
+          callbacks: { label: (data) => data.label },
         },
       },
       borderWidth: 0,
     };
   }
 
-  get started() {
-    if (!this.data.datasets) return 0;
-    return this.data.datasets[0].data[0];
-  }
+  getDatasetLabels(key, count, isTypeAssessment) {
+    const datasetLabels = isTypeAssessment ? LABELS_ASSESSMENT[key] : LABELS_PROFILE_COLLECTIONS[key];
+    const percentage = this.total !== 0 ? count / this.total : 0;
 
-  get completed() {
-    if (!this.data.datasets) return 0;
-    return this.data.datasets[0].data[1];
-  }
-
-  get shared() {
-    if (!this.data.datasets) return 0;
-    return this.data.datasets[0].data[2];
-  }
-
-  getTooltipLabel({ label, raw }) {
-    const value = this.total !== 0 ? raw / this.total : 0;
-    return this.intl.t('charts.participants-by-status.tooltip', { label, value });
+    return {
+      tooltip: this.intl.t(datasetLabels.tooltip, { percentage }),
+      legend: this.intl.t(datasetLabels.legend, { count }),
+      a11y: this.intl.t(datasetLabels.a11y, { count }),
+      color: datasetLabels.color,
+    };
   }
 }
+
+const LABELS_ASSESSMENT = {
+  started: {
+    tooltip: 'charts.participants-by-status.labels-tooltip.started',
+    legend: 'charts.participants-by-status.labels-legend.started',
+    a11y: 'charts.participants-by-status.labels-a11y.started',
+    color: '#FFBE00',
+  },
+  completed: {
+    tooltip: 'charts.participants-by-status.labels-tooltip.completed',
+    legend: 'charts.participants-by-status.labels-legend.completed',
+    a11y: 'charts.participants-by-status.labels-a11y.completed',
+    color: '#8a49ff',
+  },
+  shared: {
+    tooltip: 'charts.participants-by-status.labels-tooltip.shared',
+    legend: 'charts.participants-by-status.labels-legend.shared',
+    a11y: 'charts.participants-by-status.labels-a11y.shared',
+    color: '#038a25',
+  },
+};
+
+const LABELS_PROFILE_COLLECTIONS = {
+  started: {
+    tooltip: 'charts.participants-by-status.labels-tooltip.started',
+    legend: 'charts.participants-by-status.labels-legend.started',
+    a11y: 'charts.participants-by-status.labels-a11y.started',
+    color: '#FFBE00',
+  },
+  completed: {
+    tooltip: 'charts.participants-by-status.labels-tooltip.completed',
+    legend: 'charts.participants-by-status.labels-legend.completed',
+    a11y: 'charts.participants-by-status.labels-a11y.completed',
+    color: '#8a49ff',
+  },
+  shared: {
+    tooltip: 'charts.participants-by-status.labels-tooltip.shared-profile',
+    legend: 'charts.participants-by-status.labels-legend.shared-profile',
+    a11y: 'charts.participants-by-status.labels-a11y.shared-profile',
+    color: '#038a25',
+  },
+};
