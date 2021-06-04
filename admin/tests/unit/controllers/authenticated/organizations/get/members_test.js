8d913b6ed378f2bbf26be6068b106ee03d0c548f
Anne-G <ang.schall@gmail.com>
2021-06-04 15:49:03 +0200

    [ADMIN][ST] Add given when then in tests

--- admin/tests/unit/controllers/authenticated/organizations/get/members_test.js
+++ admin/tests/unit/controllers/authenticated/organizations/get/members_test.js
@@ -14,6 +14,7 @@ module('Unit | Controller | authenticated/organizations/get/members', function(h
   module('#createOrganizationInvitation', function() {
 
     test('it should create an organization-invitation if the email is valid', function(assert) {
+      // given
       const saveStub = sinon.stub().resolves();
       const createRecordStub = sinon.stub().returns({
         save: saveStub,
@@ -23,33 +24,44 @@ module('Unit | Controller | authenticated/organizations/get/members', function(h
 
       controller.userEmailToInvite = 'test@example.net';
 
+      // when
       controller.createOrganizationInvitation();
 
+      // then
       assert.ok(createRecordStub.calledWith('organization-invitation', { email: 'test@example.net' }));
       assert.equal(saveStub.callCount, 1);
     });
 
     test('it should fail if userEmailToInvite is undefined', function(assert) {
+      // given
       controller.userEmailToInvite = undefined;
 
+      // when
       controller.createOrganizationInvitation();
 
+      // then
       assert.equal(controller.userEmailToInviteError, 'Ce champ est requis.');
     });
 
     test('it should fail if userEmailToInvite is empty', function(assert) {
+      // given
       controller.userEmailToInvite = '';
 
+      // when
       controller.createOrganizationInvitation();
 
+      // then
       assert.equal(controller.userEmailToInviteError, 'Ce champ est requis.');
     });
 
     test('it should fail if userEmailToInvite is not a valid email address', function(assert) {
+      // given
       controller.userEmailToInvite = 'not_valid_email';
 
+      // when
       controller.createOrganizationInvitation();
 
+      // then
       assert.equal(controller.userEmailToInviteError, 'L\'adresse email saisie n\'est pas valide.');
     });
   });
