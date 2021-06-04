0fbd0e9c3c86054a9d4c2e10c11eed213b038a15
Etienne GIROT <egirot@octo.com>
2021-06-04 11:11:25 +0200

    [API] add neutralize if image strategy

--- api/lib/domain/models/CertificationIssueReportResolutionStrategies.js
+++ api/lib/domain/models/CertificationIssueReportResolutionStrategies.js
@@ -4,7 +4,26 @@ module.exports = {
   NEUTRALIZE_IF_EMBED: async () => {
     return CertificationIssueReportResolutionAttempt.failure();
   },
-  NEUTRALIZE_IF_IMAGE: async () => {
+  NEUTRALIZE_IF_IMAGE: async ({ certificationIssueReport, certificationAssessment, certificationIssueReportRepository, challengeRepository }) => {
+    const questionNumber = certificationIssueReport.questionNumber;
+    const recId = certificationAssessment.getChallengeRecIdByQuestionNumber(questionNumber);
+
+    if (!recId) {
+      return CertificationIssueReportResolutionAttempt.failure();
+    }
+
+    const challenge = await challengeRepository.get(recId);
+
+    if (!challenge.illustrationUrl) {
+      return CertificationIssueReportResolutionAttempt.failure();
+    }
+
+    const neutralizationAttempt = certificationAssessment.neutralizeChallengeByNumberIfKoOrSkipped(questionNumber);
+    if (neutralizationAttempt.hasSucceeded()) {
+      certificationIssueReport.resolve('Cette question a été neutralisée automatiquement');
+      await certificationIssueReportRepository.save(certificationIssueReport);
+      return CertificationIssueReportResolutionAttempt.succeeded();
+    }
     return CertificationIssueReportResolutionAttempt.failure();
   },
   NEUTRALIZE_WITHOUT_CHECKING: async ({ certificationIssueReport, certificationAssessment, certificationIssueReportRepository }) => {
