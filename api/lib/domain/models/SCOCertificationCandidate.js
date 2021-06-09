d66fdeaca58d09dcdc76f6736709482210d84d53
Laura Bergoens <laura.bergoens@pix.fr>
2021-06-09 08:55:36 +0200

    feat(api): add attributes sex and birthINSEECode to SCOCertificationCandidate model

--- api/lib/domain/models/SCOCertificationCandidate.js
+++ api/lib/domain/models/SCOCertificationCandidate.js
@@ -6,24 +6,29 @@ const scoCertificationCandidateValidationJoiSchema = Joi.object({
   firstName: Joi.string().required().empty(null),
   lastName: Joi.string().required().empty(null),
   birthdate: Joi.date().format('YYYY-MM-DD').greater('1900-01-01').required().empty(null),
+  birthINSEECode: Joi.string().allow(null).optional(),
+  sex: Joi.string().allow(null).optional(),
   sessionId: Joi.number().required().empty(null),
   schoolingRegistrationId: Joi.number().required().empty(null),
 });
 
 class SCOCertificationCandidate {
-  constructor(
-    {
-      id,
-      firstName,
-      lastName,
-      birthdate,
-      sessionId,
-      schoolingRegistrationId,
-    } = {}) {
+  constructor({
+    id,
+    firstName,
+    lastName,
+    birthdate,
+    birthINSEECode,
+    sex,
+    sessionId,
+    schoolingRegistrationId,
+  } = {}) {
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
     this.birthdate = birthdate;
+    this.birthINSEECode = birthINSEECode;
+    this.sex = sex;
     this.sessionId = sessionId;
     this.schoolingRegistrationId = schoolingRegistrationId;
     this.validate();
