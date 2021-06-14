eaf9ba3077d5891940013cb71688b2bc480a3d57
Mathieu Gilet <mathieu.gilet@zenika.com>
2021-06-14 12:02:30 +0200

    feat(api) Add import-certification-cpf-cities script

---
+++ api/scripts/import-certification-cpf-cities.js
@@ -0,0 +1,84 @@
+/* eslint-disable no-console */
+// Usage: node import-certification-cpf-cities path/file.csv
+// File downloaded from https://public.opendatasoft.com/explore/dataset/correspondance-code-insee-code-postal/export/?flg=fr
+
+'use strict';
+require('dotenv').config();
+const { parseCsv } = require('./helpers/csvHelpers');
+const { knex } = require('../lib/infrastructure/bookshelf');
+const { uniqBy } = require('lodash');
+
+const CITY_COLUMN_NAME = 'Nom_commune';
+const POSTAL_CODE_COLUMN_NAME = 'Code_postal';
+const INSEE_CODE_COLUMN_NAME = 'Code_commune_INSEE';
+const ALTERNATE_CITY_COLUMN_NAME = 'Ligne_5';
+
+function buildCities({ csvData }) {
+  const citiesWithAlternates = csvData.flatMap((data) => {
+    const result = [];
+    result.push({
+      name: data[CITY_COLUMN_NAME],
+      postalCode: data[POSTAL_CODE_COLUMN_NAME],
+      INSEECode: data[INSEE_CODE_COLUMN_NAME],
+      isActualName: true,
+    });
+
+    if (data[ALTERNATE_CITY_COLUMN_NAME]) {
+      result.push({
+        name: data[ALTERNATE_CITY_COLUMN_NAME],
+        postalCode: data[POSTAL_CODE_COLUMN_NAME],
+        INSEECode: data[INSEE_CODE_COLUMN_NAME],
+        isActualName: false,
+      });
+    }
+    return result;
+  });
+  return uniqBy(citiesWithAlternates, 'name');
+}
+
+async function main() {
+  console.log('Starting script import-certification-cpf-cities');
+
+  let trx;
+  try {
+    const filePath = process.argv[2];
+
+    console.log('Reading and parsing csv data file... ');
+    const csvData = await parseCsv(filePath, { header: true, delimiter: ';', skipEmptyLines: true });
+    console.log('ok');
+
+    console.log('Retrieving postal code, INSEE code and city name... ');
+    const cities = buildCities({ csvData });
+    console.log('ok');
+
+    console.log('Inserting cities in database... ');
+    trx = await knex.transaction();
+    await trx('certification-cpf-cities').del();
+    await trx.batchInsert('certification-cpf-cities', cities);
+    trx.commit();
+    console.log('ok');
+
+    console.log('\nDone.');
+
+  } catch (error) {
+    if (trx) {
+      trx.rollback();
+    }
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+if (require.main === module) {
+  main().then(
+    () => process.exit(0),
+    (err) => {
+      console.error(err);
+      process.exit(1);
+    },
+  );
+}
+
+module.exports = {
+  buildCities,
+};
