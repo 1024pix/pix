1bb75112d8b41a6a68d53d9ed46488bd4028a05d
Andreia Pena <andreia.pena@pix.fr>
2021-06-16 22:46:56 +0200

    [APP] Add student information validation

---
+++ mon-pix/app/components/recover-account-student-information-form.js
@@ -0,0 +1,129 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import isEmpty from 'lodash/isEmpty';
+
+const INE_REGEX = /^[0-9]{9}[a-zA-Z]{2}$/;
+const INA_REGEX = /^[0-9]{10}[a-zA-Z]{1}$/;
+
+const STATUS_MAP = {
+  defaultStatus: 'default',
+  errorStatus: 'error',
+  successStatus: 'success',
+};
+
+const ERROR_INPUT_MESSAGE_MAP = {
+  invalidIneInaFormat: 'pages.recover-account-after-leaving-sco.student-information.errors.invalid-ine-ina-format',
+  emptyIneIna: 'pages.recover-account-after-leaving-sco.student-information.errors.empty-ine-ina',
+  emptyLastName: 'pages.recover-account-after-leaving-sco.student-information.errors.empty-last-name',
+  emptyFirstName: 'pages.recover-account-after-leaving-sco.student-information.errors.empty-first-name',
+};
+
+class IneInaValidation {
+  @tracked status = STATUS_MAP['defaultStatus'];
+  @tracked message = null;
+}
+
+class LastNameValidation {
+  @tracked status = STATUS_MAP['defaultStatus'];
+  @tracked message = null;
+}
+
+class FirstNameValidation {
+  @tracked status = STATUS_MAP['defaultStatus'];
+  @tracked message = null;
+}
+
+export default class RecoverAccountStudentInformationFormComponent extends Component {
+
+  @service store;
+  @service intl;
+
+  @tracked ineInaValidation = new IneInaValidation();
+  @tracked lastNameValidation = new LastNameValidation();
+  @tracked firstNameValidation = new FirstNameValidation();
+
+  ineIna = '';
+  lastName = '';
+  firstName = '';
+  dayOfBirth = '';
+  monthOfBirth = '';
+  yearOfBirth = '';
+
+  @action validateIneIna() {
+    this.ineIna = this.ineIna.trim();
+    if (isEmpty(this.ineIna)) {
+      this.ineInaValidation.status = STATUS_MAP['errorStatus'];
+      this.ineInaValidation.message = this.intl.t(ERROR_INPUT_MESSAGE_MAP['emptyIneIna']);
+      return;
+    }
+
+    const isValidIne = INE_REGEX.test(this.ineIna);
+    const isValidIna = INA_REGEX.test(this.ineIna);
+    if (!isValidIne && !isValidIna) {
+      this.ineInaValidation.status = STATUS_MAP['errorStatus'];
+      this.ineInaValidation.message = this.intl.t(ERROR_INPUT_MESSAGE_MAP['invalidIneInaFormat']);
+      return;
+    }
+
+    this.ineInaValidation.status = STATUS_MAP['successStatus'];
+    this.ineInaValidation.message = null;
+  }
+
+  @action validateLastName() {
+    this.lastName = this.lastName.trim();
+    if (isEmpty(this.lastName)) {
+      this.lastNameValidation.status = STATUS_MAP['errorStatus'];
+      this.lastNameValidation.message = this.intl.t(ERROR_INPUT_MESSAGE_MAP['emptyLastName']);
+      return;
+    }
+
+    this.lastNameValidation.status = STATUS_MAP['successStatus'];
+    this.lastNameValidation.message = null;
+  }
+
+  @action validateFirstName() {
+    this.firstName = this.firstName.trim();
+    if (isEmpty(this.firstName)) {
+      this.firstNameValidation.status = STATUS_MAP['errorStatus'];
+      this.firstNameValidation.message = this.intl.t(ERROR_INPUT_MESSAGE_MAP['emptyFirstName']);
+      return;
+    }
+
+    this.firstNameValidation.status = STATUS_MAP['successStatus'];
+    this.firstNameValidation.message = null;
+  }
+
+  @action
+  async submitStudentInformationForm(event) {
+    event.preventDefault();
+    const birthdate = [this.yearOfBirth, this.monthOfBirth, this.dayOfBirth].join('-');
+
+    await this.store.queryRecord('user', {
+      ineIna: this.ineIna,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      birthdate,
+    });
+
+  }
+
+  @action
+  handleDayInputChange(event) {
+    const { value } = event.target;
+
+    if (value.length === 2) {
+      document.getElementById('monthOfBirth').focus();
+    }
+  }
+
+  @action
+  handleMonthInputChange(event) {
+    const { value } = event.target;
+
+    if (value.length === 2) {
+      document.getElementById('yearOfBirth').focus();
+    }
+  }
+}
--- mon-pix/tests/unit/routes/recover-account-after-leaving-sco-test.js
+++ mon-pix/tests/unit/routes/recover-account-after-leaving-sco-test.js
@@ -2,11 +2,11 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupTest } from 'ember-mocha';
 
-describe('Unit | Route | recovery-account-after-leaving-sco', function() {
+describe('Unit | Route | recover-account-after-leaving-sco', function() {
   setupTest();
 
   it('exists', function() {
-    const route = this.owner.lookup('route:recovery-account-after-leaving-sco');
+    const route = this.owner.lookup('route:recover-account-after-leaving-sco');
     expect(route).to.be.ok;
   });
 });
