146a8a172bd378a8a93b98b75612db269881bb9d
Pierre TOP (TOPI) <ptop@octo.com>
2021-06-17 11:49:19 +0200

    [APP] Remove unused feature toggle

--- mon-pix/tests/unit/services/feature-toggles_test.js
+++ mon-pix/tests/unit/services/feature-toggles_test.js
@@ -13,7 +13,6 @@ describe('Unit | Service | feature-toggles', function() {
   describe('feature toggles are loaded', function() {
 
     const featureToggles = Object.create({
-      certifPrescriptionSco: false,
       isPoleEmploiEnabled: false,
     });
 
