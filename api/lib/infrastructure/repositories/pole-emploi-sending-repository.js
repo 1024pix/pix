5d3aeab34dd7ba06f9a3ef5dcd4551eca7bb6770
yaelle <ymarsiac@octo.com>
2021-06-18 15:05:16 +0200

    [API] add method to repository

--- api/lib/infrastructure/repositories/pole-emploi-sending-repository.js
+++ api/lib/infrastructure/repositories/pole-emploi-sending-repository.js
@@ -1,7 +1,31 @@
+const settings = require('../../../lib/config');
+const Bookshelf = require('../bookshelf');
 const BookshelfPoleEmploiSending = require('../orm-models/PoleEmploiSending');
+const _ = require('lodash');
 
 module.exports = {
   create({ poleEmploiSending }) {
     return new BookshelfPoleEmploiSending(poleEmploiSending).save();
+  },
+  async get() {
+    const POLE_EMPLOI_SENDINGS_LIMIT = require('../../../lib/config').poleEmploi.poleEmploiSendingsLimit;
+    const IDENTITY_PROVIDER_POLE_EMPLOI = settings.poleEmploi.poleEmploiIdentityProvider;
+    const sendings = [];
+
+    const rawSendings = await Bookshelf.knex('pole-emploi-sendings')
+      .select('pole-emploi-sendings.id AS idEnvoi', 'pole-emploi-sendings.createdAt AS dateEnvoi', 'pole-emploi-sendings.payload AS resultat', 'authentication-methods.externalIdentifier AS idPoleEmploi')
+      .join('campaign-participations', 'campaign-participations.id', 'pole-emploi-sendings.campaignParticipationId')
+      .join('authentication-methods', 'authentication-methods.userId', 'campaign-participations.userId')
+      .where('authentication-methods.identityProvider', IDENTITY_PROVIDER_POLE_EMPLOI)
+      .orderBy([{ column: 'pole-emploi-sendings.createdAt', order: 'desc' }, { column: 'pole-emploi-sendings.id', order: 'desc' }])
+      .limit(POLE_EMPLOI_SENDINGS_LIMIT);
+
+    for (const rawSending of rawSendings) {
+      if (rawSending.idPoleEmploi) {
+        rawSending.resultat.individu['idPoleEmploi'] = rawSending.idPoleEmploi;
+      }
+      sendings.push(_.omit(rawSending, 'idPoleEmploi'));
+    }
+    return sendings;
   },
 };
