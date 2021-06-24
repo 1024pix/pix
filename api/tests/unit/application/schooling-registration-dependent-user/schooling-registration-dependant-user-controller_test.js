6d7dbe4c9395916a9641fd4ee18bd7907db43ac9
Anne-G <ang.schall@gmail.com>
2021-06-24 15:59:02 +0200

    [API] Add checkScoAccountRecovery controller

---
+++ api/tests/unit/application/schooling-registration-dependent-user/schooling-registration-dependant-user-controller_test.js
@@ -0,0 +1,49 @@
+const { expect, hFake, sinon } = require('../../../test-helper');
+
+const usecases = require('../../../../lib/domain/usecases');
+const studentInformationForAccountRecoverySerializer = require('../../../../lib/infrastructure/serializers/jsonapi/student-information-for-account-recovery-serializer.js');
+const schoolingRegistrationDependantUserController = require('../../../../lib/application/schooling-registration-dependent-users/schooling-registration-dependent-user-controller');
+
+describe('Unit | Application | Controller | schooling-registration-user-associations', () => {
+
+  describe('#checkScoAccountRecovery', () => {
+
+    it('should return student account information serialized', async () => {
+      // given
+      const studentInformation = {
+        ineIna: '1234567890A',
+        firstName: 'Bob',
+        lastName: 'Camond',
+        birthdate: '2001-12-08',
+      };
+      const request = {
+        payload: {
+          data: {
+            type: 'student-information',
+            attributes: {
+              'ine-ina': studentInformation.ineIna,
+              'first-name': studentInformation.firstName,
+              'last-name': studentInformation.lastName,
+              'birthdate': studentInformation.birthdate,
+            },
+          },
+        },
+      };
+      const studentInformationForAccountRecovery = Symbol();
+      const studentInformationForAccountRecoveryJSONAPI = Symbol();
+
+      sinon.stub(usecases, 'checkScoAccountRecovery');
+      usecases.checkScoAccountRecovery.withArgs({ studentInformation }).resolves(studentInformationForAccountRecovery);
+      sinon.stub(studentInformationForAccountRecoverySerializer, 'serialize')
+        .withArgs(studentInformationForAccountRecovery)
+        .returns(studentInformationForAccountRecoveryJSONAPI);
+
+      // when
+      const response = await schoolingRegistrationDependantUserController.checkScoAccountRecovery(request, hFake);
+
+      // then
+      expect(response).to.deep.equal(studentInformationForAccountRecoveryJSONAPI);
+    });
+  });
+
+});
