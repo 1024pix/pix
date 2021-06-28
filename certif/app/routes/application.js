60a279ae156bd018f284f7e922218d8c64cb559e
Christophe Charles <christophe.charles@octo.com>
2021-06-28 10:47:35 +0200

    [CERTIF] Delete mixin in adapter

--- certif/app/routes/application.js
+++ certif/app/routes/application.js
@@ -1,43 +1,17 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-// eslint-disable-next-line ember/no-mixins
-import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default class ApplicationRoute extends Route.extend(ApplicationRouteMixin) {
+export default class ApplicationRoute extends Route {
 
   @service currentUser;
-  @service url;
   @service featureToggles;
-
-  routeAfterAuthentication = 'authenticated';
 
   async beforeModel() {
     await this.featureToggles.load();
     return this._loadCurrentUser();
   }
 
-  async sessionAuthenticated() {
-    await this._loadCurrentUser();
-    this.transitionTo(this.routeAfterAuthentication);
-  }
-
-  sessionInvalidated() {
-    const redirectionUrl = this._redirectionUrl();
-    this._clearStateAndRedirect(redirectionUrl);
-  }
-
-  _clearStateAndRedirect(url) {
-    return window.location.replace(url);
-  }
-
   _loadCurrentUser() {
     return this.currentUser.load();
-  }
-
-  _redirectionUrl() {
-    const alternativeRootURL = this.session.alternativeRootURL;
-    this.session.alternativeRootURL = null;
-
-    return alternativeRootURL ? alternativeRootURL : this.url.homeUrl;
   }
 }
