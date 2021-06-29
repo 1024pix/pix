ae30e3d51bb46ad334c49857e8ea1b2b3845dee5
Stephane Bedeau <sbedeau@octo.com>
2021-06-29 18:17:27 +0200

    feat: hide info alert when warning alert has been displayed

--- mon-pix/tests/acceptance/challenge-item-qroc_test.js
+++ mon-pix/tests/acceptance/challenge-item-qroc_test.js
@@ -407,6 +407,26 @@ describe('Acceptance | Displaying a QROC challenge', () => {
         expect(find('.challenge-item__container--focused')).to.exist;
         expect(find('.assessment-challenge__focused-out-overlay')).to.exist;
       });
+
+      it('should display only the warning alert when it has been triggered', async function() {
+        // when
+        await click('.focused-challenge-instructions-action__confirmation-button');
+        const challengeItem = find('.challenge-item');
+        await triggerEvent(challengeItem, 'mouseleave');
+
+        // then
+        expect(find('.alert--info')).to.exist;
+        expect(find('.challenge-item__container--focused')).to.exist;
+        expect(find('.assessment-challenge__focused-out-overlay')).to.exist;
+
+        // when
+        await triggerEvent(window, 'blur');
+
+        expect(find('.alert--info')).to.not.exist;
+        expect(find('.alert--warning')).to.exist;
+        expect(find('.challenge-item__container--focused')).to.exist;
+        expect(find('.assessment-challenge__focused-out-overlay')).to.exist;
+      });
     });
 
     context('when challenge is not set as focused', function() {
