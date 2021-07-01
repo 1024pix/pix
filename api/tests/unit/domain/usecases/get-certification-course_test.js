32b07123549c42c860e84c1510875ad5906338ae
Etienne GIROT <egirot@octo.com>
2021-07-01 17:52:35 +0200

    [API] Allow to retrieve any CertifCourse when pix master

--- api/tests/unit/domain/usecases/get-certification-course_test.js
+++ api/tests/unit/domain/usecases/get-certification-course_test.js
@@ -5,6 +5,7 @@ describe('Unit | UseCase | get-certification-course', () => {
 
   let certificationCourse;
   let certificationCourseRepository;
+  let userRepository;
 
   beforeEach(() => {
     certificationCourse = {
@@ -14,9 +15,12 @@ describe('Unit | UseCase | get-certification-course', () => {
     certificationCourseRepository = {
       get: sinon.stub(),
     };
+    userRepository = {
+      isPixMaster: sinon.stub(),
+    };
   });
 
-  it('should get the certificationCourse', async () => {
+  it('should get the certificationCourse when the user id matches the certification course user id', async () => {
     // given
     certificationCourseRepository.get.withArgs(certificationCourse.id).resolves(certificationCourse);
 
@@ -25,21 +29,41 @@ describe('Unit | UseCase | get-certification-course', () => {
       certificationCourseId: certificationCourse.id,
       userId: 'user_id',
       certificationCourseRepository,
+      userRepository,
     });
 
     // then
     expect(actualCertificationCourse.id).to.equal(certificationCourse.id);
   });
 
-  it('should throw an error when the certification course is not linked to the user passed in parameter', () => {
+  it('should get the certificationCourse when the user id does not match the certification course user id but is pix master', async () => {
     // given
     certificationCourseRepository.get.withArgs(certificationCourse.id).resolves(certificationCourse);
+    userRepository.isPixMaster.withArgs('pix_master_user_id').resolves(true);
+
+    // when
+    const actualCertificationCourse = await getCertificationCourse({
+      certificationCourseId: certificationCourse.id,
+      userId: 'pix_master_user_id',
+      certificationCourseRepository,
+      userRepository,
+    });
+
+    // then
+    expect(actualCertificationCourse.id).to.equal(certificationCourse.id);
+  });
+
+  it('should throw an error when the certification course is not linked to the user passed in parameter and user is not pix master', () => {
+    // given
+    certificationCourseRepository.get.withArgs(certificationCourse.id).resolves(certificationCourse);
+    userRepository.isPixMaster.withArgs('other_user_id').resolves(false);
 
     // when
     const promise = getCertificationCourse({
       certificationCourseId: certificationCourse.id,
       userId: 'other_user_id',
       certificationCourseRepository,
+      userRepository,
     });
 
     // then
@@ -55,6 +79,7 @@ describe('Unit | UseCase | get-certification-course', () => {
       certificationCourseId: certificationCourse.id,
       userId: 'other_user_id',
       certificationCourseRepository,
+      userRepository,
     });
 
     // then
