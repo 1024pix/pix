e4dfe36756fa608b3e47b3ffc3e60cb05df54053
Laura Bergoens <laura.bergoens@pix.fr>
2021-07-01 17:09:44 +0200

    feat(api): add custom helper deepEqualOrderedInstances

---
+++ api/tests/tooling/chai-custom-helpers/deep-equal-ordered-instances.js
@@ -0,0 +1,23 @@
+module.exports = function(chai, _utils) {
+  chai.Assertion.addMethod('deepEqualOrderedInstances', function(referenceInstances) {
+    const assertedInstances = this._obj;
+
+    // Assert that both provided values are arrays
+    const assertedInstancesName = assertedInstances.constructor.name;
+    const referenceInstancesName = referenceInstances.constructor.name;
+    new chai.Assertion(assertedInstancesName).to.equal('Array');
+    new chai.Assertion(referenceInstancesName).to.equal('Array');
+
+    // Assert that both have the same length
+    const assertedInstancesLength = assertedInstances.length;
+    const referenceInstancesLength = referenceInstances.length;
+    new chai.Assertion(assertedInstancesLength).to.equal(referenceInstancesLength);
+
+    // Assert deepEqualInstance for each pair
+    for (let i = 0; i < assertedInstances.length; ++i) {
+      const assertedInstance = assertedInstances[i];
+      const referenceInstance = referenceInstances[i];
+      new chai.Assertion(assertedInstance).to.deepEqualInstance(referenceInstance);
+    }
+  });
+};
