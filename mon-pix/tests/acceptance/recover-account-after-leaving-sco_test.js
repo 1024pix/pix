492b96a55dcf4a370bdc3d3dae8eb85c0a698ee1
yahya youness <yahyayounes@hotmail.fr>
2021-07-02 19:20:10 +0200

    [APP] Add backup email confirmation step to recovery webflow

--- mon-pix/tests/acceptance/recover-account-after-leaving-sco_test.js
+++ mon-pix/tests/acceptance/recover-account-after-leaving-sco_test.js
@@ -150,6 +150,46 @@ describe('Acceptance | RecoverAccountAfterLeavingScoRoute', function() {
         expect(contains(this.intl.t('pages.recover-account-after-leaving-sco.student-information.title'))).to.not.exist;
       });
     });
+
+    context('when confirm student information', () => {
+
+      context('click on "Confirm" button', () => {
+
+        it('should hide recover account confirmation step and show recover account backup email confirmation', async function() {
+          // given
+          const ineIna = '0123456789A';
+          const lastName = 'Lecol';
+          const firstName = 'Manuela';
+          const dayOfBirth = 20;
+          const monthOfBirth = 5;
+          const yearOfBirth = 2000;
+          const birthdate = '2000-05-20';
+          const username = 'manuela.lecol2005';
+
+          server.create('user', { id: 1, firstName, lastName, username });
+          server.create('student-information', { id: 2, ineIna, firstName, lastName, birthdate });
+          server.create('feature-toggle', { id: 0, isScoAccountRecoveryEnabled: true });
+
+          await visit('/recuperer-mon-compte');
+          await fillInByLabel(this.intl.t('pages.recover-account-after-leaving-sco.student-information.form.ine-ina'), ineIna);
+          await fillInByLabel(this.intl.t('pages.recover-account-after-leaving-sco.student-information.form.first-name'), firstName);
+          await fillInByLabel(this.intl.t('pages.recover-account-after-leaving-sco.student-information.form.last-name'), lastName);
+          await fillInByLabel(this.intl.t('pages.recover-account-after-leaving-sco.student-information.form.label.birth-day'), dayOfBirth);
+          await fillInByLabel(this.intl.t('pages.recover-account-after-leaving-sco.student-information.form.label.birth-month'), monthOfBirth);
+          await fillInByLabel(this.intl.t('pages.recover-account-after-leaving-sco.student-information.form.label.birth-year'), yearOfBirth);
+          await clickByLabel(this.intl.t('pages.recover-account-after-leaving-sco.student-information.form.submit'));
+
+          // when
+          await clickByLabel(this.intl.t('pages.recover-account-after-leaving-sco.confirmation-step.buttons.confirm'));
+
+          // then
+          expect(contains(this.intl.t('pages.recover-account-after-leaving-sco.backup-email-confirmation.email-is-needed-message', { firstName }))).to.exist;
+          expect(contains(this.intl.t('pages.recover-account-after-leaving-sco.confirmation-step.good-news', { firstName }))).to.not.exist;
+        });
+      });
+
+    });
+
   });
 
 });
