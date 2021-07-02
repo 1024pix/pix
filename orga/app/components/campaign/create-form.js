a54950520d05163d1ebbb1f96c2c214ea82924cd
Benjamin Petetot <bpetetot@gmail.com>
2021-07-02 16:39:35 +0200

    [ORGA] Rework components in Campaign creation route

--- orga/app/components/campaign/create-form.js
+++ orga/app/components/campaign/create-form.js
@@ -3,7 +3,7 @@ import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 
-export default class New extends Component {
+export default class CreateForm extends Component {
   @service currentUser;
 
   @tracked wantIdPix = false;
