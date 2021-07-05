f21d6043093bf5d18796c8e316cc251d88c70173
Thomasevano <thomas-evano@hotmail.fr>
2021-07-05 16:00:56 +0200

    Allow {t1,t2,t3}Status to be a boolean
    This is coming very soon from the LCMS team
    
    Co-authored-by: Thomas Evano <thomas.evano@pix.fr>
    Co-authored-by: Céline Ung <celine.ung@pix.fr>
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>
    Co-authored-by: Nicolas Legrand <nicolas.legrand@pix.fr>

--- api/lib/infrastructure/adapters/solution-adapter.js
+++ api/lib/infrastructure/adapters/solution-adapter.js
@@ -1,6 +1,13 @@
 const Solution = require('../../domain/models/Solution');
 const _ = require('../../../lib/infrastructure/utils/lodash-utils');
 
+function statusToBoolean(value) {
+  if (typeof value === 'boolean') {
+    return value;
+  }
+  return value !== 'Désactivé';
+}
+
 module.exports = {
 
   fromDatasourceObject(datasourceObject) {
@@ -8,9 +15,9 @@ module.exports = {
 
     return new Solution({
       id: datasourceObject.id,
-      isT1Enabled: datasourceObject.t1Status !== 'Désactivé',
-      isT2Enabled: datasourceObject.t2Status !== 'Désactivé',
-      isT3Enabled: datasourceObject.t3Status !== 'Désactivé',
+      isT1Enabled: statusToBoolean(datasourceObject.t1Status),
+      isT2Enabled: statusToBoolean(datasourceObject.t2Status),
+      isT3Enabled: statusToBoolean(datasourceObject.t3Status),
       type: datasourceObject.type,
       value: datasourceObject.solution,
       scoring,
