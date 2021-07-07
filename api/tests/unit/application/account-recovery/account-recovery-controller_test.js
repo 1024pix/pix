175ab2daeba3d47281ec3cd46bc9f22e3566f629
Andreia Pena <andreia.pena@pix.fr>
2021-07-07 17:55:53 +0200

    [API] Add account-recovery-controller

---
+++ api/tests/unit/application/account-recovery/account-recovery-controller_test.js
@@ -0,0 +1,44 @@
+const {
+  expect,
+  sinon,
+  hFake,
+  domainBuilder,
+} = require('../../../test-helper');
+
+const accountRecoveryController = require('../../../../lib/application/account-recovery/account-recovery-controller');
+const usecases = require('../../../../lib/domain/usecases');
+
+describe('Unit | Controller | account-recovery-controller', () => {
+
+  describe('#sendEmailForAccountRecovery', () => {
+
+    it('should call sendEmailForAccountRecovery usecase', async () => {
+      // given
+      const userId = domainBuilder.buildUser({ id: 1 }).id;
+      const newEmail = 'new_email@example.net';
+
+      const request = {
+        payload: {
+          data: {
+            attributes: {
+              userId,
+              email: newEmail,
+            },
+          },
+        },
+      };
+
+      sinon.stub(usecases, 'sendEmailForAccountRecovery').resolves();
+
+      // when
+      const response = await accountRecoveryController.sendEmailForAccountRecovery(request, hFake);
+
+      // then
+      expect(usecases.sendEmailForAccountRecovery).to.have.been.calledOnce;
+      expect(usecases.sendEmailForAccountRecovery).calledWith({ userId, email: newEmail });
+      expect(response.statusCode).to.equal(201);
+    });
+
+  });
+
+});
