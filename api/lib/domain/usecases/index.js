6e5718f436fdf22187d97bac610a86b682f75e05
yahya youness <yahyayounes@hotmail.fr>
2021-07-08 09:56:55 +0200

    [API] Add usecase to find account recovery demands

--- api/lib/domain/usecases/index.js
+++ api/lib/domain/usecases/index.js
@@ -260,6 +260,7 @@ module.exports = injectDependencies({
   getShareableCertificate: require('./certificate/get-shareable-certificate'),
   getStageDetails: require('./get-stage-details'),
   getTargetProfileDetails: require('./get-target-profile-details'),
+  getUserByAccountRecoveryDemand: require('./account-recovery/get-user-by-account-recovery-demand'),
   getUserByResetPasswordDemand: require('./get-user-by-reset-password-demand'),
   getUserCampaignAssessmentResult: require('./get-user-campaign-assessment-result'),
   getUserCampaignParticipationToCampaign: require('./get-user-campaign-participation-to-campaign'),
---
+++ api/tests/unit/domain/usecases/account-recovery/get-user-by-account-recovery-demand_test.js
@@ -0,0 +1,77 @@
+const { catchErr, domainBuilder, expect, sinon } = require('../../../../test-helper');
+
+const User = require('../../../../../lib/domain/models/User');
+const {
+  NotFoundError,
+  UserNotFoundError,
+} = require('../../../../../lib/domain/errors');
+
+const getUserByAccountRecoveryDemand = require('../../../../../lib/domain/usecases/account-recovery/get-user-by-account-recovery-demand');
+
+describe('Unit | UseCase | get-user-by-account-recovery-demand', () => {
+
+  const temporaryKey = 'ZHABCDEFJSJ';
+  let userRepository;
+  let accountRecoveryDemandRepository;
+
+  beforeEach(() => {
+    accountRecoveryDemandRepository = {
+      findByTemporaryKey: sinon.stub(),
+    };
+    userRepository = {
+      get: sinon.stub(),
+    };
+
+  });
+
+  it('should throw NotFoundError if temporary key does not exist or already used', async () => {
+    // given
+    accountRecoveryDemandRepository.findByTemporaryKey.rejects(new NotFoundError('Temporary key not found or already used'));
+
+    // when
+    const error = await catchErr(getUserByAccountRecoveryDemand)({
+      temporaryKey,
+      accountRecoveryDemandRepository,
+      userRepository,
+    });
+
+    // then
+    expect(error).to.be.an.instanceOf(NotFoundError);
+  });
+
+  it('should throw UserNotFoundError if user identifier does not exist', async () => {
+    // given
+    accountRecoveryDemandRepository.findByTemporaryKey.resolves({ userId: 1234 });
+    userRepository.get.throws(new UserNotFoundError());
+
+    // when
+    const error = await catchErr(getUserByAccountRecoveryDemand)({
+      temporaryKey,
+      accountRecoveryDemandRepository,
+      userRepository,
+    });
+
+    // then
+    expect(error).to.be.an.instanceOf(UserNotFoundError);
+  });
+
+  it('should return user when the user identifier exist', async () => {
+    // given
+    const user = domainBuilder.buildUser();
+    accountRecoveryDemandRepository.findByTemporaryKey.resolves({ userId: user.id });
+    userRepository.get.resolves(user);
+
+    // when
+    const result = await getUserByAccountRecoveryDemand({
+      temporaryKey,
+      accountRecoveryDemandRepository,
+      userRepository,
+    });
+
+    // then
+    expect(result).to.be.an.instanceOf(User);
+    expect(accountRecoveryDemandRepository.findByTemporaryKey).to.have.been.calledWith(temporaryKey);
+    expect(userRepository.get).to.have.been.calledWith(user.id);
+  });
+
+});
