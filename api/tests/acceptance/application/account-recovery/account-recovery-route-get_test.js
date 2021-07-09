ec864eda7a513d9132c2e06ec3dd34fabb6cb73c
yahya youness <yahyayounes@hotmail.fr>
2021-07-09 16:31:00 +0200

    [API] Add route to check account recovery demands

---
+++ api/tests/acceptance/application/account-recovery/account-recovery-route-get_test.js
@@ -0,0 +1,35 @@
+const { expect, databaseBuilder } = require('../../../test-helper');
+const { featureToggles } = require('../../../../lib/config');
+const createServer = require('../../../../server');
+
+describe('Integration | Application | Account-Recovery | Routes', () => {
+
+  describe('GET /api/account-recovery/{temporaryKey}', () => {
+
+    it('should return 200 http status code when recovery demand found', async () => {
+      // given
+      const temporaryKey = 'DJFKDKJJSHQJ';
+      const userId = 1234;
+      databaseBuilder.factory.buildUser.withRawPassword({ id: userId });
+      databaseBuilder.factory.buildAccountRecoveryDemand({ userId, temporaryKey, used: false });
+      await databaseBuilder.commit();
+      const server = await createServer();
+      featureToggles.isScoAccountRecoveryEnabled = true;
+
+      const options = {
+        method: 'GET',
+        url: '/api/account-recovery/' + temporaryKey,
+      };
+
+      // when
+      const response = await server.inject(options);
+
+      // then
+      expect(response.statusCode).to.equal(200);
+      expect(response.result.data.type).to.equal('users');
+      expect(response.result.data.id).to.equal(userId.toString());
+
+    });
+
+  });
+});
