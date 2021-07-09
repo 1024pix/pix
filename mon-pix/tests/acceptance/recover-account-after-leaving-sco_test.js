bcc92521d8f014449831850e6dbe4988da9f2147
Andreia Pena <andreia.pena@pix.fr>
2021-07-09 10:49:35 +0200

    [APP] Display confirmation message when email is sent

--- mon-pix/tests/acceptance/recover-account-after-leaving-sco_test.js
+++ mon-pix/tests/acceptance/recover-account-after-leaving-sco_test.js
@@ -215,6 +215,42 @@ describe('Acceptance | RecoverAccountAfterLeavingScoRoute', function() {
           expect(contains(this.intl.t('pages.recover-account-after-leaving-sco.backup-email-confirmation.email-is-needed-message', { firstName }))).to.exist;
           expect(contains(this.intl.t('pages.recover-account-after-leaving-sco.confirmation-step.good-news', { firstName }))).to.not.exist;
         });
+
+        it('should show email sent confirmation when user has supplied an email and submitted', async function() {
+          // given
+          const ineIna = '0123456789A';
+          const lastName = 'Lecol';
+          const firstName = 'Manuela';
+          const dayOfBirth = 20;
+          const monthOfBirth = 5;
+          const yearOfBirth = 2000;
+          const birthdate = '2000-05-20';
+          const username = 'manuela.lecol2005';
+          const newEmail = 'john.doe@example.net';
+
+          server.create('user', { id: 1, firstName, lastName, username });
+          server.create('student-information', { id: 2, ineIna, firstName, lastName, birthdate });
+          server.create('feature-toggle', { id: 0, isScoAccountRecoveryEnabled: true });
+
+          await visit('/recuperer-mon-compte');
+          await fillInByLabel(this.intl.t('pages.recover-account-after-leaving-sco.student-information.form.ine-ina'), ineIna);
+          await fillInByLabel(this.intl.t('pages.recover-account-after-leaving-sco.student-information.form.first-name'), firstName);
+          await fillInByLabel(this.intl.t('pages.recover-account-after-leaving-sco.student-information.form.last-name'), lastName);
+          await fillInByLabel(this.intl.t('pages.recover-account-after-leaving-sco.student-information.form.label.birth-day'), dayOfBirth);
+          await fillInByLabel(this.intl.t('pages.recover-account-after-leaving-sco.student-information.form.label.birth-month'), monthOfBirth);
+          await fillInByLabel(this.intl.t('pages.recover-account-after-leaving-sco.student-information.form.label.birth-year'), yearOfBirth);
+          await clickByLabel(this.intl.t('pages.recover-account-after-leaving-sco.student-information.form.submit'));
+
+          await clickByLabel(this.intl.t('pages.recover-account-after-leaving-sco.confirmation-step.buttons.confirm'));
+
+          // when
+          await fillInByLabel(this.intl.t('pages.recover-account-after-leaving-sco.backup-email-confirmation.form.email'), newEmail);
+
+          // then
+          expect(contains(this.intl.t('pages.recover-account-after-leaving-sco.backup-email-confirmation.email-is-needed-message', { firstName }))).to.not.exist;
+          expect(contains(this.intl.t('pages.recover-account-after-leaving-sco.send-email-confirmation.title'))).to.exist;
+
+        });
       });
 
       context('click on "Cancel" button', () => {
