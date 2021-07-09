1281893f3b6d88c587c3dbb00885a3e78ad28ce5
yahya youness <yahyayounes@hotmail.fr>
2021-07-09 16:31:00 +0200

    [API] Add route to check account recovery demands

--- api/lib/application/account-recovery/index.js
+++ api/lib/application/account-recovery/index.js
@@ -35,6 +35,24 @@ exports.register = async function(server) {
         tags: ['api', 'account-recovery'],
       },
     },
+
+    {
+      method: 'GET',
+      path: '/api/account-recovery/{temporaryKey}',
+      config: {
+        auth: false,
+        handler: accountRecoveryController.checkAccountRecoveryDemand,
+        pre: [
+          {
+            method: featureToggles.isScoAccountRecoveryEnabled,
+            assign: 'isScoAccountRecoveryEnabled',
+          },
+        ],
+        notes: ['- Permet de vérifier la demande de récupération de son compte Pix.\n' +
+        '- Renvoie l’utilisateur correspondant à la demande pour une réinitialisation de mot de passe.'],
+        tags: ['api', 'account-recovery'],
+      },
+    },
   ]);
 };
 
