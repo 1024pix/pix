67682d85b4ce58819e82fb0eb4ceb23e16e08c73
Clément Bourgoin <clement.bourgoin@pix.fr>
2021-07-13 11:50:09 +0200

    refactor(certif): use getByLabel in other helpers

--- certif/tests/helpers/extended-ember-test-helpers/click-by-label.js
+++ certif/tests/helpers/extended-ember-test-helpers/click-by-label.js
@@ -1,35 +1,8 @@
-import { click, findAll } from '@ember/test-helpers';
+import { click } from '@ember/test-helpers';
+import getByLabel from './get-by-label';
 
 export default function clickByLabel(labelText) {
-  const clickableElement = _findClickableElementForLabel(labelText);
-
-  if (!clickableElement) {
-    throw new Error(`Cannot find clickable element labelled "${labelText}".`);
-  }
+  const clickableElement = getByLabel(labelText);
 
   return click(clickableElement);
-}
-
-function _findClickableElementForLabel(labelText) {
-  const clickableSelectors = ['button', 'a[href]', '[role="button"]', 'input[type="radio"]', 'input[type="checkbox"]', 'label[for]'];
-  return findAll(clickableSelectors.join(',')).find(_matchesLabel(labelText));
-}
-
-function _matchesLabel(labelText) {
-  return (element) => _matchesInnerText(element, labelText) ||
-                      _matchesTitle(element, labelText) ||
-                      _matchesAriaLabel(element, labelText);
-}
-
-function _matchesInnerText(element, labelText) {
-  return element.innerText.includes(labelText);
-}
-
-function _matchesTitle(element, labelText) {
-  return element.title && element.title.includes(labelText);
-}
-
-function _matchesAriaLabel(element, labelText) {
-  const ariaLabel = element.getAttribute('aria-label');
-  return ariaLabel && ariaLabel.includes(labelText);
 }
