39ac28c0d2c4c76e71e47091e677052e830d4d59
Clément Bourgoin <clement.bourgoin@pix.fr>
2021-07-13 11:50:09 +0200

    refactor(certif): use get-by-label other helpers

--- certif/tests/helpers/extended-ember-test-helpers/fill-in-by-label.js
+++ certif/tests/helpers/extended-ember-test-helpers/fill-in-by-label.js
@@ -1,36 +1,8 @@
-import { fillIn, findAll, find } from '@ember/test-helpers';
+import { fillIn } from '@ember/test-helpers';
+import getByLabel from './get-by-label';
 
 export default function fillInByLabel(labelText, value) {
-  const control = _findControlForLabel(labelText);
+  const control = getByLabel(labelText);
 
   return fillIn(control, value);
-}
-
-function _findControlForLabel(labelText) {
-  const label = _findLabel(labelText);
-  if (label && label.control) return label.control;
-
-  const control = _findControl(labelText);
-  if (control) return control;
-
-  if (label && !label.control) {
-    throw new Error(`Found label "${labelText}" but no associated form control.`);
-  }
-  throw new Error(`Cannot find form control labelled "${labelText}".`);
-}
-
-function _findLabel(labelText) {
-  return findAll('label').find((label) => label.innerText.includes(labelText));
-}
-
-function _findControl(labelText) {
-  const selectors = [];
-
-  for (const tag of ['input', 'textarea', 'select']) {
-    for (const attr of ['aria-label', 'title']) {
-      selectors.push(`${tag}[${attr}="${labelText}"]`);
-    }
-  }
-
-  return find(selectors.join(','));
 }
