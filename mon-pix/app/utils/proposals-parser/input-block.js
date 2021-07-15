563beb52366e9724c422ee2e6e1782aa520f7b2a
Stephane Bedeau <sbedeau@octo.com>
2021-07-15 15:13:51 +0200

    refactor: extract common methods to response-block

--- mon-pix/app/utils/proposals-parser/input-block.js
+++ mon-pix/app/utils/proposals-parser/input-block.js
@@ -1,28 +1,9 @@
 import ResponseBlock from './response-block';
-import splitters from './splitters';
-
-const {
-  ARIA_LABEL,
-  PLACEHOLDER_AND_ARIA_LABEL,
-  PLACEHOLDER,
-} = splitters;
 
 export default class InputBlock extends ResponseBlock {
 
   constructor({ input, inputIndex }) {
     super({ input, inputIndex });
     this.setType('input');
-    this._addPlaceHolderAndAriaLabelIfExist();
-  }
-
-  _addPlaceHolderAndAriaLabelIfExist() {
-    if (this.hasPlaceHolder) {
-      this.setPlaceholder(this.input.split(PLACEHOLDER)[1].split(ARIA_LABEL)[0]);
-    }
-    if (this.hasAriaLabel) {
-      this.setAriaLabel(this.input.split(ARIA_LABEL)[1]);
-      this.setAutoAriaLabel(false);
-    }
-    this.setInput(this.input.split(PLACEHOLDER_AND_ARIA_LABEL)[0]);
   }
 }
