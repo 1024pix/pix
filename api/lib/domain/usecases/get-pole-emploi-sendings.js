3724ebba11716e9c9c1d5c84d86b468ac4a312a6
ajoanny <ajoanny@octo.com>
2021-07-15 17:50:17 +0200

    [API] use filters in use case

--- api/lib/domain/usecases/get-pole-emploi-sendings.js
+++ api/lib/domain/usecases/get-pole-emploi-sendings.js
@@ -1,16 +1,16 @@
 const poleEmploiService = require('../services/pole-emploi-service');
 
-module.exports = async function getPoleEmploiSendings({ cursor, poleEmploiSendingRepository }) {
+module.exports = async function getPoleEmploiSendings({ cursor, poleEmploiSendingRepository, filters }) {
   const cursorData = await poleEmploiService.decodeCursor(cursor);
-  const sendings = await poleEmploiSendingRepository.find(cursorData);
-  const link = _generateLink(sendings);
+  const sendings = await poleEmploiSendingRepository.find(cursorData, filters);
+  const link = _generateLink(sendings, filters);
   return { sendings, link };
 };
 
-function _generateLink(sendings) {
+function _generateLink(sendings, filters) {
   if (!sendings.length) return null;
 
   const lastSending = sendings[sendings.length - 1];
-  const link = poleEmploiService.generateLink(lastSending);
+  const link = poleEmploiService.generateLink(lastSending, filters);
   return link;
 }
