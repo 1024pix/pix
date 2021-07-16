d051d127e6f0b55473d5db1a677b4ee30bba16aa
Xavier Carron <xavier.carron@zenika.com>
2021-07-16 15:16:21 +0200

    [ADMIN] Add tick or cross on stages list Co-authored-by: Xavier Carron <xavier.carron@zenika.com> Co-authored-by: Yaëlle Marsiac <yama@octo.com> Co-authored-by: Karam Haytham Mohammed <kram@octo.com>

---
+++ admin/app/components/common/tick-or-cross.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+
+export default class TickOrCross extends Component {
+
+  get icon() {
+    return this.args.isTrue ? 'check' : 'times';
+  }
+
+  get class() {
+    return this.args.isTrue ? 'tick-or-cross--valid' : 'tick-or-cross--invalid';
+  }
+}
