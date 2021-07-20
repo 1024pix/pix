53e6e2a3ac2e1df964e1e34cb44cb12cc63d3b3f
Benjamin Petetot <bpetetot@gmail.com>
2021-07-20 15:24:50 +0200

    [ORGA] Create assessment results route

---
+++ orga/app/routes/authenticated/campaigns/campaign/assessment-results.js
@@ -0,0 +1,56 @@
+import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
+
+export default class AssessmentResultsRoute extends Route {
+  queryParams = {
+    pageNumber: {
+      refreshModel: true,
+    },
+    pageSize: {
+      refreshModel: true,
+    },
+    divisions: {
+      refreshModel: true,
+    },
+    badges: {
+      refreshModel: true,
+    },
+    stages: {
+      refreshModel: true,
+    },
+  };
+
+  async model(params) {
+    const campaign = this.modelFor('authenticated.campaigns.campaign');
+    await campaign.belongsTo('campaignCollectiveResult').reload();
+    return RSVP.hash({
+      campaign,
+      participations: this.fetchSummaries({ campaignId: campaign.id, ...params }),
+    });
+  }
+
+  fetchSummaries(params) {
+    return this.store.query('campaignAssessmentParticipationSummary', {
+      page: {
+        number: params.pageNumber,
+        size: params.pageSize,
+      },
+      filter: {
+        divisions: params.divisions,
+        badges: params.badges,
+        stages: params.stages,
+      },
+      campaignId: params.campaignId,
+    });
+  }
+
+  resetController(controller, isExiting) {
+    if (isExiting) {
+      controller.pageNumber = 1;
+      controller.pageSize = 25;
+      controller.divisions = [];
+      controller.badges = [];
+      controller.stages = [];
+    }
+  }
+}
