f1e3850903f4fc9f9d4b97e0935a9b4327096502
Lise Quesnel <quesnel.lise@gmail.com>
2021-07-21 14:32:58 +0200

    [APP] fix acceptance test that did not reflect reality

--- mon-pix/mirage/serializers/campaign-participation.js
+++ mon-pix/mirage/serializers/campaign-participation.js
@@ -7,6 +7,9 @@ export default ApplicationSerializer.extend({
     'createdAt',
     'participantExternalId',
   ],
+  include: [
+    'campaign',
+  ],
   links(campaignParticipation) {
     return {
       'assessment': {
