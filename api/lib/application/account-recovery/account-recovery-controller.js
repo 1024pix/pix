d2ae60d60d9d0b3f8cb350d4f78aed9325c3fe33
yahya youness <yahyayounes@hotmail.fr>
2021-07-21 15:49:00 +0200

    [API] Add update user account in controller

--- api/lib/application/account-recovery/account-recovery-controller.js
+++ api/lib/application/account-recovery/account-recovery-controller.js
@@ -1,6 +1,7 @@
 const usecases = require('../../domain/usecases');
 const userSerializer = require('../../infrastructure/serializers/jsonapi/user-serializer');
 const studentInformationForAccountRecoverySerializer = require('../../infrastructure/serializers/jsonapi/student-information-for-account-recovery-serializer');
+const DomainTransaction = require('../../infrastructure/DomainTransaction');
 
 module.exports = {
 
@@ -16,6 +17,27 @@ module.exports = {
     const temporaryKey = request.params.temporaryKey;
     const user = await usecases.getUserByAccountRecoveryDemand({ temporaryKey });
     return userSerializer.serialize(user);
+  },
+
+  async updateUserAccountRecovery(request, h) {
+
+    const userId = request.payload.data.id;
+    const email = request.payload.data.attributes.email;
+    const password = request.payload.data.attributes.password;
+    const temporaryKey = request.payload.data.attributes['temporary-key'];
+
+    await DomainTransaction.execute(async (domainTransaction) => {
+      await usecases.updateUserAccount({
+        userId,
+        newEmail: email,
+        password,
+        temporaryKey,
+        domainTransaction,
+      },
+      );
+    });
+
+    return h.response().code(204);
   },
 
 };
