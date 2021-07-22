6b97fba88356bf3af11bcd73c387e3054ad0dc35
yaelle <ymarsiac@octo.com>
2021-07-22 16:46:11 +0200

    [API] change request to display only campaignParticipations not improved

--- api/lib/infrastructure/repositories/campaign-profiles-collection-participation-summary-repository.js
+++ api/lib/infrastructure/repositories/campaign-profiles-collection-participation-summary-repository.js
@@ -30,6 +30,7 @@ const CampaignProfilesCollectionParticipationSummaryRepository = {
           .andOn({ 'campaigns.organizationId': 'schooling-registrations.organizationId' });
       })
       .where('campaign-participations.campaignId', '=', campaignId)
+      .where('campaign-participations.isImproved', '=', false)
       .whereRaw('"campaign-participations"."sharedAt" IS NOT NULL')
       .orderByRaw('?? ASC, ?? ASC', ['lowerLastName', 'lowerFirstName'])
       .modify(_filterQuery, filters);
