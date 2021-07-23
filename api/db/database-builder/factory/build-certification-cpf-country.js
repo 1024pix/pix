3d81d2e74a5652a077b82d57234c02a8bf7c3033
Laura Bergoens <laura.bergoens@pix.fr>
2021-07-23 15:09:30 +0200

    refactor(api): rename sanitizeAndSortChars into normalizeAndSortChars

--- api/db/database-builder/factory/build-certification-cpf-country.js
+++ api/db/database-builder/factory/build-certification-cpf-country.js
@@ -1,12 +1,12 @@
 const databaseBuffer = require('../database-buffer');
-const { sanitizeAndSortChars } = require('../../../lib/infrastructure/utils/string-utils');
+const { normalizeAndSortChars } = require('../../../lib/infrastructure/utils/string-utils');
 
 module.exports = function buildCertificationCpfCountry({
   id = databaseBuffer.getNextId(),
   code = '99123',
   commonName = 'FILÉKISTANIE',
   originalName = 'RÉPUBLIQUE DE FILÉKISTAN',
-  matcher = sanitizeAndSortChars(originalName),
+  matcher = normalizeAndSortChars(originalName),
   createdAt = new Date(),
 } = {}) {
   const values = {
