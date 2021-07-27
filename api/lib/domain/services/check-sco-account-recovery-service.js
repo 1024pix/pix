d1d979102354a8ce6d3dac4d58d02efab0f05aa9
Andreia Pena <andreia.pena@pix.fr>
2021-07-27 17:33:11 +0200

    [API] Allow approximate match on account recovery

--- api/lib/domain/services/check-sco-account-recovery-service.js
+++ api/lib/domain/services/check-sco-account-recovery-service.js
@@ -1,27 +1,59 @@
-const { MultipleSchoolingRegistrationsWithDifferentNationalStudentIdError } = require('../errors');
+const { MultipleSchoolingRegistrationsWithDifferentNationalStudentIdError, UserNotFoundError } = require('../errors');
 const _ = require('lodash');
 
 async function retrieveSchoolingRegistration({
   studentInformation,
   schoolingRegistrationRepository,
   userRepository,
+  userReconciliationService,
 }) {
 
-  const { id, userId, firstName, lastName, organizationId } = await schoolingRegistrationRepository.getSchoolingRegistrationInformation({
-    ...studentInformation,
+  const latestSchoolingRegistration = await schoolingRegistrationRepository.getLatestSchoolingRegistration({
+    birthdate: studentInformation.birthdate,
     nationalStudentId: studentInformation.ineIna,
   });
+
+  const userId = await _getUserIdByMatchingStudentInformationWithSchoolingRegistration({
+    studentInformation,
+    latestSchoolingRegistration,
+    userReconciliationService,
+  });
+
+  await _checkIfThereAreMultipleUserForTheSameAccount({ userId, schoolingRegistrationRepository });
+
+  const { username, email } = await userRepository.get(userId);
+
+  const { id, firstName, lastName, organizationId } = latestSchoolingRegistration;
+
+  return { id, userId, firstName, lastName, username, organizationId, email };
+}
+
+async function _getUserIdByMatchingStudentInformationWithSchoolingRegistration({
+  studentInformation,
+  latestSchoolingRegistration,
+  userReconciliationService,
+}) {
+  const matchingSchoolingRegistrationId = await userReconciliationService.findMatchingCandidateIdForGivenUser(
+    [ latestSchoolingRegistration ],
+    { firstName: studentInformation.firstName, lastName: studentInformation.lastName },
+  );
+
+  if (!matchingSchoolingRegistrationId) {
+    throw new UserNotFoundError();
+  }
+
+  return latestSchoolingRegistration.userId;
+}
+
+async function _checkIfThereAreMultipleUserForTheSameAccount({ userId, schoolingRegistrationRepository }) {
   const schoolingRegistrations = await schoolingRegistrationRepository.findByUserId({ userId });
 
   if (_.uniqBy(schoolingRegistrations, 'nationalStudentId').length > 1) {
     throw new MultipleSchoolingRegistrationsWithDifferentNationalStudentIdError();
   }
-
-  const { username, email } = await userRepository.get(userId);
-
-  return { id, userId, firstName, lastName, username, organizationId, email };
 }
 
 module.exports = {
   retrieveSchoolingRegistration,
 };
+
