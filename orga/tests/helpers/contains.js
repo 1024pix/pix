4bd720be1c370e0654dd4565c53358bc1fcaae9a
Karam Mohd <k.haytham.mohammed@octo.com>
2021-07-27 20:01:36 +0200

    [BSR] Fix behaviour of contains.js

--- orga/tests/helpers/contains.js
+++ orga/tests/helpers/contains.js
@@ -1,48 +1,36 @@
 import { getRootElement } from '@ember/test-helpers';
 
-function getChildrenThatContainsText(element, text, isChild) {
-  if (element.textContent.trim() === text) {
-    return isChild ? element : [element];
+function _isChildrenContainsText(element, text) {
+  if (element.textContent.includes(text)) {
+    return true;
+  } else if (element.children.length > 0) {
+    for (let i = 0; i < element.children.length; i++) {
+      if (_isChildrenContainsText(element.children[i], text)) {
+        return true;
+      }
+    }
   }
-
-  if (element.children.length === 0) {
-    return null;
-  }
-
-  return [...element.children]
-    .map((child) => {
-      return getChildrenThatContainsText(child, text, true);
-    })
-    .filter(Boolean)
-    .flatMap((v) => v);
+  return false;
 }
 
 export function contains(text) {
-  const elements = getChildrenThatContainsText(getRootElement(), text);
-  const result = elements.length > 0;
+  const result = _isChildrenContainsText(getRootElement(), text);
 
   let message = `There is no elements with "${ text }"`;
   if (result) {
     message = `Element with "${ text }" found`;
   }
 
-  this.pushResult({
-    result,
-    message,
-  });
+  this.pushResult({ result, message });
 }
 
 export function notContains(text) {
-  const elements = getChildrenThatContainsText(getRootElement(), text);
-  const result = elements.length === 0;
+  const result = !_isChildrenContainsText(getRootElement(), text);
 
   let message = `Element with "${ text }" found`;
   if (result) {
     message = `There is no elements with "${ text }"`;
   }
 
-  this.pushResult({
-    result,
-    message,
-  });
+  this.pushResult({ result, message });
 }
