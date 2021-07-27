0201c90ed94b723960ae54393a41319affcc78a6
Quentin Lebouc <quentin.lebouc@pix.fr>
2021-07-27 11:34:24 +0200

    feat(admin): display candidate info in modal

--- admin/app/components/certification/candidate-edit-modal.js
+++ admin/app/components/certification/candidate-edit-modal.js
@@ -1,5 +1,6 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class CandidateEditModal extends Component {
 
@@ -8,4 +9,25 @@ export default class CandidateEditModal extends Component {
   @tracked birthdate;
   @tracked birthCity;
 
+  constructor() {
+    super(...arguments);
+    this.firstName = this.args.candidate.firstName;
+    this.lastName = this.args.candidate.lastName;
+    this.birthdate = this.args.candidate.birthdate;
+    this.birthCity = this.args.candidate.birthplace;
+  }
+
+  focus(element) {
+    element.focus();
+  }
+
+  @action
+  onUpdateCertificationBirthdate(_, _lastSelectedDateFormatted) {
+  }
+
+  @action
+  onFormSubmit(event) {
+    event.preventDefault();
+    this.args.onFormSubmit();
+  }
 }
