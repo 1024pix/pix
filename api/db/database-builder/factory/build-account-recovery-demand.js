7c830d6d80c534ccb5073a219efad9be548f9762
Pierre TOP (TOPI) <ptop@octo.com>
2021-07-28 10:43:54 +0200

    [API] Make recovery demand builder self-supporting (struct.)

--- api/db/database-builder/factory/build-account-recovery-demand.js
+++ api/db/database-builder/factory/build-account-recovery-demand.js
@@ -1,9 +1,12 @@
 const databaseBuffer = require('../database-buffer');
 const buildSchoolingRegistration = require('./build-schooling-registration');
+const buildUser = require('./build-user');
 
 module.exports = function buildAccountRecoveryDemand({
   id = databaseBuffer.getNextId(),
   userId,
+  firstName,
+  lastName,
   schoolingRegistrationId,
   oldEmail,
   newEmail = 'philipe@example.net',
@@ -11,16 +14,30 @@ module.exports = function buildAccountRecoveryDemand({
   used = false,
   createdAt,
 } = {}) {
+
+  let schoolingRegistrationAttributes;
+  let user;
+
+  if (!userId) {
+    user = buildUser();
+    schoolingRegistrationAttributes = { userId: user.id, firstName: user.firstName, lastName: user.lastName, nationalStudentId: '123456789JJ' };
+  } else {
+    schoolingRegistrationAttributes = { userId, firstName, lastName, nationalStudentId: '123456789JJ' };
+  }
+  const actualSchoolingRegistrationId = schoolingRegistrationId || buildSchoolingRegistration(schoolingRegistrationAttributes).id;
+  const actualUserId = userId || user.id;
+
   const values = {
     id,
-    userId,
-    schoolingRegistrationId: schoolingRegistrationId || buildSchoolingRegistration({ userId }).id,
+    userId: actualUserId,
+    schoolingRegistrationId: actualSchoolingRegistrationId,
     oldEmail,
     newEmail,
     temporaryKey,
     used,
     createdAt,
   };
+
   return databaseBuffer.pushInsertable({
     tableName: 'account-recovery-demands',
     values,
