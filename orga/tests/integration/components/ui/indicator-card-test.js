dba3f02e153dddb132b7205ec6126c6dd7160f50
Benjamin Petetot <bpetetot@gmail.com>
2021-07-30 15:28:38 +0200

    [ORGA] Move IndicatorCard ui component

--- orga/tests/integration/components/ui/indicator-card-test.js
+++ orga/tests/integration/components/ui/indicator-card-test.js
@@ -3,37 +3,37 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | indicator-card', function(hooks) {
+module('Integration | Component | Ui::IndicatorCard', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders title', async function(assert) {
-    await render(hbs`<IndicatorCard @title="some title" />`);
+    await render(hbs`<Ui::IndicatorCard @title="some title" />`);
 
     assert.contains('some title');
   });
 
   test('it renders icon', async function(assert) {
-    await render(hbs`<IndicatorCard @icon="archive" />`);
+    await render(hbs`<Ui::IndicatorCard @icon="archive" />`);
 
     assert.dom('[data-icon="archive"]').exists();
   });
 
   test('it renders children', async function(assert) {
-    await render(hbs`<IndicatorCard>Text</IndicatorCard>`);
+    await render(hbs`<Ui::IndicatorCard>Text</Ui::IndicatorCard>`);
 
     assert.contains('Text');
   });
 
   module('when there is no additional information', function() {
     test('it does not display question mark and its tooltip', async function(assert) {
-      await render(hbs`<IndicatorCard>Text</IndicatorCard>`);
+      await render(hbs`<Ui::IndicatorCard>Text</Ui::IndicatorCard>`);
 
       assert.dom('[data-icon="question-circle"]').doesNotExist();
       assert.dom('[role="tooltip"]').doesNotExist();
     });
 
     test('it display question mark and its tooltip', async function(assert) {
-      await render(hbs`<IndicatorCard @info="some additional information">Text</IndicatorCard>`);
+      await render(hbs`<Ui::IndicatorCard @info="some additional information">Text</Ui::IndicatorCard>`);
 
       assert.dom('[data-icon="question-circle"]').exists();
       assert.dom('[role="tooltip"]').exists();
