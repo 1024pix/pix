0913e5a9591139656b6248399a8a04ec889b3023
yschoueri <yasmina.schoueri@gmail.com>
2021-07-30 15:27:40 +0200

    [APP] manage find and update sco record errors
    use conflict component when submitting student information, updating email, and clicking on change password link

--- mon-pix/tests/integration/components/account-recovery/conflict-error-test.js
+++ mon-pix/tests/integration/components/account-recovery/conflict-error-test.js
@@ -8,29 +8,49 @@ import { contains } from '../../../helpers/contains';
 describe('Integration | Component | conflict-error', function() {
   setupIntlRenderingTest();
 
-  it('should render a account recovery conflict error', async function() {
+  it('should render an account recovery conflict error', async function() {
     // given
     const firstName = 'Philippe';
-    this.set('firstName', firstName);
+    const title = this.intl.t('pages.account-recovery.find-sco-record.conflict.found-you-but', { firstName });
+    const errorMessage = this.intl.t('pages.account-recovery.find-sco-record.conflict.precaution');
+    this.set('title', title);
+    this.set('message', errorMessage);
 
     // when
-    await render(hbs`<AccountRecovery::ConflictError @firstName={{this.firstName}} />`);
+    await render(hbs`<AccountRecovery::ConflictError @title={{this.title}} @message={{this.message}} />`);
 
     // then
-    const expectedErrorMessage = this.intl.t('pages.account-recovery.find-sco-record.conflict.found-you-but', { firstName });
-    expect(contains(expectedErrorMessage)).to.exist;
-    expect(contains(this.intl.t('pages.account-recovery.find-sco-record.conflict.precaution'))).to.exist;
+    expect(contains(title)).to.exist;
+    expect(contains(errorMessage)).to.exist;
     expect(contains(this.intl.t('pages.account-recovery.find-sco-record.conflict.support.url-text'))).to.exist;
     expect(contains(this.intl.t('pages.account-recovery.find-sco-record.conflict.support.recover'))).to.exist;
   });
 
-  it('should redirect to support url on click on link', async function() {
-    // given
-    const firstName = 'Philippe';
-    this.set('firstName', firstName);
+  it('should redirect to renew account recovery demand when expired', async function() {
+    // given;
+    this.set('showRenewLink', true);
 
     // when
-    await render(hbs`<AccountRecovery::ConflictError @firstName={{this.firstName}} />`);
+    await render(hbs`<AccountRecovery::ConflictError @showRenewLink={{this.showRenewLink}} />`);
+
+    // then
+    expect(contains(this.intl.t('pages.account-recovery.errors.key-expired-renew-demand-link'))).to.exist;
+  });
+
+  it('should redirect to home when user has already left sco', async function() {
+    // given;
+    this.set('showReturnToHomeButton', true);
+
+    // when
+    await render(hbs`<AccountRecovery::ConflictError @showReturnToHomeButton={{this.showReturnToHomeButton}} />`);
+
+    // then
+    expect(contains(this.intl.t('navigation.back-to-homepage'))).to.exist;
+  });
+
+  it('should redirect to support url on click on link', async function() {
+    // given & when
+    await render(hbs`<AccountRecovery::ConflictError />`);
 
     // then
     expect(find('a').href).to.contains(this.intl.t('pages.account-recovery.find-sco-record.conflict.support.url'));
