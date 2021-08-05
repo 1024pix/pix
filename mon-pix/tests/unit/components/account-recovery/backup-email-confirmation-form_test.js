7eb73a62deb8aa5809c0715c01bb120667c49424
Christophe Charles <christophe.charles@octo.com>
2021-08-05 19:05:01 +0200

    [APP] Add isDisabled attribute to submit button

--- mon-pix/tests/unit/components/account-recovery/backup-email-confirmation-form_test.js
+++ mon-pix/tests/unit/components/account-recovery/backup-email-confirmation-form_test.js
@@ -1,5 +1,6 @@
-import sinon from 'sinon';
 import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import sinon from 'sinon';
 import { setupTest } from 'ember-mocha';
 import createGlimmerComponent from 'mon-pix/tests/helpers/create-glimmer-component';
 
@@ -7,7 +8,8 @@ describe('Unit | Component | account-recovery | backup-email-confirmation-form',
 
   setupTest();
 
-  context('#submitBackupEmailConfirmationForm', function() {
+  describe('#submitBackupEmailConfirmationForm', function() {
+
     it('should call sendEmail', function() {
       // given
       const sendEmail = sinon.stub();
@@ -21,6 +23,45 @@ describe('Unit | Component | account-recovery | backup-email-confirmation-form',
 
       // then
       sinon.assert.calledWith(sendEmail, 'john.doe@example.net');
+    });
+  });
+
+  describe('#isSubmitButtonEnabled', function() {
+
+    it('should return false if email is empty', function() {
+      // given
+      const component = createGlimmerComponent('component:account-recovery/backup-email-confirmation-form');
+      component.email = '';
+
+      // when
+      const result = component.isSubmitButtonEnabled;
+
+      // then
+      expect(result).to.be.false;
+    });
+
+    it('should return false if email is not valid', function() {
+      // given
+      const component = createGlimmerComponent('component:account-recovery/backup-email-confirmation-form');
+      component.email = 'wrongemail';
+
+      // when
+      const result = component.isSubmitButtonEnabled;
+
+      // then
+      expect(result).to.be.false;
+    });
+
+    it('should return true if email is valid', function() {
+      // given
+      const component = createGlimmerComponent('component:account-recovery/backup-email-confirmation-form');
+      component.email = 'user@example.net';
+
+      // when
+      const result = component.isSubmitButtonEnabled;
+
+      // then
+      expect(result).to.be.true;
     });
   });
 });
