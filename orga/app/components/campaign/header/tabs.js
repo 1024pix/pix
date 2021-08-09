6dc1f287df7721363d157a974f5a5827f196e251
Benjamin Petetot <bpetetot@gmail.com>
2021-08-09 17:11:46 +0200

    [ORGA] Create Campaign::Header::Tabs component

---
+++ orga/app/components/campaign/header/tabs.js
@@ -0,0 +1,11 @@
+import { inject as service } from '@ember/service';
+import Component from '@glimmer/component';
+
+export default class CampaignTabs extends Component {
+
+  @service currentUser;
+
+  get downloadUrl() {
+    return this.args.campaign.urlToResult + `&lang=${this.currentUser.prescriber.lang}`;
+  }
+}
