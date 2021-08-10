c69b82522d6680c3a296d75875e761b060c2ec47
Mathieu Gilet <mathieu.gilet@zenika.com>
2021-08-10 16:30:09 +0200

    feat(api): add seed in order to test PIX_EMPLOI_CLEA_V2

--- api/db/seeds/data/certification/badge-acquisition-builder.js
+++ api/db/seeds/data/certification/badge-acquisition-builder.js
@@ -1,11 +1,12 @@
-const { PIX_EMPLOI_CLEA_BADGE_ID } = require('../badges-builder');
+const { PIX_EMPLOI_CLEA_BADGE_ID, PIX_EMPLOI_CLEA_BADGE_ID_V2 } = require('../badges-builder');
 const {
-  CERTIF_REGULAR_USER1_ID, CERTIF_SUCCESS_USER_ID,
+  CERTIF_REGULAR_USER1_ID, CERTIF_SUCCESS_USER_ID, CERTIF_FAILURE_USER_ID,
 } = require('./users');
 
 function badgeAcquisitionBuilder({ databaseBuilder }) {
   databaseBuilder.factory.buildBadgeAcquisition({ userId: CERTIF_REGULAR_USER1_ID, badgeId: PIX_EMPLOI_CLEA_BADGE_ID });
   databaseBuilder.factory.buildBadgeAcquisition({ userId: CERTIF_SUCCESS_USER_ID, badgeId: PIX_EMPLOI_CLEA_BADGE_ID });
+  databaseBuilder.factory.buildBadgeAcquisition({ userId: CERTIF_FAILURE_USER_ID, badgeId: PIX_EMPLOI_CLEA_BADGE_ID_V2 });
 }
 
 module.exports = {
