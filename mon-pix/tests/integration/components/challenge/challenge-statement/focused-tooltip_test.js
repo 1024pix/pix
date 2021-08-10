bb11d89d8371a9be138893d0a523d96dcce8d001
alexandrecoin <alexandre.pc.coin@gmail.com>
2021-08-10 15:28:00 +0200

    bsr: refacto focused tooltip tests suite

--- mon-pix/tests/integration/components/challenge/challenge-statement/focused-tooltip_test.js
+++ mon-pix/tests/integration/components/challenge/challenge-statement/focused-tooltip_test.js
@@ -9,20 +9,11 @@ describe('Integration | Component | FocusedTooltip', function() {
 
   setupIntlRenderingTest();
 
-  function addChallengeToContext(component, challenge) {
-    component.set('challenge', challenge);
-  }
-
-  function renderFocusedTooltip(component) {
-    component.set('onTooltipClose', () => {});
-    return render(hbs`<Challenge::ChallengeStatement::FocusedTooltip @challenge={{this.challenge}} @onTooltipClose={{this.onTooltipClose}}/>`);
-  }
-
   const tooltip = '.challenge-statement__tag-information';
   const confirmationButton = '.challenge-statement-tag-information__button';
 
   describe('when user has not seen the tooltip yet', function() {
-    it('should render the tooltip with a confirmation button', async function() {
+    beforeEach(async function() {
       // given
       class currentUser extends Service {
         user = {
@@ -32,37 +23,23 @@ describe('Integration | Component | FocusedTooltip', function() {
       this.owner.unregister('service:currentUser');
       this.owner.register('service:currentUser', currentUser);
 
-      addChallengeToContext(this, {
+      this.set('challenge', {
         instruction: 'La consigne de mon test',
         id: 'rec_challenge',
         focused: true,
       });
+      this.set('onTooltipClose', () => {});
 
-      // when
-      await renderFocusedTooltip(this);
+      await render(hbs`<Challenge::ChallengeStatement::FocusedTooltip @challenge={{this.challenge}} @onTooltipClose={{this.onTooltipClose}}/>`);
+    });
 
+    it('should render the tooltip with a confirmation button', async function() {
       // then
       expect(find(tooltip)).to.exist;
       expect(find(confirmationButton)).to.exist;
     });
 
     it('should remove the tooltip when confirmation button has been clicked', async function() {
-      // given
-      class currentUser extends Service {
-        user = {
-          hasSeenFocusedChallengeTooltip: false,
-        }
-      }
-      this.owner.unregister('service:currentUser');
-      this.owner.register('service:currentUser', currentUser);
-
-      addChallengeToContext(this, {
-        instruction: 'La consigne de mon test',
-        id: 'rec_challenge',
-        focused: true,
-      });
-      await renderFocusedTooltip(this);
-
       // when
       await click('.challenge-statement-tag-information__button');
 
@@ -72,27 +49,29 @@ describe('Integration | Component | FocusedTooltip', function() {
   });
 
   describe('when user has seen the tooltip', function() {
+    beforeEach(async function() {
+      // given
+      class currentUser extends Service {
+        user = {
+          hasSeenFocusedChallengeTooltip: true,
+        }
+      }
+
+      this.owner.unregister('service:currentUser');
+      this.owner.register('service:currentUser', currentUser);
+
+      this.set('challenge', {
+        instruction: 'La consigne de mon test',
+        id: 'rec_challenge',
+        focused: true,
+      });
+      this.set('onTooltipClose', () => {});
+
+      await render(hbs`<Challenge::ChallengeStatement::FocusedTooltip @challenge={{this.challenge}} @onTooltipClose={{this.onTooltipClose}}/>`);
+    });
+
     describe('when the challenge starts', function() {
       it('should not render the tooltip', async function() {
-        // given
-        class currentUser extends Service {
-          user = {
-            hasSeenFocusedChallengeTooltip: true,
-          }
-        }
-
-        this.owner.unregister('service:currentUser');
-        this.owner.register('service:currentUser', currentUser);
-
-        addChallengeToContext(this, {
-          instruction: 'La consigne de mon test',
-          id: 'rec_challenge',
-          focused: true,
-        });
-
-        // when
-        await renderFocusedTooltip(this);
-
         // then
         expect(find(tooltip)).to.not.exist;
       });
@@ -101,24 +80,7 @@ describe('Integration | Component | FocusedTooltip', function() {
     describe('when the user hovers the challenge icon', function() {
       describe('when using a mouse', function() {
         it('should display the tooltip without a confirmation button when entering the icon', async function() {
-          // given
-          class currentUser extends Service {
-            user = {
-              hasSeenFocusedChallengeTooltip: true,
-            }
-          }
-
-          this.owner.unregister('service:currentUser');
-          this.owner.register('service:currentUser', currentUser);
-
-          addChallengeToContext(this, {
-            instruction: 'La consigne de mon test',
-            id: 'rec_challenge',
-            focused: true,
-          });
-
           // when
-          await renderFocusedTooltip(this);
           await triggerEvent('.challenge-statement-tag__icon-button', 'mouseenter');
 
           // then
@@ -127,24 +89,7 @@ describe('Integration | Component | FocusedTooltip', function() {
         });
 
         it('should the hide tooltip when mouse leaves the icon', async function() {
-          // given
-          class currentUser extends Service {
-            user = {
-              hasSeenFocusedChallengeTooltip: true,
-            }
-          }
-
-          this.owner.unregister('service:currentUser');
-          this.owner.register('service:currentUser', currentUser);
-
-          addChallengeToContext(this, {
-            instruction: 'La consigne de mon test',
-            id: 'rec_challenge',
-            focused: true,
-          });
-
           // when
-          await renderFocusedTooltip(this);
           await triggerEvent('.challenge-statement-tag__icon-button', 'mouseenter');
           await triggerEvent('.challenge-statement-tag__icon-button', 'mouseleave');
 
@@ -154,26 +99,6 @@ describe('Integration | Component | FocusedTooltip', function() {
       });
 
       describe('when using a keyboard', function() {
-
-        beforeEach(async function() {
-          class currentUser extends Service {
-            user = {
-              hasSeenFocusedChallengeTooltip: true,
-            }
-          }
-
-          this.owner.unregister('service:currentUser');
-          this.owner.register('service:currentUser', currentUser);
-
-          addChallengeToContext(this, {
-            instruction: 'La consigne de mon test',
-            id: 'rec_challenge',
-            focused: true,
-          });
-
-          await renderFocusedTooltip(this);
-        });
-
         it('should hide the tooltip button when escaping', async function() {
           // given
           await triggerEvent('.challenge-statement-tag__icon-button', 'mouseenter');
@@ -186,30 +111,12 @@ describe('Integration | Component | FocusedTooltip', function() {
           // then
           expect(find(tooltip)).to.not.exist;
         });
-
       });
     });
 
     describe('when the user clicks on the challenge icon', function() {
       it('should display the tooltip without a confirmation button when entering the icon', async function() {
-        // given
-        class currentUser extends Service {
-          user = {
-            hasSeenFocusedChallengeTooltip: true,
-          }
-        }
-
-        this.owner.unregister('service:currentUser');
-        this.owner.register('service:currentUser', currentUser);
-
-        addChallengeToContext(this, {
-          instruction: 'La consigne de mon test',
-          id: 'rec_challenge',
-          focused: true,
-        });
-
         // when
-        await renderFocusedTooltip(this);
         await click('.challenge-statement-tag__icon-button');
 
         // then
