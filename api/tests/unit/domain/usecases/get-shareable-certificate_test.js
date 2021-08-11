699bf6239c86ab8863a1f321e425f001c4ab9f56
P-Jeremy <jemyplu@gmail.com>
2021-08-11 11:46:19 +0200

    refactor(api): Remove service in getSharebleCertificate usecase

--- api/tests/unit/domain/usecases/get-shareable-certificate_test.js
+++ api/tests/unit/domain/usecases/get-shareable-certificate_test.js
@@ -6,43 +6,27 @@ describe('Unit | UseCase | get-shareable-certificate', () => {
   const shareableCertificateRepository = {
     getByVerificationCode: () => undefined,
   };
-  const resultCompetenceTreeService = {
-    computeForCertification: () => undefined,
-  };
-  const assessmentResultRepository = 'assessmentResultRepository';
-  const competenceTreeRepository = 'competenceTreeRepository';
 
   beforeEach(() => {
     shareableCertificateRepository.getByVerificationCode = sinon.stub();
-    resultCompetenceTreeService.computeForCertification = sinon.stub();
   });
 
   it('should return certification from verification code enhanced with resultCompetenceTree', async () => {
     // given
+    const resultCompetenceTree = domainBuilder.buildResultCompetenceTree();
     const shareableCertificate = domainBuilder.buildShareableCertificate({
       id: 1,
       verificationCode: 'P-123456CC',
-      resultCompetenceTree: null,
+      resultCompetenceTree,
     });
     shareableCertificateRepository.getByVerificationCode
       .withArgs('P-123456CC')
       .resolves(shareableCertificate);
-    const resultCompetenceTree = domainBuilder.buildResultCompetenceTree();
-    resultCompetenceTreeService.computeForCertification
-      .withArgs({
-        certificationId: 1,
-        assessmentResultRepository,
-        competenceTreeRepository,
-      })
-      .resolves(resultCompetenceTree);
 
     // when
     const finalShareableCertificate = await getCertificationByVerificationCode({
       verificationCode: 'P-123456CC',
       shareableCertificateRepository,
-      assessmentResultRepository,
-      competenceTreeRepository,
-      resultCompetenceTreeService,
     });
 
     // then
