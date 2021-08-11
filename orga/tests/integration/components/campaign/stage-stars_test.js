4abe7c1c3609d40bb014dea4c21e8a2611e7f607
Benjamin Petetot <bpetetot@gmail.com>
2021-08-11 11:24:40 +0200

    [ORGA] Move Campaign::StageStars component

--- orga/tests/integration/components/campaign/stage-stars_test.js
+++ orga/tests/integration/components/campaign/stage-stars_test.js
@@ -1,18 +1,18 @@
 import { module, test } from 'qunit';
-import setupIntlRenderingTest from '../../helpers/setup-intl-rendering';
+import setupIntlRenderingTest from '../../../helpers/setup-intl-rendering';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | stage-stars', function(hooks) {
+module('Integration | Component | Campaign::StageStars', function(hooks) {
   setupIntlRenderingTest(hooks);
 
   test('should render stars for given stages', async function(assert) {
     // given
-    this.set('result', 50);
-    this.set('stages', [{ threshold: 20 }, { threshold: 70 }]);
+    this.result = 50;
+    this.stages = [{ threshold: 20 }, { threshold: 70 }];
 
     // when
-    await render(hbs`<StageStars @result={{this.result}} @stages={{this.stages}} />`);
+    await render(hbs`<Campaign::StageStars @result={{this.result}} @stages={{this.stages}} />`);
 
     const acquiredStars = this.element.querySelectorAll('.pix-stars__acquired');
     const unacquiredStars = this.element.querySelectorAll('.pix-stars__unacquired');
@@ -24,11 +24,11 @@ module('Integration | Component | stage-stars', function(hooks) {
 
   test('should render a star only when threshold > 0 and reached', async function(assert) {
     // given
-    this.set('result', 50);
-    this.set('stages', [{ threshold: 0 }, { threshold: 20 }, { threshold: 70 }]);
+    this.result = 50;
+    this.stages = [{ threshold: 0 }, { threshold: 20 }, { threshold: 70 }];
 
     // when
-    await render(hbs`<StageStars @result={{this.result}} @stages={{this.stages}} />`);
+    await render(hbs`<Campaign::StageStars @result={{this.result}} @stages={{this.stages}} />`);
 
     const acquiredStars = this.element.querySelectorAll('.pix-stars__acquired');
     const unacquiredStars = this.element.querySelectorAll('.pix-stars__unacquired');
@@ -40,11 +40,11 @@ module('Integration | Component | stage-stars', function(hooks) {
 
   test('should render a screen reader message', async function(assert) {
     // given
-    this.set('result', 50);
-    this.set('stages', [{ threshold: 20 }, { threshold: 70 }]);
+    this.result = 50;
+    this.stages = [{ threshold: 20 }, { threshold: 70 }];
 
     // when
-    await render(hbs`<StageStars @result={{this.result}} @stages={{this.stages}} />`);
+    await render(hbs`<Campaign::StageStars @result={{this.result}} @stages={{this.stages}} />`);
 
     const srOnly = this.element.querySelector('.sr-only');
 
@@ -54,11 +54,11 @@ module('Integration | Component | stage-stars', function(hooks) {
 
   test('should not display tooltip by default', async function(assert) {
     // given
-    this.set('result', 50);
-    this.set('stages', [{ threshold: 20 }, { threshold: 70 }]);
+    this.result = 50;
+    this.stages = [{ threshold: 20 }, { threshold: 70 }];
 
     // when
-    await render(hbs`<StageStars @result={{this.result}} @stages={{this.stages}}/>`);
+    await render(hbs`<Campaign::StageStars @result={{this.result}} @stages={{this.stages}}/>`);
 
     // then
     assert.dom('[role="tooltip"]').doesNotExist();
@@ -66,11 +66,11 @@ module('Integration | Component | stage-stars', function(hooks) {
 
   test('should not display tooltip when no stage has been reached yet', async function(assert) {
     // given
-    this.set('result', undefined);
-    this.set('stages', [{ threshold: 20 }, { threshold: 70 }]);
+    this.result = undefined;
+    this.stages = [{ threshold: 20 }, { threshold: 70 }];
 
     // when
-    await render(hbs`<StageStars @result={{this.result}} @stages={{this.stages}}/>`);
+    await render(hbs`<Campaign::StageStars @result={{this.result}} @stages={{this.stages}}/>`);
 
     // then
     assert.dom('[role="tooltip"]').doesNotExist();
@@ -78,11 +78,11 @@ module('Integration | Component | stage-stars', function(hooks) {
 
   test('should render tooltip with reached stage prescriber title', async function(assert) {
     // given
-    this.set('result', 50);
-    this.set('stages', [{ threshold: 20, prescriberTitle: 'title' }, { threshold: 70 }]);
+    this.result = 50;
+    this.stages = [{ threshold: 20, prescriberTitle: 'title' }, { threshold: 70 }];
 
     // when
-    await render(hbs`<StageStars @result={{this.result}} @stages={{this.stages}} @withTooltip={{true}}/>`);
+    await render(hbs`<Campaign::StageStars @result={{this.result}} @stages={{this.stages}} @withTooltip={{true}}/>`);
 
     // then
     assert.dom('[role="tooltip"]').hasText('title');
@@ -90,11 +90,11 @@ module('Integration | Component | stage-stars', function(hooks) {
 
   test('should render tooltip with reached stage prescriber description', async function(assert) {
     // given
-    this.set('result', 50);
-    this.set('stages', [{ threshold: 20, prescriberDescription: 'description' }, { threshold: 70 }]);
+    this.result = 50;
+    this.stages = [{ threshold: 20, prescriberDescription: 'description' }, { threshold: 70 }];
 
     // when
-    await render(hbs`<StageStars @result={{this.result}} @stages={{this.stages}} @withTooltip={{true}}/>`);
+    await render(hbs`<Campaign::StageStars @result={{this.result}} @stages={{this.stages}} @withTooltip={{true}}/>`);
 
     // then
     assert.dom('[role="tooltip"]').hasText('description');
@@ -102,11 +102,11 @@ module('Integration | Component | stage-stars', function(hooks) {
 
   test('should render tooltip with reached stage prescriber title and description', async function(assert) {
     // given
-    this.set('result', 50);
-    this.set('stages', [{ threshold: 20, prescriberTitle: 'title', prescriberDescription: 'description' }, { threshold: 70 }]);
+    this.result = 50;
+    this.stages = [{ threshold: 20, prescriberTitle: 'title', prescriberDescription: 'description' }, { threshold: 70 }];
 
     // when
-    await render(hbs`<StageStars @result={{this.result}} @stages={{this.stages}} @withTooltip={{true}}/>`);
+    await render(hbs`<Campaign::StageStars @result={{this.result}} @stages={{this.stages}} @withTooltip={{true}}/>`);
 
     // then
     assert.dom('[role="tooltip"]').hasText('titledescription');
@@ -114,11 +114,11 @@ module('Integration | Component | stage-stars', function(hooks) {
 
   test('should not display tooltip when reached stage has no prescriber title nor description', async function(assert) {
     // given
-    this.set('result', 50);
-    this.set('stages', [{ threshold: 20 }, { threshold: 70 }]);
+    this.result = 50;
+    this.stages = [{ threshold: 20 }, { threshold: 70 }];
 
     // when
-    await render(hbs`<StageStars @result={{this.result}} @stages={{this.stages}} @withTooltip={{true}}/>`);
+    await render(hbs`<Campaign::StageStars @result={{this.result}} @stages={{this.stages}} @withTooltip={{true}}/>`);
 
     // then
     assert.dom('[role="tooltip"]').doesNotExist();
