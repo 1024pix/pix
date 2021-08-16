d279a3f01c2f21e68d16c85e8f2fdab02aa548b2
Laura Bergoens <laura.bergoens@pix.fr>
2021-08-16 08:07:24 +0200

    clean(api): stop checking for verif code when getting private certificate

--- api/lib/domain/usecases/certificate/get-private-certificate.js
+++ api/lib/domain/usecases/certificate/get-private-certificate.js
@@ -3,16 +3,8 @@ const { NotFoundError } = require('../../errors');
 module.exports = async function getPrivateCertificate({
   certificationId,
   userId,
-  certificationRepository,
   privateCertificateRepository,
-  verifyCertificateCodeService,
 }) {
-  const hasCode = await certificationRepository.hasVerificationCode(certificationId);
-  if (!hasCode) {
-    const code = await verifyCertificateCodeService.generateCertificateVerificationCode();
-    await certificationRepository.saveVerificationCode(certificationId, code);
-  }
-
   const privateCertificate = await privateCertificateRepository.get(certificationId);
   if (privateCertificate.userId !== userId) {
     throw new NotFoundError();
--- api/tests/unit/domain/usecases/get-private-certificate_test.js
+++ api/tests/unit/domain/usecases/get-private-certificate_test.js
@@ -3,29 +3,12 @@ const { NotFoundError } = require('../../../../lib/domain/errors');
 const getPrivateCertificate = require('../../../../lib/domain/usecases/certificate/get-private-certificate');
 
 describe('Unit | UseCase | getPrivateCertificate', async () => {
-
-  const certificationRepository = {
-    hasVerificationCode: undefined,
-    saveVerificationCode: undefined,
-  };
   const privateCertificateRepository = {
     get: () => undefined,
-  };
-  const verifyCertificateCodeService = {
-    generateCertificateVerificationCode: undefined,
-  };
-
-  const dependencies = {
-    certificationRepository,
-    privateCertificateRepository,
-    verifyCertificateCodeService,
   };
 
   beforeEach(() => {
     privateCertificateRepository.get = sinon.stub();
-    verifyCertificateCodeService.generateCertificateVerificationCode = sinon.stub();
-    certificationRepository.hasVerificationCode = sinon.stub();
-    certificationRepository.saveVerificationCode = sinon.stub();
   });
 
   context('when the user is not owner of the certification', async () => {
@@ -36,11 +19,10 @@ describe('Unit | UseCase | getPrivateCertificate', async () => {
         id: 123,
         userId: 789,
       });
-      certificationRepository.hasVerificationCode.withArgs(123).resolves(true);
       privateCertificateRepository.get.withArgs(123).resolves(privateCertificate);
 
       // when
-      const error = await catchErr(getPrivateCertificate)({ certificationId: 123, userId: 456, ...dependencies });
+      const error = await catchErr(getPrivateCertificate)({ certificationId: 123, userId: 456, privateCertificateRepository });
 
       // then
       expect(error).to.be.instanceOf(NotFoundError);
@@ -48,73 +30,23 @@ describe('Unit | UseCase | getPrivateCertificate', async () => {
   });
 
   context('when the user is owner of the certification', async () => {
-    context('certification has no verification code', () => {
 
-      it('should generate and save a verification code', async () => {
-        // given
-        const privateCertificate = domainBuilder.buildPrivateCertificate({
-          id: 123,
-          userId: 456,
-        });
-        certificationRepository.hasVerificationCode.withArgs(123).resolves(false);
-        privateCertificateRepository.get.withArgs(123).resolves(privateCertificate);
-        verifyCertificateCodeService.generateCertificateVerificationCode.resolves('P-SOMECODE');
-        certificationRepository.saveVerificationCode.resolves();
-
-        // when
-        await getPrivateCertificate({ certificationId: 123, userId: 456, ...dependencies });
-
-        // then
-        expect(verifyCertificateCodeService.generateCertificateVerificationCode).to.have.been.calledOnce;
-        expect(certificationRepository.saveVerificationCode).to.have.been.calledWithExactly(123, 'P-SOMECODE');
-      });
-    });
-
-    context('certification already has verification code', () => {
-
-      it('should not generate another verification code', async () => {
-        // given
-        const privateCertificate = domainBuilder.buildPrivateCertificate({
-          id: 123,
-          userId: 456,
-        });
-        certificationRepository.hasVerificationCode.withArgs(123).resolves(true);
-        privateCertificateRepository.get.withArgs(123).resolves(privateCertificate);
-        verifyCertificateCodeService.generateCertificateVerificationCode.rejects(new Error('I should not run.'));
-        certificationRepository.saveVerificationCode.resolves(new Error('I should not run.'));
-
-        // when
-        await getPrivateCertificate({ certificationId: 123, userId: 456, ...dependencies });
-
-        // then
-        expect(verifyCertificateCodeService.generateCertificateVerificationCode).to.not.have.been.called;
-        expect(certificationRepository.saveVerificationCode).to.not.have.been.called;
-      });
-    });
-
-    it('should get the private certificate enhanced with the result competence tree', async () => {
+    it('should get the private certificate', async () => {
       // given
-      const resultCompetenceTree = domainBuilder.buildResultCompetenceTree();
       const privateCertificate = domainBuilder.buildPrivateCertificate({
         id: 123,
         userId: 456,
-        resultCompetenceTree,
       });
-      certificationRepository.hasVerificationCode.withArgs(123).resolves(true);
-      privateCertificateRepository.get.withArgs(123).resolves(privateCertificate);
-      verifyCertificateCodeService.generateCertificateVerificationCode.rejects(new Error('I should not run.'));
-      certificationRepository.saveVerificationCode.resolves(new Error('I should not run.'));
       privateCertificateRepository.get.withArgs(123).resolves(privateCertificate);
 
       // when
-      const actualPrivateCertificate = await getPrivateCertificate({ certificationId: 123, userId: 456, ...dependencies });
+      const actualPrivateCertificate = await getPrivateCertificate({ certificationId: 123, userId: 456, privateCertificateRepository });
 
       // then
       const expectedPrivateCertificate = domainBuilder.buildPrivateCertificate({
         id: 123,
         userId: 456,
       });
-      expectedPrivateCertificate.setResultCompetenceTree(resultCompetenceTree);
       expect(actualPrivateCertificate).to.deep.equal(expectedPrivateCertificate);
     });
   });
