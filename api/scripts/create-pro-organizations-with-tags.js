b8414ac35a98d7f8b4b9a297254bfefb840fc8bd
Christophe Charles <christophe.charles@octo.com>
2021-08-16 12:03:24 +0200

    [API] Use helper methods to check required fields in csv

--- api/scripts/create-pro-organizations-with-tags.js
+++ api/scripts/create-pro-organizations-with-tags.js
@@ -1,11 +1,15 @@
 /* eslint-disable no-console */
+
 // Usage: node create-pro-organizations-with-tags.js path/file.csv
-// To use on file with columns |externalId, name, provinceCode, canCollectProfiles, credit, email, locale, tags|
+// To use on file with columns |externalId, name, provinceCode, canCollectProfiles, credit, email, locale, tags, createdBy|
 
 'use strict';
 require('dotenv').config();
 
-const { parseCsvWithHeader } = require('./helpers/csvHelpers');
+const {
+  checkCsvHeader,
+  parseCsvWithHeaderAndRequiredFields,
+} = require('./helpers/csvHelpers');
 
 const createProOrganizationsWithTags = require('../lib/domain/usecases/create-pro-organizations-with-tags');
 const domainTransaction = require('../lib/infrastructure/DomainTransaction');
@@ -14,14 +18,25 @@ const organizationRepository = require('../lib/infrastructure/repositories/organ
 const organizationTagRepository = require('../lib/infrastructure/repositories/organization-tag-repository');
 const tagRepository = require('../lib/infrastructure/repositories/tag-repository');
 
+const REQUIRED_FIELD_NAMES = ['createdBy'];
+
 async function main() {
   console.log('Starting creating PRO organizations.');
 
   try {
     const filePath = process.argv[2];
 
+    console.log('Reading and checking csv header file... ');
+    await checkCsvHeader({
+      filePath,
+      requiredFieldNames: REQUIRED_FIELD_NAMES,
+    });
+
     console.log('Reading and parsing csv data file... ');
-    const organizations = await parseCsvWithHeader(filePath);
+    const organizations = await parseCsvWithHeaderAndRequiredFields({
+      filePath,
+      requiredFieldNames: REQUIRED_FIELD_NAMES,
+    });
 
     console.log('Creating PRO organizations...');
     const createdOrganizations = await createProOrganizationsWithTags({
