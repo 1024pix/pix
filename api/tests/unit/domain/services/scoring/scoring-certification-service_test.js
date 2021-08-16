b73affb9dda46908153d731e3a1e7bb960e5804d
Mathieu Gilet <mathieu.gilet@zenika.com>
2021-08-16 15:59:53 +0200

    refactor(api): Returns certification assessment score rather than untyped object

--- api/tests/unit/domain/services/scoring/scoring-certification-service_test.js
+++ api/tests/unit/domain/services/scoring/scoring-certification-service_test.js
@@ -11,20 +11,18 @@ describe('Unit | Domain | services | scoring | scoring-certification-service', (
       const certificationAssessment = domainBuilder.buildCertificationAssessment();
       const competenceWithMark_1_1 = domainBuilder.buildCompetenceMark({ competence_code: '1.1', level: 0, score: 4, area_code: '1', competenceId: 'recComp1' });
       const competenceWithMark_1_2 = domainBuilder.buildCompetenceMark({ competence_code: '1.2', level: 1, score: 8, area_code: '2', competenceId: 'recComp2' });
-      const competencesWithMark = [competenceWithMark_1_1, competenceWithMark_1_2];
-      sinon.stub(certificationResultService, 'computeResult')
-        .resolves({ competencesWithMark, percentageCorrectAnswers: 55 });
-
-      const expectedAssessmentScore = domainBuilder.buildCertificationAssessmentScore({
+      const certificationAssessmentScore = domainBuilder.buildCertificationAssessmentScore({
+        competenceMarks: [competenceWithMark_1_1, competenceWithMark_1_2],
         percentageCorrectAnswers: 55,
-        competenceMarks: competencesWithMark,
       });
+      sinon.stub(certificationResultService, 'computeResult')
+        .resolves(certificationAssessmentScore);
 
       // when
       const assessmentScore = await scoringCertificationService.calculateCertificationAssessmentScore({ certificationAssessment });
 
       // then
-      expect(assessmentScore).to.deep.equal(expectedAssessmentScore);
+      expect(assessmentScore).to.deep.equal(certificationAssessmentScore);
     });
   });
 });
