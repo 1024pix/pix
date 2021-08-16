67f47e63b6adbdf16acb9c7c00cb5b54182fab53
Mathieu Gilet <mathieu.gilet@zenika.com>
2021-08-16 15:47:04 +0200

    refactor(api): Returns competence marks rather than untyped objects

--- api/tests/unit/domain/services/scoring/scoring-certification-service_test.js
+++ api/tests/unit/domain/services/scoring/scoring-certification-service_test.js
@@ -9,65 +9,19 @@ describe('Unit | Domain | services | scoring | scoring-certification-service', (
     it('should return an CertificationAssessmentScore', async function() {
       // given
       const certificationAssessment = domainBuilder.buildCertificationAssessment();
-      const competenceWithMark_1_1 = { index: '1.1', obtainedLevel: 0, obtainedScore: 4, area_code: '1', id: 'recComp1' };
-      const competenceWithMark_1_2 = { index: '1.2', obtainedLevel: 1, obtainedScore: 8, area_code: '2', id: 'recComp2' };
+      const competenceWithMark_1_1 = domainBuilder.buildCompetenceMark({ competence_code: '1.1', level: 0, score: 4, area_code: '1', competenceId: 'recComp1' });
+      const competenceWithMark_1_2 = domainBuilder.buildCompetenceMark({ competence_code: '1.2', level: 1, score: 8, area_code: '2', competenceId: 'recComp2' });
       const competencesWithMark = [competenceWithMark_1_1, competenceWithMark_1_2];
       sinon.stub(certificationResultService, 'computeResult')
         .resolves({ competencesWithMark, percentageCorrectAnswers: 55 });
-      const expectedCompetenceMark1 = domainBuilder.buildCompetenceMark({
-        level: 0,
-        score: 4,
-        area_code: '1',
-        competence_code: '1.1',
-        competenceId: 'recComp1',
-      });
-      const expectedCompetenceMark2 = domainBuilder.buildCompetenceMark({
-        level: 1,
-        score: 8,
-        area_code: '2',
-        competence_code: '1.2',
-        competenceId: 'recComp2',
-      });
+
       const expectedAssessmentScore = domainBuilder.buildCertificationAssessmentScore({
         percentageCorrectAnswers: 55,
-        competenceMarks: [expectedCompetenceMark1, expectedCompetenceMark2],
+        competenceMarks: competencesWithMark,
       });
 
       // when
       const assessmentScore = await scoringCertificationService.calculateCertificationAssessmentScore({ certificationAssessment });
-
-      // then
-      expect(assessmentScore).to.deep.equal(expectedAssessmentScore);
-    });
-
-    it('should ceil the level and the score to a maximum threshold', async () => {
-      // given
-      const MAX_REACHABLE_LEVEL = 5;
-      const MAX_REACHABLE_PIX = 40;
-      const certificationAssessment = domainBuilder.buildCertificationAssessment();
-      const competenceWithMarkAboveThreshold = {
-        id: 'recComp1',
-        index: '1.1',
-        obtainedLevel: MAX_REACHABLE_LEVEL + 1,
-        obtainedScore: MAX_REACHABLE_PIX + 10,
-        area_code: '1',
-      };
-      const expectedCompetenceMark = domainBuilder.buildCompetenceMark({
-        level: MAX_REACHABLE_LEVEL,
-        score: MAX_REACHABLE_PIX,
-        area_code: '1',
-        competence_code: '1.1',
-        competenceId: 'recComp1',
-      });
-      const expectedAssessmentScore = domainBuilder.buildCertificationAssessmentScore({
-        competenceMarks: [expectedCompetenceMark],
-      });
-      sinon.stub(certificationResultService, 'computeResult')
-        .withArgs({ certificationAssessment, continueOnError: false })
-        .resolves({ competencesWithMark: [competenceWithMarkAboveThreshold] });
-
-      // when
-      const assessmentScore = await scoringCertificationService.calculateCertificationAssessmentScore({ certificationAssessment, continueOnError: false });
 
       // then
       expect(assessmentScore).to.deep.equal(expectedAssessmentScore);
