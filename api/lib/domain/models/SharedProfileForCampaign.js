f36b8a4f94866c4df479014a128c64091f9f3ae2
Lise Quesnel <quesnel.lise@gmail.com>
2021-08-17 14:58:32 +0200

    [API] Compute canRetry for profiles collection based on activeness of participant

--- api/lib/domain/models/SharedProfileForCampaign.js
+++ api/lib/domain/models/SharedProfileForCampaign.js
@@ -7,13 +7,25 @@ class SharedProfileForCampaign {
     id,
     sharedAt,
     campaignAllowsRetry,
+    isRegistrationActive,
     scorecards = [],
   }) {
     this.id = id;
     this.sharedAt = sharedAt;
     this.scorecards = scorecards;
     this.pixScore = _.sumBy(this.scorecards, 'earnedPix') || 0;
-    this.canRetry = campaignAllowsRetry && moment().diff(this.sharedAt, 'days', true) >= constants.MINIMUM_DELAY_IN_DAYS_BEFORE_RETRYING;
+    this.canRetry = this._computeCanRetry(campaignAllowsRetry, sharedAt, isRegistrationActive);
+  }
+
+  _computeCanRetry(campaignAllowsRetry, sharedAt, isRegistrationActive) {
+    return campaignAllowsRetry
+      && this._timeBeforeRetryingPassed(sharedAt)
+      && isRegistrationActive;
+  }
+
+  _timeBeforeRetryingPassed(sharedAt) {
+    if (!sharedAt) return false;
+    return sharedAt && moment().diff(sharedAt, 'days', true) >= constants.MINIMUM_DELAY_IN_DAYS_BEFORE_RETRYING;
   }
 }
 
