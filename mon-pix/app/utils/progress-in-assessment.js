9a40e0a89f0f946c6909d9d0302b1a48ab28b0ab
melanieb <melanie.boudard@gmail.com>
2021-08-18 16:40:08 +0200

    [APP] Use the currentChallengeNumber from URL for progress-bar

--- mon-pix/app/utils/progress-in-assessment.js
+++ mon-pix/app/utils/progress-in-assessment.js
@@ -1,41 +1,29 @@
 import ENV from 'mon-pix/config/environment';
 const PREVIEW_UNIQUE_STEP = 1;
-const PREVIEW_UNIQUE_CURRENT_STEP_INDEX = 0;
 
 function getMaxStepsNumber(assessment) {
   if (assessment.isPreview) {
     return PREVIEW_UNIQUE_STEP;
   }
-
-  if (assessment.hasCheckpoints) {
-    return ENV.APP.NUMBER_OF_CHALLENGES_BETWEEN_TWO_CHECKPOINTS;
-  }
-
   if (assessment.isCertification) {
     return assessment.get('certificationCourse.nbChallenges');
   }
-
+  if (assessment.isDemo) {
+    return assessment.get('course.nbChallenges');
+  }
+  if (assessment.hasCheckpoints) {
+    return ENV.APP.NUMBER_OF_CHALLENGES_BETWEEN_TWO_CHECKPOINTS;
+  }
   return assessment.get('course.nbChallenges');
+
 }
 
-function getCurrentStepIndex(assessment, answerId) {
-  if (assessment.isPreview) {
-    return PREVIEW_UNIQUE_CURRENT_STEP_INDEX;
-  }
-  const persistedAnswersIds = assessment.hasMany('answers').ids().filter((id) => id != null);
-  const currentAnswerId = answerId;
-
-  let index = persistedAnswersIds.indexOf(currentAnswerId);
-
-  if (index === -1) {
-    index = persistedAnswersIds.length;
-  }
-
-  return index % getMaxStepsNumber(assessment);
+function getCurrentStepIndex(assessment, currentChallengeNumber) {
+  return currentChallengeNumber % getMaxStepsNumber(assessment);
 }
 
-function getCurrentStepNumber(assessment, answerId) {
-  return getCurrentStepIndex(assessment, answerId) + 1;
+function getCurrentStepNumber(assessment, currentChallengeNumber) {
+  return getCurrentStepIndex(assessment, currentChallengeNumber) + 1;
 }
 
 export {
