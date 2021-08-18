93033e8daad6f51387f5a6fd59d17b871c9e86b7
Anne-G <ang.schall@gmail.com>
2021-08-18 15:27:41 +0200

    [admin] add pixSelect on pagination control

--- admin/app/components/pagination-control.js
+++ admin/app/components/pagination-control.js
@@ -5,6 +5,12 @@ import Component from '@glimmer/component';
 export default class PaginationControl extends Component {
 
   @service router;
+  options = [
+    { value: '10', label: '10' },
+    { value: '25', label: '25' },
+    { value: '50', label: '50' },
+    { value: '100', label: '100' },
+  ];
 
   get nextPage() {
     return Math.min(this.args.pagination.page + 1, this.args.pagination.pageCount);
