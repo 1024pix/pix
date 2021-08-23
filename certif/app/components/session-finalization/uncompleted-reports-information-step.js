75ba64ade6079a2219715bc2896c1a1a9c9fe2ac
Antoine Ceol <antoine.ceol@gmail.com>
2021-08-23 18:58:06 +0200

    SR(certif): Use helper to escape html

--- certif/app/components/session-finalization/uncompleted-reports-information-step.js
+++ certif/app/components/session-finalization/uncompleted-reports-information-step.js
@@ -1,7 +1,6 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
-import { htmlSafe } from '@ember/string';
 
 export default class UncompletedReportsInformationStep extends Component {
 
@@ -43,11 +42,4 @@ export default class UncompletedReportsInformationStep extends Component {
   closeIssueReportsModal() {
     this.showIssueReportsModal = false;
   }
-
-  get tooltipMessage() {
-    const html = '<ul><li>Abandon du candidat :<ul><li>Le candidat est parti avant la fin de son test</li><li>Le candidat n\'est pas parvenu à répondre à l\'ensemble des questions du test dans le temps imparti, et n\'a pas passé les questions restantes jusqu\'à arriver à l\'écran de fin de test</li></ul><li>Problème technique :<ul><li>Problème technique sur la plateforme Pix, ou lié au réseau du centre de certification</li></ul></li></ul>';
-
-    return htmlSafe(html);
-  }
-
 }
