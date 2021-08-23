98a66dc33e66e698a3b0100d3af1a9ed04a5fcb2
Lise Quesnel <quesnel.lise@gmail.com>
2021-08-23 15:23:22 +0200

    [ORGA] SR - Delete useless attributes

--- orga/app/models/campaign-competence-collective-result.js
+++ orga/app/models/campaign-competence-collective-result.js
@@ -22,15 +22,7 @@ export default class CampaignCompetenceCollectiveResult extends Model {
   @belongsTo('campaign-collective-result')
   campaignCollectiveResult;
 
-  get roundedAverageValidatedSkills() {
-    return Math.round(this.averageValidatedSkills * 10) / 10;
-  }
-
   get validatedSkillsPercentage() {
     return Math.round(this.averageValidatedSkills * 100 / this.targetedSkillsCount);
-  }
-
-  get totalSkillsCountPercentage() {
-    return Math.round(this.targetedSkillsCount * 100 / this.campaignCollectiveResult.get('maxTotalSkillsCountInCompetences'));
   }
 }
--- orga/tests/unit/models/campaign-assessment-participation-result_test.js
+++ orga/tests/unit/models/campaign-assessment-participation-result_test.js
@@ -4,31 +4,6 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Model | campaignAssessmentParticipationResult', function(hooks) {
   setupTest(hooks);
 
-  module('maxTotalSkillsCount', function() {
-
-    test('should calculate max total skills', function(assert) {
-      const store = this.owner.lookup('service:store');
-      const competenceResult1 = store.createRecord('campaign-assessment-participation-competence-result', {
-        targetedSkillsCount: 2,
-      });
-      const competenceResult2 = store.createRecord('campaign-assessment-participation-competence-result', {
-        targetedSkillsCount: 11,
-      });
-      const competenceResult3 = store.createRecord('campaign-assessment-participation-competence-result', {
-        targetedSkillsCount: 10,
-      });
-
-      const model = store.createRecord('campaign-assessment-participation-result', {});
-      model.set('competenceResults', [competenceResult1, competenceResult2, competenceResult3]);
-
-      // when
-      const maxTotalSkillsCount = model.get('maxTotalSkillsCount');
-
-      // then
-      assert.equal(maxTotalSkillsCount, 11);
-    });
-  });
-
   module('sortedCompetenceResults', function() {
 
     test('should sort competence results', function(assert) {
