ad60ef7771a179969334c879fc5cd4225d0de93b
yahya youness <yahyayounes@hotmail.fr>
2021-08-26 14:28:50 +0200

    [API] change log level in http agent and add correlation id

--- api/lib/infrastructure/http/http-agent.js
+++ api/lib/infrastructure/http/http-agent.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-restricted-modules
 const axios = require('axios');
-const logger = require('../logger');
+const { logInfoWithCorrelationId, logErrorWithCorrelationId } = require('../../infrastructure/performance-tools');
 
 class HttpResponse {
   constructor({
@@ -16,14 +16,14 @@ class HttpResponse {
 
 module.exports = {
   async post({ url, payload, headers }) {
-    logger.debug(`Starting POST request to ${url}`);
+    logInfoWithCorrelationId(`Starting POST request to ${url}`);
 
     try {
       const httpResponse = await axios.post(url, payload, {
         headers,
       });
 
-      logger.debug(`End POST request to ${url} success: ${httpResponse.status}`);
+      logInfoWithCorrelationId(`End POST request to ${url} success: ${httpResponse.status}`);
 
       return new HttpResponse({
         code: httpResponse.status,
@@ -41,7 +41,7 @@ module.exports = {
         data = httpErr.message;
       }
 
-      logger.debug(`End POST request to ${url} error: ${code} ${data.toString()}`);
+      logErrorWithCorrelationId(`End POST request to ${url} error: ${code} ${data.toString()}`);
 
       return new HttpResponse({
         code,
@@ -51,13 +51,13 @@ module.exports = {
     }
   },
   async get({ url, payload, headers }) {
-    logger.debug(`Starting GET request to ${url}`);
+    logInfoWithCorrelationId(`Starting GET request to ${url}`);
 
     try {
       const config = { data: payload, headers };
       const httpResponse = await axios.get(url, config);
 
-      logger.debug(`End GET request to ${url} success: ${httpResponse.status}`);
+      logInfoWithCorrelationId(`End GET request to ${url} success: ${httpResponse.status}`);
 
       return new HttpResponse({
         code: httpResponse.status,
@@ -78,7 +78,7 @@ module.exports = {
         data = null;
       }
 
-      logger.debug(`End GET request to ${url} error: ${code}`);
+      logInfoWithCorrelationId(`End GET request to ${url} error: ${code}`);
 
       return new HttpResponse({
         code,
