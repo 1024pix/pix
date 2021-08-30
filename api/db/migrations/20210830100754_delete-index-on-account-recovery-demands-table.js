babb616657909e6ef9843504c84c165217a21150
Anne-G <ang.schall@gmail.com>
2021-08-30 15:15:43 +0200

    keep userId index because it's used

--- api/db/migrations/20210830100754_delete-index-on-account-recovery-demands-table.js
+++ api/db/migrations/20210830100754_delete-index-on-account-recovery-demands-table.js
@@ -2,7 +2,6 @@ const TABLE_NAME = 'account-recovery-demands';
 
 exports.up = function(knex) {
   return knex.schema.table(TABLE_NAME, function(table) {
-    table.dropIndex('userId');
     table.dropIndex('oldEmail');
     table.dropIndex('newEmail');
     table.dropIndex('temporaryKey');
@@ -11,7 +10,6 @@ exports.up = function(knex) {
 
 exports.down = function(knex) {
   return knex.schema.table(TABLE_NAME, function(table) {
-    table.index('userId');
     table.index('oldEmail');
     table.index('newEmail');
     table.index('temporaryKey');
