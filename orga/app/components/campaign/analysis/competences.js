cf9ad58203208dad210fa1298e0d2b16f4b408c9
Karam Mohd <k.haytham.mohammed@octo.com>
2021-08-31 20:16:51 +0200

    [ORGA][BSR] Fix the display of competence collective results count on Analys tab

--- orga/app/components/campaign/analysis/competences.js
+++ orga/app/components/campaign/analysis/competences.js
@@ -6,8 +6,9 @@ export default class CompetencesAnalysis extends Component {
   @service intl;
 
   get campaignCollectiveResultLabel() {
+    const competenceCollectiveResultsCount = this.args.campaignCollectiveResult.get('campaignCompetenceCollectiveResults').length;
     return htmlSafe(this.intl.t('pages.campaign-review.table.competences.column.competences',
-      { count: this.args.sharedParticipationsCount ? this.args.campaignCollectiveResult.get('campaignCompetenceCollectiveResults').length : '-' },
+      { count: competenceCollectiveResultsCount ? competenceCollectiveResultsCount : '-' },
     ));
   }
 }
