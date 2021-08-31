55ae9974459fb22cb7a241e369062bf6cc25b76d
Antoine Ceol <antoine.ceol@gmail.com>
2021-08-31 14:35:18 +0200

    tech(admin): Import pix-ui color variables

--- admin/ember-cli-build.js
+++ admin/ember-cli-build.js
@@ -4,6 +4,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   const app = new EmberApp(defaults, {
+    sassOptions: {
+      includePaths: [
+        'node_modules/pix-ui/addon/styles',
+      ],
+    },
     sourcemaps: { enabled: true },
     babel: {
       sourceMaps: 'inline',
@@ -37,6 +42,5 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-
   return app.toTree();
 };
