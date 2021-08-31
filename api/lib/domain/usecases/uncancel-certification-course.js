cf43b3fba714af063575a0d8c6add4b974f5f137
Laura Bergoens <laura.bergoens@pix.fr>
2021-08-31 16:31:51 +0200

    feat(api): create usecase to uncancel a certification

---
+++ api/lib/domain/usecases/uncancel-certification-course.js
@@ -0,0 +1,8 @@
+module.exports = async function uncancelCertificationCourse({
+  certificationCourseId,
+  certificationCourseRepository,
+}) {
+  const certificationCourse = await certificationCourseRepository.get(certificationCourseId);
+  certificationCourse.uncancel();
+  await certificationCourseRepository.update(certificationCourse);
+};
