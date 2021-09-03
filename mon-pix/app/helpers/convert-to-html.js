9bf267dcd143b4ef5542375233ff3c3f62a440df
Stephane Bedeau <sbedeau@octo.com>
2021-09-03 16:06:30 +0200

    fix(mon-pix): use xss to remove style from instructions

--- mon-pix/app/helpers/convert-to-html.js
+++ mon-pix/app/helpers/convert-to-html.js
@@ -1,11 +1,15 @@
 import { helper } from '@ember/component/helper';
 import showdown from 'showdown';
+import xss from 'xss';
 import isArray from 'lodash/isArray';
 
 export function convertToHtml(params) {
   if (isArray(params) && params.length > 0) {
     const converter = new showdown.Converter();
-    return converter.makeHtml(params[0]);
+    const filteredOutHtml = xss(params[0], {
+      stripIgnoreTagBody: ['style'],
+    });
+    return converter.makeHtml(filteredOutHtml);
   }
   return '';
 }
