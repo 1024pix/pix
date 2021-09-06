04475957d1072ab4d951e73c2187df73f3036919
Anne-G <ang.schall@gmail.com>
2021-09-06 10:37:13 +0200

    [app] update button in certification joiner

--- mon-pix/tests/helpers/certification.js
+++ mon-pix/tests/helpers/certification.js
@@ -1,13 +1,14 @@
 import { click, fillIn } from '@ember/test-helpers';
+import { clickByLabel } from './click-by-label';
 
-export async function fillCertificationJoiner({ sessionId, firstName, lastName, dayOfBirth, monthOfBirth, yearOfBirth }) {
+export async function fillCertificationJoiner({ sessionId, firstName, lastName, dayOfBirth, monthOfBirth, yearOfBirth, intl }) {
   await fillIn('#certificationJoinerSessionId', sessionId);
   await fillIn('#certificationJoinerFirstName', firstName);
   await fillIn('#certificationJoinerLastName', lastName);
   await fillIn('#certificationJoinerDayOfBirth', dayOfBirth);
   await fillIn('#certificationJoinerMonthOfBirth', monthOfBirth);
   await fillIn('#certificationJoinerYearOfBirth', yearOfBirth);
-  await click('.certification-joiner__attempt-next-btn');
+  await clickByLabel(intl.t('pages.certification-joiner.form.actions.submit'));
 }
 
 export async function fillCertificationStarter({ accessCode }) {
