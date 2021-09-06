4228c5fa56ed54f6023f960d024fe4298fab802f
Clément Bourgoin <clement.bourgoin@pix.fr>
2021-09-06 17:28:18 +0200

    feat(api): add route /admin/api/sessions/{id}/comment

--- api/lib/application/sessions/index.js
+++ api/lib/application/sessions/index.js
@@ -483,6 +483,27 @@ exports.register = async (server) => {
     },
     {
       method: 'PUT',
+      path: '/api/admin/sessions/{id}/comment',
+      config: {
+        validate: {
+          params: Joi.object({
+            id: identifiersType.sessionId,
+          }),
+        },
+        pre: [{
+          method: securityPreHandlers.checkUserHasRolePixMaster,
+          assign: 'hasRolePixMaster',
+        }],
+        handler: sessionController.commentAsJury,
+        notes: [
+          '- **Cette route est restreinte aux utilisateurs authentifiés avec le rôle PixMaster**\n' +
+          '- Ajoute/modifie un commentaire d\'un membre du pôle certification (certification-officer)',
+        ],
+        tags: ['api', 'session', 'assignment'],
+      },
+    },
+    {
+      method: 'PUT',
       path: '/api/sessions/{id}/enroll-students-to-session',
       config: {
         validate: {
