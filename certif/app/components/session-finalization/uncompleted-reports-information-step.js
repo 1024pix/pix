6c1c6f5d9f8e8e7811f0613c7afe20009460c468
Antoine Ceol <antoine.ceol@gmail.com>
2021-09-07 16:59:49 +0200

    feat(certif): Handle success/error message on abort reason save

--- certif/app/components/session-finalization/uncompleted-reports-information-step.js
+++ certif/app/components/session-finalization/uncompleted-reports-information-step.js
@@ -1,12 +1,14 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 export default class UncompletedReportsInformationStep extends Component {
 
   @tracked reportToEdit = null;
   @tracked showAddIssueReportModal = false;
   @tracked showIssueReportsModal = false;
+  @service notifications;
 
   get certificationReportsAreNotEmpty() {
     return this.args.certificationReports.length !== 0;
