d40827eebd8a77c5ec1dac86bbde7389d42c30f4
Yvonnick FRIN <frin.yvonnick@gmail.com>
2021-09-09 16:07:27 +0200

    Add an integration test on usecase begin-campaign-participation-improvement

---
+++ api/tests/integration/domain/usecases/begin-campaign-participation-improvement.js
@@ -0,0 +1,20 @@
+const { expect, databaseBuilder, knex } = require('../../../test-helper');
+const beginCampaignParticipationImprovement = require('../../../../lib/domain/usecases/begin-campaign-participation-improvement');
+const assessmentRepository = require('../../../../lib/infrastructure/repositories/assessment-repository');
+const campaignParticipationRepository = require('../../../../lib/infrastructure/repositories/campaign-participation-repository');
+
+describe('Integration | UseCase | begin-campaign-participation-improvement', function() {
+  it('should change campaignParticipation status to STARTED', async function() {
+    const campaignParticipation = databaseBuilder.factory.buildCampaignParticipation({ status: 'TO_SHARE', isShared: false, sharedAt: null });
+    databaseBuilder.factory.buildAssessment({ userId: campaignParticipation.userId, campaignParticipationId: campaignParticipation.id, type: 'CAMPAIGN' });
+    await databaseBuilder.commit();
+
+    await beginCampaignParticipationImprovement({ campaignParticipationRepository, assessmentRepository, campaignParticipationId: campaignParticipation.id, userId: campaignParticipation.userId });
+
+    const [campaignParticipationFound] = await knex('campaign-participations')
+      .where({ id: campaignParticipation.id });
+
+    expect(campaignParticipationFound.status).to.equal('STARTED');
+  });
+});
+
