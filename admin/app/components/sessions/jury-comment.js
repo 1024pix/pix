a9a596cdd21f0a241a896f7ba45921b1aec4fbdc
Clément Bourgoin <clement.bourgoin@pix.fr>
2021-09-13 18:17:18 +0200

    feat(admin): display error notification if save fails

--- admin/app/components/sessions/jury-comment.js
+++ admin/app/components/sessions/jury-comment.js
@@ -1,38 +1,45 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import noop from 'lodash/noop';
 
 export default class JuryComment extends Component {
   @tracked editingMode = false;
-  @tracked displayedComment;
-  @tracked editedComment;
+  @tracked comment;
+  @tracked commentBeingEdited;
 
   constructor() {
     super(...arguments);
-    this.displayedComment = this.args.comment;
-    this.editedComment = this.args.comment;
+    this.comment = this.args.comment;
+    this.commentBeingEdited = this.args.comment;
   }
 
   @action
   async submitForm(event) {
     event.preventDefault();
-    await this.args.onFormSubmit(this.editedComment);
-    this.exitEditingMode();
+    try {
+      await this.args.onFormSubmit(this.commentBeingEdited);
+      this.comment = this.commentBeingEdited;
+      this.exitEditingMode();
+    } catch {
+      noop();
+    }
   }
 
   @action
   enterEditingMode() {
     this.editingMode = true;
+    this.commentBeingEdited = this.comment;
   }
 
   @action
   exitEditingMode() {
     this.editingMode = false;
-    this.editedComment = this.displayedComment;
+    this.commentBeingEdited = '';
   }
 
   get commentExists() {
-    return this.args.comment !== null;
+    return this.comment !== null;
   }
 
   get shouldDisplayForm() {
