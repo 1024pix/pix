2337d7da28c83ae29e5832c2131a3f19fcd46aed
Clément Bourgoin <clement.bourgoin@pix.fr>
2021-09-13 15:40:03 +0200

    feat(admin): leave comment unchanged if user cancels edition

--- admin/app/components/sessions/jury-comment.js
+++ admin/app/components/sessions/jury-comment.js
@@ -4,18 +4,20 @@ import { action } from '@ember/object';
 
 export default class JuryComment extends Component {
   @tracked editingMode = false;
-  @tracked commentText;
+  @tracked displayedComment;
+  @tracked editedComment;
 
   constructor() {
     super(...arguments);
-    this.commentText = this.args.comment;
+    this.displayedComment = this.args.comment;
+    this.editedComment = this.args.comment;
   }
 
   @action
-  submitForm(event) {
+  async submitForm(event) {
     event.preventDefault();
+    await this.args.onFormSubmit(this.editedComment);
     this.exitEditingMode();
-    this.args.onFormSubmit(this.commentText);
   }
 
   @action
@@ -26,6 +28,7 @@ export default class JuryComment extends Component {
   @action
   exitEditingMode() {
     this.editingMode = false;
+    this.editedComment = this.displayedComment;
   }
 
   get commentExists() {
