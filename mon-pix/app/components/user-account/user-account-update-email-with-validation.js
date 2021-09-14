f0181b77aad0ac5fd36b673ba18056e99cb4d13f
Andreia Pena <andreia.pena@pix.fr>
2021-09-14 15:44:09 +0200

    [APP] Change FormTextfield to PixInputPassword component

--- mon-pix/app/components/user-account/user-account-update-email-with-validation.js
+++ mon-pix/app/components/user-account/user-account-update-email-with-validation.js
@@ -5,20 +5,10 @@ import { tracked } from '@glimmer/tracking';
 import isEmailValid from '../../utils/email-validator';
 import isEmpty from 'lodash/isEmpty';
 
-const STATUS_MAP = {
-  defaultStatus: 'default',
-  errorStatus: 'error',
-};
-
 const ERROR_INPUT_MESSAGE_MAP = {
   invalidEmail: 'pages.user-account.account-update-email-with-validation.fields.errors.invalid-email',
   emptyPassword: 'pages.user-account.account-update-email-with-validation.fields.errors.empty-password',
 };
-
-class PasswordValidation {
-  @tracked status = STATUS_MAP['defaultStatus'];
-  @tracked message = null;
-}
 
 export default class UserAccountUpdateEmailWithValidation extends Component {
 
@@ -26,8 +16,7 @@ export default class UserAccountUpdateEmailWithValidation extends Component {
   @tracked newEmail = '';
   @tracked password = '';
   @tracked newEmailValidationMessage = null;
-
-  @tracked passwordValidation = new PasswordValidation();
+  @tracked passwordValidationMessage = null;
 
   @action
   validateNewEmail(event) {
@@ -45,12 +34,10 @@ export default class UserAccountUpdateEmailWithValidation extends Component {
   validatePassword() {
     const isInvalidInput = isEmpty(this.password);
 
-    this.passwordValidation.status = STATUS_MAP['defaultStatus'];
-    this.passwordValidation.message = null;
+    this.passwordValidationMessage = null;
 
     if (isInvalidInput) {
-      this.passwordValidation.status = STATUS_MAP['errorStatus'];
-      this.passwordValidation.message = this.intl.t(ERROR_INPUT_MESSAGE_MAP['emptyPassword']);
+      this.passwordValidationMessage = this.intl.t(ERROR_INPUT_MESSAGE_MAP['emptyPassword']);
     }
   }
 }
