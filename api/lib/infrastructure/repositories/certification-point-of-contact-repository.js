9b2025a8ec3a8852037667c0654095604dbb0cca
Laura Bergoens <laura.bergoens@pix.fr>
2021-09-14 08:43:28 +0200

    refacto(api): associate newly created model in CertificationPointOfContact model

--- api/lib/infrastructure/repositories/certification-point-of-contact-repository.js
+++ api/lib/infrastructure/repositories/certification-point-of-contact-repository.js
@@ -1,37 +1,66 @@
+const _ = require('lodash');
 const { knex } = require('../bookshelf');
 const { NotFoundError } = require('../../domain/errors');
 const CertificationPointOfContact = require('../../domain/read-models/CertificationPointOfContact');
+const AllowedCertificationCenterAccess = require('../../domain/read-models/AllowedCertificationCenterAccess');
 
 module.exports = {
+
   async get(userId) {
-    const result = await knex
+    const certificationPointOfContactDTO = await knex
       .select({
         id: 'users.id',
         firstName: 'users.firstName',
         lastName: 'users.lastName',
         email: 'users.email',
         pixCertifTermsOfServiceAccepted: 'users.pixCertifTermsOfServiceAccepted',
-        // eslint-disable-next-line no-restricted-syntax
-        certificationCenters: knex.raw(`JSON_AGG(JSON_BUILD_OBJECT(
-          'id', "certification-centers"."id",
-          'name', "certification-centers"."name",
-          'type', "certification-centers"."type",
-          'externalId', "certification-centers"."externalId",
-          'isRelatedOrganizationManagingStudents', "organizations"."isManagingStudents"
-        ) ORDER BY "certification-center-memberships"."createdAt" DESC)`),
+        certificationCenterIds: (knex.raw('array_agg(??)', 'certification-center-memberships.certificationCenterId')),
       })
       .from('users')
       .join('certification-center-memberships', 'certification-center-memberships.userId', 'users.id')
-      .join('certification-centers', 'certification-centers.id', 'certification-center-memberships.certificationCenterId')
-      .leftJoin('organizations', 'organizations.externalId', 'certification-centers.externalId')
       .where('users.id', userId)
-      .groupBy('users.id')
+      .groupByRaw('1, 2, 3, 4, 5')
       .first();
 
-    if (!result) {
+    if (!certificationPointOfContactDTO) {
       throw new NotFoundError(`Le référent de certification ${userId} n'existe pas.`);
     }
 
-    return new CertificationPointOfContact(result);
+    const certificationCenterIds = _.compact(certificationPointOfContactDTO.certificationCenterIds);
+    const allowedCertificationCenterAccesses = await _findAllowedCertificationCenterAccesses(certificationCenterIds);
+
+    return new CertificationPointOfContact({
+      ...certificationPointOfContactDTO,
+      allowedCertificationCenterAccesses,
+    });
   },
 };
+
+async function _findAllowedCertificationCenterAccesses(certificationCenterIds) {
+  const allowedCertificationCenterAccessDTOs = await knex
+    .select({
+      id: 'certification-centers.id',
+      name: 'certification-centers.name',
+      externalId: 'certification-centers.externalId',
+      type: 'certification-centers.type',
+      isRelatedToManagingStudentsOrganization: 'organizations.isManagingStudents',
+      tags: (knex.raw('array_agg(?? order by ??)', ['tags.name', 'tags.name'])),
+    })
+    .from('certification-centers')
+    .leftJoin('organizations', 'organizations.externalId', 'certification-centers.externalId')
+    .leftJoin('organization-tags', 'organization-tags.organizationId', 'organizations.id')
+    .leftJoin('tags', 'tags.id', 'organization-tags.tagId')
+    .whereIn('certification-centers.id', certificationCenterIds)
+    .orderBy('certification-centers.id')
+    .groupByRaw('1, 2, 3, 4, 5');
+
+  return _.map(allowedCertificationCenterAccessDTOs, (allowedCertificationCenterAccessDTO) => {
+    const tags = _.compact(allowedCertificationCenterAccessDTO.tags);
+    return new AllowedCertificationCenterAccess({
+      ...allowedCertificationCenterAccessDTO,
+      isRelatedToManagingStudentsOrganization: Boolean(allowedCertificationCenterAccessDTO.isRelatedToManagingStudentsOrganization),
+      relatedOrganizationTags: tags,
+    });
+  });
+}
+
--- api/lib/infrastructure/serializers/jsonapi/certification-point-of-contact-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/certification-point-of-contact-serializer.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const { Serializer } = require('jsonapi-serializer');
 
 module.exports = {
@@ -5,15 +6,42 @@ module.exports = {
   serialize(certificationPointOfContact) {
     return new Serializer('certification-point-of-contact', {
       attributes: [
-        'firstName', 'lastName', 'email', 'pixCertifTermsOfServiceAccepted', 'currentCertificationCenterId',
-        'certificationCenters',
+        'firstName',
+        'lastName',
+        'email',
+        'pixCertifTermsOfServiceAccepted',
+        'allowedCertificationCenterAccesses',
       ],
-      certificationCenters: {
+      allowedCertificationCenterAccesses: {
         ref: 'id',
         included: true,
         attributes: [
-          'name', 'type', 'externalId', 'isRelatedOrganizationManagingStudents',
+          'name',
+          'externalId',
+          'type',
+          'isRelatedToManagingStudentsOrganization',
+          'isAccessBlockedCollege',
+          'isAccessBlockedLycee',
+          'relatedOrganizationTags',
         ],
+      },
+      typeForAttribute: function(attribute) {
+        if (attribute === 'allowedCertificationCenterAccesses') {
+          return 'allowed-certification-center-access';
+        }
+        return attribute;
+      },
+      transform(certificationPointOfContact) {
+        const transformedCertificationPointOfContact = _.clone(certificationPointOfContact);
+        transformedCertificationPointOfContact.allowedCertificationCenterAccesses = _.map(certificationPointOfContact.allowedCertificationCenterAccesses, (access) => {
+          return {
+            ...access,
+            isAccessBlockedCollege: access.isAccessBlockedCollege(),
+            isAccessBlockedLycee: access.isAccessBlockedLycee(),
+          };
+        });
+
+        return transformedCertificationPointOfContact;
       },
     }).serialize(certificationPointOfContact);
   },
