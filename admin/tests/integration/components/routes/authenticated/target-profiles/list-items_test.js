57356b147bea1b99f21821f742bea76e81a2db23
Karam Mohd <k.haytham.mohammed@octo.com>
2021-09-17 15:37:57 +0200

    [ADMIN] Add status coloumn to target profile table

--- admin/tests/integration/components/routes/authenticated/target-profiles/list-items_test.js
+++ admin/tests/integration/components/routes/authenticated/target-profiles/list-items_test.js
@@ -14,16 +14,17 @@ module('Integration | Component | routes/authenticated/target-profiles | list-it
     this.goToTargetProfilePage = goToTargetProfilePage;
   });
 
-  test('it should display header with name and id', async function(assert) {
+  test('it should display header with name, id and status', async function(assert) {
     // when
     await render(hbs`<TargetProfiles::ListItems @triggerFiltering={{this.triggerFiltering}} @goToTargetProfilePage={{this.goToTargetProfilePage}}/>`);
 
     // then
     assert.contains('ID');
     assert.contains('Nom');
+    assert.contains('Statut');
   });
 
-  test('if should display search inputs', async function(assert) {
+  test('it should display search inputs', async function(assert) {
     // when
     await render(hbs`<TargetProfiles::ListItems @triggerFiltering={{this.triggerFiltering}} @goToTargetProfilePage={{this.goToTargetProfilePage}}/>`);
 
@@ -65,5 +66,35 @@ module('Integration | Component | routes/authenticated/target-profiles | list-it
     // then
     assert.dom('[aria-label="Profil cible"]').containsText(123);
     assert.dom('[aria-label="Profil cible"]').containsText('Profile Cible 1');
+  });
+
+  test('it should display target profile status as "Obsolète" when target profile is outdated', async function(assert) {
+    // given
+    const targetProfiles = [{ id: 123, name: 'Profile Cible - outdated', outdated: true }];
+    targetProfiles.meta = {
+      rowCount: 2,
+    };
+    this.targetProfiles = targetProfiles;
+
+    // when
+    await render(hbs`<TargetProfiles::ListItems @targetProfiles={{this.targetProfiles}} @triggerFiltering={{this.triggerFiltering}} @goToTargetProfilePage={{this.goToTargetProfilePage}}/>`);
+
+    // then
+    assert.contains('Obsolète');
+  });
+
+  test('it should display target profile status as "Actif" when target profile is not outdated', async function(assert) {
+    // given
+    const targetProfiles = [{ id: 123, name: 'Profile Cible - active', outdated: false }];
+    targetProfiles.meta = {
+      rowCount: 2,
+    };
+    this.targetProfiles = targetProfiles;
+
+    // when
+    await render(hbs`<TargetProfiles::ListItems @targetProfiles={{this.targetProfiles}} @triggerFiltering={{this.triggerFiltering}} @goToTargetProfilePage={{this.goToTargetProfilePage}}/>`);
+
+    // then
+    assert.contains('Actif');
   });
 });
