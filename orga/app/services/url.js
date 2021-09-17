3fde44922e2478f20074dfaabdb20d34f2f29c51
Lise Quesnel <quesnel.lise@gmail.com>
2021-09-17 16:51:29 +0200

    [ORGA] Add accessibilityUrl getter in url service

--- orga/app/services/url.js
+++ orga/app/services/url.js
@@ -36,4 +36,12 @@ export default class Url extends Service {
     }
     return `https://pix.${this.currentDomain.getExtension()}/mentions-legales`;
   }
+
+  get accessibilityUrl() {
+    const currentLanguage = this.intl.t('current-lang');
+    if (currentLanguage === 'en') {
+      return 'https://pix.org/en-gb/accessibility-pix-orga';
+    }
+    return `https://pix.${this.currentDomain.getExtension()}/accessibilite-pix-orga`;
+  }
 }
