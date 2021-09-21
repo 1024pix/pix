43afbc4bab7ad34543bf60c6e9ec180745a5f323
François de Metz <francois.de-metz@pix.fr>
2021-09-21 15:36:18 +0200

    Add mirage route to handle last-challenge-state/focusedout

--- mon-pix/mirage/routes/assessments/index.js
+++ mon-pix/mirage/routes/assessments/index.js
@@ -3,6 +3,7 @@ import findAssessments from './find-assessments';
 import getAssessment from './get-assessment';
 import getNextChallenge from './get-next-challenge';
 import postAssessments from './post-assessments';
+import setFocusedOutState from './set-focusedout-state';
 
 export default function index(config) {
   config.get('/assessments', findAssessments);
@@ -13,4 +14,5 @@ export default function index(config) {
   config.get('/assessments/:id/next', getNextChallenge);
 
   config.patch('/assessments/:id/complete-assessment', completeAssessment);
+  config.patch('/assessments/:id/last-challenge-state/focusedout', setFocusedOutState);
 }
