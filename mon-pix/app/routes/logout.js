4df2cf2f64a0e5b5efa8e708b81ac8588fddc570
Mathieu Gilet <mathieu.gilet@zenika.com>
2021-09-23 15:13:47 +0200

    Simplify logout page management

--- mon-pix/app/routes/logout.js
+++ mon-pix/app/routes/logout.js
@@ -5,7 +5,6 @@ import ENV from 'mon-pix/config/environment';
 import get from 'lodash/get';
 
 const AUTHENTICATED_SOURCE_FROM_MEDIACENTRE = ENV.APP.AUTHENTICATED_SOURCE_FROM_MEDIACENTRE;
-const AUTHENTICATED_SOURCE_FROM_POLE_EMPLOI = ENV.APP.AUTHENTICATED_SOURCE_FROM_POLE_EMPLOI;
 
 export default class LogoutRoute extends Route {
   @service session;
@@ -14,7 +13,6 @@ export default class LogoutRoute extends Route {
 
   beforeModel() {
     const session = this.session;
-    this.source = session.data.authenticated.source;
     delete session.data.externalUser;
     this.campaignStorage.clearAll();
 
@@ -24,23 +22,12 @@ export default class LogoutRoute extends Route {
         return session.singleLogout(id_token);
       }
 
+      if (session.data.authenticated.source === AUTHENTICATED_SOURCE_FROM_MEDIACENTRE) {
+        session.alternativeRootURL = '/nonconnecte';
+      } else {
+        session.alternativeRootURL = null;
+      }
       return session.invalidate();
     }
-  }
-
-  afterModel() {
-    if (this.source === AUTHENTICATED_SOURCE_FROM_MEDIACENTRE) {
-      return this._redirectToDisconnectedPage();
-    } else if (this.source !== AUTHENTICATED_SOURCE_FROM_POLE_EMPLOI) {
-      return this._redirectToHome();
-    }
-  }
-
-  _redirectToDisconnectedPage() {
-    return this.transitionTo('not-connected');
-  }
-
-  _redirectToHome() {
-    return window.location.replace(this.url.homeUrl);
   }
 }
--- mon-pix/tests/unit/routes/logout_test.js
+++ mon-pix/tests/unit/routes/logout_test.js
@@ -2,11 +2,12 @@ import Service from '@ember/service';
 import sinon from 'sinon';
 import { describe, it } from 'mocha';
 import { setupTest } from 'ember-mocha';
+import { expect } from 'chai';
 import ENV from 'mon-pix/config/environment';
 
 const AUTHENTICATED_SOURCE_FROM_MEDIACENTRE = ENV.APP.AUTHENTICATED_SOURCE_FROM_MEDIACENTRE;
 
-describe('Unit | Route | logout', () => {
+describe('Unit | Route | logout', function() {
   setupTest();
 
   let sessionStub;
@@ -37,42 +38,27 @@ describe('Unit | Route | logout', () => {
     sinon.assert.calledOnce(invalidateStub);
   });
 
-  it('should redirect to home when source of connexion is pix', function() {
+  it('should set an alternative disconnection route when source of connexion is external', function() {
     // Given
     const invalidateStub = sinon.stub();
-
-    sessionStub = Service.create({ isAuthenticated: true, invalidate: invalidateStub });
+    sessionStub = Service.create({
+      isAuthenticated: true,
+      invalidate: invalidateStub,
+      data: {
+        authenticated: {
+          source: AUTHENTICATED_SOURCE_FROM_MEDIACENTRE,
+        },
+      },
+    });
 
     const route = this.owner.lookup('route:logout');
     route.set('session', sessionStub);
-    route.set('campaignStorage', campaignStorageStub);
-    route._redirectToHome = sinon.stub();
-    route.source = 'pix';
 
     // When
-    route.afterModel();
+    route.beforeModel();
 
     // Then
-    sinon.assert.calledOnce(route._redirectToHome);
-  });
-
-  it('should redirect to disconnected page when source of connexion is external', function() {
-    // Given
-    const invalidateStub = sinon.stub();
-
-    sessionStub = Service.create({ isAuthenticated: true, invalidate: invalidateStub });
-
-    const route = this.owner.lookup('route:logout');
-    route.set('session', sessionStub);
-    route.set('campaignStorage', campaignStorageStub);
-    route._redirectToDisconnectedPage = sinon.stub();
-    route.source = AUTHENTICATED_SOURCE_FROM_MEDIACENTRE;
-
-    // When
-    route.afterModel();
-
-    // Then
-    sinon.assert.calledOnce(route._redirectToDisconnectedPage);
+    expect(sessionStub.alternativeRootURL).to.equal('/nonconnecte');
   });
 
   it('should erase campaign storage', function() {
