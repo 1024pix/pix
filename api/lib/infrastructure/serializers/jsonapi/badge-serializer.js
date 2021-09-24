c317f272dff7dd613da7c18b40c1a5d70fa6870f
Vincent Hardouin <vincent.hardouin@pix.fr>
2021-09-24 10:23:38 +0200

    sr(api): Add is-always-visible attribute in badge-serializer

--- api/lib/infrastructure/serializers/jsonapi/badge-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/badge-serializer.js
@@ -4,7 +4,7 @@ module.exports = {
   serialize(badge = {}) {
     return new Serializer('badge', {
       ref: 'id',
-      attributes: ['altMessage', 'imageUrl', 'message', 'key', 'title', 'isCertifiable'],
+      attributes: ['altMessage', 'imageUrl', 'message', 'key', 'title', 'isCertifiable', 'isAlwaysVisible'],
     }).serialize(badge);
   },
 };
--- api/tests/unit/infrastructure/serializers/jsonapi/badge-serializer_test.js
+++ api/tests/unit/infrastructure/serializers/jsonapi/badge-serializer_test.js
@@ -17,6 +17,7 @@ describe('Unit | Serializer | JSONAPI | badge-serializer', function() {
         title: 'Banana',
         targetProfileId: '1',
         isCertifiable: false,
+        isAlwaysVisible: true,
         badgeCriteria: [
           domainBuilder.buildBadgeCriterion({ partnerCompetenceIds: null }),
         ],
@@ -29,6 +30,7 @@ describe('Unit | Serializer | JSONAPI | badge-serializer', function() {
             'alt-message': 'You won a banana badge',
             'image-url': '/img/banana.svg',
             'is-certifiable': false,
+            'is-always-visible': true,
             message: 'Congrats, you won a banana badge',
             title: 'Banana',
             key: 'BANANA',
@@ -56,6 +58,7 @@ describe('Unit | Serializer | JSONAPI | badge-serializer', function() {
         title: 'Banana',
         targetProfileId: '1',
         isCertifiable: false,
+        isAlwaysVisible: true,
       });
 
       const expectedSerializedBadge = {
@@ -64,6 +67,7 @@ describe('Unit | Serializer | JSONAPI | badge-serializer', function() {
             'alt-message': 'You won a banana badge',
             'image-url': '/img/banana.svg',
             'is-certifiable': false,
+            'is-always-visible': true,
             message: 'Congrats, you won a banana badge',
             title: 'Banana',
             key: 'BANANA',
@@ -91,6 +95,7 @@ describe('Unit | Serializer | JSONAPI | badge-serializer', function() {
         title: 'Banana',
         targetProfileId: '1',
         isCertifiable: false,
+        isAlwaysVisible: true,
         badgeCriteria: [
           domainBuilder.buildBadgeCriterion({
             scope: BadgeCriterion.SCOPES.CAMPAIGN_PARTICIPATION,
@@ -106,6 +111,7 @@ describe('Unit | Serializer | JSONAPI | badge-serializer', function() {
             'alt-message': 'You won a banana badge',
             'image-url': '/img/banana.svg',
             'is-certifiable': false,
+            'is-always-visible': true,
             message: 'Congrats, you won a banana badge',
             title: 'Banana',
             key: 'BANANA',
