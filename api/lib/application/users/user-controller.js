75bc31977fb5b7d08b9cb2fe8e4a2f5f41de8b14
Andreia Pena <andreia.pena@pix.fr>
2021-09-27 14:31:39 +0200

    [API] Create updateUserEmailWithValidation controller

--- api/lib/application/users/user-controller.js
+++ api/lib/application/users/user-controller.js
@@ -9,6 +9,7 @@ const sharedProfileForCampaignSerializer = require('../../infrastructure/seriali
 const userSerializer = require('../../infrastructure/serializers/jsonapi/user-serializer');
 const emailVerificationSerializer = require('../../infrastructure/serializers/jsonapi/email-verification-serializer');
 const userDetailsForAdminSerializer = require('../../infrastructure/serializers/jsonapi/user-details-for-admin-serializer');
+const updateEmailSerializer = require('../../infrastructure/serializers/jsonapi/update-email-serializer');
 const queryParamsUtils = require('../../infrastructure/utils/query-params-utils');
 const { extractLocaleFromRequest } = require('../../infrastructure/utils/request-response-utils');
 
@@ -277,4 +278,15 @@ module.exports = {
     return h.response().code(204);
   },
 
+  async updateUserEmailWithValidation(request) {
+    const userId = request.params.id;
+    const code = request.payload.data.attributes.code;
+
+    const updatedUserAttributes = await usecases.updateUserEmailWithValidation({
+      userId,
+      code,
+    });
+
+    return updateEmailSerializer.serialize(updatedUserAttributes);
+  },
 };
