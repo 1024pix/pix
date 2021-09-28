1718556ce37bb5704b1711ac634c8ebc83e665ec
Quentin Lebouc <quentin.lebouc@pix.fr>
2021-09-28 15:52:49 +0200

    feat(api): controller method to save a certification center with accreditations

--- api/lib/infrastructure/serializers/jsonapi/accreditation-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/accreditation-serializer.js
@@ -1,10 +1,19 @@
 const { Serializer } = require('jsonapi-serializer');
 
+const Accreditation = require('../../../domain/models/Accreditation');
+
 module.exports = {
 
   serialize(accreditation) {
     return new Serializer('accreditation', {
       attributes: ['name'],
     }).serialize(accreditation);
+  },
+
+  deserialize(jsonAPI) {
+    return new Accreditation({
+      id: jsonAPI.data.id,
+      name: jsonAPI.data.attributes.name,
+    });
   },
 };
