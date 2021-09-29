fcdec5bf6aa4232b018d4639479a8eb59925d47b
Stephane Bedeau <sbedeau@octo.com>
2021-09-29 12:28:01 +0200

    sr(admin)(test): rearrange insights tests into modules

--- admin/tests/acceptance/authenticated/target-profiles/target-profile/insights_test.js
+++ admin/tests/acceptance/authenticated/target-profiles/target-profile/insights_test.js
@@ -29,72 +29,76 @@ module('Acceptance | Target Profiles | Target Profile | Insights', function (hoo
     assert.contains('My stage');
   });
 
-  test('should be able to see the details of a badge', async function (assert) {
-    await visit(`/target-profiles/${targetProfile.id}/insights`);
+  module('badges', function() {
+    test('should be able to see the details of a badge', async function (assert) {
+      await visit(`/target-profiles/${targetProfile.id}/insights`);
 
-    await click('.insights__section:nth-child(1) a');
+      await click('.insights__section:nth-child(1) a');
 
-    assert.equal(currentURL(), '/badges/100');
+      assert.equal(currentURL(), '/badges/100');
+    });
+
+    test('should redirect to badge creation page on link click', async function(assert) {
+      await visit(`/target-profiles/${targetProfile.id}/insights`);
+
+      await click('[data-test="badges-creation-redirect"]');
+
+      assert.equal(currentURL(), `/target-profiles/${targetProfile.id}/badges/new`);
+    });
   });
 
-  test('should redirect to badge creation page on link click', async function(assert) {
-    await visit(`/target-profiles/${targetProfile.id}/insights`);
+  module('stages', function() {
+    test('should be able to add a new stage', async function (assert) {
+      await visit(`/target-profiles/${targetProfile.id}/insights`);
 
-    await click('[data-test=\'badges-creation-redirect\']');
+      const stageCount = findAll('.insights__section:nth-child(2) tbody tr').length;
+      assert.equal(stageCount, 1);
 
-    assert.equal(currentURL(), `/target-profiles/${targetProfile.id}/badges/new`);
-  });
+      assert.notContains('Enregistrer');
 
-  test('should be able to add a new stage', async function (assert) {
-    await visit(`/target-profiles/${targetProfile.id}/insights`);
+      await click("button[data-test='Nouveau palier']");
 
-    const stageCount = findAll('.insights__section:nth-child(2) tbody tr').length;
-    assert.equal(stageCount, 1);
+      assert.contains('Enregistrer');
+      assert.contains('Annuler');
+      const newTableRowCount = findAll('.insights__section:nth-child(2) tbody tr').length;
+      assert.equal(newTableRowCount, 2);
 
-    assert.notContains('Enregistrer');
+      fillIn('.insights__section:nth-child(2) tbody tr td:nth-child(3) input', '0');
+      fillIn('.insights__section:nth-child(2) tbody tr td:nth-child(4) input', 'My stage title');
+      fillIn('.insights__section:nth-child(2) tbody tr td:nth-child(5) input', 'My stage message');
 
-    await click("button[data-test='Nouveau palier']");
+      await click('button[data-test="form-action-submit"]');
+      assert.notContains('Enregistrer');
 
-    assert.contains('Enregistrer');
-    assert.contains('Annuler');
-    const newTableRowCount = findAll('.insights__section:nth-child(2) tbody tr').length;
-    assert.equal(newTableRowCount, 2);
+      const newStageCount = findAll('.insights__section:nth-child(2) tbody tr').length;
+      assert.equal(newStageCount, 2);
+    });
 
-    fillIn('.insights__section:nth-child(2) tbody tr td:nth-child(3) input', '0');
-    fillIn('.insights__section:nth-child(2) tbody tr td:nth-child(4) input', 'My stage title');
-    fillIn('.insights__section:nth-child(2) tbody tr td:nth-child(5) input', 'My stage message');
+    test('should reset stage creation data after cancellation', async function (assert) {
+      // when
+      await visit(`/target-profiles/${targetProfile.id}/insights`);
+      const stageCount = findAll('.insights__section:nth-child(2) tbody tr').length;
+      assert.equal(stageCount, 1);
+      await click("button[data-test='Nouveau palier']");
+      await click('button[data-test="form-action-cancel"]');
 
-    await click('button[data-test="form-action-submit"]');
-    assert.notContains('Enregistrer');
+      // then
+      const newStageCount = findAll('.insights__section:nth-child(2) tbody tr').length;
+      assert.equal(newStageCount, 1);
+    });
 
-    const newStageCount = findAll('.insights__section:nth-child(2) tbody tr').length;
-    assert.equal(newStageCount, 2);
-  });
+    test('should remove one line of a new stage', async function (assert) {
+      // when
+      await visit(`/target-profiles/${targetProfile.id}/insights`);
+      const stageCount = findAll('.insights__section:nth-child(2) tbody tr').length;
+      assert.equal(stageCount, 1);
+      await click("button[data-test='Nouveau palier']");
+      await click("button[data-test='Nouveau palier']");
+      await click("button[aria-label='Supprimer palier']");
 
-  test('should reset stage creation data after cancellation', async function (assert) {
-    // when
-    await visit(`/target-profiles/${targetProfile.id}/insights`);
-    const stageCount = findAll('.insights__section:nth-child(2) tbody tr').length;
-    assert.equal(stageCount, 1);
-    await click("button[data-test='Nouveau palier']");
-    await click('button[data-test="form-action-cancel"]');
-
-    // then
-    const newStageCount = findAll('.insights__section:nth-child(2) tbody tr').length;
-    assert.equal(newStageCount, 1);
-  });
-
-  test('should remove one line of a new stage', async function (assert) {
-    // when
-    await visit(`/target-profiles/${targetProfile.id}/insights`);
-    const stageCount = findAll('.insights__section:nth-child(2) tbody tr').length;
-    assert.equal(stageCount, 1);
-    await click("button[data-test='Nouveau palier']");
-    await click("button[data-test='Nouveau palier']");
-    await click("button[aria-label='Supprimer palier']");
-
-    // then
-    const newStageCount = findAll('.insights__section:nth-child(2) tbody tr').length;
-    assert.equal(newStageCount, 2);
+      // then
+      const newStageCount = findAll('.insights__section:nth-child(2) tbody tr').length;
+      assert.equal(newStageCount, 2);
+    });
   });
 });
