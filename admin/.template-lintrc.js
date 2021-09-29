40c4e4abd5552aa5bf47c02f2ac8d969fb0fe0c3
Stephane Bedeau <sbedeau@octo.com>
2021-09-29 15:25:28 +0200

    lint(admin)(packages): add prettier for ember-template-lint

--- admin/.template-lintrc.js
+++ admin/.template-lintrc.js
@@ -1,7 +1,9 @@
 'use strict';
 
 module.exports = {
-  extends: ['recommended'],
+  plugins: ['ember-template-lint-plugin-prettier'],
+
+  extends: ['recommended', 'ember-template-lint-plugin-prettier:recommended'],
 
   rules: {
     'no-duplicate-landmark-elements': false,
