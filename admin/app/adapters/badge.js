cadb009f6c8183b83039a27bfa699ba2015cea25
celineung <celine.ung@zenika.com>
2021-09-30 17:39:26 +0200

    feat(admin): create urlForCreateRecord in badge adapter

--- admin/app/adapters/badge.js
+++ admin/app/adapters/badge.js
@@ -2,4 +2,8 @@ import ApplicationAdapter from './application';
 
 export default class BadgeAdapter extends ApplicationAdapter {
   namespace = 'api/admin';
+
+  urlForCreateRecord(modelName, { adapterOptions: { targetProfileId } }) {
+    return `${this.host}/${this.namespace}/target-profiles/${targetProfileId}/badges`;
+  }
 }
