305cada17cf140ce9e4573a2d642125c6ae6b1dc
celineung <celine.ung@zenika.com>
2021-09-30 11:48:37 +0200

    fix(api): display badge's criteria in pix-admin
    A badge criterion can be linked to skillIds that are 'périmés'.
    However, when searching for the whole skill given the skillId, only the operative ('actif', 'archivé') skills are considered.
    Thus, for skillId that are 'périmés' we cannot access its data, causing the GET badge request to fail.
    
    In this commit, we prevent the failure by not considering the skillId of the badge criterion that are not operative.

--- api/tests/unit/infrastructure/serializers/jsonapi/badge-with-learning-content-serializer_test.js
+++ api/tests/unit/infrastructure/serializers/jsonapi/badge-with-learning-content-serializer_test.js
@@ -247,6 +247,163 @@ describe('Unit | Serializer | JSONAPI | badge-with-learning-content-serializer',
       // then
       expect(json).to.deep.equal(expectedSerializedBadge);
     });
+
+    it('should only consider badge\'s skills', function() {
+      // given
+      const badge = domainBuilder.buildBadge({
+        id: '1',
+        altMessage: 'You won a banana badge',
+        imageUrl: '/img/banana.svg',
+        message: 'Congrats, you won a banana badge',
+        key: 'BANANA',
+        title: 'Banana',
+        targetProfileId: '1',
+        isCertifiable: false,
+      });
+
+      const skills = [
+        domainBuilder.buildSkill({ id: 'recABC', name: '@sau6', tubeId: 'recTUB123' }),
+      ];
+
+      const tubes = [
+        domainBuilder.buildTube({ id: 'recTUB123', name: '@sau', skills }),
+      ];
+
+      const badgeWithLearningContent = domainBuilder.buildBadgeWithLearningContent({ badge, skills, tubes });
+
+      const expectedSerializedBadge = {
+        data: {
+          attributes: {
+            'alt-message': 'You won a banana badge',
+            'image-url': '/img/banana.svg',
+            'is-certifiable': false,
+            message: 'Congrats, you won a banana badge',
+            title: 'Banana',
+            key: 'BANANA',
+          },
+          id: '1',
+          type: 'badges',
+          relationships: {
+            'badge-criteria': {
+              'data': [{
+                id: '1',
+                type: 'badge-criteria',
+              }, {
+                id: '2',
+                type: 'badge-criteria',
+              }],
+            },
+            'badge-partner-competences': {
+              'data': [{
+                id: '1',
+                type: 'badge-partner-competences',
+              }, {
+                id: '2',
+                type: 'badge-partner-competences',
+              }],
+            },
+          },
+        },
+        included: [
+          {
+            attributes: {
+              scope: 'CampaignParticipation',
+              threshold: 40,
+            },
+            relationships: {
+              'partner-competences': {
+                data: [],
+              },
+            },
+            id: '1',
+            type: 'badge-criteria',
+          },
+          {
+            attributes: {
+              scope: 'CampaignParticipation',
+              threshold: 40,
+            },
+            relationships: {
+              'partner-competences': {
+                data: [{
+                  id: '1',
+                  type: 'badge-partner-competences',
+                }, {
+                  id: '2',
+                  type: 'badge-partner-competences',
+                }],
+              },
+            },
+            id: '2',
+            type: 'badge-criteria',
+          },
+          {
+            type: 'tubes',
+            id: 'recTUB123',
+            attributes: {
+              'practical-title': 'titre pratique',
+            },
+            relationships: {},
+          },
+          {
+            attributes: {
+              name: '@sau6',
+              difficulty: 6,
+            },
+            id: 'recABC',
+            relationships: {
+              tube: {
+                data: {
+                  id: 'recTUB123',
+                  type: 'tubes',
+                },
+              },
+            },
+            type: 'skills',
+          },
+          {
+            attributes: {
+              name: 'name',
+            },
+            id: '1',
+            type: 'badge-partner-competences',
+            relationships: {
+              'skills': {
+                data: [
+                  {
+                    id: 'recABC',
+                    type: 'skills',
+                  },
+                ],
+              },
+            },
+          },
+          {
+            attributes: {
+              name: 'name',
+            },
+            id: '2',
+            type: 'badge-partner-competences',
+            relationships: {
+              'skills': {
+                data: [
+                  {
+                    id: 'recABC',
+                    type: 'skills',
+                  },
+                ],
+              },
+            },
+          },
+        ],
+      };
+
+      // when
+      const json = serializer.serialize(badgeWithLearningContent);
+
+      // then
+      expect(json).to.deep.equal(expectedSerializedBadge);
+    });
   });
 
 });
