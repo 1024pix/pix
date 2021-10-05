66ca3c1646704298c2197cd26af9f1ff7709b2b4
Andreia Pena <andreia.pena@pix.fr>
2021-10-05 11:51:29 +0200

    [app] Show error messages when something's wrong

--- mon-pix/app/components/user-account/email-verification-code.js
+++ mon-pix/app/components/user-account/email-verification-code.js
@@ -3,6 +3,7 @@ import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import ENV from 'mon-pix/config/environment';
+import get from 'lodash/get';
 
 export default class EmailVerificationCode extends Component {
   @service currentUser;
@@ -10,6 +11,7 @@ export default class EmailVerificationCode extends Component {
   @service intl;
   @tracked showResendCode = false;
   @tracked wasButtonClicked = false;
+  @tracked errorMessage = null;
 
   constructor() {
     super(...arguments);
@@ -40,11 +42,29 @@ export default class EmailVerificationCode extends Component {
   @action
   async onSubmitCode(code) {
     const emailVerificationCode = this.store.createRecord('email-verification-code', { code });
-    const email = await emailVerificationCode.verifyCode();
-    if (email) {
-      this.currentUser.user.email = email;
+    try {
+      const email = await emailVerificationCode.verifyCode();
+      if (email) {
+        this.currentUser.user.email = email;
+      }
+      this.args.disableEmailEditionMode();
+      this.args.displayEmailUpdateMessage();
+    } catch (response) {
+      const status = get(response, 'errors[0].status');
+
+      if (status === '403') {
+        const code = get(response, 'errors[0].code');
+
+        if (code === 'INVALID_VERIFICATION_CODE') {
+          this.errorMessage = this.intl.t('pages.user-account.email-verification.errors.incorrect-code');
+        } else if (code === 'EXPIRED_OR_NULL_EMAIL_MODIFICATION_DEMAND') {
+          this.errorMessage = this.intl.t('pages.user-account.email-verification.errors.email-modification-demand-expired');
+        }
+      } else if (status === '400') {
+        this.errorMessage = this.intl.t('pages.user-account.email-verification.errors.new-email-already-exist');
+      } else {
+        this.errorMessage = this.intl.t('pages.user-account.email-verification.errors.unknown-error');
+      }
     }
-    this.args.disableEmailEditionMode();
-    this.args.displayEmailUpdateMessage();
   }
 }
