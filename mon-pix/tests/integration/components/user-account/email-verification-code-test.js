d7e74239e747569ecfb2f9d092d7191b475ce77e
yschoueri <yasmina.schoueri@gmail.com>
2021-10-08 17:19:34 +0200

    [app] disable resend button while resending email and only show message once resent successful

--- mon-pix/tests/integration/components/user-account/email-verification-code-test.js
+++ mon-pix/tests/integration/components/user-account/email-verification-code-test.js
@@ -26,7 +26,7 @@ describe('Integration | Component | user-account | email-verification-code', fun
     });
 
     it(`should display a resend code message after ${ENV.APP.MILLISECONDS_BEFORE_MAIL_RESEND} milliseconds`, function() {
-    // given
+      // given
       const email = 'toto@example.net';
       const password = 'pix123';
       this.set('email', email);
@@ -46,8 +46,37 @@ describe('Integration | Component | user-account | email-verification-code', fun
       return result;
     });
 
+    it('should prevent multiple requests to resend verification code', function() {
+      // given
+      const email = 'toto@example.net';
+      const password = 'pix123';
+      this.set('email', email);
+      this.set('password', password);
+
+      const store = this.owner.lookup('service:store');
+      store.createRecord = sinon.stub();
+      store.createRecord
+        .withArgs('email-verification-code', { password, newEmail: email })
+        .returns({ sendNewEmail: () => new Promise(() => {}) });
+
+      const clock = sinon.useFakeTimers();
+
+      // when
+      const promise = render(hbs`<UserAccount::EmailVerificationCode @email={{this.email}} @password={{this.password}} />`);
+      clock.tick(ENV.APP.MILLISECONDS_BEFORE_MAIL_RESEND);
+
+      const result = promise.then(async () => {
+        await clickByLabel(this.intl.t('pages.user-account.email-verification.send-back-the-code'));
+
+        // then
+        expect(findByLabel(this.intl.t('pages.user-account.email-verification.send-back-the-code')).disabled).to.be.true;
+      });
+      clock.restore();
+      return result;
+    });
+
     it('should show confirmation message when resending code message', function() {
-    // given
+      // given
       const email = 'toto@example.net';
       const password = 'pix123';
       this.set('email', email);
@@ -114,7 +143,12 @@ describe('Integration | Component | user-account | email-verification-code', fun
       this.set('disableEmailEditionMode', disableEmailEditionMode);
       this.set('displayEmailUpdateMessage', displayEmailUpdateMessage);
       this.set('email', 'toto@example.net');
-      const verifyCode = sinon.stub().throws({ errors: [{ status: '403', code: 'EXPIRED_OR_NULL_EMAIL_MODIFICATION_DEMAND' }] });
+      const verifyCode = sinon.stub().throws({
+        errors: [{
+          status: '403',
+          code: 'EXPIRED_OR_NULL_EMAIL_MODIFICATION_DEMAND',
+        }],
+      });
       store.createRecord = () => ({ verifyCode });
 
       await render(hbs`<UserAccount::EmailVerificationCode
@@ -140,7 +174,12 @@ describe('Integration | Component | user-account | email-verification-code', fun
       this.set('disableEmailEditionMode', disableEmailEditionMode);
       this.set('displayEmailUpdateMessage', displayEmailUpdateMessage);
       this.set('email', 'toto@example.net');
-      const verifyCode = sinon.stub().throws({ errors: [{ status: '400', code: 'ACCOUNT_WITH_EMAIL_ALREADY_EXISTS' }] });
+      const verifyCode = sinon.stub().throws({
+        errors: [{
+          status: '400',
+          code: 'ACCOUNT_WITH_EMAIL_ALREADY_EXISTS',
+        }],
+      });
       store.createRecord = () => ({ verifyCode });
 
       await render(hbs`<UserAccount::EmailVerificationCode
