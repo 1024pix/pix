36a700578e47d0217f2dc4b8738c7c4bc5497f0a
alexandrecoin <alexandre.pc.coin@gmail.com>
2021-10-11 18:21:04 +0200

    fix: add relatedTarget check on mouse event

--- mon-pix/app/components/challenge/item.js
+++ mon-pix/app/components/challenge/item.js
@@ -4,7 +4,6 @@ import Component from '@glimmer/component';
 import ENV from 'mon-pix/config/environment';
 
 export default class Item extends Component {
-
   @service currentUser;
 
   constructor() {
@@ -22,10 +21,20 @@ export default class Item extends Component {
   }
 
   @action
-  showOutOfFocusBorder() {
+  showOutOfFocusBorder(event) {
     if (this.isFocusedChallenge && !this.args.answer) {
+      // linked to a Firefox issue where the mouseleave is triggered
+      // when hovering the select options on mouse navigation
+      // see: https://stackoverflow.com/questions/46831247/select-triggers-mouseleave-event-on-parent-element-in-mozilla-firefox
+      if (this.shouldPreventFirefoxSelectMouseLeaveBehavior(event)) {
+        return;
+      }
       this.args.onFocusOutOfChallenge();
     }
+  }
+
+  shouldPreventFirefoxSelectMouseLeaveBehavior(event) {
+    return event.relatedTarget === null;
   }
 
   _setOnBlurEventToWindow() {
