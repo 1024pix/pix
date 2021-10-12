dc981ebadd21ec73f43e4967bfc31751c4c27e88
Karam Mohd <k.haytham.mohammed@octo.com>
2021-10-12 16:01:46 +0200

    [API] Add group repository + model

---
+++ api/lib/infrastructure/repositories/group-repository.js
@@ -0,0 +1,27 @@
+const Group = require('../../domain/models/Group');
+const { knex } = require('../bookshelf');
+
+async function findByCampaignId(campaignId) {
+  const groups = await knex('campaigns')
+    .where({ 'campaigns.id': campaignId })
+    .select('group')
+    .groupBy('group')
+    .join('campaign-participations', 'campaigns.id', 'campaign-participations.campaignId')
+    .join('schooling-registrations', function () {
+      this.on('schooling-registrations.userId', '=', 'campaign-participations.userId').andOn(
+        'schooling-registrations.organizationId',
+        '=',
+        'campaigns.organizationId'
+      );
+    });
+
+  return groups.map(({ group }) => _toDomain(group));
+}
+
+function _toDomain(group) {
+  return new Group({ name: group });
+}
+
+module.exports = {
+  findByCampaignId,
+};
