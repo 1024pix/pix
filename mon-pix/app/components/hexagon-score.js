e8ababe7d13aaa97e0419b31c8cf45323bd5c742
François de Metz <francois.de-metz@pix.fr>
2021-10-13 17:00:20 +0200

    Make fun with hexagon again
    Co-authored-by: Thomas Evano <thomas.evano@pix.fr>
    Co-authored-by: Nicolas Legrand <nicolas.legrand@pix.fr>
    Co-authored-by: Andreia Pena Ferreira <andreia.pena@pix.fr>
    Co-authored-by: Quentin Lebouc <quentin.lebouc@pix.fr>
    Co-authored-by: Mickaël Alibert <mickael.alibert@pix.fr>
    Co-authored-by: Khadija Bayssir <khadija.bayssir@pix.fr>

--- mon-pix/app/components/hexagon-score.js
+++ mon-pix/app/components/hexagon-score.js
@@ -1,8 +1,15 @@
 import { isNone } from '@ember/utils';
 import Component from '@glimmer/component';
 import ENV from 'mon-pix/config/environment';
+import { inject as service } from '@ember/service';
 
 export default class HexagonScore extends Component {
+  @service featureToggles;
+
+  get isHalloween() {
+    return this.featureToggles.featureToggles.isHalloweenEnabled;
+  }
+
   get score() {
     const score = this.args.pixScore;
     return (isNone(score) || score === 0) ? '–' : Math.floor(score);
