69ddff4d880c01d1f9bf298289667c222fce77a0
Lise Quesnel <quesnel.lise@gmail.com>
2021-10-14 12:26:47 +0200

    [APP] Redirect to the good type of campaign in entrance route

--- mon-pix/app/routes/campaigns/entrance.js
+++ mon-pix/app/routes/campaigns/entrance.js
@@ -5,4 +5,12 @@ export default class Entrance extends Route.extend(SecuredRouteMixin) {
   model() {
     return this.modelFor('campaigns');
   }
+
+  redirect(campaign) {
+    if (campaign.isProfilesCollection) {
+      this.replaceWith('campaigns.profiles-collection.start-or-resume', campaign);
+    } else {
+      this.replaceWith('campaigns.assessment.start-or-resume', campaign);
+    }
+  }
 }
