3786668b10c0ef4300b97849fd52c648a031a4ce
Andreia Pena <andreia.pena@pix.fr>
2021-10-15 16:03:05 +0200

    [API][SR] Fix organization invitation serializer test

--- api/tests/unit/infrastructure/serializers/jsonapi/organization-invitation-serializer_test.js
+++ api/tests/unit/infrastructure/serializers/jsonapi/organization-invitation-serializer_test.js
@@ -3,41 +3,28 @@ const serializer = require('../../../../../lib/infrastructure/serializers/jsonap
 
 describe('Unit | Serializer | JSONAPI | organization-invitation-serializer', function () {
   describe('#serialize', function () {
-    // TODO: Fix this the next time the file is edited.
-    // eslint-disable-next-line mocha/no-setup-in-describe
-    const invitationObject = domainBuilder.buildOrganizationInvitation();
-
-    const jsonInvitationExpected = {
-      data: {
-        type: 'organization-invitations',
-        // TODO: Fix this the next time the file is edited.
-        // eslint-disable-next-line mocha/no-setup-in-describe
-        id: invitationObject.id.toString(),
-        attributes: {
-          // TODO: Fix this the next time the file is edited.
-          // eslint-disable-next-line mocha/no-setup-in-describe
-          'organization-id': invitationObject.organizationId,
-          // TODO: Fix this the next time the file is edited.
-          // eslint-disable-next-line mocha/no-setup-in-describe
-          'organization-name': invitationObject.organizationName,
-          // TODO: Fix this the next time the file is edited.
-          // eslint-disable-next-line mocha/no-setup-in-describe
-          email: invitationObject.email,
-          // TODO: Fix this the next time the file is edited.
-          // eslint-disable-next-line mocha/no-setup-in-describe
-          status: invitationObject.status,
-          // TODO: Fix this the next time the file is edited.
-          // eslint-disable-next-line mocha/no-setup-in-describe
-          'updated-at': invitationObject.updatedAt,
-        },
-      },
-    };
-
     it('should convert a organization-invitation object into JSON API data', function () {
+      // given
+      const invitationObject = domainBuilder.buildOrganizationInvitation();
+
       // when
       const json = serializer.serialize(invitationObject);
 
       // then
+      const jsonInvitationExpected = {
+        data: {
+          type: 'organization-invitations',
+          id: invitationObject.id.toString(),
+          attributes: {
+            'organization-id': invitationObject.organizationId,
+            'organization-name': invitationObject.organizationName,
+            email: invitationObject.email,
+            status: invitationObject.status,
+            'updated-at': invitationObject.updatedAt,
+          },
+        },
+      };
+
       expect(json).to.deep.equal(jsonInvitationExpected);
     });
   });
