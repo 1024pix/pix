8a5033d116b778d01b19233ab9063125b1dff081
Andreia Pena <andreia.pena@pix.fr>
2021-10-15 16:03:57 +0200

    [API] Add deserialize organization invitation method

--- api/tests/unit/infrastructure/serializers/jsonapi/organization-invitation-serializer_test.js
+++ api/tests/unit/infrastructure/serializers/jsonapi/organization-invitation-serializer_test.js
@@ -28,4 +28,30 @@ describe('Unit | Serializer | JSONAPI | organization-invitation-serializer', fun
       expect(json).to.deep.equal(jsonInvitationExpected);
     });
   });
+  describe('#deserialize()', function () {
+    it('should convert the payload json to organization invitation information', async function () {
+      //given
+      const payload = {
+        data: {
+          type: 'organization-invitations',
+          attributes: {
+            lang: 'fr-fr',
+            email: 'EMAIL@example.net',
+            role: null,
+          },
+        },
+      };
+
+      // when
+      const json = await serializer.deserialize(payload);
+
+      // then
+      const expectedJsonApi = {
+        lang: 'fr-fr',
+        email: 'email@example.net',
+        role: null,
+      };
+      expect(json).to.deep.equal(expectedJsonApi);
+    });
+  });
 });
