a2f0a94fc41532df2402639f6375933d97265a52
Lise Quesnel <quesnel.lise@gmail.com>
2021-10-15 16:04:13 +0200

    [APP] Prevent user to type directly /campagnes/code/entree
    and then create campaign participation

--- mon-pix/tests/unit/routes/campaigns/entrance_test.js
+++ mon-pix/tests/unit/routes/campaigns/entrance_test.js
@@ -15,6 +15,24 @@ describe('Unit | Route | Entrance', function () {
     route.replaceWith = sinon.stub();
   });
 
+  describe('#beforeModel', function () {
+    it('should redirect to entry point when /entree is directly set in the url', async function () {
+      //when
+      await route.beforeModel({ from: null });
+
+      //then
+      sinon.assert.calledWith(route.replaceWith, 'campaigns.entry-point');
+    });
+
+    it('should continue en entrance route when from is set', async function () {
+      //when
+      await route.beforeModel({ from: 'campaigns.entry-point' });
+
+      //then
+      sinon.assert.notCalled(route.replaceWith);
+    });
+  });
+
   describe('#model', function () {
     it('should load model', async function () {
       //when
