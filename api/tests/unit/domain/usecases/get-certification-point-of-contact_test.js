32bbb8b28b9bb2ebb54af5e7ec50b77ac85ec3c5
Mathieu Gilet <mathieu.gilet@zenika.com>
2021-10-18 15:52:49 +0200

    feat(api): remove user's link to certification center verification when retrieving certification point of contact

--- api/tests/unit/domain/usecases/get-certification-point-of-contact_test.js
+++ api/tests/unit/domain/usecases/get-certification-point-of-contact_test.js
@@ -1,46 +1,22 @@
-const { expect, sinon, catchErr, domainBuilder } = require('../../../test-helper');
+const { expect, sinon, domainBuilder } = require('../../../test-helper');
 const getCertificationPointOfContact = require('../../../../lib/domain/usecases/get-certification-point-of-contact');
-const { NotFoundError } = require('../../../../lib/domain/errors');
 
 describe('Unit | UseCase | get-certification-point-of-contact', function () {
   const userId = 123;
-  let certificationCenterMembershipRepository;
   let certificationPointOfContactRepository;
 
   beforeEach(function () {
-    certificationCenterMembershipRepository = { doesUserHaveMembershipToAnyCertificationCenter: sinon.stub() };
     certificationPointOfContactRepository = { get: sinon.stub() };
   });
 
-  it('should throw NotFoundError when user is not member of any certification center', async function () {
-    // given
-    certificationCenterMembershipRepository.doesUserHaveMembershipToAnyCertificationCenter
-      .withArgs(userId)
-      .resolves(false);
-
-    // when
-    const error = await catchErr(getCertificationPointOfContact)({
-      userId,
-      certificationCenterMembershipRepository,
-      certificationPointOfContactRepository,
-    });
-
-    // then
-    expect(error).to.be.instanceOf(NotFoundError);
-  });
-
-  it('should return the CertificationPointOfContact when user is member of a certification center', async function () {
+  it('should return the CertificationPointOfContact', async function () {
     // given
     const expectedCertificationPointOfContact = domainBuilder.buildCertificationPointOfContact({ id: userId });
-    certificationCenterMembershipRepository.doesUserHaveMembershipToAnyCertificationCenter
-      .withArgs(userId)
-      .resolves(true);
     certificationPointOfContactRepository.get.withArgs(userId).resolves(expectedCertificationPointOfContact);
 
     // when
     const actualCertificationPointOfContact = await getCertificationPointOfContact({
       userId,
-      certificationCenterMembershipRepository,
       certificationPointOfContactRepository,
     });
 
