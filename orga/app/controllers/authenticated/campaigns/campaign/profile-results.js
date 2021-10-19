0c440383560284dc82d00f061917ced3526fe927
yaelle <ymarsiac@octo.com>
2021-10-19 19:28:37 +0200

    [ORGA] add group filter in profile-results controller

--- orga/app/controllers/authenticated/campaigns/campaign/profile-results.js
+++ orga/app/controllers/authenticated/campaigns/campaign/profile-results.js
@@ -3,10 +3,11 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class ProfilesController extends Controller {
-  queryParams = ['pageNumber', 'pageSize', 'divisions'];
+  queryParams = ['pageNumber', 'pageSize', 'divisions', 'groups'];
   @tracked pageNumber = 1;
   @tracked pageSize = 25;
   @tracked divisions = [];
+  @tracked groups = [];
 
   @action
   goToProfilePage(campaignId, campaignParticipationId, event) {
@@ -19,11 +20,13 @@ export default class ProfilesController extends Controller {
   triggerFiltering(filters) {
     this.pageNumber = null;
     this.divisions = filters.divisions;
+    this.groups = filters.groups;
   }
 
   @action
   resetFiltering() {
     this.pageNumber = null;
     this.divisions = [];
+    this.groups = [];
   }
 }
