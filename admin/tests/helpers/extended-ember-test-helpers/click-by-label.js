8e46faa81a6b6c0e9f216c96a67c9846fca155e7
Clément Bourgoin <clement.bourgoin@pix.fr>
2021-10-22 17:33:07 +0200

    tests(admin): use ember-testing-library in clickByLabel helper

--- admin/tests/helpers/extended-ember-test-helpers/click-by-label.js
+++ admin/tests/helpers/extended-ember-test-helpers/click-by-label.js
@@ -1,8 +1,14 @@
 import { click } from '@ember/test-helpers';
-import getByLabel from './get-by-label';
+import { getScreen } from '@1024pix/ember-testing-library';
 
-export default function clickByLabel(labelText, options) {
-  const clickableElement = getByLabel(labelText, options);
-
-  return click(clickableElement);
+/**
+ * Click on a DOM element selected by a name.
+ *
+ * @param {*} name Name of the clickable DOM element.
+ * @returns Promise of the click.
+ */
+export default function clickByLabel(name) {
+  const { getByRole } = getScreen();
+  const element = getByRole(/button|link|radio|checkbox/, { name });
+  return click(element);
 }
