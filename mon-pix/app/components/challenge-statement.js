5eefe509769ad40d5f2a77fbbdc0fb8724c134d5
Vincent Hardouin <vincent.hardouin@pix.fr>
2021-10-25 09:03:31 +0200

    Remove formatEmail in challenge-statement

--- mon-pix/app/components/challenge-statement.js
+++ mon-pix/app/components/challenge-statement.js
@@ -2,7 +2,6 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-import config from 'mon-pix/config/environment';
 import sortBy from 'lodash/sortBy';
 import ENV from 'mon-pix/config/environment';
 
@@ -25,13 +24,7 @@ export default class ChallengeStatement extends Component {
 
   get challengeInstruction() {
     const instruction = this.args.challenge.instruction;
-    if (!instruction) {
-      return null;
-    }
-
-    const formattedEmailInstruction = this._formatEmail(instruction);
-    const formattedInstruction = this._formatLink(formattedEmailInstruction);
-    return formattedInstruction;
+    return instruction ? this._formatLink(instruction) : null;
   }
 
   get linkTitle() {
@@ -77,19 +70,6 @@ export default class ChallengeStatement extends Component {
 
   _initialiseDefaultAttachment() {
     this.selectedAttachmentUrl = this.orderedAttachments[0];
-  }
-
-  _formattedEmailForInstruction() {
-    return this.mailGenerator.generateEmail(
-      this.args.challenge.id,
-      this.args.assessment.id,
-      window.location.hostname,
-      config.environment
-    );
-  }
-
-  _formatEmail(instruction) {
-    return instruction.replace('${EMAIL}', this._formattedEmailForInstruction());
   }
 
   _formatLink(instruction) {
