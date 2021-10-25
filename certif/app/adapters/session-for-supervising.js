13125ba04e45336b730c9ebd44092733f54b31cb
P-Jeremy <jemyplu@gmail.com>
2021-10-25 15:30:14 +0200

    feat(certif): add fake authorized supervisor page

---
+++ certif/app/adapters/session-for-supervising.js
@@ -0,0 +1,13 @@
+import ApplicationAdapter from './application';
+
+export default class SessionForSupervisingAdapter extends ApplicationAdapter {
+
+  urlForQueryRecord(query) {
+    const sessionId = query.sessionId;
+    if (sessionId) {
+      delete query.sessionId;
+      return `${this.host}/${this.namespace}/sessions/${sessionId}/supervising`;
+    }
+    return super.urlForQuery(...arguments);
+  }
+}
