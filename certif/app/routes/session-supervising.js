757409012d7ee9bdcc9a7a72b7cdc9e56c8f8ab2
P-Jeremy <jemyplu@gmail.com>
2021-10-25 15:30:14 +0200

    feat(certif): add fake authorized supervisor page

---
+++ certif/app/routes/session-supervising.js
@@ -0,0 +1,8 @@
+import Route from '@ember/routing/route';
+
+export default class SessionSupervisingRoute extends Route {
+
+  model(params) {
+    return this.store.queryRecord('session-for-supervising', { sessionId: params.session_id });
+  }
+}
