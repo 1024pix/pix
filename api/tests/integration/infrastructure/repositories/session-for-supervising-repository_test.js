244355bdf22803b58d43ec4fc466b033eec6240c
P-Jeremy <jemyplu@gmail.com>
2021-10-25 15:56:18 +0200

    feat(api): add sessionForSupervising repository

---
+++ api/tests/integration/infrastructure/repositories/session-for-supervising-repository_test.js
@@ -0,0 +1,92 @@
+const { databaseBuilder, expect, catchErr } = require('../../../test-helper');
+const _ = require('lodash');
+const { NotFoundError } = require('../../../../lib/domain/errors');
+const SessionForSupervising = require('../../../../lib/domain/read-models/SessionForSupervising');
+const sessionForSupervisingRepository = require('../../../../lib/infrastructure/repositories/session-for-supervising-repository');
+
+describe('Integration | Repository | SessionForSupervising', function () {
+  describe('#get', function () {
+    it('should return session informations in a SessionForSupervising Object', async function () {
+      // given
+      databaseBuilder.factory.buildCertificationCenter({ name: 'Toto', id: 1234 });
+      const session = databaseBuilder.factory.buildSession({
+        certificationCenter: 'Tour Gamma',
+        room: 'Salle A',
+        examiner: 'Monsieur Examinateur',
+        date: '2018-02-23',
+        time: '12:00:00',
+        certificationCenterId: 1234,
+      });
+
+      await databaseBuilder.commit();
+
+      // when
+      const actualSession = await sessionForSupervisingRepository.get(session.id);
+
+      // then
+      expect(actualSession).to.be.deepEqualInstance(
+        new SessionForSupervising({
+          id: session.id,
+          certificationCenterName: 'Toto',
+          room: 'Salle A',
+          examiner: 'Monsieur Examinateur',
+          date: '2018-02-23',
+          time: '12:00:00',
+          certificationCandidates: [],
+        })
+      );
+    });
+
+    it('should return associated certifications candidates ordered by lastname and firstname', async function () {
+      // given
+      databaseBuilder.factory.buildCertificationCenter({ name: 'Toto', id: 1234 });
+      const session = databaseBuilder.factory.buildSession({
+        certificationCenter: 'Tour Gamma',
+        room: 'Salle A',
+        examiner: 'Monsieur Examinateur',
+        date: '2018-02-23',
+        time: '12:00:00',
+        certificationCenterId: 1234,
+      });
+
+      databaseBuilder.factory.buildCertificationCandidate({
+        lastName: 'Jackson',
+        firstName: 'Michael',
+        sessionId: session.id,
+      });
+      databaseBuilder.factory.buildCertificationCandidate({
+        lastName: 'Stardust',
+        firstName: 'Ziggy',
+        sessionId: session.id,
+      });
+      databaseBuilder.factory.buildCertificationCandidate({
+        lastName: 'Jackson',
+        firstName: 'Janet',
+        sessionId: session.id,
+      });
+      _.times(5, () => databaseBuilder.factory.buildCertificationCandidate());
+      await databaseBuilder.commit();
+
+      // when
+      const actualSession = await sessionForSupervisingRepository.get(session.id);
+
+      // then
+      const actualCandidates = _.map(actualSession.certificationCandidates, (item) =>
+        _.pick(item, ['sessionId', 'lastName', 'firstName'])
+      );
+      expect(actualCandidates).to.have.deep.ordered.members([
+        { lastName: 'Jackson', firstName: 'Janet' },
+        { lastName: 'Jackson', firstName: 'Michael' },
+        { lastName: 'Stardust', firstName: 'Ziggy' },
+      ]);
+    });
+
+    it('should return a Not found error when no session was found', async function () {
+      // when
+      const error = await catchErr(sessionForSupervisingRepository.get)(123123);
+
+      // then
+      expect(error).to.be.instanceOf(NotFoundError);
+    });
+  });
+});
