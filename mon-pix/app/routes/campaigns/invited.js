fdab1f24865aa372550381a1d2988a58fd93f951
Lise Quesnel <quesnel.lise@gmail.com>
2021-10-25 16:03:51 +0200

    [APP] Redirect to entry-point when /prescrit is directly set in the url

--- mon-pix/app/routes/campaigns/invited.js
+++ mon-pix/app/routes/campaigns/invited.js
@@ -2,6 +2,13 @@ import Route from '@ember/routing/route';
 import SecuredRouteMixin from 'mon-pix/mixins/secured-route-mixin';
 
 export default class InvitedRoute extends Route.extend(SecuredRouteMixin) {
+
+  beforeModel(transition) {
+    if (!transition.from) {
+      this.replaceWith('campaigns.entry-point');
+    }
+  }
+
   model() {
     return this.modelFor('campaigns');
   }
