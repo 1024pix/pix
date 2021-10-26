c90c9ac6b3735889b34cecfe60a8c82f57d8526c
Andreia Pena <andreia.pena@pix.fr>
2021-10-26 11:27:56 +0200

    [ADMIN] Display organization invitations

---
+++ admin/tests/integration/components/organization-invitations_test.js
@@ -0,0 +1,36 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+import moment from 'moment';
+
+module('Integration | Component | organization-invitations', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it should list the pending team invitations', async function (assert) {
+    // given
+    this.set('invitations', [
+      { email: 'riri@example.net', role: 'ADMIN', updatedAt: moment('2019-10-08T10:50:00Z').utcOffset(2) },
+      { email: 'fifi@example.net', role: 'MEMBER', updatedAt: moment('2019-10-08T10:50:00Z').utcOffset(2) },
+    ]);
+
+    // when
+    await render(hbs`<OrganizationInvitations @invitations={{invitations}}/>`);
+
+    // then
+    assert.contains('MEMBER');
+    assert.contains('ADMIN');
+    assert.notContains("Pas d'invitation en attente");
+  });
+
+  test('it should display a message when there is no invitations', async function (assert) {
+    // given
+    this.set('invitations', []);
+
+    // when
+    await render(hbs`<OrganizationInvitations @invitations={{invitations}}/>`);
+
+    // then
+    assert.contains("Pas d'invitation en attente");
+  });
+});
