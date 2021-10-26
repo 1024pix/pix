649c32cfe6784e88c86fef1592ddd5cb27c7f8c5
Yvonnick Frin <frin.yvonnick@gmail.com>
2021-10-26 14:28:52 +0200

    [API] Refactor acceptance test to remove lint comment

--- api/tests/acceptance/application/organizations/organization-controller_test.js
+++ api/tests/acceptance/application/organizations/organization-controller_test.js
@@ -23,11 +23,6 @@ describe('Acceptance | Application | organization-controller', function () {
 
   beforeEach(async function () {
     server = await createServer();
-  });
-
-  // TODO: Fix this the next time the file is edited.
-  // eslint-disable-next-line mocha/no-sibling-hooks
-  beforeEach(async function () {
     await insertUserWithRolePixMaster();
   });
 
