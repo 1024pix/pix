229f6c1e181c37051f9035787ca7450aba0fa277
Yvonnick Frin <frin.yvonnick@gmail.com>
2021-10-26 16:43:42 +0200

    [API] Remove conflict logic from usecase

--- api/tests/unit/domain/usecases/attach-target-profiles-to-organization_test.js
+++ api/tests/unit/domain/usecases/attach-target-profiles-to-organization_test.js
@@ -2,7 +2,6 @@ const { expect, sinon, catchErr } = require('../../../test-helper');
 const { NotFoundError } = require('../../../../lib/domain/errors');
 
 const attachTargetProfilesToOrganization = require('../../../../lib/domain/usecases/attach-target-profiles-to-organization');
-const { ConflictError } = require('../../../../lib/application/http-errors');
 
 describe('Unit | UseCase | attach-target-profiles-to-organization', function () {
   let organizationRepository;
@@ -32,31 +31,6 @@ describe('Unit | UseCase | attach-target-profiles-to-organization', function () 
     const expectedResult = { attachedIds: targetProfileIdsToAttach };
     targetProfileShareRepository.addTargetProfilesToOrganization
       .withArgs({ organizationId, targetProfileIdList: targetProfileIdsToAttach })
-      .resolves(expectedResult);
-
-    // when
-    const result = await attachTargetProfilesToOrganization({
-      targetProfileShareRepository,
-      targetProfileRepository,
-      organizationRepository,
-      organizationId,
-      targetProfileIdsToAttach,
-    });
-
-    // then
-    expect(result).to.equal(expectedResult);
-  });
-
-  it('should call repository with organizationId and targetProfileIdsToAttach even one target profile already linked', async function () {
-    // given
-    targetProfileIdsToAttach = [1, 2];
-    targetProfileRepository.findByIds.withArgs(targetProfileIdsToAttach).resolves([{ id: 1 }, { id: 2 }]);
-
-    organizationRepository.get.withArgs(organizationId).resolves({ targetProfileShares: [{ targetProfileId: 1 }] });
-
-    const expectedResult = { attachedIds: targetProfileIdsToAttach };
-    targetProfileShareRepository.addTargetProfilesToOrganization
-      .withArgs({ organizationId, targetProfileIdList: [2] })
       .resolves(expectedResult);
 
     // when
@@ -115,26 +89,5 @@ describe('Unit | UseCase | attach-target-profiles-to-organization', function () 
     // then
     expect(error).to.be.instanceOf(NotFoundError);
     expect(error.message).to.equal("Le profil cible 1 n'existe pas.");
-  });
-
-  it('should throw a NotFoundError when a target profile already attached to organization', async function () {
-    // given
-    targetProfileIdsToAttach = [1];
-    targetProfileRepository.findByIds.withArgs(targetProfileIdsToAttach).resolves([{ id: 1 }]);
-
-    organizationRepository.get.withArgs(organizationId).resolves({ targetProfileShares: [{ targetProfileId: 1 }] });
-
-    // when
-    const error = await catchErr(attachTargetProfilesToOrganization)({
-      targetProfileShareRepository,
-      targetProfileRepository,
-      organizationRepository,
-      organizationId,
-      targetProfileIdsToAttach,
-    });
-
-    // then
-    expect(error).to.be.instanceOf(ConflictError);
-    expect(error.message).to.equal('Profil(s) cible(s) déjà rattaché.');
   });
 });
