108b5f353816355f4ecc0853a336bf71543a1061
Yvonnick Frin <frin.yvonnick@gmail.com>
2021-10-26 16:43:42 +0200

    [API] Remove conflict logic from usecase

--- api/lib/domain/usecases/attach-target-profiles-to-organization.js
+++ api/lib/domain/usecases/attach-target-profiles-to-organization.js
@@ -1,18 +1,15 @@
 const _ = require('lodash');
-const { ConflictError } = require('../../application/http-errors');
 const { NotFoundError } = require('../errors');
 
 module.exports = async function attachTargetProfilesToOrganization({
   organizationId,
   targetProfileIdsToAttach,
-  organizationRepository,
   targetProfileRepository,
   targetProfileShareRepository,
 }) {
   const uniqueTargetProfileIdsToAttach = _.uniq(targetProfileIdsToAttach);
 
   const foundTargetProfiles = await targetProfileRepository.findByIds(uniqueTargetProfileIdsToAttach);
-  const organization = await organizationRepository.get(organizationId);
 
   if (foundTargetProfiles.length !== uniqueTargetProfileIdsToAttach.length) {
     const foundTargetProfileIds = _.map(foundTargetProfiles, 'id');
@@ -20,15 +17,8 @@ module.exports = async function attachTargetProfilesToOrganization({
     throw new NotFoundError(`Le profil cible ${targetProfileIdNotExisting} n'existe pas.`);
   }
 
-  const targetProfileIdOfOrganization = _.map(organization.targetProfileShares, 'targetProfileId');
-  const targetProfileShareToAttach = _.difference(uniqueTargetProfileIdsToAttach, targetProfileIdOfOrganization);
-
-  if (targetProfileShareToAttach.length === 0) {
-    throw new ConflictError('Profil(s) cible(s) déjà rattaché.');
-  }
-
   return targetProfileShareRepository.addTargetProfilesToOrganization({
     organizationId,
-    targetProfileIdList: targetProfileShareToAttach,
+    targetProfileIdList: uniqueTargetProfileIdsToAttach,
   });
 };
