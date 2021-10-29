5062680db91b7467338e76fbcd50aed85312fdc7
celineung <celine.ung@zenika.com>
2021-10-29 16:16:48 +0200

    feat(admin): add logic for badge and badge-criterion creation

--- admin/app/components/target-profiles/badge-form.js
+++ admin/app/components/target-profiles/badge-form.js
@@ -16,22 +16,53 @@ export default class BadgeForm extends Component {
     isCertifiable: false,
     isAlwaysVisible: false,
   };
+  threshold = null;
 
   constructor(...args) {
     super(...args);
   }
 
   @action
-  async createBadge(event) {
+  async createBadgeAndCriteria(event) {
     event.preventDefault();
     try {
-      const result = await this.store.createRecord('badge', this.badge);
-      await result.save({ adapterOptions: { targetProfileId: this.args.targetProfileId } });
+      const badge = await this._createBadge();
+
+      if (this.threshold) {
+        await this._createThresholdBadgeCriterion(badge.id);
+      }
+
       this.notifications.success('Le badge est créé.');
       this.router.transitionTo('authenticated.target-profiles.target-profile.insights');
     } catch (error) {
       console.error(error);
-      this.notifications.error('Erreur lors de la création du badge.');
+      this.notifications.error('Erreur lors de la création du résultat thématique.');
+    }
+  }
+
+  async _createBadge() {
+    const badge = this.store.createRecord('badge', this.badge);
+    await badge.save({
+      adapterOptions: { targetProfileId: this.args.targetProfileId },
+    });
+
+    return badge;
+  }
+
+  async _createThresholdBadgeCriterion(badgeId) {
+    try {
+      if (this.threshold < 0 || this.threshold > 100) {
+        this.notifications.error('Le taux de réussite doit être compris entre 0 et 100.');
+        return;
+      }
+      const badgeCriterion = this.store.createRecord('badge-criterion', {
+        scope: 'CampaignParticipation',
+        threshold: this.threshold,
+      });
+      await badgeCriterion.save({ adapterOptions: { badgeId } });
+    } catch (error) {
+      console.error(error);
+      this.notifications.error('Erreur lors de la création du critère du résultat thématique.');
     }
   }
 }
