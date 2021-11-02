36fe54de734bbedfdcdaf2f47156a6587dee338e
P-Jeremy <jemyplu@gmail.com>
2021-11-02 17:52:21 +0100

    feat(api): add certification candidates controller

---
+++ api/tests/unit/application/certification-candidates/certification-candidates-controller_test.js
@@ -0,0 +1,62 @@
+const { expect, sinon, catchErr, hFake } = require('../../../test-helper');
+const usecases = require('../../../../lib/domain/usecases');
+const certificationCandidateController = require('../../../../lib/application/certification-candidates/certification-candidates-controller');
+const { featureToggles } = require('../../../../lib/config');
+const { NotFoundError } = require('../../../../lib/application/http-errors');
+
+describe('Unit | Controller | certifications-candidate-controller', function () {
+  describe('#update', function () {
+    describe('when FT_END_TEST_SCREEN_REMOVAL_ENABLED is enabled', function () {
+      it('should return a 204 status code', async function () {
+        // given
+        sinon.stub(featureToggles, 'isEndTestScreenRemovalEnabled').value(true);
+        const request = {
+          auth: {
+            credentials: { userId: '111' },
+          },
+          params: {
+            id: 99,
+          },
+          payload: { 'authorized-to-start': true },
+        };
+
+        usecases.updateCertificationCandidateForSupervising = sinon.stub().rejects();
+        usecases.updateCertificationCandidateForSupervising
+          .withArgs({
+            certificationCandidateForSupervisingId: 99,
+            authorizedToStart: true,
+          })
+          .resolves();
+
+        // when
+        const response = await certificationCandidateController.update(request, hFake);
+
+        // then
+        expect(response.statusCode).to.equal(204);
+      });
+    });
+
+    describe('when FT_END_TEST_SCREEN_REMOVAL_ENABLED is not enabled', function () {
+      it('should return a 404 status code', async function () {
+        // given
+        sinon.stub(featureToggles, 'isEndTestScreenRemovalEnabled').value(false);
+
+        const request = {
+          auth: {
+            credentials: { userId: '111' },
+          },
+          params: {
+            id: 99,
+          },
+          payload: { 'authorized-to-start': true },
+        };
+
+        // when
+        const error = await catchErr(certificationCandidateController.update)(request, hFake);
+
+        // then
+        expect(error).to.be.an.instanceOf(NotFoundError);
+      });
+    });
+  });
+});
