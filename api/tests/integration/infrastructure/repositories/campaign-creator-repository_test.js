433c99d4dc40c2c8247e8c1fc8ce4c4313cb0573
ajoanny <ajoanny@octo.com>
2021-11-08 11:30:58 +0100

    [API] remove service from campaign creator repository

--- api/tests/integration/infrastructure/repositories/campaign-creator-repository_test.js
+++ api/tests/integration/infrastructure/repositories/campaign-creator-repository_test.js
@@ -1,18 +1,9 @@
-const { expect, databaseBuilder, catchErr, mockLearningContent } = require('../../../test-helper');
+const { expect, databaseBuilder, catchErr } = require('../../../test-helper');
 const campaignCreatorRepository = require('../../../../lib/infrastructure/repositories/campaign-creator-repository');
 const { UserNotAuthorizedToCreateCampaignError } = require('../../../../lib/domain/errors');
 
 describe('Integration | Repository | CampaignCreatorRepository', function () {
   describe('#get', function () {
-    let skillId;
-    beforeEach(function () {
-      skillId = 'skillId';
-
-      mockLearningContent({
-        skills: [{ id: skillId }],
-      });
-    });
-
     it('returns the creator for the given organization and user id', async function () {
       const { id: userId } = databaseBuilder.factory.buildUser();
       const { id: organizationId } = databaseBuilder.factory.buildOrganization({ canCollectProfiles: true });
@@ -26,47 +17,92 @@ describe('Integration | Repository | CampaignCreatorRepository', function () {
       expect(creator.notAllowedToCreateProfileCollectionCampaign).to.equal(false);
     });
 
-    it('returns the creator and the targetProfileId list for available targetProfileId', async function () {
-      const { id: userId } = databaseBuilder.factory.buildUser();
-      const { id: organizationId } = databaseBuilder.factory.buildOrganization({ canCollectProfiles: true });
-      databaseBuilder.factory.buildMembership({ organizationId, userId });
+    context('when there are target profiles', function () {
+      context('when target profiles are public', function () {
+        it('returns the public target profiles', async function () {
+          const { id: userId } = databaseBuilder.factory.buildUser();
+          const { id: organizationId } = databaseBuilder.factory.buildOrganization({ canCollectProfiles: true });
+          databaseBuilder.factory.buildMembership({ organizationId, userId });
 
-      const { id: targetProfilePublicId } = databaseBuilder.factory.buildTargetProfile({
-        isPublic: true,
-        outdated: false,
-      });
-      databaseBuilder.factory.buildTargetProfileSkill({ targetProfileId: targetProfilePublicId, skillId });
-      const { id: organizationTargetProfileId } = databaseBuilder.factory.buildTargetProfile({
-        isPublic: false,
-        outdated: false,
-        ownerOrganizationId: organizationId,
-      });
-      databaseBuilder.factory.buildTargetProfileSkill({ targetProfileId: organizationTargetProfileId, skillId });
-      const { id: targetProfileSharedId } = databaseBuilder.factory.buildTargetProfile({
-        isPublic: false,
-        outdated: false,
-      });
-      databaseBuilder.factory.buildTargetProfileSkill({ targetProfileId: targetProfileSharedId, skillId });
-      databaseBuilder.factory.buildTargetProfileShare({ targetProfileId: targetProfileSharedId, organizationId });
-      const { id: targetProfilePrivateId } = databaseBuilder.factory.buildTargetProfile({
-        isPublic: false,
-        outdated: false,
-      });
-      databaseBuilder.factory.buildTargetProfileSkill({ targetProfileId: targetProfilePrivateId, skillId });
-      const { id: targetProfileOutdatedId } = databaseBuilder.factory.buildTargetProfile({
-        isPublic: true,
-        outdated: true,
-      });
-      databaseBuilder.factory.buildTargetProfileSkill({ targetProfileId: targetProfileOutdatedId, skillId });
+          const { id: targetProfilePublicId } = databaseBuilder.factory.buildTargetProfile({
+            isPublic: true,
+            outdated: false,
+          });
 
-      await databaseBuilder.commit();
+          await databaseBuilder.commit();
 
-      const creator = await campaignCreatorRepository.get(userId, organizationId);
-      expect(creator.availableTargetProfileIds).to.exactlyContain([
-        targetProfilePublicId,
-        targetProfileSharedId,
-        organizationTargetProfileId,
-      ]);
+          const creator = await campaignCreatorRepository.get(userId, organizationId);
+          expect(creator.availableTargetProfileIds).to.exactlyContain([targetProfilePublicId]);
+        });
+      });
+
+      context('when the target profiles are private', function () {
+        it('returns the shared target profiles', async function () {
+          const { id: userId } = databaseBuilder.factory.buildUser();
+          const { id: organizationId } = databaseBuilder.factory.buildOrganization({ canCollectProfiles: true });
+          databaseBuilder.factory.buildMembership({ organizationId, userId });
+
+          const { id: targetProfileSharedId } = databaseBuilder.factory.buildTargetProfile({
+            isPublic: false,
+            outdated: false,
+          });
+          databaseBuilder.factory.buildTargetProfileShare({ targetProfileId: targetProfileSharedId, organizationId });
+          databaseBuilder.factory.buildTargetProfile({
+            isPublic: false,
+            outdated: false,
+          });
+
+          await databaseBuilder.commit();
+
+          const creator = await campaignCreatorRepository.get(userId, organizationId);
+          expect(creator.availableTargetProfileIds).to.exactlyContain([targetProfileSharedId]);
+        });
+
+        it('returns the target profiles is owned by the organization', async function () {
+          const { id: userId } = databaseBuilder.factory.buildUser();
+          const { id: organizationId } = databaseBuilder.factory.buildOrganization({ canCollectProfiles: true });
+          databaseBuilder.factory.buildMembership({ organizationId, userId });
+
+          const { id: organizationTargetProfileId } = databaseBuilder.factory.buildTargetProfile({
+            isPublic: false,
+            outdated: false,
+            ownerOrganizationId: organizationId,
+          });
+
+          await databaseBuilder.commit();
+
+          const creator = await campaignCreatorRepository.get(userId, organizationId);
+          expect(creator.availableTargetProfileIds).to.exactlyContain([organizationTargetProfileId]);
+        });
+      });
+
+      context('when target profiles are outdated', function () {
+        it('does not return target profiles', async function () {
+          const { id: userId } = databaseBuilder.factory.buildUser();
+          const { id: organizationId } = databaseBuilder.factory.buildOrganization({ canCollectProfiles: true });
+          databaseBuilder.factory.buildMembership({ organizationId, userId });
+
+          databaseBuilder.factory.buildTargetProfile({
+            isPublic: true,
+            outdated: true,
+          });
+          databaseBuilder.factory.buildTargetProfile({
+            isPublic: false,
+            outdated: true,
+            ownerOrganizationId: organizationId,
+          });
+          const { id: targetProfileSharedId } = databaseBuilder.factory.buildTargetProfile({
+            isPublic: false,
+            outdated: true,
+          });
+          databaseBuilder.factory.buildTargetProfileShare({ targetProfileId: targetProfileSharedId, organizationId });
+
+          await databaseBuilder.commit();
+
+          const creator = await campaignCreatorRepository.get(userId, organizationId);
+          expect(creator.availableTargetProfileIds).to.be.empty;
+        });
+      });
     });
 
     context('when the user is not a member the organization', function () {
