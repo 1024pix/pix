96e2946c961b3f8af5f147c9816cc1f4e65a25cb
P-Jeremy <jemyplu@gmail.com>
2021-11-09 15:59:10 +0100

    feat(certif): add complementary certifications in candidate detail modal

--- certif/tests/unit/models/certification-candidate_test.js
+++ certif/tests/unit/models/certification-candidate_test.js
@@ -6,6 +6,7 @@ module('Unit | Model | certification-candidate', function(hooks) {
   setupTest(hooks);
 
   test('it creates a CertificationCandidate', function(assert) {
+    // given
     const store = this.owner.lookup('service:store');
     const data = {
       firstName: 'Jean-Paul',
@@ -21,41 +22,68 @@ module('Unit | Model | certification-candidate', function(hooks) {
       birthPostalCode: 76260,
       sex: 1,
     };
-
+    // when
     const model = store.createRecord('certification-candidate', data);
+    // then
     assert.deepEqual(_pickModelData(data), _pickModelData(model));
   });
 
   module('#get sexLabel', () => {
     test('it get "Homme" for sex code "M"', function(assert) {
+      // given
       const store = this.owner.lookup('service:store');
       const data = {
         sex: 'M',
       };
-
+      // when
       const model = store.createRecord('certification-candidate', data);
-
+      // then
       assert.equal(model.sexLabel, 'Homme');
     });
 
     test('it get "Femme" for sex code "F"', function(assert) {
+      // given
       const store = this.owner.lookup('service:store');
       const data = {
         sex: 'F',
       };
-
+      // when
       const model = store.createRecord('certification-candidate', data);
-
+      // then
       assert.equal(model.sexLabel, 'Femme');
     });
 
     test('it get nothing if no sex code', function(assert) {
+      // given
       const store = this.owner.lookup('service:store');
       const data = {};
-
+      // when
       const model = store.createRecord('certification-candidate', data);
-
+      // then
       assert.equal(model.sexLabel, null);
+    });
+  });
+
+  module('#get complementaryCertificationsList', () => {
+    test('returns the complementary certification names as a string', function(assert) {
+      // given
+      const store = this.owner.lookup('service:store');
+      const data = {
+        complementaryCertifications: [
+          {
+            id: 1,
+            name: 'Pix+Edu',
+          },
+          {
+            id: 2,
+            name: 'Pix+Droit',
+          },
+        ],
+      };
+      // when
+      const model = store.createRecord('certification-candidate', data);
+      // then
+      assert.equal(model.complementaryCertificationsList, 'Pix+Edu, Pix+Droit');
     });
   });
 
