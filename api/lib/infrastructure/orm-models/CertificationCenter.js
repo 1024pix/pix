8ed81b56b5d46642560b49d761c345a3156697d0
Andreia Pena <andreia.pena@pix.fr>
2021-11-12 17:07:54 +0100

    [API] Create migration to add updatedAt column on certification centers table

--- api/lib/infrastructure/orm-models/CertificationCenter.js
+++ api/lib/infrastructure/orm-models/CertificationCenter.js
@@ -8,7 +8,7 @@ module.exports = Bookshelf.model(
   modelName,
   {
     tableName: 'certification-centers',
-    hasTimestamps: ['createdAt', null],
+    hasTimestamps: ['createdAt', 'updatedAt'],
 
     certificationCenterMemberships() {
       return this.hasMany('CertificationCenterMembership', 'certificationCenterId');
