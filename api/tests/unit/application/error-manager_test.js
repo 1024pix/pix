f8a2d8e58b5738daf616857d234fa20367f4c82d
Mathieu Gilet <mathieu.gilet@zenika.com>
2021-11-12 15:52:16 +0100

    feat(api): throw error if feature toggle is enabled and candidate is not authorized

--- api/tests/unit/application/error-manager_test.js
+++ api/tests/unit/application/error-manager_test.js
@@ -16,6 +16,7 @@ const {
   SchoolingRegistrationAlreadyLinkedToInvalidUserError,
   InvalidVerificationCodeError,
   EmailModificationDemandNotFoundOrExpiredError,
+  CandidateNotAuthorizedToJoinSessionError,
 } = require('../../../lib/domain/errors');
 const HttpErrors = require('../../../lib/application/http-errors.js');
 
@@ -321,6 +322,19 @@ describe('Unit | Application | ErrorManager', function () {
 
       // then
       expect(HttpErrors.BadRequestError).to.have.been.calledWithExactly(error.message);
+    });
+
+    it('should instantiate ForbiddenError when CandidateNotAuthorizedToJoinSessionError', async function () {
+      // given
+      const error = new CandidateNotAuthorizedToJoinSessionError();
+      sinon.stub(HttpErrors, 'ForbiddenError');
+      const params = { request: {}, h: hFake, error };
+
+      // when
+      await handle(params.request, params.h, params.error);
+
+      // then
+      expect(HttpErrors.ForbiddenError).to.have.been.calledWithExactly(error.message);
     });
   });
 });
