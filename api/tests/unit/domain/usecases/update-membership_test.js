35ae9528aff48be3e2991fc2d63f0ca5953219ef
Andreia Pena <andreia.pena@pix.fr>
2021-11-16 16:23:53 +0100

    [API] Remove certification center membership creation in update membership usecase

--- api/tests/unit/domain/usecases/update-membership_test.js
+++ api/tests/unit/domain/usecases/update-membership_test.js
@@ -2,24 +2,14 @@ const { expect, sinon, catchErr, domainBuilder } = require('../../../test-helper
 const { updateMembership } = require('../../../../lib/domain/usecases');
 const Membership = require('../../../../lib/domain/models/Membership');
 const { InvalidMembershipOrganizationRoleError } = require('../../../../lib/domain/errors');
-const DomainTransaction = require('../../../../lib/infrastructure/DomainTransaction');
 
 describe('Unit | UseCase | update-membership', function () {
   let membershipRepository;
-  let certificationCenterRepository;
-  let certificationCenterMembershipRepository;
 
   beforeEach(function () {
     membershipRepository = {
       updateById: sinon.stub(),
       get: sinon.stub(),
-    };
-    certificationCenterRepository = {
-      findByExternalId: sinon.stub(),
-    };
-    certificationCenterMembershipRepository = {
-      save: sinon.stub(),
-      isMemberOfCertificationCenter: sinon.stub(),
     };
   });
 
@@ -33,292 +23,41 @@ describe('Unit | UseCase | update-membership', function () {
     const error = await catchErr(updateMembership)({
       membership,
       membershipRepository,
-      certificationCenterRepository,
-      certificationCenterMembershipRepository,
     });
 
     // then
     expect(error).to.an.instanceOf(InvalidMembershipOrganizationRoleError);
   });
 
-  context('when the organizationRole to update is set to administrator', function () {
-    context('when the organization is SCO', function () {
-      context("when the membership's organization has a certification center", function () {
-        context('when the user is already a member of the certification center', function () {
-          it('should not create a certification center membership', async function () {
-            // given
-            const membershipId = 1;
-            const externalId = 'externalId';
-            const organization = domainBuilder.buildOrganization({ externalId, type: 'SCO' });
-            const givenMembership = new Membership({ id: membershipId, organizationRole: Membership.roles.ADMIN });
-            const userWhoseOrganizationRoleIsToUpdate = domainBuilder.buildUser();
-            const existingCertificationCenter = domainBuilder.buildCertificationCenter({ externalId });
-            const existingMembership = domainBuilder.buildMembership({
-              id: membershipId,
-              organizationRole: Membership.roles.MEMBER,
-              organization: organization,
-              user: userWhoseOrganizationRoleIsToUpdate,
-            });
+  it('should update the membership', async function () {
+    // given
+    const organization = domainBuilder.buildOrganization({ type: 'SUP' });
+    const membershipId = 100;
+    const givenMembership = new Membership({
+      id: membershipId,
+      organizationRole: Membership.roles.MEMBER,
+      updatedByUserId: 123,
+    });
+    const userWhoseOrganizationRoleIsToUpdate = domainBuilder.buildUser();
+    const existingMembership = domainBuilder.buildMembership({
+      id: membershipId,
+      organizationRole: Membership.roles.ADMIN,
+      organization,
+      user: userWhoseOrganizationRoleIsToUpdate,
+    });
+    const membershipWithRelatedUserAndOrganization = Symbol('a membership with related informations');
+    membershipRepository.get.withArgs(membershipId).resolves(existingMembership);
+    membershipRepository.updateById
+      .withArgs({ id: membershipId, membership: givenMembership })
+      .resolves(membershipWithRelatedUserAndOrganization);
 
-            membershipRepository.get.withArgs(membershipId).resolves(existingMembership);
-            certificationCenterRepository.findByExternalId
-              .withArgs({ externalId })
-              .resolves(existingCertificationCenter);
-            certificationCenterMembershipRepository.isMemberOfCertificationCenter
-              .withArgs(userWhoseOrganizationRoleIsToUpdate.id, existingCertificationCenter.id)
-              .resolves(true);
-
-            // when
-            await updateMembership({
-              membership: givenMembership,
-              membershipRepository,
-              certificationCenterRepository,
-              certificationCenterMembershipRepository,
-            });
-
-            // then
-            expect(certificationCenterMembershipRepository.save).to.not.have.been.called;
-          });
-        });
-
-        context('when the user is not yet a member of the certification center', function () {
-          it('should create a certification center membership and update organizationRole', async function () {
-            // given
-            const membershipId = 1;
-            const domainTransaction = Symbol('a domain transaction');
-            const externalId = 'externalId';
-            const organization = domainBuilder.buildOrganization({ externalId, type: 'SCO' });
-            const givenMembership = new Membership({ id: membershipId, organizationRole: Membership.roles.ADMIN });
-            const userWhoseOrganizationRoleIsToUpdate = domainBuilder.buildUser();
-            const existingCertificationCenter = domainBuilder.buildCertificationCenter({ externalId });
-            const existingMembership = domainBuilder.buildMembership({
-              id: membershipId,
-              organizationRole: Membership.roles.MEMBER,
-              organization: organization,
-              user: userWhoseOrganizationRoleIsToUpdate,
-            });
-
-            membershipRepository.get.withArgs(membershipId).resolves(existingMembership);
-            certificationCenterRepository.findByExternalId
-              .withArgs({ externalId })
-              .resolves(existingCertificationCenter);
-            certificationCenterMembershipRepository.isMemberOfCertificationCenter
-              .withArgs(userWhoseOrganizationRoleIsToUpdate.id, existingCertificationCenter.id)
-              .resolves(false);
-            DomainTransaction.execute = (lambda) => {
-              return lambda(domainTransaction);
-            };
-            const membershipUpdated = Symbol('updated membership with related organization and user');
-            membershipRepository.updateById
-              .withArgs({ id: membershipId, membership: givenMembership }, domainTransaction)
-              .resolves(membershipUpdated);
-
-            // when
-            const result = await updateMembership({
-              membership: givenMembership,
-              domainTransaction,
-              membershipRepository,
-              certificationCenterRepository,
-              certificationCenterMembershipRepository,
-            });
-
-            // then
-            expect(result).to.deep.equal(membershipUpdated);
-            expect(certificationCenterMembershipRepository.save).to.have.been.calledWith({
-              userId: userWhoseOrganizationRoleIsToUpdate.id,
-              certificationCenterId: existingCertificationCenter.id,
-              domainTransaction,
-            });
-          });
-        });
-      });
-
-      context("when the membership's organization has no certification center", function () {
-        it('should not create a certification center membership', async function () {
-          // given
-          const membershipId = 1;
-          const externalId = 'externalId';
-          const givenMembership = new Membership({ id: membershipId, organizationRole: Membership.roles.ADMIN });
-          const organization = domainBuilder.buildOrganization({ externalId, type: 'SCO' });
-          const userWhoseOrganizationRoleIsToUpdate = domainBuilder.buildUser();
-          const existingMembership = domainBuilder.buildMembership({
-            id: membershipId,
-            organization: organization,
-            user: userWhoseOrganizationRoleIsToUpdate,
-          });
-
-          membershipRepository.get.withArgs(membershipId).resolves(existingMembership);
-          certificationCenterRepository.findByExternalId.withArgs({ externalId }).resolves(null);
-
-          // when
-          await updateMembership({
-            membership: givenMembership,
-            membershipRepository,
-            certificationCenterRepository,
-            certificationCenterMembershipRepository,
-          });
-
-          // then
-          expect(certificationCenterMembershipRepository.save).to.not.have.been.called;
-        });
-
-        it('should update the membership', async function () {
-          // given
-          const organization = domainBuilder.buildOrganization({ type: 'SUP' });
-          const membershipId = 100;
-          const organizationRole = Membership.roles.ADMIN;
-          const givenMembership = new Membership({ id: membershipId, organizationRole });
-          const userWhoseOrganizationRoleIsToUpdate = domainBuilder.buildUser();
-          const existingMembership = domainBuilder.buildMembership({
-            id: membershipId,
-            organization: organization,
-            user: userWhoseOrganizationRoleIsToUpdate,
-          });
-          membershipRepository.get.withArgs(membershipId).resolves(existingMembership);
-          certificationCenterRepository.findByExternalId.resolves(null);
-          const membershipWithRelatedUserAndOrganization = Symbol('a membership with related informations');
-          membershipRepository.updateById
-            .withArgs({ id: membershipId, membership: givenMembership })
-            .resolves(membershipWithRelatedUserAndOrganization);
-
-          // when
-          const result = await updateMembership({
-            membership: givenMembership,
-            membershipRepository,
-            certificationCenterRepository,
-            certificationCenterMembershipRepository,
-          });
-
-          // then
-          expect(result).to.equal(membershipWithRelatedUserAndOrganization);
-        });
-      });
+    // when
+    const result = await updateMembership({
+      membership: givenMembership,
+      membershipRepository,
     });
 
-    context('when the organization is not SCO', function () {
-      it('should not create a certification center membership', async function () {
-        // given
-        const membershipId = 1;
-        const givenMembership = new Membership({ id: membershipId, organizationRole: Membership.roles.ADMIN });
-        const externalId = 'externalId';
-        const organization = domainBuilder.buildOrganization({ type: 'SUP', externalId });
-        const userWhoseOrganizationRoleIsToUpdate = domainBuilder.buildUser();
-        const existingMembership = domainBuilder.buildMembership({
-          id: membershipId,
-          organizationRole: Membership.roles.MEMBER,
-          organization,
-          user: userWhoseOrganizationRoleIsToUpdate,
-        });
-
-        membershipRepository.get.withArgs(membershipId).resolves(existingMembership);
-
-        // when
-        await updateMembership({
-          membership: givenMembership,
-          membershipRepository,
-          certificationCenterRepository,
-          certificationCenterMembershipRepository,
-        });
-
-        // then
-        expect(certificationCenterRepository.findByExternalId).to.not.have.been.called;
-        expect(certificationCenterMembershipRepository.isMemberOfCertificationCenter).to.not.have.been.called;
-        expect(certificationCenterMembershipRepository.save).to.not.have.been.called;
-      });
-    });
-
-    context('when the organization has no external id', function () {
-      it('should not create a certification center membership', async function () {
-        // given
-        const membershipId = 1;
-        const organization = domainBuilder.buildOrganization({ type: 'SCO', externalId: null });
-        const userWhoseOrganizationRoleIsToUpdate = domainBuilder.buildUser();
-        const existingMembership = domainBuilder.buildMembership({
-          id: membershipId,
-          organizationRole: Membership.roles.MEMBER,
-          organization,
-          user: userWhoseOrganizationRoleIsToUpdate,
-        });
-        const givenMembership = new Membership({ id: membershipId, organizationRole: Membership.roles.ADMIN });
-
-        membershipRepository.get.withArgs(membershipId).resolves(existingMembership);
-
-        // when
-        await updateMembership({
-          membership: givenMembership,
-          membershipRepository,
-          certificationCenterRepository,
-          certificationCenterMembershipRepository,
-        });
-
-        // then
-        expect(certificationCenterRepository.findByExternalId).to.not.have.been.called;
-        expect(certificationCenterMembershipRepository.isMemberOfCertificationCenter).to.not.have.been.called;
-        expect(certificationCenterMembershipRepository.save).to.not.have.been.called;
-      });
-    });
-  });
-
-  context('when the organizationRole to update is set to member', function () {
-    it('should not create a certification center membership', async function () {
-      // given
-      const organization = domainBuilder.buildOrganization({ type: 'SCO' });
-      const membershipId = 1;
-      const givenMembership = new Membership({ id: membershipId, organizationRole: Membership.roles.MEMBER });
-      const userWhoseOrganizationRoleIsToUpdate = domainBuilder.buildUser();
-      const existingMembership = domainBuilder.buildMembership({
-        id: membershipId,
-        organization: organization,
-        organizationRole: Membership.roles.ADMIN,
-        user: userWhoseOrganizationRoleIsToUpdate,
-      });
-      membershipRepository.get.withArgs(membershipId).resolves(existingMembership);
-
-      // when
-      await updateMembership({
-        membership: givenMembership,
-        membershipRepository,
-        certificationCenterRepository,
-        certificationCenterMembershipRepository,
-      });
-
-      // then
-      expect(certificationCenterMembershipRepository.save).to.not.have.been.called;
-    });
-
-    it('should update the membership', async function () {
-      // given
-      const organization = domainBuilder.buildOrganization({ type: 'SUP' });
-      const membershipId = 100;
-      const givenMembership = new Membership({
-        id: membershipId,
-        organizationRole: Membership.roles.MEMBER,
-        updatedByUserId: 123,
-      });
-      const userWhoseOrganizationRoleIsToUpdate = domainBuilder.buildUser();
-      const existingMembership = domainBuilder.buildMembership({
-        id: membershipId,
-        organizationRole: Membership.roles.ADMIN,
-        organization,
-        user: userWhoseOrganizationRoleIsToUpdate,
-      });
-      const membershipWithRelatedUserAndOrganization = Symbol('a membership with related informations');
-      membershipRepository.get.withArgs(membershipId).resolves(existingMembership);
-      certificationCenterRepository.findByExternalId.resolves(null);
-      membershipRepository.updateById
-        .withArgs({ id: membershipId, membership: givenMembership })
-        .resolves(membershipWithRelatedUserAndOrganization);
-
-      // when
-      const result = await updateMembership({
-        membership: givenMembership,
-        membershipRepository,
-        certificationCenterRepository,
-        certificationCenterMembershipRepository,
-      });
-
-      // then
-      expect(result).to.equal(membershipWithRelatedUserAndOrganization);
-    });
+    // then
+    expect(result).to.equal(membershipWithRelatedUserAndOrganization);
   });
 });
