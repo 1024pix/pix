3be47c2a7873c04d91611f3e8592c03711c00303
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-11-17 17:29:12 +0100

    feat(api): add skill sets in badge criterion deserialization

--- api/lib/infrastructure/serializers/jsonapi/badge-criteria-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/badge-criteria-serializer.js
@@ -3,16 +3,17 @@ module.exports = {
   serialize(badgeCriterion = {}) {
     return new Serializer('badge-criteria', {
       ref: 'id',
-      attributes: ['scope', 'threshold', 'skill'],
+      attributes: ['scope', 'threshold'],
     }).serialize(badgeCriterion);
   },
 
   deserialize(badgeCriterionJson) {
     const { scope, threshold } = badgeCriterionJson.data.attributes;
+    const skillSets = badgeCriterionJson.data.relationships?.['skill-sets']?.data ?? [];
     return {
       scope,
       threshold,
-      skillSetIds: [],
+      skillSetIds: skillSets.map(({ id }) => id),
     };
   },
 };
