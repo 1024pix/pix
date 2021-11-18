3d5bff911b6994cd72e81a09c9cd3edf142386e4
Clément Bourgoin <clement.bourgoin@pix.fr>
2021-11-18 13:01:08 +0100

    tests(certif): add test for method displayComplementaryCertification

--- certif/tests/unit/controllers/authenticated/sessions/details/certification-candidates_test.js
+++ certif/tests/unit/controllers/authenticated/sessions/details/certification-candidates_test.js
@@ -1,5 +1,6 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import Service from '@ember/service';
 
 module('Unit | Controller | authenticated/sessions/details/certification-candidates', function(hooks) {
   setupTest(hooks);
@@ -31,4 +32,39 @@ module('Unit | Controller | authenticated/sessions/details/certification-candida
       assert.false(shouldDisplayStudentList);
     });
   });
+
+  module('#get displayComplementaryCertification', function() {
+    test('should return false if feature toggle is false', function(assert) {
+      // given
+      _stubFeatureToggle(this, 'isComplementaryCertificationSubscriptionEnabled', false);
+      const controller = this.owner.lookup('controller:authenticated/sessions/details/certification-candidates');
+
+      // when
+      const displayComplementaryCertification = controller.displayComplementaryCertification;
+
+      // then
+      assert.false(displayComplementaryCertification);
+    });
+
+    test('should return true if feature toggle is true', function(assert) {
+      // given
+      _stubFeatureToggle(this, 'isComplementaryCertificationSubscriptionEnabled', true);
+      const controller = this.owner.lookup('controller:authenticated/sessions/details/certification-candidates');
+
+      // when
+      const displayComplementaryCertification = controller.displayComplementaryCertification;
+
+      // then
+      assert.true(displayComplementaryCertification);
+    });
+  });
 });
+
+function _stubFeatureToggle(controller, featureToggleName, featureToggleValue) {
+  const featureToggles = {};
+  featureToggles[featureToggleName] = featureToggleValue;
+  class FeatureTogglesStub extends Service {
+    featureToggles = featureToggles;
+  }
+  controller.owner.register('service:feature-toggles', FeatureTogglesStub);
+}
