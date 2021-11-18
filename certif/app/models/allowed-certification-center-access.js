7da47d1a86b8fde5494b729a44bde63eea834777
Clément Bourgoin <clement.bourgoin@pix.fr>
2021-11-18 17:00:37 +0100

    fix(certif): hide complementary certif for centers that have none

--- certif/app/models/allowed-certification-center-access.js
+++ certif/app/models/allowed-certification-center-access.js
@@ -23,4 +23,8 @@ export default class AllowedCertificationCenterAccess extends Model {
   get isAccessRestricted() {
     return this.isAccessBlockedCollege || this.isAccessBlockedLycee || this.isAccessBlockedAEFE || this.isAccessBlockedAgri;
   }
+
+  get hasHabilitations() {
+    return this.habilitations.length > 0;
+  }
 }
