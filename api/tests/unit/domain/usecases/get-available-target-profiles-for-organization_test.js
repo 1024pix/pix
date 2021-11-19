e7bb07a72f25ff6b2890bd5db53a81034d4158d3
ajoanny <ajoanny@octo.com>
2021-11-19 17:44:57 +0100

    [API] create use case

---
+++ api/tests/unit/domain/usecases/get-available-target-profiles-for-organization_test.js
@@ -0,0 +1,21 @@
+const { expect, sinon } = require('../../../test-helper');
+const getAvailableTargetProfilesForOrganization = require('../../../../lib/domain/usecases/get-available-target-profiles-for-organization');
+
+describe('Unit | UseCase | get-available-target-profiles-for-organization', function () {
+  it('returns the target profile available for the given organizations', async function () {
+    const organizationId = 12;
+    const expectedTargetProfiles = Symbol('TargetProfileForSpecifier');
+    const TargetProfileForSpecifierRepository = { availableForOrganization: sinon.stub() };
+
+    TargetProfileForSpecifierRepository.availableForOrganization
+      .withArgs(organizationId)
+      .resolves(expectedTargetProfiles);
+
+    const targetProfiles = await getAvailableTargetProfilesForOrganization({
+      organizationId,
+      TargetProfileForSpecifierRepository,
+    });
+
+    expect(targetProfiles).to.equal(expectedTargetProfiles);
+  });
+});
