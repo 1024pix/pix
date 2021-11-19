ec02d4a76adc3d6f671ab58219f59e9617791411
Anne-G <ang.schall@gmail.com>
2021-11-19 17:10:39 +0100

    [orga] hide cancelled invitations from list

---
+++ orga/tests/acceptance/invitations-list_test.js
@@ -0,0 +1,38 @@
+import { module, test } from 'qunit';
+import { visit } from '@ember/test-helpers';
+import { clickByLabel } from '../helpers/testing-library';
+import { setupApplicationTest } from 'ember-qunit';
+import authenticateSession from '../helpers/authenticate-session';
+import { createPrescriberForOrganization } from '../helpers/test-init';
+
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+
+module('Acceptance | Invitations list', function (hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  test('it should be possible to cancel an invitation', async function (assert) {
+    // given
+    const userAttributes = {};
+    const invitationAttributes = {
+      id: 123,
+      email: 'gigi@example.net',
+      status: 'pending',
+      updatedAt: '2019-10-08',
+    };
+    const organizationAttributes = { id: 777, name: 'Collège Foufoufou' };
+    const organizationRole = 'ADMIN';
+
+    const user = createPrescriberForOrganization(userAttributes, organizationAttributes, organizationRole);
+    server.create('organization-invitation', invitationAttributes);
+    await authenticateSession(user.id);
+
+    // when
+    await visit('/equipe/invitations');
+    await clickByLabel('Supprimer l’invitation');
+
+    // then
+    assert.notContains('gigi@example.net');
+    assert.ok(true);
+  });
+});
