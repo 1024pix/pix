cbb1941e65f4977d641f3f66fbdc6e86c4f842f3
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-11-23 11:53:16 +0100

    feat(admin): add skill-set serializer

---
+++ admin/app/serializers/skill-set.js
@@ -0,0 +1,9 @@
+import JSONAPISerializer from '@ember-data/serializer/json-api';
+
+export default class SkillSetSerializer extends JSONAPISerializer {
+  serialize(snapshot, options) {
+    const json = super.serialize(snapshot, options);
+    delete json.data.relationships?.badge;
+    return json;
+  }
+}
