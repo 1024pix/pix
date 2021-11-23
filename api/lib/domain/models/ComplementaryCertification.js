73a1eca2ab8990a07fe6fe7e1e98ddd7e8888903
Antoine Ceol <antoine.ceol@gmail.com>
2021-11-23 17:36:32 +0100

    refacto(api) Extract comp certif name to ComplementaryCertification

--- api/lib/domain/models/ComplementaryCertification.js
+++ api/lib/domain/models/ComplementaryCertification.js
@@ -1,8 +1,14 @@
+const PIX_PLUS_DROIT = 'Pix+ Droit';
+const CLEA = 'CléA Numérique';
+
 class ComplementaryCertification {
   constructor({ id, name }) {
     this.id = id;
     this.name = name;
   }
 }
+
+ComplementaryCertification.PIX_PLUS_DROIT = PIX_PLUS_DROIT;
+ComplementaryCertification.CLEA = CLEA;
 
 module.exports = ComplementaryCertification;
