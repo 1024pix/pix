1213edf268eae90acc68fa60732bfffe03415eb0
Pierre TOP (TOPI) <ptop@octo.com>
2021-11-25 12:52:48 +0100

    Prevent database drop on Scalingo

--- api/scripts/database/drop-database.js
+++ api/scripts/database/drop-database.js
@@ -3,6 +3,19 @@ const logger = require('../../lib/infrastructure/logger');
 const PgClient = require('../PgClient');
 const { PGSQL_NON_EXISTENT_DATABASE_ERROR } = require('../../db/pgsql-errors');
 
+function isPlatformScalingo() {
+  return Boolean(process.env.CONTAINER);
+}
+
+function preventDatabaseDropAsItCannotBeCreatedAgain() {
+  if (isPlatformScalingo()) {
+    logger.error('Database will not be dropped, as it would require to recreate the addon');
+    process.exit(1);
+  }
+}
+
+preventDatabaseDropAsItCannotBeCreatedAgain();
+
 const dbUrl = process.env.NODE_ENV === 'test' ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL;
 
 const url = new URL(dbUrl);
