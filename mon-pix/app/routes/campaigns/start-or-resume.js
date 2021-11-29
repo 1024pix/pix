7a2478cf0aa2cb44fde4618f9fb1cb671d2699c7
Lise Quesnel <quesnel.lise@gmail.com>
2021-11-29 18:30:38 +0100

    [APP] Remove local state in start-or-resume

--- mon-pix/app/routes/campaigns/start-or-resume.js
+++ mon-pix/app/routes/campaigns/start-or-resume.js
@@ -8,29 +8,17 @@ export default class StartOrResumeRoute extends Route.extend(SecuredRouteMixin) 
   @service session;
   @service campaignStorage;
 
-  state = null;
-
-  constructor() {
-    super(...arguments);
-    this._resetState();
-  }
-
   async beforeModel() {
     this.authenticationRoute = 'inscription';
     const campaign = this.modelFor('campaigns');
-    if (this._shouldResetState(campaign.code)) {
-      this._resetState();
-    }
 
-    this._updateStateFrom({ campaign, session: this.session });
-
-    if (this._shouldVisitPoleEmploiLoginPage) {
+    if (this._shouldVisitPoleEmploiLoginPage(campaign)) {
       this.authenticationRoute = 'login-pe';
-    } else if (this._shouldLoginToAccessRestrictedCampaign) {
+    } else if (this._shouldLoginToAccessSCORestrictedCampaign(campaign)) {
       this.authenticationRoute = 'campaigns.restricted.login-or-register-to-access';
-    } else if (this._shouldJoinFromMediacentre) {
+    } else if (this._shouldJoinFromMediacentre(campaign)) {
       this.authenticationRoute = 'campaigns.restricted.join-from-mediacentre';
-    } else if (this._shouldJoinSimplifiedCampaignAsAnonymous) {
+    } else if (this._shouldJoinSimplifiedCampaignAsAnonymous(campaign)) {
       this.authenticationRoute = 'campaigns.anonymous';
     }
 
@@ -56,61 +44,28 @@ export default class StartOrResumeRoute extends Route.extend(SecuredRouteMixin) 
     }
   }
 
-  _resetState() {
-    if (this?.state?.campaignCode) {
-      this.campaignStorage.clear(this.state.campaignCode);
-    }
-    this.state = {
-      campaignCode: null,
-      isCampaignRestricted: false,
-      isCampaignForSCOOrganization: false,
-      isCampaignSimplifiedAccess: false,
-      hasUserSeenJoinPage: false,
-      isUserLogged: false,
-      externalUser: null,
-      isCampaignPoleEmploi: false,
-      isUserLoggedInPoleEmploi: false,
-    };
-  }
-
-  _updateStateFrom({ campaign = {}, session }) {
+  _shouldLoginToAccessSCORestrictedCampaign(campaign) {
+    const isUserExternal = get(this.session, 'data.externalUser');
     const hasUserSeenJoinPage = this.campaignStorage.get(campaign.code, 'hasUserSeenJoinPage');
-    this.state = {
-      campaignCode: get(campaign, 'code', this.state.campaignCode),
-      isCampaignRestricted: get(campaign, 'isRestricted', this.state.isCampaignRestricted),
-      isCampaignSimplifiedAccess: get(campaign, 'isSimplifiedAccess', this.state.isCampaignSimplifiedAccess),
-      isCampaignForSCOOrganization: get(campaign, 'organizationType') === 'SCO',
-      hasUserSeenJoinPage,
-      isUserLogged: this.session.isAuthenticated,
-      externalUser: get(session, 'data.externalUser'),
-      isCampaignPoleEmploi: get(campaign, 'organizationIsPoleEmploi', this.state.isCampaignPoleEmploi),
-      isUserLoggedInPoleEmploi:
-        get(session, 'data.authenticated.source') === 'pole_emploi_connect' || this.state.isUserLoggedInPoleEmploi,
-    };
-  }
-
-  _shouldResetState(campaignCode) {
-    return campaignCode !== this.state.campaignCode;
-  }
-
-  get _shouldLoginToAccessRestrictedCampaign() {
     return (
-      this.state.isCampaignRestricted &&
-      this.state.isCampaignForSCOOrganization &&
-      !this.state.isUserLogged &&
-      (!this.state.externalUser || this.state.hasUserSeenJoinPage)
+      campaign.isRestricted &&
+      campaign.organizationType === 'SCO' &&
+      !this.session.isAuthenticated &&
+      (!isUserExternal || hasUserSeenJoinPage)
     );
   }
 
-  get _shouldVisitPoleEmploiLoginPage() {
-    return this.state.isCampaignPoleEmploi && !this.state.isUserLoggedInPoleEmploi;
+  _shouldVisitPoleEmploiLoginPage(campaign) {
+    const isUserLoggedInPoleEmploi = get(this.session, 'data.authenticated.source') === 'pole_emploi_connect';
+    return campaign.organizationIsPoleEmploi && !isUserLoggedInPoleEmploi;
   }
 
-  get _shouldJoinFromMediacentre() {
-    return this.state.isCampaignRestricted && this.state.externalUser;
+  _shouldJoinFromMediacentre(campaign) {
+    const isUserExternal = get(this.session, 'data.externalUser');
+    return campaign.isRestricted && isUserExternal;
   }
 
-  get _shouldJoinSimplifiedCampaignAsAnonymous() {
-    return this.state.isCampaignSimplifiedAccess && !this.state.isUserLogged;
+  _shouldJoinSimplifiedCampaignAsAnonymous(campaign) {
+    return campaign.isSimplifiedAccess && !this.session.isAuthenticated;
   }
 }
