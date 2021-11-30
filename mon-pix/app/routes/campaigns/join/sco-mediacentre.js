12548b72ac23bf1d7b830f69c6063c769d724457
Lise Quesnel <quesnel.lise@gmail.com>
2021-11-30 18:47:08 +0100

    [APP] Mark JoinFromMediacentreRoute as UnauthenticatedRoute

--- mon-pix/app/routes/campaigns/join/sco-mediacentre.js
+++ mon-pix/app/routes/campaigns/join/sco-mediacentre.js
@@ -1,7 +1,8 @@
 import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default class ScoMediacentreRoute extends Route {
+export default class ScoMediacentreRoute extends Route.extend(UnauthenticatedRouteMixin) {
   @service currentUser;
   @service session;
 
