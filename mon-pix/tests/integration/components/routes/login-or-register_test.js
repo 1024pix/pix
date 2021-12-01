2ef324fa24028e71eef6279fc8a72ce07b6f4779
Lise Quesnel <quesnel.lise@gmail.com>
2021-12-01 11:14:38 +0100

    [APP] Replace acceptance test by integration test

--- mon-pix/tests/integration/components/routes/login-or-register_test.js
+++ mon-pix/tests/integration/components/routes/login-or-register_test.js
@@ -3,6 +3,7 @@ import { beforeEach, describe, it } from 'mocha';
 import setupIntlRenderingTest from '../../../helpers/setup-intl-rendering';
 import { find, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { clickByLabel } from '../../../helpers/click-by-label';
 
 describe('Integration | Routes | routes/login-or-register', function () {
   setupIntlRenderingTest();
@@ -10,23 +11,38 @@ describe('Integration | Routes | routes/login-or-register', function () {
     this.set('toggleFormsVisibility', '');
   });
 
-  it('should render', async function () {
-    // when
-    await render(hbs`{{routes/login-or-register toggleFormsVisibility=toggleFormsVisibility}}`);
-
-    // then
-    expect(find('.login-or-register')).to.exist;
-  });
-
   it('should display the organization name the user is invited to', async () => {
     // when
     await render(
-      hbs`{{routes/login-or-register organizationName='Organization Aztec' toggleFormsVisibility=toggleFormsVisibility}}`
+      hbs`<Routes::LoginOrRegister @organizationName="Organization Aztec" @toggleFormsVisibility=toggleFormsVisibility/>`
     );
 
     // then
-    expect(find('.login-or-register-panel__invitation').textContent).to.equal(
-      'Organization Aztec vous invite à rejoindre Pix'
+    expect('Organization Aztec vous invite à rejoindre Pix').to.exist;
+  });
+
+  it('should contain an open register form and closed login form', async function () {
+    // when
+    await render(
+      hbs`<Routes::LoginOrRegister @displayRegisterForm={{true}} @toggleFormsVisibility=toggleFormsVisibility/>`
     );
+
+    // then
+    expect(find('.register-form')).to.exist;
+    expect(find('.login-form')).to.not.exist;
+  });
+
+  it('should open the login panel and close the register panel when clicking on login button', async function () {
+    // given
+    await render(
+      hbs`<Routes::LoginOrRegister @displayRegisterForm={{false}} @toggleFormsVisibility=toggleFormsVisibility/>`
+    );
+
+    // when
+    await clickByLabel(this.intl.t('pages.login-or-register.login-form.button'));
+
+    // then
+    expect(find('.register-form')).to.not.exist;
+    expect(find('.login-form')).to.exist;
   });
 });
