1ce576fb2e82a6b75e75f42bc0f67ff9544ce68a
Mathieu Gilet <mathieu.gilet@zenika.com>
2021-12-06 15:45:37 +0100

    refactor(app): split certification access in two distinct routes

--- mon-pix/app/controllers/certifications/join.js
+++ mon-pix/app/controllers/certifications/join.js
@@ -3,15 +3,14 @@ import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
-export default class StartCertificationController extends Controller {
+export default class JoinCertificationController extends Controller {
   @service currentUser;
+  @service router;
 
   @tracked displayCongratulationsBanner = true;
-  @tracked currentStep = 'join';
-  @tracked sessionId = null;
 
   get showCongratulationsBanner() {
-    return this.displayCongratulationsBanner && this.currentStep === 'join';
+    return this.displayCongratulationsBanner;
   }
 
   @action
@@ -21,7 +20,6 @@ export default class StartCertificationController extends Controller {
 
   @action
   changeStep(sessionId) {
-    this.sessionId = sessionId;
-    this.currentStep = 'start';
+    this.router.transitionTo('certifications.start', sessionId);
   }
 }
