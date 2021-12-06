7bd4da711838b6c523f19ccd7eef96ae5ee62c59
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-12-06 15:30:37 +0100

    fix(admin) fix acceptance tests

--- admin/app/models/skill-set.js
+++ admin/app/models/skill-set.js
@@ -1,10 +1,10 @@
-import Model, { attr, belongsTo } from '@ember-data/model';
+import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 
 export default class SkillSet extends Model {
   @attr('string') name;
   @attr('array') skillIds;
 
   @belongsTo('badge') badge;
-  //   @belongsTo('badge-criterion') criterion;
-  //   @hasMany('skill') skillIds;
+  @belongsTo('badge-criterion') criterion;
+  @hasMany('skill') skills;
 }
