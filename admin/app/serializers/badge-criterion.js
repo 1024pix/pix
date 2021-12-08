53921e3984c9ec00f4cde7bca6cf1f0d6fb7214e
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-12-08 15:24:01 +0100

    fix(admin): badge criteria serialization

--- admin/app/serializers/badge-criterion.js
+++ admin/app/serializers/badge-criterion.js
@@ -1,9 +1,12 @@
 import JSONAPISerializer from '@ember-data/serializer/json-api';
 
 export default class BadgeCriterionSerializer extends JSONAPISerializer {
-  serialize(snapshot, options) {
-    const json = super.serialize(snapshot, options);
-    delete json.data.relationships?.badge;
-    return json;
-  }
+  attrs = {
+    skillSets: {
+      serialize: true,
+    },
+    badge: {
+      serialize: false,
+    },
+  };
 }
