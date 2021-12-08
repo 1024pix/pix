360f230481e6a6757eba43f7ad3e3d1652427d8d
yschoueri <yasmina.schoueri@gmail.com>
2021-12-08 16:00:26 +0100

    [ORGA] use ember-testing-library in campaign creation acceptance test

--- orga/tests/helpers/testing-library.js
+++ orga/tests/helpers/testing-library.js
@@ -1,6 +1,5 @@
 import { click, fillIn } from '@ember/test-helpers';
 import { getScreen } from '@1024pix/ember-testing-library';
-import { within } from '@testing-library/dom';
 
 /**
  * Click on a DOM element selected by a name.
@@ -24,18 +23,4 @@ export function fillInByLabel(label, value) {
   const { getByLabelText } = getScreen();
   const element = getByLabelText(label);
   return fillIn(element, value);
-}
-
-/**
- * Select a choice for a given label.
- *
- * @param {*} label Label of field to fill.
- * @param {*} answerText Label of choice to select.
- * @returns Promise of the filling.
- */
-export function selectChoiceForLabel(label, answerText) {
-  const { getByText } = getScreen();
-  const parent = getByText(label).parentNode;
-  const answer = within(parent).getByText(answerText);
-  return click(answer);
 }
