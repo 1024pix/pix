5a6fedb6a67530e759bd534ac25fd4f45c712be9
Antoine Ceol <antoine.ceol@gmail.com>
2021-12-08 09:13:53 +0100

    refacto(api) drop unused tables

---
+++ api/db/migrations/20211208080513_drop-accreditated-badges-table.js
@@ -0,0 +1,14 @@
+const TABLE_NAME = 'accredited-badges';
+
+exports.up = (knex) => {
+  return knex.schema.dropTable(TABLE_NAME);
+};
+
+exports.down = (knex) => {
+  return knex.schema.createTable(TABLE_NAME, (t) => {
+    t.increments().primary();
+    t.integer('accreditationId').references('accreditations.id').notNullable();
+    t.integer('badgeId').references('badges.id').notNullable();
+    t.dateTime('createdAt').notNullable().defaultTo(knex.fn.now());
+  });
+};
