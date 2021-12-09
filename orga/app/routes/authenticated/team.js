85f20350d1a87e66646407661c593adc42818478
Anne-Marie Esteves <anne-marie.esteves@octo.com>
2021-12-09 11:13:56 +0100

    [ORGA] display team route for all authenticated orga members

--- orga/app/routes/authenticated/team.js
+++ orga/app/routes/authenticated/team.js
@@ -1,13 +1,3 @@
-import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 
-export default class TeamRoute extends Route {
-  @service currentUser;
-
-  beforeModel() {
-    super.beforeModel(...arguments);
-    if (!this.currentUser.isAdminInOrganization) {
-      return this.replaceWith('application');
-    }
-  }
-}
+export default class TeamRoute extends Route {}
