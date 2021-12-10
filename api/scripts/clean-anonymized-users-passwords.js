8f0f46c508e38e0b291ffc68ae3db8481c508117
Anne-G <ang.schall@gmail.com>
2021-12-10 17:57:17 +0100

    [api] add script to clean passwords from anonymized users

--- api/scripts/clean-anonymized-users-passwords.js
+++ api/scripts/clean-anonymized-users-passwords.js
@@ -1,5 +1,10 @@
+'use strict';
+require('dotenv').config();
+
+const _ = require('lodash');
 const bluebird = require('bluebird');
 
+const { parseCsvWithHeader } = require('./helpers/csvHelpers');
 const authenticationMethodRepository = require('../lib/infrastructure/repositories/authentication-method-repository');
 const AuthenticationMethod = require('../lib/domain/models/AuthenticationMethod');
 
@@ -15,6 +20,44 @@ async function cleanAnonymizedUsersPasswords({ arrayOfAnonymizedUsersIds }) {
     }
   });
   return anonymizedUserIdsWithPasswordDeleted;
+}
+
+async function main() {
+  console.log('Starting cleaning anonymized users with passwords');
+
+  try {
+    const filePath = process.argv[2];
+
+    const anonymizedUsersIds = await parseCsvWithHeader(filePath);
+    const arrayOfAnonymizedUsersIds = _(anonymizedUsersIds)
+      .map((user) => user.ID)
+      .filter((userId) => !_.isNil(userId))
+      .value();
+
+    if (arrayOfAnonymizedUsersIds.length < 1) {
+      throw new Error('ID column must be present in CSV');
+    }
+    const anonymizedUserIdsWithPasswordDeleted = await cleanAnonymizedUsersPasswords({ arrayOfAnonymizedUsersIds });
+
+    console.log(
+      "\nDone. Here the list of user's id which password was deleted : ",
+      anonymizedUserIdsWithPasswordDeleted
+    );
+  } catch (error) {
+    console.error(error);
+
+    process.exit(1);
+  }
+}
+
+if (require.main === module) {
+  main().then(
+    () => process.exit(0),
+    (err) => {
+      console.error(err);
+      process.exit(1);
+    }
+  );
 }
 
 module.exports = { cleanAnonymizedUsersPasswords };
