fd7c439d8b26060777ad9f513b2bcac9ece0c4c4
Clément Bourgoin <clement.bourgoin@pix.fr>
2021-12-13 17:08:15 +0100

    feat(certif): add supervisor password to session model

--- certif/app/models/session.js
+++ certif/app/models/session.js
@@ -29,6 +29,7 @@ export default class Session extends Model {
   @attr('string') room;
   @attr('string') status;
   @attr('string') examinerGlobalComment;
+  @attr('string') supervisorPassword;
   @attr() certificationCenterId;
   @hasMany('certificationReport') certificationReports;
 
