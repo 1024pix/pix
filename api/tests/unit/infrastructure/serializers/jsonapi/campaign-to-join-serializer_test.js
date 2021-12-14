8b84a8efdca4230bb280218cb8c5caa6b72f817d
Rousseau Corentin <cro@eonix.be>
2021-12-14 12:28:23 +0100

    fix(api):update test for  campaign-to-join-serializer & organization-controller

--- api/tests/unit/infrastructure/serializers/jsonapi/campaign-to-join-serializer_test.js
+++ api/tests/unit/infrastructure/serializers/jsonapi/campaign-to-join-serializer_test.js
@@ -32,7 +32,7 @@ describe('Unit | Serializer | JSONAPI | campaign-to-join-serializer', function (
             'organization-type': campaignToJoin.organizationType,
             'organization-logo-url': campaignToJoin.organizationLogoUrl,
             'organization-show-nps': campaignToJoin.organizationShowNPS,
-            'organizaton-form-nps-url': campaignToJoin.organizationFormNPSUrl,
+            'organization-form-nps-url': campaignToJoin.organizationFormNPSUrl,
             'target-profile-name': campaignToJoin.targetProfileName,
             'target-profile-image-url': campaignToJoin.targetProfileImageUrl,
             'custom-result-page-text': campaignToJoin.customResultPageText,
