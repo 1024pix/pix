6be287b9fa22053fc9411b5a2274975922ac5e7e
Anne-G <ang.schall@gmail.com>
2021-12-14 17:00:37 +0100

    [certif] override component-test blueprint

---
+++ certif/blueprints/component-test/files/tests/integration/components/__name___test.js
@@ -0,0 +1,18 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component |  <%= dasherizedModuleName %>', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('replace this by your real test', async function(assert) {
+    // given
+
+    //  when
+    await render(hbs`<<%= classifiedModuleName %> />`);
+
+    // then
+    assert.ok(true);
+  });
+});
