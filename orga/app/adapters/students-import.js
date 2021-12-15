bbece314b947b191fb5eaa0e4570cb352c9e8869
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2021-12-15 12:22:44 +0100

    refactor: handle file type validation in adapter and catch error in _handleError method

--- orga/app/adapters/students-import.js
+++ orga/app/adapters/students-import.js
@@ -1,6 +1,8 @@
 import ApplicationAdapter from './application';
 
 export default class StudentImportsAdapter extends ApplicationAdapter {
+  static FORMAT_NOT_SUPPORTED_ERROR = 'format-not-supported-error';
+
   addStudentsCsv(organizationId, files) {
     if (!files || files.length === 0) return;
 
@@ -15,10 +17,16 @@ export default class StudentImportsAdapter extends ApplicationAdapter {
     return this.ajax(url, 'POST', { data: files[0] });
   }
 
-  importStudentsSiecle(organizationId, files, format) {
+  importStudentsSiecle(organizationId, files, acceptedFormat) {
     if (!files || files.length === 0) return;
 
-    const url = `${this.host}/${this.namespace}/organizations/${organizationId}/schooling-registrations/import-siecle?format=${format}`;
-    return this.ajax(url, 'POST', { data: files[0] });
+    const fileToUpload = files[0];
+    const fileToUploadMimeType = fileToUpload.type;
+    if (!fileToUploadMimeType?.includes(acceptedFormat)) {
+      throw new Error(StudentImportsAdapter.FORMAT_NOT_SUPPORTED_ERROR);
+    }
+
+    const url = `${this.host}/${this.namespace}/organizations/${organizationId}/schooling-registrations/import-siecle?format=${acceptedFormat}`;
+    return this.ajax(url, 'POST', { data: fileToUpload });
   }
 }
--- orga/tests/unit/adapters/students-import_test.js
+++ orga/tests/unit/adapters/students-import_test.js
@@ -1,6 +1,7 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import sinon from 'sinon';
+import StudentImportsAdapter from '../../../app/adapters/students-import';
 
 module('Unit | Adapters | Students import', function (hooks) {
   setupTest(hooks);
@@ -36,9 +37,27 @@ module('Unit | Adapters | Students import', function (hooks) {
   });
 
   module('#importStudentsSiecle', function () {
+    const zipFile = new File([''], 'archive.zip', { type: 'application/zip' });
+    const csvFile = new File([''], 'file.csv', { type: 'text/csv' });
+    const acceptedFormat = 'csv';
+
+    test('should throw an error if format is not handled', async function (assert) {
+      assert.expect(1);
+
+      try {
+        // when
+        await adapter.importStudentsSiecle(1, [zipFile], acceptedFormat);
+        assert.fail('It should not be possible to upload a zip');
+      } catch (error) {
+        // then
+        sinon.assert.notCalled(adapter.ajax);
+        assert.equals(error.message, StudentImportsAdapter.FORMAT_NOT_SUPPORTED_ERROR);
+      }
+    });
+
     test('should build importStudentsSiecle url from organizationId and format', async function (assert) {
       // when
-      await adapter.importStudentsSiecle(1, [Symbol()], 'csv');
+      await adapter.importStudentsSiecle(1, [csvFile], acceptedFormat);
 
       // then
       assert.ok(
