7305632cef9d37fd37229adec837c7a6bd45053c
François de Metz <francois.de-metz@pix.fr>
2021-12-15 10:20:23 +0100

    Add a button to download selected tubes as json
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>
    Co-authored-by: Clarisse Damon <clarisse.damon@pix.fr>

--- orga/tests/integration/components/tube/list_test.js
+++ orga/tests/integration/components/tube/list_test.js
@@ -2,13 +2,14 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { clickByName } from '@1024pix/ember-testing-library';
 
 module('Integration | Component | tube:list', function (hooks) {
   setupRenderingTest(hooks);
+  let tubes;
 
-  test('it should display a list of tubes', async function (assert) {
-    // given
-    const tubes = [
+  hooks.beforeEach(() => {
+    tubes = [
       {
         id: 'tubeId1',
         practicalTitle: 'Titre 1',
@@ -20,6 +21,10 @@ module('Integration | Component | tube:list', function (hooks) {
         practicalDescription: 'Description 2',
       },
     ];
+  });
+
+  test('it should display a list of tubes', async function (assert) {
+    // given
     this.set('tubes', tubes);
 
     // when
@@ -28,5 +33,28 @@ module('Integration | Component | tube:list', function (hooks) {
     // then
     assert.dom('.row-tube').exists({ count: 2 });
     assert.dom(this.element.querySelector('.row-tube')).hasText('Titre 1 : Description 1');
+  });
+
+  test('Disable the download button if not tube is selected', async function (assert) {
+    // given
+    this.set('tubes', tubes);
+
+    // when
+    await render(hbs`<Tube::list @tubes={{this.tubes}}/>`);
+
+    // then
+    assert.dom('.download-file__button').hasClass('pix-button--disabled');
+  });
+
+  test('Enabled the download button if a tube is selected', async function (assert) {
+    // given
+    this.set('tubes', tubes);
+
+    // when
+    await render(hbs`<Tube::list @tubes={{this.tubes}}/>`);
+    await clickByName('Titre 1 : Description 1');
+
+    // then
+    assert.dom('.download-file__button').doesNotHaveClass('pix-button--disabled');
   });
 });
