c13d1e523210dff07e565b954662285aa04f84bf
Clément Bourgoin <clement.bourgoin@pix.fr>
2021-12-15 13:58:10 +0100

    feat(certif): add a hasCompleted prop to candidate model

--- certif/tests/unit/models/certification-candidate-for-supervising_test.js
+++ certif/tests/unit/models/certification-candidate-for-supervising_test.js
@@ -50,6 +50,44 @@ module('Unit | Model | certification-candidate-for-supervising', function(hooks)
     });
   });
 
+  module('#get hasCompleted', () => {
+    test('returns false if assessmentStatus is started', function(assert) {
+      // given
+      const store = this.owner.lookup('service:store');
+      const data = { assessmentStatus: 'started' };
+
+      // when
+      const model = store.createRecord('certification-candidate-for-supervising', data);
+
+      // then
+      assert.false(model.hasCompleted);
+    });
+
+    test('returns true if assessmentStatus is completed', function(assert) {
+      // given
+      const store = this.owner.lookup('service:store');
+      const data = { assessmentStatus: 'completed' };
+
+      // when
+      const model = store.createRecord('certification-candidate-for-supervising', data);
+
+      // then
+      assert.true(model.hasCompleted);
+    });
+
+    test('returns true if assessmentStatus is endedBySupervisor', function(assert) {
+      // given
+      const store = this.owner.lookup('service:store');
+      const data = { assessmentStatus: 'endedBySupervisor' };
+
+      // when
+      const model = store.createRecord('certification-candidate-for-supervising', data);
+
+      // then
+      assert.true(model.hasCompleted);
+    });
+  });
+
   function _pickModelData(certificationCandidate) {
     return pick(certificationCandidate, [
       'firstName',
