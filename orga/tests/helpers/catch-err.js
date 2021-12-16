71e0b88195dcaa7e1bbe03447d412b242337d77f
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2021-12-16 16:04:20 +0100

    refactor(orga): simplify test using catchErr helper

---
+++ orga/tests/helpers/catch-err.js
@@ -0,0 +1,10 @@
+export default function catchErr(promiseFn, ctx) {
+  return async (...args) => {
+    try {
+      await promiseFn.call(ctx, ...args);
+      return 'should have thrown an error';
+    } catch (err) {
+      return err;
+    }
+  };
+}
