97333e6b1da8df31b705730c6cdb47d2b2d9baf5
Anne-G <ang.schall@gmail.com>
2021-12-17 16:31:47 +0100

    [orga] override acceptance-test blueprint

---
+++ orga/blueprints/acceptance-test/files/tests/acceptance/__name___test.js
@@ -0,0 +1,13 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | <%= dasherizedModuleName %>', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /<%= dasherizedModuleName %>', async function (assert) {
+    await visit('/<%= dasherizedModuleName %>');
+
+    assert.equal(currentURL(), '/<%= dasherizedModuleName %>');
+  });
+});
