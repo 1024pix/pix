97075f15e0af204bf765982376734f133ba1f680
Anne-G <ang.schall@gmail.com>
2021-12-17 19:30:02 +0100

    [api] add createTag controller

--- api/tests/unit/application/tags/tag-controller_test.js
+++ api/tests/unit/application/tags/tag-controller_test.js
@@ -1,10 +1,30 @@
-const { expect, sinon, domainBuilder } = require('../../../test-helper');
+const { expect, sinon, domainBuilder, hFake } = require('../../../test-helper');
 
 const tagController = require('../../../../lib/application/tags/tag-controller');
 const usecases = require('../../../../lib/domain/usecases');
 const tagSerializer = require('../../../../lib/infrastructure/serializers/jsonapi/tag-serializer');
 
 describe('Unit | Application | Tags | tag-controller', function () {
+  describe('#create', function () {
+    it('should return the created tag', async function () {
+      // given
+      const createdTag = domainBuilder.buildTag({ id: 1, name: 'TAG1' });
+      const serializedTag = Symbol('a serialized tag');
+
+      sinon.stub(usecases, 'createTag').resolves(createdTag);
+      sinon.stub(tagSerializer, 'serialize').withArgs(createdTag).returns(serializedTag);
+
+      const request = { payload: { data: { attributes: { name: 'tag1' } } } };
+
+      // when
+      const result = await tagController.create(request, hFake);
+
+      // then
+      expect(result.statusCode).to.be.equal(201);
+      expect(result.source).to.be.equal(serializedTag);
+    });
+  });
+
   describe('#findAllTags', function () {
     it('should call findAllTags usecase and tag serializer', async function () {
       // given
