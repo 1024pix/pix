b229221305fbedc9ba8cacad17a327a0cfccc7e8
Andreia Pena <andreia.pena@pix.fr>
2021-12-24 11:18:23 +0100

    [API] Check if email is available

--- api/lib/domain/usecases/add-pix-authentication-method-by-email.js
+++ api/lib/domain/usecases/add-pix-authentication-method-by-email.js
@@ -1,6 +1,13 @@
 const { AuthenticationMethodAlreadyExistsError } = require('../errors');
 
-module.exports = async function addPixAuthenticationMethodByEmail({ userId, authenticationMethodRepository }) {
+module.exports = async function addPixAuthenticationMethodByEmail({
+  userId,
+  email,
+  userRepository,
+  authenticationMethodRepository,
+}) {
+  await userRepository.checkIfEmailIsAvailable(email);
+
   const alreadyHasPixAuthenticationMethod = await authenticationMethodRepository.hasIdentityProviderPIX({ userId });
 
   if (alreadyHasPixAuthenticationMethod) {
