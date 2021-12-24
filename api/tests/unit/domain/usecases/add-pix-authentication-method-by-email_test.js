3713d98b86d93d7e103cb684a0cd2cb684146d73
Andreia Pena <andreia.pena@pix.fr>
2021-12-24 17:04:50 +0100

    [API] Check if user had to validate cgu and update user attributes

--- api/tests/unit/domain/usecases/add-pix-authentication-method-by-email_test.js
+++ api/tests/unit/domain/usecases/add-pix-authentication-method-by-email_test.js
@@ -10,6 +10,8 @@ describe('Unit | UseCase | add-pix-authentication-method-by-email', function () 
   beforeEach(function () {
     userRepository = {
       checkIfEmailIsAvailable: sinon.stub(),
+      get: sinon.stub(),
+      updateUserDetailsForAdministration: sinon.stub(),
     };
     authenticationMethodRepository = {
       hasIdentityProviderPIX: sinon.stub(),
@@ -25,8 +27,15 @@ describe('Unit | UseCase | add-pix-authentication-method-by-email', function () 
 
   it('should check if email is available', async function () {
     // given
-    const email = 'user@example.net';
-    const user = domainBuilder.buildUser({ email });
+    const email = 'newEmail@example.net';
+    const generatedPassword = 'Pix12345';
+    const hashedPassword = 'ABCDEF123';
+    const user = domainBuilder.buildUser({});
+    domainBuilder.buildAuthenticationMethod.withGarAsIdentityProvider({ userId: user.id });
+
+    passwordGenerator.generateComplexPassword.returns(generatedPassword);
+    encryptionService.hashPassword.resolves(hashedPassword);
+    userRepository.get.withArgs(user.id).resolves(user);
 
     // when
     await addPixAuthenticationMethodByEmail({
@@ -50,8 +59,10 @@ describe('Unit | UseCase | add-pix-authentication-method-by-email', function () 
       const hashedPassword = 'ABCDEF123';
       const user = domainBuilder.buildUser({});
       domainBuilder.buildAuthenticationMethod.withGarAsIdentityProvider({ userId: user.id });
+
       passwordGenerator.generateComplexPassword.returns(generatedPassword);
       encryptionService.hashPassword.withArgs(generatedPassword).resolves(hashedPassword);
+      userRepository.get.withArgs(user.id).resolves(user);
 
       // when
       await addPixAuthenticationMethodByEmail({
@@ -74,6 +85,63 @@ describe('Unit | UseCase | add-pix-authentication-method-by-email', function () 
       });
       expect(authenticationMethodRepository.create).to.have.been.calledWith({
         authenticationMethod: authenticationMethodFromPix,
+      });
+    });
+
+    context('when user had to validate cgu', function () {
+      it('should update mustValidateTermsOfService', async function () {
+        // given
+        const email = 'newEmail@example.net';
+        const generatedPassword = 'Pix12345';
+        const hashedPassword = 'ABCDEF123';
+        const user = domainBuilder.buildUser({ cgu: false });
+        domainBuilder.buildAuthenticationMethod.withGarAsIdentityProvider({ userId: user.id });
+
+        passwordGenerator.generateComplexPassword.returns(generatedPassword);
+        encryptionService.hashPassword.resolves(hashedPassword);
+        userRepository.get.withArgs(user.id).resolves(user);
+
+        // when
+        await addPixAuthenticationMethodByEmail({
+          userId: user.id,
+          email,
+          passwordGenerator,
+          encryptionService,
+          authenticationMethodRepository,
+          userRepository,
+        });
+
+        // then
+        const expectedAttributes = { email, mustValidateTermsOfService: true };
+        expect(userRepository.updateUserDetailsForAdministration).to.have.been.calledWith(user.id, expectedAttributes);
+      });
+    });
+
+    context('when user has validate cgu', function () {
+      it('should not update mustValidateTermsOfService', async function () {
+        // given
+        const email = 'newEmail@example.net';
+        const generatedPassword = 'Pix12345';
+        const hashedPassword = 'ABCDEF123';
+        const user = domainBuilder.buildUser({ cgu: true });
+        domainBuilder.buildAuthenticationMethod.withGarAsIdentityProvider({ userId: user.id });
+
+        passwordGenerator.generateComplexPassword.returns(generatedPassword);
+        encryptionService.hashPassword.resolves(hashedPassword);
+        userRepository.get.withArgs(user.id).resolves(user);
+
+        // when
+        await addPixAuthenticationMethodByEmail({
+          userId: user.id,
+          email,
+          passwordGenerator,
+          encryptionService,
+          authenticationMethodRepository,
+          userRepository,
+        });
+
+        // then
+        expect(userRepository.updateUserDetailsForAdministration).to.have.been.calledWith(user.id, { email });
       });
     });
   });
