cc149c33869a4b4ccbf53dcb91152259a327f563
François de Metz <francois.de-metz@pix.fr>
2021-12-29 15:24:35 +0100

    Add thematic datasource, repository and model
    Co-authored-by: Clarisse Damon <clarisse.damon@pix.fr>

---
+++ api/lib/infrastructure/repositories/thematic-repository.js
@@ -0,0 +1,18 @@
+const Thematic = require('../../domain/models/Thematic');
+const thematicDatasource = require('../datasources/learning-content/thematic-datasource');
+
+function _toDomain(thematicData) {
+  return new Thematic({
+    id: thematicData.id,
+    name: thematicData.name,
+    index: thematicData.index,
+    tubeIds: thematicData.tubeIds,
+  });
+}
+
+module.exports = {
+  async list() {
+    const thematicData = await thematicDatasource.list();
+    return thematicData.map(_toDomain);
+  },
+};
