d804a9def77ff06c6447b3edb3c9d313c60cfbd9
Anne-G <ang.schall@gmail.com>
2021-12-30 14:58:46 +0100

    [admin/certif/orga] change visit from testing-library

--- admin/blueprints/component-test/files/tests/integration/components/__name___test.js
+++ admin/blueprints/component-test/files/tests/integration/components/__name___test.js
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { render as renderScreen } from '@1024pix/ember-testing-library';
 
 module('Integration | Component |  <%= dasherizedModuleName %>', function(hooks) {
   setupRenderingTest(hooks);
@@ -10,7 +10,7 @@ module('Integration | Component |  <%= dasherizedModuleName %>', function(hooks)
     // given
 
     //  when
-    await render(hbs`<<%= classifiedModuleName %> />`);
+    await renderScreen(hbs`<<%= classifiedModuleName %> />`);
 
     // then
     assert.ok(true);
