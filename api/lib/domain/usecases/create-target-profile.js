cfbf75e10d495c1d9b5424c6f85bd828d1d00d30
ajoanny <ajoanny@octo.com>
2022-01-03 17:06:58 +0100

    [API] use model in use case

--- api/lib/domain/usecases/create-target-profile.js
+++ api/lib/domain/usecases/create-target-profile.js
@@ -1,21 +1,10 @@
-const { TargetProfileInvalidError } = require('../errors');
-const _ = require('lodash');
-const DEFAULT_IMAGE_URL = 'https://images.pix.fr/profil-cible/Illu_GEN.svg';
-
+const TargetProfileForCreation = require('../models/TargetProfileForCreation');
 module.exports = async function createTargetProfile({
   targetProfileData,
   targetProfileRepository,
   targetProfileWithLearningContentRepository,
 }) {
-  if (_.isEmpty(targetProfileData.skillsId)) {
-    throw new TargetProfileInvalidError('Vous ne pouvez pas créer un profil cible sans acquis ciblés.');
-  }
-
-  if (_.isEmpty(targetProfileData.imageUrl)) {
-    targetProfileData.imageUrl = DEFAULT_IMAGE_URL;
-  }
-
-  const targetProfileId = await targetProfileRepository.create(targetProfileData);
-
+  const targetProfile = new TargetProfileForCreation(targetProfileData);
+  const targetProfileId = await targetProfileRepository.create(targetProfile);
   return targetProfileWithLearningContentRepository.get({ id: targetProfileId });
 };
