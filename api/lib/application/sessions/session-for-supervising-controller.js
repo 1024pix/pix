c9e727f8135d6a4efefcb99642e4524cb4987b96
Antoine Ceol <antoine.ceol@gmail.com>
2022-01-05 22:26:24 +0100

    :sparkles: feat(api) Feature toggle remove end test screen depends on certification center for session-for-supervising-controller

--- api/lib/application/sessions/session-for-supervising-controller.js
+++ api/lib/application/sessions/session-for-supervising-controller.js
@@ -1,26 +1,32 @@
 const usecases = require('../../domain/usecases');
 const sessionForSupervisingSerializer = require('../../infrastructure/serializers/jsonapi/session-for-supervising-serializer');
-const { featureToggles } = require('../../config');
 const { NotFoundError } = require('../http-errors');
+const endTestScreenRemovalService = require('../../domain/services/end-test-screen-removal-service');
 
 module.exports = {
   async get(request) {
-    if (!featureToggles.isEndTestScreenRemovalEnabled) {
+    const sessionId = request.params.id;
+    const isEndTestScreenRemovalEnabled = await endTestScreenRemovalService.isEndTestScreenRemovalEnabledBySessionId(
+      sessionId
+    );
+    if (!isEndTestScreenRemovalEnabled) {
       throw new NotFoundError();
     }
 
-    const sessionId = request.params.id;
     const session = await usecases.getSessionForSupervising({ sessionId });
     return sessionForSupervisingSerializer.serialize(session);
   },
 
   async supervise(request, h) {
-    if (!featureToggles.isEndTestScreenRemovalEnabled) {
+    const { 'supervisor-password': supervisorPassword, 'session-id': sessionId } = request.payload.data.attributes;
+    const isEndTestScreenRemovalEnabled = await endTestScreenRemovalService.isEndTestScreenRemovalEnabledBySessionId(
+      // eslint-disable-next-line no-restricted-syntax
+      parseInt(sessionId, 10)
+    );
+    if (!isEndTestScreenRemovalEnabled) {
       throw new NotFoundError();
     }
-
     const { userId } = request.auth.credentials;
-    const { 'supervisor-password': supervisorPassword, 'session-id': sessionId } = request.payload.data.attributes;
     await usecases.superviseSession({ sessionId, userId, supervisorPassword });
     return h.response().code(204);
   },
