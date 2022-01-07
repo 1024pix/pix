8a2a2dc5e4ef1298ea1da99a561ea96e57364682
Xavier Carron <xavier.carron@zenika.com>
2022-01-07 15:48:33 +0100

    [A11Y] Add role columnheader on Pix Orga Table & use Table::Header components "everywhere" (PIX-4115)

--- orga/app/components/team/members-list.js
+++ orga/app/components/team/members-list.js
@@ -3,4 +3,8 @@ import Component from '@glimmer/component';
 
 export default class MembersList extends Component {
   @service currentUser;
+
+  get displayManagingColumn() {
+    return this.currentUser.isAdminInOrganization;
+  }
 }
