0ec46f892bdc36b9e77375d9a9d3dedda11d549f
P-Jeremy <jemyplu@gmail.com>
2022-01-10 17:40:59 +0100

    :recycle:refactor(api): add white list end test screen removal prehandler

---
+++ api/tests/unit/application/preHandlers/end-test-screen-removal-enabled_test.js
@@ -0,0 +1,81 @@
+const { expect, sinon, hFake } = require('../../../test-helper');
+const endTestScreenRemovalEnabled = require('../../../../lib/application/preHandlers/end-test-screen-removal-enabled');
+const endTestScreenRemovalService = require('../../../../lib/domain/services/end-test-screen-removal-service');
+
+describe('Unit | Pre-handler | end test screen removal', function () {
+  describe('#verifyBySessionId', function () {
+    const request = {
+      params: {
+        id: 8,
+      },
+    };
+
+    beforeEach(function () {
+      sinon.stub(endTestScreenRemovalService, 'isEndTestScreenRemovalEnabledBySessionId');
+    });
+
+    describe('When session certification center is in the whitelist', function () {
+      it('should return true', async function () {
+        // given
+        endTestScreenRemovalService.isEndTestScreenRemovalEnabledBySessionId.withArgs(8).resolves(true);
+
+        // when
+        const response = await endTestScreenRemovalEnabled.verifyBySessionId(request, hFake);
+
+        // then
+        expect(response).to.be.true;
+      });
+    });
+
+    describe('When session certification center is not in the whitelist', function () {
+      it('should return a 404 status code', async function () {
+        // given
+        endTestScreenRemovalService.isEndTestScreenRemovalEnabledBySessionId.withArgs(8).resolves(false);
+
+        // when
+        const response = await endTestScreenRemovalEnabled.verifyBySessionId(request, hFake);
+
+        // then
+        expect(response.statusCode).to.equal(404);
+      });
+    });
+  });
+
+  describe('#verifyByCertificationCandidateId', function () {
+    const request = {
+      params: {
+        id: 5,
+      },
+    };
+
+    beforeEach(function () {
+      sinon.stub(endTestScreenRemovalService, 'isEndTestScreenRemovalEnabledByCandidateId');
+    });
+
+    describe("When candidate's certification center is in the whitelist", function () {
+      it('should return true', async function () {
+        // given
+        endTestScreenRemovalService.isEndTestScreenRemovalEnabledByCandidateId.withArgs(5).resolves(true);
+
+        // when
+        const response = await endTestScreenRemovalEnabled.verifyByCertificationCandidateId(request, hFake);
+
+        // then
+        expect(response).to.be.true;
+      });
+    });
+
+    describe("When candidate's certification center is not in the whitelist", function () {
+      it('should return a 404 status code', async function () {
+        // given
+        endTestScreenRemovalService.isEndTestScreenRemovalEnabledByCandidateId.withArgs(5).resolves(false);
+
+        // when
+        const response = await endTestScreenRemovalEnabled.verifyByCertificationCandidateId(request, hFake);
+
+        // then
+        expect(response.statusCode).to.equal(404);
+      });
+    });
+  });
+});
