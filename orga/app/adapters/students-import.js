4a9cfefef5a981add7d0cb69014e3de77c8c1946
yahya <youness.yahya@pix.fr>
2022-01-11 10:40:21 +0100

    revert commits PR #3780 #3918

--- orga/app/adapters/students-import.js
+++ orga/app/adapters/students-import.js
@@ -1,5 +1,4 @@
 import ApplicationAdapter from './application';
-import ENV from 'pix-orga/config/environment';
 
 export default class StudentImportsAdapter extends ApplicationAdapter {
   addStudentsCsv(organizationId, files) {
@@ -16,20 +15,10 @@ export default class StudentImportsAdapter extends ApplicationAdapter {
     return this.ajax(url, 'POST', { data: files[0] });
   }
 
-  importStudentsSiecle(organizationId, files, acceptedFormatName, acceptedFormatMimeTypes) {
+  importStudentsSiecle(organizationId, files, format) {
     if (!files || files.length === 0) return;
 
-    const fileToUpload = files[0];
-    const fileToUploadMimeType = fileToUpload.type;
-    const isFormatSupported =
-      acceptedFormatMimeTypes.find((acceptedFormatMimeType) =>
-        fileToUploadMimeType?.includes(acceptedFormatMimeType)
-      ) !== undefined;
-    if (!isFormatSupported) {
-      throw new Error(ENV.APP.ERRORS.FILE_UPLOAD.FORMAT_NOT_SUPPORTED_ERROR);
-    }
-
-    const url = `${this.host}/${this.namespace}/organizations/${organizationId}/schooling-registrations/import-siecle?format=${acceptedFormatName}`;
-    return this.ajax(url, 'POST', { data: fileToUpload });
+    const url = `${this.host}/${this.namespace}/organizations/${organizationId}/schooling-registrations/import-siecle?format=${format}`;
+    return this.ajax(url, 'POST', { data: files[0] });
   }
 }
