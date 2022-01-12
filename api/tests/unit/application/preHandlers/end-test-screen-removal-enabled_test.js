52f2a6bb3260177b77e4ca2c8546cb44b8127a96
P-Jeremy <jemyplu@gmail.com>
2022-01-12 12:21:56 +0100

    :recycle:refactor(api): throw an error when certification center is not in the white list

--- api/tests/unit/application/preHandlers/end-test-screen-removal-enabled_test.js
+++ api/tests/unit/application/preHandlers/end-test-screen-removal-enabled_test.js
@@ -1,6 +1,7 @@
-const { expect, sinon, hFake } = require('../../../test-helper');
+const { catchErr, expect, sinon, hFake } = require('../../../test-helper');
 const endTestScreenRemovalEnabled = require('../../../../lib/application/preHandlers/end-test-screen-removal-enabled');
 const endTestScreenRemovalService = require('../../../../lib/domain/services/end-test-screen-removal-service');
+const { SupervisorAccessNotAuthorizedError } = require('../../../../lib/domain/errors');
 
 describe('Unit | Pre-handler | end test screen removal', function () {
   describe('#verifyBySessionId', function () {
@@ -28,7 +29,7 @@ describe('Unit | Pre-handler | end test screen removal', function () {
       });
 
       describe('When session certification center is not in the whitelist', function () {
-        it('should return a 404 status code', async function () {
+        it('should throw', async function () {
           // given
           const request = {
             payload: {
@@ -38,10 +39,10 @@ describe('Unit | Pre-handler | end test screen removal', function () {
           endTestScreenRemovalService.isEndTestScreenRemovalEnabledBySessionId.withArgs(8).resolves(false);
 
           // when
-          const response = await endTestScreenRemovalEnabled.verifyBySessionId(request, hFake);
+          const error = await catchErr(endTestScreenRemovalEnabled.verifyBySessionId)(request, hFake);
 
           // then
-          expect(response.statusCode).to.equal(404);
+          expect(error).to.be.instanceOf(SupervisorAccessNotAuthorizedError);
         });
       });
     });
@@ -65,7 +66,7 @@ describe('Unit | Pre-handler | end test screen removal', function () {
       });
 
       describe('When session certification center is not in the whitelist', function () {
-        it('should return a 404 status code', async function () {
+        it('should throw', async function () {
           // given
           const request = {
             params: {
@@ -75,10 +76,10 @@ describe('Unit | Pre-handler | end test screen removal', function () {
           endTestScreenRemovalService.isEndTestScreenRemovalEnabledBySessionId.withArgs(8).resolves(false);
 
           // when
-          const response = await endTestScreenRemovalEnabled.verifyBySessionId(request, hFake);
+          const error = await catchErr(endTestScreenRemovalEnabled.verifyBySessionId)(request, hFake);
 
           // then
-          expect(response.statusCode).to.equal(404);
+          expect(error).to.be.instanceOf(SupervisorAccessNotAuthorizedError);
         });
       });
     });
