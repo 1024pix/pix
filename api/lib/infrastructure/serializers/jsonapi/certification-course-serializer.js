c951cc46984819dab833a8d0d9c23e2ee46f5a52
Antoine Ceol <antoine.ceol@gmail.com>
2022-01-14 09:48:14 +0100

    :sparkles: feat(api,app) display end test screen depending on certification center in whitelist

--- api/lib/infrastructure/serializers/jsonapi/certification-course-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/certification-course-serializer.js
@@ -1,16 +1,24 @@
 const { Serializer } = require('jsonapi-serializer');
 
 module.exports = {
-  serialize(certificationCourse) {
+  serialize(certificationCourse, isEndTestScreenRemovalEnabled) {
     return new Serializer('certification-course', {
       transform(currentCertificationCourse) {
         const certificationCourseDTO = currentCertificationCourse.toDTO();
         certificationCourseDTO.nbChallenges = certificationCourseDTO?.challenges?.length ?? 0;
         certificationCourseDTO.examinerComment = certificationCourseDTO.certificationIssueReports?.[0]?.description;
-
+        certificationCourseDTO.isEndTestScreenRemovalEnabled = isEndTestScreenRemovalEnabled;
         return certificationCourseDTO;
       },
-      attributes: ['assessment', 'nbChallenges', 'examinerComment', 'hasSeenEndTestScreen', 'firstName', 'lastName'],
+      attributes: [
+        'assessment',
+        'nbChallenges',
+        'examinerComment',
+        'hasSeenEndTestScreen',
+        'firstName',
+        'lastName',
+        'isEndTestScreenRemovalEnabled',
+      ],
       assessment: {
         ref: 'id',
         ignoreRelationshipData: true,
