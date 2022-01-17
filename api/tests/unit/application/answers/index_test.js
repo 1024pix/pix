5de6adf24318746feef74eea721ff75b3978ae2e
Pierre TOP (TOPI) <ptop@octo.com>
2022-01-17 10:00:18 +0100

    [API] Prevent overflow on user answer entry

--- api/tests/unit/application/answers/index_test.js
+++ api/tests/unit/application/answers/index_test.js
@@ -31,6 +31,39 @@ describe('Unit | Application | Router | answer-router', function () {
       // then
       expect(response.statusCode).to.equal(201);
     });
+
+    it('should return BAD_REQUEST with message if answer length is too long (security issue)', async function () {
+      // given
+      sinon.stub(answerController, 'save').callsFake((request, h) => h.response().created());
+      const httpTestServer = new HttpTestServer();
+      await httpTestServer.register(moduleUnderTest);
+      const USER_ANSWERS_MAX_LENGTH = 500;
+      const value = 'X'.repeat(USER_ANSWERS_MAX_LENGTH + 1);
+
+      const payload = {
+        data: {
+          attributes: {
+            value,
+            result: null,
+            'result-details': null,
+            timeout: null,
+          },
+          relationships: {},
+          assessment: {},
+          challenge: {},
+          type: 'answers',
+        },
+      };
+
+      // when
+      const response = await httpTestServer.request('POST', '/api/answers', payload);
+
+      // then
+      expect(response.statusCode).to.equal(400);
+      expect(response.result.errors[0].detail).to.equal(
+        '"data.attributes.value" length must be less than or equal to 500 characters long'
+      );
+    });
   });
 
   describe('GET /api/answers/{id}', function () {
