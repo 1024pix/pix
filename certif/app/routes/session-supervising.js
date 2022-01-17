b53faacb02b1660b55688d4d64993a22a5c9749d
Mathieu Gilet <mathieu.gilet@zenika.com>
2022-01-17 16:48:35 +0100

    :sparkles: feat(certif): refresh student list periodically

--- certif/app/routes/session-supervising.js
+++ certif/app/routes/session-supervising.js
@@ -1,7 +1,20 @@
 import Route from '@ember/routing/route';
+import ENV from 'pix-certif/config/environment';
 
 export default class SessionSupervisingRoute extends Route {
   model(params) {
-    return this.store.queryRecord('session-for-supervising', { sessionId: params.session_id });
+    const sessionForSupervising = this.store.queryRecord('session-for-supervising', { sessionId: params.session_id });
+
+    this.poller = setInterval(() => {
+      this.store.queryRecord('session-for-supervising', { sessionId: params.session_id });
+    }, ENV.APP.sessionSupervisingPollingRate);
+
+    return sessionForSupervising;
+  }
+
+  deactivate() {
+    if (this.poller) {
+      clearInterval(this.poller);
+    }
   }
 }
