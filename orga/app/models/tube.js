bc65054d9f5a9d63d65578bd8092e134d4eb7ba4
jeremie <jeremie.jade@pix.fr>
2022-01-21 15:53:57 +0100

    Add responsive status on tube selection
    Co-authored-by: Clarisse Damon <clarisse.damon@pix.fr>
    Co-authored-by: Thomas Evano <thomas.evano@pix.fr>

--- orga/app/models/tube.js
+++ orga/app/models/tube.js
@@ -3,4 +3,6 @@ import Model, { attr } from '@ember-data/model';
 export default class Tube extends Model {
   @attr('string') practicalTitle;
   @attr('string') practicalDescription;
+  @attr('boolean') mobile;
+  @attr('boolean') tablet;
 }
