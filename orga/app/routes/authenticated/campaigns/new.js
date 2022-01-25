b6630dccb10cef545a9e76d5ebb4027b34c8538d
Anne-G <ang.schall@gmail.com>
2022-01-25 17:17:16 +0100

    [orga] add sorted members to new campaign route

--- orga/app/routes/authenticated/campaigns/new.js
+++ orga/app/routes/authenticated/campaigns/new.js
@@ -6,11 +6,25 @@ export default class NewRoute extends Route {
   @service store;
   @service currentUser;
 
-  model() {
+  async model(params) {
     const organization = this.currentUser.organization;
+
+    const memberships = await this.store.query('membership', {
+      filter: {
+        organizationId: organization.id,
+      },
+      page: {
+        number: params.pageNumber,
+        size: params.pageSize,
+      },
+    });
+    const members = memberships.map((membership) => membership.user);
+    const membersSortedByFullName = members.sortBy('firstName', 'lastName');
+
     return RSVP.hash({
       campaign: this.store.createRecord('campaign', { organizationId: organization.id }),
       targetProfiles: organization.targetProfiles,
+      membersSortedByFullName,
     });
   }
 }
