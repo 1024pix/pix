3c3fbd9e5a1a8cda54cca1879a39f739e4b27f1c
Anne-G <ang.schall@gmail.com>
2022-01-25 17:57:09 +0100

    [orga] add sorted members to update campaign route

--- orga/tests/unit/routes/authenticated/campaigns/update_test.js
+++ orga/tests/unit/routes/authenticated/campaigns/update_test.js
@@ -1,11 +1,48 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import EmberObject from '@ember/object';
+import sinon from 'sinon';
 
 module('Unit | Route | authenticated/campaigns/update', function (hooks) {
   setupTest(hooks);
 
-  test('it exists', function (assert) {
+  test('should return members list sorted by full name', async function (assert) {
+    // given
     const route = this.owner.lookup('route:authenticated/campaigns/update');
-    assert.ok(route);
+
+    route.currentUser = { organization: { id: 123 } };
+
+    const membership1 = EmberObject.create({
+      user: EmberObject.create({
+        firstName: 'Alice',
+        lastName: 'Delamer',
+      }),
+    });
+    const membership2 = EmberObject.create({
+      user: EmberObject.create({
+        firstName: 'Alice',
+        lastName: 'Delamare',
+      }),
+    });
+    const campaign = EmberObject.create({ ownerFirstName: 'Marc', ownerLastName: 'Dupont' });
+    const queryStub = sinon.stub();
+    const findRecordStub = sinon.stub();
+    const storeStub = {
+      query: queryStub.resolves([membership1, membership2]),
+      findRecord: findRecordStub.resolves(campaign),
+    };
+    route.store = storeStub;
+
+    const params = {
+      pageNumber: 1,
+      pageSize: 2,
+    };
+
+    // when
+    const result = await route.model(params);
+
+    //then
+    assert.strictEqual(result.membersSortedByFullName[0].lastName, 'Delamare');
+    assert.strictEqual(result.membersSortedByFullName[1].lastName, 'Delamer');
   });
 });
