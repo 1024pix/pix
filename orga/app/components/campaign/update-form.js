a7f83d2b0af87777084a66e26533b78f1e6592d2
Andreia Pena <andreia.pena@pix.fr>
2022-01-26 16:17:44 +0100

    [ORGA] Add select input in update campaign form

--- orga/app/components/campaign/update-form.js
+++ orga/app/components/campaign/update-form.js
@@ -1,7 +1,26 @@
-import { action } from '@ember/object';
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class UpdateForm extends Component {
+  get campaignOwnerOptions() {
+    if (!this.args.membersSortedByFullName) return [];
+
+    return this.args.membersSortedByFullName.map((member) => {
+      return { value: member.get('id'), label: member.get('fullName') };
+    });
+  }
+
+  @action
+  selectOwner(event) {
+    const newOwnerFullName = event.target.value;
+    const selectedMember = this.args.membersSortedByFullName.find(
+      (member) => newOwnerFullName === member.get('fullName')
+    );
+    if (selectedMember) {
+      this.args.campaign.ownerId = selectedMember.get('id');
+    }
+  }
+
   @action
   onChangeCampaignName(event) {
     this.args.campaign.name = event.target.value?.trim();
