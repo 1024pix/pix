538aca2eb73a3c950b401f39bbf72f32e5ebbffe
P-Jeremy <jemyplu@gmail.com>
2022-01-27 11:18:42 +0100

    :sparkles: feat(certif): add current user email in supervisor login form

--- certif/app/controllers/login-session-supervisor.js
+++ certif/app/controllers/login-session-supervisor.js
@@ -5,6 +5,11 @@ import { inject as service } from '@ember/service';
 export default class LoginSessionSupervisorController extends Controller {
   @service store;
   @service router;
+  @service currentUser;
+
+  get currentUserEmail() {
+    return this.currentUser.certificationPointOfContact.email;
+  }
 
   @action
   async authenticateSupervisor({ sessionId, supervisorPassword }) {
