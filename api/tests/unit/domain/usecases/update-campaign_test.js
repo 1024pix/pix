eb102cfa066bbee4d6ca03a948d18eb08e69c666
Andreia Pena <andreia.pena@pix.fr>
2022-01-27 17:52:37 +0100

    [API][SR] Delete some tests and promise in tests

--- api/tests/unit/domain/usecases/update-campaign_test.js
+++ api/tests/unit/domain/usecases/update-campaign_test.js
@@ -1,7 +1,6 @@
 const { expect, sinon, catchErr, domainBuilder } = require('../../../test-helper');
 const updateCampaign = require('../../../../lib/domain/usecases/update-campaign');
 const { UserNotAuthorizedToUpdateResourceError } = require('../../../../lib/domain/errors');
-const { EntityValidationError } = require('../../../../lib/domain/errors');
 
 describe('Unit | UseCase | update-campaign', function () {
   let originalCampaign;
@@ -13,36 +12,37 @@ describe('Unit | UseCase | update-campaign', function () {
   const creatorId = 1;
 
   beforeEach(function () {
-    originalCampaign = domainBuilder.buildCampaign({
-      id: 1,
-      name: 'Old name',
-      title: 'Old title',
-      type: 'ASSESSMENT',
-      customLandingPageText: 'Old text',
-      targetProfile: { id: 1 },
-      creator: { id: creatorId },
-      organization: { id: organizationId },
-    });
-    userWithMembership = {
-      id: 1,
-      memberships: [{ organization: { id: organizationId } }],
-      hasAccessToOrganization: sinon.stub(),
-    };
     userRepository = { getWithMemberships: sinon.stub() };
     campaignRepository = {
       get: sinon.stub(),
       update: sinon.stub(),
     };
-    // This has to be done separated from the stub declaration, see :
-    // http://nikas.praninskas.com/javascript/2015/07/28/quickie-sinon-withargs-not-working/
-    campaignRepository.get.withArgs(originalCampaign.id).resolves(originalCampaign);
-    campaignRepository.update.callsFake((updatedCampaign) => updatedCampaign);
-    userRepository.getWithMemberships.withArgs(userWithMembership.id).resolves(userWithMembership);
-    userWithMembership.hasAccessToOrganization.withArgs(organizationId).returns(true);
   });
 
   context('when campaign exists', function () {
-    it('should update the campaign title only', function () {
+    beforeEach(function () {
+      originalCampaign = domainBuilder.buildCampaign({
+        id: 1,
+        name: 'Old name',
+        title: 'Old title',
+        type: 'ASSESSMENT',
+        customLandingPageText: 'Old text',
+        targetProfile: { id: 1 },
+        creator: { id: creatorId },
+        organization: { id: organizationId },
+      });
+      userWithMembership = {
+        id: 1,
+        memberships: [{ organization: { id: organizationId } }],
+        hasAccessToOrganization: sinon.stub(),
+      };
+      campaignRepository.get.withArgs(originalCampaign.id).resolves(originalCampaign);
+      campaignRepository.update.callsFake((updatedCampaign) => updatedCampaign);
+      userRepository.getWithMemberships.withArgs(userWithMembership.id).resolves(userWithMembership);
+      userWithMembership.hasAccessToOrganization.withArgs(organizationId).returns(true);
+    });
+
+    it('should update the campaign title only', async function () {
       // given
       const updatedCampaign = domainBuilder.buildCampaign.ofTypeAssessment({
         ...originalCampaign,
@@ -50,7 +50,7 @@ describe('Unit | UseCase | update-campaign', function () {
       });
 
       // when
-      const promise = updateCampaign({
+      const resultCampaign = await updateCampaign({
         userId: userWithMembership.id,
         campaignId: updatedCampaign.id,
         title: updatedCampaign.title,
@@ -59,14 +59,12 @@ describe('Unit | UseCase | update-campaign', function () {
       });
 
       // then
-      return promise.then((resultCampaign) => {
-        expect(campaignRepository.update).to.have.been.calledWithExactly(updatedCampaign);
-        expect(resultCampaign.title).to.equal(updatedCampaign.title);
-        expect(resultCampaign.customLandingPageText).to.equal(originalCampaign.customLandingPageText);
-      });
+      expect(campaignRepository.update).to.have.been.calledWithExactly(updatedCampaign);
+      expect(resultCampaign.title).to.equal(updatedCampaign.title);
+      expect(resultCampaign.customLandingPageText).to.equal(originalCampaign.customLandingPageText);
     });
 
-    it('should update the campaign page text only', function () {
+    it('should update the campaign page text only', async function () {
       // given
       const updatedCampaign = domainBuilder.buildCampaign({
         ...originalCampaign,
@@ -74,7 +72,7 @@ describe('Unit | UseCase | update-campaign', function () {
       });
 
       // when
-      const promise = updateCampaign({
+      const resultCampaign = await updateCampaign({
         userId: userWithMembership.id,
         campaignId: updatedCampaign.id,
         customLandingPageText: updatedCampaign.customLandingPageText,
@@ -83,19 +81,17 @@ describe('Unit | UseCase | update-campaign', function () {
       });
 
       // then
-      return promise.then((resultCampaign) => {
-        expect(campaignRepository.update).to.have.been.calledWithExactly(updatedCampaign);
-        expect(resultCampaign.title).to.equal(originalCampaign.title);
-        expect(resultCampaign.customLandingPageText).to.equal(updatedCampaign.customLandingPageText);
-      });
+      expect(campaignRepository.update).to.have.been.calledWithExactly(updatedCampaign);
+      expect(resultCampaign.title).to.equal(originalCampaign.title);
+      expect(resultCampaign.customLandingPageText).to.equal(updatedCampaign.customLandingPageText);
     });
 
-    it('should update the campaign archive date only', function () {
+    it('should update the campaign archive date only', async function () {
       // given
       const updatedCampaign = domainBuilder.buildCampaign({ ...originalCampaign });
 
       // when
-      const promise = updateCampaign({
+      const resultCampaign = await updateCampaign({
         userId: userWithMembership.id,
         campaignId: updatedCampaign.id,
         userRepository,
@@ -103,14 +99,12 @@ describe('Unit | UseCase | update-campaign', function () {
       });
 
       // then
-      return promise.then((resultCampaign) => {
-        expect(campaignRepository.update).to.have.been.calledWithExactly(updatedCampaign);
-        expect(resultCampaign.title).to.equal(originalCampaign.title);
-        expect(resultCampaign.customLandingPageText).to.equal(originalCampaign.customLandingPageText);
-      });
+      expect(campaignRepository.update).to.have.been.calledWithExactly(updatedCampaign);
+      expect(resultCampaign.title).to.equal(originalCampaign.title);
+      expect(resultCampaign.customLandingPageText).to.equal(originalCampaign.customLandingPageText);
     });
 
-    it('should update the campaign name only', function () {
+    it('should update the campaign name only', async function () {
       // given
       const updatedCampaign = domainBuilder.buildCampaign({
         ...originalCampaign,
@@ -118,7 +112,7 @@ describe('Unit | UseCase | update-campaign', function () {
       });
 
       // when
-      const promise = updateCampaign({
+      const resultCampaign = await updateCampaign({
         userId: userWithMembership.id,
         campaignId: updatedCampaign.id,
         name: updatedCampaign.name,
@@ -128,19 +122,17 @@ describe('Unit | UseCase | update-campaign', function () {
       });
 
       // then
-      return promise.then((resultCampaign) => {
-        expect(campaignRepository.update).to.have.been.calledWithExactly(updatedCampaign);
-        expect(resultCampaign.name).to.equal(updatedCampaign.name);
-        expect(resultCampaign.customLandingPageText).to.equal(originalCampaign.customLandingPageText);
-      });
+      expect(campaignRepository.update).to.have.been.calledWithExactly(updatedCampaign);
+      expect(resultCampaign.name).to.equal(updatedCampaign.name);
+      expect(resultCampaign.customLandingPageText).to.equal(originalCampaign.customLandingPageText);
     });
 
-    it('should not update the campaign name if campaign name is undefined', function () {
+    it('should not update the campaign name if campaign name is undefined', async function () {
       // given
       const updatedCampaign = domainBuilder.buildCampaign({ ...originalCampaign });
 
       // when
-      const promise = updateCampaign({
+      const resultCampaign = await updateCampaign({
         userId: userWithMembership.id,
         campaignId: updatedCampaign.id,
         name: undefined,
@@ -150,101 +142,35 @@ describe('Unit | UseCase | update-campaign', function () {
       });
 
       // then
-      return promise.then((resultCampaign) => {
-        expect(campaignRepository.update).to.have.been.calledWithExactly(updatedCampaign);
-        expect(resultCampaign.name).to.equal(originalCampaign.name);
-        expect(resultCampaign.customLandingPageText).to.equal(originalCampaign.customLandingPageText);
-      });
+      expect(campaignRepository.update).to.have.been.calledWithExactly(updatedCampaign);
+      expect(resultCampaign.name).to.equal(originalCampaign.name);
+      expect(resultCampaign.customLandingPageText).to.equal(originalCampaign.customLandingPageText);
     });
   });
 
   context('when an error occurred', function () {
-    it('should throw an error when the campaign could not be retrieved', function () {
+    it('should throw an error when the user does not have an access to the campaign organization', async function () {
       // given
-      campaignRepository.get.withArgs(originalCampaign.id).rejects();
-
-      // when
-      const promise = updateCampaign({
-        userId: userWithMembership.id,
-        campaignId: originalCampaign.id,
-        userRepository,
-        campaignRepository,
-      });
-
-      // then
-      return expect(promise).to.be.rejected;
-    });
-
-    it('should throw an error when the user with memberships could not be retrieved', function () {
-      // given
-      userRepository.getWithMemberships.withArgs(userWithMembership.id).rejects();
-
-      // when
-      const promise = updateCampaign({
-        userId: userWithMembership.id,
-        campaignId: originalCampaign.id,
-        userRepository,
-        campaignRepository,
-      });
-
-      // then
-      return expect(promise).to.be.rejected;
-    });
-
-    it('should throw an error when the user does not have an access to the campaign organization', function () {
-      // given
-      userWithMembership.hasAccessToOrganization.withArgs(organizationId).returns(false);
-
-      // when
-      const promise = updateCampaign({
-        userId: userWithMembership.id,
-        campaignId: originalCampaign.id,
-        userRepository,
-        campaignRepository,
-      });
-
-      // then
-      return expect(promise).to.be.rejectedWith(UserNotAuthorizedToUpdateResourceError);
-    });
-
-    it('should throw an error when the campaign could not be updated', function () {
-      // given
-      campaignRepository.update.withArgs(originalCampaign).rejects();
-
-      // when
-      const promise = updateCampaign({
-        userId: userWithMembership.id,
-        campaignId: originalCampaign.id,
-        userRepository,
-        campaignRepository,
-      });
-
-      // then
-      return expect(promise).to.be.rejected;
-    });
-
-    it('should throw an error when the campaign is not valid', async function () {
-      originalCampaign = domainBuilder.buildCampaign.ofTypeProfilesCollection({
+      const userWithoutMembership = {
         id: 1,
-        name: 'I cannot have title',
-        customLandingPageText: 'All our hopes now lie with one little test',
-        organization: { id: organizationId },
-        creator: { id: creatorId },
-      });
+        hasAccessToOrganization: sinon.stub(),
+      };
+      originalCampaign = domainBuilder.buildCampaign({ organization: { id: organizationId } });
 
       campaignRepository.get.withArgs(originalCampaign.id).resolves(originalCampaign);
+      userRepository.getWithMemberships.withArgs(userWithoutMembership.id).resolves(userWithoutMembership);
+      userWithoutMembership.hasAccessToOrganization.withArgs(organizationId).returns(false);
 
       // when
       const error = await catchErr(updateCampaign)({
-        userId: userWithMembership.id,
+        userId: userWithoutMembership.id,
         campaignId: originalCampaign.id,
-        title: 'You shall not pass !',
         userRepository,
         campaignRepository,
       });
 
       // then
-      expect(error).to.be.instanceOf(EntityValidationError);
+      expect(error).to.be.instanceOf(UserNotAuthorizedToUpdateResourceError);
     });
   });
 });
