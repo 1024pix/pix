51e50123de4b5f0006856f7cebe4b3e2d8ec51c7
Clément Bourgoin <clement.bourgoin@pix.fr>
2022-01-27 17:08:18 +0100

    feat(api): add billing columns to certification-candidates table

---
+++ api/db/migrations/20220127154426_add-billing-columns-to-certification-candidate.js
@@ -0,0 +1,19 @@
+const TABLE_NAME = 'certification-candidates';
+
+exports.up = async function (knex) {
+  await knex.schema.table(TABLE_NAME, (table) => {
+    table.text('billingMode').defaultsTo(null);
+    table.text('prepaymentCode').defaultsTo(null);
+  });
+
+  await knex.raw(
+    'ALTER TABLE "certification-candidates" ADD CONSTRAINT "certification-candidates_billingMode_check" CHECK ( "billingMode" IN ( \'Gratuite\', \'Payante\', \'Prépayée\'))'
+  );
+};
+
+exports.down = function (knex) {
+  return knex.schema.alterTable(TABLE_NAME, (table) => {
+    table.dropColumn('billingMode');
+    table.dropColumn('prepaymentCode');
+  });
+};
