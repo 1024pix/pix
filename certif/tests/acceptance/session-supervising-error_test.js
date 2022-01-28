ae224fdf05fa558cb7e0a798bd123753f63271ed
Quentin Lebouc <quentin.lebouc@pix.fr>
2022-01-28 15:40:17 +0100

    :sparkles: feat(certif): display an error page when supervisor has no access to the session

---
+++ certif/tests/acceptance/session-supervising-error_test.js
@@ -0,0 +1,53 @@
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import { click, fillIn } from '@ember/test-helpers';
+import { visit as visitScreen } from '@1024pix/ember-testing-library';
+import { authenticateSession } from '../helpers/test-init';
+
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+
+module('Acceptance | Session supervising error', function (hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  hooks.beforeEach(async function () {
+    const certificationPointOfContact = server.create('certification-point-of-contact', {
+      firstName: 'Aude',
+      lastName: 'Hébussavabien',
+      pixCertifTermsOfServiceAccepted: true,
+      allowedCertificationCenterAccesses: [],
+    });
+    await authenticateSession(certificationPointOfContact.id);
+  });
+
+  module('When there are candidates on the session', function () {
+    test('it should display candidates entries', async function (assert) {
+      // given
+      this.sessionForSupervising = server.create('session-for-supervising', {
+        id: 2000,
+      });
+      this.secondSession = server.create('session', {
+        id: 2001,
+      });
+
+      const screen = await visitScreen('/connexion-espace-surveillant');
+      await fillIn(screen.getByRole('spinbutton', { name: 'Numéro de la session' }), '2000');
+      await fillIn(screen.getByLabelText('Mot de passe de la session'), '6789');
+      await click(screen.getByRole('button', { name: 'Surveiller la session' }));
+
+      // when
+      const secondScreen = await visitScreen('/sessions/2001/surveiller');
+
+      // then
+      assert.dom(secondScreen.getByRole('heading', { name: 'Une erreur est survenue' })).exists();
+      assert
+        .dom(
+          secondScreen.getByText(
+            'Pour accéder à cette session, cliquez sur le bouton "Surveiller une session" et renseignez les informations de la session'
+          )
+        )
+        .exists();
+      assert.dom(secondScreen.getByRole('link', { name: 'Surveiller une session' })).exists();
+    });
+  });
+});
