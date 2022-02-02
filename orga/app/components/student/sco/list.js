6ca7436db4a8596b0d03dd5caf69261045108723
Lise Quesnel <quesnel.lise@gmail.com>
2022-02-02 17:02:04 +0100

    [ORGA] Remove unused dissociation feature

--- orga/app/components/student/sco/list.js
+++ orga/app/components/student/sco/list.js
@@ -2,15 +2,12 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import ENV from 'pix-orga/config/environment';
 
 export default class ScoList extends Component {
   @service currentUser;
 
   @tracked student = null;
   @tracked isShowingAuthenticationMethodModal = false;
-  @tracked isShowingDissociateModal = false;
-  @tracked isDissociateButtonEnabled = ENV.APP.IS_DISSOCIATE_BUTTON_ENABLED;
 
   loadDivisions = async () => {
     const divisions = await this.currentUser.organization.divisions;
@@ -31,16 +28,5 @@ export default class ScoList extends Component {
   @action
   closeAuthenticationMethodModal() {
     this.isShowingAuthenticationMethodModal = false;
-  }
-
-  @action
-  openDissociateModal(student) {
-    this.student = student;
-    this.isShowingDissociateModal = true;
-  }
-
-  @action
-  closeDissociateModal() {
-    this.isShowingDissociateModal = false;
   }
 }
