c049426c15b6f563f87d43d16b4f6f59281a538d
P-Jeremy <jemyplu@gmail.com>
2022-02-02 10:27:27 +0100

    :sparkles: feat(api): add route to fetch supervisor kit pdf

---
+++ api/tests/acceptance/application/session/session-controller-get-supervisor-kit-PDF_test.js
@@ -0,0 +1,71 @@
+const { expect, databaseBuilder, generateValidRequestAuthorizationHeader } = require('../../../test-helper');
+const createServer = require('../../../../server');
+
+describe('Acceptance | Controller | session-controller-get-supervisor-kit-pdf', function () {
+  let server;
+
+  beforeEach(async function () {
+    server = await createServer();
+  });
+
+  describe('GET /api/sessions/{id}/supervisor-kit', function () {
+    let user, sessionIdAllowed, sessionIdNotAllowed;
+    beforeEach(async function () {
+      // given
+      user = databaseBuilder.factory.buildUser();
+      databaseBuilder.factory.buildOrganization({ externalId: 'EXT1234' });
+      const certificationCenterId = databaseBuilder.factory.buildCertificationCenter({ externalId: 'EXT1234' }).id;
+      databaseBuilder.factory.buildCertificationCenterMembership({ userId: user.id, certificationCenterId });
+
+      const otherUserId = databaseBuilder.factory.buildUser().id;
+      const otherCertificationCenterId = databaseBuilder.factory.buildCertificationCenter().id;
+      databaseBuilder.factory.buildCertificationCenterMembership({
+        userId: otherUserId,
+        certificationCenterId: otherCertificationCenterId,
+      });
+
+      sessionIdAllowed = databaseBuilder.factory.buildSession({ certificationCenterId }).id;
+      sessionIdNotAllowed = databaseBuilder.factory.buildSession({
+        certificationCenterId: otherCertificationCenterId,
+      }).id;
+
+      await databaseBuilder.commit();
+    });
+
+    it('should respond with a 200 when session can be found', async function () {
+      // when
+      const authHeader = generateValidRequestAuthorizationHeader(user.id);
+      const token = authHeader.replace('Bearer ', '');
+      const options = {
+        method: 'GET',
+        url: `/api/sessions/${sessionIdAllowed}/supervisor-kit?accessToken=${token}`,
+        payload: {},
+      };
+      // when
+      const promise = server.inject(options);
+
+      // then
+      return promise.then((response) => {
+        expect(response.statusCode).to.equal(200);
+      });
+    });
+
+    it('should respond with a 403 when user cant access the session', async function () {
+      // when
+      const authHeader = generateValidRequestAuthorizationHeader(user.id);
+      const token = authHeader.replace('Bearer ', '');
+      const options = {
+        method: 'GET',
+        url: `/api/sessions/${sessionIdNotAllowed}/supervisor-kit?accessToken=${token}`,
+        payload: {},
+      };
+      // when
+      const promise = server.inject(options);
+
+      // then
+      return promise.then((response) => {
+        expect(response.statusCode).to.equal(403);
+      });
+    });
+  });
+});
