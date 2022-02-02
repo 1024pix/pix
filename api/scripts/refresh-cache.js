0cb6820addb3669e50eb84b540216250587000a7
François de Metz <francois.de-metz@pix.fr>
2022-02-02 11:49:08 +0100

    Use the logger when refreshing learning content

--- api/scripts/refresh-cache.js
+++ api/scripts/refresh-cache.js
@@ -1,12 +1,15 @@
 #! /usr/bin/env node
 'use strict';
 require('dotenv').config();
+const logger = require('../lib/infrastructure/logger');
 const cache = require('../lib/infrastructure/caches/learning-content-cache');
 const learningContentDatasource = require('../lib/infrastructure/datasources/learning-content/datasource');
 
-if (require.main === module) {
-  learningContentDatasource
-    .refreshLearningContentCacheRecords()
-    .catch((e) => console.error('Error while reloading cache', e))
-    .finally(() => cache.quit());
-}
+logger.info('Starting refreshing Learning Content');
+learningContentDatasource
+  .refreshLearningContentCacheRecords()
+  .then(() => {
+    logger.info('Learning Content refreshed');
+  })
+  .catch((e) => logger.error('Error while reloading cache', e))
+  .finally(() => cache.quit());
