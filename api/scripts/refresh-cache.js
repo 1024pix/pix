0a5929e898ef71043969628129098f41ccb5e8f5
François de Metz <francois.de-metz@pix.fr>
2022-02-02 10:23:14 +0100

    Close the redis connection after reloading the cache

--- api/scripts/refresh-cache.js
+++ api/scripts/refresh-cache.js
@@ -1,10 +1,12 @@
 #! /usr/bin/env node
 'use strict';
 require('dotenv').config();
+const cache = require('../lib/infrastructure/caches/learning-content-cache');
 const learningContentDatasource = require('../lib/infrastructure/datasources/learning-content/datasource');
 
 if (require.main === module) {
   learningContentDatasource
     .refreshLearningContentCacheRecords()
-    .catch((e) => console.error('Error while reloading cache', e));
+    .catch((e) => console.error('Error while reloading cache', e))
+    .finally(() => cache.quit());
 }
