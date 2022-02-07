8e441365f4fb18d11d1136a8113412cc7d1dc001
Antoine Ceol <antoine.ceol@gmail.com>
2022-02-07 15:47:14 +0100

    :technologist: tech(api) fix logs missing in webstorm

--- api/lib/infrastructure/logger.js
+++ api/lib/infrastructure/logger.js
@@ -1,28 +1,30 @@
 const pino = require('pino');
 const { logging: logSettings } = require('../config');
 
-let transport;
-
+let prettyPrint = undefined;
 if (logSettings.logForHumans) {
-  const omitDay = 'h:MM:ss';
-
-  transport = {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: omitDay,
-      ignore: 'pid,hostname',
-    },
-  };
+  const pretty = require('pino-pretty');
+  const omitDay = 'HH:MM:ss';
+  prettyPrint = pretty({
+    sync: true,
+    colorize: true,
+    translateTime: omitDay,
+    ignore: 'pid,hostname',
+  });
 }
 
-const logger = pino({
-  level: logSettings.logLevel,
-  redact: ['req.headers.authorization'],
-  transport,
-  enabled: logSettings.enabled,
-});
+const logger = pino(
+  {
+    level: logSettings.logLevel,
+    redact: ['req.headers.authorization'],
+    enabled: logSettings.enabled,
+  },
+  prettyPrint
+);
 
+logger.error('ERROR logs enabled');
+logger.warn('WARN logs enabled');
+logger.info('INFO logs enabled');
 logger.debug('DEBUG logs enabled');
 logger.trace('TRACE logs enabled');
 
