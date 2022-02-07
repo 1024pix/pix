edb689c7bcd1d28103245ced8878cabf91f067b5
Mathieu Gilet <mathieu.gilet@zenika.com>
2022-02-07 10:57:55 +0100

    :recycle: refactor(api): refactor getSession usecase test

--- api/tests/unit/domain/usecases/get-session_test.js
+++ api/tests/unit/domain/usecases/get-session_test.js
@@ -1,9 +1,8 @@
-const { expect, sinon, catchErr } = require('../../../test-helper');
+const { expect, sinon, catchErr, domainBuilder } = require('../../../test-helper');
 const getSession = require('../../../../lib/domain/usecases/get-session');
 const { NotFoundError } = require('../../../../lib/domain/errors');
 
 describe('Unit | UseCase | get-session', function () {
-  const sessionId = 'sessionId';
   let sessionRepository;
 
   beforeEach(function () {
@@ -13,29 +12,26 @@ describe('Unit | UseCase | get-session', function () {
   });
 
   context('when the session exists', function () {
-    // TODO: Fix this the next time the file is edited.
-    // eslint-disable-next-line mocha/no-setup-in-describe
-    const sessionToFind = Symbol('sessionToFind');
-
-    beforeEach(function () {
-      sessionRepository.get.withArgs(sessionId).resolves(sessionToFind);
-    });
-
     it('should get the session', async function () {
+      // given
+      const sessionId = 123;
+      const sessionToFind = domainBuilder.buildSession({ id: sessionId });
+      sessionRepository.get.withArgs(sessionId).resolves(sessionToFind);
+
       // when
       const actualSession = await getSession({ sessionId, sessionRepository });
 
       // then
-      expect(actualSession).to.equal(sessionToFind);
+      expect(actualSession).to.deepEqualInstance(sessionToFind);
     });
   });
 
   context('when the session does not exist', function () {
-    beforeEach(function () {
-      sessionRepository.get.withArgs(sessionId).rejects(new NotFoundError());
-    });
-
     it('should throw an error the session', async function () {
+      // given
+      const sessionId = 123;
+      sessionRepository.get.withArgs(sessionId).rejects(new NotFoundError());
+
       // when
       const err = await catchErr(getSession)({ sessionId, sessionRepository });
 
