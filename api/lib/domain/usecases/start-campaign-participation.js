a7f603a35d6610c419f211c8316b22bb7afb809e
ajoanny <ajoanny@octo.com>
2022-02-08 15:29:03 +0100

    [API] refactoring start campaign particpation

--- api/lib/domain/usecases/start-campaign-participation.js
+++ api/lib/domain/usecases/start-campaign-participation.js
@@ -1,118 +1,29 @@
-const Assessment = require('../models/Assessment');
 const CampaignParticipationStarted = require('../events/CampaignParticipationStarted');
-const CampaignParticipation = require('../models/CampaignParticipation');
-const { EntityValidationError, AlreadyExistingCampaignParticipationError } = require('../../domain/errors');
 
 module.exports = async function startCampaignParticipation({
   campaignParticipation,
   userId,
+  campaignParticipantRepository,
   campaignParticipationRepository,
-  assessmentRepository,
-  campaignToJoinRepository,
-  schoolingRegistrationRepository,
   domainTransaction,
 }) {
-  const campaignToJoin = await campaignToJoinRepository.get(campaignParticipation.campaignId, domainTransaction);
-
-  await campaignToJoinRepository.checkCampaignIsJoinableByUser(campaignToJoin, userId, domainTransaction);
-
-  const hasAlreadyParticipated = await campaignParticipationRepository.hasAlreadyParticipated(
-    campaignToJoin.id,
+  const campaignParticipant = await campaignParticipantRepository.get({
     userId,
+    campaignId: campaignParticipation.campaignId,
+    domainTransaction,
+  });
+
+  campaignParticipant.start({ participantExternalId: campaignParticipation.participantExternalId });
+
+  const campaignParticipationId = await campaignParticipantRepository.save(campaignParticipant, domainTransaction);
+
+  const createdCampaignParticipation = await campaignParticipationRepository.get(
+    campaignParticipationId,
     domainTransaction
   );
-  if (campaignToJoin.idPixLabel && !campaignParticipation.participantExternalId) {
-    if (campaignToJoin.multipleSendings && hasAlreadyParticipated) {
-      campaignParticipation.participantExternalId = await campaignParticipationRepository.findParticipantExternalId(
-        campaignToJoin.id,
-        userId,
-        domainTransaction
-      );
-    } else {
-      throw new EntityValidationError({
-        invalidAttributes: [
-          {
-            attribute: 'participantExternalId',
-            message: 'Un identifiant externe est requis pour accèder à la campagne.',
-          },
-        ],
-      });
-    }
-  }
-
-  if (hasAlreadyParticipated && !campaignToJoin.multipleSendings) {
-    throw new AlreadyExistingCampaignParticipationError(
-      `User ${userId} has already a campaign participation with campaign ${campaignToJoin.id}`
-    );
-  }
-
-  let createdCampaignParticipation;
-  if (hasAlreadyParticipated) {
-    await campaignParticipationRepository.markPreviousParticipationsAsImproved(
-      campaignToJoin.id,
-      userId,
-      domainTransaction
-    );
-    createdCampaignParticipation = await _saveCampaignParticipation(
-      campaignParticipation,
-      userId,
-      campaignToJoin,
-      campaignParticipationRepository,
-      schoolingRegistrationRepository,
-      domainTransaction
-    );
-    if (campaignToJoin.isAssessment) {
-      const assessment = Assessment.createImprovingForCampaign({
-        userId,
-        campaignParticipationId: createdCampaignParticipation.id,
-        method: campaignToJoin.assessmentMethod,
-      });
-      await assessmentRepository.save({ assessment, domainTransaction });
-    }
-  } else {
-    createdCampaignParticipation = await _saveCampaignParticipation(
-      campaignParticipation,
-      userId,
-      campaignToJoin,
-      campaignParticipationRepository,
-      schoolingRegistrationRepository,
-      domainTransaction
-    );
-    if (campaignToJoin.isAssessment) {
-      const assessment = Assessment.createForCampaign({
-        userId,
-        campaignParticipationId: createdCampaignParticipation.id,
-        method: campaignToJoin.assessmentMethod,
-      });
-      await assessmentRepository.save({ assessment, domainTransaction });
-    }
-  }
 
   return {
-    event: new CampaignParticipationStarted({ campaignParticipationId: createdCampaignParticipation.id }),
+    event: new CampaignParticipationStarted({ campaignParticipationId }),
     campaignParticipation: createdCampaignParticipation,
   };
 };
-
-async function _saveCampaignParticipation(
-  campaignParticipation,
-  userId,
-  campaign,
-  campaignParticipationRepository,
-  schoolingRegistrationRepository,
-  domainTransaction
-) {
-  const schoolingRegistration = await schoolingRegistrationRepository.findOneByUserIdAndOrganizationId({
-    organizationId: campaign.organizationId,
-    userId,
-    domainTransaction,
-  });
-  const userParticipation = CampaignParticipation.start({
-    ...campaignParticipation,
-    campaign,
-    userId,
-    schoolingRegistrationId: schoolingRegistration?.id,
-  });
-
-  return campaignParticipationRepository.save(userParticipation, domainTransaction);
-}
