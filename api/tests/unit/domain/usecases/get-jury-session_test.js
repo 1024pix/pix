b093d8bf820635d4355ba4f4ed739d9b80512a88
Mathieu Gilet <mathieu.gilet@zenika.com>
2022-02-08 11:35:15 +0100

    :recycle: refactor(api): use domainBuilder in test

--- api/tests/unit/domain/usecases/get-jury-session_test.js
+++ api/tests/unit/domain/usecases/get-jury-session_test.js
@@ -1,9 +1,8 @@
-const { expect, sinon, catchErr } = require('../../../test-helper');
+const { expect, sinon, catchErr, domainBuilder } = require('../../../test-helper');
 const getJurySession = require('../../../../lib/domain/usecases/get-jury-session');
 const { NotFoundError } = require('../../../../lib/domain/errors');
 
 describe('Unit | UseCase | get-jury-session', function () {
-  const sessionId = 'sessionId';
   let jurySessionRepository;
 
   beforeEach(function () {
@@ -13,29 +12,26 @@ describe('Unit | UseCase | get-jury-session', function () {
   });
 
   context('when the session exists', function () {
-    // TODO: Fix this the next time the file is edited.
-    // eslint-disable-next-line mocha/no-setup-in-describe
-    const sessionToFind = Symbol('sessionToFind');
-
-    beforeEach(function () {
-      jurySessionRepository.get.withArgs(sessionId).resolves(sessionToFind);
-    });
-
     it('should get the session', async function () {
+      // given
+      const sessionId = 123;
+      const sessionToFind = domainBuilder.buildJurySession({ id: sessionId });
+      jurySessionRepository.get.withArgs(sessionId).resolves(sessionToFind);
+
       // when
       const actualSession = await getJurySession({ sessionId, jurySessionRepository });
 
       // then
-      expect(actualSession).to.equal(sessionToFind);
+      expect(actualSession).to.deepEqualInstance(sessionToFind);
     });
   });
 
   context('when the session does not exist', function () {
-    beforeEach(function () {
-      jurySessionRepository.get.withArgs(sessionId).rejects(new NotFoundError());
-    });
-
     it('should throw an error the session', async function () {
+      // given
+      const sessionId = 123;
+      jurySessionRepository.get.withArgs(sessionId).rejects(new NotFoundError());
+
       // when
       const err = await catchErr(getJurySession)({ sessionId, jurySessionRepository });
 
