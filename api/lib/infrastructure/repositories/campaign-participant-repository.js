c41dec76c5a28eebcf9a220cd79e14e66d9a8715
ajoanny <ajoanny@octo.com>
2022-02-08 15:28:25 +0100

    [API] create CampaignParticipantRepository

---
+++ api/lib/infrastructure/repositories/campaign-participant-repository.js
@@ -0,0 +1,123 @@
+const pick = require('lodash/pick');
+const CampaignParticipant = require('../../domain/models/CampaignParticipant');
+const CampaignToStartParticipation = require('../../domain/models/CampaignToStartParticipation');
+const { AlreadyExistingCampaignParticipationError, NotFoundError } = require('../../domain/errors');
+const skillDatasource = require('../datasources/learning-content/skill-datasource');
+
+async function save(campaignParticipant, domainTransaction) {
+  await _updatePreviousParticipation(
+    campaignParticipant.previousCampaignParticipation,
+    domainTransaction.knexTransaction
+  );
+  const campaignParticipationId = await _createNewCampaignParticipation(
+    domainTransaction.knexTransaction,
+    campaignParticipant.campaignParticipation
+  );
+  await _createAssessment(campaignParticipant.assessment, campaignParticipationId, domainTransaction.knexTransaction);
+  return campaignParticipationId;
+}
+
+async function _updatePreviousParticipation(campaignParticipation, queryBuilder) {
+  if (campaignParticipation) {
+    await queryBuilder('campaign-participations')
+      .update({ isImproved: campaignParticipation.isImproved })
+      .where({ id: campaignParticipation.id });
+  }
+}
+
+async function _createNewCampaignParticipation(queryBuilder, campaignParticipation) {
+  try {
+    const [id] = await queryBuilder('campaign-participations')
+      .insert({
+        campaignId: campaignParticipation.campaignId,
+        userId: campaignParticipation.userId,
+        status: campaignParticipation.status,
+        schoolingRegistrationId: campaignParticipation.schoolingRegistrationId,
+      })
+      .returning('id');
+
+    return id;
+  } catch (error) {
+    if (error.constraint === 'campaign_participations_campaignid_userid_isimproved') {
+      throw new AlreadyExistingCampaignParticipationError(
+        `User ${campaignParticipation.userId} has already a campaign participation with campaign ${campaignParticipation.campaignId}`
+      );
+    }
+    throw error;
+  }
+}
+
+async function _createAssessment(assessment, campaignParticipationId, queryBuilder) {
+  if (assessment) {
+    const assessmentAttributes = pick(assessment, ['userId', 'method', 'state', 'type', 'courseId', 'isImproving']);
+    await queryBuilder('assessments').insert({ campaignParticipationId, ...assessmentAttributes });
+  }
+}
+
+async function get({ userId, campaignId, domainTransaction }) {
+  const campaignToStartParticipation = await _getCampaignToStart(campaignId, domainTransaction);
+
+  const schoolingRegistrationId = await _getSchoolingRegistrationId(campaignId, userId, domainTransaction);
+
+  const previousCampaignParticipation = await _getPreviousCampaignParticipation(campaignId, userId, domainTransaction);
+
+  return new CampaignParticipant({
+    userId,
+    campaignToStartParticipation,
+    schoolingRegistrationId,
+    previousCampaignParticipation,
+  });
+}
+
+async function _getCampaignToStart(campaignId, domainTransaction) {
+  const campaignAttributes = await domainTransaction
+    .knexTransaction('campaigns')
+    .join('organizations', 'organizations.id', 'organizationId')
+    .select([
+      'campaigns.id',
+      'campaigns.type',
+      'idPixLabel',
+      'archivedAt',
+      'isManagingStudents AS isRestricted',
+      'multipleSendings',
+      'assessmentMethod',
+    ])
+    .where({ 'campaigns.id': campaignId })
+    .first();
+
+  if (!campaignAttributes) {
+    throw new NotFoundError(`La campagne d'id ${campaignId} n'existe pas ou son accès est restreint`);
+  }
+  const skillIds = await domainTransaction
+    .knexTransaction('target-profiles_skills')
+    .join('campaigns', 'campaigns.targetProfileId', 'target-profiles_skills.targetProfileId')
+    .where({ 'campaigns.id': campaignId })
+    .pluck('skillId');
+
+  const skills = await skillDatasource.findOperativeByRecordIds(skillIds);
+
+  return new CampaignToStartParticipation({ ...campaignAttributes, skillCount: skills.length });
+}
+
+async function _getSchoolingRegistrationId(campaignId, userId, domainTransaction) {
+  const [id] = await domainTransaction
+    .knexTransaction('campaigns')
+    .join('schooling-registrations', 'schooling-registrations.organizationId', 'campaigns.organizationId')
+    .where({ 'campaigns.id': campaignId, userId, isDisabled: false })
+    .pluck('schooling-registrations.id');
+
+  return id;
+}
+
+async function _getPreviousCampaignParticipation(campaignId, userId, domainTransaction) {
+  return domainTransaction
+    .knexTransaction('campaign-participations')
+    .select('id', 'participantExternalId', 'validatedSkillsCount', 'status')
+    .where({ campaignId, userId, isImproved: false })
+    .first();
+}
+
+module.exports = {
+  get,
+  save,
+};
