39711b6db506295404c804e8a7ae7276534b0159
alexandrecoin <alexandre.pc.coin@gmail.com>
2022-02-09 10:13:56 +0100

    feat(admin): delete badge

--- admin/tests/integration/components/target-profiles/badges_test.js
+++ admin/tests/integration/components/target-profiles/badges_test.js
@@ -1,8 +1,10 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { find, render } from '@ember/test-helpers';
+import { click, find, render } from '@ember/test-helpers';
 import EmberObject from '@ember/object';
 import hbs from 'htmlbars-inline-precompile';
+import clickByLabel from '../../../helpers/extended-ember-test-helpers/click-by-label';
+import sinon from 'sinon';
 
 module('Integration | Component | TargetProfiles::Badges', function (hooks) {
   setupRenderingTest(hooks);
@@ -60,5 +62,56 @@ module('Integration | Component | TargetProfiles::Badges', function (hooks) {
     // then
     assert.dom('table').doesNotExist();
     assert.contains('Aucun résultat thématique associé');
+  });
+
+  module('when deleting a badge', function (hooks) {
+    let badge;
+
+    hooks.beforeEach(async function () {
+      // given
+      const store = this.owner.lookup('service:store');
+      badge = store.push({
+        data: {
+          id: 'badgeId',
+          type: 'badge',
+          attributes: {
+            key: 'My key',
+            title: 'My title',
+            message: 'My message',
+            imageUrl: 'data:,',
+            altMessage: 'My alt message',
+          },
+        },
+      });
+      badge.destroyRecord = sinon.stub();
+      this.set('badges', [badge]);
+
+      // when
+      await render(hbs`<TargetProfiles::Badges @badges={{this.badges}} />`);
+      await clickByLabel('Supprimer');
+    });
+
+    test('should open confirm modal', function (assert) {
+      // then
+      assert.dom('.modal-dialog').exists();
+      assert.contains("Suppression d'un résultat thématique");
+      assert.contains('Êtes-vous sûr de vouloir supprimer ce résultat thématique ?');
+    });
+
+    test('should close confirm modal on click on cancel', async function (assert) {
+      // when
+      await click('.modal-footer > button.btn-secondary');
+
+      // then
+      assert.dom('.modal-dialog').doesNotExist();
+    });
+
+    test('should delete the badge on confirmation click', async function (assert) {
+      // when
+      await click('.modal-footer > button.btn-primary');
+
+      // then
+      assert.ok(badge.destroyRecord.called);
+    });
   });
 });
