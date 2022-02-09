d6aa0a92e7b79e1df64fd790efefb516ab110ca1
Anne-Marie Esteves <anne-marie.esteves@octo.com>
2022-02-09 11:20:20 +0100

    [ORGA] Add a clear all filters button on campaign filters component

--- orga/tests/integration/components/campaign/filter/filters_test.js
+++ orga/tests/integration/components/campaign/filter/filters_test.js
@@ -9,6 +9,7 @@ module('Integration | Component | Campaign::Filter::Filters', function (hooks) {
   hooks.beforeEach(function () {
     this.set('triggerFilteringSpy', () => {});
     this.set('onClickStatusFilterSpy', () => {});
+    this.set('onClickClearFiltersSpy', () => {});
   });
 
   test('it should display filters', async function (assert) {
@@ -17,6 +18,7 @@ module('Integration | Component | Campaign::Filter::Filters', function (hooks) {
       hbs`<Campaign::Filter::Filters
         @onFilter={{this.triggerFilteringSpy}}
         @onClickStatusFilter={{this.onClickStatusFilterSpy}}
+        @onClearFilters={{this.onClickClearFiltersSpy}}
         @numResults={{1}} />`
     );
 
@@ -29,6 +31,20 @@ module('Integration | Component | Campaign::Filter::Filters', function (hooks) {
     assert.dom(screen.getByText('1 campagne')).exists();
   });
 
+  test('it should display reset filters button', async function (assert) {
+    // when
+    const screen = await renderScreen(
+      hbs`<Campaign::Filter::Filters
+        @onFilter={{this.triggerFilteringSpy}}
+        @onClickStatusFilter={{this.onClickStatusFilterSpy}}
+        @onClearFilters={{this.onClickClearFiltersSpy}}
+        @numResults={{1}} />`
+    );
+
+    // then
+    assert.dom(screen.getByText(this.intl.t('pages.campaigns-list.filter.clear'))).exists();
+  });
+
   module('when showing current user campaign list', function () {
     test('it should not show creator input', async function (assert) {
       // given / when
@@ -36,6 +52,7 @@ module('Integration | Component | Campaign::Filter::Filters', function (hooks) {
         hbs`<Campaign::Filter::Filters
         @onFilter={{this.triggerFilteringSpy}}
         @onClickStatusFilter={{this.onClickStatusFilterSpy}}
+        @onClearFilters={{this.onClickClearFiltersSpy}}
         @numResults={{1}}
         @listOnlyCampaignsOfCurrentUser={{true}} />`
       );
@@ -51,6 +68,7 @@ module('Integration | Component | Campaign::Filter::Filters', function (hooks) {
       hbs`<Campaign::Filter::Filters
         @onFilter={{this.triggerFilteringSpy}}
         @onClickStatusFilter={{this.onClickStatusFilterSpy}}
+        @onClearFilters={{this.onClickClearFiltersSpy}}
         @numResults={{1}} />`
     );
 
