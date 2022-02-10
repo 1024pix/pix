3731bdd36643d8ed20942c7ae4c9b54aec43026c
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2022-02-10 11:03:01 +0100

    test(mon-pix): tutorial card content

--- mon-pix/tests/integration/components/tutorials/card_test.js
+++ mon-pix/tests/integration/components/tutorials/card_test.js
@@ -11,7 +11,7 @@ describe('Integration | Component | Tutorials | Card', function () {
     // given
     this.set('tutorial', {
       title: 'Mon super tutoriel',
-      link: 'https://exemple.net',
+      link: 'https://exemple.net/',
       source: 'mon-tuto',
       format: 'vidéo',
       duration: '00:01:00',
@@ -26,12 +26,20 @@ describe('Integration | Component | Tutorials | Card', function () {
     expect(find('.tutorial-card-v2')).to.exist;
     expect(find('.tutorial-card-v2__domain-border')).to.exist;
     expect(find('.tutorial-card-v2__content')).to.exist;
-    expect(find('.tutorial-card-v2-content__skill')).to.exist;
-    expect(find('.tutorial-card-v2-content__link')).to.exist;
-    expect(find('.tutorial-card-v2-content__details')).to.exist;
-    expect(find('.tutorial-card-v2-content__spacer')).to.exist;
+    expect(find('.tutorial-card-v2-content__skill'))
+      .to.have.property('textContent')
+      .that.contains("Mener une recherche et une veille d'information");
+    expect(find('.tutorial-card-v2-content__link')).to.have.property('textContent').that.contains('Mon super tutoriel');
+    expect(find('.tutorial-card-v2-content__link')).to.have.property('href').that.equals('https://exemple.net/');
+    expect(find('.tutorial-card-v2-content__details'))
+      .to.have.property('textContent')
+      .that.contains('mon-tuto')
+      .and.contains('vidéo')
+      .and.contains('une minute');
     expect(find('.tutorial-card-v2-content__actions')).to.exist;
-    expect(find('.tutorial-card-v2-content-actions__save')).to.exist;
+    expect(find('.tutorial-card-v2-content-actions__save'))
+      .to.have.property('textContent')
+      .that.contains(this.intl.t('pages.user-tutorials.list.tutorial.actions.remove.label'));
     expect(find('.tutorial-card-v2-content-actions__evaluate')).to.exist;
   });
 });
