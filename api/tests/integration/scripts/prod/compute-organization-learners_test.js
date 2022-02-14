de0887ea7ad33c90b7483f111a7d4e6ebb1d0916
Lise Quesnel <quesnel.lise@gmail.com>
2022-02-14 12:13:36 +0100

    feat(api): Update campaign participation with schoolingRegistrationId

--- api/tests/integration/scripts/prod/compute-organization-learners_test.js
+++ api/tests/integration/scripts/prod/compute-organization-learners_test.js
@@ -19,4 +19,26 @@ describe('computeOrganizationLearners', function () {
 
     expect(campaignParticipation.schoolingRegistrationId).to.equal(schoolingRegistrationId);
   });
+
+  it('links campaign participation with schooling registration', async function () {
+    const { id: organizationId } = databaseBuilder.factory.buildOrganization();
+    const { id: userId } = databaseBuilder.factory.buildUser();
+    const { id: campaignId } = databaseBuilder.factory.buildCampaign({ organizationId });
+    databaseBuilder.factory.buildCampaignParticipation({
+      userId,
+      campaignId,
+      schoolingRegistrationId: null,
+    });
+    const { id: schoolingRegistrationId } = databaseBuilder.factory.buildSchoolingRegistration({
+      id: 1,
+      userId,
+      organizationId,
+    });
+    await databaseBuilder.commit();
+
+    await computeTrainees(1, false);
+    const campaignParticipation = await knex('campaign-participations').select('schoolingRegistrationId').first();
+
+    expect(campaignParticipation.schoolingRegistrationId).to.equal(schoolingRegistrationId);
+  });
 });
