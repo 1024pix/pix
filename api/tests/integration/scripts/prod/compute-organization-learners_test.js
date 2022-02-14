2bde899b8501deadb4766abed358b2c8195628a6
Lise Quesnel <quesnel.lise@gmail.com>
2022-02-14 14:24:44 +0100

    test(api): Add tests on organizationId and userId

--- api/tests/integration/scripts/prod/compute-organization-learners_test.js
+++ api/tests/integration/scripts/prod/compute-organization-learners_test.js
@@ -25,6 +25,58 @@ describe('computeOrganizationLearners', function () {
     expect(campaignParticipation.schoolingRegistrationId).to.equal(schoolingRegistrationId);
   });
 
+  it('does not update campaign participation with schooling registration in another organization', async function () {
+    const { id: otherOrganizationId } = databaseBuilder.factory.buildOrganization();
+    const { id: organizationId } = databaseBuilder.factory.buildOrganization();
+    const { id: userId } = databaseBuilder.factory.buildUser();
+    const { id: campaignId } = databaseBuilder.factory.buildCampaign({ organizationId });
+    databaseBuilder.factory.buildCampaignParticipation({
+      userId,
+      campaignId,
+      schoolingRegistrationId: null,
+    });
+    databaseBuilder.factory.buildSchoolingRegistration({
+      userId,
+      organizationId: otherOrganizationId,
+    });
+    await databaseBuilder.commit();
+
+    await computeOrganizationLearners(1, false);
+
+    const campaignParticipation = await knex('campaign-participations').select('schoolingRegistrationId').first();
+    const schoolingRegistration = await knex('schooling-registrations')
+      .select('organizationId')
+      .where({ id: campaignParticipation.schoolingRegistrationId })
+      .first();
+    expect(schoolingRegistration.organizationId).to.equal(organizationId);
+  });
+
+  it('does not update campaign participation with schooling registration for another user', async function () {
+    const { id: otherUserId } = databaseBuilder.factory.buildUser();
+    const { id: organizationId } = databaseBuilder.factory.buildOrganization();
+    const { id: userId } = databaseBuilder.factory.buildUser();
+    const { id: campaignId } = databaseBuilder.factory.buildCampaign({ organizationId });
+    databaseBuilder.factory.buildCampaignParticipation({
+      userId,
+      campaignId,
+      schoolingRegistrationId: null,
+    });
+    databaseBuilder.factory.buildSchoolingRegistration({
+      userId: otherUserId,
+      organizationId,
+    });
+    await databaseBuilder.commit();
+
+    await computeOrganizationLearners(1, false);
+
+    const campaignParticipation = await knex('campaign-participations').select('schoolingRegistrationId').first();
+    const schoolingRegistration = await knex('schooling-registrations')
+      .select('userId')
+      .where({ id: campaignParticipation.schoolingRegistrationId })
+      .first();
+    expect(schoolingRegistration.userId).to.equal(userId);
+  });
+
   context('when a schooling registration is already existing', function () {
     it('links campaign participation with schooling registration', async function () {
       const { id: organizationId } = databaseBuilder.factory.buildOrganization();
