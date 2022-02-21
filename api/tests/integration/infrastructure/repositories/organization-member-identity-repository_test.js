af653dd6c0bb2a6838f07ccc7586c3fc36e1c324
Anne-G <ang.schall@gmail.com>
2022-02-21 14:07:07 +0100

    [api] improve repository integration test

--- api/tests/integration/infrastructure/repositories/organization-member-identity-repository_test.js
+++ api/tests/integration/infrastructure/repositories/organization-member-identity-repository_test.js
@@ -4,26 +4,61 @@ const organizationMemberIdentityRepository = require('../../../../lib/infrastruc
 
 describe('Integration | Repository | organizationMemberIdentityRepository', function () {
   describe('#findAllByOrganizationId', function () {
-    it('should return all actives members of organization sorted by firstName and lastName', async function () {
+    it('should return only actives members identities', async function () {
+      // given
+      const organizationId = databaseBuilder.factory.buildOrganization().id;
+      const activeMemberId = databaseBuilder.factory.buildUser({ firstName: 'Jean', lastName: 'Némard' }).id;
+      const disabledMemberId = databaseBuilder.factory.buildUser().id;
+      databaseBuilder.factory.buildMembership({ organizationId, userId: activeMemberId });
+      databaseBuilder.factory.buildMembership({ organizationId, userId: disabledMemberId, disabledAt: new Date() });
+      await databaseBuilder.commit();
+
+      // when
+      const members = await organizationMemberIdentityRepository.findAllByOrganizationId({ organizationId });
+
+      // then
+      expect(members).to.have.lengthOf(1);
+      expect(members[0]).to.be.an.instanceof(OrganizationMemberIdentity);
+      expect(members[0].id).to.equal(activeMemberId);
+      expect(members[0].firstName).to.equal('Jean');
+      expect(members[0].lastName).to.equal('Némard');
+    });
+
+    it('should return all members of the organization', async function () {
       // given
       const organizationId = databaseBuilder.factory.buildOrganization().id;
       const otherOrganizationId = databaseBuilder.factory.buildOrganization().id;
       const activeMemberId = databaseBuilder.factory.buildUser({ firstName: 'Jean', lastName: 'Némard' }).id;
-      const otherActiveMemberId = databaseBuilder.factory.buildUser({ firstName: 'Jean', lastName: 'Registre' }).id;
-      const otherActiveMemberId2 = databaseBuilder.factory.buildUser({ firstName: 'Anne', lastName: 'Registre' }).id;
-      const disabledMemberId = databaseBuilder.factory.buildUser().id;
       const memberOfAnotherOrganization = databaseBuilder.factory.buildUser({
         firstName: 'Jean',
         lastName: 'Tanrien',
       }).id;
+
       databaseBuilder.factory.buildMembership({ organizationId, userId: activeMemberId });
-      databaseBuilder.factory.buildMembership({ organizationId, userId: otherActiveMemberId });
-      databaseBuilder.factory.buildMembership({ organizationId, userId: otherActiveMemberId2 });
-      databaseBuilder.factory.buildMembership({ organizationId, userId: disabledMemberId, disabledAt: new Date() });
       databaseBuilder.factory.buildMembership({
         organizationId: otherOrganizationId,
         userId: memberOfAnotherOrganization,
       });
+      await databaseBuilder.commit();
+
+      // when
+      const members = await organizationMemberIdentityRepository.findAllByOrganizationId({ organizationId });
+
+      // then
+      expect(members).to.have.lengthOf(1);
+      expect(members[0].id).to.deep.equal(activeMemberId);
+    });
+
+    it('should return members sorted by firstName and lastName', async function () {
+      // given
+      const organizationId = databaseBuilder.factory.buildOrganization().id;
+      const activeMemberId = databaseBuilder.factory.buildUser({ firstName: 'Jean', lastName: 'Némard' }).id;
+      const otherActiveMemberId = databaseBuilder.factory.buildUser({ firstName: 'Jean', lastName: 'Registre' }).id;
+      const otherActiveMemberId2 = databaseBuilder.factory.buildUser({ firstName: 'Anne', lastName: 'Registre' }).id;
+
+      databaseBuilder.factory.buildMembership({ organizationId, userId: activeMemberId });
+      databaseBuilder.factory.buildMembership({ organizationId, userId: otherActiveMemberId });
+      databaseBuilder.factory.buildMembership({ organizationId, userId: otherActiveMemberId2 });
       await databaseBuilder.commit();
 
       // when
