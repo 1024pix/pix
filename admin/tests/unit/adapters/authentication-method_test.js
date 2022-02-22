d24f16604780c42668d0720f905d87bac1cdf3ca
Anne-G <ang.schall@gmail.com>
2022-02-22 17:15:37 +0100

    [admin] refacto into better ember way

---
+++ admin/tests/unit/adapters/authentication-method_test.js
@@ -0,0 +1,42 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import sinon from 'sinon';
+
+module('Unit | Adapter | authentication-method', function (hooks) {
+  setupTest(hooks);
+
+  module('#deleteRecord', function () {
+    module('when reassign authentication method adapterOptions is passed', function () {
+      test('should send a POST request to user authentication method endpoint', async function (assert) {
+        // given
+        const adapter = this.owner.lookup('adapter:authentication-method');
+        sinon.stub(adapter, 'ajax');
+
+        const targetUserId = 1;
+        const originUserId = 123;
+        const identityProvider = 'GAR';
+        const adapterOptions = {
+          reassignAuthenticationMethodToAnotherUser: true,
+          targetUserId,
+          originUserId,
+          identityProvider,
+        };
+
+        // when
+        await adapter.deleteRecord(null, { modelName: 'authentication-method' }, { id: 2, adapterOptions });
+
+        // then
+        const expectedUrl = 'http://localhost:3000/api/admin/users/123/authentication-methods/2';
+        const expectedPayload = {
+          data: {
+            data: {
+              attributes: { 'identity-provider': 'GAR', 'user-id': targetUserId },
+            },
+          },
+        };
+        sinon.assert.calledWith(adapter.ajax, expectedUrl, 'POST', expectedPayload);
+        assert.ok(adapter); /* required because QUnit wants at least one expect (and does not accept Sinon's one) */
+      });
+    });
+  });
+});
