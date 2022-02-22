3a3dc6a9f5cea7084101a4c1fd89d27623fb39ab
François de Metz <francois.de-metz@pix.fr>
2022-02-22 14:17:27 +0100

    Update the number of bcrypt rounds by default

--- api/lib/domain/services/encryption-service.js
+++ api/lib/domain/services/encryption-service.js
@@ -1,13 +1,13 @@
 const bcrypt = require('bcrypt');
 const PasswordNotMatching = require('../errors').PasswordNotMatching;
 
-const NUMBER_OF_SALT_ROUNDS = 5;
+const NUMBER_OF_ROUNDS = 10;
 
 module.exports = {
-  hashPassword: (password) => bcrypt.hash(password, NUMBER_OF_SALT_ROUNDS),
+  hashPassword: (password) => bcrypt.hash(password, NUMBER_OF_ROUNDS),
 
   /* eslint-disable-next-line no-sync */
-  hashPasswordSync: (password) => bcrypt.hashSync(password, NUMBER_OF_SALT_ROUNDS),
+  hashPasswordSync: (password) => bcrypt.hashSync(password, NUMBER_OF_ROUNDS),
 
   checkPassword: async ({ password, passwordHash }) => {
     const matching = await bcrypt.compare(password, passwordHash);
