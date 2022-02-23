bdd2ac65b6be6c50f0837e4db3774fa3ff0cd77b
Mathieu Gilet <mathieu.gilet@zenika.com>
2022-02-23 10:57:20 +0100

    :sparkles: feat(api): update unique constraint in table certification-center-memberships

---
+++ api/db/migrations/20220221103255_update-unique-constraint-on-certification-center-memberships.js
@@ -0,0 +1,15 @@
+exports.up = async function (knex) {
+  await knex.schema.table('certification-center-memberships', function (table) {
+    table.dropUnique(['userId', 'certificationCenterId']);
+  });
+  return knex.raw(
+    'CREATE UNIQUE INDEX "certification-center-memberships_userid_certificationcenterid_disabledAt_unique" ON "certification-center-memberships" ("userId", "certificationCenterId") WHERE "disabledAt" IS NULL;'
+  );
+};
+
+exports.down = function (knex) {
+  return knex.schema.table('certification-center-memberships', function (table) {
+    table.dropUnique(null, 'certification-center-memberships_userid_certificationcenterid_disabledAt_unique');
+    table.unique(['userId', 'certificationCenterId']);
+  });
+};
