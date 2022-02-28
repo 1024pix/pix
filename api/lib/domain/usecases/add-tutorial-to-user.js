b357f36c278023ab55e69aeee0fe2b1cc37d6132
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-02-28 17:48:33 +0100

    feat(api): handle skillId in add-tutorial-to-user use-case

--- api/lib/domain/usecases/add-tutorial-to-user.js
+++ api/lib/domain/usecases/add-tutorial-to-user.js
@@ -1,10 +1,13 @@
 module.exports = async function addTutorialToUser({
   tutorialRepository,
+  skillRepository,
   userTutorialRepository,
   userId,
   tutorialId,
+  skillId,
 } = {}) {
   await tutorialRepository.get(tutorialId);
+  if (skillId != null) await skillRepository.get(skillId);
 
-  return userTutorialRepository.addTutorial({ userId, tutorialId });
+  return userTutorialRepository.addTutorial({ userId, tutorialId, skillId });
 };
