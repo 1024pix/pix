1fbc45f7e18fa7572d0b57047b3917143f820598
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-02-28 17:52:15 +0100

    feat(api): add user-tutorial deserialize method

--- api/lib/infrastructure/serializers/jsonapi/user-tutorial-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/user-tutorial-serializer.js
@@ -1,5 +1,6 @@
 const { Serializer } = require('jsonapi-serializer');
 const tutorial = require('./tutorial-attributes.js');
+const UserSavedTutorial = require('../../../domain/models/UserSavedTutorial');
 
 module.exports = {
   serialize(userTutorial) {
@@ -7,5 +8,12 @@ module.exports = {
       attributes: ['tutorial', 'userId', 'tutorialId', 'skillId', 'updatedAt'],
       tutorial,
     }).serialize(userTutorial);
+  },
+
+  deserialize(json) {
+    return new UserSavedTutorial({
+      id: json?.data.id,
+      skillId: json?.data.attributes['skill-id'],
+    });
   },
 };
--- api/tests/unit/infrastructure/serializers/jsonapi/user-tutorial-serializer_test.js
+++ api/tests/unit/infrastructure/serializers/jsonapi/user-tutorial-serializer_test.js
@@ -1,5 +1,6 @@
 const { expect, domainBuilder } = require('../../../../test-helper');
 const serializer = require('../../../../../lib/infrastructure/serializers/jsonapi/user-tutorial-serializer');
+const UserSavedTutorial = require('../../../../../lib/domain/models/UserSavedTutorial');
 
 describe('Unit | Serializer | JSONAPI | user-tutorial-serializer', function () {
   describe('#serialize', function () {
@@ -30,53 +31,79 @@ describe('Unit | Serializer | JSONAPI | user-tutorial-serializer', function () {
         expect(json).to.be.deep.equal(expectedJsonUserTutorial);
       });
     });
-  });
 
-  context('when there is user tutorial and tutorial', function () {
-    it('should serialize', function () {
-      // given
-      const tutorial = domainBuilder.buildTutorial({ id: 'tutorialId' });
-      const userTutorialWithTutorial = domainBuilder.buildUserSavedTutorialWithTutorial({
-        id: 123,
-        userId: 456,
-        tutorial,
-      });
-      const expectedJsonUserTutorial = {
-        data: {
-          type: 'user-tutorials',
-          id: '123',
-          attributes: {
-            'user-id': 456,
-          },
-          relationships: {
-            tutorial: {
-              data: {
-                id: 'tutorialId',
-                type: 'tutorials',
+    context('when there is user tutorial and tutorial', function () {
+      it('should serialize', function () {
+        // given
+        const tutorial = domainBuilder.buildTutorial({ id: 'tutorialId' });
+        const userTutorialWithTutorial = domainBuilder.buildUserSavedTutorialWithTutorial({
+          id: 123,
+          userId: 456,
+          tutorial,
+          skillId: 'skillId',
+        });
+        const expectedJsonUserTutorial = {
+          data: {
+            type: 'user-tutorials',
+            id: '123',
+            attributes: {
+              'user-id': 456,
+              'skill-id': 'skillId',
+            },
+            relationships: {
+              tutorial: {
+                data: {
+                  id: 'tutorialId',
+                  type: 'tutorials',
+                },
               },
             },
           },
-        },
-        included: [
-          {
-            attributes: {
-              duration: '00:01:30',
-              format: 'video',
+          included: [
+            {
+              attributes: {
+                duration: '00:01:30',
+                format: 'video',
+                id: 'tutorialId',
+                link: 'https://youtube.fr',
+                source: 'Youtube',
+                title: 'Savoir regarder des vidéos youtube.',
+              },
               id: 'tutorialId',
-              link: 'https://youtube.fr',
-              source: 'Youtube',
-              title: 'Savoir regarder des vidéos youtube.',
+              type: 'tutorials',
             },
-            id: 'tutorialId',
-            type: 'tutorials',
+          ],
+        };
+        // when
+        const json = serializer.serialize(userTutorialWithTutorial);
+
+        // then
+        expect(json).to.be.deep.equal(expectedJsonUserTutorial);
+      });
+    });
+  });
+
+  describe('#deserialize', function () {
+    it('should convert JSON API data into an User model object', function () {
+      // given
+      const jsonUserTutorial = {
+        data: {
+          id: 123,
+          type: 'user-tutorials',
+          attributes: {
+            'skill-id': 'skillId',
           },
-        ],
+          relationships: {},
+        },
       };
+
       // when
-      const json = serializer.serialize(userTutorialWithTutorial);
+      const userTutorial = serializer.deserialize(jsonUserTutorial);
 
       // then
-      expect(json).to.be.deep.equal(expectedJsonUserTutorial);
+      expect(userTutorial).to.be.instanceOf(UserSavedTutorial);
+      expect(userTutorial.id).to.be.equal(123);
+      expect(userTutorial.skillId).to.be.equal('skillId');
     });
   });
 });
