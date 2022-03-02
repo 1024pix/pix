69ddbd780a0c3d0da7067172b216a4d23a90e1c9
Yvonnick Frin <frin.yvonnick@gmail.com>
2022-03-02 11:18:30 +0100

    feat(api): Add serializer for UserWithActivity model

---
+++ api/lib/infrastructure/serializers/jsonapi/user-with-activity-serializer.js
@@ -0,0 +1,103 @@
+const { Serializer } = require('jsonapi-serializer');
+
+module.exports = {
+  serialize(users, meta) {
+    return new Serializer('user', {
+      transform(record) {
+        record.profile = null;
+        return record;
+      },
+      attributes: [
+        'firstName',
+        'lastName',
+        'email',
+        'username',
+        'cgu',
+        'lastTermsOfServiceValidatedAt',
+        'mustValidateTermsOfService',
+        'pixOrgaTermsOfServiceAccepted',
+        'pixCertifTermsOfServiceAccepted',
+        'lang',
+        'isAnonymous',
+        'memberships',
+        'certificationCenterMemberships',
+        'pixScore',
+        'scorecards',
+        'profile',
+        'campaignParticipations',
+        'hasSeenAssessmentInstructions',
+        'isCertifiable',
+        'hasSeenNewDashboardInfo',
+        'hasSeenFocusedChallengeTooltip',
+        'hasSeenOtherChallengesTooltip',
+        'hasAssessmentParticipations',
+        'codeForLastProfileToShare',
+      ],
+      memberships: {
+        ref: 'id',
+        ignoreRelationshipData: true,
+        relationshipLinks: {
+          related(record, current, parent) {
+            return `/api/users/${parent.id}/memberships`;
+          },
+        },
+      },
+      certificationCenterMemberships: {
+        ref: 'id',
+        ignoreRelationshipData: true,
+        relationshipLinks: {
+          related: function (record, current, parent) {
+            return `/api/users/${parent.id}/certification-center-memberships`;
+          },
+        },
+      },
+      pixScore: {
+        ref: 'id',
+        ignoreRelationshipData: true,
+        relationshipLinks: {
+          related: function (record, current, parent) {
+            return `/api/users/${parent.id}/pixscore`;
+          },
+        },
+      },
+      scorecards: {
+        ref: 'id',
+        ignoreRelationshipData: true,
+        relationshipLinks: {
+          related: function (record, current, parent) {
+            return `/api/users/${parent.id}/scorecards`;
+          },
+        },
+      },
+      profile: {
+        ref: 'id',
+        ignoreRelationshipData: true,
+        relationshipLinks: {
+          related: function (record, current, parent) {
+            return `/api/users/${parent.id}/profile`;
+          },
+        },
+      },
+      campaignParticipations: {
+        ref: 'id',
+        ignoreRelationshipData: true,
+        relationshipLinks: {
+          related: function (record, current, parent) {
+            return `/api/users/${parent.id}/campaign-participations`;
+          },
+        },
+      },
+      isCertifiable: {
+        ref: 'id',
+        ignoreRelationshipData: true,
+        nullIfMissing: true,
+        relationshipLinks: {
+          related: function (record, current, parent) {
+            return `/api/users/${parent.id}/is-certifiable`;
+          },
+        },
+      },
+      meta,
+    }).serialize(users);
+  },
+};
