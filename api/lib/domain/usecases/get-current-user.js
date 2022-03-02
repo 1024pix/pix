7448c7bb752a904b1c476db9f1edd572773548e1
Yvonnick Frin <frin.yvonnick@gmail.com>
2022-03-02 10:50:55 +0100

    feat(api): Improve get-current-user usecase with UserWithActivity model

--- api/lib/domain/usecases/get-current-user.js
+++ api/lib/domain/usecases/get-current-user.js
@@ -1,3 +1,18 @@
-module.exports = function getCurrentUser({ authenticatedUserId, userRepository }) {
-  return userRepository.get(authenticatedUserId);
+const UserWithActivity = require('../read-models/UserWithActivity');
+
+module.exports = async function getCurrentUser({
+  authenticatedUserId,
+  userRepository,
+  campaignParticipationRepository,
+}) {
+  const [hasAssessmentParticipations, codeForLastProfileToShare] = await Promise.all([
+    campaignParticipationRepository.hasAssessmentParticipations(authenticatedUserId),
+    campaignParticipationRepository.getCodeOfLastParticipationToProfilesCollectionCampaignForUser(authenticatedUserId),
+  ]);
+  const user = await userRepository.get(authenticatedUserId);
+  return new UserWithActivity({
+    user,
+    hasAssessmentParticipations,
+    codeForLastProfileToShare,
+  });
 };
