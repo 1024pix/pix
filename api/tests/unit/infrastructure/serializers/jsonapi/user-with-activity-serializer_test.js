f3e86d37c29d7ebb69eae13970a195ca0c3a27d0
Yvonnick Frin <frin.yvonnick@gmail.com>
2022-03-02 11:18:30 +0100

    feat(api): Add serializer for UserWithActivity model

---
+++ api/tests/unit/infrastructure/serializers/jsonapi/user-with-activity-serializer_test.js
@@ -0,0 +1,109 @@
+const { expect } = require('../../../../test-helper');
+
+const UserWithActivity = require('../../../../../lib/domain/read-models/UserWithActivity');
+const User = require('../../../../../lib/domain/models/User');
+const serializer = require('../../../../../lib/infrastructure/serializers/jsonapi/user-with-activity-serializer');
+
+describe('Unit | Serializer | JSONAPI | user-with-activity-serializer', function () {
+  describe('#serialize', function () {
+    let userModelObject;
+
+    beforeEach(function () {
+      userModelObject = new UserWithActivity({
+        user: new User({
+          id: '234567',
+          firstName: 'Luke',
+          lastName: 'Skywalker',
+          email: 'lskywalker@deathstar.empire',
+          username: 'luke.skywalker1234',
+          cgu: true,
+          lang: 'fr',
+          isAnonymous: false,
+          lastTermsOfServiceValidatedAt: '2020-05-04T13:18:26.323Z',
+          mustValidateTermsOfService: true,
+          pixOrgaTermsOfServiceAccepted: false,
+          pixCertifTermsOfServiceAccepted: false,
+          hasSeenAssessmentInstructions: false,
+          hasSeenFocusedChallengeTooltip: false,
+          hasSeenOtherChallengesTooltip: false,
+        }),
+        hasAssessmentParticipations: false,
+        codeForLastProfileToShare: 'SOMECODE',
+      });
+    });
+
+    describe('when user has no userOrgaSettings', function () {
+      it('should serialize excluding password', function () {
+        // given
+        const expectedSerializedUser = {
+          data: {
+            type: 'users',
+            id: userModelObject.id,
+            attributes: {
+              'first-name': userModelObject.firstName,
+              'last-name': userModelObject.lastName,
+              email: userModelObject.email,
+              username: userModelObject.username,
+              cgu: userModelObject.cgu,
+              lang: userModelObject.lang,
+              'is-anonymous': userModelObject.isAnonymous,
+              'last-terms-of-service-validated-at': userModelObject.lastTermsOfServiceValidatedAt,
+              'must-validate-terms-of-service': userModelObject.mustValidateTermsOfService,
+              'pix-orga-terms-of-service-accepted': userModelObject.pixOrgaTermsOfServiceAccepted,
+              'pix-certif-terms-of-service-accepted': userModelObject.pixCertifTermsOfServiceAccepted,
+              'has-seen-assessment-instructions': userModelObject.hasSeenAssessmentInstructions,
+              'has-seen-new-dashboard-info': userModelObject.hasSeenNewDashboardInfo,
+              'has-seen-focused-challenge-tooltip': userModelObject.hasSeenFocusedChallengeTooltip,
+              'has-seen-other-challenges-tooltip': userModelObject.hasSeenOtherChallengesTooltip,
+              'has-assessment-participations': userModelObject.hasAssessmentParticipations,
+              'code-for-last-profile-to-share': userModelObject.codeForLastProfileToShare,
+            },
+            relationships: {
+              memberships: {
+                links: {
+                  related: `/api/users/${userModelObject.id}/memberships`,
+                },
+              },
+              'certification-center-memberships': {
+                links: {
+                  related: `/api/users/${userModelObject.id}/certification-center-memberships`,
+                },
+              },
+              'pix-score': {
+                links: {
+                  related: `/api/users/${userModelObject.id}/pixscore`,
+                },
+              },
+              profile: {
+                links: {
+                  related: `/api/users/${userModelObject.id}/profile`,
+                },
+              },
+              scorecards: {
+                links: {
+                  related: `/api/users/${userModelObject.id}/scorecards`,
+                },
+              },
+              'campaign-participations': {
+                links: {
+                  related: `/api/users/${userModelObject.id}/campaign-participations`,
+                },
+              },
+              'is-certifiable': {
+                links: {
+                  related: `/api/users/${userModelObject.id}/is-certifiable`,
+                },
+              },
+            },
+          },
+        };
+
+        // when
+        const json = serializer.serialize(userModelObject);
+
+        // then
+        expect(json).to.be.deep.equal(expectedSerializedUser);
+      });
+    });
+  });
+});
