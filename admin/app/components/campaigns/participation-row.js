13c1bdc3bdf1ca21e78168aaf78fb2eeab306fb2
yaelle <ymarsiac@octo.com>
2022-03-04 10:44:46 +0100

    [ADMIN] add button to modify the participantExternalId

---
+++ admin/app/components/campaigns/participation-row.js
@@ -0,0 +1,46 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+
+export default class ParticipationRow extends Component {
+  @service notifications;
+
+  @tracked isEditionMode = false;
+  @tracked newParticipantExternalId;
+
+  constructor() {
+    super(...arguments);
+  }
+
+  _checkIfParticipantExternalIdIsNull(newParticipantExternalId) {
+    const trimedNewParticipantExternalId = newParticipantExternalId.trim();
+    return trimedNewParticipantExternalId || null;
+  }
+
+  @action
+  updateParticipantExternalId() {
+    this.isEditionMode = false;
+    this.args.participation.participantExternalId = this._checkIfParticipantExternalIdIsNull(
+      this.newParticipantExternalId
+    );
+    return this.args.updateParticipantExternalId(this.args.participation);
+  }
+
+  @action
+  cancelUpdateParticipantExternalId() {
+    this.isEditionMode = false;
+    this.newParticipantExternalId = null;
+  }
+
+  @action
+  editParticipantExternalId() {
+    this.isEditionMode = true;
+    this.newParticipantExternalId = null;
+  }
+
+  @action
+  handleChange(e) {
+    this.newParticipantExternalId = e.target.value;
+  }
+}
