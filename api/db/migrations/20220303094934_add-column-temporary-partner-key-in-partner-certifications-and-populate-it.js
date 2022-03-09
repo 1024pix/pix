c2dc4af857819b73bbc2f6c4115c43aac110ba4c
Clément Bourgoin <clement.bourgoin@pix.fr>
2022-03-09 17:55:25 +0100

    feat(api): use a single update query in migration

--- api/db/migrations/20220303094934_add-column-temporary-partner-key-in-partner-certifications-and-populate-it.js
+++ api/db/migrations/20220303094934_add-column-temporary-partner-key-in-partner-certifications-and-populate-it.js
@@ -1,4 +1,3 @@
-const bluebird = require('bluebird');
 const {
   PIX_EDU_FORMATION_INITIALE_2ND_DEGRE_INITIE,
   PIX_EDU_FORMATION_INITIALE_2ND_DEGRE_CONFIRME,
@@ -7,49 +6,32 @@ const {
   PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_EXPERT,
 } = require('../../lib/domain/models/Badge').keys;
 
+const pixEduBadges = [
+  PIX_EDU_FORMATION_INITIALE_2ND_DEGRE_INITIE,
+  PIX_EDU_FORMATION_INITIALE_2ND_DEGRE_CONFIRME,
+  PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_CONFIRME,
+  PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_AVANCE,
+  PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_EXPERT,
+]
+  .map((badge) => `'${badge}'`)
+  .join(',');
+
 exports.up = async function (knex) {
   await knex.schema.alterTable('partner-certifications', function (table) {
     table.string('partnerKey').nullable().alter();
     table.string('temporaryPartnerKey').references('badges.key').nullable();
   });
 
-  const partnerCertificationsToUpdate = await knex('partner-certifications')
-    .select('certificationCourseId', 'partnerKey')
-    .whereIn('partnerKey', [
-      PIX_EDU_FORMATION_INITIALE_2ND_DEGRE_INITIE,
-      PIX_EDU_FORMATION_INITIALE_2ND_DEGRE_CONFIRME,
-      PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_CONFIRME,
-      PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_AVANCE,
-      PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_EXPERT,
-    ]);
-
-  await bluebird.mapSeries(partnerCertificationsToUpdate, async function ({ partnerKey, certificationCourseId }) {
-    await knex('partner-certifications').where({ partnerKey, certificationCourseId }).update({
-      temporaryPartnerKey: partnerKey,
-      partnerKey: null,
-    });
-  });
+  // eslint-disable-next-line knex/avoid-injections
+  await knex.raw(
+    `UPDATE "partner-certifications" SET "temporaryPartnerKey" = "partnerKey", "partnerKey" = NULL WHERE "partnerKey" IN (${pixEduBadges})`
+  );
 };
 
 exports.down = async function (knex) {
-  const partnerCertificationsToUpdate = await knex('partner-certifications')
-    .select('certificationCourseId', 'temporaryPartnerKey')
-    .whereIn('temporaryPartnerKey', [
-      PIX_EDU_FORMATION_INITIALE_2ND_DEGRE_INITIE,
-      PIX_EDU_FORMATION_INITIALE_2ND_DEGRE_CONFIRME,
-      PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_CONFIRME,
-      PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_AVANCE,
-      PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_EXPERT,
-    ]);
-
-  await bluebird.mapSeries(
-    partnerCertificationsToUpdate,
-    async function ({ temporaryPartnerKey, certificationCourseId }) {
-      await knex('partner-certifications').where({ temporaryPartnerKey, certificationCourseId }).update({
-        partnerKey: temporaryPartnerKey,
-        temporaryPartnerKey: null,
-      });
-    }
+  // eslint-disable-next-line knex/avoid-injections
+  await knex.raw(
+    `UPDATE "partner-certifications" SET "partnerKey" = "temporaryPartnerKey" WHERE "temporaryPartnerKey" IN (${pixEduBadges})`
   );
 
   await knex.schema.alterTable('partner-certifications', function (table) {
