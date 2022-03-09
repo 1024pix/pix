94e7ceca2651d2001fa0fabd9ef1fb0305deae74
Matthias FERRAINA <matthias.ferraina@pix.fr>
2022-03-09 10:34:00 +0100

    feat(api): create pagination function

--- api/lib/domain/usecases/find-paginated-recommended-tutorials.js
+++ api/lib/domain/usecases/find-paginated-recommended-tutorials.js
@@ -1,35 +1,17 @@
+const paginate = require('../../infrastructure/utils/paginate');
+
 module.exports = async function findPaginatedRecommendedTutorials({
   userId,
   knowledgeElementRepository,
   skillRepository,
   tutorialRepository,
-  page: { size: pageSize = 10, number: pageNumber = 1 } = {},
+  page,
 }) {
   const invalidatedKnowledgeElements = await knowledgeElementRepository.findInvalidatedAndDirectByUserId(userId);
-
-  if (!invalidatedKnowledgeElements.length) {
-    return {
-      results: [],
-      pagination: {
-        page: pageNumber,
-        pageSize: pageSize,
-        rowCount: 0,
-        pageCount: 0,
-      },
-    };
-  }
 
   const skills = await skillRepository.findOperativeByIds(invalidatedKnowledgeElements.map(({ skillId }) => skillId));
 
   const tutorials = await tutorialRepository.findByRecordIds(skills.flatMap((skill) => skill.tutorialIds));
 
-  return {
-    results: tutorials.slice((pageNumber - 1) * pageSize, pageNumber * pageSize),
-    pagination: {
-      page: pageNumber,
-      pageSize: pageSize,
-      rowCount: tutorials.length,
-      pageCount: Math.ceil(tutorials.length / pageSize),
-    },
-  };
+  return paginate(tutorials, page);
 };
