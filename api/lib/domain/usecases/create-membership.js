7f08a4a0ccd39b92bc3c6af7eeafb2f8462e3b36
Andreia Pena <andreia.pena@pix.fr>
2022-03-10 11:09:33 +0100

    [api] Throw OrganizationArchived error when adding member to organization in Pix Admin

--- api/lib/domain/usecases/create-membership.js
+++ api/lib/domain/usecases/create-membership.js
@@ -1,6 +1,18 @@
 const { roles } = require('../models/Membership');
+const { OrganizationArchivedError } = require('../errors');
 
-module.exports = async function createMembership({ membershipRepository, userId, organizationId }) {
+module.exports = async function createMembership({
+  userId,
+  organizationId,
+  membershipRepository,
+  organizationRepository,
+}) {
+  const organization = await organizationRepository.get(organizationId);
+
+  if (organization.archivedAt) {
+    throw new OrganizationArchivedError();
+  }
+
   const memberships = await membershipRepository.findByOrganizationId({ organizationId });
   const organizationRole = memberships.length ? roles.MEMBER : roles.ADMIN;
 
--- api/tests/unit/domain/usecases/create-membership_test.js
+++ api/tests/unit/domain/usecases/create-membership_test.js
@@ -1,39 +1,73 @@
-const { expect, sinon } = require('../../../test-helper');
+const { expect, sinon, catchErr, domainBuilder } = require('../../../test-helper');
 const createMembership = require('../../../../lib/domain/usecases/create-membership');
 const Membership = require('../../../../lib/domain/models/Membership');
+const { OrganizationArchivedError } = require('../../../../lib/domain/errors');
 
 describe('Unit | UseCase | create-membership', function () {
   it('should insert a new membership with role ADMIN', async function () {
     // given
+    const organization = domainBuilder.buildOrganization();
     const membershipRepository = {
       create: sinon.stub(),
       findByOrganizationId: sinon.stub().resolves([]),
     };
+    const organizationRepository = {
+      get: sinon.stub().resolves(organization),
+    };
     const userId = 1;
-    const organizationId = 2;
     const role = Membership.roles.ADMIN;
 
     // when
-    await createMembership({ membershipRepository, userId, organizationId });
+    await createMembership({ userId, organizationId: organization.id, membershipRepository, organizationRepository });
 
     // then
-    expect(membershipRepository.create).to.has.been.calledWithExactly(userId, organizationId, role);
+    expect(membershipRepository.create).to.has.been.calledWithExactly(userId, organization.id, role);
   });
 
   it('should insert a new membership with role MEMBER', async function () {
     // given
+    const organization = domainBuilder.buildOrganization();
     const membershipRepository = {
       create: sinon.stub(),
       findByOrganizationId: sinon.stub().resolves([{}]),
     };
+    const organizationRepository = {
+      get: sinon.stub().resolves(organization),
+    };
     const userId = 1;
-    const organizationId = 2;
     const role = Membership.roles.MEMBER;
 
     // when
-    await createMembership({ membershipRepository, userId, organizationId });
+    await createMembership({ userId, organizationId: organization.id, membershipRepository, organizationRepository });
 
     // then
-    expect(membershipRepository.create).to.has.been.calledWithExactly(userId, organizationId, role);
+    expect(membershipRepository.create).to.has.been.calledWithExactly(userId, organization.id, role);
+  });
+
+  describe('when organization is archived', function () {
+    it('should throw an organization archived error', async function () {
+      // given
+      const userId = domainBuilder.buildUser().id;
+      const archivedOrganization = domainBuilder.buildOrganization({ archivedAt: '2022-02-02' });
+      const membershipRepository = {
+        create: sinon.stub(),
+      };
+      const organizationRepository = {
+        get: sinon.stub().resolves(archivedOrganization),
+      };
+
+      // when
+      const error = await catchErr(createMembership)({
+        userId,
+        organizationId: archivedOrganization.id,
+        membershipRepository,
+        organizationRepository,
+      });
+
+      // then
+      expect(error).to.be.instanceOf(OrganizationArchivedError);
+      expect(error.message).to.be.equal("L'organisation est archivée.");
+      expect(membershipRepository.create).to.not.have.been.called;
+    });
   });
 });
