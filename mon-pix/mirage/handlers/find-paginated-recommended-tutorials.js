9b7247accfcf6e9670856197ac7459da05d5e213
alexandrecoin <alexandre.pc.coin@gmail.com>
2022-03-10 10:46:28 +0100

    feat(mon-pix): add acceptance tests for recommended paginated tutorials

---
+++ mon-pix/mirage/handlers/find-paginated-recommended-tutorials.js
@@ -0,0 +1,20 @@
+import { getPaginationFromQueryParams, applyPagination } from './pagination-utils';
+
+export function findPaginatedRecommendedTutorials(schema, request) {
+  const queryParams = request.queryParams;
+  const tutorials = schema.tutorials.all().models;
+  const rowCount = tutorials.length;
+
+  const pagination = getPaginationFromQueryParams(queryParams);
+  const paginatedTutorials = applyPagination(tutorials, pagination);
+
+  const json = this.serialize({ modelName: 'tutorial', models: paginatedTutorials }, 'tutorial');
+
+  json.meta = {
+    page: pagination.page,
+    pageSize: pagination.pageSize,
+    rowCount,
+    pageCount: Math.ceil(rowCount / pagination.pageSize),
+  };
+  return json;
+}
