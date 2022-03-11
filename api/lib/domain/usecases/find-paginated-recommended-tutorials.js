46b910a15e09929a7d1b0f0ed18656625c78bb85
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2022-03-11 16:55:19 +0100

    docs: add JSDoc on paginated recommended tutorials

--- api/lib/domain/usecases/find-paginated-recommended-tutorials.js
+++ api/lib/domain/usecases/find-paginated-recommended-tutorials.js
@@ -1,5 +1,13 @@
 const paginate = require('../../infrastructure/utils/paginate');
+const { PageInput, Page } = require('../../infrastructure/utils/paginate');
 
+/**
+ * @param {Object} params
+ * @param {string} params.userId
+ * @param {Object} params.tutorialRepository
+ * @param {PageInput} params.page
+ * @returns {Promise<Page>}
+ */
 module.exports = async function findPaginatedRecommendedTutorials({ userId, tutorialRepository, page }) {
   const tutorials = await tutorialRepository.findRecommendedByUserId(userId);
 
--- api/lib/infrastructure/utils/paginate.js
+++ api/lib/infrastructure/utils/paginate.js
@@ -3,6 +3,31 @@ const DEFAULT_PAGINATION = {
   PAGE_SIZE: 10,
 };
 
+/**
+ * @typedef PageInput
+ * @type {object}
+ * @property {number} [number]
+ * @property {number} [size]
+ */
+
+/**
+ * @typedef Page
+ * @type {object}
+ * @property {Object[]} results
+ * @property {Object} pagination
+ * @property {number} pagination.page
+ * @property {number} pagination.pageSize
+ * @property {number} pagination.rowCount
+ * @property {number} pagination.pageCount
+ */
+
+/**
+ * @param {Object[]} data
+ * @param {Object} [config]
+ * @param {number} [config.number=DEFAULT_PAGINATION.PAGE]
+ * @param {number} [config.size=DEFAULT_PAGINATION.PAGE_SIZE]
+ * @returns {Page}
+ */
 function paginate(data, { number = DEFAULT_PAGINATION.PAGE, size = DEFAULT_PAGINATION.PAGE_SIZE } = {}) {
   const pageCount = Math.ceil(data.length / size);
   const page = Math.min(Math.max(number, 1), Math.max(pageCount, 1));
