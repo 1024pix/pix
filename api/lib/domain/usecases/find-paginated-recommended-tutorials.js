a1852f7e2426f7ea83ad4badb26e22778af7571e
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-03-15 16:33:30 +0100

    feat(api): add locale in use-case
    Co-authored-by: Matthias Ferraina <matthias.ferraina@pix.fr>

--- api/lib/domain/usecases/find-paginated-recommended-tutorials.js
+++ api/lib/domain/usecases/find-paginated-recommended-tutorials.js
@@ -1,6 +1,6 @@
 const paginateModule = require('../../infrastructure/utils/paginate');
 
-module.exports = async function findPaginatedRecommendedTutorials({ userId, tutorialRepository, page }) {
-  const tutorials = await tutorialRepository.findRecommendedByUserId(userId);
+module.exports = async function findPaginatedRecommendedTutorials({ userId, tutorialRepository, page, locale }) {
+  const tutorials = await tutorialRepository.findRecommendedByUserId({ userId, locale });
   return paginateModule.paginate(tutorials, page);
 };
