6e0b7055c580a7631c1eda77a0090c36c329d479
yaelle <ymarsiac@octo.com>
2022-03-16 12:13:51 +0100

    [APP] add user firstname in the landing page + add logout

--- mon-pix/app/controllers/campaigns/campaign-landing-page.js
+++ mon-pix/app/controllers/campaigns/campaign-landing-page.js
@@ -3,11 +3,10 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 
 export default class CampaignLandingPageController extends Controller {
-  @service campaignStorage;
   @service router;
 
   @action
   startCampaignParticipation() {
-    return this.router.transitionTo('campaigns.access', this.model.code);
+    this.router.transitionTo('campaigns.access', this.model.code);
   }
 }
