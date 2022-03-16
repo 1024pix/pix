eba7da2aedcbfccdf7ebec7f4d747317cfde6869
Andreia Pena <andreia.pena@pix.fr>
2022-03-16 11:20:31 +0100

    [api][sr] Clean usecase unit test

--- api/tests/unit/domain/usecases/send-sco-invitation_test.js
+++ api/tests/unit/domain/usecases/send-sco-invitation_test.js
@@ -1,90 +1,78 @@
-const { expect, sinon, catchErr } = require('../../../test-helper');
+const { expect, sinon, catchErr, domainBuilder } = require('../../../test-helper');
 const usecases = require('../../../../lib/domain/usecases');
-const Organization = require('../../../../lib/domain/models/Organization');
 const {
   OrganizationNotFoundError,
   OrganizationWithoutEmailError,
   ManyOrganizationsFoundError,
 } = require('../../../../lib/domain/errors');
 
-describe('Unit | UseCase | find-organizations', function () {
-  const organizationRepository = {
-    // TODO: Fix this the next time the file is edited.
-    // eslint-disable-next-line mocha/no-setup-in-describe
-    findScoOrganizationByUai: sinon.stub(),
-    // TODO: Fix this the next time the file is edited.
-    // eslint-disable-next-line mocha/no-setup-in-describe
-    get: sinon.stub(),
-  };
+describe('Unit | UseCase | send-sco-invitation', function () {
+  let organizationRepository;
 
-  it('should throw an NotFoundOrganization when UAI did not match', async function () {
-    // given
-    const uai = '1234567A';
-    const message = "L'UAI/RNE 1234567A de l'établissement n’est pas reconnu.";
-
-    organizationRepository.findScoOrganizationByUai.withArgs(uai).resolves([]);
-
-    const requestErr = await catchErr(usecases.sendScoInvitation)({
-      uai,
-      organizationRepository,
-    });
-
-    expect(requestErr).to.be.instanceOf(OrganizationNotFoundError);
-    expect(requestErr.message).to.be.equal(message);
+  beforeEach(function () {
+    organizationRepository = {
+      findScoOrganizationByUai: sinon.stub(),
+    };
   });
 
-  it('should throw an OrganizationWithoutEmailError when email is not present', async function () {
-    // given
-    const uai = '1234567A';
-    const organization = new Organization({
-      id: 2,
-      type: 'SCO',
-      name: 'organization 2',
-      externalId: '1234568',
-      email: null,
-    });
-    const message =
-      "Nous n’avons pas d’adresse e-mail de contact associée à l'établissement concernant l'UAI/RNE 1234567A.";
-    organizationRepository.findScoOrganizationByUai.withArgs(uai).resolves([organization]);
+  context('when UAI did not match', function () {
+    it('should throw an NotFoundOrganizationError', async function () {
+      // given
+      const uai = '1234567A';
+      domainBuilder.buildOrganization({ type: 'SCO', externalId: uai });
 
-    const requestErr = await catchErr(usecases.sendScoInvitation)({
-      uai,
-      organizationRepository,
-    });
+      organizationRepository.findScoOrganizationByUai.withArgs(uai).resolves([]);
 
-    expect(requestErr).to.be.instanceOf(OrganizationWithoutEmailError);
-    expect(requestErr.message).to.be.equal(message);
+      const requestErr = await catchErr(usecases.sendScoInvitation)({
+        uai,
+        organizationRepository,
+      });
+
+      expect(requestErr).to.be.instanceOf(OrganizationNotFoundError);
+      expect(requestErr.message).to.be.equal("L'UAI/RNE 1234567A de l'établissement n’est pas reconnu.");
+    });
   });
 
-  it('should throw a ManyOrganizationsFoundError when many organizations found', async function () {
-    // given
-    const uai = '1234567A';
-    const organization1 = new Organization({
-      id: 2,
-      type: 'SCO',
-      name: 'organization 2',
-      externalId: '1234568',
-      email: 'sco.generic.account@example.net',
-    });
-    const organization2 = new Organization({
-      id: 2,
-      type: 'SCO',
-      name: 'organization 2',
-      externalId: '1234568',
-      email: 'sco.generic.account@example.net',
-    });
-    const message = "Plusieurs établissements de type SCO ont été retrouvés pour L'UAI/RNE 1234567A.";
+  context('when email is not present', function () {
+    it('should throw an OrganizationWithoutEmailError', async function () {
+      // given
+      const uai = '1234567A';
+      const organization = domainBuilder.buildOrganization({ type: 'SCO', externalId: uai, email: null });
 
-    organizationRepository.findScoOrganizationByUai.withArgs(uai).resolves([organization1, organization2]);
+      organizationRepository.findScoOrganizationByUai.withArgs(uai).resolves([organization]);
 
-    // when
-    const requestErr = await catchErr(usecases.sendScoInvitation)({
-      uai,
-      organizationRepository,
+      const requestErr = await catchErr(usecases.sendScoInvitation)({
+        uai,
+        organizationRepository,
+      });
+
+      expect(requestErr).to.be.instanceOf(OrganizationWithoutEmailError);
+      expect(requestErr.message).to.be.equal(
+        "Nous n’avons pas d’adresse e-mail de contact associée à l'établissement concernant l'UAI/RNE 1234567A."
+      );
     });
+  });
 
-    // then
-    expect(requestErr).to.be.instanceOf(ManyOrganizationsFoundError);
-    expect(requestErr.message).to.be.equal(message);
+  context('when many organizations found', function () {
+    it('should throw a ManyOrganizationsFoundError', async function () {
+      // given
+      const uai = '1234567A';
+      const organization1 = domainBuilder.buildOrganization({ type: 'SCO', externalId: uai });
+      const organization2 = domainBuilder.buildOrganization({ type: 'SCO', externalId: uai });
+
+      organizationRepository.findScoOrganizationByUai.withArgs(uai).resolves([organization1, organization2]);
+
+      // when
+      const requestErr = await catchErr(usecases.sendScoInvitation)({
+        uai,
+        organizationRepository,
+      });
+
+      // then
+      expect(requestErr).to.be.instanceOf(ManyOrganizationsFoundError);
+      expect(requestErr.message).to.be.equal(
+        "Plusieurs établissements de type SCO ont été retrouvés pour L'UAI/RNE 1234567A."
+      );
+    });
   });
 });
