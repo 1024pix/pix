4abd7ad5a2c6fb1c6f4a9c04c7dffc98c830fa64
Andreia Pena <andreia.pena@pix.fr>
2022-03-17 12:16:34 +0100

    [orga] Clean error messages on join request page

--- orga/app/controllers/join-request.js
+++ orga/app/controllers/join-request.js
@@ -5,13 +5,18 @@ import get from 'lodash/get';
 
 export default class JoinRequestController extends Controller {
   @tracked isSubmit = false;
-  @tracked isOrganizationNotFound = false;
-  @tracked organizationHasNoEmail = false;
-  @tracked hasErrorMessage = false;
+  @tracked errorMessage = null;
+
+  ERROR_MESSAGES = {
+    DEFAULT: 'Une erreur est survenue.',
+    ORGANIZATION_HAS_NO_EMAIL: 'Nous n’avons pas d’adresse e-mail de contact associée à votre établissement.',
+    ORGANIZATION_NOT_FOUND: "L'UAI/RNE de l'établissement n’est pas reconnu.",
+    ORGANIZATION_ARCHIVED: "L'UAI/RNE de l'établissement n’est pas reconnu.",
+  };
 
   @action
   async createScoOrganizationInvitation(scoOrganizationInvitation) {
-    this._resetErrorState();
+    this.errorMessage = null;
     try {
       await this.store.createRecord('sco-organization-invitation', scoOrganizationInvitation).save();
       this.isSubmit = true;
@@ -26,27 +31,24 @@ export default class JoinRequestController extends Controller {
       const firstError = response.errors[0];
       this._showErrorMessages(firstError.status);
     } else {
-      this.hasErrorMessage = true;
+      this.errorMessage = this.ERROR_MESSAGES.DEFAULT;
     }
   }
 
   _showErrorMessages(status) {
     switch (status) {
       case '404':
-        this.isOrganizationNotFound = true;
+        this.errorMessage = this.ERROR_MESSAGES.ORGANIZATION_NOT_FOUND;
         break;
       case '412':
-        this.organizationHasNoEmail = true;
+        this.errorMessage = this.ERROR_MESSAGES.ORGANIZATION_HAS_NO_EMAIL;
+        break;
+      case '422':
+        this.errorMessage = this.ERROR_MESSAGES.ORGANIZATION_ARCHIVED;
         break;
       case '409':
       default:
-        this.hasErrorMessage = true;
+        this.errorMessage = this.ERROR_MESSAGES.DEFAULT;
     }
-  }
-
-  _resetErrorState() {
-    this.isOrganizationNotFound = false;
-    this.organizationHasNoEmail = false;
-    this.hasErrorMessage = false;
   }
 }
