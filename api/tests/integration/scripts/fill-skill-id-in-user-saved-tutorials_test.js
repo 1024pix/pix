9f65a94fdb56ebd68b729526c470d88bab7e6dbd
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-03-17 14:36:22 +0100

    feat(api): add getAllTutorials function
    Co-authored-by: Matthias Ferraina <matthias.ferraina@pix.fr>

--- api/tests/integration/scripts/fill-skill-id-in-user-saved-tutorials_test.js
+++ api/tests/integration/scripts/fill-skill-id-in-user-saved-tutorials_test.js
@@ -1,5 +1,8 @@
-const { expect, databaseBuilder } = require('../../test-helper');
-const { getAllUserSavedTutorialsWithoutSkillId } = require('../../../scripts/fill-skill-id-in-user-saved-tutorials');
+const { expect, databaseBuilder, learningContentBuilder, mockLearningContent } = require('../../test-helper');
+const {
+  getAllUserSavedTutorialsWithoutSkillId,
+  getAllTutorials,
+} = require('../../../scripts/fill-skill-id-in-user-saved-tutorials');
 const UserSavedTutorial = require('../../../lib/domain/models/UserSavedTutorial');
 
 describe('Integration | Scripts | fill-skillId-in-user-saved-tutorials', function () {
@@ -20,6 +23,109 @@ describe('Integration | Scripts | fill-skillId-in-user-saved-tutorials', functio
       expect(userSavedTutorials[0].tutorialId).to.equal('tuto1');
       expect(userSavedTutorials[1].tutorialId).to.equal('tuto3');
       expect(userSavedTutorials.every((userSavedTutorials) => userSavedTutorials.skillId === null)).to.be.true;
+    });
+  });
+
+  describe('#getAllTutorials', function () {
+    it('should retrieve all tutorials', async function () {
+      // given
+      const learningContentObjects = learningContentBuilder.buildLearningContent([
+        {
+          id: 'recArea1',
+          titleFrFr: 'area1_Title',
+          color: 'specialColor',
+          competences: [
+            {
+              id: 'recCompetence1',
+              name: 'Fabriquer un meuble',
+              index: '1.1',
+              tubes: [
+                {
+                  id: 'recTube1',
+                  skills: [
+                    {
+                      id: 'recSkill1',
+                      nom: '@web1',
+                      challenges: [],
+                      tutorialIds: ['tuto1', 'tuto2'],
+                      tutorials: [
+                        {
+                          id: 'tuto1',
+                          locale: 'fr-fr',
+                          duration: '00:00:54',
+                          format: 'video',
+                          link: 'http://www.example.com/this-is-an-example.html',
+                          source: 'tuto.com',
+                          title: 'tuto1',
+                        },
+                        {
+                          id: 'tuto2',
+                          locale: 'fr-fr',
+                          duration: '00:01:51',
+                          format: 'video',
+                          link: 'http://www.example.com/this-is-an-example2.html',
+                          source: 'tuto.com',
+                          title: 'tuto2',
+                        },
+                      ],
+                    },
+                    {
+                      id: 'recSkill2',
+                      nom: '@web2',
+                      challenges: [],
+                      tutorialIds: ['tuto1'],
+                      tutorials: [
+                        {
+                          id: 'tuto1',
+                          locale: 'fr-fr',
+                          duration: '00:00:54',
+                          format: 'video',
+                          link: 'http://www.example.com/this-is-an-example.html',
+                          source: 'tuto.com',
+                          title: 'tuto1',
+                        },
+                      ],
+                    },
+                    {
+                      id: 'recSkill3',
+                      nom: '@web3',
+                      challenges: [],
+                      tutorialIds: ['tuto4'],
+                      tutorials: [
+                        {
+                          id: 'tuto4',
+                          locale: 'fr-fr',
+                          duration: '00:04:38',
+                          format: 'vidéo',
+                          link: 'http://www.example.com/this-is-an-example4.html',
+                          source: 'tuto.com',
+                          title: 'tuto4',
+                        },
+                        {
+                          id: 'tuto5',
+                          locale: 'en-us',
+                          duration: '00:04:38',
+                          format: 'vidéo',
+                          link: 'http://www.example.com/this-is-an-example4.html',
+                          source: 'tuto.com',
+                          title: 'tuto4',
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ]);
+      mockLearningContent(learningContentObjects);
+
+      // when
+      const tutorials = await getAllTutorials();
+
+      // then
+      expect(tutorials).to.be.lengthOf(5);
     });
   });
 });
