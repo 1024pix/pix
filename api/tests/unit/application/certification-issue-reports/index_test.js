af30c2dc44fe061e0e97c6bd17a90e702588887f
Antoine Ceol <antoine.ceol@gmail.com>
2022-03-18 15:15:44 +0100

    :sparkles: api: Allow empty resolution message

--- api/tests/unit/application/certification-issue-reports/index_test.js
+++ api/tests/unit/application/certification-issue-reports/index_test.js
@@ -1,6 +1,7 @@
 const { expect, HttpTestServer, sinon } = require('../../../test-helper');
 const certificationIssueReportController = require('../../../../lib/application/certification-issue-reports/certification-issue-report-controller');
 
+const securityPreHandlers = require('../../../../lib/application/security-pre-handlers');
 const moduleUnderTest = require('../../../../lib/application/certification-issue-reports');
 
 describe('Unit | Application | Certifications Issue Report | Route', function () {
@@ -16,5 +17,51 @@ describe('Unit | Application | Certifications Issue Report | Route', function ()
 
     // then
     expect(response.statusCode).to.equal(200);
+  });
+
+  describe('PATCH /api/certification-issue-reports/{id}', function () {
+    context('when user is pixmaster', function () {
+      context('when no resolution is given', function () {
+        it('should return 204', async function () {
+          // given
+          sinon.stub(certificationIssueReportController, 'manuallyResolve').callsFake((_, h) => h.response().code(204));
+          sinon.stub(securityPreHandlers, 'checkUserHasRolePixMaster');
+          securityPreHandlers.checkUserHasRolePixMaster.callsFake((_, h) => h.response(true));
+
+          const httpTestServer = new HttpTestServer();
+          await httpTestServer.register(moduleUnderTest);
+          const payload = {
+            data: {
+              resolution: null,
+            },
+          };
+          // when
+          const response = await httpTestServer.request('PATCH', '/api/certification-issue-reports/1', payload);
+
+          // then
+          expect(response.statusCode).to.equal(204);
+        });
+      });
+    });
+
+    context('when user is not pixmaster', function () {
+      it('should throw 403', async function () {
+        // given
+        sinon.stub(certificationIssueReportController, 'manuallyResolve').callsFake((_, h) => h.response().code(204));
+        sinon.stub(securityPreHandlers, 'checkUserHasRolePixMaster');
+        securityPreHandlers.checkUserHasRolePixMaster.callsFake((request, h) => h.response().code(403).takeover());
+
+        const httpTestServer = new HttpTestServer();
+        await httpTestServer.register(moduleUnderTest);
+        const payload = {
+          data: {},
+        };
+        // when
+        const response = await httpTestServer.request('PATCH', '/api/certification-issue-reports/1', payload);
+
+        // then
+        expect(response.statusCode).to.equal(403);
+      });
+    });
   });
 });
