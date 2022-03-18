81588cc4a85ec66a7f97bf748d28a77b1f9db325
P-Jeremy <jemyplu@gmail.com>
2022-03-18 15:06:13 +0100

    :sparkles: feat(app): update certification focused out error message

--- mon-pix/tests/integration/components/challenge-actions_test.js
+++ mon-pix/tests/integration/components/challenge-actions_test.js
@@ -33,4 +33,56 @@ describe('Integration | Component | challenge actions', function () {
       expect(find('.challenge-actions__action-continue')).to.exist;
     });
   });
+
+  describe('when user has focused out', function () {
+    context('when assessent is of type certification', function () {
+      it("should show certification focus out's error message", async function () {
+        // given
+        this.set('isValidateButtonEnabled', true);
+        this.set('isCertification', true);
+        this.set('hasFocusedOutOfWindow', true);
+        this.set('hasChallengeTimedOut', false);
+        this.set('isSkipButtonEnabled', true);
+        this.set('validateActionStub', () => {});
+
+        // when
+        await render(hbs`<ChallengeActions
+                            @isCertification={{this.isCertification}}
+                            @validateAnswer={{this.validateActionStub}}
+                            @hasFocusedOutOfWindow={{this.hasFocusedOutOfWindow}}
+                            @hasChallengeTimedOut={{this.hasChallengeTimedOut}}
+                            @isValidateButtonEnabled={{this.isValidateButtonEnabled}}
+                            @isSkipButtonEnabled={{this.isSkipButtonEnabled}}/>`);
+
+        // then
+        expect(find('[data-test="certification-focused-out-error-message"]')).to.exist;
+        expect(find('[data-test="default-focused-out-error-message"]')).not.to.exist;
+      });
+    });
+
+    context('when assessent is not of type certification', function () {
+      it("should show default focus out's error message", async function () {
+        // given
+        this.set('isValidateButtonEnabled', true);
+        this.set('isCertification', false);
+        this.set('hasFocusedOutOfWindow', true);
+        this.set('hasChallengeTimedOut', false);
+        this.set('isSkipButtonEnabled', true);
+        this.set('validateActionStub', () => {});
+
+        // when
+        await render(hbs`<ChallengeActions
+                            @isCertification={{this.isCertification}}
+                            @validateAnswer={{this.validateActionStub}}
+                            @hasFocusedOutOfWindow={{this.hasFocusedOutOfWindow}}
+                            @hasChallengeTimedOut={{this.hasChallengeTimedOut}}
+                            @isValidateButtonEnabled={{this.isValidateButtonEnabled}}
+                            @isSkipButtonEnabled={{this.isSkipButtonEnabled}}/>`);
+
+        // then
+        expect(find('[data-test="certification-focused-out-error-message"]')).not.to.exist;
+        expect(find('[data-test="default-focused-out-error-message"]')).to.exist;
+      });
+    });
+  });
 });
