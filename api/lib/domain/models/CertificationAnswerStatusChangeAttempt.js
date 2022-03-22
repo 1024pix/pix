47d104d298259a2642bfe362047acccd294792d6
Mathieu Gilet <mathieu.gilet@zenika.com>
2022-03-22 15:59:07 +0100

    :sparkles: feat(api) add function validateAnswerByNumberIfFocusedOut() in CertificationAssessment

---
+++ api/lib/domain/models/CertificationAnswerStatusChangeAttempt.js
@@ -0,0 +1,36 @@
+module.exports = class CertificationAnswerStatusChangeAttempt {
+  constructor(questionNumber, status) {
+    this.questionNumber = questionNumber;
+    this.status = status;
+  }
+
+  static changed(questionNumber) {
+    return new CertificationAnswerStatusChangeAttempt(questionNumber, CertificationAnswerStatusChangeStatus.CHANGED);
+  }
+
+  static failed(questionNumber) {
+    return new CertificationAnswerStatusChangeAttempt(questionNumber, CertificationAnswerStatusChangeStatus.FAILURE);
+  }
+
+  static skipped(questionNumber) {
+    return new CertificationAnswerStatusChangeAttempt(questionNumber, CertificationAnswerStatusChangeStatus.SKIPPED);
+  }
+
+  hasFailed() {
+    return this.status === CertificationAnswerStatusChangeStatus.FAILURE;
+  }
+
+  hasSucceeded() {
+    return this.status === CertificationAnswerStatusChangeStatus.CHANGED;
+  }
+
+  wasSkipped() {
+    return this.status === CertificationAnswerStatusChangeStatus.SKIPPED;
+  }
+};
+
+const CertificationAnswerStatusChangeStatus = {
+  CHANGED: 'CHANGED',
+  FAILURE: 'FAILURE',
+  SKIPPED: 'SKIPPED',
+};
