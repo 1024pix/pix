50684f2725b5b36c7731a1b88e32ed44a2c8489a
Jonathan Perret <j-github@jonathanperret.net>
2022-03-24 16:50:43 +0100

    TemporaryStorage: add withPrefix method

--- api/tests/unit/infrastructure/temporary-storage/TemporaryStorage_test.js
+++ api/tests/unit/infrastructure/temporary-storage/TemporaryStorage_test.js
@@ -50,4 +50,36 @@ describe('Unit | Infrastructure | temporary-storage | TemporaryStorage', functio
       expect(result).to.be.ok;
     });
   });
+
+  describe('#withPrefix', function () {
+    it('should return a wrapper that adds a prefix to all methods', async function () {
+      // given
+      const store = {};
+      class TestStorage extends TemporaryStorage {
+        async save({ key, value }) {
+          store[key] = value;
+        }
+        async get(key) {
+          return store[key];
+        }
+        async delete(key) {
+          delete store[key];
+        }
+      }
+      const storage = new TestStorage().withPrefix('a-prefix:');
+
+      // when & then
+      expect(await storage.save({ key: 'a-key', value: 'a-value' })).to.equal('a-key');
+      expect(store).to.deep.equal({ 'a-prefix:a-key': 'a-value' });
+
+      expect(await storage.get('a-key')).to.equal('a-value');
+      await storage.delete('a-key');
+      expect(await storage.get('a-key')).to.be.undefined;
+
+      const randomKey = await storage.save({ value: 'random-key-value' });
+      expect(randomKey).to.exist;
+      expect(store['a-prefix:' + randomKey]).to.exist;
+      expect(await storage.get(randomKey)).to.equal('random-key-value');
+    });
+  });
 });
