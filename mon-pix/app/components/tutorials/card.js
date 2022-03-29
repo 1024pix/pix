a69c0719863b1a2f746893df4d9544e2683576c5
alexandrecoin <alexandre.pc.coin@gmail.com>
2022-03-29 16:56:01 +0200

    feat(mon-pix): replicate tutorial evaluation on new tutos page

--- mon-pix/app/components/tutorials/card.js
+++ mon-pix/app/components/tutorials/card.js
@@ -1,12 +1,15 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 import buttonStatusTypes from 'mon-pix/utils/button-status-types';
 import { inject as service } from '@ember/service';
 
 export default class Card extends Component {
   @service intl;
+  @service store;
 
   @tracked savingStatus = buttonStatusTypes.unrecorded;
+  @tracked evaluationStatus = buttonStatusTypes.unrecorded;
 
   constructor(owner, args) {
     super(owner, args);
@@ -18,5 +21,21 @@ export default class Card extends Component {
     return this.savingStatus === buttonStatusTypes.recorded
       ? this.intl.t('pages.user-tutorials.list.tutorial.actions.remove.label')
       : this.intl.t('pages.user-tutorials.list.tutorial.actions.save.label');
+  }
+
+  get isEvaluateButtonDisabled() {
+    return this.evaluationStatus !== buttonStatusTypes.unrecorded;
+  }
+
+  @action
+  async evaluateTutorial() {
+    this.evaluationStatus = buttonStatusTypes.pending;
+    const tutorialEvaluation = this.store.createRecord('tutorialEvaluation', { tutorial: this.args.tutorial });
+    try {
+      await tutorialEvaluation.save({ adapterOptions: { tutorialId: this.args.tutorial.id } });
+      this.evaluationStatus = buttonStatusTypes.recorded;
+    } catch (e) {
+      this.evaluationStatus = buttonStatusTypes.unrecorded;
+    }
   }
 }
