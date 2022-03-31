19c079c4d7be14d07efb9d5330c32d7d92b909d2
Andreia Pena <andreia.pena@pix.fr>
2022-03-31 19:57:31 +0200

    [admin] Delete data-test in tools page

--- admin/tests/acceptance/tools_test.js
+++ admin/tests/acceptance/tools_test.js
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
-import { currentURL, visit } from '@ember/test-helpers';
+import { currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
-import { fillByLabel, clickByName, visit as visitScreen } from '@1024pix/ember-testing-library';
+import { fillByLabel, clickByName, visit } from '@1024pix/ember-testing-library';
 import { createAuthenticateSession } from 'pix-admin/tests/helpers/test-init';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
@@ -27,20 +27,31 @@ module('Acceptance | tools', function (hooks) {
   });
 
   module('Rendering', function () {
-    test('Should content "Learning content" section', async function (assert) {
+    test('Should display "Learning content" information', async function (assert) {
       // given & when
-      await visit('/tools');
+      const screen = await visit('/tools');
 
       // then
-      assert.dom('section[data-test-id="learning-content"]').exists();
-      assert.dom('button').exists();
+      assert.dom(screen.getByRole('heading', { name: 'Référentiel' })).exists();
+      assert
+        .dom(
+          screen.getByText(
+            'Une version du référentiel de données pédagogique est créée quotidiennement (vers 4h00) et le référentiel ' +
+              "utilisé par l'application est mis à jour (vers 6h00)."
+          )
+        )
+        .exists();
+      assert.dom(screen.getByRole('button', { name: 'Recharger le cache' })).exists();
+      assert
+        .dom(screen.getByRole('button', { name: 'Créer une nouvelle version du référentiel et recharger le cache' }))
+        .exists();
     });
   });
 
   module('Refresh cache content', function () {
     test('it request the cache refresh', async function (assert) {
       // given
-      const screen = await visitScreen('/tools');
+      const screen = await visit('/tools');
 
       // when
       await clickByName('Recharger le cache');
@@ -53,7 +64,7 @@ module('Acceptance | tools', function (hooks) {
   module('Create release and refresh cache content', function () {
     test('it request the release creation and refresh cache', async function (assert) {
       // given
-      const screen = await visitScreen('/tools');
+      const screen = await visit('/tools');
 
       // when
       await clickByName('Créer une nouvelle version du référentiel et recharger le cache');
@@ -71,7 +82,7 @@ module('Acceptance | tools', function (hooks) {
 
   test('it should be possible to create a new tag', async function (assert) {
     // given
-    const screen = await visitScreen('/tools');
+    const screen = await visit('/tools');
 
     // when
     await fillByLabel('Nom du tag', 'Mon super tag');
