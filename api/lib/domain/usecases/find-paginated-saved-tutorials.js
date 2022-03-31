809af3c7d96142a52fdbc087e68ecba894e3eb11
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-03-31 16:40:23 +0200

    sr(api): remove find evaluation tutorial in findPaginatedSavedTutorials use-case
    Co-authored-by: Alexandre Coin <alexandre.coin@pix.fr>
    Co-authored-by: Yann Bertrand <yann.bertrand@pix.fr>
    Co-authored-by: Corentin Rousseau <corentin.rousseau@pix.fr>

--- api/lib/domain/usecases/find-paginated-saved-tutorials.js
+++ api/lib/domain/usecases/find-paginated-saved-tutorials.js
@@ -1,30 +1,10 @@
-module.exports = async function findPaginatedSavedTutorials({
-  tutorialEvaluationRepository,
-  tutorialRepository,
-  userId,
-  page,
-} = {}) {
-  const tutorialEvaluations = await tutorialEvaluationRepository.find({ userId });
-  const { models: tutorials, meta } = await tutorialRepository.findPaginatedForCurrentUser({
+module.exports = async function findPaginatedSavedTutorials({ tutorialRepository, userId, page } = {}) {
+  const { models: results, meta } = await tutorialRepository.findPaginatedForCurrentUser({
     userId,
     page,
   });
-  const savedTutorials = tutorials.map(_setTutorialEvaluation(tutorialEvaluations));
   return {
-    results: savedTutorials,
+    results,
     meta,
   };
 };
-
-function _setTutorialEvaluation(tutorialEvaluations) {
-  return (tutorial) => {
-    const tutorialEvaluation = tutorialEvaluations.find(
-      (tutorialEvaluation) => tutorialEvaluation.tutorialId === tutorial.id
-    );
-    if (!tutorialEvaluation) return tutorial;
-    return {
-      ...tutorial,
-      tutorialEvaluation,
-    };
-  };
-}
