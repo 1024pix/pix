63d8081d6bf8b2c04fa5d7c859c9195e838387b0
Karam Mohd <k.haytham.mohammed@octo.com>
2022-04-03 19:32:59 +0200

    [API] Add deletion attributes to serializer

--- api/lib/infrastructure/serializers/jsonapi/participation-for-campaign-management-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/participation-for-campaign-management-serializer.js
@@ -3,7 +3,17 @@ const { Serializer } = require('jsonapi-serializer');
 module.exports = {
   serialize(participationsForCampaignManagement, meta) {
     return new Serializer('campaign-participation', {
-      attributes: ['lastName', 'firstName', 'participantExternalId', 'status', 'createdAt', 'sharedAt'],
+      attributes: [
+        'lastName',
+        'firstName',
+        'participantExternalId',
+        'status',
+        'createdAt',
+        'sharedAt',
+        'deletedAt',
+        'deletedBy',
+        'deletedByFullName',
+      ],
       meta,
     }).serialize(participationsForCampaignManagement);
   },
