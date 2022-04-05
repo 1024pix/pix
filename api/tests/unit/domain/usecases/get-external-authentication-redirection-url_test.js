c72a760087f4d029987bc83421d0213582758283
Clément Bourgoin <clement.bourgoin@pix.fr>
2022-04-05 17:49:58 +0200

    feat(api): save user first and last name upon GAR login and update modification date
    Co-authored-by: Andreia Pena <58915422+AndreiaPena@users.noreply.github.com>

--- api/tests/unit/domain/usecases/get-external-authentication-redirection-url_test.js
+++ api/tests/unit/domain/usecases/get-external-authentication-redirection-url_test.js
@@ -1,9 +1,11 @@
 const { expect, sinon, domainBuilder } = require('../../../test-helper');
 const User = require('../../../../lib/domain/models/User');
+const AuthenticationMethod = require('../../../../lib/domain/models/AuthenticationMethod');
 const getExternalAuthenticationRedirectionUrl = require('../../../../lib/domain/usecases/get-external-authentication-redirection-url');
 
 describe('Unit | UseCase | get-external-authentication-redirection-url', function () {
   let userRepository;
+  let authenticationMethodRepository;
   let tokenService;
   let samlSettings;
 
@@ -11,6 +13,11 @@ describe('Unit | UseCase | get-external-authentication-redirection-url', functio
     userRepository = {
       getBySamlId: sinon.stub(),
       updateLastLoggedAt: sinon.stub(),
+    };
+
+    authenticationMethodRepository = {
+      findOneByUserIdAndIdentityProvider: sinon.stub(),
+      update: sinon.stub(),
     };
 
     tokenService = {
@@ -55,6 +62,17 @@ describe('Unit | UseCase | get-external-authentication-redirection-url', functio
   });
 
   context('when user already exists in database', function () {
+    let clock;
+    const now = new Date('2022-03-13');
+
+    beforeEach(function () {
+      clock = sinon.useFakeTimers(now);
+    });
+
+    afterEach(function () {
+      clock.restore();
+    });
+
     it('should return access token url', async function () {
       // given
       const userAttributes = {
@@ -70,12 +88,16 @@ describe('Unit | UseCase | get-external-authentication-redirection-url', functio
       });
 
       userRepository.getBySamlId.withArgs('saml-id-for-adele').resolves(expectedUser);
+      authenticationMethodRepository.findOneByUserIdAndIdentityProvider.resolves(
+        domainBuilder.buildAuthenticationMethod.withGarAsIdentityProvider()
+      );
       tokenService.createAccessTokenForSaml.returns('access-token');
 
       // when
       const result = await getExternalAuthenticationRedirectionUrl({
         userAttributes,
         userRepository,
+        authenticationMethodRepository,
         tokenService,
         settings: samlSettings,
       });
@@ -95,12 +117,16 @@ describe('Unit | UseCase | get-external-authentication-redirection-url', functio
       const user = domainBuilder.buildUser({ id: 777 });
 
       userRepository.getBySamlId.resolves(user);
+      authenticationMethodRepository.findOneByUserIdAndIdentityProvider.resolves(
+        domainBuilder.buildAuthenticationMethod.withGarAsIdentityProvider()
+      );
       tokenService.createIdTokenForUserReconciliation.returns('external-user-token');
 
       // when
       await getExternalAuthenticationRedirectionUrl({
         userAttributes,
         userRepository,
+        authenticationMethodRepository,
         tokenService,
         settings: samlSettings,
       });
@@ -108,5 +134,153 @@ describe('Unit | UseCase | get-external-authentication-redirection-url', functio
       // then
       expect(userRepository.updateLastLoggedAt).to.have.been.calledWith({ userId: 777 });
     });
+
+    context("when user's authentication method does not contain first and last name", function () {
+      it('should save first and last name as the authentication complement and the authentication method modification date', async function () {
+        // given
+        const user = domainBuilder.buildUser();
+        const authenticationMethodWithoutFirstAndLastName = new AuthenticationMethod({
+          id: 1234,
+          userId: user.id,
+          identityProvider: AuthenticationMethod.identityProviders.GAR,
+          authenticationComplement: null,
+          externalIdentifier: 'saml-id',
+          createdAt: new Date('2020-01-01'),
+          updatedAt: new Date('2020-02-01'),
+        });
+        userRepository.getBySamlId.withArgs('saml-id').resolves(user);
+        authenticationMethodRepository.findOneByUserIdAndIdentityProvider
+          .withArgs({ userId: user.id, identityProvider: AuthenticationMethod.identityProviders.GAR })
+          .resolves(authenticationMethodWithoutFirstAndLastName);
+
+        // when
+        await getExternalAuthenticationRedirectionUrl({
+          userAttributes: { IDO: 'saml-id', NOM: 'Lisitsa', PRE: 'Vassili' },
+          userRepository,
+          authenticationMethodRepository,
+          tokenService,
+          settings: samlSettings,
+        });
+
+        // then
+        const expectedAuthenticationMethod = domainBuilder.buildAuthenticationMethod.withGarAsIdentityProvider({
+          id: authenticationMethodWithoutFirstAndLastName.id,
+          userId: user.id,
+          identityProvider: AuthenticationMethod.identityProviders.GAR,
+          userFirstName: 'Vassili',
+          userLastName: 'Lisitsa',
+          externalIdentifier: 'saml-id',
+        });
+        expect(authenticationMethodRepository.update).to.have.been.calledWith(expectedAuthenticationMethod);
+      });
+    });
+
+    context("when user's authentication method contains a different first name", function () {
+      it('should update first and last name in the authentication complement and the authentication method modification date', async function () {
+        // given
+        const { user, authenticationMethod } = _buildUserWithAuthenticationMethod({
+          firstName: 'Vassili',
+          lastName: 'Lisitsa',
+          externalIdentifier: 'saml-id',
+        });
+        userRepository.getBySamlId.withArgs('saml-id').resolves(user);
+        authenticationMethodRepository.findOneByUserIdAndIdentityProvider
+          .withArgs({ userId: user.id, identityProvider: AuthenticationMethod.identityProviders.GAR })
+          .resolves(authenticationMethod);
+
+        // when
+        await getExternalAuthenticationRedirectionUrl({
+          userAttributes: { IDO: 'saml-id', NOM: 'Lisitsa', PRE: 'Valentina' },
+          userRepository,
+          authenticationMethodRepository,
+          tokenService,
+          settings: samlSettings,
+        });
+
+        // then
+        const expectedAuthenticationMethod = domainBuilder.buildAuthenticationMethod.withGarAsIdentityProvider({
+          id: authenticationMethod.id,
+          userId: user.id,
+          identityProvider: AuthenticationMethod.identityProviders.GAR,
+          userFirstName: 'Valentina',
+          userLastName: 'Lisitsa',
+          externalIdentifier: 'saml-id',
+        });
+        expect(authenticationMethodRepository.update).to.have.been.calledWith(expectedAuthenticationMethod);
+      });
+    });
+
+    context("when user's authentication method contains a different last name", function () {
+      it('should update first and last name in the authentication complement and the authentication method modification date', async function () {
+        // given
+        const { user, authenticationMethod } = _buildUserWithAuthenticationMethod({
+          firstName: 'Valentina',
+          lastName: 'Lisitsa',
+          externalIdentifier: 'saml-id',
+        });
+        userRepository.getBySamlId.withArgs('saml-id').resolves(user);
+        authenticationMethodRepository.findOneByUserIdAndIdentityProvider
+          .withArgs({ userId: user.id, identityProvider: AuthenticationMethod.identityProviders.GAR })
+          .resolves(authenticationMethod);
+
+        // when
+        await getExternalAuthenticationRedirectionUrl({
+          userAttributes: { IDO: 'saml-id', NOM: 'Volk', PRE: 'Valentina' },
+          userRepository,
+          authenticationMethodRepository,
+          tokenService,
+          settings: samlSettings,
+        });
+
+        // then
+        const expectedAuthenticationMethod = domainBuilder.buildAuthenticationMethod.withGarAsIdentityProvider({
+          id: authenticationMethod.id,
+          userId: user.id,
+          identityProvider: AuthenticationMethod.identityProviders.GAR,
+          userFirstName: 'Valentina',
+          userLastName: 'Volk',
+          externalIdentifier: 'saml-id',
+        });
+        expect(authenticationMethodRepository.update).to.have.been.calledWith(expectedAuthenticationMethod);
+      });
+    });
+
+    context("when user's authentication method contains the same first and last name", function () {
+      it('should not update first and last name in the authentication complement', async function () {
+        // given
+        const { user, authenticationMethod } = _buildUserWithAuthenticationMethod({
+          firstName: 'Valentina',
+          lastName: 'Volk',
+          externalIdentifier: 'saml-id',
+        });
+        userRepository.getBySamlId.withArgs('saml-id').resolves(user);
+        authenticationMethodRepository.findOneByUserIdAndIdentityProvider
+          .withArgs({ userId: user.id, identityProvider: AuthenticationMethod.identityProviders.GAR })
+          .resolves(authenticationMethod);
+
+        // when
+        await getExternalAuthenticationRedirectionUrl({
+          userAttributes: { IDO: 'saml-id', NOM: 'Volk', PRE: 'Valentina' },
+          userRepository,
+          authenticationMethodRepository,
+          tokenService,
+          settings: samlSettings,
+        });
+
+        // then
+        expect(authenticationMethodRepository.update).to.not.have.been.called;
+      });
+    });
   });
 });
+
+function _buildUserWithAuthenticationMethod({ firstName, lastName, externalIdentifier }) {
+  const user = domainBuilder.buildUser();
+  const authenticationMethod = domainBuilder.buildAuthenticationMethod.withGarAsIdentityProvider({
+    userId: user.id,
+    userFirstName: firstName,
+    userLastName: lastName,
+    externalIdentifier: externalIdentifier,
+  });
+  return { user, authenticationMethod };
+}
