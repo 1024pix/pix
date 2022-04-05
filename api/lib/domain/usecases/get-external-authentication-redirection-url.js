0b7009fef18472d1c1b2856deb01ae662d7613e9
Clément Bourgoin <clement.bourgoin@pix.fr>
2022-04-05 17:49:58 +0200

    feat(api): save user first and last name upon GAR login and update modification date
    Co-authored-by: Andreia Pena <58915422+AndreiaPena@users.noreply.github.com>

--- api/lib/domain/usecases/get-external-authentication-redirection-url.js
+++ api/lib/domain/usecases/get-external-authentication-redirection-url.js
@@ -1,6 +1,9 @@
+const AuthenticationMethod = require('../models/AuthenticationMethod');
+
 module.exports = async function getExternalAuthenticationRedirectionUrl({
   userAttributes,
   userRepository,
+  authenticationMethodRepository,
   tokenService,
   settings,
 }) {
@@ -12,17 +15,61 @@ module.exports = async function getExternalAuthenticationRedirectionUrl({
   };
 
   const user = await userRepository.getBySamlId(externalUser.samlId);
+
   if (user) {
-    return await _getUrlWithAccessToken({ user, tokenService, userRepository });
+    return await _getUrlWithAccessToken({
+      user,
+      externalUser,
+      tokenService,
+      userRepository,
+      authenticationMethodRepository,
+    });
   }
 
   return _getUrlForReconciliationPage({ tokenService, externalUser });
 };
 
-async function _getUrlWithAccessToken({ user, tokenService, userRepository }) {
+async function _getUrlWithAccessToken({
+  user,
+  externalUser,
+  tokenService,
+  userRepository,
+  authenticationMethodRepository,
+}) {
   const token = tokenService.createAccessTokenForSaml(user.id);
   await userRepository.updateLastLoggedAt({ userId: user.id });
+  await _saveUserFirstAndLastName({ authenticationMethodRepository, user, externalUser });
   return `/?token=${encodeURIComponent(token)}&user-id=${user.id}`;
+}
+
+async function _saveUserFirstAndLastName({ authenticationMethodRepository, user, externalUser }) {
+  const authenticationMethod = await authenticationMethodRepository.findOneByUserIdAndIdentityProvider({
+    userId: user.id,
+    identityProvider: AuthenticationMethod.identityProviders.GAR,
+  });
+
+  if (
+    _externalUserFirstAndLastNameMatchesAuthenticationMethodFirstAndLastName({ authenticationMethod, externalUser })
+  ) {
+    return;
+  }
+
+  authenticationMethod.authenticationComplement = new AuthenticationMethod.GARAuthenticationComplement({
+    firstName: externalUser.firstName,
+    lastName: externalUser.lastName,
+  });
+
+  authenticationMethodRepository.update(authenticationMethod);
+}
+
+function _externalUserFirstAndLastNameMatchesAuthenticationMethodFirstAndLastName({
+  authenticationMethod,
+  externalUser,
+}) {
+  return (
+    externalUser.firstName === authenticationMethod.authenticationComplement?.firstName &&
+    externalUser.lastName === authenticationMethod.authenticationComplement?.lastName
+  );
 }
 
 function _getUrlForReconciliationPage({ tokenService, externalUser }) {
