f1918730900b0dbc95614d12082a4f73a280e45a
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-04-07 17:11:39 +0200

    tech(mon-pix): refresh Model instead of using tutorial.unloadRecord()

--- mon-pix/app/controllers/user-tutorials-v2/saved.js
+++ mon-pix/app/controllers/user-tutorials-v2/saved.js
@@ -1,4 +1,5 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
 export default class SavedController extends Controller {
   pageOptions = [
@@ -8,7 +9,8 @@ export default class SavedController extends Controller {
     { label: '100', value: 100 },
   ];
 
-  unloadRecord(tutorial) {
-    return tutorial.unloadRecord();
+  @action
+  refresh() {
+    this.send('refreshModel');
   }
 }
