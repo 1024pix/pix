ebd94b62cce3cf5e18f154695da3f735d6b5aaae
Xavier Carron <xavier.carron@zenika.com>
2022-04-08 14:24:09 +0200

    [ORGA] redirect to campaign list when participation is unavailable

--- orga/app/routes/authenticated/campaigns/participant-profile.js
+++ orga/app/routes/authenticated/campaigns/participant-profile.js
@@ -1,12 +1,18 @@
 import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
-
+import { inject as service } from '@ember/service';
 export default class ProfileRoute extends Route {
-  model(params) {
-    const { campaign_id: campaignId, campaign_participation_id: campaignParticipationId } = params;
-    return RSVP.hash({
-      campaign: this.store.findRecord('campaign', campaignId),
-      campaignProfile: this.store.queryRecord('campaign-profile', { campaignId, campaignParticipationId }),
-    });
+  @service router;
+
+  async model(params) {
+    try {
+      const { campaign_id: campaignId, campaign_participation_id: campaignParticipationId } = params;
+      return await RSVP.hash({
+        campaign: this.store.findRecord('campaign', campaignId),
+        campaignProfile: this.store.queryRecord('campaign-profile', { campaignId, campaignParticipationId }),
+      });
+    } catch (error) {
+      this.send('error', error, this.router.replaceWith('not-found', params.campaign_id));
+    }
   }
 }
