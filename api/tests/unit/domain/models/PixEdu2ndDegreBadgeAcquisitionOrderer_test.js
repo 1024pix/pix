f1b04c52edafc511e478cc10bc25f4b23ea7e418
Mathieu Gilet <mathieu.gilet@zenika.com>
2022-04-11 11:42:09 +0200

    :recycle: api: use deepEqualInstance rather than deep.equal

--- api/tests/unit/domain/models/PixEdu2ndDegreBadgeAcquisitionOrderer_test.js
+++ api/tests/unit/domain/models/PixEdu2ndDegreBadgeAcquisitionOrderer_test.js
@@ -36,7 +36,7 @@ describe('Unit | Domain | Models | PixEdu2ndDegreBadgeAcquisitionOrderer', funct
         const highestBadge = pixEdu2ndDegreBadgeAcquisitionOrderer.getHighestBadge();
 
         // then
-        expect(highestBadge).to.deep.equal(
+        expect(highestBadge).to.deepEqualInstance(
           domainBuilder.buildBadgeAcquisition.forPixEduFormationContinue2ndDegreExpert()
         );
       });
@@ -60,7 +60,7 @@ describe('Unit | Domain | Models | PixEdu2ndDegreBadgeAcquisitionOrderer', funct
           const highestBadge = pixEdu2ndDegreBadgeAcquisitionOrderer.getHighestBadge();
 
           // then
-          expect(highestBadge).to.deep.equal(
+          expect(highestBadge).to.deepEqualInstance(
             domainBuilder.buildBadgeAcquisition.forPixEduFormationContinue2ndDegreAvance()
           );
         });
@@ -83,7 +83,7 @@ describe('Unit | Domain | Models | PixEdu2ndDegreBadgeAcquisitionOrderer', funct
             const highestBadge = pixEdu2ndDegreBadgeAcquisitionOrderer.getHighestBadge();
 
             // then
-            expect(highestBadge).to.deep.equal(
+            expect(highestBadge).to.deepEqualInstance(
               domainBuilder.buildBadgeAcquisition.forPixEduFormationContinue2ndDegreConfirme()
             );
           });
@@ -105,7 +105,7 @@ describe('Unit | Domain | Models | PixEdu2ndDegreBadgeAcquisitionOrderer', funct
               const highestBadge = pixEdu2ndDegreBadgeAcquisitionOrderer.getHighestBadge();
 
               // then
-              expect(highestBadge).to.deep.equal(
+              expect(highestBadge).to.deepEqualInstance(
                 domainBuilder.buildBadgeAcquisition.forPixEduFormationInitiale2ndDegreConfirme()
               );
             });
@@ -125,7 +125,7 @@ describe('Unit | Domain | Models | PixEdu2ndDegreBadgeAcquisitionOrderer', funct
                 const highestBadge = pixEdu2ndDegreBadgeAcquisitionOrderer.getHighestBadge();
 
                 // then
-                expect(highestBadge).to.deep.equal(
+                expect(highestBadge).to.deepEqualInstance(
                   domainBuilder.buildBadgeAcquisition.forPixEduFormationInitiale2ndDegreInitie()
                 );
               });
