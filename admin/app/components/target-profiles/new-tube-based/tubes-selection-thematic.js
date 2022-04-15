8eef27454512e59342b71e688e0d7275bfbb045a
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2022-04-15 16:41:33 +0200

    Manage tubes selection with a reactive state

---
+++ admin/app/components/target-profiles/new-tube-based/tubes-selection-thematic.js
@@ -0,0 +1,48 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+export default class TubesSelectionThematic extends Component {
+  get state() {
+    const checked = this.args.thematic.tubes.every((tube) => this.isTubeSelected(tube));
+    if (checked) return 'checked';
+
+    const indeterminate = this.args.thematic.tubes.any((tube) => this.isTubeSelected(tube));
+    if (indeterminate) return 'indeterminate';
+
+    return 'unchecked';
+  }
+
+  isTubeSelected(tube) {
+    return this.args.tubesSelected.some((selectedTube) => selectedTube.id === tube.id);
+  }
+
+  @action
+  onChange(event) {
+    if (event.target.checked) {
+      this.check();
+    } else {
+      this.uncheck();
+    }
+  }
+
+  @action
+  toggle() {
+    if (this.state === 'checked') {
+      this.uncheck();
+    } else {
+      this.check();
+    }
+  }
+
+  check() {
+    this.args.thematic.tubes.forEach((tube) => {
+      this.args.checkTube(tube);
+    });
+  }
+
+  uncheck() {
+    this.args.thematic.tubes.forEach((tube) => {
+      this.args.uncheckTube(tube);
+    });
+  }
+}
