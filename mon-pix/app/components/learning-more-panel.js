493c2599a24bdb512e355bb8bb5c8623f7098ca2
alexandrecoin <alexandre.pc.coin@gmail.com>
2022-04-15 16:52:50 +0200

    feat(mon-pix): add tutorial card on checkpoint page's learning more panel

--- mon-pix/app/components/learning-more-panel.js
+++ mon-pix/app/components/learning-more-panel.js
@@ -1,8 +1,15 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
 
 export default class LearningMorePanel extends Component {
+  @service featureToggles;
+
   get hasLearningMoreItems() {
     const learningMoreTutorials = this.args.learningMoreTutorials || [];
     return learningMoreTutorials.length > 0;
+  }
+
+  get areNewTutorialsEnabled() {
+    return this.featureToggles.featureToggles.isNewTutorialsPageEnabled;
   }
 }
