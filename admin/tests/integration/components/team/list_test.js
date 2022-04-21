78656013c7739b88885fac3e8f051053f11db39b
yschoueri <yasmina.schoueri@gmail.com>
2022-04-21 16:20:57 +0200

    feat(admin): display list of team members

---
+++ admin/tests/integration/components/team/list_test.js
@@ -0,0 +1,40 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { hbs } from 'ember-cli-htmlbars';
+import { render } from '@1024pix/ember-testing-library';
+
+module('Integration | Component | team | list', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('should display the list of members', async function (assert) {
+    // given
+    this.set('members', [
+      {
+        firstName: 'Marie',
+        lastName: 'Tim',
+        email: 'marie.tim@example.net',
+        role: 'SUPER_ADMIN',
+      },
+    ]);
+
+    // when
+    const screen = await render(hbs`<Team::List @members={{this.members}}/>`);
+
+    // then
+    assert.dom(screen.getByText('Marie')).exists();
+    assert.dom(screen.getByText('Tim')).exists();
+    assert.dom(screen.getByText('marie.tim@example.net')).exists();
+    assert.dom(screen.getByText('SUPER_ADMIN')).exists();
+  });
+
+  test('should display no results in table', async function (assert) {
+    // given
+    this.set('members', []);
+
+    // when
+    const screen = await render(hbs`<Team::List @members={{this.members}}/>`);
+
+    // then
+    assert.dom(screen.getByText('Aucun résultat')).exists();
+  });
+});
