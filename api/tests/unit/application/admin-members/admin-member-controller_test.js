1cf5cba0ada0fba9965d57f96a2dfccd57ee93db
Anne-G <ang.schall@gmail.com>
2022-04-22 15:52:53 +0200

    api: add update admin role in controller

--- api/tests/unit/application/admin-members/admin-member-controller_test.js
+++ api/tests/unit/application/admin-members/admin-member-controller_test.js
@@ -3,6 +3,7 @@ const { expect, sinon, domainBuilder } = require('../../../test-helper');
 const adminMemberController = require('../../../../lib/application/admin-members/admin-member-controller');
 const usecases = require('../../../../lib/domain/usecases');
 const adminMemberSerializer = require('../../../../lib/infrastructure/serializers/jsonapi/admin-member-serializer');
+const { ROLES } = require('../../../../lib/domain/constants').PIX_ADMIN;
 
 describe('Unit | Controller | admin-member-controller', function () {
   describe('#findAll', function () {
@@ -20,6 +21,29 @@ describe('Unit | Controller | admin-member-controller', function () {
       // then
       expect(usecases.getAdminMembers).to.have.been.calledOnce;
       expect(result).to.equal(serializedMembers);
+    });
+  });
+
+  describe('#updateAdminMember', function () {
+    it('should return the serialized admin member with updated values', async function () {
+      // given
+      const id = 7;
+      const role = ROLES.SUPPORT;
+
+      const updatedMember = Symbol('updatedMember');
+      sinon.stub(usecases, 'updateAdminMember').withArgs({ id, role }).resolves(updatedMember);
+
+      const serializedUpdatedMember = Symbol('serializedUpdatedMember');
+      sinon.stub(adminMemberSerializer, 'serialize').withArgs(updatedMember).returns(serializedUpdatedMember);
+
+      // when
+      const result = await adminMemberController.updateAdminMember({
+        params: { id },
+        payload: { data: { attributes: { role: ROLES.SUPPORT } } },
+      });
+
+      // then
+      expect(result).to.equal(serializedUpdatedMember);
     });
   });
 });
