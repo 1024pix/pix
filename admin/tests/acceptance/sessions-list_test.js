ad5933f2edc1b65386531fbbc8a6e092388c9dbf
Andreia Pena <andreia.pena@pix.fr>
2022-04-22 13:17:27 +0200

    [admin] Use TL methods instead of class and add aria-label in list-items

--- admin/tests/acceptance/sessions-list_test.js
+++ admin/tests/acceptance/sessions-list_test.js
@@ -61,11 +61,14 @@ module('Acceptance | Session List', function (hooks) {
       module('Default display', function () {
         test('it should display the first page of finalized sessions', async function (assert) {
           // when
-          await visit('/sessions/list');
+          const screen = await visit('/sessions/list');
 
           // then
           assert.dom('select#pageSize').hasValue('10');
-          assert.dom('.table-admin tbody tr').exists({ count: 10 });
+          const sessionCount = screen.getAllByLabelText('Informations de la session de certification', {
+            exact: false,
+          }).length;
+          assert.strictEqual(sessionCount, 10);
           assert.dom('div.page-navigation__current-page').hasText('1');
         });
       });
@@ -78,7 +81,10 @@ module('Acceptance | Session List', function (hooks) {
 
           // then
           assert.dom('select#pageSize').hasValue('10');
-          assert.dom('.table-admin tbody tr').exists({ count: 10 });
+          const sessionCount = screen.getAllByLabelText('Informations de la session de certification', {
+            exact: false,
+          }).length;
+          assert.strictEqual(sessionCount, 10);
           assert.dom('div.page-navigation__current-page').hasText('2');
         });
       });
@@ -86,12 +92,15 @@ module('Acceptance | Session List', function (hooks) {
       module('when selecting a different pageSize', function () {
         test('it should display all the finalized sessions', async function (assert) {
           // when
-          await visit('/sessions/list');
+          const screen = await visit('/sessions/list');
           await fillIn('select#pageSize', '25');
 
           // then
           assert.dom('select#pageSize').hasValue('25');
-          assert.dom('.table-admin tbody tr').exists({ count: 25 });
+          const sessionCount = screen.getAllByLabelText('Informations de la session de certification', {
+            exact: false,
+          }).length;
+          assert.strictEqual(sessionCount, 25);
           assert.dom('div.page-navigation__current-page').hasText('1');
         });
       });
@@ -99,13 +108,13 @@ module('Acceptance | Session List', function (hooks) {
       module('when invalid filter value are typed in', function () {
         test('it should display an empty list', async function (assert) {
           // given
-          await visit('/sessions/list');
+          const screen = await visit('/sessions/list');
 
           // when
           await fillIn('#id', 'azere');
 
           //then
-          assert.dom('.table__empty').hasText('Aucun résultat');
+          assert.dom(screen.getByText('Aucun résultat')).exists();
         });
       });
     });
@@ -121,11 +130,11 @@ module('Acceptance | Session List', function (hooks) {
 
         test('it should display the session with the ID specified in the input field', async function (assert) {
           // when
-          await visit('/sessions/list');
+          const screen = await visit('/sessions/list');
           await fillIn('#id', expectedSession.id);
 
           // then
-          assert.dom('.table-admin tbody tr').exists({ count: 1 });
+          assert.dom(screen.getByRole('link', { name: '1' })).exists();
         });
       });
 
@@ -133,17 +142,22 @@ module('Acceptance | Session List', function (hooks) {
         let expectedSession;
 
         hooks.beforeEach(function () {
-          expectedSession = server.create('session', 'finalized');
+          expectedSession = server.create('session', {
+            certificationCenterName: 'Erdman, Bode and Walker',
+            status: 'finalized',
+          });
           server.createList('session', 10, 'finalized');
         });
 
         test('it should display the session with a certification center name alike the one specified in the field', async function (assert) {
           // when
-          await visit('/sessions/list');
+          const screen = await visit('/sessions/list');
           await fillIn('#certificationCenterName', expectedSession.certificationCenterName.toUpperCase());
 
           // then
-          assert.dom('.table-admin tbody tr').exists({ count: 1 });
+          assert
+            .dom(screen.getByLabelText('Informations de la session de certification 1'))
+            .containsText('Erdman, Bode and Walker');
         });
       });
 
@@ -155,11 +169,14 @@ module('Acceptance | Session List', function (hooks) {
 
         test('it should display the session with status as specified in the dropdown', async function (assert) {
           // when
-          await visit('/sessions/list');
+          const screen = await visit('/sessions/list');
           await fillIn('select#status', 'processed');
 
           // then
-          assert.dom('.table-admin tbody tr').exists({ count: 5 });
+          const sessionProcessedCount = screen.getAllByLabelText('Informations de la session de certification', {
+            exact: false,
+          }).length;
+          assert.strictEqual(sessionProcessedCount, 5);
         });
       });
 
@@ -171,28 +188,37 @@ module('Acceptance | Session List', function (hooks) {
 
         test('it should display sessions regardless the results have been sent or not', async function (assert) {
           // when
-          await visit('/sessions/list');
+          const screen = await visit('/sessions/list');
 
           // then
-          assert.dom('.table-admin tbody tr').exists({ count: 8 });
+          const sessionCount = screen.getAllByLabelText('Informations de la session de certification', {
+            exact: false,
+          }).length;
+          assert.strictEqual(sessionCount, 8);
         });
 
         test('it should only display sessions which results have been sent', async function (assert) {
           // when
-          await visit('/sessions/list');
+          const screen = await visit('/sessions/list');
           await fillIn('select#resultsSentToPrescriberAt', 'true');
 
           // then
-          assert.dom('.table-admin tbody tr').exists({ count: 5 });
+          const sessionWithResultSentCount = screen.getAllByLabelText('Informations de la session de certification', {
+            exact: false,
+          }).length;
+          assert.strictEqual(sessionWithResultSentCount, 5);
         });
 
         test('it should only display sessions which results have not been sent', async function (assert) {
           // when
-          await visit('/sessions/list');
+          const screen = await visit('/sessions/list');
           await fillIn('select#resultsSentToPrescriberAt', 'false');
 
           // then
-          assert.dom('.table-admin tbody tr').exists({ count: 3 });
+          const sessionCount = screen.getAllByLabelText('Informations de la session de certification', {
+            exact: false,
+          }).length;
+          assert.strictEqual(sessionCount, 3);
         });
       });
     });
