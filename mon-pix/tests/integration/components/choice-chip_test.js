60298e59fc72ec8dd753a8f17284681e493358f8
Rousseau Corentin <cro@eonix.be>
2022-04-22 09:40:35 +0200

    test(mon-pix): create unit and integration tests for choice-chip

--- mon-pix/tests/integration/components/choice-chip_test.js
+++ mon-pix/tests/integration/components/choice-chip_test.js
@@ -4,22 +4,14 @@ import setupIntlRenderingTest from '../../helpers/setup-intl-rendering';
 import { find, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-describe('Integration | Component | chips-choice', function () {
+describe('Integration | Component | choice-chip', function () {
   setupIntlRenderingTest();
 
   it('renders', async function () {
     //when
-    await render(hbs`<ChipsChoice>Test</ChipsChoice>`);
+    await render(hbs`<ChoiceChip>Test</ChoiceChip>`);
 
     //then
     expect(find('.choice-chip')).to.exist;
-  });
-
-  it("should have disabled class when 'isDisabled' is true", async function () {
-    // when
-    await render(hbs`<ChipsChoice @isDisabled=true>Test</ChipsChoice>`);
-
-    // then
-    expect(find('.choice-chip--disabled')).to.exist;
   });
 });
