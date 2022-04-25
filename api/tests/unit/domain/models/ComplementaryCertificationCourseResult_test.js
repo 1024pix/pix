4a9d72d4fc9f49b607313928300c1b2460d37098
P-Jeremy <jemyplu@gmail.com>
2022-04-25 22:19:30 +0200

    :recycle: api: add methods and builder to ComplementaryCertificationCourseResult

--- api/tests/unit/domain/models/ComplementaryCertificationCourseResult_test.js
+++ api/tests/unit/domain/models/ComplementaryCertificationCourseResult_test.js
@@ -1,3 +1,4 @@
+const ComplementaryCertificationCourseResult = require('../../../../lib/domain/models/ComplementaryCertificationCourseResult');
 const { expect, domainBuilder } = require('../../../test-helper');
 const {
   PIX_DROIT_MAITRE_CERTIF,
@@ -211,6 +212,130 @@ describe('Unit | Domain | Models | ComplementaryCertificationCourseResult', func
 
         // then
         expect(result).to.equal(expected);
+      });
+    });
+  });
+
+  describe('#buildFromJuryLevel', function () {
+    describe('when the jury level is not "REJECTED"', function () {
+      it('should return an acquired ComplementaryCertificationCourseResult with an external source', function () {
+        // given
+        const complementaryCertificationCourseResult = domainBuilder.buildComplementaryCertificationCourseResult({
+          partnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_CONFIRME,
+          source: ComplementaryCertificationCourseResult.sources.PIX,
+          acquired: true,
+          complementaryCertificationCourseId: 12,
+        });
+
+        // when
+        const result = ComplementaryCertificationCourseResult.buildFromJuryLevel({
+          juryLevel: complementaryCertificationCourseResult.partnerKey,
+          complementaryCertificationCourseId: 12,
+          pixPartnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_CONFIRME,
+        });
+
+        // then
+        expect(result).to.deepEqualInstance(
+          new ComplementaryCertificationCourseResult({
+            acquired: true,
+            partnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_CONFIRME,
+            source: ComplementaryCertificationCourseResult.sources.EXTERNAL,
+            complementaryCertificationCourseId: 12,
+          })
+        );
+      });
+    });
+
+    describe('when the jury level is "REJECTED"', function () {
+      it('should return an acquired ComplementaryCertificationCourseResult with an external source', function () {
+        // given
+        const complementaryCertificationCourseResult = domainBuilder.buildComplementaryCertificationCourseResult({
+          partnerKey: 'REJECTED',
+          source: ComplementaryCertificationCourseResult.sources.PIX,
+          acquired: true,
+          complementaryCertificationCourseId: 12,
+        });
+
+        // when
+        const result = ComplementaryCertificationCourseResult.buildFromJuryLevel({
+          juryLevel: complementaryCertificationCourseResult.partnerKey,
+          complementaryCertificationCourseId: 12,
+          pixPartnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_CONFIRME,
+        });
+
+        // then
+        expect(result).to.deepEqualInstance(
+          new ComplementaryCertificationCourseResult({
+            acquired: false,
+            partnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_CONFIRME,
+            source: ComplementaryCertificationCourseResult.sources.EXTERNAL,
+            complementaryCertificationCourseId: 12,
+          })
+        );
+      });
+    });
+  });
+
+  describe('#isFromPixSource', function () {
+    describe('when source is PIX', function () {
+      it('should return true', function () {
+        // given
+        const complementaryCertificationCourseResult = domainBuilder.buildComplementaryCertificationCourseResult({
+          partnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_CONFIRME,
+          source: ComplementaryCertificationCourseResult.sources.PIX,
+          acquired: true,
+          complementaryCertificationCourseId: 12,
+        });
+
+        // when then
+        expect(complementaryCertificationCourseResult.isFromPixSource()).to.be.true;
+      });
+    });
+
+    describe('when source is not PIX', function () {
+      it('should return false', function () {
+        // given
+        const complementaryCertificationCourseResult = domainBuilder.buildComplementaryCertificationCourseResult({
+          partnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_CONFIRME,
+          source: ComplementaryCertificationCourseResult.sources.EXTERNAL,
+          acquired: true,
+          complementaryCertificationCourseId: 12,
+        });
+
+        // when then
+        expect(complementaryCertificationCourseResult.isFromPixSource()).to.be.false;
+      });
+    });
+  });
+
+  describe('#isFromExternalSource', function () {
+    describe('when source is EXTERNAL', function () {
+      it('should return true', function () {
+        // given
+        const complementaryCertificationCourseResult = domainBuilder.buildComplementaryCertificationCourseResult({
+          partnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_CONFIRME,
+          source: ComplementaryCertificationCourseResult.sources.EXTERNAL,
+          acquired: true,
+          complementaryCertificationCourseId: 12,
+        });
+
+        // when then
+        expect(complementaryCertificationCourseResult.isFromExternalSource()).to.be.true;
+      });
+    });
+
+    describe('when source is not EXTERNAL', function () {
+      it('should return false', function () {
+        // given
+        const complementaryCertificationCourseResult = domainBuilder.buildComplementaryCertificationCourseResult({
+          partnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_CONFIRME,
+          source: ComplementaryCertificationCourseResult.sources.PIX,
+          acquired: true,
+          complementaryCertificationCourseId: 12,
+        });
+
+        // when then
+        expect(complementaryCertificationCourseResult.isFromExternalSource()).to.be.false;
       });
     });
   });
