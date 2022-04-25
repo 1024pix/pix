9f837d1399e79f53169bfc501f778e7c7437e8ef
Quentin Lebouc <quentin.lebouc@pix.fr>
2022-04-25 17:19:16 +0200

    :recycle: feat(api): remove clea certification grey zone

--- api/tests/unit/domain/models/CleaCertificationScoring_test.js
+++ api/tests/unit/domain/models/CleaCertificationScoring_test.js
@@ -1,9 +1,9 @@
 const CleaCertificationScoring = require('../../../../lib/domain/models/CleaCertificationScoring');
-const { expect, catchErr, domainBuilder } = require('../../../test-helper');
+const { expect, catchErr } = require('../../../test-helper');
 const { ObjectValidationError, NotEligibleCandidateError } = require('../../../../lib/domain/errors');
 
-const GREEN_ZONE_REPRO = [80, 90, 100];
-const RED_ZONE_REPRO = [1, 50];
+const GREEN_ZONE_REPRO = [50, 70, 90];
+const RED_ZONE_REPRO = [1, 49];
 
 describe('Unit | Domain | Models | CleaCertificationScoring', function () {
   describe('constructor', function () {
@@ -15,8 +15,6 @@ describe('Unit | Domain | Models | CleaCertificationScoring', function () {
         cleaBadgeKey: 'partnerKey',
         hasAcquiredBadge: true,
         reproducibilityRate: 80,
-        cleaCompetenceMarks: [1],
-        expectedPixByCompetenceForClea: { competence1: 1 },
       };
     });
 
@@ -43,24 +41,6 @@ describe('Unit | Domain | Models | CleaCertificationScoring', function () {
           new CleaCertificationScoring({
             ...validArguments,
             reproducibilityRate: 'coucou',
-          })
-      ).to.throw(ObjectValidationError);
-    });
-
-    it('should throw an ObjectValidationError when cleaCompetenceMarks is not valid', function () {
-      // when
-      expect(() => new CleaCertificationScoring({ ...validArguments, cleaCompetenceMarks: null })).to.throw(
-        ObjectValidationError
-      );
-    });
-
-    it('should throw an ObjectValidationError when expectedPixByCompetenceForClea is not valid', function () {
-      // when
-      expect(
-        () =>
-          new CleaCertificationScoring({
-            ...validArguments,
-            expectedPixByCompetenceForClea: null,
           })
       ).to.throw(ObjectValidationError);
     });
@@ -159,62 +139,6 @@ describe('Unit | Domain | Models | CleaCertificationScoring', function () {
       );
     });
 
-    context('reproducibility rate in grey zone', function () {
-      it("for 70 reproducibility rate, it should obtain certification when the pixScore for each certifiable competences is above a floored 75% of Clea corresponding competence's pixScore for at least 75% of them", async function () {
-        // given
-        const cleaCertificationScoring =
-          await _buildCleaCertificationScoringInGreyZoneAndAtLeast75PercentOfCertifiableCompetences();
-
-        // when
-        const hasAcquiredCertif = cleaCertificationScoring.isAcquired();
-
-        // then
-        expect(hasAcquiredCertif).to.be.true;
-      });
-
-      it('for 70 reproducibility rate, it should not obtain certification when there are no competence marks for clea eligible competences', async function () {
-        // given
-        const competenceId1 = 'competenceId1',
-          competenceId2 = 'competenceId2',
-          competenceId3 = 'competenceId3',
-          competenceId4 = 'competenceId4';
-
-        const expectedPixByCompetenceForClea = {
-          [competenceId1]: 20,
-          [competenceId2]: 10,
-          [competenceId3]: 15,
-          [competenceId4]: 12,
-          competenceId5: 30,
-        };
-
-        const cleaCompetenceMarks = [];
-        const cleaCertificationScoring = _buildCleaCertificationScoring({
-          withBadge: true,
-          reproducibilityRate: 70,
-          cleaCompetenceMarks,
-          expectedPixByCompetenceForClea,
-        });
-
-        // when
-        const hasAcquiredCertif = cleaCertificationScoring.isAcquired();
-
-        // then
-        expect(hasAcquiredCertif).to.be.false;
-      });
-
-      it("for 70 reproducibility rate, it should not obtain certification when the pixScore for each certifiable competences is under a floored 75% of Clea corresponding competence's pixScore for less than 75% of them", async function () {
-        // given
-        const cleaCertificationScoring =
-          await _buildCleaCertificationScoringInGreyZoneAndLessThan75PercentOfCertifiableCompetences();
-
-        // when
-        const hasAcquiredCertif = cleaCertificationScoring.isAcquired();
-
-        // then
-        expect(hasAcquiredCertif).to.be.false;
-      });
-    });
-
     context('reproducibility rate in red zone', function () {
       // eslint-disable-next-line mocha/no-setup-in-describe
       RED_ZONE_REPRO.forEach((reproducibilityRate) =>
@@ -250,92 +174,7 @@ function _buildCleaCertificationScoringWithReproducibilityRate(reproducibilityRa
   return _buildCleaCertificationScoring({ withBadge: true, reproducibilityRate });
 }
 
-function _buildCleaCertificationScoringInGreyZoneAndAtLeast75PercentOfCertifiableCompetences() {
-  const competenceId1 = 'competenceId1',
-    competenceId2 = 'competenceId2',
-    competenceId3 = 'competenceId3',
-    competenceId4 = 'competenceId4';
-
-  const expectedPixByCompetenceForClea = {
-    [competenceId1]: 20,
-    [competenceId2]: 10,
-    [competenceId3]: 15,
-    [competenceId4]: 12,
-    competenceId5: 30,
-  };
-
-  const cleaCompetenceMarks = [
-    domainBuilder.buildCompetenceMark({
-      competenceId: competenceId1,
-      score: 15,
-    }),
-    domainBuilder.buildCompetenceMark({
-      competenceId: competenceId2,
-      score: 8,
-    }),
-    domainBuilder.buildCompetenceMark({
-      competenceId: competenceId3,
-      score: 3,
-    }),
-    domainBuilder.buildCompetenceMark({
-      competenceId: competenceId4,
-      score: 10,
-    }),
-  ];
-  return _buildCleaCertificationScoring({
-    withBadge: true,
-    reproducibilityRate: 70,
-    cleaCompetenceMarks,
-    expectedPixByCompetenceForClea,
-  });
-}
-
-function _buildCleaCertificationScoringInGreyZoneAndLessThan75PercentOfCertifiableCompetences() {
-  const competenceId1 = 'competenceId1',
-    competenceId2 = 'competenceId2',
-    competenceId3 = 'competenceId3',
-    competenceId4 = 'competenceId4';
-
-  const expectedPixByCompetenceForClea = {
-    [competenceId1]: 18,
-    [competenceId2]: 10,
-    [competenceId3]: 15,
-    [competenceId4]: 12,
-    competenceId5: 30,
-  };
-
-  const cleaCompetenceMarks = [
-    domainBuilder.buildCompetenceMark({
-      competenceId: competenceId1,
-      score: 12,
-    }),
-    domainBuilder.buildCompetenceMark({
-      competenceId: competenceId2,
-      score: 8,
-    }),
-    domainBuilder.buildCompetenceMark({
-      competenceId: competenceId3,
-      score: 3,
-    }),
-    domainBuilder.buildCompetenceMark({
-      competenceId: competenceId4,
-      score: 10,
-    }),
-  ];
-  return _buildCleaCertificationScoring({
-    withBadge: true,
-    reproducibilityRate: 70,
-    cleaCompetenceMarks,
-    expectedPixByCompetenceForClea,
-  });
-}
-
-function _buildCleaCertificationScoring({
-  withBadge = false,
-  reproducibilityRate = 0,
-  cleaCompetenceMarks = [domainBuilder.buildCompetenceMark()],
-  expectedPixByCompetenceForClea = { competence1: 1 },
-}) {
+function _buildCleaCertificationScoring({ withBadge = false, reproducibilityRate = 0 }) {
   const certificationCourseId = 42;
   const complementaryCertificationCourseId = 999;
   return new CleaCertificationScoring({
@@ -343,8 +182,6 @@ function _buildCleaCertificationScoring({
     certificationCourseId,
     hasAcquiredBadge: withBadge,
     reproducibilityRate,
-    cleaCompetenceMarks,
-    expectedPixByCompetenceForClea,
     cleaBadgeKey: 'pix_clea_badge_key',
   });
 }
