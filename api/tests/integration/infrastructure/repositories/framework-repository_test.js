8a5c8f0555963ed5adea948400dca9030ddeda7f
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2022-04-26 11:15:49 +0200

    Add framework getByName in repository
    Co-authored-by: Clarisse Damon <clarisse.damon@pix.fr>
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>
    Co-authored-by: Thomasevano <thomas-evano@hotmail.fr>

--- api/tests/integration/infrastructure/repositories/framework-repository_test.js
+++ api/tests/integration/infrastructure/repositories/framework-repository_test.js
@@ -1,24 +1,25 @@
-const { expect, mockLearningContent } = require('../../../test-helper');
+const { expect, mockLearningContent, catchErr } = require('../../../test-helper');
 const frameworkRepository = require('../../../../lib/infrastructure/repositories/framework-repository');
 const Framework = require('../../../../lib/domain/models/Framework');
+const { NotFoundError } = require('../../../../lib/domain/errors');
 
 describe('Integration | Repository | framework-repository', function () {
+  const framework0 = {
+    id: 'recId0',
+    name: 'mon framework 0',
+  };
+  const framework1 = {
+    id: 'recId0',
+    name: 'mon framework 1',
+  };
+
+  const learningContent = { frameworks: [framework0, framework1] };
+
+  beforeEach(function () {
+    mockLearningContent(learningContent);
+  });
+
   describe('#list', function () {
-    const framework0 = {
-      id: 'recId0',
-      name: 'mon framework 0',
-    };
-    const framework1 = {
-      id: 'recId0',
-      name: 'mon framework 1',
-    };
-
-    const learningContent = { frameworks: [framework0, framework1] };
-
-    beforeEach(function () {
-      mockLearningContent(learningContent);
-    });
-
     it('should return all frameworks', async function () {
       // when
       const frameworks = await frameworkRepository.list();
@@ -26,6 +27,31 @@ describe('Integration | Repository | framework-repository', function () {
       // then
       expect(frameworks).to.have.lengthOf(2);
       expect(frameworks[0]).to.be.instanceof(Framework);
+    });
+  });
+
+  describe('#getByName', function () {
+    it('should return a framework', async function () {
+      // when
+      const framework = await frameworkRepository.getByName('mon framework 1');
+
+      // then
+      expect(framework).to.be.instanceof(Framework);
+      expect(framework).to.deep.equal(framework1);
+    });
+
+    describe('when framework is not found', function () {
+      it('should return a rejection', async function () {
+        //given
+        const frameworkName = 'framework123';
+
+        // when
+        const error = await catchErr(frameworkRepository.getByName)(frameworkName);
+
+        // then
+        expect(error).to.be.an.instanceof(NotFoundError);
+        expect(error.message).to.equal('Framework not found for name framework123');
+      });
     });
   });
 });
