d8912945f370214518ba2aa0c929e68edf817746
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2022-04-26 11:15:49 +0200

    Add framework getByName in repository
    Co-authored-by: Clarisse Damon <clarisse.damon@pix.fr>
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>
    Co-authored-by: Thomasevano <thomas-evano@hotmail.fr>

--- api/lib/infrastructure/repositories/framework-repository.js
+++ api/lib/infrastructure/repositories/framework-repository.js
@@ -1,5 +1,6 @@
 const Framework = require('../../domain/models/Framework');
 const frameworkDatasource = require('../datasources/learning-content/framework-datasource');
+const { NotFoundError } = require('../../domain/errors');
 
 async function list() {
   const frameworkDataObjects = await frameworkDatasource.list();
@@ -13,6 +14,16 @@ function _toDomain(frameworkData) {
   });
 }
 
+async function getByName(name) {
+  const framework = await frameworkDatasource.getByName(name);
+
+  if (framework === undefined) {
+    throw new NotFoundError(`Framework not found for name ${name}`);
+  }
+  return _toDomain(framework);
+}
+
 module.exports = {
   list,
+  getByName,
 };
