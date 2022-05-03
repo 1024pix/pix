633545dd83fda9a6f7424531c579a50eb4ea8b21
Mathieu Gilet <mathieu.gilet@zenika.com>
2022-05-03 16:55:50 +0200

    :recycle: refactor: use methods in ComplementaryCertification

--- api/lib/domain/usecases/retrieve-last-or-create-certification-course.js
+++ api/lib/domain/usecases/retrieve-last-or-create-certification-course.js
@@ -1,7 +1,6 @@
 const CertificationCourse = require('../models/CertificationCourse');
 const Assessment = require('../models/Assessment');
 const ComplementaryCertificationCourse = require('../models/ComplementaryCertificationCourse');
-const { PIX_PLUS_DROIT, CLEA, PIX_PLUS_EDU } = require('../models/ComplementaryCertification');
 const {
   UserNotAuthorizedToCertifyError,
   NotFoundError,
@@ -140,7 +139,7 @@ async function _startNewCertification({
 
   if (certificationCenter.isHabilitatedClea && certificationCandidate.isGrantedCleA()) {
     if (await certificationBadgesService.hasStillValidCleaBadgeAcquisition({ userId })) {
-      const cleAComplementaryCertification = complementaryCertifications.find((comp) => comp.name === CLEA);
+      const cleAComplementaryCertification = complementaryCertifications.find((comp) => comp.isClea());
       if (cleAComplementaryCertification) {
         complementaryCertificationIds.push(cleAComplementaryCertification.id);
       }
@@ -157,9 +156,7 @@ async function _startNewCertification({
       badgeAcquisition.isPixDroit()
     );
     if (pixDroitBadgeAcquisition) {
-      const pixDroitComplementaryCertification = complementaryCertifications.find(
-        (comp) => comp.name === PIX_PLUS_DROIT
-      );
+      const pixDroitComplementaryCertification = complementaryCertifications.find((comp) => comp.isPixPlusDroit());
       if (pixDroitComplementaryCertification) {
         complementaryCertificationIds.push(pixDroitComplementaryCertification.id);
       }
@@ -185,7 +182,7 @@ async function _startNewCertification({
     );
     challengesForCertification.push(...certificationChallengesForPixEdu);
     if (certificationChallengesForPixEdu.length) {
-      const pixEduComplementaryCertification = complementaryCertifications.find((comp) => comp.name === PIX_PLUS_EDU);
+      const pixEduComplementaryCertification = complementaryCertifications.find((comp) => comp.isPixPlusEdu());
       if (pixEduComplementaryCertification) {
         complementaryCertificationIds.push(pixEduComplementaryCertification.id);
       }
