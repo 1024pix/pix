2936e06ee4e9cd54b845c805d3fa0862c62b7490
Laura Bergoens <laura.bergoens@pix.fr>
2022-05-05 21:56:47 +0200

    :sparkles: feat(certif): change E3 label depending on FT

--- certif/tests/unit/models/certification-issue-report_test.js
+++ certif/tests/unit/models/certification-issue-report_test.js
@@ -1,7 +1,9 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import { run } from '@ember/runloop';
+import Service from '@ember/service';
 import map from 'lodash/map';
+import find from 'lodash/find';
 
 import {
   certificationIssueReportCategories,
@@ -14,6 +16,21 @@ import {
 
 module('Unit | Model | certification issue report', function (hooks) {
   setupTest(hooks);
+
+  const featureToggles = {
+    isCertificationFreeFieldsDeletionEnabled: true,
+  };
+
+  hooks.beforeEach(function () {
+    class FeatureTogglesStub extends Service {
+      featureToggles = featureToggles;
+    }
+    this.owner.register('service:feature-toggles', FeatureTogglesStub);
+  });
+
+  hooks.afterEach(function () {
+    featureToggles.isCertificationFreeFieldsDeletionEnabled = true;
+  });
 
   test('it should return the right label for the category', function (assert) {
     assert.expect(7);
@@ -30,7 +47,7 @@ module('Unit | Model | certification issue report', function (hooks) {
     }
   });
 
-  test('it should return the right label for the subcategory', function (assert) {
+  test('it should return the right label for the subcategory when FT enabled', function (assert) {
     const expectedAssertNumber = 12;
     assert.expect(expectedAssertNumber);
     // given
@@ -44,6 +61,20 @@ module('Unit | Model | certification issue report', function (hooks) {
     for (const model of models) {
       assert.strictEqual(model.subcategoryLabel, subcategoryToLabel[model.subcategory]);
     }
+  });
+
+  test('it should return the right label for the subcategory FILE_NOT_OPENING when FT disabled', function (assert) {
+    featureToggles.isCertificationFreeFieldsDeletionEnabled = false;
+    // given
+    const store = this.owner.lookup('service:store');
+
+    const models = map(certificationIssueReportSubcategories, (subcategory) => {
+      return run(() => store.createRecord('certification-issue-report', { subcategory }));
+    });
+    const fileNotOpeningModel = find(models, (m) => m.subcategory === 'FILE_NOT_OPENING');
+
+    // when / then
+    assert.strictEqual(fileNotOpeningModel.subcategoryLabel, "Le fichier à télécharger ne s'ouvre pas");
   });
 
   test('it should return the right code for the category', function (assert) {
