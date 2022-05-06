d16c5d7806fabaab3d01ade017ca86e95e55faf6
Andreia Pena <andreia.pena@pix.fr>
2022-05-06 17:18:50 +0200

    ✨ [app] Create cnav cgu page

--- mon-pix/app/routes/login-cnav.js
+++ mon-pix/app/routes/login-cnav.js
@@ -1,5 +1,7 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+
+import get from 'lodash/get';
 import ENV from 'mon-pix/config/environment';
 import fetch from 'fetch';
 
@@ -22,6 +24,39 @@ export default class LoginCnavRoute extends Route {
 
     if (!queryParams.code && !queryParams.error) {
       return this._handleRedirectRequest();
+    }
+  }
+
+  async model(_, transition) {
+    const queryParams = transition.to ? transition.to.queryParams : transition.queryParams;
+    if (queryParams.code) {
+      return this._handleCallbackRequest(queryParams.code, queryParams.state);
+    }
+  }
+
+  afterModel({ shouldValidateCgu, authenticationKey } = {}) {
+    if (shouldValidateCgu && authenticationKey) {
+      return this.replaceWith('terms-of-service-cnav', { queryParams: { authenticationKey } });
+    }
+  }
+
+  async _handleCallbackRequest(code, state) {
+    try {
+      await this.session.authenticate('authenticator:cnav', {
+        code,
+        redirectUri: this.redirectUri,
+        state,
+      });
+    } catch (response) {
+      const shouldValidateCgu = get(response, 'errors[0].code') === 'SHOULD_VALIDATE_CGU';
+      const authenticationKey = get(response, 'errors[0].meta.authenticationKey');
+      if (shouldValidateCgu && authenticationKey) {
+        return { shouldValidateCgu, authenticationKey };
+      }
+      throw new Error(JSON.stringify(response.errors));
+    } finally {
+      this.session.set('data.state', undefined);
+      this.session.set('data.nonce', undefined);
     }
   }
 
