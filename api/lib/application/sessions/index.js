ec4873d6ce9f90528aa3ea082740664b9f8f36c8
Anne-G <ang.schall@gmail.com>
2022-05-06 17:16:21 +0200

    api: prevent metier to assign themself a session

--- api/lib/application/sessions/index.js
+++ api/lib/application/sessions/index.js
@@ -650,7 +650,6 @@ exports.register = async (server) => {
                 securityPreHandlers.checkUserHasRoleSuperAdmin,
                 securityPreHandlers.checkUserHasRoleCertif,
                 securityPreHandlers.checkUserHasRoleSupport,
-                securityPreHandlers.checkUserHasRoleMetier,
               ])(request, h),
             assign: 'hasAuthorizationToAccessAdminScope',
           },
