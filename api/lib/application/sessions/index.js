4a2f06668af4d11b7090be1a6ae072152ae7263b
Anne-G <ang.schall@gmail.com>
2022-05-06 17:14:19 +0200

    api: prevent metier to unpublish session

--- api/lib/application/sessions/index.js
+++ api/lib/application/sessions/index.js
@@ -591,7 +591,6 @@ exports.register = async (server) => {
                 securityPreHandlers.checkUserHasRoleSuperAdmin,
                 securityPreHandlers.checkUserHasRoleCertif,
                 securityPreHandlers.checkUserHasRoleSupport,
-                securityPreHandlers.checkUserHasRoleMetier,
               ])(request, h),
             assign: 'hasAuthorizationToAccessAdminScope',
           },
