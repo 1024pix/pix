e01a4a53304ccab525d7a1366433545296778034
Andreia Pena <andreia.pena@pix.fr>
2022-05-06 18:40:45 +0200

    ✨ [api] Add createUser method on cnav controller

--- api/lib/application/cnav/cnav-controller.js
+++ api/lib/application/cnav/cnav-controller.js
@@ -1,6 +1,20 @@
 const cnavAuthenticationService = require('../../../lib/domain/services/authentication/cnav-authentication-service');
+const usecases = require('../../domain/usecases');
+const userRepository = require('../../infrastructure/repositories/user-repository');
 
 module.exports = {
+  async createUser(request, h) {
+    const authenticationKey = request.query['authentication-key'];
+
+    const userId = await usecases.createUserFromCnav({ authenticationKey });
+
+    const accessToken = cnavAuthenticationService.createAccessToken(userId);
+    await userRepository.updateLastLoggedAt({ userId });
+
+    const response = { access_token: accessToken };
+    return h.response(response).code(200);
+  },
+
   async getAuthUrl(request, h) {
     const result = cnavAuthenticationService.getAuthUrl({
       redirectUri: request.query['redirect_uri'],
