8264d34d6bdff8f81b34270fa2f4ce2fd47cff53
Ismael Gorissen <ismael.gorissen@gmail.com>
2022-05-06 12:30:14 +0200

    feat(admin): restrict route access to `/equipe`
    Only an admin member with a role "SUPER_ADMIN" can access this route.

--- admin/app/routes/authenticated/team/list.js
+++ admin/app/routes/authenticated/team/list.js
@@ -1,6 +1,13 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class ListRoute extends Route {
+  @service accessControl;
+
+  beforeModel() {
+    this.accessControl.restrictAccessTo(['isSuperAdmin'], 'authenticated');
+  }
+
   async model() {
     return this.store.query('admin-member', {});
   }
