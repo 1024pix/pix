e6d46aee2817f355e2b2ed329c82c0430062616e
Ismael Gorissen <ismael.gorissen@gmail.com>
2022-05-06 09:54:38 +0200

    feat(api): restrict access for GET /api/admin/admin-members
    Only an admin member with a role "SUPER_ADMIN" can call this route.

---
+++ api/tests/unit/application/admin-members/index_test.js
@@ -0,0 +1,41 @@
+const { domainBuilder, expect, sinon, HttpTestServer } = require('../../../test-helper');
+const adminMemberController = require('../../../../lib/application/admin-members/admin-member-controller');
+const adminMembersRouter = require('../../../../lib/application/admin-members');
+const securityPreHandlers = require('../../../../lib/application/security-pre-handlers');
+
+describe('Unit | Application | Router | admin-members-router', function () {
+  describe('GET /api/admin/admin-members', function () {
+    it('should return a response with an HTTP status code 200 when user has role "SUPER_ADMIN"', async function () {
+      // given
+      const adminMembers = [domainBuilder.buildAdminMember()];
+      sinon.stub(adminMemberController, 'findAll').returns(adminMembers);
+      sinon.stub(securityPreHandlers, 'checkUserHasRoleSuperAdmin').returns(true);
+      const httpTestServer = new HttpTestServer();
+      await httpTestServer.register(adminMembersRouter);
+
+      // when
+      const { statusCode } = await httpTestServer.request('GET', '/api/admin/admin-members');
+
+      // then
+      expect(statusCode).to.equal(200);
+      sinon.assert.calledOnce(adminMemberController.findAll);
+    });
+
+    it('should return a response with an HTTP status code 403 if user does not have the rights', async function () {
+      // given
+      const adminMembers = [domainBuilder.buildAdminMember()];
+      sinon.stub(adminMemberController, 'findAll').returns(adminMembers);
+      sinon
+        .stub(securityPreHandlers, 'checkUserHasRoleSuperAdmin')
+        .callsFake((request, h) => h.response().code(403).takeover());
+      const httpTestServer = new HttpTestServer();
+      await httpTestServer.register(adminMembersRouter);
+
+      // when
+      const { statusCode } = await httpTestServer.request('GET', '/api/admin/admin-members');
+
+      // then
+      expect(statusCode).to.equal(403);
+    });
+  });
+});
