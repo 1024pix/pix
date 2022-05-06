4a0f39eaddefd93af8370206dfd18728a21c1a71
Anne-G <ang.schall@gmail.com>
2022-05-06 15:33:07 +0200

    admin: prevent certif/support/metier to access to tool page

--- admin/app/routes/authenticated/tools.js
+++ admin/app/routes/authenticated/tools.js
@@ -1,3 +1,13 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class ToolsRoute extends Route {}
+export default class ToolsRoute extends Route {
+  @service router;
+  @service currentUser;
+
+  beforeModel() {
+    if (!this.currentUser.adminMember.hasAccess(['isSuperAdmin'])) {
+      this.router.transitionTo('authenticated');
+    }
+  }
+}
--- admin/tests/unit/routes/authenticated/tools_test.js
+++ admin/tests/unit/routes/authenticated/tools_test.js
@@ -1,11 +1,46 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import sinon from 'sinon';
+import Service from '@ember/service';
 
 module('Unit | Route | authenticated/tools', function (hooks) {
   setupTest(hooks);
 
-  test('it exists', function (assert) {
+  test('it should allow super admin to access page', function (assert) {
+    // given
     const route = this.owner.lookup('route:authenticated/tools');
-    assert.ok(route);
+    const router = this.owner.lookup('service:router');
+    router.transitionTo = sinon.stub();
+
+    class CurrentUserStub extends Service {
+      adminMember = { hasAccess: sinon.stub().returns(true) };
+    }
+    this.owner.register('service:current-user', CurrentUserStub);
+
+    // when
+    route.beforeModel();
+
+    // then
+    assert.ok(router.transitionTo.notCalled);
+  });
+
+  test('it should redirect all other pix member (certif, metier, support) into home page', function (assert) {
+    // given
+    const route = this.owner.lookup('route:authenticated/tools');
+    const router = this.owner.lookup('service:router');
+    router.transitionTo = sinon.stub();
+
+    const hasAccessStub = sinon.stub().returns(true);
+    hasAccessStub.withArgs(['isSuperAdmin']).returns(false);
+    class CurrentUserStub extends Service {
+      adminMember = { hasAccess: hasAccessStub };
+    }
+    this.owner.register('service:current-user', CurrentUserStub);
+
+    // when
+    route.beforeModel();
+
+    // then
+    assert.ok(router.transitionTo.called);
   });
 });
