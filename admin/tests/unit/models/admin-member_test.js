6b1d357934a356a7a6284f59cbf5f8ae9ec982a3
Anne-G <ang.schall@gmail.com>
2022-05-09 15:26:06 +0200

    admin: refacto with adding access control service

--- admin/tests/unit/models/admin-member_test.js
+++ admin/tests/unit/models/admin-member_test.js
@@ -24,41 +24,4 @@ module('Unit | Model | admin-member', function (hooks) {
       assert.strictEqual(fullName, 'Jean-Baptiste Poquelin');
     });
   });
-
-  module('hasAccess', function () {
-    test('it should return true if current user has the given right', function (assert) {
-      // given
-      const adminMember = run(() => {
-        return store.createRecord('adminMember', {
-          firstName: 'Jean-Baptiste',
-          lastName: 'Poquelin',
-          isSuperAdmin: true,
-        });
-      });
-
-      // when
-      const hasAdminMemberAccess = adminMember.hasAccess(['isCertif', 'isSuperAdmin']);
-
-      // then
-      assert.true(hasAdminMemberAccess);
-    });
-
-    test('it should return false if current user does not have the given right', function (assert) {
-      // given
-      const adminMember = run(() => {
-        return store.createRecord('adminMember', {
-          firstName: 'Jean-Baptiste',
-          lastName: 'Poquelin',
-          isSuperAdmin: false,
-          isCertif: false,
-        });
-      });
-
-      // when
-      const hasAdminMemberAccess = adminMember.hasAccess(['isCertif', 'isSuperAdmin']);
-
-      // then
-      assert.false(hasAdminMemberAccess);
-    });
-  });
 });
