d19c076071a20bbc4b3eb0278edce1abefd1aead
Anne-G <ang.schall@gmail.com>
2022-05-09 11:11:02 +0200

    admin: prevent metier to publish one session

--- admin/app/components/to-be-published-sessions/list-items.js
+++ admin/app/components/to-be-published-sessions/list-items.js
@@ -1,14 +1,25 @@
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 export default class ToBePublishedSessionsList extends Component {
+  @service currentUser;
+
   @tracked shouldShowModal = false;
   currentSelectedSession;
 
   _cancelModalSelection() {
     this.shouldShowModal = false;
     this.currentSelectedSession = null;
+  }
+
+  get isCurrentUserAllowedToPublishSession() {
+    return (
+      this.currentUser.adminMember.isSuperAdmin ||
+      this.currentUser.adminMember.isSupport ||
+      this.currentUser.adminMember.isCertif
+    );
   }
 
   @action
--- admin/tests/integration/components/routes/authenticated/to-be-published-sessions/list-items_test.js
+++ admin/tests/integration/components/routes/authenticated/to-be-published-sessions/list-items_test.js
@@ -9,6 +9,8 @@ module('Integration | Component | routes/authenticated/to-be-published-sessions 
 
   test('it should display to be published sessions list', async function (assert) {
     // given
+    const currentUser = this.owner.lookup('service:currentUser');
+    currentUser.adminMember = { isCertif: true };
     const firstSession = {
       id: '1',
       certificationCenterName: 'Centre SCO des Anne-Étoiles',
@@ -46,6 +48,8 @@ module('Integration | Component | routes/authenticated/to-be-published-sessions 
 
   test('it should "Aucun résultat" if there are no sessions to show', async function (assert) {
     // given
+    const currentUser = this.owner.lookup('service:currentUser');
+    currentUser.adminMember = { isCertif: true };
     this.toBePublishedSessions = [];
 
     // when
@@ -57,48 +61,78 @@ module('Integration | Component | routes/authenticated/to-be-published-sessions 
     assert.dom(screen.getByText('Aucun résultat')).exists();
   });
 
-  test('it should show confirmation modal when one clicks on "Publier" button', async function (assert) {
-    // given
-    const session = {
-      id: '1',
-      certificationCenterName: 'Centre SCO des Anne-Étoiles',
-      sessionDate: '2021-01-01',
-      sessionTime: '11:00:00',
-      finalizedAt: new Date('2021-01-02T03:00:00Z'),
-    };
-    this.toBePublishedSessions = [session];
-    const screen = await render(
-      hbs`<ToBePublishedSessions::ListItems @toBePublishedSessions={{this.toBePublishedSessions}}/>`
-    );
+  module('Publish a session', function () {
+    test('it should not show button if current user is metier', async function (assert) {
+      // given
+      const currentUser = this.owner.lookup('service:currentUser');
+      currentUser.adminMember = { isMetier: true };
+      const session = {
+        id: '1',
+        certificationCenterName: 'Centre SCO des Anne-Étoiles',
+        sessionDate: '2021-01-01',
+        sessionTime: '11:00:00',
+        finalizedAt: new Date('2021-01-02T03:00:00Z'),
+      };
+      this.toBePublishedSessions = [session];
 
-    // when
-    await clickByName('Publier la session numéro 1');
+      // when
+      const screen = await render(
+        hbs`<ToBePublishedSessions::ListItems @toBePublishedSessions={{this.toBePublishedSessions}}/>`
+      );
 
-    // then
-    assert.dom(screen.getByText('Souhaitez-vous publier la session ?')).exists();
-  });
+      // then
+      assert.dom(screen.queryByText('Publier')).doesNotExist();
+    });
 
-  test('it should call provided publishModel "action" with the right published session as argument', async function (assert) {
-    // given
-    const session = {
-      id: '1',
-      certificationCenterName: 'Centre SCO des Anne-Étoiles',
-      sessionDate: '2021-01-01',
-      sessionTime: '11:00:00',
-      finalizedAt: new Date('2021-01-02T03:00:00Z'),
-    };
-    this.toBePublishedSessions = [session];
-    this.publishSession = sinon.stub();
-    await render(
-      hbs`<ToBePublishedSessions::ListItems @toBePublishedSessions={{this.toBePublishedSessions}} @publishSession={{this.publishSession}}/>`
-    );
-    await clickByName('Publier la session numéro 1');
+    test('it should show confirmation modal when one clicks on "Publier" button', async function (assert) {
+      // given
+      const currentUser = this.owner.lookup('service:currentUser');
+      currentUser.adminMember = { isCertif: true };
 
-    // when
-    await clickByName('Confirmer');
+      const session = {
+        id: '1',
+        certificationCenterName: 'Centre SCO des Anne-Étoiles',
+        sessionDate: '2021-01-01',
+        sessionTime: '11:00:00',
+        finalizedAt: new Date('2021-01-02T03:00:00Z'),
+      };
+      this.toBePublishedSessions = [session];
+      const screen = await render(
+        hbs`<ToBePublishedSessions::ListItems @toBePublishedSessions={{this.toBePublishedSessions}}/>`
+      );
 
-    // then
-    sinon.assert.calledWith(this.publishSession, session);
-    assert.ok(true);
+      // when
+      await clickByName('Publier la session numéro 1');
+
+      // then
+      assert.dom(screen.getByText('Souhaitez-vous publier la session ?')).exists();
+    });
+
+    test('it should call provided publishModel "action" with the right published session as argument', async function (assert) {
+      // given
+      const currentUser = this.owner.lookup('service:currentUser');
+      currentUser.adminMember = { isCertif: true };
+
+      const session = {
+        id: '1',
+        certificationCenterName: 'Centre SCO des Anne-Étoiles',
+        sessionDate: '2021-01-01',
+        sessionTime: '11:00:00',
+        finalizedAt: new Date('2021-01-02T03:00:00Z'),
+      };
+      this.toBePublishedSessions = [session];
+      this.publishSession = sinon.stub();
+      await render(
+        hbs`<ToBePublishedSessions::ListItems @toBePublishedSessions={{this.toBePublishedSessions}} @publishSession={{this.publishSession}}/>`
+      );
+      await clickByName('Publier la session numéro 1');
+
+      // when
+      await clickByName('Confirmer');
+
+      // then
+      sinon.assert.calledWith(this.publishSession, session);
+      assert.ok(true);
+    });
   });
 });
