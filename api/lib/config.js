1a6dfa79135f8c01f20f4b2a8d9d828d4b703d73
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-05-11 14:31:05 +0200

    feat(api): run RedisClient tests in CI

--- api/lib/config.js
+++ api/lib/config.js
@@ -300,6 +300,9 @@ module.exports = (function () {
     config.poleEmploi.sendingUrl = 'http://sendingUrl.fr';
     config.poleEmploi.userInfoUrl = 'http://userInfoUrl.fr';
     config.poleEmploi.authUrl = 'http://authurl.fr';
+    config.poleEmploi.temporaryStorage.redisUrl = null;
+
+    config.temporaryStorage.redisUrl = null;
 
     config.cnav.clientId = 'PIX_CNAV_CLIENT_ID';
     config.cnav.authUrl = 'http://idp.cnav/auth';
@@ -348,6 +351,11 @@ module.exports = (function () {
     config.caching.redisCacheLockedWaitBeforeRetry = 0;
 
     config.sentry.enabled = false;
+
+    config.redis = {
+      url: process.env.TEST_REDIS_URL,
+      database: 1,
+    };
   }
 
   return config;
