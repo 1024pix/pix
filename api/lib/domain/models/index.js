f18ec974eb68255ec7f8cbfda136a236f51ea821
Matthias FERRAINA <matthias.ferraina@pix.fr>
2022-05-11 18:16:41 +0200

    feat(api): setup migration for models

--- api/lib/domain/models/index.js
+++ api/lib/domain/models/index.js
@@ -1,10 +1,11 @@
 const path = require('path');
-
 /* eslint-disable-next-line no-sync */
 require('fs')
   .readdirSync(__dirname)
   .forEach(function (file) {
     if (file === 'index.js') return;
-
-    module.exports[path.basename(file, '.js')] = require(path.join(__dirname, file));
+    const fileExtension = path.extname(file);
+    const className = path.basename(file, path.extname(file));
+    module.exports[className] =
+      fileExtension === '.ts' ? require(path.join(__dirname, file))[className] : require(path.join(__dirname, file));
   });
