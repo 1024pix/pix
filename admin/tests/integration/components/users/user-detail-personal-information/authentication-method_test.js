86415093c10c9309464d90a6e1c50d64ac08508d
Ismael Gorissen <ismael.gorissen@gmail.com>
2022-05-12 16:11:03 +0200

    feat(admin): restrict call to actions on AuthenticationMethod component

--- admin/tests/integration/components/users/user-detail-personal-information/authentication-method_test.js
+++ admin/tests/integration/components/users/user-detail-personal-information/authentication-method_test.js
@@ -2,71 +2,21 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { hbs } from 'ember-cli-htmlbars';
 import { render } from '@1024pix/ember-testing-library';
+import Service from '@ember/service';
 
 module('Integration | Component | users | user-detail-personal-information/authentication-method', function (hooks) {
   setupRenderingTest(hooks);
 
-  module('When user has authentication methods', function () {
-    test('should display user’s email authentication method', async function (assert) {
-      // given
-      this.set('user', { hasEmailAuthenticationMethod: true });
+  module('When the admin member has access to users actions scope', function () {
+    class AccessControlStub extends Service {
+      hasAccessToUsersActionsScope = true;
+    }
 
-      // when
-      const screen = await render(hbs`
-        <Users::UserDetailPersonalInformation::AuthenticationMethod
-          @user={{this.user}}
-        />`);
-
-      // then
-      assert.dom(screen.getByLabelText("L'utilisateur a une méthode de connexion avec adresse e-mail")).exists();
-    });
-
-    test('should display user’s username authentication method', async function (assert) {
-      // given
-      this.set('user', { hasUsernameAuthenticationMethod: true });
-
-      // when
-      const screen = await render(hbs`
-        <Users::UserDetailPersonalInformation::AuthenticationMethod
-          @user={{this.user}}
-        />`);
-
-      // then
-      assert.dom(screen.getByLabelText("L'utilisateur a une méthode de connexion avec identifiant")).exists();
-    });
-
-    test('should display user’s Pole Emploi authentication method', async function (assert) {
-      // given
-      this.set('user', { hasPoleEmploiAuthenticationMethod: true });
-
-      // when
-      const screen = await render(hbs`
-        <Users::UserDetailPersonalInformation::AuthenticationMethod
-          @user={{this.user}}
-        />`);
-
-      // then
-      assert.dom(screen.getByLabelText("L'utilisateur a une méthode de connexion Pôle Emploi")).exists();
-    });
-
-    test('should display user’s gar authentication method', async function (assert) {
-      // given
-      this.set('user', { hasGarAuthenticationMethod: true });
-
-      // when
-      const screen = await render(hbs`
-        <Users::UserDetailPersonalInformation::AuthenticationMethod
-          @user={{this.user}}
-        />`);
-
-      // then
-      assert.dom(screen.getByLabelText("L'utilisateur a une méthode de connexion Médiacentre")).exists();
-    });
-
-    module('When user has only one authentication method', function () {
-      test('it should not display a remove authentication method link', async function (assert) {
+    module('When user has authentication methods', function () {
+      test('should display user’s email authentication method', async function (assert) {
         // given
-        this.set('user', { hasOnlyOneAuthenticationMethod: true });
+        this.set('user', { hasEmailAuthenticationMethod: true });
+        this.owner.register('service:access-control', AccessControlStub);
 
         // when
         const screen = await render(hbs`
@@ -75,17 +25,116 @@ module('Integration | Component | users | user-detail-personal-information/authe
         />`);
 
         // then
-        assert.dom(screen.queryByRole('button', { name: 'Supprimer' })).doesNotExist();
+        assert.dom(screen.getByLabelText("L'utilisateur a une méthode de connexion avec adresse e-mail")).exists();
       });
 
-      module('When user does not have pix authentication method', function () {
-        test('it should display add authentication method button', async function (assert) {
+      test('should display user’s username authentication method', async function (assert) {
+        // given
+        this.set('user', { hasUsernameAuthenticationMethod: true });
+        this.owner.register('service:access-control', AccessControlStub);
+
+        // when
+        const screen = await render(hbs`
+        <Users::UserDetailPersonalInformation::AuthenticationMethod
+          @user={{this.user}}
+        />`);
+
+        // then
+        assert.dom(screen.getByLabelText("L'utilisateur a une méthode de connexion avec identifiant")).exists();
+      });
+
+      test('should display user’s Pole Emploi authentication method', async function (assert) {
+        // given
+        this.set('user', { hasPoleEmploiAuthenticationMethod: true });
+        this.owner.register('service:access-control', AccessControlStub);
+
+        // when
+        const screen = await render(hbs`
+        <Users::UserDetailPersonalInformation::AuthenticationMethod
+          @user={{this.user}}
+        />`);
+
+        // then
+        assert.dom(screen.getByLabelText("L'utilisateur a une méthode de connexion Pôle Emploi")).exists();
+      });
+
+      test('should display user’s gar authentication method', async function (assert) {
+        // given
+        this.set('user', { hasGarAuthenticationMethod: true });
+        this.owner.register('service:access-control', AccessControlStub);
+
+        // when
+        const screen = await render(hbs`
+        <Users::UserDetailPersonalInformation::AuthenticationMethod
+          @user={{this.user}}
+        />`);
+
+        // then
+        assert.dom(screen.getByLabelText("L'utilisateur a une méthode de connexion Médiacentre")).exists();
+      });
+
+      module('When user has only one authentication method', function () {
+        test('it should not display a remove authentication method link', async function (assert) {
+          // given
+          this.set('user', { hasOnlyOneAuthenticationMethod: true });
+          this.owner.register('service:access-control', AccessControlStub);
+
+          // when
+          const screen = await render(hbs`
+        <Users::UserDetailPersonalInformation::AuthenticationMethod
+          @user={{this.user}}
+        />`);
+
+          // then
+          assert.dom(screen.queryByRole('button', { name: 'Supprimer' })).doesNotExist();
+        });
+
+        module('When user does not have pix authentication method', function () {
+          test('it should display add authentication method button', async function (assert) {
+            // given
+            this.set('user', {
+              isAllowedToAddEmailAuthenticationMethod: true,
+              hasOnlyOneAuthenticationMethod: true,
+              hasPixAuthenticationMethod: false,
+            });
+            this.owner.register('service:access-control', AccessControlStub);
+
+            // when
+            const screen = await render(hbs`
+          <Users::UserDetailPersonalInformation::AuthenticationMethod
+            @user={{this.user}}
+          />`);
+
+            // then
+            assert.dom(screen.getByRole('button', { name: 'Ajouter une adresse e-mail' })).exists();
+          });
+        });
+
+        module('When user has a pix authentication method', function () {
+          test('it should not display add authentication method button', async function (assert) {
+            // given
+            this.set('user', { hasOnlyOneAuthenticationMethod: true, hasPixAuthenticationMethod: true });
+            this.owner.register('service:access-control', AccessControlStub);
+
+            // when
+            const screen = await render(hbs`
+          <Users::UserDetailPersonalInformation::AuthenticationMethod
+            @user={{this.user}}
+          />`);
+
+            // then
+            assert.dom(screen.queryByRole('button', { name: 'Ajouter une adresse e-mail' })).doesNotExist();
+          });
+        });
+      });
+
+      module('When user has gar authentication method', function () {
+        test('it should display reassign authentication method button', async function (assert) {
           // given
           this.set('user', {
-            isAllowedToAddEmailAuthenticationMethod: true,
-            hasOnlyOneAuthenticationMethod: true,
-            hasPixAuthenticationMethod: false,
+            hasGarAuthenticationMethod: true,
           });
+          this.owner.register('service:access-control', AccessControlStub);
 
           // when
           const screen = await render(hbs`
@@ -94,14 +143,17 @@ module('Integration | Component | users | user-detail-personal-information/authe
           />`);
 
           // then
-          assert.dom(screen.getByRole('button', { name: 'Ajouter une adresse e-mail' })).exists();
+          assert.dom(screen.getByRole('button', { name: 'Déplacer cette méthode de connexion' })).exists();
         });
       });
 
-      module('When user has a pix authentication method', function () {
-        test('it should not display add authentication method button', async function (assert) {
+      module('When user has Pole Emploi authentication method', function () {
+        test('it should display reassign authentication method button', async function (assert) {
           // given
-          this.set('user', { hasOnlyOneAuthenticationMethod: true, hasPixAuthenticationMethod: true });
+          this.set('user', {
+            hasPoleEmploiAuthenticationMethod: true,
+          });
+          this.owner.register('service:access-control', AccessControlStub);
 
           // when
           const screen = await render(hbs`
@@ -110,45 +162,32 @@ module('Integration | Component | users | user-detail-personal-information/authe
           />`);
 
           // then
-          assert.dom(screen.queryByRole('button', { name: 'Ajouter une adresse e-mail' })).doesNotExist();
+          assert.dom(screen.getByRole('button', { name: 'Déplacer cette méthode de connexion' })).exists();
         });
       });
     });
+  });
 
-    module('When user has gar authentication method', function () {
-      test('it should display reassign authentication method button', async function (assert) {
-        // given
-        this.set('user', {
-          hasGarAuthenticationMethod: true,
-        });
-
-        // when
-        const screen = await render(hbs`
-          <Users::UserDetailPersonalInformation::AuthenticationMethod
-            @user={{this.user}}
-          />`);
-
-        // then
-        assert.dom(screen.getByRole('button', { name: 'Déplacer cette méthode de connexion' })).exists();
+  module('When the admin member does not have access to users actions scope', function () {
+    test('it should not be able to see action buttons "Supprimer" and "Déplacer cette méthode de connexion"', async function (assert) {
+      // given
+      class AccessControlStub extends Service {
+        hasAccessToUsersActionsScope = false;
+      }
+      this.set('user', {
+        isAllowedToRemoveGarAuthenticationMethod: true,
+        hasGarAuthenticationMethod: true,
       });
-    });
+      this.owner.register('service:access-control', AccessControlStub);
 
-    module('When user has Pole Emploi authentication method', function () {
-      test('it should display reassign authentication method button', async function (assert) {
-        // given
-        this.set('user', {
-          hasPoleEmploiAuthenticationMethod: true,
-        });
+      // when
+      const screen = await render(
+        hbs`<Users::UserDetailPersonalInformation::AuthenticationMethod @user={{this.user}}/>`
+      );
 
-        // when
-        const screen = await render(hbs`
-          <Users::UserDetailPersonalInformation::AuthenticationMethod
-            @user={{this.user}}
-          />`);
-
-        // then
-        assert.dom(screen.getByRole('button', { name: 'Déplacer cette méthode de connexion' })).exists();
-      });
+      // then
+      assert.dom(screen.queryByRole('button', { name: 'Supprimer' })).doesNotExist();
+      assert.dom(screen.queryByRole('button', { name: 'Déplacer cette méthode de connexion' })).doesNotExist();
     });
   });
 });
