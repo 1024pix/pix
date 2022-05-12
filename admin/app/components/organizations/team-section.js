effc1a1bc56c86ee423ab15a0e73d406f2f92485
yschoueri <yasmina.schoueri@gmail.com>
2022-05-12 16:47:56 +0200

    feat(admin): block CERTIF users from modifying or deactivating a member

--- admin/app/components/organizations/team-section.js
+++ admin/app/components/organizations/team-section.js
@@ -1,6 +1,9 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
 
 export default class OrganizationTeamSection extends Component {
+  @service accessControl;
+
   options = [
     { value: 'ADMIN', label: 'Administrateur' },
     { value: 'MEMBER', label: 'Membre' },
