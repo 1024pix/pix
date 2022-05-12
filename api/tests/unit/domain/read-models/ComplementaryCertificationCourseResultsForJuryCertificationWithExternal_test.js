6c2ae45cadb1b37534e5af6aeda9e6dd5da4c371
Mathieu Gilet <mathieu.gilet@zenika.com>
2022-05-12 10:36:31 +0200

    :sparkles: api: add getter pixResult and externalResult

--- api/tests/unit/domain/read-models/ComplementaryCertificationCourseResultsForJuryCertificationWithExternal_test.js
+++ api/tests/unit/domain/read-models/ComplementaryCertificationCourseResultsForJuryCertificationWithExternal_test.js
@@ -289,4 +289,183 @@ describe('Unit | Domain | Models | ComplementaryCertificationCourseResultsForJur
       );
     });
   });
+
+  describe('#pixResult', function () {
+    context('when pix section is not evaluated', function () {
+      it('should return null', function () {
+        // given
+        const complementaryCertificationCourseResultsForJuryCertificationWithExternal =
+          new ComplementaryCertificationCourseResultsForJuryCertificationWithExternal({});
+
+        // when
+        const pixResult = complementaryCertificationCourseResultsForJuryCertificationWithExternal.pixResult;
+
+        // then
+        expect(pixResult).to.be.null;
+      });
+    });
+
+    context('when pix section is evaluated', function () {
+      [
+        {
+          partnerKey: PIX_EDU_FORMATION_INITIALE_2ND_DEGRE_INITIE,
+          acquired: true,
+          expectedResult: 'Pix+ Édu Initié (entrée dans le métier)',
+        },
+        {
+          partnerKey: PIX_EDU_FORMATION_INITIALE_2ND_DEGRE_CONFIRME,
+          acquired: true,
+          expectedResult: 'Pix+ Édu Confirmé',
+        },
+        {
+          partnerKey: PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_CONFIRME,
+          acquired: true,
+          expectedResult: 'Pix+ Édu Confirmé',
+        },
+        { partnerKey: PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_AVANCE, acquired: true, expectedResult: 'Pix+ Édu Avancé' },
+        { partnerKey: PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_EXPERT, acquired: true, expectedResult: 'Pix+ Édu Expert' },
+        {
+          partnerKey: PIX_EDU_FORMATION_INITIALE_1ER_DEGRE_INITIE,
+          acquired: true,
+          expectedResult: 'Pix+ Édu Initié (entrée dans le métier)',
+        },
+        {
+          partnerKey: PIX_EDU_FORMATION_INITIALE_1ER_DEGRE_CONFIRME,
+          acquired: true,
+          expectedResult: 'Pix+ Édu Confirmé',
+        },
+        {
+          partnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_CONFIRME,
+          acquired: true,
+          expectedResult: 'Pix+ Édu Confirmé',
+        },
+        { partnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_AVANCE, acquired: true, expectedResult: 'Pix+ Édu Avancé' },
+        { partnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_EXPERT, acquired: true, expectedResult: 'Pix+ Édu Expert' },
+        { partnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_EXPERT, acquired: false, expectedResult: 'Rejetée' },
+      ].forEach(({ partnerKey, acquired, expectedResult }) => {
+        it(`should return ${expectedResult} when pix section partner key is ${partnerKey} and acquired is ${acquired}`, function () {
+          // given
+          const complementaryCertificationCourseResultsForJuryCertificationWithExternal =
+            new ComplementaryCertificationCourseResultsForJuryCertificationWithExternal({
+              pixPartnerKey: partnerKey,
+              pixAcquired: acquired,
+            });
+
+          // when
+          const pixResult = complementaryCertificationCourseResultsForJuryCertificationWithExternal.pixResult;
+
+          // then
+          expect(pixResult).to.equal(expectedResult);
+        });
+      });
+    });
+  });
+
+  describe('#externalResult', function () {
+    context('when pix section is not acquired', function () {
+      it('should return "-"', function () {
+        // given
+        const complementaryCertificationCourseResultsForJuryCertificationWithExternal =
+          new ComplementaryCertificationCourseResultsForJuryCertificationWithExternal({
+            pixPartnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_CONFIRME,
+            pixAcquired: false,
+          });
+
+        // when
+        const externalResult = complementaryCertificationCourseResultsForJuryCertificationWithExternal.externalResult;
+
+        // then
+        expect(externalResult).to.equal('-');
+      });
+    });
+
+    context('when piw section is acquired and external section is not yet evaluated', function () {
+      it('should return "En attente"', function () {
+        // given
+        const complementaryCertificationCourseResultsForJuryCertificationWithExternal =
+          new ComplementaryCertificationCourseResultsForJuryCertificationWithExternal({
+            pixPartnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_CONFIRME,
+            pixAcquired: true,
+          });
+
+        // when
+        const externalResult = complementaryCertificationCourseResultsForJuryCertificationWithExternal.externalResult;
+
+        // then
+        expect(externalResult).to.equal('En attente');
+      });
+    });
+
+    context('when pix section is acquired and external section is evaluated', function () {
+      context('when acquired is true', function () {
+        [
+          {
+            partnerKey: PIX_EDU_FORMATION_INITIALE_2ND_DEGRE_INITIE,
+            expectedResult: 'Pix+ Édu Initié (entrée dans le métier)',
+          },
+          {
+            partnerKey: PIX_EDU_FORMATION_INITIALE_2ND_DEGRE_CONFIRME,
+            expectedResult: 'Pix+ Édu Confirmé',
+          },
+          {
+            partnerKey: PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_CONFIRME,
+            expectedResult: 'Pix+ Édu Confirmé',
+          },
+          { partnerKey: PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_AVANCE, expectedResult: 'Pix+ Édu Avancé' },
+          { partnerKey: PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_EXPERT, expectedResult: 'Pix+ Édu Expert' },
+          {
+            partnerKey: PIX_EDU_FORMATION_INITIALE_1ER_DEGRE_INITIE,
+            expectedResult: 'Pix+ Édu Initié (entrée dans le métier)',
+          },
+          {
+            partnerKey: PIX_EDU_FORMATION_INITIALE_1ER_DEGRE_CONFIRME,
+            expectedResult: 'Pix+ Édu Confirmé',
+          },
+          {
+            partnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_CONFIRME,
+            expectedResult: 'Pix+ Édu Confirmé',
+          },
+          { partnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_AVANCE, expectedResult: 'Pix+ Édu Avancé' },
+          { partnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_EXPERT, expectedResult: 'Pix+ Édu Expert' },
+        ].forEach(({ partnerKey, expectedResult }) => {
+          it(`should return ${expectedResult} when external section partner key is ${partnerKey}`, function () {
+            // given
+            const complementaryCertificationCourseResultsForJuryCertificationWithExternal =
+              new ComplementaryCertificationCourseResultsForJuryCertificationWithExternal({
+                pixPartnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_AVANCE,
+                pixAcquired: true,
+                externalPartnerKey: partnerKey,
+                externalAcquired: true,
+              });
+
+            // when
+            const externalResult =
+              complementaryCertificationCourseResultsForJuryCertificationWithExternal.externalResult;
+
+            // then
+            expect(externalResult).to.equal(expectedResult);
+          });
+        });
+      });
+
+      context('when acquired is false', function () {
+        it(`should return Rejetée`, function () {
+          // given
+          const complementaryCertificationCourseResultsForJuryCertificationWithExternal =
+            new ComplementaryCertificationCourseResultsForJuryCertificationWithExternal({
+              pixPartnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_AVANCE,
+              pixAcquired: true,
+              externalPartnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_AVANCE,
+              externalAcquired: false,
+            });
+
+          // when
+          const externalResult = complementaryCertificationCourseResultsForJuryCertificationWithExternal.externalResult;
+
+          // then
+          expect(externalResult).to.equal('Rejetée');
+        });
+      });
+    });
+  });
 });
