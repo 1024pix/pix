c3cec9f3d5281927a1b70f4afcc48fa30dcbf68a
yschoueri <yasmina.schoueri@gmail.com>
2022-05-13 12:21:30 +0200

    feat(admin): block CERTIF users from modifying a participant's external identifier

--- admin/app/components/campaigns/participation-row.js
+++ admin/app/components/campaigns/participation-row.js
@@ -5,6 +5,7 @@ import { inject as service } from '@ember/service';
 
 export default class ParticipationRow extends Component {
   @service notifications;
+  @service accessControl;
 
   @tracked isEditionMode = false;
   @tracked newParticipantExternalId;
