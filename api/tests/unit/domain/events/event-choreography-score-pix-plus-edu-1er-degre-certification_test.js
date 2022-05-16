e8cc9ee414718a3638c9d580cae103e99477d3d2
Mathieu Gilet <mathieu.gilet@zenika.com>
2022-05-16 11:35:04 +0200

    :sparkles: app: add handle-pix-plus-edu-1er-degre-certifications-scoring.js

---
+++ api/tests/unit/domain/events/event-choreography-score-pix-plus-edu-1er-degre-certification_test.js
@@ -0,0 +1,51 @@
+const { expect } = require('../../../test-helper');
+const buildEventDispatcherAndHandlersForTest = require('../../../tooling/events/event-dispatcher-builder');
+const AssessmentCompleted = require('../../../../lib/domain/events/AssessmentCompleted');
+const CertificationScoringCompleted = require('../../../../lib/domain/events/CertificationScoringCompleted');
+const CertificationRescoringCompleted = require('../../../../lib/domain/events/CertificationRescoringCompleted');
+
+describe('Event Choreography | Score Pix+ Édu 1er degre Certification', function () {
+  it('chains Certification Scoring and Pix+ Édu 1er degre Certification Scoring on Certification Scoring Completed', async function () {
+    // given
+    const { handlerStubs, eventDispatcher } = buildEventDispatcherAndHandlersForTest();
+
+    const domainTransaction = Symbol('a transaction');
+    const assessmentCompleted = new AssessmentCompleted();
+    const certificationScoringCompleted = new CertificationScoringCompleted({});
+
+    handlerStubs.handleCertificationScoring
+      .withArgs({ event: assessmentCompleted, domainTransaction })
+      .resolves(certificationScoringCompleted);
+
+    // when
+    await eventDispatcher.dispatch(certificationScoringCompleted);
+
+    // then
+    expect(handlerStubs.handlePixPlusEdu1erDegreCertificationsScoring).to.have.been.calledWith({
+      event: certificationScoringCompleted,
+      domainTransaction: undefined,
+    });
+  });
+
+  it('chains Certification Rescoring and Pix+ Édu 1er degre Certification Rescoring on Certification Rescoring Completed', async function () {
+    // given
+    const { handlerStubs, eventDispatcher } = buildEventDispatcherAndHandlersForTest();
+
+    const domainTransaction = Symbol('a transaction');
+    const assessmentCompleted = new AssessmentCompleted();
+    const certificationRescoringCompleted = new CertificationRescoringCompleted({});
+
+    handlerStubs.handleCertificationRescoring
+      .withArgs({ event: assessmentCompleted, domainTransaction })
+      .resolves(certificationRescoringCompleted);
+
+    // when
+    await eventDispatcher.dispatch(certificationRescoringCompleted);
+
+    // then
+    expect(handlerStubs.handlePixPlusEdu1erDegreCertificationsScoring).to.have.been.calledWith({
+      event: certificationRescoringCompleted,
+      domainTransaction: undefined,
+    });
+  });
+});
