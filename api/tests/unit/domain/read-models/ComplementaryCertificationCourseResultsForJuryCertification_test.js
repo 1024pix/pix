2357b0b4dfa3cd07c454a5dbc58dc3f65f67278d
Mathieu Gilet <mathieu.gilet@zenika.com>
2022-05-16 15:26:11 +0200

    :fire: app: remove unused badges from ComplementaryCertificationCourseResultsForJuryCertification

--- api/tests/unit/domain/read-models/ComplementaryCertificationCourseResultsForJuryCertification_test.js
+++ api/tests/unit/domain/read-models/ComplementaryCertificationCourseResultsForJuryCertification_test.js
@@ -1,22 +1,7 @@
 const { expect } = require('../../../test-helper');
 const ComplementaryCertificationCourseResultsForJuryCertification = require('../../../../lib/domain/read-models/ComplementaryCertificationCourseResultsForJuryCertification');
-const {
-  PIX_EMPLOI_CLEA_V1,
-  PIX_EMPLOI_CLEA_V2,
-  PIX_EMPLOI_CLEA_V3,
-  PIX_DROIT_MAITRE_CERTIF,
-  PIX_DROIT_EXPERT_CERTIF,
-  PIX_EDU_FORMATION_INITIALE_2ND_DEGRE_INITIE,
-  PIX_EDU_FORMATION_INITIALE_2ND_DEGRE_CONFIRME,
-  PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_CONFIRME,
-  PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_AVANCE,
-  PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_EXPERT,
-  PIX_EDU_FORMATION_INITIALE_1ER_DEGRE_INITIE,
-  PIX_EDU_FORMATION_INITIALE_1ER_DEGRE_CONFIRME,
-  PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_CONFIRME,
-  PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_AVANCE,
-  PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_EXPERT,
-} = require('../../../../lib/domain/models/Badge').keys;
+const { PIX_EMPLOI_CLEA_V1, PIX_EMPLOI_CLEA_V2, PIX_EMPLOI_CLEA_V3, PIX_DROIT_MAITRE_CERTIF, PIX_DROIT_EXPERT_CERTIF } =
+  require('../../../../lib/domain/models/Badge').keys;
 
 describe('Unit | Domain | Models | ComplementaryCertificationCourseResultsForJuryCertification', function () {
   describe('#label', function () {
@@ -27,22 +12,6 @@ describe('Unit | Domain | Models | ComplementaryCertificationCourseResultsForJur
       { partnerKey: PIX_EMPLOI_CLEA_V3, expectedLabel: 'CléA Numérique' },
       { partnerKey: PIX_DROIT_MAITRE_CERTIF, expectedLabel: 'Pix+ Droit Maître' },
       { partnerKey: PIX_DROIT_EXPERT_CERTIF, expectedLabel: 'Pix+ Droit Expert' },
-      {
-        partnerKey: PIX_EDU_FORMATION_INITIALE_2ND_DEGRE_INITIE,
-        expectedLabel: 'Pix+ Édu Initié (entrée dans le métier)',
-      },
-      { partnerKey: PIX_EDU_FORMATION_INITIALE_2ND_DEGRE_CONFIRME, expectedLabel: 'Pix+ Édu Confirmé' },
-      { partnerKey: PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_CONFIRME, expectedLabel: 'Pix+ Édu Confirmé' },
-      { partnerKey: PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_AVANCE, expectedLabel: 'Pix+ Édu Avancé' },
-      { partnerKey: PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_EXPERT, expectedLabel: 'Pix+ Édu Expert' },
-      {
-        partnerKey: PIX_EDU_FORMATION_INITIALE_1ER_DEGRE_INITIE,
-        expectedLabel: 'Pix+ Édu Initié (entrée dans le métier)',
-      },
-      { partnerKey: PIX_EDU_FORMATION_INITIALE_1ER_DEGRE_CONFIRME, expectedLabel: 'Pix+ Édu Confirmé' },
-      { partnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_CONFIRME, expectedLabel: 'Pix+ Édu Confirmé' },
-      { partnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_AVANCE, expectedLabel: 'Pix+ Édu Avancé' },
-      { partnerKey: PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_EXPERT, expectedLabel: 'Pix+ Édu Expert' },
     ].forEach(({ partnerKey, expectedLabel }) => {
       it(`should return ${expectedLabel} for partner key ${partnerKey}`, function () {
         // given
