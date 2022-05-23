a60a89d91b8e073dec99875fdea036330c3cff0a
Anne-G <ang.schall@gmail.com>
2022-05-23 19:18:00 +0200

    admin:sr: remove authentication in beforeEach

--- admin/tests/acceptance/authenticated/certifications/certification/neutralization_test.js
+++ admin/tests/acceptance/authenticated/certifications/certification/neutralization_test.js
@@ -8,13 +8,10 @@ module('Acceptance | Route | routes/authenticated/certifications/certification |
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  hooks.beforeEach(async function () {
-    await authenticateAdminMemberWithRole({ isSuperAdmin: true })(server);
-  });
-
   module('when there is no challenge for this certification', function () {
     test('it renders "Aucune épreuve posée"', async function (assert) {
       // given
+      await authenticateAdminMemberWithRole({ isSuperAdmin: true })(server);
       const certificationId = this.server.create('certification').id;
       this.server.create('certification-detail', {
         id: certificationId,
@@ -35,6 +32,7 @@ module('Acceptance | Route | routes/authenticated/certifications/certification |
     module('it renders a challenge list', function () {
       test('it renders as many rows as there are challenges', async function (assert) {
         // given
+        await authenticateAdminMemberWithRole({ isSuperAdmin: true })(server);
         const listChallengesAndAnswers = [
           {
             result: 'ok',
@@ -81,6 +79,7 @@ module('Acceptance | Route | routes/authenticated/certifications/certification |
 
       test('it renders the challenge info', async function (assert) {
         // given
+        await authenticateAdminMemberWithRole({ isSuperAdmin: true })(server);
         const listChallengesAndAnswers = [
           {
             result: 'ok',
@@ -109,6 +108,7 @@ module('Acceptance | Route | routes/authenticated/certifications/certification |
 
       test('it renders a "Neutraliser" button when challenge is not neutralized', async function (assert) {
         // given
+        await authenticateAdminMemberWithRole({ isSuperAdmin: true })(server);
         const listChallengesAndAnswers = [
           {
             result: 'ok',
@@ -137,6 +137,7 @@ module('Acceptance | Route | routes/authenticated/certifications/certification |
 
       test('it renders a "Dé-neutraliser" button when challenge is neutralized', async function (assert) {
         // given
+        await authenticateAdminMemberWithRole({ isSuperAdmin: true })(server);
         const listChallengesAndAnswers = [
           {
             result: 'ok',
@@ -165,6 +166,7 @@ module('Acceptance | Route | routes/authenticated/certifications/certification |
 
       test('it toggles the "Dé-neutraliser" button into a "Neutraliser" button when deneutralizing a neutralized challenge', async function (assert) {
         // given
+        await authenticateAdminMemberWithRole({ isSuperAdmin: true })(server);
         const listChallengesAndAnswers = [
           {
             result: 'ok',
@@ -194,6 +196,7 @@ module('Acceptance | Route | routes/authenticated/certifications/certification |
 
       test('it toggles the "Neutraliser" button into a "Dé-neutraliser" button when neutralizing a deneutralized challenge', async function (assert) {
         // given
+        await authenticateAdminMemberWithRole({ isSuperAdmin: true })(server);
         const listChallengesAndAnswers = [
           {
             result: 'ok',
@@ -224,6 +227,7 @@ module('Acceptance | Route | routes/authenticated/certifications/certification |
 
     test('it sort challenges by order property', async function (assert) {
       // given
+      await authenticateAdminMemberWithRole({ isSuperAdmin: true })(server);
       const listChallengesAndAnswers = [
         {
           result: 'ok',
