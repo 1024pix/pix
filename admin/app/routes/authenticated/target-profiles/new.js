dac445cd7197047d4abddfd8d172267715490b2c
Ismael Gorissen <ismael.gorissen@gmail.com>
2022-05-25 15:39:45 +0200

    feat(admin): restrict access to target profile creation page

--- admin/app/routes/authenticated/target-profiles/new.js
+++ admin/app/routes/authenticated/target-profiles/new.js
@@ -3,6 +3,11 @@ import { inject as service } from '@ember/service';
 
 export default class NewRoute extends Route {
   @service store;
+  @service accessControl;
+
+  beforeModel() {
+    this.accessControl.restrictAccessTo(['isSuperAdmin', 'isSupport', 'isMetier'], 'authenticated');
+  }
 
   model() {
     return this.store.createRecord('target-profile', { category: 'OTHER' });
