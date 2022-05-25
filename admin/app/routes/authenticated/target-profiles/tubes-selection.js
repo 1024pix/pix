b54cb1b4de81d1bb579061d6108c8cfe47bdd181
Ismael Gorissen <ismael.gorissen@gmail.com>
2022-05-25 15:39:14 +0200

    feat(admin): restrict access to tubes selection page

--- admin/app/routes/authenticated/target-profiles/tubes-selection.js
+++ admin/app/routes/authenticated/target-profiles/tubes-selection.js
@@ -3,6 +3,11 @@ import { inject as service } from '@ember/service';
 
 export default class TubesSelectionRoute extends Route {
   @service store;
+  @service accessControl;
+
+  beforeModel() {
+    this.accessControl.restrictAccessTo(['isSuperAdmin', 'isSupport', 'isMetier'], 'authenticated');
+  }
 
   async model() {
     const frameworks = await this.store.findAll('framework');
