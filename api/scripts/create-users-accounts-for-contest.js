37e6f34b7ef2cb160b3c080a6295442c56f41ae2
Anne-G <ang.schall@gmail.com>
2022-05-27 12:22:13 +0200

    api: add script to create user account for contest

---
+++ api/scripts/create-users-accounts-for-contest.js
@@ -0,0 +1,78 @@
+const { parseCsvWithHeader } = require('./helpers/csvHelpers');
+const bluebird = require('bluebird');
+const userToCreateRepository = require('../lib/infrastructure/repositories/user-to-create-repository');
+const authenticationMethodRepository = require('../lib/infrastructure/repositories/authentication-method-repository');
+const userService = require('../lib/domain/services/user-service');
+const encryptionService = require('../lib/domain/services/encryption-service');
+
+function prepareDataForInsert(rawUsers) {
+  return rawUsers.map(({ firstName, lastName, email, password }) => {
+    return {
+      firstName: firstName.trim(),
+      lastName: lastName.trim(),
+      email: email.trim().toLowerCase(),
+      password: password.trim(),
+    };
+  });
+}
+
+async function createUsers({ usersInRaw }) {
+  const now = new Date();
+  await bluebird.mapSeries(usersInRaw, async (userDTO) => {
+    const userToCreate = {
+      firstName: userDTO.firstName,
+      lastName: userDTO.lastName,
+      email: userDTO.email,
+      createAt: now,
+      updatedAt: now,
+      cgu: true,
+      lang: 'fr',
+    };
+    const hashedPassword = await encryptionService.hashPassword(userDTO.password);
+
+    await userService.createUserWithPassword({
+      user: userToCreate,
+      hashedPassword,
+      userToCreateRepository,
+      authenticationMethodRepository,
+    });
+  });
+}
+
+async function main() {
+  console.log('Starting creating users accounts for contest.');
+
+  try {
+    const filePath = process.argv[2];
+
+    console.log('Reading and parsing csv data file... ');
+    const csvData = await parseCsvWithHeader(filePath);
+    console.log('ok');
+
+    console.log('Preparing data... ');
+    const usersInRaw = prepareDataForInsert(csvData);
+    console.log('ok');
+
+    console.log('Creating users...');
+    await createUsers({ usersInRaw });
+    console.log('\nDone.');
+  } catch (error) {
+    console.error('\n', error);
+    process.exit(1);
+  }
+}
+
+if (require.main === module) {
+  main().then(
+    () => process.exit(0),
+    (err) => {
+      console.error(err);
+      process.exit(1);
+    }
+  );
+}
+
+module.exports = {
+  prepareDataForInsert,
+  createUsers,
+};
---
+++ api/tests/unit/scripts/create-users-accounts-for-contest_test.js
@@ -0,0 +1,34 @@
+const { expect } = require('../../test-helper');
+
+const { prepareDataForInsert } = require('../../../scripts/create-users-accounts-for-contest');
+
+describe('Unit | Scripts | create-users-accounts-for-contest.js', function () {
+  describe('#prepareDataForInsert', function () {
+    it('should trim firstName, lastName, email and password', function () {
+      // given
+      const data = [
+        {
+          firstName: '  Salim    ',
+          lastName: ' Bienléongle     ',
+          email: '  salim@example.net  ',
+          password: '     pix123   ',
+        },
+        { firstName: '  Samantha      ', lastName: '  Lo ', email: '  lo@example.net  ', password: '     pixou123   ' },
+      ];
+
+      // when
+      const result = prepareDataForInsert(data);
+
+      // then
+      expect(result).to.deep.equal([
+        {
+          firstName: 'Salim',
+          lastName: 'Bienléongle',
+          email: 'salim@example.net',
+          password: 'pix123',
+        },
+        { firstName: 'Samantha', lastName: 'Lo', email: 'lo@example.net', password: 'pixou123' },
+      ]);
+    });
+  });
+});
