ed5c619f3c5a0b0d90e804b52c66e95493571bf7
Marianne Bost <marianne.bost@pix.fr>
2022-05-30 16:32:32 +0200

    :sparkles: app: display ended by finalization error

--- mon-pix/app/controllers/certifications/results.js
+++ mon-pix/app/controllers/certifications/results.js
@@ -1,7 +1,12 @@
 import Controller from '@ember/controller';
+import { assessmentStates } from 'mon-pix/models/assessment';
 
 export default class CertificationResultsController extends Controller {
   get isEndedBySupervisor() {
-    return this.model.assessment.get('state') === 'endedBySupervisor';
+    return this.model.assessment.get('state') === assessmentStates.ENDED_BY_SUPERVISOR;
+  }
+
+  get isEndedByFinalization() {
+    return this.model.assessment.get('state') === assessmentStates.ENDED_BY_FINALIZATION;
   }
 }
