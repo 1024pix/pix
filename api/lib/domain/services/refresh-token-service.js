c7e7e8142720cab75a8419e5bebd3e5417889b91
Anne-G <ang.schall@gmail.com>
2022-06-02 15:06:48 +0200

    api: include userId in refreshtoken value

--- api/lib/domain/services/refresh-token-service.js
+++ api/lib/domain/services/refresh-token-service.js
@@ -6,7 +6,7 @@ const { v4: uuidv4 } = require('uuid');
 
 async function createRefreshTokenFromUserId({ userId, source }) {
   const expirationDelaySeconds = settings.authentication.refreshTokenLifespanMs / 1000;
-  const token = uuidv4();
+  const token = `${userId}:${uuidv4()}`;
   return await temporaryStorage.save({
     key: token,
     value: { type: 'refresh_token', userId, source },
