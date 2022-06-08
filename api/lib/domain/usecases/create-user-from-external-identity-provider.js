16516d491bed3f8a96fecca092aa8940b79922e5
Clément Bourgoin <clement.bourgoin@pix.fr>
2022-06-08 16:05:45 +0200

    refactor(api): merge createUserFromCnav/PoleEmploi use cases into a generic one
    Co-authored-by: Andreia Pena <andreia.pena@pix.fr>

---
+++ api/lib/domain/usecases/create-user-from-external-identity-provider.js
@@ -0,0 +1,54 @@
+const UserToCreate = require('../models/UserToCreate');
+const {
+  InvalidExternalAPIResponseError,
+  AuthenticationKeyExpired,
+  UserAlreadyExistsWithAuthenticationMethodError,
+} = require('../errors');
+const logger = require('../../infrastructure/logger');
+
+module.exports = async function createUserFromExternalIdentityProvider({
+  identityProvider,
+  authenticationKey,
+  authenticationSessionService,
+  authenticationServiceRegistry,
+  authenticationMethodRepository,
+  userToCreateRepository,
+}) {
+  const sessionContent = await authenticationSessionService.getByKey(authenticationKey);
+  if (!sessionContent) {
+    throw new AuthenticationKeyExpired();
+  }
+  const selectedAuthenticationService = await authenticationServiceRegistry.lookupAuthenticationService(
+    identityProvider
+  );
+  const userInfo = await selectedAuthenticationService.getUserInfo(sessionContent);
+
+  if (!userInfo.firstName || !userInfo.lastName || !userInfo.externalIdentityId) {
+    logger.error(`Un des champs obligatoires n'a pas été renvoyé : ${JSON.stringify(userInfo)}.`);
+    throw new InvalidExternalAPIResponseError('Les informations utilisateurs récupérées sont incorrectes.');
+  }
+
+  const authenticationMethod = await authenticationMethodRepository.findOneByExternalIdentifierAndIdentityProvider({
+    externalIdentifier: userInfo.externalIdentityId,
+    identityProvider,
+  });
+
+  if (authenticationMethod) {
+    throw new UserAlreadyExistsWithAuthenticationMethodError(
+      'Authentication method already exists for this external identifier.'
+    );
+  }
+
+  const user = UserToCreate.createWithTermsOfServiceAccepted({
+    firstName: userInfo.firstName,
+    lastName: userInfo.lastName,
+  });
+
+  return await selectedAuthenticationService.createUserAccount({
+    user,
+    sessionContent,
+    externalIdentityId: userInfo.externalIdentityId,
+    userToCreateRepository,
+    authenticationMethodRepository,
+  });
+};
