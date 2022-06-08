3d69608fa813f4d99fb192b354949011ab3f8d87
Thomasevano <thomas.evano@pix.fr>
2022-06-08 11:17:08 +0200

    Add target-profile-controller integration test
    Co-authored-by: Nicolas Lepage <nicolas.lepage@pix.fr>
    Co-authored-by: Lucas Herrador <lucas.herrador@pix.fr>

--- api/tests/integration/application/target-profile/index_test.js
+++ api/tests/integration/application/target-profile/index_test.js
@@ -5,14 +5,18 @@ const targetProfilesRouter = require('../../../../lib/application/target-profile
 const { NotFoundError } = require('../../../../lib/domain/errors');
 
 describe('Integration | Application | Route | target-profile-router', function () {
+  let httpTestServer;
+  beforeEach(async function () {
+    httpTestServer = new HttpTestServer();
+    await httpTestServer.register(targetProfilesRouter);
+    sinon.stub(securityPreHandlers, 'userHasAtLeastOneAccessOf').returns(() => true);
+  });
+
   describe('GET /api/admin/target-profile-templates/{id}', function () {
-    let httpTestServer, targetProfileTemplateId, targetProfileTemplate;
+    let targetProfileTemplateId, targetProfileTemplate;
     const targetProfileTemplateNotFoundId = 456;
 
     beforeEach(async function () {
-      httpTestServer = new HttpTestServer();
-      await httpTestServer.register(targetProfilesRouter);
-      sinon.stub(securityPreHandlers, 'userHasAtLeastOneAccessOf').returns(() => true);
       targetProfileTemplate = domainBuilder.buildTargetProfileTemplate();
     });
 
@@ -77,6 +81,118 @@ describe('Integration | Application | Route | target-profile-router', function (
 
         // then
         expect(response.statusCode).to.equal(400);
+      });
+    });
+  });
+
+  describe('GET api/admin/target-profiles/{id}', function () {
+    it('should return a target profile with template', async function () {
+      // given
+      const targetProfile = domainBuilder.buildTargetProfileWithLearningContent({
+        id: 1,
+        name: 'target-profile',
+        createdAt: '2021-04-30',
+        outdated: '2024-05-23',
+        isPublic: false,
+        ownerOrganizationId: 2,
+        description: 'description',
+        comment: 'comment',
+        imageUrl: 'imageUrl',
+        skills: [],
+        tubes: [],
+        competences: [],
+        areas: [],
+        isSimplifiedAccess: false,
+        template: domainBuilder.buildTargetProfileTemplate({ id: 123 }),
+      });
+
+      sinon
+        .stub(usecases, 'getTargetProfileDetails')
+        .withArgs({ targetProfileId: targetProfile.id })
+        .resolves(targetProfile);
+
+      // when
+      const response = await httpTestServer.request('GET', `/api/admin/target-profiles/${targetProfile.id}`);
+
+      // then
+      expect(response.statusCode).to.equal(200);
+      expect(response.result.data.id).to.equal('1');
+      expect(response.result.data.attributes).to.deep.equal({
+        name: 'target-profile',
+        'created-at': '2021-04-30',
+        outdated: '2024-05-23',
+        'is-public': false,
+        'owner-organization-id': 2,
+        description: 'description',
+        category: 'OTHER',
+        comment: 'comment',
+        'image-url': 'imageUrl',
+        'is-simplified-access': false,
+      });
+      expect(response.result.data.relationships.template).to.deep.equal({
+        data: {
+          id: '123',
+          type: 'target-profile-templates',
+        },
+      });
+    });
+
+    it('should return a target profile without template', async function () {
+      // given
+      const targetProfile = domainBuilder.buildTargetProfileWithLearningContent({
+        id: 1,
+        name: 'target-profile',
+        createdAt: '2021-04-30',
+        outdated: '2024-05-23',
+        isPublic: false,
+        ownerOrganizationId: 2,
+        description: 'description',
+        comment: 'comment',
+        imageUrl: 'imageUrl',
+        skills: [],
+        tubes: [],
+        competences: [],
+        areas: [],
+        isSimplifiedAccess: false,
+        template: null,
+      });
+
+      sinon
+        .stub(usecases, 'getTargetProfileDetails')
+        .withArgs({ targetProfileId: targetProfile.id })
+        .resolves(targetProfile);
+
+      // when
+      const response = await httpTestServer.request('GET', `/api/admin/target-profiles/${targetProfile.id}`);
+
+      // then
+      expect(response.statusCode).to.equal(200);
+      expect(response.result.data.id).to.equal('1');
+      expect(response.result.data.attributes).to.deep.equal({
+        name: 'target-profile',
+        'created-at': '2021-04-30',
+        outdated: '2024-05-23',
+        'is-public': false,
+        'owner-organization-id': 2,
+        description: 'description',
+        category: 'OTHER',
+        comment: 'comment',
+        'image-url': 'imageUrl',
+        'is-simplified-access': false,
+      });
+      expect(response.result.data.relationships.template).to.deep.equal({ data: null });
+    });
+
+    describe('when target profile id is not found', function () {
+      it('should return 404 status code', async function () {
+        // given
+        sinon.stub(usecases, 'getTargetProfileDetails').withArgs({ targetProfileId: 666 }).rejects(new NotFoundError());
+
+        // when
+        const response = await httpTestServer.request('GET', `/api/admin/target-profiles/666`);
+
+        // then
+        expect(response.statusCode).to.equal(404);
       });
     });
   });
