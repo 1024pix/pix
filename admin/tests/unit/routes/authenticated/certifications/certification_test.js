63ae14ed3c0accece21afd08af8b74da0f1f23c2
Anne-G <ang.schall@gmail.com>
2022-06-10 12:07:17 +0200

    admin: prevent metier to access certif info page

--- admin/tests/unit/routes/authenticated/certifications/certification_test.js
+++ admin/tests/unit/routes/authenticated/certifications/certification_test.js
@@ -1,6 +1,7 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import sinon from 'sinon';
+import Service from '@ember/service';
 
 module('Unit | Route | authenticated/certifications/certification', function (hooks) {
   setupTest(hooks);
@@ -18,6 +19,25 @@ module('Unit | Route | authenticated/certifications/certification', function (ho
     // TODO: Fix this the next time the file is edited.
     // eslint-disable-next-line qunit/no-assert-equal
     assert.equal(certifications.inputId, id);
+  });
+
+  module('#beforeModel', function () {
+    test('it should check if current user is super admin, certif, or support', function (assert) {
+      // given
+      const route = this.owner.lookup('route:authenticated/certifications/certification');
+
+      const restrictAccessToStub = sinon.stub().returns();
+      class AccessControlStub extends Service {
+        restrictAccessTo = restrictAccessToStub;
+      }
+      this.owner.register('service:access-control', AccessControlStub);
+
+      // when
+      route.beforeModel();
+
+      // then
+      assert.ok(restrictAccessToStub.calledWith(['isSuperAdmin', 'isCertif', 'isSupport'], 'authenticated'));
+    });
   });
 
   test('#error', function (assert) {
