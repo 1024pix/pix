e74060385f3fa5866b1496f4976de51cd0748a16
P-Jeremy <jemyplu@gmail.com>
2022-06-13 17:14:11 +0200

    :sparkles: mon-pix: add #shouldDisplayProfessionalizingWarning to Certification

--- mon-pix/tests/unit/models/certification_test.js
+++ mon-pix/tests/unit/models/certification_test.js
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupTest } from 'ember-mocha';
 import { ACQUIRED } from 'mon-pix/models/certification';
+import Service from '@ember/service';
 
 describe('Unit | Model | certification', function () {
   setupTest();
@@ -35,6 +36,53 @@ describe('Unit | Model | certification', function () {
     it('should be false when certification has no certified badge image', function () {
       const model = store.createRecord('certification', { certifiedBadgeImages: [] });
       expect(model.hasAcquiredComplementaryCertifications).to.be.false;
+    });
+  });
+
+  describe('#shouldDisplayProfessionalizingWarning', function () {
+    context('when domain is french', function () {
+      beforeEach(function () {
+        class UrlServiceStub extends Service {
+          get isFrenchDomainExtension() {
+            return true;
+          }
+        }
+
+        this.owner.register('service:url', UrlServiceStub);
+      });
+
+      it('should be true when deliveredAt >= 2022-01-01 ', function () {
+        // given
+        const model = store.createRecord('certification', { deliveredAt: '2022-01-01' });
+
+        // when / then
+        expect(model.shouldDisplayProfessionalizingWarning).to.be.true;
+      });
+
+      it('should be false when when deliveredAt < 2022-01-01', function () {
+        // given
+        const model = store.createRecord('certification', { deliveredAt: '2021-01-01' });
+
+        // when / then
+        expect(model.shouldDisplayProfessionalizingWarning).to.be.false;
+      });
+    });
+
+    context('when domain is not french', function () {
+      it('should be false', function () {
+        // given
+        class UrlServiceStub extends Service {
+          get isFrenchDomainExtension() {
+            return false;
+          }
+        }
+
+        this.owner.register('service:url', UrlServiceStub);
+        const model = store.createRecord('certification', { deliveredAt: '2022-01-01' });
+
+        // when / then
+        expect(model.shouldDisplayProfessionalizingWarning).to.be.false;
+      });
     });
   });
 });
