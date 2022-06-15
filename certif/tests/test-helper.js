cd0d25e0cb6720fe15588318521c88f56f14c257
Antoine Ceol <antoine.ceol@gmail.com>
2022-06-15 08:26:10 +0200

    :arrow_up: certif: Bump ember-cli
    npx ember-cli-update --to 3.28.5
    Update ember-cli for Node 16 compatibility

--- certif/tests/test-helper.js
+++ certif/tests/test-helper.js
@@ -1,6 +1,8 @@
 import Application from 'pix-certif/app';
 import config from 'pix-certif/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 
 import NotificationMessageService from 'ember-cli-notifications/services/notifications';
@@ -17,5 +19,7 @@ NotificationMessageService.reopen({
 });
 
 setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
 
 start();
