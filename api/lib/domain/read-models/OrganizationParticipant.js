6d1e034a28eeecc1dad39e67eb9fbed846c1d9e6
Yvonnick Frin <frin.yvonnick@gmail.com>
2022-06-20 12:23:40 +0200

    feat(repository) Add method to get participants from an organization

---
+++ api/lib/domain/read-models/OrganizationParticipant.js
@@ -0,0 +1,9 @@
+class OrganizationParticipant {
+  constructor({ id, firstName, lastName } = {}) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+
+module.exports = OrganizationParticipant;
