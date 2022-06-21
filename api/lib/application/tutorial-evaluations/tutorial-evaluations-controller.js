713f267771b036f2570832db0f7e23590cef3c6a
alexandrecoin <alexandre.pc.coin@gmail.com>
2022-06-21 11:28:27 +0200

    feat(api): add default status value for retro-compatibility
    Co-authored-by: Alexandre Coin <alexandre.coin@pix.fr>
    Co-authored-by: Clarisse Damon <clarisse.damon@pix.fr>
    Co-authored-by: Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
    Co-authored-by: Matthias Ferraina <matthias.ferraina@pix.fr>
    Co-authored-by: Vincent Hardouin <vincent.hardouin@pix.fr>

--- api/lib/application/tutorial-evaluations/tutorial-evaluations-controller.js
+++ api/lib/application/tutorial-evaluations/tutorial-evaluations-controller.js
@@ -1,11 +1,12 @@
 const usecases = require('../../domain/usecases');
 const tutorialEvaluationSerializer = require('../../infrastructure/serializers/jsonapi/tutorial-evaluation-serializer');
+const TutorialEvaluation = require('../../../lib/domain/models/TutorialEvaluation');
 
 module.exports = {
   async evaluate(request, h) {
     const { userId } = request.auth.credentials;
     const { tutorialId } = request.params;
-    const { status } = tutorialEvaluationSerializer.deserialize(request.payload);
+    const { status = TutorialEvaluation.status.LIKED } = tutorialEvaluationSerializer.deserialize(request.payload);
 
     const tutorialEvaluation = await usecases.addTutorialEvaluation({ userId, tutorialId, status });
 
