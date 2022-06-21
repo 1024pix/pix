95cac45c5168660bbc1fb5759adddf6d6495d9d0
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-06-21 13:55:03 +0200

    fix(mon-pix): resolve promise before returning in profile route

--- mon-pix/app/routes/profile.js
+++ mon-pix/app/routes/profile.js
@@ -1,17 +1,18 @@
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 import SecuredRouteMixin from 'mon-pix/mixins/secured-route-mixin';
 import Route from '@ember/routing/route';
 
 export default class ProfileRoute extends Route.extend(SecuredRouteMixin) {
   @service currentUser;
 
-  model() {
+  async model() {
+    await this.currentUser.user.belongsTo('profile').reload();
     return this.currentUser.user;
   }
 
-  async afterModel(user) {
-    // This reloads are necessary to keep the ui in sync when the
-    // user navigates back to this route
-    user.belongsTo('profile').reload();
+  @action
+  loading() {
+    return false;
   }
 }
