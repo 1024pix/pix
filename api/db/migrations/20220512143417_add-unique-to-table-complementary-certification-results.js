c98c59f1dd458fa7be8ef8c6f8fc317b6680d665
Matthias FERRAINA <matthias.ferraina@pix.fr>
2022-06-23 17:41:05 +0200

    tech(api): migrate file to typescript

--- api/db/migrations/20220512143417_add-unique-to-table-complementary-certification-results.js
+++ api/db/migrations/20220512143417_add-unique-to-table-complementary-certification-results.js
@@ -1,7 +1,7 @@
 const TABLE_NAME = 'complementary-certification-course-results';
 const CONSTRAINT_NAME = 'complementary-certification-course-results-cccrId-source-unique';
 
-exports.up = async (knex) => {
+exports.up = (knex) => {
   return knex.schema.alterTable(TABLE_NAME, (table) => {
     table.unique(['complementaryCertificationCourseId', 'source'], CONSTRAINT_NAME);
   });
