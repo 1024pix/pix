3f2d641d00e50dd1b04bc54a089631f0a0c310ff
Lise Quesnel <quesnel.lise@gmail.com>
2022-06-23 14:37:24 +0200

    refactor(api): deprecate use of /organizations/id/schooling-registrations/import-csv
    in favor of /api/organizations/id/sup-organization-learners/import-csv

--- api/lib/application/organizations/organization-controller.js
+++ api/lib/application/organizations/organization-controller.js
@@ -251,8 +251,18 @@ module.exports = {
     const supOrganizationLearnerParser = new SupOrganizationLearnerParser(buffer, organizationId, request.i18n);
     const warnings = await usecases.importSupOrganizationLearners({ supOrganizationLearnerParser });
 
-    const response = supOrganizationLearnerWarningSerializer.serialize({ id: organizationId, warnings });
-    return h.response(response).code(200);
+    const response = h
+      .response(supOrganizationLearnerWarningSerializer.serialize({ id: organizationId, warnings }))
+      .code(200);
+    if (h.request.path === `/api/organizations/${request.params.id}/schooling-registrations/import-csv`) {
+      response
+        .header('Deprecation', 'true')
+        .header(
+          'Link',
+          `/api/organizations/${request.params.id}/sup-organization-learners/import-csv; rel="successor-version"`
+        );
+    }
+    return response;
   },
 
   async replaceSupOrganizationLearners(request, h) {
