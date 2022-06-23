9046c998db983b2de42a0cbf8c9a53afac41c8e2
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2022-06-23 15:27:29 +0200

    Move submitting state into form component
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>
    Co-authored-by: Lucas Herrador <lucas.herrador@pix.fr>

--- admin/app/controllers/authenticated/target-profiles/new.js
+++ admin/app/controllers/authenticated/target-profiles/new.js
@@ -2,14 +2,10 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
 
-import { tracked } from '@glimmer/tracking';
-
 export default class NewController extends Controller {
   @service notifications;
   @service store;
   @service router;
-
-  @tracked isSaving = false;
 
   @action
   goBackToTargetProfileList() {
@@ -29,15 +25,12 @@ export default class NewController extends Controller {
     }
 
     try {
-      this.isSaving = true;
       await targetProfile.save();
 
       this.notifications.success('Le profil cible a été créé avec succès.');
       this.router.transitionTo('authenticated.target-profiles.target-profile', targetProfile.id);
     } catch (error) {
       this._handleResponseError(error);
-    } finally {
-      this.isSaving = false;
     }
   }
 
