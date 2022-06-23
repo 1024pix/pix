6533fb29049fdc67304a9207ea8b5c853d4d04c1
Lise Quesnel <quesnel.lise@gmail.com>
2022-06-23 12:08:51 +0200

    refactor(api): deprecate use of /organizations/id/schooling-registrations/csv-template
    in favor of /api/organizations/id/sup-organization-learners/csv-template

--- api/lib/application/organizations/organization-controller.js
+++ api/lib/application/organizations/organization-controller.js
@@ -304,10 +304,19 @@ module.exports = {
       i18n: request.i18n,
     });
 
-    return h
+    const response = h
       .response(template)
       .header('Content-Type', 'text/csv;charset=utf-8')
       .header('Content-Disposition', `attachment; filename=${request.i18n.__('csv-template.template-name')}.csv`);
+    if (h.request.path === `/api/organizations/${request.params.id}/schooling-registrations/csv-template`) {
+      response
+        .header('Deprecation', 'true')
+        .header(
+          'Link',
+          `/api/organizations/${request.params.id}/sup-organization-learners/csv-template; rel="successor-version"`
+        );
+    }
+    return response;
   },
 
   async archiveOrganization(request) {
