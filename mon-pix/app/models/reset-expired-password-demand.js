d0cf62ca17b898b05d737d01034056803ee30f84
Andreia Pena <andreia.pena@pix.fr>
2022-06-24 15:06:00 +0200

    ✨(mon-pix): Replace values for passwordResetToken in model

--- mon-pix/app/models/reset-expired-password-demand.js
+++ mon-pix/app/models/reset-expired-password-demand.js
@@ -1,7 +1,5 @@
 import Model, { attr } from '@ember-data/model';
 
 export default class ResetExpiredPasswordDemand extends Model {
-  @attr('string') username;
-  @attr('string') newPassword;
-  @attr('string') oneTimePassword;
+  @attr('string') passwordResetToken;
 }
