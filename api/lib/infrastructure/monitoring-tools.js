a08a9ba2c4ab5b4915b692cbc1fea7a744b00fff
Laura Bergoens <laura.bergoens@pix.fr>
2022-06-24 11:24:15 +0200

    fix(api) change compared version of Hapi

--- api/lib/infrastructure/monitoring-tools.js
+++ api/lib/infrastructure/monitoring-tools.js
@@ -88,7 +88,7 @@ function pushInContext(path, value) {
 function installHapiHook() {
   if (!settings.hapi.enableRequestMonitoring) return;
 
-  if (require('@hapi/hapi/package.json').version !== '20.2.1') {
+  if (require('@hapi/hapi/package.json').version !== '20.2.2') {
     throw new Error('Hapi version changed, please check if patch still works');
   }
 
