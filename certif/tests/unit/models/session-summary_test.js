c7e08d98fc197cd26b62e3be520ffa9b26bfe06c
Quentin Lebouc <quentin.lebouc@pix.fr>
2022-06-29 14:22:22 +0200

    :recyle: certif: SR: Remove unnecessary Run functions
    Co-authored-by: Antoine Ceol <antoine.ceol@gmail.com>
    Co-authored-by: Marianne Bost <marianne.bost@pix.fr>
    Co-authored-by: Mathieu Gilet <mathieu.gilet@zenika.com>
    Co-authored-by: Pierre Top <ptop@octo.com>
    Co-authored-by: Alexandre Coin <alexandre.coin@pix.fr>

--- certif/tests/unit/models/session-summary_test.js
+++ certif/tests/unit/models/session-summary_test.js
@@ -1,6 +1,5 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
-import { run } from '@ember/runloop';
 
 module('Unit | Model | session-summary', function (hooks) {
   setupTest(hooks);
@@ -9,12 +8,10 @@ module('Unit | Model | session-summary', function (hooks) {
     test('it should return "Finalisée" when status is finalized', function (assert) {
       // given
       const store = this.owner.lookup('service:store');
-      const sessionSummary = run(() =>
-        store.createRecord('session-summary', {
-          id: 123,
-          status: 'finalized',
-        })
-      );
+      const sessionSummary = store.createRecord('session-summary', {
+        id: 123,
+        status: 'finalized',
+      });
 
       // when/then
       assert.strictEqual(sessionSummary.statusLabel, 'Finalisée');
@@ -23,12 +20,10 @@ module('Unit | Model | session-summary', function (hooks) {
     test('it should return "Résultats transmis par Pix" when status is processed', function (assert) {
       // given
       const store = this.owner.lookup('service:store');
-      const sessionSummary = run(() =>
-        store.createRecord('session-summary', {
-          id: 123,
-          status: 'processed',
-        })
-      );
+      const sessionSummary = store.createRecord('session-summary', {
+        id: 123,
+        status: 'processed',
+      });
 
       // when/then
       assert.strictEqual(sessionSummary.statusLabel, 'Résultats transmis par Pix');
@@ -37,12 +32,10 @@ module('Unit | Model | session-summary', function (hooks) {
     test('it should return Créée else', function (assert) {
       // given
       const store = this.owner.lookup('service:store');
-      const sessionSummary = run(() =>
-        store.createRecord('session-summary', {
-          id: 123,
-          status: 'created',
-        })
-      );
+      const sessionSummary = store.createRecord('session-summary', {
+        id: 123,
+        status: 'created',
+      });
 
       // when/then
       assert.strictEqual(sessionSummary.statusLabel, 'Créée');
@@ -50,34 +43,25 @@ module('Unit | Model | session-summary', function (hooks) {
   });
 
   module('#hasEffectiveCandidates', function () {
-    module('when at least one candidate has joined the session', function () {
-      test('it should return true', function (assert) {
-        // given
-        const store = this.owner.lookup('service:store');
-        const sessionSummary = run(() =>
-          store.createRecord('session-summary', {
-            effectiveCandidatesCount: 2,
-          })
-        );
-
-        // when/then
-        assert.true(sessionSummary.hasEffectiveCandidates);
+    test('it should return true when at least one candidate has joined the session', function (assert) {
+      // given
+      const store = this.owner.lookup('service:store');
+      const sessionSummary = store.createRecord('session-summary', {
+        effectiveCandidatesCount: 2,
       });
+
+      // when/then
+      assert.true(sessionSummary.hasEffectiveCandidates);
     });
-
-    module('when no candidate has joined the session', function () {
-      test('it should return false ', function (assert) {
-        // given
-        const store = this.owner.lookup('service:store');
-        const sessionSummary = run(() =>
-          store.createRecord('session-summary', {
-            effectiveCandidatesCount: 0,
-          })
-        );
-
-        // when/then
-        assert.false(sessionSummary.hasEffectiveCandidates);
+    test('it should return false when no candidate has joined the session', function (assert) {
+      // given
+      const store = this.owner.lookup('service:store');
+      const sessionSummary = store.createRecord('session-summary', {
+        effectiveCandidatesCount: 0,
       });
+
+      // when/then
+      assert.false(sessionSummary.hasEffectiveCandidates);
     });
   });
 });
