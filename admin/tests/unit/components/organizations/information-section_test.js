8a658a115e3519fba9534b3e62cb275836b0646b
Anne-G <ang.schall@gmail.com>
2022-06-29 11:28:25 +0200

    admin:sr: simply unit test

--- admin/tests/unit/components/organizations/information-section_test.js
+++ admin/tests/unit/components/organizations/information-section_test.js
@@ -6,25 +6,13 @@ import ENV from 'pix-admin/config/environment';
 module('Unit | Component | organizations/information-section', function (hooks) {
   setupTest(hooks);
 
-  let component;
-
-  hooks.beforeEach(function () {
-    component = createGlimmerComponent('component:organizations/information-section');
-  });
-
   test('it should generate link based on environment and object', async function (assert) {
     // given
-    const args = { organization: { id: 1 } };
-    const baseURL = 'https://metabase.pix.fr/dashboard/137/?id=';
-    const expectedURL = baseURL + args.organization.id;
+    const component = createGlimmerComponent('component:organizations/information-section');
+    ENV.APP.ORGANIZATION_DASHBOARD_URL = 'https://metabase.pix.fr/dashboard/137/?id=';
+    component.args = { organization: { id: 1 } };
 
-    ENV.APP.ORGANIZATION_DASHBOARD_URL = baseURL;
-    component.args = args;
-
-    // when
-    // then
-    // TODO: Fix this the next time the file is edited.
-    // eslint-disable-next-line qunit/no-assert-equal
-    assert.equal(component.externalURL, expectedURL);
+    // when & then
+    assert.strictEqual(component.externalURL, 'https://metabase.pix.fr/dashboard/137/?id=1');
   });
 });
