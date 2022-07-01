43188a12b5a795fac20f7c86a5414bef613ee825
P-Jeremy <jemyplu@gmail.com>
2022-07-01 14:35:56 +0200

    :sparkles: api: add locale to getPrivateCertificate usecase

--- api/lib/domain/usecases/certificate/get-private-certificate.js
+++ api/lib/domain/usecases/certificate/get-private-certificate.js
@@ -1,7 +1,12 @@
 const { NotFoundError } = require('../../errors');
 
-module.exports = async function getPrivateCertificate({ certificationId, userId, privateCertificateRepository }) {
-  const privateCertificate = await privateCertificateRepository.get(certificationId);
+module.exports = async function getPrivateCertificate({
+  certificationId,
+  userId,
+  locale,
+  privateCertificateRepository,
+}) {
+  const privateCertificate = await privateCertificateRepository.get(certificationId, { locale });
   if (privateCertificate.userId !== userId) {
     throw new NotFoundError();
   }
