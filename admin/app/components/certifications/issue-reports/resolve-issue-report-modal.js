babb26ad05b2a0a696763d234e151a2feb5db605
Pierre TOP (TOPI) <ptop@octo.com>
2022-07-04 10:36:42 +0200

    :sparkles: admin: prevent empty string on resolution update

--- admin/app/components/certifications/issue-reports/resolve-issue-report-modal.js
+++ admin/app/components/certifications/issue-reports/resolve-issue-report-modal.js
@@ -1,15 +1,19 @@
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
-import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
 export default class CertificationIssueReportModal extends Component {
-  @service notifications;
-
   @tracked label = null;
+  @tracked errorMessage = null;
 
   @action
   resolve() {
+    if (this._isInvalid()) {
+      this.errorMessage = 'Le motif de résolution doit être renseigné.';
+      return;
+    }
+
+    this.errorMessage = null;
     this.args.resolveIssueReport(this.args.issueReport, this.label);
     this.args.closeResolveModal();
   }
@@ -21,5 +25,9 @@ export default class CertificationIssueReportModal extends Component {
 
   get actionName() {
     return this.args.issueReport.isResolved ? 'Modifier le signalement' : 'Résoudre ce signalement';
+  }
+
+  _isInvalid() {
+    return this.args.issueReport.isResolved && !this.label?.trim();
   }
 }
