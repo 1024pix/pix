f84b90f84c32af3a2b6a9ee0a79b9c4b6ab554a0
Clarisse Damon <clarisse.damon@pix.fr>
2022-07-07 16:12:58 +0200

    feat(mon-pix): add filter button
    Co-authored-by: Matthias Ferraina <matthias.ferraina@pix.fr>
    Co-authored-by: Vincent Hardouin <vincent.hardouin@pix.fr>

--- mon-pix/tests/integration/components/tutorials/header_test.js
+++ mon-pix/tests/integration/components/tutorials/header_test.js
@@ -1,9 +1,10 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
-import { find, render, findAll } from '@ember/test-helpers';
+import { find, findAll } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import setupIntlRenderingTest from '../../../helpers/setup-intl-rendering';
 import Service from '@ember/service';
+import { render } from '@1024pix/ember-testing-library';
 
 describe('Integration | Component | Tutorials | Header', function () {
   setupIntlRenderingTest();
@@ -28,5 +29,19 @@ describe('Integration | Component | Tutorials | Header', function () {
     expect(find('a.pix-choice-chip,a.pix-choice-chip--active'))
       .to.have.property('textContent')
       .that.contains('Recommandés');
+  });
+
+  it('should render filter button when tutorial filter feature toggle is activate', async function () {
+    // given
+    class FeatureTogglesStub extends Service {
+      featureToggles = { isPixAppTutoFiltersEnabled: true };
+    }
+    this.owner.register('service:featureToggles', FeatureTogglesStub);
+
+    // when
+    const screen = await render(hbs`<Tutorials::Header />`);
+
+    // then
+    expect(screen.getByRole('button', { name: 'Filtrer' })).to.exist;
   });
 });
