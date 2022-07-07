e379f859793e236ec6b2696de220226ebd95bee8
Anne-Marie Esteves <anne-marie.esteves@octo.com>
2022-07-07 17:06:02 +0200

    sr(admin): replace equal by deepEqual in tests

--- admin/tests/unit/routes/authenticated/sessions/list/to-be-published_test.js
+++ admin/tests/unit/routes/authenticated/sessions/list/to-be-published_test.js
@@ -33,9 +33,7 @@ module('Unit | Route | authenticated/sessions/list/to-be-published', function (h
       const result = await route.model();
 
       // then
-      // TODO: Fix this the next time the file is edited.
-      // eslint-disable-next-line qunit/no-assert-equal
-      assert.equal(result, toBePublishedSessions);
+      assert.deepEqual(result, toBePublishedSessions);
     });
   });
 });
