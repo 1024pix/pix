5ecb9d50d787c130f9e80c0b86cefb1dd99665d8
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-07-08 11:56:11 +0200

    feat(api): add computeAllBadgeAcquisitions method

--- api/tests/integration/scripts/prod/compute-badge-acquisitions_test.js
+++ api/tests/integration/scripts/prod/compute-badge-acquisitions_test.js
@@ -7,6 +7,7 @@ const {
   knex,
 } = require('../../../test-helper');
 const {
+  computeAllBadgeAcquisitions,
   computeBadgeAcquisition,
   getCampaignParticipationsBetweenIds,
 } = require('../../../../scripts/prod/compute-badge-acquisitions');
@@ -35,6 +36,147 @@ describe('Script | Prod | Compute Badge Acquisitions', function () {
       expect(campaignParticipations.length).to.equal(3);
       expect(campaignParticipations.map(({ id }) => id)).to.deep.equal([id2, id3, id4]);
       expect(campaignParticipations[0]).to.be.instanceOf(CampaignParticipation);
+    });
+  });
+
+  describe('Integration | #computeAllBadgeAcquisitions', function () {
+    let userId1, userId2;
+    let badgeCompleted;
+    let campaignParticipation1, campaignParticipation2;
+
+    beforeEach(async function () {
+      const listSkill = ['web1', 'web2', 'web3', 'web4'];
+
+      const learningContent = [
+        {
+          id: 'recArea1',
+          titleFrFr: 'area1_Title',
+          color: 'someColor',
+          competences: [
+            {
+              id: 'competenceId',
+              nameFrFr: 'Mener une recherche et une veille d’information',
+              index: '1.1',
+              tubes: [
+                {
+                  id: 'recTube0_0',
+                  skills: [
+                    {
+                      id: listSkill[0],
+                      nom: '@web1',
+                      status: 'actif',
+                      challenges: [],
+                    },
+                    {
+                      id: listSkill[1],
+                      nom: '@web2',
+                      status: 'actif',
+                      challenges: [],
+                    },
+                    {
+                      id: listSkill[2],
+                      nom: 'web3',
+                      status: 'actif',
+                      challenges: [],
+                    },
+                    {
+                      id: listSkill[3],
+                      nom: 'web4',
+                      status: 'actif',
+                      challenges: [],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ];
+
+      userId1 = databaseBuilder.factory.buildUser().id;
+      userId2 = databaseBuilder.factory.buildUser().id;
+      const targetProfileId = databaseBuilder.factory.buildTargetProfile().id;
+      listSkill.forEach((skillId) => databaseBuilder.factory.buildTargetProfileSkill({ targetProfileId, skillId }));
+      databaseBuilder.factory.buildKnowledgeElement({ userId: userId1, skillId: 'web1', status: 'validated' });
+      databaseBuilder.factory.buildKnowledgeElement({ userId: userId1, skillId: 'web2', status: 'validated' });
+      databaseBuilder.factory.buildKnowledgeElement({ userId: userId1, skillId: 'web3', status: 'validated' });
+      databaseBuilder.factory.buildKnowledgeElement({ userId: userId1, skillId: 'web4', status: 'invalidated' });
+
+      databaseBuilder.factory.buildKnowledgeElement({ userId: userId2, skillId: 'web1', status: 'validated' });
+      databaseBuilder.factory.buildKnowledgeElement({ userId: userId2, skillId: 'web2', status: 'validated' });
+      databaseBuilder.factory.buildKnowledgeElement({ userId: userId2, skillId: 'web3', status: 'validated' });
+      databaseBuilder.factory.buildKnowledgeElement({ userId: userId2, skillId: 'web4', status: 'invalidated' });
+
+      const campaignId = databaseBuilder.factory.buildCampaign({ targetProfileId }).id;
+      campaignParticipation1 = databaseBuilder.factory.buildCampaignParticipation({ campaignId, userId: userId1 });
+      campaignParticipation2 = databaseBuilder.factory.buildCampaignParticipation({ campaignId, userId: userId2 });
+
+      badgeCompleted = databaseBuilder.factory.buildBadge({
+        targetProfileId,
+        key: 'Badge1',
+      });
+      databaseBuilder.factory.buildBadgeCriterion({
+        scope: 'CampaignParticipation',
+        badgeId: badgeCompleted.id,
+        threshold: 40,
+      });
+
+      const badgeNotCompletedId = databaseBuilder.factory.buildBadge({
+        targetProfileId,
+        key: 'Badge2',
+      }).id;
+      databaseBuilder.factory.buildBadgeCriterion({
+        scope: 'CampaignParticipation',
+        badgeId: badgeNotCompletedId,
+        threshold: 90,
+      });
+
+      databaseBuilder.factory.buildBadge({
+        targetProfileId,
+        badgeCriteria: [],
+        key: 'Badge3',
+      }).id;
+
+      const learningContentObjects = learningContentBuilder.buildLearningContent(learningContent);
+      mockLearningContent(learningContentObjects);
+
+      return databaseBuilder.commit();
+    });
+
+    afterEach(async function () {
+      await knex('badge-acquisitions').delete();
+    });
+
+    it('should compute badge acquisition for all campaign participations', async function () {
+      // when
+      const numberOfCreatedBadges = await computeAllBadgeAcquisitions({
+        idMin: campaignParticipation1.id,
+        idMax: campaignParticipation2.id,
+      });
+
+      // then
+      const badgeAcquisitions = await knex('badge-acquisitions');
+      expect(badgeAcquisitions.length).to.equal(2);
+      expect(numberOfCreatedBadges).to.equal(2);
+    });
+
+    context('when dry run option is provided', function () {
+      it('should not create badge', async function () {
+        // given
+        const dryRun = true;
+
+        // when
+        const numberOfSupposedlyCreatedBadges = await computeAllBadgeAcquisitions({
+          idMin: campaignParticipation1.id,
+          idMax: campaignParticipation2.id,
+          dryRun,
+        });
+
+        // then
+        const badgeAcquisitions = await knex('badge-acquisitions');
+        expect(badgeAcquisitions.length).to.equal(0);
+        expect(numberOfSupposedlyCreatedBadges).to.equal(2);
+      });
     });
   });
 
