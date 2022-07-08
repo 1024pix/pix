4b7c5880e21d9718f3251210af7af8b2a42e1e95
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-07-08 11:23:43 +0200

    feat(api): add computeBadgeAcquisition method

--- api/tests/integration/scripts/prod/compute-badge-acquisitions_test.js
+++ api/tests/integration/scripts/prod/compute-badge-acquisitions_test.js
@@ -1,6 +1,22 @@
-const { expect, databaseBuilder } = require('../../../test-helper');
-const { getCampaignParticipationsBetweenIds } = require('../../../../scripts/prod/compute-badge-acquisitions');
+const {
+  expect,
+  databaseBuilder,
+  sinon,
+  learningContentBuilder,
+  mockLearningContent,
+  knex,
+} = require('../../../test-helper');
+const {
+  computeBadgeAcquisition,
+  getCampaignParticipationsBetweenIds,
+} = require('../../../../scripts/prod/compute-badge-acquisitions');
+const _ = require('lodash');
 const CampaignParticipation = require('../../../../lib/domain/models/CampaignParticipation');
+const badgeCriteriaService = require('../../../../lib/domain/services/badge-criteria-service');
+const badgeAcquisitionRepository = require('../../../../lib/infrastructure/repositories/badge-acquisition-repository');
+const badgeRepository = require('../../../../lib/infrastructure/repositories/badge-repository');
+const knowledgeElementRepository = require('../../../../lib/infrastructure/repositories/knowledge-element-repository');
+const targetProfileRepository = require('../../../../lib/infrastructure/repositories/target-profile-repository');
 
 describe('Script | Prod | Compute Badge Acquisitions', function () {
   describe('#getCampaignParticipationsBetweenIds', function () {
@@ -19,6 +35,327 @@ describe('Script | Prod | Compute Badge Acquisitions', function () {
       expect(campaignParticipations.length).to.equal(3);
       expect(campaignParticipations.map(({ id }) => id)).to.deep.equal([id2, id3, id4]);
       expect(campaignParticipations[0]).to.be.instanceOf(CampaignParticipation);
+    });
+  });
+
+  describe('Unit | #computeBadgeAcquisition', function () {
+    let badgeRepository, targetProfileRepository, knowledgeElementRepository, badgeAcquisitionRepository;
+    let badgeCriteriaService;
+    let dependencies;
+    let campaignParticipation;
+
+    beforeEach(function () {
+      campaignParticipation = {
+        id: 1,
+        userId: 123,
+      };
+
+      badgeRepository = {
+        findByCampaignParticipationId: _.noop,
+      };
+      targetProfileRepository = {
+        getByCampaignParticipationId: _.noop,
+      };
+      knowledgeElementRepository = {
+        findUniqByUserId: _.noop,
+      };
+      badgeAcquisitionRepository = {
+        createOrUpdate: _.noop,
+      };
+      badgeCriteriaService = {
+        areBadgeCriteriaFulfilled: _.noop,
+      };
+
+      dependencies = {
+        badgeAcquisitionRepository,
+        badgeCriteriaService,
+        badgeRepository,
+        knowledgeElementRepository,
+        targetProfileRepository,
+      };
+    });
+
+    context('when the campaign is associated to one badge', function () {
+      let badge;
+      let targetProfile;
+      let knowledgeElements;
+      let badgeId;
+
+      beforeEach(function () {
+        badgeId = Symbol('badgeId');
+        targetProfile = Symbol('targetProfile');
+        knowledgeElements = Symbol('knowledgeElements');
+
+        sinon.stub(badgeRepository, 'findByCampaignParticipationId');
+        badge = {
+          id: badgeId,
+          badgeCriteria: Symbol('badgeCriteria'),
+        };
+        badgeRepository.findByCampaignParticipationId.withArgs(campaignParticipation.id).resolves([badge]);
+
+        sinon.stub(targetProfileRepository, 'getByCampaignParticipationId');
+        targetProfileRepository.getByCampaignParticipationId.withArgs(campaignParticipation.id).resolves(targetProfile);
+
+        sinon.stub(knowledgeElementRepository, 'findUniqByUserId');
+        knowledgeElementRepository.findUniqByUserId
+          .withArgs({ userId: campaignParticipation.userId })
+          .resolves(knowledgeElements);
+        sinon.stub(badgeCriteriaService, 'areBadgeCriteriaFulfilled');
+        sinon.stub(badgeAcquisitionRepository, 'createOrUpdate');
+      });
+
+      it('should create a badge when badge requirements are fulfilled', async function () {
+        // given
+        badgeCriteriaService.areBadgeCriteriaFulfilled
+          .withArgs({ targetProfile, knowledgeElements, badge })
+          .returns(true);
+
+        // when
+        await computeBadgeAcquisition({ campaignParticipation, ...dependencies });
+
+        // then
+        expect(badgeAcquisitionRepository.createOrUpdate).to.have.been.calledWithExactly([
+          {
+            badgeId,
+            userId: campaignParticipation.userId,
+            campaignParticipationId: campaignParticipation.id,
+          },
+        ]);
+      });
+
+      it('should not create a badge when badge requirements are not fulfilled', async function () {
+        // given
+        badgeCriteriaService.areBadgeCriteriaFulfilled
+          .withArgs({ targetProfile, knowledgeElements, badge })
+          .returns(false);
+
+        // when
+        await computeBadgeAcquisition({ campaignParticipation, ...dependencies });
+
+        // then
+        expect(badgeAcquisitionRepository.createOrUpdate).to.not.have.been.called;
+      });
+    });
+
+    context('when the campaign is associated to two badges', function () {
+      let badge1, badge2;
+      let badgeId_1, badgeId_2;
+      let targetProfile;
+      let knowledgeElements;
+
+      beforeEach(function () {
+        badgeId_1 = Symbol('badgeId_1');
+        badgeId_2 = Symbol('badgeId_2');
+        targetProfile = Symbol('targetProfile');
+        knowledgeElements = Symbol('knowledgeElements');
+
+        sinon.stub(badgeRepository, 'findByCampaignParticipationId');
+        badge1 = {
+          id: badgeId_1,
+          badgeCriteria: Symbol('badgeCriteria'),
+        };
+        badge2 = {
+          id: badgeId_2,
+          badgeCriteria: Symbol('badgeCriteria'),
+        };
+        badgeRepository.findByCampaignParticipationId.withArgs(campaignParticipation.id).resolves([badge1, badge2]);
+
+        sinon.stub(targetProfileRepository, 'getByCampaignParticipationId');
+        targetProfileRepository.getByCampaignParticipationId.withArgs(campaignParticipation.id).resolves(targetProfile);
+
+        sinon.stub(knowledgeElementRepository, 'findUniqByUserId');
+        knowledgeElementRepository.findUniqByUserId
+          .withArgs({ userId: campaignParticipation.userId })
+          .resolves(knowledgeElements);
+
+        sinon.stub(badgeCriteriaService, 'areBadgeCriteriaFulfilled');
+        sinon.stub(badgeAcquisitionRepository, 'createOrUpdate');
+      });
+
+      it('should create one badge when only one badge requirements are fulfilled', async function () {
+        // given
+        badgeCriteriaService.areBadgeCriteriaFulfilled
+          .withArgs({ targetProfile, knowledgeElements, badge: badge1 })
+          .returns(true);
+        badgeCriteriaService.areBadgeCriteriaFulfilled
+          .withArgs({ targetProfile, knowledgeElements, badge: badge2 })
+          .returns(false);
+
+        // when
+        await computeBadgeAcquisition({ campaignParticipation, ...dependencies });
+
+        // then
+        expect(badgeAcquisitionRepository.createOrUpdate).to.have.been.calledWithExactly([
+          {
+            badgeId: badge1.id,
+            userId: campaignParticipation.userId,
+            campaignParticipationId: campaignParticipation.id,
+          },
+        ]);
+      });
+
+      it('should create two badges when both badges requirements are fulfilled', async function () {
+        // given
+        badgeCriteriaService.areBadgeCriteriaFulfilled
+          .withArgs({ targetProfile, knowledgeElements, badge: badge1 })
+          .returns(true);
+        badgeCriteriaService.areBadgeCriteriaFulfilled
+          .withArgs({ targetProfile, knowledgeElements, badge: badge2 })
+          .returns(true);
+
+        // when
+        await computeBadgeAcquisition({ campaignParticipation, ...dependencies });
+
+        // then
+        expect(badgeAcquisitionRepository.createOrUpdate).to.have.been.calledWithExactly([
+          {
+            badgeId: badge1.id,
+            userId: campaignParticipation.userId,
+            campaignParticipationId: campaignParticipation.id,
+          },
+          {
+            badgeId: badge2.id,
+            userId: campaignParticipation.userId,
+            campaignParticipationId: campaignParticipation.id,
+          },
+        ]);
+      });
+    });
+
+    context('when the campaign is not associated to a badge', function () {
+      it('should not create a badge', async function () {
+        // given
+        sinon.stub(badgeRepository, 'findByCampaignParticipationId');
+        badgeRepository.findByCampaignParticipationId.withArgs(campaignParticipation.id).resolves([]);
+
+        sinon.stub(badgeAcquisitionRepository, 'createOrUpdate');
+
+        // when
+        await computeBadgeAcquisition({ campaignParticipation, ...dependencies });
+
+        // then
+        expect(badgeAcquisitionRepository.createOrUpdate).to.not.have.been.called;
+      });
+    });
+  });
+
+  describe('Integration | #computeBadgeAcquisition', function () {
+    let userId;
+    let badgeCompleted;
+    let campaignParticipation;
+
+    beforeEach(async function () {
+      const listSkill = ['web1', 'web2', 'web3', 'web4'];
+
+      const learningContent = [
+        {
+          id: 'recArea1',
+          titleFrFr: 'area1_Title',
+          color: 'someColor',
+          competences: [
+            {
+              id: 'competenceId',
+              nameFrFr: 'Mener une recherche et une veille d’information',
+              index: '1.1',
+              tubes: [
+                {
+                  id: 'recTube0_0',
+                  skills: [
+                    {
+                      id: listSkill[0],
+                      nom: '@web1',
+                      status: 'actif',
+                      challenges: [],
+                    },
+                    {
+                      id: listSkill[1],
+                      nom: '@web2',
+                      status: 'actif',
+                      challenges: [],
+                    },
+                    {
+                      id: listSkill[2],
+                      nom: 'web3',
+                      status: 'actif',
+                      challenges: [],
+                    },
+                    {
+                      id: listSkill[3],
+                      nom: 'web4',
+                      status: 'actif',
+                      challenges: [],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ];
+
+      userId = databaseBuilder.factory.buildUser().id;
+      const targetProfileId = databaseBuilder.factory.buildTargetProfile().id;
+      listSkill.forEach((skillId) => databaseBuilder.factory.buildTargetProfileSkill({ targetProfileId, skillId }));
+      databaseBuilder.factory.buildKnowledgeElement({ userId, skillId: 'web1', status: 'validated' });
+      databaseBuilder.factory.buildKnowledgeElement({ userId, skillId: 'web2', status: 'validated' });
+      databaseBuilder.factory.buildKnowledgeElement({ userId, skillId: 'web3', status: 'validated' });
+      databaseBuilder.factory.buildKnowledgeElement({ userId, skillId: 'web4', status: 'invalidated' });
+
+      const campaignId = databaseBuilder.factory.buildCampaign({ targetProfileId }).id;
+      campaignParticipation = databaseBuilder.factory.buildCampaignParticipation({ campaignId, userId });
+
+      badgeCompleted = databaseBuilder.factory.buildBadge({
+        targetProfileId,
+        key: 'Badge1',
+      });
+      databaseBuilder.factory.buildBadgeCriterion({
+        scope: 'CampaignParticipation',
+        badgeId: badgeCompleted.id,
+        threshold: 40,
+      });
+
+      const badgeNotCompletedId = databaseBuilder.factory.buildBadge({
+        targetProfileId,
+        key: 'Badge2',
+      }).id;
+      databaseBuilder.factory.buildBadgeCriterion({
+        scope: 'CampaignParticipation',
+        badgeId: badgeNotCompletedId,
+        threshold: 90,
+      });
+
+      databaseBuilder.factory.buildBadge({
+        targetProfileId,
+        badgeCriteria: [],
+        key: 'Badge3',
+      }).id;
+
+      const learningContentObjects = learningContentBuilder.buildLearningContent(learningContent);
+      mockLearningContent(learningContentObjects);
+
+      return databaseBuilder.commit();
+    });
+
+    afterEach(async function () {
+      await knex('badge-acquisitions').delete();
+    });
+
+    it('should save only the validated badges', async function () {
+      // when
+      await computeBadgeAcquisition({
+        campaignParticipation,
+        badgeCriteriaService,
+        badgeAcquisitionRepository,
+        badgeRepository,
+        knowledgeElementRepository,
+        targetProfileRepository,
+      });
+
+      // then
+      const badgeAcquisitions = await knex('badge-acquisitions').where({ userId: userId });
+      expect(badgeAcquisitions.length).to.equal(1);
+      expect(badgeAcquisitions[0].userId).to.equal(userId);
+      expect(badgeAcquisitions[0].badgeId).to.equal(badgeCompleted.id);
     });
   });
 });
