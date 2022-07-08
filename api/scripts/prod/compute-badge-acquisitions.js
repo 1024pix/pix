ee65f808150ea4bbc24b9506a72a9b4b055d5d34
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-07-08 10:31:14 +0200

    feat(api): add getAllArgs method

--- api/scripts/prod/compute-badge-acquisitions.js
+++ api/scripts/prod/compute-badge-acquisitions.js
@@ -2,13 +2,36 @@ require('dotenv').config();
 const { performance } = require('perf_hooks');
 const logger = require('../../lib/infrastructure/logger');
 const { disconnect } = require('../../db/knex-database-connection');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
 
 async function main() {
   const startTime = performance.now();
   logger.info(`Script compute badge acquisitions has started`);
+  const args = _getAllArgs();
+
   const endTime = performance.now();
   const duration = Math.round(endTime - startTime);
   logger.info(`Script has ended: took ${duration} milliseconds`);
+}
+
+function _getAllArgs() {
+  return yargs(hideBin(process.argv))
+    .option('idMin', {
+      type: 'number',
+      demand: true,
+      description: 'id de la première campagne participation',
+    })
+    .option('idMax', {
+      type: 'number',
+      demand: true,
+      description: 'id de la dernière campagne participation',
+    })
+    .option('dryRun', {
+      type: 'boolean',
+      description: 'permet de lancer le script sans créer les badges manquants',
+    })
+    .help().argv;
 }
 
 const isLaunchedFromCommandLine = require.main === module;
