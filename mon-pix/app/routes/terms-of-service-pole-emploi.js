9f4845fe92d4d62dd53e311339b3378727ccdad9
Andreia Pena <andreia.pena@pix.fr>
2022-07-11 11:53:14 +0200

    (mon-pix): Remove UnauthenticatedRouteMixin

--- mon-pix/app/routes/terms-of-service-pole-emploi.js
+++ mon-pix/app/routes/terms-of-service-pole-emploi.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
-import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
+import { inject as service } from '@ember/service';
 
-export default class TermsOfServicePoleEmploiRoute extends Route.extend(UnauthenticatedRouteMixin) {}
+export default class TermsOfServicePoleEmploiRoute extends Route {
+  @service session;
+
+  beforeModel() {
+    this.session.prohibitAuthentication('user-dashboard');
+  }
+}
