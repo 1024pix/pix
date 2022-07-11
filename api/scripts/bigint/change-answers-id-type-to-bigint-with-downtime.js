f2cdad5818243f2ce18ef6efdf5a8a4b7d29df47
yahya <youness.yahya@pix.fr>
2022-07-11 13:16:26 +0200

    Wrap alter table with knex transaction

--- api/scripts/bigint/change-answers-id-type-to-bigint-with-downtime.js
+++ api/scripts/bigint/change-answers-id-type-to-bigint-with-downtime.js
@@ -3,21 +3,23 @@ const { knex } = require('../../db/knex-database-connection');
 const logger = require('../../lib/infrastructure/logger');
 
 const changeAnswerIdTypeToBigint = async () => {
-  logger.info('Altering knowledge-elements.answerId type to BIGINT - In progress');
-  await knex.raw(`ALTER TABLE "knowledge-elements" ALTER COLUMN "answerId" TYPE BIGINT`);
-  logger.info('Altering knowledge-elements.answerId type to BIGINT - Done');
+  await knex.transaction(async (trx) => {
+    logger.info('Altering knowledge-elements.answerId type to BIGINT - In progress');
+    await knex.raw(`ALTER TABLE "knowledge-elements" ALTER COLUMN "answerId" TYPE BIGINT`).transacting(trx);
+    logger.info('Altering knowledge-elements.answerId type to BIGINT - Done');
 
-  logger.info('Altering flash-assessment-results.answerId type to BIGINT - In progress');
-  await knex.raw(`ALTER TABLE "flash-assessment-results" ALTER COLUMN "answerId" TYPE BIGINT`);
-  logger.info('Altering flash-assessment-results.answerId type to BIGINT - Done');
+    logger.info('Altering flash-assessment-results.answerId type to BIGINT - In progress');
+    await knex.raw(`ALTER TABLE "flash-assessment-results" ALTER COLUMN "answerId" TYPE BIGINT`).transacting(trx);
+    logger.info('Altering flash-assessment-results.answerId type to BIGINT - Done');
 
-  logger.info('Altering answers.id type to BIGINT - In progress');
-  await knex.raw(`ALTER TABLE "answers" ALTER COLUMN "id" TYPE BIGINT`);
-  logger.info('Altering answers.id type to BIGINT - Done');
+    logger.info('Altering answers.id type to BIGINT - In progress');
+    await knex.raw(`ALTER TABLE "answers" ALTER COLUMN "id" TYPE BIGINT`).transacting(trx);
+    logger.info('Altering answers.id type to BIGINT - Done');
 
-  logger.info('Altering answers_id_seq type to BIGINT - In progress');
-  await knex.raw(`ALTER SEQUENCE "answers_id_seq" AS BIGINT`);
-  logger.info('Altering answers_id_seq type to BIGINT - Done');
+    logger.info('Altering answers_id_seq type to BIGINT - In progress');
+    await knex.raw(`ALTER SEQUENCE "answers_id_seq" AS BIGINT`).transacting(trx);
+    logger.info('Altering answers_id_seq type to BIGINT - Done');
+  });
 };
 
 const preventOneOffContainerTimeout = () => {
@@ -36,3 +38,5 @@ const preventOneOffContainerTimeout = () => {
     process.exit(1);
   }
 })();
+
+module.exports = { changeAnswerIdTypeToBigint };
