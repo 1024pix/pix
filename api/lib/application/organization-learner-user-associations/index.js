a8357c8cf9cb3a19e81c8a9b19180746baf4b4ec
Lise Quesnel <quesnel.lise@gmail.com>
2022-07-12 18:49:12 +0200

    refactor(api): deprecate POST /schooling-registration-user-associations/student
    in favor of POST /sup-organization-learners/association

--- api/lib/application/organization-learner-user-associations/index.js
+++ api/lib/application/organization-learner-user-associations/index.js
@@ -8,38 +8,6 @@ const identifiersType = require('../../domain/types/identifiers-type');
 exports.register = async function (server) {
   server.route([
     {
-      method: 'POST',
-      path: '/api/schooling-registration-user-associations/student',
-      config: {
-        handler: organizationLearnerUserAssociationController.reconcileSupOrganizationLearner,
-        validate: {
-          options: {
-            allowUnknown: false,
-          },
-          payload: Joi.object({
-            data: {
-              attributes: {
-                'student-number': Joi.string().empty(Joi.string().regex(/^\s*$/)).required(),
-                'first-name': Joi.string().empty(Joi.string().regex(/^\s*$/)).required(),
-                'last-name': Joi.string().empty(Joi.string().regex(/^\s*$/)).required(),
-                birthdate: Joi.date().format('YYYY-MM-DD').required(),
-                'campaign-code': Joi.string().empty(Joi.string().regex(/^\s*$/)).required(),
-              },
-              type: 'schooling-registration-user-associations',
-            },
-          }),
-          failAction: (request, h) => {
-            return sendJsonApiError(new UnprocessableEntityError('Un des champs saisis n’est pas valide.'), h);
-          },
-        },
-        notes: [
-          '- **Cette route est restreinte aux utilisateurs authentifiés**\n' +
-            '- Elle réconcilie l’utilisateur à l’inscription d’un étudiant dans cette organisation',
-        ],
-        tags: ['api', 'organizationLearnerUserAssociation'],
-      },
-    },
-    {
       method: 'PATCH',
       path: '/api/organizations/{id}/schooling-registration-user-associations/{schoolingRegistrationId}',
       config: {
