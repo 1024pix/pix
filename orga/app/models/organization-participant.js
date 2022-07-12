2667940481e94e4fe96844a5bbaa318b5491e07d
yaelle <ymarsiac@octo.com>
2022-07-12 15:22:37 +0200

    feat(orga): Add participation count column in organization participant table

--- orga/app/models/organization-participant.js
+++ orga/app/models/organization-participant.js
@@ -3,4 +3,5 @@ import Model, { attr } from '@ember-data/model';
 export default class OrganizationParticipant extends Model {
   @attr('string') lastName;
   @attr('string') firstName;
+  @attr('number') participationCount;
 }
