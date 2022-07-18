a86575da363cc710a72243950b59740756786650
Lise Quesnel <quesnel.lise@gmail.com>
2022-07-18 19:28:56 +0200

    feat(api): Add participations link to admin/users/id route

--- api/tests/unit/infrastructure/serializers/jsonapi/user-details-for-admin-serializer_test.js
+++ api/tests/unit/infrastructure/serializers/jsonapi/user-details-for-admin-serializer_test.js
@@ -70,6 +70,11 @@ describe('Unit | Serializer | JSONAPI | user-details-for-admin-serializer', func
                 related: `/api/admin/users/${userDetailsForAdmin.id}/profile`,
               },
             },
+            participations: {
+              links: {
+                related: `/api/admin/users/${userDetailsForAdmin.id}/participations`,
+              },
+            },
           },
           id: `${userDetailsForAdmin.id}`,
           type: 'users',
