4439d732032aa5075b087762c749a47509e7db7b
yaelle <ymarsiac@octo.com>
2022-07-18 17:21:02 +0200

    feat(api): add lastParticipationDate in repo and model

--- api/lib/domain/read-models/OrganizationParticipant.js
+++ api/lib/domain/read-models/OrganizationParticipant.js
@@ -1,9 +1,10 @@
 class OrganizationParticipant {
-  constructor({ id, firstName, lastName, participationCount } = {}) {
+  constructor({ id, firstName, lastName, participationCount, lastParticipationDate } = {}) {
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
     this.participationCount = participationCount;
+    this.lastParticipationDate = lastParticipationDate;
   }
 }
 
