443b331e32a807dd3fc67330d54b1faf9f222f3b
alexandrecoin <alexandre.pc.coin@gmail.com>
2022-07-18 15:32:08 +0200

    :recycle: certif: replace hardcoded date by translation

--- certif/app/components/layout/footer.js
+++ certif/app/components/layout/footer.js
@@ -1,8 +1,11 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
 
 export default class Footer extends Component {
+  @service intl;
+
   get currentYear() {
-    const date = new Date();
-    return date.getFullYear().toString();
+    const currentYear = new Date().getFullYear();
+    return this.intl.t('navigation.footer.current-year', { currentYear });
   }
 }
