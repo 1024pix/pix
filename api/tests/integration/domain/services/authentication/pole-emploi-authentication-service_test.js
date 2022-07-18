9554201c79af3bb84422098144db1fbf0d9e9991
Ismael Gorissen <ismael.gorissen@gmail.com>
2022-07-18 10:28:36 +0200

    feat(api): add getRedirectLogoutUrl method
    for Pôle Emploi authentication service

--- api/tests/integration/domain/services/authentication/pole-emploi-authentication-service_test.js
+++ api/tests/integration/domain/services/authentication/pole-emploi-authentication-service_test.js
@@ -8,7 +8,7 @@ describe('Integration | Domain | Services | pole-emploi-authentication-service',
   describe('#saveIdToken', function () {
     it('should return an uuid', async function () {
       // given
-      const v4RexExp = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i);
+      const uuidPattern = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i);
       const idToken =
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
       const userId = '123';
@@ -18,9 +18,34 @@ describe('Integration | Domain | Services | pole-emploi-authentication-service',
       const result = await logoutUrlTemporaryStorage.get(`123:${uuid}`);
 
       // then
-      expect(uuid.match(v4RexExp)).to.be.ok;
+      expect(uuid.match(uuidPattern)).to.be.ok;
       expect(result).to.equal(
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+      );
+    });
+  });
+
+  describe('#getRedirectLogoutUrl', function () {
+    it('should return a redirect logout url', async function () {
+      // given
+      const redirectUri = 'https://example.org/please-redirect-to-pix';
+      const idToken =
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+      const userId = '1';
+      const logoutUrlUUID = '1f3dbb71-f399-4c1c-85ae-0a863c78aeea';
+      const key = `${userId}:${logoutUrlUUID}`;
+      await logoutUrlTemporaryStorage.save({ key, value: idToken, expirationDelaySeconds: 1140 });
+
+      // when
+      const redirectTarget = await poleEmploiAuthenticationService.getRedirectLogoutUrl({
+        userId,
+        logoutUrlUUID,
+        redirectUri,
+      });
+
+      // then
+      expect(redirectTarget).to.equal(
+        'http://logout-url.fr/?id_token_hint=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c&redirect_uri=https%3A%2F%2Fexample.org%2Fplease-redirect-to-pix'
       );
     });
   });
