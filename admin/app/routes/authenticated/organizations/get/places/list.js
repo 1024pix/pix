da69eaea66a33b23c650342c91baa2c0103a2fdb
Karam Mohd <k.haytham.mohammed@octo.com>
2022-07-19 18:00:30 +0200

    [ADMIN] Add route + adapter

--- admin/app/routes/authenticated/organizations/get/places/list.js
+++ admin/app/routes/authenticated/organizations/get/places/list.js
@@ -5,7 +5,7 @@ export default class Places extends Route {
   @service store;
 
   async model() {
-    const organization = this.modelFor('authenticated.organizations.get');
+    const organization = await this.modelFor('authenticated.organizations.get');
     const places = await this.store.query('organization-place', {
       organizationId: organization.id,
     });
