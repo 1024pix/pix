d30b0d6e33f020770cb87003e2b98b4875a67c79
Karam Mohd <k.haytham.mohammed@octo.com>
2022-07-19 17:07:13 +0200

    [API] Rename model + add validation + categories constant

---
+++ api/lib/domain/constants/organization-places-categories.js
@@ -0,0 +1,12 @@
+module.exports = {
+  T0: 'T0',
+  T1: 'T1',
+  T2: 'T2',
+  T2bis: 'T2bis',
+  T3: 'T3',
+  FREE_RATE: 'FREE_RATE',
+  PUBLIC_RATE: 'PUBLIC_RATE',
+  REDUCE_RATE: 'REDUCE_RATE',
+  SPECIAL_REDUCE_RATE: 'SPECIAL_REDUCE_RATE',
+  FULL_RATE: 'FULL_RATE',
+};
