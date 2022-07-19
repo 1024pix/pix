909d64b4bb340e75d952753d6fbe494fc276e9e4
Lise Quesnel <quesnel.lise@gmail.com>
2022-07-19 18:16:07 +0200

    feat(admin): add button to delete learner participation on user page

--- admin/mirage/serializers/user.js
+++ admin/mirage/serializers/user.js
@@ -4,4 +4,12 @@ const _includes = ['schoolingRegistrations', 'authenticationMethods'];
 
 export default ApplicationSerializer.extend({
   include: _includes,
+
+  links(user) {
+    return {
+      participations: {
+        related: `/api/admin/users/${user.id}/participations`,
+      },
+    };
+  },
 });
