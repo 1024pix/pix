d16932da043e9c3ecaa4241db0de93844e728781
yschoueri <yasmina.schoueri@gmail.com>
2022-07-19 15:27:21 +0200

    mon-pix: use generic terms of service for cnav and remove no longer used translation keys

--- mon-pix/app/controllers/terms-of-service-cnav.js
+++ mon-pix/app/controllers/terms-of-service-cnav.js
@@ -1,51 +1,8 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-import get from 'lodash/get';
-
-const ERROR_INPUT_MESSAGE_MAP = {
-  termsOfServiceNotSelected: 'pages.terms-of-service-cnav.form.error-message',
-  unknownError: 'common.error',
-  expiredAuthenticationKey: 'pages.terms-of-service-cnav.form.expired-authentication-key',
-};
 
 export default class TermsOfServiceCnavController extends Controller {
   queryParams = ['authenticationKey'];
 
-  @service session;
-  @service store;
-  @service url;
-  @service intl;
-
   @tracked authenticationKey = null;
-  @tracked isTermsOfServiceValidated = false;
-  @tracked errorMessage = null;
-  @tracked isAuthenticationKeyExpired = false;
-
-  get homeUrl() {
-    return this.url.homeUrl;
-  }
-
-  @action
-  async submit() {
-    if (this.isTermsOfServiceValidated) {
-      this.errorMessage = null;
-      try {
-        await this.session.authenticate('authenticator:cnav', { authenticationKey: this.authenticationKey });
-      } catch (error) {
-        const status = get(error, 'errors[0].status');
-        if (status === '401') {
-          this.isAuthenticationKeyExpired = true;
-          this.errorMessage = this.intl.t(ERROR_INPUT_MESSAGE_MAP['expiredAuthenticationKey']);
-        } else {
-          const errorDetail = get(error, 'errors[0].detail');
-          this.errorMessage =
-            this.intl.t(ERROR_INPUT_MESSAGE_MAP['unknownError']) + (errorDetail ? ` (${errorDetail})` : '');
-        }
-      }
-    } else {
-      this.errorMessage = this.intl.t(ERROR_INPUT_MESSAGE_MAP['termsOfServiceNotSelected']);
-    }
-  }
 }
--- mon-pix/tests/acceptance/pole-emploi/pole-emploi-authentication-flow_test.js
+++ mon-pix/tests/acceptance/pole-emploi/pole-emploi-authentication-flow_test.js
@@ -2,42 +2,42 @@
 
 import { setupApplicationTest } from 'ember-mocha';
 import { setupMirage } from 'ember-cli-mirage/test-support';
-import { click, visit } from '@ember/test-helpers';
+import { click } from '@ember/test-helpers';
+import { visit } from '@1024pix/ember-testing-library';
 import Service from '@ember/service';
 import sinon from 'sinon';
 import setupIntl from '../../helpers/setup-intl';
-import { clickByLabel } from '../../helpers/click-by-label';
 
 describe('Acceptance | Pôle Emploi | authentication flow', function () {
   setupApplicationTest();
   setupMirage();
   setupIntl();
 
-  context('when user is logged in', function () {
-    context('on log out', function () {
-      it('should redirect to logout url', async function () {
-        // given
-        await visit('/connexion-pole-emploi?code=code&state=state');
-        const replaceLocationStub = sinon.stub().resolves();
-        this.owner.register(
-          'service:location',
-          Service.extend({
-            replace: replaceLocationStub,
-          })
-        );
+  context('when user is logged in with Pole Emploi and logs out', function () {
+    it('should redirect the user to logout url', async function () {
+      // given
+      const screen = await visit('/connexion-pole-emploi?code=code&state=state');
+      const replaceLocationStub = sinon.stub().resolves();
+      this.owner.register(
+        'service:location',
+        Service.extend({
+          replace: replaceLocationStub,
+        })
+      );
 
-        // when
-        await click('#pix-cgu');
-        await clickByLabel(this.intl.t('pages.terms-of-service-pole-emploi.form.button'));
-        await clickByLabel('Paul');
-        await clickByLabel(this.intl.t('navigation.user.sign-out'));
+      // when
+      await click(
+        screen.getByLabelText("J'accepte les conditions d'utilisation et la politique de confidentialité de Pix")
+      );
+      await click(screen.getByRole('button', { name: 'Je continue' }));
+      await click(screen.getByRole('button', { name: 'Paul Consulter mes informations' }));
+      await click(screen.getByRole('link', { name: 'Se déconnecter' }));
 
-        // then
-        sinon.assert.calledWith(
-          replaceLocationStub,
-          'http://identity_provider_base_url/deconnexion?id_token_hint=ID_TOKEN&redirect_uri=http%3A%2F%2Flocalhost.fr%3A4200%2Fconnexion'
-        );
-      });
+      // then
+      sinon.assert.calledWith(
+        replaceLocationStub,
+        'http://identity_provider_base_url/deconnexion?id_token_hint=ID_TOKEN&redirect_uri=http%3A%2F%2Flocalhost.fr%3A4200%2Fconnexion'
+      );
     });
   });
 });
