dd20a50af437fc3a783c6fe3c455d863ad5a7320
Andreia Pena <andreia.pena@pix.fr>
2022-07-25 17:14:41 +0200

    feat(api): add parameters in cnav and pe authentication services

--- api/lib/domain/services/authentication/pole-emploi-oidc-authentication-service.js
+++ api/lib/domain/services/authentication/pole-emploi-oidc-authentication-service.js
@@ -8,8 +8,28 @@ class PoleEmploiOidcAuthenticationService extends OidcAuthenticationService {
     const identityProvider = constants.IDENTITY_PROVIDER.POLE_EMPLOI;
     const expirationDelaySeconds = settings.poleEmploi.accessTokenLifespanMs / 1000;
     const jwtOptions = { expiresIn: expirationDelaySeconds };
+    const clientSecret = settings.poleEmploi.clientSecret;
+    const clientId = settings.poleEmploi.clientId;
+    const tokenUrl = settings.poleEmploi.tokenUrl;
+    const authenticationUrl = settings.poleEmploi.authenticationUrl;
+    const authenticationUrlParameters = [
+      { key: 'realm', value: '/individu' },
+      {
+        key: 'scope',
+        value: `application_${clientId} api_peconnect-individuv1 openid profile`,
+      },
+    ];
 
-    super({ source, identityProvider, jwtOptions });
+    super({
+      source,
+      identityProvider,
+      jwtOptions,
+      clientSecret,
+      clientId,
+      tokenUrl,
+      authenticationUrl,
+      authenticationUrlParameters,
+    });
   }
 }
 
