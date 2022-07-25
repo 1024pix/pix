d23ff292f9170faf5065cdc761209334e6199d0b
Anne-G <ang.schall@gmail.com>
2022-07-25 16:50:28 +0200

    feat(api): add exchangeCodeForTokens in oidc authentication service

--- api/lib/domain/services/authentication/oidc-authentication-service.js
+++ api/lib/domain/services/authentication/oidc-authentication-service.js
@@ -1,11 +1,18 @@
 const jsonwebtoken = require('jsonwebtoken');
 const settings = require('../../../config');
+const httpAgent = require('../../../infrastructure/http/http-agent');
+const querystring = require('querystring');
+const { AuthenticationTokenRetrievalError } = require('../../errors');
+const AuthenticationSessionContent = require('../../models/AuthenticationSessionContent');
 
 class OidcAuthenticationService {
-  constructor({ source, identityProvider, jwtOptions }) {
+  constructor({ source, identityProvider, jwtOptions, clientSecret, clientId, tokenUrl }) {
     this.source = source;
     this.identityProvider = identityProvider;
     this.jwtOptions = jwtOptions;
+    this.clientSecret = clientSecret;
+    this.clientId = clientId;
+    this.tokenUrl = tokenUrl;
   }
 
   createAccessToken(userId) {
@@ -18,6 +25,34 @@ class OidcAuthenticationService {
       settings.authentication.secret,
       this.jwtOptions
     );
+  }
+
+  async exchangeCodeForTokens({ code, redirectUri }) {
+    const data = {
+      client_secret: this.clientSecret,
+      grant_type: 'authorization_code',
+      code,
+      client_id: this.clientId,
+      redirect_uri: redirectUri,
+    };
+
+    const response = await httpAgent.post({
+      url: this.tokenUrl,
+      payload: querystring.stringify(data),
+      headers: { 'content-type': 'application/x-www-form-urlencoded' },
+    });
+
+    if (!response.isSuccessful) {
+      const errorMessage = JSON.stringify(response.data);
+      throw new AuthenticationTokenRetrievalError(errorMessage, response.code);
+    }
+
+    return new AuthenticationSessionContent({
+      idToken: response.data['id_token'],
+      accessToken: response.data['access_token'],
+      expiresIn: response.data['expires_in'],
+      refreshToken: response.data['refresh_token'],
+    });
   }
 }
 
