67ef7476eb7a5aa618da799d44293a0878ad15bb
Andreia Pena <andreia.pena@pix.fr>
2022-07-27 14:44:31 +0200

    recycle(api): remove unused redirectUri parameter in oidc controller

--- api/lib/application/authentication/oidc/oidc-controller.js
+++ api/lib/application/authentication/oidc/oidc-controller.js
@@ -3,16 +3,11 @@ const authenticationServiceRegistry = require('../../../domain/services/authenti
 module.exports = {
   async getRedirectLogoutUrl(request, h) {
     const userId = request.auth.credentials.userId;
-    const {
-      identity_provider: identityProvider,
-      redirect_uri: redirectUri,
-      logout_url_uuid: logoutUrlUUID,
-    } = request.query;
+    const { identity_provider: identityProvider, logout_url_uuid: logoutUrlUUID } = request.query;
     const oidcAuthenticationService = authenticationServiceRegistry.lookupAuthenticationService(identityProvider);
     const redirectLogoutUrl = await oidcAuthenticationService.getRedirectLogoutUrl({
       userId,
       logoutUrlUUID,
-      redirectUri,
     });
 
     return h.response({ redirectLogoutUrl }).code(200);
