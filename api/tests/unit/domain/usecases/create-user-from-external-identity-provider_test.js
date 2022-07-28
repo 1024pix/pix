96104b5f5de131e28fa94227b0fe1ae8c3931e1c
Andreia Pena <andreia.pena@pix.fr>
2022-07-28 17:16:03 +0200

    recycle(api): remove useless verification in usecase

--- api/tests/unit/domain/usecases/create-user-from-external-identity-provider_test.js
+++ api/tests/unit/domain/usecases/create-user-from-external-identity-provider_test.js
@@ -1,10 +1,8 @@
 const { expect, sinon, catchErr } = require('../../../test-helper');
 const {
-  InvalidExternalAPIResponseError,
   AuthenticationKeyExpired,
   UserAlreadyExistsWithAuthenticationMethodError,
 } = require('../../../../lib/domain/errors');
-const logger = require('../../../../lib/infrastructure/logger');
 const createUserFromExternalIdentityProvider = require('../../../../lib/domain/usecases/create-user-from-external-identity-provider');
 
 describe('Unit | UseCase | create-user-from-external-identity-provider', function () {
@@ -88,40 +86,6 @@ describe('Unit | UseCase | create-user-from-external-identity-provider', functio
       // then
       expect(error).to.be.instanceOf(UserAlreadyExistsWithAuthenticationMethodError);
       expect(error.message).to.equal('Authentication method already exists for this external identifier.');
-    });
-  });
-
-  context('when required properties are not returned by external API', function () {
-    it('should raise an error and log details', async function () {
-      // given
-      const authenticationSessionContent = 'SESSION_CONTENT';
-      authenticationSessionService.getByKey.resolves(authenticationSessionContent);
-      authenticationServiceRegistry.lookupAuthenticationService.resolves(oidcAuthenticationService);
-      oidcAuthenticationService.getUserInfo.resolves({
-        firstName: 'Jean',
-        lastName: undefined,
-        externalIdentityId: 'externalIdentityId',
-      });
-      sinon.stub(logger, 'error');
-
-      // when
-      const error = await catchErr(createUserFromExternalIdentityProvider)({
-        authenticationKey: 'AUTHENTICATION_KEY',
-        authenticationSessionService,
-        authenticationServiceRegistry,
-        authenticationMethodRepository,
-        userToCreateRepository,
-      });
-
-      // then
-      expect(error).to.be.instanceOf(InvalidExternalAPIResponseError);
-      expect(error.message).to.be.equal('Les informations utilisateurs récupérées sont incorrectes.');
-      const expectedMessage = `Un des champs obligatoires n'a pas été renvoyé : ${JSON.stringify({
-        firstName: 'Jean',
-        lastName: undefined,
-        externalIdentityId: 'externalIdentityId',
-      })}.`;
-      expect(logger.error).to.have.been.calledWith(expectedMessage);
     });
   });
 
