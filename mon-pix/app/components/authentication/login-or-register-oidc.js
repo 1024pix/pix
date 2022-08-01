e7c264530a025c54b8c23aeab026312c5d8402e0
Andreia Pena <andreia.pena@pix.fr>
2022-08-01 17:42:49 +0200

    feat(mon-pix): add register form on new page

--- mon-pix/app/components/authentication/login-or-register-oidc.js
+++ mon-pix/app/components/authentication/login-or-register-oidc.js
@@ -1,9 +1,25 @@
 import Component from '@glimmer/component';
-import IdentityProviders from 'mon-pix/identity-providers';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import get from 'lodash/get';
+import IdentityProviders from 'mon-pix/identity-providers';
+
+const ERROR_INPUT_MESSAGE_MAP = {
+  termsOfServiceNotSelected: 'pages.login-or-register-oidc.error.error-message',
+  unknownError: 'common.error',
+  expiredAuthenticationKey: 'pages.login-or-register-oidc.error.expired-authentication-key',
+};
 
 export default class LoginOrRegisterOidcComponent extends Component {
   @service url;
+  @service intl;
+  @service session;
+  @service currentDomain;
+
+  @tracked isTermsOfServiceValidated = false;
+  @tracked isAuthenticationKeyExpired = false;
+  @tracked errorMessage = null;
 
   get identityProviderOrganizationName() {
     return IdentityProviders[this.args.identityProviderSlug].organizationName;
@@ -11,5 +27,46 @@ export default class LoginOrRegisterOidcComponent extends Component {
 
   get homeUrl() {
     return this.url.homeUrl;
+  }
+
+  get cguUrl() {
+    const currentLanguage = this.intl.t('current-lang');
+    if (currentLanguage === 'en') {
+      return 'https://pix.org/en-gb/terms-and-conditions';
+    }
+    return `https://pix.${this.currentDomain.getExtension()}/conditions-generales-d-utilisation`;
+  }
+
+  get dataProtectionPolicyUrl() {
+    const currentLanguage = this.intl.t('current-lang');
+    if (currentLanguage === 'en') {
+      return 'https://pix.org/en-gb/personal-data-protection-policy';
+    }
+    return `https://pix.${this.currentDomain.getExtension()}/politique-protection-donnees-personnelles-app`;
+  }
+
+  @action
+  async submit() {
+    if (this.isTermsOfServiceValidated) {
+      this.errorMessage = null;
+      try {
+        await this.session.authenticate('authenticator:oidc', {
+          authenticationKey: this.args.authenticationKey,
+          identityProviderSlug: this.args.identityProviderSlug,
+        });
+      } catch (error) {
+        const status = get(error, 'errors[0].status');
+        if (status === '401') {
+          this.isAuthenticationKeyExpired = true;
+          this.errorMessage = this.intl.t(ERROR_INPUT_MESSAGE_MAP['expiredAuthenticationKey']);
+        } else {
+          const errorDetail = get(error, 'errors[0].detail');
+          this.errorMessage =
+            this.intl.t(ERROR_INPUT_MESSAGE_MAP['unknownError']) + (errorDetail ? ` (${errorDetail})` : '');
+        }
+      }
+    } else {
+      this.errorMessage = this.intl.t(ERROR_INPUT_MESSAGE_MAP['termsOfServiceNotSelected']);
+    }
   }
 }
