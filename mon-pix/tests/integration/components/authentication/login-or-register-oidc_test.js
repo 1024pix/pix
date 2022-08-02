88a925a3cea97ee91b2095442f1df0576ca2c479
Andreia Pena <andreia.pena@pix.fr>
2022-08-02 17:17:35 +0200

    feat(mon-pix): add login form

--- mon-pix/tests/integration/components/authentication/login-or-register-oidc_test.js
+++ mon-pix/tests/integration/components/authentication/login-or-register-oidc_test.js
@@ -8,13 +8,8 @@ describe('Integration | Component | authentication::login-or-register-oidc', fun
   setupIntlRenderingTest();
 
   it('should display elements for OIDC identity provider', async function () {
-    // given
-    this.set('identityProviderSlug', 'cnav');
-
-    // when
-    const screen = await render(
-      hbs`<Authentication::LoginOrRegisterOidc @identityProviderSlug={{this.identityProviderSlug}} />`
-    );
+    // given & when
+    const screen = await render(hbs`<Authentication::LoginOrRegisterOidc />`);
 
     // then
     expect(
@@ -23,24 +18,67 @@ describe('Integration | Component | authentication::login-or-register-oidc', fun
         level: 1,
       })
     ).to.exist;
-    expect(
-      screen.getByRole('heading', {
-        name: this.intl.t('pages.login-or-register-oidc.register-form.title'),
-        level: 2,
-      })
-    ).to.exist;
-    expect(
-      screen.getByRole('heading', {
-        name: this.intl.t('pages.login-or-register-oidc.login-form.title'),
-        level: 2,
-      })
-    ).to.exist;
     expect(screen.getByRole('img', { name: this.intl.t('common.pix') })).to.exist;
     expect(screen.getByRole('link', { name: this.intl.t('common.pix') })).to.exist;
-    expect(screen.getByRole('checkbox', { name: this.intl.t('common.cgu.label') })).to.exist;
-    expect(screen.getByRole('link', { name: this.intl.t('common.cgu.cgu') })).to.exist;
-    expect(screen.getByRole('link', { name: this.intl.t('common.cgu.data-protection-policy') })).to.exist;
-    expect(screen.getByRole('button', { name: this.intl.t('pages.login-or-register-oidc.register-form.button') })).to
-      .exist;
+  });
+
+  context('on login form', function () {
+    it('should display elements for OIDC identity provider', async function () {
+      // given
+      this.set('identityProviderSlug', 'pole-emploi');
+
+      // when
+      const screen = await render(
+        hbs`<Authentication::LoginOrRegisterOidc @identityProviderSlug={{this.identityProviderSlug}} />`
+      );
+
+      // then
+      expect(
+        screen.getByRole('heading', {
+          name: this.intl.t('pages.login-or-register-oidc.register-form.title'),
+          level: 2,
+        })
+      ).to.exist;
+      expect(screen.getByRole('checkbox', { name: this.intl.t('common.cgu.label') })).to.exist;
+      expect(screen.getByRole('link', { name: this.intl.t('common.cgu.cgu') })).to.exist;
+      expect(screen.getByRole('link', { name: this.intl.t('common.cgu.data-protection-policy') })).to.exist;
+      expect(
+        screen.getByRole('button', { name: this.intl.t('pages.login-or-register-oidc.register-form.button') })
+      ).to.exist;
+      expect(
+        screen.getByText(
+          this.intl.t('pages.login-or-register-oidc.register-form.description', {
+            identityProviderOrganizationName: 'Pôle Emploi',
+          })
+        )
+      ).to.exist;
+    });
+  });
+
+  context('on register form', function () {
+    it('should display elements for OIDC identity provider', async function () {
+      // given
+      this.set('identityProviderSlug', 'cnav');
+
+      // when
+      const screen = await render(
+        hbs`<Authentication::LoginOrRegisterOidc @identityProviderSlug={{this.identityProviderSlug}} />`
+      );
+
+      // then
+      expect(
+        screen.getByRole('heading', {
+          name: this.intl.t('pages.login-or-register-oidc.register-form.title'),
+          level: 2,
+        })
+      ).to.exist;
+      expect(
+        screen.getByRole('textbox', { name: this.intl.t('pages.login-or-register-oidc.login-form.email') })
+      ).to.exist;
+      expect(screen.getByRole('link', { name: this.intl.t('pages.sign-in.forgotten-password') })).to.exist;
+      expect(
+        screen.getByRole('button', { name: this.intl.t('pages.login-or-register-oidc.login-form.button') })
+      ).to.exist;
+    });
   });
 });
