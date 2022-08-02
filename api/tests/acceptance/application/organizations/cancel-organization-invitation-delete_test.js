fbdb5809a348619d54ab6230b2dfc177620448a5
Ismael Gorissen <ismael.gorissen@gmail.com>
2022-08-02 11:10:03 +0200

    feat(api): add new route for admin app
    An admin member can use this route to cancel an organization invitation.

--- api/tests/acceptance/application/organizations/cancel-organization-invitation-delete_test.js
+++ api/tests/acceptance/application/organizations/cancel-organization-invitation-delete_test.js
@@ -3,13 +3,14 @@ const {
   expect,
   generateValidRequestAuthorizationHeader,
   insertOrganizationUserWithRoleAdmin,
+  insertUserWithRoleSuperAdmin,
 } = require('../../../test-helper');
 const createServer = require('../../../../server');
 const OrganizationInvitation = require('../../../../lib/domain/models/OrganizationInvitation');
 
 describe('Acceptance | Route | Organizations', function () {
   describe('DELETE /api/organizations/{id}/invitations/{invitationId}', function () {
-    it('should return 200 HTTP status code', async function () {
+    it('should return 204 HTTP status code', async function () {
       // given
       const server = await createServer();
 
@@ -33,7 +34,37 @@ describe('Acceptance | Route | Organizations', function () {
       const response = await server.inject(options);
 
       // then
-      expect(response.statusCode).to.equal(200);
+      expect(response.statusCode).to.equal(204);
+    });
+  });
+
+  describe('DELETE /api/admin/organizations/{organizationId}/invitations/{invitationId}', function () {
+    it('should return 204 HTTP status code', async function () {
+      // given
+      const server = await createServer();
+
+      const adminMember = await insertUserWithRoleSuperAdmin();
+      const { organization } = await insertOrganizationUserWithRoleAdmin();
+      const invitation = databaseBuilder.factory.buildOrganizationInvitation({
+        organizationId: organization.id,
+        status: OrganizationInvitation.StatusType.PENDING,
+      });
+
+      const options = {
+        method: 'DELETE',
+        url: `/api/admin/organizations/${organization.id}/invitations/${invitation.id}`,
+        headers: {
+          authorization: generateValidRequestAuthorizationHeader(adminMember.id),
+        },
+      };
+
+      await databaseBuilder.commit();
+
+      // when
+      const response = await server.inject(options);
+
+      // then
+      expect(response.statusCode).to.equal(204);
     });
   });
 });
