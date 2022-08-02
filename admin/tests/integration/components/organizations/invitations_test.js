19f0786b06f7e747e0f620f0118e4b9e3a0df84f
Ismael Gorissen <ismael.gorissen@gmail.com>
2022-08-02 14:36:12 +0200

    refactor(admin): reorder tests suites

--- admin/tests/integration/components/organizations/invitations_test.js
+++ admin/tests/integration/components/organizations/invitations_test.js
@@ -7,47 +7,51 @@ import dayjs from 'dayjs';
 module('Integration | Component | organization-invitations', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it should list the pending team invitations', async function (assert) {
-    // given
-    this.set('invitations', [
-      {
-        email: 'riri@example.net',
-        role: 'ADMIN',
-        roleInFrench: 'Administrateur',
-        updatedAt: dayjs('2019-10-08T10:50:00Z').utcOffset(2),
-      },
-      {
-        email: 'fifi@example.net',
-        role: 'MEMBER',
-        roleInFrench: 'Membre',
-        updatedAt: dayjs('2019-10-08T10:50:00Z').utcOffset(2),
-      },
-      {
-        email: 'loulou@example.net',
-        role: null,
-        roleInFrench: '-',
-        updatedAt: dayjs('2019-10-08T10:50:00Z').utcOffset(2),
-      },
-    ]);
+  module('without invitation', function () {
+    test('it should display a message when there is no invitations', async function (assert) {
+      // given
+      this.set('invitations', []);
 
-    // when
-    const screen = await render(hbs`<Organizations::Invitations @invitations={{invitations}}/>`);
+      // when
+      const screen = await render(hbs`<Organizations::Invitations @invitations={{invitations}}/>`);
 
-    // then
-    assert.dom(screen.getByText('Membre')).exists();
-    assert.dom(screen.getByText('Administrateur')).exists();
-    assert.dom(screen.getByText('-')).exists();
-    assert.dom(screen.queryByText('Aucune invitation en attente')).doesNotExist();
+      // then
+      assert.dom(screen.getByText('Aucune invitation en attente')).exists();
+    });
   });
 
-  test('it should display a message when there is no invitations', async function (assert) {
-    // given
-    this.set('invitations', []);
+  module('with invitations', function () {
+    test('it should list the pending team invitations', async function (assert) {
+      // given
+      this.set('invitations', [
+        {
+          email: 'riri@example.net',
+          role: 'ADMIN',
+          roleInFrench: 'Administrateur',
+          updatedAt: dayjs('2019-10-08T10:50:00Z').utcOffset(2),
+        },
+        {
+          email: 'fifi@example.net',
+          role: 'MEMBER',
+          roleInFrench: 'Membre',
+          updatedAt: dayjs('2019-10-08T10:50:00Z').utcOffset(2),
+        },
+        {
+          email: 'loulou@example.net',
+          role: null,
+          roleInFrench: '-',
+          updatedAt: dayjs('2019-10-08T10:50:00Z').utcOffset(2),
+        },
+      ]);
 
-    // when
-    const screen = await render(hbs`<Organizations::Invitations @invitations={{invitations}}/>`);
+      // when
+      const screen = await render(hbs`<Organizations::Invitations @invitations={{invitations}}/>`);
 
-    // then
-    assert.dom(screen.getByText('Aucune invitation en attente')).exists();
+      // then
+      assert.dom(screen.getByText('Membre')).exists();
+      assert.dom(screen.getByText('Administrateur')).exists();
+      assert.dom(screen.getByText('-')).exists();
+      assert.dom(screen.queryByText('Aucune invitation en attente')).doesNotExist();
+    });
   });
 });
