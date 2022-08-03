c91228e90c0690e6ff9f18ef8fe174c6b8f13411
Lise Quesnel <quesnel.lise@gmail.com>
2022-08-03 15:48:06 +0200

    refactor(orga): use new route organizations/id/sup-organization-learners/import-csv
    instead of organizations/id/schooling-registrations/import-csv

--- orga/tests/unit/adapters/students-import_test.js
+++ orga/tests/unit/adapters/students-import_test.js
@@ -18,7 +18,10 @@ module('Unit | Adapters | Students import', function (hooks) {
 
       // then
       assert.ok(
-        adapter.ajax.calledWith('http://localhost:3000/api/organizations/1/sup-organization-learners/import-csv', 'POST')
+        adapter.ajax.calledWith(
+          'http://localhost:3000/api/organizations/1/sup-organization-learners/import-csv',
+          'POST'
+        )
       );
     });
   });
