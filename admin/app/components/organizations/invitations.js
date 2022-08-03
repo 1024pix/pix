83fb5eda40cc3beec564b7534111497704a89624
Ismael Gorissen <ismael.gorissen@gmail.com>
2022-08-03 09:45:22 +0200

    feat(admin): restrict access to execute cancel action
    only for admin members with "CERTIF" as role

--- admin/app/components/organizations/invitations.js
+++ admin/app/components/organizations/invitations.js
@@ -1,6 +1,9 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
 
 export default class OrganizationInvitations extends Component {
+  @service accessControl;
+
   get sortedInvitations() {
     return this.args.invitations.sortBy('updatedAt').reverse();
   }
