1d671a095b0c719bd5b1d439178bd9b79e232ad2
Lise Quesnel <quesnel.lise@gmail.com>
2022-08-03 15:40:35 +0200

    refactor(orga): delete useless getter

--- orga/app/components/student/sco/header-actions.js
+++ orga/app/components/student/sco/header-actions.js
@@ -1,6 +1,5 @@
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
-import ENV from 'pix-orga/config/environment';
 
 export default class ScoHeaderActions extends Component {
   @service currentUser;
@@ -17,9 +16,5 @@ export default class ScoHeaderActions extends Component {
   get importButtonLabel() {
     const types = this.acceptedFileType.join(this.intl.t('pages.students-sco.actions.import-file.file-type-separator'));
     return this.intl.t('pages.students-sco.actions.import-file.label', { types });
-  }
-
-  get urlToDownloadCsvTemplate() {
-    return `${ENV.APP.API_HOST}/api/organizations/${this.currentUser.organization.id}/schooling-registrations/csv-template?accessToken=${this.session.data.authenticated.access_token}&lang=${this.currentUser.prescriber.lang}`;
   }
 }
