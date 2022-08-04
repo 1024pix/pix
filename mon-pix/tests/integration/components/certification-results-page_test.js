f051b150321f447bf7c66d09bf2dcee8ec55d71e
Andreia Pena <andreia.pena@pix.fr>
2022-08-04 10:11:50 +0200

    feat(mon-pix): use pixCheckbox component in certification results page form

--- mon-pix/tests/integration/components/certification-results-page_test.js
+++ mon-pix/tests/integration/components/certification-results-page_test.js
@@ -1,9 +1,10 @@
 import { expect } from 'chai';
 import { beforeEach, describe, it } from 'mocha';
 import setupIntlRenderingTest from '../../helpers/setup-intl-rendering';
-import { find, click, render } from '@ember/test-helpers';
+import { find, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { clickByLabel } from '../../helpers/click-by-label';
+import { render } from '@1024pix/ember-testing-library';
 
 describe('Integration | Component | certification results template', function () {
   setupIntlRenderingTest();
@@ -26,8 +27,10 @@ describe('Integration | Component | certification results template', function ()
 
     it('should be able to click on validation when we check to show the last message', async function () {
       // when
-      await render(hbs`{{certification-results-page certificationNumber=certificationNumber}}`);
-      await click('#validSupervisor');
+      const screen = await render(hbs`{{certification-results-page certificationNumber=certificationNumber}}`);
+      await click(
+        screen.getByRole('checkbox', { name: this.intl.t('pages.certification-results.not-finished.checkbox-label') })
+      );
       await clickByLabel(this.intl.t('pages.certification-results.action.confirm'));
 
       // then
@@ -38,8 +41,10 @@ describe('Integration | Component | certification results template', function ()
 
     it('should have a button to logout at the end of certification', async function () {
       // when
-      await render(hbs`{{certification-results-page certificationNumber=certificationNumber}}`);
-      await click('#validSupervisor');
+      const screen = await render(hbs`{{certification-results-page certificationNumber=certificationNumber}}`);
+      await click(
+        screen.getByRole('checkbox', { name: this.intl.t('pages.certification-results.not-finished.checkbox-label') })
+      );
       await clickByLabel(this.intl.t('pages.certification-results.action.confirm'));
 
       // then
