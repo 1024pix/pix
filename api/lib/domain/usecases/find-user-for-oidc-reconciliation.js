cbf25d501d01cdaece6d975690db92c1ba347935
Andreia Pena <andreia.pena@pix.fr>
2022-08-08 16:34:11 +0200

    feat(mon-pix): find user oidc authentication method in new use case

--- api/lib/domain/usecases/find-user-for-oidc-reconciliation.js
+++ api/lib/domain/usecases/find-user-for-oidc-reconciliation.js
@@ -1,12 +1,19 @@
 module.exports = async function findUserForOidcReconciliation({
   email,
   password,
+  identityProvider,
   pixAuthenticationService,
+  authenticationMethodRepository,
   userRepository,
 }) {
-  await pixAuthenticationService.getUserByUsernameAndPassword({
+  const foundUser = await pixAuthenticationService.getUserByUsernameAndPassword({
     username: email,
     password,
     userRepository,
+  });
+
+  await authenticationMethodRepository.findOneByUserIdAndIdentityProvider({
+    userId: foundUser.id,
+    identityProvider: identityProvider,
   });
 };
--- api/tests/unit/domain/usecases/find-user-for-oidc-reconciliation_test.js
+++ api/tests/unit/domain/usecases/find-user-for-oidc-reconciliation_test.js
@@ -2,16 +2,20 @@ const { expect, sinon } = require('../../../test-helper');
 const findUserForOidcReconciliation = require('../../../../lib/domain/usecases/find-user-for-oidc-reconciliation');
 
 describe('Unit | UseCase | find-user-for-oidc-reconciliation', function () {
-  it('should find pix user', async function () {
+  it('should find pix user and their oidc authentication method', async function () {
     // given
+    const authenticationMethodRepository = { findOneByUserIdAndIdentityProvider: sinon.stub() };
     const userRepository = {};
     const pixAuthenticationService = { getUserByUsernameAndPassword: sinon.stub() };
+    pixAuthenticationService.getUserByUsernameAndPassword.resolves({ id: 2 });
 
     // when
     await findUserForOidcReconciliation({
       email: 'ane.trotro@example.net',
       password: 'pix123',
+      identityProvider: 'oidc',
       pixAuthenticationService,
+      authenticationMethodRepository,
       userRepository,
     });
 
@@ -20,6 +24,11 @@ describe('Unit | UseCase | find-user-for-oidc-reconciliation', function () {
       username: 'ane.trotro@example.net',
       password: 'pix123',
       userRepository,
+    });
+
+    expect(authenticationMethodRepository.findOneByUserIdAndIdentityProvider).to.be.calledOnceWith({
+      userId: 2,
+      identityProvider: 'oidc',
     });
   });
 });
