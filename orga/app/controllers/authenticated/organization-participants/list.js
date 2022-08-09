16615d237fabe6790d55280cd054b880acbdaaea
Marie-Hélène <marie-helene.rots@pix.fr>
2022-08-09 15:31:37 +0200

    feat(orga): display no-participant-panel when there is not participants

--- orga/app/controllers/authenticated/organization-participants/list.js
+++ orga/app/controllers/authenticated/organization-participants/list.js
@@ -1,11 +1,13 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
 export default class ListController extends Controller {
   @tracked pageNumber = 1;
   @tracked pageSize = 25;
   @tracked fullName = null;
+  @service currentUser;
 
   @action
   triggerFiltering(fieldName, value) {
