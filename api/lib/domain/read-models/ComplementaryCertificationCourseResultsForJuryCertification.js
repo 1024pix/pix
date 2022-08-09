049d1b4eb016c4c822660dd3feec4d8527717adc
Marianne Bost <marianne.bost@pix.fr>
2022-08-09 16:29:11 +0200

    :sparkles: api: add label in constructor ComplementaryCertificationCourseResultsForJuryCertification Co-authored-by: Jérémy Pluquet <jemyplu@gmail.com> Co-authored-by: Mathieu Gilet <mathieu.gilet@zenika.com> Co-authored-by: Alexandre Coin <alexandre.coin@pix.fr>

--- api/lib/domain/read-models/ComplementaryCertificationCourseResultsForJuryCertification.js
+++ api/lib/domain/read-models/ComplementaryCertificationCourseResultsForJuryCertification.js
@@ -1,36 +1,21 @@
-const { PIX_EMPLOI_CLEA_V1, PIX_EMPLOI_CLEA_V2, PIX_EMPLOI_CLEA_V3, PIX_DROIT_MAITRE_CERTIF, PIX_DROIT_EXPERT_CERTIF } =
-  require('../models/Badge').keys;
-
 const complementaryCertificationStatus = {
   ACQUIRED: 'Validée',
   REJECTED: 'Rejetée',
 };
 
-const complementaryCertificationLabel = {
-  [PIX_EMPLOI_CLEA_V1]: 'CléA Numérique',
-  [PIX_EMPLOI_CLEA_V2]: 'CléA Numérique',
-  [PIX_EMPLOI_CLEA_V3]: 'CléA Numérique',
-  [PIX_DROIT_MAITRE_CERTIF]: 'Pix+ Droit Maître',
-  [PIX_DROIT_EXPERT_CERTIF]: 'Pix+ Droit Expert',
-};
-
 class ComplementaryCertificationCourseResultsForJuryCertification {
-  constructor({ id, partnerKey, acquired }) {
+  constructor({ id, partnerKey, acquired, label }) {
     this.id = id;
     this.partnerKey = partnerKey;
     this.acquired = acquired;
+    this.label = label;
   }
 
   get status() {
     return this.acquired ? complementaryCertificationStatus.ACQUIRED : complementaryCertificationStatus.REJECTED;
   }
-
-  get label() {
-    return complementaryCertificationLabel[this.partnerKey];
-  }
 }
 
 ComplementaryCertificationCourseResultsForJuryCertification.statuses = complementaryCertificationStatus;
-ComplementaryCertificationCourseResultsForJuryCertification.labels = complementaryCertificationLabel;
 
 module.exports = ComplementaryCertificationCourseResultsForJuryCertification;
--- api/tests/unit/domain/read-models/ComplementaryCertificationCourseResultsForJuryCertification_test.js
+++ api/tests/unit/domain/read-models/ComplementaryCertificationCourseResultsForJuryCertification_test.js
@@ -1,32 +1,7 @@
 const { expect } = require('../../../test-helper');
 const ComplementaryCertificationCourseResultsForJuryCertification = require('../../../../lib/domain/read-models/ComplementaryCertificationCourseResultsForJuryCertification');
-const { PIX_EMPLOI_CLEA_V1, PIX_EMPLOI_CLEA_V2, PIX_EMPLOI_CLEA_V3, PIX_DROIT_MAITRE_CERTIF, PIX_DROIT_EXPERT_CERTIF } =
-  require('../../../../lib/domain/models/Badge').keys;
 
 describe('Unit | Domain | Models | ComplementaryCertificationCourseResultsForJuryCertification', function () {
-  describe('#label', function () {
-    // eslint-disable-next-line mocha/no-setup-in-describe
-    [
-      { partnerKey: PIX_EMPLOI_CLEA_V1, expectedLabel: 'CléA Numérique' },
-      { partnerKey: PIX_EMPLOI_CLEA_V2, expectedLabel: 'CléA Numérique' },
-      { partnerKey: PIX_EMPLOI_CLEA_V3, expectedLabel: 'CléA Numérique' },
-      { partnerKey: PIX_DROIT_MAITRE_CERTIF, expectedLabel: 'Pix+ Droit Maître' },
-      { partnerKey: PIX_DROIT_EXPERT_CERTIF, expectedLabel: 'Pix+ Droit Expert' },
-    ].forEach(({ partnerKey, expectedLabel }) => {
-      it(`should return ${expectedLabel} for partner key ${partnerKey}`, function () {
-        // given
-        const complementaryCertificationCourseResultsForJuryCertification =
-          new ComplementaryCertificationCourseResultsForJuryCertification({ partnerKey });
-
-        // when
-        const label = complementaryCertificationCourseResultsForJuryCertification.label;
-
-        // then
-        expect(label).to.equal(expectedLabel);
-      });
-    });
-  });
-
   describe('#status', function () {
     describe('when the complementary certification course result is acquired', function () {
       it('should return Validée', function () {
