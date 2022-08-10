176b1f8f571914fb6f73d1640b129a6932e129d0
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-08-10 11:19:30 +0200

    sr(api): return event and assessment in complete-assessment usecase
    Co-authored-by: Yann Bertrand <yann.bertrand@pix.fr>
    Co-authored-by: Vincent Hardouin <vincent.hardouin@pix.fr>

--- api/lib/application/assessments/assessment-controller.js
+++ api/lib/application/assessments/assessment-controller.js
@@ -82,7 +82,8 @@ module.exports = {
 
     let event;
     await DomainTransaction.execute(async (domainTransaction) => {
-      event = await usecases.completeAssessment({ assessmentId, domainTransaction });
+      const result = await usecases.completeAssessment({ assessmentId, domainTransaction });
+      event = result.event;
     });
 
     await events.eventDispatcher.dispatch(event);
