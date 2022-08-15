cb32d5ee6887e70358f871503fecd8c5a381c1e4
Laura Bergoens <laura.bergoens@pix.fr>
2022-08-15 23:18:33 +0200

    add repo method to thematic and add competenceId attribute

--- api/lib/infrastructure/datasources/learning-content/thematic-datasource.js
+++ api/lib/infrastructure/datasources/learning-content/thematic-datasource.js
@@ -7,4 +7,9 @@ module.exports = datasource.extend({
     const thematics = await this.list();
     return thematics.filter((thematic) => competenceIds.includes(thematic.competenceId));
   },
+
+  async findByRecordIds(thematicIds) {
+    const thematics = await this.list();
+    return thematics.filter(({ id }) => thematicIds.includes(id));
+  },
 });
