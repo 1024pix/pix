945607744f4bf427017126211de9c90bd9f6293a
ajoanny <ajoanny@octo.com>
2022-08-17 14:19:54 +0200

    [API] create use case

---
+++ api/tests/unit/domain/usecases/archive-campaign-from-campaign-code_test.js
@@ -0,0 +1,35 @@
+const { expect, sinon } = require('../../../test-helper');
+const archiveCampaignFromCampaignCode = require('../../../../lib/domain/usecases/archive-campaign-from-campaign-code');
+const Campaign = require('../../../../lib/domain/models/CampaignForArchiving');
+
+describe('Unit | UseCase | archive-campaign', function () {
+  let campaignForArchivingRepository;
+  let clock;
+  let now;
+
+  beforeEach(function () {
+    now = new Date('2022-01-01');
+    clock = sinon.useFakeTimers(now);
+    campaignForArchivingRepository = {
+      getByCode: sinon.stub(),
+      save: sinon.stub(),
+    };
+  });
+
+  afterEach(function () {
+    clock.restore();
+  });
+
+  it('updates the campaign', async function () {
+    const userId = 1;
+    const campaignCode = 2;
+    const campaign = new Campaign({ id: 1, code: 'ABCD', archivedAt: null, archivedBy: null });
+    const expectedCampaign = new Campaign({ id: 1, code: 'ABCD', archivedAt: now, archivedBy: userId });
+    campaignForArchivingRepository.getByCode.resolves(campaign);
+
+    await archiveCampaignFromCampaignCode({ userId, campaignCode, campaignForArchivingRepository });
+
+    expect(campaignForArchivingRepository.getByCode).to.have.been.calledWithExactly(campaignCode);
+    expect(campaignForArchivingRepository.save).to.have.been.calledWith(expectedCampaign);
+  });
+});
