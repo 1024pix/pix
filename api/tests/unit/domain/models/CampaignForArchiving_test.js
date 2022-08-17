617b31f6e1ea5811b658120bd4d5e29c5c197a03
ajoanny <ajoanny@octo.com>
2022-08-17 14:19:29 +0200

    [API] create model campaign for archiving

---
+++ api/tests/unit/domain/models/CampaignForArchiving_test.js
@@ -0,0 +1,45 @@
+const { expect, catchErr } = require('../../../test-helper');
+const Campaign = require('../../../../lib/domain/models/CampaignForArchiving');
+const { ArchivedCampaignError, ObjectValidationError } = require('../../../../lib/domain/errors');
+
+describe('Unit | Domain | Models | CampaignForArchiving', function () {
+  describe('#archive', function () {
+    it('archives the campaigns', function () {
+      const campaign = new Campaign({ code: 'ABC123', archivedAt: null, archivedBy: null });
+
+      campaign.archive('2023-02-27', 1);
+
+      expect(campaign).to.deep.equal({ code: 'ABC123', archivedAt: '2023-02-27', archivedBy: 1 });
+    });
+
+    context('when the campaign is already archived', function () {
+      it('archives the campaigns', async function () {
+        const campaign = new Campaign({ code: 'ABC123', archivedAt: '2023-01-01', archivedBy: 2 });
+
+        const error = await catchErr(campaign.archive, campaign)('2023-02-27', 1);
+
+        expect(error).to.be.an.instanceOf(ArchivedCampaignError);
+      });
+    });
+
+    context('when the given date is  null', function () {
+      it('throws an exception', async function () {
+        const campaign = new Campaign({ id: 1, code: 'ABC123', archivedAt: null, archivedBy: null });
+
+        const error = await catchErr(campaign.archive, campaign)(null, 1);
+
+        expect(error).to.be.an.instanceOf(ObjectValidationError);
+      });
+    });
+
+    context('when the given userId is null', function () {
+      it('throws an exception', async function () {
+        const campaign = new Campaign({ id: 1, code: 'ABC123', archivedAt: null, archivedBy: null });
+
+        const error = await catchErr(campaign.archive, campaign)('2023-02-27', null);
+
+        expect(error).to.be.an.instanceOf(ObjectValidationError);
+      });
+    });
+  });
+});
