9c11c3449210d43add2c832c81232d499e9baec2
Andreia Pena <andreia.pena@pix.fr>
2022-08-17 10:49:34 +0200

    fix(api): rename feature toggle function

--- api/lib/application/preHandlers/feature-toggles.js
+++ api/lib/application/preHandlers/feature-toggles.js
@@ -2,7 +2,7 @@ const config = require('../../config');
 const { NotFoundError } = require('../../application/http-errors');
 
 module.exports = {
-  async checkIfSSOAccountReconciliationIsEnabled() {
+  async checkIfSsoAccountReconciliationIsEnabled() {
     if (!config.featureToggles.isSsoAccountReconciliationEnabled) {
       throw new NotFoundError('Cette route est désactivée');
     }
