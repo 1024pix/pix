a5475c73b445f1455d1f2a4dec432d1e3d2262ea
Laura Bergoens <laura.bergoens@pix.fr>
2022-08-17 23:25:57 +0200

    (api) simplifies targetProfileShareRepository.addTargetProfilesToOrganization method

--- api/lib/infrastructure/repositories/target-profile-share-repository.js
+++ api/lib/infrastructure/repositories/target-profile-share-repository.js
@@ -5,18 +5,9 @@ module.exports = {
     const targetProfileShareToAdd = targetProfileIdList.map((targetProfileId) => {
       return { organizationId, targetProfileId };
     });
-
-    const insertedTargetProfileShares = await knex('target-profile-shares')
+    await knex('target-profile-shares')
       .insert(targetProfileShareToAdd)
       .onConflict(['targetProfileId', 'organizationId'])
-      .ignore()
-      .returning('targetProfileId');
-
-    const attachedTargetProfileIds = insertedTargetProfileShares.map(({ targetProfileId }) => targetProfileId);
-    const duplicatedTargetProfileIds = targetProfileIdList.filter(
-      (targetProfileId) => !attachedTargetProfileIds.includes(targetProfileId)
-    );
-
-    return { duplicatedIds: duplicatedTargetProfileIds, attachedIds: attachedTargetProfileIds };
+      .ignore();
   },
 };
