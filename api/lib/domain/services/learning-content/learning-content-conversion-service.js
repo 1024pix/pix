82857aa59b3318a0cdec33040b208fef874511e6
Laura Bergoens <laura.bergoens@pix.fr>
2022-08-18 12:18:01 +0200

    (api) add service to get difficulty capped skills by capped tubes

---
+++ api/lib/domain/services/learning-content/learning-content-conversion-service.js
@@ -0,0 +1,15 @@
+const skillRepository = require('../../../infrastructure/repositories/skill-repository');
+
+async function findActiveSkillsForCappedTubes(cappedTubes) {
+  const skills = [];
+  for (const cappedTube of cappedTubes) {
+    const skillsForTube = await skillRepository.findActiveByTubeId(cappedTube.id);
+    const skillsCapped = skillsForTube.filter((skill) => skill.difficulty <= parseInt(cappedTube.level));
+    skills.push(...skillsCapped);
+  }
+  return skills;
+}
+
+module.exports = {
+  findActiveSkillsForCappedTubes,
+};
