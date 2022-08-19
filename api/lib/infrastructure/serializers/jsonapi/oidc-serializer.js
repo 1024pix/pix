aa26cb812c964061b549c87465eec4b87846fa32
Andreia Pena <andreia.pena@pix.fr>
2022-08-19 18:13:20 +0200

    feat(api): serialize full names, and authentication methods

--- api/lib/infrastructure/serializers/jsonapi/oidc-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/oidc-serializer.js
@@ -3,7 +3,15 @@ const { Serializer } = require('jsonapi-serializer');
 module.exports = {
   serialize(authenticationContent) {
     return new Serializer('user-oidc-authentication-requests', {
-      attributes: ['accessToken', 'logoutUrlUUID'],
+      attributes: [
+        'accessToken',
+        'logoutUrlUUID',
+        'fullNameFromPix',
+        'fullNameFromExternalIdentityProvider',
+        'email',
+        'username',
+        'authenticationMethods',
+      ],
     }).serialize(authenticationContent);
   },
 };
