15866020ab417e9f32ca795401cb6e3c4692e63e
lherrador <lucas.herrador@octo.com>
2022-08-24 15:32:22 +0200

    Create campaign skills relationship table script
    Co-authored-by: Nicolas Lepage <nicolas.lepage@pix.fr>
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>

---
+++ api/db/migrations/20220824131510_create-campaign-skill-relationship-table.js
@@ -0,0 +1,16 @@
+const CAMPAIGN_TABLE_NAME = 'campaigns';
+const CAMPAIGN_SKILL_TABLE_NAME = 'campaign_skills';
+const CAMPAIGN_ID_COLUMN = 'campaignId';
+const SKILL_ID_COLUMN = 'skillId';
+
+exports.up = async function (knex) {
+  await knex.schema.createTable(CAMPAIGN_SKILL_TABLE_NAME, (t) => {
+    t.integer(CAMPAIGN_ID_COLUMN).notNullable().references(`${CAMPAIGN_TABLE_NAME}.id`);
+    t.string(SKILL_ID_COLUMN).notNullable();
+    t.primary([CAMPAIGN_ID_COLUMN, SKILL_ID_COLUMN]);
+  });
+};
+
+exports.down = async function (knex) {
+  await knex.schema.dropTable(CAMPAIGN_SKILL_TABLE_NAME);
+};
