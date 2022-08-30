538a6a3b698700289bad6a0a932502f398cdf63b
Lise Quesnel <quesnel.lise@gmail.com>
2022-08-30 11:57:23 +0200

    feat(orga): add nullable boolean transform

--- orga/app/models/sco-organization-participant.js
+++ orga/app/models/sco-organization-participant.js
@@ -21,7 +21,7 @@ export default class ScoOrganizationParticipant extends Model {
   @attr('string') campaignName;
   @attr('string') campaignType;
   @attr('string') participationStatus;
-  @attr() isCertifiable;
+  @attr('nullable-boolean') isCertifiable;
   @belongsTo('organization') organization;
 
   get hasUsername() {
