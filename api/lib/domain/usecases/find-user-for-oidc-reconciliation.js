3bfc48e0d0c31c53cd430ecc91f188c1d37635e0
yschoueri <yasmina.schoueri@gmail.com>
2022-08-31 13:53:28 +0200

    api: remove from code parts that will never happen

--- api/lib/domain/usecases/find-user-for-oidc-reconciliation.js
+++ api/lib/domain/usecases/find-user-for-oidc-reconciliation.js
@@ -7,10 +7,14 @@ module.exports = async function findUserForOidcReconciliation({
   identityProvider,
   authenticationSessionService,
   pixAuthenticationService,
-  oidcAuthenticationService,
   authenticationMethodRepository,
   userRepository,
 }) {
+  const sessionContentAndUserInfo = await authenticationSessionService.getByKey(authenticationKey);
+  if (!sessionContentAndUserInfo) {
+    throw new AuthenticationKeyExpired();
+  }
+
   const foundUser = await pixAuthenticationService.getUserByUsernameAndPassword({
     username: email,
     password,
@@ -18,72 +22,27 @@ module.exports = async function findUserForOidcReconciliation({
   });
 
   const authenticationMethods = await authenticationMethodRepository.findByUserId({ userId: foundUser.id });
-
-  const sessionContentAndUserInfo = await authenticationSessionService.getByKey(authenticationKey);
-  if (!sessionContentAndUserInfo) {
-    throw new AuthenticationKeyExpired();
-  }
-
   const oidcAuthenticationMethod = authenticationMethods.find(
     (authenticationMethod) => authenticationMethod.identityProvider === identityProvider
   );
 
-  if (!oidcAuthenticationMethod) {
-    sessionContentAndUserInfo.userInfo.userId = foundUser.id;
-    await authenticationSessionService.update(authenticationKey, sessionContentAndUserInfo);
-
-    const fullNameFromPix = `${foundUser.firstName} ${foundUser.lastName}`;
-    const fullNameFromExternalIdentityProvider = `${sessionContentAndUserInfo.userInfo.firstName} ${sessionContentAndUserInfo.userInfo.lastName}`;
-
-    return {
-      fullNameFromPix,
-      fullNameFromExternalIdentityProvider,
-      email: foundUser.email,
-      username: foundUser.username,
-      authenticationMethods,
-    };
-  }
-
   const isSameExternalIdentifier =
-    oidcAuthenticationMethod.externalIdentifier === sessionContentAndUserInfo.userInfo.externalIdentityId;
-
-  if (!isSameExternalIdentifier) {
+    oidcAuthenticationMethod?.externalIdentifier === sessionContentAndUserInfo.userInfo.externalIdentityId;
+  if (oidcAuthenticationMethod && !isSameExternalIdentifier) {
     throw new DifferentExternalIdentifierError();
   }
 
-  await _updateAuthenticationComplement({
-    identityProvider,
-    userId: foundUser.id,
-    sessionContent: sessionContentAndUserInfo.sessionContent,
-    oidcAuthenticationService,
-    authenticationMethodRepository,
-  });
+  sessionContentAndUserInfo.userInfo.userId = foundUser.id;
+  await authenticationSessionService.update(authenticationKey, sessionContentAndUserInfo);
 
-  const accessToken = await oidcAuthenticationService.createAccessToken(foundUser.id);
+  const fullNameFromPix = `${foundUser.firstName} ${foundUser.lastName}`;
+  const fullNameFromExternalIdentityProvider = `${sessionContentAndUserInfo.userInfo.firstName} ${sessionContentAndUserInfo.userInfo.lastName}`;
 
-  const logoutUrlUUID = await oidcAuthenticationService.saveIdToken({
-    idToken: sessionContentAndUserInfo.sessionContent.idToken,
-    userId: foundUser.id,
-  });
-
-  userRepository.updateLastLoggedAt({ userId: foundUser.id });
-
-  return { accessToken, logoutUrlUUID };
+  return {
+    fullNameFromPix,
+    fullNameFromExternalIdentityProvider,
+    email: foundUser.email,
+    username: foundUser.username,
+    authenticationMethods,
+  };
 };
-
-async function _updateAuthenticationComplement({
-  identityProvider,
-  userId,
-  sessionContent,
-  oidcAuthenticationService,
-  authenticationMethodRepository,
-}) {
-  const authenticationComplement = oidcAuthenticationService.createAuthenticationComplement({ sessionContent });
-  if (!authenticationComplement) return;
-
-  await authenticationMethodRepository.updateAuthenticationComplementByUserIdAndIdentityProvider({
-    authenticationComplement,
-    userId,
-    identityProvider,
-  });
-}
