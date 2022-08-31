9d424d207bd53aa781363d13c830e35e22277b0c
Laura Bergoens <laura.bergoens@pix.fr>
2022-08-31 12:27:03 +0200

    refacto(api): script populate-organizations-email

--- api/scripts/populate-organizations-email.js
+++ api/scripts/populate-organizations-email.js
@@ -1,7 +1,6 @@
 const bluebird = require('bluebird');
-
+const { disconnect } = require('../db/knex-database-connection');
 const { parseCsvWithHeader } = require('../scripts/helpers/csvHelpers');
-
 const BookshelfOrganization = require('../lib/infrastructure/orm-models/Organization');
 
 async function updateOrganizationEmailByExternalId(externalId, email) {
@@ -22,38 +21,37 @@ async function populateOrganizations(objectsFromFile) {
   });
 }
 
+const isLaunchedFromCommandLine = require.main === module;
+
 async function main() {
   console.log("Start populating organization's email");
+  const filePath = process.argv[2];
 
-  try {
-    const filePath = process.argv[2];
-
-    if (!filePath) {
-      console.log('Usage: populate-organizations-email.js <FILE_NAME>.csv');
-      process.exit(1);
-    }
-
-    console.log('Reading and parsing csv file (checking headers)... ');
-    const csvData = await parseCsvWithHeader(filePath);
-    console.log(`Successfully read ${csvData.length} records.`);
-
-    console.log('Populating organizations (existing emails will be updated)... ');
-    await populateOrganizations(csvData);
-    console.log('\nOrganization successfully populated.');
-  } catch (error) {
-    console.error('\n', error);
+  if (!filePath) {
+    console.log('Usage: populate-organizations-email.js <FILE_NAME>.csv');
     process.exit(1);
   }
+
+  console.log('Reading and parsing csv file (checking headers)... ');
+  const csvData = await parseCsvWithHeader(filePath);
+  console.log(`Successfully read ${csvData.length} records.`);
+
+  console.log('Populating organizations (existing emails will be updated)... ');
+  await populateOrganizations(csvData);
+  console.log('\nOrganization successfully populated.');
 }
 
-if (require.main === module) {
-  main().then(
-    () => process.exit(0),
-    (err) => {
-      console.error(err);
-      process.exit(1);
+(async () => {
+  if (isLaunchedFromCommandLine) {
+    try {
+      await main();
+    } catch (error) {
+      console.error(error);
+      process.exitCode = 1;
+    } finally {
+      await disconnect();
     }
-  );
-}
+  }
+})();
 
 module.exports = populateOrganizations;
