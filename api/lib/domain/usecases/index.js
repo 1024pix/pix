d55d894aaf955419f70284b4e6fc57591dfb749b
P-Jeremy <jemyplu@gmail.com>
2022-09-05 15:42:15 +0200

    :recycle: api: remove dead code

--- api/lib/domain/usecases/index.js
+++ api/lib/domain/usecases/index.js
@@ -58,7 +58,6 @@ const dependencies = {
   certificationResultRepository: require('../../infrastructure/repositories/certification-result-repository'),
   challengeRepository: require('../../infrastructure/repositories/challenge-repository'),
   challengeForPixAutoAnswerRepository: require('../../infrastructure/repositories/challenge-for-pix-auto-answer-repository'),
-  cleaCertificationResultRepository: require('../../infrastructure/repositories/clea-certification-result-repository'),
   competenceEvaluationRepository: require('../../infrastructure/repositories/competence-evaluation-repository'),
   competenceMarkRepository: require('../../infrastructure/repositories/competence-mark-repository'),
   competenceRepository: require('../../infrastructure/repositories/competence-repository'),
