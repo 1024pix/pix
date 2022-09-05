b2d4d1c674718023bd6c2bdf92d69da6d9b9134d
Rousseau Corentin <cro@eonix.be>
2022-09-05 14:08:18 +0200

    feat(mon-pix): use sidebar component in user-tutorials pages

--- mon-pix/app/controllers/authenticated/user-tutorials/recommended.js
+++ mon-pix/app/controllers/authenticated/user-tutorials/recommended.js
@@ -1,10 +1,24 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class RecommendedController extends Controller {
+  @tracked isSidebarVisible = false;
+
   pageOptions = [
     { label: '10', value: 10 },
     { label: '20', value: 20 },
     { label: '50', value: 50 },
     { label: '100', value: 100 },
   ];
+
+  @action
+  showSidebar() {
+    this.isSidebarVisible = true;
+  }
+
+  @action
+  closeSidebar() {
+    this.isSidebarVisible = false;
+  }
 }
