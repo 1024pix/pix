f5d1a35b6d067f0feacd92c654d340a93d1b017c
P-Jeremy <jemyplu@gmail.com>
2022-09-06 15:56:21 +0200

    :hammer: api: add switch-campaign-to-flash script

---
+++ api/tests/integration/scripts/switch-campaign-to-flash_test.js
@@ -0,0 +1,27 @@
+const { expect, databaseBuilder, knex } = require('../../test-helper');
+
+const { switchCampaignToFlash } = require('../../../scripts/switch-campaign-to-flash.js');
+
+describe('Integration | Scripts | switch-campaign-to-flash.js', function () {
+  describe('#switchCampaignToFlash', function () {
+    it('should switch campaign assessment-method from smart-random to flash', async function () {
+      // given
+      const campaignId = databaseBuilder.factory.buildCampaign({ assessmentMethod: 'SMART_RANDOM' }).id;
+      const nonModifiedCampaignId = databaseBuilder.factory.buildCampaign({ assessmentMethod: 'SMART_RANDOM' }).id;
+
+      await databaseBuilder.commit();
+
+      // when
+      await switchCampaignToFlash(campaignId);
+
+      // then
+      const modifiedCampaign = await knex('campaigns').select('assessmentMethod').where({ id: campaignId }).first();
+      const nonModifiedCampaign = await knex('campaigns')
+        .select('assessmentMethod')
+        .where({ id: nonModifiedCampaignId })
+        .first();
+      expect(modifiedCampaign.assessmentMethod).to.equal('FLASH');
+      expect(nonModifiedCampaign.assessmentMethod).to.equal('SMART_RANDOM');
+    });
+  });
+});
