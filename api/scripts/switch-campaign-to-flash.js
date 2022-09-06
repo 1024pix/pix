59714cf93299627481dd5fc096d9a98ea1e468c6
P-Jeremy <jemyplu@gmail.com>
2022-09-06 15:56:21 +0200

    :hammer: api: add switch-campaign-to-flash script

---
+++ api/scripts/switch-campaign-to-flash.js
@@ -0,0 +1,28 @@
+'use strict';
+const _ = require('lodash');
+require('dotenv').config();
+const { knex } = require('../db/knex-database-connection');
+const Assessment = require('../lib/domain/models/Assessment');
+
+async function switchCampaignToFlash(id) {
+  await knex('campaigns').update({ assessmentMethod: Assessment.methods.FLASH }).where({ id });
+}
+
+async function main(id) {
+  if (!_.isFinite(id)) {
+    throw new Error("L'id de campagne est obligatoire, node switch-campaign-to-flash.js <:id>");
+  }
+
+  await switchCampaignToFlash(id);
+}
+
+if (require.main === module) {
+  const id = parseInt(process.argv[2]);
+
+  main(id).catch((err) => {
+    console.error(err);
+    process.exitCode = 1; // ou throw err
+  });
+}
+
+module.exports = { switchCampaignToFlash };
