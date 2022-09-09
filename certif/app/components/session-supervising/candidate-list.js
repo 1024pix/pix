1ffcc0cb091a3827b35fb05c8c753d68b9f1785e
Quentin Lebouc <quentin.lebouc@pix.fr>
2022-09-09 12:22:40 +0200

    :sparkles: certif: add filter on candidate list
    Co-authored-by: Antoine Ceol <antoine.ceol@gmail.com>

--- certif/app/components/session-supervising/candidate-list.js
+++ certif/app/components/session-supervising/candidate-list.js
@@ -1,7 +1,10 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class CandidateList extends Component {
+  @tracked filter = '';
+
   @action
   async toggleCandidate(candidate) {
     await this.args.toggleCandidate(candidate);
@@ -15,5 +18,18 @@ export default class CandidateList extends Component {
   @action
   async endAssessmentBySupervisor(candidate) {
     await this.args.endAssessmentBySupervisor(candidate);
+  }
+
+  @action
+  setFilter(event) {
+    this.filter = event.target.value;
+  }
+
+  get filteredCandidates() {
+    return this.args.candidates.filter(
+      (candidate) =>
+        candidate.firstName.toLowerCase().indexOf(this.filter.toLowerCase()) === 0 ||
+        candidate.lastName.toLowerCase().indexOf(this.filter.toLowerCase()) === 0
+    );
   }
 }
