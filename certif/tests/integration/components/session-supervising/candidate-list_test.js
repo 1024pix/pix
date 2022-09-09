678271a99a4f572a5a70b60ec4db5e039ea1e380
Quentin Lebouc <quentin.lebouc@pix.fr>
2022-09-09 11:37:56 +0200

    :recycle: certif: remove useless sessionForSupervising and fix modules Co-authored-by: Antoine Ceol <antoine.ceol@gmail.com>

--- certif/tests/integration/components/session-supervising/candidate-list_test.js
+++ certif/tests/integration/components/session-supervising/candidate-list_test.js
@@ -16,64 +16,50 @@ module('Integration | Component | SessionSupervising::CandidateList', function (
   module('when there are candidates', function () {
     test('it renders the candidates information', async function (assert) {
       // given
-      this.sessionForSupervising = store.createRecord('session-for-supervising', {
-        certificationCandidates: [
-          store.createRecord('certification-candidate-for-supervising', {
-            id: 123,
-            firstName: 'Gamora',
-            lastName: 'Zen Whoberi Ben Titan',
-            birthdate: '1984-05-28',
-            extraTimePercentage: '8',
-            authorizedToStart: true,
-            assessmentStatus: null,
-          }),
-          store.createRecord('certification-candidate-for-supervising', {
-            id: 456,
-            firstName: 'Star',
-            lastName: 'Lord',
-            birthdate: '1983-06-28',
-            extraTimePercentage: '12',
-            authorizedToStart: false,
-            assessmentStatus: null,
-          }),
-          store.createRecord('certification-candidate-for-supervising', {
-            id: 789,
-            firstName: 'Rocket',
-            lastName: 'Racoon',
-            birthdate: '1982-07-28',
-            extraTimePercentage: null,
-            authorizedToStart: true,
-            assessmentStatus: 'started',
-          }),
-        ],
-      });
+      this.certificationCandidates = [
+        store.createRecord('certification-candidate-for-supervising', {
+          id: 123,
+          firstName: 'Gamora',
+          lastName: 'Zen Whoberi Ben Titan',
+          birthdate: '1984-05-28',
+          extraTimePercentage: '8',
+          authorizedToStart: true,
+          assessmentStatus: null,
+        }),
+        store.createRecord('certification-candidate-for-supervising', {
+          id: 456,
+          firstName: 'Star',
+          lastName: 'Lord',
+          birthdate: '1983-06-28',
+          extraTimePercentage: '12',
+          authorizedToStart: false,
+          assessmentStatus: null,
+        }),
+      ];
 
       // when
       const screen = await renderScreen(
-        hbs`<SessionSupervising::CandidateList @candidates={{this.sessionForSupervising.certificationCandidates}}  />`
+        hbs`<SessionSupervising::CandidateList @candidates={{this.certificationCandidates}}  />`
       );
 
       // then
       assert.dom(screen.getByText('Zen Whoberi Ben Titan Gamora')).exists();
       assert.dom(screen.getByText('Lord Star')).exists();
-      assert.dom(screen.getByText('Racoon Rocket')).exists();
     });
+  });
 
-    module('when there is no candidate', function () {
-      test('it renders a message', async function (assert) {
-        // given
-        this.sessionForSupervising = store.createRecord('session-for-supervising', {
-          certificationCandidates: [],
-        });
+  module('when there is no candidate', function () {
+    test('it renders a message', async function (assert) {
+      // given
+      this.certificationCandidates = [];
 
-        // when
-        const screen = await renderScreen(
-          hbs`<SessionSupervising::CandidateList @candidates={{this.sessionForSupervising.certificationCandidates}} />`
-        );
+      // when
+      const screen = await renderScreen(
+        hbs`<SessionSupervising::CandidateList @candidates={{this.certificationCandidates}} />`
+      );
 
-        // then
-        assert.dom(screen.getByText('Aucun candidat inscrit à cette session')).exists();
-      });
+      // then
+      assert.dom(screen.getByText('Aucun candidat inscrit à cette session')).exists();
     });
   });
 });
