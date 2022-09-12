a7ef6fc099613a5f7a24e80f2cc066543162019e
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2022-09-12 17:11:31 +0200

    feat(api): Add user-organization serializer
    Co-authored-by: Marc-Aurèle Darche <marc-aurele.darche@pix.fr>
    Co-authored-by: Ismael Gorissen <ismael.gorissen@gmail.com>
    Co-authored-by: Anne-G <ang.schall@gmail.com>
    Co-authored-by: Anne-Marie Esteves <anne-marie.esteves@pix.fr>
    Co-authored-by: Andreia Pena <andreia.pena@pix.fr>

---
+++ api/tests/unit/infrastructure/serializers/jsonapi/user-organization-for-admin-serializer_test.js
@@ -0,0 +1,65 @@
+const { expect } = require('../../../../test-helper');
+const UserOrganizationForAdmin = require('../../../../../lib/domain/read-models/UserOrganizationForAdmin');
+const serializer = require('../../../../../lib/infrastructure/serializers/jsonapi/user-organization-for-admin-serializer');
+
+describe('Unit | Serializer | JSONAPI | user-organization-for-admin-serializer', function () {
+  describe('#serialize', function () {
+    it('should serialize a user’s organization memberships', function () {
+      // given
+      const userOrganizationForAdmin1 = new UserOrganizationForAdmin({
+        id: 42,
+        updatedAt: '2001-01-01',
+        role: 'MEMBER',
+        organizationId: 52,
+        organizationName: 'Organization 1',
+        organizationType: 'SCO',
+        organizationExternalId: '1234',
+      });
+      const userOrganizationForAdmin2 = new UserOrganizationForAdmin({
+        id: 43,
+        updatedAt: '2001-01-01',
+        role: 'ADMIN',
+        organizationId: 53,
+        organizationName: 'Organization 2',
+        organizationType: 'SUP',
+        organizationExternalId: '5678',
+      });
+      const modelObjects = [userOrganizationForAdmin1, userOrganizationForAdmin2];
+
+      // when
+      const json = serializer.serialize(modelObjects);
+
+      // then
+      expect(json).to.be.deep.equal({
+        data: [
+          {
+            attributes: {
+              id: 42,
+              'organization-external-id': '1234',
+              'organization-id': 52,
+              'organization-name': 'Organization 1',
+              'organization-type': 'SCO',
+              role: 'MEMBER',
+              'updated-at': '2001-01-01',
+            },
+            id: '42',
+            type: 'organization-memberships',
+          },
+          {
+            attributes: {
+              id: 43,
+              'organization-external-id': '5678',
+              'organization-id': 53,
+              'organization-name': 'Organization 2',
+              'organization-type': 'SUP',
+              role: 'ADMIN',
+              'updated-at': '2001-01-01',
+            },
+            id: '43',
+            type: 'organization-memberships',
+          },
+        ],
+      });
+    });
+  });
+});
