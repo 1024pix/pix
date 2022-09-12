d85e8956e68fad1f68b043f2eed74d6a82b9d203
alexandrecoin <alexandre.pc.coin@gmail.com>
2022-09-12 17:40:52 +0200

    :recycle: api: remove unused getCleaCertificationScoring method

--- api/lib/infrastructure/repositories/partner-certification-scoring-repository.js
+++ api/lib/infrastructure/repositories/partner-certification-scoring-repository.js
@@ -1,39 +1,9 @@
 const { knex } = require('../../../db/knex-database-connection');
 const DomainTransaction = require('../DomainTransaction');
 const ComplementaryCertificationCourseResultBookshelf = require('../orm-models/ComplementaryCertificationCourseResult');
-const CleaCertificationScoring = require('../../domain/models/CleaCertificationScoring');
-const Badge = require('../../domain/models/Badge');
 const ComplementaryCertificationCourseResult = require('../../domain/models/ComplementaryCertificationCourseResult');
-const ComplementaryCertification = require('../../domain/models/ComplementaryCertification');
-const _ = require('lodash');
 
 module.exports = {
-  async getCleaCertificationScoring({
-    complementaryCertificationCourseId,
-    certificationCourseId,
-    userId,
-    reproducibilityRate,
-    domainTransaction = DomainTransaction.emptyTransaction(),
-  }) {
-    const cleaBadgeKey = await _getAcquiredCleaBadgeKey(userId, certificationCourseId, domainTransaction);
-    const hasAcquiredBadge = Boolean(cleaBadgeKey);
-    if (!hasAcquiredBadge) {
-      return CleaCertificationScoring.buildNotEligible({ complementaryCertificationCourseId });
-    }
-    const pixScore = await _getLatestPixScoreByCertificationCourseId(certificationCourseId);
-    const { minimumEarnedPix, minimumReproducibilityRate } = await _getMinimumReproducibilityRateAndMinimumEarnedPix();
-
-    return new CleaCertificationScoring({
-      complementaryCertificationCourseId,
-      hasAcquiredBadge,
-      reproducibilityRate,
-      cleaBadgeKey,
-      pixScore,
-      minimumEarnedPix,
-      minimumReproducibilityRate,
-    });
-  },
-
   async save({ partnerCertificationScoring, domainTransaction = DomainTransaction.emptyTransaction() }) {
     const partnerCertificationToSave = new ComplementaryCertificationCourseResultBookshelf(
       _adaptModelToDB({
@@ -70,49 +40,4 @@ module.exports = {
 
 function _adaptModelToDB({ complementaryCertificationCourseId, partnerKey, source, acquired }) {
   return { complementaryCertificationCourseId, partnerKey, source, acquired };
-}
-
-async function _getAcquiredCleaBadgeKey(userId, certificationCourseId, domainTransaction) {
-  const badgeAcquisitionQuery = knex('badge-acquisitions')
-    .pluck('badges.key')
-    .innerJoin('badges', 'badges.id', 'badgeId')
-    .innerJoin('certification-courses', 'certification-courses.userId', 'badge-acquisitions.userId')
-    .where('badge-acquisitions.userId', userId)
-    .whereIn('badges.key', [
-      Badge.keys.PIX_EMPLOI_CLEA_V1,
-      Badge.keys.PIX_EMPLOI_CLEA_V2,
-      Badge.keys.PIX_EMPLOI_CLEA_V3,
-    ])
-    .where('certification-courses.id', certificationCourseId)
-    .where('badge-acquisitions.createdAt', '<', knex.ref('certification-courses.createdAt'))
-    .orderBy('badge-acquisitions.createdAt', 'DESC');
-  if (domainTransaction.knexTransaction) {
-    badgeAcquisitionQuery.transacting(domainTransaction.knexTransaction);
-  }
-  const [acquiredBadgeKey] = await badgeAcquisitionQuery;
-  return acquiredBadgeKey;
-}
-
-async function _getLatestPixScoreByCertificationCourseId(certificationCourseId) {
-  const { pixScore } = await knex
-    .select('assessment-results.pixScore')
-    .from('assessments')
-    .innerJoin('assessment-results', 'assessment-results.assessmentId', 'assessments.id')
-    .where({ 'assessments.certificationCourseId': certificationCourseId })
-    .orderBy('assessment-results.createdAt', 'DESC')
-    .first();
-
-  return pixScore;
-}
-
-async function _getMinimumReproducibilityRateAndMinimumEarnedPix() {
-  const { minimumReproducibilityRate, minimumEarnedPix } = await knex('complementary-certifications')
-    .select('minimumReproducibilityRate', 'minimumEarnedPix')
-    .where({ key: ComplementaryCertification.CLEA })
-    .first();
-
-  return {
-    minimumEarnedPix,
-    minimumReproducibilityRate: _.toNumber(minimumReproducibilityRate),
-  };
 }
