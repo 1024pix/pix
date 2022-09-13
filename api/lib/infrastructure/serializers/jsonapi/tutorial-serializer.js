b63ef6fe9eddbdc0cbbf22e785bacfaa13401c0f
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-09-13 15:06:19 +0200

    feat(api): handle backward retrocompatibility for tutorial serializer
    Co-authored-by: Clarisse Damon <clarisse.damon@pix.fr>

--- api/lib/infrastructure/serializers/jsonapi/tutorial-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/tutorial-serializer.js
@@ -5,6 +5,12 @@ const userSavedTutorialAttributes = require('./user-saved-tutorial-attributes');
 module.exports = {
   serialize(tutorial = {}, pagination) {
     return new Serializer('tutorials', {
+      transform(tutorial) {
+        return {
+          ...tutorial,
+          userTutorial: tutorial.userSavedTutorial,
+        };
+      },
       attributes: [
         'duration',
         'format',
@@ -16,12 +22,16 @@ module.exports = {
         'tubePracticalDescription',
         'tutorialEvaluation',
         'userSavedTutorial',
+        'userTutorial',
         'skillId',
       ],
       tutorialEvaluation: tutorialEvaluationAttributes,
       userSavedTutorial: userSavedTutorialAttributes,
+      userTutorial: userSavedTutorialAttributes,
       typeForAttribute(attribute) {
-        return attribute === 'userSavedTutorial' ? 'user-saved-tutorial' : attribute;
+        if (attribute === 'userSavedTutorial') return 'user-saved-tutorial';
+        if (attribute === 'userTutorial') return 'user-tutorial';
+        return attribute;
       },
       meta: pagination,
     }).serialize(tutorial);
