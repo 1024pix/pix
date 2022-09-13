e0e3ffeeb3abd03df13ad09388b853bff640019f
Anne-Marie Esteves <anne-marie.esteves@octo.com>
2022-09-13 09:29:29 +0200

    feat(orga): update prescriber and organization model to get organisation idp name

--- orga/tests/unit/models/prescriber_test.js
+++ orga/tests/unit/models/prescriber_test.js
@@ -5,11 +5,44 @@ import { run } from '@ember/runloop';
 module('Unit | Model | prescriber', function (hooks) {
   setupTest(hooks);
 
+  module('#hasCurrentOrganizationWithGARAsIdentityProvider', function () {
+    test('it should return false if the current organization has not GAR as identity provider', function (assert) {
+      // given
+      const store = this.owner.lookup('service:store');
+      const organization = run(() => store.createRecord('organization', { name: 'Willow school' }));
+      const userOrgaSettings = run(() => store.createRecord('userOrgaSetting', { organization }));
+      const membership = run(() => store.createRecord('membership', { organizationRole: 'MEMBER', organization }));
+      const memberships = [membership];
+      const model = run(() => store.createRecord('prescriber', { memberships, userOrgaSettings }));
+
+      // when / then
+      assert.false(model.hasCurrentOrganizationWithGARAsIdentityProvider);
+    });
+
+    test('it should return true if the current organization has GAR as identity provider', function (assert) {
+      // given
+      const store = this.owner.lookup('service:store');
+      const organization = run(() =>
+        store.createRecord('organization', {
+          name: 'Willow school',
+          identityProviderForCampaigns: 'GAR',
+        })
+      );
+      const userOrgaSettings = run(() => store.createRecord('userOrgaSetting', { organization }));
+      const membership = run(() => store.createRecord('membership', { organizationRole: 'MEMBER', organization }));
+      const memberships = [membership];
+      const model = run(() => store.createRecord('prescriber', { memberships, userOrgaSettings }));
+
+      // when / then
+      assert.true(model.hasCurrentOrganizationWithGARAsIdentityProvider);
+    });
+  });
+
   module('#isAdminOfTheCurrentOrganization', function () {
     test('it should return true if prescriber is ADMIN of the current organization', function (assert) {
       // given
       const store = this.owner.lookup('service:store');
-      const organization = store.createRecord('organization', { name: 'Willow school' });
+      const organization = run(() => store.createRecord('organization', { name: 'Willow school' }));
       const userOrgaSettings = run(() => store.createRecord('userOrgaSetting', { organization }));
       const membership = run(() => store.createRecord('membership', { organizationRole: 'ADMIN', organization }));
       const memberships = [membership];
@@ -22,7 +55,7 @@ module('Unit | Model | prescriber', function (hooks) {
     test('it should return false if prescriber is MEMBER of the current organization', function (assert) {
       // given
       const store = this.owner.lookup('service:store');
-      const organization = store.createRecord('organization', { name: 'Willow school' });
+      const organization = run(() => store.createRecord('organization', { name: 'Willow school' }));
       const userOrgaSettings = run(() => store.createRecord('userOrgaSetting', { organization }));
       const membership = run(() => store.createRecord('membership', { organizationRole: 'MEMBER', organization }));
       const memberships = [membership];
@@ -35,8 +68,8 @@ module('Unit | Model | prescriber', function (hooks) {
     test('it should return false if prescriber is MEMBER of the current organization and ADMIN in another', function (assert) {
       // given
       const store = this.owner.lookup('service:store');
-      const currentOrganization = store.createRecord('organization', { id: 7, name: 'Willow school' });
-      const otherOrganization = store.createRecord('organization', { id: 123, name: 'Tanglewood school' });
+      const currentOrganization = run(() => store.createRecord('organization', { id: 7, name: 'Willow school' }));
+      const otherOrganization = run(() => store.createRecord('organization', { id: 123, name: 'Tanglewood school' }));
       const userOrgaSettings = run(() => store.createRecord('userOrgaSetting', { organization: currentOrganization }));
       const membership = run(() =>
         store.createRecord('membership', { organizationRole: 'MEMBER', currentOrganization })
