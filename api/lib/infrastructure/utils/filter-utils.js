19f467854fd04cab0808322032965cdb20f02cca
ajoanny <ajoanny@octo.com>
2022-09-14 15:18:07 +0200

    [API] correct test for keyword proximity

--- api/lib/infrastructure/utils/filter-utils.js
+++ api/lib/infrastructure/utils/filter-utils.js
@@ -5,6 +5,9 @@ module.exports = {
   filterByFullName(queryBuilder, search, firstName, lastName) {
     const searchLowerCase = search.trim().toLowerCase();
     queryBuilder.where(function () {
+      //the search is performed by pg_search which uses the distances between the pattern and the data
+      //if the distance is too large then pg-search won't find anything
+      //this is why the others where are used
       this.where(knex.raw(`CONCAT (??, ' ', ??) <-> ?`, [firstName, lastName, searchLowerCase]), '<=', DISTANCE);
       this.orWhereRaw('LOWER(??) LIKE ?', [firstName, `%${searchLowerCase}%`]);
       this.orWhereRaw('LOWER(??) LIKE ?', [lastName, `%${searchLowerCase}%`]);
