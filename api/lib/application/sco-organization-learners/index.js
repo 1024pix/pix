12c9dc9a4cdc62ab45e0786d68fa6008f02a6b2d
Lise Quesnel <quesnel.lise@gmail.com>
2022-09-14 15:36:11 +0200

    clean(api): remove post schooling-registration-dependent-users/recover-account

--- api/lib/application/sco-organization-learners/index.js
+++ api/lib/application/sco-organization-learners/index.js
@@ -233,35 +233,6 @@ exports.register = async function (server) {
     },
     {
       method: 'POST',
-      path: '/api/schooling-registration-dependent-users/recover-account',
-      config: {
-        auth: false,
-        handler: scoOrganizationLearnerController.checkScoAccountRecovery,
-        validate: {
-          payload: Joi.object({
-            data: {
-              attributes: {
-                'first-name': Joi.string().empty(Joi.string().regex(/^\s*$/)).required(),
-                'last-name': Joi.string().empty(Joi.string().regex(/^\s*$/)).required(),
-                'ine-ina': Joi.alternatives().try(
-                  Joi.string().regex(inePattern).required(),
-                  Joi.string().regex(inaPattern).required()
-                ),
-                birthdate: Joi.date().format('YYYY-MM-DD').required(),
-              },
-            },
-          }).options({ allowUnknown: true }),
-        },
-        notes: [
-          "- Recherche d'un ancien élève par son ine/ina, prénom, nom, date de naissance \n" +
-            '- On renvoie les informations permettant de récupérer son compte Pix.',
-          "- L'usage de cette route est **dépréciée** en faveur de /api/sco-organization-learners/account-recovery",
-        ],
-        tags: ['api', 'organizationLearnerDependentUser', 'recovery'],
-      },
-    },
-    {
-      method: 'POST',
       path: '/api/sco-organization-learners/account-recovery',
       config: {
         auth: false,
