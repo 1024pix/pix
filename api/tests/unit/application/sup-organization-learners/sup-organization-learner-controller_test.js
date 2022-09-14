3b2fe2f65a78673f3e11aa86114939a2c66e7bd4
Lise Quesnel <quesnel.lise@gmail.com>
2022-09-14 15:56:09 +0200

    clean(api): remove post schooling-registration-user-associations/student

--- api/tests/unit/application/sup-organization-learners/sup-organization-learner-controller_test.js
+++ api/tests/unit/application/sup-organization-learners/sup-organization-learner-controller_test.js
@@ -6,42 +6,6 @@ const supOrganizationLearnerController = require('../../../../lib/application/su
 const securityPreHandlers = require('../../../../lib/application/security-pre-handlers');
 
 describe('Unit | Application | Controller | sup-organization-learner', function () {
-  describe('#reconcileSupOrganizationLearner', function () {
-    const userId = 2;
-    const request = {
-      auth: { credentials: { userId } },
-      payload: { data: { attributes: {} } },
-    };
-
-    beforeEach(function () {
-      sinon.stub(usecases, 'reconcileSupOrganizationLearner');
-      usecases.reconcileSupOrganizationLearner.resolves();
-    });
-
-    it('should return information about deprecation when old route is used', async function () {
-      // when
-      hFake.request = {
-        path: '/api/schooling-registration-user-associations/student',
-      };
-      const response = await supOrganizationLearnerController.reconcileSupOrganizationLearner(request, hFake);
-
-      // then
-      expect(response.headers['Deprecation']).to.equal('true');
-      expect(response.headers['Link']).to.equal('/api/sup-organization-learners/association; rel="successor-version"');
-    });
-
-    it('should not return information about deprecation when new route is used', async function () {
-      // when
-      hFake.request = {
-        path: '/api/sup-organization-learners/association',
-      };
-      const response = await supOrganizationLearnerController.reconcileSupOrganizationLearner(request, hFake);
-
-      // then
-      expect(response.headers['Deprecation']).to.not.exist;
-    });
-  });
-
   describe('#updateStudentNumber', function () {
     const userId = 2;
     const request = {
