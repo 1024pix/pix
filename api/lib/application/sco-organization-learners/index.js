5d94862cbbc72c027f9f444361c7eccd8d4c3a13
Lise Quesnel <quesnel.lise@gmail.com>
2022-09-14 14:47:55 +0200

    clean(api): remove post schooling-registration-dependent-users/external-user-token

--- api/lib/application/sco-organization-learners/index.js
+++ api/lib/application/sco-organization-learners/index.js
@@ -260,37 +260,6 @@ exports.register = async function (server) {
     },
     {
       method: 'POST',
-      path: '/api/schooling-registration-dependent-users/external-user-token',
-      config: {
-        auth: false,
-        handler: scoOrganizationLearnerController.createUserAndReconcileToOrganizationLearnerFromExternalUser,
-        validate: {
-          options: {
-            allowUnknown: false,
-          },
-          payload: Joi.object({
-            data: {
-              attributes: {
-                'campaign-code': Joi.string().empty(Joi.string().regex(/^\s*$/)).required(),
-                'external-user-token': Joi.string().empty(Joi.string().regex(/^\s*$/)).required(),
-                birthdate: Joi.date().format('YYYY-MM-DD').raw().required(),
-                'access-token': Joi.string().allow(null).optional(),
-              },
-              type: 'external-users',
-            },
-          }),
-        },
-        notes: [
-          "Cette route crée un compte utilisateur suite à une connexion provenant d'un IDP externe (GAR). " +
-            "Les informations sont fournies dans un token. Elle réconcilie également cet utilisateur avec l'inscription " +
-            "de l'élève au sein de l'organisation qui a créé la campagne.",
-          "- L'usage de cette route est **dépréciée** en faveur de /api/sco-organization-learners/external",
-        ],
-        tags: ['api', 'organizationLearnerDependentUser'],
-      },
-    },
-    {
-      method: 'POST',
       path: '/api/sco-organization-learners/external',
       config: {
         auth: false,
