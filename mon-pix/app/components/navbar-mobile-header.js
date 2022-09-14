f54a517a7ba1a6e3709998d651f6b7384a08230b
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-09-14 16:39:13 +0200

    feat(mon-pix): change navbar mobile header to pix-sidebar
    Co-authored-by: Clarisse Damon <clarisse.damon@pix.fr>

--- mon-pix/app/components/navbar-mobile-header.js
+++ mon-pix/app/components/navbar-mobile-header.js
@@ -1,11 +1,29 @@
 /* eslint ember/no-computed-properties-in-native-classes: 0 */
 
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 import { alias } from '@ember/object/computed';
+import { tracked } from '@glimmer/tracking';
 import Component from '@glimmer/component';
 
 export default class NavbarMobileHeader extends Component {
   @service session;
+  @service currentUser;
+  @tracked isMenuOpen = false;
 
   @alias('session.isAuthenticated') isUserLogged;
+
+  get title() {
+    return this.isUserLogged ? this.currentUser.user.fullName : '';
+  }
+
+  @action
+  openMenu() {
+    this.isMenuOpen = true;
+  }
+
+  @action
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
