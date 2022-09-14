7656f6af192622fe490df9687a0bea7915d11c4f
ajoanny <ajoanny@octo.com>
2022-09-14 23:33:54 +0200

    [API] create dependencies builder for event handler without transaction
    event

---
+++ api/lib/infrastructure/events/EventHandlerDependenciesBuilder.js
@@ -0,0 +1,62 @@
+const monitoringTools = require('../../infrastructure/monitoring-tools');
+const ParticipationResultCalculationJob = require('../jobs/campaign-result/ParticipationResultCalculationJob');
+
+function build(classToInstanciate, domainTransaction) {
+  const dependencies = _buildDependencies(domainTransaction);
+
+  const instance = new classToInstanciate(dependencies);
+  return new EventErrorHandler(instance, monitoringTools);
+}
+
+function _buildDependencies(domainTransaction) {
+  return {
+    monitoringTools,
+    participationResultCalculationJob: new ParticipationResultCalculationJob(domainTransaction.knexTransaction),
+  };
+}
+
+module.exports = {
+  build,
+};
+
+class EventErrorHandler {
+  constructor(handler, logger) {
+    this.handler = handler;
+    this.logger = logger;
+  }
+
+  async handle(event) {
+    let result;
+    try {
+      this.logHandlerStarting(event);
+      result = await this.handler.handle(event);
+    } catch (error) {
+      this.logHandlerFailed(event, error);
+      throw error;
+    }
+    return result;
+  }
+
+  logHandlerStarting(event) {
+    this.logger.logInfoWithCorrelationIds({
+      message: {
+        event,
+        handlerName: this.handler.name,
+        type: 'EVENT_LOG',
+        info: 'EventBus : Event dispatched',
+      },
+    });
+  }
+
+  logHandlerFailed(event, error) {
+    this.logger.logErrorWithCorrelationIds({
+      message: {
+        event,
+        handlerName: this.handler.name,
+        error: error?.message ? error.message + ' (see dedicated log for more information)' : undefined,
+        type: 'EVENT_LOG',
+        info: 'EventBus : Event Handling Error',
+      },
+    });
+  }
+}
