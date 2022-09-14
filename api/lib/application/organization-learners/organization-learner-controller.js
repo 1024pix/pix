c7e76a340667cb968cd327467efe8e04644cedf0
Lise Quesnel <quesnel.lise@gmail.com>
2022-09-14 15:51:21 +0200

    clean(api): remove delete schooling-registration-user-associations/id

--- api/lib/application/organization-learners/organization-learner-controller.js
+++ api/lib/application/organization-learners/organization-learner-controller.js
@@ -6,13 +6,7 @@ module.exports = {
   async dissociate(request, h) {
     const organizationLearnerId = request.params.id;
     await usecases.dissociateUserFromOrganizationLearner({ organizationLearnerId });
-    const response = h.response().code(204);
-    if (h.request.path === `/api/schooling-registration-user-associations/${request.params.id}`) {
-      response
-        .header('Deprecation', 'true')
-        .header('Link', `/api/organization-learners/${request.params.id}/association; rel="successor-version"`);
-    }
-    return response;
+    return h.response().code(204);
   },
 
   async findAssociation(request, h) {
