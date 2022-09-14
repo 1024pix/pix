83cafa5042e356fc09e6d2348b695cbb5b17ebec
Lise Quesnel <quesnel.lise@gmail.com>
2022-09-14 15:25:35 +0200

    clean(api): remove post schooling-registration-dependent-users

--- api/tests/integration/application/sco-organization-learners/sco-organization-learner-controller_test.js
+++ api/tests/integration/application/sco-organization-learners/sco-organization-learner-controller_test.js
@@ -28,75 +28,7 @@ describe('Integration | Application | sco-organization-learners | sco-organizati
     sandbox.restore();
   });
 
-  describe('#createAndReconcileUserToOrganizationLearner for api/schooling-registration-dependent-users', function () {
-    const payload = { data: { attributes: {} } };
-
-    beforeEach(function () {
-      payload.data.attributes = {
-        'first-name': 'Robert',
-        'last-name': 'Smith',
-        birthdate: '2012-12-12',
-        'campaign-code': 'RESTRICTD',
-        password: 'P@ssw0rd',
-        username: 'robert.smith1212',
-        'with-username': true,
-      };
-    });
-
-    context('Success cases', function () {
-      context('When email is used', function () {
-        it('should return an HTTP response with status code 204', async function () {
-          // given
-          const createdUser = domainBuilder.buildUser();
-          payload.data.attributes.email = 'toto@example.net';
-          delete payload.data.attributes.username;
-          payload.data.attributes['with-username'] = false;
-          usecases.createAndReconcileUserToOrganizationLearner.resolves(createdUser);
-
-          // when
-          const response = await httpTestServer.request('POST', '/api/schooling-registration-dependent-users', payload);
-
-          // then
-          expect(response.statusCode).to.equal(204);
-        });
-      });
-
-      context('When username is used', function () {
-        it('should return an HTTP response with status code 204', async function () {
-          // given
-          const createdUser = domainBuilder.buildUser();
-          delete payload.data.attributes.email;
-          payload.data.attributes.username = 'robert.smith1212';
-          payload.data.attributes['with-username'] = true;
-          usecases.createAndReconcileUserToOrganizationLearner.resolves(createdUser);
-
-          // when
-          const response = await httpTestServer.request('POST', '/api/schooling-registration-dependent-users', payload);
-
-          // then
-          expect(response.statusCode).to.equal(204);
-        });
-      });
-    });
-
-    context('Error cases', function () {
-      context('when a NotFoundError is thrown', function () {
-        it('should resolve a 404 HTTP response', async function () {
-          // given
-          delete payload.data.attributes.username;
-          usecases.createAndReconcileUserToOrganizationLearner.rejects(new NotFoundError());
-
-          // when
-          const response = await httpTestServer.request('POST', '/api/schooling-registration-dependent-users', payload);
-
-          // then
-          expect(response.statusCode).to.equal(404);
-        });
-      });
-    });
-  });
-
-  describe('#createAndReconcileUserToOrganizationLearner for api/sco-organization-learners/dependent', function () {
+  describe('#createAndReconcileUserToOrganizationLearner', function () {
     const payload = { data: { attributes: {} } };
 
     beforeEach(function () {
