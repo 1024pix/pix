876cd82d53d4fcc9554ecc82e28536517919e80d
Mathieu Gilet <mathieu.gilet@zenika.com>
2022-09-15 16:54:36 +0200

    :sparkles: api: retrieve stickerUrl in certification-attestation-repository.js

--- api/lib/infrastructure/repositories/certification-attestation-repository.js
+++ api/lib/infrastructure/repositories/certification-attestation-repository.js
@@ -132,6 +132,7 @@ async function _getCertifiedBadges(certificationCourseId) {
       'complementary-certification-course-results.acquired',
       'complementary-certification-course-results.complementaryCertificationCourseId',
       'complementary-certification-badges.imageUrl',
+      'complementary-certification-badges.stickerUrl',
       'complementary-certification-badges.label',
       'complementary-certification-badges.level',
       'complementary-certification-badges.certificateMessage',
