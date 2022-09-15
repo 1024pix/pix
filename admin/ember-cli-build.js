40e1f6b4363a75b18b6bce4c4e3b55867913c377
Laura Bergoens <laura.bergoens@pix.fr>
2022-09-15 22:20:39 +0200

    admin: install dependencies and add necessary files such as fonts

--- admin/ember-cli-build.js
+++ admin/ember-cli-build.js
@@ -24,6 +24,34 @@ module.exports = function (defaults) {
     },
   });
 
+  app.import('vendor/AmpleSoft-normal.js', {
+    using: [{ transformation: 'es6', as: 'AmpleSoft-normal.js' }],
+  });
+
+  app.import('vendor/AmpleSoft-bold.js', {
+    using: [{ transformation: 'es6', as: 'AmpleSoft-bold.js' }],
+  });
+
+  app.import('vendor/Roboto-normal.js', {
+    using: [{ transformation: 'es6', as: 'Roboto-normal.js' }],
+  });
+
+  app.import('vendor/Roboto-condensed.js', {
+    using: [{ transformation: 'es6', as: 'Roboto-condensed.js' }],
+  });
+
+  app.import('vendor/Roboto-condensedBold.js', {
+    using: [{ transformation: 'es6', as: 'Roboto-condensedBold.js' }],
+  });
+
+  app.import('vendor/Roboto-condensedLight.js', {
+    using: [{ transformation: 'es6', as: 'Roboto-condensedLight.js' }],
+  });
+
+  app.import('vendor/pdf-assets.js', {
+    using: [{ transformation: 'es6', as: 'pdf-assets.js' }],
+  });
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
