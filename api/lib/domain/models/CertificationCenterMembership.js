f1f95499ba9364374793534e976d5bd269b06dda
Marianne Bost <marianne.bost@pix.fr>
2022-09-16 12:07:52 +0200

    :sparkles: api: add isReferer to certification-center-memberships model and domain builder

--- api/lib/domain/models/CertificationCenterMembership.js
+++ api/lib/domain/models/CertificationCenterMembership.js
@@ -1,10 +1,11 @@
 class CertificationCenterMembership {
-  constructor({ id, certificationCenter, user, createdAt, disabledAt } = {}) {
+  constructor({ id, certificationCenter, user, createdAt, disabledAt, isReferer } = {}) {
     this.id = id;
     this.certificationCenter = certificationCenter;
     this.user = user;
     this.createdAt = createdAt;
     this.disabledAt = disabledAt;
+    this.isReferer = isReferer;
   }
 }
 
