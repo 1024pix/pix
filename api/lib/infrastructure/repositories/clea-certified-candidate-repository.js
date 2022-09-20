47f57fcc96bf403bfd44a7f2dc3cf80b03b3f86f
Marianne Bost <marianne.bost@pix.fr>
2022-09-20 18:55:39 +0200

    :sparkles: api: create clea certified candidate repository

---
+++ api/lib/infrastructure/repositories/clea-certified-candidate-repository.js
@@ -0,0 +1,45 @@
+const { knex } = require('../../../db/knex-database-connection');
+const { CLEA } = require('../../domain/models/ComplementaryCertification');
+const CleaCertifiedCandidate = require('../../domain/read-models/CleaCertifiedCandidate');
+
+module.exports = {
+  async getBySessionId(sessionId) {
+    const results = await knex
+      .from('certification-courses')
+      .select(
+        'certification-courses.firstName',
+        'certification-courses.lastName',
+        'users.email',
+        'certification-courses.birthdate',
+        'certification-courses.birthplace',
+        'certification-courses.birthPostalCode',
+        'certification-courses.birthINSEECode',
+        'certification-courses.birthCountry',
+        'certification-courses.sex',
+        'certification-courses.createdAt'
+      )
+      .innerJoin('users', 'certification-courses.userId', 'users.id')
+      .innerJoin(
+        'complementary-certification-courses',
+        'complementary-certification-courses.certificationCourseId',
+        'certification-courses.id'
+      )
+      .innerJoin(
+        'complementary-certifications',
+        'complementary-certifications.id',
+        'complementary-certification-courses.complementaryCertificationId'
+      )
+      .innerJoin(
+        'complementary-certification-course-results',
+        'complementary-certification-course-results.complementaryCertificationCourseId',
+        'complementary-certification-courses.id'
+      )
+      .where({
+        'certification-courses.sessionId': sessionId,
+        'certification-courses.isPublished': true,
+        'complementary-certifications.key': CLEA,
+        'complementary-certification-course-results.acquired': true,
+      });
+    return results.map((candidate) => new CleaCertifiedCandidate(candidate));
+  },
+};
