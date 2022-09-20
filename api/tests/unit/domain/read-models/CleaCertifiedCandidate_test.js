60bc99bc0150a429354cf63acd3d9df39aeff43e
Marianne Bost <marianne.bost@pix.fr>
2022-09-20 18:55:04 +0200

    :sparkles: api: create clea certified candidate read model and domain builder

---
+++ api/tests/unit/domain/read-models/CleaCertifiedCandidate_test.js
@@ -0,0 +1,110 @@
+const { expect } = require('../../../test-helper');
+const CleaCertifiedCandidate = require('../../../../lib/domain/read-models/CleaCertifiedCandidate');
+
+describe('Unit | Domain | Models | CleaCertifiedCandidate', function () {
+  describe('#isBornInAForeignCountry', function () {
+    describe('when the candidate is born in a foreign country', function () {
+      it('should return true', function () {
+        // given
+        const cleaCertifiedCandidate = new CleaCertifiedCandidate({ birthINSEECode: '99100' });
+
+        // when
+        const bornInAForeignCountry = cleaCertifiedCandidate.isBornInAForeignCountry;
+
+        // then
+        expect(bornInAForeignCountry).to.be.true;
+      });
+    });
+    describe('when the candidate is born in France', function () {
+      it('should return false', function () {
+        // given
+        const cleaCertifiedCandidate = new CleaCertifiedCandidate({ birthINSEECode: '94700' });
+
+        // when
+        const bornInAForeignCountry = cleaCertifiedCandidate.isBornInAForeignCountry;
+
+        // then
+        expect(bornInAForeignCountry).to.be.false;
+      });
+    });
+  });
+
+  describe('#geographicAreaOfBirthCode', function () {
+    describe('when the candidate is born in a foreign country', function () {
+      // eslint-disable-next-line mocha/no-setup-in-describe
+      [
+        {
+          code: '100',
+          INSEECode: '99102',
+        },
+        {
+          code: '200',
+          INSEECode: '99201',
+        },
+        {
+          code: '300',
+          INSEECode: '99302',
+        },
+        {
+          code: '400',
+          INSEECode: '99402',
+        },
+        {
+          code: '500',
+          INSEECode: '99502',
+        },
+      ].forEach(({ code, INSEECode }) => {
+        it(`should return ${code}`, function () {
+          // given
+          const cleaCertifiedCandidate = new CleaCertifiedCandidate({ birthINSEECode: INSEECode });
+
+          // when
+          const geographicAreaOfBirthCode = cleaCertifiedCandidate.geographicAreaOfBirthCode;
+
+          // then
+          expect(geographicAreaOfBirthCode).to.equal(code);
+        });
+      });
+    });
+    describe('when the candidate is born in France', function () {
+      it('should return null', function () {
+        // given
+        const cleaCertifiedCandidate = new CleaCertifiedCandidate({ birthINSEECode: '94700' });
+
+        // when
+        const geographicAreaOfBirthCode = cleaCertifiedCandidate.geographicAreaOfBirthCode;
+
+        // then
+        expect(geographicAreaOfBirthCode).to.be.null;
+      });
+    });
+  });
+
+  describe('#isBornInFrenchOutermostRegion', function () {
+    describe('when the candidate is born in outermost region', function () {
+      it('should return true', function () {
+        // given
+        const cleaCertifiedCandidate = new CleaCertifiedCandidate({ birthINSEECode: '97302' });
+
+        // when
+        const bornInFrenchOutermostRegion = cleaCertifiedCandidate.isBornInFrenchOutermostRegion;
+
+        // then
+        expect(bornInFrenchOutermostRegion).to.be.true;
+      });
+    });
+
+    describe('when the candidate is not born in outermost region', function () {
+      it('should return false', function () {
+        // given
+        const cleaCertifiedCandidate = new CleaCertifiedCandidate({ birthINSEECode: '37300' });
+
+        // when
+        const bornInFrenchOutermostRegion = cleaCertifiedCandidate.isBornInFrenchOutermostRegion;
+
+        // then
+        expect(bornInFrenchOutermostRegion).to.be.false;
+      });
+    });
+  });
+});
