d3bb66302239b04de4e9dcb961d3bbf735482cfa
alexandrecoin <alexandre.pc.coin@gmail.com>
2022-09-21 15:12:46 +0200

    :recycle: refactor shareable-certificate-repository to certificate-repository

--- api/lib/infrastructure/repositories/certificate-repository.js
+++ api/lib/infrastructure/repositories/certificate-repository.js
@@ -1,11 +1,13 @@
 const _ = require('lodash');
 const { knex } = require('../../../db/knex-database-connection');
 const PrivateCertificate = require('../../domain/models/PrivateCertificate');
+const ShareableCertificate = require('../../domain/models/ShareableCertificate');
 const CertifiedBadges = require('../../../lib/domain/read-models/CertifiedBadges');
 const { NotFoundError } = require('../../../lib/domain/errors');
 const competenceTreeRepository = require('./competence-tree-repository');
 const ResultCompetenceTree = require('../../domain/models/ResultCompetenceTree');
 const CompetenceMark = require('../../domain/models/CompetenceMark');
+const AssessmentResult = require('../../domain/models/AssessmentResult');
 
 module.exports = {
   async getPrivateCertificate(id, { locale } = {}) {
@@ -45,6 +47,23 @@ module.exports = {
       privateCertificates.push(privateCertificate);
     }
     return privateCertificates;
+  },
+
+  async getShareableCertificateByVerificationCode(verificationCode, { locale } = {}) {
+    const shareableCertificateDTO = await _selectShareableCertificates()
+      .groupBy('certification-courses.id', 'sessions.id', 'assessments.id', 'assessment-results.id')
+      .where({ verificationCode })
+      .first();
+
+    if (!shareableCertificateDTO) {
+      throw new NotFoundError(`There is no certification course with verification code "${verificationCode}"`);
+    }
+
+    const competenceTree = await competenceTreeRepository.get({ locale });
+
+    const certifiedBadges = await _getCertifiedBadges(shareableCertificateDTO.id);
+
+    return _toDomainForShareableCertificate({ shareableCertificateDTO, competenceTree, certifiedBadges });
   },
 };
 
@@ -118,6 +137,50 @@ async function _getCertifiedBadges(certificationCourseId) {
   }).getAcquiredCertifiedBadgesDTO();
 }
 
+function _selectShareableCertificates() {
+  return knex
+    .select({
+      id: 'certification-courses.id',
+      firstName: 'certification-courses.firstName',
+      lastName: 'certification-courses.lastName',
+      birthdate: 'certification-courses.birthdate',
+      birthplace: 'certification-courses.birthplace',
+      isPublished: 'certification-courses.isPublished',
+      userId: 'certification-courses.userId',
+      date: 'certification-courses.createdAt',
+      deliveredAt: 'sessions.publishedAt',
+      certificationCenter: 'sessions.certificationCenter',
+      maxReachableLevelOnCertificationDate: 'certification-courses.maxReachableLevelOnCertificationDate',
+      pixScore: 'assessment-results.pixScore',
+      assessmentResultId: 'assessment-results.id',
+      competenceMarks: knex.raw(`
+        json_agg(
+          json_build_object('score', "competence-marks".score, 'level', "competence-marks".level, 'competence_code', "competence-marks"."competence_code")
+          ORDER BY "competence-marks"."competence_code" asc
+        )`),
+    })
+    .from('certification-courses')
+    .join('assessments', 'assessments.certificationCourseId', 'certification-courses.id')
+    .join('assessment-results', 'assessment-results.assessmentId', 'assessments.id')
+    .join('competence-marks', 'competence-marks.assessmentResultId', 'assessment-results.id')
+    .join('sessions', 'sessions.id', 'certification-courses.sessionId')
+    .modify(_filterMostRecentValidatedAssessmentResult)
+    .where('certification-courses.isPublished', true)
+    .where('certification-courses.isCancelled', false);
+}
+
+function _filterMostRecentValidatedAssessmentResult(qb) {
+  return qb
+    .whereNotExists(
+      knex
+        .select(1)
+        .from({ 'last-assessment-results': 'assessment-results' })
+        .whereRaw('"last-assessment-results"."assessmentId" = assessments.id')
+        .whereRaw('"assessment-results"."createdAt" < "last-assessment-results"."createdAt"')
+    )
+    .where('assessment-results.status', AssessmentResult.status.VALIDATED);
+}
+
 function _toDomainForPrivateCertificate({ certificationCourseDTO, competenceTree, certifiedBadges }) {
   if (competenceTree) {
     const competenceMarks = _.compact(certificationCourseDTO.competenceMarks).map(
@@ -140,6 +203,21 @@ function _toDomainForPrivateCertificate({ certificationCourseDTO, competenceTree
 
   return PrivateCertificate.buildFrom({
     ...certificationCourseDTO,
+    certifiedBadgeImages: certifiedBadges,
+  });
+}
+
+function _toDomainForShareableCertificate({ shareableCertificateDTO, competenceTree, certifiedBadges }) {
+  const resultCompetenceTree = ResultCompetenceTree.generateTreeFromCompetenceMarks({
+    competenceTree,
+    competenceMarks: _.compact(shareableCertificateDTO.competenceMarks),
+    certificationId: shareableCertificateDTO.id,
+    assessmentResultId: shareableCertificateDTO.assessmentResultId,
+  });
+
+  return new ShareableCertificate({
+    ...shareableCertificateDTO,
+    resultCompetenceTree,
     certifiedBadgeImages: certifiedBadges,
   });
 }
