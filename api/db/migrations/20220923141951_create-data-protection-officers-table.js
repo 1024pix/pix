0dadf55205ac0809c2dc0f46c9ebc481d2c385b3
Ismael Gorissen <ismael.gorissen@gmail.com>
2022-09-23 16:57:04 +0200

    feat(api): create new database table

---
+++ api/db/migrations/20220923141951_create-data-protection-officers-table.js
@@ -0,0 +1,24 @@
+const TABLE_NAME = 'data-protection-officers';
+
+exports.up = function (knex) {
+  return knex.schema.createTable(TABLE_NAME, (t) => {
+    t.bigIncrements().primary();
+    t.string('firstName').nullable();
+    t.string('lastName').nullable();
+    t.string('email').nullable();
+    t.bigInteger('organizationId').references('organizations.id').unique().nullable();
+    t.bigInteger('certificationCenterId').references('certification-centers.id').unique().nullable();
+    t.dateTime('createdAt').notNullable().defaultTo(knex.fn.now());
+    t.dateTime('updatedAt').notNullable().defaultTo(knex.fn.now());
+    t.check('(?? IS NOT NULL AND ?? IS NULL) OR (?? IS NULL AND ?? IS NOT NULL)', [
+      'organizationId',
+      'certificationCenterId',
+      'organizationId',
+      'certificationCenterId',
+    ]);
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable(TABLE_NAME);
+};
