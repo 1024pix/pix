4979cffda5808f2f2202953efe2f79e707e2d85c
Anne-G <ang.schall@gmail.com>
2022-09-23 15:26:22 +0200

    admin: add user role and action component

---
+++ admin/app/components/actions-on-users-role-in-organization.js
@@ -0,0 +1,71 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+
+export default class ActionsOnUsersRoleInOrganization extends Component {
+  @service notifications;
+  @service accessControl;
+
+  @tracked isEditionMode = false;
+  @tracked selectedNewRole = null;
+  @tracked displayConfirm = false;
+
+  get organizationRoles() {
+    return [
+      { value: 'ADMIN', label: 'Administrateur' },
+      { value: 'MEMBER', label: 'Membre' },
+    ];
+  }
+
+  @action
+  setRoleSelection(event) {
+    this.selectedNewRole = event.target.value;
+    this.isEditionMode = true;
+  }
+
+  @action
+  async updateRoleOfMember() {
+    if (!this.selectedNewRole) return;
+
+    try {
+      this.args.organizationMembership.role = this.selectedNewRole;
+      await this.args.organizationMembership.save();
+      this.notifications.success('Le rôle du membre a été mis à jour avec succès.');
+    } catch (e) {
+      this.notifications.error('Une erreur est survenue lors de la mise à jour du rôle du membre.');
+    } finally {
+      this.isEditionMode = false;
+    }
+  }
+
+  @action
+  cancelUpdateRoleOfMember() {
+    this.isEditionMode = false;
+    this.selectedNewRole = null;
+  }
+
+  @action
+  editRoleOfMember() {
+    this.isEditionMode = true;
+    this.selectedNewRole = null;
+  }
+
+  @action
+  toggleDisplayConfirm() {
+    this.displayConfirm = !this.displayConfirm;
+  }
+
+  @action
+  async disableOrganizationMembership() {
+    try {
+      await this.args.organizationMembership.save({ adapterOptions: { disable: true } });
+      await this.args.organizationMembership.reload();
+      this.notifications.success('Le membre a été désactivé avec succès.');
+    } catch (e) {
+      this.notifications.error('Une erreur est survenue lors de la désactivation du membre.');
+    } finally {
+      this.displayConfirm = false;
+    }
+  }
+}
---
+++ admin/tests/integration/components/actions-on-users-role-in-organization_test.js
@@ -0,0 +1,97 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import { render, clickByName, selectByLabelAndOption } from '@1024pix/ember-testing-library';
+import Service from '@ember/service';
+import sinon from 'sinon';
+
+module('Integration | Component |  actions-on-users-role-in-organization', function (hooks) {
+  setupRenderingTest(hooks);
+
+  module('when user has access to organization actions scope', function () {
+    test('it should be possible to modify user role', async function (assert) {
+      // given
+      class AccessControlStub extends Service {
+        hasAccessToOrganizationActionsScope = true;
+      }
+      this.owner.register('service:access-control', AccessControlStub);
+
+      const store = this.owner.lookup('service:store');
+      const organizationMembership = store.createRecord('organizationMembership', {
+        role: 'ADMIN',
+        save: sinon.stub(),
+      });
+      this.set('organizationMembership', organizationMembership);
+
+      const notificationSuccessStub = sinon.stub();
+      class NotificationsStub extends Service {
+        success = notificationSuccessStub;
+      }
+      this.owner.register('service:notifications', NotificationsStub);
+
+      // when
+      const screen = await render(
+        hbs`<ActionsOnUsersRoleInOrganization @organizationMembership={{this.organizationMembership}} />`
+      );
+      await clickByName('Modifier le rôle');
+      await selectByLabelAndOption('Sélectionner un rôle', 'MEMBER');
+      await clickByName('Enregistrer');
+
+      // then
+      assert.ok(organizationMembership.save.called);
+      sinon.assert.calledWith(notificationSuccessStub, 'Le rôle du membre a été mis à jour avec succès.');
+      assert.dom(screen.queryByRole('button', { name: 'Modifier le rôle' })).exists();
+      assert.dom(screen.getByText('Membre')).exists();
+    });
+
+    test('it should be possible to disable user in a specific organization', async function (assert) {
+      // given
+      class AccessControlStub extends Service {
+        hasAccessToOrganizationActionsScope = true;
+      }
+      this.owner.register('service:access-control', AccessControlStub);
+
+      const store = this.owner.lookup('service:store');
+      const organizationMembership = store.createRecord('organizationMembership', {
+        role: 'ADMIN',
+        save: sinon.stub(),
+        reload: sinon.stub(),
+      });
+      this.set('organizationMembership', organizationMembership);
+
+      const notificationSuccessStub = sinon.stub();
+      class NotificationsStub extends Service {
+        success = notificationSuccessStub;
+      }
+      this.owner.register('service:notifications', NotificationsStub);
+
+      // when
+      await render(hbs`<ActionsOnUsersRoleInOrganization @organizationMembership={{this.organizationMembership}} />`);
+      await clickByName("Désactiver l'agent");
+      await clickByName('Confirmer');
+
+      // then
+      sinon.assert.calledWith(notificationSuccessStub, 'Le membre a été désactivé avec succès.');
+      assert.ok(organizationMembership.save.calledWith({ adapterOptions: { disable: true } }));
+    });
+  });
+
+  module('when user has not access to organization actions scope', function () {
+    test('it should not show actions buttons', async function (assert) {
+      // given
+      class AccessControlStub extends Service {
+        hasAccessToOrganizationActionsScope = false;
+      }
+      this.owner.register('service:access-control', AccessControlStub);
+
+      // when
+      const screen = await render(
+        hbs`<ActionsOnUsersRoleInOrganization @organizationMembership={{this.organizationMembership}} />`
+      );
+
+      // expect
+      assert.dom(screen.queryByRole('button', { name: 'Modifier le rôle' })).doesNotExist();
+      assert.dom(screen.queryByRole('button', { name: 'Désactiver' })).doesNotExist();
+    });
+  });
+});
