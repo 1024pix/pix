09443b0d3a8c7052e8c570518685687ce3a71672
ARJ <ajoanny@octo.com>
2022-09-26 18:09:34 +0200

    [API] create job scheduler for pole emploi

---
+++ api/tests/integration/infrastructure/jobs/campaign-result/SendSharedParticipationResultsToPoleEmploiJob_test.js
@@ -0,0 +1,21 @@
+const { expect, knex } = require('../../../../test-helper');
+const SendSharedParticipationResultsToPoleEmploiJob = require('../../../../../lib/infrastructure/jobs/campaign-result/SendSharedParticipationResultsToPoleEmploiJob');
+
+describe('Integration | Infrastructure | Jobs | CampaignResult | ParticipationResultCalculation', function () {
+  afterEach(async function () {
+    await knex('pgboss.job').where({ name: 'SendSharedParticipationResultsToPoleEmploi' }).delete();
+  });
+
+  describe('#schedule', function () {
+    it('creates the send results job', async function () {
+      const job = new SendSharedParticipationResultsToPoleEmploiJob(knex);
+
+      await job.schedule({ params: 12 });
+
+      const jobBDD = await knex('pgboss.job').where({ name: 'SendSharedParticipationResultsToPoleEmploi' }).first();
+
+      expect(jobBDD.retrylimit).to.equal(0);
+      expect(jobBDD.data).to.deep.equal({ params: 12 });
+    });
+  });
+});
