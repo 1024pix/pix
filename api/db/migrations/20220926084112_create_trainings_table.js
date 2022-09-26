8c32573e3479f16e8e28f1eb2bff6b1bf9b10025
Clarisse Damon <clarisse.damon@pix.fr>
2022-09-26 10:58:31 +0200

    feat(api): create new trainings table
    Co-authored-by: Corentin Rousseau <corentin.rousseau@pix.fr>

---
+++ api/db/migrations/20220926084112_create_trainings_table.js
@@ -0,0 +1,18 @@
+const TABLE_NAME = 'trainings';
+
+exports.up = (knex) => {
+  return knex.schema.createTable(TABLE_NAME, (t) => {
+    t.increments().primary();
+    t.string('title').notNullable();
+    t.string('link').notNullable();
+    t.string('type').notNullable();
+    t.specificType('duration', 'interval').notNullable();
+    t.string('locale').notNullable();
+    t.dateTime('createdAt').notNullable().defaultTo(knex.fn.now());
+    t.dateTime('updatedAt').notNullable().defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = (knex) => {
+  return knex.schema.dropTable(TABLE_NAME);
+};
