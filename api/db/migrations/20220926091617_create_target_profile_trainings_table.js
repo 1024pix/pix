f9ece8a9db682742e6baf2acce213d0181472703
Rousseau Corentin <cro@eonix.be>
2022-09-26 11:33:38 +0200

    feat(api): create target-profile-trainings table

---
+++ api/db/migrations/20220926091617_create_target_profile_trainings_table.js
@@ -0,0 +1,15 @@
+const TABLE_NAME = 'target-profile-trainings';
+
+exports.up = function (knex) {
+  return knex.schema.createTable(TABLE_NAME, (t) => {
+    t.increments().primary();
+    t.integer('targetProfileId').references('target-profiles.id').notNullable();
+    t.integer('trainingId').references('trainings.id').notNullable();
+    t.dateTime('createdAt').notNullable().defaultTo(knex.fn.now());
+    t.dateTime('updatedAt').notNullable().defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable(TABLE_NAME);
+};
