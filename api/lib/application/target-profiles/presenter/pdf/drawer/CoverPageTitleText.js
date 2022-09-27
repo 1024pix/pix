b495091c401586d1501a4f07365343c2eada9a32
lucas.herrador <lucas.herrador@octo.com>
2022-09-27 17:37:54 +0200

    improve draw align center for text
    Co-authored-by: Laura Bergoens <laura.bergoens@pix.fr>

--- api/lib/application/target-profiles/presenter/pdf/drawer/CoverPageTitleText.js
+++ api/lib/application/target-profiles/presenter/pdf/drawer/CoverPageTitleText.js
@@ -21,37 +21,4 @@ module.exports = class CoverPageTitleText extends Text {
       maxWidth: PositionManager.coverPageTitleWidth,
     });
   }
-
-  draw(page) {
-    const textWidth = this.font.widthOfTextAtSize(this.text, this.fontSize);
-    if (textWidth <= this.maxWidth) return super.draw(page);
-    const textParts = _splitText(this.text, this.font, this.fontSize, this.maxWidth);
-    let positionY = this.position.y;
-    for (const textPart of textParts) {
-      this.text = textPart;
-      this.position.x = Text._positionXForHorizontalCentering(this.text, page, this.font, this.fontSize);
-      this.position.y = positionY;
-      positionY = super.draw(page);
-    }
-    return positionY;
-  }
 };
-
-function _splitText(text, font, fontSize, maxWidth) {
-  const words = text.split(' ');
-  const textParts = [];
-  let currentTextPart = [];
-  while (words.length > 0) {
-    const currentWord = words.shift();
-    const reconstitutedText = currentTextPart.join(' ') + ' ' + currentWord;
-    const reconstitutedTextWidth = font.widthOfTextAtSize(reconstitutedText, fontSize);
-    if (reconstitutedTextWidth > maxWidth) {
-      textParts.push(currentTextPart.join(' '));
-      currentTextPart = [currentWord];
-    } else {
-      currentTextPart.push(currentWord);
-    }
-  }
-  textParts.push(currentTextPart.join(' '));
-  return textParts;
-}
