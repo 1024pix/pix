06f98f29a584f140f69413c97eaf19709da4192e
Marianne Bost <marianne.bost@pix.fr>
2022-09-28 15:08:27 +0200

    :sparkles: api : add controller to get clea candidate data csv

--- api/lib/infrastructure/utils/csv/certification-results.js
+++ api/lib/infrastructure/utils/csv/certification-results.js
@@ -19,6 +19,13 @@ async function getSessionCertificationResultsCsv({ session, certificationResults
   return getCsvContent({ data, fileHeaders });
 }
 
+async function getCleaCertifiedCandidateCsv(cleaCertifiedCandidates) {
+  const fileHeaders = _buildFileHeadersForCleaCandidates(cleaCertifiedCandidates);
+  const data = _buildFileDataForCleaCandidates(cleaCertifiedCandidates);
+
+  return getCsvContent({ data, fileHeaders });
+}
+
 function _buildFileDataWithoutCertificationCenterName({ certificationResults }) {
   return certificationResults.map(_getRowItemsFromResults);
 }
@@ -65,9 +72,73 @@ function _buildFileHeadersWithoutCertificationCenterName() {
   );
 }
 
+function _buildFileHeadersForCleaCandidates() {
+  return [
+    _headers.SIREN,
+    _headers.SIRET,
+    _headers.REGISTRATION_STATUS,
+    _headers.LEVEL,
+    _headers.ORIGIN,
+    _headers.FUNDER,
+    _headers.GENDER,
+    _headers.BIRTHNAME,
+    _headers.USUAL_NAME,
+    _headers.FIRSTNAME,
+    _headers.EMAIL,
+    _headers.PHONE,
+    _headers.ADDRESS,
+    _headers.ADDITIONAL_ADDRESS,
+    _headers.CITY,
+    _headers.POSTAL_CODE,
+    _headers.BIRTHDATE,
+    _headers.FOREIGN_BORN,
+    _headers.GEOGRAPHIC_AREA,
+    _headers.OUTERMOST_BORN,
+    _headers.BIRTH_CITY,
+    _headers.BIRTH_POSTAL_CODE,
+    _headers.PASSING_DATE,
+    _headers.CCPI,
+    _headers.STATUS,
+    _headers.FIRST_SHOT,
+  ];
+}
+
 function _buildFileData({ session, certificationResults }) {
   const sessionComplementaryCertificationsLabels = _getComplementaryCertificationResultsLabels(certificationResults);
   return certificationResults.map(_getRowItemsFromSessionAndResults(session, sessionComplementaryCertificationsLabels));
+}
+
+function _buildFileDataForCleaCandidates(cleaCertifiedCandidates) {
+  return cleaCertifiedCandidates.map((candidate) => {
+    return {
+      [_headers.SIREN]: null,
+      [_headers.SIRET]: null,
+      [_headers.REGISTRATION_STATUS]: null,
+      [_headers.LEVEL]: null,
+      [_headers.ORIGIN]: null,
+      [_headers.FUNDER]: null,
+      [_headers.GENDER]: _getGenderCandidate(candidate.sex),
+      [_headers.BIRTHNAME]: candidate.lastName,
+      [_headers.USUAL_NAME]: null,
+      [_headers.FIRSTNAME]: candidate.firstName,
+      [_headers.EMAIL]: candidate.email,
+      [_headers.PHONE]: null,
+      [_headers.ADDRESS]: null,
+      [_headers.ADDITIONAL_ADDRESS]: null,
+      [_headers.CITY]: null,
+      [_headers.POSTAL_CODE]: null,
+      [_headers.BIRTHDATE]: _formatDate(candidate.birthdate),
+      [_headers.FOREIGN_BORN]: _getValueForBoolean(candidate.isBornInAForeignCountry),
+      [_headers.GEOGRAPHIC_AREA]: candidate.geographicAreaOfBirthCode,
+      [_headers.OUTERMOST_BORN]: _getValueForBoolean(candidate.isBornInFrenchOutermostRegion),
+      [_headers.BIRTH_CITY]: candidate.birthplace,
+      [_headers.BIRTH_POSTAL_CODE]: candidate.birthPostalCode,
+      [_headers.PASSING_DATE]: _formatDate(candidate.createdAt),
+      [_headers.CCPI]: 'CléA Numérique by Pix',
+      [_headers.STATUS]: 'CERTIFIE',
+      [_headers.FIRST_SHOT]: null,
+    };
+  });
 }
 
 function _getComplementaryCertificationResultsHeaders(certificationResults) {
@@ -207,6 +278,14 @@ function _getCommentForOrganization(certificationResult) {
   return certificationResult.commentForOrganization;
 }
 
+function _getGenderCandidate(sex) {
+  return sex === 'F' ? 'MME' : 'M';
+}
+
+function _getValueForBoolean(value) {
+  return value ? 'OUI' : 'NON';
+}
+
 const _competenceIndexes = [
   '1.1',
   '1.2',
@@ -239,10 +318,34 @@ const _headers = {
   CERTIFICATION_CENTER: 'Centre de certification',
   CERTIFICATION_DATE: 'Date de passage de la certification',
   JURY_COMMENT_FOR_ORGANIZATION: 'Commentaire jury pour l’organisation',
+  SIREN: "SIREN de l'organisme",
+  SIRET: "Siret de l'établissement",
+  REGISTRATION_STATUS: "Statut à l'inscription",
+  LEVEL: "Niveau d'instruction",
+  ORIGIN: 'Origine de la démarche',
+  FUNDER: 'Financeur',
+  GENDER: 'Civilité',
+  BIRTHNAME: 'Nom de naissance',
+  USUAL_NAME: "Nom d'usage",
+  EMAIL: 'Email',
+  PHONE: 'Téléphone',
+  ADDRESS: 'Adresse',
+  ADDITIONAL_ADDRESS: "Complément d'adresse",
+  CITY: 'Ville',
+  POSTAL_CODE: 'Code postal',
+  FOREIGN_BORN: "Né à l'étranger",
+  GEOGRAPHIC_AREA: 'Zone géographique de naissance',
+  OUTERMOST_BORN: "Né en collectivité d'outre-mer",
+  BIRTH_CITY: 'Ville de naissance',
+  BIRTH_POSTAL_CODE: 'Code postal de naissance',
+  PASSING_DATE: 'Date de passage',
+  CCPI: 'CCPI',
+  FIRST_SHOT: 'Obtention après la première évaluation ?',
 };
 
 module.exports = {
   getSessionCertificationResultsCsv,
   getDivisionCertificationResultsCsv,
+  getCleaCertifiedCandidateCsv,
   REJECTED_AUTOMATICALLY_COMMENT,
 };
--- api/tests/integration/infrastructure/utils/csv/certification-results_test.js
+++ api/tests/integration/infrastructure/utils/csv/certification-results_test.js
@@ -3,6 +3,7 @@ const {
   getSessionCertificationResultsCsv,
   getDivisionCertificationResultsCsv,
   REJECTED_AUTOMATICALLY_COMMENT,
+  getCleaCertifiedCandidateCsv,
 } = require('../../../../../lib/infrastructure/utils/csv/certification-results');
 const {
   PIX_EMPLOI_CLEA_V3,
@@ -464,4 +465,119 @@ describe('Integration | Infrastructure | Utils | csv | certification-results', f
       });
     });
   });
+  context('#getCleaCertifiedCandidateCsv', function () {
+    context('when at least one candidate has passed a clea certification', function () {
+      it("returns a csv with candidate's information", async function () {
+        // given
+        const BOM = '\uFEFF';
+        const CleaCertifiedCandidate1 = domainBuilder.buildCleaCertifiedCandidate({
+          firstName: 'Léane',
+          lastName: 'Bern',
+          email: 'princesse-lele@gg.fr',
+          birthdate: '2001-05-10',
+          birthplace: 'Paris',
+          birthPostalCode: '75019',
+          birthINSEECode: '75119',
+          birthCountry: 'FRANCE',
+          sex: 'F',
+          createdAt: new Date('2020-02-01'),
+        });
+        const CleaCertifiedCandidate2 = domainBuilder.buildCleaCertifiedCandidate({
+          firstName: 'Jean-Mi',
+          lastName: 'Mi',
+          email: 'jean-mi@coco.fr',
+          birthdate: '2001-02-07',
+          birthplace: 'Paris',
+          birthPostalCode: '75015',
+          birthINSEECode: '75115',
+          birthCountry: 'FRANCE',
+          sex: 'M',
+          createdAt: new Date('2020-02-01'),
+        });
+
+        // when
+        const result = await getCleaCertifiedCandidateCsv([CleaCertifiedCandidate1, CleaCertifiedCandidate2]);
+
+        // then
+        const headers =
+          '"SIREN de l\'organisme";"Siret de l\'établissement";"Statut à l\'inscription";"Niveau d\'instruction";"Origine de la démarche";"Financeur";"Civilité";"Nom de naissance";"Nom d\'usage";"Prénom";"Email";"Téléphone";"Adresse";"Complément d\'adresse";"Ville";"Code postal";"Date de naissance";"Né à l\'étranger";"Zone géographique de naissance";"Né en collectivité d\'outre-mer";"Ville de naissance";"Code postal de naissance";"Date de passage";"CCPI";"Statut";"Obtention après la première évaluation ?"\n';
+        const CleaCertifiedCandidate1Data =
+          ';;;;;;"MME";"Bern";;"Léane";"princesse-lele@gg.fr";;;;;;"10/05/2001";"NON";;"NON";"Paris";"75019";"01/02/2020";"CléA Numérique by Pix";"CERTIFIE";\n';
+        const CleaCertifiedCandidate2Data =
+          ';;;;;;"M";"Mi";;"Jean-Mi";"jean-mi@coco.fr";;;;;;"07/02/2001";"NON";;"NON";"Paris";"75015";"01/02/2020";"CléA Numérique by Pix";"CERTIFIE";';
+
+        const expectedResult = BOM + headers + CleaCertifiedCandidate1Data + CleaCertifiedCandidate2Data;
+
+        expect(result).to.equal(expectedResult);
+      });
+      context('when clea certified candidates are born in french outermost region', function () {
+        it('should return csvContent with correct geographic area code and correct outermost born value', async function () {
+          // given
+          const CleaCertifiedCandidate = domainBuilder.buildCleaCertifiedCandidate({
+            firstName: 'Léane',
+            lastName: 'Bern',
+            email: 'princesse-lele@gg.fr',
+            birthdate: '2001-05-10',
+            birthplace: 'STE MARIE',
+            birthPostalCode: '97418',
+            birthINSEECode: '97418',
+            birthCountry: 'FRANCE',
+            sex: 'F',
+            createdAt: new Date('2020-02-01'),
+          });
+
+          // when
+          const result = await getCleaCertifiedCandidateCsv([CleaCertifiedCandidate]);
+
+          // then
+          const expectedResult = [
+            { header: '"Né à l\'étranger"', value: '"NON"' },
+            { header: '"Zone géographique de naissance"', value: '' },
+            { header: '"Né en collectivité d\'outre-mer"', value: '"OUI"' },
+          ];
+          expectedResult.map(({ header, value }) => {
+            expect(_getValueFromHeader(header, result)).to.be.equal(value);
+          });
+        });
+      });
+      context('when clea certified candidates are born in foreign country', function () {
+        it('should return csvContent with correct foreign born value', async function () {
+          // given
+          const CleaCertifiedCandidate = domainBuilder.buildCleaCertifiedCandidate({
+            firstName: 'Léane',
+            lastName: 'Bern',
+            email: 'princesse-lele@gg.fr',
+            birthdate: '2001-05-10',
+            birthplace: 'STE MARIE',
+            birthPostalCode: '99416',
+            birthINSEECode: '99416',
+            birthCountry: 'BRESIL',
+            sex: 'F',
+            createdAt: new Date('2020-02-01'),
+          });
+
+          // when
+          const result = await getCleaCertifiedCandidateCsv([CleaCertifiedCandidate]);
+
+          // then
+          const expectedResult = [
+            { header: '"Né à l\'étranger"', value: '"OUI"' },
+            { header: '"Zone géographique de naissance"', value: '"400"' },
+            { header: '"Né en collectivité d\'outre-mer"', value: '"NON"' },
+          ];
+          expectedResult.map(({ header, value }) => {
+            expect(_getValueFromHeader(header, result)).to.be.equal(value);
+          });
+        });
+      });
+    });
+  });
+
+  function _getValueFromHeader(nameHeader, result) {
+    const csv = result.split('\n');
+    const csvHeader = csv[0].split(';');
+    const csvData = csv[1].split(';');
+    const indexHeader = csvHeader.findIndex((header) => header === nameHeader);
+    return csvData[indexHeader];
+  }
 });
