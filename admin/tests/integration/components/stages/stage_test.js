a0faed939a5f22e4edf66908e0c2be3daf3c2ab3
jeremie <jeremie.jade@pix.fr>
2022-09-30 14:40:56 +0200

    Display stage with type level on detail stage

--- admin/tests/integration/components/stages/stage_test.js
+++ admin/tests/integration/components/stages/stage_test.js
@@ -12,61 +12,105 @@ module('Integration | Component | Stages::Stage', function (hooks) {
 
   setupRenderingTest(hooks);
 
-  hooks.beforeEach(function () {
-    isEditMode = false;
-    toggleEditMode = sinon.stub();
-    stage = {
-      id: 34,
-      threshold: 60,
-      title: 'palier 3',
-      message: 'mon message',
-      prescriberTitle: 'titre du prescriteur',
-      prescriberDescription: 'description de prescripteur',
-    };
+  module('when stage type is threshold', function (hooks) {
+    hooks.beforeEach(function () {
+      isEditMode = false;
+      toggleEditMode = sinon.stub();
+      stage = {
+        id: 34,
+        threshold: 60,
+        isTypeLevel: false,
+        title: 'palier 3',
+        message: 'mon message',
+        prescriberTitle: 'titre du prescriteur',
+        prescriberDescription: 'description de prescripteur',
+      };
 
-    this.set('isEditMode', isEditMode);
-    this.set('stage', stage);
-    this.set('toggleEditMode', toggleEditMode);
+      this.set('isEditMode', isEditMode);
+      this.set('stage', stage);
+      this.set('toggleEditMode', toggleEditMode);
+    });
+
+    test('should render all details about the stage when the isEditMode is false', async function (assert) {
+      //when
+      const screen = await render(
+        hbs`<Stages::Stage @model={{this.stage}} @toggleEditMode = {{this.toggleEditMode}} @isEditMode={{this.isEditMode}}/>`
+      );
+
+      //then
+      assert.dom(screen.getByRole('button', { name: 'Éditer' })).exists();
+      assert.dom(screen.getByText('ID : 34', { exact: false })).exists();
+      assert.dom(screen.getByText('Seuil : 60', { exact: false })).exists();
+      assert.dom(screen.getByText('Titre : palier 3', { exact: false })).exists();
+      assert.dom(screen.getByText('Message : mon message', { exact: false })).exists();
+    });
+
+    test('should call toggleEditMode function when the edit button is clicked', async function (assert) {
+      //when
+      await render(
+        hbs`<Stages::Stage @model={{this.stage}} @toggleEditMode = {{this.toggleEditMode}} @isEditMode={{this.isEditMode}}/>`
+      );
+
+      await click('button');
+
+      //then
+      assert.ok(toggleEditMode.called);
+    });
+
+    test('should render updateStage component when the isEditMode is true', async function (assert) {
+      //given
+      this.set('isEditMode', true);
+
+      //when
+      const screen = await render(
+        hbs`<Stages::Stage @model={{this.stage}} @toggleEditMode = {{this.toggleEditMode}} @isEditMode={{this.isEditMode}}/>`
+      );
+
+      //then
+      assert.dom(screen.getByRole('button', { name: 'Annuler' })).exists();
+      assert.dom(screen.getByRole('button', { name: 'Enregistrer' })).exists();
+      assert.dom(screen.getByRole('textbox', { name: 'Titre pour le prescripteur' })).exists();
+    });
   });
+  module('when stage type is level', function (hooks) {
+    hooks.beforeEach(function () {
+      isEditMode = false;
+      toggleEditMode = sinon.stub();
+      stage = {
+        id: 34,
+        isTypeLevel: true,
+        level: 3,
+        title: 'palier 3',
+        message: 'mon message',
+        prescriberTitle: 'titre du prescriteur',
+        prescriberDescription: 'description de prescripteur',
+      };
 
-  test('should render all details about the stage when the isEditMode is false', async function (assert) {
-    //when
-    const screen = await render(
-      hbs`<Stages::Stage @model={{this.stage}} @toggleEditMode = {{this.toggleEditMode}} @isEditMode={{this.isEditMode}}/>`
-    );
+      this.set('isEditMode', isEditMode);
+      this.set('stage', stage);
+      this.set('toggleEditMode', toggleEditMode);
+    });
 
-    //then
-    assert.dom(screen.getByRole('button', { name: 'Editer' })).exists();
-    assert.dom(screen.getByText('ID : 34', { exact: false })).exists();
-    assert.dom(screen.getByText('Seuil : 60', { exact: false })).exists();
-    assert.dom(screen.getByText('Titre : palier 3', { exact: false })).exists();
-    assert.dom(screen.getByText('Message : mon message', { exact: false })).exists();
-  });
+    test('it should render all details about the stage', async function (assert) {
+      //when
+      const screen = await render(
+        hbs`<Stages::Stage @model={{this.stage}} @toggleEditMode = {{this.toggleEditMode}} @isEditMode={{this.isEditMode}}/>`
+      );
 
-  test('should call toggleEditMode function when the edit button is clicked', async function (assert) {
-    //when
-    await render(
-      hbs`<Stages::Stage @model={{this.stage}} @toggleEditMode = {{this.toggleEditMode}} @isEditMode={{this.isEditMode}}/>`
-    );
+      //then
+      assert.dom(screen.getByText('ID : 34', { exact: false })).exists();
+      assert.dom(screen.getByText('Niveau : 3', { exact: false })).exists();
+      assert.dom(screen.getByText('Titre : palier 3', { exact: false })).exists();
+      assert.dom(screen.getByText('Message : mon message', { exact: false })).exists();
+    });
 
-    await click('button');
-
-    //then
-    assert.ok(toggleEditMode.called);
-  });
-
-  test('should render updateStage component when the isEditMode is true', async function (assert) {
-    //given
-    this.set('isEditMode', true);
-
-    //when
-    const screen = await render(
-      hbs`<Stages::Stage @model={{this.stage}} @toggleEditMode = {{this.toggleEditMode}} @isEditMode={{this.isEditMode}}/>`
-    );
-
-    //then
-    assert.dom(screen.getByRole('button', { name: 'Annuler' })).exists();
-    assert.dom(screen.getByRole('button', { name: 'Enregistrer' })).exists();
-    assert.dom(screen.getByRole('textbox', { name: 'Titre pour le prescripteur' })).exists();
+    test('it should not render the edit button action', async function (assert) {
+      // when
+      const screen = await render(
+        hbs`<Stages::Stage @model={{this.stage}} @toggleEditMode = {{this.toggleEditMode}} @isEditMode={{this.isEditMode}}/>`
+      );
+      // then
+      assert.dom(screen.queryByText('Éditer')).doesNotExist();
+    });
   });
 });
