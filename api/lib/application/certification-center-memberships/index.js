9cea480817c998d22248ead7448c370bd3d0242c
Ismael Gorissen <ismael.gorissen@gmail.com>
2022-09-30 15:53:57 +0200

    feat(api): move routes under admin path

--- api/lib/application/certification-center-memberships/index.js
+++ api/lib/application/certification-center-memberships/index.js
@@ -2,7 +2,35 @@ const certificationCenterMembershipController = require('./certification-center-
 const securityPreHandlers = require('../security-pre-handlers');
 
 exports.register = async function (server) {
+  const adminRoutes = [
+    {
+      method: 'DELETE',
+      path: '/api/admin/certification-center-memberships/{id}',
+      config: {
+        handler: certificationCenterMembershipController.disable,
+        pre: [
+          {
+            method: (request, h) =>
+              securityPreHandlers.adminMemberHasAtLeastOneAccessOf([
+                securityPreHandlers.checkAdminMemberHasRoleSuperAdmin,
+                securityPreHandlers.checkAdminMemberHasRoleCertif,
+                securityPreHandlers.checkAdminMemberHasRoleSupport,
+                securityPreHandlers.checkAdminMemberHasRoleMetier,
+              ])(request, h),
+            assign: 'hasAuthorizationToAccessAdminScope',
+          },
+        ],
+        notes: [
+          "- **Cette route est restreinte aux utilisateurs ayant les droits d'accès**\n" +
+            '- Désactivation d‘un lien entre un utilisateur et un centre de certification\n',
+        ],
+        tags: ['api', 'certification-center-membership'],
+      },
+    },
+  ];
+
   server.route([
+    ...adminRoutes,
     {
       method: 'POST',
       path: '/api/certification-center-memberships',
@@ -23,30 +51,6 @@ exports.register = async function (server) {
         notes: [
           "- **Cette route est restreinte aux utilisateurs ayant les droits d'accès**\n" +
             '- Création d‘un lien entre un utilisateur et un centre de certification\n',
-        ],
-        tags: ['api', 'certification-center-membership'],
-      },
-    },
-    {
-      method: 'DELETE',
-      path: '/api/certification-center-memberships/{id}',
-      config: {
-        handler: certificationCenterMembershipController.disable,
-        pre: [
-          {
-            method: (request, h) =>
-              securityPreHandlers.adminMemberHasAtLeastOneAccessOf([
-                securityPreHandlers.checkAdminMemberHasRoleSuperAdmin,
-                securityPreHandlers.checkAdminMemberHasRoleCertif,
-                securityPreHandlers.checkAdminMemberHasRoleSupport,
-                securityPreHandlers.checkAdminMemberHasRoleMetier,
-              ])(request, h),
-            assign: 'hasAuthorizationToAccessAdminScope',
-          },
-        ],
-        notes: [
-          "- **Cette route est restreinte aux utilisateurs ayant les droits d'accès**\n" +
-            '- Désactivation d‘un lien entre un utilisateur et un centre de certification\n',
         ],
         tags: ['api', 'certification-center-membership'],
       },
