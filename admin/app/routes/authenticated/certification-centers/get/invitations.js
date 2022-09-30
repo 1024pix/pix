fd7bc0592506dfad0b2cde53b3420a40bf8f1954
Anne-G <ang.schall@gmail.com>
2022-09-30 15:35:13 +0200

    admin: fetch invitations in route

--- admin/app/routes/authenticated/certification-centers/get/invitations.js
+++ admin/app/routes/authenticated/certification-centers/get/invitations.js
@@ -1,4 +1,13 @@
 import Route from '@ember/routing/route';
 
 export default class AuthenticatedCertificationCentersGetInvitationsRoute extends Route {
+  async model() {
+    const { certificationCenter } = this.modelFor('authenticated.certification-centers.get');
+    const certificationCenterId = certificationCenter.id;
+    return await this.store.query('certification-center-invitation', {
+      filter: {
+        certificationCenterId,
+      },
+    });
+  }
 }
--- admin/tests/unit/routes/authenticated/certification-centers/get/invitations_test.js
+++ admin/tests/unit/routes/authenticated/certification-centers/get/invitations_test.js
@@ -1,11 +1,30 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import sinon from 'sinon';
 
 module('Unit | Route | authenticated/certification-centers/get/invitations', function (hooks) {
   setupTest(hooks);
 
-  test('it exists', function (assert) {
-    const route = this.owner.lookup('route:AuthenticatedCertificationCentersGetInvitations');
-    assert.ok(route);
+  test('it should return certification center invitations', async function (assert) {
+    // given
+    const route = this.owner.lookup('route:authenticated/certification-centers/get/invitations');
+    const store = this.owner.lookup('service:store');
+
+    const certificationCenterInvitations = Symbol('some certification center invitations');
+    store.query = sinon.stub();
+    store.query
+      .withArgs('certification-center-invitation', {
+        filter: { certificationCenterId: 777 },
+      })
+      .resolves(certificationCenterInvitations);
+
+    route.modelFor = sinon.stub();
+    route.modelFor.withArgs('authenticated.certification-centers.get').returns({ certificationCenter: { id: 777 } });
+
+    // when
+    const expectedCertificationCenterInvitations = await route.model();
+
+    // then
+    assert.strictEqual(expectedCertificationCenterInvitations, certificationCenterInvitations);
   });
 });
