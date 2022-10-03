8949f0da3cf1e01ac3ec72a998261df535ef8cac
Mathieu Gilet <mathieu.gilet@zenika.com>
2022-10-03 14:47:06 +0200

    :sparkles: api: map InvalidJuryLevelError to InfrastructureError

--- api/tests/integration/application/complementary-certification-course-results/complementary-certification-course-results-controller_test.js
+++ api/tests/integration/application/complementary-certification-course-results/complementary-certification-course-results-controller_test.js
@@ -1,6 +1,7 @@
 const { expect, sinon, HttpTestServer } = require('../../../test-helper');
 const securityPreHandlers = require('../../../../lib/application/security-pre-handlers');
 const usecases = require('../../../../lib/domain/usecases');
+const { InvalidJuryLevelError } = require('../../../../lib/domain/errors');
 const moduleUnderTest = require('../../../../lib/application/complementary-certification-course-results');
 
 describe('Integration | Application | complementary-certification-course-results | complementary-certification-course-results-controller', function () {
@@ -65,7 +66,7 @@ describe('Integration | Application | complementary-certification-course-results
 
     context('Error cases', function () {
       context('when juryLevel is not valid', function () {
-        it('should resolve a 400 HTTP response', async function () {
+        it('should resolve a 400 HTTP response by joi validation', async function () {
           // given
           const payload = {
             data: {
@@ -87,6 +88,31 @@ describe('Integration | Application | complementary-certification-course-results
 
           // then
           expect(response.statusCode).to.equal(400);
+        });
+
+        it('should resolve a 400 HTTP response', async function () {
+          // given
+          const payload = {
+            data: {
+              attributes: {
+                juryLevel: 'PIX_EDU_FORMATION_INITIALE_2ND_DEGRE_INITIE',
+                complementaryCertificationCourseId: 123456,
+              },
+            },
+          };
+          usecases.saveJuryComplementaryCertificationCourseResult.rejects(new InvalidJuryLevelError());
+          securityPreHandlers.checkAdminMemberHasRoleSuperAdmin.callsFake((request, h) => h.response(true));
+
+          // when
+          const response = await httpTestServer.request(
+            'POST',
+            '/api/admin/complementary-certification-course-results',
+            payload
+          );
+
+          // then
+          expect(response.statusCode).to.equal(400);
+          expect(response.result.errors[0].detail).to.equal('Le niveau jury renseigné est invalide.');
         });
       });
 
