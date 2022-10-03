a23547f8395c398e2ed5c3e7e684be5b51049f57
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2022-10-03 15:46:54 +0200

    feat(api): Add /api/admin/users/{id}/certification-center-memberships route
    Co-authored-by: Marc-Aurèle Darche <marc-aurele.darche@pix.fr>
    Co-authored-by: Anne-G <ang.schall@gmail.com>
    Co-authored-by: Andreia Pena <andreia.pena@pix.fr>

---
+++ api/tests/acceptance/application/users/find-user-certification-centers-for-admin-route-get_test.js
@@ -0,0 +1,42 @@
+const { databaseBuilder, expect, generateValidRequestAuthorizationHeader } = require('../../../test-helper');
+const createServer = require('../../../../server');
+
+describe('Acceptance | Route | Users', function () {
+  describe('GET /api/admin/users/{id}/certification-center-memberships', function () {
+    it('should return 200 HTTP status code', async function () {
+      // given
+      const server = await createServer();
+      const adminUser = databaseBuilder.factory.buildUser.withRole();
+      const userId = databaseBuilder.factory.buildUser().id;
+
+      const certificationCenter = databaseBuilder.factory.buildCertificationCenter({
+        name: 'Centre Takeo',
+        type: 'SCO',
+        externalId: '1234',
+      });
+
+      const certificationCenterMembership = databaseBuilder.factory.buildCertificationCenterMembership({
+        certificationCenterId: certificationCenter.id,
+        userId,
+        disabledAt: null,
+      });
+
+      await databaseBuilder.commit();
+
+      // when
+      const response = await server.inject({
+        method: 'GET',
+        url: `/api/admin/users/${userId}/certification-center-memberships`,
+        headers: {
+          authorization: generateValidRequestAuthorizationHeader(adminUser.id),
+        },
+      });
+
+      // then
+      expect(response.statusCode).to.equal(200);
+      expect(response.result.data).to.be.instanceOf(Array);
+      expect(response.result.data).to.have.length(1);
+      expect(response.result.data[0]).to.have.property('id', certificationCenterMembership.id.toString());
+    });
+  });
+});
