0a30ab286ff16aba788106d71ee3bcc64933f21e
Mathieu Gilet <mathieu.gilet@zenika.com>
2022-10-03 14:56:05 +0200

    :sparkles: api: remove Joi validation on jury level

--- api/tests/integration/application/complementary-certification-course-results/complementary-certification-course-results-controller_test.js
+++ api/tests/integration/application/complementary-certification-course-results/complementary-certification-course-results-controller_test.js
@@ -24,72 +24,33 @@ describe('Integration | Application | complementary-certification-course-results
 
   describe('#saveJuryComplementaryCertificationCourseResult', function () {
     context('Success cases', function () {
-      // eslint-disable-next-line mocha/no-setup-in-describe
-      [
-        'PIX_EDU_FORMATION_INITIALE_2ND_DEGRE_INITIE',
-        'PIX_EDU_FORMATION_INITIALE_2ND_DEGRE_CONFIRME',
-        'PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_CONFIRME',
-        'PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_AVANCE',
-        'PIX_EDU_FORMATION_CONTINUE_2ND_DEGRE_EXPERT',
-        'PIX_EDU_FORMATION_INITIALE_1ER_DEGRE_INITIE',
-        'PIX_EDU_FORMATION_INITIALE_1ER_DEGRE_CONFIRME',
-        'PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_CONFIRME',
-        'PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_AVANCE',
-        'PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_EXPERT',
-        'REJECTED',
-      ].forEach((juryLevel) => {
-        it(`should resolve a 200 HTTP response for ${juryLevel} juryLevel`, async function () {
-          // given
-          const payload = {
-            data: {
-              attributes: {
-                juryLevel,
-                complementaryCertificationCourseId: 123456,
-              },
+      it(`should resolve a 200 HTTP response`, async function () {
+        // given
+        const payload = {
+          data: {
+            attributes: {
+              juryLevel: 'JURY_LEVEL',
+              complementaryCertificationCourseId: 123456,
             },
-          };
-          usecases.saveJuryComplementaryCertificationCourseResult.resolves();
-          securityPreHandlers.checkAdminMemberHasRoleSuperAdmin.callsFake((request, h) => h.response(true));
+          },
+        };
+        usecases.saveJuryComplementaryCertificationCourseResult.resolves();
+        securityPreHandlers.checkAdminMemberHasRoleSuperAdmin.callsFake((request, h) => h.response(true));
 
-          // when
-          const response = await httpTestServer.request(
-            'POST',
-            '/api/admin/complementary-certification-course-results',
-            payload
-          );
+        // when
+        const response = await httpTestServer.request(
+          'POST',
+          '/api/admin/complementary-certification-course-results',
+          payload
+        );
 
-          // then
-          expect(response.statusCode).to.equal(200);
-        });
+        // then
+        expect(response.statusCode).to.equal(200);
       });
     });
 
     context('Error cases', function () {
       context('when juryLevel is not valid', function () {
-        it('should resolve a 400 HTTP response by joi validation', async function () {
-          // given
-          const payload = {
-            data: {
-              attributes: {
-                juryLevel: 'INVALID_JURY_LEVEL',
-                complementaryCertificationCourseId: 123456,
-              },
-            },
-          };
-          usecases.saveJuryComplementaryCertificationCourseResult.resolves();
-          securityPreHandlers.checkAdminMemberHasRoleSuperAdmin.callsFake((request, h) => h.response(true));
-
-          // when
-          const response = await httpTestServer.request(
-            'POST',
-            '/api/admin/complementary-certification-course-results',
-            payload
-          );
-
-          // then
-          expect(response.statusCode).to.equal(400);
-        });
-
         it('should resolve a 400 HTTP response', async function () {
           // given
           const payload = {
