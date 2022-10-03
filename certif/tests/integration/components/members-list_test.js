d076f48dec45eb1001746d75e173345581d9841b
P-Jeremy <jemyplu@gmail.com>
2022-10-03 15:31:58 +0200

    :sparkles: certif: display referer tag in member list

--- certif/tests/integration/components/members-list_test.js
+++ certif/tests/integration/components/members-list_test.js
@@ -25,4 +25,36 @@ module('Integration | Component | members-list', function (hooks) {
     assert.dom(screen.getByText('John')).exists();
     assert.dom(screen.getByText('Williams')).exists();
   });
+
+  module('when a member is referer', function () {
+    test('it should show the referer tag', async function (assert) {
+      // given
+      const certifMember1 = EmberObject.create({ firstName: 'Maria', lastName: 'Carré' });
+      const certifMember2 = EmberObject.create({ firstName: 'John', lastName: 'Williams', isReferer: true });
+      const members = [certifMember1, certifMember2];
+      this.set('members', members);
+
+      // when
+      const screen = await renderScreen(hbs`<MembersList @members={{this.members}} />`);
+
+      // then
+      assert.dom(screen.getByText('Référent Pix')).exists();
+    });
+  });
+
+  module('when there is no referer', function () {
+    test('it should not show the referer tag', async function (assert) {
+      // given
+      const certifMember1 = EmberObject.create({ firstName: 'Maria', lastName: 'Carré', isReferer: false });
+      const certifMember2 = EmberObject.create({ firstName: 'John', lastName: 'Williams', isReferer: false });
+      const members = [certifMember1, certifMember2];
+      this.set('members', members);
+
+      // when
+      const screen = await renderScreen(hbs`<MembersList @members={{this.members}} />`);
+
+      // then
+      assert.dom(screen.queryByText('Référent Pix')).doesNotExist();
+    });
+  });
 });
