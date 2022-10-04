5c5c35dc37ce76ab45e55d1e7c46046b72207c21
Anne-G <ang.schall@gmail.com>
2022-10-04 16:21:48 +0200

    admin: do not show other certification center invitations

--- admin/tests/unit/routes/authenticated/certification-centers/get/invitations_test.js
+++ admin/tests/unit/routes/authenticated/certification-centers/get/invitations_test.js
@@ -5,12 +5,29 @@ import sinon from 'sinon';
 module('Unit | Route | authenticated/certification-centers/get/invitations', function (hooks) {
   setupTest(hooks);
 
+  test("it should unload certification center invitation because it's maybe other certification center's invitations", async function (assert) {
+    // given
+    const route = this.owner.lookup('route:authenticated/certification-centers/get/invitations');
+    const store = this.owner.lookup('service:store');
+
+    store.query = sinon.stub();
+    store.unloadAll = sinon.stub();
+    route.modelFor = sinon.stub().returns({ certificationCenter: { id: 777 } });
+
+    // when
+    await route.model();
+
+    // then
+    assert.ok(store.unloadAll.calledWith('certification-center-invitation'));
+  });
+
   test('it should return certification center invitations', async function (assert) {
     // given
     const route = this.owner.lookup('route:authenticated/certification-centers/get/invitations');
     const store = this.owner.lookup('service:store');
 
     const certificationCenterInvitations = Symbol('some certification center invitations');
+    store.unloadAll = sinon.stub();
     store.query = sinon.stub();
     store.query
       .withArgs('certification-center-invitation', {
