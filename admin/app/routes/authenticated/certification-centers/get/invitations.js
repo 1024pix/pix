b122f9d49247cfed0b45991e4db2251bfb17d9fa
Anne-G <ang.schall@gmail.com>
2022-10-04 16:21:48 +0200

    admin: do not show other certification center invitations

--- admin/app/routes/authenticated/certification-centers/get/invitations.js
+++ admin/app/routes/authenticated/certification-centers/get/invitations.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 
 export default class AuthenticatedCertificationCentersGetInvitationsRoute extends Route {
   async model() {
+    this.store.unloadAll('certification-center-invitation');
     const { certificationCenter } = this.modelFor('authenticated.certification-centers.get');
     const certificationCenterId = certificationCenter.id;
     return await this.store.query('certification-center-invitation', {
