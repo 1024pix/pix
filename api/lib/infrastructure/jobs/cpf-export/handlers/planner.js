a26e24b8ea17870a53f5497912f4e568ab554761
Antoine Ceol <antoine.ceol@gmail.com>
2022-10-11 12:32:34 +0200

    :recycle: api: Replace moment by dayjs

--- api/lib/infrastructure/jobs/cpf-export/handlers/planner.js
+++ api/lib/infrastructure/jobs/cpf-export/handlers/planner.js
@@ -1,14 +1,14 @@
-const moment = require('moment');
+const dayjs = require('dayjs');
 const { plannerJob } = require('../../../../config').cpf;
 const times = require('lodash/times');
 
 module.exports = async function planner({ pgBoss, cpfCertificationResultRepository }) {
-  const startDate = moment()
+  const startDate = dayjs()
     .utc()
     .subtract(plannerJob.minimumReliabilityPeriod + (plannerJob.monthsToProcess - 1), 'months')
     .startOf('month')
     .toDate();
-  const endDate = moment().utc().subtract(plannerJob.minimumReliabilityPeriod, 'months').endOf('month').toDate();
+  const endDate = dayjs().utc().subtract(plannerJob.minimumReliabilityPeriod, 'months').endOf('month').toDate();
 
   const certificationsCount = await cpfCertificationResultRepository.countByTimeRange({ startDate, endDate });
   const jobCount = Math.ceil(certificationsCount / plannerJob.chunkSize);
