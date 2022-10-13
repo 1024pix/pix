8672902ba91ba081a6469c22591daca31b6d3aa0
Ismael Gorissen <ismael.gorissen@gmail.com>
2022-10-13 17:41:24 +0200

    feat(api): update usecase to handle DPO

--- api/lib/domain/usecases/update-certification-center.js
+++ api/lib/domain/usecases/update-certification-center.js
@@ -1,17 +1,39 @@
 const bluebird = require('bluebird');
 const certificationCenterCreationValidator = require('../validators/certification-center-creation-validator');
 const ComplementaryCertificationHabilitation = require('../../domain/models/ComplementaryCertificationHabilitation');
+const DataProtectionOfficer = require('../models/DataProtectionOfficer');
+const ComplementaryCertification = require('../models/ComplementaryCertification');
+
+async function _addOrUpdateDataProtectionOfficer({ certificationCenter, dataProtectionOfficerRepository }) {
+  const dataProtectionOfficer = new DataProtectionOfficer({
+    firstName: certificationCenter.dataProtectionOfficerFirstName ?? '',
+    lastName: certificationCenter.dataProtectionOfficerLastName ?? '',
+    email: certificationCenter.dataProtectionOfficerEmail ?? '',
+    certificationCenterId: certificationCenter.id,
+  });
+
+  const dataProtectionOfficerFound = await dataProtectionOfficerRepository.get({
+    certificationCenterId: certificationCenter.id,
+  });
+
+  if (dataProtectionOfficerFound) return dataProtectionOfficerRepository.update(dataProtectionOfficer);
+
+  return dataProtectionOfficerRepository.create(dataProtectionOfficer);
+}
 
 module.exports = async function updateCertificationCenter({
   certificationCenter,
   complementaryCertificationIds,
-  certificationCenterRepository,
+  certificationCenterForAdminRepository,
   complementaryCertificationHabilitationRepository,
+  dataProtectionOfficerRepository,
 }) {
   certificationCenterCreationValidator.validate(certificationCenter);
+
   if (certificationCenter.id) {
     await complementaryCertificationHabilitationRepository.deleteByCertificationCenterId(certificationCenter.id);
   }
+
   if (complementaryCertificationIds) {
     await bluebird.mapSeries(complementaryCertificationIds, (complementaryCertificationId) => {
       const complementaryCertificationHabilitation = new ComplementaryCertificationHabilitation({
@@ -22,5 +44,26 @@ module.exports = async function updateCertificationCenter({
     });
   }
 
-  return certificationCenterRepository.save(certificationCenter);
+  const updatedCertificationCenter = await certificationCenterForAdminRepository.update(certificationCenter);
+
+  const habilitations = await complementaryCertificationHabilitationRepository.findByCertificationCenterId(
+    updatedCertificationCenter.id
+  );
+  updatedCertificationCenter.habilitations = habilitations.map((habilitation) => {
+    return new ComplementaryCertification({
+      id: habilitation.id,
+      key: habilitation.key,
+      label: habilitation.label,
+    });
+  });
+
+  const dataProtectionOfficer = await _addOrUpdateDataProtectionOfficer({
+    dataProtectionOfficerRepository,
+    certificationCenter,
+  });
+  updatedCertificationCenter.dataProtectionOfficerFirstName = dataProtectionOfficer.firstName;
+  updatedCertificationCenter.dataProtectionOfficerLastName = dataProtectionOfficer.lastName;
+  updatedCertificationCenter.dataProtectionOfficerEmail = dataProtectionOfficer.email;
+
+  return updatedCertificationCenter;
 };
