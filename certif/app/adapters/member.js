d0b77c10aa79c0f4b3ba43bdcb496766a92ce940
P-Jeremy <jemyplu@gmail.com>
2022-10-14 16:17:56 +0200

    :sparkles: certif: add #buildUrl to member adapter

--- certif/app/adapters/member.js
+++ certif/app/adapters/member.js
@@ -1,7 +1,20 @@
 import ApplicationAdapter from './application';
+import { inject as service } from '@ember/service';
 
 export default class MemberAdapter extends ApplicationAdapter {
+  @service currentUser;
+
   urlForQuery(query) {
     return `${this.host}/${this.namespace}/certification-centers/${query.certificationCenterId}/members`;
+  }
+
+  buildURL(modelName, id, snapshot, requestType, query) {
+    if (requestType === 'update-referer') {
+      const certificationCenterId = this.currentUser.currentAllowedCertificationCenterAccess.id;
+
+      return `${this.host}/${this.namespace}/certif/certification-centers/${certificationCenterId}/update-referer`;
+    } else {
+      return super.buildURL(modelName, id, snapshot, requestType, query);
+    }
   }
 }
