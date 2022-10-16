86889d51b34a3735ae56a2c6a9027a74dc6fec4a
Laura Bergoens <laura.bergoens@pix.fr>
2022-10-16 21:25:40 +0200

    use mock in course-controller acceptance test

--- api/tests/acceptance/application/course-controller_test.js
+++ api/tests/acceptance/application/course-controller_test.js
@@ -1,15 +1,10 @@
-const {
-  expect,
-  nock,
-  generateValidRequestAuthorizationHeader,
-  mockLearningContent,
-  learningContentBuilder,
-} = require('../../test-helper');
+const { expect, nock, generateValidRequestAuthorizationHeader, LearningContentMock } = require('../../test-helper');
 const createServer = require('../../../server');
 
 describe('Acceptance | API | Courses', function () {
   let server;
   const userId = 42;
+  const courseId = 'recCourse1';
 
   beforeEach(async function () {
     server = await createServer();
@@ -17,38 +12,7 @@ describe('Acceptance | API | Courses', function () {
 
   describe('GET /api/courses/:course_id', function () {
     beforeEach(function () {
-      const learningContent = [
-        {
-          id: '1. Information et données',
-          competences: [
-            {
-              id: 'competence_id',
-              tubes: [
-                {
-                  id: 'recTube1',
-                  skills: [
-                    {
-                      challenges: [{ id: 'k_challenge_id' }],
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-          courses: [
-            {
-              id: 'rec_course_id',
-              name: "A la recherche de l'information #01",
-              description: "Mener une recherche et une veille d'information",
-              competenceId: 'competence_id',
-              challengeIds: ['k_challenge_id'],
-            },
-          ],
-        },
-      ];
-
-      const learningContentObjects = learningContentBuilder.buildLearningContent.fromAreas(learningContent);
-      mockLearningContent(learningContentObjects);
+      LearningContentMock.mockCommon();
     });
 
     after(function () {
@@ -61,45 +25,40 @@ describe('Acceptance | API | Courses', function () {
       beforeEach(function () {
         options = {
           method: 'GET',
-          url: '/api/courses/rec_course_id',
+          url: `/api/courses/${courseId}`,
           headers: {
             authorization: generateValidRequestAuthorizationHeader(userId),
           },
         };
       });
 
-      it('should return 200 HTTP status code', function () {
+      it('should return 200 HTTP status code', async function () {
         // when
-        const promise = server.inject(options);
+        const response = await server.inject(options);
 
         // then
-        return promise.then((response) => {
-          expect(response.statusCode).to.equal(200);
-        });
+        expect(response.statusCode).to.equal(200);
       });
 
-      it('should return application/json', function () {
+      it('should return application/json', async function () {
         // when
-        const promise = server.inject(options);
+        const response = await server.inject(options);
 
         // then
-        return promise.then((response) => {
-          const contentType = response.headers['content-type'];
-          expect(contentType).to.contain('application/json');
-        });
+        const contentType = response.headers['content-type'];
+        expect(contentType).to.contain('application/json');
       });
 
-      it('should return the expected course', function () {
+      it('should return the expected course', async function () {
         // when
-        const promise = server.inject(options);
+        const response = await server.inject(options);
 
         // then
-        return promise.then((response) => {
-          const course = response.result.data;
-          expect(course.id).to.equal('rec_course_id');
-          expect(course.attributes.name).to.equal("A la recherche de l'information #01");
-          expect(course.attributes.description).to.equal("Mener une recherche et une veille d'information");
-        });
+        const courseData = LearningContentMock.getCourseDTO(courseId);
+        const course = response.result.data;
+        expect(course.id).to.equal(courseId);
+        expect(course.attributes.name).to.equal(courseData.name);
+        expect(course.attributes.description).to.equal(courseData.description);
       });
     });
 
@@ -113,14 +72,12 @@ describe('Acceptance | API | Courses', function () {
         };
       });
 
-      it('should return 404 HTTP status code', function () {
+      it('should return 404 HTTP status code', async function () {
         // when
-        const promise = server.inject(options);
+        const response = await server.inject(options);
 
         // then
-        return promise.then((response) => {
-          expect(response.statusCode).to.equal(404);
-        });
+        expect(response.statusCode).to.equal(404);
       });
     });
   });
