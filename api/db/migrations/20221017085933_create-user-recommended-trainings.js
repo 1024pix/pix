421781feca7a381ca06150d06239cf1d2c2eb902
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-10-17 11:15:48 +0200

    feat(api): create user-recommended-trainings table

---
+++ api/db/migrations/20221017085933_create-user-recommended-trainings.js
@@ -0,0 +1,17 @@
+const TABLE_NAME = 'user-recommended-trainings';
+
+exports.up = (knex) => {
+  return knex.schema.createTable(TABLE_NAME, (t) => {
+    t.increments().primary();
+    t.integer('userId').references('users.id').notNullable();
+    t.integer('trainingId').references('trainings.id').notNullable();
+    t.integer('campaignParticipationId').references('campaign-participations.id').notNullable();
+    t.dateTime('createdAt').notNullable().defaultTo(knex.fn.now());
+    t.dateTime('updatedAt').notNullable().defaultTo(knex.fn.now());
+    t.unique(['userId', 'trainingId', 'campaignParticipationId']);
+  });
+};
+
+exports.down = (knex) => {
+  return knex.schema.dropTable(TABLE_NAME);
+};
