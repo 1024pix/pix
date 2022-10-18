736feac6ac507f1a5dbb1e16deaed56ab06290cf
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-10-18 14:32:22 +0200

    feat(api): add findByCampaignParticipationId in user-recommended-training repository
    Co-authored-by: Clarisse Damon <clarisse.damon@pix.fr>
    Co-authored-by: Corentin Rousseau <corentin.rousseau@pix.fr>
    Co-authored-by: Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
    Co-authored-by: Matthias Ferraina <matthias.ferraina@pix.fr>

--- api/lib/infrastructure/repositories/user-recommended-training-repository.js
+++ api/lib/infrastructure/repositories/user-recommended-training-repository.js
@@ -1,5 +1,6 @@
 const { knex } = require('../../../db/knex-database-connection');
 const DomainTransaction = require('../DomainTransaction');
+const UserRecommendedTraining = require('../../domain/read-models/UserRecommendedTraining');
 
 const TABLE_NAME = 'user-recommended-trainings';
 
@@ -8,4 +9,22 @@ module.exports = {
     const knexConn = domainTransaction?.knexTransaction || knex;
     return knexConn(TABLE_NAME).insert({ userId, trainingId, campaignParticipationId });
   },
+
+  async findByCampaignParticipationId({
+    campaignParticipationId,
+    locale,
+    domainTransaction = DomainTransaction.emptyTransaction(),
+  }) {
+    const knexConn = domainTransaction?.knexTransaction || knex;
+    const trainings = await knexConn(TABLE_NAME)
+      .select('trainings.*')
+      .innerJoin('trainings', 'trainings.id', `${TABLE_NAME}.trainingId`)
+      .where({ campaignParticipationId, locale })
+      .orderBy('id', 'asc');
+    return trainings.map(_toDomain);
+  },
 };
+
+function _toDomain(training) {
+  return new UserRecommendedTraining({ ...training });
+}
--- api/tests/integration/infrastructure/repositories/user-recommended-training-repository_test.js
+++ api/tests/integration/infrastructure/repositories/user-recommended-training-repository_test.js
@@ -1,13 +1,14 @@
 const _ = require('lodash');
 const { expect, databaseBuilder, knex } = require('../../../test-helper');
 const userRecommendedTrainingRepository = require('../../../../lib/infrastructure/repositories/user-recommended-training-repository');
+const UserRecommendedTraining = require('../../../../lib/domain/read-models/UserRecommendedTraining');
 
 describe('Integration | Repository | user-recommended-training-repository', function () {
-  afterEach(async function () {
-    await knex('user-recommended-trainings').delete();
-  });
-
   describe('#save', function () {
+    afterEach(async function () {
+      await knex('user-recommended-trainings').delete();
+    });
+
     it('should persist userRecommendedTraining', async function () {
       // given
       const userRecommendedTraining = {
@@ -31,6 +32,73 @@ describe('Integration | Repository | user-recommended-training-repository', func
       expect(_.omit(persistedUserRecommendedTraining, ['id', 'createdAt', 'updatedAt'])).to.deep.equal(
         userRecommendedTraining
       );
+    });
+  });
+
+  describe('#findByCampaignParticipationId', function () {
+    it('should return saved recommended trainings for given campaignParticipationId and locale', async function () {
+      // given
+      const { id: campaignParticipationId, userId } = databaseBuilder.factory.buildCampaignParticipation();
+      const training = databaseBuilder.factory.buildTraining();
+      const userRecommendedTraining1 = {
+        userId,
+        trainingId: training.id,
+        campaignParticipationId,
+      };
+      const userRecommendedTraining2 = {
+        userId,
+        trainingId: databaseBuilder.factory.buildTraining().id,
+        campaignParticipationId,
+      };
+      const userRecommendedTrainingWithAnotherLocale = {
+        userId,
+        trainingId: databaseBuilder.factory.buildTraining({ locale: 'en-gb' }).id,
+        campaignParticipationId,
+      };
+      const anotherCampaignParticipation = databaseBuilder.factory.buildCampaignParticipation();
+      const anotherUserRecommendedTraining = {
+        userId: anotherCampaignParticipation.userId,
+        trainingId: databaseBuilder.factory.buildTraining().id,
+        campaignParticipationId: anotherCampaignParticipation.id,
+      };
+      databaseBuilder.factory.buildUserRecommendedTraining(userRecommendedTraining1);
+      databaseBuilder.factory.buildUserRecommendedTraining(userRecommendedTraining2);
+      databaseBuilder.factory.buildUserRecommendedTraining(userRecommendedTrainingWithAnotherLocale);
+      databaseBuilder.factory.buildUserRecommendedTraining(anotherUserRecommendedTraining);
+      await databaseBuilder.commit();
+
+      // when
+      const result = await userRecommendedTrainingRepository.findByCampaignParticipationId({
+        campaignParticipationId,
+        locale: 'fr-fr',
+      });
+
+      // then
+      expect(result.length).to.equal(2);
+      expect(result[0]).to.be.instanceOf(UserRecommendedTraining);
+      expect(result[0]).to.deep.equal(new UserRecommendedTraining({ ...training, duration: { hours: 6 } }));
+    });
+
+    it('should return an empty array when user has no recommended training for this campaignParticipation', async function () {
+      // given
+      const { id: campaignParticipationId, userId } = databaseBuilder.factory.buildCampaignParticipation();
+      const anotherCampaignParticipation = databaseBuilder.factory.buildCampaignParticipation();
+      const userRecommendedTraining = {
+        userId,
+        trainingId: databaseBuilder.factory.buildTraining().id,
+        campaignParticipationId,
+      };
+      databaseBuilder.factory.buildUserRecommendedTraining(userRecommendedTraining);
+      await databaseBuilder.commit();
+
+      // when
+      const result = await userRecommendedTrainingRepository.findByCampaignParticipationId({
+        campaignParticipationId: anotherCampaignParticipation.id,
+        locale: 'fr-fr',
+      });
+
+      // then
+      expect(result.length).to.equal(0);
     });
   });
 });
