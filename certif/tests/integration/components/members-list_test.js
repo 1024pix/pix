0ce070456cc04120e23ffd3b212947027bcfc0b9
P-Jeremy <jemyplu@gmail.com>
2022-10-18 14:39:54 +0200

    :recycle: certif: show referer column if certification center is habilitated clea

--- certif/tests/integration/components/members-list_test.js
+++ certif/tests/integration/components/members-list_test.js
@@ -34,8 +34,8 @@ module('Integration | Component | members-list', function (hooks) {
   });
 
   module('when FT_CLEA_RESULTS_RETRIEVAL_BY_HABILITATED_CERTIFICATION_CENTERS is enabled', function () {
-    module('when a member is referer', function () {
-      test('it should show the referer tag', async function (assert) {
+    module('when certification center is habilitated CléA', function () {
+      test('it should show the referer column', async function (assert) {
         // given
         class FeatureTogglesStub extends Service {
           featureToggles = { isCleaResultsRetrievalByHabilitatedCertificationCentersEnabled: true };
@@ -45,17 +45,65 @@ module('Integration | Component | members-list', function (hooks) {
         const certifMember2 = EmberObject.create({ firstName: 'John', lastName: 'Williams', isReferer: true });
         const members = [certifMember1, certifMember2];
         this.set('members', members);
+        this.set('hasCleaHabilitation', true);
 
         // when
-        const screen = await renderScreen(hbs`<MembersList @members={{this.members}} />`);
+        const screen = await renderScreen(
+          hbs`<MembersList @members={{this.members}} @hasCleaHabilitation={{this.hasCleaHabilitation}} />`
+        );
 
         // then
-        assert.dom(screen.getByRole('cell', { name: this.intl.t('pages.team.pix-referer') })).exists();
+        assert.dom(screen.getByRole('columnheader', { name: this.intl.t('pages.team.referer') })).exists();
+      });
+
+      module('when a member is referer', function () {
+        test('it should show the referer tag', async function (assert) {
+          // given
+          class FeatureTogglesStub extends Service {
+            featureToggles = { isCleaResultsRetrievalByHabilitatedCertificationCentersEnabled: true };
+          }
+          this.owner.register('service:featureToggles', FeatureTogglesStub);
+          const certifMember1 = EmberObject.create({ firstName: 'Maria', lastName: 'Carré', isReferer: false });
+          const certifMember2 = EmberObject.create({ firstName: 'John', lastName: 'Williams', isReferer: true });
+          const members = [certifMember1, certifMember2];
+          this.set('members', members);
+          this.set('hasCleaHabilitation', true);
+
+          // when
+          const screen = await renderScreen(
+            hbs`<MembersList @members={{this.members}} @hasCleaHabilitation={{this.hasCleaHabilitation}} />`
+          );
+
+          // then
+          assert.dom(screen.getByRole('cell', { name: this.intl.t('pages.team.pix-referer') })).exists();
+        });
+      });
+
+      module('when there is no referer', function () {
+        test('it should not show the referer tag', async function (assert) {
+          // given
+          class FeatureTogglesStub extends Service {
+            featureToggles = { isCleaResultsRetrievalByHabilitatedCertificationCentersEnabled: true };
+          }
+          this.owner.register('service:featureToggles', FeatureTogglesStub);
+          const certifMember1 = EmberObject.create({ firstName: 'Maria', lastName: 'Carré', isReferer: false });
+          const members = [certifMember1];
+          this.set('members', members);
+          this.set('hasCleaHabilitation', true);
+
+          // when
+          const screen = await renderScreen(
+            hbs`<MembersList @members={{this.members}} @hasCleaHabilitation={{this.hasCleaHabilitation}} />`
+          );
+
+          // then
+          assert.dom(screen.queryByRole('cell', { name: this.intl.t('pages.team.pix-referer') })).doesNotExist();
+        });
       });
     });
 
-    module('when there is no referer', function () {
-      test('it should not show the referer tag', async function (assert) {
+    module('when certification center is not habilitated CléA', function () {
+      test('it should not show the referer column', async function (assert) {
         // given
         class FeatureTogglesStub extends Service {
           featureToggles = { isCleaResultsRetrievalByHabilitatedCertificationCentersEnabled: true };
@@ -64,12 +112,15 @@ module('Integration | Component | members-list', function (hooks) {
         const certifMember1 = EmberObject.create({ firstName: 'Maria', lastName: 'Carré', isReferer: false });
         const members = [certifMember1];
         this.set('members', members);
+        this.set('hasCleaHabilitation', false);
 
         // when
-        const screen = await renderScreen(hbs`<MembersList @members={{this.members}} />`);
+        const screen = await renderScreen(
+          hbs`<MembersList @members={{this.members}} @hasCleaHabilitation={{this.hasCleaHabilitation}} />`
+        );
 
         // then
-        assert.dom(screen.queryByRole('cell', { name: this.intl.t('pages.team.pix-referer') })).doesNotExist();
+        assert.dom(screen.queryByRole('columnheader', { name: this.intl.t('pages.team.referer') })).doesNotExist();
       });
     });
   });
@@ -84,9 +135,12 @@ module('Integration | Component | members-list', function (hooks) {
       const certifMember1 = EmberObject.create({ firstName: 'Maria', lastName: 'Carré', isReferer: true });
       const members = [certifMember1];
       this.set('members', members);
+      this.set('hasCleaHabilitation', true);
 
       // when
-      const screen = await renderScreen(hbs`<MembersList @members={{this.members}} />`);
+      const screen = await renderScreen(
+        hbs`<MembersList @members={{this.members}} @hasCleaHabilitation={{this.hasCleaHabilitation}} />`
+      );
 
       // then
       assert.dom(screen.queryByRole('cell', { name: this.intl.t('pages.team.pix-referer') })).doesNotExist();
