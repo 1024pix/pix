a961c6628e2156278f48fde72acc5edd01db2ba9
Pierre TOP (TOPI) <ptop@octo.com>
2022-10-19 14:08:27 +0200

    Allow execution on non-unix platform

--- api/nyc.config.js
+++ api/nyc.config.js
@@ -1,15 +1,10 @@
-{
-  "all": true,
-  "reporter": [
-    "html"
-  ],
-  "include": [
-    "lib/**/*.js"
-  ],
-  "exclude": [
-    "**/.eslintrc.js",
-    "./lib/infrastructure/validate-environment-variables.js"
-  ],
-  "temp-dir": "/tmp/pix-api-test-coverage/tmp",
-  "report-dir": "/tmp/pix-api-test-coverage/report"
-}
+const { tmpdir } = require('node:os');
+const baseDirectory = `${tmpdir()}/pix-api-test-coverage`;
+module.exports = {
+  all: true,
+  reporter: ['html'],
+  include: ['lib/**/*.js'],
+  exclude: ['**/.eslintrc.js', './lib/infrastructure/validate-environment-variables.js'],
+  'temp-dir': `${baseDirectory}/tmp`,
+  'report-dir': `${baseDirectory}/report`,
+};
