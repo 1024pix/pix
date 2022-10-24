49c8ab5147b74404cca1bde57ea6354c06b81172
P-Jeremy <jemyplu@gmail.com>
2022-10-24 16:32:26 +0200

    :sparkles: certif: add filter on member list, sort by lastname and isReferer

--- certif/app/components/members-list.js
+++ certif/app/components/members-list.js
@@ -4,6 +4,24 @@ import { inject as service } from '@ember/service';
 export default class MembersList extends Component {
   @service featureToggles;
 
+  get orderedMemberListWithRefererFirst() {
+    const membersArray = this.args.members.toArray();
+
+    membersArray.sort((member1, member2) => {
+      if (member1.isReferer) {
+        return -1;
+      }
+
+      if (member2.isReferer) {
+        return 1;
+      }
+
+      return member1.lastName.localeCompare(member2.lastName, 'fr-FR', { sensitivity: 'base' });
+    });
+
+    return membersArray;
+  }
+
   get shouldDisplayRefererColumn() {
     return (
       this.args.hasCleaHabilitation &&
