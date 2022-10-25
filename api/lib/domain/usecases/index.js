ef1f74aed4be07b3584c2e429ac80a847d030c15
Andreia Pena <andreia.pena@pix.fr>
2022-10-25 14:53:45 +0200

    feat(api): create repository and get method to return certification-center-invitation

--- api/lib/domain/usecases/index.js
+++ api/lib/domain/usecases/index.js
@@ -42,6 +42,7 @@ const dependencies = {
   certificationCandidateForSupervisingRepository: require('../../infrastructure/repositories/certification-candidate-for-supervising-repository'),
   certificationCandidatesOdsService: require('../../domain/services/certification-candidates-ods-service'),
   certificationCenterInvitationRepository: require('../../infrastructure/repositories/certification-center-invitation-repository'),
+  certificationCenterInvitedUserRepository: require('../../infrastructure/repositories/certification-center-invited-user-repository'),
   certificationCenterMembershipRepository: require('../../infrastructure/repositories/certification-center-membership-repository'),
   certificationCenterForAdminRepository: require('../../infrastructure/repositories/certification-center-for-admin-repository'),
   certificationCenterRepository: require('../../infrastructure/repositories/certification-center-repository'),
