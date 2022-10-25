e59a44a3084e59a948f25cc46597906a63403dd4
Mickaël Alibert <mickael.alibert@pix.fr>
2022-10-25 11:57:07 +0200

    :wrench: Rename feature flag

--- api/lib/infrastructure/validate-environment-variables.js
+++ api/lib/infrastructure/validate-environment-variables.js
@@ -55,7 +55,7 @@ const schema = Joi.object({
   CPF_PLANNER_JOB_CRON: Joi.string().optional(),
   CPF_SEND_EMAIL_JOB_RECIPIENT: Joi.string().optional(),
   CPF_SEND_EMAIL_JOB_CRON: Joi.string().optional(),
-  LOG_KNEX_PERFORMANCE: Joi.string().optional().valid('true', 'false'),
+  ENABLE_KNEX_PERFORMANCE_MONITORING: Joi.string().optional().valid('true', 'false'),
 }).options({ allowUnknown: true });
 
 const validateEnvironmentVariables = function () {
