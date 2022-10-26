7e90d14d7613abec5b961da770c5ca874d760dcb
Laura Bergoens <laura.bergoens@pix.fr>
2022-10-26 20:57:36 +0200

    use new attributes in get framework areas usecase

--- api/tests/unit/domain/usecases/get-framework-areas_test.js
+++ api/tests/unit/domain/usecases/get-framework-areas_test.js
@@ -2,11 +2,9 @@ const { expect, sinon } = require('../../../test-helper');
 const usecases = require('../../../../lib/domain/usecases');
 
 describe('Unit | UseCase | get-framework-areas', function () {
-  let expectedChallengeResult,
-    expectedTubesResult,
+  let expectedTubesResult,
     expectedThematicsResult,
     expectedAreasResult,
-    challengeRepository,
     tubeRepository,
     thematicRepository,
     areaRepository,
@@ -14,18 +12,14 @@ describe('Unit | UseCase | get-framework-areas', function () {
     expectedFrameworkResult;
 
   beforeEach(function () {
-    expectedChallengeResult = [{ id: 'challengeId1', responsive: '', skill: { tubeId: 'tubeId1' } }];
-    expectedTubesResult = [{ id: 'tubeId1' }];
+    expectedTubesResult = [{ id: 'tubeId1', mobile: false, tablet: true }];
     expectedThematicsResult = [{ id: 'thematicId', tubeIds: ['tubeId1'] }];
     expectedAreasResult = [{ id: 'areaId1', competences: [{ id: 'competenceId1' }] }];
     expectedFrameworkResult = { id: 'frameworkId', name: 'framework' };
 
-    challengeRepository = {
-      findValidatedPrototype: sinon.stub().resolves(expectedChallengeResult),
-    };
-
+    const tubesFromRepository = [{ id: 'tubeId1', isMobileCompliant: false, isTabletCompliant: true }];
     tubeRepository = {
-      findActiveByRecordIds: sinon.stub().resolves(expectedTubesResult),
+      findActiveByRecordIds: sinon.stub().resolves(tubesFromRepository),
     };
 
     thematicRepository = {
@@ -46,7 +40,6 @@ describe('Unit | UseCase | get-framework-areas', function () {
     const response = await usecases.getFrameworkAreas({
       frameworkId: 'frameworkId',
       locale: 'locale',
-      challengeRepository,
       tubeRepository,
       thematicRepository,
       areaRepository,
@@ -57,7 +50,6 @@ describe('Unit | UseCase | get-framework-areas', function () {
       thematics: expectedThematicsResult,
       areas: expectedAreasResult,
     });
-    expect(challengeRepository.findValidatedPrototype).to.have.been.calledWithExactly();
     expect(tubeRepository.findActiveByRecordIds).to.have.been.calledWith(['tubeId1'], 'locale');
     expect(thematicRepository.findByCompetenceIds).to.have.been.calledWith(['competenceId1'], 'locale');
     expect(areaRepository.findByFrameworkIdWithCompetences).to.have.been.calledWith({
@@ -70,7 +62,6 @@ describe('Unit | UseCase | get-framework-areas', function () {
     const response = await usecases.getFrameworkAreas({
       frameworkName: 'framework',
       locale: 'locale',
-      challengeRepository,
       tubeRepository,
       thematicRepository,
       areaRepository,
@@ -82,7 +73,6 @@ describe('Unit | UseCase | get-framework-areas', function () {
       thematics: expectedThematicsResult,
       areas: expectedAreasResult,
     });
-    expect(challengeRepository.findValidatedPrototype).to.have.been.calledWithExactly();
     expect(tubeRepository.findActiveByRecordIds).to.have.been.calledWith(['tubeId1'], 'locale');
     expect(thematicRepository.findByCompetenceIds).to.have.been.calledWith(['competenceId1'], 'locale');
     expect(areaRepository.findByFrameworkIdWithCompetences).to.have.been.calledWith({
@@ -91,124 +81,4 @@ describe('Unit | UseCase | get-framework-areas', function () {
     });
     expect(frameworkRepository.getByName).to.have.been.calledWithExactly('framework');
   });
-
-  /* eslint-disable mocha/no-setup-in-describe */
-  [
-    {
-      challengeResult: [
-        {
-          id: 'challengeId1',
-          responsive: 'Tablette/Smartphone',
-          skill: { tubeId: 'tubeId1' },
-        },
-      ],
-      expectedResult: {
-        mobile: true,
-        tablet: true,
-      },
-    },
-    {
-      challengeResult: [
-        {
-          id: 'challengeId1',
-          responsive: 'Tablette/Smartphone',
-          skill: { tubeId: 'tubeId1' },
-        },
-        {
-          id: 'challengeId2',
-          responsive: 'Tablette',
-          skill: { tubeId: 'tubeId1' },
-        },
-      ],
-      expectedResult: {
-        mobile: false,
-        tablet: true,
-      },
-    },
-    {
-      challengeResult: [
-        {
-          id: 'challengeId1',
-          responsive: 'Tablette/Smartphone',
-          skill: { tubeId: 'tubeId1' },
-        },
-        {
-          id: 'challengeId2',
-          responsive: 'Smartphone',
-          skill: { tubeId: 'tubeId1' },
-        },
-      ],
-      expectedResult: {
-        mobile: true,
-        tablet: false,
-      },
-    },
-    {
-      challengeResult: [
-        {
-          id: 'challengeId1',
-          responsive: 'Tablette/Smartphone',
-          skill: { tubeId: 'tubeId1' },
-        },
-        {
-          id: 'challengeId2',
-          responsive: 'Non',
-          skill: { tubeId: 'tubeId1' },
-        },
-      ],
-      expectedResult: {
-        mobile: false,
-        tablet: false,
-      },
-    },
-    {
-      challengeResult: [],
-      expectedResult: {
-        mobile: false,
-        tablet: false,
-      },
-    },
-    {
-      challengeResult: [
-        {
-          id: 'challengeId1',
-          responsive: 'Tablette/Smartphone',
-          skill: { tubeId: 'tubeId2' },
-        },
-      ],
-      expectedResult: {
-        mobile: false,
-        tablet: false,
-      },
-    },
-  ].forEach(({ challengeResult, expectedResult }) => {
-    context(
-      `when challenges have responsive=${challengeResult.map(
-        ({ responsive }) => `${responsive}`
-      )} and tubeId=${challengeResult.map(({ skill: { tubeId } }) => tubeId)}`,
-      function () {
-        it(`should return a tube with mobile=${expectedResult.mobile} and tablet=${expectedResult.tablet}`, async function () {
-          // given
-          challengeRepository = {
-            findValidatedPrototype: sinon.stub().resolves(challengeResult),
-          };
-
-          // when
-          const { tubes } = await usecases.getFrameworkAreas({
-            frameworkId: 'frameworkId',
-            challengeRepository,
-            tubeRepository,
-            thematicRepository,
-            areaRepository,
-            frameworkRepository,
-          });
-
-          // then
-          expect(tubes[0].mobile).to.equal(expectedResult.mobile);
-          expect(tubes[0].tablet).to.equal(expectedResult.tablet);
-        });
-      }
-    );
-  });
-  /* eslint-enable mocha/no-setup-in-describe */
 });
