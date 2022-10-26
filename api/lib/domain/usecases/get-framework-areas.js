4b91b15e92e3bc2b8cdf0306efa9b20288407b46
Laura Bergoens <laura.bergoens@pix.fr>
2022-10-26 20:57:36 +0200

    use new attributes in get framework areas usecase

--- api/lib/domain/usecases/get-framework-areas.js
+++ api/lib/domain/usecases/get-framework-areas.js
@@ -2,7 +2,6 @@ module.exports = async function getFrameworkAreas({
   frameworkId,
   frameworkName,
   locale,
-  challengeRepository,
   tubeRepository,
   thematicRepository,
   areaRepository,
@@ -22,34 +21,12 @@ module.exports = async function getFrameworkAreas({
 
   const tubeIds = thematics.flatMap((thematic) => thematic.tubeIds);
   const tubes = await tubeRepository.findActiveByRecordIds(tubeIds, locale);
+  for (const tube of tubes) {
+    tube.mobile = tube.isMobileCompliant;
+    tube.tablet = tube.isTabletCompliant;
+    delete tube.isMobileCompliant;
+    delete tube.isTabletCompliant;
+  }
 
-  const validatedChallenges = await challengeRepository.findValidatedPrototype();
-
-  const tubesWithResponsiveStatus = tubes.map((tube) => {
-    const tubeChallenges = validatedChallenges.filter((challenge) => {
-      return challenge.skill.tubeId === tube.id;
-    });
-    tube.mobile = _areChallengesMobileResponsive(tubeChallenges);
-    tube.tablet = _areChallengesTabletResponsive(tubeChallenges);
-    return tube;
-  });
-
-  return { areas: areasWithCompetences, thematics, tubes: tubesWithResponsiveStatus };
+  return { areas: areasWithCompetences, thematics, tubes };
 };
-
-function _areChallengesMobileResponsive(challenges) {
-  return _areChallengesResponsive(challenges, 'Smartphone');
-}
-
-function _areChallengesTabletResponsive(challenges) {
-  return _areChallengesResponsive(challenges, 'Tablet');
-}
-
-function _areChallengesResponsive(challenges, type) {
-  return (
-    challenges.length > 0 &&
-    challenges.every((challenge) => {
-      return challenge.responsive?.includes(type);
-    })
-  );
-}
