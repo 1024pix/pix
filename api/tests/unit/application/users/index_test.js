e81669741df367ba533600aa7e224a849e0188f9
Matthias FERRAINA <matthias.ferraina@pix.fr>
2022-10-27 18:01:52 +0200

    feat(api): add route and acceptance test
    Co-authored-by: Clarisse Damon <clarisse.damon@pix.fr>
    Co-authored-by: Vincent Hardouin <vincent.hardouin@pix.fr>

--- api/tests/unit/application/users/index_test.js
+++ api/tests/unit/application/users/index_test.js
@@ -550,6 +550,44 @@ describe('Unit | Router | user-router', function () {
     });
   });
 
+  describe('GET /api/users/{id}/trainings', function () {
+    const method = 'GET';
+
+    it('should return 200', async function () {
+      // given
+      sinon.stub(userController, 'findPaginatedUserRecommendedTrainings').returns('ok');
+      const securityPreHandlersStub = sinon
+        .stub(securityPreHandlers, 'checkRequestedUserIsAuthenticatedUser')
+        .callsFake((request, h) => h.response(true));
+      const httpTestServer = new HttpTestServer();
+      await httpTestServer.register(moduleUnderTest);
+
+      const url = '/api/users/1/trainings';
+
+      // when
+      const result = await httpTestServer.request(method, url);
+
+      // then
+      expect(result.statusCode).to.equal(200);
+      expect(securityPreHandlersStub).to.have.been.called;
+    });
+
+    it('should return 400 when userId is not a number', async function () {
+      // given
+      const httpTestServer = new HttpTestServer();
+      await httpTestServer.register(moduleUnderTest);
+
+      const userId = 'wrongId';
+      const url = `/api/users/${userId}/trainings`;
+
+      // when
+      const result = await httpTestServer.request(method, url);
+
+      // then
+      expect(result.statusCode).to.equal(400);
+    });
+  });
+
   context('Routes /admin', function () {
     describe('GET /api/admin/users', function () {
       it('should return an HTTP status code 200', async function () {
