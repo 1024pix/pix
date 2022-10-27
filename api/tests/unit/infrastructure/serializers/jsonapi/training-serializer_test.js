ec45c04697fe9ef65101a8398be09bf1d9945e1c
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-10-27 16:15:13 +0200

    feat(api): add deserialize training method

--- api/tests/unit/infrastructure/serializers/jsonapi/training-serializer_test.js
+++ api/tests/unit/infrastructure/serializers/jsonapi/training-serializer_test.js
@@ -30,4 +30,34 @@ describe('Unit | Serializer | JSONAPI | training-serializer', function () {
       expect(json).to.deep.equal(expectedSerializedTraining);
     });
   });
+
+  describe('#deserialize', function () {
+    it('should convert JSON API data to Training object', async function () {
+      // given
+      const jsonTraining = {
+        data: {
+          type: 'training',
+          attributes: {
+            title: 'title',
+            link: 'https://example.net',
+            duration: '6h',
+            type: 'webinaire',
+            locale: 'fr-fr',
+          },
+        },
+      };
+
+      // when
+      const training = await serializer.deserialize(jsonTraining);
+
+      // then
+      expect(training).to.deep.equal({
+        title: 'title',
+        link: 'https://example.net',
+        locale: 'fr-fr',
+        duration: '6h',
+        type: 'webinaire',
+      });
+    });
+  });
 });
