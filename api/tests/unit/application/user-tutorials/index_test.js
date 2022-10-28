acdf946542081869a9e1a20b03a8bf7c2dd3b7d2
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-10-28 16:03:04 +0200

    feat(api): add GET /users/{userId}/tutorials endpoint

--- api/tests/unit/application/user-tutorials/index_test.js
+++ api/tests/unit/application/user-tutorials/index_test.js
@@ -1,8 +1,33 @@
 const { expect, HttpTestServer, sinon } = require('../../../test-helper');
+const securityPreHandlers = require('../../../../lib/application/security-pre-handlers');
 const userTutorialsController = require('../../../../lib/application/user-tutorials/user-tutorials-controller');
 const moduleUnderTest = require('../../../../lib/application/user-tutorials');
 
 describe('Unit | Router | user-tutorials-router', function () {
+  describe('GET /api/users/{userId}/tutorials', function () {
+    it('should verify if requested user is authenticated user', async function () {
+      // given
+      sinon
+        .stub(securityPreHandlers, 'checkRequestedUserIsAuthenticatedUser')
+        .callsFake((request, h) => h.response(true));
+      sinon.stub(userTutorialsController, 'find').callsFake((request, h) => h.response('ok').code(200));
+
+      const httpTestServer = new HttpTestServer();
+      await httpTestServer.register(moduleUnderTest);
+
+      const method = 'GET';
+      const url = '/api/users/123/tutorials';
+
+      // when
+      const response = await httpTestServer.request(method, url);
+
+      // then
+      expect(securityPreHandlers.checkRequestedUserIsAuthenticatedUser).have.been.called;
+      expect(userTutorialsController.find).have.been.called;
+      expect(response.statusCode).to.equal(200);
+    });
+  });
+
   describe('PUT /api/users/tutorials/{tutorialId}', function () {
     it('should exist', async function () {
       // given
