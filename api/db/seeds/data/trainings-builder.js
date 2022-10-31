bd945b6278b4da3c76f1b742b8cf0c11a7cc17fe
Matthias FERRAINA <matthias.ferraina@pix.fr>
2022-10-31 15:47:14 +0100

    feat(api): add more training seeds

--- api/db/seeds/data/trainings-builder.js
+++ api/db/seeds/data/trainings-builder.js
@@ -36,6 +36,41 @@ function trainingBuilder({ databaseBuilder }) {
     duration: '06:00:00',
     locale: 'fr-fr',
   });
+  const training6 = databaseBuilder.factory.buildTraining({
+    title: 'Apprendre à jouer à la coinche',
+    link: 'http://www.example6.net',
+    type: 'autoformation',
+    duration: '50:00:50',
+    locale: 'fr-fr',
+  });
+  const training7 = databaseBuilder.factory.buildTraining({
+    title: 'Savoir coudre',
+    link: 'http://www.example7.net',
+    type: 'webinaire',
+    duration: '00:05:00',
+    locale: 'fr-fr',
+  });
+  const training8 = databaseBuilder.factory.buildTraining({
+    title: 'Apprendre à faire du cidre breton',
+    link: 'http://www.example8.net',
+    type: 'autoformation',
+    duration: '01:00:00',
+    locale: 'fr-fr',
+  });
+  const training9 = databaseBuilder.factory.buildTraining({
+    title: 'Apprendre à compter',
+    link: 'http://www.example9.net',
+    type: 'webinaire',
+    duration: '10:00:00',
+    locale: 'fr-fr',
+  });
+  const training10 = databaseBuilder.factory.buildTraining({
+    title: 'Devenir influenceur de bonheur',
+    link: 'http://www.example10.net',
+    type: 'autoformation',
+    duration: '10:00:00',
+    locale: 'fr-fr',
+  });
   databaseBuilder.factory.buildTargetProfileTraining({
     trainingId: training1.id,
     targetProfileId: TARGET_PROFILE_PIX_EDU_FORMATION_INITIALE_2ND_DEGRE,
@@ -54,6 +89,26 @@ function trainingBuilder({ databaseBuilder }) {
   });
   databaseBuilder.factory.buildTargetProfileTraining({
     trainingId: training5.id,
+    targetProfileId: TARGET_PROFILE_PIX_EDU_FORMATION_INITIALE_2ND_DEGRE,
+  });
+  databaseBuilder.factory.buildTargetProfileTraining({
+    trainingId: training6.id,
+    targetProfileId: TARGET_PROFILE_PIX_EDU_FORMATION_INITIALE_2ND_DEGRE,
+  });
+  databaseBuilder.factory.buildTargetProfileTraining({
+    trainingId: training7.id,
+    targetProfileId: TARGET_PROFILE_PIX_EDU_FORMATION_INITIALE_2ND_DEGRE,
+  });
+  databaseBuilder.factory.buildTargetProfileTraining({
+    trainingId: training8.id,
+    targetProfileId: TARGET_PROFILE_PIX_EDU_FORMATION_INITIALE_2ND_DEGRE,
+  });
+  databaseBuilder.factory.buildTargetProfileTraining({
+    trainingId: training9.id,
+    targetProfileId: TARGET_PROFILE_PIX_EDU_FORMATION_INITIALE_2ND_DEGRE,
+  });
+  databaseBuilder.factory.buildTargetProfileTraining({
+    trainingId: training10.id,
     targetProfileId: TARGET_PROFILE_PIX_EDU_FORMATION_INITIALE_2ND_DEGRE,
   });
 }
