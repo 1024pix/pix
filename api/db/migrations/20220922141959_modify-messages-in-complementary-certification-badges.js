ad51848b02d46c7448f5e988f8de205d930e1e11
Antoine Ceol <antoine.ceol@gmail.com>
2022-11-02 00:07:00 +0100

    :recycle: api: Remove complementary badge keys from domain

--- api/db/migrations/20220922141959_modify-messages-in-complementary-certification-badges.js
+++ api/db/migrations/20220922141959_modify-messages-in-complementary-certification-badges.js
@@ -10,7 +10,7 @@ const {
   PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_CONFIRME,
   PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_AVANCE,
   PIX_EDU_FORMATION_CONTINUE_1ER_DEGRE_EXPERT,
-} = require('../../lib/domain/models/Badge').keys;
+} = require('../constants').badges.keys;
 /**
  * @param { import('knex').Knex } knex
  * @returns { Promise<void> }
