f7c3696de9e89c524786ccbc3b3da1c25dd26168
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2022-11-03 17:15:19 +0100

    api: add POST /api/admin/certification-centers/{certificationCenterId}/invitations route

--- api/tests/acceptance/application/certification-centers/index_test.js
+++ api/tests/acceptance/application/certification-centers/index_test.js
@@ -4,10 +4,11 @@ const {
   generateValidRequestAuthorizationHeader,
   insertUserWithRoleSuperAdmin,
   knex,
+  sinon,
 } = require('../../../test-helper');
 const createServer = require('../../../../server');
 
-describe('Acceptance | API | Certification Centers', function () {
+describe('Acceptance | Route | Certification Centers', function () {
   let server;
 
   beforeEach(async function () {
@@ -17,6 +18,7 @@ describe('Acceptance | API | Certification Centers', function () {
   afterEach(async function () {
     await knex('complementary-certification-habilitations').delete();
     await knex('data-protection-officers').delete();
+    await knex('certification-center-invitations').delete();
   });
 
   describe('PATCH /api/admin/certification-centers/{id}', function () {
@@ -55,6 +57,55 @@ describe('Acceptance | API | Certification Centers', function () {
         expect(result.data.attributes['data-protection-officer-last-name']).to.equal('Ptipeu');
         expect(result.data.attributes['data-protection-officer-email']).to.equal('justin.ptipeu@example.net');
         expect(result.data.attributes.name).to.equal('Justin Ptipeu Orga');
+      });
+    });
+  });
+
+  describe('POST /api/admin/certification-centers/{certificationCenterId}/invitations', function () {
+    let clock;
+    const now = new Date('2021-05-01');
+
+    beforeEach(async function () {
+      clock = sinon.useFakeTimers({
+        now,
+        toFake: ['Date'],
+      });
+    });
+
+    afterEach(async function () {
+      clock.restore();
+    });
+
+    it('should return 201 HTTP status code with created invitation', async function () {
+      // given
+      const adminMember = await insertUserWithRoleSuperAdmin();
+      const certificationCenterId = databaseBuilder.factory.buildCertificationCenter().id;
+      await databaseBuilder.commit();
+
+      // when
+      const { result, statusCode } = await server.inject({
+        headers: {
+          authorization: generateValidRequestAuthorizationHeader(adminMember.id),
+        },
+        method: 'POST',
+        payload: {
+          data: {
+            attributes: {
+              email: 'some.user@example.net',
+              lang: 'fr-fr',
+            },
+          },
+        },
+        url: `/api/admin/certification-centers/${certificationCenterId}/invitations`,
+      });
+
+      // then
+      expect(statusCode).to.equal(201);
+      expect(result.data.type).to.equal('certification-center-invitations');
+      expect(result.data).to.have.property('id');
+      expect(result.data.attributes).to.deep.equal({
+        'updated-at': now,
+        email: 'some.user@example.net',
       });
     });
   });
