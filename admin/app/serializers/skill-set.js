ee635ef7c612172e7c81fb2b4eaff1137c34c9eb
Laura Bergoens <laura.bergoens@pix.fr>
2022-11-04 16:22:19 +0100

    remove unused code in admin
     Co-authored-by: Lucas Herrador <lucas.herrador@pix.fr>
     Co-authored-by: Nicolas Lepage <nicolas.lepage@pix.fr>

--- admin/app/serializers/skill-set.js
+++
@@ -1,9 +0,0 @@
-import JSONAPISerializer from '@ember-data/serializer/json-api';
-
-export default class SkillSetSerializer extends JSONAPISerializer {
-  attrs = {
-    badge: {
-      serialize: false,
-    },
-  };
-}
