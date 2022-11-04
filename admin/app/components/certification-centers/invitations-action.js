21f050d309f1ef2301d7475421e63f722e987449
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2022-11-04 10:59:01 +0100

    admin: new invitations-action component

---
+++ admin/app/components/certification-centers/invitations-action.js
@@ -0,0 +1,29 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class CertificationCenterInvitationsAction extends Component {
+  @tracked invitationLanguage = this.languagesOptions[0].value;
+
+  get languagesOptions() {
+    return [
+      {
+        label: 'Français',
+        value: 'fr-fr',
+      },
+      {
+        label: 'Francophone',
+        value: 'fr',
+      },
+      {
+        label: 'Anglais',
+        value: 'en',
+      },
+    ];
+  }
+
+  @action
+  changeInvitationLanguage(event) {
+    this.invitationLanguage = event.target.value;
+  }
+}
