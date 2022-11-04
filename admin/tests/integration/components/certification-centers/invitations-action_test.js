dfece1a4291f7dff622a25d8702e27325e084f8f
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2022-11-04 10:59:01 +0100

    admin: new invitations-action component

---
+++ admin/tests/integration/components/certification-centers/invitations-action_test.js
@@ -0,0 +1,47 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, selectByLabelAndOption } from '@1024pix/ember-testing-library';
+import hbs from 'htmlbars-inline-precompile';
+import sinon from 'sinon';
+import { clickByText } from '@1024pix/ember-testing-library';
+
+module('Integration | Component | certification-center-invitations-action', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it should create certification-center invitation with default language', async function (assert) {
+    // given
+    const createInvitationStub = sinon.stub();
+    this.set('createInvitation', createInvitationStub);
+    this.set('noop', () => {});
+
+    // when
+    await render(
+      hbs`<CertificationCenters::InvitationsAction
+        @createInvitation={{createInvitation}}
+        @onChangeUserEmailToInvite={{noop}}/>`
+    );
+    await clickByText('Inviter');
+
+    // then
+    assert.ok(createInvitationStub.calledWith('fr-fr'));
+  });
+
+  test('it should create certification-center invitation with choosen language', async function (assert) {
+    // given
+    const createInvitationStub = sinon.stub();
+    this.set('createInvitation', createInvitationStub);
+    this.set('noop', () => {});
+
+    // when
+    await render(
+      hbs`<CertificationCenters::InvitationsAction
+        @createInvitation={{createInvitation}}
+        @onChangeUserEmailToInvite={{noop}}/>`
+    );
+    await selectByLabelAndOption('Choisir la langue de l’email d’invitation', 'en');
+    await clickByText('Inviter');
+
+    // then
+    assert.ok(createInvitationStub.calledWith('en'));
+  });
+});
