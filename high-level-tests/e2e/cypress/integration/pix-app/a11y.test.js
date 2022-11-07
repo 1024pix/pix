5b60ddd160157fdfaca7f07c8b9483075959c192
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2022-11-07 16:02:23 +0100

    feat(mon-pix): add error page to a11y e2e tests

--- high-level-tests/e2e/cypress/integration/pix-app/a11y.test.js
+++ high-level-tests/e2e/cypress/integration/pix-app/a11y.test.js
@@ -62,7 +62,8 @@ describe('a11y', () => {
       { url: '/mon-compte/informations-personnelles' },
       { url: '/mon-compte/langue' },
       { url: '/mon-compte/methodes-de-connexion' },
-      { url: '/plan-du-site'},
+      { url: '/plan-du-site' },
+      { url: '/assessments/fake-assessment', skipFailures: true },
     ];
 
     authenticatedPages.forEach(({ url, skipFailures = false }) => {
