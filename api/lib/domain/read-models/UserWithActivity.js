8dacc1eb73f89c458abd552e1c6be74d789447ad
Matthias FERRAINA <matthias.ferraina@pix.fr>
2022-11-07 16:49:29 +0100

    feat(api): update get-current-user usecase to retrieve hasRecommendedTrainings boolean
    Co-authored-by: Geoffroy Begouaussel <geoffroy.begouaussel@zenika.com>
    Co-authored-by: Rousseau Corentin <cro@eonix.be>

--- api/lib/domain/read-models/UserWithActivity.js
+++ api/lib/domain/read-models/UserWithActivity.js
@@ -1,7 +1,8 @@
 class UserWithActivity {
-  constructor({ user, hasAssessmentParticipations, codeForLastProfileToShare }) {
+  constructor({ user, hasAssessmentParticipations, codeForLastProfileToShare, hasRecommendedTrainings }) {
     this.hasAssessmentParticipations = hasAssessmentParticipations;
     this.codeForLastProfileToShare = codeForLastProfileToShare;
+    this.hasRecommendedTrainings = hasRecommendedTrainings;
     Object.assign(this, user);
   }
 }
--- api/tests/unit/domain/usecases/get-current-user_test.js
+++ api/tests/unit/domain/usecases/get-current-user_test.js
@@ -4,12 +4,16 @@ const getCurrentUser = require('../../../../lib/domain/usecases/get-current-user
 describe('Unit | UseCase | get-current-user', function () {
   let userRepository;
   let campaignParticipationRepository;
+  let userRecommendedTrainingRepository;
 
   beforeEach(function () {
     userRepository = { get: sinon.stub() };
     campaignParticipationRepository = {
       hasAssessmentParticipations: sinon.stub(),
       getCodeOfLastParticipationToProfilesCollectionCampaignForUser: sinon.stub(),
+    };
+    userRecommendedTrainingRepository = {
+      hasRecommendedTrainings: sinon.stub(),
     };
   });
 
@@ -20,15 +24,22 @@ describe('Unit | UseCase | get-current-user', function () {
     campaignParticipationRepository.getCodeOfLastParticipationToProfilesCollectionCampaignForUser
       .withArgs(1)
       .resolves('SOMECODE');
+    userRecommendedTrainingRepository.hasRecommendedTrainings.withArgs(1).resolves(false);
 
     // when
     const result = await getCurrentUser({
       authenticatedUserId: 1,
       userRepository,
       campaignParticipationRepository,
+      userRecommendedTrainingRepository,
     });
 
     // then
-    expect(result).to.deep.equal({ id: 1, hasAssessmentParticipations: false, codeForLastProfileToShare: 'SOMECODE' });
+    expect(result).to.deep.equal({
+      id: 1,
+      hasAssessmentParticipations: false,
+      codeForLastProfileToShare: 'SOMECODE',
+      hasRecommendedTrainings: false,
+    });
   });
 });
