56b4df21f56cb668d9b592fda2e0eed18d686d1f
Matthias FERRAINA <matthias.ferraina@pix.fr>
2022-11-07 16:50:28 +0100

    feat(api): add hasRecommendedTrainings boolean to serializer
    Co-authored-by: Geoffroy Begouaussel <geoffroy.begouaussel@zenika.com>
    Co-authored-by: Rousseau Corentin <cro@eonix.be>

--- api/lib/infrastructure/serializers/jsonapi/user-with-activity-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/user-with-activity-serializer.js
@@ -26,6 +26,7 @@ module.exports = {
         'hasSeenFocusedChallengeTooltip',
         'hasSeenOtherChallengesTooltip',
         'hasAssessmentParticipations',
+        'hasRecommendedTrainings',
         'codeForLastProfileToShare',
         'trainings',
       ],
