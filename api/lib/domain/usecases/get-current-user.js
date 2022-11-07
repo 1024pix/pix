1ab460f0f8a8eb57374c1163a7f88551d744a9cb
Matthias FERRAINA <matthias.ferraina@pix.fr>
2022-11-07 16:49:29 +0100

    feat(api): update get-current-user usecase to retrieve hasRecommendedTrainings boolean
    Co-authored-by: Geoffroy Begouaussel <geoffroy.begouaussel@zenika.com>
    Co-authored-by: Rousseau Corentin <cro@eonix.be>

--- api/lib/domain/usecases/get-current-user.js
+++ api/lib/domain/usecases/get-current-user.js
@@ -4,15 +4,18 @@ module.exports = async function getCurrentUser({
   authenticatedUserId,
   userRepository,
   campaignParticipationRepository,
+  userRecommendedTrainingRepository,
 }) {
-  const [hasAssessmentParticipations, codeForLastProfileToShare] = await Promise.all([
+  const [hasAssessmentParticipations, codeForLastProfileToShare, hasRecommendedTrainings] = await Promise.all([
     campaignParticipationRepository.hasAssessmentParticipations(authenticatedUserId),
     campaignParticipationRepository.getCodeOfLastParticipationToProfilesCollectionCampaignForUser(authenticatedUserId),
+    userRecommendedTrainingRepository.hasRecommendedTrainings(authenticatedUserId),
   ]);
   const user = await userRepository.get(authenticatedUserId);
   return new UserWithActivity({
     user,
     hasAssessmentParticipations,
     codeForLastProfileToShare,
+    hasRecommendedTrainings,
   });
 };
