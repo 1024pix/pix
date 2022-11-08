4eec1a1684b9803a0174c495ade801dc3dd821e3
Anne-G <ang.schall@gmail.com>
2022-11-08 10:33:31 +0100

    api: refacto usecase to make only one return

--- api/lib/domain/usecases/create-or-update-certification-center-invitation-for-admin.js
+++ api/lib/domain/usecases/create-or-update-certification-center-invitation-for-admin.js
@@ -1,21 +1,25 @@
 const CertificationCenterInvitation = require('../models/CertificationCenterInvitation');
 
 module.exports = async function ({ email, certificationCenterId, certificationCenterInvitationRepository }) {
+  let certificationCenterInvitation, isInvitationCreated;
+
   const alreadyExistingPendingInvitationForThisEmail =
     await certificationCenterInvitationRepository.findOnePendingByEmailAndCertificationCenterId({
       email,
       certificationCenterId,
     });
-
   const shouldCreateInvitation = !alreadyExistingPendingInvitationForThisEmail;
+
   if (shouldCreateInvitation) {
     const newInvitation = CertificationCenterInvitation.create({ email, certificationCenterId });
-    const certificationCenterInvitationCreated = await certificationCenterInvitationRepository.create(newInvitation);
-    return { isInvitationCreated: true, certificationCenterInvitation: certificationCenterInvitationCreated };
+    certificationCenterInvitation = await certificationCenterInvitationRepository.create(newInvitation);
+    isInvitationCreated = true;
+  } else {
+    certificationCenterInvitation = await certificationCenterInvitationRepository.update(
+      alreadyExistingPendingInvitationForThisEmail
+    );
+    isInvitationCreated = false;
   }
 
-  const updatedCertificationCenterInvitation = await certificationCenterInvitationRepository.update(
-    alreadyExistingPendingInvitationForThisEmail
-  );
-  return { isInvitationCreated: false, certificationCenterInvitation: updatedCertificationCenterInvitation };
+  return { isInvitationCreated, certificationCenterInvitation };
 };
