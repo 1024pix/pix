c0c057f091a631a1f74ad485adb91b9354d2e945
Anne-Marie Esteves <anne-marie.esteves@octo.com>
2022-11-09 17:51:56 +0100

    feat(certif): accept invitation and handle error responses

--- certif/app/components/auth/login-form.js
+++ certif/app/components/auth/login-form.js
@@ -8,6 +8,8 @@ import isEmailValid from '../../utils/email-validator';
 export default class LoginForm extends Component {
   @service intl;
   @service url;
+  @service store;
+  @service session;
 
   @tracked errorMessage = null;
   @tracked isErrorMessagePresent = false;
@@ -17,16 +19,50 @@ export default class LoginForm extends Component {
   @tracked passwordValidationMessage = null;
   @tracked emailValidationMessage = null;
 
+  ERROR_MESSAGES = {
+    DEFAULT: this.intl.t('common.api-errors-messages.default'),
+    STATUS_400: this.intl.t('common.api-errors-messages.bad-request'),
+    STATUS_401: this.intl.t('pages.login-or-register.login-form.errors.status.401'),
+    STATUS_403: this.intl.t('pages.login-or-register.login-form.errors.status.403'),
+    STATUS_404: this.intl.t('pages.login-or-register.login-form.errors.status.404'),
+    STATUS_409: this.intl.t('pages.login-or-register.login-form.errors.status.409'),
+    STATUS_412: this.intl.t('pages.login-or-register.login-form.errors.status.412'),
+    STATUS_429: this.intl.t('pages.login-or-register.login-form.errors.status.429'),
+  };
+
   @action
   async authenticate(event) {
     event.preventDefault();
     this.isLoading = true;
 
+    const email = this.email;
+    const password = this.password;
+
     if (!this.isFormValid) {
       this.isLoading = false;
       return;
     }
-    // TODO
+
+    if (this.args.isWithInvitation) {
+      try {
+        await this.args.certificationCenterInvitation.accept({
+          id: this.args.certificationCenterInvitationId,
+          code: this.args.certificationCenterInvitationCode,
+          email,
+        });
+        await this._authenticate(password, email);
+      } catch (errorResponse) {
+        this.errorMessage = this._handleResponseError(errorResponse);
+        this.isErrorMessagePresent = true;
+      } finally {
+        this.isLoading = false;
+      }
+    }
+  }
+
+  async _authenticate(password, email) {
+    const scope = 'pix-certif';
+    await this.session.authenticate('authenticator:oauth2', email, password, scope);
   }
 
   @action
@@ -63,5 +99,32 @@ export default class LoginForm extends Component {
 
   get forgottenPasswordUrl() {
     return this.url.forgottenPasswordUrl;
+  }
+
+  _handleResponseError(errorResponse) {
+    if (Array.isArray(errorResponse?.errors)) {
+      const error = errorResponse?.errors[0];
+
+      switch (error.status) {
+        case '400':
+          return this.ERROR_MESSAGES.STATUS_400;
+        case '401':
+          return this.ERROR_MESSAGES.STATUS_401;
+        case '403':
+          return this.ERROR_MESSAGES.STATUS_403;
+        case '404':
+          return this.ERROR_MESSAGES.STATUS_404;
+        case '409':
+          return this.ERROR_MESSAGES.STATUS_409;
+        case '412':
+          return this.ERROR_MESSAGES.STATUS_412;
+        case '429':
+          return this.ERROR_MESSAGES.STATUS_429;
+        default:
+          return this.ERROR_MESSAGES.DEFAULT;
+      }
+    } else {
+      return this.ERROR_MESSAGES.DEFAULT;
+    }
   }
 }
