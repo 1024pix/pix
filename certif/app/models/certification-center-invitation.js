2f8d198b1e4e5da0ed54218b10f497bb1234b927
Anne-Marie Esteves <anne-marie.esteves@octo.com>
2022-11-09 14:48:50 +0100

    feat(certif): add member action to accept certification center invitation

--- certif/app/models/certification-center-invitation.js
+++ certif/app/models/certification-center-invitation.js
@@ -1,5 +1,26 @@
 import Model, { attr } from '@ember-data/model';
+import { memberAction } from 'ember-api-actions';
 
-export default class CertificationCenterInvitationModel extends Model {
-  @attr() certificationCenterName;
+export default class CertificationCenterInvitation extends Model {
+  @attr('string') email;
+  @attr('string') status;
+  @attr('string') certificationCenterName;
+
+  accept = memberAction({
+    path: 'accept',
+    type: 'post',
+    urlType: 'saveRecord',
+    before({ id, code, email }) {
+      return {
+        data: {
+          id,
+          type: 'certification-center-invitations-accept',
+          attributes: {
+            code,
+            email,
+          },
+        },
+      };
+    },
+  });
 }
