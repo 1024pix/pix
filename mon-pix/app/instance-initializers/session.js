03bb28186c80a3ae8cc510042880f496c22c421f
Ismael Gorissen <ismael.gorissen@gmail.com>
2022-11-15 14:35:55 +0100

    feat(mon-pix): handle authentication with externalUser
    Co-authored-by: Clément Latzarus <clement.latzarus@pix.fr>

--- mon-pix/app/instance-initializers/session.js
+++ mon-pix/app/instance-initializers/session.js
@@ -1,22 +1,18 @@
 import PixWindow from 'mon-pix/utils/pix-window';
 
-function _removeCurrentSessionFromLocalStorage() {
-  window.localStorage.removeItem('ember_simple_auth-session');
-}
-
 export function initialize(/* applicationInstance */) {
   const currentURL = PixWindow.getLocationHref();
 
-  const isGarAuthenticationURL = currentURL.match(/\/connexion\/gar/i)?.length === 1;
-  if (isGarAuthenticationURL) {
-    _removeCurrentSessionFromLocalStorage();
-    return;
-  }
+  const isGarAuthenticationURL = currentURL.includes('/connexion/gar');
+  const isAuthenticatedExternalUser = currentURL.includes('externalUser=');
 
-  const isAuthenticatedExternalUser = currentURL.match(/externalUser=/i)?.length === 1;
-  if (isAuthenticatedExternalUser) {
+  if (isGarAuthenticationURL || isAuthenticatedExternalUser) {
     _removeCurrentSessionFromLocalStorage();
   }
+}
+
+function _removeCurrentSessionFromLocalStorage() {
+  window.localStorage.removeItem('ember_simple_auth-session');
 }
 
 export default {
