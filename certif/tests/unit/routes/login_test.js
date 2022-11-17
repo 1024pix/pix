119c51552c0004b10996e155e4041f2668aac57f
Ismael Gorissen <ismael.gorissen@gmail.com>
2022-11-17 16:46:09 +0100

    feat(certif): override setup controller method

--- certif/tests/unit/routes/login_test.js
+++ certif/tests/unit/routes/login_test.js
@@ -1,5 +1,6 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import sinon from 'sinon';
 
 module('Unit | Route | login', function (hooks) {
   setupTest(hooks);
@@ -7,5 +8,35 @@ module('Unit | Route | login', function (hooks) {
   test('it exists', function (assert) {
     const route = this.owner.lookup('route:login');
     assert.ok(route);
+  });
+
+  module('#setupController', function () {
+    test('it should set property "isInvitationCancelled" to true', function (assert) {
+      // given
+      const route = this.owner.lookup('route:login');
+      const controller = { set: sinon.stub() };
+      const model = {};
+      const transition = { data: { isInvitationCancelled: true } };
+
+      // when
+      route.setupController(controller, model, transition);
+
+      // then
+      assert.ok(controller.set.calledWith('isInvitationCancelled', true));
+    });
+
+    test('it should set property "hasInvitationAlreadyBeenAccepted" to true', function (assert) {
+      // given
+      const route = this.owner.lookup('route:login');
+      const controller = { set: sinon.stub() };
+      const model = {};
+      const transition = { data: { hasInvitationAlreadyBeenAccepted: true } };
+
+      // when
+      route.setupController(controller, model, transition);
+
+      // then
+      assert.ok(controller.set.calledWith('hasInvitationAlreadyBeenAccepted', true));
+    });
   });
 });
