d3a86504fa13868d1506b18ecb3866d73e35310b
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-11-21 14:06:40 +0100

    sr(mon-pix): fix hasAttributes

--- mon-pix/tests/acceptance/challenge-item-qrocm_test.js
+++ mon-pix/tests/acceptance/challenge-item-qrocm_test.js
@@ -167,7 +167,7 @@ describe('Acceptance | Displaying a QROCM challenge', function () {
 
       it('should set the select with previous answer and propose to continue', async function () {
         // then
-        expect(findAll('select[data-test="challenge-response-proposal-selector"] option')[1].hasAttribute('selected'));
+        expect(findAll('select[data-test="challenge-response-proposal-selector"] option')[1].selected);
 
         expect(find('.challenge-actions__action-continue')).to.exist;
         expect(find('.challenge-actions__action-validate')).to.not.exist;
