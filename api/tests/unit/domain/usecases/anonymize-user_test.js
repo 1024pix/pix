94a0a53858372987069f5953ed41b023cfce2fbe
Ismael Gorissen <ismael.gorissen@gmail.com>
2022-11-22 18:06:49 +0100

    sr(api): combine same tests

--- api/tests/unit/domain/usecases/anonymize-user_test.js
+++ api/tests/unit/domain/usecases/anonymize-user_test.js
@@ -2,7 +2,7 @@ const { expect, sinon } = require('../../../test-helper');
 const anonymizeUser = require('../../../../lib/domain/usecases/anonymize-user');
 
 describe('Unit | UseCase | anonymize-user', function () {
-  it('should anonymize user and delete all authentication methods', async function () {
+  it("should delete all authentication methods, revoke all user's refresh tokens and anonymize user", async function () {
     // given
     const userId = 1;
     const expectedAnonymizedUser = {
@@ -23,24 +23,10 @@ describe('Unit | UseCase | anonymize-user', function () {
     expect(authenticationMethodRepository.removeAllAuthenticationMethodsByUserId).to.have.been.calledWithExactly({
       userId,
     });
-
+    expect(refreshTokenService.revokeRefreshTokensForUserId).to.have.been.calledWithExactly({ userId });
     expect(userRepository.updateUserDetailsForAdministration).to.have.been.calledWithExactly(
       userId,
       expectedAnonymizedUser
     );
-  });
-
-  it("should revoke all user's refresh tokens", async function () {
-    // given
-    const userId = 1;
-    const userRepository = { updateUserDetailsForAdministration: sinon.stub() };
-    const authenticationMethodRepository = { removeAllAuthenticationMethodsByUserId: sinon.stub() };
-    const refreshTokenService = { revokeRefreshTokensForUserId: sinon.stub() };
-
-    // when
-    await anonymizeUser({ userId, userRepository, authenticationMethodRepository, refreshTokenService });
-
-    // then
-    expect(refreshTokenService.revokeRefreshTokensForUserId).to.have.been.calledWithExactly({ userId });
   });
 });
