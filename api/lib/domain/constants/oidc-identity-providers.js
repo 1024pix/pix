3f2dabf80dcb936e26aa3e791cde04358a852285
Ismael Gorissen <ismael.gorissen@gmail.com>
2022-11-23 16:57:03 +0100

    feat(api): add FWB authentication service as constant

--- api/lib/domain/constants/oidc-identity-providers.js
+++ api/lib/domain/constants/oidc-identity-providers.js
@@ -1,7 +1,9 @@
 const PoleEmploiOidcAuthenticationService = require('../services/authentication/pole-emploi-oidc-authentication-service');
 const CnavOidcAuthenticationService = require('../services/authentication/cnav-oidc-authentication-service');
+const FwbOidcAuthenticationService = require('../services/authentication/fwb-oidc-authentication-service');
 
 module.exports = {
   POLE_EMPLOI: new PoleEmploiOidcAuthenticationService(),
   CNAV: new CnavOidcAuthenticationService(),
+  FWB: new FwbOidcAuthenticationService(),
 };
