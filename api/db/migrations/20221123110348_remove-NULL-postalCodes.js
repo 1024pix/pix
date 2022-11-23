4b5e6c07275e6adf04febc73e40f8b21121ed332
Antoine Ceol <antoine.ceol@gmail.com>
2022-11-23 12:56:44 +0100

    :fire: api: Remove specific code for incorrect data
    Du to a import issue, we had a few 'NULL' values which should never have existed.
    No need to keep specific code for that. Let's remove thos values in DB

---
+++ api/db/migrations/20221123110348_remove-NULL-postalCodes.js
@@ -0,0 +1,6 @@
+exports.up = async function (knex) {
+  await knex('certification-candidates').where({ birthPostalCode: 'NULL' }).update({ birthPostalCode: null });
+  await knex('certification-candidates').where({ birthINSEECode: 'NULL' }).update({ birthINSEECode: null });
+};
+
+exports.down = function () {};
