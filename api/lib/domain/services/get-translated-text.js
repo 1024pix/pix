c4d97c30e76e36b405ba2820bbae7c7093ab34d0
François de Metz <francois.de-metz@pix.fr>
2022-11-23 17:58:58 +0100

    Cleanup get-translated-text service

--- api/lib/domain/services/get-translated-text.js
+++ api/lib/domain/services/get-translated-text.js
@@ -1,18 +1,7 @@
-const { ENGLISH_SPOKEN } = require('../../domain/constants').LOCALE;
-
-function getTranslatedText(locale, translations = { frenchText: '', englishText: '' }) {
-  if (locale === ENGLISH_SPOKEN) {
-    return translations.englishText;
-  }
-
-  return translations.frenchText;
-}
+const { FRENCH_SPOKEN } = require('../../domain/constants').LOCALE;
 
 function getTranslatedKey(key, locale) {
-  if (key && key[locale]) {
-    return key[locale];
-  }
-  return key?.fr;
+  return key?.[locale] || key?.[FRENCH_SPOKEN];
 }
 
-module.exports = { getTranslatedText, getTranslatedKey };
+module.exports = { getTranslatedKey };
