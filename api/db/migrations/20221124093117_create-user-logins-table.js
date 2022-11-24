9d56372594687872d31001e44d8c5895850a32e1
Ismael Gorissen <ismael.gorissen@gmail.com>
2022-11-24 10:49:36 +0100

    feat(api): add script to create user-logins table

---
+++ api/db/migrations/20221124093117_create-user-logins-table.js
@@ -0,0 +1,17 @@
+const TABLE_NAME = 'user-logins';
+
+exports.up = (knex) => {
+  return knex.schema.createTable(TABLE_NAME, (t) => {
+    t.bigIncrements().primary();
+    t.bigInteger('userId').references('users.id').notNullable().unique();
+    t.dateTime('createdAt').notNullable().defaultTo(knex.fn.now());
+    t.dateTime('updatedAt').notNullable().defaultTo(knex.fn.now());
+    t.dateTime('blockedAt').nullable();
+    t.dateTime('temporaryBlockedUntil').nullable();
+    t.integer('failureCount').notNullable().defaultTo(0);
+  });
+};
+
+exports.down = (knex) => {
+  return knex.schema.dropTable(TABLE_NAME);
+};
