4a4275ca7ac624696dbe3dc3ae3776b9c45b1176
Andreia Pena <andreia.pena@pix.fr>
2022-11-24 09:41:12 +0100

    sr(mon-pix): user-logged-menu tests : use testing-library

--- mon-pix/tests/integration/components/user-logged-menu_test.js
+++ mon-pix/tests/integration/components/user-logged-menu_test.js
@@ -1,7 +1,7 @@
 import Service from '@ember/service';
 import { module, test } from 'qunit';
 import setupIntlRenderingTest from '../../helpers/setup-intl-rendering';
-import { click, find, triggerKeyEvent } from '@ember/test-helpers';
+import { click, triggerKeyEvent } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { render } from '@1024pix/ember-testing-library';
 
@@ -14,7 +14,6 @@ module('Integration | Component | user logged menu', function (hooks) {
       class currentUserService extends Service {
         user = {
           firstName: 'Hermione',
-          email: 'hermione.granger@hogwarts.com',
           fullName: 'Hermione Granger',
         };
       }
@@ -22,101 +21,155 @@ module('Integration | Component | user logged menu', function (hooks) {
       this.owner.register('service:currentUser', currentUserService);
     });
 
-    test('should render component', async function (assert) {
-      // when
-      await render(hbs`<UserLoggedMenu/>`);
-
-      // then
-      assert.dom('.logged-user-details').exists();
-    });
-
     test('should display logged user name with a11y guidance', async function (assert) {
       // when
-      await render(hbs`<UserLoggedMenu/>`);
+      const screen = await render(hbs`<UserLoggedMenu/>`);
 
       // then
-      const nodes = find('.logged-user-name__link').childNodes;
+      const buttonMenu = screen.getByRole('button', {
+        name: `Hermione ${this.intl.t('navigation.user-logged-menu.details')}`,
+      });
+      const nodes = buttonMenu.childNodes;
       const buttonTextContent = nodes[1].textContent;
       const a11yText = nodes[3].textContent;
 
-      assert.dom('.logged-user-name').exists();
-      assert.dom('.logged-user-name__link').exists();
-      // TODO: Fix this the next time the file is edited.
-      // eslint-disable-next-line qunit/no-assert-equal
-      assert.equal(a11yText, this.intl.t('navigation.user-logged-menu.details'));
-      // TODO: Fix this the next time the file is edited.
-      // eslint-disable-next-line qunit/no-assert-equal
-      assert.equal(buttonTextContent, 'Hermione');
+      assert.strictEqual(a11yText, this.intl.t('navigation.user-logged-menu.details'));
+      assert.strictEqual(buttonTextContent, 'Hermione');
     });
 
     test('should hide user menu, when no action on user-name', async function (assert) {
       // when
-      await render(hbs`<UserLoggedMenu/>`);
+      const screen = await render(hbs`<UserLoggedMenu/>`);
 
       // then
-      assert.dom('.logged-user-menu').doesNotExist();
+      assert
+        .dom(
+          screen.getByRole('button', {
+            name: `Hermione ${this.intl.t('navigation.user-logged-menu.details')}`,
+            expanded: false,
+          })
+        )
+        .exists();
+      assert.dom(screen.queryByRole('link', { name: this.intl.t('navigation.user.account') })).doesNotExist();
+      assert.dom(screen.queryByRole('link', { name: this.intl.t('navigation.user.certifications') })).doesNotExist();
+      assert.dom(screen.queryByRole('link', { name: this.intl.t('navigation.main.help') })).doesNotExist();
+      assert.dom(screen.queryByRole('link', { name: this.intl.t('navigation.user.sign-out') })).doesNotExist();
     });
 
     test('should display a user menu, when user-name is clicked', async function (assert) {
-      // given
-      const MENU_ITEMS_COUNT = 4;
-
       // when
       const screen = await render(hbs`<UserLoggedMenu/>`);
-      await click('.logged-user-name__link');
+      await click(
+        screen.getByRole('button', {
+          name: `Hermione ${this.intl.t('navigation.user-logged-menu.details')}`,
+        })
+      );
 
       // then
-      assert.dom('.logged-user-menu').exists();
-      assert.dom('.logged-user-menu__link').exists({ count: MENU_ITEMS_COUNT });
+      assert
+        .dom(
+          screen.getByRole('button', {
+            name: `Hermione ${this.intl.t('navigation.user-logged-menu.details')}`,
+            expanded: true,
+          })
+        )
+        .exists();
       assert.ok(screen.getByText('Hermione Granger'));
     });
 
     test('should display link to user certifications', async function (assert) {
       // when
       const screen = await render(hbs`<UserLoggedMenu/>`);
-      await click('.logged-user-name');
+      await click(
+        screen.getByRole('button', {
+          name: `Hermione ${this.intl.t('navigation.user-logged-menu.details')}`,
+        })
+      );
 
       // then
-      assert.ok(screen.getByRole('link', { name: 'Mes certifications' }));
+      assert.ok(screen.getByRole('link', { name: this.intl.t('navigation.user.certifications') }));
     });
 
     test('should display link to help center', async function (assert) {
       // when
       const screen = await render(hbs`<UserLoggedMenu/>`);
-      await click('.logged-user-name');
+      await click(
+        screen.getByRole('button', {
+          name: `Hermione ${this.intl.t('navigation.user-logged-menu.details')}`,
+        })
+      );
 
       // then
-      assert.ok(screen.getByRole('link', { name: 'Aide' }));
+      assert.ok(screen.getByRole('link', { name: this.intl.t('navigation.main.help') }));
     });
 
     test('should hide user menu, when it was previously open and user-name is clicked one more time', async function (assert) {
+      // given
+      const screen = await render(hbs`<UserLoggedMenu/>`);
+      const buttonMenu = screen.getByRole('button', {
+        name: `Hermione ${this.intl.t('navigation.user-logged-menu.details')}`,
+      });
+
       // when
-      await render(hbs`<UserLoggedMenu/>`);
-      await click('.logged-user-name');
-      await click('.logged-user-name');
+      await click(buttonMenu);
+      await click(buttonMenu);
 
       // then
-      assert.dom('.logged-user-menu').doesNotExist();
+      assert
+        .dom(
+          screen.getByRole('button', {
+            name: `Hermione ${this.intl.t('navigation.user-logged-menu.details')}`,
+            expanded: false,
+          })
+        )
+        .exists();
+      assert.dom(screen.queryByRole('link', { name: this.intl.t('navigation.user.account') })).doesNotExist();
     });
 
     test('should hide user menu, when it was previously open and user press key escape', async function (assert) {
+      // given
+      const screen = await render(hbs`<UserLoggedMenu/>`);
+      const buttonMenu = screen.getByRole('button', {
+        name: `Hermione ${this.intl.t('navigation.user-logged-menu.details')}`,
+      });
+
       // when
-      await render(hbs`<UserLoggedMenu/>`);
-      await click('.logged-user-name');
-      await triggerKeyEvent('.logged-user-name', 'keydown', 27);
+      await click(buttonMenu);
+      await triggerKeyEvent(buttonMenu, 'keydown', 27);
 
       // then
-      assert.dom('.logged-user-menu').doesNotExist();
+      assert
+        .dom(
+          screen.getByRole('button', {
+            name: `Hermione ${this.intl.t('navigation.user-logged-menu.details')}`,
+            expanded: false,
+          })
+        )
+        .exists();
+      assert.dom(screen.queryByRole('link', { name: this.intl.t('navigation.user.account') })).doesNotExist();
     });
 
     test('should hide user menu, when it was previously open and user press shift-tab key', async function (assert) {
+      // given
+      const screen = await render(hbs`<UserLoggedMenu/>`);
+      const buttonMenu = screen.getByRole('button', {
+        name: `Hermione ${this.intl.t('navigation.user-logged-menu.details')}`,
+      });
+
       // when
-      await render(hbs`<UserLoggedMenu/>`);
-      await click('.logged-user-name');
-      await triggerKeyEvent('.logged-user-name', 'keydown', 9, { shiftKey: true });
+      await click(buttonMenu);
+      await triggerKeyEvent(buttonMenu, 'keydown', 9, { shiftKey: true });
 
       // then
-      assert.dom('.logged-user-menu').doesNotExist();
+      assert
+        .dom(
+          screen.getByRole('button', {
+            name: `Hermione ${this.intl.t('navigation.user-logged-menu.details')}`,
+            expanded: false,
+          })
+        )
+        .exists();
+      assert.dom(screen.queryByRole('link', { name: this.intl.t('navigation.user.account') })).doesNotExist();
     });
 
     test('should hide user menu, when the menu is opened then closed', async function (assert) {
@@ -134,6 +187,7 @@ module('Integration | Component | user logged menu', function (hooks) {
         hooks.beforeEach(function () {
           class currentUserService extends Service {
             user = {
+              firstName: 'Hermione',
               hasAssessmentParticipations: true,
             };
           }
@@ -144,10 +198,14 @@ module('Integration | Component | user logged menu', function (hooks) {
         test('should display link to user tests', async function (assert) {
           // when
           const screen = await render(hbs`<UserLoggedMenu/>`);
-          await click('.logged-user-name');
+          await click(
+            screen.getByRole('button', {
+              name: `Hermione ${this.intl.t('navigation.user-logged-menu.details')}`,
+            })
+          );
 
           // then
-          assert.ok(screen.getByRole('link', { name: 'Mes parcours' }));
+          assert.ok(screen.getByRole('link', { name: this.intl.t('navigation.user.tests') }));
         });
       });
 
@@ -155,10 +213,14 @@ module('Integration | Component | user logged menu', function (hooks) {
         test('should not display link to user tests', async function (assert) {
           // when
           const screen = await render(hbs`<UserLoggedMenu/>`);
-          await click('.logged-user-name');
+          await click(
+            screen.getByRole('button', {
+              name: `Hermione ${this.intl.t('navigation.user-logged-menu.details')}`,
+            })
+          );
 
           // then
-          assert.notOk(screen.queryByRole('link', { name: 'Mes parcours' }));
+          assert.notOk(screen.queryByRole('link', { name: this.intl.t('navigation.user.tests') }));
         });
       });
     });
@@ -174,10 +236,16 @@ module('Integration | Component | user logged menu', function (hooks) {
 
     test('should not display user information, for unlogged', async function (assert) {
       // when
-      await render(hbs`<UserLoggedMenu/>`);
+      const screen = await render(hbs`<UserLoggedMenu/>`);
 
       // then
-      assert.dom('.logged-user-name').doesNotExist();
+      assert
+        .dom(
+          screen.queryByRole('button', {
+            name: this.intl.t('navigation.user-logged-menu.details'),
+          })
+        )
+        .doesNotExist();
     });
   });
 });
