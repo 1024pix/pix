c13aaa66ce099c8e63c9682e2e94291eea3fbfc5
Anne-G <ang.schall@gmail.com>
2022-11-24 14:37:56 +0100

    api: add create() in user login repo

--- api/tests/integration/infrastructure/repositories/user-login-repository_test.js
+++ api/tests/integration/infrastructure/repositories/user-login-repository_test.js
@@ -1,4 +1,4 @@
-const { databaseBuilder, expect } = require('../../../test-helper');
+const { databaseBuilder, expect, knex } = require('../../../test-helper');
 const userLoginRepository = require('../../../../lib/infrastructure/repositories/user-login-repository');
 const UserLogin = require('../../../../lib/domain/models/UserLogin');
 
@@ -28,6 +28,29 @@ describe('Integration | Repository | UserLoginRepository', function () {
 
       // then
       expect(result).to.be.null;
+    });
+  });
+
+  describe('#create', function () {
+    afterEach(async function () {
+      await knex('user-logins').delete();
+    });
+
+    it('should return the created user-login', async function () {
+      // given
+      const userId = databaseBuilder.factory.buildUser().id;
+      const userLogin = new UserLogin({ userId });
+      await databaseBuilder.commit();
+
+      // when
+      const result = await userLoginRepository.create(userLogin);
+
+      // then
+      expect(result).to.be.an.instanceOf(UserLogin);
+      expect(result.userId).to.equal(userId);
+      expect(result.createdAt).to.be.not.null;
+      expect(result.updatedAt).to.be.not.null;
+      expect(result.failureCount).to.equal(0);
     });
   });
 });
