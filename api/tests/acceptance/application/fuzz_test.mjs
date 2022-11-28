dd34677dc1d680ed9ff7524e4b8c5c7aec07b08d
François de Metz <francois.de-metz@pix.fr>
2022-11-28 14:19:52 +0100

    Rework fuzzing testing to generate tests

---
+++ api/tests/acceptance/application/fuzz_test.mjs
@@ -0,0 +1,23 @@
+import SmartFuzz from '../../tooling/smart-fuzz.js';
+import { expect } from '../../test-helper.js';
+import createServer from '../../../server.js';
+
+const server = await createServer();
+const routes = server.table();
+
+describe('Acceptance | Fuzz', function () {
+  routes.forEach(function (route) {
+    it(`${route.method} ${route.path} should pass fuzzing`, async function () {
+      const { url, payload, headers } = SmartFuzz(route);
+      const testResponse = await server.inject({
+        payload,
+        headers: Object.assign({}, headers),
+        method: route.method,
+        url,
+      });
+      const { statusCode } = testResponse;
+
+      expect(statusCode).to.be.below(500);
+    });
+  });
+});
--- api/tests/tooling/smart-fuzz.js
+++ api/tests/tooling/smart-fuzz.js
@@ -15,43 +15,25 @@ const matchPathParameter = (name = '[^}]+') =>
     'x'
   );
 
-module.exports = async ({ defaultHeaders, server }) => {
-  const routes = server.table();
-  // We're OK with any 2xx, 3xx, 4xx, should not get 5xx.  This is testing that
-  // the code runs, and that any errors that might be encountered are
-  // appropriately caught, handled, and reflected in the response.  BUT it's
-  // important to note the caveat that this may not be exhaustive or cover every
-  // or even the normal code path.
-  const routeTests = routes.map(async (route) => {
-    const { method, path, settings } = route;
-    const { validate } = settings;
+module.exports = function routeTest(route) {
+  const { path, settings } = route;
+  const { validate } = settings;
 
-    const { headers, params, payload, query } = validate;
+  const { headers, params, payload, query } = validate;
 
-    const validPayload = payload ? Felicity.example(payload) : null;
-    const validHeaders = headers ? Felicity.example(headers) : null;
-    const validParams = params ? Felicity.example(params) : null;
-    const validPath = validParams
-      ? Object.entries(validParams).reduce((a, b) => a.replace(matchPathParameter(b[0]), b[1]), path)
-      : path.replace(matchPathParameter(), 'ajklsdfjklasdf');
-    const validQuery = query ? Felicity.example(query) : null;
-    const urlParams = new URLSearchParams(validQuery);
-    const validQueryString = validQuery ? `?${urlParams.toString()}` : '';
-    const url = `${validPath}${validQueryString}`;
-    const testResponse = await server.inject({
-      payload: validPayload,
-      headers: Object.assign({}, validHeaders, defaultHeaders),
-      method,
-      url,
-    });
-    const { statusCode } = testResponse;
-
-    if (statusCode >= 500) {
-      // eslint-disable-next-line no-console
-      console.log(url, method, validPayload, statusCode);
-      throw new Error(`Fuzz test caused a ${statusCode} response.`);
-    }
-  });
-
-  await Promise.all(routeTests);
+  const validPayload = payload ? Felicity.example(payload) : null;
+  const validHeaders = headers ? Felicity.example(headers) : null;
+  const validParams = params ? Felicity.example(params) : null;
+  const validPath = validParams
+    ? Object.entries(validParams).reduce((a, b) => a.replace(matchPathParameter(b[0]), b[1]), path)
+    : path.replace(matchPathParameter(), 'ajklsdfjklasdf');
+  const validQuery = query ? Felicity.example(query) : null;
+  const urlParams = new URLSearchParams(validQuery);
+  const validQueryString = validQuery ? `?${urlParams.toString()}` : '';
+  const url = `${validPath}${validQueryString}`;
+  return {
+    url,
+    payload: validPayload,
+    headers: validHeaders,
+  };
 };
