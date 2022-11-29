068a9298668be2e278b9dd868f510ba9cc750dc0
P-Jeremy <jemyplu@gmail.com>
2022-11-29 14:42:17 +0100

    :recycle: api: add protected route to save session

--- api/tests/unit/application/certification-centers/index_test.js
+++ api/tests/unit/application/certification-centers/index_test.js
@@ -5,6 +5,56 @@ const moduleUnderTest = require('../../../../lib/application/certification-cente
 const certificationCenterController = require('../../../../lib/application/certification-centers/certification-center-controller');
 
 describe('Unit | Router | certification-center-router', function () {
+  describe('POST /api/certification-centers/{certificationCenterId}/session', function () {
+    it('should return CREATED (200) when everything does as expected', async function () {
+      // given
+      sinon.stub(certificationCenterController, 'saveSession').returns('ok');
+      sinon
+        .stub(securityPreHandlers, 'checkUserIsMemberOfCertificationCenter')
+        .callsFake((request, h) => h.response(true));
+
+      const httpTestServer = new HttpTestServer();
+      await httpTestServer.register(moduleUnderTest);
+
+      /// when
+      const response = await httpTestServer.request('POST', '/api/certification-centers/123/session');
+
+      // then
+      expect(response.statusCode).to.equal(200);
+    });
+
+    it('should reject an invalid certification-centers id', async function () {
+      //given
+      const httpTestServer = new HttpTestServer();
+      await httpTestServer.register(moduleUnderTest);
+
+      // when
+      const response = await httpTestServer.request('POST', '/api/certification-centers/invalid/session');
+
+      // then
+      expect(response.statusCode).to.equal(400);
+    });
+
+    it('should return 403 if user is not member of the given certification center', async function () {
+      //given
+      sinon.stub(securityPreHandlers, 'checkUserIsMemberOfCertificationCenter').callsFake((request, h) =>
+        h
+          .response({ errors: new Error('forbidden') })
+          .code(403)
+          .takeover()
+      );
+
+      const httpTestServer = new HttpTestServer();
+      await httpTestServer.register(moduleUnderTest);
+
+      // when
+      const response = await httpTestServer.request('POST', '/api/certification-centers/123/session');
+
+      // then
+      expect(response.statusCode).to.equal(403);
+    });
+  });
+
   describe('GET /api/certification-centers/{certificationCenterId}/divisions', function () {
     it('should reject an invalid certification center id', async function () {
       // given
