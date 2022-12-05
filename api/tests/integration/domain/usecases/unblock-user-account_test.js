d6c3b3e482d237c9203f470b8abec09226d20914
Anne-G <ang.schall@gmail.com>
2022-12-05 17:17:54 +0100

    api: add unblock user account usecase

---
+++ api/tests/integration/domain/usecases/unblock-user-account_test.js
@@ -0,0 +1,30 @@
+const { expect, databaseBuilder } = require('../../../test-helper');
+
+const usecases = require('../../../../lib/domain/usecases');
+const UserLogin = require('../../../../lib/domain/models/UserLogin');
+
+describe('Integration | UseCases | unblockUserAccount', function () {
+  it('should reset failure count, temporary blocked until date and blocked at date', async function () {
+    // given
+    const userId = databaseBuilder.factory.buildUser({ email: 'email@example.net' }).id;
+    databaseBuilder.factory.buildUser({ email: 'alreadyexist@example.net' });
+    const userLoginId = databaseBuilder.factory.buildUserLogin({
+      userId,
+      failureCount: 50,
+      blockedAt: new Date('2022-11-11'),
+      temporaryBlockedUntil: new Date('2022-11-10'),
+    }).id;
+    await databaseBuilder.commit();
+
+    // when
+    const result = await usecases.unblockUserAccount({ userId });
+
+    // then
+    expect(result).to.be.an.instanceOf(UserLogin);
+    expect(result.id).equal(userLoginId);
+    expect(result.userId).equal(userId);
+    expect(result.failureCount).equal(0);
+    expect(result.temporaryBlockedUntil).equal(null);
+    expect(result.blockedAt).equal(null);
+  });
+});
