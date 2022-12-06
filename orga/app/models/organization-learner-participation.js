28d776987e1074ef4d42c76abe94a96fb8e73913
Karam Mohd <k.haytham.mohammed@octo.com>
2022-12-06 18:36:41 +0100

    feat(ORGA): add organization-learner activity and participations models

---
+++ orga/app/models/organization-learner-participation.js
@@ -0,0 +1,11 @@
+import Model, { attr, belongsTo } from '@ember-data/model';
+
+export default class OrganizationLearnerParticipation extends Model {
+  @attr('string') campaignType;
+  @attr('string') campaignName;
+  @attr('date') createdAt;
+  @attr('date') sharedAt;
+  @attr('string') status;
+
+  @belongsTo('OrganizationLearnerActivity') organizationLeanerActivity;
+}
