117c8be2094c5e67f1592588bf22560b241771a3
Karam Mohd <k.haytham.mohammed@octo.com>
2022-12-06 18:35:18 +0100

    feat(ORGA): add organization learner activity adapter

---
+++ orga/tests/unit/adapters/organization-learner-activity_test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Adapters | organization-learner-activity', function (hooks) {
+  setupTest(hooks);
+
+  module('#urlForQueryRecord', () => {
+    test('should build query url from organizationLearnerId', async function (assert) {
+      const adapter = this.owner.lookup('adapter:organization-learner-activity');
+      const query = { organizationLearnerId: '123' };
+      const url = await adapter.urlForQueryRecord(query);
+
+      assert.ok(url.endsWith('/api/organization-learners/123/activity'));
+      assert.strictEqual(query.organizationLearnerId, undefined);
+    });
+  });
+});
