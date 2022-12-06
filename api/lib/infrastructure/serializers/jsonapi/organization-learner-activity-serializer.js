d47fbd151dde8fb4fda1ca0570c9418b5b6a1fe3
Karam Mohd <k.haytham.mohammed@octo.com>
2022-12-06 18:27:26 +0100

    feat(api): transform serialized organization learner activity object to rename serialized object key

--- api/lib/infrastructure/serializers/jsonapi/organization-learner-activity-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/organization-learner-activity-serializer.js
@@ -1,15 +1,19 @@
 const { Serializer } = require('jsonapi-serializer');
 
 module.exports = {
-  serialize(organizationLearnerParticipation) {
+  serialize(organizationLearnerActivity) {
     return new Serializer('organization-learner-activity', {
+      transform: (organizationLearnerActivity) => ({
+        ...organizationLearnerActivity,
+        organizationLearnerParticipations: organizationLearnerActivity.participations,
+      }),
       id: 'organizationLearnerId',
-      attributes: ['participations'],
-      participations: {
+      attributes: ['organizationLearnerParticipations'],
+      organizationLearnerParticipations: {
         ref: 'id',
         includes: true,
         attributes: ['campaignType', 'campaignName', 'createdAt', 'sharedAt', 'status'],
       },
-    }).serialize(organizationLearnerParticipation);
+    }).serialize(organizationLearnerActivity);
   },
 };
