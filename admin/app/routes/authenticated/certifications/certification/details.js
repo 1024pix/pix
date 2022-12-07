f921820c5f2d2e4d41c7e70cbaf5053b7c8f6346
Antoine Ceol <antoine.ceol@gmail.com>
2022-12-07 23:02:01 +0100

    :bug: admin: Get certification details

--- admin/app/routes/authenticated/certifications/certification/details.js
+++ admin/app/routes/authenticated/certifications/certification/details.js
@@ -1,7 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 export default class CertificationDetailsRoute extends Route {
+  @service store;
+
   model() {
-    return this.modelFor('authenticated.certifications.certification');
+    const { certification_id } = this.paramsFor('authenticated.certifications.certification');
+    return this.store.findRecord('certification-details', certification_id);
   }
 
   setupController(controller, model) {
