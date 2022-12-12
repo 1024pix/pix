6fc78a8421c403316e7284662230bbd5b490c9a8
Andreia Pena <andreia.pena@pix.fr>
2022-12-12 13:13:50 +0100

    feat(api): add lastDataProtectionPolicySeenAt on user serializer

--- api/lib/infrastructure/serializers/jsonapi/user-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/user-serializer.js
@@ -28,6 +28,7 @@ module.exports = {
         'hasSeenFocusedChallengeTooltip',
         'hasSeenOtherChallengesTooltip',
         'trainings',
+        'lastDataProtectionPolicySeenAt',
       ],
       memberships: {
         ref: 'id',
