75a036a00fc64130bfb24110fe5ad7f90374ead8
Andreia Pena <andreia.pena@pix.fr>
2022-12-12 14:48:08 +0100

    feat(api): add PATCH route

---
+++ api/tests/acceptance/application/users/remember-user-has-seen-last-data-protection-policy-information_test.js
@@ -0,0 +1,40 @@
+const { expect, generateValidRequestAuthorizationHeader, databaseBuilder, sinon } = require('../../../test-helper');
+const createServer = require('../../../../server');
+
+describe('Acceptance | Route | remember-user-has-seen-last-data-protection-policy-information', function () {
+  describe('Success case', function () {
+    let clock;
+    let server;
+    const now = new Date('2022-12-07');
+
+    beforeEach(async function () {
+      clock = sinon.useFakeTimers({
+        now,
+        toFake: ['Date'],
+      });
+
+      server = await createServer();
+    });
+
+    afterEach(function () {
+      clock.restore();
+    });
+
+    it('should return a response with HTTP status code 200', async function () {
+      // given
+      const user = databaseBuilder.factory.buildUser({ lastDataProtectionPolicySeenAt: null });
+      await databaseBuilder.commit();
+
+      // when
+      const response = await server.inject({
+        method: 'PATCH',
+        url: `/api/users/${user.id}/has-seen-last-data-protection-policy-information`,
+        headers: { authorization: generateValidRequestAuthorizationHeader(user.id) },
+      });
+
+      // then
+      expect(response.statusCode).to.equal(200);
+      expect(response.result.data.attributes['last-data-protection-policy-seen-at']).to.deep.equal(now);
+    });
+  });
+});
