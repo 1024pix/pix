81d0b2b87ecc79baec3ff4d7b26626bcc11616f0
Geoffroy Begouaussel <geoffroy.begouaussel@zenika.com>
2022-12-12 11:11:07 +0100

    feat(api): add editorName and editorLogoUrl table columns to training table

---
+++ api/db/migrations/20221212100331_add-training-editor-name-logo-column.js
@@ -0,0 +1,28 @@
+const TABLE_NAME = 'trainings';
+const EDITOR_NAME_COLUMN = 'editorName';
+const EDITOR_LOGO_URL_COLUMN = 'editorLogoUrl';
+const editorName = "Ministère de l'Éducation nationale et de la Jeunesse";
+const editorLogoUrl =
+  'https://images.pix.fr/contenu-formatif/editeur/logo-ministere-education-nationale-et-jeunesse.svg';
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.table(TABLE_NAME, function (table) {
+    table.string(EDITOR_NAME_COLUMN).default(editorName).notNullable();
+    table.string(EDITOR_LOGO_URL_COLUMN).default(editorLogoUrl).notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.table(TABLE_NAME, function (table) {
+    table.dropColumn(EDITOR_NAME_COLUMN);
+    table.dropColumn(EDITOR_LOGO_URL_COLUMN);
+  });
+};
