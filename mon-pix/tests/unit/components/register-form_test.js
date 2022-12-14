2b0e66361d347f4f9da27bfcd51fd2dc16c4444f
Andreia Pena <andreia.pena@pix.fr>
2022-12-14 12:04:15 +0100

    fix(mon-pix): fix sco creation account

---
+++ mon-pix/tests/unit/components/register-form_test.js
@@ -0,0 +1,45 @@
+import { module, test } from 'qunit';
+import sinon from 'sinon';
+import { setupTest } from 'ember-qunit';
+import createGlimmerComponent from '../../helpers/create-glimmer-component';
+
+module('Unit | Component | register-form', function (hooks) {
+  setupTest(hooks);
+
+  module('#resetForm', function () {
+    module('when click on "It\'s not me"', function () {
+      test('should reset register form', async function (assert) {
+        // given
+        const store = this.owner.lookup('service:store');
+        const unloadRecordStub = sinon.stub();
+        store.createRecord('sco-organization-learner', { unloadRecord: unloadRecordStub });
+        store.createRecord('dependent-user', { unloadRecord: unloadRecordStub });
+        const component = createGlimmerComponent('component:routes/register-form');
+        component.firstName = 'Lili';
+        component.lastName = 'Coptère';
+        component.monthOfBirth = '05';
+        component.yearOfBirth = '2002';
+        component.dayOfBirth = '02';
+        component.email = 'lili.coptere@ciel.clair';
+        component.username = 'lilicoptere0205';
+        component.matchingStudentFound = true;
+        component.loginWithUsername = false;
+
+        // when
+        await component.resetForm();
+
+        // then
+        assert.strictEqual(component.firstName, null);
+        assert.strictEqual(component.lastName, null);
+        assert.strictEqual(component.monthOfBirth, null);
+        assert.strictEqual(component.yearOfBirth, null);
+        assert.strictEqual(component.dayOfBirth, null);
+        assert.strictEqual(component.email, null);
+        assert.strictEqual(component.username, null);
+        assert.strictEqual(component.password, null);
+        assert.false(component.matchingStudentFound);
+        assert.true(component.loginWithUsername);
+      });
+    });
+  });
+});
