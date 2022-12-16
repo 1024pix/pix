7c599d9b03be3abe423890ce2a43cdd8571914ee
Andreia Pena <andreia.pena@pix.fr>
2022-12-16 16:21:28 +0100

    sr(mon-pix): navbar header : use Testing Library

--- mon-pix/tests/integration/components/navbar-header_test.js
+++ mon-pix/tests/integration/components/navbar-header_test.js
@@ -1,12 +1,9 @@
-/* eslint ember/no-classic-classes: 0 */
-
 import Service from '@ember/service';
 import { module, test } from 'qunit';
 import setupIntlRenderingTest from '../../helpers/setup-intl-rendering';
-import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { setBreakpoint } from 'ember-responsive/test-support';
-import { contains } from '../../helpers/contains';
+import { render } from '@1024pix/ember-testing-library';
 
 module('Integration | Component | navbar-header', function (hooks) {
   setupIntlRenderingTest(hooks);
@@ -17,11 +14,11 @@ module('Integration | Component | navbar-header', function (hooks) {
       setBreakpoint('desktop');
 
       // when
-      await render(hbs`<NavbarHeader/>`);
+      const screen = await render(hbs`<NavbarHeader/>`);
 
       // then
-      assert.ok(contains(this.intl.t('common.skip-links.skip-to-content')));
-      assert.ok(contains(this.intl.t('common.skip-links.skip-to-footer')));
+      assert.ok(screen.getByRole('link', { name: this.intl.t('common.skip-links.skip-to-content') }));
+      assert.ok(screen.getByRole('link', { name: this.intl.t('common.skip-links.skip-to-footer') }));
     });
   });
 
@@ -29,47 +26,49 @@ module('Integration | Component | navbar-header', function (hooks) {
     test('should be rendered in mobile/tablet mode with a burger', async function (assert) {
       // given
       setBreakpoint('tablet');
-      this.owner.register('service:session', Service.extend({ isAuthenticated: true }));
-      this.owner.register('service:currentUser', Service.extend({ user: { fullName: 'John Doe' } }));
-      this.set('burger', {
-        state: {
-          actions: {
-            toggle: () => true,
-          },
-        },
-      });
+      class SessionStub extends Service {
+        isAuthenticated = true;
+      }
+      this.owner.register('service:session', SessionStub);
+      class CurrentUserStub extends Service {
+        user = { fullName: 'John Doe' };
+      }
+      this.owner.register('service:current-user', CurrentUserStub);
 
       // when
-      await render(hbs`<NavbarHeader @burger={{this.burger}} />`);
+      const screen = await render(hbs`<NavbarHeader />`);
 
       // then
-      assert.dom('.navbar-mobile-header__container').exists();
-      assert.dom('.navbar-mobile-header__burger-icon').exists();
+      assert.ok(screen.getByRole('navigation', { name: this.intl.t('navigation.main.label') }));
+      assert.ok(screen.getByRole('button', { name: this.intl.t('navigation.mobile-button-title') }));
     });
 
     test('should be rendered in mobile/tablet mode without burger', async function (assert) {
       // given
       setBreakpoint('tablet');
+      class SessionStub extends Service {
+        isAuthenticated = false;
+      }
+      this.owner.register('service:session', SessionStub);
 
       // when
-      await render(hbs`<NavbarHeader/>`);
+      const screen = await render(hbs`<NavbarHeader/>`);
 
       // then
-      assert.dom('.navbar-mobile-header__container').exists();
-      assert.dom('.navbar-mobile-header__burger-icon').doesNotExist();
+      assert.dom(screen.queryByRole('navigation', { name: this.intl.t('navigation.main.label') })).doesNotExist();
+      assert.dom(screen.queryByRole('button', { name: this.intl.t('navigation.mobile-button-title') })).doesNotExist();
     });
 
     test('should render skip links', async function (assert) {
       // given
       setBreakpoint('tablet');
-      this.owner.register('service:currentUser', Service.extend({ user: { fullName: 'John Doe' } }));
 
       // when
-      await render(hbs`<NavbarHeader/>`);
+      const screen = await render(hbs`<NavbarHeader/>`);
 
       // then
-      assert.ok(contains(this.intl.t('common.skip-links.skip-to-content')));
-      assert.ok(contains(this.intl.t('common.skip-links.skip-to-footer')));
+      assert.ok(screen.getByRole('link', { name: this.intl.t('common.skip-links.skip-to-content') }));
+      assert.ok(screen.getByRole('link', { name: this.intl.t('common.skip-links.skip-to-footer') }));
     });
   });
 });
