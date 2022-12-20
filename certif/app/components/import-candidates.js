cc2c22d61e3c5db03ca1ea94fd08b42dfd3caae2
Quentin Lebouc <quentin.lebouc@pix.fr>
2022-12-20 16:44:16 +0100

    :recycle: certif: use Pix UI upload button for candidate import

--- certif/app/components/import-candidates.js
+++ certif/app/components/import-candidates.js
@@ -1,49 +1,57 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
-import { htmlSafe } from '@ember/template';
 import { action } from '@ember/object';
+import { htmlSafe } from '@ember/template';
 
 export default class ImportCandidates extends Component {
   @service session;
   @service notifications;
+  @service store;
 
   @action
-  async importCertificationCandidates(file) {
-    const { access_token } = this.session.data.authenticated;
+  async importCertificationCandidates(files) {
+    const adapter = this.store.adapterFor('certification-candidates-import');
+    const sessionId = this.args.session.id;
 
+    this.isLoading = true;
     this.notifications.clearAll();
-
     try {
-      await file.upload(this.args.session.urlToUpload, {
-        headers: { Authorization: `Bearer ${access_token}` },
-      });
-      this.args.reloadCertificationCandidate();
+      await adapter.addCertificationCandidatesFromOds(sessionId, files);
       this.notifications.success('La liste des candidats a été importée avec succès.');
-    } catch (err) {
-      const errorPrefix = 'Aucun candidat n’a été importé. <br>';
-      let errorMessage = `${errorPrefix} Veuillez réessayer ou nous contacter via le formulaire du centre d'aide`;
-      if (err.body.errors) {
-        err.body.errors.forEach((error) => {
-          if (error.status === '422') {
-            if (error.code === 'INVALID_DOCUMENT') {
-              errorMessage = htmlSafe(`
+      this.args.reloadCertificationCandidate();
+    } catch (errorResponse) {
+      const errorMessage = this._handleErrorMessage(errorResponse);
+      this.notifications.error(htmlSafe(errorMessage), { cssClasses: 'certification-candidates-notification' });
+    } finally {
+      this.isLoading = false;
+    }
+  }
+
+  _handleErrorMessage(errorResponse) {
+    const errorPrefix = 'Aucun candidat n’a été importé. <br>';
+    let errorMessage = `${errorPrefix} Veuillez réessayer ou nous contacter via le formulaire du centre d'aide`;
+
+    if (errorResponse.errors) {
+      errorResponse.errors.forEach((error) => {
+        if (error.status === '422') {
+          if (error.code === 'INVALID_DOCUMENT') {
+            errorMessage = htmlSafe(`
               <p>
                 ${errorPrefix}<b>${error.detail}</b><br>
                 Veuillez télécharger à nouveau le modèle de liste des candidats et l'importer à nouveau.
               </p>`);
-            } else {
-              errorMessage = htmlSafe(`
+          } else {
+            errorMessage = htmlSafe(`
               <p>
                 ${errorPrefix}<b>${error.detail}</b>
               </p>`);
-            }
           }
-          if (error.status === '403' && error.detail === 'At least one candidate is already linked to a user') {
-            errorMessage = "La session a débuté, il n'est plus possible de modifier la liste des candidats.";
-          }
-        });
-      }
-      this.notifications.error(htmlSafe(errorMessage), { cssClasses: 'certification-candidates-notification' });
+        }
+        if (error.status === '403' && error.detail === 'At least one candidate is already linked to a user') {
+          errorMessage = "La session a débuté, il n'est plus possible de modifier la liste des candidats.";
+        }
+      });
     }
+    return errorMessage;
   }
 }
