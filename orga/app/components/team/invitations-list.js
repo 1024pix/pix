766e3aa2d585ba55dbf168c6216723144fd05ec8
Anne-G <ang.schall@gmail.com>
2022-12-21 11:55:59 +0100

    feat(orga): use InvitationsListItem component in InvitationsList component

--- orga/app/components/team/invitations-list.js
+++ orga/app/components/team/invitations-list.js
@@ -1,6 +1,6 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class TeamInvitationsListComponent extends Component {
   @service store;
