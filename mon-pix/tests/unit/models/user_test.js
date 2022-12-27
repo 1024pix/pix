c70f1efce93aa0db6ea41170c2e87feb80ee78b4
Anne-Marie Esteves <anne-marie.esteves@octo.com>
2022-12-27 15:33:58 +0100

    feat(admin): get lastDataProtectionPolicySeenAt in user model

--- mon-pix/tests/unit/models/user_test.js
+++ mon-pix/tests/unit/models/user_test.js
@@ -26,9 +26,7 @@ module('Unit | Model | user model', function (hooks) {
       const fullName = model.fullName;
 
       // then
-      // TODO: Fix this the next time the file is edited.
-      // eslint-disable-next-line qunit/no-assert-equal
-      assert.equal(fullName, 'Manu Phillip');
+      assert.strictEqual(fullName, 'Manu Phillip');
     });
   });
 });
