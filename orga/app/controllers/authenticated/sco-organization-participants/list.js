375ad2d122100d648ce3a3d5981962f4f5fdddb1
Alexandre Monney <alexandre.monney77@gmail.com>
2023-01-03 15:39:09 +0100

    refacto(orga): renaming connexionType into connectionTypes

--- orga/app/controllers/authenticated/sco-organization-participants/list.js
+++ orga/app/controllers/authenticated/sco-organization-participants/list.js
@@ -13,7 +13,7 @@ export default class ListController extends Controller {
 
   @tracked search = null;
   @tracked divisions = [];
-  @tracked connexionType = [];
+  @tracked connectionTypes = [];
   @tracked certificability = [];
   @tracked pageNumber = null;
   @tracked pageSize = 50;
@@ -28,7 +28,7 @@ export default class ListController extends Controller {
   resetFiltering() {
     this.pageNumber = null;
     this.divisions = [];
-    this.connexionType = [];
+    this.connectionTypes = [];
     this.search = null;
     this.certificability = [];
   }
