6b94617a004b7a93804235519a6b6015ee577a51
Alexandre Monney <alexandre.monney77@gmail.com>
2023-01-03 10:26:24 +0100

    feat(orga): add route to get learner details

--- orga/app/routes/authenticated/organization-participants/organization-participant.js
+++ orga/app/routes/authenticated/organization-participants/organization-participant.js
@@ -1,7 +1,13 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class OrganizationParticipantRoute extends Route {
+  @service store;
+  @service router;
+
   model(params) {
-    return { id: params.participant_id };
+    return this.store
+      .findRecord('organization-learner', params.participant_id)
+      .catch(() => this.router.replaceWith('authenticated.organization-participants'));
   }
 }
