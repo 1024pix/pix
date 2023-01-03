729bdb92fc9bb7db353e44fd4c05c394be1e9119
Andreia Pena <andreia.pena@pix.fr>
2023-01-03 16:32:56 +0100

    feat(api): create anonymisedByFullName getter on UserDetails model

---
+++ api/tests/unit/domain/models/UserDetailsForAdmin_test.js
@@ -0,0 +1,22 @@
+const { expect } = require('../../../test-helper');
+const UserDetailsForAdmin = require('../../../../lib/domain/models/UserDetailsForAdmin');
+
+describe('Unit | Domain | Models | UserDetailsForAdmin', function () {
+  describe('#anonymisedByFullName', function () {
+    it('should return the full name of user who anonymised the user', function () {
+      // given
+      const user = new UserDetailsForAdmin({ anonymisedByFirstName: 'Sarah', anonymisedByLastName: 'Visseuse' });
+
+      // when / then
+      expect(user.anonymisedByFullName).equal('Sarah Visseuse');
+    });
+
+    it('should return null if user is not anonymised', function () {
+      // given
+      const user = new UserDetailsForAdmin({ anonymisedByFirstName: null, anonymisedByLastName: null });
+
+      // when / then
+      expect(user.anonymisedByFullName).to.be.null;
+    });
+  });
+});
