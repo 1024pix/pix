8eb444dbec907060148c4631fdb5dc1753fa85b8
Andreia Pena <andreia.pena@pix.fr>
2023-01-04 14:11:29 +0100

    feat(admin): disable actions button when user is anonymised

--- admin/tests/integration/components/users/user-overview_test.js
+++ admin/tests/integration/components/users/user-overview_test.js
@@ -417,6 +417,20 @@ module('Integration | Component | users | user-overview', function (hooks) {
         assert.dom(screen.getByText('Utilisateur anonymisé par Laurent Gina.')).exists();
       });
 
+      test('should disable action buttons "Modifier" and "Anonymiser cet utilisateur"', async function (assert) {
+        // given
+        const store = this.owner.lookup('service:store');
+        const user = store.createRecord('user', { hasBeenAnonymised: true, anonymisedByFullName: 'Laurent Gina' });
+        this.set('user', user);
+
+        // when
+        const screen = await render(hbs`<Users::UserOverview @user={{this.user}} />`);
+
+        // then
+        assert.dom(screen.getByRole('button', { name: 'Modifier' })).hasAttribute('disabled');
+        assert.dom(screen.getByRole('button', { name: 'Anonymiser cet utilisateur' })).hasAttribute('disabled');
+      });
+
       module('When the admin member who anonymised the user is not set in database', function () {
         test('should display an anonymisation message', async function (assert) {
           // given
