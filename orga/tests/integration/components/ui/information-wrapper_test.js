2b968468a8c529f13789f95ed314b575ef534105
Xavier Carron <xavier.carron@zenika.com>
2023-01-05 11:25:33 +0100

    add wrapper for information definition

---
+++ orga/tests/integration/components/ui/information-wrapper_test.js
@@ -0,0 +1,19 @@
+import { module, test } from 'qunit';
+import setupIntlRenderingTest from '../../../helpers/setup-intl-rendering';
+import { render } from '@1024pix/ember-testing-library';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | Ui | Information Wrapper', function (hooks) {
+  setupIntlRenderingTest(hooks);
+
+  module('yield', function () {
+    test('should display yield content', async function (assert) {
+      await render(hbs`
+            <Ui::InformationWrapper>
+              toto
+            </Ui::InformationWrapper>`);
+
+      assert.contains('toto');
+    });
+  });
+});
