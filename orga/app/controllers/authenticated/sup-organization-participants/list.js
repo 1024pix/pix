553d89c204780c9d532edc92e5a1ce246d069cd8
Alexandre Monney <alexandre.monney77@gmail.com>
2023-01-05 12:48:11 +0100

    feat(orga): add clickable student on list

--- orga/app/controllers/authenticated/sup-organization-participants/list.js
+++ orga/app/controllers/authenticated/sup-organization-participants/list.js
@@ -1,14 +1,22 @@
 import { action } from '@ember/object';
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 export default class ListController extends Controller {
+  @service router;
   @tracked search = null;
   @tracked studentNumber = null;
   @tracked groups = [];
   @tracked certificability = [];
   @tracked pageNumber = null;
   @tracked pageSize = 50;
+
+  @action
+  goToLearnerPage(learnerId, event) {
+    event.preventDefault();
+    this.router.transitionTo('authenticated.sup-organization-participants.sup-organization-participant', learnerId);
+  }
 
   @action
   triggerFiltering(fieldName, value) {
