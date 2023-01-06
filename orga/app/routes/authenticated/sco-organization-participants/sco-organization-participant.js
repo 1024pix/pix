96400e08a3ebcfde509b4225c41c2a21399fc8eb
Alexandre Monney <alexandre.monney77@gmail.com>
2023-01-06 10:21:29 +0100

    feat(orga): add route to get student detail

--- orga/app/routes/authenticated/sco-organization-participants/sco-organization-participant.js
+++ orga/app/routes/authenticated/sco-organization-participants/sco-organization-participant.js
@@ -1,7 +1,13 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class ScoOrganizationParticipantRoute extends Route {
+  @service store;
+  @service router;
+
   model(params) {
-    return { id: params.eleve_id };
+    return this.store
+      .findRecord('organization-learner', params.eleve_id)
+      .catch(() => this.router.replaceWith('authenticated.sco-organization-participants'));
   }
 }
