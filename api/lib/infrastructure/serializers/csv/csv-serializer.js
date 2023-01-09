d652f4c8e4aea2e49acc4e97c95f428aaadc568b
alexandrecoin <alexandre.pc.coin@gmail.com>
2023-01-09 17:45:47 +0100

    :sparkles: verify presence of all file headers before session creation

--- api/lib/infrastructure/serializers/csv/csv-serializer.js
+++ api/lib/infrastructure/serializers/csv/csv-serializer.js
@@ -1,4 +1,5 @@
-const logger = require('../../../infrastructure/logger');
+const logger = require('../../logger');
+const { FileValidationError } = require('../../../../lib/domain/errors');
 
 function _csvFormulaEscapingPrefix(data) {
   const mayBeInterpretedAsFormula = /^[-@=+]/.test(data);
@@ -21,30 +22,36 @@ function _csvSerializeValue(data) {
 }
 
 function deserializeForSessionsImport(parsedCsvData) {
+  const headers = {
+    address: '* Nom du site',
+    room: '* Nom de la salle',
+    date: '* Date de début',
+    time: '* Heure de début (heure locale)',
+    examiner: '* Surveillant(s)',
+    description: 'Observations (optionnel)',
+    lastName: '* Nom de naissance',
+    firstName: '* Prénom',
+    birthdate: '* Date de naissance (format: jj/mm/aaaa)',
+    sex: '* Sexe (M ou F)',
+    birthINSEECode: 'Code Insee',
+    birthPostalCode: 'Code postal',
+    birthCity: 'Nom de la commune',
+    birthCountry: '* Pays',
+    resultRecipientEmail: 'E-mail du destinataire des résultats (formateur, enseignant…)',
+    email: 'E-mail de convocation',
+    externalId: 'Identifiant local',
+    extraTimePercentage: 'Temps majoré ?',
+    billingMode: 'Tarification part Pix',
+    prepaymentCode: 'Code de prépaiement',
+  };
+
   const sessions = [];
+  const csvLineKeys = Object.keys(headers);
+
+  _verifyHeaders({ csvLineKeys, headers, parsedCsvLine: parsedCsvData[0] });
+
   parsedCsvData.forEach((line) => {
-    const data = {
-      address: line['* Nom du site'],
-      room: line['* Nom de la salle'],
-      date: line['* Date de début'],
-      time: line['* Heure de début (heure locale)'],
-      examiner: line['* Surveillant(s)'],
-      description: line['Observations (optionnel)'],
-      lastName: line['* Nom de naissance'],
-      firstName: line['* Prénom'],
-      birthdate: line['* Date de naissance (format: jj/mm/aaaa)'],
-      sex: line['* Sexe (M ou F)'],
-      birthINSEECode: line['Code Insee'],
-      birthPostalCode: line['Code postal'],
-      birthCity: line['Nom de la commune'],
-      birthCountry: line['* Pays'],
-      resultRecipientEmail: line['E-mail du destinataire des résultats (formateur, enseignant…)'],
-      email: line['E-mail de convocation'],
-      externalId: line['Identifiant local'],
-      extraTimePercentage: line['Temps majoré ?'],
-      billingMode: line['Tarification part Pix'] ?? null,
-      prepaymentCode: line['Code de prépaiement'] ?? null,
-    };
+    const data = getDataFromColumnNames({ csvLineKeys, headers, line });
 
     let existingSession;
     if (_hasSessionInformation(data)) {
@@ -63,6 +70,22 @@ function deserializeForSessionsImport(parsedCsvData) {
   });
 
   return sessions;
+}
+
+function getDataFromColumnNames({ csvLineKeys, headers, line }) {
+  const data = {};
+  csvLineKeys.forEach((key) => {
+    data[key] = line[headers[key]];
+  });
+  return data;
+}
+
+function _verifyHeaders({ csvLineKeys, parsedCsvLine, headers }) {
+  csvLineKeys.forEach((key) => {
+    if (parsedCsvLine[headers[key]] === undefined) {
+      throw new FileValidationError(`La colonne : ${headers[key]} est absente du fichier`);
+    }
+  });
 }
 
 function _hasSessionInformation({ room }) {
