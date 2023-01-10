38155ad62db25dd765b1900c41db8632894f75d4
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-01-10 16:51:07 +0100

    feat(admin): update redirection link after organization creation
    Redirect the user to tags list tab instead of team list tab

--- admin/app/controllers/authenticated/organizations/new.js
+++ admin/app/controllers/authenticated/organizations/new.js
@@ -17,7 +17,7 @@ export default class NewController extends Controller {
     try {
       await this.model.save();
       this.notifications.success('L’organisation a été créée avec succès.');
-      this.router.transitionTo('authenticated.organizations.get', this.model.id);
+      this.router.transitionTo('authenticated.organizations.get.all-tags', this.model.id);
     } catch (error) {
       this.notifications.error('Une erreur est survenue.');
     }
