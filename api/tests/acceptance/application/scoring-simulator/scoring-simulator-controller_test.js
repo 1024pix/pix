67ee0caf399117a30719ecb743b91f1e5f51aa61
Quentin Lebouc <quentin.lebouc@pix.fr>
2023-01-12 11:07:46 +0100

    :sparkles: api: create flash scoring simulator route
    Co-authored-by: Nicolas Lepage <nicolas.lepage@pix.fr>

--- api/tests/acceptance/application/scoring-simulator/scoring-simulator-controller_test.js
+++ api/tests/acceptance/application/scoring-simulator/scoring-simulator-controller_test.js
@@ -70,4 +70,56 @@ describe('Acceptance | Controller | scoring-simulator-controller', function () {
       });
     });
   });
+
+  describe('#calculateFlashScores', function () {
+    let options;
+
+    beforeEach(async function () {
+      options = {
+        method: 'POST',
+        url: `/api/scoring-simulator/flash`,
+        payload: {},
+        headers: {},
+      };
+    });
+
+    it('should return status code 200', async function () {
+      // given
+      options.headers.authorization = adminAuthorization;
+
+      // when
+      const response = await server.inject(options);
+
+      // then
+      expect(response).to.have.property('statusCode', 200);
+    });
+
+    describe('when there is no connected user', function () {
+      it('should return status code 401', async function () {
+        // given
+        options.headers.authorization = undefined;
+
+        // when
+        const response = await server.inject(options);
+
+        // then
+        expect(response).to.have.property('statusCode', 401);
+      });
+    });
+
+    describe('when connected user does not have role SUPER_ADMIN', function () {
+      it('should return status code 403', async function () {
+        // given
+        const { id: userId } = databaseBuilder.factory.buildUser();
+        options.headers.authorization = generateValidRequestAuthorizationHeader(userId);
+        await databaseBuilder.commit();
+
+        // when
+        const response = await server.inject(options);
+
+        // then
+        expect(response).to.have.property('statusCode', 403);
+      });
+    });
+  });
 });
