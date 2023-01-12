935736d3af4c2889a86b1c1fab39345f769f12b9
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-01-12 14:24:03 +0100

    feat(api): add migration script
    to update all pending organization invitations with an updatedAt date older than a year

---
+++ api/db/migrations/20230112103200_cancel-pending-organization-invitations-with-an-updatedAt-greater-than-a-year.js
@@ -0,0 +1,11 @@
+const dayjs = require('dayjs');
+const { StatusType } = require('../../lib/domain/models/OrganizationInvitation');
+
+exports.up = function (knex) {
+  return knex('organization-invitations')
+    .update({ status: StatusType.CANCELLED, updatedAt: new Date() })
+    .where('status', StatusType.PENDING)
+    .andWhere('updatedAt', '<', dayjs().subtract(1, 'year'));
+};
+
+exports.down = function () {};
