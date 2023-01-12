9e787f5de022731212c859075e7937e3784b5f4e
Antoine Ceol <antoine.ceol@gmail.com>
2023-01-13 00:36:35 +0100

    :fire: api: Remove end test screen removal from infra

--- api/lib/infrastructure/repositories/certification-center-repository.js
+++ api/lib/infrastructure/repositories/certification-center-repository.js
@@ -15,15 +15,7 @@ function _toDomain(bookshelfCertificationCenter) {
     });
   });
   return new CertificationCenter({
-    ..._.pick(dbCertificationCenter, [
-      'id',
-      'name',
-      'type',
-      'externalId',
-      'isSupervisorAccessEnabled',
-      'createdAt',
-      'updatedAt',
-    ]),
+    ..._.pick(dbCertificationCenter, ['id', 'name', 'type', 'externalId', 'createdAt', 'updatedAt']),
     habilitations,
   });
 }
