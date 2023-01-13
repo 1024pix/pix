f8b5c9d0e038b295460ea046b3c135dfdd9a4a85
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-01-13 14:08:20 +0100

    sr(mon-pix): remove useless attributes key in scorecard serializer

--- mon-pix/mirage/serializers/scorecard.js
+++ mon-pix/mirage/serializers/scorecard.js
@@ -1,18 +1,6 @@
 import ApplicationSerializer from './application';
 
 export default ApplicationSerializer.extend({
-  attributes: [
-    'name',
-    'description',
-    'index',
-    'competenceId',
-    'earnedPix',
-    'level',
-    'pixScoreAheadOfNextLevel',
-    'status',
-    'remainingDaysBeforeReset',
-    'remainingDaysBeforeImproving',
-  ],
   include: ['area'],
   links(record) {
     return {
