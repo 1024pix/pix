50fecd182d44d1730e5fdc4d5d01aeac3a4b0d84
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-01-13 16:24:56 +0100

    feat(api): add get recently used tags usecase

---
+++ api/tests/integration/domain/usecases/get-recently-used-tags_test.js
@@ -0,0 +1,52 @@
+const _ = require('lodash');
+const { expect, databaseBuilder } = require('../../../test-helper');
+const getRecentlyUsedTags = require('../../../../lib/domain/usecases/get-recently-used-tags');
+const Tag = require('../../../../lib/domain/models/Tag');
+const organizationTagRepository = require('../../../../lib/infrastructure/repositories/organization-tag-repository');
+
+describe('Integration | UseCase | get-recently-used-tags', function () {
+  it('returns 10 recently used tags based on a tag id and ordered by the most used first', async function () {
+    // given
+    const basedTag = databaseBuilder.factory.buildTag({ name: 'konoha' });
+    const mostUsedTag = databaseBuilder.factory.buildTag({ name: 'kumo' });
+    const leastUsedTag = databaseBuilder.factory.buildTag({ name: 'hueco mundo' });
+    const tags = [
+      mostUsedTag,
+      databaseBuilder.factory.buildTag({ name: 'kiri' }),
+      databaseBuilder.factory.buildTag({ name: 'suna' }),
+      databaseBuilder.factory.buildTag({ name: 'oto' }),
+      databaseBuilder.factory.buildTag({ name: 'ame' }),
+      databaseBuilder.factory.buildTag({ name: 'iwa' }),
+      databaseBuilder.factory.buildTag({ name: 'uzushio' }),
+      databaseBuilder.factory.buildTag({ name: 'seireitei' }),
+      databaseBuilder.factory.buildTag({ name: 'karakura' }),
+      leastUsedTag,
+      databaseBuilder.factory.buildTag({ name: 'yuei' }),
+    ];
+    const organizations = [];
+
+    _.times(11, () => organizations.push(databaseBuilder.factory.buildOrganization()));
+
+    for (const [index, organization] of organizations.entries()) {
+      const tagIds = tags.slice(0, index + 1).map(({ id }) => id);
+      tagIds.push(basedTag.id);
+      _buildOrganizationTags(organization.id, tagIds);
+    }
+
+    await databaseBuilder.commit();
+
+    // when
+    const recentlyUsedTags = await getRecentlyUsedTags({ tagId: basedTag.id, organizationTagRepository });
+
+    // then
+    expect(recentlyUsedTags.length).to.equal(10);
+    expect(recentlyUsedTags[0]).to.deepEqualInstance(new Tag(mostUsedTag));
+    expect(recentlyUsedTags[recentlyUsedTags.length - 1]).to.deepEqualInstance(new Tag(leastUsedTag));
+  });
+});
+
+function _buildOrganizationTags(organizationId, tagIds) {
+  tagIds.forEach((tagId) => {
+    databaseBuilder.factory.buildOrganizationTag({ organizationId, tagId });
+  });
+}
