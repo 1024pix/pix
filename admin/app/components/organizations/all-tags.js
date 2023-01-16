9b41ce4152848dc13be41f52cfa25f1da6b2c3ff
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-01-16 17:48:51 +0100

    feat(admin): get recently used tags
    after associating a tag to an organization

--- admin/app/components/organizations/all-tags.js
+++ admin/app/components/organizations/all-tags.js
@@ -2,8 +2,15 @@ import Component from '@glimmer/component';
 import map from 'lodash/map';
 import sortBy from 'lodash/sortBy';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 export default class OrganizationAllTags extends Component {
+  @service store;
+
+  @tracked selectedTag = null;
+  @tracked recentlyUsedTags = null;
+
   get allTags() {
     const organizationTagsNames = map(this.args.model.organization.tags.toArray(), 'name');
     const allTags = sortBy(this.args.model.allTags.toArray(), 'name');
@@ -16,6 +23,14 @@ export default class OrganizationAllTags extends Component {
 
   @action
   async addTagToOrganization(tagToAdd) {
+    this.args.model.organization.tags.pushObject(tagToAdd);
+    await this.args.model.organization.save();
+    this.selectedTag = tagToAdd;
+    this.recentlyUsedTags = await this.store.query('tag', { tagId: tagToAdd.id, recentlyUsedTags: true });
+  }
+
+  @action
+  async addRecentlyUsedTagToOrganization(tagToAdd) {
     this.args.model.organization.tags.pushObject(tagToAdd);
     await this.args.model.organization.save();
   }
