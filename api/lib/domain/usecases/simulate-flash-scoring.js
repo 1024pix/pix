040702845ab4f4c63914b75708aa66fbc1264b14
mcampourcy <mireille.campourcy@pix.fr>
2023-01-17 15:49:31 +0100

    :sparkles: api: Add calculating level when there is no given estimated level

--- api/lib/domain/usecases/simulate-flash-scoring.js
+++ api/lib/domain/usecases/simulate-flash-scoring.js
@@ -5,24 +5,35 @@ module.exports = async function simulateFlashScoring({
   flashAlgorithmService,
   successProbabilityThreshold,
   simulations,
+  calculateEstimatedLevel = false,
   locale = 'fr',
 }) {
   const challenges = await challengeRepository.findFlashCompatible({ locale, successProbabilityThreshold });
   const challengeIds = new Set(challenges.map(({ id }) => id));
 
-  return simulations.map(({ id, estimatedLevel, answers: allAnswers }) => {
-    if (estimatedLevel == undefined) {
+  return simulations.map(({ id, estimatedLevel: givenEstimatedLevel, answers: allAnswers }) => {
+    let finalEstimatedLevel = givenEstimatedLevel;
+
+    if (!calculateEstimatedLevel && givenEstimatedLevel == undefined) {
       return new SimulationResult({
         id,
         error: 'Simulation should have an estimated level',
       });
     }
 
-    if (!allAnswers || allAnswers.length === 0) {
-      return new SimulationResult({
-        id,
-        error: 'Simulation should have answers in order to calculate estimated level',
+    if (calculateEstimatedLevel) {
+      if (!allAnswers || allAnswers.length === 0) {
+        return new SimulationResult({
+          id,
+          error: 'Simulation should have answers in order to calculate estimated level',
+        });
+      }
+
+      const { estimatedLevel: calculatedEstimatedLevel } = flashAlgorithmService.getEstimatedLevelAndErrorRate({
+        allAnswers,
+        challenges,
       });
+      finalEstimatedLevel = calculatedEstimatedLevel;
     }
 
     for (const answer of allAnswers) {
@@ -36,10 +47,10 @@ module.exports = async function simulateFlashScoring({
 
     const pixScore = flashAlgorithmService.calculateTotalPixScore({
       challenges,
-      estimatedLevel,
+      estimatedLevel: finalEstimatedLevel,
       allAnswers,
     });
 
-    return new SimulationResult({ id, estimatedLevel, pixScore });
+    return new SimulationResult({ id, estimatedLevel: finalEstimatedLevel, pixScore });
   });
 };
