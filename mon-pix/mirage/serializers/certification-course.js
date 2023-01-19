4c50d91fbcdbf67daa4b4eeb4d4cf0b3f103fde4
Antoine Ceol <antoine.ceol@gmail.com>
2023-01-19 15:53:44 +0100

    :fire: mon-pix: Drop end test screen feature

--- mon-pix/mirage/serializers/certification-course.js
+++ mon-pix/mirage/serializers/certification-course.js
@@ -1,14 +1,7 @@
 import ApplicationSerializer from './application';
 
 export default ApplicationSerializer.extend({
-  attrs: [
-    'nbChallenges',
-    'examinerComment',
-    'hasSeenEndTestScreen',
-    'firstName',
-    'lastName',
-    'isEndTestScreenRemovalEnabled',
-  ],
+  attrs: ['nbChallenges', 'examinerComment', 'hasSeenEndTestScreen', 'firstName', 'lastName'],
   links(certificationCourse) {
     return {
       assessment: {
