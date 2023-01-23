c1d99b79a94f3714cda33dded32bd9925cd632b8
Xavier Carron <xavier.carron@zenika.com>
2023-01-23 15:48:22 +0100

    [TECH] Split Tooling route into Administration and Tools (PIX-6882)

--- admin/app/routes/authenticated/administration.js
+++ admin/app/routes/authenticated/administration.js
@@ -1,10 +1,10 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
-export default class ToolsRoute extends Route {
+export default class AdministrationRoute extends Route {
   @service accessControl;
 
   beforeModel() {
-    this.accessControl.restrictAccessTo(['isMetier', 'isSuperAdmin'], 'authenticated');
+    this.accessControl.restrictAccessTo(['isSuperAdmin'], 'authenticated');
   }
 }
--- admin/tests/acceptance/tools_test.js
+++ admin/tests/acceptance/tools_test.js
@@ -10,7 +10,7 @@ module('Acceptance | tools', function (hooks) {
   setupMirage(hooks);
 
   hooks.beforeEach(async function () {
-    await authenticateAdminMemberWithRole({ isSuperAdmin: true })(server);
+    await authenticateAdminMemberWithRole({ isMetier: true })(server);
   });
 
   module('Access', function () {
@@ -20,60 +20,6 @@ module('Acceptance | tools', function (hooks) {
 
       // then
       assert.strictEqual(currentURL(), '/tools');
-    });
-  });
-
-  module('Rendering', function () {
-    test('Should display "Learning content" information', async function (assert) {
-      // given & when
-      const screen = await visit('/tools');
-
-      // then
-      assert.dom(screen.getByRole('heading', { name: 'Référentiel' })).exists();
-      assert
-        .dom(
-          screen.getByText(
-            'Une version du référentiel de données pédagogique est créée quotidiennement (vers 4h00) et le référentiel ' +
-              "utilisé par l'application est mis à jour (vers 6h00)."
-          )
-        )
-        .exists();
-      assert.dom(screen.getByRole('button', { name: 'Recharger le cache' })).exists();
-      assert
-        .dom(screen.getByRole('button', { name: 'Créer une nouvelle version du référentiel et recharger le cache' }))
-        .exists();
-    });
-  });
-
-  module('Refresh cache content', function () {
-    test('it request the cache refresh', async function (assert) {
-      // given
-      const screen = await visit('/tools');
-
-      // when
-      await clickByName('Recharger le cache');
-
-      // then
-      assert.dom(screen.getByText('La demande de rechargement du cache a bien été prise en compte.')).exists();
-    });
-  });
-
-  module('Create release and refresh cache content', function () {
-    test('it request the release creation and refresh cache', async function (assert) {
-      // given
-      const screen = await visit('/tools');
-
-      // when
-      await clickByName('Créer une nouvelle version du référentiel et recharger le cache');
-
-      // then
-      assert
-        .dom(
-          screen.getByText(
-            'La création de la version du référentiel et le rechargement du cache a bien été prise en compte.'
-          )
-        )
-        .exists();
     });
   });
 
