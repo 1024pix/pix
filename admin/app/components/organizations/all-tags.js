b5166e455d245a3b8604fdbf85219a189db33963
Anne-G <ang.schall@gmail.com>
2023-01-23 09:15:29 +0100

    admin: refacto tags list management

--- admin/app/components/organizations/all-tags.js
+++ admin/app/components/organizations/all-tags.js
@@ -1,6 +1,4 @@
 import Component from '@glimmer/component';
-import map from 'lodash/map';
-import sortBy from 'lodash/sortBy';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
@@ -10,16 +8,8 @@ export default class OrganizationAllTags extends Component {
 
   @tracked selectedTag = null;
   @tracked recentlyUsedTags = null;
-
-  get allTags() {
-    const organizationTagsNames = map(this.args.model.organization.tags.toArray(), 'name');
-    const allTags = sortBy(this.args.model.allTags.toArray(), 'name');
-
-    return map(allTags, (tag) => {
-      tag.isTagAssignedToOrganization = organizationTagsNames.includes(tag.name);
-      return tag;
-    });
-  }
+  @tracked allTags = this.args.model.allTags;
+  @tracked tagsToShow = this.args.model.allTags;
 
   @action
   async addTagToOrganization(tagToAdd) {
@@ -27,12 +17,14 @@ export default class OrganizationAllTags extends Component {
     await this.args.model.organization.save();
     this.selectedTag = tagToAdd;
     this.recentlyUsedTags = await this.store.query('tag', { tagId: tagToAdd.id, recentlyUsedTags: true });
+    tagToAdd.isTagAssignedToOrganization = true;
   }
 
   @action
   async addRecentlyUsedTagToOrganization(tagToAdd) {
     this.args.model.organization.tags.pushObject(tagToAdd);
     await this.args.model.organization.save();
+    tagToAdd.isTagAssignedToOrganization = true;
   }
 
   @action
@@ -41,5 +33,6 @@ export default class OrganizationAllTags extends Component {
     await this.args.model.organization.save();
     this.selectedTag = null;
     this.recentlyUsedTags = null;
+    tagToRemove.isTagAssignedToOrganization = false;
   }
 }
--- admin/tests/unit/routes/authenticated/organizations/all-tags_test.js
+++ admin/tests/unit/routes/authenticated/organizations/all-tags_test.js
@@ -100,4 +100,34 @@ module('Unit | Route | authenticated/organizations/get/all-tags', function (hook
     // then
     assert.ok(router.transitionTo.called);
   });
+
+  test('it returns sorted tags with assigned to organization information', async function (assert) {
+    // given
+    const route = this.owner.lookup('route:authenticated/organizations/get/all-tags');
+    const store = this.owner.lookup('service:store');
+
+    const tag1 = store.createRecord('tag', { id: 1, name: 'MEDNUM' });
+    const tag2 = store.createRecord('tag', { id: 2, name: 'AEFE' });
+    const tag3 = store.createRecord('tag', { id: 3, name: 'CFA' });
+    const tag4 = store.createRecord('tag', { id: 4, name: 'POLE EMPLOI' });
+    const organization = store.createRecord('organization', { id: 7, tags: [tag1, tag2] });
+    route.modelFor = sinon.stub().returns(organization);
+    store.query = sinon.stub().resolves([tag1, tag2, tag3, tag4]);
+
+    // when
+    const result = await route.model();
+
+    // then
+    assert.strictEqual(result.allTags[0].name, 'AEFE');
+    assert.true(result.allTags[0].isTagAssignedToOrganization);
+
+    assert.strictEqual(result.allTags[1].name, 'CFA');
+    assert.false(result.allTags[1].isTagAssignedToOrganization);
+
+    assert.strictEqual(result.allTags[2].name, 'MEDNUM');
+    assert.true(result.allTags[2].isTagAssignedToOrganization);
+
+    assert.strictEqual(result.allTags[3].name, 'POLE EMPLOI');
+    assert.false(result.allTags[3].isTagAssignedToOrganization);
+  });
 });
