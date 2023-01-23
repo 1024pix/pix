15efbe7a176b8f1b6484deaef7b9a52d5c4f10f9
P-Jeremy <jemyplu@gmail.com>
2023-01-23 11:39:56 +0100

    :sparkles: api: add #hasBillingMode to CertificationCenter

--- api/tests/unit/domain/models/CertificationCenter_test.js
+++ api/tests/unit/domain/models/CertificationCenter_test.js
@@ -18,6 +18,23 @@ describe('Unit | Domain | Models | CertificationCenter', function () {
       expect(certificationCenter.isSco).is.false;
     });
   });
+  describe('#hasBillingMode', function () {
+    it('should return false when certification center is of type SCO', function () {
+      // given
+      const certificationCenter = domainBuilder.buildCertificationCenter({ type: 'SCO' });
+
+      // when / then
+      expect(certificationCenter.hasBillingMode).is.false;
+    });
+
+    it('should return true when certification center is not of type SCO', function () {
+      // given
+      const certificationCenter = domainBuilder.buildCertificationCenter({ type: 'SUP' });
+
+      // when / then
+      expect(certificationCenter.hasBillingMode).is.true;
+    });
+  });
   describe('#isHabilitated', function () {
     it('should return false when the certification center does not have the complementary certification', function () {
       // given
