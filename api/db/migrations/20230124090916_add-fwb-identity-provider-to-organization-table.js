be9962f7dd2891a6baa7af2cc1d905faf55a491c
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-01-24 10:15:24 +0100

    feat(api): add migration script

---
+++ api/db/migrations/20230124090916_add-fwb-identity-provider-to-organization-table.js
@@ -0,0 +1,13 @@
+exports.up = async function (knex) {
+  await knex.raw('ALTER TABLE "organizations" DROP CONSTRAINT "organizations_identityProviderForCampaigns_check" ');
+  return knex.raw(
+    "ALTER TABLE \"organizations\" ADD CONSTRAINT \"organizations_identityProviderForCampaigns_check\" CHECK ( \"identityProviderForCampaigns\" IN ('POLE_EMPLOI', 'CNAV', 'GAR', 'FWB') )"
+  );
+};
+
+exports.down = async function (knex) {
+  await knex.raw('ALTER TABLE "organizations" DROP CONSTRAINT "organizations_identityProviderForCampaigns_check" ');
+  return knex.raw(
+    'ALTER TABLE "organizations" ADD CONSTRAINT "organizations_identityProviderForCampaigns_check" CHECK ( "identityProviderForCampaigns" IN (\'POLE_EMPLOI\', \'CNAV\', \'GAR\') )'
+  );
+};
