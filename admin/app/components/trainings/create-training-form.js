59c5aefaf61007da77634b537f47a1683da187a3
Matthias FERRAINA <matthias.ferraina@pix.fr>
2023-01-27 16:41:01 +0100

    fix(admin): repare broken select on training creation form

--- admin/app/components/trainings/create-training-form.js
+++ admin/app/components/trainings/create-training-form.js
@@ -5,33 +5,56 @@ import { optionsLocaleList, optionsTypeList } from '../../models/training';
 import { tracked } from '@glimmer/tracking';
 import set from 'lodash/set';
 
+class Form {
+  @tracked title;
+  @tracked link;
+  @tracked type;
+  @tracked duration = {
+    days: 0,
+    hours: 0,
+    minutes: 0,
+  };
+  @tracked locale;
+  @tracked editorLogoUrl;
+  @tracked editorName;
+}
+
 export default class CreateTrainingForm extends Component {
   @service notifications;
 
   @tracked submitting = false;
 
-  @tracked form = { duration: { days: 0, hours: 0, minutes: 0 } };
-
   constructor() {
     super(...arguments);
     this.optionsTypeList = optionsTypeList;
     this.optionsLocaleList = optionsLocaleList;
+    this.form = new Form();
   }
 
   @action
   updateForm(key, event) {
-    set(this.form, key, event.target.value);
+    set(this.form, key, event.target.value.trim());
   }
 
   @action
-  updateEditorLogoUrl(event) {
-    this.form.editorLogoUrl = `https://images.pix.fr/contenu-formatif/editeur/${event.target.value}`;
+  updateSelect(key, value) {
+    set(this.form, key, value);
   }
 
   @action
   async onSubmit(event) {
     event.preventDefault();
-    const training = { ...this.form };
+
+    const training = {
+      title: this.form.title,
+      link: this.form.link,
+      type: this.form.type,
+      duration: this.form.duration,
+      locale: this.form.locale,
+      editorName: this.form.editorName,
+    };
+    training.editorLogoUrl = `https://images.pix.fr/contenu-formatif/editeur/${this.form.editorLogoUrl}`;
+
     try {
       this.submitting = true;
       await this.args.onSubmit(training);
