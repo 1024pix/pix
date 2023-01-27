fd46fcdb8bd08d80670e1b9f203250d26461c373
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2023-01-27 11:34:13 +0100

    test(admin): add training detail card duration formating tests

--- admin/app/components/trainings/training-details-card.js
+++ admin/app/components/trainings/training-details-card.js
@@ -1,12 +1,12 @@
 import Component from '@glimmer/component';
 import { localeCategories } from '../../models/training';
 
-export default class CreateTrainingForm extends Component {
+export default class TrainingDetailsCard extends Component {
   get formattedDuration() {
     const days = this.args.training.duration.days ? `${this.args.training.duration.days}j ` : '';
     const hours = this.args.training.duration.hours ? `${this.args.training.duration.hours}h ` : '';
     const minutes = this.args.training.duration.minutes ? `${this.args.training.duration.minutes}min` : '';
-    return days + hours + minutes;
+    return `${days}${hours}${minutes}`.trim();
   }
 
   get formattedLocale() {
