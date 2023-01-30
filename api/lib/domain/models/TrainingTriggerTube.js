4f5cdeae87f15ee0f44ddf0406bf862f4d6b6761
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-01-30 12:06:42 +0100

    feat(api): create training trigger model

---
+++ api/lib/domain/models/TrainingTriggerTube.js
@@ -0,0 +1,9 @@
+class TrainingTriggerTube {
+  constructor({ id, tubeId, level } = {}) {
+    this.id = id;
+    this.tubeId = tubeId;
+    this.level = level;
+  }
+}
+
+module.exports = TrainingTriggerTube;
