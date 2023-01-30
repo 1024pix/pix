b043e58a3f04e57f37e883742e6d524afbe347af
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-01-30 15:35:03 +0100

    feat(api): add create-or-update-training-trigger usecase

---
+++ api/tests/unit/domain/usecases/create-or-update-training-trigger_test.js
@@ -0,0 +1,63 @@
+const createOrUpdateTrainingTrigger = require('../../../../lib/domain/usecases/create-or-update-training-trigger');
+const { expect, catchErr, sinon } = require('../../../test-helper');
+
+describe('Unit | UseCase | create-or-update-training-trigger', function () {
+  let trainingRepository;
+  let trainingTriggerRepository;
+
+  beforeEach(function () {
+    trainingRepository = {
+      get: sinon.stub(),
+    };
+    trainingTriggerRepository = {
+      createOrUpdate: sinon.stub(),
+    };
+  });
+
+  context('when training does not exist', function () {
+    it('should throw an error when training does not exist', async function () {
+      // given
+      const trainingId = Symbol('trainingId');
+      trainingRepository.get.withArgs(trainingId).throws(new Error('Not Found'));
+
+      // when
+      const error = await catchErr(createOrUpdateTrainingTrigger)({ trainingId, trainingRepository });
+
+      // then
+      expect(error).to.be.instanceOf(Error);
+      expect(error.message).to.be.equal('Not Found');
+    });
+  });
+
+  context('when training exists', function () {
+    it('should call create or update trigger repository method', async function () {
+      // given
+      const trainingId = Symbol('trainingId');
+      const tubes = Symbol('tubes');
+      const type = Symbol('type');
+      const threshold = Symbol('threshold');
+      const expectedTrainingTrigger = Symbol('trainingTrigger');
+      trainingRepository.get.withArgs(trainingId).resolves();
+      trainingTriggerRepository.createOrUpdate.resolves(expectedTrainingTrigger);
+
+      // when
+      const result = await createOrUpdateTrainingTrigger({
+        trainingId,
+        tubes,
+        type,
+        threshold,
+        trainingRepository,
+        trainingTriggerRepository,
+      });
+
+      // then
+      expect(trainingTriggerRepository.createOrUpdate).to.have.been.calledWith({
+        trainingId,
+        tubes,
+        type,
+        threshold,
+      });
+      expect(result).to.equal(expectedTrainingTrigger);
+    });
+  });
+});
