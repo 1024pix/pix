3b4d9ddfb6f3019df87ca6734d350090704041af
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2023-02-01 17:38:55 +0100

    sr(admin): add some training page translations

--- admin/tests/acceptance/authenticated/trainings/training_test.js
+++ admin/tests/acceptance/authenticated/trainings/training_test.js
@@ -1,6 +1,7 @@
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import setupIntl from '../../../helpers/setup-intl';
 import { visit, screen } from '@1024pix/ember-testing-library';
 import { currentURL } from '@ember/test-helpers';
 import { authenticateAdminMemberWithRole } from 'pix-admin/tests/helpers/test-init';
@@ -8,13 +9,25 @@ import { authenticateAdminMemberWithRole } from 'pix-admin/tests/helpers/test-in
 module('Acceptance | Trainings | Training', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
+  setupIntl(hooks);
 
   module('When admin member is logged in', function (hooks) {
+    let triggersTabName;
+    let targetProfilesTabName;
+    let prerequisiteTriggerHeading;
+    let goalTriggerHeading;
+    let targetProfileTabTitle;
     let trainingId;
 
-    hooks.beforeEach(async () => {
-      await authenticateAdminMemberWithRole({ isSuperAdmin: true })(server);
+    hooks.beforeEach(async function () {
+      triggersTabName = this.intl.t('pages.trainings.training.triggers.tabName');
+      targetProfilesTabName = this.intl.t('pages.trainings.training.targetProfiles.tabName');
+      prerequisiteTriggerHeading = this.intl.t('pages.trainings.training.triggers.prerequisite.title');
+      goalTriggerHeading = this.intl.t('pages.trainings.training.triggers.goal.title');
+      targetProfileTabTitle = this.intl.t('pages.trainings.training.targetProfiles.title');
       trainingId = 2;
+
+      await authenticateAdminMemberWithRole({ isSuperAdmin: true })(server);
 
       server.create('training', {
         id: 1,
@@ -50,11 +63,11 @@ module('Acceptance | Trainings | Training', function (hooks) {
       // then
       assert.strictEqual(currentURL(), `/trainings/${trainingId}/triggers`);
       assert.dom(screen.getByRole('heading', { name: 'Apprendre à piloter des chauves-souris' })).exists();
-      assert.dom(screen.getByRole('link', { name: 'Déclencheurs' })).exists();
-      assert.dom(screen.getByRole('link', { name: 'Déclencheurs' })).hasClass('active');
-      assert.dom(screen.getByRole('link', { name: 'Profils Cibles' })).exists();
-      assert.dom(screen.getByRole('heading', { name: 'Objectifs à atteindre' })).exists();
-      assert.dom(screen.getByRole('heading', { name: 'Objectif à ne pas dépasser' })).exists();
+      assert.dom(screen.getByRole('link', { name: triggersTabName })).exists();
+      assert.dom(screen.getByRole('link', { name: triggersTabName })).hasClass('active');
+      assert.dom(screen.getByRole('link', { name: targetProfilesTabName })).exists();
+      assert.dom(screen.getByRole('heading', { name: prerequisiteTriggerHeading })).exists();
+      assert.dom(screen.getByRole('heading', { name: goalTriggerHeading })).exists();
     });
 
     test('target profiles should be accessible for an authenticated user', async function (assert) {
@@ -64,10 +77,10 @@ module('Acceptance | Trainings | Training', function (hooks) {
       // then
       assert.strictEqual(currentURL(), `/trainings/${trainingId}/target-profiles`);
       assert.dom(screen.getByRole('heading', { name: 'Apprendre à piloter des chauves-souris' })).exists();
-      assert.dom(screen.getByRole('link', { name: 'Déclencheurs' })).exists();
-      assert.dom(screen.getByRole('link', { name: 'Profils Cibles' })).exists();
-      assert.dom(screen.getByRole('link', { name: 'Profils Cibles' })).hasClass('active');
-      assert.dom(screen.getByRole('heading', { name: 'Rattacher un ou plusieurs profils cibles' })).exists();
+      assert.dom(screen.getByRole('link', { name: triggersTabName })).exists();
+      assert.dom(screen.getByRole('link', { name: targetProfilesTabName })).exists();
+      assert.dom(screen.getByRole('link', { name: targetProfilesTabName })).hasClass('active');
+      assert.dom(screen.getByRole('heading', { name: targetProfileTabTitle })).exists();
     });
   });
 });
