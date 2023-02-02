486d96eb3c65cfe13911a5d0fe0424f71bbf15e7
Karam Mohd <k.haytham.mohammed@octo.com>
2023-02-02 11:19:37 +0100

    feat(api): rename organization learner serializer in orga controller

--- api/lib/application/organization-learners/organization-learner-controller.js
+++ api/lib/application/organization-learners/organization-learner-controller.js
@@ -1,7 +1,7 @@
 const usecases = require('../../domain/usecases');
 const organizationLearnerIdentitySerializer = require('../../infrastructure/serializers/jsonapi/organization-learner-identity-serializer');
 const organizationLearnerActivitySerializer = require('../../infrastructure/serializers/jsonapi/organization-learner-activity-serializer');
-const organizationLearnerSerializer = require('../../infrastructure/serializers/jsonapi/organization-learner-serializer');
+const organizationLearnerSerializer = require('../../infrastructure/serializers/jsonapi/organization-learner-follow-up/organization-learner-serializer');
 
 module.exports = {
   async dissociate(request, h) {
