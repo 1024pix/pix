50df85cf8014e49c185d3f92b761730e912430d1
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-02-02 18:20:46 +0100

    feat: add toggleIsForAbsoluteNoviceCampaignAttribute script

---
+++ api/tests/integration/scripts/toggle-is-for-absolute-novice-campaign-attribute_test.js
@@ -0,0 +1,62 @@
+const { expect, sinon, databaseBuilder, knex } = require('../../test-helper');
+const logger = require('../../../lib/infrastructure/logger');
+const {
+  toggleIsForAbsoluteNoviceCampaignAttribute,
+} = require('../../../scripts/toggle-is-for-absolute-novice-campaign-attribute');
+
+describe('Toggle isForAbsoluteNovice campaign attribute', function () {
+  describe('#toggleIsForAbsoluteNoviceCampaignAttribute', function () {
+    context('when campaign does not exist', function () {
+      it('should log an error', async function () {
+        // given
+        sinon.stub(logger, 'error');
+
+        // when
+        await toggleIsForAbsoluteNoviceCampaignAttribute(1234);
+
+        // then
+        expect(logger.error).to.have.been.calledWith('Campaign not found for id 1234');
+      });
+    });
+
+    context('when campaign exists', function () {
+      context('when isForAbsoluteNovice is true', function () {
+        it('should disable isForAbsoluteNovice', async function () {
+          // given
+          const campaign = databaseBuilder.factory.buildCampaign({ isForAbsoluteNovice: true });
+          await databaseBuilder.commit();
+
+          // when
+          await toggleIsForAbsoluteNoviceCampaignAttribute(campaign.id);
+
+          // then
+          const updatedCampaign = await knex
+            .select('isForAbsoluteNovice')
+            .from('campaigns')
+            .where({ id: campaign.id })
+            .first();
+          expect(updatedCampaign.isForAbsoluteNovice).to.be.false;
+        });
+      });
+
+      context('when isForAbsoluteNovice is false', function () {
+        it('should enable isForAbsoluteNovice', async function () {
+          // given
+          const campaign = databaseBuilder.factory.buildCampaign({ isForAbsoluteNovice: false });
+          await databaseBuilder.commit();
+
+          // when
+          await toggleIsForAbsoluteNoviceCampaignAttribute(campaign.id);
+
+          // then
+          const updatedCampaign = await knex
+            .select('isForAbsoluteNovice')
+            .from('campaigns')
+            .where({ id: campaign.id })
+            .first();
+          expect(updatedCampaign.isForAbsoluteNovice).to.be.true;
+        });
+      });
+    });
+  });
+});
