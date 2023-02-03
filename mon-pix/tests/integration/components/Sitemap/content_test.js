9eacaae7febb5319bfc839d61a1632a9ea79634e
Andreia Pena <andreia.pena@pix.fr>
2023-02-03 12:32:15 +0100

    sr(mon-pix): improve tests with testing library

--- mon-pix/tests/integration/components/Sitemap/content_test.js
+++ mon-pix/tests/integration/components/Sitemap/content_test.js
@@ -2,45 +2,42 @@ import { module, test } from 'qunit';
 import setupIntlRenderingTest from '../../../helpers/setup-intl-rendering';
 import { render } from '@1024pix/ember-testing-library';
 import hbs from 'htmlbars-inline-precompile';
-import { contains } from '../../../helpers/contains';
 import sinon from 'sinon';
 
 module('Integration | Component | Sitemap::Content', function (hooks) {
   setupIntlRenderingTest(hooks);
-
-  let model;
-
-  hooks.beforeEach(function () {
-    model = {
-      scorecards: [
-        {
-          id: 1,
-          name: 'Name',
-        },
-        {
-          id: 2,
-          name: 'Name 2',
-        },
-      ],
-    };
-  });
 
   test('should display the sitemap menu with expected elements', async function (assert) {
     // given & when
     const screen = await render(hbs`<Sitemap::Content />`);
 
     // then
-    assert.dom('.sitemap-content-items__link').exists({ count: 11 });
-    assert.ok(contains(this.intl.t('pages.sitemap.title')));
-    assert.ok(contains(this.intl.t('navigation.main.dashboard')));
-    assert.ok(contains(this.intl.t('navigation.main.skills')));
-    assert.ok(contains(this.intl.t('navigation.main.start-certification')));
-    assert.ok(contains(this.intl.t('navigation.main.tutorials')));
-    assert.ok(contains(this.intl.t('navigation.main.code')));
-    assert.ok(contains(this.intl.t('navigation.user.account')));
-    assert.ok(contains(this.intl.t('navigation.user.tests')));
-    assert.ok(contains(this.intl.t('navigation.user.certifications')));
-    assert.ok(contains(this.intl.t('pages.sitemap.resources')));
+    assert.dom(screen.getByRole('heading', { name: this.intl.t('pages.sitemap.title') })).exists();
+    assert.dom(screen.getByText(this.intl.t('pages.sitemap.resources'))).exists();
+    assert
+      .dom(screen.getByRole('link', { name: this.intl.t('navigation.main.dashboard') }))
+      .hasAttribute('href', '/accueil');
+    assert
+      .dom(screen.getByRole('link', { name: this.intl.t('navigation.main.skills') }))
+      .hasAttribute('href', '/competences');
+    assert
+      .dom(screen.getByRole('link', { name: this.intl.t('navigation.main.start-certification') }))
+      .hasAttribute('href', '/certifications');
+    assert
+      .dom(screen.getByRole('link', { name: this.intl.t('navigation.main.tutorials') }))
+      .hasAttribute('href', '/mes-tutos');
+    assert
+      .dom(screen.getByRole('link', { name: this.intl.t('navigation.main.code') }))
+      .hasAttribute('href', '/campagnes');
+    assert
+      .dom(screen.getByRole('link', { name: this.intl.t('navigation.user.account') }))
+      .hasAttribute('href', '/mon-compte');
+    assert
+      .dom(screen.getByRole('link', { name: this.intl.t('navigation.user.tests') }))
+      .hasAttribute('href', '/mes-parcours');
+    assert
+      .dom(screen.getByRole('link', { name: this.intl.t('navigation.user.certifications') }))
+      .hasAttribute('href', '/mes-certifications');
     assert
       .dom(screen.getByRole('link', { name: this.intl.t('navigation.main.trainings') }))
       .hasAttribute('href', '/mes-formations');
@@ -48,13 +45,20 @@ module('Integration | Component | Sitemap::Content', function (hooks) {
 
   test('should display a sublist within skills containing a link to each skill', async function (assert) {
     // given
+    const store = this.owner.lookup('service:store');
+    const scorecard1 = store.createRecord('scorecard', { id: 1, name: 'Name 1' });
+    const scorecard2 = store.createRecord('scorecard', { id: 2, name: 'Name 2' });
+    const model = {
+      scorecards: [scorecard1, scorecard2],
+    };
     this.set('model', model);
 
     // when
-    await render(hbs`<Sitemap::Content @model={{this.model}}/>`);
+    const screen = await render(hbs`<Sitemap::Content @model={{this.model}}/>`);
 
     // then
-    assert.dom('.sitemap-content-items-link-skills__skill').exists({ count: 2 });
+    assert.dom(screen.getByRole('link', { name: 'Name 1' })).exists();
+    assert.dom(screen.getByRole('link', { name: 'Name 2' })).exists();
   });
 
   test('should contain an external link to the list of recipient processors of Pix users ’personal data', async function (assert) {
