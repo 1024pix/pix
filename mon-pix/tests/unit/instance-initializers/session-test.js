62a71ade1d92aa4bdc9bc71a5c90052ef895bcd1
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-02-07 17:17:35 +0100

    feat(mon-pix): handle re-authentication of anonymous user

--- mon-pix/tests/unit/instance-initializers/session-test.js
+++ mon-pix/tests/unit/instance-initializers/session-test.js
@@ -9,7 +9,7 @@ import sinon from 'sinon';
 module('Unit | Instance Initializer | session', function () {
   module('when a session exists', function () {
     module('when a user finalizes a GAR authentication process', function () {
-      test('should remove the current session before the application loads', async function (assert) {
+      test('it removes the current session before the application loads', async function (assert) {
         // given
         run(() => {
           // eslint-disable-next-line ember/no-classic-classes
@@ -51,7 +51,7 @@ module('Unit | Instance Initializer | session', function () {
     });
 
     module('when current URL contains externalUser as query parameter', function () {
-      test('should remove the current session before the application loads', async function (assert) {
+      test('it removes the current session before the application loads', async function (assert) {
         // given
         run(() => {
           // eslint-disable-next-line ember/no-classic-classes
@@ -76,6 +76,122 @@ module('Unit | Instance Initializer | session', function () {
               refresh_token: 'refresh_token',
               expires_in: 45,
               expires_at: 1667837187635,
+            },
+          })
+        );
+
+        // when
+        await this.instance.boot();
+
+        // then
+        const session = window.localStorage.getItem(key);
+        assert.notOk(session);
+        run(this.instance, 'destroy');
+        run(this.application, 'destroy');
+        sinon.restore();
+      });
+    });
+  });
+
+  module('when user is anonymously authenticated', function () {
+    module('when current URL is a campaign URL', function () {
+      test('it removes the current session before the application loads', async function (assert) {
+        // given
+        run(() => {
+          // eslint-disable-next-line ember/no-classic-classes
+          this.TestApplication = Application.extend();
+          this.TestApplication.instanceInitializer({
+            name: 'initializer under test',
+            initialize,
+          });
+          this.application = this.TestApplication.create({ autoboot: false, Resolver, modulePrefix: 'mon-pix-test' });
+          this.instance = this.application.buildInstance();
+        });
+        const key = 'ember_simple_auth-session';
+        sinon.stub(PixWindow, 'getLocationHref').returns('/campagnes');
+        window.localStorage.setItem(
+          key,
+          JSON.stringify({
+            authenticated: {
+              authenticator: 'authenticator:anonymous',
+              access_token: 'access_token',
+              user_id: 1,
+            },
+          })
+        );
+
+        // when
+        await this.instance.boot();
+
+        // then
+        const session = window.localStorage.getItem(key);
+        assert.notOk(session);
+        run(this.instance, 'destroy');
+        run(this.application, 'destroy');
+        sinon.restore();
+      });
+    });
+
+    module('when current URL is a campaign URL with a code', function () {
+      test('it removes the current session before the application loads', async function (assert) {
+        // given
+        run(() => {
+          // eslint-disable-next-line ember/no-classic-classes
+          this.TestApplication = Application.extend();
+          this.TestApplication.instanceInitializer({
+            name: 'initializer under test',
+            initialize,
+          });
+          this.application = this.TestApplication.create({ autoboot: false, Resolver, modulePrefix: 'mon-pix-test' });
+          this.instance = this.application.buildInstance();
+        });
+        const key = 'ember_simple_auth-session';
+        sinon.stub(PixWindow, 'getLocationHref').returns('/campagnes/SIMPLIFIE');
+        window.localStorage.setItem(
+          key,
+          JSON.stringify({
+            authenticated: {
+              authenticator: 'authenticator:anonymous',
+              access_token: 'access_token',
+              user_id: 1,
+            },
+          })
+        );
+
+        // when
+        await this.instance.boot();
+
+        // then
+        const session = window.localStorage.getItem(key);
+        assert.notOk(session);
+        run(this.instance, 'destroy');
+        run(this.application, 'destroy');
+        sinon.restore();
+      });
+    });
+
+    module('when current URL is a campaign tutorial URL with a code', function () {
+      test('it removes the current session before the application loads', async function (assert) {
+        // given
+        run(() => {
+          // eslint-disable-next-line ember/no-classic-classes
+          this.TestApplication = Application.extend();
+          this.TestApplication.instanceInitializer({
+            name: 'initializer under test',
+            initialize,
+          });
+          this.application = this.TestApplication.create({ autoboot: false, Resolver, modulePrefix: 'mon-pix-test' });
+          this.instance = this.application.buildInstance();
+        });
+        const key = 'ember_simple_auth-session';
+        sinon.stub(PixWindow, 'getLocationHref').returns('/campagnes/SIMPLIFIE/evaluation/didacticiel');
+        window.localStorage.setItem(
+          key,
+          JSON.stringify({
+            authenticated: {
+              authenticator: 'authenticator:anonymous',
+              access_token: 'access_token',
+              user_id: 1,
             },
           })
         );
