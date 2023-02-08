d1ccebcf6db29ef291e96c842354b5256a21228a
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-02-08 16:00:47 +0100

    feat(api): add findTargetProfileSummariesForTraining usecase
    Co-authored-by: Matthias Ferraina <matthias.ferraina@pix.fr>

---
+++ api/tests/unit/domain/usecases/find-target-profile-summaries-for-training_test.js
@@ -0,0 +1,24 @@
+const { sinon, expect } = require('../../../test-helper');
+const findTargetProfileSummariesForTraining = require('../../../../lib/domain/usecases/find-target-profile-summaries-for-training');
+
+describe('Unit | UseCase | find-target-profile-summaries-for-training', function () {
+  it('should call the repository with the right arguments and return summaries', async function () {
+    // given
+    const trainingId = Symbol('trainingId');
+    const targetProfileSummaryForAdminRepository = {
+      findByTraining: sinon.stub(),
+    };
+    const targetProfileSummaries = Symbol('targetProfileSummaries');
+    targetProfileSummaryForAdminRepository.findByTraining.resolves(targetProfileSummaries);
+
+    // when
+    const result = await findTargetProfileSummariesForTraining({
+      trainingId,
+      targetProfileSummaryForAdminRepository,
+    });
+
+    // then
+    expect(targetProfileSummaryForAdminRepository.findByTraining).to.have.been.calledWith({ trainingId });
+    expect(result).to.equal(targetProfileSummaries);
+  });
+});
