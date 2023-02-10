3021cdcca8822017eb059a804ebd6a453791ace9
Matthias FERRAINA <matthias.ferraina@pix.fr>
2023-02-10 10:33:22 +0100

    feat(api): create use case to attach target profiles to a training

---
+++ api/lib/domain/usecases/attach-target-profiles-to-training.js
@@ -0,0 +1,22 @@
+const _ = require('lodash');
+const { NotFoundError } = require('../errors');
+
+module.exports = async function attachTargetProfilesToTraining({
+  trainingId,
+  targetProfileIds,
+  targetProfileRepository,
+  targetProfileTrainingRepository,
+}) {
+  const uniqTargetProfileIds = _.uniq(targetProfileIds);
+  const foundTargetProfiles = await targetProfileRepository.findByIds(uniqTargetProfileIds);
+  const foundTargetProfileIds = foundTargetProfiles.map((tp) => tp.id);
+  const unknownTargetProfileIds = _.difference(uniqTargetProfileIds, foundTargetProfileIds);
+  if (unknownTargetProfileIds.length > 0) {
+    throw new NotFoundError(`Le(s) profil cible(s) [${unknownTargetProfileIds.join(', ')}] n'existe(nt) pas.`);
+  }
+
+  return targetProfileTrainingRepository.create({
+    trainingId,
+    targetProfileIds: uniqTargetProfileIds,
+  });
+};
