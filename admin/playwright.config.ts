a955ee71a1b38d5502c8d222afebe8a4d68aac26
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2023-02-14 16:04:46 +0100

    Integrate in CircleCI

--- admin/playwright.config.ts
+++ admin/playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   globalSetup: require.resolve('./tests/e2e/global-setup'),
@@ -81,7 +81,7 @@ export default defineConfig({
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
+  outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
   // webServer: {
