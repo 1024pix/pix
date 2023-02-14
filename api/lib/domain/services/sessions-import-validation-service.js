c90bc19632e20d12766e30a78b8d3ed7bfc9cf19
alexandrecoin <alexandre.pc.coin@gmail.com>
2023-02-14 11:52:06 +0100

    :recycle: api: use service to check if session has unnecessary info

--- api/lib/domain/services/sessions-import-validation-service.js
+++ api/lib/domain/services/sessions-import-validation-service.js
@@ -1,13 +1,28 @@
 const { UnprocessableEntityError } = require('../../application/http-errors');
 const Session = require('../models/Session');
+const { SessionWithIdAndInformationOnMassImportError } = require('../errors');
 
 module.exports = {
   async validate({ sessions }) {
     sessions.forEach((session) => {
+      const { sessionId } = session;
+
       const domainSession = new Session(session);
       if (domainSession.isSessionScheduledInThePast()) {
         throw new UnprocessableEntityError(`Une session ne peut pas être programmée dans le passé`);
       }
+
+      if (sessionId) {
+        if (_hasSessionInfo(domainSession)) {
+          throw new SessionWithIdAndInformationOnMassImportError(
+            `Merci de ne pas renseigner les informations de session pour la session: ${sessionId}`
+          );
+        }
+      }
     });
   },
 };
+
+function _hasSessionInfo(session) {
+  return session.address || session.room || session.date || session.time || session.examiner;
+}
--- api/tests/unit/domain/services/sessions-import-validation-service_test.js
+++ api/tests/unit/domain/services/sessions-import-validation-service_test.js
@@ -1,5 +1,6 @@
 const { catchErr, expect, sinon } = require('../../../test-helper');
 const sessionsImportValidationService = require('../../../../lib/domain/services/sessions-import-validation-service');
+const { SessionWithIdAndInformationOnMassImportError } = require('../../../../lib/domain/errors');
 
 describe('Unit | Service | sessions import validation Service', function () {
   describe('#validate', function () {
@@ -65,5 +66,73 @@ describe('Unit | Service | sessions import validation Service', function () {
         });
       });
     });
+
+    context('unnecessary session information validation', function () {
+      context('when there is a sessionId and session information', function () {
+        it('should throw', async function () {
+          const sessions = [
+            {
+              ..._createValidSessionData(),
+              sessionId: 1234,
+              certificationCandidates: [],
+            },
+          ];
+
+          // when
+          const error = await catchErr(sessionsImportValidationService.validate)({
+            sessions,
+          });
+
+          // then
+          expect(error).to.be.an.instanceOf(SessionWithIdAndInformationOnMassImportError);
+          expect(error.message).to.equal(
+            'Merci de ne pas renseigner les informations de session pour la session: 1234'
+          );
+        });
+      });
+
+      context('when there is session information but no sessionId', function () {
+        it('should not throw', async function () {
+          const sessions = [
+            {
+              ..._createValidSessionData(),
+              certificationCandidates: [],
+            },
+          ];
+
+          // when
+          // then
+          expect(sessionsImportValidationService.validate({ sessions })).not.to.throw;
+        });
+      });
+
+      context('when there is a sessionId but no session information', function () {
+        it('should not throw', async function () {
+          const sessions = [
+            {
+              certificationCandidates: [],
+              sessionId: 123,
+            },
+          ];
+
+          // when
+          // then
+          expect(sessionsImportValidationService.validate({ sessions })).not.to.throw;
+        });
+      });
+    });
   });
 });
+
+function _createValidSessionData() {
+  return {
+    sessionId: undefined,
+    address: 'Site 1',
+    room: 'Salle 1',
+    date: '2023-03-12',
+    time: '01:00',
+    examiner: 'Pierre',
+    description: 'desc',
+    certificationCandidates: [],
+  };
+}
