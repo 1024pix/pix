db32904a34d9623d8fe7c68858110cf0150d7840
Matthias FERRAINA <matthias.ferraina@pix.fr>
2023-02-14 14:33:17 +0100

    feat(admin): redirect to training details page after creation

--- admin/app/controllers/authenticated/trainings/new.js
+++ admin/app/controllers/authenticated/trainings/new.js
@@ -8,16 +8,16 @@ export default class NewController extends Controller {
   @service router;
 
   @action
-  goBackToTrainingList() {
-    this.router.transitionTo('authenticated.trainings.list');
+  goToTrainingDetails(trainingId) {
+    this.router.transitionTo('authenticated.trainings.training', trainingId);
   }
 
   @action
   async createOrUpdateTraining(trainingFormData) {
     try {
-      await this.store.createRecord('training', trainingFormData).save();
+      const { id } = await this.store.createRecord('training', trainingFormData).save();
       this.notifications.success('Le contenu formatif a été créé avec succès.');
-      this.goBackToTrainingList();
+      this.goToTrainingDetails(id);
     } catch (error) {
       this.notifications.error('Une erreur est survenue.');
     }
