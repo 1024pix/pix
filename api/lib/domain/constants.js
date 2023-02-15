913f457e7356e71eaf12de54e33fafff82ed2063
Anne-Marie Esteves <anne-marie.esteves@octo.com>
2023-02-15 15:47:12 +0100

    feat(api): add specific pole emploi error details as constants

--- api/lib/domain/constants.js
+++ api/lib/domain/constants.js
@@ -91,6 +91,13 @@ module.exports = {
     },
   },
 
+  OIDC_ERRORS: {
+    USER_INFO: {
+      missingFields: { shortCode: 'OIDC01', code: 'USER_INFO_MISSING_FIELDS' },
+      badResponseFormat: { shortCode: 'OIDC02', code: 'USER_INFO_BAD_RESPONSE_FORMAT' },
+    },
+  },
+
   CERTIFICATION_CENTER_TYPES: {
     SUP: 'SUP',
     SCO: 'SCO',
