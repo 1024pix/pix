d789aec42f923a7e8ef96763b897aa02d3479efe
Xavier Carron <xavier.carron@zenika.com>
2023-02-15 14:25:13 +0100

    feat(api) Happy test create / update stage route
    Co-authored-by: Laura BERGOENS <48727874+laura-bergoens@users.noreply.github.com>
    Co-authored-by: Mâche <mariehelene.rots@gmail.com>

---
+++ api/tests/acceptance/application/stages/post-stage_test.js
@@ -0,0 +1,43 @@
+const { databaseBuilder, expect, generateValidRequestAuthorizationHeader, knex } = require('../../../test-helper');
+const createServer = require('../../../../server');
+
+describe('Acceptance | Stages | POST /api/admin/stages', function () {
+  let server, options;
+
+  beforeEach(async function () {
+    const adminUserId = databaseBuilder.factory.buildUser.withRole().id;
+    const targetProfileId = databaseBuilder.factory.buildTargetProfile().id;
+    server = await createServer();
+    options = {
+      method: 'POST',
+      url: '/api/admin/stages',
+      headers: { authorization: generateValidRequestAuthorizationHeader(adminUserId) },
+      payload: {
+        data: {
+          attributes: {
+            level: null,
+            threshold: 65,
+            title: 'nouveau palier titre',
+            message: 'nouveau palier message',
+          },
+          relationships: {
+            'target-profile': { data: { id: targetProfileId } },
+          },
+        },
+      },
+    };
+    await databaseBuilder.commit();
+  });
+
+  afterEach(async function () {
+    return knex('stages').delete();
+  });
+
+  it('should return 201 status code', async function () {
+    // when
+    const response = await server.inject(options);
+
+    // then
+    expect(response.statusCode).to.equal(201);
+  });
+});
