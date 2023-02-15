ffe60e5f0d4085590a1e4c6762c891d3224f55cd
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-02-15 16:52:20 +0100

    feat(admin): add edit mode in training details
    Co-authored-by: Matthias Ferraina <matthias.ferraina@pix.fr>

--- admin/tests/integration/components/trainings/create-or-update-training-form_test.js
+++ admin/tests/integration/components/trainings/create-or-update-training-form_test.js
@@ -4,6 +4,7 @@ import { triggerEvent } from '@ember/test-helpers';
 import { render, clickByName } from '@1024pix/ember-testing-library';
 import sinon from 'sinon';
 import hbs from 'htmlbars-inline-precompile';
+import { typeCategories, localeCategories } from 'pix-admin/models/training';
 
 module('Integration | Component | Trainings::CreateOrUpdateTrainingForm', function (hooks) {
   setupRenderingTest(hooks);
@@ -28,10 +29,12 @@ module('Integration | Component | Trainings::CreateOrUpdateTrainingForm', functi
 
     this.set('onSubmit', onSubmit);
     this.set('onCancel', onCancel);
-    this.set('form', form);
   });
 
   test('it should display the items', async function (assert) {
+    // given
+    this.set('form', form);
+
     // when
     const screen = await render(hbs`<Trainings::CreateOrUpdateTrainingForm
   @onSubmit={{this.onSubmit}}
@@ -55,6 +58,9 @@ module('Integration | Component | Trainings::CreateOrUpdateTrainingForm', functi
   });
 
   test('it should call onSubmit when form is valid', async function (assert) {
+    // given
+    this.set('form', form);
+
     // when
     await render(hbs`<Trainings::CreateOrUpdateTrainingForm
   @onSubmit={{this.onSubmit}}
@@ -68,6 +74,9 @@ module('Integration | Component | Trainings::CreateOrUpdateTrainingForm', functi
   });
 
   test('it should call onCancel when form is cancel', async function (assert) {
+    // given
+    this.set('form', form);
+
     // when
     await render(hbs`<Trainings::CreateOrUpdateTrainingForm
   @onSubmit={{this.onSubmit}}
@@ -78,5 +87,45 @@ module('Integration | Component | Trainings::CreateOrUpdateTrainingForm', functi
 
     // then
     assert.ok(onCancel.called);
+  });
+
+  module('when model is provided', function () {
+    test('it should display the items with model values', async function (assert) {
+      // given
+      const editorLogo = 'un-logo.svg';
+      const model = {
+        title: 'Un contenu formatif',
+        link: 'https://un-contenu-formatif',
+        type: 'webinaire',
+        locale: 'fr-fr',
+        editorName: 'Un éditeur de contenu formatif',
+        editorLogoUrl: `https://example.net/${editorLogo}`,
+        duration: { days: 0, hours: 0, minutes: 0 },
+      };
+
+      this.set('model', model);
+
+      // when
+      const screen = await render(hbs`<Trainings::CreateOrUpdateTrainingForm
+  @onSubmit={{this.onSubmit}}
+  @onCancel={{this.onCancel}}
+  @model={{this.model}}
+/>`);
+
+      // then
+      assert.dom(screen.getByLabelText('Titre')).hasValue(model.title);
+      assert.dom(screen.getByLabelText('Lien')).hasValue(model.link);
+      assert.strictEqual(screen.getByLabelText('Format').innerText, typeCategories[model.type]);
+      assert.dom(screen.getByLabelText('Jours (JJ)')).hasValue(model.duration.days.toString());
+      assert.dom(screen.getByLabelText('Heures (HH)')).hasValue(model.duration.hours.toString());
+      assert.dom(screen.getByLabelText('Minutes (MM)')).hasValue(model.duration.minutes.toString());
+      assert.strictEqual(screen.getByLabelText('Langue localisée').innerText, localeCategories[model.locale]);
+      assert.dom(screen.getByLabelText('Nom du fichier du logo éditeur')).hasValue(editorLogo);
+      assert
+        .dom(screen.getByLabelText("Nom de l'éditeur Exemple: Ministère de l'Éducation nationale et de la Jeunesse"))
+        .exists();
+      assert.dom(screen.getByRole('button', { name: 'Annuler' })).exists();
+      assert.dom(screen.getByRole('button', { name: 'Modifier le contenu formatif' })).exists();
+    });
   });
 });
