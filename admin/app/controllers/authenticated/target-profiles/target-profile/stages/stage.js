1deb59b5387df60ce62b345e5927907d4603139f
Laura Bergoens <laura.bergoens@pix.fr>
2023-02-16 12:21:17 +0100

    limit level choices to those available when creating or editing stage

--- admin/app/controllers/authenticated/target-profiles/target-profile/stages/stage.js
+++ admin/app/controllers/authenticated/target-profiles/target-profile/stages/stage.js
@@ -1,9 +1,18 @@
+import difference from 'lodash/difference';
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class StageController extends Controller {
   @tracked isEditMode = false;
+
+  get availableLevels() {
+    const unavailableLevels = this.model.targetProfile.stages.map((stage) =>
+      stage.id === this.model.stage.id ? null : stage.level
+    );
+    const allLevels = Array.from({ length: 7 }, (_, i) => i + 1);
+    return difference(allLevels, unavailableLevels);
+  }
 
   @action
   toggleEditMode() {
