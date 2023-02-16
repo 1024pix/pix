e747552fb24145875fd7cc467bc89203cf2c86c7
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-02-16 13:15:45 +0100

    feat(mon-pix): improve api error display
    Co-authored-by: Anne-Marie Esteves <anne-marie.esteves@octo.com>

--- mon-pix/tests/unit/routes/error_test.js
+++ mon-pix/tests/unit/routes/error_test.js
@@ -1,21 +1,72 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import JSONApiError from 'mon-pix/errors/json-api-error';
 
 module('Unit | Route | error', function (hooks) {
   setupTest(hooks);
 
+  let route;
+
+  hooks.beforeEach(function () {
+    route = this.owner.lookup('route:error');
+  });
+
   test('exists', function (assert) {
-    const route = this.owner.lookup('route:error');
     assert.ok(route);
   });
 
-  module('#hasUnauthorizedError', function (hooks) {
-    let route;
+  module('#setupController', function () {
+    test('handles api errors', function (assert) {
+      // Given
+      const controller = {};
+      const error = { errors: [{ status: '503', title: 'Service Unavailable', detail: 'The service is unavailable' }] };
 
-    hooks.beforeEach(function () {
-      route = this.owner.lookup('route:error');
+      // When
+      route.setupController(controller, error);
+
+      // Then
+      assert.strictEqual(controller.errorMessage, error);
+      assert.strictEqual(controller.errorDetail, error.errors[0].detail);
+      assert.strictEqual(controller.errorStatus, error.errors[0].status);
+      assert.strictEqual(controller.errorTitle, error.errors[0].title);
     });
 
+    test('handles json api errors', function (assert) {
+      // Given
+      const controller = {};
+      const error = new JSONApiError('This is an error message', {
+        status: '503',
+        title: 'Service Unavailable',
+        detail: 'The service is unavailable',
+      });
+
+      // When
+      route.setupController(controller, error);
+
+      // Then
+      assert.strictEqual(controller.errorMessage, error.message);
+      assert.strictEqual(controller.errorDetail, null);
+      assert.strictEqual(controller.errorStatus, error.status);
+      assert.strictEqual(controller.errorTitle, error.title);
+    });
+
+    test('handles unhandled errors', function (assert) {
+      // Given
+      const controller = {};
+      const error = 'This is an error message';
+
+      // When
+      route.setupController(controller, error);
+
+      // Then
+      assert.strictEqual(controller.errorMessage, error);
+      assert.strictEqual(controller.errorDetail, null);
+      assert.strictEqual(controller.errorStatus, null);
+      assert.strictEqual(controller.errorTitle, null);
+    });
+  });
+
+  module('#hasUnauthorizedError', function () {
     test('finds an unauthorized code in the first error object', function (assert) {
       // Given
       const errorEvent = { errors: [{ status: '401' }] };
