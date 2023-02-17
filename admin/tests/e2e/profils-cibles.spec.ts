923bd62a27daed973578b61d4dfe96bd18266340
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2023-02-17 16:23:47 +0100

    Use minimal LCMS and improve locators

--- admin/tests/e2e/profils-cibles.spec.ts
+++ admin/tests/e2e/profils-cibles.spec.ts
@@ -7,7 +7,7 @@ test.describe('Profils cibles', () => {
     await page.goto('/');
   });
 
-  test('Créer un profil cible et ses clés de lecture', async ({ page }) => {
+  test('Créer un profil cible et ses clés de lecture', async ({ page, browserName }) => {
     // Création du profil cible
     await page
       .getByRole('listitem')
@@ -16,21 +16,24 @@ test.describe('Profils cibles', () => {
     await page.getByRole('link', { name: 'Nouveau profil cible' }).click();
     await page.getByLabel('Nom (obligatoire) :').fill('Mon profil cible');
     await page.getByPlaceholder('7777').fill('1');
-    await page.getByRole('button', { name: '3 · Création de contenu' }).click();
-    await page.getByRole('button', { name: '3.4 Programmer' }).click();
-    await page.getByLabel('Programme', { exact: true }).check();
-    await page
-      .getByRole('cell', { name: 'Sélection du niveau du sujet suivant : Programme 8' })
-      .getByRole('button', { name: '8' })
-      .click();
+    await page.getByRole('button', { name: '1 · Savoirs essentiels' }).click();
+    await page.getByRole('button', { name: '1.4 Français' }).click();
+    await page.getByRole('checkbox', { name: 'Test 4' }).click();
+    await page.getByRole('cell', { name: 'Sélection du niveau du sujet suivant : Grammaire' }).getByRole('button').click();
     await page.getByRole('option', { name: '4' }).click();
+    await page.getByRole('cell', { name: 'Sélection du niveau du sujet suivant : Vocabulaire' }).getByRole('button').click();
+    await page.getByRole('option', { name: '6' }).click();
     await page.getByRole('button', { name: 'Créer le profil cible' }).click();
-    await page.getByRole('button', { name: '3 · Création de contenu' }).click();
-    await page.getByRole('button', { name: '3.4 Programmer' }).click();
+
     await expect.soft(page.getByRole('heading', { name: 'Mon profil cible' })).toBeVisible();
     await expect.soft(page.getByText('Organisation de référence : 1')).toBeVisible();
-    await expect.soft(page.getByRole('button', { name: '3 · Création de contenu' })).toBeVisible();
-    await expect.soft(page.getByRole('button', { name: '3.4 Programmer' })).toBeVisible();
+
+    await page.getByRole('button', { name: '1 · Savoirs essentiels' }).click();
+    await page.getByRole('button', { name: '1.4 Français' }).click();
+
+    await expect.soft(page.getByRole('cell', { name: 'Test 4' })).toBeVisible();
+    await expect.soft(page.getByRole('cell', { name: 'grammaire : Grammaire' })).toBeVisible();
+    await expect.soft(page.getByRole('cell', { name: 'vocabulaire : Vocabulaire' })).toBeVisible();
 
     // Création du RT
     await page.getByRole('link', { name: 'Clés de lecture' }).click();
@@ -39,29 +42,43 @@ test.describe('Profils cibles', () => {
     await page.getByPlaceholder('exemple: clea_num.svg').fill('border-collie.svg');
     await page.getByLabel("Texte alternatif pour l'image :").fill('Le plus beau chien du monde');
     await page.getByLabel('Message :').fill('Message du RT');
-    await page.getByLabel("Clé (texte unique , vérifier qu'il n'existe pas) :").fill('BORDER_COLLIE');
+    await page.getByLabel("Clé").fill(`BORDER_COLLIE_${browserName}`);
     await page.getByLabel('Lacunes :').check();
+
     await page.getByLabel("Sur l'ensemble du profil cible").check();
-    await page.locator('#campaignThreshold').fill('80');
+    const critereGlobal = page.locator('section').filter({ hasText: 'Critère d’obtention sur l’ensemble du profil cible' });
+    await critereGlobal.getByLabel('Taux de réussite requis').fill('80');
+
     await page.getByLabel('Sur une sélection de sujets du profil cible').check();
-    await page.getByRole('button', { name: 'Ajouter une nouvelle sélection de sujets' }).click();
-    await page.locator('#cappedTubeCriterion0criterionName').fill('Critère 1');
-    await page.locator('#cappedTubeCriterion0').fill('20');
-    await page.getByRole('button', { name: '3 · Création de contenu' }).nth(0).click();
-    await page.getByRole('button', { name: '3.4 Programmer' }).nth(0).click();
-    await page.getByLabel('@baseDeDonnées', { exact: false }).check();
-    await page
-      .getByRole('cell', { name: 'Sélection du niveau du sujet suivant : Requête SQL 8' })
-      .getByRole('button', { name: '8' })
+    const premierCritereSurSelection = page
+      .locator('section')
+      .filter({ hasText: 'Critère d’obtention sur une sélection de sujets du profil cible' })
+      .nth(0);
+    await premierCritereSurSelection.getByLabel('Nom du critère').fill('Critère 1');
+    await premierCritereSurSelection.getByLabel('Taux de réussite requis').fill('20');
+    await premierCritereSurSelection.getByRole('button', { name: '1 · Savoirs essentiels' }).click();
+    await premierCritereSurSelection.getByRole('button', { name: '1.4 Français' }).click();
+    await premierCritereSurSelection.getByRole('checkbox', { name: 'grammaire : Grammaire' }).check();
+    await premierCritereSurSelection
+      .getByRole('cell', { name: 'Sélection du niveau du sujet suivant : Grammaire' })
+      .getByRole('button')
       .click();
-    await page.getByRole('option', { name: '4' }).click();
-    await page.locator('#cappedTubeCriterion1criterionName').fill('Critère 2');
-    await page.locator('#cappedTubeCriterion1').fill('50');
-    await page.getByRole('button', { name: '3 · Création de contenu' }).nth(1).click();
-    await page.getByRole('button', { name: '3.4 Programmer' }).nth(1).click();
-    await page.getByLabel('Programme', { exact: true }).nth(1).check();
+    await premierCritereSurSelection.getByRole('option', { name: '2' }).click();
+
+    await page.getByRole('button', { name: 'Ajouter une nouvelle sélection de sujets' }).click();
+    const deuxiemeCritereSurSelection = page
+      .locator('section')
+      .filter({ hasText: 'Critère d’obtention sur une sélection de sujets du profil cible' })
+      .nth(1);
+    await deuxiemeCritereSurSelection.getByLabel('Nom du critère').fill('Critère 2');
+    await deuxiemeCritereSurSelection.getByLabel('Taux de réussite requis').fill('50');
+    await deuxiemeCritereSurSelection.getByRole('button', { name: '1 · Savoirs essentiels' }).click();
+    await deuxiemeCritereSurSelection.getByRole('button', { name: '1.4 Français' }).click();
+    await deuxiemeCritereSurSelection.getByRole('checkbox', { name: 'vocabulaire : Vocabulaire' }).check();
+
     await page.getByRole('button', { name: 'Enregistrer le RT' }).click();
     await page.getByRole('link', { name: 'Détails du badge Mon super RT' }).click();
+
     await expect.soft(page.getByText('Nom du résultat thématique : Mon super RT')).toBeVisible();
     await expect.soft(page.getByText("Nom de l'image : border-collie.svg")).toBeVisible();
     await expect.soft(page.getByText('Clé : BORDER_COLLIE')).toBeVisible();
@@ -94,9 +111,8 @@ test.describe('Profils cibles', () => {
     await page.getByLabel('Titre pour le prescripteur').fill('Titre prescripteur palier 2');
     await page.getByLabel('Description pour le prescripteur').fill('Description prescripteur palier 2');
     await page.getByRole('button', { name: 'Enregistrer' }).click();
-    await expect.soft(page.getByText('Titre pour le prescripteur : Titre prescripteur palier 2')).toBeVisible();
-    await expect
-      .soft(page.getByText('Description pour le prescripteur : Description prescripteur palier 2'))
+    await expect(page.getByText('Titre pour le prescripteur : Titre prescripteur palier 2')).toBeVisible();
+    await expect(page.getByText('Description pour le prescripteur : Description prescripteur palier 2'))
       .toBeVisible();
   });
 });
