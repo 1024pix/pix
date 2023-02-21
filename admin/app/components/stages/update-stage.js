ad86d3bb8353be9f4ea1525fceba715a9346d84f
Marie-Hélène <marie-helene.rots@pix.fr>
2023-02-21 11:27:47 +0100

    feat(admin): add visual errors while updating a stage when invalid field

--- admin/app/components/stages/update-stage.js
+++ admin/app/components/stages/update-stage.js
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
+import isInteger from 'lodash/isInteger';
 
 export default class UpdateStage extends Component {
   @service notifications;
@@ -12,6 +13,9 @@ export default class UpdateStage extends Component {
   @tracked message;
   @tracked prescriberTitle;
   @tracked prescriberDescription;
+  @tracked thresholdStatus = 'default';
+  @tracked titleStatus = 'default';
+  @tracked messageError = null;
 
   constructor() {
     super(...arguments);
@@ -44,11 +48,57 @@ export default class UpdateStage extends Component {
   @action
   async updateStage(event) {
     event.preventDefault();
+    if ([this.thresholdStatus, this.titleStatus].includes('error')) return;
     await this._updateStage();
   }
 
   @action
   setLevel(level) {
     this.level = level;
+  }
+
+  @action
+  checkThresholdValidity(event) {
+    const newThreshold = Number(event.target.value);
+    if (
+      !isInteger(newThreshold) ||
+      newThreshold < 0 ||
+      newThreshold > 100 ||
+      this.args.unavailableThresholds.includes(newThreshold)
+    ) {
+      this.thresholdStatus = 'error';
+      this.threshold = null;
+      return;
+    }
+
+    this.thresholdStatus = 'success';
+    this.threshold = newThreshold;
+  }
+
+  @action
+  checkTitleValidity(event) {
+    const title = event.target.value.trim();
+
+    if (!title) {
+      this.titleStatus = 'error';
+      this.title = null;
+      return;
+    }
+    this.titleStatus = 'success';
+    this.title = title;
+  }
+
+  @action
+  checkMessageValidity(event) {
+    const message = event.target.value.trim();
+
+    if (!message) {
+      this.message = null;
+
+      this.messageError = 'Message vide.';
+      return;
+    }
+    this.messageError = null;
+    this.message = message;
   }
 }
