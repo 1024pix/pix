e01d6c3fb71eacafcd8168ec5c7b5ed1ac444e67
Andreia Pena <andreia.pena@pix.fr>
2023-02-22 16:45:06 +0100

    :sparkles: certif: use legalNoticeUrl from url service on footer

--- certif/app/components/layout/footer.js
+++ certif/app/components/layout/footer.js
@@ -3,9 +3,14 @@ import { inject as service } from '@ember/service';
 
 export default class Footer extends Component {
   @service intl;
+  @service url;
 
   get currentYear() {
     const currentYear = new Date().getFullYear();
     return this.intl.t('navigation.footer.current-year', { currentYear });
+  }
+
+  get legalNoticeUrl() {
+    return this.url.legalNoticeUrl;
   }
 }
