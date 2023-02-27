5c556a765d769fedd2d6c94f3cfe9e0a9537f09c
Marie-Hélène <marie-helene.rots@pix.fr>
2023-02-27 10:02:40 +0100

    feat(admin): add suppression fonctionnality on stages

--- admin/app/adapters/stage.js
+++ admin/app/adapters/stage.js
@@ -2,4 +2,11 @@ import ApplicationAdapter from './application';
 
 export default class StageAdapter extends ApplicationAdapter {
   namespace = 'api/admin';
+
+  deleteRecord(store, type, snapshot) {
+    const data = {};
+    const serializer = store.serializerFor(type.modelName);
+    serializer.serializeIntoHash(data, type, snapshot, { includeId: true, onlyInformation: true });
+    return this.ajax(this.buildURL(type.modelName, snapshot.id, snapshot, 'deleteRecord'), 'DELETE', { data: data });
+  }
 }
