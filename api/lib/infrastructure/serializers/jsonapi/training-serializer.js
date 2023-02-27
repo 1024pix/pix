dc73132d9105fbdc608e67cfb3594a538079e6a6
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-02-27 17:48:55 +0100

    feat(api): add training serialize for admin

--- api/lib/infrastructure/serializers/jsonapi/training-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/training-serializer.js
@@ -1,6 +1,72 @@
 const { Serializer, Deserializer } = require('jsonapi-serializer');
 
 module.exports = {
+  serializeForAdmin(training = {}, meta) {
+    return new Serializer('trainings', {
+      transform(training) {
+        const duration = training.duration;
+        duration.days = duration.days || 0;
+        duration.hours = duration.hours || 0;
+        duration.minutes = duration.minutes || 0;
+
+        return {
+          ...training,
+          triggers: training.triggers.map((trigger) => ({
+            ...trigger,
+            triggerTubes: trigger.triggerTubes.map((triggerTube) => ({
+              ...triggerTube,
+              tube: { ...triggerTube.tube },
+            })),
+          })),
+        };
+      },
+      attributes: [
+        'duration',
+        'link',
+        'locale',
+        'title',
+        'type',
+        'editorName',
+        'editorLogoUrl',
+        'triggers',
+        'targetProfileSummaries',
+      ],
+      triggers: {
+        ref: 'id',
+        attributes: ['threshold', 'triggerTubes', 'type'],
+        triggerTubes: {
+          ref: 'id',
+          attributes: ['level', 'tube'],
+          tube: {
+            ref: 'id',
+            attributes: ['name', 'practicalTitle', 'skills'],
+          },
+        },
+      },
+      targetProfileSummaries: {
+        ref: 'id',
+        ignoreRelationshipData: true,
+        nullIfMissing: true,
+        relationshipLinks: {
+          related: function (record, current, parent) {
+            return `/api/admin/trainings/${parent.id}/target-profile-summaries`;
+          },
+        },
+      },
+      typeForAttribute(attribute) {
+        switch (attribute) {
+          case 'triggerTubes':
+            return 'trigger-tubes';
+          case 'tube':
+            return 'tubes';
+          default:
+            return attribute;
+        }
+      },
+      meta,
+    }).serialize(training);
+  },
+
   serialize(training = {}, meta) {
     return new Serializer('trainings', {
       attributes: [
--- api/tests/tooling/domain-builder/factory/build-training.js
+++ api/tests/tooling/domain-builder/factory/build-training.js
@@ -12,6 +12,7 @@ module.exports = function buildTraining({
   targetProfileIds = [1],
   editorName = 'Ministère education nationale',
   editorLogoUrl = 'https://images.pix.fr/contenu-formatif/editeur/editor_logo_url.svg',
+  triggers,
 } = {}) {
   return new Training({
     id,
@@ -23,5 +24,6 @@ module.exports = function buildTraining({
     targetProfileIds,
     editorName,
     editorLogoUrl,
+    triggers,
   });
 };
