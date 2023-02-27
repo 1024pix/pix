d1cbc0a9320b10f5f28dfeaf444a175d0773d1a4
Quentin Lebouc <quentin.lebouc@pix.fr>
2023-02-27 15:44:54 +0100

    :sparkles: allow access to sessions import only to not isScoManagingStudent POC
    Co-authored-by: Andreia Pena <andreia.pena@pix.fr>
    Co-authored-by: Jérémy Pluquet <jemyplu@gmail.com>

--- certif/app/routes/authenticated/sessions/import.js
+++ certif/app/routes/authenticated/sessions/import.js
@@ -4,11 +4,15 @@ import Route from '@ember/routing/route';
 export default class ImportRoute extends Route {
   @service featureToggles;
   @service router;
+  @service currentUser;
 
   beforeModel() {
     const { isMassiveSessionManagementEnabled } = this.featureToggles.featureToggles;
 
-    if (!isMassiveSessionManagementEnabled) {
+    if (
+      !isMassiveSessionManagementEnabled ||
+      this.currentUser.currentAllowedCertificationCenterAccess.isScoManagingStudents
+    ) {
       return this.router.replaceWith('authenticated.sessions.list');
     }
   }
