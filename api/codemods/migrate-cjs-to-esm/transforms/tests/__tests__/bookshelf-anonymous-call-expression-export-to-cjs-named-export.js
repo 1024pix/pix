ccc41605bb374c579cc1b59945334c5cecc7e1d0
Matthias FERRAINA <matthias.ferraina@pix.fr>
2023-02-28 16:13:17 +0100

    feat(api): add codemod to migrate bookshelf cjs anonymous call expression exports to cjs named exports

---
+++ api/codemods/migrate-cjs-to-esm/transforms/tests/__tests__/bookshelf-anonymous-call-expression-export-to-cjs-named-export.js
@@ -0,0 +1,5 @@
+const { defineTests } = require('../__testutils__/defineTests');
+
+describe('bookshelf-anonymous-call-expression-export-to-cjs-named-export', () => {
+  defineTests(__dirname, 'bookshelf-anonymous-call-expression-export-to-cjs-named-export');
+});
