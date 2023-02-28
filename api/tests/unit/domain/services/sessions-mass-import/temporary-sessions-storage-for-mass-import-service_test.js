2a5ba0a2c030908b28e6d32caf1ebad5ad769c19
Andreia Pena <andreia.pena@pix.fr>
2023-02-28 09:52:30 +0100

    :sparkles: api: create get temporary storage method
    Co-authored-by: Jérémy Pluquet <jemyplu@gmail.com>

--- api/tests/unit/domain/services/sessions-mass-import/temporary-sessions-storage-for-mass-import-service_test.js
+++ api/tests/unit/domain/services/sessions-mass-import/temporary-sessions-storage-for-mass-import-service_test.js
@@ -19,4 +19,41 @@ describe('Unit | Domain | Services | sessions mass import', function () {
       expect(result).to.deep.equal([{ id: 1, name: 'session 1' }]);
     });
   });
+
+  describe('#getByKeyAndUserId', function () {
+    context('when key not exists', function () {
+      it('should return undefined', async function () {
+        // given
+        const key = 'key';
+        const userId = 123;
+
+        // when
+        const result = await temporarySessionsStorageForMassImport.getByKeyAndUserId({
+          cachedValidatedSessionsKey: key,
+          userId,
+        });
+
+        // then
+        expect(result).to.be.undefined;
+      });
+    });
+
+    context('when key exists', function () {
+      it('should get sessions accessible with cached validated sessions key and user id', async function () {
+        // given
+        const sessions = [{ id: 1, name: 'session 1' }];
+        const userId = '123';
+        const key = await temporarySessionsStorageForMassImport.save({ sessions, userId });
+
+        // when
+        const result = await temporarySessionsStorageForMassImport.getByKeyAndUserId({
+          cachedValidatedSessionsKey: key,
+          userId,
+        });
+
+        // then
+        expect(result).to.deep.equal([{ id: 1, name: 'session 1' }]);
+      });
+    });
+  });
 });
