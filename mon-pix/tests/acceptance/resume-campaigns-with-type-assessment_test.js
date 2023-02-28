6b6f4f5590b1af8c796116c6d5c2b473d1331534
Andreia Pena <andreia.pena@pix.fr>
2023-02-28 18:00:35 +0100

    :recycle: mon-pix: improve acceptance campaigns-with-type-assessment tests

--- mon-pix/tests/acceptance/resume-campaigns-with-type-assessment_test.js
+++ mon-pix/tests/acceptance/resume-campaigns-with-type-assessment_test.js
@@ -1,8 +1,8 @@
-import { click, fillIn, currentURL, visit } from '@ember/test-helpers';
+import { click, fillIn, currentURL } from '@ember/test-helpers';
+import { visit } from '@1024pix/ember-testing-library';
 import { module, test } from 'qunit';
 import { authenticate } from '../helpers/authentication';
 import { resumeCampaignOfTypeAssessmentByCode } from '../helpers/campaign';
-import { clickByLabel } from '../helpers/click-by-label';
 import { invalidateSession } from '../helpers/invalidate-session';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
@@ -22,25 +22,30 @@ module('Acceptance | Campaigns | Resume Campaigns with type Assessment', functio
     await resumeCampaignOfTypeAssessmentByCode(campaign.code, true);
   });
 
-  module('When the user is not logged', function (hooks) {
-    hooks.beforeEach(async function () {
+  module('When the user is not logged', function () {
+    test('should propose to signup', async function (assert) {
+      // given
       await invalidateSession();
-      await visit(`/campagnes/${campaign.code}`);
-      await clickByLabel('Je commence');
-    });
+      const screen = await visit(`/campagnes/${campaign.code}`);
 
-    test('should propose to signup', function (assert) {
+      // when
+      await click(screen.getByRole('button', { name: 'Je commence' }));
+
+      // then
       assert.ok(currentURL().includes('/inscription'));
     });
 
     test('should redirect to campaign participation when user logs in', async function (assert) {
       // given
-      await click('[href="/connexion"]');
-      await fillIn('#login', studentInfo.email);
-      await fillIn('#password', studentInfo.password);
+      await invalidateSession();
+      const screen = await visit(`/campagnes/${campaign.code}`);
+      await click(screen.getByRole('button', { name: 'Je commence' }));
+      await click(screen.getByRole('link', { name: 'connectez-vous à votre compte' }));
+      await fillIn(screen.getByRole('textbox', { name: 'Adresse e-mail ou identifiant' }), studentInfo.email);
+      await fillIn(screen.getByLabelText('Mot de passe'), studentInfo.password);
 
       // when
-      await click('.sign-form-body__bottom-button button');
+      await click(screen.getByRole('button', { name: 'Je me connecte' }));
 
       // then
       assert.ok(currentURL().includes('/assessments/'));
@@ -50,7 +55,7 @@ module('Acceptance | Campaigns | Resume Campaigns with type Assessment', functio
   module('When user is logged', function () {
     module('When there is no more questions', function () {
       test('should redirect to last checkpoint page', async function (assert) {
-        // when
+        // given & when
         await visit(`/campagnes/${campaign.code}`);
 
         // then
@@ -61,8 +66,8 @@ module('Acceptance | Campaigns | Resume Campaigns with type Assessment', functio
     module('When user has completed his campaign participation', function () {
       test('should redirect directly to results', async function (assert) {
         // given
-        await visit(`/campagnes/${campaign.code}`);
-        await clickByLabel('Voir mes résultats');
+        const screen = await visit(`/campagnes/${campaign.code}`);
+        await click(screen.getByRole('link', { name: 'Voir mes résultats' }));
 
         // when
         await visit(`/campagnes/${campaign.code}`);
@@ -75,9 +80,9 @@ module('Acceptance | Campaigns | Resume Campaigns with type Assessment', functio
     module('When user has already send his results', function () {
       test('should redirect directly to shared results', async function (assert) {
         // given
-        await visit(`/campagnes/${campaign.code}`);
-        await clickByLabel('Voir mes résultats');
-        await clickByLabel("J'envoie mes résultats");
+        const screen = await visit(`/campagnes/${campaign.code}`);
+        await click(screen.getByRole('link', { name: 'Voir mes résultats' }));
+        await click(screen.getByRole('button', { name: "J'envoie mes résultats" }));
 
         // when
         await visit(`/campagnes/${campaign.code}`);
@@ -103,8 +108,8 @@ module('Acceptance | Campaigns | Resume Campaigns with type Assessment', functio
 
       test('should display results page', async function (assert) {
         // when
-        await visit(`/campagnes/${campaign.code}`);
-        await clickByLabel('Voir mes résultats');
+        const screen = await visit(`/campagnes/${campaign.code}`);
+        await click(screen.getByRole('link', { name: 'Voir mes résultats' }));
 
         // then
         assert.ok(currentURL().includes(`/campagnes/${campaign.code}/evaluation/resultats`));
