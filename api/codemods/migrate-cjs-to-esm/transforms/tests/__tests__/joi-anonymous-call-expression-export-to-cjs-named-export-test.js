c1d69b68cd1636223d9708a14488d5555cf3421e
Matthias FERRAINA <matthias.ferraina@pix.fr>
2023-02-28 17:19:21 +0100

    feat(api): add codemod to migrate joi cjs anonymous call expression exports to cjs named exports

---
+++ api/codemods/migrate-cjs-to-esm/transforms/tests/__tests__/joi-anonymous-call-expression-export-to-cjs-named-export-test.js
@@ -0,0 +1,6 @@
+
+const { defineTests } = require('../__testutils__/defineTests');
+
+describe('joi-anonymous-call-expression-export-to-cjs-named-export', () => {
+  defineTests(__dirname, 'joi-anonymous-call-expression-export-to-cjs-named-export');
+});
