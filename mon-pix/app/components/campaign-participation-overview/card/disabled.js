8aea199a2ac1aadc3186576eff3519ed433ab3c4
Laura Bergoens <laura.bergoens@pix.fr>
2023-03-02 16:41:31 +0100

    move logic of conversion stage/star in front for campaign overview

--- mon-pix/app/components/campaign-participation-overview/card/disabled.js
+++ mon-pix/app/components/campaign-participation-overview/card/disabled.js
@@ -4,4 +4,12 @@ export default class Disabled extends Component {
   get hasStages() {
     return this.args.model.totalStagesCount > 0;
   }
+
+  get count() {
+    return this.args.model.validatedStagesCount - 1;
+  }
+
+  get total() {
+    return this.args.model.totalStagesCount - 1;
+  }
 }
