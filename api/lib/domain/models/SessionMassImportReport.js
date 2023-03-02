686dd8394ad106a9ceedfe52204b88a5e4e2a3cd
Andreia Pena <andreia.pena@pix.fr>
2023-03-02 17:54:04 +0100

    :sparkles: api: return errors report
    Co-authored-by: Jérémy Pluquet <jemyplu@gmail.com>
    Co-authored-by: Alexandre Coin <alexandre.pc.coin@gmail.com>
    Co-authored-by: Quentin Lebouc <quentin.lebouc@pix.fr>
    Co-authored-by: Marianne Bost <marianne.bost@pix.fr>

--- api/lib/domain/models/SessionMassImportReport.js
+++ api/lib/domain/models/SessionMassImportReport.js
@@ -1,9 +1,16 @@
 class SessionMassImportReport {
-  constructor({ cachedValidatedSessionsKey, sessionsCount, sessionsWithoutCandidatesCount, candidatesCount } = {}) {
+  constructor({
+    cachedValidatedSessionsKey,
+    sessionsCount,
+    sessionsWithoutCandidatesCount,
+    candidatesCount,
+    errorsReport,
+  } = {}) {
     this.cachedValidatedSessionsKey = cachedValidatedSessionsKey;
     this.sessionsCount = sessionsCount;
     this.sessionsWithoutCandidatesCount = sessionsWithoutCandidatesCount;
     this.candidatesCount = candidatesCount;
+    this.errorsReport = errorsReport;
   }
 }
 
--- api/tests/unit/domain/usecases/sessions-mass-import/validate-sessions_test.js
+++ api/tests/unit/domain/usecases/sessions-mass-import/validate-sessions_test.js
@@ -1,13 +1,10 @@
-const { domainBuilder, expect, catchErr, sinon } = require('../../../../test-helper');
+const { domainBuilder, expect, sinon } = require('../../../../test-helper');
 const validateSessions = require('../../../../../lib/domain/usecases/sessions-mass-import/validate-sessions');
-const { InvalidCertificationCandidate } = require('../../../../../lib/domain/errors');
 const sessionCodeService = require('../../../../../lib/domain/services/session-code-service');
 const Session = require('../../../../../lib/domain/models/Session');
-const certificationCpfService = require('../../../../../lib/domain/services/certification-cpf-service');
 const sessionsImportValidationService = require('../../../../../lib/domain/services/sessions-mass-import/sessions-import-validation-service');
 const temporarySessionsStorageForMassImportService = require('../../../../../lib/domain/services/sessions-mass-import/temporary-sessions-storage-for-mass-import-service');
 const { CpfBirthInformationValidation } = require('../../../../../lib/domain/services/certification-cpf-service');
-const { UnprocessableEntityError } = require('../../../../../lib/application/http-errors');
 const CertificationCandidate = require('../../../../../lib/domain/models/CertificationCandidate');
 
 describe('Unit | UseCase | sessions-mass-import | validate-sessions', function () {
@@ -17,7 +14,9 @@ describe('Unit | UseCase | sessions-mass-import | validate-sessions', function (
   let certificationCenter;
   let certificationCenterRepository;
   let certificationCandidateRepository;
+  let certificationCourseRepository;
   let sessionRepository;
+  let complementaryCertificationRepository;
 
   beforeEach(function () {
     accessCode = 'accessCode';
@@ -28,75 +27,74 @@ describe('Unit | UseCase | sessions-mass-import | validate-sessions', function (
       name: certificationCenterName,
     });
     certificationCenterRepository = { get: sinon.stub() };
-    certificationCandidateRepository = { saveInSession: sinon.stub(), deleteBySessionId: sinon.stub() };
-    sessionRepository = { save: sinon.stub(), isSessionExisting: sinon.stub() };
+    complementaryCertificationRepository = { getByLabel: sinon.stub() };
+    certificationCourseRepository = sinon.stub();
+    sessionRepository = sinon.stub();
     sinon.stub(sessionCodeService, 'getNewSessionCode');
-    sinon.stub(certificationCpfService, 'getBirthInformation');
     sessionCodeService.getNewSessionCode.returns(accessCode);
-    certificationCenterRepository.get.withArgs(certificationCenterId).resolves(certificationCenter);
-    sessionsImportValidationService.validateSession = sinon.stub();
     sessionsImportValidationService.getValidatedCandidateBirthInformation = sinon.stub();
+    sessionsImportValidationService.validateSession = sinon.stub();
     temporarySessionsStorageForMassImportService.save = sinon.stub();
+    certificationCenterRepository.get.withArgs(certificationCenterId).resolves(certificationCenter);
   });
 
-  context('when sessions are valid', function () {
-    context('when user has certification center membership', function () {
-      it('should validate every session one by one and return a sessionsMassImportReport', async function () {
-        // given
-        const userId = 1234;
-        const cachedValidatedSessionsKey = 'uuid';
-        const validSessionData = createValidSessionData();
-        const sessions = [
-          {
-            ...validSessionData,
-            room: 'Salle 1',
-          },
-          {
-            ...validSessionData,
-            room: 'Salle 2',
-          },
-        ];
+  context('when sessions and candidates are valid', function () {
+    it('return a sessions report', async function () {
+      // given
+      const userId = 1234;
+      const cachedValidatedSessionsKey = 'uuid';
+      const validSessionData = createValidSessionData();
+      const sessions = [
+        {
+          ...validSessionData,
+          room: 'Salle 1',
+        },
+        {
+          ...validSessionData,
+          room: 'Salle 2',
+        },
+      ];
 
-        const expectedSessions = [
-          new Session({
-            ...sessions[0],
-            certificationCenterId,
-            certificationCenter: certificationCenterName,
-            accessCode,
-            supervisorPassword: sinon.match(/^[2346789BCDFGHJKMPQRTVWXY]{5}$/),
-          }),
-          new Session({
-            ...sessions[1],
-            certificationCenterId,
-            certificationCenter: certificationCenterName,
-            accessCode,
-            supervisorPassword: sinon.match(/^[2346789BCDFGHJKMPQRTVWXY]{5}$/),
-          }),
-        ];
+      temporarySessionsStorageForMassImportService.save.resolves(cachedValidatedSessionsKey);
 
-        temporarySessionsStorageForMassImportService.save.resolves(cachedValidatedSessionsKey);
+      // when
+      const sessionsMassImportReport = await validateSessions({
+        sessions,
+        userId,
+        certificationCenterId,
+        certificationCenterRepository,
+        sessionRepository,
+      });
 
-        // when
-        const sessionsMassImportReport = await validateSessions({
-          sessions,
-          userId,
+      // then
+      const expectedSessions = [
+        new Session({
+          ...sessions[0],
           certificationCenterId,
-          certificationCenterRepository,
-          sessionRepository,
-        });
+          certificationCenter: certificationCenterName,
+          accessCode,
+          supervisorPassword: sinon.match(/^[2346789BCDFGHJKMPQRTVWXY]{5}$/),
+        }),
+        new Session({
+          ...sessions[1],
+          certificationCenterId,
+          certificationCenter: certificationCenterName,
+          accessCode,
+          supervisorPassword: sinon.match(/^[2346789BCDFGHJKMPQRTVWXY]{5}$/),
+        }),
+      ];
 
-        // then
-        expect(temporarySessionsStorageForMassImportService.save).to.have.been.calledOnceWith({
-          sessions: expectedSessions,
-          userId,
-        });
+      expect(temporarySessionsStorageForMassImportService.save).to.have.been.calledOnceWith({
+        sessions: expectedSessions,
+        userId,
+      });
 
-        expect(sessionsMassImportReport).to.deep.equal({
-          cachedValidatedSessionsKey,
-          sessionsCount: 2,
-          sessionsWithoutCandidatesCount: 2,
-          candidatesCount: 0,
-        });
+      expect(sessionsMassImportReport).to.deep.equal({
+        cachedValidatedSessionsKey,
+        sessionsCount: 2,
+        sessionsWithoutCandidatesCount: 2,
+        candidatesCount: 0,
+        errorsReport: [],
       });
     });
 
@@ -119,6 +117,33 @@ describe('Unit | UseCase | sessions-mass-import | validate-sessions', function (
           },
         ];
 
+        const cpfBirthInformationValidation1 = CpfBirthInformationValidation.success({ ...candidate1 });
+        const cpfBirthInformationValidation2 = CpfBirthInformationValidation.success({ ...candidate2 });
+        const cpfBirthInformationValidation3 = CpfBirthInformationValidation.success({ ...candidate3 });
+        sessionsImportValidationService.getValidatedCandidateBirthInformation
+          .onCall(0)
+          .resolves({ cpfBirthInformation: cpfBirthInformationValidation1 });
+        sessionsImportValidationService.getValidatedCandidateBirthInformation
+          .onCall(1)
+          .resolves({ cpfBirthInformation: cpfBirthInformationValidation2 });
+        sessionsImportValidationService.getValidatedCandidateBirthInformation
+          .onCall(2)
+          .resolves({ cpfBirthInformation: cpfBirthInformationValidation3 });
+
+        temporarySessionsStorageForMassImportService.save.resolves(cachedValidatedSessionsKey);
+
+        // when
+        const sessionsMassImportReport = await validateSessions({
+          sessions,
+          userId,
+          certificationCenterId,
+          certificationCenterRepository,
+          certificationCandidateRepository,
+          certificationCourseRepository,
+          sessionRepository,
+        });
+
+        // then
         const expectedSessions = [
           new Session({
             ...sessions[0],
@@ -145,48 +170,25 @@ describe('Unit | UseCase | sessions-mass-import | validate-sessions', function (
           }),
         ];
 
-        const cpfBirthInformationValidation1 = CpfBirthInformationValidation.success({ ...candidate1 });
-        const cpfBirthInformationValidation2 = CpfBirthInformationValidation.success({ ...candidate2 });
-        const cpfBirthInformationValidation3 = CpfBirthInformationValidation.success({ ...candidate3 });
-        sessionsImportValidationService.getValidatedCandidateBirthInformation
-          .onCall(0)
-          .resolves(cpfBirthInformationValidation1);
-        sessionsImportValidationService.getValidatedCandidateBirthInformation
-          .onCall(1)
-          .resolves(cpfBirthInformationValidation2);
-        sessionsImportValidationService.getValidatedCandidateBirthInformation
-          .onCall(2)
-          .resolves(cpfBirthInformationValidation3);
-
-        temporarySessionsStorageForMassImportService.save.resolves(cachedValidatedSessionsKey);
-
-        // when
-        const result = await validateSessions({
-          sessions,
-          userId,
-          certificationCenterId,
-          certificationCenterRepository,
-          certificationCandidateRepository,
-          sessionRepository,
-        });
-
-        // then
         expect(temporarySessionsStorageForMassImportService.save).to.have.been.calledOnceWithExactly({
           sessions: expectedSessions,
           userId,
         });
-        expect(result).to.deep.equal({
+        expect(sessionsMassImportReport).to.deep.equal({
           cachedValidatedSessionsKey,
           sessionsCount: 2,
           sessionsWithoutCandidatesCount: 0,
           candidatesCount: 3,
+          errorsReport: [],
         });
       });
     });
   });
-  context('when at least one of the sessions is not valid', function () {
-    it('should throw an error', async function () {
+
+  context('when session or candidate information is not valid', function () {
+    it('should not save in temporary storage', async function () {
       // given
+      sessionsImportValidationService.validateSession.resolves(['Veuillez indiquer un nom de site.']);
       const validSessionData = createValidSessionData();
 
       const sessions = [
@@ -196,10 +198,8 @@ describe('Unit | UseCase | sessions-mass-import | validate-sessions', function (
         },
       ];
 
-      sessionsImportValidationService.validateSession.rejects(new UnprocessableEntityError());
-
       // when
-      const error = await catchErr(validateSessions)({
+      await validateSessions({
         sessions,
         certificationCenterId,
         certificationCenterRepository,
@@ -207,37 +207,47 @@ describe('Unit | UseCase | sessions-mass-import | validate-sessions', function (
       });
 
       // then
-      expect(error).to.be.an.instanceOf(UnprocessableEntityError);
+      expect(temporarySessionsStorageForMassImportService.save).to.not.have.been.called;
     });
-  });
 
-  context('when at least one of the candidates is not valid', function () {
-    it('should throw an error', async function () {
-      // given
-      const validSessionData = createValidSessionData();
-      const candidate = createValidCandidateData();
+    context('when at least one of the sessions is not valid', function () {
+      it('should return sessionsMassImportReport', async function () {
+        // given
+        const validSessionData = createValidSessionData();
+        const candidate = createValidCandidateData();
 
-      const sessions = [
-        {
-          ...validSessionData,
-          certificationCandidates: [{ ...candidate, name: null }],
-        },
-      ];
+        const sessions = [
+          {
+            ...validSessionData,
+            address: null,
+            certificationCandidates: [{ ...candidate, name: null }],
+          },
+        ];
 
-      sessionsImportValidationService.getValidatedCandidateBirthInformation.rejects(
-        new InvalidCertificationCandidate({ error: { key: '', why: '' } })
-      );
+        sessionsImportValidationService.validateSession.resolves(['Veuillez indiquer un nom de site.']);
+        sessionsImportValidationService.getValidatedCandidateBirthInformation.resolves({
+          certificationCandidateErrors: ['lastName required'],
+        });
 
-      // when
-      const error = await catchErr(validateSessions)({
-        sessions,
-        certificationCenterId,
-        certificationCenterRepository,
-        sessionRepository,
+        // when
+        const sessionsMassImportReport = await validateSessions({
+          sessions,
+          certificationCenterId,
+          certificationCenterRepository,
+          sessionRepository,
+        });
+
+        // then
+        expect(sessionsMassImportReport).to.deep.equal({
+          cachedValidatedSessionsKey: undefined,
+          sessionsCount: 1,
+          sessionsWithoutCandidatesCount: 0,
+          candidatesCount: 1,
+          errorsReport: ['Veuillez indiquer un nom de site.', 'lastName required'],
+        });
+
+        expect(complementaryCertificationRepository.getByLabel).to.not.have.been.called;
       });
-
-      // then
-      expect(error).to.be.an.instanceOf(InvalidCertificationCandidate);
     });
   });
 });
