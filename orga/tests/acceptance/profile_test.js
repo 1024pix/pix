990dc2d56a6b88dd49a3509d233bbe803a499e98
Aurelie Crouillebois <aurelie.crouillebois@gmail.com>
2023-03-03 16:46:41 +0100

    feat(orga): add a breadcrumb to profile page

--- orga/tests/acceptance/profile_test.js
+++ orga/tests/acceptance/profile_test.js
@@ -1,15 +1,18 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
-import { clickByName } from '@1024pix/ember-testing-library';
+import { visit, currentURL, click } from '@ember/test-helpers';
+import { clickByName, visit as visitScreen } from '@1024pix/ember-testing-library';
 import { setupApplicationTest } from 'ember-qunit';
 import authenticateSession from '../helpers/authenticate-session';
 import { createUserWithMembershipAndTermsOfServiceAccepted, createPrescriberByUser } from '../helpers/test-init';
+import { within } from '@testing-library/dom';
 
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import setupIntl from '../helpers/setup-intl';
 
 module('Acceptance | Campaign Profile', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
+  setupIntl(hooks);
 
   hooks.beforeEach(async () => {
     const user = createUserWithMembershipAndTermsOfServiceAccepted();
@@ -18,13 +21,29 @@ module('Acceptance | Campaign Profile', function (hooks) {
     await authenticateSession(user.id);
   });
 
-  test('it allows user to return to campaign activity page', async function (assert) {
+  test('it should go to campaigns', async function (assert) {
+    // given
     server.create('campaign', { id: 1 });
     server.create('campaignProfile', { campaignId: 1, campaignParticipationId: 1 });
 
     // when
-    await visit('/campagnes/1/profils/1');
-    await clickByName('Retour');
+    await visitScreen('/campagnes/1/profils/1');
+    await click(
+      within(document.querySelector('main')).getByRole('link', { name: this.intl.t('navigation.main.campaigns') })
+    );
+
+    // then
+    assert.strictEqual(currentURL(), '/campagnes/les-miennes');
+  });
+
+  test('it should go to CampagneEtPrairie', async function (assert) {
+    // given
+    server.create('campaign', { id: 1, name: 'CampagneEtPrairie' });
+    server.create('campaignProfile', { campaignId: 1, campaignParticipationId: 1 });
+
+    // when
+    await visitScreen('/campagnes/1/profils/1');
+    await clickByName('CampagneEtPrairie');
 
     // then
     assert.strictEqual(currentURL(), '/campagnes/1');
