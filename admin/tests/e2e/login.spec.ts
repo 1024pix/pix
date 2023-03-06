cb48d6b0c529d07b3d4b3f66c44387d33e0478e9
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2023-03-06 14:41:48 +0100

    Add logout steps in login test

--- admin/tests/e2e/login.spec.ts
+++ admin/tests/e2e/login.spec.ts
@@ -1,14 +1,21 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from './test';
 
 test.describe('Login', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
   })
 
-  test('s\'identifier en tant que super admin', async ({ page }) => {
+  test('s\'identifier puis se déconnecter', async ({ menuNavigation, page }) => {
+    await expect(page).toHaveURL('/login');
+
     await page.getByRole('textbox', { name: 'Adresse e-mail' }).fill('superadmin@example.net');
     await page.getByRole('textbox', { name: 'Mot de passe' }).fill('pix123');
     await page.getByRole('button', { name: 'Je me connecte' }).click();
-    await expect(page).toHaveURL('/organizations/list');
+
+    await expect(page).not.toHaveURL('/login');
+
+    await menuNavigation.click('Se déconnecter');
+
+    await expect(page).toHaveURL('/login');
   });
 })
