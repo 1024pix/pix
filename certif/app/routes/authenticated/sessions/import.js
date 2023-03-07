c03b266dafa9669b115d1bd73815e3944c148373
Andreia Pena <andreia.pena@pix.fr>
2023-03-07 11:05:38 +0100

    :sparkles: certif : redirect to session list for org tld with english
    current lang

--- certif/app/routes/authenticated/sessions/import.js
+++ certif/app/routes/authenticated/sessions/import.js
@@ -5,13 +5,20 @@ export default class ImportRoute extends Route {
   @service featureToggles;
   @service router;
   @service currentUser;
+  @service currentDomain;
+  @service intl;
 
   beforeModel() {
     const { isMassiveSessionManagementEnabled } = this.featureToggles.featureToggles;
 
+    const topLevelDomain = this.currentDomain.getExtension();
+    const currentLanguage = this.intl.t('current-lang');
+    const isOrgTldAndEnglishCurrentLanguage = topLevelDomain === 'org' && currentLanguage === 'en';
+
     if (
       !isMassiveSessionManagementEnabled ||
-      this.currentUser.currentAllowedCertificationCenterAccess.isScoManagingStudents
+      this.currentUser.currentAllowedCertificationCenterAccess.isScoManagingStudents ||
+      isOrgTldAndEnglishCurrentLanguage
     ) {
       return this.router.replaceWith('authenticated.sessions.list');
     }
