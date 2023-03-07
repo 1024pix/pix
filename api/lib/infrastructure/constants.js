a540a93a88a8f5d763d1b0cdb1f7e0d91ee535a0
Matthias FERRAINA <matthias.ferraina@pix.fr>
2023-03-07 12:33:52 +0100

    chore(api): fix files
    Co-authored-by: Pierre TOP (TOPI) <ptop@octo.com>

--- api/lib/infrastructure/constants.js
+++ api/lib/infrastructure/constants.js
@@ -1,7 +1,11 @@
 const settings = require('../config.js');
 
+const CONCURRENCY_HEAVY_OPERATIONS = settings.infra.concurrencyForHeavyOperations;
+const CHUNK_SIZE_CAMPAIGN_RESULT_PROCESSING = settings.infra.chunkSizeForCampaignResultProcessing;
+const ORGANIZATION_LEARNER_CHUNK_SIZE = settings.infra.chunkSizeForOrganizationLearnerDataProcessing;
+
 module.exports = {
-  CONCURRENCY_HEAVY_OPERATIONS: settings.infra.concurrencyForHeavyOperations,
-  CHUNK_SIZE_CAMPAIGN_RESULT_PROCESSING: settings.infra.chunkSizeForCampaignResultProcessing,
-  ORGANIZATION_LEARNER_CHUNK_SIZE: settings.infra.chunkSizeForOrganizationLearnerDataProcessing,
+  CONCURRENCY_HEAVY_OPERATIONS,
+  CHUNK_SIZE_CAMPAIGN_RESULT_PROCESSING,
+  ORGANIZATION_LEARNER_CHUNK_SIZE,
 };
