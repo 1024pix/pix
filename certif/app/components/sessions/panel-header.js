1cc6b94ed007ea71aa3b9b444e0f49b1ee63cba1
Andreia Pena <andreia.pena@pix.fr>
2023-03-07 14:27:25 +0100

    :sparkles: certif : do not display button for org tld with english
    current lang in panel-header

--- certif/app/components/sessions/panel-header.js
+++ certif/app/components/sessions/panel-header.js
@@ -4,11 +4,18 @@ import { inject as service } from '@ember/service';
 export default class PanelHeader extends Component {
   @service featureToggles;
   @service currentUser;
+  @service currentDomain;
+  @service intl;
 
   get shouldRenderImportTemplateButton() {
+    const topLevelDomain = this.currentDomain.getExtension();
+    const currentLanguage = this.intl.t('current-lang');
+    const isOrgTldAndEnglishCurrentLanguage = topLevelDomain === 'org' && currentLanguage === 'en';
+
     return (
       this.featureToggles.featureToggles.isMassiveSessionManagementEnabled &&
-      !this.currentUser.currentAllowedCertificationCenterAccess.isScoManagingStudents
+      !this.currentUser.currentAllowedCertificationCenterAccess.isScoManagingStudents &&
+      !isOrgTldAndEnglishCurrentLanguage
     );
   }
 }
