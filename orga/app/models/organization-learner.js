0d1a2b8152e6fc685b6f93862848a9396e19e283
Alexandre Monney <alexandre.monney77@gmail.com>
2023-03-07 17:16:11 +0100

    feat(orga): improve sco participant header with certificability information

--- orga/app/models/organization-learner.js
+++ orga/app/models/organization-learner.js
@@ -1,12 +1,12 @@
 import Model, { attr } from '@ember-data/model';
 import { inject as service } from '@ember/service';
 
-export const CONNECTION_TYPES = {
-  empty: 'components.connection-types.empty',
-  none: 'components.connection-types.none',
-  email: 'components.connection-types.email',
-  identifiant: 'components.connection-types.identifiant',
-  mediacentre: 'components.connection-types.mediacentre',
+const CONNECTION_TYPES = {
+  empty: 'pages.sco-organization-participants.connection-types.empty',
+  none: 'pages.sco-organization-participants.connection-types.none',
+  email: 'pages.sco-organization-participants.connection-types.email',
+  identifiant: 'pages.sco-organization-participants.connection-types.identifiant',
+  mediacentre: 'pages.sco-organization-participants.connection-types.mediacentre',
 };
 export default class OrganizationLearner extends Model {
   @service intl;
