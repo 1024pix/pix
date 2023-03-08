68f872a8f1a34f71c2923ac732d6594aacaf072f
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2023-03-08 17:25:36 +0100

    feat(api): build learning content tree in training trigger model

--- api/lib/domain/models/TrainingTrigger.js
+++ api/lib/domain/models/TrainingTrigger.js
@@ -13,12 +13,47 @@ class TrainingTrigger {
     }
     this.type = type;
     this.threshold = threshold;
-    this.areas = areas;
-    this.competences = competences;
-    this.thematics = thematics;
+    this.areas = areas.map((area) => new _Area({ ...area, competences, thematics, triggerTubes }));
   }
 }
 
 TrainingTrigger.types = types;
+
+class _Area {
+  constructor({ id, title, code, color, competences = [], thematics = [], triggerTubes = [] } = {}) {
+    this.id = id;
+    this.title = title;
+    this.code = code;
+    this.color = color;
+
+    this.competences = competences
+      .filter((competence) => competence.areaId === id)
+      .map((competence) => new _Competence({ ...competence, thematics, triggerTubes }));
+  }
+}
+
+class _Competence {
+  constructor({ id, name, index, thematics = [], triggerTubes = [] } = {}) {
+    this.id = id;
+    this.name = name;
+    this.index = index;
+
+    this.thematics = thematics
+      .filter((thematic) => thematic.competenceId === id)
+      .map((thematic) => new _Thematic({ ...thematic, triggerTubes }));
+  }
+}
+
+class _Thematic {
+  constructor({ id, name, index, triggerTubes = [] } = {}) {
+    this.id = id;
+    this.name = name;
+    this.index = index;
+
+    this.triggerTubes = triggerTubes
+      .filter((trainingTriggerTube) => trainingTriggerTube.tube.thematicId === id)
+      .map((trainingTriggerTube) => trainingTriggerTube.id);
+  }
+}
 
 module.exports = TrainingTrigger;
