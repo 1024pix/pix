4f8be37e47f1f1b501a98ff41d7c65641ee23ed2
Aurelie Crouillebois <aurelie.crouillebois@gmail.com>
2023-03-09 17:49:36 +0100

    fix(api): return 422 error on target profile creation with unknown org

--- api/lib/domain/usecases/create-target-profile.js
+++ api/lib/domain/usecases/create-target-profile.js
@@ -1,12 +1,21 @@
 const TargetProfileForCreation = require('../models/TargetProfileForCreation.js');
 const learningContentConversionService = require('../services/learning-content/learning-content-conversion-service.js');
-
+const { TargetProfileCannotBeCreated } = require('../errors');
 module.exports = async function createTargetProfile({
   targetProfileCreationCommand,
   domainTransaction,
   targetProfileRepository,
+  organizationRepository,
 }) {
   const targetProfileForCreation = TargetProfileForCreation.fromCreationCommand(targetProfileCreationCommand);
+  try {
+    await organizationRepository.get(targetProfileForCreation.ownerOrganizationId);
+  } catch (error) {
+    throw new TargetProfileCannotBeCreated(
+      `Cannot create target profile for non existant organization id: ${targetProfileForCreation.ownerOrganizationId}`
+    );
+  }
+
   const targetProfileId = await targetProfileRepository.createWithTubes({
     targetProfileForCreation,
     domainTransaction,
