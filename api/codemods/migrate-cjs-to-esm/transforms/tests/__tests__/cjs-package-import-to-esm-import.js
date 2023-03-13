1a805bd9979daa15415ed5b918e7ea390cc6cc52
Matthias FERRAINA <matthias.ferraina@pix.fr>
2023-03-13 17:24:04 +0100

    feat(api): create codemod for package import

---
+++ api/codemods/migrate-cjs-to-esm/transforms/tests/__tests__/cjs-package-import-to-esm-import.js
@@ -0,0 +1,5 @@
+const { defineTests } = require('../__testutils__/defineTests');
+
+describe('cjs-package-import-to-esm-import', () => {
+  defineTests(__dirname, 'cjs-package-import-to-esm-import');
+});
