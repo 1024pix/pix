ad83023f12c45452d28e49119478f91c5d564b3d
Alexandre Monney <alexandre.monney77@gmail.com>
2023-03-16 14:35:31 +0100

    feat(orga): create participation row component

---
+++ orga/tests/unit/components/organization-learner/activity/participation-row_test.js
@@ -0,0 +1,33 @@
+import { module, test } from 'qunit';
+import createGlimmerComponent from '../../../../helpers/create-glimmer-component';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Component | OrganizationLearner | Activity::ParticipationRow', function (hooks) {
+  setupTest(hooks);
+
+  test('should return route to assessment participation detail', async function (assert) {
+    // given
+    const component = await createGlimmerComponent('component:organization-learner/activity/participation-row');
+    component.args.participation = {
+      campaignType: 'ASSESSMENT',
+    };
+    // when
+    const route = component.routeName;
+
+    // then
+    assert.strictEqual(route, 'authenticated.campaigns.participant-assessment');
+  });
+
+  test('should return route to profile collection participation detail', async function (assert) {
+    // given
+    const component = await createGlimmerComponent('component:organization-learner/activity/participation-row');
+    component.args.participation = {
+      campaignType: 'PROFILE_COLLECTION',
+    };
+    // when
+    const route = component.routeName;
+
+    // then
+    assert.strictEqual(route, 'authenticated.campaigns.participant-profile');
+  });
+});
