bdc4bfa57d10631143f251867ff956392e5c0179
Marie-Hélène <marie-helene.rots@pix.fr>
2023-03-16 16:55:11 +0100

    feat(api): add an unicity constraint to the campaigns codes

--- api/scripts/prod/swap-campaign-codes.js
+++ api/scripts/prod/swap-campaign-codes.js
@@ -1,12 +1,16 @@
 const { knex } = require('../../db/knex-database-connection');
+const { generate } = require('../../lib/domain/services/campaigns/campaign-code-generator');
 const DomainTransaction = require('../../lib/infrastructure/DomainTransaction');
+const campaignRepository = require('../../lib/infrastructure/repositories/campaign-repository');
 const { disconnect } = require('../../db/knex-database-connection');
 
 async function swapCampaignCodes(campaignId, otherCampaignId) {
+  const temporaryCode = await generate(campaignRepository);
   const { code: campaignCode } = await knex('campaigns').where({ id: campaignId }).first();
   const { code: otherCampaignCode } = await knex('campaigns').where({ id: otherCampaignId }).first();
 
   await DomainTransaction.execute(async (domainTransaction) => {
+    await domainTransaction.knexTransaction('campaigns').where({ id: otherCampaignId }).update({ code: temporaryCode });
     await domainTransaction.knexTransaction('campaigns').where({ id: campaignId }).update({ code: otherCampaignCode });
     await domainTransaction.knexTransaction('campaigns').where({ id: otherCampaignId }).update({ code: campaignCode });
   });
