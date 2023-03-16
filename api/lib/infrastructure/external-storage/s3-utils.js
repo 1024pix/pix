1d2a5238b1c8c55a3a450240261eac6386d60463
Matthias FERRAINA <matthias.ferraina@pix.fr>
2023-03-16 15:03:42 +0100

    chore(api): fix package imports

--- api/lib/infrastructure/external-storage/s3-utils.js
+++ api/lib/infrastructure/external-storage/s3-utils.js
@@ -1,21 +1,7 @@
-import @awsSdk/clientS3 from '@aws-sdk/client-s3';
+import { S3Client, ListObjectsV2Command, GetObjectCommand } from '@aws-sdk/client-s3';
+import { Upload } from '@aws-sdk/lib-storage';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
-const {
-  getSignedUrl
-} = @awsSdk/s3RequestPresigner;
-
-const {
-  Upload
-} = @awsSdk/libStorage;
-
-const {
-  S3Client,
-  ListObjectsV2Command,
-  GetObjectCommand
-} = @awsSdk/clientS3;
-
-import @awsSdk/libStorage from '@aws-sdk/lib-storage';
-import @awsSdk/s3RequestPresigner from '@aws-sdk/s3-request-presigner';
 import bluebird from 'bluebird';
 
 const getS3Client = function ({ accessKeyId, secretAccessKey, endpoint, region }) {
