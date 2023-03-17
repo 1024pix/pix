fbbf96d4a5ed0f303aaea3867b80a30cec674259
Antoine Ceol <antoine.ceol@gmail.com>
2023-03-17 21:45:07 +0100

    :sparkles: api: Handle empty sessions warning

--- certif/app/components/import/step-two-section.js
+++ certif/app/components/import/step-two-section.js
@@ -4,14 +4,32 @@ import { inject as service } from '@ember/service';
 export default class StepTwoSectionComponent extends Component {
   @service intl;
 
-  get translatedErrorReport() {
+  get translatedBlockingErrorReport() {
     return this.args.blockingErrorReports.map(({ line, code }) => ({
       line,
       message: _translatedErrorCodeToMessage(this.intl, code),
     }));
   }
+  get translatedNonBlockingErrorReport() {
+    return this.args.nonBlockingErrorReports.map(({ line, code }) => ({
+      line,
+      message: _translatedNonBlockingErrorCodeToMessage(this.intl, code),
+    }));
+  }
+
+  get blockingErrorReportsCount() {
+    return this.args.blockingErrorReports?.length;
+  }
+
+  get nonBlockingErrorReportsCount() {
+    return this.args.nonBlockingErrorReports?.length;
+  }
 }
 
 function _translatedErrorCodeToMessage(intl, code) {
   return intl.t(`pages.sessions.import.step-two.blocking-errors.errors.${code}`);
+}
+
+function _translatedNonBlockingErrorCodeToMessage(intl, code) {
+  return intl.t(`pages.sessions.import.step-two.non-blocking-errors.errors.${code}`);
 }
