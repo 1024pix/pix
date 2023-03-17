7fdcca5a2c64fcf3a7afe02ae0eeacdaafe13b4d
Antoine Ceol <antoine.ceol@gmail.com>
2023-03-17 12:51:30 +0100

    :recycle: api: Add methods to domain model

--- api/lib/domain/models/SessionMassImportReport.js
+++ api/lib/domain/models/SessionMassImportReport.js
@@ -1,16 +1,44 @@
 class SessionMassImportReport {
   constructor({
     cachedValidatedSessionsKey,
-    sessionsCount,
-    sessionsWithoutCandidatesCount,
-    candidatesCount,
-    errorsReport,
+    sessionsCount = 0,
+    sessionsWithoutCandidatesCount = 0,
+    candidatesCount = 0,
+    blockingErrorReports = [],
+    nonBlockingErrorReports = [],
   } = {}) {
     this.cachedValidatedSessionsKey = cachedValidatedSessionsKey;
     this.sessionsCount = sessionsCount;
     this.sessionsWithoutCandidatesCount = sessionsWithoutCandidatesCount;
     this.candidatesCount = candidatesCount;
-    this.errorsReport = errorsReport;
+    this.blockingErrorReports = blockingErrorReports;
+    this.nonBlockingErrorReports = nonBlockingErrorReports;
+  }
+
+  get isValid() {
+    return this.blockingErrorReports.length === 0;
+  }
+
+  addBlockingErrorReports(errors) {
+    if (errors?.length) {
+      this.blockingErrorReports.push(...errors);
+    }
+  }
+  addNonBlockingErrorReports(errors) {
+    if (errors?.length) {
+      this.nonBlockingErrorReports.push(...errors);
+    }
+  }
+
+  updateSessionsCounters(sessions) {
+    this.sessionsWithoutCandidatesCount = sessions.filter(
+      (session) => session.certificationCandidates.length === 0
+    ).length;
+    this.sessionsCount = sessions.length;
+    this.candidatesCount = sessions.reduce(
+      (currentCandidateCount, currentSession) => currentCandidateCount + currentSession.certificationCandidates.length,
+      0
+    );
   }
 }
 
