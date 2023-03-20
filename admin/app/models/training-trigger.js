312a480ce7e92dffc25b9663069da786a1b0cb72
Matthias FERRAINA <matthias.ferraina@pix.fr>
2023-03-20 17:07:15 +0100

    feat(admin): display training trigger tubes count

--- admin/app/models/training-trigger.js
+++ admin/app/models/training-trigger.js
@@ -4,6 +4,7 @@ export default class TrainingTrigger extends Model {
   @attr('string') type;
   @attr('number') trainingId;
   @attr('number') threshold;
+  @attr('number') tubesCount;
 
   @hasMany('area') areas;
 }
