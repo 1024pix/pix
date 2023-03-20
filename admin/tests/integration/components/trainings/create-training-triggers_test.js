9f6d0c8386683617c895df46e5a28252334f69a2
Matthias FERRAINA <matthias.ferraina@pix.fr>
2023-03-20 17:07:15 +0100

    feat(admin): display training trigger tubes count

--- admin/tests/integration/components/trainings/create-training-triggers_test.js
+++ admin/tests/integration/components/trainings/create-training-triggers_test.js
@@ -122,6 +122,7 @@ module('Integration | Component | Trainings::CreateTrainingTriggers', function (
         type: 'goal',
         areas: [area1, area2],
         threshold: 19,
+        tubesCount: 5,
       });
       this.set('training', {
         goalTrigger: trainingTrigger,
@@ -147,6 +148,7 @@ module('Integration | Component | Trainings::CreateTrainingTriggers', function (
         .dom(screen.queryByLabelText(this.intl.t('pages.trainings.training.triggers.goal.alternative-title')))
         .doesNotExist();
       assert.dom(screen.getByText('Seuil : 19%')).exists();
+      assert.dom(screen.getByText('5 sujets')).exists();
       assert.dom(screen.getByText('area1 code · area1 title')).exists();
       await clickByText('area1 code · area1 title');
       assert.dom(screen.getByText('competence1Area1 index competence1Area1 name')).exists();
@@ -281,6 +283,7 @@ module('Integration | Component | Trainings::CreateTrainingTriggers', function (
         type: 'prerequisite',
         areas: [area1, area2],
         threshold: 20,
+        tubesCount: 4,
       });
       this.set('training', {
         prerequisiteTrigger: trainingTrigger,
@@ -296,6 +299,7 @@ module('Integration | Component | Trainings::CreateTrainingTriggers', function (
         .dom(screen.queryByLabelText(this.intl.t('pages.trainings.training.triggers.prerequisite.alternative-title')))
         .doesNotExist();
       assert.dom(screen.getByText('Seuil : 20%')).exists();
+      assert.dom(screen.getByText('4 sujets')).exists();
       assert.dom(screen.getByText('area1 code · area1 title')).exists();
       await clickByText('area1 code · area1 title');
       assert.dom(screen.getByText('competence1Area1 index competence1Area1 name')).exists();
