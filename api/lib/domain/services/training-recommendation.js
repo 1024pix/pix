9a34674be56b24766670abc8178f61598a7de129
Matthias FERRAINA <matthias.ferraina@pix.fr>
2023-03-22 15:45:13 +0100

    feat(api): filter knowledge elements for given capped skills
    Co-authored-by: Vincent Hardouin <vincent.hardouin@pix.fr>

--- api/lib/domain/services/training-recommendation.js
+++ api/lib/domain/services/training-recommendation.js
@@ -2,6 +2,12 @@ function getCappedSkills({ skills, cappedLevel }) {
   return skills.filter((skill) => skill.difficulty <= cappedLevel);
 }
 
+function getCappedKnowledgeElements({ knowledgeElements, cappedSkills }) {
+  const skillIds = cappedSkills.map((skill) => skill.id);
+  return knowledgeElements.filter((knowledgeElement) => skillIds.includes(knowledgeElement.skillId));
+}
+
 module.exports = {
   getCappedSkills,
+  getCappedKnowledgeElements,
 };
