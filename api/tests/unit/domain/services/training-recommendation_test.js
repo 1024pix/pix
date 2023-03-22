b966c885682e64e801553c2c805616001a86c28c
Matthias FERRAINA <matthias.ferraina@pix.fr>
2023-03-22 15:45:13 +0100

    feat(api): filter knowledge elements for given capped skills
    Co-authored-by: Vincent Hardouin <vincent.hardouin@pix.fr>

--- api/tests/unit/domain/services/training-recommendation_test.js
+++ api/tests/unit/domain/services/training-recommendation_test.js
@@ -1,4 +1,7 @@
-const { getCappedSkills } = require('../../../../lib/domain/services/training-recommendation');
+const {
+  getCappedSkills,
+  getCappedKnowledgeElements,
+} = require('../../../../lib/domain/services/training-recommendation');
 const { expect, domainBuilder } = require('../../../test-helper');
 
 describe('Unit | Service | Training Recommendation', function () {
@@ -32,6 +35,45 @@ describe('Unit | Service | Training Recommendation', function () {
 
       // then
       expect(cappedSkills).to.be.empty;
+    });
+  });
+  describe('#getCappedKnowledgeElements', function () {
+    it('should return capped knowledge elements for given capped skills', function () {
+      // given
+      const skillLevel1 = domainBuilder.buildSkill({ id: 'recSkill1', difficulty: 1 });
+      const skillLevel2 = domainBuilder.buildSkill({ id: 'recSkill2', difficulty: 2 });
+
+      const cappedSkills = [skillLevel1, skillLevel2];
+
+      const knowledgeElement1 = domainBuilder.buildKnowledgeElement({ skillId: skillLevel1.id });
+      const knowledgeElement2 = domainBuilder.buildKnowledgeElement({ skillId: skillLevel2.id });
+      const knowledgeElement3 = domainBuilder.buildKnowledgeElement({ skillId: 'anotherSkillId' });
+
+      const knowledgeElements = [knowledgeElement1, knowledgeElement2, knowledgeElement3];
+
+      // when
+      const cappedKnowledgeElements = getCappedKnowledgeElements({ knowledgeElements, cappedSkills });
+
+      // then
+      expect(cappedKnowledgeElements).to.deep.equal([knowledgeElement1, knowledgeElement2]);
+    });
+
+    it('should return empty array when no knowledge elements corresponds for given capped skills', function () {
+      // given
+      const skillLevel1 = domainBuilder.buildSkill({ id: 'recSkill1', difficulty: 1 });
+      const skillLevel2 = domainBuilder.buildSkill({ id: 'recSkill2', difficulty: 2 });
+
+      const cappedSkills = [skillLevel1, skillLevel2];
+
+      const knowledgeElement3 = domainBuilder.buildKnowledgeElement({ skillId: 'anotherSkillId' });
+
+      const knowledgeElements = [knowledgeElement3];
+
+      // when
+      const cappedKnowledgeElements = getCappedKnowledgeElements({ knowledgeElements, cappedSkills });
+
+      // then
+      expect(cappedKnowledgeElements).to.be.empty;
     });
   });
 });
