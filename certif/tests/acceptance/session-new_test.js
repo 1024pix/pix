7d23489731519ad0c998e92428b42ddf0fb2d660
mcampourcy <mireille.campourcy@pix.fr>
2023-03-23 14:13:34 +0100

    :globe_with_meridians: certif: Move session labels in common part in translation files

--- certif/tests/acceptance/session-new_test.js
+++ certif/tests/acceptance/session-new_test.js
@@ -62,17 +62,17 @@ module('Acceptance | Session creation', function (hooks) {
       const screen = await visit('/sessions/creation');
       assert.dom(screen.getByRole('heading', { name: t('pages.sessions.new.title') })).exists();
       assert
-        .dom(screen.getByRole('textbox', { name: t('common.forms.session-labels.description') }))
+        .dom(screen.getByRole('textbox', { name: t('common.forms.session-labels.observations') }))
         .hasAttribute('maxLength', '350');
       assert
         .dom(screen.getByRole('button', { name: t('pages.sessions.new.actions.cancel-extra-information') }))
         .exists();
 
-      await fillIn(screen.getByRole('textbox', { name: t('common.forms.session-labels.address') }), 'My address');
-      await fillIn(screen.getByRole('textbox', { name: t('common.forms.session-labels.room') }), 'My room');
-      await fillIn(screen.getByRole('textbox', { name: t('common.forms.session-labels.examiner') }), 'My examiner');
+      await fillIn(screen.getByRole('textbox', { name: t('common.forms.session-labels.center-name') }), 'My address');
+      await fillIn(screen.getByRole('textbox', { name: t('common.forms.session-labels.room-name') }), 'My room');
+      await fillIn(screen.getByRole('textbox', { name: t('common.forms.session-labels.invigilator') }), 'My examiner');
       await fillIn(
-        screen.getByRole('textbox', { name: t('common.forms.session-labels.description') }),
+        screen.getByRole('textbox', { name: t('common.forms.session-labels.observations') }),
         'My description'
       );
       await setFlatpickrDate('#session-date', sessionDate);
