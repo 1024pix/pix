3bbbc16b6fa095f3eb17fac4e6b0c0514651b8b3
François de Metz <francois.de-metz@pix.fr>
2023-03-28 15:50:38 +0200

    Refactor the test

--- mon-pix/app/authenticators/gar.js
+++ mon-pix/app/authenticators/gar.js
@@ -3,11 +3,9 @@ import RSVP from 'rsvp';
 import { decodeToken } from 'mon-pix/helpers/jwt';
 
 export default class GarAuthenticator extends BaseAuthenticator {
-  authenticate(token) {
+  authenticate(token, tokenDecoder = decodeToken) {
     const token_type = 'bearer';
-    const decodedAccessToken = decodeToken(token);
-    const user_id = decodedAccessToken.user_id;
-    const source = decodedAccessToken.source;
+    const { user_id, source } = tokenDecoder(token);
     return RSVP.resolve({
       token_type,
       access_token: token,
