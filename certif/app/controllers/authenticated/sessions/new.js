5ca640b61e3b4ec2b391c567c71fcb1595c9f94f
Quentin Lebouc <quentin.lebouc@pix.fr>
2023-03-29 14:52:58 +0200

    :sparkles: certif: display an error when date or time are missing due to lack of required parameter on emberFlatPickr

--- certif/app/controllers/authenticated/sessions/new.js
+++ certif/app/controllers/authenticated/sessions/new.js
@@ -3,11 +3,15 @@ import { action } from '@ember/object';
 // eslint-disable-next-line ember/no-computed-properties-in-native-classes
 import { alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class SessionsNewController extends Controller {
   @alias('model') session;
   @service router;
   @service intl;
+
+  @tracked isSessionDateMissing;
+  @tracked isSessionTimeMissing;
 
   get pageTitle() {
     return this.intl.t('pages.sessions.new.extra-information');
@@ -16,6 +20,13 @@ export default class SessionsNewController extends Controller {
   @action
   async createSession(event) {
     event.preventDefault();
+    if (!this.session.date) {
+      this.isSessionDateMissing = true;
+    }
+    if (!this.session.time) {
+      this.isSessionTimeMissing = true;
+    }
+    if (this.isSessionDateMissing || this.isSessionTimeMissing) return;
     await this.session.save();
     this.router.transitionTo('authenticated.sessions.details', this.session.id);
   }
@@ -28,10 +39,12 @@ export default class SessionsNewController extends Controller {
   @action
   onDatePicked(selectedDates, lastSelectedDateFormatted) {
     this.session.date = lastSelectedDateFormatted;
+    this.isSessionDateMissing = false;
   }
 
   @action
   onTimePicked(selectedTimes, lastSelectedTimeFormatted) {
     this.session.time = lastSelectedTimeFormatted;
+    this.isSessionTimeMissing = false;
   }
 }
