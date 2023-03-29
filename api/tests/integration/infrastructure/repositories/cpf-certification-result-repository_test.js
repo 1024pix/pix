8e4e764c6f487715d41d1e840d831e0a3c69b661
P-Jeremy <jemyplu@gmail.com>
2023-03-29 16:52:00 +0200

    :sparkles: api: get cpf certification results count

--- api/tests/integration/infrastructure/repositories/cpf-certification-result-repository_test.js
+++ api/tests/integration/infrastructure/repositories/cpf-certification-result-repository_test.js
@@ -172,6 +172,174 @@ describe('Integration | Repository | CpfCertificationResult', function () {
     });
   });
 
+  describe('#countByTimeRange', function () {
+    it('should return the count of the certifications', async function () {
+      // given
+      const startDate = new Date('2022-01-01');
+      const endDate = new Date('2022-01-10');
+
+      const firstPublishedSessionId = databaseBuilder.factory.buildSession({ publishedAt: new Date('2022-01-04') }).id;
+      createCertificationCourseWithCompetenceMarks({ certificationCourseId: 545, sessionId: firstPublishedSessionId });
+      createCertificationCourseWithCompetenceMarks({ certificationCourseId: 245, sessionId: firstPublishedSessionId });
+      const secondPublishedSessionId = databaseBuilder.factory.buildSession({ publishedAt: new Date('2022-01-10') }).id;
+      createCertificationCourseWithCompetenceMarks({ certificationCourseId: 345, sessionId: secondPublishedSessionId });
+
+      await databaseBuilder.commit();
+
+      // when
+      const count = await cpfCertificationResultRepository.countByTimeRange({
+        startDate,
+        endDate,
+      });
+
+      // then
+      expect(count).to.equals(3);
+    });
+
+    context('when the certification course is not published', function () {
+      it('should return 0', async function () {
+        // given
+        const startDate = new Date('2022-01-01');
+        const endDate = new Date('2022-01-10');
+        const unpublishedSessionId = databaseBuilder.factory.buildSession({ publishedAt: null }).id;
+        createCertificationCourseWithCompetenceMarks({ sessionId: unpublishedSessionId, isPublished: false });
+        await databaseBuilder.commit();
+
+        // when
+        const count = await cpfCertificationResultRepository.countByTimeRange({
+          startDate,
+          endDate,
+        });
+
+        // then
+        expect(count).to.equal(0);
+      });
+    });
+
+    context('when the certification course is cancelled', function () {
+      it('should return 0', async function () {
+        // given
+        const startDate = new Date('2022-01-01');
+        const endDate = new Date('2022-01-10');
+
+        const publishedSessionId = databaseBuilder.factory.buildSession({
+          publishedAt: new Date('2022-01-08'),
+        }).id;
+        createCertificationCourseWithCompetenceMarks({
+          sessionId: publishedSessionId,
+          certificationCourseCancelled: true,
+        });
+        await databaseBuilder.commit();
+
+        // when
+        const count = await cpfCertificationResultRepository.countByTimeRange({
+          startDate,
+          endDate,
+        });
+
+        // then
+        expect(count).to.equal(0);
+      });
+    });
+
+    context('when the latest assessment result is not validated', function () {
+      it('should return 0', async function () {
+        // given
+        const startDate = new Date('2022-01-01');
+        const endDate = new Date('2022-01-10');
+
+        const publishedSessionId = databaseBuilder.factory.buildSession({
+          publishedAt: new Date('2022-01-08'),
+        }).id;
+        createCertificationCourseWithCompetenceMarks({
+          sessionId: publishedSessionId,
+          assessmentResultStatus: AssessmentResult.status.REJECTED,
+        });
+        await databaseBuilder.commit();
+
+        // when
+        const count = await cpfCertificationResultRepository.countByTimeRange({
+          startDate,
+          endDate,
+        });
+
+        // then
+        expect(count).to.equal(0);
+      });
+    });
+
+    context('when the session date is ouf of bounds', function () {
+      it('should return 0', async function () {
+        // given
+        const startDate = new Date('2022-01-01');
+        const endDate = new Date('2022-01-10');
+
+        const publishedSessionId = databaseBuilder.factory.buildSession({
+          publishedAt: new Date('2022-02-08'),
+        }).id;
+        createCertificationCourseWithCompetenceMarks({
+          sessionId: publishedSessionId,
+          assessmentResultStatus: AssessmentResult.status.VALIDATED,
+        });
+        await databaseBuilder.commit();
+
+        // when
+        const count = await cpfCertificationResultRepository.countByTimeRange({
+          startDate,
+          endDate,
+        });
+
+        // then
+        expect(count).to.equal(0);
+      });
+    });
+
+    context('when the certification course sex is not defined', function () {
+      it('should return 0', async function () {
+        // given
+        const startDate = new Date('2022-01-01');
+        const endDate = new Date('2022-01-10');
+        const publishedSessionId = databaseBuilder.factory.buildSession({
+          publishedAt: new Date('2022-02-08'),
+        }).id;
+        createCertificationCourseWithCompetenceMarks({ sessionId: publishedSessionId, sex: null });
+        await databaseBuilder.commit();
+
+        // when
+        const count = await cpfCertificationResultRepository.countByTimeRange({
+          startDate,
+          endDate,
+        });
+
+        // then
+        expect(count).to.equal(0);
+      });
+    });
+
+    context('when the certification course has already been exported', function () {
+      it('should return 0', async function () {
+        // given
+        const startDate = new Date('2022-01-01');
+        const endDate = new Date('2022-01-10');
+
+        const publishedSessionId = databaseBuilder.factory.buildSession({
+          publishedAt: new Date('2022-02-08'),
+        }).id;
+        createCertificationCourseWithCompetenceMarks({ sessionId: publishedSessionId, cpfImportStatus: 'SUCCESS' });
+        await databaseBuilder.commit();
+
+        // when
+        const count = await cpfCertificationResultRepository.countByTimeRange({
+          startDate,
+          endDate,
+        });
+
+        // then
+        expect(count).to.equal(0);
+      });
+    });
+  });
+
   describe('#findByBatchId', function () {
     context('when the certification course is planned for the batch', function () {
       it('should return an array of CpfCertificationResult ordered by certification course id', async function () {
@@ -282,26 +450,125 @@ describe('Integration | Repository | CpfCertificationResult', function () {
   });
 
   describe('#markCertificationToExport', function () {
-    it('should save batchId in cpfFilename', async function () {
-      // given
-      databaseBuilder.factory.buildCertificationCourse({ id: 123 });
-      databaseBuilder.factory.buildCertificationCourse({ id: 456 });
-      databaseBuilder.factory.buildCertificationCourse({ id: 789 });
-      await databaseBuilder.commit();
+    context('when there is no offset, limit', function () {
+      it('should save batchId in cpfFilename', async function () {
+        // given
+        const startDate = new Date('2022-01-01');
+        const endDate = new Date('2022-01-10');
 
-      // when
-      await cpfCertificationResultRepository.markCertificationToExport({
-        certificationCourseIds: [456, 789],
-        batchId: '1234-75834#0',
+        const firstPublishedSessionId = databaseBuilder.factory.buildSession({
+          publishedAt: new Date('2022-01-04'),
+        }).id;
+        createCertificationCourseWithCompetenceMarks({
+          certificationCourseId: 123,
+          sessionId: firstPublishedSessionId,
+        });
+        createCertificationCourseWithCompetenceMarks({
+          certificationCourseId: 456,
+          sessionId: firstPublishedSessionId,
+        });
+        const secondPublishedSessionId = databaseBuilder.factory.buildSession({
+          publishedAt: new Date('2023-01-09'),
+        }).id;
+        createCertificationCourseWithCompetenceMarks({
+          certificationCourseId: 789,
+          sessionId: secondPublishedSessionId,
+        });
+        createCertificationCourseWithCompetenceMarks({
+          certificationCourseId: 101112,
+          sessionId: secondPublishedSessionId,
+        });
+
+        await databaseBuilder.commit();
+
+        // when
+        await cpfCertificationResultRepository.markCertificationToExport({
+          startDate,
+          endDate,
+          batchId: 'toto#1',
+        });
+
+        // then
+        const certificationCourses = await knex('certification-courses')
+          .select('id', 'cpfImportStatus', 'cpfFilename')
+          .orderBy('id');
+        expect(certificationCourses).to.deep.equal([
+          { id: 123, cpfImportStatus: 'PENDING', cpfFilename: 'toto#1' },
+          { id: 456, cpfImportStatus: 'PENDING', cpfFilename: 'toto#1' },
+          { id: 789, cpfImportStatus: null, cpfFilename: null },
+          { id: 101112, cpfImportStatus: null, cpfFilename: null },
+        ]);
       });
+    });
 
-      // then
-      const certificationCourses = await knex('certification-courses').select('id', 'cpfImportStatus').orderBy('id');
-      expect(certificationCourses).to.deep.equal([
-        { id: 123, cpfImportStatus: null },
-        { id: 456, cpfImportStatus: 'PENDING' },
-        { id: 789, cpfImportStatus: 'PENDING' },
-      ]);
+    context('when there are pending certification courses', function () {
+      context('when there are offset/limit', function () {
+        it('should save partially batchId in cpfFilename', async function () {
+          // given
+          const startDate = new Date('2022-01-01');
+          const endDate = new Date('2022-01-10');
+
+          const firstPublishedSessionId = databaseBuilder.factory.buildSession({
+            publishedAt: new Date('2022-01-04'),
+          }).id;
+          createCertificationCourseWithCompetenceMarks({
+            certificationCourseId: 123,
+            sessionId: firstPublishedSessionId,
+            cpfImportStatus: 'PENDING',
+            cpfFilename: 'toto#0',
+          });
+          createCertificationCourseWithCompetenceMarks({
+            certificationCourseId: 456,
+            sessionId: firstPublishedSessionId,
+            cpfImportStatus: 'PENDING',
+            cpfFilename: 'toto#0',
+          });
+          const secondPublishedSessionId = databaseBuilder.factory.buildSession({
+            publishedAt: new Date('2022-01-09'),
+          }).id;
+          createCertificationCourseWithCompetenceMarks({
+            certificationCourseId: 789,
+            sessionId: secondPublishedSessionId,
+            cpfImportStatus: null,
+            cpfFilename: null,
+          });
+          createCertificationCourseWithCompetenceMarks({
+            certificationCourseId: 101112,
+            sessionId: secondPublishedSessionId,
+            cpfImportStatus: null,
+            cpfFilename: null,
+          });
+          createCertificationCourseWithCompetenceMarks({
+            certificationCourseId: 131415,
+            sessionId: secondPublishedSessionId,
+            cpfImportStatus: null,
+            cpfFilename: null,
+          });
+
+          await databaseBuilder.commit();
+
+          // when
+          await cpfCertificationResultRepository.markCertificationToExport({
+            startDate,
+            endDate,
+            offset: 2,
+            limit: 2,
+            batchId: 'toto#1',
+          });
+
+          // then
+          const certificationCourses = await knex('certification-courses')
+            .select('id', 'cpfImportStatus', 'cpfFilename')
+            .orderBy('id');
+          expect(certificationCourses).to.deep.equal([
+            { id: 123, cpfImportStatus: 'PENDING', cpfFilename: 'toto#0' },
+            { id: 456, cpfImportStatus: 'PENDING', cpfFilename: 'toto#0' },
+            { id: 789, cpfImportStatus: 'PENDING', cpfFilename: 'toto#1' },
+            { id: 101112, cpfImportStatus: 'PENDING', cpfFilename: 'toto#1' },
+            { id: 131415, cpfImportStatus: null, cpfFilename: null },
+          ]);
+        });
+      });
     });
   });
 
@@ -346,7 +613,10 @@ function createCertificationCourseWithCompetenceMarks({
   birthCountry = 'FRANCE',
   cpfFilename = null,
   cpfImportStatus = null,
-  competenceMarks = [{ level: 2, competence_code: '2.1' }],
+  competenceMarks = [
+    { level: 2, competence_code: '2.1' },
+    { level: 1, competence_code: '3.1' },
+  ],
 }) {
   databaseBuilder.factory.buildCertificationCourse({
     id: certificationCourseId,
