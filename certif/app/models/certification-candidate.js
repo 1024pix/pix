ac62ff6efdfd75399e25d14b27593e68852ba013
Quentin Lebouc <quentin.lebouc@pix.fr>
2023-03-30 15:40:09 +0200

    :recycle: certif: change structure of translation files

--- certif/app/models/certification-candidate.js
+++ certif/app/models/certification-candidate.js
@@ -30,10 +30,10 @@ export default class CertificationCandidate extends Model {
     const candidateGender = this.sex;
 
     if (candidateGender === 'M') {
-      return this.intl.t('common.forms.common-labels.gender.man');
+      return this.intl.t('common.labels.candidate.gender.man');
     }
     if (candidateGender === 'F') {
-      return this.intl.t('common.forms.common-labels.gender.woman');
+      return this.intl.t('common.labels.candidate.gender.woman');
     }
     return '-';
   }
