259a6437848f834a9be13dcf7065b94e33b995fc
Pierre TOP (TOPI) <ptop@octo.com>
2023-04-03 16:26:09 +0200

    Fix untrigerred existing eslint-comment violations

--- api/db/migrations/20220225130743_rename-user-tutorials-to-user-saved-tutorials.js
+++ api/db/migrations/20220225130743_rename-user-tutorials-to-user-saved-tutorials.js
@@ -1,12 +1,13 @@
-/* eslint-disable knex/avoid-injections */
 const OLD_TABLE_NAME = 'user_tutorials';
 const NEW_TABLE_NAME = 'user-saved-tutorials';
 
 const up = async function (knex) {
   await knex.schema.renameTable(OLD_TABLE_NAME, NEW_TABLE_NAME);
+  // eslint-disable-next-line knex/avoid-injections
   await knex.raw(
     `ALTER TABLE "${NEW_TABLE_NAME}" RENAME CONSTRAINT "${OLD_TABLE_NAME}_pkey" TO "${NEW_TABLE_NAME}_pkey"`
   );
+  // eslint-disable-next-line knex/avoid-injections
   await knex.raw(
     `ALTER TABLE "${NEW_TABLE_NAME}" RENAME CONSTRAINT "${OLD_TABLE_NAME}_userid_tutorialid_unique" TO "${NEW_TABLE_NAME}_userid_tutorialid_unique"`
   );
@@ -14,9 +15,11 @@ const up = async function (knex) {
 
 const down = async function (knex) {
   await knex.schema.renameTable(NEW_TABLE_NAME, OLD_TABLE_NAME);
+  // eslint-disable-next-line knex/avoid-injections
   await knex.raw(
     `ALTER TABLE "${OLD_TABLE_NAME}" RENAME CONSTRAINT "${NEW_TABLE_NAME}_pkey" TO "${OLD_TABLE_NAME}_pkey"`
   );
+  // eslint-disable-next-line knex/avoid-injections
   await knex.raw(
     `ALTER TABLE "${OLD_TABLE_NAME}" RENAME CONSTRAINT "${NEW_TABLE_NAME}_userid_tutorialid_unique" TO "${OLD_TABLE_NAME}_userid_tutorialid_unique"`
   );
