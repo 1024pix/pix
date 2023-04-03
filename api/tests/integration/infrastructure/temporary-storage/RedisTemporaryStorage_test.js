c36520a73bbb8bcf05f83a2a5b3fd1f986fcdbff
Pierre TOP (TOPI) <ptop@octo.com>
2023-04-03 16:28:20 +0200

    Fix import-default violations

--- api/tests/integration/infrastructure/temporary-storage/RedisTemporaryStorage_test.js
+++ api/tests/integration/infrastructure/temporary-storage/RedisTemporaryStorage_test.js
@@ -1,10 +1,9 @@
-import uuid from 'uuid';
-
-const { v4 } = uuid;
+import { v4 as uuidv4 } from 'uuid';
 
 import { RedisTemporaryStorage } from '../../../../lib/infrastructure/temporary-storage/RedisTemporaryStorage.js';
 import { expect } from '../../../test-helper.js';
 import { config as settings } from '../../../../lib/config.js';
+
 const REDIS_URL = settings.redis.url;
 
 describe('Integration | Infrastructure | TemporaryStorage | RedisTemporaryStorage', function () {
