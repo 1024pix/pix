22d1e1420565deac3ccce89fc0145cd91c569ef5
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-04-04 16:58:37 +0200

    feat(api): add getRedirectLogoutUrl to FWB authentication service
    Co-Authored-By: Eric Lim <11760574+er-lim@users.noreply.github.com>

--- api/tests/integration/domain/services/authentication/fwb-oidc-authentication-service_test.js
+++ api/tests/integration/domain/services/authentication/fwb-oidc-authentication-service_test.js
@@ -5,8 +5,31 @@ const FwbOidcAuthenticationService = require('../../../../../lib/domain/services
 const logoutUrlTemporaryStorage = temporaryStorage.withPrefix('logout-url:');
 
 describe('Integration | Domain | Service | fwb-oidc-authentication-service', function () {
+  describe('#getRedirectLogoutUrl', function () {
+    it('removes the idToken from temporary storage and returns a redirect logout url', async function () {
+      // given
+      const idToken =
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+      const userId = 1;
+      const logoutUrlUUID = '1f3dbb71-f399-4c1c-85ae-0a863c78aeea';
+      const key = `${userId}:${logoutUrlUUID}`;
+      const fwbOidcAuthenticationService = new FwbOidcAuthenticationService();
+      await logoutUrlTemporaryStorage.save({ key, value: idToken, expirationDelaySeconds: 1140 });
+
+      // when
+      const redirectTarget = await fwbOidcAuthenticationService.getRedirectLogoutUrl({ userId, logoutUrlUUID });
+
+      // then
+      expect(redirectTarget).to.equal(
+        'http://logout-url.org/?id_token_hint=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+      );
+      const expectedResult = await logoutUrlTemporaryStorage.get(key);
+      expect(expectedResult).to.be.undefined;
+    });
+  });
+
   describe('#saveIdToken', function () {
-    it('save an idToken and returns an uuid', async function () {
+    it('saves an idToken and returns an uuid', async function () {
       // given
       const uuidPattern = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i);
       const userId = 123;
