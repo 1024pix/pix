d2e0f3962447275c361df375974b2fda1ea3fa58
Anne-Marie Esteves <anne-marie.esteves@octo.com>
2023-04-05 18:30:49 +0200

    feat(mon-pix): add isFwbActivated method in oidcIdp service

--- mon-pix/app/services/oidc-identity-providers.js
+++ mon-pix/app/services/oidc-identity-providers.js
@@ -1,4 +1,5 @@
 import Service, { inject as service } from '@ember/service';
+import isEmpty from 'lodash/isEmpty';
 
 export default class OidcIdentityProviders extends Service {
   @service store;
@@ -12,6 +13,12 @@ export default class OidcIdentityProviders extends Service {
     return this.list
       .filter((provider) => identityProviderCodes.includes(provider.code))
       .map((provider) => provider.organizationName);
+  }
+
+  isFwbActivated() {
+    const identityProviderSlug = 'fwb';
+    const oidcIdentityProviders = this.list.filter((provider) => provider.id === identityProviderSlug);
+    return !isEmpty(oidcIdentityProviders);
   }
 
   async load() {
