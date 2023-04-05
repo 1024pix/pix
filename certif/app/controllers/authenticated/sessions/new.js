601950e5d516e4564d909193797557f954c06011
Quentin Lebouc <quentin.lebouc@pix.fr>
2023-04-05 17:38:53 +0200

    :sparkles: certif: Display errors for empty fields and add aria-required to mandatory fields

--- certif/app/controllers/authenticated/sessions/new.js
+++ certif/app/controllers/authenticated/sessions/new.js
@@ -9,9 +9,13 @@ export default class SessionsNewController extends Controller {
   @alias('model') session;
   @service router;
   @service intl;
+  @service notifications;
 
+  @tracked isSessionAddressMissing;
+  @tracked isSessionRoomMissing;
   @tracked isSessionDateMissing;
   @tracked isSessionTimeMissing;
+  @tracked isSessionExaminerMissing;
 
   get pageTitle() {
     return this.intl.t('pages.sessions.new.extra-information');
@@ -20,20 +24,42 @@ export default class SessionsNewController extends Controller {
   @action
   async createSession(event) {
     event.preventDefault();
-    if (!this.session.date) {
-      this.isSessionDateMissing = true;
+    if (this.checkMissingSessionFields()) return;
+    try {
+      await this.session.save();
+    } catch (error) {
+      if (error.errors) {
+        switch (error.errors[0].status) {
+          case '400':
+            this.notifications.error(this.intl.t('common.api-error-messages.bad-request-error'));
+            break;
+          case '500':
+            this.notifications.error(this.intl.t('common.api-error-messages.internal-server-error'));
+            break;
+          default:
+            this.notifications.error(this.intl.t('common.api-error-messages.internal-error'));
+            break;
+        }
+      }
     }
-    if (!this.session.time) {
-      this.isSessionTimeMissing = true;
-    }
-    if (this.isSessionDateMissing || this.isSessionTimeMissing) return;
-    await this.session.save();
     this.router.transitionTo('authenticated.sessions.details', this.session.id);
   }
 
   @action
   cancel() {
     this.router.transitionTo('authenticated.sessions.list');
+  }
+
+  @action
+  onChangeAddress(event) {
+    this.session.address = event.target.value;
+    this.isSessionAddressMissing = false;
+  }
+
+  @action
+  onChangeRoom(event) {
+    this.session.room = event.target.value;
+    this.isSessionRoomMissing = false;
   }
 
   @action
@@ -46,5 +72,27 @@ export default class SessionsNewController extends Controller {
   onTimePicked(selectedTimes, lastSelectedTimeFormatted) {
     this.session.time = lastSelectedTimeFormatted;
     this.isSessionTimeMissing = false;
+  }
+
+  @action
+  onChangeExaminer(event) {
+    this.session.examiner = event.target.value;
+    this.isSessionExaminerMissing = false;
+  }
+
+  checkMissingSessionFields() {
+    this.isSessionAddressMissing = !this.session.address;
+    this.isSessionRoomMissing = !this.session.room;
+    this.isSessionDateMissing = !this.session.date;
+    this.isSessionTimeMissing = !this.session.time;
+    this.isSessionExaminerMissing = !this.session.examiner;
+
+    return (
+      this.isSessionAddressMissing ||
+      this.isSessionRoomMissing ||
+      this.isSessionDateMissing ||
+      this.isSessionTimeMissing ||
+      this.isSessionExaminerMissing
+    );
   }
 }
