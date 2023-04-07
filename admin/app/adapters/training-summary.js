00a32cdae9a03f31baf7bccc6b579da55b889d69
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-04-07 15:18:59 +0200

    feat(admin): add trainingSummaries in targetProfile model

--- admin/app/adapters/training-summary.js
+++ admin/app/adapters/training-summary.js
@@ -2,4 +2,13 @@ import ApplicationAdapter from './application';
 
 export default class TrainingSummaryAdapter extends ApplicationAdapter {
   namespace = 'api/admin';
+
+  urlForQuery(query) {
+    if (query.targetProfileId) {
+      const targetProfileId = query.targetProfileId;
+      delete query.targetProfileId;
+      return `${this.host}/${this.namespace}/target-profiles/${targetProfileId}/training-summaries`;
+    }
+    return super.urlForQuery(...arguments);
+  }
 }
