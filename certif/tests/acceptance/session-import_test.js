928e1042a89cc16d6a8b948579f9d4eb9c5513db
Andreia Pena <andreia.pena@pix.fr>
2023-04-11 12:14:00 +0200

    ✨certif: improve import errors

--- certif/tests/acceptance/session-import_test.js
+++ certif/tests/acceptance/session-import_test.js
@@ -484,6 +484,31 @@ module('Acceptance | Session Import', function (hooks) {
               .exists();
           });
         });
+
+        module('when sessions validation fails with a 500 http error', function () {
+          test('it should display an error notification with the default error message', async function (assert) {
+            //given
+            const blob = new Blob(['foo']);
+            const file = new File([blob], 'fichier.csv', { type: 'text/csv' });
+            this.server.post('/certification-centers/:id/sessions/validate-for-mass-import', () => new Response(500));
+
+            // when
+            screen = await visit('/sessions/import');
+            const input = await screen.findByLabelText('Importer le modèle complété');
+            await triggerEvent(input, 'change', { files: [file] });
+            const importButton = screen.getByRole('button', { name: 'Continuer' });
+            await click(importButton);
+
+            // then
+            assert
+              .dom(
+                screen.getByText(
+                  'Une erreur interne est survenue, nos équipes sont en train de résoudre le problème. Veuillez réessayer ultérieurement.'
+                )
+              )
+              .exists();
+          });
+        });
       });
     });
   });
