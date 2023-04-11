dcae4d362c8e8526a0160344438d00ebf3ccb05b
Eithliu <iris.benoit@pix.fr>
2023-04-11 17:34:52 +0200

    refactor(api): inject dependencies in certification center controller and tests
    co-authored-by: Clément Latzarus <clement.latzarus@pix.fr>

--- api/tests/unit/application/certification-centers/certification-center-controller_test.js
+++ api/tests/unit/application/certification-centers/certification-center-controller_test.js
@@ -2,7 +2,6 @@ const { expect, sinon, domainBuilder, hFake, catchErr } = require('../../../test
 const usecases = require('../../../../lib/domain/usecases/index.js');
 const certificationCenterMembershipSerializer = require('../../../../lib/infrastructure/serializers/jsonapi/certification-center-membership-serializer');
 const certificationCenterInvitationSerializer = require('../../../../lib/infrastructure/serializers/jsonapi/certification-center-invitation-serializer');
-const sessionSerializer = require('../../../../lib/infrastructure/serializers/jsonapi/session-serializer');
 const Session = require('../../../../lib/domain/models/Session');
 
 const certificationCenterController = require('../../../../lib/application/certification-centers/certification-center-controller');
@@ -14,7 +13,9 @@ describe('Unit | Controller | certifications-center-controller', function () {
   describe('#saveSession', function () {
     let request;
     let expectedSession;
+    let sessionSerializerStub;
     const userId = 274939274;
+
     beforeEach(function () {
       expectedSession = new Session({
         certificationCenter: 'Université de dressage de loutres',
@@ -28,8 +29,11 @@ describe('Unit | Controller | certifications-center-controller', function () {
       });
 
       sinon.stub(usecases, 'createSession').resolves();
-      sinon.stub(sessionSerializer, 'deserialize').returns(expectedSession);
-      sinon.stub(sessionSerializer, 'serialize');
+      sessionSerializerStub = {
+        serialize: sinon.stub(),
+        deserialize: sinon.stub(),
+      };
+      sessionSerializerStub.deserialize.returns(expectedSession);
 
       request = {
         payload: {
@@ -56,7 +60,7 @@ describe('Unit | Controller | certifications-center-controller', function () {
 
     it('should save the session', async function () {
       // when
-      await certificationCenterController.saveSession(request, hFake);
+      await certificationCenterController.saveSession(request, hFake, { sessionSerializer: sessionSerializerStub });
 
       // then
       expect(usecases.createSession).to.have.been.calledWith({ userId, session: expectedSession });
@@ -77,14 +81,16 @@ describe('Unit | Controller | certifications-center-controller', function () {
       });
 
       usecases.createSession.resolves(savedSession);
-      sessionSerializer.serialize.returns(jsonApiSession);
+      sessionSerializerStub.serialize.returns(jsonApiSession);
 
       // when
-      const response = await certificationCenterController.saveSession(request, hFake);
+      const response = await certificationCenterController.saveSession(request, hFake, {
+        sessionSerializer: sessionSerializerStub,
+      });
 
       // then
       expect(response).to.deep.equal(jsonApiSession);
-      expect(sessionSerializer.serialize).to.have.been.calledWith({ session: savedSession });
+      expect(sessionSerializerStub.serialize).to.have.been.calledWith({ session: savedSession });
     });
   });
 
@@ -196,10 +202,13 @@ describe('Unit | Controller | certifications-center-controller', function () {
         certificationCenterId,
       },
     };
+    let certificationCenterMembershipSerializerStub;
 
     beforeEach(function () {
       sinon.stub(usecases, 'findCertificationCenterMembershipsByCertificationCenter');
-      sinon.stub(certificationCenterMembershipSerializer, 'serialize');
+      certificationCenterMembershipSerializerStub = {
+        serialize: sinon.stub(),
+      };
     });
 
     it('should call usecase and serializer and return ok', async function () {
@@ -209,11 +218,13 @@ describe('Unit | Controller | certifications-center-controller', function () {
           certificationCenterId,
         })
         .resolves({});
-      certificationCenterMembershipSerializer.serialize.withArgs({}).returns('ok');
+      certificationCenterMembershipSerializerStub.serialize.withArgs({}).returns('ok');
 
       // when
       const response = await certificationCenterController.findCertificationCenterMembershipsByCertificationCenter(
-        request
+        request,
+        hFake,
+        { certificationCenterMembershipSerializer: certificationCenterMembershipSerializerStub }
       );
 
       // then
