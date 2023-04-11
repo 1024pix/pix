903a1740fca3e2f67415614e5dbc9903a7c82df8
alexandrecoin <alexandre.pc.coin@gmail.com>
2023-04-11 16:20:32 +0200

    fix: api: campaign controller campaignAdministrationArchiveCampaign

--- api/lib/application/campaigns-administration/campaign-controller.js
+++ api/lib/application/campaigns-administration/campaign-controller.js
@@ -1,10 +1,10 @@
 const usecases = require('../../domain/usecases/index.js');
-const csvCampaingsIdsParser = require('../../infrastructure/serializers/csv/campaigns-administration/csv-campaigns-ids-parser.js');
+const csvCampaignsIdsParser = require('../../infrastructure/serializers/csv/campaigns-administration/csv-campaigns-ids-parser.js');
 
 module.exports = {
-  async archiveCampaigns(request, h) {
+  async archiveCampaigns(request, h, dependencies = { csvCampaignsIdsParser }) {
     const { userId } = request.auth.credentials;
-    const campaignIds = await csvCampaingsIdsParser.extractCampaignsIds(request.payload.path);
+    const campaignIds = await dependencies.csvCampaignsIdsParser.extractCampaignsIds(request.payload.path);
 
     await usecases.campaignAdministrationArchiveCampaign({
       userId,
