e7ae33f5b84585d38b523d8fff7544c8223515fc
Andreia Pena <andreia.pena@pix.fr>
2023-04-12 16:57:52 +0200

    ✨certif: translate supervising invigilator's exit modal

--- certif/app/components/session-supervising/header.js
+++ certif/app/components/session-supervising/header.js
@@ -10,14 +10,16 @@ export default class Header extends Component {
   @tracked modalInstructionText = 'Information';
   @tracked isConfirmationModalDisplayed = false;
   @service router;
+  @service intl;
 
   @action
   askUserToConfirmLeaving() {
-    this.modalDescriptionText =
-      'Attention, assurez-vous que tous les candidats aient terminé leur test avant de quitter la surveillance. Pour reprendre la surveillance de cette session, vous devrez entrer à nouveau son numéro de session et son mot de passe.';
-    this.modalCancelText = 'Annuler';
-    this.modalConfirmationText = `Quitter la surveillance`;
-    this.modalInstructionText = `Quitter la surveillance de la session ${this.args.session.id}`;
+    this.modalDescriptionText = this.intl.t('pages.session-supervising.header.information');
+    this.modalCancelText = this.intl.t('common.actions.cancel');
+    this.modalConfirmationText = this.intl.t('pages.session-supervising.header.actions.confirmation');
+    this.modalInstructionText = this.intl.t('pages.session-supervising.header.actions.exit-extra-information', {
+      sessionId: this.args.session.id,
+    });
     this.isConfirmationModalDisplayed = true;
   }
 
