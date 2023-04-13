2494af28cab3306af865bb0e09dedec2b4fc5dda
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-04-13 14:50:11 +0200

    feat(api): return new field in serialization
    Co-authored-by: Matthias Ferraina <matthias.ferraina@pix.fr>

--- api/lib/infrastructure/serializers/jsonapi/training-summary-serializer.js
+++ api/lib/infrastructure/serializers/jsonapi/training-summary-serializer.js
@@ -3,7 +3,7 @@ const { Serializer } = require('jsonapi-serializer');
 module.exports = {
   serialize(trainingSummaries, meta) {
     return new Serializer('training-summaries', {
-      attributes: ['title'],
+      attributes: ['title', 'isRecommendable'],
       meta,
     }).serialize(trainingSummaries);
   },
--- api/tests/unit/infrastructure/serializers/jsonapi/training-summary-serializer_test.js
+++ api/tests/unit/infrastructure/serializers/jsonapi/training-summary-serializer_test.js
@@ -6,7 +6,7 @@ describe('Unit | Serializer | JSONAPI | training-summary-serializer', function (
     it('should serialize training summaries to JSONAPI with meta data', function () {
       // given
       const trainingSummaries = [
-        domainBuilder.buildTrainingSummary({ id: 1, title: 'Training Summary 1' }),
+        domainBuilder.buildTrainingSummary({ id: 1, title: 'Training Summary 1', isRecommendable: true }),
         domainBuilder.buildTrainingSummary({ id: 2, title: 'Training Summary 2' }),
       ];
       const meta = { pagination: { page: 1, pageSize: 3, pageCount: 1, rowCount: 2 } };
@@ -22,6 +22,7 @@ describe('Unit | Serializer | JSONAPI | training-summary-serializer', function (
             id: '1',
             attributes: {
               title: 'Training Summary 1',
+              'is-recommendable': true,
             },
           },
           {
@@ -29,6 +30,7 @@ describe('Unit | Serializer | JSONAPI | training-summary-serializer', function (
             id: '2',
             attributes: {
               title: 'Training Summary 2',
+              'is-recommendable': false,
             },
           },
         ],
