49a15cd002b23148fc6cb89b2a7245c95868a584
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-04-13 18:59:48 +0200

    chore(api): inject dependencies in scorecard-controller

--- api/lib/application/scorecards/scorecard-controller.js
+++ api/lib/application/scorecards/scorecard-controller.js
@@ -1,22 +1,34 @@
 const scorecardSerializer = require('../../infrastructure/serializers/jsonapi/scorecard-serializer.js');
 const tutorialSerializer = require('../../infrastructure/serializers/jsonapi/tutorial-serializer.js');
-const { extractLocaleFromRequest } = require('../../infrastructure/utils/request-response-utils.js');
+const requestResponseUtils = require('../../infrastructure/utils/request-response-utils.js');
 const usecases = require('../../domain/usecases/index.js');
 
 module.exports = {
-  getScorecard(request) {
-    const locale = extractLocaleFromRequest(request);
+  getScorecard(request, h, dependencies = { requestResponseUtils, scorecardSerializer }) {
+    const locale = dependencies.requestResponseUtils.extractLocaleFromRequest(request);
     const authenticatedUserId = request.auth.credentials.userId;
     const scorecardId = request.params.id;
 
-    return usecases.getScorecard({ authenticatedUserId, scorecardId, locale }).then(scorecardSerializer.serialize);
+    return usecases
+      .getScorecard({
+        authenticatedUserId,
+        scorecardId,
+        locale,
+      })
+      .then(dependencies.scorecardSerializer.serialize);
   },
 
-  findTutorials(request) {
-    const locale = extractLocaleFromRequest(request);
+  findTutorials(request, h, dependencies = { requestResponseUtils, tutorialSerializer }) {
+    const locale = dependencies.requestResponseUtils.extractLocaleFromRequest(request);
     const authenticatedUserId = request.auth.credentials.userId;
     const scorecardId = request.params.id;
 
-    return usecases.findTutorials({ authenticatedUserId, scorecardId, locale }).then(tutorialSerializer.serialize);
+    return usecases
+      .findTutorials({
+        authenticatedUserId,
+        scorecardId,
+        locale,
+      })
+      .then(dependencies.tutorialSerializer.serialize);
   },
 };
