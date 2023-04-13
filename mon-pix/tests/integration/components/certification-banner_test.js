19c5af192785707d5dcda4b6239599ce27c4c668
Andreia Pena <andreia.pena@pix.fr>
2023-04-13 14:39:20 +0200

    ♻️ mon-pix: improve tests with Testing Library

--- mon-pix/tests/integration/components/certification-banner_test.js
+++ mon-pix/tests/integration/components/certification-banner_test.js
@@ -1,7 +1,7 @@
 import EmberObject from '@ember/object';
 import { module, test } from 'qunit';
 import setupIntlRenderingTest from '../../helpers/setup-intl-rendering';
-import { find, render } from '@ember/test-helpers';
+import { render } from '@1024pix/ember-testing-library';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | Certification Banner', function (hooks) {
@@ -26,18 +26,19 @@ module('Integration | Component | Certification Banner', function (hooks) {
 
     test('should render component with user fullName', async function (assert) {
       // when
-      await render(hbs`<CertificationBanner @certification={{this.certification}} />`);
+      const screen = await render(hbs`<CertificationBanner @certification={{this.certification}} />`);
 
       // then
-      assert.strictEqual(find('.assessment-banner__title').textContent.trim(), fullName);
+      assert.dom(screen.getByRole('heading', { name: fullName })).exists();
     });
 
     test('should render component with certificationNumber', async function (assert) {
       // when
-      await render(hbs`<CertificationBanner @certificationNumber={{this.certificationNumber}} />`);
+      const screen = await render(hbs`<CertificationBanner @certificationNumber={{this.certificationNumber}} />`);
 
       // then
-      assert.strictEqual(find('.certification-number__value').textContent.trim(), `${certificationNumber}`);
+      assert.dom(screen.getByText('N° de certification')).exists();
+      assert.dom(screen.getByText(certificationNumber)).exists();
     });
   });
 });
