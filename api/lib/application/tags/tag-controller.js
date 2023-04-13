751081f7d2a28fcb9effe0071b3a5088a7dad546
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-04-13 19:55:41 +0200

    chore(api): inject dependencies in tag-controller

--- api/lib/application/tags/tag-controller.js
+++ api/lib/application/tags/tag-controller.js
@@ -2,20 +2,20 @@ const tagSerializer = require('../../infrastructure/serializers/jsonapi/tag-seri
 const usecases = require('../../domain/usecases/index.js');
 
 module.exports = {
-  async create(request, h) {
+  async create(request, h, dependencies = { tagSerializer }) {
     const tagName = request.payload.data.attributes['name'].toUpperCase();
     const createdTag = await usecases.createTag({ tagName });
-    return h.response(tagSerializer.serialize(createdTag)).created();
+    return h.response(dependencies.tagSerializer.serialize(createdTag)).created();
   },
 
-  async findAllTags() {
+  async findAllTags(request, h, dependencies = { tagSerializer }) {
     const organizationsTags = await usecases.findAllTags();
-    return tagSerializer.serialize(organizationsTags);
+    return dependencies.tagSerializer.serialize(organizationsTags);
   },
 
-  async getRecentlyUsedTags(request) {
+  async getRecentlyUsedTags(request, h, dependencies = { tagSerializer }) {
     const tagId = request.params.id;
     const recentlyUsedTags = await usecases.getRecentlyUsedTags({ tagId });
-    return tagSerializer.serialize(recentlyUsedTags);
+    return dependencies.tagSerializer.serialize(recentlyUsedTags);
   },
 };
