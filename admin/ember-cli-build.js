f5b169bd28fcb7deef9b12ef64e36b6bb949eb56
François de Metz <francois.de-metz@pix.fr>
2023-04-17 10:03:13 +0200

    Migrate to embroider

--- admin/ember-cli-build.js
+++ admin/ember-cli-build.js
@@ -37,5 +37,7 @@ module.exports = function (defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  return app.toTree();
+
+  const { Webpack } = require('@embroider/webpack');
+  return require('@embroider/compat').compatBuild(app, Webpack);
 };
