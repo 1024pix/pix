1edc8d05fe4c70b90ce6ce00239a3bd97715cefd
Aurelie Crouillebois <aurelie.crouillebois@gmail.com>
2023-04-18 12:01:43 +0200

    fix(pix1d): rename challenge_number to challenge_id to match with pix app behaviour

--- pix1d/mirage/config.js
+++ pix1d/mirage/config.js
@@ -3,7 +3,7 @@ export default function () {
   this.namespace = 'api/pix1d';
   this.timing = 0; // response delay
 
-  this.get('/challenges/:challenge_number', (schema, request) => {
-    return schema.challenges.find(request.params.challenge_number);
+  this.get('/challenges/:challenge_id', (schema, request) => {
+    return schema.challenges.find(request.params.challenge_id);
   });
 }
