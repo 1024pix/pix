1f2f04950cebaab95f9714a1ee1c3a0509db6662
alexandrecoin <alexandre.pc.coin@gmail.com>
2023-04-19 16:11:38 +0200

    :hammer: api: add npm command for translation files linting

--- certif/.eslintrc.js
+++ certif/.eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     'plugin:ember/recommended',
     'plugin:qunit/recommended',
     'plugin:prettier/recommended',
+    'plugin:i18n-json/recommended',
   ],
   env: {
     browser: true,
