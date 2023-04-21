f9d3789a347c2ef0f4cf31bec71a770be7f1852d
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2023-04-21 15:33:00 +0200

    chore(api): inject dependencies in remember-user-has-seen-assessment-instructions usecase test

--- api/tests/unit/domain/usecases/remember-user-has-seen-assessment-instructions_test.js
+++ api/tests/unit/domain/usecases/remember-user-has-seen-assessment-instructions_test.js
@@ -1,10 +1,10 @@
 const { expect, sinon } = require('../../../test-helper');
 const rememberUserHasSeenAssessmentInstructions = require('../../../../lib/domain/usecases/remember-user-has-seen-assessment-instructions');
-const userRepository = require('../../../../lib/infrastructure/repositories/user-repository');
 
 describe('Unit | UseCase | remember-user-has-seen-assessment-instructions', function () {
+  let userRepository;
   beforeEach(function () {
-    sinon.stub(userRepository, 'updateHasSeenAssessmentInstructionsToTrue');
+    userRepository = { updateHasSeenAssessmentInstructionsToTrue: sinon.stub() };
   });
 
   it('should update has seen assessment instructions', async function () {
