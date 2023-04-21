dc22d1cd358d400dd81e6009c9d9ac55d72808c3
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2023-04-21 14:02:31 +0200

    chore(api): inject dependencies in create-or-update-user-orga usecase test

--- api/tests/unit/domain/usecases/create-or-update-user-orga-settings_test.js
+++ api/tests/unit/domain/usecases/create-or-update-user-orga-settings_test.js
@@ -1,16 +1,16 @@
 const createOrUpdateUserOrgaSettings = require('../../../../lib/domain/usecases/create-or-update-user-orga-settings');
 const { expect, catchErr, sinon } = require('../../../test-helper');
 const { UserNotMemberOfOrganizationError } = require('../../../../lib/domain/errors');
-const userOrgaSettingsRepository = require('../../../../lib/infrastructure/repositories/user-orga-settings-repository');
 const membershipRepository = require('../../../../lib/infrastructure/repositories/membership-repository');
 
 describe('Unit | UseCase | create-or-update-user-orga-settings', function () {
   const userId = 1;
   const organizationId = 3;
+  let userOrgaSettingsRepository;
 
   beforeEach(function () {
     membershipRepository.findByUserIdAndOrganizationId = sinon.stub();
-    sinon.stub(userOrgaSettingsRepository, 'createOrUpdate');
+    userOrgaSettingsRepository = { createOrUpdate: sinon.stub() };
   });
 
   it('should create or update the user orga settings if user is a member of the organization', async function () {
@@ -18,7 +18,12 @@ describe('Unit | UseCase | create-or-update-user-orga-settings', function () {
     membershipRepository.findByUserIdAndOrganizationId.withArgs({ userId, organizationId }).resolves([{}]);
 
     // when
-    await createOrUpdateUserOrgaSettings({ userId, organizationId, userOrgaSettingsRepository, membershipRepository });
+    await createOrUpdateUserOrgaSettings({
+      userId,
+      organizationId,
+      userOrgaSettingsRepository,
+      membershipRepository,
+    });
 
     // then
     expect(userOrgaSettingsRepository.createOrUpdate).to.have.been.calledWithExactly({ userId, organizationId });
