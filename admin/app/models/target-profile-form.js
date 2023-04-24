6cd9f697a7e9e424bcec88580c6e2148fe6c02ab
François de Metz <francois.de-metz@pix.fr>
2023-04-24 16:00:04 +0200

    Move update target profile form to a model

---
+++ admin/app/models/target-profile-form.js
@@ -0,0 +1,49 @@
+import { buildValidations, validator } from 'ember-cp-validations';
+import Model, { attr } from '@ember-data/model';
+
+const Validations = buildValidations({
+  name: {
+    validators: [
+      validator('presence', {
+        presence: true,
+        ignoreBlank: true,
+        message: 'Le nom ne peut pas être vide',
+      }),
+      validator('length', {
+        min: 1,
+        max: 255,
+        message: 'La longueur du nom ne doit pas excéder 255 caractères',
+      }),
+    ],
+  },
+  description: {
+    validators: [
+      validator('length', {
+        max: 500,
+        message: 'La longueur de la description ne doit pas excéder 500 caractères',
+      }),
+    ],
+  },
+  comment: {
+    validators: [
+      validator('length', {
+        max: 500,
+        message: 'La longueur du commentaire ne doit pas excéder 500 caractères',
+      }),
+    ],
+  },
+  category: {
+    validators: [
+      validator('presence', {
+        presence: true,
+      }),
+    ],
+  },
+});
+
+export default class TargetProfileForm extends Model.extend(Validations) {
+  @attr('string') name;
+  @attr('string') description;
+  @attr('string') comment;
+  @attr('string') category;
+}
