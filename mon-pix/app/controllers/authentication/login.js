afabdf83d34078686dd0585780f39ee4a210afad
Clément Latzarus <clement.latzarus@pix.fr>
2023-04-24 17:54:03 +0200

    feat(app): allow to switch language on login page

---
+++ mon-pix/app/controllers/authentication/login.js
@@ -0,0 +1,31 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+
+const DEFAULT_LOCALE = 'fr';
+
+export default class LoginController extends Controller {
+  @service intl;
+  @service dayjs;
+  @service currentDomain;
+  @service router;
+
+  @tracked selectedLanguage = this.intl.primaryLocale;
+
+  get isInternationalDomain() {
+    return !this.currentDomain.isFranceDomain;
+  }
+
+  @action
+  onLanguageChange(value) {
+    this.selectedLanguage = value;
+    this._setLocale(this.selectedLanguage);
+    this.router.replaceWith('authentication.login', { queryParams: { lang: null } });
+  }
+
+  _setLocale(language) {
+    this.intl.setLocale([language, DEFAULT_LOCALE]);
+    this.dayjs.setLocale(language);
+  }
+}
