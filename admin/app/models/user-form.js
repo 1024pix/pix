449c09f43b4b4f08805bd8d485889259b1444868
François de Metz <francois.de-metz@pix.fr>
2023-04-24 18:39:13 +0200

    Remove disabled

--- admin/app/models/user-form.js
+++ admin/app/models/user-form.js
@@ -1,7 +1,5 @@
 import { buildValidations, validator } from 'ember-cp-validations';
 import Model, { attr } from '@ember-data/model';
-// eslint-disable-next-line ember/no-computed-properties-in-native-classes
-import { none } from '@ember/object/computed';
 
 const Validations = buildValidations({
   firstName: {
@@ -38,18 +36,15 @@ const Validations = buildValidations({
         presence: true,
         ignoreBlank: true,
         message: "L'adresse e-mail ne peut pas être vide.",
-        disabled: none('model.email'),
       }),
       validator('length', {
         max: 255,
         message: "La longueur de l'adresse e-mail ne doit pas excéder 255 caractères.",
-        disabled: none('model.email'),
       }),
       validator('format', {
         ignoreBlank: true,
         type: 'email',
         message: "L'adresee e-mail n'a pas le bon format.",
-        disabled: none('model.email'),
       }),
     ],
   },
@@ -59,13 +54,11 @@ const Validations = buildValidations({
         presence: true,
         ignoreBlank: true,
         message: "L'identifiant ne peut pas être vide.",
-        disabled: none('model.username'),
       }),
       validator('length', {
         min: 1,
         max: 255,
         message: 'La longueur du nom ne doit pas excéder 255 caractères.',
-        disabled: none('model.username'),
       }),
     ],
   },
