ed553c5be499d2278da84c39b725ef09d8070464
Clément Latzarus <clement.latzarus@pix.fr>
2023-04-25 17:46:49 +0200

    refactor(app): use LanguageSwitcher component in inscription

--- mon-pix/app/controllers/inscription.js
+++ mon-pix/app/controllers/inscription.js
@@ -13,22 +13,15 @@ export default class InscriptionController extends Controller {
 
   @tracked selectedLanguage = this.intl.primaryLocale;
 
-  queryParams = ['lang'];
-  availableLanguages = [
-    { label: 'Français', value: 'fr' },
-    { label: 'English', value: 'en' },
-  ];
-
   get isInternationalDomain() {
     return !this.currentDomain.isFranceDomain;
   }
 
   @action
   onLanguageChange(value) {
-    const queryParams = { lang: null };
     this.selectedLanguage = value;
     this._setLocale(this.selectedLanguage);
-    this.router.replaceWith('inscription', { queryParams });
+    this.router.replaceWith('inscription', { queryParams: { lang: null } });
   }
 
   _setLocale(language) {
