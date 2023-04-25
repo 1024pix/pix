6dc6b426de9563f948e408f1ff9fe26ea4412474
alexandrecoin <alexandre.pc.coin@gmail.com>
2023-04-25 16:39:44 +0200

    refactor(api): ESM migration add specific dependency injection on reset-password-service

--- api/lib/domain/usecases/get-user-by-reset-password-demand.js
+++ api/lib/domain/usecases/get-user-by-reset-password-demand.js
@@ -3,8 +3,9 @@ module.exports = async function getUserByResetPasswordDemand({
   resetPasswordService,
   tokenService,
   userRepository,
+  resetPasswordDemandRepository,
 }) {
   await tokenService.decodeIfValid(temporaryKey);
-  const { email } = await resetPasswordService.verifyDemand(temporaryKey);
+  const { email } = await resetPasswordService.verifyDemand(temporaryKey, resetPasswordDemandRepository);
   return userRepository.getByEmail(email);
 };
