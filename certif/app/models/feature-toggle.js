19005ec3f9127e84de9675cf2e0ae78d5707a165
Andreia Pena <andreia.pena@pix.fr>
2023-04-25 10:28:44 +0200

    🔧 certif: add isDifferentiatedTimeInvigilatorPortalEnabled
    FT to FeatureToggle model

--- certif/app/models/feature-toggle.js
+++ certif/app/models/feature-toggle.js
@@ -3,4 +3,5 @@ import Model, { attr } from '@ember-data/model';
 export default class FeatureToggle extends Model {
   @attr('boolean') isCleaResultsRetrievalByHabilitatedCertificationCentersEnabled;
   @attr('boolean') isMassiveSessionManagementEnabled;
+  @attr('boolean') isDifferentiatedTimeInvigilatorPortalEnabled;
 }
