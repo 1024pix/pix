bb7641703a3892df31fd2a10f9ebd7f600366882
Eric Lim <eric.lim@zenika.com>
2023-04-27 14:28:52 +0200

    feat(mon-pix): save locale and update user language in language controller

--- mon-pix/tests/unit/controllers/authenticated/user-account/language_test.js
+++ mon-pix/tests/unit/controllers/authenticated/user-account/language_test.js
@@ -5,20 +5,56 @@ import sinon from 'sinon';
 module('Unit | Controller | user-account/language', function (hooks) {
   setupTest(hooks);
 
+  let controller;
+  let dayjsSetLocaleStub;
+  let intlSetLocaleStub;
+  let userSaveStub;
+
+  hooks.beforeEach(function () {
+    dayjsSetLocaleStub = sinon.stub();
+    intlSetLocaleStub = sinon.stub();
+    userSaveStub = sinon.stub();
+
+    controller = this.owner.lookup('controller:authenticated/user-account/language');
+    controller.currentUser = { user: { save: userSaveStub } };
+    controller.dayjs = { setLocale: dayjsSetLocaleStub };
+    controller.intl = { setLocale: intlSetLocaleStub };
+  });
+
   module('#onLanguageChange', function () {
-    test('should refresh page on change lang if domain is not french', function (assert) {
-      // given
-      const controller = this.owner.lookup('controller:authenticated/user-account/language');
-      controller.url = { isFranceDomain: false };
-      const replaceStub = sinon.stub();
-      controller.location = { replace: replaceStub };
+    module('when domain is international', function () {
+      test('set app locale and save user language', async function (assert) {
+        // given
+        const language = 'en';
 
-      // when
-      controller.onLanguageChange('en');
+        controller.currentDomain = { isFranceDomain: false };
 
-      // then
-      sinon.assert.calledWith(replaceStub, '/mon-compte/langue?lang=en');
-      assert.ok(true);
+        // when
+        controller.onLanguageChange(language);
+
+        // then
+        sinon.assert.calledWith(dayjsSetLocaleStub, language);
+        sinon.assert.calledWith(intlSetLocaleStub, [language]);
+        sinon.assert.calledWith(userSaveStub, { adapterOptions: { lang: language } });
+        assert.ok(true);
+      });
+    });
+    module('when domain is french', function () {
+      test('not set locale and save user language', async function (assert) {
+        // given
+        const language = 'en';
+
+        controller.currentDomain = { isFranceDomain: true };
+
+        // when
+        controller.onLanguageChange(language);
+
+        // then
+        sinon.assert.notCalled(dayjsSetLocaleStub);
+        sinon.assert.notCalled(intlSetLocaleStub);
+        sinon.assert.notCalled(userSaveStub);
+        assert.ok(true);
+      });
     });
   });
 });
