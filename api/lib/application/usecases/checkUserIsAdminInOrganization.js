d77d029ee10d4ff8627cd0881f3b565a9931ecb5
Matthias FERRAINA <matthias.ferraina@pix.fr>
2023-04-27 19:32:08 +0200

    refactor(api): setup dependencies injection for checkUserIsAdminInOrganization usecase

--- api/lib/application/usecases/checkUserIsAdminInOrganization.js
+++ api/lib/application/usecases/checkUserIsAdminInOrganization.js
@@ -1,8 +1,8 @@
 const membershipRepository = require('../../infrastructure/repositories/membership-repository.js');
 
 module.exports = {
-  execute(userId, organizationId) {
-    return membershipRepository
+  execute(userId, organizationId, dependencies = { membershipRepository }) {
+    return dependencies.membershipRepository
       .findByUserIdAndOrganizationId({ userId, organizationId })
       .then((memberships) =>
         memberships.reduce((isAdminInOrganization, membership) => isAdminInOrganization || membership.isAdmin, false)
