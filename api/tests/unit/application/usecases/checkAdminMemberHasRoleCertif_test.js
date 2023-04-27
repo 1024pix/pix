89025e4770c466156e50506cd245649d584be2bd
Matthias FERRAINA <matthias.ferraina@pix.fr>
2023-04-27 19:19:11 +0200

    refactor(api): setup dependencies injection for checkAdminMemberHasRoleCertif usecase

--- api/tests/unit/application/usecases/checkAdminMemberHasRoleCertif_test.js
+++ api/tests/unit/application/usecases/checkAdminMemberHasRoleCertif_test.js
@@ -1,22 +1,23 @@
 const { expect, sinon } = require('../../../test-helper');
 const useCase = require('../../../../lib/application/usecases/checkAdminMemberHasRoleCertif');
 const tokenService = require('../../../../lib/domain/services/token-service');
-const adminMemberRepository = require('../../../../lib/infrastructure/repositories/admin-member-repository');
 
 describe('Unit | Application | Use Case | checkAdminMemberHasRoleCertifUseCase', function () {
   const userId = '1234';
-
+  let adminMemberRepositoryStub;
   beforeEach(function () {
     sinon.stub(tokenService, 'extractUserId').resolves(userId);
-    sinon.stub(adminMemberRepository, 'get');
+    adminMemberRepositoryStub = {
+      get: sinon.stub(),
+    };
   });
 
   it('should resolve true when the admin member has role certif', async function () {
     // given
-    adminMemberRepository.get.resolves({ isCertif: true });
+    adminMemberRepositoryStub.get.resolves({ isCertif: true });
 
     // when
-    const result = await useCase.execute(userId);
+    const result = await useCase.execute(userId, { adminMemberRepository: adminMemberRepositoryStub });
 
     // then
     expect(result).to.be.true;
@@ -24,10 +25,10 @@ describe('Unit | Application | Use Case | checkAdminMemberHasRoleCertifUseCase',
 
   it('should resolve false when the admin member does not have role certif', async function () {
     // given
-    adminMemberRepository.get.resolves({ isCertif: false });
+    adminMemberRepositoryStub.get.resolves({ isCertif: false });
 
     // when
-    const result = await useCase.execute(userId);
+    const result = await useCase.execute(userId, { adminMemberRepository: adminMemberRepositoryStub });
 
     // then
     expect(result).to.be.false;
