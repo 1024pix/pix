97225141e38a4ad2a288eb1444f0736c729cf080
Matthias FERRAINA <matthias.ferraina@pix.fr>
2023-04-27 19:24:56 +0200

    refactor(api): setup dependencies injection for checkUserBelongsToOrganization usecase

--- api/lib/application/usecases/checkUserBelongsToOrganization.js
+++ api/lib/application/usecases/checkUserBelongsToOrganization.js
@@ -2,8 +2,8 @@ const _ = require('lodash');
 const membershipRepository = require('../../infrastructure/repositories/membership-repository.js');
 
 module.exports = {
-  execute(userId, organizationId) {
-    return membershipRepository
+  execute(userId, organizationId, dependencies = { membershipRepository }) {
+    return dependencies.membershipRepository
       .findByUserIdAndOrganizationId({ userId, organizationId })
       .then((memberships) => !_.isEmpty(memberships));
   },
