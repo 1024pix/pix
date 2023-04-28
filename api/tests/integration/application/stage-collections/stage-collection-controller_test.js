cf85efbac715e893440f4443db49ea4d6366e61f
Pierre TOP (TOPI) <ptop@octo.com>
2023-04-28 18:17:11 +0200

    Fix flaky test due unsorted array

--- api/tests/integration/application/stage-collections/stage-collection-controller_test.js
+++ api/tests/integration/application/stage-collections/stage-collection-controller_test.js
@@ -103,56 +103,56 @@ describe('Integration | Application | stage-collection-controller', function () 
 
       // then
       const currentStageCollection = await stageCollectionRepository.getByTargetProfileId(targetProfileId);
+      const currentStages = currentStageCollection.toDTO();
       const newStageIds = currentStageCollection.stages
         .map(({ id }) => id)
         .filter((value) => ![123, 456, 789].includes(value))
         .sort();
-      expect(currentStageCollection.toDTO()).to.deep.equal({
-        id: targetProfileId,
-        targetProfileId,
-        stages: [
-          {
-            id: newStageIds[0],
-            level: 2,
-            threshold: null,
-            isFirstSkill: false,
-            title: 'Palier niveau 2 titre',
-            message: 'Palier niveau 2 message',
-            prescriberTitle: 'Palier niveau 2 titre prescripteur',
-            prescriberDescription: 'Palier niveau 2 description prescripteur',
-          },
-          {
-            id: newStageIds[1],
-            level: null,
-            threshold: null,
-            isFirstSkill: true,
-            title: 'Palier premier acquis titre',
-            message: 'Palier premier acquis message',
-            prescriberTitle: 'Palier premier acquis titre prescripteur',
-            prescriberDescription: 'Palier premier acquis description prescripteur',
-          },
-          {
-            id: 123,
-            level: 0,
-            threshold: null,
-            isFirstSkill: false,
-            title: 'Palier niveau 0 titre',
-            message: 'Palier niveau 0 message',
-            prescriberTitle: 'Palier niveau 0 titre prescripteur',
-            prescriberDescription: 'Palier niveau 0 description prescripteur',
-          },
-          {
-            id: 456,
-            level: 1,
-            threshold: null,
-            isFirstSkill: false,
-            title: 'Palier niveau 1 titre',
-            message: 'Palier niveau 1 message',
-            prescriberTitle: 'Palier niveau 1 titre prescripteur',
-            prescriberDescription: 'Palier niveau 1 description prescripteur',
-          },
-        ],
-      });
+
+      expect(currentStages.id).to.equal(targetProfileId);
+      expect(currentStages.targetProfileId).to.equal(targetProfileId);
+      expect(currentStages.stages).to.have.deep.members([
+        {
+          id: newStageIds[0],
+          level: 2,
+          threshold: null,
+          isFirstSkill: false,
+          title: 'Palier niveau 2 titre',
+          message: 'Palier niveau 2 message',
+          prescriberTitle: 'Palier niveau 2 titre prescripteur',
+          prescriberDescription: 'Palier niveau 2 description prescripteur',
+        },
+        {
+          id: newStageIds[1],
+          level: null,
+          threshold: null,
+          isFirstSkill: true,
+          title: 'Palier premier acquis titre',
+          message: 'Palier premier acquis message',
+          prescriberTitle: 'Palier premier acquis titre prescripteur',
+          prescriberDescription: 'Palier premier acquis description prescripteur',
+        },
+        {
+          id: 123,
+          level: 0,
+          threshold: null,
+          isFirstSkill: false,
+          title: 'Palier niveau 0 titre',
+          message: 'Palier niveau 0 message',
+          prescriberTitle: 'Palier niveau 0 titre prescripteur',
+          prescriberDescription: 'Palier niveau 0 description prescripteur',
+        },
+        {
+          id: 456,
+          level: 1,
+          threshold: null,
+          isFirstSkill: false,
+          title: 'Palier niveau 1 titre',
+          message: 'Palier niveau 1 message',
+          prescriberTitle: 'Palier niveau 1 titre prescripteur',
+          prescriberDescription: 'Palier niveau 1 description prescripteur',
+        },
+      ]);
     });
   });
 });
