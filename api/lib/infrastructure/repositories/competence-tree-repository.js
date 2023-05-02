b2249714e8f1f295f6cb2c60c3364fa72f9e1e07
Matthias FERRAINA <matthias.ferraina@pix.fr>
2023-05-02 17:34:15 +0200

    refactor(api): setup dependencies injection for competence-tree-repository

--- api/lib/infrastructure/repositories/competence-tree-repository.js
+++ api/lib/infrastructure/repositories/competence-tree-repository.js
@@ -2,8 +2,8 @@ const areaRepository = require('./area-repository.js');
 const CompetenceTree = require('../../domain/models/CompetenceTree.js');
 
 module.exports = {
-  async get({ locale } = {}) {
-    const areas = await areaRepository.listWithPixCompetencesOnly({ locale });
+  async get({ locale, dependencies = { areaRepository } } = {}) {
+    const areas = await dependencies.areaRepository.listWithPixCompetencesOnly({ locale });
     return new CompetenceTree({ areas });
   },
 };
