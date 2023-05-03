79abca3b4a8d6d2772778eb488b76ea7a0b617c5
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2023-05-03 16:22:05 +0200

    feat(mon-pix): Add pshuffle utility function

---
+++ mon-pix/tests/unit/utils/pshuffle_test.js
@@ -0,0 +1,33 @@
+import { module, test } from 'qunit';
+import { pshuffle } from 'mon-pix/utils/pshuffle';
+
+const shuffleWithSeed = (arr, seed) => {
+  pshuffle(arr, seed);
+  return arr;
+};
+
+module('Unit | Utility | pshuffle', function () {
+  test('shuffles the given array', function (assert) {
+    const arr = shuffleWithSeed([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 123);
+
+    assert.notDeepEqual(arr, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    assert.deepEqual(
+      arr.sort((a, b) => a - b),
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    );
+  });
+
+  test('shuffles stably with the same seed', function (assert) {
+    const arr1 = shuffleWithSeed([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 456);
+    const arr2 = shuffleWithSeed([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 456);
+
+    assert.deepEqual(arr1, arr2);
+  });
+
+  test('shuffles differently with a different seed', function (assert) {
+    const arr1 = shuffleWithSeed([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 789);
+    const arr2 = shuffleWithSeed([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 987);
+
+    assert.notDeepEqual(arr1, arr2);
+  });
+});
