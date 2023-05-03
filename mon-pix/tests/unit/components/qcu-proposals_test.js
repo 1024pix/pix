97ae407c50be930c234a4509e5c57ae9a11f4387
Eithliu <iris.benoit@pix.fr>
2023-05-03 17:58:33 +0200

    Shuffle QCU proposals
    Co-authored-by: Nicolas Lepage <nicolas.lepage@pix.fr>

--- mon-pix/tests/unit/components/qcu-proposals_test.js
+++ mon-pix/tests/unit/components/qcu-proposals_test.js
@@ -1,6 +1,7 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import createGlimmerComponent from '../../helpers/create-glimmer-component';
+import { pshuffle } from 'mon-pix/utils/pshuffle';
 
 module('Unit | Component | QCU proposals', function (hooks) {
   setupTest(hooks);
@@ -11,77 +12,102 @@ module('Unit | Component | QCU proposals', function (hooks) {
     let answerValue;
     let proposals;
     let component;
+    let shuffleSeed;
+    let shuffled;
 
     hooks.beforeEach(function () {
       proposals = DEFAULT_PROPOSALS;
       answerValue = '2';
+      shuffleSeed = 64;
+      shuffled = false;
     });
 
     function initComponent() {
-      component = createGlimmerComponent('component:qcu-proposals', { proposals, answerValue });
+      component = createGlimmerComponent('component:qcu-proposals', { proposals, answerValue, shuffleSeed, shuffled });
     }
+    module('when shuffled is false', function () {
+      test('should return an array of [<proposal_text>, <boolean_answer>]', function (assert) {
+        // Given
+        answerValue = '2';
+        const expectedLabeledRadios = [
+          { checked: false, label: 'prop 1', value: 1 },
+          { checked: true, label: 'prop 2', value: 2 },
+          { checked: false, label: 'prop 3', value: 3 },
+        ];
+        initComponent.call(this);
 
-    test('should return an array of [<proposal_text>, <boolean_answer>]', function (assert) {
-      // Given
-      answerValue = '2';
-      const expectedLabeledRadios = [
-        { checked: false, label: 'prop 1', value: 1 },
-        { checked: true, label: 'prop 2', value: 2 },
-        { checked: false, label: 'prop 3', value: 3 },
-      ];
-      initComponent.call(this);
+        // When
+        const labeledRadios = component.labeledRadios;
 
-      // When
-      const labeledRadios = component.labeledRadios;
+        // Then
+        assert.deepEqual(labeledRadios, expectedLabeledRadios);
+      });
 
-      // Then
-      assert.deepEqual(labeledRadios, expectedLabeledRadios);
+      test('should return an array of [<proposal_text>, <boolean_answer>] with as many items as challenge proposals', function (assert) {
+        // given
+        proposals = '- prop 1\n- prop 2\n- prop 3\n- prop 4\n- prop 5';
+        initComponent.call(this);
+
+        // when
+        const labeledRadios = component.labeledRadios;
+
+        // then
+        assert.strictEqual(labeledRadios.length, 5);
+      });
+
+      test('should not select a radio when given answer is null', function (assert) {
+        // given
+        answerValue = null;
+        const expectedLabeledRadios = [
+          { checked: false, label: 'prop 1', value: 1 },
+          { checked: false, label: 'prop 2', value: 2 },
+          { checked: false, label: 'prop 3', value: 3 },
+        ];
+        initComponent.call(this);
+
+        // when
+        const labeledRadios = component.labeledRadios;
+
+        // then
+        assert.deepEqual(labeledRadios, expectedLabeledRadios);
+      });
+
+      test('should not select a radio when no answer is given', function (assert) {
+        // given
+        answerValue = '';
+        const expectedLabeledRadios = [
+          { checked: false, label: 'prop 1', value: 1 },
+          { checked: false, label: 'prop 2', value: 2 },
+          { checked: false, label: 'prop 3', value: 3 },
+        ];
+        initComponent.call(this);
+
+        // when
+        const labeledRadios = component.labeledRadios;
+
+        // then
+        assert.deepEqual(labeledRadios, expectedLabeledRadios);
+      });
     });
+    module('when shuffled is true', function () {
+      test('should return an array of [<proposal_text>, <boolean_answer>]', function (assert) {
+        // Given
+        shuffled = true;
+        answerValue = '2';
+        const expectedLabeledRadios = [
+          { checked: false, label: 'prop 1', value: 1 },
+          { checked: true, label: 'prop 2', value: 2 },
+          { checked: false, label: 'prop 3', value: 3 },
+        ];
+        initComponent.call(this);
+        pshuffle(expectedLabeledRadios, shuffleSeed);
 
-    test('should return an array of [<proposal_text>, <boolean_answer>] with as many items as challenge proposals', function (assert) {
-      // given
-      proposals = '- prop 1\n- prop 2\n- prop 3\n- prop 4\n- prop 5';
-      initComponent.call(this);
+        // When
+        const labeledRadios = component.labeledRadios;
 
-      // when
-      const labeledRadios = component.labeledRadios;
-
-      // then
-      assert.strictEqual(labeledRadios.length, 5);
-    });
-
-    test('should not select a radio when given answer is null', function (assert) {
-      // given
-      answerValue = null;
-      const expectedLabeledRadios = [
-        { checked: false, label: 'prop 1', value: 1 },
-        { checked: false, label: 'prop 2', value: 2 },
-        { checked: false, label: 'prop 3', value: 3 },
-      ];
-      initComponent.call(this);
-
-      // when
-      const labeledRadios = component.labeledRadios;
-
-      // then
-      assert.deepEqual(labeledRadios, expectedLabeledRadios);
-    });
-
-    test('should not select a radio when no answer is given', function (assert) {
-      // given
-      answerValue = '';
-      const expectedLabeledRadios = [
-        { checked: false, label: 'prop 1', value: 1 },
-        { checked: false, label: 'prop 2', value: 2 },
-        { checked: false, label: 'prop 3', value: 3 },
-      ];
-      initComponent.call(this);
-
-      // when
-      const labeledRadios = component.labeledRadios;
-
-      // then
-      assert.deepEqual(labeledRadios, expectedLabeledRadios);
+        // Then
+        assert.deepEqual(labeledRadios, expectedLabeledRadios);
+      });
     });
   });
 });
