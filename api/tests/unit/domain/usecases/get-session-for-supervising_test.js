39b636cd7a890dcb66dc187a2624805a631aba0f
alexandrecoin <alexandre.pc.coin@gmail.com>
2023-05-03 15:11:10 +0200

    :sparkles: api: check still valid badge acquisition

--- api/tests/unit/domain/usecases/get-session-for-supervising_test.js
+++ api/tests/unit/domain/usecases/get-session-for-supervising_test.js
@@ -15,5 +15,99 @@ describe('Unit | UseCase | get-session-for-supervising', function () {
       // then
       expect(actualSession).to.equal(expectedSession);
     });
+
+    context('when there are candidates', function () {
+      context('when some candidates are still eligible to complementary certifications', function () {
+        it("should return the session with the candidates' eligibility", async function () {
+          // given
+          const stillValidBadgeAcquisition = domainBuilder.buildCertifiableBadgeAcquisition({
+            complementaryCertificationBadgeLabel: 'une certif complémentaire',
+          });
+
+          const retrievedSessionForSupervising = domainBuilder.buildSessionForSupervising({
+            certificationCandidates: [
+              domainBuilder.buildCertificationCandidateForSupervising({
+                userId: 1234,
+                enrolledComplementaryCertification: 'une certif complémentaire',
+                stillValidBadgeAcquisitions: [],
+              }),
+            ],
+          });
+
+          const sessionForSupervisingRepository = { get: sinon.stub() };
+          sessionForSupervisingRepository.get.resolves(retrievedSessionForSupervising);
+
+          const certificationBadgesService = {
+            findStillValidBadgeAcquisitions: sinon.stub(),
+          };
+          certificationBadgesService.findStillValidBadgeAcquisitions
+            .withArgs({ userId: 1234 })
+            .resolves([stillValidBadgeAcquisition]);
+
+          // when
+          const actualSession = await getSessionForSupervising({
+            sessionId: 1,
+            sessionForSupervisingRepository,
+            certificationBadgesService,
+          });
+
+          // then
+          expect(actualSession).to.deep.equal(
+            domainBuilder.buildSessionForSupervising({
+              certificationCandidates: [
+                domainBuilder.buildCertificationCandidateForSupervising({
+                  userId: 1234,
+                  enrolledComplementaryCertification: 'une certif complémentaire',
+                  stillValidBadgeAcquisitions: [stillValidBadgeAcquisition],
+                }),
+              ],
+            })
+          );
+        });
+      });
+
+      context('when some candidates are not eligible to complementary certifications', function () {
+        it("should return the session with the candidates' non eligibility", async function () {
+          // given
+          const retrievedSessionForSupervising = domainBuilder.buildSessionForSupervising({
+            certificationCandidates: [
+              domainBuilder.buildCertificationCandidateForSupervising({
+                userId: 1234,
+                enrolledComplementaryCertification: 'une certif complémentaire',
+                stillValidBadgeAcquisitions: [],
+              }),
+            ],
+          });
+
+          const sessionForSupervisingRepository = { get: sinon.stub() };
+          sessionForSupervisingRepository.get.resolves(retrievedSessionForSupervising);
+
+          const certificationBadgesService = {
+            findStillValidBadgeAcquisitions: sinon.stub(),
+          };
+          certificationBadgesService.findStillValidBadgeAcquisitions.withArgs({ userId: 1234 }).resolves([]);
+
+          // when
+          const actualSession = await getSessionForSupervising({
+            sessionId: 1,
+            sessionForSupervisingRepository,
+            certificationBadgesService,
+          });
+
+          // then
+          expect(actualSession).to.deep.equal(
+            domainBuilder.buildSessionForSupervising({
+              certificationCandidates: [
+                domainBuilder.buildCertificationCandidateForSupervising({
+                  userId: 1234,
+                  enrolledComplementaryCertification: 'une certif complémentaire',
+                  stillValidBadgeAcquisitions: [],
+                }),
+              ],
+            })
+          );
+        });
+      });
+    });
   });
 });
