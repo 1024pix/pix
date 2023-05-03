dace9a7e32013b275894cf98d662fc180f1bff1c
alexandrecoin <alexandre.pc.coin@gmail.com>
2023-05-03 15:11:10 +0200

    :sparkles: api: check still valid badge acquisition

--- api/lib/domain/usecases/get-session-for-supervising.js
+++ api/lib/domain/usecases/get-session-for-supervising.js
@@ -1,3 +1,19 @@
-module.exports = async function getSessionForSupervising({ sessionId, sessionForSupervisingRepository }) {
-  return await sessionForSupervisingRepository.get(sessionId);
+const bluebird = require('bluebird');
+module.exports = async function getSessionForSupervising({
+  sessionId,
+  sessionForSupervisingRepository,
+  certificationBadgesService,
+}) {
+  const sessionForSupervising = await sessionForSupervisingRepository.get(sessionId);
+
+  await bluebird.mapSeries(sessionForSupervising.certificationCandidates, async (certificationCandidate) => {
+    if (certificationCandidate.enrolledComplementaryCertification) {
+      certificationCandidate.stillValidBadgeAcquisitions =
+        await certificationBadgesService.findStillValidBadgeAcquisitions({
+          userId: certificationCandidate.userId,
+        });
+    }
+  });
+
+  return sessionForSupervising;
 };
