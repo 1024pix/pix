8e3b53c3636060bd1975ae401913c782f0ef592a
Anne-Marie Esteves <anne-marie.esteves@octo.com>
2023-05-03 15:50:31 +0200

    feat(orga): add language switcher to login or register form

--- orga/tests/integration/components/auth/login-or-register_test.js
+++ orga/tests/integration/components/auth/login-or-register_test.js
@@ -1,7 +1,8 @@
 import { module, test } from 'qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
+import Service from '@ember/service';
 import { hbs } from 'ember-cli-htmlbars';
-import { clickByName } from '@1024pix/ember-testing-library';
+import { clickByName, render as renderScreen } from '@1024pix/ember-testing-library';
 import setupIntlRenderingTest from '../../../helpers/setup-intl-rendering';
 
 module('Integration | Component | Auth::LoginOrRegister', function (hooks) {
@@ -12,16 +13,7 @@ module('Integration | Component | Auth::LoginOrRegister', function (hooks) {
   hooks.beforeEach(function () {
     loginButton = this.intl.t('pages.login-or-register.login-form.button');
   });
-
-  test('it renders', async function (assert) {
-    // when
-    await render(hbs`<Auth::LoginOrRegister />`);
-
-    // then
-    assert.dom('.login-or-register').exists();
-  });
-
-  test('it display the organization name the user is invited to', async function (assert) {
+  test('it displays the organization name the user is invited to', async function (assert) {
     // when
     const invitationMessage = this.intl.t('pages.login-or-register.title', { organizationName: 'Organization Aztec' });
 
@@ -31,7 +23,7 @@ module('Integration | Component | Auth::LoginOrRegister', function (hooks) {
     assert.dom('.login-or-register-panel__invitation').hasText(`${invitationMessage}`);
   });
 
-  test('it toggle the register form by default', async function (assert) {
+  test('it toggles the register form by default', async function (assert) {
     // when
     await render(hbs`<Auth::LoginOrRegister />`);
 
@@ -39,7 +31,7 @@ module('Integration | Component | Auth::LoginOrRegister', function (hooks) {
     assert.dom('.register-form').exists();
   });
 
-  test('it toggle the login form on click on login button', async function (assert) {
+  test('it toggles the login form on click on login button', async function (assert) {
     // given
     await render(hbs`<Auth::LoginOrRegister />`);
 
@@ -50,7 +42,7 @@ module('Integration | Component | Auth::LoginOrRegister', function (hooks) {
     assert.dom('.login-form').exists();
   });
 
-  test('it toggle the register form on click on register button', async function (assert) {
+  test('it toggles the register form on click on register button', async function (assert) {
     // given
     const registerButtonLabel = this.intl.t('pages.login-or-register.register-form.button');
 
@@ -62,5 +54,51 @@ module('Integration | Component | Auth::LoginOrRegister', function (hooks) {
 
     // then
     assert.dom('.register-form').exists();
+  });
+
+  module('when domain is not .fr', function () {
+    test('displays the language switcher and translate to language selected', async function (assert) {
+      // given
+      class CurrentDomainServiceStub extends Service {
+        get isFranceDomain() {
+          return false;
+        }
+      }
+      class routerServiceStub extends Service {
+        replaceWith() {
+          return false;
+        }
+      }
+      this.owner.register('service:currentDomain', CurrentDomainServiceStub);
+      this.owner.register('service:router', routerServiceStub);
+
+      // when
+      const screen = await renderScreen(hbs`<Auth::LoginOrRegister @organizationName='Organization Aztec' />`);
+
+      // then
+      await click(screen.getByRole('button', { name: 'Français' }));
+      await screen.findByRole('listbox');
+      await click(screen.getByRole('option', { name: 'English' }));
+      assert.dom(screen.getByText('You have been invited to join the organisation Organization Aztec')).exists();
+    });
+  });
+
+  module('when domain is .fr', function () {
+    test('does not display the language switcher', async function (assert) {
+      // given
+      class CurrentDomainServiceStub extends Service {
+        get isFranceDomain() {
+          return true;
+        }
+      }
+      this.owner.register('service:currentDomain', CurrentDomainServiceStub);
+
+      // when
+      const screen = await renderScreen(hbs`<Auth::LoginOrRegister @organizationName='Organization Aztec' />`);
+
+      // then
+      assert.dom(screen.getByText("Vous êtes invité(e) à rejoindre l'organisation Organization Aztec")).exists();
+      assert.dom(screen.queryByRole('button', { name: 'Français' })).doesNotExist();
+    });
   });
 });
