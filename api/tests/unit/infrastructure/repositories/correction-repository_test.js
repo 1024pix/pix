afa1c8fa8e4a37bacc4376103d05d518700091f9
Guillaume Olejniczak <la.toile.cosmique@gmail.com>
2023-05-03 10:19:40 +0200

    fix(api): restore correction test after merge

--- api/tests/unit/infrastructure/repositories/correction-repository_test.js
+++ api/tests/unit/infrastructure/repositories/correction-repository_test.js
@@ -32,8 +32,6 @@ describe('Unit | Repository | correction-repository', function () {
     const userSavedTutorial3 = { id: 'userSavedTutorialId3', userId, tutorialId: 'recTuto3' };
     const tutorialEvaluation3 = { id: 'tutorialEvaluationId3', userId, tutorialId: 'recTuto3' };
 
-    let challengeDataObject;
-
     beforeEach(function () {
       expectedHint = domainBuilder.buildHint({
         skillName: '@web1',
@@ -54,56 +52,59 @@ describe('Unit | Repository | correction-repository', function () {
       ];
       expectedLearningMoreTutorials[0].userSavedTutorial = userSavedTutorial3;
       expectedLearningMoreTutorials[0].tutorialEvaluation = tutorialEvaluation3;
-
-      const skillDatas = [
-        SkillLearningContentDataObjectFixture({
-          name: '@web1',
-          hintStatus: 'Validé',
-          tutorialIds: ['recTuto1'],
-          learningMoreTutorialIds: ['recTuto3'],
-        }),
-        SkillLearningContentDataObjectFixture({
-          name: '@web2',
-          hintStatus: 'Proposé',
-          tutorialIds: ['recTuto2'],
-          learningMoreTutorialIds: ['recTuto4'],
-        }),
-        SkillLearningContentDataObjectFixture({
-          name: '@web3',
-          hintStatus: 'pré-validé',
-          tutorialIds: [],
-          learningMoreTutorialIds: [],
-        }),
-      ];
-
-      skillDatas.forEach((skillData, index) => skillDatasource.get.onCall(index).resolves(skillData));
-      tutorialRepository.findByRecordIdsForCurrentUser
-        .withArgs({ ids: ['recTuto1'], userId, locale })
-        .resolves(expectedTutorials);
-      tutorialRepository.findByRecordIdsForCurrentUser
-        .withArgs({ ids: ['recTuto3'], userId, locale })
-        .resolves(expectedLearningMoreTutorials);
     });
 
-    it('should return a correction with the solution and solutionToDisplay', async function () {
-      // given
-      const expectedCorrection = new Correction({
-        id: 'recwWzTquPlvIl4So',
-        solution: '1, 5',
-        solutionToDisplay: '1',
-        hint: expectedHint,
-        tutorials: expectedTutorials,
-        learningMoreTutorials: expectedLearningMoreTutorials,
-      });
-      challengeDataObject = ChallengeLearningContentDataObjectFixture({
-        skillId: 'recIdSkill003',
-        solution: '1, 5',
-        solutionToDisplay: '1',
-      });
-      challengeDatasource.get.resolves(challengeDataObject);
+    context('normal challenge', function () {
+      let challengeDataObject;
 
-      // when
-      const result = await correctionRepository.getByChallengeId({ challengeId: recordId, userId, locale });
+      beforeEach(function () {
+        // given
+        const skillDatas = [
+          SkillLearningContentDataObjectFixture({
+            name: '@web1',
+            hintStatus: 'Validé',
+            tutorialIds: ['recTuto1'],
+            learningMoreTutorialIds: ['recTuto3'],
+          }),
+          SkillLearningContentDataObjectFixture({
+            name: '@web2',
+            hintStatus: 'Proposé',
+            tutorialIds: ['recTuto2'],
+            learningMoreTutorialIds: ['recTuto4'],
+          }),
+          SkillLearningContentDataObjectFixture({
+            name: '@web3',
+            hintStatus: 'pré-validé',
+            tutorialIds: [],
+            learningMoreTutorialIds: [],
+          }),
+        ];
+
+        skillDatas.forEach((skillData, index) => skillDatasource.get.onCall(index).resolves(skillData));
+        tutorialRepository.findByRecordIdsForCurrentUser
+          .withArgs({ ids: ['recTuto1'], userId, locale })
+          .resolves(expectedTutorials);
+        tutorialRepository.findByRecordIdsForCurrentUser
+          .withArgs({ ids: ['recTuto3'], userId, locale })
+          .resolves(expectedLearningMoreTutorials);
+      });
+
+      it('should return a correction with the solution and solutionToDisplay', async function () {
+        // given
+        const expectedCorrection = new Correction({
+          id: 'recwWzTquPlvIl4So',
+          solution: '1, 5',
+          solutionToDisplay: '1',
+          hint: expectedHint,
+          tutorials: expectedTutorials,
+          learningMoreTutorials: expectedLearningMoreTutorials,
+        });
+        challengeDataObject = ChallengeLearningContentDataObjectFixture({
+          skillId: 'recIdSkill003',
+          solution: '1, 5',
+          solutionToDisplay: '1',
+        });
+        challengeDatasource.get.resolves(challengeDataObject);
 
         // when
         const result = await correctionRepository.getByChallengeId({
@@ -113,15 +114,22 @@ describe('Unit | Repository | correction-repository', function () {
           dependencies: { tutorialRepository },
         });
 
-    it('should return the correction with validated hint', async function () {
-      // given
-      challengeDataObject = ChallengeLearningContentDataObjectFixture({
-        skillId: 'recIdSkill003',
+        // then
+        expect(result).to.be.an.instanceof(Correction);
+        expect(result).to.deep.equal(expectedCorrection);
+        expect(challengeDatasource.get).to.have.been.calledWith(recordId);
+        expect(expectedCorrection.tutorials.map(({ skillId }) => skillId)).to.deep.equal([
+          'recSK0X22abcdefgh',
+          'recSK0X22abcdefgh',
+        ]);
       });
-      challengeDatasource.get.resolves(challengeDataObject);
 
-      // when
-      const result = await correctionRepository.getByChallengeId({ challengeId: recordId, userId, locale });
+      it('should return the correction with validated hint', async function () {
+        // given
+        challengeDataObject = ChallengeLearningContentDataObjectFixture({
+          skillId: 'recIdSkill003',
+        });
+        challengeDatasource.get.resolves(challengeDataObject);
 
         // when
         const result = await correctionRepository.getByChallengeId({
