e4ac225539aec618b9abb5526bf23cbc1e130a67
Thomasevano <thomas.evano@pix.fr>
2023-05-03 14:44:05 +0200

    refactor(certif): add setLocale in localeService
    Co-authored-by: Eric Lim <eric.lim@zenika.com>

--- certif/tests/helpers/setup-intl.js
+++ certif/tests/helpers/setup-intl.js
@@ -1,9 +1,7 @@
 export default function setupIntl(hooks, locale = ['fr']) {
   hooks.beforeEach(function () {
     this.intl = this.owner.lookup('service:intl');
-    this.intl.setLocale(locale);
-
-    this.dayjs = this.owner.lookup('service:dayjs');
-    this.dayjs.setLocale(locale[0]);
+    this.localeService = this.owner.lookup('service:locale');
+    this.localeService.setLocale(locale[0]);
   });
 }
