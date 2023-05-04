b6cf7d4acf002790a0c952415a890c752f91c419
Alexandre Monney <alexandre.monney77@gmail.com>
2023-05-04 11:25:36 +0200

    feat(api): change sort on user participation

--- api/lib/infrastructure/repositories/participations-for-user-management-repository.js
+++ api/lib/infrastructure/repositories/participations-for-user-management-repository.js
@@ -23,7 +23,8 @@ module.exports = {
       .innerJoin('organization-learners', 'organization-learners.id', 'campaign-participations.organizationLearnerId')
       .leftJoin('users as deletedByUsers', 'deletedByUsers.id', 'campaign-participations.deletedBy')
       .where('campaign-participations.userId', userId)
-      .orderBy('createdAt', 'desc');
+      .orderBy('campaignCode', 'asc')
+      .orderBy('sharedAt', 'desc');
 
     return campaignParticipations.map((attributes) => new CampaignParticipationForUserManagement(attributes));
   },
