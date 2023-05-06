8e1a34de23843ed0eac95d9f6d7c7fc76c66b7ed
Laura Bergoens <laura.bergoens@pix.fr>
2023-05-06 13:38:50 +0200

    add basic session tooling

---
+++ api/db/seeds/data/common/session-tooling.js
@@ -0,0 +1,138 @@
+module.exports = {
+  createSession,
+};
+
+/**
+ * Fonction générique pour créer une session selon une configuration donnée.
+ * Retourne l'ID de la session.
+ *
+ * @param {DatabaseBuilder} databaseBuilder
+ * @param {number} sessionId
+ * @param {string} accessCode
+ * @param {string} address
+ * @param {string} certificationCenter
+ * @param {number} certificationCenterId
+ * @param {Date} date
+ * @param {string} description
+ * @param {string} examiner
+ * @param {string} room
+ * @param {string} time
+ * @param {string} examinerGlobalComment
+ * @param {boolean} hasIncident
+ * @param {boolean} hasJoiningIssue
+ * @param {Date} createdAt
+ * @param {Date} finalizedAt
+ * @param {Date} resultsSentToPrescriberAt
+ * @param {Date} publishedAt
+ * @param {number} assignedCertificationOfficerId
+ * @param {string} juryComment
+ * @param {number} juryCommentAuthorId
+ * @param {Date} juryCommentedAt
+ * @param {string} supervisorPassword
+ * @returns {number} sessionId
+ */
+function createSession({
+  databaseBuilder,
+  sessionId,
+  accessCode,
+  address,
+  certificationCenter,
+  certificationCenterId,
+  date,
+  description,
+  examiner,
+  room,
+  time,
+  examinerGlobalComment,
+  hasIncident,
+  hasJoiningIssue,
+  createdAt,
+  finalizedAt,
+  resultsSentToPrescriberAt,
+  publishedAt,
+  assignedCertificationOfficerId,
+  juryComment,
+  juryCommentAuthorId,
+  juryCommentedAt,
+  supervisorPassword,
+}) {
+  _buildSession({
+    databaseBuilder,
+    sessionId,
+    accessCode,
+    address,
+    certificationCenter,
+    certificationCenterId,
+    date,
+    description,
+    examiner,
+    room,
+    time,
+    examinerGlobalComment,
+    hasIncident,
+    hasJoiningIssue,
+    createdAt,
+    finalizedAt,
+    resultsSentToPrescriberAt,
+    publishedAt,
+    assignedCertificationOfficerId,
+    juryComment,
+    juryCommentAuthorId,
+    juryCommentedAt,
+    supervisorPassword,
+  });
+
+  return { sessionId };
+}
+
+function _buildSession({
+  databaseBuilder,
+  sessionId,
+  accessCode,
+  address,
+  certificationCenter,
+  certificationCenterId,
+  date,
+  description,
+  examiner,
+  room,
+  time,
+  examinerGlobalComment,
+  hasIncident,
+  hasJoiningIssue,
+  createdAt,
+  finalizedAt,
+  resultsSentToPrescriberAt,
+  publishedAt,
+  assignedCertificationOfficerId,
+  juryComment,
+  juryCommentAuthorId,
+  juryCommentedAt,
+  supervisorPassword,
+}) {
+  databaseBuilder.factory.buildSession({
+    databaseBuilder,
+    id: sessionId,
+    accessCode,
+    address,
+    certificationCenter,
+    certificationCenterId,
+    date,
+    description,
+    examiner,
+    room,
+    time,
+    examinerGlobalComment,
+    hasIncident,
+    hasJoiningIssue,
+    createdAt,
+    finalizedAt,
+    resultsSentToPrescriberAt,
+    publishedAt,
+    assignedCertificationOfficerId,
+    juryComment,
+    juryCommentAuthorId,
+    juryCommentedAt,
+    supervisorPassword,
+  });
+}
