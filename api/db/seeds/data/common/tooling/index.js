6858543d42712ef9571b4e13ff9409baad83f685
P-Jeremy <jemyplu@gmail.com>
2023-05-09 14:57:59 +0200

    complete and document generic tooling for certification center creation

--- api/db/seeds/data/common/tooling/index.js
+++ api/db/seeds/data/common/tooling/index.js
@@ -2,9 +2,11 @@ const campaign = require('./campaign-tooling');
 const targetProfile = require('./target-profile-tooling');
 const training = require('./training-tooling');
 const organization = require('./organization-tooling');
+const certificationCenter = require('./certification-center-tooling');
 
 module.exports = {
   campaign,
+  certificationCenter,
   organization,
   targetProfile,
   training,
