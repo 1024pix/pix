09e7b2dc222d0d1fd798b6b6760a066076b6d90d
Matthias FERRAINA <matthias.ferraina@pix.fr>
2023-05-09 17:44:40 +0200

    chore(api): fix linting errors for seeds

--- api/db/seeds/data/common/tooling/index.js
+++ api/db/seeds/data/common/tooling/index.js
@@ -1,9 +1,9 @@
-import { campaign } from './campaign-tooling.js';
-import { session } from './session-tooling.js';
-import { profile } from './profile-tooling.js';
-import { targetProfile } from './target-profile-tooling.js';
-import { training } from './training-tooling.js';
-import { organization } from './organization-tooling.js';
-import { certificationCenter } from './certification-center-tooling.js';
+import * as campaign from './campaign-tooling.js';
+import * as session from './session-tooling.js';
+import * as profile from './profile-tooling.js';
+import * as targetProfile from './target-profile-tooling.js';
+import * as training from './training-tooling.js';
+import * as organization from './organization-tooling.js';
+import * as certificationCenter from './certification-center-tooling.js';
 
 export { campaign, certificationCenter, organization, session, targetProfile, training, profile };
