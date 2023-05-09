07495cc02b1f2aacc4d7cf8f5f46302dbe0ad87f
P-Jeremy <jemyplu@gmail.com>
2023-05-09 14:45:51 +0200

    complete and document generic tooling for organization creation

--- api/db/seeds/data/common/tooling/index.js
+++ api/db/seeds/data/common/tooling/index.js
@@ -1,9 +1,11 @@
 const campaign = require('./campaign-tooling');
 const targetProfile = require('./target-profile-tooling');
 const training = require('./training-tooling');
+const organization = require('./organization-tooling');
 
 module.exports = {
   campaign,
+  organization,
   targetProfile,
   training,
 };
