ba5140841db409248027427cf1dbc3db50dd8391
Laura Bergoens <laura.bergoens@pix.fr>
2023-05-10 15:40:07 +0200

    make learners certifiable

--- api/db/seeds/data/common/tooling/learning-content.js
+++ api/db/seeds/data/common/tooling/learning-content.js
@@ -1,14 +1,27 @@
 const _ = require('lodash');
 const competenceRepository = require('../../../../../lib/infrastructure/repositories/competence-repository');
+const challengeRepository = require('../../../../../lib/infrastructure/repositories/challenge-repository');
 const { skillDatasource } = require('../../../../../lib/infrastructure/datasources/learning-content/skill-datasource');
 
-let ALL_COMPETENCES, ALL_ACTIVE_SKILLS;
+let ALL_COMPETENCES, ALL_ACTIVE_SKILLS, ALL_CHALLENGES;
 
 async function getAllCompetences() {
   if (!ALL_COMPETENCES) {
     ALL_COMPETENCES = await competenceRepository.list();
   }
   return ALL_COMPETENCES;
+}
+
+async function getAllChallenges() {
+  if (!ALL_CHALLENGES) {
+    ALL_CHALLENGES = await challengeRepository.list();
+  }
+  return ALL_CHALLENGES;
+}
+
+async function getCoreCompetences() {
+  const allCompetences = await getAllCompetences();
+  return allCompetences.filter((competence) => competence.origin === 'Pix');
 }
 
 async function getAllActiveSkills() {
@@ -28,8 +41,15 @@ async function findActiveSkillsByTubeId(tubeId) {
   return _.filter(allSkills, { tubeId });
 }
 
+async function findFirstValidatedChallengeBySkillId(skillId) {
+  const allChallenges = await getAllChallenges();
+  return _.find(allChallenges, { status: 'validé', skill: { id: skillId } });
+}
+
 module.exports = {
   getAllCompetences,
+  getCoreCompetences,
   findActiveSkillsByCompetenceId,
   findActiveSkillsByTubeId,
+  findFirstValidatedChallengeBySkillId,
 };
