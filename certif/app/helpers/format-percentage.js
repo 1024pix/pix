2fcdde11315cd8fc8a4bd92016f53452b4cc937f
Antoine Ceol <antoine.ceol@gmail.com>
2023-05-10 17:23:43 +0200

    :sparkles: certif: Round percentage
    Floating value are tricky with javascript, we need to round
    1.15 * 100 => 1.14999999

--- certif/app/helpers/format-percentage.js
+++ certif/app/helpers/format-percentage.js
@@ -2,9 +2,9 @@ import { helper } from '@ember/component/helper';
 
 export function formatPercentage([value, ..._]) {
   if (value) {
-    const percentageValue = value * 100;
+    const percentageValue = Math.round(value * 100);
 
-    return `${Math.trunc(percentageValue)} %`;
+    return `${percentageValue} %`;
   }
   return '';
 }
