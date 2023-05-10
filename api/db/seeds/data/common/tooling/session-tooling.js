d938321a46d2315b6efac6cb1d320cbf16412717
P-Jeremy <jemyplu@gmail.com>
2023-05-10 11:45:18 +0200

    add sco session tooling builder

--- api/db/seeds/data/common/tooling/session-tooling.js
+++ api/db/seeds/data/common/tooling/session-tooling.js
@@ -1,5 +1,6 @@
 module.exports = {
   createSession,
+  createDraftScoSession,
 };
 
 /**
@@ -83,6 +84,114 @@ function createSession({
   });
 
   return { sessionId };
+}
+
+/**
+ * Fonction générique pour créer une session sco avec candidats non démarrée selon une configuration donnée.
+ * Retourne l'ID de la session.
+ *
+ * @param {DatabaseBuilder} databaseBuilder
+ * @param {number} sessionId
+ * @param {number} organizationId
+ * @param {string} accessCode
+ * @param {string} address
+ * @param {string} certificationCenter
+ * @param {number} certificationCenterId
+ * @param {Date} date
+ * @param {string} description
+ * @param {string} examiner
+ * @param {string} room
+ * @param {string} time
+ * @param {Date} createdAt
+ * @param {string} supervisorPassword
+ * @param configSession {learnersToRegisterCount: number }
+
+ * @returns {Promise<{sessionId: number}>} sessionId
+ */
+async function createDraftScoSession({
+  databaseBuilder,
+  sessionId,
+  organizationId,
+  accessCode,
+  address,
+  certificationCenter,
+  certificationCenterId,
+  date,
+  description,
+  examiner,
+  room,
+  time,
+  createdAt,
+  configSession,
+  supervisorPassword,
+}) {
+  _buildSession({
+    databaseBuilder,
+    sessionId,
+    accessCode,
+    address,
+    certificationCenter,
+    certificationCenterId,
+    date,
+    description,
+    examiner,
+    room,
+    time,
+    examinerGlobalComment: null,
+    hasIncident: false,
+    hasJoiningIssue: false,
+    createdAt,
+    finalizedAt: null,
+    resultsSentToPrescriberAt: null,
+    publishedAt: null,
+    assignedCertificationOfficerId: null,
+    juryComment: null,
+    juryCommentAuthorId: null,
+    juryCommentedAt: null,
+    supervisorPassword,
+  });
+
+  await _registerOrganizationLearnersToSession({
+    databaseBuilder,
+    sessionId,
+    organizationId,
+    configSession,
+  });
+
+  return { sessionId };
+}
+
+async function _registerOrganizationLearnersToSession({ databaseBuilder, sessionId, organizationId, configSession }) {
+  if (configSession && configSession.learnersToRegisterCount > 0) {
+    const extraTimePercentages = [null, 0.3, 0.5];
+    const organizationLearners = await databaseBuilder
+      .knex('organization-learners')
+      .where({ organizationId })
+      .limit(configSession.learnersToRegisterCount);
+
+    organizationLearners.forEach((organizationLearner, index) => {
+      databaseBuilder.factory.buildCertificationCandidate({
+        firstName: organizationLearner.firstName,
+        lastName: organizationLearner.lastName,
+        sex: organizationLearner.sex,
+        birthPostalCode: null,
+        birthCityCode: null,
+        birthINSEECode: '75115',
+        birthCity: 'PARIS 15',
+        birthCountry: 'France',
+        email: `${organizationLearner.firstName}-${organizationLearner.lastName}@example.net`,
+        birthdate: '2000-01-04',
+        sessionId,
+        createdAt: new Date(),
+        extraTimePercentage: extraTimePercentages[index % extraTimePercentages.length],
+        userId: null,
+        organizationLearnerId: organizationLearner.id,
+        authorizedToStart: false,
+        billingMode: null,
+        prepaymentCode: null,
+      });
+    });
+  }
 }
 
 function _buildSession({
