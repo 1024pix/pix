90bd2b440a7f84704d40fe1ff9d3551359d22c3c
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-05-11 10:45:58 +0200

    refactor(admin): move languages constant in references service

--- admin/app/services/references.js
+++ admin/app/services/references.js
@@ -1,3 +1,10 @@
 import Service from '@ember/service';
 
-export default class ReferencesService extends Service {}
+export default class ReferencesService extends Service {
+  get availableLanguages() {
+    return [
+      { value: 'fr', label: 'Français' },
+      { value: 'en', label: 'Anglais' },
+    ];
+  }
+}
