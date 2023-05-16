8293c15e9c2a9caef3403e808d374eb3c521743c
Quentin Lebouc <quentin.lebouc@pix.fr>
2023-05-16 17:29:17 +0200

    :sparkles: certif: handle backend error translations and display

--- certif/app/controllers/authenticated/sessions/update.js
+++ certif/app/controllers/authenticated/sessions/update.js
@@ -31,10 +31,13 @@ export default class SessionsUpdateController extends Controller {
     if (this.checkMissingSessionFields()) return;
     try {
       await this.session.save();
-    } catch (error) {
-      const status = error?.errors?.[0]?.status;
+    } catch (responseError) {
+      const error = responseError?.errors?.[0];
 
-      if (status === '400') {
+      if (error?.code) {
+        return this.notifications.error(this.intl.t(`common.api-error-messages.${error.code}`));
+      }
+      if (error?.status === '400') {
         return this.notifications.error(this.intl.t('common.api-error-messages.bad-request-error'));
       }
       return this.notifications.error(this.intl.t('common.api-error-messages.internal-server-error'));
