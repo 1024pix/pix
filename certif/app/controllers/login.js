54cfd3be6cb274534f2629a0eb9b44c20bd3f03a
Rébecca Kaci <rebecca.kaci@gmail.com>
2023-05-17 16:51:20 +0200

    feat(certif): allow to switch language on login page

---
+++ certif/app/controllers/login.js
@@ -0,0 +1,24 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+
+export default class LoginController extends Controller {
+  @service intl;
+  @service currentDomain;
+  @service router;
+  @service locale;
+
+  @tracked selectedLanguage = this.intl.primaryLocale;
+
+  get isInternationalDomain() {
+    return !this.currentDomain.isFranceDomain;
+  }
+
+  @action
+  onLanguageChange(value) {
+    this.selectedLanguage = value;
+    this.locale.setLocale(this.selectedLanguage);
+    this.router.replaceWith('login', { queryParams: { lang: null } });
+  }
+}
---
+++ certif/tests/acceptance/login_test.js
@@ -0,0 +1,64 @@
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import { click, currentURL } from '@ember/test-helpers';
+import { visit } from '@1024pix/ember-testing-library';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+import setupIntl from '../helpers/setup-intl';
+
+module('Acceptance | Login', function (hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+  setupIntl(hooks, ['fr', 'en']);
+
+  module('when current url does not contain french tld (.fr)', function () {
+    module('when accessing the login page with "Français" as default language', function () {
+      test('displays the login page with "Français" as selected language', async function (assert) {
+        // when
+        const screen = await visit('/connexion');
+
+        // then
+        assert.strictEqual(currentURL(), '/connexion');
+        assert.dom(screen.getByRole('heading', { name: 'Connectez-vous', level: 1 })).exists();
+      });
+
+      module('when the user select "English" as his language', function () {
+        test('displays the login page with "English" as selected language', async function (assert) {
+          // when
+          const screen = await visit('/connexion');
+          await click(screen.getByRole('button', { name: 'Français' }));
+          await screen.findByRole('listbox');
+          await click(screen.getByRole('option', { name: 'English' }));
+
+          // then
+          assert.strictEqual(currentURL(), '/connexion');
+          assert.dom(screen.getByRole('heading', { name: 'Login', level: 1 })).exists();
+        });
+      });
+    });
+
+    module('when accessing the login page with "English" as selected language', function () {
+      test('displays the login page with "English"', async function (assert) {
+        // when
+        const screen = await visit('/connexion?lang=en');
+
+        // then
+        assert.strictEqual(currentURL(), '/connexion?lang=en');
+        assert.dom(screen.getByRole('heading', { name: 'Login', level: 1 })).exists();
+      });
+
+      module('when the user select "Français" as his language', function () {
+        test('displays the login page with "Français" as selected language', async function (assert) {
+          // given & when
+          const screen = await visit('/connexion?lang=en');
+          await click(screen.getByRole('button', { name: 'English' }));
+          await screen.findByRole('listbox');
+          await click(screen.getByRole('option', { name: 'Français' }));
+
+          // then
+          assert.strictEqual(currentURL(), '/connexion');
+          assert.dom(screen.getByRole('heading', { name: 'Connectez-vous', level: 1 })).exists();
+        });
+      });
+    });
+  });
+});
