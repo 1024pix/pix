fc58c3baeaf6475e11da730c77d71c744c28a16d
Clarisse Damon <clarisse.damon@pix.fr>
2023-05-17 18:09:06 +0200

    feat(mon-pix): use design system component for QROCm

--- mon-pix/tests/acceptance/challenge-item-qrocm_test.js
+++ mon-pix/tests/acceptance/challenge-item-qrocm_test.js
@@ -31,8 +31,8 @@ module('Acceptance | Displaying a QROCM challenge', function (hooks) {
         );
 
         assert.dom('.challenge-response__proposal').exists({ count: 2 });
-        assert.false(findAll('.challenge-response__proposal')[0].disabled);
-        assert.false(findAll('.challenge-response__proposal')[1].disabled);
+        assert.false(findAll('.challenge-response__proposal input')[0].disabled);
+        assert.false(findAll('.challenge-response__proposal input')[1].disabled);
         assert.ok(find('div[data-test="qrocm-label-0"]').innerHTML.includes('Station <strong>1</strong> :'));
         assert.ok(find('div[data-test="qrocm-label-1"]').innerHTML.includes('Station <em>2</em> :'));
 
@@ -143,10 +143,10 @@ module('Acceptance | Displaying a QROCM challenge', function (hooks) {
         // then
         assert.ok(find('div[data-test="qrocm-label-0"]').innerHTML.includes('Station <strong>1</strong> :'));
         assert.ok(find('div[data-test="qrocm-label-1"]').innerHTML.includes('Station <em>2</em> :'));
-        assert.strictEqual(findAll('.challenge-response__proposal')[0].value, 'Republique');
-        assert.true(findAll('.challenge-response__proposal')[0].disabled);
-        assert.strictEqual(findAll('.challenge-response__proposal')[1].value, 'Chatelet');
-        assert.true(findAll('.challenge-response__proposal')[1].disabled);
+        assert.strictEqual(findAll('.challenge-response__proposal input')[0].value, 'Republique');
+        assert.true(findAll('.challenge-response__proposal input')[0].disabled);
+        assert.strictEqual(findAll('.challenge-response__proposal input')[1].value, 'Chatelet');
+        assert.true(findAll('.challenge-response__proposal input')[1].disabled);
 
         assert.dom('.challenge-actions__action-continue').exists();
         assert.dom('.challenge-actions__action-validate').doesNotExist();
