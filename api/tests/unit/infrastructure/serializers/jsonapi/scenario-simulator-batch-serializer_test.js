46c7da1f194e76a33f1546e9da0dbbaab84cf530
alexandrecoin <alexandre.pc.coin@gmail.com>
2023-05-17 15:53:20 +0200

    :sparkles: api: add controller for passing csv to assessment scenario simulator

---
+++ api/tests/unit/infrastructure/serializers/jsonapi/scenario-simulator-batch-serializer_test.js
@@ -0,0 +1,54 @@
+import { expect, domainBuilder } from '../../../../test-helper.js';
+import { scenarioSimulatorBatchSerializer } from '../../../../../lib/infrastructure/serializers/jsonapi/scenario-simulator-batch-serializer.js';
+
+describe('Unit | Serializer | JSONAPI | scenario-simulator-batch-serializer', function () {
+  describe('#serialize', function () {
+    it('should convert scenario simulator objects into JSON API data', function () {
+      // given
+      const challenge = domainBuilder.buildChallenge({
+        id: '1',
+        successProbabilityThreshold: 0.5,
+        difficulty: 0.6,
+        discriminant: 0.5,
+      });
+      const scenarioSimulator = [
+        {
+          index: 0,
+          simulationReport: [
+            {
+              challenge,
+              reward: 2,
+              estimatedLevel: 1,
+              errorRate: 1.5,
+              answer: 'ok',
+            },
+          ],
+        },
+      ];
+
+      // when
+      const scenarioSimulatorMember = scenarioSimulatorBatchSerializer.serialize(scenarioSimulator);
+
+      // then
+      expect(scenarioSimulatorMember).to.deep.equal({
+        data: [
+          {
+            attributes: {
+              'simulation-report': [
+                {
+                  answer: 'ok',
+                  'error-rate': 1.5,
+                  'estimated-level': 1,
+                  'minimum-capability': 0.6,
+                  reward: 2,
+                },
+              ],
+            },
+            id: '0',
+            type: 'scenario-simulator-batches',
+          },
+        ],
+      });
+    });
+  });
+});
