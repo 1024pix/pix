d9af56813f5f506eaca8aeec2cdc6315ed1e7968
Aurelie Crouillebois <aurelie.crouillebois@gmail.com>
2023-05-19 15:48:02 +0200

    refactor(pix1d): add assessmentId into answer on create-answer test

--- api/tests/integration/domain/usecases/correct-answer_test.js
+++ api/tests/integration/domain/usecases/correct-answer_test.js
@@ -1,10 +1,11 @@
-const { expect, knex, domainBuilder, sinon } = require('../../../test-helper');
+const { expect, knex, domainBuilder, sinon, databaseBuilder } = require('../../../test-helper');
 const correctAnswer = require('../../../../lib/domain/usecases/correct-answer');
 const answerRepository = require('../../../../lib/infrastructure/repositories/answer-repository');
 
 describe('Integration | UseCases | create-answer', function () {
+  let createdAnswerRecordId;
   afterEach(async function () {
-    await knex('answers').delete();
+    await knex('answers').where({ id: createdAnswerRecordId }).delete();
   });
 
   it('returns newly created answer', async function () {
@@ -12,19 +13,24 @@ describe('Integration | UseCases | create-answer', function () {
     const challenge = domainBuilder.buildChallenge();
     const challengeRepository = { get: sinon.stub().resolves(challenge) };
 
+    const { id: assessmentId } = databaseBuilder.factory.buildAssessment({});
+    await databaseBuilder.commit();
+
     const answer = domainBuilder.buildAnswer({
       id: null,
       challengeId: challenge.id,
-      assessmentId: null,
+      assessmentId,
     });
     const dependencies = { challengeRepository, answerRepository };
 
     // when
     const record = await correctAnswer({ answer, ...dependencies });
+    // For cleanup purpose
+    createdAnswerRecordId = record.id;
     const savedAnswer = await knex('answers').where({ id: record.id }).first();
 
     // then
     expect(savedAnswer.challengeId).to.equal(challenge.id);
-    expect(savedAnswer.assessmentId).to.equal(null);
+    expect(savedAnswer.assessmentId).to.equal(assessmentId);
   });
 });
