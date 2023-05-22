a3892cc98585969595beea5316d5ba1885d6412a
François de Metz <francois.de-metz@pix.fr>
2023-05-22 15:34:07 +0200

    Fix pole emploi queue name and keep compatibility with the previous one
    Co-authored-by: Matthias Ferraina <matthias.ferraina@pix.fr>

--- api/lib/infrastructure/jobs/campaign-result/SendSharedParticipationResultsToPoleEmploiJob.js
+++ api/lib/infrastructure/jobs/campaign-result/SendSharedParticipationResultsToPoleEmploiJob.js
@@ -2,8 +2,8 @@ import { JobPgBoss } from '../JobPgBoss.js';
 
 class SendSharedParticipationResultsToPoleEmploiJob extends JobPgBoss {
   constructor(queryBuilder) {
-    super({ name: 'SendSharedParticipationResultsToPoleEmploi', retryLimit: 0 }, queryBuilder);
+    super({ name: 'SendSharedParticipationResultsToPoleEmploiJob', retryLimit: 0 }, queryBuilder);
   }
 }
 
-export { SendSharedParticipationResultsToPoleEmploiJob as SendSharedParticipationResultsToPoleEmploiJob };
+export { SendSharedParticipationResultsToPoleEmploiJob };
