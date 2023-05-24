a0238ef6837b0b3627661f19124882ac6517944c
Matthias FERRAINA <matthias.ferraina@pix.fr>
2023-05-24 16:53:34 +0200

    refactor(api): create feedback-repository

---
+++ api/lib/infrastructure/repositories/feedback-repository.js
@@ -0,0 +1,7 @@
+import { knex } from '../../../db/knex-database-connection.js';
+import { Feedback } from '../../domain/models/Feedback.js';
+
+export const save = async function (feedback) {
+  const result = await knex('feedbacks').insert(feedback).returning('*');
+  return new Feedback(result[0]);
+};
