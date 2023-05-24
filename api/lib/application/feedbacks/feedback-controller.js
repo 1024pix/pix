df728c717385683402b2c27d47b8b8484d5abdbe
Matthias FERRAINA <matthias.ferraina@pix.fr>
2023-05-24 17:27:04 +0200

    refactor(api): replace bookshelf method invocation by save-feedback usecase

--- api/lib/application/feedbacks/feedback-controller.js
+++ api/lib/application/feedbacks/feedback-controller.js
@@ -1,12 +1,13 @@
 import * as serializer from '../../infrastructure/serializers/jsonapi/feedback-serializer.js';
+import { usecases } from '../../domain/usecases/index.js';
 
-const save = async function (request, h) {
+const save = async function (request, h, dependencies = { usecases }) {
   const newUserAgent = request.headers['user-agent'].slice(0, 255);
   const feedback = await serializer.deserialize(request.payload, newUserAgent);
 
-  const persistedFeedback = await feedback.save();
+  const persistedFeedback = await dependencies.usecases.saveFeedback({ feedback });
 
-  return h.response(serializer.serialize(persistedFeedback.toJSON())).created();
+  return h.response(serializer.serialize(persistedFeedback)).created();
 };
 
 const feedbackController = { save };
