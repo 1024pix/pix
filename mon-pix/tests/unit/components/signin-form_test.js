02c42b7a02673f3a3c9d9b1dd607ab7dd6c53795
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2023-05-24 15:22:24 +0200

    refactor(mon-pix): reorganize tests context in signin-form_test
    Co-authored-by: Anne-Marie Esteves <anne-marie.esteves@octo.com>
    Co-authored-by: LEGO Technix <109212476+lego-technix@users.noreply.github.com
    Co-authored-by: Rébecca Kaci <rebecca.kaci@gmail.com>
    Co-authored-by: Simon Chareyron

--- mon-pix/tests/unit/components/signin-form_test.js
+++ mon-pix/tests/unit/components/signin-form_test.js
@@ -9,44 +9,8 @@ module('Unit | Component | signin-form', function (hooks) {
   setupTest(hooks);
 
   module('#signin', function () {
-    module('when user should change password', function () {
-      test('should save reset password token and redirect to update-expired-password', async function (assert) {
-        // given
-        const eventStub = { preventDefault: sinon.stub() };
-        const createRecordStub = sinon.stub();
-        const storeStub = { createRecord: createRecordStub };
-        const sessionStub = {
-          authenticateUser: sinon.stub().rejects({
-            responseJSON: {
-              errors: [
-                {
-                  title: 'PasswordShouldChange',
-                  code: 'SHOULD_CHANGE_PASSWORD',
-                  meta: 'PASSWORD_RESET_TOKEN',
-                },
-              ],
-            },
-          }),
-        };
-        const component = createGlimmerComponent('signin-form');
-        component.session = sessionStub;
-        component.args.updateExpiredPassword = sinon.stub();
-        component.store = storeStub;
-        component.router = { replaceWith: sinon.stub() };
-
-        // when
-        await component.signin(eventStub);
-
-        // then
-        sinon.assert.calledWith(createRecordStub, 'reset-expired-password-demand', {
-          passwordResetToken: 'PASSWORD_RESET_TOKEN',
-        });
-        assert.ok(true);
-      });
-    });
-
-    module('when user sign in', function () {
-      test('should authenticate the user even if email contains spaces', async function (assert) {
+    module('completes', function () {
+      test('authenticates the user even if email contains spaces', async function (assert) {
         // given
         const foundUser = EmberObject.create({ id: 12 });
         const eventStub = { preventDefault: sinon.stub() };
@@ -77,6 +41,44 @@ module('Unit | Component | signin-form', function (hooks) {
         // then
         sinon.assert.calledWith(authenticateStub, trimedEmail, password);
         assert.ok(true);
+      });
+    });
+
+    module('completes with error', function () {
+      module('when user should change password', function () {
+        test('saves reset password token and redirect to update-expired-password', async function (assert) {
+          // given
+          const eventStub = { preventDefault: sinon.stub() };
+          const createRecordStub = sinon.stub();
+          const storeStub = { createRecord: createRecordStub };
+          const sessionStub = {
+            authenticateUser: sinon.stub().rejects({
+              responseJSON: {
+                errors: [
+                  {
+                    title: 'PasswordShouldChange',
+                    code: 'SHOULD_CHANGE_PASSWORD',
+                    meta: 'PASSWORD_RESET_TOKEN',
+                  },
+                ],
+              },
+            }),
+          };
+          const component = createGlimmerComponent('signin-form');
+          component.session = sessionStub;
+          component.args.updateExpiredPassword = sinon.stub();
+          component.store = storeStub;
+          component.router = { replaceWith: sinon.stub() };
+
+          // when
+          await component.signin(eventStub);
+
+          // then
+          sinon.assert.calledWith(createRecordStub, 'reset-expired-password-demand', {
+            passwordResetToken: 'PASSWORD_RESET_TOKEN',
+          });
+          assert.ok(true);
+        });
       });
     });
   });
