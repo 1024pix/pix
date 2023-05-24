87648828785a2b471a82649157df7895021ab78c
Andreia Pena <andreia.pena@pix.fr>
2023-05-24 17:51:16 +0200

    ✨ certif: create support url for all languages

--- certif/app/controllers/authenticated/sessions/add-student.js
+++ certif/app/controllers/authenticated/sessions/add-student.js
@@ -1,7 +1,14 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default class SessionsAddStudentController extends Controller {
+  @service url;
+
   get pageTitle() {
     return `Inscription des candidats | Session ${this.model.session.id} | Pix Certif`;
+  }
+
+  get supportUrl() {
+    return this.url.supportUrl;
   }
 }
