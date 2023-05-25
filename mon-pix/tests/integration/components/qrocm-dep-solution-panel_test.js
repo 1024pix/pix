cbfe4edcfd429e739be6f28decaf58d5ca95f7ac
Clarisse Damon <clarisse.damon@pix.fr>
2023-05-25 11:58:15 +0200

    feat(mon-pix): use design system component for QROCm solution

--- mon-pix/tests/integration/components/qrocm-dep-solution-panel_test.js
+++ mon-pix/tests/integration/components/qrocm-dep-solution-panel_test.js
@@ -7,9 +7,9 @@ import { render } from '@1024pix/ember-testing-library';
 import { hbs } from 'ember-cli-htmlbars';
 
 const ANSWER = '.correction-qrocm__answer';
-const INPUT = 'input.correction-qrocm__answer--input';
-const PARAGRAPH = 'textarea.correction-qrocm__answer--paragraph';
-const SENTENCE = 'input.correction-qrocm__answer--sentence';
+const INPUT = 'input.correction-qrocm-answer__input';
+const PARAGRAPH = 'textarea.correction-qrocm-answer__input-paragraph';
+const SENTENCE = 'input.correction-qrocm-answer__input-sentence';
 const SOLUTION_BLOCK = '.comparison-window-solution';
 const SOLUTION_TEXT = '.correction-qrocm__solution-text';
 
@@ -31,175 +31,180 @@ module('Integration | Component | QROCm dep solution panel', function (hooks) {
   });
   const solution = 'rightAnswer:\n' + '- rightAnswer1\n' + '- rightAnswer2';
 
-  [{ format: 'petit' }, { format: 'phrase' }, { format: 'paragraphe' }, { format: 'unreferenced_format' }].forEach(
-    (data) => {
-      module(`Whatever the format (testing ${data.format} format)`, function (hooks) {
-        hooks.beforeEach(function () {
-          this.set('solution', solution);
-          this.set('challenge', challenge);
-          this.challenge.set('format', data.format);
+  [
+    { format: 'petit', input: INPUT },
+    { format: 'phrase', input: SENTENCE },
+    { format: 'paragraphe', input: PARAGRAPH },
+    { format: 'unreferenced_format', input: INPUT },
+  ].forEach((data) => {
+    module(`Whatever the format (testing ${data.format} format)`, function (hooks) {
+      hooks.beforeEach(function () {
+        this.set('solution', solution);
+        this.set('challenge', challenge);
+        this.challenge.set('format', data.format);
+      });
+
+      module('When the answer is correct', function (hooks) {
+        hooks.beforeEach(async function () {
+          // given
+          const answer = EmberObject.create({
+            id: 'answer_id',
+            value: "key1: 'rightAnswer1' key2: 'rightAnswer2'",
+            result: classByResultKey.ok,
+            assessment,
+            challenge,
+          });
+          this.set('answer', answer);
+
+          const correctionBlocks = [
+            {
+              validated: true,
+              alternativeSolutions: [],
+            },
+            {
+              validated: false,
+              alternativeSolutions: [],
+            },
+          ];
+          this.set('correctionBlocks', correctionBlocks);
+
+          // when
+          await render(
+            hbs`<QrocmDepSolutionPanel @challenge={{this.challenge}} @solution={{this.solution}} @answer={{this.answer}} @correctionBlocks={{this.correctionBlocks}} />`
+          );
         });
 
-        module('When the answer is correct', function (hooks) {
-          hooks.beforeEach(async function () {
-            // given
-            const answer = EmberObject.create({
-              id: 'answer_id',
-              value: "key1: 'rightAnswer1' key2: 'rightAnswer2'",
-              result: classByResultKey.ok,
-              assessment,
-              challenge,
-            });
-            this.set('answer', answer);
-
-            const correctionBlocks = [
-              {
-                validated: true,
-                alternativeSolutions: [],
-              },
-              {
-                validated: false,
-                alternativeSolutions: [],
-              },
-            ];
-            this.set('correctionBlocks', correctionBlocks);
-
-            // when
-            await render(
-              hbs`<QrocmDepSolutionPanel @challenge={{this.challenge}} @solution={{this.solution}} @answer={{this.answer}} @correctionBlocks={{this.correctionBlocks}} />`
-            );
-          });
-
-          test('should display the correct answer in green bold', function (assert) {
-            // then
-            const firstAnswerInput = findAll(ANSWER)[0];
-            assert.true(firstAnswerInput.classList.contains('correction-qroc-box-answer--correct'));
-          });
-
-          test('should not have solution block', function (assert) {
-            // then
-            const solutionBlockList = findAll(SOLUTION_BLOCK);
-
-            assert.strictEqual(solutionBlockList.length, 0);
-          });
+        test('should display the correct answer in green bold', function (assert) {
+          // then
+          const firstAnswerInput = findAll(ANSWER)[0];
+          assert.true(firstAnswerInput.classList.contains('correction-qroc-box-answer--correct'));
         });
 
-        module('When there is no answer', function (hooks) {
-          const EMPTY_DEFAULT_MESSAGE = 'Pas de réponse';
+        test('should not have solution block', function (assert) {
+          // then
+          const solutionBlockList = findAll(SOLUTION_BLOCK);
 
-          hooks.beforeEach(async function () {
-            // given
-            const answer = EmberObject.create({
-              id: 'answer_id',
-              value: "key1: '' key2: ''",
-              result: classByResultKey.aband,
-              assessment,
-              challenge,
-            });
-            this.set('answer', answer);
-
-            const correctionBlocks = [
-              {
-                validated: false,
-                alternativeSolutions: [],
-              },
-              {
-                validated: false,
-                alternativeSolutions: [],
-              },
-            ];
-
-            this.set('correctionBlocks', correctionBlocks);
-
-            // when
-            await render(
-              hbs`<QrocmDepSolutionPanel @challenge={{this.challenge}} @solution={{this.solution}} @answer={{this.answer}} @correctionBlocks={{this.correctionBlocks}} />`
-            );
-          });
-
-          test('should display one solution in bold green', async function (assert) {
-            // then
-            const noAnswerSolutionBlockList = findAll(SOLUTION_BLOCK);
-
-            assert.strictEqual(noAnswerSolutionBlockList.length, 1);
-          });
-
-          test('should display the empty answer with the default message "Pas de réponse" in italic', async function (assert) {
-            // then
-            const firstAnswerInput = findAll(ANSWER)[0];
-            assert.ok(firstAnswerInput);
-            assert.strictEqual(firstAnswerInput.value, EMPTY_DEFAULT_MESSAGE);
-            assert.true(firstAnswerInput.classList.contains('correction-qroc-box-answer--aband'));
-          });
-        });
-
-        module('When the answer is wrong', function (hooks) {
-          hooks.beforeEach(async function () {
-            // given
-            const answer = EmberObject.create({
-              id: 'answer_id',
-              value: "key1: 'rightAnswer1' key2: 'wrongAnswer2'",
-              result: classByResultKey.ko,
-              assessment,
-              challenge,
-            });
-            this.set('answer', answer);
-
-            const correctionBlocks = [
-              {
-                validated: true,
-                alternativeSolutions: [],
-              },
-              {
-                validated: false,
-                alternativeSolutions: [],
-              },
-            ];
-            this.set('correctionBlocks', correctionBlocks);
-          });
-
-          test('should display the solutionToDisplay if exist', async function (assert) {
-            // when
-            const solutionToDisplay = 'Ceci est la solution !';
-            this.set('solutionToDisplay', solutionToDisplay);
-            await render(
-              hbs`<QrocmDepSolutionPanel @challenge={{this.challenge}} @solution={{this.solution}} @answer={{this.answer}} @solutionToDisplay={{this.solutionToDisplay}} @correctionBlocks={{this.correctionBlocks}}/>`
-            );
-
-            // then
-            assert.dom(SOLUTION_BLOCK).exists();
-            assert.ok(find(SOLUTION_TEXT).textContent.includes(solutionToDisplay));
-          });
-
-          test('should display the first answer input in bold green', async function (assert) {
-            // when
-            await render(
-              hbs`<QrocmDepSolutionPanel @challenge={{this.challenge}} @solution={{this.solution}} @answer={{this.answer}} @correctionBlocks={{this.correctionBlocks}} />`
-            );
-
-            // then
-            const firstAnswerInput = findAll(ANSWER)[0];
-
-            assert.ok(firstAnswerInput);
-            assert.true(firstAnswerInput.classList.contains('correction-qroc-box-answer--correct'));
-          });
-
-          test('should strike the second answer input', async function (assert) {
-            // when
-            await render(
-              hbs`<QrocmDepSolutionPanel @challenge={{this.challenge}} @solution={{this.solution}} @answer={{this.answer}} @correctionBlocks={{this.correctionBlocks}} />`
-            );
-
-            // then
-            const secondAnswerInput = findAll(ANSWER)[1];
-
-            assert.ok(secondAnswerInput);
-            assert.true(secondAnswerInput.classList.contains('correction-qroc-box-answer--wrong'));
-          });
+          assert.strictEqual(solutionBlockList.length, 0);
         });
       });
-    }
-  );
+
+      module('When there is no answer', function (hooks) {
+        const EMPTY_DEFAULT_MESSAGE = 'Pas de réponse';
+
+        hooks.beforeEach(async function () {
+          // given
+          const answer = EmberObject.create({
+            id: 'answer_id',
+            value: "key1: '' key2: ''",
+            result: classByResultKey.aband,
+            assessment,
+            challenge,
+          });
+          this.set('answer', answer);
+
+          const correctionBlocks = [
+            {
+              validated: false,
+              alternativeSolutions: [],
+            },
+            {
+              validated: false,
+              alternativeSolutions: [],
+            },
+          ];
+
+          this.set('correctionBlocks', correctionBlocks);
+
+          // when
+          await render(
+            hbs`<QrocmDepSolutionPanel @challenge={{this.challenge}} @solution={{this.solution}} @answer={{this.answer}} @correctionBlocks={{this.correctionBlocks}} />`
+          );
+        });
+
+        test('should display one solution in bold green', async function (assert) {
+          // then
+          const noAnswerSolutionBlockList = findAll(SOLUTION_BLOCK);
+
+          assert.strictEqual(noAnswerSolutionBlockList.length, 1);
+        });
+
+        test('should display the empty answer with the default message "Pas de réponse" in italic', async function (assert) {
+          // then
+          const firstAnswerInputContainer = findAll(ANSWER)[0];
+          const firstAnswerInput = findAll(data.input)[0];
+
+          assert.ok(firstAnswerInput);
+          assert.strictEqual(firstAnswerInput.value, EMPTY_DEFAULT_MESSAGE);
+          assert.true(firstAnswerInputContainer.classList.contains('correction-qroc-box-answer--aband'));
+        });
+      });
+
+      module('When the answer is wrong', function (hooks) {
+        hooks.beforeEach(async function () {
+          // given
+          const answer = EmberObject.create({
+            id: 'answer_id',
+            value: "key1: 'rightAnswer1' key2: 'wrongAnswer2'",
+            result: classByResultKey.ko,
+            assessment,
+            challenge,
+          });
+          this.set('answer', answer);
+
+          const correctionBlocks = [
+            {
+              validated: true,
+              alternativeSolutions: [],
+            },
+            {
+              validated: false,
+              alternativeSolutions: [],
+            },
+          ];
+          this.set('correctionBlocks', correctionBlocks);
+        });
+
+        test('should display the solutionToDisplay if exist', async function (assert) {
+          // when
+          const solutionToDisplay = 'Ceci est la solution !';
+          this.set('solutionToDisplay', solutionToDisplay);
+          await render(
+            hbs`<QrocmDepSolutionPanel @challenge={{this.challenge}} @solution={{this.solution}} @answer={{this.answer}} @solutionToDisplay={{this.solutionToDisplay}} @correctionBlocks={{this.correctionBlocks}}/>`
+          );
+
+          // then
+          assert.dom(SOLUTION_BLOCK).exists();
+          assert.ok(find(SOLUTION_TEXT).textContent.includes(solutionToDisplay));
+        });
+
+        test('should display the first answer input in bold green', async function (assert) {
+          // when
+          await render(
+            hbs`<QrocmDepSolutionPanel @challenge={{this.challenge}} @solution={{this.solution}} @answer={{this.answer}} @correctionBlocks={{this.correctionBlocks}} />`
+          );
+
+          // then
+          const firstAnswerInput = findAll(ANSWER)[0];
+
+          assert.ok(firstAnswerInput);
+          assert.true(firstAnswerInput.classList.contains('correction-qroc-box-answer--correct'));
+        });
+
+        test('should strike the second answer input', async function (assert) {
+          // when
+          await render(
+            hbs`<QrocmDepSolutionPanel @challenge={{this.challenge}} @solution={{this.solution}} @answer={{this.answer}} @correctionBlocks={{this.correctionBlocks}} />`
+          );
+
+          // then
+          const secondAnswerInput = findAll(ANSWER)[1];
+
+          assert.ok(secondAnswerInput);
+          assert.true(secondAnswerInput.classList.contains('correction-qroc-box-answer--wrong'));
+        });
+      });
+    });
+  });
 
   module('When format is neither a paragraph nor a sentence', function (hooks) {
     hooks.beforeEach(function () {
