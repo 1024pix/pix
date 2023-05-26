926dbf6858d0b43c20cef91d5e8be3558a467c03
alexandrecoin <alexandre.pc.coin@gmail.com>
2023-05-26 15:44:52 +0200

    :recycle: api: replace answer status by answer in capacity simulator

--- api/lib/domain/services/pick-answer-service.js
+++ api/lib/domain/services/pick-answer-service.js
@@ -1,5 +1,3 @@
-import { AnswerStatus } from '../models/index.js';
-
 function pickAnswersFromArray(array) {
   return function ({ answerIndex }) {
     return array[answerIndex];
@@ -9,7 +7,7 @@ function pickAnswersFromArray(array) {
 function pickAnswerForCapacity(capacity) {
   return function ({ nextChallenge }) {
     const successProbability = 1 / (1 + Math.exp(-nextChallenge.discriminant * (capacity - nextChallenge.difficulty)));
-    return successProbability > Math.random() ? AnswerStatus.OK : AnswerStatus.KO;
+    return successProbability > Math.random() ? 'ok' : 'ko';
   };
 }
 
