a11e52ee2bfbcd1721ff87400a2218f0b4ca4c3b
alexandrecoin <alexandre.pc.coin@gmail.com>
2023-06-02 09:11:37 +0200

    :sparkles: api warm up length and forced competecences parameters

--- api/lib/domain/models/FlashAssessmentAlgorithm.js
+++ api/lib/domain/models/FlashAssessmentAlgorithm.js
@@ -6,11 +6,18 @@ import {
 } from '../services/algorithm-methods/flash.js';
 
 class FlashAssessmentAlgorithm {
+  constructor(warmUpLength, forcedCompetences) {
+    this.warmUpLength = warmUpLength;
+    this.forcedCompetences = forcedCompetences;
+  }
+
   getPossibleNextChallenges({ allAnswers, challenges, estimatedLevel }) {
     const { possibleChallenges, hasAssessmentEnded } = getPossibleNextChallenges({
       allAnswers,
       challenges,
       estimatedLevel,
+      warmUpLength: this.warmUpLength,
+      forcedCompetences: this.forcedCompetences,
     });
 
     if (hasAssessmentEnded) {
