78b035e5dd596ef6c83ee9148d4e5ef6d31cf939
Aurelie Crouillebois <aurelie.crouillebois@gmail.com>
2023-06-02 14:56:45 +0200

    refactor(pix1d): move challenge actions outside auto-reply component

--- pix1d/app/pods/components/challenge/item/component.js
+++ pix1d/app/pods/components/challenge/item/component.js
@@ -8,22 +8,34 @@ export default class Item extends Component {
   @service router;
   @tracked answerHasBeenValidated = false;
   @tracked answer = null;
+  @tracked answerValue = null;
+
+  @action
+  setAnswerValue(value) {
+    this.answerValue = value;
+  }
 
   _createAnswer(challenge) {
     return this.store.createRecord('answer', { challenge });
   }
 
   @action
-  async answerValidated(challenge, assessment, answerValue) {
-    this.answer = this._createAnswer(challenge);
-    this.answer.value = answerValue;
-    this.answer.assessment = assessment;
+  async validateAnswer() {
+    this.answer = this._createAnswer(this.args.challenge);
+    this.answer.value = this.answerValue;
+    this.answer.assessment = this.args.assessment;
     try {
       await this.answer.save();
       this.answerHasBeenValidated = true;
     } catch (error) {
       this.answer.rollbackAttributes();
     }
+  }
+
+  @action
+  skipChallenge() {
+    this.setAnswerValue('#ABAND#');
+    return this.validateAnswer();
   }
 
   @action
