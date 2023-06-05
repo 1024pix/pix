dd1ef838fb2fd54b8f90f74abc9a5684adf79d87
cberthou <clement.berthou.tsp@gmail.com>
2023-06-05 16:29:30 +0200

    :sparkles: api: add nextgen pilot flag in database

---
+++ api/db/migrations/20230605135419_add-is-v3-pilot-column-to-certification-center-table.js
@@ -0,0 +1,23 @@
+const TABLE_CERTIFICATION_CENTERS = 'certification-centers';
+const COLUMN_IS_V3_PILOT = 'isV3Pilot';
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const up = async function (knex) {
+  await knex.schema.table(TABLE_CERTIFICATION_CENTERS, function (table) {
+    table.boolean(COLUMN_IS_V3_PILOT).defaultTo(false);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const down = async function (knex) {
+  await knex.schema.table(TABLE_CERTIFICATION_CENTERS, function (table) {
+    table.dropColumn(COLUMN_IS_V3_PILOT);
+  });
+};
+
+export { up, down };
