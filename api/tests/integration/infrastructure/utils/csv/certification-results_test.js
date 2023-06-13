9daf49fa6d865bec25d91bbe15c0b70856e45b2c
Steph0 <Steph0@users.noreply.github.com>
2023-06-13 12:05:03 +0200

    :sparkles: api: Translate session certif result csv filename
    * Filename now includes more logic since it has to be translated
    * That logic has been migrated out of the controller layer in order to keep the controller 'dumb'

--- api/tests/integration/infrastructure/utils/csv/certification-results_test.js
+++ api/tests/integration/infrastructure/utils/csv/certification-results_test.js
@@ -44,11 +44,12 @@ describe('Integration | Infrastructure | Utils | csv | certification-results', f
         const result = await getSessionCertificationResultsCsv({ session, certificationResults, i18n });
 
         // then
-        const expectedResult =
+        const expectedFilename = '20210101_1430_resultats_session_777.csv';
+        const expectedContent =
           '\uFEFF' +
           '"Numéro de certification";"Prénom";"Nom";"Date de naissance";"Lieu de naissance";"Identifiant Externe";"Statut";"Nombre de Pix";"1.1";"1.2";"1.3";"2.1";"2.2";"2.3";"2.4";"3.1";"3.2";"3.3";"3.4";"4.1";"4.2";"4.3";"5.1";"5.2";"Commentaire jury pour l’organisation";"Session";"Centre de certification";"Date de passage de la certification"\n' +
           '123;"Lili";"Oxford";"04/01/1990";"Torreilles";"LOLORD";"Validée";55;0;1;5;"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";0;0;"RAS";777;"CentreCertif";"01/01/2020"';
-        expect(result).to.equal(expectedResult);
+        expect(result).to.deep.equal({ filename: expectedFilename, content: expectedContent });
       });
 
       context('when certification has been rejected automatically', function () {
@@ -80,11 +81,12 @@ describe('Integration | Infrastructure | Utils | csv | certification-results', f
           const result = await getSessionCertificationResultsCsv({ session, certificationResults, i18n });
 
           // then
-          const expectedResult =
+          const expectedFilename = '20210101_1430_resultats_session_777.csv';
+          const expectedContent =
             '\uFEFF' +
             '"Numéro de certification";"Prénom";"Nom";"Date de naissance";"Lieu de naissance";"Identifiant Externe";"Statut";"Nombre de Pix";"1.1";"1.2";"1.3";"2.1";"2.2";"2.3";"2.4";"3.1";"3.2";"3.3";"3.4";"4.1";"4.2";"4.3";"5.1";"5.2";"Commentaire jury pour l’organisation";"Session";"Centre de certification";"Date de passage de la certification"\n' +
             `456;"Tom";"Cambridge";"21/05/1993";"TheMoon";"TOTODGE";"Rejetée";"0";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";0;0;"${REJECTED_AUTOMATICALLY_COMMENT}";777;"CentreCertif";"02/02/2020"`;
-          expect(result).to.equal(expectedResult);
+          expect(result).to.deep.equal({ filename: expectedFilename, content: expectedContent });
         });
       });
     });
@@ -118,11 +120,12 @@ describe('Integration | Infrastructure | Utils | csv | certification-results', f
         const result = await getSessionCertificationResultsCsv({ session, certificationResults, i18n });
 
         // then
-        const expectedResult =
+        const expectedFilename = '20210101_1430_resultats_session_777.csv';
+        const expectedContent =
           '\uFEFF' +
           '"Numéro de certification";"Prénom";"Nom";"Date de naissance";"Lieu de naissance";"Identifiant Externe";"Statut";"Nombre de Pix";"1.1";"1.2";"1.3";"2.1";"2.2";"2.3";"2.4";"3.1";"3.2";"3.3";"3.4";"4.1";"4.2";"4.3";"5.1";"5.2";"Commentaire jury pour l’organisation";"Session";"Centre de certification";"Date de passage de la certification"\n' +
           '123;"Lili";"Oxford";"04/01/1990";"Torreilles";"LOLORD";"Annulée";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"RAS";777;"CentreCertif";"01/01/2020"';
-        expect(result).to.equal(expectedResult);
+        expect(result).to.deep.equal({ filename: expectedFilename, content: expectedContent });
       });
     });
 
@@ -154,11 +157,12 @@ describe('Integration | Infrastructure | Utils | csv | certification-results', f
         const result = await getSessionCertificationResultsCsv({ session, certificationResults, i18n });
 
         // then
-        const expectedResult =
+        const expectedFilename = '20210101_1430_resultats_session_777.csv';
+        const expectedContent =
           '\uFEFF' +
           '"Numéro de certification";"Prénom";"Nom";"Date de naissance";"Lieu de naissance";"Identifiant Externe";"Statut";"Nombre de Pix";"1.1";"1.2";"1.3";"2.1";"2.2";"2.3";"2.4";"3.1";"3.2";"3.3";"3.4";"4.1";"4.2";"4.3";"5.1";"5.2";"Commentaire jury pour l’organisation";"Session";"Centre de certification";"Date de passage de la certification"\n' +
           '123;"Lili";"Oxford";"04/01/1990";"Torreilles";"LOLORD";"En erreur";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"RAS";777;"CentreCertif";"01/01/2020"';
-        expect(result).to.equal(expectedResult);
+        expect(result).to.deep.equal({ filename: expectedFilename, content: expectedContent });
       });
     });
 
@@ -195,11 +199,12 @@ describe('Integration | Infrastructure | Utils | csv | certification-results', f
         const result = await getSessionCertificationResultsCsv({ session, certificationResults, i18n });
 
         // then
-        const expectedResult =
+        const expectedFilename = '20210101_1430_resultats_session_777.csv';
+        const expectedContent =
           '\uFEFF' +
           `"Numéro de certification";"Prénom";"Nom";"Date de naissance";"Lieu de naissance";"Identifiant Externe";"Statut";"${expectedHeader}";"Nombre de Pix";"1.1";"1.2";"1.3";"2.1";"2.2";"2.3";"2.4";"3.1";"3.2";"3.3";"3.4";"4.1";"4.2";"4.3";"5.1";"5.2";"Commentaire jury pour l’organisation";"Session";"Centre de certification";"Date de passage de la certification"\n` +
           '123;"Lili";"Oxford";"04/01/1990";"Torreilles";"LOLORD";"Validée";"Validée";55;"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";3;0;"RAS";777;"CentreCertif";"01/01/2020"';
-        expect(result).to.equal(expectedResult);
+        expect(result).to.deep.equal({ filename: expectedFilename, content: expectedContent });
       });
 
       it(`should return a cancelled complementary certification when certification pix is cancelled`, async function () {
@@ -234,11 +239,12 @@ describe('Integration | Infrastructure | Utils | csv | certification-results', f
         const result = await getSessionCertificationResultsCsv({ session, certificationResults, i18n });
 
         // then
-        const expectedResult =
+        const expectedFilename = '20210101_1430_resultats_session_777.csv';
+        const expectedContent =
           '\uFEFF' +
           `"Numéro de certification";"Prénom";"Nom";"Date de naissance";"Lieu de naissance";"Identifiant Externe";"Statut";"${expectedHeader}";"Nombre de Pix";"1.1";"1.2";"1.3";"2.1";"2.2";"2.3";"2.4";"3.1";"3.2";"3.3";"3.4";"4.1";"4.2";"4.3";"5.1";"5.2";"Commentaire jury pour l’organisation";"Session";"Centre de certification";"Date de passage de la certification"\n` +
           '123;"Lili";"Oxford";"04/01/1990";"Torreilles";"LOLORD";"Annulée";"Annulée";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"RAS";777;"CentreCertif";"01/01/2020"';
-        expect(result).to.equal(expectedResult);
+        expect(result).to.deep.equal({ filename: expectedFilename, content: expectedContent });
       });
     });
 
@@ -283,14 +289,16 @@ describe('Integration | Infrastructure | Utils | csv | certification-results', f
         const result = await getSessionCertificationResultsCsv({ session, certificationResults, i18n });
 
         // then
-        const expectedResult =
+        const expectedFilename = '20210101_1430_resultats_session_777.csv';
+        const expectedContent =
           '\uFEFF' +
           '"Numéro de certification";"Prénom";"Nom";"Date de naissance";"Lieu de naissance";"Identifiant Externe";"Statut";"Certification Pix+ Bandjo Maître";"Certification Pix+ Bandjo Expert";"Nombre de Pix";"1.1";"1.2";"1.3";"2.1";"2.2";"2.3";"2.4";"3.1";"3.2";"3.3";"3.4";"4.1";"4.2";"4.3";"5.1";"5.2";"Commentaire jury pour l’organisation";"Session";"Centre de certification";"Date de passage de la certification"\n' +
           '123;"Lili";"Oxford";"04/01/1990";"Torreilles";"LOLORD";"Validée";"Rejetée";"Validée";55;"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";3;0;"RAS";777;"CentreCertif";"01/01/2020"';
-        expect(result).to.equal(expectedResult);
+        expect(result).to.deep.equal({ filename: expectedFilename, content: expectedContent });
       });
     });
   });
+
   context('#getDivisionCertificationResultsCsv', function () {
     context('when at least one candidate has passed a certification', function () {
       it('returns a csv without session informations', async function () {
