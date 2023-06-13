80e72e2e8318ce4bbe050f5001ba92e10cae4d56
Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
2023-06-13 17:48:29 +0200

    refactor(api): use BadgeCriterionForCalculation getAcquisitionPercentage method in isFulfilled method

--- api/tests/unit/domain/models/BadgeCriterionForCalculation_test.js
+++ api/tests/unit/domain/models/BadgeCriterionForCalculation_test.js
@@ -63,4 +63,66 @@ describe('Unit | Domain | Models | BadgeCriterionForCalculation', function () {
       });
     });
   });
+
+  describe('#isFulfilled', function () {
+    context('when enough knowledge elements are valid to fulfill the criterion', function () {
+      it('should return true', function () {
+        // given
+        const knowledgeElements = [
+          { skillId: 1, isValidated: true },
+          { skillId: 2, isValidated: true },
+          { skillId: 3, isValidated: false },
+          { skillId: 4 },
+        ];
+        const skillIds = [1, 2, 3];
+        const badgeCriterion = new BadgeCriterionForCalculation({ skillIds, threshold: 60 });
+
+        // when
+        const isFulfilled = badgeCriterion.isFulfilled(knowledgeElements);
+
+        // then
+        expect(isFulfilled).to.be.true;
+      });
+    });
+
+    context('when not enough knowledge elements are valid to fulfill the criterion', function () {
+      it('should return false', function () {
+        // given
+        const knowledgeElements = [
+          { skillId: 1, isValidated: true },
+          { skillId: 2, isValidated: true },
+          { skillId: 3, isValidated: false },
+          { skillId: 4 },
+        ];
+        const skillIds = [1, 2, 3];
+        const badgeCriterion = new BadgeCriterionForCalculation({ skillIds, threshold: 80 });
+
+        // when
+        const isFulfilled = badgeCriterion.isFulfilled(knowledgeElements);
+
+        // then
+        expect(isFulfilled).to.be.false;
+      });
+    });
+
+    context('when no knowledge elements are valid to fulfill the criterion', function () {
+      it('should return false', function () {
+        // given
+        const knowledgeElements = [
+          { skillId: 1, isValidated: false },
+          { skillId: 2, isValidated: false },
+          { skillId: 3, isValidated: false },
+          { skillId: 4 },
+        ];
+        const skillIds = [1, 2, 3];
+        const badgeCriterion = new BadgeCriterionForCalculation({ skillIds, threshold: 80 });
+
+        // when
+        const isFulfilled = badgeCriterion.isFulfilled(knowledgeElements);
+
+        // then
+        expect(isFulfilled).to.be.false;
+      });
+    });
+  });
 });
