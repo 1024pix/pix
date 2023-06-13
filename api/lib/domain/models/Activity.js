9eb975f07645fe629344117c5c61aebd6fda928f
Yaelle YAMA <yaelle.marsiac@pix.fr>
2023-06-13 17:50:15 +0200

    feat(api-pix1d): add status to activities table

--- api/lib/domain/models/Activity.js
+++ api/lib/domain/models/Activity.js
@@ -4,16 +4,24 @@ const levels = {
   VALIDATION: 'VALIDATION',
   CHALLENGE: 'CHALLENGE',
 };
+const status = {
+  STARTED: 'STARTED',
+  SUCCEEDED: 'SUCCEEDED',
+  FAILED: 'FAILED',
+  SKIPPED: 'SKIPPED',
+};
 
 class Activity {
-  constructor({ id, assessmentId, createdAt, level } = {}) {
+  constructor({ id, assessmentId, createdAt, level, status } = {}) {
     this.id = id;
     this.assessmentId = assessmentId;
     this.createdAt = createdAt;
     this.level = level;
+    this.status = status;
   }
 }
 
 Activity.levels = levels;
+Activity.status = status;
 
 export { Activity };
