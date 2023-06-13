b3d23cffcb3121ad04a775065d594aa46b6ed8c1
Yaelle YAMA <yaelle.marsiac@pix.fr>
2023-06-13 17:50:15 +0200

    feat(api-pix1d): add status to activities table

---
+++ api/db/migrations/20230613153633_add_status_on_activities_table.js
@@ -0,0 +1,16 @@
+const TABLE_NAME = 'activities';
+const COLUMN_NAME = 'status';
+
+const up = function (knex) {
+  return knex.schema.table(TABLE_NAME, function (table) {
+    table.string(COLUMN_NAME).defaultTo('started');
+  });
+};
+
+const down = function (knex) {
+  return knex.schema.table(TABLE_NAME, function (table) {
+    table.dropColumn(COLUMN_NAME);
+  });
+};
+
+export { up, down };
