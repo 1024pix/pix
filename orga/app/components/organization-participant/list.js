bd14db54345b6ece209075cfbcd9870cee30992c
Alexandre Monney <alexandre.monney77@gmail.com>
2023-06-14 11:14:37 +0200

    feat(orga): add main checkbox on participant list

--- orga/app/components/organization-participant/list.js
+++ orga/app/components/organization-participant/list.js
@@ -1,10 +1,19 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { guidFor } from '@ember/object/internals';
 
 export default class List extends Component {
   @action
   onClick(toggleParticipant, event) {
     event.stopPropagation();
     toggleParticipant();
+  }
+
+  get mainCheckboxId() {
+    return guidFor(this);
+  }
+
+  get mainCheckbox() {
+    return document.getElementById(this.mainCheckboxId);
   }
 }
