7782077067aceafff629593ccacfdbf41f774c48
Quentin Lebouc <quentin.lebouc@pix.fr>
2023-06-14 11:01:31 +0200

    feat(api): add getAcquisitionPercentage method in BadgeForCalculation

--- api/lib/domain/models/BadgeForCalculation.js
+++ api/lib/domain/models/BadgeForCalculation.js
@@ -7,4 +7,12 @@ export class BadgeForCalculation {
   shouldBeObtained(knowledgeElements) {
     return this.badgeCriteria.every((badgeCriterion) => badgeCriterion.isFulfilled(knowledgeElements));
   }
+
+  getAcquisitionPercentage(knowledgeElements) {
+    const summary = this.badgeCriteria.reduce((accumulator, badgeCriterion) => {
+      return accumulator + badgeCriterion.getAcquisitionPercentage(knowledgeElements);
+    }, 0);
+
+    return Math.round(summary / this.badgeCriteria.length);
+  }
 }
