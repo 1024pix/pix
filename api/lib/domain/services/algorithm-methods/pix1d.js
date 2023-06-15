d33dd9301863f94fc041aff468d4ffdd58a5dbd8
Aurelie Crouillebois <aurelie.crouillebois@gmail.com>
2023-06-15 15:36:29 +0200

    feat(api-pix1d): add new exit case on algo & refacto

--- api/lib/domain/services/algorithm-methods/pix1d.js
+++ api/lib/domain/services/algorithm-methods/pix1d.js
@@ -2,37 +2,34 @@ import { Activity } from '../../models/Activity.js';
 
 export { getNextActivityLevel };
 
+const { TUTORIAL, TRAINING, VALIDATION, CHALLENGE } = Activity.levels;
+const { SUCCEEDED, FAILED, SKIPPED } = Activity.status;
+
 function getNextActivityLevel(activities) {
   if (activities.length === 0) {
-    return Activity.levels.VALIDATION;
+    return VALIDATION;
   }
-  if (_nbOfActivitiesOfLevel(activities, Activity.levels.VALIDATION) >= 3) {
+  if (_nbOfActivitiesOfLevel(activities, VALIDATION) >= 3) {
     return undefined;
   }
   const lastActivity = activities[0];
-  if (
-    _nbOfActivitiesOfLevel(activities, Activity.levels.TRAINING) === 3 &&
-    lastActivity.status === Activity.status.FAILED
-  ) {
+  if (_nbOfActivitiesOfLevel(activities, TRAINING) === 3 && (_hasFailed(lastActivity) || _hasSkipped(lastActivity))) {
     return undefined;
   }
   if (_hasSucceeded(lastActivity)) {
-    if (
-      _nbOfActivitiesOfLevel(activities, Activity.levels.TUTORIAL) !== 0 &&
-      lastActivity.level === Activity.levels.VALIDATION
-    ) {
+    if (_alreadyDoneActivity(activities, TUTORIAL) && lastActivity.level === VALIDATION) {
       return undefined;
     } else {
       return _higherLevelActivity(lastActivity);
     }
   } else {
-    if (_nbOfActivitiesOfLevel(activities, Activity.levels.TRAINING) === 0) {
-      if (lastActivity.level === Activity.levels.VALIDATION) {
-        return Activity.levels.TRAINING;
+    if (_neverDoneActivity(activities, TRAINING)) {
+      if (lastActivity.level === VALIDATION) {
+        return TRAINING;
       }
     } else {
-      if (lastActivity.level === Activity.levels.TRAINING || lastActivity.level === Activity.levels.VALIDATION) {
-        return Activity.levels.TUTORIAL;
+      if (lastActivity.level === TRAINING || lastActivity.level === VALIDATION) {
+        return TUTORIAL;
       } else {
         return undefined;
       }
@@ -44,17 +41,28 @@ function _nbOfActivitiesOfLevel(activities, level) {
   return activities.filter((activity) => activity.level === level).length;
 }
 
+function _alreadyDoneActivity(activities, level) {
+  return _nbOfActivitiesOfLevel(activities, level) !== 0;
+}
+
+function _neverDoneActivity(activities, level) {
+  return _nbOfActivitiesOfLevel(activities, level) === 0;
+}
+
 function _hasSucceeded(lastActivity) {
-  return lastActivity.status === Activity.status.SUCCEEDED;
+  return lastActivity.status === SUCCEEDED;
+}
+
+function _hasFailed(lastActivity) {
+  return lastActivity.status === FAILED;
+}
+
+function _hasSkipped(lastActivity) {
+  return lastActivity.status === SKIPPED;
 }
 
 function _higherLevelActivity(lastActivity) {
-  const orderedActivityLevels = [
-    Activity.levels.TUTORIAL,
-    Activity.levels.TRAINING,
-    Activity.levels.VALIDATION,
-    Activity.levels.CHALLENGE,
-  ];
+  const orderedActivityLevels = [TUTORIAL, TRAINING, VALIDATION, CHALLENGE];
 
   return orderedActivityLevels[orderedActivityLevels.indexOf(lastActivity.level) + 1];
 }
