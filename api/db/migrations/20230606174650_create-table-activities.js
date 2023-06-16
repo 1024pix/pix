8bae6fe1d86c302873fe3c83efce08c2952f402e
Aurelie Crouillebois <aurelie.crouillebois@gmail.com>
2023-06-16 10:53:01 +0200

    fix(api): add bdd script for retro-compatibility

---
+++ api/db/migrations/20230606174650_create-table-activities.js
@@ -0,0 +1,17 @@
+const TABLE_NAME = 'activities';
+
+const up = function (knex) {
+  return knex.schema.createTable(TABLE_NAME, (t) => {
+    t.increments().primary();
+    t.integer('assessmentId').notNullable().unsigned().references('assessments.id');
+    t.string('level').notNullable();
+    t.dateTime('createdAt').notNullable().defaultTo(knex.fn.now());
+    t.index('assessmentId');
+  });
+};
+
+const down = function (knex) {
+  return knex.schema.dropTable(TABLE_NAME);
+};
+
+export { up, down };
---
+++ api/db/migrations/20230606174700_add-activity-id-on-answer.js
@@ -0,0 +1,9 @@
+// Migration vide, car l'ajout effectif d'une colonne sur la table answer ne passe pas en prod
+
+// eslint-disable-next-line no-empty-function
+const up = async function () {};
+
+// eslint-disable-next-line no-empty-function
+const down = async function () {};
+
+export { up, down };
