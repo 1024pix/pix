e44fbb119132a175ca43ae8287064ce04bea0cd8
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-06-19 14:14:29 +0200

    feat(api): add new seed builder for simple users

---
+++ api/db/seeds/data/team-acces/build-users.js
@@ -0,0 +1,43 @@
+import * as OidcIdentityProviders from '../../../../lib/domain/constants/oidc-identity-providers.js';
+
+function _buildUserWithCnavAuthenticationMethod(databaseBuilder) {
+  const user = databaseBuilder.factory.buildUser.withoutPixAuthenticationMethod({
+    firstName: 'David',
+    lastName: 'Cnav',
+  });
+
+  databaseBuilder.factory.buildAuthenticationMethod.withIdentityProvider({
+    identityProvider: OidcIdentityProviders.CNAV.service.code,
+    userId: user.id,
+  });
+}
+
+function _buildUserWithFwbAuthenticationMethod(databaseBuilder) {
+  const user = databaseBuilder.factory.buildUser.withoutPixAuthenticationMethod({
+    firstName: 'Henry',
+    lastName: 'Fwb',
+  });
+
+  databaseBuilder.factory.buildAuthenticationMethod.withIdentityProvider({
+    identityProvider: OidcIdentityProviders.FWB.service.code,
+    userId: user.id,
+  });
+}
+
+function _buildUserWithPoleEmploiAuthenticationMethod(databaseBuilder) {
+  const user = databaseBuilder.factory.buildUser.withoutPixAuthenticationMethod({
+    firstName: 'Paul',
+    lastName: 'Emploi',
+  });
+
+  databaseBuilder.factory.buildAuthenticationMethod.withIdentityProvider({
+    identityProvider: OidcIdentityProviders.POLE_EMPLOI.service.code,
+    userId: user.id,
+  });
+}
+
+export function buildUsers(databaseBuilder) {
+  _buildUserWithCnavAuthenticationMethod(databaseBuilder);
+  _buildUserWithFwbAuthenticationMethod(databaseBuilder);
+  _buildUserWithPoleEmploiAuthenticationMethod(databaseBuilder);
+}
