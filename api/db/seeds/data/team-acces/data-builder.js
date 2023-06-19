6364ee0b57764727228894570dffc800d658be59
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-06-19 14:14:29 +0200

    feat(api): add new seed builder for simple users

--- api/db/seeds/data/team-acces/data-builder.js
+++ api/db/seeds/data/team-acces/data-builder.js
@@ -1,12 +1,12 @@
 import { buildBlockedUsers } from './build-blocked-users.js';
 import { buildPixAdminRoles } from './build-pix-admin-roles.js';
 import { buildTemporaryBlockedUsers } from './build-temporary-blocked-user.js';
+import { buildUsers } from './build-users.js';
 
 async function teamAccesDataBuilder(databaseBuilder) {
-  /**
-   * TODO:
-   */
   buildPixAdminRoles(databaseBuilder);
+
+  buildUsers(databaseBuilder);
 
   buildBlockedUsers(databaseBuilder);
   buildTemporaryBlockedUsers(databaseBuilder);
