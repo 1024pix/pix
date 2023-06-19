915a4ba91b1ec597b149fe9f3a9a86718547829e
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-06-19 10:20:05 +0200

    feat(api): add new seed builder for PixAdmin roles

---
+++ api/db/seeds/data/team-acces/build-pix-admin-roles.js
@@ -0,0 +1,40 @@
+import { DEFAULT_PASSWORD } from './constants.js';
+import { PIX_ADMIN } from '../../../../lib/domain/constants.js';
+
+const { ROLES } = PIX_ADMIN;
+
+function _buildCertifRole(databaseBuilder) {
+  databaseBuilder.factory.buildUser.withRole({
+    firstName: 'Pix',
+    lastName: 'Certif',
+    email: 'pixcertif@example.net',
+    rawPassword: DEFAULT_PASSWORD,
+    role: ROLES.CERTIF,
+  });
+}
+
+function _buildMetierRole(databaseBuilder) {
+  databaseBuilder.factory.buildUser.withRole({
+    firstName: 'Pix',
+    lastName: 'Metier',
+    email: 'pixmetier@example.net',
+    rawPassword: DEFAULT_PASSWORD,
+    role: ROLES.METIER,
+  });
+}
+
+function _buildSupportRole(databaseBuilder) {
+  databaseBuilder.factory.buildUser.withRole({
+    firstName: 'Pix',
+    lastName: 'Support',
+    email: 'pixsupport@example.net',
+    rawPassword: DEFAULT_PASSWORD,
+    role: ROLES.SUPPORT,
+  });
+}
+
+export function buildPixAdminRoles(databaseBuilder) {
+  _buildSupportRole(databaseBuilder);
+  _buildCertifRole(databaseBuilder);
+  _buildMetierRole(databaseBuilder);
+}
--- api/db/seeds/data/team-acces/data-builder.js
+++ api/db/seeds/data/team-acces/data-builder.js
@@ -1,12 +1,13 @@
 import { buildBlockedUsers } from './build-blocked-users.js';
+import { buildPixAdminRoles } from './build-pix-admin-roles.js';
 import { buildTemporaryBlockedUsers } from './build-temporary-blocked-user.js';
 
 async function teamAccesDataBuilder(databaseBuilder) {
   /**
    * TODO:
-   * - pix admin roles (superadmin - différent du common?, metier, certif, support)
-   * - ... à continuer
    */
+  buildPixAdminRoles(databaseBuilder);
+
   buildBlockedUsers(databaseBuilder);
   buildTemporaryBlockedUsers(databaseBuilder);
 }
