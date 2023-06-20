a803f7564740c777ff15b70cd192b75c8c2598b3
Quentin Lebouc <quentin.lebouc@pix.fr>
2023-06-20 15:39:01 +0200

    :recycle: admin: replace faker usage for certification center factory by a fixed value

--- admin/mirage/factories/certification-center.js
+++ admin/mirage/factories/certification-center.js
@@ -1,9 +1,8 @@
 import { Factory } from 'miragejs';
-import { faker } from '@faker-js/faker';
 
 export default Factory.extend({
   name() {
-    return faker.company.name();
+    return 'Centre des certifs';
   },
 
   type() {
