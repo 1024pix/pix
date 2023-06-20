9d3bffa8df1bf97ba74c420ce22c68cc3c66284d
Quentin Lebouc <quentin.lebouc@pix.fr>
2023-06-20 15:51:01 +0200

    :recycle: certif: replace faker usage for student factory by fixed values

--- certif/mirage/factories/student.js
+++ certif/mirage/factories/student.js
@@ -1,21 +1,20 @@
 import { Factory } from 'miragejs';
-import { faker } from '@faker-js/faker';
 import dayjs from 'dayjs';
 
 export default Factory.extend({
   firstName() {
-    return faker.person.firstName();
+    return 'Barish';
   },
 
   lastName() {
-    return faker.person.lastName();
+    return 'Joel';
   },
 
   birthdate() {
-    return dayjs(faker.date.past({ years: 30 })).format('YYYY-MM-DD');
+    return dayjs('1975-10-18');
   },
 
   division() {
-    return faker.lorem.word();
+    return '3emeB';
   },
 });
