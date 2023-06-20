27776e2a0f3cae17e763035b4f68c7e045c30427
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-06-20 10:36:02 +0200

    feat(api): auto import database builder factory

--- api/db/database-builder/database-builder.js
+++ api/db/database-builder/database-builder.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import bluebird from 'bluebird';
-import * as factory from './factory/index.js';
+import { factory } from './factory/index.js';
 import { databaseBuffer } from './database-buffer.js';
 
 class DatabaseBuilder {
