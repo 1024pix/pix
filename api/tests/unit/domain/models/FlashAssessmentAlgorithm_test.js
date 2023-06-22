fbb6b3abdd17e965da6e308bfc767876f54799c1
cberthou <clement.berthou.tsp@gmail.com>
2023-06-22 15:10:52 +0200

    :sparkles: api: end next gen certification when max answers count is reached

---
+++ api/tests/unit/domain/models/FlashAssessmentAlgorithm_test.js
@@ -0,0 +1,31 @@
+import { FlashAssessmentAlgorithm } from '../../../../lib/domain/models/index.js';
+import { domainBuilder, expect } from '../../../test-helper.js';
+import { AssessmentEndedError } from '../../../../lib/domain/errors.js';
+
+describe('FlashAssessmentAlgorithm', function () {
+  describe('#getPossibleNextChallenges', function () {
+    context('when enough challenges have been answered', function () {
+      it('should throw an AssessmentEndedError', function () {
+        const allAnswers = [domainBuilder.buildAnswer({ id: 1 }), domainBuilder.buildAnswer({ id: 2 })];
+        const skill1 = domainBuilder.buildSkill({ id: 1 });
+        const skill2 = domainBuilder.buildSkill({ id: 2 });
+        const challenges = [
+          domainBuilder.buildChallenge({ id: allAnswers[0].challengeId, skill: skill1 }),
+          domainBuilder.buildChallenge({ competenceId: 'comp2', skill: skill2 }),
+        ];
+        const estimatedLevel = 0;
+        const algorithm = new FlashAssessmentAlgorithm({
+          maximumAssessmentLength: 2,
+        });
+
+        expect(() =>
+          algorithm.getPossibleNextChallenges({
+            allAnswers,
+            challenges,
+            estimatedLevel,
+          })
+        ).to.throw(AssessmentEndedError);
+      });
+    });
+  });
+});
