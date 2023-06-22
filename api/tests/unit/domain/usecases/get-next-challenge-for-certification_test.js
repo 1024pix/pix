72598b057b31ee86eddbd11ce7589d9fc6890d6a
cberthou <clement.berthou.tsp@gmail.com>
2023-06-22 11:58:42 +0200

    :sparkles: api: use v3 algorithm to pick next challenge in v3 certification

--- api/tests/unit/domain/usecases/get-next-challenge-for-certification_test.js
+++ api/tests/unit/domain/usecases/get-next-challenge-for-certification_test.js
@@ -1,54 +1,131 @@
-import { expect, sinon } from '../../../test-helper.js';
+import { domainBuilder, expect, sinon } from '../../../test-helper.js';
 import { getNextChallengeForCertification } from '../../../../lib/domain/usecases/get-next-challenge-for-certification.js';
 import { Assessment } from '../../../../lib/domain/models/Assessment.js';
+import { CertificationVersion } from '../../../../lib/domain/models/CertificationVersion.js';
 
-describe('Unit | Domain | Use Cases | get-next-challenge-for-certification', function () {
+describe('Unit | Domain | Use Cases | get-next-challenge-for-certification', function () {
   describe('#getNextChallengeForCertification', function () {
-    let certificationChallengeRepository;
-    let challengeRepository;
+    context('for a v2 certification', function () {
+      let certificationChallengeRepository;
+      let challengeRepository;
+      let certificationCourseRepository;
 
-    beforeEach(function () {
-      certificationChallengeRepository = { getNextNonAnsweredChallengeByCourseId: sinon.stub().resolves() };
-      challengeRepository = { get: sinon.stub().resolves() };
-    });
-
-    it('should use the assessmentService to select the next CertificationChallenge', async function () {
-      // given
-      const nextChallenge = Symbol('nextChallenge');
-      const assessment = new Assessment({ id: 156, certificationCourseId: 54516 });
-
-      certificationChallengeRepository.getNextNonAnsweredChallengeByCourseId.resolves(nextChallenge);
-
-      // when
-      await getNextChallengeForCertification({ assessment, certificationChallengeRepository, challengeRepository });
-
-      // then
-      expect(certificationChallengeRepository.getNextNonAnsweredChallengeByCourseId).to.have.been.calledWith(
-        156,
-        54516
-      );
-    });
-
-    it('should return the next Challenge', async function () {
-      // given
-      const challengeId = 15167432;
-      const nextChallengeToAnswer = Symbol('nextChallengeToAnswer');
-      const nextCertificationChallenge = { challengeId };
-      const assessment = new Assessment({ id: 156, courseId: 54516 });
-
-      certificationChallengeRepository.getNextNonAnsweredChallengeByCourseId.resolves(nextCertificationChallenge);
-      challengeRepository.get.resolves(nextChallengeToAnswer);
-
-      // when
-      const challenge = await getNextChallengeForCertification({
-        assessment,
-        certificationChallengeRepository,
-        challengeRepository,
+      beforeEach(function () {
+        certificationCourseRepository = { get: sinon.stub() };
+        certificationChallengeRepository = { getNextNonAnsweredChallengeByCourseId: sinon.stub().resolves() };
+        challengeRepository = { get: sinon.stub().resolves() };
       });
 
-      // then
-      expect(challenge).to.equal(nextChallengeToAnswer);
-      expect(challengeRepository.get).to.have.been.calledWith(challengeId);
+      it('should use the assessmentService to select the next CertificationChallenge', async function () {
+        // given
+        const nextChallenge = Symbol('nextChallenge');
+        const assessment = new Assessment({ id: 156, certificationCourseId: 54516 });
+        const certificationCourse = domainBuilder.buildCertificationCourse();
+
+        certificationChallengeRepository.getNextNonAnsweredChallengeByCourseId.resolves(nextChallenge);
+        certificationCourseRepository.get.withArgs(assessment.certificationCourseId).resolves(certificationCourse);
+
+        // when
+        await getNextChallengeForCertification({
+          assessment,
+          certificationChallengeRepository,
+          certificationCourseRepository,
+          challengeRepository,
+        });
+
+        // then
+        expect(certificationChallengeRepository.getNextNonAnsweredChallengeByCourseId).to.have.been.calledWith(
+          156,
+          54516
+        );
+      });
+
+      it('should return the next Challenge', async function () {
+        // given
+        const challengeId = 15167432;
+        const nextChallengeToAnswer = Symbol('nextChallengeToAnswer');
+        const nextCertificationChallenge = { challengeId };
+        const assessment = new Assessment({ id: 156, courseId: 54516 });
+        const certificationCourse = domainBuilder.buildCertificationCourse();
+
+        certificationChallengeRepository.getNextNonAnsweredChallengeByCourseId.resolves(nextCertificationChallenge);
+        challengeRepository.get.resolves(nextChallengeToAnswer);
+        certificationCourseRepository.get.withArgs(assessment.certificationCourseId).resolves(certificationCourse);
+
+        // when
+        const challenge = await getNextChallengeForCertification({
+          assessment,
+          certificationChallengeRepository,
+          certificationCourseRepository,
+          challengeRepository,
+        });
+
+        // then
+        expect(challenge).to.equal(nextChallengeToAnswer);
+        expect(challengeRepository.get).to.have.been.calledWith(challengeId);
+      });
+    });
+
+    context('for a v3 certification', function () {
+      it('should return the next Challenge', async function () {
+        // given
+        const answerRepository = Symbol('AnswerRepository');
+        const challengeRepository = Symbol('ChallengeRepository');
+        const flashAssessmentResultRepository = Symbol('flashAssessmentResultRepository');
+        const nextChallengeToAnswer = domainBuilder.buildChallenge();
+        const v3CertificationCourse = domainBuilder.buildCertificationCourse({
+          version: CertificationVersion.V3,
+        });
+        const assessment = domainBuilder.buildAssessment();
+        const certificationCourseRepository = {
+          get: sinon.stub(),
+        };
+        const algorithmDataFetcherService = {
+          fetchForFlashCampaigns: sinon.stub(),
+        };
+        const pickChallengeService = {
+          chooseNextChallenge: sinon.stub(),
+        };
+        const locale = 'fr-FR';
+
+        certificationCourseRepository.get.withArgs(assessment.certificationCourseId).resolves(v3CertificationCourse);
+        algorithmDataFetcherService.fetchForFlashCampaigns
+          .withArgs({
+            answerRepository,
+            challengeRepository,
+            flashAssessmentResultRepository,
+            assessmentId: assessment.id,
+            locale,
+          })
+          .resolves({
+            allAnswers: [],
+            challenges: [nextChallengeToAnswer],
+            estimatedLevel: 0,
+          });
+
+        const chooseNextChallengeImpl = sinon.stub();
+        chooseNextChallengeImpl
+          .withArgs({
+            possibleChallenges: [nextChallengeToAnswer],
+          })
+          .returns(nextChallengeToAnswer);
+        pickChallengeService.chooseNextChallenge.withArgs(assessment.id).returns(chooseNextChallengeImpl);
+
+        // when
+        const challenge = await getNextChallengeForCertification({
+          algorithmDataFetcherService,
+          assessment,
+          answerRepository,
+          challengeRepository,
+          flashAssessmentResultRepository,
+          pickChallengeService,
+          certificationCourseRepository,
+          locale,
+        });
+
+        // then
+        expect(challenge).to.equal(nextChallengeToAnswer);
+      });
     });
   });
 });
