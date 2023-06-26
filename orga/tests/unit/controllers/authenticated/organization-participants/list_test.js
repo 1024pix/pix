b1c60042d73e040f60cc167e9f5474b367b677e3
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2023-06-26 15:00:16 +0200

    feat(orga): add success toaster

--- orga/tests/unit/controllers/authenticated/organization-participants/list_test.js
+++ orga/tests/unit/controllers/authenticated/organization-participants/list_test.js
@@ -1,8 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import { setupIntl } from 'ember-intl/test-support';
+import sinon from 'sinon';
 
 module('Unit | Controller | authenticated/organization-participants', function (hooks) {
   setupTest(hooks);
+  setupIntl(hooks);
 
   module('#triggerFiltering', function () {
     module('when the filters contain the field fullName', function () {
@@ -50,6 +53,37 @@ module('Unit | Controller | authenticated/organization-participants', function (
       // then
       assert.strictEqual(controller.fullName, null);
       assert.strictEqual(controller.pageNumber, null);
+    });
+  });
+
+  module('#deleteOrganizationLearners', function (hooks) {
+    let controller;
+    let deleteParticipants;
+    hooks.beforeEach(function () {
+      const store = this.owner.lookup('service:store');
+      const adapter = store.adapterFor('organization-participant');
+      deleteParticipants = sinon.stub(adapter, 'deleteParticipants');
+      controller = this.owner.lookup('controller:authenticated/organization-participants/list');
+      controller.notifications = { sendSuccess: sinon.stub() };
+      controller.send = sinon.stub();
+      controller.currentUser = { organization: { id: 1 } };
+    });
+
+    test('success case', async function(assert) {
+      //given
+      const listLearners = [{ id: 1, firstName: 'Red', lastName: 'Bull' }];
+      deleteParticipants.resolves();  
+
+      // when
+      await controller.deleteOrganizationLearners(listLearners);
+
+      //then
+      assert.true(deleteParticipants.calledWith(1, [1]));
+      assert.true(controller.send.calledWith('refreshModel'));
+      assert.true(controller.notifications.sendSuccess.calledWith(
+        this.intl.t('pages.organization-participants.action-bar.success-message', 
+        { count: listLearners.length, firstname: listLearners[0].firstName, lastname: listLearners[0].lastName})
+      ));
     });
   });
 });
