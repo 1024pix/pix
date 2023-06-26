0c8c46da755dcf5b54a67c807ee288f82851350f
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2023-06-26 09:05:59 +0200

    refactor(api): add missing test on authenticationRegistry + fix actual sync tests

--- api/tests/unit/domain/services/authentication/authentication-service-registry_test.js
+++ api/tests/unit/domain/services/authentication/authentication-service-registry_test.js
@@ -1,26 +1,38 @@
-import { expect, catchErr } from '../../../../test-helper.js';
+import { expect, catchErrSync } from '../../../../test-helper.js';
 import * as authenticationRegistry from '../../../../../lib/domain/services/authentication/authentication-service-registry.js';
 import { InvalidIdentityProviderError } from '../../../../../lib/domain/errors.js';
 
 describe('Unit | Domain | Services | authentication registry', function () {
+  describe('#getOidcProviderServices', function () {
+    it('returns all ready OIDC Providers', async function () {
+      // when
+      const services = authenticationRegistry.getOidcProviderServices();
+
+      // then
+      const serviceCodes = services.map((service) => service.code);
+      expect(serviceCodes).to.contain('POLE_EMPLOI');
+      expect(serviceCodes).to.contain('CNAV');
+    });
+  });
+
   describe('#getOidcProviderServiceByCode', function () {
-    it('finds an OIDC Provider', async function () {
+    it('returns a ready OIDC Provider', function () {
       // given
       const identityProvider = 'POLE_EMPLOI';
 
       // when
-      const service = await authenticationRegistry.getOidcProviderServiceByCode(identityProvider);
+      const service = authenticationRegistry.getOidcProviderServiceByCode(identityProvider);
 
       // then
       expect(service.code).to.equal('POLE_EMPLOI');
     });
 
-    it('throws an error when identity provider is not supported', async function () {
+    it('throws an error when identity provider is not supported', function () {
       // given
       const identityProvider = 'UNSUPPORTED_OIDC_PROVIDER';
 
       // when
-      const error = await catchErr(authenticationRegistry.getOidcProviderServiceByCode)(identityProvider);
+      const error = catchErrSync(authenticationRegistry.getOidcProviderServiceByCode)(identityProvider);
 
       // then
       expect(error).to.be.an.instanceOf(InvalidIdentityProviderError);
