ace30d0e76c62e69b75ce06816464e76109f23fe
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2023-06-28 11:36:14 +0200

    feat(orga) Add feature toggle to activate delete participant

--- orga/app/components/organization-participant/list.js
+++ orga/app/components/organization-participant/list.js
@@ -3,14 +3,16 @@ import { action } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-
+import ENV from 'pix-orga/config/environment';
 export default class List extends Component {
   @tracked showDeletionModal = false;
 
   @service currentUser;
 
-  get isUserAdmin() {
-    return this.currentUser.isAdminInOrganization;
+  get canDeleteParticipant() {
+    const isFeatureEnabled = ENV.APP.FT_DELETE_PARTICIPANT;
+
+    return isFeatureEnabled && this.currentUser.isAdminInOrganization;
   }
 
   get headerId() {
