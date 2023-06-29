1e7bdc4dcfd5a609f8b648bd5b67474e0dc4e97d
alexandrecoin <alexandre.pc.coin@gmail.com>
2023-06-29 09:07:56 +0200

    :sparkles: api: allow V3 pilot certif centers to mass import sessions

--- api/lib/domain/usecases/sessions-mass-import/create-sessions.js
+++ api/lib/domain/usecases/sessions-mass-import/create-sessions.js
@@ -3,12 +3,15 @@ import bluebird from 'bluebird';
 import { DomainTransaction } from '../../../infrastructure/DomainTransaction.js';
 import { Session } from '../../models/Session.js';
 import { CertificationCandidate } from '../../models/CertificationCandidate.js';
+import { CertificationVersion } from '../../models/CertificationVersion.js';
 
 const createSessions = async function ({
   userId,
   cachedValidatedSessionsKey,
+  certificationCenterId,
   certificationCandidateRepository,
   sessionRepository,
+  certificationCenterRepository,
   temporarySessionsStorageForMassImportService,
 }) {
   const temporaryCachedSessions = await temporarySessionsStorageForMassImportService.getByKeyAndUserId({
@@ -19,6 +22,8 @@ const createSessions = async function ({
   if (!temporaryCachedSessions) {
     throw new NotFoundError();
   }
+
+  const { isV3Pilot } = await certificationCenterRepository.get(certificationCenterId);
 
   await DomainTransaction.execute(async (domainTransaction) => {
     return await bluebird.mapSeries(temporaryCachedSessions, async (sessionDTO) => {
@@ -32,6 +37,7 @@ const createSessions = async function ({
           sessionRepository,
           sessionDTO,
           domainTransaction,
+          isV3Pilot,
         });
         sessionId = id;
       }
@@ -59,8 +65,11 @@ function _hasCandidates(certificationCandidates) {
   return certificationCandidates.length > 0;
 }
 
-async function _saveNewSessionReturningId({ sessionRepository, sessionDTO, domainTransaction }) {
-  const sessionToSave = new Session({ ...sessionDTO });
+async function _saveNewSessionReturningId({ sessionRepository, sessionDTO, domainTransaction, isV3Pilot }) {
+  const sessionToSave = new Session({
+    ...sessionDTO,
+    version: isV3Pilot ? CertificationVersion.V3 : CertificationVersion.V2,
+  });
   return await sessionRepository.save(sessionToSave, domainTransaction);
 }
 
