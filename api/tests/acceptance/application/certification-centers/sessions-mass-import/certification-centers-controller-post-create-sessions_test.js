926fd4f1855762aa114bc462a45cf4ac16c56b2c
alexandrecoin <alexandre.pc.coin@gmail.com>
2023-06-29 09:07:56 +0200

    :sparkles: api: allow V3 pilot certif centers to mass import sessions

--- api/tests/acceptance/application/certification-centers/sessions-mass-import/certification-centers-controller-post-create-sessions_test.js
+++ api/tests/acceptance/application/certification-centers/sessions-mass-import/certification-centers-controller-post-create-sessions_test.js
@@ -11,58 +11,121 @@ describe('Acceptance | Controller | certification-centers-controller-post-create
       await knex('users').delete();
     });
 
-    context('when user confirm sessions for import', function () {
-      it('should return status 201', async function () {
-        // given
-        const userId = databaseBuilder.factory.buildUser().id;
-        const { name: certificationCenter, id: certificationCenterId } =
-          databaseBuilder.factory.buildCertificationCenter({
-            type: 'SUP',
-            externalId: '1234AB',
+    context('when certification center is not V3 Pilot', function () {
+      context('when user confirm sessions for import', function () {
+        it('should return status 201', async function () {
+          // given
+          const userId = databaseBuilder.factory.buildUser().id;
+          const { name: certificationCenter, id: certificationCenterId } =
+            databaseBuilder.factory.buildCertificationCenter({
+              type: 'SUP',
+              externalId: '1234AB',
+              isV3Pilot: false,
+            });
+          databaseBuilder.factory.buildOrganization({ externalId: '1234AB', isManagingStudents: false, type: 'SUP' });
+          databaseBuilder.factory.buildCertificationCenterMembership({
+            userId,
+            certificationCenterId,
           });
-        databaseBuilder.factory.buildOrganization({ externalId: '1234AB', isManagingStudents: false, type: 'SUP' });
-        databaseBuilder.factory.buildCertificationCenterMembership({
-          userId,
-          certificationCenterId,
+          const sessionToSave = {
+            id: undefined,
+            certificationCenterId,
+            certificationCenter,
+            address: '3 rue du Chateau',
+            examiner: 'John Smith',
+            room: 'room',
+            date: '2023-01-01',
+            time: '11:00',
+            accessCode: 'accessCode',
+            supervisorPassword: 'KV2CP',
+            certificationCandidates: [],
+          };
+          const newCachedSessionUUID = await temporarySessionsStorageForMassImportService.save({
+            sessions: [sessionToSave],
+            userId,
+          });
+          await databaseBuilder.commit();
+          const server = await createServer();
+
+          const options = {
+            method: 'POST',
+            url: `/api/certification-centers/${certificationCenterId}/sessions/confirm-for-mass-import`,
+            headers: {
+              authorization: generateValidRequestAuthorizationHeader(userId),
+            },
+            payload: { data: { attributes: { cachedValidatedSessionsKey: newCachedSessionUUID } } },
+          };
+
+          // when
+          const response = await server.inject(options);
+
+          // then
+          const sessions = await knex('sessions');
+          expect(sessions.length).to.equal(1);
+          expect(sessions[0].certificationCenter).to.equal(certificationCenter);
+          expect(sessions[0].supervisorPassword).to.equal(sessionToSave.supervisorPassword);
+          expect(sessions[0].version).to.equal(2);
+          expect(response.statusCode).to.equal(201);
         });
-        const sessionToSave = {
-          id: undefined,
-          certificationCenterId,
-          certificationCenter,
-          address: '3 rue du Chateau',
-          examiner: 'John Smith',
-          room: 'room',
-          date: '2023-01-01',
-          time: '11:00',
-          accessCode: 'accessCode',
-          supervisorPassword: 'KV2CP',
-          certificationCandidates: [],
-        };
-        const newCachedSessionUUID = await temporarySessionsStorageForMassImportService.save({
-          sessions: [sessionToSave],
-          userId,
+      });
+    });
+
+    context('when certification center is V3 Pilot', function () {
+      context('when user confirm sessions for import', function () {
+        it('should return status 201', async function () {
+          // given
+          const userId = databaseBuilder.factory.buildUser().id;
+          const { name: certificationCenter, id: certificationCenterId } =
+            databaseBuilder.factory.buildCertificationCenter({
+              type: 'SUP',
+              externalId: '1234AB',
+              isV3Pilot: true,
+            });
+          databaseBuilder.factory.buildOrganization({ externalId: '1234AB', isManagingStudents: false, type: 'SUP' });
+          databaseBuilder.factory.buildCertificationCenterMembership({
+            userId,
+            certificationCenterId,
+          });
+          const sessionToSave = {
+            id: undefined,
+            certificationCenterId,
+            certificationCenter,
+            address: '3 rue du Chateau',
+            examiner: 'John Smith',
+            room: 'room',
+            date: '2023-01-01',
+            time: '11:00',
+            accessCode: 'accessCode',
+            supervisorPassword: 'KV2CP',
+            certificationCandidates: [],
+          };
+          const newCachedSessionUUID = await temporarySessionsStorageForMassImportService.save({
+            sessions: [sessionToSave],
+            userId,
+          });
+          await databaseBuilder.commit();
+          const server = await createServer();
+
+          const options = {
+            method: 'POST',
+            url: `/api/certification-centers/${certificationCenterId}/sessions/confirm-for-mass-import`,
+            headers: {
+              authorization: generateValidRequestAuthorizationHeader(userId),
+            },
+            payload: { data: { attributes: { cachedValidatedSessionsKey: newCachedSessionUUID } } },
+          };
+
+          // when
+          const response = await server.inject(options);
+
+          // then
+          const sessions = await knex('sessions');
+          expect(sessions.length).to.equal(1);
+          expect(sessions[0].certificationCenter).to.equal(certificationCenter);
+          expect(sessions[0].supervisorPassword).to.equal(sessionToSave.supervisorPassword);
+          expect(sessions[0].version).to.equal(3);
+          expect(response.statusCode).to.equal(201);
         });
-        await databaseBuilder.commit();
-        const server = await createServer();
-
-        const options = {
-          method: 'POST',
-          url: `/api/certification-centers/${certificationCenterId}/sessions/confirm-for-mass-import`,
-          headers: {
-            authorization: generateValidRequestAuthorizationHeader(userId),
-          },
-          payload: { data: { attributes: { cachedValidatedSessionsKey: newCachedSessionUUID } } },
-        };
-
-        // when
-        const response = await server.inject(options);
-
-        // then
-        const sessions = await knex('sessions');
-        expect(sessions.length).to.equal(1);
-        expect(sessions[0].certificationCenter).to.equal(certificationCenter);
-        expect(sessions[0].supervisorPassword).to.equal(sessionToSave.supervisorPassword);
-        expect(response.statusCode).to.equal(201);
       });
     });
   });
