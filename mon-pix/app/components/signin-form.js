8bbb880101fa9732a33bd34102166ff8939fc772
Eithliu <iris.benoit@pix.fr>
2023-07-03 16:36:53 +0200

    add webcomponent in login page (signinform)
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>
    Co-authored-by: LEGO Technix <109212476+lego-technix@users.noreply.github.com>
    Co-authored-by: Rebecca Kaci <rebecca.kaci@pix.fr>

--- mon-pix/app/components/signin-form.js
+++ mon-pix/app/components/signin-form.js
@@ -2,7 +2,7 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-
+import '@1024pix/pix-ui-webcomponents';
 import ENV from 'mon-pix/config/environment';
 import get from 'lodash/get';
 
