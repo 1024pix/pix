e26b324d5ea6749b9638865e3c97164dbc855d69
Guillaume Lagorce <guillaume@hey-gul.com>
2023-07-06 11:36:49 +0200

    feat(api): add parsing and campaign creation in controller
    Co-authored-by: Marie-Hélène Rots <machestla@users.noreply.github.com>

--- api/lib/application/campaigns-administration/campaign-controller.js
+++ api/lib/application/campaigns-administration/campaign-controller.js
@@ -1,5 +1,6 @@
 import { usecases } from '../../domain/usecases/index.js';
 import * as csvCampaignsIdsParser from '../../infrastructure/serializers/csv/campaigns-administration/csv-campaigns-ids-parser.js';
+import * as csvSerializer from '../../infrastructure/serializers/csv/csv-serializer.js';
 
 const archiveCampaigns = async function (request, h, dependencies = { csvCampaignsIdsParser }) {
   const { userId } = request.auth.credentials;
@@ -13,7 +14,9 @@ const archiveCampaigns = async function (request, h, dependencies = { csvCampaig
   return h.response(null).code(204);
 };
 
-const createCampaigns = async function (_, h) {
+const createCampaigns = async function (request, h, dependencies = { csvSerializer }) {
+  const campaignsToCreate = await dependencies.csvSerializer.deserializeForCampaignsImport(request.payload.path);
+  await usecases.createCampaigns({ campaignsToCreate });
   return h.response(null).code(204);
 };
 
