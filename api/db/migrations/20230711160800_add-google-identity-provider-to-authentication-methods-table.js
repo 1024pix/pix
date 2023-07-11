7a9336496fd495d5795136d8bc0a9d29db6c7d1f
Andreia Pena <andreia.pena@pix.fr>
2023-07-11 16:15:05 +0200

    api : add google identity provider check on authentication methods table

---
+++ api/db/migrations/20230711160800_add-google-identity-provider-to-authentication-methods-table.js
@@ -0,0 +1,19 @@
+const up = async function (knex) {
+  await knex.raw(
+    'ALTER TABLE "authentication-methods" DROP CONSTRAINT "authentication_methods_identityProvider_check" '
+  );
+  return knex.raw(
+    "ALTER TABLE \"authentication-methods\" ADD CONSTRAINT \"authentication_methods_identityProvider_check\" CHECK ( \"identityProvider\" IN ('PIX', 'GAR', 'POLE_EMPLOI', 'CNAV', 'FWB', 'GOOGLE') )"
+  );
+};
+
+const down = async function (knex) {
+  await knex.raw(
+    'ALTER TABLE "authentication-methods" DROP CONSTRAINT "authentication_methods_identityProvider_check" '
+  );
+  return knex.raw(
+    "ALTER TABLE \"authentication-methods\" ADD CONSTRAINT \"authentication_methods_identityProvider_check\" CHECK ( \"identityProvider\" IN ('PIX', 'GAR', 'POLE_EMPLOI', 'CNAV', 'FWB') )"
+  );
+};
+
+export { up, down };
