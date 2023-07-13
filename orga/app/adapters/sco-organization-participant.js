49a5c3dc55154f3b899448141dabd029f83985ee
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-07-13 11:07:32 +0200

    feat(orga): reset students password
    Co-authored-by: Eric Lim <eric.lim@zenika.com>

--- orga/app/adapters/sco-organization-participant.js
+++ orga/app/adapters/sco-organization-participant.js
@@ -7,4 +7,15 @@ export default class ScoOrganizationParticipantAdapter extends ApplicationAdapte
 
     return `${this.host}/${this.namespace}/organizations/${organizationId}/sco-participants`;
   }
+
+  resetOrganizationLearnersPassword(organizationId, organizationLearnersIds) {
+    const url = `${this.host}/${this.namespace}/sco-organization-learners/passwords`;
+    return this.ajax(url, 'PUT', {
+      data: {
+        data: {
+          attributes: { 'organization-id': organizationId, 'organization-learners-id': organizationLearnersIds },
+        },
+      },
+    });
+  }
 }
