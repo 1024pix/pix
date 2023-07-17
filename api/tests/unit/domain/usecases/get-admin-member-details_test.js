bd1d917387caf043af9c0e29e599f25b98dc6cad
Andreia Pena <andreia.pena@pix.fr>
2023-07-17 16:12:10 +0200

    ♻️ api: add pre-handler in /admin-members/me ROUTE

--- api/tests/unit/domain/usecases/get-admin-member-details_test.js
+++ api/tests/unit/domain/usecases/get-admin-member-details_test.js
@@ -1,39 +1,19 @@
-import { NotFoundError } from '../../../../lib/domain/errors.js';
 import { getAdminMemberDetails } from '../../../../lib/domain/usecases/get-admin-member-details.js';
-import { catchErr, domainBuilder, expect, sinon } from '../../../test-helper.js';
+import { domainBuilder, expect, sinon } from '../../../test-helper.js';
 
 describe('Unit | UseCase | get-admin-member-details', function () {
-  context('when it exists', function () {
-    it('should return an admin member details', async function () {
-      // given
-      const adminMemberRepository = {
-        get: sinon.stub(),
-      };
-      const adminMember = domainBuilder.buildAdminMember();
-      adminMemberRepository.get.withArgs({ userId: adminMember.id }).resolves(adminMember);
+  it('should return an admin member details', async function () {
+    // given
+    const adminMemberRepository = {
+      get: sinon.stub(),
+    };
+    const adminMember = domainBuilder.buildAdminMember();
+    adminMemberRepository.get.withArgs({ userId: adminMember.id }).resolves(adminMember);
 
-      // when
-      const adminMemberDetails = await getAdminMemberDetails({ adminMemberRepository, userId: adminMember.id });
+    // when
+    const adminMemberDetails = await getAdminMemberDetails({ adminMemberRepository, userId: adminMember.id });
 
-      // then
-      expect(adminMemberDetails).to.deep.equal(adminMember);
-    });
-  });
-
-  context('when it does not exist', function () {
-    it('should thrown a NotFound error', async function () {
-      // given
-      const adminMemberRepository = {
-        get: sinon.stub(),
-      };
-      const adminMember = domainBuilder.buildAdminMember();
-      adminMemberRepository.get.withArgs({ userId: adminMember.id }).resolves(undefined);
-
-      // when
-      const error = await catchErr(getAdminMemberDetails)({ adminMemberRepository, userId: adminMember.id });
-
-      // then
-      expect(error).to.be.instanceOf(NotFoundError);
-    });
+    // then
+    expect(adminMemberDetails).to.deep.equal(adminMember);
   });
 });
