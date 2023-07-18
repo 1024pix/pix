2a67f44372a8923ee6960dd0e1efbfa93026acc0
Matthias FERRAINA <matthias.ferraina@pix.fr>
2023-07-18 10:57:49 +0200

    feat(api): send doughnut chart to slack

--- api/scripts/arborescence-monitoring.js
+++ api/scripts/arborescence-monitoring.js
@@ -22,22 +22,89 @@ function formatResult({ path, count }) {
   };
 }
 
-function _formatMessageForSlack(message) {
-  return JSON.stringify({
-    text: message,
-  });
+function _formatMessageForSlack(image_url) {
+  const slackBlocks = {
+    blocks: [
+      {
+        type: 'image',
+        image_url,
+        alt_text: 'inspiration',
+      },
+    ],
+  };
+  return JSON.stringify(slackBlocks);
 }
 
 async function main() {
-  const pathsToAnalyse = ['./lib', './src'];
+  const pathsToAnalyse = ['./src', './lib'];
   const result = await Promise.all(
     pathsToAnalyse.map(async (path) => {
       const count = await countFilesInPath(path);
       return formatResult({ path, count });
     })
   );
+  const data = result.map(({ count }) => count);
+  const chartUrl = getDoughnutChartUrl(data);
+  return console.log(_formatMessageForSlack(chartUrl));
+}
 
-  return console.log(_formatMessageForSlack(JSON.stringify(result)));
+function getDoughnutChartUrl(data) {
+  const backgroundColor = 'snow';
+  const doughnutChartConfiguration = generateDoughnutChartConfiguration(data);
+  const encodedChartConfiguration = encodeURIComponent(JSON.stringify(doughnutChartConfiguration));
+  return `https://quickchart.io/chart?c=${encodedChartConfiguration}&backgroundColor=${backgroundColor}`;
+}
+
+function generateDoughnutChartConfiguration(data) {
+  return {
+    type: 'doughnut',
+    data: {
+      datasets: [
+        {
+          data,
+          backgroundColor: ['green', '#eee'],
+          label: 'Dataset 1',
+          borderWidth: 0,
+        },
+      ],
+      labels: ['src', 'lib'],
+    },
+    options: {
+      circumference: Math.PI,
+      rotation: Math.PI,
+      cutoutPercentage: 90,
+      layout: {
+        padding: 60,
+      },
+      legend: {
+        display: true,
+        position: 'bottom',
+      },
+      plugins: {
+        datalabels: {
+          color: '#404040',
+          anchor: 'end',
+          align: 'end',
+          font: {
+            size: 25,
+            weight: 'bold',
+          },
+        },
+        doughnutlabel: {
+          labels: [
+            {
+              text: '\n\nmigration progress',
+              color: '#000',
+              font: {
+                size: 22,
+                weight: 'bold',
+              },
+            },
+          ],
+        },
+      },
+    },
+  };
 }
 
 await main();
