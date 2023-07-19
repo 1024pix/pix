d882516147802b0f316ce1288f85449d51d3f3cf
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-07-19 14:39:59 +0200

    feat(api): verify new attribute with Joi
    Co-authored-by: Guillaume Lagorce <guillaume.lagorce@pix.fr>
    Co-authored-by: Alexandre Monney <alexandre.monney77@gmail.com>
    Co-authored-by: miranda.lin-guignard <miranda.w.lin@gmail.com>

--- api/lib/application/target-profiles/index.js
+++ api/lib/application/target-profiles/index.js
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
-import { sendJsonApiError, BadRequestError } from '../http-errors.js';
+import { BadRequestError, sendJsonApiError } from '../http-errors.js';
 import { securityPreHandlers } from '../security-pre-handlers.js';
 import { targetProfileController } from './target-profile-controller.js';
 import { identifiersType } from '../../domain/types/identifiers-type.js';
@@ -190,6 +190,7 @@ const register = async function (server) {
                     level: Joi.number().required(),
                   })
                   .required(),
+                'are-knowledge-elements-resettable': Joi.boolean().required(),
               },
             },
           }),
