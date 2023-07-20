b826ab48c2784a786e77a3727c9a59da0782ee04
Andreia Pena <andreia.pena@pix.fr>
2023-07-20 17:36:55 +0200

    ✨ admin: fill information into first section
    Co-authored-by: Marianne Bost <marianne.bost@pix.fr>

--- admin/app/models/complementary-certification.js
+++ admin/app/models/complementary-certification.js
@@ -3,4 +3,5 @@ import Model, { attr } from '@ember-data/model';
 export default class ComplementaryCertification extends Model {
   @attr() key;
   @attr() label;
+  @attr() currentTargetProfile;
 }
