a17ae91d0067fae654ba1ab99c51eda27d6da2b8
Andreia Pena <andreia.pena@pix.fr>
2023-07-21 14:36:15 +0200

    ✨ admin:  allow access to complementary certification details page

--- admin/app/routes/authenticated/complementary-certifications/complementary-certification/details.js
+++ admin/app/routes/authenticated/complementary-certifications/complementary-certification/details.js
@@ -1,6 +1,13 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
 export default class ComplementaryCertificationDetailsRoute extends Route {
+  @service accessControl;
+
+  beforeModel() {
+    this.accessControl.restrictAccessTo(['isSuperAdmin', 'isMetier', 'isSupport'], 'authenticated');
+  }
+
   async model() {
     return this.modelFor('authenticated.complementary-certifications.complementary-certification');
   }
