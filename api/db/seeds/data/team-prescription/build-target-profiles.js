3ef32e77d1e171da805a906f99ffc387aa7daf5e
Alexandre Monney <alexandre.monney@pix.fr>
2023-07-21 16:49:33 +0200

    chore(api): generate correct seed for prescription and new target profile seed

---
+++ api/db/seeds/data/team-prescription/build-target-profiles.js
@@ -0,0 +1,31 @@
+import { createTargetProfile } from '../common/tooling/target-profile-tooling.js';
+import { SCO_ORGANIZATION_ID, TARGET_PROFILE_ID } from './constants.js';
+
+async function _createTargetProfile(databaseBuilder) {
+  const configTargetProfile = {
+    frameworks: [
+      {
+        chooseCoreFramework: true,
+        countTubes: 30,
+        minLevel: 3,
+        maxLevel: 5,
+      },
+    ],
+  };
+
+  await createTargetProfile({
+    databaseBuilder,
+    targetProfileId: TARGET_PROFILE_ID,
+    name: 'Profil cible Pur Pix (Niv3 ~ 5)',
+    ownerOrganizationId: SCO_ORGANIZATION_ID,
+    isPublic: true,
+    isSimplifiedAccess: false,
+    description: 'Profil cible pur pix (Niv3 ~ 5)',
+    configTargetProfile,
+  });
+}
+
+export async function buildTargetProfiles(databaseBuilder) {
+  await _createTargetProfile(databaseBuilder);
+  return databaseBuilder.commit();
+}
