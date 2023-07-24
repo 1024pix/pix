8b9e8bf6a625ece785c5ef9ebd5718f44d78735a
Andreia Pena <andreia.pena@pix.fr>
2023-07-24 18:19:46 +0200

    ✨ admin: create target profiles log table

--- admin/tests/acceptance/authenticated/complementary-certifications/complementary-certification/details_test.js
+++ admin/tests/acceptance/authenticated/complementary-certifications/complementary-certification/details_test.js
@@ -4,6 +4,7 @@ import { setupApplicationTest } from 'ember-qunit';
 import { visit } from '@1024pix/ember-testing-library';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { authenticateAdminMemberWithRole } from 'pix-admin/tests/helpers/test-init';
+import dayjs from 'dayjs';
 
 module('Acceptance | Complementary certifications | Complementary certification | details ', function (hooks) {
   setupApplicationTest(hooks);
@@ -14,27 +15,58 @@ module('Acceptance | Complementary certifications | Complementary certification 
   });
 
   module('when admin member has role "SUPER_ADMIN", "SUPPORT" or "METIER"', function () {
-    test('it should redirect to target profile details on link click', async function (assert) {
-      // given
-      server.create('feature-toggle', { isTargetProfileVersioningEnabled: true });
-      await authenticateAdminMemberWithRole({ isSuperAdmin: true })(server);
-      server.create('complementary-certification', {
-        id: 1,
-        key: 'KEY',
-        label: 'MARIANNE CERTIF',
-        currentTargetProfile: { name: 'ALEX TARGET', id: 3 },
-      });
-      server.create('target-profile', {
-        id: 3,
-        name: 'ALEX TARGET',
-      });
-      const screen = await visit('/complementary-certifications/1/details');
+    module('on information section', function () {
+      test('it should display current target profile link and redirect to details page on click', async function (assert) {
+        // given
+        server.create('feature-toggle', { isTargetProfileVersioningEnabled: true });
+        await authenticateAdminMemberWithRole({ isSuperAdmin: true })(server);
+        server.create('complementary-certification', {
+          id: 1,
+          key: 'KEY',
+          label: 'MARIANNE CERTIF',
+          targetProfilesLog: [{ name: 'ALEX TARGET', id: 3, attachedAt: dayjs('2023-10-10T10:50:00Z') }],
+        });
+        server.create('target-profile', {
+          id: 3,
+          name: 'ALEX TARGET',
+        });
+        const screen = await visit('/complementary-certifications/1/details');
+        const currentTargetProfileLinks = screen.getAllByRole('link', { name: 'ALEX TARGET' });
 
-      // when
-      await click(screen.getByRole('link', { name: 'ALEX TARGET' }));
+        // when
+        await click(currentTargetProfileLinks[0]);
 
-      // then
-      assert.strictEqual(currentURL(), '/target-profiles/3/details');
+        // then
+        assert.strictEqual(currentURL(), '/target-profiles/3/details');
+      });
+    });
+
+    module('on log section', function () {
+      test('it should display target profiles links and redirect to details page on click', async function (assert) {
+        // given
+        server.create('feature-toggle', { isTargetProfileVersioningEnabled: true });
+        await authenticateAdminMemberWithRole({ isSuperAdmin: true })(server);
+        server.create('complementary-certification', {
+          id: 1,
+          key: 'KEY',
+          label: 'MARIANNE CERTIF',
+          targetProfilesLog: [
+            { name: 'ALEX TARGET', id: 3, attachedAt: dayjs('2023-10-10T10:50:00Z') },
+            { name: 'JEREM TARGET', id: 2, attachedAt: dayjs('2020-10-10T10:50:00Z') },
+          ],
+        });
+        server.create('target-profile', {
+          id: 2,
+          name: 'JEREM TARGET',
+        });
+        const screen = await visit('/complementary-certifications/1/details');
+
+        // when
+        await click(screen.getByRole('link', { name: 'JEREM TARGET' }));
+
+        // then
+        assert.strictEqual(currentURL(), '/target-profiles/2/details');
+      });
     });
   });
 
