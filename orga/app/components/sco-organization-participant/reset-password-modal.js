47f1a58bd98c058ee8679c27b4bc22f5cb509eec
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2023-07-24 10:50:15 +0200

    feat(orga): improve condition wording
    Co-authored-by: Ismael Gorissen <ismael.gorissen@gmail.com>
    Co-authored-by: LEGO Technix <109212476+lego-technix@users.noreply.github.com>
    Co-authored-by: BKsensei <rebecca.kaci@gmail.com>
    Co-authored-by: Thomas Evano <thomas.evano@pix.fr>

--- orga/app/components/sco-organization-participant/reset-password-modal.js
+++ orga/app/components/sco-organization-participant/reset-password-modal.js
@@ -1,7 +1,7 @@
 import Component from '@glimmer/component';
 
 export default class ResetPasswordModal extends Component {
-  get canResetPasswords() {
+  get canResetSomePasswords() {
     return this.args.totalAffectedStudents > 0;
   }
 }
