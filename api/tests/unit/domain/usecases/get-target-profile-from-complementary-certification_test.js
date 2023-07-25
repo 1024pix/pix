4c420af08e38840acfe32ca8e628f4abe1c2d1e8
P-Jeremy <jemyplu@gmail.com>
2023-07-25 15:09:54 +0200

    :sparkles: api: use complementaryCertificationTargetProfileHistoryRepository in usecase

--- api/tests/unit/domain/usecases/get-target-profile-from-complementary-certification_test.js
+++ api/tests/unit/domain/usecases/get-target-profile-from-complementary-certification_test.js
@@ -2,23 +2,23 @@ import { expect, domainBuilder, sinon } from '../../../test-helper.js';
 import { getTargetProfileFromComplementaryCertification } from '../../../../lib/domain/usecases/get-target-profile-from-complementary-certification.js';
 
 describe('Unit | UseCase | get-target-profile-from-complementary-certification', function () {
-  let complementaryCertificationRepository;
+  let complementaryCertificationTargetProfileHistoryRepository;
 
   beforeEach(function () {
-    complementaryCertificationRepository = {
-      getTargetProfileById: sinon.stub(),
+    complementaryCertificationTargetProfileHistoryRepository = {
+      getById: sinon.stub(),
     };
   });
 
   it('should get the complementary certification', async function () {
     // given
     const complementaryCertification = domainBuilder.buildComplementaryCertificationTargetProfileHistory();
-    complementaryCertificationRepository.getTargetProfileById.resolves(complementaryCertification);
+    complementaryCertificationTargetProfileHistoryRepository.getById.resolves(complementaryCertification);
 
     // when
     const result = await getTargetProfileFromComplementaryCertification({
       complementaryCertificationId: 1,
-      complementaryCertificationRepository,
+      complementaryCertificationTargetProfileHistoryRepository,
     });
 
     // then
