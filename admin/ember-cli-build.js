16ed20ea613de33838fd81e26e8573a93743903f
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2023-07-25 16:10:54 +0200

    Migrate to embroider

--- admin/ember-cli-build.js
+++ admin/ember-cli-build.js
@@ -34,5 +34,12 @@ module.exports = function (defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  return app.toTree();
+
+  const { Webpack } = require('@embroider/webpack');
+
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
+    staticModifiers: true,
+  });
 };
