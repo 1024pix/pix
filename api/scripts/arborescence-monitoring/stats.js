5f76974579fb182104c8ca0d9ca04c01ebd352c5
Mickaël Alibert <mickael.alibert@pix.fr>
2023-07-26 16:03:53 +0200

    :bug: Fix file count in path using node18 new recursive option

--- api/scripts/arborescence-monitoring/stats.js
+++ api/scripts/arborescence-monitoring/stats.js
@@ -3,12 +3,8 @@ import { readdir } from 'node:fs/promises';
 export async function countFilesInPath(path) {
   let fileNumber = 0;
   const files = await readdir(path, { recursive: true, withFileTypes: true });
-
-  for await (const file of files) {
-    if (file.isDirectory()) {
-      const directoryFilesNumber = await countFilesInPath(`${path}/${file.name}`);
-      fileNumber += directoryFilesNumber;
-    } else if (file.isFile()) {
+  for (const file of files) {
+    if (file.isFile()) {
       fileNumber++;
     }
   }
