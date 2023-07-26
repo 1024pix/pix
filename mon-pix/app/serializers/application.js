777eb13f56c2be5edb2823de2be61c4d639d02cf
François de Metz <francois.de-metz@pix.fr>
2023-07-26 16:47:20 +0200

    Debugging ember-data for fun and profit
    Co-authored-by: Alexandre Coin <alexandre.coin@pix.fr>
    Co-authored-by: Vincent Hardouin <vincent.hardouin@pix.fr>
    Co-authored-by: Marianne Bost <marianne.bost@pix.fr>

--- mon-pix/app/serializers/application.js
+++ mon-pix/app/serializers/application.js
@@ -1,3 +1,11 @@
 import JSONAPISerializer from '@ember-data/serializer/json-api';
 
-export default class ApplicationSerializer extends JSONAPISerializer {}
+export default class ApplicationSerializer extends JSONAPISerializer {
+  extractErrors(_store, typeClass, payload, _id) {
+    if (payload && typeof payload === 'object' && payload.errors) {
+      payload = payload.errors;
+      this.normalizeErrors(typeClass, payload);
+    }
+    return payload;
+  }
+}
