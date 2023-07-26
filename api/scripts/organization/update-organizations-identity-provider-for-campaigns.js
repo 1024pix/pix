2f1d80c68790ae338f6d45b971d1ffa77c5cff13
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-07-26 11:26:32 +0200

    feat(api): add script to update a list of organizations identity provider for campaigns

---
+++ api/scripts/organization/update-organizations-identity-provider-for-campaigns.js
@@ -0,0 +1,93 @@
+import { fileURLToPath } from 'node:url';
+
+import _ from 'lodash';
+import bluebird from 'bluebird';
+
+import { checkCsvHeader, parseCsvWithHeader } from '../helpers/csvHelpers.js';
+import { disconnect } from '../../db/knex-database-connection.js';
+import { updateOrganization } from '../../lib/domain/usecases/update-organization.js';
+import * as organizationForAdminRepository from '../../lib/infrastructure/repositories/organization-for-admin-repository.js';
+
+const modulePath = fileURLToPath(import.meta.url);
+const IS_LAUNCHED_FROM_CLI = process.argv[1] === modulePath;
+
+async function main() {
+  console.time('Organisations identityProviderForCampaigns updated');
+  console.log('Starting updating organisation identityProviderForCampaigns...');
+
+  const filePath = process.argv[2];
+  await _updateOrganizationsProvinceCode(filePath);
+
+  console.timeEnd('Organisations identityProviderForCampaigns updated');
+}
+
+async function _updateOrganizationsProvinceCode(filePath) {
+  const errors = [];
+  const requiredFieldNames = ['organizationId', 'identityProviderForCampaigns'];
+  const csvParsingOptions = {
+    skipEmptyLines: true,
+    header: true,
+    transform: (value, columnName) => {
+      if (typeof value === 'string') {
+        value = value.trim();
+      }
+      if (!_.isEmpty(value)) {
+        if (columnName === 'organizationId') {
+          value = Number(value);
+        }
+      } else {
+        value = null;
+      }
+
+      return value;
+    },
+  };
+
+  console.log('Checking CSV header...');
+  await checkCsvHeader({ filePath, requiredFieldNames });
+  console.log('CSV header checked');
+
+  console.log('Parsing CSV data...');
+  const organizationsDTO = await parseCsvWithHeader(filePath, csvParsingOptions);
+  console.log('CSV data parsed');
+
+  console.log('Updating data...');
+  await bluebird.mapSeries(organizationsDTO, async (organizationDTO) => {
+    const organization = {
+      id: organizationDTO.organizationId,
+      identityProviderForCampaigns: organizationDTO.identityProviderForCampaigns,
+    };
+
+    try {
+      await updateOrganization({
+        organization,
+        organizationForAdminRepository,
+      });
+    } catch (error) {
+      errors.push({ organizationDTO, error });
+    }
+  });
+
+  if (errors.length === 0) {
+    return;
+  }
+
+  console.log(`Errors occurs on ${errors.length} element!`);
+  errors.forEach((error) => {
+    console.log(JSON.stringify(error.organizationDTO));
+    console.error(error.error?.message);
+  });
+
+  throw new Error('Process done with errors');
+}
+
+if (IS_LAUNCHED_FROM_CLI) {
+  try {
+    await main();
+  } catch (error) {
+    console.error(error);
+    process.exitCode = 1;
+  } finally {
+    await disconnect();
+  }
+}
