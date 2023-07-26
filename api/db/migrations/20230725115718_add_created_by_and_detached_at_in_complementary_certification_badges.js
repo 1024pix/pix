64e29dae5645ec341b551a06f5e377c7516e475b
Andreia Pena <andreia.pena@pix.fr>
2023-07-26 11:46:07 +0200

    🗃 api: add createdBy and detachedAt columns on complementary-c-badge table

---
+++ api/db/migrations/20230725115718_add_created_by_and_detached_at_in_complementary_certification_badges.js
@@ -0,0 +1,19 @@
+const TABLE_NAME = 'complementary-certification-badges';
+const CREATED_BY_COLUMN = 'createdBy';
+const DETACHED_AT_COLUMN = 'detachedAt';
+
+const up = async function (knex) {
+  await knex.schema.table(TABLE_NAME, async (table) => {
+    table.dateTime(DETACHED_AT_COLUMN).nullable();
+    table.integer(CREATED_BY_COLUMN).references('users.id');
+  });
+};
+
+const down = async function (knex) {
+  await knex.schema.table(TABLE_NAME, async (table) => {
+    table.dropColumn(CREATED_BY_COLUMN);
+    table.dropColumn(DETACHED_AT_COLUMN);
+  });
+};
+
+export { up, down };
