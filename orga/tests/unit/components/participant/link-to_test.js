036b4190647bd26d1b18f40167b60b25e7e433a1
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2023-07-27 10:02:59 +0200

    fix(orga): align naming between breadcrumb and sidebar

--- orga/tests/unit/components/participant/link-to_test.js
+++ orga/tests/unit/components/participant/link-to_test.js
@@ -5,51 +5,96 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Component | Participant::LinkTo', function (hooks) {
   setupTest(hooks);
 
-  test('should return route to sco participant details', async function (assert) {
-    // given
-    const currentUser = this.owner.lookup('service:currentUser');
-    currentUser.organization = {
-      isSco: true,
-      isSup: false,
-    };
-    const component = await createGlimmerComponent('component:participant/link-to');
+  module('organization is managing student', function () {
+    test('should return route to sco participant details', async function (assert) {
+      // given
+      const currentUser = this.owner.lookup('service:currentUser');
+      currentUser.organization = {
+        isSco: true,
+        isSup: false,
+      };
+      currentUser.isSCOManagingStudents = true;
 
-    // when
-    const route = component.route;
+      const component = await createGlimmerComponent('component:participant/link-to');
 
-    // then
-    assert.strictEqual(route, 'authenticated.sco-organization-participants.sco-organization-participant');
+      // when
+      const route = component.route;
+
+      // then
+      assert.strictEqual(route, 'authenticated.sco-organization-participants.sco-organization-participant');
+    });
+
+    test('should return route to sup participant details', async function (assert) {
+      // given
+      const currentUser = this.owner.lookup('service:currentUser');
+      currentUser.organization = {
+        isSco: false,
+        isSup: true,
+      };
+      currentUser.isSUPManagingStudents = true;
+
+      const component = await createGlimmerComponent('component:participant/link-to');
+
+      // when
+      const route = component.route;
+
+      // then
+      assert.strictEqual(route, 'authenticated.sup-organization-participants.sup-organization-participant');
+    });
   });
 
-  test('should return route to sup participant details', async function (assert) {
-    // given
-    const currentUser = this.owner.lookup('service:currentUser');
-    currentUser.organization = {
-      isSco: false,
-      isSup: true,
-    };
-    const component = await createGlimmerComponent('component:participant/link-to');
+  module('organization is not managing student', function () {
+    test('should return route to participant details for sco', async function (assert) {
+      // given
+      const currentUser = this.owner.lookup('service:currentUser');
+      currentUser.organization = {
+        isSco: true,
+        isSup: false,
+      };
+      currentUser.isSCOManagingStudents = false;
 
-    // when
-    const route = component.route;
+      const component = await createGlimmerComponent('component:participant/link-to');
 
-    // then
-    assert.strictEqual(route, 'authenticated.sup-organization-participants.sup-organization-participant');
-  });
+      // when
+      const route = component.route;
 
-  test('should return route to participant details', async function (assert) {
-    // given
-    const currentUser = this.owner.lookup('service:currentUser');
-    currentUser.organization = {
-      isSco: false,
-      isSup: false,
-    };
-    const component = await createGlimmerComponent('component:participant/link-to');
+      // then
+      assert.strictEqual(route, 'authenticated.organization-participants.organization-participant');
+    });
 
-    // when
-    const route = component.route;
+    test('should return route to participant details for sup', async function (assert) {
+      // given
+      const currentUser = this.owner.lookup('service:currentUser');
+      currentUser.organization = {
+        isSco: false,
+        isSup: true,
+      };
+      currentUser.isSUPManagingStudents = false;
 
-    // then
-    assert.strictEqual(route, 'authenticated.organization-participants.organization-participant');
+      const component = await createGlimmerComponent('component:participant/link-to');
+
+      // when
+      const route = component.route;
+
+      // then
+      assert.strictEqual(route, 'authenticated.organization-participants.organization-participant');
+    });
+
+    test('should return route to participant details for pro', async function (assert) {
+      // given
+      const currentUser = this.owner.lookup('service:currentUser');
+      currentUser.organization = {
+        isSco: false,
+        isSup: false,
+      };
+
+      const component = await createGlimmerComponent('component:participant/link-to');
+
+      // when
+      const route = component.route;
+
+      // then
+      assert.strictEqual(route, 'authenticated.organization-participants.organization-participant');
+    });
   });
 });
