2b5821761d1afa6095242e919920132622fd3166
Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
2023-07-28 12:27:44 +0200

    fix(mon-pix): split skill-review page with a new share-badge-icons component

---
+++ mon-pix/app/components/campaigns/assessment/skill-review/share-badge-icons.js
@@ -0,0 +1,15 @@
+import Component from '@glimmer/component';
+
+export default class SkillReviewCompetenceStagesResult extends Component {
+  get acquiredAndValidBadges() {
+    const acquiredAndValidBadges = this.args.badges.filter((badge) => badge.isAcquired && badge.isValid);
+    const badgesCertifiableFirst = acquiredAndValidBadges.sort((a) => {
+      return a.isCertifiable ? -1 : 1;
+    });
+    return badgesCertifiableFirst;
+  }
+
+  get showBadgeIcons() {
+    return this.acquiredAndValidBadges.length > 0;
+  }
+}
