bcfd6eb31dcdd304b19f052343b1bfd6e6fa956a
Yannick François <yannick.francois@pix.fr>
2023-07-31 17:22:38 +0200

    tech(1d): bring activity's level to front
    co-authored-by: Yaelle YAMA ymarsiac@octo.com
    co-authored-by: lgoubin lou.goubin@pix.fr

--- 1d/app/pods/assessment/challenge/route.js
+++ 1d/app/pods/assessment/challenge/route.js
@@ -15,8 +15,10 @@ export default class ChallengeRoute extends Route {
       } else {
         challenge = await this.store.queryRecord('challenge', { assessmentId: assessment.id });
       }
-      return { assessment, challenge };
+      const activity = await this.store.queryRecord('activity', { assessmentId: assessment.id });
+      return { assessment, challenge, activity };
     } catch (err) {
+      //TODO manage error instead of says it's the end...
       return this.router.replaceWith('assessment.resume', assessment.id, {
         queryParams: { assessmentHasNoMoreQuestions: true },
       });
---
+++ api/tests/unit/domain/usecases/get-current-activity_test.js
@@ -0,0 +1,16 @@
+import { expect, sinon } from '../../../test-helper.js';
+import { getCurrentActivity } from '../../../../lib/domain/usecases/get-current-activity.js';
+
+describe('Unit | Domain | Use Cases | get-current-activity', function () {
+
+  context('should return current activity', function () {
+    it('should call the activityRepository with an assessment id equal to id_assessment ', async function () {
+      const activityRepository = { getLastActivity: sinon.stub()};
+      const assessmentId = 'id_assessment';
+
+      await getCurrentActivity({ assessmentId, activityRepository });
+
+      expect(activityRepository.getLastActivity).to.have.been.calledWith(assessmentId);
+    });
+  });
+});
