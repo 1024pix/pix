7a4e304f0c78a48a27fa40ca4642fcf46f1ab7ea
Yvonnick Frin <frin.yvonnick@gmail.com>
2023-07-31 14:37:17 +0200

    feat(db): add columns certifiableAt and isCertifiable on organization-learners table

---
+++ api/db/migrations/20230731122538_add-columns-certifiable-at-and-is-certifiable-on-organization-learners.js
@@ -0,0 +1,28 @@
+// Make sure you properly test your migration, especially DDL (Data Definition Language)
+// ! If the target table is large, and the migration take more than 20 minutes, the deployment will fail !
+
+// You can design and test your migration to avoid this by following this guide
+// https://1024pix.atlassian.net/wiki/spaces/DEV/pages/2153512965/Cr+er+une+migration
+
+// If your migrations target `answers` or `knowledge-elements`
+// contact @team-captains, because automatic migrations are not active on `pix-datawarehouse-production`
+// this may prevent data replication to succeed the day after your migration is deployed on `pix-api-production`
+const TABLE_NAME = 'organization-learners';
+const IS_CERTIFIABLE_COLUMN = 'isCertifiable';
+const CERTIFIABLE_AT_COLUMN = 'certifiableAt';
+
+const up = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.date(CERTIFIABLE_AT_COLUMN).defaultTo(null);
+    table.boolean(IS_CERTIFIABLE_COLUMN).defaultTo(null);
+  });
+};
+
+const down = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.dropColumn(CERTIFIABLE_AT_COLUMN);
+    table.dropColumn(IS_CERTIFIABLE_COLUMN);
+  });
+};
+
+export { up, down };
