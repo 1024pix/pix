de4c83bfaa105e7cb1c2a75fca86c8a3ab007eb6
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-08-01 12:18:51 +0200

    feat(api): update team acces databaseBuilder tooling
    Co-authored-by: Rébecca Kaci <rebecca.kaci@gmail.com>
    Co-authored-by: Thomas Evano <thomas.evano@pix.fr>

--- api/db/seeds/data/team-acces/data-builder.js
+++ api/db/seeds/data/team-acces/data-builder.js
@@ -1,15 +1,17 @@
 import { buildBlockedUsers } from './build-blocked-users.js';
 import { buildPixAdminRoles } from './build-pix-admin-roles.js';
+import { buildScoOrganizations } from './build-sco-organizations.js';
 import { buildTemporaryBlockedUsers } from './build-temporary-blocked-user.js';
 import { buildUsers } from './build-users.js';
+import { buildOrganizationUsers } from './build-organization-users.js';
 
 async function teamAccesDataBuilder(databaseBuilder) {
   buildPixAdminRoles(databaseBuilder);
-
   buildUsers(databaseBuilder);
-
   buildBlockedUsers(databaseBuilder);
   buildTemporaryBlockedUsers(databaseBuilder);
+  buildOrganizationUsers(databaseBuilder);
+  buildScoOrganizations(databaseBuilder);
 }
 
 export { teamAccesDataBuilder };
