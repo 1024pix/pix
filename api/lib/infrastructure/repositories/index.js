d83cf6b48fc1d89a4379b30119ed13606b5f7820
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-08-01 14:14:52 +0200

    sr(api): use repositories index for training-repository

--- api/lib/infrastructure/repositories/index.js
+++ api/lib/infrastructure/repositories/index.js
@@ -1,12 +1,14 @@
 import { injectDependencies } from '../utils/dependency-injection.js';
 
 import * as correctionRepository from './correction-repository.js';
+import * as trainingRepository from './training-repository.js';
 
 import { fromDatasourceObject } from '../adapters/solution-adapter.js';
 import * as tutorialRepository from './tutorial-repository.js';
 
 const repositoriesWithoutInjectedDependencies = {
   correctionRepository,
+  trainingRepository,
 };
 
 const dependencies = {
