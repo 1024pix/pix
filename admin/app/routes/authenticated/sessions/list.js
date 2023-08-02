fbfbcb593a8f7f01e675bdf36b9330c2f6824864
alexandrecoin <alexandre.pc.coin@gmail.com>
2023-08-02 10:58:05 +0200

    :sparkles: admin: display version in with required action sessions

--- admin/app/routes/authenticated/sessions/list.js
+++ admin/app/routes/authenticated/sessions/list.js
@@ -4,7 +4,19 @@ import { service } from '@ember/service';
 export default class AuthenticatedSessionsListRoute extends Route {
   @service store;
 
-  model() {
-    return this.store.query('with-required-action-session', {});
+  async model() {
+    const v3Sessions = await this.store.query('with-required-action-session', {
+      filter: {
+        version: 3,
+      },
+    });
+
+    const v2Sessions = await this.store.query('with-required-action-session', {
+      filter: {
+        version: 2,
+      },
+    });
+
+    return { v2Sessions, v3Sessions };
   }
 }
