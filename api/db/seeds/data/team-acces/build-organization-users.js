31557a13de3019d18ee755419b541f9b3b84fa09
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-08-02 15:16:23 +0200

    feat(api): add new admin for organization as seed

--- api/db/seeds/data/team-acces/build-organization-users.js
+++ api/db/seeds/data/team-acces/build-organization-users.js
@@ -1,16 +1,36 @@
 import { DEFAULT_PASSWORD } from '../../../constants.js';
 
 export const PIX_ORGA_ALL_ORGA_ID = 10001;
+export const PIX_ORGA_ADMIN_LEAVING_ID = 10002;
 
 export function buildOrganizationUsers(databaseBuilder) {
-  databaseBuilder.factory.buildUser.withRawPassword({
-    id: PIX_ORGA_ALL_ORGA_ID,
-    firstName: 'Rhaenyra',
-    lastName: 'Targaryen',
-    email: 'allorga@example.net',
-    rawPassword: DEFAULT_PASSWORD,
-    cgu: true,
-    pixOrgaTermsOfServiceAccepted: true,
-    lastPixOrgaTermsOfServiceValidatedAt: new Date(),
-  });
+  [
+    {
+      id: PIX_ORGA_ALL_ORGA_ID,
+      firstName: 'Rhaenyra',
+      lastName: 'Targaryen',
+      email: 'allorga@example.net',
+    },
+    {
+      id: PIX_ORGA_ADMIN_LEAVING_ID,
+      firstName: 'Admin',
+      lastName: 'Leaving',
+      email: 'admin.leaving@example.net',
+    },
+  ].forEach(_buildUser(databaseBuilder));
+}
+
+function _buildUser(databaseBuilder) {
+  return function ({ id, firstName, lastName, email, rawPassword = DEFAULT_PASSWORD }) {
+    databaseBuilder.factory.buildUser.withRawPassword({
+      id,
+      firstName,
+      lastName,
+      email,
+      rawPassword,
+      cgu: true,
+      pixOrgaTermsOfServiceAccepted: true,
+      lastPixOrgaTermsOfServiceValidatedAt: new Date(),
+    });
+  };
 }
