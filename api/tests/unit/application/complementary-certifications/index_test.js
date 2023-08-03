630c76a36fa3fea14e2ecbf8678ca558f6f83b6f
Andreia Pena <andreia.pena@pix.fr>
2023-08-03 16:24:58 +0200

    🐛 api: allow admin member certif role access to complementary certification details

--- api/tests/unit/application/complementary-certifications/index_test.js
+++ api/tests/unit/application/complementary-certifications/index_test.js
@@ -5,23 +5,6 @@ import * as moduleUnderTest from '../../../../lib/application/complementary-cert
 
 describe('Unit | Application | Router | complementary-certifications-router', function () {
   describe('GET /api/admin/complementary-certifications', function () {
-    describe('when the user authenticated has certif role', function () {
-      it('should return 403 HTTP status code', async function () {
-        // given
-        sinon.stub(securityPreHandlers, 'checkAdminMemberHasRoleCertif').callsFake((request, h) => h.response(true));
-        sinon.stub(complementaryCertificationController, 'findComplementaryCertifications').returns('ok');
-        const httpTestServer = new HttpTestServer();
-        await httpTestServer.register(moduleUnderTest);
-
-        // when
-        const response = await httpTestServer.request('GET', '/api/admin/complementary-certifications');
-
-        // then
-        expect(response.statusCode).to.equal(403);
-        sinon.assert.notCalled(complementaryCertificationController.findComplementaryCertifications);
-      });
-    });
-
     describe('when the user authenticated has no role', function () {
       it('should return 403 HTTP status code', async function () {
         // given
