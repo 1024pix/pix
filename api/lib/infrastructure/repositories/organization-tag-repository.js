32d45fc73eb03e11d43dc05d98f0e1113e0898aa
Yvonnick Frin <frin.yvonnick@gmail.com>
2023-08-09 09:16:28 +0200

    chore(api): remove unused methods from OrganizationTagRepository and TagRepository

--- api/lib/infrastructure/repositories/organization-tag-repository.js
+++ api/lib/infrastructure/repositories/organization-tag-repository.js
@@ -2,7 +2,7 @@ import { BookshelfOrganizationTag } from '../orm-models/OrganizationTag.js';
 import { Bookshelf } from '../bookshelf.js';
 import * as knexUtils from '../utils/knex-utils.js';
 import * as bookshelfToDomainConverter from '../utils/bookshelf-to-domain-converter.js';
-import { AlreadyExistingEntityError, OrganizationTagNotFound } from '../../domain/errors.js';
+import { AlreadyExistingEntityError } from '../../domain/errors.js';
 import lodash from 'lodash';
 
 const { omit } = lodash;
@@ -24,25 +24,6 @@ const create = async function (organizationTag) {
     }
     throw err;
   }
-};
-
-const remove = async function ({ organizationTagId }) {
-  try {
-    await BookshelfOrganizationTag.where({ id: organizationTagId }).destroy({ require: true });
-  } catch (err) {
-    throw new OrganizationTagNotFound('An error occurred while deleting the organization tag');
-  }
-};
-
-const findOneByOrganizationIdAndTagId = async function ({ organizationId, tagId }) {
-  const bookshelfOrganizationTags = await BookshelfOrganizationTag.query((qb) => {
-    qb.where('organizationId', organizationId);
-    qb.where('tagId', tagId);
-  }).fetchAll();
-
-  return bookshelfOrganizationTags.length > 0
-    ? bookshelfToDomainConverter.buildDomainObjects(BookshelfOrganizationTag, bookshelfOrganizationTags)[0]
-    : [];
 };
 
 const batchCreate = async function (organizationsTags, domainTransaction = DomainTransaction.emptyTransaction()) {
@@ -75,8 +56,6 @@ const getRecentlyUsedTags = async function ({ tagId, numberOfRecentTags }) {
 
 export {
   create,
-  remove,
-  findOneByOrganizationIdAndTagId,
   batchCreate,
   isExistingByOrganizationIdAndTagId,
   getRecentlyUsedTags,
