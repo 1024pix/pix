ba6979e756b09f93e16d362bd2b7d3c17bb1a664
Marie-Hélène <marie-helene.rots@pix.fr>
2023-08-09 16:07:03 +0200

    feat(orga): remove new school year campaign info banner

--- orga/app/components/banner/information.js
+++ orga/app/components/banner/information.js
@@ -23,17 +23,6 @@ export default class InformationBanner extends Component {
     return 'https://view.genial.ly/62cd67b161c1e3001759e818?idSlide=e11f61b2-3047-4be3-9a4d-dd9e7cc698ba';
   }
 
-  get displayNewYearCampaignsBanner() {
-    return this.currentUser.organization.isSco && !this._isOnCertificationsPage;
-  }
-
-  get campaignDocumentationLink() {
-    if (this.currentUser.isSCOManagingStudents) {
-      return 'https://view.genial.ly/62cd67b161c1e3001759e818?idSlide=e11f61b2-3047-4be3-9a4d-dd9e7cc698ba';
-    }
-    return 'https://view.genial.ly/5fea2c3d6157fe0d69196ed9?idSlide=16cedb0c-3c1c-4cd3-a00b-49c01b0afcc2';
-  }
-
   get displayCertificationBanner() {
     const timeToDisplay = ENV.APP.CERTIFICATION_BANNER_DISPLAY_DATES.split(' ');
     const actualMonth = this.dayjs.self().format('MM');
