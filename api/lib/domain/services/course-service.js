a73721cfe3e0cabc19c56fcc3f8fc1ca43535790
Laura Bergoens <laura.bergoens@pix.fr>
2023-08-10 16:17:08 +0200

    delete dead code

--- api/lib/domain/services/course-service.js
+++ api/lib/domain/services/course-service.js
@@ -1,13 +1,6 @@
-import _ from 'lodash';
-import { Course } from '../models/Course.js';
 import * as courseRepository from '../../infrastructure/repositories/course-repository.js';
 
 const getCourse = async function ({ courseId, dependencies = { courseRepository } }) {
-  // TODO: delete when campaign assessment does not have courses anymore
-  if (_.startsWith(courseId, '[NOT USED] Campaign')) {
-    return new Course({ id: courseId });
-  }
-
   return dependencies.courseRepository.get(courseId);
 };
 
