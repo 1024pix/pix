8cdf86e1efd74d912267a034c24035777673530f
Steph0 <Steph0@users.noreply.github.com>
2023-08-10 16:10:40 +0200

    :recycle: api: removing 'for-admin' in filenames

---
+++ api/tests/unit/infrastructure/serializers/jsonapi/attachable-target-profiles-serializer_test.js
@@ -0,0 +1,31 @@
+import { expect } from '../../../../test-helper.js';
+import * as serializer from '../../../../../lib/infrastructure/serializers/jsonapi/attachable-target-profiles-serializer.js';
+import { AttachableTargetProfile } from '../../../../../lib/domain/models/AttachableTargetProfile.js';
+
+describe('Unit | Serializer | JSONAPI | attachable-target-profiles-serializer', function () {
+  describe('#serialize', function () {
+    it('should serialize attachable target profiles to JSONAPI', function () {
+      // given
+      const attachableTargetProfile = new AttachableTargetProfile({
+        id: 1,
+        name: 'AttachableTargetProfiles',
+      });
+
+      // when
+      const serializedAttachableTargetProfiles = serializer.serialize([attachableTargetProfile]);
+
+      // then
+      expect(serializedAttachableTargetProfiles).to.deep.equal({
+        data: [
+          {
+            attributes: {
+              name: 'AttachableTargetProfiles',
+            },
+            id: '1',
+            type: 'attachable-target-profiles',
+          },
+        ],
+      });
+    });
+  });
+});
