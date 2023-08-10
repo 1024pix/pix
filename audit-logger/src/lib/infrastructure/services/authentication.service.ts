0f496eeb63567c7459b78a7e1775d5f009012822
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-08-10 16:19:54 +0200

    feat(audit): use newly created environment variable
    Co-authored-by: Clément Latzarus <clement.latzarus@pix.fr>
    Co-authored-by: Rébecca Kaci <rebecca.kaci@gmail.com>
    Co-authored-by: Thomas Evano <thomas.evano@pix.fr>

--- audit-logger/src/lib/infrastructure/services/authentication.service.ts
+++ audit-logger/src/lib/infrastructure/services/authentication.service.ts
@@ -1,9 +1,11 @@
+import { config } from '../../config.js';
+
 export const validate = (username: string, password: string): object => {
   if (username !== 'pix-api') {
     return { isValid: false, credentials: null };
   }
 
-  if (password !== 'password') {
+  if (password !== config.pixApiClientSecret) {
     return { isValid: false, credentials: null };
   }
 
