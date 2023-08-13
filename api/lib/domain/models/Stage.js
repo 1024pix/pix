f1996e64fff01ffe74f93932884a102de0f4f7b2
Guillaume Olejniczak <la.toile.cosmique@gmail.com>
2023-08-13 11:38:52 +0200

    feat(api): update Stage model with isFirstSkill property

--- api/lib/domain/models/Stage.js
+++ api/lib/domain/models/Stage.js
@@ -1,13 +1,61 @@
 class Stage {
-  constructor({ id, title, message, threshold, level, prescriberTitle, prescriberDescription, targetProfileId } = {}) {
+  /**
+   * @param {number} id
+   * @param {string} title
+   * @param {string} message
+   * @param {number|undefined} threshold
+   * @param {number|undefined} level
+   * @param {string} prescriberTitle
+   * @param {string} prescriberDescription
+   * @param {number} targetProfileId
+   * @param {boolean} isFirstSkill
+   */
+  constructor({
+    id,
+    isFirstSkill,
+    level,
+    message,
+    prescriberDescription,
+    prescriberTitle,
+    targetProfileId,
+    threshold,
+    title,
+  } = {}) {
     this.id = id;
-    this.title = title;
-    this.message = message;
-    this.threshold = threshold;
+    this.isFirstSkill = isFirstSkill;
     this.level = level;
-    this.prescriberTitle = prescriberTitle;
+    this.message = message;
     this.prescriberDescription = prescriberDescription;
+    this.prescriberTitle = prescriberTitle;
     this.targetProfileId = targetProfileId;
+    this.threshold = threshold;
+    this.title = title;
+  }
+
+  /**
+   * @returns {boolean}
+   */
+  get isZeroStage() {
+    return this.level === 0 || this.threshold === 0;
+  }
+
+  /**
+   * Stage can be defined by stage or threshold.
+   *
+   * @returns {boolean}
+   */
+  get isLevelStage() {
+    return Boolean(this.level);
+  }
+
+  /**
+   * Used to convert level into threshold.
+   *
+   * @param {number} value
+   */
+  setThreshold(value) {
+    this.level = undefined;
+    this.threshold = value;
   }
 }
 
