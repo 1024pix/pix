f916553a54a590fb036fa89b28b5f5f5e098b87a
Guillaume Olejniczak <la.toile.cosmique@gmail.com>
2023-08-13 11:39:52 +0200

    test(api): create buildStageAcquisition factory

---
+++ api/tests/tooling/domain-builder/factory/build-stage-acquisition.js
@@ -0,0 +1,17 @@
+import { StageAcquisition } from '../../../../lib/domain/models/StageAcquisition.js';
+
+const buildStageAcquisition = function ({
+  id = 1,
+  userId = 3000,
+  stageId = 4000,
+  campaignParticipationId = 5000,
+} = {}) {
+  return new StageAcquisition({
+    id,
+    userId,
+    stageId,
+    campaignParticipationId,
+  });
+};
+
+export { buildStageAcquisition };
