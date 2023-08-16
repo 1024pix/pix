a8bbdd729287c9577607922760c8c985fc3cb8a0
Yvonnick Frin <frin.yvonnick@gmail.com>
2023-08-16 14:58:56 +0200

    feat(api): create job and handler to schedule jobs that compute organization learners' certificability

---
+++ api/lib/infrastructure/jobs/organization-learner/ScheduleComputeOrganizationLearnersCertificabilityJobHandler.js
@@ -0,0 +1,30 @@
+import { ScheduleComputeOrganizationLearnersCertificabilityJob } from './ScheduleComputeOrganizationLearnersCertificabilityJob.js';
+import { ComputeCertificabilityJob } from './ComputeCertificabilityJob.js';
+
+class ScheduleComputeOrganizationLearnersCertificabilityJobHandler {
+  constructor({ organizationLearnerRepository, pgBoss }) {
+    this.organizationLearnerRepository = organizationLearnerRepository;
+    this.pgBoss = pgBoss;
+  }
+
+  async handle() {
+    const organizationLearnerIds =
+      await this.organizationLearnerRepository.findByOrganizationsWhichNeedToComputeCertificability();
+
+    await this.pgBoss.insert(
+      organizationLearnerIds.map((organizationLearnerId) => ({
+        name: ComputeCertificabilityJob.name,
+        data: { organizationLearnerId },
+        retryLimit: 0,
+        retryDelay: 30,
+        on_complete: true,
+      })),
+    );
+  }
+
+  get name() {
+    return ScheduleComputeOrganizationLearnersCertificabilityJob.name;
+  }
+}
+
+export { ScheduleComputeOrganizationLearnersCertificabilityJobHandler };
