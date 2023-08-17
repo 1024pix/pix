256f7076bd4f273ac6b41e190350b08f5b367279
Marianne Bost <marianne.bost@pix.fr>
2023-08-17 15:27:58 +0200

    :recycle: admin : fix toggle value compared with current target profile

--- admin/app/components/complementary-certifications/target-profiles/information.js
+++ admin/app/components/complementary-certifications/target-profiles/information.js
@@ -1,16 +1,7 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 
 export default class Information extends Component {
-  @tracked isToggled = true;
-
   get isMultipleCurrentTargetProfiles() {
     return this.args.complementaryCertification.currentTargetProfiles?.length > 1;
-  }
-
-  @action onChange() {
-    this.isToggled = !this.isToggled;
-    this.args.switchTargetProfile();
   }
 }
