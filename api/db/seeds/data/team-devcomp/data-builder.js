f58a53da4faaf59765b2b2eb9fd50b7ae3da1e53
miranda.lin-guignard <miranda.w.lin@gmail.com>
2023-08-17 11:03:58 +0200

    tech(api): improve devcomp seed
    Co-authored-by: Vincent Hardouin <vincent.hardouin@pix.fr>

--- api/db/seeds/data/team-devcomp/data-builder.js
+++ api/db/seeds/data/team-devcomp/data-builder.js
@@ -1,10 +1,12 @@
 import { buildCampaigns } from './build-campaigns.js';
 import { buildOrganizations } from './build-organization.js';
 import { buildTargetProfiles } from './build-target-profiles.js';
+import { buildTrainings } from './build-trainings.js';
 
 async function teamDevcompDataBuilder({ databaseBuilder }) {
   await buildOrganizations(databaseBuilder);
   await buildTargetProfiles(databaseBuilder);
+  await buildTrainings(databaseBuilder);
   await databaseBuilder.commit();
   await buildCampaigns(databaseBuilder);
 }
--- api/db/seeds/seed.js
+++ api/db/seeds/seed.js
@@ -1,13 +1,13 @@
 import { DatabaseBuilder } from '../database-builder/database-builder.js';
 import { commonBuilder } from './data/common/common-builder.js';
 import { featuresBuilder } from './data/common/feature-builder.js';
+import { teamDevcompDataBuilder } from './data/team-devcomp/data-builder.js';
+import { teamAccesDataBuilder } from './data/team-acces/data-builder.js';
 import { team1dDataBuilder } from './data/team-1d/data-builder.js';
 import { teamContenuDataBuilder } from './data/team-contenu/data-builder.js';
 import { teamCertificationDataBuilder } from './data/team-certification/data-builder.js';
-import { teamDevcompDataBuilder } from './data/team-devcomp/data-builder.js';
-import { teamEvaluationDataBuilder } from './data/team-evaluation/data-builder.js';
 import { teamPrescriptionDataBuilder } from './data/team-prescription/data-builder.js';
-import { teamAccesDataBuilder } from './data/team-acces/data-builder.js';
+import { teamEvaluationDataBuilder } from './data/team-evaluation/data-builder.js';
 
 const seed = async function (knex) {
   const databaseBuilder = new DatabaseBuilder({ knex });
