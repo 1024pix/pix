0346e9273648144b7c2c554a1fcff08e1e1da73b
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2023-08-23 16:30:56 +0200

    bsr(tooling): use static id for features

--- api/db/seeds/data/common/feature-builder.js
+++ api/db/seeds/data/common/feature-builder.js
@@ -1,11 +1,17 @@
 import { ORGANIZATION_FEATURE } from '../../../../lib/domain/constants.js';
+import {
+  FEATURE_MULTIPLE_SENDING_ASSESSMENT_ID,
+  FEATURE_COMPUTE_ORGANIZATION_LEARNER_CERTIFICABILITY_ID,
+} from './constants.js';
 
 const featuresBuilder = async function ({ databaseBuilder }) {
   databaseBuilder.factory.buildFeature({
+    id: FEATURE_MULTIPLE_SENDING_ASSESSMENT_ID,
     key: ORGANIZATION_FEATURE.MULTIPLE_SENDING_ASSESSMENT.key,
     description: ORGANIZATION_FEATURE.MULTIPLE_SENDING_ASSESSMENT.description,
   });
   databaseBuilder.factory.buildFeature({
+    id: FEATURE_COMPUTE_ORGANIZATION_LEARNER_CERTIFICABILITY_ID,
     key: ORGANIZATION_FEATURE.COMPUTE_ORGANIZATION_LEARNER_CERTIFICABILITY.key,
     description: ORGANIZATION_FEATURE.COMPUTE_ORGANIZATION_LEARNER_CERTIFICABILITY.description,
   });
