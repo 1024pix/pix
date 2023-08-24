7db3c001fded559364161d7bbc9a9ef5454f161b
P-Jeremy <jemyplu@gmail.com>
2023-08-24 15:34:45 +0200

    :sparkles: api: add complementary certification badge repository

---
+++ api/src/certification/shared/domain/errors.js
@@ -0,0 +1,10 @@
+import { DomainError } from '../../../../lib/domain/errors.js';
+
+class InvalidBadgeLevelError extends DomainError {
+  constructor(message = 'Badge level inconsistency') {
+    super(message);
+    this.code = 'INVALID_BADGE_LEVEL';
+  }
+}
+
+export { InvalidBadgeLevelError };
