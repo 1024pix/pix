51f20142c1c364e278d20af190fa23d97192dcb2
Clément Latzarus <c@ltzr.io>
2023-08-24 11:46:33 +0200

    feat(api): enable retry when logging anonymize event

--- api/lib/infrastructure/jobs/audit-log/UserAnonymizedEventLoggingJob.js
+++ api/lib/infrastructure/jobs/audit-log/UserAnonymizedEventLoggingJob.js
@@ -2,6 +2,14 @@ import { JobPgBoss } from '../JobPgBoss.js';
 
 export class UserAnonymizedEventLoggingJob extends JobPgBoss {
   constructor(queryBuilder) {
-    super({ name: 'UserAnonymizedEventLoggingJob' }, queryBuilder);
+    super(
+      {
+        name: 'UserAnonymizedEventLoggingJob',
+        retryLimit: 10,
+        retryDelay: 30,
+        retryBackoff: true,
+      },
+      queryBuilder,
+    );
   }
 }
