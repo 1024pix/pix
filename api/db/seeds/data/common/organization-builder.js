ffe13dfee30ebccf6ef6bb8cfb8ab3b8c043da3e
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2023-08-29 15:57:48 +0200

    refactor(seeds) common orga

--- api/db/seeds/data/common/organization-builder.js
+++ api/db/seeds/data/common/organization-builder.js
@@ -1,6 +1,6 @@
-import { FEATURE_MULTIPLE_SENDING_ASSESSMENT_ID } from '../common/constants.js';
 import {
   ALL_ORGANIZATION_USER_ID,
+  FEATURE_MULTIPLE_SENDING_ASSESSMENT_ID,
   DEFAULT_PASSWORD,
   PRO_ORGANIZATION_ID,
   PRO_ORGANIZATION_USER_ID,
@@ -8,15 +8,25 @@ import {
   SCO_ORGANIZATION_USER_ID,
   SUP_ORGANIZATION_ID,
   SUP_ORGANIZATION_USER_ID,
+  SUP_MANAGING_ORGANIZATION_ID,
+  SCO_MANAGING_ORGANIZATION_ID,
 } from './constants.js';
 
 function _createScoOrganization(databaseBuilder) {
   databaseBuilder.factory.buildOrganization({
+    id: SCO_MANAGING_ORGANIZATION_ID,
+    type: 'SCO',
+    name: 'SCO Orga - Managing Students',
+    isManagingStudents: true,
+    externalId: 'SCO_MANAGING',
+  });
+
+  databaseBuilder.factory.buildOrganization({
     id: SCO_ORGANIZATION_ID,
     type: 'SCO',
-    name: 'SCO Orga - team prescription',
-    isManagingStudents: true,
-    externalId: 'PRESCRIPTION',
+    name: 'SCO Orga - Not Managing Students',
+    isManagingStudents: false,
+    externalId: 'SCO_NOT_MANAGING',
   });
 
   databaseBuilder.factory.buildOrganizationFeature({
@@ -26,13 +36,22 @@ function _createScoOrganization(databaseBuilder) {
 
   _createUserAdminForScoOrganization(databaseBuilder);
 }
+
 function _createSupOrganization(databaseBuilder) {
+  databaseBuilder.factory.buildOrganization({
+    id: SUP_MANAGING_ORGANIZATION_ID,
+    type: 'SUP',
+    name: 'SUP Orga - Managing Students',
+    isManagingStudents: true,
+    externalId: 'SUP_MANAGING',
+  });
+
   databaseBuilder.factory.buildOrganization({
     id: SUP_ORGANIZATION_ID,
     type: 'SUP',
-    name: 'SUP Orga - team prescription',
-    isManagingStudents: true,
-    externalId: 'PRESCRIPTION',
+    name: 'SUP Orga - Not Managing Students',
+    isManagingStudents: false,
+    externalId: 'SUP_NOT_MANAGING',
   });
 
   databaseBuilder.factory.buildOrganizationFeature({
@@ -46,9 +65,9 @@ function _createProOrganization(databaseBuilder) {
   databaseBuilder.factory.buildOrganization({
     id: PRO_ORGANIZATION_ID,
     type: 'PRO',
-    name: 'PRO Orga - team prescription',
+    name: 'PRO Orga',
     isManagingStudents: false,
-    externalId: 'PRESCRIPTION',
+    externalId: 'PRO_NOT_MANAGING',
   });
 
   databaseBuilder.factory.buildOrganizationFeature({
@@ -62,9 +81,9 @@ function _createProOrganization(databaseBuilder) {
 function _createUserAdminForScoOrganization(databaseBuilder) {
   databaseBuilder.factory.buildUser.withRawPassword({
     id: SCO_ORGANIZATION_USER_ID,
-    firstName: 'Orga Sco',
-    lastName: 'Team Prescription',
-    email: 'prescription-sco@example.net',
+    firstName: 'Scholar',
+    lastName: 'pleure',
+    email: 'sco-orga@example.net',
     cgu: true,
     lang: 'fr',
     rawPassword: DEFAULT_PASSWORD,
@@ -76,14 +95,20 @@ function _createUserAdminForScoOrganization(databaseBuilder) {
     organizationId: SCO_ORGANIZATION_ID,
     organizationRole: 'ADMIN',
   });
+
+  databaseBuilder.factory.buildMembership({
+    userId: SCO_ORGANIZATION_USER_ID,
+    organizationId: SCO_MANAGING_ORGANIZATION_ID,
+    organizationRole: 'ADMIN',
+  });
 }
 
 function _createUserAdminForSupOrganization(databaseBuilder) {
   databaseBuilder.factory.buildUser.withRawPassword({
     id: SUP_ORGANIZATION_USER_ID,
-    firstName: 'Orga Sup',
-    lastName: 'Team Prescription',
-    email: 'prescription-sup@example.net',
+    firstName: 'Supra',
+    lastName: 'conducteur',
+    email: 'sup-orga@example.net',
     cgu: true,
     lang: 'fr',
     rawPassword: DEFAULT_PASSWORD,
@@ -95,14 +120,20 @@ function _createUserAdminForSupOrganization(databaseBuilder) {
     organizationId: SUP_ORGANIZATION_ID,
     organizationRole: 'ADMIN',
   });
+
+  databaseBuilder.factory.buildMembership({
+    userId: SUP_ORGANIZATION_USER_ID,
+    organizationId: SUP_MANAGING_ORGANIZATION_ID,
+    organizationRole: 'ADMIN',
+  });
 }
 
 function _createUserAdminForProOrganization(databaseBuilder) {
   databaseBuilder.factory.buildUser.withRawPassword({
     id: PRO_ORGANIZATION_USER_ID,
-    firstName: 'Orga Pro',
-    lastName: 'Team Prescription',
-    email: 'prescription-pro@example.net',
+    firstName: 'Pro',
+    lastName: 'tips',
+    email: 'pro-orga@example.net',
     cgu: true,
     lang: 'fr',
     rawPassword: DEFAULT_PASSWORD,
@@ -119,11 +150,11 @@ function _createUserAdminForProOrganization(databaseBuilder) {
 function _createUserMemberWithAllTypesOfOrga(databaseBuilder) {
   databaseBuilder.factory.buildUser.withRawPassword({
     id: ALL_ORGANIZATION_USER_ID,
-    firstName: 'All Orga',
-    lastName: 'Team Prescription',
-    email: 'prescription-all@example.net',
+    firstName: 'Juste',
+    lastName: 'un member',
+    email: 'all-orga@example.net',
     cgu: true,
-    lang: 'fr',
+    lang: 'en',
     rawPassword: DEFAULT_PASSWORD,
     shouldChangePassword: false,
     pixOrgaTermsOfServiceAccepted: true,
@@ -131,7 +162,18 @@ function _createUserMemberWithAllTypesOfOrga(databaseBuilder) {
 
   databaseBuilder.factory.buildMembership({
     userId: ALL_ORGANIZATION_USER_ID,
+    organizationId: SCO_MANAGING_ORGANIZATION_ID,
+    organizationRole: 'MEMBER',
+  });
+  databaseBuilder.factory.buildMembership({
+    userId: ALL_ORGANIZATION_USER_ID,
     organizationId: SCO_ORGANIZATION_ID,
+    organizationRole: 'MEMBER',
+  });
+
+  databaseBuilder.factory.buildMembership({
+    userId: ALL_ORGANIZATION_USER_ID,
+    organizationId: SUP_MANAGING_ORGANIZATION_ID,
     organizationRole: 'MEMBER',
   });
   databaseBuilder.factory.buildMembership({
@@ -139,6 +181,7 @@ function _createUserMemberWithAllTypesOfOrga(databaseBuilder) {
     organizationId: SUP_ORGANIZATION_ID,
     organizationRole: 'MEMBER',
   });
+
   databaseBuilder.factory.buildMembership({
     userId: ALL_ORGANIZATION_USER_ID,
     organizationId: PRO_ORGANIZATION_ID,
@@ -146,11 +189,11 @@ function _createUserMemberWithAllTypesOfOrga(databaseBuilder) {
   });
 }
 
-export function buildOrganizations(databaseBuilder) {
+export async function organizationBuilder({ databaseBuilder }) {
   _createScoOrganization(databaseBuilder);
   _createSupOrganization(databaseBuilder);
   _createProOrganization(databaseBuilder);
   _createUserMemberWithAllTypesOfOrga(databaseBuilder);
 
-  return databaseBuilder.commit();
+  await databaseBuilder.commit();
 }
--- api/db/seeds/data/team-prescription/build-campaigns.js
+++ api/db/seeds/data/team-prescription/build-campaigns.js
@@ -1,13 +1,13 @@
 import {
-  SCO_ORGANIZATION_ID,
+  SCO_MANAGING_ORGANIZATION_ID,
   SCO_ORGANIZATION_USER_ID,
-  SUP_ORGANIZATION_ID,
+  SUP_MANAGING_ORGANIZATION_ID,
   SUP_ORGANIZATION_USER_ID,
   PRO_ORGANIZATION_ID,
   PRO_ORGANIZATION_USER_ID,
-  TARGET_PROFILE_NO_BADGES_NO_STAGES_ID,
-  TARGET_PROFILE_BADGES_STAGES_ID,
-} from './constants.js';
+} from '../common/constants.js';
+
+import { TARGET_PROFILE_NO_BADGES_NO_STAGES_ID, TARGET_PROFILE_BADGES_STAGES_ID } from './constants.js';
 import { createProfilesCollectionCampaign, createAssessmentCampaign } from '../common/tooling/campaign-tooling.js';
 import dayjs from 'dayjs';
 
@@ -15,7 +15,7 @@ async function _createScoCampaigns(databaseBuilder) {
   await createAssessmentCampaign({
     databaseBuilder,
     targetProfileId: TARGET_PROFILE_BADGES_STAGES_ID,
-    organizationId: SCO_ORGANIZATION_ID,
+    organizationId: SCO_MANAGING_ORGANIZATION_ID,
     ownerId: SCO_ORGANIZATION_USER_ID,
     name: "Campagne d'évaluation SCO",
     createdAt: dayjs().subtract(30, 'days').toDate(),
@@ -32,7 +32,7 @@ async function _createScoCampaigns(databaseBuilder) {
 
   await createProfilesCollectionCampaign({
     databaseBuilder,
-    organizationId: SCO_ORGANIZATION_ID,
+    organizationId: SCO_MANAGING_ORGANIZATION_ID,
     ownerId: SCO_ORGANIZATION_USER_ID,
     name: 'Campagne de collecte de profil SCO',
     multipleSendings: true,
@@ -46,7 +46,7 @@ async function _createSupCampaigns(databaseBuilder) {
   await createAssessmentCampaign({
     databaseBuilder,
     targetProfileId: TARGET_PROFILE_NO_BADGES_NO_STAGES_ID,
-    organizationId: SUP_ORGANIZATION_ID,
+    organizationId: SUP_MANAGING_ORGANIZATION_ID,
     ownerId: SUP_ORGANIZATION_USER_ID,
     name: "Campagne d'évaluation SUP",
     createdAt: dayjs().subtract(30, 'days').toDate(),
@@ -63,7 +63,7 @@ async function _createSupCampaigns(databaseBuilder) {
 
   await createProfilesCollectionCampaign({
     databaseBuilder,
-    organizationId: SUP_ORGANIZATION_ID,
+    organizationId: SUP_MANAGING_ORGANIZATION_ID,
     ownerId: SUP_ORGANIZATION_USER_ID,
     name: 'Campagne de collecte de profil SUP',
     multipleSendings: true,
