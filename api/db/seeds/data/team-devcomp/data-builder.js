75796ddc4a0603eb8f3da013713296b19e8bac08
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2023-08-29 16:09:42 +0200

    refactor(seeds) use common orga on devcomp seeds

--- api/db/seeds/data/team-devcomp/data-builder.js
+++ api/db/seeds/data/team-devcomp/data-builder.js
@@ -1,13 +1,10 @@
 import { buildCampaigns } from './build-campaigns.js';
-import { buildOrganizations } from './build-organization.js';
 import { buildTargetProfiles } from './build-target-profiles.js';
 import { buildTrainings } from './build-trainings.js';
 
 async function teamDevcompDataBuilder({ databaseBuilder }) {
-  await buildOrganizations(databaseBuilder);
   await buildTargetProfiles(databaseBuilder);
   await buildTrainings(databaseBuilder);
-  await databaseBuilder.commit();
   await buildCampaigns(databaseBuilder);
 }
 
