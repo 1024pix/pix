73dc7c5a39a829c22ce2d277b8990600964b5f03
Guillaume Olejniczak <la.toile.cosmique@gmail.com>
2023-08-29 15:09:48 +0200

    feat(api): update campaign overview usecase and model

--- api/lib/domain/usecases/find-user-campaign-participation-overviews.js
+++ api/lib/domain/usecases/find-user-campaign-participation-overviews.js
@@ -1,18 +1,54 @@
+import * as defaultStageRepository from '../../infrastructure/repositories/stage-repository.js';
+import * as defaultStageAcquisitionRepository from '../../infrastructure/repositories/stage-acquisition-repository.js';
+import * as defaultStageAndStageAcquisitionComparisonService from '../services/stages/stage-and-stage-acquisition-comparison-service.js';
+
 const findUserCampaignParticipationOverviews = async function ({
   userId,
   states,
   page,
   campaignParticipationOverviewRepository,
+  stageAndStageAcquisitionComparisonService = defaultStageAndStageAcquisitionComparisonService,
+  stageRepository = defaultStageRepository,
+  stageAcquisitionRepository = defaultStageAcquisitionRepository,
 }) {
   const concatenatedStates = states ? [].concat(states) : undefined;
 
-  const campaignParticipationOverviews = await campaignParticipationOverviewRepository.findByUserIdWithFilters({
-    userId,
-    states: concatenatedStates,
-    page,
-  });
+  const { campaignParticipationOverviews, pagination } =
+    await campaignParticipationOverviewRepository.findByUserIdWithFilters({
+      userId,
+      states: concatenatedStates,
+      page,
+    });
 
-  return campaignParticipationOverviews;
+  const campaignIds = campaignParticipationOverviews.map(({ campaignId }) => campaignId);
+  const campaignParticipationIds = campaignParticipationOverviews.map(({ id }) => id);
+
+  const [stages, acquiredStages] = await Promise.all([
+    stageRepository.getByCampaignIds(campaignIds),
+    stageAcquisitionRepository.getByCampaignParticipations(campaignParticipationIds),
+  ]);
+
+  const campaignParticipationOverviewsWithStages = campaignParticipationOverviews.map(
+    (campaignParticipationOverview) => {
+      const stagesForThisCampaign = stages.filter(
+        ({ targetProfileId }) => targetProfileId === campaignParticipationOverview.targetProfileId,
+      );
+      const acquiredStagesForThisCampaign = acquiredStages.filter(
+        ({ campaignParticipationId }) => campaignParticipationId === campaignParticipationOverview.id,
+      );
+      const stagesComparison = stageAndStageAcquisitionComparisonService.compare(
+        stagesForThisCampaign,
+        acquiredStagesForThisCampaign,
+      );
+
+      campaignParticipationOverview.totalStagesCount = stagesComparison.highestReachedStageNumber;
+      campaignParticipationOverview.validatedStagesCount = stagesComparison.totalNumberOfStages;
+
+      return campaignParticipationOverview;
+    },
+  );
+
+  return { campaignParticipationOverviews: campaignParticipationOverviewsWithStages, pagination };
 };
 
 export { findUserCampaignParticipationOverviews };
