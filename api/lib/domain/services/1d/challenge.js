2fe300da0e029c779fc2f38b78b5c2ba0b4d3c61
Aurelie Crouillebois <aurelie.crouillebois@gmail.com>
2023-08-30 15:34:49 +0200

    feat(api-1d): fallback on prototype when specific version of challenge does not exist

--- api/lib/domain/services/1d/challenge.js
+++ api/lib/domain/services/1d/challenge.js
@@ -4,11 +4,14 @@ async function getChallenge({ missionId, activityLevel, challengeNumber, alterna
   return await _filterChallenges(
     { missionId, activityLevel, challengeNumber, challengeRepository },
     function (challenges) {
-      if (alternativeVersion === 0) {
-        alternativeVersion = undefined;
+      const alternativeVersionChallenge = challenges.find(
+        (challenge) => challenge.alternativeVersion === alternativeVersion
+      );
+      if (alternativeVersionChallenge === undefined) {
+        return challenges[0];
       }
-      return challenges.find((challenge) => challenge.alternativeVersion === alternativeVersion);
-    },
+      return alternativeVersionChallenge;
+    }
   );
 }
 
@@ -23,19 +26,19 @@ async function getStartChallenge({
     { missionId, activityLevel, challengeNumber, challengeRepository },
     function (challenges) {
       const neverPlayedChallenges = challenges.filter(
-        (challenge) => !alreadyPlayedAlternativeVersions.includes(challenge.alternativeVersion),
+        (challenge) => !alreadyPlayedAlternativeVersions.includes(challenge.alternativeVersion)
       );
       if (neverPlayedChallenges.length === 0) {
         return challenges[_randomIndexForChallenges(challenges.length)];
       }
       return neverPlayedChallenges[_randomIndexForChallenges(neverPlayedChallenges.length)];
-    },
+    }
   );
 }
 
 const _filterChallenges = async (
   { missionId, activityLevel, challengeNumber, challengeRepository },
-  filterFunction,
+  filterFunction
 ) => {
   try {
     const challenges = await challengeRepository.getChallengeFor1d({
