8b77efe512dd4627fec96aff9384a5169d24269d
Yoan de LUCA <yoan.de-luca@pix.fr>
2023-09-06 16:49:21 +0200

    feat(admin): add role column to certification center members list
    Co-authored-by: Ismael Gorissen <ismael.gorissen@gmail.com>
    Co-authored-by: Eric Lim <eric.lim@zenika.com>

--- admin/app/models/certification-center-membership.js
+++ admin/app/models/certification-center-membership.js
@@ -1,8 +1,17 @@
 import Model, { attr, belongsTo } from '@ember-data/model';
 
+const ROLE_LABEL_KEYS = {
+  ADMIN: 'components.memberships-section.roles.admin',
+  MEMBER: 'components.memberships-section.roles.member',
+};
+
 export default class CertificationCenterMembership extends Model {
   @attr('date') createdAt;
-
+  @attr() role;
   @belongsTo('certification-center') certificationCenter;
   @belongsTo('user') user;
+
+  get roleLabelKey() {
+    return ROLE_LABEL_KEYS[this.role];
+  }
 }
--- admin/tests/integration/components/certification-centers/memberships-section_test.js
+++ admin/tests/integration/components/certification-centers/memberships-section_test.js
@@ -1,25 +1,31 @@
 import dayjs from 'dayjs';
 import { render } from '@1024pix/ember-testing-library';
 import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
 import { hbs } from 'ember-cli-htmlbars';
-import EmberObject from '@ember/object';
 import sinon from 'sinon';
+import setupIntlRenderingTest from '../../../helpers/setup-intl-rendering';
 
 module('Integration | Component | certification-centers/memberships-section', function (hooks) {
-  setupRenderingTest(hooks);
+  setupIntlRenderingTest(hooks);
+
+  let store;
+
+  hooks.beforeEach(function () {
+    store = this.owner.lookup('service:store');
+  });
 
   test('it should display certification center membership details', async function (assert) {
     // given
-    const user = EmberObject.create({
+    const user = store.createRecord('user', {
       id: 123,
       firstName: 'Jojo',
       lastName: 'La Gringue',
       email: 'jojo@example.net',
     });
-    const certificationCenterMembership = EmberObject.create({
+    const certificationCenterMembership = store.createRecord('certification-center-membership', {
       id: 1,
       user,
+      role: 'MEMBER',
       createdAt: new Date('2018-02-15T05:06:07Z'),
     });
     this.set('certificationCenterMemberships', [certificationCenterMembership]);
@@ -43,22 +49,33 @@ module('Integration | Component | certification-centers/memberships-section', fu
     assert
       .dom(screen.getByLabelText('Informations du membre Jojo La Gringue'))
       .containsText(certificationCenterMembership.id);
+    assert.dom(screen.getByLabelText('Informations du membre Jojo La Gringue')).containsText('Membre');
   });
 
   test('it should display a list of certification center memberships', async function (assert) {
     // given
-    const user1 = EmberObject.create({
+    const user1 = store.createRecord('user', {
       firstName: 'Jojo',
       lastName: 'La Gringue',
       email: 'jojo@example.net',
     });
-    const user2 = EmberObject.create({
+    const user2 = store.createRecord('user', {
       firstName: 'Froufrou',
       lastName: 'Le froussard',
       email: 'froufrou@example.net',
     });
-    const certificationCenterMembership1 = EmberObject.create({ id: 1, user: user1 });
-    const certificationCenterMembership2 = EmberObject.create({ id: 2, user: user2 });
+    const certificationCenterMembership1 = store.createRecord('certification-center-membership', {
+      id: 1,
+      user: user1,
+      role: 'ADMIN',
+      createdAt: new Date('2018-02-15T05:06:07Z'),
+    });
+    const certificationCenterMembership2 = store.createRecord('certification-center-membership', {
+      id: 2,
+      user: user2,
+      role: 'MEMBER',
+      createdAt: new Date('2018-02-15T05:06:07Z'),
+    });
     const certificationCenterMemberships = [certificationCenterMembership1, certificationCenterMembership2];
     this.set('certificationCenterMemberships', certificationCenterMemberships);
     this.set('disableCertificationCenterMembership', sinon.stub());
