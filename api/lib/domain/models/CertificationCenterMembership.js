700c0d79a4649ce7433f92e310c9bf4b2dcd3b8b
Eric Lim <eric.lim@zenika.com>
2023-09-06 11:28:30 +0200

    feat(api): add role attribute in certification-center-membership
    Co-authored-by: Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>
    Co-authored-by: Yoan de LUCA <yoan.de-luca@pix.fr>
    Co-authored-by: Ismael Gorissen <ismael.gorissen@gmail.com>

--- api/lib/domain/models/CertificationCenterMembership.js
+++ api/lib/domain/models/CertificationCenterMembership.js
@@ -1,11 +1,12 @@
 class CertificationCenterMembership {
-  constructor({ id, certificationCenter, user, createdAt, disabledAt, isReferer } = {}) {
+  constructor({ id, certificationCenter, user, createdAt, disabledAt, isReferer, role } = {}) {
     this.id = id;
     this.certificationCenter = certificationCenter;
     this.user = user;
     this.createdAt = createdAt;
     this.disabledAt = disabledAt;
     this.isReferer = isReferer;
+    this.role = role;
   }
 }
 
