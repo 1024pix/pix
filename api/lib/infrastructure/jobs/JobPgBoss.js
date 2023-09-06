401a8315fcc0cc989ae09fdab74434a1fa824102
Marie-Hélène <marie-helene.rots@pix.fr>
2023-09-06 16:37:49 +0200

    feat(api): set expireIn when inserting a job

--- api/lib/infrastructure/jobs/JobPgBoss.js
+++ api/lib/infrastructure/jobs/JobPgBoss.js
@@ -4,17 +4,19 @@ class JobPgBoss {
     this.retryLimit = config.retryLimit || 0;
     this.retryDelay = config.retryDelay || 30;
     this.retryBackoff = config.retryBackoff || false;
+    this.expireIn = config.expireIn || '00:15:00';
     this.queryBuilder = queryBuilder;
   }
 
   async schedule(data) {
     await this.queryBuilder.raw(
-      'INSERT INTO pgboss.job (name, data, retryLimit, retryDelay, retryBackoff, on_complete) VALUES (:name, :data, :retryLimit, :retryDelay, :retryBackoff, :on_complete)',
+      'INSERT INTO pgboss.job (name, data, retrylimit, retrydelay, retrybackoff, expirein, on_complete) VALUES (:name, :data, :retryLimit, :retryDelay, :retryBackoff, :expireIn, :on_complete)',
       {
         name: this.name,
         retryLimit: this.retryLimit,
         retryDelay: this.retryDelay,
         retryBackoff: this.retryBackoff,
+        expireIn: this.expireIn,
         data,
         on_complete: true,
       },
