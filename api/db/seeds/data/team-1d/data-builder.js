94c1bcd12f805e7f3c5cb2091f84d3ab3f1f3540
Yannick François <yannick.francois@pix.fr>
2023-09-11 16:00:45 +0200

    feat(1d): add orga to 1d seed file
    co-authored-by: Aurelie Crouillebois aurelie.crouillebois@gmail.com

--- api/db/seeds/data/team-1d/data-builder.js
+++ api/db/seeds/data/team-1d/data-builder.js
@@ -1,9 +1,67 @@
-async function team1dDataBuilder({ databaseBuilder }) {
-  _createActivity(databaseBuilder);
+import * as tooling from '../common/tooling/index.js';
+import { ALL_ORGANIZATION_USER_ID } from '../common/constants.js';
+
+const TEAM_1D_OFFSET_ID = 9000;
+
+const TEAM_1D_ORGANIZATION_1_ID = TEAM_1D_OFFSET_ID;
+const TEAM_1D_ORGANIZATION_2_ID = TEAM_1D_OFFSET_ID + 1;
+
+const TEAM_1D_USER_ID = TEAM_1D_OFFSET_ID;
+
+async function team1dDataBuilder(databaseBuilder) {
+  await _createSco1dUser(databaseBuilder);
+  await _createSco1dOrganizations(databaseBuilder);
+}
+
+async function _createSco1dUser(databaseBuilder) {
+  await databaseBuilder.factory.buildUser.withRawPassword({
+    id: TEAM_1D_USER_ID,
+    firstName: 'Jules',
+    lastName: 'Ferry',
+    email: '1d-orga@example.net',
+    cgu: true,
+    lang: 'fr',
+    lastTermsOfServiceValidatedAt: new Date(),
+    lastPixOrgaTermsOfServiceValidatedAt: new Date(),
+    mustValidateTermsOfService: false,
+    pixOrgaTermsOfServiceAccepted: false,
+    pixCertifTermsOfServiceAccepted: false,
+    hasSeenAssessmentInstructions: false,
+    rawPassword: 'pix123',
+    shouldChangePassword: false,
+  });
+  await databaseBuilder.commit();
+}
+
+async function _createSco1dOrganizations(databaseBuilder) {
+  await tooling.organization.createOrganization({
+    databaseBuilder,
+    organizationId: TEAM_1D_ORGANIZATION_1_ID,
+    type: 'SCO',
+    name: 'Ecole des Pyrénées - Pix1D',
+    isManagingStudents: true,
+    externalId: 'PIX1D_1',
+    configOrganization: {
+      learnerCount: 8,
+    },
+    tagIds: [],
+    adminIds: [TEAM_1D_USER_ID],
+    memberIds: [ALL_ORGANIZATION_USER_ID],
+  });
+
+  await tooling.organization.createOrganization({
+    databaseBuilder,
+    organizationId: TEAM_1D_ORGANIZATION_2_ID,
+    type: 'SCO',
+    name: 'Ecole des Alpes - Pix1D',
+    isManagingStudents: true,
+    externalId: 'PIX1D_2',
+    configOrganization: {
+      learnerCount: 8,
+    },
+    tagIds: [],
+    adminIds: [TEAM_1D_USER_ID],
+  });
 }
 
 export { team1dDataBuilder };
-
-function _createActivity(databaseBuilder) {
-  databaseBuilder.factory.buildActivity();
-}
