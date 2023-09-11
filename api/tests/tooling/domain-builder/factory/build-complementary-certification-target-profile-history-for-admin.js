58ed3fd388687e113da6b5180a721aff9fb48f10
Antoine Ceol <antoine.ceol@gmail.com>
2023-09-11 22:22:44 +0200

    :fire: api: remove unused currentTargetProfileBadges

--- api/tests/tooling/domain-builder/factory/build-complementary-certification-target-profile-history-for-admin.js
+++ api/tests/tooling/domain-builder/factory/build-complementary-certification-target-profile-history-for-admin.js
@@ -4,14 +4,12 @@ const buildComplementaryCertificationTargetProfileHistory = function ({
   id = 1,
   label = 'Complementary certification name',
   key = 'COMPLEMENTARY_CERTIFICATION_KEY',
-  currentTargetProfileBadges = [],
   targetProfilesHistory = [],
 } = {}) {
   return new ComplementaryCertificationTargetProfileHistory({
     id,
     label,
     key,
-    currentTargetProfileBadges,
     targetProfilesHistory,
   });
 };
