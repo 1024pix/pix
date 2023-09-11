a1685ea5336b93a17af5469a64a67ab275e5f27e
Antoine Ceol <antoine.ceol@gmail.com>
2023-09-11 23:00:43 +0200

    :recycle: api: Update attachable-target-profiles-repository.find

--- api/lib/infrastructure/repositories/attachable-target-profiles-repository.js
+++ api/lib/infrastructure/repositories/attachable-target-profiles-repository.js
@@ -3,56 +3,46 @@ import { knex } from '../../../db/knex-database-connection.js';
 import { AttachableTargetProfile } from '../../domain/models/AttachableTargetProfile.js';
 
 const find = async function ({ searchTerm } = {}) {
-  const targetProfilesSearchQuery = knex('target-profiles')
+  const targetProfiles = await knex('target-profiles')
     .select('target-profiles.id', 'target-profiles.name')
     .leftJoin('badges', 'target-profiles.id', 'badges.targetProfileId')
     .leftJoin('complementary-certification-badges', 'badges.id', 'complementary-certification-badges.badgeId')
     .orderBy('target-profiles.name', 'ASC')
-    .orderBy('target-profiles.id', 'DESC');
+    .orderBy('target-profiles.id', 'DESC')
+    .where('target-profiles.outdated', false)
+    .where((builder) => _includeNeverAttachedTargetProfile(builder).orWhere(_includeDetachedTargetProfile))
+    .where((builder) => _searchByCritieria({ builder, searchTerm }));
 
-  targetProfilesSearchQuery.andWhere((queryBuilder) => {
-    return _excludeAllOutdatedTargetProfiles(queryBuilder).andWhere((queryBuilder) => {
-      return _includeAllTargetProfilesNotLinkedToAComplementaryCertification(queryBuilder).orWhere(
-        _includeTargetProfileLinkedToAComplementaryOnlyWhenDetached,
-      );
-    });
-  });
-
-  if (!_.isBlank(searchTerm)) {
-    targetProfilesSearchQuery.andWhere((builder) => {
-      return _searchByTargetProfileName({ builder, searchTerm }).orWhere((builder) => {
-        _searchOnIdColumnWhenSearchTermsContainsOnlyNumbers({ builder, searchTerm });
-      });
-    });
-  }
-
-  const targetProfiles = await targetProfilesSearchQuery;
   return _toDomain(targetProfiles);
 };
 
 export { find };
 
+function _searchByCritieria({ builder, searchTerm }) {
+  if (!_.isBlank(searchTerm)) {
+    const filteredBuilder = _searchByTargetProfileName({ builder, searchTerm });
+    const isNumberOnly = /^\d+$/.test(searchTerm);
+    if (isNumberOnly) {
+      return filteredBuilder.orWhere((builder) => _searchOnId({ builder, searchTerm }));
+    }
+
+    return filteredBuilder;
+  }
+  return builder;
+}
 function _searchByTargetProfileName({ builder, searchTerm }) {
   return builder.whereILike('target-profiles.name', `%${searchTerm}%`);
 }
 
-function _searchOnIdColumnWhenSearchTermsContainsOnlyNumbers({ builder, searchTerm }) {
-  if (/^\d+$/.test(searchTerm)) {
-    return builder.whereRaw('CAST("target-profiles"."id" AS TEXT) LIKE ?', [`%${searchTerm}%`]);
-  }
-
-  return builder;
+function _searchOnId({ builder, searchTerm }) {
+  return builder.whereRaw('CAST("target-profiles"."id" AS TEXT) LIKE ?', [`%${searchTerm}%`]);
 }
 
-function _excludeAllOutdatedTargetProfiles(builder) {
-  return builder.where('target-profiles.outdated', false);
-}
-
-function _includeAllTargetProfilesNotLinkedToAComplementaryCertification(builder) {
+function _includeNeverAttachedTargetProfile(builder) {
   return builder.whereNull('complementary-certification-badges.badgeId');
 }
 
-function _includeTargetProfileLinkedToAComplementaryOnlyWhenDetached(builder) {
+function _includeDetachedTargetProfile(builder) {
   return builder
     .whereNotNull('badges.targetProfileId')
     .andWhereNot('complementary-certification-badges.detachedAt', null);
