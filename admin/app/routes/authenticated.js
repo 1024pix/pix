799c48d3c47371f4ec0b2dd9ef352cf077a34252
Andreia Pena <andreia.pena@pix.fr>
2023-09-12 18:19:55 +0200

    ♻️ admin: call /oidc-providers route only when needed

--- admin/app/routes/authenticated.js
+++ admin/app/routes/authenticated.js
@@ -3,8 +3,13 @@ import { service } from '@ember/service';
 
 export default class AuthenticatedRoute extends Route {
   @service session;
+  @service oidcIdentityProviders;
 
   beforeModel(transition) {
     this.session.requireAuthentication(transition, 'login');
+  }
+
+  async model() {
+    await this.oidcIdentityProviders.loadAllAvailableIdentityProviders();
   }
 }
