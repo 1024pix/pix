ceb6545fc7bd8c64020288a51209a62360594115
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-09-12 11:24:44 +0200

    feat(api): add "updateRole" method
    to CertificationCenterMembership model
    
    Co-authored-by: Yoan de LUCA <yoan.de-luca@pix.fr>
    Co-authored-by: Eric Lim <eric.lim@zenika.com>

---
+++ api/tests/unit/domain/models/CertificationCenterMembership_test.js
@@ -0,0 +1,36 @@
+import { expect, sinon } from '../../../test-helper.js';
+import { CertificationCenterMembership } from '../../../../lib/domain/models/CertificationCenterMembership.js';
+
+describe('Unit | Domain | Models | CertificationCenterMembership', function () {
+  const now = new Date('2023-09-12');
+  let clock;
+
+  beforeEach(function () {
+    clock = sinon.useFakeTimers(now);
+  });
+
+  afterEach(function () {
+    clock.restore();
+  });
+
+  describe('#updateRole', function () {
+    it('updates certification center membership "role" and "updatedByUserId" attributes', async function () {
+      // given
+      const role = 'ADMIN';
+      const updatedByUserId = 1;
+      const certificationCenterMembership = new CertificationCenterMembership();
+      const expectedCertificationCenterMembership = new CertificationCenterMembership({
+        ...certificationCenterMembership,
+        role: 'ADMIN',
+        updatedAt: now,
+        updatedByUserId,
+      });
+
+      // when
+      certificationCenterMembership.updateRole({ role, updatedByUserId });
+
+      // then
+      expect(certificationCenterMembership).to.deep.equal(expectedCertificationCenterMembership);
+    });
+  });
+});
