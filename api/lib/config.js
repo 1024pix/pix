49a7531825055da8b7ae1ad4b8ab75a4ebb34902
Pierre TOP - TOPI <56302715+octo-topi@users.noreply.github.com>
2023-09-13 17:54:17 +0200

    Update config.js

--- api/lib/config.js
+++ api/lib/config.js
@@ -5,6 +5,7 @@ import path from 'path';
 import moment from 'moment';
 import ms from 'ms';
 
+
 import { getArrayOfStrings } from '../lib/infrastructure/utils/string-utils.js';
 
 import * as url from 'url';
