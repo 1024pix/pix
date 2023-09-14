e4ffdc88b7517ec417f1945fb2029dc84ed2f9aa
Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>
2023-09-14 14:52:50 +0200

    feat(audit): add healthcheck route
    Co-authored-by: Ismael Gorissen <ismael.gorissen@gmail.com>
     Co-authored-by: LEGO Technix <109212476+lego-technix@users.noreply.github.com>
     Co-authored-by: Eric Lim <eric.lim@zenika.com>
    
     Co-authored-by: Yoan de LUCA <yoan.de-luca@pix.fr>

--- audit-logger/src/lib/routes.ts
+++ audit-logger/src/lib/routes.ts
@@ -1,5 +1,6 @@
 import { CREATE_AUDIT_LOG_ROUTE } from './controllers/create-audit-log.controller.js';
-
+import { HEALTHCHECK_ROUTE } from './controllers/healthcheck.controller.js';
 export const ROUTES = [
   CREATE_AUDIT_LOG_ROUTE,
+  HEALTHCHECK_ROUTE,
 ];
