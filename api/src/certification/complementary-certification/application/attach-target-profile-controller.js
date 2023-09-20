3d2b90f52040cdb02309bba3a539d228bf78afba
Antoine Ceol <antoine.ceol@gmail.com>
2023-09-20 14:58:19 +0200

    :sparkles: api: Send email after target profil detach

--- api/src/certification/complementary-certification/application/attach-target-profile-controller.js
+++ api/src/certification/complementary-certification/application/attach-target-profile-controller.js
@@ -4,12 +4,13 @@ import { usecases } from '../../shared/domain/usecases/index.js';
 const attachTargetProfile = async function (request, h, dependencies = { complementaryCertificationBadgeSerializer }) {
   const { userId } = request.auth.credentials;
   const { complementaryCertificationId } = request.params;
-  const { targetProfileId, complementaryCertificationBadges } =
+  const { targetProfileId, notifyOrganizations, complementaryCertificationBadges } =
     await dependencies.complementaryCertificationBadgeSerializer.deserialize(request.payload);
   await usecases.attachBadges({
     userId,
     complementaryCertificationId,
     targetProfileIdToDetach: targetProfileId,
+    notifyOrganizations,
     complementaryCertificationBadgesToAttachDTO: complementaryCertificationBadges,
   });
 
