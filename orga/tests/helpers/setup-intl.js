7cc72022628f85e2e7116096e2f314b776975f20
François de Metz <francois.de-metz@pix.fr>
2023-09-20 16:17:32 +0200

    Use setupIntl from ember-intl

--- orga/tests/helpers/setup-intl.js
+++ orga/tests/helpers/setup-intl.js
@@ -1,8 +1,11 @@
-export default function setupIntl(hooks, locale = ['fr']) {
+import { setupIntl as setupIntlFromEmberIntl } from 'ember-intl/test-support';
+
+export default function setupIntl(hooks, locale = 'fr') {
+  setupIntlFromEmberIntl(hooks, locale);
+
   hooks.beforeEach(function () {
-    this.intl = this.owner.lookup('service:intl');
     this.localeService = this.owner.lookup('service:locale');
-    this.localeService.setLocale(locale[0]);
+    this.localeService.setLocale(locale);
     this.dayjs = this.owner.lookup('service:dayjs');
   });
 }
