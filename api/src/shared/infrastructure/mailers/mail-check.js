02a357af32d8738f245bca140bc68a5d7e83a614
Antoine Ceol <antoine.ceol@gmail.com>
2023-09-25 09:16:03 +0200

    :recycle: api: Move mail-check to src

---
+++ api/src/shared/infrastructure/mailers/mail-check.js
@@ -0,0 +1,22 @@
+import { promises } from 'node:dns';
+
+const { Resolver } = promises;
+
+const resolver = new Resolver();
+
+let resolveMx = resolver.resolveMx.bind(resolver);
+
+const checkDomainIsValid = function (emailAddress) {
+  const domain = emailAddress.replace(/.*@/g, '');
+  return resolveMx(domain).then(() => true);
+};
+
+const setResolveMx = function (resolveMxFn) {
+  resolveMx = resolveMxFn;
+};
+
+const clearResolveMx = function () {
+  resolveMx = resolver.resolveMx.bind(resolver);
+};
+
+export { checkDomainIsValid, setResolveMx, clearResolveMx };
