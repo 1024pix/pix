b8505eaaafdcad264b0c58d6970f417878e48441
Steph0 <steph0@users.noreply.github.com>
2023-09-25 17:29:31 +0200

    :sparkles: mon-pix: add new outdated complementaries banner Co-authored-by: Antoine Ceol <antoine.ceol@gmail.com> Co-authored-by: Marianne Bost <marianne.bost@pix.fr>

--- mon-pix/app/components/congratulations-certification-banner.js
+++ mon-pix/app/components/congratulations-certification-banner.js
@@ -1,7 +1,11 @@
 import Component from '@glimmer/component';
 
 export default class CongratulationsCertificationBanner extends Component {
-  get isEligible() {
-    return this.args.certificationEligibility.complementaryCertifications?.length > 0;
+  get numberOfEligibleComplementaryCertifications() {
+    return this.args.certificationEligibility.complementaryCertifications?.filter(c => !c.isOutdated).length ?? 0;
+  }
+
+  get numberOfOutdatedComplementaryCertifications() {
+    return this.args.certificationEligibility.complementaryCertifications?.filter(c => c.isOutdated).length ?? 0;
   }
 }
--- mon-pix/tests/integration/components/congratulations-certification-banner_test.js
+++ mon-pix/tests/integration/components/congratulations-certification-banner_test.js
@@ -50,7 +50,13 @@ module('Integration | Component | Congratulations Certification Banner', functio
         // given
         const store = this.owner.lookup('service:store');
         const certificationEligibility = store.createRecord('is-certifiable', {
-          eligibleComplementaryCertifications: [{ label: 'CléA Numérique', imageUrl: 'http://www.image-clea.com' }],
+          complementaryCertifications: [
+            {
+              label: 'CléA Numérique',
+              imageUrl: 'http://www.image-clea.com',
+              isOutdated: false,
+            },
+          ],
         });
         this.set('certificationEligibility', certificationEligibility);
         this.set('fullName', 'Fifi Brindacier');
@@ -73,9 +79,9 @@ module('Integration | Component | Congratulations Certification Banner', functio
         // given
         const store = this.owner.lookup('service:store');
         const certificationEligibility = store.createRecord('is-certifiable', {
-          eligibleComplementaryCertifications: [
-            { label: 'CléA Numérique', imageUrl: 'http://www.image-clea.com' },
-            { label: 'Pix+ Édu 1er degré Confirmé', imageUrl: 'http://www.image-clea.com' },
+          complementaryCertifications: [
+            { label: 'CléA Numérique', imageUrl: 'http://www.image-clea.com', isOutdated: false },
+            { label: 'Pix+ Édu 1er degré Confirmé', imageUrl: 'http://www.image-clea.com', isOutdated: false },
           ],
         });
         this.set('certificationEligibility', certificationEligibility);
@@ -93,6 +99,55 @@ module('Integration | Component | Congratulations Certification Banner', functio
         assert.ok(screen.getByText('Pix+ Édu 1er degré Confirmé'));
         assert.ok(screen.getByRole('img', { name: 'CléA Numérique' }));
         assert.ok(screen.getByRole('img', { name: 'Pix+ Édu 1er degré Confirmé' }));
+      });
+    });
+
+    module('When there are outdated complementary certifications', function () {
+      test(`renders the outdated complementary certification`, async function (assert) {
+        // given
+        const store = this.owner.lookup('service:store');
+        const certificationEligibility = store.createRecord('is-certifiable', {
+          complementaryCertifications: [
+            { label: 'CléA Numérique', imageUrl: 'http://www.image-clea.com', isOutdated: true },
+            { label: 'Pix+ Édu 1er degré Confirmé', imageUrl: 'http://www.image-clea.com', isOutdated: true },
+          ],
+        });
+        this.set('certificationEligibility', certificationEligibility);
+        this.set('closeBanner', () => {});
+
+        // when
+        const screen = await render(
+          hbs`<CongratulationsCertificationBanner @certificationEligibility={{this.certificationEligibility}} @fullName={{this.fullName}} @closeBanner={{this.closeBanner}}/>`,
+        );
+
+        // then
+        assert.dom(screen.getByRole('img', { name: 'CléA Numérique' })).exists();
+        assert
+          .dom(screen.getByText("Vous n'êtes plus éligible à la certification CléA Numérique suite à son évolution."))
+          .exists();
+        assert
+          .dom(
+            screen.getAllByText(
+              "Recontactez votre établissement ou l’organisation vous ayant proposé le parcours afin de repasser une campagne et ainsi redevenir éligible. Votre progression a été conservée et vous n'aurez qu'à jouer les nouvelles épreuves, cela devrait être rapide.",
+            )[0],
+          )
+          .exists();
+
+        assert.ok(screen.getByRole('img', { name: 'Pix+ Édu 1er degré Confirmé' }));
+        assert
+          .dom(
+            screen.getByText(
+              "Vous n'êtes plus éligible à la certification Pix+ Édu 1er degré Confirmé suite à son évolution.",
+            ),
+          )
+          .exists();
+        assert
+          .dom(
+            screen.getAllByText(
+              "Recontactez votre établissement ou l’organisation vous ayant proposé le parcours afin de repasser une campagne et ainsi redevenir éligible. Votre progression a été conservée et vous n'aurez qu'à jouer les nouvelles épreuves, cela devrait être rapide.",
+            )[1],
+          )
+          .exists();
       });
     });
   });
