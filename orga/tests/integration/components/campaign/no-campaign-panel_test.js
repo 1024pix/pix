31994b6ba9a003093ba4747364471521afee0853
Marie-Hélène <marie-helene.rots@pix.fr>
2023-09-26 17:00:18 +0200

    refactor(orga): use setupIntlRenderingTest from helpers no-campaign-panel test

--- orga/tests/integration/components/campaign/no-campaign-panel_test.js
+++ orga/tests/integration/components/campaign/no-campaign-panel_test.js
@@ -1,17 +1,15 @@
 import { module, test } from 'qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
-import { setupRenderingTest } from 'ember-qunit';
-import { setupIntl, t } from 'ember-intl/test-support';
+import setupIntlRenderingTest from '../../../helpers/setup-intl-rendering';
 
 module('Integration | Component | Campaign::NoCampaignPanel', function (hooks) {
-  setupRenderingTest(hooks);
-  setupIntl(hooks);
+  setupIntlRenderingTest(hooks);
 
   test('it displays the empty message', async function (assert) {
     await render(hbs`<Campaign::NoCampaignPanel />`);
 
-    assert.contains(t('pages.campaigns-list.no-campaign'));
+    assert.contains(this.intl.t('pages.campaigns-list.no-campaign'));
   });
 
   test('it displays the empty image', async function (assert) {
