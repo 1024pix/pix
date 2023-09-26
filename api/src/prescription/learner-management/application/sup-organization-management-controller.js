36759b139b8ba7ccf4a9c83cdd4151ad073dcc7e
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2023-09-26 11:46:43 +0200

    feat(api): add userId from token to controller

--- api/src/prescription/learner-management/application/sup-organization-management-controller.js
+++ api/src/prescription/learner-management/application/sup-organization-management-controller.js
@@ -1,5 +1,5 @@
 import { usecases } from '../../shared/usecases/index.js';
-
+import { extractUserIdFromRequest } from '../../../../lib/infrastructure/utils/request-response-utils.js';
 import * as supOrganizationLearnerWarningSerializer from '../infrastructure/serializers/jsonapi/sup-organization-learner-warnings-serializer.js';
 import { SupOrganizationLearnerParser } from '../infrastructure/serializers/csv/sup-organization-learner-parser.js';
 
@@ -13,11 +13,13 @@ const importSupOrganizationLearners = async function (request, h) {
 };
 
 const replaceSupOrganizationLearners = async function (request, h) {
+  const userId = extractUserIdFromRequest(request);
   const organizationId = request.params.id;
   const buffer = request.payload;
   const supOrganizationLearnerParser = new SupOrganizationLearnerParser(buffer, organizationId, request.i18n);
   const warnings = await usecases.replaceSupOrganizationLearners({
     organizationId,
+    userId,
     supOrganizationLearnerParser,
   });
 
