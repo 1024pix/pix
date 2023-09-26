84ff871ca786a1ce07ed1096db5c18c40d883a07
Marie-Hélène <marie-helene.rots@pix.fr>
2023-09-26 16:58:30 +0200

    refactor(orga): use setupIntlRenderingTest from helpers in campaign emptyState test

--- orga/tests/integration/components/campaign/empty-state_test.js
+++ orga/tests/integration/components/campaign/empty-state_test.js
@@ -1,16 +1,14 @@
 import { module, test } from 'qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
-import { setupRenderingTest } from 'ember-qunit';
-import { setupIntl, t } from 'ember-intl/test-support';
+import setupIntlRenderingTest from '../../../helpers/setup-intl-rendering';
 
 module('Integration | Component | Campaign::EmptyState', function (hooks) {
-  setupRenderingTest(hooks);
-  setupIntl(hooks);
+  setupIntlRenderingTest(hooks);
 
   test('it displays the empty message', async function (assert) {
     await render(hbs`<Campaign::EmptyState @campaignCode='ABDC123' />`);
 
-    assert.contains(t('pages.campaign.empty-state'));
+    assert.contains(this.intl.t('pages.campaign.empty-state'));
   });
 });
