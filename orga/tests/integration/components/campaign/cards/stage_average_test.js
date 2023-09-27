407d48b13b943b5fe6699a1d858c25a585bdf126
Marie-Hélène <marie-helene.rots@pix.fr>
2023-09-27 10:28:26 +0200

    bsr(orga): use getByText instead of contains in campaign cards stageAverage test

--- orga/tests/integration/components/campaign/cards/stage_average_test.js
+++ orga/tests/integration/components/campaign/cards/stage_average_test.js
@@ -20,6 +20,7 @@ module('Integration | Component | Campaign::Cards::StageAverage', function (hook
     );
 
     assert.contains(this.intl.t('cards.participants-average-stages.title'));
+    assert.dom(screen.getByText(this.intl.t('cards.participants-average-stages.title'))).exists();
     assert.dom(screen.getByText(this.intl.t('common.result.stages', { count: 1, total: 2 }))).exists();
   });
 });
