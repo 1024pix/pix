b2f9ffe0f3a43e536e55f0c229a6df62ca7a1ae5
Marie-Hélène <marie-helene.rots@pix.fr>
2023-09-27 16:04:33 +0200

    bsr(orga): use getByText instead of contains in copy-pasts button test

--- orga/tests/integration/components/campaign/copy-paste-button_test.js
+++ orga/tests/integration/components/campaign/copy-paste-button_test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { render } from '@ember/test-helpers';
+import { render } from '@1024pix/ember-testing-library';
 import { hbs } from 'ember-cli-htmlbars';
 import { setupRenderingTest } from 'ember-qunit';
 
@@ -12,12 +12,12 @@ module('Integration | Component | Campaign::CopyPasteButton', function (hooks) {
     this.successMessage = successMessage;
     this.defaultMessage = defaultMessage;
 
-    await render(hbs`<Campaign::CopyPasteButton
+    const screen = await render(hbs`<Campaign::CopyPasteButton
   @clipboardText='textToCopy'
   @successMessage={{this.successMessage}}
   @defaultMessage={{this.defaultMessage}}
 />`);
 
-    assert.contains(defaultMessage);
+    assert.dom(screen.getByText(defaultMessage)).exists();
   });
 });
