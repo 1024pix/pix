49259d9888bb31119650225e95621fe154bddaba
Marianne Bost <marianne.bost@pix.fr>
2023-09-28 11:45:28 +0200

    :recycle: mon-pix: split congratulations-certification-banner component Co-authored-by: Steph0 <Steph0@users.noreply.github.com>

--- mon-pix/app/components/certification-banners/index.js
+++ mon-pix/app/components/certification-banners/index.js
@@ -1,11 +1,24 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class Index extends Component {
+  @tracked displayCongratulationsBanner = true;
+
+  get showCongratulationsBanner() {
+    return this.displayCongratulationsBanner;
+  }
+
   get eligibleComplementaryCertifications() {
     return this.args.certificationEligibility.complementaryCertifications?.filter((c) => !c.isOutdated) ?? [];
   }
 
   get outdatedComplementaryCertifications() {
     return this.args.certificationEligibility.complementaryCertifications?.filter((c) => c.isOutdated) ?? [];
+  }
+
+  @action
+  closeBanner() {
+    this.displayCongratulationsBanner = false;
   }
 }
--- mon-pix/app/controllers/authenticated/certifications/join.js
+++ mon-pix/app/controllers/authenticated/certifications/join.js
@@ -1,22 +1,10 @@
 import Controller from '@ember/controller';
 import { service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class JoinCertificationController extends Controller {
   @service currentUser;
   @service router;
-
-  @tracked displayCongratulationsBanner = true;
-
-  get showCongratulationsBanner() {
-    return this.displayCongratulationsBanner;
-  }
-
-  @action
-  closeBanner() {
-    this.displayCongratulationsBanner = false;
-  }
 
   @action
   changeStep(certificationCandidateId) {
