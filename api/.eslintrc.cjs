c39fedf61e90a2a4107afac65d18ff646501a9c3
Pierre TOP (TOPI) <ptop@octo.com>
2023-09-28 12:27:34 +0200

    Add exception for constants

--- api/.eslintrc.cjs
+++ api/.eslintrc.cjs
@@ -52,6 +52,6 @@ module.exports = {
         ],
       },
     ],
-    'import/no-restricted-paths': ['error', { zones: [{ target: 'lib/domain', from: 'lib/infrastructure', except: ["logger.js"] }] }],
+    'import/no-restricted-paths': ['error', { zones: [{ target: 'lib/domain', from: 'lib/infrastructure', except: ["logger.js","constants.js"] }] }],
   },
 };
