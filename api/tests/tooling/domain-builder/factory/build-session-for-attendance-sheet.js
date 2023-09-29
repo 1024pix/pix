013ad89d4e2474c73ba68c67724115e6a2d65ea8
Andreia Pena <andreia.pena@pix.fr>
2023-09-29 15:17:13 +0200

    ✨ api: add builders

---
+++ api/tests/tooling/domain-builder/factory/build-session-for-attendance-sheet.js
@@ -0,0 +1,30 @@
+import { SessionForAttendanceSheet } from '../../../../lib/domain/read-models/SessionForAttendanceSheet.js';
+import { buildCertificationCandidateForAttendanceSheet } from './build-certification-candidate-for-attendance-sheet.js';
+
+const buildSessionForAttendanceSheet = function ({
+  id = 123,
+  date = '2023-01-01',
+  time = '10:30',
+  room = 'Room 42',
+  examiner = 'Examinator',
+  address = '22 rue des exams',
+  certificationCenterName = 'Centre pour les pros',
+  certificationCenterType = 'PRO',
+  certificationCandidates = [buildCertificationCandidateForAttendanceSheet()],
+  isOrganizationManagingStudents = false,
+} = {}) {
+  return new SessionForAttendanceSheet({
+    id,
+    examiner,
+    address,
+    date,
+    room,
+    time,
+    certificationCenterName,
+    certificationCenterType,
+    certificationCandidates,
+    isOrganizationManagingStudents,
+  });
+};
+
+export { buildSessionForAttendanceSheet };
