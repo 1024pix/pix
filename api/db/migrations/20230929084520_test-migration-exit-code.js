1497dfd733c0b04e30cd859c62434927e73df6b8
Mickaël Alibert <mickael.alibert@pix.fr>
2023-09-29 10:46:10 +0200

    :alembic: New migration

---
+++ api/db/migrations/20230929084520_test-migration-exit-code.js
@@ -0,0 +1,16 @@
+const TABLE_NAME = 'users';
+const COLUMN_NAME = 'hasBeenAnonymised';
+
+const up = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.dropColumn(COLUMN_NAME);
+  });
+};
+
+const down = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.dateTime(COLUMN_NAME).defaultTo(null);
+  });
+};
+
+export { up, down };
