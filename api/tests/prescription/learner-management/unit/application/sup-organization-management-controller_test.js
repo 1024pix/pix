c99caefc088d7d8961203257c8fda3c9150a863b
Marie-Hélène <marie-helene.rots@pix.fr>
2023-10-02 16:51:50 +0200

    feat(api): add unit test on replace student controller

---
+++ api/tests/prescription/learner-management/unit/application/sup-organization-management-controller_test.js
@@ -0,0 +1,111 @@
+import { usecases } from '../../../../../src/prescription/shared/usecases/index.js';
+import { supOrganizationManagementController } from '../../../../../src/prescription/learner-management/application/sup-organization-management-controller.js';
+import { expect, hFake, sinon } from '../../../../test-helper.js';
+
+describe('Unit | Controller | sup-organization-management-controller', function () {
+  context('#importSupOrganizationLearners', function () {
+    it('should call importSupOrganizationLearners usecase and return 200', async function () {
+      const organizationId = Symbol('organizationId');
+      const supOrganizationLearnerParser = Symbol('supOrgnaizationLearnerParser');
+      const payload = Symbol('payload');
+      const params = { id: organizationId };
+      const i18n = Symbol('i18n');
+      const warnings = Symbol('warnings');
+      const serializedResponse = Symbol('serializedResponse');
+      const request = {
+        payload,
+        params,
+        i18n,
+      };
+
+      const supOrganizationLearnerWarningSerializerStub = { serialize: sinon.stub() };
+      supOrganizationLearnerWarningSerializerStub.serialize
+        .withArgs({ id: organizationId, warnings })
+        .returns(serializedResponse);
+
+      sinon.stub(usecases, 'importSupOrganizationLearners');
+      usecases.importSupOrganizationLearners
+        .withArgs({
+          supOrganizationLearnerParser,
+        })
+        .resolves(warnings);
+
+      const makeOrganizationLearnerParserStub = sinon.stub();
+      makeOrganizationLearnerParserStub
+        .withArgs(payload, organizationId, i18n)
+        .returns(supOrganizationLearnerParser);
+
+      // when
+      const dependencies = {
+        makeOrganizationLearnerParser: makeOrganizationLearnerParserStub,
+        supOrganizationLearnerWarningSerializer: supOrganizationLearnerWarningSerializerStub,
+      };
+
+      const response = await supOrganizationManagementController.importSupOrganizationLearners(
+        request,
+        hFake,
+        dependencies,
+      );
+
+      // then
+      expect(response.statusCode).to.be.equal(200);
+      expect(response.source).to.be.equal(serializedResponse);
+    });
+  });
+  context('#replaceSupOrganizationLearner', function () {
+    it('should call replaceSupOrganizationLearner usecase and return 200', async function () {
+      const organizationId = Symbol('organizationId');
+      const userId = Symbol('userId');
+      const supOrganizationLearnerParser = Symbol('supOrgnaizationLearnerParser');
+      const payload = Symbol('payload');
+      const params = { id: organizationId };
+      const i18n = Symbol('i18n');
+      const warnings = Symbol('warnings');
+      const serializedResponse = Symbol('serializedResponse');
+      const request = {
+        payload,
+        params,
+        i18n,
+      };
+
+      const requestResponseUtilsStub = { extractUserIdFromRequest: sinon.stub() };
+      requestResponseUtilsStub.extractUserIdFromRequest.withArgs(request).returns(userId);
+
+      const supOrganizationLearnerWarningSerializerStub = { serialize: sinon.stub() };
+      supOrganizationLearnerWarningSerializerStub.serialize
+        .withArgs({ id: organizationId, warnings })
+        .returns(serializedResponse);
+
+      sinon.stub(usecases, 'replaceSupOrganizationLearners');
+      usecases.replaceSupOrganizationLearners
+        .withArgs({
+          organizationId,
+          userId,
+          supOrganizationLearnerParser,
+        })
+        .resolves(warnings);
+
+      const makeOrganizationLearnerParserStub = sinon.stub();
+      makeOrganizationLearnerParserStub
+        .withArgs(payload, organizationId, i18n)
+        .returns(supOrganizationLearnerParser);
+
+      // when
+      const dependencies = {
+        requestResponseUtils: requestResponseUtilsStub,
+        makeOrganizationLearnerParser: makeOrganizationLearnerParserStub,
+        supOrganizationLearnerWarningSerializer: supOrganizationLearnerWarningSerializerStub,
+      };
+
+      const response = await supOrganizationManagementController.replaceSupOrganizationLearners(
+        request,
+        hFake,
+        dependencies,
+      );
+
+      // then
+      expect(response.statusCode).to.be.equal(200);
+      expect(response.source).to.be.equal(serializedResponse);
+    });
+  });
+});
