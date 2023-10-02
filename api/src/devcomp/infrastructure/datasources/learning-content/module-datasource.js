073a8af0c02184d754f3ceea46f515795c238829
Dimitri Lahaye <dimitri.lahaye@pix.fr>
2023-10-02 11:25:23 +0200

    feat(api): create moduleDatasource

---
+++ api/src/devcomp/infrastructure/datasources/learning-content/module-datasource.js
@@ -0,0 +1,18 @@
+import { LearningContentResourceNotFound } from '../../../../../lib/infrastructure/datasources/learning-content/LearningContentResourceNotFound.js';
+import { Module } from '../../../domain/models/Module.js';
+
+const moduleDatasource = {
+  getBySlug: async (slug) => {
+    const availableModules = await Promise.resolve({
+      'les-adresses-mail': new Module({ id: 'f7b3a2e1-0d5c-4c6c-9c4d-1a3d8f7e9f5d', title: 'Les adresses mail' }),
+    });
+
+    if (availableModules[slug] === undefined) {
+      throw new LearningContentResourceNotFound();
+    }
+
+    return availableModules[slug];
+  },
+};
+
+export default moduleDatasource;
---
+++ api/tests/devcomp/unit/infrastructure/datasources/learning-content/module-datasource_test.js
@@ -0,0 +1,31 @@
+import moduleDatasource from '../../../../../../src/devcomp/infrastructure/datasources/learning-content/module-datasource.js';
+import { expect } from '../../../../../test-helper.js';
+import { Module } from '../../../../../../src/devcomp/domain/models/Module.js';
+import { LearningContentResourceNotFound } from '../../../../../../lib/infrastructure/datasources/learning-content/LearningContentResourceNotFound.js';
+
+describe('Unit | Infrastructure | Datasource | Learning Content | ModuleDatasource', function () {
+  describe('#getBySlug', function () {
+    describe('when exists', function () {
+      it('should return a Module instance', async function () {
+        // given
+        const slug = 'les-adresses-mail';
+
+        // when
+        const module = await moduleDatasource.getBySlug(slug);
+
+        // then
+        expect(module).to.be.instanceOf(Module);
+      });
+    });
+
+    describe("when doesn't exist", function () {
+      it('should throw an LearningContentResourceNotFound', async function () {
+        // given
+        const slug = 'dresser-un-pokemon';
+
+        // when & then
+        await expect(moduleDatasource.getBySlug(slug)).to.have.been.rejectedWith(LearningContentResourceNotFound);
+      });
+    });
+  });
+});
