120a5b41cdb11ba4a2b4a212dc16a30894044ae3
Marie-Hélène <marie-helene.rots@pix.fr>
2023-10-05 16:14:36 +0200

    tech(orga): use render from @1024pix in other components integration tests

--- orga/tests/integration/components/auth/join-request-form_test.js
+++ orga/tests/integration/components/auth/join-request-form_test.js
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, triggerEvent } from '@ember/test-helpers';
-import { fillByLabel } from '@1024pix/ember-testing-library';
+import { triggerEvent } from '@ember/test-helpers';
+import { render, fillByLabel } from '@1024pix/ember-testing-library';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | Auth::JoinRequestForm', function (hooks) {
