b1024fdf15eac62ce53e350412dd4ea128684f11
Alexandre Monney <alexandre.monney@pix.fr>
2023-10-09 15:11:24 +0200

    feat(api): add seed with sup organization learners disabled

--- api/db/seeds/data/team-prescription/data-builder.js
+++ api/db/seeds/data/team-prescription/data-builder.js
@@ -1,9 +1,11 @@
 import { buildCampaigns } from './build-campaigns.js';
 import { buildTargetProfiles } from './build-target-profiles.js';
+import { buildOrganizationLearners } from './build-learners.js';
 
 async function teamPrescriptionDataBuilder({ databaseBuilder }) {
   await buildTargetProfiles(databaseBuilder);
   await buildCampaigns(databaseBuilder);
+  await buildOrganizationLearners(databaseBuilder);
 }
 
 export { teamPrescriptionDataBuilder };
