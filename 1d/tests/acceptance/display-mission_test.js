e85bfed4a50c18f9a5cecd155dbc5930802aed81
Yaelle YAMA <yaelle.marsiac@pix.fr>
2023-10-09 14:49:38 +0200

    feat(1d): update mission start page design

--- 1d/tests/acceptance/display-mission_test.js
+++ 1d/tests/acceptance/display-mission_test.js
@@ -2,10 +2,12 @@ import { visit } from '@1024pix/ember-testing-library';
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
+import { setupIntl } from 'ember-intl/test-support';
 
 module('Acceptance | Mission', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
+  setupIntl(hooks);
 
   test('displays mission page', async function (assert) {
     // given
@@ -13,8 +15,8 @@ module('Acceptance | Mission', function (hooks) {
     // when
     const screen = await visit(`/missions/${mission.id}`);
     // then
-    assert.equal(screen.getAllByText(`${mission.name}`).length, 2);
-    assert.dom(screen.getByText('Je commence')).exists();
+    assert.dom(screen.getByText('Connaître la notion de données personnelles et savoir les protéger')).exists();
+    assert.dom(screen.getByText(this.intl.t('pages.missions.start-page.start-mission')));
     assert.dom(`[href="/missions/${mission.id}/resume"]`).exists();
   });
 });
--- 1d/tests/acceptance/start-mission_test.js
+++ 1d/tests/acceptance/start-mission_test.js
@@ -3,10 +3,12 @@ import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import { currentURL } from '@ember/test-helpers';
+import { setupIntl } from 'ember-intl/test-support';
 
 module('Acceptance | Challenge', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
+  setupIntl(hooks);
 
   test('redirect to first challenge page after clicking on start mission', async function (assert) {
     // given
@@ -14,7 +16,7 @@ module('Acceptance | Challenge', function (hooks) {
     this.server.create('challenge');
     // when
     await visit(`/missions/${mission.id}`);
-    await clickByText('Je commence');
+    await clickByText(this.intl.t('pages.missions.start-page.start-mission'));
     // then
     assert.strictEqual(currentURL(), `/assessments/1/challenges`);
   });
