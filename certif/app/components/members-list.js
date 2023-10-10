ba4dff6ad55eca08538b84c9bd3f90d26444c87c
Rébecca Kaci <rebecca.kaci@gmail.com>
2023-10-10 12:18:45 +0200

    refactor(certif): remove ordered members function by referer first

--- certif/app/components/members-list.js
+++ certif/app/components/members-list.js
@@ -4,24 +4,6 @@ import { service } from '@ember/service';
 export default class MembersList extends Component {
   @service featureToggles;
 
-  get orderedMemberListWithRefererFirst() {
-    const membersArray = [...this.args.members];
-
-    membersArray.sort((member1, member2) => {
-      if (member1.isReferer) {
-        return -1;
-      }
-
-      if (member2.isReferer) {
-        return 1;
-      }
-
-      return member1.lastName.localeCompare(member2.lastName, 'fr-FR', { sensitivity: 'base' });
-    });
-
-    return membersArray;
-  }
-
   get shouldDisplayRefererColumn() {
     return this.args.hasCleaHabilitation;
   }
