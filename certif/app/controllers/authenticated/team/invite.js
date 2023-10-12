578dee691827b3b7c9697036ca16a74235bf2425
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-10-12 17:12:02 +0200

    feat(certif): send invitations
    Co-authored-by: Rébecca Kaci <rebecca.kaci@gmail.com>
    Co-authored-by: LEGO Technix <109212476+lego-technix@users.noreply.github.com>

--- certif/app/controllers/authenticated/team/invite.js
+++ certif/app/controllers/authenticated/team/invite.js
@@ -3,18 +3,40 @@ import { service } from '@ember/service';
 import { action } from '@ember/object';
 
 export default class AuthenticatedTeamInviteController extends Controller {
+  @service currentUser;
+  @service intl;
+  @service notifications;
   @service router;
+  @service store;
 
   emails = null;
 
   @action
   async createCertificationCenterInvitation(event) {
     event.preventDefault();
+
+    const certificationCenterId = this.currentUser.currentAllowedCertificationCenterAccess.id;
+    const emails = this.emails?.replace(/ /g, '').split(',');
+
+    try {
+      await this.store.adapterFor('certification-center-invitation').sendInvitations({ certificationCenterId, emails });
+
+      const message = this.intl.t('pages.team-invite.notifications.success.invitations', {
+        emailsCount: emails.length,
+        email: emails[0],
+      });
+
+      this.notifications.success(message);
+
+      this.router.transitionTo('authenticated.team.list.invitations');
+    } catch (error) {
+      this.notifications.error();
+    }
   }
 
   @action
   updateEmail(event) {
-    this.emails = event.target?.value?.trim();
+    this.emails = event.target?.value;
   }
 
   @action
