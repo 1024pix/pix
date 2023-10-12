ef98b69deefb0af5c8a5c238ae1abf705f719e14
Dimitri Lahaye <dimitri.lahaye@pix.fr>
2023-10-12 12:08:32 +0200

    fix(mon-pix): remove tests utils initially built for pods

--- mon-pix/app/utils/tests/setup-intl-rendering.js
+++
@@ -1,7 +0,0 @@
-import setupIntl from './setup-intl';
-import { setupRenderingTest } from 'ember-qunit';
-
-export default function setupIntlRenderingTest(hooks) {
-  setupRenderingTest(hooks);
-  setupIntl(hooks);
-}
