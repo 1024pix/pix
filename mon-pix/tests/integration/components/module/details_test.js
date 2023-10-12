184111587e31c0d967b5dd2573138f2c18087122
Dimitri Lahaye <dimitri.lahaye@pix.fr>
2023-10-12 12:08:08 +0200

    fix(mon-pix): move module tests from pods to tests folder

--- mon-pix/tests/integration/components/module/details_test.js
+++ mon-pix/tests/integration/components/module/details_test.js
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
 import { render } from '@1024pix/ember-testing-library';
 import { hbs } from 'ember-cli-htmlbars';
-import setupIntlRenderingTest from '../../../../utils/tests/setup-intl-rendering';
+import setupIntlRenderingTest from '../../../helpers/setup-intl-rendering';
 
 module('Integration | Component | Module | Details', function (hooks) {
   setupIntlRenderingTest(hooks);
