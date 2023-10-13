64b08b40df636a5e2cf19d2c7740dde078d1a5b2
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-10-13 11:20:17 +0200

    feat(api): add updateModificationDate method
    to certification-center-invitation-repository

---
+++ api/tests/integration/infrastructure/repositories/certification-center-invitation-repository_test.js
@@ -0,0 +1,31 @@
+import { databaseBuilder, knex, expect, sinon } from '../../../test-helper.js';
+import * as certificationCenterInvitationRepository from '../../../../lib/infrastructure/repositories/certification-center-invitation-repository.js';
+
+describe('Integration | Infrastructure | Repositories | CertificationCenterInvitationRepository', function () {
+  describe('#updateModificationDate', function () {
+    it('updates the certification center invitation modification date', async function () {
+      // given
+      const certificationCenter = databaseBuilder.factory.buildCertificationCenter();
+      const certificationCenterInvitation = databaseBuilder.factory.buildCertificationCenterInvitation({
+        certificationCenterId: certificationCenter.id,
+        createdAt: new Date('2023-10-10'),
+        updatedAt: new Date('2023-10-10'),
+      });
+
+      await databaseBuilder.commit();
+
+      const now = new Date('2023-10-13');
+      const clock = sinon.useFakeTimers(now);
+
+      // when
+      await certificationCenterInvitationRepository.updateModificationDate(certificationCenterInvitation.id);
+
+      // then
+      const updatedCertificationCenterInvitation = await knex('certification-center-invitations')
+        .where({ id: certificationCenterInvitation.id })
+        .first();
+      expect(updatedCertificationCenterInvitation.updatedAt).to.deep.equal(now);
+      clock.restore();
+    });
+  });
+});
