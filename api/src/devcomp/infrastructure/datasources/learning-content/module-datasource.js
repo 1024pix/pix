25238e70a6d481701430c1218e07a65cb88adda4
Dimitri Lahaye <dimitri.lahaye@pix.fr>
2023-10-16 11:59:11 +0200

    feat(api): add empty list to module datasource

--- api/src/devcomp/infrastructure/datasources/learning-content/module-datasource.js
+++ api/src/devcomp/infrastructure/datasources/learning-content/module-datasource.js
@@ -4,7 +4,11 @@ import { Module } from '../../../domain/models/Module.js';
 const moduleDatasource = {
   getBySlug: async (slug) => {
     const availableModules = await Promise.resolve({
-      'les-adresses-mail': new Module({ id: 'f7b3a2e1-0d5c-4c6c-9c4d-1a3d8f7e9f5d', title: 'Les adresses mail' }),
+      'les-adresses-mail': new Module({
+        id: 'f7b3a2e1-0d5c-4c6c-9c4d-1a3d8f7e9f5d',
+        title: 'Les adresses mail',
+        list: [],
+      }),
     });
 
     if (availableModules[slug] === undefined) {
