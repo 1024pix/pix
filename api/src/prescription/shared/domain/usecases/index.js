206b351e96b3b6099385e4b9b663ccea01d25df7
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2023-10-16 12:02:40 +0200

    bsr(api): add domain model for shared usecase

--- api/src/prescription/shared/domain/usecases/index.js
+++ api/src/prescription/shared/domain/usecases/index.js
@@ -1,10 +1,10 @@
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 
-import * as supOrganizationLearnerRepository from '../../learner-management/infrastructure/repositories/sup-organization-learner-repository.js';
+import * as supOrganizationLearnerRepository from '../../../learner-management/infrastructure/repositories/sup-organization-learner-repository.js';
 
-import { importNamedExportsFromDirectory } from '../../../shared/infrastructure/utils/import-named-exports-from-directory.js';
-import { injectDependencies } from '../../../shared/infrastructure/utils/dependency-injection.js';
+import { importNamedExportsFromDirectory } from '../../../../shared/infrastructure/utils/import-named-exports-from-directory.js';
+import { injectDependencies } from '../../../../shared/infrastructure/utils/dependency-injection.js';
 
 const dependencies = {
   supOrganizationLearnerRepository,
@@ -14,7 +14,7 @@ const path = dirname(fileURLToPath(import.meta.url));
 
 const usecasesWithoutInjectedDependencies = {
   ...(await importNamedExportsFromDirectory({
-    path: join(path, '../../learner-management/domain/usecases/'),
+    path: join(path, '../../../learner-management/domain/usecases/'),
     ignoredFileNames: ['index.js'],
   })),
 };
