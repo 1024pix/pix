9d6fe48c34f1b485351c3dec42558f12ee659ea8
Dimitri Lahaye <dimitri.lahaye@pix.fr>
2023-10-18 15:26:54 +0200

    feat(mon-pix): add Element ember model

--- mon-pix/tests/unit/models/module_test.js
+++ mon-pix/tests/unit/models/module_test.js
@@ -5,10 +5,17 @@ module('Unit | Model | Module', function (hooks) {
   setupTest(hooks);
 
   test('Module model should exist with the right properties', function (assert) {
+    // given
     const title = 'Les adresses mail';
     const store = this.owner.lookup('service:store');
-    const model = store.createRecord('module', { title });
-    assert.ok(model);
-    assert.strictEqual(model.title, title);
+    const element = store.createRecord('element', { content: '' });
+
+    // when
+    const module = store.createRecord('module', { title, elements: [element] });
+
+    // then
+    assert.ok(module);
+    assert.strictEqual(module.title, title);
+    assert.strictEqual(module.elements.length, 1);
   });
 });
