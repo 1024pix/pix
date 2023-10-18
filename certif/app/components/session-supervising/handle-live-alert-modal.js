f263db26318c53d963cc2440e237b60cb63371b3
Alexandre COIN <alexandre@mbpdealexandre-2.home>
2023-10-18 15:24:53 +0200

    :sparkles: api: clear modal form on actions

--- certif/app/components/session-supervising/handle-live-alert-modal.js
+++ certif/app/components/session-supervising/handle-live-alert-modal.js
@@ -5,24 +5,48 @@ import {
   subcategoryToLabel,
 } from 'pix-certif/models/certification-issue-report';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class HandleLiveAlertModal extends Component {
-  reportOptions = inChallengeIssueReportSubCategories.map((subcategory) => ({
-    subcategory,
-    code: subcategoryToCode[subcategory],
-    label: subcategoryToLabel[subcategory],
+  @tracked issueReportReason = null;
+
+  reportOptions = inChallengeIssueReportSubCategories.map((subCategory) => ({
+    subCategory,
+    code: subcategoryToCode[subCategory],
+    label: subcategoryToLabel[subCategory],
   }));
+
+  @action
+  setIssueReportReason(event) {
+    this.issueReportReason = event.target.value;
+  }
+  get isValidateButtonDisabled() {
+    return this.issueReportReason === null;
+  }
+
+  @action
+  onClose() {
+    this._clearIssueReportReason();
+    this.args.closeConfirmationModal();
+  }
+
+  @action
+  onReject() {
+    this._clearIssueReportReason();
+    this.args.rejectLiveAlert();
+  }
 
   @action
   onSubmit(event) {
     event.preventDefault();
-
     const form = event.target;
-
     const formData = new FormData(form);
-
     const reason = formData.get('reportCategory');
-
     this.args.validateLiveAlert(reason);
+    this._clearIssueReportReason();
+  }
+
+  _clearIssueReportReason() {
+    this.issueReportReason = null;
   }
 }
