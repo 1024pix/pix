c7074f9bfce025045069eefc13dc1d0de57e7709
Marie-Hélène <marie-helene.rots@pix.fr>
2023-10-19 10:31:10 +0200

    feat(orga): add main checkbox in sup participant list

--- orga/app/components/sup-organization-participant/list.js
+++ orga/app/components/sup-organization-participant/list.js
@@ -2,7 +2,7 @@ import { service } from '@ember/service';
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
-
+import ENV from 'pix-orga/config/environment';
 export default class ListItems extends Component {
   @service currentUser;
   @tracked selectedStudent = null;
@@ -17,6 +17,15 @@ export default class ListItems extends Component {
       this.isLoadingGroups = false;
     });
   }
+
+  get showCheckbox() {
+    const isFeatureEnabled = ENV.APP.FT_DELETE_PARTICIPANT;
+
+    return isFeatureEnabled && this.currentUser.isAdminInOrganization;
+  }
+
+  @action
+  onToggleAll() {}
 
   get groups() {
     const groups = this.currentUser.organization.groups;
