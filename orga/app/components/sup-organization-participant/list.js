29db708f8a4e2cbec10208c7199797c6d0d4d368
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2023-10-19 15:21:19 +0200

    feat(orga): implement in element component to handle checkbox on sup participant list

--- orga/app/components/sup-organization-participant/list.js
+++ orga/app/components/sup-organization-participant/list.js
@@ -1,8 +1,10 @@
 import { service } from '@ember/service';
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { guidFor } from '@ember/object/internals';
 import { tracked } from '@glimmer/tracking';
 import ENV from 'pix-orga/config/environment';
+
 export default class ListItems extends Component {
   @service currentUser;
   @tracked selectedStudent = null;
@@ -24,8 +26,21 @@ export default class ListItems extends Component {
     return isFeatureEnabled && this.currentUser.isAdminInOrganization;
   }
 
-  @action
-  onToggleAll() {}
+  get headerId() {
+    return guidFor(this) + 'header';
+  }
+
+  get paginationId() {
+    return guidFor(this) + 'pagination';
+  }
+
+  get filtersId() {
+    return guidFor(this) + 'filters';
+  }
+
+  get hasStudents() {
+    return Boolean(this.args.students.length);
+  }
 
   get groups() {
     const groups = this.currentUser.organization.groups;
@@ -60,5 +75,17 @@ export default class ListItems extends Component {
   closeEditStudentNumberModal() {
     this.selectedStudent = null;
     this.isShowingEditStudentNumberModal = false;
+  }
+
+  @action
+  async addResetOnFunction(wrappedFunction, resetParticipants, ...args) {
+    await wrappedFunction(...args);
+    resetParticipants();
+  }
+
+  @action
+  addStopPropagationOnFunction(toggleParticipant, event) {
+    event.stopPropagation();
+    toggleParticipant();
   }
 }
