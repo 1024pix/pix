76077c48552332d7b51504e5992143b94dfae830
Guillaume Lagorce <guillaume@hey-gul.com>
2023-10-23 17:16:50 +0200

    Add acceptance test on cache controller
    Co-authored-by: <francois.de-metz@pix.fr>
    Co-authored-by: <iris.benoit@pix.fr>
    Co-authored-by: <jeremie.jade@pix.fr>
    Co-authored-by: <nicolas.lepage@pix.fr>

--- api/lib/infrastructure/datasources/learning-content/index.js
+++ api/lib/infrastructure/datasources/learning-content/index.js
@@ -2,6 +2,7 @@ import { areaDatasource } from './area-datasource.js';
 import { challengeDatasource } from './challenge-datasource.js';
 import { competenceDatasource } from './competence-datasource.js';
 import { courseDatasource } from './course-datasource.js';
+import { frameworkDatasource } from './framework-datasource.js';
 import { skillDatasource } from './skill-datasource.js';
 import { tubeDatasource } from './tube-datasource.js';
 import { tutorialDatasource } from './tutorial-datasource.js';
@@ -11,6 +12,7 @@ export {
   challengeDatasource,
   competenceDatasource,
   courseDatasource,
+  frameworkDatasource,
   skillDatasource,
   tubeDatasource,
   tutorialDatasource,
