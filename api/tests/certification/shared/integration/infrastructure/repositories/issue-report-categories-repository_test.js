0ae3dbdb6c112e4d1ed62c4b41c63aa7196d9820
Alexandre COIN <alexandre@mbpdealexandre-2.home>
2023-10-23 17:29:07 +0200

    :sparkles: api: save certification issue report

---
+++ api/tests/certification/shared/integration/infrastructure/repositories/issue-report-categories-repository_test.js
@@ -0,0 +1,22 @@
+import { expect, databaseBuilder, knex } from '../../../../../test-helper.js';
+import * as issueReportCategoryRepository from '../../../../../../src/certification/shared/infrastructure/repositories/issue-report-category-repository.js';
+
+describe('Integration | Repository | Issue Report Categories', function () {
+  afterEach(async function () {
+    await knex('issue-report-categories').delete();
+  });
+
+  describe('#get', function () {
+    it('should return a certification issue report', async function () {
+      // given
+      const issueReportCategory = databaseBuilder.factory.buildIssueReportCategory({ category: 'OTHER' });
+      await databaseBuilder.commit();
+
+      // when
+      const result = await issueReportCategoryRepository.get({ name: issueReportCategory.name });
+
+      // then
+      expect(result).to.deep.equal({ ...issueReportCategory });
+    });
+  });
+});
