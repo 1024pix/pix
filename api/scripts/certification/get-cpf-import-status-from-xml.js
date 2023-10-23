bd156664f8c9caf8e75ad13f0365977b278219e5
Steph0 <Steph0@users.noreply.github.com>
2023-10-23 14:38:03 +0200

    :recycle: api: cpf model into session bounded context

--- api/scripts/certification/get-cpf-import-status-from-xml.js
+++ api/scripts/certification/get-cpf-import-status-from-xml.js
@@ -7,7 +7,7 @@ import xml2js from 'xml2js';
 
 import { disconnect } from '../../db/knex-database-connection.js';
 import * as cpfCertificationResultRepository from '../../lib/infrastructure/repositories/cpf-certification-result-repository.js';
-import { CpfImportStatus } from '../../src/certification/courses/domain/models/CpfImportStatus.js';
+import { CpfImportStatus } from '../../src/certification/session/domain/models/CpfImportStatus.js';
 import * as url from 'url';
 
 const modulePath = url.fileURLToPath(import.meta.url);
