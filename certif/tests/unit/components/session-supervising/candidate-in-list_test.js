f2537774a65d09a45fecff189e46e85bf3b01851
Alexandre COIN <alexandre@mbpdealexandre-2.home>
2023-10-24 15:01:40 +0200

    :white_check_mark: certif: add test for reject live alert method

--- certif/tests/unit/components/session-supervising/candidate-in-list_test.js
+++ certif/tests/unit/components/session-supervising/candidate-in-list_test.js
@@ -65,4 +65,61 @@ module('Unit | Component | session-supervising/candidate-in-list', function (hoo
       });
     });
   });
+
+  module('rejectLiveAlert', function () {
+    module('when there is no error', function () {
+      test('it should call the method with the correct arguments', async function (assert) {
+        // given
+        const component = createGlimmerComponent('component:session-supervising/candidate-in-list');
+        component.args.sessionId = 123;
+        component.args.candidate = { userId: 456 };
+        component.notifications = { error: sinon.spy() };
+        const store = this.owner.lookup('service:store');
+        const adapter = store.adapterFor('session');
+        adapter.dismissLiveAlert = sinon.stub();
+        adapter.dismissLiveAlert.resolves();
+
+        class IntlStub extends Service {
+          t = sinon.stub();
+        }
+
+        this.owner.register('service:intl', IntlStub);
+
+        // when
+        await component.rejectLiveAlert();
+
+        // then
+        sinon.assert.calledWithExactly(adapter.dismissLiveAlert, 123, 456);
+        sinon.assert.notCalled(component.notifications.error);
+        assert.ok(true);
+      });
+    });
+
+    module('when there is an error', function () {
+      test('it should call the notification service', async function (assert) {
+        // given
+        const component = createGlimmerComponent('component:session-supervising/candidate-in-list');
+        component.args.sessionId = 123;
+        component.args.candidate = { userId: 456 };
+        component.notifications = { error: sinon.spy() };
+        const store = this.owner.lookup('service:store');
+        const adapter = store.adapterFor('session');
+        adapter.dismissLiveAlert = sinon.stub();
+        adapter.dismissLiveAlert.rejects();
+
+        class IntlStub extends Service {
+          t = sinon.stub();
+        }
+
+        this.owner.register('service:intl', IntlStub);
+
+        // when
+        await component.rejectLiveAlert();
+
+        // then
+        sinon.assert.calledOnce(component.notifications.error);
+        assert.ok(true);
+      });
+    });
+  });
 });
