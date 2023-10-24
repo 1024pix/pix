6a093f1722cf018d949976093ba8691104338fed
jeremie <jeremie.jade@pix.fr>
2023-10-24 15:33:28 +0200

    :recycle: fix toArray on promise
    Co-authored-by: Guillaume Lagorce <guillame.lagorce@pix.fr>
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>
    Co-authored-by: Antoine Ceol <antoine.ceol@pix.fr>
    Co-authored-by: Jérémie Picard <jeremie.picard@pix.fr>
    Co-authored-by: François de Metz <francois.de-metz@pix.fr>

--- certif/app/routes/authenticated/team/list/members.js
+++ certif/app/routes/authenticated/team/list/members.js
@@ -1,9 +1,9 @@
 import Route from '@ember/routing/route';
 
 export default class MembersRoute extends Route {
-  model() {
+  async model() {
     return {
-      members: this.modelFor('authenticated.team.list').members,
+      members: await this.modelFor('authenticated.team.list').members,
       hasCleaHabilitation: this.modelFor('authenticated.team.list').hasCleaHabilitation,
     };
   }
