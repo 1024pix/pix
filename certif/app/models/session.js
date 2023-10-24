0e8f37be4d449b1bb3ebdaa3f7483bca6a7e390d
François de Metz <francois.de-metz@pix.fr>
2023-10-24 14:37:17 +0200

    Fix some warnings in relations on ember certif
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>
    Co-authored-by: Guillaume Lagorce <guillaume.lagorce@pix.fr>
    Co-authored-by: Antoine Ceol <antoine.ceol@pix.fr>
    Co-authored-by: Jérémie Picard <jeremie.picard@pix.fr>

--- certif/app/models/session.js
+++ certif/app/models/session.js
@@ -29,7 +29,7 @@ export default class Session extends Model {
   @attr('boolean') hasIncident;
   @attr('boolean') hasJoiningIssue;
   @attr() certificationCenterId;
-  @hasMany('certificationReport') certificationReports;
+  @hasMany('certificationReport', { async: true, inverse: null }) certificationReports;
 
   @computed('status')
   get isFinalized() {
