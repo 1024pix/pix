41e61d16ad11052b64f3ad692a782bf0e88d4bb6
Yvonnick Frin <frin.yvonnick@gmail.com>
2023-10-25 16:12:43 +0200

    feat(orga): add success notifications when deleting students with success

--- orga/app/controllers/authenticated/sup-organization-participants/list.js
+++ orga/app/controllers/authenticated/sup-organization-participants/list.js
@@ -7,6 +7,8 @@ export default class ListController extends Controller {
   @service router;
   @service store;
   @service currentUser;
+  @service notifications;
+  @service intl;
   @tracked search = null;
   @tracked studentNumber = null;
   @tracked groups = [];
@@ -56,6 +58,13 @@ export default class ListController extends Controller {
     await this.store.adapterFor('sup-organization-participant').deleteParticipants(
       this.currentUser.organization.id,
       listLearners.map(({ id }) => id),
+    );
+    this.notifications.sendSuccess(
+      this.intl.t('pages.sup-organization-participants.action-bar.success-message', {
+        count: listLearners.length,
+        firstname: listLearners[0].firstName,
+        lastname: listLearners[0].lastName,
+      }),
     );
     this.send('refreshModel');
   }
