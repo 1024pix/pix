1078e42d10ead0103de4a8f97cc19307e9703d6a
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-10-25 16:14:00 +0200

    refactor(api): improve code execution flow for clarity

--- api/lib/domain/usecases/create-certification-center-membership-for-sco-organization-member.js
+++ api/lib/domain/usecases/create-certification-center-membership-for-sco-organization-member.js
@@ -6,25 +6,25 @@ const createCertificationCenterMembershipForScoOrganizationMember = async functi
 }) {
   const existingMembership = await membershipRepository.get(membership.id);
 
-  if (membership.isAdmin && existingMembership.organization.isScoAndHasExternalId) {
-    const existingCertificationCenter = await certificationCenterRepository.findByExternalId({
-      externalId: existingMembership.organization.externalId,
+  if (!membership.isAdmin || !existingMembership.organization.isScoAndHasExternalId) return;
+
+  const existingCertificationCenter = await certificationCenterRepository.findByExternalId({
+    externalId: existingMembership.organization.externalId,
+  });
+
+  if (!existingCertificationCenter) return;
+
+  const isAlreadyMemberOfCertificationCenter =
+    await certificationCenterMembershipRepository.isMemberOfCertificationCenter({
+      userId: existingMembership.user.id,
+      certificationCenterId: existingCertificationCenter.id,
     });
 
-    if (existingCertificationCenter) {
-      const isAlreadyMemberOfCertificationCenter =
-        await certificationCenterMembershipRepository.isMemberOfCertificationCenter({
-          userId: existingMembership.user.id,
-          certificationCenterId: existingCertificationCenter.id,
-        });
-
-      if (!isAlreadyMemberOfCertificationCenter) {
-        return await certificationCenterMembershipRepository.save({
-          userId: existingMembership.user.id,
-          certificationCenterId: existingCertificationCenter.id,
-        });
-      }
-    }
+  if (!isAlreadyMemberOfCertificationCenter) {
+    return await certificationCenterMembershipRepository.save({
+      userId: existingMembership.user.id,
+      certificationCenterId: existingCertificationCenter.id,
+    });
   }
 };
 
