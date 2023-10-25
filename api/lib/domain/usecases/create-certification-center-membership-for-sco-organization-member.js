9b416417f55430b8b35efcf1f14c5dfae0af0cdc
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-10-25 17:35:38 +0200

    feat(api): modify usecase business rules

--- api/lib/domain/usecases/create-certification-center-membership-for-sco-organization-member.js
+++ api/lib/domain/usecases/create-certification-center-membership-for-sco-organization-member.js
@@ -1,31 +1,43 @@
+import { CERTIFICATION_CENTER_MEMBERSHIP_ROLES } from '../models/CertificationCenterMembership.js';
+
 const createCertificationCenterMembershipForScoOrganizationMember = async function ({
   membership,
   membershipRepository,
   certificationCenterRepository,
   certificationCenterMembershipRepository,
 }) {
-  const existingMembership = await membershipRepository.get(membership.id);
+  const existingOrganizationMembership = await membershipRepository.get(membership.id);
 
-  if (!membership.isAdmin || !existingMembership.organization.isScoAndHasExternalId) return;
+  if (!membership.isAdmin || !existingOrganizationMembership.organization.isScoAndHasExternalId) return;
 
   const existingCertificationCenter = await certificationCenterRepository.findByExternalId({
-    externalId: existingMembership.organization.externalId,
+    externalId: existingOrganizationMembership.organization.externalId,
   });
 
   if (!existingCertificationCenter) return;
 
-  const isAlreadyMemberOfCertificationCenter =
-    await certificationCenterMembershipRepository.isMemberOfCertificationCenter({
-      userId: existingMembership.user.id,
+  const certificationCenterMembership =
+    await certificationCenterMembershipRepository.findByCertificationCenterIdAndUserId({
       certificationCenterId: existingCertificationCenter.id,
+      userId: existingOrganizationMembership.user.id,
     });
 
-  if (!isAlreadyMemberOfCertificationCenter) {
-    return await certificationCenterMembershipRepository.save({
-      userId: existingMembership.user.id,
+  if (!certificationCenterMembership) {
+    return await certificationCenterMembershipRepository.create({
       certificationCenterId: existingCertificationCenter.id,
+      role: CERTIFICATION_CENTER_MEMBERSHIP_ROLES.ADMIN,
+      userId: existingOrganizationMembership.user.id,
     });
   }
+
+  if (certificationCenterMembership.hasAdminRole) return;
+
+  certificationCenterMembership.updateRole({
+    role: CERTIFICATION_CENTER_MEMBERSHIP_ROLES.ADMIN,
+    updatedByUserId: membership.updatedByUserId,
+  });
+
+  await certificationCenterMembershipRepository.update(certificationCenterMembership);
 };
 
 export { createCertificationCenterMembershipForScoOrganizationMember };
