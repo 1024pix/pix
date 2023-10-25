5620e862818812a1aa2beb4902b797cdf62d5e43
Dimitri Lahaye <dimitri.lahaye@pix.fr>
2023-10-25 16:01:56 +0200

    feat(mon-pix): display qcu in template

--- mon-pix/app/pods/element/model.js
+++ mon-pix/app/pods/element/model.js
@@ -3,4 +3,12 @@ import Model, { belongsTo, attr } from '@ember-data/model';
 export default class Element extends Model {
   @belongsTo('module', { inverse: 'elements' }) module;
   @attr('string') type;
+
+  get isLesson() {
+    return this.type === 'lessons';
+  }
+
+  get isQcu() {
+    return this.type === 'qcus';
+  }
 }
