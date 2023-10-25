a7aabd1d66385a8c3ea1b71b70813369072a2f61
Simon Chareyron <schareyron@ippon.fr>
2023-10-25 17:11:44 +0200

    :sparkles: update add metrics to gist with the newly created time series class

--- api/scripts/arborescence-monitoring/add-metrics-to-gist.js
+++ api/scripts/arborescence-monitoring/add-metrics-to-gist.js
@@ -1,49 +1,27 @@
 import { readFile, writeFile } from 'fs/promises';
 import { countFilesInPath } from './stats.js';
+import { TimeSeries } from './time-series.js';
 
 async function parseTimeSeriesMetrics({ metricsFilepath }) {
   const currentRawMetrics = await readFile(metricsFilepath, { encoding: 'utf-8' });
   return JSON.parse(currentRawMetrics);
 }
 
-function sortTimeSeriesMetrics(existingTimeSeriesMetrics) {
-  return existingTimeSeriesMetrics.sort((a, b) => new Date(a.x).getTime() - new Date(b.x).getTime());
-}
-
 async function main() {
   const metricsFilepath = process.argv[2];
   const existingTimeSeriesMetrics = await parseTimeSeriesMetrics({ metricsFilepath });
 
-  const updatedTimeSeriesMetrics = await _addOrUpdateTodayValue(existingTimeSeriesMetrics);
-  const sortedTimeSeriesMetrics = sortTimeSeriesMetrics(updatedTimeSeriesMetrics);
-
-  const stringifiedMetrics = JSON.stringify(sortedTimeSeriesMetrics);
-  await writeFile(metricsFilepath, stringifiedMetrics);
+  let timeSeries = new TimeSeries(existingTimeSeriesMetrics);
+  timeSeries = await _addOrUpdateTodayValue(timeSeries);
+  await writeFile(metricsFilepath, timeSeries.toString());
 }
 
-async function _addOrUpdateTodayValue(existingTimeSeriesMetrics) {
+async function _addOrUpdateTodayValue(timeSeries) {
   const usecasesCount = await countFilesInPath('./lib/domain/usecases');
-
-  const todayIndex = _todayIndex(existingTimeSeriesMetrics);
-  existingTimeSeriesMetrics.splice(todayIndex, _addOrUpdate(todayIndex), {
+  return timeSeries.add({
     x: new Date().toJSON(),
     y: usecasesCount,
   });
-  return existingTimeSeriesMetrics;
-}
-
-function _todayIndex(existingTimeSeriesMetrics) {
-  return existingTimeSeriesMetrics.findIndex(function (metric) {
-    return _isSameDay(new Date(metric.x), new Date());
-  });
-}
-
-function _isSameDay(d1, d2) {
-  return d1.getFullYear() === d2.getFullYear() && d1.getDate() === d2.getDate() && d1.getMonth() === d2.getMonth();
-}
-
-function _addOrUpdate(todayIndex) {
-  return todayIndex >= 0 ? 1 : 0;
 }
 
 await main();
