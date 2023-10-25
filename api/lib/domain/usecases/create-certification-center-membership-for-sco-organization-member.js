227218423a6b623071bba9184defd84aef338c04
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-10-25 17:35:38 +0200

    feat(api): modify usecase business rules

--- api/lib/domain/usecases/create-certification-center-membership-for-sco-organization-member.js
+++ api/lib/domain/usecases/create-certification-center-membership-for-sco-organization-member.js
@@ -1,31 +1,76 @@
+import { CERTIFICATION_CENTER_MEMBERSHIP_ROLES } from '../models/CertificationCenterMembership.js';
+
 const createCertificationCenterMembershipForScoOrganizationMember = async function ({
   membership,
   membershipRepository,
   certificationCenterRepository,
   certificationCenterMembershipRepository,
 }) {
-  const existingMembership = await membershipRepository.get(membership.id);
+  const existingOrganizationMembership = await membershipRepository.get(membership.id);
 
-  if (!membership.isAdmin || !existingMembership.organization.isScoAndHasExternalId) return;
+  if (!membership.isAdmin || !existingOrganizationMembership.organization.isScoAndHasExternalId) return;
 
   const existingCertificationCenter = await certificationCenterRepository.findByExternalId({
-    externalId: existingMembership.organization.externalId,
+    externalId: existingOrganizationMembership.organization.externalId,
   });
 
   if (!existingCertificationCenter) return;
 
-  const isAlreadyMemberOfCertificationCenter =
-    await certificationCenterMembershipRepository.isMemberOfCertificationCenter({
-      userId: existingMembership.user.id,
+  const certificationCenterMembership =
+    await certificationCenterMembershipRepository.findByCertificationCenterIdAndUserId({
       certificationCenterId: existingCertificationCenter.id,
+      userId: existingOrganizationMembership.user.id,
     });
 
-  if (!isAlreadyMemberOfCertificationCenter) {
-    return await certificationCenterMembershipRepository.save({
-      userId: existingMembership.user.id,
+  if (certificationCenterMembership?.hasAdminRole) return;
+
+  const membersCount = await certificationCenterMembershipRepository.countMembersForCertificationCenter(
+    existingCertificationCenter.id,
+  );
+  const adminMembersCount = await certificationCenterMembershipRepository.countAdminMembersForCertificationCenter(
+    existingCertificationCenter.id,
+  );
+
+  if (membersCount === 0) {
+    return await _createCertificationCenterMembership({
       certificationCenterId: existingCertificationCenter.id,
+      role: CERTIFICATION_CENTER_MEMBERSHIP_ROLES.ADMIN,
+      userId: existingOrganizationMembership.user.id,
+      certificationCenterMembershipRepository,
     });
   }
+
+  if (!certificationCenterMembership) {
+    const role =
+      adminMembersCount === 0
+        ? CERTIFICATION_CENTER_MEMBERSHIP_ROLES.ADMIN
+        : CERTIFICATION_CENTER_MEMBERSHIP_ROLES.MEMBER;
+    return await _createCertificationCenterMembership({
+      certificationCenterId: existingCertificationCenter.id,
+      role,
+      userId: existingOrganizationMembership.user.id,
+      certificationCenterMembershipRepository,
+    });
+  }
+
+  if (certificationCenterMembership.hasMemberRole && adminMembersCount > 0) return;
+
+  certificationCenterMembership.updateRole({ role: CERTIFICATION_CENTER_MEMBERSHIP_ROLES.ADMIN });
+
+  await certificationCenterMembershipRepository.update(certificationCenterMembership);
+};
+
+const _createCertificationCenterMembership = async function ({
+  certificationCenterId,
+  role,
+  userId,
+  certificationCenterMembershipRepository,
+}) {
+  await certificationCenterMembershipRepository.create({
+    certificationCenterId,
+    role,
+    userId,
+  });
 };
 
 export { createCertificationCenterMembershipForScoOrganizationMember };
--- api/tests/integration/domain/usecases/create-certification-center-membership-for-sco-organization-member_test.js
+++ api/tests/integration/domain/usecases/create-certification-center-membership-for-sco-organization-member_test.js
@@ -6,6 +6,7 @@ import * as certificationCenterMembershipRepository from '../../../../lib/infras
 
 import { Membership } from '../../../../lib/domain/models/Membership.js';
 import { createCertificationCenterMembershipForScoOrganizationMember } from '../../../../lib/domain/usecases/create-certification-center-membership-for-sco-organization-member.js';
+import { CERTIFICATION_CENTER_MEMBERSHIP_ROLES } from '../../../../lib/domain/models/CertificationCenterMembership.js';
 
 describe('Integration | UseCases | create-certification-center-membership-for-sco-organization-member', function () {
   afterEach(function () {
@@ -53,6 +54,50 @@ describe('Integration | UseCases | create-certification-center-membership-for-sc
         expect(certificationCenterMembership).not.to.be.undefined;
         expect(certificationCenterMembership.userId).to.equal(userWhoseOrganizationRoleIsToUpdate.id);
         expect(certificationCenterMembership.certificationCenterId).to.equal(certificationCenterId);
+      });
+
+      context('when there is no member with the role "ADMIN"', function () {
+        it('creates a certification center membership with the role "ADMIN"', async function () {
+          // given
+          const externalId = 'foo';
+          const organizationId = databaseBuilder.factory.buildOrganization({ type: 'SCO', externalId }).id;
+          const userWhoseOrganizationRoleIsToUpdate = databaseBuilder.factory.buildUser();
+          const adminWhoWantsToMakeTheOrganizationRoleChange = databaseBuilder.factory.buildUser();
+          const membership = databaseBuilder.factory.buildMembership({
+            organizationRole: Membership.roles.ADMIN,
+            organizationId,
+            userId: userWhoseOrganizationRoleIsToUpdate.id,
+          });
+          const certificationCenterId = databaseBuilder.factory.buildCertificationCenter({ externalId }).id;
+          await databaseBuilder.commit();
+
+          const givenMembership = new Membership({
+            id: membership.id,
+            organizationRole: membership.organizationRole,
+            updatedByUserId: adminWhoWantsToMakeTheOrganizationRoleChange.id,
+          });
+
+          // when
+          await createCertificationCenterMembershipForScoOrganizationMember({
+            membership: givenMembership,
+            membershipRepository,
+            certificationCenterRepository,
+            certificationCenterMembershipRepository,
+          });
+
+          // then
+          const certificationCenterMembership = await knex('certification-center-memberships')
+            .where({
+              userId: userWhoseOrganizationRoleIsToUpdate.id,
+              certificationCenterId,
+            })
+            .first();
+
+          expect(certificationCenterMembership).not.to.be.undefined;
+          expect(certificationCenterMembership.userId).to.equal(userWhoseOrganizationRoleIsToUpdate.id);
+          expect(certificationCenterMembership.certificationCenterId).to.equal(certificationCenterId);
+          expect(certificationCenterMembership.role).to.equal(CERTIFICATION_CENTER_MEMBERSHIP_ROLES.ADMIN);
+        });
       });
     });
   });
