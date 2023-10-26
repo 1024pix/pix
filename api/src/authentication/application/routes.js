54227a94f22eedca791aac168a67348c5d37159f
Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>
2023-10-26 15:20:19 +0200

    refactor(api) : migrate authentication to src
    Team Accès 🫶
    
    Co-authored-by: Clément Latzarus <clement.latzarus@pix.fr>
    Co-authored-by: Ismael Gorissen <ismael.gorissen@gmail.com>
    Co-authored-by: LEGO Technix <109212476+lego-technix@users.noreply.github.com>
    Co-authored-by: Rébecca Kaci <rebecca.kaci@gmail.com>
    Co-authored-by: Eric Lim <eric.lim@zenika.com>
    Co-authored-by: Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>
    Co-authored-by: Yoan de LUCA <yoan.de-luca@pix.fr>

---
+++ api/src/authentication/application/routes.js
@@ -0,0 +1,45 @@
+import Joi from 'joi';
+import { securityPreHandlers } from '../../../lib/application/security-pre-handlers.js';
+import { authenticationController as AuthenticationController } from './authentication-controller.js';
+
+const register = async function (server) {
+  server.route([
+    {
+      method: 'POST',
+      path: '/api/token',
+      config: {
+        auth: false,
+        payload: {
+          allow: 'application/x-www-form-urlencoded',
+        },
+        validate: {
+          payload: Joi.alternatives().try(
+            Joi.object().required().keys({
+              grant_type: 'password',
+              username: Joi.string().required(),
+              password: Joi.string().required(),
+              scope: Joi.string(),
+            }),
+            Joi.object().required().keys({
+              grant_type: 'refresh_token',
+              refresh_token: Joi.string(),
+              scope: Joi.string(),
+            }),
+          ),
+        },
+        pre: [{ method: securityPreHandlers.checkIfUserIsBlocked }],
+        handler: AuthenticationController.createToken,
+        tags: ['api'],
+        notes: [
+          "Cette route permet d'obtenir un refresh token et access token à partir d'un couple identifiant / mot de passe" +
+            " ou un access token à partir d'un refresh token valide.",
+        ],
+      },
+    },
+  ]);
+};
+
+const name = 'authentication-api';
+const authenticationRoutes = [{ register, name }];
+
+export { authenticationRoutes };
