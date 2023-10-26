e414473c87d2da7143b2abcd69c745766c9b7b0f
Alexandre COIN <alexandre@mbpdealexandre-2.home>
2023-10-26 16:47:50 +0200

    :sparkles: certif: remove E category for V3 certification

--- certif/tests/integration/components/issue-report-modal/add-issue-report-modal_test.js
+++ certif/tests/integration/components/issue-report-modal/add-issue-report-modal_test.js
@@ -6,6 +6,7 @@ import { hbs } from 'ember-cli-htmlbars';
 import sinon from 'sinon';
 import EmberObject from '@ember/object';
 import {
+  v3CertificationIssueReportCategories,
   certificationIssueReportCategories,
   categoryToLabel,
   categoryToCode,
@@ -42,38 +43,85 @@ module('Integration | Component | add-issue-report-modal', function (hooks) {
     assert.dom(screen.getByText('Signalement du candidat : Lisa Monpud', { exact: false })).exists();
   });
 
-  test('it should show all categories code & label', async function (assert) {
-    // given
-    const report = EmberObject.create({
-      certificationCourseId: 1,
-      firstName: 'Lisa',
-      lastName: 'Monpud',
-      hasSeenEndTestScreen: false,
+  module('when session is V2', function () {
+    test('it should show all categories code & label', async function (assert) {
+      // given
+      const report = EmberObject.create({
+        certificationCourseId: 1,
+        firstName: 'Lisa',
+        lastName: 'Monpud',
+        hasSeenEndTestScreen: false,
+      });
+      const closeAddIssueReportModalStub = sinon.stub();
+      this.set('closeAddIssueReportModal', closeAddIssueReportModalStub);
+      this.set('reportToEdit', report);
+      this.set('maxlength', 500);
+
+      // when
+      const screen = await renderScreen(hbs`
+            <IssueReportModal::AddIssueReportModal
+              @closeModal={{this.closeAddIssueReportModal}}
+              @report={{this.reportToEdit}}
+              @maxlength={{@issueReportDescriptionMaxLength}}
+              @version={{2}}
+            />
+          `);
+
+      // then
+      for (const category of Object.values(certificationIssueReportCategories)) {
+        assert
+          .dom(
+            screen.getByLabelText(`${categoryToCode[category]} ${this.intl.t(categoryToLabel[category])}`, {
+              exact: false,
+            }),
+          )
+          .exists();
+      }
     });
-    const closeAddIssueReportModalStub = sinon.stub();
-    this.set('closeAddIssueReportModal', closeAddIssueReportModalStub);
-    this.set('reportToEdit', report);
-    this.set('maxlength', 500);
+  });
 
-    // when
-    const screen = await renderScreen(hbs`
-          <IssueReportModal::AddIssueReportModal
-            @closeModal={{this.closeAddIssueReportModal}}
-            @report={{this.reportToEdit}}
-            @maxlength={{@issueReportDescriptionMaxLength}}
-          />
-        `);
+  module('when session is V3', function () {
+    test('it should show all V3 categories code & label', async function (assert) {
+      // given
+      const report = EmberObject.create({
+        certificationCourseId: 1,
+        firstName: 'Lisa',
+        lastName: 'Monpud',
+        hasSeenEndTestScreen: false,
+      });
+      const closeAddIssueReportModalStub = sinon.stub();
+      this.set('closeAddIssueReportModal', closeAddIssueReportModalStub);
+      this.set('reportToEdit', report);
+      this.set('maxlength', 500);
 
-    // then
-    for (const category of Object.values(certificationIssueReportCategories)) {
+      // when
+      const screen = await renderScreen(hbs`
+            <IssueReportModal::AddIssueReportModal
+              @closeModal={{this.closeAddIssueReportModal}}
+              @report={{this.reportToEdit}}
+              @maxlength={{@issueReportDescriptionMaxLength}}
+              @version={{3}}
+            />
+          `);
+
+      // then
+      for (const category of Object.values(v3CertificationIssueReportCategories)) {
+        assert
+          .dom(
+            screen.getByLabelText(`${categoryToCode[category]} ${this.intl.t(categoryToLabel[category])}`, {
+              exact: false,
+            }),
+          )
+          .exists();
+      }
       assert
         .dom(
-          screen.getByLabelText(`${categoryToCode[category]} ${this.intl.t(categoryToLabel[category])}`, {
+          screen.queryByLabelText('E1-E12 Problème technique sur une question', {
             exact: false,
           }),
         )
-        .exists();
-    }
+        .doesNotExist();
+    });
   });
 
   test('it should show an error when trying to submit without selecting a category', async function (assert) {
