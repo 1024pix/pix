27945046a7d446db6ba2a239eb71d9cdacc55250
Yaelle YAMA <yaelle.marsiac@pix.fr>
2023-10-27 12:11:27 +0200

    feat(api-1d): retrieve the missionId in mission-assessment

---
+++ api/tests/school/integration/infrastructure/repositories/mission-assessment-repository_test.js
@@ -0,0 +1,18 @@
+import { databaseBuilder, expect } from '../../../../test-helper.js';
+import * as missionAssessmentRepository from '../../../../../src/school/infrastructure/repositories/mission-assessment-repository.js';
+import { MissionAssessment } from '../../../../../src/school/domain/models/MissionAssessment.js';
+
+describe('Integration | Repository | mission-assessment-repository', function () {
+  describe('#getByAssessmentId', function () {
+    it('returns the missionAssessment corresponding to the assessmentId', async function () {
+      const missionId = 'flute78';
+      const assessmentId = databaseBuilder.factory.buildPix1dAssessment({ missionId }).id;
+      databaseBuilder.factory.buildMissionAssessment({ missionId, assessmentId });
+      await databaseBuilder.commit();
+
+      const result = await missionAssessmentRepository.getByAssessmentId({ assessmentId });
+
+      expect(result).to.deep.equal(new MissionAssessment({ missionId, assessmentId }));
+    });
+  });
+});
