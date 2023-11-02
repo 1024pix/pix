1f7beb081801ade71be5f2b0a7fcd8283cb12a83
Rébecca Kaci <rebecca.kaci@gmail.com>
2023-11-02 15:48:23 +0100

    feat(admin): display list of pending invitations

--- admin/app/models/certification-center-invitation.js
+++ admin/app/models/certification-center-invitation.js
@@ -3,6 +3,18 @@ import Model, { attr, belongsTo } from '@ember-data/model';
 export default class CertificationCenterInvitationModel extends Model {
   @attr email;
   @attr updatedAt;
+  @attr role;
 
   @belongsTo('certificationCenter') certificationCenter;
+
+  get roleLabel() {
+    switch (this.role) {
+      case 'ADMIN':
+        return 'Administrateur';
+      case 'MEMBER':
+        return 'Membre';
+      default:
+        return '-';
+    }
+  }
 }
