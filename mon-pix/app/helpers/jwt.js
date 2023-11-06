a6228af36faf3db1410b4c63bdace57eac6fa064
François de Metz <francois.de-metz@pix.fr>
2023-11-06 10:43:50 +0100

    Update import name

--- mon-pix/app/helpers/jwt.js
+++ mon-pix/app/helpers/jwt.js
@@ -1,8 +1,8 @@
 import { helper } from '@ember/component/helper';
-import jwt_decode from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 
 export function decodeToken(accessToken) {
-  return jwt_decode(accessToken);
+  return jwtDecode(accessToken);
 }
 
 export default helper(decodeToken);
