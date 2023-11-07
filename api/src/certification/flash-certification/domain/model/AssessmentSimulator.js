527ec1a11762dd485e77af7c63cd133493036cc1
cberthou <clement.berthou.tsp@gmail.com>
2023-11-07 11:07:44 +0100

    :recycle: api: extract AssessmentSimulator strategy into another class

--- api/src/certification/flash-certification/domain/model/AssessmentSimulator.js
+++ api/src/certification/flash-certification/domain/model/AssessmentSimulator.js
@@ -1,64 +1,6 @@
-import { Answer } from '../../../../evaluation/domain/models/Answer.js';
-
 export class AssessmentSimulator {
-  constructor({ algorithm, challenges, pickChallenge, pickAnswerStatus, initialCapacity }) {
-    this.algorithm = algorithm;
-    this.challenges = challenges;
-    this.pickAnswerStatus = pickAnswerStatus;
-    this.pickChallenge = pickChallenge;
-    this.initialCapacity = initialCapacity;
-  }
-
-  _runSimulatorStep({ challengesAnswers, stepIndex }) {
-    const possibleChallenges = this.algorithm.getPossibleNextChallenges({
-      allAnswers: challengesAnswers,
-      challenges: this.challenges,
-      initialCapacity: this.initialCapacity,
-    });
-
-    const nextChallenge = this.pickChallenge({ possibleChallenges });
-
-    const answerStatus = this.pickAnswerStatus({
-      answerIndex: stepIndex,
-      nextChallenge,
-    });
-
-    const noMoreAnswerRemaining = !answerStatus;
-
-    if (noMoreAnswerRemaining) {
-      return null;
-    }
-
-    const estimatedLevelBeforeAnswering = this.algorithm.getEstimatedLevelAndErrorRate({
-      allAnswers: challengesAnswers,
-      challenges: this.challenges,
-      initialCapacity: this.initialCapacity,
-    }).estimatedLevel;
-
-    const newAnswers = [...challengesAnswers, new Answer({ result: answerStatus, challengeId: nextChallenge.id })];
-
-    const { estimatedLevel, errorRate } = this.algorithm.getEstimatedLevelAndErrorRate({
-      allAnswers: newAnswers,
-      challenges: this.challenges,
-      initialCapacity: this.initialCapacity,
-    });
-
-    const reward = this.algorithm.getReward({
-      estimatedLevel: estimatedLevelBeforeAnswering,
-      difficulty: nextChallenge.difficulty,
-      discriminant: nextChallenge.discriminant,
-    });
-
-    return {
-      result: {
-        challenge: nextChallenge,
-        errorRate,
-        estimatedLevel,
-        reward,
-        answerStatus,
-      },
-      challengesAnswers: newAnswers,
-    };
+  constructor({ strategy }) {
+    this.strategy = strategy;
   }
 
   run() {
@@ -67,7 +9,7 @@ export class AssessmentSimulator {
 
     for (let i = 0; i < Infinity; i++) {
       try {
-        const simulatorStepResult = this._runSimulatorStep({ challengesAnswers, stepIndex: i });
+        const simulatorStepResult = this.strategy.run({ challengesAnswers, stepIndex: i });
 
         if (!simulatorStepResult) {
           break;
