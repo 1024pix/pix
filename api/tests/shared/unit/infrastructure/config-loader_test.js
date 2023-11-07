ba8b003db02e3e6a5987214876e871a0d15c357c
Steph0 <Steph0@users.noreply.github.com>
2023-11-07 15:42:16 +0100

    :sparkles: api: dotenv config file loading
    Co-authored-by: sims07 <simon.chareyron@gmail.com>

--- api/tests/shared/unit/infrastructure/config-loader_test.js
+++ api/tests/shared/unit/infrastructure/config-loader_test.js
@@ -1,16 +1,31 @@
 import { expect } from '../../../test-helper.js';
-import { configLoader } from '../../../../src/shared/infrastructure/config-loader.js';
+import { ConfigLoader } from '../../../../src/shared/infrastructure/config-loader.js';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 describe('Unit | Shared | infrastructure | config-loader', function () {
-  describe('test.env file with property KEY=one', function () {
-    it('should return one', function () {
-      // given
+  describe('test.env file with properties KEY=one, KEYTWO=kiloutou', function () {
+    let configLoader;
+    beforeEach(async function () {
+      configLoader = new ConfigLoader({ configDirectoryPath: __dirname });
+      await configLoader.loadConfigFile();
+    });
 
-      // when
+    it('should return one', function () {
+      // given, when
       const result = configLoader.get('KEY');
 
       // then
       expect(result).to.equal('one');
+    });
+
+    it('should return kiloutou', function () {
+      // given, when
+      const result = configLoader.get('KEYTWO');
+
+      // then
+      expect(result).to.equal('kiloutou');
     });
   });
 });
