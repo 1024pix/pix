64cca6cd80bdcf667a6d6a6cc970c8b7862c678c
Andreia Pena <andreia.pena@pix.fr>
2023-11-08 14:21:10 +0100

    🚚 api: move model to src

--- api/tests/tooling/domain-builder/factory/build-session-for-supervisor-kit.js
+++ api/tests/tooling/domain-builder/factory/build-session-for-supervisor-kit.js
@@ -1,4 +1,4 @@
-import { SessionForSupervisorKit } from '../../../../lib/domain/read-models/SessionForSupervisorKit.js';
+import { SessionForSupervisorKit } from '../../../../src/certification/session/domain/read-models/SessionForSupervisorKit.js';
 
 const buildSessionForSupervisorKit = function ({
   id = 123,
