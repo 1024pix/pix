52ca782d4c49368a08a3a713ea04053e7d2b6742
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-11-08 15:09:36 +0100

    sr(api): improve error handling in getBySlug
    Co-authored-by: Miranda Lin-Guignard <miranda.lin-guignard@pix.fr>

--- api/tests/devcomp/integration/repositories/module-repository_test.js
+++ api/tests/devcomp/integration/repositories/module-repository_test.js
@@ -2,13 +2,12 @@ import { catchErr, expect } from '../../../test-helper.js';
 import { NotFoundError } from '../../../../src/shared/domain/errors.js';
 import * as moduleRepository from '../../../../src/devcomp/infrastructure/repositories/module-repository.js';
 import { Module } from '../../../../src/devcomp/domain/models/Module.js';
-import { Element } from '../../../../src/devcomp/domain/models/element/Element.js';
 import { Grain } from '../../../../src/devcomp/domain/models/Grain.js';
 import moduleDatasource from '../../../../src/devcomp/infrastructure/datasources/learning-content/module-datasource.js';
 
 describe('Integration | DevComp | Repositories | ModuleRepository', function () {
   describe('#getBySlug', function () {
-    it('should throw an error if the module does not exist', async function () {
+    it('should throw a NotFoundError if the module does not exist', async function () {
       // given
       const nonExistingModuleSlug = 'dresser-des-pokemons';
 
@@ -17,6 +16,28 @@ describe('Integration | DevComp | Repositories | ModuleRepository', function () 
 
       // then
       expect(error).to.be.instanceOf(NotFoundError);
+    });
+
+    it('should throw a Error if the module instanciation throw an error', async function () {
+      // given
+      const moduleSlug = 'incomplete-module';
+      const moduleDatasourceStub = {
+        getBySlug: async () => {
+          return {
+            id: 1,
+            slug: moduleSlug,
+          };
+        },
+      };
+
+      // when
+      const error = await catchErr(moduleRepository.getBySlug)({
+        slug: moduleSlug,
+        moduleDatasource: moduleDatasourceStub,
+      });
+
+      // then
+      expect(error).not.to.be.instanceOf(NotFoundError);
     });
 
     it('should return a module if it exists', async function () {
@@ -32,7 +53,6 @@ describe('Integration | DevComp | Repositories | ModuleRepository', function () 
       // then
       expect(module).to.be.instanceOf(Module);
       expect(module.grains.every((grain) => grain instanceof Grain)).to.be.true;
-      expect(module.elements.every((element) => element instanceof Element)).to.be.true;
     });
   });
 });
