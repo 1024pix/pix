5018a650f79d6ded450bfdb038cab11747c4f8a4
cberthou <clement.berthou.tsp@gmail.com>
2023-11-08 10:55:55 +0100

    :sparkles: api: add double measure strategy in simulator

---
+++ api/src/certification/flash-certification/domain/model/AssessmentSimulatorDoubleMeasureStrategy.js
@@ -0,0 +1,73 @@
+import { Answer } from '../../../../evaluation/domain/models/Answer.js';
+
+const NUMBER_OF_MEASURES = 2;
+export class AssessmentSimulatorDoubleMeasureStrategy {
+  constructor({ algorithm, challenges, pickChallenge, pickAnswerStatus, initialCapacity }) {
+    this.algorithm = algorithm;
+    this.challenges = challenges;
+    this.pickAnswerStatus = pickAnswerStatus;
+    this.pickChallenge = pickChallenge;
+    this.initialCapacity = initialCapacity;
+  }
+
+  run({ challengesAnswers, stepIndex }) {
+    const result = [];
+    const challengeAnswers = [];
+    const possibleChallenges = this.algorithm.getPossibleNextChallenges({
+      allAnswers: challengesAnswers,
+      challenges: this.challenges,
+      initialCapacity: this.initialCapacity,
+    });
+
+    for (let index = 0; index < NUMBER_OF_MEASURES; index++) {
+      const availableChallenges = possibleChallenges.filter(({ id }) => {
+        return challengeAnswers.every(({ challengeId }) => challengeId !== id);
+      });
+      const { hasAssessmentEnded, nextChallenge, answer } = this._getNextChallengeAndAnswer({
+        possibleChallenges: availableChallenges,
+        stepIndex: stepIndex + index,
+      });
+
+      if (hasAssessmentEnded) {
+        return null;
+      }
+
+      result.push({
+        challenge: nextChallenge,
+      });
+
+      challengeAnswers.push(answer);
+    }
+
+    return {
+      result,
+      challengeAnswers,
+    };
+  }
+
+  _getNextChallengeAndAnswer({ possibleChallenges, stepIndex }) {
+    const nextChallenge = this.pickChallenge({ possibleChallenges });
+
+    const answerStatus = this.pickAnswerStatus({
+      answerIndex: stepIndex,
+      nextChallenge,
+    });
+
+    const noMoreAnswerRemaining = !answerStatus;
+
+    if (noMoreAnswerRemaining) {
+      return {
+        hasAssessmentEnded: true,
+      };
+    }
+
+    return {
+      nextChallenge,
+      answer: new Answer({
+        result: answerStatus,
+        challengeId: nextChallenge.id,
+      }),
+      hasAssessmentEnded: false,
+    };
+  }
+}
