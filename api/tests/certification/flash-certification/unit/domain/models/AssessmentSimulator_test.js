4c8f619b5e628c821aea15bb8b00a75e81dec8b1
cberthou <clement.berthou.tsp@gmail.com>
2023-11-08 10:26:15 +0100

    :white_check_mark: api: improve AssessmentSimulator tests

--- api/tests/certification/flash-certification/unit/domain/models/AssessmentSimulator_test.js
+++ api/tests/certification/flash-certification/unit/domain/models/AssessmentSimulator_test.js
@@ -1,18 +1,28 @@
 import { expect, domainBuilder, sinon } from '../../../../../test-helper.js';
-import { AnswerStatus, AssessmentSimulator } from '../../../../../../lib/domain/models/index.js';
+import { Answer, AnswerStatus, AssessmentSimulator } from '../../../../../../lib/domain/models/index.js';
 
 describe('Unit | Domain | Models | AssessmentSimulator', function () {
   describe('#run', function () {
     it('should return the list of all the challenges', function () {
       // given
-      const challenge = domainBuilder.buildChallenge({ id: 'rec2' });
-      const answerForSimulator = AnswerStatus.OK;
-      const expectedEstimatedLevel = 0.4;
-      const expectedErrorRate = 0.2;
-      const expectedReward = 5;
+      const firstChallenge = domainBuilder.buildChallenge({ id: 'rec1' });
+      const secondChallenge = domainBuilder.buildChallenge({ id: 'rec2' });
+      const answersForSimulator = [AnswerStatus.OK, AnswerStatus.KO];
+      const expectedEstimatedLevels = [0.4, 0.2];
+      const expectedErrorRates = [0.2, 0.1];
+      const expectedRewards = [5, 3];
       const strategy = {
         run: sinon.stub(),
       };
+
+      const firstRunAnswer = new Answer({
+        result: answersForSimulator[0],
+        challengeId: firstChallenge.id,
+      });
+      const secondRunAnswer = new Answer({
+        result: answersForSimulator[1],
+        challengeId: secondChallenge.id,
+      });
 
       strategy.run
         .withArgs({
@@ -21,26 +31,44 @@ describe('Unit | Domain | Models | AssessmentSimulator', function () {
         })
         .returns({
           result: {
-            challenge: challenge,
-            estimatedLevel: expectedEstimatedLevel,
-            errorRate: expectedErrorRate,
-            reward: expectedReward,
-            answerStatus: answerForSimulator,
+            challenge: firstChallenge,
+            estimatedLevel: expectedEstimatedLevels[0],
+            errorRate: expectedErrorRates[0],
+            reward: expectedRewards[0],
+            answerStatus: answersForSimulator[0],
           },
-          challengeAnswer: domainBuilder.buildAnswer({
-            result: answerForSimulator,
-            challengeId: challenge.id,
-          }),
+          challengeAnswer: firstRunAnswer,
+        })
+        .withArgs({
+          challengesAnswers: [firstRunAnswer],
+          stepIndex: 1,
+        })
+        .returns({
+          result: {
+            challenge: secondChallenge,
+            estimatedLevel: expectedEstimatedLevels[1],
+            errorRate: expectedErrorRates[1],
+            reward: expectedRewards[1],
+            answerStatus: answersForSimulator[1],
+          },
+          challengeAnswer: secondRunAnswer,
         });
 
       // when
       const expectedResult = [
         {
-          challenge,
-          estimatedLevel: expectedEstimatedLevel,
-          errorRate: expectedErrorRate,
-          reward: expectedReward,
-          answerStatus: answerForSimulator,
+          challenge: firstChallenge,
+          estimatedLevel: expectedEstimatedLevels[0],
+          errorRate: expectedErrorRates[0],
+          reward: expectedRewards[0],
+          answerStatus: answersForSimulator[0],
+        },
+        {
+          challenge: secondChallenge,
+          estimatedLevel: expectedEstimatedLevels[1],
+          errorRate: expectedErrorRates[1],
+          reward: expectedRewards[1],
+          answerStatus: answersForSimulator[1],
         },
       ];
 
