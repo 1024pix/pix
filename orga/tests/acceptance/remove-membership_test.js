542b7b6b72b36adc3914f960d2f28131fd0deaf9
Marie-Hélène <marie-helene.rots@pix.fr>
2023-11-08 14:45:45 +0100

    chore(orga): stop using contain in remove-membership test

--- orga/tests/acceptance/remove-membership_test.js
+++ orga/tests/acceptance/remove-membership_test.js
@@ -6,23 +6,19 @@ import authenticateSession from '../helpers/authenticate-session';
 import { createPrescriberByUser, createUserMembershipWithRole } from '../helpers/test-init';
 
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
-
-async function createAuthenticatedSession() {
-  const user = createUserMembershipWithRole('ADMIN');
-  createPrescriberByUser(user);
-
-  await authenticateSession(user.id);
-
-  return user;
-}
+import setupIntl from '../helpers/setup-intl';
 
 module('Acceptance | Remove membership', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
+  setupIntl(hooks);
   let user;
 
   hooks.beforeEach(async function () {
-    const adminUser = await createAuthenticatedSession();
+    const adminUser = createUserMembershipWithRole('ADMIN');
+    createPrescriberByUser(adminUser);
+
+    await authenticateSession(adminUser.id);
     const organizationId = adminUser.memberships.models.firstObject.organizationId;
 
     user = server.create('user', { firstName: 'John', lastName: 'Doe' });
@@ -32,15 +28,23 @@ module('Acceptance | Remove membership', function (hooks) {
   test('should remove the membership', async function (assert) {
     // given
     const screen = await visit('/equipe');
-    await clickByName('Gérer');
-    await clickByName('Supprimer');
+
+    await clickByName(this.intl.t('pages.team-members.actions.manage'));
+    await clickByName(this.intl.t('pages.team-members.actions.remove-membership'));
 
     await screen.findByRole('dialog');
 
     // when
-    await clickByName('Oui, supprimer le membre');
+    await clickByName(this.intl.t('pages.team-members.remove-membership-modal.actions.remove'));
 
     // then
-    assert.contains(`${user.firstName} ${user.lastName} a été supprimé avec succès de votre équipe Pix Orga.`);
+    assert.ok(
+      screen.findByText(
+        this.intl.t('pages.team-members.notifications.remove-membership.success', {
+          memberFirstName: user.firstName,
+          memberLastName: user.lastName,
+        }),
+      ),
+    );
   });
 });
