a804f677076d4918810e257971e6703c627d5855
Yaelle YAMA <yaelle.marsiac@pix.fr>
2023-11-09 10:09:06 +0100

    feat(1d): replace button-link to button with loading state

--- 1d/tests/acceptance/display-mission-info_test.js
+++ 1d/tests/acceptance/display-mission-info_test.js
@@ -18,7 +18,6 @@ module('Acceptance | Display informations about the mission', function (hooks) {
     const screen = await visit(`/missions/${mission.id}`);
     // then
     assert.dom(screen.getByText('Connaître la notion de données personnelles et savoir les protéger')).exists();
-    assert.dom(screen.getByText(this.intl.t('pages.missions.start-page.start-mission')));
-    assert.dom(`[href="/missions/${mission.id}/resume"]`).exists();
+    assert.dom(screen.getByText(this.intl.t('pages.missions.start-page.start-mission'))).exists();
   });
 });
