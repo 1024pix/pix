cbeec5443d64344fcc919e9fe913b3b2cd7f1606
Steph0 <Steph0@users.noreply.github.com>
2023-11-10 14:26:11 +0100

    :sparkles: api: update certification courses from CPF receipt

--- api/src/certification/session/domain/usecases/integrate-cpf-processing-receipts.js
+++ api/src/certification/session/domain/usecases/integrate-cpf-processing-receipts.js
@@ -1,11 +1,16 @@
 import { logger } from '../../../../shared/infrastructure/utils/logger.js';
+import bluebird from 'bluebird';
+import { CONCURRENCY_HEAVY_OPERATIONS } from '../../../../../lib/infrastructure/constants.js';
 
-const integrateCpfProccessingReceipts = async function ({ cpfReceiptsStorage }) {
+const integrateCpfProccessingReceipts = async function ({ cpfReceiptsStorage, cpfCertificationResultRepository }) {
   const cpfReceipts = await cpfReceiptsStorage.findAll();
   for (const cpfReceipt of cpfReceipts) {
     logger.debug('Treatment of CPF receipt %o', cpfReceipt);
     const cpfInfos = await cpfReceiptsStorage.getCpfInfosByReceipt({ cpfReceipt });
-    return cpfInfos;
+
+    await bluebird.map(cpfInfos, async (cpfInfos) => cpfCertificationResultRepository.updateCpfInfos({ cpfInfos }), {
+      concurrency: CONCURRENCY_HEAVY_OPERATIONS,
+    });
   }
 };
 
