cdb452d6b30f1a6b381db1a0e6382e17b569b484
Steph0 <Steph0@users.noreply.github.com>
2023-11-10 18:06:43 +0100

    :sparkles: api: CPF import status script plugged to new usecase
    * Deletion of previous usecase using one-off container local file
    * A bit of logging added

--- api/scripts/certification/get-cpf-import-status-from-xml.js
+++ api/scripts/certification/get-cpf-import-status-from-xml.js
@@ -1,19 +1,14 @@
-import { logger } from '../../src/shared/infrastructure/utils/logger.js';
-
-import { disconnect } from '../../db/knex-database-connection.js';
 import * as url from 'url';
-
-import { getCPfImportstatusFromXml } from '../../src/certification/session/domain/usecases/get-cpf-import-status-from-xml.js';
+import { logger } from '../../src/shared/infrastructure/utils/logger.js';
+import { disconnect } from '../../db/knex-database-connection.js';
+import { usecases } from '../../src/certification/shared/domain/usecases/index.js';
 
 const modulePath = url.fileURLToPath(import.meta.url);
 const isLaunchedFromCommandLine = process.argv[1] === modulePath;
 
-async function main(path) {
-  logger.info("Récupération des résultats d'import CPF...");
-  const filePath = path || process.argv[2];
-
-  await getCPfImportstatusFromXml({ filePath });
-
+async function main() {
+  logger.info('Integrating CPF processing receipts files...');
+  await usecases.integrateCpfProccessingReceipts();
   logger.info(`Done! ...`);
 }
 
@@ -22,7 +17,7 @@ async function main(path) {
     try {
       await main();
     } catch (error) {
-      console.error(error);
+      console.error('An error occured', error);
       process.exitCode = 1;
     } finally {
       await disconnect();
