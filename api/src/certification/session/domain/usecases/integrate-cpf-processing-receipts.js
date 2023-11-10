a7b77196ff8afc47ef1b3c7efa4351a0f215f819
Steph0 <Steph0@users.noreply.github.com>
2023-11-10 16:48:09 +0100

    :sparkles: api: failure on one CPF receipt does not block other receipts integrations

--- api/src/certification/session/domain/usecases/integrate-cpf-processing-receipts.js
+++ api/src/certification/session/domain/usecases/integrate-cpf-processing-receipts.js
@@ -4,21 +4,31 @@ import { CONCURRENCY_HEAVY_OPERATIONS } from '../../../../../lib/infrastructure/
 
 const integrateCpfProccessingReceipts = async function ({ cpfReceiptsStorage, cpfCertificationResultRepository }) {
   logger.info('Starting CPF receipts integration from external storage');
+
   const cpfReceipts = await cpfReceiptsStorage.findAll();
-
   for (const cpfReceipt of cpfReceipts) {
-    logger.debug('Treatment of CPF receipt %o', cpfReceipt);
-    const cpfInfos = await cpfReceiptsStorage.getCpfInfosByReceipt({ cpfReceipt });
+    try {
+      logger.debug('Integration of CPF receipt %o', cpfReceipt);
+      await _updateCertificationCourses({ cpfReceipt, cpfReceiptsStorage, cpfCertificationResultRepository });
 
-    await bluebird.map(cpfInfos, async (cpfInfos) => cpfCertificationResultRepository.updateCpfInfos({ cpfInfos }), {
-      concurrency: CONCURRENCY_HEAVY_OPERATIONS,
-    });
-    logger.debug('CPF receipt integrated %o', cpfReceipt);
-
-    logger.debug('Delete CPF receipt %o', cpfReceipt);
-    await cpfReceiptsStorage.deleteReceipt({ cpfReceipt });
+      logger.debug('Delete CPF receipt %o', cpfReceipt);
+      await cpfReceiptsStorage.deleteReceipt({ cpfReceipt });
+    } catch (error) {
+      // Do not block next CPF receipt integration
+      logger.error('An error occured while integrating a CPF receipt', error);
+    }
   }
+
   logger.info('%d CPF receipts integrated', cpfReceipts.length);
 };
 
 export { integrateCpfProccessingReceipts };
+
+async function _updateCertificationCourses({ cpfReceipt, cpfReceiptsStorage, cpfCertificationResultRepository }) {
+  const cpfInfos = await cpfReceiptsStorage.getCpfInfosByReceipt({ cpfReceipt });
+
+  await bluebird.map(cpfInfos, async (cpfInfos) => cpfCertificationResultRepository.updateCpfInfos({ cpfInfos }), {
+    concurrency: CONCURRENCY_HEAVY_OPERATIONS,
+  });
+  logger.debug('CPF receipt integrated %o', cpfReceipt);
+}
