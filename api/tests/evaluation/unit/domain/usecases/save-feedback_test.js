0963ba43416b7243969b32e4e73143db815b6c1d
Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
2023-11-10 14:48:58 +0100

    refactor(api): move save-feedback usecase

--- api/tests/evaluation/unit/domain/usecases/save-feedback_test.js
+++ api/tests/evaluation/unit/domain/usecases/save-feedback_test.js
@@ -1,5 +1,5 @@
-import { expect, sinon } from '../../../test-helper.js';
-import { saveFeedback } from '../../../../lib/domain/usecases/save-feedback.js';
+import { expect, sinon } from '../../../../test-helper.js';
+import { saveFeedback } from '../../../../../src/evaluation/domain/usecases/save-feedback.js';
 
 describe('Unit | UseCase | save-feedback', function () {
   describe('when there is a feedback to save', function () {
