bba5bc7e73723f096821b60295353e56a6f8c705
Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
2023-11-10 14:45:09 +0100

    refactor(api): move feedback repository

--- api/tests/evaluation/integration/infrastructure/repositories/feedback-repository_test.js
+++ api/tests/evaluation/integration/infrastructure/repositories/feedback-repository_test.js
@@ -1,6 +1,6 @@
-import { expect } from '../../../test-helper.js';
-import { save } from '../../../../lib/infrastructure/repositories/feedback-repository.js';
-import { Feedback } from '../../../../lib/domain/models/Feedback.js';
+import { expect } from '../../../../test-helper.js';
+import { save } from '../../../../../src/evaluation/infrastructure/repositories/feedback-repository.js';
+import { Feedback } from '../../../../../src/evaluation/domain/models/Feedback.js';
 
 describe('Integration | Repository | Feedback', function () {
   describe('save', function () {
