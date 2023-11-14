6eafc2b4790c983e6c48de61d7fa422ec93dac3e
P-Jeremy <jemyplu@gmail.com>
2023-11-14 14:56:21 +0100

    :sparkles: admin: hide neutralization tab if certification is v3

--- admin/tests/acceptance/authenticated/certifications/certification/certification_test.js
+++ admin/tests/acceptance/authenticated/certifications/certification/certification_test.js
@@ -39,6 +39,37 @@ module('Acceptance | Route | routes/authenticated/certifications/certification',
       // then
       assert.dom(screen.queryByRole('link', { name: 'Profil' })).doesNotExist();
     });
+
+    test('it should not display the neutralization tab', async function (assert) {
+      // given
+      this.server.create('user', { id: 888 });
+
+      const certification = this.server.create('certification', {
+        id: 123,
+        firstName: 'Bora Horza',
+        lastName: 'Gobuchul',
+        birthdate: '1987-07-24',
+        birthplace: 'Sorpen',
+        userId: 888,
+        sex: 'M',
+        isCancelled: false,
+        birthCountry: 'JAPON',
+        birthInseeCode: '99217',
+        birthPostalCode: null,
+        competencesWithMark: [],
+        listChallengesAndAnswers: [],
+        createdAt: new Date('2020-01-01'),
+        version: 3,
+      });
+
+      await authenticateAdminMemberWithRole({ isSuperAdmin: true })(server);
+
+      // when
+      const screen = await visit(`/certifications/${certification.id}`);
+
+      // then
+      assert.dom(screen.queryByRole('link', { name: 'Neutralisation' })).doesNotExist();
+    });
   });
 
   module('when certification is V2', function () {
@@ -71,6 +102,37 @@ module('Acceptance | Route | routes/authenticated/certifications/certification',
 
       // then
       assert.dom(screen.getByRole('link', { name: 'Profil' })).exists();
+    });
+
+    test('it should display the neutralization tab', async function (assert) {
+      // given
+      this.server.create('user', { id: 888 });
+
+      const certification = this.server.create('certification', {
+        id: 123,
+        firstName: 'Bora Horza',
+        lastName: 'Gobuchul',
+        birthdate: '1987-07-24',
+        birthplace: 'Sorpen',
+        userId: 888,
+        sex: 'M',
+        isCancelled: false,
+        birthCountry: 'JAPON',
+        birthInseeCode: '99217',
+        birthPostalCode: null,
+        competencesWithMark: [],
+        listChallengesAndAnswers: [],
+        createdAt: new Date('2020-01-01'),
+        version: 2,
+      });
+
+      await authenticateAdminMemberWithRole({ isSuperAdmin: true })(server);
+
+      // when
+      const screen = await visit(`/certifications/${certification.id}`);
+
+      // then
+      assert.dom(screen.getByRole('link', { name: 'Neutralisation' })).exists();
     });
   });
 });
