832e91b07c2eb920f2caa818b4c7476a9bba2f24
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-11-15 15:00:00 +0100

    feat(api): add ElementAnswer model
    Co-authored-by: Vincent Hardouin <vincent.hardouin@pix.fr>
    Co-authored-by: Rébecca Kaci <rebecca.kaci@pix.fr>
    Co-authored-by: Dimitri Lahaye <dimitri.lahaye@pix.fr>

---
+++ api/src/devcomp/domain/models/ElementAnswer.js
@@ -0,0 +1,18 @@
+import { assertNotNullOrUndefined } from '../../../shared/domain/models/asserts.js';
+
+class ElementAnswer {
+  constructor({ elementId, userResponseValue, correction }) {
+    assertNotNullOrUndefined(elementId, "L'id de l'élément est obligatoire pour une réponse d'élément");
+    assertNotNullOrUndefined(
+      userResponseValue,
+      "La réponse de l'utilisateur est obligatoire pour une réponse d'élément",
+    );
+    assertNotNullOrUndefined(correction, "La correction est obligatoire pour une réponse d'élément");
+
+    this.elementId = elementId;
+    this.userResponseValue = userResponseValue;
+    this.correction = correction;
+  }
+}
+
+export { ElementAnswer };
---
+++ api/tests/devcomp/unit/domain/models/ElementAnswer_test.js
@@ -0,0 +1,41 @@
+import { ElementAnswer } from '../../../../../src/devcomp/domain/models/ElementAnswer.js';
+import { expect } from '../../../../test-helper.js';
+
+describe('Unit | Devcomp | Models | ElementAnswer', function () {
+  describe('#constructor', function () {
+    it('should create an element answer and keep attributes', function () {
+      // given
+      const elementId = 1;
+      const userResponseValue = 'foo';
+      const correction = Symbol('correction');
+
+      // when
+      const answer = new ElementAnswer({ elementId, userResponseValue: 'foo', correction });
+
+      // then
+      expect(answer.elementId).to.equal(elementId);
+      expect(answer.userResponseValue).to.equal(userResponseValue);
+      expect(answer.correction).to.equal(correction);
+    });
+
+    describe('An element answer without element id', function () {
+      it('should throw an error', function () {
+        expect(() => new ElementAnswer({})).to.throw("L'id de l'élément est obligatoire pour une réponse d'élément");
+      });
+    });
+    describe('An element answer without user response value', function () {
+      it('should throw an error', function () {
+        expect(() => new ElementAnswer({ elementId: 'elementId' })).to.throw(
+          "La réponse de l'utilisateur est obligatoire pour une réponse d'élément",
+        );
+      });
+    });
+    describe('An element answer without correction', function () {
+      it('should throw an error', function () {
+        expect(() => new ElementAnswer({ elementId: 'elementId', userResponseValue: 'userResponseValue' })).to.throw(
+          "La correction est obligatoire pour une réponse d'élément",
+        );
+      });
+    });
+  });
+});
