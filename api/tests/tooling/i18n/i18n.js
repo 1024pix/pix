d6cacce039067b99dc4885dd38b927f3c6e94e49
Steph0 <Steph0@users.noreply.github.com>
2023-11-15 12:18:11 +0100

    :bug: api: isolate i18n instances between tests
    Co-authored-by: Andreia Pena <andreia.pena@pix.fr>
    Co-authored-by: Antoine Ceol <antoine.ceol@gmail.com>

--- api/tests/tooling/i18n/i18n.js
+++ api/tests/tooling/i18n/i18n.js
@@ -1,20 +1,16 @@
 import path from 'path';
-import i18n from 'i18n';
+import { options } from '../../../lib/infrastructure/plugins/i18n.js';
+import { I18n } from 'i18n';
 import * as url from 'url';
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 function getI18n() {
   const directory = path.resolve(path.join(__dirname, '../../../translations'));
+
+  const i18n = new I18n();
   i18n.configure({
-    locales: ['fr', 'en'],
-    defaultLocale: 'fr',
+    ...options,
     directory,
-    objectNotation: true,
-    updateFiles: false,
-    mustacheConfig: {
-      tags: ['{', '}'],
-      disable: false,
-    },
   });
   return i18n;
 }
