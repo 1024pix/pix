79a3232eb3a03d32a16f2bfd5f2c0049a36d4263
Jérémie Picard <jeremie.picard@pix.fr>
2023-11-15 10:50:58 +0100

    ♻️ admin: rename method to fix typo

--- admin/app/controllers/authenticated/certifications/certification/informations.js
+++ admin/app/controllers/authenticated/certifications/certification/informations.js
@@ -144,7 +144,7 @@ export default class CertificationInformationsController extends Controller {
   async onCandidateResultsSave() {
     this.displayConfirm = false;
     try {
-      await this.saveAssessementResult();
+      await this.saveAssessmentResult();
 
       this.notifications.success('Modifications enregistrées');
       this.editingCandidateResults = false;
@@ -160,7 +160,7 @@ export default class CertificationInformationsController extends Controller {
     }
   }
 
-  saveAssessementResult() {
+  saveAssessmentResult() {
     return this.certification.save({ adapterOptions: { updateMarks: true } });
   }
 
