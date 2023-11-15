cc044bd1372035767351d1953c7cd7634a143bcf
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-11-15 18:54:54 +0100

    feat(mon-pix): improve qcu component to handle submit answer and display correction feedback
    Co-authored-by: Dimitri Lahaye <dimitri.lahayed@pix.fr>
    Co-authored-by: Miranda Lin-Guignard <miranda.lin-guignard@pix.fr>
    Co-authored-by: Yann Bertrand <yann.bertrand@pix.fr>

--- mon-pix/app/pods/components/module/qcu/component.js
+++ mon-pix/app/pods/components/module/qcu/component.js
@@ -1,10 +1,22 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class ModuleQcu extends Component {
+  @tracked selectedAnswerId = null;
+  @tracked correctionResponse = null;
+
   @action
-  radioClicked(value) {
-    // eslint-disable-next-line no-console
-    console.info(value);
+  radioClicked(proposalId) {
+    this.selectedAnswerId = proposalId;
+  }
+
+  @action
+  async submitAnswer(event) {
+    event.preventDefault();
+    const elementId = this.args.qcu.id;
+    const answerData = { elementId, userResponse: [this.selectedAnswerId] };
+    const elementAnswer = await this.args.submitAnswer(answerData);
+    this.correctionResponse = elementAnswer.correction;
   }
 }
--- mon-pix/tests/integration/components/module/qcu_test.js
+++ mon-pix/tests/integration/components/module/qcu_test.js
@@ -2,7 +2,8 @@ import { module, test } from 'qunit';
 import { render } from '@1024pix/ember-testing-library';
 import { hbs } from 'ember-cli-htmlbars';
 import setupIntlRenderingTest from '../../../helpers/setup-intl-rendering';
-import { findAll } from '@ember/test-helpers';
+import { click, findAll } from '@ember/test-helpers';
+import sinon from 'sinon';
 
 module('Integration | Component | Module | QCU', function (hooks) {
   setupIntlRenderingTest(hooks);
@@ -18,10 +19,10 @@ module('Integration | Component | Module | QCU', function (hooks) {
       ],
       type: 'qcus',
     });
+    const givenSubmitAnswerStub = sinon.stub();
     this.set('qcu', qcuElement);
-
-    //  when
-    const screen = await render(hbs`<Module::Qcu @qcu={{this.qcu}} />`);
+    this.set('submitAnswer', givenSubmitAnswerStub);
+    const screen = await render(hbs`<Module::Qcu @qcu={{this.qcu}} @submitAnswer={{this.submitAnswer}} />`);
 
     // then
     assert.ok(screen);
@@ -29,8 +30,103 @@ module('Integration | Component | Module | QCU', function (hooks) {
     assert.strictEqual(findAll('.element-qcu-header__direction').length, 1);
     assert.ok(screen.getByText('Instruction'));
     assert.ok(screen.getByText(this.intl.t('pages.modulix.qcu.direction')));
+
     assert.strictEqual(screen.getAllByRole('radio').length, qcuElement.proposals.length);
     assert.ok(screen.getByLabelText('radio1'));
     assert.ok(screen.getByLabelText('radio2'));
+
+    const verifyButton = screen.queryByRole('button', { name: 'Vérifier' });
+    assert.dom(verifyButton).exists();
+    assert.true(verifyButton.disabled);
+  });
+
+  test('should call action when verify button is clicked', async function (assert) {
+    // given
+    const store = this.owner.lookup('service:store');
+    const answeredProposal = { id: 'a1b2c3d4-e5f6-7g8h-9i0j-k1l2m3n4o5p6', content: 'radio1' };
+    const qcuElement = store.createRecord('qcu', {
+      id: 'qcu-id-1',
+      instruction: 'Instruction',
+      proposals: [answeredProposal, { id: 'b5a4c3d2-e1f6-7g8h-9i0j-k1l2m3n4o5p6', content: 'radio2' }],
+      type: 'qcus',
+    });
+    this.set('qcu', qcuElement);
+    const elementId = qcuElement.id;
+    const answerId = answeredProposal.id;
+    const givenSubmitAnswerSpy = sinon.spy();
+    this.set('submitAnswer', givenSubmitAnswerSpy);
+    const screen = await render(hbs`<Module::Qcu @qcu={{this.qcu}} @submitAnswer={{this.submitAnswer}} />`);
+    const verifyButton = screen.queryByRole('button', { name: 'Vérifier' });
+
+    // when
+    await click(screen.getByLabelText(answeredProposal.content));
+    await click(verifyButton);
+
+    // then
+    assert.false(verifyButton.disabled);
+    sinon.assert.calledWith(givenSubmitAnswerSpy, { elementId, answerId });
+  });
+
+  test('should display an ok feedback when exists', async function (assert) {
+    // given
+    const store = this.owner.lookup('service:store');
+    const qcuElement = store.createRecord('qcu', {
+      instruction: 'Instruction',
+      proposals: [
+        { id: 'a1b2c3d4-e5f6-7g8h-9i0j-k1l2m3n4o5p6', content: 'radio1' },
+        { id: 'b5a4c3d2-e1f6-7g8h-9i0j-k1l2m3n4o5p6', content: 'radio2' },
+      ],
+      type: 'qcus',
+    });
+    this.set('qcu', qcuElement);
+    const correctionResponse = store.createRecord('correction-response', {
+      feedback: 'Good job!',
+      globalResult: 'ok',
+      solutionId: 'solutionId',
+    });
+
+    const screen = await renderQcuWithCorrectionResponse.call(this, correctionResponse);
+
+    assertsWhenCorrectionResponseHasBeenGiven(assert, screen);
+    assert.strictEqual(findAll('.element-qcu__feedback--ok').length, 1);
+  });
+
+  test('should display a ko feedback when exists', async function (assert) {
+    // given
+    const store = this.owner.lookup('service:store');
+    const qcuElement = store.createRecord('qcu', {
+      instruction: 'Instruction',
+      proposals: [
+        { id: 'a1b2c3d4-e5f6-7g8h-9i0j-k1l2m3n4o5p6', content: 'radio1' },
+        { id: 'b5a4c3d2-e1f6-7g8h-9i0j-k1l2m3n4o5p6', content: 'radio2' },
+      ],
+      type: 'qcus',
+    });
+    this.set('qcu', qcuElement);
+    const correctionResponse = store.createRecord('correction-response', {
+      feedback: 'Good job!',
+      globalResult: 'ko',
+      solutionId: 'solutionId',
+    });
+    const screen = await renderQcuWithCorrectionResponse.call(this, correctionResponse);
+
+    assertsWhenCorrectionResponseHasBeenGiven(assert, screen);
+    assert.strictEqual(findAll('.element-qcu__feedback--ko').length, 1);
   });
 });
+
+async function renderQcuWithCorrectionResponse(correctionResponse) {
+  this.set('submitAnswer', () => {});
+  this.set('correctionResponse', correctionResponse);
+
+  return await render(
+    hbs`<Module::Qcu @qcu={{this.qcu}} @submitAnswer={{this.submitAnswer}} @correctionResponse={{this.correctionResponse}} />`,
+  );
+}
+
+function assertsWhenCorrectionResponseHasBeenGiven(assert, screen) {
+  assert.ok(screen.getByText('Good job!'));
+  assert.ok(screen.getByLabelText('radio1').disabled);
+  assert.ok(screen.getByLabelText('radio2').disabled);
+  assert.dom(screen.queryByRole('button', { name: 'Vérifier' })).doesNotExist();
+}
