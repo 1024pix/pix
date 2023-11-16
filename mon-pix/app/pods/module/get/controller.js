e314262b0340a7cc435db9f3d48f7dd8209e4f81
Dimitri Lahaye <contact@dimitrilahaye.net>
2023-11-16 17:43:04 +0100

    feat(mon-pix): add Matomo event sending on submit answer
    Co-authored-by: Vincent Hardouin <vincent.hardouin@pix.fr>
    Co-authored-by: Miranda Lin-Guignard <miranda.lin-guignard@pix.fr>
    Co-authored-by: Dimitri Lahaye <dimitri.lahaye@pix.fr>

--- mon-pix/app/pods/module/get/controller.js
+++ mon-pix/app/pods/module/get/controller.js
@@ -5,6 +5,7 @@ import { tracked } from '@glimmer/tracking';
 
 export default class GetController extends Controller {
   @service store;
+  @service metrics;
 
   @tracked correctionResponse = {};
 
@@ -23,5 +24,12 @@ export default class GetController extends Controller {
       ...this.correctionResponse,
     };
     this.correctionResponse[answerData.elementId] = await elementAnswer.correction;
+
+    this.metrics.add({
+      event: 'custom-event',
+      'pix-event-category': 'Modulix',
+      'pix-event-action': `Passage du module : ${this.model.id}`,
+      'pix-event-name': `Click sur le bouton vérifier de l'élément : ${answerData.elementId}`,
+    });
   }
 }
