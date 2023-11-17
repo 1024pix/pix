afc6c55c7dddd0f2d356e17cca4f62b0c765cc52
Alexandre COIN <alexandre@mbpdealexandre-1.home>
2023-11-17 16:30:42 +0100

    :sparkles: certif: display issue report choices depending on challenge typology

--- certif/app/components/session-supervising/handle-live-alert-modal.js
+++ certif/app/components/session-supervising/handle-live-alert-modal.js
@@ -1,20 +1,12 @@
 import Component from '@glimmer/component';
-import {
-  inChallengeIssueReportSubCategories,
-  subcategoryToCode,
-  subcategoryToLabel,
-} from 'pix-certif/models/certification-issue-report';
+import { subcategoryToCode, subcategoryToLabel } from 'pix-certif/models/certification-issue-report';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { certificationIssueReportSubcategories } from '../../models/certification-issue-report';
+import orderBy from 'lodash/orderBy';
 
 export default class HandleLiveAlertModal extends Component {
   @tracked issueReportReason = null;
-
-  reportOptions = inChallengeIssueReportSubCategories.map((subCategory) => ({
-    subCategory,
-    code: subcategoryToCode[subCategory],
-    label: subcategoryToLabel[subCategory],
-  }));
 
   @action
   setIssueReportReason(event) {
@@ -48,5 +40,45 @@ export default class HandleLiveAlertModal extends Component {
 
   _clearIssueReportReason() {
     this.issueReportReason = null;
+  }
+
+  get issueReportOptions() {
+    const { hasEmbed, hasImage, isFocus, hasAttachment } = this.args.liveAlert;
+
+    const availableSubcategories = [
+      certificationIssueReportSubcategories.WEBSITE_UNAVAILABLE,
+      certificationIssueReportSubcategories.WEBSITE_BLOCKED,
+      certificationIssueReportSubcategories.EXTRA_TIME_EXCEEDED,
+      certificationIssueReportSubcategories.SOFTWARE_NOT_WORKING,
+      certificationIssueReportSubcategories.SKIP_ON_OOPS,
+      certificationIssueReportSubcategories.ACCESSIBILITY_ISSUE,
+    ];
+
+    if (hasEmbed) {
+      availableSubcategories.push(certificationIssueReportSubcategories.EMBED_NOT_WORKING);
+    }
+
+    if (hasImage) {
+      availableSubcategories.push(certificationIssueReportSubcategories.IMAGE_NOT_DISPLAYING);
+    }
+
+    if (isFocus) {
+      availableSubcategories.push(certificationIssueReportSubcategories.UNINTENTIONAL_FOCUS_OUT);
+    }
+
+    if (hasAttachment) {
+      availableSubcategories.push(certificationIssueReportSubcategories.FILE_NOT_OPENING);
+    }
+
+    const options = orderBy(
+      availableSubcategories.map((subCategory) => ({
+        subCategory,
+        code: subcategoryToCode[subCategory],
+        label: subcategoryToLabel[subCategory],
+      })),
+      ({ code }) => +code.substring(1),
+    );
+
+    return options;
   }
 }
