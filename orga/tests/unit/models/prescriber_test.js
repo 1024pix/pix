38db07c523351c21dde1094cc4993ccc84b3a739
Yvonnick Frin <frin.yvonnick@gmail.com>
2023-11-17 14:30:54 +0100

    tests?(orga) remove run from prescriber model tests

--- orga/tests/unit/models/prescriber_test.js
+++ orga/tests/unit/models/prescriber_test.js
@@ -1,6 +1,5 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
-import { run } from '@ember/runloop';
 
 module('Unit | Model | prescriber', function (hooks) {
   setupTest(hooks);
@@ -9,11 +8,11 @@ module('Unit | Model | prescriber', function (hooks) {
     test('it should return false if the current organization has not GAR as identity provider', function (assert) {
       // given
       const store = this.owner.lookup('service:store');
-      const organization = run(() => store.createRecord('organization', { name: 'Willow school' }));
-      const userOrgaSettings = run(() => store.createRecord('userOrgaSetting', { organization }));
-      const membership = run(() => store.createRecord('membership', { organizationRole: 'MEMBER', organization }));
+      const organization = store.createRecord('organization', { name: 'Willow school' });
+      const userOrgaSettings = store.createRecord('userOrgaSetting', { organization });
+      const membership = store.createRecord('membership', { organizationRole: 'MEMBER', organization });
       const memberships = [membership];
-      const model = run(() => store.createRecord('prescriber', { memberships, userOrgaSettings }));
+      const model = store.createRecord('prescriber', { memberships, userOrgaSettings });
 
       // when / then
       assert.false(model.hasCurrentOrganizationWithGARAsIdentityProvider);
@@ -22,16 +21,14 @@ module('Unit | Model | prescriber', function (hooks) {
     test('it should return true if the current organization has GAR as identity provider', function (assert) {
       // given
       const store = this.owner.lookup('service:store');
-      const organization = run(() =>
-        store.createRecord('organization', {
-          name: 'Willow school',
-          identityProviderForCampaigns: 'GAR',
-        }),
-      );
-      const userOrgaSettings = run(() => store.createRecord('userOrgaSetting', { organization }));
-      const membership = run(() => store.createRecord('membership', { organizationRole: 'MEMBER', organization }));
+      const organization = store.createRecord('organization', {
+        name: 'Willow school',
+        identityProviderForCampaigns: 'GAR',
+      });
+      const userOrgaSettings = store.createRecord('userOrgaSetting', { organization });
+      const membership = store.createRecord('membership', { organizationRole: 'MEMBER', organization });
       const memberships = [membership];
-      const model = run(() => store.createRecord('prescriber', { memberships, userOrgaSettings }));
+      const model = store.createRecord('prescriber', { memberships, userOrgaSettings });
 
       // when / then
       assert.true(model.hasCurrentOrganizationWithGARAsIdentityProvider);
@@ -42,11 +39,11 @@ module('Unit | Model | prescriber', function (hooks) {
     test('it should return true if prescriber is ADMIN of the current organization', function (assert) {
       // given
       const store = this.owner.lookup('service:store');
-      const organization = run(() => store.createRecord('organization', { name: 'Willow school' }));
-      const userOrgaSettings = run(() => store.createRecord('userOrgaSetting', { organization }));
-      const membership = run(() => store.createRecord('membership', { organizationRole: 'ADMIN', organization }));
+      const organization = store.createRecord('organization', { name: 'Willow school' });
+      const userOrgaSettings = store.createRecord('userOrgaSetting', { organization });
+      const membership = store.createRecord('membership', { organizationRole: 'ADMIN', organization });
       const memberships = [membership];
-      const model = run(() => store.createRecord('prescriber', { memberships, userOrgaSettings }));
+      const model = store.createRecord('prescriber', { memberships, userOrgaSettings });
 
       // when / then
       assert.true(model.isAdminOfTheCurrentOrganization);
@@ -55,11 +52,11 @@ module('Unit | Model | prescriber', function (hooks) {
     test('it should return false if prescriber is MEMBER of the current organization', function (assert) {
       // given
       const store = this.owner.lookup('service:store');
-      const organization = run(() => store.createRecord('organization', { name: 'Willow school' }));
-      const userOrgaSettings = run(() => store.createRecord('userOrgaSetting', { organization }));
-      const membership = run(() => store.createRecord('membership', { organizationRole: 'MEMBER', organization }));
+      const organization = store.createRecord('organization', { name: 'Willow school' });
+      const userOrgaSettings = store.createRecord('userOrgaSetting', { organization });
+      const membership = store.createRecord('membership', { organizationRole: 'MEMBER', organization });
       const memberships = [membership];
-      const model = run(() => store.createRecord('prescriber', { memberships, userOrgaSettings }));
+      const model = store.createRecord('prescriber', { memberships, userOrgaSettings });
 
       // when / then
       assert.false(model.isAdminOfTheCurrentOrganization);
@@ -68,15 +65,13 @@ module('Unit | Model | prescriber', function (hooks) {
     test('it should return false if prescriber is MEMBER of the current organization and ADMIN in another', function (assert) {
       // given
       const store = this.owner.lookup('service:store');
-      const currentOrganization = run(() => store.createRecord('organization', { id: 7, name: 'Willow school' }));
-      const otherOrganization = run(() => store.createRecord('organization', { id: 123, name: 'Tanglewood school' }));
-      const userOrgaSettings = run(() => store.createRecord('userOrgaSetting', { organization: currentOrganization }));
-      const membership = run(() =>
-        store.createRecord('membership', { organizationRole: 'MEMBER', currentOrganization }),
-      );
-      const membership2 = run(() => store.createRecord('membership', { organizationRole: 'ADMIN', otherOrganization }));
+      const currentOrganization = store.createRecord('organization', { id: 7, name: 'Willow school' });
+      const otherOrganization = store.createRecord('organization', { id: 123, name: 'Tanglewood school' });
+      const userOrgaSettings = store.createRecord('userOrgaSetting', { organization: currentOrganization });
+      const membership = store.createRecord('membership', { organizationRole: 'MEMBER', currentOrganization });
+      const membership2 = store.createRecord('membership', { organizationRole: 'ADMIN', otherOrganization });
       const memberships = [membership, membership2];
-      const model = run(() => store.createRecord('prescriber', { memberships, userOrgaSettings }));
+      const model = store.createRecord('prescriber', { memberships, userOrgaSettings });
 
       // when / then
       assert.false(model.isAdminOfTheCurrentOrganization);
@@ -87,12 +82,9 @@ module('Unit | Model | prescriber', function (hooks) {
     test('it returns true when feature is enabled', function (assert) {
       // given
       const store = this.owner.lookup('service:store');
-      const model = run(() =>
-        store.createRecord('prescriber', {
-          features: { ['MULTIPLE_SENDING_ASSESSMENT']: true },
-        }),
-      );
-
+      const model = store.createRecord('prescriber', {
+        features: { ['MULTIPLE_SENDING_ASSESSMENT']: true },
+      });
       // when
       const enableMultipleSendingAssessment = model.enableMultipleSendingAssessment;
 
@@ -103,12 +95,9 @@ module('Unit | Model | prescriber', function (hooks) {
     test('it returns false when feature is disabled', function (assert) {
       // given
       const store = this.owner.lookup('service:store');
-      const model = run(() =>
-        store.createRecord('prescriber', {
-          features: { ['MULTIPLE_SENDING_ASSESSMENT']: false },
-        }),
-      );
-
+      const model = store.createRecord('prescriber', {
+        features: { ['MULTIPLE_SENDING_ASSESSMENT']: false },
+      });
       // when
       const enableMultipleSendingAssessment = model.enableMultipleSendingAssessment;
 
@@ -121,12 +110,9 @@ module('Unit | Model | prescriber', function (hooks) {
     test('it returns true when feature is enabled', function (assert) {
       // given
       const store = this.owner.lookup('service:store');
-      const model = run(() =>
-        store.createRecord('prescriber', {
-          features: { ['COMPUTE_ORGANIZATION_LEARNER_CERTIFICABILITY']: true },
-        }),
-      );
-
+      const model = store.createRecord('prescriber', {
+        features: { ['COMPUTE_ORGANIZATION_LEARNER_CERTIFICABILITY']: true },
+      });
       // when
       const computeOrganizationLearnerCertificability = model.computeOrganizationLearnerCertificability;
 
@@ -137,12 +123,9 @@ module('Unit | Model | prescriber', function (hooks) {
     test('it returns false when feature is disabled', function (assert) {
       // given
       const store = this.owner.lookup('service:store');
-      const model = run(() =>
-        store.createRecord('prescriber', {
-          features: { ['COMPUTE_ORGANIZATION_LEARNER_CERTIFICABILITY']: false },
-        }),
-      );
-
+      const model = store.createRecord('prescriber', {
+        features: { ['COMPUTE_ORGANIZATION_LEARNER_CERTIFICABILITY']: false },
+      });
       // when
       const computeOrganizationLearnerCertificability = model.computeOrganizationLearnerCertificability;
 
