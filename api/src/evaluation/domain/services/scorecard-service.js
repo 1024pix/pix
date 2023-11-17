b610474433ef2ad87b97062ed2965deb7bef4e25
Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
2023-11-17 11:26:11 +0100

    refactor(api): move scorecard service

--- api/src/evaluation/domain/services/scorecard-service.js
+++ api/src/evaluation/domain/services/scorecard-service.js
@@ -1,7 +1,7 @@
-import { Assessment } from '../../../src/shared/domain/models/Assessment.js';
-import { CompetenceEvaluation } from '../../../src/evaluation/domain/models/CompetenceEvaluation.js';
-import { KnowledgeElement } from '../models/KnowledgeElement.js';
-import { Scorecard } from '../../../src/evaluation/domain/models/Scorecard.js';
+import { Assessment } from '../../../shared/domain/models/Assessment.js';
+import { CompetenceEvaluation } from '../models/CompetenceEvaluation.js';
+import { KnowledgeElement } from '../../../../lib/domain/models/KnowledgeElement.js';
+import { Scorecard } from '../models/Scorecard.js';
 import _ from 'lodash';
 
 async function computeScorecard({
