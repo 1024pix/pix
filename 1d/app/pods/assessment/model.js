d48d22e4523d75fd6b6b220a74f7c50f120cc4ff
Yaelle YAMA <yaelle.marsiac@pix.fr>
2023-11-20 16:21:48 +0100

    tech(1d): use only usefull assessment attributes and clean model

--- 1d/app/pods/assessment/model.js
+++ 1d/app/pods/assessment/model.js
@@ -1,17 +1,7 @@
-import Model, { attr, hasMany } from '@ember-data/model';
+import Model, { attr } from '@ember-data/model';
 
 export default class Assessment extends Model {
-  // attributes
-  @attr('string') certificationNumber;
-  @attr('string') codeCampaign;
   @attr('string') state;
-  @attr('string') title;
-  @attr('string') type;
-  @attr('string') lastQuestionState;
-  @attr('string') method;
-
-  // references
-  @attr('string') competenceId;
   @attr('string') missionId;
-  @hasMany('activity', { async: true, inverse: 'assessment' }) activities;
+  @attr('string') organizationLearnerId;
 }
