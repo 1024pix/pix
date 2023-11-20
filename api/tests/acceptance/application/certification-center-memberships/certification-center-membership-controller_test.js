afe81ffc14e41704ca0c2020b4d1963306e6d5d3
Eric Lim <eric.lim@zenika.com>
2023-11-20 15:34:15 +0100

    feat(api): call disableCertificationCenterMembership usecase with extracted userId from request in controller

--- api/tests/acceptance/application/certification-center-memberships/certification-center-membership-controller_test.js
+++ api/tests/acceptance/application/certification-center-memberships/certification-center-membership-controller_test.js
@@ -210,5 +210,92 @@ describe('Acceptance | API | Certification Center Membership', function () {
         });
       });
     });
+
+    describe('DELETE /api/admin/certification-center-memberships/{id}', function () {
+      let certificationCenter;
+      let certificationCenterMembership;
+      let user;
+
+      beforeEach(async function () {
+        certificationCenter = databaseBuilder.factory.buildCertificationCenter();
+        user = databaseBuilder.factory.buildUser();
+        certificationCenterMembership = databaseBuilder.factory.buildCertificationCenterMembership({
+          certificationCenterId: certificationCenter.id,
+          userId: user.id,
+        });
+        await databaseBuilder.commit();
+      });
+
+      context('Success cases', function () {
+        context('when parameters are valid', function () {
+          it('returns a 204 HTTP status code', async function () {
+            const pixAgentWithCertifRole = databaseBuilder.factory.buildUser.withRole({ role: 'CERTIF' });
+
+            const request = {
+              method: 'DELETE',
+              url: `/api/admin/certification-center-memberships/${certificationCenterMembership.id}`,
+              headers: {
+                authorization: generateValidRequestAuthorizationHeader(pixAgentWithCertifRole.id),
+              },
+            };
+
+            await databaseBuilder.commit();
+
+            // when
+            const { statusCode } = await server.inject(request);
+
+            // then
+            expect(statusCode).to.equal(204);
+          });
+        });
+      });
+
+      context('Error cases', function () {
+        context('when user does not have a valid role', function () {
+          it('returns a 403 HTTP status code', async function () {
+            const userWithoutRole = databaseBuilder.factory.buildUser();
+
+            const request = {
+              method: 'DELETE',
+              url: `/api/admin/certification-center-memberships/${certificationCenterMembership.id}`,
+              headers: {
+                authorization: generateValidRequestAuthorizationHeader(userWithoutRole.id),
+              },
+            };
+
+            await databaseBuilder.commit();
+
+            // when
+            const { statusCode } = await server.inject(request);
+
+            // then
+            expect(statusCode).to.equal(403);
+          });
+        });
+
+        context('when certification-center-membership does not exist', function () {
+          it('returns a 400 HTTP status code', async function () {
+            const pixAgentWithAdminRole = databaseBuilder.factory.buildUser.withRole({ role: 'SUPER_ADMIN' });
+            const nonexistentCertificationCenterMembershipId = 'TEST';
+
+            const request = {
+              method: 'DELETE',
+              url: `/api/admin/certification-center-memberships/${nonexistentCertificationCenterMembershipId}`,
+              headers: {
+                authorization: generateValidRequestAuthorizationHeader(pixAgentWithAdminRole.id),
+              },
+            };
+
+            await databaseBuilder.commit();
+
+            // when
+            const { statusCode } = await server.inject(request);
+
+            // then
+            expect(statusCode).to.equal(400);
+          });
+        });
+      });
+    });
   });
 });
