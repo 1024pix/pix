4da8cf2f786994af704adcf143a025f8cafbb01b
Yvonnick Frin <frin.yvonnick@gmail.com>
2023-11-20 15:41:03 +0100

    feat(api): add feature to manage places on PixOrga

--- api/db/seeds/data/common/feature-builder.js
+++ api/db/seeds/data/common/feature-builder.js
@@ -2,6 +2,7 @@ import { ORGANIZATION_FEATURE } from '../../../../lib/domain/constants.js';
 import {
   FEATURE_MULTIPLE_SENDING_ASSESSMENT_ID,
   FEATURE_COMPUTE_ORGANIZATION_LEARNER_CERTIFICABILITY_ID,
+  FEATURE_PLACES_MANAGEMENT_ID,
 } from './constants.js';
 
 const featuresBuilder = async function ({ databaseBuilder }) {
@@ -14,6 +15,11 @@ const featuresBuilder = async function ({ databaseBuilder }) {
     id: FEATURE_COMPUTE_ORGANIZATION_LEARNER_CERTIFICABILITY_ID,
     key: ORGANIZATION_FEATURE.COMPUTE_ORGANIZATION_LEARNER_CERTIFICABILITY.key,
     description: ORGANIZATION_FEATURE.COMPUTE_ORGANIZATION_LEARNER_CERTIFICABILITY.description,
+  });
+  databaseBuilder.factory.buildFeature({
+    id: FEATURE_PLACES_MANAGEMENT_ID,
+    key: ORGANIZATION_FEATURE.PLACES_MANAGEMENT.key,
+    description: ORGANIZATION_FEATURE.PLACES_MANAGEMENT.description,
   });
   await databaseBuilder.commit();
 };
