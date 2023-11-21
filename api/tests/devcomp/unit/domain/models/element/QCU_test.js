6544222a0553280ce61e3ef7ed8a79354effef7b
Dimitri Lahaye <contact@dimitrilahaye.net>
2023-11-21 11:35:01 +0100

    refactor(api): implement QCUForAnswerVerification domain model

--- api/tests/devcomp/unit/domain/models/element/QCU_test.js
+++ api/tests/devcomp/unit/domain/models/element/QCU_test.js
@@ -1,6 +1,5 @@
 import { expect } from '../../../../../test-helper.js';
 import { QCU } from '../../../../../../src/devcomp/domain/models/element/QCU.js';
-import { Feedbacks } from '../../../../../../src/devcomp/domain/models/Feedbacks.js';
 
 describe('Unit | Devcomp | Domain | Models | QCU', function () {
   describe('#constructor', function () {
@@ -8,7 +7,6 @@ describe('Unit | Devcomp | Domain | Models | QCU', function () {
       // Given
       const proposal1 = Symbol('proposal1');
       const proposal2 = Symbol('proposal2');
-      const solution = Symbol('solution');
 
       // When
       const qcu = new QCU({
@@ -16,7 +14,6 @@ describe('Unit | Devcomp | Domain | Models | QCU', function () {
         instruction: 'instruction',
         locales: ['fr-FR'],
         proposals: [proposal1, proposal2],
-        solution,
       });
 
       // Then
@@ -24,34 +21,7 @@ describe('Unit | Devcomp | Domain | Models | QCU', function () {
       expect(qcu.instruction).equal('instruction');
       expect(qcu.locales).deep.equal(['fr-FR']);
       expect(qcu.proposals).deep.equal([proposal1, proposal2]);
-      expect(qcu.solution).deep.equal(solution);
       expect(qcu.feedbacks).to.be.undefined;
-    });
-
-    it('should instanciate a QCU with feedbacks if given', function () {
-      // Given
-      const proposal1 = Symbol('proposal1');
-      const proposal2 = Symbol('proposal2');
-      const feedbacks = { valid: 'valid', invalid: 'invalid' };
-      const solution = Symbol('solution');
-
-      // When
-      const qcu = new QCU({
-        id: '123',
-        instruction: 'instruction',
-        locales: ['fr-FR'],
-        proposals: [proposal1, proposal2],
-        feedbacks,
-        solution,
-      });
-
-      // Then
-      expect(qcu.id).equal('123');
-      expect(qcu.instruction).equal('instruction');
-      expect(qcu.locales).deep.equal(['fr-FR']);
-      expect(qcu.proposals).deep.equal([proposal1, proposal2]);
-      expect(qcu.solution).deep.equal(solution);
-      expect(qcu.feedbacks).to.be.instanceof(Feedbacks);
     });
   });
 
@@ -79,14 +49,6 @@ describe('Unit | Devcomp | Domain | Models | QCU', function () {
     it('should throw an error', function () {
       expect(() => new QCU({ id: '123', instruction: 'toto', proposals: 'toto' })).to.throw(
         'Les propositions doivent apparaître dans une liste',
-      );
-    });
-  });
-
-  describe('A QCU without a solution', function () {
-    it('should throw an error', function () {
-      expect(() => new QCU({ id: '123', instruction: 'toto', proposals: [Symbol('proposal1')] })).to.throw(
-        'La solution est obligatoire pour un QCU',
       );
     });
   });
