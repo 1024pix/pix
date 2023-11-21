4a5d74c1e66082e08dd6c2595921ddb2ea1233fc
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-11-21 14:52:23 +0100

    sr(api): move find-campaign-participation-trainings in devcomp

--- api/src/devcomp/domain/usecases/index.js
+++ api/src/devcomp/domain/usecases/index.js
@@ -4,6 +4,7 @@ import { importNamedExportsFromDirectory } from '../../../shared/infrastructure/
 import { injectDependencies } from '../../../shared/infrastructure/utils/dependency-injection.js';
 import { repositories } from '../../infrastructure/repositories/index.js';
 import * as campaignRepository from '../../../../lib/infrastructure/repositories/campaign-repository.js';
+import * as campaignParticipationRepository from '../../../../lib/infrastructure/repositories/campaign-participation-repository.js';
 import * as knowledgeElementRepository from '../../../../lib/infrastructure/repositories/knowledge-element-repository.js';
 
 const path = dirname(fileURLToPath(import.meta.url));
@@ -11,6 +12,7 @@ const path = dirname(fileURLToPath(import.meta.url));
 const dependencies = {
   ...repositories,
   campaignRepository,
+  campaignParticipationRepository,
   knowledgeElementRepository,
 };
 
