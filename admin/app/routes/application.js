6af12c30311c7b9fffd8e8e8a55b4cda64dc6751
Andreia Pena <andreia.pena@pix.fr>
2023-11-21 17:29:00 +0100

    ✨ admin: add audience parameter to oidc providers call
    Co-authored-by: LEGO Technix <109212476+lego-technix@users.noreply.github.com>
    Co-authored-by: Clément Latzarus <clement.latzarus@pix.fr>

--- admin/app/routes/application.js
+++ admin/app/routes/application.js
@@ -8,12 +8,15 @@ export default class ApplicationRoute extends Route {
   @service intl;
   @service currentUser;
   @service featureToggles;
+  @service oidcIdentityProviders;
 
   async beforeModel() {
     await this.session.setup();
     this.intl.setLocale([defaultLocale]);
 
     await this.featureToggles.load();
+
+    await this.oidcIdentityProviders.loadReadyIdentityProviders();
 
     return this._loadCurrentUser();
   }
