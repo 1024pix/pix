72b60adebed7b35c67f14a49e98f36ceab6cad8d
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-11-21 15:25:40 +0100

    sr(api): move trainings-trigger-serializer in devcomp

--- api/lib/application/trainings/training-controller.js
+++ api/lib/application/trainings/training-controller.js
@@ -1,6 +1,6 @@
 import * as trainingSerializer from '../../../src/devcomp/infrastructure/serializers/jsonapi/training-serializer.js';
 import * as trainingSummarySerializer from '../../../src/devcomp/infrastructure/serializers/jsonapi/training-summary-serializer.js';
-import * as trainingTriggerSerializer from '../../infrastructure/serializers/jsonapi/training-trigger-serializer.js';
+import * as trainingTriggerSerializer from '../../../src/devcomp/infrastructure/serializers/jsonapi/training-trigger-serializer.js';
 import * as targetProfileSummaryForAdminSerializer from '../../infrastructure/serializers/jsonapi/target-profile-summary-for-admin-serializer.js';
 import { usecases } from '../../domain/usecases/index.js';
 import { DomainTransaction } from '../../infrastructure/DomainTransaction.js';
