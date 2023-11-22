846209c32bafec7ec05bf3ec5820abf3e9ff2e8c
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-11-22 14:15:33 +0100

    feat(api): remove devcomp usecases import in lib
    All devcomp use cases should be imported in specific controller with `import { usescases as devcompUsecases }`

--- api/lib/domain/usecases/index.js
+++ api/lib/domain/usecases/index.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line eslint-comments/disable-enable-pair
 /* eslint-disable import/no-restricted-paths */
+
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 
@@ -413,10 +414,6 @@ const usecasesWithoutInjectedDependencies = {
   ...(await importNamedExportsFromDirectory({ path: join(path, './sessions-mass-import') })),
   ...(await importNamedExportsFromDirectory({ path: join(path, './stages') })),
   ...(await importNamedExportsFromDirectory({ path: join(path, './target-profile-management') })),
-  ...(await importNamedExportsFromDirectory({
-    path: join(path, '../../../src/devcomp/domain/usecases'),
-    ignoredFileNames: ['index.js'],
-  })),
   findPaginatedFilteredTargetProfileOrganizations,
   getCampaignDetailsManagement,
 };
