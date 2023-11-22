aa90c1246f26d9a6a20fe64fd06fda44597971fe
P-Jeremy <jemyplu@gmail.com>
2023-11-22 16:12:52 +0100

    :recycle: api: move deleteSession usecase to src

--- api/src/certification/session/domain/usecases/delete-session.js
+++ api/src/certification/session/domain/usecases/delete-session.js
@@ -1,5 +1,14 @@
-import { SessionStartedDeletionError } from '../errors.js';
+import { SessionStartedDeletionError } from '../../../../../lib/domain/errors.js';
 
+/**
+ * @typedef {import ('../../../shared/domain/usecases/index.js').dependencies} deps
+ */
+
+/**
+ * @param {Object} params
+ * @param {deps['sessionRepository']} params.sessionRepository
+ * @param {deps['certificationCourseRepository']} params.certificationCourseRepository
+ */
 const deleteSession = async function ({ sessionId, sessionRepository, certificationCourseRepository }) {
   if (await _isSessionStarted(certificationCourseRepository, sessionId)) {
     throw new SessionStartedDeletionError();
