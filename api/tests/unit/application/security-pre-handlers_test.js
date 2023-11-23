5b6c265b7169001a9ebb42486716b7e7b5ca51fc
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2023-11-23 16:58:29 +0100

    feat(api): add new security pre handler

--- api/tests/unit/application/security-pre-handlers_test.js
+++ api/tests/unit/application/security-pre-handlers_test.js
@@ -1215,6 +1215,57 @@ describe('Unit | Application | SecurityPreHandlers', function () {
     });
   });
 
+  describe('#checkAuthorizationToAccessCampaign', function () {
+    context('Successful case', function () {
+      it('should authorize access to resource when the user is authenticated and is admin in organization and owner of the campaign', async function () {
+        // given
+        const request = {
+          auth: { credentials: { accessToken: 'valid.access.token', userId: Symbol('UserId') } },
+          params: { id: Symbol('campaignId') },
+        };
+
+        const checkAuthorizationToAccessCampaignUsecaseStub = {
+          execute: sinon.stub(),
+        };
+        checkAuthorizationToAccessCampaignUsecaseStub.execute
+          .withArgs({ campaignId: request.params.id, userId: request.auth.credentials.userId })
+          .resolves(true);
+        // when
+        const response = await securityPreHandlers.checkAuthorizationToAccessCampaign(request, hFake, {
+          checkAuthorizationToAccessCampaignUsecase: checkAuthorizationToAccessCampaignUsecaseStub,
+        });
+
+        // then
+        expect(response.source).to.be.true;
+      });
+    });
+
+    context('Error cases', function () {
+      it('should forbid resource access when user is member but does not own the campaign', async function () {
+        // given
+        const request = {
+          auth: { credentials: { accessToken: 'valid.access.token', userId: Symbol('UserId') } },
+          params: { id: Symbol('campaignId') },
+        };
+
+        const checkAuthorizationToAccessCampaignUsecaseStub = {
+          execute: sinon.stub(),
+        };
+        checkAuthorizationToAccessCampaignUsecaseStub.execute
+          .withArgs({ campaignId: request.params.id, userId: request.auth.userId })
+          .resolves(false);
+        // when
+        const response = await securityPreHandlers.checkAuthorizationToAccessCampaign(request, hFake, {
+          checkAuthorizationToAccessCampaignUsecase: checkAuthorizationToAccessCampaignUsecaseStub,
+        });
+
+        // then
+        expect(response.statusCode).to.equal(403);
+        expect(response.isTakeOver).to.be.true;
+      });
+    });
+  });
+
   describe('#checkUserIsMemberOfCertificationCenterSessionFromCertificationCourseId', function () {
     context('Successful case', function () {
       it('should authorize access to resource when the user is a member of the organization center', async function () {
