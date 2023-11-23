6d3a6187b57f87bf25873c8410f72056463c8c24
Yannick François <yaf@elsif.fr>
2023-11-23 17:00:20 +0100

    :recycle: admin: random name for certif center

--- admin/mirage/factories/certification-center.js
+++ admin/mirage/factories/certification-center.js
@@ -2,7 +2,7 @@ import { Factory } from 'miragejs';
 
 export default Factory.extend({
   name() {
-    return 'Centre des certifs';
+    return `Centre des certifs ${Math.random().toString(36).slice(2, 8)}`;
   },
 
   type() {
