2ebed41935983377ac30b0e2a78c7fb84dfe9807
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-11-23 18:54:57 +0100

    feat(mon-pix): add metrics for continue button

--- mon-pix/app/pods/components/module/grain/component.js
+++ mon-pix/app/pods/components/module/grain/component.js
@@ -1,11 +1,26 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class ModuleGrain extends Component {
+  @service metrics;
+
   get shouldDisplayContinueButton() {
     return this.args.canDisplayContinueButton && this.allElementsAreAnswered;
   }
 
   get allElementsAreAnswered() {
     return this.args.grain.allElementsAreAnswered;
+  }
+
+  @action
+  async continueAction() {
+    await this.args.continueAction();
+    this.metrics.add({
+      event: 'custom-event',
+      'pix-event-category': 'Modulix',
+      'pix-event-action': `Passage du module : ${this.args.grain.module.id}`,
+      'pix-event-name': `Click sur le bouton continuer du grain : ${this.args.grain.id}`,
+    });
   }
 }
