ea3c9721f93bc9951356a8276c51279fc9eb8771
Andreia Pena <andreia.pena@pix.fr>
2023-11-24 11:12:50 +0100

    🚚 api: first route : move get session certification reports usecase
    to src directory
    
    Co-authored-by: Jérémie Picard <jeremie.picard@pix.fr>

--- api/tests/certification/course/unit/domain/usecases/get-session-certification-reports_test.js
+++ api/tests/certification/course/unit/domain/usecases/get-session-certification-reports_test.js
@@ -1,5 +1,5 @@
-import { expect, sinon } from '../../../test-helper.js';
-import { getSessionCertificationReports } from '../../../../lib/domain/usecases/get-session-certification-reports.js';
+import { expect, sinon } from '../../../../../test-helper.js';
+import { getSessionCertificationReports } from '../../../../../../src/certification/course/domain/usecases/get-session-certification-reports.js';
 
 describe('Unit | Domain | Use Cases | get-session-certification-reports', function () {
   it('should return the certification reports', async function () {
