110e3ac945599a8922c117b4a9fdc6a3b637fd05
Jérémie Picard <jeremie.picard@pix.fr>
2023-11-24 10:40:02 +0100

    🚚api: move certificationCourseUpdateError from lib to src
    Co-authored-by: Andreia Pena <andreia.pena@pix.fr>

---
+++ api/tests/certification/shared/unit/domain/errors_test.js
@@ -0,0 +1,8 @@
+import { expect } from '../../../../test-helper.js';
+import * as errors from '../../../../../src/certification/shared/domain/errors.js';
+
+describe('Certification | shared | Unit | Domain | Errors', function () {
+  it('should export a CertificationCourseUpdateError', function () {
+    expect(errors.CertificationCourseUpdateError).to.exist;
+  });
+});
