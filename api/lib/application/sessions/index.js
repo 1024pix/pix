9dbbfd223732324ab39d0e59c32a60b116435808
Andreia Pena <andreia.pena@pix.fr>
2023-11-27 11:38:23 +0100

    🚚 api: second route: move session finalization route
     from lib to src
    
     Co-authored-by: Jérémie Picard <jeremie.picard@pix.fr>

--- api/lib/application/sessions/index.js
+++ api/lib/application/sessions/index.js
@@ -151,41 +151,6 @@ const register = async function (server) {
       },
     },
     {
-      method: 'PUT',
-      path: '/api/sessions/{id}/finalization',
-      config: {
-        validate: {
-          options: {
-            allowUnknown: true,
-          },
-          params: Joi.object({
-            id: identifiersType.sessionId,
-          }),
-          payload: Joi.object({
-            data: {
-              attributes: {
-                'examiner-global-comment': Joi.string().optional().allow(null).allow('').max(500),
-                'has-incident': Joi.boolean().required(),
-                'has-joining-issue': Joi.boolean().required(),
-              },
-            },
-          }),
-        },
-        pre: [
-          {
-            method: authorization.verifySessionAuthorization,
-            assign: 'authorizationCheck',
-          },
-        ],
-        handler: sessionController.finalize,
-        tags: ['api', 'sessions'],
-        notes: [
-          '- **Cette route est restreinte aux utilisateurs authentifiés**\n' +
-            '- Elle permet de finaliser une session de certification afin de la signaler comme terminée',
-        ],
-      },
-    },
-    {
       method: 'POST',
       path: '/api/sessions/{id}/certification-candidates/import',
       config: {
