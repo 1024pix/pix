f8533544528f6066ac86103c5a26a161a8c2a4ad
dlahaye <dimitrilahaye.dev@gmail.com>
2023-11-27 12:00:00 +0100

    refactor(mon-pix): remove `isAnswerable` field from Element front model

--- mon-pix/tests/unit/models/modules/grain_test.js
+++ mon-pix/tests/unit/models/modules/grain_test.js
@@ -9,8 +9,8 @@ module('Unit | Model | Grain', function (hooks) {
       test('should return only answerable elements', function (assert) {
         // given
         const store = this.owner.lookup('service:store');
-        const qcu = store.createRecord('qcu', { type: 'qcus' });
-        const text = store.createRecord('text', { type: 'texts' });
+        const qcu = store.createRecord('qcu', { type: 'qcus', isAnswerable: true });
+        const text = store.createRecord('text', { type: 'texts', isAnswerable: false });
         const grain = store.createRecord('grain', {
           elements: [qcu, text],
         });
@@ -67,7 +67,7 @@ module('Unit | Model | Grain', function (hooks) {
       test('should return false', function (assert) {
         // given
         const store = this.owner.lookup('service:store');
-        const qcu = store.createRecord('qcu', { type: 'qcus' });
+        const qcu = store.createRecord('qcu', { type: 'qcus', isAnswerable: true });
         const grain = store.createRecord('grain', {
           elements: [qcu],
         });
