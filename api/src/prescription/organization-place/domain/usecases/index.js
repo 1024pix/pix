1891f8479ce6b1616206f63464bb7f9ce108fdf0
Alexandre Monney <alexandre.monney@pix.fr>
2023-11-29 14:10:11 +0100

    feat(api): create get organization places statistics usecase

--- api/src/prescription/organization-place/domain/usecases/index.js
+++ api/src/prescription/organization-place/domain/usecases/index.js
@@ -4,11 +4,13 @@ import { dirname, join } from 'node:path';
 import * as organizationPlacesLotRepository from '../../infrastructure/repositories/organization-places-lot-repository.js';
 import * as organizationPlacesCapacityRepository from '../../infrastructure/repositories/organization-places-capacity-repository.js';
 import * as organizationRepository from '../../../../../lib/infrastructure/repositories/organization-repository.js';
+import * as organizationLearnerRepository from '../../../../../lib/infrastructure/repositories/organization-learner-repository.js';
 
 import { importNamedExportsFromDirectory } from '../../../../shared/infrastructure/utils/import-named-exports-from-directory.js';
 import { injectDependencies } from '../../../../shared/infrastructure/utils/dependency-injection.js';
 
 const dependencies = {
+  organizationLearnerRepository,
   organizationPlacesLotRepository,
   organizationRepository,
   organizationPlacesCapacityRepository,
