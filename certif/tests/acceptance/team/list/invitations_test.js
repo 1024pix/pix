ca22cc8fca76adc1f1b1fbbfd724ecb37bd7c366
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-11-29 17:10:57 +0100

    feat(certif): handle cancel invitation action

--- certif/tests/acceptance/team/list/invitations_test.js
+++ certif/tests/acceptance/team/list/invitations_test.js
@@ -1,13 +1,14 @@
 import { module, test } from 'qunit';
-import { currentURL, visit } from '@ember/test-helpers';
+import { currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
-import setupIntl from '../../helpers/setup-intl';
+import setupIntl from '../../../helpers/setup-intl';
 
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import {
   authenticateSession,
   createCertificationPointOfContactWithTermsOfServiceAccepted,
-} from '../../helpers/test-init';
+} from '../../../helpers/test-init';
+import { clickByName, visit } from '@1024pix/ember-testing-library';
 
 module('Acceptance | Team | Invitations', function (hooks) {
   setupApplicationTest(hooks);
@@ -29,35 +30,54 @@ module('Acceptance | Team | Invitations', function (hooks) {
     });
   });
 
-  module('When user is admin', function () {
-    test('displays invitations list', async function (assert) {
-      // given
+  module('When user is admin', function (hooks) {
+    hooks.beforeEach(async function () {
       const certificationPointOfContact = createCertificationPointOfContactWithTermsOfServiceAccepted(
         undefined,
         'Centre de certification du pix',
         false,
         'ADMIN',
       );
+      await authenticateSession(certificationPointOfContact.id);
+    });
 
-      server.create('certification-center-invitation', {
+    test('displays invitations list', async function (assert) {
+      // given
+      this.server.create('certification-center-invitation', {
         certificationCenterId: 123,
         email: 'camile.onette@example.net',
         updatedAt: new Date('2023-09-20'),
       });
-
-      server.create('certification-center-invitation', {
+      this.server.create('certification-center-invitation', {
         certificationCenterId: 123,
         email: 'camile.onette@example.net',
         updatedAt: new Date('2023-09-19'),
       });
-
-      await authenticateSession(certificationPointOfContact.id);
 
       // when
       await visit('/equipe/invitations');
 
       // then
       assert.strictEqual(currentURL(), '/equipe/invitations');
+    });
+
+    module('when user clicks on cancel invitation button', function () {
+      test('removes the invitation', async function (assert) {
+        // given
+        this.server.create('certification-center-invitation', {
+          certificationCenterId: 1,
+          email: 'daisy.draté@example.net',
+          updatedAt: new Date('2023-11-30'),
+        });
+
+        const screen = await visit('/equipe/invitations');
+
+        // when
+        await clickByName(this.intl.t('pages.team-invitations.actions.cancel-invitation'));
+
+        // then
+        assert.dom(screen.queryByText('daisy.draté@example.net')).doesNotExist();
+      });
     });
   });
 });
