35fdb49fad8ea5dcd14f2ec9c4891c5dfb6d056f
dlahaye <dimitrilahaye.dev@gmail.com>
2023-12-04 16:00:38 +0100

    feat(api): add QROCMs to referential
    * update datasource test

--- api/tests/devcomp/unit/infrastructure/datasources/learning-content/module-datasource_test.js
+++ api/tests/devcomp/unit/infrastructure/datasources/learning-content/module-datasource_test.js
@@ -42,6 +42,59 @@ describe('Unit | Infrastructure | Datasources | Learning Content | ModuleDatasou
           solution: uuidSchema,
         }).required();
 
+        const blockInputSchema = Joi.object({
+          input: Joi.string().required(),
+          type: Joi.string().valid('input').required(),
+          inputType: Joi.string().valid('text', 'number').required(),
+          size: Joi.string().valid('small', 'medium', 'large').required(),
+          display: Joi.string().valid('inline', 'block').required(),
+          placeholder: Joi.string().allow('').required(),
+          ariaLabel: Joi.string().required(),
+          defaultValue: Joi.string().allow('').required(),
+          tolerances: Joi.array()
+            .unique()
+            .items(Joi.string().valid('t1', 't2', 't3'))
+            .required(),
+          solutions: Joi.array().items(Joi.string().required()).required(),
+        }).required();
+
+        const blockSelectSchema = Joi.object({
+          input: Joi.string().required(),
+          type: Joi.string().valid('select').required(),
+          display: Joi.string().valid('inline', 'block').required(),
+          placeholder: Joi.string().allow('').required(),
+          ariaLabel: Joi.string().required(),
+          defaultValue: Joi.string().allow('').required(),
+          tolerances: Joi.array().empty().required(),
+          options: Joi.array()
+            .items(
+              Joi.object({
+                id: Joi.string().required(),
+                content: Joi.string().required(),
+              }),
+            )
+            .required(),
+          solutions: Joi.array().items(Joi.string().required()).required(),
+        }).required();
+
+        const blockTextSchema = Joi.object({
+          type: Joi.string().valid('text').required(),
+          content: Joi.string().required(),
+        }).required();
+
+        const qrocmElementSchema = Joi.object({
+          id: uuidSchema,
+          type: Joi.string().valid('qrocm').required(),
+          instruction: Joi.string().required(),
+          proposals: Joi.array()
+            .items(Joi.alternatives().try(blockTextSchema, blockInputSchema, blockSelectSchema))
+            .required(),
+          feedbacks: Joi.object({
+            valid: Joi.string().required(),
+            invalid: Joi.string().required(),
+          }).required(),
+        });
+
         moduleSchema = Joi.object({
           id: uuidSchema,
           slug: Joi.string()
@@ -55,7 +108,9 @@ describe('Unit | Infrastructure | Datasources | Learning Content | ModuleDatasou
                 type: Joi.string().valid('lesson', 'activity').required(),
                 title: Joi.string().required(),
                 elements: Joi.array()
-                  .items(Joi.alternatives().try(textElementSchema, imageElementSchema, qcuElementSchema))
+                  .items(
+                    Joi.alternatives().try(textElementSchema, imageElementSchema, qcuElementSchema, qrocmElementSchema),
+                  )
                   .required(),
               }).required(),
             )
