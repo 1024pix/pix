c13fe6868161bc0914fcdc305bb6d29096bdcfee
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-12-04 11:08:13 +0100

    feat(mon-pix): add tracking on alternative text button

--- mon-pix/app/pods/components/module/image/component.js
+++ mon-pix/app/pods/components/module/image/component.js
@@ -1,13 +1,21 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 export default class ModuleImage extends Component {
   @tracked modalIsOpen = false;
+  @service metrics;
 
   @action
   showModal() {
     this.modalIsOpen = true;
+    this.metrics.add({
+      event: 'custom-event',
+      'pix-event-category': 'Modulix',
+      'pix-event-action': `Afficher l’alternative textuelle : ${this.args.image.id}`,
+      'pix-event-name': `Click sur le bouton alternative textuelle : ${this.args.image.id}`,
+    });
   }
 
   @action
