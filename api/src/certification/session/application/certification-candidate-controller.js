0bd04e5f0b7ed1ba7d708cef462d93857c6c2d4d
Steph0 <Steph0@users.noreply.github.com>
2023-12-04 12:18:41 +0100

    :recycle: api: uniformization of controller methods calls
    * 'delete' keyword is reserved in 'strict' mode, so uniformization of other methods names

--- api/src/certification/session/application/certification-candidate-controller.js
+++ api/src/certification/session/application/certification-candidate-controller.js
@@ -1,7 +1,7 @@
 import * as certificationCandidateSerializer from '../../shared/infrastructure/serializers/jsonapi/certification-candidate-serializer.js';
 import { usecases } from '../../shared/domain/usecases/index.js';
 
-const add = async function (request, h, dependencies = { certificationCandidateSerializer }) {
+const addCandidate = async function (request, h, dependencies = { certificationCandidateSerializer }) {
   const sessionId = request.params.id;
   const certificationCandidate = await dependencies.certificationCandidateSerializer.deserialize(request.payload);
   const complementaryCertification = request.payload.data.attributes['complementary-certification'] ?? null;
@@ -14,7 +14,7 @@ const add = async function (request, h, dependencies = { certificationCandidateS
   return h.response(dependencies.certificationCandidateSerializer.serialize(addedCertificationCandidate)).created();
 };
 
-const get = async function (request, h, dependencies = { certificationCandidateSerializer }) {
+const getCandidate = async function (request, h, dependencies = { certificationCandidateSerializer }) {
   const sessionId = request.params.id;
 
   const certificationCandidates = await usecases.getSessionCertificationCandidates({ sessionId });
@@ -30,8 +30,8 @@ const deleteCandidate = async function (request) {
 };
 
 const certificationCandidateController = {
-  add,
-  get,
+  addCandidate,
+  getCandidate,
   deleteCandidate,
 };
 export { certificationCandidateController };
