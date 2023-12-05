69006bcd5f5330c9268a630b616685e6a5de60a9
Yaelle YAMA <yaelle.marsiac@pix.fr>
2023-12-05 17:09:27 +0100

    tech(api-1d): improve error management when the mission is not found

--- api/tests/school/integration/infrastructure/repositories/mission-repository_test.js
+++ api/tests/school/integration/infrastructure/repositories/mission-repository_test.js
@@ -1,30 +1,47 @@
-import { expect, mockLearningContent } from '../../../../test-helper.js';
+import { expect, mockLearningContent, catchErr } from '../../../../test-helper.js';
 import * as missionRepository from '../../../../../src/school/infrastructure/repositories/mission-repository.js';
 import { Mission } from '../../../../../src/school/domain/models/Mission.js';
-
+import { NotFoundError } from '../../../../../lib/domain/errors.js';
 describe('Integration | Repository | mission-repository', function () {
   describe('#get', function () {
-    it('should return the correct mission with FR default language', async function () {
-      // given
-      const expectedMission = new Mission({
-        id: 'recThematic1',
-        name: 'nameThemaFR1',
+    context('when there is a mission for the given id', function () {
+      it('should return the correct mission with FR default language', async function () {
+        // given
+        const expectedMission = new Mission({
+          id: 'recThematic1',
+          name: 'nameThemaFR1',
+        });
+
+        mockLearningContent({
+          thematics: [
+            {
+              id: 'recThematic1',
+              name_i18n: { fr: 'nameThemaFR1' },
+            },
+          ],
+        });
+
+        // when
+        const mission = await missionRepository.get('recThematic1');
+
+        // then
+        expect(mission).to.deep.equal(expectedMission);
       });
+    });
+    context('when there is no mission for the given id', function () {
+      it('should return the not found error', async function () {
+        // given
+        mockLearningContent({
+          thematics: [],
+        });
+        const missionId = 'recThematic1';
+        // when
+        const error = await catchErr(missionRepository.get)(missionId);
 
-      mockLearningContent({
-        thematics: [
-          {
-            id: 'recThematic1',
-            name_i18n: { fr: 'nameThemaFR1' },
-          },
-        ],
+        // then
+        expect(error).to.be.instanceOf(NotFoundError);
+        expect(error.message).to.deep.equal(`Il n'existe pas de mission ayant pour id ${missionId}`);
       });
-
-      // when
-      const mission = await missionRepository.get('recThematic1');
-
-      // then
-      expect(mission).to.deep.equal(expectedMission);
     });
   });
 });
