428236c41c82dd060095c40a54365d998ddae23a
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-12-06 17:24:31 +0100

    feat(api): add resendCertificationCenterInvitation route
    Co-authored-by: Eric Lim <eric.lim@zenika.com>

--- api/lib/application/certification-center-invitations/index.js
+++ api/lib/application/certification-center-invitations/index.js
@@ -105,6 +105,29 @@ const register = async function (server) {
         tags: ['api', 'certification-center-invitation'],
       },
     },
+    {
+      method: 'PATCH',
+      path: '/api/certification-center-invitations/{certificationCenterInvitationId}',
+      config: {
+        handler: certificationCenterInvitationController.resendCertificationCenterInvitation,
+        pre: [
+          {
+            method: securityPreHandlers.checkUserIsAdminOfCertificationCenterWithCertificationCenterInvitationId,
+            assign: 'isAdminOfCertificationCenter',
+          },
+        ],
+        validate: {
+          params: Joi.object({
+            certificationCenterInvitationId: identifiersType.certificationCenterInvitationId.required(),
+          }),
+        },
+        notes: [
+          '- **Cette route est restreinte aux utilisateurs appartenant à un centre de certification**\n',
+          "- Cette route permet de renvoyer une invitation en attente selon un **id d'invitation**",
+        ],
+        tags: ['api', 'certification-center-invitation'],
+      },
+    },
   ]);
 };
 
