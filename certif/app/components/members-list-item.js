d0470bbac09d1d441f62a0591c12184a32e2986b
Eric Lim <eric.lim@zenika.com>
2023-12-08 15:57:53 +0100

    feat(certif): update member role feature
    Co-authored-by: Ismael Gorissen <ismael.gorissen@gmail.com>
    Co-authored-by: LEGO Technix <109212476+lego-technix@users.noreply.github.com>
    Co-authored-by: Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>

---
+++ certif/app/components/members-list-item.js
@@ -0,0 +1,79 @@
+import { service } from '@ember/service';
+import { action } from '@ember/object';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+
+const ARIA_LABEL_MEMBER_TRANSLATION = 'pages.team.members.actions.select-role.options.member';
+const ARIA_LABEL_ADMIN_TRANSLATION = 'pages.team.members.actions.select-role.options.admin';
+
+export default class MembersListItem extends Component {
+  @service currentUser;
+  @service notifications;
+  @service intl;
+  @service session;
+  @tracked isEditionMode = false;
+
+  roleOptions = [
+    {
+      value: 'ADMIN',
+      label: this.intl.t(ARIA_LABEL_ADMIN_TRANSLATION),
+      disabled: false,
+    },
+    {
+      value: 'MEMBER',
+      label: this.intl.t(ARIA_LABEL_MEMBER_TRANSLATION),
+      disabled: false,
+    },
+  ];
+
+  displayRoleByOrganizationRole = {
+    ADMIN: this.intl.t(ARIA_LABEL_ADMIN_TRANSLATION),
+    MEMBER: this.intl.t(ARIA_LABEL_MEMBER_TRANSLATION),
+  };
+
+  get shouldDisplayManagingColumn() {
+    return this.currentUser.isAdminOfCurrentCertificationCenter;
+  }
+
+  get shouldDisplayManageMemberRoleButton() {
+    if (this.isCurrentUserMembership) {
+      return false;
+    }
+    if (!this.currentUser.isAdminOfCurrentCertificationCenter) {
+      return false;
+    }
+    return !this.isEditionMode;
+  }
+
+  get isCurrentUserMembership() {
+    return this.currentUser.certificationPointOfContact.id === this.args.member.id;
+  }
+
+  @action
+  setRoleSelection(value) {
+    this.args.member.role = value;
+  }
+
+  @action
+  toggleEditionMode() {
+    this.isEditionMode = true;
+  }
+
+  @action
+  async updateMember(member) {
+    this.isEditionMode = false;
+    try {
+      await member.save();
+      this.notifications.success(this.intl.t('pages.team.members.notifications.change-member-role.success'));
+    } catch (e) {
+      member.rollbackAttributes();
+      this.notifications.error(this.intl.t('pages.team.members.notifications.change-member-role.error'));
+    }
+  }
+
+  @action
+  cancelUpdateRoleOfMember() {
+    this.isEditionMode = false;
+    this.args.member.rollbackAttributes();
+  }
+}
--- certif/tests/integration/components/members-list-item_test.js
+++ certif/tests/integration/components/members-list-item_test.js
@@ -1,46 +1,193 @@
 import { module, test } from 'qunit';
-import { render as renderScreen } from '@1024pix/ember-testing-library';
+import sinon from 'sinon';
+import { clickByName, render as renderScreen } from '@1024pix/ember-testing-library';
 import { hbs } from 'ember-cli-htmlbars';
 import setupIntlRenderingTest from '../../helpers/setup-intl-rendering';
+import { click } from '@ember/test-helpers';
 
 module('Integration | Component | MembersListItem', function (hooks) {
   setupIntlRenderingTest(hooks);
 
-  module('when member is a member', function () {
-    test('it shows member firstName, lastName and role', async function (assert) {
-      // given
-      const store = this.owner.lookup('service:store');
-      const memberMembership = store.createRecord('member', {
-        firstName: 'John',
-        lastName: 'Williams',
-        role: 'MEMBER',
+  let store;
+  let currentUser;
+
+  hooks.beforeEach(function () {
+    store = this.owner.lookup('service:store');
+    currentUser = this.owner.lookup('service:current-user');
+  });
+
+  hooks.afterEach(function () {
+    sinon.restore();
+  });
+
+  module('when current user has a member role', function (hooks) {
+    hooks.beforeEach(function () {
+      sinon.stub(currentUser, 'isAdminOfCurrentCertificationCenter').value(false);
+    });
+
+    module('when member has a member role', function () {
+      test('it shows member firstName, lastName and role', async function (assert) {
+        // given
+        const memberWithMemberRole = store.createRecord('member', {
+          firstName: 'John',
+          lastName: 'Williams',
+          role: 'MEMBER',
+        });
+
+        sinon.stub(currentUser, 'certificationPointOfContact').value({ id: memberWithMemberRole.id });
+        this.set('member', memberWithMemberRole);
+
+        // when
+        const screen = await renderScreen(hbs`<MembersListItem @member={{this.member}} />`);
+
+        // then
+        assert.dom(screen.getByRole('cell', { name: 'John' })).exists();
+        assert.dom(screen.getByRole('cell', { name: 'Williams' })).exists();
+        assert.dom(screen.getByRole('cell', { name: this.intl.t('pages.team.members.role.member') })).exists();
+        assert
+          .dom(screen.queryByRole('button', { name: this.intl.t('pages.team.members.role.member') }))
+          .doesNotExist();
       });
-      this.set('member', memberMembership);
+    });
 
-      // when
-      const screen = await renderScreen(hbs`<MembersListItem @member={{this.member}} />`);
+    module('when member has an admin role', function () {
+      test('it shows admin firstName, lastName and role', async function (assert) {
+        // given
+        const memberWithAdminRole = store.createRecord('member', {
+          firstName: 'Maria',
+          lastName: 'Carré',
+          role: 'ADMIN',
+        });
 
-      // then
-      assert.dom(screen.getByRole('cell', { name: 'John' })).exists();
-      assert.dom(screen.getByRole('cell', { name: 'Williams' })).exists();
-      assert.dom(screen.getByRole('cell', { name: this.intl.t('pages.team.members.role.member') })).exists();
+        sinon.stub(currentUser, 'certificationPointOfContact').value({ id: memberWithAdminRole.id });
+        this.set('member', memberWithAdminRole);
+
+        // when
+        const screen = await renderScreen(hbs`<MembersListItem @member={{this.member}} />`);
+
+        // then
+        assert.dom(screen.getByRole('cell', { name: 'Maria' })).exists();
+        assert.dom(screen.getByRole('cell', { name: 'Carré' })).exists();
+        assert.dom(screen.getByRole('cell', { name: this.intl.t('pages.team.members.role.admin') })).exists();
+        assert
+          .dom(screen.queryByRole('button', { name: this.intl.t('pages.team.members.role.member') }))
+          .doesNotExist();
+      });
     });
   });
 
-  module('when member is an administrator', function () {
-    test('it shows admin firstName, lastName and role', async function (assert) {
-      // given
-      const store = this.owner.lookup('service:store');
-      const adminMembership = store.createRecord('member', { firstName: 'Maria', lastName: 'Carré', role: 'ADMIN' });
-      this.set('member', adminMembership);
+  module('when current user has an admin role', function (hooks) {
+    hooks.beforeEach(function () {
+      sinon.stub(currentUser, 'isAdminOfCurrentCertificationCenter').value(true);
+    });
 
-      // when
-      const screen = await renderScreen(hbs`<MembersListItem @member={{this.member}} />`);
+    module('when not in edit mode', function () {
+      module('when member is not the connected user', function () {
+        test('it shows member firstName, lastName, role and manage button', async function (assert) {
+          // given
+          const memberWithMemberRole = store.createRecord('member', {
+            id: 123,
+            firstName: 'John',
+            lastName: 'Williams',
+            role: 'MEMBER',
+          });
 
-      // then
-      assert.dom(screen.getByRole('cell', { name: 'Maria' })).exists();
-      assert.dom(screen.getByRole('cell', { name: 'Carré' })).exists();
-      assert.dom(screen.getByRole('cell', { name: this.intl.t('pages.team.members.role.admin') })).exists();
+          sinon.stub(currentUser, 'certificationPointOfContact').value({ id: 1 });
+          this.set('member', memberWithMemberRole);
+
+          // when
+          const screen = await renderScreen(hbs`<MembersListItem @member={{this.member}} />`);
+
+          // then
+          assert.dom(screen.getByRole('cell', { name: 'John' })).exists();
+          assert.dom(screen.getByRole('cell', { name: 'Williams' })).exists();
+          assert.dom(screen.getByRole('cell', { name: this.intl.t('pages.team.members.role.member') })).exists();
+          assert.dom(screen.getByRole('button', { name: this.intl.t('pages.team.members.actions.manage') })).exists();
+        });
+      });
+
+      module('when member is the connected user', function () {
+        test('it shows member firstName, lastName, role but does not show manage button', async function (assert) {
+          // given
+          const memberWithMemberRole = store.createRecord('member', {
+            id: 123,
+            firstName: 'John',
+            lastName: 'Williams',
+            role: 'MEMBER',
+          });
+          this.set('member', memberWithMemberRole);
+          sinon.stub(currentUser, 'certificationPointOfContact').value({ id: memberWithMemberRole.id });
+
+          // when
+          const screen = await renderScreen(hbs`<MembersListItem @member={{this.member}} />`);
+
+          // then
+          assert.dom(screen.getByRole('cell', { name: 'John' })).exists();
+          assert.dom(screen.getByRole('cell', { name: 'Williams' })).exists();
+          assert.dom(screen.getByRole('cell', { name: this.intl.t('pages.team.members.role.member') })).exists();
+          assert
+            .dom(screen.queryByRole('button', { name: this.intl.t('pages.team.members.actions.manage') }))
+            .doesNotExist();
+        });
+      });
+    });
+
+    module('when mode edition is enabled', function (hooks) {
+      hooks.beforeEach(function () {
+        const memberWithMemberRole = store.createRecord('member', {
+          id: 123,
+          firstName: 'John',
+          lastName: 'Williams',
+          role: 'MEMBER',
+        });
+
+        sinon.stub(currentUser, 'certificationPointOfContact').value({ id: 1 });
+        this.set('member', memberWithMemberRole);
+      });
+
+      test('it shows role selection menu, save button and cancel button', async function (assert) {
+        // given
+        // when
+        const screen = await renderScreen(hbs`<MembersListItem @member={{this.member}} />`);
+        await clickByName(this.intl.t('pages.team.members.actions.manage'));
+        await clickByName(this.intl.t('pages.team.members.actions.edit-role'));
+
+        // then
+        assert.dom(screen.getByLabelText(this.intl.t('pages.team.members.actions.select-role.label'))).exists();
+        assert.dom(screen.getByRole('button', { name: this.intl.t('pages.team.members.actions.save') })).exists();
+        assert.dom(screen.getByRole('button', { name: this.intl.t('common.actions.cancel') })).exists();
+        assert
+          .dom(screen.queryByRole('button', { name: this.intl.t('pages.team.members.actions.manage') }))
+          .doesNotExist();
+        assert
+          .dom(
+            screen.queryByRole('button', {
+              name: this.intl.t('pages.team.members.actions.edit-role'),
+            }),
+          )
+          .doesNotExist();
+      });
+
+      module('when selecting a new role', function () {
+        test('it displays the selected role', async function (assert) {
+          // given
+          const screen = await renderScreen(hbs`<MembersListItem @member={{this.member}} />`);
+          await clickByName(this.intl.t('pages.team.members.actions.manage'));
+          await clickByName(this.intl.t('pages.team.members.actions.edit-role'));
+
+          // when
+          await clickByName(this.intl.t('pages.team.members.actions.select-role.label'));
+          await screen.findByRole('listbox');
+          await click(
+            screen.getByRole('option', { name: this.intl.t('pages.team.members.actions.select-role.options.admin') }),
+          );
+
+          // then
+          assert
+            .dom(screen.getByRole('button', { name: this.intl.t('pages.team.members.actions.select-role.label') }))
+            .containsText(this.intl.t('pages.team.members.actions.select-role.options.admin'));
+        });
+      });
     });
   });
 });
