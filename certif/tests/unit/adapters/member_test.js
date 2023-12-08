34f4036661ca598a82e34731481165f4163164fe
Eric Lim <eric.lim@zenika.com>
2023-12-08 15:55:21 +0100

    feat(certif): modify update url and payload serialization
    Co-authored-by: Ismael Gorissen <ismael.gorissen@gmail.com>
    Co-authored-by: LEGO Technix <109212476+lego-technix@users.noreply.github.com>
    Co-authored-by: Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>

--- certif/tests/unit/adapters/member_test.js
+++ certif/tests/unit/adapters/member_test.js
@@ -1,6 +1,8 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import sinon from 'sinon';
 import Service from '@ember/service';
+import ENV from 'pix-certif/config/environment';
 
 module('Unit | Adapter | member', function (hooks) {
   setupTest(hooks);
@@ -16,6 +18,71 @@ module('Unit | Adapter | member', function (hooks) {
 
       // then
       assert.true(url.endsWith(`certification-centers/${certificationCenterId}/members`));
+    });
+  });
+
+  module('#urlForUpdateRecord', function () {
+    test('calls the right url', async function (assert) {
+      // given
+      const adapter = this.owner.lookup('adapter:member');
+      const store = this.owner.lookup('service:store');
+
+      const certificationCenterId = 123;
+      const currentAllowedCertificationCenterAccess = store.createRecord('allowed-certification-center-access', {
+        id: certificationCenterId,
+      });
+      class CurrentUserStub extends Service {
+        currentAllowedCertificationCenterAccess = currentAllowedCertificationCenterAccess;
+      }
+      this.owner.register('service:current-user', CurrentUserStub);
+
+      const certificationCenterMembershipId = 456;
+
+      // when
+      const url = await adapter.urlForUpdateRecord(certificationCenterMembershipId, 'member', {});
+
+      // then
+      assert.true(url.endsWith('/certification-centers/123/certification-center-memberships/456'));
+    });
+  });
+
+  module('#updateRecord', () => {
+    test('triggers an ajax call with the url, data and method', async function (assert) {
+      // given
+      const certificationCenterId = 123;
+      const certificationCenterMembershipId = 456;
+      const url = `${ENV.APP.API_HOST}/api/certification-centers/${certificationCenterId}/certification-center-memberships/456`;
+      const userId = 888;
+      const role = 'MEMBER';
+      const adapter = this.owner.lookup('adapter:member');
+      sinon.stub(adapter, 'ajax').resolves();
+      sinon.stub(adapter, 'buildURL').returns(url);
+      sinon.stub(adapter, 'serialize').returns({
+        id: userId,
+        data: { attributes: { 'certification-center-membership-id': certificationCenterMembershipId, role } },
+      }); //
+      const snapshot = { id: userId };
+
+      const store = this.owner.lookup('service:store');
+
+      // when
+      await adapter.updateRecord(store, { modelName: 'certificationCenterMembership' }, snapshot);
+
+      // then
+      const expectedModelName = 'certificationCenterMembership';
+      const expectedId = 456;
+      const expectedSnapshot = { id: userId };
+      sinon.assert.calledWith(adapter.buildURL, expectedModelName, expectedId, expectedSnapshot);
+
+      const expectedMethod = 'PATCH';
+      const expectedData = {
+        data: {
+          id: userId,
+          data: { attributes: { 'certification-center-membership-id': certificationCenterMembershipId, role } },
+        },
+      };
+      sinon.assert.calledWith(adapter.ajax, url, expectedMethod, expectedData);
+      assert.ok(adapter);
     });
   });
 
