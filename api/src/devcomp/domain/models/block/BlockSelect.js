99cc1d8b5f8bc807424d81e73bc4061ac334554e
dlahaye <dimitrilahaye.dev@gmail.com>
2023-12-08 15:32:05 +0100

    feat(api): add Block Select domain model
    Co-authored-by: miranda.lin-guignard <miranda.w.lin@gmail.com>

---
+++ api/src/devcomp/domain/models/block/BlockSelect.js
@@ -0,0 +1,27 @@
+import { assertNotNullOrUndefined } from '../../../../shared/domain/models/asserts.js';
+
+class BlockSelect {
+  constructor({ type, input, display, placeholder, ariaLabel, defaultValue, tolerances, options, solutions }) {
+    assertNotNullOrUndefined(type, 'Le type est obligatoire pour un bloc de selection');
+    assertNotNullOrUndefined(input, "L'input est obligatoire pour un bloc de selection");
+    assertNotNullOrUndefined(display, 'Le display est obligatoire pour un bloc de selection');
+    assertNotNullOrUndefined(placeholder, 'Le placeholder est obligatoire pour un bloc de selection');
+    assertNotNullOrUndefined(ariaLabel, "L'aria Label est obligatoire pour un bloc de selection");
+    assertNotNullOrUndefined(defaultValue, 'La valeur par défaut est obligatoire pour un bloc de selection');
+    assertNotNullOrUndefined(tolerances, 'Les tolérances sont obligatoires pour un bloc de selection');
+    assertNotNullOrUndefined(options, 'Les options sont obligatoires pour un bloc de selection');
+    assertNotNullOrUndefined(solutions, 'Les solutions sont obligatoires pour un bloc de selection');
+
+    this.type = type;
+    this.input = input;
+    this.display = display;
+    this.placeholder = placeholder;
+    this.ariaLabel = ariaLabel;
+    this.defaultValue = defaultValue;
+    this.tolerances = tolerances;
+    this.options = options;
+    this.solutions = solutions;
+  }
+}
+
+export { BlockSelect };
