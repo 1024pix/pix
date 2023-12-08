8ea7b345193f96a71699f024647082fe28dbd34d
Eric Lim <eric.lim@zenika.com>
2023-12-08 15:41:24 +0100

    feat(certif): add icon trigger component
    Co-authored-by: Ismael Gorissen <ismael.gorissen@gmail.com>
    Co-authored-by: Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>

---
+++ certif/tests/integration/components/dropdown/icon-trigger_test.js
@@ -0,0 +1,35 @@
+import { module, test } from 'qunit';
+import { hbs } from 'ember-cli-htmlbars';
+import { clickByName, render } from '@1024pix/ember-testing-library';
+import setupIntlRenderingTest from '../../../helpers/setup-intl-rendering';
+
+module('Integration | Component | Dropdown | icon-trigger', function (hooks) {
+  setupIntlRenderingTest(hooks);
+
+  test('should display actions menu', async function (assert) {
+    // when
+    await render(hbs`<Dropdown::IconTrigger @ariaLabel='Afficher les actions' />`);
+
+    // then
+    assert.dom('[aria-label="Afficher les actions"]').exists();
+  });
+
+  test('should display actions on click', async function (assert) {
+    // when
+    await render(hbs`<Dropdown::IconTrigger @ariaLabel='Afficher les actions'>Something</Dropdown::IconTrigger>`);
+    await clickByName('Afficher les actions');
+
+    // then
+    assert.contains('Something');
+  });
+
+  test('should hide actions on click again', async function (assert) {
+    // when
+    await render(hbs`<Dropdown::IconTrigger @ariaLabel='Afficher les actions'>Something</Dropdown::IconTrigger>`);
+    await clickByName('Afficher les actions');
+    await clickByName('Afficher les actions');
+
+    // then
+    assert.notContains('Something');
+  });
+});
