058f8651e381860047366449223a88d991c89061
Antoine Ceol <antoine.ceol@gmail.com>
2023-12-13 09:10:26 +0100

    :zap: admin: add back deleteRecord after fix in ember-data 4.12

--- admin/app/controllers/authenticated/complementary-certifications/complementary-certification/attach-target-profile.js
+++ admin/app/controllers/authenticated/complementary-certifications/complementary-certification/attach-target-profile.js
@@ -72,6 +72,7 @@ export default class AttachTargetProfileController extends Controller {
 
       complementaryCertificationBadges.forEach((complementaryCertificationBadge) => {
         complementaryCertification.complementaryCertificationBadges.removeObject(complementaryCertificationBadge);
+        complementaryCertificationBadge.deleteRecord();
       });
 
       this.#targetProfileBadges.forEach((badge, badgeId) => {
