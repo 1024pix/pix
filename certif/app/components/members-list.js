bde80c776997c95c3c9c63ba1f660dd54fe71f2b
Ismael Gorissen <ismael.gorissen@gmail.com>
2023-12-14 15:02:59 +0100

    feat(certif): add closeLeaveCertificationCenterModal method
    in MembersList component

--- certif/app/components/members-list.js
+++ certif/app/components/members-list.js
@@ -1,9 +1,14 @@
 import Component from '@glimmer/component';
 import { service } from '@ember/service';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class MembersList extends Component {
   @service featureToggles;
   @service currentUser;
+
+  @tracked
+  isLeaveCertificationCenterModalOpen = false;
 
   get shouldDisplayRefererColumn() {
     return this.args.hasCleaHabilitation;
@@ -16,5 +21,10 @@ export default class MembersList extends Component {
   get isMultipleAdminsAvailable() {
     const adminMembers = this.args.members?.filter((member) => member.isAdmin);
     return adminMembers.length > 1;
+  }
+
+  @action
+  closeLeaveCertificationCenterModal() {
+    this.isLeaveCertificationCenterModalOpen = false;
   }
 }
