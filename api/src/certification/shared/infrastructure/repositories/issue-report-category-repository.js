5b5dffb57db6fe50a89bf8f721c42efe9c69dec8
matthieu-octo <matthieu.gros@octo.com>
2023-12-14 17:17:39 +0100

    :sparkles: certif: return a NotFound domain error when the issue report category is not found in the database by its name or a CertificationIssueReport read model

--- api/src/certification/shared/infrastructure/repositories/issue-report-category-repository.js
+++ api/src/certification/shared/infrastructure/repositories/issue-report-category-repository.js
@@ -1,7 +1,18 @@
 import { knex } from '../../../../../db/knex-database-connection.js';
+import { CertificationIssueReportCategory } from '../../../issue-reports/domain/read-models/CertificationIssueReportCategory.js';
+import { NotFoundError } from '../../../../shared/domain/errors.js';
+
+function _toDomain(issueReportCategoryModel) {
+  return new CertificationIssueReportCategory({ id: issueReportCategoryModel.id });
+}
 
 const get = async function ({ name }) {
-  return knex('issue-report-categories').where({ name }).first();
+  const issueReportCategory = await knex('issue-report-categories').where({ name }).first();
+
+  if (!issueReportCategory) {
+    throw new NotFoundError('The issue report category name does not exist');
+  }
+  return _toDomain(issueReportCategory);
 };
 
 export { get };
--- api/tests/certification/shared/integration/infrastructure/repositories/issue-report-categories-repository_test.js
+++ api/tests/certification/shared/integration/infrastructure/repositories/issue-report-categories-repository_test.js
@@ -1,18 +1,43 @@
-import { expect, databaseBuilder } from '../../../../../test-helper.js';
+import { expect, databaseBuilder, catchErr } from '../../../../../test-helper.js';
 import * as issueReportCategoryRepository from '../../../../../../src/certification/shared/infrastructure/repositories/issue-report-category-repository.js';
+import { CertificationIssueReportCategory } from '../../../../../../src/certification/issue-reports/domain/read-models/CertificationIssueReportCategory.js';
+import { NotFoundError } from '../../../../../../src/shared/domain/errors.js';
 
 describe('Integration | Repository | Issue Report Categories', function () {
   describe('#get', function () {
-    it('should return a certification issue report', async function () {
-      // given
-      const issueReportCategory = databaseBuilder.factory.buildIssueReportCategory({ category: 'OTHER' });
-      await databaseBuilder.commit();
+    describe('when the issue report category name exists', function () {
+      it('should return a CertificationIssueReportCategory domain read model', async function () {
+        // given
+        const categoryId = 1;
+        const issueReportCategory = databaseBuilder.factory.buildIssueReportCategory({
+          id: categoryId,
+          category: 'OTHER',
+        });
+        await databaseBuilder.commit();
 
-      // when
-      const result = await issueReportCategoryRepository.get({ name: issueReportCategory.name });
+        // when
+        const result = await issueReportCategoryRepository.get({ name: issueReportCategory.name });
 
-      // then
-      expect(result).to.deep.equal({ ...issueReportCategory });
+        // then
+        expect(result).to.be.instanceof(CertificationIssueReportCategory);
+        expect(result.id).to.equal(categoryId);
+      });
+    });
+
+    describe('when the issue report category name does not exist', function () {
+      it('should throw a Not Found domain error', async function () {
+        // given
+        databaseBuilder.factory.buildIssueReportCategory({ category: 'OTHER', name: 'Some problem' });
+        await databaseBuilder.commit();
+
+        // when
+        const nonExistingCategoryName = 'NON_EXISTING_CATEGORY_NAME';
+        const result = await catchErr(issueReportCategoryRepository.get)({ name: nonExistingCategoryName });
+
+        // then
+        expect(result).to.be.instanceOf(NotFoundError);
+        expect(result.message).to.equal(`The issue report category name does not exist`);
+      });
     });
   });
 });
