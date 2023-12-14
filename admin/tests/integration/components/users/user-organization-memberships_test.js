b0595648e63b10956a18ddaf3f397f9bfa66a0f3
Steph0 <Steph0@users.noreply.github.com>
2023-12-14 17:38:18 +0100

    refactor(admin): test displayed date on user-organization-memberships component

--- admin/tests/integration/components/users/user-organization-memberships_test.js
+++ admin/tests/integration/components/users/user-organization-memberships_test.js
@@ -36,20 +36,27 @@ module('Integration | Component | users | organization-memberships', function (h
         hasAccessToOrganizationActionsScope = false;
       }
       this.owner.register('service:accessControl', SessionStub);
+      const dayjsService = this.owner.lookup('service:dayjs');
 
+      const membershipUpdatedAt1 = new Date('2023-12-05T08:00:00Z');
+      const membershipUpdatedAt2 = new Date('2023-12-05T09:00:00Z');
       const organizationMembership1 = EmberObject.create({
+        id: 111,
         role: 'MEMBER',
         organizationId: 100025,
         organizationName: 'Dragon & Co',
         organizationType: 'PRO',
+        updatedAt: membershipUpdatedAt1,
       });
 
       const organizationMembership2 = EmberObject.create({
+        id: 222,
         role: 'MEMBER',
         organizationId: 100095,
         organizationName: 'Collège The Night Watch',
         organizationType: 'SCO',
         organizationExternalId: '1237457A',
+        updatedAt: membershipUpdatedAt2,
       });
 
       const organizationMemberships = [organizationMembership1, organizationMembership2];
@@ -61,8 +68,21 @@ module('Integration | Component | users | organization-memberships', function (h
       );
 
       // then
-      assert.dom(screen.getByText('Collège The Night Watch')).exists();
-      assert.dom(screen.getByText('Dragon & Co')).exists();
+      const formattedUpdatedAt1 = dayjsService.self(membershipUpdatedAt1).format('DD/MM/YYYY [-] HH:mm');
+      const formattedUpdatedAt2 = dayjsService.self(membershipUpdatedAt1).format('DD/MM/YYYY [-] HH:mm');
+
+      assert.dom(screen.getByRole('cell', { name: '111' })).exists();
+      assert.dom(screen.getByRole('cell', { name: '100025' })).exists();
+      assert.dom(screen.getByRole('cell', { name: 'Dragon & Co' })).exists();
+      assert.dom(screen.getByRole('cell', { name: 'PRO' })).exists();
+      assert.dom(screen.getByRole('cell', { name: formattedUpdatedAt1 })).exists();
+
+      assert.dom(screen.getByRole('cell', { name: '222' })).exists();
+      assert.dom(screen.getByRole('cell', { name: '100095' })).exists();
+      assert.dom(screen.getByRole('cell', { name: 'Collège The Night Watch' })).exists();
+      assert.dom(screen.getByRole('cell', { name: 'SCO' })).exists();
+      assert.dom(screen.getByRole('cell', { name: '1237457A' })).exists();
+      assert.dom(screen.getByRole('cell', { name: formattedUpdatedAt2 })).exists();
     });
   });
 });
