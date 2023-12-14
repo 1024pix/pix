dc5d12b23613695e993cba2b0a079c35fd5ab817
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-12-14 14:01:16 +0100

    Update ember-data to 4.7

--- mon-pix/app/serializers/application.js
+++ mon-pix/app/serializers/application.js
@@ -1,3 +1,7 @@
 import JSONAPISerializer from '@ember-data/serializer/json-api';
 
-export default class ApplicationSerializer extends JSONAPISerializer {}
+export default class ApplicationSerializer extends JSONAPISerializer {
+  // This bypasses extractErrors emberData behavior which relies on the property being a function.
+  // This should not be needed after upgrading to ember-data v5
+  extractErrors = false;
+}
