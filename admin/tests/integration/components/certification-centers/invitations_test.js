538a49aeceeccf93689207cbacec3950da1eb276
Steph0 <Steph0@users.noreply.github.com>
2023-12-14 17:25:50 +0100

    refactor(admin): add displayed date tests on certification centers invitation management page

--- admin/tests/integration/components/certification-centers/invitations_test.js
+++ admin/tests/integration/components/certification-centers/invitations_test.js
@@ -26,13 +26,17 @@ module('Integration | Component | Certification Centers | Invitations', function
     test('should show invitations list', async function (assert) {
       // given
       const store = this.owner.lookup('service:store');
+      const dayjsService = this.owner.lookup('service:dayjs');
+
+      const invitationUpdatedAt1 = new Date('2020-02-02T09:00:00Z');
+      const invitationUpdatedAt2 = new Date('2022-02-02T15:12:00Z');
       const certificationCenterInvitation1 = store.createRecord('certification-center-invitation', {
         email: 'elo.dela@example.net',
-        updatedAt: new Date('2020-02-02'),
+        updatedAt: invitationUpdatedAt1,
       });
       const certificationCenterInvitation2 = store.createRecord('certification-center-invitation', {
         email: 'alain.finis@example.net',
-        updatedAt: new Date('2022-02-02'),
+        updatedAt: invitationUpdatedAt2,
       });
       this.certificationCenterInvitations = [certificationCenterInvitation1, certificationCenterInvitation2];
       this.cancelCertificationCenterInvitation = sinon.stub();
@@ -46,11 +50,16 @@ module('Integration | Component | Certification Centers | Invitations', function
       );
 
       // then
+      const formattedInvitationUpdatedAt1 = dayjsService.self(invitationUpdatedAt1).format('DD/MM/YYYY [-] HH:mm');
+      const formattedInvitationUpdatedAt2 = dayjsService.self(invitationUpdatedAt2).format('DD/MM/YYYY [-] HH:mm');
+
       assert.dom(screen.getByRole('heading', { name: 'Invitations' })).exists();
       assert.dom(screen.getByRole('columnheader', { name: 'Adresse e-mail' })).exists();
       assert.dom(screen.getByRole('columnheader', { name: 'Date de dernier envoi' })).exists();
       assert.dom(screen.getByRole('cell', { name: 'elo.dela@example.net' })).exists();
+      assert.dom(screen.getByRole('cell', { name: formattedInvitationUpdatedAt1 })).exists();
       assert.dom(screen.getByRole('cell', { name: 'alain.finis@example.net' })).exists();
+      assert.dom(screen.getByRole('cell', { name: formattedInvitationUpdatedAt2 })).exists();
     });
   });
 });
