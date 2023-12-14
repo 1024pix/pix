f18e83c795a1c697f6aeb1c6642e15f0194d21d8
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2023-12-14 16:07:24 +0100

    refactor(api): remove innapropriate validation inside usecases

--- api/lib/domain/usecases/update-campaign.js
+++ api/lib/domain/usecases/update-campaign.js
@@ -1,40 +1,31 @@
 import _ from 'lodash';
-import { UserNotAuthorizedToUpdateResourceError } from '../errors.js';
 import { EntityValidationError } from '../../../src/shared/domain/errors.js';
 import * as campaignValidator from '../validators/campaign-validator.js';
 
 const updateCampaign = async function ({
-  userId,
   campaignId,
   name,
   title,
   customLandingPageText,
   ownerId,
-  userRepository,
   campaignRepository,
   membershipRepository,
 }) {
-  const [user, campaign] = await Promise.all([
-    userRepository.getWithMemberships(userId),
-    campaignRepository.get(campaignId),
-  ]);
+  const [campaign] = await campaignRepository.get(campaignId);
 
   const organizationId = campaign.organizationId;
-  if (!user.hasAccessToOrganization(organizationId)) {
-    throw new UserNotAuthorizedToUpdateResourceError(
-      `User does not have an access to the organization ${organizationId}`,
-    );
-  }
 
-  const ownerMembership = await membershipRepository.findByUserIdAndOrganizationId({
-    userId: ownerId,
-    organizationId,
-  });
-
-  if (_.isEmpty(ownerMembership)) {
-    throw new EntityValidationError({
-      invalidAttributes: [{ attribute: 'ownerId', message: 'OWNER_NOT_IN_ORGANIZATION' }],
+  if (ownerId) {
+    const ownerMembership = await membershipRepository.findByUserIdAndOrganizationId({
+      userId: ownerId,
+      organizationId,
     });
+
+    if (_.isEmpty(ownerMembership)) {
+      throw new EntityValidationError({
+        invalidAttributes: [{ attribute: 'ownerId', message: 'OWNER_NOT_IN_ORGANIZATION' }],
+      });
+    }
   }
 
   if (name !== undefined) campaign.name = name;
