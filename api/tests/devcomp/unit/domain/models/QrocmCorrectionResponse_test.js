cd8ef9287d18c1ea9be39b0483a9d9b1365b898d
dlahaye <dimitrilahaye.dev@gmail.com>
2023-12-15 15:01:50 +0100

    feat(api): add QrocmCorrectionResponse domain model

---
+++ api/tests/devcomp/unit/domain/models/QrocmCorrectionResponse_test.js
@@ -0,0 +1,45 @@
+import { expect } from '../../../../test-helper.js';
+import { QrocmCorrectionResponse } from '../../../../../src/devcomp/domain/models/QrocmCorrectionResponse.js';
+import { AnswerStatus } from '../../../../../src/devcomp/domain/models/validator/AnswerStatus.js';
+
+describe('Unit | Devcomp | Domain | Models | QrocmCorrectionResponse', function () {
+  describe('#constructor', function () {
+    it('should create a QROCM correction response and keep attributes', function () {
+      // given
+      const status = AnswerStatus.OK;
+      const feedback = 'Bien joué !';
+      const solutionValue = Symbol('solutionValue');
+
+      // when
+      const qcuCorrectionResponse = new QrocmCorrectionResponse({ status, feedback, solutionValue });
+
+      // then
+      expect(qcuCorrectionResponse).not.to.be.undefined;
+      expect(qcuCorrectionResponse.status).to.deep.equal(status);
+      expect(qcuCorrectionResponse.feedback).to.equal(feedback);
+      expect(qcuCorrectionResponse.solutionValue).to.equal(solutionValue);
+    });
+  });
+
+  describe('A QROCM correction response without status', function () {
+    it('should throw an error', function () {
+      expect(() => new QrocmCorrectionResponse({})).to.throw('Le résultat est obligatoire pour une réponse de QROCM');
+    });
+  });
+
+  describe('A QROCM correction response without feedback', function () {
+    it('should throw an error', function () {
+      expect(() => new QrocmCorrectionResponse({ status: AnswerStatus.OK })).to.throw(
+        'Le feedback est obligatoire pour une réponse de QROCM',
+      );
+    });
+  });
+
+  describe('A QROCM correction response without solutions', function () {
+    it('should throw an error', function () {
+      expect(() => new QrocmCorrectionResponse({ status: AnswerStatus.OK, feedback: 'Bien joué !' })).to.throw(
+        'La solution est obligatoire pour une réponse de QROCM',
+      );
+    });
+  });
+});
