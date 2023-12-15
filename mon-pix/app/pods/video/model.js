c57d3ba4cbf51671b3c9833e4f1fb5451c887e85
miranda.lin-guignard <miranda.w.lin@gmail.com>
2023-12-15 18:12:32 +0100

    feat(mon-pix): add video model for modulix

---
+++ mon-pix/app/pods/video/model.js
@@ -0,0 +1,11 @@
+import { attr } from '@ember-data/model';
+import Element from '../element/model';
+
+export default class Video extends Element {
+  @attr('string') title;
+  @attr('string') url;
+  @attr('string') subtitles;
+  @attr('string') alternativeText;
+}
+
+export { Video };
