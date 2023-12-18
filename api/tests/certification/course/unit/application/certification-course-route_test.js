9ac2e53440ab5f1eb04f6166f67c8a43365e7ebe
Alexandre COIN <alexandre@mbpdealexandre-1.home>
2023-12-18 11:57:04 +0100

    :sparkles: api: add endpoint to get v3 details

--- api/tests/certification/course/unit/application/certification-course-route_test.js
+++ api/tests/certification/course/unit/application/certification-course-route_test.js
@@ -29,6 +29,7 @@ describe('Unit | Route | certification-course-route', function () {
       expect(response.statusCode).to.equal(403);
     });
   });
+
   describe('POST /api/admin/certification-courses/{id}/unreject', function () {
     it('return forbidden access if user has METIER role', async function () {
       // given
@@ -54,6 +55,29 @@ describe('Unit | Route | certification-course-route', function () {
 
       // then
       expect(response.statusCode).to.equal(403);
+    });
+  });
+
+  describe('GET /api/admin/certification-courses-v3/{id}/details', function () {
+    it('returns a 200', async function () {
+      // given
+      sinon
+        .stub(securityPreHandlers, 'adminMemberHasAtLeastOneAccessOf')
+        .withArgs([
+          securityPreHandlers.checkAdminMemberHasRoleSuperAdmin,
+          securityPreHandlers.checkAdminMemberHasRoleCertif,
+          securityPreHandlers.checkAdminMemberHasRoleSupport,
+          securityPreHandlers.checkAdminMemberHasRoleMetier,
+        ])
+        .callsFake(() => (request, h) => h.response().code(200).takeover());
+      const httpTestServer = new HttpTestServer();
+      await httpTestServer.register(moduleUnderTest);
+
+      // when
+      const response = await httpTestServer.request('GET', '/api/admin/certification-courses-v3/1/details');
+
+      // then
+      expect(response.statusCode).to.equal(200);
     });
   });
 });
