4d49bb6028ae00554826a50dca31f00eacf6a11b
dlahaye <dimitrilahaye.dev@gmail.com>
2023-12-19 12:29:45 +0100

    feat(mon-pix) allow QROCM answer verification

--- mon-pix/app/pods/components/module/qrocm/component.js
+++ mon-pix/app/pods/components/module/qrocm/component.js
@@ -9,21 +9,53 @@ export default class ModuleQrocm extends Component {
   qrocm = this.args.qrocm;
 
   @action
-  onAnswerChanged(block, value) {
-    this.selectedValues = {
-      ...this.selectedValues,
-      [block.input]: value,
-    };
+  onInputChanged(block, { target }) {
+    this.#updateSelectedValues(block, target.value);
+  }
+
+  @action
+  onSelectChanged(block, value) {
+    this.#updateSelectedValues(block, value);
+  }
+
+  get disableInput() {
+    return !!this.qrocm.lastCorrection;
+  }
+
+  get feedbackType() {
+    return this.qrocm.lastCorrection?.isOk ? 'success' : 'error';
+  }
+
+  get shouldDisplayFeedback() {
+    return !!this.qrocm.lastCorrection;
   }
 
   @action
   async submitAnswer(event) {
     event.preventDefault();
-    this.requiredMessage = this.qrocm.proposals.some(({ input }) => {
-      return !this.selectedValues?.[input];
-    });
+    this.requiredMessage = this.qrocm.proposals
+      .filter(({ type }) => ['input', 'select'].includes(type))
+      .some(({ input }) => {
+        return !this.selectedValues?.[input];
+      });
     if (this.requiredMessage) {
       return;
     }
+    const answers = [];
+    for (const [key, value] of Object.entries(this.selectedValues)) {
+      answers.push({
+        input: key,
+        answer: value,
+      });
+    }
+    const answerData = { userResponse: answers, element: this.qrocm };
+    await this.args.submitAnswer(answerData);
+  }
+
+  #updateSelectedValues(block, value) {
+    this.selectedValues = {
+      ...this.selectedValues,
+      [block.input]: value,
+    };
   }
 }
