b2e549f8bc96c44d55a309cc22e5e6bc9acdf12f
Yvonnick Frin <frin.yvonnick@gmail.com>
2023-12-20 13:22:33 +0100

    feat(api): add a api to get target profile details by id

--- api/src/prescription/target-profile/application/api/target-profile-api.js
+++ api/src/prescription/target-profile/application/api/target-profile-api.js
@@ -1,4 +1,5 @@
 import { usecases } from '../../domain/usecases/index.js';
+import { usecases as libUsecases } from '../../../../../lib/domain/usecases/index.js';
 import { TargetProfile } from './TargetProfile.js';
 
 /**
@@ -18,4 +19,17 @@ export const getByOrganizationId = async (organizationId) => {
   const targetProfiles = await usecases.getAvailableTargetProfilesForOrganization({ organizationId });
 
   return targetProfiles.map((targetProfileForSpecifier) => new TargetProfile(targetProfileForSpecifier));
+};
+
+/**
+ * @function
+ * @name getById
+ *
+ * @param {number} id
+ * @returns {Promise<TargetProfile>}
+ */
+export const getById = async (id) => {
+  const targetProfileForAdmin = await libUsecases.getTargetProfileForAdmin({ targetProfileId: id });
+
+  return new TargetProfile(targetProfileForAdmin);
 };
