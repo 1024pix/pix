f81be2103bbebb429e0b38c38f533a553f65d7e3
Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
2023-12-20 16:17:04 +0100

    feat(api): find paginated list of autonomous courses

--- api/src/evaluation/infrastructure/repositories/autonomous-course-repository.js
+++ api/src/evaluation/infrastructure/repositories/autonomous-course-repository.js
@@ -53,4 +53,19 @@ async function get({ autonomousCourseId, campaignApi }) {
   return _toDomain(autonomousCourseDTO);
 }
 
-export { save, get };
+/**
+ * @param {Object} page custom pagination properties
+ * @param {number} page.size pagination items per page
+ * @param {number} page.number current pagination page
+ * @param {CampaignApi} campaignApi prescription campaign API
+ * @returns {Promise<{autonomousCourses: Array<CampaignListItem>, meta: { page: number, pageSize: number, rowCount: number, pageCount: number} }>} returns a paginated list of autonomous courses
+ */
+const findAllPaginated = async function ({ page, campaignApi }) {
+  const { models: autonomousCourses, meta } = await campaignApi.findAllForOrganization({
+    organizationId: constants.AUTONOMOUS_COURSES_ORGANIZATION_ID,
+    page,
+  });
+  return { autonomousCourses, meta };
+};
+
+export { save, get, findAllPaginated };
