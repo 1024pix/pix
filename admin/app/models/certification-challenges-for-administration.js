4df607060192ff85ac436cac4ba36240d5ca2872
Alexandre COIN <alexandre@mbpdealexandre-1.home>
2023-12-21 15:01:57 +0100

    :sparkles: admin: add challenge list table

--- admin/app/models/certification-challenges-for-administration.js
+++ admin/app/models/certification-challenges-for-administration.js
@@ -1,7 +1,11 @@
 import Model, { attr } from '@ember-data/model';
 export default class CertificationChallengesForAdministration extends Model {
   @attr() answerStatus;
+  @attr() answeredAt;
+  @attr() competenceIndex;
+  @attr() competenceName;
   @attr() validatedLiveAlert;
+  @attr() skillName;
   version = 3;
 
   isOk() {
