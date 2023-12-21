4dda1425c255dc2296df9551d4982c122cc4c467
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2023-12-21 16:19:28 +0100

    fix(api): add exhaustive test on profiles-collection-participations

--- api/tests/prescription/campaign/integration/application/campaign-results-route_test.js
+++ api/tests/prescription/campaign/integration/application/campaign-results-route_test.js
@@ -71,4 +71,66 @@ describe('Integration | Application | campaign-results-route', function () {
       expect(response.statusCode).to.equal(400);
     });
   });
+
+  describe('GET /api/campaigns/{id}/profiles-collection-participations', function () {
+    const method = 'GET';
+
+    let headers, httpTestServer, organizationId, campaignId, url;
+    beforeEach(async function () {
+      sinon.stub(campaignResultsController, 'findProfilesCollectionParticipations').returns('ok');
+      httpTestServer = new HttpTestServer();
+      await httpTestServer.register(moduleUnderTest);
+      httpTestServer.setupAuthentication();
+      organizationId = databaseBuilder.factory.buildOrganization().id;
+      campaignId = databaseBuilder.factory.buildCampaign({ organizationId }).id;
+    });
+
+    it('return a 401 status code when trying to call route unauthenticated', async function () {
+      url = '/api/campaigns/2/profiles-collection-participations';
+      // given
+      headers = {
+        authorization: null,
+      };
+
+      // when
+      const response = await httpTestServer.request(method, url, null, null, headers);
+
+      // then
+      expect(response.statusCode).to.equal(401);
+    });
+
+    it('return a 403 status code when trying to call route but user is not member', async function () {
+      // given
+      url = `/api/campaigns/${campaignId}/profiles-collection-participations`;
+      const simpleUserId = databaseBuilder.factory.buildUser().id;
+      await databaseBuilder.commit();
+
+      headers = {
+        authorization: generateValidRequestAuthorizationHeader(simpleUserId),
+      };
+
+      // when
+      const response = await httpTestServer.request(method, url, null, null, headers);
+
+      // then
+      expect(response.statusCode).to.equal(403);
+    });
+
+    it('return a 400 status code when trying to call route with illegal organization id', async function () {
+      // given
+      const wrongUrl = `/api/campaigns/GodZilla/profiles-collection-participations`;
+      const userId = databaseBuilder.factory.buildUser().id;
+      databaseBuilder.factory.buildMembership({ organizationId, userId, organizationRole: 'MEMBER' });
+      await databaseBuilder.commit();
+      headers = {
+        authorization: generateValidRequestAuthorizationHeader(userId),
+      };
+
+      // when
+      const response = await httpTestServer.request(method, wrongUrl, null, null, headers);
+
+      // then
+      expect(response.statusCode).to.equal(400);
+    });
+  });
 });
