392e3766858f3b4fcbbdea6142d99391ad528cf5
Alexandre COIN <alexandre@mbpdealexandre-1.home>
2023-12-21 15:02:55 +0100

    :sparkles: api: get answer time

--- api/src/certification/course/domain/models/V3CertificationChallengeForAdministration.js
+++ api/src/certification/course/domain/models/V3CertificationChallengeForAdministration.js
@@ -1,7 +1,8 @@
 export class V3CertificationChallengeForAdministration {
-  constructor({ challengeId, answerStatus, validatedLiveAlert }) {
+  constructor({ challengeId, answerStatus, validatedLiveAlert, answeredAt }) {
     this.challengeId = challengeId;
     this.answerStatus = answerStatus;
     this.validatedLiveAlert = validatedLiveAlert;
+    this.answeredAt = answeredAt;
   }
 }
