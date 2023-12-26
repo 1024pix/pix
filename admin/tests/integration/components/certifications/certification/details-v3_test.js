b457393c752100cd90d3958bf06279026b87c8c7
Alexandre COIN <alexandre@mbpdealexandre-1.home>
2023-12-26 14:33:35 +0100

    :sparkles: admin: add modal to display answer

--- admin/tests/integration/components/certifications/certification/details-v3_test.js
+++ admin/tests/integration/components/certifications/certification/details-v3_test.js
@@ -2,6 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render, within } from '@1024pix/ember-testing-library';
 import { hbs } from 'ember-cli-htmlbars';
+import { click } from '@ember/test-helpers';
 
 const answers = [
   {
@@ -54,7 +55,7 @@ module('Integration | Component | Certifications | certification > details v3', 
   setupRenderingTest(hooks);
 
   module('#display', function () {
-    test('it should display the certification complementary info section with the right info', async function (assert) {
+    test('displays the certification complementary info section with the right info', async function (assert) {
       // given
       const store = this.owner.lookup('service:store');
       this.model = store.createRecord('v3-certification-course-details-for-administration', {
@@ -97,7 +98,7 @@ module('Integration | Component | Certifications | certification > details v3', 
       assert.deepEqual(expected, result);
     });
 
-    test('it should display the table with every questions asked during certification', async function (assert) {
+    test('displays the table with every questions asked during certification', async function (assert) {
       // given
       const store = this.owner.lookup('service:store');
       this.model = store.createRecord('v3-certification-course-details-for-administration', {
@@ -116,7 +117,7 @@ module('Integration | Component | Certifications | certification > details v3', 
       const result = [];
 
       linesWithResults.forEach((lineElement) => {
-        const line = within(lineElement).getAllByRole('cell');
+        const line = _getAllCellsButAction(lineElement);
         result.push(line.map((cell) => cell.innerText));
       });
 
@@ -127,9 +128,27 @@ module('Integration | Component | Certifications | certification > details v3', 
         `${answer.competenceIndex} ${answer.competenceName}`,
         answer.skillName,
         `${answer.id.toString()} `,
-        '',
       ]);
       assert.deepEqual(expected, result);
+    });
+
+    test('displays the modal with the candidate answer on icon click', async function (assert) {
+      // given
+      const store = this.owner.lookup('service:store');
+      this.model = store.createRecord('v3-certification-course-details-for-administration', {
+        certificationChallengesForAdministration: createChallengesForAdministration(['ok', 'ko'], store),
+      });
+
+      // when
+      const screen = await render(hbs`<Certifications::Certification::DetailsV3 @details={{this.model}} />`);
+
+      const modalButtons = screen.getAllByRole('button', { name: 'Afficher la réponse du candidat' });
+      await click(modalButtons[0]);
+
+      // then
+      const modal = within(await screen.findByRole('dialog'));
+      assert.dom(modal.getByRole('heading', { name: 'Réponse question 1' })).exists();
+      assert.dom(screen.getByText('Réponse 1')).exists();
     });
   });
 });
@@ -138,7 +157,9 @@ function createChallengesForAdministration(answerStatuses, store) {
   return answerStatuses.map((answerStatus, index) =>
     store.createRecord('certification-challenges-for-administration', {
       id: index,
+      questionNumber: index + 1,
       answerStatus,
+      answerValue: `Réponse ${index + 1}`,
       validatedLiveAlert: !answerStatus,
     }),
   );
@@ -146,4 +167,10 @@ function createChallengesForAdministration(answerStatuses, store) {
 
 function createDetailedAnswers(answers, store) {
   return answers.map((answer) => store.createRecord('certification-challenges-for-administration', answer));
+}
+
+function _getAllCellsButAction(lineElement) {
+  const line = within(lineElement).getAllByRole('cell');
+  line.splice(-1);
+  return line;
 }
