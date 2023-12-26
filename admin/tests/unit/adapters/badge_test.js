ea94a112846b098af4d76c84e06b580db90ca81b
Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
2023-12-26 17:25:19 +0100

    fix(admin): prevent unexpected properties update on badge edition

--- admin/tests/unit/adapters/badge_test.js
+++ admin/tests/unit/adapters/badge_test.js
@@ -1,3 +1,4 @@
+import sinon from 'sinon';
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
@@ -18,6 +19,56 @@ module('Unit | Adapters | badge', function (hooks) {
 
       // then
       assert.true(url.endsWith('/api/admin/target-profiles/788/badges'));
+    });
+  });
+
+  module('#updateRecord', function () {
+    test('should trigger an ajax call with the right method and payload', function (assert) {
+      // given
+      const updatedBadgeData = {
+        key: 'dummy-key',
+        title: 'Dummy title',
+        message: 'dummy message',
+        'image-url': 'dummy.svg',
+        'alt-message': 'gummy alt message',
+        'is-certifiable': true,
+        'is-always-visible': true,
+      };
+
+      const expectedPayload = {
+        data: {
+          data: {
+            attributes: { ...updatedBadgeData },
+          },
+        },
+      };
+
+      const snapshot = {
+        id: 99,
+        serialize: sinon.stub().returns({
+          data: {
+            attributes: {
+              ...updatedBadgeData,
+              'campaign-threshold': Symbol('should-be-removed'),
+              'capped-tubes-criteria': Symbol('should-be-removed'),
+            },
+          },
+        }),
+      };
+
+      adapter.ajax = sinon.stub();
+
+      // when
+      adapter.updateRecord(null, { modelName: 'badge' }, snapshot);
+
+      // then
+      sinon.assert.calledWithExactly(
+        adapter.ajax,
+        'http://localhost:3000/api/admin/badges/99',
+        'PATCH',
+        expectedPayload,
+      );
+      assert.ok(adapter);
     });
   });
 });
