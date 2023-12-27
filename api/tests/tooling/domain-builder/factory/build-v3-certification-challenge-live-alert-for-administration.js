74c128726b95ec40a35aa1d823adc6bde6e4bca9
Alexandre COIN <alexandre@mbpdealexandre-1.home>
2023-12-27 14:47:23 +0100

    :sparkles: api: get issue report category

--- api/tests/tooling/domain-builder/factory/build-v3-certification-challenge-live-alert-for-administration.js
+++ api/tests/tooling/domain-builder/factory/build-v3-certification-challenge-live-alert-for-administration.js
@@ -1,8 +1,9 @@
 import { V3CertificationChallengeLiveAlertForAdministration } from '../../../../src/certification/course/domain/models/V3CertificationChallengeLiveAlertForAdministration.js';
 
-const buildV3CertificationChallengeLiveAlertForAdministration = function ({ id = 456 } = {}) {
+const buildV3CertificationChallengeLiveAlertForAdministration = function ({ id = 456, issueReportSubcategory } = {}) {
   return new V3CertificationChallengeLiveAlertForAdministration({
     id,
+    issueReportSubcategory,
   });
 };
 
