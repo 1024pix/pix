7b2a0c1cdbe6bf39820f60ccfc13afab50b74397
Clément Latzarus <c@ltzr.io>
2023-12-28 13:03:35 +0100

    feat(api): add route to disable certif member

--- api/tests/acceptance/application/certification-center-memberships/certification-center-membership-controller_test.js
+++ api/tests/acceptance/application/certification-center-memberships/certification-center-membership-controller_test.js
@@ -444,5 +444,72 @@ describe('Acceptance | API | Certification Center Membership', function () {
         });
       });
     });
+
+    describe('DELETE /api/certification-center-memberships/{id}', function () {
+      let certificationCenter;
+      let certificationCenterMembership;
+      let user;
+
+      beforeEach(async function () {
+        certificationCenter = databaseBuilder.factory.buildCertificationCenter();
+        user = databaseBuilder.factory.buildUser();
+        certificationCenterMembership = databaseBuilder.factory.buildCertificationCenterMembership({
+          certificationCenterId: certificationCenter.id,
+          userId: user.id,
+        });
+        await databaseBuilder.commit();
+      });
+
+      context('Success cases', function () {
+        context('when parameters are valid', function () {
+          it('returns a 204 HTTP status code', async function () {
+            const pixCertifAdminUser = databaseBuilder.factory.buildUser.withCertificationCenterMembership({
+              role: 'ADMIN',
+              certificationCenterId: certificationCenter.id,
+            });
+
+            const request = {
+              method: 'DELETE',
+              url: `/api/certification-center-memberships/${certificationCenterMembership.id}`,
+              headers: {
+                authorization: generateValidRequestAuthorizationHeader(pixCertifAdminUser.id),
+              },
+            };
+
+            await databaseBuilder.commit();
+
+            // when
+            const { statusCode } = await server.inject(request);
+
+            // then
+            expect(statusCode).to.equal(204);
+          });
+        });
+      });
+
+      context('Error cases', function () {
+        context('when user does not have a valid role', function () {
+          it('returns a 403 HTTP status code', async function () {
+            const userWithoutRole = databaseBuilder.factory.buildUser();
+
+            const request = {
+              method: 'DELETE',
+              url: `/api/certification-center-memberships/${certificationCenterMembership.id}`,
+              headers: {
+                authorization: generateValidRequestAuthorizationHeader(userWithoutRole.id),
+              },
+            };
+
+            await databaseBuilder.commit();
+
+            // when
+            const { statusCode } = await server.inject(request);
+
+            // then
+            expect(statusCode).to.equal(403);
+          });
+        });
+      });
+    });
   });
 });
