996f11b980450569ee35a62f0af45b78ad364dd4
Clément Latzarus <c@ltzr.io>
2023-12-28 12:10:21 +0100

    tests(api): remove redundant DELETE route test

--- api/tests/acceptance/application/certification-center-memberships/certification-center-membership-controller_test.js
+++ api/tests/acceptance/application/certification-center-memberships/certification-center-membership-controller_test.js
@@ -4,7 +4,6 @@ import {
   generateValidRequestAuthorizationHeader,
   insertUserWithRoleSuperAdmin,
   databaseBuilder,
-  sinon,
 } from '../../../test-helper.js';
 
 import { createServer } from '../../../../server.js';
@@ -15,37 +14,6 @@ describe('Acceptance | API | Certification Center Membership', function () {
   beforeEach(async function () {
     server = await createServer();
     await insertUserWithRoleSuperAdmin();
-  });
-
-  describe('DELETE /api/admin/certification-center-memberships/{id}', function () {
-    it('should return 200 HTTP status', async function () {
-      // given
-      const now = new Date();
-      const clock = sinon.useFakeTimers({
-        now,
-        toFake: ['Date'],
-      });
-      const userId = databaseBuilder.factory.buildUser().id;
-      const certificationCenterId = databaseBuilder.factory.buildCertificationCenter().id;
-      const certificationCenterMembershipId = databaseBuilder.factory.buildCertificationCenterMembership({
-        userId,
-        certificationCenterId,
-      }).id;
-      await databaseBuilder.commit();
-
-      const options = {
-        method: 'DELETE',
-        url: `/api/admin/certification-center-memberships/${certificationCenterMembershipId}`,
-        headers: { authorization: generateValidRequestAuthorizationHeader() },
-      };
-
-      // when
-      const response = await server.inject(options);
-
-      // then
-      expect(response.statusCode).to.equal(204);
-      clock.restore();
-    });
   });
 
   context('Admin routes', function () {
