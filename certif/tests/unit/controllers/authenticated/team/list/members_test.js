9478559e52bf7caa32190c070d44e3eae5fc1518
Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>
2023-12-29 16:36:17 +0100

    feat(certif): add controller to handle delete member feature.
    Co-authored-by: Eric Lim <eric.lim@zenika.com>
    Co-authored-by: Ismael Gorissen <ismael.gorissen@gmail.com>

--- certif/tests/unit/controllers/authenticated/team/list/members_test.js
+++ certif/tests/unit/controllers/authenticated/team/list/members_test.js
@@ -9,9 +9,11 @@ module('Unit | Controller | authenticated/team/list/members', function (hooks) {
   setupIntl(hooks);
 
   let controller;
+  let store;
 
   hooks.beforeEach(function () {
     controller = this.owner.lookup('controller:authenticated/team/list/members');
+    store = this.owner.lookup('service:store');
   });
 
   hooks.afterEach(function () {
@@ -30,7 +32,26 @@ module('Unit | Controller | authenticated/team/list/members', function (hooks) {
       await controller.leaveCertificationCenter();
 
       // then
-      assert.true(currentUser.currentCertificationCenterMembership.destroyRecord.calledWith());
+      assert.true(currentUser.currentCertificationCenterMembership.destroyRecord.calledOnce);
+    });
+  });
+
+  module('#removeMember', function () {
+    test('deletes membership', async function (assert) {
+      // given
+      const member = store.createRecord('member', {
+        id: 1,
+        firstName: 'Matt',
+        lastName: 'Ador',
+        role: 'MEMBER',
+      });
+      sinon.stub(member, 'destroyRecord');
+
+      // when
+      await controller.removeMember(member);
+
+      // then
+      assert.true(member.destroyRecord.calledOnce);
     });
   });
 });
