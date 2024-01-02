dff5e896af1a3f15f6e093b8dfb86bf3c9f283ca
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2024-01-02 12:19:55 +0100

    feat(orga): remove access token to export url

--- orga/app/components/campaign/header/tabs.js
+++ orga/app/components/campaign/header/tabs.js
@@ -1,10 +1,19 @@
 import { service } from '@ember/service';
 import Component from '@glimmer/component';
-
+import { action } from '@ember/object';
 export default class CampaignTabs extends Component {
   @service intl;
+  @service notifications;
+  @service fileSaver;
+  @service session;
 
-  get downloadUrl() {
-    return this.args.campaign.urlToResult + `&lang=${this.intl.locale[0]}`;
+  @action
+  async exportData() {
+    try {
+      const token = this.session.data.authenticated.access_token;
+      await this.fileSaver.save({ url: this.args.campaign.urlToResult, token });
+    } catch (err) {
+      this.notifications.sendError(this.intl.t('api-error-messages.global'));
+    }
   }
 }
--- orga/tests/integration/components/campaign/header/tabs_test.js
+++ orga/tests/integration/components/campaign/header/tabs_test.js
@@ -1,39 +1,74 @@
 import { module, test } from 'qunit';
+import { click } from '@ember/test-helpers';
 import setupIntlRenderingTest from '../../../../helpers/setup-intl-rendering';
 import { render } from '@1024pix/ember-testing-library';
 import { hbs } from 'ember-cli-htmlbars';
+import sinon from 'sinon';
+import ENV from 'pix-orga/config/environment';
 import Service from '@ember/service';
-
-class CurrentUserStub extends Service {
-  prescriber = {
-    lang: 'fr',
-  };
-}
 
 module('Integration | Component | Campaign::Header::Tabs', function (hooks) {
   setupIntlRenderingTest(hooks);
 
-  let store;
+  const originalAppHost = ENV.APP.API_HOST;
+
+  hooks.afterEach(function () {
+    ENV.APP.API_HOST = originalAppHost;
+  });
+
+  let store, screen, fileSaver, notifications, access_token;
 
   hooks.beforeEach(function () {
+    class sessionService extends Service {
+      isAuthenticated = true;
+      data = {
+        authenticated: {
+          access_token,
+        },
+      };
+    }
+    this.owner.register('service:session', sessionService);
+
+    ENV.APP.API_HOST = 'https://myapp.com';
+    access_token = Symbol('ACCESS_TOKEN');
     store = this.owner.lookup('service:store');
-    this.owner.register('service:current-user', CurrentUserStub);
-    this.owner.lookup('service:current-user', CurrentUserStub);
+    this.owner.lookup('service:current-user');
+    fileSaver = this.owner.lookup('service:file-saver');
+    notifications = this.owner.lookup('service:notifications');
     this.owner.setupRouter();
+
+    sinon.stub(fileSaver, 'save');
+    sinon.stub(notifications, 'sendError');
+
+    fileSaver.save.resolves();
   });
 
   module('Common campaign navigation', function (hooks) {
     hooks.beforeEach(async function () {
       this.campaign = store.createRecord('campaign', { id: 12 });
-      await render(hbs`<Campaign::Header::Tabs @campaign={{this.campaign}} />`);
+      screen = await render(hbs`<Campaign::Header::Tabs @campaign={{this.campaign}} />`);
     });
 
     test('it should display campaign settings item', async function (assert) {
-      assert.dom('nav a[href="/campagnes/12/parametres"]').hasText('Paramètres');
+      const settingsLink = screen.getByRole('link', { name: this.intl.t('pages.campaign.tab.settings') });
+
+      assert.dom(settingsLink).hasAttribute('href', '/campagnes/12/parametres');
     });
 
     test('it should display activity item', async function (assert) {
-      assert.dom('nav a[href="/campagnes/12"]').hasText('Activité');
+      const activityLink = screen.getByRole('link', { name: this.intl.t('pages.campaign.tab.activity') });
+      assert.dom(activityLink).hasAttribute('href', '/campagnes/12');
+    });
+
+    test('it should display export button result', async function (assert) {
+      assert.ok(screen.getByRole('button', { name: this.intl.t('pages.campaign.actions.export-results') }));
+    });
+
+    test('dipslay notification error on data export', async function (assert) {
+      fileSaver.save.rejects();
+      await click(screen.getByRole('button', { name: this.intl.t('pages.campaign.actions.export-results') }));
+
+      assert.ok(notifications.sendError.calledWithExactly(this.intl.t('api-error-messages.global')));
     });
   });
 
@@ -45,15 +80,31 @@ module('Integration | Component | Campaign::Header::Tabs', function (hooks) {
         type: 'ASSESSMENT',
       });
 
-      await render(hbs`<Campaign::Header::Tabs @campaign={{this.campaign}} />`);
+      screen = await render(hbs`<Campaign::Header::Tabs @campaign={{this.campaign}} />`);
     });
 
     test('it should display evaluation results item', async function (assert) {
-      assert.dom('nav a[href="/campagnes/13/resultats-evaluation"]').hasText('Résultats (10)');
+      const resultsLink = screen.getByRole('link', { name: this.intl.t('pages.campaign.tab.results', { count: 10 }) });
+
+      assert.dom(resultsLink).hasAttribute('href', '/campagnes/13/resultats-evaluation');
     });
 
     test('it should display campaign analyse item', async function (assert) {
-      assert.dom('nav a[href="/campagnes/13/analyse"]').hasText('Analyse');
+      const resultsLink = screen.getByRole('link', { name: this.intl.t('pages.campaign.tab.review') });
+
+      assert.dom(resultsLink).hasAttribute('href', '/campagnes/13/analyse');
+    });
+
+    test('it should call export result with right context', async function (assert) {
+      await click(screen.getByRole('button', { name: this.intl.t('pages.campaign.actions.export-results') }));
+
+      assert.ok(notifications.sendError.notCalled);
+      assert.ok(
+        fileSaver.save.calledWithExactly({
+          url: `${ENV.APP.API_HOST}/api/campaigns/13/csv-assessment-results`,
+          token: access_token,
+        }),
+      );
     });
   });
 
@@ -66,23 +117,29 @@ module('Integration | Component | Campaign::Header::Tabs', function (hooks) {
         sharedParticipationsCount: 6,
       });
 
-      await render(hbs`<Campaign::Header::Tabs @campaign={{this.campaign}} />`);
+      screen = await render(hbs`<Campaign::Header::Tabs @campaign={{this.campaign}} />`);
     });
 
-    test('it should display profile results item', async function (assert) {
-      assert.dom('nav a[href="/campagnes/13/profils"]').hasText('Résultats (6)');
-    });
+    test('it should display  profile results item', async function (assert) {
+      const resultsLink = screen.getByRole('link', { name: this.intl.t('pages.campaign.tab.results', { count: 6 }) });
 
-    test('it should not display participation item', async function (assert) {
-      assert.dom('nav a[href="/campagnes/13/evaluations"]').doesNotExist();
+      assert.dom(resultsLink).hasAttribute('href', '/campagnes/13/profils');
     });
 
     test('it should not display analyse item', async function (assert) {
-      assert.dom('nav a[href="/campagnes/13/analyse"]').doesNotExist();
+      assert.notOk(screen.queryByRole('link', { name: this.intl.t('pages.campaign.tab.review') }));
     });
 
-    test('it should not display evaluation results item', async function (assert) {
-      assert.dom('nav a[href="/campagnes/13/resultats-evaluation"]').doesNotExist();
+    test('it should call export result with right context', async function (assert) {
+      await click(screen.getByRole('button', { name: this.intl.t('pages.campaign.actions.export-results') }));
+
+      assert.ok(notifications.sendError.notCalled);
+      assert.ok(
+        fileSaver.save.calledWithExactly({
+          url: `${ENV.APP.API_HOST}/api/campaigns/13/csv-profiles-collection-results`,
+          token: access_token,
+        }),
+      );
     });
   });
 });
