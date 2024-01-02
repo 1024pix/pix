f16e6e5c8a05757a66c7a65b47a7b1beefdff237
Antoine Ceol <antoine.ceol@gmail.com>
2024-01-02 22:22:55 +0100

    :recycle: certif: Avoid using token to get supervisor kit

--- certif/app/controllers/authenticated/sessions/details.js
+++ certif/app/controllers/authenticated/sessions/details.js
@@ -1,5 +1,6 @@
 import Controller from '@ember/controller';
 import { service } from '@ember/service';
+import { action } from '@ember/object';
 /* eslint-disable ember/no-computed-properties-in-native-classes*/
 import { computed } from '@ember/object';
 import { alias } from '@ember/object/computed';
@@ -9,12 +10,23 @@ export default class SessionsDetailsController extends Controller {
   @service currentUser;
   @service intl;
   @service url;
+  @service session;
+  @service notifications;
+  @service fileSaver;
 
-  @alias('model.session') session;
   @alias('model.certificationCandidates') certificationCandidates;
 
+  @action
+  async fetchInvigilatorKit() {
+    try {
+      const token = this.session.data.authenticated.access_token;
+      await this.fileSaver.save({ url: this.model.session.urlToDownloadSupervisorKitPdf, token });
+    } catch (err) {
+      this.notifications.error(this.intl.t('common.api-error-messages.internal-server-error'));
+    }
+  }
   get pageTitle() {
-    return `${this.intl.t('pages.sessions.detail.page-title')} | Session ${this.session.id} | Pix Certif`;
+    return `${this.intl.t('pages.sessions.detail.page-title')} | Session ${this.model.session.id} | Pix Certif`;
   }
 
   @computed('certificationCandidates.length')
@@ -39,7 +51,7 @@ export default class SessionsDetailsController extends Controller {
   }
 
   get urlToDownloadSessionIssueReportSheet() {
-    if (this.session.version === 3) {
+    if (this.model.session.version === 3) {
       return this.url.urlToDownloadSessionV3IssueReportSheet;
     }
     return this.url.urlToDownloadSessionIssueReportSheet;
