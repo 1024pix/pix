e4428a08d9a4724b6c7daedf9dbe1ab3bc52fd0e
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2024-01-03 15:02:26 +0100

    feat(api): add route to swap code

--- api/tests/prescription/campaign/acceptance/application/campaign-administration-controller_test.js
+++ api/tests/prescription/campaign/acceptance/application/campaign-administration-controller_test.js
@@ -276,4 +276,31 @@ describe('Acceptance | API | Campaign Administration Controller', function () {
       });
     });
   });
+
+  describe('POST /api/admin/campaigns/swap-codes', function () {
+    it('it swap campaign code', async function () {
+      const userId = databaseBuilder.factory.buildUser.withRole({ role: ROLES.SUPER_ADMIN }).id;
+      const organizationId = databaseBuilder.factory.buildOrganization().id;
+      const firstCampaignId = databaseBuilder.factory.buildCampaign({ code: 'ABC', organizationId }).id;
+      const secondCampaignId = databaseBuilder.factory.buildCampaign({ code: 'EFG', organizationId }).id;
+      await databaseBuilder.commit();
+
+      const payload = {
+        firstCampaignId,
+        secondCampaignId,
+      };
+
+      const options = {
+        method: 'POST',
+        url: '/api/admin/campaigns/swap-codes',
+        headers: {
+          authorization: generateValidRequestAuthorizationHeader(userId),
+        },
+        payload,
+      };
+
+      const response = await server.inject(options);
+      expect(response.statusCode).to.equal(204);
+    });
+  });
 });
