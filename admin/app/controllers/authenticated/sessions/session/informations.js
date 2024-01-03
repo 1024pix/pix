98ccbb12e150b2e3fdd84b1be64f732c772049d9
matthieu-octo <matthieu.gros@octo.com>
2024-01-03 15:52:57 +0100

    ✨admin: add unfinalize session button

--- admin/app/controllers/authenticated/sessions/session/informations.js
+++ admin/app/controllers/authenticated/sessions/session/informations.js
@@ -41,6 +41,16 @@ export default class IndexController extends Controller {
   }
 
   @action
+  async unfinalizeSession() {
+    try {
+      await this.sessionModel.save({ adapterOptions: { unfinalize: true } });
+      this.notifications.success('La session a bien été définalisée');
+    } catch (err) {
+      this.notifications.error('Erreur lors de la définalisation de la session');
+    }
+  }
+
+  @action
   async checkForAssignment() {
     if (this.isAssigned) {
       this.isShowingAssignmentModal = true;
