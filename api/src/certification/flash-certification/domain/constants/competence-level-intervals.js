045112e91b345429ae3037aa5301eceb5ed96043
Alexandre COIN <alexandre@mbpdealexandre-1.home>
2024-01-04 11:59:42 +0100

    :sparkles: api: calculate level for each competence

---
+++ api/src/certification/flash-certification/domain/constants/competence-level-intervals.js
@@ -0,0 +1,922 @@
+export const competenceLevelIntervals = [
+  {
+    competence: '1.1',
+    values: [
+      {
+        bounds: {
+          max: -1.4290127754211426,
+          min: -5.3399882316589355,
+        },
+        competenceLevel: 0,
+      },
+      {
+        bounds: {
+          max: 0.4440346658229828,
+          min: -1.4290127754211426,
+        },
+        competenceLevel: 1,
+      },
+      {
+        bounds: {
+          max: 1.5982208251953125,
+          min: 0.4440346658229828,
+        },
+        competenceLevel: 2,
+      },
+      {
+        bounds: {
+          max: 1.6521581411361694,
+          min: 1.5982208251953125,
+        },
+        competenceLevel: 3,
+      },
+      {
+        bounds: {
+          max: 2.6340444087982178,
+          min: 1.6521581411361694,
+        },
+        competenceLevel: 4,
+      },
+      {
+        bounds: {
+          max: 3.2923648357391357,
+          min: 2.6340444087982178,
+        },
+        competenceLevel: 5,
+      },
+      {
+        bounds: {
+          max: 5.012345314025879,
+          min: 3.2923648357391357,
+        },
+        competenceLevel: 6,
+      },
+      {
+        bounds: {
+          max: 7.374109745025635,
+          min: 5.012345314025879,
+        },
+        competenceLevel: 7,
+      },
+    ],
+  },
+  {
+    competence: '1.2',
+    values: [
+      {
+        bounds: {
+          max: -1.816159963607788,
+          min: -5.3399882316589355,
+        },
+        competenceLevel: 0,
+      },
+      {
+        bounds: {
+          max: -0.9718785285949707,
+          min: -1.816159963607788,
+        },
+        competenceLevel: 1,
+      },
+      {
+        bounds: {
+          max: 0.9403377175331116,
+          min: -0.9718785285949707,
+        },
+        competenceLevel: 2,
+      },
+      {
+        bounds: {
+          max: 2.816800832748413,
+          min: 0.9403377175331116,
+        },
+        competenceLevel: 3,
+      },
+      {
+        bounds: {
+          max: 2.167752742767334,
+          min: 2.816800832748413,
+        },
+        competenceLevel: 4,
+      },
+      {
+        bounds: {
+          max: 5.497085094451904,
+          min: 2.167752742767334,
+        },
+        competenceLevel: 5,
+      },
+      {
+        bounds: {
+          max: 6.849133491516113,
+          min: 5.497085094451904,
+        },
+        competenceLevel: 6,
+      },
+      {
+        bounds: {
+          max: 7.374109745025635,
+          min: 6.849133491516113,
+        },
+        competenceLevel: 7,
+      },
+    ],
+  },
+  {
+    competence: '1.3',
+    values: [
+      {
+        bounds: {
+          max: -0.6393653750419617,
+          min: -5.3399882316589355,
+        },
+        competenceLevel: 0,
+      },
+      {
+        bounds: {
+          max: 0.24656331539154053,
+          min: -0.6393653750419617,
+        },
+        competenceLevel: 1,
+      },
+      {
+        bounds: {
+          max: 0.8987030982971191,
+          min: 0.24656331539154053,
+        },
+        competenceLevel: 2,
+      },
+      {
+        bounds: {
+          max: 1.7216020822525024,
+          min: 0.8987030982971191,
+        },
+        competenceLevel: 3,
+      },
+      {
+        bounds: {
+          max: 2.062549114227295,
+          min: 1.7216020822525024,
+        },
+        competenceLevel: 4,
+      },
+      {
+        bounds: {
+          max: 2.6246471405029297,
+          min: 2.062549114227295,
+        },
+        competenceLevel: 5,
+      },
+      {
+        bounds: {
+          max: 3.2087042331695557,
+          min: 2.6246471405029297,
+        },
+        competenceLevel: 6,
+      },
+      {
+        bounds: {
+          max: 7.374109745025635,
+          min: 3.2087042331695557,
+        },
+        competenceLevel: 7,
+      },
+    ],
+  },
+  {
+    competence: '2.1',
+    values: [
+      {
+        bounds: {
+          max: -1.61019766330719,
+          min: -5.3399882316589355,
+        },
+        competenceLevel: 0,
+      },
+      {
+        bounds: {
+          max: -0.22345975041389465,
+          min: -1.61019766330719,
+        },
+        competenceLevel: 1,
+      },
+      {
+        bounds: {
+          max: 0.19168642163276672,
+          min: -0.22345975041389465,
+        },
+        competenceLevel: 2,
+      },
+      {
+        bounds: {
+          max: 1.6601377725601196,
+          min: 0.19168642163276672,
+        },
+        competenceLevel: 3,
+      },
+      {
+        bounds: {
+          max: 3.844524621963501,
+          min: 1.6601377725601196,
+        },
+        competenceLevel: 4,
+      },
+      {
+        bounds: {
+          max: 3.3029592037200928,
+          min: 3.844524621963501,
+        },
+        competenceLevel: 5,
+      },
+      {
+        bounds: {
+          max: 4.262047290802002,
+          min: 3.3029592037200928,
+        },
+        competenceLevel: 6,
+      },
+      {
+        bounds: {
+          max: 7.374109745025635,
+          min: 4.262047290802002,
+        },
+        competenceLevel: 7,
+      },
+    ],
+  },
+  {
+    competence: '2.2',
+    values: [
+      {
+        bounds: {
+          max: -1.6842528581619263,
+          min: -5.3399882316589355,
+        },
+        competenceLevel: 0,
+      },
+      {
+        bounds: {
+          max: -0.1598442941904068,
+          min: -1.6842528581619263,
+        },
+        competenceLevel: 1,
+      },
+      {
+        bounds: {
+          max: 1.3476699590682983,
+          min: -0.1598442941904068,
+        },
+        competenceLevel: 2,
+      },
+      {
+        bounds: {
+          max: 0.5933959484100342,
+          min: 1.3476699590682983,
+        },
+        competenceLevel: 3,
+      },
+      {
+        bounds: {
+          max: 1.5587767362594604,
+          min: 0.5933959484100342,
+        },
+        competenceLevel: 4,
+      },
+      {
+        bounds: {
+          max: 6.855661869049072,
+          min: 1.5587767362594604,
+        },
+        competenceLevel: 5,
+      },
+      {
+        bounds: {
+          max: 7.374109745025635,
+          min: 6.855661869049072,
+        },
+        competenceLevel: 6,
+      },
+    ],
+  },
+  {
+    competence: '2.3',
+    values: [
+      {
+        bounds: {
+          max: -1.290342926979065,
+          min: -5.3399882316589355,
+        },
+        competenceLevel: 0,
+      },
+      {
+        bounds: {
+          max: -0.7296794056892395,
+          min: -1.290342926979065,
+        },
+        competenceLevel: 1,
+      },
+      {
+        bounds: {
+          max: 1.8645321130752563,
+          min: -0.7296794056892395,
+        },
+        competenceLevel: 2,
+      },
+      {
+        bounds: {
+          max: 2.075043201446533,
+          min: 1.8645321130752563,
+        },
+        competenceLevel: 3,
+      },
+      {
+        bounds: {
+          max: 1.5695146322250366,
+          min: 2.075043201446533,
+        },
+        competenceLevel: 4,
+      },
+      {
+        bounds: {
+          max: 4.602512359619141,
+          min: 1.5695146322250366,
+        },
+        competenceLevel: 5,
+      },
+      {
+        bounds: {
+          max: 7.374109745025635,
+          min: 4.602512359619141,
+        },
+        competenceLevel: 6,
+      },
+    ],
+  },
+  {
+    competence: '2.4',
+    values: [
+      {
+        bounds: {
+          max: -2.0458545684814453,
+          min: -5.3399882316589355,
+        },
+        competenceLevel: 0,
+      },
+      {
+        bounds: {
+          max: 0.6524333357810974,
+          min: -2.0458545684814453,
+        },
+        competenceLevel: 1,
+      },
+      {
+        bounds: {
+          max: 1.3245508670806885,
+          min: 0.6524333357810974,
+        },
+        competenceLevel: 2,
+      },
+      {
+        bounds: {
+          max: 1.8602700233459473,
+          min: 1.3245508670806885,
+        },
+        competenceLevel: 3,
+      },
+      {
+        bounds: {
+          max: 3.5241336822509766,
+          min: 1.8602700233459473,
+        },
+        competenceLevel: 4,
+      },
+      {
+        bounds: {
+          max: 4.221365928649902,
+          min: 3.5241336822509766,
+        },
+        competenceLevel: 5,
+      },
+      {
+        bounds: {
+          max: 7.374109745025635,
+          min: 4.221365928649902,
+        },
+        competenceLevel: 6,
+      },
+    ],
+  },
+  {
+    competence: '3.1',
+    values: [
+      {
+        bounds: {
+          max: -1.3573087453842163,
+          min: -5.3399882316589355,
+        },
+        competenceLevel: 0,
+      },
+      {
+        bounds: {
+          max: -0.4166012108325958,
+          min: -1.3573087453842163,
+        },
+        competenceLevel: 1,
+      },
+      {
+        bounds: {
+          max: 1.6104117631912231,
+          min: -0.4166012108325958,
+        },
+        competenceLevel: 2,
+      },
+      {
+        bounds: {
+          max: 1.7363076210021973,
+          min: 1.6104117631912231,
+        },
+        competenceLevel: 3,
+      },
+      {
+        bounds: {
+          max: 2.7112088203430176,
+          min: 1.7363076210021973,
+        },
+        competenceLevel: 4,
+      },
+      {
+        bounds: {
+          max: 3.0218923091888428,
+          min: 2.7112088203430176,
+        },
+        competenceLevel: 5,
+      },
+      {
+        bounds: {
+          max: 7.374109745025635,
+          min: 3.0218923091888428,
+        },
+        competenceLevel: 6,
+      },
+    ],
+  },
+  {
+    competence: '3.2',
+    values: [
+      {
+        bounds: {
+          max: -2.3695690631866455,
+          min: -5.3399882316589355,
+        },
+        competenceLevel: 0,
+      },
+      {
+        bounds: {
+          max: -0.374176025390625,
+          min: -2.3695690631866455,
+        },
+        competenceLevel: 1,
+      },
+      {
+        bounds: {
+          max: 0.11621341854333878,
+          min: -0.374176025390625,
+        },
+        competenceLevel: 2,
+      },
+      {
+        bounds: {
+          max: 1.95041823387146,
+          min: 0.11621341854333878,
+        },
+        competenceLevel: 3,
+      },
+      {
+        bounds: {
+          max: 4.512263774871826,
+          min: 1.95041823387146,
+        },
+        competenceLevel: 4,
+      },
+      {
+        bounds: {
+          max: 3.2162301540374756,
+          min: 4.512263774871826,
+        },
+        competenceLevel: 5,
+      },
+      {
+        bounds: {
+          max: 7.374109745025635,
+          min: 3.2162301540374756,
+        },
+        competenceLevel: 6,
+      },
+    ],
+  },
+  {
+    competence: '3.3',
+    values: [
+      {
+        bounds: {
+          max: -1.4244885444641113,
+          min: -5.3399882316589355,
+        },
+        competenceLevel: 0,
+      },
+      {
+        bounds: {
+          max: 1.598168969154358,
+          min: -1.4244885444641113,
+        },
+        competenceLevel: 1,
+      },
+      {
+        bounds: {
+          max: 0.8326226472854614,
+          min: 1.598168969154358,
+        },
+        competenceLevel: 2,
+      },
+      {
+        bounds: {
+          max: 2.065073251724243,
+          min: 0.8326226472854614,
+        },
+        competenceLevel: 3,
+      },
+      {
+        bounds: {
+          max: 2.385699510574341,
+          min: 2.065073251724243,
+        },
+        competenceLevel: 4,
+      },
+      {
+        bounds: {
+          max: 3.189551830291748,
+          min: 2.385699510574341,
+        },
+        competenceLevel: 5,
+      },
+      {
+        bounds: {
+          max: 7.374109745025635,
+          min: 3.189551830291748,
+        },
+        competenceLevel: 6,
+      },
+    ],
+  },
+  {
+    competence: '3.4',
+    values: [
+      {
+        bounds: {
+          max: 2.1766011714935303,
+          min: -5.3399882316589355,
+        },
+        competenceLevel: 0,
+      },
+      {
+        bounds: {
+          max: -0.1402435302734375,
+          min: 2.1766011714935303,
+        },
+        competenceLevel: 1,
+      },
+      {
+        bounds: {
+          max: 1.49162757396698,
+          min: -0.1402435302734375,
+        },
+        competenceLevel: 2,
+      },
+      {
+        bounds: {
+          max: 1.632764458656311,
+          min: 1.49162757396698,
+        },
+        competenceLevel: 3,
+      },
+      {
+        bounds: {
+          max: 2.2066235542297363,
+          min: 1.632764458656311,
+        },
+        competenceLevel: 4,
+      },
+      {
+        bounds: {
+          max: 2.659951686859131,
+          min: 2.2066235542297363,
+        },
+        competenceLevel: 5,
+      },
+      {
+        bounds: {
+          max: 2.088264226913452,
+          min: 2.659951686859131,
+        },
+        competenceLevel: 6,
+      },
+      {
+        bounds: {
+          max: 7.374109745025635,
+          min: 2.088264226913452,
+        },
+        competenceLevel: 7,
+      },
+    ],
+  },
+  {
+    competence: '4.1',
+    values: [
+      {
+        bounds: {
+          max: -0.5992878079414368,
+          min: -5.3399882316589355,
+        },
+        competenceLevel: 0,
+      },
+      {
+        bounds: {
+          max: 0.34635910391807556,
+          min: -0.5992878079414368,
+        },
+        competenceLevel: 1,
+      },
+      {
+        bounds: {
+          max: 0.5798754096031189,
+          min: 0.34635910391807556,
+        },
+        competenceLevel: 2,
+      },
+      {
+        bounds: {
+          max: 3.021275520324707,
+          min: 0.5798754096031189,
+        },
+        competenceLevel: 3,
+      },
+      {
+        bounds: {
+          max: 3.289098024368286,
+          min: 3.021275520324707,
+        },
+        competenceLevel: 4,
+      },
+      {
+        bounds: {
+          max: 3.9777281284332275,
+          min: 3.289098024368286,
+        },
+        competenceLevel: 5,
+      },
+      {
+        bounds: {
+          max: 7.374109745025635,
+          min: 3.9777281284332275,
+        },
+        competenceLevel: 6,
+      },
+    ],
+  },
+  {
+    competence: '4.2',
+    values: [
+      {
+        bounds: {
+          max: -0.5439767837524414,
+          min: -5.3399882316589355,
+        },
+        competenceLevel: 0,
+      },
+      {
+        bounds: {
+          max: -0.2565813958644867,
+          min: -0.5439767837524414,
+        },
+        competenceLevel: 1,
+      },
+      {
+        bounds: {
+          max: 1.3130837678909302,
+          min: -0.2565813958644867,
+        },
+        competenceLevel: 2,
+      },
+      {
+        bounds: {
+          max: 3.375800371170044,
+          min: 1.3130837678909302,
+        },
+        competenceLevel: 3,
+      },
+      {
+        bounds: {
+          max: 2.1648192405700684,
+          min: 3.375800371170044,
+        },
+        competenceLevel: 4,
+      },
+      {
+        bounds: {
+          max: 3.933257579803467,
+          min: 2.1648192405700684,
+        },
+        competenceLevel: 5,
+      },
+      {
+        bounds: {
+          max: 4.868575096130371,
+          min: 3.933257579803467,
+        },
+        competenceLevel: 6,
+      },
+      {
+        bounds: {
+          max: 7.374109745025635,
+          min: 4.868575096130371,
+        },
+        competenceLevel: 7,
+      },
+    ],
+  },
+  {
+    competence: '4.3',
+    values: [
+      {
+        bounds: {
+          max: -1.5177412033081055,
+          min: -5.3399882316589355,
+        },
+        competenceLevel: 0,
+      },
+      {
+        bounds: {
+          max: -0.6453593969345093,
+          min: -1.5177412033081055,
+        },
+        competenceLevel: 1,
+      },
+      {
+        bounds: {
+          max: 1.5937426090240479,
+          min: -0.6453593969345093,
+        },
+        competenceLevel: 2,
+      },
+      {
+        bounds: {
+          max: 2.0802462100982666,
+          min: 1.5937426090240479,
+        },
+        competenceLevel: 3,
+      },
+      {
+        bounds: {
+          max: 3.965420722961426,
+          min: 2.0802462100982666,
+        },
+        competenceLevel: 4,
+      },
+      {
+        bounds: {
+          max: 4.159452438354492,
+          min: 3.965420722961426,
+        },
+        competenceLevel: 5,
+      },
+      {
+        bounds: {
+          max: 7.374109745025635,
+          min: 4.159452438354492,
+        },
+        competenceLevel: 6,
+      },
+    ],
+  },
+  {
+    competence: '5.1',
+    values: [
+      {
+        bounds: {
+          max: -1.9220428466796875,
+          min: -5.3399882316589355,
+        },
+        competenceLevel: 0,
+      },
+      {
+        bounds: {
+          max: -2.1660995483398438,
+          min: -1.9220428466796875,
+        },
+        competenceLevel: 1,
+      },
+      {
+        bounds: {
+          max: 0.7407174706459045,
+          min: -2.1660995483398438,
+        },
+        competenceLevel: 2,
+      },
+      {
+        bounds: {
+          max: 1.57998526096344,
+          min: 0.7407174706459045,
+        },
+        competenceLevel: 3,
+      },
+      {
+        bounds: {
+          max: 2.226930856704712,
+          min: 1.57998526096344,
+        },
+        competenceLevel: 4,
+      },
+      {
+        bounds: {
+          max: 4.622072219848633,
+          min: 2.226930856704712,
+        },
+        competenceLevel: 5,
+      },
+      {
+        bounds: {
+          max: 4.048096656799316,
+          min: 4.622072219848633,
+        },
+        competenceLevel: 6,
+      },
+      {
+        bounds: {
+          max: 7.374109745025635,
+          min: 4.048096656799316,
+        },
+        competenceLevel: 7,
+      },
+    ],
+  },
+  {
+    competence: '5.2',
+    values: [
+      {
+        bounds: {
+          max: -0.8218916654586792,
+          min: -5.3399882316589355,
+        },
+        competenceLevel: 0,
+      },
+      {
+        bounds: {
+          max: -0.20230929553508759,
+          min: -0.8218916654586792,
+        },
+        competenceLevel: 1,
+      },
+      {
+        bounds: {
+          max: 0.15905971825122833,
+          min: -0.20230929553508759,
+        },
+        competenceLevel: 2,
+      },
+      {
+        bounds: {
+          max: 1.8799850940704346,
+          min: 0.15905971825122833,
+        },
+        competenceLevel: 3,
+      },
+      {
+        bounds: {
+          max: 2.9971206188201904,
+          min: 1.8799850940704346,
+        },
+        competenceLevel: 4,
+      },
+      {
+        bounds: {
+          max: 5.060043811798096,
+          min: 2.9971206188201904,
+        },
+        competenceLevel: 5,
+      },
+      {
+        bounds: {
+          max: 4.473289489746094,
+          min: 5.060043811798096,
+        },
+        competenceLevel: 6,
+      },
+      {
+        bounds: {
+          max: 7.374109745025635,
+          min: 4.473289489746094,
+        },
+        competenceLevel: 7,
+      },
+    ],
+  },
+];
--- api/tests/unit/domain/models/CertificationAssessmentScoreV3_test.js
+++ api/tests/unit/domain/models/CertificationAssessmentScoreV3_test.js
@@ -109,6 +109,61 @@ describe('Unit | Domain | Models | CertificationAssessmentScoreV3 ', function ()
 
       expect(score.nbPix).to.equal(expectedScoreForEstimatedLevel);
     });
+
+    it('should return a level for all competences', function () {
+      const expectedEstimatedLevel = 2;
+      const numberOfQuestions = 32;
+
+      const challenges = _buildChallenges(0, numberOfQuestions);
+      const allAnswers = _buildAnswersForChallenges(challenges, AnswerStatus.OK);
+
+      answerRepository.findByAssessment.withArgs(assessmentId).resolves(baseAnswers);
+      challengeRepository.findFlashCompatible.withArgs().resolves(baseChallenges);
+      algorithm.getEstimatedLevelAndErrorRate
+        .withArgs({
+          challenges,
+          allAnswers,
+        })
+        .returns({
+          estimatedLevel: expectedEstimatedLevel,
+        });
+
+      algorithm.getConfiguration.returns(
+        domainBuilder.buildFlashAlgorithmConfiguration({
+          maximumAssessmentLength: numberOfQuestions,
+        }),
+      );
+
+      const expectedCompetenceLevels = {
+        1.1: 4,
+        1.2: 3,
+        1.3: 4,
+        2.1: 4,
+        2.2: 5,
+        2.3: 3,
+        2.4: 4,
+        3.1: 4,
+        3.2: 4,
+        3.3: 3,
+        3.4: 0,
+        4.1: 3,
+        4.2: 3,
+        4.3: 3,
+        5.1: 4,
+        5.2: 4,
+      };
+
+      // when
+      const score = CertificationAssessmentScoreV3.fromChallengesAndAnswers({
+        challenges,
+        allAnswers,
+        algorithm,
+        maxReachableLevelOnCertificationDate,
+      });
+
+      // then
+      expect(score.competenceLevels).to.deep.equal(expectedCompetenceLevels);
+    });
   });
 
   describe('when the candidate did not finish the test', function () {
@@ -225,6 +280,55 @@ describe('Unit | Domain | Models | CertificationAssessmentScoreV3 ', function ()
       // then
       expect(score.nbPix).to.equal(0);
     });
+
+    it('should return the minimum level for all competences', function () {
+      // given
+      const veryLowEstimatedLevel = -9;
+      const veryEasyDifficulty = -8;
+      const numberOfChallenges = 32;
+      const challenges = _buildChallenges(veryEasyDifficulty, numberOfChallenges);
+      const allAnswers = _buildAnswersForChallenges(challenges, AnswerStatus.KO);
+
+      algorithm.getEstimatedLevelAndErrorRate
+        .withArgs({
+          challenges,
+          allAnswers,
+        })
+        .returns({
+          estimatedLevel: veryLowEstimatedLevel,
+        });
+      algorithm.getConfiguration.returns(domainBuilder.buildFlashAlgorithmConfiguration());
+
+      const expectedCompetenceLevels = {
+        1.1: 0,
+        1.2: 0,
+        1.3: 0,
+        2.1: 0,
+        2.2: 0,
+        2.3: 0,
+        2.4: 0,
+        3.1: 0,
+        3.2: 0,
+        3.3: 0,
+        3.4: 0,
+        4.1: 0,
+        4.2: 0,
+        4.3: 0,
+        5.1: 0,
+        5.2: 0,
+      };
+
+      // when
+      const score = CertificationAssessmentScoreV3.fromChallengesAndAnswers({
+        challenges,
+        allAnswers,
+        algorithm,
+        maxReachableLevelOnCertificationDate,
+      });
+
+      // then
+      expect(score.competenceLevels).to.deep.equal(expectedCompetenceLevels);
+    });
   });
 
   describe('when we reach an estimated level above the MAXIMUM', function () {
@@ -256,6 +360,55 @@ describe('Unit | Domain | Models | CertificationAssessmentScoreV3 ', function ()
 
       // then
       expect(score.nbPix).to.equal(896);
+    });
+
+    it('return the maximum level for all competences', function () {
+      // given
+      const veryHighEstimatedLevel = 1200;
+      const veryHardDifficulty = 8;
+      const numberOfChallenges = 32;
+      const challenges = _buildChallenges(veryHardDifficulty, numberOfChallenges);
+      const allAnswers = _buildAnswersForChallenges(challenges, AnswerStatus.OK);
+
+      algorithm.getEstimatedLevelAndErrorRate
+        .withArgs({
+          challenges,
+          allAnswers,
+        })
+        .returns({
+          estimatedLevel: veryHighEstimatedLevel,
+        });
+      algorithm.getConfiguration.returns(domainBuilder.buildFlashAlgorithmConfiguration());
+
+      const expectedCompetenceLevels = {
+        1.1: 7,
+        1.2: 7,
+        1.3: 7,
+        2.1: 7,
+        2.2: 6,
+        2.3: 6,
+        2.4: 6,
+        3.1: 6,
+        3.2: 6,
+        3.3: 6,
+        3.4: 7,
+        4.1: 6,
+        4.2: 7,
+        4.3: 6,
+        5.1: 7,
+        5.2: 7,
+      };
+
+      // when
+      const score = CertificationAssessmentScoreV3.fromChallengesAndAnswers({
+        challenges,
+        allAnswers,
+        algorithm,
+        maxReachableLevelOnCertificationDate,
+      });
+
+      // then
+      expect(score.competenceLevels).to.deep.equal(expectedCompetenceLevels);
     });
   });
 
