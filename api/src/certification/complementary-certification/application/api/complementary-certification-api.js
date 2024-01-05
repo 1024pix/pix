71757ced4367a821581d9f1164b2dffd1258dfbd
Steph0 <Steph0@users.noreply.github.com>
2024-01-05 14:31:10 +0100

    :recycle: api: deletion of unused API assertion

--- api/src/certification/complementary-certification/application/api/complementary-certification-api.js
+++ api/src/certification/complementary-certification/application/api/complementary-certification-api.js
@@ -12,16 +12,8 @@ import { ComplementaryCertification } from './models/ComplementaryCertification.
  * @param {number} params.id mandatory
  *
  * @throws {NotFoundError} Complementary certification does not exist
- * @throws {Error} Id parameter was not provided
  */
 export const getById = async ({ id }) => {
-  _assertIdExists(id);
   const complementaryCertification = await usecases.getById({ id });
   return new ComplementaryCertification(complementaryCertification);
-};
-
-const _assertIdExists = (id) => {
-  if (!id) {
-    throw new Error('Complementary certification id parameter is mandatory');
-  }
 };
