859a00669d0b0827d7eedaabf2becd4587473d4d
Steph0 <Steph0@users.noreply.github.com>
2024-01-05 16:44:01 +0100

    :recycle: api: create missing Acceptance test for session mass import

--- api/tests/certification/session/acceptance/application/session-mass-import-route_test.js
+++ api/tests/certification/session/acceptance/application/session-mass-import-route_test.js
@@ -263,6 +263,48 @@ describe('Acceptance | Controller | Session | session-mass-import-route', functi
       });
     });
   });
+
+  describe('GET /api/certification-centers/{certificationCenterId}/import', function () {
+    const BOM_CHAR = '\ufeff';
+    it('should return the CSV template', async function () {
+      // given
+      const userId = databaseBuilder.factory.buildUser().id;
+      const { id: certificationCenterId } = databaseBuilder.factory.buildCertificationCenter({
+        type: 'SCO',
+        externalId: '1234AB',
+        isV3Pilot: false,
+      });
+      const { id: complementaryCertificationId } = databaseBuilder.factory.buildComplementaryCertification.droit({});
+      databaseBuilder.factory.buildComplementaryCertificationHabilitation({
+        certificationCenterId,
+        complementaryCertificationId,
+      });
+      databaseBuilder.factory.buildOrganization({ externalId: '1234AB', isManagingStudents: false, type: 'SUP' });
+      databaseBuilder.factory.buildCertificationCenterMembership({
+        userId,
+        certificationCenterId,
+      });
+      await databaseBuilder.commit();
+      const server = await createServer();
+
+      const options = {
+        method: 'GET',
+        url: `/api/certification-centers/${certificationCenterId}/import`,
+        headers: {
+          authorization: generateValidRequestAuthorizationHeader(userId),
+        },
+      };
+
+      // when
+      const response = await server.inject(options);
+
+      // then
+      expect(response.statusCode).to.equal(200);
+      expect(response.payload).to.equal(
+        `${BOM_CHAR}"Numéro de session préexistante";"* Nom du site";"* Nom de la salle";"* Date de début (format: JJ/MM/AAAA)";"* Heure de début (heure locale format: HH:MM)";"* Surveillant(s)";"Observations (optionnel)";"* Nom de naissance";"* Prénom";"* Date de naissance (format: JJ/MM/AAAA)";"* Sexe (M ou F)";"Code INSEE de la commune de naissance";"Code postal de la commune de naissance";"Nom de la commune de naissance";"* Pays de naissance";"E-mail du destinataire des résultats (formateur, enseignant…)";"E-mail de convocation";"Identifiant externe";"Temps majoré ? (exemple format: 33%)";"Pix+ Droit ('oui' ou laisser vide)"`,
+      );
+    });
+  });
 });
 
 function _checkIfValidUUID(str) {
