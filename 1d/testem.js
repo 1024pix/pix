df2c47e35f2d4872e46cc5ca25569638e0cad1ff
Antoine Ceol <antoine.ceol@gmail.com>
2024-01-08 21:35:46 +0100

    :recycle: admin: update  testem config for ci

--- 1d/testem.js
+++ 1d/testem.js
@@ -1,23 +1,29 @@
-'use strict';
-
-module.exports = {
+const config = {
   test_page: 'tests/index.html?hidepassed',
+  reporter: 'dot',
   disable_watching: true,
   launch_in_ci: ['Chrome'],
   launch_in_dev: ['Chrome'],
   browser_start_timeout: 120,
   browser_args: {
-    Chrome: {
-      ci: [
-        // --no-sandbox is needed when running Chrome inside a container
-        process.env.CI ? '--no-sandbox' : null,
-        '--headless',
-        '--disable-dev-shm-usage',
-        '--disable-software-rasterizer',
-        '--mute-audio',
-        '--remote-debugging-port=0',
-        '--window-size=1440,900',
-      ].filter(Boolean),
-    },
+    Chrome: [
+      // --no-sandbox is needed when running Chrome inside a container
+      process.env.CI ? '--no-sandbox' : null,
+      '--headless',
+      '--disable-dev-shm-usage',
+      '--disable-software-rasterizer',
+      '--mute-audio',
+      '--remote-debugging-port=0',
+      '--window-size=1440,900',
+    ].filter(Boolean),
   },
+  browser_disconnect_timeout: 20000,
+  timeout: 20000,
 };
+
+module.exports = process.env.CI
+  ? {
+      ...config,
+      reporter: 'xunit',
+    }
+  : config;
