a44602ca2e5f9912ab64ce74009eade948aca381
Ismael Gorissen <ismael.gorissen@gmail.com>
2024-01-09 10:13:54 +0100

    feat(api): add findChildrenOrganizationsForAdmin method
    in organizations controller

--- api/tests/unit/application/organizations/organization-controller_test.js
+++ api/tests/unit/application/organizations/organization-controller_test.js
@@ -663,4 +663,31 @@ describe('Unit | Application | Organizations | organization-controller', functio
       expect(response).to.deep.equal(archivedOrganizationSerialized);
     });
   });
+
+  describe('#findChildrenOrganizationsForAdmin', function () {
+    it('calls findChildrenOrganizationsForAdmin usecase and returns a serialized list of organizations', async function () {
+      // given
+      const parentOrganizationId = 1;
+      const organizations = Symbol('child organizations list');
+      const childOrganizationsSerialized = Symbol('child organizations serialized list');
+      const dependencies = {
+        organizationForAdminSerializer: { serialize: sinon.stub() },
+      };
+
+      sinon.stub(usecases, 'findChildrenOrganizationsForAdmin').resolves(organizations);
+      dependencies.organizationForAdminSerializer.serialize.resolves(childOrganizationsSerialized);
+
+      const request = {
+        params: { organizationId: parentOrganizationId },
+      };
+
+      // when
+      const response = await organizationController.findChildrenOrganizationsForAdmin(request, hFake, dependencies);
+
+      // then
+      expect(usecases.findChildrenOrganizationsForAdmin).to.have.been.calledWithExactly({ parentOrganizationId });
+      expect(dependencies.organizationForAdminSerializer.serialize).to.have.been.calledWithExactly(organizations);
+      expect(response).to.deep.equal(childOrganizationsSerialized);
+    });
+  });
 });
