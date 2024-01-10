837aa1a75e5a00abc00cb789dafc35c19255775d
dlahaye <dimitrilahaye.dev@gmail.com>
2024-01-10 15:22:29 +0100

    feat(api): add passage-answer route
    Co-authored-by: Vincent Hardouin <vincent.hardouin@pix.fr>
    Co-authored-by: miranda.lin-guignard <miranda.lin-guignard@pix.fr>

--- api/src/devcomp/application/http-error-mapper-configuration.js
+++ api/src/devcomp/application/http-error-mapper-configuration.js
@@ -1,10 +1,16 @@
 import { HttpErrors } from '../../shared/application/http-errors.js';
-import { ModuleDoesNotExistError } from '../domain/errors.js';
+import { ModuleDoesNotExistError, PassageDoesNotExistError } from '../domain/errors.js';
 import { DomainErrorMappingConfiguration } from '../../shared/application/models/domain-error-mapping-configuration.js';
 
 const devcompDomainErrorMappingConfiguration = [
   {
     name: ModuleDoesNotExistError.name,
+    httpErrorFn: (error) => {
+      return new HttpErrors.UnprocessableEntityError(error.message, error.code, error.meta);
+    },
+  },
+  {
+    name: PassageDoesNotExistError.name,
     httpErrorFn: (error) => {
       return new HttpErrors.UnprocessableEntityError(error.message, error.code, error.meta);
     },
--- api/tests/devcomp/integration/application/passages/index_test.js
+++ api/tests/devcomp/integration/application/passages/index_test.js
@@ -1,9 +1,9 @@
 import { expect, HttpTestServer, sinon } from '../../../../test-helper.js';
 import * as moduleUnderTest from '../../../../../src/devcomp/application/passages/index.js';
 import { passageController } from '../../../../../src/devcomp/application/passages/controller.js';
-import { ModuleDoesNotExistError } from '../../../../../src/devcomp/domain/errors.js';
+import { ModuleDoesNotExistError, PassageDoesNotExistError } from '../../../../../src/devcomp/domain/errors.js';
 
-describe('Unit | Devcomp | Application | Passage | Router | passage-router', function () {
+describe('Integration | Devcomp | Application | Passage | Router | passage-router', function () {
   describe('POST /api/passages/', function () {
     describe('when controller throw a ModuleDoesNotExistError', function () {
       it('should return a 422', async function () {
@@ -21,6 +21,31 @@ describe('Unit | Devcomp | Application | Passage | Router | passage-router', fun
 
         // when
         const response = await httpTestServer.request('POST', '/api/passages', invalidPayload);
+
+        // then
+        expect(response.statusCode).to.equal(422);
+      });
+    });
+  });
+
+  describe('POST /api/passages/{passageId}/answers', function () {
+    describe('when controller throw a PassageDoesNotExistError', function () {
+      it('should return a 422', async function () {
+        // given
+        sinon.stub(passageController, 'verifyAndSaveAnswer').throws(new PassageDoesNotExistError());
+        const httpTestServer = new HttpTestServer();
+        await httpTestServer.register(moduleUnderTest);
+        const payload = {
+          data: {
+            attributes: {
+              'element-id': '69257809-d0fe-44c0-9a47-cbd89d9cbdc6',
+              'user-response': ['user-response'],
+            },
+          },
+        };
+
+        // when
+        const response = await httpTestServer.request('POST', '/api/passages/123/answers', payload);
 
         // then
         expect(response.statusCode).to.equal(422);
