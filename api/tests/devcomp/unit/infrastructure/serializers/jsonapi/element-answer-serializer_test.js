5b4b4ed8ea6fc4777efd4a00b75509255a05d10d
Vincent Hardouin <vincent.hardouin@pix.fr>
2024-01-10 11:16:40 +0100

    feat(api): provide id from element-answer in serializer
    Co-authored-by: Dimitri Lahaye <dimitri.lahayed@pix.fr>

--- api/tests/devcomp/unit/infrastructure/serializers/jsonapi/element-answer-serializer_test.js
+++ api/tests/devcomp/unit/infrastructure/serializers/jsonapi/element-answer-serializer_test.js
@@ -9,111 +9,233 @@ import { QrocmCorrectionResponse } from '../../../../../../src/devcomp/domain/mo
 describe('Unit | DevComp | Infrastructure | Serializers | Jsonapi | ElementAnswerSerializer', function () {
   describe('#serialize', function () {
     describe('When correction response is for QCU', function () {
-      it('should return a serialized ElementAnswer', function () {
-        // given
-        const givenCorrectionResponse = new QcuCorrectionResponse({
-          status: AnswerStatus.OK,
-          feedback: 'Good job!',
-          solution: 'a1b2c3d4-e5f6-7g8h-9i0j-k1l2m3n4o5p6',
-        });
+      describe('when element answer does not provide id', function () {
+        it('should return a serialized ElementAnswer', function () {
+          // given
+          const givenCorrectionResponse = new QcuCorrectionResponse({
+            status: AnswerStatus.OK,
+            feedback: 'Good job!',
+            solution: 'a1b2c3d4-e5f6-7g8h-9i0j-k1l2m3n4o5p6',
+          });
 
-        const elementAnswer = new ElementAnswer({
-          elementId: '123',
-          userResponseValue: 'a1b2c3d4-e5f6-7g8h-9i0j-k1l2m3n4o5p6',
-          correction: givenCorrectionResponse,
-        });
-        const expectedResult = {
-          data: {
-            attributes: {
-              'element-id': '123',
-              'user-response-value': 'a1b2c3d4-e5f6-7g8h-9i0j-k1l2m3n4o5p6',
-            },
-            relationships: {
-              correction: {
-                data: {
-                  type: 'correction-responses',
+          const elementAnswer = new ElementAnswer({
+            elementId: '123',
+            userResponseValue: 'a1b2c3d4-e5f6-7g8h-9i0j-k1l2m3n4o5p6',
+            correction: givenCorrectionResponse,
+          });
+          const expectedResult = {
+            data: {
+              attributes: {
+                'element-id': '123',
+                'user-response-value': 'a1b2c3d4-e5f6-7g8h-9i0j-k1l2m3n4o5p6',
+              },
+              relationships: {
+                correction: {
+                  data: {
+                    type: 'correction-responses',
+                  },
                 },
               },
+              type: 'element-answers',
             },
-            type: 'element-answers',
-          },
-          included: [
-            {
-              attributes: {
-                feedback: 'Good job!',
-                status: 'ok',
-                solution: givenCorrectionResponse.solution,
+            included: [
+              {
+                attributes: {
+                  feedback: 'Good job!',
+                  status: 'ok',
+                  solution: givenCorrectionResponse.solution,
+                },
+                type: 'correction-responses',
               },
-              type: 'correction-responses',
+            ],
+          };
+
+          // when
+          const result = elementAnswerSerializer.serialize(elementAnswer);
+
+          // then
+          expect(omit(result, ['data.id', 'data.relationships.correction.data.id', 'included[0].id'])).to.deep.equal(
+            expectedResult,
+          );
+        });
+      });
+
+      describe('when element answer provides id', function () {
+        it('should return a serialized ElementAnswer', function () {
+          // given
+          const givenCorrectionResponse = new QcuCorrectionResponse({
+            status: AnswerStatus.OK,
+            feedback: 'Good job!',
+            solution: 'a1b2c3d4-e5f6-7g8h-9i0j-k1l2m3n4o5p6',
+          });
+
+          const elementAnswer = new ElementAnswer({
+            id: 233,
+            elementId: '123',
+            userResponseValue: 'a1b2c3d4-e5f6-7g8h-9i0j-k1l2m3n4o5p6',
+            correction: givenCorrectionResponse,
+          });
+
+          const expectedResult = {
+            data: {
+              attributes: {
+                'element-id': '123',
+                'user-response-value': 'a1b2c3d4-e5f6-7g8h-9i0j-k1l2m3n4o5p6',
+              },
+              relationships: {
+                correction: {
+                  data: {
+                    id: '233',
+                    type: 'correction-responses',
+                  },
+                },
+              },
+              id: '233',
+              type: 'element-answers',
             },
-          ],
-        };
+            included: [
+              {
+                attributes: {
+                  feedback: 'Good job!',
+                  status: 'ok',
+                  solution: givenCorrectionResponse.solution,
+                },
+                id: '233',
+                type: 'correction-responses',
+              },
+            ],
+          };
 
-        // when
-        const result = elementAnswerSerializer.serialize(elementAnswer);
+          // when
+          const result = elementAnswerSerializer.serialize(elementAnswer);
 
-        // then
-        expect(omit(result, ['data.id', 'data.relationships.correction.data.id', 'included[0].id'])).to.deep.equal(
-          expectedResult,
-        );
+          // then
+          expect(result).to.deep.equal(expectedResult);
+        });
       });
     });
-    describe('When correction response is for QROCM-ind', function () {
-      it('should return a serialized ElementAnswer', function () {
-        // given
-        const solution = {
-          inputBlock: ['@'],
-          selectBlock: ['1'],
-        };
-        const userAnswerValue = {
-          inputBlock: '@',
-          selectBlock: '1',
-        };
-        const givenCorrectionResponse = new QrocmCorrectionResponse({
-          status: AnswerStatus.OK,
-          feedback: 'Good job!',
-          solution,
-        });
 
-        const elementAnswer = new ElementAnswer({
-          elementId: '123',
-          userResponseValue: userAnswerValue,
-          correction: givenCorrectionResponse,
-        });
-        const expectedResult = {
-          data: {
-            attributes: {
-              'element-id': '123',
-              'user-response-value': userAnswerValue,
-            },
-            relationships: {
-              correction: {
-                data: {
-                  type: 'correction-responses',
+    describe('When correction response is for QROCM-ind', function () {
+      describe('when element answer does not provide id', function () {
+        it('should return a serialized ElementAnswer', function () {
+          // given
+          const solution = {
+            inputBlock: ['@'],
+            selectBlock: ['1'],
+          };
+          const userAnswerValue = {
+            inputBlock: '@',
+            selectBlock: '1',
+          };
+          const givenCorrectionResponse = new QrocmCorrectionResponse({
+            status: AnswerStatus.OK,
+            feedback: 'Good job!',
+            solution,
+          });
+
+          const elementAnswer = new ElementAnswer({
+            elementId: '123',
+            userResponseValue: userAnswerValue,
+            correction: givenCorrectionResponse,
+          });
+          const expectedResult = {
+            data: {
+              attributes: {
+                'element-id': '123',
+                'user-response-value': userAnswerValue,
+              },
+              relationships: {
+                correction: {
+                  data: {
+                    type: 'correction-responses',
+                  },
                 },
               },
+              type: 'element-answers',
             },
-            type: 'element-answers',
-          },
-          included: [
-            {
-              attributes: {
-                feedback: 'Good job!',
-                status: 'ok',
-                solution: givenCorrectionResponse.solution,
+            included: [
+              {
+                attributes: {
+                  feedback: 'Good job!',
+                  status: 'ok',
+                  solution: givenCorrectionResponse.solution,
+                },
+                type: 'correction-responses',
               },
-              type: 'correction-responses',
-            },
-          ],
-        };
+            ],
+          };
 
-        // when
-        const result = elementAnswerSerializer.serialize(elementAnswer);
+          // when
+          const result = elementAnswerSerializer.serialize(elementAnswer);
 
-        // then
-        expect(omit(result, ['data.id', 'data.relationships.correction.data.id', 'included[0].id'])).to.deep.equal(
-          expectedResult,
-        );
+          // then
+          expect(omit(result, ['data.id', 'data.relationships.correction.data.id', 'included[0].id'])).to.deep.equal(
+            expectedResult,
+          );
+        });
+
+        describe('when element answer provides id', function () {
+          it('should return a serialized ElementAnswer', function () {
+            // given
+            const solution = {
+              emailSeparatorCharacter: ['@'],
+              emailFirstPartSelect: ['1'],
+              emailSecondPartSelect: ['2'],
+            };
+            const userAnswerValue = {
+              emailSeparatorCharacter: '@',
+              emailFirstPartSelect: '1',
+              emailSecondPartSelect: '2',
+            };
+            const givenCorrectionResponse = new QrocmCorrectionResponse({
+              status: AnswerStatus.OK,
+              feedback: 'Good job!',
+              solution,
+            });
+
+            const elementAnswer = new ElementAnswer({
+              id: 222,
+              elementId: '123',
+              userResponseValue: userAnswerValue,
+              correction: givenCorrectionResponse,
+            });
+            const expectedResult = {
+              data: {
+                attributes: {
+                  'element-id': '123',
+                  'user-response-value': userAnswerValue,
+                },
+                relationships: {
+                  correction: {
+                    data: {
+                      id: '222',
+                      type: 'correction-responses',
+                    },
+                  },
+                },
+                id: '222',
+                type: 'element-answers',
+              },
+              included: [
+                {
+                  attributes: {
+                    feedback: 'Good job!',
+                    status: 'ok',
+                    solution: givenCorrectionResponse.solution,
+                  },
+                  id: '222',
+                  type: 'correction-responses',
+                },
+              ],
+            };
+
+            // when
+            const result = elementAnswerSerializer.serialize(elementAnswer);
+
+            // then
+            expect(result).to.deep.equal(expectedResult);
+          });
+        });
       });
     });
   });
