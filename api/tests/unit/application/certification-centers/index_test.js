b324ddfbddb8f7f89f871b6f52fbd64fd06e4ae9
P-Jeremy <jemyplu@gmail.com>
2024-01-15 15:29:40 +0100

    :bug: api: protect get divisions route

--- api/tests/unit/application/certification-centers/index_test.js
+++ api/tests/unit/application/certification-centers/index_test.js
@@ -6,6 +6,27 @@ import * as moduleUnderTest from '../../../../lib/application/certification-cent
 
 describe('Unit | Router | certification-center-router', function () {
   describe('GET /api/certification-centers/{certificationCenterId}/divisions', function () {
+    describe('when user is not member of certification center', function () {
+      it('should return 403', async function () {
+        // given
+        const httpTestServer = new HttpTestServer();
+        await httpTestServer.register(moduleUnderTest);
+        sinon.stub(securityPreHandlers, 'checkUserIsMemberOfCertificationCenter').callsFake(
+          () => (request, h) =>
+            h
+              .response({ errors: new Error('forbidden') })
+              .code(403)
+              .takeover(),
+        );
+
+        // when
+        const result = await httpTestServer.request('GET', '/api/certification-centers/1234/divisions');
+
+        // then
+        expect(result.statusCode).to.equal(403);
+      });
+    });
+
     it('should reject an invalid certification center id', async function () {
       // given
       const httpTestServer = new HttpTestServer();
