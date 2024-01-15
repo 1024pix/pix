591550b2433db762c312977d9034d8a13a9e33ab
P-Jeremy <jemyplu@gmail.com>
2024-01-15 15:29:40 +0100

    :bug: api: protect get divisions route

--- api/tests/acceptance/application/certification-centers/index_test.js
+++ api/tests/acceptance/application/certification-centers/index_test.js
@@ -108,4 +108,50 @@ describe('Acceptance | Route | Certification Centers', function () {
       });
     });
   });
+
+  describe('GET /api/certification-centers/{certificationCenterId}/divisions', function () {
+    it('should return 200 with divisions', async function () {
+      // given
+      const userId = databaseBuilder.factory.buildUser().id;
+      const externalId = 'baldurs-gates';
+      const certificationCenterId = databaseBuilder.factory.buildCertificationCenter({
+        externalId,
+        type: 'SCO',
+      }).id;
+      databaseBuilder.factory.buildCertificationCenterMembership({ userId, certificationCenterId });
+      const organizationId = databaseBuilder.factory.buildOrganization({ externalId, type: 'SCO' }).id;
+      databaseBuilder.factory.buildOrganizationLearner({
+        id: 1,
+        organizationId,
+        division: '2ndB',
+        firstName: 'Laura',
+        lastName: 'Certif4Ever',
+      });
+      await databaseBuilder.commit();
+
+      // when
+      const { result, statusCode } = await server.inject({
+        headers: {
+          authorization: generateValidRequestAuthorizationHeader(userId),
+        },
+        method: 'GET',
+        payload: {},
+        url: `/api/certification-centers/${certificationCenterId}/divisions`,
+      });
+
+      // then
+      expect(statusCode).to.equal(200);
+      expect(result).to.deep.equal({
+        data: [
+          {
+            type: 'divisions',
+            id: '2ndB',
+            attributes: {
+              name: '2ndB',
+            },
+          },
+        ],
+      });
+    });
+  });
 });
