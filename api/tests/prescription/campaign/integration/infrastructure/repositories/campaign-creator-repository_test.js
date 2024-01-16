35dcf262390d3317ed4ca363c625ce785c9340e2
Yvonnick Frin <frin.yvonnick@gmail.com>
2024-01-16 15:23:08 +0100

    refactor(api): remove domain checks from campaign-creator-repository

--- api/tests/prescription/campaign/integration/infrastructure/repositories/campaign-creator-repository_test.js
+++ api/tests/prescription/campaign/integration/infrastructure/repositories/campaign-creator-repository_test.js
@@ -1,8 +1,6 @@
-import { expect, databaseBuilder, catchErr } from '../../../../../test-helper.js';
+import { expect, databaseBuilder } from '../../../../../test-helper.js';
 import * as campaignCreatorRepository from '../../../../../../src/prescription/campaign/infrastructure/repositories/campaign-creator-repository.js';
-import { UserNotAuthorizedToCreateCampaignError } from '../../../../../../lib/domain/errors.js';
 import * as apps from '../../../../../../lib/domain/constants.js';
-import { CampaignCreator } from '../../../../../../src/prescription/campaign/domain/models/CampaignCreator.js';
 
 describe('Integration | Repository | CampaignCreatorRepository', function () {
   describe('#get', function () {
@@ -17,7 +15,7 @@ describe('Integration | Repository | CampaignCreatorRepository', function () {
       });
       await databaseBuilder.commit();
 
-      const creator = await campaignCreatorRepository.get({ userId, organizationId, ownerId: userId });
+      const creator = await campaignCreatorRepository.get(organizationId);
 
       expect(creator.availableTargetProfileIds).to.deep.equal([targetProfileId]);
     });
@@ -33,7 +31,7 @@ describe('Integration | Repository | CampaignCreatorRepository', function () {
         databaseBuilder.factory.buildOrganizationFeature({ organizationId, featureId });
         await databaseBuilder.commit();
 
-        const creator = await campaignCreatorRepository.get({ userId, organizationId, ownerId: userId });
+        const creator = await campaignCreatorRepository.get(organizationId);
 
         expect(creator.isMultipleSendingsAssessmentEnable).to.be.true;
       });
@@ -53,7 +51,7 @@ describe('Integration | Repository | CampaignCreatorRepository', function () {
 
           await databaseBuilder.commit();
 
-          const creator = await campaignCreatorRepository.get({ userId, organizationId, ownerId: userId });
+          const creator = await campaignCreatorRepository.get(organizationId);
           expect(creator.availableTargetProfileIds).to.exactlyContain([targetProfilePublicId]);
         });
       });
@@ -76,7 +74,7 @@ describe('Integration | Repository | CampaignCreatorRepository', function () {
 
           await databaseBuilder.commit();
 
-          const creator = await campaignCreatorRepository.get({ userId, organizationId, ownerId: userId });
+          const creator = await campaignCreatorRepository.get(organizationId);
           expect(creator.availableTargetProfileIds).to.exactlyContain([targetProfileSharedId]);
         });
 
@@ -93,7 +91,7 @@ describe('Integration | Repository | CampaignCreatorRepository', function () {
 
           await databaseBuilder.commit();
 
-          const creator = await campaignCreatorRepository.get({ userId, organizationId, ownerId: userId });
+          const creator = await campaignCreatorRepository.get(organizationId);
           expect(creator.availableTargetProfileIds).to.exactlyContain([organizationTargetProfileId]);
         });
       });
@@ -121,93 +119,9 @@ describe('Integration | Repository | CampaignCreatorRepository', function () {
 
           await databaseBuilder.commit();
 
-          const creator = await campaignCreatorRepository.get({ userId, organizationId, ownerId: userId });
+          const creator = await campaignCreatorRepository.get(organizationId);
           expect(creator.availableTargetProfileIds).to.be.empty;
         });
-      });
-    });
-
-    context('when the user is not a member of the organization', function () {
-      it('throws an error', async function () {
-        const { id: userId } = databaseBuilder.factory.buildUser();
-        const { id: organizationMemberId } = databaseBuilder.factory.buildUser();
-        const { id: organizationId } = databaseBuilder.factory.buildOrganization();
-        databaseBuilder.factory.buildMembership({ organizationId, userId: organizationMemberId });
-
-        await databaseBuilder.commit();
-
-        const error = await catchErr(campaignCreatorRepository.get)({
-          userId,
-          organizationId,
-          ownerId: organizationMemberId,
-        });
-
-        expect(error).to.be.instanceOf(UserNotAuthorizedToCreateCampaignError);
-        expect(error.message).to.equal(`User does not have an access to the organization ${organizationId}`);
-      });
-
-      it('return the campaign creator', async function () {
-        // given
-        const userId = databaseBuilder.factory.buildUser().id;
-        const ownerId = databaseBuilder.factory.buildUser().id;
-
-        const organizationId = databaseBuilder.factory.buildOrganization().id;
-        databaseBuilder.factory.buildMembership({ organizationId, userId: ownerId });
-        const shouldCreatorBeFromOrganization = false;
-
-        await databaseBuilder.commit();
-
-        // when
-        const result = await campaignCreatorRepository.get({
-          userId,
-          organizationId,
-          ownerId,
-          shouldCreatorBeFromOrganization,
-        });
-
-        // then
-        expect(result).to.be.instanceOf(CampaignCreator);
-      });
-    });
-
-    context('when the owner is not a member of the organization', function () {
-      it('throws an error', async function () {
-        // given
-        const userId = databaseBuilder.factory.buildUser().id;
-        const ownerId = databaseBuilder.factory.buildUser().id;
-        const organizationId = databaseBuilder.factory.buildOrganization().id;
-        databaseBuilder.factory.buildMembership({ organizationId, userId });
-
-        await databaseBuilder.commit();
-
-        // when
-        const error = await catchErr(campaignCreatorRepository.get)({ userId, organizationId, ownerId });
-
-        // then
-        expect(error).to.be.instanceOf(UserNotAuthorizedToCreateCampaignError);
-        expect(error.message).to.equal(`Owner does not have an access to the organization ${organizationId}`);
-      });
-
-      it('return the campaign creator', async function () {
-        // given
-        const userId = databaseBuilder.factory.buildUser().id;
-        const ownerId = databaseBuilder.factory.buildUser().id;
-        const organizationId = databaseBuilder.factory.buildOrganization().id;
-        const shouldOwnerBeFromOrganization = false;
-        databaseBuilder.factory.buildMembership({ organizationId, userId });
-
-        await databaseBuilder.commit();
-
-        // when
-        const result = await campaignCreatorRepository.get({
-          userId,
-          organizationId,
-          ownerId,
-          shouldOwnerBeFromOrganization,
-        });
-
-        // then
-        expect(result).to.be.instanceOf(CampaignCreator);
       });
     });
   });
