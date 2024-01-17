605a76c12dcae78843364d8e58e7980df5f6e7b1
cberthou <clement.berthou.tsp@gmail.com>
2024-01-17 11:52:07 +0100

    :sparkles: api: add endpoint to update the flash algorithm configuration

--- api/src/certification/flash-certification/application/flash-assessment-configuration-controller.js
+++ api/src/certification/flash-certification/application/flash-assessment-configuration-controller.js
@@ -5,6 +5,15 @@ const getActiveFlashAssessmentConfiguration = async (req, h) => {
   return h.response(flashAssessmentConfiguration);
 };
 
+const updateActiveFlashAssessmentConfiguration = async (req, h) => {
+  const { payload } = req;
+  await usecases.updateActiveFlashAssessmentConfiguration({
+    configuration: payload,
+  });
+  return h.response().code(204);
+};
+
 export const flashAssessmentConfigurationController = {
   getActiveFlashAssessmentConfiguration,
+  updateActiveFlashAssessmentConfiguration,
 };
