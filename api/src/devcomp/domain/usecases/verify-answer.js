08cc45d0a7082875873fe4ffe20b5050d7e9bfaa
miranda.lin-guignard <miranda.w.lin@gmail.com>
2024-01-18 11:07:56 +0100

    refactor(api): delete old verify answers route and usecase in modules
    Co-authored-by: Dimitri Lahaye <dimitri.lahaye@pix.fr>

--- api/src/devcomp/domain/usecases/verify-answer.js
+++
@@ -1,7 +0,0 @@
-async function verifyAnswer({ moduleSlug, userResponse, elementId, moduleRepository }) {
-  const foundModule = await moduleRepository.getBySlugForVerification({ slug: moduleSlug });
-  const element = foundModule.getElementById(elementId);
-  return element.assess(userResponse);
-}
-
-export { verifyAnswer };
