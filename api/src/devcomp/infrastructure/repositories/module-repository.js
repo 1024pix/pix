1a6f52b5a83b47941e07d4f914327f428e0182e5
dlahaye <dimitrilahaye.dev@gmail.com>
2024-01-19 11:00:15 +0100

    feat(api): add details to module repository
    Co-authored-by: miranda.lin-guignard <miranda.lin-guignard@pix.fr>

--- api/src/devcomp/infrastructure/repositories/module-repository.js
+++ api/src/devcomp/infrastructure/repositories/module-repository.js
@@ -16,6 +16,7 @@ import { QROCM } from '../../domain/models/element/QROCM.js';
 import { logger } from '../../../shared/infrastructure/utils/logger.js';
 import { QROCMForAnswerVerification } from '../../domain/models/element/QROCM-for-answer-verification.js';
 import { Video } from '../../domain/models/element/Video.js';
+import { Details } from '../../domain/models/module/Details.js';
 
 async function getBySlug({ slug, moduleDatasource }) {
   try {
@@ -49,6 +50,7 @@ function _toDomain(moduleData) {
     slug: moduleData.slug,
     title: moduleData.title,
     transitionTexts: moduleData.transitionTexts?.map((transitionText) => new TransitionText(transitionText)) ?? [],
+    details: new Details(moduleData.details),
     grains: moduleData.grains.map((grain) => {
       return new Grain({
         id: grain.id,
@@ -86,6 +88,7 @@ function _toDomainForVerification(moduleData) {
     id: moduleData.id,
     slug: moduleData.slug,
     title: moduleData.title,
+    details: new Details(moduleData.details),
     grains: moduleData.grains.map((grain) => {
       return new Grain({
         id: grain.id,
