6aa6de193f1571b028005e8535f3f35d55a35d75
miranda.lin-guignard <miranda.w.lin@gmail.com>
2024-01-19 11:24:44 +0100

    feat(mon-pix): add details to Module model
    Co-authored-by: Dimitri Lahaye <dimitri.lahaye@pix.fr>

--- mon-pix/app/pods/module/model.js
+++ mon-pix/app/pods/module/model.js
@@ -3,6 +3,6 @@ import Model, { attr, hasMany } from '@ember-data/model';
 export default class Module extends Model {
   @attr('string') title;
   @attr({ defaultValue: () => [] }) transitionTexts;
-
+  @attr() details;
   @hasMany('grain', { async: false, inverse: 'module' }) grains;
 }
