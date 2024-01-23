c2b5523f4dbba207e60aa6a39f9d4952eccc960d
Ismael Gorissen <ismael.gorissen@gmail.com>
2024-01-23 11:02:20 +0100

    feat(admin): add new component
    Organizations::Children::AttachChildForm component

---
+++ admin/tests/unit/components/organizations/children/attach-children-form_test.js
@@ -0,0 +1,47 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import sinon from 'sinon';
+
+import createGlimmerComponent from '../../../../helpers/create-glimmer-component';
+
+module('Unit | Component | organizations/children/attach-child-form', function (hooks) {
+  setupTest(hooks);
+
+  let component;
+
+  hooks.beforeEach(function () {
+    component = createGlimmerComponent('component:organizations/children/attach-child-form');
+  });
+
+  module('when form input value changes', function () {
+    test('updates "childOrganization" component attribute', function (assert) {
+      // given
+      const event = { target: { value: '5432' } };
+
+      // when
+      component.childOrganizationInputValueChanged(event);
+
+      // then
+      assert.strictEqual(component.childOrganization, '5432');
+    });
+  });
+
+  module('when submitting the form', function () {
+    test('emits an event with "childOrganization" component attribute value and resets form input', function (assert) {
+      // given
+      const onFormSubmitted = sinon.stub();
+      const event = { preventDefault: sinon.stub() };
+
+      component.childOrganization = '1234';
+      component.args.onFormSubmitted = onFormSubmitted;
+
+      // when
+      component.submitForm(event);
+
+      // then
+      assert.true(event.preventDefault.calledOnce);
+      assert.true(onFormSubmitted.calledOnceWithExactly('1234'));
+      assert.strictEqual(component.childOrganization, '');
+    });
+  });
+});
