7245d7430ea288b6d52d2741e3b9af3f5c634ee8
Alexandre Monney <alexandre.monney@pix.fr>
2024-01-24 11:47:44 +0100

    refactor(api): move decode "curseur" logic to controller

--- api/lib/application/pole-emploi/pole-emploi-controller.js
+++ api/lib/application/pole-emploi/pole-emploi-controller.js
@@ -1,9 +1,12 @@
 import { usecases } from '../../domain/usecases/index.js';
+import * as poleEmploiService from '../../domain/services/pole-emploi-service.js';
 
 const getSendings = async function (request, h) {
   const cursor = request.query.curseur;
+
+  const cursorData = await poleEmploiService.decodeCursor(cursor);
   const filters = _extractFilters(request);
-  const { sendings, link } = await usecases.getPoleEmploiSendings({ cursor, filters });
+  const { sendings, link } = await usecases.getPoleEmploiSendings({ cursorData, filters });
   return h.response(sendings).header('link', link).code(200);
 };
 
