7dcd4d62991cbffdfbec5a072c67d29dd7e06dfe
Alexandre Monney <alexandre.monney@pix.fr>
2024-01-24 11:47:44 +0100

    refactor(api): move decode "curseur" logic to controller

--- api/tests/unit/application/pole-emploi/pole-emploi-controller_test.js
+++ api/tests/unit/application/pole-emploi/pole-emploi-controller_test.js
@@ -4,19 +4,28 @@ import { usecases } from '../../../../lib/domain/usecases/index.js';
 
 describe('Unit | Controller | pole-emploi-controller', function () {
   describe('#getSendings', function () {
+    let curseur, decodedCurseur, sending, poleEmploiService;
+    beforeEach(function () {
+      curseur = Symbol('curseur');
+      decodedCurseur = Symbol('decoded curseur');
+      sending = [{ idEnvoi: 1 }];
+      poleEmploiService = { decodeCursor: sinon.stub() };
+    });
     context('when there is a cursor in the url', function () {
       it('should return the pole emploi sending', async function () {
         // given
-        const request = { query: { curseur: 'azefvbjljhgrEDJNH' } };
-        const sending = [{ idEnvoi: 456 }];
+        const curseur = Symbol('curseur');
+        const decodedCurseur = Symbol('decoded curseur');
+        const request = { query: { curseur } };
         sinon.stub(usecases, 'getPoleEmploiSendings').resolves(sending);
+        poleEmploiService.decodeCursor.withArgs(curseur).resolves(decodedCurseur);
 
         // when
-        await poleEmploiController.getSendings(request, hFake);
+        await poleEmploiController.getSendings(request, hFake, { poleEmploiService });
 
         //then
         expect(usecases.getPoleEmploiSendings).have.been.calledWithExactly({
-          cursor: 'azefvbjljhgrEDJNH',
+          cursorData: decodedCurseur,
           filters: {},
         });
       });
@@ -25,16 +34,17 @@ describe('Unit | Controller | pole-emploi-controller', function () {
       context("when enErreur is 'false'", function () {
         it('should return the pole emploi sending', async function () {
           // given
-          const request = { query: { curseur: 'azefvbjljhgrEDJNH', enErreur: false } };
+          const request = { query: { curseur, enErreur: false } };
           const sending = [{ idEnvoi: 456 }];
+          poleEmploiService.decodeCursor.withArgs(curseur).resolves(decodedCurseur);
           sinon.stub(usecases, 'getPoleEmploiSendings').resolves(sending);
 
           // when
-          await poleEmploiController.getSendings(request, hFake);
+          await poleEmploiController.getSendings(request, hFake, { poleEmploiService });
 
           //then
           expect(usecases.getPoleEmploiSendings).have.been.calledWithExactly({
-            cursor: 'azefvbjljhgrEDJNH',
+            cursorData: decodedCurseur,
             filters: { isSuccessful: true },
           });
         });
@@ -42,16 +52,17 @@ describe('Unit | Controller | pole-emploi-controller', function () {
       context("when enErreur is 'true'", function () {
         it('should return the pole emploi sending', async function () {
           // given
-          const request = { query: { curseur: 'azefvbjljhgrEDJNH', enErreur: true } };
+          const request = { query: { curseur, enErreur: true } };
           const sending = [{ idEnvoi: 456 }];
+          poleEmploiService.decodeCursor.withArgs(curseur).resolves(decodedCurseur);
           sinon.stub(usecases, 'getPoleEmploiSendings').resolves(sending);
 
           // when
-          await poleEmploiController.getSendings(request, hFake);
+          await poleEmploiController.getSendings(request, hFake, { poleEmploiService });
 
           //then
           expect(usecases.getPoleEmploiSendings).have.been.calledWithExactly({
-            cursor: 'azefvbjljhgrEDJNH',
+            cursorData: decodedCurseur,
             filters: { isSuccessful: false },
           });
         });
