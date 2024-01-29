77f152ba5c366d8e6df80891543c716e5862fc7a
Alexandre Monney <alexandre.monney@pix.fr>
2024-01-29 15:12:29 +0100

    fix(db): add new migration to fix export campaign result bug

---
+++ api/db/migrations/20240129134258_replace-empty-participantExternalId-by-null-on-campaign-participations.js
@@ -0,0 +1,21 @@
+// Make sure you properly test your migration, especially DDL (Data Definition Language)
+// ! If the target table is large, and the migration take more than 20 minutes, the deployment will fail !
+
+// You can design and test your migration to avoid this by following this guide
+// https://1024pix.atlassian.net/wiki/spaces/DEV/pages/2153512965/Cr+er+une+migration
+
+// If your migrations target `answers` or `knowledge-elements`
+// contact @team-captains, because automatic migrations are not active on `pix-datawarehouse-production`
+// this may prevent data replication to succeed the day after your migration is deployed on `pix-api-production`
+const TABLE_NAME = 'campaign-participations';
+const COLUMN_NAME = 'participantExternalId';
+
+const up = async function (knex) {
+  await knex(TABLE_NAME).where(COLUMN_NAME, '').update(COLUMN_NAME, null);
+};
+
+const down = async function () {
+  // Do nothing because it's a bugfix, so we don't want this to be rollbacked
+};
+
+export { up, down };
---
+++ api/tests/integration/migration/20240129134258_replace-empty-participantExternalId-by-null-on-campaign-participations_test.js
@@ -0,0 +1,42 @@
+import { expect, databaseBuilder, knex } from '../../test-helper.js';
+import { up as migrationToTest } from '../../../db/migrations/20240129134258_replace-empty-participantExternalId-by-null-on-campaign-participations.js';
+
+describe('Integration | Migration | replace-empty-participantExternalId-by-null-on-campaign-participations', function () {
+  it('should set to null participantExternalId when it is empty', async function () {
+    // given
+    const campaignParticipationId = databaseBuilder.factory.buildCampaignParticipation({
+      participantExternalId: '',
+    }).id;
+
+    await databaseBuilder.commit();
+
+    // when
+    await migrationToTest(knex);
+
+    // then
+    const campaignParticipationUpdated = await knex('campaign-participations')
+      .where('id', campaignParticipationId)
+      .first();
+
+    expect(campaignParticipationUpdated.participantExternalId).to.be.null;
+  });
+
+  it('should not update participantExternalId when it is not empty', async function () {
+    // given
+    const campaignParticipationId = databaseBuilder.factory.buildCampaignParticipation({
+      participantExternalId: "Allez l'om",
+    }).id;
+
+    await databaseBuilder.commit();
+
+    // when
+    await migrationToTest(knex);
+
+    // then
+    const campaignParticipationNotUpdated = await knex('campaign-participations')
+      .where('id', campaignParticipationId)
+      .first();
+
+    expect(campaignParticipationNotUpdated.participantExternalId).to.equal("Allez l'om");
+  });
+});
