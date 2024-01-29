20a5defe347b40c75e93753a4584e34d42360429
Clément Latzarus <c@ltzr.io>
2024-01-29 17:25:37 +0100

    feat(api): add usecase for oidc reconciliation
    when logging in with Google in Pix Admin

---
+++ api/lib/domain/usecases/reconcile-oidc-user-for-admin.js
@@ -0,0 +1,86 @@
+/**
+ * @typedef {import ('./index.js').OidcAuthenticationService} OidcAuthenticationService
+ * @typedef {import ('./index.js').AuthenticationSessionService} AuthenticationSessionService
+ * @typedef {import ('./index.js').AuthenticationMethodRepository} AuthenticationMethodRepository
+ * @typedef {import ('./index.js').UserRepository} UserRepository
+ * @typedef {import ('./index.js').UserLoginRepository} UserLoginRepository
+ */
+
+import { AuthenticationKeyExpired, DifferentExternalIdentifierError } from '../errors.js';
+import { AuthenticationMethod } from '../models/index.js';
+
+/**
+ * @param {Object} params
+ * @param {string} params.authenticationKey
+ * @param {string} params.email
+ * @param {string} params.identityProvider
+ * @param {OidcAuthenticationService} params.oidcAuthenticationService
+ * @param {AuthenticationSessionService} params.authenticationSessionService
+ * @param {AuthenticationMethodRepository} params.authenticationMethodRepository
+ * @param {UserRepository} params.userRepository
+ * @param {UserLoginRepository} params.userLoginRepository
+ */
+const reconcileOidcUserForAdmin = async function ({
+  authenticationKey,
+  email,
+  identityProvider,
+  oidcAuthenticationService,
+  authenticationSessionService,
+  authenticationMethodRepository,
+  userRepository,
+  userLoginRepository,
+}) {
+  const sessionContentAndUserInfo = await authenticationSessionService.getByKey(authenticationKey);
+  if (!sessionContentAndUserInfo) {
+    throw new AuthenticationKeyExpired();
+  }
+
+  const foundUser = await userRepository.getByEmail(email);
+  const userId = foundUser.id;
+
+  await _assertExternalIdentifier({
+    sessionContentAndUserInfo,
+    identityProvider,
+    userId,
+    authenticationMethodRepository,
+  });
+
+  const { userInfo } = sessionContentAndUserInfo;
+  const { externalIdentityId } = userInfo;
+
+  const authenticationComplement = oidcAuthenticationService.createAuthenticationComplement({ userInfo });
+  await authenticationMethodRepository.create({
+    authenticationMethod: new AuthenticationMethod({
+      identityProvider: oidcAuthenticationService.identityProvider,
+      userId,
+      externalIdentifier: externalIdentityId,
+      authenticationComplement,
+    }),
+  });
+
+  const accessToken = await oidcAuthenticationService.createAccessToken(userId);
+  userLoginRepository.updateLastLoggedAt({ userId });
+
+  return accessToken;
+};
+
+export { reconcileOidcUserForAdmin };
+
+async function _assertExternalIdentifier({
+  sessionContentAndUserInfo,
+  identityProvider,
+  userId,
+  authenticationMethodRepository,
+}) {
+  const oidcAuthenticationMethod = await authenticationMethodRepository.findOneByUserIdAndIdentityProvider({
+    userId,
+    identityProvider,
+  });
+
+  const isSameExternalIdentifier =
+    oidcAuthenticationMethod?.externalIdentifier === sessionContentAndUserInfo.userInfo.externalIdentityId;
+
+  if (oidcAuthenticationMethod && !isSameExternalIdentifier) {
+    throw new DifferentExternalIdentifierError();
+  }
+}
