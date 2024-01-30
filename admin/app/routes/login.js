e794a48cfc9c9ad42da35847bad93cb8fe3df8a1
Clément Latzarus <c@ltzr.io>
2024-01-30 15:28:50 +0100

    refacto(admin): call only ready idp on login page

--- admin/app/routes/login.js
+++ admin/app/routes/login.js
@@ -3,8 +3,13 @@ import { service } from '@ember/service';
 
 export default class LoginRoute extends Route {
   @service session;
+  @service oidcIdentityProviders;
 
   beforeModel() {
     this.session.prohibitAuthentication('authenticated');
+  }
+
+  async model() {
+    await this.oidcIdentityProviders.loadReadyIdentityProviders();
   }
 }
--- admin/tests/unit/routes/login_test.js
+++ admin/tests/unit/routes/login_test.js
@@ -1,5 +1,6 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import sinon from 'sinon';
 
 module('Unit | Route | login', function (hooks) {
   setupTest(hooks);
@@ -7,5 +8,25 @@ module('Unit | Route | login', function (hooks) {
   test('it exists', function (assert) {
     const route = this.owner.lookup('route:login');
     assert.ok(route);
+  });
+
+  module('model', function (hooks) {
+    hooks.afterEach(function () {
+      sinon.restore();
+    });
+
+    test('loads ready identity providers', async function (assert) {
+      // given
+      const route = this.owner.lookup('route:login');
+
+      sinon.stub(route.oidcIdentityProviders, 'loadReadyIdentityProviders');
+      route.oidcIdentityProviders.loadReadyIdentityProviders.resolves();
+
+      // when
+      await route.model();
+
+      // then
+      assert.ok(route.oidcIdentityProviders.loadReadyIdentityProviders.called);
+    });
   });
 });
