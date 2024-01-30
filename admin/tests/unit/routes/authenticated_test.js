d8e58e64ef95941a64a30c903b9c40b17f0c4fa7
Clément Latzarus <c@ltzr.io>
2024-01-30 15:29:25 +0100

    refacto(admin): load all idp from user page
    Co-authored-by: Andreia Pena <andreia.pena@pix.fr>

--- admin/tests/unit/routes/authenticated_test.js
+++ admin/tests/unit/routes/authenticated_test.js
@@ -1,28 +1,10 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
-import sinon from 'sinon';
-
 module('Unit | Route | authenticated', function (hooks) {
   setupTest(hooks);
 
   test('it exists', function (assert) {
     const route = this.owner.lookup('route:authenticated');
     assert.ok(route);
-  });
-
-  module('model', function () {
-    test('loads all available identity providers', async function (assert) {
-      // given
-      const route = this.owner.lookup('route:authenticated');
-
-      sinon.stub(route.oidcIdentityProviders, 'loadAllAvailableIdentityProviders');
-      route.oidcIdentityProviders.loadAllAvailableIdentityProviders.resolves();
-
-      // when
-      await route.model();
-
-      // then
-      assert.ok(route.oidcIdentityProviders.loadAllAvailableIdentityProviders.called);
-    });
   });
 });
