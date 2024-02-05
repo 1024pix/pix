d99336271bbbadfacbb070ef5116f8080f7d138b
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2024-02-05 12:11:38 +0100

    test(api): make createServerWithTestOidcProvider provide an openid configuration

--- api/tests/acceptance/application/authentication/oidc/authentication-url-route-get_test.js
+++ api/tests/acceptance/application/authentication/oidc/authentication-url-route-get_test.js
@@ -1,7 +1,6 @@
-import { expect } from '../../../../test-helper.js';
-import { createServer } from '../../../../../server.js';
-import * as OidcIdentityProviders from '../../../../../lib/domain/constants/oidc-identity-providers.js';
 import querystring from 'querystring';
+
+import { createServer, expect } from '../../../../test-helper.js';
 
 describe('Acceptance | Route | oidc authentication url', function () {
   let server;
@@ -12,11 +11,11 @@ describe('Acceptance | Route | oidc authentication url', function () {
 
   describe('GET /api/oidc/authentication-url', function () {
     context('When the request returns 200', function () {
-      it('should return the authentication url for cnav', async function () {
+      it('returns the authentication url', async function () {
         // given
         const query = querystring.stringify({
-          identity_provider: OidcIdentityProviders.CNAV.code,
-          redirect_uri: 'http://app.pix.fr/connexion/cnav',
+          identity_provider: 'OIDC_EXAMPLE_NET',
+          redirect_uri: 'https://app.dev.pix.org/connexion/oidc-example-net',
         });
 
         // when
@@ -30,40 +29,14 @@ describe('Acceptance | Route | oidc authentication url', function () {
 
         const redirectTargetUrl = new URL(response.result.redirectTarget);
 
-        expect(redirectTargetUrl.origin).to.equal('http://idp.cnav');
-        expect(redirectTargetUrl.pathname).to.equal('/auth');
-        expect(redirectTargetUrl.searchParams.get('redirect_uri')).to.equal('http://app.pix.fr/connexion/cnav');
-        expect(redirectTargetUrl.searchParams.get('client_id')).to.equal('PIX_CNAV_CLIENT_ID');
+        expect(redirectTargetUrl.origin).to.equal('https://oidc.example.net');
+        expect(redirectTargetUrl.pathname).to.equal('/ea5ac20c-5076-4806-860a-b0aeb01645d4/oauth2/v2.0/authorize');
+        expect(redirectTargetUrl.searchParams.get('redirect_uri')).to.equal(
+          'https://app.dev.pix.org/connexion/oidc-example-net',
+        );
+        expect(redirectTargetUrl.searchParams.get('client_id')).to.equal('client');
         expect(redirectTargetUrl.searchParams.get('response_type')).to.equal('code');
         expect(redirectTargetUrl.searchParams.get('scope')).to.equal('openid profile');
-      });
-
-      it('should return the authentication url for pole emploi', async function () {
-        // given
-        const query = querystring.stringify({
-          identity_provider: OidcIdentityProviders.POLE_EMPLOI.code,
-          redirect_uri: 'http://app.pix.fr/connexion/pole-emploi',
-        });
-
-        // when
-        const response = await server.inject({
-          method: 'GET',
-          url: `/api/oidc/authentication-url?${query}`,
-        });
-
-        // then
-        expect(response.statusCode).to.equal(200);
-
-        const redirectTargetUrl = new URL(response.result.redirectTarget);
-
-        expect(redirectTargetUrl.origin).to.equal('http://authurl.fr');
-        expect(redirectTargetUrl.searchParams.get('redirect_uri')).to.equal('http://app.pix.fr/connexion/pole-emploi');
-        expect(redirectTargetUrl.searchParams.get('client_id')).to.equal('PIX_POLE_EMPLOI_CLIENT_ID');
-        expect(redirectTargetUrl.searchParams.get('response_type')).to.equal('code');
-        expect(redirectTargetUrl.searchParams.get('realm')).to.equal('/individu');
-        expect(redirectTargetUrl.searchParams.get('scope')).to.equal(
-          'application_PIX_POLE_EMPLOI_CLIENT_ID api_peconnect-individuv1 openid profile serviceDigitauxExposition api_peconnect-servicesdigitauxv1',
-        );
       });
     });
   });
