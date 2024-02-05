af43b1f03a1f893104d2211d53175aef468079a8
dlahaye <dimitrilahaye.dev@gmail.com>
2024-02-05 11:24:00 +0100

    refactor(mon-pix) implement a better redirection strategy
    Co-authored-by: Vincent Hardouin <vincent.hardouin@pix.fr>

--- mon-pix/app/pods/module/route.js
+++ mon-pix/app/pods/module/route.js
@@ -3,15 +3,8 @@ import { service } from '@ember/service';
 
 export default class ModuleRoute extends Route {
   @service store;
-  @service router;
 
   async model(params) {
     return await this.store.findRecord('module', params.slug);
-  }
-
-  redirect(model, transition) {
-    if (transition.targetName !== 'module.get') {
-      this.router.transitionTo('module.details', model);
-    }
   }
 }
