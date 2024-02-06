7bfb6722dac3e1eff9c9c69c72d37d33990a1942
Antoine Ceol <antoine.ceol@gmail.com>
2024-02-06 21:52:44 +0100

    :sparkles: Display notification if no internet or disconnection error

--- certif/app/routes/session-supervising.js
+++ certif/app/routes/session-supervising.js
@@ -3,8 +3,12 @@ import ENV from 'pix-certif/config/environment';
 import { action } from '@ember/object';
 import { service } from '@ember/service';
 
+const NO_INTERNET_MESSAGE = 'Failed to fetch';
 export default class SessionSupervisingRoute extends Route {
   @service store;
+  @service router;
+  @service notifications;
+  @service intl;
 
   async model(params) {
     return this.store.queryRecord('session-for-supervising', {
@@ -14,21 +18,35 @@ export default class SessionSupervisingRoute extends Route {
 
   afterModel(model) {
     this.poller = setInterval(async () => {
-      await this.store.queryRecord('session-for-supervising', { sessionId: model.id });
+      try {
+        await this.store.queryRecord('session-for-supervising', { sessionId: model.id });
+      } catch (response) {
+        this.#stopPolling();
+        if (response?.errors?.[0]?.status === '401') {
+          this.router.replaceWith('login-session-supervisor');
+        }
+
+        if (response.message === NO_INTERNET_MESSAGE) {
+          this.notifications.error(this.intl.t('pages.session-supervising-error.no-internet-error'));
+        }
+      }
     }, ENV.APP.sessionSupervisingPollingRate);
   }
 
   deactivate() {
-    if (this.poller) {
-      clearInterval(this.poller);
-    }
+    this.#stopPolling();
   }
 
   @action
   error() {
+    this.#stopPolling();
+    return true;
+  }
+
+  #stopPolling() {
     if (this.poller) {
       clearInterval(this.poller);
+      this.poller = null;
     }
-    return true;
   }
 }
