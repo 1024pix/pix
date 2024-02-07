7f6c560952bc9c70e242f5399fe28bdc4bbdbff8
Steph0 <Steph0@users.noreply.github.com>
2024-02-07 14:38:56 +0100

    :bug: api: keep only last SCO candidate certification session in SCO extract
    * We want only the last participation to a certification session in Pix
      Orga SCO extract
    * Adding seeds to enforce the possibility that a student can pass
      multiple times certifications
    
    Co-authored-by: Jeremy Pluquet <jeremy.pluquet@pix.fr>

--- api/src/certification/course/infrastructure/repositories/sco-certification-candidate-repository.js
+++ api/src/certification/course/infrastructure/repositories/sco-certification-candidate-repository.js
@@ -4,22 +4,29 @@ import { knex } from '../../../../../db/knex-database-connection.js';
  * @returns {Array<number>} certification candidates ids
  */
 const findIdsByOrganizationIdAndDivision = async function ({ organizationId, division }) {
-  return knex
-    .select(['certification-candidates.id'])
-    .from('certification-candidates')
-    .join(
-      'view-active-organization-learners',
-      'view-active-organization-learners.id',
-      'certification-candidates.organizationLearnerId',
+  const uniqLastCandidatesByOrganizationLearners = knex
+    .select(
+      'certification-candidates.id',
+      knex.raw(
+        'row_number() OVER (PARTITION BY "certification-candidates"."organizationLearnerId" ORDER BY "sessions"."publishedAt" DESC NULLS LAST) as session_number',
+      ),
     )
+    .from('view-active-organization-learners as learners')
+    .innerJoin('certification-candidates', 'learners.id', 'certification-candidates.organizationLearnerId')
+    .innerJoin('sessions', 'certification-candidates.sessionId', 'sessions.id')
     .where({
-      'view-active-organization-learners.organizationId': organizationId,
-      'view-active-organization-learners.isDisabled': false,
+      'learners.organizationId': organizationId,
+      'learners.isDisabled': false,
     })
-    .whereRaw('LOWER("view-active-organization-learners"."division") = ?', division.toLowerCase())
+    .whereRaw('LOWER(learners.division) = ?', division.toLowerCase())
     .orderBy('certification-candidates.lastName', 'ASC')
     .orderBy('certification-candidates.firstName', 'ASC')
-    .pluck('certification-candidates.id');
+    .as('uniqLastCandidatesByOrganizationLearners');
+
+  return knex
+    .pluck('id')
+    .from(uniqLastCandidatesByOrganizationLearners)
+    .where('uniqLastCandidatesByOrganizationLearners.session_number', 1);
 };
 
 export { findIdsByOrganizationIdAndDivision };
