5faf9e8bd1260f32b3b5a81d16133f1303212827
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2024-02-07 11:15:50 +0100

    feat: generate challenge variables in getChallenge shared usecase (aka challenge preview)

--- api/src/shared/domain/usecases/get-challenge.js
+++ api/src/shared/domain/usecases/get-challenge.js
@@ -1,3 +1,9 @@
-export async function getChallenge({ challengeId, challengeRepository }) {
-  return challengeRepository.get(challengeId);
+export async function getChallenge({ challengeId, assessmentId = 1, challengeRepository, randomDataService }) {
+  const challenge = await challengeRepository.get(challengeId);
+
+  if (!challenge.hasVariables) return challenge;
+
+  randomDataService.generateChallengeVariables({ challenge, assessmentId });
+
+  return challenge;
 }
--- api/tests/shared/unit/domain/usecases/get-challenge_test.js
+++ api/tests/shared/unit/domain/usecases/get-challenge_test.js
@@ -9,6 +9,9 @@ describe('Unit | Shared | Domain | UseCases | getChallenge', function () {
       challengeRepository: {
         get: sinon.stub(),
       },
+      randomDataService: {
+        generateChallengeVariables: sinon.stub(),
+      },
     };
   });
 
@@ -16,6 +19,7 @@ describe('Unit | Shared | Domain | UseCases | getChallenge', function () {
     // given
     const challengeId = 'challenge3094783295';
     const challenge = domainBuilder.buildChallenge({ id: challengeId });
+
     dependencies.challengeRepository.get.withArgs(challengeId).resolves(challenge);
 
     // when
@@ -24,5 +28,58 @@ describe('Unit | Shared | Domain | UseCases | getChallenge', function () {
     // then
     expect(result).to.equal(challenge);
     expect(dependencies.challengeRepository.get).to.have.been.calledOnceWithExactly(challengeId);
+  });
+
+  describe("when challenge's instruction has variables", function () {
+    const instruction = `---
+variables:
+  - name: foo
+    type: integer
+    params:
+      min: 0
+      max: 1000
+---
+Est-ce que {% $foo %} c'est beaucoup ?`;
+
+    it('should generate challenge variables using randomDataService', async function () {
+      // given
+      const challengeId = 'challenge3094783295';
+      const assessmentId = 937625;
+      const challenge = domainBuilder.buildChallenge({ id: challengeId, instruction });
+
+      dependencies.challengeRepository.get.withArgs(challengeId).resolves(challenge);
+
+      // when
+      const result = await getChallenge({ challengeId, assessmentId, ...dependencies });
+
+      // then
+      expect(result).to.equal(challenge);
+      expect(dependencies.challengeRepository.get).to.have.been.calledOnceWithExactly(challengeId);
+      expect(dependencies.randomDataService.generateChallengeVariables).to.have.been.calledOnceWith({
+        challenge,
+        assessmentId,
+      });
+    });
+
+    describe('when no assessmentId is received', function () {
+      it('should give default assessmentId 1 for randomDataService', async function () {
+        // given
+        const challengeId = 'challenge3094783295';
+        const challenge = domainBuilder.buildChallenge({ id: challengeId, instruction });
+
+        dependencies.challengeRepository.get.withArgs(challengeId).resolves(challenge);
+
+        // when
+        const result = await getChallenge({ challengeId, ...dependencies });
+
+        // then
+        expect(result).to.equal(challenge);
+        expect(dependencies.challengeRepository.get).to.have.been.calledOnceWithExactly(challengeId);
+        expect(dependencies.randomDataService.generateChallengeVariables).to.have.been.calledOnceWith({
+          challenge,
+          assessmentId: 1,
+        });
+      });
+    });
   });
 });
