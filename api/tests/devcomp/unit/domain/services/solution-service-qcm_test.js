0c6452b3f82a7b05263f5d8283e27c11ff41a687
miranda.lin-guignard <miranda.w.lin@gmail.com>
2024-02-07 15:07:50 +0100

    feat(api): copy QCM validator and solution service from lib to devcomp
    Co-authored-by: Dimitri Lahaye <dimitri.lahaye@pix.fr>

---
+++ api/tests/devcomp/unit/domain/services/solution-service-qcm_test.js
@@ -0,0 +1,49 @@
+import { expect } from '../../../../test-helper.js';
+import { AnswerStatus } from '../../../../../src/devcomp/domain/models/validator/AnswerStatus.js';
+import * as service from '../../../../../src/devcomp/domain/services/solution-service-qcm.js';
+
+describe('Unit | Devcomp | Domain | Services | SolutionServiceQCM ', function () {
+  describe('if solution type is QCM', function () {
+    const successfulCases = [
+      { answerValue: '1', solutionValue: '1' },
+      { answerValue: '1, 2', solutionValue: '1, 2' },
+      { answerValue: '1, 2, 3', solutionValue: '1, 2, 3' },
+      { answerValue: '1,2,3', solutionValue: '1,2,3' },
+      { answerValue: '3, 2, 1', solutionValue: '1, 2, 3' },
+      { answerValue: '1,2,3', solutionValue: '1, 2, 3' },
+      { answerValue: '1,   2,   3   ', solutionValue: '1, 2, 3' },
+      { answerValue: '1, 2, 3', solutionValue: '1, 2, 3' },
+    ];
+
+    // Rule disabled to allow dynamic generated tests. See https://github.com/lo1tuma/eslint-plugin-mocha/blob/master/docs/rules/no-setup-in-describe.md#disallow-setup-in-describe-blocks-mochano-setup-in-describe
+    // eslint-disable-next-line mocha/no-setup-in-describe
+    successfulCases.forEach(({ answerValue, solutionValue }) => {
+      it(
+        'should return "ok" when answer is "' + answerValue + '" and solution is "' + solutionValue + '"',
+        function () {
+          const result = service.match(answerValue, solutionValue);
+          expect(AnswerStatus.isOK(result)).to.be.true;
+        },
+      );
+    });
+
+    const failedCases = [
+      { answerValue: '2', solutionValue: '1' },
+      { answerValue: '1, 3', solutionValue: '1, 2' },
+      { answerValue: '1, 2, 3', solutionValue: '1, 2' },
+      { answerValue: '3, 1', solutionValue: '1, 2' },
+    ];
+
+    // Rule disabled to allow dynamic generated tests. See https://github.com/lo1tuma/eslint-plugin-mocha/blob/master/docs/rules/no-setup-in-describe.md#disallow-setup-in-describe-blocks-mochano-setup-in-describe
+    // eslint-disable-next-line mocha/no-setup-in-describe
+    failedCases.forEach(({ answerValue, solutionValue }) => {
+      it(
+        'should return "ko" when answer is "' + answerValue + '" and solution is "' + solutionValue + '"',
+        function () {
+          const result = service.match(answerValue, solutionValue);
+          expect(AnswerStatus.isKO(result)).to.be.true;
+        },
+      );
+    });
+  });
+});
