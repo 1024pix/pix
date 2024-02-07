36fdb419a7b759bc18a17436636572047ba576e7
dlahaye <dimitrilahaye.dev@gmail.com>
2024-02-07 12:09:15 +0100

    fix(api) use tolerances for QROCM-ind
    Co-authored-by: miranda.lin-guignard <miranda.lin-guignard@pix.fr>

---
+++ api/tests/devcomp/integration/domain/models/element/QROCMForAnswerVerification_test.js
@@ -0,0 +1,81 @@
+import { expect } from '../../../../../test-helper.js';
+import { QROCMForAnswerVerification } from '../../../../../../src/devcomp/domain/models/element/QROCM-for-answer-verification.js';
+import { AnswerStatus } from '../../../../../../src/shared/domain/models/AnswerStatus.js';
+
+describe('Integration | Devcomp | Domain | Models | Element | QROCMForAnswerVerification', function () {
+  it('should return a valid answer when using tolerances with a right user response', function () {
+    // given
+    const qrocm = new QROCMForAnswerVerification({
+      id: '98c51fa7-03b7-49b1-8c5e-49341d35909c',
+      instruction: '<p>Complétez le texte ci-dessous.</p>',
+      proposals: [
+        {
+          input: 'appName',
+          type: 'input',
+          inputType: 'text',
+          size: 1,
+          display: 'inline',
+          placeholder: '',
+          ariaLabel: 'Réponse 1',
+          defaultValue: '',
+          tolerances: ['t1', 't2'],
+          solutions: ['Modulix'],
+        },
+      ],
+      feedbacks: {
+        valid: 'Bravo!',
+        invalid: 'Mince!',
+      },
+    });
+    qrocm.setUserResponse([
+      {
+        input: 'appName',
+        answer: 'modulix!',
+      },
+    ]);
+
+    // when
+    const correction = qrocm.assess();
+
+    // then
+    expect(correction.status).to.deep.equal(AnswerStatus.OK);
+  });
+
+  it('should return an invalid answer when using tolerances with a wrong user response', function () {
+    // given
+    const qrocm = new QROCMForAnswerVerification({
+      id: '98c51fa7-03b7-49b1-8c5e-49341d35909c',
+      instruction: '<p>Complétez le texte ci-dessous.</p>',
+      proposals: [
+        {
+          input: 'appName',
+          type: 'input',
+          inputType: 'text',
+          size: 1,
+          display: 'inline',
+          placeholder: '',
+          ariaLabel: 'Réponse 1',
+          defaultValue: '',
+          tolerances: [],
+          solutions: ['Modulix'],
+        },
+      ],
+      feedbacks: {
+        valid: 'Bravo!',
+        invalid: 'Mince!',
+      },
+    });
+    qrocm.setUserResponse([
+      {
+        input: 'appName',
+        answer: 'modulix',
+      },
+    ]);
+
+    // when
+    const correction = qrocm.assess();
+
+    // then
+    expect(correction.status).to.deep.equal(AnswerStatus.KO);
+  });
+});
