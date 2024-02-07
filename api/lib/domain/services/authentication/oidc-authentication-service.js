8730e55ea2b98f677a9091dc760a6135127afe49
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2024-02-07 16:10:24 +0100

    refactor(api): remove return of nonce from OidcAuthenticationService#getUserInfo
    since it is now useless because the nonce check is now done by the openid-client
    library in the following call of Pix:
    
        await this.client.callback(this.redirectUri, { code, state }, { nonce, state: sessionState });
    
    through the openid-client.Client#validateIdToken function.
    
    Co-authored-by: Ismael Gorissen <ismael.gorissen@gmail.com>

--- api/lib/domain/services/authentication/oidc-authentication-service.js
+++ api/lib/domain/services/authentication/oidc-authentication-service.js
@@ -214,7 +214,6 @@ class OidcAuthenticationService {
       firstName: userInfo.given_name,
       lastName: userInfo.family_name,
       externalIdentityId: userInfo.sub,
-      nonce: userInfo.nonce,
     };
 
     if (this.claimsToStore) {
