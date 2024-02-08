49b711c01014e12fef69f622dbaaca85ee2308ad
LionelB <lionel@lumographe.fr>
2024-02-08 15:25:59 +0100

    refactor(api): use a stream to read csv file

--- api/src/prescription/learner-management/domain/services/organization-learners-csv-service.js
+++ api/src/prescription/learner-management/domain/services/organization-learners-csv-service.js
@@ -1,11 +1,18 @@
-import * as fs from 'fs/promises';
-
 import { OrganizationLearnerParser } from '../../infrastructure/serializers/csv/organization-learner-parser.js';
-
 export { extractOrganizationLearnersInformation };
 
-async function extractOrganizationLearnersInformation(path, organization, i18n) {
-  const buffer = await fs.readFile(path);
-  const parser = OrganizationLearnerParser.buildParser(buffer, organization.id, i18n);
+async function extractOrganizationLearnersInformation(readableStream, organization, i18n) {
+  const dataPromise = new Promise((resolve, reject) => {
+    const chunks = [];
+    readableStream.on('data', (data) => {
+      chunks.push(data);
+    });
+    readableStream.on('error', (err) => reject(err));
+    readableStream.once('end', () => {
+      resolve(Buffer.concat(chunks));
+    });
+  });
+  const data = await dataPromise;
+  const parser = OrganizationLearnerParser.buildParser(data, organization.id, i18n);
   return parser.parse().learners;
 }
