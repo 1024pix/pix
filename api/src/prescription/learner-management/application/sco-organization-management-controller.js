f08bd22e73c87321bac55d56dc658a54159c04b7
pix-service-auto-merge <69905988+pix-service-auto-merge@users.noreply.github.com>
2024-02-08 11:30:56 +0100

    [BUGFIX] corrige la suppression de fichier temporaire après un import siecle (PIX-11118)
     #8005

--- api/src/prescription/learner-management/application/sco-organization-management-controller.js
+++ api/src/prescription/learner-management/application/sco-organization-management-controller.js
@@ -1,23 +1,31 @@
 import fs from 'fs/promises';
 import { usecases } from '../domain/usecases/index.js';
+import { logErrorWithCorrelationIds } from '../../../../lib/infrastructure/monitoring-tools.js';
 
-const importOrganizationLearnersFromSIECLE = async function (request, h) {
+const importOrganizationLearnersFromSIECLE = async function (
+  request,
+  h,
+  dependencies = { logErrorWithCorrelationIds },
+) {
   const organizationId = request.params.id;
   const { format } = request.query;
+  try {
+    await usecases.importOrganizationLearnersFromSIECLEFormat({
+      organizationId,
+      payload: request.payload,
+      format,
+      i18n: request.i18n,
+    });
+  } finally {
+    // see https://hapi.dev/api/?v=21.3.3#-routeoptionspayloadoutput
+    // add a catch to avoid an error if unlink fails
+    try {
+      await fs.unlink(request.payload.path);
+    } catch (err) {
+      dependencies.logErrorWithCorrelationIds(err);
+    }
+  }
 
-  await usecases.importOrganizationLearnersFromSIECLEFormat({
-    organizationId,
-    payload: request.payload,
-    format,
-    i18n: request.i18n,
-  });
-  // see https://hapi.dev/api/?v=21.3.3#-routeoptionspayloadoutput
-  request.server.events.on('response', async () => {
-    fs.access(request.payload.path).then(
-      () => fs.unlink(request.payload.path),
-      () => undefined, // nothing to do since file not exists
-    );
-  });
   return h.response(null).code(204);
 };
 
