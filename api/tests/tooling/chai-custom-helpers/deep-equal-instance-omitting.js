e26b2fbe99e50f051b0828429ba6a6834076e438
François de Metz <francois.de-metz@pix.fr>
2024-02-12 14:41:55 +0100

    Update chai assertions

--- api/tests/tooling/chai-custom-helpers/deep-equal-instance-omitting.js
+++ api/tests/tooling/chai-custom-helpers/deep-equal-instance-omitting.js
@@ -1,22 +1,23 @@
 import _ from 'lodash';
+import { Assertion } from 'chai';
 
-const deepEqualInstanceOmitting = function (chai, _utils) {
-  chai.Assertion.addMethod('deepEqualInstanceOmitting', function (referenceInstance, omittedAttributes) {
+const deepEqualInstanceOmitting = function () {
+  Assertion.addMethod('deepEqualInstanceOmitting', function (referenceInstance, omittedAttributes) {
     const assertedInstance = this._obj;
 
-    _assertAreSameType(chai, assertedInstance, referenceInstance);
-    _assertHaveSameContent(chai, assertedInstance, referenceInstance, omittedAttributes);
+    _assertAreSameType(assertedInstance, referenceInstance);
+    _assertHaveSameContent(assertedInstance, referenceInstance, omittedAttributes);
   });
 };
 
-function _assertAreSameType(chai, value1, value2) {
+function _assertAreSameType(value1, value2) {
   const instanceClassName1 = value1.constructor.name;
   const instanceClassName2 = value2.constructor.name;
-  new chai.Assertion(instanceClassName1).to.equal(instanceClassName2);
+  new Assertion(instanceClassName1).to.equal(instanceClassName2);
 }
 
-function _assertHaveSameContent(chai, value1, value2, omittedAttributes) {
-  new chai.Assertion(_.omit(value1, omittedAttributes)).to.deep.equal(_.omit(value2, omittedAttributes));
+function _assertHaveSameContent(value1, value2, omittedAttributes) {
+  new Assertion(_.omit(value1, omittedAttributes)).to.deep.equal(_.omit(value2, omittedAttributes));
 }
 
 export { deepEqualInstanceOmitting };
