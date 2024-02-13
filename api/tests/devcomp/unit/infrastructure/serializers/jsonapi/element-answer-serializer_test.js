f859eca822ed9cc304518d5c1d86faf7d64d1b86
dlahaye <dimitrilahaye.dev@gmail.com>
2024-02-13 11:58:23 +0100

    feat(api): add tests for QCM element-answer-serializer
    Co-authored-by: miranda.lin-guignard <miranda.lin-guignard@pix.fr>

--- api/tests/devcomp/unit/infrastructure/serializers/jsonapi/element-answer-serializer_test.js
+++ api/tests/devcomp/unit/infrastructure/serializers/jsonapi/element-answer-serializer_test.js
@@ -4,6 +4,7 @@ import { AnswerStatus } from '../../../../../../lib/domain/models/index.js';
 import * as elementAnswerSerializer from '../../../../../../src/devcomp/infrastructure/serializers/jsonapi/element-answer-serializer.js';
 import { ElementAnswer } from '../../../../../../src/devcomp/domain/models/ElementAnswer.js';
 import { QrocmCorrectionResponse } from '../../../../../../src/devcomp/domain/models/QrocmCorrectionResponse.js';
+import { QcmCorrectionResponse } from '../../../../../../src/devcomp/domain/models/QcmCorrectionResponse.js';
 
 describe('Unit | DevComp | Infrastructure | Serializers | Jsonapi | ElementAnswerSerializer', function () {
   describe('#serialize', function () {
@@ -109,6 +110,61 @@ describe('Unit | DevComp | Infrastructure | Serializers | Jsonapi | ElementAnswe
                 feedback: 'Good job!',
                 status: 'ok',
                 solution: givenCorrectionResponse.solution,
+              },
+              id: '222',
+              type: 'correction-responses',
+            },
+          ],
+        };
+
+        // when
+        const result = elementAnswerSerializer.serialize(elementAnswer);
+
+        // then
+        expect(result).to.deep.equal(expectedResult);
+      });
+    });
+
+    describe('When correction response is for QCM', function () {
+      it('should return a serialized ElementAnswer', function () {
+        // given
+        const solutions = ['1', '2'];
+        const userAnswerValue = ['3', '4'];
+        const givenCorrectionResponse = new QcmCorrectionResponse({
+          status: AnswerStatus.OK,
+          feedback: 'Good job!',
+          solutions,
+        });
+
+        const elementAnswer = new ElementAnswer({
+          id: 222,
+          elementId: '123',
+          userResponseValue: userAnswerValue,
+          correction: givenCorrectionResponse,
+        });
+        const expectedResult = {
+          data: {
+            attributes: {
+              'element-id': '123',
+              'user-response-value': userAnswerValue,
+            },
+            relationships: {
+              correction: {
+                data: {
+                  id: '222',
+                  type: 'correction-responses',
+                },
+              },
+            },
+            id: '222',
+            type: 'element-answers',
+          },
+          included: [
+            {
+              attributes: {
+                feedback: 'Good job!',
+                status: 'ok',
+                solutions: givenCorrectionResponse.solutions,
               },
               id: '222',
               type: 'correction-responses',
