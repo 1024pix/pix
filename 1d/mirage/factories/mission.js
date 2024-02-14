5bd9fc898bad767e50365e799d1f73cd799e47e8
rouxxi <gwenvael.laskar@pix.fr>
2024-02-14 14:32:02 +0100

    feat(1d): display mission learning objectives

--- 1d/mirage/factories/mission.js
+++ 1d/mirage/factories/mission.js
@@ -2,4 +2,7 @@ import { Factory } from 'miragejs';
 
 export default Factory.extend({
   name: 'Recherche sur internet',
+  areaCode: 1,
+  learningObjectives: 'learningObjectives',
+  validatedObjectives: 'validatedObjectives',
 });
