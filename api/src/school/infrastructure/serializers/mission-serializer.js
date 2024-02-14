0f3867d0884662d008caf388c57d1a45e952d4ef
rouxxi <gwenvael.laskar@pix.fr>
2024-02-14 14:31:17 +0100

    feat:(api): Add areaCode to mission

--- api/src/school/infrastructure/serializers/mission-serializer.js
+++ api/src/school/infrastructure/serializers/mission-serializer.js
@@ -2,7 +2,7 @@ import { Serializer } from 'jsonapi-serializer';
 
 const serialize = function (missions) {
   return new Serializer('missions', {
-    attributes: ['name', 'areaCode'],
+    attributes: ['name', 'areaCode', 'validatedObjectives', 'learningObjectives'],
   }).serialize(missions);
 };
 export { serialize };
