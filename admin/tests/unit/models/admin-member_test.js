af0671b1bd812da322dc341f4fac50bedb8a822d
François de Metz <francois.de-metz@pix.fr>
2024-02-14 10:39:08 +0100

    Fix new lint rules in tests

--- admin/tests/unit/models/admin-member_test.js
+++ admin/tests/unit/models/admin-member_test.js
@@ -1,6 +1,5 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
-import { run } from '@ember/runloop';
 
 module('Unit | Model | admin-member', function (hooks) {
   setupTest(hooks);
@@ -13,9 +12,7 @@ module('Unit | Model | admin-member', function (hooks) {
   module('#fullName', function () {
     test('it should return the fullname, combination of first and last name', function (assert) {
       // given
-      const adminMember = run(() => {
-        return store.createRecord('adminMember', { firstName: 'Jean-Baptiste', lastName: 'Poquelin' });
-      });
+      const adminMember = store.createRecord('adminMember', { firstName: 'Jean-Baptiste', lastName: 'Poquelin' });
 
       // when
       const fullName = adminMember.fullName;
