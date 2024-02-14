051e796bbf5b8ee4f3a0babf405c7e19241dda5c
rouxxi <gwenvael.laskar@pix.fr>
2024-02-14 14:32:02 +0100

    feat(1d): display mission learning objectives

--- 1d/app/models/mission.js
+++ 1d/app/models/mission.js
@@ -3,4 +3,6 @@ import Model, { attr } from '@ember-data/model';
 export default class Mission extends Model {
   @attr('string') name;
   @attr('string') areaCode;
+  @attr('string') learningObjectives;
+  @attr('string') validatedObjectives;
 }
