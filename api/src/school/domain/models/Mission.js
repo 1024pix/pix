2a915fb8d8c9d0a4d0c5f77254cd9bed20b06475
Yaelle YAMA <yaelle.marsiac@pix.fr>
2024-02-16 17:10:19 +0100

    feat(api): create secure route to display mission

--- api/src/school/domain/models/Mission.js
+++ api/src/school/domain/models/Mission.js
@@ -1,8 +1,19 @@
 class Mission {
-  constructor({ id, name, competenceId, thematicId, status, learningObjectives, validatedObjectives, areaCode } = {}) {
+  constructor({
+    id,
+    name,
+    competenceId,
+    competenceName,
+    thematicId,
+    status,
+    learningObjectives,
+    validatedObjectives,
+    areaCode,
+  } = {}) {
     this.id = id;
     this.name = name;
     this.competenceId = competenceId;
+    this.competenceName = competenceName;
     this.thematicId = thematicId;
     this.status = status;
     this.areaCode = areaCode;
