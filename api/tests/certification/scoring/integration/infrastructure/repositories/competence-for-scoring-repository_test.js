ed5df2931108eed36fdfcd81e43fb3856c7c84a8
Alexandre COIN <alexandre@mbpdealexandre-1.home>
2024-02-16 17:48:43 +0100

    :sparkles: api: add method to save competence scoring configuration

--- api/tests/certification/scoring/integration/infrastructure/repositories/competence-for-scoring-repository_test.js
+++ api/tests/certification/scoring/integration/infrastructure/repositories/competence-for-scoring-repository_test.js
@@ -3,43 +3,41 @@ import { buildArea, buildCompetence, buildFramework } from '../../../../../tooli
 import { buildLearningContent } from '../../../../../tooling/learning-content-builder/index.js';
 import { competenceLevelIntervals } from '../../../../../../src/certification/flash-certification/domain/constants/competence-level-intervals.js';
 import _ from 'lodash';
-import { listByLocale } from '../../../../../../src/certification/scoring/infrastructure/repositories/competence-for-scoring-repository.js';
+import {
+  listByLocale,
+  save,
+} from '../../../../../../src/certification/scoring/infrastructure/repositories/competence-for-scoring-repository.js';
 import { CompetenceForScoring } from '../../../../../../src/certification/scoring/domain/models/CompetenceForScoring.js';
+import { knex } from '../../../../../../db/knex-database-connection.js';
 
 describe('Unit | Repository | competence-for-scoring-repository', function () {
-  beforeEach(function () {
-    const frameworkId = 'frameworkId';
-
-    const getAreaCode = (competenceCode) => competenceCode.split('.').shift();
-    const competenceLevelIntervalsWithAreaCode = competenceLevelIntervals.map((competenceLevelInterval) => ({
-      ...competenceLevelInterval,
-      areaCode: getAreaCode(competenceLevelInterval.competence),
-    }));
-
-    const competenceLevelIntervalsByArea = _.groupBy(competenceLevelIntervalsWithAreaCode, 'areaCode');
-
-    const areas = Object.entries(competenceLevelIntervalsByArea).map(([areaCode, competenceLevelIntervals]) => {
-      const areaId = `recArea${areaCode}`;
-
-      const competences = competenceLevelIntervals.map((competenceLevelInterval) => {
-        const competenceIndex = competenceLevelInterval.competence;
-        const competenceId = `recCompetence${competenceIndex}`;
-
-        return buildCompetence({ id: competenceId, areaId, index: competenceIndex });
-      });
-
-      return buildArea({ id: areaId, frameworkId, code: areaCode, competences });
-    });
-
-    const framework = buildFramework({ id: frameworkId, name: 'someFramework', areas });
-
-    const learningContent = buildLearningContent([framework]);
-
-    mockLearningContent(learningContent);
-  });
-
   describe('#listByLocale', function () {
     it('should return a list of competences for scoring', async function () {
+      // given
+      const frameworkId = 'frameworkId';
+      const getAreaCode = (competenceCode) => competenceCode.split('.').shift();
+      const competenceLevelIntervalsWithAreaCode = competenceLevelIntervals.map((competenceLevelInterval) => ({
+        ...competenceLevelInterval,
+        areaCode: getAreaCode(competenceLevelInterval.competence),
+      }));
+      const competenceLevelIntervalsByArea = _.groupBy(competenceLevelIntervalsWithAreaCode, 'areaCode');
+      const areas = Object.entries(competenceLevelIntervalsByArea).map(([areaCode, competenceLevelIntervals]) => {
+        const areaId = `recArea${areaCode}`;
+
+        const competences = competenceLevelIntervals.map((competenceLevelInterval) => {
+          const competenceIndex = competenceLevelInterval.competence;
+          const competenceId = `recCompetence${competenceIndex}`;
+
+          return buildCompetence({ id: competenceId, areaId, index: competenceIndex });
+        });
+
+        return buildArea({ id: areaId, frameworkId, code: areaCode, competences });
+      });
+      const framework = buildFramework({ id: frameworkId, name: 'someFramework', areas });
+      const learningContent = buildLearningContent([framework]);
+
+      mockLearningContent(learningContent);
+
       // when
       const resultList = await listByLocale({ locale: 'fr-fr' });
 
@@ -48,6 +46,21 @@ describe('Unit | Repository | competence-for-scoring-repository', function () {
         expect(result).to.be.instanceOf(CompetenceForScoring);
       });
       expect(resultList.length).to.equal(16);
+    });
+  });
+
+  describe('#save', function () {
+    it('should save a configuration for competence scoring', async function () {
+      // given
+      const data = { some: 'data' };
+
+      // when
+      await save(data);
+
+      // then
+      const configurations = await knex('competence-scoring-configurations');
+      expect(configurations.length).to.equal(1);
+      expect(configurations[0].configuration).to.deep.equal({ some: 'data' });
     });
   });
 });
