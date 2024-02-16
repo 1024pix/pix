a5767efea1924e9201d7dfeb3f976319a75c7886
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2024-02-16 15:27:12 +0100

    feat(scripts): add modulix elements cli generators

---
+++ api/scripts/modulix/get-sample-qcu-element.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+import { getQcuSample } from '../../src/devcomp/infrastructure/datasources/learning-content/samples/elements/qcu.sample.js';
+
+console.log('');
+console.log(JSON.stringify(getQcuSample(), null, 2));
+console.log('');
+console.log('Voici un QCU.');
---
+++ api/scripts/modulix/get-sample-text-element.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+import { getTextSample } from '../../src/devcomp/infrastructure/datasources/learning-content/samples/elements/text.sample.js';
+
+console.log('');
+console.log(JSON.stringify(getTextSample(), null, 2));
+console.log('');
+console.log('Voici un petit texte.');
