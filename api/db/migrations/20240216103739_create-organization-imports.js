3f8270157b61ada10b8791acd7eddd8e506cd696
Yvonnick Frin <frin.yvonnick@gmail.com>
2024-02-16 14:13:03 +0100

    feat(db): create table organization-imports

---
+++ api/db/migrations/20240216103739_create-organization-imports.js
@@ -0,0 +1,29 @@
+const TABLE_NAME = 'organization-imports';
+
+const up = async function (knex) {
+  await knex.schema.raw(`CREATE TYPE "organization-imports-statuses" AS ENUM ( 'UPLOADED',
+	'VALIDATED',
+	'IMPORTED',
+	'VALIDATION_ERROR',
+	'IMPORT_ERROR'
+);`);
+
+  await knex.schema.createTable(TABLE_NAME, function (table) {
+    table.increments('id').primary().notNullable();
+    table.specificType('status', '"organization-imports-statuses"');
+    table.text('filename').notNullable();
+    table.text('encoding').nullable();
+    table.jsonb('errors').nullable();
+    table.dateTime('createdAt').notNullable().defaultTo(knex.fn.now());
+    table.bigInteger('createdBy').notNullable().index().references('users.id');
+    table.dateTime('updatedAt').notNullable().defaultTo(knex.fn.now());
+    table.integer('organizationId').notNullable().unsigned().references('organizations.id').index();
+  });
+};
+
+const down = async function (knex) {
+  await knex.schema.dropTable(TABLE_NAME);
+  await knex.schema.raw(`DROP TYPE "organization-imports-statuses"`);
+};
+
+export { up, down };
