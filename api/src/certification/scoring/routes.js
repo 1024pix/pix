4a903d5bdf5ea86c1d46500cf78e79c3dc453559
Alexandre COIN <alexandre@mbpdealexandre-1.home>
2024-02-16 17:49:22 +0100

    :sparkles: api: add route to save competence scoring configuration

---
+++ api/src/certification/scoring/routes.js
@@ -0,0 +1,5 @@
+import * as competenceForScoringConfiguration from './application/competence-for-scoring-configuration-route.js';
+
+const scoringRoutes = [competenceForScoringConfiguration];
+
+export { scoringRoutes };
