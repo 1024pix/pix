8884ae71fa170fc3f9e0856eec4c999111d962f1
dlahaye <dimitrilahaye.dev@gmail.com>
2024-02-19 09:07:14 +0100

    feat(mon-pix): add mapping between passage and element-answer

--- mon-pix/app/pods/passage/model.js
+++ mon-pix/app/pods/passage/model.js
@@ -1,5 +1,7 @@
-import Model, { attr } from '@ember-data/model';
+import Model, { attr, hasMany } from '@ember-data/model';
 
 export default class Passage extends Model {
   @attr('string') moduleId;
+
+  @hasMany('element-answer', { async: false, inverse: 'passage' }) elementAnswers;
 }
