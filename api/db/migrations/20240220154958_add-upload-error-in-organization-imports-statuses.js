4f9cef9dbd85345c02c717e40aedf00213dfe55a
Yvonnick Frin <frin.yvonnick@gmail.com>
2024-02-20 16:54:17 +0100

    feat(db): add missing value UPLOAD_ERROR in import statuses

---
+++ api/db/migrations/20240220154958_add-upload-error-in-organization-imports-statuses.js
@@ -0,0 +1,9 @@
+const up = async function (knex) {
+  await knex.schema.raw(`ALTER TYPE "organization-imports-statuses" ADD VALUE 'UPLOAD_ERROR'`);
+};
+
+const down = async function () {
+  // nothing
+};
+
+export { up, down };
