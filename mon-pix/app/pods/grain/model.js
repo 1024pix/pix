50722dcd3c5090f246bda4a22f0ba5b548dae1b4
Vincent Hardouin <vincent.hardouin@pix.fr>
2024-02-21 14:52:55 +0100

    feat(mon-pix): allow API to return new element type without breaking frontend
    The element model is a read-only template. In this case, Ember recommends not using it. (cf: https://guides.emberjs.com/release/models/relationships/#toc_readonly-nested-data)

--- mon-pix/app/pods/grain/model.js
+++ mon-pix/app/pods/grain/model.js
@@ -1,17 +1,17 @@
-import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
+import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class Grain extends Model {
   @attr('string') title;
+  @attr({ defaultValue: () => [] }) rawElements;
 
-  @hasMany('element', { async: false, polymorphic: true, inverse: 'grain' }) elements;
   @belongsTo('module', { async: false, inverse: 'grains' }) module;
 
   get hasAnswerableElements() {
-    return this.elements.some((element) => element.isAnswerable);
+    return this.rawElements.some((element) => element.isAnswerable);
   }
 
   get answerableElements() {
-    return this.elements.filter((element) => {
+    return this.rawElements.filter((element) => {
       return element.isAnswerable;
     });
   }
