d8ec666803a69352ac0fd6adbaf91eb85d6c9c11
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2024-02-21 14:20:09 +0100

    fix(scripts): improve element samples copy-pasting by removing overhead from generators

--- api/scripts/modulix/get-sample-text-element.js
+++ api/scripts/modulix/get-sample-text-element.js
@@ -1,6 +1,3 @@
 import { getTextSample } from '../../src/devcomp/infrastructure/datasources/learning-content/samples/elements/text.sample.js';
 
-console.log('');
 console.log(JSON.stringify(getTextSample(), null, 2));
-console.log('');
-console.log('Voici un petit texte.');
