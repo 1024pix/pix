a2c0e246160c310dba29a90863be8b7cd775396e
Marie-Hélène <marie-helene.rots@pix.fr>
2024-02-21 12:19:12 +0100

    fix(orga): add lastSharedOrCurrentCampaignParticipationId to organizationLearnerActivity

--- api/tests/prescription/organization-learner/unit/infrastructure/serializers/jsonapi/organization-learner-activity-serializer_test.js
+++ api/tests/prescription/organization-learner/unit/infrastructure/serializers/jsonapi/organization-learner-activity-serializer_test.js
@@ -19,6 +19,7 @@ describe('Unit | Serializer | JSONAPI | organization-learner-participation-seria
             sharedAt: '2000-02-01T10:00:00Z',
             status: 'SHARED',
             participationCount: '1',
+            lastSharedOrCurrentCampaignParticipationId: '99999',
           }),
           new OrganizationLearnerParticipation({
             id: '100000',
@@ -29,6 +30,7 @@ describe('Unit | Serializer | JSONAPI | organization-learner-participation-seria
             sharedAt: '2000-04-01T10:00:00Z',
             status: 'STARTED',
             participationCount: '2',
+            lastSharedOrCurrentCampaignParticipationId: '100000',
           }),
         ],
         statistics: [
@@ -95,6 +97,7 @@ describe('Unit | Serializer | JSONAPI | organization-learner-participation-seria
               'shared-at': '2000-02-01T10:00:00Z',
               status: 'SHARED',
               'participation-count': '1',
+              'last-shared-or-current-campaign-participation-id': '99999',
             },
           },
           {
@@ -108,6 +111,7 @@ describe('Unit | Serializer | JSONAPI | organization-learner-participation-seria
               'shared-at': '2000-04-01T10:00:00Z',
               status: 'STARTED',
               'participation-count': '2',
+              'last-shared-or-current-campaign-participation-id': '100000',
             },
           },
           {
