1934e38f38aba2caddea63a6f9128fb7a12468d1
Marianne Bost <marianne.bost@pix.fr>
2024-02-23 16:52:11 +0100

    feat(api): handle html error in joi error parser
    Co-authored-by: Yann Bertrand <yann.bertrand@pix.fr>
    Co-authored-by: miranda.lin-guignard <miranda.lin-guignard@pix.fr>
    Co-authored-by: Dimitri Lahaye <dimitri.lahaye@pix.fr>

--- api/tests/devcomp/unit/infrastructure/datasources/learning-content/validation/joi-error-parser.js
+++ api/tests/devcomp/unit/infrastructure/datasources/learning-content/validation/joi-error-parser.js
@@ -1,12 +1,34 @@
 export const joiErrorParser = {
   format(error) {
-    const objectErrorSeparator = `\n${'─'.repeat(60)}\n`;
+    const severity = ['', 'Warning', 'Error'];
     const visualSeparator = `\n${'='.repeat(60)}\n`;
+    const objectErrorSeparator = `\n${'─'.repeat(60)}\n`;
+
     return `${visualSeparator}${error.details
-      .map(
-        (error) =>
-          `\nError: ${error.message}.\nValeur concernée à rechercher : ${JSON.stringify(error.context.value)}\n`,
-      )
+      .map((errorDetail) => {
+        if (errorDetail.type === 'external') {
+          const report = errorDetail.context.value;
+          const errorLogs = [];
+          for (const result of report.results) {
+            const line = result.source ?? '';
+            for (const message of result.messages) {
+              let errorLog = '';
+              errorLog = errorLog.concat('\n');
+              errorLog = errorLog.concat('\n', severity[message.severity], `(${message.ruleId}): `, message.message);
+              errorLog = errorLog.concat('\n', message.ruleUrl);
+              errorLog = errorLog.concat('\n');
+              errorLog = errorLog.concat('\n', 'Valeur concernée à rechercher :\n', line);
+              errorLog = errorLog.concat('\n');
+              errorLogs.push(errorLog);
+            }
+          }
+          return errorLogs.join(objectErrorSeparator);
+        } else {
+          return `\nError: ${errorDetail.message}.\nValeur concernée à rechercher : ${JSON.stringify(
+            errorDetail.context.value,
+          )}\n`;
+        }
+      })
       .join(objectErrorSeparator)}${visualSeparator}`;
   },
 };
