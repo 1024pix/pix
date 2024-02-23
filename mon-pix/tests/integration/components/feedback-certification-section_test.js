70f7877b723dbdac0aafffd5a5cfc0a67c909fa2
Andreia Pena <andreia.pena@pix.fr>
2024-02-23 16:48:54 +0100

    💬 mon-pix: fix wording on feedback certification section

--- mon-pix/tests/integration/components/feedback-certification-section_test.js
+++ mon-pix/tests/integration/components/feedback-certification-section_test.js
@@ -1,22 +1,28 @@
 import { module, test } from 'qunit';
 import setupIntlRenderingTest from '../../helpers/setup-intl-rendering';
-// eslint-disable-next-line no-restricted-imports
-import { find } from '@ember/test-helpers';
-import { render } from '@1024pix/ember-testing-library';
+import { render, within } from '@1024pix/ember-testing-library';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | feedback-certification-section', function (hooks) {
   setupIntlRenderingTest(hooks);
 
   test('renders', async function (assert) {
-    await render(hbs`<FeedbackCertificationSection />`);
+    // given & when
+    const screen = await render(hbs`<FeedbackCertificationSection />`);
 
-    assert.ok(
-      find('.feedback-certification-section__div')
-        .textContent.trim()
-        .includes(
+    // then
+    const list = screen.getByRole('list');
+    const items = within(list).getAllByRole('listitem');
+    assert.strictEqual(items[0].textContent.trim(), "votre numéro de certification (en haut à droite de l'écran)");
+    assert.strictEqual(items[1].textContent.trim(), "le numéro de la question (en haut à droite de l'écran)");
+    assert.strictEqual(items[2].textContent.trim(), 'le problème rencontré');
+
+    assert
+      .dom(
+        screen.getByText(
           'Pour signaler un problème, appelez votre surveillant et communiquez-lui les informations suivantes :',
         ),
-    );
+      )
+      .exists();
   });
 });
