48026e94c5f47ce92ecc672dba6a7a5af66bcd26
Marie-Hélène <marie-helene.rots@pix.fr>
2024-02-23 15:59:43 +0100

    fix(api): add certificability calculation possibility to organizationLearner details page

---
+++ api/tests/unit/domain/read-models/organization-learner-follow-up/OrganizationLearner_test.js
@@ -0,0 +1,68 @@
+import { expect } from '../../../../test-helper.js';
+import { OrganizationLearner } from '../../../../../lib/domain/read-models/organization-learner-follow-up/OrganizationLearner.js';
+
+describe('Unit | Domain | Read-models | OrganizationLearner', function () {
+  it('should return certificability from learner', function () {
+    // given
+
+    // when
+    const organizationLearner = new OrganizationLearner({
+      certifiableAtFromCampaign: new Date('2022-01-01'),
+      isCertifiableFromCampaign: false,
+      isCertifiableFromLearner: true,
+      certifiableAtFromLearner: new Date('2023-01-01'),
+    });
+
+    // then
+    expect(organizationLearner.isCertifiable).to.be.true;
+    expect(organizationLearner.certifiableAt).to.be.deep.equal(new Date('2023-01-01'));
+  });
+
+  it('should return certificability from campaign', function () {
+    // given
+
+    // when
+    const organizationLearner = new OrganizationLearner({
+      certifiableAtFromCampaign: new Date('2023-01-01'),
+      isCertifiableFromCampaign: false,
+      isCertifiableFromLearner: true,
+      certifiableAtFromLearner: new Date('2022-01-01'),
+    });
+
+    // then
+    expect(organizationLearner.isCertifiable).to.be.false;
+    expect(organizationLearner.certifiableAt).to.be.deep.equal(new Date('2023-01-01'));
+  });
+
+  it('should return certificability from learner when campaign is null', function () {
+    // given
+
+    // when
+    const organizationLearner = new OrganizationLearner({
+      certifiableAtFromCampaign: null,
+      isCertifiableFromCampaign: null,
+      isCertifiableFromLearner: true,
+      certifiableAtFromLearner: new Date('2022-01-01'),
+    });
+
+    // then
+    expect(organizationLearner.isCertifiable).to.be.true;
+    expect(organizationLearner.certifiableAt).to.be.deep.equal(new Date('2022-01-01'));
+  });
+
+  it('should return certificability from campaign when learner is null', function () {
+    // given
+
+    // when
+    const organizationLearner = new OrganizationLearner({
+      certifiableAtFromCampaign: new Date('2023-01-01'),
+      isCertifiableFromCampaign: true,
+      isCertifiableFromLearner: null,
+      certifiableAtFromLearner: null,
+    });
+
+    // then
+    expect(organizationLearner.isCertifiable).to.be.true;
+    expect(organizationLearner.certifiableAt).to.be.deep.equal(new Date('2023-01-01'));
+  });
+});
