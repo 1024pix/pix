f2ed54c5ae5a9a53c1acbb1767cec5769d36de22
Marianne Bost <marianne.bost@pix.fr>
2024-02-27 11:44:00 +0100

    feat(mon-pix): add footer and header to details module page
    Co-authored-by: miranda.lin-guignard <miranda.lin-guignard@pix.fr>
    Co-authored-by: Dimitri Lahaye <dimitri.lahaye@pix.fr>
    Co-authored-by: Vincent Hardouin <vincent.hardouin@pix.fr>

--- mon-pix/tests/acceptance/module/visit-module-details-page_test.js
+++ mon-pix/tests/acceptance/module/visit-module-details-page_test.js
@@ -4,37 +4,11 @@ import { currentURL } from '@ember/test-helpers';
 import { visit } from '@1024pix/ember-testing-library';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 
-module('Acceptance | Module | Routes | get', function (hooks) {
+module('Acceptance | Module | Routes | details', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  test('can visit /modules/:slug/details', async function (assert) {
-    // given
-    const grain = server.create('grain', {
-      id: 'grain1',
-    });
-    server.create('module', {
-      id: 'bien-ecrire-son-adresse-mail',
-      title: 'Bien écrire son adresse mail',
-      details: {
-        image: 'https://images.pix.fr/modulix/bien-ecrire-son-adresse-mail-details.svg',
-        description:
-          'Apprendre à rédiger correctement une adresse e-mail pour assurer une meilleure communication et éviter les erreurs courantes.',
-        duration: 12,
-        level: 'Débutant',
-        objectives: ['Écrire une adresse mail correctement, en évitant les erreurs courantes'],
-      },
-      grains: [grain],
-    });
-
-    // when
-    await visit('/modules/bien-ecrire-son-adresse-mail/details');
-
-    // then
-    assert.strictEqual(currentURL(), '/modules/bien-ecrire-son-adresse-mail/details');
-  });
-
-  test('should include the module title inside the page title', async function (assert) {
+  test('should visit and include the module title, header and footer', async function (assert) {
     // given
     const module = server.create('module', {
       id: 'bien-ecrire-son-adresse-mail',
@@ -51,10 +25,14 @@ module('Acceptance | Module | Routes | get', function (hooks) {
     });
 
     // when
-    await visit('/modules/bien-ecrire-son-adresse-mail/details');
+    const screen = await visit('/modules/bien-ecrire-son-adresse-mail/details');
 
     // then
+    assert.strictEqual(currentURL(), '/modules/bien-ecrire-son-adresse-mail/details');
     assert.ok(document.title.includes(module.title));
+    assert.dom(screen.getByRole('alert')).exists();
+    assert.dom(screen.getByRole('banner')).exists();
+    assert.dom(screen.getByRole('contentinfo')).exists();
   });
 
   test('should redirect /modules/:slug to /modules/:slug/details', async function (assert) {
