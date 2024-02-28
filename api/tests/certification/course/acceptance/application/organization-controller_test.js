7974534b72dedcde28c2371ad29dc7ca0c400140
Steph0 <Steph0@users.noreply.github.com>
2024-02-28 18:41:34 +0100

    :bug: api:  get candidates only when they entered a session to which they were registered

--- api/tests/certification/course/acceptance/application/organization-controller_test.js
+++ api/tests/certification/course/acceptance/application/organization-controller_test.js
@@ -8,8 +8,10 @@ import {
 import { createServer } from '../../../../../server.js';
 import { Membership } from '../../../../../lib/domain/models/Membership.js';
 import { AutoJuryCommentKeys } from '../../../../../src/certification/shared/domain/models/JuryComment.js';
+import { AssessmentResult } from '../../../../../lib/domain/models/index.js';
 
 describe('Certification | Course | Acceptance | Application | organization-controller', function () {
+  const BOM_CHAR = '\ufeff';
   let server;
 
   beforeEach(async function () {
@@ -29,22 +31,42 @@ describe('Certification | Course | Acceptance | Application | organization-contr
         organizationRole: Membership.roles.ADMIN,
       });
 
+      const sessionId = databaseBuilder.factory.buildSession({ id: 10242048, publishedAt: new Date('2024-01-01') }).id;
+
       const organizationLearner = databaseBuilder.factory.buildOrganizationLearner({
         organizationId: organization.id,
         division: 'aDivision',
       });
       const candidate = databaseBuilder.factory.buildCertificationCandidate({
         organizationLearnerId: organizationLearner.id,
+        sessionId,
       });
       const certificationCourse = databaseBuilder.factory.buildCertificationCourse({
+        id: 20484096,
         userId: candidate.userId,
         sessionId: candidate.sessionId,
         isPublished: true,
+        isCancelled: true,
       });
-
       databaseBuilder.factory.buildAssessmentResult.last({
+        pixScore: 0,
+        status: AssessmentResult.status.REJECTED,
         certificationCourseId: certificationCourse.id,
         commentByAutoJury: AutoJuryCommentKeys.CANCELLED_DUE_TO_NEUTRALIZATION,
+      });
+
+      const organizationLearnerDidNotComeToTheSession = databaseBuilder.factory.buildOrganizationLearner({
+        firstName: 'didNotCome',
+        organizationId: organization.id,
+        division: 'aDivision',
+      });
+      const candidateDidNotComeToTheSession = databaseBuilder.factory.buildCertificationCandidate({
+        organizationLearnerId: organizationLearnerDidNotComeToTheSession.id,
+        sessionId,
+      });
+      databaseBuilder.factory.buildCertificationCourse({
+        sessionId: candidateDidNotComeToTheSession.sessionId,
+        isPublished: true,
       });
       await databaseBuilder.commit();
 
@@ -59,6 +81,9 @@ describe('Certification | Course | Acceptance | Application | organization-contr
 
       // then
       expect(response.statusCode).to.equal(200);
+      expect(response.payload).to.equal(
+        `${BOM_CHAR}"Numéro de certification";"Prénom";"Nom";"Date de naissance";"Lieu de naissance";"Identifiant Externe";"Statut";"Nombre de Pix";"1.1";"1.2";"1.3";"2.1";"2.2";"2.3";"2.4";"3.1";"3.2";"3.3";"3.4";"4.1";"4.2";"4.3";"5.1";"5.2";"Commentaire jury pour l’organisation";"Session";"Date de passage de la certification"\n20484096;"first-name";"last-name";"21/05/2001";"Paris";"externalId";"Annulée";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"-";"Un ou plusieurs problème(s) technique(s), a/ont affecté le bon déroulement du test de certification. Nous ne sommes pas en mesure de délivrer la certification, celle-ci est donc annulée. Cette information peut vous conduire à proposer une nouvelle session de certification pour ce(cette) candidat(e).";10242048;"01/01/2022"`,
+      );
     });
   });
 });
