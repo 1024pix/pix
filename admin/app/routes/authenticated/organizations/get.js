396f5f9aa20b70cc1463a0bf90d5b1131b2f4073
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2024-02-28 18:34:16 +0100

    fix(admin): loadAllAvailableIdentityProviders in beforeModel for organization

--- admin/app/routes/authenticated/organizations/get.js
+++ admin/app/routes/authenticated/organizations/get.js
@@ -2,7 +2,12 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
 export default class GetRoute extends Route {
+  @service oidcIdentityProviders;
   @service store;
+
+  async beforeModel() {
+    await this.oidcIdentityProviders.loadAllAvailableIdentityProviders();
+  }
 
   model(params) {
     return this.store.findRecord('organization', params.organization_id, { reload: true });
