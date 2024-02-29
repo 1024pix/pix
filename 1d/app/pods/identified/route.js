87c79332447c5048df09e7626002c8dd03c64d6e
rouxxi <gwenvael.laskar@pix.fr>
2024-02-29 12:07:36 +0100

    feat(1d): Change background image on 3 pages

--- 1d/app/pods/identified/route.js
+++ 1d/app/pods/identified/route.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import { styleToolkit } from '../../utils/layout';
 
 export default class IdentifiedRoute extends Route {
   @service router;
@@ -9,6 +10,7 @@ export default class IdentifiedRoute extends Route {
     if (!this.currentLearner.learner) {
       this.router.transitionTo('organization-code');
     }
+    styleToolkit.backgroundBlob.reset();
   }
 
   async model() {
