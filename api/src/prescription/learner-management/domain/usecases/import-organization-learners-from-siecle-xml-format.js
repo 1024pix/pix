325d7c50afe00046dfad62eb9e255da602e7123a
Yvonnick Frin <frin.yvonnick@gmail.com>
2024-02-29 15:22:18 +0100

    feat(api): make import usecase store import state at each step

--- api/src/prescription/learner-management/domain/usecases/import-organization-learners-from-siecle-xml-format.js
+++ api/src/prescription/learner-management/domain/usecases/import-organization-learners-from-siecle-xml-format.js
@@ -12,6 +12,7 @@ import { SiecleParser } from '../../infrastructure/serializers/xml/siecle-parser
 import { SiecleFileStreamer } from '../../infrastructure/utils/xml/siecle-file-streamer.js';
 import * as zip from '../../infrastructure/utils/xml/zip.js';
 import { detectEncoding } from '../../infrastructure/utils/xml/detect-encoding.js';
+import { OrganizationImport } from '../models/OrganizationImport.js';
 
 const ERRORS = {
   EMPTY: 'EMPTY',
@@ -19,11 +20,13 @@ const ERRORS = {
 };
 
 const importOrganizationLearnersFromSIECLEXMLFormat = async function ({
+  userId,
   organizationId,
   payload,
   organizationLearnerRepository,
   organizationRepository,
   importStorage,
+  organizationImportRepository,
   siecleService = {
     unzip: zip.unzip,
     detectEncoding,
@@ -31,18 +34,30 @@ const importOrganizationLearnersFromSIECLEXMLFormat = async function ({
 }) {
   let organizationLearnerData = [];
 
+  let organizationImport = OrganizationImport.create({ organizationId, createdBy: userId });
+
   const organization = await organizationRepository.get(organizationId);
   const path = payload.path;
 
   const { file: filePath, directory } = await siecleService.unzip(path);
   const encoding = await siecleService.detectEncoding(filePath);
 
-  const filename = await importStorage.sendFile({ filepath: filePath });
-
+  let filename;
+  const errors = [];
   try {
+    filename = await importStorage.sendFile({ filepath: filePath });
     if (directory) {
       await fs.rm(directory, { recursive: true });
     }
+  } catch (error) {
+    errors.push(error);
+    throw error;
+  } finally {
+    organizationImport.upload({ filename, encoding, errors });
+    await organizationImportRepository.save(organizationImport);
+  }
+
+  try {
     const readableStreamForUAJ = await importStorage.readFile({ filename });
     const siecleFileStreamerForUAJ = await SiecleFileStreamer.create(readableStreamForUAJ, encoding);
     const parserForUAJ = SiecleParser.create(organization, siecleFileStreamerForUAJ);
@@ -52,32 +67,46 @@ const importOrganizationLearnersFromSIECLEXMLFormat = async function ({
     const siecleFileStreamer = await SiecleFileStreamer.create(readableStream, encoding);
     const parser = SiecleParser.create(organization, siecleFileStreamer);
     organizationLearnerData = await parser.parse();
+    if (isEmpty(organizationLearnerData)) {
+      throw new SiecleXmlImportError(ERRORS.EMPTY);
+    }
+  } catch (error) {
+    errors.push(error);
+    throw error;
   } finally {
+    organizationImport = await organizationImportRepository.getByOrganizationId(organizationId);
+    organizationImport.validate({ errors });
+    await organizationImportRepository.save(organizationImport);
     await importStorage.deleteFile({ filename });
   }
 
-  if (isEmpty(organizationLearnerData)) {
-    throw new SiecleXmlImportError(ERRORS.EMPTY);
-  }
-
-  const organizationLearnersChunks = chunk(organizationLearnerData, ORGANIZATION_LEARNER_CHUNK_SIZE);
-
-  const nationalStudentIdData = organizationLearnerData.map((learner) => learner.nationalStudentId, []);
-
   return DomainTransaction.execute(async (domainTransaction) => {
-    await organizationLearnerRepository.disableAllOrganizationLearnersInOrganization({
-      domainTransaction,
-      organizationId,
-      nationalStudentIds: nationalStudentIdData,
-    });
+    try {
+      const organizationLearnersChunks = chunk(organizationLearnerData, ORGANIZATION_LEARNER_CHUNK_SIZE);
 
-    await bluebird.mapSeries(organizationLearnersChunks, (chunk) => {
-      return organizationLearnerRepository.addOrUpdateOrganizationOfOrganizationLearners(
-        chunk,
-        organizationId,
+      const nationalStudentIdData = organizationLearnerData.map((learner) => learner.nationalStudentId, []);
+
+      await organizationLearnerRepository.disableAllOrganizationLearnersInOrganization({
         domainTransaction,
-      );
-    });
+        organizationId,
+        nationalStudentIds: nationalStudentIdData,
+      });
+
+      await bluebird.mapSeries(organizationLearnersChunks, (chunk) => {
+        return organizationLearnerRepository.addOrUpdateOrganizationOfOrganizationLearners(
+          chunk,
+          organizationId,
+          domainTransaction,
+        );
+      });
+    } catch (error) {
+      errors.push(error);
+      throw error;
+    } finally {
+      organizationImport = await organizationImportRepository.getByOrganizationId(organizationId);
+      organizationImport.process({ errors });
+      await organizationImportRepository.save(organizationImport);
+    }
   });
 };
 
