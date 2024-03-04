4bde2bd977c88b0bbb78f86284c24299b7969a07
Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
2024-03-04 12:09:11 +0100

    fix(api): modify badge creation payload check

--- api/lib/application/target-profiles/index.js
+++ api/lib/application/target-profiles/index.js
@@ -279,19 +279,17 @@ const register = async function (server) {
                 title: Joi.string().required().allow('').allow(null),
                 'is-certifiable': Joi.boolean().required(),
                 'is-always-visible': Joi.boolean().required(),
-                'campaign-threshold': Joi.number().min(0).max(100),
-                'capped-tubes-criteria': Joi.array()
-                  .min(1)
-                  .items({
-                    name: Joi.string(),
-                    threshold: Joi.string().required(),
-                    cappedTubes: Joi.array()
-                      .min(1)
-                      .items({
-                        id: Joi.string(),
-                        level: Joi.number().min(0),
-                      }),
-                  }),
+                'campaign-threshold': Joi.number().min(0).max(100).allow(null),
+                'capped-tubes-criteria': Joi.array().items({
+                  name: Joi.string(),
+                  threshold: Joi.string().required(),
+                  cappedTubes: Joi.array()
+                    .min(1)
+                    .items({
+                      id: Joi.string(),
+                      level: Joi.number().min(0),
+                    }),
+                }),
               })
                 .or('campaign-threshold', 'capped-tubes-criteria')
                 .required(),
