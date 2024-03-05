b52325880079763e4376aeb43e710f698e2d1da5
Yaelle YAMA <yaelle.marsiac@pix.fr>
2024-03-05 11:47:30 +0100

    feat(orga): add no division state

--- orga/tests/acceptance/missions-list_test.js
+++ orga/tests/acceptance/missions-list_test.js
@@ -37,9 +37,41 @@ module('Acceptance | Missions List', function (hooks) {
       const screen = await visit('/missions');
 
       // then
-      assert.deepEqual(currentURL(), '/missions');
       assert.dom(screen.getByText('Super Mission')).exists();
       assert.dom(screen.getByText('Super competence')).exists();
+    });
+
+    module('display divisions', function () {
+      test('should display division list when there are some', async function (assert) {
+        // given
+        const user = createUserWithMembershipAndTermsOfServiceAccepted();
+        const prescriber = createPrescriberByUser(user);
+        prescriber.features = { ...prescriber.features, MISSIONS_MANAGEMENT: true };
+        await authenticateSession(user.id);
+
+        server.create('mission', { name: 'Super Mission', competenceName: 'Super competence', startedBy: 'CM2' });
+
+        // when
+        const screen = await visit('/missions');
+
+        // then
+        assert.dom(screen.getByText('CM2')).exists();
+      });
+      test('should display no division message when there are none', async function (assert) {
+        // given
+        const user = createUserWithMembershipAndTermsOfServiceAccepted();
+        const prescriber = createPrescriberByUser(user);
+        prescriber.features = { ...prescriber.features, MISSIONS_MANAGEMENT: true };
+        await authenticateSession(user.id);
+
+        server.create('mission', { name: 'Super Mission', competenceName: 'Super competence', startedBy: '' });
+
+        // when
+        const screen = await visit('/missions');
+
+        // then
+        assert.dom(screen.getByText(this.intl.t('pages.missions.list.no-division'))).exists();
+      });
     });
 
     test('it should see empty state when there is no mission', async function (assert) {
@@ -54,7 +86,7 @@ module('Acceptance | Missions List', function (hooks) {
 
       // then
       assert.deepEqual(currentURL(), '/missions');
-      assert.dom(screen.getByText(this.intl.t('pages.missions.list.tab.empty-state'))).exists();
+      assert.dom(screen.getByText(this.intl.t('pages.missions.list.empty-state'))).exists();
     });
 
     test('user should acces to detail when he click on a row', async function (assert) {
