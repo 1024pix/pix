8634ed2c7a6705d45b931881dd94b9fa6ebc156a
Alexandre COIN <alexandre@mbpdealexandre-1.home>
2024-03-07 11:34:55 +0100

    :sparkles: api: get algo configuration depending on date

--- api/src/certification/flash-certification/infrastructure/repositories/flash-algorithm-configuration-repository.js
+++ api/src/certification/flash-certification/infrastructure/repositories/flash-algorithm-configuration-repository.js
@@ -1,5 +1,6 @@
 import { knex } from '../../../../../db/knex-database-connection.js';
 import { FlashAssessmentAlgorithmConfiguration } from '../../domain/models/FlashAssessmentAlgorithmConfiguration.js';
+import { NotFoundError } from '../../../../../lib/domain/errors.js';
 
 const TABLE_NAME = 'flash-algorithm-configurations';
 
@@ -17,4 +18,23 @@ const get = async function () {
   return FlashAssessmentAlgorithmConfiguration.fromDTO(flashAlgorithmConfiguration);
 };
 
-export { get, save };
+const getMostRecentBeforeDate = async (date) => {
+  const flashAlgorithmConfiguration = await knex(TABLE_NAME)
+    .where('createdAt', '<=', date)
+    .orderBy('createdAt', 'desc')
+    .first();
+
+  if (flashAlgorithmConfiguration) {
+    return FlashAssessmentAlgorithmConfiguration.fromDTO(flashAlgorithmConfiguration);
+  }
+
+  const firstFlashAlgoConfiguration = await knex(TABLE_NAME).orderBy('createdAt', 'asc').first();
+
+  if (!firstFlashAlgoConfiguration) {
+    throw new NotFoundError('Configuration not found');
+  }
+
+  return FlashAssessmentAlgorithmConfiguration.fromDTO(firstFlashAlgoConfiguration);
+};
+
+export { save, get, getMostRecentBeforeDate };
