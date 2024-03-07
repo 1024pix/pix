7c00b372f2492f1b41a8f444c7da5d204b06199c
Vincent Hardouin <vincent.hardouin@pix.fr>
2024-03-07 14:55:32 +0100

    feat(mon-pix): send terminate passage action
    Co-authored-by: Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>

--- mon-pix/mirage/routes/passages/index.js
+++ mon-pix/mirage/routes/passages/index.js
@@ -1,7 +1,9 @@
-import submitAnswer from './submit-answer';
 import createPassage from './create-passage';
+import submitAnswer from './submit-answer';
+import terminatePassage from './terminate-passage';
 
 export default function index(config) {
-  config.post('/passages/:passageId/answers', submitAnswer);
   config.post('/passages', createPassage);
+  config.post('/passages/:passageId/answers', submitAnswer);
+  config.post('/passages/:passageId/terminate', terminatePassage);
 }
