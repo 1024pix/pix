2b6ffc3ded548351e10fbe0d5b890daa201d8003
miranda.lin-guignard <miranda.w.lin@gmail.com>
2024-03-07 17:25:58 +0100

    test(api): corrige acceptance test qui est liee au contenu

--- api/tests/devcomp/acceptance/application/passages/passage-controller_test.js
+++ api/tests/devcomp/acceptance/application/passages/passage-controller_test.js
@@ -107,7 +107,7 @@ describe('Acceptance | Controller | passage-controller', function () {
           userResponse: [{ input: 'email', answer: 'naomizao457@yahoo.com' }],
           expectedUserResponseValue: { email: 'naomizao457@yahoo.com' },
           expectedFeedback:
-            '<p>Correct ! <span aria-hidden="true">🎉</span> Tout est en ordre : identifiant, arobase, fournisseur d\'adresse mail</p>',
+            "<p>Correct. <span aria-hidden=\"true\">🎉</span> Tout est dans l'ordre : l'identifiant, l'arobase puis le fournisseur d'adresse mail</p>",
           expectedSolution: {
             email: ['naomizao457@yahoo.com', 'naomizao457@yahoo.fr'],
           },
