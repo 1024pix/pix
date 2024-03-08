8279016e69150b729f6430fd84f6526ed79a9526
Quentin Lebouc <quentin.lebouc@pix.fr>
2024-03-08 17:27:57 +0100

    feat(admin): create smart random simulator interface

---
+++ admin/app/components/smart-random-simulator/previous-challenges.js
@@ -0,0 +1,7 @@
+import Component from '@glimmer/component';
+
+export default class PreviousChallenges extends Component {
+  incrementIndex(index) {
+    return index + 1;
+  }
+}
---
+++ admin/app/components/smart-random-simulator/smart-random-params.js
@@ -0,0 +1,111 @@
+import { action } from '@ember/object';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import Joi from 'joi';
+
+export default class SmartRandomParams extends Component {
+  @tracked errors = {};
+
+  acceptedLocales = [
+    { value: 'en', label: 'en' },
+    { value: 'fr', label: 'fr' },
+    { value: 'fr-fr', label: 'fr-fr' },
+    { value: 'nl', label: 'nl' },
+  ];
+
+  skillsExample = [{ id: 'skillId', name: '@requete2', difficulty: 3 }];
+  challengesExample = [
+    {
+      id: 'rec1xAgCoZux1Lxq8',
+      skill: { name: '@requete2', difficulty: 2, id: 'skillId' },
+      locales: ['fr-fr'],
+    },
+  ];
+
+  knowledgeElementsExample = [
+    {
+      id: 'rec1xAgCoZux1Lxq8',
+      source: 'direct',
+      status: 'validated',
+      skillId: 'skill123',
+      answerId: '12345',
+    },
+  ];
+  answersExample = [
+    { id: '12345', result: 'ok', assessmentId: 123456, challengeId: 'rec1234567', value: 'User answer' },
+  ];
+
+  validationSchema = {
+    skills: Joi.array()
+      .items({
+        id: Joi.string().required(),
+        difficulty: Joi.number().integer().min(1).max(8).required(),
+        name: Joi.string().required(),
+      })
+      .min(1)
+      .required(),
+    challenges: Joi.array()
+      .items({
+        id: Joi.string().required(),
+        timer: Joi.number(),
+        skill: {
+          id: Joi.string().required(),
+          name: Joi.string().required(),
+        },
+        locales: Joi.array()
+          .items(Joi.string().valid(...this.acceptedLocales.map((locale) => locale.value)))
+          .required(),
+      })
+      .min(1)
+      .required(),
+    knowledgeElements: Joi.array()
+      .items({
+        source: Joi.string().valid('direct', 'inferred').required(),
+        status: Joi.string().valid('validated', 'invalidated', 'reset').required(),
+        answerId: Joi.string().required(),
+        skillId: Joi.string().required(),
+      })
+      .required(),
+    answers: Joi.array()
+      .items({
+        id: Joi.number().required(),
+        result: Joi.string().valid('ok', 'ko', 'aband', 'timedout', 'focusedOut', 'partially').required(),
+        challengeId: Joi.string().required(),
+      })
+      .required(),
+  };
+
+  stringify(value) {
+    return JSON.stringify(value);
+  }
+
+  @action
+  updateJsonFieldValue(key, event) {
+    const value = event.target.value;
+    delete this.errors[key];
+    // necessary to trigger a reactivity update
+    this.errors = { ...this.errors };
+    try {
+      const parsedValue = JSON.parse(value);
+      Joi.assert(parsedValue, this.validationSchema[key], { allowUnknown: true });
+      this.args.updateParametersValue(key, parsedValue);
+    } catch (error) {
+      switch (error.constructor) {
+        case SyntaxError:
+          this.errors[key] = 'Invalid JSON';
+          break;
+        default:
+          this.errors[key] = error.message;
+      }
+    }
+  }
+
+  @action
+  updateFormFieldValue(key, event) {
+    this.args.updateParametersValue(key, event.target.value);
+  }
+
+  @action updateLocaleValue(value) {
+    this.args.updateParametersValue('locale', value);
+  }
+}
