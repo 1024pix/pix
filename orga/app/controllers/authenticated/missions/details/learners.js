5ce77ece27b4ac5ea6f1241c453bdb654ad73c63
Yaelle YAMA <yaelle.marsiac@pix.fr>
2024-03-12 15:10:07 +0100

    feat(orga): Display mission learners on the mission page

---
+++ orga/app/controllers/authenticated/missions/details/learners.js
@@ -0,0 +1,17 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+
+const DEFAULT_PAGE_NUMBER = 1;
+
+export default class MissionLearnersController extends Controller {
+  @service router;
+  @tracked pageNumber = DEFAULT_PAGE_NUMBER;
+  @tracked pageSize = 25;
+
+  @action
+  clearFilters() {
+    this.pageNumber = null;
+  }
+}
