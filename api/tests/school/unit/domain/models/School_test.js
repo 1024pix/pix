29cbc847b80ba4dc96fe82e65960b883897a9189
Guillaume Lagorce <guillaume@hey-gul.com>
2024-03-12 10:59:28 +0100

    refactor(api-1d): use School model to redact learners private data

--- api/tests/school/unit/domain/models/School_test.js
+++ api/tests/school/unit/domain/models/School_test.js
@@ -1,24 +1,24 @@
-import { mapToOrganizationLearnerDtos } from '../../../../../src/school/domain/mappers/map-to-organization-learner-dtos.js';
 import { OrganizationLearner } from '../../../../../src/school/domain/models/OrganizationLearner.js';
+import { School } from '../../../../../src/school/domain/models/School.js';
 import { OrganizationLearnerDTO } from '../../../../../src/school/domain/read-models/OrganizationLearnerDTO.js';
 import { expect } from '../../../../test-helper.js';
 
-describe('Unit | Service | MapToOrganizationLearnerDtos', function () {
-  describe('#mapToOrganizationLearnerDtos', function () {
+describe('Unit | Domain | School', function () {
+  describe('#get organizationLearners', function () {
     context('when there are learners with different first names', function () {
       it('should return the learners with their first names as display names', async function () {
         // given
-        const learners = [
+        const organizationLearners = [
           new OrganizationLearner({ firstName: 'Ernest', division: 'CM1' }),
           new OrganizationLearner({ firstName: 'Hector', division: 'CM1' }),
         ];
 
         // when
-        const learnersWithUniqueDisplayNameByDivision = mapToOrganizationLearnerDtos(learners);
+        const school = new School({ organizationLearners });
 
         // then
 
-        expect(learnersWithUniqueDisplayNameByDivision).to.deep.equal([
+        expect(school.organizationLearners).to.deep.equal([
           new OrganizationLearnerDTO({ firstName: 'Ernest', displayName: 'Ernest', division: 'CM1' }),
           new OrganizationLearnerDTO({ firstName: 'Hector', displayName: 'Hector', division: 'CM1' }),
         ]);
@@ -41,16 +41,16 @@ describe('Unit | Service | MapToOrganizationLearnerDtos', function () {
         tests.forEach(function ({ lastNames, expectedLastNameLetters }) {
           it(`for last names: [${lastNames}] should return display names: first name + [${expectedLastNameLetters}]`, async function () {
             // given
-            const learners = lastNames.map(
+            const organizationLearners = lastNames.map(
               (lastName) => new OrganizationLearner({ firstName: 'Ernest', lastName: lastName, division: 'CM1' }),
             );
 
             // when
-            const learnersWithUniqueDisplayNameByDivision = mapToOrganizationLearnerDtos(learners);
+            const school = new School({ organizationLearners });
 
             // then
 
-            expect(learnersWithUniqueDisplayNameByDivision).to.deep.equal(
+            expect(school.organizationLearners).to.deep.equal(
               expectedLastNameLetters.map(
                 (lastNameLetters) =>
                   new OrganizationLearnerDTO({
@@ -66,17 +66,17 @@ describe('Unit | Service | MapToOrganizationLearnerDtos', function () {
       context('but different divisions', function () {
         it('should return the learners with their first names as display names', async function () {
           // given
-          const learners = [
+          const organizationLearners = [
             new OrganizationLearner({ firstName: 'Ernest', lastName: 'Abea', division: 'CM1' }),
             new OrganizationLearner({ firstName: 'Ernest', lastName: 'Beaba', division: 'CM2' }),
           ];
 
           // when
-          const learnersWithUniqueDisplayNameByDivision = mapToOrganizationLearnerDtos(learners);
+          const school = new School({ organizationLearners });
 
           // then
 
-          expect(learnersWithUniqueDisplayNameByDivision).to.deep.equal([
+          expect(school.organizationLearners).to.deep.equal([
             new OrganizationLearnerDTO({ firstName: 'Ernest', displayName: 'Ernest', division: 'CM1' }),
             new OrganizationLearnerDTO({ firstName: 'Ernest', displayName: 'Ernest', division: 'CM2' }),
           ]);
