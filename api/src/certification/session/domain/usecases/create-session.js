09555031cdb3323d588a3eda4cc1b709f4811012
Antoine Ceol <antoine.ceol@gmail.com>
2024-03-13 17:11:11 +0100

    :fire: api: Remove check done in pre-handler

--- api/src/certification/session/domain/usecases/create-session.js
+++ api/src/certification/session/domain/usecases/create-session.js
@@ -1,12 +1,10 @@
 /**
  * @typedef {import ('../../../shared/domain/usecases/index.js').CertificationCenterRepository} CertificationCenterRepository
  * @typedef {import ('../../../shared/domain/usecases/index.js').SessionRepository} SessionRepository
- * @typedef {import ('../../../shared/domain/usecases/index.js').UserRepository} UserRepository
  * @typedef {import ('../../../shared/domain/usecases/index.js').SessionValidator} SessionValidator
  * @typedef {import ('../../../shared/domain/usecases/index.js').SessionCodeService} SessionCodeService
  */
 
-import { ForbiddenAccess } from '../../../../shared/domain/errors.js';
 import { CertificationVersion } from '../../../../shared/domain/models/CertificationVersion.js';
 import { Session } from '../models/Session.js';
 
@@ -14,7 +12,6 @@ import { Session } from '../models/Session.js';
  * @param {Object} params
  * @param {CertificationCenterRepository} params.certificationCenterRepository
  * @param {SessionRepository} params.sessionRepository
- * @param {UserRepository} params.userRepository
  * @param {SessionValidator} params.sessionValidator
  * @param {SessionCodeService} params.sessionCodeService
  */
@@ -23,19 +20,12 @@ const createSession = async function ({
   session,
   certificationCenterRepository,
   sessionRepository,
-  userRepository,
   sessionValidator,
   sessionCodeService,
 }) {
   sessionValidator.validate(session);
 
   const certificationCenterId = session.certificationCenterId;
-  const userWithCertifCenters = await userRepository.getWithCertificationCenterMemberships(userId);
-  if (!userWithCertifCenters.hasAccessToCertificationCenter(certificationCenterId)) {
-    throw new ForbiddenAccess(
-      "L'utilisateur n'est pas membre du centre de certification dans lequel il souhaite créer une session",
-    );
-  }
 
   const accessCode = sessionCodeService.getNewSessionCode();
   const { isV3Pilot, name: certificationCenterName } = await certificationCenterRepository.get({
