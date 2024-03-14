d782c059eb657826e6e16c36404320d198dd80c3
Yaelle YAMA <yaelle.marsiac@pix.fr>
2024-03-14 12:26:44 +0100

    tech(api): create onde import seeds

---
+++ api/db/database-builder/factory/build-organization-learner-import-format.js
@@ -0,0 +1,36 @@
+import _ from 'lodash';
+
+import { databaseBuffer } from '../database-buffer.js';
+import { buildUser } from './build-user.js';
+
+const buildOrganizationLearnerImportFormat = function ({
+  id = databaseBuffer.getNextId(),
+  name = 'MY_CUSTOM_EXPORT',
+  fileType = 'csv',
+  config = {
+    encoding: ['utf-8'],
+    validationRules: { unicity: ['my_column'], formats: [{ name: 'my_column', type: 'string' }] },
+    headers: [{ name: 'my_column', required: true }],
+  },
+  createdAt = new Date('2021-01-01'),
+  createdBy,
+  updatedAt = new Date('2021-02-01'),
+} = {}) {
+  createdBy = _.isUndefined(createdBy) ? buildUser().id : createdBy;
+  const values = {
+    id,
+    name,
+    fileType,
+    config,
+    createdAt,
+    updatedAt,
+    createdBy,
+  };
+
+  return databaseBuffer.pushInsertable({
+    tableName: 'organization-learner-import-formats',
+    values,
+  });
+};
+
+export { buildOrganizationLearnerImportFormat };
---
+++ api/db/seeds/data/team-prescription/build-organization-learner-import-formats.js
@@ -0,0 +1,31 @@
+import { TEAM_1D_USER_ID } from '../team-1d/data-builder.js';
+
+export async function buildOrganizationLearnerImportFormat(databaseBuilder) {
+  await databaseBuilder.factory.buildOrganizationLearnerImportFormat({
+    name: 'ONDE',
+    fileType: 'csv',
+    config: {
+      encoding: [],
+      validationRules: {
+        unicity: ['identifiant'],
+        formats: [
+          { name: 'nom', type: 'string' },
+          { name: 'prénom', type: 'string' },
+          { name: 'identifiant', type: 'string' },
+          { name: 'classe', type: 'string' },
+          { name: 'date de naissance', type: 'date', format: 'DD/MM/YYYY' },
+        ],
+      },
+      headers: [
+        { name: 'nom', required: true },
+        { name: 'prénom', required: true },
+        { name: 'identifiant', required: true },
+        { name: 'classe', required: true },
+        { name: 'date de naissance', required: true },
+      ],
+    },
+    createdAt: new Date('2024-01-01'),
+    createdBy: TEAM_1D_USER_ID,
+    updatedAt: new Date('2021-02-01'),
+  });
+}
