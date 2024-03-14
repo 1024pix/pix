3d5f57a6f19d7636b6db55a51cb39fc0e94560e4
dlahaye <dimitrilahaye.dev@gmail.com>
2024-03-14 15:18:03 +0100

    feat(api): Return 502 error on module instantiation errors

--- api/src/devcomp/application/http-error-mapper-configuration.js
+++ api/src/devcomp/application/http-error-mapper-configuration.js
@@ -1,12 +1,23 @@
 import { HttpErrors } from '../../shared/application/http-errors.js';
 import { DomainErrorMappingConfiguration } from '../../shared/application/models/domain-error-mapping-configuration.js';
-import { ModuleDoesNotExistError, PassageDoesNotExistError, PassageTerminatedError } from '../domain/errors.js';
+import {
+  ModuleDoesNotExistError,
+  ModuleInstantiationError,
+  PassageDoesNotExistError,
+  PassageTerminatedError,
+} from '../domain/errors.js';
 
 const devcompDomainErrorMappingConfiguration = [
   {
     name: ModuleDoesNotExistError.name,
     httpErrorFn: (error) => {
       return new HttpErrors.UnprocessableEntityError(error.message, error.code, error.meta);
+    },
+  },
+  {
+    name: ModuleInstantiationError.name,
+    httpErrorFn: (error) => {
+      return new HttpErrors.BadGatewayError(error.message, error.code, error.meta);
     },
   },
   {
