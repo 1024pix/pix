60ca8b3ed7f07b3d7444ff7578a0d20488534224
Aurelie Crouillebois <aurelie.crouillebois@gmail.com>
2024-03-15 17:29:55 +0100

    feat(api-1d): resume mission assessment if started

--- api/tests/school/integration/application/assessment-controller_test.js
+++ api/tests/school/integration/application/assessment-controller_test.js
@@ -4,7 +4,7 @@ import { usecases } from '../../../../src/school/domain/usecases/index.js';
 import { expect, hFake, sinon } from '../../../test-helper.js';
 
 describe('Integration | Controller | assessment-controller', function () {
-  describe('#getCurrent', function () {
+  describe('#create', function () {
     it('should call the expected usecase and return the serialized assessment', async function () {
       const missionId = 'mission-id';
       const assessmentId = 1234;
@@ -22,7 +22,7 @@ describe('Integration | Controller | assessment-controller', function () {
         .resolves(createdAssessment);
       const request = { payload: { missionId, learnerId: 34567 } };
 
-      const result = await assessmentController.getCurrent(request, hFake);
+      const result = await assessmentController.create(request, hFake);
 
       expect(result.statusCode).to.be.equal(201);
       expect(result.source.data).to.deep.equal({
