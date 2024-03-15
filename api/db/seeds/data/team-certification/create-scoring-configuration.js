45ea1c9feb76ed4a2a7afa84c7cfc3025721c04a
Alexandre COIN <alexandre@mbpdealexandre-1.home>
2024-03-15 11:01:16 +0100

    :seedling: api: add scoring configuration seed

---
+++ api/db/seeds/data/team-certification/create-scoring-configuration.js
@@ -0,0 +1,67 @@
+import { REAL_PIX_SUPER_ADMIN_ID } from '../common/common-builder.js';
+
+const configuration = [
+  {
+    meshLevel: 0,
+    bounds: {
+      min: -8,
+      max: -1.4,
+    },
+  },
+  {
+    meshLevel: 1,
+    bounds: {
+      min: -1.4,
+      max: -0.519,
+    },
+  },
+  {
+    meshLevel: 2,
+    bounds: {
+      min: -0.519,
+      max: 0.6,
+    },
+  },
+  {
+    meshLevel: 3,
+    bounds: {
+      min: 0.6,
+      max: 1.5,
+    },
+  },
+  {
+    meshLevel: 4,
+    bounds: {
+      min: 1.5,
+      max: 2.25,
+    },
+  },
+  {
+    meshLevel: 5,
+    bounds: {
+      min: 2.25,
+      max: 3.1,
+    },
+  },
+  {
+    meshLevel: 6,
+    bounds: {
+      min: 3.1,
+      max: 4,
+    },
+  },
+  {
+    meshLevel: 7,
+    bounds: {
+      min: 4,
+      max: 8,
+    },
+  },
+];
+
+export const createScoringConfiguration = ({ databaseBuilder }) => {
+  databaseBuilder.factory.buildScoringConfiguration({
+    configuration,
+    createdByUserId: REAL_PIX_SUPER_ADMIN_ID,
+  });
+};
