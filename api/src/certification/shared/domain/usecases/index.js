232399671ad8429666a6db75e2006f5f39f2f5d2
Antoine Ceol <antoine.ceol@gmail.com>
2024-03-15 09:39:01 +0100

    :recycle: api: Remove unused injections

--- api/src/certification/shared/domain/usecases/index.js
+++ api/src/certification/shared/domain/usecases/index.js
@@ -17,19 +17,11 @@ import * as certificateRepository from '../../../course/infrastructure/repositor
 import * as v3CertificationCourseDetailsForAdministrationRepository from '../../../course/infrastructure/repositories/v3-certification-course-details-for-administration-repository.js';
 import * as flashAlgorithmService from '../../../flash-certification/domain/services/algorithm-methods/flash.js';
 import * as flashAlgorithmConfigurationRepository from '../../../flash-certification/infrastructure/repositories/flash-algorithm-configuration-repository.js';
-import * as sessionCodeService from '../../../session/domain/services/session-code-service.js';
-import * as sessionsImportValidationService from '../../../session/domain/services/sessions-import-validation-service.js';
-import * as sessionValidator from '../../../session/domain/validators/session-validator.js';
 import * as candidateRepository from '../../../session/infrastructure/repositories/candidate-repository.js';
-import * as cpfCertificationResultRepository from '../../../session/infrastructure/repositories/cpf-certification-result-repository.js';
-import * as cpfExportRepository from '../../../session/infrastructure/repositories/cpf-export-repository.js';
-import * as sessionRepository from '../../../session/infrastructure/repositories/session-repository.js';
 import * as certificationAssessmentRepository from '../../../shared/infrastructure/repositories/certification-assessment-repository.js';
-import * as certificationCenterRepository from '../../../shared/infrastructure/repositories/certification-center-repository.js';
 import * as certificationChallengeLiveAlertRepository from '../../../shared/infrastructure/repositories/certification-challenge-live-alert-repository.js';
 import * as certificationIssueReportRepository from '../../../shared/infrastructure/repositories/certification-issue-report-repository.js';
 import * as certificationReportRepository from '../../../shared/infrastructure/repositories/certification-report-repository.js';
-import * as issueReportCategoryRepository from '../../../shared/infrastructure/repositories/issue-report-category-repository.js';
 import * as certificationCourseRepository from '../../infrastructure/repositories/certification-course-repository.js';
 import * as mailService from '../services/mail-service.js';
 
@@ -39,25 +31,17 @@ import * as mailService from '../services/mail-service.js';
  * @typedef {answerRepository} AnswerRepository
  * @typedef {assessmentRepository} AssessmentRepository
  * @typedef {assessmentResultRepository} AssessmentResultRepository
- * @typedef {badgeRepository} BadgeRepository
  * @typedef {candidateRepository} CandidateRepository
  * @typedef {certificationAssessmentRepository} CertificationAssessmentRepository
- * @typedef {certificationCenterRepository} CertificationCenterRepository
  * @typedef {certificationChallengeLiveAlertRepository} CertificationChallengeLiveAlertRepository
  * @typedef {certificationCourseRepository} CertificationCourseRepository
  * @typedef {challengeRepository} ChallengeRepository
  * @typedef {competenceMarkRepository} CompetenceMarkRepository
  * @typedef {competenceRepository} CompetenceRepository
  * @typedef {complementaryCertificationRepository} ComplementaryCertificationRepository
- * @typedef {cpfExportRepository} CpfExportRepository
  * @typedef {flashAlgorithmService} FlashAlgorithmService
- * @typedef {issueReportCategoryRepository} IssueReportCategoryRepository
  * @typedef {mailService} MailService
- * @typedef {sessionCodeService} SessionCodeService
- * @typedef {cpfCertificationResultRepository} CpfCertificationResultRepository
  * @typedef {v3CertificationCourseDetailsForAdministrationRepository} V3CertificationCourseDetailsForAdministrationRepository
- * @typedef {sessionRepository} SessionRepository
- * @typedef {sessionValidator} SessionValidator
  * @typedef {userRepository} UserRepository
  */
 const dependencies = {
@@ -66,7 +50,6 @@ const dependencies = {
   assessmentResultRepository,
   candidateRepository,
   certificationAssessmentRepository,
-  certificationCenterRepository,
   certificationChallengeLiveAlertRepository,
   certificationCourseRepository,
   certificationIssueReportRepository,
@@ -78,14 +61,7 @@ const dependencies = {
   complementaryCertificationRepository,
   flashAlgorithmService,
   flashAlgorithmConfigurationRepository,
-  issueReportCategoryRepository,
   mailService,
-  sessionCodeService,
-  sessionsImportValidationService,
-  cpfCertificationResultRepository,
-  cpfExportRepository,
-  sessionRepository,
-  sessionValidator,
   userRepository,
   v3CertificationCourseDetailsForAdministrationRepository,
 };
