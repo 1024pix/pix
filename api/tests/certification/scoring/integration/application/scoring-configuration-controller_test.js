a34f74de1ad309cd3d079896e76b87b516151384
Alexandre COIN <alexandre@mbpdealexandre-1.home>
2024-03-20 16:17:05 +0100

    :white_check_mark: api: add missing given-when-then

--- api/tests/certification/scoring/integration/application/scoring-configuration-controller_test.js
+++ api/tests/certification/scoring/integration/application/scoring-configuration-controller_test.js
@@ -5,6 +5,7 @@ import { expect, hFake, sinon } from '../../../../test-helper.js';
 describe('Integration | Application | ScoringConfigurationController', function () {
   describe('#saveCompetenceForScoringConfiguration', function () {
     it('should save the competence for scoring configuration', async function () {
+      // given
       sinon.stub(usecases, 'saveCompetenceForScoringConfiguration');
 
       const request = {
@@ -15,8 +16,10 @@ describe('Integration | Application | ScoringConfigurationController', function 
         },
       };
 
+      // when
       const response = await scoringConfigurationController.saveCompetenceForScoringConfiguration(request, hFake);
 
+      // then
       expect(response.statusCode).to.equal(201);
       expect(usecases.saveCompetenceForScoringConfiguration).to.have.been.calledWith({
         data: request.payload,
@@ -26,6 +29,7 @@ describe('Integration | Application | ScoringConfigurationController', function 
 
   describe('#saveCertificationScoringConfiguration', function () {
     it('should save the certification scoring configuration', async function () {
+      // given
       const userId = 123;
       sinon.stub(usecases, 'saveCertificationScoringConfiguration');
 
@@ -42,8 +46,10 @@ describe('Integration | Application | ScoringConfigurationController', function 
         },
       };
 
+      // when
       const response = await scoringConfigurationController.saveCertificationScoringConfiguration(request, hFake);
 
+      // then
       expect(response.statusCode).to.equal(201);
       expect(usecases.saveCertificationScoringConfiguration).to.have.been.calledWith({
         data: request.payload,
