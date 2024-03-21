3ad12d60a1adb52bfc64e8bc7849fc2a853d90b3
Ismael Gorissen <ismael.gorissen@gmail.com>
2024-03-21 09:41:50 +0100

    feat(certif): add Banner::LanguageAvailability component

---
+++ certif/tests/integration/components/banner/language-availability_test.js
@@ -0,0 +1,67 @@
+import { render } from '@1024pix/ember-testing-library';
+import { hbs } from 'ember-cli-htmlbars';
+import { module, test } from 'qunit';
+
+import setupIntlRenderingTest from '../../../helpers/setup-intl-rendering';
+
+module('Integration | Component | Banner::LanguageAvailability', function (hooks) {
+  setupIntlRenderingTest(hooks);
+
+  let session;
+
+  hooks.beforeEach(function () {
+    session = this.owner.lookup('service:session');
+  });
+
+  module('when user language is available', function () {
+    test('does not display the language availability banner', async function (assert) {
+      // given
+      session.data.localeNotSupported = false;
+      session.data.localeNotSupportedBannerClosed = false;
+
+      // when
+      const screen = await render(hbs`<Banner::LanguageAvailability />`);
+
+      // then
+      assert.dom(screen.queryByRole('alert')).doesNotExist();
+    });
+  });
+
+  module('when user language is not available', function (hooks) {
+    hooks.beforeEach(function () {
+      session.data.localeNotSupported = true;
+    });
+
+    module('when the user has not closed the banner', function () {
+      test('displays the language availability banner', async function (assert) {
+        // given
+        session.data.localeNotSupportedBannerClosed = false;
+
+        // when
+        const screen = await render(hbs`<Banner::LanguageAvailability />`);
+
+        // then
+        assert
+          .dom(
+            screen.getByText(
+              `Votre langue n'est pas encore disponible sur Pix Certif. Pour votre confort, l'application sera présentée en anglais. Toute l'équipe de Pix travaille à l'ajout de votre langue.`,
+            ),
+          )
+          .exists();
+      });
+    });
+
+    module('when the user has closed the banner', function () {
+      test('does not display the language availability banner', async function (assert) {
+        // given
+        session.data.localeNotSupportedBannerClosed = true;
+
+        // when
+        const screen = await render(hbs`<Banner::LanguageAvailability />`);
+
+        // then
+        assert.dom(screen.queryByRole('alert')).doesNotExist();
+      });
+    });
+  });
+});
