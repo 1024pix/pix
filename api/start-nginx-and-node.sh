1bc1b3a000666818c4f6430f00e4a4f9c739297e
Steph0 <Steph0@users.noreply.github.com>
2024-03-21 19:10:23 +0100

    Change error page

--- api/start-nginx-and-node.sh
+++ api/start-nginx-and-node.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # -x : see commands
-# -e : stop if subscript fails
 set -x
 
 echo "Attach API binded to env NODE_PORT:[${NODE_PORT}}]"
@@ -9,7 +8,3 @@ node index.js &
 
 echo "Start Nginx binded to Scalingo env PORT:[${PORT}]"
 bash /app/bin/run
-# NGINX_EXIT_CODE=$?
-# echo "${NGINX_OUTPUT}"
-# echo "Nginx exited with exit code:[${NGINX_EXIT_CODE}]"
-
