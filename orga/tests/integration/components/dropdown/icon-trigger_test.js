001b18f2f4763dc990290b8fb7abad045ddd96c0
Marie-Hélène <marie-helene.rots@pix.fr>
2024-03-21 10:25:37 +0100

    tech(orga): replace contains by testing library in icon-trigger integration test

--- orga/tests/integration/components/dropdown/icon-trigger_test.js
+++ orga/tests/integration/components/dropdown/icon-trigger_test.js
@@ -9,28 +9,32 @@ module('Integration | Component | Dropdown | icon-trigger', function (hooks) {
 
   test('should display actions menu', async function (assert) {
     // when
-    await render(hbs`<Dropdown::IconTrigger @ariaLabel='Afficher les actions' />`);
+    const screen = await render(hbs`<Dropdown::IconTrigger @ariaLabel='Afficher les actions' />`);
 
     // then
-    assert.dom('[aria-label="Afficher les actions"]').exists();
+    assert.ok(screen.getByLabelText('Afficher les actions'));
   });
 
   test('should display actions on click', async function (assert) {
     // when
-    await render(hbs`<Dropdown::IconTrigger @ariaLabel='Afficher les actions'>Something</Dropdown::IconTrigger>`);
+    const screen = await render(
+      hbs`<Dropdown::IconTrigger @ariaLabel='Afficher les actions'>Something</Dropdown::IconTrigger>`,
+    );
     await clickByName('Afficher les actions');
 
     // then
-    assert.contains('Something');
+    assert.ok(screen.getByText('Something'));
   });
 
   test('should hide actions on click again', async function (assert) {
     // when
-    await render(hbs`<Dropdown::IconTrigger @ariaLabel='Afficher les actions'>Something</Dropdown::IconTrigger>`);
+    const screen = await render(
+      hbs`<Dropdown::IconTrigger @ariaLabel='Afficher les actions'>Something</Dropdown::IconTrigger>`,
+    );
     await clickByName('Afficher les actions');
     await clickByName('Afficher les actions');
 
     // then
-    assert.notContains('Something');
+    assert.notOk(screen.queryByText('Something'));
   });
 });
