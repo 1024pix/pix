98c32781806b8e5f38cbde30ca32016287ceb8f6
Alexandre Monney <alexandre.monney@pix.fr>
2024-03-22 18:02:55 +0100

    fix(api): handle payload too large error

---
+++ api/tests/prescription/learner-management/unit/domain/usecases/handle-payload-too-large-error_test.js
@@ -0,0 +1,33 @@
+import { PayloadTooLargeError } from '../../../../../../lib/application/http-errors.js';
+import { OrganizationImport } from '../../../../../../src/prescription/learner-management/domain/models/OrganizationImport.js';
+import { handlePayloadTooLargeError } from '../../../../../../src/prescription/learner-management/domain/usecases/handle-payload-too-large-error.js';
+import { catchErr, expect, sinon } from '../../../../../test-helper.js';
+
+describe('Unit | UseCase | Organization Learners Management | Handle Payload Too Large Error', function () {
+  let organizationImportRepository;
+
+  beforeEach(function () {
+    organizationImportRepository = {
+      save: sinon.stub(),
+    };
+    sinon.stub(OrganizationImport, 'create');
+  });
+
+  it('should save payload too large error and throw it', async function () {
+    // given
+    const userId = 777;
+    const organizationId = 111;
+    const organizationImportStub = { upload: sinon.stub() };
+    OrganizationImport.create.returns(organizationImportStub);
+    // when
+    const error = await catchErr(handlePayloadTooLargeError)({
+      organizationId,
+      userId,
+      organizationImportRepository,
+    });
+
+    // then
+    expect(organizationImportRepository.save).to.have.been.calledWithExactly(organizationImportStub);
+    expect(error).to.be.an.instanceof(PayloadTooLargeError);
+  });
+});
