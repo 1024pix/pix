22629d19d0a5f699cef01084d1817aa67c2d479b
miranda.lin-guignard <miranda.w.lin@gmail.com>
2024-03-25 11:25:29 +0100

    fix(mon-pix): move aria-live assertive to the whole Grain div
    Co-authored-by: Dimitri Lahaye <dimitri.lahaye@pix.fr>

--- mon-pix/tests/integration/components/module/passage_test.js
+++ mon-pix/tests/integration/components/module/passage_test.js
@@ -189,50 +189,6 @@ module('Integration | Component | Module | Passage', function (hooks) {
       assert.strictEqual(grainsAfterContinueAction.length, 2);
     });
 
-    test('should only set the aria-live="assertive" attribute on the last grain', async function (assert) {
-      // given
-      const store = this.owner.lookup('service:store');
-      const text1Element = { content: 'content', type: 'text' };
-      const text2Element = { content: 'content 2', type: 'text' };
-      const text3Element = { content: 'content 3', type: 'text' };
-      const grain1 = store.createRecord('grain', { elements: [text1Element] });
-      const grain2 = store.createRecord('grain', { elements: [text2Element] });
-      const grain3 = store.createRecord('grain', { elements: [text3Element] });
-
-      const module = store.createRecord('module', { title: 'Module title', grains: [grain1, grain2, grain3] });
-      this.set('module', module);
-
-      const passage = store.createRecord('passage');
-      this.set('passage', passage);
-
-      const screen = await render(hbs`<Module::Passage @module={{this.module}} @passage={{this.passage}} />`);
-
-      const grainsBeforeAnyAction = screen.getAllByRole('article');
-      assert.strictEqual(grainsBeforeAnyAction.length, 1);
-      const firstGrain = grainsBeforeAnyAction.at(-1);
-      assert.strictEqual(firstGrain.getAttribute('aria-live'), null);
-
-      // when
-      await clickByName(continueButtonName);
-
-      // then
-      const grainsAfterOneContinueActions = screen.getAllByRole('article');
-      assert.strictEqual(grainsAfterOneContinueActions.length, 2);
-      const secondGrain = grainsAfterOneContinueActions.at(-1);
-      assert.strictEqual(firstGrain.getAttribute('aria-live'), null);
-      assert.strictEqual(secondGrain.getAttribute('aria-live'), 'assertive');
-
-      // when
-      await clickByName(continueButtonName);
-
-      // then
-      const grainsAfterTwoContinueActions = screen.getAllByRole('article');
-      assert.strictEqual(grainsAfterTwoContinueActions.length, 3);
-      const thirdGrain = grainsAfterTwoContinueActions.at(-1);
-      assert.strictEqual(secondGrain.getAttribute('aria-live'), null);
-      assert.strictEqual(thirdGrain.getAttribute('aria-live'), 'assertive');
-    });
-
     test('should give focus on the last grain when appearing', async function (assert) {
       // given
       const store = this.owner.lookup('service:store');
