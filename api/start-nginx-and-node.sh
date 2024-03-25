c091d53e75fbef81903fb055cd6f74579e41447b
Steph0 <Steph0@users.noreply.github.com>
2024-03-25 16:10:44 +0100

    Okay.. not working

--- api/start-nginx-and-node.sh
+++ api/start-nginx-and-node.sh
@@ -6,9 +6,5 @@ set -x
 echo "Attach API binded to env NODE_PORT:[${NODE_PORT}}]"
 node index.js &
 
-cp /app/ngx_http_redis_module.so /app/vendor/nginx/modules/ngx_http_modsecurity_module.so
-
-sed -i '1iload_module modules/ngx_http_modsecurity_module.so;' /app/vendor/nginx/conf/nginx.conf
-
 echo "Start Nginx binded to Scalingo env PORT:[${PORT}]"
 bash /app/bin/run
