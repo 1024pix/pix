43c4c50d8d7fb6183cd3dea30e195630a462711d
Steph0 <Steph0@users.noreply.github.com>
2024-03-25 15:58:20 +0100

    oups

--- api/start-nginx-and-node.sh
+++ api/start-nginx-and-node.sh
@@ -8,7 +8,7 @@ node index.js &
 
 cp /app/ngx_http_redis_module.so /app/vendor/nginx/modules/ngx_http_modsecurity_module.so
 
-sed -i 'load_module modules/ngx_http_modsecurity_module.so;' /app/vendor/nginx/conf/nginx.conf
+sed -i '1iload_module modules/ngx_http_modsecurity_module.so;' /app/vendor/nginx/conf/nginx.conf
 
 echo "Start Nginx binded to Scalingo env PORT:[${PORT}]"
 bash /app/bin/run
