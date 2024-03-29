9ccadff43961caa014c8c89bd09d7c6b5522b9b3
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2024-03-29 16:11:57 +0100

    feat(api): retrieve organization import configuration

---
+++ api/src/prescription/learner-management/domain/models/OrganizationLearnerImportFormat.js
@@ -0,0 +1,9 @@
+class OrganizationLearnerImportFormat {
+  constructor({ name, fileType, config } = {}) {
+    this.name = name;
+    this.fileType = fileType;
+    this.config = config;
+  }
+}
+
+export { OrganizationLearnerImportFormat };
---
+++ api/tests/prescription/learner-management/integration/infrastructure/repositories/organization-learner-import-format-repository_test.js
@@ -0,0 +1,59 @@
+import { OrganizationLearnerImportFormat } from '../../../../../../src/prescription/learner-management/domain/models/OrganizationLearnerImportFormat.js';
+import * as organizationLearnerImportFormatRepository from '../../../../../../src/prescription/learner-management/infrastructure/repositories/organization-learner-import-format-repository.js';
+import { ORGANIZATION_FEATURE } from '../../../../../../src/shared/domain/constants.js';
+import { databaseBuilder, expect } from '../../../../../test-helper.js';
+
+describe('Integration | Repository | Organization Learner Management | Organization Learner Import Format', function () {
+  describe('#get', function () {
+    it('should return import state', async function () {
+      // given
+      const importConfig = {
+        name: 'MY_TEST_EXPORT',
+        fileType: 'csv',
+        config: {
+          encoding: ['utf-8'],
+          validationRules: {
+            unicity: ['my_column1'],
+            formats: [
+              { name: 'my_column1', type: 'string' },
+              { name: 'my_column2', type: 'string' },
+            ],
+          },
+          headers: [
+            { name: 'my_column1', required: true, property: 'lastName' },
+            { name: 'my_column2', required: true, property: 'firstName' },
+          ],
+        },
+      };
+
+      const organizationId = databaseBuilder.factory.buildOrganization().id;
+
+      const featureId = databaseBuilder.factory.buildFeature({
+        key: ORGANIZATION_FEATURE.LEARNER_IMPORT.key,
+      }).id;
+
+      const organizationLearnerImportId = databaseBuilder.factory.buildOrganizationLearnerImportFormat(importConfig).id;
+
+      databaseBuilder.factory.buildOrganizationFeature({
+        organizationId,
+        featureId,
+        params: { organizationLearnerImportId },
+      });
+
+      await databaseBuilder.commit();
+
+      // when
+      const result = await organizationLearnerImportFormatRepository.get(organizationId);
+
+      // then
+      expect(result).to.be.an.instanceOf(OrganizationLearnerImportFormat);
+      expect(result).to.be.deep.equal(new OrganizationLearnerImportFormat(importConfig));
+    });
+
+    it('should return null if nothing was found', async function () {
+      const result = await organizationLearnerImportFormatRepository.get(1);
+
+      expect(result).to.equal(null);
+    });
+  });
+});
