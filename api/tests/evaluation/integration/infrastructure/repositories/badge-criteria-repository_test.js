dcc33a7979f032dcbc97d356718c562ccd92ab50
Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
2024-03-29 11:16:32 +0100

    feat(api): add updateCriterion badge-criteria-repository method

--- api/tests/evaluation/integration/infrastructure/repositories/badge-criteria-repository_test.js
+++ api/tests/evaluation/integration/infrastructure/repositories/badge-criteria-repository_test.js
@@ -1,5 +1,8 @@
+import { BadRequestError } from '../../../../../lib/application/http-errors.js';
+import BadgeCriterion from '../../../../../src/evaluation/domain/models/BadgeCriterion.js';
 import * as badgeCriteriaRepository from '../../../../../src/evaluation/infrastructure/repositories/badge-criteria-repository.js';
-import { databaseBuilder, expect, knex } from '../../../../test-helper.js';
+import { NotFoundError } from '../../../../../src/shared/domain/errors.js';
+import { catchErr, databaseBuilder, expect, knex } from '../../../../test-helper.js';
 
 describe('Integration | Repository | Badge Criteria Repository', function () {
   describe('#save', function () {
@@ -60,6 +63,71 @@ describe('Integration | Repository | Badge Criteria Repository', function () {
           { id: 'tubeABC', level: 2 },
           { id: 'tubeDEF', level: 8 },
         ],
+      });
+    });
+  });
+
+  describe('#update-criterion', function () {
+    context('when badge-criterion exists', function () {
+      it('should update CampaignParticipation badge-criterion', async function () {
+        // given
+        const badgeCriterion = databaseBuilder.factory.buildBadgeCriterion.scopeCampaignParticipation({
+          name: 'dummy criterion',
+          threshold: 10,
+        });
+        await databaseBuilder.commit();
+
+        const attributesToUpdate = {
+          name: 'updated name',
+          threshold: 99,
+        };
+
+        // when
+        const updatedBadgeCriterion = await badgeCriteriaRepository.updateCriterion(
+          badgeCriterion.id,
+          attributesToUpdate,
+        );
+
+        // then
+        expect(updatedBadgeCriterion).to.be.instanceOf(BadgeCriterion);
+        expect(updatedBadgeCriterion).to.deep.equal({
+          ...badgeCriterion,
+          ...attributesToUpdate,
+        });
+      });
+    });
+
+    context('when badge-criterion does not exist', function () {
+      it('should throw a not found message error', async function () {
+        // given
+        const notExistingBadgeCriterionId = 9999;
+
+        // when
+        const error = await catchErr(badgeCriteriaRepository.updateCriterion)(notExistingBadgeCriterionId, {
+          name: 'dummy ',
+        });
+
+        // then
+        expect(error).to.be.instanceOf(NotFoundError);
+      });
+    });
+
+    context('when no attributes are going to be updated', function () {
+      it('should throw a not found message error', async function () {
+        // given
+        const badgeCriterion = databaseBuilder.factory.buildBadgeCriterion.scopeCampaignParticipation({
+          name: 'dummy criterion',
+          threshold: 10,
+        });
+        await databaseBuilder.commit();
+
+        const attributesToUpdate = {};
+
+        // when
+        const error = await catchErr(badgeCriteriaRepository.updateCriterion)(badgeCriterion.id, attributesToUpdate);
+
+        // then
+        expect(error).to.be.instanceOf(BadRequestError);
       });
     });
   });
