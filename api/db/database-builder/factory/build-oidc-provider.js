fe16f964a12100d5f44b664a731265c183beed69
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2024-04-02 10:39:02 +0200

    feat(api): add data-builder for oidc-providers
    through the OIDC_PROVIDERS environment variable.
    
    Co-authored-by: Ismael Gorissen <ismael.gorissen@gmail.com>
    Co-authored-by: Clément Latzarus <clement.latzarus@pix.fr>

---
+++ api/db/database-builder/factory/build-oidc-provider.js
@@ -0,0 +1,57 @@
+import * as cryptoService from '../../../src/shared/domain/services/crypto-service.js';
+import { databaseBuffer } from '../database-buffer.js';
+
+const OIDC_PROVIDERS_TABLE_NAME = 'oidc-providers';
+
+export async function buildOidcProvider({
+  id = databaseBuffer.getNextId(),
+  accessTokenLifespan,
+  claimsToStore,
+  clientId,
+  clientSecret,
+  customProperties,
+  enabled,
+  enabledForPixAdmin,
+  extraAuthorizationUrlParameters,
+  identityProvider,
+  idTokenLifespan,
+  openidClientExtraMetadata,
+  openidConfigurationUrl,
+  organizationName,
+  postLogoutRedirectUri,
+  redirectUri,
+  scope,
+  shouldCloseSession,
+  slug,
+  source,
+}) {
+  const encryptedClientSecret = await cryptoService.encrypt(clientSecret);
+
+  const values = {
+    id,
+    accessTokenLifespan,
+    claimsToStore,
+    clientId,
+    customProperties,
+    enabled,
+    enabledForPixAdmin,
+    encryptedClientSecret,
+    extraAuthorizationUrlParameters,
+    identityProvider,
+    idTokenLifespan,
+    openidClientExtraMetadata,
+    openidConfigurationUrl,
+    organizationName,
+    postLogoutRedirectUri,
+    redirectUri,
+    scope,
+    shouldCloseSession,
+    slug,
+    source,
+  };
+
+  return databaseBuffer.pushInsertable({
+    tableName: OIDC_PROVIDERS_TABLE_NAME,
+    values,
+  });
+}
