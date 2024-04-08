0207951d639392f6e0f5765268bca033347fc994
Antoine Ceol <antoine.ceol@gmail.com>
2024-04-08 17:03:59 +0200

    :fire: api: Remove pixCertificationStatus column

---
+++ api/db/migrations/20240408150129_remove-column-pix-certification-status-in-certification-courses-table.js
@@ -0,0 +1,16 @@
+const TABLE_NAME = 'certification-courses';
+const COLUMN_NAME = 'pixCertificationStatus';
+
+const up = function (knex) {
+  return knex.schema.table(TABLE_NAME, function (table) {
+    table.dropColumn(COLUMN_NAME);
+  });
+};
+
+const down = function (knex) {
+  return knex.schema.table(TABLE_NAME, function (table) {
+    table.string(COLUMN_NAME).defaultTo(null);
+  });
+};
+
+export { down, up };
