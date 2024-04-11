26981d60f055965e6cf19075e1b195dd9edf5192
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2024-04-11 15:21:24 +0200

    feat(api): add builder for learner with attributes

---
+++ api/db/database-builder/factory/prescription/organization-learners/build-organization-learner.js
@@ -0,0 +1,43 @@
+import _ from 'lodash';
+
+import { databaseBuffer } from '../../../database-buffer.js';
+import { buildOrganization } from '../../build-organization.js';
+import { buildUser } from '../../build-user.js';
+
+const buildOrganizationLearner = function ({
+  id = databaseBuffer.getNextId(),
+  firstName = 'first-name',
+  lastName = 'last-name',
+  isDisabled = false,
+  createdAt = new Date('2021-01-01'),
+  updatedAt = new Date('2021-02-01'), // for BEGINNING_OF_THE_2020_SCHOOL_YEAR, can outdate very fast! ;)
+  organizationId,
+  userId,
+  deletedBy = null,
+  deletedAt = null,
+  attributes = null,
+} = {}) {
+  organizationId = _.isUndefined(organizationId) ? buildOrganization().id : organizationId;
+  userId = _.isUndefined(userId) ? buildUser().id : userId;
+
+  const values = {
+    id,
+    lastName,
+    firstName,
+    isDisabled,
+    createdAt,
+    updatedAt,
+    organizationId,
+    userId,
+    deletedBy,
+    deletedAt,
+    attributes,
+  };
+
+  return databaseBuffer.pushInsertable({
+    tableName: 'organization-learners',
+    values,
+  });
+};
+
+export { buildOrganizationLearner };
