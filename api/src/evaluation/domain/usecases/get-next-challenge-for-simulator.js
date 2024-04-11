a8352a01cb0817371c58b97f3c7ad48cc5bdc3b2
Guillaume Olejniczak <la.toile.cosmique@gmail.com>
2024-04-11 12:15:22 +0200

    feat(api): return smart random details for simulator route

--- api/src/evaluation/domain/usecases/get-next-challenge-for-simulator.js
+++ api/src/evaluation/domain/usecases/get-next-challenge-for-simulator.js
@@ -1,27 +1,37 @@
+import { STEPS_NAMES } from '../models/SmartRandomStep.js';
+
 /**
  * @param {simulationParameters: SimulationParameters} simulationParameters
  * @param pickChallengeService
  * @param smartRandomService
- * @returns {Promise<Challenge | null>}
+ * @returns {Promise<{challenge: Challenge, smartRandomDetails: SmartRandomDetails} | null>}
  */
 const getNextChallengeForSimulator = function ({ simulationParameters, pickChallengeService, smartRandomService }) {
-  const { possibleSkillsForNextChallenge, hasAssessmentEnded } = smartRandomService.getPossibleSkillsForNextChallenge({
-    knowledgeElements: simulationParameters.knowledgeElements,
-    challenges: simulationParameters.challenges,
-    locale: simulationParameters.locale,
-    targetSkills: simulationParameters.skills,
-    allAnswers: simulationParameters.answers,
-  });
+  const { possibleSkillsForNextChallenge, hasAssessmentEnded, smartRandomDetails } =
+    smartRandomService.getPossibleSkillsForNextChallenge({
+      knowledgeElements: simulationParameters.knowledgeElements,
+      challenges: simulationParameters.challenges,
+      locale: simulationParameters.locale,
+      targetSkills: simulationParameters.skills,
+      allAnswers: simulationParameters.answers,
+      lastAnswer: simulationParameters.answers.length
+        ? simulationParameters.answers[simulationParameters.answers.length - 1]
+        : null,
+    });
 
   if (hasAssessmentEnded) {
     return null;
   }
 
-  return pickChallengeService.pickChallenge({
+  const challenge = pickChallengeService.pickChallenge({
     skills: possibleSkillsForNextChallenge,
     randomSeed: simulationParameters.assessmentId,
     locale: simulationParameters.locale,
   });
+
+  smartRandomDetails.addStep(STEPS_NAMES.RANDOM_PICK, [challenge.skill]);
+
+  return { challenge, smartRandomDetails };
 };
 
 export { getNextChallengeForSimulator };
