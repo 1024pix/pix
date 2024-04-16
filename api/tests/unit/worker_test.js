71a641d099f13bc53e2284a9b7100834cba716cb
Alexandre Monney <alexandre.monney@pix.fr>
2024-04-16 10:40:45 +0200

    feat(api): add test on worker for previously created job

--- api/tests/unit/worker_test.js
+++ api/tests/unit/worker_test.js
@@ -1,5 +1,7 @@
 import { ImportOrganizationLearnersJob } from '../../src/prescription/learner-management/infrastructure/jobs/ImportOrganizationLearnersJob.js';
 import { ImportOrganizationLearnersJobHandler } from '../../src/prescription/learner-management/infrastructure/jobs/ImportOrganizationLearnersJobHandler.js';
+import { ValidateOrganizationImportFileJob } from '../../src/prescription/learner-management/infrastructure/jobs/ValidateOrganizationImportFileJob.js';
+import { ValidateOrganizationImportFileJobHandler } from '../../src/prescription/learner-management/infrastructure/jobs/ValidateOrganizationImportFileJobHandler.js';
 import { runJobs } from '../../worker.js';
 import { expect, sinon } from '../test-helper.js';
 
@@ -22,10 +24,35 @@ describe('#runjobs', function () {
     });
 
     // then
-    const call = monitoredJobQueueStub.performJob
+    const calls = monitoredJobQueueStub.performJob
       .getCalls()
       .find(({ args }) => args[0] === ImportOrganizationLearnersJob.name);
 
-    expect(call.args[1]).to.equal(ImportOrganizationLearnersJobHandler);
+    expect(calls.args[1]).to.equal(ImportOrganizationLearnersJobHandler);
+  });
+
+  it('should register ValidateOrganizationImportFileJob', async function () {
+    //given
+    const pgBossStub = { schedule: sinon.stub() };
+    const monitoredJobQueueStub = { performJob: sinon.stub() };
+    const startPgBossStub = sinon.stub();
+    startPgBossStub.resolves(pgBossStub);
+    const createMonitoredJobQueueStub = sinon.stub();
+    createMonitoredJobQueueStub.withArgs(pgBossStub).returns(monitoredJobQueueStub);
+    const scheduleCpfJobsStub = sinon.stub();
+
+    // when
+    await runJobs({
+      startPgBoss: startPgBossStub,
+      createMonitoredJobQueue: createMonitoredJobQueueStub,
+      scheduleCpfJobs: scheduleCpfJobsStub,
+    });
+
+    // then
+    const calls = monitoredJobQueueStub.performJob
+      .getCalls()
+      .find(({ args }) => args[0] === ValidateOrganizationImportFileJob.name);
+
+    expect(calls.args[1]).to.equal(ValidateOrganizationImportFileJobHandler);
   });
 });
