aed464f77bc69feaeee719058d86ef13291d20d7
Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
2024-04-16 18:00:46 +0200

    feat(admin): add badge campaign criterion edit modal

--- admin/app/adapters/badge-criterion.js
+++ admin/app/adapters/badge-criterion.js
@@ -6,4 +6,12 @@ export default class BadgeCriterionAdapter extends ApplicationAdapter {
   urlForCreateRecord(_modelName, badgeCriterion) {
     return `${this.host}/${this.namespace}/badges/${badgeCriterion.belongsTo('badge').id}/badge-criteria`;
   }
+
+  updateRecord(store, type, snapshot) {
+    const payload = this.serialize(snapshot);
+    delete payload.data.attributes['scope'];
+
+    const url = this.buildURL(type.modelName, snapshot.id, snapshot, 'updateRecord');
+    return this.ajax(url, 'PATCH', { data: payload });
+  }
 }
