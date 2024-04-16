a8701075f7f56629b6717bb64b2749e2fb3789dc
Antoine Ceol <antoine.ceol@gmail.com>
2024-04-16 14:35:28 +0200

    mon-pix: Call stop certification postMessage on certification stop
    Co-authored-by: Nicolas Lepage <19571875+nlepage@users.noreply.github.com>

--- mon-pix/app/routes/authenticated/certifications/results.js
+++ mon-pix/app/routes/authenticated/certifications/results.js
@@ -3,10 +3,15 @@ import { service } from '@ember/service';
 
 export default class ResultsRoute extends Route {
   @service store;
+  @service windowPostMessage;
 
   async model(params) {
     const certificationCourse = await this.store.findRecord('certification-course', params.certification_id);
     await certificationCourse.assessment.reload();
     return certificationCourse;
+  }
+
+  afterModel() {
+    return this.windowPostMessage.stopCertification();
   }
 }
