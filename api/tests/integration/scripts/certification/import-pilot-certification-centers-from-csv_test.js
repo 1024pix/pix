c83400278ae4e5e5fc55621068f13af1d23608ce
Steph0 <Steph0@users.noreply.github.com>
2024-04-17 15:32:53 +0200

    :sparkles: api: block V3 centers from pilot centers
    Co-authored-by: Andreia Pena <andreia.pena@pix.fr>

--- api/tests/integration/scripts/certification/import-pilot-certification-centers-from-csv_test.js
+++ api/tests/integration/scripts/certification/import-pilot-certification-centers-from-csv_test.js
@@ -3,14 +3,17 @@ import * as url from 'node:url';
 
 import { main } from '../../../../scripts/certification/import-pilot-certification-centers-from-csv.js';
 import { CERTIFICATION_FEATURES } from '../../../../src/certification/shared/domain/constants.js';
-import { databaseBuilder, expect, knex } from '../../../test-helper.js';
+import { catchErr, databaseBuilder, expect, knex } from '../../../test-helper.js';
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 describe('Integration | Scripts | Certification | import-pilot-certification-centers-from-csv', function () {
   context('when pilot certification center list from a csv file is imported', function () {
     it('should import the pilot certification center list', async function () {
       // given
-      const csvFilePath = path.join(__dirname, 'files/pilot-certification-centers.csv');
+      const csvFilePath = path.join(
+        __dirname,
+        'files/import-pilot-certification-centers-from-csv/pilot-certification-centers.csv',
+      );
       const certificationCenterId1 = 1001;
       const certificationCenterId2 = 1002;
       const certificationCenterId3 = 1003;
@@ -41,6 +44,33 @@ describe('Integration | Scripts | Certification | import-pilot-certification-cen
         { certificationCenterId: certificationCenterId1, featureId },
         { certificationCenterId: certificationCenterId2, featureId },
       ]);
+    });
+
+    context('when there is a V3 certification center in the csv file', function () {
+      it('should not import the certification center list', async function () {
+        // given
+        const csvFilePath = path.join(
+          __dirname,
+          'files/import-pilot-certification-centers-from-csv/pilot-certification-centers-with-v3.csv',
+        );
+        const v2CertificationCenterId = 2001;
+        const v3CertificationCenterId = 2002;
+
+        const featureId = databaseBuilder.factory.buildFeature({
+          key: CERTIFICATION_FEATURES.CAN_REGISTER_FOR_A_COMPLEMENTARY_CERTIFICATION_ALONE.key,
+        }).id;
+
+        databaseBuilder.factory.buildCertificationCenter({ id: v2CertificationCenterId });
+        databaseBuilder.factory.buildCertificationCenter({ id: v3CertificationCenterId, isV3Pilot: true });
+        databaseBuilder.factory.buildCertificationCenterFeature({ v2CertificationCenterId, featureId });
+        await databaseBuilder.commit();
+
+        // when
+        const error = await catchErr(main)(csvFilePath);
+
+        // then
+        expect(error.message).to.equal('V3 certification centers : 2002 are not allowed as pilots');
+      });
     });
   });
 });
