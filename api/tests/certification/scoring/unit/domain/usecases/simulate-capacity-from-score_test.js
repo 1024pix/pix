db48f3719dd6ee4c1caf7caf4f627407cd97d92e
Alexandre COIN <alexandre@macbook-pro-de-alexandre.home>
2024-04-18 11:16:59 +0200

    :sparkles: api: add usecase to use CapacitySimulator

---
+++ api/tests/certification/scoring/unit/domain/usecases/simulate-capacity-from-score_test.js
@@ -0,0 +1,67 @@
+import { simulateCapacityFromScore } from '../../../../../../src/certification/scoring/domain/usecases/simulate-capacity-from-score.js';
+import { domainBuilder, expect, sinon } from '../../../../../test-helper.js';
+
+describe('Unit | UseCase | simulate-capacity-from-score', function () {
+  let scoringConfigurationRepository;
+
+  beforeEach(function () {
+    scoringConfigurationRepository = {
+      getLatestByDateAndLocale: sinon.stub(),
+    };
+  });
+
+  it('should return a capacity', async function () {
+    // given
+    const date = new Date();
+
+    const v3CertificationScoring = domainBuilder.buildV3CertificationScoring({
+      competencesForScoring: [
+        domainBuilder.buildCompetenceForScoring({
+          competenceId: 'recCompetenceId',
+          areaCode: '1',
+          competenceCode: '1.1',
+          intervals: [
+            {
+              bounds: {
+                max: 4,
+                min: -4,
+              },
+              competenceLevel: 0,
+            },
+          ],
+        }),
+      ],
+      certificationScoringConfiguration: [
+        {
+          bounds: { max: 4, min: -4 },
+          meshLevel: 0,
+        },
+      ],
+    });
+
+    scoringConfigurationRepository.getLatestByDateAndLocale
+      .withArgs({ date, locale: 'fr-fr' })
+      .resolves(v3CertificationScoring);
+
+    // when
+    const result = await simulateCapacityFromScore({
+      score: 768,
+      date,
+      scoringConfigurationRepository,
+    });
+
+    // then
+    expect(result).to.deepEqualInstance(
+      domainBuilder.buildScoringAndCapacitySimulatorReport({
+        capacity: 2,
+        score: undefined,
+        competences: [
+          {
+            level: 0,
+            competenceCode: '1.1',
+          },
+        ],
+      }),
+    );
+  });
+});
