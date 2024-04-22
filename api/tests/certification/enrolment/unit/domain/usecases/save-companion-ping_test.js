8502bcc6ed9c3b85ad2c4370f7a4cdfc888deb5e
Antoine Ceol <antoine.ceol@gmail.com>
2024-04-22 09:52:00 +0200

    :sparkles: api: Add use case to store companion ping
    Co-authored-by: Nicolas Lepage <19571875+nlepage@users.noreply.github.com>

--- api/tests/certification/enrolment/unit/domain/usecases/save-companion-ping_test.js
+++ api/tests/certification/enrolment/unit/domain/usecases/save-companion-ping_test.js
@@ -1,6 +1,6 @@
 import { NotFoundError } from '../../../../../../lib/domain/errors.js';
-import { saveCompanionPing } from '../../../../../../src/certification/session/domain/usecases/save-companion-ping.js';
-import { catchErr, expect } from '../../../../../test-helper.js';
+import { saveCompanionPing } from '../../../../../../src/certification/enrolment/domain/usecases/save-companion-ping.js';
+import { catchErr, expect, sinon } from '../../../../../test-helper.js';
 
 describe('Unit | UseCase | save-companion-ping', function () {
   describe('#saveCompanionPing', function () {
@@ -8,6 +8,10 @@ describe('Unit | UseCase | save-companion-ping', function () {
       it('should persist the companion ping in redis', async function () {
         // given
         const userId = 123;
+        const companionRepository = { getSessionInfo: sinon.stub() };
+        const temporaryStorageService = { saveCompanionPing: sinon.stub() };
+        companionRepository.getSessionInfo.withArgs(userId).resolves({ sessionId: 99, certificationCandidate: 101 });
+        temporaryStorageService.saveCompanionPing.withArgs({ sessionId: 99, certificationCandidate: 101 }).resolves();
 
         await saveCompanionPing({
           userId,
@@ -17,7 +21,8 @@ describe('Unit | UseCase | save-companion-ping', function () {
         const response = await saveCompanionPing({ userId });
 
         // then
-        expect(response).to.deep.equal(123);
+        expect(response).to.deep.equal();
+        expect(temporaryStorageService).to.have.been.calledWithExactly({ sessionId: 99, certificationCandidate: 101 });
       });
     });
 
