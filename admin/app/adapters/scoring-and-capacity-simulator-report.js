3a9c1eeca9cf05182503598cc786f8f80d374262
matthieu.gros <matthieu.gros@octo.com>
2024-04-24 11:34:53 +0200

    :sparkles: admin: add adapter

---
+++ admin/app/adapters/scoring-and-capacity-simulator-report.js
@@ -0,0 +1,18 @@
+import ApplicationAdapter from './application';
+
+export default class ScoringAndCapacitySimulatorReportAdapter extends ApplicationAdapter {
+  namespace = 'api/admin';
+
+  getSimulationResult({ score, capacity }) {
+    let data;
+    if (score) {
+      data = { score };
+    } else if (capacity) {
+      data = { capacity };
+    }
+    const url = `${this.host}/${this.namespace}/simulate-score-or-capacity`;
+    return this.ajax(url, 'POST', {
+      data: { data },
+    });
+  }
+}
