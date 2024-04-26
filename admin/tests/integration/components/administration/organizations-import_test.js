a7da017b83836ef98feeffab40932531d53a304b
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2024-04-26 10:26:50 +0200

    test(admin): fix module test encapsulation

--- admin/tests/integration/components/administration/organizations-import_test.js
+++ admin/tests/integration/components/administration/organizations-import_test.js
@@ -12,30 +12,31 @@ module('Integration | Component |  administration/organizations-import', functio
   setupIntlRenderingTest(hooks);
   setupMirage(hooks);
 
-  module('when import succeeds', function () {});
-  test('it displays a success notification', async function (assert) {
-    // given
-    const file = new Blob(['foo'], { type: `valid-file` });
-    const notificationSuccessStub = sinon.stub();
-    class NotificationsStub extends Service {
-      success = notificationSuccessStub;
-      clearAll = sinon.stub();
-    }
-    this.owner.register('service:notifications', NotificationsStub);
+  module('when import succeeds', function () {
+    test('it displays a success notification', async function (assert) {
+      // given
+      const file = new Blob(['foo'], { type: `valid-file` });
+      const notificationSuccessStub = sinon.stub();
+      class NotificationsStub extends Service {
+        success = notificationSuccessStub;
+        clearAll = sinon.stub();
+      }
+      this.owner.register('service:notifications', NotificationsStub);
 
-    // when
-    const screen = await render(hbs`<Administration::OrganizationsImport />`);
-    const input = await screen.findByLabelText(
-      this.intl.t('components.administration.organizations-import.upload-button'),
-    );
-    await triggerEvent(input, 'change', { files: [file] });
+      // when
+      const screen = await render(hbs`<Administration::OrganizationsImport />`);
+      const input = await screen.findByLabelText(
+        this.intl.t('components.administration.organizations-import.upload-button'),
+      );
+      await triggerEvent(input, 'change', { files: [file] });
 
-    // then
-    assert.ok(true);
-    sinon.assert.calledWith(
-      notificationSuccessStub,
-      this.intl.t('components.administration.organizations-import.notifications.success'),
-    );
+      // then
+      assert.ok(true);
+      sinon.assert.calledWith(
+        notificationSuccessStub,
+        this.intl.t('components.administration.organizations-import.notifications.success'),
+      );
+    });
   });
 
   module('when import fails', function () {
