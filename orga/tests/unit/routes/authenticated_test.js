92cc6fe73201e83c104391dcadf8b003d61c298b
Marie-Hélène <marie-helene.rots@pix.fr>
2024-05-02 10:02:44 +0200

    test?(orga): add tests to authenticated route before model

--- orga/tests/unit/routes/authenticated_test.js
+++ orga/tests/unit/routes/authenticated_test.js
@@ -6,6 +6,75 @@ import sinon from 'sinon';
 module('Unit | Route | authenticated', function (hooks) {
   setupTest(hooks);
 
+  module('beforeModel', function () {
+    test('should abort transition if user not logged in', async function (assert) {
+      // given
+      const route = this.owner.lookup('route:authenticated');
+      const transition = { isAborted: true };
+      const requireAuthenticationStub = sinon.stub(route.session, 'requireAuthentication');
+      const replaceWithStub = sinon.stub(route.router, 'replaceWith');
+
+      // when
+      route.beforeModel(transition);
+
+      // then
+      assert.ok(requireAuthenticationStub.calledWith(transition, 'login'));
+      assert.notOk(replaceWithStub.called);
+    });
+
+    test('should redirect towards cgu if not accepted yet', async function (assert) {
+      // given
+      const organizationId = Symbol('organizationId');
+      class CurrentUserStub extends Service {
+        prescriber = { placesManagement: true, pixOrgaTermsOfServiceAccepted: false };
+        organization = {
+          id: organizationId,
+        };
+      }
+
+      const route = this.owner.lookup('route:authenticated');
+      const transition = { isAborted: false };
+
+      sinon.stub(route.session, 'requireAuthentication');
+
+      const replaceWithStub = sinon.stub(route.router, 'replaceWith');
+
+      this.owner.register('service:current-user', CurrentUserStub);
+
+      // when
+      route.beforeModel(transition);
+
+      // then
+      assert.ok(replaceWithStub.calledWithExactly('terms-of-service'));
+    });
+
+    test('should not redirect towards cgu if already accepted yet', async function (assert) {
+      // given
+      const organizationId = Symbol('organizationId');
+      class CurrentUserStub extends Service {
+        prescriber = { placesManagement: true, pixOrgaTermsOfServiceAccepted: true };
+        organization = {
+          id: organizationId,
+        };
+      }
+
+      const route = this.owner.lookup('route:authenticated');
+      const transition = { isAborted: false };
+
+      sinon.stub(route.session, 'requireAuthentication');
+
+      const replaceWithStub = sinon.stub(route.router, 'replaceWith');
+
+      this.owner.register('service:current-user', CurrentUserStub);
+
+      // when
+      route.beforeModel(transition);
+
+      // then
+      assert.notOk(replaceWithStub.calledWithExactly('terms-of-service'));
+    });
+  });
+
   module('model', function () {
     test('should query record organization-place-statistic', async function (assert) {
       // given
