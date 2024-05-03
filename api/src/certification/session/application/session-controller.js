e7a220d3796a83a25b502ab612f1713fa61f1248
Antoine Ceol <antoine.ceol@gmail.com>
2024-05-03 10:16:08 +0200

    :fire: api: Drop duplicated Session serializer

--- api/src/certification/session/application/session-controller.js
+++ api/src/certification/session/application/session-controller.js
@@ -3,14 +3,14 @@ import { usecases as sessionUsecases } from '../../session/domain/usecases/index
 import * as certificationReportSerializer from '../../shared/infrastructure/serializers/jsonapi/certification-report-serializer.js';
 import * as sessionSerializer from '../infrastructure/serializers/jsonapi/session-serializer.js';
 
-const createSession = async function (request, _h, dependencies = { sessionSerializer }) {
+async function createSession(request, _h, dependencies = { sessionSerializer }) {
   const userId = request.auth.credentials.userId;
   const session = dependencies.sessionSerializer.deserialize(request.payload);
 
   const newSession = await sessionUsecases.createSession({ userId, session });
 
   return dependencies.sessionSerializer.serialize({ session: newSession });
-};
+}
 
 const update = async function (request, h, dependencies = { sessionSerializer }) {
   const userId = request.auth.credentials.userId;
