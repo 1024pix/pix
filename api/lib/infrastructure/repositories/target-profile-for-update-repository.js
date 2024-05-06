a169a8eb08443867c90f1503270c85bc6702d1e3
Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
2024-05-06 10:58:54 +0200

    feat(api): improve target-profile-for-update-repository update method

--- api/lib/infrastructure/repositories/target-profile-for-update-repository.js
+++ api/lib/infrastructure/repositories/target-profile-for-update-repository.js
@@ -1,23 +1,45 @@
 import { knex } from '../../../db/knex-database-connection.js';
+import { DomainTransaction } from '../DomainTransaction.js';
 
-const update = async function ({
-  targetProfileId,
-  name,
-  imageUrl,
-  description,
-  comment,
-  category,
-  areKnowledgeElementsResettable,
-}) {
-  const targetProfileToUpdate = {
+const TARGET_PROFILE_TABLE_NAME = 'target-profiles';
+const TARGET_PROFILE_TUBES_TABLE_NAME = 'target-profile_tubes';
+
+const update = async function (targetProfile, domainTransaction = DomainTransaction.emptyTransaction()) {
+  const knexConn = domainTransaction?.knexTransaction || (await knex.transaction());
+
+  const {
+    id: targetProfileId,
     name,
     imageUrl,
     description,
     comment,
     category,
     areKnowledgeElementsResettable,
-  };
-  return knex('target-profiles').where({ id: targetProfileId }).update(targetProfileToUpdate);
+  } = targetProfile;
+
+  await knexConn(TARGET_PROFILE_TABLE_NAME).where({ id: targetProfileId }).update({
+    name,
+    imageUrl,
+    description,
+    comment,
+    category,
+    areKnowledgeElementsResettable,
+  });
+
+  if (targetProfile.tubes?.length > 0) {
+    const tubesData = targetProfile.tubes.map((tube) => ({
+      targetProfileId,
+      tubeId: tube.id,
+      level: tube.level,
+    }));
+
+    await knexConn(TARGET_PROFILE_TUBES_TABLE_NAME).delete().where({ targetProfileId });
+    await knexConn(TARGET_PROFILE_TUBES_TABLE_NAME).insert(tubesData);
+  }
+
+  if (!domainTransaction?.knexTransaction) {
+    await knexConn.commit();
+  }
 };
 
 export { update };
--- api/tests/integration/infrastructure/repositories/target-profile-for-update-repository_test.js
+++ api/tests/integration/infrastructure/repositories/target-profile-for-update-repository_test.js
@@ -3,39 +3,118 @@ import { databaseBuilder, expect, knex } from '../../../test-helper.js';
 
 describe('Integration | Repository | Target-profile-for-update', function () {
   describe('#update', function () {
-    it('should update the target profile', async function () {
-      // given
-      databaseBuilder.factory.buildTargetProfile({
-        id: 123,
+    let existingTargetProfile, tube1, tube2;
+
+    beforeEach(async function () {
+      existingTargetProfile = databaseBuilder.factory.buildTargetProfile({
         name: 'old name',
-        imageUrl: 'old image url',
+        category: 'old category',
         description: 'old description',
         comment: 'old comment',
-        category: 'old category',
-        areKnowledgeElementsResettable: false,
+        isPublic: false,
+        imageUrl: 'old image url',
       });
-      await databaseBuilder.commit();
-      const updatedData = {
-        targetProfileId: 123,
-        name: 'new-name',
-        imageUrl: 'new-image-url',
-        description: 'new description',
-        comment: 'new comment',
-        category: 'new category',
-        areKnowledgeElementsResettable: true,
+
+      tube1 = {
+        targetProfileId: existingTargetProfile.id,
+        tubeId: '1',
+        level: 1,
       };
+      tube2 = {
+        targetProfileId: existingTargetProfile.id,
+        tubeId: '2',
+        level: 2,
+      };
+      databaseBuilder.factory.buildTargetProfileTube(tube1);
+      databaseBuilder.factory.buildTargetProfileTube(tube2);
 
-      // when
-      await targetProfileForUpdateRepository.update(updatedData);
+      await databaseBuilder.commit();
+    });
 
-      // then
-      const targetProfileFromDB = await knex('target-profiles').where({ id: 123 }).first();
-      expect(targetProfileFromDB.name).to.equal('new-name');
-      expect(targetProfileFromDB.imageUrl).to.equal('new-image-url');
-      expect(targetProfileFromDB.description).to.equal('new description');
-      expect(targetProfileFromDB.comment).to.equal('new comment');
-      expect(targetProfileFromDB.category).to.equal('new category');
-      expect(targetProfileFromDB.areKnowledgeElementsResettable).to.be.true;
+    context('with tubes', function () {
+      it('should update the target profile and the tubes', async function () {
+        // given
+        const updatedData = {
+          name: 'new-name',
+          category: 'new category',
+          description: 'new description',
+          comment: 'new comment',
+          isPublic: true,
+          imageUrl: 'new-image-url',
+          tubes: [
+            {
+              id: '1',
+              level: 2,
+            },
+            {
+              id: '3',
+              level: 8,
+            },
+          ],
+        };
+
+        // when
+        await targetProfileForUpdateRepository.update({ ...existingTargetProfile, ...updatedData });
+
+        // then
+        const targetProfileFromDB = await knex('target-profiles').where({ id: existingTargetProfile.id }).first();
+
+        expect(targetProfileFromDB.name).to.equal('new-name');
+        expect(targetProfileFromDB.imageUrl).to.equal('new-image-url');
+        expect(targetProfileFromDB.description).to.equal('new description');
+        expect(targetProfileFromDB.comment).to.equal('new comment');
+        expect(targetProfileFromDB.category).to.equal('new category');
+
+        const targetProfileTubesFromDB = await knex('target-profile_tubes').where({
+          targetProfileId: existingTargetProfile.id,
+        });
+        expect(targetProfileTubesFromDB).to.have.length(2);
+        // eslint-disable-next-line no-unused-vars
+        expect(targetProfileTubesFromDB.map(({ id, ...tube }) => tube)).to.deep.equal([
+          {
+            ...tube1,
+            level: 2,
+          },
+          {
+            tubeId: '3',
+            level: 8,
+            targetProfileId: existingTargetProfile.id,
+          },
+        ]);
+      });
+    });
+
+    context('without tubes', function () {
+      it('should update the target profile informations', async function () {
+        // given
+        const updatedData = {
+          name: 'new-name',
+          category: 'new category',
+          description: 'new description',
+          comment: 'new comment',
+          isPublic: true,
+          imageUrl: 'new-image-url',
+        };
+
+        // when
+        await targetProfileForUpdateRepository.update({ ...existingTargetProfile, ...updatedData });
+
+        // then
+        const targetProfileFromDB = await knex('target-profiles').where({ id: existingTargetProfile.id }).first();
+
+        expect(targetProfileFromDB.name).to.equal('new-name');
+        expect(targetProfileFromDB.imageUrl).to.equal('new-image-url');
+        expect(targetProfileFromDB.description).to.equal('new description');
+        expect(targetProfileFromDB.comment).to.equal('new comment');
+        expect(targetProfileFromDB.category).to.equal('new category');
+
+        const targetProfileTubesFromDB = await knex('target-profile_tubes').where({
+          targetProfileId: existingTargetProfile.id,
+        });
+        expect(targetProfileTubesFromDB).to.have.length(2);
+        // eslint-disable-next-line no-unused-vars
+        expect(targetProfileTubesFromDB.map(({ id, ...tube }) => tube)).to.deep.equal([tube1, tube2]);
+      });
     });
   });
 });
