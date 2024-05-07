1e7fda786afd7fc145d4a504f4a6f787901387f4
Yvonnick Frin <frin.yvonnick@gmail.com>
2024-05-07 09:54:55 +0200

    fix(orga): handle async relationship in activity component

--- orga/app/components/organization-learner/activity.js
+++ orga/app/components/organization-learner/activity.js
@@ -1,11 +1,21 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
 export default class Activity extends Component {
+  @tracked statistics = [];
+
+  constructor() {
+    super(...arguments);
+    Promise.resolve(this.args.statistics).then((statistics) => {
+      this.statistics = statistics;
+    });
+  }
+
   get assessmentStatistics() {
-    return this.args.statistics.find((stat) => stat.id === 'ASSESSMENT');
+    return this.statistics.find((stat) => stat.id === 'ASSESSMENT');
   }
 
   get profileCollectionsStatistics() {
-    return this.args.statistics.find((stat) => stat.id === 'PROFILES_COLLECTION');
+    return this.statistics.find((stat) => stat.id === 'PROFILES_COLLECTION');
   }
 }
