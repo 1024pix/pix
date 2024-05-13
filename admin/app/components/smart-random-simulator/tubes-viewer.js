256698dde57ce2027c6e944493e8c02fcc4e6b28
Guillaume Olejniczak <la.toile.cosmique@gmail.com>
2024-05-13 16:11:52 +0200

    refactor(admin): extract smart random steps in another file

--- admin/app/components/smart-random-simulator/tubes-viewer.js
+++ admin/app/components/smart-random-simulator/tubes-viewer.js
@@ -1,6 +1,8 @@
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
 
+import { SMART_RANDOM_STEPS } from './SMART_RANDOM_STEPS';
+
 const SKILLS_STATUSES = {
   MISSING: 'missing',
   CURRENT: 'current',
@@ -10,49 +12,6 @@ const SKILLS_STATUSES = {
 
 const PRIMARY_COLOR = 'primary';
 const NEUTRAL_COLOR = 'neutral';
-
-const STEPS = {
-  NO_CHALLENGE: {
-    translatedName: 'Acquis sans épreuves',
-    description:
-      'Supprime les acquis qui ne contiennent aucun challenge ou qui ne contiennent que des challenges déjà joués dans cette évaluation',
-  },
-  EASY_TUBES: {
-    translatedName: 'Tubes faciles',
-    description:
-      'Si applicable, garde seulement les tubes qui contiennent uniquement des acquis inférieurs au niveau 3',
-  },
-  TIMED_SKILLS: {
-    translatedName: 'Épreuves chronométrées',
-    description:
-      "Si l'utilisateur vient de commencer ou que la précédente épreuve était chronométrée, " +
-      'cette étape supprime les acquis avec une épreuve chronométré.' +
-      "Ne s'applique pas si il ne reste plus que des épreuves chronométrés",
-  },
-  DEFAULT_LEVEL: {
-    translatedName: 'Niveau par défaut',
-    description:
-      "Ne garde que les épreuves de niveau 2, si aucun acquis de niveau 2 n'est disponible, garde seulement les acquis de plus petit niveau. Ne se déclenche que pour la première épreuve de l'évaluation",
-  },
-  RANDOM_PICK: {
-    translatedName: 'Choix aléatoire',
-    description: 'Sélectionne un acquis au hasard parmi les acquis restants',
-  },
-  MAX_REWARDING_SKILLS: {
-    translatedName: 'Acquis les plus lucratifs',
-    description:
-      'Calculés à partir de la probabilité de bonne réponse d’un utilisateur à un niveau donné ainsi que la présence d’autres acquis dans le sujet de l’acquis évalué.',
-  },
-  TOO_DIFFICULT: {
-    translatedName: 'Acquis trop difficiles',
-    description:
-      'Supprime les acquis de niveau strictement supérieur à 2 par rapport au niveau estimé de l’utilisateur',
-  },
-  ALREADY_TESTED: {
-    translatedName: 'Déjà testés',
-    description: "Supprime les acquis déjà présents dans les ke de l'utilisateur",
-  },
-};
 
 export default class TubesViewer extends Component {
   levels = [1, 2, 3, 4, 5, 6, 7, 8];
@@ -119,10 +78,10 @@ export default class TubesViewer extends Component {
   }
 
   getStepName(stepName) {
-    return STEPS[stepName].translatedName;
+    return SMART_RANDOM_STEPS[stepName].translatedName;
   }
 
   getStepDescription(stepName) {
-    return STEPS[stepName].description;
+    return SMART_RANDOM_STEPS[stepName].description;
   }
 }
