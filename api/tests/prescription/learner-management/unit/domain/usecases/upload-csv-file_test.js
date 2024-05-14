41bdf87c51b03392c0cce883483bb42fb54481d3
LionelB <lionel@lumographe.fr>
2024-05-14 15:50:49 +0200

    refactor(api): extract uploading csv file into a usecase

---
+++ api/tests/prescription/learner-management/unit/domain/usecases/upload-csv-file_test.js
@@ -0,0 +1,106 @@
+import { IMPORT_STATUSES } from '../../../../../../src/prescription/learner-management/domain/constants.js';
+import { OrganizationImport } from '../../../../../../src/prescription/learner-management/domain/models/OrganizationImport.js';
+import { uploadCsvFile } from '../../../../../../src/prescription/learner-management/domain/usecases/upload-csv-file.js';
+import { SupOrganizationLearnerImportHeader } from '../../../../../../src/prescription/learner-management/infrastructure/serializers/csv/sup-organization-learner-import-header.js';
+import { catchErr, createTempFile, expect, removeTempFile, sinon } from '../../../../../test-helper.js';
+import { getI18n } from '../../../../../tooling/i18n/i18n.js';
+
+const i18n = getI18n();
+const supOrganizationLearnerImportHeader = new SupOrganizationLearnerImportHeader(i18n).columns
+  .map((column) => column.name)
+  .join(';');
+
+describe('Unit | UseCase | ImportSupOrganizationLearner', function () {
+  const organizationId = 1;
+  const userId = 2;
+  let timer, fakeDate, organizationImportRepositoryStub, importStorageStub, payload, filepath, s3Filename;
+
+  beforeEach(async function () {
+    s3Filename = Symbol('filename');
+    filepath = await createTempFile(
+      'file.csv',
+      `${supOrganizationLearnerImportHeader}
+    Beatrix;The;Bride;Kiddo;Black Mamba;01/01/1970;thebride@example.net;12346;Assassination Squad;Hattori Hanzo;Deadly Viper Assassination Squad;Master;hello darkness my old friend;
+    O-Ren;;;Ishii;Cottonmouth;01/01/1980;ishii@example.net;789;Assassination Squad;Bill;Deadly Viper Assassination Squad;DUT;;`,
+    );
+    payload = { path: filepath };
+    fakeDate = new Date('2019-01-10');
+    timer = sinon.useFakeTimers({
+      now: fakeDate,
+      toFake: ['Date'],
+    });
+
+    importStorageStub = {
+      sendFile: sinon.stub(),
+    };
+
+    organizationImportRepositoryStub = {
+      save: sinon.stub(),
+    };
+  });
+
+  afterEach(async function () {
+    timer.restore();
+    await removeTempFile(filepath);
+  });
+
+  context('when there is no errors', function () {
+    it('save import state in database', async function () {
+      // given
+      importStorageStub.sendFile.withArgs({ filepath: payload.path }).resolves(s3Filename);
+
+      // when
+      await uploadCsvFile({
+        payload,
+        userId,
+        organizationId,
+        i18n,
+        organizationImportRepository: organizationImportRepositoryStub,
+        importStorage: importStorageStub,
+      });
+
+      // then
+      expect(organizationImportRepositoryStub.save).to.have.been.calledOnce;
+      expect(organizationImportRepositoryStub.save.firstCall.args[0]).to.deep.equals(
+        new OrganizationImport({
+          organizationId,
+          createdBy: userId,
+          createdAt: fakeDate,
+          updatedAt: fakeDate,
+          encoding: 'utf-8',
+          filename: s3Filename,
+          status: IMPORT_STATUSES.UPLOADED,
+        }),
+      );
+    });
+  });
+  context('when there is an upload error', function () {
+    it('save UPLOAD_ERROR state in database', async function () {
+      // given
+      importStorageStub.sendFile.withArgs({ filepath: payload.path }).rejects();
+
+      // when
+      const error = await catchErr(uploadCsvFile)({
+        payload,
+        userId,
+        organizationId,
+        i18n,
+        organizationImportRepository: organizationImportRepositoryStub,
+        importStorage: importStorageStub,
+      });
+
+      // then
+      expect(organizationImportRepositoryStub.save).to.have.been.calledOnce;
+      expect(organizationImportRepositoryStub.save.firstCall.args[0]).to.deep.equals(
+        new OrganizationImport({
+          organizationId,
+          createdBy: userId,
+          createdAt: fakeDate,
+          errors: [error],
+          status: IMPORT_STATUSES.UPLOAD_ERROR,
+          updatedAt: fakeDate,
+        }),
+      );
+    });
+  });
+});
