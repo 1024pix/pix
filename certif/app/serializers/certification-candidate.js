52d78731aa0d7f75e0661f97150d11bd3cba296d
Steph0 <Steph0@users.noreply.github.com>
2024-05-14 18:35:21 +0200

    :recycle: certif: fix custom error handling via JSONAPISerializer

--- certif/app/serializers/certification-candidate.js
+++ certif/app/serializers/certification-candidate.js
@@ -1,6 +1,10 @@
 import JSONAPISerializer from '@ember-data/serializer/json-api';
 
 export default class CertificationCandidateSerializer extends JSONAPISerializer {
+  // This bypasses extractErrors emberData behavior which relies on the property being a function.
+  // This should not be needed after upgrading to ember-data v5
+  extractErrors = false;
+
   serialize(...args) {
     const json = super.serialize(...args);
 
