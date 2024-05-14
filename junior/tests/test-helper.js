5f07351470c233aee50763f1ceab0d0080af026b
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2024-05-14 16:58:47 +0200

    chore(junior): add ember exam

--- junior/tests/test-helper.js
+++ junior/tests/test-helper.js
@@ -1,12 +1,11 @@
 import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
-import Application from 'junior/app';
-import config from 'junior/config/environment';
+import start from 'ember-exam/test-support/start';
 import * as QUnit from 'qunit';
 import { setup } from 'qunit-dom';
 
+import Application from '../app';
+import config from '../config/environment';
+
 setApplication(Application.create(config.APP));
-
 setup(QUnit.assert);
-
 start();
