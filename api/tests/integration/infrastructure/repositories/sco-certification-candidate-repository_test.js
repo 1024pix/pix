6fcb807be62f4ab8a0beb9e18ffc5a0e94583d3c
Andreia Pena <andreia.pena@pix.fr>
2024-05-16 17:17:57 +0200

    ✨ api:  add subscriptions on sco candidate enrollment

--- api/tests/integration/infrastructure/repositories/sco-certification-candidate-repository_test.js
+++ api/tests/integration/infrastructure/repositories/sco-certification-candidate-repository_test.js
@@ -77,6 +77,50 @@ describe('Integration | Repository | SCOCertificationCandidate', function () {
       expect(actualCandidates).to.exactlyContain(expectedCandidates);
     });
 
+    it('saves candidate subscriptions', async function () {
+      // given
+      const organizationLearnerId1 = databaseBuilder.factory.buildOrganizationLearner().id;
+      const scoCandidateAlreadySaved1 = databaseBuilder.factory.buildCertificationCandidate({
+        sessionId,
+        organizationLearnerId: organizationLearnerId1,
+      });
+      const organizationLearnerId3 = databaseBuilder.factory.buildOrganizationLearner().id;
+      const organizationLearnerId4 = databaseBuilder.factory.buildOrganizationLearner().id;
+      await databaseBuilder.commit();
+
+      const scoCandidates = [
+        domainBuilder.buildSCOCertificationCandidate({
+          ...scoCandidateAlreadySaved1,
+          organizationLearnerId: scoCandidateAlreadySaved1.organizationLearnerId,
+        }),
+        domainBuilder.buildSCOCertificationCandidate({
+          id: null,
+          firstName: 'Bobby',
+          lastName: 'LaPointe',
+          birthdate: '2001-01-04',
+          sex: 'M',
+          birthINSEECode: '75005',
+          organizationLearnerId: organizationLearnerId3,
+          sessionId,
+        }),
+        domainBuilder.buildSCOCertificationCandidate({
+          id: null,
+          organizationLearnerId: organizationLearnerId4,
+          sessionId,
+        }),
+      ];
+
+      // when
+      await scoCertificationCandidateRepository.addNonEnrolledCandidatesToSession({
+        sessionId,
+        scoCertificationCandidates: scoCandidates,
+      });
+
+      // then
+      const { count: subscriptionsCount } = await knex('certification-subscriptions').count().first();
+      expect(subscriptionsCount).to.equal(2);
+    });
+
     it('does nothing when no candidate is given', async function () {
       // when
       await scoCertificationCandidateRepository.addNonEnrolledCandidatesToSession({
