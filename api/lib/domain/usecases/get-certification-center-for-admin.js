c9d266dc0fc2ab3684caab2e1cc81b2170872fd8
mcampourcy <mireille.campourcy@pix.fr>
2024-05-16 14:30:30 +0200

    :recycle: api: Create a CenterForAdmin model

--- api/lib/domain/usecases/get-certification-center-for-admin.js
+++
@@ -1,5 +0,0 @@
-const getCertificationCenterForAdmin = function ({ id, certificationCenterForAdminRepository }) {
-  return certificationCenterForAdminRepository.get(id);
-};
-
-export { getCertificationCenterForAdmin };
