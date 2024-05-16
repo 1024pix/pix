27994f9eebee7e43a6526b2c2254a7efc29b96e9
Eric Lim <eric.lim@zenika.com>
2024-05-16 09:41:55 +0200

    feat(mon-pix): map new fields return with oidc provider error
    for identity provider code and id token
    Co-authored-by: LEGO Technix <109212476+lego-technix@users.noreply.github.com>
    Co-authored-by: Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>
    Co-authored-by: Marianne Bost <marianne.bost@pix.fr>

--- mon-pix/app/routes/error.js
+++ mon-pix/app/routes/error.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 import get from 'lodash/get';
+import ENV from 'mon-pix/config/environment';
 import { ApplicationError } from 'mon-pix/errors/application-error';
 import JSONApiError from 'mon-pix/errors/json-api-error';
 
@@ -27,7 +28,11 @@ export default class ErrorRoute extends Route {
       controller.errorMessage = error.message;
       controller.errorStatus = error.status;
       controller.errorTitle = error.title;
+      if (error.code === 'USER_INFO_MISSING_FIELDS') {
+        const { identityProviderCode, idToken } = error.meta;
 
+        controller.disconnectAndRetryUrl = `${ENV.APP.API_HOST}/api/oidc/unauthenticated-redirect-logout-url?identity_provider=${identityProviderCode}&id_token_hint=${idToken}`;
+      }
       if (error.shortCode) {
         controller.errorStatus += ` (${error.shortCode})`;
       }
