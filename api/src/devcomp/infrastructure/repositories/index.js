e84c25537d612eacfa4f09e848884beed86e6115
Rébecca Kaci <rebecca.kaci@gmail.com>
2024-05-17 16:58:40 +0200

    feat(api): use elementRepository in verifyAndSaveAnswer usecase
    Co-authored-by: dlahaye <dimitrilahaye.dev@gmail.com>

--- api/src/devcomp/infrastructure/repositories/index.js
+++ api/src/devcomp/infrastructure/repositories/index.js
@@ -1,6 +1,7 @@
 import { injectDependencies } from '../../../shared/infrastructure/utils/dependency-injection.js';
 import moduleDatasource from '../datasources/learning-content/module-datasource.js';
 import * as elementAnswerRepository from './element-answer-repository.js';
+import * as elementRepository from './element-repository.js';
 import * as moduleRepository from './module-repository.js';
 import * as passageRepository from './passage-repository.js';
 import * as trainingRepository from './training-repository.js';
@@ -12,6 +13,7 @@ import * as userSavedTutorialRepository from './user-saved-tutorial-repository.j
 
 const repositoriesWithoutInjectedDependencies = {
   elementAnswerRepository,
+  elementRepository,
   moduleRepository,
   passageRepository,
   trainingRepository,
