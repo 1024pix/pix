092f6fbf2b52e8bf73d90d7c49141909a2ed1289
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2024-05-17 12:18:38 +0200

    refactor(api): clearer redisUrl variable name
    wrt the REDIS_URL environment variable.

--- api/lib/infrastructure/temporary-storage/index.js
+++ api/lib/infrastructure/temporary-storage/index.js
@@ -1,12 +1,13 @@
 import { config } from '../../config.js';
-const REDIS_URL = config.temporaryStorage.redisUrl;
+
+const redisUrl = config.temporaryStorage.redisUrl;
 
 import { InMemoryTemporaryStorage } from './InMemoryTemporaryStorage.js';
 import { RedisTemporaryStorage } from './RedisTemporaryStorage.js';
 
 function _createTemporaryStorage() {
-  if (REDIS_URL) {
-    return new RedisTemporaryStorage(REDIS_URL);
+  if (redisUrl) {
+    return new RedisTemporaryStorage(redisUrl);
   } else {
     return new InMemoryTemporaryStorage();
   }
