8c062de912a4581290a6d4dd64a54621cd31d66a
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2024-05-22 17:13:35 +0200

    feat(api): add method on feature repository to list feature available on given organization

---
+++ api/src/organizational-entities/domain/models/OrganizationFeatureItem.js
@@ -0,0 +1,6 @@
+export class OrganizationFeatureItem {
+  constructor({ key, params }) {
+    this.name = key;
+    this.params = params;
+  }
+}
--- api/src/organizational-entities/infrastructure/repositories/organization-feature-repository.js
+++ api/src/organizational-entities/infrastructure/repositories/organization-feature-repository.js
@@ -1,13 +1,16 @@
 /**
  * @module OrganizationFeatureRepository
  */
-
 import { knex } from '../../../../db/knex-database-connection.js';
 import * as knexUtils from '../../../../src/shared/infrastructure/utils/knex-utils.js';
 import { AlreadyExistingOrganizationFeatureError, FeatureNotFound, OrganizationNotFound } from '../../domain/errors.js';
+import { OrganizationFeatureItem } from '../../domain/models/OrganizationFeatureItem.js';
 
 /**
  * @typedef {import('../../domain/models/OrganizationFeature.js').OrganizationFeature} OrganizationFeature
+ */
+/**
+ * @typedef {import('../../domain/models/OrganizationFeatureItem.js').OrganizationFeatureItem} OrganizationFeatureItem
  */
 
 const DEFAULT_BATCH_SIZE = 100;
@@ -34,4 +37,27 @@ async function saveInBatch(organizationFeatures, batchSize = DEFAULT_BATCH_SIZE)
   }
 }
 
-export { saveInBatch };
+/**
+ * @typedef GetOrganizationFeaturePayload
+ * @type {object}
+ * @property {number} organizationId
+ */
+
+/**
+ * @function
+ * @name findAllOrganizationFeaturesFromOrganizationId
+ *
+ * @param {GetOrganizationFeaturePayload} payload
+ * @returns {Promise<OrganizationFeatureItem>}
+ */
+async function findAllOrganizationFeaturesFromOrganizationId({ organizationId }) {
+  const organizationFeatures = await knex
+    .select('key', 'params')
+    .from('organization-features')
+    .join('features', 'features.id', 'organization-features.featureId')
+    .where({ organizationId });
+
+  return organizationFeatures.map((organizationFeature) => new OrganizationFeatureItem(organizationFeature));
+}
+
+export { findAllOrganizationFeaturesFromOrganizationId, saveInBatch };
