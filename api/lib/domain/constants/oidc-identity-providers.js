29653a83a1ff4388793928c692c215f81096a307
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2024-05-22 11:28:47 +0200

    refactor(api): delete now unused OIDC SSO constants
    Co-authored-by: Ismael Gorissen <ismael.gorissen@gmail.com>
    Co-authored-by: LEGO Technix <109212476+lego-technix@users.noreply.github.com>
    Co-authored-by: Eric Lim <eric.lim@pix.fr>
    Co-authored-by: Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>

--- api/lib/domain/constants/oidc-identity-providers.js
+++ api/lib/domain/constants/oidc-identity-providers.js
@@ -2,29 +2,5 @@ const POLE_EMPLOI = {
   code: 'POLE_EMPLOI',
   configKey: 'poleEmploi',
 };
-const CNAV = {
-  code: 'CNAV',
-  configKey: 'cnav',
-};
-const FWB = {
-  code: 'FWB',
-  configKey: 'fwb',
-};
-const GOOGLE = {
-  code: 'GOOGLE',
-  configKey: 'google',
-};
-const PAYSDELALOIRE = {
-  code: 'PAYSDELALOIRE',
-  configKey: 'paysdelaloire',
-};
-const PROSANTECONNECT = {
-  code: 'PROSANTECONNECT',
-  configKey: 'proSanteConnect',
-};
 
-function getValidOidcProviderCodes() {
-  return [POLE_EMPLOI.code, CNAV.code, FWB.code, GOOGLE.code, PAYSDELALOIRE.code, PROSANTECONNECT.code];
-}
-
-export { CNAV, FWB, getValidOidcProviderCodes, GOOGLE, PAYSDELALOIRE, POLE_EMPLOI, PROSANTECONNECT };
+export { POLE_EMPLOI };
