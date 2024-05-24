6deadb2618b26dc2ce86acf123e15b4c1476ea68
Quentin Lebouc <quentin.lebouc@pix.fr>
2024-05-24 15:29:27 +0200

    feat(mon-pix): add banner text to speech activation button

--- mon-pix/app/components/assessment-banner.js
+++ mon-pix/app/components/assessment-banner.js
@@ -1,9 +1,27 @@
 import { action } from '@ember/object';
+import { service } from '@ember/service';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 
 export default class AssessmentBanner extends Component {
+  @service intl;
+  @service featureToggles;
+
   @tracked showClosingModal = false;
+
+  get textToSpeechIcon() {
+    return this.args.isTextToSpeechActivated ? 'volume-high' : 'volume-xmark';
+  }
+
+  get textToSpeechTooltipText() {
+    return this.args.isTextToSpeechActivated
+      ? this.intl.t('pages.challenge.statement.text-to-speech.deactivate')
+      : this.intl.t('pages.challenge.statement.text-to-speech.activate');
+  }
+
+  get showTextToSpeechActivationButton() {
+    return this.featureToggles.featureToggles?.isTextToSpeechButtonEnabled;
+  }
 
   @action toggleClosingModal() {
     this.showClosingModal = !this.showClosingModal;
