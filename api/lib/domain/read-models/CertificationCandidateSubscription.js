becaafc1115b3e71267145106eb6baf7c5ad8841
Alexandre COIN <alexandre@macbook-pro-de-alexandre.home>
2024-05-29 15:40:35 +0200

    :sparkles: api: get session version for candidate subscription

--- api/lib/domain/read-models/CertificationCandidateSubscription.js
+++ api/lib/domain/read-models/CertificationCandidateSubscription.js
@@ -1,9 +1,10 @@
 class CertificationCandidateSubscription {
-  constructor({ id, sessionId, eligibleSubscription, nonEligibleSubscription }) {
+  constructor({ id, sessionId, eligibleSubscription, nonEligibleSubscription, sessionVersion }) {
     this.id = id;
     this.sessionId = sessionId;
     this.eligibleSubscription = eligibleSubscription;
     this.nonEligibleSubscription = nonEligibleSubscription;
+    this.sessionVersion = sessionVersion;
   }
 }
 
