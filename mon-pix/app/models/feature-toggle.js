dc9dad043e327c1e986ce60e44344f0eabfea207
P-Jeremy <jemyplu@gmail.com>
2024-05-30 14:48:35 +0200

    :sparkles: mon-pix: add feature toggle

--- mon-pix/app/models/feature-toggle.js
+++ mon-pix/app/models/feature-toggle.js
@@ -1,5 +1,6 @@
 import Model, { attr } from '@ember-data/model';
 
 export default class FeatureToggle extends Model {
+  @attr areV3InfoScreensEnabled;
   @attr('boolean') isTextToSpeechButtonEnabled;
 }
