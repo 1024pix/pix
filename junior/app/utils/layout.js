ef9af2c1fecb56221910eb5b6c35ba3946c3a109
Yaelle YAMA <yaelle.marsiac@pix.fr>
2024-05-30 16:34:29 +0200

    feat(junior): apply new color and fix some problems

--- junior/app/utils/layout.js
+++ junior/app/utils/layout.js
@@ -2,7 +2,8 @@ const bodyStyle = document.querySelector('body').style;
 export const styleToolkit = {
   backgroundBlob: {
     apply: (imageBlobUrl) => {
-      bodyStyle.backgroundImage = `url('${imageBlobUrl}'), linear-gradient(74.05deg, rgb(255 188 87 / 6%) 12.69%, rgb(61 104 255 / 6%) 75.92%), linear-gradient(0deg, #FFFFFF, #FFFFFF)`;
+      bodyStyle.backgroundColor = 'rgb(var(--pix-primary-50) 0.2)';
+      bodyStyle.backgroundImage = `url('${imageBlobUrl}')`;
       bodyStyle.backgroundPositionX = 'right';
       bodyStyle.backgroundPositionY = 'bottom';
       bodyStyle.backgroundRepeat = 'no-repeat';
