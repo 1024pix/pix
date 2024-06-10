b904d4ec86afa7548700ce25546eaaa44e879208
Benjamin Petetot <bpetetot@gmail.com>
2024-06-10 15:37:20 +0200

    feat(api): add logEvents repository

--- api/lib/infrastructure/repositories/audit-logger-repository.js
+++ api/lib/infrastructure/repositories/audit-logger-repository.js
@@ -5,6 +5,17 @@ import { httpAgent } from '../http/http-agent.js';
 const { auditLogger } = config;
 const basicAuthorizationToken = btoa(`pix-api:${auditLogger.clientSecret}`);
 
+/**
+ * Log an event
+ *
+ * @param {Object} event Event to log
+ * @param {string} event.targetUserId User id which is concerned by the event
+ * @param {string} event.userId User id which is logging the event
+ * @param {Date} event.occurredAt Date of the event
+ * @param {AuditLogAction} event.action Action type of the event
+ * @param {AuditLogRole} event.role Role of the event
+ * @param {AuditLogClient} event.client Client system of the event
+ */
 const logEvent = async function (event) {
   const url = `${auditLogger.baseUrl}/api/audit-logs`;
   const headers = {
@@ -18,6 +29,21 @@ const logEvent = async function (event) {
   }
 };
 
-const auditLoggerRepository = { logEvent };
+/**
+ * Log multiple events
+ *
+ * @param {Object[]} events Event to log
+ * @param {string} event[].targetUserId User id which is concerned by the event
+ * @param {string} event[].userId User id which is logging the event
+ * @param {Date} events[].occurredAt Date of the event
+ * @param {AuditLogAction} events[].action Action type of the event
+ * @param {AuditLogRole} events[].role Role of the event
+ * @param {AuditLogClient} events[].client Client system of the event
+ */
+const logEvents = async function (events) {
+  await logEvent(events);
+};
+
+const auditLoggerRepository = { logEvent, logEvents };
 
 export { auditLoggerRepository };
--- api/tests/unit/infrastructure/repositories/audit-logger-repository_test.js
+++ api/tests/unit/infrastructure/repositories/audit-logger-repository_test.js
@@ -68,4 +68,75 @@ describe('Unit | Infrastructure | Repositories | audit-logger-repository', funct
       });
     });
   });
+
+  describe('#logEvents', function () {
+    context('success', function () {
+      it('logs multiple events', async function () {
+        // given
+        const payload = [
+          {
+            userId: 1,
+            targetUserId: 2,
+            action: 'ANONYMIZATION',
+            occurredAt: new Date(),
+            role: 'SUPPORT',
+            client: 'PIX_ADMIN',
+          },
+          {
+            userId: 1,
+            targetUserId: 2,
+            action: 'ANONYMIZATION',
+            occurredAt: new Date(),
+            role: 'SUPPORT',
+            client: 'PIX_ADMIN',
+          },
+        ];
+        const url = `${auditLogger.baseUrl}/api/audit-logs`;
+        const headers = {
+          Authorization: `Basic ${btoa(`pix-api:${auditLogger.clientSecret}`)}`,
+        };
+        sinon.stub(httpAgent, 'post').resolves({ isSuccessful: true, code: 204 });
+
+        // when
+        await auditLoggerRepository.logEvents(payload);
+
+        // then
+        expect(httpAgent.post).to.have.been.calledWithExactly({ url, payload, headers });
+      });
+    });
+
+    context('failure', function () {
+      context('when response is not successful', function () {
+        it('throws an error', async function () {
+          // given
+          const payload = [
+            {
+              userId: 1,
+              targetUserId: 2,
+              action: 'ANONYMIZATION',
+              occurredAt: new Date(),
+              role: 'SUPPORT',
+              client: 'PIX_ADMIN',
+            },
+            {
+              userId: 1,
+              targetUserId: 2,
+              action: 'ANONYMIZATION',
+              occurredAt: new Date(),
+              role: 'SUPPORT',
+              client: 'PIX_ADMIN',
+            },
+          ];
+          sinon.stub(httpAgent, 'post').resolves({ code: 400, isSuccessful: false });
+
+          // when
+          const error = await catchErr(auditLoggerRepository.logEvent)(payload);
+
+          // then
+          expect(error).to.be.instanceOf(AuditLoggerApiError);
+          expect(error.message).to.equal('Pix Audit Logger Api answered with status 400');
+        });
+      });
+    });
+  });
 });
