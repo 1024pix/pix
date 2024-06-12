6bead7fe00aa059e24f513e4b1e9b76db3b4ddb7
Yaelle YAMA <yaelle.marsiac@pix.fr>
2024-06-12 12:09:10 +0200

    feat(junior): Fix design issues

--- junior/app/utils/layout.js
+++ junior/app/utils/layout.js
@@ -3,20 +3,24 @@ const bodyStyle = document.querySelector('body').style;
 export const styleToolkit = {
   backgroundBlob: {
     apply: (imageBlobUrl) => {
+      const size = window.innerWidth;
       bodyStyle.backgroundColor = 'rgb(var(--pix-primary-50) 0.2)';
       bodyStyle.backgroundImage = `url('${imageBlobUrl}')`;
       bodyStyle.backgroundPositionX = 'right';
       bodyStyle.backgroundPositionY = 'bottom';
       bodyStyle.backgroundRepeat = 'no-repeat';
+      bodyStyle.backgroundSize = '50%';
       bodyStyle.backgroundAttachment = 'fixed';
-      bodyStyle.minHeight = '100dvh';
+      if (size > 1920) {
+        bodyStyle.backgroundSize = '40%';
+      }
     },
     reset: () => {
       bodyStyle.backgroundImage = '';
       bodyStyle.backgroundPositionX = '';
       bodyStyle.backgroundPositionY = '';
       bodyStyle.backgroundRepeat = '';
-      bodyStyle.minHeight = '';
+      bodyStyle.backgroundSize = '';
       bodyStyle.backgroundAttachment = '';
     },
   },
