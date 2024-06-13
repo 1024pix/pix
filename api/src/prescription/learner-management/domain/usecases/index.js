91593e001bf6442473ae762383507a590da749fa
LionelB <lionel@lumographe.fr>
2024-06-13 16:48:57 +0200

    feat(api): add reconcileCommonOrganizationLearner usecase
    Co-authored-by: Lionel <160320+lionelB@users.noreply.github.com>

--- api/src/prescription/learner-management/domain/usecases/index.js
+++ api/src/prescription/learner-management/domain/usecases/index.js
@@ -1,7 +1,9 @@
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
+import * as campaignRepository from '../../../../../lib/infrastructure/repositories/campaign-repository.js';
 import * as membershipRepository from '../../../../../lib/infrastructure/repositories/membership-repository.js';
+import * as organizationFeatureApi from '../../../../organizational-entities/application/api/organization-features-api.js';
 import * as organizationRepository from '../../../../shared/infrastructure/repositories/organization-repository.js';
 import { injectDependencies } from '../../../../shared/infrastructure/utils/dependency-injection.js';
 import { importNamedExportsFromDirectory } from '../../../../shared/infrastructure/utils/import-named-exports-from-directory.js';
@@ -11,8 +13,8 @@ import * as organizationLearnerImportFormatRepository from '../../infrastructure
 import * as organizationLearnerRepository from '../../infrastructure/repositories/organization-learner-repository.js';
 import * as supOrganizationLearnerRepository from '../../infrastructure/repositories/sup-organization-learner-repository.js';
 import { importStorage } from '../../infrastructure/storage/import-storage.js';
-
 const dependencies = {
+  campaignRepository,
   campaignParticipationRepository,
   importStorage,
   membershipRepository,
@@ -21,6 +23,7 @@ const dependencies = {
   organizationRepository,
   organizationImportRepository,
   supOrganizationLearnerRepository,
+  organizationFeatureApi,
 };
 
 const path = dirname(fileURLToPath(import.meta.url));
---
+++ api/tests/prescription/learner-management/unit/domain/usecases/reconcile-common-organization-learner_test.js
@@ -0,0 +1,219 @@
+import { ReconcileCommonOrganizationLearnerError } from '../../../../../../src/prescription/learner-management/domain/errors.js';
+import { CommonOrganizationLearner } from '../../../../../../src/prescription/learner-management/domain/models/CommonOrganizationLearner.js';
+import { reconcileCommonOrganizationLearner } from '../../../../../../src/prescription/learner-management/domain/usecases/reconcile-common-organization-learner.js';
+import { catchErr, expect, sinon } from '../../../../../test-helper.js';
+
+describe('Unit | UseCase | reconcile-common-organization-learner', function () {
+  let campaignCode,
+    organizationId,
+    importFormat,
+    userId,
+    reconcileInfos,
+    reconcilePayload,
+    campaignRepository,
+    organizationFeatureApi,
+    organizationLearnerImportFormatRepository,
+    organizationLearnerRepository;
+
+  beforeEach(function () {
+    campaignCode = 'PIX123456';
+    organizationId = 'organization id';
+    userId = Symbol('userId');
+    reconcileInfos = Symbol('reconcile infos');
+    reconcilePayload = Symbol('reconcile payload');
+    importFormat = { transformReconciliationData: sinon.stub().withArgs(reconcileInfos).returns(reconcilePayload) };
+    campaignRepository = {
+      getByCode: sinon.stub(),
+    };
+
+    organizationFeatureApi = {
+      getAllFeaturesFromOrganization: sinon.stub(),
+    };
+
+    organizationLearnerImportFormatRepository = {
+      get: sinon.stub(),
+    };
+
+    organizationLearnerRepository = {
+      findAllCommonOrganizationLearnerByReconciliationInfos: sinon.stub(),
+      update: sinon.stub(),
+    };
+  });
+
+  context('when there is no matching campaign code', function () {
+    it('should throw a ReconcileCommonOrganizationLearnerError', async function () {
+      // given
+      campaignRepository.getByCode.resolves(null);
+
+      // when
+      const error = await catchErr(reconcileCommonOrganizationLearner)({
+        campaignCode,
+        userId,
+        reconcileInfos,
+        campaignRepository,
+        organizationFeatureApi,
+        organizationLearnerImportFormatRepository,
+        organizationLearnerRepository,
+      });
+
+      //then
+      expect(error).to.be.an.instanceOf(ReconcileCommonOrganizationLearnerError);
+      expect(error.reason).to.equal('CAMPAIGN_NOT_FOUND');
+    });
+  });
+
+  context('when there is no import feature on organization', function () {
+    it('should throw a ReconcileCommonOrganizationLearnerError', async function () {
+      // given
+      campaignRepository.getByCode.withArgs(campaignCode).resolves({ organizationId });
+      organizationFeatureApi.getAllFeaturesFromOrganization.withArgs(organizationId).resolves({});
+
+      //when
+      const error = await catchErr(reconcileCommonOrganizationLearner)({
+        campaignCode,
+        userId,
+        reconcileInfos,
+        campaignRepository,
+        organizationFeatureApi,
+        organizationLearnerImportFormatRepository,
+        organizationLearnerRepository,
+      });
+
+      //then
+      expect(error).to.be.an.instanceOf(ReconcileCommonOrganizationLearnerError);
+      expect(error.reason).to.equal('MISSING_IMPORT_FEATURE');
+    });
+  });
+
+  context('when there is no import format for organization', function () {
+    it('should throw a ReconcileCommonOrganizationLearnerError', async function () {
+      // given
+      campaignRepository.getByCode.withArgs(campaignCode).resolves({ organizationId });
+      organizationFeatureApi.getAllFeaturesFromOrganization
+        .withArgs(organizationId)
+        .resolves({ hasLeanersImportFeature: true });
+
+      //when
+      const error = await catchErr(reconcileCommonOrganizationLearner)({
+        campaignCode,
+        userId,
+        reconcileInfos,
+        campaignRepository,
+        organizationFeatureApi,
+        organizationLearnerImportFormatRepository,
+        organizationLearnerRepository,
+      });
+
+      //then
+      expect(error).to.be.an.instanceOf(ReconcileCommonOrganizationLearnerError);
+      expect(error.reason).to.equal('IMPORT_FORMAT_NOT_FOUND');
+    });
+  });
+
+  context('when there is no matching learner', function () {
+    it('should throw a ReconcileCommonOrganizationLearnerError', async function () {
+      // given
+      campaignRepository.getByCode.withArgs(campaignCode).resolves({ organizationId });
+      organizationFeatureApi.getAllFeaturesFromOrganization
+        .withArgs(organizationId)
+        .resolves({ hasLeanersImportFeature: true });
+      organizationLearnerImportFormatRepository.get.withArgs(organizationId).resolves(importFormat);
+      organizationLearnerRepository.findAllCommonOrganizationLearnerByReconciliationInfos
+        .withArgs({
+          organizationId,
+          reconciliationInformations: reconcilePayload,
+        })
+        .resolves([]);
+
+      //when
+      const error = await catchErr(reconcileCommonOrganizationLearner)({
+        campaignCode,
+        userId,
+        reconcileInfos,
+        campaignRepository,
+        organizationFeatureApi,
+        organizationLearnerImportFormatRepository,
+        organizationLearnerRepository,
+      });
+
+      //then
+      expect(error).to.be.an.instanceOf(ReconcileCommonOrganizationLearnerError);
+      expect(error.reason).to.equal('NO_MATCH');
+    });
+  });
+
+  context('when there is multiple matching learners', function () {
+    it('should throw a ReconcileCommonOrganizationLearnerError', async function () {
+      // given
+      campaignRepository.getByCode.withArgs(campaignCode).resolves({ organizationId });
+      organizationFeatureApi.getAllFeaturesFromOrganization
+        .withArgs(organizationId)
+        .resolves({ hasLeanersImportFeature: true });
+      organizationLearnerImportFormatRepository.get.withArgs(organizationId).resolves(importFormat);
+      organizationLearnerRepository.findAllCommonOrganizationLearnerByReconciliationInfos
+        .withArgs({
+          organizationId,
+          reconciliationInformations: reconcilePayload,
+        })
+        .resolves([
+          { id: 1, firstName: 'Uno', lastName: 'Bono' },
+          { id: 2, firstName: 'Due', lastName: 'Bono' },
+        ]);
+
+      //when
+      const error = await catchErr(reconcileCommonOrganizationLearner)({
+        campaignCode,
+        userId,
+        reconcileInfos,
+        campaignRepository,
+        organizationFeatureApi,
+        organizationLearnerImportFormatRepository,
+        organizationLearnerRepository,
+      });
+
+      //then
+      expect(error).to.be.an.instanceOf(ReconcileCommonOrganizationLearnerError);
+      expect(error.reason).to.equal('MULTIPLE_MATCHES');
+    });
+  });
+
+  context('when reconciliation works', function () {
+    it('should return nothing', async function () {
+      // given
+      const learner = new CommonOrganizationLearner({
+        firstName: 'Amanda',
+        lastName: 'Rine',
+        id: 1,
+        organizationId,
+        info: 'infos',
+      });
+      campaignRepository.getByCode.withArgs(campaignCode).resolves({ organizationId });
+      organizationFeatureApi.getAllFeaturesFromOrganization
+        .withArgs(organizationId)
+        .resolves({ hasLeanersImportFeature: true });
+      organizationLearnerImportFormatRepository.get.withArgs(organizationId).resolves(importFormat);
+      organizationLearnerRepository.findAllCommonOrganizationLearnerByReconciliationInfos
+        .withArgs({
+          organizationId,
+          reconciliationInformations: reconcilePayload,
+        })
+        .resolves([learner]);
+      organizationLearnerRepository.update.withArgs(learner).resolves();
+
+      //when
+      const result = await reconcileCommonOrganizationLearner({
+        campaignCode,
+        userId,
+        reconcileInfos,
+        campaignRepository,
+        organizationFeatureApi,
+        organizationLearnerImportFormatRepository,
+        organizationLearnerRepository,
+      });
+
+      //then
+      expect(learner.userId).to.equal(userId);
+      expect(result).to.be.undefined;
+    });
+  });
+});
