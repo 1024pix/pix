b411b396147bff57fcb810aa35669422d98fb90d
matthieu.gros <matthieu.gros@octo.com>
2024-06-14 16:05:57 +0200

    :recycle: api: addNonEnrolledCandidatesToSession repository method refactoring

--- api/lib/infrastructure/repositories/sco-certification-candidate-repository.js
+++ api/lib/infrastructure/repositories/sco-certification-candidate-repository.js
@@ -18,11 +18,7 @@ const addNonEnrolledCandidatesToSession = async function ({ sessionId, scoCertif
     .filter((candidate) => !alreadyEnrolledCandidateOrganizationLearnerIds.includes(candidate.organizationLearnerId))
     .map(scoCandidateToDTO);
 
-  const addedCandidateIds = await knex
-    .batchInsert('certification-candidates', candidatesToBeEnrolledDTOs)
-    .returning(knex.raw('id as "certificationCandidateId", \'CORE\' as type'));
-
-  await knex.batchInsert('certification-subscriptions', addedCandidateIds);
+  await _batchInsertCandidatesAndCoreSubscriptions(candidatesToBeEnrolledDTOs);
 };
 
 export { addNonEnrolledCandidatesToSession };
@@ -41,4 +37,13 @@ function _scoCandidateToDTOForSession(sessionId) {
       sessionId,
     };
   };
+}
+
+async function _batchInsertCandidatesAndCoreSubscriptions(candidatesToBeEnrolledDTOs) {
+  await knex
+    .batchInsert('certification-candidates', candidatesToBeEnrolledDTOs)
+    .returning([knex.raw('id as "certificationCandidateId"'), knex.raw("'CORE' as type")])
+    .then(async function (coreSubscriptions) {
+      await knex.batchInsert('certification-subscriptions', coreSubscriptions);
+    });
 }
