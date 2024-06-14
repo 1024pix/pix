85601c3c990b674632d3e0c21f55e8ecafc0ee1a
Yaelle YAMA <yaelle.marsiac@pix.fr>
2024-06-14 12:13:46 +0200

    feat(junior): Add a frame around media

--- junior/app/pods/components/challenge/item/component.js
+++ junior/app/pods/components/challenge/item/component.js
@@ -2,10 +2,10 @@ import Component from '@glimmer/component';
 
 export default class Item extends Component {
   get isMediaWithForm() {
-    return this.args.challenge.hasForm && this.#hasMedia;
+    return this.args.challenge.hasForm && this.hasMedia;
   }
 
-  get #hasMedia() {
+  get hasMedia() {
     return this.args.challenge.illustrationUrl || this.args.challenge.hasValidEmbedDocument;
   }
 }
