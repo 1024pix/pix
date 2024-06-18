9d6ff94234d190ede83b40ab9ae89f156d5d741d
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2024-06-18 09:23:59 +0200

    chore(orga): add comment on why calling currentUser.load in authenticated model

--- orga/app/routes/authenticated.js
+++ orga/app/routes/authenticated.js
@@ -32,6 +32,8 @@ export default class AuthenticatedRoute extends Route {
       });
     }
     if (this.currentUser.canAccessMissionsPage) {
+      // when user can access mission page, we force load currentUser
+      // so the page is re rendered and session status can be live updated
       await this.currentUser.load();
     }
   }
