8db769138f72f0b0aef4b545a0ea631f7e6abc16
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2024-06-18 10:29:10 +0200

    fix(orga): fix display places value

--- orga/app/routes/authenticated.js
+++ orga/app/routes/authenticated.js
@@ -2,7 +2,6 @@ import { action } from '@ember/object';
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 import get from 'lodash/get';
-import RSVP from 'rsvp';
 
 export default class AuthenticatedRoute extends Route {
   @service currentUser;
@@ -25,12 +24,10 @@ export default class AuthenticatedRoute extends Route {
 
   async model() {
     if (this.currentUser.prescriber.placesManagement) {
-      return RSVP.hash({
-        available: this.store.queryRecord('organization-place-statistic', {
-          organizationId: this.currentUser.organization.id,
-        }),
+      return this.store.queryRecord('organization-place-statistic', {
+        organizationId: this.currentUser.organization.id,
       });
-    }
+    } else return null;
   }
 
   @action
