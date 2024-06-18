886e60e0e6747ff73804a92e5c529ac45727a7e1
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2024-06-18 15:38:11 +0200

    refactor(api): migrate route controller

--- api/src/prescription/organization-learner/routes.js
+++ api/src/prescription/organization-learner/routes.js
@@ -1,8 +1,15 @@
 import * as learnerActivityRoute from './application/learner-activity-route.js';
 import * as learnerListRoute from './application/learner-list-route.js';
+import * as registrationOrganizationLearnerRoutes from './application/registration-organization-learner-route.js';
 import * as scoLearnerListRoute from './application/sco-learner-list-route.js';
 import * as supLearnerListRoute from './application/sup-learner-list-route.js';
 
-const organizationLearnerRoutes = [supLearnerListRoute, scoLearnerListRoute, learnerListRoute, learnerActivityRoute];
+const organizationLearnerRoutes = [
+  scoLearnerListRoute,
+  learnerListRoute,
+  learnerActivityRoute,
+  registrationOrganizationLearnerRoutes,
+  supLearnerListRoute,
+];
 
 export { organizationLearnerRoutes };
