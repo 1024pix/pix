b82f45c4371ff3feac4eeae106c6c5f1bb5ba162
Ismael Gorissen <ismael.gorissen@gmail.com>
2024-06-19 10:08:49 +0200

    refactor(api): move controller method
    sendEmailForAccountRecovery

--- api/src/identity-access-management/application/account-recovery/account-recovery.controller.js
+++ api/src/identity-access-management/application/account-recovery/account-recovery.controller.js
@@ -12,6 +12,20 @@ const checkAccountRecoveryDemand = async function (
   return dependencies.studentInformationForAccountRecoverySerializer.serializeAccountRecovery(studentInformation);
 };
 
+const sendEmailForAccountRecovery = async function (
+  request,
+  h,
+  dependencies = { studentInformationForAccountRecoverySerializer },
+) {
+  const studentInformation = await dependencies.studentInformationForAccountRecoverySerializer.deserialize(
+    request.payload,
+  );
+
+  await usecases.sendEmailForAccountRecovery({ studentInformation });
+
+  return h.response().code(204);
+};
+
 const updateUserAccountFromRecoveryDemand = async function (request, h) {
   const temporaryKey = request.payload.data.attributes['temporary-key'];
   const password = request.payload.data.attributes.password;
@@ -29,5 +43,6 @@ const updateUserAccountFromRecoveryDemand = async function (request, h) {
 
 export const accountRecoveryController = {
   checkAccountRecoveryDemand,
+  sendEmailForAccountRecovery,
   updateUserAccountFromRecoveryDemand,
 };
