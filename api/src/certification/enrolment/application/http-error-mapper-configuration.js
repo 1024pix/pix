7e74ef84dd795cd1e0bfd16a83acda1b5f02349d
P-Jeremy <jemyplu@gmail.com>
2024-06-20 14:55:57 +0200

    :sparkles: api: add new error on certification candidate not found

--- api/src/certification/enrolment/application/http-error-mapper-configuration.js
+++ api/src/certification/enrolment/application/http-error-mapper-configuration.js
@@ -1,11 +1,19 @@
 import { HttpErrors } from '../../../shared/application/http-errors.js';
 import { DomainErrorMappingConfiguration } from '../../../shared/application/models/domain-error-mapping-configuration.js';
-import { CertificationCandidateForbiddenDeletionError, SessionStartedDeletionError } from '../domain/errors.js';
+import {
+  CertificationCandidateForbiddenDeletionError,
+  CertificationCandidateNotFoundError,
+  SessionStartedDeletionError,
+} from '../domain/errors.js';
 
 const enrolmentDomainErrorMappingConfiguration = [
   {
     name: CertificationCandidateForbiddenDeletionError.name,
     httpErrorFn: (error) => new HttpErrors.ForbiddenError(error.message, error.code),
+  },
+  {
+    name: CertificationCandidateNotFoundError.name,
+    httpErrorFn: (error) => new HttpErrors.NotFoundError(error.message),
   },
   { name: SessionStartedDeletionError.name, httpErrorFn: (error) => new HttpErrors.ConflictError(error.message) },
 ].map((domainErrorMappingConfiguration) => new DomainErrorMappingConfiguration(domainErrorMappingConfiguration));
