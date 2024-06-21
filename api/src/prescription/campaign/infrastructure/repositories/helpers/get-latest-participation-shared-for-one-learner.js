27350b8954bf0564ad8c07336ab9573879bc22be
Yvonnick Frin <frin.yvonnick@gmail.com>
2024-06-21 14:52:37 +0200

    chore(api): add jsdoc on getLatestParticipationSharedForOneLearner

--- api/src/prescription/campaign/infrastructure/repositories/helpers/get-latest-participation-shared-for-one-learner.js
+++ api/src/prescription/campaign/infrastructure/repositories/helpers/get-latest-participation-shared-for-one-learner.js
@@ -1,8 +1,21 @@
+/**
+ * @function
+ * @name getLatestParticipationSharedForOneLearner
+ * @param {Knex.QueryBuilder} queryBuilder
+ * @param {string} columnName
+ * @param {number} campaignId
+ * @description
+ * This function is meant to be use as a subquery on the table campaign-participations aliased as "cp"
+ * to get latest campaign participation of an organization learner for given campaignId
+ *
+ * @returns {Knex.QueryBuilder}
+ */
 export const getLatestParticipationSharedForOneLearner = (queryBuilder, columnName, campaignId) =>
   queryBuilder('campaign-participations')
     .select(columnName)
     .orderBy('sharedAt', 'desc')
     .where('organizationLearnerId', queryBuilder.ref('cp.organizationLearnerId'))
     .where({ campaignId })
+    .whereNotNull('sharedAt')
     .limit(1)
     .as(columnName);
