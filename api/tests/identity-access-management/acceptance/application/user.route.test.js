4e1d009fb084a8cbd94cd1ca3a8d9fd40bc0ad53
Ismael Gorissen <ismael.gorissen@gmail.com>
2024-06-22 15:02:27 +0200

    refactor(api): move route

--- api/tests/identity-access-management/acceptance/application/user.route.test.js
+++ api/tests/identity-access-management/acceptance/application/user.route.test.js
@@ -600,6 +600,41 @@ describe('Acceptance | Identity Access Management | Application | Route | User',
       });
     });
   });
+
+  describe('PATCH /api/users/{id}/has-seen-last-data-protection-policy-information', function () {
+    describe('Success case', function () {
+      let clock;
+      const now = new Date('2022-12-07');
+
+      beforeEach(async function () {
+        clock = sinon.useFakeTimers({
+          now,
+          toFake: ['Date'],
+        });
+      });
+
+      afterEach(function () {
+        clock.restore();
+      });
+
+      it('returns a response with HTTP status code 200', async function () {
+        // given
+        const user = databaseBuilder.factory.buildUser({ lastDataProtectionPolicySeenAt: null });
+        await databaseBuilder.commit();
+
+        // when
+        const response = await server.inject({
+          method: 'PATCH',
+          url: `/api/users/${user.id}/has-seen-last-data-protection-policy-information`,
+          headers: { authorization: generateValidRequestAuthorizationHeader(user.id) },
+        });
+
+        // then
+        expect(response.statusCode).to.equal(200);
+        expect(response.result.data.attributes['last-data-protection-policy-seen-at']).to.deep.equal(now);
+      });
+    });
+  });
 });
 
 function _insertPasswordResetDemand(temporaryKey, email) {
