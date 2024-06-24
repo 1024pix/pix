9d6e6aed9866e8f116a5596d44ddb80292e0b69e
Alexandre COIN <alexandre.pc.coin@gmail.com>
2024-06-24 09:32:47 +0200

    wip run capacity degradation after simulator

--- api/src/certification/flash-certification/domain/models/AssessmentSimulator.js
+++ api/src/certification/flash-certification/domain/models/AssessmentSimulator.js
@@ -4,9 +4,9 @@ export class AssessmentSimulator {
   }
 
   run(
-    { challengesAnswers = [], startCapacityDegradationAt = 32 } = {
+    { challengesAnswers = [], simulatorLoops = 32 } = {
       challengesAnswers: [],
-      startCapacityDegradationAt: 32,
+      simulatorLoops: 32,
     },
   ) {
     const result = [];
@@ -14,7 +14,7 @@ export class AssessmentSimulator {
     let stepIndex = 0;
 
     // eslint-disable-next-line no-constant-condition
-    while (this._shouldRunSimulator(startCapacityDegradationAt, stepIndex)) {
+    while (stepIndex < simulatorLoops) {
       try {
         const simulatorStepResult = this.getStrategy(stepIndex).run({ challengesAnswers, stepIndex });
 
@@ -30,9 +30,5 @@ export class AssessmentSimulator {
     }
 
     return result;
-  }
-
-  _shouldRunSimulator(startCapacityDegradationAt, stepIndex) {
-    return startCapacityDegradationAt ? stepIndex < startCapacityDegradationAt : true;
   }
 }
