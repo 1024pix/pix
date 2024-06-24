52b49666e9cee1039ed76846c6879460fe6278ae
Ismael Gorissen <ismael.gorissen@gmail.com>
2024-06-24 23:00:41 +0200

    feat(api): add getEmailValidationUrl method to url-builder

--- api/src/shared/infrastructure/utils/url-builder.js
+++ api/src/shared/infrastructure/utils/url-builder.js
@@ -13,4 +13,18 @@ function getCampaignUrl(locale, campaignCode) {
   return `${config.domain.pixApp + config.domain.tldFr}/campagnes/${campaignCode}`;
 }
 
-export { getCampaignUrl };
+/**
+ * @param {string} locale
+ * @param {string} redirectUri - URL to redirect the user to after email validation
+ * @param {string} token
+ * @returns {string} - generated URL to validate user account email
+ */
+function getEmailValidationUrl({ locale, redirectUri, token }) {
+  const baseUrl = `${config.domain.pixApp + (locale ? config.domain.tldOrg : config.domain.tldFr)}`;
+  return `${baseUrl}/api/users/validate-email?token=${token}&redirect_uri=${redirectUri}`;
+}
+
+/**
+ * @typedef UrlBuilder
+ */
+export { getCampaignUrl, getEmailValidationUrl };
--- api/tests/shared/unit/infrastructure/utils/url-builder_test.js
+++ api/tests/shared/unit/infrastructure/utils/url-builder_test.js
@@ -1,29 +1,62 @@
-import { getCampaignUrl } from '../../../../../src/shared/infrastructure/utils/url-builder.js';
+import { urlBuilder } from '../../../../../src/shared/infrastructure/utils/url-builder.js';
 import { expect } from '../../../../test-helper.js';
 
 describe('Unit | Shared | Infrastructure | Utils | url-builder', function () {
   describe('#getCampaignUrl', function () {
     it('should return null if campaignCode is not defined', function () {
-      expect(getCampaignUrl('fr', null)).to.be.null;
+      expect(urlBuilder.getCampaignUrl('fr', null)).to.be.null;
     });
 
     describe('when campaignCode is defined', function () {
       const campaignCode = 'AZERTY123';
 
       it('should return campaignUrl with fr domain when locale is fr-fr', function () {
-        expect(getCampaignUrl('fr-fr', campaignCode)).to.be.equal(`https://app.pix.fr/campagnes/${campaignCode}`);
+        expect(urlBuilder.getCampaignUrl('fr-fr', campaignCode)).to.be.equal(
+          `https://app.pix.fr/campagnes/${campaignCode}`,
+        );
       });
 
       it('should return campaignUrl with org domain when locale is fr', function () {
-        expect(getCampaignUrl('fr', campaignCode)).to.be.equal(
+        expect(urlBuilder.getCampaignUrl('fr', campaignCode)).to.be.equal(
           `https://app.pix.org/campagnes/${campaignCode}/?lang=fr`,
         );
       });
 
       it('should return campaignUrl with org domain when locale is en', function () {
-        expect(getCampaignUrl('en', campaignCode)).to.be.equal(
+        expect(urlBuilder.getCampaignUrl('en', campaignCode)).to.be.equal(
           `https://app.pix.org/campagnes/${campaignCode}/?lang=en`,
         );
+      });
+    });
+  });
+
+  describe('getEmailValidationUrl', function () {
+    context('when locale is given', function () {
+      it('returns email validation URL with domain .org', function () {
+        // given
+        const token = '00000000-0000-0000-0000-000000000000';
+        const redirectUri = 'https://app.pix.org/connexion?lang=nl';
+        const locale = 'en';
+
+        // when
+        const url = urlBuilder.getEmailValidationUrl({ locale, redirectUri, token });
+
+        // then
+        expect(url).to.equal(`https://app.pix.org/api/users/validate-email?token=${token}&redirect_uri=${redirectUri}`);
+      });
+    });
+
+    context('when locale is not given', function () {
+      it('returns email validation URL with domain .fr', function () {
+        // given
+        const token = '00000000-0000-0000-0000-000000000000';
+        const redirectUri = 'https://app.pix.fr/connexion';
+
+        // when
+        const url = urlBuilder.getEmailValidationUrl({ redirectUri, token });
+
+        // then
+        expect(url).to.equal(`https://app.pix.fr/api/users/validate-email?token=${token}&redirect_uri=${redirectUri}`);
       });
     });
   });
