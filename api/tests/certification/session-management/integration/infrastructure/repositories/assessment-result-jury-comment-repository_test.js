de202e4393a00306f88dc5526b3303e970c9b04f
Antoine Ceol <antoine.ceol@gmail.com>
2024-06-25 08:37:23 +0200

    :sparkles: api: update assessment result jury comment

--- api/tests/certification/session-management/integration/infrastructure/repositories/assessment-result-jury-comment-repository_test.js
+++ api/tests/certification/session-management/integration/infrastructure/repositories/assessment-result-jury-comment-repository_test.js
@@ -1,6 +1,6 @@
 import { AssessmentResultJuryComment } from '../../../../../../src/certification/session-management/domain/models/AssessmentResultJuryComment.js';
 import * as assessmentResultJuryCommentRepository from '../../../../../../src/certification/session-management/infrastructure/repositories/assessment-result-jury-comment-repository.js';
-import { databaseBuilder, expect } from '../../../../../test-helper.js';
+import { databaseBuilder, expect, knex } from '../../../../../test-helper.js';
 
 describe('Integration | Repository | Certification | Session-management | AssessmentResultJuryCommentRepository', function () {
   describe('#getLatestAssessmentResult', function () {
@@ -22,6 +22,32 @@ describe('Integration | Repository | Certification | Session-management | Assess
 
       // then
       expect(result).to.deepEqualInstance(new AssessmentResultJuryComment(latestAssessmentResultData));
+    });
+  });
+
+  describe('#save', function () {
+    it('should save the jury id and comment only', async function () {
+      // given
+      databaseBuilder.factory.buildUser({ id: 21 });
+      databaseBuilder.factory.buildUser({ id: 22 });
+      databaseBuilder.factory.buildCertificationCourse({ id: 55 });
+      databaseBuilder.factory.buildAssessment({ id: 51, certificationCourseId: 55 });
+      const assessmentResult = databaseBuilder.factory.buildAssessmentResult({
+        id: 11,
+        commentByJury: 'Old',
+        commentByAutoJury: 'otherComment',
+        juryId: 21,
+        assessmentId: 51,
+      });
+      await databaseBuilder.commit();
+
+      // when
+      const latestAssessmentResultData = { id: 11, commentByJury: 'New', juryId: 22 };
+      await assessmentResultJuryCommentRepository.update(latestAssessmentResultData);
+
+      // then
+      const expected = await knex('assessment-results').select('*').where({ id: 11 }).first();
+      expect({ ...assessmentResult, ...latestAssessmentResultData }).to.deep.equal(expected);
     });
   });
 });
