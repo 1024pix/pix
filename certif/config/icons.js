6004172d02100a3b1685bd4f11218d07e5e89826
Andreia Pena <andreia.pena@pix.fr>
2024-06-28 17:47:15 +0200

    ✨(certif):  add icons

---
+++ certif/config/icons.js
@@ -0,0 +1,21 @@
+module.exports = function () {
+  return {
+    'free-solid-svg-icons': [
+      'book',
+      'users',
+      'trash-can',
+      'trash-alt',
+      'bell',
+      'redo',
+      'ellipsis-vertical',
+      'user-circle',
+      'file-download',
+      'copy',
+      'cloud-arrow-up',
+      'file-arrow-down',
+      'info',
+      'clipboard-list',
+      'power-off',
+    ],
+  };
+};
