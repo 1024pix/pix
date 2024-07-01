83526956f17a8c42bb3d1b0a1d9d0c768f1ebd1c
dlahaye <dimitrilahaye.dev@gmail.com>
2024-07-01 11:24:53 +0200

    sr(api): remove unused ModuleRepository.getBySlugForVerification method
    Co-authored-by: Clément Latzarus <clement.latzarus@pix.fr>

--- api/src/devcomp/infrastructure/repositories/module-repository.js
+++ api/src/devcomp/infrastructure/repositories/module-repository.js
@@ -1,6 +1,5 @@
 import { NotFoundError } from '../../../shared/domain/errors.js';
 import { LearningContentResourceNotFound } from '../../../shared/infrastructure/datasources/learning-content/LearningContentResourceNotFound.js';
-import { Module } from '../../domain/models/module/Module.js';
 import { ModuleFactory } from '../factories/module-factory.js';
 
 async function getBySlug({ slug, moduleDatasource }) {
@@ -16,17 +15,4 @@ async function getBySlug({ slug, moduleDatasource }) {
   }
 }
 
-async function getBySlugForVerification({ slug, moduleDatasource }) {
-  try {
-    const moduleData = await moduleDatasource.getBySlug(slug);
-
-    return Module.toDomainForVerification(moduleData);
-  } catch (e) {
-    if (e instanceof LearningContentResourceNotFound) {
-      throw new NotFoundError();
-    }
-    throw e;
-  }
-}
-
-export { getBySlug, getBySlugForVerification };
+export { getBySlug };
