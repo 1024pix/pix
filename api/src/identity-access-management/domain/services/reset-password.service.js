d5d2159ab3bfd611c1f77d2840a911d7dd6bdb88
theotime2005 <theotime.berthod@epitech.digital>
2024-07-03 10:14:39 +0200

    WIP

--- api/src/identity-access-management/domain/services/reset-password.service.js
+++ api/src/identity-access-management/domain/services/reset-password.service.js
@@ -1,14 +1,17 @@
 import crypto from 'node:crypto';
+import util from 'node:util';
 
 import jsonwebtoken from 'jsonwebtoken';
 
 import { config } from '../../../shared/config.js';
 import * as passwordResetDemandRepository from '../../infrastructure/repositories/reset-password-demand.repository.js';
 
-const generateTemporaryKey = function () {
+const randomBytes = util.promisify(crypto.randomBytes);
+const generateTemporaryKey = async function () {
+  const base64RandomBytes = await randomBytes(16).toString('base64');
   return jsonwebtoken.sign(
     {
-      data: crypto.randomBytes(16).toString('base64'),
+      data: base64RandomBytes,
     },
     config.temporaryKey.secret,
     { expiresIn: config.temporaryKey.tokenLifespan },
