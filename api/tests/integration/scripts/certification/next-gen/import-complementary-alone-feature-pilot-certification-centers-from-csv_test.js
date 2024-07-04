d4d96ce9a0656166d5de1308c955ad137c560881
matthieu.gros <matthieu.gros@octo.com>
2024-07-04 15:11:48 +0200

    :sparkles: add filter to only delete centers pilots for complementary alone feature

--- api/tests/integration/scripts/certification/next-gen/import-complementary-alone-feature-pilot-certification-centers-from-csv_test.js
+++ api/tests/integration/scripts/certification/next-gen/import-complementary-alone-feature-pilot-certification-centers-from-csv_test.js
@@ -18,7 +18,6 @@ describe('Integration | Scripts | Certification | import-pilot-certification-cen
 
       const certificationCenterId1 = 1001;
       const certificationCenterId2 = 1002;
-      const certificationCenterId3 = 1003;
 
       const featureId = databaseBuilder.factory.buildFeature({
         key: CERTIFICATION_FEATURES.CAN_REGISTER_FOR_A_COMPLEMENTARY_CERTIFICATION_ALONE.key,
@@ -26,9 +25,6 @@ describe('Integration | Scripts | Certification | import-pilot-certification-cen
 
       databaseBuilder.factory.buildCertificationCenter({ id: certificationCenterId1, isV3Pilot: true });
       databaseBuilder.factory.buildCertificationCenter({ id: certificationCenterId2, isV3Pilot: true });
-      databaseBuilder.factory.buildCertificationCenter({ id: certificationCenterId3, isV3Pilot: true });
-
-      databaseBuilder.factory.buildCertificationCenterFeature({ certificationCenterId1, featureId });
 
       await databaseBuilder.commit();
 
@@ -47,7 +43,84 @@ describe('Integration | Scripts | Certification | import-pilot-certification-cen
       ]);
     });
 
-    context('when there is a V3 certification center in the csv file', function () {
+    context('when pilot certification centers are already imported', function () {
+      it('should removes all pilot certification centers tied to the feature ', async function () {
+        // given
+        const file = 'pilot-certification-centers-valid.csv';
+        const dataWithSingleCertificationCenterId = 'certification_center_id;\n1002;\n';
+        const csvFilePath = await createTempFile(file, dataWithSingleCertificationCenterId);
+
+        const certificationCenterId1 = 1001;
+        const certificationCenterId2 = 1002;
+
+        const featureId = databaseBuilder.factory.buildFeature({
+          key: CERTIFICATION_FEATURES.CAN_REGISTER_FOR_A_COMPLEMENTARY_CERTIFICATION_ALONE.key,
+        }).id;
+
+        databaseBuilder.factory.buildCertificationCenter({ id: certificationCenterId1, isV3Pilot: true });
+        databaseBuilder.factory.buildCertificationCenter({ id: certificationCenterId2, isV3Pilot: true });
+
+        databaseBuilder.factory.buildCertificationCenterFeature({ certificationCenterId1, featureId });
+
+        await databaseBuilder.commit();
+
+        // when
+        await main(csvFilePath);
+
+        // then
+        const pilotCertificationCenterList = await knex('certification-center-features').select(
+          'certificationCenterId',
+          'featureId',
+        );
+
+        expect(pilotCertificationCenterList).to.deep.equal([
+          { certificationCenterId: certificationCenterId2, featureId },
+        ]);
+      });
+
+      it('should NOT removes pilot certification centers for another feature', async function () {
+        // given
+        const file = 'pilot-certification-centers-valid.csv';
+        const data = 'certification_center_id;\n1002;\n';
+        const csvFilePath = await createTempFile(file, data);
+
+        const certificationCenterId1 = 1001;
+        const certificationCenterId2 = 1002;
+
+        const complementaryAloneFeatureId = databaseBuilder.factory.buildFeature({
+          key: CERTIFICATION_FEATURES.CAN_REGISTER_FOR_A_COMPLEMENTARY_CERTIFICATION_ALONE.key,
+        }).id;
+        const anotherFeatureId = databaseBuilder.factory.buildFeature({
+          key: 'ANOTHER_FEATURE',
+        }).id;
+
+        databaseBuilder.factory.buildCertificationCenter({ id: certificationCenterId1, isV3Pilot: true });
+        databaseBuilder.factory.buildCertificationCenter({ id: certificationCenterId2, isV3Pilot: true });
+
+        databaseBuilder.factory.buildCertificationCenterFeature({
+          certificationCenterId: certificationCenterId1,
+          featureId: anotherFeatureId,
+        });
+
+        await databaseBuilder.commit();
+
+        // when
+        await main(csvFilePath);
+
+        // then
+        const pilotCertificationCenterList = await knex('certification-center-features').select(
+          'certificationCenterId',
+          'featureId',
+        );
+
+        expect(pilotCertificationCenterList).to.deep.equal([
+          { certificationCenterId: certificationCenterId1, featureId: anotherFeatureId },
+          { certificationCenterId: certificationCenterId2, featureId: complementaryAloneFeatureId },
+        ]);
+      });
+    });
+
+    context('when there is a V2 certification center in the csv file', function () {
       it('should not import the certification center list', async function () {
         // given
         const file = 'pilot-certification-centers-invalid-v3-centers.csv';
@@ -55,23 +128,24 @@ describe('Integration | Scripts | Certification | import-pilot-certification-cen
         const csvFilePath = await createTempFile(file, data);
         const v2CertificationCenterId = 2001;
         const v3CertificationCenterId = 2002;
-        const v2CertificationCenterId2 = 2003;
 
         const featureId = databaseBuilder.factory.buildFeature({
           key: CERTIFICATION_FEATURES.CAN_REGISTER_FOR_A_COMPLEMENTARY_CERTIFICATION_ALONE.key,
         }).id;
 
-        databaseBuilder.factory.buildCertificationCenter({ id: v2CertificationCenterId, isV3Pilot: true });
-        databaseBuilder.factory.buildCertificationCenter({ id: v3CertificationCenterId, isV3Pilot: false });
-        databaseBuilder.factory.buildCertificationCenter({ id: v2CertificationCenterId2, isV3Pilot: true });
-        databaseBuilder.factory.buildCertificationCenterFeature({ v2CertificationCenterId, featureId });
+        databaseBuilder.factory.buildCertificationCenter({ id: v2CertificationCenterId, isV3Pilot: false });
+        databaseBuilder.factory.buildCertificationCenter({ id: v3CertificationCenterId, isV3Pilot: true });
+        databaseBuilder.factory.buildCertificationCenterFeature({
+          certificationCenterId: v3CertificationCenterId,
+          featureId,
+        });
         await databaseBuilder.commit();
 
         // when
         const error = await catchErr(main)(csvFilePath);
 
         // then
-        expect(error.message).to.equal('V2 certification centers : 2002 are not allowed as pilots');
+        expect(error.message).to.equal('V2 certification centers : 2001 are not allowed as pilots');
       });
     });
   });
