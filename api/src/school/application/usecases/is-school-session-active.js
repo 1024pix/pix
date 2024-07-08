025ef04ac0ad13cc643e300d6d7849c9dfffb16e
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2024-07-08 08:59:01 +0200

    refactor(api): extract method for session expiration computation

--- api/src/school/application/usecases/is-school-session-active.js
+++ api/src/school/application/usecases/is-school-session-active.js
@@ -1,9 +1,13 @@
 import * as schoolRepository from '../../infrastructure/repositories/school-repository.js';
 
 const execute = async function ({ schoolCode, dependencies = { schoolRepository } }) {
-  const result = await dependencies.schoolRepository.getSessionExpirationDate({ code: schoolCode });
+  const sessionExpirationDate = await dependencies.schoolRepository.getSessionExpirationDate({ code: schoolCode });
 
-  return result && new Date() < new Date(result) ? true : false;
+  return _isSessionActive(sessionExpirationDate);
 };
+
+function _isSessionActive(sessionExpirationDate) {
+  return new Date() < new Date(sessionExpirationDate);
+}
 
 export { execute };
