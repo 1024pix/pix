eab5d39a9aad76bac3d60a286126fb7374326c1b
Quentin Lebouc <quentin.lebouc@pix.fr>
2024-07-10 17:24:35 +0200

    feat(api): add clone badge method

--- api/src/evaluation/domain/models/Badge.js
+++ api/src/evaluation/domain/models/Badge.js
@@ -1,3 +1,5 @@
+import { BADGE_COPY_NAME_PREFIX } from '../../../shared/domain/constants.js';
+
 const UPDATABLE_PROPERTIES = ['message', 'altMessage', 'key', 'title', 'imageUrl', 'isCertifiable', 'isAlwaysVisible'];
 
 class Badge {
@@ -28,6 +30,20 @@ class Badge {
   updateBadgeProperties(badgeToUpdate) {
     UPDATABLE_PROPERTIES.forEach((property) => {
       if (badgeToUpdate[property]) this[property] = badgeToUpdate[property];
+    });
+  }
+
+  clone(newTargetProfileId) {
+    return new Badge({
+      altMessage: this.altMessage,
+      imageUrl: this.imageUrl,
+      message: this.message,
+      title: this.title,
+      key: BADGE_COPY_NAME_PREFIX + this.key,
+      isCertifiable: this.isCertifiable,
+      targetProfileId: newTargetProfileId,
+      isAlwaysVisible: this.isAlwaysVisible,
+      complementaryCertificationBadge: this.complementaryCertificationBadge,
     });
   }
 }
