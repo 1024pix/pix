142d037131e553c949510012410ae058dbb6ef28
Yvonnick Frin <frin.yvonnick@gmail.com>
2024-07-10 15:35:49 +0200

    feat(orga) add delete multiple campaigns from my campaigns

--- orga/tests/integration/components/campaign/list_test.js
+++ orga/tests/integration/components/campaign/list_test.js
@@ -1,4 +1,6 @@
-import { render } from '@1024pix/ember-testing-library';
+import { fillByLabel, render } from '@1024pix/ember-testing-library';
+import Service from '@ember/service';
+import { click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { module, test } from 'qunit';
 import sinon from 'sinon';
@@ -345,6 +347,191 @@ module('Integration | Component | Campaign::List', function (hooks) {
 
       // then
       assert.dom(screen.getByText(this.intl.t('pages.campaigns-list.table.description-all-campaigns'))).exists();
+    });
+  });
+
+  module('When there are campaigns not only owned by current user', function () {
+    test('should not display checkboxes', async function (assert) {
+      // given
+      const store = this.owner.lookup('service:store');
+
+      const campaign1 = store.createRecord('campaign', {
+        id: '1',
+        name: 'campagne 1',
+        code: 'AAAAAA111',
+        type: 'PROFILES_COLLECTION',
+      });
+      const campaigns = [campaign1];
+      campaigns.meta = {
+        rowCount: 2,
+      };
+      this.set('campaigns', campaigns);
+
+      // when
+      const screen = await render(
+        hbs`<Campaign::List @campaigns={{this.campaigns}} @onFilter={{this.noop}} @onClickCampaign={{this.noop}} />`,
+      );
+
+      // then
+
+      assert.dom(screen.queryByRole('checkbox')).doesNotExist();
+    });
+  });
+
+  module('When there are only campaigns owned by current user', function (hooks) {
+    hooks.beforeEach(function () {
+      const routerService = this.owner.lookup('service:router');
+      sinon.stub(routerService, 'replaceWith');
+      const store = this.owner.lookup('service:store');
+
+      const campaign1 = store.createRecord('campaign', {
+        id: '1',
+        name: 'campagne 1',
+        code: 'AAAAAA111',
+        type: 'PROFILES_COLLECTION',
+      });
+      const campaign2 = store.createRecord('campaign', {
+        id: '2',
+        name: 'campagne 2',
+        code: 'BBBBBB222',
+        type: 'ASSESSMENT',
+      });
+      const campaigns = [campaign1, campaign2];
+      campaigns.meta = { page: 1, pageSize: 1, rowCount: 2, pageCount: 2 };
+      this.set('campaigns', campaigns);
+    });
+    test('should display checkboxes', async function (assert) {
+      // when
+      const screen = await render(
+        hbs`<Campaign::List
+  @campaigns={{this.campaigns}}
+  @onFilter={{this.noop}}
+  @onClickCampaign={{this.noop}}
+  @listOnlyCampaignsOfCurrentUser={{true}}
+/>`,
+      );
+
+      // then
+      assert.strictEqual(screen.queryAllByRole('checkbox').length, 3);
+    });
+
+    test('should reset selected campaigns when using pagination', async function (assert) {
+      // when
+      const screen = await render(
+        hbs`<Campaign::List
+  @campaigns={{this.campaigns}}
+  @onFilter={{this.noop}}
+  @onClickCampaign={{this.noop}}
+  @listOnlyCampaignsOfCurrentUser={{true}}
+/>`,
+      );
+
+      const firstCampaignCheckbox = screen.getAllByRole('checkbox')[1];
+      await click(firstCampaignCheckbox);
+
+      const nextButton = await screen.findByLabelText(this.intl.t('common.pagination.action.next'));
+      await click(nextButton);
+
+      // then
+      assert.false(firstCampaignCheckbox.checked);
+    });
+
+    test('should reset selected campaigns when using filters', async function (assert) {
+      // when
+      const screen = await render(
+        hbs`<Campaign::List
+  @campaigns={{this.campaigns}}
+  @onFilter={{this.noop}}
+  @onClickCampaign={{this.noop}}
+  @listOnlyCampaignsOfCurrentUser={{true}}
+/>`,
+      );
+
+      const firstCampaignCheckbox = screen.getAllByRole('checkbox')[1];
+      await click(firstCampaignCheckbox);
+
+      await fillByLabel(this.intl.t('pages.campaigns-list.filter.by-name'), '1');
+
+      // then
+      assert.false(firstCampaignCheckbox.checked);
+    });
+
+    test('should reset selected campaigns when resetting filters', async function (assert) {
+      // when
+      const screen = await render(
+        hbs`<Campaign::List
+  @campaigns={{this.campaigns}}
+  @onFilter={{this.noop}}
+  @onClickCampaign={{this.noop}}
+  @listOnlyCampaignsOfCurrentUser={{true}}
+  @nameFilter='1'
+  @onClear={{this.noop}}
+/>`,
+      );
+
+      const firstCampaignCheckbox = screen.getAllByRole('checkbox')[1];
+      await click(firstCampaignCheckbox);
+
+      const resetButton = await screen.findByRole('button', {
+        name: this.intl.t('common.filters.actions.clear'),
+      });
+      await click(resetButton);
+
+      // then
+      assert.false(firstCampaignCheckbox.checked);
+    });
+
+    test('should delete campaigns', async function (assert) {
+      class CurrentUserStub extends Service {
+        organization = { id: 1 };
+      }
+      this.owner.register('service:current-user', CurrentUserStub);
+      const deleteStub = sinon.stub();
+      class StoreStub extends Service {
+        adapterFor = () => ({ delete: deleteStub });
+      }
+      // We have to do this because of previous usage of lookup
+      this.owner.unregister('service:store');
+      this.owner.register('service:store', StoreStub);
+      // when
+      const onDeleteCampaignsStub = sinon.stub();
+      this.set('onDeleteCampaigns', onDeleteCampaignsStub);
+
+      const screen = await render(
+        hbs`<Campaign::List
+  @campaigns={{this.campaigns}}
+  @onFilter={{this.noop}}
+  @onClickCampaign={{this.noop}}
+  @listOnlyCampaignsOfCurrentUser={{true}}
+  @onDeleteCampaigns={{this.onDeleteCampaigns}}
+/>`,
+      );
+
+      await click(screen.getAllByRole('checkbox')[1]);
+      await click(screen.getAllByRole('checkbox')[2]);
+
+      const deleteButton = await screen.findByRole('button', {
+        name: this.intl.t('pages.campaigns-list.action-bar.delete-button'),
+      });
+      await click(deleteButton);
+
+      await screen.findByRole('dialog');
+
+      const allowMultipleDeletionCheckbox = await screen.findByRole('checkbox', {
+        name: this.intl.t('components.ui.deletion-modal.confirmation-checkbox', { count: 2 }),
+      });
+      await click(allowMultipleDeletionCheckbox);
+
+      const confirmationButton = await screen.findByRole('button', {
+        name: this.intl.t('components.ui.deletion-modal.confirm-deletion'),
+      });
+      await click(confirmationButton);
+
+      //then
+      assert.ok(onDeleteCampaignsStub.called);
+      assert.ok(deleteStub.called);
+      const mainCheckbox = screen.getAllByRole('checkbox')[0];
+      assert.false(mainCheckbox.checked);
     });
   });
 });
