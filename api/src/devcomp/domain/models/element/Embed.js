26057a0568bd76e66ed56416d5838c20a66335f7
Rébecca Kaci <rebecca.kaci@gmail.com>
2024-07-10 11:15:26 +0200

    feat(api): add embed model
    Co-authored-by: Dimitri Lahaye <dimitri.lahaye@pix.fr>

---
+++ api/src/devcomp/domain/models/element/Embed.js
@@ -0,0 +1,20 @@
+import { assertNotNullOrUndefined } from '../../../../shared/domain/models/asserts.js';
+import { Element } from './Element.js';
+
+class Embed extends Element {
+  constructor({ id, isCompletionRequired, title, url, height }) {
+    super({ id, type: 'embed' });
+
+    assertNotNullOrUndefined(isCompletionRequired, 'The isCompletionRequired attribute is required for an embed');
+    assertNotNullOrUndefined(title, 'The title is required for an embed');
+    assertNotNullOrUndefined(url, 'The url is required for an embed');
+    assertNotNullOrUndefined(height, 'The height is required for an embed');
+
+    this.isCompletionRequired = isCompletionRequired;
+    this.title = title;
+    this.url = url;
+    this.height = height;
+  }
+}
+
+export { Embed };
---
+++ api/tests/devcomp/unit/domain/models/element/Embed_test.js
@@ -0,0 +1,60 @@
+import { Embed } from '../../../../../../src/devcomp/domain/models/element/Embed.js';
+import { expect } from '../../../../../test-helper.js';
+
+describe('Unit | Devcomp | Domain | Models | Element | Embed', function () {
+  describe('#constructor', function () {
+    it('should create an embed and keep attributes', function () {
+      // given
+      const props = { id: 'id', isCompletionRequired: false, title: 'title', url: 'https://embed.com', height: 150 };
+
+      // when
+      const embed = new Embed(props);
+
+      // then
+      expect(embed.id).to.equal('id');
+      expect(embed.type).to.equal('embed');
+      expect(embed.isCompletionRequired).to.equal(false);
+      expect(embed.title).to.equal('title');
+      expect(embed.url).to.equal('https://embed.com');
+      expect(embed.height).to.equal(150);
+    });
+
+    describe('errors', function () {
+      describe('An embed without an id', function () {
+        it('should throw an error', function () {
+          expect(() => new Embed({})).to.throw('The id is required for an element');
+        });
+      });
+
+      describe('An embed without the isCompletionRequired attribute', function () {
+        it('should throw an error', function () {
+          expect(() => new Embed({ id: 'id' })).to.throw('The isCompletionRequired attribute is required for an embed');
+        });
+      });
+
+      describe('An embed without a title', function () {
+        it('should throw an error', function () {
+          expect(() => new Embed({ id: 'id', isCompletionRequired: false })).to.throw(
+            'The title is required for an embed',
+          );
+        });
+      });
+
+      describe('An embed without an url', function () {
+        it('should throw an error', function () {
+          expect(() => new Embed({ id: 'id', isCompletionRequired: false, title: 'title' })).to.throw(
+            'The url is required for an embed',
+          );
+        });
+      });
+
+      describe('An embed without a height', function () {
+        it('should throw an error', function () {
+          expect(
+            () => new Embed({ id: 'id', isCompletionRequired: false, title: 'title', url: 'https://embed.com' }),
+          ).to.throw('The height is required for an embed');
+        });
+      });
+    });
+  });
+});
