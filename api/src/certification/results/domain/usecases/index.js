a14db78a793ee56dd22de503dca961bedae89243
Andreia Pena <andreia.pena@pix.fr>
2024-07-11 14:32:26 +0200

    ♻️ api: Clean injected repositories

---
+++ api/src/certification/results/domain/usecases/index.js
@@ -0,0 +1,46 @@
+// eslint-disable import/no-restricted-paths
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import * as certificationResultRepository from '../../../../../lib/infrastructure/repositories/certification-result-repository.js';
+import { injectDependencies } from '../../../../shared/infrastructure/utils/dependency-injection.js';
+import { importNamedExportsFromDirectory } from '../../../../shared/infrastructure/utils/import-named-exports-from-directory.js';
+import * as certificationCourseRepository from '../../../shared/infrastructure/repositories/certification-course-repository.js';
+import * as certificationReportRepository from '../../../shared/infrastructure/repositories/certification-report-repository.js';
+import * as certificateRepository from '../../infrastructure/repositories/certificate-repository.js';
+import * as scoCertificationCandidateRepository from '../../infrastructure/repositories/sco-certification-candidate-repository.js';
+
+/**
+ * Using {@link https://jsdoc.app/tags-type "Closure Compiler's syntax"} to document injected dependencies
+ *
+ * @typedef {certificationResultRepository} CertificationResultRepository
+ * @typedef {scoCertificationCandidateRepository} ScoCertificationCandidateRepository
+ * @typedef {certificationCourseRepository} CertificationCourseRepository
+ * @typedef {certificateRepository} CertificateRepository
+ * @typedef {certificationReportRepository} CertificationReportRepository
+ **/
+const dependencies = {
+  certificationCourseRepository,
+  certificationResultRepository,
+  scoCertificationCandidateRepository,
+  certificateRepository,
+  certificationReportRepository,
+};
+
+const path = dirname(fileURLToPath(import.meta.url));
+
+/**
+ * Note : current ignoredFileNames are injected in * {@link file://./../../../shared/domain/usecases/index.js}
+ * This is in progress, because they should be injected in this file and not by shared sub-domain
+ * The only remaining file ignored should be index.js
+ */
+const usecasesWithoutInjectedDependencies = {
+  ...(await importNamedExportsFromDirectory({
+    path: join(path, './'),
+    ignoredFileNames: ['index.js'],
+  })),
+};
+
+const usecases = injectDependencies(usecasesWithoutInjectedDependencies, dependencies);
+
+export { usecases };
