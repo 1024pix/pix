fd64d287076946d714aa438eb541afa5c0404688
Benjamin Petetot <benjamin.petetot@pix.fr>
2024-07-11 15:22:35 +0200

    refactor(api): add a test to  anonymize-gar-authentication-methods when audit logger disabled

--- api/tests/identity-access-management/unit/domain/usecases/anonymize-gar-authentication-methods.usecase.test.js
+++ api/tests/identity-access-management/unit/domain/usecases/anonymize-gar-authentication-methods.usecase.test.js
@@ -1,3 +1,4 @@
+import { config } from '../../../../../lib/config.js';
 import { PIX_ADMIN } from '../../../../../src/authorization/domain/constants.js';
 import { GarAuthenticationMethodAnonymized } from '../../../../../src/identity-access-management/domain/models/GarAuthenticationMethodAnonymized.js';
 import { anonymizeGarAuthenticationMethods } from '../../../../../src/identity-access-management/domain/usecases/anonymize-gar-authentication-methods.usecase.js';
@@ -18,6 +19,7 @@ describe('Unit | Identity Access Management | Domain | UseCase | anonymize-gar-a
       schedule: sinon.stub().resolves(),
     };
     domainTransaction = Symbol('domain transaction');
+    sinon.stub(config.auditLogger, 'isEnabled').value(true);
   });
 
   afterEach(function () {
@@ -79,5 +81,31 @@ describe('Unit | Identity Access Management | Domain | UseCase | anonymize-gar-a
       role: ROLES.SUPER_ADMIN,
     });
     expect(garAnonymizedBatchEventsLoggingJob.schedule).to.have.been.calledWith(payload);
+  });
+
+  it('does not trigger a garAnonymizedBatchEventsLogging job when audit logger config is disabled', async function () {
+    // given
+    const userIds = [1001, 1002, 1003];
+    const adminMemberId = 1;
+    const garAnonymizedUserIds = [1002, 1003];
+
+    const authenticationMethodRepository = {
+      batchAnonymizeByUserIds: sinon.stub().resolves({ garAnonymizedUserIds }),
+    };
+
+    sinon.stub(DomainTransaction, 'execute').callsFake((lambda) => lambda(domainTransaction));
+    sinon.stub(config.auditLogger, 'isEnabled').value(false);
+
+    // when
+    await anonymizeGarAuthenticationMethods({
+      userIds,
+      adminMemberId,
+      authenticationMethodRepository,
+      garAnonymizedBatchEventsLoggingJob,
+      domainTransaction,
+    });
+
+    // then
+    expect(garAnonymizedBatchEventsLoggingJob.schedule).to.have.not.been.called;
   });
 });
