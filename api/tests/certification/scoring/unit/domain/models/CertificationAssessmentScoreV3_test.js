b7c616ace05c0cc2d69ba823a68af91874107888
Alexandre COIN <alexandre.pc.coin@gmail.com>
2024-07-12 09:31:46 +0200

    :recycle: api: use scoring configuration from tests in domain builder

--- api/tests/certification/scoring/unit/domain/models/CertificationAssessmentScoreV3_test.js
+++ api/tests/certification/scoring/unit/domain/models/CertificationAssessmentScoreV3_test.js
@@ -26,16 +26,6 @@ describe('Unit | Domain | Models | CertificationAssessmentScoreV3 ', function ()
     scoringDegradationService = { downgradeCapacity: sinon.stub() };
     v3CertificationScoring = domainBuilder.buildV3CertificationScoring({
       competencesForScoring: [domainBuilder.buildCompetenceForScoring()],
-      certificationScoringConfiguration: [
-        { bounds: { max: -2.6789, min: -5.12345 }, meshLevel: 0 },
-        { bounds: { max: -0.23456, min: -2.6789 }, meshLevel: 1 },
-        { bounds: { max: 0.78901, min: -0.23456 }, meshLevel: 2 },
-        { bounds: { max: 1.34567, min: 0.78901 }, meshLevel: 3 },
-        { bounds: { max: 2.89012, min: 1.34567 }, meshLevel: 4 },
-        { bounds: { max: 2.45678, min: 2.89012 }, meshLevel: 5 },
-        { bounds: { max: 4.90123, min: 2.45678 }, meshLevel: 6 },
-        { bounds: { max: 6.56789, min: 4.90123 }, meshLevel: 7 },
-      ],
     });
   });
 
--- api/tests/tooling/domain-builder/factory/certification/scoring/build-v3-certification-scoring.js
+++ api/tests/tooling/domain-builder/factory/certification/scoring/build-v3-certification-scoring.js
@@ -2,7 +2,16 @@ import { V3CertificationScoring } from '../../../../../../src/certification/scor
 
 export const buildV3CertificationScoring = ({
   competencesForScoring = [],
-  certificationScoringConfiguration = [{ meshLevel: 0, bounds: { min: -8, max: 8 } }],
+  certificationScoringConfiguration = [
+    { bounds: { max: -2.6789, min: -5.12345 }, meshLevel: 0 },
+    { bounds: { max: -0.23456, min: -2.6789 }, meshLevel: 1 },
+    { bounds: { max: 0.78901, min: -0.23456 }, meshLevel: 2 },
+    { bounds: { max: 1.34567, min: 0.78901 }, meshLevel: 3 },
+    { bounds: { max: 2.89012, min: 1.34567 }, meshLevel: 4 },
+    { bounds: { max: 2.45678, min: 2.89012 }, meshLevel: 5 },
+    { bounds: { max: 4.90123, min: 2.45678 }, meshLevel: 6 },
+    { bounds: { max: 6.56789, min: 4.90123 }, meshLevel: 7 },
+  ],
 } = {}) => {
   return new V3CertificationScoring({
     competencesForScoring,
