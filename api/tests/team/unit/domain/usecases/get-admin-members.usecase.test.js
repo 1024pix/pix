87254d940bfb0cccec54c7b184cef8aae4592518
dlahaye <dimitrilahaye.dev@gmail.com>
2024-07-15 11:28:21 +0200

    refactor(api): move get-admin-members.usecase.js to src/team
    Co-authored-by: Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>

--- api/tests/team/unit/domain/usecases/get-admin-members.usecase.test.js
+++ api/tests/team/unit/domain/usecases/get-admin-members.usecase.test.js
@@ -1,8 +1,8 @@
-import { getAdminMembers } from '../../../../lib/domain/usecases/get-admin-members.js';
-import { domainBuilder, expect, sinon } from '../../../test-helper.js';
+import { getAdminMembers } from '../../../../../src/team/domain/usecases/get-admin-members.usecase.js';
+import { domainBuilder, expect, sinon } from '../../../../test-helper.js';
 
-describe('Unit | UseCase | get-admin-members', function () {
-  it('should return all admin members', async function () {
+describe('Unit | Team | Domain | UseCase | get-admin-members', function () {
+  it('returns all admin members', async function () {
     // given
     const adminMemberRepository = {
       findAll: sinon.stub(),
