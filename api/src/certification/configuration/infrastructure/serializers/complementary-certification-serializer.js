1b12e79315a55477da20cb264aa049c3cffac044
Andreia Pena <andreia.pena@pix.fr>
2024-07-15 12:06:05 +0200

    🔥 api: remove unused deserialize method

--- api/src/certification/configuration/infrastructure/serializers/complementary-certification-serializer.js
+++ api/src/certification/configuration/infrastructure/serializers/complementary-certification-serializer.js
@@ -2,20 +2,10 @@ import jsonapiSerializer from 'jsonapi-serializer';
 
 const { Serializer } = jsonapiSerializer;
 
-import { ComplementaryCertification } from '../../../../../lib/domain/models/index.js';
-
 const serialize = function (complementaryCertifications) {
   return new Serializer('complementary-certification', {
     attributes: ['label', 'key'],
   }).serialize(complementaryCertifications);
 };
 
-const deserialize = function (jsonAPI) {
-  return new ComplementaryCertification({
-    id: jsonAPI.data.id,
-    label: jsonAPI.data.attributes.label,
-    key: jsonAPI.data.attributes.key,
-  });
-};
-
-export { deserialize, serialize };
+export { serialize };
