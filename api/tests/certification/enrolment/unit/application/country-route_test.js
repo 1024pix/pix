748f74c1d27281e89d84055bc01258c15c0a7ca7
Andreia Pena <andreia.pena@pix.fr>
2024-07-16 14:33:15 +0200

    🚚️ api: move route to src/enrolment context

--- api/tests/certification/enrolment/unit/application/country-route_test.js
+++ api/tests/certification/enrolment/unit/application/country-route_test.js
@@ -1,8 +1,8 @@
-import { countryController } from '../../../../lib/application/countries/country-controller.js';
-import * as moduleUnderTest from '../../../../lib/application/countries/index.js';
-import { expect, HttpTestServer, sinon } from '../../../test-helper.js';
+import { countryController } from '../../../../../src/certification/enrolment/application/country-controller.js';
+import * as moduleUnderTest from '../../../../../src/certification/enrolment/application/country-route.js';
+import { expect, HttpTestServer, sinon } from '../../../../test-helper.js';
 
-describe('Unit | Router | country-router', function () {
+describe('Certification | Enrolment | Unit | Application | country-route', function () {
   describe('GET /api/countries', function () {
     it('should exist', async function () {
       // given
