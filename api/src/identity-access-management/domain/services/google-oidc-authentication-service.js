42383c54377b1629a08c6fbce23a68b979dc86a1
Benjamin Petetot <benjamin.petetot@pix.fr>
2024-07-16 11:51:27 +0200

    refactor(api): set claims mapping for GoogleOidcAuthenticationService

--- api/src/identity-access-management/domain/services/google-oidc-authentication-service.js
+++ api/src/identity-access-management/domain/services/google-oidc-authentication-service.js
@@ -1,18 +1,12 @@
-import jsonwebtoken from 'jsonwebtoken';
-
 import { OidcAuthenticationService } from './oidc-authentication-service.js';
+
+const GOOGLE_CLAIM_MAPPING = {
+  email: ['email'],
+  externalIdentityId: ['sub'],
+};
 
 export class GoogleOidcAuthenticationService extends OidcAuthenticationService {
   constructor(oidcProvider) {
-    super(oidcProvider);
-  }
-
-  async getUserInfo({ idToken }) {
-    const userInfo = jsonwebtoken.decode(idToken);
-
-    return {
-      externalIdentityId: userInfo.sub,
-      email: userInfo.email,
-    };
+    super({ ...oidcProvider, claimMapping: GOOGLE_CLAIM_MAPPING });
   }
 }
