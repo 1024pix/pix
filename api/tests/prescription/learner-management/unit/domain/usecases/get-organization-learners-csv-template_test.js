0ba8a1fcb5fe061d269acfbb0cc064224fa25a49
Yvonnick Frin <frin.yvonnick@gmail.com>
2024-07-17 15:55:50 +0200

    feat(api): make endpoint handle import format template

--- api/tests/prescription/learner-management/unit/domain/usecases/get-organization-learners-csv-template_test.js
+++ api/tests/prescription/learner-management/unit/domain/usecases/get-organization-learners-csv-template_test.js
@@ -1,18 +1,55 @@
 import _ from 'lodash';
 
 import { UserNotAuthorizedToAccessEntityError } from '../../../../../../lib/domain/errors.js';
+import { OrganizationLearnerImportFormat } from '../../../../../../src/prescription/learner-management/domain/models/OrganizationLearnerImportFormat.js';
 import { getOrganizationLearnersCsvTemplate } from '../../../../../../src/prescription/learner-management/domain/usecases/get-organization-learners-csv-template.js';
 import { catchErr, domainBuilder, expect, sinon } from '../../../../../test-helper.js';
 import { getI18n } from '../../../../../tooling/i18n/i18n.js';
 
 describe('Unit | UseCase | get-organization-learners-csv-template', function () {
-  let userId, organizationId, membershipRepository, i18n;
+  let userId, organizationId, membershipRepository, organizationLearnerImportFormatRepository, i18n;
 
   beforeEach(function () {
     userId = Symbol('userId');
     organizationId = Symbol('organizationId');
     membershipRepository = { findByUserIdAndOrganizationId: _.noop };
+    organizationLearnerImportFormatRepository = { get: sinon.stub() };
     i18n = getI18n();
+  });
+
+  context('When user is ADMIN in an organization with the import feature', function () {
+    it('should return headers line', async function () {
+      // given
+      const organization = domainBuilder.buildOrganization({ isManagingStudents: true, type: 'PRO' });
+      const membership = domainBuilder.buildMembership({ organizationRole: 'ADMIN', organization });
+      sinon.stub(membershipRepository, 'findByUserIdAndOrganizationId').resolves([membership]);
+      organizationLearnerImportFormatRepository.get.resolves(
+        new OrganizationLearnerImportFormat({
+          name: 'GENERIC',
+          filetype: 'csv',
+          config: {
+            headers: [
+              { name: 'Nom', property: 'lastName', required: true },
+              { name: 'Prénom', property: 'firstName', required: true },
+              { name: 'Date de naissance', required: true },
+            ],
+          },
+        }),
+      );
+
+      // when
+      const result = await getOrganizationLearnersCsvTemplate({
+        userId,
+        organizationId,
+        i18n,
+        membershipRepository,
+        organizationLearnerImportFormatRepository,
+      });
+
+      // then
+      const csvExpected = '\uFEFF"Nom";' + '"Prénom";' + '"Date de naissance"\n';
+      expect(result).to.deep.equal(csvExpected);
+    });
   });
 
   context('When user is ADMIN in a SUP organization managing students', function () {
@@ -23,7 +60,13 @@ describe('Unit | UseCase | get-organization-learners-csv-template', function () 
       sinon.stub(membershipRepository, 'findByUserIdAndOrganizationId').resolves([membership]);
 
       // when
-      const result = await getOrganizationLearnersCsvTemplate({ userId, organizationId, i18n, membershipRepository });
+      const result = await getOrganizationLearnersCsvTemplate({
+        userId,
+        organizationId,
+        i18n,
+        membershipRepository,
+        organizationLearnerImportFormatRepository,
+      });
 
       // then
       const csvExpected =
@@ -56,6 +99,7 @@ describe('Unit | UseCase | get-organization-learners-csv-template', function () 
         userId,
         organizationId,
         membershipRepository,
+        organizationLearnerImportFormatRepository,
       });
 
       // then
@@ -63,7 +107,7 @@ describe('Unit | UseCase | get-organization-learners-csv-template', function () 
     });
   });
 
-  context('When user is not ADMIN in a SUP organization', function () {
+  context('When user is not ADMIN', function () {
     it('should throw an error', async function () {
       // given
       sinon
@@ -75,6 +119,7 @@ describe('Unit | UseCase | get-organization-learners-csv-template', function () 
         userId,
         organizationId,
         membershipRepository,
+        organizationLearnerImportFormatRepository,
       });
 
       // then
@@ -92,6 +137,7 @@ describe('Unit | UseCase | get-organization-learners-csv-template', function () 
         userId,
         organizationId,
         membershipRepository,
+        organizationLearnerImportFormatRepository,
       });
 
       // then
