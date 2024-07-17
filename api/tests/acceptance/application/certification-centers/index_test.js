20b4c13b7b5c9549f4179c5f12179b73d51ba036
dlahaye <dimitrilahaye.dev@gmail.com>
2024-07-17 09:36:45 +0200

    refactor(api): delete legacy test

--- api/tests/acceptance/application/certification-centers/index_test.js
+++ api/tests/acceptance/application/certification-centers/index_test.js
@@ -1,11 +1,9 @@
-import { CertificationCenterInvitation } from '../../../../src/team/domain/models/CertificationCenterInvitation.js';
 import {
   createServer,
   databaseBuilder,
   expect,
   generateValidRequestAuthorizationHeader,
   insertUserWithRoleSuperAdmin,
-  sinon,
 } from '../../../test-helper.js';
 
 describe('Acceptance | Route | Certification Centers', function () {
@@ -75,57 +73,6 @@ describe('Acceptance | Route | Certification Centers', function () {
         expect(result.data.attributes['data-protection-officer-email']).to.equal('justin.ptipeu@example.net');
         expect(result.data.attributes['is-v3-pilot']).to.equal(true);
         expect(result.data.attributes.name).to.equal('Justin Ptipeu Orga');
-      });
-    });
-  });
-
-  describe('POST /api/admin/certification-centers/{certificationCenterId}/invitations', function () {
-    let clock;
-    const now = new Date('2021-05-01');
-
-    beforeEach(async function () {
-      clock = sinon.useFakeTimers({
-        now,
-        toFake: ['Date'],
-      });
-    });
-
-    afterEach(async function () {
-      clock.restore();
-    });
-
-    it('should return 201 HTTP status code with created invitation', async function () {
-      // given
-      const adminMember = await insertUserWithRoleSuperAdmin();
-      const certificationCenterId = databaseBuilder.factory.buildCertificationCenter().id;
-      await databaseBuilder.commit();
-
-      // when
-      const { result, statusCode } = await server.inject({
-        headers: {
-          authorization: generateValidRequestAuthorizationHeader(adminMember.id),
-        },
-        method: 'POST',
-        payload: {
-          data: {
-            attributes: {
-              email: 'some.user@example.net',
-              lang: 'fr-fr',
-              role: CertificationCenterInvitation.Roles.ADMIN,
-            },
-          },
-        },
-        url: `/api/admin/certification-centers/${certificationCenterId}/invitations`,
-      });
-
-      // then
-      expect(statusCode).to.equal(201);
-      expect(result.data.type).to.equal('certification-center-invitations');
-      expect(result.data).to.have.property('id');
-      expect(result.data.attributes).to.deep.equal({
-        'updated-at': now,
-        email: 'some.user@example.net',
-        role: 'ADMIN',
       });
     });
   });
