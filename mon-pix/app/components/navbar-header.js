4075a057752d231032d426365b62ab2dc74e258c
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2024-07-17 15:20:32 +0200

    feat: use ra fragment

--- mon-pix/app/components/navbar-header.js
+++ mon-pix/app/components/navbar-header.js
@@ -1,16 +1,29 @@
 import { action } from '@ember/object';
 import { service } from '@ember/service';
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
 export default class NavbarHeader extends Component {
   @service currentDomain;
+  @tracked fragment;
+
+  constructor() {
+    super(...arguments);
+    fetch('https://epreuves-pr1787.review.pix.fr/fr/qcu_image/1d_MiseEnForme_mots.fragment.html')
+      .then((response) => {
+        return response.text();
+      })
+      .then((html) => {
+        this.fragment = html;
+      });
+  }
 
   get isFrenchDomainExtension() {
     return this.currentDomain.isFranceDomain;
   }
 
   @action
-  async triggerAction(event) {
+  async handleAnswer(event) {
     console.log(event.detail);
   }
 }
