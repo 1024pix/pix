0f3f6f957c1a29bf555419f2f70a00f2e7bdf4d5
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2024-07-18 12:04:03 +0200

    feat(orga): display mention of anonymous seat when necessary

--- orga/app/components/places/statistics.gjs
+++ orga/app/components/places/statistics.gjs
@@ -4,12 +4,21 @@ import { t } from 'ember-intl';
 <template>
   <section class="statistics">
     <PixIndicatorCard @title={{t "cards.available-seats-count.title"}} @color="success" @icon="users">
-      {{@available}}
-      <span class="statistics__total">{{t "cards.available-seats-count.value" total=@total}}</span>
+      {{@model.available}}
+      <span class="statistics__total">{{t "cards.available-seats-count.value" total=@model.total}}</span>
     </PixIndicatorCard>
     <PixIndicatorCard @title={{t "cards.occupied-seats-count.title"}} @color="warning" @icon="users">
-      {{@occupied}}
-      <span class="statistics__total">{{t "cards.occupied-seats-count.value" total=@total}}</span>
+      <:default>
+        {{@model.occupied}}
+
+        <span class="statistics__total">{{t "cards.occupied-seats-count.value" total=@model.total}}</span>
+      </:default>
+
+      <:sub>
+        {{#if @model.hasAnonymousSeat}}
+          <span>{{t "cards.occupied-seats-count.anonymous" count=@model.anonymousSeat}}</span>
+        {{/if}}
+      </:sub>
     </PixIndicatorCard>
   </section>
 </template>
--- orga/tests/integration/components/places/statistics_test.js
+++ orga/tests/integration/components/places/statistics_test.js
@@ -10,18 +10,37 @@ module('Integration | Component | Places::Statistics', function (hooks) {
 
   test('it should display occupied and available places', async function (assert) {
     // given
-    this.total = 30;
-    this.occupied = 20;
-    this.available = 10;
+    const store = this.owner.lookup('service:store');
+    this.model = store.createRecord('organization-place-statistic', {
+      total: 30,
+      occupied: 20,
+      available: 10,
+      anonymousSeat: 0,
+    });
     // when
-    const screen = await render(
-      hbs`<Places::Statistics @total={{this.total}} @occupied={{this.occupied}} @available={{this.available}} />`,
-    );
+    const screen = await render(hbs`<Places::Statistics @model={{this.model}} />`);
 
     // then
-    assert.ok(screen.getByText(this.available));
-    assert.ok(screen.getAllByText(t('cards.available-seats-count.value', { total: this.total }))[0]);
-    assert.ok(screen.getByText(this.occupied));
-    assert.ok(screen.getAllByText(t('cards.occupied-seats-count.value', { total: this.total }))[1]);
+    assert.ok(screen.getByText(this.model.available));
+    assert.ok(screen.getAllByText(t('cards.available-seats-count.value', { total: this.model.total }))[0]);
+    assert.ok(screen.getByText(this.model.occupied));
+    assert.ok(screen.getAllByText(t('cards.occupied-seats-count.value', { total: this.model.total }))[1]);
+    assert.notOk(screen.queryByText(t('cards.occupied-seats-count.anonymous', { count: this.model.anonymousSeat })));
+  });
+
+  test('it should display anonymous seat informations', async function (assert) {
+    // given
+    const store = this.owner.lookup('service:store');
+    this.model = store.createRecord('organization-place-statistic', {
+      total: 30,
+      occupied: 20,
+      available: 10,
+      anonymousSeat: 1,
+    });
+    // when
+    const screen = await render(hbs`<Places::Statistics @model={{this.model}} />`);
+
+    // then
+    assert.ok(screen.getByText(t('cards.occupied-seats-count.anonymous', { count: this.model.anonymousSeat })));
   });
 });
