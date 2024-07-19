39f1b567b8ac104ef21fae2957399df2295f75b3
Alexandre COIN <alexandre.pc.coin@gmail.com>
2024-07-19 14:09:15 +0200

    :truck: api: move repository methods to src

--- api/lib/infrastructure/repositories/complementary-certification-course-result-repository.js
+++ api/lib/infrastructure/repositories/complementary-certification-course-result-repository.js
@@ -1,34 +1,4 @@
 import { knex } from '../../../db/knex-database-connection.js';
-import { ComplementaryCertificationCourseResult } from '../../../src/certification/shared/domain/models/ComplementaryCertificationCourseResult.js';
-
-const getPixSourceResultByComplementaryCertificationCourseId = async function ({ complementaryCertificationCourseId }) {
-  const result = await knex
-    .select('*')
-    .from('complementary-certification-course-results')
-    .where({ complementaryCertificationCourseId, source: ComplementaryCertificationCourseResult.sources.PIX })
-    .first();
-
-  if (!result) return null;
-
-  return ComplementaryCertificationCourseResult.from(result);
-};
-
-const getAllowedJuryLevelIdsByComplementaryCertificationBadgeId = async function (complementaryCertificationBadgeId) {
-  return knex
-    .pluck('complementary-certification-badges.id')
-    .from('badges')
-    .innerJoin('complementary-certification-badges', 'badges.id', 'complementary-certification-badges.badgeId')
-    .where(
-      'targetProfileId',
-      '=',
-      knex('badges')
-        .select('targetProfileId')
-        .innerJoin('complementary-certification-badges', 'badges.id', 'complementary-certification-badges.badgeId')
-        .where({ 'complementary-certification-badges.id': complementaryCertificationBadgeId })
-        .first(),
-    )
-    .orderBy('complementary-certification-badges.level', 'asc');
-};
 
 const save = async function ({
   complementaryCertificationCourseId,
@@ -42,15 +12,4 @@ const save = async function ({
     .merge();
 };
 
-const removeExternalJuryResult = async function ({ complementaryCertificationCourseId }) {
-  await knex('complementary-certification-course-results')
-    .where({ complementaryCertificationCourseId, source: ComplementaryCertificationCourseResult.sources.EXTERNAL })
-    .delete();
-};
-
-export {
-  getAllowedJuryLevelIdsByComplementaryCertificationBadgeId,
-  getPixSourceResultByComplementaryCertificationCourseId,
-  removeExternalJuryResult,
-  save,
-};
+export { save };
---
+++ api/src/certification/shared/infrastructure/repositories/complementary-certification-course-result-repository.js
@@ -0,0 +1,56 @@
+import { knex } from '../../../../../db/knex-database-connection.js';
+import { ComplementaryCertificationCourseResult } from '../../domain/models/ComplementaryCertificationCourseResult.js';
+
+const getPixSourceResultByComplementaryCertificationCourseId = async function ({ complementaryCertificationCourseId }) {
+  const result = await knex
+    .select('*')
+    .from('complementary-certification-course-results')
+    .where({ complementaryCertificationCourseId, source: ComplementaryCertificationCourseResult.sources.PIX })
+    .first();
+
+  if (!result) return null;
+
+  return ComplementaryCertificationCourseResult.from(result);
+};
+
+const getAllowedJuryLevelIdsByComplementaryCertificationBadgeId = async function (complementaryCertificationBadgeId) {
+  return knex
+    .pluck('complementary-certification-badges.id')
+    .from('badges')
+    .innerJoin('complementary-certification-badges', 'badges.id', 'complementary-certification-badges.badgeId')
+    .where(
+      'targetProfileId',
+      '=',
+      knex('badges')
+        .select('targetProfileId')
+        .innerJoin('complementary-certification-badges', 'badges.id', 'complementary-certification-badges.badgeId')
+        .where({ 'complementary-certification-badges.id': complementaryCertificationBadgeId })
+        .first(),
+    )
+    .orderBy('complementary-certification-badges.level', 'asc');
+};
+
+const removeExternalJuryResult = async function ({ complementaryCertificationCourseId }) {
+  await knex('complementary-certification-course-results')
+    .where({ complementaryCertificationCourseId, source: ComplementaryCertificationCourseResult.sources.EXTERNAL })
+    .delete();
+};
+
+const save = async function ({
+  complementaryCertificationCourseId,
+  complementaryCertificationBadgeId,
+  acquired,
+  source,
+}) {
+  return knex('complementary-certification-course-results')
+    .insert({ complementaryCertificationBadgeId, acquired, complementaryCertificationCourseId, source })
+    .onConflict(['complementaryCertificationCourseId', 'source'])
+    .merge();
+};
+
+export {
+  getAllowedJuryLevelIdsByComplementaryCertificationBadgeId,
+  getPixSourceResultByComplementaryCertificationCourseId,
+  removeExternalJuryResult,
+  save,
+};
