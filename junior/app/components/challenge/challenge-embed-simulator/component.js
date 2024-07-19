2f189ef2a1ef43d7d8657c2b46e2d10833e33100
Aurelie Crouillebois <aurelie.crouillebois@gmail.com>
2024-07-19 16:56:53 +0200

    feat(junior): display qcu-image-vue webcomponent

--- junior/app/components/challenge/challenge-embed-simulator/component.js
+++ junior/app/components/challenge/challenge-embed-simulator/component.js
@@ -1,8 +1,22 @@
 import { action } from '@ember/object';
 import { htmlSafe } from '@ember/template';
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
 export default class ChallengeEmbedSimulator extends Component {
+  @tracked props;
+
+  constructor(...args) {
+    super(...args);
+    fetch('https://epreuves-pr1787.review.pix.fr/fr/qcu_image/1d_MiseEnForme_mots.json')
+      .then((response) => {
+        return response.json();
+      })
+      .then(({ props }) => {
+        this.props = props;
+      });
+  }
+
   get embedDocumentHeightStyle() {
     const baseHeight = this.args.height ?? '600';
     const itemMedia = document.getElementsByClassName('challenge-item__media ')[0];
