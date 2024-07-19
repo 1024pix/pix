e0fa89accbe38aaf35f60d6c8b65454f9bd08eb3
Aurelie Crouillebois <aurelie.crouillebois@gmail.com>
2024-07-19 10:56:44 +0200

    feat(api): fetch html fragment on eligible challenges

--- api/src/shared/application/challenges/challenge-controller.js
+++ api/src/shared/application/challenges/challenge-controller.js
@@ -1,8 +1,19 @@
+import { httpAgent } from '../../../../lib/infrastructure/http/http-agent.js';
+import { NotFoundError } from '../../domain/errors.js';
 import * as challengeRepository from '../../infrastructure/repositories/challenge-repository.js';
 import * as challengeSerializer from '../../infrastructure/serializers/jsonapi/challenge-serializer.js';
 
-const get = async function (request, h, dependencies = { challengeRepository, challengeSerializer }) {
+const get = async (request, h, dependencies = { challengeRepository, challengeSerializer }) => {
   const challenge = await dependencies.challengeRepository.get(request.params.id);
+  if (challenge.hasEmbedFragment()) {
+    const embedFragmentResponse = await httpAgent.get({ url: challenge.embedUrl });
+    if (!embedFragmentResponse.isSuccessful) {
+      throw new NotFoundError(`Embed fragment with URL ${challenge.embedUrl} in challenge ${challenge.id} not found`);
+    }
+
+    challenge.embedFragment = embedFragmentResponse.data;
+  }
+
   return dependencies.challengeSerializer.serialize(challenge);
 };
 
--- api/tests/shared/unit/application/challenges/challenge-controller_test.js
+++ api/tests/shared/unit/application/challenges/challenge-controller_test.js
@@ -1,5 +1,6 @@
-import { challengeController } from '../../../../../src/shared/application/challenges/challenge-controller.js';
-import { expect, hFake, sinon } from '../../../../test-helper.js';
+import {challengeController} from '../../../../../src/shared/application/challenges/challenge-controller.js';
+import {NotFoundError} from '../../../../../src/shared/domain/errors.js';
+import {catchErr, domainBuilder, expect, hFake, nock, sinon} from '../../../../test-helper.js';
 
 describe('Unit | Controller | challenge-controller', function () {
   let challengeRepository;
@@ -14,7 +15,7 @@ describe('Unit | Controller | challenge-controller', function () {
     it('should fetch and return the given challenge, serialized as JSONAPI', async function () {
       // given
       const challengeId = 123;
-      const challenge = Symbol('someChallenge');
+      const challenge = domainBuilder.buildChallenge({ id: challengeId });
       const expectedResult = Symbol('serialized-challenge');
       challengeRepository.get.resolves(challenge);
       challengeSerializer.serialize.resolves(expectedResult);
@@ -29,6 +30,52 @@ describe('Unit | Controller | challenge-controller', function () {
       expect(challengeRepository.get).to.have.been.calledWithExactly(challengeId);
       expect(challengeSerializer.serialize).to.have.been.calledOnce;
       expect(response).to.deep.equal(expectedResult);
+    });
+
+    describe('when has embed fragment', function () {
+      it('should fetch fragment if challenge has embed fragment', async function () {
+        // given
+        const challengeId = 123;
+        const challenge = domainBuilder.buildChallenge({
+          embedUrl: 'https://serveur/fr/qcu_image/1d_MiseEnForme_mots.fragment.html',
+        });
+        challengeRepository.get.resolves(challenge);
+        const fragmentServerCall = nock('https://serveur')
+          .get('/fr/qcu_image/1d_MiseEnForme_mots.fragment.html')
+          .reply(200, 'my html fragment');
+
+        // when
+        await challengeController.get({ params: { id: challengeId } }, hFake, {
+          challengeRepository,
+          challengeSerializer,
+        });
+
+        // then
+        expect(fragmentServerCall.isDone()).to.equal(true);
+        expect(challengeSerializer.serialize).to.have.been.calledWithMatch({
+          embedFragment: 'my html fragment',
+        });
+      });
+
+      it('should throw NotFoundError if URL returns 404', async function () {
+        // given
+        const challengeId = 123;
+        const challenge = domainBuilder.buildChallenge({
+          embedUrl: 'https://serveur/fr/qcu_image/non_existing.fragment.html',
+        });
+        challengeRepository.get.resolves(challenge);
+        const fragmentServerCall = nock('https://serveur').get('/fr/qcu_image/non_existing.fragment.html').reply(404);
+
+        // when
+        const error = await catchErr(challengeController.get)({ params: { id: challengeId } }, hFake, {
+          challengeRepository,
+          challengeSerializer,
+        });
+
+        // then
+        expect(fragmentServerCall.isDone()).to.equal(true);
+        expect(error).to.be.instanceOf(NotFoundError);
+      });
     });
   });
 });
