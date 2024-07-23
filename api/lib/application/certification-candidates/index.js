5e87d54279de3eb61b5bd7265f53f55df29e47c1
Andreia Pena <andreia.pena@pix.fr>
2024-07-23 15:29:22 +0200

    🚚 api: move route to session-management context

--- api/lib/application/certification-candidates/index.js
+++ api/lib/application/certification-candidates/index.js
@@ -7,29 +7,6 @@ import { certificationCandidatesController } from './certification-candidates-co
 const register = async function (server) {
   server.route([
     {
-      method: 'POST',
-      path: '/api/certification-candidates/{id}/authorize-to-resume',
-      config: {
-        validate: {
-          params: Joi.object({
-            id: identifiersType.certificationCandidateId,
-          }),
-        },
-        pre: [
-          {
-            method: assessmentSupervisorAuthorization.verifyByCertificationCandidateId,
-            assign: 'authorizationCheck',
-          },
-        ],
-        handler: certificationCandidatesController.authorizeToResume,
-        notes: [
-          '- **Cette route est restreinte aux utilisateurs authentifiés**\n' +
-            '- Autoriser la reprise du test par le candidat',
-        ],
-        tags: ['api', 'certification-candidates'],
-      },
-    },
-    {
       method: 'PATCH',
       path: '/api/certification-candidates/{id}/end-assessment-by-supervisor',
       config: {
--- api/src/certification/session-management/application/certification-candidate-route.js
+++ api/src/certification/session-management/application/certification-candidate-route.js
@@ -35,6 +35,29 @@ const register = async function (server) {
         tags: ['api', 'certification-candidates'],
       },
     },
+    {
+      method: 'POST',
+      path: '/api/certification-candidates/{id}/authorize-to-resume',
+      config: {
+        validate: {
+          params: Joi.object({
+            id: identifiersType.certificationCandidateId,
+          }),
+        },
+        pre: [
+          {
+            method: assessmentSupervisorAuthorization.verifyByCertificationCandidateId,
+            assign: 'authorizationCheck',
+          },
+        ],
+        handler: certificationCandidateController.authorizeToResume,
+        notes: [
+          '- **Cette route est restreinte aux utilisateurs authentifiés**\n' +
+            '- Autoriser la reprise du test par le candidat',
+        ],
+        tags: ['api', 'certification-candidates'],
+      },
+    },
   ]);
 };
 
