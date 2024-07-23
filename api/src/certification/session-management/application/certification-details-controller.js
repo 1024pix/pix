bff0775321cc089bf48fbe3f3943745db08ba4d9
Alexandre COIN <alexandre.pc.coin@gmail.com>
2024-07-23 14:38:41 +0200

    :truck: api: move certification details serializer to src

--- api/src/certification/session-management/application/certification-details-controller.js
+++ api/src/certification/session-management/application/certification-details-controller.js
@@ -1,5 +1,5 @@
-import * as certificationDetailsSerializer from '../../../../lib/infrastructure/serializers/jsonapi/certification-details-serializer.js';
 import { usecases } from '../domain/usecases/index.js';
+import * as certificationDetailsSerializer from '../infrastructure/serializers/certification-details-serializer.js';
 
 const getCertificationDetails = async function (request, h, dependencies = { certificationDetailsSerializer }) {
   const certificationCourseId = request.params.id;
