72675d24688c699c45c2f3560a4d193d153399a6
Alexandre COIN <alexandre.pc.coin@gmail.com>
2024-07-24 14:51:58 +0200

    :truck: api: move cancel certif course usecase to src

--- api/tests/certification/session-management/unit/domain/usecases/cancel-certification-course_test.js
+++ api/tests/certification/session-management/unit/domain/usecases/cancel-certification-course_test.js
@@ -1,7 +1,7 @@
-import { cancelCertificationCourse } from '../../../../lib/domain/usecases/cancel-certification-course.js';
-import { domainBuilder, expect, sinon } from '../../../test-helper.js';
+import { cancelCertificationCourse } from '../../../../../../src/certification/session-management/domain/usecases/cancel-certification-course.js';
+import { domainBuilder, expect, sinon } from '../../../../../test-helper.js';
 
-describe('Unit | UseCase | cancel-certification-course', function () {
+describe('Certification | Session-management | Unit | Domain | UseCases | cancel-certification-course', function () {
   it('should cancel the certification course', async function () {
     // given
     const certificationCourse = domainBuilder.buildCertificationCourse({ id: 123 });
