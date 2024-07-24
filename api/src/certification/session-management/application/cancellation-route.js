b1e4a34f15704e830505ecddd0abf91621a3ff55
Alexandre COIN <alexandre.pc.coin@gmail.com>
2024-07-24 15:17:36 +0200

    :truck: api: move cancel certif route to src

---
+++ api/src/certification/session-management/application/cancellation-route.js
@@ -0,0 +1,37 @@
+import Joi from 'joi';
+
+import { securityPreHandlers } from '../../../shared/application/security-pre-handlers.js';
+import { identifiersType } from '../../../shared/domain/types/identifiers-type.js';
+import { cancellationController } from './cancellation-controller.js';
+
+const register = async function (server) {
+  server.route([
+    {
+      method: 'POST',
+      path: '/api/admin/certification-courses/{id}/cancel',
+      config: {
+        validate: {
+          params: Joi.object({
+            id: identifiersType.certificationCourseId,
+          }),
+        },
+        pre: [
+          {
+            method: (request, h) =>
+              securityPreHandlers.hasAtLeastOneAccessOf([
+                securityPreHandlers.checkAdminMemberHasRoleSuperAdmin,
+                securityPreHandlers.checkAdminMemberHasRoleCertif,
+                securityPreHandlers.checkAdminMemberHasRoleSupport,
+              ])(request, h),
+            assign: 'hasAuthorizationToAccessAdminScope',
+          },
+        ],
+        handler: cancellationController.cancel,
+        tags: ['api'],
+      },
+    },
+  ]);
+};
+
+const name = 'cancellation-api';
+export { name, register };
---
+++ api/tests/certification/session-management/unit/application/cancellation-route_test.js
@@ -0,0 +1,48 @@
+import { cancellationController } from '../../../../../src/certification/session-management/application/cancellation-controller.js';
+import * as moduleUnderTest from '../../../../../src/certification/session-management/application/cancellation-route.js';
+import { securityPreHandlers } from '../../../../../src/shared/application/security-pre-handlers.js';
+import { expect, HttpTestServer, sinon } from '../../../../test-helper.js';
+
+describe('Certification | Session-management | Unit | Application | cancellation-route', function () {
+  describe('POST /api/admin/certification-courses/{id}/cancel', function () {
+    it('return forbidden access if user has METIER role', async function () {
+      // given
+      sinon
+        .stub(securityPreHandlers, 'hasAtLeastOneAccessOf')
+        .withArgs([
+          securityPreHandlers.checkAdminMemberHasRoleSuperAdmin,
+          securityPreHandlers.checkAdminMemberHasRoleCertif,
+          securityPreHandlers.checkAdminMemberHasRoleSupport,
+        ])
+        .callsFake(
+          () => (request, h) =>
+            h
+              .response({ errors: new Error('forbidden') })
+              .code(403)
+              .takeover(),
+        );
+      const httpTestServer = new HttpTestServer();
+      await httpTestServer.register(moduleUnderTest);
+
+      // when
+      const response = await httpTestServer.request('POST', '/api/admin/certification-courses/1/cancel');
+
+      // then
+      expect(response.statusCode).to.equal(403);
+    });
+
+    it('should call handler when user is ', async function () {
+      // given
+      sinon.stub(securityPreHandlers, 'hasAtLeastOneAccessOf').returns(() => true);
+      sinon.stub(cancellationController, 'cancel').returns('ok');
+      const httpTestServer = new HttpTestServer();
+      await httpTestServer.register(moduleUnderTest);
+
+      // when
+      await httpTestServer.request('POST', '/api/admin/certification-courses/1/cancel');
+
+      // then
+      expect(cancellationController.cancel).to.have.been.calledOnce;
+    });
+  });
+});
