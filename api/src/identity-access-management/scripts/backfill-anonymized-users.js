55fc94b27a58c307b89e60bdae064af245b78b0a
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2024-07-25 16:49:21 +0200

    feat(api): leverage usecase in backfill script
    Co-authored-by: LEGO Technix <109212476+lego-technix@users.noreply.github.com>
    Co-authored-by: Marianne Bost <marianne.bost@pix.fr>
    Co-authored-by: Benjamin Petetot <benjamin.petetot@pix.fr>

--- api/src/identity-access-management/scripts/backfill-anonymized-users.js
+++ api/src/identity-access-management/scripts/backfill-anonymized-users.js
@@ -1,7 +1,12 @@
 import * as url from 'node:url';
 
 import { disconnect } from '../../../db/knex-database-connection.js';
+import { usecases } from '../../../lib/domain/usecases/index.js';
+import { withTransaction } from '../../shared/domain/DomainTransaction.js';
+import { learningContentCache } from '../../shared/infrastructure/caches/learning-content-cache.js';
+import { temporaryStorage } from '../../shared/infrastructure/temporary-storage/index.js';
 import { logger } from '../../shared/infrastructure/utils/logger.js';
+import { anonymizedUserRepository } from '../infrastructure/repositories/anonymized-user.repository.js';
 
 const modulePath = url.fileURLToPath(import.meta.url);
 const isLaunchedFromCommandLine = process.argv[1] === modulePath;
@@ -13,10 +18,22 @@ if (isLaunchedFromCommandLine) {
     logger.error('\x1b[31mErreur : %s\x1b[0m', error.message);
     process.exitCode = 1;
   } finally {
-    disconnect();
+    await disconnect();
+    await learningContentCache.quit();
+    await temporaryStorage.quit();
   }
 }
 
 export async function backfillAnonymizedUsers() {
-  //
+  const anonymizedUserIds = await anonymizedUserRepository.findIds();
+
+  logger.info(`Total anonymized users to backfill: ${anonymizedUserIds.length}`);
+
+  let current = 1;
+  for (const anonymizedUserId of anonymizedUserIds) {
+    logger.info(`Backfill anonymized user ${current++}/${anonymizedUserIds.length}: "${anonymizedUserId}"`);
+    await withTransaction(usecases.anonymizeUser)({ userId: anonymizedUserId });
+  }
+
+  logger.info(`Anonymized users backfill finished.`);
 }
--- api/tests/identity-access-management/acceptance/scripts/backfill-anonymized-users.script.test.js
+++ api/tests/identity-access-management/acceptance/scripts/backfill-anonymized-users.script.test.js
@@ -4,14 +4,29 @@ import { databaseBuilder, expect, knex } from '../../../test-helper.js';
 describe('Acceptance | Identity Access Management | Scripts | backfill-anonymized-users', function () {
   it('backfills the anonymized users', async function () {
     // given
-    const oldAnonymizedUser = databaseBuilder.factory.buildUser({ hasBeenAnonymised: true, firstName: 'Jane' });
+    const admin = databaseBuilder.factory.buildUser.withRole();
+    const legacyAnonymizedUser = databaseBuilder.factory.buildUser({
+      firstName: 'Jane',
+      hasBeenAnonymised: true,
+      hasBeenAnonymisedBy: admin.id,
+    });
+    const userNotAnonymized = databaseBuilder.factory.buildUser({
+      firstName: 'Jack',
+      hasBeenAnonymised: false,
+    });
     await databaseBuilder.commit();
 
     // when
     await backfillAnonymizedUsers();
 
     // then
-    const anonymizedUser = await knex('users').where({ id: oldAnonymizedUser.id }).first();
+    const anonymizedUser = await knex('users').where({ id: legacyAnonymizedUser.id }).first();
     expect(anonymizedUser.firstName).to.be.equal('(anonymised)');
+    expect(anonymizedUser.hasBeenAnonymised).to.be.true;
+    expect(anonymizedUser.hasBeenAnonymisedBy).to.be.equal(admin.id);
+
+    const notAnonymizedUser = await knex('users').where({ id: userNotAnonymized.id }).first();
+    expect(notAnonymizedUser.firstName).to.be.equal('Jack');
+    expect(notAnonymizedUser.hasBeenAnonymised).to.be.false;
   });
 });
