589790f17c3c48fbef09da7e55067cce9e9eb9c7
Andreia Pena <andreia.pena@pix.fr>
2024-07-25 18:02:17 +0200

    🚚 api: [CERTIF] move constants to certification/shared context

--- api/tests/certification/enrolment/unit/domain/usecases/validate-sessions_test.js
+++ api/tests/certification/enrolment/unit/domain/usecases/validate-sessions_test.js
@@ -1,8 +1,8 @@
-import { CERTIFICATION_SESSIONS_ERRORS } from '../../../../../../lib/domain/constants/sessions-errors.js';
 import { SessionEnrolment } from '../../../../../../src/certification/enrolment/domain/models/SessionEnrolment.js';
 import { SessionMassImportReport } from '../../../../../../src/certification/enrolment/domain/models/SessionMassImportReport.js';
 import { validateSessions } from '../../../../../../src/certification/enrolment/domain/usecases/validate-sessions.js';
 import { CERTIFICATION_CANDIDATES_ERRORS } from '../../../../../../src/certification/shared/domain/constants/certification-candidates-errors.js';
+import { CERTIFICATION_SESSIONS_ERRORS } from '../../../../../../src/certification/shared/domain/constants/sessions-errors.js';
 import { CpfBirthInformationValidation } from '../../../../../../src/certification/shared/domain/services/certification-cpf-service.js';
 import { domainBuilder, expect, sinon } from '../../../../../test-helper.js';
 import { getI18n } from '../../../../../tooling/i18n/i18n.js';
