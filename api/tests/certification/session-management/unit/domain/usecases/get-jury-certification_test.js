a24ea0b9f388007660981b9e1947e33accb99bd2
Alexandre COIN <alexandre.pc.coin@gmail.com>
2024-07-26 10:31:12 +0200

    :truck: api: move get jury certification usecase to src

--- api/tests/certification/session-management/unit/domain/usecases/get-jury-certification_test.js
+++ api/tests/certification/session-management/unit/domain/usecases/get-jury-certification_test.js
@@ -1,7 +1,7 @@
-import { getJuryCertification } from '../../../../lib/domain/usecases/get-jury-certification.js';
-import { domainBuilder, expect, sinon } from '../../../test-helper.js';
+import { getJuryCertification } from '../../../../../../src/certification/session-management/domain/usecases/get-jury-certification.js';
+import { domainBuilder, expect, sinon } from '../../../../../test-helper.js';
 
-describe('Unit | Usecase | get-jury-certification', function () {
+describe('Certification | Session-management | Unit | Domain | Usecases | get-jury-certification', function () {
   it('should return the jury certification', async function () {
     // given
     const expectedJuryCertification = domainBuilder.buildJuryCertification({ certificationCourseId: 777 });
