c75cf9334908fff193f8d8133b9686ef21a61eae
Benjamin Petetot <benjamin.petetot@pix.fr>
2024-07-30 13:52:14 +0200

    chore(audit-logger): upgrade vitest

--- audit-logger/vitest.config.ts
+++ audit-logger/vitest.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    globals: true,
     clearMocks: true,
     watch: false,
+    pool: 'threads',
+    poolOptions: { threads: { singleThread: true } },
   },
 });
