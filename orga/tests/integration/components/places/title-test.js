a45df2058f5e3a22112f1aef238b12e6f0221d84
Yvonnick Frin <frin.yvonnick@gmail.com>
2024-08-01 09:43:57 +0200

    fix(orga): fix test preventing CI to pass because of date issue

--- orga/tests/integration/components/places/title-test.js
+++ orga/tests/integration/components/places/title-test.js
@@ -3,11 +3,22 @@ import dayjs from 'dayjs';
 import { hbs } from 'ember-cli-htmlbars';
 import { t } from 'ember-intl/test-support';
 import { module, test } from 'qunit';
+import sinon from 'sinon';
 
 import setupIntlRenderingTest from '../../../helpers/setup-intl-rendering';
 
 module('Integration | Component | Places::Title', function (hooks) {
   setupIntlRenderingTest(hooks);
+
+  let clock;
+
+  hooks.beforeEach(function () {
+    clock = sinon.useFakeTimers({ now: new Date('2023-11-15') });
+  });
+
+  hooks.afterEach(function () {
+    clock.restore();
+  });
 
   test('it should display date of today', async function (assert) {
     // given
