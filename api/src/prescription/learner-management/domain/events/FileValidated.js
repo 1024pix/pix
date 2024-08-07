eb14398bf9496452e1bc6aea9437ef087a2b2e12
Yvonnick Frin <frin.yvonnick@gmail.com>
2024-08-07 16:26:35 +0200

    fix(api): save validate error in case of publish event fails

--- api/src/prescription/learner-management/domain/events/FileValidated.js
+++ api/src/prescription/learner-management/domain/events/FileValidated.js
@@ -1,6 +1,10 @@
 import { Event } from '../../../../../lib/domain/events/Event.js';
 
 export class FileValidated extends Event {
+  static create({ organizationImportId }) {
+    return new FileValidated({ organizationImportId });
+  }
+
   constructor({ organizationImportId }) {
     super();
     this.organizationImportId = organizationImportId;
