9fe50dac64047902c59305bee430fbbae9f8fbff
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2024-08-13 14:15:06 +0200

    tech(api): clean unecessary file

--- api/src/shared/infrastructure/jobs/campaign-result/ParticipationResultCalculationJob.js
+++
@@ -1,9 +0,0 @@
-import { JobPgBoss } from '../JobPgBoss.js';
-
-class ParticipationResultCalculationJob extends JobPgBoss {
-  constructor(queryBuilder) {
-    super({ name: 'ParticipationResultCalculationJob', retryLimit: 3 }, queryBuilder);
-  }
-}
-
-export { ParticipationResultCalculationJob };
