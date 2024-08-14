2789780e6deb9ce861ac8ff16604b0b00751f908
Laura Bergoens <laura.bergoens@pix.fr>
2024-08-14 14:39:26 +0200

    just doing as the comment says !

--- certif/app/serializers/certification-candidate.js
+++
@@ -1,7 +0,0 @@
-import JSONAPISerializer from '@ember-data/serializer/json-api';
-
-export default class CertificationCandidateSerializer extends JSONAPISerializer {
-  // This bypasses extractErrors emberData behavior which relies on the property being a function.
-  // This should not be needed after upgrading to ember-data v5
-  extractErrors = false;
-}
