b99773ca443807fa8284b450bdd9e4a4c2f15f6f
Benjamin Petetot <benjamin.petetot@pix.fr>
2024-08-16 16:42:59 +0200

    feat(api): Add Chai assertions for jobs

--- api/tests/tooling/jobs/expect-job.js
+++ api/tests/tooling/jobs/expect-job.js
@@ -1,7 +1,38 @@
-import { knex } from '../../test-helper.js';
+import { assert, Assertion } from 'chai';
 
-function jobs(jobName) {
-  return knex('pgboss.job').where({ name: jobName });
-}
+export const jobChai = (knex) => (_chai, utils) => {
+  utils.addProperty(Assertion.prototype, 'performed', function () {
+    return this;
+  });
 
-export { jobs };
+  Assertion.addMethod('withJobsCount', async function (expectedCount) {
+    const jobName = this._obj;
+    const jobs = await knex('pgboss.job').where({ name: jobName });
+    assert.strictEqual(
+      jobs.length,
+      expectedCount,
+      `expected ${jobName} to have been performed ${expectedCount} times, but it was performed ${jobs.length} times`,
+    );
+  });
+
+  Assertion.addMethod('withJob', async function (jobData) {
+    await this.withJobsCount(1);
+
+    const jobName = this._obj;
+    const jobs = await knex('pgboss.job').select(knex.raw(`*, expirein::varchar`)).where({ name: jobName });
+    assert.deepInclude(jobs[0], jobData, `Job '${jobName}' was performed with a different payload`);
+  });
+
+  Assertion.addMethod('withJobPayloads', async function (payloads) {
+    await this.withJobsCount(payloads.length);
+
+    const jobName = this._obj;
+    const jobs = await knex('pgboss.job').where({ name: jobName });
+    const actualPayloads = jobs.map((job) => job.data);
+    assert.deepEqual(actualPayloads, payloads, `Job '${jobName}' was performed with a different payload`);
+  });
+
+  Assertion.addMethod('withJobPayload', async function (payload) {
+    await this.withJobPayloads([payload]);
+  });
+};
