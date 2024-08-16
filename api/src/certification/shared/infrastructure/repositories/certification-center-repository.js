fcd8e5a2eed7cbd13cbe9d103031571d28d75784
Guillaume Olejniczak <la.toile.cosmique@gmail.com>
2024-08-16 17:30:58 +0200

    wip

--- api/src/certification/shared/infrastructure/repositories/certification-center-repository.js
+++ api/src/certification/shared/infrastructure/repositories/certification-center-repository.js
@@ -1,97 +1,56 @@
-import _ from 'lodash';
-
 import { knex } from '../../../../../db/knex-database-connection.js';
-import { BookshelfCertificationCenter } from '../../../../../src/shared/infrastructure/orm-models/CertificationCenter.js';
+import { DomainTransaction } from '../../../../shared/domain/DomainTransaction.js';
 import { NotFoundError } from '../../../../shared/domain/errors.js';
 import { CertificationCenter } from '../../../../shared/domain/models/CertificationCenter.js';
 import { ComplementaryCertification } from '../../../complementary-certification/domain/models/ComplementaryCertification.js';
 
-function _toDomain(bookshelfCertificationCenter) {
-  const dbCertificationCenter = bookshelfCertificationCenter.toJSON();
-  const habilitations = _.map(dbCertificationCenter.habilitations, (dbComplementaryCertification) => {
+const CERTIFICATION_CENTERS_TABLE = 'certification-centers';
+const HABILITATION_TABLE = 'complementary-certification-habilitations';
+
+const toDomain = (certificationCenter, habilitations) => {
+  const complementaryCertifications = habilitations.map((complementaryCertification) => {
     return new ComplementaryCertification({
-      id: dbComplementaryCertification.id,
-      key: dbComplementaryCertification.key,
-      label: dbComplementaryCertification.label,
+      id: complementaryCertification.id,
+      key: complementaryCertification.key,
+      label: complementaryCertification.label,
     });
   });
   return new CertificationCenter({
-    ..._.pick(dbCertificationCenter, ['id', 'name', 'type', 'externalId', 'createdAt', 'updatedAt', 'isV3Pilot']),
-    habilitations,
+    ...certificationCenter,
+    habilitations: complementaryCertifications,
   });
-}
-
-/**
- *@deprecated implemented without bookshelf in {@link file://./../../../session-management/infrastructure/repositories/center-repository.js}
- * note that the new implementations does not provide the lazy loading on habilitations
- */
-const get = async function ({ id }) {
-  const certificationCenterBookshelf = await BookshelfCertificationCenter.query((q) => q.orderBy('id', 'desc'))
-    .where({ id })
-    .fetch({
-      require: false,
-      withRelated: [
-        {
-          habilitations: function (query) {
-            query.orderBy('id');
-          },
-        },
-      ],
-    });
-
-  if (certificationCenterBookshelf) {
-    return _toDomain(certificationCenterBookshelf);
-  }
-  throw new NotFoundError(`Certification center with id: ${id} not found`);
 };
 
-const getBySessionId = async function ({ sessionId }) {
-  const certificationCenterBookshelf = await BookshelfCertificationCenter.where({ 'sessions.id': sessionId })
-    .query((qb) => {
-      qb.innerJoin('sessions', 'sessions.certificationCenterId', 'certification-centers.id');
-    })
-    .fetch({
-      require: false,
-      withRelated: [
-        {
-          habilitations: function (query) {
-            query.orderBy('id');
-          },
-        },
-      ],
-    });
+export const getBySessionId = async function ({ sessionId }) {
+  const knexConnection = DomainTransaction.getConnection();
+  const certificationCenter = await knexConnection(CERTIFICATION_CENTERS_TABLE)
+    .innerJoin('sessions', 'sessions.certificationCenterId', 'certification-centers.id')
+    .where({ 'sessions.id': sessionId })
+    .first();
 
-  if (certificationCenterBookshelf) {
-    return _toDomain(certificationCenterBookshelf);
-  }
-  throw new NotFoundError(`Could not find certification center for sessionId ${sessionId}.`);
+  if (!certificationCenter) throw new NotFoundError(`Could not find certification center for sessionId ${sessionId}.`);
+
+  const habilitations = await knexConnection(HABILITATION_TABLE).where('certificationCenterId', certificationCenter.id);
+
+  return toDomain(certificationCenter, habilitations);
 };
 
-// @deprecated
-const save = async function ({ certificationCenter }) {
-  const cleanedCertificationCenter = _.omit(certificationCenter, ['createdAt', 'habilitations']);
-  const certificationCenterBookshelf = await new BookshelfCertificationCenter(cleanedCertificationCenter).save();
-  await certificationCenterBookshelf.related('habilitations').fetch();
-  return _toDomain(certificationCenterBookshelf);
+export const findByExternalId = async ({ externalId }) => {
+  const knexConnection = DomainTransaction.getConnection();
+
+  const certificationCenter = await knexConnection(CERTIFICATION_CENTERS_TABLE).where({ externalId }).first();
+
+  if (!certificationCenter) return null;
+
+  const habilitations = await knexConnection(HABILITATION_TABLE)
+    .where('certificationCenterId', certificationCenter.id)
+    .orderBy('id');
+
+  return toDomain(certificationCenter, habilitations);
 };
 
-const findByExternalId = async function ({ externalId }) {
-  const certificationCenterBookshelf = await BookshelfCertificationCenter.where({ externalId }).fetch({
-    require: false,
-    withRelated: [
-      {
-        habilitations: function (query) {
-          query.orderBy('id');
-        },
-      },
-    ],
-  });
-
-  return certificationCenterBookshelf ? _toDomain(certificationCenterBookshelf) : null;
-};
-
-const getRefererEmails = async function ({ id }) {
-  const refererEmails = await knex('certification-centers')
+export const getRefererEmails = async ({ id }) =>
+  await knex('certification-centers')
     .select('users.email')
     .join(
       'certification-center-memberships',
@@ -101,8 +60,3 @@ const getRefererEmails = async function ({ id }) {
     .join('users', 'users.id', 'certification-center-memberships.userId')
     .where('certification-centers.id', id)
     .where('certification-center-memberships.isReferer', true);
-
-  return refererEmails;
-};
-
-export { findByExternalId, get, getBySessionId, getRefererEmails, save };
