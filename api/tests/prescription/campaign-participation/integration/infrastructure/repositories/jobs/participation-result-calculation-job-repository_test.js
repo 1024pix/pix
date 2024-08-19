f7599c507c91ec44c82ae99405dee32e12c8d93d
Benjamin Petetot <benjamin.petetot@pix.fr>
2024-08-19 15:41:54 +0200

    refactor(api): Use job expectation util into participation result job test

--- api/tests/prescription/campaign-participation/integration/infrastructure/repositories/jobs/participation-result-calculation-job-repository_test.js
+++ api/tests/prescription/campaign-participation/integration/infrastructure/repositories/jobs/participation-result-calculation-job-repository_test.js
@@ -1,7 +1,6 @@
 import { ParticipationResultCalculationJob } from '../../../../../../../src/prescription/campaign-participation/domain/models/ParticipationResultCalculationJob.js';
 import { participationResultCalculationJobRepository } from '../../../../../../../src/prescription/campaign-participation/infrastructure/repositories/jobs/participation-result-calculation-job-repository.js';
 import { expect } from '../../../../../../test-helper.js';
-import { jobs } from '../../../../../../tooling/jobs/expect-job.js';
 
 describe('Integration | Repository | Jobs | ParticipationResultCalculationJobRepository', function () {
   describe('#performAsync', function () {
@@ -10,9 +9,8 @@ describe('Integration | Repository | Jobs | ParticipationResultCalculationJobRep
       await participationResultCalculationJobRepository.performAsync({ campaignParticipationId: 3 });
 
       // then
-      const results = await jobs(ParticipationResultCalculationJob.name);
-      expect(results).to.have.lengthOf(1);
-      expect(results[0]).to.deep.contains({
+      await expect(ParticipationResultCalculationJob.name).to.have.been.performed.withJob({
+        name: ParticipationResultCalculationJob.name,
         retrylimit: 10,
         retrydelay: 30,
         retrybackoff: true,
