de968ae44e3280cfef38533c6068a63b1c604579
Mathieu Gilet <mathieu.gilet@pix.fr>
2024-08-19 11:20:29 +0200

    tech(api): add property name to ValidateOrganizationImportFileJobHandler

--- api/src/prescription/learner-management/infrastructure/jobs/ValidateOrganizationImportFileJobHandler.js
+++ api/src/prescription/learner-management/infrastructure/jobs/ValidateOrganizationImportFileJobHandler.js
@@ -1,10 +1,15 @@
 import { usecases } from '../../domain/usecases/index.js';
+import { ValidateOrganizationImportFileJob } from './ValidateOrganizationImportFileJob.js';
 
 class ValidateOrganizationImportFileJobHandler {
   async handle(event) {
     const { organizationImportId } = event;
 
     await usecases.validateSiecleXmlFile({ organizationImportId });
+  }
+
+  get name() {
+    return ValidateOrganizationImportFileJob.name;
   }
 }
 
--- api/tests/prescription/learner-management/unit/infrastructure/jobs/ValidateOrganizationImportFileJobHandler_test.js
+++ api/tests/prescription/learner-management/unit/infrastructure/jobs/ValidateOrganizationImportFileJobHandler_test.js
@@ -1,4 +1,5 @@
 import { usecases } from '../../../../../../src/prescription/learner-management/domain/usecases/index.js';
+import { ValidateOrganizationImportFileJob } from '../../../../../../src/prescription/learner-management/infrastructure/jobs/ValidateOrganizationImportFileJob.js';
 import { ValidateOrganizationImportFileJobHandler } from '../../../../../../src/prescription/learner-management/infrastructure/jobs/ValidateOrganizationImportFileJobHandler.js';
 import { expect, sinon } from '../../../../../test-helper.js';
 
@@ -13,6 +14,14 @@ describe('Unit | Handler | ValidateOrganizationImportFileJobHandler', function (
 
     expect(usecases.validateSiecleXmlFile).to.have.been.calledWithExactly({
       organizationImportId: event.organizationImportId,
+    });
+  });
+
+  describe('#name', function () {
+    it('should return the name of the job', function () {
+      const handler = new ValidateOrganizationImportFileJobHandler();
+
+      expect(handler.name).to.equal(ValidateOrganizationImportFileJob.name);
     });
   });
 });
