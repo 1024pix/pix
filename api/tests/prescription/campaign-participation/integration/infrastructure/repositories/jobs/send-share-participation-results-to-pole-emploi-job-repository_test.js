13258a3f6670f8e254a40c40e5aa9ee6e7a99901
Benjamin Petetot <benjamin.petetot@pix.fr>
2024-08-19 15:42:29 +0200

    refactor(api): Use job expectation util into send share participation result job test

--- api/tests/prescription/campaign-participation/integration/infrastructure/repositories/jobs/send-share-participation-results-to-pole-emploi-job-repository_test.js
+++ api/tests/prescription/campaign-participation/integration/infrastructure/repositories/jobs/send-share-participation-results-to-pole-emploi-job-repository_test.js
@@ -1,7 +1,6 @@
 import { SendSharedParticipationResultsToPoleEmploiJob } from '../../../../../../../src/prescription/campaign-participation/domain/models/SendSharedParticipationResultsToPoleEmploiJob.js';
 import { sendSharedParticipationResultsToPoleEmploiJobRepository } from '../../../../../../../src/prescription/campaign-participation/infrastructure/repositories/jobs/send-share-participation-results-to-pole-emploi-job-repository.js';
 import { expect } from '../../../../../../test-helper.js';
-import { jobs } from '../../../../../../tooling/jobs/expect-job.js';
 
 describe('Integration | Repository | Jobs| SendSharedParticipationResultsToPoleEmploiJobRepository', function () {
   describe('#performAsync', function () {
@@ -10,9 +9,9 @@ describe('Integration | Repository | Jobs| SendSharedParticipationResultsToPoleE
       await sendSharedParticipationResultsToPoleEmploiJobRepository.performAsync({ campaignParticipationId: 2 });
 
       // then
-      const results = await jobs(SendSharedParticipationResultsToPoleEmploiJob.name);
-      expect(results).to.have.lengthOf(1);
-      expect(results[0]).to.deep.contains({
+
+      await expect(SendSharedParticipationResultsToPoleEmploiJob.name).to.have.been.performed.withJob({
+        name: SendSharedParticipationResultsToPoleEmploiJob.name,
         retrylimit: 0,
         retrydelay: 0,
         retrybackoff: false,
