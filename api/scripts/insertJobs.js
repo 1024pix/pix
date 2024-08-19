1661b13e298b9c8be44c9b2efcacc86084d21c99
Yvonnick Frin <frin.yvonnick@gmail.com>
2024-08-19 11:43:34 +0200

    Make insertJobs use params

--- api/scripts/insertJobs.js
+++ api/scripts/insertJobs.js
@@ -1,17 +1,31 @@
+import lodash from 'lodash';
+
 import { knex } from '../db/knex-database-connection.js';
-import { JobPgBoss } from '../src/shared/infrastructure/jobs/JobPgBoss.js';
 
-const JOBS_COUNT = 50000;
+const { range } = lodash;
 
-for (let i = 0; i < JOBS_COUNT; i++) {
-  const job = new JobPgBoss(
-    {
-      name: 'answer',
-    },
-    knex,
-  );
+const JOBS_COUNT = process.argv[2];
+const USER_ID = process.argv[3];
+const MAX = process.argv[4];
 
-  await job.schedule({
-    userId: 107371,
-  });
-}
+let seconds = 1;
+const max = MAX;
+const intervalId = setInterval(async () => {
+  console.log('batch: ' + seconds);
+
+  const jobs = range(Math.ceil(JOBS_COUNT / max)).map(() => ({
+    name: 'answer',
+    data: { userId: USER_ID },
+    on_complete: true,
+  }));
+
+  await knex.batchInsert('pgboss.job', jobs);
+
+  seconds++;
+
+  if (seconds > max) {
+    console.log('exit');
+    clearInterval(intervalId);
+    return process.exit();
+  }
+}, 1000);
