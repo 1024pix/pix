d7002cc6a09b087f87cef298424822c354555b24
P-Jeremy <jemyplu@gmail.com>
2024-08-20 14:28:41 +0200

    :recycle: migrate list items in component gjs

--- admin/app/components/certification-centers/list-items.js
+++
@@ -1,8 +0,0 @@
-import Component from '@glimmer/component';
-
-export default class CertificationCenterListItems extends Component {
-  searchedId = this.args.id;
-  searchedName = this.args.name;
-  searchedType = this.args.type;
-  searchedExternalId = this.args.externalId;
-}
