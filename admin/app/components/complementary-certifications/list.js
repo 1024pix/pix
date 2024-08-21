fb55280f86fe5d6c933092ea0ff2839f5cb37db1
Marianne Bost <marianne.bost@pix.fr>
2024-08-21 11:21:14 +0200

    refactor(admin): migrate complementary certifications list into gjs

--- admin/app/components/complementary-certifications/list.js
+++
@@ -1,7 +0,0 @@
-import Component from '@glimmer/component';
-
-export default class List extends Component {
-  get sortedComplementaryCertifications() {
-    return this.args.complementaryCertifications.sortBy('label');
-  }
-}
