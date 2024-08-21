430ee6bbf64172597b3cf4805a5fa196f04a464e
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2024-08-21 11:27:55 +0200

    feat(api): add test on isJobEnabled

--- api/tests/prescription/learner-management/unit/application/jobs/validate-organization-learners-import-file-job-controller_test.js
+++ api/tests/prescription/learner-management/unit/application/jobs/validate-organization-learners-import-file-job-controller_test.js
@@ -1,8 +1,32 @@
 import { ValidateOrganizationLearnersImportFileJobController } from '../../../../../../src/prescription/learner-management/application/jobs/validate-organization-learners-import-file-job-controller.js';
 import { usecases } from '../../../../../../src/prescription/learner-management/domain/usecases/index.js';
+import { config } from '../../../../../../src/shared/config.js';
 import { expect, sinon } from '../../../../../test-helper.js';
 
 describe('Unit | Prescription | Application | Jobs | validateOrganizationLearnersImportFileJobController', function () {
+  describe('#isJobEnabled', function () {
+    it('return true when job is enabled', function () {
+      //given
+      sinon.stub(config.pgBoss, 'validationFileJobEnabled').value(true);
+
+      // when
+      const handler = new ValidateOrganizationLearnersImportFileJobController();
+
+      // then
+      expect(handler.isJobEnabled()).to.be.true;
+    });
+
+    it('return false when job is disabled', function () {
+      //given
+      sinon.stub(config.pgBoss, 'validationFileJobEnabled').value(false);
+
+      //when
+      const handler = new ValidateOrganizationLearnersImportFileJobController();
+
+      //then
+      expect(handler.isJobEnabled()).to.be.false;
+    });
+  });
 
   describe('#handle', function () {
     it('should call usecase', async function () {
