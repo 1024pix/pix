dc09dc9e910669759d180031d7d0720ae3aa6482
P-Jeremy <jemyplu@gmail.com>
2024-08-23 10:39:46 +0200

    :recycle: admin: remove member action from certification detail model
    Co-authored-by: Eithliu <iris.benoit@pix.fr>
    Co-authored-by: Marianne Bost <marianne.bost@pix.fr>

--- admin/app/controllers/authenticated/certifications/certification/neutralization.js
+++ admin/app/controllers/authenticated/certifications/certification/neutralization.js
@@ -12,9 +12,12 @@ export default class NeutralizationController extends Controller {
   @action
   async neutralize(challengeRecId, questionIndex) {
     try {
-      await this.certificationDetails.neutralizeChallenge({
-        certificationCourseId: this.certificationDetails.id,
-        challengeRecId,
+      await this.certificationDetails.save({
+        adapterOptions: {
+          isNeutralizeChallenge: true,
+          certificationCourseId: this.certificationDetails.id,
+          challengeRecId,
+        },
       });
       this._updateModel(challengeRecId, true);
       return this.notifications.success(`La question n°${questionIndex} a été neutralisée avec succès.`);
@@ -28,9 +31,12 @@ export default class NeutralizationController extends Controller {
   @action
   async deneutralize(challengeRecId, questionIndex) {
     try {
-      await this.certificationDetails.deneutralizeChallenge({
-        certificationCourseId: this.certificationDetails.id,
-        challengeRecId,
+      await this.certificationDetails.save({
+        adapterOptions: {
+          isDeneutralizeChallenge: true,
+          certificationCourseId: this.certificationDetails.id,
+          challengeRecId,
+        },
       });
       this._updateModel(challengeRecId, false);
       return this.notifications.success(`La question n°${questionIndex} a été dé-neutralisée avec succès.`);
--- admin/tests/unit/adapters/certification-details-test.js
+++ admin/tests/unit/adapters/certification-details-test.js
@@ -1,5 +1,6 @@
 import { setupTest } from 'ember-qunit';
 import { module, test } from 'qunit';
+import sinon from 'sinon';
 
 module('Unit | Adapter | certification details', function (hooks) {
   setupTest(hooks);
@@ -15,14 +16,114 @@ module('Unit | Adapter | certification details', function (hooks) {
     });
   });
 
-  module('#buildURL', function () {
-    test('should build challenge neutralization base URL when called with according requestType', function (assert) {
+  module('#urlForNeutralizeChallenge', function () {
+    test('should build url for challenge neutralization', function (assert) {
       // when
       const adapter = this.owner.lookup('adapter:certification-details');
-      const url = adapter.buildURL(123, 'certification-details', null, 'challenge-neutralization');
+      const url = adapter.urlForNeutralizeChallenge();
 
       // then
-      assert.ok(url.endsWith('/admin/certification/'));
+      assert.ok(url.endsWith('/admin/certification/neutralize-challenge'));
+    });
+  });
+
+  module('#urlForDeneutralizeChallenge', function () {
+    test('should build url for challenge deneutralization', function (assert) {
+      // when
+      const adapter = this.owner.lookup('adapter:certification-details');
+      const url = adapter.urlForDeneutralizeChallenge();
+
+      // then
+      assert.ok(url.endsWith('/admin/certification/deneutralize-challenge'));
+    });
+  });
+
+  module('#updateRecord', function () {
+    test('it should trigger an ajax call to neutralize a challenge', async function (assert) {
+      // given
+      const adapter = this.owner.lookup('adapter:certification-details');
+      sinon
+        .stub(adapter, 'urlForNeutralizeChallenge')
+        .returns('https://example.net/api/admin/certification/neutralize-challenge');
+      sinon.stub(adapter, 'ajax');
+      const store = Symbol();
+
+      // when
+      await adapter.updateRecord(
+        store,
+        { modelName: 'certification-details' },
+        {
+          id: 123,
+          adapterOptions: {
+            isNeutralizeChallenge: true,
+            certificationCourseId: 1234,
+            challengeRecId: 'challengeRecId',
+          },
+        },
+      );
+
+      const expectedPayload = {
+        data: {
+          data: {
+            attributes: {
+              certificationCourseId: 1234,
+              challengeRecId: 'challengeRecId',
+            },
+          },
+        },
+      };
+
+      // then
+      assert.ok(
+        adapter.ajax.calledWith(
+          'https://example.net/api/admin/certification/neutralize-challenge',
+          'POST',
+          expectedPayload,
+        ),
+      );
+    });
+    test('it should trigger an ajax call to deneutralize a challenge', async function (assert) {
+      // given
+      const adapter = this.owner.lookup('adapter:certification-details');
+      sinon
+        .stub(adapter, 'urlForDeneutralizeChallenge')
+        .returns('https://example.net/api/admin/certification/deneutralize-challenge');
+      sinon.stub(adapter, 'ajax');
+      const store = Symbol();
+
+      // when
+      await adapter.updateRecord(
+        store,
+        { modelName: 'certification-details' },
+        {
+          id: 123,
+          adapterOptions: {
+            isDeneutralizeChallenge: true,
+            certificationCourseId: 1234,
+            challengeRecId: 'challengeRecId',
+          },
+        },
+      );
+
+      const expectedPayload = {
+        data: {
+          data: {
+            attributes: {
+              certificationCourseId: 1234,
+              challengeRecId: 'challengeRecId',
+            },
+          },
+        },
+      };
+
+      // then
+      assert.ok(
+        adapter.ajax.calledWith(
+          'https://example.net/api/admin/certification/deneutralize-challenge',
+          'POST',
+          expectedPayload,
+        ),
+      );
     });
   });
 });
