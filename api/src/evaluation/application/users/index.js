bb935e70e1f5b63fc69f72650d3ba66229927183
Andreia Pena <andreia.pena@pix.fr>
2024-08-27 14:35:50 +0200

    🚚 api: move level seven info route to evaluation context

---
+++ api/src/evaluation/application/users/index.js
@@ -0,0 +1,38 @@
+import Joi from 'joi';
+
+import { securityPreHandlers } from '../../../shared/application/security-pre-handlers.js';
+import { identifiersType } from '../../../shared/domain/types/identifiers-type.js';
+import { userController } from './user-controller.js';
+
+const register = async function (server) {
+  server.route([
+    {
+      method: 'PATCH',
+      path: '/api/users/{id}/user-has-seen-level-seven-info',
+      config: {
+        pre: [
+          {
+            method: securityPreHandlers.checkRequestedUserIsAuthenticatedUser,
+            assign: 'requestedUserIsAuthenticatedUser',
+          },
+        ],
+        validate: {
+          params: Joi.object({
+            id: identifiersType.userId,
+          }),
+        },
+        handler: userController.rememberUserHasSeenLevelSevenInfo,
+        notes: [
+          '- **Cette route est restreinte aux utilisateurs authentifiés**\n' +
+            "- Sauvegarde le fait que l'utilisateur ait vu le message d'information d'ouverture du niveau 7" +
+            '- L’id demandé doit correspondre à celui de l’utilisateur authentifié',
+          "- Le contenu de la requête n'est pas pris en compte.",
+        ],
+        tags: ['api', 'user'],
+      },
+    },
+  ]);
+};
+
+const name = 'evaluation-users-api';
+export { name, register };
---
+++ api/tests/evaluation/acceptance/application/users/user-controller_test.js
@@ -0,0 +1,71 @@
+import {
+  createServer,
+  databaseBuilder,
+  expect,
+  generateValidRequestAuthorizationHeader,
+} from '../../../../test-helper.js';
+
+describe('Acceptance | Route | Users ', function () {
+  let server;
+  let user;
+  let options;
+
+  describe('PATCH /api/users/{id}/user-has-seen-level-seven-info', function () {
+    async function givenUserHasSeenLevelSevenInfo(hasSeenLevelSevenInfo) {
+      server = await createServer();
+
+      user = databaseBuilder.factory.buildUser({ hasSeenLevelSevenInfo });
+
+      options = {
+        method: 'PATCH',
+        url: `/api/users/${user.id}/user-has-seen-level-seven-info`,
+        headers: { authorization: generateValidRequestAuthorizationHeader(user.id) },
+      };
+
+      await databaseBuilder.commit();
+    }
+
+    describe('Resource access management', function () {
+      beforeEach(async function () {
+        await givenUserHasSeenLevelSevenInfo(false);
+      });
+
+      it('should respond with a 401 - unauthorized access - if user is not authenticated', async function () {
+        options.headers.authorization = 'invalid.access.token';
+
+        const response = await server.inject(options);
+
+        expect(response.statusCode).to.equal(401);
+      });
+
+      it('should respond with a 403 - forbidden access - if requested user is not the same as authenticated user', async function () {
+        const otherUserId = 9999;
+        options.headers.authorization = generateValidRequestAuthorizationHeader(otherUserId);
+
+        const response = await server.inject(options);
+
+        expect(response.statusCode).to.equal(403);
+      });
+    });
+
+    describe('User has seen level seven info', function () {
+      it('should return the user with hasSeenLevelSevenInfo to true', async function () {
+        await givenUserHasSeenLevelSevenInfo(true);
+
+        const response = await server.inject(options);
+
+        expect(response.result.data.attributes['has-seen-level-seven-info']).to.be.true;
+      });
+    });
+
+    describe('User has not seen level seven info', function () {
+      it('should return the user with hasSeenLevelSevenInfo to true', async function () {
+        await givenUserHasSeenLevelSevenInfo(false);
+
+        const response = await server.inject(options);
+
+        expect(response.result.data.attributes['has-seen-level-seven-info']).to.be.true;
+      });
+    });
+  });
+});
