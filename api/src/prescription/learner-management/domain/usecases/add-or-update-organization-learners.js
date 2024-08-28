a4c2d16882edf5b95fe79fc4eded206fe8196887
Alexandre Monney <alexandre.monney@pix.fr>
2024-08-28 17:48:06 +0200

    feat(api): add logs on siecle import usecase

--- api/src/prescription/learner-management/domain/usecases/add-or-update-organization-learners.js
+++ api/src/prescription/learner-management/domain/usecases/add-or-update-organization-learners.js
@@ -5,6 +5,7 @@ import { DomainTransaction } from '../../../../../lib/infrastructure/DomainTrans
 import { ORGANIZATION_LEARNER_CHUNK_SIZE } from '../../../../shared/infrastructure/constants.js';
 import { SiecleParser } from '../../infrastructure/serializers/xml/siecle-parser.js';
 import { SiecleFileStreamer } from '../../infrastructure/utils/xml/siecle-file-streamer.js';
+import { loggerForImport } from '../../utils/loggerForImport.js';
 
 const { chunk } = lodash;
 
@@ -20,21 +21,32 @@ async function addOrUpdateOrganizationLearners({
   const organizationImport = await organizationImportRepository.get(organizationImportId);
 
   return DomainTransaction.execute(async () => {
+    loggerForImport('IMPORT_LOG -> <<addOrUpdateOrganizationLearners>> Debut du usecase');
     try {
       const readableStream = await importStorage.readFile({ filename: organizationImport.filename });
+      loggerForImport('IMPORT_LOG -> <<addOrUpdateOrganizationLearners>> Apres readableStream');
+
       const siecleFileStreamer = await SiecleFileStreamer.create(readableStream, organizationImport.encoding);
+      loggerForImport('IMPORT_LOG -> <<addOrUpdateOrganizationLearners>> Apres le siecleFileStreamer');
+
       const parser = SiecleParser.create(siecleFileStreamer);
+      loggerForImport('IMPORT_LOG -> <<addOrUpdateOrganizationLearners>> Aprés le parser');
 
       const organizationLearnerData = await parser.parse();
+      loggerForImport('IMPORT_LOG -> <<addOrUpdateOrganizationLearners>> Recuperation des OL datas');
 
       const organizationLearnersChunks = chunk(organizationLearnerData, chunkSize);
 
       const nationalStudentIdData = organizationLearnerData.map((learner) => learner.nationalStudentId);
+      loggerForImport('IMPORT_LOG -> <<addOrUpdateOrganizationLearners>> Recuperation des nationalStudentId');
 
       await organizationLearnerRepository.disableAllOrganizationLearnersInOrganization({
         organizationId: organizationImport.organizationId,
         nationalStudentIds: nationalStudentIdData,
       });
+      loggerForImport(
+        'IMPORT_LOG -> <<addOrUpdateOrganizationLearners>> Aprés la methode disableAllOrganizationLearnersInOrganization',
+      );
 
       await bluebird.mapSeries(organizationLearnersChunks, (chunk) => {
         return organizationLearnerRepository.addOrUpdateOrganizationOfOrganizationLearners(
@@ -42,17 +54,25 @@ async function addOrUpdateOrganizationLearners({
           organizationImport.organizationId,
         );
       });
+      loggerForImport(
+        'IMPORT_LOG -> <<addOrUpdateOrganizationLearners>> Aprés la methode addOrUpdateOrganizationOfOrganizationLearners',
+      );
     } catch (error) {
       errors.push(error);
       throw error;
     } finally {
+      loggerForImport('IMPORT_LOG -> <<addOrUpdateOrganizationLearners>> Debut du finally');
+
       organizationImport.process({ errors });
       await organizationImportRepository.save(organizationImport);
+      loggerForImport("IMPORT_LOG -> <<addOrUpdateOrganizationLearners>> Aprés le save de l'organizationImport");
+
       try {
         await importStorage.deleteFile({ filename: organizationImport.filename });
       } catch (e) {
         logErrorWithCorrelationIds(e);
       }
+      loggerForImport('IMPORT_LOG -> <<addOrUpdateOrganizationLearners>> Fin du usecase');
     }
   });
 }
