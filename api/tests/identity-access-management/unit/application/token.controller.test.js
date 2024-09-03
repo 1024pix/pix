9ae9e8f337ab573794348bf28707e9dfded05f4c
Benjamin Petetot <benjamin.petetot@pix.fr>
2024-09-03 10:50:47 +0200

    feat(api): pass scope through /api/token controller

--- api/tests/identity-access-management/unit/application/token.controller.test.js
+++ api/tests/identity-access-management/unit/application/token.controller.test.js
@@ -27,8 +27,8 @@ describe('Unit | Identity Access Management | Application | Controller | Token',
   });
 
   describe('#createToken', function () {
+    const userId = 1;
     const accessToken = 'jwt.access.token';
-    const USER_ID = 1;
     const username = 'user@email.com';
     const password = 'user_password';
     const scope = 'pix-orga';
@@ -37,59 +37,14 @@ describe('Unit | Identity Access Management | Application | Controller | Token',
     /**
      * @see https://www.oauth.com/oauth2-servers/access-tokens/access-token-response/
      */
-    it('returns an OAuth 2 token response (even if we do not really implement OAuth 2 authorization protocol)', async function () {
-      // given
-      const expirationDelaySeconds = 6666;
-      const refreshToken = 'refresh.token';
-      const request = {
-        headers: {
-          'content-type': 'application/x-www-form-urlencoded',
-        },
-        payload: {
-          grant_type: 'password',
-          username,
-          password,
-          scope,
-        },
-      };
-
-      sinon.stub(usecases, 'authenticateUser').resolves({ accessToken, refreshToken, expirationDelaySeconds });
-
-      const tokenServiceStub = { extractUserId: sinon.stub() };
-      tokenServiceStub.extractUserId.withArgs(accessToken).returns(USER_ID);
-
-      const dependencies = { tokenService: tokenServiceStub };
-
-      // when
-      const response = await tokenController.createToken(request, hFake, dependencies);
-
-      // then
-      const expectedResponseResult = {
-        token_type: 'bearer',
-        access_token: accessToken,
-        user_id: USER_ID,
-        refresh_token: refreshToken,
-        expires_in: expirationDelaySeconds,
-      };
-      expect(response.source).to.deep.equal(expectedResponseResult);
-      expect(response.statusCode).to.equal(200);
-      expect(response.headers).to.deep.equal({
-        'Content-Type': 'application/json;charset=UTF-8',
-        'Cache-Control': 'no-store',
-        Pragma: 'no-cache',
-      });
-    });
-
-    context('when there is a locale cookie', function () {
-      it('retrieves the value from the cookie and pass it as an argument to the use case', async function () {
+    context('when the user authenticates (grant_type: password)', function () {
+      it('returns an OAuth 2 token response (even if we do not really implement OAuth 2 authorization protocol)', async function () {
         // given
-        const expirationDelaySeconds = 777;
-        const refreshToken = 'cookie.refresh.token';
+        const expirationDelaySeconds = 6666;
+        const refreshToken = 'refresh.token';
         const localeFromCookie = 'fr-FR';
         const request = {
-          headers: {
-            'content-type': 'application/x-www-form-urlencoded',
-          },
+          headers: { 'content-type': 'application/x-www-form-urlencoded' },
           payload: {
             grant_type: 'password',
             username,
@@ -100,22 +55,77 @@ describe('Unit | Identity Access Management | Application | Controller | Token',
             locale: localeFromCookie,
           },
         };
-        sinon.stub(usecases, 'authenticateUser').resolves({ accessToken, refreshToken, expirationDelaySeconds });
+
+        sinon
+          .stub(usecases, 'authenticateUser')
+          .withArgs({ username, password, scope, source, localeFromCookie })
+          .resolves({ accessToken, refreshToken, expirationDelaySeconds });
+
         const tokenServiceStub = { extractUserId: sinon.stub() };
-        tokenServiceStub.extractUserId.withArgs(accessToken).returns(USER_ID);
+        tokenServiceStub.extractUserId.withArgs(accessToken).returns(userId);
 
         const dependencies = { tokenService: tokenServiceStub };
 
         // when
-        await tokenController.createToken(request, hFake, dependencies);
+        const response = await tokenController.createToken(request, hFake, dependencies);
 
         // then
-        expect(usecases.authenticateUser).to.have.been.calledWithExactly({
-          username,
-          password,
+        const expectedResponseResult = {
+          token_type: 'bearer',
+          access_token: accessToken,
+          user_id: userId,
+          refresh_token: refreshToken,
+          expires_in: expirationDelaySeconds,
           scope,
-          source,
-          localeFromCookie,
+        };
+        expect(response.source).to.deep.equal(expectedResponseResult);
+        expect(response.statusCode).to.equal(200);
+        expect(response.headers).to.deep.equal({
+          'Content-Type': 'application/json;charset=UTF-8',
+          'Cache-Control': 'no-store',
+          Pragma: 'no-cache',
+        });
+      });
+    });
+
+    context('when the user refresh its token (grant_type: refresh_token)', function () {
+      it('returns an OAuth 2 token response', async function () {
+        // given
+        const expirationDelaySeconds = 6666;
+        const refreshToken = 'refresh.token';
+        const request = {
+          headers: { 'content-type': 'application/x-www-form-urlencoded' },
+          payload: { grant_type: 'refresh_token', refresh_token: refreshToken, scope },
+        };
+
+        sinon
+          .stub(usecases, 'createAccessTokenFromRefreshToken')
+          .withArgs({ refreshToken })
+          .resolves({ accessToken, expirationDelaySeconds });
+
+        const tokenServiceStub = { extractUserId: sinon.stub() };
+        tokenServiceStub.extractUserId.withArgs(accessToken).returns(userId);
+
+        const dependencies = { tokenService: tokenServiceStub };
+
+        // when
+        const response = await tokenController.createToken(request, hFake, dependencies);
+
+        // then
+        const expectedResponseResult = {
+          token_type: 'bearer',
+          access_token: accessToken,
+          user_id: userId,
+          refresh_token: refreshToken,
+          expires_in: expirationDelaySeconds,
+          scope,
+        };
+        expect(response.source).to.deep.equal(expectedResponseResult);
+        expect(response.statusCode).to.equal(200);
+        expect(response.headers).to.deep.equal({
+          'Content-Type': 'application/json;charset=UTF-8',
+          'Cache-Control': 'no-store',
+          Pragma: 'no-cache',
         });
       });
     });
