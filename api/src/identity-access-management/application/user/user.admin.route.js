a097bd3931dc3ef34e70b651b74ea69bb20bc4ce
Eric Lim <eric.lim@zenika.com>
2024-09-03 13:45:50 +0200

    refactor(api) move route config in src
    for GET /api/admin/users route

--- api/src/identity-access-management/application/user/user.admin.route.js
+++ api/src/identity-access-management/application/user/user.admin.route.js
@@ -1,5 +1,6 @@
 import Joi from 'joi';
 
+import { userController } from '../../../../lib/application/users/user-controller.js';
 import { securityPreHandlers } from '../../../shared/application/security-pre-handlers.js';
 import { identifiersType } from '../../../shared/domain/types/identifiers-type.js';
 import { userAdminController } from './user.admin.controller.js';
@@ -26,6 +27,48 @@ export const userAdminRoutes = [
       handler: userAdminController.unblockUserAccount,
       notes: ["- Permet à un administrateur de débloquer le compte d'un utilisateur"],
       tags: ['api', 'user-account', 'admin'],
+    },
+  },
+  {
+    method: 'GET',
+    path: '/api/admin/users',
+    config: {
+      pre: [
+        {
+          method: (request, h) =>
+            securityPreHandlers.hasAtLeastOneAccessOf([
+              securityPreHandlers.checkAdminMemberHasRoleSuperAdmin,
+              securityPreHandlers.checkAdminMemberHasRoleCertif,
+              securityPreHandlers.checkAdminMemberHasRoleSupport,
+              securityPreHandlers.checkAdminMemberHasRoleMetier,
+            ])(request, h),
+        },
+      ],
+      validate: {
+        options: {
+          allowUnknown: true,
+        },
+        query: Joi.object({
+          filter: Joi.object({
+            id: identifiersType.userId.empty('').allow(null).optional(),
+            firstName: Joi.string().empty('').allow(null).optional(),
+            lastName: Joi.string().empty('').allow(null).optional(),
+            email: Joi.string().empty('').allow(null).optional(),
+            username: Joi.string().empty('').allow(null).optional(),
+          }).default({}),
+          page: Joi.object({
+            number: Joi.number().integer().empty('').allow(null).optional(),
+            size: Joi.number().integer().empty('').allow(null).optional(),
+          }).default({}),
+        }),
+      },
+      handler: (request, h) => userController.findPaginatedFilteredUsers(request, h),
+      notes: [
+        "- **Cette route est restreinte aux utilisateurs authentifiés ayant les droits d'accès**\n" +
+          '- Elle permet de récupérer & chercher une liste d’utilisateurs\n' +
+          '- Cette liste est paginée et filtrée selon un **id**, **firstName**, un **lastName**, un **email** et **identifiant** donnés',
+      ],
+      tags: ['api', 'admin', 'user'],
     },
   },
 ];
