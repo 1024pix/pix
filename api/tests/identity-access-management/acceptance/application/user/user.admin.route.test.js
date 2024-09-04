698ddaf1279b258fc1d4925d2ad5eed41b0ee924
Eric Lim <eric.lim@zenika.com>
2024-09-04 10:14:49 +0200

    test(api): add acceptance test
    for /api/admin/users route

--- api/tests/identity-access-management/acceptance/application/user/user.admin.route.test.js
+++ api/tests/identity-access-management/acceptance/application/user/user.admin.route.test.js
@@ -7,10 +7,15 @@ import {
 } from '../../../../test-helper.js';
 
 describe('Acceptance | Identity Access Management | Application | Route | Admin | User', function () {
+  let server;
+
+  beforeEach(async function () {
+    server = await createServer();
+  });
+
   describe('PUT /admin/users/:id/unblock', function () {
     it('unblocks user how has tried to many wrong password', async function () {
       // given
-      const server = await createServer();
       const userId = databaseBuilder.factory.buildUser.withRawPassword().id;
       const userLoginId = databaseBuilder.factory.buildUserLogin({ userId }).id;
       const superAdmin = await insertUserWithRoleSuperAdmin();
@@ -33,6 +38,37 @@ describe('Acceptance | Identity Access Management | Application | Route | Admin 
       expect(response.result.data.attributes['failure-count']).to.equal(0);
       expect(response.result.data.attributes['temporary-blocked-until']).to.be.null;
       expect(response.result.data.attributes['blocked-at']).to.be.null;
+    });
+  });
+
+  describe('GET /api/admin/users', function () {
+    context('When filters match a list of users', function () {
+      let requestOptions;
+
+      beforeEach(async function () {
+        const user = await insertUserWithRoleSuperAdmin();
+        const params = '?filter[firstName]=Ann' + '&page[number]=1&page[size]=25';
+
+        requestOptions = {
+          method: 'GET',
+          url: `/api/admin/users${params}`,
+          headers: { authorization: generateValidRequestAuthorizationHeader(user.id) },
+        };
+
+        await databaseBuilder.factory.buildUser({ firstName: 'Ann' });
+        await databaseBuilder.factory.buildUser({ firstName: 'Anne' });
+        await databaseBuilder.factory.buildUser({ firstName: 'Annie' });
+        await databaseBuilder.commit();
+      });
+
+      it('retrieves this list of users', async function () {
+        // when
+        const response = await server.inject(requestOptions);
+        // then
+        const { result, statusCode } = response;
+        expect(statusCode).to.equal(200);
+        expect(result.data).to.have.length(3);
+      });
     });
   });
 });
