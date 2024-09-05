2beacef3dcd6592bad07ba8a1e0eb608f20aea06
Marie-Hélène <marie-helene.rots@pix.fr>
2024-09-05 15:57:37 +0200

    feat(api): add statuses to placesLot model

--- api/src/prescription/organization-place/domain/read-models/PlacesLot.js
+++ api/src/prescription/organization-place/domain/read-models/PlacesLot.js
@@ -11,6 +11,12 @@ const validationSchema = Joi.object({
   deletedAt: Joi.date().required().allow(null),
 });
 
+const statuses = {
+  ACTIVE: 'ACTIVE',
+  EXPIRED: 'EXPIRED',
+  PENDING: 'PENDING',
+};
+
 export class PlacesLot {
   #activationDate;
   #expirationDate;
@@ -25,6 +31,28 @@ export class PlacesLot {
   }
 
   get isActive() {
-    return this.#expirationDate > Date.now() && this.#activationDate < Date.now() && !this.#deletedAt;
+    return this.status == statuses.ACTIVE && !this.#deletedAt;
+  }
+
+  get activationDate() {
+    return this.#activationDate;
+  }
+
+  get expirationDate() {
+    return this.#expirationDate;
+  }
+
+  get status() {
+    const today = new Date();
+
+    if (Boolean(this.#expirationDate) && this.#expirationDate < today) {
+      return statuses.EXPIRED;
+    }
+
+    if (this.#activationDate < today) {
+      return statuses.ACTIVE;
+    }
+
+    return statuses.PENDING;
   }
 }
