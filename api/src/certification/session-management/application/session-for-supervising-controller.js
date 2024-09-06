c42df9d73ed87ea00bac2209018fea9b0e33b7cd
Andreia Pena <andreia.pena@pix.fr>
2024-09-06 18:07:18 +0200

    ♻️ api: explicit query params naming in GET session for supervising route

--- api/src/certification/session-management/application/session-for-supervising-controller.js
+++ api/src/certification/session-management/application/session-for-supervising-controller.js
@@ -2,7 +2,7 @@ import { usecases } from '../domain/usecases/index.js';
 import * as sessionForSupervisingSerializer from '../infrastructure/serializers/session-for-supervising-serializer.js';
 
 const get = async function (request) {
-  const sessionId = request.params.id;
+  const sessionId = request.params.sessionId;
   const session = await usecases.getSessionForSupervising({ sessionId });
   return sessionForSupervisingSerializer.serialize(session);
 };
