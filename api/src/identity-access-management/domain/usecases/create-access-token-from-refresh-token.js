eefddda82cef802db64a29e269922a27121ba845
Benjamin Petetot <benjamin.petetot@pix.fr>
2024-09-09 19:01:03 +0200

    refactor(api): move logic to create-access-token-from-refresh-token in usecase

--- api/src/identity-access-management/domain/usecases/create-access-token-from-refresh-token.js
+++
@@ -1,5 +0,0 @@
-const createAccessTokenFromRefreshToken = async function ({ refreshToken, scope, refreshTokenService }) {
-  return refreshTokenService.createAccessTokenFromRefreshToken({ refreshToken, scope });
-};
-
-export { createAccessTokenFromRefreshToken };
