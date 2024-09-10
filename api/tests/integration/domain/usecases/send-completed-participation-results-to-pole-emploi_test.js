fbafa87793ef168e20453872989f2a2aad68d493
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2024-09-10 16:35:24 +0200

    fix(api): add usecase completed participation pole emploi to use dependenciy injection of pole emploi notifier custom

---
+++ api/tests/integration/domain/usecases/send-completed-participation-results-to-pole-emploi_test.js
@@ -0,0 +1,80 @@
+import { usecases } from '../../../../lib/domain/usecases/index.js';
+import * as poleEmploiNotifier from '../../../../lib/infrastructure/externals/pole-emploi/pole-emploi-notifier.js';
+import {
+  databaseBuilder,
+  expect,
+  knex,
+  learningContentBuilder,
+  mockLearningContent,
+  sinon,
+} from '../../../test-helper.js';
+
+describe('Integration | Domain | UseCases | send-completed-participation-results-to-pole-emploi', function () {
+  let campaignParticipationId, userId, responseCode;
+  let httpAgentStub, httpErrorsHelperStub, loggerStub;
+
+  beforeEach(async function () {
+    httpAgentStub = { post: sinon.stub() };
+    loggerStub = { info: sinon.stub(), error: sinon.stub() };
+    httpErrorsHelperStub = { serializeHttpErrorResponse: sinon.stub() };
+    responseCode = Symbol('responseCode');
+
+    userId = databaseBuilder.factory.buildUser().id;
+    databaseBuilder.factory.buildAuthenticationMethod.withPoleEmploiAsIdentityProvider({ userId });
+
+    const organizationId = databaseBuilder.factory.buildOrganization().id;
+    const tagId = databaseBuilder.factory.buildTag({ name: 'POLE EMPLOI' }).id;
+    databaseBuilder.factory.buildOrganizationTag({ organizationId, tagId });
+    const campaignId = databaseBuilder.factory.buildCampaign({ organizationId }).id;
+    databaseBuilder.factory.buildCampaignSkill({ campaignId });
+    campaignParticipationId = databaseBuilder.factory.buildCampaignParticipation({ campaignId, userId }).id;
+    databaseBuilder.factory.buildAssessment({ campaignParticipationId, userId });
+    const learningContentObjects = learningContentBuilder.fromAreas([]);
+    mockLearningContent(learningContentObjects);
+    return databaseBuilder.commit();
+  });
+
+  it('should save success of this notification', async function () {
+    // given
+    httpAgentStub.post.resolves({
+      isSuccessful: true,
+      code: responseCode,
+      data: {
+        access_token: 'token',
+        expires_in: new Date(),
+        refresh_token: 'refresh_token',
+      },
+    });
+
+    // when
+    await usecases.sendCompletedParticipationResultsToPoleEmploi({
+      campaignParticipationId,
+      poleEmploiNotifier,
+      notifierDependencies: {
+        httpAgent: httpAgentStub,
+        httpErrorsHelper: httpErrorsHelperStub,
+        logger: loggerStub,
+      },
+    });
+
+    // then
+    const poleEmploiSendings = await knex('pole-emploi-sendings').where({ campaignParticipationId });
+    expect(poleEmploiSendings.length).to.equal(1);
+    expect(poleEmploiSendings[0].responseCode).to.equal(responseCode.toString());
+    expect(poleEmploiSendings[0].type).to.equal('CAMPAIGN_PARTICIPATION_COMPLETION');
+  });
+
+  it('should return a disable send notification by default (if push is disabled) ', async function () {
+    // when
+    await usecases.sendCompletedParticipationResultsToPoleEmploi({
+      campaignParticipationId,
+    });
+
+    // then
+    const poleEmploiSendings = await knex('pole-emploi-sendings').where({ campaignParticipationId });
+    expect(poleEmploiSendings.length).to.equal(1);
+    expect(poleEmploiSendings[0].isSuccessful).to.be.false;
+    expect(poleEmploiSendings[0].responseCode).to.equal('SENDING-DISABLED');
+    expect(poleEmploiSendings[0].type).to.equal('CAMPAIGN_PARTICIPATION_COMPLETION');
+  });
+});
--- api/tests/prescription/campaign-participation/unit/application/jobs/participation-completed-job-controller_test.js
+++ api/tests/prescription/campaign-participation/unit/application/jobs/participation-completed-job-controller_test.js
@@ -1,220 +1,24 @@
-import { PoleEmploiPayload } from '../../../../../../lib/infrastructure/externals/pole-emploi/PoleEmploiPayload.js';
+import { usecases } from '../../../../../../lib/domain/usecases/index.js';
 import { ParticipationCompletedJobController } from '../../../../../../src/prescription/campaign-participation/application/jobs/participation-completed-job-controller.js';
-import { ParticipationCompletedJob } from '../../../../../../src/prescription/campaign-participation/domain/models/ParticipationCompletedJob.js';
-import { PoleEmploiSending } from '../../../../../../src/shared/domain/models/PoleEmploiSending.js';
-import { domainBuilder, expect, sinon } from '../../../../../test-helper.js';
+import { expect, sinon } from '../../../../../test-helper.js';
 
-describe('Unit | Prescription | Application | Jobs | ParticipationCompletedJobController', function () {
-  context('#handle', function () {
-    let data, dependencies, expectedResults;
-    let campaignId, userId, organizationId, assessmentId;
-    let campaignParticipationCompletedJobController;
-    let httpAgent, logger, httpErrorsHelper;
-    let assessmentRepository,
-      campaignRepository,
-      campaignParticipationRepository,
-      organizationRepository,
-      targetProfileRepository,
-      userRepository,
-      poleEmploiNotifier,
-      poleEmploiSendingRepository,
-      authenticationMethodRepository;
-
-    beforeEach(function () {
-      campaignId = Symbol('campaignId');
-      userId = Symbol('userId');
-      organizationId = Symbol('organizationId');
-      assessmentId = Symbol('assessmentId');
-
-      httpAgent = sinon.stub();
-      logger = sinon.stub();
-      httpErrorsHelper = sinon.stub();
-      assessmentRepository = { get: sinon.stub() };
-      campaignRepository = { get: sinon.stub() };
-      campaignParticipationRepository = { get: sinon.stub() };
-      organizationRepository = { get: sinon.stub() };
-      targetProfileRepository = { get: sinon.stub() };
-      userRepository = { get: sinon.stub() };
-      poleEmploiNotifier = { notify: sinon.stub() };
-      poleEmploiSendingRepository = { create: sinon.stub() };
-      authenticationMethodRepository = {
-        findOneByUserIdAndIdentityProvider: sinon.stub(),
-        updateAuthenticationComplementByUserIdAndIdentityProvider: sinon.stub(),
-      };
-      campaignParticipationCompletedJobController = new ParticipationCompletedJobController();
-
-      dependencies = {
-        assessmentRepository,
-        campaignRepository,
-        campaignParticipationRepository,
-        organizationRepository,
-        targetProfileRepository,
-        userRepository,
-        poleEmploiNotifier,
-        poleEmploiSendingRepository,
-        authenticationMethodRepository,
-        httpErrorsHelper,
-        httpAgent,
-        logger,
+describe('Unit | Prescription | Application | Jobs | participationCompletedJobController', function () {
+  describe('#handle', function () {
+    it('should call usecase', async function () {
+      sinon.stub(usecases, 'sendCompletedParticipationResultsToPoleEmploi');
+      // given
+      const handler = new ParticipationCompletedJobController();
+      const data = {
+        campaignParticipationId: Symbol('campaignParticipationId'),
       };
 
-      expectedResults = new PoleEmploiPayload({
-        campagne: {
-          nom: 'Campagne Pôle Emploi',
-          dateDebut: new Date('2020-01-01'),
-          dateFin: new Date('2020-02-01'),
-          type: 'EVALUATION',
-          codeCampagne: 'CODEPE123',
-          urlCampagne: 'https://app.pix.fr/campagnes/CODEPE123',
-          nomOrganisme: 'Pix',
-          typeOrganisme: 'externe',
-        },
-        individu: {
-          nom: 'Bonneau',
-          prenom: 'Jean',
-        },
-        test: {
-          etat: 3,
-          progression: 100,
-          typeTest: 'DI',
-          referenceExterne: 55667788,
-          dateDebut: new Date('2020-01-02'),
-          dateProgression: new Date('2020-01-03'),
-          dateValidation: null,
-          evaluation: null,
-          uniteEvaluation: 'A',
-          elementsEvalues: [],
-        },
-      });
-    });
+      // when
+      await handler.handle({ data });
 
-    context('when campaign is of type ASSESSMENT and organization is Pole Emploi', function () {
-      beforeEach(function () {
-        const campaign = domainBuilder.buildCampaign({
-          id: campaignId,
-          name: 'Campagne Pôle Emploi',
-          code: 'CODEPE123',
-          createdAt: new Date('2020-01-01'),
-          archivedAt: new Date('2020-02-01'),
-          type: 'ASSESSMENT',
-          targetProfile: { id: 'targetProfileId1' },
-          organization: { id: organizationId },
-        });
-        const campaignParticipation = domainBuilder.buildCampaignParticipation({
-          id: 55667788,
-          userId,
-          campaign,
-          assessments: [domainBuilder.buildAssessment({ id: assessmentId })],
-          createdAt: new Date('2020-01-02'),
-        });
-        data = new ParticipationCompletedJob({
-          campaignParticipationId: campaignParticipation.id,
-        });
-
-        campaignParticipationRepository.get.withArgs(campaignParticipation.id).resolves(campaignParticipation);
-        assessmentRepository.get.withArgs(assessmentId).resolves(
-          domainBuilder.buildAssessment({
-            updatedAt: new Date('2020-01-03'),
-          }),
-        );
-        organizationRepository.get.withArgs(organizationId).resolves({ isPoleEmploi: true });
-        userRepository.get
-          .withArgs(userId)
-          .resolves(domainBuilder.buildUser({ id: userId, firstName: 'Jean', lastName: 'Bonneau' }));
-        campaignRepository.get.withArgs(campaignId).resolves(campaign);
-        targetProfileRepository.get.withArgs('targetProfileId1').resolves({ name: 'Diagnostic initial' });
-      });
-
-      it('should notify pole emploi and create pole emploi sending accordingly', async function () {
-        // given
-        const expectedResponse = { isSuccessful: 'someValue', code: 'someCode' };
-        poleEmploiNotifier.notify
-          .withArgs(userId, expectedResults, {
-            authenticationMethodRepository,
-            httpErrorsHelper,
-            httpAgent,
-            logger,
-          })
-          .resolves(expectedResponse);
-        const poleEmploiSending = Symbol('Pole emploi sending');
-
-        sinon
-          .stub(PoleEmploiSending, 'buildForParticipationFinished')
-          .withArgs({
-            campaignParticipationId: 55667788,
-            payload: expectedResults.toString(),
-            isSuccessful: expectedResponse.isSuccessful,
-            responseCode: expectedResponse.code,
-          })
-          .returns(poleEmploiSending);
-
-        // when
-        await campaignParticipationCompletedJobController.handle({ data, dependencies });
-
-        // then
-        expect(poleEmploiSendingRepository.create).to.have.been.calledWithExactly({ poleEmploiSending });
-      });
-    });
-
-    context('when campaign is of type ASSESSMENT but organization is not Pole Emploi', function () {
-      beforeEach(function () {
-        const campaign = domainBuilder.buildCampaign({
-          id: campaignId,
-          type: 'ASSESSMENT',
-          organization: { id: organizationId },
-        });
-        const campaignParticipation = domainBuilder.buildCampaignParticipation({
-          id: 55667788,
-          userId,
-          campaign,
-          createdAt: new Date('2020-01-02'),
-        });
-        data = new ParticipationCompletedJob({
-          campaignParticipationId: campaignParticipation.id,
-        });
-
-        campaignParticipationRepository.get.withArgs(campaignParticipation.id).resolves(campaignParticipation);
-        campaignRepository.get.withArgs(campaignId).resolves(domainBuilder.buildCampaign(campaign));
-        organizationRepository.get.withArgs(organizationId).resolves({ isPoleEmploi: false });
-      });
-
-      it('should not notify to Pole Emploi', async function () {
-        // when
-        await campaignParticipationCompletedJobController.handle({ data, dependencies });
-
-        // then
-        sinon.assert.notCalled(poleEmploiNotifier.notify);
-      });
-    });
-
-    context('when organization is Pole Emploi but campaign is of type PROFILES_COLLECTION', function () {
-      beforeEach(function () {
-        const campaign = domainBuilder.buildCampaign({
-          id: campaignId,
-          type: 'PROFILES_COLLECTION',
-          organization: { id: organizationId },
-        });
-        const campaignParticipation = domainBuilder.buildCampaignParticipation({
-          id: 55667788,
-          userId,
-          campaign,
-          createdAt: new Date('2020-01-02'),
-        });
-        data = new ParticipationCompletedJob({
-          campaignParticipationId: campaignParticipation.id,
-        });
-
-        campaignParticipationRepository.get.withArgs(campaignParticipation.id).resolves(campaignParticipation);
-        campaignRepository.get.withArgs(campaignId).resolves(domainBuilder.buildCampaign(campaign));
-        organizationRepository.get.withArgs(organizationId).resolves({ isPoleEmploi: true });
-      });
-
-      it('should not notify to Pole Emploi', async function () {
-        // when
-        await campaignParticipationCompletedJobController.handle({ data, dependencies });
-
-        // then
-        sinon.assert.notCalled(poleEmploiNotifier.notify);
+      // then
+      expect(usecases.sendCompletedParticipationResultsToPoleEmploi).to.have.been.calledOnce;
+      expect(usecases.sendCompletedParticipationResultsToPoleEmploi).to.have.been.calledWithExactly({
+        campaignParticipationId: data.campaignParticipationId,
       });
     });
   });
