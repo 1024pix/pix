24e65f67d1f1c12da5943cd8f0e5f163ea794cb6
Marianne Bost <marianne.bost@pix.fr>
2024-09-12 14:13:40 +0200

    feat(mon-pix): use isNewAuthenticationDesignEnabled toggle to display the new authentication page
    Co-authored-by: LEGO Technix <109212476+lego-technix@users.noreply.github.com>
    Co-authored-by: Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>

---
+++ mon-pix/app/components/authentication/login-form.gjs
@@ -0,0 +1,5 @@
+import { t } from 'ember-intl';
+
+<template>
+  <h1>{{t "pages.sign-in.title"}}</h1>
+</template>
--- mon-pix/tests/acceptance/authentication/login-test.js
+++ mon-pix/tests/acceptance/authentication/login-test.js
@@ -12,57 +12,101 @@ module('Acceptance | Login', function (hooks) {
   setupMirage(hooks);
   setupIntl(hooks);
 
-  module('when current url does not contain french tld (.fr)', function () {
-    module('when accessing the signin page with "Français" as default language', function () {
-      test('displays the signin page with "Français" as selected language', async function (assert) {
-        // when
-        const screen = await visit('/connexion');
-
-        // then
-        assert.strictEqual(currentURL(), '/connexion');
-        assert.dom(screen.getByRole('heading', { name: t('pages.sign-in.first-title'), level: 1 })).exists();
-        assert.dom(screen.getByRole('button', { name: 'Sélectionnez une langue' })).exists();
-      });
-
-      module('when the user select "English" as his language', function () {
-        test('displays the login page with "English" as selected language', async function (assert) {
-          // when
-          const screen = await visit('/connexion');
-          await click(screen.getByRole('button', { name: 'Sélectionnez une langue' }));
-          await screen.findByRole('listbox');
-          await click(screen.getByRole('option', { name: 'English' }));
-
-          // then
-          assert.strictEqual(currentURL(), '/connexion');
-          assert.dom(screen.getByRole('heading', { name: t('pages.sign-in.first-title'), level: 1 })).exists();
-          assert.dom(screen.getByRole('button', { name: 'Select a language' })).exists();
-        });
+  module('when feature toggle is false', function (hooks) {
+    hooks.beforeEach(function () {
+      server.create('feature-toggle', {
+        id: 0,
+        isNewAuthenticationDesignEnabled: false,
       });
     });
 
-    module('when accessing the login page with "English" as selected language', function () {
-      test('displays the login page with "English"', async function (assert) {
-        // when
-        const screen = await visit('/connexion?lang=en');
+    module('when current url does not contain french tld (.fr)', function () {
+      module('when accessing the signin page with "Français" as default language', function () {
+        test('displays the signin page with "Français" as selected language', async function (assert) {
+          // when
 
-        // then
-        assert.strictEqual(currentURL(), '/connexion?lang=en');
-        assert.dom(screen.getByRole('heading', { name: t('pages.sign-in.first-title'), level: 1 })).exists();
-        assert.dom(screen.getByRole('button', { name: 'Select a language' })).exists();
-      });
-
-      module('when the user select "Français" as his language', function () {
-        test('displays the login page with "Français" as selected language', async function (assert) {
-          // given & when
-          const screen = await visit('/connexion?lang=en');
-          await click(screen.getByRole('button', { name: 'Select a language' }));
-          await screen.findByRole('listbox');
-          await click(screen.getByRole('option', { name: 'Français' }));
+          const screen = await visit('/connexion');
 
           // then
           assert.strictEqual(currentURL(), '/connexion');
           assert.dom(screen.getByRole('heading', { name: t('pages.sign-in.first-title'), level: 1 })).exists();
           assert.dom(screen.getByRole('button', { name: 'Sélectionnez une langue' })).exists();
+        });
+
+        module('when the user select "English" as his language', function () {
+          test('displays the login page with "English" as selected language', async function (assert) {
+            // when
+            const screen = await visit('/connexion');
+            await click(screen.getByRole('button', { name: 'Sélectionnez une langue' }));
+            await screen.findByRole('listbox');
+            await click(screen.getByRole('option', { name: 'English' }));
+
+            // then
+            assert.strictEqual(currentURL(), '/connexion');
+            assert.dom(screen.getByRole('heading', { name: t('pages.sign-in.first-title'), level: 1 })).exists();
+            assert.dom(screen.getByRole('button', { name: 'Select a language' })).exists();
+          });
+        });
+      });
+
+      module('when accessing the login page with "English" as selected language', function () {
+        test('displays the login page with "English"', async function (assert) {
+          // when
+          const screen = await visit('/connexion?lang=en');
+
+          // then
+          assert.strictEqual(currentURL(), '/connexion?lang=en');
+          assert.dom(screen.getByRole('heading', { name: t('pages.sign-in.first-title'), level: 1 })).exists();
+          assert.dom(screen.getByRole('button', { name: 'Select a language' })).exists();
+        });
+
+        module('when the user select "Français" as his language', function () {
+          test('displays the login page with "Français" as selected language', async function (assert) {
+            // given & when
+            const screen = await visit('/connexion?lang=en');
+            await click(screen.getByRole('button', { name: 'Select a language' }));
+            await screen.findByRole('listbox');
+            await click(screen.getByRole('option', { name: 'Français' }));
+
+            // then
+            assert.strictEqual(currentURL(), '/connexion');
+            assert.dom(screen.getByRole('heading', { name: t('pages.sign-in.first-title'), level: 1 })).exists();
+            assert.dom(screen.getByRole('button', { name: 'Sélectionnez une langue' })).exists();
+          });
+        });
+      });
+    });
+  });
+
+  module('when feature toggle is true', function (hooks) {
+    hooks.beforeEach(function () {
+      server.create('feature-toggle', {
+        id: 0,
+        isNewAuthenticationDesignEnabled: true,
+      });
+    });
+
+    test('displays the new layout with a footer', async function (assert) {
+      // when
+      const screen = await visit('/connexion');
+      // then
+      assert.dom(screen.getByRole('contentinfo')).exists();
+    });
+    module('when current url does not contain french tld (.fr)', function () {
+      module('when accessing the login page with "English" as selected language', function () {
+        module('when the user select "Français" as his language', function () {
+          test('displays the login page with "Français" as selected language', async function (assert) {
+            // given & when
+            const screen = await visit('/connexion?lang=en');
+            await click(screen.getByRole('button', { name: 'Select a language' }));
+            await screen.findByRole('listbox');
+            await click(screen.getByRole('option', { name: 'Français' }));
+
+            // then
+            assert.strictEqual(currentURL(), '/connexion');
+            assert.dom(screen.getByRole('heading', { name: t('pages.sign-in.title'), level: 1 })).exists();
+            assert.dom(screen.getByRole('button', { name: 'Sélectionnez une langue' })).exists();
+          });
         });
       });
     });
