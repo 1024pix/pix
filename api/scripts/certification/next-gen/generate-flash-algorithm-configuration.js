8cd51b56740ffae813a33a9e3eede04586082c2a
Laura Bergoens <laura.bergoens@pix.fr>
2024-09-12 18:26:40 +0200

    use script logger in scripts/certification/next-gen/generate-flash-algorithm-configuration.js

--- api/scripts/certification/next-gen/generate-flash-algorithm-configuration.js
+++ api/scripts/certification/next-gen/generate-flash-algorithm-configuration.js
@@ -4,6 +4,7 @@ import { disconnect } from '../../../db/knex-database-connection.js';
 import * as flashAlgorithmConfigurationRepository from '../../../src/certification/flash-certification/infrastructure/repositories/flash-algorithm-configuration-repository.js';
 import { FlashAssessmentAlgorithmConfiguration } from '../../../src/certification/shared/domain/models/FlashAssessmentAlgorithmConfiguration.js';
 import { logger } from '../../../src/shared/infrastructure/utils/logger.js';
+import { executeAndLogScript } from '../../tooling/tooling.js';
 
 const modulePath = url.fileURLToPath(import.meta.url);
 const isLaunchedFromCommandLine = process.argv[1] === modulePath;
@@ -35,9 +36,8 @@ async function main() {
 (async () => {
   if (isLaunchedFromCommandLine) {
     try {
-      await main();
+      await executeAndLogScript({ processArgvs: process.argv, scriptFn: main });
     } catch (error) {
-      console.error(error);
       process.exitCode = 1;
     } finally {
       await disconnect();
