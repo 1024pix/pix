5411b2026c03d2fda5a5f76cd7e825c1bdc8a2d1
Eric Lim <eric.lim@zenika.com>
2024-09-16 16:16:44 +0200

    feat(api): add organization learner identity model

---
+++ api/src/identity-access-management/domain/models/OrganizationLearnerIdentity.js
@@ -0,0 +1,10 @@
+export class OrganizationLearnerIdentity {
+  constructor({ birthdate, division, firstName, lastName, userId, username }) {
+    this.birthdate = birthdate;
+    this.division = division;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.userId = userId;
+    this.username = username;
+  }
+}
