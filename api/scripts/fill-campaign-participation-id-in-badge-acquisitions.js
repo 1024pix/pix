96fca609f25d9a8e5b1e41718ee27fdf6829563d
Laura Bergoens <laura.bergoens@pix.fr>
2024-09-16 09:40:03 +0200

    use script logger scripts/fill-campaign-participation-id-in-badge-acquisitions.js

--- api/scripts/fill-campaign-participation-id-in-badge-acquisitions.js
+++ api/scripts/fill-campaign-participation-id-in-badge-acquisitions.js
@@ -1,6 +1,7 @@
 import * as url from 'node:url';
 
 import { disconnect, knex } from '../db/knex-database-connection.js';
+import { executeAndLogScript } from './tooling/tooling.js';
 
 async function getAllBadgeAcquistionsWithoutCampaignParticipationId() {
   return knex('badge-acquisitions').select().where({ campaignParticipationId: null });
@@ -77,9 +78,8 @@ async function main() {
 (async () => {
   if (isLaunchedFromCommandLine) {
     try {
-      await main();
+      await executeAndLogScript({ processArgvs: process.argv, scriptFn: main });
     } catch (error) {
-      console.error(error);
       process.exitCode = 1;
     } finally {
       await disconnect();
