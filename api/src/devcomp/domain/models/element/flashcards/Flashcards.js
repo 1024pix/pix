c21d71c6af21e4b9930e78c7ad6b887b7f88d6b1
dlahaye <dimitrilahaye.dev@gmail.com>
2024-09-18 14:53:07 +0200

    feat(api): finalize flashcards model with all the attributes
    Co-authored-by: Diane Cordier <diane.cordier@pix.fr>

--- api/src/devcomp/domain/models/element/flashcards/Flashcards.js
+++ api/src/devcomp/domain/models/element/flashcards/Flashcards.js
@@ -1,8 +1,13 @@
 import { Element } from '../Element.js';
 
 class Flashcards extends Element {
-  constructor({ id }) {
+  constructor({ id, title, instruction, introImage, cards }) {
     super({ id, type: 'flashcards' });
+
+    this.title = title;
+    this.instruction = instruction;
+    this.introImage = introImage;
+    this.cards = cards;
   }
 }
 export { Flashcards };
--- api/tests/devcomp/unit/domain/models/element/flashcards/Flashcards_test.js
+++ api/tests/devcomp/unit/domain/models/element/flashcards/Flashcards_test.js
@@ -1,14 +1,35 @@
+import { Card } from '../../../../../../../src/devcomp/domain/models/element/flashcards/Card.js';
 import { Flashcards } from '../../../../../../../src/devcomp/domain/models/element/flashcards/Flashcards.js';
 import { expect } from '../../../../../../test-helper.js';
 
 describe('Unit | Devcomp | Domain | Models | Element | Flashcards | Flashcards', function () {
   describe('#constructor', function () {
     it('should create a Flashcards element and keep attributes', function () {
+      // given
+      const attributes = {
+        id: 'id',
+        title: 'title',
+        instruction: 'instruction',
+        introImage: Symbol('introImage'),
+        cards: [
+          new Card({
+            id: 'e1de6394-ff88-4de3-8834-a40057a50ff4',
+            recto: Symbol('recto'),
+            verso: Symbol('verso'),
+          }),
+        ],
+      };
+
       // when
-      const flashcards = new Flashcards({ id: 'id' });
+      const flashcards = new Flashcards(attributes);
 
       // then
-      expect(flashcards.id).to.equal('id');
+      expect(flashcards.id).to.equal(attributes.id);
+      expect(flashcards.type).to.equal('flashcards');
+      expect(flashcards.title).to.equal(attributes.title);
+      expect(flashcards.instruction).to.equal(attributes.instruction);
+      expect(flashcards.introImage).to.equal(attributes.introImage);
+      expect(flashcards.cards[0]).to.be.instanceof(Card);
     });
   });
 });
