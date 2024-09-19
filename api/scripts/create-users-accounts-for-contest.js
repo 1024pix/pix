d39c6e552b4ebc0b29b4a522efe1cc1ab03ddbc0
Steph0 <Steph0@users.noreply.github.com>
2024-09-19 17:38:14 +0200

    Revert "The concours de la mairie de Paris has ended a long time ago !"
    Acces team is still using this script
    This reverts commit cd332983b286413c430f278f6fa7156998484fd6.

---
+++ api/scripts/create-users-accounts-for-contest.js
@@ -0,0 +1,79 @@
+import * as url from 'node:url';
+
+import { disconnect } from '../db/knex-database-connection.js';
+import * as authenticationMethodRepository from '../src/identity-access-management/infrastructure/repositories/authentication-method.repository.js';
+import { userToCreateRepository } from '../src/identity-access-management/infrastructure/repositories/user-to-create.repository.js';
+import { cryptoService } from '../src/shared/domain/services/crypto-service.js';
+import * as userService from '../src/shared/domain/services/user-service.js';
+import { parseCsvWithHeader } from './helpers/csvHelpers.js';
+
+function prepareDataForInsert(rawUsers) {
+  return rawUsers.map(({ firstName, lastName, email, password }) => {
+    return {
+      firstName: firstName.trim(),
+      lastName: lastName.trim(),
+      email: email.trim().toLowerCase(),
+      password: password.trim(),
+    };
+  });
+}
+
+async function createUsers({ usersInRaw }) {
+  const now = new Date();
+
+  for (const userDTO of usersInRaw) {
+    const userToCreate = {
+      firstName: userDTO.firstName,
+      lastName: userDTO.lastName,
+      email: userDTO.email,
+      createAt: now,
+      updatedAt: now,
+      cgu: true,
+      lang: 'fr',
+    };
+    const hashedPassword = await cryptoService.hashPassword(userDTO.password);
+
+    await userService.createUserWithPassword({
+      user: userToCreate,
+      hashedPassword,
+      userToCreateRepository,
+      authenticationMethodRepository,
+    });
+  }
+}
+
+const modulePath = url.fileURLToPath(import.meta.url);
+const isLaunchedFromCommandLine = process.argv[1] === modulePath;
+
+async function main() {
+  console.log('Starting creating users accounts for contest.');
+
+  const filePath = process.argv[2];
+
+  console.log('Reading and parsing csv data file... ');
+  const csvData = await parseCsvWithHeader(filePath);
+  console.log('ok');
+
+  console.log('Preparing data... ');
+  const usersInRaw = prepareDataForInsert(csvData);
+  console.log('ok');
+
+  console.log('Creating users...');
+  await createUsers({ usersInRaw });
+  console.log('\nDone.');
+}
+
+(async () => {
+  if (isLaunchedFromCommandLine) {
+    try {
+      await main();
+    } catch (error) {
+      console.error(error);
+      process.exitCode = 1;
+    } finally {
+      await disconnect();
+    }
+  }
+})();
+
+export { createUsers, prepareDataForInsert };
