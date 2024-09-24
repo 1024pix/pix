1525add56decce9706ab8d8f2948bfb4f2c32f8d
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2024-09-24 10:15:16 +0200

    tech(api): add method to remove job

--- api/tests/tooling/jobs/expect-job.js
+++ api/tests/tooling/jobs/expect-job.js
@@ -5,6 +5,10 @@ export const jobChai = (knex) => (_chai, utils) => {
     return this;
   });
 
+  utils.addProperty(Assertion.prototype, 'schedule', function () {
+    return this;
+  });
+
   Assertion.addMethod('withJobsCount', async function (expectedCount) {
     const jobName = this._obj;
     const jobs = await knex('pgboss.job').where({ name: jobName });
@@ -21,6 +25,27 @@ export const jobChai = (knex) => (_chai, utils) => {
     const jobName = this._obj;
     const jobs = await knex('pgboss.job').select(knex.raw(`*, expirein::varchar`)).where({ name: jobName });
     assert.deepInclude(jobs[0], jobData, `Job '${jobName}' was performed with a different payload`);
+  });
+
+  Assertion.addMethod('withCronJobsCount', async function (expectedCount) {
+    const jobName = this._obj;
+    const jobs = await knex('pgboss.schedule').where({ name: jobName });
+    assert.strictEqual(
+      jobs.length,
+      expectedCount,
+      `expected ${jobName} to have been performed ${expectedCount} times, but it was performed ${jobs.length} times`,
+    );
+  });
+
+  Assertion.addMethod('withCronJob', async function (jobData) {
+    await this.withCronJobsCount(1);
+
+    const jobName = this._obj;
+    const job = await knex('pgboss.schedule')
+      .select('name', 'cron', 'data', 'options')
+      .where({ name: jobName })
+      .first();
+    assert.deepInclude(job, jobData, `Job '${jobName}' was schedule with a different payload`);
   });
 
   Assertion.addMethod('withJobPayloads', async function (payloads) {
