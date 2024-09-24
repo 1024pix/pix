803b5db4fbe3c427525ec072272e3c4ef423c606
Eric Lim <eric.lim@zenika.com>
2024-09-24 12:04:45 +0200

    [BUGFIX] Supprimer l'initialisation en lang fr par défaut de l'application (PIX-14387) (#10143)

--- mon-pix/app/routes/application.js
+++ mon-pix/app/routes/application.js
@@ -18,17 +18,6 @@ export default class ApplicationRoute extends Route {
   async beforeModel(transition) {
     this.metrics.initialize();
     await this.session.setup();
-    /*
-    Ce code permet de définir une locale par défaut différente de celle d'ember-intl.
-
-    Ember-intl utilise "en-us" comme locale par défaut.
-
-    Si aucun fichier de traduction n'existe dans le dossier spécifié dans la configuration d'ember-intl,
-    on obtient une URL de ce type : "/?lang=Missing%20translation%20%22current-lang%22%20for%20locale%20%22en-us%22".
-
-    Pour régler ce problème, il faut définir une locale ayant un fichier dans le dossier de traduction.
-     */
-    this.intl.setLocale('fr');
 
     await this.featureToggles.load().catch();
 
