cf68f4350fec3359b6e49224c872bd1676d60fad
LionelB <lionel@lumographe.fr>
2024-09-25 16:21:32 +0200

    feat(api): allow override division and birthdate from extraInformations

--- api/tests/prescription/learner-management/unit/domain/read-models/OrganizationLearnerForAdmin_test.js
+++ api/tests/prescription/learner-management/unit/domain/read-models/OrganizationLearnerForAdmin_test.js
@@ -1,3 +1,4 @@
+import { IMPORT_KEY_FIELD } from '../../../../../../src/prescription/learner-management/domain/constants.js';
 import { OrganizationLearnerForAdmin } from '../../../../../../src/prescription/learner-management/domain/read-models/OrganizationLearnerForAdmin.js';
 import { ObjectValidationError } from '../../../../../../src/shared/domain/errors.js';
 import { expect } from '../../../../../test-helper.js';
@@ -26,6 +27,25 @@ describe('Unit | Domain | Read-models | OrganizationLearnerForAdmin', function (
     it('should successfully instantiate object when passing all valid arguments', function () {
       // when
       expect(() => new OrganizationLearnerForAdmin(validArguments)).not.to.throw(ObjectValidationError);
+    });
+
+    it('should overide birthdate and division if additionalColumns and additionalInfos are given', function () {
+      const learner = new OrganizationLearnerForAdmin({
+        ...validArguments,
+        additionalColumns: [
+          {
+            name: IMPORT_KEY_FIELD.COMMON_BIRTHDATE,
+            key: 'dateofbirth',
+          },
+          { name: IMPORT_KEY_FIELD.COMMON_DIVISION, key: 'groupe' },
+        ],
+        additionalInformations: {
+          groupe: 'CP',
+          dateofbirth: '2020-02-01',
+        },
+      });
+      expect(learner.division).to.equal('CP');
+      expect(learner.birthdate).to.deep.equal('2020-02-01');
     });
 
     it('should throw an ObjectValidationError when id is not valid', function () {
