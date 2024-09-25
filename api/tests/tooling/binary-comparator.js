124681767b677b05cd17bf4b68bdc6ecb8d319bd
Yvonnick Frin <frin.yvonnick@gmail.com>
2024-09-25 15:59:48 +0200

    feat(api): create serializer to make pdf from template and data

--- api/tests/tooling/binary-comparator.js
+++ api/tests/tooling/binary-comparator.js
@@ -1,22 +1,8 @@
-import crypto from 'node:crypto';
 import { readFile } from 'node:fs/promises';
 
-const { createHash } = crypto;
-
-function _hash(buffer) {
-  const h = createHash('sha1');
-  h.update(buffer);
-  h.end();
-  return h.digest('hex');
-}
-
 async function isSameBinary(referencePath, buffer) {
-  const actualHash = _hash(buffer);
-
   const expectedBuffer = await readFile(referencePath);
-  const expectedHash = _hash(expectedBuffer);
-
-  return expectedHash == actualHash;
+  return expectedBuffer.equals(buffer);
 }
 
 export { isSameBinary };
