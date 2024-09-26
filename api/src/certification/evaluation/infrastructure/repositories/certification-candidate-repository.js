c93836b99f9c105e65accd6190e4e6c1c780ddc7
Alexandre COIN <alexandre.pc.coin@gmail.com>
2024-09-26 16:31:55 +0200

    feat(api): use newly created candidate model in evaluation subdomain

--- api/src/certification/evaluation/infrastructure/repositories/certification-candidate-repository.js
+++ api/src/certification/evaluation/infrastructure/repositories/certification-candidate-repository.js
@@ -1,6 +1,6 @@
 import { knex } from '../../../../../db/knex-database-connection.js';
 import { CertificationCandidateNotFoundError } from '../../../../shared/domain/errors.js';
-import { Candidate } from '../../../enrolment/domain/models/Candidate.js';
+import { Candidate } from '../../domain/models/Candidate.js';
 
 const findByAssessmentId = async function ({ assessmentId }) {
   const result = await knex('certification-candidates')
--- api/tests/certification/evaluation/integration/infrastructure/repositories/certification-candidate-repository_test.js
+++ api/tests/certification/evaluation/integration/infrastructure/repositories/certification-candidate-repository_test.js
@@ -36,9 +36,8 @@ describe('Integration | Repository | certification candidate', function () {
 
         // then
         expect(result).to.deep.equal(
-          domainBuilder.certification.enrolment.buildCandidate({
+          domainBuilder.certification.evaluation.buildCandidate({
             ...candidate,
-            subscriptions: [],
           }),
         );
       });
@@ -124,9 +123,8 @@ describe('Integration | Repository | certification candidate', function () {
 
           // then
           expect(result).to.deep.equal(
-            domainBuilder.certification.enrolment.buildCandidate({
+            domainBuilder.certification.evaluation.buildCandidate({
               ...candidate,
-              subscriptions: [],
             }),
           );
         });
