de76cfb01abd9374c7d953d740afd392a2f6efd9
Thomas BAZIN <thomas.bazin@pix.fr>
2024-09-27 10:39:14 +0200

    chore(api): remove useless loggerForImport function

--- api/src/prescription/learner-management/utils/loggerForImport.js
+++
@@ -1,8 +0,0 @@
-import { config } from '../../../shared/config.js';
-import { logger } from '../../../shared/infrastructure/utils/logger.js';
-
-export const loggerForImport = (message) => {
-  if (config.import.logEnabled) {
-    logger.info(message);
-  }
-};
