3edfb370bed86deef3b0da5ac53457f500ce3384
Benjamin Petetot <benjamin.petetot@pix.fr>
2024-10-02 18:19:02 +0200

    feat(mon-pix): log user email changed by support

--- api/src/identity-access-management/domain/usecases/update-user-details-by-admin.usecase.js
+++ api/src/identity-access-management/domain/usecases/update-user-details-by-admin.usecase.js
@@ -7,32 +7,40 @@ import {
   AlreadyRegisteredEmailAndUsernameError,
   AlreadyRegisteredUsernameError,
 } from '../../../../src/shared/domain/errors.js';
+import { EventLoggingJob } from '../models/jobs/EventLoggingJob.js';
 
-const updateUserDetailsByAdmin = async function ({ userId, userDetailsToUpdate, userRepository }) {
+const updateUserDetailsByAdmin = async function ({
+  userId,
+  userDetailsToUpdate,
+  updatedByAdminId,
+  userRepository,
+  eventLoggingJobRepository,
+}) {
   const { email, username } = userDetailsToUpdate;
 
-  const foundUsersWithEmailAlreadyUsed = email && (await userRepository.findAnotherUserByEmail(userId, email));
-  const foundUsersWithUsernameAlreadyUsed =
-    username && (await userRepository.findAnotherUserByUsername(userId, username));
+  await _checkEmailAndUsernameAreAvailable({ userId, email, username, userRepository });
 
-  await _checkEmailAndUsernameAreAvailable({
-    usersWithEmail: foundUsersWithEmailAlreadyUsed,
-    usersWithUsername: foundUsersWithUsernameAlreadyUsed,
-  });
+  const currentUser = await userRepository.get(userId);
 
-  const userMustValidateTermsOfService = await _isAddingEmailForFirstTime({ userId, email, userRepository });
+  const userMustValidateTermsOfService = _isAddingEmailForFirstTime({ currentUser, newEmail: email });
   if (userMustValidateTermsOfService) {
     userDetailsToUpdate.mustValidateTermsOfService = true;
   }
 
   await userRepository.updateUserDetailsForAdministration({ id: userId, userAttributes: userDetailsToUpdate });
 
+  await _auditLogForEmailChanged({ currentUser, newEmail: email, updatedByAdminId, eventLoggingJobRepository });
+
   return userRepository.getUserDetailsForAdmin(userId);
 };
 
-async function _checkEmailAndUsernameAreAvailable({ usersWithEmail, usersWithUsername }) {
-  const isEmailAlreadyUsed = has(usersWithEmail, '[0].email');
-  const isUsernameAlreadyUsed = has(usersWithUsername, '[0].username');
+async function _checkEmailAndUsernameAreAvailable({ userId, email, username, userRepository }) {
+  const foundUsersWithEmailAlreadyUsed = email && (await userRepository.findAnotherUserByEmail(userId, email));
+  const isEmailAlreadyUsed = has(foundUsersWithEmailAlreadyUsed, '[0].email');
+
+  const foundUsersWithUsernameAlreadyUsed =
+    username && (await userRepository.findAnotherUserByUsername(userId, username));
+  const isUsernameAlreadyUsed = has(foundUsersWithUsernameAlreadyUsed, '[0].username');
 
   if (isEmailAlreadyUsed && isUsernameAlreadyUsed) {
     throw new AlreadyRegisteredEmailAndUsernameError();
@@ -43,12 +51,27 @@ async function _checkEmailAndUsernameAreAvailable({ usersWithEmail, usersWithUse
   }
 }
 
-async function _isAddingEmailForFirstTime({ userId, email, userRepository }) {
-  const user = await userRepository.get(userId);
-  const userWithoutEmail = !user.email;
-  const userHasUsername = !!user.username;
-  const shouldChangeEmail = !!email;
+function _isAddingEmailForFirstTime({ currentUser, newEmail }) {
+  const userWithoutEmail = !currentUser.email;
+  const userHasUsername = !!currentUser.username;
+  const shouldChangeEmail = !!newEmail;
   return userWithoutEmail && userHasUsername && shouldChangeEmail;
+}
+
+async function _auditLogForEmailChanged({ currentUser, newEmail, updatedByAdminId, eventLoggingJobRepository }) {
+  if (!newEmail || newEmail === currentUser.email) return;
+
+  // Currently only used in Pix Admin, which is why app name is hard-coded for the audit log
+  await eventLoggingJobRepository.performAsync(
+    new EventLoggingJob({
+      client: 'PIX_ADMIN',
+      action: 'EMAIL_CHANGED',
+      role: 'SUPPORT',
+      userId: updatedByAdminId,
+      targetUserId: currentUser.id,
+      data: { oldEmail: currentUser.email, newEmail },
+    }),
+  );
 }
 
 export { updateUserDetailsByAdmin };
