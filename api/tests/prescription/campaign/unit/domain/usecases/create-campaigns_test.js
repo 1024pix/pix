bca28a66cb93621c41985fd17844a9f30b927e95
Alexandre Monney <alexandre.monney@pix.fr>
2024-10-02 16:57:29 +0200

    tests?(api): refacto usecase tests

--- api/tests/prescription/campaign/unit/domain/usecases/create-campaigns_test.js
+++ api/tests/prescription/campaign/unit/domain/usecases/create-campaigns_test.js
@@ -4,92 +4,102 @@ import { NotFoundError, UserNotFoundError } from '../../../../../../src/shared/d
 import { catchErr, domainBuilder, expect, sinon } from '../../../../../test-helper.js';
 
 describe('Unit | UseCase | campaign-administration | create-campaigns', function () {
+  let codeGeneratorStub;
+  let campaignsToCreate;
+  let campaignAdministrationRepositoryStub;
+  let campaignCreatorRepositoryStub;
+  let userRepositoryStub;
+  let organizationRepositoryStub;
+  let createdCampaignsSymbol;
+
+  beforeEach(function () {
+    campaignAdministrationRepositoryStub = {
+      save: sinon.stub(),
+    };
+    userRepositoryStub = {
+      get: sinon.stub(),
+    };
+    organizationRepositoryStub = {
+      get: sinon.stub(),
+    };
+    const code1 = Symbol('code1');
+    const code2 = Symbol('code2');
+    const someoneId = 454756;
+    const otherOrganization = domainBuilder.buildOrganization({ id: 3 });
+    const organization = domainBuilder.buildOrganization({ id: 4 });
+
+    codeGeneratorStub = {
+      generate: sinon
+        .stub()
+        .withArgs(campaignAdministrationRepositoryStub)
+        .onFirstCall()
+        .resolves(code1)
+        .onSecondCall()
+        .resolves(code2),
+    };
+
+    campaignsToCreate = [
+      {
+        organizationId: organization.id,
+        name: 'My Campaign',
+        targetProfileId: 3,
+        creatorId: 2,
+        ownerId: someoneId,
+      },
+      {
+        organizationId: otherOrganization.id,
+        name: 'My other Campaign',
+        targetProfileId: 3,
+        creatorId: 1,
+        ownerId: someoneId,
+      },
+    ];
+
+    const campaignsWithAllData = [
+      {
+        ...campaignsToCreate[0],
+        type: 'ASSESSMENT',
+        code: code1,
+      },
+      {
+        ...campaignsToCreate[1],
+        type: 'ASSESSMENT',
+        code: code2,
+      },
+    ];
+
+    const campaignCreatorPOJO = {
+      createCampaign: sinon.stub(),
+    };
+
+    campaignCreatorPOJO.createCampaign
+      .withArgs({ ...campaignsToCreate[0], type: CampaignTypes.ASSESSMENT, code: code1, ownerId: someoneId })
+      .resolves(campaignsWithAllData[0]);
+    campaignCreatorPOJO.createCampaign
+      .withArgs({ ...campaignsToCreate[1], type: CampaignTypes.ASSESSMENT, code: code2, ownerId: someoneId })
+      .resolves(campaignsWithAllData[1]);
+
+    campaignCreatorRepositoryStub = {
+      get: sinon.stub(),
+    };
+
+    campaignCreatorRepositoryStub.get.withArgs(campaignsToCreate[0].organizationId).resolves(campaignCreatorPOJO);
+    campaignCreatorRepositoryStub.get.withArgs(campaignsToCreate[1].organizationId).resolves(campaignCreatorPOJO);
+
+    createdCampaignsSymbol = Symbol('');
+    campaignAdministrationRepositoryStub.save.withArgs(campaignsWithAllData).resolves(createdCampaignsSymbol);
+  });
+
   describe('success case', function () {
     it('should create campaigns', async function () {
-      const campaignAdministrationRepository = {
-        save: sinon.stub(),
-      };
-      const userRepositoryStub = {
-        get: sinon.stub(),
-      };
-      const organizationRepositoryStub = {
-        get: sinon.stub(),
-      };
-      const code1 = Symbol('code1');
-      const code2 = Symbol('code2');
-      const someoneId = 454756;
-      const otherOrganization = domainBuilder.buildOrganization({ id: 3 });
-      const organization = domainBuilder.buildOrganization({ id: 4 });
-
-      const codeGeneratorStub = {
-        generate: sinon
-          .stub()
-          .withArgs(campaignAdministrationRepository)
-          .onFirstCall()
-          .resolves(code1)
-          .onSecondCall()
-          .resolves(code2),
-      };
-
-      const campaignsToCreate = [
-        {
-          organizationId: organization.id,
-          name: 'My Campaign',
-          targetProfileId: 3,
-          creatorId: 2,
-          ownerId: someoneId,
-        },
-        {
-          organizationId: otherOrganization.id,
-          name: 'My other Campaign',
-          targetProfileId: 3,
-          creatorId: 1,
-          ownerId: someoneId,
-        },
-      ];
-
-      const campaignsWithAllData = [
-        {
-          ...campaignsToCreate[0],
-          type: 'ASSESSMENT',
-          code: code1,
-        },
-        {
-          ...campaignsToCreate[1],
-          type: 'ASSESSMENT',
-          code: code2,
-        },
-      ];
-
-      const campaignCreatorPOJO = {
-        createCampaign: sinon.stub(),
-      };
-
-      campaignCreatorPOJO.createCampaign
-        .withArgs({ ...campaignsToCreate[0], type: CampaignTypes.ASSESSMENT, code: code1, ownerId: someoneId })
-        .resolves(campaignsWithAllData[0]);
-      campaignCreatorPOJO.createCampaign
-        .withArgs({ ...campaignsToCreate[1], type: CampaignTypes.ASSESSMENT, code: code2, ownerId: someoneId })
-        .resolves(campaignsWithAllData[1]);
-
-      const campaignCreatorRepositoryStub = {
-        get: sinon.stub(),
-      };
-
       organizationRepositoryStub.get.withArgs(campaignsToCreate[0].organizationId).resolves();
       userRepositoryStub.get.withArgs(campaignsToCreate[0].ownerId).resolves();
       organizationRepositoryStub.get.withArgs(campaignsToCreate[1].organizationId).resolves();
       userRepositoryStub.get.withArgs(campaignsToCreate[1].ownerId).resolves();
 
-      campaignCreatorRepositoryStub.get.withArgs(campaignsToCreate[0].organizationId).resolves(campaignCreatorPOJO);
-      campaignCreatorRepositoryStub.get.withArgs(campaignsToCreate[1].organizationId).resolves(campaignCreatorPOJO);
-
-      const createdCampaignsSymbol = Symbol('');
-      campaignAdministrationRepository.save.withArgs(campaignsWithAllData).resolves(createdCampaignsSymbol);
-
       const result = await createCampaigns({
         campaignsToCreate,
-        campaignAdministrationRepository,
+        campaignAdministrationRepository: campaignAdministrationRepositoryStub,
         codeGenerator: codeGeneratorStub,
         campaignCreatorRepository: campaignCreatorRepositoryStub,
         userRepository: userRepositoryStub,
@@ -102,88 +112,12 @@ describe('Unit | UseCase | campaign-administration | create-campaigns', function
 
   describe('errors case', function () {
     it('should throw error if ownerId does not exist', async function () {
-      const campaignAdministrationRepository = {
-        save: sinon.stub(),
-      };
-      const userRepositoryStub = {
-        get: sinon.stub(),
-      };
-      const organizationRepositoryStub = {
-        get: sinon.stub(),
-      };
-      const code1 = Symbol('code1');
-      const code2 = Symbol('code2');
-      const someoneId = 454756;
-      const otherOrganization = domainBuilder.buildOrganization({ id: 3 });
-      const organization = domainBuilder.buildOrganization({ id: 4 });
-
-      const codeGeneratorStub = {
-        generate: sinon
-          .stub()
-          .withArgs(campaignAdministrationRepository)
-          .onFirstCall()
-          .resolves(code1)
-          .onSecondCall()
-          .resolves(code2),
-      };
-
-      const campaignsToCreate = [
-        {
-          organizationId: organization.id,
-          name: 'My Campaign',
-          targetProfileId: 3,
-          creatorId: 2,
-          ownerId: someoneId,
-        },
-        {
-          organizationId: otherOrganization.id,
-          name: 'My other Campaign',
-          targetProfileId: 3,
-          creatorId: 1,
-          ownerId: someoneId,
-        },
-      ];
-
-      const campaignsWithAllData = [
-        {
-          ...campaignsToCreate[0],
-          type: 'ASSESSMENT',
-          code: code1,
-        },
-        {
-          ...campaignsToCreate[1],
-          type: 'ASSESSMENT',
-          code: code2,
-        },
-      ];
-
-      const campaignCreatorPOJO = {
-        createCampaign: sinon.stub(),
-      };
-
-      campaignCreatorPOJO.createCampaign
-        .withArgs({ ...campaignsToCreate[0], type: CampaignTypes.ASSESSMENT, code: code1, ownerId: someoneId })
-        .resolves(campaignsWithAllData[0]);
-      campaignCreatorPOJO.createCampaign
-        .withArgs({ ...campaignsToCreate[1], type: CampaignTypes.ASSESSMENT, code: code2, ownerId: someoneId })
-        .resolves(campaignsWithAllData[1]);
-
-      const campaignCreatorRepositoryStub = {
-        get: sinon.stub(),
-      };
-
       organizationRepositoryStub.get.withArgs(campaignsToCreate[0].organizationId).resolves();
       userRepositoryStub.get.withArgs(campaignsToCreate[0].ownerId).rejects(new UserNotFoundError());
 
-      campaignCreatorRepositoryStub.get.withArgs(campaignsToCreate[0].organizationId).resolves(campaignCreatorPOJO);
-      campaignCreatorRepositoryStub.get.withArgs(campaignsToCreate[1].organizationId).resolves(campaignCreatorPOJO);
-
-      const createdCampaignsSymbol = Symbol('');
-      campaignAdministrationRepository.save.withArgs(campaignsWithAllData).resolves(createdCampaignsSymbol);
-
       const error = await catchErr(createCampaigns)({
         campaignsToCreate,
-        campaignAdministrationRepository,
+        campaignAdministrationRepository: campaignAdministrationRepositoryStub,
         codeGenerator: codeGeneratorStub,
         campaignCreatorRepository: campaignCreatorRepositoryStub,
         userRepository: userRepositoryStub,
@@ -195,88 +129,12 @@ describe('Unit | UseCase | campaign-administration | create-campaigns', function
     });
 
     it('should throw error if organization does not exist', async function () {
-      const campaignAdministrationRepository = {
-        save: sinon.stub(),
-      };
-      const userRepositoryStub = {
-        get: sinon.stub(),
-      };
-      const organizationRepositoryStub = {
-        get: sinon.stub(),
-      };
-      const code1 = Symbol('code1');
-      const code2 = Symbol('code2');
-      const someoneId = 454756;
-      const otherOrganization = domainBuilder.buildOrganization({ id: 3 });
-      const organization = domainBuilder.buildOrganization({ id: 4 });
-
-      const codeGeneratorStub = {
-        generate: sinon
-          .stub()
-          .withArgs(campaignAdministrationRepository)
-          .onFirstCall()
-          .resolves(code1)
-          .onSecondCall()
-          .resolves(code2),
-      };
-
-      const campaignsToCreate = [
-        {
-          organizationId: organization.id,
-          name: 'My Campaign',
-          targetProfileId: 3,
-          creatorId: 2,
-          ownerId: someoneId,
-        },
-        {
-          organizationId: otherOrganization.id,
-          name: 'My other Campaign',
-          targetProfileId: 3,
-          creatorId: 1,
-          ownerId: someoneId,
-        },
-      ];
-
-      const campaignsWithAllData = [
-        {
-          ...campaignsToCreate[0],
-          type: 'ASSESSMENT',
-          code: code1,
-        },
-        {
-          ...campaignsToCreate[1],
-          type: 'ASSESSMENT',
-          code: code2,
-        },
-      ];
-
-      const campaignCreatorPOJO = {
-        createCampaign: sinon.stub(),
-      };
-
-      campaignCreatorPOJO.createCampaign
-        .withArgs({ ...campaignsToCreate[0], type: CampaignTypes.ASSESSMENT, code: code1, ownerId: someoneId })
-        .resolves(campaignsWithAllData[0]);
-      campaignCreatorPOJO.createCampaign
-        .withArgs({ ...campaignsToCreate[1], type: CampaignTypes.ASSESSMENT, code: code2, ownerId: someoneId })
-        .resolves(campaignsWithAllData[1]);
-
-      const campaignCreatorRepositoryStub = {
-        get: sinon.stub(),
-      };
-
       organizationRepositoryStub.get.withArgs(campaignsToCreate[0].organizationId).rejects(new NotFoundError());
       userRepositoryStub.get.withArgs(campaignsToCreate[0].ownerId).resolves();
 
-      campaignCreatorRepositoryStub.get.withArgs(campaignsToCreate[0].organizationId).resolves(campaignCreatorPOJO);
-      campaignCreatorRepositoryStub.get.withArgs(campaignsToCreate[1].organizationId).resolves(campaignCreatorPOJO);
-
-      const createdCampaignsSymbol = Symbol('');
-      campaignAdministrationRepository.save.withArgs(campaignsWithAllData).resolves(createdCampaignsSymbol);
-
       const error = await catchErr(createCampaigns)({
         campaignsToCreate,
-        campaignAdministrationRepository,
+        campaignAdministrationRepository: campaignAdministrationRepositoryStub,
         codeGenerator: codeGeneratorStub,
         campaignCreatorRepository: campaignCreatorRepositoryStub,
         userRepository: userRepositoryStub,
