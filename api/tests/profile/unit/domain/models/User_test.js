e50f4f0737f9c8635066025da2be5acec37f1fb0
Yvonnick Frin <frin.yvonnick@gmail.com>
2024-10-03 14:04:54 +0200

    feat(api): add User model in profile context

---
+++ api/tests/profile/unit/domain/models/User_test.js
@@ -0,0 +1,40 @@
+import { User } from '../../../../../src/profile/domain/models/User.js';
+import { expect, sinon } from '../../../../test-helper.js';
+
+describe('Unit | Profile | Domain | Models | User', function () {
+  let clock;
+  const now = new Date('2022-12-25');
+
+  beforeEach(function () {
+    clock = sinon.useFakeTimers({
+      now,
+      toFake: ['Date'],
+    });
+  });
+
+  afterEach(async function () {
+    clock.restore();
+  });
+
+  it('should return fullname', function () {
+    // when
+    const user = new User({ firstName: 'Théo', lastName: 'Courant' });
+
+    // then
+    expect(user.fullName).to.equal('Théo COURANT');
+  });
+
+  it('should transform to form', function () {
+    // given
+    const user = new User({ firstName: 'Théo', lastName: 'Courant' });
+    const date = new Date('2024-10-02');
+
+    // when
+    const form = user.toForm(date);
+
+    // then
+    expect(form.get('fullName')).to.deep.equal(user.fullName);
+    expect(form.get('filename')).to.deep.equal(user.fullName + Date.now());
+    expect(form.get('date')).to.deep.equal(date);
+  });
+});
