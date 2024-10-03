552af1abfe2681d5c9b8539b1520487e1ab83f71
Yvonnick Frin <frin.yvonnick@gmail.com>
2024-10-03 14:04:25 +0200

    feat(api): add createdAt in ProfileReward model

--- api/src/profile/domain/models/ProfileReward.js
+++ api/src/profile/domain/models/ProfileReward.js
@@ -1,7 +1,8 @@
 export class ProfileReward {
-  constructor({ id, rewardId, rewardType } = {}) {
+  constructor({ id, rewardId, rewardType, createdAt } = {}) {
     this.id = id;
     this.rewardId = rewardId;
     this.rewardType = rewardType;
+    this.createdAt = createdAt;
   }
 }
