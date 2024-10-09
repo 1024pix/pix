ef380803c8561c82c98a1fef09656b4c0af71d74
Quentin Lebouc <quentin.lebouc@pix.fr>
2024-10-09 14:45:58 +0200

    WIP eval repo to identity access management

--- api/src/identity-access-management/application/api/users-api.js
+++ api/src/identity-access-management/application/api/users-api.js
@@ -12,6 +12,10 @@ export const markNewDashboardInfoAsSeen = async ({ userId }) => {
   return usecases.markUserHasSeenNewDashboardInfo({ userId });
 };
 
+export const markNewDashboardInfoAsSeen = async ({ userId }) => {
+  return usecases.markUserHasSeenNewDashboardInfo({ userId });
+};
+
 export const getUserDetailsByUserIds = async ({ userIds }) => {
   const users = await usecases.getUserDetailsByUserIds({ userIds });
   return users.map((user) => new UserDTO(user));
