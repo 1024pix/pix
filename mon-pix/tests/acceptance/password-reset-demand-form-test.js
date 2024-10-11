e1f7a109dd491b2154886d502fd4071dfa7653a5
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2024-10-11 16:07:17 +0200

    feat(mon-pix): use new component authentication/password-reset-demand

--- mon-pix/tests/acceptance/password-reset-demand-form-test.js
+++ mon-pix/tests/acceptance/password-reset-demand-form-test.js
@@ -29,7 +29,7 @@ module('Acceptance | Password reset demand form', function (hooks) {
       assert.strictEqual(currentURL(), '/mot-de-passe-oublie');
     });
 
-    test('should stay on mot de passe oublié page, and show success message, when email sent correspond to an existing user', async function (assert) {
+    test('stays on mot de passe oublié page, and shows success message when email sent correspond to an existing user', async function (assert) {
       // given
       this.server.create('user', {
         id: 1,
@@ -48,7 +48,7 @@ module('Acceptance | Password reset demand form', function (hooks) {
       assert.dom('.password-reset-demand-form__body').exists();
     });
 
-    test('should stay in mot-passe-oublie page when sent email do not correspond to any existing user', async function (assert) {
+    test('stays in mot-passe-oublie page when sent email do not correspond to any existing user', async function (assert) {
       // given
       this.server.create('user', {
         id: 1,
@@ -66,6 +66,44 @@ module('Acceptance | Password reset demand form', function (hooks) {
       // then
       assert.strictEqual(currentURL(), '/mot-de-passe-oublie');
       assert.dom(screen.getByText(t('pages.password-reset-demand.error.message'))).exists();
+    });
+  });
+
+  module('when "New authentication design" feature toggle is enabled', function (hooks) {
+    hooks.beforeEach(function () {
+      server.create('feature-toggle', {
+        id: 0,
+        isNewAuthenticationDesignEnabled: true,
+      });
+    });
+
+    test('can visit /mot-passe-oublie', async function (assert) {
+      // when
+      await visit('/mot-de-passe-oublie');
+
+      // then
+      assert.strictEqual(currentURL(), '/mot-de-passe-oublie');
+    });
+
+    test('stays on "mot de passe oublié" page, and shows success message when email sent correspond to an existing user', async function (assert) {
+      // given
+      this.server.create('user', {
+        id: 1,
+        firstName: 'Brandone',
+        lastName: 'Martins',
+        email: 'brandone.martins@pix.com',
+        password: '1024pix!',
+      });
+      const screen = await visit('/mot-de-passe-oublie');
+      await fillIn(
+        screen.getByRole('textbox', { name: t('pages.password-reset-demand.fields.email.label') }),
+        'brandone.martins@pix.com',
+      );
+
+      // when
+      await clickByLabel(t('components.authentication.password-reset-demand-form.actions.receive-reset-button'));
+
+      assert.strictEqual(currentURL(), '/mot-de-passe-oublie');
     });
   });
 });
