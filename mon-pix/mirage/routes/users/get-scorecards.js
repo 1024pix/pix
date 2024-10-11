77a60e8a55a06e8cf28063e3535cf973c9402f67
Benjamin Petetot <benjamin.petetot@pix.fr>
2024-10-11 18:34:19 +0200

    feat(mon-pix): add acceptance tests for signup and login

--- mon-pix/mirage/routes/users/get-scorecards.js
+++ mon-pix/mirage/routes/users/get-scorecards.js
@@ -1,4 +1,8 @@
 export default function (schema, request) {
   const userId = request.params.id;
-  return schema.users.find(userId).scorecards;
+  const user = schema.users.find(userId);
+
+  if (!user.scorecards) return { data: null };
+
+  return user.scorecards;
 }
