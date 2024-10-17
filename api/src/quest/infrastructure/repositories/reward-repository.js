286262221370534bfbe25d8b91555fe37c850708
Alexandre Monney <alexandre.monney@pix.fr>
2024-10-17 19:17:30 +0200

    feat(api): add getByQuestAndUserId method in reward repository

--- api/src/quest/infrastructure/repositories/reward-repository.js
+++ api/src/quest/infrastructure/repositories/reward-repository.js
@@ -1,7 +1,46 @@
+import { QuestResult } from '../../domain/models/QuestResult.js';
+
 export const reward = async ({ userId, rewardId, profileRewardApi }) => {
   return profileRewardApi.save(userId, rewardId);
 };
 
 export const getByUserId = async ({ userId, profileRewardApi }) => {
   return profileRewardApi.getByUserId(userId);
+};
+
+export const getByQuestAndUserId = async ({
+  userId,
+  quest,
+  rewardApi,
+  profileRewardApi,
+  profileRewardTemporaryStorage,
+}) => {
+  const reward = await rewardApi.getByIdAndType({ rewardId: quest.rewardId, rewardType: quest.rewardType });
+  const profileRewards = await profileRewardApi.getByUserId(userId);
+
+  const profileRewardForQuest = profileRewards.find(
+    (profileReward) => profileReward.rewardType === quest.rewardType && profileReward.rewardId === quest.rewardId,
+  );
+
+  if (profileRewardForQuest) {
+    return new QuestResult({
+      id: quest.id,
+      obtained: true,
+      reward,
+    });
+  }
+
+  let obtained = false;
+
+  const isProcessing = Number(await profileRewardTemporaryStorage.get(userId)) > 0;
+
+  if (isProcessing) {
+    obtained = null;
+  }
+
+  return new QuestResult({
+    id: quest.id,
+    obtained,
+    reward,
+  });
 };
