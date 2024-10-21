32dfcd7531aac9fa590d3894542f03584f2b3a02
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2024-10-21 09:41:25 +0200

    feat(certif): add script to migrate v2 centers to v3

--- api/tests/integration/scripts/certification/next-gen/convert-all-centers-to-v3_test.js
+++ api/tests/integration/scripts/certification/next-gen/convert-all-centers-to-v3_test.js
@@ -2,13 +2,31 @@ import { main } from '../../../../../scripts/certification/next-gen/convert-all-
 import { expect, sinon } from '../../../../test-helper.js';
 
 describe('Integration | Scripts | Certification | convert-centers-to-v3', function () {
-  it('should save call convertSessionsWithNoCoursesToV3 usecase', async function () {
+  it('should call convertSessionsWithNoCoursesToV3 usecase', async function () {
     const dependencies = {
       convertSessionsWithNoCoursesToV3: sinon.stub().resolves(),
+      convertCentersToV3: sinon.stub().resolves(),
     };
-    await main({ dependencies });
+    const isDryRun = Symbol('isDryRun');
+    await main({ isDryRun, dependencies });
 
     // then
-    expect(dependencies.convertSessionsWithNoCoursesToV3).to.have.been.calledOnce;
+    expect(dependencies.convertSessionsWithNoCoursesToV3).to.have.been.calledOnceWith({ isDryRun });
+  });
+
+  it('should call convertCentersToV3 usecase', async function () {
+    const dependencies = {
+      convertSessionsWithNoCoursesToV3: sinon.stub().resolves(),
+      convertCentersToV3: sinon.stub().resolves(),
+    };
+    const isDryRun = Symbol('isDryRun');
+    const preservedCenterIds = Symbol('preservedCenterIds');
+    await main({ isDryRun, preservedCenterIds, dependencies });
+
+    // then
+    expect(dependencies.convertCentersToV3).to.have.been.calledOnceWith({
+      isDryRun,
+      preservedCenterIds,
+    });
   });
 });
