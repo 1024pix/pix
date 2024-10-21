f1ee9605dae1f61136cd5f3c68a127d2d2080da5
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2024-10-21 11:30:08 +0200

    test(mon-pix): add isNewAuthenticationDesignEnabled on tests/acceptance/reset-password-form-test.js

--- mon-pix/tests/acceptance/reset-password-form-test.js
+++ mon-pix/tests/acceptance/reset-password-form-test.js
@@ -14,75 +14,84 @@ module('Acceptance | Reset Password Form', function (hooks) {
   setupMirage(hooks);
   setupIntl(hooks);
 
-  test('can visit /changer-mot-de-passe when temporaryKey exists', async function (assert) {
-    // given
-    server.create('user', {
-      id: 1000,
-      firstName: 'Brandone',
-      lastName: 'Martins',
-      email: 'brandone.martins@pix.com',
-      password: '1024pix!',
+  module('when "New authentication design" feature toggle is disabled', function (hooks) {
+    hooks.beforeEach(function () {
+      server.create('feature-toggle', {
+        id: 0,
+        isNewAuthenticationDesignEnabled: false,
+      });
     });
 
-    server.create('password-reset-demand', {
-      temporaryKey: 'temporaryKey',
-      email: 'brandone.martins@pix.com',
+    test('can visit /changer-mot-de-passe when temporaryKey exists', async function (assert) {
+      // given
+      server.create('user', {
+        id: 1000,
+        firstName: 'Brandone',
+        lastName: 'Martins',
+        email: 'brandone.martins@pix.com',
+        password: '1024pix!',
+      });
+
+      server.create('password-reset-demand', {
+        temporaryKey: 'temporaryKey',
+        email: 'brandone.martins@pix.com',
+      });
+
+      // when
+      await visit('/changer-mot-de-passe/temporaryKey');
+
+      // then
+      assert.strictEqual(currentURL(), '/changer-mot-de-passe/temporaryKey');
     });
 
-    // when
-    await visit('/changer-mot-de-passe/temporaryKey');
+    test('stays on /changer-mot-de-passe when password is successfully reset', async function (assert) {
+      // given
+      server.create('user', {
+        id: 1000,
+        firstName: 'Brandone',
+        lastName: 'Martins',
+        email: 'brandone.martins@pix.com',
+        password: '1024pix!',
+      });
 
-    // then
-    assert.strictEqual(currentURL(), '/changer-mot-de-passe/temporaryKey');
-  });
+      server.create('password-reset-demand', {
+        temporaryKey: 'brandone-reset-key',
+        email: 'brandone.martins@pix.com',
+      });
 
-  test('should stay on changer-mot-de-passe when password is successfully reset', async function (assert) {
-    // given
-    server.create('user', {
-      id: 1000,
-      firstName: 'Brandone',
-      lastName: 'Martins',
-      email: 'brandone.martins@pix.com',
-      password: '1024pix!',
+      const screen = await visit('/changer-mot-de-passe/brandone-reset-key');
+      const passwordInput = screen.getByLabelText('Mot de passe');
+      await fillIn(passwordInput, 'newPass12345!');
+
+      // when
+      await clickByLabel(t('pages.reset-password.actions.submit'));
+
+      // then
+      assert.strictEqual(currentURL(), '/changer-mot-de-passe/brandone-reset-key');
     });
 
-    server.create('password-reset-demand', {
-      temporaryKey: 'brandone-reset-key',
-      email: 'brandone.martins@pix.com',
+    test('allows connected user to visit reset-password page', async function (assert) {
+      // given
+      const user = server.create('user', {
+        id: 1000,
+        firstName: 'Brandone',
+        lastName: 'Martins',
+        email: 'brandone.martins@pix.com',
+        password: '1024pix!',
+      });
+
+      server.create('password-reset-demand', {
+        temporaryKey: 'brandone-reset-key',
+        email: 'brandone.martins@pix.com',
+      });
+
+      await authenticate(user);
+
+      // when
+      await visit('/changer-mot-de-passe/brandone-reset-key');
+
+      // then
+      assert.strictEqual(currentURL(), '/changer-mot-de-passe/brandone-reset-key');
     });
-
-    const screen = await visit('/changer-mot-de-passe/brandone-reset-key');
-    const passwordInput = screen.getByLabelText('Mot de passe');
-    await fillIn(passwordInput, 'newPass12345!');
-
-    // when
-    await clickByLabel(t('pages.reset-password.actions.submit'));
-
-    // then
-    assert.strictEqual(currentURL(), '/changer-mot-de-passe/brandone-reset-key');
-  });
-
-  test('should allow connected user to visit reset-password page', async function (assert) {
-    // given
-    const user = server.create('user', {
-      id: 1000,
-      firstName: 'Brandone',
-      lastName: 'Martins',
-      email: 'brandone.martins@pix.com',
-      password: '1024pix!',
-    });
-
-    server.create('password-reset-demand', {
-      temporaryKey: 'brandone-reset-key',
-      email: 'brandone.martins@pix.com',
-    });
-
-    await authenticate(user);
-
-    // when
-    await visit('/changer-mot-de-passe/brandone-reset-key');
-
-    // then
-    assert.strictEqual(currentURL(), '/changer-mot-de-passe/brandone-reset-key');
   });
 });
