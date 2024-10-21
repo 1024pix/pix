9157fcb11daa01483f049908e0ebd60b91f5e607
Eric Lim <eric.lim@zenika.com>
2024-10-21 09:29:59 +0200

    feat(mon-pix): use new component in reset password page

--- mon-pix/tests/acceptance/reset-password-form-test.js
+++ mon-pix/tests/acceptance/reset-password-form-test.js
@@ -94,4 +94,87 @@ module('Acceptance | Reset Password Form', function (hooks) {
       assert.strictEqual(currentURL(), '/changer-mot-de-passe/brandone-reset-key');
     });
   });
+
+  module('when "New authentication design" feature toggle is enabled', function (hooks) {
+    hooks.beforeEach(function () {
+      server.create('feature-toggle', {
+        id: 0,
+        isNewAuthenticationDesignEnabled: true,
+      });
+    });
+
+    test('can visit /changer-mot-de-passe when temporaryKey exists', async function (assert) {
+      // given
+      server.create('user', {
+        id: 1000,
+        firstName: 'Brandone',
+        lastName: 'Martins',
+        email: 'brandone.martins@pix.com',
+        password: '1024pix!',
+      });
+
+      server.create('password-reset-demand', {
+        temporaryKey: 'temporaryKey',
+        email: 'brandone.martins@pix.com',
+      });
+
+      // when
+      await visit('/changer-mot-de-passe/temporaryKey');
+
+      // then
+      assert.strictEqual(currentURL(), '/changer-mot-de-passe/temporaryKey');
+    });
+
+    test('stays on /changer-mot-de-passe when password is successfully reset', async function (assert) {
+      // given
+      server.create('user', {
+        id: 1000,
+        firstName: 'Brandone',
+        lastName: 'Martins',
+        email: 'brandone.martins@pix.com',
+        password: '1024pix!',
+      });
+
+      server.create('password-reset-demand', {
+        temporaryKey: 'brandone-reset-key',
+        email: 'brandone.martins@pix.com',
+      });
+
+      const screen = await visit('/changer-mot-de-passe/brandone-reset-key');
+      const passwordInput = screen.getByLabelText(
+        t('components.authentication.password-reset-form.fields.password.label'),
+      );
+      await fillIn(passwordInput, 'newPass12345!');
+
+      // when
+      await clickByLabel(t('components.authentication.password-reset-form.actions.submit'));
+
+      // then
+      assert.strictEqual(currentURL(), '/changer-mot-de-passe/brandone-reset-key');
+    });
+
+    test('allows connected user to visit reset-password page', async function (assert) {
+      // given
+      const user = server.create('user', {
+        id: 1000,
+        firstName: 'Brandone',
+        lastName: 'Martins',
+        email: 'brandone.martins@pix.com',
+        password: '1024pix!',
+      });
+
+      server.create('password-reset-demand', {
+        temporaryKey: 'brandone-reset-key',
+        email: 'brandone.martins@pix.com',
+      });
+
+      await authenticate(user);
+
+      // when
+      await visit('/changer-mot-de-passe/brandone-reset-key');
+
+      // then
+      assert.strictEqual(currentURL(), '/changer-mot-de-passe/brandone-reset-key');
+    });
+  });
 });
