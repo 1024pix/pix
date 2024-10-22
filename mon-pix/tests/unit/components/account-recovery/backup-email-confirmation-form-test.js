2bea71b46e967c0dc4935169cd74eebd174309f8
Guillaume Olejniczak <la.toile.cosmique@gmail.com>
2024-10-22 15:51:59 +0200

    WIP

--- mon-pix/tests/unit/components/account-recovery/backup-email-confirmation-form-test.js
+++ mon-pix/tests/unit/components/account-recovery/backup-email-confirmation-form-test.js
@@ -26,6 +26,26 @@ module('Unit | Component | account-recovery | backup-email-confirmation-form', f
     });
   });
 
+  module('#submitBackupEmailConfirmationForm', function () {
+    test('should call sendEmail', function (assert) {
+      // given
+      const sendEmail = sinon.stub();
+      const component = createGlimmerComponent('account-recovery/backup-email-confirmation-form', {
+        sendEmail,
+      });
+      component.email = 'john.doe@example.net';
+      component.args.sendEmail = sendEmail;
+      const event = { preventDefault: sinon.stub() };
+
+      // when
+      component.submitBackupEmailConfirmationForm(event);
+
+      // then
+      sinon.assert.calledWith(sendEmail, 'john.doe@example.net');
+      assert.ok(true);
+    });
+  });
+
   module('#isSubmitButtonEnabled', function () {
     test('should return false if email is empty', function (assert) {
       // given
