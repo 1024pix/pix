4880436353e5f79e117b67c743310cef66e62495
Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
2024-10-22 15:33:40 +0200

    fix(mon-pix): display only always visible not-acquired badges

--- mon-pix/tests/integration/components/campaigns/assessment/skill-review/rewards/rewards-test.js
+++ mon-pix/tests/integration/components/campaigns/assessment/skill-review/rewards/rewards-test.js
@@ -1,4 +1,4 @@
-import { render } from '@1024pix/ember-testing-library';
+import { render, within } from '@1024pix/ember-testing-library';
 import { hbs } from 'ember-cli-htmlbars';
 import { t } from 'ember-intl/test-support';
 import { module, test } from 'qunit';
@@ -22,11 +22,13 @@ module('Integration | Components | Campaigns | Assessment | Evaluation Results T
 
     const badgeNotAcquired1 = store.createRecord('campaign-participation-badge', {
       isAcquired: false,
+      isAlwaysVisible: true,
       acquisitionPercentage: 30,
     });
 
     const badgeNotAcquired2 = store.createRecord('campaign-participation-badge', {
       isAcquired: false,
+      isAlwaysVisible: true,
       acquisitionPercentage: 70,
     });
 
@@ -94,6 +96,43 @@ module('Integration | Components | Campaigns | Assessment | Evaluation Results T
   });
 
   module('in not acquired badges list', function () {
+    test('it should only display isAlwaysVisible badges', async function (assert) {
+      // given
+      const store = this.owner.lookup('service:store');
+
+      const notVisibleBadge1 = store.createRecord('campaign-participation-badge', {
+        isAcquired: false,
+        isAlwaysVisible: false,
+        acquisitionPercentage: 66,
+      });
+      const notVisibleBadge2 = store.createRecord('campaign-participation-badge', {
+        isAcquired: false,
+        isAlwaysVisible: false,
+        acquisitionPercentage: 66,
+      });
+
+      const alwaysVisibleBadge = store.createRecord('campaign-participation-badge', {
+        isAcquired: false,
+        isAlwaysVisible: true,
+        acquisitionPercentage: 33,
+      });
+
+      this.set('badges', [notVisibleBadge1, notVisibleBadge2, alwaysVisibleBadge]);
+
+      // when
+      const screen = await render(
+        hbs`<Campaigns::Assessment::SkillReview::EvaluationResultsTabs::Rewards @badges={{this.badges}} />`,
+      );
+
+      // then
+      const notAcquiredBadges = screen.getAllByRole('listitem', {
+        name: t('pages.skill-review.badge-card.not-acquired-full'),
+      });
+
+      assert.strictEqual(notAcquiredBadges.length, 1);
+      assert.ok(within(notAcquiredBadges[0]).getAllByText(/33/).length);
+    });
+
     test('it should display certifiable badge first', async function (assert) {
       // given
       const store = this.owner.lookup('service:store');
@@ -101,17 +140,20 @@ module('Integration | Components | Campaigns | Assessment | Evaluation Results T
       const certifiableBadge = store.createRecord('campaign-participation-badge', {
         isAcquired: false,
         isCertifiable: true,
+        isAlwaysVisible: true,
         acquisitionPercentage: 60,
       });
 
       const notCertifiableBadge1 = store.createRecord('campaign-participation-badge', {
         isAcquired: false,
         isCertifiable: false,
+        isAlwaysVisible: true,
         acquisitionPercentage: 60,
       });
       const notCertifiableBadge2 = store.createRecord('campaign-participation-badge', {
         isAcquired: false,
         isCertifiable: false,
+        isAlwaysVisible: true,
         acquisitionPercentage: 60,
       });
 
