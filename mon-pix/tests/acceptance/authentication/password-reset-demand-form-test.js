f8e7d631b799606162a833fc736ebfaa845581e9
Benjamin Petetot <benjamin.petetot@pix.fr>
2024-10-31 17:15:53 +0100

    refactor(mon-pix): remove legacy tests with FT disabled

--- mon-pix/tests/acceptance/authentication/password-reset-demand-form-test.js
+++ mon-pix/tests/acceptance/authentication/password-reset-demand-form-test.js
@@ -13,62 +13,6 @@ module('Acceptance | Password reset demand form', function (hooks) {
   setupMirage(hooks);
   setupIntl(hooks);
 
-  module('when "New authentication design" feature toggle is disabled', function (hooks) {
-    hooks.beforeEach(function () {
-      server.create('feature-toggle', {
-        id: 0,
-        isNewAuthenticationDesignEnabled: false,
-      });
-    });
-
-    test('can visit /mot-passe-oublie', async function (assert) {
-      // when
-      await visit('/mot-de-passe-oublie');
-
-      // then
-      assert.strictEqual(currentURL(), '/mot-de-passe-oublie');
-    });
-
-    test('stays on "mot de passe oublié" page, and shows success message when email sent correspond to an existing user', async function (assert) {
-      // given
-      this.server.create('user', {
-        id: 1,
-        firstName: 'Brandone',
-        lastName: 'Martins',
-        email: 'brandone.martins@pix.com',
-        password: '1024pix!',
-      });
-      await visit('/mot-de-passe-oublie');
-      await fillIn('#email', 'brandone.martins@pix.com');
-
-      // when
-      await clickByLabel(t('pages.password-reset-demand.actions.reset'));
-
-      assert.strictEqual(currentURL(), '/mot-de-passe-oublie');
-      assert.dom('.password-reset-demand-form__body').exists();
-    });
-
-    test('stays in mot-passe-oublie page when sent email do not correspond to any existing user', async function (assert) {
-      // given
-      this.server.create('user', {
-        id: 1,
-        firstName: 'Brandone',
-        lastName: 'Martins',
-        email: 'brandone.martins@pix.com',
-        password: '1024pix!',
-      });
-      const screen = await visit('/mot-de-passe-oublie');
-      await fillIn('#email', 'unexisting@user.com');
-
-      // when
-      await clickByLabel(t('pages.password-reset-demand.actions.reset'));
-
-      // then
-      assert.strictEqual(currentURL(), '/mot-de-passe-oublie');
-      assert.dom(screen.getByText(t('pages.password-reset-demand.error.message'))).exists();
-    });
-  });
-
   module('when "New authentication design" feature toggle is enabled', function (hooks) {
     hooks.beforeEach(function () {
       server.create('feature-toggle', {
--- mon-pix/tests/acceptance/authentication/signup-test.js
+++ mon-pix/tests/acceptance/authentication/signup-test.js
@@ -1,5 +1,5 @@
 import { clickByName, fillByLabel, visit } from '@1024pix/ember-testing-library';
-import { click, currentURL } from '@ember/test-helpers';
+import { currentURL } from '@ember/test-helpers';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import { t } from 'ember-intl/test-support';
 import { setupApplicationTest } from 'ember-qunit';
@@ -29,72 +29,6 @@ module('Acceptance | authentication | Signup', function (hooks) {
 
     domainService = this.owner.lookup('service:currentDomain');
     sinon.stub(domainService, 'getExtension');
-  });
-
-  module('when "New authentication design" feature toggle is disabled', function (hooks) {
-    hooks.beforeEach(function () {
-      server.create('feature-toggle', { id: 0, isNewAuthenticationDesignEnabled: false });
-    });
-
-    module('When on International domain (.org)', function (hooks) {
-      hooks.beforeEach(function () {
-        domainService.getExtension.returns('org');
-      });
-
-      module('when accessing the inscription page with "Français" as default language', function () {
-        test('displays the inscription page with "Français" as selected language', async function (assert) {
-          // given & when
-          const screen = await visit('/inscription');
-
-          // then
-          assert.strictEqual(currentURL(), '/inscription');
-          assert.dom(screen.getByRole('heading', { name: 'Inscrivez-vous', level: 1 })).exists();
-          assert.dom(screen.getByRole('button', { name: 'Sélectionnez une langue' })).exists();
-        });
-
-        module('when the user select "English" as his language', function () {
-          test('displays the inscription page with "English" as selected language', async function (assert) {
-            // given & when
-            const screen = await visit('/inscription');
-            await click(screen.getByRole('button', { name: 'Sélectionnez une langue' }));
-            await screen.findByRole('listbox');
-            await click(screen.getByRole('option', { name: 'English' }));
-
-            // then
-            assert.strictEqual(currentURL(), '/inscription');
-            assert.dom(screen.getByRole('heading', { name: 'Sign up', level: 1 })).exists();
-            assert.dom(screen.getByRole('button', { name: 'Select a language' })).exists();
-          });
-        });
-      });
-
-      module('when accessing the inscription page with "English" as selected language', function () {
-        test('displays the inscription page with "English"', async function (assert) {
-          // given && when
-          const screen = await visit('/inscription?lang=en');
-
-          // then
-          assert.strictEqual(currentURL(), '/inscription?lang=en');
-          assert.dom(screen.getByRole('heading', { name: 'Sign up', level: 1 })).exists();
-          assert.dom(screen.getByRole('button', { name: 'Select a language' })).exists();
-        });
-
-        module('when the user select "Français" as his language', function () {
-          test('displays the inscription page with "Français" as selected language', async function (assert) {
-            // given & when
-            const screen = await visit('/inscription?lang=en');
-            await click(screen.getByRole('button', { name: 'Select a language' }));
-            await screen.findByRole('listbox');
-            await click(screen.getByRole('option', { name: 'Français' }));
-
-            // then
-            assert.strictEqual(currentURL(), '/inscription');
-            assert.dom(screen.getByRole('heading', { name: 'Inscrivez-vous', level: 1 })).exists();
-            assert.dom(screen.getByRole('button', { name: 'Sélectionnez une langue' })).exists();
-          });
-        });
-      });
-    });
   });
 
   module('when "New authentication design" feature toggle is enabled', function (hooks) {
