41d03c3d827e52b7616007fc569499a255835be0
Geoffroy Begouaussel <geoffroy.begouaussel@pix.fr>
2024-11-18 17:44:38 +0100

    feat(mon-pix): prevent anonymous user to be redirected to dashboard

--- mon-pix/app/routes/authentication/login.js
+++ mon-pix/app/routes/authentication/login.js
@@ -2,11 +2,14 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
 export default class LoginRoute extends Route {
+  @service currentUser;
   @service session;
   @service store;
   @service router;
 
   beforeModel() {
-    this.session.prohibitAuthentication('authenticated.user-dashboard');
+    if (!this.currentUser?.user?.isAnonymous) {
+      this.session.prohibitAuthentication('authenticated.user-dashboard');
+    }
   }
 }
