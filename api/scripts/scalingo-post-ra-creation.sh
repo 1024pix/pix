c1e4f65b8772c10b9a2abf85e48b78917fdcaa24
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2025-02-05 16:50:45 +0100

    adapt post review app creation for maddo
    Co-authored-by: Nicolas Lepage <nicolas.lepage@pix.fr>
    Co-authored-by: Vincent Hardouin <vincent.hardouin@pix.fr>

--- api/scripts/scalingo-post-ra-creation.sh
+++ api/scripts/scalingo-post-ra-creation.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+set -ex
 
-npm run postdeploy
-npm run db:seed
+install-scalingo-cli
+
+if [[ -z "$MADDO" ]]; then
+  npm run postdeploy
+  scalingo -a pix-api-maddo-review-pr$PR_NUMBER set-env DATABASE_URL="$SCALINGO_POSTGRESQL_URL"
+  scalingo -a pix-api-maddo-review-pr$PR_NUMBER restart
+  npm run db:seed
+else
+  npm run postdeploy:maddo
+  scalingo -a pix-api-review-pr$PR_NUMBER set-env DATAMART_URL="$SCALINGO_POSTGRESQL_URL"
+  scalingo -a pix-api-review-pr$PR_NUMBER restart
+fi
+
