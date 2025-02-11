42ce3a3bd3e0113233c2c642af5684d7e4e64d3c
Yvonnick Frin <frin.yvonnick@gmail.com>
2025-02-11 13:35:54 +0100

    test(api): Add test for EligibilityRequirement model

--- api/src/quest/domain/models/EligibilityRequirement.js
+++ api/src/quest/domain/models/EligibilityRequirement.js
@@ -39,6 +39,10 @@ export class EligibilityRequirement {
     return this.#comparison;
   }
 
+  #getComparisonFunction(comparison) {
+    return comparison === COMPARISON.ONE_OF ? 'some' : 'every';
+  }
+
   isEligible(eligibility) {
     const comparisonFunction = this.#getComparisonFunction(this.#comparison);
     if (this.#requirement_type === COMPOSE_TYPE) {
@@ -47,6 +51,18 @@ export class EligibilityRequirement {
       );
     }
     return this.#checkRequirement(eligibility);
+  }
+
+  #checkRequirement(eligibility) {
+    const comparisonFunction = this.#getComparisonFunction(this.#comparison);
+
+    if (Array.isArray(eligibility[this.#requirement_type])) {
+      return eligibility[this.#requirement_type].some((item) => {
+        return this.#criterion.check({ item, comparisonFunction });
+      });
+    }
+
+    return this.#criterion.check({ item: eligibility[this.#requirement_type], comparisonFunction });
   }
 
   toDTO() {
@@ -61,21 +77,5 @@ export class EligibilityRequirement {
       data,
       comparison: this.#comparison,
     };
-  }
-
-  #getComparisonFunction(comparison) {
-    return comparison === COMPARISON.ONE_OF ? 'some' : 'every';
-  }
-
-  #checkRequirement(eligibility) {
-    const comparisonFunction = this.#getComparisonFunction(this.#comparison);
-
-    if (Array.isArray(eligibility[this.#requirement_type])) {
-      return eligibility[this.#requirement_type].some((item) => {
-        return this.#criterion.check({ item, comparisonFunction });
-      });
-    }
-
-    return this.#criterion.check({ item: eligibility[this.#requirement_type], comparisonFunction });
   }
 }
--- api/tests/quest/unit/domain/models/EligibilityRequirement_test.js
+++ api/tests/quest/unit/domain/models/EligibilityRequirement_test.js
@@ -1,11 +1,12 @@
 import { TYPES } from '../../../../../src/quest/domain/models/Eligibility.js';
+import { Eligibility } from '../../../../../src/quest/domain/models/Eligibility.js';
 import { COMPOSE_TYPE, EligibilityRequirement } from '../../../../../src/quest/domain/models/EligibilityRequirement.js';
 import { COMPARISON } from '../../../../../src/quest/domain/models/Quest.js';
 import { expect } from '../../../../test-helper.js';
 
 describe('Quest | Unit | Domain | Models | EligibilityRequirement ', function () {
   describe('#constructor', function () {
-    context('when requirement_type is compose', function () {
+    describe('when requirement_type is compose', function () {
       it('should build the same instance whether we are passing through raw requirements or instanciated requirements', function () {
         // given
         const eligibilityRequirementA = new EligibilityRequirement({
@@ -38,7 +39,8 @@ describe('Quest | Unit | Domain | Models | EligibilityRequirement ', function ()
         expect(eligibilityRequirementA.toDTO()).to.deep.equal(eligibilityRequirementB.toDTO());
       });
     });
-    context('when requirement_type is not compose', function () {
+
+    describe('when requirement_type is not compose', function () {
       it('should build the same instance whether we are passing through raw criterion or instanciated criterion', function () {
         // given
         const eligibilityRequirementA = new EligibilityRequirement({
@@ -56,6 +58,196 @@ describe('Quest | Unit | Domain | Models | EligibilityRequirement ', function ()
 
         // when / then
         expect(eligibilityRequirementA.toDTO()).to.deep.equal(eligibilityRequirementB.toDTO());
+      });
+    });
+  });
+
+  describe('isEligible', function () {
+    describe('when requirement_type is compose', function () {
+      describe('when comparison is ALL', function () {
+        it('should return true if all requirements are met', function () {
+          const eligibilityRequirement = new EligibilityRequirement({
+            requirement_type: COMPOSE_TYPE,
+            comparison: COMPARISON.ALL,
+            data: [
+              {
+                requirement_type: TYPES.CAMPAIGN_PARTICIPATIONS,
+                data: {
+                  targetProfileId: 1,
+                },
+                comparison: COMPARISON.ALL,
+              },
+              {
+                requirement_type: TYPES.CAMPAIGN_PARTICIPATIONS,
+                data: {
+                  targetProfileId: 2,
+                },
+                comparison: COMPARISON.ALL,
+              },
+            ],
+          });
+          const eligibility = new Eligibility({
+            campaignParticipations: [{ targetProfileId: 1 }, { targetProfileId: 2 }],
+          });
+
+          expect(eligibilityRequirement.isEligible(eligibility)).to.be.true;
+        });
+
+        it('should return false if all requirements are not met', function () {
+          const eligibilityRequirement = new EligibilityRequirement({
+            requirement_type: COMPOSE_TYPE,
+            comparison: COMPARISON.ALL,
+            data: [
+              {
+                requirement_type: TYPES.CAMPAIGN_PARTICIPATIONS,
+                data: {
+                  targetProfileId: 1,
+                },
+                comparison: COMPARISON.ALL,
+              },
+              {
+                requirement_type: TYPES.CAMPAIGN_PARTICIPATIONS,
+                data: {
+                  targetProfileId: 2,
+                },
+                comparison: COMPARISON.ALL,
+              },
+            ],
+          });
+          const eligibility = new Eligibility({
+            campaignParticipations: [{ targetProfileId: 1 }, { targetProfileId: 3 }],
+          });
+
+          expect(eligibilityRequirement.isEligible(eligibility)).to.be.false;
+        });
+      });
+
+      describe('when comparison is ONE_OF', function () {
+        it('should return true if one of the requirements is met', function () {
+          const eligibilityRequirement = new EligibilityRequirement({
+            requirement_type: COMPOSE_TYPE,
+            comparison: COMPARISON.ONE_OF,
+            data: [
+              {
+                requirement_type: TYPES.CAMPAIGN_PARTICIPATIONS,
+                data: {
+                  targetProfileId: 1,
+                },
+                comparison: COMPARISON.ALL,
+              },
+              {
+                requirement_type: TYPES.CAMPAIGN_PARTICIPATIONS,
+                data: {
+                  targetProfileId: 2,
+                },
+                comparison: COMPARISON.ALL,
+              },
+            ],
+          });
+          const eligibility = new Eligibility({
+            campaignParticipations: [{ targetProfileId: 1 }],
+          });
+
+          expect(eligibilityRequirement.isEligible(eligibility)).to.be.true;
+        });
+
+        it('should return false if none of the requirements are met', function () {
+          const eligibilityRequirement = new EligibilityRequirement({
+            requirement_type: COMPOSE_TYPE,
+            comparison: COMPARISON.ONE_OF,
+            data: [
+              {
+                requirement_type: TYPES.CAMPAIGN_PARTICIPATIONS,
+                data: {
+                  targetProfileId: 1,
+                },
+                comparison: COMPARISON.ALL,
+              },
+              {
+                requirement_type: TYPES.CAMPAIGN_PARTICIPATIONS,
+                data: {
+                  targetProfileId: 2,
+                },
+                comparison: COMPARISON.ALL,
+              },
+            ],
+          });
+          const eligibility = new Eligibility({
+            campaignParticipations: [{ targetProfileId: 3 }],
+          });
+
+          expect(eligibilityRequirement.isEligible(eligibility)).to.be.false;
+        });
+      });
+    });
+
+    describe('when requirement_type is not compose', function () {
+      describe('when eligibility attribute is not an array', function () {
+        it('it should return true if criterion is valid', function () {
+          const eligibilityRequirement = new EligibilityRequirement({
+            requirement_type: TYPES.ORGANIZATION,
+            comparison: COMPARISON.ALL,
+            data: {
+              type: 'SCO',
+            },
+          });
+          const eligibility = new Eligibility({
+            organization: {
+              type: 'SCO',
+            },
+          });
+
+          expect(eligibilityRequirement.isEligible(eligibility)).to.be.true;
+        });
+
+        it('it should return false if criterion is not valid', function () {
+          const eligibilityRequirement = new EligibilityRequirement({
+            requirement_type: TYPES.ORGANIZATION,
+            comparison: COMPARISON.ALL,
+            data: {
+              type: 'SCO',
+            },
+          });
+          const eligibility = new Eligibility({
+            organization: {
+              type: 'PRO',
+            },
+          });
+
+          expect(eligibilityRequirement.isEligible(eligibility)).to.be.false;
+        });
+      });
+
+      describe('when eligibility attribute is an array', function () {
+        it('it should return true if one of the eligibility item validate criterion', function () {
+          const eligibilityRequirement = new EligibilityRequirement({
+            requirement_type: TYPES.CAMPAIGN_PARTICIPATIONS,
+            comparison: COMPARISON.ALL,
+            data: {
+              targetProfileId: 1,
+            },
+          });
+          const eligibility = new Eligibility({
+            campaignParticipations: [{ targetProfileId: 2 }, { targetProfileId: 1 }],
+          });
+
+          expect(eligibilityRequirement.isEligible(eligibility)).to.be.true;
+        });
+
+        it('it should return false if none of the eligibility item validate criterion', function () {
+          const eligibilityRequirement = new EligibilityRequirement({
+            requirement_type: TYPES.CAMPAIGN_PARTICIPATIONS,
+            comparison: COMPARISON.ALL,
+            data: {
+              targetProfileId: 3,
+            },
+          });
+          const eligibility = new Eligibility({
+            campaignParticipations: [{ targetProfileId: 2 }, { targetProfileId: 1 }],
+          });
+
+          expect(eligibilityRequirement.isEligible(eligibility)).to.be.false;
+        });
       });
     });
   });
