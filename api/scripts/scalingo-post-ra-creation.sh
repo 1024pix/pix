47ea019a0c1adf5302f6ddf93429a6f6d6e0cd56
Steph0 <Steph0@users.noreply.github.com>
2025-02-21 18:04:04 +0100

    :sparkles: api: use RA database for Parcoursup

--- api/scripts/scalingo-post-ra-creation.sh
+++ api/scripts/scalingo-post-ra-creation.sh
@@ -8,6 +8,11 @@ if [[ -z "$MADDO" ]]; then
   scalingo -a pix-api-maddo-review-pr$PR_NUMBER env-set DATABASE_URL="$SCALINGO_POSTGRESQL_URL"
   scalingo -a pix-api-maddo-review-pr$PR_NUMBER restart || echo
   npm run db:seed
+
+  # Parcoursup datamarts
+  npm run postdeploy:datamart
+  npm run datamart:seed
+
 else
   npm run postdeploy:maddo
   scalingo -a pix-api-review-pr$PR_NUMBER env-set DATAMART_URL="$SCALINGO_POSTGRESQL_URL"
