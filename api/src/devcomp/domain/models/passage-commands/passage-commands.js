3ef953ddba34e9aeb79ba220c2ab131032f05e4e
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2025-02-28 11:29:59 +0100

    feat(api): create a StartPassageCommand class

--- api/src/devcomp/domain/models/passage-commands/passage-commands.js
+++ api/src/devcomp/domain/models/passage-commands/passage-commands.js
@@ -1,4 +1,24 @@
+import { assertNotNullOrUndefined } from '../../../../shared/domain/models/asserts.js';
 import { PassageCommand } from './PassageCommand.js';
+
+/**
+ * @class StartPassageCommand
+ *
+ * A StartPassageCommand is an instruction to start a Modulix passage.
+ * See CQRS pattern for more information.
+ *
+ * It becomes a PassageStartedEvent when saved to the DB.
+ * It takes a contentHash as a parameter.
+ */
+class StartPassageCommand extends PassageCommand {
+  constructor({ occuredAt, passageId, contentHash }) {
+    super({ type: 'START_PASSAGE', occuredAt, passageId, commandData: { contentHash } });
+
+    assertNotNullOrUndefined(contentHash, 'The contentHash is required for a StartPassageCommand');
+
+    this.contentHash = contentHash;
+  }
+}
 
 /**
  * @class TerminatePassageCommand
@@ -14,4 +34,4 @@ class TerminatePassageCommand extends PassageCommand {
   }
 }
 
-export { TerminatePassageCommand };
+export { StartPassageCommand, TerminatePassageCommand };
--- api/tests/devcomp/integration/domain/models/passage-commands/passage-commands_test.js
+++ api/tests/devcomp/integration/domain/models/passage-commands/passage-commands_test.js
@@ -1,7 +1,45 @@
-import { TerminatePassageCommand } from '../../../../../../src/devcomp/domain/models/passage-commands/passage-commands.js';
-import { expect } from '../../../../../test-helper.js';
+import {
+  StartPassageCommand,
+  TerminatePassageCommand,
+} from '../../../../../../src/devcomp/domain/models/passage-commands/passage-commands.js';
+import { DomainError } from '../../../../../../src/shared/domain/errors.js';
+import { catchErrSync, expect } from '../../../../../test-helper.js';
 
 describe('Integration | Devcomp | Domain | Models | passage-commands | passage-commands', function () {
+  describe('#StartPassageCommand', function () {
+    it('should init and keep attributes', function () {
+      // given
+      const occuredAt = Symbol('date');
+      const passageId = Symbol('passage');
+      const contentHash = Symbol('contentHash');
+
+      // when
+      const startPassageCommand = new StartPassageCommand({ occuredAt, passageId, contentHash });
+
+      // then
+      expect(startPassageCommand.type).to.equal('START_PASSAGE');
+      expect(startPassageCommand.occuredAt).to.equal(occuredAt);
+      expect(startPassageCommand.passageId).to.equal(passageId);
+      expect(startPassageCommand.contentHash).to.equal(contentHash);
+      expect(startPassageCommand.commandData).to.deep.equal({ contentHash });
+    });
+
+    describe('when contentHash is not given', function () {
+      it('should throw an error', function () {
+        // given
+        const occuredAt = Symbol('date');
+        const passageId = Symbol('passage');
+
+        // when
+        const error = catchErrSync(() => new StartPassageCommand({ occuredAt, passageId }))();
+
+        // then
+        expect(error).to.be.instanceOf(DomainError);
+        expect(error.message).to.equal('The contentHash is required for a StartPassageCommand');
+      });
+    });
+  });
+
   describe('#TerminatePassageCommand', function () {
     it('should init and keep attributes', function () {
       // given
