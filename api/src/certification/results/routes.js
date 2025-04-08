20198d4d19001043118f903a32a58e968f82c0c5
Yannick François <yannick.francois@pix.fr>
2025-04-08 11:49:27 +0200

    :recycle: chore(api) fix import problems

--- api/src/certification/results/routes.js
+++ api/src/certification/results/routes.js
@@ -1,6 +1,6 @@
-import * as certificationAttestation from './application/certification-attestation-route.js';
 import * as certificationReports from './application/certification-reports-route.js';
 import * as certificationResults from './application/certification-results-route.js';
+import * as certificationAttestation from './application/certification-route.js';
 import * as certification from './application/certification-route.js';
 import * as livretScolaire from './application/livret-scolaire-route.js';
 import * as organization from './application/organization-route.js';
