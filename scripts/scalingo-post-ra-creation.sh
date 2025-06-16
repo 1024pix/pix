54986196efe56bfcdd45ace0b2e23e46e3c8c3d6
Theotime2005 <theotime.berthod@pix.fr>
2025-06-16 15:39:46 +0200

    feat: add curl command for front apps

--- scripts/scalingo-post-ra-creation.sh
+++ scripts/scalingo-post-ra-creation.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "nothing to do"
+# notify succeeded deployment
+curl -X POST --location "$NOTIFY_URL" \
+    -H "Content-Type: application/json" \
+    -d '{
+          "app-name": "$APP_NAME",
+          "environment": "$ENVIRONMENT",
+          "version": "v$npm pkg get version"
+        }'
