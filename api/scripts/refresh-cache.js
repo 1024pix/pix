348784f3433d84daaec226e6303562d296fc43e1
Laura Bergoens <laura.bergoens@pix.fr>
2025-07-04 01:18:31 +0200

    bugfix(api): refacto refresh cache script so it does not hang

--- api/scripts/refresh-cache.js
+++ api/scripts/refresh-cache.js
@@ -1,18 +1,26 @@
 import 'dotenv/config';
 
-import { databaseConnections } from '../db/database-connections.js';
 import { usecases } from '../src/learning-content/domain/usecases/index.js';
-import { learningContentCache } from '../src/shared/infrastructure/caches/learning-content-cache.js';
-import { logger, SCOPES } from '../src/shared/infrastructure/utils/logger.js';
+import { Script } from '../src/shared/application/scripts/script.js';
+import { ScriptRunner } from '../src/shared/application/scripts/script-runner.js';
 
-logger.info({ event: SCOPES.LEARNING_CONTENT }, 'Starting refreshing Learning Content');
+export class RefreshCache extends Script {
+  constructor() {
+    super({
+      description: 'This script will refresh API learning content caches with a release fetch from LCMS',
+      permanent: true,
+      options: {},
+    });
+  }
 
-try {
-  await usecases.refreshLearningContentCache();
-  logger.info({ event: SCOPES.LEARNING_CONTENT }, 'Learning Content refreshed');
-} catch (e) {
-  logger.error({ err: e, event: SCOPES.LEARNING_CONTENT }, 'Error while reloading cache');
-} finally {
-  await learningContentCache.quit();
-  await databaseConnections.disconnect();
+  async handle({ logger }) {
+    try {
+      await usecases.refreshLearningContentCache();
+      logger.info('Learning Content refreshed');
+    } catch {
+      logger.error('Error while reloading cache');
+    }
+  }
 }
+
+await ScriptRunner.execute(import.meta.url, RefreshCache);
