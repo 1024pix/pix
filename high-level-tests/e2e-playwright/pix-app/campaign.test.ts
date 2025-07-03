70bf75f02fcf092dc899ee154fb5f90749a77b1b
Laura Bergoens <laura.bergoens@pix.fr>
2025-07-04 01:21:01 +0200

    tech(e2e): remove dupe test

--- high-level-tests/e2e-playwright/pix-app/campaign.test.ts
+++ high-level-tests/e2e-playwright/pix-app/campaign.test.ts
@@ -35,25 +35,3 @@ test('starts a campaign with code', async ({ page }) => {
   await page.getByRole('button', { name: "J'envoie mes résultats" }).click();
   await expect(page.getByText("Merci d'avoir envoyé vos résultats.")).toBeVisible();
 });
-
-test('starts a campaign with code 2', async ({ page }) => {
-  await page.goto('/');
-  await expect(page).toHaveTitle('Accueil | Pix');
-
-  await page.getByRole('link', { name: "J'ai un code" }).click();
-  await expect(page.getByRole('heading', { name: /saisissez votre code/ })).toBeVisible();
-
-  await page.getByRole('textbox', { name: 'Saisir votre code pour' }).fill(campaign.code);
-  await page.getByRole('button', { name: 'Accéder au parcours' }).click();
-  await expect(page.getByRole('heading', { name: /prêt à évaluer vos compétences numériques/ })).toBeVisible();
-
-  await page.getByRole('button', { name: 'Je commence' }).click();
-  await page.getByRole('button', { name: 'Ignorer' }).click();
-  await expect(page.getByRole('heading', { name: 'Vous avez déjà répondu !' })).toBeVisible();
-
-  await page.getByRole('link', { name: 'Voir mes résultats' }).click();
-  await expect(page.getByRole('heading', { name: /Bravo/ })).toBeVisible();
-
-  await page.getByRole('button', { name: "J'envoie mes résultats" }).click();
-  await expect(page.getByText("Merci d'avoir envoyé vos résultats.")).toBeVisible();
-});
