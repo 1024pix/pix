6b5c29dcc51543b9a1487ddad7ce8e28e7c5f095
Laura Bergoens <laura.bergoens@pix.fr>
2025-07-04 18:28:41 +0200

    tech(e2e): add preview test

---
+++ high-level-tests/e2e-playwright/tests/evaluations/preview.spec.ts
@@ -0,0 +1,54 @@
+import { expect } from '@playwright/test';
+import * as fs from 'fs';
+
+import { SEED_FOR_RANDOM_DATABASE } from '../../helpers/constants';
+import { commonSeeds, databaseBuilder } from '../../helpers/db';
+import { test } from '../../helpers/fixtures';
+import { ChallengePage } from '../../pages/pix-app';
+
+let PREVIEW_CHALLENGE_ID: string;
+test.beforeEach(async () => {
+  await commonSeeds();
+  await databaseBuilder.knex.raw('SELECT setseed(?)', [SEED_FOR_RANDOM_DATABASE]);
+  const { id } = await databaseBuilder
+    .knex('learningcontent.challenges')
+    .select('id')
+    .where('status', '=', 'validé')
+    .whereRaw('? = ANY(locales)', ['fr'])
+    .orderByRaw('random()')
+    .first();
+  PREVIEW_CHALLENGE_ID = id;
+});
+
+test('user assesses on preview challenge', async ({ page, testMode }) => {
+  test.setTimeout(100_000);
+  let results;
+  const resultFilePath = './tests/evaluations/data/preview.json';
+  if (testMode === 'record') {
+    results = {
+      challengeImprints: [],
+    };
+  } else {
+    results = fs.readFileSync(resultFilePath, 'utf-8');
+    results = JSON.parse(results);
+  }
+  await test.step(`PREVIEW assessment started`, async () => {
+    await page.goto(process.env.PIX_APP_URL + '/challenges/' + PREVIEW_CHALLENGE_ID + '/preview');
+    const challengePage = new ChallengePage(page);
+    const challengeImprint = await challengePage.getChallengeImprint();
+    if (testMode === 'record') {
+      results.challengeImprints.push(challengeImprint);
+    } else {
+      expect(challengeImprint).toBe(results.challengeImprints[0]);
+      await expect(page.getByLabel('Votre progression')).toContainText('1 / 1');
+    }
+    await challengePage.setRightOrWrongAnswer(true);
+    await challengePage.validateAnswer();
+  });
+
+  if (testMode === 'record') {
+    fs.writeFileSync(resultFilePath, JSON.stringify(results));
+  } else {
+    await expect(page.locator('body')).toContainText(/Vos réponses/);
+  }
+});
