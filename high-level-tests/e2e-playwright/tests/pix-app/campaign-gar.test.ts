976c534e508d6925f98da8948109183356161637
Laura Bergoens <laura.bergoens@pix.fr>
2025-07-05 12:25:50 +0200

    tech(e2e): move campaign-gar test

---
+++ high-level-tests/e2e-playwright/tests/pix-app/campaign-gar.test.ts
@@ -0,0 +1,22 @@
+import { expect } from '@playwright/test';
+
+import { getGarTokenForNewUser } from '../../helpers/auth.js';
+import { databaseBuilder } from '../../helpers/db.js';
+import { test } from '../../helpers/fixtures.js';
+
+let campaignCode: string;
+
+test.beforeEach(async () => {
+  campaignCode = databaseBuilder.factory.buildCampaign().code;
+  await databaseBuilder.commit();
+});
+
+test('creates an account from GAR', async ({ page }) => {
+  const garUserToken = getGarTokenForNewUser('marie', 'toupie');
+  await page.goto(process.env.PIX_APP_URL + `/campagnes/?externalUser=${garUserToken}`);
+  await expect(page.getByRole('heading', { name: 'Saisissez votre code' })).toBeVisible();
+
+  await page.getByRole('textbox', { name: 'Saisir votre code pour' }).fill(campaignCode);
+  await page.getByRole('button', { name: 'Accéder au parcours' }).click();
+  await expect(page.getByRole('heading', { name: 'Commencez votre parcours Pix' })).toBeVisible();
+});
