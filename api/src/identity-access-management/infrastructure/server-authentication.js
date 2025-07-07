e4a27a2200dd2bd4b63345bbc082fb65fb875a50
rouxxi <gwenvael.laskar@pix.fr>
2025-07-07 14:39:17 +0200

    tech(api): removed temporary code for poc auth

--- api/src/identity-access-management/infrastructure/server-authentication.js
+++ api/src/identity-access-management/infrastructure/server-authentication.js
@@ -59,8 +59,7 @@ async function validateUser(decodedAccessToken, { request, revokedUserAccessRepo
   }
 
   const audience = getForwardedOrigin(request.headers);
-  // todo(auth): added for the poc
-  if (decodedAccessToken.aud !== 'http://localhost:4206' && decodedAccessToken.aud !== audience) {
+  if (decodedAccessToken.aud !== audience) {
     monitoringTools.logWarnWithCorrelationIds({
       message: 'User AccessToken audience mismatch',
       audience,
