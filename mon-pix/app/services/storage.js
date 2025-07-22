f67f6045670bfe8e0203fee265b2fb049ce098ed
theotime2005 <theotime.berthod@epitech.digital>
2025-07-22 14:17:37 +0200

    feat(mon-pix): add methods to save text to speech setting in localStorage

--- mon-pix/app/services/storage.js
+++ mon-pix/app/services/storage.js
@@ -1,6 +1,8 @@
 import Service from '@ember/service';
+import { toBool } from 'ember-source/@glimmer/global-context';
 
 const SESSIONSTORAGE_LOGIN = 'PIX_LOGIN';
+const LOCALSTORAGE_TEXT_TO_SPEECH = 'PIX_TEXT_TO_SPEECH';
 
 export default class Storage extends Service {
   setLogin(login) {
@@ -13,5 +15,17 @@ export default class Storage extends Service {
 
   clear() {
     return sessionStorage.clear();
+  }
+
+  getTextToSpeech() {
+    const value = localStorage.getItem(LOCALSTORAGE_TEXT_TO_SPEECH);
+    if (value !== null) {
+      return toBool(value);
+    }
+    return true;
+  }
+
+  setTextToSpeech(value) {
+    localStorage.setItem(LOCALSTORAGE_TEXT_TO_SPEECH, value);
   }
 }
--- mon-pix/tests/unit/services/storage-test.js
+++ mon-pix/tests/unit/services/storage-test.js
@@ -3,41 +3,77 @@ import { module, test } from 'qunit';
 
 module('Unit | Service | storage', function (hooks) {
   setupTest(hooks);
+  let service;
 
-  test('setLogin', function (assert) {
-    // given
-    const service = this.owner.lookup('service:storage');
-    const login = 'someone@example.net';
-
-    // when
-    service.setLogin(login);
-
-    // then
-    assert.strictEqual(sessionStorage.getItem('PIX_LOGIN'), login);
+  hooks.beforeEach(function () {
+    service = this.owner.lookup('service:storage');
   });
 
-  test('getLogin', function (assert) {
-    // given
-    const service = this.owner.lookup('service:storage');
-    const login = 'someone@example.net';
-    sessionStorage.setItem('PIX_LOGIN', login);
+  module('login', function () {
+    test('setLogin', function (assert) {
+      // given
+      const login = 'someone@example.net';
 
-    // when
-    const result = service.getLogin();
+      // when
+      service.setLogin(login);
 
-    // then
-    assert.strictEqual(result, login);
+      // then
+      assert.strictEqual(sessionStorage.getItem('PIX_LOGIN'), login);
+    });
+
+    test('getLogin', function (assert) {
+      // given
+      const login = 'someone@example.net';
+      sessionStorage.setItem('PIX_LOGIN', login);
+
+      // when
+      const result = service.getLogin();
+
+      // then
+      assert.strictEqual(result, login);
+    });
+
+    test('getLogin with no login', function (assert) {
+      // given
+      sessionStorage.clear();
+
+      // when
+      const result = service.getLogin();
+
+      // then
+      assert.strictEqual(result, null);
+    });
   });
 
-  test('getLogin with no login', function (assert) {
-    // given
-    const service = this.owner.lookup('service:storage');
-    sessionStorage.clear();
+  module('text to speech', function () {
+    test('should return the value of text to speech if is not null', function (assert) {
+      // given
+      localStorage.setItem('PIX_TEXT_TO_SPEECH', true);
 
-    // when
-    const result = service.getLogin();
+      // when
+      const result = service.getTextToSpeech();
 
-    // then
-    assert.strictEqual(result, null);
+      // then
+      assert.true(result);
+    });
+
+    test('should set the value correctly', function (assert) {
+      // when
+      service.setTextToSpeech(true);
+
+      // then
+      assert.strictEqual(localStorage.getItem('PIX_TEXT_TO_SPEECH'), 'true');
+    });
+
+    test('return true if value is not defined in localstorage', function (assert) {
+      // given
+      localStorage.removeItem('PIX_TEXT_TO_SPEECH');
+
+      // when
+      const result = service.getTextToSpeech();
+
+      // then
+      assert.true(result);
+    });
   });
 });
