28a04354851481721d1cb0753604737afb9720ea
Guillaume Olejniczak <la.toile.cosmique@gmail.com>
2025-09-01 11:14:12 +0200

    feat(orga): remove 'to-share' status from participation statistics display

--- orga/app/models/organization-learner-statistic.js
+++ orga/app/models/organization-learner-statistic.js
@@ -3,7 +3,6 @@ import Model, { attr, belongsTo } from '@ember-data/model';
 export default class OrganizationLearnerStatistic extends Model {
   @attr('number') shared;
   @attr('number') started;
-  @attr('number') to_share;
   @attr('number') total;
 
   @belongsTo('organization-learner-activity', { async: true, inverse: 'organizationLearnerStatistics' })
