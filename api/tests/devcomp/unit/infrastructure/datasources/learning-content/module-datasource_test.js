06f2bbdce710b60173e963a5ffe2796948ac5040
dianeCdrPix <diane.cordier@pix.fr>
2025-10-01 17:38:37 +0200

    tech(api): increase timeout delay
    in modules content tests

--- api/tests/devcomp/unit/infrastructure/datasources/learning-content/module-datasource_test.js
+++ api/tests/devcomp/unit/infrastructure/datasources/learning-content/module-datasource_test.js
@@ -94,7 +94,7 @@ describe('Unit | Infrastructure | Datasources | Learning Content | ModuleDatasou
       modules.forEach((module) => {
         it(`module "${module.slug}" should contain a valid structure`, async function () {
           // We need to increase the timeout because the validation can be slow for large modules
-          this.timeout(5000);
+          this.timeout(30000);
           try {
             await moduleSchema.validateAsync(module, { abortEarly: false });
           } catch (joiError) {
