e466d7876f40eb067aca25212c098685c47d7ff1
Yannick François <yannick.francois@pix.fr>
2025-10-27 14:52:47 +0100

    🗃️ api: remove not null constraint on version

---
+++ api/db/migrations/20251027132926_remove-not-null-version-constraint-on-consolidated-frameworks-challenges-table.js
@@ -0,0 +1,24 @@
+const TABLE_NAME = 'certification-frameworks-challenges';
+const COLUMN_NAME = 'version';
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const up = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.string(COLUMN_NAME).nullable().alter();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const down = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.string(COLUMN_NAME).notNullable().alter();
+  });
+};
+
+export { down, up };
