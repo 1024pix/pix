3a4155d9eb8212fce70676a2bd3d6b0e839810b7
Benjamin Petetot <benjamin.petetot@pix.fr>
2025-11-19 11:25:41 +0100

    feat(admin): setup test in CI with ember-exam

--- admin/tests/test-helper.js
+++ admin/tests/test-helper.js
@@ -1,35 +1,13 @@
-// import { setApplication } from '@ember/test-helpers';
-// import { clearAllCookies } from 'ember-cookies/test-support';
-// import start from 'ember-exam/test-support/start';
-// import * as QUnit from 'qunit';
-// import { setup } from 'qunit-dom';
-
-// import Application from '../app';
-// import config from '../config/environment';
-
-// // Set default browser locale
-// const BROWSER_LOCALE = 'fr';
-// Object.defineProperty(window.navigator, 'language', { value: BROWSER_LOCALE, configurable: true });
-// Object.defineProperty(window.navigator, 'languages', { value: [BROWSER_LOCALE], configurable: true });
-
-// // Reset all cookies before each test to avoid side-effects
-// QUnit.hooks.beforeEach(function () {
-//   clearAllCookies();
-// });
-
-// setup(QUnit.assert);
-// setApplication(Application.create(config.APP));
-// start();
-
 import { setApplication } from '@ember/test-helpers';
 import { clearAllCookies } from 'ember-cookies/test-support';
-import { setupEmberOnerrorValidation, start as qunitStart } from 'ember-qunit';
+import { start as startEmberExam } from 'ember-exam/test-support';
+import { setupEmberOnerrorValidation } from 'ember-qunit';
 import Application from 'pix-admin/app';
 import config from 'pix-admin/config/environment';
 import * as QUnit from 'qunit';
 import { setup } from 'qunit-dom';
 
-export function start() {
+export async function start({ availableModules }) {
   // Set default browser locale
   const BROWSER_LOCALE = 'fr';
   Object.defineProperty(window.navigator, 'language', { value: BROWSER_LOCALE, configurable: true });
@@ -45,5 +23,5 @@ export function start() {
   setup(QUnit.assert);
   setupEmberOnerrorValidation();
 
-  qunitStart();
+  await startEmberExam({ availableModules });
 }
