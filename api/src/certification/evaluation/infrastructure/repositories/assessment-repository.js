87311b37f156252e9b70451ab07c73007629f987
Alexandre COIN <alexandre.pc.coin@gmail.com>
2025-12-30 14:20:00 +0100

    tech(api): add jsdoc in assessment repository

--- api/src/certification/evaluation/infrastructure/repositories/assessment-repository.js
+++ api/src/certification/evaluation/infrastructure/repositories/assessment-repository.js
@@ -1,7 +1,14 @@
+// @ts-check
 import { DomainTransaction } from '../../../../shared/domain/DomainTransaction.js';
 import { NotFoundError } from '../../../../shared/domain/errors.js';
 import { Assessment } from '../../../../shared/domain/models/Assessment.js';
 
+/**
+ * @function
+ * @param {number} id
+ * @returns {Promise<Assessment>}
+ * @throws {NotFoundError}
+ */
 const get = async function (id) {
   const knexConn = DomainTransaction.getConnection();
   const assessment = await knexConn('assessments').where({ id }).forUpdate().first();
@@ -12,6 +19,11 @@ const get = async function (id) {
   return new Assessment(assessment);
 };
 
+/**
+ * @function
+ * @param {Assessment} assessment
+ * @returns {Promise<number>}
+ */
 const updateLastQuestionDate = async function (assessment) {
   const knexConn = DomainTransaction.getConnection();
   return knexConn('assessments')
@@ -19,6 +31,11 @@ const updateLastQuestionDate = async function (assessment) {
     .update({ lastQuestionDate: assessment.lastQuestionDate, updatedAt: new Date() });
 };
 
+/**
+ * @function
+ * @param {Assessment} assessment
+ * @returns {Promise<number>}
+ */
 const updateWhenNewChallengeIsAsked = async function (assessment) {
   const knexConn = DomainTransaction.getConnection();
   return knexConn('assessments').where({ id: assessment.id }).update({
