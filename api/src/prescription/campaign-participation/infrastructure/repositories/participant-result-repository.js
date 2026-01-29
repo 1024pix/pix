1cc5d3c69458c1e8ddd34b8f3ad756e8cd04876d
laura bergoens <laura.bergoens@pix.fr>
2026-01-29 16:27:50 +0100

    tech(api): make api/src/prescription/campaign-participation/infrastructure/repositories/participant-result-repository.js DT compliant

--- api/src/prescription/campaign-participation/infrastructure/repositories/participant-result-repository.js
+++ api/src/prescription/campaign-participation/infrastructure/repositories/participant-result-repository.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 
 import { knex } from '../../../../../db/knex-database-connection.js';
 import { convertLevelStagesIntoThresholds } from '../../../../evaluation/domain/services/stages/convert-level-stages-into-thresholds-service.js';
+import { DomainTransaction } from '../../../../shared/domain/DomainTransaction.js';
 import { NotFoundError } from '../../../../shared/domain/errors.js';
 import { Assessment } from '../../../../shared/domain/models/Assessment.js';
 import { AssessmentResult } from '../../../../shared/domain/read-models/participant-results/AssessmentResult.js';
@@ -98,7 +99,8 @@ async function _getParticipationResults(userId, campaignId) {
 }
 
 async function _getParticipationAttributes(userId, campaignId) {
-  const participationAttributes = await knex('campaign-participations')
+  const knexConn = DomainTransaction.getConnection();
+  const participationAttributes = await knexConn('campaign-participations')
     .select([
       'state',
       'campaignParticipationId',
@@ -157,11 +159,13 @@ async function _findTargetedKnowledgeElements(campaignId, userId, campaignPartic
 }
 
 async function _getAcquiredBadgeIds(userId, campaignParticipationId) {
-  return knex('badge-acquisitions').select('badgeId').where({ userId, campaignParticipationId });
+  const knexConn = DomainTransaction.getConnection();
+  return knexConn('badge-acquisitions').select('badgeId').where({ userId, campaignParticipationId });
 }
 
 async function _getTargetProfileResetAllowed(campaignId) {
-  const targetProfile = await knex('target-profiles')
+  const knexConn = DomainTransaction.getConnection();
+  const targetProfile = await knexConn('target-profiles')
     .join('campaigns', 'campaigns.targetProfileId', 'target-profiles.id')
     .where('campaigns.id', campaignId)
     .first('areKnowledgeElementsResettable');
@@ -190,7 +194,8 @@ async function _findTargetedCompetences(campaignId, locale) {
 }
 
 function _getCampaignDTO(campaignId) {
-  return knex('campaigns').select('*').where({ 'campaigns.id': campaignId }).first();
+  const knexConn = DomainTransaction.getConnection();
+  return knexConn('campaigns').select('*').where({ 'campaigns.id': campaignId }).first();
 }
 
 function _isCampaignMultipleSendings(campaignDTO) {
@@ -206,7 +211,8 @@ function _isCampaignDeleted(campaignDTO) {
 }
 
 async function _isOrganizationLearnerActive(userId, campaignId) {
-  const organizationLearner = await knex('view-active-organization-learners')
+  const knexConn = DomainTransaction.getConnection();
+  const organizationLearner = await knexConn('view-active-organization-learners')
     .select('view-active-organization-learners.isDisabled')
     .join('organizations', 'organizations.id', 'view-active-organization-learners.organizationId')
     .join('campaigns', 'campaigns.organizationId', 'organizations.id')
@@ -217,7 +223,8 @@ async function _isOrganizationLearnerActive(userId, campaignId) {
 }
 
 async function getCampaignParticipationStatus({ userId, campaignId }) {
-  const participationStatus = await knex('campaign-participations')
+  const knexConn = DomainTransaction.getConnection();
+  const participationStatus = await knexConn('campaign-participations')
     .select('status')
     .where({ campaignId, userId, isImproved: false })
     .first();
