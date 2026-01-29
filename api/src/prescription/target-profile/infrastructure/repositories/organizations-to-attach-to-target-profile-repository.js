6c60b288c968dad56ac5ed7181712bb3bf3bfa35
laura bergoens <laura.bergoens@pix.fr>
2026-01-29 16:41:09 +0100

    tech(api): make api/src/prescription/target-profile/infrastructure/repositories/organizations-to-attach-to-target-profile-repository.js DT compliant

--- api/src/prescription/target-profile/infrastructure/repositories/organizations-to-attach-to-target-profile-repository.js
+++ api/src/prescription/target-profile/infrastructure/repositories/organizations-to-attach-to-target-profile-repository.js
@@ -1,4 +1,4 @@
-import { knex } from '../../../../../db/knex-database-connection.js';
+import { DomainTransaction } from '../../../../shared/domain/DomainTransaction.js';
 import { NotFoundError } from '../../../../shared/domain/errors.js';
 import { foreignKeyConstraintViolated } from '../../../../shared/infrastructure/utils/knex-utils.js';
 
@@ -21,18 +21,20 @@ const attachOrganizations = async function (targetProfile) {
 };
 
 const addTargetProfilesToOrganization = async function ({ organizationId, targetProfileIdList }) {
+  const knexConn = DomainTransaction.getConnection();
   const targetProfileShareToAdd = targetProfileIdList.map((targetProfileId) => {
     return { organizationId, targetProfileId };
   });
-  await knex('target-profile-shares')
+  await knexConn('target-profile-shares')
     .insert(targetProfileShareToAdd)
     .onConflict(['targetProfileId', 'organizationId'])
     .ignore();
 };
 
 async function _createTargetProfileShares(targetProfileShares) {
+  const knexConn = DomainTransaction.getConnection();
   try {
-    const insertedTargetProfileShares = await knex('target-profile-shares')
+    const insertedTargetProfileShares = await knexConn('target-profile-shares')
       .insert(targetProfileShares)
       .onConflict(['targetProfileId', 'organizationId'])
       .ignore()
