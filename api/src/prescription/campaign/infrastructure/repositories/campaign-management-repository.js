7548474ad01f75619c78cd30238be11f0ca8b13f
laura bergoens <laura.bergoens@pix.fr>
2026-01-29 16:11:21 +0100

    tech(api): make api/src/prescription/campaign/infrastructure/repositories/campaign-management-repository.js DT compliant

--- api/src/prescription/campaign/infrastructure/repositories/campaign-management-repository.js
+++ api/src/prescription/campaign/infrastructure/repositories/campaign-management-repository.js
@@ -1,4 +1,3 @@
-import { knex } from '../../../../../db/knex-database-connection.js';
 import * as CombinedCourseRepository from '../../../../quest/infrastructure/repositories/combined-course-repository.js';
 import { CAMPAIGN_FEATURES } from '../../../../shared/domain/constants.js';
 import { DomainTransaction } from '../../../../shared/domain/DomainTransaction.js';
@@ -9,7 +8,8 @@ import { CampaignManagement } from '../../domain/models/CampaignManagement.js';
 const { SHARED, TO_SHARE, STARTED } = CampaignParticipationStatuses;
 
 const get = async function (campaignId) {
-  let campaign = await knex('campaigns')
+  const knexConn = DomainTransaction.getConnection();
+  let campaign = await knexConn('campaigns')
     .select({
       id: 'campaigns.id',
       code: 'campaigns.code',
@@ -46,13 +46,13 @@ const get = async function (campaignId) {
     return null;
   }
 
-  const externalIdFeature = await knex('campaign-features')
+  const externalIdFeature = await knexConn('campaign-features')
     .select('params')
     .join('features', 'features.id', 'featureId')
     .where({ campaignId: campaign.id, 'features.key': CAMPAIGN_FEATURES.EXTERNAL_ID.key })
     .first();
 
-  const participationCountByStatus = await _countParticipationsByStatus(campaignId, campaign.type);
+  const participationCountByStatus = await _countParticipationsByStatus(knexConn, campaignId, campaign.type);
   campaign = {
     ...campaign,
     ...participationCountByStatus,
@@ -67,7 +67,8 @@ const get = async function (campaignId) {
 };
 
 const findPaginatedCampaignManagements = async function ({ organizationId, page }) {
-  const query = knex('campaigns')
+  const knexConn = DomainTransaction.getConnection();
+  const query = knexConn('campaigns')
     .select({
       id: 'campaigns.id',
       code: 'campaigns.code',
@@ -104,12 +105,12 @@ const findPaginatedCampaignManagements = async function ({ organizationId, page 
   return { models: campaignManagements, meta: { ...pagination } };
 };
 
-async function _countParticipationsByStatus(campaignId, campaignType) {
-  const row = await knex('campaign-participations')
+async function _countParticipationsByStatus(knexConn, campaignId, campaignType) {
+  const row = await knexConn('campaign-participations')
     .select([
-      knex.raw(`sum(case when status = ? then 1 else 0 end) as shared`, SHARED),
-      knex.raw(`sum(case when status = ? then 1 else 0 end) as completed`, TO_SHARE),
-      knex.raw(`sum(case when status = ? then 1 else 0 end) as started`, STARTED),
+      knexConn.raw(`sum(case when status = ? then 1 else 0 end) as shared`, SHARED),
+      knexConn.raw(`sum(case when status = ? then 1 else 0 end) as completed`, TO_SHARE),
+      knexConn.raw(`sum(case when status = ? then 1 else 0 end) as started`, STARTED),
     ])
     .where({ campaignId, isImproved: false })
     .whereNull('campaign-participations.deletedAt')
