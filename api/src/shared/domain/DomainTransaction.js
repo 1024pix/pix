2944a8a42dfd488eca5df34bf70b27c2f80c2ea9
Fael BASSETTI <fael.bassetti@pix.fr>
2026-01-29 16:00:33 +0100

    tech(api): improve DomainTransaction's autocompletion

--- api/src/shared/domain/DomainTransaction.js
+++ api/src/shared/domain/DomainTransaction.js
@@ -2,13 +2,28 @@ import { AsyncLocalStorage } from 'node:async_hooks';
 
 import { knex } from '../../../db/knex-database-connection.js';
 
+/**
+ * @typedef {import('knex').Knex} Knex
+ * @typedef {import('knex').Knex.Transaction} Transaction
+ * @typedef {import('knex').Knex.TransactionConfig} TransactionConfig
+ */
+
 const asyncLocalStorage = new AsyncLocalStorage();
 
 class DomainTransaction {
+  /**
+   * @param {Transaction} knexTransaction
+   */
   constructor(knexTransaction) {
     this.knexTransaction = knexTransaction;
   }
 
+  /**
+   * @template {Function} Lambda
+   * @param {Lambda} lambda
+   * @param {TransactionConfig=} transactionConfig
+   * @returns {ReturnType<Lambda> | Promise<ReturnType<Lambda>>}
+   */
   static execute(lambda, transactionConfig) {
     const existingConn = DomainTransaction.getConnection();
     if (existingConn.isTransaction) {
@@ -28,7 +43,7 @@ class DomainTransaction {
   }
 
   /**
-   * @returns {import('knex').Knex | import('knex').Knex.Transaction}
+   * @returns {Knex | Transaction}
    */
   static getConnection() {
     const store = asyncLocalStorage.getStore();
@@ -46,9 +61,9 @@ class DomainTransaction {
 }
 
 /**
- * @template F
+ * @template {Function} F
  * @param {F} func
- * @param {import('knex').Knex.TransactionConfig=} transactionConfig
+ * @param {TransactionConfig=} transactionConfig
  * @returns {F}
  */
 function withTransaction(func, transactionConfig) {
