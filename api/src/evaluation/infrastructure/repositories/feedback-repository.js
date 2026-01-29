0c1474256c8ced4f6c3e5c9867eaa563266072cd
laura bergoens <laura.bergoens@pix.fr>
2026-01-29 15:33:02 +0100

    tech(api): make api/src/evaluation/infrastructure/repositories/feedback-repository.js DT compliant

--- api/src/evaluation/infrastructure/repositories/feedback-repository.js
+++ api/src/evaluation/infrastructure/repositories/feedback-repository.js
@@ -1,12 +1,13 @@
 import omit from 'lodash/omit.js';
 
-import { knex } from '../../../../db/knex-database-connection.js';
+import { DomainTransaction } from '../../../shared/domain/DomainTransaction.js';
 import { Feedback } from '../../domain/models/Feedback.js';
 
 export const save = async function (feedback) {
+  const knexConn = DomainTransaction.getConnection();
   const dataToInsert = omit(feedback, ['id']);
 
-  const result = await knex('feedbacks').insert(dataToInsert).returning('*');
+  const result = await knexConn('feedbacks').insert(dataToInsert).returning('*');
 
   return new Feedback(result[0]);
 };
