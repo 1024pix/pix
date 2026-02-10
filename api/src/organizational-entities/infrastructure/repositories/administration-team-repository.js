90a596730946a7d0114940125a2a607f77db5ca2
Alexandre COIN <alexandre.pc.coin@gmail.com>
2026-02-10 14:23:11 +0100

    tech(api): add jsdoc in administration-team-repository.js

--- api/src/organizational-entities/infrastructure/repositories/administration-team-repository.js
+++ api/src/organizational-entities/infrastructure/repositories/administration-team-repository.js
@@ -1,6 +1,11 @@
+// @ts-check
 import { DomainTransaction } from '../../../shared/domain/DomainTransaction.js';
 import { AdministrationTeam } from '../../domain/models/AdministrationTeam.js';
 
+/**
+ * @function
+ * @returns {Promise<Array<AdministrationTeam>>}
+ */
 const findAll = async function () {
   const knexConn = DomainTransaction.getConnection();
   const administrationTeams = await knexConn.select('id', 'name').from('administration_teams').orderBy('name', 'asc');
@@ -8,6 +13,11 @@ const findAll = async function () {
   return administrationTeams.map(_toDomain);
 };
 
+/**
+ * @function
+ * @param {number} id
+ * @returns {Promise<AdministrationTeam | null>}
+ */
 const getById = async function (id) {
   const knexConn = DomainTransaction.getConnection();
   const administrationTeam = await knexConn.select('id', 'name').from('administration_teams').where({ id }).first();
@@ -19,6 +29,18 @@ const getById = async function (id) {
   return _toDomain(administrationTeam);
 };
 
+/**
+ * @typedef {{
+ *  id: number,
+ *  name: string,
+ * }} AdministrationTeamDTO
+ */
+
+/**
+ * @function
+ * @param {AdministrationTeamDTO} administrationTeamDTO
+ * @returns {AdministrationTeam}
+ */
 const _toDomain = function (administrationTeamDTO) {
   return new AdministrationTeam({
     ...administrationTeamDTO,
