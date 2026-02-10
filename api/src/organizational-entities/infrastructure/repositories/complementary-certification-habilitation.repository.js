4f6cd66e3f4ebb41522a0f4e288a7fd0f83fa5a6
Alexandre COIN <alexandre.pc.coin@gmail.com>
2026-02-10 16:25:42 +0100

    tech(api): add jsdoc in complementary-certification-habiliation repository

--- api/src/organizational-entities/infrastructure/repositories/complementary-certification-habilitation.repository.js
+++ api/src/organizational-entities/infrastructure/repositories/complementary-certification-habilitation.repository.js
@@ -1,16 +1,31 @@
+// @ts-check
 import { DomainTransaction } from '../../../shared/domain/DomainTransaction.js';
 
 const COMPLEMENTARY_CERTIFICATION_HABILITATIONS_TABLE_NAME = 'complementary-certification-habilitations';
 
-const save = async function (complementaryCertification) {
+/**
+ * @typedef {import('../../domain/model/ComplementaryCertificationHabilitation.js').ComplementaryCertificationHabilitation} ComplementaryCertificationHabilitation
+ */
+
+/**
+ * @function
+ * @param {ComplementaryCertificationHabilitation} complementaryCertificationHabilitation
+ * @returns {Promise<number>}
+ */
+const save = async function (complementaryCertificationHabilitation) {
   const knexConn = DomainTransaction.getConnection();
   const columnsToSave = {
-    complementaryCertificationId: complementaryCertification.complementaryCertificationId,
-    certificationCenterId: complementaryCertification.certificationCenterId,
+    complementaryCertificationId: complementaryCertificationHabilitation.complementaryCertificationId,
+    certificationCenterId: complementaryCertificationHabilitation.certificationCenterId,
   };
   return knexConn(COMPLEMENTARY_CERTIFICATION_HABILITATIONS_TABLE_NAME).insert(columnsToSave);
 };
 
+/**
+ * @function
+ * @param {number} certificationCenterId
+ * @returns {Promise<number>}
+ */
 const deleteByCertificationCenterId = async function (certificationCenterId) {
   const knexConn = DomainTransaction.getConnection();
   return knexConn(COMPLEMENTARY_CERTIFICATION_HABILITATIONS_TABLE_NAME).delete().where({ certificationCenterId });
