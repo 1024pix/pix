927698fc9c44575fbee696d28e6935b88b79ddb3
alicegoarnisson <alice.goarnisson@pix.fr>
2026-02-23 09:52:16 +0100

    feat(api): adds questId on combined-course-blueprint

---
+++ api/db/migrations/20260223084910_adds-questId-on-combined-course-blueprint.js
@@ -0,0 +1,24 @@
+const TABLE_NAME = 'combined_course_blueprints';
+const COLUMN_NAME = 'questId';
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const up = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.integer(COLUMN_NAME).defaultTo(null).references('quests.id');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const down = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.dropColumn(COLUMN_NAME);
+  });
+};
+
+export { down, up };
