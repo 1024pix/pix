dfbfa411c54c24381600e111cec426a30d4a460d
Alexandre COIN <alexandre.pc.coin@gmail.com>
2026-03-02 16:16:03 +0100

    tech(api): add structures table

---
+++ api/db/migrations/20260302142338_create-structures-table.js
@@ -0,0 +1,24 @@
+const TABLE_NAME = 'structures';
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const up = async function (knex) {
+  await knex.schema.createTable(TABLE_NAME, function (table) {
+    table.comment('This table contains all structures.');
+    table.increments('id').primary();
+    table.dateTime('created_at').notNullable().defaultTo(knex.fn.now());
+    table.dateTime('updated_at').defaultTo(knex.fn.now());
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const down = async function (knex) {
+  await knex.schema.dropTable(TABLE_NAME);
+};
+
+export { down, up };
