bb118b7de4243300416c15c2d06cba38b4410346
Thomas Bazin <thomas.bazin@pix.fr>
2026-03-02 17:24:47 +0100

    feat(db): add structureId foreign key in organizations table

---
+++ api/db/migrations/20260302155633_add-structureId-relationship-to-organizations.js
@@ -0,0 +1,29 @@
+const TABLE_NAME = 'organizations';
+const COLUMN_NAME = 'structureId';
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const up = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table
+      .integer(COLUMN_NAME)
+      .defaultTo(null)
+      .nullable()
+      .references('structures.id')
+      .comment("Reference to the organization's structure id.");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const down = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.dropColumn(COLUMN_NAME);
+  });
+};
+
+export { down, up };
