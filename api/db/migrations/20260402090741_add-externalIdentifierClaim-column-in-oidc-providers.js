ed6481c2562f4dec21bc925c399d6d61e905a6fd
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2026-04-02 11:16:19 +0200

    feat(api): add migration for 1 new column (externalIdentifierClaim) to oidc-providers table

---
+++ api/db/migrations/20260402090741_add-externalIdentifierClaim-column-in-oidc-providers.js
@@ -0,0 +1,24 @@
+const TABLE_NAME = 'oidc-providers';
+const COLUMN_NAME = 'externalIdentifierClaim';
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const up = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.string(COLUMN_NAME).comment('claim to use as the externalIdentifier');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const down = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.dropColumn(COLUMN_NAME);
+  });
+};
+
+export { down, up };
