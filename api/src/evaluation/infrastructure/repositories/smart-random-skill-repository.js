ce00c0a5f1cd5c2212c69503ac63115abc641065
laura bergoens <laura.bergoens@pix.fr>
2026-04-05 19:59:52 +0200

    tech(api): get smart random skills from repository for get next challenge campaign

--- api/src/evaluation/infrastructure/repositories/smart-random-skill-repository.js
+++ api/src/evaluation/infrastructure/repositories/smart-random-skill-repository.js
@@ -1,3 +1,4 @@
+import { DomainTransaction } from '../../../shared/domain/DomainTransaction.js';
 import * as skillRepository from '../../../shared/infrastructure/repositories/skill-repository.js';
 import { SmartRandomSkill } from '../../domain/models/SmartRandomSkill.js';
 
@@ -8,6 +9,32 @@ import { SmartRandomSkill } from '../../domain/models/SmartRandomSkill.js';
  */
 export async function findActiveByCompetenceId(competenceId) {
   const lcmsSkills = await skillRepository.findActiveByCompetenceId_proxy(competenceId);
+  return lcmsSkills.map(
+    (lcmsSkill) =>
+      new SmartRandomSkill({
+        id: lcmsSkill.id,
+        name: lcmsSkill.name,
+        difficulty: lcmsSkill.level,
+      }),
+  );
+}
+
+/**
+ *
+ * @param {number} campaignParticipationId
+ * @returns {Promise<SmartRandomSkill[]>}
+ */
+export async function findOperativeByCampaignParticipationId(campaignParticipationId) {
+  const knexConn = DomainTransaction.getConnection();
+  const skillIds = await knexConn
+    .pluck('campaign_skills.skillId')
+    .from('campaign-participations')
+    .join('campaign_skills', 'campaign_skills.campaignId', 'campaign-participations.campaignId')
+    .where('campaign-participations.id', campaignParticipationId);
+  if (skillIds.length === 0) {
+    return [];
+  }
+  const lcmsSkills = await skillRepository.findOperativeByIds_proxy(skillIds);
   return lcmsSkills.map(
     (lcmsSkill) =>
       new SmartRandomSkill({
--- api/tests/evaluation/integration/infrastructure/repositories/smart-random-skill-repository_test.js
+++ api/tests/evaluation/integration/infrastructure/repositories/smart-random-skill-repository_test.js
@@ -83,4 +83,96 @@ describe('Evaluation | Integration | Infrastructure| Repository | smart-random-s
       });
     });
   });
+
+  describe('#findOperativeByCampaignParticipationId', function () {
+    let campaignId, campaignParticipationId;
+    const skillIdInCampaign1 = 'skill1FromCampaign';
+    const skillIdInCampaign2 = 'skill2FromCampaign';
+    const skillIdInCampaign3 = 'skill3FromCampaign';
+    beforeEach(function () {
+      campaignId = databaseBuilder.factory.buildCampaign().id;
+      campaignParticipationId = databaseBuilder.factory.buildCampaignParticipation({ campaignId }).id;
+      databaseBuilder.factory.buildCampaignSkill({ campaignId, skillId: skillIdInCampaign1 });
+      databaseBuilder.factory.buildCampaignSkill({ campaignId, skillId: skillIdInCampaign2 });
+      databaseBuilder.factory.buildCampaignSkill({ campaignId, skillId: skillIdInCampaign3 });
+      return databaseBuilder.commit();
+    });
+
+    context('when no skills match the constraints', function () {
+      it('returns an empty array', async function () {
+        const someSkillData = {
+          id: 'skillNotInCampaign',
+          status: domainBuilder.learningContent.buildSkill.STATUSES.ARCHIVED,
+        };
+        databaseBuilder.factory.learningContent.build(
+          {
+            skills: [someSkillData],
+          },
+          { noDefaultValues: true },
+        );
+        await databaseBuilder.commit();
+
+        const smartRandomSkills =
+          await smartRandomSkillRepository.findOperativeByCampaignParticipationId(campaignParticipationId);
+
+        expect(smartRandomSkills).to.deep.equal([]);
+      });
+    });
+
+    context('when some skills match the constraints', function () {
+      it('returns the smart random skills ordered by id', async function () {
+        const notMatchingSkillData = {
+          id: 'skillNotInCampaign',
+          status: domainBuilder.learningContent.buildSkill.STATUSES.ARCHIVED,
+        };
+        const rightCampaignWrongStatusData = {
+          id: skillIdInCampaign3,
+          status: domainBuilder.learningContent.buildSkill.STATUSES.OBSOLETE,
+        };
+        const rightStatusWrongCampaignData = {
+          id: 'rightStatusWrongCampaignData',
+          status: domainBuilder.learningContent.buildSkill.STATUSES.ACTIVE,
+        };
+        const matchingSkill1 = {
+          id: skillIdInCampaign2,
+          name: '@coucou1',
+          level: 1,
+          status: domainBuilder.learningContent.buildSkill.STATUSES.ACTIVE,
+        };
+        const matchingSkill2 = {
+          id: skillIdInCampaign1,
+          name: '@fruits5',
+          level: 5,
+          status: domainBuilder.learningContent.buildSkill.STATUSES.ARCHIVED,
+        };
+        databaseBuilder.factory.learningContent.build(
+          {
+            skills: [
+              notMatchingSkillData,
+              rightCampaignWrongStatusData,
+              rightStatusWrongCampaignData,
+              matchingSkill1,
+              matchingSkill2,
+            ],
+          },
+          { noDefaultValues: true },
+        );
+        await databaseBuilder.commit();
+
+        const smartRandomSkills =
+          await smartRandomSkillRepository.findOperativeByCampaignParticipationId(campaignParticipationId);
+
+        expect(smartRandomSkills).to.deepEqualArray([
+          domainBuilder.evaluation.buildSmartRandomSkill({
+            ...matchingSkill2,
+            difficulty: matchingSkill2.level,
+          }),
+          domainBuilder.evaluation.buildSmartRandomSkill({
+            ...matchingSkill1,
+            difficulty: matchingSkill1.level,
+          }),
+        ]);
+      });
+    });
+  });
 });
