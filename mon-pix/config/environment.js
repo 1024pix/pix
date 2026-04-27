37cd581c4528ad868c0c1176f6424ab4bdf17b25
Xavier Carron <33637571+xav-car@users.noreply.github.com>
2026-04-27 16:01:56 +0200

    active log transitions

--- mon-pix/config/environment.js
+++ mon-pix/config/environment.js
@@ -191,6 +191,12 @@ module.exports = function (environment) {
     },
   };
 
+  ENV.APP.LOG_RESOLVER = true;
+  ENV.APP.LOG_ACTIVE_GENERATION = true;
+  ENV.APP.LOG_TRANSITIONS = true;
+  ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+  ENV.APP.LOG_VIEW_LOOKUPS = true;
+
   if (environment === 'development') {
     ENV.APP.LOG_RESOLVER = false;
     ENV.APP.LOG_ACTIVE_GENERATION = false;
