1bd26d85aa1dc268d50f376d9a88f0decaa670e7
Guillaume Olejniczak <la.toile.cosmique@gmail.com>
2026-05-19 10:43:02 +0200

    update FregataOrganizationLearnerSet style

--- api/src/prescription/learner-management/domain/models/FregataOrganizationLearnerSet.js
+++ api/src/prescription/learner-management/domain/models/FregataOrganizationLearnerSet.js
@@ -2,65 +2,55 @@ import { DomainError } from '../../../../shared/domain/errors.js';
 import { validateFregataOrganizationLearner } from '../validators/fregata-organization-learner-validator.js';
 import { OrganizationLearner } from './OrganizationLearner.js';
 
-const sexPossibleValues = {
-  M: 'M',
-  F: 'F',
-};
-
 class FregataOrganizationLearnerSet {
-  constructor() {
-    this.learners = [];
-    this.existingNationalStudentIds = [];
+  #learners = [];
+  #existingNationalStudentIds = [];
+
+  get learners() {
+    return this.#learners;
   }
 
   addLearner(learnerAttributes) {
-    this._performValidation(learnerAttributes);
-
-    const transformedAttributes = this._transform(learnerAttributes);
-    const organizationLearner = new OrganizationLearner(transformedAttributes);
-    this.learners.push(organizationLearner);
+    this.#performValidation(learnerAttributes);
+    const transformedAttributes = this.#transform(learnerAttributes);
+    this.#learners.push(new OrganizationLearner(transformedAttributes));
   }
 
-  _performValidation(learnerAttributes) {
+  #performValidation(learnerAttributes) {
     const errors = validateFregataOrganizationLearner(learnerAttributes);
-
-    const unicityError = this._checkOrganizationLearnersUnicity(learnerAttributes.nationalIdentifier);
+    const unicityError = this.#checkOrganizationLearnersUnicity(learnerAttributes.nationalIdentifier);
     if (unicityError) errors.push(unicityError);
-
     if (errors.length > 0) throw errors;
   }
 
-  _transform(learnerAttributes) {
+  #transform(learnerAttributes) {
     const { birthCountryCode, nationalIdentifier, division } = learnerAttributes;
-
     return {
       ...learnerAttributes,
       birthCountryCode: birthCountryCode.slice(-3),
       nationalStudentId: nationalIdentifier,
       division: division?.trim().replace(/\s+/g, ' '),
-      sex: _convertSexCodeToLabel(learnerAttributes.sex),
+      sex: FregataOrganizationLearnerSet.#convertSexCodeToLabel(learnerAttributes.sex),
     };
   }
 
-  _checkOrganizationLearnersUnicity(nationalIdentifier) {
+  #checkOrganizationLearnersUnicity(nationalIdentifier) {
     // we removed JOI unicity validation (uniq)
-    // because it took too much time (2h30 for 10000 learners)
+    // because it took too much time (2h30 for 10.000 learners)
     // we did the same validation but manually
-    if (this.existingNationalStudentIds.includes(nationalIdentifier)) {
+    if (this.#existingNationalStudentIds.includes(nationalIdentifier)) {
       const err = new DomainError();
       err.key = 'nationalIdentifier';
       err.why = 'uniqueness';
-
       return err;
     }
-
-    this.existingNationalStudentIds.push(nationalIdentifier);
+    this.#existingNationalStudentIds.push(nationalIdentifier);
     return null;
   }
-}
 
-function _convertSexCodeToLabel(sexCode) {
-  return sexPossibleValues[sexCode.toUpperCase().charAt(0)];
+  static #convertSexCodeToLabel(sexCode) {
+    return sexCode.toUpperCase().charAt(0);
+  }
 }
 
 export { FregataOrganizationLearnerSet };
