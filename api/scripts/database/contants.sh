bb700340f6cee0bec31a4bcd3ff9e5678dd5cb7c
Yannick François <yannick.francois@pix.fr>
2026-05-21 16:56:45 +0200

    Don't snapshot pgboss

--- api/scripts/database/contants.sh
+++ api/scripts/database/contants.sh
@@ -3,4 +3,3 @@ set -ex
 
 BINARY_SNAPSHOT=db/snapshots/schema.dump
 LEARNINGCONTENT_BINARY_SNAPSHOT=db/snapshots/learningcontent-schema.dump
-PGBOSS_BINARY_SNAPSHOT=db/snapshots/pgboss-schema.dump
--- api/scripts/database/make-a-db-snapshot.sh
+++ api/scripts/database/make-a-db-snapshot.sh
@@ -6,7 +6,4 @@ source 'scripts/database/contants.sh'
 npm run db:reset
 
 pg_dump -U postgres -h localhost -p 5432 pix --no-privileges --no-owner --create --exclude-schema=learningcontent,pgboss -Fc > $BINARY_SNAPSHOT
-
 pg_dump -U postgres -h localhost -p 5432 pix --no-privileges --schema-only --no-owner --create --schema=learningcontent -Fc > $LEARNINGCONTENT_BINARY_SNAPSHOT
-
-pg_dump -U postgres -h localhost -p 5432 pix --no-privileges --schema-only --no-owner --create --schema=pgboss -Fc > $PGBOSS_BINARY_SNAPSHOT
