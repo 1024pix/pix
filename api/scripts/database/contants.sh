cb1e71e3cda36bdb3dccee957f1c19ee8dfcb259
Yannick François <yannick.francois@pix.fr>
2026-05-21 16:42:00 +0200

    chore(api): stop snapshot into SQL files

--- api/scripts/database/contants.sh
+++ api/scripts/database/contants.sh
@@ -2,6 +2,5 @@
 set -ex
 
 BINARY_SNAPSHOT=db/snapshots/schema.dump
-SQL_SNAPSHOT=db/snapshots/schema.sql
 LEARNINGCONTENT_BINARY_SNAPSHOT=db/snapshots/learningcontent-schema.dump
-LEARNINGCONTENT_SQL_SNAPSHOT=db/snapshots/learningcontent-schema.sql
+PGBOSS_BINARY_SNAPSHOT=db/snapshots/pgboss-schema.dump
