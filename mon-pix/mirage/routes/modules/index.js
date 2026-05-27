fc29419ab919bff12bce53b4e8d65b9350d9bf5e
Eric Lim <eric.lim@zenika.com>
2026-05-27 16:35:06 +0200

    :fire: (mon-pix: remove old-module route

--- mon-pix/mirage/routes/modules/index.js
+++ mon-pix/mirage/routes/modules/index.js
@@ -1,7 +1,5 @@
 import getModule from './get-module';
-import getOldModule from './get-old-module';
 
 export default function index(config) {
   config.get('/modules/v2/:shortId', getModule);
-  config.get('/modules/:slug', getOldModule);
 }
