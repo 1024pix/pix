23abd3ebc2838439bb67f1f3a0809c8a3ec8f1d0
Alexandre COIN <alexandre.pc.coin@gmail.com>
2026-06-02 17:02:13 +0200

    tech(api): add dryRun mode

--- api/scripts/prod/get-answers-from-assessments.js
+++ api/scripts/prod/get-answers-from-assessments.js
@@ -7,10 +7,19 @@ export class GetAnswersFromAssessments extends Script {
     super({
       description: 'Get answers from a list of assessments',
       permanent: true,
+      options: {
+        dryRun: {
+          type: 'boolean',
+          describe: 'Run the script without making any database changes',
+          default: true,
+        },
+      },
     });
   }
 
-  async handle() {
+  async handle({ logger, options }) {
+    const { dryRun } = options;
+
     const todayDate = new Date();
     const oneYearAgoString = new Date(todayDate.getFullYear() - 1, todayDate.getMonth(), todayDate.getDate())
       .toISOString()
@@ -20,15 +29,18 @@ export class GetAnswersFromAssessments extends Script {
 
     const answerToBeDeleted = await knex
       .select('answers.*')
-      .from('answers')
-      .join('assessments', 'answers.assessmentId', 'assessments.id')
+      .from('assessments')
+      .join('answers', 'answers.assessmentId', 'assessments.id')
       .whereIn('assessments.type', targetTypes)
       .where('assessments.state', 'completed')
       .whereRaw('DATE(assessments."updatedAt") = ?', [oneYearAgoString]);
 
-    const answerToBeDeletedIds = answerToBeDeleted.map(({ id }) => id);
-
-    await knex.delete().from('answers').whereIn('id', answerToBeDeletedIds);
+    if (dryRun) {
+      logger.info(`${answerToBeDeleted.length} would be inserted to parquet file`);
+    } else {
+      const answerToBeDeletedIds = answerToBeDeleted.map(({ id }) => id);
+      await knex.delete().from('answers').whereIn('id', answerToBeDeletedIds);
+    }
   }
 }
 await ScriptRunner.execute(import.meta.url, GetAnswersFromAssessments);
--- api/tests/integration/scripts/get-answers-from-assessments_test.js
+++ api/tests/integration/scripts/get-answers-from-assessments_test.js
@@ -1,45 +1,101 @@
 import { expect } from 'chai';
+import sinon from 'sinon';
 
 import { knex } from '../../../db/knex-database-connection.js';
 import { GetAnswersFromAssessments } from '../../../scripts/prod/get-answers-from-assessments.js';
 import { databaseBuilder } from '../../tooling/databases.js';
 
 describe('GetAnswersFromAssessments', function () {
-  it('gets answers from a list of assessments', async function () {
-    const todayDate = new Date();
-    const oneYearAgo = new Date(todayDate.getFullYear() - 1, todayDate.getMonth(), todayDate.getDate());
+  describe('when running in dryRun mode', function () {
+    it('does not delete selected answers', async function () {
+      const logger = {
+        info: sinon.stub(),
+      };
+      const todayDate = new Date();
+      const oneYearAgo = new Date(todayDate.getFullYear() - 1, todayDate.getMonth(), todayDate.getDate());
 
-    const validAssessment = databaseBuilder.factory.buildAssessment({
-      updatedAt: oneYearAgo,
-      state: 'completed',
-      type: 'CAMPAIGN',
+      const validAssessment = databaseBuilder.factory.buildAssessment({
+        updatedAt: oneYearAgo,
+        state: 'completed',
+        type: 'CAMPAIGN',
+      });
+      databaseBuilder.factory.buildAnswer({ assessmentId: validAssessment.id });
+
+      const olderAssessment = databaseBuilder.factory.buildAssessment({
+        updatedAt: new Date('2020-01-01'),
+        state: 'completed',
+        type: 'CAMPAIGN',
+      });
+      databaseBuilder.factory.buildAnswer({
+        assessmentId: olderAssessment.id,
+      });
+
+      const certificationAssessment = databaseBuilder.factory.buildAssessment({
+        updatedAt: oneYearAgo,
+        state: 'completed',
+        type: 'CERTIFICATION',
+      });
+      databaseBuilder.factory.buildAnswer({
+        assessmentId: certificationAssessment.id,
+      });
+
+      await databaseBuilder.commit();
+
+      const script = new GetAnswersFromAssessments();
+      await script.handle({
+        logger,
+        options: { dryRun: true },
+      });
+
+      const remainingAnswers = await knex('answers');
+      expect(remainingAnswers.length).to.equal(3);
+      expect(logger.info.calledOnce).true;
     });
-    databaseBuilder.factory.buildAnswer({ assessmentId: validAssessment.id });
+  });
 
-    const olderAssessment = databaseBuilder.factory.buildAssessment({
-      updatedAt: new Date('2020-01-01'),
-      state: 'completed',
-      type: 'CAMPAIGN',
+  describe('when not running in dryRun mode', function () {
+    it('delete answers from a list of assessments', async function () {
+      const logger = {
+        info: sinon.stub(),
+      };
+      const todayDate = new Date();
+      const oneYearAgo = new Date(todayDate.getFullYear() - 1, todayDate.getMonth(), todayDate.getDate());
+
+      const validAssessment = databaseBuilder.factory.buildAssessment({
+        updatedAt: oneYearAgo,
+        state: 'completed',
+        type: 'CAMPAIGN',
+      });
+      databaseBuilder.factory.buildAnswer({ assessmentId: validAssessment.id });
+
+      const olderAssessment = databaseBuilder.factory.buildAssessment({
+        updatedAt: new Date('2020-01-01'),
+        state: 'completed',
+        type: 'CAMPAIGN',
+      });
+      databaseBuilder.factory.buildAnswer({
+        assessmentId: olderAssessment.id,
+      });
+
+      const certificationAssessment = databaseBuilder.factory.buildAssessment({
+        updatedAt: oneYearAgo,
+        state: 'completed',
+        type: 'CERTIFICATION',
+      });
+      databaseBuilder.factory.buildAnswer({
+        assessmentId: certificationAssessment.id,
+      });
+
+      await databaseBuilder.commit();
+
+      const script = new GetAnswersFromAssessments();
+      await script.handle({
+        logger,
+        options: { dryRun: false },
+      });
+
+      const remainingAnswers = await knex('answers');
+      expect(remainingAnswers.length).to.equal(2);
     });
-    databaseBuilder.factory.buildAnswer({
-      assessmentId: olderAssessment.id,
-    });
-
-    const certificationAssessment = databaseBuilder.factory.buildAssessment({
-      updatedAt: oneYearAgo,
-      state: 'completed',
-      type: 'CERTIFICATION',
-    });
-    databaseBuilder.factory.buildAnswer({
-      assessmentId: certificationAssessment.id,
-    });
-
-    await databaseBuilder.commit();
-
-    const script = new GetAnswersFromAssessments();
-    await script.handle();
-
-    const remainingAnswers = await knex('answers');
-    expect(remainingAnswers.length).to.equal(2);
   });
 });
