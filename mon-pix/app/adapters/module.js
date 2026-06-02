6795800ab8c3b173e33819343d733883fc5165d5
dianeCdrPix <diane.cordier@pix.fr>
2026-06-02 11:05:45 +0200

    🔥(mon-pix): remove use of slug to create queryRecord
    for modules

--- mon-pix/app/adapters/module.js
+++ mon-pix/app/adapters/module.js
@@ -2,13 +2,8 @@ import ApplicationAdapter from './application';
 
 export default class Module extends ApplicationAdapter {
   queryRecord(store, type, query) {
-    if (query.shortId || query.slug) {
-      let url = '';
-      if (query.shortId) {
-        url = `${this.host}/${this.namespace}/modules/v2/${query.shortId}`;
-      } else if (query.slug) {
-        url = `${this.host}/${this.namespace}/modules/${query.slug}`;
-      }
+    if (query.shortId) {
+      let url = `${this.host}/${this.namespace}/modules/v2/${query.shortId}`;
 
       if (query.encryptedRedirectionUrl) {
         const encryptedRedirectionUrl = encodeURIComponent(query.encryptedRedirectionUrl);
--- mon-pix/tests/unit/adapters/module-test.js
+++ mon-pix/tests/unit/adapters/module-test.js
@@ -6,50 +6,6 @@ module('Unit | Adapter | Module | Module', function (hooks) {
   setupTest(hooks);
 
   module('#queryRecord', function () {
-    module('when query.slug is not defined', function () {
-      test('should trigger an ajax call with the default url and method', async function (assert) {
-        // given
-        const query = {
-          'other-prop': 'bac-a-sable',
-        };
-
-        const store = this.owner.lookup('service:store');
-        const adapter = this.owner.lookup('adapter:module');
-        const type = { modelName: 'module' };
-        sinon.stub(adapter, 'ajax').resolves();
-        const expectedUrl = `http://localhost:3000/api/modules`;
-
-        // when
-        await adapter.queryRecord(store, type, query);
-
-        // then
-        sinon.assert.calledWithExactly(adapter.ajax, expectedUrl, 'GET', { data: { 'other-prop': 'bac-a-sable' } });
-        assert.ok(true);
-      });
-    });
-
-    module('when query.slug is defined', function () {
-      test('should trigger an ajax call with the right url and method', async function (assert) {
-        // given
-        const query = {
-          slug: 'bac-a-sable',
-        };
-
-        const store = this.owner.lookup('service:store');
-        const adapter = this.owner.lookup('adapter:module');
-        const type = { modelName: 'module' };
-        sinon.stub(adapter, 'ajax').resolves();
-        const expectedUrl = `http://localhost:3000/api/modules/${query.slug}`;
-
-        // when
-        await adapter.queryRecord(store, type, query);
-
-        // then
-        sinon.assert.calledWith(adapter.ajax, expectedUrl, 'GET');
-        assert.ok(true);
-      });
-    });
-
     module('when query.shortId is not defined', function () {
       test('should trigger an ajax call with the default url and method', async function (assert) {
         // given
