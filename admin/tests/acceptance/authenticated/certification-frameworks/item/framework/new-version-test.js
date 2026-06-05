1c3141b235a86d2f6f2c45d8631cdb4a4aafb496
rouxxi <gwenvael.laskar@pix.fr>
2026-06-05 15:07:44 +0200

    test(admin): add acceptance test for certification-version creation

---
+++ admin/tests/acceptance/authenticated/certification-frameworks/item/framework/new-version-test.js
@@ -0,0 +1,83 @@
+import { visit } from '@1024pix/ember-testing-library';
+import { click, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import { authenticateAdminMemberWithRole } from 'pix-admin/tests/helpers/test-init';
+import { setupMirage } from 'pix-admin/tests/test-support/setup-mirage';
+import { module, test } from 'qunit';
+
+module('Acceptance | Certification Framework | item | Framework | new-version', function (hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  hooks.beforeEach(function () {
+    server.create('certification-framework', { id: 'DROIT', name: 'DROIT' });
+    server.create('framework', { id: 'Pix', name: 'Pix' });
+    server.create('framework-history', { id: 'DROIT' });
+    const tube = server.create('tube', {
+      id: 'tubeId2',
+      name: '@tubeName2',
+      practicalTitle: 'Tube 2',
+      skills: [],
+      level: 8,
+    });
+    const thematics = [server.create('thematic', { id: 'thematicId1', name: 'Thématique 1', tubes: [tube] })];
+    const competences = [
+      server.create('competence', {
+        id: 'competenceId',
+        index: '1',
+        name: 'Titre competence',
+        thematics,
+      }),
+    ];
+    const areas = [
+      server.create('area', {
+        id: 'areaId',
+        title: 'Titre domaine',
+        code: 1,
+        competences,
+        frameworkId: 'frameworkId',
+      }),
+    ];
+    server.create('framework', { id: 'frameworkId', name: 'DROIT', areas });
+
+    server.create('certification-version', { id: 12, startDate: new Date(), expirationDate: null, areas });
+  });
+
+  module('when admin member has role "SUPER ADMIN"', function () {
+    test('should be redirected to the tube selection route with preselected tubes ', async function (assert) {
+      // given
+      await authenticateAdminMemberWithRole({ isSuperAdmin: true })(server);
+
+      // when
+      const screen = await visit(`/certification-frameworks/DROIT/framework/new-version?activeVersionId=12`);
+
+      // then
+      assert.strictEqual(
+        currentURL(),
+        '/certification-frameworks/DROIT/framework/new-version/tubes?activeVersionId=12',
+      );
+
+      assert.dom(screen.getByRole('button', { name: 'Référentiels :' })).exists();
+      await click(screen.getByRole('button', { name: 'Référentiels :' }));
+      assert.dom(await screen.findByRole('checkbox', { name: 'DROIT' })).isChecked();
+      assert.dom(screen.getByText('1/1 sujet(s) sélectionné(s)'));
+    });
+
+    test('should redirect to the configuration page when click on next', async function (assert) {
+      // given
+      await authenticateAdminMemberWithRole({ isSuperAdmin: true })(server);
+
+      const screen = await visit(`/certification-frameworks/DROIT/framework/new-version/tubes?activeVersionId=12`);
+
+      await click(screen.getByRole('button', { name: 'Suivant' }));
+
+      assert.dom(await screen.findByText('configuration wow !')).exists();
+
+      // then
+      assert.strictEqual(
+        currentURL(),
+        '/certification-frameworks/DROIT/framework/new-version/13/configuration?activeVersionId=12',
+      );
+    });
+  });
+});
--- admin/tests/mirage/routes.js
+++ admin/tests/mirage/routes.js
@@ -393,8 +393,13 @@ export default function routes() {
   this.get('/admin/organizations/:id/statistics', getOrganizationStatistics);
   this.post('/admin/organizations/:id/archive', archiveOrganization);
 
-  this.get('/admin/frameworks');
+  this.get('/admin/frameworks', function (schema) {
+    return schema.frameworks.all();
+  });
   this.get('/admin/frameworks/:id/areas', findFrameworkAreas);
+  this.post('/admin/frameworks/:scope/version', function (schema) {
+    return schema.certificationVersions.create();
+  });
 
   this.post('/admin/target-profiles', createTargetProfile);
   this.get('/admin/target-profiles/:id');
