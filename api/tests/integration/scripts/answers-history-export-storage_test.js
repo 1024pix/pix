db438a2f589bc8d6076107b4b2df5cb98a947e6c
Clément Latzarus <clement.latzarus@pix.fr>
2026-06-05 17:04:53 +0200

    tests(api): add sendFile method test

--- api/tests/integration/scripts/answers-history-export-storage_test.js
+++ api/tests/integration/scripts/answers-history-export-storage_test.js
@@ -1,8 +1,12 @@
+import { Readable } from "node:stream";
+
 import sinon from 'sinon';
 
 import { AnswersHistoryExportStorage } from '../../../scripts/prod/answers-history-export-storage.js';
+import { FileValidationError } from "../../../src/shared/domain/errors.js";
 import { S3ObjectStorageProvider } from '../../../src/shared/storage/infrastructure/providers/S3ObjectStorageProvider.js';
 import { expect } from '../../test-helper.js';
+import { catchErr } from "../../tooling/test-utils/error.js";
 
 describe('Unit | Storage | AnswerHistoryExportsStorage', function () {
   describe('constructor', function () {
@@ -29,4 +33,57 @@ describe('Unit | Storage | AnswerHistoryExportsStorage', function () {
       sinon.restore();
     });
   });
+
+  describe('sendFile', function () {
+    it('should send a file', async function() {
+      // given
+      const providerStub = sinon.createStubInstance(S3ObjectStorageProvider);
+      sinon.stub(S3ObjectStorageProvider, 'createClient').returns(providerStub);
+      const answersHistoryExportStorage = new AnswersHistoryExportStorage();
+      providerStub.startUpload.resolves();
+      const fileContent = new ArrayBuffer(1);
+
+      // when
+      await answersHistoryExportStorage.sendFile({
+        filename: 'hey.gz',
+        fileContent: fileContent,
+      });
+
+      // then
+      const actualReadableStream = providerStub.startUpload.firstCall.args[0].readableStream;
+      expect(providerStub.startUpload).to.have.been.calledWith(sinon.match({
+        filename: 'hey.gz',
+        readableStream: sinon.match.instanceOf(Readable),
+      }));
+      await _compareReadableStreamToFileContent(actualReadableStream, fileContent);
+    });
+
+    describe('when file content is invalid', function () {
+      it('should throw an error', async function() {
+        // given
+        const providerStub = sinon.createStubInstance(S3ObjectStorageProvider);
+        sinon.stub(S3ObjectStorageProvider, 'createClient').returns(providerStub);
+        const answersHistoryExportStorage = new AnswersHistoryExportStorage();
+        providerStub.startUpload.resolves();
+
+        // when
+        const error = await catchErr(answersHistoryExportStorage.sendFile)({
+          filename: 'hey.gz',
+        });
+
+        // then
+        expect(error).to.be.instanceOf(FileValidationError);
+        expect(error.message).to.equal('An error occurred, file is invalid');
+      })
+    });
+  });
 });
+
+async function _compareReadableStreamToFileContent(readableStream, fileContent) {
+  const actualStream = readableStream;
+  const actualStreamChucks = [];
+  for await (const chunk of actualStream) {
+    actualStreamChucks.push(chunk);
+  }
+  expect(Buffer.concat(actualStreamChucks)).to.deep.equal(Buffer.from(fileContent));
+}
