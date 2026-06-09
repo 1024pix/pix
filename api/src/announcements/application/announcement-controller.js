32076b36520023a16622c682f1fbaae4371d978e
Benjamin Petetot <benjamin.petetot@pix.fr>
2026-06-09 16:37:14 +0200

    refactor(api): announcements - remove serializer injections in controllers

--- api/src/announcements/application/announcement-controller.js
+++ api/src/announcements/application/announcement-controller.js
@@ -1,17 +1,17 @@
 import { usecases } from '../domain/usecases/index.js';
 import * as announcementSerializer from '../infrastructure/serializers/jsonapi/announcement-serializer.js';
 
-const get = async (request, h, dependencies = { announcementSerializer }) => {
+const get = async (request) => {
   const { name } = request.params;
   const announcement = await usecases.getAnnouncement({ name });
-  return dependencies.announcementSerializer.serialize(announcement);
+  return announcementSerializer.serialize(announcement);
 };
 
-const update = async (request, h, dependencies = { announcementSerializer }) => {
+const update = async (request) => {
   const { name } = request.params;
   const content = request.payload?.data?.attributes?.content ?? null;
   const announcement = await usecases.updateAnnouncement({ name, content });
-  return dependencies.announcementSerializer.serialize(announcement);
+  return announcementSerializer.serialize(announcement);
 };
 
 export const announcementController = {
