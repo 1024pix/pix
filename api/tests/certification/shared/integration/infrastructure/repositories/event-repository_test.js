7063f0f3a06401abc358242d3bdeab0fd63e846b
laura bergoens <laura.bergoens@pix.fr>
2026-06-11 18:38:53 +0200

    tech(api): put certification event sourcing under FT

--- api/tests/certification/shared/integration/infrastructure/repositories/event-repository_test.js
+++ api/tests/certification/shared/integration/infrastructure/repositories/event-repository_test.js
@@ -1,37 +1,60 @@
+import { setImmediate } from 'node:timers/promises';
+
 import sinon from 'sinon';
 
 import * as eventRepository from '../../../../../../src/certification/shared/infrastructure/repositories/event-repository.js';
+import { featureToggles } from '../../../../../../src/shared/infrastructure/feature-toggles/index.js';
+import { expect } from '../../../../../test-helper.js';
 import { knex } from '../../../../../tooling/databases.js';
 import { domainBuilder } from '../../../../../tooling/domain-builder/domain-builder.js';
 
 describe('Certification | Shared | Integration | Repository | Event', function () {
-  afterEach(function () {
-    return knex('certification_events').truncate();
-  });
-
   describe('#push', function () {
-    it('should persist the event in db', async function () {
-      const event = domainBuilder.certification.shared.buildEvent({
-        id: null,
-        name: 'SomeEvent',
-        candidateId: 123,
-        createdAt: new Date('2021-01-01T00:00:00Z'),
-        metadata: { foo: 'bar' },
-      });
-
-      await eventRepository.push(event);
-
-      // then
-      const events = await knex('certification_events').select();
-      sinon.assert.match(events, [
-        {
-          id: sinon.match.number,
-          eventName: 'SomeEvent',
+    context('when FT isEventSourcingCertificationEnabled is true', function () {
+      it('persists the event in db', async function () {
+        await featureToggles.set('isEventSourcingCertificationEnabled', true);
+        await setImmediate();
+        const event = domainBuilder.certification.shared.buildEvent({
+          id: null,
+          name: 'SomeEvent',
           candidateId: 123,
           createdAt: new Date('2021-01-01T00:00:00Z'),
           metadata: { foo: 'bar' },
-        },
-      ]);
+        });
+
+        await eventRepository.push(event);
+
+        const events = await knex('certification_events').select();
+        sinon.assert.match(events, [
+          {
+            id: sinon.match.number,
+            eventName: 'SomeEvent',
+            candidateId: 123,
+            createdAt: new Date('2021-01-01T00:00:00Z'),
+            metadata: { foo: 'bar' },
+          },
+        ]);
+      });
+    });
+
+    context('when FT isEventSourcingCertificationEnabled is false', function () {
+      it('does nothing', async function () {
+        await featureToggles.set('isEventSourcingCertificationEnabled', false);
+        await setImmediate();
+        const event = domainBuilder.certification.shared.buildEvent({
+          id: null,
+          name: 'SomeEvent',
+          candidateId: 123,
+          createdAt: new Date('2021-01-01T00:00:00Z'),
+          metadata: { foo: 'bar' },
+        });
+
+        await eventRepository.push(event);
+
+        // then
+        const events = await knex('certification_events').select();
+        expect(events).to.have.length(0);
+      });
     });
   });
 });
