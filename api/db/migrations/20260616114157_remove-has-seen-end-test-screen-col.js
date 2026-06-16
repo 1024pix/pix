66ec015980277a0a259effae4b04cecff0b851c8
Yannick François <yannick.francois@pix.fr>
2026-06-16 14:06:21 +0200

    :broom: refactor(db) remove unused column of certification-courses

---
+++ api/db/migrations/20260616114157_remove-has-seen-end-test-screen-col.js
@@ -0,0 +1,24 @@
+const TABLE_NAME = 'certification-courses';
+const COLUMN_NAME = 'hasSeenEndTestScreen';
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const up = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.dropColumn(COLUMN_NAME);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const down = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.boolean(COLUMN_NAME).defaultTo(false);
+  });
+};
+
+export { down, up };
