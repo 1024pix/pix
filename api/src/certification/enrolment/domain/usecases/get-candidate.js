01923e9182d73c8a10d8cc741f54bc1a4a1ed7c9
Lou Goubin <103123143+lgoubin@users.noreply.github.com>
2026-06-16 17:42:44 +0200

    chore(api): move eligibility logic into getCandidate usecase

--- api/src/certification/enrolment/domain/usecases/get-candidate.js
+++ api/src/certification/enrolment/domain/usecases/get-candidate.js
@@ -6,6 +6,34 @@
  * @param {object} params
  * @param {CandidateRepository} params.candidateRepository
  */
-export async function getCandidate({ certificationCandidateId, candidateRepository }) {
-  return candidateRepository.get({ certificationCandidateId });
+export async function getCandidate({
+  certificationCandidateId,
+  candidateRepository,
+  certificationBadgesService,
+  certificationCenterRepository,
+}) {
+  const candidate = await candidateRepository.get({ certificationCandidateId });
+
+  if (!candidate.isRegisteredToDoubleCertification()) {
+    candidate.doubleCertificationEligibility = false;
+    return candidate;
+  }
+
+  const center = await certificationCenterRepository.getBySessionId({ sessionId: candidate.sessionId });
+
+  if (!center.isHabilitated(candidate.subscription)) {
+    candidate.doubleCertificationEligibility = false;
+    return candidate;
+  }
+
+  const certifiableBadgeAcquisitions = await certificationBadgesService.findStillValidBadgeAcquisitions({
+    userId: candidate.userId,
+    limitDate: candidate.reconciledAt,
+  });
+
+  candidate.doubleCertificationEligibility = certifiableBadgeAcquisitions.some(
+    (b) => b.complementaryCertificationKey === candidate.subscription,
+  );
+
+  return candidate;
 }
