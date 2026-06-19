8c4ac249081654c6b152c31a15efe69a18383235
Simon Bertaux <simon.bertaux@pix.fr>
2026-06-19 17:16:52 +0200

    feat(api): add method findOrganizationLearnersForAdmin to learner list controller

--- api/src/prescription/organization-learner/application/learner-list-controller.js
+++ api/src/prescription/organization-learner/application/learner-list-controller.js
@@ -1,5 +1,6 @@
 import { divisionSerializer } from '../../campaign/infrastructure/serializers/jsonapi/division-serializer.js';
 import { usecases } from '../domain/usecases/index.js';
+import * as adminOrganizationLearnerSerializer from '../infrastructure/serializers/jsonapi/admin-organization-learner-serializer.js';
 import { organizationParticipantsSerializer } from '../infrastructure/serializers/jsonapi/organization-participants-serializer.js';
 import { mapCertificabilityByLabel } from './../../shared/application/helpers.js';
 
@@ -25,12 +26,22 @@ const findPaginatedFilteredParticipants = async function (
   return dependencies.organizationParticipantsSerializer.serialize(results);
 };
 
+const findOrganizationLearnersForAdmin = async function (
+  request,
+  _,
+  dependencies = { adminOrganizationLearnerSerializer },
+) {
+  const { page, filter } = request.query;
+  const { learners, pagination } = await usecases.findOrganizationLearnersForAdmin({ page, filter });
+  return dependencies.adminOrganizationLearnerSerializer.serialize({ learners, pagination });
+};
+
 const getDivisions = async function (request) {
   const organizationId = request.params.id;
   const divisions = await usecases.findDivisionsByOrganization({ organizationId });
   return divisionSerializer.serialize(divisions);
 };
 
-const learnerListController = { findPaginatedFilteredParticipants, getDivisions };
+const learnerListController = { findPaginatedFilteredParticipants, findOrganizationLearnersForAdmin, getDivisions };
 
 export { learnerListController };
