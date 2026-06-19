586d3d6008931518f1f80fadcc914795bcf3b9e3
Quentin Lebouc <quentin.lebouc@pix.fr>
2026-06-19 14:51:56 +0200

    feat(mon-pix): update user TOS acceptance payload for mirage
    Signed-off-by: Quentin Lebouc <quentin.lebouc@pix.fr>

--- mon-pix/mirage/routes/users/patch-terms-of-service-acceptance.js
+++ mon-pix/mirage/routes/users/patch-terms-of-service-acceptance.js
@@ -1,7 +1,11 @@
 export default function (schema, request) {
   const userId = request.params.id;
   const user = schema.users.find(userId);
-  user.update({ mustValidateTermsOfService: false, lastTermsOfServiceValidatedAt: '2020-06-06' });
+  user.update({
+    mustValidateTermsOfService: false,
+    lastTermsOfServiceValidatedAt: '2020-06-06',
+    pixAppTermsOfServiceStatus: 'accepted',
+  });
 
   return user;
 }
