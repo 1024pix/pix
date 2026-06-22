247e1f49bab6c4af09e5a281f8ca278f0edaa831
Benjamin Petetot <bpetetot@gmail.com>
2026-06-22 15:23:02 +0200

    refactor(api): learner-management - delete campaign-participation-repository.js

--- api/src/prescription/learner-management/infrastructure/repositories/index.js
+++ api/src/prescription/learner-management/infrastructure/repositories/index.js
@@ -1,11 +1,9 @@
 import * as organizationFeatureApi from '../../../../organizational-entities/application/api/organization-features-api.js';
 import { injectDependencies } from '../../../../shared/infrastructure/utils/dependency-injection.js';
-import * as campaignParticipationRepository from './campaign-participation-repository.js';
 import * as organizationFeatureRepository from './organization-feature-repository.js';
 
 const repositoriesWithoutInjectedDependencies = {
   organizationFeatureRepository,
-  campaignParticipationRepository,
 };
 
 const dependencies = {
