2cc523b615a64cb02a32401eac9905dacdd032c7
Benjamin Petetot <bpetetot@gmail.com>
2026-06-22 15:19:19 +0200

    refactor(api): iam - delete anonymized-user.repository.js

--- api/src/identity-access-management/infrastructure/repositories/anonymized-user.repository.js
+++
@@ -1,8 +0,0 @@
-import { DomainTransaction } from '../../../shared/domain/DomainTransaction.js';
-
-async function findLegacyAnonymizedIds() {
-  const knexConn = DomainTransaction.getConnection();
-  return knexConn('users').where({ hasBeenAnonymised: true }).whereNot({ lastName: '(anonymised)' }).pluck('id');
-}
-
-export const anonymizedUserRepository = { findLegacyAnonymizedIds };
