8f4503cd5e470a6452a1c0ad40ee48dafc03cbc6
laura bergoens <laura.bergoens@pix.fr>
2026-06-24 18:01:04 +0200

    tech(api): build certificationCandidates through correlated subquery

--- api/src/certification/session-management/infrastructure/repositories/session-for-supervising-repository.js
+++ api/src/certification/session-management/infrastructure/repositories/session-for-supervising-repository.js
@@ -16,7 +16,9 @@ const get = async function ({ id }) {
       examiner: 'sessions.examiner',
       accessCode: 'sessions.accessCode',
       address: 'sessions.address',
-      certificationCandidates: knexConn.raw(`
+      certificationCandidates: knexConn
+        .select(
+          knexConn.raw(`
         json_agg(json_build_object(
           'userId', "certification-candidates"."userId",
           'firstName', "certification-candidates"."firstName",
@@ -42,26 +44,27 @@ const get = async function ({ id }) {
           'subscription', "certification-candidates"."subscription"
         ) order by "certification-companion-live-alerts".status, "certification-challenge-live-alerts".status, lower("certification-candidates"."lastName"), lower("certification-candidates"."firstName"))
     `),
+        )
+        .from('certification-candidates')
+        .leftJoin('certification-courses', 'certification-courses.candidateId', 'certification-candidates.id')
+        .leftJoin('assessments', 'assessments.certificationCourseId', 'certification-courses.id')
+        .leftJoin('certification-challenge-live-alerts', function () {
+          this.on('certification-challenge-live-alerts.assessmentId', '=', 'assessments.id').andOnVal(
+            'certification-challenge-live-alerts.status',
+            '=',
+            CertificationChallengeLiveAlertStatus.ONGOING,
+          );
+        })
+        .leftJoin('certification-companion-live-alerts', function () {
+          this.on('certification-companion-live-alerts.assessmentId', '=', 'assessments.id').andOnVal(
+            'certification-companion-live-alerts.status',
+            '=',
+            CertificationCompanionLiveAlertStatus.ONGOING,
+          );
+        })
+        .whereRaw('"certification-candidates"."sessionId" = sessions.id'),
     })
     .from('sessions')
-    .leftJoin('certification-candidates', 'certification-candidates.sessionId', 'sessions.id')
-    .leftJoin('certification-courses', 'certification-courses.candidateId', 'certification-candidates.id')
-    .leftJoin('assessments', 'assessments.certificationCourseId', 'certification-courses.id')
-    .leftJoin('certification-challenge-live-alerts', function () {
-      this.on('certification-challenge-live-alerts.assessmentId', '=', 'assessments.id').andOnVal(
-        'certification-challenge-live-alerts.status',
-        '=',
-        CertificationChallengeLiveAlertStatus.ONGOING,
-      );
-    })
-    .leftJoin('certification-companion-live-alerts', function () {
-      this.on('certification-companion-live-alerts.assessmentId', '=', 'assessments.id').andOnVal(
-        'certification-companion-live-alerts.status',
-        '=',
-        CertificationCompanionLiveAlertStatus.ONGOING,
-      );
-    })
-    .groupBy('sessions.id')
     .where({ 'sessions.id': id })
     .first();
   if (!results) {
@@ -73,9 +76,10 @@ const get = async function ({ id }) {
 export { get };
 
 function _toDomain(results) {
-  const certificationCandidates = results.certificationCandidates
-    .filter((candidate) => candidate?.id !== null)
-    .map((candidate) => new CertificationCandidateForSupervising(candidate));
+  const certificationCandidates =
+    results.certificationCandidates
+      ?.filter((candidate) => candidate?.id !== null)
+      ?.map((candidate) => new CertificationCandidateForSupervising(candidate)) ?? [];
 
   return new SessionForSupervising({
     ...results,
