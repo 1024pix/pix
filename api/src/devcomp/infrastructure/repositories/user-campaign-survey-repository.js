20c3f5088c1f2c74c4c3ab4f320925078f52931c
Eric Lim <eric.lim@zenika.com>
2026-06-25 15:23:51 +0200

    ⚡️(api): add findByCampaignIdAndUserId method
    in user-campaign-survey-repository
    
    Co-authored-by: Matthias Ferraina <matthias.ferraina@pix.fr>

--- api/src/devcomp/infrastructure/repositories/user-campaign-survey-repository.js
+++ api/src/devcomp/infrastructure/repositories/user-campaign-survey-repository.js
@@ -1,11 +1,14 @@
 import { DomainTransaction } from '../../../shared/domain/DomainTransaction.js';
 import { AlreadyExistingEntityError } from '../../../shared/domain/errors.js';
 import { isUniqConstraintViolated } from '../../../shared/infrastructure/utils/knex-utils.js';
+import { UserCampaignSurvey } from '../../domain/models/UserCampaignSurvey.js';
+
+const TABLE_NAME = 'user-campaign-surveys';
 
 export async function save(userCampaignSurvey) {
   const knexConn = DomainTransaction.getConnection();
   try {
-    const [result] = await knexConn('user-campaign-surveys').insert(userCampaignSurvey).returning('id');
+    const [result] = await knexConn(TABLE_NAME).insert(userCampaignSurvey).returning('id');
     return result.id;
   } catch (error) {
     if (isUniqConstraintViolated(error)) {
@@ -13,4 +16,19 @@ export async function save(userCampaignSurvey) {
     }
     throw error;
   }
+}
+
+export async function findByCampaignIdAndUserId({ campaignId, userId }) {
+  const knexConn = DomainTransaction.getConnection();
+  const userCampaignSurvey = await knexConn(TABLE_NAME).where({ campaignId, userId }).first();
+
+  if (!userCampaignSurvey) {
+    return null;
+  }
+
+  return _toDomain(userCampaignSurvey);
+}
+
+function _toDomain({ campaignId, userId, satisfactionScore } = {}) {
+  return new UserCampaignSurvey({ campaignId, userId, satisfactionScore });
 }
