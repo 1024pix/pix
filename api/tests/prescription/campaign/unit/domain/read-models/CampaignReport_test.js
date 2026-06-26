06afbdc99f6369cf12dff6bef920dbe136991172
Benjamin Petetot <bpetetot@gmail.com>
2026-06-26 09:09:17 +0200

    chore(api): move CampaignReport_test in prescription/campaign

--- api/tests/prescription/campaign/unit/domain/read-models/CampaignReport_test.js
+++ api/tests/prescription/campaign/unit/domain/read-models/CampaignReport_test.js
@@ -1,7 +1,7 @@
-import { CampaignReport } from '../../../../src/prescription/campaign/domain/read-models/CampaignReport.js';
-import { CampaignTypes } from '../../../../src/prescription/shared/domain/constants.js';
-import { expect } from '../../../test-helper.js';
-import { domainBuilder } from '../../../tooling/domain-builder/domain-builder.js';
+import { CampaignReport } from '../../../../../../src/prescription/campaign/domain/read-models/CampaignReport.js';
+import { CampaignTypes } from '../../../../../../src/prescription/shared/domain/constants.js';
+import { expect } from '../../../../../test-helper.js';
+import { domainBuilder } from '../../../../../tooling/domain-builder/domain-builder.js';
 
 describe('Unit | Domain | Models | CampaignReport', function () {
   describe('getters', function () {
