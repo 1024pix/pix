6ec3b4ada93d82df4ffff1419f71be9faf9f0565
Benjamin Petetot <bpetetot@gmail.com>
2026-06-26 08:25:54 +0200

    chore(api): move organization-learner-validator_test in prescription/learner-management

--- api/tests/prescription/learner-management/unit/domain/validators/organization-learner-validator_test.js
+++ api/tests/prescription/learner-management/unit/domain/validators/organization-learner-validator_test.js
@@ -1,8 +1,8 @@
 import {
   checkValidation,
   FRANCE_COUNTRY_CODE,
-} from '../../../../src/prescription/learner-management/domain/validators/organization-learner-validator.js';
-import { expect } from '../../../test-helper.js';
+} from '../../../../../../src/prescription/learner-management/domain/validators/organization-learner-validator.js';
+import { expect } from '../../../../../test-helper.js';
 
 describe('Unit | Domain | Organization Learner validator', function () {
   context('#checkValidation', function () {
