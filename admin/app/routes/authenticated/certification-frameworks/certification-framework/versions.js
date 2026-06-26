7508be7c44743d399df5583a4ee09fa3c244d84c
rouxxi <gwenvael.laskar@pix.fr>
2026-06-26 10:35:04 +0200

    feat(admin): add redirection in version route and his children route
    Co-authored-by: Laura Bergoens <laura.bergoens@pix.fr>

--- admin/app/routes/authenticated/certification-frameworks/certification-framework/versions.js
+++ admin/app/routes/authenticated/certification-frameworks/certification-framework/versions.js
@@ -3,6 +3,14 @@ import { inject as service } from '@ember/service';
 
 export default class VersionsRoute extends Route {
   @service store;
+  @service accessControl;
+
+  beforeModel() {
+    this.accessControl.restrictAccessTo(
+      ['isSuperAdmin'],
+      'authenticated.certification-frameworks.certification-framework',
+    );
+  }
 
   async model() {
     return this.modelFor('authenticated.certification-frameworks.certification-framework');
