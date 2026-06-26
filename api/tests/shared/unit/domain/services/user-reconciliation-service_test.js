f028223136212edd2dafac27ccc1b12dfc2f3ae4
Benjamin Petetot <bpetetot@gmail.com>
2026-06-26 08:35:42 +0200

    chore(api): move user-reconciliation-service_test in shared

--- api/tests/shared/unit/domain/services/user-reconciliation-service_test.js
+++ api/tests/shared/unit/domain/services/user-reconciliation-service_test.js
@@ -5,11 +5,11 @@ import {
   NotFoundError,
   OrganizationLearnerAlreadyLinkedToInvalidUserError,
   OrganizationLearnerAlreadyLinkedToUserError,
-} from '../../../../src/shared/domain/errors.js';
-import * as userReconciliationService from '../../../../src/shared/domain/services/user-reconciliation-service.js';
-import { expect } from '../../../test-helper.js';
-import { domainBuilder } from '../../../tooling/domain-builder/domain-builder.js';
-import { catchErr } from '../../../tooling/test-utils/error.js';
+} from '../../../../../src/shared/domain/errors.js';
+import * as userReconciliationService from '../../../../../src/shared/domain/services/user-reconciliation-service.js';
+import { expect } from '../../../../test-helper.js';
+import { domainBuilder } from '../../../../tooling/domain-builder/domain-builder.js';
+import { catchErr } from '../../../../tooling/test-utils/error.js';
 
 describe('Unit | Service | user-reconciliation-service', function () {
   let organizationLearners;
