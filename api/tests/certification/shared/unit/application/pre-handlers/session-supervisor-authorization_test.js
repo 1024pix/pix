ab7b496a413ee6f261ace58f35448650e99b72a6
Benjamin Petetot <bpetetot@gmail.com>
2026-06-26 08:06:03 +0200

    chore(api): move session-supervisor-authorization_test in certification/shared

--- api/tests/certification/shared/unit/application/pre-handlers/session-supervisor-authorization_test.js
+++ api/tests/certification/shared/unit/application/pre-handlers/session-supervisor-authorization_test.js
@@ -1,9 +1,9 @@
 import sinon from 'sinon';
 
-import { assessmentInvigilatorAuthorization as sessionInvigilatorAuthorization } from '../../../../src/certification/shared/application/pre-handlers/session-invigilator-authorization.js';
-import { expect } from '../../../test-helper.js';
-import { hFake } from '../../../tooling/mocks/hapi.mock.js';
-import { generateAuthenticatedUserRequestHeaders } from '../../../tooling/test-utils/http-server.js';
+import { assessmentInvigilatorAuthorization as sessionInvigilatorAuthorization } from '../../../../../../src/certification/shared/application/pre-handlers/session-invigilator-authorization.js';
+import { expect } from '../../../../../test-helper.js';
+import { hFake } from '../../../../../tooling/mocks/hapi.mock.js';
+import { generateAuthenticatedUserRequestHeaders } from '../../../../../tooling/test-utils/http-server.js';
 
 describe('Unit | Pre-handler | Invigilator Authorization', function () {
   let invigilatorAccessRepository;
