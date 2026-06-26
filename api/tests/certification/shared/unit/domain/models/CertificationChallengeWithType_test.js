17692493bf8280e19912d55a87e15c149f5210dd
Benjamin Petetot <bpetetot@gmail.com>
2026-06-26 08:44:47 +0200

    chore(api): move CertificationChallengeWithType_test in certification/shared

--- api/tests/certification/shared/unit/domain/models/CertificationChallengeWithType_test.js
+++ api/tests/certification/shared/unit/domain/models/CertificationChallengeWithType_test.js
@@ -1,7 +1,7 @@
-import { CertificationChallengeWithType } from '../../../../src/certification/shared/domain/models/CertificationChallengeWithType.js';
-import { Type } from '../../../../src/shared/domain/models/Challenge.js';
-import { expect } from '../../../test-helper.js';
-import { domainBuilder } from '../../../tooling/domain-builder/domain-builder.js';
+import { CertificationChallengeWithType } from '../../../../../../src/certification/shared/domain/models/CertificationChallengeWithType.js';
+import { Type } from '../../../../../../src/shared/domain/models/Challenge.js';
+import { expect } from '../../../../../test-helper.js';
+import { domainBuilder } from '../../../../../tooling/domain-builder/domain-builder.js';
 
 describe('Unit | Domain | Models | CertificationChallengeWithType', function () {
   describe('#constructor', function () {
