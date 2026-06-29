82ebf8092b25ab6da59d7bbc3ab64a03fea82790
Solene Livran <solene.livran@pix.fr>
2026-06-29 11:10:32 +0200

    add-column-reward-requirements-description-to-quest

---
+++ api/db/migrations/20260629090621_add-column-reward-requirements-description-in-quests-table.js
@@ -0,0 +1,24 @@
+const TABLE_NAME = 'quests';
+const COLUMN_NAME = 'rewardRequirementsDescription';
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const up = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.text(COLUMN_NAME).nullable().comment('The description of the necessary requirements to obtain a reward');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const down = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.dropColumn(COLUMN_NAME);
+  });
+};
+
+export { down, up };
