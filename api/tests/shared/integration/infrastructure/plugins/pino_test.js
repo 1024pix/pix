cfc927686fc3a0cf68957cf3d70ae6ec0dc6635c
Benjamin Petetot <bpetetot@gmail.com>
2026-06-29 16:39:18 +0200

    refactor(api): move pino test into shared

--- api/tests/shared/integration/infrastructure/plugins/pino_test.js
+++ api/tests/shared/integration/infrastructure/plugins/pino_test.js
@@ -5,11 +5,11 @@ import pino from 'pino';
 import {
   incrementInContext,
   installHapiHook,
-} from '../../../../src/shared/infrastructure/execution-context-manager.js';
-import * as pinoPlugin from '../../../../src/shared/infrastructure/plugins/pino.js';
-import { expect } from '../../../test-helper.js';
-import { HttpTestServer } from '../../../tooling/server/http-test-server.js';
-import { generateAuthenticatedUserRequestHeaders } from '../../../tooling/test-utils/http-server.js';
+} from '../../../../../src/shared/infrastructure/execution-context-manager.js';
+import * as pinoPlugin from '../../../../../src/shared/infrastructure/plugins/pino.js';
+import { expect } from '../../../../test-helper.js';
+import { HttpTestServer } from '../../../../tooling/server/http-test-server.js';
+import { generateAuthenticatedUserRequestHeaders } from '../../../../tooling/test-utils/http-server.js';
 
 describe('Integration | Infrastructure | plugins | pino', function () {
   let httpTestServer;
