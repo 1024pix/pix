01bbc1a1683ea2ba2c7540604028c2a25fae36a7
Alexandre COIN <alexandre.pc.coin@gmail.com>
2026-06-30 14:45:08 +0200

    tech(api): update getAnswersByAssessmentTypeAndDateAndState to use index on updateAt

--- api/src/db-history/infrastructure/repositories/answers-repository.js
+++ api/src/db-history/infrastructure/repositories/answers-repository.js
@@ -2,13 +2,16 @@ import { knex } from '../../../../db/knex-database-connection.js';
 import { DomainTransaction } from '../../../shared/domain/DomainTransaction.js';
 
 export const getAnswersByAssessmentTypeAndDateAndState = async ({ targetTypes, targetDate, targetState }) => {
+  const nextDay = new Date(targetDate);
+  nextDay.setDate(nextDay.getDate() + 1);
+
   return knex
     .select('answers.*')
     .from('assessments')
     .innerJoin('answers', 'answers.assessmentId', 'assessments.id')
     .whereIn('assessments.type', targetTypes)
     .where('assessments.state', targetState)
-    .whereRaw('DATE(assessments."updatedAt") = ?', [targetDate]);
+    .whereRaw('assessments."updatedAt" >= ? AND assessments."updatedAt" < ?', [new Date(targetDate), nextDay]);
 };
 
 export const deleteAnswersByIds = ({ ids }) => {
