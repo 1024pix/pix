ef51d6a20baf93090ee1e684148cd5390263faa8
Benjamin Petetot <bpetetot@gmail.com>
2026-07-03 14:21:07 +0200

    refactor(api): add tests got accessCodeGenerator

--- api/src/shared/domain/services/access-code-generator.js
+++ api/src/shared/domain/services/access-code-generator.js
@@ -1,19 +1,5 @@
 import { generateCode } from '../../infrastructure/utils/code-generator.js';
 
-async function generate(repository, pendingList = [], dependencies = { generateCode }) {
-  const letters = dependencies.generateCode(6, 'alphaSafe').toUpperCase();
-  const numbers = dependencies.generateCode(3, 'numericSafe');
-
-  const generatedCode = letters.concat(numbers);
-
-  if (pendingList.includes(generatedCode)) {
-    return generate(repository, pendingList, dependencies);
-  }
-
-  const isCodeAvailable = await repository.isCodeAvailable({ code: generatedCode });
-  return isCodeAvailable ? generatedCode : generate(repository, pendingList, dependencies);
-}
-
 export async function generateAvailableAccessCode(isAvailableCallback, dependencies = { generateCode }) {
   const letters = dependencies.generateCode(6, 'alphaSafe').toUpperCase();
   const numbers = dependencies.generateCode(3, 'numericSafe');
@@ -24,5 +10,3 @@ export async function generateAvailableAccessCode(isAvailableCallback, dependenc
 
   return generateAvailableAccessCode(isAvailableCallback, dependencies);
 }
-
-export { generate };
--- api/tests/shared/unit/domain/services/code-generator_test.js
+++ api/tests/shared/unit/domain/services/code-generator_test.js
@@ -1,47 +1,26 @@
 import sinon from 'sinon';
 
-import { generate } from '../../../../../src/shared/domain/services/access-code-generator.js';
+import { generateAvailableAccessCode } from '../../../../../src/shared/domain/services/access-code-generator.js';
 import { expect } from '../../../../test-helper.js';
 
-describe('Unit | Domain | Services | code generator', function () {
-  describe('#createCode', function () {
-    const accessCodeRepository = {
-      isCodeAvailable: () => undefined,
-    };
-
-    beforeEach(function () {
-      sinon.stub(accessCodeRepository, 'isCodeAvailable');
-      accessCodeRepository.isCodeAvailable.resolves(true);
-    });
-
-    it('should create a code with a length of 9 characters', async function () {
+describe('Unit | Domain | Services | access code generator', function () {
+  describe('generateAvailableAccessCode', function () {
+    it('generates an access code beginning with 6 letters and finishing with 3 numbers', async function () {
       // given
       const generateCode = sinon.stub();
       generateCode.onFirstCall().returns('abcdef');
       generateCode.onSecondCall().returns('123');
 
       // when
-      const code = await generate(accessCodeRepository, [], { generateCode });
-
-      // then
-      expect(code).to.have.lengthOf(9);
-    });
-
-    it('should create a code beginning with 6 letters and finishing with 3 numbers', async function () {
-      // given
-      const generateCode = sinon.stub();
-      generateCode.onFirstCall().returns('abcdef');
-      generateCode.onSecondCall().returns('123');
-
-      // when
-      await generate(accessCodeRepository, [], { generateCode });
+      const code = await generateAvailableAccessCode(() => true, { generateCode });
 
       // then
       expect(generateCode).to.have.been.calledWith(6, 'alphaSafe');
       expect(generateCode).to.have.been.calledWith(3, 'numericSafe');
+      expect(code).to.equal('ABCDEF123');
     });
 
-    it('should not be already assigned', async function () {
+    it('checks the availability of the generated code', async function () {
       // given
       const generateCode = sinon.stub();
       generateCode.onFirstCall().returns('abcdef');
@@ -49,36 +28,15 @@ describe('Unit | Domain | Services | code generator', function () {
       generateCode.onThirdCall().returns('ghijkl');
       generateCode.onCall(3).returns('456');
 
-      accessCodeRepository.isCodeAvailable.onCall(0).resolves(false);
-      accessCodeRepository.isCodeAvailable.onCall(1).resolves(true);
+      const availabilityCallback = (code) => {
+        return code === 'GHIJKL456';
+      };
 
       // when
-      const generatedCode = await generate(accessCodeRepository, [], { generateCode });
+      const generatedCode = await generateAvailableAccessCode(availabilityCallback, { generateCode });
 
       // then
-      expect(accessCodeRepository.isCodeAvailable).to.have.been.called;
       expect(generatedCode).to.equal('GHIJKL456');
-    });
-
-    it('should returns different campaign code from a given list when batch insert campaigns', async function () {
-      // given
-      const pendingCodeNumbers = '345';
-      const pendingCodeLetters = 'AZERTY';
-
-      const pendingCode = pendingCodeLetters.concat(pendingCodeNumbers);
-
-      const generateCode = sinon.stub();
-      generateCode.onCall(0).returns(pendingCodeLetters);
-      generateCode.onCall(1).returns(pendingCodeNumbers);
-      generateCode.onCall(2).returns('YTREZA');
-      generateCode.onCall(3).returns('543');
-
-      // when
-      const generatedCode = await generate(accessCodeRepository, [pendingCode], { generateCode });
-
-      // then
-      expect(generatedCode).to.not.equal(pendingCode);
-      expect(generatedCode).to.equal('YTREZA543');
     });
   });
 });
