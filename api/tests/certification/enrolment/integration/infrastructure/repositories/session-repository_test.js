903f01372763d461d92a48fe8e9810375ec2243d
Lou Goubin <103123143+lgoubin@users.noreply.github.com>
2026-07-03 15:01:48 +0200

    chore(api): linter

--- api/tests/certification/enrolment/integration/infrastructure/repositories/session-repository_test.js
+++ api/tests/certification/enrolment/integration/infrastructure/repositories/session-repository_test.js
@@ -1,12 +1,11 @@
-import { SessionEnrolment } from "../../../../../../src/certification/enrolment/domain/models/SessionEnrolment.js";
-import * as sessionRepository
-  from "../../../../../../src/certification/enrolment/infrastructure/repositories/session-repository.js";
-import { CERTIFICATION_CENTER_TYPES } from "../../../../../../src/shared/domain/constants.js";
-import { NotFoundError } from "../../../../../../src/shared/domain/errors.js";
-import { expect } from "../../../../../test-helper.js";
-import { databaseBuilder, knex } from "../../../../../tooling/databases.js";
-import { domainBuilder } from "../../../../../tooling/domain-builder/domain-builder.js";
-import { catchErr } from "../../../../../tooling/test-utils/error.js";
+import { SessionEnrolment } from '../../../../../../src/certification/enrolment/domain/models/SessionEnrolment.js';
+import * as sessionRepository from '../../../../../../src/certification/enrolment/infrastructure/repositories/session-repository.js';
+import { CERTIFICATION_CENTER_TYPES } from '../../../../../../src/shared/domain/constants.js';
+import { NotFoundError } from '../../../../../../src/shared/domain/errors.js';
+import { expect } from '../../../../../test-helper.js';
+import { databaseBuilder, knex } from '../../../../../tooling/databases.js';
+import { domainBuilder } from '../../../../../tooling/domain-builder/domain-builder.js';
+import { catchErr } from '../../../../../tooling/test-utils/error.js';
 
 describe('Integration | Repository | certification | enrolment | SessionEnrolment', function () {
   describe('#save', function () {
