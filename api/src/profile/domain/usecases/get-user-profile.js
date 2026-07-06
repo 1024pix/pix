5f67f8967343d9d7f674e432d200b2a57f6fce00
Fael BASSETTI <fael.bassetti@pix.fr>
2026-07-06 17:25:02 +0200

    DELETEME: add error in get-user-profile usecase

--- api/src/profile/domain/usecases/get-user-profile.js
+++ api/src/profile/domain/usecases/get-user-profile.js
@@ -2,6 +2,8 @@ import _ from 'lodash';
 
 import { Scorecard } from '../../../evaluation/domain/models/Scorecard.js';
 import { constants } from '../../../shared/domain/constants.js';
+import { logger } from '../../../shared/infrastructure/utils/logger.js';
+import { AttestationNotFoundError } from '../errors.js';
 
 const getUserProfile = async function ({
   userId,
@@ -17,6 +19,15 @@ const getUserProfile = async function ({
     competenceEvaluationRepository.findByUserId(userId),
   ]);
   const allAreas = await areaRepository.list({ locale });
+
+  const errorProbability = Math.random();
+  if (errorProbability > 0.8) {
+    throw new Error('unhandled error')
+  }
+  if (errorProbability > 0.6) {
+    throw new AttestationNotFoundError();
+  }
+  logger.info('ERROR PROBABILITY', errorProbability);
 
   const scorecards = _.map(competences, (competence) => {
     const competenceId = competence.id;
