8e71d26dcf864a06781a673381cda6f412d84efa
Gilles Vieira <vieira.gille@gmail.com>
2026-07-06 10:53:21 +0200

    feat: remove down option
    Signed-off-by: Gilles Vieira <vieira.gille@gmail.com>

--- api/db/migrations/20260609150710_drop_fk_constraint_answer_on_ke.js
+++ api/db/migrations/20260609150710_drop_fk_constraint_answer_on_ke.js
@@ -1,3 +1,5 @@
+import { logger } from '../../src/shared/infrastructure/utils/logger.js';
+
 const TABLE_NAME = 'knowledge-elements';
 
 const up = async function (knex) {
@@ -6,10 +8,12 @@ const up = async function (knex) {
   });
 };
 
-const down = async function (knex) {
-  await knex.schema.table(TABLE_NAME, function (table) {
-    table.foreign('answerId').references('answers.id');
-  });
+const down = function () {
+  logger.info(
+    'No down function => it will failed as we have to many ke and answers, and also missing answers.',
+    'In case of revert, we will use another process, or scripts dedicated to that only.',
+    'It will include the rollback of the answers then it will add the FK',
+  );
 };
 
 export { down, up };
