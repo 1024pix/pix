97bfe0ae0ea9d5e7d2a1e7362c19a770d1ad2cf7
Quentin Lebouc <quentin.lebouc@pix.fr>
2026-07-07 16:17:51 +0200

    feat(api): add a session ID param to refresh token
    Signed-off-by: Quentin Lebouc <quentin.lebouc@pix.fr>

--- api/src/identity-access-management/domain/models/RefreshToken.js
+++ api/src/identity-access-management/domain/models/RefreshToken.js
@@ -1,21 +1,36 @@
 import crypto from 'node:crypto';
 
+import Joi from 'joi';
+
 import { config } from '../../../shared/config.js';
+import { validateEntity } from '../../../shared/domain/validators/entity-validator.js';
 
 const SEPARATOR = ':';
 
 export class RefreshToken {
-  constructor({ userId, source, value, audience }) {
+  constructor({ userId, value, audience, sessionId, source }) {
     this.userId = userId;
-    this.source = source;
     this.value = value;
     this.audience = audience;
+    this.sessionId = sessionId;
+    this.source = source;
+
+    validateEntity(
+      Joi.object({
+        userId: Joi.number().required(),
+        value: Joi.string().required(),
+        audience: Joi.string().required(),
+        sessionId: Joi.string().required(),
+        source: Joi.string().optional(),
+      }),
+      this,
+    );
   }
 
-  static generate({ userId, source, audience }) {
+  static generate({ userId, source, audience, sessionId }) {
     const uuid = crypto.randomUUID();
     const value = [userId, uuid].filter(Boolean).join(SEPARATOR);
-    return new RefreshToken({ userId, source, value, audience });
+    return new RefreshToken({ userId, source, value, audience, sessionId });
   }
 
   get expirationDelaySeconds() {
