edd034c7014a3ab63f28685943ab90983417db11
Nicolas Marsal <3530655+hakkanicko@users.noreply.github.com>
2026-07-08 18:18:12 +0200

    feat: add fs instrumentation

--- api/src/shared/infrastructure/open-telemetry/initialize-open-telemetry.js
+++ api/src/shared/infrastructure/open-telemetry/initialize-open-telemetry.js
@@ -1,5 +1,6 @@
 import { diag, DiagLogLevel } from '@opentelemetry/api';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { FsInstrumentation } from '@opentelemetry/instrumentation-fs';
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { PgInstrumentation } from '@opentelemetry/instrumentation-pg';
 import { UndiciInstrumentation } from '@opentelemetry/instrumentation-undici';
@@ -15,54 +16,58 @@ import { scalingoDetector } from './scalingo-detector.js';
 const { resourceFromAttributes } = resources;
 
 export function initializeOpenTelemetry(serviceName) {
-  if (config.logging.otelEnabled) {
-    diag.setLogger(
-      {
-        ...logger,
-        verbose: logger.debug,
-      },
-      DiagLogLevel.ERROR,
-    );
-
-    const exporter = new OTLPTraceExporter();
-
-    const sdk = new NodeSDK({
-      resource: resourceFromAttributes({
-        [ATTR_SERVICE_NAME]: serviceName,
-      }),
-      resourceDetectors: [envDetector, hostDetector, osDetector, processDetector, containerDetector, scalingoDetector],
-      traceExporter: exporter,
-      instrumentations: [
-        new HttpInstrumentation(),
-        new UndiciInstrumentation({
-          requestHook(span, request) {
-            span.updateName(`${request.method} ${request.origin}${request.path}`);
-          },
-        }),
-        new PgInstrumentation({
-          requireParentSpan: true,
-          enhancedDatabaseReporting: true,
-        }),
-      ],
-    });
-
-    try {
-      sdk.start();
-      logger.info('OpenTelemetry initialized');
-    } catch (error) {
-      logger.error('Error initializing OpenTelemetry', error);
-    }
-
-    async function shutdown() {
-      try {
-        await sdk.shutdown();
-        logger.info('OpenTelemetry shut down');
-      } catch (error) {
-        logger.error('Error shutting down OpenTelemetry', error);
-      }
-    }
-
-    process.on('SIGTERM', shutdown);
-    process.on('SIGINT', shutdown);
+  if (!config.logging.otelEnabled) {
+    return;
   }
+  diag.setLogger(
+    {
+      ...logger,
+      verbose: logger.debug,
+    },
+    DiagLogLevel.DEBUG,
+  );
+
+  const exporter = new OTLPTraceExporter();
+
+  const sdk = new NodeSDK({
+    resource: resourceFromAttributes({
+      [ATTR_SERVICE_NAME]: serviceName,
+    }),
+    resourceDetectors: [envDetector, hostDetector, osDetector, processDetector, containerDetector, scalingoDetector],
+    traceExporter: exporter,
+    instrumentations: [
+      new HttpInstrumentation(),
+      new UndiciInstrumentation({
+        requestHook(span, request) {
+          span.updateName(`${request.method} ${request.origin}${request.path}`);
+        },
+      }),
+      new PgInstrumentation({
+        requireParentSpan: true,
+        enhancedDatabaseReporting: true,
+      }),
+      new FsInstrumentation({
+        requireParentSpan: true,
+      }),
+    ],
+  });
+
+  try {
+    sdk.start();
+    logger.info('OpenTelemetry initialized');
+  } catch (error) {
+    logger.error('Error initializing OpenTelemetry', error);
+  }
+
+  async function shutdown() {
+    try {
+      await sdk.shutdown();
+      logger.info('OpenTelemetry shut down');
+    } catch (error) {
+      logger.error('Error shutting down OpenTelemetry', error);
+    }
+  }
+
+  process.on('SIGTERM', shutdown);
+  process.on('SIGINT', shutdown);
 }
