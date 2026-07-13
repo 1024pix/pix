2841299342027287ef4422cc73121074d4b853f9
Matthias FERRAINA <matthias.ferraina@pix.fr>
2026-07-13 17:45:46 +0200

    :zap:(api): transform POST to PUT

--- api/src/devcomp/application/user-campaign-surveys/user-campaign-survey-controller.js
+++ api/src/devcomp/application/user-campaign-surveys/user-campaign-survey-controller.js
@@ -2,11 +2,26 @@ import { usecases } from '../../domain/usecases/index.js';
 
 async function saveUserCampaignSurvey(request, h) {
   const { userId } = request.auth.credentials;
-  const { 'campaign-id': campaignId, 'satisfaction-score': satisfactionScore } = request.payload.data.attributes;
+  const {
+    'campaign-id': campaignId,
+    'satisfaction-score': satisfactionScore,
+    'usefulness-score': usefulnessScore,
+    'personalization-score': personalizationScore,
+    'attractiveness-score': attractivenessScore,
+    comment,
+  } = request.payload.data.attributes;
 
-  const id = await usecases.saveUserCampaignSurvey({ userId, campaignId, satisfactionScore });
+  const id = await usecases.saveUserCampaignSurvey({
+    userId,
+    campaignId,
+    satisfactionScore,
+    usefulnessScore,
+    personalizationScore,
+    attractivenessScore,
+    comment,
+  });
 
-  return h.response({ data: { id: String(id), type: 'user-campaign-surveys' } }).created();
+  return h.response({ data: { id: String(id), type: 'user-campaign-surveys' } });
 }
 
 async function verifyExistingUserCampaignSurvey(request, h) {
