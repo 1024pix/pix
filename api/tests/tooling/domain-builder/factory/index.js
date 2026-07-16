1ef0aaf81e2d010fda887d656c6fe2115183f833
laura bergoens <laura.bergoens@pix.fr>
2026-07-16 09:59:56 +0200

    tech(api): elmo fire
    Co-authored-by: Alexandre Coin <alexandre@mbpdealexandre-1.home>
    Co-authored-by: Gwenvael Laskar <gwenvael.laskar@pix.fr>

--- api/tests/tooling/domain-builder/factory/index.js
+++ api/tests/tooling/domain-builder/factory/index.js
@@ -164,9 +164,7 @@ import { buildValidation } from './build-validation.js';
 import { buildValidator } from './build-validator.js';
 import { buildComplementaryCertification } from './certification/complementary-certification/build-complementary-certification.js';
 import { buildComplementaryCertificationBadge } from './certification/complementary-certification/build-complementary-certification-badge.js';
-import { buildActiveCalibratedChallenge } from './certification/configuration/build-active-calibrated-challenge.js';
 import { buildCenter as buildConfigurationCenter } from './certification/configuration/build-center.js';
-import { buildCertificationFrameworksChallenge } from './certification/configuration/build-certification-frameworks-challenge.js';
 import { buildCertificationInfo } from './certification/configuration/build-certification-info.js';
 import { buildFrameworkHistoryEntry } from './certification/configuration/build-framework-history-entry.js';
 import {
@@ -255,9 +253,7 @@ const banner = {
 
 const certification = {
   configuration: {
-    buildActiveCalibratedChallenge,
     buildCenter: buildConfigurationCenter,
-    buildCertificationFrameworksChallenge,
     buildFrameworkHistoryEntry,
     buildCertificationInfo,
     buildVersion: buildConfigurationVersion,
