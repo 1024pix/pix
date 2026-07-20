6fd970d5f016d117d57ae8818b2125b9aec3aa7f
Thomas Bazin <thomas.bazin@pix.fr>
2026-07-20 09:46:34 +0200

    chore(admin): remove RSVP.hash from certification center get route

--- admin/app/routes/authenticated/certification-centers/get.js
+++ admin/app/routes/authenticated/certification-centers/get.js
@@ -1,7 +1,6 @@
 import { action } from '@ember/object';
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
-import RSVP from 'rsvp';
 
 export default class CertificationCentersGetRoute extends Route {
   @service store;
@@ -10,10 +9,10 @@ export default class CertificationCentersGetRoute extends Route {
     const certificationCenter = await this.store.findRecord('certification-center', params.certification_center_id);
     const habilitations = await this.store.findAll('complementary-certification');
 
-    return RSVP.hash({
+    return {
       certificationCenter,
       habilitations,
-    });
+    };
   }
 
   @action
