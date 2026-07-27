fa522b8abe82296ec90b3ed2fd35c1b49885ba3c
Benjamin Petetot <bpetetot@gmail.com>
2026-07-27 15:24:49 +0200

    refactor(api): move CampaignAuthorization and prescriber-role-repository to prescription/campaign

--- api/src/prescription/campaign/application/usecases/checkAuthorizationToAccessCampaign.js
+++ api/src/prescription/campaign/application/usecases/checkAuthorizationToAccessCampaign.js
@@ -1,5 +1,5 @@
-import { CampaignAuthorization } from '../../../../shared/application/pre-handlers/CampaignAuthorization.js';
-import * as prescriberRoleRepository from '../../../../shared/infrastructure/repositories/prescriber-role-repository.js';
+import { CampaignAuthorization } from '../../domain/read-models/CampaignAuthorization.js';
+import * as prescriberRoleRepository from '../../infrastructure/repositories/prescriber-role-repository.js';
 
 const execute = async function ({ userId, campaignId }) {
   const prescriberRole = await prescriberRoleRepository.getForCampaign({ userId, campaignId });
