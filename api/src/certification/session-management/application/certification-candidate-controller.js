94eadd43fac131f5410e168e460e751fb7586e17
laura bergoens <laura.bergoens@pix.fr>
2026-07-27 22:02:24 +0200

    tech(api): push events when relevant

--- api/src/certification/session-management/application/certification-candidate-controller.js
+++ api/src/certification/session-management/application/certification-candidate-controller.js
@@ -1,23 +1,27 @@
 import { usecases } from '../domain/usecases/index.js';
+import * as eventAdapter from '../infrastructure/adapters/event-adapter.js';
 import * as supervisedCandidateRepository from '../infrastructure/repositories/supervised-candidate-repository.js';
 
-async function authorizeToStart(request, h, dependencies = { supervisedCandidateRepository }) {
-  const certificationCandidateForSupervisingId = request.params.certificationCandidateId;
+async function authorizeToStart(request, h, dependencies = { supervisedCandidateRepository, eventAdapter }) {
+  const candidateId = request.params.certificationCandidateId;
   const authorizedToStart = request.payload['authorized-to-start'];
 
   if (authorizedToStart) {
-    await dependencies.supervisedCandidateRepository.authorizeToStart(certificationCandidateForSupervisingId);
+    const authorizedToStartAt = await dependencies.supervisedCandidateRepository.authorizeToStart(candidateId);
+    await dependencies.eventAdapter.onCandidateAuthorizedToStart({ candidateId, authorizedToStartAt });
   } else {
-    await dependencies.supervisedCandidateRepository.unauthorizeToStart(certificationCandidateForSupervisingId);
+    await dependencies.supervisedCandidateRepository.unauthorizeToStart(candidateId);
+    await dependencies.eventAdapter.onCandidateUnauthorizedToStart({ candidateId });
   }
 
   return h.response().code(204);
 }
 
-async function authorizeToResume(request, h, dependencies = { supervisedCandidateRepository }) {
-  const certificationCandidateId = request.params.certificationCandidateId;
+async function authorizeToResume(request, h, dependencies = { supervisedCandidateRepository, eventAdapter }) {
+  const candidateId = request.params.certificationCandidateId;
 
-  await dependencies.supervisedCandidateRepository.authorizeToStart(certificationCandidateId);
+  const authorizedToStartAt = await dependencies.supervisedCandidateRepository.authorizeToStart(candidateId);
+  await dependencies.eventAdapter.onCandidateAuthorizedToResume({ candidateId, authorizedToStartAt });
 
   return h.response().code(204);
 }
