eae143a705271ffb307bde43df1fa60cbf239d95
Gilles Vieira <vieira.gille@gmail.com>
2026-07-28 18:31:11 +0200

    feat: remove fk asnswer from flash-assessment-results
    Signed-off-by: Gilles Vieira <vieira.gille@gmail.com>

---
+++ api/db/migrations/20260728161125_remove-answer-fk-from-flash-assessment-results.js
@@ -0,0 +1,38 @@
+// Make sure you properly test your migration, especially DDL (Data Definition Language)
+// ! If the target table is large, and the migration take more than 20 minutes, the deployment will fail !
+
+// You can design and test your migration to avoid this by following this guide
+// https://1024pix.atlassian.net/wiki/spaces/EDTDT/pages/3849323922/Cr+er+une+migration
+
+// If your migrations target :
+//
+// `answers`
+// `knowledge-elements`
+// `knowledge-element-snapshots`
+//
+// contact @team-captains, because automatic migrations are not active on `pix-datawarehouse-production`
+// this may prevent data replication to succeed the day after your migration is deployed on `pix-api-production`
+const TABLE_NAME = 'flash-assessment-results';
+const COLUMN_NAME = 'answerId';
+const REF_TABLE_NAME = 'answers';
+const REF_COLUMN_NAME = 'id';
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = async function (knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.dropForeign(COLUMN_NAME);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  await knex.schema.alterTable(TABLE_NAME, (table) => {
+    table.foreign(COLUMN_NAME).references(REF_COLUMN_NAME).inTable(REF_TABLE_NAME);
+  });
+}
