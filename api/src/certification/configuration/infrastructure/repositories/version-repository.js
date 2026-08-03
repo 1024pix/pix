c7fcbf3026cbe3b0c8d925563fe8e60c3118a182
laura bergoens <laura.bergoens@pix.fr>
2026-08-03 06:37:18 +0200

    tech(api): refacto repository so it does not throw

--- api/src/certification/configuration/infrastructure/repositories/version-repository.js
+++ api/src/certification/configuration/infrastructure/repositories/version-repository.js
@@ -4,7 +4,6 @@
  * @typedef {import("../../../../shared/domain/models/Challenge.js").Challenge} Challenge
  */
 import { DomainTransaction } from '../../../../shared/domain/DomainTransaction.js';
-import { NotFoundError } from '../../../../shared/domain/errors.js';
 import { FlashAssessmentAlgorithmConfiguration } from '../../../shared/domain/models/FlashAssessmentAlgorithmConfiguration.js';
 import { Version } from '../../domain/models/Version.js';
 
@@ -20,14 +19,13 @@ export async function findAll() {
 /**
  * @param {object} params
  * @param {number} params.id
- * @returns {Promise<Version>}
- * @throws {NotFoundError}
+ * @returns {Promise<Version|null>}
  */
 export async function getById({ id }) {
   const versionData = await buildBaseQuery().where({ id }).first();
 
   if (!versionData) {
-    throw new NotFoundError(`Version with id ${id} not found`);
+    return null;
   }
 
   return _toDomain(versionData);
