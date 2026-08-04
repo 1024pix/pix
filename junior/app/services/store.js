bee6c2a7e5bd12691236a206d21d3dbfecace316
Eric Lim <eric.lim@pix.fr>
2026-08-04 18:09:45 +0200

    feat(junior): create json handler
    Co-authored-by: Baptiste H <baptiste.hetet@pix.fr>
    Co-authored-by: Alice Goarnisson <alice.goarnisson@pix.fr>
    Co-authored-by: rouxxi <gwenvael.laskar@pix.fr>

--- junior/app/services/store.js
+++ junior/app/services/store.js
@@ -1,9 +1,11 @@
 import { JSONAPICache } from '@warp-drive/json-api';
 import { useLegacyStore } from '@warp-drive/legacy';
 
+import { JsonHandler } from '../handlers/json-handler';
+
 export default useLegacyStore({
   linksMode: false,
   cache: JSONAPICache,
-  handlers: [],
+  handlers: [JsonHandler],
   schemas: [],
 });
