874b8283adb56cd15386ea48f2ca4c35ff848400
Benjamin Petetot <bpetetot@gmail.com>
2026-08-06 18:49:14 +0200

    refactor(api): add specific sco account recovery to prescription/organization-learner

--- api/tests/prescription/organization-learner/unit/application/sco-organization-learner-controller_test.js
+++ api/tests/prescription/organization-learner/unit/application/sco-organization-learner-controller_test.js
@@ -133,4 +133,61 @@ describe('Prescription | Unit | Application | Controller | sco-organization-lear
       });
     });
   });
+
+  describe('#checkScoAccountRecovery', function () {
+    const userId = 2;
+    let request = {
+      auth: { credentials: { userId } },
+      payload: { data: { attributes: {} } },
+    };
+
+    beforeEach(function () {
+      sinon.stub(usecases, 'checkScoAccountRecovery');
+      usecases.checkScoAccountRecovery.resolves();
+    });
+
+    it('should return student account information serialized', async function () {
+      // given
+      const studentInformationForAccountRecoverySerializer = {
+        serialize: sinon.stub(),
+        deserialize: sinon.stub(),
+      };
+      hFake.request = { path: {} };
+      const studentInformation = {
+        ineIna: '1234567890A',
+        firstName: 'Bob',
+        lastName: 'Camond',
+        birthdate: '2001-12-08',
+      };
+      request = {
+        payload: {
+          data: {
+            type: 'student-information',
+            attributes: {
+              'ine-ina': studentInformation.ineIna,
+              'first-name': studentInformation.firstName,
+              'last-name': studentInformation.lastName,
+              birthdate: studentInformation.birthdate,
+            },
+          },
+        },
+      };
+      const studentInformationForAccountRecovery = Symbol();
+      const studentInformationForAccountRecoveryJSONAPI = Symbol();
+
+      studentInformationForAccountRecoverySerializer.deserialize.withArgs(request.payload).resolves(studentInformation);
+      usecases.checkScoAccountRecovery.withArgs({ studentInformation }).resolves(studentInformationForAccountRecovery);
+      studentInformationForAccountRecoverySerializer.serialize
+        .withArgs(studentInformationForAccountRecovery)
+        .returns(studentInformationForAccountRecoveryJSONAPI);
+
+      // when
+      const response = await scoOrganizationLearnerController.checkScoAccountRecovery(request, hFake, {
+        studentInformationForAccountRecoverySerializer,
+      });
+
+      // then
+      expect(response.source).to.deep.equal(studentInformationForAccountRecoveryJSONAPI);
+    });
+  });
 });
---
+++ api/tests/prescription/organization-learner/unit/infrastructure/serializers/jsonapi/student-information-for-account-recovery.serializer.test.js
@@ -0,0 +1,94 @@
+import { StudentInformationForAccountRecovery } from '../../../../../../../src/prescription/organization-learner/domain/read-models/StudentInformationForAccountRecovery.js';
+import { studentInformationForAccountRecoverySerializer } from '../../../../../../../src/prescription/organization-learner/infrastructure/serializers/jsonapi/student-information-for-account-recovery.serializer.js';
+import { expect } from '../../../../../../test-helper.js';
+
+describe('Unit | Serializer | JSONAPI | student-information-for-account-recovery', function () {
+  describe('#serialize()', function () {
+    it('converts a StudentInformationForAccountRecovery model object into JSON API data', function () {
+      // given
+      const modelStudentInformationForAccountRecovery = new StudentInformationForAccountRecovery({
+        firstName: 'Jude',
+        lastName: 'Law',
+        username: 'jude.law0106',
+        email: 'judelaw@example.net',
+        latestOrganizationName: 'Hollywood',
+      });
+
+      // when
+      const json = studentInformationForAccountRecoverySerializer.serialize(modelStudentInformationForAccountRecovery);
+
+      // then
+      const expectedJsonApi = {
+        data: {
+          type: 'student-information-for-account-recoveries',
+          attributes: {
+            'first-name': modelStudentInformationForAccountRecovery.firstName,
+            'last-name': modelStudentInformationForAccountRecovery.lastName,
+            username: modelStudentInformationForAccountRecovery.username,
+            email: modelStudentInformationForAccountRecovery.email,
+            'latest-organization-name': modelStudentInformationForAccountRecovery.latestOrganizationName,
+          },
+        },
+      };
+      expect(json).to.deep.equal(expectedJsonApi);
+    });
+  });
+
+  describe('#deserialize()', function () {
+    it('converts the payload json to student information', async function () {
+      // given
+      const payload = {
+        data: {
+          type: 'student-information-for-account-recoveries',
+          attributes: {
+            'ine-ina': '123456789BB',
+            'first-name': 'george',
+            'last-name': 'de cambridge',
+            birthdate: '2013-07-22',
+            email: 'email@example.net',
+          },
+        },
+      };
+
+      // when
+      const json = await studentInformationForAccountRecoverySerializer.deserialize(payload);
+
+      // then
+      const expectedJsonApi = {
+        firstName: 'george',
+        lastName: 'de cambridge',
+        ineIna: '123456789BB',
+        birthdate: '2013-07-22',
+        email: 'email@example.net',
+      };
+      expect(json).to.deep.equal(expectedJsonApi);
+    });
+
+    it('does not return undefined email when none exist in payload', async function () {
+      // given
+      const payload = {
+        data: {
+          type: 'student-information-for-account-recoveries',
+          attributes: {
+            'ine-ina': '123456789BB',
+            'first-name': 'george',
+            'last-name': 'de cambridge',
+            birthdate: '2013-07-22',
+          },
+        },
+      };
+
+      // when
+      const json = await studentInformationForAccountRecoverySerializer.deserialize(payload);
+
+      // then
+      const expectedJsonApi = {
+        firstName: 'george',
+        lastName: 'de cambridge',
+        ineIna: '123456789BB',
+        birthdate: '2013-07-22',
+      };
+      expect(json).to.deep.equal(expectedJsonApi);
+    });
+  });
+});
