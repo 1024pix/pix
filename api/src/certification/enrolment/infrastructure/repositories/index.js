a676b06acfa4de2f21f178af7e6a24f271378bc8
Lou Goubin <103123143+lgoubin@users.noreply.github.com>
2026-08-11 15:34:07 +0200

    refactor(api): clean dead import

--- api/src/certification/enrolment/infrastructure/repositories/index.js
+++ api/src/certification/enrolment/infrastructure/repositories/index.js
@@ -1,5 +1,4 @@
 import * as centerRepository from '../../../../organizational-entities/infrastructure/repositories/center-repository.js';
-import * as dataProtectionOfficerRepository from '../../../../organizational-entities/infrastructure/repositories/data-protection-officer.repository.js';
 import { injectDependencies } from '../../../../shared/infrastructure/utils/dependency-injection.js';
 import * as sessionManagementRepository from '../../../session-management/infrastructure/repositories/session-management-repository.js';
 import * as certificationCenterRepository from '../../../shared/infrastructure/repositories/certification-center-repository.js';
@@ -26,7 +25,6 @@ import * as sessionRepository from './session-repository.js';
  * @typedef {certificationCpfCityRepository} CertificationCpfCityRepository
  * @typedef {sessionForAttendanceSheetRepository} SessionForAttendanceSheetRepository
  * @typedef {sessionManagementRepository} SessionManagementRepository
- * @typedef {dataProtectionOfficerRepository} DataProtectionOfficerRepository
  * @typedef {userRepository} UserRepository
  * @typedef {targetProfileHistoryRepository} TargetProfileHistoryRepository
  * @typedef {complementaryCertificationCourseRepository} ComplementaryCertificationCourseRepository
@@ -40,7 +38,6 @@ const repositoriesWithoutInjectedDependencies = {
   divisionRepository,
   certificationCpfCountryRepository,
   certificationCpfCityRepository,
-  dataProtectionOfficerRepository,
   sessionForAttendanceSheetRepository,
   sessionManagementRepository,
   sessionRepository,
