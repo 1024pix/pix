beb056836e7089bade0f5f5a399734000e5346af
Alexandre COIN <alexandre.pc.coin@gmail.com>
2026-08-14 11:27:12 +0200

    refactor(api): remove thrown SessionNotFoundError from session-for-attendance-sheet-repository getWithCertificationCandidates

--- api/tests/certification/enrolment/unit/domain/usecases/get-attendance-sheet_test.js
+++ api/tests/certification/enrolment/unit/domain/usecases/get-attendance-sheet_test.js
@@ -1,19 +1,48 @@
 import sinon from 'sinon';
 
 import { getAttendanceSheet } from '../../../../../../src/certification/enrolment/domain/usecases/get-attendance-sheet.js';
+import { NotFoundError } from '../../../../../../src/shared/domain/errors.js';
 import { expect } from '../../../../../test-helper.js';
+import { catchErr } from '../../../../../tooling/test-utils/error.js';
 
 describe('Unit | UseCase | get-attendance-sheet', function () {
   describe('getAttendanceSheet', function () {
+    describe('when no session is found', function () {
+      it('throws a SessionNotFound error', async function () {
+        // given
+        const userId = 'dummyUserId';
+        const i18n = 'dummyi18n';
+        const sessionForAttendanceSheetRepository = { getWithCertificationCandidates: sinon.stub() };
+
+        sessionForAttendanceSheetRepository.getWithCertificationCandidates.withArgs({ id: 1 }).resolves(null);
+
+        const attendanceSheetPdfUtilsStub = {
+          getAttendanceSheetPdfBuffer: sinon.stub(),
+        };
+
+        // when
+        const error = await catchErr(getAttendanceSheet)({
+          userId,
+          sessionId: 1,
+          i18n,
+          sessionForAttendanceSheetRepository,
+          attendanceSheetPdfUtils: attendanceSheetPdfUtilsStub,
+        });
+
+        // then
+        expect(error).to.deepEqualInstance(
+          new NotFoundError("La session n'existe pas ou aucun candidat n'est inscrit à celle-ci"),
+        );
+      });
+    });
+
     it('should return the attendance sheet in pdf format', async function () {
       // given
       const userId = 'dummyUserId';
       const i18n = 'dummyi18n';
-      const sessionRepository = { doesUserHaveCertificationCenterMembershipForSession: sinon.stub() };
       const sessionForAttendanceSheetRepository = { getWithCertificationCandidates: sinon.stub() };
       const session = _buildSessionWithCandidate('SUP', true);
 
-      sessionRepository.doesUserHaveCertificationCenterMembershipForSession.resolves(true);
       sessionForAttendanceSheetRepository.getWithCertificationCandidates.withArgs({ id: 1 }).resolves(session);
 
       const pdfBuffer = Buffer.from('some pdf file');
@@ -31,7 +60,6 @@ describe('Unit | UseCase | get-attendance-sheet', function () {
         userId,
         sessionId: 1,
         i18n,
-        sessionRepository,
         sessionForAttendanceSheetRepository,
         attendanceSheetPdfUtils: attendanceSheetPdfUtilsStub,
       });
