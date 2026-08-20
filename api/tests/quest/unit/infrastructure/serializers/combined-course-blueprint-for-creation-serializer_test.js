b055c8e66ea2ee939ea50396682af29a7f4731ab
Sebastien Seguin <80358666+sebaseg-dev@users.noreply.github.com>
2026-08-20 07:21:47 +0000

    [BUGFIX] Ajouter une validation des données lors de la création d'un schéma de parcours combiné (PIX-23906) (#17162)
    ## ☀️ Problème
    
    Lorsque l’on crée un schéma de parcours combiné avec une mauvaise URL de
    questionnaire de fin de parcours, on ne peut pas créer de parcours
    combiné côté Pix Orga.
    
    ## ⛱️ Proposition
    
    Ajouter une validation des données côté API pour s'assurer du formattage
    des URL.
    
    ## 🧴 Remarques
    
    Lors des travaux, nous avons remarqué que le modèle
    CombinedCourseBlueprintForUpdate pourrait bénéficier lui aussi d'une
    validation des données. Ticket ajouté en dette technique.
    
    ## 🏊 Pour tester
    
    1. Dans Pix Admin, créer un nouveau schéma de parcours combiné
    2. Renseigner un nom externe, un nom interne et "survey-link" dans le
    champ "Url du questionnaire de fin de parcours"
    3. Valider la création -> ❌ un toast d'erreur s'affiche et empêche la
    création du schéma
    4. Remplacer "survey-link" par "http://survey-link.pix" -> ✅ Le schéma
    est créé

--- api/tests/quest/unit/infrastructure/serializers/combined-course-blueprint-for-creation-serializer_test.js
+++ api/tests/quest/unit/infrastructure/serializers/combined-course-blueprint-for-creation-serializer_test.js
@@ -11,7 +11,7 @@ import {
 import { combinedCourseBlueprintForCreationSerializer } from '../../../../../src/quest/infrastructure/serializers/combined-course-blueprint-for-creation-serializer.js';
 import { expect } from '../../../../test-helper.js';
 
-describe('Quest | Unit | Infrastructure | Serializers | admin-combined-course-blueprint', function () {
+describe('Quest | Unit | Infrastructure | Serializers | combined-course-blueprint-for-creation-serializer', function () {
   it('#deserialize', async function () {
     const date = new Date();
     const moduleId = 'eeeb4951-6f38-4467-a4ba-0c85ed71321a';
@@ -25,7 +25,7 @@ describe('Quest | Unit | Infrastructure | Serializers | admin-combined-course-bl
         attributes: {
           name: 'Mon parcours',
           'internal-name': 'Mon modèle de parcours',
-          illustration: '/illustrations/image.svg',
+          illustration: 'http://example.pix/illustrations/image.svg',
           description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
           'prescriber-description': 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
           'reward-id': 5,
@@ -34,7 +34,7 @@ describe('Quest | Unit | Infrastructure | Serializers | admin-combined-course-bl
           content: items,
           'created-at': date,
           'updated-at': date,
-          'survey-link': 'http://survey',
+          'survey-link': 'http://survey.fr',
           'capped-tube-requirements': [
             {
               tubes: [
@@ -59,7 +59,7 @@ describe('Quest | Unit | Infrastructure | Serializers | admin-combined-course-bl
         id: '1',
         name: 'Mon parcours',
         internalName: 'Mon modèle de parcours',
-        illustration: '/illustrations/image.svg',
+        illustration: 'http://example.pix/illustrations/image.svg',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
         prescriberDescription: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         rewardId: 5,
@@ -117,7 +117,7 @@ describe('Quest | Unit | Infrastructure | Serializers | admin-combined-course-bl
           rewardId: 5,
           rewardType: REWARD_TYPES.ATTESTATION,
         }),
-        surveyLink: 'http://survey',
+        surveyLink: 'http://survey.fr',
         rewardRequirementsDescription: 'Description of the reward requirements',
       }),
     );
