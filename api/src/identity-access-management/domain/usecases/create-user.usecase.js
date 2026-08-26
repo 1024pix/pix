aa679a6d2dcd28745c0ce959f3c0d2d64f2dca11
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2026-08-26 15:19:14 +0200

    tech(api): adds logs for user creation validation errors
    Co-Authored-By: "Emmanuelle Bonnemay" <emmanuelle.bonnemay@pix.fr>

--- api/src/identity-access-management/domain/usecases/create-user.usecase.js
+++ api/src/identity-access-management/domain/usecases/create-user.usecase.js
@@ -1,7 +1,10 @@
 import { DomainTransaction } from '../../../shared/domain/DomainTransaction.js';
 import { EntityValidationError } from '../../../shared/domain/errors.js';
+import { child, SCOPES } from '../../../shared/infrastructure/utils/logger.js';
 import { createAccountCreationEmail } from '../emails/create-account-creation.email.js';
 import { InvalidOrAlreadyUsedEmailError } from '../errors.js';
+
+const logger = child('iam:create-user', { event: SCOPES.IAM });
 
 /**
  * @param {Object} params
@@ -154,6 +157,9 @@ async function _assertValidData({ password, user, userRepository, userValidator,
 
   if (validationErrors.some((error) => error instanceof Error)) {
     const relevantErrors = validationErrors.filter((error) => error instanceof Error);
+    for (const error of relevantErrors) {
+      logger.warn(error, 'user creation validation error');
+    }
     throw EntityValidationError.fromMultipleEntityValidationErrors(relevantErrors);
   }
 }
