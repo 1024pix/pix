f256d0111e71eb0f4a9633d1ef97c1d541a4ee10
Alexandre COIN <alexandre.pc.coin@gmail.com>
2026-08-26 15:27:44 +0200

    refactor(api): remove unreachable different scope alert

--- api/tests/certification/configuration/unit/domain/models/Calibration_test.js
+++ api/tests/certification/configuration/unit/domain/models/Calibration_test.js
@@ -1,6 +1,12 @@
 import { expect } from 'chai';
 
-import { CalibrationScoringMesh } from '../../../../../../src/certification/configuration/domain/models/Calibration.js';
+import {
+  CALIBRATION_SCOPES,
+  CalibrationScoringMesh,
+  fromCalibrationScope,
+  toCalibrationScope,
+} from '../../../../../../src/certification/configuration/domain/models/Calibration.js';
+import { SCOPES } from '../../../../../../src/certification/shared/domain/models/Scopes.js';
 import { domainBuilder } from '../../../../../tooling/domain-builder/domain-builder.js';
 
 describe('Unit | Certification | Configuration | Domain | Models | Calibration', function () {
@@ -67,6 +73,33 @@ describe('Unit | Certification | Configuration | Domain | Models | Calibration',
         .build();
 
       expect(calibration.scoringMeshes).to.have.lengthOf(1);
+    });
+  });
+
+  describe('#toCalibrationScope', function () {
+    it('translates every certification scope into its Data counterpart', function () {
+      expect(toCalibrationScope(SCOPES.CORE)).to.equal(CALIBRATION_SCOPES.COEUR);
+      expect(toCalibrationScope(SCOPES.PIX_PLUS_DROIT)).to.equal(CALIBRATION_SCOPES.DROIT);
+      expect(toCalibrationScope(SCOPES.PIX_PLUS_EDU_1ER_DEGRE)).to.equal(CALIBRATION_SCOPES.EDU_1ER_DEGRE);
+      expect(toCalibrationScope(SCOPES.PIX_PLUS_EDU_2ND_DEGRE)).to.equal(CALIBRATION_SCOPES.EDU_2ND_DEGRE);
+      expect(toCalibrationScope(SCOPES.PIX_PLUS_EDU_CPE)).to.equal(CALIBRATION_SCOPES.EDU_CPE);
+      expect(toCalibrationScope(SCOPES.PIX_PLUS_PRO_SANTE)).to.equal(CALIBRATION_SCOPES.PRO_SANTE);
+    });
+
+    it('returns undefined for an unknown scope', function () {
+      expect(toCalibrationScope('UNKNOWN')).to.be.undefined;
+    });
+  });
+
+  describe('#fromCalibrationScope', function () {
+    it('is the exact inverse of toCalibrationScope', function () {
+      for (const scope of Object.values(SCOPES)) {
+        expect(fromCalibrationScope(toCalibrationScope(scope))).to.equal(scope);
+      }
+    });
+
+    it('returns undefined for an unknown calibration scope', function () {
+      expect(fromCalibrationScope('UNKNOWN')).to.be.undefined;
     });
   });
 });
