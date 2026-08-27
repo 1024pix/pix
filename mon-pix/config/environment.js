daf4bca45b8a1d1932cff3358a7ffc244362b831
AndreiaPena <andreia.pena@pix.fr>
2026-08-27 10:18:54 +0200

    ⚡️(mon-pix): create DRAWER_REVEAL_DELAY_MS environment variable

--- mon-pix/config/environment.js
+++ mon-pix/config/environment.js
@@ -64,6 +64,11 @@ module.exports = function (environment) {
         defaultValue: 7000,
         minValue: 0,
       }),
+      DRAWER_REVEAL_DELAY_MS: _getEnvironmentVariableAsNumber({
+        environmentVariableName: 'DRAWER_REVEAL_DELAY_MS',
+        defaultValue: 15000,
+        minValue: 0,
+      }),
       BANNER_CONTENT: process.env.BANNER_CONTENT || '',
       BANNER_TYPE: process.env.BANNER_TYPE || '',
       INFORMATION_BANNER_POLLING_TIME:
