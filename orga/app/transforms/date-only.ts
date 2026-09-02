a0f5ca8e45e76b523cc5303d13ed5847d5f9b7f4
Guillaume Olejniczak <la.toile.cosmique@gmail.com>
2026-09-02 12:15:50 +0200

    tech(orga): convert date-only transform to TS

--- orga/app/transforms/date-only.ts
+++ orga/app/transforms/date-only.ts
@@ -1,11 +1,14 @@
+import { TransformName } from '@warp-drive/core/types/symbols';
 import { Transform } from '@warp-drive/legacy/serializer/transform';
 
 export default class DateOnlyTransform extends Transform {
-  serialize(date) {
+  declare [TransformName]: 'date-only';
+
+  serialize(date: string | null): string | null {
     return date;
   }
 
-  deserialize(date) {
+  deserialize(date: string | null): string | null {
     const dateRegex = '^[0-9]{4}-[0-9]{2}-[0-9]{2}$';
     if (date && date.search(dateRegex) === 0) {
       return date;
