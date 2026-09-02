30891b034de1e7b4157833a2065058727d380707
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2026-09-02 12:04:22 +0200

    fix: use redis storage for feature toggle in integration test

--- api/src/shared/infrastructure/feature-toggles/index.js
+++ api/src/shared/infrastructure/feature-toggles/index.js
@@ -1,7 +1,6 @@
 import config from '../../../../config/feature-toggles-config.js';
 import { config as appConfig } from '../../config.js';
 import { featureTogglesStorage } from '../key-value-storages/index.js';
-import { InMemoryKeyValueStorage } from '../key-value-storages/InMemoryKeyValueStorage.js';
 import { FeatureTogglesClient } from './feature-toggles-client.js';
 
 const isTestEnv = process.env.NODE_ENV === 'test';
@@ -15,8 +14,7 @@ let _instance = null;
 async function getInstance() {
   if (!_instance) {
     const environment = getFeatureTogglesEnv();
-    const storage = isTestEnv ? new InMemoryKeyValueStorage() : featureTogglesStorage;
-    _instance = new FeatureTogglesClient(storage, environment);
+    _instance = new FeatureTogglesClient(featureTogglesStorage, environment);
 
     await _instance.init(config);
   }
