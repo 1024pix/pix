77cbaaf267d1a167b07b7e7d8a6016552d32a36c
Guillaume Olejniczak <la.toile.cosmique@gmail.com>
2026-09-03 14:26:40 +0200

    tech(orga): convert html-unsafe to TS

--- orga/app/helpers/html-unsafe.ts
+++ orga/app/helpers/html-unsafe.ts
@@ -1,7 +1,9 @@
 import { helper } from '@ember/component/helper';
+import type { SafeString } from '@ember/template';
 import { htmlSafe as emberHtmlUnsafe } from '@ember/template';
 
-export function htmlUnsafe(text) {
+export function htmlUnsafe([text]: [string]): SafeString {
   return emberHtmlUnsafe(text);
 }
+
 export default helper(htmlUnsafe);
