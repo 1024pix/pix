a751780810dba21d4e909688859ee32c034ecdec
Guillaume Olejniczak <la.toile.cosmique@gmail.com>
2026-09-03 14:24:12 +0200

    tech(orga): remove unused helpers

--- orga/app/helpers/join.js
+++
@@ -1,7 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export function join([values, separator]) {
-  return values.join(separator);
-}
-
-export default helper(join);
--- orga/app/helpers/sum.js
+++
@@ -1,7 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export function sum(numbers) {
-  return numbers.reduce((a, b) => Number(a) + Number(b), 0);
-}
-
-export default helper(sum);
