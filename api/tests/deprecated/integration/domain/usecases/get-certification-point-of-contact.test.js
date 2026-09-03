39aa06a77b357b561a9484b95cb0e2fd4f968513
Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>
2026-09-03 15:41:59 +0200

    feat(api): get certif tos status in getCertificationPointOfContact usecase

--- api/tests/deprecated/integration/domain/usecases/get-certification-point-of-contact.test.js
+++ api/tests/deprecated/integration/domain/usecases/get-certification-point-of-contact.test.js
@@ -1,6 +1,8 @@
 import { expect } from 'chai';
 
 import { usecases } from '../../../../../src/deprecated/domain/usecases/index.js';
+import { STATUS } from '../../../../../src/legal-documents/domain/models/LegalDocumentStatus.js';
+import { featureToggles } from '../../../../../src/shared/infrastructure/feature-toggles/index.js';
 import { CERTIFICATION_CENTER_MEMBERSHIP_ROLES } from '../../../../../src/team/domain/models/CertificationCenterMembership.js';
 import { databaseBuilder } from '../../../../tooling/databases.js';
 import { domainBuilder } from '../../../../tooling/domain-builder/domain-builder.js';
@@ -8,46 +10,71 @@ import { domainBuilder } from '../../../../tooling/domain-builder/domain-builder
 const { getCertificationPointOfContact } = usecases;
 
 describe('Integration | Deprecated | Domain | UseCase | get-certification-point-of-contact', function () {
-  it('returns the CertificationPointOfContact', async function () {
-    // given
-    const user = databaseBuilder.factory.buildUser();
-    const certificationCenter = databaseBuilder.factory.buildCertificationCenter();
-    const membership = databaseBuilder.factory.buildCertificationCenterMembership({
-      userId: user.id,
-      certificationCenterId: certificationCenter.id,
-      role: CERTIFICATION_CENTER_MEMBERSHIP_ROLES.MEMBER,
+  context('when ft is not enabled', function () {
+    beforeEach(async function () {
+      await featureToggles.set('newPixCertifLegalDocumentsVersioning', false);
     });
-    await databaseBuilder.commit();
+    it('returns the CertificationPointOfContact', async function () {
+      // given
+      //const legalDocumentPublishedAt = new Date('2024-06-01');
+      const acceptedAt = new Date('2024-06-02');
 
-    // when
-    const actualCertificationPointOfContact = await getCertificationPointOfContact({
-      userId: user.id,
-    });
+      const user = databaseBuilder.factory.buildUser({
+        pixCertifTermsOfServiceAccepted: true,
+        lastPixCertifTermsOfServiceValidatedAt: acceptedAt,
+      });
+      const certificationCenter = databaseBuilder.factory.buildCertificationCenter();
+      const membership = databaseBuilder.factory.buildCertificationCenterMembership({
+        userId: user.id,
+        certificationCenterId: certificationCenter.id,
+        role: CERTIFICATION_CENTER_MEMBERSHIP_ROLES.MEMBER,
+      });
+      /*const documentVersion = databaseBuilder.factory.buildLegalDocumentVersion({
+        service: 'pix-certif',
+        type: 'TOS',
+        versionAt,
+      });
+      databaseBuilder.factory.buildLegalDocumentVersionUserAcceptance({
+        userId: user.id,
+        legalDocumentVersionId: documentVersion.id,
+        acceptedAt: new Date('2024-06-15'),
+      });*/
+      await databaseBuilder.commit();
 
-    // then
-    const expectedCertificationPointOfContact = domainBuilder.buildCertificationPointOfContact({
-      id: user.id,
-      email: user.email,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      pixCertifTermsOfServiceAccepted: user.pixCertifTermsOfServiceAccepted,
-      allowedCertificationCenterAccesses: [
-        domainBuilder.buildAllowedCertificationCenterAccess({
-          id: certificationCenter.id,
-          type: certificationCenter.type,
-          name: certificationCenter.name,
-          externalId: certificationCenter.externalId,
-        }),
-      ],
-      certificationCenterMemberships: [
-        {
-          id: membership.id,
-          certificationCenterId: certificationCenter.id,
-          userId: user.id,
-          role: membership.role,
+      // when
+      const actualCertificationPointOfContact = await getCertificationPointOfContact({
+        userId: user.id,
+      });
+
+      // then
+      const expectedCertificationPointOfContact = domainBuilder.buildCertificationPointOfContact({
+        id: user.id,
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        pixCertifTosStatus: {
+          status: STATUS.ACCEPTED,
+          acceptedAt: acceptedAt,
+          documentPath: null,
         },
-      ],
+        allowedCertificationCenterAccesses: [
+          domainBuilder.buildAllowedCertificationCenterAccess({
+            id: certificationCenter.id,
+            type: certificationCenter.type,
+            name: certificationCenter.name,
+            externalId: certificationCenter.externalId,
+          }),
+        ],
+        certificationCenterMemberships: [
+          {
+            id: membership.id,
+            certificationCenterId: certificationCenter.id,
+            userId: user.id,
+            role: membership.role,
+          },
+        ],
+      });
+      expect(actualCertificationPointOfContact).to.deep.equal(expectedCertificationPointOfContact);
     });
-    expect(actualCertificationPointOfContact).to.deep.equal(expectedCertificationPointOfContact);
   });
 });
