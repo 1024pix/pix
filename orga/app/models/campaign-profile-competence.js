162fc67748e367581ced004903cdf6fafe3e9ac5
Guillaume Olejniczak <la.toile.cosmique@gmail.com>
2026-09-04 11:11:41 +0200

    tech(orga): convert campaign models to TS

--- orga/app/models/campaign-profile-competence.js
+++
@@ -1,9 +0,0 @@
-import Model, { attr } from '@warp-drive/legacy/model';
-
-export default class CampaignProfileCompetence extends Model {
-  @attr('string') name;
-  @attr('string') index;
-  @attr('number') pixScore;
-  @attr('number') estimatedLevel;
-  @attr('string') areaColor;
-}
