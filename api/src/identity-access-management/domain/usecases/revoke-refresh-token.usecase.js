63f3cdefedfa6478846f3b880ca63f153c9a907a
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2026-09-07 20:32:24 +0200

    feat(api): revoking a JWT refresh token does nothing

--- api/src/identity-access-management/domain/usecases/revoke-refresh-token.usecase.js
+++ api/src/identity-access-management/domain/usecases/revoke-refresh-token.usecase.js
@@ -1,10 +1,15 @@
+import { RefreshToken } from '../models/RefreshToken.js';
+
 /**
  * @param {{
  *   refreshToken: string,
  *   refreshTokenRepository: RefreshTokenRepository
  * }} params
  * @return {Promise<void>}
+ * @deprecated replaced by revokeSession usecase
  */
 export const revokeRefreshToken = async function ({ refreshToken, refreshTokenRepository }) {
+  if (!RefreshToken.isStatefulRefreshToken(refreshToken)) return;
+
   await refreshTokenRepository.revokeByToken({ token: refreshToken });
 };
