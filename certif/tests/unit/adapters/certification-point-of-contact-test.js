533e9a12c37aeea34181096cc8fd6f7f1bbb2041
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2026-09-09 11:01:51 +0200

    tech(certif): remove RSVP usage

--- certif/tests/unit/adapters/certification-point-of-contact-test.js
+++ certif/tests/unit/adapters/certification-point-of-contact-test.js
@@ -1,6 +1,5 @@
 import { setupTest } from 'ember-qunit';
 import { module, test } from 'qunit';
-import { resolve } from 'rsvp';
 
 module('Unit | Adapters | certification-point-of-contact', function (hooks) {
   setupTest(hooks);
@@ -9,7 +8,7 @@ module('Unit | Adapters | certification-point-of-contact', function (hooks) {
 
   hooks.beforeEach(function () {
     adapter = this.owner.lookup('adapter:certification-point-of-contact');
-    const ajaxStub = () => resolve();
+    const ajaxStub = async () => {};
     adapter.ajax = ajaxStub;
   });
 
--- certif/tests/unit/services/feature-toggles-test.js
+++ certif/tests/unit/services/feature-toggles-test.js
@@ -2,7 +2,6 @@ import Object from '@ember/object';
 import Service from '@ember/service';
 import { setupTest } from 'ember-qunit';
 import { module, test } from 'qunit';
-import { resolve } from 'rsvp';
 
 module('Unit | Service | feature toggles', function (hooks) {
   setupTest(hooks);
@@ -12,7 +11,7 @@ module('Unit | Service | feature toggles', function (hooks) {
       // Given
       const featureToggles = Object.create({});
       const storeStub = Service.create({
-        queryRecord: () => resolve(featureToggles),
+        queryRecord: async () => featureToggles,
       });
       const featureToggleService = this.owner.lookup('service:featureToggles');
       featureToggleService.set('store', storeStub);
