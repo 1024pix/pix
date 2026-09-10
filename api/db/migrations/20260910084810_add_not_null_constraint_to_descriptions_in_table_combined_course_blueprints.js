9ecce834cb2b168ee1a1c7a3ef1527816faf8ac4
MathieuGilet <mathieu.gilet@pix.fr>
2026-09-10 11:59:34 +0200

    tech(api): add not null constraint to description and prescriberDescription in combined_course_blueprints table

---
+++ api/db/migrations/20260910084810_add_not_null_constraint_to_descriptions_in_table_combined_course_blueprints.js
@@ -0,0 +1,37 @@
+// Make sure you properly test your migration, especially DDL (Data Definition Language)
+// ! If the target table is large, and the migration take more than 20 minutes, the deployment will fail !
+
+// You can design and test your migration to avoid this by following this guide
+// https://1024pix.atlassian.net/wiki/spaces/EDTDT/pages/3849323922/Cr+er+une+migration
+
+// If your migrations target :
+//
+// `answers`
+// `knowledge-elements`
+// `knowledge-element-snapshots`
+//
+// contact @team-captains, because automatic migrations are not active on `pix-datawarehouse-production`
+// this may prevent data replication to succeed the day after your migration is deployed on `pix-api-production`
+const TABLE_NAME = 'combined_course_blueprints';
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.text('description').notNullable().alter();
+    table.text('prescriberDescription').notNullable().alter();
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.text('description').nullable().alter();
+    table.text('prescriberDescription').nullable().alter();
+  });
+}
