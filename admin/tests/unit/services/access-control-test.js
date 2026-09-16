917a5426bfd7f22f11d2c28a516f24b853c6b109
Thomas Bazin <thomas.bazin@pix.fr>
2026-09-16 16:40:57 +0200

    feat(admin): allow role metier to access attach child organizations feature

--- admin/tests/unit/services/access-control-test.js
+++ admin/tests/unit/services/access-control-test.js
@@ -290,17 +290,28 @@ module('Unit | Service | access-control', function (hooks) {
       });
     });
 
+    module(`when user has role "METIER"`, function () {
+      test(`returns "true"`, function (assert) {
+        // given
+        const currentUser = this.owner.lookup('service:currentUser');
+        const service = this.owner.lookup('service:access-control');
+        currentUser.adminMember = { isMetier: true };
+
+        // then
+        assert.true(service.hasAccessToAttachChildOrganizationActionsScope);
+      });
+    });
+
     [
-      { role: 'METIER', roleKey: 'isMetier', hasAccess: false },
-      { role: 'CERTIF', roleKey: 'isCertif', hasAccess: false },
-      { role: 'SUPPORT', roleKey: 'isSupport', hasAccess: false },
-    ].forEach(({ role, roleKey, hasAccess }) => {
+      { role: 'CERTIF', roleKey: 'isCertif' },
+      { role: 'SUPPORT', roleKey: 'isSupport' },
+    ].forEach(({ role, roleKey }) => {
       module(`when user has role "${role}"`, function () {
-        test(`returns "${hasAccess}"`, function (assert) {
+        test(`returns "false"`, function (assert) {
           // given
           const currentUser = this.owner.lookup('service:currentUser');
           const service = this.owner.lookup('service:access-control');
-          currentUser.adminMember = { [roleKey]: hasAccess };
+          currentUser.adminMember = { [roleKey]: true };
 
           // then
           assert.false(service.hasAccessToAttachChildOrganizationActionsScope);
