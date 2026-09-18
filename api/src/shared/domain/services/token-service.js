03be9400e963820e9f240bc0df776b931c052842
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2026-09-18 11:38:09 +0200

    feat(api): ensures backward compatibility for older access tokens

--- api/src/shared/domain/services/token-service.js
+++ api/src/shared/domain/services/token-service.js
@@ -45,7 +45,18 @@ function getDecodedToken(token, secret = config.authentication.secret, { expecte
   try {
     const decodedToken = jsonwebtoken.verify(token, secret, { complete: true });
 
-    Joi.assert(decodedToken.header.typ, Joi.string().valid(expectedType).required());
+    const allowedTypes = [expectedType];
+    // ensure backward compatibility for older access tokens
+    if (expectedType === tokenType.ACCESS_TOKEN) {
+      allowedTypes.push('JWT');
+    }
+
+    Joi.assert(
+      decodedToken.header.typ,
+      Joi.string()
+        .valid(...allowedTypes)
+        .required(),
+    );
 
     return decodedToken.payload;
   } catch {
