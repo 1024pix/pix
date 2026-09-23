99ec4e2857fad4a23e061549fccb18919a7b05a4
LionelB <160320+lionelB@users.noreply.github.com>
2026-09-23 15:35:12 +0200

    feat(orga): track catalogue search value

--- orga/app/controllers/authenticated/catalogue/list.js
+++ orga/app/controllers/authenticated/catalogue/list.js
@@ -1,6 +1,9 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+
+import { EVENT_NAME } from '../../../constants/metrics-event-name';
 
 export default class CatalogueListController extends Controller {
   queryParams = ['search', 'category', 'areas', 'competences', 'targetProfileId', 'blueprintId'];
@@ -11,6 +14,7 @@ export default class CatalogueListController extends Controller {
   @tracked competences = [];
   @tracked targetProfileId = null;
   @tracked blueprintId = null;
+  @service pixMetrics;
 
   get isFiltered() {
     return this.search !== '' || this.category !== '' || this.areas.length > 0 || this.competences.length > 0;
@@ -18,6 +22,9 @@ export default class CatalogueListController extends Controller {
 
   @action
   updateFilter(fieldName, value) {
+    if (fieldName === 'search' && value.trim()) {
+      this.pixMetrics.trackEvent(EVENT_NAME.CATALOGUE.SEARCH, { catalogue_search: value });
+    }
     this[fieldName] = value;
   }
 
--- orga/tests/unit/controllers/authenticated/catalogue/list-test.js
+++ orga/tests/unit/controllers/authenticated/catalogue/list-test.js
@@ -1,13 +1,18 @@
+import { EVENT_NAME } from 'pix-orga/constants/metrics-event-name';
 import { module, test } from 'qunit';
+import sinon from 'sinon';
 
 import setupIntlRenderingTest from '../../../../helpers/setup-intl-rendering';
 
 module('Unit | Controller | authenticated/catalogue/list', function (hooks) {
   setupIntlRenderingTest(hooks);
   let controller;
+  let trackEventStub;
 
   hooks.beforeEach(function () {
     controller = this.owner.lookup('controller:authenticated/catalogue/list');
+    const pixMetrics = this.owner.lookup('service:pix-metrics');
+    trackEventStub = sinon.spy(pixMetrics, 'trackEvent');
   });
 
   module('#isFiltered', function () {
@@ -74,6 +79,39 @@ module('Unit | Controller | authenticated/catalogue/list', function (hooks) {
 
       // then
       assert.strictEqual(controller.search, expectedValue);
+    });
+    test('it should call trackEvent when search filter change', async function (assert) {
+      // given
+      controller.search = '';
+      const expectedValue = 'Parcours';
+
+      // when
+      await controller.updateFilter('search', expectedValue);
+
+      // then
+      assert.ok(trackEventStub.calledOnceWithExactly(EVENT_NAME.CATALOGUE.SEARCH, { catalogue_search: expectedValue }));
+    });
+    test('it should not call trackEvent when other filter changed', async function (assert) {
+      // given
+      controller.search = '';
+      const expectedValue = ['area1'];
+
+      // when
+      await controller.updateFilter('areas', expectedValue);
+
+      // then
+      assert.ok(trackEventStub.notCalled);
+    });
+    test('it should not call trackEvent when search filter change to empty', async function (assert) {
+      // given
+      controller.search = '';
+      const expectedValue = '   ';
+
+      // when
+      await controller.updateFilter('search', expectedValue);
+
+      // then
+      assert.ok(trackEventStub.notCalled);
     });
   });
 
