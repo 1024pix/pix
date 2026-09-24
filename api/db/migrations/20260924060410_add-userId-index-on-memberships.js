b3de262422e605c39e7c8737d341c1115d251bc9
Guillaume Lagorce <guillaume.lagorce@pix.fr>
2026-09-24 08:12:44 +0200

    tech(api): add index on userId column of memberships table

---
+++ api/db/migrations/20260924060410_add-userId-index-on-memberships.js
@@ -0,0 +1,22 @@
+const TABLE_NAME = 'memberships';
+const COLUMN_NAME = 'userId';
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.index(COLUMN_NAME);
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  await knex.schema.table(TABLE_NAME, function (table) {
+    table.dropIndex(COLUMN_NAME);
+  });
+}
